3508a094c3524e29645efcd031d2a1e4d8042fc3
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-07-29 15:15:51 +0000

    Implemented ALL switch
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/dir.c
+++ workbench/c/dir.c
@@ -38,43 +38,25 @@ struct DosLibrary *DOSBase;
 struct UtilityBase *UtilityBase;
 const char dosname[]="dos.library";
 
+struct table
+{
+    char ** entries;
+    int     num, max;
+};
+
 char ** files;
 int num_files, max_files;
 char ** dirs;
 int num_dirs, max_dirs;
 
-static int strlen (const char * ptr)
+static int AddEntry (struct table * table, char * entry)
 {
-    int len=0;
-
-    while (*ptr++) len++;
-
-    return len;
-}
-
-static int AddEntry (const char * entry, int flag /* 0-dir, 1-file */)
-{
-    char *** entries;
-    int * num_entries, * max_entries;
     char * dup;
     int len;
 
-    if (flag)
+    if (table->num == table->max)
     {
-	entries = &files;
-	num_entries = &num_files;
-	max_entries = &max_files;
-    }
-    else
-    {
-	entries = &dirs;
-	num_entries = &num_dirs;
-	max_entries = &max_dirs;
-    }
-
-    if (*num_entries == *max_entries)
-    {
-	int new_max = *max_entries + 128;
+	int new_max = table->max + 128;
 	char ** new_entries;
 
 	new_entries = AllocVec (sizeof(char *)*new_max, MEMF_ANY);
@@ -82,14 +64,14 @@ static int AddEntry (const char * entry, int flag /* 0-dir, 1-file */)
 	if (!new_entries)
 	    return 0;
 
-	if (*num_entries)
+	if (table->num)
 	{
-	    CopyMemQuick (*entries, new_entries, sizeof(char *)* *num_entries);
-	    FreeVec (*entries);
+	    CopyMemQuick (table->entries, new_entries, sizeof(char *)* table->num);
+	    FreeVec (table->entries);
 	}
 
-	*entries = new_entries;
-	*max_entries = new_max;
+	table->entries = new_entries;
+	table->max = new_max;
     }
 
     len = strlen (entry) + 1;
@@ -99,7 +81,7 @@ static int AddEntry (const char * entry, int flag /* 0-dir, 1-file */)
 
     CopyMem ((char *)entry, dup, len);
 
-    (*entries)[(*num_entries) ++] = dup;
+    table->entries[table->num ++] = dup;
     return 1;
 }
 
@@ -108,260 +90,185 @@ static int compare_strings (const void * s1, const void * s2)
     return Stricmp (*(char **)s1, *(char **)s2);
 }
 
-#if 1
-#include <sys/types.h>
-/* #include <stdlib.h> */
+int indent = 0;
+static void showline (char * fmt, LONG args[])
+{
+    int t;
 
-static inline char	*med3 __P((char *, char *, char *, int (*)()));
-static inline void	 swapfunc __P((char *, char *, int, int));
+    for (t=0; t<indent; t++)
+	VPrintf ("    ", NULL);
 
-#define min(a, b)       (a) < (b) ? a : b
-
-/*
- * Qsort routine from Bentley & McIlroy's "Engineering a Sort Function".
- */
-#define swapcode(TYPE, parmi, parmj, n) {               \
-	long i = (n) / sizeof (TYPE);                   \
-	register TYPE *pi = (TYPE *) (parmi);           \
-	register TYPE *pj = (TYPE *) (parmj);           \
-	do {						\
-		register TYPE	t = *pi;		\
-		*pi++ = *pj;				\
-		*pj++ = t;				\
-	} while (--i > 0);                              \
+    VPrintf (fmt, args);
 }
 
-#define SWAPINIT(a, es) swaptype = ((char *)a - (char *)0) % sizeof(long) || \
-	es % sizeof(long) ? 2 : es == sizeof(long)? 0 : 1;
-
-static inline void
-swapfunc(a, b, n, swaptype)
-	char *a, *b;
-	int n, swaptype;
+struct
 {
-	if(swaptype <= 1)
-		swapcode(long, a, b, n)
-	else
-		swapcode(char, a, b, n)
-}
+    char * dir;
+    char * opt;
+    ULONG all;
+} args = {
+    NULL,
+    NULL,
+    0
+};
+char path[1024];
 
-#define swap(a, b)                                      \
-	if (swaptype == 0) {                            \
-		long t = *(long *)(a);                  \
-		*(long *)(a) = *(long *)(b);            \
-		*(long *)(b) = t;                       \
-	} else						\
-		swapfunc(a, b, es, swaptype)
-
-#define vecswap(a, b, n)        if ((n) > 0) swapfunc(a, b, n, swaptype)
-
-static inline char *
-med3(a, b, c, cmp)
-	char *a, *b, *c;
-	int (*cmp)();
+static LONG do_dir (void)
 {
-	return cmp(a, b) < 0 ?
-	       (cmp(b, c) < 0 ? b : (cmp(a, c) < 0 ? c : a ))
-	      :(cmp(b, c) > 0 ? b : (cmp(a, c) < 0 ? a : c ));
-}
-
-void
-qsort(a, n, es, cmp)
-	void *a;
-	size_t n, es;
-	int (*cmp)();
-{
-	char *pa, *pb, *pc, *pd, *pl, *pm, *pn;
-	int d, r, swaptype, swap_cnt;
-
-loop:	SWAPINIT(a, es);
-	swap_cnt = 0;
-	if (n < 7) {
-		for (pm = a + es; pm < (char *) a + n * es; pm += es)
-			for (pl = pm; pl > (char *) a && cmp(pl - es, pl) > 0;
-			     pl -= es)
-				swap(pl, pl - es);
-		return;
-	}
-	pm = a + (n / 2) * es;
-	if (n > 7) {
-		pl = a;
-		pn = a + (n - 1) * es;
-		if (n > 40) {
-			d = (n / 8) * es;
-			pl = med3(pl, pl + d, pl + 2 * d, cmp);
-			pm = med3(pm - d, pm, pm + d, cmp);
-			pn = med3(pn - 2 * d, pn - d, pn, cmp);
-		}
-		pm = med3(pl, pm, pn, cmp);
-	}
-	swap(a, pm);
-	pa = pb = a + es;
-
-	pc = pd = a + (n - 1) * es;
-	for (;;) {
-		while (pb <= pc && (r = cmp(pb, a)) <= 0) {
-			if (r == 0) {
-				swap_cnt = 1;
-				swap(pa, pb);
-				pa += es;
-			}
-			pb += es;
-		}
-		while (pb <= pc && (r = cmp(pc, a)) >= 0) {
-			if (r == 0) {
-				swap_cnt = 1;
-				swap(pc, pd);
-				pd -= es;
-			}
-			pc -= es;
-		}
-		if (pb > pc)
-			break;
-		swap(pb, pc);
-		swap_cnt = 1;
-		pb += es;
-		pc -= es;
-	}
-	if (swap_cnt == 0) {  /* Switch to insertion sort */
-		for (pm = a + es; pm < (char *) a + n * es; pm += es)
-			for (pl = pm; pl > (char *) a && cmp(pl - es, pl) > 0;
-			     pl -= es)
-				swap(pl, pl - es);
-		return;
-	}
-
-	pn = a + n * es;
-	r = min(pa - (char *)a, pb - pa);
-	vecswap(a, pb - r, r);
-	r = min(pd - pc, pn - pd - es);
-	vecswap(pb, pn - r, r);
-	if ((r = pb - pa) > es)
-		qsort(a, r / es, es, cmp);
-	if ((r = pd - pc) > es) {
-		/* Iterate rather than recurse to save stack space */
-		a = pn - r;
-		n = r / es;
-		goto loop;
-	}
-/*		qsort(pn - r, r / es, es, cmp);*/
-}
-#endif
-
-static LONG tinymain(void)
-{
-    char *args[1]={ 0 };
-    struct RDArgs *rda;
     BPTR dir;
     LONG loop;
     struct ExAllControl *eac;
     struct ExAllData *ead;
     static UBYTE buffer[4096];
     LONG error=0;
+    struct table dirs, files;
 
-    VPrintf ("dir\n", NULL);
+    dirs.entries = files.entries = NULL;
+    dirs.max = files.max = 0;
+    dirs.num = files.num = 0;
 
-    rda=ReadArgs("DIR",(ULONG *)args,NULL);
+    dir=Lock(path,SHARED_LOCK);
+    if(dir)
+    {
+	eac=AllocDosObject(DOS_EXALLCONTROL,NULL);
+	if(eac!=NULL)
+	{
+	    int t;
+	    LONG argv[3];
+
+	    eac->eac_LastKey=0;
+	    do
+	    {
+		loop=ExAll(dir,(struct ExAllData *)buffer,4096,ED_COMMENT,eac);
+		if(!loop&&IoErr()!=ERROR_NO_MORE_ENTRIES)
+		{
+		    error=RETURN_ERROR;
+		    break;
+		}
+		if(eac->eac_Entries)
+		{
+		    ead=(struct ExAllData *)buffer;
+		    do
+		    {
+			if (!AddEntry (ead->ed_Type > 0 ? &dirs : &files, ead->ed_Name))
+			{
+			    loop = 0;
+			    error=RETURN_ERROR;
+			    VPrintf ("out of memory\n", NULL);
+			    break;
+			}
+
+			ead=ead->ed_Next;
+		    }while(ead!=NULL);
+		}
+	    }while(loop);
+	    FreeDosObject(DOS_EXALLCONTROL,eac);
+
+	    if (!error)
+	    {
+		if (dirs.num)
+		{
+		    char * ptr;
+		    int added_slash = 0;
+
+		    indent ++;
+
+		    qsort (dirs.entries, dirs.num, sizeof (char *),
+			compare_strings);
+
+		    ptr = path + strlen (path);
+
+		    if (*path && ptr[-1] != ':' && ptr[-1] != '/')
+		    {
+			*ptr ++ = '/';
+			*ptr = 0;
+
+			added_slash = 1;
+		    }
+
+		    for (t=0; t<dirs.num; t++)
+		    {
+			argv[0] = (LONG) dirs.entries[t];
+
+			if (args.all)
+			{
+			    strcpy (ptr, dirs.entries[t]);
+
+			    showline ("%-25.s <DIR>\n", argv);
+			    do_dir ();
+			}
+			else
+			    showline ("    %-25.s <DIR>\n", argv);
+		    }
+
+		    if (added_slash)
+			ptr[-1] = 0;
+
+		    indent --;
+		}
+
+		if (files.num)
+		{
+		    qsort (files.entries, files.num, sizeof (char *),
+			compare_strings);
+
+		    for (t=0; t<files.num; t+=2)
+		    {
+			argv[0] = (LONG) (files.entries[t]);
+			argv[1] = (LONG) (t+1 < files.num ? files.entries[t+1] : "");
+			if (args.all)
+			    showline ("    %-25.s %-25.s\n", argv);
+			else
+			    showline ("%-25.s %-25.s\n", argv);
+		    }
+		}
+	    }
+
+	    if (dirs.num)
+	    {
+		for (t=0; t<dirs.num; t++)
+		{
+		    FreeVec (dirs.entries[t]);
+		}
+
+		if (dirs.entries)
+		    FreeVec (dirs.entries);
+	    }
+
+	    if (files.num)
+	    {
+		for (t=0; t<files.num; t++)
+		{
+		    FreeVec (files.entries[t]);
+		}
+
+		if (files.entries)
+		    FreeVec (files.entries);
+	    }
+	}else
+	{
+	    SetIoErr(ERROR_NO_FREE_STORE);
+	    error=RETURN_ERROR;
+	}
+	UnLock(dir);
+    }
+
+    return error;
+}
+
+static LONG tinymain(void)
+{
+    struct RDArgs *rda;
+    LONG error=0;
+
+    rda=ReadArgs("Dir,OPT/K,ALL/S",(ULONG *)&args,NULL);
     if(rda!=NULL)
     {
-	dir=Lock(args[0]!=NULL?args[0]:"",SHARED_LOCK);
-	if(dir)
-	{
-	    eac=AllocDosObject(DOS_EXALLCONTROL,NULL);
-	    if(eac!=NULL)
-	    {
-		int t;
-		LONG argv[3];
+	strcpy (path, args.dir!=NULL?args.dir:"");
 
-		eac->eac_LastKey=0;
-		do
-		{
-		    loop=ExAll(dir,(struct ExAllData *)buffer,4096,ED_COMMENT,eac);
-		    if(!loop&&IoErr()!=ERROR_NO_MORE_ENTRIES)
-		    {
-			error=RETURN_ERROR;
-			break;
-		    }
-		    if(eac->eac_Entries)
-		    {
-			ead=(struct ExAllData *)buffer;
-			do
-			{
-			    if (!AddEntry (ead->ed_Name, ead->ed_Type < 0))
-			    {
-				loop = 0;
-				error=RETURN_ERROR;
-				VPrintf ("out of memory\n", NULL);
-				break;
-			    }
+	VPrintf ("Dir %ld %ld %ld\n", (LONG *)&args);
+	error = do_dir ();
 
-			    ead=ead->ed_Next;
-			}while(ead!=NULL);
-		    }
-		}while(loop);
-		FreeDosObject(DOS_EXALLCONTROL,eac);
-
-		if (!error)
-		{
-		    if (num_dirs)
-		    {
-			qsort (dirs, num_dirs, sizeof (char *),
-			    compare_strings);
-
-			for (t=0; t<num_dirs; t++)
-			{
-			    argv[0] = (LONG) dirs[t];
-			    VPrintf ("    %-25.s <DIR>\n", argv);
-			}
-		    }
-
-		    if (num_files)
-		    {
-			qsort (files, num_files, sizeof (char *),
-			    compare_strings);
-
-			for (t=0; t<num_files; t+=3)
-			{
-			    argv[0] = (LONG) (files[t]);
-			    argv[1] = (LONG) (t+1 < num_files ? files[t+1] : "");
-			    argv[2] = (LONG) (t+2 < num_files ? files[t+2] : "");
-			    VPrintf ("%-25.s %-25.s %s\n", argv);
-			}
-		    }
-		}
-
-		if (max_dirs)
-		{
-		    for (t=0; t<num_dirs; t++)
-		    {
-			FreeVec (dirs[t]);
-		    }
-
-		    FreeVec (dirs);
-
-		    max_dirs = 0;
-		    num_dirs = 0;
-		}
-
-		if (max_files)
-		{
-		    for (t=0; t<num_files; t++)
-		    {
-			FreeVec (files[t]);
-		    }
-
-		    FreeVec (files);
-
-		    max_files = 0;
-		    num_files = 0;
-		}
-	    }else
-	    {
-		SetIoErr(ERROR_NO_FREE_STORE);
-		error=RETURN_ERROR;
-	    }
-	    UnLock(dir);
-	}
 	FreeArgs(rda);
     }else
 	error=RETURN_FAIL;
@@ -370,3 +277,6 @@ static LONG tinymain(void)
     return error;
 }
 
+#include "strcpy.c"
+#include "strlen.c"
+#include "qsort.c"
