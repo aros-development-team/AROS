952f9a668dd1a55044166337c34245949b98ff2e
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-08-12 14:27:01 +0000

    *** empty log message ***
    git-svn-id: https://svn.aros.org/svn/aros/trunk@82 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/graphics/graphics_functable.c
@@ -0,0 +1,79 @@
+/*
+        (C) 1995-96 AROS - The Amiga Replacement OS
+        *** Automatic generated file. Do not edit ***
+        Desc: Funktion table for Graphics
+        Lang: english
+*/
+#ifndef NULL
+#define NULL ((void *)0)
+#endif
+
+void Graphics_open (void);
+void Graphics_close (void);
+void Graphics_expunge (void);
+void Graphics_null (void);
+void Graphics_SetAPen (void);
+
+void *const Graphics_functable[]=
+{
+    Graphics_open, /* 1 */
+    Graphics_close, /* 2 */
+    Graphics_expunge, /* 3 */
+    Graphics_null, /* 4 */
+    NULL, /* 5 */
+    NULL, /* 6 */
+    NULL, /* 7 */
+    NULL, /* 8 */
+    NULL, /* 9 */
+    NULL, /* 10 */
+    NULL, /* 11 */
+    NULL, /* 12 */
+    NULL, /* 13 */
+    NULL, /* 14 */
+    NULL, /* 15 */
+    NULL, /* 16 */
+    NULL, /* 17 */
+    NULL, /* 18 */
+    NULL, /* 19 */
+    NULL, /* 20 */
+    NULL, /* 21 */
+    NULL, /* 22 */
+    NULL, /* 23 */
+    NULL, /* 24 */
+    NULL, /* 25 */
+    NULL, /* 26 */
+    NULL, /* 27 */
+    NULL, /* 28 */
+    NULL, /* 29 */
+    NULL, /* 30 */
+    NULL, /* 31 */
+    NULL, /* 32 */
+    NULL, /* 33 */
+    NULL, /* 34 */
+    NULL, /* 35 */
+    NULL, /* 36 */
+    NULL, /* 37 */
+    NULL, /* 38 */
+    NULL, /* 39 */
+    NULL, /* 40 */
+    NULL, /* 41 */
+    NULL, /* 42 */
+    NULL, /* 43 */
+    NULL, /* 44 */
+    NULL, /* 45 */
+    NULL, /* 46 */
+    NULL, /* 47 */
+    NULL, /* 48 */
+    NULL, /* 49 */
+    NULL, /* 50 */
+    NULL, /* 51 */
+    NULL, /* 52 */
+    NULL, /* 53 */
+    NULL, /* 54 */
+    NULL, /* 55 */
+    NULL, /* 56 */
+    Graphics_SetAPen, /* 57 */
+    (void *)-1L
+};
+
+char Graphics_end;
