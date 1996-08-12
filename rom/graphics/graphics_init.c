af7b8facb35ec5d22f22379bbded307087b13cd3
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-08-12 14:27:51 +0000

    Base of graphics library
    git-svn-id: https://svn.aros.org/svn/aros/trunk@84 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/graphics/graphics_init.c
@@ -0,0 +1,142 @@
+/*
+    (C) 1995-96 AROS - The Amiga Replacement OS
+    $Id$
+    $Log$
+    Revision 1.1  1996/08/12 14:27:50  digulla
+    Base of graphics library
+
+    Revision 1.2  1996/08/01 17:41:42  digulla
+    Added standard header for all files
+
+    Desc:
+    Lang:
+*/
+#include <exec/resident.h>
+#include <clib/exec_protos.h>
+#include <aros/libcall.h>
+#include <dos/dos.h>
+#include <graphics/gfxbase.h>
+#include "graphics_intern.h"
+
+static const char name[];
+static const char version[];
+static const APTR inittabl[4];
+static void *const Graphics_functable[];
+struct GfxBase *Graphics_init();
+extern const char Graphics_end;
+
+int Graphics_entry(void)
+{
+    /* If the library was executed by accident return error code. */
+    return -1;
+}
+
+const struct Resident Graphics_resident=
+{
+    RTC_MATCHWORD,
+    (struct Resident *)&Graphics_resident,
+    (APTR)&Graphics_end,
+    RTF_AUTOINIT,
+    39,
+    NT_LIBRARY,
+    0,
+    (char *)name,
+    (char *)&version[6],
+    (ULONG *)inittabl
+};
+
+static const char name[]=GRAPHICSNAME;
+
+static const char version[]="$VER: graphics.library 39.0 (12.8.96)\n\015";
+
+static const APTR inittabl[4]=
+{
+    (APTR)sizeof(struct GfxBase),
+    (APTR)Graphics_functable,
+    NULL,
+    &Graphics_init
+};
+
+__AROS_LH2(struct GfxBase *, init,
+ __AROS_LA(struct GfxBase *, GfxBase, D0),
+ __AROS_LA(BPTR,               segList,   A0),
+	   struct ExecBase *, sysBase, 0, Graphics)
+{
+    __AROS_FUNC_INIT
+
+    /* You would return NULL if the init failed */
+    return GfxBase;
+    __AROS_FUNC_EXIT
+}
+
+__AROS_LH1(struct GfxBase *, open,
+ __AROS_LA(ULONG, version, D0),
+	   struct GfxBase *, GfxBase, 1, Graphics)
+{
+    __AROS_FUNC_INIT
+
+    /* Keep compiler happy */
+    version=0;
+
+    /* I have one more opener. */
+    GfxBase->LibNode.lib_OpenCnt++;
+    GfxBase->LibNode.lib_Flags&=~LIBF_DELEXP;
+
+    /* You would return NULL if the open failed. */
+    return GfxBase;
+    __AROS_FUNC_EXIT
+}
+
+__AROS_LH0(BPTR, close,
+	   struct GfxBase *, GfxBase, 2, Graphics)
+{
+    __AROS_FUNC_INIT
+
+    /* I have one fewer opener. */
+    if(!--GfxBase->LibNode.lib_OpenCnt)
+    {
+	/* Delayed expunge pending? */
+	if(GfxBase->LibNode.lib_Flags&LIBF_DELEXP)
+	    /* Then expunge the library */
+	    return expunge();
+    }
+    return 0;
+    __AROS_FUNC_EXIT
+}
+
+__AROS_LH0(BPTR, expunge,
+	   struct GfxBase *, GfxBase, 3, Graphics)
+{
+    __AROS_FUNC_INIT
+
+    BPTR ret;
+
+    /* Test for openers. */
+    if(GfxBase->LibNode.lib_OpenCnt)
+    {
+	/* Set the delayed expunge flag and return. */
+	GfxBase->LibNode.lib_Flags|=LIBF_DELEXP;
+	return 0;
+    }
+
+    /* Get rid of the library. Remove it from the list. */
+    Remove(&GfxBase->LibNode.lib_Node);
+
+    /* Get returncode here - FreeMem() will destroy the field. */
+    ret=0L;
+
+    /* Free the memory. */
+    FreeMem((char *)GfxBase-GfxBase->LibNode.lib_NegSize,
+	    GfxBase->LibNode.lib_NegSize+GfxBase->LibNode.lib_PosSize);
+
+    return ret;
+    __AROS_FUNC_EXIT
+}
+
+__AROS_LH0I(int, null,
+	    struct GfxBase *, GfxBase, 4, Graphics)
+{
+    __AROS_FUNC_INIT
+    return 0;
+    __AROS_FUNC_EXIT
+}
---
+++ rom/graphics/graphics_intern.h
@@ -0,0 +1,26 @@
+#ifndef GRAPHICS_INTERN_H
+#define GRAPHICS_INTERN_H
+/*
+    (C) 1995-96 AROS - The Amiga Replacement OS
+    $Id$
+    $Log$
+    Revision 1.1  1996/08/12 14:27:51  digulla
+    Base of graphics library
+
+    Desc:
+    Lang:
+*/
+#ifndef AROS_LIBCALL_H
+#   include <aros/libcall.h>
+#endif
+#ifndef GRAPHICS_GFXBASE_H
+#   include <graphics/gfxbase.h>
+#endif
+
+extern struct GfxBase * GfxBase;
+
+/* Needed for close() */
+#define expunge() \
+__AROS_LC0(BPTR, expunge, struct GfxBase *, GfxBase, 3, Gfx)
+
+#endif /* GRAPHICS_INTERN_H */
