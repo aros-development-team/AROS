9ac95c4e83d3b9867b4a0f826d36ee782a5ba4d1
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-08-13 13:59:42 +0000

    Added SysBase as a define and to GfxBase
    git-svn-id: https://svn.aros.org/svn/aros/trunk@101 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/graphics/graphics_intern.h
+++ rom/graphics/graphics_intern.h
@@ -4,6 +4,9 @@
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
     $Log$
+    Revision 1.2  1996/08/13 13:59:42  digulla
+    Added SysBase as a define and to GfxBase
+
     Revision 1.1  1996/08/12 14:27:51  digulla
     Base of graphics library
 
@@ -13,11 +16,19 @@
 #ifndef AROS_LIBCALL_H
 #   include <aros/libcall.h>
 #endif
+#ifndef EXEC_EXECBASE_H
+#   include <exec/execbase.h>
+#endif
 #ifndef GRAPHICS_GFXBASE_H
 #   include <graphics/gfxbase.h>
 #endif
 
 extern struct GfxBase * GfxBase;
+
+#ifdef SysBase
+#undef SysBase
+#endif
+#define SysBase ((struct ExecBase *)(GfxBase->ExecBase))
 
 /* Needed for close() */
 #define expunge() \
