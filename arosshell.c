7a538c82ef412abfc9cd8459c5ce8449568bec23
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-08-13 15:32:25 +0000

    Externel reference to DOSBase was missing
    git-svn-id: https://svn.aros.org/svn/aros/trunk@113 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arosshell.c
+++ arosshell.c
@@ -2,6 +2,9 @@
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
     $Log$
+    Revision 1.3  1996/08/13 15:32:25  digulla
+    Externel reference to DOSBase was missing
+
     Revision 1.2  1996/08/01 17:40:37  digulla
     Added standard header for all files
 
@@ -11,6 +14,8 @@
 #include <dos/dostags.h>
 #include <dos/dos.h>
 #include <clib/dos_protos.h>
+
+extern struct DosBase * DOSBase;
 
 int main(void)
 {
