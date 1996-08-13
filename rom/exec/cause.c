779259f54e90243be1992943574455de143e218b
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-08-13 13:55:07 +0000

    The function was missing. Must be filled with code
    git-svn-id: https://svn.aros.org/svn/aros/trunk@96 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/exec/cause.c
@@ -0,0 +1,48 @@
+/*
+    (C) 1995 AROS - The Amiga Replacement OS
+    $Id$    $Log
+    Desc:
+    Lang: english
+*/
+#include "exec_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+	#include <exec/interrupts.h>
+	#include <clib/exec_protos.h>
+
+	__AROS_LH1(void, Cause,
+
+/*  SYNOPSIS */
+	__AROS_LHA(struct Interrupt *, interrupt, A1),
+
+/*  LOCATION */
+	struct ExecBase *, SysBase, 30, Exec)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+	29-10-95    digulla automatically created from
+			    exec_lib.fd and clib/exec_protos.h
+
+*****************************************************************************/
+{
+    __AROS_FUNC_INIT
+    __AROS_BASE_EXT_DECL(struct ExecBase *,SysBase)
+    __AROS_FUNC_EXIT
+} /* Cause */
