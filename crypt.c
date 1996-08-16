5b6d31c6a26e8d0ac3779abfa3034b717b65c0a3
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-08-16 14:01:40 +0000

    The salt may be specified now
    git-svn-id: https://svn.aros.org/svn/aros/trunk@149 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- crypt.c
+++ crypt.c
@@ -2,6 +2,9 @@
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
     $Log$
+    Revision 1.3  1996/08/16 14:01:40  digulla
+    The salt may be specified now
+
     Revision 1.2  1996/08/01 17:40:38  digulla
     Added standard header for all files
 
@@ -15,12 +18,18 @@ int main (int argc, char ** argv)
 {
     char salt[3];
     char * set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789./";
-    
+
     srand (time (NULL));
 
     salt[0] = set[getpid() % sizeof(set)];
     salt[1] = set[rand() % sizeof(set)];
     salt[2] = 0;
+
+    if (argc > 2)
+    {
+	salt[0] = argv[2][0];
+	salt[1] = argv[2][1];
+    }
 
     printf ("Encrypting %s: %s\n", argv[1], crypt (argv[1], salt));
 }
