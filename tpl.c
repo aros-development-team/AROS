6c37ee158f88414625741a6b3e216b8d50992b98
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-08-23 17:26:09 +0000

    Template for new AROS files
    git-svn-id: https://svn.aros.org/svn/aros/trunk@187 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ tpl.c
@@ -0,0 +1,38 @@
+/*
+    (C) 1995-96 AROS - The Amiga Replacement OS
+    $Id$
+    $Log$
+
+    Desc:
+    Lang:
+*/
+/* Local prototypes */
+
+/*****************************************************************************
+
+    NAME */
+
+/*  SYNOPSIS */
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+{
+} /* */
+
