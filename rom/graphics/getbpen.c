3898670473e517b8c97204c70284fe5264e57045
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-08-23 17:31:26 +0000

    Some functions to read values from the rastport
    git-svn-id: https://svn.aros.org/svn/aros/trunk@192 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/graphics/getbpen.c
@@ -0,0 +1,51 @@
+/*
+    (C) 1995 AROS - The Amiga Replacement OS
+    $Id$    $Log
+    Desc:
+    Lang: english
+*/
+#include "graphics_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+	#include <graphics/rastport.h>
+	#include <clib/graphics_protos.h>
+
+	__AROS_LH1(ULONG, GetBPen,
+
+/*  SYNOPSIS */
+	__AROS_LHA(struct RastPort *, rp, A0),
+
+/*  LOCATION */
+	struct GfxBase *, GfxBase, 144, Graphics)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+	29-10-95    digulla automatically created from
+			    graphics_lib.fd and clib/graphics_protos.h
+
+*****************************************************************************/
+{
+    __AROS_FUNC_INIT
+    __AROS_BASE_EXT_DECL(struct GfxBase *,GfxBase)
+
+    return rp->BgPen;
+
+    __AROS_FUNC_EXIT
+} /* GetBPen */
