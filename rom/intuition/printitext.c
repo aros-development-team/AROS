da31a8e8ce56407d60057dba8da56ac57d720162
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-08-29 15:13:32 +0000

    Wrote code
    git-svn-id: https://svn.aros.org/svn/aros/trunk@210 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/intuition/printitext.c
+++ rom/intuition/printitext.c
@@ -2,6 +2,9 @@
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
     $Log$
+    Revision 1.3  1996/08/29 15:13:32  digulla
+    Wrote code
+
     Revision 1.2  1996/08/29 13:57:37  digulla
     Commented
     Moved common code from driver to Intuition
@@ -14,6 +17,8 @@
     Lang: english
 */
 #include "intuition_intern.h"
+#include <string.h>
+#include <clib/graphics_protos.h>
 
 /*****************************************************************************
 
@@ -27,8 +32,8 @@
 /*  SYNOPSIS */
 	__AROS_LHA(struct RastPort  *, rp, A0),
 	__AROS_LHA(struct IntuiText *, iText, A1),
-	__AROS_LHA(long              , left, D0),
-	__AROS_LHA(long              , top, D1),
+	__AROS_LHA(long              , leftOffset, D0),
+	__AROS_LHA(long              , topOffset, D1),
 
 /*  LOCATION */
 	struct IntuitionBase *, IntuitionBase, 36, Intuition)
@@ -40,8 +45,8 @@
     INPUTS
 	rp - Draw into this RastPort
 	iText - Render this text
-	left, top - Starting-Point. All coordinates in the IntuiText
-		structures are relative to this point.
+	leftOffset, topOffset - Starting-Point. All coordinates in the
+		IntuiText structures are relative to this point.
 
     RESULT
 	None.
@@ -64,5 +69,57 @@
 {
     __AROS_FUNC_INIT
     __AROS_BASE_EXT_DECL(struct IntuitionBase *,IntuitionBase)
+    ULONG  apen;
+    ULONG  bpen;
+    ULONG  drmd;
+    struct TextFont * font;
+    struct TextFont * newfont;
+
+    /* Store important variables of the RastPort */
+    apen = GetAPen (rp);
+    bpen = GetBPen (rp);
+    drmd = GetDrMd (rp);
+
+    font = rp->Font;
+
+    /* For all borders... */
+    for ( ; iText; iText=iText->NextText)
+    {
+	/* Change RastPort to the colors/mode specified */
+	SetAPen (rp, iText->FrontPen);
+	SetBPen (rp, iText->BackPen);
+	SetDrMd (rp, iText->DrawMode);
+
+	if (iText->ITextFont)
+	{
+	    newfont = OpenFont (iText->ITextFont);
+
+	    if (newfont)
+		SetFont (rp, newfont);
+	}
+
+	/* Move to initial position */
+	Move (rp
+	    , iText->LeftEdge + leftOffset
+	    , iText->TopEdge + topOffset
+	);
+	Text (rp, iText->IText, strlen (iText->IText));
+
+	if (iText->ITextFont)
+	{
+	    if (newfont)
+	    {
+		SetFont (rp, newfont);
+		CloseFont (newfont);
+	    }
+	}
+    }
+
+    /* Restore RastPort */
+    SetAPen (rp, apen);
+    SetBPen (rp, bpen);
+    SetDrMd (rp, drmd);
+    SetFont (rp, font);
+
     __AROS_FUNC_EXIT
 } /* PrintIText */
