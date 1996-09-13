6801a813ac791f6b0358b98c755b4f4d74bf8fa7
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-09-13 17:55:10 +0000

    AROS special include file exec/types.h. Supports QUAD and IPTR
    git-svn-id: https://svn.aros.org/svn/aros/trunk@301 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/exec/types.h
@@ -0,0 +1,118 @@
+#ifndef EXEC_TYPES_H
+#define EXEC_TYPES_H
+
+/*
+    (C) 1995-96 AROS - The Amiga Replacement OS
+    $Id$
+    $Log$
+    Revision 1.1  1996/09/13 17:55:10  digulla
+    AROS special include file exec/types.h. Supports QUAD and IPTR
+
+
+    Desc: Data typing - must be included before any other file.
+    Lang: english
+
+    This version of exec/types.h does not contain any of the definitions
+    of obsolete types (eg SHORT, CPTR, ...). If your code contains any
+    of these they should have been changed a long time ago.
+*/
+#ifndef AROS_SYSTEM_H
+#   include <aros/system.h>
+#endif
+
+/*************************************
+ ***** Basic Data types 	 *****
+ *************************************/
+
+#ifndef APTR_TYPEDEF
+#define APTR_TYPEDEF
+typedef void *		    APTR;   /* memory pointer */
+#endif
+
+/* An integer which can store a pointer */
+#ifdef AROS_IPTR_TYPE
+typedef AROS_IPTR_TYPE		    IPTR;
+#else
+typedef unsigned long		    IPTR;
+#endif
+
+/* Distinguish between 64 and 32bit systems */
+#ifdef AROS_32BIT_TYPE
+typedef   signed AROS_32BIT_TYPE    LONG;   /* signed 32-bit value */
+typedef unsigned AROS_32BIT_TYPE    ULONG;  /* unsigned 32-bit value */
+#else
+typedef   signed long		    LONG;   /* signed 32-bit value */
+typedef unsigned long		    ULONG;  /* unsigned 32-bit value */
+#endif
+
+#ifdef AROS_64BIT_TYPE
+typedef   signed AROS_64BIT_TYPE    QUAD;   /* signed 64-bit value */
+typedef unsigned AROS_64BIT_TYPE    UQUAD;  /* unsigned 64-bit-value */
+#else
+#ifdef __GNUC__
+typedef   signed long long	    QUAD;   /* signed 64-bit value */
+typedef unsigned long long	    UQUAD;  /* unsigned 64-bit-value */
+#else
+typedef struct {  LONG high, low }  QUAD;   /* signed 64-bit value */
+typedef struct { ULONG high, low }  QUAD;   /* unsigned 64-bit value */
+#endif /* __GNUC__ */
+#endif /* AROS_64BIT_TYPE */
+
+#ifdef AROS_16BIT_TYPE
+typedef   signed AROS_16BIT_TYPE    WORD;   /* signed 64-bit value */
+typedef unsigned AROS_16BIT_TYPE    UWORD;  /* unsigned 64-bit-value */
+#else
+typedef   signed short		    WORD;   /* signed 16-bit value */
+typedef unsigned short		    UWORD;  /* unsigned 16-bit value */
+#endif
+
+typedef signed char		    BYTE;   /* signed 8-bit value */
+typedef unsigned char		    UBYTE;  /* unsigned 8-bit value */
+
+
+/*************************************
+ ***** Other interesting types	 *****
+ *************************************/
+
+typedef UBYTE * STRPTR;     /* Pointer to string (NULL terminated) */
+typedef short	BOOL;	    /* A Boolean value */
+typedef float	FLOAT;	    /* 32bit IEEE floating point value */
+typedef double	DOUBLE;     /* 64bit IEEE floating point value */
+
+/*************************************
+ ***** Some useful definitions	 *****
+ *************************************/
+
+#if !defined(FALSE)
+#define FALSE	0L
+#endif
+
+#if !defined(TRUE)
+#define TRUE	1L
+#endif
+
+#if !defined(NULL)
+#define NULL	((void *)0L)
+#endif
+
+#if !defined(VOID)
+#define VOID	void
+#endif
+
+#define GLOBAL	 extern
+#define IMPORT	 extern
+#define STATIC	 static
+#define REGISTER register
+
+/*
+    Minimum support library version. AROS doesn´t have system libraries
+    below V40
+*/
+#define LIBRARY_MINIMUM 40
+
+/*
+    The current version of the includes. Do not use this value in calls
+    to OpenLibrary(). Some system libraries may not be at this version. */
+#define INCLUDE_VERSION 40
+
+#endif
