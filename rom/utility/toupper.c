2148cc2a605e2c2c88a2ca665188147febaccb7f
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-09-13 17:16:35 +0000

    Removed the macro TOUPPER. Use this library function instead
    git-svn-id: https://svn.aros.org/svn/aros/trunk@291 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/utility/toupper.c
+++ rom/utility/toupper.c
@@ -2,6 +2,9 @@
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
     $Log$
+    Revision 1.2  1996/09/13 17:16:35  digulla
+    Removed the macro TOUPPER. Use this library function instead
+
     Revision 1.1  1996/08/31 12:58:13  aros
     Merged in/modified for FreeBSD.
 
@@ -16,60 +19,68 @@
 /*****************************************************************************
 
     NAME */
-        #include <clib/utility_protos.h>
+	#include <clib/utility_protos.h>
 
-        __AROS_LH1I(UBYTE, ToUpper,
+	__AROS_LH1I(UBYTE, ToUpper,
 
 /*  SYNOPSIS */
-        __AROS_LHA(unsigned long, character, D0),
+	__AROS_LHA(unsigned long, character, D0),
 
 /*  LOCATION */
-        struct UtilityBase *, UtilityBase, 29, Utility)
+	struct UtilityBase *, UtilityBase, 29, Utility)
 
 /*  FUNCTION
-        Convert a character to uppercase
+	Convert a character to uppercase
 
     INPUTS
-        character   - The character that you want changed.
+	character   - The character that you want changed.
 
     RESULT
-        The uppercase version of that character.
+	The uppercase version of that character.
 
     NOTES
-        Currently only works for ASCII characters. Would not be difficult
-        to adapt for other character sets (Unicode for example).
+	Currently only works for ASCII characters. Would not be difficult
+	to adapt for other character sets (Unicode for example).
 
-	This function is patched by the locale.library, so you should be 
+	This function is patched by the locale.library, so you should be
 	prepared for different results when running under different
 	languages.
 
     EXAMPLE
-        STRPTR string; UBYTE chr;
+	STRPTR string; UBYTE chr;
 
-        \* Convert a string to uppercase *\
-        while( chr = *string )
-        {
-            *string = ToUpper( chr );
-            string++;
-        }
+	\* Convert a string to uppercase *\
+	while( chr = *string )
+	{
+	    *string = ToUpper( chr );
+	    string++;
+	}
 
     BUGS
 
     SEE ALSO
-        utility/ToLower()
+	utility/ToLower()
 
     INTERNALS
-        This function is patched by locale.library.
+	This function is patched by locale.library.
 
     HISTORY
-        29-10-95    digulla automatically created from
-                            utility_lib.fd and clib/utility_protos.h
-        10-08-96    iaint   Created from tolower.c from AROSdev15
+	29-10-95    digulla automatically created from
+			    utility_lib.fd and clib/utility_protos.h
+	10-08-96    iaint   Created from tolower.c from AROSdev15
 *****************************************************************************/
 {
     __AROS_FUNC_INIT
 
-    return TOUPPER(character);
+    return
+    (
+	(character >= 'a' && character <= 'z')
+	|| (character >= 0xE0
+	    && character <= 0xEE
+	    && character != 0xE7)
+	? character - 0x20
+	: character
+    );
 
     __AROS_FUNC_EXIT
 } /* ToLower */
