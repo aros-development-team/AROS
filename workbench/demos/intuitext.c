4425938c2f403458710def299b19af5fc465111a
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-09-17 16:43:53 +0000

    Two demos from the RKRMs to show how simple it is to compile Amiga code with AROS
    git-svn-id: https://svn.aros.org/svn/aros/trunk@322 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/demos/intuitext.c
@@ -0,0 +1,137 @@
+;/* intuitext.c - Execute me to compile me with SAS C 5.10
+LC -b1 -cfistq -v -y -j73 intuitext.c
+Blink FROM LIB:c.o,intuitext.o TO intuitext LIBRARY LIB:LC.lib,LIB:Amiga.lib
+quit
+*/
+
+/*
+Copyright (c) 1992 Commodore-Amiga, Inc.
+
+This example is provided in electronic form by Commodore-Amiga, Inc. for
+use with the "Amiga ROM Kernel Reference Manual: Libraries", 3rd Edition,
+published by Addison-Wesley (ISBN 0-201-56774-1).
+
+The "Amiga ROM Kernel Reference Manual: Libraries" contains additional
+information on the correct usage of the techniques and operating system
+functions presented in these examples.	The source and executable code
+of these examples may only be distributed in free electronic form, via
+bulletin board or as part of a fully non-commercial and freely
+redistributable diskette.  Both the source and executable code (including
+comments) must be included, without modification, in any copy.	This
+example may not be published in printed form or distributed with any
+commercial product.  However, the programming techniques and support
+routines set forth in these examples may be used in the development
+of original executable software products for Commodore Amiga computers.
+
+All other rights reserved.
+
+This example is provided "as-is" and is subject to change; no
+warranties are made.  All use is at your own risk. No liability or
+responsibility is assumed.
+*/
+
+
+/*
+** intuitext.c - program to show the use of an Intuition IntuiText object.
+*/
+#define INTUI_V36_NAMES_ONLY
+
+#include <exec/types.h>
+#include <intuition/intuition.h>
+
+#include <clib/exec_protos.h>
+#include <clib/dos_protos.h>
+#include <clib/intuition_protos.h>
+
+#include <stdio.h>
+
+#ifdef __AROS
+#define __chip
+extern struct ExecBase * SysBase;
+extern struct DosLibrary *DOSBase;
+#endif
+
+#ifdef LATTICE
+int CXBRK(void)    { return(0); }  /* Disable Lattice CTRL/C handling */
+int chkabort(void) { return(0); }  /* really */
+#endif
+
+struct Library *IntuitionBase = NULL;
+
+#define MYTEXT_LEFT (0)
+#define MYTEXT_TOP  (0)
+
+/*
+** main routine. Open required library and window and draw the images.
+** This routine opens a very simple window with no IDCMP.  See the
+** chapters on "Windows" and "Input and Output Methods" for more info.
+** Free all resources when done.
+*/
+VOID main(int argc, char **argv)
+{
+struct Screen	 *screen;
+struct DrawInfo  *drawinfo;
+struct Window	 *win;
+struct IntuiText  myIText;
+struct TextAttr   myTextAttr;
+
+ULONG myTEXTPEN;
+ULONG myBACKGROUNDPEN;
+
+IntuitionBase = OpenLibrary("intuition.library",37);
+if (IntuitionBase)
+    {
+    if (screen = LockPubScreen(NULL))
+	{
+	if (drawinfo = GetScreenDrawInfo(screen))
+	    {
+	    /* Get a copy of the correct pens for the screen.
+	    ** This is very important in case the user or the
+	    ** application has the pens set in a unusual way.
+	    */
+	    myTEXTPEN = drawinfo->dri_Pens[TEXTPEN];
+	    myBACKGROUNDPEN  = drawinfo->dri_Pens[BACKGROUNDPEN];
+
+	    /* create a TextAttr that matches the specified font. */
+	    myTextAttr.ta_Name	= drawinfo->dri_Font->tf_Message.mn_Node.ln_Name;
+	    myTextAttr.ta_YSize = drawinfo->dri_Font->tf_YSize;
+	    myTextAttr.ta_Style = drawinfo->dri_Font->tf_Style;
+	    myTextAttr.ta_Flags = drawinfo->dri_Font->tf_Flags;
+
+	    /* open a simple window on the workbench screen for displaying
+	    ** a text string.  An application would probably never use such a
+	    ** window, but it is useful for demonstrating graphics...
+	    */
+	    if (win = OpenWindowTags(NULL,
+				WA_PubScreen,	 screen,
+				WA_RMBTrap,	 TRUE,
+				TAG_END))
+		{
+		myIText.FrontPen    = myTEXTPEN;
+		myIText.BackPen     = myBACKGROUNDPEN;
+		myIText.DrawMode    = JAM2;
+		myIText.LeftEdge    = MYTEXT_LEFT;
+		myIText.TopEdge     = MYTEXT_TOP;
+		myIText.ITextFont   = &myTextAttr;
+		myIText.IText	    = "Hello, World.  ;-)";
+		myIText.NextText    = NULL;
+
+		/* Draw the text string at 10,10 */
+		PrintIText(win->RPort,&myIText,10,10);
+
+		/* Wait a bit, then quit.
+		** In a real application, this would be an event loop,
+		** like the one described in the Intuition Input and
+		** Output Methods chapter.
+		*/
+		Delay(200);
+
+		CloseWindow(win);
+		}
+	    FreeScreenDrawInfo(screen,drawinfo);
+	    }
+	UnlockPubScreen(NULL,screen);
+	}
+    CloseLibrary(IntuitionBase);
+    }
+}
---
+++ workbench/demos/simpleimage.c
@@ -0,0 +1,130 @@
+;/* simpleimage.c - program to show the use of a simple Intuition Image.
+lc -b1 -cfist -v -y -j73 simpleimage.c
+blink FROM LIB:c.o+"simpleimage.o" TO "simpleimage" LIB LIB:lc.lib LIB:amiga.lib
+quit
+*/
+
+/*
+Copyright (c) 1992 Commodore-Amiga, Inc.
+
+This example is provided in electronic form by Commodore-Amiga, Inc. for
+use with the "Amiga ROM Kernel Reference Manual: Libraries", 3rd Edition,
+published by Addison-Wesley (ISBN 0-201-56774-1).
+
+The "Amiga ROM Kernel Reference Manual: Libraries" contains additional
+information on the correct usage of the techniques and operating system
+functions presented in these examples.	The source and executable code
+of these examples may only be distributed in free electronic form, via
+bulletin board or as part of a fully non-commercial and freely
+redistributable diskette.  Both the source and executable code (including
+comments) must be included, without modification, in any copy.	This
+example may not be published in printed form or distributed with any
+commercial product.  However, the programming techniques and support
+routines set forth in these examples may be used in the development
+of original executable software products for Commodore Amiga computers.
+
+All other rights reserved.
+
+This example is provided "as-is" and is subject to change; no
+warranties are made.  All use is at your own risk. No liability or
+responsibility is assumed.
+*/
+
+#define INTUI_V36_NAMES_ONLY
+
+#include <exec/types.h>
+#include <intuition/intuition.h>
+#include <intuition/intuitionbase.h>
+
+#include <clib/exec_protos.h>
+#include <clib/dos_protos.h>
+#include <clib/intuition_protos.h>
+
+#include <stdio.h>
+
+#ifdef __AROS
+#define __chip
+extern struct ExecBase * SysBase;
+extern struct DosLibrary *DOSBase;
+#endif
+
+#ifdef LATTICE
+int CXBRK(void)    { return(0); }  /* Disable Lattice CTRL/C handling */
+int chkabort(void) { return(0); }  /* really */
+#endif
+
+struct IntuitionBase *IntuitionBase = NULL;
+
+#define MYIMAGE_LEFT	(0)
+#define MYIMAGE_TOP	(0)
+#define MYIMAGE_WIDTH	(24)
+#define MYIMAGE_HEIGHT	(10)
+#define MYIMAGE_DEPTH	(1)
+
+/* This is the image data.  It is a one bit-plane open rectangle which is 24
+** pixels wide and 10 high.  Make sure that it is in CHIP memory, or allocate
+** a block of chip memory with a call like this: AllocMem(data_size,MEMF_CHIP),
+** and then copy the data to that block.  See the Exec chapter on Memory
+** Allocation for more information on AllocMem().
+*/
+UWORD __chip myImageData[] =
+    {
+    0xFFFF, 0xFF00,
+    0xC000, 0x0300,
+    0xC000, 0x0300,
+    0xC000, 0x0300,
+    0xC000, 0x0300,
+    0xC000, 0x0300,
+    0xC000, 0x0300,
+    0xC000, 0x0300,
+    0xC000, 0x0300,
+    0xFFFF, 0xFF00,
+    };
+
+/*
+** main routine. Open required library and window and draw the images.
+** This routine opens a very simple window with no IDCMP.  See the
+** chapters on "Windows" and "Input and Output Methods" for more info.
+** Free all resources when done.
+*/
+VOID main(int argc, char *argv[])
+{
+struct Window *win;
+struct Image myImage;
+
+IntuitionBase = (struct IntuitionBase *)OpenLibrary("intuition.library",37);
+if (IntuitionBase != NULL)
+    {
+    if (NULL != (win = OpenWindowTags(NULL,
+			WA_Width,	200,
+			WA_Height,	100,
+			WA_RMBTrap,	TRUE,
+			TAG_END)))
+	{
+	myImage.LeftEdge    = MYIMAGE_LEFT;
+	myImage.TopEdge     = MYIMAGE_TOP;
+	myImage.Width	    = MYIMAGE_WIDTH;
+	myImage.Height	    = MYIMAGE_HEIGHT;
+	myImage.Depth	    = MYIMAGE_DEPTH;
+	myImage.ImageData   = myImageData;
+	myImage.PlanePick   = 0x1;		/* use first bit-plane	   */
+	myImage.PlaneOnOff  = 0x0;		/* clear all unused planes */
+	myImage.NextImage   = NULL;
+
+	/* Draw the 1 bit-plane image into the first bit-plane (color 1) */
+	DrawImage(win->RPort,&myImage,10,10);
+
+	/* Draw the same image at a new location */
+	DrawImage(win->RPort,&myImage,100,10);
+
+	/* Wait a bit, then quit.
+	** In a real application, this would be an event loop, like the
+	** one described in the Intuition Input and Output Methods chapter.
+	*/
+	Delay(200);
+
+	CloseWindow(win);
+	}
+    CloseLibrary((struct Library *)IntuitionBase);
+    }
+}
