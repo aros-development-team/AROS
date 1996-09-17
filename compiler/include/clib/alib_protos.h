4fe29c359b640ce71a9a14f32e0e50b2b1ab7153
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-09-17 16:51:59 +0000

    Prototypes for functions in the amiga.lib
    git-svn-id: https://svn.aros.org/svn/aros/trunk@323 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/clib/alib_protos.h
@@ -0,0 +1,33 @@
+#ifndef  CLIB_ALIB_PROTOS_H
+#define  CLIB_ALIB_PROTOS_H
+
+/*
+**	$VER: alib_protos.h 1.0 (26.10.95)
+**
+**	C prototypes for things in amiga.lib.
+**
+*/
+
+#ifndef  EXEC_TYPES_H
+#   include <exec/types.h>
+#endif
+#ifndef INTUITION_INTUITION_H
+#   include <intuition/intuition.h>
+#endif
+#ifndef INTUITION_CLASSUSR_H
+#   include <intuition/classusr.h>
+#endif
+
+/*
+    Prototypes
+*/
+ULONG DoMethodA (Object * obj, Msg message);
+ULONG DoMethod (Object * obj, ULONG MethodID, ...);
+ULONG DoSuperMethodA (Class  * cl, Object * obj, Msg message);
+ULONG DoSuperMethod (Class * cl, Object * obj, ULONG MethodID, ...);
+void SetAttrs (Object * obj, ULONG tag1, ...);
+APTR NewObject (struct IClass * classPtr, UBYTE * classID, ULONG tag1, ...);
+
+struct Window * OpenWindowTags (struct NewWindow * newWindow, ULONG tag1, ...);
+
+#endif /* CLIB_ALIB_PROTOS_H */
