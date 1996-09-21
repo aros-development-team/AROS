4a235e46f49eb4fccd78698c24f32840fa54dce5
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-09-21 15:54:01 +0000

    Fill private fields
    git-svn-id: https://svn.aros.org/svn/aros/trunk@382 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/intuition/openscreen.c
+++ rom/intuition/openscreen.c
@@ -2,6 +2,9 @@
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
     $Log$
+    Revision 1.2  1996/09/21 15:54:01  digulla
+    Fill private fields
+
     Revision 1.1  1996/09/21 14:11:39  digulla
     Open and close screens
 
@@ -61,8 +64,8 @@
 {
     __AROS_FUNC_INIT
     __AROS_BASE_EXT_DECL(struct IntuitionBase *,IntuitionBase)
-    struct Screen * screen;
-#define COPY(x)     screen->x = newScreen->x
+    struct IntScreen * screen;
+#define COPY(x)     screen->Screen.x = newScreen->x
 
     D(bug("OpenScreen (%p = { Left=%d Top=%d Width=%d Height=%d Depth=%d })\n"
 	, newScreen
@@ -73,7 +76,7 @@
 	, newScreen->Depth
     ));
 
-    if ((screen = AllocMem (sizeof (struct Screen), MEMF_ANY | MEMF_CLEAR)))
+    if ((screen = AllocMem (sizeof (struct IntScreen), MEMF_ANY | MEMF_CLEAR)))
     {
 	COPY(LeftEdge);
 	COPY(TopEdge);
@@ -84,30 +87,57 @@
 	COPY(Font);
 	COPY(DefaultTitle);
 
-	screen->Flags = newScreen->Type;
+	screen->Screen.Flags = newScreen->Type;
 
-	screen->BitMap.Depth = newScreen->Depth;
+	screen->Screen.BitMap.Depth = newScreen->Depth;
 
-	screen->BarHeight = 0;
-	screen->BarVBorder = 0;
-	screen->BarHBorder = 0;
-	screen->MenuVBorder = 0;
-	screen->MenuHBorder = 0;
+	screen->Screen.BarHeight = 0;
+	screen->Screen.BarVBorder = 0;
+	screen->Screen.BarHBorder = 0;
+	screen->Screen.MenuVBorder = 0;
+	screen->Screen.MenuHBorder = 0;
 
-	screen->WBorTop = 0;
-	screen->WBorLeft = 0;
-	screen->WBorRight = 0;
-	screen->WBorBottom = 0;
+	screen->Screen.WBorTop = 0;
+	screen->Screen.WBorLeft = 0;
+	screen->Screen.WBorRight = 0;
+	screen->Screen.WBorBottom = 0;
 
-	InitRastPort (&screen->RastPort);
+	InitRastPort (&screen->Screen.RastPort);
 
-	screen->RastPort.BitMap = &screen->BitMap;
+	screen->Screen.RastPort.BitMap = &screen->Screen.BitMap;
 
-	screen->Title = newScreen->DefaultTitle;
+	screen->Screen.Title = newScreen->DefaultTitle;
 
-	screen->NextScreen = IntuitionBase->FirstScreen;
+	screen->Screen.NextScreen = IntuitionBase->FirstScreen;
 	IntuitionBase->FirstScreen =
-	    IntuitionBase->ActiveScreen = screen;
+	    IntuitionBase->ActiveScreen = &screen->Screen;
+
+	screen->DInfo.dri_Version = DRI_VERSION;
+	screen->DInfo.dri_NumPens = NUMDRIPENS;
+	screen->DInfo.dri_Pens = screen->Pens;
+	screen->DInfo.dri_Depth = newScreen->Depth;
+	screen->DInfo.dri_Resolution.X = 44;
+	screen->DInfo.dri_Resolution.Y = 44;
+	screen->DInfo.dri_Flags = 0;
+
+	if (screen->Screen.Font)
+	    screen->DInfo.dri_Font = OpenFont (screen->Screen.Font);
+
+	if (!screen->DInfo.dri_Font)
+	    screen->DInfo.dri_Font = GfxBase->DefaultFont;
+
+	screen->Pens[DETAILPEN] = screen->Screen.DetailPen;
+	screen->Pens[BLOCKPEN]	= screen->Screen.BlockPen;
+	screen->Pens[TEXTPEN] = 1;
+	screen->Pens[SHINEPEN] = 2;
+	screen->Pens[SHADOWPEN] = 1;
+	screen->Pens[FILLPEN] = 3;
+	screen->Pens[FILLTEXTPEN] = 2;
+	screen->Pens[BACKGROUNDPEN] = 0;
+	screen->Pens[HIGHLIGHTTEXTPEN] = 1;
+	screen->Pens[BARDETAILPEN] = 1;
+	screen->Pens[BARBLOCKPEN] = 2;
+	screen->Pens[BARTRIMPEN] = 1;
     }
 
     ReturnPtr ("OpenScreen", struct Screen *, screen);
