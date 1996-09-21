46c162a5e538554404d2707be4010a8265c4a5c4
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-09-21 14:12:12 +0000

    Enable debuggin in the intuition.library on a per-function basis
    git-svn-id: https://svn.aros.org/svn/aros/trunk@353 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/intuition/intuition_debug.h
@@ -0,0 +1,11 @@
+#ifndef _INTUITION_DEBUG_H_
+#define _INTUITION_DEBUG_H_ 1
+
+#define DEBUG_OpenScreen 1
+#define DEBUG_OpenScreenTagList 1
+#define DEBUG_OpenWindow 1
+#define DEBUG_OpenWindowTagList 1
+#define DEBUG_CloseScreen 1
+#define DEBUG_CloseWindow 1
+
+#endif /* _INTUITION_DEBUG_H_ */
