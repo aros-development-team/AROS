c7d5ffe390cfc092c9505a6494ad23298e76e9bc
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-10-02 18:10:47 +0000

    Fixed a bug (coordinates are relative to offset and not to last point)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@410 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/intuition/drawborder.c
+++ rom/intuition/drawborder.c
@@ -2,6 +2,9 @@
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
     $Log$
+    Revision 1.3  1996/10/02 18:10:47  digulla
+    Fixed a bug (coordinates are relative to offset and not to last point)
+
     Revision 1.2  1996/08/29 13:33:30  digulla
     Moved common code from driver to Intuition
     More docs
@@ -38,8 +41,7 @@
 	Draws one or more borders in the specified RastPort. Rendering
 	will start at the position which you get when you add the offsets
 	leftOffset and topOffset to the LeftEdge and TopEdge specified
-	in the Border structure. All subsequent adresses in the Border
-	structure are relative to the previous position.
+	in the Border structure. All coordinates are relative to that point.
 
     INPUTS
 	rp - The RastPort to render into
@@ -53,16 +55,17 @@
 
     EXAMPLE
 	// Draw a house with one stroke
+	// The drawing starts at the lower left edge
 	WORD XY[] =
 	{
-	    +10, +10,
-	    -10,   0,	// Top
-	    +10, -10,
-	    -10,   0,	// Bottom
-	      0, -10,	// Left
-	     +5,  +5,	// Roof
-	     +5,  -5,
-	      0, +10	// Right
+	    10, -10,
+	    10,   0,
+	     0, -10,
+	    10, -10,
+	     5, -15,
+	     0, -10,
+	     0,   0,
+	    10,   0,
 	};
 	struct Border demo =
 	{
@@ -96,6 +99,7 @@
     ULONG  drmd;
     WORD * ptr;
     WORD   x, y;
+    WORD   xoff, yoff;
     int    t;
 
     /* Store important variables of the RastPort */
@@ -123,11 +127,11 @@
 	for (t=0; t<border->Count; t++)
 	{
 	    /* Add vector offset to current position */
-	    x += *ptr ++;
-	    y += *ptr ++;
+	    xoff = *ptr ++;
+	    yoff = *ptr ++;
 
 	    /* Stroke */
-	    Draw (rp, x, y);
+	    Draw (rp, x + xoff, y + yoff);
 	}
     }
 
