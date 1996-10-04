660f7b4beb1351e08d3f59ebdbd03fffcd051854
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-10-04 14:35:41 +0000

    Make "path x: ADD" work as expected. "path x:" works now, too
    git-svn-id: https://svn.aros.org/svn/aros/trunk@417 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/path.c
+++ workbench/c/path.c
@@ -2,6 +2,9 @@
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
     $Log$
+    Revision 1.5  1996/10/04 14:35:41  digulla
+    Make "path x: ADD" work as expected. "path x:" works now, too
+
     Revision 1.4  1996/09/17 16:43:01  digulla
     Use general startup code
 
@@ -19,10 +22,13 @@
 #include <dos/dosextens.h>
 #include <clib/dos_protos.h>
 
+UBYTE Buffer[4096];
+
 int main (int argc, char ** argv)
 {
     struct RDArgs *rda;
     IPTR args[6]={ 0, 0, 0, 0, 0, 0 };
+    IPTR parg[1];
 
     rda=ReadArgs("PATH/M,ADD/S,SHOW/S,RESET/S,REMOVE/S,QUIET/S",args,NULL);
     if(rda!=NULL)
@@ -31,31 +37,54 @@ int main (int argc, char ** argv)
 	BPTR *cur, *next;
 	struct CommandLineInterface *cli;
 	cli=Cli();
-	cur=(BPTR *)BADDR(cli->cli_CommandDir);
-	while(cur)
+	if (*names)
 	{
-	    next=(BPTR *)cur[0];
-	    UnLock(cur[1]);
-	    FreeVec(cur);
-	    cur=next;
-	}
-	cur=&cli->cli_CommandDir;
-	while(*names!=NULL)
-	{
-	    next=(BPTR *)AllocVec(2*sizeof(BPTR),MEMF_ANY);
-	    next[1]=Lock(*names,SHARED_LOCK);
-	    if(!next[1])
+	    /* Search last entry */
+	    cur=&cli->cli_CommandDir;
+	    while (cur[0])
+		cur=(BPTR *)BADDR(cur[0]);
+
+	    while(*names!=NULL)
 	    {
-		FreeVec(next);
-		break;
+		next=(BPTR *)AllocVec(2*sizeof(BPTR),MEMF_ANY);
+		next[1]=Lock(*names,SHARED_LOCK);
+		if(!next[1])
+		{
+		    FreeVec(next);
+		    break;
+		}
+		cur[0]=MKBADDR(next);
+		cur=next;
+		if(!args[5])
+		    VPrintf("%s added.\n",(ULONG *)names);
+		names++;
 	    }
-	    cur[0]=MKBADDR(next);
-	    cur=next;
-	    if(!args[5])
-		VPrintf("%s added.\n",(ULONG *)names);
-	    names++;
+	    cur[0] = 0;
 	}
-	cur[0]=0;
+	else
+	{
+	    BPTR l;
+
+	    l = Lock ("", SHARED_LOCK);
+	    if (l)
+	    {
+		NameFromLock (l, Buffer, sizeof (Buffer));
+		parg[0] = (IPTR) Buffer;
+		VPrintf ("Current Directory: %s\n", parg);
+		UnLock (l);
+	    }
+
+	    cur=(BPTR *)BADDR(cli->cli_CommandDir);
+	    while(cur)
+	    {
+		NameFromLock (cur[1], Buffer, sizeof (Buffer));
+		parg[0] = (IPTR) Buffer;
+		VPrintf ("%s\n", parg);
+		cur=(BPTR *)BADDR(cur[0]);
+	    }
+	    VPrintf ("C:\n", NULL);
+	}
+
 	FreeArgs(rda);
     }
     return 0;
