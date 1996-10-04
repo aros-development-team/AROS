265d710b671735d8873f73f59b2784a3b8dbbb86
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-10-04 17:09:44 +0000

    More readable way to access arguments
    git-svn-id: https://svn.aros.org/svn/aros/trunk@437 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/path.c
+++ workbench/c/path.c
@@ -2,6 +2,9 @@
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
     $Log$
+    Revision 1.6  1996/10/04 17:09:44  digulla
+    More readable way to access arguments
+
     Revision 1.5  1996/10/04 14:35:41  digulla
     Make "path x: ADD" work as expected. "path x:" works now, too
 
@@ -27,13 +30,20 @@ UBYTE Buffer[4096];
 int main (int argc, char ** argv)
 {
     struct RDArgs *rda;
-    IPTR args[6]={ 0, 0, 0, 0, 0, 0 };
+    IPTR args[6]={ 0, 0, 1, 0, 0, 0 };
+#define ARG_Path	((STRPTR *)args[0])
+#define ARG_Add 	((BOOL)args[1])
+#define ARG_Show	((BOOL)args[2])
+#define ARG_Reset	((BOOL)args[3])
+#define ARG_Remove	((BOOL)args[4])
+#define ARG_Quiet	((BOOL)args[5])
+
     IPTR parg[1];
 
     rda=ReadArgs("PATH/M,ADD/S,SHOW/S,RESET/S,REMOVE/S,QUIET/S",args,NULL);
     if(rda!=NULL)
     {
-	STRPTR *names=(STRPTR *)args[0];
+	STRPTR *names=ARG_Path;
 	BPTR *cur, *next;
 	struct CommandLineInterface *cli;
 	cli=Cli();
@@ -55,7 +65,7 @@ int main (int argc, char ** argv)
 		}
 		cur[0]=MKBADDR(next);
 		cur=next;
-		if(!args[5])
+		if(!ARG_Quiet)
 		    VPrintf("%s added.\n",(ULONG *)names);
 		names++;
 	    }
