94425d2f71e4bf46aa3a1fc7791a75ade237b55c
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-10-10 13:28:18 +0000

    ? (Fleischer)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@471 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/dos/filesystem.h
+++ compiler/include/dos/filesystem.h
@@ -13,51 +13,49 @@ struct IOFileSys
     Filesystem actions. Look into one of the filesystem sources for more
     information on required parameters.
 */
-#define FSA_MOUNT		1
-#define FSA_DISMOUNT		2
-#define FSA_OPEN		3
-#define FSA_CLOSE		4
-#define FSA_READ		5
-#define FSA_WRITE		6
-#define FSA_SEEK		7
-#define FSA_SET_FILE_SIZE	8
-#define FSA_WAIT_CHAR		9
-#define FSA_FILE_MODE		10
-#define FSA_IS_INTERACTIVE	11
-#define FSA_SAME_LOCK		12
-#define FSA_EXAMINE		13
-#define FSA_EXAMINE_ALL 	14
-#define FSA_EXAMINE_ALL_END	15
-#define FSA_OPEN_FILE		16
-#define FSA_CREATE_DIR		17
-#define FSA_CREATE_HARDLINK	18
-#define FSA_CREATE_SOFTLINK	19
-#define FSA_RENAME		20
-#define FSA_READ_SOFTLINK	21
-#define FSA_DELETE_OBJECT	22
-#define FSA_SET_COMMENT 	23
-#define FSA_SET_PROTECT 	24
-#define FSA_SET_OWNER		25
-#define FSA_SET_DATE		26
-#define FSA_IS_FILESYSTEM	27
-#define FSA_MORE_CACHE		28
-#define FSA_FORMAT		29
-#define FSA_MOUNT_MODE		30
+#define FSA_OPEN		1
+#define FSA_CLOSE		2
+#define FSA_READ		3
+#define FSA_WRITE		4
+#define FSA_SEEK		5
+#define FSA_SET_FILE_SIZE	6
+#define FSA_WAIT_CHAR		7
+#define FSA_FILE_MODE		8
+#define FSA_IS_INTERACTIVE	9
+#define FSA_SAME_LOCK		10
+#define FSA_EXAMINE		11
+#define FSA_EXAMINE_ALL		12
+#define FSA_EXAMINE_ALL_END	13
+#define FSA_OPEN_FILE		14
+#define FSA_CREATE_DIR		15
+#define FSA_CREATE_HARDLINK	16
+#define FSA_CREATE_SOFTLINK	17
+#define FSA_RENAME		18
+#define FSA_READ_SOFTLINK	19
+#define FSA_DELETE_OBJECT	20
+#define FSA_SET_COMMENT		21
+#define FSA_SET_PROTECT		22
+#define FSA_SET_OWNER		23
+#define FSA_SET_DATE		24
+#define FSA_IS_FILESYSTEM	25
+#define FSA_MORE_CACHE		26
+#define FSA_FORMAT		27
+#define FSA_MOUNT_MODE		28
 #if 0
-#define FSA_SERIALIZE_DISK	30
-#define FSA_FLUSH		31
-#define FSA_INHIBIT		32
-#define FSA_WRITE_PROTECT	33
-#define FSA_DISK_CHANGE 	34
-#define FSA_ADD_NOTIFY		35
-#define FSA_REMOVE_NOTIFY	36
-#define FSA_DISK_INFO		37
-#define FSA_CHANGE_SIGNAL	38
-#define FSA_LOCK_RECORD 	39
-#define FSA_UNLOCK_RECORD	40
+#define FSA_SERIALIZE_DISK	29
+#define FSA_FLUSH		30
+#define FSA_INHIBIT		31
+#define FSA_WRITE_PROTECT	32
+#define FSA_DISK_CHANGE		33
+#define FSA_ADD_NOTIFY		34
+#define FSA_REMOVE_NOTIFY	35
+#define FSA_DISK_INFO		36
+#define FSA_CHANGE_SIGNAL	37
+#define FSA_LOCK_RECORD		38
+#define FSA_UNLOCK_RECORD	39
 #endif
 
-/* Modes for FSA_OPEN_FILE and FSA_FILE_MODE */
+/* Modes for FSA_OPEN, FSA_OPEN_FILE and FSA_FILE_MODE */
 #define FMF_LOCK	1 /* Lock exclusively */
 #define FMF_EXECUTE	2 /* open for executing */
 #define FMF_WRITE	4 /* open for writing */
