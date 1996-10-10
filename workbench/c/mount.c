ee58839f2ac4831b77a960df4894722dae7046ac
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-10-10 13:12:30 +0000

    Rewrote mount (Fleischer)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@455 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/mount.c
+++ workbench/c/mount.c
@@ -5,6 +5,35 @@
 #include <clib/dos_protos.h>
 #include <utility/tagitem.h>
 
+static LONG mount(struct IOFileSys *iofs, STRPTR filesys, STRPTR device)
+{
+    struct DosList *entry;
+    LONG error;
+    if(!OpenDevice(filesys,0,&iofs->IOFS,0))
+    {
+        entry=MakeDosEntry(device,DLT_DEVICE);
+        if(entry!=NULL)
+        {
+            if(AddDosEntry(entry))
+            {
+                entry->dol_Unit  =iofs->IOFS.io_Unit;
+                entry->dol_Device=iofs->IOFS.io_Device;
+                /*
+                    Neither close the device nor free the DosEntry.
+                    Both will stay in the dos list as long as the
+                    device is mounted.
+                */
+		return 0;
+	    }else
+	        error=IoErr();
+	    FreeDosEntry(entry);
+	}else
+	    error=ERROR_NO_FREE_STORE;
+    }else
+        error=ERROR_OBJECT_NOT_FOUND;
+    return error;
+}
+
 int main (int argc, char ** argv)
 {
     STRPTR args[2]={ NULL, NULL };
@@ -13,50 +42,21 @@ int main (int argc, char ** argv)
     struct Process *me=(struct Process *)FindTask(NULL);
     LONG error=0;
 
-    rda=ReadArgs("FILESYS/A,DEVICE/A",(IPTR *)args,NULL);
+    rda=ReadArgs("FILESYS/A,DEVICE/A",(ULONG *)args,NULL);
     if(rda!=NULL)
     {
 	iofs=(struct IOFileSys *)CreateIORequest(&me->pr_MsgPort,sizeof(struct IOFileSys));
 	if(iofs!=NULL)
 	{
-	    if(!OpenDevice(args[0],0,&iofs->IOFS,0))
-	    {
-		iofs->IOFS.io_Command=FSA_MOUNT;
-		iofs->io_Args[0]=(IPTR)args[1];
-		iofs->io_Args[1]=0;
-		if (DoIO(&iofs->IOFS))
-		{
-		    VPrintf ("Mount of %s: failed\n", (ULONG *)&args[1]);
+            SetIoErr(mount(iofs,args[0],args[1]));
+            if(IoErr())
 		    error = RETURN_FAIL;
-		    PrintFault(IoErr(),"Mount");
-		}
-
-		/*
-		    Don't close the device so that it has a non-zero
-		    open count as long as it is mounted. (Whoever wants
-		    to dismount it must close it as well).
-		    CloseDevice(&iofs->IOFS);
-		*/
-	    }
-	    else
-	    {
-		VPrintf ("OpenDevice (%s) failed\n", (ULONG *)args);
-		error = RETURN_FAIL;
-		PrintFault(IoErr(),"Mount");
-	    }
-
 	    DeleteIORequest(&iofs->IOFS);
 	}
-	else
-	    error = RETURN_FAIL;
-
 	FreeArgs(rda);
-    }
-    else
-    {
+    }else
 	error=RETURN_FAIL;
+    if(error)
 	PrintFault(IoErr(),"Mount");
-    }
-
     return error;
 }
