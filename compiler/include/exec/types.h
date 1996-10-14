bac6fd6e087a3dd3f474f286ff0c5bfce3cd6614
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-10-14 11:16:25 +0000

    Protect all typedefs with #define __typedef_*
    git-svn-id: https://svn.aros.org/svn/aros/trunk@476 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/exec/types.h
+++ compiler/include/exec/types.h
@@ -5,6 +5,9 @@
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
     $Log$
+    Revision 1.2  1996/10/14 11:16:25  digulla
+    Protect all typedefs with #define __typedef_*
+
     Revision 1.1  1996/09/13 17:55:10  digulla
     AROS special include file exec/types.h. Supports QUAD and IPTR
 
@@ -24,19 +27,24 @@
  ***** Basic Data types 	 *****
  *************************************/
 
-#ifndef APTR_TYPEDEF
-#define APTR_TYPEDEF
+#ifndef __typedef_APTR
+#define __typedef_APTR
 typedef void *		    APTR;   /* memory pointer */
 #endif
 
 /* An integer which can store a pointer */
-#ifdef AROS_IPTR_TYPE
-typedef AROS_IPTR_TYPE		    IPTR;
-#else
-typedef unsigned long		    IPTR;
+#ifndef __typedef_IPTR
+#   define __typedef_IPTR
+#   ifdef AROS_IPTR_TYPE
+        typedef AROS_IPTR_TYPE		    IPTR;
+#   else
+        typedef unsigned long		    IPTR;
+#   endif
 #endif
 
 /* Distinguish between 64 and 32bit systems */
+#ifndef __typedef_LONG
+#define __typedef_LONG
 #ifdef AROS_32BIT_TYPE
 typedef   signed AROS_32BIT_TYPE    LONG;   /* signed 32-bit value */
 typedef unsigned AROS_32BIT_TYPE    ULONG;  /* unsigned 32-bit value */
@@ -44,7 +52,10 @@ typedef unsigned AROS_32BIT_TYPE    ULONG;  /* unsigned 32-bit value */
 typedef   signed long		    LONG;   /* signed 32-bit value */
 typedef unsigned long		    ULONG;  /* unsigned 32-bit value */
 #endif
+#endif
 
+#ifndef __typedef_QUAD
+#define __typedef_QUAD
 #ifdef AROS_64BIT_TYPE
 typedef   signed AROS_64BIT_TYPE    QUAD;   /* signed 64-bit value */
 typedef unsigned AROS_64BIT_TYPE    UQUAD;  /* unsigned 64-bit-value */
@@ -57,7 +68,10 @@ typedef struct {  LONG high, low }  QUAD;   /* signed 64-bit value */
 typedef struct { ULONG high, low }  QUAD;   /* unsigned 64-bit value */
 #endif /* __GNUC__ */
 #endif /* AROS_64BIT_TYPE */
+#endif
 
+#ifndef __typedef_WORD
+#define __typedef_WORD
 #ifdef AROS_16BIT_TYPE
 typedef   signed AROS_16BIT_TYPE    WORD;   /* signed 64-bit value */
 typedef unsigned AROS_16BIT_TYPE    UWORD;  /* unsigned 64-bit-value */
@@ -65,19 +79,39 @@ typedef unsigned AROS_16BIT_TYPE    UWORD;  /* unsigned 64-bit-value */
 typedef   signed short		    WORD;   /* signed 16-bit value */
 typedef unsigned short		    UWORD;  /* unsigned 16-bit value */
 #endif
+#endif
 
+#ifndef __typedef_BYTE
+#define __typedef_BYTE
 typedef signed char		    BYTE;   /* signed 8-bit value */
 typedef unsigned char		    UBYTE;  /* unsigned 8-bit value */
+#endif
 
 
 /*************************************
  ***** Other interesting types	 *****
  *************************************/
 
+#ifndef __typedef_STRPTR
+#define __typedef_STRPTR
 typedef UBYTE * STRPTR;     /* Pointer to string (NULL terminated) */
+#endif
+
+#ifndef __typedef_BOOL
+#define __typedef_BOOL
 typedef short	BOOL;	    /* A Boolean value */
+#endif
+
+#ifndef __typedef_FLOAT
+#define __typedef_FLOAT
 typedef float	FLOAT;	    /* 32bit IEEE floating point value */
+#endif
+
+#ifndef __typedef_DOUBLE
+#define __typedef_DOUBLE
 typedef double	DOUBLE;     /* 64bit IEEE floating point value */
+#endif
+
 
 /*************************************
  ***** Some useful definitions	 *****
@@ -115,4 +149,4 @@ typedef double	DOUBLE;     /* 64bit IEEE floating point value */
     to OpenLibrary(). Some system libraries may not be at this version. */
 #define INCLUDE_VERSION 40
 
-#endif
+#endif /* exec/types.h */
