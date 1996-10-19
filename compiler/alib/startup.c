8b0e634db31de78dc9613018588c7f8084ab83d4
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-10-19 17:02:33 +0000

    Added support for malloc() and free(). I added it to the general startup, because it uses only a few bytes and with MemHeaders, it would have been quite a mess :-) This way, everything is *very* simple.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@514 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/alib/startup.c
+++ compiler/alib/startup.c
@@ -2,6 +2,11 @@
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
     $Log$
+    Revision 1.3  1996/10/19 17:02:33  aros
+    Added support for malloc() and free(). I added it to the general startup,
+    because it uses only a few bytes and with MemHeaders, it would have been
+    quite a mess :-) This way, everything is *very* simple.
+
     Revision 1.2  1996/09/17 18:07:19  digulla
     DOSBase and SysBase are now declared in the respective header files.
     The type of DOSBase is "struct DosLibrary *". Fixed everywhere
@@ -20,6 +25,7 @@
 
 /* Don't define symbols before the entry point. */
 extern int main (int argc, char ** argv);
+extern APTR __startup_mempool; /* malloc() and free() */
 
 __AROS_LH0(LONG,entry,struct ExecBase *,sysbase,,)
 {
@@ -39,9 +45,15 @@ __AROS_LH0(LONG,entry,struct ExecBase *,sysbase,,)
 
 	CloseLibrary((struct Library *)DOSBase);
     }
+
+    if (__startup_mempool)
+	DeletePool (__startup_mempool);
+
     return error;
     __AROS_FUNC_EXIT
 }
 
 struct ExecBase *SysBase;
 struct DosLibrary *DOSBase;
+
+APTR __startup_mempool = NULL;
