d102de630848e00a2acf564f9eeee7b4a78c7d50
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-10-21 07:40:50 +0000

    Removed the Log because it causes too much trouble
    git-svn-id: https://svn.aros.org/svn/aros/trunk@534 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-all/disable.s
+++ arch/i386-all/disable.s
@@ -1,32 +1,8 @@
 #    (C) 1995-96 AROS - The Amiga Replacement OS
 #    $Id$
-#    $Log$
-#    Revision 1.8  1996/10/18 16:48:06  aros
-#    It's sigprocmask for Linux and others use the #define above
 #
-#    Revision 1.7  1996/10/18 01:12:52	aros
-#    Added small patch to tell FreeBSD to use _sigprocmask not sigprocmask.
-#
-#    Revision 1.6  1996/10/10 13:24:47	digulla
-#    Make timer work (Fleischer)
-#
-#    Revision 1.5  1996/09/11 16:54:26	digulla
-#    Always use __AROS_SLIB_ENTRY() to access shared external symbols, because
-#	some systems name an external symbol "x" as "_x" and others as "x".
-#	(The problem arises with assembler symbols which might differ)
-#
-#    Revision 1.4  1996/08/23 16:49:20	digulla
-#    With some systems, .align 16 aligns to 64K instead of 16bytes. Therefore
-#	I replaced it with .balign which does what we want.
-#
-#    Revision 1.3  1996/08/13 14:03:18	digulla
-#    Added standard headers
-#
-#    Revision 1.2  1996/08/01 17:41:24	digulla
-#    Added standard header for all files
-#
-#    Desc:
-#    Lang:
+#    Desc: Exec function Disable
+#    Lang: english
 
 #*****************************************************************************
 #
