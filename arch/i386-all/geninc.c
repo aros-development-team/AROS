64babb4aa3f78e72ea82abffc9a739af0db285ef
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-10-23 14:22:04 +0000

    Added more constants for stack checking code
    Use hex output for flags and bits
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@590 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-all/geninc.c
+++ arch/i386-all/geninc.c
@@ -1,3 +1,4 @@
+#include <exec/alerts.h>
 #include <exec/types.h>
 #include <exec/execbase.h>
 #include <exec/tasks.h>
@@ -19,7 +20,6 @@ int main (void)
     printf ("\n# struct Task\n");
     printf ("\ttc_State      = %d\n", offsetof (struct Task, tc_State));
     printf ("\ttc_Flags      = %d\n", offsetof (struct Task, tc_Flags));
-    printf ("\ttc_SPLower    = %d\n", offsetof (struct Task, tc_SPLower));
     printf ("\ttc_ExceptCode = %d\n", offsetof (struct Task, tc_ExceptCode));
     printf ("\ttc_ExceptData = %d\n", offsetof (struct Task, tc_ExceptData));
     printf ("\ttc_SigExcept  = %d\n", offsetof (struct Task, tc_SigExcept));
@@ -27,6 +27,8 @@ int main (void)
     printf ("\ttc_Launch     = %d\n", offsetof (struct Task, tc_Launch));
     printf ("\ttc_Switch     = %d\n", offsetof (struct Task, tc_Switch));
     printf ("\ttc_SPReg      = %d\n", offsetof (struct Task, tc_SPReg));
+    printf ("\ttc_SPLower    = %d\n", offsetof (struct Task, tc_SPLower));
+    printf ("\ttc_SPUpper    = %d\n", offsetof (struct Task, tc_SPUpper));
     printf ("\ttc_IDNestCnt  = %d\n", offsetof (struct Task, tc_IDNestCnt));
 
     printf ("\n# struct DosBase\n");
@@ -35,17 +37,22 @@ int main (void)
     printf ("\n# Task Flags\n");
     printf ("\tTS_RUN        = %d\n", TS_RUN);
     printf ("\tTS_READY      = %d\n", TS_READY);
-    printf ("\tTF_EXCEPT     = %ld\n", TF_EXCEPT);
-    printf ("\tTF_SWITCH     = %ld\n", TF_SWITCH);
+    printf ("\tTF_EXCEPT     = 0x%04lX\n", TF_EXCEPT);
+    printf ("\tTF_SWITCH     = 0x%04lX\n", TF_SWITCH);
 
     printf ("\n# Exec functions\n");
     printf ("\tSwitch        = %d\n", FuncOffset (6));
     printf ("\tDispatch      = %d\n", FuncOffset (7));
     printf ("\tException     = %d\n", FuncOffset (8));
+    printf ("\tAlert         = %d\n", FuncOffset (18));
     printf ("\tDisable       = %d\n", FuncOffset (20));
     printf ("\tEnable        = %d\n", FuncOffset (21));
     printf ("\tEnqueue       = %d\n", FuncOffset (45));
     printf ("\tStackSwap     = %d\n", FuncOffset (122));
+
+    printf ("\n# Constants\n");
+    printf ("\tAT_DeadEnd    = 0x%08X\n", AT_DeadEnd);
+    printf ("\tAN_StackProbe = 0x%08X\n", AN_StackProbe);
 
     return 0;
 }
