21f278649be35f83a0443219e6229ad5dd63f41b
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-10-24 01:30:56 +0000

    A small file to generate all offsets for the assembler files
    git-svn-id: https://svn.aros.org/svn/aros/trunk@612 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/.unmaintained/m68k-native/geninc.c
@@ -0,0 +1,60 @@
+#include <exec/alerts.h>
+#include <exec/types.h>
+#include <exec/execbase.h>
+#include <exec/tasks.h>
+#include <dos/dosextens.h>
+#include <stdio.h>
+#include <stddef.h>
+
+#define FuncOffset(x)       (int)__AROS_GETJUMPVEC(0,x)
+
+int main (void)
+{
+    printf ("# ExecBase\n");
+    printf ("\tAttnResched   = %d\n", (int)offsetof (struct ExecBase, AttnResched));
+    printf ("\tIDNestCnt     = %d\n", (int)offsetof (struct ExecBase, IDNestCnt));
+    printf ("\tTDNestCnt     = %d\n", (int)offsetof (struct ExecBase, TDNestCnt));
+    printf ("\tTaskReady     = %d\n", (int)offsetof (struct ExecBase, TaskReady));
+    printf ("\tThisTask      = %d\n", (int)offsetof (struct ExecBase, ThisTask));
+
+    printf ("\n# struct Task\n");
+    printf ("\ttc_State      = %d\n", (int)offsetof (struct Task, tc_State));
+    printf ("\ttc_Flags      = %d\n", (int)offsetof (struct Task, tc_Flags));
+    printf ("\ttc_ExceptCode = %d\n", (int)offsetof (struct Task, tc_ExceptCode));
+    printf ("\ttc_ExceptData = %d\n", (int)offsetof (struct Task, tc_ExceptData));
+    printf ("\ttc_SigExcept  = %d\n", (int)offsetof (struct Task, tc_SigExcept));
+    printf ("\ttc_SigRecvd   = %d\n", (int)offsetof (struct Task, tc_SigRecvd));
+    printf ("\ttc_Launch     = %d\n", (int)offsetof (struct Task, tc_Launch));
+    printf ("\ttc_Switch     = %d\n", (int)offsetof (struct Task, tc_Switch));
+    printf ("\ttc_SPReg      = %d\n", (int)offsetof (struct Task, tc_SPReg));
+    printf ("\ttc_SPLower    = %d\n", (int)offsetof (struct Task, tc_SPLower));
+    printf ("\ttc_SPUpper    = %d\n", (int)offsetof (struct Task, tc_SPUpper));
+    printf ("\ttc_IDNestCnt  = %d\n", (int)offsetof (struct Task, tc_IDNestCnt));
+
+    printf ("\n# struct DosBase\n");
+    printf ("\tdl_SysBase    = %d\n", (int)offsetof (struct DosLibrary, dl_SysBase));
+
+    printf ("\n# Task Flags\n");
+    printf ("\tTS_RUN        = %d\n", TS_RUN);
+    printf ("\tTS_READY      = %d\n", TS_READY);
+    printf ("\tTF_EXCEPT     = 0x%04lX\n", TF_EXCEPT);
+    printf ("\tTF_SWITCH     = 0x%04lX\n", TF_SWITCH);
+
+    printf ("\n# Exec functions\n");
+    printf ("\tSwitch        = %d\n", FuncOffset (6));
+    printf ("\tDispatch      = %d\n", FuncOffset (7));
+    printf ("\tException     = %d\n", FuncOffset (8));
+    printf ("\tAlert         = %d\n", FuncOffset (18));
+    printf ("\tDisable       = %d\n", FuncOffset (20));
+    printf ("\tEnable        = %d\n", FuncOffset (21));
+    printf ("\tEnqueue       = %d\n", FuncOffset (45));
+    printf ("\tStackSwap     = %d\n", FuncOffset (122));
+
+    printf ("\n# Constants\n");
+    printf ("\tAT_DeadEnd    = 0x%08X\n", AT_DeadEnd);
+    printf ("\tAN_StackProbe = 0x%08X\n", AN_StackProbe);
+
+    return 0;
+}
+
+
