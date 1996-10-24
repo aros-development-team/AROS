d5e72366792278f8cf1c126f165073e473438f39
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-10-24 01:33:10 +0000

    Needed by GCC
    git-svn-id: https://svn.aros.org/svn/aros/trunk@614 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/bcopy.c
@@ -0,0 +1,54 @@
+/*
+    (C) 1995-96 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: bcopy()
+    Lang: english
+*/
+#include <clib/exec_protos.h>
+
+/*****************************************************************************
+
+    NAME */
+	#include <string.h>
+
+	void bcopy (
+
+/*  SYNOPSIS */
+	const void * src,
+	void *       dst,
+	size_t       len)
+
+/*  FUNCTION
+	Copy the contents of a part of memory to another. Both areas
+	must not overlap. If they do, use memmove().
+
+    INPUTS
+	src - The first byte of the source area in memory
+	dst - The first byte of the destination area in memory
+	len - How many bytes to copy.
+
+    RESULT
+	dest.
+
+    NOTES
+	The original bcopy() allows overlapping src and dst.
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	memmove(), exec/CopyMem()
+
+    INTERNALS
+
+    HISTORY
+	22-10-96    ldp created
+
+******************************************************************************/
+{
+    CopyMem ((UBYTE *)src, dst, len);
+
+} /* bcopy */
+
