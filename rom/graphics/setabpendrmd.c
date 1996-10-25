14a3afefaf35e9e94fd7d520c8419d45d6b7d7cf
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-10-25 14:17:31 +0000

    New function
    git-svn-id: https://svn.aros.org/svn/aros/trunk@643 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/graphics/setabpendrmd.c
@@ -0,0 +1,72 @@
+/*
+    (C) 1995 AROS - The Amiga Replacement OS
+    $Id$    $Log
+    Desc:
+    Lang: english
+*/
+#include "graphics_intern.h"
+
+extern void driver_SetABPenDrMd (struct RastPort *, ULONG, ULONG, ULONG,
+				struct GfxBase * GfxBase);
+
+/*****************************************************************************
+
+    NAME */
+	#include <graphics/rastport.h>
+	#include <clib/graphics_protos.h>
+
+	AROS_LH4(void, SetABPenDrMd,
+
+/*  SYNOPSIS */
+	AROS_LHA(struct RastPort *, rp, A1),
+	AROS_LHA(ULONG            , apen, D0),
+	AROS_LHA(ULONG            , bpen, D1),
+	AROS_LHA(ULONG            , drawMode, D2),
+
+/*  LOCATION */
+	struct GfxBase *, GfxBase, 149, Graphics)
+
+/*  FUNCTION
+	Changes the foreground and background pen and the drawmode in one
+	step.
+
+    INPUTS
+	rp - Modify this RastPort
+	apen - The new foreground pen
+	bpen - The new background pen
+	drawmode - The new drawmode
+
+    RESULT
+	None.
+
+    NOTES
+	This function is faster than the sequence SetAPen(), SetBPen(),
+	SetDrMd().
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+	29-10-95    digulla automatically created from
+			    graphics_lib.fd and clib/graphics_protos.h
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+    AROS_LIBBASE_EXT_DECL(struct GfxBase *,GfxBase)
+
+    /* Allow the driver to do its magic */
+    driver_SetABPenDrMd (rp, apen, bpen, drawMode, GfxBase);
+
+    /* Do it after the driver to allow it to inspect the previous value */
+    rp->FgPen = apen;
+    rp->BgPen = bpen;
+    rp->DrawMode = drawMode;
+
+    AROS_LIBFUNC_EXIT
+} /* SetABPenDrMd */
