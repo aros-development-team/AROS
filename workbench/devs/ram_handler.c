befea72f021581f0d3255c742326557f034dcbb3
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-11-13 14:11:17 +0000

    Removed log and added Desc
    git-svn-id: https://svn.aros.org/svn/aros/trunk@741 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/devs/ram_handler.c
+++ workbench/devs/ram_handler.c
@@ -1,31 +1,9 @@
 /*
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
-    $Log$
-    Revision 1.7  1996/10/24 15:50:22  aros
-    Use the official AROS macros over the __AROS versions.
 
-    Revision 1.6  1996/10/23 14:21:17  aros
-    Renamed a few macros from XYZ to AROS_XYZ so we know which if from AROS and
-    which not.
-
-    Revision 1.5  1996/10/19 17:07:21  aros
-    Include <aros/machine.h> instead of machine.h
-
-    Revision 1.4  1996/10/10 13:16:27  digulla
-    ? (Fleischer)
-
-    Revision 1.3  1996/09/13 17:57:05  digulla
-    Use IPTR
-
-    Revision 1.2  1996/09/12 14:52:01  digulla
-    Use correct way to access external names (was missing)
-
-    Revision 1.1  1996/09/11 12:52:53  digulla
-    Two new devices by M. Fleischer: RAM: and NIL:
-
-    Desc:
-    Lang:
+    Desc: RAM: handler
+    Lang: english
 */
 #include <exec/errors.h>
 #include <exec/types.h>
