be352b0ffcadef4a8ad34d9f9ea00cf86de2b2cc
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-11-16 01:27:30 +0000

    Stub functions (for now)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@749 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/.unmaintained/amiga/graphics_driver.c
@@ -0,0 +1,185 @@
+
+#define DEBUG_FreeMem 1
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <exec/memory.h>
+#include <clib/exec_protos.h>
+#include <graphics/rastport.h>
+#include <graphics/gfxbase.h>
+#include <graphics/text.h>
+#include <clib/graphics_protos.h>
+#include <clib/aros_protos.h>
+#include "graphics_intern.h"
+
+#define static	/* nothing */
+
+extern void _aros_not_implemented(void);
+
+int driver_init (struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+    return FALSE;
+}
+
+int driver_open (struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+    return FALSE;
+}
+
+void driver_close (struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+    return;
+}
+
+void driver_expunge (struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+    return;
+}
+
+void driver_SetABPenDrMd (struct RastPort * rp, ULONG apen, ULONG bpen,
+	ULONG drmd, struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+}
+
+void driver_SetAPen (struct RastPort * rp, ULONG pen,
+		    struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+}
+
+void driver_SetBPen (struct RastPort * rp, ULONG pen,
+		    struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+}
+
+void driver_SetOutlinePen (struct RastPort * rp, ULONG pen,
+		    struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+}
+
+void driver_SetDrMd (struct RastPort * rp, ULONG mode,
+		    struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+}
+
+void driver_EraseRect (struct RastPort * rp, LONG x1, LONG y1, LONG x2, LONG y2,
+		    struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+}
+
+void driver_RectFill (struct RastPort * rp, LONG x1, LONG y1, LONG x2, LONG y2,
+		    struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+}
+
+void driver_ScrollRaster (struct RastPort * rp, LONG dx, LONG dy,
+	LONG x1, LONG y1, LONG x2, LONG y2, struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+}
+
+void driver_DrawEllipse (struct RastPort * rp, LONG x, LONG y, LONG rx, LONG ry,
+		struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+}
+
+void driver_Text (struct RastPort * rp, STRPTR string, LONG len,
+		struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+}
+
+WORD driver_TextLength (struct RastPort * rp, STRPTR string, ULONG len,
+		    struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+    return 0;
+}
+
+void driver_Move (struct RastPort * rp, LONG x, LONG y,
+		    struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+    return;
+}
+
+void driver_Draw (struct RastPort * rp, LONG x, LONG y,
+		    struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+}
+
+ULONG driver_ReadPixel (struct RastPort * rp, LONG x, LONG y,
+		    struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+    return 0;
+}
+
+LONG driver_WritePixel (struct RastPort * rp, LONG x, LONG y,
+		    struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+    return 0;
+}
+
+void driver_PolyDraw (struct RastPort * rp, LONG count, WORD * coords,
+		    struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+}
+
+void driver_SetRast (struct RastPort * rp, ULONG color,
+		    struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+}
+
+void driver_SetFont (struct RastPort * rp, struct TextFont * font,
+		    struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+}
+
+struct TextFont * driver_OpenFont (struct TextAttr * ta,
+	struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+    return 0;
+}
+
+void driver_CloseFont (struct TextFont * tf, struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+}
+
+int driver_InitRastPort (struct RastPort * rp, struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+    return FALSE;
+}
+
+int driver_CloneRastPort (struct RastPort * newRP, struct RastPort * oldRP,
+			struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+    return FALSE;
+}
+
+void driver_DeinitRastPort (struct RastPort * rp, struct GfxBase * GfxBase)
+{
+    _aros_not_implemented();
+}
+
---
+++ arch/.unmaintained/amiga/intuition_driver.c
@@ -0,0 +1,134 @@
+
+#define DEBUG_FreeMem 1
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <exec/memory.h>
+#include <dos/dos.h>
+#include <utility/tagitem.h>
+#include <clib/exec_protos.h>
+#include <clib/intuition_protos.h>
+#include <clib/graphics_protos.h>
+#include <clib/aros_protos.h>
+#include "intuition_intern.h"
+#include "gadgets.h"
+#include "propgadgets.h"
+
+#define DEBUG	0
+#define DEBUG_ProcessXEvents	0
+
+#if DEBUG
+#   define D(x)     x
+#else
+#   define D(x)     /* eps */
+#endif
+
+#define bug	    kprintf
+
+extern void _aros_not_implemented(void);
+
+int intui_init (struct IntuitionBase * IntuitionBase)
+{
+    _aros_not_implemented();
+    return FALSE;
+}
+
+int intui_open (struct IntuitionBase * IntuitionBase)
+{
+    _aros_not_implemented();
+    return FALSE;
+}
+
+void intui_close (struct IntuitionBase * IntuitionBase)
+{
+    _aros_not_implemented();
+    return;
+}
+
+void intui_expunge (struct IntuitionBase * IntuitionBase)
+{
+    _aros_not_implemented();
+    return;
+}
+
+void intui_SetWindowTitles (struct Window * win, UBYTE * text, UBYTE * screen)
+{
+    _aros_not_implemented();
+}
+
+int intui_GetWindowSize (void)
+{
+    _aros_not_implemented();
+    return 0;
+}
+
+int intui_OpenWindow (struct Window * iw,
+	struct IntuitionBase * IntuitionBase)
+{
+    _aros_not_implemented();
+    return 0;
+}
+
+void intui_CloseWindow (struct Window * iw,
+	    struct IntuitionBase * IntuitionBase)
+{
+    _aros_not_implemented();
+}
+
+void intui_WindowToFront (struct Window * window)
+{
+    _aros_not_implemented();
+}
+
+void intui_WindowToBack (struct Window * window)
+{
+    _aros_not_implemented();
+}
+
+long StateToQualifier (unsigned long state)
+{
+    _aros_not_implemented();
+    return 0;
+} /* StateToQualifier */
+
+void intui_SizeWindow (struct Window * win, long dx, long dy)
+{
+    _aros_not_implemented();
+}
+
+void intui_ActivateWindow (struct Window * win)
+{
+    _aros_not_implemented();
+}
+
+LONG intui_RawKeyConvert (struct InputEvent * ie, STRPTR buf,
+	LONG size, struct KeyMap * km)
+{
+    _aros_not_implemented();
+    return 0;
+} /* intui_RawKeyConvert */
+
+void intui_BeginRefresh (struct Window * win,
+	struct IntuitionBase * IntuitionBase)
+{
+    _aros_not_implemented();
+} /* intui_BeginRefresh */
+
+void intui_EndRefresh (struct Window * win, BOOL free, struct IntuitionBase * IntuitionBase)
+{
+    _aros_not_implemented();
+} /* intui_EndRefresh */
+
+
+struct Gadget * FindGadget (struct Window * window, int x, int y)
+{
+    _aros_not_implemented();
+    return 0;
+} /* FindGadget */
+
+void intui_ProcessEvents (void)
+{
+    _aros_not_implemented();
+}
+
