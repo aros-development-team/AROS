46fb71583c4c83258a3e33108c3cbcde1fb21fc9
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-11-16 01:32:00 +0000

    Cheat and use the Amiga's real Supervisor() routine (for now)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@753 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/.unmaintained/m68k-native/supervisor.s
+++ arch/.unmaintained/m68k-native/supervisor.s
@@ -1,6 +1,9 @@
 #    (C) 1995-96 AROS - The Amiga Replacement OS
 #    $Id$
 #    $Log$
+#    Revision 1.6  1996/11/16 01:32:00  aros
+#    Cheat and use the Amiga's real Supervisor() routine (for now)
+#
 #    Revision 1.5  1996/11/01 02:05:25  aros
 #    Motorola syntax (no more MIT)
 #
@@ -55,48 +58,62 @@
 #
 #******************************************************************************
 
-	ThisTask    =	0x114
+	.include "machine.i"
+
 	tc_TrapCode =	0x32
 
+	.text
+	.balign 16
 	.globl	_Exec_Supervisor
+	.type   _Exec_Supervisor,@function
 _Exec_Supervisor:
-	| a privileged do-nothing instruction
-	or.w	#0x0000,sr
-	| No trap? Then this was called from supervisor mode. Prepare a rte.
-	move.w	sr,-(sp)
-	jmp	(a5)
+	| I'm cheating my way through this routine, because ixemul traps all
+	| exceptions. We would never be able to finish this function.
+	| I need to change one register, though, because I need to get this
+	| Amiga's original ExecBase. I'll use register a4 for this. I also
+	| assume the original Supervisor() function doesn't expect it's SysBase
+	| in a6.
 
-	| CPU privilege violation vector points to here
-	.globl	_TrapLevel8
-_TrapLevel8:
+	move.l	4.w,a4			| get original SysBase
+	jmp	Supervisor(a4)		| call that Supervisor() function
 
-	| There's only one legal location which may do a privilege
-	| violation - and that's the instruction above.
-	cmp.l	#_Exec_Supervisor,2.w(sp)
-	jne	pv
-	| All OK. Prepare returnaddress and go to the right direction.
-	move.l	#end,2.w(sp)
-	jmp	(a5)
-end:	rts
-
-	| Store trap number
-pv:	move.l	#8,-(sp)
-	bra	_TrapEntry
-
-	| And handle the trap
-_TrapEntry:
-	| Simple disable
-	or.w	#0x0700,sr
-
-	| get some room for destination address
-	subq.w	#4,sp
-
-	| calculate destination address without clobbering any registers
-	move.l	a0,-(sp)
-	move.l	4,a0
-	move.l	ThisTask(a0),a0
-	move.l	tc_TrapCode(a0),4(sp)
-	move.l	(sp)+,a0
-
-	| and jump
-	rts
+|	| a privileged do-nothing instruction
+|	or.w	#0x0000,sr
+|	| No trap? Then this was called from supervisor mode. Prepare a rte.
+|	move.w	sr,-(sp)
+|	jmp	(a5)
+|
+|	| CPU privilege violation vector points to here
+|	.globl	_TrapLevel8
+|_TrapLevel8:
+|
+|	| There's only one legal location which may do a privilege
+|	| violation - and that's the instruction above.
+|	cmp.l	#_Exec_Supervisor,2.w(sp)
+|	jne	pv
+|	| All OK. Prepare returnaddress and go to the right direction.
+|	move.l	#end,2.w(sp)
+|	jmp	(a5)
+|end:	rts
+|
+|	| Store trap number
+|pv:	move.l	#8,-(sp)
+|	bra	_TrapEntry
+|
+|	| And handle the trap
+|_TrapEntry:
+|	| Simple disable
+|	or.w	#0x0700,sr
+|
+|	| get some room for destination address
+|	subq.w	#4,sp
+|
+|	| calculate destination address without clobbering any registers
+|	move.l	a0,-(sp)
+|	move.l	4,a0
+|	move.l	ThisTask(a0),a0
+|	move.l	tc_TrapCode(a0),4(sp)
+|	move.l	(sp)+,a0
+|
+|	| and jump
+|	rts
