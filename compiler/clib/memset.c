f9dd2c22fabdcce8d70e52e16b608f2d998281c0
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-11-25 17:42:57 +0000

    New function memset()
    git-svn-id: https://svn.aros.org/svn/aros/trunk@825 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/memset.c
@@ -0,0 +1,78 @@
+/*
+    (C) 1995-96 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: ANSI C function memcpy()
+    Lang: english
+*/
+#include <clib/exec_protos.h>
+
+/*****************************************************************************
+
+    NAME */
+	#include <string.h>
+
+	void * memset (
+
+/*  SYNOPSIS */
+	void * dest,
+	int    c,
+	size_t count)
+
+/*  FUNCTION
+	Fill the memory at dest with count times c.
+
+    INPUTS
+	dest - The first byte of the destination area in memory
+	c - The byte to fill memory with
+	count - How many bytes to write
+
+    RESULT
+	dest.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	memmove(), memcpy()
+
+    INTERNALS
+
+    HISTORY
+	24-12-95    digulla created
+
+******************************************************************************/
+{
+    UBYTE * ptr = dest;
+
+    while (((IPTR)ptr)&(AROS_LONGALIGN-1) && count)
+    {
+	*ptr ++ = c;
+	count --;
+    }
+
+    if (count > sizeof(ULONG))
+    {
+	ULONG * ulptr = (ULONG *)ptr;
+	ULONG fill;
+
+	fill = (ULONG)(c & 0xFF);
+	fill = (fill <<  8) | fill;
+	fill = (fill << 16) | fill;
+
+	while (count > sizeof(ULONG))
+	{
+	    *ulptr ++ = fill;
+	    count -= sizeof(ULONG);
+	}
+    }
+
+    while (count --)
+	*ptr ++ = c;
+
+    return dest;
+} /* memset */
+
