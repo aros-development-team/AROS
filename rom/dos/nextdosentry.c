d42d1d68eadbe6fe275247028743e8da32e093ab
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-12-05 15:42:23 +0000

    Initial revision
    git-svn-id: https://svn.aros.org/svn/aros/trunk@884 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/dos/nextdosentry.c
@@ -0,0 +1,76 @@
+/*
+    (C) 1995-96 AROS - The Amiga Replacement OS
+    $Id$
+    $Log$
+    Revision 1.1  1996/12/05 15:42:23  aros
+    Initial revision
+
+
+    Desc:
+    Lang: english
+*/
+#include <dos/dosextens.h>
+#include "dos_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+	#include <clib/dos_protos.h>
+
+	AROS_LH2I(struct DosList *, NextDosEntry,
+
+/*  SYNOPSIS */
+	AROS_LHA(struct DosList *, dlist, D1),
+	AROS_LHA(ULONG           , flags, D2),
+
+/*  LOCATION */
+	struct DosLibrary *, DOSBase, 115, Dos)
+
+/*  FUNCTION
+	Looks for the next dos list entry with the right type. The list
+	must be locked for this.
+
+    INPUTS
+	dlist - the value given by LockDosList() or the last call to
+		FindDosEntry().
+	flags - the same flags as given to LockDosList() or a subset
+		of them.
+
+    RESULT
+	Pointer to dos list entry found or NULL if the are no more entries.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+	27-11-96    digulla automatically created from
+			    dos_lib.fd and clib/dos_protos.h
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    static const ULONG flagarray[]=
+    { 0, LDF_DEVICES, LDF_ASSIGNS, LDF_VOLUMES, LDF_ASSIGNS, LDF_ASSIGNS };
+
+    /* Follow the list */
+    for(;;)
+    {
+        /* Get next entry. Return NULL if there is none. */
+        dlist=dlist->dol_Next;
+        if(dlist==NULL)
+            return NULL;
+
+        /* Check type */
+        if(flags&flagarray[dlist->dol_Type+1])
+            return dlist;
+    }
+    AROS_LIBFUNC_EXIT
+} /* NextDosEntry */
