734a6f69f2fb4644974e369b32ce0789c1c64789
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-12-06 15:44:53 +0000

    Implementation for Linux/m68k
    git-svn-id: https://svn.aros.org/svn/aros/trunk@914 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/.unmaintained/m68k-emul/cachecleare.s
+++ arch/.unmaintained/m68k-emul/cachecleare.s
@@ -1,70 +1,74 @@
-#    (C) 1995-96 AROS - The Amiga Replacement OS
-#    $Id$
-#    $Log$
-#    Revision 1.1  1996/12/05 15:30:58  aros
-#    Patches by Geert Uytterhoeven integrated
-#
-#    Revision 1.6  1996/10/24 15:51:08  aros
-#    Use the official AROS macros over the __AROS versions.
-#
-#    Revision 1.5  1996/09/11 16:54:25  digulla
-#    Always use __AROS_SLIB_ENTRY() to access shared external symbols, because
-#    	some systems name an external symbol "x" as "_x" and others as "x".
-#    	(The problem arises with assembler symbols which might differ)
-#
-#    Revision 1.4  1996/08/23 16:49:19	digulla
-#    With some systems, .align 16 aligns to 64K instead of 16bytes. Therefore
-#	I replaced it with .balign which does what we want.
-#
-#    Revision 1.3  1996/08/13 14:03:17	digulla
-#    Added standard headers
-#
-#    Revision 1.2  1996/08/01 17:41:06	digulla
-#    Added standard header for all files
-#
-#    Desc:
-#    Lang:
+/*
+    (C) 1995-96 AROS - The Amiga Replacement OS
+    $Id$
 
-#*****************************************************************************
-#
-#   NAME
-#	AROS_LH3(void, CacheClearE,
-#
-#   SYNOPSIS
-#	AROS_LHA(APTR,  address, A0),
-#	AROS_LHA(ULONG, length,  D0),
-#	AROS_LHA(ULONG, caches,  D1),
-#
-#   LOCATION
-#	struct ExecBase *, SysBase, 107, Exec)
-#
-#   FUNCTION
-#
-#   INPUTS
-#
-#   RESULT
-#
-#   NOTES
-#
-#   EXAMPLE
-#
-#   BUGS
-#
-#   SEE ALSO
-#
-#   INTERNALS
-#
-#   HISTORY
-#
-#******************************************************************************
+    Desc:
+    Lang:
+*/
 
-# This really should use the Linux/m68k `cacheflush' system call!
+/******************************************************************************
+
+    NAME
+        AROS_LH3(void, CacheClearE,
+
+    SYNOPSIS
+        AROS_LHA(APTR,  address, A0),
+        AROS_LHA(ULONG, length,  D0),
+        AROS_LHA(ULONG, caches,  D1),
+
+    LOCATION
+        struct ExecBase *, SysBase, 107, Exec)
+
+    FUNCTION
+        Flushes the contents of the CPU caches for a given area of memory
+        and a given cache.
+
+    INPUTS
+        address - First byte in memory
+        length  - number of bytes (~0 means all addresses).
+        caches  - CPU caches affected
+                    CACRF_ClearI - Instruction cache
+                    CACRF_ClearD - Data cache
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+	Currently this only works on Linux/m68k.
+	It ignores the caches argument and clears all caches.
+	
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+
+        #include "machine.i"
+
+	FirstArg	= 8
+	address		= FirstArg
+	length		= address+4
+	caches		= length+4
 
 	.text
 	.balign 16
-	.globl	_Exec_CacheClearE
-	.type	_Exec_CacheClearE,@function
-_Exec_CacheClearE:
-	/* Dummy */
+	.globl	AROS_SLIB_ENTRY(CacheClearE,Exec)
+	.type	AROS_SLIB_ENTRY(CacheClearE,Exec),@function
+AROS_SLIB_ENTRY(CacheClearE,Exec):
+	linkw	%fp,#0
+	movem.l	%d2-%d4,-(%sp)
+	moveq	#123,%d0
+	move.l	address(%fp),%d1
+	move.l	#1,%d2
+	move.l	#3,%d3
+	move.l	length(%fp),%d4
+	trap	#0
+	movem.l	-12(%fp),%d2-%d4
+	unlk	%fp
 	rts
 
