f077f69dee3ea18e06cbad5958fa0c9a5d43db22
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-12-06 08:50:01 +0000

    Polish
    git-svn-id: https://svn.aros.org/svn/aros/trunk@899 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-all/supervisor.s
+++ arch/i386-all/supervisor.s
@@ -12,9 +12,8 @@
 	AROS_LH0(void, Supervisor,
 
     SYNOPSIS
-/* Aaron: please enable this */
-/* 	AROS_LHA(ULONG_FUNC, userFunction, A5), */
- 
+	AROS_LHA(ULONG_FUNC, userFunction, A5),
+
     LOCATION
 	struct ExecBase *, SysBase, 5, Exec)
 
@@ -46,9 +45,9 @@
 	.type	AROS_SLIB_ENTRY(Supervisor,Exec),@function
 AROS_SLIB_ENTRY(Supervisor,Exec):
 	/* The emulation has no real supervisor mode. */
-	subl	$4,%esp
-	pushl	%eax
-	movl	12(%esp),%eax
-	movl	%eax,4(%esp)
-	popl	%eax
-	ret
+	subl	$4,%esp 	/* Delete return addess */
+	pushl	%eax		/* Save a register */
+	movl	12(%esp),%eax   /* Get function pointer */
+	movl	%eax,4(%esp)    /* Put function pointer on the stack */
+	popl	%eax		/* Restore register */
+	ret			/* Call user function */
