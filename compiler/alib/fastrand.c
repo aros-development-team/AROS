f170d762a2b91ff96082276e64d0bb3ab89abe75
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-12-06 15:02:02 +0000

    Two more functions after libnix code
    git-svn-id: https://svn.aros.org/svn/aros/trunk@910 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/alib/fastrand.c
@@ -0,0 +1,47 @@
+/*
+    (C) 1995-96 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc:
+    Lang: english
+*/
+
+/*****************************************************************************
+
+    NAME */
+#include <clib/alib_protos.h>
+
+	ULONG FastRand (
+
+/*  SYNOPSIS */
+	ULONG seed)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+	06.12.96 digulla Created after original from libnix
+
+******************************************************************************/
+{
+    ULONG a = seed << 1;
+
+    if ((LONG)seed <= 0)
+	a ^= 0x1d872b41;
+
+    return a;
+} /* FastRand */
+
---
+++ compiler/alib/rangerand.c
@@ -0,0 +1,64 @@
+/*
+    (C) 1995-96 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc:
+    Lang: english
+*/
+#include <exec/types.h>
+
+ULONG RangeSeed;
+
+/*****************************************************************************
+
+    NAME */
+#include <clib/alib_protos.h>
+
+	ULONG RangeRand (
+
+/*  SYNOPSIS */
+	ULONG maxValue)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+	06.12.96 digulla Created after original from libnix
+
+******************************************************************************/
+{
+    ULONG a = RangeSeed;
+    UWORD i = maxValue - 1;
+
+    do
+    {
+	ULONG b = a;
+
+	a <<= 1;
+
+	if ((LONG)b <= 0)
+	    a ^= 0x1d872b41;
+
+    } while ((i >>= 1));
+
+    RangeSeed = a;
+
+    if ((UWORD)maxValue)
+	return (UWORD)((UWORD)a * (UWORD)maxValue >> 16);
+
+    return (UWORD)a;
+} /* RangeRand */
+
