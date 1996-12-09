d524c6dc2406a0e2603e0a577dd3f2c694199c6c
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-12-09 13:27:14 +0000

    Use stack arguments and AROS_* macros
    git-svn-id: https://svn.aros.org/svn/aros/trunk@937 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/.unmaintained/m68k-emul/semaphoreglue.s
+++ arch/.unmaintained/m68k-emul/semaphoreglue.s
@@ -1,52 +1,52 @@
-#    (C) 1995-96 AROS - The Amiga Replacement OS
-#    $Id$
-#    $Log$
-#    Revision 1.1  1996/12/05 15:31:01  aros
-#    Patches by Geert Uytterhoeven integrated
-#
-#    Revision 1.5  1996/11/01 02:05:24  aros
-#    Motorola syntax (no more MIT)
-#
-#    Revision 1.4  1996/10/24 15:51:31  aros
-#    Use the official AROS macros over the __AROS versions.
-#
-#    Revision 1.3  1996/10/21 21:08:58  aros
-#    Changed AROS_LA to AROS_LHA
-#
-#    Revision 1.2  1996/08/01 17:41:36  digulla
-#    Added standard header for all files
-#
-#    Desc:
-#    Lang:
+/*
+    (C) 1995-96 AROS - The Amiga Replacement OS
+    $Id$
 
-	# The following functions are guaranteed to preserve
-	# all registers. But I don't want to write them completely
-	# in assembly - C is generally more readable.
-	# So I use those stubs to preserve the registers.
+    Desc: Semaphore glue functions
+    Lang: english
+*/
+
+#include "machine.i"
+
+/*
+    The following functions are guaranteed to preserve
+    all registers. But I don't want to write them completely
+    in assembly - C is generally more readable.
+    So I use those stubs to preserve the registers.
+*/
 
 	.text
 	.balign 16
-	.globl	_Exec__ObtainSemaphore
-	.type	_Exec__Obtainsemaphore,@function
-_Exec__ObtainSemaphore:
-	moveml	%d0-%d1/%a0-%a1,%sp@-
-	bsr	_Exec_ObtainSemaphore
-	moveml	%sp@+,%d0-%d1/%a0-%a1
+	.globl	AROS_SLIB_ENTRY(_ObtainSemaphore,Exec)
+	.type	AROS_SLIB_ENTRY(_ObtainSemaphore,Exec),@function
+AROS_SLIB_ENTRY(_ObtainSemaphore,Exec):
+	movem.l	%d0-%d1/%a0-%a1,-(%sp)
+	move.l	24(%sp),-(%sp)
+	move.l	24(%sp),-(%sp)
+	bsr.l	AROS_SLIB_ENTRY(ObtainSemaphore,Exec)
+	addq.w	#8,%sp
+	movem.l	(%sp)+,%d0-%d1/%a0-%a1
 	rts
 
-	.globl	_Exec__ReleaseSemaphore
-	.type	_Exec__ReleaseSemaphore,@function
-_Exec__ReleaseSemaphore:
-	moveml	%d0-%d1/%a0-%a1,%sp@-
-	bsr	_Exec_ReleaseSemaphore
-	moveml	%sp@+,%d0-%d1/%a0-%a1
+	.globl	AROS_SLIB_ENTRY(_ReleaseSemaphore,Exec)
+	.type	AROS_SLIB_ENTRY(_ReleaseSemaphore,Exec),@function
+AROS_SLIB_ENTRY(_ReleaseSemaphore,Exec):
+	movem.l	%d0-%d1/%a0-%a1,-(%sp)
+	move.l	24(%sp),-(%sp)
+	move.l	24(%sp),-(%sp)
+	bsr.l	AROS_SLIB_ENTRY(ReleaseSemaphore,Exec)
+	addq.w	#8,%sp
+	movem.l	(%sp)+,%d0-%d1/%a0-%a1
 	rts
 
-	.globl	_Exec__ObtainSemaphoreShared
-	.type	_Exec__ObtainSemaphoreShared,@function
-_Exec__ObtainSemaphoreShared:
-	moveml	%d0-%d1/%a0-%a1,%sp@-
-	bsr	_Exec_ObtainSemaphoreShared
-	moveml	%sp@+,%d0-%d1/%a0-%a1
+	.globl	AROS_SLIB_ENTRY(_ObtainSemaphoreShared,Exec)
+	.type	AROS_SLIB_ENTRY(_ObtainSemaphoreShared,Exec),@function
+AROS_SLIB_ENTRY(_ObtainSemaphoreShared,Exec):
+	movem.l	%d0-%d1/%a0-%a1,-(%sp)
+	move.l	24(%sp),-(%sp)
+	move.l	24(%sp),-(%sp)
+	bsr.l	AROS_SLIB_ENTRY(ObtainSemaphoreShared,Exec)
+	addq.w	#8,%sp
+	movem.l	(%sp)+,%d0-%d1/%a0-%a1
 	rts
 
