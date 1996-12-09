83e691f3ad46f0ba22bbea10ff90972d825543e5
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-12-09 14:08:26 +0000

    Use AROS_* macros
    git-svn-id: https://svn.aros.org/svn/aros/trunk@947 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/.unmaintained/m68k-emul/cachepredma.s
+++ arch/.unmaintained/m68k-emul/cachepredma.s
@@ -1,65 +1,46 @@
-#    (C) 1995-96 AROS - The Amiga Replacement OS
-#    $Id$
-#    $Log$
-#    Revision 1.1  1996/12/05 15:30:58  aros
-#    Patches by Geert Uytterhoeven integrated
-#
-#    Revision 1.6  1996/10/24 15:51:09  aros
-#    Use the official AROS macros over the __AROS versions.
-#
-#    Revision 1.5  1996/09/11 16:54:26  digulla
-#    Always use __AROS_SLIB_ENTRY() to access shared external symbols, because
-#    	some systems name an external symbol "x" as "_x" and others as "x".
-#    	(The problem arises with assembler symbols which might differ)
-#
-#    Revision 1.4  1996/08/23 16:49:20	digulla
-#    With some systems, .align 16 aligns to 64K instead of 16bytes. Therefore
-#	I replaced it with .balign which does what we want.
-#
-#    Revision 1.3  1996/08/13 14:03:18	digulla
-#    Added standard headers
-#
-#    Revision 1.2  1996/08/01 17:41:06	digulla
-#    Added standard header for all files
-#
-#    Desc:
-#    Lang:
+/*
+    (C) 1995-96 AROS - The Amiga Replacement OS
+    $Id$
 
-#*****************************************************************************
-#
-#   NAME
-#	AROS_LH0(void, CachePreDMA,
-#
-#   LOCATION
-#	struct ExecBase *, SysBase, 127, Exec)
-#
-#   FUNCTION
-#
-#   INPUTS
-#
-#   RESULT
-#
-#   NOTES
-#
-#   EXAMPLE
-#
-#   BUGS
-#
-#   SEE ALSO
-#
-#   INTERNALS
-#
-#   HISTORY
-#
-#******************************************************************************
+    Desc: Exec function CachePreDMA
+    Lang: english
+*/
 
-# This really should use the Linux/m68k `cacheflush' system call!
+/******************************************************************************
+
+    NAME
+	AROS_LH0(void, CachePreDMA,
+
+    LOCATION
+	struct ExecBase *, SysBase, 127, Exec)
+
+    FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+
+	#include "machine.i"
 
 	.text
 	.balign 16
-	.globl	_Exec_CachePreDMA
-	.type	_Exec_CachePreDMA,@function
-_Exec_CachePreDMA:
+	.globl	AROS_SLIB_ENTRY(CachePreDMA,Exec)
+	.type	AROS_SLIB_ENTRY(CachePreDMA,Exec),@function
+AROS_SLIB_ENTRY(CachePreDMA,Exec):
 	/* Dummy */
 	rts
 
--- arch/.unmaintained/m68k-emul/userstate.s
+++ arch/.unmaintained/m68k-emul/userstate.s
@@ -1,61 +1,46 @@
-#    (C) 1995-96 AROS - The Amiga Replacement OS
-#    $Id$
-#    $Log$
-#    Revision 1.1  1996/12/05 15:31:02  aros
-#    Patches by Geert Uytterhoeven integrated
-#
-#    Revision 1.5  1996/11/01 02:05:25  aros
-#    Motorola syntax (no more MIT)
-#
-#    Revision 1.4  1996/10/24 15:51:33  aros
-#    Use the official AROS macros over the __AROS versions.
-#
-#    Revision 1.3  1996/10/21 21:08:59  aros
-#    Changed AROS_LA to AROS_LHA
-#
-#    Revision 1.2  1996/08/01 17:41:38  digulla
-#    Added standard header for all files
-#
-#    Desc:
-#    Lang:
+/*
+    (C) 1995-96 AROS - The Amiga Replacement OS
+    $Id$
 
-#*****************************************************************************
-#
-#   NAME
-#
-#	AROS_LH1(void, UserState,
-#
-#   SYNOPSIS
-#	AROS_LHA(APTR, sysStack, D0),
-#
-#   LOCATION
-#	struct ExecBase *, SysBase, 26, Exec)
-#
-#   FUNCTION
-#	Return to user mode after a call to SuperState().
-#
-#   INPUTS
-#	sysStack - The returncode from SuperState().
-#
-#   RESULT
-#
-#   NOTES
-#
-#   EXAMPLE
-#
-#   BUGS
-#
-#   SEE ALSO
-#	SuperState(), Supervisor()
-#
-#   INTERNALS
-#
-#   HISTORY
-#
-#******************************************************************************
+    Desc: Exec function UserState
+    Lang: english
+*/
 
-	.globl	_Exec_UserState
-	.type	_Exec_UserState,@function
-_Exec_UserState:
-	| Dummy
+/******************************************************************************
+
+    NAME
+	AROS_LH0(void, UserState,
+
+    LOCATION
+	struct ExecBase *, SysBase, 26, Exec)
+
+    FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+
+	#include "machine.i"
+
+	.text
+	.balign 16
+	.globl	AROS_SLIB_ENTRY(UserState,Exec)
+	.type	AROS_SLIB_ENTRY(UserState,Exec),@function
+AROS_SLIB_ENTRY(UserState,Exec):
+	/* Dummy */
 	rts
+
