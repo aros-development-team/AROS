610da691a8e8ae8e3c4fbf6234b3971871ab0f61
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-12-10 16:09:34 +0000

    New functions
    git-svn-id: https://svn.aros.org/svn/aros/trunk@988 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/include/string.h
+++ compiler/clib/include/string.h
@@ -16,9 +16,13 @@ size_t strlen (const char * str);
 int strcasecmp (const char * s1, const char * s2);
 int stricmp (const char * s1, const char * s2);
 int strcmp (const char * s1, const char * s2);
+int strncmp (const char * s1, const char * s2);
 char * strdup (const char * str);
 char * strcpy (char * dest, const char * src);
+
+void *memchr(const void *dest, int c, size_t n);
 void *memcpy(void *dest, const void *src, size_t n);
+void *memmove(void *dest, const void *src, size_t n);
 void *memset(void *dest, int c, size_t n);
 void bcopy (const void *src, void *dst, size_t len);
 
---
+++ compiler/clib/memchr.c
@@ -0,0 +1,62 @@
+/*
+    (C) 1995-96 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: ANSI C function memcpy()
+    Lang: english
+*/
+#include <clib/exec_protos.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <string.h>
+
+	void * memchr (
+
+/*  SYNOPSIS */
+	const void * mem,
+	int	     c,
+	size_t	     n)
+
+/*  FUNCTION
+	Copy the contents of a part of memory to another. Both areas
+	must not overlap. If they do, use memmove().
+
+    INPUTS
+	dest - The first byte of the destination area in memory
+	src - The first byte of the source area in memory
+	count - How many bytes to copy
+
+    RESULT
+	dest.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+	24-12-95    digulla created
+
+******************************************************************************/
+{
+    const char * ptr = (char *)mem;
+
+    while (n)
+    {
+	if (*ptr == c)
+	    return ptr;
+
+	n --;
+	ptr ++;
+    }
+
+    return NULL;
+} /* memchr */
+
