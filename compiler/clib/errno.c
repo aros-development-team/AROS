51081b615427dad158904776b301ef1fc1d2e358
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-12-11 11:24:06 +0000

    Added headers
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1005 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/errno.c
+++ compiler/clib/errno.c
@@ -1,1 +1,9 @@
+/*
+    (C) 1995-96 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: Global variable errno
+    Lang: english
+*/
+
 int errno = 0;
--- compiler/clib/feof.c
+++ compiler/clib/feof.c
@@ -2,12 +2,44 @@
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
 
-    Desc:
+    Desc: ANSI C function feof()
     Lang: english
 */
+
+/*****************************************************************************
+
+    NAME */
 #include <stdio.h>
 
-int feof (FILE * stream)
+	int feof (
+
+/*  SYNOPSIS */
+	FILE * stream)
+
+/*  FUNCTION
+	Test the EOF-Flag of a stream. This flag is set automatically by
+	any function which recognises EOF. To clear it, call clearerr().
+
+    INPUTS
+	stream - The stream to be tested.
+
+    RESULT
+	!= 0, if the stream is at the end of the file, 0 otherwise.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	ferror(), clearerr()
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
 {
     return (stream->flags & _STDIO_FILEFLAG_EOF) != 0;
 } /* feof */
