b5343690152933fa0f319b52b8076711fec1742a
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-12-11 09:58:37 +0000

    Dump frame pointer
    git-svn-id: https://svn.aros.org/svn/aros/trunk@991 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/.unmaintained/m68k-emul/exception.s
+++ arch/.unmaintained/m68k-emul/exception.s
@@ -41,17 +41,16 @@
         .globl  AROS_SLIB_ENTRY(Exception,Exec)
         .type   AROS_SLIB_ENTRY(Exception,Exec),@function
 AROS_SLIB_ENTRY(Exception,Exec):
-	linkw	%fp,#0
-	movem.l	%a2/%d2,-(%sp)
+	movem.l	%a2/%a6/%d2,-(%sp)
 	/* First clear task exception bit. */
-	move.l	8(%fp),%a0
-	move.l	ThisTask(%a0),%a2
+	move.l	16(%sp),%a6
+	move.l	ThisTask(%a6),%a2
 	bclr	#TB_EXCEPT,tc_Flags(%a2)
 
 	/* If the exception is raised out of Wait IDNestCnt may be >0 */
-	move.b	IDNestCnt(%a0),%d2
+	move.b	IDNestCnt(%a6),%d2
 	/* Set it to a defined value */
-	clr.b	IDNestCnt(%a0)
+	clr.b	IDNestCnt(%a6)
 
 exloop:	/* Get mask of signals causing the exception */
 	move.l	tc_SigExcept(%a2),%d0
@@ -64,15 +63,15 @@ exloop:	/* Get mask of signals causing the exception */
 
 	/* Raise exception. Enable Interrupts */
 	move.l	tc_ExceptData(%a2),%a1
-	move.l	%a0,-(%sp)
-	jsr	Enable(%a0)
-	move.l	%a0,-(%sp)
+	move.l	%a6,-(%sp)
+	jsr	Enable(%a6)
+	move.l	%a6,-(%sp)
 	move.l	%a1,-(%sp)
 	move.l	%d0,-(%sp)
 	move.l	tc_ExceptCode(%a2),%a1
 	jsr	(%a1)
-	move.l	%a0,-(%sp)
-	jsr	Disable(%a0)
+	move.l	%a6,-(%sp)
+	jsr	Disable(%a6)
 	add.w	#20,%sp
 
 	/* Re-use returned bits */
@@ -80,8 +79,7 @@ exloop:	/* Get mask of signals causing the exception */
 	bra	exloop
 
 excend:	/* Restore IDNestCnt and return */
-	move.b	%d2,IDNestCnt(%a0)
-	movem.l	-8(%fp),%a2/%d2
-	unlk	%fp
+	move.b	%d2,IDNestCnt(%a6)
+	movem.l	(%sp)+,%a2/%a6/%d2
 	rts
 
