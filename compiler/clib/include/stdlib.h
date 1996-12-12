3a8532b67c74b1a5ba365affeaa6bc985a668b0c
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-12-12 16:08:31 +0000

    Moved memory functions in this file because ANSI expects them here
    Added a couple of new functions
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1014 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/include/stdlib.h
+++ compiler/clib/include/stdlib.h
@@ -12,7 +12,28 @@
 #   include <sys/types.h>
 #endif
 
-void qsort(void *array, size_t count, size_t elementsize,
+int abs (int j);
+long labs (long j);
+int atoi (const char * str);
+long atol (const char * str);
+long strtol (const char * str, char ** endptr, int base);
+unsigned long strtoul (const char * str, char ** endptr, int base);
+
+int rand (void);
+void srand (unsigned int seed);
+/* Max. number returned by rand() */
+#ifndef RAND_MAX
+#   define RAND_MAX	   2147483647
+#endif
+
+void qsort (void * array, size_t count, size_t elementsize,
 	int (*comparefunction)(const void * element1, const void * element2));
+void * bsearch (const void * key, const void * base, size_t count,
+	size_t size, int (*comparefunction)(const void *, const void *));
+
+void * malloc (size_t size);
+void * calloc (size_t count, size_t size);
+void * realloc (void * oldmem, size_t newsize);
+void free (void * memory);
 
 #endif /* _STDLIB_H */
