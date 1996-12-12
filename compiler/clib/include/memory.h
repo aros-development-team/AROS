9574c792a7c2bf723c98aa778c276784b7e3972e
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-12-12 16:07:51 +0000

    Moved functions into stdlib.h (where ANSI expects them)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1013 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/include/memory.h
+++ compiler/clib/include/memory.h
@@ -5,16 +5,11 @@
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
 
-    Desc: ANSI-C header file memory.h
+    Desc: Header file memory.h
     Lang: english
 */
-#ifndef _SYS_TYPES_H
-#   include <sys/types.h>
+#ifndef _STDLIB_H
+#   include <stdlib.h>
 #endif
-
-void * malloc (size_t size);
-void * calloc (size_t count, size_t size);
-void * realloc (void * oldmem, size_t newsize);
-void free (void * memory);
 
 #endif /* _MEMORY_H */
