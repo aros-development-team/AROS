80f1a09037a9a868763c559933a58d7f85559f99
iaint <iaint@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1996-12-27 08:19:34 +0000

    Use UFC macro for registerized params
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1037 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/utility/callhookpkt.c
+++ rom/utility/callhookpkt.c
@@ -1,6 +1,10 @@
 /*
     (C) 1995 AROS - The Amiga Replacement OS
-    $Id$    $Log
+    $Id$
+    $Log$
+    Revision 1.4  1996/12/27 08:19:34  iaint
+    Use UFC macro for registerized params
+
     Desc:
     Lang: english
 */
@@ -23,7 +27,7 @@
 	struct Library *, UtilityBase, 17, Utility)
 
 /*  FUNCTION
-
+	
     INPUTS
 
     RESULT
@@ -47,7 +51,11 @@
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct Library *,UtilityBase)
 
-    return ((*(hook->h_Entry)) (hook, object, paramPacket));
+    return AROS_UFC3(IPTR, hook->h_Entry,
+	AROS_UFCA(struct Hook *, hook, A0),
+	AROS_UFCA(APTR,		 object, A2),
+	AROS_UFCA(APTR,	 paramPacket, A1)
+    );
 
     AROS_LIBFUNC_EXIT
 } /* CallHookPkt */
