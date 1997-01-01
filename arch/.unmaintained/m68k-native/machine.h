de271981e34c7f322825069426f5ddf01c601c1e
ldp <ldp@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-01-01 03:34:24 +0000

    Added inline support for the AROS_LVO_CALL macros and made no-return (void) version of these.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1042 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/.unmaintained/m68k-native/machine.h
+++ arch/.unmaintained/m68k-native/machine.h
@@ -30,10 +30,10 @@
 /*
     BCPL datatypes
 */
-#define AROS_BPTR_TYPE	long
-#define AROS_BSTR_TYPE  long
-#define MKBADDR(a)	(((long)(a))>>2)
-#define BADDR(a)	(((long)(a))<<2)
+#define AROS_BPTR_TYPE	unsigned long
+#define AROS_BSTR_TYPE  unsigned long
+#define MKBADDR(a)	(((unsigned long)(a))>>2)
+#define BADDR(a)	(((unsigned long)(a))<<2)
 
 /*
     One entry in a libraries' jumptable. For assembler compatibility, the
@@ -59,6 +59,233 @@ struct JumpVec
 #define __AROS_SETVECADDR(lib,n,addr)   (__AROS_SET_VEC(__AROS_GETJUMPVEC(lib,n),(APTR)(addr)))
 #define __AROS_INITVEC(lib,n)           __AROS_GETJUMPVEC(lib,n)->jmp = __AROS_ASMJMP, \
 					__AROS_SETVECADDR(lib,n,_aros_not_implemented)
+
+/*
+    The following AROS_LVO_CALLs are not protected by #ifdef/#endif pairs.
+    This is because these are really the ones to be defined, not the (protected)
+    ones from <aros/libcall.h>. Better to have a compiler warning, than to
+    define the wrong ones.
+
+    The reason to define them this way (jsr offset(a6)), is to jump over the
+    library vector, instead of getting the address from offset+2 and jumping
+    to that address. Some libraries may be putting something else (e.g. rts)
+    in the vector, instead of a 'jsr address'. Fetching the address from such
+    a vector would crash the AROS_LVO_CALLs in <aros/libcall.h>
+
+    Furthermore, this (and the shorter code) is exactly the reason
+    AROS/include/clib/ *.h and AROS/include/proto/ *.h containing the
+    non-registerized versions have to be removed from the directory tree.
+    If they are removed, gcc will fetch the compiler-specific protos, with
+    the inlines (in case of gcc).
+*/
+
+#define LVO_ARGT(t,v,r) t
+#define LVO_ARGV(t,v,r) v
+#define LVO_ARGR(t,v,r) r
+
+#define AROS_LVO_CALL0(rt,bt,bn,offs,name) \
+({							\
+   {							\
+      register rt _##name##_re __asm("d0");		\
+      register struct Library *const _##name##_bn __asm("a6") = (struct Library*)(bn);\
+      __asm volatile ("jsr a6@(-"#offs"*6:W)"		\
+      : "=r" (_##name##_re)				\
+      : "r" (_##name##_bn)				\
+      : "d0", "d1", "a0", "a1", "cc", "memory");	\
+      _##name##_re;					\
+   }							\
+})
+
+#define AROS_LVO_CALL0NR(bt,bn,offs,name)		\
+({							\
+   {							\
+      register struct Library *const _##name##_bn __asm("a6") = (struct Library*)(bn);\
+      __asm volatile ("jsr a6@(-"#offs":W)"		\
+      : /* no output */					\
+      : "r" (_##name##_bn)				\
+      : "d0", "d1", "a0", "a1", "cc", "memory");	\
+   }							\
+})
+
+#define AROS_LVO_CALL1(rt,a1,bt,bn,offs,name) \
+({							\
+   LVO_ARGT(a1) _##name##_v1 = (LVO_ARGV(a1));	\
+   {							\
+      register rt _##name##_re __asm("d0");		\
+      register struct Library *const _##name##_bn __asm("a6") = (struct Library*)(bn);\
+      register LVO_ARGT(a1) _n1 __asm(LVO_ARGR(a1)) = _##name##_v1;	\
+      __asm volatile ("jsr a6@(-"#offs"*6:W)"		\
+      : "=r" (_##name##_re)				\
+      : "r" (_##name##_bn), "r"(_n1)			\
+      : "d0", "d1", "a0", "a1", "cc", "memory");	\
+      _##name##_re;					\
+   }							\
+})
+
+#define AROS_LVO_CALL1NR(a1,bt,bn,offs,name) \
+({							\
+   LVO_ARGT(a1) _##name##_v1 = (LVO_ARGV(a1));	\
+   {							\
+      register struct Library *const _##name##_bn __asm("a6") = (struct Library*)(bn);\
+      register LVO_ARGT(a1) _n1 __asm(LVO_ARGR(a1)) = _##name##_v1;	\
+      __asm volatile ("jsr a6@(-"#offs"*6:W)"		\
+      : /* no output */                                 \
+      : "r" (_##name##_bn), "r"(_n1)			\
+      : "d0", "d1", "a0", "a1", "cc", "memory");	\
+   }							\
+})
+
+#define AROS_LVO_CALL2(rt,a1,a2,bt,bn,offs,name) \
+({							\
+   LVO_ARGT(a1) _##name##_v1 = (LVO_ARGV(a1));	\
+   LVO_ARGT(a2) _##name##_v2 = (LVO_ARGV(a2));	\
+   {							\
+      register rt _##name##_re __asm("d0");		\
+      register struct Library *const _##name##_bn __asm("a6") = (struct Library*)(bn);\
+      register LVO_ARGT(a1) _n1 __asm(LVO_ARGR(a1)) = _##name##_v1;	\
+      register LVO_ARGT(a2) _n2 __asm(LVO_ARGR(a2)) = _##name##_v2;	\
+      __asm volatile ("jsr a6@(-"#offs"*6:W)"		\
+      : "=r" (_##name##_re)				\
+      : "r" (_##name##_bn), "r"(_n1), "r"(_n2)		\
+      : "d0", "d1", "a0", "a1", "cc", "memory");	\
+      _##name##_re;					\
+   }							\
+})
+
+#define AROS_LVO_CALL2NR(a1,a2,bt,bn,offs,name) \
+({							\
+   LVO_ARGT(a1) _##name##_v1 = (LVO_ARGV(a1));	\
+   LVO_ARGT(a2) _##name##_v2 = (LVO_ARGV(a2));	\
+   {							\
+      register struct Library *const _##name##_bn __asm("a6") = (struct Library*)(bn);\
+      register LVO_ARGT(a1) _n1 __asm(LVO_ARGR(a1)) = _##name##_v1;	\
+      register LVO_ARGT(a2) _n2 __asm(LVO_ARGR(a2)) = _##name##_v2;	\
+      __asm volatile ("jsr a6@(-"#offs"*6:W)"		\
+      : /* no output */                                 \
+      : "r" (_##name##_bn), "r"(_n1), "r"(_n2)		\
+      : "d0", "d1", "a0", "a1", "cc", "memory");	\
+   }							\
+})
+
+#define AROS_LVO_CALL3(rt,a1,a2,a3,bt,bn,offs,name) \
+({							\
+   LVO_ARGT(a1) _##name##_v1 = (LVO_ARGV(a1));	\
+   LVO_ARGT(a2) _##name##_v2 = (LVO_ARGV(a2));	\
+   LVO_ARGT(a3) _##name##_v3 = (LVO_ARGV(a3));	\
+   {							\
+      register rt _##name##_re __asm("d0");		\
+      register struct Library *const _##name##_bn __asm("a6") = (struct Library*)(bn);\
+      register LVO_ARGT(a1) _n1 __asm(LVO_ARGR(a1)) = _##name##_v1;	\
+      register LVO_ARGT(a2) _n2 __asm(LVO_ARGR(a2)) = _##name##_v2;	\
+      register LVO_ARGT(a3) _n3 __asm(LVO_ARGR(a3)) = _##name##_v3;	\
+      __asm volatile ("jsr a6@(-"#offs"*6:W)"		\
+      : "=r" (_##name##_re)				\
+      : "r" (_##name##_bn), "r"(_n1), "r"(_n2), "r"(_n3)\
+      : "d0", "d1", "a0", "a1", "cc", "memory");	\
+      _##name##_re;					\
+   }							\
+})
+
+#define AROS_LVO_CALL3NR(a1,a2,a3,bt,bn,offs,name) \
+({							\
+   LVO_ARGT(a1) _##name##_v1 = (LVO_ARGV(a1));	\
+   LVO_ARGT(a2) _##name##_v2 = (LVO_ARGV(a2));	\
+   LVO_ARGT(a3) _##name##_v3 = (LVO_ARGV(a3));	\
+   {							\
+      register struct Library *const _##name##_bn __asm("a6") = (struct Library*)(bn);\
+      register LVO_ARGT(a1) _n1 __asm(LVO_ARGR(a1)) = _##name##_v1;	\
+      register LVO_ARGT(a2) _n2 __asm(LVO_ARGR(a2)) = _##name##_v2;	\
+      register LVO_ARGT(a3) _n3 __asm(LVO_ARGR(a3)) = _##name##_v3;	\
+      __asm volatile ("jsr a6@(-"#offs"*6:W)"		\
+      : /* no output */                                 \
+      : "r" (_##name##_bn), "r"(_n1), "r"(_n2), "r"(_n3)\
+      : "d0", "d1", "a0", "a1", "cc", "memory");	\
+   }							\
+})
+
+#define AROS_LVO_CALL4(rt,a1,a2,a3,a4,bt,bn,offs,name) \
+({							\
+   LVO_ARGT(a1) _##name##_v1 = (LVO_ARGV(a1));	\
+   LVO_ARGT(a2) _##name##_v2 = (LVO_ARGV(a2));	\
+   LVO_ARGT(a3) _##name##_v3 = (LVO_ARGV(a3));	\
+   LVO_ARGT(a4) _##name##_v4 = (LVO_ARGV(a4));	\
+   {							\
+      register rt _##name##_re __asm("d0");		\
+      register struct Library *const _##name##_bn __asm("a6") = (struct Library*)(bn);\
+      register LVO_ARGT(a1) _n1 __asm(LVO_ARGR(a1)) = _##name##_v1;	\
+      register LVO_ARGT(a2) _n2 __asm(LVO_ARGR(a2)) = _##name##_v2;	\
+      register LVO_ARGT(a3) _n3 __asm(LVO_ARGR(a3)) = _##name##_v3;	\
+      register LVO_ARGT(a4) _n4 __asm(LVO_ARGR(a4)) = _##name##_v4;	\
+      __asm volatile ("jsr a6@(-"#offs"*6:W)"		\
+      : "=r" (_##name##_re)				\
+      : "r" (_##name##_bn), "r"(_n1), "r"(_n2), "r"(_n3), "r"(_n4) \
+      : "d0", "d1", "a0", "a1", "cc", "memory");	\
+      _##name##_re;					\
+   }							\
+})
+
+#define AROS_LVO_CALL4NR(a1,a2,a3,a4,bt,bn,offs,name) \
+({							\
+   LVO_ARGT(a1) _##name##_v1 = (LVO_ARGV(a1));	\
+   LVO_ARGT(a2) _##name##_v2 = (LVO_ARGV(a2));	\
+   LVO_ARGT(a3) _##name##_v3 = (LVO_ARGV(a3));	\
+   LVO_ARGT(a4) _##name##_v4 = (LVO_ARGV(a4));	\
+   {							\
+      register struct Library *const _##name##_bn __asm("a6") = (struct Library*)(bn);\
+      register LVO_ARGT(a1) _n1 __asm(LVO_ARGR(a1)) = _##name##_v1;	\
+      register LVO_ARGT(a2) _n2 __asm(LVO_ARGR(a2)) = _##name##_v2;	\
+      register LVO_ARGT(a3) _n3 __asm(LVO_ARGR(a3)) = _##name##_v3;	\
+      register LVO_ARGT(a4) _n4 __asm(LVO_ARGR(a4)) = _##name##_v4;	\
+      __asm volatile ("jsr a6@(-"#offs"*6:W)"		\
+      : /* no output */                                 \
+      : "r" (_##name##_bn), "r"(_n1), "r"(_n2), "r"(_n3), "r"(_n4) \
+      : "d0", "d1", "a0", "a1", "cc", "memory");	\
+   }							\
+})
+
+#define AROS_LVO_CALL5(rt,a1,a2,a3,a4,a5,bt,bn,offs,name) \
+({							\
+   LVO_ARGT(a1) _##name##_v1 = (LVO_ARGV(a1));	\
+   LVO_ARGT(a2) _##name##_v2 = (LVO_ARGV(a2));	\
+   LVO_ARGT(a3) _##name##_v3 = (LVO_ARGV(a3));	\
+   LVO_ARGT(a4) _##name##_v4 = (LVO_ARGV(a4));	\
+   LVO_ARGT(a5) _##name##_v5 = (LVO_ARGV(a5));	\
+   {							\
+      register rt _##name##_re __asm("d0");		\
+      register struct Library *const _##name##_bn __asm("a6") = (struct Library*)(bn);\
+      register LVO_ARGT(a1) _n1 __asm(LVO_ARGR(a1)) = _##name##_v1;	\
+      register LVO_ARGT(a2) _n2 __asm(LVO_ARGR(a2)) = _##name##_v2;	\
+      register LVO_ARGT(a3) _n3 __asm(LVO_ARGR(a3)) = _##name##_v3;	\
+      register LVO_ARGT(a4) _n4 __asm(LVO_ARGR(a4)) = _##name##_v4;	\
+      register LVO_ARGT(a5) _n5 __asm(LVO_ARGR(a5)) = _##name##_v5;	\
+      __asm volatile ("jsr a6@(-"#offs"*6:W)"		\
+      : "=r" (_##name##_re)				\
+      : "r" (_##name##_bn), "r"(_n1), "r"(_n2), "r"(_n3), "r"(_n4), "r"(_n5) \
+      : "d0", "d1", "a0", "a1", "cc", "memory");	\
+      _##name##_re;					\
+   }							\
+})
+
+#define AROS_LVO_CALL5NR(a1,a2,a3,a4,a5,bt,bn,offs,name) \
+({							\
+   LVO_ARGT(a1) _##name##_v1 = (LVO_ARGV(a1));	\
+   LVO_ARGT(a2) _##name##_v2 = (LVO_ARGV(a2));	\
+   LVO_ARGT(a3) _##name##_v3 = (LVO_ARGV(a3));	\
+   LVO_ARGT(a4) _##name##_v4 = (LVO_ARGV(a4));	\
+   LVO_ARGT(a5) _##name##_v5 = (LVO_ARGV(a5));	\
+   {							\
+      register struct Library *const _##name##_bn __asm("a6") = (struct Library*)(bn);\
+      register LVO_ARGT(a1) _n1 __asm(LVO_ARGR(a1)) = _##name##_v1;	\
+      register LVO_ARGT(a2) _n2 __asm(LVO_ARGR(a2)) = _##name##_v2;	\
+      register LVO_ARGT(a3) _n3 __asm(LVO_ARGR(a3)) = _##name##_v3;	\
+      register LVO_ARGT(a4) _n4 __asm(LVO_ARGR(a4)) = _##name##_v4;	\
+      register LVO_ARGT(a5) _n5 __asm(LVO_ARGR(a5)) = _##name##_v5;	\
+      __asm volatile ("jsr a6@(-"#offs"*6:W)"		\
+      : /* no output */                                 \
+      : "r" (_##name##_bn), "r"(_n1), "r"(_n2), "r"(_n3), "r"(_n4), "r"(_n5) \
+      : "d0", "d1", "a0", "a1", "cc", "memory");	\
+   }							\
+})
 
 /* ??? */
 #define RDFCALL(hook,data,dptr) ((void(*)(UBYTE,APTR))(hook))(data,dptr);
@@ -91,6 +318,26 @@ extern void _aros_not_implemented (void);
 #define A4 "a4"
 #define A5 "a5"
 #define A6 "a6"
+
+/* SAS/C compatible declarations for register variables */
+#ifndef __SASC
+#define __d0 __asm("d0")
+#define __d1 __asm("d1")
+#define __d2 __asm("d2")
+#define __d3 __asm("d3")
+#define __d4 __asm("d4")
+#define __d5 __asm("d5")
+#define __d6 __asm("d6")
+#define __d7 __asm("d7")
+
+#define __a0 __asm("a0")
+#define __a1 __asm("a1")
+#define __a2 __asm("a2")
+#define __a3 __asm("a3")
+#define __a4 __asm("a4")
+#define __a5 __asm("a5")
+#define __a6 __asm("a6")
+#endif
 
 /* What to do with the library base in header, prototype and call */
 #define __AROS_LH_BASE(basetype,basename)   basetype basename __asm("a6")
