6f4ac20955830e0c3ea9beaf7c39b023b1854dcb
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-01-03 12:29:07 +0000

    Polish
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1058 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/insert.c
+++ rom/exec/insert.c
@@ -1,28 +1,6 @@
 /*
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
-    $Log$
-    Revision 1.9  1997/01/01 03:46:11  ldp
-    Committed Amiga native (support) code
-
-    Changed clib to proto
-
-    Revision 1.8  1996/12/10 13:51:48  aros
-    Moved all #include's in the first column so makedepend can see it.
-
-    Revision 1.7  1996/10/24 15:50:51  aros
-    Use the official AROS macros over the __AROS versions.
-
-    Revision 1.6  1996/10/21 20:47:53  aros
-    Changed struct SysBase to struct ExecBase
-
-    Revision 1.5  1996/08/13 13:56:03  digulla
-    Replaced AROS_LA by AROS_LHA
-    Replaced some AROS_LH*I by AROS_LH*
-    Sorted and added includes
-
-    Revision 1.4  1996/08/01 17:41:13  digulla
-    Added standard header for all files
 
     Desc:
     Lang: english
