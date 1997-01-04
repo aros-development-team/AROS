dde3d5aa83b51de80f38c58bdaffb93039fc274c
ldp <ldp@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-01-04 00:05:59 +0000

    Added native forbid permit disable getcc cacheclearu
    Protect certain SetFunction()s with Disable()/Enable()
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1061 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/execstrap_init.c
+++ rom/exec/execstrap_init.c
@@ -1,16 +1,25 @@
 #include <exec/types.h>
 #include <exec/resident.h>
 #include <exec/nodes.h>
+#include <exec/execbase.h>
 
 #include <proto/exec.h>
 
 #include "exec_extfuncs.h"
+
+/*
+    Architecture dependent function variations:
+*/
+extern void AROS_SLIB_ENTRY(GetCC_10,Exec)();
+extern void AROS_SLIB_ENTRY(CacheClearU_20,Exec)();
+extern void AROS_SLIB_ENTRY(CacheClearU_40,Exec)();
 
 int start(void)
 {
     struct ExecBase *SysBase;
     register ULONG x, y;
     UWORD *color00 = (void *)0xdff180;
+    UWORD cpuflags;
 
     /*
 	High-tech display tricks (blue effects) :-)
@@ -22,19 +31,29 @@ int start(void)
     }
 
     SysBase = *(void **)4;
+    cpuflags = SysBase->AttnFlags;
 
     /*
-	The biggie: SetFunction() as many library vectors as possible. Protection
-	from multitasking is provided by SetFunction (Forbid/Permit), but for
-	safety also arbitrate for interrupts (Disable/Enable).
+	The biggie: SetFunction() as many library vectors as possible.
+	Protection from multitasking is provided by SetFunction (Forbid/Permit).
+    */
+
+    /*
+	Some functions are safe to call even from interrupts, so protect these
+	with Disable/Enable.
+
+	Alert/Cause/Disable/Enable/FindName/FindPort/FindTask/PutMsg/ReplyMsg/Signal
+
+	AddHead/AddTail/Enqueue/RemHead/RemTail/Insert/Remove
     */
 
     Disable();
-
+    SetFunction((struct Library *)SysBase, ( 20 * -6), (APTR)&AROS_SLIB_ENTRY(Disable,Exec));
+    SetFunction((struct Library *)SysBase, ( 22 * -6), (APTR)&AROS_SLIB_ENTRY(Forbid,Exec));
 #if 0
-    /* Instant machine hang-up: */
-    SetFunction((struct Library *)SysBase, ( 28 * -6), (APTR)&AROS_SLIB_ENTRY(AddIntServer,Exec));
-    SetFunction((struct Library *)SysBase, ( 29 * -6), (APTR)&AROS_SLIB_ENTRY(RemIntServer,Exec));
+    /* "Some trouble prevented CycleToMenu to initialize itself properly"
+       Related to the microkernel */
+    SetFunction((struct Library *)SysBase, ( 23 * -6), (APTR)&AROS_SLIB_ENTRY(Permit,Exec));
 #endif
 #if 0
     /* E.g. grep gurus: */
@@ -46,15 +65,57 @@ int start(void)
     SetFunction((struct Library *)SysBase, ( 43 * -6), (APTR)&AROS_SLIB_ENTRY(RemHead,Exec));
     SetFunction((struct Library *)SysBase, ( 44 * -6), (APTR)&AROS_SLIB_ENTRY(RemTail,Exec));
     SetFunction((struct Library *)SysBase, ( 45 * -6), (APTR)&AROS_SLIB_ENTRY(Enqueue,Exec));
+
     SetFunction((struct Library *)SysBase, ( 46 * -6), (APTR)&AROS_SLIB_ENTRY(FindName,Exec));
     SetFunction((struct Library *)SysBase, ( 49 * -6), (APTR)&AROS_SLIB_ENTRY(FindTask,Exec));
+    SetFunction((struct Library *)SysBase, ( 65 * -6), (APTR)&AROS_SLIB_ENTRY(FindPort,Exec));
+    SetFunction((struct Library *)SysBase, ( 61 * -6), (APTR)&AROS_SLIB_ENTRY(PutMsg,Exec));
+
+#if 0
+    /* The "move.w ccr,d0" should really be implemented as part of the jumptable, for speed.
+       Is this desirable? */
+    if ((cpuflags & AFF_68010) == AFF_68010)
+	SetFunction((struct Library *)SysBase, ( 88 * -6), (APTR)&AROS_SLIB_ENTRY(GetCC_10,Exec));
+#endif
+
+    /*
+       BTW: What bit(s) is (are) set for the MC68060?
+       BTW2: They would really be set by the 68060.library, which will obviously
+             not have executed at this point in the reset-procedure.
+       BTW3: If there is an agreed upon bit for the 68060, we could examine the
+             type of processor for ourselves in exec.strap, and update AttnFlags
+             accordingly.
+       BTW4: The 68060 can be recognized by its Processor Configuration Register (PCR).
+             This register also contains the bit to enable Superscalar Operation,
+             which we could set at this point in the reset-procedure to speed
+             things up considerably (if nothing breaks).
+       BTW5: For the MC68060, we could also enable the Branch Cache at this point.
+    */
+    if ((cpuflags & AFF_68020) == AFF_68020)
+    {
+	/* If 68040 is set, it implies 020 and 030 bits also set. */
+	if ((cpuflags & AFF_68040) == AFF_68040)
+	{
+	    SetFunction((struct Library *)SysBase, (106 * -6), (APTR)&AROS_SLIB_ENTRY(CacheClearU_40,Exec));
+	}
+	else SetFunction((struct Library *)SysBase, (106 * -6), (APTR)&AROS_SLIB_ENTRY(CacheClearU_20,Exec));
+    }
+    else
+    {
+	SetFunction((struct Library *)SysBase, (106 * -6), (APTR)&AROS_SLIB_ENTRY(CacheClearU,Exec));
+    }
+    Enable();
+
+#if 0
+    /* Instant machine hang-up: */
+    SetFunction((struct Library *)SysBase, ( 28 * -6), (APTR)&AROS_SLIB_ENTRY(AddIntServer,Exec));
+    SetFunction((struct Library *)SysBase, ( 29 * -6), (APTR)&AROS_SLIB_ENTRY(RemIntServer,Exec));
+#endif
     SetFunction((struct Library *)SysBase, ( 51 * -6), (APTR)&AROS_SLIB_ENTRY(SetSignal,Exec));
     SetFunction((struct Library *)SysBase, ( 59 * -6), (APTR)&AROS_SLIB_ENTRY(AddPort,Exec));
     SetFunction((struct Library *)SysBase, ( 60 * -6), (APTR)&AROS_SLIB_ENTRY(RemPort,Exec));
-    SetFunction((struct Library *)SysBase, ( 61 * -6), (APTR)&AROS_SLIB_ENTRY(PutMsg,Exec));
     SetFunction((struct Library *)SysBase, ( 62 * -6), (APTR)&AROS_SLIB_ENTRY(GetMsg,Exec));
     SetFunction((struct Library *)SysBase, ( 64 * -6), (APTR)&AROS_SLIB_ENTRY(WaitPort,Exec));
-    SetFunction((struct Library *)SysBase, ( 65 * -6), (APTR)&AROS_SLIB_ENTRY(FindPort,Exec));
     SetFunction((struct Library *)SysBase, ( 66 * -6), (APTR)&AROS_SLIB_ENTRY(AddLibrary,Exec));
     SetFunction((struct Library *)SysBase, ( 67 * -6), (APTR)&AROS_SLIB_ENTRY(RemLibrary,Exec));
     SetFunction((struct Library *)SysBase, ( 68 * -6), (APTR)&AROS_SLIB_ENTRY(OldOpenLibrary,Exec));
@@ -99,8 +160,6 @@ int start(void)
     SetFunction((struct Library *)SysBase, (129 * -6), (APTR)&AROS_SLIB_ENTRY(AddMemHandler,Exec));
     SetFunction((struct Library *)SysBase, (130 * -6), (APTR)&AROS_SLIB_ENTRY(RemMemHandler,Exec));
     SetFunction((struct Library *)SysBase, (135 * -6), (APTR)&AROS_SLIB_ENTRY(TaggedOpenLibrary,Exec));
-
-    Enable();
 
     /*
 	High-tech display tricks (green effects) :-)
