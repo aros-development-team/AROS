8eb2ee6e3796361f7acf56519a4aab5ba5018fa2
ldp <ldp@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-01-10 04:07:56 +0000

    Removed log.
    Use funcInit instead of funcUnit+1 as a base for relative vectors.
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1104 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/makelibrary.c
+++ rom/exec/makelibrary.c
@@ -1,31 +1,6 @@
 /*
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
-    $Log$
-    Revision 1.9  1997/01/01 03:46:12  ldp
-    Committed Amiga native (support) code
-
-    Changed clib to proto
-
-    Revision 1.8  1996/12/10 13:51:48  aros
-    Moved all #include's in the first column so makedepend can see it.
-
-    Revision 1.7  1996/10/24 15:50:52  aros
-    Use the official AROS macros over the __AROS versions.
-
-    Revision 1.6  1996/10/23 14:28:54  aros
-    Use the respective macros to access and manipulate a libraries' jumptable
-
-    Revision 1.5  1996/10/19 17:07:27  aros
-    Include <aros/machine.h> instead of machine.h
-
-    Revision 1.4  1996/08/13 13:56:04  digulla
-    Replaced AROS_LA by AROS_LHA
-    Replaced some AROS_LH*I by AROS_LH*
-    Sorted and added includes
-
-    Revision 1.3  1996/08/01 17:41:14  digulla
-    Added standard header for all files
 
     Desc:
     Lang: english
@@ -33,6 +8,7 @@
 #include <exec/execbase.h>
 #include <exec/memory.h>
 #include <dos/dos.h>
+#include <aros/libcall.h>
 #include <aros/asmcall.h>
 #include <proto/exec.h>
 
@@ -129,7 +105,7 @@
 	/* Build jumptable */
 	if(*(WORD *)funcInit==-1)
 	    /* offsets */
-	    MakeFunctions(library,(WORD *)funcInit+1,(WORD *)funcInit+1);
+	    MakeFunctions(library,(WORD *)funcInit+1,(WORD *)funcInit);
 	else
 	    /* function pointers */
 	    MakeFunctions(library,funcInit,NULL);
