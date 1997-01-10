2d236c485ace5decd534534f6c87c31f7bbbe478
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-01-10 12:44:17 +0000

    Define _SIZE_T (Linux expects this when size_t is defined)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1109 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/include/sys/types.h
+++ compiler/clib/include/sys/types.h
@@ -1,8 +1,9 @@
 #ifndef _SYS_TYPES_H
 #define _SYS_TYPES_H
 
-#ifndef __typedef_size_t
+#if !defined(_SITE_T) && !defined(__typedef_size_t)
 #define __typedef_size_t
+#define _SIZE_T
 typedef unsigned int size_t;
 #endif
 
