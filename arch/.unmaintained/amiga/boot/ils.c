3ad779926e7d175c708c7e8e677bc90b374a0eb1
ldp <ldp@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-01-11 23:59:02 +0000

    Formatting.
    Removed unneeded local variable.
    
    Extra comments.
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1112 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/.unmaintained/amiga/boot/ils.c
+++ arch/.unmaintained/amiga/boot/ils.c
@@ -19,16 +19,25 @@
 
 extern struct ilsMemList ils_mem;
 
-LONG ils_read(BPTR handle __d1, void *buffer __d2, LONG length __d3, struct DosLibrary *DOSBase __a6)
+LONG ils_read(BPTR handle __d1, void *buffer __d2, LONG length __d3,
+	      struct DosLibrary *DOSBase __a6)
 {
     return( Read(handle, buffer, length) );
 }
 
-void *ils_alloc(ULONG size __d0, ULONG attrib __d1, struct ExecBase *SysBase __a6)
+void *ils_alloc(ULONG size __d0, ULONG attrib __d1,
+		struct ExecBase *SysBase __a6)
 {
     void *result;
-    ULONG savesize = size;
 
+    /*
+	Memory to be used for Resident modules can not be any kind of memory
+	available. It must be of a special type, MEMF_KICK, which indicates
+	that this memory is available very early during the reset procedure.
+	Also allocate memory from the top of the memory list, MEMF_REVERSE,
+	to keep all our allocations in one place, and to keep potential early
+	memory fragmentation down.
+    */
     attrib |= MEMF_KICK|MEMF_REVERSE;
 
     result = AllocMem(size, attrib);
@@ -45,23 +54,28 @@ void *ils_alloc(ULONG size __d0, ULONG attrib __d1, struct ExecBase *SysBase __a
 	if( (node = AllocMem(sizeof(struct ilsMemNode), MEMF_CLEAR)) )
 	{
 	    node->imn_Addr = result;
-	    node->imn_Size = savesize;
+	    node->imn_Size = size;
 	    AddHead((struct List *)&ils_mem, (struct Node *)node);
+
 	    /*
 		Keep a counter so we don't have to count nodes later.
 	    */
 	    ils_mem.iml_Num++;
+
 	    /*
 		This counts number of nodes since the loading of the last
 		module. This field is reset in the FindResMod() routine.
 	    */
 	    ils_mem.iml_NewNum++;
 	}
+	return(result);
     }
-    return(result);
+
+    return 0;
 }
 
-void ils_free(void *block __a1, ULONG size __d0, struct ExecBase *SysBase __a6)
+void ils_free(void *block __a1, ULONG size __d0,
+	      struct ExecBase *SysBase __a6)
 {
     void *saveblock = block;
     struct ilsMemNode *node;
@@ -70,7 +84,9 @@ void ils_free(void *block __a1, ULONG size __d0, struct ExecBase *SysBase __a6)
 
     /* now remove this block from our list */
     /* find it */
-    for(node = (struct ilsMemNode *)ils_mem.iml_List.mlh_Head; node->imn_Node.mln_Succ; node = (struct ilsMemNode *)node->imn_Node.mln_Succ)
+    for(node = (struct ilsMemNode *)ils_mem.iml_List.mlh_Head;
+	node->imn_Node.mln_Succ;
+	node = (struct ilsMemNode *)node->imn_Node.mln_Succ)
     {
 	/* is this the right block? */
 	if(node->imn_Addr == saveblock)
@@ -78,6 +94,7 @@ void ils_free(void *block __a1, ULONG size __d0, struct ExecBase *SysBase __a6)
 	    /* yes: remove from list and free it's memory */
 	    Remove((struct Node *)node);
 	    ils_mem.iml_Num--;
+	    ils_mem.iml_NewNum--;
 	    FreeMem(node, sizeof(struct ilsMemNode));
 	}
     }
