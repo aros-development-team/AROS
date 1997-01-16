e7e5033a583d3a280e697e569995d67518492753
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-01-16 17:42:13 +0000

    Polish
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1131 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/lddemon.c
+++ rom/dos/lddemon.c
@@ -2,35 +2,11 @@
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
     $Log$
-    Revision 1.10  1997/01/07 12:29:07  digulla
-    Removed AROS_LVO_CALL*NR() macros
+    Revision 1.11  1997/01/16 17:42:13  digulla
+    Polish
 
-    Revision 1.9  1997/01/01 05:01:13  ldp
-    Changed AROS_LVO_CALL3 to AROS_LVO_CALL3NR
 
-    Revision 1.8  1996/10/24 15:50:31  aros
-    Use the official AROS macros over the __AROS versions.
-
-    Revision 1.7  1996/10/23 14:13:43  aros
-    Use AROS_ALIGN() to align pointers
-
-    Revision 1.6  1996/10/21 20:44:51  aros
-    Changed d0 to D0
-
-    Revision 1.5  1996/09/12 14:53:20  digulla
-    The loader code should use symbolic names
-
-    Revision 1.4  1996/09/11 13:00:16  digulla
-    Use correct alignment (M. Fleischer)
-
-    Revision 1.3  1996/08/13 13:52:48  digulla
-    Replaced <dos/dosextens.h> by "dos_intern.h" or added "dos_intern.h"
-    Replaced AROS_LA by AROS_LHA
-
-    Revision 1.2  1996/08/01 17:40:53  digulla
-    Added standard header for all files
-
-    Desc:
+    Desc: Loader for shared libraries and devices
     Lang: english
 */
 #include <exec/execbase.h>
