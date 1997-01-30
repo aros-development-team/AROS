df982730b949fa0a291927f0271e290a82597961
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-01-30 09:31:55 +0000

    Added lots of comments.
    Added picture of the stack.
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1278 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-all/runprocess.s
+++ arch/i386-all/runprocess.s
@@ -15,14 +15,32 @@
 
 	#include "machine.i"
 
-	.set FirstArg, 4+(4*4)   /* Return-Adress + 4 Registers */
+/*
+    This is how the stack looks when this function is called:
+
+    20	DOSBase(4)
+    16	entry(4)
+    12	argsize(4)
+     8	argptr(4)
+     4	sss(4)
+     0	proc(4)
+	Return Address(4)
+
+
+/*	  .set FirstArg, 4+(4*4)   / * Return-Adress + 4 Registers * /
 	.set proc,     FirstArg
-	.set sss,      proc+4	 /* 24 */
+	.set sss,      proc+4
 	.set argptr,   sss+4
 	.set argsize,  argptr+4
-	.set entry,    argsize+4 /* 36 */
+	.set entry,    argsize+4 */
 
-#define DOSBase        entry+4
+#define FirstArg	4+(4*4)     /* Return-Adress + 4 Registers */
+#define proc		FirstArg+0
+#define sss		FirstArg+4
+#define argptr		FirstArg+8
+#define argsize 	FirstArg+12
+#define entry		FirstArg+18
+#define DOSBase 	FirstArg+20
 
 	.text
 	.balign 16
@@ -30,7 +48,7 @@
 	.type	AROS_SLIB_ENTRY(RunProcess,Dos),@function
 
 AROS_SLIB_ENTRY(RunProcess,Dos):
-	/* Save some registers */
+	/* Save some registers. This changes the offset of the arguments by 4*4 */
 	pushl %edi
 	pushl %esi
 	pushl %ebx
@@ -43,7 +61,15 @@ AROS_SLIB_ENTRY(RunProcess,Dos):
 	/* Move upper bounds of the new stack into eax */
 	movl stk_Upper(%ebx),%eax
 
-	/* Push arguments for entry onto stack */
+	/*
+	    Push arguments for entry onto the stack of the new process.
+	    This new stack looks like this when the new process is called:
+
+		sss
+		SysBase
+		argsize
+		argptr
+	*/
 	movl argptr(%esp),%edx
 	movl %edx,-16(%eax)
 	movl argsize(%esp),%edx
@@ -55,6 +81,7 @@ AROS_SLIB_ENTRY(RunProcess,Dos):
 	/* Get SysBase */
 	movl DOSBase(%esp),%edx
 	movl dl_SysBase(%edx),%edx
+
 	/* Push SysBase on the new stack */
 	movl %edx,-8(%eax)
 
@@ -65,28 +92,33 @@ AROS_SLIB_ENTRY(RunProcess,Dos):
 	/* Push SysBase and sss on our stack */
 	pushl %edx /* SysBase */
 	pushl %ebx /* sss */
+
 	/* Switch stacks */
 	leal StackSwap(%edx),%edx
 	call *%edx
+
 	/* Clean new stack from call to StackSwap */
 	addl $8,%esp
 
 	/* Call the specified routine */
 	call *%edi
-	/* Clean (new) stack, leaving SysBase behind */
+
+	/* Clean (new) stack partially, leaving SysBase behind */
 	addl $8,%esp
 
 	/* Store the result of the routine in esi */
 	movl %eax,%esi
 
-	/* Swap the upper two values on the stack */
+	/* Swap the upper two values on the new stack */
 	popl %edx /* SysBase */
 	popl %ebx /* sss */
 	pushl %edx /* SysBase */
 	pushl %ebx /* sss */
+
 	/* Switch stacks back */
 	leal StackSwap(%edx),%edx
 	call *%edx
+
 	/* Clean old stack */
 	addl $8,%esp
 
--- arch/i386-all/stackswap.s
+++ arch/i386-all/stackswap.s
@@ -2,25 +2,33 @@
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
 
-    Desc:
-    Lang:
+    Desc: Change the stack of a task.
+    Lang: english
 */
 
 /******************************************************************************
 
     NAME
-	AROS_LH0(void, StackSwap,
+	AROS_LH1(void, StackSwap,
+
+    SYNOPSIS
+	ARHS_LHA(struct StackSwapStruct *, sss, A0)
 
     LOCATION
 	struct ExecBase *, SysBase, 122, Exec)
 
     FUNCTION
+	Change the stack of a task.
 
     INPUTS
+	sss - The description of the new stack
 
     RESULT
+	There will be a new stack.
 
     NOTES
+	Calling this routine the first time will change sss and
+	calling it a second time, the changes will be undone.
 
     EXAMPLE
 
@@ -29,6 +37,8 @@
     SEE ALSO
 
     INTERNALS
+	This is a symmetrical routine. If you call it twice, then
+	everything will be as it was before.
 
     HISTORY
 
@@ -40,36 +50,72 @@
 	.balign 16
 	.globl	AROS_SLIB_ENTRY(StackSwap,Exec)
 	.type	AROS_SLIB_ENTRY(StackSwap,Exec),@function
+
+	/* The stack looks like this:
+
+	    8 SysBase
+	    4 sss
+	    0 return address
+	*/
+
+#define sss	    4
+#define SysBase     8
+
 AROS_SLIB_ENTRY(StackSwap,Exec):
-	movl 4(%esp),%edx
-	movl 8(%edx),%ecx
-	popl %eax
-	movl %eax,-12(%ecx)
-	popl %eax
-	movl %eax,-8(%ecx)
+	/* Read parameter sss */
+	movl sss(%esp),%edx
+
+	/* copy new SP into ecx */
+	movl stk_Pointer(%edx),%ecx
+
+	/* Pop return address and sss from the current stack and copy them
+	    onto the one specified in sss */
+	popl %eax		    /* pop Return address */
+	movl %eax,-12(%ecx)         /* Push return address on new stack */
+	popl %eax		    /* pop sss */
+	movl %eax,-8(%ecx)          /* Push sss on new stack */
+
+	/* Copy SysBase from the current stack onto the one in sss */
 	movl (%esp),%eax
-	movl %eax,-4(%ecx)
+	movl %eax,-4(%ecx)          /* Push SysBase on new stack */
+
+	/* Calc new start of stack in sss */
 	addl $-12,%ecx
+
+	/* Call Disable() (SysBase is still on the stack) */
 	leal Disable(%eax),%eax
 	call *%eax
-	popl %eax
-	movl %esp,8(%edx)
-	movl %ecx,%esp
+	popl %eax		    /* Remove SysBase from current stack */
+
+	movl %esp,stk_Pointer(%edx) /* Save current SP in sss */
+	movl %ecx,%esp		    /* Load the new stack */
+
 	movl ThisTask(%eax),%ecx
-	leal tc_SPLower(%ecx),%ecx
-	push %ebx
-	movl (%edx),%eax
+	leal tc_SPLower(%ecx),%ecx  /* ecx = &SysBase->ThisTask->tc_SPLower */
+
+	push %ebx		    /* Save register */
+
+	/* Swap ThisTask->tc_SPLower and sss->stk_Lower */
+	movl stk_Lower(%edx),%eax
 	movl (%ecx),%ebx
 	movl %eax,(%ecx)
-	movl %ebx,(%edx)
-	movl 4(%edx),%eax
+	movl %ebx,stk_Lower(%edx)
+
+	/* Swap tc_SPUpper and sss->stk_Upper, too */
+	movl stk_Upper(%edx),%eax
 	movl 4(%ecx),%ebx
 	movl %eax,4(%ecx)
-	movl %ebx,4(%edx)
-	popl %ebx
-	movl 8(%esp),%eax
-	pushl %eax
-	leal Enable(%eax),%eax
+	movl %ebx,stk_Upper(%edx)
+
+	popl %ebx		    /* Restore register */
+
+	/* Call Enable() */
+	movl SysBase(%esp),%eax
+	pushl %eax		    /* push SysBase on new stack */
+	leal Enable(%eax),%eax      /* call enable */
 	call *%eax
-	addl $4,%esp
+	addl $4,%esp		    /* Clean stack */
+
+	/* Note that at this time, the new stack from sss contains the
+	   same values as the previous stack */
 	ret
