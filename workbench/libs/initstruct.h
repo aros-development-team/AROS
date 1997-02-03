83a4568d8f147451033e43baa695ecd96e89ee2f
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-02-03 14:20:57 +0000

    Polish
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1325 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/initstruct.h
+++ workbench/libs/initstruct.h
@@ -1,21 +1,9 @@
 /*
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
-    $Log$
-    Revision 1.5  1996/10/23 14:05:42  aros
-    #define was renamed
 
-    Revision 1.4  1996/10/21 20:53:17  aros
-    Changed BIG_ENDIAN to AROS_BIG_ENDIAN
-
-    Revision 1.3  1996/10/19 17:07:29  aros
-    Include <aros/machine.h> instead of machine.h
-
-    Revision 1.2  1996/08/01 17:41:30  digulla
-    Added standard header for all files
-
-    Desc:
-    Lang:
+    Desc: Defines and macros to create and fill an inittable
+    Lang: english
 */
 #ifndef _INITSTRUCT_H_
 #define _INITSTRUCT_H_
