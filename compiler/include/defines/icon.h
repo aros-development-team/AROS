1efbd8e6044335442feecc4facfdda2f0945f9b8
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-02-03 14:16:21 +0000

    New library
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1319 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/defines/icon.h
+++ compiler/include/defines/icon.h
@@ -11,7 +11,55 @@
 /*
     Defines
 */
+#define AddFreeList(freelist, mem, size) \
+    AROS_LC3(BOOL, AddFreeList, \
+    AROS_LCA(struct FreeList *, freelist, A0), \
+    AROS_LCA(APTR             , mem, A1), \
+    AROS_LCA(unsigned long    , size, A2), \
+    struct Library *, IconBase, 12, Icon)
 
+#define BumpRevision(newname, oldname) \
+    AROS_LC2(UBYTE *, BumpRevision, \
+    AROS_LCA(UBYTE *, newname, A0), \
+    AROS_LCA(UBYTE *, oldname, A1), \
+    struct Library *, IconBase, 18, Icon)
+
+#define DeleteDiskObject(name) \
+    AROS_LC1(BOOL, DeleteDiskObject, \
+    AROS_LCA(UBYTE *, name, A0), \
+    struct Library *, IconBase, 23, Icon)
+
+#define FindToolType(toolTypeArray, typeName) \
+    AROS_LC2(UBYTE *, FindToolType, \
+    AROS_LCA(UBYTE **, toolTypeArray, A0), \
+    AROS_LCA(UBYTE  *, typeName, A1), \
+    struct Library *, IconBase, 16, Icon)
+
+#define FreeFreeList(freelist) \
+    AROS_LC1(void, FreeFreeList, \
+    AROS_LCA(struct FreeList *, freelist, A0), \
+    struct Library *, IconBase, 9, Icon)
+
+#define GetDefDiskObject(type) \
+    AROS_LC1(struct DiskObject *, GetDefDiskObject, \
+    AROS_LCA(long, type, D0), \
+    struct Library *, IconBase, 20, Icon)
+
+#define GetDiskObjectNew(name) \
+    AROS_LC1(struct DiskObject *, GetDiskObjectNew, \
+    AROS_LCA(UBYTE *, name, A0), \
+    struct Library *, IconBase, 22, Icon)
+
+#define MatchToolValue(typeString, value) \
+    AROS_LC2(BOOL, MatchToolValue, \
+    AROS_LCA(UBYTE *, typeString, A0), \
+    AROS_LCA(UBYTE *, value, A1), \
+    struct Library *, IconBase, 17, Icon)
+
+#define PutDefDiskObject(diskObject) \
+    AROS_LC1(BOOL, PutDefDiskObject, \
+    AROS_LCA(struct DiskObject *, diskObject, A0), \
+    struct Library *, IconBase, 21, Icon)
 
 
 #endif /* DEFINES_ICON_H */
