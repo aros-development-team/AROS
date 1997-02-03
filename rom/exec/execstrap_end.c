d8ce59d86fde845ba01316869eff275b0091c759
ldp <ldp@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-02-03 02:57:19 +0000

    Moved within execstrap_init.c
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1314 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/execstrap_end.c
+++
@@ -1,9 +0,0 @@
-/*
-    (C) 1995-97 AROS - The Amiga Replacement OS
-    $Id$
-
-    Desc:
-    Lang: english
-*/
-
-const char end = 0;
--- rom/exec/execstrap_init.c
+++ rom/exec/execstrap_init.c
@@ -34,6 +34,36 @@ extern void AROS_SLIB_ENTRY(CachePostDMA_40,Exec)();
     Expand, improve and generally make the world a better place. :)
 */
 
+int entry(void)
+{
+    return 0;
+}
+
+extern const char name[];
+extern const char version[];
+extern int start(void);
+extern const char end;
+
+struct Resident resident =
+{
+    RTC_MATCHWORD,
+    &resident,
+    (APTR)&end,
+    RTF_COLDSTART,
+    1,			/* version */
+    NT_KICKMEM,
+    106,		/* Just above exec.library.
+			   Because exec is RTF_SINGLETASK, and this is
+			   RTF_COLDSTART, we'll still be started after
+			   exec */
+    (char *)name,
+    (char *)&version[6],
+    &start
+};
+
+const char name[] = "exec.strap";
+const char version[] = "$VER: AROS exec.strap 1.2 (2.2.97)";
+
 int start(void)
 {
     struct ExecBase *SysBase;
@@ -269,3 +299,4 @@ int start(void)
     return 0;
 }
 
+const char end = 0;
