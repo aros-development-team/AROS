2a3710e36c223df87acf56ec4e2cf0f2cd2ff556
ldp <ldp@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-02-08 00:16:50 +0000

    Tool to print the SysBase->ResModules list
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1400 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/.unmaintained/amiga/boot/printresmodules.c
@@ -0,0 +1,60 @@
+#include <exec/execbase.h>
+#include <exec/resident.h>
+
+#include <proto/dos.h>
+
+#include <stdio.h>
+
+extern struct ExecBase *SysBase;
+
+void ResType(UBYTE);
+
+int main(void)
+{
+    int i;
+    ULONG *ptr;
+
+    for(i = 1, ptr = SysBase->ResModules; *ptr; i++, ptr++)
+    {
+	struct Resident *res = (struct Resident *)*ptr;
+
+	Printf("%2lu: 0x%08lx %4ld ", i, *ptr, res->rt_Pri);
+	ResType(res->rt_Flags);
+	Printf("%s\n", (ULONG)res->rt_Name);
+    }
+
+    return 0;
+}
+
+void ResType(UBYTE type)
+{
+    int i = 0;
+
+    if (type == RTF_AUTOINIT)
+    {
+	PutStr("               RTF_AUTOINIT   "); i++;
+    }
+    else
+    if (type == RTF_SINGLETASK)
+    {
+	PutStr("               RTF_SINGLETASK "); i++;
+    }
+    else
+    {
+	if (type & RTF_COLDSTART ) { PutStr("RTF_COLDSTART  "); i++; }
+	if (type & RTF_AUTOINIT  ) { PutStr("RTF_AUTOINIT   "); i++; }
+	if (type & RTF_AFTERDOS  ) { PutStr("RTF_AFTERDOS   "); i++; }
+	if (type & RTF_SINGLETASK) { PutStr("RTF_SINGLETASK "); i++; }
+	switch(i)
+	{
+	    case 0:
+		PutStr("                              ");
+		break;
+	    case 1:
+		PutStr("               ");
+		break;
+	    default:
+		break;
+	} /* switch */
+    }
+}
