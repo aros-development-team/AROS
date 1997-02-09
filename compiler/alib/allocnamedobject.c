aedf1773dca728536a20a212e3ad108e2facccce
iaint <iaint@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-02-09 14:18:04 +0000

    AllocNamedObject() varargs function
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1405 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/alib/allocnamedobject.c
@@ -0,0 +1,62 @@
+/*
+    (C) 1995-97 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: Allocate a NamedObject with varargs.
+    Lang: english
+*/
+#define AROS_TAGRETURNTYPE  struct NamedObject *
+
+#include <proto/utility.h>
+#undef CallHookPkt
+#include "alib_intern.h"
+extern struct UtilityBase * UtilityBase;
+
+/*****************************************************************************
+
+    NAME */
+#include <utility/tagitem.h>
+#include <utility/name.h>
+#include <proto/utility.h>
+
+	struct NamedObject * AllocNamedObject (
+
+/*  SYNOPSIS */
+	STRPTR             name,
+	ULONG              tag1,
+	...                )
+
+/*  FUNCTION
+	Allocate a NamedObject. This is the varargs version of the function.
+	For more information see AllocNamedObjectA() in utility.library.
+
+    INPUTS
+	name        -   The name of the object to allocate.
+	tag1        -   The first Tag of the arguments. End the list with
+			TAG_DONE.
+
+    RESULT
+	The address of a NamedObject, or NULL if the allocation failed.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	utility/AllocNamedObjectA()
+
+    INTERNALS
+
+    HISTORY
+	09-02-1997  iaint       Adapted from another function.
+
+*****************************************************************************/
+{
+    AROS_SLOWSTACKTAGS_PRE(tag1)
+
+    retval = AllocNamedObjectA(name, AROS_SLOWSTACKTAGS_ARG(tag1));
+
+    AROS_SLOWSTACKTAGS_POST
+} /* AllocNamedObject */
