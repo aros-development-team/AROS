75a2e140ad9b15c52d4b33486bcf5c4f21610867
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-02-10 17:39:49 +0000

    BEIO: Call hooks instead of direct DOS-Calls
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1424 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/clib/aros_protos.h
+++ compiler/include/clib/aros_protos.h
@@ -21,6 +21,9 @@
 #ifndef  DOS_DOS_H
 #   include <dos/dos.h>
 #endif
+#ifndef  UTILITY_HOOKS_H
+#   include <utility/hooks.h>
+#endif
 
 #ifdef DEBUG_FreeMem
 #   ifndef PROTO_EXEC_H
@@ -42,22 +45,23 @@ int	kprintf      (const UBYTE * fmt, ...);
 void	NastyFreeMem (APTR mem, ULONG size);
 APTR	RemoveSList  (APTR * list, APTR node);
 void	hexdump      (const void * data, IPTR offset, ULONG count);
+int	strrncasecmp (const char *, const char *, int);
 
 /* AROS enhancements */
-BOOL ReadByte	 (BPTR fh, UBYTE  * dataptr);
-BOOL ReadWord	 (BPTR fh, UWORD  * dataptr);
-BOOL ReadLong	 (BPTR fh, ULONG  * dataptr);
-BOOL ReadFloat	 (BPTR fh, FLOAT  * dataptr);
-BOOL ReadDouble  (BPTR fh, DOUBLE * dataptr);
-BOOL ReadString  (BPTR fh, STRPTR * dataptr);
-BOOL ReadStruct  (BPTR fh, APTR   * dataptr, const IPTR * desc);
-BOOL WriteByte	 (BPTR fh, UBYTE  data);
-BOOL WriteWord	 (BPTR fh, UWORD  data);
-BOOL WriteLong	 (BPTR fh, ULONG  data);
-BOOL WriteFloat  (BPTR fh, FLOAT  data);
-BOOL WriteDouble (BPTR fh, DOUBLE data);
-BOOL WriteString (BPTR fh, STRPTR data);
-BOOL WriteStruct (BPTR fh, APTR   data, const IPTR * desc);
+BOOL ReadByte	 (struct Hook *, UBYTE  * dataptr, void * stream);
+BOOL ReadWord	 (struct Hook *, UWORD  * dataptr, void * stream);
+BOOL ReadLong	 (struct Hook *, ULONG  * dataptr, void * stream);
+BOOL ReadFloat	 (struct Hook *, FLOAT  * dataptr, void * stream);
+BOOL ReadDouble  (struct Hook *, DOUBLE * dataptr, void * stream);
+BOOL ReadString  (struct Hook *, STRPTR * dataptr, void * stream);
+BOOL ReadStruct  (struct Hook *, APTR   * dataptr, void * stream, const IPTR * desc);
+BOOL WriteByte	 (struct Hook *, UBYTE  data, void * stream);
+BOOL WriteWord	 (struct Hook *, UWORD  data, void * stream);
+BOOL WriteLong	 (struct Hook *, ULONG  data, void * stream);
+BOOL WriteFloat  (struct Hook *, FLOAT  data, void * stream);
+BOOL WriteDouble (struct Hook *, DOUBLE data, void * stream);
+BOOL WriteString (struct Hook *, STRPTR data, void * stream);
+BOOL WriteStruct (struct Hook *, APTR   data, void * stream, const IPTR * desc);
 void FreeStruct  (APTR s,  const IPTR * desc);
 
 
