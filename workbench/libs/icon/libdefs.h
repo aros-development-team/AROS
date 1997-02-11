9625f979456240d974f865803a5b180c045aa4fb
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-02-11 16:32:25 +0000

    Maybe this file shouldn't be under CVS control but it gets distributed :-/
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1458 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/icon/libdefs.h
@@ -0,0 +1,11 @@
+#define LIBNAME      "icon.library"
+#define LIBBASE      IconBase
+#define LIBBASETYPE  IconBase
+#define LIBVERSION   41
+#define LIBREVISION  1
+#define _LIBnAME     Icon
+#define _LIBNAME     icon
+#define VERSION      "$VER: icon 41.1 (11.02.1997)\n\r"
+#define END          Icon_end
+#define FUNCTABLE    Icon_functable
+#define INIT         AROS_SLIB_ENTRY(init,Icon)
