04e3d2a274928675663072f4720dbf27bb1c18eb
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-02-13 18:59:51 +0000

    New function
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1469 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/alib/easyrequest.c
@@ -0,0 +1,58 @@
+/*
+    (C) 1995-96 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc:
+    Lang: english
+*/
+#include <stdarg.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/intuition.h>
+
+	LONG EasyRequest (
+
+/*  SYNOPSIS */
+	struct Window	  * window,
+	struct EasyStruct * easyStruct,
+	ULONG		  * idcmpPtr,
+	...)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+	27-11-96    digulla automatically created from
+			    intuition_lib.fd and clib/intuition_protos.h
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+    AROS_LIBBASE_EXT_DECL(struct IntuitionBase *,IntuitionBase)
+    va_list args;
+    LONG    rc;
+
+    va_start (args, idcmpPtr);
+
+    rc = EasyRequestArgs (window, easyStruct, idcmpPtr, (APTR)args);
+
+    va_end (args);
+
+    return rc;
+    AROS_LIBFUNC_EXIT
+} /* EasyRequest */
