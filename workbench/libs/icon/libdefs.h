701c88ecbd34a95f6f3ccd60b025c4c9bec83ad0
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-02-14 15:20:01 +0000

    New version (no idea what might have changed, but to show some progress :-)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1495 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/icon/libdefs.h
+++ workbench/libs/icon/libdefs.h
@@ -2,10 +2,9 @@
 #define LIBBASE      IconBase
 #define LIBBASETYPE  IconBase
 #define LIBVERSION   41
-#define LIBREVISION  1
-#define _LIBnAME     Icon
-#define _LIBNAME     icon
-#define VERSION      "$VER: icon 41.1 (11.02.1997)\n\r"
+#define LIBREVISION  2
+#define BASENAME     Icon
+#define VERSION      "$VER: icon 41.2 (14.02.1997)\n\r"
 #define END          Icon_end
 #define FUNCTABLE    Icon_functable
 #define INIT         AROS_SLIB_ENTRY(init,Icon)
