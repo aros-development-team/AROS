99a7449174f15e036a057d0835d0b79e290f4623
srittau <srittau@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-02-18 19:56:30 +0000

    Initial version
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1532 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/utility/date.h
@@ -0,0 +1,28 @@
+#ifndef UTILITY_DATE_H
+#define UTILITY_DATE_H
+
+/*
+    (C) 1997 AROS - The Amiga Replacement OS
+
+    $Id$
+
+    Desc: ClockData
+    Lang: English
+*/
+
+#ifndef EXEC_TYPES_H
+#   include <exec/types.h>
+#endif
+
+struct ClockData
+{
+    UWORD sec;
+    UWORD min;
+    UWORD hour;
+    UWORD mday;
+    UWORD month;
+    UWORD year;
+    UWORD wday;
+};
+
+#endif /* UTILITY_DATE_H */
