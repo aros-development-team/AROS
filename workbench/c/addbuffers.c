874bb8d4950dd736851d91d66486a2994578efec
srittau <srittau@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-02-18 22:55:10 +0000

    Initial revision
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1533 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/c/addbuffers.c
@@ -0,0 +1,48 @@
+/*
+    (C) 1997 AROS - The Amiga Replacement OS
+
+    Desc: Addbuffers CLI command
+    Lang: english
+*/
+
+#include <stdio.h>
+#include <proto/dos.h>
+#include <dos/dos.h>
+
+#define ARG_STRING "DRIVE/A,BUFFERS/N"
+#define ARG_DRIVE 0
+#define ARG_BUFFERS 1
+#define ARG_COUNT 2
+
+static const char version[] = "$VER: AddBuffers 1.0 (18.2.1997)\n";
+
+int main (int argc, char ** argv)
+{
+    IPTR args[ARG_COUNT] = { 0, 0 };
+    struct RDArgs *rda;
+    int result;
+    int error = RETURN_OK;
+    ULONG *bufsptr;
+    ULONG buffers = 0;
+
+    rda = ReadArgs(ARG_STRING, args, NULL);
+    if (rda != NULL)
+    {
+        bufsptr = (ULONG *)args[ARG_BUFFERS];
+        if (bufsptr != NULL) buffers = *bufsptr;
+        result = AddBuffers((char *)args[ARG_DRIVE], buffers);
+        if (result != 0)
+            printf("%s has %ld buffers\n", (char *)args[ARG_DRIVE], IoErr());
+        else
+	{
+            PrintFault(IoErr(), "AddBuffers");
+            error = RETURN_FAIL;
+        }
+        FreeArgs(rda);
+    } else
+    {
+        PrintFault(IoErr(), "AddBuffers");
+	error = RETURN_FAIL;
+    }
+    return(error);
+}
