9bb9e7066b276cdf5d8a4c52a70b9c55a4dacaa5
srittau <srittau@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-02-27 13:45:38 +0000

    Some useful math macros
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1575 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/clib/macros.h
@@ -0,0 +1,16 @@
+#ifndef CLIB_MACROS_H
+#define CLIB_MACROS_H
+
+/*
+    (C) 1997 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: C macros
+    Lang: english
+*/
+
+#define MIN(x,y) ((x)<(y)?(x):(y))
+#define MAX(x,y) ((x)>(y)?(x):(y))
+#define ABS(x) (((x)<0)?(-(x)):(X))
+
+#endif /* CLIB_MACROS_H */
