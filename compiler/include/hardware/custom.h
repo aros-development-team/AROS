cb5f55e28c964f3b6416bd23352baee30b27954e
srittau <srittau@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-02-27 16:46:55 +0000

    Initial revision. This include is empty at the moment
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1585 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/hardware/custom.h
@@ -0,0 +1,13 @@
+#ifndef HARDWARE_CUSTOM_H
+#define HARDWARE_CUSTOM_H
+
+/*
+    (C) 1997 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: Custom Amiga chips
+    Lang: english
+*/
+
+
+#endif /* HARDWARE_CUSTOM_H */
