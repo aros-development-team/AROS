ba708036f3e63c0b9da713c27c8433d717b3b4f1
ldp <ldp@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-03-06 04:07:44 +0000

    Removed log.
    Typos.
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1647 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/setfunction.c
+++ rom/exec/setfunction.c
@@ -1,41 +1,6 @@
 /*
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
-    $Log$
-    Revision 1.11  1997/02/28 00:11:54  ldp
-    AROSfA: Refine way to sign-extend from 16 to 32 bits
-
-    Revision 1.10  1997/02/26 01:33:40  ldp
-    AROSfA: added hack to keep dos.library patches working. See source for
-    extensive comments.
-
-    Revision 1.9  1997/01/01 03:46:16  ldp
-    Committed Amiga native (support) code
-
-    Changed clib to proto
-
-    Revision 1.8  1996/12/10 13:51:54  aros
-    Moved all #include's in the first column so makedepend can see it.
-
-    Revision 1.7  1996/10/24 15:36:47  aros
-    Named all macros which the user/developer can use as "AROS".
-
-    For some strange reason, GCC produces incorrect code for "x /= -5;". "x = (-x)
-    / 5" works...
-
-    Revision 1.6  1996/10/23 14:28:54  aros
-    Use the respective macros to access and manipulate a libraries' jumptable
-
-    Revision 1.5  1996/10/19 17:07:27  aros
-    Include <aros/machine.h> instead of machine.h
-
-    Revision 1.4  1996/08/13 13:56:08  digulla
-    Replaced AROS_LA by AROS_LHA
-    Replaced some AROS_LH*I by AROS_LH*
-    Sorted and added includes
-
-    Revision 1.3  1996/08/01 17:41:19  digulla
-    Added standard header for all files
 
     Desc:
     Lang: english
@@ -112,6 +77,7 @@
     AROS_LIBFUNC_INIT
     APTR ret;
 
+#if (AROS_FLAVOUR == AROS_FLAVOUR_NATIVE)
     /*
 	Fix dos.library/ramlib attempts to SetFunction() CloseDevice/
 	CloseLibrary/RemDevice/RemLibrary/OpenDevice/OpenLibrary.
@@ -119,7 +85,7 @@
 	This also effectively limits the max offset to 32k, but this limit was
 	already in the original, though not really documented.
 
-	What happes is this: the prototype for the funcOffset says it is a
+	What happens is this: the prototype for the funcOffset says it is a
 	long, but the autodoc also says that only a0.w (lower 16 bits) is used.
 	Dos.library/ramlib only sets the lower 16 bits of a0 to the required
 	offset, without sign-extending to the upper 16 bits, in fact without
@@ -135,9 +101,8 @@
 	In my [ldp] opinion, the autodoc should never have said that only A0.W
 	is used for the funcOffset, while specifying a "long" in the prototype.
 	This will stay broken and this fix will stay here until we fix
-	dos.library.
+	dos.library/ramlib.
     */
-#if (AROS_FLAVOUR == AROS_FLAVOUR_NATIVE)
     if (funcOffset & 0x00008000)
     {
 	funcOffset |= 0xffff0000;
