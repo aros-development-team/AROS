9a4aa04c79d7efa46d89bb4a0d7862c0810060ee
srittau <srittau@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-03-13 22:47:16 +0000

    Initial revision
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1731 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/graphics/regions.h
@@ -0,0 +1,32 @@
+#ifndef GRAPHICS_REGIONS_H
+#define GRAPHICS_REGIONS_H
+
+/*
+    (C) 1997 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: Graphics region definitions.
+    Lang: english
+*/
+
+#ifndef EXEC_TYPES_H
+#   include <exec/types.h>
+#endif
+#ifndef GRAPHICS_GFX_H
+#   include <graphics/gfx.h>
+#endif
+
+struct Region
+{
+    struct Rectangle         bounds;
+    struct RegionRectangle * RegionRectangle;
+};
+
+struct RegionRectangle
+{
+    struct RegionRectangle * Next;
+    struct RegionRectangle * Prev;
+    struct Rectangle         bounds;
+};
+
+#endif /* GRAPHICS_REGIONS_H */
