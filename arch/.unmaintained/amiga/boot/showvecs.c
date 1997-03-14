bfc184c180fd4b074ee361558237a6ce33cafaea
ldp <ldp@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-03-14 04:12:53 +0000

    Don't print a comprehensive report (only show vector contents) if the checksum is incorrect.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1735 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/.unmaintained/amiga/boot/showvecs.c
+++ arch/.unmaintained/amiga/boot/showvecs.c
@@ -1,59 +1,76 @@
+/*
+    (C) 1995-97 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: prints Kick Vector pointers
+    Lang: english
+*/
 #include <exec/types.h>
 #include <exec/execbase.h>
 #include <exec/resident.h>
 #include <exec/memory.h>
 #include <proto/dos.h>
+#include <proto/exec.h>
 #include <stdio.h>
 
 extern struct ExecBase *SysBase;
 
-const UBYTE ver[] = "$VER: showvecs 41.1 (17.2.97)\n\r";
+const UBYTE ver[] = "$VER: showvecs 41.2 (14.3.97)\n\r";
 
 int main(void)
 {
     Printf("KickTagPtr = 0x%08lx\nKickMemPtr = 0x%08lx\nKickChkSum = 0x%08lx\n",
 		(ULONG)SysBase->KickTagPtr, (ULONG)SysBase->KickMemPtr, (ULONG)SysBase->KickCheckSum);
 
-    if(SysBase->KickTagPtr)
+    if(SumKickData() == (ULONG)SysBase->KickCheckSum)
     {
-	ULONG *list;
-
-	Printf("Modules in use:\n");
-
-	list = SysBase->KickTagPtr;
-
-	while(*list)
+	if(SysBase->KickTagPtr)
 	{
-	    if(*list & 0x80000000) list = (ULONG *)(*list & 0x7fffffff);
-	    Printf("\t0x%08lx\t%s\n", *list, (ULONG)((struct Resident *)*list)->rt_IdString);
-	    list++;
+	    ULONG *list;
+
+	    Printf("Modules in use:\n");
+
+	    list = SysBase->KickTagPtr;
+
+	    while(*list)
+	    {
+		if(*list & 0x80000000) list = (ULONG *)(*list & 0x7fffffff);
+		Printf("\t0x%08lx\t%s\n", *list, (ULONG)((struct Resident *)*list)->rt_IdString);
+		list++;
+	    }
+        }
+
+        if(SysBase->KickMemPtr)
+        {
+	    struct MemList *memlist;
+
+	    Printf("MemLists in use:\n");
+
+	    memlist = (struct MemList *)SysBase->KickMemPtr;
+
+	    while(memlist)
+	    {
+		struct MemEntry *me = &memlist->ml_ME[0];
+		UWORD i;
+
+		Printf("ml_NumEntries = %ld\n", memlist->ml_NumEntries);
+
+		for(i = 0; i < memlist->ml_NumEntries; i++)
+		{
+		    Printf("ml_ME[%2ld] = 0x%08lx size 0x%08lx (%8ld)\n",
+		     i, (ULONG)me->me_Addr, me->me_Length, me->me_Length);
+		    me = (struct MemEntry *)((ULONG)me + (ULONG)sizeof(struct MemEntry));
+		}
+
+		memlist = (struct MemList *)memlist->ml_Node.ln_Succ;
+	    }
 	}
     }
-
-    if(SysBase->KickMemPtr)
+    else
     {
-	struct MemList *memlist;
-
-	Printf("MemLists in use:\n");
-
-	memlist = (struct MemList *)SysBase->KickMemPtr;
-
-	while(memlist)
-	{
-	    struct MemEntry *me = &memlist->ml_ME[0];
-	    UWORD i;
-
-	    Printf("ml_NumEntries = %ld\n", memlist->ml_NumEntries);
-
-	    for(i = 0; i < memlist->ml_NumEntries; i++)
-	    {
-		Printf("ml_ME[%2ld] = 0x%08lx size 0x%08lx (%8ld)\n",
-		    i, (ULONG)me->me_Addr, me->me_Length, me->me_Length);
-		me = (struct MemEntry *)((ULONG)me + (ULONG)sizeof(struct MemEntry));
-	    }
-
-	    memlist = (struct MemList *)memlist->ml_Node.ln_Succ;
-	}
+	/* Don't print message if all vectors are NULL */
+	if(SysBase->KickMemPtr || SysBase->KickTagPtr || SysBase->KickCheckSum)
+	    Printf("Vectors have incorrect checksum.\n");
     }
 
     return(0);
