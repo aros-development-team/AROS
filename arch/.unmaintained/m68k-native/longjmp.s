70ad59ff3f25043dc027b506f3c3baeac5085ddf
ldp <ldp@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-03-14 04:33:37 +0000

    Added
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1747 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/.unmaintained/m68k-native/longjmp.s
@@ -0,0 +1,101 @@
+/*
+    (C) 1995-96 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: longjmp
+    Lang: english
+*/
+
+/******************************************************************************
+
+    NAME
+#include <setjmp.h>
+
+	void longjmp (jmp_buf env, int val);
+
+    FUNCTION
+	Save the current context so that you can return to it later.
+
+    INPUTS
+	env - The context/environment to restore
+	val - This value is returned by setjmp() when you return to the
+		saved context. You cannot return 0. If val is 0, then
+		setjmp() returns with 1.
+
+    RESULT
+	This function doesn't return.
+
+    NOTES
+
+    EXAMPLE
+	jmp_buf env;
+
+	... some code ...
+
+	if (!setjmp (env))
+	{
+	    ... this code is executed after setjmp() returns ...
+
+	    // This is no good example on how to use this function
+	    // You should not do that
+	    if (error)
+		longjmp (env, 5);
+
+	    ... some code ...
+	}
+	else
+	{
+	    ... this code is executed if you call longjmp(env) ...
+	}
+
+    BUGS
+
+    SEE ALSO
+	setjmp()
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+
+	#include "machine.i"
+
+	.text
+	.balign 16
+	.globl	AROS_CDEFNAME(longjmp)
+	.type	AROS_CDEFNAME(longjmp),@function
+
+	.set	FirstArg, 4 /* Skip Return-Adress */
+	.set	env, FirstArg
+	.set	val, env+4
+	.set	retaddr, 0
+AROS_CDEFNAME(longjmp):
+	move.l	env(sp),a0		/* save area pointer */
+	move.l	val(sp),d0
+	jne	okret
+	moveq.l	#1,d0			/* make sure it isn't 0 */
+okret:
+	move.l	4(a0),sp		/* restore SP */
+	move.l	0(a0),(sp)		/* restore PC */
+	movem.l	8(a0),d2-d7/a2-a6	/* restore remaining non-scratch regs */
+	rts
+
+/*
+	The jmp_buf is filled as follows (d0/d1/a0/a1 are not saved):
+
+	_jmp_buf	offset	contents
+	[0]   		0	old pc
+	[1]		4	old sp
+	[2]		8	d2
+	[3]		12	d3
+	[4]		16	d4
+	[5]		20	d5
+	[6]		24	d6
+	[7]		28	d7
+	[8]		32	a2
+	[9]		36	a3
+	[10]		40	a4
+	[11]		44	a5
+	[12]		48	a6
+*/
