5c24f3542ec5e01cb495061e07872c47ad6bbb93
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-03-20 16:05:44 +0000

    Internal BOOPSI prototypes
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1825 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/intuition/boopsi.h
@@ -0,0 +1,17 @@
+#ifndef _BOOPSI_H
+#define _BOOPSI_H
+/*
+    (C) 1995-96 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: BOOPSI internals
+    Lang: english
+*/
+#ifndef INTUITION_CLASSES_H
+#   include <intuition/classes.h>
+#endif
+
+/* Internal prototypes */
+extern Class * FindClass (ClassID, struct IntuitionBase *);
+
+#endif /* _BOOPSI_H */
