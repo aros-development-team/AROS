a2d2cdc1d07ba1578afd1f4f6004b59077133f35
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-03-26 19:21:35 +0000

    Polish.
    Added DOS_RDARGS object
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1890 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/allocdosobject.c
+++ rom/dos/allocdosobject.c
@@ -1,24 +1,6 @@
 /*
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
-    $Log$
-    Revision 1.6  1997/01/27 00:36:14  ldp
-    Polish
-
-    Revision 1.5  1996/12/09 13:53:20  aros
-    Added empty templates for all missing functions
-
-    Moved #include's into first column
-
-    Revision 1.4  1996/10/24 15:50:23  aros
-    Use the official AROS macros over the __AROS versions.
-
-    Revision 1.3  1996/08/13 13:52:44  digulla
-    Replaced <dos/dosextens.h> by "dos_intern.h" or added "dos_intern.h"
-    Replaced AROS_LA by AROS_LHA
-
-    Revision 1.2  1996/08/01 17:40:47  digulla
-    Added standard header for all files
 
     Desc:
     Lang: english
@@ -27,6 +9,7 @@
 #include <proto/exec.h>
 #include <dos/exall.h>
 #include <utility/tagitem.h>
+#include <dos/rdargs.h>
 #include "dos_intern.h"
 
 /*****************************************************************************
@@ -74,14 +57,16 @@
 
     switch(type)
     {
-	case DOS_FILEHANDLE:
-	    return AllocMem(sizeof(struct FileHandle),MEMF_CLEAR);
-	case DOS_FIB:
-	    return AllocMem(sizeof(struct FileInfoBlock),MEMF_CLEAR);
-	case DOS_EXALLCONTROL:
-	    return AllocMem(sizeof(struct ExAllControl),MEMF_CLEAR);
-	case DOS_CLI:
-	    return AllocMem(sizeof(struct CommandLineInterface),MEMF_CLEAR);
+    case DOS_FILEHANDLE:
+	return AllocMem(sizeof(struct FileHandle),MEMF_CLEAR);
+    case DOS_FIB:
+	return AllocMem(sizeof(struct FileInfoBlock),MEMF_CLEAR);
+    case DOS_EXALLCONTROL:
+	return AllocMem(sizeof(struct ExAllControl),MEMF_CLEAR);
+    case DOS_CLI:
+	return AllocMem(sizeof(struct CommandLineInterface),MEMF_CLEAR);
+    case DOS_RDARGS:
+	return AllocVec(sizeof(struct RDArgs),MEMF_CLEAR);
     }
     return NULL;
     AROS_LIBFUNC_EXIT
--- rom/dos/freedosobject.c
+++ rom/dos/freedosobject.c
@@ -1,24 +1,6 @@
 /*
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
-    $Log$
-    Revision 1.6  1997/01/27 00:36:21  ldp
-    Polish
-
-    Revision 1.5  1996/12/09 13:53:29  aros
-    Added empty templates for all missing functions
-
-    Moved #include's into first column
-
-    Revision 1.4  1996/10/24 15:50:30  aros
-    Use the official AROS macros over the __AROS versions.
-
-    Revision 1.3  1996/08/13 13:52:47  digulla
-    Replaced <dos/dosextens.h> by "dos_intern.h" or added "dos_intern.h"
-    Replaced AROS_LA by AROS_LHA
-
-    Revision 1.2  1996/08/01 17:40:52  digulla
-    Added standard header for all files
 
     Desc:
     Lang: english
@@ -26,6 +8,7 @@
 #include <exec/memory.h>
 #include <proto/exec.h>
 #include <dos/exall.h>
+#include <dos/rdargs.h>
 #include "dos_intern.h"
 
 /*****************************************************************************
@@ -99,6 +82,9 @@
 		FreeVec(cur);
 		cur=next;
 	    }
+	    break;
+	case DOS_RDARGS:
+	    FreeArgs (ptr);
 	    break;
 	}
     }
