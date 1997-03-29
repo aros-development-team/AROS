6f692c67ad75d60849b6e508dc9f1121d19f6c24
ldp <ldp@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-03-29 21:06:16 +0000

    Some defines for name and minimum version
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1946 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/aros/arosbase.h
@@ -0,0 +1,23 @@
+#ifndef AROS_AROS_H
+#define AROS_AROS_H
+/*
+    (C) 1995-97 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: aros.library general defines
+    Lang: english
+*/
+
+#ifndef EXEC_LIBRARIES_H
+#include <exec/libraries.h>
+#endif
+
+#define AROSLIBNAME "aros.library"
+
+/* Minimum version that supports everything from the current includes. */
+/* Will be bumped whenever new functions are added to the library.     */
+#define AROSLIBVERSION 41
+#define AROSLIBREVISION 1
+
+
+#endif /* AROS_AROSSUPPORTBASE_H */
