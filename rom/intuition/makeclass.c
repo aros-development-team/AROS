9bc6215ffa4482f3ebd87867aa9d0030470f1e35
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-04-01 10:26:34 +0000

    Removed log
    git-svn-id: https://svn.aros.org/svn/aros/trunk@1959 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/intuition/makeclass.c
+++ rom/intuition/makeclass.c
@@ -1,18 +1,6 @@
 /*
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
-    $Log$
-    Revision 1.9  1997/03/27 15:43:33  digulla
-    Updated comments
-
-    Renamed SigSem into IBaseLock
-
-    Added semaphore for ClassList. This semaphore is always requested when the
-    ClassList is accessed.
-
-    Revision 1.8  1997/03/20 16:05:08  digulla
-    Fixed bug: Added FindClass()
-
 
     Desc: Initialize a BOOPSI class
     Lang: english
