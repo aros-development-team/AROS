f38e14718328d6698f0a52cec3c619d42a7fb43f
ldp <ldp@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-04-04 14:46:13 +0000

    Renamed ArosInquire to ArosInquireA
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2060 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/alib/arosinquire.c
+++ compiler/alib/arosinquire.c
@@ -18,7 +18,7 @@ extern struct ArosBase * ArosBase;
 #define NO_INLINE_STDARG /* turn off inline def */
 #include <proto/aros.h>
 
-	ULONG ArosInquireTags(
+	ULONG ArosInquire(
 
 /*  SYNOPSIS */
 	ULONG tag1,
@@ -47,7 +47,7 @@ extern struct ArosBase * ArosBase;
 {
     AROS_SLOWSTACKTAGS_PRE(tag1)
 
-    retval = ArosInquireTags(AROS_SLOWSTACKTAGS_ARG(tag1));
+    retval = ArosInquireA(AROS_SLOWSTACKTAGS_ARG(tag1));
 
     AROS_SLOWSTACKTAGS_POST
 } /* ArowInquire */
