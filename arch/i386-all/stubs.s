e73013cee60aae86c86b6cf0d45507c6f6bf462a
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-04-07 13:47:24 +0000

    Merged semaphoreglue.s into stubs.s
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2101 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-all/stubs.s
+++ arch/i386-all/stubs.s
@@ -195,4 +195,55 @@ AROS_CDEFNAME(os_enable):
 	popl %eax
 	ret
 
+	.globl	AROS_SLIB_ENTRY(_ObtainSemaphore,Exec)
+	.type	AROS_SLIB_ENTRY(_ObtainSemaphore,Exec),@function
+AROS_SLIB_ENTRY(_ObtainSemaphore,Exec):
+	pushl	%eax
+	pushl	%ecx
+	pushl	%edx
+	movl	20(%esp),%eax
+	pushl	%eax
+	movl	20(%esp),%eax
+	pushl	%eax
+	call	AROS_SLIB_ENTRY(ObtainSemaphore,Exec)
+	addl	$8,%esp
+	popl	%edx
+	popl	%ecx
+	popl	%eax
+	ret
+
+	.globl	AROS_SLIB_ENTRY(_ReleaseSemaphore,Exec)
+	.type	AROS_SLIB_ENTRY(_ReleaseSemaphore,Exec),@function
+AROS_SLIB_ENTRY(_ReleaseSemaphore,Exec):
+	pushl	%eax
+	pushl	%ecx
+	pushl	%edx
+	movl	20(%esp),%eax
+	pushl	%eax
+	movl	20(%esp),%eax
+	pushl	%eax
+	call	AROS_SLIB_ENTRY(ReleaseSemaphore,Exec)
+	addl	$8,%esp
+	popl	%edx
+	popl	%ecx
+	popl	%eax
+	ret
+
+	.globl	AROS_SLIB_ENTRY(_ObtainSemaphoreShared,Exec)
+	.type	AROS_SLIB_ENTRY(_ObtainSemaphoreShared,Exec),@function
+AROS_SLIB_ENTRY(_ObtainSemaphoreShared,Exec):
+	pushl	%eax
+	pushl	%ecx
+	pushl	%edx
+	movl	20(%esp),%eax
+	pushl	%eax
+	movl	20(%esp),%eax
+	pushl	%eax
+	call	AROS_SLIB_ENTRY(ObtainSemaphoreShared,Exec)
+	addl	$8,%esp
+	popl	%edx
+	popl	%ecx
+	popl	%eax
+	ret
+
 
