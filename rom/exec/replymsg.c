0fc73f2f2f4c6bdc531f5c868ce7bbebe0ab8856
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-04-15 07:57:40 +0000

    Polish
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2148 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/replymsg.c
+++ rom/exec/replymsg.c
@@ -1,32 +1,8 @@
 /*
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
-    $Log$
-    Revision 1.8  1997/02/13 23:49:14  ldp
-    Added extra check: take no signalling action if mp_SigTask==0. This fixes
-    streams of Enforcer hits coming from input.device (does input.device make
-    assumptions about ReplyMsg()/PutMsg() internal functioning?
 
-    Revision 1.7  1997/01/01 03:46:15  ldp
-    Committed Amiga native (support) code
-
-    Changed clib to proto
-
-    Revision 1.6  1996/12/10 13:51:53  aros
-    Moved all #include's in the first column so makedepend can see it.
-
-    Revision 1.5  1996/10/24 15:50:57  aros
-    Use the official AROS macros over the __AROS versions.
-
-    Revision 1.4  1996/08/13 13:56:08  digulla
-    Replaced AROS_LA by AROS_LHA
-    Replaced some AROS_LH*I by AROS_LH*
-    Sorted and added includes
-
-    Revision 1.3  1996/08/01 17:41:18  digulla
-    Added standard header for all files
-
-    Desc:
+    Desc: Reply a message
     Lang: english
 */
 #include "exec_intern.h"
