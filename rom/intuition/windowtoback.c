42ef67b039867bba75fb3ff6b6fb1713e0e4371e
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-04-17 12:00:11 +0000

    Added ScreenToBack() for RT
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2167 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/intuition/windowtoback.c
+++ rom/intuition/windowtoback.c
@@ -1,26 +1,8 @@
 /*
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
-    $Log$
-    Revision 1.5  1997/01/27 00:36:45  ldp
-    Polish
 
-    Revision 1.4  1996/12/10 14:00:10  aros
-    Moved #include into first column to allow makedepend to see it.
-
-    Revision 1.3  1996/11/08 11:28:05  aros
-    All OS function use now Amiga types
-
-    Moved intuition-driver protos to intuition_intern.h
-
-    Revision 1.2  1996/10/24 15:51:25  aros
-    Use the official AROS macros over the __AROS versions.
-
-    Revision 1.1  1996/10/21 17:56:23  aros
-    Two new functions
-
-
-    Desc:
+    Desc: Move window behind all other windows.
     Lang: english
 */
 #include "intuition_intern.h"
