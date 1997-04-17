36f1abb5ffb92915ba38a5425a693cd8ecd9a3e9
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-04-17 13:18:13 +0000

    Removed assembler code (was inside a comment)
    If you compile with -DTEST -NO_MAIN, then the testcode is generated and
    no main().
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2180 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-unix/exec/stackswap.c
+++ arch/all-unix/exec/stackswap.c
@@ -197,7 +197,6 @@ struct ExecBase * SysBase = (struct ExecBase *)0x0BAD0BAD;
 	this->tc_SPUpper = ptr2;
 	sss->stk_Upper	 = (ULONG)ptr1;
 
-
 	longjmp (env, 1); /* Switch stack */
     }
 
@@ -205,84 +204,7 @@ struct ExecBase * SysBase = (struct ExecBase *)0x0BAD0BAD;
     Enable ();
 } /* StackSwap */
 
-#if 0
-	#include "machine.i"
-
-	.text
-	.balign 16
-	.globl	AROS_SLIB_ENTRY(StackSwap,Exec)
-	.type	AROS_SLIB_ENTRY(StackSwap,Exec),@function
-
-	/* The stack looks like this:
-
-	    8 SysBase
-	    4 sss
-	    0 return address
-	*/
-
-#define sss	    4
-#define SysBase     8
-
-AROS_SLIB_ENTRY(StackSwap,Exec):
-	/* Read parameter sss */
-	movl sss(%esp),%edx
-
-	/* copy new SP into ecx */
-	movl stk_Pointer(%edx),%ecx
-
-	/* Pop return address and sss from the current stack and copy them
-	    onto the one specified in sss */
-	popl %eax		    /* pop Return address */
-	movl %eax,-12(%ecx)         /* Push return address on new stack */
-	popl %eax		    /* pop sss */
-	movl %eax,-8(%ecx)          /* Push sss on new stack */
-
-	/* Copy SysBase from the current stack onto the one in sss */
-	movl (%esp),%eax
-	movl %eax,-4(%ecx)          /* Push SysBase on new stack */
-
-	/* Calc new start of stack in sss */
-	addl $-12,%ecx
-
-	/* Call Disable() (SysBase is still on the stack) */
-	leal Disable(%eax),%eax
-	call *%eax
-	popl %eax		    /* Remove SysBase from current stack */
-
-	movl %esp,stk_Pointer(%edx) /* Save current SP in sss */
-	movl %ecx,%esp		    /* Load the new stack */
-
-	movl ThisTask(%eax),%ecx
-	leal tc_SPLower(%ecx),%ecx  /* ecx = &SysBase->ThisTask->tc_SPLower */
-
-	push %ebx		    /* Save register */
-
-	/* Swap ThisTask->tc_SPLower and sss->stk_Lower */
-	movl stk_Lower(%edx),%eax
-	movl (%ecx),%ebx
-	movl %eax,(%ecx)
-	movl %ebx,stk_Lower(%edx)
-
-	/* Swap tc_SPUpper and sss->stk_Upper, too */
-	movl stk_Upper(%edx),%eax
-	movl 4(%ecx),%ebx
-	movl %eax,4(%ecx)
-	movl %ebx,stk_Upper(%edx)
-
-	popl %ebx		    /* Restore register */
-
-	/* Call Enable() */
-	movl SysBase(%esp),%eax
-	pushl %eax		    /* push SysBase on new stack */
-	leal Enable(%eax),%eax      /* call enable */
-	call *%eax
-	addl $4,%esp		    /* Clean stack */
-
-	/* Note that at this time, the new stack from sss contains the
-	   same values as the previous stack */
-	ret
-#endif
-
+#ifndef NO_MAIN
 #ifdef TEST
 
 ULONG teststack[4096];
@@ -335,4 +257,5 @@ int main (int argc, char ** argv)
     return 0;
 }
 
-#endif
+#endif /* TEST */
+#endif /* !NO_MAIN */
