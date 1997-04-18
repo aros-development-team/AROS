8e88c23db8b83537dd5ded9d6bf348eff5a44b10
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-04-18 14:49:36 +0000

    Polish
    Make all tasks ETasks by default
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2193 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/addprocess.c
+++ rom/dos/addprocess.c
@@ -1,28 +1,32 @@
 /*
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
-    $Log$
-    Revision 1.5  1997/03/19 16:35:07  digulla
-    Removed log
 
-    Corrected pr_ReturnAddr
-
-
-    Desc:
+    Desc: Add a new Process
     Lang: english
 */
 #include <dos/dosextens.h>
 #include <proto/exec.h>
 #include "dos_intern.h"
 
-struct Process *AddProcess(struct Process *process, STRPTR argPtr,
-ULONG argSize, APTR initialPC, APTR finalPC, struct DosLibrary *DOSBase)
+struct Process * AddProcess (
+    struct Process    * process,
+    STRPTR		argPtr,
+    ULONG		argSize,
+    APTR		initialPC,
+    APTR		finalPC,
+    struct DosLibrary * DOSBase)
 {
-    APTR *sp=process->pr_Task.tc_SPUpper;
-    *--sp=SysBase;
-    *--sp=(APTR)argSize;
-    *--sp=argPtr;
-    process->pr_ReturnAddr=sp-3;
-    process->pr_Task.tc_SPReg=(STRPTR)sp-SP_OFFSET;
-    return (struct Process *)AddTask(&process->pr_Task,initialPC,finalPC);
+    APTR * sp = process->pr_Task.tc_SPUpper;
+
+    *--sp = SysBase;
+    *--sp = (APTR)argSize;
+    *--sp = argPtr;
+
+    process->pr_ReturnAddr = sp-3;
+
+    process->pr_Task.tc_SPReg  = (STRPTR)sp-SP_OFFSET;
+    process->pr_Task.tc_Flags |= TF_ETASK;
+
+    return (struct Process *)AddTask (&process->pr_Task, initialPC, finalPC);
 } /* AddProcess */
