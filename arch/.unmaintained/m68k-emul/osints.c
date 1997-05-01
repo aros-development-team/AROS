99fc2cb3e6c0aeef30220a79898449f556790153
aros <aros@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-05-01 13:55:43 +0000

    New file
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2239 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/.unmaintained/m68k-emul/osints.c
@@ -0,0 +1,33 @@
+/*
+    (C) 1995-97 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: OS specific functions for signal blocking
+    Lang: english
+*/
+#include <signal.h>
+#include <stdio.h>
+
+/* The real functions are written in assembler as stubs which call these */
+void _os_enable (void)
+{
+    sigset_t set;
+
+    /* if (supervisor)
+    {
+        fprintf (stderr, "Enable() called in supervisor mode\n");
+    } */
+
+    sigfillset (&set);
+
+    sigprocmask (SIG_UNBLOCK, &set, NULL);
+} /* _os_enable */
+
+void _os_disable (void)
+{
+    sigset_t set;
+
+    sigfillset (&set);
+
+    sigprocmask (SIG_BLOCK, &set, NULL);
+} /* _os_disable */
