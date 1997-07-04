b3aa9af1974c742282d435b66b4b41ff98bd3675
srittau <srittau@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-07-04 19:08:25 +0000

    Modified to behave a bit more like the AmigaOS version. Added first version of assign listing.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2413 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/assign.c
+++ workbench/c/assign.c
@@ -6,34 +6,93 @@
     Lang: english
 */
 
+#include <stdio.h>
+#include <exec/types.h>
 #include <exec/memory.h>
 #include <exec/execbase.h>
 #include <proto/exec.h>
 #include <dos/dos.h>
+#include <dos/dosextens.h>
 #include <proto/dos.h>
 #include <utility/tagitem.h>
 
-static const char version[] = "$VER: assign 41.1 (14.3.1997)\n";
+static const char version[] = "$VER: assign 41.2 (4.7.1997)\n";
+
+void dolist()
+{
+    struct DosList *dlist, *curlist;
+    int count;
+
+    dlist = LockDosList(LDF_VOLUMES|LDF_ASSIGNS|LDF_DEVICES|LDF_READ);
+    printf("Volumes:\n");
+    curlist = dlist;
+    while ((curlist = NextDosEntry(curlist, LDF_VOLUMES)))
+    {
+        printf("%s\n", curlist->dol_DevName);
+	/* !!! mounted !!! */
+    }
+    printf("\nDirectories:\n");
+    curlist = dlist;
+    while ((curlist = NextDosEntry(curlist, LDF_ASSIGNS)))
+    {
+        printf(curlist->dol_DevName);
+	for (count=15-strlen(curlist->dol_DevName); count>0; count--)
+	    printf(" ");
+	printf("\n"); /* !!! print directory !!! */
+    }
+    /* !!! late/nonbinding !!! */
+    printf("\nDevices:\n");
+    count = 0;
+    curlist = dlist;
+    while ((curlist = NextDosEntry(curlist, LDF_DEVICES)))
+    {
+        printf("%s ", curlist->dol_DevName);
+	count++;
+	if (count == 5)
+	{
+	    printf("\n");
+	    count = 0;
+	}
+    }
+    if (count < 5)
+        printf("\n");
+    UnLockDosList(LDF_VOLUMES|LDF_ASSIGNS|LDF_DEVICES|LDF_READ);
+}
+
+
+int doassign(STRPTR name, STRPTR target)
+{
+    int error = RETURN_OK;
+    BPTR dir;
+
+    dir=Lock(target,SHARED_LOCK);
+    if(dir)
+    {
+        STRPTR s=name;
+	while(*s)
+	    if((*s)++==':')
+		s[-1]=0;
+	if (!AssignLock(name,dir))
+	    error = RETURN_FAIL;
+    } else
+        error = RETURN_FAIL;
+    return error;
+}
+
 
 int main (int argc, char ** argv)
 {
-    STRPTR args[2]={ NULL, NULL };
+    STRPTR args[3]={ NULL, NULL, NULL };
     struct RDArgs *rda;
-    BPTR dir;
-    LONG error=0;
+    int error=RETURN_OK;
 
-    rda=ReadArgs("DEVICE/A,DIR/A",(IPTR *)args,NULL);
+    rda=ReadArgs("NAME,TARGET,LIST/S",(IPTR *)args,NULL);
     if(rda!=NULL)
     {
-	dir=Lock(args[1],SHARED_LOCK);
-	if(dir)
-	{
-	    STRPTR s=args[0];
-	    while(*s)
-		if(*s++==':')
-		    s[-1]=0;
-	    AssignLock(args[0],dir);
-	}
+	if (args[0] != NULL && args[1] != NULL)
+	    error = doassign(args[0], args[1]);
+        if (args[0] == NULL || args[2] != NULL)
+	    dolist();
 	FreeArgs(rda);
     }else
 	error=RETURN_FAIL;
