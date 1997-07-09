e6d620dd83724f56ea86600e5e6bbb6c267702e3
srittau <srittau@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-07-09 16:42:04 +0000

    Addd SetGadgetAttrs().
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2431 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/alib/setgadgetattrs.c
@@ -0,0 +1,69 @@
+/*
+    (C) 1995-96 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: Change the attributes of a BOOPSI gadget
+    Lang: english
+*/
+#include <intuition/classes.h>
+#include <intuition/intuitionbase.h>
+#include "alib_intern.h"
+
+extern struct IntuitionBase * IntuitionBase;
+
+/*****************************************************************************
+
+    NAME */
+#include <intuition/classusr.h>
+#include <intuition/intuition.h>
+#define NO_INLINE_STDARG /* turn off inline def */
+#include <proto/intuition.h>
+
+	IPTR SetGadgetAttrs (
+
+/*  SYNOPSIS */
+	struct Gadget    * Gadget,
+	struct Window    * Window,
+	struct Requester * Requester,
+	ULONG tag1,
+	...)
+
+/*  FUNCTION
+	Changes several attributes of a gadget at the same time. How the
+	gadget interprets the new attributes depends on the class.
+
+    INPUTS
+	Gadget - Change the attributes of this object
+	Window - The window the gadget is in
+	Requester - The Requester the gadget is in, may be NULL
+	tag1 - The first of a list of attribute/value-pairs. The last
+		attribute in this list must be TAG_END or TAG_DONE.
+		The value for this last attribute is not examined (ie.
+		you need not specify it).
+
+    RESULT
+	This value is non-zero if the gadget needs redrawing after the
+	values have changed.
+
+    NOTES
+	This function sends OM_SET to the object.
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	NewObject(), SetAttrs(),
+	"Basic Object-Oriented Programming System for Intuition" and
+	"boopsi Class Reference" Dokument.
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+    AROS_SLOWSTACKTAGS_PRE(tag1)
+    retval = SetGadgetAttrsA (Gadget, Window, Requester, AROS_SLOWSTACKTAGS_ARG(tag1));
+    AROS_SLOWSTACKTAGS_POST
+} /* SetGadgetAttrs */
