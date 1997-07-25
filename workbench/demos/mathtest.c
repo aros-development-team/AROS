ebf7bc663ea6c6882ab9d272b00c9d4b783da981
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-07-25 15:28:01 +0000

    Polish.
    Show what's wrong in case of an error.
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2494 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/demos/mathtest.c
+++ workbench/demos/mathtest.c
@@ -13,37 +13,42 @@ struct Library * MathTransBase;
 
 int main(int argc, char ** argv)
 {
-  if (!(MathBase = OpenLibrary("mathffp.library", 0L)))
-  {
-    FPrintf((BPTR)stderr, "Couldn't open mathffp.library\n");
+    LONG res;
+
+    if (!(MathBase = OpenLibrary("mathffp.library", 0L)))
+    {
+	FPrintf((BPTR)stderr, "Couldn't open mathffp.library\n");
+	return (0);
+    }
+
+    printf("Basic mathffp functionality test...\n");
+
+    /* this should set the zero-bit*/
+    if ( 0 != SPAbs(0))
+	Printf("Error with the SPAbs-function!\n");
+    else
+	Printf("SPAbs-function ok!\n");
+
+    CloseLibrary(MathBase);
+
+
+    if (!(MathTransBase = OpenLibrary("mathtrans.library", 0L)))
+    {
+	fprintf (stderr, "Couldn't open mathtrans.library\n");
+	return (0);
+    }
+
+    printf("Basic mathtrans functionality test...\n");
+
+    res = SPLog (0x80000042);
+
+    if (res != 0xb1721840)
+	Printf ("Error with the SPLog-function (got=%08lx expected=0xb1721840)\n",
+	    res);
+    else
+	Printf ("SPLog-function ok!\n");
+
+    CloseLibrary(MathTransBase);
+
     return (0);
-  }
-
-  Printf("Basic mathffp functionality test...\n");
-
-  /* this should set the zero-bit*/
-  if ( 0 != SPAbs(0))
-     Printf("Error with the SPAbs-function!\n");
-  else
-     Printf("SPAbs-function ok!\n");
-
-  CloseLibrary(MathBase);
-
-
-  if (!(MathTransBase = OpenLibrary("mathtrans.library", 0L)))
-  {
-    FPrintf((BPTR)stderr, "Couldn't open mathtrans.library\n");
-    return (0);
-  }
-
-  printf("Basic mathtrans functionality test...\n");
-
-  if ( 0xb1721840 != SPLog(0x80000042))
-    Printf("Error with the SPLog-function");
-  else
-    Printf("SPLog-function ok!\n");
-
-  CloseLibrary(MathTransBase);
-
-  return (0);
 }
