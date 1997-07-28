867bb08b674a83c4f70abc68f063c0923e45980d
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-07-28 20:42:18 +0000

    new file, needed by sin,cos and tan
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2509 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/mathtrans/intern_spisodd.c
@@ -0,0 +1,28 @@
+/*
+    (C) 1995-97 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc:
+    Lang: english
+*/
+
+
+#include <libraries/mathffp.h>
+#include <aros/libcall.h>
+#include <proto/mathffp.h>
+#include <proto/mathtrans.h>
+#include <proto/exec.h>
+#include <exec/types.h>
+#include "mathtrans_intern.h"
+
+LONG intern_SPisodd(ULONG fnum)
+{
+  char Exponent = ((fnum & FFPExponent_Mask)) - 0x41;
+  ULONG Mask = (0x80000000 >> Exponent);
+
+  if ((fnum & Mask) != 0)
+    return TRUE;
+  else
+    return FALSE;
+
+} /* intern_SPisodd  */
