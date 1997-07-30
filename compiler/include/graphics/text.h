8b39a427b1a734381f4c04ac49367cbef857bf47
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-07-30 08:10:46 +0000

    Added a macro to get the replyport of a font.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2517 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/graphics/text.h
+++ compiler/include/graphics/text.h
@@ -22,39 +22,44 @@
 struct TextFont
 {
     struct Message tf_Message;
-    UWORD          tf_YSize;
-    UBYTE          tf_Style;
-    UBYTE          tf_Flags;
-    UWORD          tf_XSize;
-    UWORD          tf_Baseline;
-    UWORD          tf_BoldSmear;
-    UWORD          tf_Accessors;
-    UBYTE          tf_LoChar;
-    UBYTE          tf_HiChar;
-    APTR           tf_CharData;
-    UWORD          tf_Modulo;
-    APTR           tf_CharLoc;
-    APTR           tf_CharSpace;
-    APTR           tf_CharKern;
+    UWORD	   tf_YSize;
+    UBYTE	   tf_Style;
+    UBYTE	   tf_Flags;
+    UWORD	   tf_XSize;
+    UWORD	   tf_Baseline;
+    UWORD	   tf_BoldSmear;
+    UWORD	   tf_Accessors;
+    UBYTE	   tf_LoChar;
+    UBYTE	   tf_HiChar;
+    APTR	   tf_CharData;
+    UWORD	   tf_Modulo;
+    APTR	   tf_CharLoc;
+    APTR	   tf_CharSpace;
+    APTR	   tf_CharKern;
 };
-#define tf_Extension tf_Message.mn_ReplyPort
+#define tf_Extension ((struct TextFontExtension *)tf_Message.mn_ReplyPort)
+#define GetTextFontReplyPort(font) \
+	((font->tf_Style & FSF_EXTENDED) \
+	    ? font->tf_Extension->tfe_OrigReplyPort \
+	    : font->tf_Message.mn_ReplyPort \
+	)
 
 struct TextFontExtension
 {
-    UWORD tfe_MatchWord;
-    UBYTE tfe_Flags0;
-    UBYTE tfe_Flags1;
+    UWORD	      tfe_MatchWord;
+    UBYTE	      tfe_Flags0;
+    UBYTE	      tfe_Flags1;
 
     struct TextFont * tfe_BackPtr;
     struct MsgPort  * tfe_OrigReplyPort;
     struct TagItem  * tfe_Tags;
 
-    UWORD * tfe_OFontPatchS;
-    UWORD * tfe_OFontPatchK;
+    UWORD	    * tfe_OFontPatchS;
+    UWORD	    * tfe_OFontPatchK;
 };
 
 /* tfe_Flags0 */
-#define TEOB_NOREMFONT     0
+#define TEOB_NOREMFONT	   0
 #define TEOF_NOREMFONT (1<<0)
 
 /* Text Attributes */
@@ -79,40 +84,40 @@ struct TTextAttr
 };
 
 /* ta_Style/tta_Style */
-#define FS_NORMAL          0
-#define FSB_UNDERLINED     0
+#define FS_NORMAL	   0
+#define FSB_UNDERLINED	   0
 #define FSF_UNDERLINED (1<<0)
-#define FSB_BOLD           1
+#define FSB_BOLD	   1
 #define FSF_BOLD       (1<<1)
-#define FSB_ITALIC         2
+#define FSB_ITALIC	   2
 #define FSF_ITALIC     (1<<2)
-#define FSB_EXTENDED       3
+#define FSB_EXTENDED	   3
 #define FSF_EXTENDED   (1<<3)
-#define FSB_COLORFONT      6
+#define FSB_COLORFONT	   6
 #define FSF_COLORFONT  (1<<6)
-#define FSB_TAGGED         7
+#define FSB_TAGGED	   7
 #define FSF_TAGGED     (1<<7)
 
 /* ta_Flags/tta_Flags */
-#define FPB_ROMFONT          0
-#define FPF_ROMFONT      (1<<0)
-#define FPB_DISKFONT         1
-#define FPF_DISKFONT     (1<<1)
-#define FPB_REVPATH          2
-#define FPF_REVPATH      (1<<2)
-#define FPB_TALLDOT          3
-#define FPF_TALLDOT      (1<<3)
-#define FPB_WIDEDOT          4
-#define FPF_WIDEDOT      (1<<4)
+#define FPB_ROMFONT	     0
+#define FPF_ROMFONT	 (1<<0)
+#define FPB_DISKFONT	     1
+#define FPF_DISKFONT	 (1<<1)
+#define FPB_REVPATH	     2
+#define FPF_REVPATH	 (1<<2)
+#define FPB_TALLDOT	     3
+#define FPF_TALLDOT	 (1<<3)
+#define FPB_WIDEDOT	     4
+#define FPF_WIDEDOT	 (1<<4)
 #define FPB_PROPORTIONAL     5
 #define FPF_PROPORTIONAL (1<<5)
-#define FPB_DESIGNED         6
-#define FPF_DESIGNED     (1<<6)
-#define FPB_REMOVED          7
-#define FPF_REMOVED      (1<<7)
+#define FPB_DESIGNED	     6
+#define FPF_DESIGNED	 (1<<6)
+#define FPB_REMOVED	     7
+#define FPF_REMOVED	 (1<<7)
 
 /* tta_Tags */
-#define TA_DeviceDPI       (TAG_USER | 1)
+#define TA_DeviceDPI	   (TAG_USER + 1)
 
 #define MAXFONTMATCHWEIGHT 32767
 
@@ -141,7 +146,7 @@ struct ColorTextFont
 };
 
 /* ctf_Flags */
-#define CTB_MAPCOLOR     0
+#define CTB_MAPCOLOR	 0
 #define CTF_MAPCOLOR (1<<0)
 #define CT_COLORFONT (1<<0)
 #define CT_GREYFONT  (1<<1)
