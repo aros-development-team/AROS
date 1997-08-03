9d8c6a3be88ec69dcc151e7446be9c69ab4e1d98
ldp <ldp@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-08-03 01:49:29 +0000

    Include IsDosEntry() prototype
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2536 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/clib/arossupport_protos.h
+++ compiler/include/clib/arossupport_protos.h
@@ -47,6 +47,7 @@ APTR	RemoveSList  (APTR * list, APTR node);
 void	hexdump      (const void * data, IPTR offset, ULONG count);
 int	strrncasecmp (const char *, const char *, int);
 void	RawPutChars  (const UBYTE * string, int len);
+BOOL    IsDosEntryA  (char *Name, ULONG Flags);
 
 /* AROS enhancements */
 BOOL ReadByte	 (struct Hook *, UBYTE  * dataptr, void * stream);
