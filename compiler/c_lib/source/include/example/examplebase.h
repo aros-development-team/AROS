883a5610cdb1bf5c7ebfbf81b68fbea858cbc160
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-08-14 18:33:17 +0000

    Use new headers
    SegList is a BPTR
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2608 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/c_lib/source/include/example/examplebase.h
+++ compiler/c_lib/source/include/example/examplebase.h
@@ -1,9 +1,9 @@
 /*
-**	$VER: examplebase.h 37.1 (4.12.96)
+**	$VER: examplebase.h 37.15 (14.8.97)
 **
 **	definition of ExampleBase
 **
-**	(C) Copyright 1996 Andreas R. Kleinert
+**	(C) Copyright 1996-97 Andreas R. Kleinert
 **	All Rights Reserved.
 */
 
@@ -11,20 +11,31 @@
 #define EXAMPLE_EXAMPLEBASE_H
 
 #ifdef	 __MAXON__
-#ifndef  EXEC_LIBRARIES_H
-#include <exec/libraries.h>
-#endif
+#   ifndef  EXEC_LIBRARIES
+#	include <exec/libraries.h>
+#   endif
+#   ifndef  DOS_DOS
+#	include <dos/dos.h>
+#   endif
 #else
-#ifndef  EXEC_LIBRARIES
-#include <exec/libraries.h>
-#endif /* EXEC_LIBRARIES_H */
+#   ifndef  EXEC_LIBRARIES_H
+#	include <exec/libraries.h>
+#   endif /* EXEC_LIBRARIES_H */
+#   ifndef  DOS_DOS_H
+#	include <dos/dos.h>
+#   endif /* DOS_DOS_H */
+#endif
+
+#ifndef LIBCORE_BASE_H
+#   include <libcore/base.h>
 #endif
 
 struct ExampleBase
 {
-    struct Library	   exb_LibNode;
-    APTR		   exb_SegList;
-    struct ExecBase	  *exb_SysBase;
+    /* This is the header which is the same for all libraries */
+    struct LibHeader	   exb_LibHeader;
+
+    /* This is the library specific data. */
     struct IntuitionBase  *exb_IntuitionBase;
     struct GfxBase	  *exb_GfxBase;
 };
