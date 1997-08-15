40047a3a0400ce5dfda48df8e9c1a7fd9b0adfad
nlorentz <nlorentz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-08-15 20:47:13 +0000

    Added protos for new functions in propgadgets.c
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2627 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/intuition/propgadgets.h
+++ rom/intuition/propgadgets.h
@@ -17,6 +17,7 @@
 #   include "gadgets.h"
 #endif
 
+
 int CalcKnobSize (struct Gadget * propGadget, struct BBox * knobbox);
 void RefreshPropGadget (struct Gadget * gadget, struct Window * window,
 	struct IntuitionBase * IntuitionBase);
@@ -24,4 +25,14 @@ void RefreshPropGadgetKnob (UWORD flags, struct BBox * clear,
 	struct BBox * knob, struct Window * window,
 	struct IntuitionBase * IntuitionBase);
 
+VOID HandlePropSelectDown (struct Gadget *gadget, struct Window	*win,
+     struct Requester	*req, UWORD mouse_x, UWORD mouse_y,
+     struct IntuitionBase *IntuitionBase);
+
+VOID HandlePropSelectUp (struct Gadget *gadget, struct Window *w,
+    struct Requester *req, struct IntuitionBase *IntuitionBase);	
+
+VOID HandlePropMouseMove (struct Gadget	*gadget, struct Window *w,
+    struct Requester *req, LONG dx, LONG dy,
+    struct IntuitionBase *IntuitionBase);
 #endif /* _PROPGADGETS_H_ */
