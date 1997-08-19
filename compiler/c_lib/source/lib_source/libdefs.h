abaa7b19ba3b2c9a61647a9290bd127768fa335b
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-08-19 09:31:49 +0000

    Regenerated
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2664 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/c_lib/source/lib_source/libdefs.h
+++ compiler/c_lib/source/lib_source/libdefs.h
@@ -1,8 +1,7 @@
 #ifndef _LIBDEFS_H
 #define _LIBDEFS_H
-#define LIBNAME_STRING   "example.library"
-#define DEVNAME_STRING   "example.device"
-#define RESNAME_STRING   "example.resource"
+#define NAME_STRING      "example.library"
+#define NT_TYPE          NT_LIBRARY
 #define LIBBASE          ExampleBase
 #define LIBBASETYPE      struct ExampleBase
 #define LIBBASETYPEPTR   struct ExampleBase *
@@ -10,7 +9,7 @@
 #define REVISION_NUMBER  15
 #define BASENAME         Example
 #define BASENAME_STRING  "Example"
-#define VERSION_STRING   "$VER: example 37.15 (14.08.1997)\n\r"
+#define VERSION_STRING   "$VER: example 37.15 (18.08.1997)\n\r"
 #define LIBEND           Example_end
 #define LIBFUNCTABLE     Example_functable
 #define COPYRIGHT_STRING "(C)opyright 1996-97 by Andreas R. Kleinert. All rights reserved."
