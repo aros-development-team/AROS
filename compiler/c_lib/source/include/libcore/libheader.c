b43ef3e23d8275a801c48819b32b94bb6af7b8fb
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-08-20 16:04:23 +0000

    Added lots of macros to allow to customize almost every aspect of this code. Code can be optimized with some macros, now. It's also possible to generate unique names for all symbols in this file.
    Moved LibStart() behind #include "libdefs.h"
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2681 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/c_lib/source/include/libcore/libheader.c
+++ compiler/c_lib/source/include/libcore/libheader.c
@@ -26,69 +26,93 @@
 #include <libcore/compiler.h>
 #include <libcore/base.h>
 
-LONG ASM LibStart(void)
+#ifndef LC_LIBHEADERTYPEPTR
+#   define LC_LIBHEADERTYPEPTR	       struct LibHeader *
+#endif
+#ifndef LC_LIB_FIELD
+#   define LC_LIB_FIELD(libBase)       (libBase)->lh_LibNode
+#endif
+#ifndef LC_SYSBASE_FIELD
+#   define LC_SYSBASE_FIELD(libBase)   (libBase)->lh_SysBase
+#endif
+#ifndef LC_SEGLIST_FIELD
+#   define LC_SEGLIST_FIELD(libBase)   (libBase)->lh_SegList
+#endif
+#ifndef LC_RESIDENTNAME
+#   define LC_RESIDENTNAME     LC_BUILDNAME(ROMTag)
+#endif
+#ifndef LC_RESIDENTPRI
+#   define LC_RESIDENTPRI      0
+#endif
+#ifndef LC_RESIDENTFLAGS
+#   define LC_RESIDENTFLAGS    RTF_AUTOINIT
+#endif
+#ifndef LC_LIBBASESIZE
+#   define LC_LIBBASESIZE  sizeof (struct LibHeader)
+#endif
+/* If the file with the #defines for this library is not "libdefs.h",
+    then you can redefine it. */
+#ifndef LC_LIBDEFS_FILE
+#   define LC_LIBDEFS_FILE "libdefs.h"
+#endif
+
+/* Include the file with the #defines for this library */
+#include LC_LIBDEFS_FILE
+
+LONG ASM LC_BUILDNAME(LibStart) (void)
 {
     return(-1);
 }
 
-/* If the file with the #defines for this library is not "libdefs.h",
-    then you can redefine it. */
-#ifndef LIBDEFS_FILE
-#   define LIBDEFS_FILE "libdefs.h"
-#endif
-
-/* Include the file with the #defines for this library */
-#include LIBDEFS_FILE
-
 /* Predeclarations */
 extern const int LIBEND;	  /* The end of the library */
 extern const APTR LIBFUNCTABLE[]; /* The function table */
-extern const struct InitTable InitTab;
-extern const struct DataTable DataTab;
+static const struct InitTable LC_BUILDNAME(InitTab);
+static const struct DataTable LC_BUILDNAME(DataTab);
 
-extern const char ALIGNED ExLibName [];
-extern const char ALIGNED ExLibID   [];
-extern const char ALIGNED Copyright [];
+extern const char ALIGNED LC_BUILDNAME(LibName)   [];
+extern const char ALIGNED LC_BUILDNAME(LibID)     [];
+extern const char ALIGNED LC_BUILDNAME(Copyright) [];
 
-extern AROS_LH2 (struct LibHeader *, InitLib,
-    AROS_LHA(struct LibHeader *, lh,      D0),
-    AROS_LHA(BPTR,               segList, A0),
+extern AROS_LH2 (LC_LIBHEADERTYPEPTR, LC_BUILDNAME(InitLib),
+    AROS_LHA(LC_LIBHEADERTYPEPTR, lh,      D0),
+    AROS_LHA(BPTR,                segList, A0),
     struct ExecBase *, sysBase, 0, LibHeader
 );
-extern AROS_LH0 (BPTR, ExpungeLib,
-    struct LibHeader *, lh, 3, LibHeader
+extern AROS_LH0 (BPTR, LC_BUILDNAME(ExpungeLib),
+    LC_LIBHEADERTYPEPTR, lh, 3, LibHeader
 );
 
-struct Resident const ALIGNED ROMTag =	   /* do not change */
+struct Resident const ALIGNED LC_RESIDENTNAME =
 {
-    RTC_MATCHWORD,		    /* This is a romtag */
-    (struct Resident *)&ROMTag,     /* This is a valid romtag */
+    RTC_MATCHWORD,			    /* This is a romtag */
+    (struct Resident *)&LC_RESIDENTNAME,
     (APTR) &LIBEND,
-    RTF_AUTOINIT,
+    LC_RESIDENTFLAGS,
     VERSION_NUMBER,
     NT_TYPE,
-    0,
-    (char *) &ExLibName[0],
-    (char *) &ExLibID[0],
-    (APTR) &InitTab
+    LC_RESIDENTPRI,
+    (char *) &LC_BUILDNAME(LibName)[0],
+    (char *) &LC_BUILDNAME(LibID)[0],
+    (APTR) &LC_BUILDNAME(InitTab)
 };
 
-struct InitTable		       /* do not change */
+static struct InitTable 		      /* do not change */
 {
     ULONG		    LibBaseSize;
     const APTR		   *FunctionTable;
     const struct DataTable *DataTable;
     APTR		    InitLibTable;
 }
-const InitTab =
+const LC_BUILDNAME(InitTab) =
 {
-    sizeof (struct LibHeader),
+    LC_LIBBASESIZE,
     &LIBFUNCTABLE[0],
-    &DataTab,
-    (APTR) AROS_SLIB_ENTRY(InitLib, LibHeader)
+    &LC_BUILDNAME(DataTab),
+    (APTR) AROS_SLIB_ENTRY(LC_BUILDNAME(InitLib), LibHeader)
 };
 
-struct DataTable		    /* do not change */
+static struct DataTable 		   /* do not change */
 {
     UWORD ln_Type_Init;      UWORD ln_Type_Offset;	UWORD ln_Type_Content;
     UBYTE ln_Name_Init;      UBYTE ln_Name_Offset;	ULONG ln_Name_Content;
@@ -98,53 +122,63 @@ struct DataTable		    /* do not change */
     UBYTE lib_IdString_Init; UBYTE lib_IdString_Offset; ULONG lib_IdString_Content;
     ULONG ENDMARK;
 }
-const DataTab =
+const LC_BUILDNAME(DataTab) =
 {
     INITBYTE(OFFSET(Node,         ln_Type),      NT_TYPE),
-    0x80, (UBYTE) OFFSET(Node,    ln_Name),      (ULONG) &ExLibName[0],
+    0x80, (UBYTE) OFFSET(Node,    ln_Name),      (ULONG) &LC_BUILDNAME(LibName[0]),
     INITBYTE(OFFSET(Library,      lib_Flags),    LIBF_SUMUSED|LIBF_CHANGED),
     INITWORD(OFFSET(Library,      lib_Version),  VERSION_NUMBER),
     INITWORD(OFFSET(Library,      lib_Revision), REVISION_NUMBER),
-    0x80, (UBYTE) OFFSET(Library, lib_IdString), (ULONG) &ExLibID[0],
+    0x80, (UBYTE) OFFSET(Library, lib_IdString), (ULONG) &LC_BUILDNAME(LibID)[0],
     (ULONG) 0
 };
 
-const char ALIGNED ExLibName [] = NAME_STRING;
-const char ALIGNED ExLibID   [] = VERSION_STRING;
-const char ALIGNED Copyright [] = COPYRIGHT_STRING;
-
-#ifndef SYSBASE_FIELD
-#   define SYSBASE_FIELD(libBase)   (libBase)->lh_SysBase
-#endif
-#ifndef SEGLIST_FIELD
-#   define SEGLIST_FIELD(libBase)   (libBase)->lh_SegList
-#endif
+const char ALIGNED LC_BUILDNAME(LibName)   [] = NAME_STRING;
+const char ALIGNED LC_BUILDNAME(LibID)     [] = VERSION_STRING;
+const char ALIGNED LC_BUILDNAME(Copyright) [] = COPYRIGHT_STRING;
 
 /* Use supplied functions to initialize the non-standard parts of the
    library */
-ULONG SAVEDS STDARGS L_OpenLibs(struct LibHeader * lh);
-ULONG SAVEDS STDARGS L_CloseLibs(struct LibHeader * lh);
+#ifndef LC_NO_INITLIB
+ULONG SAVEDS STDARGS L_InitLib	(LC_LIBHEADERTYPEPTR lh);
+#else
+#   undef L_InitLib
+#   define L_InitLib(x)         1
+#endif
+#ifndef LC_NO_OPENLIB
+ULONG SAVEDS STDARGS L_OpenLib	(LC_LIBHEADERTYPEPTR lh);
+#else
+#   undef L_OpenLib
+#   define L_OpenLib(x)         1
+#endif
+#ifndef LC_NO_CLOSELIB
+void  SAVEDS STDARGS L_CloseLib (LC_LIBHEADERTYPEPTR lh);
+#else
+#   undef L_CloseLib
+#   define L_CloseLib(x)        /* eps */
+#endif
 
-AROS_LH2 (struct LibHeader *, InitLib,
-    AROS_LHA(struct LibHeader *, lh,      D0),
-    AROS_LHA(BPTR,               segList, A0),
+AROS_LH2 (LC_LIBHEADERTYPEPTR, LC_BUILDNAME(InitLib),
+    AROS_LHA(LC_LIBHEADERTYPEPTR, lh,      D0),
+    AROS_LHA(BPTR,                segList, A0),
     struct ExecBase *, sysBase, 0, LibHeader
 )
 {
-    SYSBASE_FIELD(lh) = sysBase;
-    SEGLIST_FIELD(lh) = segList;
+    LC_SYSBASE_FIELD(lh) = sysBase;
+    LC_SEGLIST_FIELD(lh) = segList;
 
-    if (L_OpenLibs (lh))
+#ifndef LC_NO_INITLIB
+    if (L_InitLib (lh))
 	return (lh);
 
-    L_CloseLibs (lh);
+    L_CloseLib (lh);
 
     {
 	ULONG negsize, possize, fullsize;
 	UBYTE *negptr = (UBYTE *) lh;
 
-	negsize  = lh->lh_LibNode.lib_NegSize;
-	possize  = lh->lh_LibNode.lib_PosSize;
+	negsize  = LC_LIB_FIELD(lh).lib_NegSize;
+	possize  = LC_LIB_FIELD(lh).lib_PosSize;
 	fullsize = negsize + possize;
 	negptr	-= negsize;
 
@@ -152,11 +186,14 @@ AROS_LH2 (struct LibHeader *, InitLib,
     }
 
     return (NULL);
+#else
+    return (lh);
+#endif /* LC_NO_INITLIB */
 }
 
-AROS_LH1 (struct LibHeader *, OpenLib,
+AROS_LH1 (LC_LIBHEADERTYPEPTR, LC_BUILDNAME(OpenLib),
     AROS_LHA (ULONG, version, D0),
-    struct LibHeader *, lh, 1, LibHeader
+    LC_LIBHEADERTYPEPTR, lh, 1, LibHeader
 )
 {
 #ifdef __MAXON__
@@ -164,29 +201,32 @@ AROS_LH1 (struct LibHeader *, OpenLib,
     InitModules();
 #endif
 
+    if (L_OpenLib (lh))
+    {
 #ifndef NOEXPUNGE
-    lh->lh_LibNode.lib_OpenCnt++;
+	LC_LIB_FIELD(lh).lib_OpenCnt++;
 #else
-    lh->lh_LibNode.lib_OpenCnt = 1;
+	LC_LIB_FIELD(lh).lib_OpenCnt = 1;
 #endif /* NOEXPUNGE */
 
-    lh->lh_LibNode.lib_Flags &= ~LIBF_DELEXP;
+	LC_LIB_FIELD(lh).lib_Flags &= ~LIBF_DELEXP;
+    }
 
     return(lh);
 }
 
-AROS_LH0 (BPTR, CloseLib,
-    struct LibHeader *, lh, 2, LibHeader
+AROS_LH0 (BPTR, LC_BUILDNAME(CloseLib),
+    LC_LIBHEADERTYPEPTR, lh, 2, LibHeader
 )
 {
 #ifndef NOEXPUNGE
-    lh->lh_LibNode.lib_OpenCnt--;
+    LC_LIB_FIELD(lh).lib_OpenCnt--;
 
-    if(!lh->lh_LibNode.lib_OpenCnt)
+    if(!LC_LIB_FIELD(lh).lib_OpenCnt)
     {
-	if(lh->lh_LibNode.lib_Flags & LIBF_DELEXP)
+	if(LC_LIB_FIELD(lh).lib_Flags & LIBF_DELEXP)
 	{
-	    return (AROS_SLIB_ENTRY(ExpungeLib,LibHeader)(lh));
+	    return (AROS_SLIB_ENTRY(LC_BUILDNAME(ExpungeLib),LibHeader)(lh));
 	}
     }
 #endif /* NOEXPUNGE */
@@ -194,26 +234,26 @@ AROS_LH0 (BPTR, CloseLib,
     return (NULL);
 }
 
-AROS_LH0 (BPTR, ExpungeLib,
-    struct LibHeader *, lh, 3, LibHeader
+AROS_LH0 (BPTR, LC_BUILDNAME(ExpungeLib),
+    LC_LIBHEADERTYPEPTR, lh, 3, LibHeader
 )
 {
 #ifndef NOEXPUNGE
     BPTR seglist;
 
-    if(!lh->lh_LibNode.lib_OpenCnt)
+    if(!LC_LIB_FIELD(lh).lib_OpenCnt)
     {
 	ULONG negsize, possize, fullsize;
 	UBYTE *negptr = (UBYTE *)lh;
 
-	seglist = SEGLIST_FIELD(lh);
+	seglist = LC_SEGLIST_FIELD(lh);
 
 	Remove((struct Node *)lh);
 
-	L_CloseLibs(lh);
+	L_CloseLib (lh);
 
-	negsize  = lh->lh_LibNode.lib_NegSize;
-	possize  = lh->lh_LibNode.lib_PosSize;
+	negsize  = LC_LIB_FIELD(lh).lib_NegSize;
+	possize  = LC_LIB_FIELD(lh).lib_PosSize;
 	fullsize = negsize + possize;
 	negptr	-= negsize;
 
@@ -226,14 +266,14 @@ AROS_LH0 (BPTR, ExpungeLib,
 	return(seglist);
     }
 
-    lh->lh_LibNode.lib_Flags |= LIBF_DELEXP;
+    LC_LIB_FIELD(lh).lib_Flags |= LIBF_DELEXP;
 #endif /* NOEXPUNGE */
 
     return (NULL);
 }
 
-AROS_LH0 (struct LibHeader *, ExtFuncLib,
-    struct LibHeader *, lh, 4, LibHeader
+AROS_LH0 (LC_LIBHEADERTYPEPTR, LC_BUILDNAME(ExtFuncLib),
+    LC_LIBHEADERTYPEPTR, lh, 4, LibHeader
 )
 {
     return(NULL);
