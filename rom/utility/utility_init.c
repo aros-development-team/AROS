368bcdff82961b6ec1b756ca9d95015839525bcc
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-08-20 16:16:34 +0000

    Use CLib code
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2690 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/utility/utility_init.c
+++ rom/utility/utility_init.c
@@ -1,32 +1,30 @@
 /*
-    Copyright (C) 1995-97 AROS - The Amiga Replacement OS
+    (C) 1995-97 AROS - The Amiga Replacement OS
     $Id$
 
     Desc: Utility Resident and initialization.
     Lang: english
 */
-#include <exec/types.h>
-#include <aros/system.h>
-#include <aros/libcall.h>
-#include <exec/alerts.h>
-#include <exec/libraries.h>
-#include <exec/resident.h>
-#include <exec/execbase.h>
-#include <dos/dos.h>
-#include <proto/exec.h>
-#include <proto/utility.h>
-
-#include "utility_intern.h"
+#include "intern.h"
 #include "libdefs.h"
 
-#define INIT	AROS_SLIB_ENTRY(init,Utility)
+#ifdef SysBase
+#   undef SysBase
+#endif
 
-extern const UBYTE name[];
-extern const UBYTE version[];
-extern const APTR inittabl[4];
-extern void *const FUNCTABLE[];
-struct LIBBASETYPE *INIT();
-extern const char END;
+/* Customize libheader.c */
+#define LC_SYSBASE_FIELD(lib)   (((struct IntUtilityBase *)(lib))->ub_SysBase)
+#define LC_SEGLIST_FIELD(lib)   (((struct IntUtilityBase *)(lib))->ub_SegList)
+#define LC_RESIDENTNAME 	Utility_resident
+#define LC_RESIDENTFLAGS	RTF_AUTOINIT|RTF_COLDSTART
+#define LC_RESIDENTPRI		103
+#define LC_LIBBASESIZE		sizeof (struct IntUtilityBase)
+#define LC_LIBHEADERTYPEPTR	LIBBASETYPEPTR
+#define LC_LIB_FIELD(lib)       (((struct IntUtilityBase *)(lib))->UBase.ub_LibNode)
+#define LC_NO_OPENLIB
+#define LC_NO_CLOSELIB
+
+#include <libcore/libheader.c>
 
 extern ULONG AROS_SLIB_ENTRY(SMult32_020,Utility)();
 extern ULONG AROS_SLIB_ENTRY(UMult32_020,Utility)();
@@ -35,48 +33,12 @@ extern ULONG AROS_SLIB_ENTRY(UMult64_020,Utility)();
 extern ULONG AROS_SLIB_ENTRY(SDivMod32_020,Utility)();
 extern ULONG AROS_SLIB_ENTRY(UDivMod32_020,Utility)();
 
-int Utility_entry(void)
-{
-    /* If the library was executed by accident return error code. */
-    return -1;
-}
-
-const struct Resident Utility_resident=
-{
-    RTC_MATCHWORD,
-    (struct Resident *)&Utility_resident,
-    (APTR)&END,
-    RTF_AUTOINIT|RTF_COLDSTART,
-    LIBVERSION,
-    NT_LIBRARY,
-    103,
-    (STRPTR)name,
-    (STRPTR)&version[6],
-    (ULONG *)inittabl
-};
-
-const UBYTE name[]=LIBNAME;
-
-const UBYTE version[]=VERSION;
-
-const APTR inittabl[4]=
-{
-    (APTR)sizeof(struct IntUtilityBase),
-    (APTR)FUNCTABLE,
-    NULL,
-    &INIT
-};
-
-#ifdef SysBase
-#undef SysBase
-#endif
-
 /* iaint:
     Sigh, I require this to compile this with DICE, I will
     remove it at a later date...or at least change it :)
 */
 
-#ifdef __GNUC__
+#ifndef _DCC
 #define SysBase GetIntUtilityBase(LIBBASE)->ub_SysBase
 #else
 struct ExecBase *SysBase = 0L;
@@ -84,28 +46,11 @@ struct ExecBase *SysBase = 0L;
 
 #define SetFunc(a,b) SetFunction((struct Library *)LIBBASETYPE, a * -LIB_VECTSIZE, AROS_SLIB_ENTRY(b,Utility))
 
-AROS_LH2(struct LIBBASETYPE *, init,
-    AROS_LHA(struct LIBBASETYPE *, LIBBASE, D0),
-    AROS_LHA(BPTR,               segList,   A0),
-    struct ExecBase *, sysBase, 0, BASENAME)
+ULONG SAVEDS STDARGS L_InitLib (LIBBASETYPEPTR LIBBASE)
 {
-    AROS_LIBFUNC_INIT
-
-    /* Store arguments */
-    GetIntUtilityBase(LIBBASE)->ub_SysBase=sysBase;
-    GetIntUtilityBase(LIBBASE)->ub_SegList=segList;
 #ifdef _DCC
-    SysBase = sysBase;
+    SysBase = GetIntUtilityBase(LIBBASE)->ub_SysBase;
 #endif
-
-    /* Set up UtilityBase */
-    LIBBASE->ub_LibNode.lib_Node.ln_Pri = 0;
-    LIBBASE->ub_LibNode.lib_Node.ln_Type = NT_LIBRARY;
-    (const)UtilityBase->ub_LibNode.lib_Node.ln_Name = name;
-    LIBBASE->ub_LibNode.lib_Flags = LIBF_SUMUSED | LIBF_CHANGED;
-    LIBBASE->ub_LibNode.lib_Version = LIBVERSION;
-    LIBBASE->ub_LibNode.lib_Revision = LIBREVISION;
-    (const)LIBBASE->ub_LibNode.lib_IdString = &version[6];
 
     GetIntUtilityBase(LIBBASE)->ub_LastID = 0;
 
@@ -143,83 +88,6 @@ AROS_LH2(struct LIBBASETYPE *, init,
 #endif
 
     /* You would return NULL if the init failed */
-    return LIBBASE;
-    AROS_LIBFUNC_EXIT
+    return TRUE;
 }
 
-AROS_LH1(struct LIBBASETYPE *, open,
- AROS_LHA(ULONG, version, D0),
-	   struct LIBBASETYPE *, LIBBASE, 1, BASENAME)
-{
-    AROS_LIBFUNC_INIT
-
-    /* Keep compiler happy */
-    version=0;
-
-    /* I have one more opener. */
-    LIBBASE->ub_LibNode.lib_OpenCnt++;
-    LIBBASE->ub_LibNode.lib_Flags&=~LIBF_DELEXP;
-
-    /* You would return NULL if the open failed. */
-    return LIBBASE;
-    AROS_LIBFUNC_EXIT
-}
-
-AROS_LH0(BPTR, close,
-	   struct LIBBASETYPE *, LIBBASE, 2, Utility)
-{
-    AROS_LIBFUNC_INIT
-
-    /* I have one fewer opener. */
-    if(!--LIBBASE->ub_LibNode.lib_OpenCnt)
-    {
-#ifdef DISK_BASED
-	/* Delayed expunge pending? */
-	if(LIBBASE->ub_LibNode.lib_Flags&LIBF_DELEXP)
-	    /* Then expunge the library */
-	    return expunge();
-#endif
-    }
-    return 0;
-    AROS_LIBFUNC_EXIT
-}
-
-AROS_LH0(BPTR, expunge,
-	   struct LIBBASETYPE *, LIBBASE, 3, Utility)
-{
-    AROS_LIBFUNC_INIT
-
-    BPTR ret;
-
-    /* Test for openers. */
-    if(LIBBASE->ub_LibNode.lib_OpenCnt)
-    {
-	/* Set the delayed expunge flag and return. */
-	LIBBASE->ub_LibNode.lib_Flags|=LIBF_DELEXP;
-	return 0;
-    }
-#ifdef DISK_BASED
-    /* Get rid of the library. Remove it from the list. */
-    Remove(&LIBBASE->ub_LibNode.lib_Node);
-
-    /* Get returncode here - FreeMem() will destroy the field. */
-    ret=GetIntUtilityBase(LIBBASE)->ub_SegList;
-
-    /* Free the memory. */
-    FreeMem((char *)LIBBASE-LIBBASE->ub_LibNode.lib_NegSize,
-	    LIBBASE->ub_LibNode.lib_NegSize+LIBBASE->ub_LibNode.lib_PosSize);
-#else
-    ret = 0;
-#endif
-
-    return ret;
-    AROS_LIBFUNC_EXIT
-}
-
-AROS_LH0I(int, null,
-	    struct LIBBASETYPE *, LIBBASE, 4, Utility)
-{
-    AROS_LIBFUNC_INIT
-    return 0;
-    AROS_LIBFUNC_EXIT
-}
