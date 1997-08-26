07dcd99693a0a86a2ae318fff0801c318eb42e5e
iaint <iaint@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-08-26 22:49:55 +0000

    (Finally) Put the AROS_SLIB_ENTRY() change that prevented me from compiling utility.library
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2731 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/arossupport/include/freebsd.h
+++ compiler/arossupport/include/freebsd.h
@@ -4,22 +4,6 @@
 /*
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
-    $Log$
-    Revision 1.1  1997/03/27 01:13:52  ldp
-    libaros.a -> libarossupport.a
-
-    Revision 1.1  1997/01/09 18:21:16  digulla
-    Accidentially deleted :-( Shouldn't work when it's getting late
-
-    Revision 1.3  1996/11/25 02:33:54  aros
-    Changed for object files in a.out format to work again.
-
-    Revision 1.2  1996/11/16 12:05:27  aros
-    Overdue... changed __AROS_SLIB_ENTRY to AROS_SLIB_ENTRY.
-
-    Revision 1.1  1996/10/14 02:04:45  iaint
-    Added aros/freebsd.h for FreeBSD.
-
 
     Desc: FreeBSD specific things
     Lang: english
@@ -29,7 +13,7 @@
 #define __AROS_STRUCTURE_ALIGNMENT  8
 
 /* To handle the slightly different procedure naming */
-#define AROS_SLIB_ENTRY(n,s)          s##_##n
+#define AROS_SLIB_ENTRY(n,s)          s ## _ ## n
 #define AROS_ASMSYMNAME(s)	      s
 
 
