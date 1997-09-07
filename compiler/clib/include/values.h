9ea2168155f580a68642f452868401cb1580f15d
srittau <srittau@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-09-07 15:10:54 +0000

    First (incomplete) version of file used by C++ compilers. Included to resolve conflicts with <dos/dos.h>
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2770 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/include/values.h
@@ -0,0 +1,22 @@
+#ifndef _VALUES_H
+#define _VALUES_H
+/*
+    (C) 1997 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: Sizes of c(++) types
+    Lang: english
+*/
+
+/* How many bits/bytes has a type? */
+#define BITSPERBYTE 8
+#define BYTESPERLONG 4
+#define BITSPERLONG (BITSPERBYTE * BYTESPERLONG)
+#define BYTESPERQUAD 8
+#define BITSPERQUAD (BITSPERBYTE * BYTESPERQUAD)
+
+/* Minimum and maximum value of a signed type */
+#define MININT (1 << (((sizeof(int) * BITSPERBYTE) - 1))
+#define MAXINT (~MININT)
+
+#endif /* _VALUES_H */
