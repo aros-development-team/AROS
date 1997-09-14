293d6b78da921cc14325218bdf3e9612c6c73348
iaint <iaint@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-09-14 09:24:19 +0000

    A few more amiga.lib files.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2848 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/alib/opencatalog.c
@@ -0,0 +1,74 @@
+/*
+    Copyright (C) 1995-97 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: Varargs version of OpenCatalog()
+    Lang: english
+*/
+#define AROS_TAGRETURNTYPE  struct Catalog *
+#include <utility/tagitem.h>
+
+extern struct LocaleBase *LocaleBase;
+
+/*****************************************************************************
+
+    NAME */
+#include <libraries/locale.h>
+#include <proto/locale.h>
+
+	struct Catalog * OpenCatalog (
+
+/*  SYNOPSIS */
+	struct Locale * locale,
+	STRPTR          name,
+	Tag             tag1,
+	...             )
+
+/*  FUNCTION
+	This is the varargs version of the locale.library OpenCatalogA().
+	For information see locale.library/OpenCatalog()
+
+    INPUTS
+	locale      -   The locale describing the language the users
+			wants.
+	name        -   Name of the catalog file.
+	tag1        -   TagList of extra arguments.
+
+    RESULT
+	Either a pointer to a Catalog, or NULL.
+
+	Although the function may have returned NULL, that does not
+	necessarily meant there is an error. If dos/IoErr() returns
+	0, then there was no error, but the language of the built in
+	strings is the same as that of a catalog.
+
+	If IoErr() != 0, then there was an error however.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	locale/OpenCatalogA(), locale/CloseCatalog(),
+	locale/GetCatalogStr()
+
+    INTERNALS
+
+    HISTORY
+	15-02-1997  iaint   Wrote.
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+    AROS_LIBBASE_EXT_DECL(struct LocaleBase *,LocaleBase)
+
+    AROS_SLOWSTACKTAGS_PRE(tag1)
+
+    retval = OpenCatalogA(locale, name, AROS_SLOWSTACKTAGS_ARG(tag1));
+
+    AROS_SLOWSTACKTAGS_POST
+
+    AROS_LIBFUNC_EXIT
+} /* OpenCatalog */
---
+++ compiler/alib/timedelay.c
@@ -0,0 +1,90 @@
+/*
+    (C) 1995-97 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: TimeDelay() - wait a specified time.
+    Lang: english
+*/
+
+#include <exec/types.h>
+#include <aros/asmcall.h>
+#include <devices/timer.h>
+#include <exec/tasks.h>
+#include <proto/exec.h>
+
+/*****************************************************************************
+
+    NAME */
+	AROS_UFH3(ULONG, TimeDelay,
+
+/*  SYNOPSIS */
+	AROS_UFHA(LONG, Unit, D0),
+	AROS_UFHA(ULONG, Seconds, D1),
+	AROS_UFHA(ULONG, MicroSeconds, D2))
+
+/*  FUNCTION
+	TimeDelay() waits for the specified period of time before returning
+	to the caller.
+
+    INPUTS
+	Unit         -  The timer.device unit to use for this command.
+	Seconds      -  The number of seconds to wait.
+	MicroSeconds -  The number of microseconds to wait.
+
+    RESULT
+	Zero if everything went ok, non-zero if there was a problem.
+
+    NOTES
+	If this function fails, the most likely reasons are:
+	- invalid timer.device unit numbers
+
+	This function uses the SIGF_SINGLE signal, strange things can
+	happen if you are waiting on this signal when you call this
+	function. Basically: Don't use it and call this function.
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	timer/TR_ADDREQUEST
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+{
+    struct timerequest tr;
+    struct MsgPort mp;
+    UBYTE error = 0;
+
+    /* Create a message port */
+    mp.mp_Node.ln_Type = NT_MSGPORT;
+    mp.mp_Node.ln_Pri = 0;
+    mp.mp_Node.ln_Name = NULL;
+    mp.mp_Flags = PA_SIGNAL;
+    mp.mp_SigTask = FindTask(NULL);
+    mp.mp_SigBit = SIGB_SINGLE;
+
+    tr.tr_node.io_Message.mn_Node.ln_Type = NT_MESSAGE;
+    tr.tr_node.io_Message.mn_Node.ln_Pri = 0;
+    tr.tr_node.io_Message.mn_Node.ln_Name = NULL;
+    tr.tr_node.io_Message.mn_ReplyPort = &mp;
+    tr.tr_node.io_Message.mn_Length = sizeof(struct timerequest);
+
+    if(OpenDevice("timer.device", Unit, (struct IORequest *)&tr, 0) == 0)
+    {
+	tr.tr_node.io_Command = TR_ADDREQUEST;
+	tr.tr_node.io_Flags = 0;
+	tr.tr_time.tv_secs = Seconds;
+	tr.tr_time.tv_micro = MicroSeconds;
+	DoIO((struct IORequest *)&tr);
+
+	CloseDevice((struct IORequest *)&tr);
+	error = 1;
+    }
+
+    return error;
+
+} /* TimeDelay() */
