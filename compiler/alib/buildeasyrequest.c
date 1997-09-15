8e58f1ba6dbe7056c4bb04325d6b24748c354336
srittau <srittau@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-09-15 21:35:52 +0000

    Added stub BuildEasyRequest()
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2859 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/alib/buildeasyrequest.c
@@ -0,0 +1,58 @@
+/*
+    (C) 1995-96 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: Varargs version of BuildEasyRequestArgs() (intuition.library)
+    Lang: english
+*/
+#include <stdarg.h>
+
+/*****************************************************************************
+
+    NAME */
+#define NO_INLINE_STDARG /* turn off inline def */
+#include <proto/intuition.h>
+#include <intuition/intuition.h>
+
+	struct Window * BuildEasyRequest (
+
+/*  SYNOPSIS */
+	struct Window	  * RefWindow,
+	struct EasyStruct * easyStruct,
+	ULONG		    IDCMP,
+	...)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+    AROS_LIBBASE_EXT_DECL(struct IntuitionBase *,IntuitionBase)
+    va_list args;
+    struct Window * rc;
+
+    va_start (args, IDCMP);
+
+    rc = BuildEasyRequestArgs (RefWindow, easyStruct, IDCMP, (APTR)args);
+
+    va_end (args);
+
+    return rc;
+    AROS_LIBFUNC_EXIT
+} /* BuildEasyRequest */
