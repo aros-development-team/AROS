b12ae147f27369646d24a1bd75b82b729ca46fc1
turrican <turrican@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-09-22 14:46:04 +0000

    Implemented.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2929 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/findcliproc.c
+++ rom/dos/findcliproc.c
@@ -2,9 +2,14 @@
     (C) 1995-96 AROS - The Amiga Replacement OS
     $Id$
 
-    Desc:
+    Desc: Dos function FindCliProc
     Lang: english
 */
+#define AROS_ALMOST_COMPATIBLE
+#include <exec/lists.h>
+#include <exec/execbase.h>
+#include <proto/exec.h>
+#include <dos/dosextens.h>
 #include "dos_intern.h"
 
 /*****************************************************************************
@@ -44,9 +49,29 @@
 {
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct DosLibrary *,DOSBase)
-    extern void aros_print_not_implemented (char *);
+    struct Task *task = NULL;
 
-    aros_print_not_implemented ("FindCliProc");
+    /* Okay, simple one first: This task */
+    task = FindTask(NULL);
+    if (task->tc_Node.ln_Type == NT_PROCESS)
+	if (((struct Process *)task)->pr_TaskNum == (LONG)num)
+	    return (struct Process *)task;
+
+    /* The ready list */
+    ForeachNode(&SysBase->TaskReady, task)
+    {
+	if (task->tc_Node.ln_Type == NT_PROCESS)
+	    if (((struct Process *)task)->pr_TaskNum == (LONG)num)
+		return (struct Process *)task;
+    }
+
+    /* The waiting list */
+    ForeachNode(&SysBase->TaskWait, task)
+    {
+	if (task->tc_Node.ln_Type == NT_PROCESS)
+	    if (((struct Process *)task)->pr_TaskNum == (LONG)num)
+		return (struct Process *)task;
+    }
 
     return NULL;
     AROS_LIBFUNC_EXIT
