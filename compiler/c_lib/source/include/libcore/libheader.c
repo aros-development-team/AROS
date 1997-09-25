4a01c99cdbd907fd6d687ccb9eb36542ccd8da23
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-09-25 23:25:27 +0000

    Replaced the LH-prototype by  AROS_SLIB_ENTRY
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2964 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/c_lib/source/include/libcore/libheader.c
+++ compiler/c_lib/source/include/libcore/libheader.c
@@ -81,13 +81,15 @@ extern const char ALIGNED LC_BUILDNAME(LibName)   [];
 extern const char ALIGNED LC_BUILDNAME(LibID)     [];
 extern const char ALIGNED LC_BUILDNAME(Copyright) [];
 
-extern AROS_LH2 (LC_LIBHEADERTYPEPTR, LC_BUILDNAME(InitLib),
-    AROS_LHA(LC_LIBHEADERTYPEPTR, lh,      D0),
-    AROS_LHA(BPTR,                segList, A0),
-    struct ExecBase *, sysBase, 0, LibHeader
+LC_LIBHEADERTYPEPTR AROS_SLIB_ENTRY(LC_BUILDNAME(InitLib),LibHeader)
+(
+	LC_LIBHEADERTYPEPTR   lh,
+	BPTR                  segList,
+	struct ExecBase     * sysBase
 );
-extern AROS_LH0 (BPTR, LC_BUILDNAME(ExpungeLib),
-    LC_LIBHEADERTYPEPTR, lh, 3, LibHeader
+BPTR AROS_SLIB_ENTRY(LC_BUILDNAME(ExpungeLib),LibHeader)
+(
+	LC_LIBHEADERTYPEPTR lh
 );
 
 /* -------------------------------------------------------------------------
