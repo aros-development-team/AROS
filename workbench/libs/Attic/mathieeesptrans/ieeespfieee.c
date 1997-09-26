d2f7df40aacf7d5d6a9a9076ff098fa19cc208f2
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-09-26 23:43:15 +0000

    Some new functions
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2968 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/Attic/mathieeesptrans/ieeespfieee.c
@@ -0,0 +1,58 @@
+/*
+    (C) 1995-97 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc:
+    Lang:
+*/
+
+#include <libraries/mathieeesp.h>
+#include <aros/libcall.h>
+#include <proto/mathieeesingbas.h>
+#include <proto/mathieeesingtrans.h>
+#include <proto/exec.h>
+#include <exec/types.h>
+#include "mathieeesingtrans_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+
+      AROS_LH1(LONG, IEEESPFieee,
+
+/*  SYNOPSIS */
+      AROS_LHA(LONG, y, D0),
+
+/*  LOCATION */
+      struct Library *, MathIeeeSingTransBase, 18, Mathieeesingtrans)
+
+
+/*  FUNCTION
+        Convert IEEE single to IEEE single
+        It just returns the input parameter.
+
+    INPUTS
+        y  - IEEE single precision floating point number
+
+    RESULT
+        IEEE single precision floting point number
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+{
+AROS_LIBFUNC_INIT
+  return y;
+AROS_LIBFUNC_EXIT
+} /* IEEESPFieee */
+
