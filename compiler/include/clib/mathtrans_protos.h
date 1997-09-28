735b558c449a2b3574fa6349396711080b68880e
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-09-28 11:33:46 +0000

    MathTrans instead of mathtrans
    git-svn-id: https://svn.aros.org/svn/aros/trunk@2976 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/clib/mathtrans_protos.h
+++ compiler/include/clib/mathtrans_protos.h
@@ -21,73 +21,73 @@
 */
 AROS_LP1(LONG, SPAcos,
     AROS_LPA(LONG, fnum1, D0),
-    struct Library *, MathtransBase, 20, Mathtrans)
+    struct Library *, MathTransBase, 20, MathTrans)
 
 AROS_LP1(LONG, SPAsin,
     AROS_LPA(LONG, fnum1, D0),
-    struct Library *, MathtransBase, 19, Mathtrans)
+    struct Library *, MathTransBase, 19, MathTrans)
 
 AROS_LP1(LONG, SPAtan,
     AROS_LPA(LONG , fnum1 , D0),
-    struct Library *, MathtransBase, 5, Mathtrans)
+    struct Library *, MathTransBase, 5, MathTrans)
 
 AROS_LP1(LONG, SPCos,
     AROS_LPA(LONG, fnum1, D0),
-    struct Library *, MathtransBase, 7, Mathtrans)
+    struct Library *, MathTransBase, 7, MathTrans)
 
 AROS_LP1(LONG, SPCosh,
     AROS_LPA(LONG, fnum1, D0),
-    struct Library *, MathtransBase, 11, Mathtrans)
+    struct Library *, MathTransBase, 11, MathTrans)
 
 AROS_LP1(LONG, SPExp,
     AROS_LPA(LONG, fnum1, D0),
-    struct Library *, MathtransBase, 13, Mathtrans)
+    struct Library *, MathTransBase, 13, MathTrans)
 
 AROS_LP1(LONG, SPFieee,
     AROS_LPA(LONG, ieeenum, D0),
-    struct Library *, MathtransBase, 18, Mathtrans)
+    struct Library *, MathTransBase, 18, MathTrans)
 
 AROS_LP1(LONG, SPLog,
     AROS_LPA(LONG, fnum1, D0),
-    struct Library *, MathtransBase, 14, Mathtrans)
+    struct Library *, MathTransBase, 14, MathTrans)
 
 AROS_LP1(LONG, SPLog10,
     AROS_LPA(LONG, fnum1, D0),
-    struct Library *, MathtransBase, 21, Mathtrans)
+    struct Library *, MathTransBase, 21, MathTrans)
 
 AROS_LP2(LONG, SPPow,
     AROS_LPA(LONG, fnum1, D1),
     AROS_LPA(LONG, fnum2, D0),
-    struct Library *, MathtransBase, 15, Mathtrans)
+    struct Library *, MathTransBase, 15, MathTrans)
 
 AROS_LP1(LONG, SPSin,
     AROS_LPA(LONG, fnum1, D0),
-    struct Library *, MathtransBase, 6, Mathtrans)
+    struct Library *, MathTransBase, 6, MathTrans)
 
 AROS_LP2(LONG, SPSincos,
     AROS_LPA(IPTR*, pfnum2, D1),
     AROS_LPA(LONG , fnum1 , D0),
-    struct Library *, MathtransBase, 9, Mathtrans)
+    struct Library *, MathTransBase, 9, MathTrans)
 
 AROS_LP1(LONG, SPSinh,
     AROS_LPA(LONG, fnum1 , D0),
-    struct Library *, MathtransBase, 10, Mathtrans)
+    struct Library *, MathTransBase, 10, MathTrans)
 
 AROS_LP1(LONG, SPSqrt,
     AROS_LPA(LONG, fnum1, D0),
-    struct Library *, MathtransBase, 16, Mathtrans)
+    struct Library *, MathTransBase, 16, MathTrans)
 
 AROS_LP1(LONG, SPTan,
     AROS_LPA(LONG, fnum1, D0),
-    struct Library *, MathtransBase, 8, Mathtrans)
+    struct Library *, MathTransBase, 8, MathTrans)
 
 AROS_LP1(LONG, SPTanh,
     AROS_LPA(LONG, fnum1, D0),
-    struct Library *, MathtransBase, 12, Mathtrans)
+    struct Library *, MathTransBase, 12, MathTrans)
 
 AROS_LP1(LONG, SPTieee,
     AROS_LPA(LONG, fnum, D0),
-    struct Library *, MathtransBase, 17, Mathtrans)
+    struct Library *, MathTransBase, 17, MathTrans)
 
 
 #endif /* CLIB_MATHTRANS_PROTOS_H */
