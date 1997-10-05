30993440086429f1b290b51b71a3f56811aa39bd
iaint <iaint@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-10-05 07:52:44 +0000

    Added these for later work
    git-svn-id: https://svn.aros.org/svn/aros/trunk@3072 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/defines/battclock.h
+++ compiler/include/defines/battclock.h
@@ -11,7 +11,18 @@
 /*
     Defines
 */
+#define ReadBattClock() \
+    AROS_LC0(ULONG, ReadBattClock, \
+    APTR, BattClockBase, 2, Battclock)
 
+#define ResetBattClock() \
+    AROS_LC0(void, ResetBattClock, \
+    APTR, BattClockBase, 1, Battclock)
+
+#define WriteBattClock(time) \
+    AROS_LC1(void, WriteBattClock, \
+    AROS_LCA(ULONG, time, D0), \
+    APTR *, BattClockBase, 3, Battclock)
 
 
 #endif /* DEFINES_BATTCLOCK_H */
--- compiler/include/defines/timer.h
+++ compiler/include/defines/timer.h
@@ -11,7 +11,33 @@
 /*
     Defines
 */
+#define AddTime(dest, src) \
+    AROS_LC2(void, AddTime, \
+    AROS_LCA(struct timeval *, dest, A0), \
+    AROS_LCA(struct timeval *, src, A1), \
+    struct Device *, TimerBase, 7, Timer)
 
+#define CmpTime(dest, src) \
+    AROS_LC2(LONG, CmpTime, \
+    AROS_LCA(struct timeval *, dest, A0), \
+    AROS_LCA(struct timeval *, src, A1), \
+    struct Device *, TimerBase, 9, Timer)
+
+#define GetSysTime(dest) \
+    AROS_LC1(void, GetSysTime, \
+    AROS_LCA(struct timeval *, dest, A0), \
+    struct TimerBase *, TimerBase, 11, Timer)
+
+#define ReadEClock(dest) \
+    AROS_LC1(ULONG, ReadEClock, \
+    AROS_LCA(struct EClockVal *, dest, A0), \
+    struct TimerBase *, TimerBase, 10, Timer)
+
+#define SubTime(dest, src) \
+    AROS_LC2(void, SubTime, \
+    AROS_LCA(struct timeval *, dest, A0), \
+    AROS_LCA(struct timeval *, src, A1), \
+    struct Device *, TimerBase, 8, Timer)
 
 
 #endif /* DEFINES_TIMER_H */
