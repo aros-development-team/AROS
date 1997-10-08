df08a39e73bf77abebbe1525306b21f88a3b5a5d
srittau <srittau@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-10-08 21:27:40 +0000

    Removed accidently added file (please don't ask...)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@3130 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/classes/gadgets/aroscheckbox/aroscheckbox_endtag.c
+++
@@ -1,8 +0,0 @@
-/*
-        (C) 1995-96 AROS - The Amiga Replacement OS
-        *** Automatic generated file. Do not edit ***
-        Desc: Resident endskip for AROSCheckbox
-        Lang: english
-*/
-
-const char AROSCheckbox_end = 0;
