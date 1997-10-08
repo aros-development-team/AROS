42ff7b89e70638f4bac6b7e0e44b246a1ab08906
srittau <srittau@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-10-08 00:43:19 +0000

    Small improvements
    Removed Log
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@3109 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/duplock.c
+++ rom/dos/duplock.c
@@ -1,29 +1,8 @@
 /*
-    (C) 1995-96 AROS - The Amiga Replacement OS
+    (C) 1995-97 AROS - The Amiga Replacement OS
     $Id$
-    $Log$
-    Revision 1.7  1997/01/27 00:36:17  ldp
-    Polish
 
-    Revision 1.6  1996/12/09 13:53:24  aros
-    Added empty templates for all missing functions
-
-    Moved #include's into first column
-
-    Revision 1.5  1996/10/24 15:50:26  aros
-    Use the official AROS macros over the __AROS versions.
-
-    Revision 1.4  1996/08/13 13:52:54  digulla
-    Replaced <dos/dosextens.h> by "dos_intern.h" or added "dos_intern.h"
-    Replaced AROS_LA by AROS_LHA
-
-    Revision 1.3  1996/08/12 14:20:37  digulla
-    Added aliases
-
-    Revision 1.2  1996/08/01 17:40:49  digulla
-    Added standard header for all files
-
-    Desc:
+    Desc: dos.library function DupLock()
     Lang: english
 */
 #include <proto/exec.h>
@@ -50,7 +29,7 @@
 	lock - Old lock.
 
     RESULT
-	The new lock or 0 in case of an error. IoErr() will give additional
+	The new lock or NULL in case of an error. IoErr() will give additional
 	information in that case.
 
     NOTES
@@ -115,13 +94,11 @@
     AROS_LIBBASE_EXT_DECL(struct DosLibrary *,DOSBase)
 
     BPTR old, new;
-    struct Process *me=(struct Process *)FindTask(NULL);
 
     /* Use Lock() to clone the handle. cd to it first. */
-    old=me->pr_CurrentDir;
-    me->pr_CurrentDir=lock;
+    old = CurrentDir(lock);
     new=Lock("",SHARED_LOCK);
-    me->pr_CurrentDir=old;
+    CurrentDir(old);
     return new;
     AROS_LIBFUNC_EXIT
 } /* DupLock */
