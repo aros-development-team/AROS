4e367c0fb3d1a82d1b60eabcd2746f492698bfe1
srittau <srittau@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-10-08 00:39:42 +0000

    Include file for the upcoming class
    git-svn-id: https://svn.aros.org/svn/aros/trunk@3107 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/gadgets/aroscheckbox.h
@@ -0,0 +1,26 @@
+#ifndef GADGETS_AROSCHECKBOX_H
+#define GADGETS_AROSCHECKBOX_H
+
+/*
+    (C) 1997 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: MethodIDs and AttrIDs for the AROS checkbox class.
+    Lang: english
+*/
+
+#ifndef EXEC_TYPES_H
+#   include <exec/types.h>
+#endif
+#ifndef UTILITY_TAGITEM_H
+#   include <utility/tagitem.h>
+#endif
+#ifndef LIBRARIES_GADTOOLS_H
+#   include <libraries/gadtools.h>
+#endif
+
+#define AROSCHECKBOXCLASS "checkbox.aros"
+
+#define AROSCB_Checked GTCB_Checked
+
+#endif /* GADGETS_AROSCHECKBOX_H */
