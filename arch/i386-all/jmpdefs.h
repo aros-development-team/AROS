80dd10e824166ebb1d947457501a1719440a9274
turrican <turrican@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-10-10 14:18:06 +0000

    Moved sigcore.h from config/unix/i386, added jmpdefs.h
    git-svn-id: https://svn.aros.org/svn/aros/trunk@3151 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/i386-all/jmpdefs.h
@@ -0,0 +1,21 @@
+/* $Id$ */
+#ifndef JMPDEFS_H
+#define JMPDEFS_H
+
+#include <setjmp.h>
+
+#ifdef __linux__
+#   define SP(env)	((APTR)(env[0].__sp))
+#   define FP(env)	((APTR)(env[0].__bp))
+#   define PC(env)	((APTR)(env[0].__pc))
+#endif
+#ifdef __FreeBSD__
+#   define SP(env)	((APTR)(env[0]._jb[2]))
+#   define FP(env)	((APTR)(env[0]._jb[3]))
+#   define PC(env)	((APTR)(env[0]._jb[0]))
+#endif
+
+/* The number of stack longs that have to be copied from the old stack */
+#define NUM_LONGS	4
+
+#endif /* !JMPDEFS_H */
