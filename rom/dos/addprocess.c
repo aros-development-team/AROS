45753d8c8333f8fba14263cc2a742e0ed1ea807a
turrican <turrican@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-10-14 11:45:12 +0000

    Fix for register args versions of AROS
    git-svn-id: https://svn.aros.org/svn/aros/trunk@3191 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/addprocess.c
+++ rom/dos/addprocess.c
@@ -7,7 +7,21 @@
 */
 #include <dos/dosextens.h>
 #include <proto/exec.h>
+#include <aros/asmcall.h>
 #include "dos_intern.h"
+
+LONG DosEntry (
+    STRPTR argPtr,
+    ULONG argSize,
+    APTR initialPC,
+    struct ExecBase * sysBase)
+{
+    return AROS_UFC3 (LONG, initialPC,
+	AROS_UFCA (STRPTR,            argPtr,  A0),
+	AROS_UFCA (ULONG,             argSize, D0),
+	AROS_UFCA (struct ExecBase *, sysBase, A6)
+	);
+}
 
 struct Process * AddProcess (
     struct Process    * process,
@@ -20,13 +34,14 @@ struct Process * AddProcess (
     APTR * sp = process->pr_Task.tc_SPUpper;
 
     *--sp = SysBase;
+    *--sp = initialPC;
     *--sp = (APTR)argSize;
     *--sp = argPtr;
 
-    process->pr_ReturnAddr = sp-3;
+    process->pr_ReturnAddr = sp-4;
 
     process->pr_Task.tc_SPReg  = (STRPTR)sp-SP_OFFSET;
     process->pr_Task.tc_Flags |= TF_ETASK;
 
-    return (struct Process *)AddTask (&process->pr_Task, initialPC, finalPC);
+    return (struct Process *)AddTask (&process->pr_Task, (APTR)DosEntry, finalPC);
 } /* AddProcess */
