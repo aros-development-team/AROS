4bc904c33060e9915c18517d13c2bd69ee5b28f8
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-10-28 17:10:13 +0000

    Include all necessary files.
    Added typedef HIDD.
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@3347 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/hidd/include/hidd.h
+++ rom/hidd/include/hidd.h
@@ -10,13 +10,16 @@
 */
 
 #ifndef EXEC_TYPES_H
-#include <exec/types.h>
+#   include <exec/types.h>
 #endif
 #ifndef UTILITY_TAGITEM_H
-#include <utility/tagitem.h>
+#   include <utility/tagitem.h>
+#endif
+#ifndef INTUITION_CLASSES_H
+#   include <intuition/classes.h>
 #endif
 #ifndef INTUITION_CLASSUSR_H
-#include <intuition/classusr.h>
+#   include <intuition/classusr.h>
 #endif
 
 /* ---------------------------------------------------------------------
@@ -25,6 +28,11 @@
 
 /* The name of the topmost HIDD class */
 #define HIDDCLASS		"hiddclass"
+
+#ifndef __typedef_HIDD
+#   define __typedef_HIDD
+    typedef APTR HIDD;
+#endif
 
 /*
     Tags for the root HIDD class "hiddclass".
@@ -39,7 +47,7 @@
 #define HIDDA_HardwareName	(HIDDA_Base + 5)    /* --G-- STRPTR */
 #define HIDDA_Active		(HIDDA_Base + 6)    /* ISGNU BOOL */
 #define HIDDA_Status		(HIDDA_Base + 7)    /* --GNU ULONG */
-#define HIDDA_ErrorCode		(HIDDA_Base + 8)    /* --G-- ULONG */
+#define HIDDA_ErrorCode 	(HIDDA_Base + 8)    /* --G-- ULONG */
 #define HIDDA_Locking		(HIDDA_Base + 9)    /* --G-- ULONG */
 
 /* Values for the HIDD_Type Tag */
@@ -54,19 +62,19 @@
 #define HIDDV_StatusUnknown	-1
 
 /* Methods for all HIDD's */
-#define HIDDM_Root_Base		0x80000
-#define HIDDM_Class_Get		0x80001	/* LONG M (struct opGet *) 	*/
-#define HIDDM_Class_MGet	0x80002 /* LONG M (struct op??? *) 	*/ 
-#define HIDDM_BeginIO		0x80003	/* LONG M ( hmIO *) 		*/
-#define HIDDM_AbortIO		0x80004	/* LONG M ( hmIO *)		*/
-#define HIDDM_LoadConfigPlugin	0x80005	/* HIDDT_Config M ( hmPlugin *) */
-#define HIDDM_Lock		0x80006	/* IPTR M ( hmLock *)		*/
-#define HIDDM_Unlock		0x80007 /* void M ( hmLock *)		*/
-#define HIDDM_AddHIDD		0x80008 /* BOOL M ( hmAdd *)		*/
-#define HIDDM_RemoveHIDD	0x80009 /* void M ( hmAdd *)		*/
+#define HIDDM_Root_Base 	0x80000
+#define HIDDM_Class_Get 	0x80001 /* LONG M (struct opGet *)      */
+#define HIDDM_Class_MGet	0x80002 /* LONG M (struct op??? *)      */
+#define HIDDM_BeginIO		0x80003 /* LONG M ( hmIO *)             */
+#define HIDDM_AbortIO		0x80004 /* LONG M ( hmIO *)             */
+#define HIDDM_LoadConfigPlugin	0x80005 /* HIDDT_Config M ( hmPlugin *) */
+#define HIDDM_Lock		0x80006 /* IPTR M ( hmLock *)           */
+#define HIDDM_Unlock		0x80007 /* void M ( hmLock *)           */
+#define HIDDM_AddHIDD		0x80008 /* BOOL M ( hmAdd *)            */
+#define HIDDM_RemoveHIDD	0x80009 /* void M ( hmAdd *)            */
 
 /* Used for HIDDM_BeginIO, HIDDM_AbortIO */
-typedef struct hmIO 
+typedef struct hmIO
 {
     STACKULONG		MethodID;
     struct IORequest   *hmi_ioRequest;
@@ -77,7 +85,7 @@ typedef struct hmPlugin
 {
     STACKULONG		MethodID;
     STACKIPTR		hmp_PluginData;
-} hmPlugin; 
+} hmPlugin;
 
 /* Combined structure for HIDDM_Lock, HIDDM_Unlock */
 typedef struct hmLock
