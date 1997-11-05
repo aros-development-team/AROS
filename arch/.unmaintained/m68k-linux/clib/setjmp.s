858d1abba150b1f7df7c7c7f721375ecd8e7d5d5
turrican <turrican@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-11-05 17:22:57 +0000

    Sync with m68k-native version
    git-svn-id: https://svn.aros.org/svn/aros/trunk@3438 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/.unmaintained/m68k-linux/clib/setjmp.s
+++ arch/.unmaintained/m68k-linux/clib/setjmp.s
@@ -67,6 +67,7 @@
 	.globl	AROS_CDEFNAME(setjmp)
 	.type	AROS_CDEFNAME(setjmp),@function
 AROS_CDEFNAME(setjmp):
+#ifdef OLDJMP
 	move.l	env(%sp),%a0		/* save area pointer */
 	move.l	retaddr(%sp),(%a0)+	/* save old PC */
 	lea.l	env(%sp),%a1		/* adjust saved SP since we won't rts */
@@ -74,22 +75,29 @@ AROS_CDEFNAME(setjmp):
 	movem.l	%d2-%d7/%a2-%a6,(%a0)	/* save remaining non-scratch regs */
 	clr.l	%d0			/* return 0 */
 	rts
+#else
+	move.l	env(%sp),%a0			/* get address of jmp_buf */
+	move.l	(%sp),(%a0)+			/* store return address */
+	movem.l	%d2-%d7/%a2-%a6/%sp,(%a0)	/* store all registers except scratch */
+	moveq.l	#0,%d0				/* return 0 */
+	rts
+#endif
 
 /*
 	The jmp_buf is filled as follows (d0/d1/a0/a1 are not saved):
 
 	_jmp_buf	offset	contents
 	[0]   		0	old pc
-	[1]		4	old sp
-	[2]		8	d2
-	[3]		12	d3
-	[4]		16	d4
-	[5]		20	d5
-	[6]		24	d6
-	[7]		28	d7
-	[8]		32	a2
-	[9]		36	a3
-	[10]		40	a4
-	[11]		44	a5
-	[12]		48	a6
+	[1]		4	d2
+	[2]		8	d3
+	[3]		12	d4
+	[4]		16	d5
+	[5]		20	d6
+	[6]		24	d7
+	[7]		28	a2
+	[8]		32	a3
+	[9]		36	a4
+	[10]		40	a5
+	[11]		44	a6
+	[12]		48	old sp
 */
