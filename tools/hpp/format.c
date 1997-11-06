ea29afe5f74ae954e6ef70dc96b4bf4ffee1bc37
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-11-06 14:39:31 +0000

    Format C code for HTML output
    git-svn-id: https://svn.aros.org/svn/aros/trunk@3462 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- tools/hpp/format.c
+++ tools/hpp/format.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 #include <toollib/error.h>
+#include <toollib/vstring.h>
+#include "db.h"
+#include "var.h"
+
+enum modes
+{
+    m_space = 0,
+    m_prespace,
+    m_punct,
+    m_comment,
+    m_cpp,
+    m_identifier,
+    m_string,
+    m_header
+};
+
+static const char * ModeDelim[] =
+{
+    "",
+    "",
+    "cpunctuation",
+    "ccomment",
+    "cppkeyword",
+    "cidentifier",
+    "cstring",
+    "cheader",
+};
 
 int
 main (int argc, char ** argv)
@@ -10,6 +39,23 @@ main (int argc, char ** argv)
     char * outformat;
     char * infilename;
     int    t;
+    enum modes mode = m_space;
+    String ident;
+    char * data;
+
+    Var_Init ();
+    DB_Init ();
+    DB_Add ("c-html", "c-html.db");
+
+#define NEWMODE(nm)             \
+    if ((nm) != mode)           \
+    {				\
+	if (ModeDelim[mode][0]) \
+	    printf ("</%s>", ModeDelim[mode]);    \
+	mode = (nm);            \
+	if (ModeDelim[mode][0]) \
+	    printf ("<%s>", ModeDelim[mode]);    \
+    }
 
     type = outformat = infilename = NULL;
 
@@ -48,19 +94,229 @@ main (int argc, char ** argv)
     else
 	in = stdin;
 
+    if (strcasecmp (type, "c"))
+    {
+	Error ("Unknown type %s. Supported types: C", type);
+	ErrorExit (10);
+    }
+
+    if (strcasecmp (outformat, "html"))
+    {
+	Error ("Unknown output format %s. Supported formats: HTML", outformat);
+	ErrorExit (10);
+    }
+
+    ident = VS_New (NULL);
+
     while ((c = getc (in)) != EOF)
     {
 	switch (c)
 	{
-	case '&': fputs ("&amp;", stdout); break;
-	case '<': fputs ("&lt;",  stdout); break;
-	case '>': fputs ("&gt;",  stdout); break;
-	default: putchar (c);
-	}
-    }
+	case '&':
+	    NEWMODE(m_punct);
+	    fputs ("&amp;", stdout);
+	    break;
+	case '<':
+	    NEWMODE(m_punct);
+	    fputs ("&lt;",  stdout);
+	    break;
+	case '>':
+	    NEWMODE(m_punct);
+	    fputs ("&gt;",  stdout);
+	    break;
+	case '.': case '!': case '^': case '%':
+	case '(': case ')': case '=': case '?': case '{':
+	case '[': case ']': case '}': case '+': case '*':
+	case '~': case '-': case ':': case ',':
+	case ';': case '|':
+	    NEWMODE(m_punct);
+	    putchar (c);
+	    break;
+
+	case ' ':
+	    if (mode != m_prespace)
+	    {
+		NEWMODE(m_space);
+	    }
+	    else
+	    {
+		fputs ("&nbsp", stdout);
+	    }
+	    putchar (c);
+	    break;
+
+	case '/': /* Comment or / */
+	    c = getc (in);
+
+	    if (c == '*')
+	    {
+		NEWMODE(m_comment);
+		fputs ("/*",  stdout);
+
+		while ((c = getc (in)) != EOF)
+		{
+rem_again:
+		    if (c == '*')
+		    {
+			c = getc (in);
+
+			if (c == '/')
+			{
+			    fputs ("*/",  stdout);
+			    break;
+			}
+			else
+			{
+			    putchar ('/');
+			    goto rem_again;
+			}
+		    }
+		    else
+			putchar (c);
+		}
+	    }
+	    else
+	    {
+		NEWMODE(m_punct);
+		putchar ('/');
+		ungetc (c, in);
+	    }
+	    break;
+
+	case '\n':
+	    NEWMODE(m_space);
+	    fputs ("<BR>\n", stdout);
+	    NEWMODE(m_prespace);
+	    break;
+
+	case '"':
+	    NEWMODE(m_string);
+	    putchar (c);
+
+	    while ((c = getc (in)) != EOF)
+	    {
+		putchar (c);
+
+		if (c == '\\')
+		{
+		    c = getc (in);
+		    if (c == '\n')
+			fputs ("<BR>", stdout);
+		    putchar (c);
+		}
+		else if (c == '"')
+		{
+		    break;
+		}
+	    }
+
+	    break;
+
+	case '#':
+	    NEWMODE(m_cpp);
+
+	    VS_Clear (ident);
+	    VS_AppendChar (ident, c);
+
+	    while ((c = getc (in)) != EOF)
+	    {
+		if (!isalpha (c))
+		{
+		    ungetc (c, in);
+		    break;
+		}
+
+		VS_AppendChar (ident, c);
+	    }
+
+	    fputs (ident->buffer, stdout);
+
+	    if (!strcmp (ident->buffer, "#include"))
+	    {
+		putchar (' ');
+		VS_Clear (ident);
+
+		while ((c = getc (in)) != EOF)
+		{
+		    if (!isspace (c))
+			break;
+		}
+
+		VS_AppendChar (ident, c);
+
+		while ((c = getc (in)) != EOF)
+		{
+		    if (isspace (c))
+		    {
+			ungetc (c, in);
+			break;
+		    }
+
+		    VS_AppendChar (ident, c);
+		}
+
+		data = DB_FindData ("c-html", ident->buffer);
+
+		if (data)
+		{
+		    NEWMODE(m_space);
+		    fputs (data, stdout);
+		}
+		else
+		{
+		    NEWMODE(m_header);
+		    fputs (ident->buffer, stdout);
+		}
+	    }
+
+	    break;
+
+	default:
+	    if (isalpha (c) || c == '_')
+	    {
+		VS_Clear (ident);
+		VS_AppendChar (ident, c);
+
+		while ((c = getc (in)) != EOF)
+		{
+		    if (!isalnum (c) && c != '_')
+		    {
+			ungetc (c, in);
+			break;
+		    }
+
+		    VS_AppendChar (ident, c);
+		}
+
+		data = DB_FindData ("c-html", ident->buffer);
+
+		if (data)
+		{
+		    NEWMODE(m_space);
+		    fputs (data, stdout);
+		}
+		else
+		{
+		    NEWMODE(m_identifier);
+		    fputs (ident->buffer, stdout);
+		}
+	    }
+	    else
+		putchar (c);
+
+	    break;
+
+	} /* switch (c) */
+    } /* while !EOF */
+
+    if (ModeDelim[mode][0])
+	printf ("</%s>\n", ModeDelim[mode]);
 
     if (infilename)
 	fclose (in);
+
+    DB_Exit ();
+    Var_Exit ();
 
     ErrorExit (0);
 }
