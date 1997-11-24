7dfa5ce624a79277e1ec21f7813de280429b1a37
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-11-24 20:38:47 +0000

    Update, bugfixes, more 64bit defines
    git-svn-id: https://svn.aros.org/svn/aros/trunk@3646 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/mathieeedoubtrans/mathieee64bitdefines.h
+++ workbench/libs/mathieeedoubtrans/mathieee64bitdefines.h
@@ -69,7 +69,7 @@
 
 #if defined AROS_64BIT_TYPE || defined __GNUC__
 #define OR64QC(Dest64, Const64_Hi, Const64_Lo, Const64) \
-  Dest64 |= | Const64;
+  Dest64 |= Const64;
 #else
 #define OR64QC(Dest64, Const64_Hi, Const64_Lo, Const64) \
   Set_High32of64(Dest64, Get_High32of64(Dest64) | Const64_Hi); \
@@ -130,7 +130,7 @@
    Dest64 = ((QUAD)Src32) << (Shift);
 #else
 #define SHL32(Dest64, Src32, Shift) \
-	 if( Shift < 32) \
+         if( Shift < 32) \
    { \
      Set_High32of64(Dest64, (Src32 >> (32-(Shift)) ) ); \
      Set_Low32of64 (Dest64, (Src32 << (Shift)      ) ); \
@@ -162,7 +162,7 @@
      Set_High32of64(Dest64, (ULONG)Get_High32of64(Src64) >> (Shift)       ); \
    } \
    else \
-   	 {\
+         {\
        if ((Shift) < 64) \
        { \
          Set_Low32of64 (Dest64, (ULONG)Get_High32of64(Src64) >> ((Shift)-32) ); \
@@ -188,7 +188,7 @@
                      Get_High32of64(Src64) << (32-(Shift)) \
    } \
    else \
-   	 {\
+         {\
        if ((Shift) < 64) \
        { \
          Dest32 = (ULONG)Get_High32of64(Src64) >> ((Shift)-32) ; \
@@ -273,8 +273,8 @@
   (Var1_64 == Const64)
 #else
 #define is_eqC(Var64, Const64_Hi, Const64_Lo, Const64) \
-  (Get_Low32of64(Var64) == Const64_Lo  && \
-   Get_High32of64(Var64) == Const64_Hi )
+  (Get_Low32of64(Var64)  == Const64_Lo  && \
+   Get_High32of64(Var64) == Const64_Hi )     
 #endif
 
 #if defined AROS_64BIT_TYPE || defined __GNUC__
@@ -284,6 +284,15 @@
 #define is_neq(Var1_64, Var2_64) \
   (Get_Low32of64 (Var1_64) != Get_Low32of64 (Var2_64) || \
    Get_High32of64(Var1_64) != Get_High32of64(Var2_64)    )
+#endif
+
+#if defined AROS_64BIT_TYPE || defined __GNUC__
+#define is_neqC(Var1_64, Const64_Hi, Const64_Lo, Const64) \
+  (Var1_64 != Const64)
+#else
+#define is_neqC(Var64, Const64_Hi, Const64_Lo, Const64) \
+  (Get_Low32of64(Var64)  != Const64_Lo  || \
+   Get_High32of64(Var64) != Const64_Hi )     
 #endif
 
 #if defined AROS_64BIT_TYPE || defined __GNUC__
@@ -378,6 +387,17 @@
      (ULONG)Get_Low32of64(Var1_64)  <= (ULONG)Const64_Lo)  )
 #endif
 
+#if defined AROS_64BIT_TYPE || defined __GNUC__
+#define is_leqSC(Var1_64, Const64_Hi, Const64_Lo, Const64 ) \
+  ((QUAD)Var1_64 <= (QUAD)Const64)
+#else
+#define is_leqSC(Var1_64, Const64_Hi, Const64_Lo, Const64 ) \
+  /* first comparison has to be a "<" !!! */ \
+  ( (( LONG)Get_High32of64(Var1_64) <  ( LONG)Const64_Hi) || \
+    (( LONG)Get_High32of64(Var1_64) == ( LONG)Const64_Hi  &&  \
+     (ULONG)Get_Low32of64(Var1_64)  <= (ULONG)Const64_Lo)  )
+#endif
+
 /* arithmetic */
 
 #if defined AROS_64BIT_TYPE || defined __GNUC__
@@ -436,6 +456,34 @@
      }      
 #endif
 
+#if defined AROS_64BIT_TYPE || defined __GNUC__
+#define ADD64QC(Dest64, Const64_Hi, Const64_Lo, Const64) \
+   Dest64 += Const64;
+#else
+#define ADD64QC(Dest64, Const64_Hi, Const64_Lo, Const64) \
+   if ((LONG) Get_Low32of64(Dest64) < 0  && \
+       (LONG) Const64_Lo  < 0  )  \
+   { \
+     Set_Low32of64 (Dest64, Get_Low32of64 (Dest64) + Const64_Lo ); \
+     Set_High32of64(Dest64, Get_High32of64(Dest64) + Const64_Hi + 1 ); \
+   } \
+   else \
+     if ((LONG) Get_Low32of64(Dest64) >= 0  && \
+         (LONG) Const64_Lo            >= 0  )  \
+     { \
+       Set_Low32of64 (Dest64, Get_Low32of64 (Dest64) + Const64_Lo ); \
+       Set_High32of64(Dest64, Get_High32of64(Dest64) + Const64_Hi ); \
+     } \
+     else \
+     { \
+       Set_Low32of64 (Dest64, Get_Low32of64 (Dest64) + Const64_Lo ); \
+       if ((LONG)Get_Low32of64(Dest64) >= 0 ) \
+         Set_High32of64(Dest64, Get_High32of64(Dest64) + Const64_Hi + 1 ); \
+       else \      
+         Set_High32of64(Dest64, Get_High32of64(Dest64) + Const64_Hi ); \
+     }      
+#endif
+
 
 #if defined AROS_64BIT_TYPE || defined __GNUC__
 #define SUB64(Dest64, Var1_64, Var2_64) \
@@ -445,15 +493,19 @@
    printf("using SUB! still incomplete define!!\n"); 
 #endif
 
-/* NEG might still produce a wrong result for certain arguments */
 
 #if defined AROS_64BIT_TYPE || defined __GNUC__
 #define NEG64(Dest64) \
    Dest64 = -Dest64;
 #else
 #define NEG64(Dest64) \
-  Set_High32of64( Dest64, Get_High32of64(Dest64) ^ 0xFFFFFFFF ); \
-  Set_Low32of64 ( Dest64, -Get_High32of64(Dest64)); 
+  if (0 == Get_Low32of64(Dest64)) \
+    Set_High32of64( Dest64, -Get_High32of64(Dest64) ); \
+  else \
+  { \
+    Set_High32of64( Dest64,  Get_High32of64(Dest64) ^ 0xFFFFFFFF ); \
+    Set_Low32of64 ( Dest64, -Get_Low32of64(Dest64)); \
+  }
 #endif
 
 
