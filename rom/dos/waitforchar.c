2f59a66f8e74ada60cd1fb5337326cb3616f898f
srittau <srittau@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-12-05 20:20:31 +0000

    New function
    git-svn-id: https://svn.aros.org/svn/aros/trunk@3703 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/waitforchar.c
+++ rom/dos/waitforchar.c
@@ -1,16 +1,18 @@
 /*
-    (C) 1995-96 AROS - The Amiga Replacement OS
+    (C) 1995-97 AROS - The Amiga Replacement OS
     $Id$
 
-    Desc:
+    Desc: Waits for a character to arrive at a filehandle.
     Lang: english
 */
+#include <proto/exec.h>
 #include "dos_intern.h"
 
 /*****************************************************************************
 
     NAME */
 #include <proto/dos.h>
+#include <exec/types.h>
 
 	AROS_LH2(LONG, WaitForChar,
 
@@ -45,10 +47,36 @@
 {
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct DosLibrary *,DOSBase)
-    extern void aros_print_not_implemented (char *);
 
-    aros_print_not_implemented ("WaitForChar");
+    /* Get pointer to process structure */
+    struct Process *me=(struct Process *)FindTask(NULL);
+    struct FileHandle *fh=(struct FileHandle *)BADDR(file);
 
-    return EOF;
+    /* Get pointer to I/O request. Use stackspace for now. */
+    struct IOFileSys io,*iofs=&io;
+
+    /* Null-pointers are okay. */
+    if (!file)
+        return 0;
+
+    /* Prepare I/O request. */
+    iofs->IOFS.io_Message.mn_Node.ln_Type=NT_REPLYMSG;
+    iofs->IOFS.io_Message.mn_ReplyPort   =&me->pr_MsgPort;
+    iofs->IOFS.io_Message.mn_Length      =sizeof(struct IOFileSys);
+    iofs->IOFS.io_Device =fh->fh_Device;
+    iofs->IOFS.io_Unit   =fh->fh_Unit;
+    iofs->IOFS.io_Command=FSA_WAIT_CHAR;
+    iofs->IOFS.io_Flags  =0;
+    iofs->io_Union.io_WAIT_CHAR.io_Timeout=timeout;
+
+    /* Send the request. */
+    DoIO(&iofs->IOFS);
+
+    /* Set error code */
+    if(!iofs->io_DosError)
+        return iofs->io_Union.io_WAIT_CHAR.io_Success;
+    else
+        SetIoErr(iofs->io_DosError);
+    return 0;
     AROS_LIBFUNC_EXIT
 } /* WaitForChar */
