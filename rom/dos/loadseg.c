c94bef3882877803ecfa5d2722c6ea624589054d
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-12-11 14:32:56 +0000

    LoadSeg() built on top of InternalLoadSeg().
    git-svn-id: https://svn.aros.org/svn/aros/trunk@3728 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/loadseg.c
+++ rom/dos/loadseg.c
@@ -4,109 +4,95 @@
 
     Desc: DOS function LoadSeg()
     Lang: english
+
+    Revision 1.14
+      LoadSeg now simply calls InternalLoadSeg() with the
+      array of functions
 */
 #include <dos/dos.h>
-#include <proto/dos.h>
 #include <dos/dosextens.h>
+#include <proto/dos.h>
 #define DEBUG 1
 #include <aros/debug.h>
 #include "dos_intern.h"
 
-BPTR LoadSeg_AOS(BPTR file);
-BPTR LoadSeg_ELF(BPTR file);
-BPTR LoadSeg_AOUT(BPTR file);
+extern Dos_Read();
+extern Exec_AllocMem();
+extern Exec_FreeMem();
+
 
 /*****************************************************************************
 
     NAME */
 #include <proto/dos.h>
 
-	AROS_LH1(BPTR, LoadSeg,
+        AROS_LH1(BPTR, LoadSeg,
 
 /*  SYNOPSIS */
-	AROS_LHA(STRPTR, name, D1),
+        AROS_LHA(STRPTR, name, D1),
 
 /*  LOCATION */
-	struct DosLibrary *, DOSBase, 25, Dos)
+        struct DosLibrary *, DOSBase, 25, Dos)
 
 /*  FUNCTION
-	Loads an executable file into memory. Each hunk of the loadfile
-	is loaded into his own memory section and a handle on all of them
-	is returned. The segments can be freed with UnLoadSeg().
+        Loads an executable file into memory. Each hunk of the loadfile
+        is loaded into his own memory section and a handle on all of them
+        is returned. The segments can be freed with UnLoadSeg().
 
     INPUTS
-	name - NUL terminated name of the file.
+        name - NUL terminated name of the file.
 
     RESULT
-	Handle to the loaded executable or 0 if the load failed.
-	IoErr() gives additional information in that case.
+        Handle to the loaded executable or 0 if the load failed.
+        IoErr() gives additional information in that case.
 
     NOTES
+        This function is built on top of InternalLoadSeg()
 
     EXAMPLE
 
     BUGS
 
     SEE ALSO
-	UnLoadSeg()
+        UnLoadSeg()
 
     INTERNALS
 
     HISTORY
-	29-10-95    digulla automatically created from
-			    dos_lib.fd and clib/dos_protos.h
+        29-10-95    digulla automatically created from
+                            dos_lib.fd and clib/dos_protos.h
 
 *****************************************************************************/
 {
-    AROS_LIBFUNC_INIT
-    AROS_LIBBASE_EXT_DECL(struct DosLibrary *,DOSBase)
+  AROS_LIBFUNC_INIT
+  AROS_LIBBASE_EXT_DECL(struct DosLibrary *,DOSBase)
 
-    BPTR file, segs=0;
+  void (* FunctionArray[])() = { (void *)&Dos_Read,
+                                 (void *)&Exec_AllocMem,
+                                 (void *)&Exec_FreeMem  };
 
-    /* Open the file */
-    file = Open (name,MODE_OLDFILE);
+  BPTR file, segs=0;
 
-    if (file)
-    {
+  /* Open the file */
+  file = Open (name,MODE_OLDFILE);
+
+  if (file)
+  {
 D(bug("Loading \"%s\"...\n", name));
-	/* Then try to load the different file formats */
-	if (!segs)
-	{
-	    segs = LoadSeg_AOS (file);
-#if DEBUG > 1
-	    if (segs)
-		bug("Loaded as AmigaOS exe\n");
-#endif
-	}
-	if (!segs)
-	{
-	    segs = LoadSeg_ELF (file);
-#if DEBUG > 1
-	    if (segs)
-		bug("Loaded as ELF exe\n");
-#endif
-	}
-	if (!segs)
-	{
-	    segs = LoadSeg_AOUT (file);
-#if DEBUG > 1
-	    if (segs)
-		bug("Loaded as a.out exe\n");
-#endif
-	}
 
-	/* Clean up */
-	Close (file);
-    }
+    segs = InternalLoadSeg (file, NULL, (void *)FunctionArray, NULL);
 
     if (segs)
-	SetIoErr (0);
+      SetIoErr (0);
 #if DEBUG > 1
     else
-	bug ("Loading failed\n");
+      bug ("Loading failed\n");
 #endif
+    Close(file);
+  }
 
-    /* And return */
-    return segs;
-    AROS_LIBFUNC_EXIT
+  /* And return */
+  return segs;
+
+  AROS_LIBFUNC_EXIT
 } /* LoadSeg */
