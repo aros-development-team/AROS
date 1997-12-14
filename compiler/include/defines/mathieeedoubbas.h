882b6b2f62ee708574160042f1574238ab859b69
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-12-14 17:33:11 +0000

    new defines
    git-svn-id: https://svn.aros.org/svn/aros/trunk@3752 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/defines/mathieeedoubbas.h
+++ compiler/include/defines/mathieeedoubbas.h
@@ -11,7 +11,70 @@
 /*
     Defines
 */
+#define IEEEDPAbs(y) \
+    AROS_LCQUAD1(QUAD, IEEEDPAbs, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    struct MathIeeeDoubBasBase *, MathIeeeDoubBasBase, 9, Mathieeedoubbas)
 
+#define IEEEDPAdd(y, z) \
+    AROS_LCQUAD2(QUAD, IEEEDPAdd, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    AROS_LCAQUAD(QUAD, z, D2, D3), \
+    struct MathIeeeDoubBasBase *, MathIeeeDoubBasBase, 11, Mathieeedoubbas)
+
+#define IEEEDPCeil(y) \
+    AROS_LCQUAD1(QUAD, IEEEDPCeil, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    struct MathIeeeDoubBasBase *, MathIeeeDoubBasBase, 16, Mathieeedoubbas)
+
+#define IEEEDPCmp(y, z) \
+    AROS_LCQUAD2(QUAD, IEEEDPCmp, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    AROS_LCAQUAD(QUAD, z, D2, D3), \
+    struct MathIeeeDoubBasBase *, MathIeeeDoubBasBase, 7, Mathieeedoubbas)
+
+#define IEEEDPDiv(y, z) \
+    AROS_LCQUAD2(QUAD, IEEEDPDiv, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    AROS_LCAQUAD(QUAD, z, D2, D3), \
+    struct MathIeeeDoubBasBase *, MathIeeeDoubBasBase, 14, Mathieeedoubbas)
+
+#define IEEEDPFix(y) \
+    AROS_LCQUAD1(QUAD, IEEEDPFix, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    struct MathIeeeDoubBasBase *, MathIeeeDoubBasBase, 5, Mathieeedoubbas)
+
+#define IEEEDPFloor(y) \
+    AROS_LCQUAD1(QUAD, IEEEDPFloor, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    struct MathIeeeDoubBasBase *, MathIeeeDoubBasBase, 15, Mathieeedoubbas)
+
+#define IEEEDPFlt(y) \
+    AROS_LCQUAD1(QUAD, IEEEDPFlt, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    struct MathIeeeDoubBasBase *, MathIeeeDoubBasBase, 6, Mathieeedoubbas)
+
+#define IEEEDPMul(y, z) \
+    AROS_LCQUAD2(QUAD, IEEEDPMul, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    AROS_LCAQUAD(QUAD, z, D2, D3), \
+    struct MathIeeeDoubBasBase *, MathIeeeDoubBasBase, 13, Mathieeedoubbas)
+
+#define IEEEDPNeg(y) \
+    AROS_LCQUAD1(QUAD, IEEEDPNeg, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    struct MathIeeeDoubBasBase *, MathIeeeDoubBasBase, 10, Mathieeedoubbas)
+
+#define IEEEDPSub(y, z) \
+    AROS_LCQUAD2(QUAD, IEEEDPSub, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    AROS_LCAQUAD(QUAD, z, D2, D3), \
+    struct MathIeeeDoubBasBase *, MathIeeeDoubBasBase, 12, Mathieeedoubbas)
+
+#define IEEEDPTst(y) \
+    AROS_LCQUAD1(QUAD, IEEEDPTst, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    struct MathIeeeDoubBasBase *, MathIeeeDoubBasBase, 8, Mathieeedoubbas)
 
 
 #endif /* DEFINES_MATHIEEEDOUBBAS_H */
--- compiler/include/defines/mathieeedoubtrans.h
+++ compiler/include/defines/mathieeedoubtrans.h
@@ -12,6 +12,81 @@
     Defines
 */
 
+#define IEEEDPAcos(y) \
+    AROS_LCQUAD1(QUAD, IEEEDPAcos, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    struct MathIeeeDoubTransBase *, MathIeeeDoubTransBase, 20, Mathieeedoubtrans)
+
+#define IEEEDPAsin(y) \
+    AROS_LCQUAD1(QUAD, IEEEDPAsin, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    struct MathIeeeDoubTransBase *, MathIeeeDoubTransBase, 19, Mathieeedoubtrans)
+
+#define IEEEDPCos(y) \
+    AROS_LCQUAD1(QUAD, IEEEDPCos, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    struct MathIeeeDoubTransBase *, MathIeeeDoubTransBase, 7, Mathieeedoubtrans)
+
+#define IEEEDPCosh(y) \
+    AROS_LCQUAD1(QUAD, IEEEDPCosh, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    struct MathIeeeDoubTransBase *, MathIeeeDoubTransBase, 11, Mathieeedoubtrans)
+
+#define IEEEDPExp(y) \
+    AROS_LCQUAD1(QUAD, IEEEDPExp, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    struct MathIeeeDoubTransBase *, MathIeeeDoubTransBase, 13, Mathieeedoubtrans)
+
+#define IEEEDPFieee(y) \
+    AROS_LCQUAD1(QUAD, IEEEDPFieee, \
+    AROS_LCAQUAD(LONG, y, D0, D1), \
+    struct MathIeeeDoubTransBase *, MathIeeeDoubTransBase, 18, Mathieeedoubtrans)
+
+#define IEEEDPLog(y) \
+    AROS_LCQUAD1(QUAD, IEEEDPLog, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    struct MathIeeeDoubTransBase *, MathIeeeDoubTransBase, 14, Mathieeedoubtrans)
+
+#define IEEEDPLog10(y) \
+    AROS_LCQUAD1(QUAD, IEEEDPLog10, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    struct MathIeeeDoubTransBase *, MathIeeeDoubTransBase, 21, Mathieeedoubtrans)
+
+#define IEEEDPPow(x, y) \
+    AROS_LC2(QUAD, IEEEDPPow, \
+    AROS_LCAQUAD(QUAD, x, D2, D3), \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    struct MathIeeeDoubTransBase *, MathIeeeDoubTransBase, 15, Mathieeedoubtrans)
+
+#define IEEEDPSin(y) \
+    AROS_LCQUAD1(QUAD, IEEEDPSin, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    struct MathIeeeDoubTransBase *, MathIeeeDoubTransBase, 6, Mathieeedoubtrans)
+
+#define IEEEDPSinh(y) \
+    AROS_LCQUAD1(QUAD, IEEEDPSinh, \
+    AROS_LCAQUAD(QUAD, y , D0, D1), \
+    struct MathIeeeDoubTransBase *, MathIeeeDoubTransBase, 10, Mathieeedoubtrans)
+
+#define IEEEDPSqrt(y) \
+    AROS_LCQUAD1(QUAD, IEEEDPSqrt, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    struct MathIeeeDoubTransBase *, MathIeeeDoubTransBase, 16, Mathieeedoubtrans)
+
+#define IEEEDPTan(y) \
+    AROS_LCQUAD1(QUAD, IEEEDPTan, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    struct MathIeeeDoubTransBase *, MathIeeeDoubTransBase, 8, Mathieeedoubtrans)
+
+#define IEEEDPTanh(y) \
+    AROS_LCQUAD1(QUAD, IEEEDPTanh, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    struct MathIeeeDoubTransBase *, MathIeeeDoubTransBase, 12, Mathieeedoubtrans)
+
+#define IEEEDPTieee(y) \
+    AROS_LCQUAD1(LONG, IEEEDPTieee, \
+    AROS_LCAQUAD(QUAD, y, D0, D1), \
+    struct MathIeeeDoubTransBase *, MathIeeeDoubTransBase, 17, Mathieeedoubtrans)
 
 
 #endif /* DEFINES_MATHIEEEDOUBTRANS_H */
