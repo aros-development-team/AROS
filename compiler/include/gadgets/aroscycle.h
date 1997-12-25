2bb62f72368d98c69bdaa84edb39fea5900d0012
srittau <srittau@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1997-12-25 20:01:30 +0000

    First version
    git-svn-id: https://svn.aros.org/svn/aros/trunk@3798 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/gadgets/aroscycle.h
@@ -0,0 +1,34 @@
+#ifndef GADGETS_AROSCYCLE_H
+#define GADGETS_AROSCYCLE_H
+
+/*
+    (C) 1997 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: MethodIDs and AttrIDs for the AROS cycle class.
+    Lang: english
+*/
+#ifndef EXEC_TYPES_H
+#   include <exec/types.h>
+#endif
+#ifndef UTILITY_TAGITEM_H
+#   include <utility/tagitem.h>
+#endif
+#ifndef LIBRARIES_GADTOOLS_H
+#   include <libraries/gadtools.h>
+#endif
+
+/* The AROSCycleClass ist a subclass of GadgetClass. */
+
+/* Use that #define instead of a string. */
+#define AROSCYCLECLASS "cycle.aros"
+
+
+/* Tags to be passed to AROSCYCLECLASS. */
+#define AROSCYCLE_Dummy 40000L
+  /* [ISG] (STRPTR *) Null-terminated list of labels for gadget. */
+#define AROSCYCLE_Labels GTCY_Labels
+  /* [ISG] (UWORD) Active label (starting with 0). */
+#define AROSCYCLE_Active GTCY_Active
+
+#endif /* GADGETS_AROSCYCLE_H */
