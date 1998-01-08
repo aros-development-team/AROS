e079ccfe9ca7edf9a3b631a8ee7abbd031b609d5
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-01-08 21:33:26 +0000

    new functions
    git-svn-id: https://svn.aros.org/svn/aros/trunk@3856 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/graphics/initview.c
@@ -0,0 +1,68 @@
+/*
+    (C) 1995-97 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: Graphics function InitView()
+    Lang: english
+*/
+#include "graphics_intern.h"
+#include <graphics/view.h>
+#include <proto/exec.h>
+
+static const struct View defaultView =
+{
+  NULL, /* ViewPort */
+  NULL, /* LOFCprList */
+  NULL, /* SHFCprList */
+  0,    /* DyOffset */
+  0,    /* DxOffset */
+  0     /* Modes */
+};
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/graphics.h>
+
+	AROS_LH1(void, InitView,
+
+/*  SYNOPSIS */
+	AROS_LHA(struct View *, view, A1),
+
+/*  LOCATION */
+	struct GfxBase *, GfxBase, 60, Graphics)
+
+/*  FUNCTION
+	Initializes a View structure.
+
+    INPUTS
+	view - The View to initialize.
+
+    RESULT
+        View is initialized to it`s default values - doesn't care about
+        previous contents of this structure.
+        All values except for DxOffset,DyOffset are set to 0's.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+    AROS_LIBBASE_EXT_DECL(struct GfxBase *,GfxBase)
+
+    CopyMem ((UBYTE *)&defaultView, view, sizeof (struct View));
+
+    driver_InitView(view, GfxBase);
+
+    AROS_LIBFUNC_EXIT
+} /* InitView */
---
+++ rom/graphics/initvport.c
@@ -0,0 +1,76 @@
+/*
+    (C) 1995-97 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: Graphics function InitVPort()
+    Lang: english
+*/
+#include "graphics_intern.h"
+#include <graphics/view.h>
+#include <proto/exec.h>
+
+static const struct ViewPort defaultViewPort =
+{
+  NULL, /* Next */
+  NULL, /* ColorMap */
+  NULL, /* DspIns */
+  NULL, /* SprIns */
+  NULL, /* ClrIns */
+  NULL, /* UCopIns */
+  0,    /* DWidth */
+  0,    /* DHeight */
+  0,    /* DxOffset */
+  0,    /* DyOffset */
+  0,    /* Modes */
+  0x24, /* SpritePriorities */
+  0,    /* ExtendedModes */
+  NULL  /* RasInfo */
+};
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/graphics.h>
+
+	AROS_LH1(void, InitVPort,
+
+/*  SYNOPSIS */
+	AROS_LHA(struct ViewPort *, vp, A0),
+
+/*  LOCATION */
+	struct GfxBase *, GfxBase, 34, Graphics)
+
+/*  FUNCTION
+	Initializes a ViewPort structure.
+
+    INPUTS
+	view - The View to initialize.
+
+    RESULT
+        ViewPort is initialized to it`s default values - doesn't care about
+        previous contents of this structure.
+        All values except for SpritePriorities are set to 0's.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+    AROS_LIBBASE_EXT_DECL(struct GfxBase *,GfxBase)
+
+    CopyMem ((UBYTE *)&defaultViewPort, vp, sizeof (struct ViewPort));
+
+    driver_InitVPort(vp, GfxBase);
+
+    AROS_LIBFUNC_EXIT
+} /* InitView */
