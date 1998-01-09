7e9e43563e9d1550553d1cc6cfcbc556d14fcdcd
hkiel <hkiel@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-01-09 21:05:19 +0000

    Now contains only the struct for internal driver information.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@3863 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-x11/graphics_internal.h
+++ arch/all-x11/graphics_internal.h
@@ -2,6 +2,11 @@
     (C) 1995-98 AROS - The Amiga Replacement OS
     $Id$
 
+    $Log$
+    Revision 1.2  1998/01/09 21:05:19  hkiel
+    Now contains only the struct for internal driver information.
+
+
     Desc: X11 specific Internal Information Management
     Lang: english
 */
@@ -18,110 +23,4 @@ struct gfx_driverdata {
     UWORD	dd_LinePtrn;		/* Amiga current LinePtrn	*/
     char	* dd_DashList;		/* X11 converted DashList	*/
     struct RastPort	* dd_RastPort;	/* This RastPort 		*/
-}
-
-
-/* Functions to access struct gfx_driverdata for a given rp		*/
-
-/* Read values */
-Window GetXWindow (struct RastPort * rp)
-{
-    return (Window) rp->longreserved[0]->dd_Window;
-}
-
-GC GetGC (struct RastPort * rp)
-{
-    return (GC) rp->longreserved[0]->dd_GC;
-}
-
-UWORD * GetAreaPtrn (struct RastPort * rp)
-{
-    return (UWORD *) rp->longreserved[0]->dd_AreaPtrn;
-}
-
-BYTE GetAreaPtSz (struct RastPort * rp)
-{
-    return rp->longreserved[0]->dd_AreaPtSz;
-}
-
-Pixmap GetPixmap (struct RastPort * rp)
-{
-    return rp->longreserved[0]->dd_Pixmap;
-}
-
-UWORD GetLinePtrn (struct RastPort * rp)
-{
-    return rp->longreserved[0]->dd_LinePtrn;
-}
-
-char * GetDashList (struct RastPort * rp)
-{
-    return rp->longreserved[0]->dd_DashList;
-}
-
-struct RastPort * GetRP (struct RastPort * rp)
-{
-    return (struct RastPort *) rp->longreserved[0]->dd_RastPort;
-}
-
-/* Set values */
-void SetXWindow (struct RastPort * rp, int win)
-{
-    if (rp->BitMap)
-    {
-	int width, height, depth, dummy;
-	Window dummywin;
-
-LX11
-	XGetGeometry (sysDisplay, win, &dummywin, &dummy, &dummy
-	    , &width, &height
-	    , &dummy
-	    , &depth
-	);
-UX11
-
-	rp->BitMap->BytesPerRow = ((width+15) >> 4)*2;
-	rp->BitMap->Rows = height;
-	rp->BitMap->Depth = depth;
-	rp->BitMap->Flags = 0;
-	rp->BitMap->Pad = BMT_XWINDOW;
-	rp->BitMap->Planes[0] = (PLANEPTR)win;
-    }
-
-    rp->longreserved[0]->dd_Window = win;
-}
-
-void SetGC (struct RastPort * rp, GC gc)
-{
-    rp->longreserved[0]->dd_GC = gc;
-}
-
-void SetAreaPtrn (struct RastPort * rp, UWORD *AreaPtrn)
-{
-    rp->longreserved[0]->dd_AreaPtrn = AreaPtrn;
-}
-
-void SetAreaPtSz (struct RastPort * rp, BYTE AreaPtSz)
-{
-    rp->longreserved[0]->dd_AreaPtSz = AreaPtSz;
-}
-
-void SetPixmap (struct RastPort * rp, Pixmap AreaPixmap)
-{
-    rp->longreserved[0]->dd_Pixmap = AreaPixmap;
-}
-
-void SetLinePtrn (struct RastPort * rp, UWORD LinePtrn)
-{
-    rp->longreserved[0]->dd_LinePtrn = LinePtrn;
-}
-
-void SetDashList (struct RastPort * rp, char * DashList)
-{
-    rp->longreserved[0]->dd_DashList = DashList;
-}
-
-void SetRP (struct RastPort * rp, struct RastPort * rp_new)
-{
-    rp->longreserved[0]->dd_RastPort = rp_new;
-}
+};
