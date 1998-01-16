7dc207d466d20a548343aa32d2e569180f5b0366
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-01-16 22:26:54 +0000

    New function. More to follow ;-))
    git-svn-id: https://svn.aros.org/svn/aros/trunk@3896 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/graphics/scalerdiv.c
@@ -0,0 +1,71 @@
+/*
+    (C) 1995-98 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: Graphics function ScalerDiv()
+    Lang: english
+*/
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/graphics.h>
+
+	AROS_LH3(void, ScalerDiv,
+
+/*  SYNOPSIS */
+	AROS_LHA(UWORD, factor, D0),
+	AROS_LHA(UWORD, numerator, D1),
+	AROS_LHA(UWORD, denominator, D2),
+
+/*  LOCATION */
+	struct GfxBase *, GfxBase, 114, Graphics)
+
+/*  FUNCTION
+	Use this to precalculate the width/height of the destination
+	bitmap. As factor give the width/height of the original bitmap
+	that is to be scaled via ScaleBitMap(), as numerator give
+	the value you will write into bsa_XSrcFactor/bsa_YSrcFactor
+	and as denominator the value of bsa_XDestFactor/bsa_YDestFactor.
+
+    INPUTS
+	factor      - a number in the range of 0..16383
+	numerator   - a number in the range of 1..16383
+	denominator - a number in the range of 1..16383
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	BitMapScale()
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+  AROS_LIBFUNC_INIT
+  AROS_LIBBASE_EXT_DECL(struct GfxBase *,GfxBase)
+
+  ULONG res;
+  if (0 == factor)
+    return 0;
+  else
+    res = (ULONG)((ULONG)factor * (ULONG)numerator) / denominator;
+
+  if (0 == res)
+    return 1;
+
+  if (((factor * numerator) % denominator) >= ((denominator + 1) >> 1))
+    res++;
+
+  return res;
+
+  AROS_LIBFUNC_EXIT
+} /* ScalerDiv */
