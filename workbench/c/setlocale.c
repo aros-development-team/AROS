aee626c1899f0f448512f8f1c511de42504fc250
iaint <iaint@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-01-29 05:00:13 +0000

    Useful for testing languages, but you need my locale.library first
    git-svn-id: https://svn.aros.org/svn/aros/trunk@3949 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/c/setlocale.c
@@ -0,0 +1,38 @@
+#include <exec/types.h>
+#include <dos/dos.h>
+#include <libraries/locale.h>
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <proto/locale.h>
+
+struct LocaleBase *LocaleBase = NULL;
+
+int main(int argc, char **av)
+{
+    struct Locale *new;
+
+    if(argc != 2)
+    {
+	PrintFault(ERROR_REQUIRED_ARG_MISSING, "SetLocale");
+	return 20;
+    }
+
+    LocaleBase = (struct LocaleBase *)OpenLibrary("locale.library", 38);
+    if(LocaleBase)
+    {
+	new = OpenLocale("piglatin.prefs" /* av[1] */);
+	FPuts(Output(), "Locale opened\n");
+	if(new)
+	{
+	    PrefsUpdate(new);
+	    FPuts(Output(), "Locale set\n");
+	    CloseLocale(new);
+	}
+	else
+	{
+	    PrintFault(IoErr(), "SetLocale");
+	}
+	CloseLibrary((struct Library *)LocaleBase);
+    }
+    return 0;
+}
