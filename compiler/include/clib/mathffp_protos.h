477eb13a9bee9fafe3dda66c7ce30b789ead5399
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-02-01 21:49:50 +0000

    Update for use with floats now.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@3960 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/clib/mathffp_protos.h
+++ compiler/include/clib/mathffp_protos.h
@@ -19,57 +19,57 @@
 /*
     Prototypes
 */
-AROS_LP1(LONG, SPAbs,
-    AROS_LPA(LONG, fnum1, D0),
+AROS_LP1(float, SPAbs,
+    AROS_LPA(float, fnum1, D0),
     struct MathBase *, MathBase, 9, Mathffp)
 
-AROS_LP2(LONG, SPAdd,
-    AROS_LPA(LONG, fnum1, D1),
-    AROS_LPA(LONG, fnum2, D0),
+AROS_LP2(float, SPAdd,
+    AROS_LPA(float, fnum1, D1),
+    AROS_LPA(float, fnum2, D0),
     struct MathBase *, MathBase, 11, Mathffp)
 
-AROS_LP1(LONG, SPCeil,
-    AROS_LPA(LONG, y, D0),
+AROS_LP1(float, SPCeil,
+    AROS_LPA(float, y, D0),
     struct MathBase *, MathBase, 16, Mathffp)
 
 AROS_LP2(LONG, SPCmp,
-    AROS_LPA(LONG, fnum1, D0),
-    AROS_LPA(LONG, fnum2, D1),
+    AROS_LPA(float, fnum1, D0),
+    AROS_LPA(float, fnum2, D1),
     struct MathBase *, MathBase, 7, Mathffp)
 
-AROS_LP2(LONG, SPDiv,
-    AROS_LPA(LONG, fnum1, D1),
-    AROS_LPA(LONG, fnum2, D0),
+AROS_LP2(float, SPDiv,
+    AROS_LPA(float, fnum1, D1),
+    AROS_LPA(float, fnum2, D0),
     struct MathBase *, MathBase, 14, Mathffp)
 
 AROS_LP1(LONG, SPFix,
-    AROS_LPA(LONG, fnum, D0),
+    AROS_LPA(float, fnum, D0),
     struct MathBase *, MathBase, 5, Mathffp)
 
-AROS_LP1(LONG, SPFloor,
-    AROS_LPA(LONG, y, D0),
+AROS_LP1(float, SPFloor,
+    AROS_LPA(float, y, D0),
     struct MathBase *, MathBase, 15, Mathffp)
 
-AROS_LP1(LONG, SPFlt,
+AROS_LP1(float, SPFlt,
     AROS_LPA(LONG, inum, D0),
     struct MathBase *, MathBase, 6, Mathffp)
 
-AROS_LP2(LONG, SPMul,
-    AROS_LPA(LONG, fnum1, D1),
-    AROS_LPA(LONG, fnum2, D0),
+AROS_LP2(float, SPMul,
+    AROS_LPA(float, fnum1, D1),
+    AROS_LPA(float, fnum2, D0),
     struct MathBase *, MathBase, 13, Mathffp)
 
-AROS_LP1(LONG, SPNeg,
-    AROS_LPA(LONG, fnum1, D0),
+AROS_LP1(float, SPNeg,
+    AROS_LPA(float, fnum1, D0),
     struct MathBase *, MathBase, 10, Mathffp)
 
-AROS_LP2(LONG, SPSub,
-    AROS_LPA(LONG, fnum1, D0),
-    AROS_LPA(LONG, fnum2, D1),
+AROS_LP2(float, SPSub,
+    AROS_LPA(float, fnum1, D0),
+    AROS_LPA(float, fnum2, D1),
     struct MathBase *, MathBase, 12, Mathffp)
 
 AROS_LP1(LONG, SPTst,
-    AROS_LPA(LONG, fnum, D1),
+    AROS_LPA(float, fnum, D1),
     struct MathBase *, MathBase, 8, Mathffp)
 
 
--- compiler/include/clib/mathtrans_protos.h
+++ compiler/include/clib/mathtrans_protos.h
@@ -19,74 +19,74 @@
 /*
     Prototypes
 */
-AROS_LP1(LONG, SPAcos,
-    AROS_LPA(LONG, fnum1, D0),
+AROS_LP1(float, SPAcos,
+    AROS_LPA(float, fnum1, D0),
     struct Library *, MathTransBase, 20, MathTrans)
 
-AROS_LP1(LONG, SPAsin,
-    AROS_LPA(LONG, fnum1, D0),
+AROS_LP1(float, SPAsin,
+    AROS_LPA(float, fnum1, D0),
     struct Library *, MathTransBase, 19, MathTrans)
 
-AROS_LP1(LONG, SPAtan,
-    AROS_LPA(LONG , fnum1 , D0),
+AROS_LP1(float, SPAtan,
+    AROS_LPA(float , fnum1 , D0),
     struct Library *, MathTransBase, 5, MathTrans)
 
-AROS_LP1(LONG, SPCos,
-    AROS_LPA(LONG, fnum1, D0),
+AROS_LP1(float, SPCos,
+    AROS_LPA(float, fnum1, D0),
     struct Library *, MathTransBase, 7, MathTrans)
 
-AROS_LP1(LONG, SPCosh,
-    AROS_LPA(LONG, fnum1, D0),
+AROS_LP1(float, SPCosh,
+    AROS_LPA(float, fnum1, D0),
     struct Library *, MathTransBase, 11, MathTrans)
 
-AROS_LP1(LONG, SPExp,
-    AROS_LPA(LONG, fnum1, D0),
+AROS_LP1(float, SPExp,
+    AROS_LPA(float, fnum1, D0),
     struct Library *, MathTransBase, 13, MathTrans)
 
-AROS_LP1(LONG, SPFieee,
-    AROS_LPA(LONG, ieeenum, D0),
+AROS_LP1(float, SPFieee,
+    AROS_LPA(float, ieeenum, D0),
     struct Library *, MathTransBase, 18, MathTrans)
 
-AROS_LP1(LONG, SPLog,
-    AROS_LPA(LONG, fnum1, D0),
+AROS_LP1(float, SPLog,
+    AROS_LPA(float, fnum1, D0),
     struct Library *, MathTransBase, 14, MathTrans)
 
-AROS_LP1(LONG, SPLog10,
-    AROS_LPA(LONG, fnum1, D0),
+AROS_LP1(float, SPLog10,
+    AROS_LPA(float, fnum1, D0),
     struct Library *, MathTransBase, 21, MathTrans)
 
-AROS_LP2(LONG, SPPow,
-    AROS_LPA(LONG, fnum1, D1),
-    AROS_LPA(LONG, fnum2, D0),
+AROS_LP2(float, SPPow,
+    AROS_LPA(float, fnum1, D1),
+    AROS_LPA(float, fnum2, D0),
     struct Library *, MathTransBase, 15, MathTrans)
 
-AROS_LP1(LONG, SPSin,
-    AROS_LPA(LONG, fnum1, D0),
+AROS_LP1(float, SPSin,
+    AROS_LPA(float, fnum1, D0),
     struct Library *, MathTransBase, 6, MathTrans)
 
-AROS_LP2(LONG, SPSincos,
+AROS_LP2(float, SPSincos,
     AROS_LPA(IPTR*, pfnum2, D1),
-    AROS_LPA(LONG , fnum1 , D0),
+    AROS_LPA(float , fnum1 , D0),
     struct Library *, MathTransBase, 9, MathTrans)
 
-AROS_LP1(LONG, SPSinh,
-    AROS_LPA(LONG, fnum1 , D0),
+AROS_LP1(float, SPSinh,
+    AROS_LPA(float, fnum1 , D0),
     struct Library *, MathTransBase, 10, MathTrans)
 
-AROS_LP1(LONG, SPSqrt,
-    AROS_LPA(LONG, fnum1, D0),
+AROS_LP1(float, SPSqrt,
+    AROS_LPA(float, fnum1, D0),
     struct Library *, MathTransBase, 16, MathTrans)
 
-AROS_LP1(LONG, SPTan,
-    AROS_LPA(LONG, fnum1, D0),
+AROS_LP1(float, SPTan,
+    AROS_LPA(float, fnum1, D0),
     struct Library *, MathTransBase, 8, MathTrans)
 
-AROS_LP1(LONG, SPTanh,
-    AROS_LPA(LONG, fnum1, D0),
+AROS_LP1(float, SPTanh,
+    AROS_LPA(float, fnum1, D0),
     struct Library *, MathTransBase, 12, MathTrans)
 
-AROS_LP1(LONG, SPTieee,
-    AROS_LPA(LONG, fnum, D0),
+AROS_LP1(float, SPTieee,
+    AROS_LPA(float, fnum, D0),
     struct Library *, MathTransBase, 17, MathTrans)
 
 
