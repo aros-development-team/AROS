3afc21ecd7e870df0ec115925fd1fbca40041a81
turrican <turrican@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-02-12 15:29:57 +0000

    Linux/m68k uses a different sigcore.h.src
    git-svn-id: https://svn.aros.org/svn/aros/trunk@3977 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-linux/exec/mksigcore.sh
+++ arch/all-linux/exec/mksigcore.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 type=`grep "^struct sigcontext" /usr/include/asm/sigcontext.h | sed 's/\\{//'`
-sed "s/@sigcontext@/$type/" ${1-.}/sigcore.h.src > ${2}
+if [ `uname -m` = "m68k" ]; then
+  sed "s/@sigcontext@/$type/" ${1-.}/../m68k/sigcore.h.src > ${2}
+else
+  sed "s/@sigcontext@/$type/" ${1-.}/sigcore.h.src > ${2}
+fi
