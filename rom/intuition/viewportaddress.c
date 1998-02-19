5e202eeaf7b364ead942bb007a2438fe75416c67
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-02-19 20:37:04 +0000

    New functions.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@3997 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/intuition/viewportaddress.c
@@ -0,0 +1,57 @@
+/*
+    (C) 1995-98 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: Intuition function ViewPortAddress()
+    Lang: english
+*/
+#include <graphics/view.h>
+#include <intuition/intuition.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/intuition.h>
+
+	AROS_LH1(struct ViewPort *, ViewPortAddress,
+
+/*  SYNOPSIS */
+	AROS_LHA(struct Window *, Window, D0),
+
+/*  LOCATION */
+	struct IntuitionBase *, IntuitionBase, 50, Intuition)
+
+/*  FUNCTION
+	Returns the address of the viewport of a given window. Use this
+	call, if you want to use any graphics, text or animation functions
+	that require the address of a viewport for your window.
+	
+    INPUTS
+	Window - pointer to a Window structure
+
+    RESULT
+	Address of the Intuition ViewPort structure for the screen that your
+	window is displayed on.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	graphics.library
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+  AROS_LIBFUNC_INIT
+  AROS_LIBBASE_EXT_DECL(struct IntuitionBase *, IntuitionBase)
+
+  return &(Window->WScreen->ViewPort);
+
+  AROS_LIBFUNC_EXIT
+} /* ViewPortAddress */
