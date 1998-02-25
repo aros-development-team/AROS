4d44ee7c1d503cb84af56e6283e242cd624724d7
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-02-25 22:27:00 +0000

    *** empty log message ***
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4015 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/mathtrans/spatan.c
+++ workbench/libs/mathtrans/spatan.c
@@ -28,7 +28,7 @@
 
 /*  LOCATION */
 
-      struct MathTransBase *, MathTransBase, 5, MathTrans)
+      struct Library *, MathTransBase, 5, Mathtrans)
 
 /*  FUNCTION
 
@@ -56,9 +56,68 @@
     HISTORY
 
 ******************************************************************************/
-
 {
-AROS_LIBFUNC_INIT
-  return 0;
-AROS_LIBFUNC_EXIT
+  LONG fnumabs = fnum1 & (FFPMantisse_Mask | FFPExponent_Mask);
+  LONG fnumsquared, fnumcubed;
+
+  /* check for +- infinity -> output: +-pi/2 */
+  if (FFP_Pinfty == fnumabs )
+    return (pio2 | (fnum1 & FFPSign_Mask));
+
+  /* atan(0) = 0 */
+  if (0 == fnumabs)
+  {
+     SetSR(Zero_Bit, Zero_Bit | Negative_Bit | Overflow_Bit);
+     return 0;
+  }
+
+  /* atan(x>= 128) = pi/2 - 1/x */
+  if ((BYTE) fnumabs >= 0x48)
+    if (fnumabs == fnum1) /* arg has ppositive sign */
+    {
+      SetSR(0, Zero_Bit | Negative_Bit | Overflow_Bit);
+      return SPSub(SPDiv(fnumabs,one),pio2);
+    }
+    else
+    {
+      SetSR(Negative_Bit, Zero_Bit | Negative_Bit | Overflow_Bit);
+      return (SPSub(SPDiv(fnumabs,one),pio2)) | FFPSign_Mask;
+    }
+
+  /* atan(x >= 64) = pi/2 - 1/x +1/(3*x^3) */
+
+  fnumsquared = SPMul(fnumabs, fnumabs);
+
+  if((BYTE) fnumabs >= 0x47)
+  {
+    fnumcubed   = SPMul(fnumabs, fnumsquared);
+
+    /* pi/2 - 1/x + 1/(3*x^3) = pi/2 + (1-3*x^2)/(3*x^3)*/
+    if (fnumabs == fnum1) /* arg has positive sign */
+    {
+      SetSR(0, Zero_Bit | Negative_Bit | Overflow_Bit);
+      return SPAdd(pio2,
+              SPDiv(
+               SPMul(three, fnumcubed),
+              SPAdd(
+               SPMul(three, fnumsquared) | FFPSign_Mask,
+                    one )
+                   ));
+    }
+    else
+    {
+      SetSR(Negative_Bit, Zero_Bit | Negative_Bit | Overflow_Bit);
+      return SPAdd(pio2,
+              SPDiv(
+               SPMul(three, fnumcubed),
+              SPAdd(
+               SPMul(three, fnumsquared) | FFPSign_Mask,
+                    one )
+                   )) | FFPSign_Mask;
+    }
+  }
+
+  /* atan(x <= 64) */
+  return SPAsin(SPDiv(SPSqrt(SPAdd(one,fnumsquared)),fnum1));
+
 } /* SPAtan */
