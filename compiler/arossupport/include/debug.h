e38124b176f103968b5ca37a6d4d8f7c5202954d
nlorentz <nlorentz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-03-12 10:04:25 +0000

    New debug macros that indent for each functioncall made. Makes debug output more readable (at least IMO). Look into the file for usage description
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4064 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/arossupport/include/debug.h
+++ compiler/arossupport/include/debug.h
@@ -7,10 +7,17 @@
 #ifndef CLIB_AROS_PROTOS_H
 #   include <proto/arossupport.h>
 #endif
+#ifndef EXEC_TASKS_H
+#   include <exec/tasks.h>
+#endif
 
 #ifndef DEBUG
 #   define DEBUG 0
 #endif
+#ifndef SDEBUG
+#   define SDEBUG 0
+#endif
+
 
 /* Remove all macros. They get new values each time this file is
     included */
@@ -25,38 +32,95 @@
 #undef ReturnSpecial
 #undef ReturnBool
 
+/*  Macros for "stair debugging" */
+#undef SDInit
+#undef EnterFunc
+#undef Indent
+#undef ExitFunc
+
+#if SDEBUG
+
+/* This is some new macros for making debug output more readable,
+** by indenting for each functioncall made.
+** Usage: Call the SDInit() macro before anything else in your main().
+** Start the functions you want to debug with EnterFunc(bug("something"))
+** and ALWAYS match these with a Returnxxxx type macro
+** at the end of the func.
+** Inside the func you can use the normal D(bug()) macro.
+** 
+** To enable the macros, just add a #define SDEBUG 1
+*/
+
+/* User macro */
+#define EnterFunc(x) {   			\
+	struct Task *sd_task = FindTask(NULL);	\
+   	int sd_spaceswritten;					\
+   	for (sd_spaceswritten = 0; sd_spaceswritten < (ULONG)sd_task->tc_UserData; sd_spaceswritten ++) kprintf(" "); \
+   	((ULONG)sd_task->tc_UserData) ++;		} \
+	x
+
+/* User macro. Add into start of your main() routine */
+#   define SDInit()	\
+	{ struct Task *sd_task = FindTask(NULL); sd_task->tc_UserData = NULL; }
+
+
+/* Internal */
+#   define Indent {   		\
+	struct Task *sd_task = FindTask(NULL);	\
+   	int sd_spaceswritten;					\
+   	for (sd_spaceswritten = 0; sd_spaceswritten < (ULONG)sd_task->tc_UserData; sd_spaceswritten ++) kprintf(" "); }
+   	
+/* Internal */
+#define ExitFunc { 				\
+	struct Task *sd_task = FindTask(NULL);	\
+   	int sd_spaceswritten;					\
+   	((ULONG)sd_task->tc_UserData) --;		\
+   	for (sd_spaceswritten = 0; sd_spaceswritten < (ULONG)sd_task->tc_UserData; sd_spaceswritten ++) kprintf(" "); } 
+
+
+#else
+
+#   define SDInit()
+#   define Indent
+#   define EnterFunc(x) D(x)
+#   define ExitFunc
+
+#endif
+
 #if DEBUG
-#   define D(x)     x
+#   define D(x)     Indent x
 
 #   if DEBUG > 1
 #	define DB2(x)    x
 #   else
 #	define DB2(x)    /* eps */
-#endif
+#   endif
+
+
 
     /* return-macros. NOTE: I make a copy of the value in __aros_val, because
        the return-value might have side effects (like return x++;). */
-#   define ReturnVoid(name)         { kprintf ("Exit " name "()\n"); return; }
-#   define ReturnPtr(name,type,val) { type __aros_val = (type)val; \
-				    kprintf ("Exit " name "=%08lx\n", \
+#   define ReturnVoid(name)         { ExitFunc kprintf ("Exit " name "()\n"); return; }
+#   define ReturnPtr(name,type,val) {  type __aros_val = (type)val; \
+				    ExitFunc kprintf ("Exit " name "=%08lx\n", \
 				    (ULONG)__aros_val); return __aros_val; }
 #   define ReturnStr(name,type,val) { type __aros_val = (type)val; \
-				    kprintf ("Exit " name "=\"%s\"\n", \
+				    ExitFunc kprintf ("Exit " name "=\"%s\"\n", \
 				    __aros_val); return __aros_val; }
 #   define ReturnInt(name,type,val) { type __aros_val = (type)val; \
-				    kprintf ("Exit " name "=%ld\n", \
+				    ExitFunc kprintf ("Exit " name "=%ld\n", \
 				    (ULONG)__aros_val); return __aros_val; }
 #   define ReturnXInt(name,type,val) { type __aros_val = (type)val; \
-				    kprintf ("Exit " name "=%lx\n", \
+				    ExitFunc kprintf ("Exit " name "=%lx\n", \
 				    (ULONG)__aros_val); return __aros_val; }
 #   define ReturnFloat(name,type,val) { type __aros_val = (type)val; \
-				    kprintf ("Exit " name "=%g\n", \
+				    ExitFunc kprintf ("Exit " name "=%g\n", \
 				    (ULONG)__aros_val); return __aros_val; }
 #   define ReturnSpecial(name,type,val,fmt) { type __aros_val = (type)val; \
-				    kprintf ("Exit " name "=" fmt "\n", \
+				    ExitFunc kprintf ("Exit " name "=" fmt "\n", \
 				    (ULONG)__aros_val); return __aros_val; }
 #   define ReturnBool(name,val)     { BOOL __aros_val = (val != 0); \
-				    kprintf ("Exit " name "=%s\n", \
+				    ExitFunc kprintf ("Exit " name "=%s\n", \
 				    __aros_val ? "TRUE" : "FALSE"); \
 				    return __aros_val; }
 #else /* !DEBUG */
