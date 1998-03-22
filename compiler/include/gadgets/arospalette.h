5fb6fee47476d885ffcffa39ca1523bcc8e29608
nlorentz <nlorentz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-03-22 20:47:35 +0000

    Include file for palette class
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4122 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/gadgets/arospalette.h
@@ -0,0 +1,19 @@
+#ifndef GADGETS_AROSPALETTE_H
+#   define GADGETS_AROSPALETTE_H
+
+#ifndef LIBRARIES_GADTOOLS_H
+#   include <libraries/gadtools.h>
+#endif
+
+#define AROSPALETTECLASS "palette.aros"
+#define AROSPALETTECLASSPATH "SYS:Classes/Gadgets/arospalette.gadget"
+
+#define AROSA_Palette_Depth		GTPA_Depth
+#define AROSA_Palette_Color		GTPA_Color
+#define AROSA_Palette_ColorOffset	GTPA_ColorOffset
+#define AROSA_Palette_IndicatorWidth	GTPA_IndicatorWidth
+#define AROSA_Palette_IndicatorHeight	GTPA_IndicatorHeight
+#define AROSA_Palette_NumColors		GTPA_NumColors
+#define AROSA_Palette_ColorTable	GTPA_ColorTable
+
+#endif /* GADGETS_AROSPALETTE#_H */
