f863ab4095dc75e276e5acde98547f664589e914
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-03-28 23:48:56 +0000

    Include file for arp.library
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4142 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/proto/arp.h
@@ -0,0 +1,20 @@
+/*
+    (C) 1995-97 AROS - The Amiga Replacement OS
+    $Id$
+*/
+#ifndef PROTO_ARP_H
+#define PROTO_ARP_H
+
+#ifndef AROS_SYSTEM_H
+#include <aros/system.h>
+#endif
+
+#include <clib/arp_protos.h>
+
+#if defined(_AMIGA) && defined(__GNUC__)
+#include <inline/arp.h>
+#else
+#include <defines/arp.h>
+#endif
+
+#endif /* PROTO_ARP_H */
