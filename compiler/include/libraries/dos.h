56f0cbf751ef4d6b4ae8e8b84303df52f4693a40
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-03-29 19:25:18 +0000

    Include file
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4151 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/libraries/dos.h
@@ -0,0 +1,15 @@
+#ifndef LIBRARIES_DOS_H
+#define LIBRARIES_DOS_H 1
+/*
+    (C) 1997 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: Definitions for dos.library
+    Lang: english
+*/
+
+#ifndef DOS_DOS_H
+#include <dos/dos.h>
+#endif
+
+#endif          /* LIBRARIES_DOS_H */
