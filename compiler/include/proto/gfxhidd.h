61f80355a71d4723fbf33cc904827961e8c9e84d
indy <indy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-04-07 19:36:37 +0000

    gfxhidd.library proto file
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4201 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/proto/gfxhidd.h
@@ -0,0 +1,21 @@
+/* $Id$ */
+/* Automatically generated header! Do not edit! */
+
+#ifndef PROTO_GFXHIDD_H
+#define PROTO_GFXHIDD_H
+
+#include <clib/gfxhidd_protos.h>
+
+#ifdef __GNUC__
+#include <inline/gfxhidd.h>
+#endif /* __GNUC__ */
+
+#ifndef __NOLIBBASE__
+extern struct Library *
+#ifdef __CONSTLIBBASEDECL__
+__CONSTLIBBASEDECL__
+#endif /* __CONSTLIBBASEDECL__ */
+GfxHiddBase;
+#endif /* !__NOLIBBASE__ */
+
+#endif /* !PROTO_GFXHIDD_H */
