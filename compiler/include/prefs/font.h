c20c0e5bbdd6e30af07a42838d6cea510f0f6ac0
srittau <srittau@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-04-15 15:36:11 +0000

    Include-file formatted as the standard (dos/*.h) reqires it. I had changed this include-file quite a long time ago and want to get rid of the "M" when checking out :)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4250 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/prefs/font.h
+++ compiler/include/prefs/font.h
@@ -2,43 +2,44 @@
 #define PREFS_FONT_H
 
 /*
-    (C) 1995-97 AROS - The Amiga Replacement OS
+    (C) 1997 AROS - The Amiga Replacement OS
     $Id$
 
-    Desc:
+    Desc: Font prefs definitions
     Lang: english
 */
 
 #ifndef EXEC_TYPES_H
 #   include <exec/types.h>
 #endif
-
-#ifndef LIBRARIES_IFFPARSE_H
-#   include <libraries/iffparse.h>
-#endif
 #ifndef GRAPHICS_TEXT_H
 #   include <graphics/text.h>
 #endif
+#ifndef LIBRARIES_IFFPARSE_H
+#   include <libraries/iffparse.h>
+#endif
+
 
 #define ID_FONT MAKE_ID('F','O','N','T')
 
-#define FONTNAMESIZE (128)
+/* The maximum length the name of a font may have. */
+#define FONTNAMESIZE 128
 
 struct FontPrefs
 {
-    LONG	    fp_Reserved[3];
-    UWORD	    fp_Reserved2;
-    UWORD	    fp_Type;
-    UBYTE	    fp_FrontPen;
-    UBYTE	    fp_BackPen;
-    UBYTE	    fp_DrawMode;
+    LONG            fp_Reserved[3]; /* PRIVATE */
+    UWORD           fp_Reserved2;   /* PRIVATE */
+    UWORD           fp_Type;        /* see below */
+    UBYTE           fp_FrontPen;
+    UBYTE           fp_BackPen;
+    UBYTE           fp_DrawMode;
     struct TextAttr fp_TextAttr;
-    BYTE	    fp_Name[FONTNAMESIZE];
+    BYTE            fp_Name[FONTNAMESIZE];
 };
 
-/* FontPrefs->fp_Type */
-#define FP_WBFONT	0
-#define FP_SYSFONT	1
-#define FP_SCREENFONT	2
+/* fp_Type */
+#define FP_WBFONT     0
+#define FP_SYSFONT    1
+#define FP_SCREENFONT 2
 
 #endif /* PREFS_FONT_H */
