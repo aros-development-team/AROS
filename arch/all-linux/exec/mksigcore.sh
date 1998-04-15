284084841d5741ef8165fac62e3837d19b2c0c5f
srittau <srittau@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-04-15 15:38:45 +0000

    Some changes I had to make to make AROS run on my i386 Linux box. This may break other Linux systems, so don't be surprised and change this
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4251 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-linux/exec/mksigcore.sh
+++ arch/all-linux/exec/mksigcore.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 type=`grep "^struct sigcontext" /usr/include/asm/sigcontext.h | sed 's/\\{//'`
+handler=__sighandler_t
 if [ `uname -m` = "m68k" ]; then
   sed "s/@sigcontext@/$type/" ${1-.}/../m68k/sigcore.h.src > ${2}
 else
-  sed "s/@sigcontext@/$type/" ${1-.}/sigcore.h.src > ${2}
+  sed "s/@sigcontext@/$type/;s/@sighandler@/$handler/" ${1-.}/sigcore.h.src > ${2}
 fi
