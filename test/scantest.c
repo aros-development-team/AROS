6bbbf8fba116d35767fae1902f685110f1b084cb
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-04-28 18:25:39 +0000

    Recursively scans a directory tree via Examine() and ExNext().
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4295 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/scantest.c
@@ -0,0 +1,64 @@
+#include <dos/dosasl.h>
+#include <dos/dos.h>
+#include <proto/dos.h>
+#include <exec/types.h>
+#include <exec/memory.h>
+#include <proto/exec.h>
+
+#include <stdio.h>
+#include <string.h>
+
+void ReadAll(BPTR lock)
+{
+  struct FileInfoBlock * FIB = AllocVec(sizeof(struct FileInfoBlock), MEMF_CLEAR);
+  BOOL success;
+  int count = 1;
+
+  success = Examine(lock, FIB);
+  success = ExNext(lock,FIB);
+  while (success)
+  {
+    printf("%s",FIB->fib_FileName);
+    if (FIB->fib_DirEntryType > 0)
+    {
+      printf(" (Dir)\n");
+      count++;
+      if (count > 1)
+      {
+        char * name = AllocVec(1024,0);
+        BPTR tmplock;
+        NameFromLock(lock,name,1024);
+        AddPart(name,FIB->fib_FileName,1024);
+        printf("Entering %s\n",name);
+        tmplock = Lock(name , ACCESS_READ);
+        ReadAll(tmplock);
+        NameFromLock(lock,name,1024);
+        printf("Returning to %s\n",name);
+        UnLock(tmplock);
+        FreeVec(name);
+      }
+    }
+    else
+    {
+      printf("\n");
+    }
+    success = ExNext(lock,FIB);
+  }
+  FreeVec(FIB);
+}
+
+void main(int argc, char *argv[])
+{
+  BPTR lock;
+  argv[1][strlen(argv[1])-1] = '\0';
+
+  printf("Trying to scan %s \n",argv[1]);
+  lock = Lock(argv[1],ACCESS_READ);
+  if (lock)
+  {
+    ReadAll(lock);
+    UnLock(lock);
+  }
+  else
+    printf("no such directory/assign!\n");
+}
