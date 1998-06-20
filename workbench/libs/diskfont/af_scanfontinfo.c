fc63d482324c615211413c5919f0d608f3479952
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-06-20 22:58:47 +0000

    Reformatted that file.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4355 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/diskfont/af_scanfontinfo.c
+++ workbench/libs/diskfont/af_scanfontinfo.c
@@ -31,168 +31,168 @@ extern struct AFHookDescr hdescr[];
 
 BOOL ScanFontInfo
 (   
-    ULONG           userflags,
-    struct MinList  *filist,
-    struct DiskfontBase_intern *DiskfontBase
+  ULONG           userflags,
+  struct MinList  *filist,
+  struct DiskfontBase_intern *DiskfontBase
 )
 {
     
-    ULONG retval;
-	UWORD idx;
+  ULONG retval;
+  UWORD idx;
 	
-    struct AFHookDescr      *afhd;
-    struct FontInfoNode     *finode = NULL;
-    struct FontHookCommand  fhc = {0};
+  struct AFHookDescr      *afhd;
+  struct FontInfoNode     *finode = NULL;
+  struct FontHookCommand  fhc = {0};
 
-   	struct TTextAttr *srcattr, *destattr = NULL;
-	UWORD sfi_state;
+  struct TTextAttr *srcattr, *destattr = NULL;
+  UWORD sfi_state;
 	
-	BOOL  success	= TRUE;
+  BOOL  success	= TRUE;
 	
-    D(bug("ScanFontInfo(userflags=%d, filist=%p)\n", userflags,	filist));
+  D(bug("ScanFontInfo(userflags=%d, filist=%p)\n", userflags,	filist));
    
     
-    /* The hook's deatination TextAttr becomes our source TextAttr */
-    srcattr = &(fhc.fhc_DestTAttr);
+  /* The hook's deatination TextAttr becomes our source TextAttr */
+  srcattr = &(fhc.fhc_DestTAttr);
 
-    /* Execute all fitting hooks.*/
+  /* Execute all fitting hooks.*/
     
-	for (idx = 0; idx < NUMFONTHOOKS; idx ++)
-    {
-        BOOL  done		= FALSE;
+  for (idx = 0; idx < NUMFONTHOOKS; idx ++)
+  {
+    BOOL  done = FALSE;
 	    
-     	afhd = &DFB(DiskfontBase)->hdescr[idx];
+    afhd = &DFB(DiskfontBase)->hdescr[idx];
     	
-        /* Call this hook only if it has the flags input by the user */
-        if (afhd->ahd_Flags != ( afhd->ahd_Flags & userflags ) )
-        	continue;
+    /* Call this hook only if it has the flags input by the user */
+    if (afhd->ahd_Flags != ( afhd->ahd_Flags & userflags ) )
+      continue;
         
-        /* Ask the hook to initialize itself */
-         fhc.fhc_Command = FHC_AF_INIT;
+    /* Ask the hook to initialize itself */
+    fhc.fhc_Command = FHC_AF_INIT;
   
-        /* If the initialization of this hook fails, we continue with the next hook */             
-		if ( !CallHookPkt( &afhd->ahd_Hook, &fhc, DFB(DiskfontBase) ))
-        	continue;
+    /* If the initialization of this hook fails, we continue with the next hook */
+    if ( !CallHookPkt( &afhd->ahd_Hook, &fhc, DFB(DiskfontBase) ))
+      continue;
                 
-      	/* Read all the fonts available through the hook. Update command type */
-        fhc.fhc_Command = FHC_AF_READFONTINFO;
+    /* Read all the fonts available through the hook. Update command type */
+    fhc.fhc_Command = FHC_AF_READFONTINFO;
       
-       	sfi_state = SFI_NEWDESCRNODE;          
-        do
-        {
+    sfi_state = SFI_NEWDESCRNODE;
+    do
+    {
         	
-       		switch (sfi_state)
-            {
-            	case SFI_NEWDESCRNODE:
+      switch (sfi_state)
+      {
+       	case SFI_NEWDESCRNODE:
                 
-                    /* Allocate a new FontInfoNode */
-                    if ( !(finode = AllocFIN(filist, DFB(DiskfontBase) )))
-                    	{ success = FALSE;  break; }
-					
-					destattr = &(finode->TAF.taf_Attr);
-		            
-                    /* Note that there are no break here, because
-                       READDESCR should be executed if NEWDESCRNODE is
-                    */
-					
-				case SFI_READDESCR:
-                    /* Reset the DestTExtattr to all 0s */		
-                    memset(&(fhc.fhc_DestTAttr), 0, sizeof (struct TTextAttr));
+          /* Allocate a new FontInfoNode */
+          if ( !(finode = AllocFIN(filist, DFB(DiskfontBase) )))
+            { success = FALSE;  break; }
+	
+          destattr = &(finode->TAF.taf_Attr);
+      	
+          /* Note that there are no break here, because
+             READDESCR should be executed if NEWDESCRNODE is
+          */
+      				
+        case SFI_READDESCR:
+          /* Reset the DestTExtattr to all 0s */		
+          memset(&(fhc.fhc_DestTAttr), 0, sizeof (struct TTextAttr));
+
+          /* Read the font description */
+          retval = CallHookPkt( &afhd->ahd_Hook, &fhc, DFB(DiskfontBase) );
                     
-                	/* Read the font description */
-                    retval = CallHookPkt( &afhd->ahd_Hook, &fhc, DFB(DiskfontBase) );                           
-                    
-                    /* If FALSE, then there is a fatal error */
-                    if (!retval)
-                    	{ success = FALSE; break; }
+          /* If FALSE, then there is a fatal error */
+          if (!retval)
+            { success = FALSE; break; }
                             
                             
-                    /* Is there a non-fatal error ? If so reuse the fontinfonode */
-                    if (retval & FH_SINGLEERROR)
-                    	{ sfi_state = SFI_READDESCR; break; }
+          /* Is there a non-fatal error ? If so reuse the fontinfonode */
+          if (retval & FH_SINGLEERROR)
+            { sfi_state = SFI_READDESCR; break; }
                       
-                    /* Otherwise start allocate a new fontinfonode */
-                    sfi_state = SFI_NEWDESCRNODE;
+          /* Otherwise start allocate a new fontinfonode */
+          sfi_state = SFI_NEWDESCRNODE;
                             
-                    /* Are we finished scanning ? */
-                    if (retval & FH_SCANFINISHED)
-                    {
-                		FreeFIN(finode, DFB(DiskfontBase) );
-                		done = TRUE;
-                        break;
-                    }
+          /* Are we finished scanning ? */
+          if (retval & FH_SCANFINISHED)
+          {
+             FreeFIN(finode, DFB(DiskfontBase) );
+             done = TRUE;
+             break;
+          }
            			
-           			/* If we don't want scaled fonts and the font isn't designed,
-           				then skip it
-           			*/
-           			if ( !(userflags & AFF_SCALED) && !(srcattr->tta_Flags & FPF_DESIGNED))
-           				{ sfi_state = SFI_READDESCR; break; }
+          /* If we don't want scaled fonts and the font isn't designed,
+             then skip it
+          */
+          if ( !(userflags & AFF_SCALED) && !(srcattr->tta_Flags & FPF_DESIGNED))
+            { sfi_state = SFI_READDESCR; break; }
                             
-                    /* Everything went OK, now process the info we read */
+          /* Everything went OK, now process the info we read */
+
+          destattr->tta_YSize = srcattr->tta_YSize;
+          destattr->tta_Style = srcattr->tta_Style;
+          destattr->tta_Flags = srcattr->tta_Flags;
 					
-                    destattr->tta_YSize = srcattr->tta_YSize;
-                    destattr->tta_Style = srcattr->tta_Style;
-                    destattr->tta_Flags = srcattr->tta_Flags;
 					
-					
-					/* Set AvailFonts type */
-					finode->TAF.taf_Type =
-						(destattr->tta_Flags & FPF_DESIGNED ? 0 : AFF_SCALED) |
-						afhd->ahd_Flags;
+          /* Set AvailFonts type */
+          finode->TAF.taf_Type =
+            (destattr->tta_Flags & FPF_DESIGNED ? 0 : AFF_SCALED) |
+                        afhd->ahd_Flags;
 						
                     
-                   	/* Should we reuse last fontname ? */
+          /* Should we reuse last fontname ? */
                             
-                    if (retval & FH_REUSENAME)
-                    	finode->Flags |= FDF_REUSENAME;
-                    else
-                    {
-                    	/* Copy fontname */
+          if (retval & FH_REUSENAME)
+            finode->Flags |= FDF_REUSENAME;
+          else
+          {
+            /* Copy fontname */
                     	
-                    	finode->NameLength = strlen(srcattr->tta_Name) + 1;
+            finode->NameLength = strlen(srcattr->tta_Name) + 1;
                     	
-                    	destattr->tta_Name = AllocVec( finode->NameLength, MEMF_ANY );    
-                        if (!destattr->tta_Name)
-                        	{success = FALSE; break; }
+            destattr->tta_Name = AllocVec( finode->NameLength, MEMF_ANY );
+            if (!destattr->tta_Name)
+              {success = FALSE; break; }
                         
-                        strcpy(destattr->tta_Name, srcattr->tta_Name);
-                    }
+              strcpy(destattr->tta_Name, srcattr->tta_Name);
+          }
                     
-                    /* Likewise with tags */
-                    if (userflags & AFF_TAGGED)
-                    {
-                    	if (retval & FH_REUSETAGS)
-                        	finode->Flags |= FDF_REUSETAGS;
-                        else
-                        {
-                        	if (srcattr->tta_Tags)
-                        	{
-              		          	destattr->tta_Tags = CloneTagItems(srcattr->tta_Tags);        
-                    	        if (!destattr->tta_Tags)
-        					      	{ success = FALSE; break; }
+          /* Likewise with tags */
+          if (userflags & AFF_TAGGED)
+          {
+            if (retval & FH_REUSETAGS)
+              finode->Flags |= FDF_REUSETAGS;
+            else
+            {
+              if (srcattr->tta_Tags)
+              {
+                destattr->tta_Tags = CloneTagItems(srcattr->tta_Tags);
+                if (!destattr->tta_Tags)
+                  { success = FALSE; break; }
         				    
-       		 				    /* Count number of tags for later */
-        					    finode->NumTags = NumTags(destattr->tta_Tags, DFB(DiskfontBase) );
-							}
-                    	}
-                    }
-                	break;
-                	
-            } /* switch (sfi_state) */
+                /* Count number of tags for later */
+                finode->NumTags = NumTags(destattr->tta_Tags, DFB(DiskfontBase) );
+              }
+            }
+          }
+        break;
+              	
+      } /* switch (sfi_state) */
                 
-        /* Do while successfull and not yet finished scanning */
-		}
-        while (success && !done);
+      /* Do while successfull and not yet finished scanning */
+    }
+    while (success && !done);
 
-        /* Tell the hook to clean up */
-        fhc.fhc_Command = FHC_AF_CLEANUP;
+    /* Tell the hook to clean up */
+    fhc.fhc_Command = FHC_AF_CLEANUP;
       
-        CallHookPkt( &afhd->ahd_Hook, &fhc, DFB(DiskfontBase) );
+    CallHookPkt( &afhd->ahd_Hook, &fhc, DFB(DiskfontBase) );
             
         
-	}  /* for (idx = 0; idx < NUMFONTHOOKS; idx ++) */
+  }  /* for (idx = 0; idx < NUMFONTHOOKS; idx ++) */
     
-    ReturnBool ("ScanFontInfo", success);
+  ReturnBool ("ScanFontInfo", success);
 }
 
 
