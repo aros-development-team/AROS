56da9ac3c5a326c8bf4dfdf293ab6c463f82fa92
hkiel <hkiel@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-06-24 22:44:26 +0000

    Added new function
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4361 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/rewind.c
@@ -0,0 +1,45 @@
+/*
+    (C) 1995-96 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: Change the position in a stream
+    Lang: english
+*/
+
+/*****************************************************************************
+
+    NAME */
+#include <stdio.h>
+
+	void rewind (
+
+/*  SYNOPSIS */
+	FILE * stream)
+
+/*  FUNCTION
+	Change the current position in a stream to the beginning.
+
+    INPUTS
+	stream - Modify this stream
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	fopen(), fwrite(), fseek()
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+{
+    fseek (stream, 0L, SEEK_SET);
+    clearerr (stream);
+} /* rewind */
+
