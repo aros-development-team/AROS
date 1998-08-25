87b6352c398cd8355384305c1bc7762947a8af40
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-08-25 20:22:29 +0000

    Some old programs always miss the same includes (protos). I'm sick of writing the same lines again and again.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4552 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/arossupport/include/oldprograms.h
@@ -0,0 +1,21 @@
+#define USHORT UWORD
+#define SHORT WORD
+
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <proto/intuition.h>
+#include <proto/graphics.h>
+#include <proto/mathffp.h>
+#include <proto/mathtrans.h>
+/*
+  #include <proto/layers.h>
+*/
+#include <intuition/iobsolete.h>
+
+#ifndef TOPAZ_EIGHTY
+  #define TOPAZ_EIGHTY 8
+#endif
+
+#ifndef TOPAZ_SIXTY
+  #define TOPAZ_SIXTY 6
+#endif
