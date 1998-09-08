d5928c84acbdf5caf7cbc4dcab5d3f51250e0045
bernie <bernie@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-09-08 04:24:47 +0000

    inclusion of string.h
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4622 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/expansion/makedosnode.c
+++ rom/expansion/makedosnode.c
@@ -9,6 +9,7 @@
 #include <exec/memory.h>
 #include <dos/dos.h>
 #include <proto/exec.h>
+#include <string.h>
 /*****************************************************************************
 
     NAME */
