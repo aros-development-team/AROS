2f6204e2b247749bfda9440e6a85dce7bb8b658b
bernie <bernie@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-09-08 04:26:23 +0000

    moved inclusion of string.h where symbol _AMIGA might already be defined
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4623 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/keymap/support.c
+++ rom/keymap/support.c
@@ -1,6 +1,6 @@
-#include <string.h>
 #include <devices/inputevent.h>
 #include "keymap_intern.h"
+#include <string.h>
 
 #include <aros/debug.h>
 
@@ -18,10 +18,10 @@ BOOL WriteToBuffer(struct BufInfo *bufinfo, UBYTE *string, LONG numchars)
 {
     if (bufinfo->CharsWritten + numchars > bufinfo->BufLength)
     	return (FALSE);
-    	
+
     strncpy(bufinfo->Buffer, string, numchars);
     bufinfo->CharsWritten += numchars;
-    
+
     return (TRUE);
 }
 
@@ -31,65 +31,65 @@ BOOL WriteToBuffer(struct BufInfo *bufinfo, UBYTE *string, LONG numchars)
 BOOL GetKeyInfo(struct KeyInfo *ki, UWORD code, UWORD qual, struct KeyMap *km)
 {
     BOOL valid = TRUE; /* valid code is default */
-    
-    
+
+
     if (code & IECODE_UP_PREFIX) /* Key pressed ? */
         valid = FALSE;
     else
     {
     	BYTE capsable;
     	BYTE repeatable;
-    	
+
     	ki->KCFQual = KC_NOQUAL; /* == 0 */
-    
+
     	code &= ~IECODE_UP_PREFIX;
-    
+
     	/* Convert from IEQUAL_xxx into KCF_xx */
     	if (qual & (IEQUALIFIER_LSHIFT|IEQUALIFIER_RSHIFT))
     	    ki->KCFQual |= KCF_SHIFT;
-    	
+
     	if (qual & (IEQUALIFIER_LALT|IEQUALIFIER_RALT))
     	    ki->KCFQual |= KCF_ALT;
-    	
+
     	if (qual & IEQUALIFIER_CONTROL)
     	    ki->KCFQual |= KCF_CONTROL;
-    	
+
     	D(bug("mrk: KCF qual: %d\n", ki->KCFQual));
-    	
+
         /* Get the type of the key */
         if (code <= 0x3F)
         {
     	    /* Get key info from low keymap */
     	    ki->Key_MapType = km->km_LoKeyMapTypes[code];
     	    ki->Key_Mapping = km->km_LoKeyMap[code];
-    	    
+
     	    capsable    = GetBitProperty(km->km_LoCapsable,   code);
     	    repeatable  = GetBitProperty(km->km_LoRepeatable, code);
-    	} 
+    	}
    	else
     	{
     	    code -= 0x40; /* hex 40 is first indexed */
-    	    
+
     	    /* Get key info from high keymap */
     	    ki->Key_MapType = km->km_HiKeyMapTypes[code];
     	    ki->Key_Mapping = km->km_HiKeyMap[code];
     	    capsable    = GetBitProperty(km->km_LoCapsable,   code);
     	    repeatable  = GetBitProperty(km->km_LoRepeatable, code);
     	}
-    
+
     	D(bug("mrk: capsable=%d\n", capsable));
-    
+
     	if ((qual & IEQUALIFIER_CAPSLOCK) && capsable)
     	    ki->KCFQual |= KCF_SHIFT;
-    
+
     	if ((qual & IEQUALIFIER_REPEAT) && (!repeatable))
     	    valid = FALSE; /* Repeating not supported for key, skip keypress */
-    	
+
     	D(bug("mrk:repeat test passed\n"));
 
    	D(bug("mrk: key mapping: %04x\n", ki->Key_Mapping));
     }
-    
+
     return (valid);
 }
 
@@ -100,27 +100,27 @@ WORD GetDeadKeyIndex(UWORD code, UWORD qual, struct KeyMap *km)
 {
     struct KeyInfo ki;
     WORD retval = -1;
-    
+
     /* Get the key info for the key */
     if (GetKeyInfo(&ki, code, qual, km))
     {
-    	
+
     	if (ki.Key_MapType & KCF_DEAD)
     	{
     	    BYTE idx;
-    	    
+
 	    /* Use keymap_str table to get idx to right key descriptor */
 	    idx = keymapstr_table[ki.Key_MapType & KC_VANILLA][ki.KCFQual];
 	    if (idx != -1)
 	    {
 	        UBYTE *dead_descr = (UBYTE *)ki.Key_Mapping;
-	        
+
 	        if (dead_descr[idx] == DPF_DEAD)
 	        {
 	            /* Clear first */
-	            
+
 	            retval = dead_descr[idx + 1];
-	            
+
 	            retval &= 0x00FF; /* Clear upper byte */
 	        }
 	    }
