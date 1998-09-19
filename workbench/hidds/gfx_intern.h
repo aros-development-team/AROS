9f88f7d39cae1a2863ed28a8e9e628bd873a8901
nlorentz <nlorentz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-09-19 18:27:10 +0000

    Added gfx.hidd baseclass and moved x11gfx.hidd to config/x11/hidd/
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4689 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/hidds/gfx_intern.h
+++ workbench/hidds/gfx_intern.h
@@ -1,5 +1,12 @@
-#ifndef X11GFX_INTERN_H
-#define X11GFX_INTERN_H
+#ifndef GFX_INTERN_H
+#define GFX_INTERN_H
+/*
+    (C) 1997 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: X11 gfx HIDD for AROS.
+    Lang: English.
+*/
 
 #ifndef EXEC_TYPES_H
 #   include <exec/types.h>
@@ -17,7 +24,7 @@
 
 
 /* Library stuff */
-struct x11gfxbase
+struct gfxhiddbase
 {
     struct Library 	library;
     struct ExecBase	*sysbase;
@@ -28,18 +35,17 @@ struct x11gfxbase
     
     Class		*gfxclass;
     Class		*gcclass;
-    Class		*bitmapclass;
 	
 };
 
 
-Class *init_gfxclass (struct x11gfxbase *x11gfxbase);
-Class *init_gcclass(struct x11gfxbase *X11GfxBase);
-Class *init_bitmapclass(struct x11gfxbase *X11GfxBase);
-VOID cleanup_class(Class *class, struct x11gfxbase *X11GfxBase);
+static Class *init_gfxclass (struct gfxhiddbase *gfxhiddbase);
+static Class *init_gcclass(struct gfxhiddbase *);
+static VOID cleanup_gfxclass(Class *class, struct gfxhiddbase *);
+static VOID cleanup_gcclass(Class *class, struct gfxhiddbase *);
 
 
 #define expunge() \
-AROS_LC0(BPTR, expunge, struct x11gfxbase *, LIBBASE, 3, X11Gfx)
+AROS_LC0(BPTR, expunge, struct gfxhiddbase *, LIBBASE, 3, X11Gfx)
 
 #endif /* X11GFX_INTERN_H */
