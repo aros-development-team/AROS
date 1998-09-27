0e7ebb1982ddd05aaab5b6203a20886f4570156b
ldp <ldp@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-09-27 04:25:37 +0000

    New stuff.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4734 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/layers/layers_intern.h
+++ rom/layers/layers_intern.h
@@ -5,21 +5,90 @@
     Desc: Internal information for layers.library.
     Lang:
 */
-#ifndef LAYERS_INTERN_H
-#define LAYERS_INTERN_H
+#ifndef _LAYERS_INTERN_H_
+#define _LAYERS_INTERN_H_
 
+#include <exec/types.h>
+#include <exec/lists.h>
 #include <exec/libraries.h>
+#include <graphics/gfx.h>
+#include <graphics/rastport.h>
+#include <graphics/clip.h>
+#include <graphics/layers.h>
+#include <setjmp.h>
+
+#include "libdefs.h"
+
+/* Can these be safely removed ... */
 #include <aros/libcall.h>
 #include <dos/dos.h>
 #include <graphics/gfxbase.h>
+/* ... ??? */
 
 extern struct GfxBase * GfxBase;
 
-struct LayersBase
+struct LIBBASETYPE
 {
-    struct Library       LibNode;
-    struct ExecBase    * lb_SysBase;
-    BPTR                 lb_SegList;
+    struct Library   lb_LibNode;
+
+    struct Library * lb_GfxBase;
+    struct ExecBase *lb_SysBase;
 };
 
+struct LayerInfo_extra
+{
+#if 0
+    ULONG          lie_ReturnAddr;     // used by setjmp/longjmp, equals jmp_buf
+    ULONG          lie_Regs[12];       // D2-D7/A2-SP
+#else
+    jmp_buf        lie_JumpBuf;
 #endif
+    struct MinList lie_ResourceList;
+    UBYTE          lie_pad[4];
+};
+
+#define RD_REGION -1
+#define RD_BITMAP -2
+
+struct ResData
+{
+    void *ptr;
+    ULONG Size;
+};
+
+struct ResourceNode
+{
+    struct Node	    rn_Link;
+    struct ResData *rn_FirstFree;
+    LONG            rn_FreeCnt;
+    struct ResData  rn_Data[48];
+};
+
+struct InternalClipRect
+{
+    struct ClipRect  *Next;
+    struct ClipRect  *prev;
+    struct Layer     *lobs;		/* TRUE if the ClipRect has a BitMap */
+    struct BitMap    *BitMap;
+    struct Rectangle  bounds;
+    union cr_u
+    {
+	struct Rectangle r;		/* copy of Bounds */
+	struct cr_s
+	{
+	    struct ClipRect *NextCR;
+	    struct Layer    *_p2;
+	};
+    };
+    LONG	      reserved;
+    LONG	      Flags;
+};
+
+/* digulla again... Needed for close() */
+#define expunge() \
+ AROS_LC0(BPTR, expunge, struct LIBBASETYPE *, LIBBASE, 3, BASENAME)
+
+#define SysBase         LIBBASE->lb_SysBase
+#define GfxBase		LIBBASE->lb_GfxBase
+
+#endif /* _LAYERS_INTERN_H */
