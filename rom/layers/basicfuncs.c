025a90bf27ef2a08cb74005c6fa0f16f151e4ae7
ldp <ldp@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-09-27 04:19:16 +0000

    Committed a _very_ basic file. More routines will be added soon.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4733 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/layers/basicfuncs.c
@@ -0,0 +1,124 @@
+/*
+    (C) 1995-96 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: Basic support functions for layers.library.
+    Lang: English.
+*/
+
+#include <aros/config.h>
+#include <aros/asmcall.h>
+#include <exec/memory.h>
+#include <graphics/rastport.h>
+#include <graphics/clip.h>
+#include <graphics/regions.h>
+#include <graphics/layers.h>
+#include <graphics/gfx.h>
+#include <utility/hooks.h>
+#include <setjmp.h>
+
+#include <proto/exec.h>
+#include <proto/alib.h>
+#include <proto/graphics.h>
+#include <proto/layers.h>
+
+#include "layers_intern.h"
+#include "basicfuncs.h"
+
+#define DEBUG 1
+#include <aros/debug.h>
+#undef kprintf
+
+/*
+ *  Sections:
+ *
+ *  + Blitter
+ *  + Hook
+ *  + Layer
+ *  + LayerInfo
+ *  + Rectangle
+ *  + Miscellaneous
+ *
+ */
+
+/***************************************************************************/
+/*                                 BLITTER                                 */
+/***************************************************************************/
+
+#define CR2NR_NOBITMAP 0
+#define CR2NR_BITMAP   1
+
+#if !(AROS_FLAVOUR & AROS_FLAVOUR_NATIVE)
+/*
+ * These functions cause the infamous "fixed or forbidden register was spilled"
+ * bug/feature in m68k gcc, so these were written straight in asm. They can be
+ * found in config/m68k-native/layers, for the m68k AROSfA target. Other targets,
+ * that use stack passing, can use these versions.
+ */
+
+AROS_UFH4(void, _BltRPtoCR,
+    AROS_UFHA(struct RastPort *,   rp,         A0),
+    AROS_UFHA(struct ClipRect *,   cr,         A1),
+    AROS_UFHA(ULONG,               Mode,       D0),
+    AROS_UFHA(struct LayersBase *, LayersBase, A6))
+{
+    BltBitMap(rp->BitMap, cr->bounds.MinX, cr->bounds.MinY,
+	      cr->BitMap, cr->bounds.MinX & 0xf, 0,
+	      cr->bounds.MaxX - cr->bounds.MinX + 1,
+	      cr->bounds.MaxY - cr->bounds.MinY + 1,
+	      Mode, ~0, NULL);
+}
+
+AROS_UFH4(void, _BltCRtoRP,
+    AROS_UFHA(struct RastPort *,   rp,         A0),
+    AROS_UFHA(struct ClipRect *,   cr,         A1),
+    AROS_UFHA(ULONG,               Mode,       D0),
+    AROS_UFHA(struct LayersBase *, LayersBase, A6))
+{
+    BltBitMap(cr->BitMap, cr->bounds.MinX & 0xf, 0,
+	      rp->BitMap, cr->bounds.MinX, cr->bounds.MinY,
+	      cr->bounds.MaxX - cr->bounds.MinX + 1,
+	      cr->bounds.MaxY - cr->bounds.MinY + 1,
+	      Mode, ~0, NULL);
+}
+
+#endif /* if !native */
+
+/***************************************************************************/
+/*                                  HOOK                                   */
+/***************************************************************************/
+
+/* Will be filled in a bit later... */
+
+/***************************************************************************/
+/*                                 LAYER                                   */
+/***************************************************************************/
+
+/* Will be filled in a bit later... */
+
+/***************************************************************************/
+/*                               LAYERINFO                                 */
+/***************************************************************************/
+
+/* Will be filled in a bit later... */
+
+/***************************************************************************/
+/*                                RECTANGLE                                */
+/***************************************************************************/
+
+/* Will be filled in a bit later... */
+
+/***************************************************************************/
+/*                              MISCELLANEOUS                              */
+/***************************************************************************/
+
+/* Will be filled in a bit later... */
+
+
+
+/*-----------------------------------END-----------------------------------*/
+
+/***************************************************************************/
+/*                                                                         */
+/***************************************************************************/
+
