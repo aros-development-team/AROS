d34fc71aef9d2bf6dc569b09300c10b15b2b150d
ldp <ldp@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-10-19 20:42:15 +0000

    Obsolete
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4813 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/layers/layers_endtag.c
+++
@@ -1,8 +0,0 @@
-/*
-        (C) 1995-96 AROS - The Amiga Replacement OS
-        *** Automatic generated file. Do not edit ***
-        Desc: Resident endskip for Layers
-        Lang: english
-*/
-
-const char Layers_end = 0;
