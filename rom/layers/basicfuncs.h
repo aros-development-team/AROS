eaad885ad11fe987e0e7dc2d91101646055f9847
ldp <ldp@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-10-19 20:38:41 +0000

    Forgot some prototypes
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4811 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/layers/basicfuncs.h
+++ rom/layers/basicfuncs.h
@@ -1,1 +1,68 @@
+/*
+    (C) 1995-96 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Basic support functions for layers.library.
+    Lang: English.
+*/
+
+#include <aros/asmcall.h>
+
 #define _abcdef abcdef
+
+/*
+ *  Sections:
+ *
+ *  + Blitter
+ *  + Hook
+ *  + Layer
+ *  + LayerInfo
+ *  + Rectangle
+ *  + Miscellaneous
+ *
+ */
+
+/***************************************************************************/
+/*                                 BLITTER                                 */
+/***************************************************************************/
+
+AROS_UFP4(void, _BltRPtoCR,
+    AROS_UFPA(struct RastPort *,   rp,         A0),
+    AROS_UFPA(struct ClipRect *,   cr,         A1),
+    AROS_UFPA(ULONG,               Mode,       D0),
+    AROS_UFPA(struct LayersBase *, LayersBase, A6));
+
+AROS_UFP4(void, _BltCRtoRP,
+    AROS_UFPA(struct RastPort *,   rp,         A0),
+    AROS_UFPA(struct ClipRect *,   cr,         A1),
+    AROS_UFPA(ULONG,               Mode,       D0),
+    AROS_UFPA(struct LayersBase *, LayersBase, A6));
+
+/***************************************************************************/
+/*                                  HOOK                                   */
+/***************************************************************************/
+
+/***************************************************************************/
+/*                                 LAYER                                   */
+/***************************************************************************/
+
+/***************************************************************************/
+/*                               LAYERINFO                                 */
+/***************************************************************************/
+
+AROS_UFP2(BOOL, _AllocExtLayerInfo,
+    AROS_UFPA(struct Layer_Info *, li,         A0),
+    AROS_UFPA(struct LayersBase *, LayersBase, A6));
+
+void _FreeExtLayerInfo(struct Layer_Info *li, struct LayersBase *LayersBase);
+
+/***************************************************************************/
+/*                                RECTANGLE                                */
+/***************************************************************************/
+
+/***************************************************************************/
+/*                              MISCELLANEOUS                              */
+/***************************************************************************/
+
+
+/*-----------------------------------END-----------------------------------*/
