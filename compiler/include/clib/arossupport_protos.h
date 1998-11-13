60f0709a8f91faf372d11503a222eff10a245545
indy <indy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-11-13 18:53:10 +0000

    don't use SysBase->DebugData on AmigaOS
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4861 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/clib/arossupport_protos.h
+++ compiler/include/clib/arossupport_protos.h
@@ -27,11 +27,11 @@
 
 #ifdef DEBUG_FreeMem
 #   ifndef PROTO_EXEC_H
-#	include <proto/exec.h>
+#       include <proto/exec.h>
 #   endif
 #   if DEBUG_FreeMem
-#	undef FreeMem
-#	define FreeMem NastyFreeMem
+#       undef FreeMem
+#       define FreeMem NastyFreeMem
 #   endif
 #endif
 
@@ -40,26 +40,26 @@ extern struct ExecBase * Sysbase;
 /*
     Prototypes
 */
-ULONG	CalcChecksum (APTR mem, ULONG size);
-int	kprintf      (const UBYTE * fmt, ...);
-void	NastyFreeMem (APTR mem, ULONG size);
-APTR	RemoveSList  (APTR * list, APTR node);
-void	hexdump      (const void * data, IPTR offset, ULONG count);
-int	strrncasecmp (const char *, const char *, int);
-void	RawPutChars  (const UBYTE * string, int len);
-BOOL	IsDosEntryA  (char *Name, ULONG Flags);
+ULONG   CalcChecksum (APTR mem, ULONG size);
+int     kprintf      (const UBYTE * fmt, ...);
+void    NastyFreeMem (APTR mem, ULONG size);
+APTR    RemoveSList  (APTR * list, APTR node);
+void    hexdump      (const void * data, IPTR offset, ULONG count);
+int     strrncasecmp (const char *, const char *, int);
+void    RawPutChars  (const UBYTE * string, int len);
+BOOL    IsDosEntryA  (char *Name, ULONG Flags);
 
 /* AROS enhancements */
-BOOL ReadByte	 (struct Hook *, UBYTE  * dataptr, void * stream);
-BOOL ReadWord	 (struct Hook *, UWORD  * dataptr, void * stream);
-BOOL ReadLong	 (struct Hook *, ULONG  * dataptr, void * stream);
-BOOL ReadFloat	 (struct Hook *, FLOAT  * dataptr, void * stream);
+BOOL ReadByte    (struct Hook *, UBYTE  * dataptr, void * stream);
+BOOL ReadWord    (struct Hook *, UWORD  * dataptr, void * stream);
+BOOL ReadLong    (struct Hook *, ULONG  * dataptr, void * stream);
+BOOL ReadFloat   (struct Hook *, FLOAT  * dataptr, void * stream);
 BOOL ReadDouble  (struct Hook *, DOUBLE * dataptr, void * stream);
 BOOL ReadString  (struct Hook *, STRPTR * dataptr, void * stream);
 BOOL ReadStruct  (struct Hook *, APTR   * dataptr, void * stream, const IPTR * desc);
-BOOL WriteByte	 (struct Hook *, UBYTE  data, void * stream);
-BOOL WriteWord	 (struct Hook *, UWORD  data, void * stream);
-BOOL WriteLong	 (struct Hook *, ULONG  data, void * stream);
+BOOL WriteByte   (struct Hook *, UBYTE  data, void * stream);
+BOOL WriteWord   (struct Hook *, UWORD  data, void * stream);
+BOOL WriteLong   (struct Hook *, ULONG  data, void * stream);
 BOOL WriteFloat  (struct Hook *, FLOAT  data, void * stream);
 BOOL WriteDouble (struct Hook *, DOUBLE data, void * stream);
 BOOL WriteString (struct Hook *, STRPTR data, void * stream);
@@ -67,6 +67,9 @@ BOOL WriteStruct (struct Hook *, APTR   data, void * stream, const IPTR * desc);
 void FreeStruct  (APTR s,  const IPTR * desc);
 
 
+/* don't use SysBase->DebugData on AmigaOS */
+#ifndef __amigaos__
 #define kprintf     (((struct AROSSupportBase *)(SysBase->DebugData))->kprintf)
+#endif
 
 #endif /* CLIB_AROSSUPPORT_PROTOS_H */
