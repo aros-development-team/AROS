88bfe75330c50df8d318ed39ce396697437c7ca6
indy <indy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-11-13 18:58:54 +0000

    test for new methods of GCClass
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4865 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/HiddGraphics/GCDrawText.c
@@ -0,0 +1,220 @@
+/*
+    (C) 1998 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Test for graphics.hidd
+    Lang: english
+*/
+
+/*****************************************************************************
+
+    NAME
+
+        GCDrawText
+
+    SYNOPSIS
+
+        GCDrawText HIDD/K,WIDTH/N/K,HEIGHT/N/K,DEPTH/N/K,CHUNKY/S,DISPLAYABLE=DP/S
+
+    LOCATION
+
+        test/HiddGraphics
+
+    FUNCTION
+
+        Creates a gc, draws some texts and disposes the gc.
+
+    INPUTS
+        HIDD   - name of the hidd to use e.g. "graphics-X11.hidd"
+                 (default: graphics.hidd)
+        WIDTH  - width of bitmap (default: 320)
+        HEIGHT - height of bitmap (default: 200)
+        DEPTH  - depth of bitmap (default: 8)
+        CHUNKY - create bitmap in chunky-mode (default: planar)
+        DISPLAYABLE - show bitmap (default: FALSE)
+
+    RESULT
+        RETURN_OK    - hidd works
+        RETURN_ERROR - hidd produce errors
+        RETURN_FAIL  - could not test hidd i.e. OpenLibrary() fails
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+
+#define AROS_USE_OOP
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <aros/config.h>
+
+#include <exec/types.h>
+
+#include <proto/exec.h>
+#include <proto/graphics.h>
+#include <proto/diskfont.h>
+#include <proto/dos.h>
+#include <proto/oop.h>
+#include <proto/utility.h>
+
+#include <graphics/text.h>
+#include <utility/tagitem.h>
+
+#include <oop/oop.h>
+#include <hidd/graphics.h>
+#include <hidd/graphics-amiga-intuition.h>
+
+#include "gfxhiddtool.h"
+
+#undef  SDEBUG
+#undef  DEBUG
+#define DEBUG 1
+#include <aros/debug.h>
+
+struct DosLibrary    *DOSBase;
+struct Library       *OOPBase;
+struct Library       *HIDDGraphicsBase;
+struct Library       *DiskfontBase;
+struct GfxBase       *GfxBase;
+
+struct ght_OpenLibs LibsArray[] =
+{
+    GHT_LIB("dos.library"      , 37, &DOSBase),
+    GHT_LIB(AROSOOP_NAME       ,  0, &OOPBase),
+    GHT_LIB("diskfont.library" , 37, &DiskfontBase),
+    GHT_LIB("graphics.library" , 37, &GfxBase),
+    GHT_LIB(NULL               ,  0, NULL)
+};
+/***************************************************************/
+
+int main(int argc, char **argv)
+{
+    ULONG ret = RETURN_FAIL;
+
+    AttrBase HiddGCAttrBase;
+    AttrBase HiddGfxAttrBase;
+    AttrBase HiddBitMapAttrBase;
+
+    Object   *gfxHidd;
+    Object   *bitMap;
+    Object   *gc;
+
+    STRPTR hiddName = "graphics.hidd";
+    ULONG  width    = 320;
+    ULONG  height   = 200;
+    ULONG  depth    = 8;
+    ULONG  format   = vHIDD_BitMap_Format_Planar;
+    char   wait;
+
+    struct TextFont *font;
+    struct TextAttr attr  = {"ruby.font", 12, 0, 0};
+
+    struct Args
+    {
+        STRPTR hiddName;
+        IPTR   *width;
+        IPTR   *height;
+        IPTR   *depth;
+        IPTR   *chunky;
+        ULONG  displayable;
+    };
+
+    struct Args args = {hiddName, &width, &height, &depth, 0, 0};
+    struct RDArgs *rda;
+
+
+    if(ght_OpenLibs(LibsArray))
+    {
+        rda = ReadArgs("HIDD/K,WIDTH/N/K,HEIGHT/N/K,DEPTH/N/K,CHUNKY/S,DISPLAYABLE=DP/S", (IPTR *)&args, NULL);
+        if (rda != NULL)
+        {
+            if(args.chunky      != 0) format           = vHIDD_BitMap_Format_Chunky;
+            if(args.displayable != 0) args.displayable = (ULONG) TRUE;
+
+            HIDDGraphicsBase = OpenLibrary(args.hiddName, 0);
+            if(HIDDGraphicsBase)
+            {
+                ret = RETURN_ERROR;
+
+                HiddGfxAttrBase    = ObtainAttrBase(IID_Hidd_Gfx);
+                HiddBitMapAttrBase = ObtainAttrBase(IID_Hidd_BitMap);
+                HiddGCAttrBase     = ObtainAttrBase(IID_Hidd_GC);
+        
+                if(HiddGfxAttrBase && HiddBitMapAttrBase && HiddGCAttrBase)
+                {
+                    gfxHidd = NewObject(NULL, args.hiddName, NULL);
+                    if(gfxHidd)
+                    {
+                        struct TagItem bm_tags[] =
+                        {
+                            {aHidd_BitMap_Width,       (IPTR) *args.width},
+                            {aHidd_BitMap_Height,      (IPTR) *args.height},
+                            {aHidd_BitMap_Depth,       (IPTR) *args.depth},
+                            {aHidd_BitMap_Format,      (IPTR) format},
+                            {aHidd_BitMap_Displayable, (IPTR) args.displayable},
+                            {TAG_DONE, 0UL}
+                        };
+    
+                        bitMap = HIDD_Gfx_NewBitMap(gfxHidd, bm_tags);
+                        if(bitMap)
+                        {
+                            struct TagItem gc_tags[] =
+                            {
+                                {aHidd_GC_BitMap,     (IPTR) bitMap},
+                                {TAG_DONE, 0UL}
+                            };
+        
+                            gc = HIDD_Gfx_NewGC(gfxHidd, vHIDD_Gfx_GCType_Quick, gc_tags);
+                            if(gc)
+                            {
+                                font = OpenDiskFont(&attr);
+                                if(font)
+                                {
+                                    SetAttrsTags(gc, aHidd_GC_Font, (IPTR) font, TAG_END);
+                                    HIDD_GC_DrawText(gc, 10, 30, "AROS - The Amiga Research OS", 28);
+
+                                    printf("Press enter to continue");
+                                    scanf("%c", &wait);
+
+                                    CloseFont(font);
+                                }
+
+                                ret = RETURN_OK;
+                            }
+        
+                            HIDD_Gfx_DisposeBitMap(gfxHidd, bitMap);
+                        }
+    
+                        if(gfxHidd) DisposeObject(gfxHidd);
+                    }
+                }  /* if(HiddGfxAttrBase && HiddBitMapAttrBase && HiddGCAttrBase) */
+    
+                if(HiddGfxAttrBase)    ReleaseAttrBase(IID_Hidd_Gfx);
+                if(HiddBitMapAttrBase) ReleaseAttrBase(IID_Hidd_BitMap);
+                if(HiddGCAttrBase)     ReleaseAttrBase(IID_Hidd_GC);
+
+                CloseLibrary(HIDDGraphicsBase);
+            } /* if(HIDDGraphicsBase) */
+            FreeArgs(rda);
+        }
+        else
+        {
+           PrintFault(IoErr(), "");
+        }  /* if (rda != NULL) */
+    } /* if OpenLibs() */
+
+    ght_CloseLibs(LibsArray);
+
+    return(ret);
+}
