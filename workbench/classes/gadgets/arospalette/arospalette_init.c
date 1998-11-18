560ba5f54e08d9d457aa2ea00b4ef172c02844c0
nlorentz <nlorentz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-11-18 20:17:54 +0000

    converted to c_lib, and simplified makefile using new %genwblib template
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4880 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/classes/gadgets/arospalette/arospalette_init.c
+++ workbench/classes/gadgets/arospalette/arospalette_init.c
@@ -6,268 +6,110 @@
     Lang: English.
 */
 
-#include <proto/intuition.h>
-#include "initstruct.h"
-#include "arospalette_intern.h"
-#include "libdefs.h"
 #include <stddef.h>
-#include <exec/libraries.h>
 #include <exec/types.h>
-#include <exec/resident.h>
-#include <aros/libcall.h>
 #include <proto/exec.h>
+#include <proto/intuition.h>
+#include "arospalette_intern.h"
 
-#define INIT AROS_SLIB_ENTRY(init, BASENAME)
-
-struct inittable;
-extern const char name[];
-extern const char version[];
-extern const APTR inittabl[4];
-extern void *const LIBFUNCTABLE[];
-extern const struct inittable datatable;
-extern struct PaletteBase_intern *INIT();
-extern struct PaletteBase_intern *AROS_SLIB_ENTRY(open,BASENAME)();
-extern BPTR AROS_SLIB_ENTRY(close,BASENAME)();
-extern BPTR AROS_SLIB_ENTRY(expunge,BASENAME)();
-extern int AROS_SLIB_ENTRY(null,BASENAME)();
-extern const char LIBEND;
-
-/* FIXME: egcs 1.1b and possibly other incarnations of gcc have
- * two nasty problems with entry() that prevents using the
- * C version of this function on AROS 68k native.
- *
- * First of all, if inlining is active (-O3), the optimizer will decide
- * that entry() is simple enough to be inlined, and it doesn't generate
- * its code until all other functions have been compiled. Delaying asm
- * output for a global (non static) function is probably silly because
- * the optimizer can't eliminate its stand alone istance anyway.
- *
- * The second problem is that even without inlining, the code generator
- * adds a nop instruction immediately after rts. This is probably done
- * to help the 68040/60 pipelines, but it adds two more bytes before the
- * library resident tag, which causes all kinds of problems on native
- * AmigaOS.
- *
- * The workaround is to embed the required assembler instructions
- * (moveq #-1,d0 ; rts) in a constant variable.
- */
-#if (defined(__mc68000__) && (AROS_FLAVOUR & AROS_FLAVOUR_NATIVE))
-const LONG entry = 0x70FF4E75;
-#else
-int entry(void)
-{
-    /* If the library was executed by accident return error code. */
-    return -1;
-}
-#endif
-
-const struct Resident resident=
-{
-    RTC_MATCHWORD,
-    (struct Resident *)&resident,
-    (APTR)&LIBEND,
-    RTF_AUTOINIT,
-    VERSION_NUMBER,
-    NT_TYPE,
-    0,	/* WARNING: residents with negative priority won't be inited on AmigaOS! */
-    (char *)name,
-    (char *)&version[6],
-    (ULONG *)inittabl
-};
-
-const char name[]=NAME_STRING;
-
-const char version[]=VERSION_STRING;
-
-const APTR inittabl[4]=
-{
-    (APTR)sizeof(struct PaletteBase_intern),
-    (APTR)LIBFUNCTABLE,
-    (APTR)&datatable,
-    &INIT
-};
-
-struct inittable
-{
-    S_CPYO(1,1,B);
-    S_CPYO(2,1,L);
-    S_CPYO(3,1,B);
-    S_CPYO(4,1,W);
-    S_CPYO(5,1,W);
-    S_CPYO(6,1,L);
-    S_END (LIBEND);
-};
-
-#define O(n) offsetof(struct PaletteBase_intern,n)
-
-const struct inittable datatable=
-{
-    { { I_CPYO(1,B,O(library.lib_Node.ln_Type)), { NT_TYPE } } },
-    { { I_CPYO(1,L,O(library.lib_Node.ln_Name)), { (IPTR)name } } },
-    { { I_CPYO(1,B,O(library.lib_Flags       )), { LIBF_SUMUSED|LIBF_CHANGED } } },
-    { { I_CPYO(1,W,O(library.lib_Version     )), { VERSION_NUMBER } } },
-    { { I_CPYO(1,W,O(library.lib_Revision    )), { REVISION_NUMBER } } },
-    { { I_CPYO(1,L,O(library.lib_IdString    )), { (IPTR)&version[6] } } },
-  I_END ()
-};
 
 /* Global IntuitionBase */
 #ifdef GLOBAL_INTUIBASE
 struct IntuitionBase *IntuitionBase;
 #endif
 
-/* #undef O
-#undef SysBase */
 
+#undef SysBase
 
-AROS_LH2(struct PaletteBase_intern *, init,
-    AROS_LHA(struct PaletteBase_intern *, LIBBASE, D0),
-    AROS_LHA(BPTR,               segList,   A0),
-    struct ExecBase *, sysBase, 0, BASENAME)
-{
-    AROS_LIBFUNC_INIT
+/* Customize libheader.c */
+#define LC_SYSBASE_FIELD(lib)   (((LIBBASETYPEPTR       )(lib))->sysbase)
+#define LC_SEGLIST_FIELD(lib)   (((LIBBASETYPEPTR       )(lib))->seglist)
+#define LC_LIBBASESIZE          sizeof(LIBBASETYPE)
+#define LC_LIBHEADERTYPEPTR     LIBBASETYPEPTR
+#define LC_LIB_FIELD(lib)       (((LIBBASETYPEPTR)(lib))->library)
 
-    /* This function is single-threaded by exec by calling Forbid. */
+#define LC_NO_OPENLIB
+#define LC_NO_CLOSELIB
 
-    SysBase=sysBase;
+/* to avoid removing the gfxhiddclass from memory add #define NOEXPUNGE */
 
-    LIBBASE->seglist=segList;
+#include <libcore/libheader.c>
 
-    /* You would return NULL here if the init failed. */
-    return LIBBASE;
-    AROS_LIBFUNC_EXIT
-}
+#undef  SDEBUG
+#undef  DEBUG
+#define DEBUG 0
+#include <aros/debug.h>
 
-/* Use This from now on * /
-#define SysBase LIBBASE->sysbase */
+#define SysBase      (LC_SYSBASE_FIELD(lh))
+
+#define AROSPaletteBase lh
 
 /* Predeclaration */
 struct IClass *InitPaletteClass(struct PaletteBase_intern *);
 
-AROS_LH1(struct PaletteBase_intern *, open,
-    AROS_LHA(ULONG, version, D0),
-    struct PaletteBase_intern *, LIBBASE, 1, BASENAME)
+ULONG SAVEDS STDARGS LC_BUILDNAME(L_InitLib) (LC_LIBHEADERTYPEPTR lh)
 {
-    AROS_LIBFUNC_INIT
-    /*
-	This function is single-threaded by exec by calling Forbid.
-	If you break the Forbid() another task may enter this function
-	at the same time. Take care.
-    */
 
-
-    /* Keep compiler happy */
-    version=0;
 
     if (!GfxBase)
     	GfxBase = (GraphicsBase *)OpenLibrary("graphics.library", 37);
     if (!GfxBase)
 	return(NULL);
 
+kprintf("apal: gfx inited\n");
     if (!UtilityBase)
 	UtilityBase = OpenLibrary("utility.library", 37);
     if (!UtilityBase)
 	return(NULL);
-
+kprintf("apal: utility inited\n");
     if (!IntuitionBase)
     	IntuitionBase = (IntuiBase *)OpenLibrary("intuition.library", 37);
     if (!IntuitionBase)
 	return (NULL);
-
+kprintf("apal: intui inited\n");
 
     /* ------------------------- */
     /* Create the class itself */
 
-    LIBBASE->classptr = InitPaletteClass(LIBBASE);
-    if (!LIBBASE->classptr)
+    if (!lh->classptr)
+    	lh->classptr = InitPaletteClass(lh);
+    if (!lh->classptr)
     	return (NULL);
-
+kprintf("apal: class inited\n");
     /* ------------------------- */
 
+    /* You would return NULL if the init failed. */
+    return TRUE;
 
-    /* I have one more opener. */
-    LIBBASE->library.lib_OpenCnt++;
-    LIBBASE->library.lib_Flags&=~LIBF_DELEXP;
-
-    /* You would return NULL if the open failed. */
-    return LIBBASE;
-    AROS_LIBFUNC_EXIT
 }
 
-AROS_LH0(BPTR, close, struct PaletteBase_intern *, LIBBASE, 2, BASENAME)
-{
-    AROS_LIBFUNC_INIT
-    /*
-	This function is single-threaded by exec by calling Forbid.
-	If you break the Forbid() another task may enter this function
-	at the same time. Take care.
-    */
 
-    /* I have one fewer opener. */
-    if(!--LIBBASE->library.lib_OpenCnt)
+VOID SAVEDS STDARGS LC_BUILDNAME(L_ExpungeLib) (LC_LIBHEADERTYPEPTR lh)
+{
+    if (lh->classptr)
     {
-	if (LIBBASE->classptr)
-	{
-	    RemoveClass(LIBBASE->classptr);
-	    FreeClass(LIBBASE->classptr);
-	    LIBBASE->classptr = NULL;
-	}
-
-	CloseLibrary(UtilityBase);
-	UtilityBase = NULL;
-
-	CloseLibrary((struct Library *)GfxBase);
-	GfxBase = NULL;
-
-	CloseLibrary((struct Library *)IntuitionBase);
-	IntuitionBase = NULL;
-
-	/* Delayed expunge pending? */
-	if(LIBBASE->library.lib_Flags&LIBF_DELEXP)
-	    /* Then expunge the library */
-	    return expunge();
+	RemoveClass(lh->classptr);
+	FreeClass(lh->classptr);
+	lh->classptr = NULL;
     }
-    return 0;
-    AROS_LIBFUNC_EXIT
+    kprintf("apal: class removed\n");
+
+    /* CloseLibrary() checks for NULL-pointers */
+    CloseLibrary(UtilityBase);
+    UtilityBase = NULL;
+    
+    kprintf("apal: utility closed\n");
+
+    CloseLibrary((struct Library *)GfxBase);
+    GfxBase = NULL;
+    
+    kprintf("apal: gfx closed\n");
+    
+    CloseLibrary((struct Library *)IntuitionBase);
+    IntuitionBase = NULL;
+    kprintf("apal: intui closed\n");
+    
+    return;
+
 }
-
-AROS_LH0(BPTR, expunge, struct PaletteBase_intern *, LIBBASE, 3, BASENAME)
-{
-    AROS_LIBFUNC_INIT
-
-    BPTR ret;
-    /*
-	This function is single-threaded by exec by calling Forbid.
-	Never break the Forbid() or strange things might happen.
-    */
-
-    /* Test for openers. */
-    if(LIBBASE->library.lib_OpenCnt)
-    {
-	/* Set the delayed expunge flag and return. */
-	LIBBASE->library.lib_Flags|=LIBF_DELEXP;
-	return 0;
-    }
-
-    /* Get rid of the library. Remove it from the list. */
-    Remove(&LIBBASE->library.lib_Node);
-
-    /* Get returncode here - FreeMem() will destroy the field. */
-    ret=LIBBASE->seglist;
-
-    /* Free the memory. */
-    FreeMem((char *)LIBBASE-LIBBASE->library.lib_NegSize,
-	LIBBASE->library.lib_NegSize+LIBBASE->library.lib_PosSize);
-
-    return ret;
-    AROS_LIBFUNC_EXIT
-}
-
-AROS_LH0I(int, null, struct PaletteBase_intern *, LIBBASE, 4, BASENAME)
-{
-    AROS_LIBFUNC_INIT
-    return 0;
-    AROS_LIBFUNC_EXIT
-}
-
 
