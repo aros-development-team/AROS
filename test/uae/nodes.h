9abd8e619f1828805a013dd51c12dc3154844767
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-11-20 16:31:47 +0000

    Complete test case
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4885 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/uae/nodes.h
@@ -0,0 +1,173 @@
+#ifndef EXEC_NODES_H
+#define EXEC_NODES_H
+/* (C) 1995 AROS - The Amiga Research OS */
+
+/******************************************************************************
+
+    MODUL
+	$Id$
+
+    DESCRIPTION
+	Header-file for nodes.
+
+******************************************************************************/
+
+/**************************************
+		Includes
+**************************************/
+/* #ifndef AROS_CONFIG_H
+#   include <aros/config.h>
+#endif
+#ifndef EXEC_TYPES_H
+#   include <exec/types.h>
+#endif */
+#include "types.h"
+
+class NodePtr : APTR
+{
+public:
+    /* Here I overload the -> (dereference) operator. When you dereferenece
+       a NodePtr, then you get a struct Node *. Here is how it works:
+
+	    NodePtr nptr;
+
+	    ... give nptr a valid value ...
+
+	    name = nptr->ln_Name;
+
+	The C++ compiler creates this code for the last line:
+
+	    struct Node * temp = nptr.operator-> ();
+	    name = temp->ln_Name;
+
+	This means that you can use any field name which is valid after
+	"struct Node *" after an NodePtr.
+    */
+    inline struct Node * operator -> ()
+    {
+	return (struct Node *) ntohl (data);
+    }
+
+    /* Convert a NodePtr to a Node */
+    inline operator struct Node * ()
+    {
+	return (struct Node *) ntohl (data);
+    }
+
+    /* Convert it to void * */
+    inline operator void * ()
+    {
+	return (struct Node *) ntohl (data);
+    }
+
+    /* Create a NodePtr from a struct Node pointer. */
+    inline NodePtr (struct Node * v)
+    {
+	data = htonl ((long)v);
+    }
+
+    inline NodePtr ()
+    {
+	return;
+    }
+};
+
+/* Pretty much the same but this time for MinNodePtr */
+class MinNodePtr : APTR
+{
+public:
+    inline struct MinNode * operator -> ()
+    {
+	return (struct MinNode *) ntohl (data);
+    }
+
+    inline MinNodePtr (struct MinNode * v)
+    {
+	data = htonl ((long)v);
+    }
+
+    inline MinNodePtr ()
+    {
+	return;
+    }
+};
+
+/**************************************
+	       Structures
+**************************************/
+
+#if (AROS_FLAVOUR & AROS_FLAVOUR_BINCOMPAT)
+struct Node
+{
+    NodePtr ln_Succ,
+	    ln_Pred;
+    UBYTE   ln_Type;
+    BYTE    ln_Pri;
+    /* AROS: pointer should be 32bit aligned, but we can not do this on
+       the native machine because of binary compatibility.
+    */
+    STRPTR  ln_Name;
+
+public:
+    inline Node () { return; }
+};
+
+#else
+struct Node
+{
+    NodePtr ln_Succ,
+	    ln_Pred;
+    /* AROS: pointer should be 32bit aligned */
+    STRPTR  ln_Name;
+    UBYTE   ln_Type;
+    BYTE    ln_Pri;
+
+public:
+    inline Node () { return; }
+};
+#endif /* AROS_FLAVOUR */
+
+struct MinNode
+{
+    MinNodePtr mln_Succ,
+	       mln_Pred;
+
+public:
+    inline MinNode () { return; }
+};
+
+
+/**************************************
+		 Defines
+**************************************/
+/* Values for ln_Type */
+#define NT_UNKNOWN	0	/* Unknown node 			    */
+#define NT_TASK 	1	/* Exec task				    */
+#define NT_INTERRUPT	2	/* Interrupt				    */
+#define NT_DEVICE	3	/* Device				    */
+#define NT_MSGPORT	4	/* Message-Port 			    */
+#define NT_MESSAGE	5	/* Indicates message currently pending	    */
+#define NT_FREEMSG	6
+#define NT_REPLYMSG	7	/* Message has been replied		    */
+#define NT_RESOURCE	8
+#define NT_LIBRARY	9
+#define NT_MEMORY	10
+#define NT_SOFTINT	11	/* Internal flag used by SoftInits	    */
+#define NT_FONT 	12
+#define NT_PROCESS	13	/* AmigaDOS Process			    */
+#define NT_SEMAPHORE	14
+#define NT_SIGNALSEM	15	/* signal semaphores			    */
+#define NT_BOOTNODE	16
+#define NT_KICKMEM	17
+#define NT_GRAPHICS	18
+#define NT_DEATHMESSAGE 19
+
+#define NT_USER 	254	/* User node types work down from here	    */
+#define NT_EXTENDED	255
+
+
+/******************************************************************************
+*****  ENDE exec/nodes.h
+******************************************************************************/
+
+#endif /* EXEC_NODES_H */
--- test/uae/types.h
+++ test/uae/types.h
@@ -1,3 +1,5 @@
+#ifndef EXEC_TYPES_H
+#define EXEC_TYPES_H
 
 #include <netinet/in.h>
 
@@ -6,46 +8,57 @@ typedef unsigned char UBYTE;
 
 class WORD
 {
-private:
+/* This is the data. It's defined in protected mode so the child classes
+   can directly access it. Note that the value is encoded in *big endian*. */
+protected:
     short data;
 
+/* Now the initializations and the conversions. They are defined in public
+   mode because everyone must be able to access them. */
 public:
+    /* Convert the data to int in host-endianess */
     inline operator int ()
     {
 	return (int) ntohs (data);
     }
 
+    /* Same for short */
     inline operator short ()
     {
 	return ntohs (data);
     }
 
+    /* Create a variable of type WORD from a short */
     inline WORD (short v)
     {
 	data = htons (v);
     }
 
+    /* Same but from an int */
     inline WORD (int v)
     {
-	printf ("WORD(int): v=%d\n", v);
+	//printf ("WORD(int): v=%d\n", v);
 	data = htons (((short)v));
-	printf ("WORD(int): data=%d\n", data);
+	//printf ("WORD(int): data=%d\n", data);
     }
 
+    /* How to copy a variable of type WORD */
     inline WORD (const WORD& v)
     {
 	data = v.data;
     }
 
+    /* How to create an uninitilized WORD variable */
     inline WORD ()
     {
 	return;
     }
 };
 
+/* This is pretty much the same but different types */
 class APTR
 {
-private:
+protected:
     long data;
 
 public:
@@ -66,9 +79,7 @@ public:
 
     inline APTR (void * v)
     {
-	printf ("APTR(void *): v=%p\n", v);
 	data = htonl (((long)v));
-	printf ("APTR(void *): data=%p\n", (void *)data);
     }
 
     inline APTR (const APTR& v)
@@ -80,5 +91,41 @@ public:
     {
 	return;
     }
+
+#ifdef DEBUG
+    /* Debugging */
+    inline void print ()
+    {
+	printf ("%08lx", data);
+    }
+#endif
 };
+
+/* This is an example how to inhert the functionality for other
+   pointers. */
+class STRPTR : APTR
+{
+public:
+    inline operator char * ()
+    {
+	return (char *) ntohl (data);
+    }
+
+    inline operator const char * ()
+    {
+	return (const char *) ntohl (data);
+    }
+
+    inline STRPTR ()
+    {
+	return;
+    }
+
+    inline STRPTR (char * v)
+    {
+	data = htonl ((long)v);
+    }
+};
+
+#endif /* EXEC_TYPES_H */
 
