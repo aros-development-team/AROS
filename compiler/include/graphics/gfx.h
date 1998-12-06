7e994ca86edd30c3022c1b9bf67382183d9d31ae
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-12-06 02:30:09 +0000

    Added to AROS-specific flags for the BitMpa->Flags.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4911 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/graphics/gfx.h
+++ compiler/include/graphics/gfx.h
@@ -67,6 +67,12 @@ struct Rect32
 #define BMB_MINPLANES        4
 #define BMF_MINPLANES   (1L<<4)
 
+#define BMB_AROS_OLDWINDOW        6
+#define BMF_AROS_OLDWINDOW (1L << 6)
+
+#define BMB_AROS_DISPLAYED        7
+#define BMF_AROS_DISPLAYED (1L << 7)
+
 #define BMA_HEIGHT 0
 #define BMA_DEPTH  4
 #define BMA_WIDTH  8
