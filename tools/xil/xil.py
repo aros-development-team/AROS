0be782bab05d1af266405ea4d72e87495672da41
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1998-12-14 18:55:24 +0000

    Added comments.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@4924 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- tools/xil/xil.py
+++ tools/xil/xil.py
@@ -14,12 +14,12 @@ class Parser:
     the beginning of another string)."""
     stringdelimiters = ["'''", '"""', "'", '"']
 
-    EOF = 'EOF'
-    BEGIN = 'BEGIN'
-    END = 'END'
-    TEXT = 'TEXT'
-
     def __init__ (self, rf, wf, handlers, startLine=1):
+	"""Initialize a new instance of a XIL parser. When you run() the
+	new parser, it will read from rf and write the result to wf.
+	The handlers will be called for each tag in the input stream.
+	The startLine is used for error reports when a new parser is
+	create to parse the middle of a file."""
 	self.beginRE = re.compile ('(' + re.escape (self.begin) + ')', re.IGNORECASE)
 	self.endRE = re.compile ('(' + re.escape (self.end) + ')', re.IGNORECASE)
 	self.rf, self.wf = rf, wf
@@ -43,7 +43,9 @@ class Parser:
 	self.argNameEndPattern = re.compile ('\s+|' + re.escape (self.end) + '|=')
 
     def run (self):
+	"""Read the stream and execute for commands in it."""
 	while 1:
+	    # Read the text upto the next command or EOF
 	    text, eof = self.readText ()
 	    if text:
 		#print '<<<TEXT<<<',
@@ -51,14 +53,16 @@ class Parser:
 		#print '>>>TEXT>>>',
 	    if eof: break
 
+	    # We *must* have a command here. Read it.
 	    cmd, args = self.readCmd ()
 	    #print 'cmd=',cmd,args
-	    if not cmd: break
 
+	    # Find the handler for this command
 	    o = self.handlers.get (cmd, None)
 	    if not o:
 		self.error ('Unknown command %s in line %d\n' % (cmd, self.lineno))
 	    else:
+		# Check if this is a command or an environment handler.
 		if isinstance (o, CmdHandler):
 		    o.handle (self, args)
 		else:
@@ -66,6 +70,9 @@ class Parser:
 		    o.handle (self, args, body)
 
     def readText (self):
+	""" This method must read all the text upto the next command. The
+	start delimiter for the command must be left in the input stream."""
+
 	text = self.read ()
 	if not text:
 	    eof = 1
@@ -88,6 +95,9 @@ class Parser:
 	return text, eof
 
     def calcNewLines (str, start, end):
+	"""For return the number of lines between two positions in the
+	string."""
+
 	pos = start
 	count = 0
 	while 1:
@@ -99,6 +109,14 @@ class Parser:
 	return count
 
     def readCmd (self):
+	"""Parse a command. This method must return a string (the name of the
+	command) and the arguments. This implementation returns a dictionary
+	for the arguments. The key is the name of the argument and the value
+	is the value of the argument. But that's not neccessary. The 
+	arguments are passed on to the handlers which must be able to
+	process them. No other parts of Xil look at the arguments."""
+
+	# Read the command
 	text = self.read ()
 	#print 'text=|%s|'%`text[:20]`
 	if not text:
@@ -122,39 +140,53 @@ class Parser:
 	if cmd == self.end:
 	    self.error ('Missing command name in line %d\n' % self.lineno)
 
+	# Is this the end of the tag ?
 	if match.group (0) == self.end:
 	    self.yytext = text[0:pos]
 	    self.rest = text[pos:]
 	    return cmd, {}
 
+	# Read the arguments
 	args = {}
 	while 1:
+	    # Skip all whilespace
 	    pos = self.optSpacePattern.match (text, pos).end (0)
 
+	    # The name of the argument is terminated by a) whitespace,
+	    # b) the end delimiter or c) a '='
 	    #print '1',`string.strip (text[pos:pos+10])`
 	    match = self.argNameEndPattern.search (text, pos)
 	    if not match:
-		self.calcNewLines (text, 0, pos)
+		self.lineno = self.lineno + self.calcNewLines (text, 0, pos)
 		error ('Unterminated tag in line %d\n' % self.lineno)
 
 	    argname = string.upper (text[pos:match.start (0)])
 	    pos = match.end (0)
 	    #print 'argname=',argname
+	    # If the argument name is empty, we must have hit something else.
+	    # It can't be a space, because spaces have already been skipped
+	    # by the first operation in this loop
 	    if not argname:
 		if match.group (0) == '=':
-		    self.calcNewLines (text, 0, pos)
+		    self.lineno = self.lineno + self.calcNewLines (text, 0, pos)
 		    error ('Missing argument name in line %d\n' % self.lineno)
 		break
 	    
+	    # If a '=' follows, then there must be some value
 	    if match.group (0) == '=':
+		# Skip whitespace
 		pos = self.optSpacePattern.match (text, pos).end (0)
 
+		# Is the end delimiter the next thing in the input ?
 		if self.endRE.match (text, pos):
-		    self.calcNewLines (text, 0, pos)
+		    self.lineno = self.lineno + self.calcNewLines (text, 0, pos)
 		    self.error ('Missing value for argument %s in line %d\n', (argname, self.lineno))
 
+		# Is there a string delimiter ?
 		match = self.strDelimPattern.match (text, pos)
 		if not match:
+		    # If not, then the value of the argument is 
+		    # terminated by whitespace or the end delimiter.
 		    m2 = self.endRE.search (text, pos)
 		    match = self.nonSpacePattern.match (text, pos)
 		    if m2 and m2.start (0) < match.end (0):
@@ -164,43 +196,61 @@ class Parser:
 		    value = text[pos:end]
 		    pos = end
 		else:
+		    # It's a normal string. Check which delimiter is used
+		    # and then search for it.
 		    delim = match.group (0)
 		    pos = strstart = match.end (0)
-		    #print 'Found string delim=',delim,'value=',value
 		    epat = re.compile (re.escape (delim))
 		    match = epat.search (text, pos)
 		    if not match:
-			self.calcNewLines (text, 0, pos)
+			self.lineno = self.lineno + self.calcNewLines (text, 0, pos)
 			self.error ('Missing end for string starting in line %d\n', self.lineno)
 		    value = text[pos:match.start (0)]
 		    pos = match.end (0)
 	    else:
+		# No value
 		value = None
 
 	    #print 'found %s="%s"'%(argname,value)
 	    args[argname] = value
 
+	# self.yytext contains the complete command *unprocessed*.
 	self.yytext = text[0:pos]
 	self.rest = text[pos:]
 	#print 'yytext=',self.yytext
+
+	# Update the line number
+	self.lineno = self.lineno + self.calcNewLines (text, 0, pos)
 	return cmd, args
 
     def skipWhiteSpace (self):
+	"""This method can be called from the handlers to skip
+	obsolete white space"""
 	text = self.read ()
 	pos = self.optSpacePattern.match (text).end (0)
 	self.rest = text[pos:]
 
     def readBody (self, tag):
+	"""Read the body of an environment. Notice that an environment
+	must contain valid text (it must conform to the normal XIL
+	syntax)."""
 	text = cStringIO.StringIO ()
 	startline = self.lineno
 	level = 0
 	while 1:
+	    # Read the plain text
 	    part, eof = self.readText ()
 	    if eof:
 		self.error ('Missing end of env %s in line %d\n' % (tag.begin, startline))
 	    text.write (part)
+
+	    # Read the command.
 	    cmd, args = self.readCmd ()
 	    #print cmd
+
+	    # Check if the env nests. If yes, update a counter. The
+	    # code exist when the end of an end is read and the counter
+	    # is 0.
 	    if not cmd:
 		self.error ('Missing end of env %s in line %d\n' % (tag.begin, startline))
 	    if cmd == tag.end:
@@ -211,11 +261,18 @@ class Parser:
 	    elif cmd == tag.begin:
 		level = level + 1
 	    #print 'yytext=',self.yytext ()
+
+	    # yytext contains the unprocessed tag which was read by
+	    # readCmd()
 	    text.write (self.yytext)
 
 	return text.getvalue ()
 	    
     def read (self):
+	"""This is called if more input is needed. In this implementation,
+	all available data is read at once to speed up the parsing.
+	When some other method needs data, it calls this and puts all
+	data it didn't process into self.rest."""
 	if self.rest:
 	    line = self.rest
 	    self.rest = ''
@@ -226,27 +283,54 @@ class Parser:
 	return line
 
     def error (self, msg):
+	"""Call this in case of an error"""
 	raise ParseError, msg
 
     def write (self, str):
+	"Write something to the output stream"""
 	self.wf.write (str)
 
     def writeline (self, str):
+	"Write a line to the output stream. This also writes an EOL to the
+	stream."""
 	self.wf.write (str)
 	self.wf.write ('\n')
 
 class CmdHandler:
+    """This is a handler for commands. Commands only take arguments, no
+    text."""
     def __init__ (self, name='dummy'):
+	"""Initialize the command. This must set self.name to some 
+	reasonable value."""
 	self.name = string.upper (name)
 
     def handle (self, parser, args):
+	"""Handle the command. parser is the parser instance which
+	has read the command. The command is invoked immediately
+	and the input stream points to the first character after the
+	command. You may read from the stream and you can invoke
+	methods of the parser. args contains the arguments which were
+	found in the tag. The type of this depends on the parser which
+	is used."""
 	print self.name,`args`,
 
 class EnvHandler:
     def __init__ (self, begin='env', end='/env'):
+	"""Initialize the environment. This must set self.begin and self.end
+	to some reasonable value. self.end is used in the parser to
+	find the end of the environment."""
 	self.begin, self.end = string.upper (begin), string.upper (end)
 
     def handle (self, parser, args, text):
+	"""This is called when the environment has been found and the
+	text in it has been read. The parser doesn't process the text
+	in any way except that it searches for the end of the environment
+	in it. Since environments can nest, it's no good idea to read from
+	the input stream in this method. args contains the arguments
+	which were found by the parser and text is the unprocessed text
+	in the environment. If you want to process the text, you
+	*must* create a new parser instance. Use the StringIO modules
+	to convert the text into a stream."""
 	print self.begin,`args`,'text=<<<%s>>>'%text
 
 if __name__ == '__main__':
