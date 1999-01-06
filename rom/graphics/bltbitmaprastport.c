2babdbb5bd5b68c71fcba782520b45c05eecd901
nlorentz <nlorentz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-01-06 22:21:03 +0000

    Implemented function
    git-svn-id: https://svn.aros.org/svn/aros/trunk@5021 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/graphics/bltbitmaprastport.c
@@ -0,0 +1,61 @@
+/*
+    (C) 1997 AROS - The Amiga Research OS
+    $Id$
+
+    Desc:
+    Lang: english
+*/
+#include "graphics_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+#include <clib/graphics_protos.h>
+
+	void BltBitMapRastPort (
+
+/*  SYNOPSIS */
+	struct BitMap   * srcBitMap,
+	LONG              xSrc,
+	LONG              ySrc,
+	struct RastPort * destRP,
+	LONG              xDest,
+	LONG              yDest,
+	LONG              xSize,
+	LONG              ySize,
+	ULONG             minterm)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+	27-11-96    digulla automatically created from
+			    graphics_lib.fd and clib/graphics_protos.h
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+     
+    driver_BltBitMapRastPort(srcBitMap
+    	, xSrc, ySrc
+	, destRP
+	, xDest, yDest
+	, xSize, ySize
+	, minterm
+    );
+
+    AROS_LIBFUNC_EXIT
+} /* BltBitMapRastPort */
