4d36210080bb941b11903cddead521bb2b1bca60
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-01-09 23:37:57 +0000

    Implementation of that function. Not much to do...
    git-svn-id: https://svn.aros.org/svn/aros/trunk@5044 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/graphics/inittmpras.c
@@ -0,0 +1,66 @@
+/*
+    (C) 1995 AROS - The Amiga Research OS
+    $Id$	$Log
+
+    Desc: Graphics function InitRastPort()
+    Lang: english
+*/
+#include "graphics_intern.h"
+#include <graphics/rastport.h>
+#include <proto/exec.h>
+#include <proto/graphics.h>
+
+/*****************************************************************************
+
+    NAME */
+
+	AROS_LH3(void, InitTmpRas,
+
+/*  SYNOPSIS */
+	AROS_LHA(struct TmpRas  *, tmpras, A0),
+	AROS_LHA(void *          , buffer, A1),
+	AROS_LHA(ULONG           , size  , D0),
+
+/*  LOCATION */
+	struct GfxBase *, GfxBase, 78, Graphics)
+
+/*  FUNCTION
+	Initializes a TmpRas structure. The user has to conect the
+	TmpRas structure to the rastport.
+	Some routines need extra memory in order to be able to operate
+	properly.
+
+    INPUTS
+	tmpras - pointer to a TmpRas structure to be initialized
+	buffer - pointer to a piece of chip memory.
+	size   - size in bytes of buffer.
+
+    RESULT
+        Properly initilized TmpRas struture to link to RastPort structure
+        for use with functions like Flood(), Text() and AreaEnd().
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+        The function itself is a bug.
+        Why does this function exist at all? The necessary memory should 
+        be allocated in InitRastPort() or the functions that need it.
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+  AROS_LIBFUNC_INIT
+  AROS_LIBBASE_EXT_DECL(struct GfxBase *,GfxBase)
+
+  tmpras->RasPtr = buffer;
+  tmpras->Size   = size;
+
+  AROS_LIBFUNC_EXIT
+} /* InitTmpRas */
