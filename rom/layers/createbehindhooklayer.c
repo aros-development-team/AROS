a979002abf603d074db65b86002b29d6460e8c76
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-01-11 03:50:44 +0000

    Another update.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@5064 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/layers/createbehindhooklayer.c
+++ rom/layers/createbehindhooklayer.c
@@ -6,16 +6,13 @@
     Lang: english
 */
 #include <aros/libcall.h>
-
-#define DEBUG 0
-#include <aros/debug.h>
-#undef kprintf
+#include <proto/layers.h>
+#include <graphics/clip.h>
+#include <graphics/layers.h>
 
 /*****************************************************************************
 
     NAME */
-#include <proto/layers.h>
-#include "layers_intern.h"
 
 	AROS_LH9(struct Layer *, CreateBehindHookLayer,
 
@@ -55,12 +52,175 @@
 
 *****************************************************************************/
 {
-    AROS_LIBFUNC_INIT
-    AROS_LIBBASE_EXT_DECL(struct LayersBase *,LayersBase)
+  AROS_LIBFUNC_INIT
+  AROS_LIBBASE_EXT_DECL(struct LayersBase *,LayersBase)
+/*
+  D(bug("CreateBehindHookLayer(li@$lx, bm@$lx, x0 %ld, y0 %ld, x1 %ld, y1 %ld, flags %ld, hook@$%lx, bm2@$lx)\n", li, bm, x0, y0, x1, y1, flags, hook, bm2));
+*/
 
-    D(bug("CreateBehindHookLayer(li@$lx, bm@$lx, x0 %ld, y0 %ld, x1 %ld, y1 %ld, flags %ld, hook@$%lx, bm2@$lx)\n",
-	li, bm, x0, y0, x1, y1, flags, hook, bm2));
+  struct Layer * L;
+  struct ClipRect * CR;
+  struct RastPort * RP;
 
-    return NULL;
-    AROS_LIBFUNC_EXIT
+  L  = (struct Layer    *) AllocMem(sizeof(struct Layer)   , MEMF_CLEAR|MEMF_PUBLIC);
+  CR = (struct ClipRect *) AllocMem(sizeof(struct ClipRect), MEMF_CLEAR|MEMF_PUBLIC);
+  RP = (struct RastPort *) AllocMem(sizeof(struct RastPort), MEMF_CLEAR|MEMF_PUBLIC);
+
+  /* is everything there that I need?  */
+  if (NULL != L && NULL != CR && NULL != RP)
+  {
+    struct Layer * L_front;
+    /* first of all we init. the layers structure */
+    L->ClipRect = CR;
+    L->rp       = RP;
+
+    L->bounds.MinX = x0;
+    L->bounds.MinY = y0;
+    L->bounds.MaxX = x1;
+    L->bounds.MaxY = y1;
+
+    L->Flags     = (WORD) flags;
+    L->LayerInfo = li;
+    L->Width     = x1-x0+1;
+    L->Height    = y1-y0+1;
+
+    InitSemaphore(&L->Lock);
+    LockLayer(0, L);
+
+    L->DamageList = NewRegion();
+
+    /*
+      L->priority = ;
+    */
+
+    /* and now init the ClipRect structure */
+    CR->bounds.MinX = x0;
+    CR->bounds.MinY = y0;
+    CR->bounds.MaxX = x1;
+    CR->bounds.MaxY = y1;
+
+    /* and now init the RastPort structure */
+    InitRastPort(RP);
+
+    RP->Layer  = L;
+    RP->BitMap = bm;
+
+    /* 
+       add the layer at the correct position in the linked list of layers
+       non-BACKDROP layer: insert it before the first non-BACKDROP layer
+           BACKDROP layer: insert it at the very end of the list  
+    */
+   
+    L_front = li->top_layer;
+
+    /* Now nobody else may play around with the layers while I am
+     * doing this here 
+     */
+    LockLayers(li);
+
+    if ((NULL == L_front) ||
+        ( (0 != (L_front->Flags & LAYERBACKDROP)) && 
+          (0 == (flags & LAYERBACKDROP) ) 
+        ))
+    {
+kprintf("CreateBehindLayer: New layer is first one.\n");
+      /* the new one is the very first one in the list */
+      if (NULL != li->top_layer)
+      {
+        L ->back             = li->top_layer;
+        li->top_layer->front = L;
+      }
+      /* make the new layer the top layer */
+      li->top_layer          = L; 
+    }
+    else
+    {
+      /* search for the exact place in the list */
+      if (0 != (flags & LAYERBACKDROP))
+      {
+        /* 
+          for a BACKDROP layer we have to go to the very end of the list
+         */
+        while (NULL != L_front->back)
+          L_front = L_front ->back;
+        L_front->back = L;
+        L -> front    = L_front;
+      }
+      else
+      {
+         /* 
+           for a non BACKDROP layer we have to go to the end of the non-
+           BACKDROP layers list
+         */
+         while  (NULL != L_front->back && 
+               ((L_front->back->Flags & LAYERBACKDROP) == 0) )
+           L_front = L_front->back;
+         /* insert it behind L_front */
+         if (L_front->back != NULL)
+	 {
+           L_front->back->front = L;
+           L->back              = L_front->back;
+         }
+         L_front->back = L;
+         L->front      = L_front;
+      }
+    }
+    
+    /* 
+      Now create all ClipRects of all Layers correctly.   
+      Comment: CreateClipRects is the onlt function that does the
+               job correctly if you want to create a layer somewhere
+               behind other layers.
+    */
+
+    CreateClipRects(li, L);
+
+    if (li->top_layer == L && NULL != L->back)
+      CreateClipRectsSelf(L->back, FALSE); 
+
+    /*
+       Ok, all other layers were visited and pixels are backed up.
+       Now we can draw the new layer by clearing these parts of the
+       displaybitmap for which the ClipRects of this layer have no
+       entry in lobs (these are not hidden, but might be hiding other
+       layers behind them, but those parts are backed up now)
+    */
+
+    CR = L->ClipRect;
+    while (CR != NULL)
+    {
+      if (NULL == CR->lobs)
+      {
+kprintf("cbhl: width %d, height %d\n",CR->bounds.MaxX-CR->bounds.MinX+1,
+                                      CR->bounds.MaxY-CR->bounds.MinY+1);
+        BltBitMap(
+          bm /* Source Bitmap - we don't need one for clearing, but this
+                one will also do :-) */,
+          0,
+          0,
+          bm /* Destination Bitmap - */,
+          CR->bounds.MinX,
+          CR->bounds.MinY,
+          CR->bounds.MaxX-CR->bounds.MinX+1,
+          CR->bounds.MaxY-CR->bounds.MinY+1,
+          0x000 /* supposed to clear the destination */,
+          0xff,
+          NULL
+        );
+      }
+      CR = CR->Next;
+    }   
+    UnlockLayers(li);
+  } /* if (all memory is there) */  
+  else /* not enough memory */
+  {
+    if (NULL != L ) FreeMem(L , sizeof(struct Layer));
+    if (NULL != RP) FreeMem(RP, sizeof(struct RastPort));
+    if (NULL != CR) FreeMem(CR, sizeof(struct ClipRect));
+    L = NULL;
+  }
+
+  return L;
+  
+  AROS_LIBFUNC_EXIT
 } /* CreateBehindHookLayer */
