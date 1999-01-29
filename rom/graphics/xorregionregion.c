e495b9d563f5679e15b4dac1048842f02753f485
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-01-29 13:19:37 +0000

    Correct implementation.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@5151 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/graphics/xorregionregion.c
+++ rom/graphics/xorregionregion.c
@@ -32,10 +32,11 @@
 	region2 - pointer to a region structure
 
     RESULT
-	TRUE if the operation was succesful, else FALSE
-	(out of memory)
+	TRUE if the operation was succesful, 
+        FALSE otherwise (out of memory)
 
     NOTES
+
 	
     EXAMPLE
 
@@ -45,6 +46,8 @@
 	AndRegionRegion(), OrRegionRegion()
 
     INTERNALS
+        Two regions A and B consist of rectangles a1,...,a3 and b1,...,b3.
+        A xor B := A*NOT(B) + NOT(A)*B
 
     HISTORY
 	27-11-96    digulla automatically created from
@@ -53,25 +56,78 @@
 
 *****************************************************************************/
 {
-    AROS_LIBFUNC_INIT
-    
-    struct Rectangle CurRectangle;
-    struct RegionRectangle * CurRR = region1 -> RegionRectangle;
-    
-    while (NULL != CurRR)
-    {
-      CurRectangle.MinX = region1->bounds.MinX + CurRR->bounds.MinX;
-      CurRectangle.MaxX = region1->bounds.MinX + CurRR->bounds.MaxX;
-      CurRectangle.MinY = region1->bounds.MinY + CurRR->bounds.MinY;
-      CurRectangle.MaxY = region1->bounds.MinY + CurRR->bounds.MaxY;
-      
-      if (FALSE == XorRectRegion(region2, &CurRectangle))
-        return FALSE;
-      
-      CurRR = CurRR -> Next;
-    }
- 
-    return TRUE;
+  AROS_LIBFUNC_INIT
 
-    AROS_LIBFUNC_EXIT
+  struct Rectangle R;
+  struct Region Work1, Work2;
+
+  /* First I have to create a backup of the whole region1 and 2 */
+  Work1.bounds = region1->bounds;
+  Work2.bounds = region2->bounds;
+  
+  /* make a duplcate of region1 and region 2 */
+  if (NULL == (Work1.RegionRectangle = copyrrects(region1->RegionRectangle)))
+  {
+    /* no more memory */
+    return FALSE;
+  }
+
+  if (NULL == (Work2.RegionRectangle = copyrrects(region2->RegionRectangle)))
+  {
+    /* no more memory */
+    /* free duplicate of region1 */
+    ClearRegion(&Work1);
+    return FALSE;
+  }
+
+
+  /* I need not(BackupRegion2) now and will also calculate 
+     region1 AND NOT(region2) in the next step     */
+  R = Work2.bounds;
+  if (FALSE == XorRectRegion(&Work2, &R) /* NOT(region2) */ ||
+      FALSE == AndRegionRegion(region1, &Work2)  /* reg1 and NOT(reg2) */)
+  {
+    /* no more memory - region1 and region2 are still correct */
+    ClearRegion(&Work1);
+    ClearRegion(&Work2);
+    return FALSE;
+  } 
+
+  /* Work2 now contains reg1 and NOT(reg2), the first part */
+
+  /* create NOT(reg1) and       NOT(reg1) AND reg2   in the next step */
+  R = Work1.bounds;
+  if (FALSE == XorRectRegion(&Work1, &R) /* NOT(region1) */ ||
+      FALSE == AndRegionRegion(region1, &Work1) /* NOT(reg1) and reg2 */)
+  {
+    /* no more memory, region1 and region2 are still correct */
+    ClearRegion(&Work1);
+    ClearRegion(&Work2);
+    return FALSE;
+  }
+
+  /* combine the two parts: (NOT(reg1) * reg2) + (reg1 * NOT(reg2)) */
+  if (FALSE == OrRegionRegion(&Work2, &Work1))
+  {
+    /* no more memory, region1 and region2 are still correct */
+    ClearRegion(&Work1);
+    ClearRegion(&Work2);
+    return FALSE;
+  }
+
+  ClearRegion(&Work2);
+
+  /* Work1 contains the result now, nothing went wrong. So i can dispose whatever
+     was in region2 */
+  ClearRegion(region2);
+
+  region2->bounds = Work2.bounds;
+  region2->RegionRectangle = Work2.RegionRectangle;
+
+  /* must not free Work2 as it is the result that is in region2 now */
+  
+  /* everything went alright */
+  return TRUE; 
+  
+  AROS_LIBFUNC_EXIT
 }
