a4a4125b8cd0fc0e7e2be93e500c7f802579755e
hkiel <hkiel@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-01-30 22:46:13 +0000

    Added empty skeletons
    git-svn-id: https://svn.aros.org/svn/aros/trunk@5158 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/intuition/freescreenbuffer.c
@@ -0,0 +1,66 @@
+/*
+    (C) 1995-99 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Intuition function FreeScreenBuffer()
+    Lang: english
+*/
+#include "intuition_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+#include <intuition/screens.h>
+#include <proto/intuition.h>
+
+	AROS_LH2(void, FreeScreenBuffer,
+
+/*  SYNOPSIS */
+	AROS_LHA(struct Screen *      , screen, A0),
+	AROS_LHA(struct ScreenBuffer *, screenbuffer, A1),
+
+/*  LOCATION */
+	struct IntuitionBase *, IntuitionBase, 129, Intuition)
+
+/*  FUNCTION
+	Frees a ScreenBuffer allocated by AllocScreenBuffer() and releases
+	associated resources. You have to call this before closing your
+	screen.
+
+    INPUTS
+	screen - The screen this screenbuffer belongs to
+	screenbuffer - The screenbuffer obtained by AllocScreenBuffer()
+		It is safe to pass NULL.
+
+    RESULT
+	None.
+
+    NOTES
+	When used SB_SCREEN_BITMAP on allocating the ScreenBuffer
+	(ie. the ScreenBuffer only refers to the screen's BitMap) you must
+	FreeScreenBuffer() the ScreenBuffer before closing the screen.
+	Intuition will recognize when FreeScreenBuffer() is called for the
+	currently installed ScreenBuffer that it must not free the BitMap.
+	This is left to the CloseScreen() function.
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	AllocScreenBuffer(), ChangeScreenBuffer()
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+    AROS_LIBBASE_EXT_DECL(struct IntuitionBase *,IntuitionBase)
+
+#warning TODO: Write intuition/FreeScreenBuffer()
+    aros_print_not_implemented ("FreeScreenBuffer");
+
+    AROS_LIBFUNC_EXIT
+} /* FreeScreenBuffer */
