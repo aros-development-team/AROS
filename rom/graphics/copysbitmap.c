470397a367659db20b1fcece5b9092d699be29ef
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-01-30 23:23:41 +0000

    Implementation of these two functions.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@5161 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/graphics/copysbitmap.c
@@ -0,0 +1,86 @@
+/*
+    (C) 1995-98 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Graphics function SyncSBitMap()
+    Lang: english
+*/
+#include <graphics/layers.h>
+#include <graphics/clip.h>
+#include "graphics_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/graphics.h>
+
+	AROS_LH1(void, CopySBitMap,
+
+/*  SYNOPSIS */
+	AROS_LHA(struct Layer *, l, A0),
+
+/*  LOCATION */
+	struct GfxBase *, GfxBase, 75, Graphics)
+
+/*  FUNCTION
+        If the layer has a superbitmap all the parts that are visible will
+        be refreshed with what is in the superbitmap. This function should
+        be called after the SuperBitMap of the layer was synchronized with
+        SyncSBitMap() and manipulated. 
+
+    INPUTS
+        l  - pointer to superbitmapped layer 
+
+    RESULT
+        The layer will show the true contents of the superbitmap that is
+        attatched to it
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+        SyncSBitMap()
+ 
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+  AROS_LIBFUNC_INIT
+  AROS_LIBBASE_EXT_DECL(struct GfxBase *,GfxBase)
+
+  struct ClipRect * CR = l->ClipRect;
+
+  if (NULL == l->SuperBitMap || (l->Flags & LAYERSUPER) == 0)
+    return;
+
+  while (NULL != CR)
+  {
+    /* a cliprect of a superbitmapped layer is visible if lobs==NULL,
+       only these I have to copy into the rastport's bitmap */
+    if (NULL == CR->lobs)
+    {
+      /* I have to backup this part into the SuperBitMap! I find the
+         data in the bitmap of the rastport of this layer */
+      BltBitMap(l->SuperBitMap,
+                CR->bounds.MinX - l->bounds.MinX + l->Scroll_X,
+                CR->bounds.MinY - l->bounds.MinY + l->Scroll_Y,
+                l->rp->BitMap,
+                CR->bounds.MinX,
+                CR->bounds.MinY,
+                CR->bounds.MaxX - CR->bounds.MinX + 1,
+                CR->bounds.MaxY - CR->bounds.MinY + 1,
+                0x0c0,
+                0xff,
+                NULL
+            );
+    }
+    CR = CR->Next;
+  }
+
+  AROS_LIBFUNC_EXIT
+} /* CopySBitMap */
