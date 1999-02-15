d57eec8d144f4591ad5500c9321c327c42cd0f30
nlorentz <nlorentz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-02-15 21:28:44 +0000

    Initial revision
    git-svn-id: https://svn.aros.org/svn/aros/trunk@5264 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/graphics/bltpattern.c
@@ -0,0 +1,63 @@
+/*
+    (C) 1997 AROS - The Amiga Research OS
+    $Id$
+
+    Desc:
+    Lang: english
+*/
+#include "graphics_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+#include <clib/graphics_protos.h>
+
+	AROS_LH7(void, BltPattern,
+
+/*  SYNOPSIS */
+	AROS_LHA(struct RastPort *, rp, A1),
+	AROS_LHA(PLANEPTR         , mask, A0),
+	AROS_LHA(LONG             , xMin, D0),
+	AROS_LHA(LONG             , yMin, D1),
+	AROS_LHA(LONG             , xMax, D2),
+	AROS_LHA(LONG             , yMax, D3),
+	AROS_LHA(ULONG            , byteCnt, D4),
+
+/*  LOCATION */
+	struct GfxBase *, GfxBase, 52, Graphics)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+	27-11-96    digulla automatically created from
+			    graphics_lib.fd and clib/graphics_protos.h
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+    AROS_LIBBASE_EXT_DECL(struct GfxBase *,GfxBase)
+
+    driver_BltPattern(rp
+    	, mask
+	, xMin, yMin
+	, xMax, yMax
+	, byteCnt
+	, GfxBase
+    );
+    
+    AROS_LIBFUNC_EXIT
+} /* BltPattern */
