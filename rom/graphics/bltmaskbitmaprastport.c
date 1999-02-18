c44548b3183108c8330ff25c338368b52c3f2c00
nlorentz <nlorentz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-02-18 06:36:55 +0000

    Initial revision
    git-svn-id: https://svn.aros.org/svn/aros/trunk@5292 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/graphics/bltmaskbitmaprastport.c
@@ -0,0 +1,68 @@
+/*
+    (C) 1997 AROS - The Amiga Research OS
+    $Id$
+
+    Desc:
+    Lang: english
+*/
+#include "graphics_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+#include <clib/graphics_protos.h>
+
+	AROS_LH10(void, BltMaskBitMapRastPort,
+
+/*  SYNOPSIS */
+	AROS_LHA(struct BitMap   *, srcBitMap, A0),
+	AROS_LHA(LONG             , xSrc, D0),
+	AROS_LHA(LONG             , ySrc, D1),
+	AROS_LHA(struct RastPort *, destRP, A1),
+	AROS_LHA(LONG             , xDest, D2),
+	AROS_LHA(LONG             , yDest, D3),
+	AROS_LHA(LONG             , xSize, D4),
+	AROS_LHA(LONG             , ySize, D5),
+	AROS_LHA(ULONG            , minterm, D6),
+	AROS_LHA(PLANEPTR         , bltMask, A2),
+
+/*  LOCATION */
+	struct GfxBase *, GfxBase, 106, Graphics)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+	27-11-96    digulla automatically created from
+			    graphics_lib.fd and clib/graphics_protos.h
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+    AROS_LIBBASE_EXT_DECL(struct GfxBase *,GfxBase)
+    
+    driver_BltMaskBitMapRastPort(srcBitMap
+    		, xSrc, ySrc
+		, destRP
+		, xDest, yDest
+		, xSize, ySize
+		, minterm
+		, bltMask
+		, GfxBase
+    );
+
+    AROS_LIBFUNC_EXIT
+} /* BltMaskBitMapRastPort */
