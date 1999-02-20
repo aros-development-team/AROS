0c597f0a6a467ff8153b88d2a004c761abe30b62
srittau <srittau@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-02-20 19:46:17 +0000

    (I wrote this version, while keyboard.h was still missing from CVS - I commit it because it uses AROS' standard include layout - see dos/*.h)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@5335 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/devices/keyboard.h
+++ compiler/include/devices/keyboard.h
@@ -2,23 +2,23 @@
 #define DEVICES_KEYBOARD_H
 
 /*
-    (C) 1998 AROS - The Amiga Research OS
+    (C) 1999 AROS - The Amiga Research OS
     $Id$
 
-    Desc:
-    Lang: English
+    Desc: Important defines and structures for keyboard.device
+    Lang: english
 */
 
+#include <exec/io.h>
 
+/**********************************************************************
+ ********************** Keyboard Device Commands **********************
+ **********************************************************************/
 
-#ifndef	 EXEC_IO_H
-#include "exec/io.h"
-#endif
+#define KBD_READEVENT        (CMD_NONSTD + 0)
+#define KBD_READMATRIX       (CMD_NONSTD + 1)
+#define KBD_ADDRESETHANDLER  (CMD_NONSTD + 2)
+#define KBD_REMRESETHANDLER  (CMD_NONSTD + 3)
+#define KBD_RESETHANDLERDONE (CMD_NONSTD + 4)
 
-#define	 KBD_READEVENT	      (CMD_NONSTD+0)
-#define	 KBD_READMATRIX	      (CMD_NONSTD+1)
-#define	 KBD_ADDRESETHANDLER  (CMD_NONSTD+2)
-#define	 KBD_REMRESETHANDLER  (CMD_NONSTD+3)
-#define	 KBD_RESETHANDLERDONE (CMD_NONSTD+4)
-
-#endif	/* DEVICES_KEYBOARD_H */
+#endif /* DEVICES_KEYBOARD_H */
