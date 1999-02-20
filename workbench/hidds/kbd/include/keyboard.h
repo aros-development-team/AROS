73751fe09e0c3ad404ac8a9d3824fe91bb78a61a
nlorentz <nlorentz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-02-20 17:11:58 +0000

    Include for the keyboard hidd
    git-svn-id: https://svn.aros.org/svn/aros/trunk@5325 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/hidds/kbd/include/keyboard.h
@@ -0,0 +1,45 @@
+#ifndef HIDD_KEYBOARD_H
+#define HIDD_KEYBOARD_H
+/*
+    (C) 1997 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Include for the keyboard hidd.
+    Lang: English.
+*/
+
+#ifndef OOP_OOP_H
+#   include <oop/oop.h>
+#endif
+
+
+#define IID_Hidd_Kbd "hidd.kbd"
+
+#define HiddKbdAB __abHidd_Kbd
+
+
+extern AttrBase HiddKbdAB;
+
+
+enum {
+   aoHidd_Kbd_IrqHandler,
+   aoHidd_Kbd_IrqHandlerData,
+   
+   num_Hidd_Kbd_Attrs
+};
+
+
+/* Parameter values for the IRQ handler */
+
+enum {
+   vHidd_Kbd_Press,
+   vHidd_Kbd_Release
+};
+
+
+#define aHidd_Kbd_IrqHandler		(aoHidd_Kbd_IrqHandler     + HiddKbdAB)
+#define aHidd_Kbd_IrqHandlerData	(aoHidd_Kbd_IrqHandlerData + HiddKbdAB)
+
+#define IS_HIDDKBD_ATTR(attr, idx) IS_IF_ATTR(attr, idx, HiddKbdAB, num_Hidd_Kbd_Attrs)
+
+#endif HIDD_KEYBOARD_H
