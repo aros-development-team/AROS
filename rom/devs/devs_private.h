d0be3aa3d72ffdb01dffe9a01ffd77cbae17804a
nlorentz <nlorentz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-02-21 20:56:27 +0000

    Include file containing private stuff for devices
    git-svn-id: https://svn.aros.org/svn/aros/trunk@5344 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/devs/devs_private.h
@@ -0,0 +1,16 @@
+#ifndef DEVS_PRIVATE_H
+#define DEVS_PRIVATE_H
+
+/*
+    (C) 1997 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Private device stuff
+    Lang: english
+*/
+
+#define CMD_HIDDINIT 32000
+
+
+
+#endif /* DEVS_PRIVATE_H */
