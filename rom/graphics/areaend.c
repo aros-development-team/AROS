bd52029a6d1e92624226c1b4fed8f8ea315c9cc2
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-03-22 04:46:48 +0000

    Calls the area fill algorithm for polygons.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@5588 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/graphics/areaend.c
+++ rom/graphics/areaend.c
@@ -61,7 +61,9 @@
   /* is there anything in the matrix at all ? */
   if (0 != (areainfo -> Count))
   {
-
+     WORD first_idx = 0;
+     WORD last_idx  = -1;
+    UWORD BytesPerRow;
     UWORD * CurVctr  = areainfo -> VctrTbl;
     BYTE  * CurFlag  = areainfo -> FlagTbl;
     UWORD Count;
@@ -71,7 +73,10 @@
 
     UWORD Rem_cp_x   = rp->cp_x;
     UWORD Rem_cp_y   = rp->cp_y;
+    /* This rectangle serves as a "frame" for the tmpras for filling */
+    struct Rectangle bounds;
 
+ 
     if (0 != (rp->Flags & AREAOUTLINE))
        SetAPen(rp, GetOutlinePen(rp));
 
@@ -79,35 +84,101 @@
     if ( areainfo->FlagPtr[-1] != 0x02 &&
         (areainfo->VctrPtr[-1] != areainfo->FirstY ||
          areainfo->VctrPtr[-2] != areainfo->FirstX   ) )
+    {
        if (-1 == AreaDraw(rp, areainfo->FirstX, areainfo->FirstY))
             return -1;
+    }
+    /* mark the previous polygon as closed */
+    areainfo->FlagPtr[-1] = 2;
 
     Count = areainfo->Count;  
 
+kprintf("%d coord to process\n",Count);
     /* process the list of vectors */
     while (Count > 0)
     {
+kprintf("Flags:%d Coord: (%d,%d)\n",CurFlag[0], CurVctr[0],CurVctr[1]);
+      last_idx ++;
       switch((unsigned char)CurFlag[0])
       {
         case 0x00:
-          /* fill the previous shape if there was one to fill */
-          /* !!! missing !!!! */
-
 	  /* set the graphical cursor to a starting position */
             Move(rp, CurVctr[0], CurVctr[1]);
+
+            bounds.MinX = CurVctr[0];
+            bounds.MaxX = CurVctr[0];
+            bounds.MinY = CurVctr[1];
+            bounds.MaxY = CurVctr[1];
+
             CurVctr = &CurVctr[2];
             CurFlag = &CurFlag[1];
           break;
 
         case 0x02:
-          /* Nothing to do here */
+          /* 2 indicates that this Polygon is closed with this coordinate */
+            Draw(rp, CurVctr[0], CurVctr[1]);
             CurVctr = &CurVctr[2];
             CurFlag = &CurFlag[1];
+            /* 
+               no need to set the bundaries here like in case 0x00 as
+               this coord closes the polygon and therefore is the same
+               one as the first coordinate of the polygon. 
+            */
+            /* check whether there's anything to fill at all. I cannot
+               fill a line (=3 coordinates) */
+            if (first_idx+2 <= last_idx)
+	    {
+              /* BytesPerRow must be a multiple of 2 bytes */
+
+              BytesPerRow = bounds.MaxX - bounds.MinX + 1;
+              if (0 != (BytesPerRow & 0x0f ))
+                BytesPerRow =((BytesPerRow >> 3) & 0xfffe )+ 2;
+              else
+                BytesPerRow = (BytesPerRow >> 3) & 0xfffe;
+/*              
+              kprintf("first: %d, last: %d\n",first_idx,last_idx);
+              kprintf("(%d,%d)-(%d,%d)\n",bounds.MinX,bounds.MinY,
+                                          bounds.MaxX,bounds.MaxY);
+              kprintf("width: %d, bytesperrow: %d\n",bounds.MaxX-bounds.MinX+1,
+                                                     BytesPerRow);
+*/
+              if (TRUE == areafillpolygon(rp,
+                                          &bounds, 
+                                          first_idx, 
+                                          last_idx,
+                                          BytesPerRow,
+                                          GfxBase))
+	      {
+                /* 
+                 Blit the area fill pattern through the mask provided
+                 by rp->TmpRas.
+                */
+                BltPattern(
+                   rp,
+                   rp->TmpRas->RasPtr,
+                   bounds.MinX,
+                   bounds.MinY,
+                   bounds.MaxX,
+                   bounds.MaxY,
+                   BytesPerRow
+                );
+	      }
+	    }
+            /* set first_idx for a possible next polygon to draw */
+            first_idx = last_idx+1;
           break;
 
         case 0x03:
           /* Draw a line to new position */
             Draw(rp, CurVctr[0], CurVctr[1]);
+            if (bounds.MinX > CurVctr[0])
+              bounds.MinX = CurVctr[0];
+            if (bounds.MaxX < CurVctr[0])
+              bounds.MaxX = CurVctr[0];
+            if (bounds.MinY > CurVctr[1])
+              bounds.MinY = CurVctr[1];
+            if (bounds.MaxY < CurVctr[1])
+              bounds.MaxY = CurVctr[1];
             CurVctr = &CurVctr[2];
             CurFlag = &CurFlag[1];
           break;
@@ -139,7 +210,7 @@
             CurVctr = &CurVctr[4];
             CurFlag = &CurFlag[2];
             Count--;
-          
+            last_idx++; /* there were two coords here! */
           break;
 
 	default:
