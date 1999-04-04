a10a59382b2aafc4afac932b6d8266d89931885a
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-04-04 02:33:04 +0000

    Update.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@5686 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/intuition/intuition_intern.h
+++ rom/intuition/intuition_intern.h
@@ -200,6 +200,8 @@ extern int  intui_OpenWindow (struct Window *, struct IntuitionBase *, struct Bi
 extern void intui_SetWindowTitles (struct Window *, UBYTE *, UBYTE *);
 extern void intui_RefreshWindowFrame(struct Window *win);
 extern struct Window *intui_FindActiveWindow(struct InputEvent *ie, BOOL *swallow_event, struct IntuitionBase *IntuitionBase);
+void intui_ScrollWindowRaster(struct Window * win, WORD dx, WORD dy, WORD xmin,
+                           WORD ymin, WORD xmax, WORD ymax, struct IntuitionBase * IntuitionBase);
 
 /* Miscellaneous prototypes */
 void easyrequest_freelabels(STRPTR *gadgetlabels);
