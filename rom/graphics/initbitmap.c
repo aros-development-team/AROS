f91e92c3ddaaf5799419e5570f7cf6e78ec8b7f1
SDuvan <SDuvan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-04-06 20:12:43 +0000

    Implemented
    git-svn-id: https://svn.aros.org/svn/aros/trunk@5700 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/graphics/initbitmap.c
@@ -0,0 +1,70 @@
+/*
+    (C) 1998 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc:
+    Lang: English
+*/
+
+/*****************************************************************************
+
+    NAME */
+
+#include <graphics/gfx.h>
+
+    AROS_LH4I(VOID, InitBitMap,
+
+/*  SYNOPSIS */
+
+	AROS_LHA(struct BitMap *, bm,     A0),
+	AROS_LHA(BYTE           , depth,  A1),
+	AROS_LHA(UWORD          , width,  D0),
+	AROS_LHA(UWORD          , height, D1),
+
+/*  LOCATION */
+
+	struct Library *, GfxBase, 65, Graphics)
+
+/*  FUNCTION
+
+    Initialize BitMap structure. A bitmap MUST be initialized before it's
+    used in any (other) graphics library function.
+
+    INPUTS
+
+    bm     --  pointer to BitMap structure
+    depth  --  number of bitplanes
+    width  --  width in pixels of this bitmap
+    height --  height in pixels of this bitmap
+
+    RESULT
+
+    NOTES
+
+    The Planes[] is not affected and must be set up the caller.
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    <graphics/gfx.h>
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+
+{
+    AROS_LIBFUNC_INIT
+
+    bm->Depth = depth;
+    bm->Rows  = height;
+    bm->Flags = 0;
+    bm->Pad   = 0;
+    bm->BytesPerRow = ((width + 15) >> 3) & ~0x1;
+
+    AROS_LIBFUNC_EXIT
+} /* InitBitMap */
