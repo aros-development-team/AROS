d9759f631f8d0424e71cfcde4369a0fc92b82831
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-04-08 20:31:13 +0000

    nicer looking
    git-svn-id: https://svn.aros.org/svn/aros/trunk@5711 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/classes/gadgets/aroscycle/support.c
+++ workbench/classes/gadgets/aroscycle/support.c
@@ -30,6 +30,7 @@ void drawdisabledpattern(struct CycleBase_intern *AROSCycleBase,
     RectFill(rport, left, top, left+width-1, top+height-1);
 }
 
+#define CYCLEIMAGEWIDTH 19
 
 void renderlabel(struct CycleBase_intern *AROSCycleBase,
                  struct Gadget *gad,
@@ -38,11 +39,40 @@ void renderlabel(struct CycleBase_intern *AROSCycleBase,
                  struct GadgetInfo *ginfo)
 {
     UWORD *pens = ginfo->gi_DrInfo->dri_Pens;
+    WORD x,y,h;
     int len = strlen(string);
 
     SetABPenDrMd(rport, pens[TEXTPEN], pens[BACKGROUNDPEN], JAM1);
     Move(rport,
-         gad->LeftEdge + (gad->Width - TextLength(rport, string, len)) / 2,
-         gad->TopEdge + rport->Font->tf_YSize);
+         gad->LeftEdge + (gad->Width - CYCLEIMAGEWIDTH - TextLength(rport, string, len)) / 2,
+         gad->TopEdge + (gad->Height - rport->Font->tf_YSize) / 2 + rport->Font->tf_Baseline);
     Text(rport, string, len);
+    
+    x = gad->LeftEdge + gad->Width - CYCLEIMAGEWIDTH;
+    
+    /* separator bar */
+    
+    SetAPen(rport, pens[SHINEPEN]);
+    RectFill(rport, x + 1, gad->TopEdge + 2, x + 1, gad->TopEdge + gad->Height - 1 - 2);
+    SetAPen(rport, pens[SHADOWPEN]);
+    RectFill(rport, x, gad->TopEdge + 2, x, gad->TopEdge + gad->Height - 1 - 2);
+
+    /* cycle image */
+    
+    h = gad->Height / 2;
+    
+    x += 6;
+
+    for(y = 0; y < 4; y++)
+    {
+    	RectFill(rport,x + y,
+		       gad->TopEdge + gad->Height - 1 - h - y - 1,
+		       x + 6 - y, 
+		       gad->TopEdge + gad->Height - 1 - h - y - 1); 
+		       
+	RectFill(rport,x + y,
+		       gad->TopEdge + h + y + 1,
+		       x + 6 - y,
+		       gad->TopEdge + h + y + 1); 
+    }
 }
