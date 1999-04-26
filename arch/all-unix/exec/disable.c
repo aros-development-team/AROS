48f43bef355e6fb84aba23f0db2e97e38fcc359f
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-04-26 20:26:51 +0000

    bug fix
    git-svn-id: https://svn.aros.org/svn/aros/trunk@5757 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-unix/exec/disable.c
+++ arch/all-unix/exec/disable.c
@@ -25,6 +25,7 @@ void _Exec_Disable(struct ExecBase * SysBase)
 {
     AROS_LIBFUNC_INIT
 
+#if 0
     if(SysBase->IDNestCnt++ < 0)
     {
 	/* Block all signals. We should really only block those that
@@ -32,6 +33,19 @@ void _Exec_Disable(struct ExecBase * SysBase)
 	*/
 	sigprocmask(SIG_BLOCK, &sig_int_mask, NULL);
     }
+#endif
 
+    /* Georg Steger */
+    
+    sigprocmask(SIG_BLOCK, &sig_int_mask, NULL);
+    if (++SysBase->IDNestCnt < 0)
+    {
+    	/* If we get here we have big trouble. Someone called
+	   1x Disable() and 2x Enable(). IDNestCnt < 0 would
+	   mean enable interrupts, but the caller of Disable
+	   relies on the function to disable them, so we don´t
+	   do anything here (or maybe a deadend alert?) */
+    }
+    
     AROS_LIBFUNC_EXIT
 }
