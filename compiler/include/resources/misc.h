0fea6be890c3f75e6897fed130057471e8513152
SDuvan <SDuvan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-06-19 09:13:48 +0000

    Missing file
    git-svn-id: https://svn.aros.org/svn/aros/trunk@5894 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/resources/misc.h
@@ -0,0 +1,24 @@
+/*
+    Copyright (C) 1998-99 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: 
+    Lang: English
+*/
+
+#ifndef RESOURCES_MISC_H
+#define RESOURCES_MISC_H
+
+
+enum
+{
+    MR_SERIALPORT = 0,
+    MR_SERIALBITS,
+    MR_PARALLELPORT,
+    MR_PARALLELBITS,
+    MR_MAXUNIT
+};
+
+#define MISCNAME "misc.resource"
+
+#endif /* RESOURCES_MISC_H */
