17f8f1a842295b0316211aef3124c121760cbed2
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-07-21 11:56:23 +0000

    Bugfix by B. Fastenrath and more tests.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@6007 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/uae-tmpl/test-1.c
+++ test/uae-tmpl/test-1.c
@@ -4,38 +4,140 @@
 
 #include "aros_types.h"
 
+void hexdump (const void * start, int size);
+
+/* Make sure that all variables contain the same values */
+#define __check(v1,op,v2) \
+    if (debug) \
+    { \
+	printf ("Check: " #v1 " " #op " " #v2 "\n"); \
+	hexdump (&v1, sizeof (v1)); \
+	hexdump (&v2, sizeof (v2)); \
+    } \
+    if (!(v1 op v2) ) \
+    { \
+	printf ("%s:%d: Check failed: " #v1 " " #op " " #v2 " (%d - %d)\n", \
+		__FILE__, __LINE__, (int)v1, (int)v2); \
+    }
+#define check(op) \
+    do { \
+	__check (s,op,w) \
+	__check (us,op,uw) \
+	__check (l,op,L) \
+	__check (ul,op,UL) \
+    } \
+    while (0)
+
+#define unoptest(op) \
+    s op; us op; l op; ul op; \
+    w op; uw op; L op; UL op; \
+    check (==)
+
+#define binoptest(op) \
+    s = s op; us = us op; l = l op; ul = ul op; \
+    w = w op; uw = uw op; L = L op; UL = UL op; \
+    check (==)
+
+#define cmptest(v1,op,v2) \
+    s = v1; us = v1; l = v1; ul = v1; \
+    w = v2; uw = v2; L = v2; UL = v2; \
+    check (op)
+    
 int main (int argc, char ** argv)
 {
     short	   s;
     unsigned short us;
+    long	   l;
+    unsigned long  ul;
     WORD	   w;
     UWORD	   uw;
-    ULONG	   ul;
+    LONG	   L;
+    ULONG	   UL;
 
-    s  = 0;
-    us = 0;
-    w  = 0;
-    uw = 0;
+    int debug = (argc != 1);
 
-    assert (s == w);
-    assert (us == uw);
+    int test = 0x123456;
+    hexdump (&test, sizeof (test));
+    
+    printf ("Check assignments\n");
+    unoptest (= 0);
+    unoptest (= 0x1234);
+    unoptest (= 0x12345678);
+    unoptest (= 0x92345678);
+    w = s; uw = us; L = l; UL = ul; check (==);
+    s = w; us = uw; l = L; ul = UL; check (==);
 
-    s  = 0x1234;
-    us = 0x1234;
-    w  = 0x1234;
-    uw = 0x1234;
-    ul = 0x1234;
+    printf ("Check + operator\n");
+    binoptest (+ 1);
+    binoptest (+ 0x1234);
+    binoptest (+ 0x9234);
+    binoptest (+ 0x12345678);
+    binoptest (+ 0x92345678);
 
-    assert (s == w);
-    printf ("us=%x uw=%x\n", us, int (uw));
-    printf ("ul=%lx\n", (unsigned long)ul);
-    assert (us == uw);
+    printf ("Check - operator\n");
+    binoptest (- 1);
+    binoptest (- 0x1234);
+    binoptest (- 0x9234);
+    binoptest (- 0x12345678);
+    binoptest (- 0x92345678);
 
-    s  = w;
-    us = uw;
-    w  = s;
-    uw = us;
+    printf ("Check * operator\n");
+    binoptest (* 2);
+    binoptest (* 0x1234);
+    binoptest (* 0x9234);
+    binoptest (* 0x12345678);
+    binoptest (* 0x92345678);
 
-    assert (s == w);
-    assert (us == uw);
+    printf ("Check / operator\n");
+    binoptest (/ 2);
+    binoptest (/ 0x1234);
+    binoptest (/ 0x9234);
+    binoptest (/ 0x12345678);
+    binoptest (/ 0x92345678);
+
+    printf ("Check %% operator\n");
+    binoptest (% 2);
+    binoptest (% 0x1234);
+    binoptest (% 0x9234);
+    binoptest (% 0x12345678);
+    binoptest (% 0x92345678);
+
+    printf ("Check >> operator\n");
+    unoptest (= 0x12345678);
+    binoptest (>> 2);
+
+    printf ("Check << operator\n");
+    unoptest (= 0x12345678);
+    binoptest (<< 2);
+
+    printf ("Check < comparison\n");
+    cmptest (0x12345678, <, 0x12345679);
+    cmptest (0x12345678, <=, 0x12345679);
+    cmptest (0x12345678, <=, 0x12345678);
+    
+    printf ("Check > comparison\n");
+    cmptest (0x12345679, >, 0x12345678);
+    cmptest (0x12345679, >=, 0x12345678);
+    cmptest (0x12345678, >=, 0x12345678);
+    
+    printf ("All tests succeeded\n");
+    exit (0);
 }
+
+/* Print the contents of a piece of memory. */
+void hexdump (const void * start, int size)
+{
+    int t;
+    const unsigned char * ptr = (const unsigned char *)start;
+
+    for (t=0; size > 0; t++, size--)
+    {
+        if (!(t & 15)) printf ("%08lx: ", ((long)ptr));
+        printf ("%02x", *ptr++);
+        if ((t & 3) == 3) putchar (' ');
+        if ((t & 15) == 15) putchar ('\n');
+    }
+
+    if (t & 15) putchar ('\n');
+}
+
---
+++ test/uae-tmpl/test-2.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+#include <assert.h>
+
+#include "aros_types.h"
+
+int main (int argc, char ** argv)
+{
+    unsigned char *u;
+    char   *c = "foo";
+    void   *v = &c;
+    APTR    a;
+    be_ptr<char> s;
+
+    u = c;
+    s = c;
+    a = v;
+
+    long *l = (long *) c;
+    be_ptr<long> bl = l; // calls constructor
+    bl = l;              // calls assignment operator
+    bl = c;
+
+    assert (s == c);
+    assert (strcmp (s, c) == 0);
+}
