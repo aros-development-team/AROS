a5104206dbdee859d715f78746a02b302701c93b
SDuvan <SDuvan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-07-30 20:16:48 +0000

    Added missing includes
    git-svn-id: https://svn.aros.org/svn/aros/trunk@6075 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/datatypes/datatypes.h
@@ -0,0 +1,191 @@
+#ifndef DATATYPES_DATATYPES_H
+#define DATATYPES_DATATYPES_H
+
+/*
+    (C) 1999 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Includes for datatypes.library
+    Lang: English
+*/
+
+#ifndef    EXEC_TYPES_H
+#include  <exec/types.h>
+#endif
+#ifndef    EXEC_LISTS_H
+#include  <exec/lists.h>
+#endif
+#ifndef    EXEC_NODES_H
+#include  <exec/nodes.h>
+#endif
+#ifndef    EXEC_LIBRARIES_H
+#include  <exec/libraries.h>
+#endif
+#ifndef    LIBRARIES_IFFPARSE_H
+#include  <libraries/iffparse.h>
+#endif
+#ifndef	   DOS_DOS_H
+#include  <dos/dos.h>
+#endif
+
+
+#define ID_DTYP MAKE_ID('D','T','Y','P')
+#define ID_DTHD MAKE_ID('D','T','H','D')
+
+
+struct DataTypeHeader
+{
+    STRPTR	 dth_Name;           /* Name of the data type */
+    STRPTR	 dth_BaseName;       /* Base name of the data type */
+    STRPTR	 dth_Pattern;        /* File name match pattern */
+    WORD	*dth_Mask;           /* Comparision mask (binary) */
+    ULONG	 dth_GroupID;        /* DataType Group */
+    ULONG	 dth_ID;             /* DataType ID (same as IFF FORM type) */
+    WORD	 dth_MaskLen;        /* Length of the comparision mask */
+    WORD	 dth_Pad;            /* Unused at present (must be 0) */
+    UWORD	 dth_Flags;          /* Flags -- see below */
+    UWORD	 dth_Priority;
+};
+
+#define	 DTHSIZE       sizeof(struct DataTypeHeader)
+
+/* Types */
+#define  DTF_TYPE_MASK  0x000F
+#define  DTF_BINARY     0x0000
+#define  DTF_ASCII      0x0001
+#define  DTF_IFF        0x0002
+#define  DTF_MISC       0x0003
+
+#define  DTF_CASE       0x0010      /* Case is important */
+
+#define  DTF_SYSTEM1    0x1000      /* For system use only */
+
+
+/*****   Group ID and ID   ************************************************/
+
+/* System file -- executable, directory, library, font and so on. */
+#define	 GID_SYSTEM      MAKE_ID('s','y','s','t')
+
+/* Text, formatted or not */
+#define	 GID_TEXT        MAKE_ID('t','e','x','t')
+
+/* Formatted text combined with graphics or other DataTypes */
+#define  GID_DOCUMENT    MAKE_ID('d','o','c','u')
+
+/* Sound */
+#define  GID_SOUND       MAKE_ID('s','o','u','n')
+
+/* Musical instrument */
+#define  GID_INSTRUMENT  MAKE_ID('i','n','s','t')
+
+/* Musical score */
+#define  GID_MUSIC       MAKE_ID('m','u','s','i')
+
+/* Picture */
+#define  GID_PICTURE     MAKE_ID('p','i','c','t')
+
+/* Animated pictures */
+#define  GID_ANIMATION   MAKE_ID('a','n','i','m')
+
+/* Animation with audio */
+#define  GID_MOVIE       MAKE_ID('m','o','v','i')
+
+
+/**************************************************************************/
+
+
+#define  ID_CODE         MAKE_ID('D','T','C','D')
+
+
+struct DTHookContext
+{
+    struct Library       *dthc_SysBase;
+    struct Library       *dthc_DOSBase;
+    struct Library       *dthc_IFFParseBase;
+    struct Library       *dthc_UtilityBase;
+
+    /* File context */
+    BPTR                  dthc_Lock;
+    struct FileInfoBlock *dthc_FIB;
+    BPTR                  dthc_FileHandle;   /* Pointer to file handle 
+						(may be NULL) */
+    struct IFFHandle     *dthc_IFF;          /* Pointer to IFFHandle 
+						(may be NULL) */
+    STRPTR                dthc_Buffer;       /* Buffer... */
+    ULONG                 dthc_BufferLength; /* ... and corresponding length */
+};
+
+
+#define ID_TOOL MAKE_ID('D','T','T','L')
+
+struct Tool
+{
+    UWORD    tn_Which;
+    UWORD    tn_Flags;           /* Flags -- see below */
+    STRPTR   tn_Program;         /* Application to use */
+};
+
+
+#define  TF_LAUNCH_MASK       0x000F
+#define  TF_SHELL             0x0001
+#define  TF_WORKBENCH         0x0002
+#define  TF_RX                0x0003
+
+
+#define  ID_TAGS      MAKE_ID('D','T','T','G')
+
+
+/*************************************************************************/
+
+
+#ifndef	DATATYPE
+#define	DATATYPE
+struct DataType
+{
+    struct Node            dtn_Node1;         /* These two nodes are for... */
+    struct Node            dtn_Node2;         /* ...system use only! */
+    struct DataTypeHeader *dtn_Header;
+    struct List            dtn_ToolList;      /* Tool nodes */
+    STRPTR                 dtn_FunctionName;  /* Name of comparision routine */
+    struct TagItem        *dtn_AttrList;      /* Object creation tags */
+    ULONG                  dtn_Length;        /* Length of the memory block */
+};
+#endif
+
+#define	DTNSIZE	sizeof(struct DataType)
+
+
+struct ToolNode
+{
+    struct Node	 tn_Node;
+    struct Tool  tn_Tool;
+    ULONG	 tn_Length;  /* Length of the memory block */
+};
+
+#define  TNSIZE  sizeof(struct ToolNode)
+
+
+#ifndef  ID_NAME
+#define  ID_NAME   MAKE_ID('N','A','M','E')
+#endif
+
+
+/* Text ID:s */
+#define  DTERROR_UNKNOWN_DATATYPE        2000
+#define  DTERROR_COULDNT_SAVE            2001
+#define  DTERROR_COULDNT_OPEN            2002
+#define  DTERROR_COULDNT_SEND_MESSAGE    2003
+
+/* new for V40 */
+#define  DTERROR_COULDNT_OPEN_CLIPBOARD  2004
+#define  DTERROR_Reserved                2005
+#define  DTERROR_UNKNOWN_COMPRESSION     2006
+#define  DTERROR_NOT_ENOUGH_DATA         2007
+#define  DTERROR_INVALID_DATA            2008
+
+#define  DTMSG_TYPE_OFFSET               2100
+
+
+#endif /* DATATYPES_DATATYPES_H */
+
+
---
+++ compiler/include/datatypes/datatypesclass.h
@@ -0,0 +1,429 @@
+#ifndef DATATYPES_DATATYPESCLASS_H
+#define DATATYPES_DATATYPESCLASS_H
+
+/*
+    (C) 1999 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: 
+    Lang: English
+*/
+
+#ifndef	UTILITY_TAGITEM_H
+#include <utility/tagitem.h>
+#endif
+#ifndef	DATATYPES_DATATYPES_H
+#include <datatypes/datatypes.h>
+#endif
+#ifndef	INTUITION_INTUITION_H
+#include <intuition/intuition.h>
+#endif
+
+#warning "No printer stuff yet"
+/*
+#ifndef	DEVICES_PRINTER_H
+#include <devices/printer.h>
+#endif
+#ifndef	DEVICES_PRTBASE_H
+#include <devices/prtbase.h>
+#endif
+*/
+
+#define  DATATYPESCLASS         "datatypesclass"
+
+#define  DTA_Dummy              (TAG_USER + 0x1000)
+
+/* Default TextAttr to use for text within the object (struct TextAttr *) */
+#define  DTA_TextAttr           (DTA_Dummy+10)
+
+/* Top vertical unit (LONG) */
+#define  DTA_TopVert            (DTA_Dummy + 11)
+
+/* Number of visible vertical units (LONG) */
+#define  DTA_VisibleVert        (DTA_Dummy + 12)
+
+/* Total number of vertical units */
+#define  DTA_TotalVert          (DTA_Dummy + 13)
+
+/* Number of pixels per vertical unit (LONG) */
+#define  DTA_VertUnit           (DTA_Dummy + 14)
+
+/* Top horizontal unit (LONG) */
+#define  DTA_TopHoriz           (DTA_Dummy + 15)
+
+/* Number of visible horizontal units (LONG) */
+#define  DTA_VisibleHoriz       (DTA_Dummy + 16)
+
+/* Total number of horiziontal units */
+#define  DTA_TotalHoriz         (DTA_Dummy + 17)
+
+/* Number of pixels per horizontal unit (LONG) */
+#define  DTA_HorizUnit          (DTA_Dummy + 18)
+
+/* Name of the current element within the object (UBYTE *) */
+#define  DTA_NodeName           (DTA_Dummy + 19)
+
+/* Object's title */
+#define  DTA_Title              (DTA_Dummy + 20)
+
+/* Pointer to a NULL terminated array of trigger methods (struct DTMethod *) */
+#define  DTA_TriggerMethods     (DTA_Dummy + 21)
+
+/* Object data (APTR) */
+#define  DTA_Data               (DTA_Dummy + 22)
+
+/* Default font to use (struct TextFont *) */
+#define  DTA_TextFont           (DTA_Dummy + 23)
+
+/* Pointer to an array (terminated with ~0) of methods that the object
+   supports (ULONG *) */
+#define  DTA_Methods            (DTA_Dummy + 24)
+
+/* Printer error message -- numbers are defined in <devices/printer.h>
+   (LONG) */
+#define  DTA_PrinterStatus      (DTA_Dummy + 25)
+
+/* PRIVATE! Pointer to the print process (struct Process *) */
+#define  DTA_PrinterProc        (DTA_Dummy + 26)
+
+/* PRIVATE! Pointer to the layout process (struct Process *) */
+#define  DTA_LayoutProc         (DTA_Dummy + 27)
+
+/* Turns the application's busy pointer on and off */
+#define  DTA_Busy               (DTA_Dummy + 28)
+
+/* Indicate that new information has been loaded into an object.
+   (This is used for models that cache the DTA_TopVert-like tags.) */
+#define  DTA_Sync               (DTA_Dummy + 29)
+
+/* Base name of the class */
+#define  DTA_BaseName           (DTA_Dummy + 30)
+
+/* Group that the object must belong to */
+#define  DTA_GroupID            (DTA_Dummy + 31)
+
+/* Error level */
+#define  DTA_ErrorLevel         (DTA_Dummy + 32)
+
+/* datatypes.library error number */
+#define  DTA_ErrorNumber        (DTA_Dummy + 33)
+
+/* Argument for datatypes.library error */
+#define  DTA_ErrorString        (DTA_Dummy + 34)
+
+/* Name of a realtime.library conductor -- defaults to "Main" (UBYTE *) */
+#define  DTA_Conductor          (DTA_Dummy + 35)
+
+/* Specify whether a control panel should be embedded into the object or not
+   (for example in the animation datatype) -- defaults to TRUE (BOOL) */
+#define  DTA_ControlPanel       (DTA_Dummy + 36)
+
+/* Should the object begin playing immediately? -- defaults to FALSE (BOOL) */
+#define  DTA_Immediate          (DTA_Dummy + 37)
+
+/* Indicate that the object should repeat playing -- defaults to FALSE (BOOL)*/
+#define	DTA_Repeat		(DTA_Dummy+38)
+
+
+/* DTObject attributes */
+#define  DTA_Name         (DTA_Dummy + 100)
+#define  DTA_SourceType	  (DTA_Dummy + 101)
+#define  DTA_Handle       (DTA_Dummy + 102)
+#define  DTA_DataType     (DTA_Dummy + 103)
+#define  DTA_Domain       (DTA_Dummy + 104)
+
+#if 0
+/* These should not be used, and is therefore not available -- use the
+   corresponding tags in <intuition/gadgetclass> instead */
+#define  DTA_Left         (DTA_Dummy + 105)
+#define  DTA_Top          (DTA_Dummy + 106)
+#define  DTA_Width        (DTA_Dummy + 107)
+#define  DTA_Height       (DTA_Dummy + 108)
+#endif
+
+
+#define  DTA_ObjName         (DTA_Dummy + 109)
+#define  DTA_ObjAuthor       (DTA_Dummy + 110)
+#define  DTA_ObjAnnotation   (DTA_Dummy + 111)
+#define  DTA_ObjCopyright    (DTA_Dummy + 112)
+#define  DTA_ObjVersion      (DTA_Dummy + 113)
+#define  DTA_ObjectID        (DTA_Dummy + 114)
+#define  DTA_UserData        (DTA_Dummy + 115)
+#define  DTA_FrameInfo       (DTA_Dummy + 116)
+
+
+#if 0
+/* These should not be used, and is therefore not available -- use the
+   corresponding tags in <intuition/gadgetclass> instead */
+#define  DTA_RelRight        (DTA_Dummy + 117)
+#define  DTA_RelBottom       (DTA_Dummy + 118)
+#define  DTA_RelWidth        (DTA_Dummy + 119)
+#define  DTA_RelHeight       (DTA_Dummy + 120)
+#endif
+
+
+#define  DTA_SelectDomain    (DTA_Dummy + 121)
+#define  DTA_TotalPVert      (DTA_Dummy + 122)
+#define  DTA_TotalPHoriz     (DTA_Dummy + 123)
+#define  DTA_NominalVert     (DTA_Dummy + 124)
+#define  DTA_NominalHoriz    (DTA_Dummy + 125)
+
+
+/* Printing attributes */
+
+/* Destination x width (LONG) */
+#define  DTA_DestCols        (DTA_Dummy + 400)
+
+/* Destination y width (LONG) */
+#define  DTA_DestRows        (DTA_Dummy + 401)
+
+/* Option flags (UWORD) */
+#define  DTA_Special         (DTA_Dummy + 402)
+
+/* RastPort used when printing (struct RastPort *) */
+#define  DTA_RastPort        (DTA_Dummy + 403)
+
+/* Pointer to base name for ARexx port */
+#define  DTA_ARexxPortName   (DTA_Dummy + 404)
+
+
+/**************************************************************************/
+
+#define  DTST_RAM         1
+#define  DTST_FILE        2
+#define  DTST_CLIPBOARD   3
+#define  DTST_HOTLINK     4
+
+
+/* This structure is attached to the Gadget.SpecialInfo field of the gadget.
+   Use Get/Set calls to access it. */
+struct DTSpecialInfo
+{
+    struct SignalSemaphore si_Lock;
+    ULONG                  si_Flags;
+    LONG                   si_TopVert;    /* Top row (in units) */
+    LONG                   si_VisVert;    /* Number of visible rows (in 
+					     units) */
+    LONG                   si_TotVert;    /* Total number of rows (in units) */
+    LONG                   si_OTopVert;   /* Previous top (in units) */
+    LONG                   si_VertUnit;   /* Number of pixels in vertical
+					     unit */
+    LONG                   si_TopHoriz;   /* Top column (in units) */
+    LONG                   si_VisHoriz;   /* Number of visible columns (in
+					     units) */
+    LONG                   si_TotHoriz;   /* Total number of columns (in
+					     units) */
+    LONG                   si_OTopHoriz;  /* Previous top (in units) */
+    LONG                   si_HorizUnit;  /* Number of pixels in horizontal
+					     unit */
+};
+
+
+/* Object is in layout processing */
+#define  DTSIF_LAYOUT           (1L << 0)
+
+/* Object needs to be layed out */
+#define  DTSIF_NEWSIZE          (1L << 1)
+
+#define  DTSIF_DRAGGING         (1L << 2)
+#define  DTSIF_DRAGSELECT       (1L << 3)
+
+#define  DTSIF_HIGHLIGHT        (1L << 4)
+
+/* Object is being printed */
+#define  DTSIF_PRINTING         (1L << 5)
+
+/* Object is in layout process */
+#define  DTSIF_LAYOUTPROC       (1L << 6)
+
+
+
+struct DTMethod
+{
+    STRPTR	 dtm_Label;
+    STRPTR	 dtm_Command;
+    ULONG	 dtm_Method;
+};
+
+
+
+#define  DTM_Dummy            (0x600)
+
+/* Get the environment an object requires */
+#define  DTM_FRAMEBOX         (0x601)
+
+#define  DTM_PROCLAYOUT       (0x602)
+#define  DTM_ASYNCLAYOUT      (0x603)
+
+/* When RemoveDTObject() is called */
+#define  DTM_REMOVEDTOBJECT   (0x604)
+
+#define  DTM_SELECT           (0x605)
+#define  DTM_CLEARSELECTED    (0x606)
+
+#define  DTM_COPY             (0x607)
+#define  DTM_PRINT            (0x608)
+#define  DTM_ABORTPRINT       (0x609)
+
+#define  DTM_NEWMEMBER        (0x610)
+#define  DTM_DISPOSEMEMBER    (0x611)
+
+#define  DTM_GOTO             (0x630)
+#define  DTM_TRIGGER          (0x631)
+
+#define  DTM_OBTAINDRAWINFO   (0x640)
+#define  DTM_DRAW             (0x641)
+#define  DTM_RELEASEDRAWINFO  (0x642)
+
+#define  DTM_WRITE            (0x650)
+
+
+struct FrameInfo
+{
+    ULONG       fri_PropertyFlags;   /* DisplayInfo (graphics/displayinfo.h) */
+    Point       fri_Resolution;      /* DisplayInfo */
+
+    UBYTE       fri_RedBits;
+    UBYTE       fri_GreenBits;
+    UBYTE       fri_BlueBits;
+
+    struct
+    {
+	ULONG Width;
+	ULONG Height;
+	ULONG Depth;
+	
+    } fri_Dimensions;
+    
+    struct Screen    *fri_Screen;
+    struct ColorMap  *fri_ColorMap;
+
+    ULONG             fri_Flags;    /* See below */
+};
+
+#define	FIF_SCALABLE	0x1
+#define	FIF_SCROLLABLE	0x2
+#define	FIF_REMAPPABLE	0x4
+
+
+/* DTM_REMOVEDTOBJECT, DTM_CLEARSELECTED, DTM_COPY, DTM_ABORTPRINT */
+struct dtGeneral
+{
+    ULONG              MethodID;
+    struct GadgetInfo *dtg_GInfo;
+};
+
+/* DTM_SELECT */
+struct dtSelect
+{
+    ULONG              MethodID;
+    struct GadgetInfo *dts_GInfo;
+    struct Rectangle   dts_Select;
+};
+
+/* DTM_FRAMEBOX */
+struct dtFrameBox
+{
+    ULONG              MethodID;
+    struct GadgetInfo *dtf_GInfo;
+    struct FrameInfo  *dtf_ContentsInfo;        /* Input */
+    struct FrameInfo  *dtf_FrameInfo;           /* Output */
+    ULONG              dtf_SizeFrameInfo;
+    ULONG              dtf_FrameFlags;
+};
+
+#ifndef  FRAMEF_SPECIFY
+#define  FRAMEF_SPECIFY  (1 << 0)
+#endif
+
+
+/* DTM_GOTO */
+struct dtGoto
+{
+    ULONG              MethodID;
+    struct GadgetInfo *dtg_GInfo;
+    STRPTR             dtg_NodeName;          /* Node to goto */
+    struct TagItem    *dtg_AttrList;          /* Additional attributes */
+};
+
+/* DTM_TRIGGER */
+struct dtTrigger
+{
+    ULONG              MethodID;
+    struct GadgetInfo *dtt_GInfo;
+    ULONG              dtt_Function;
+    APTR               dtt_Data;
+};
+
+#define  STM_PAUSE           1
+#define  STM_PLAY            2
+#define  STM_CONTENTS        3
+#define  STM_INDEX	     4
+#define  STM_RETRACE         5
+#define  STM_BROWSE_PREV     6
+#define  STM_BROWSE_NEXT     7
+
+#define  STM_NEXT_FIELD      8
+#define  STM_PREV_FIELD	     9
+#define  STM_ACTIVATE_FIELD  10
+
+#define  STM_COMMAND         11
+
+#define  STM_REWIND          12
+#define  STM_FASTFORWARD     13
+#define  STM_STOP            14
+#define  STM_RESUME          15
+#define  STM_LOCATE          16
+
+/* Printer IO request */
+union printerIO
+{
+    struct IOStdReq ios;
+    /* TODO: No printer stuff yet.
+       struct IODRPReq iodrp;
+       struct IOPrtCmdReq iopc; */
+};
+
+
+/* DTM_PRINT */
+struct dtPrint
+{
+    ULONG              MethodID;
+    struct GadgetInfo *dtp_GInfo;           /* Gadget information */
+    union printerIO   *dtp_PIO;             /* Printer IO request */
+    struct TagItem    *dtp_AttrList;        /* Additional attributes */
+};
+
+/* DTM_DRAW */
+struct dtDraw
+{
+    ULONG            MethodID;
+    struct RastPort *dtd_RPort;
+    LONG             dtd_Left;
+    LONG             dtd_Top;
+    LONG             dtd_Width;
+    LONG             dtd_Height;
+    LONG             dtd_TopHoriz;
+    LONG             dtd_TopVert;
+    struct TagItem  *dtd_AttrList;          /* Additional attributes */
+};
+
+/* DTM_WRITE */
+struct dtWrite
+{
+    ULONG              MethodID;
+    struct GadgetInfo *dtw_GInfo;          /* Gadget information */
+    BPTR               dtw_FileHandle;     /* File handle to write to */
+    ULONG              dtw_Mode;
+    struct TagItem    *dtw_AttrList;       /* Additional attributes */
+};
+
+
+/* Save data as IFF data */
+#define  DTWM_IFF        0
+
+/* Save data as local data format */
+#define  DTWM_RAW        1
+
+
+#endif /* DATATYPES_DATATYPESCLASS_H */
