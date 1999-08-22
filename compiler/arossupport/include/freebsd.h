c3b6d9991a51cf5adc5b30195b094d18887bd3e0
iaint <iaint@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-08-22 23:17:45 +0000

    Changes for FreeBSD 3.0
    git-svn-id: https://svn.aros.org/svn/aros/trunk@6215 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/arossupport/include/freebsd.h
+++ compiler/arossupport/include/freebsd.h
@@ -12,8 +12,16 @@
 #define HAS_STRING_H
 #define __AROS_STRUCTURE_ALIGNMENT  8
 
-/* To handle the slightly different procedure naming */
+/*
+    To handle the slightly different procedure naming under different
+    version of the FreeBSD compiler. Thankfully we have the very helpful
+    preprocessor symbol __ELF__ defined for us. 
+
+    If we don't have these, then we can use the defaults.
+*/
+#if !defined(__ELF__)
 #define __AROS_SLIB_ENTRY(n,s)          s ## _ ## n
 #define AROS_ASMSYMNAME(s)	      s
+#endif
 
 #endif /* AROS_FREEBSD_H */
