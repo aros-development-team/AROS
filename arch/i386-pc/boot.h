d27982e3b846cc99aaa0533729b31464985f11a8
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-09-18 09:34:44 +0000

    Base for native-i386 AROS
    git-svn-id: https://svn.aros.org/svn/aros/trunk@6253 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/i386-pc/boot.h
@@ -0,0 +1,9 @@
+#ifndef AROS_BOOT_H
+#define AROS_BOOT_H
+
+#define DEF_INITSEG	0x9800
+#define DEF_SYSSEG	0x0100
+#define DEF_SETUPSEG	0x9820
+#define DEF_SYSSIZE	580 /* kernel size in sectors */
+
+#endif /* AROS_BOOT_H */
---
+++ arch/i386-pc/segments.h
@@ -0,0 +1,7 @@
+#ifndef _ASM_SEGMENTS_H
+#define _ASM_SEGMENTS_H
+
+#define KERNEL_CS 0x08
+#define KERNEL_DS 0x10
+
+#endif
