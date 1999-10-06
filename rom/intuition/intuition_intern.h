790ba9adc45198bfcac19169f5035cadc20d60e0
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-10-06 19:57:35 +0000

    new intui_openwindow parameter for backfill hook
    git-svn-id: https://svn.aros.org/svn/aros/trunk@6371 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/intuition/intuition_intern.h
+++ rom/intuition/intuition_intern.h
@@ -234,7 +234,8 @@ extern void intui_EndRefresh (struct Window * window,
 	    BOOL complete,
 	    struct IntuitionBase * IntuitionBase);
 extern void intui_MoveWindow (struct Window * window, WORD dx, WORD dy);
-extern int  intui_OpenWindow (struct Window *, struct IntuitionBase *, struct BitMap * SuperBitMap);
+extern int  intui_OpenWindow (struct Window *, struct IntuitionBase *, 
+			      struct BitMap * SuperBitMap, struct Hook *backfillhook);
 extern void intui_SetWindowTitles (struct Window *, UBYTE *, UBYTE *);
 extern void intui_RefreshWindowFrame(struct Window *win);
 extern struct Window *intui_FindActiveWindow(struct InputEvent *ie, BOOL *swallow_event, struct IntuitionBase *IntuitionBase);
