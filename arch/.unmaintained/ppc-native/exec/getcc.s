4e0a03eb713ba8915e11e17138448f5ec6ff44f6
przemek <przemek@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-10-18 10:32:44 +0000

    Not very usable on PPC, but getcc implemented, anyway.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@6462 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/.unmaintained/ppc-native/exec/getcc.s
@@ -0,0 +1,76 @@
+/*
+     (C) 1995-96 AROS - The Amiga Research OS
+     $Id$
+ 
+     Desc:
+     Lang:
+*/
+
+/*****************************************************************************
+ 
+    NAME
+ 
+ 	AROS_LH0(UWORD, GetCC,
+ 
+    LOCATION
+ 	struct ExecBase *, SysBase, 88, Exec)
+ 
+    FUNCTION
+ 	Read the contents of the sr in a easy and compatible way.
+ 
+    INPUTS
+ 
+    RESULT
+ 	The contents of sr as a UWORD.
+ 
+    NOTES
+ 
+    EXAMPLE
+ 
+    BUGS
+ 
+    SEE ALSO
+ 	SetSR()
+ 
+    INTERNALS
+	I don't think this function will have any real use on PPC...
+	This function maps PPC:
+		CA to C
+		OV to V
+		EQ to Z
+		?? to N
+		CA to X
+ 
+    HISTORY
+ 
+******************************************************************************/
+
+	#include "machine.i"
+
+	.text
+	.balign 4
+	.globl	AROS_SLIB_ENTRY(GetCC,Exec)
+	.type	AROS_SLIB_ENTRY(GetCC,Exec),@function
+
+AROS_SLIB_ENTRY(GetCC,Exec):
+	subr
+	push	scr
+	li	ret,0
+	bne	_notequal	# Z=0
+	ori	ret,ret,4	# Z=1
+_notequal:
+	bge	_positive	# N=0
+	ori	ret,ret,8	# N=1
+_positive:
+	mfxer	scr
+	rlwinm.	scr,scr,1,0,0
+	beq	_noovfl		# 0? so no Overflow
+	ori	ret,ret,2	# V=1
+_noovfl:
+	mfxer	scr
+	rlwinm.	scr,scr,2,0,0
+	beq	_nocarry	# 0? so no Carry	
+	ori	ret,ret,17	# X=1 C=1
+_nocarry:	
+	pop	scr
+	rts
---
+++ arch/.unmaintained/ppc-native/exec/setsr.s
@@ -0,0 +1,56 @@
+/*
+     (C) 1995-96 AROS - The Amiga Research OS
+     $Id$
+ 
+     Desc:
+     Lang:
+*/
+
+/*****************************************************************************
+ 
+    NAME
+ 
+ 	AROS_LH2(ULONG, SetSR,
+ 
+    SYNOPSIS
+ 	AROS_LHA(ULONG, newSR, D0),
+ 	AROS_LHA(ULONG, mask,  D1),
+ 
+    LOCATION
+ 	struct ExecBase *, SysBase, 24, Exec)
+ 
+    FUNCTION
+ 	Read/Modify the CPU status register in an easy way. Only the bits set in
+ 	the mask parameter will be changed.
+ 
+    INPUTS
+ 	newSR - New contents of sr.
+ 	mask  - Bits to change.
+ 
+    RESULT
+ 	Old contents of sr.
+ 
+    NOTES
+ 
+    EXAMPLE
+ 
+    BUGS
+ 
+    SEE ALSO
+ 
+    INTERNALS
+ 
+    HISTORY
+ 
+******************************************************************************/
+
+	#include "machine.i"
+
+	.text
+	.balign 4
+	.globl	AROS_SLIB_ENTRY(SetSR,Exec)
+	.type	AROS_SLIB_ENTRY(SetSR,Exec),@function
+AROS_SLIB_ENTRY(SetSR,Exec):
+	subr
+	li	ret,0
+	rts
