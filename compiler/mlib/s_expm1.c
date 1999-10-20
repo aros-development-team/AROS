f6902c8270d762a48dcfbfa316693d80e13e70cb
bernie <bernie@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
1999-10-20 22:25:04 +0000

    removed compiler warnings
    git-svn-id: https://svn.aros.org/svn/aros/trunk@6473 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/mlib/s_expm1.c
+++ compiler/mlib/s_expm1.c
@@ -192,9 +192,11 @@ Q5  =  -2.01099218183624371326e-07; /* BE8AFDB7 6E09C32D */
 	    e  = (x*(e-c)-c);
 	    e -= hxs;
 	    if(k== -1) return 0.5*(x-e)-0.5;
-	    if(k==1) 
+	    if(k==1)
+	    {
 	       	if(x < -0.25) return -2.0*(e-(x+0.5));
 	       	else 	      return  one+2.0*(x-e);
+	    }
 	    if (k <= -2 || k>56) {   /* suffice to return exp(x)-1 */
 	        y = one-(e-x);
 	        __HI(y) += (k<<20);	/* add k to y's exponent */
