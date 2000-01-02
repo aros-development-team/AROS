96eeaba52ea76e2f25eb3535f57f964c49230920
hkiel <hkiel@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-01-02 01:02:18 +0000

    Fixed sign problem
    git-svn-id: https://svn.aros.org/svn/aros/trunk@6668 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/classes/gadgets/arospalette/support.c
+++ workbench/classes/gadgets/arospalette/support.c
@@ -53,25 +53,27 @@ VOID GetGadgetIBox(Object *o, struct GadgetInfo *gi, struct IBox *ibox)
 UWORD GetPalettePen(struct PaletteData *data, struct DrawInfo *dri, UWORD idx)
 {
     UWORD pen;
-    
+
     if (data->pd_ColorTable)
-    	pen = data->pd_ColorTable[idx];
+	pen = data->pd_ColorTable[idx];
     else
-    	pen = dri->dri_Pens[idx];
-    
+//	pen = dri->dri_Pens[idx];
+	pen = idx + data->pd_ColorOffset;
+
     return (pen);
 }
 
 /*********************
 **  Colors2Depth()  **
 *********************/
+
 UBYTE Colors2Depth(UWORD numcolors)
 {
     UBYTE depth = 0;
-    
+
     while ((1 << depth) < numcolors)
     	depth ++;
-    	
+
     return (depth);
 }
 
@@ -82,15 +84,15 @@ UBYTE Colors2Depth(UWORD numcolors)
 BOOL InsidePalette(struct PaletteData *data, WORD x, WORD y)
 {
     /* Georg Steger */
-    
+
     return TRUE;
-    
+
 #if 0
     BOOL inside = FALSE;
     struct IBox *pbox = &(data->pd_PaletteBox);
 
     /* Inside palette bounding box ? */
-    
+
     if (    (x > pbox->Left)
 	 && (x < pbox->Left + pbox->Width - 1)
 	 && (y > pbox->Top)
@@ -100,7 +102,7 @@ BOOL InsidePalette(struct PaletteData *data, WORD x, WORD y)
    	/* Must do additional testing, since we might
    	** not have 2^n number of colors
    	*/
-   	
+
    	if (data->pd_NumColors > ComputeColor(data, x, y))
    	{
    	    inside = TRUE;
@@ -110,7 +112,7 @@ BOOL InsidePalette(struct PaletteData *data, WORD x, WORD y)
 #endif
 
 }
-    	
+
 
 
 /*********************
@@ -120,14 +122,14 @@ BOOL InsidePalette(struct PaletteData *data, WORD x, WORD y)
 UWORD ComputeColor(struct PaletteData *data, WORD x, WORD y)
 {
      UWORD row, col;
-     
-     UBYTE color;
-  
+
+     WORD color;
+
      col = (x - data->pd_PaletteBox.Left)	/ data->pd_ColWidth;
      row = (y - data->pd_PaletteBox.Top)	/ data->pd_RowHeight;
-     
+
      color = data->pd_NumCols * row + col;
-     
+
      /* Georg Steger */
      if (color < 0)
      {
@@ -138,7 +140,7 @@ UWORD ComputeColor(struct PaletteData *data, WORD x, WORD y)
     	 color = data->pd_NumColors - 1;
      }
      
-     return (color);
+     return (UWORD)color;
 }
 
 /********************
@@ -153,22 +155,22 @@ VOID RenderPalette(struct PaletteData *data, struct DrawInfo *dri,
     WORD left, top;
     register UWORD col, row;
     struct IBox *pbox = &(data->pd_PaletteBox);
-    
+
     EnterFunc(bug("RenderPalette(data=%p, dri=%p, rp=%p)\n", data, dri, rp));
 
     top  = pbox->Top;
 
     colors_left = data->pd_NumColors;    
     SetDrMd(rp, JAM1);
-        	
+
     for (row = data->pd_NumRows; row; row --)
     {
     	left = pbox->Left;
     	for (col = MIN(data->pd_NumCols, colors_left); col; col --)
     	{
-    	    
+
     	    SetAPen(rp, GetPalettePen(data, dri, currentcolor));
-    	    
+
     	    RectFill(rp, left, top,
     	    	left + data->pd_ColWidth - VSPACING - 1, 
     	    	top + data->pd_RowHeight - HSPACING - 1 );
@@ -176,18 +178,18 @@ VOID RenderPalette(struct PaletteData *data, struct DrawInfo *dri,
     	    D(bug("Rectfilling area (%d, %d, %d, %d)\n with color %d", left, top,
     	    left + data->pd_ColWidth - VSPACING - 1, top + data->pd_RowHeight - HSPACING,
     	    currentcolor));
-    	    	
+
 	    currentcolor ++;
 
     	    left += data->pd_ColWidth;
-    	
+
     	} /* for (each row) */
     	top += data->pd_RowHeight;
 
     	colors_left -= data->pd_NumCols;
-    	
+
     } /* for (each column) */
-    
+
     ReturnVoid("RenderPalette");
 }
 
@@ -202,21 +204,21 @@ VOID UpdateActiveColor( struct PaletteData	*data,
 {
 
     WORD left, top, right, bottom;
-    
+
     struct IBox framebox;
-    
+
     EnterFunc(bug("UpdateActiveColor(data=%p, dri=%p, rp=%p)\n",
     			data, dri, rp));
-    
+
     SetAPen(rp, dri->dri_Pens[BACKGROUNDPEN]);
     SetDrMd(rp, JAM1);
 
     if (data->pd_OldColor != data->pd_Color)
     {
-    	
+
     	left = data->pd_PaletteBox.Left + (data->pd_OldColor % data->pd_NumCols) * data->pd_ColWidth;
     	top  = data->pd_PaletteBox.Top  + (data->pd_OldColor / data->pd_NumCols) * data->pd_RowHeight;
-    
+
     	D(bug("clearing old selected: (%d, %d, %d, %d) oldcolor=%d\n",
     		left, top, left + data->pd_ColWidth, top + data->pd_RowHeight, data->pd_OldColor));
 
@@ -229,16 +231,16 @@ VOID UpdateActiveColor( struct PaletteData	*data,
 
     	/* Rerender in original color */
     	SetAPen(rp, GetPalettePen(data, dri, data->pd_OldColor + data->pd_ColorOffset));
-    
+
     	RectFill(rp, left, top,
     	    left + data->pd_ColWidth  - VSPACING - 1,
     	    top  + data->pd_RowHeight - HSPACING - 1);
-    
+
     }
 
     left  = data->pd_PaletteBox.Left + (data->pd_Color % data->pd_NumCols) * data->pd_ColWidth;
     top   = data->pd_PaletteBox.Top  + (data->pd_Color / data->pd_NumCols) * data->pd_RowHeight;
-    
+
     /* Right & bottom of *colored* area */
     right  = left + data->pd_ColWidth  - VSPACING - 1;
     bottom = top  + data->pd_RowHeight - HSPACING - 1;
@@ -246,20 +248,20 @@ VOID UpdateActiveColor( struct PaletteData	*data,
     /* Render new active entry */
     D(bug("rendering new selected: (%d, %d, %d, %d), color=%d\n",
     	left, top, right, bottom, data->pd_Color));
-    
-    
+
+
     /* Draw some borders */
-    
+
     SetAPen(rp, dri->dri_Pens[BACKGROUNDPEN]);
-    
+
     /* We draw left & right from top to bottom, but draw top & bottom
     ** so they don't overlap with what's allready drawn
     */
-    
+
     /* left */
     RectFill(rp, left, top,
     	left + 1, bottom);
-    	
+
     /* right */	
     RectFill(rp, right - 1, top,
     	right, bottom);
@@ -267,22 +269,22 @@ VOID UpdateActiveColor( struct PaletteData	*data,
     /* top */
     RectFill(rp, left + 2, top,
     	right - 2, top + 1);
-    	
+
     /* bottom */
     RectFill(rp, left + 2, bottom - 1,
     	right - 2, bottom);
-    	
+
     /* Draw recessed frame around selected color */
     framebox.Left   = left - VBORDER;
     framebox.Top    = top  - HBORDER;
     framebox.Width  = data->pd_ColWidth  + VBORDER;
     framebox.Height = data->pd_RowHeight + HBORDER;
-    
+
     RenderFrame(rp, &framebox, dri->dri_Pens, TRUE, AROSPaletteBase);
-    	
+
     /* The newly update color becomes the new OldColor */
     data->pd_OldColor = data->pd_Color;
-    
+
     ReturnVoid("UpdateActiveColor");
 }
 
@@ -299,7 +301,7 @@ void DrawDisabledPattern(struct RastPort *rport, struct IBox *gadbox, UWORD pen,
 {
 
     WORD x, y;
-    
+
     EnterFunc(bug("DrawDisabledPattern(rp=%p, gadbox=%p, pen=%d)\n",
     		rport, gadbox, pen));
 
@@ -325,7 +327,7 @@ void DrawDisabledPattern(struct RastPort *rport, struct IBox *gadbox, UWORD pen,
             }
 	}
     }
-    	
+
     ReturnVoid("DrawDisabledPattern");
 }
 
@@ -337,7 +339,7 @@ struct TextFont *PrepareFont(struct RastPort *rport, struct IntuiText *itext,
 			     struct PaletteBase_intern *AROSPaletteBase)
 {
     struct TextFont *font;
-    
+
     EnterFunc(bug("PrepareFont(rp=%p, itext=%p)\n", rport, itext));
 
     if (itext->ITextFont)
@@ -375,7 +377,7 @@ void DisposeFont(struct RastPort *rport,
 	SetFont(rport, oldfont);
 	CloseFont(font);
     }
-    
+ 
     ReturnVoid("DisposeFont");
 }
 
@@ -391,7 +393,7 @@ BOOL RenderLabel( struct Gadget *gad, struct IBox *gadbox,
     STRPTR text;
     int len = 0, x, y;
     UWORD width, height;
-    
+
     EnterFunc(bug("RenderLabel(gad=%p, gadbox=%p, rp=%p, labelplace=%ld)\n",
     	gad, gadbox, rport, labelplace));
 
@@ -475,13 +477,13 @@ VOID RenderFrame(struct RastPort *rp, struct IBox *gadbox, UWORD *pens,
 {
     WORD left, top, right, bottom;
     UBYTE left_top_pen, right_bottom_pen;
-    
+
     EnterFunc(bug("RenderFrame(rp=%p, gadbox=%p, pens=%p)\n",
     		rp, gadbox, pens));
-   
+
     left = gadbox->Left; top = gadbox->Top;
     right = left + gadbox->Width - 1; bottom = top + gadbox->Height - 1;
-    
+ 
     if (recessed)
     {
     	left_top_pen	 = SHADOWPEN;
@@ -492,11 +494,11 @@ VOID RenderFrame(struct RastPort *rp, struct IBox *gadbox, UWORD *pens,
     	left_top_pen	 = SHINEPEN;
     	right_bottom_pen = SHADOWPEN;
     }
-    
+
     /* Left */
     SetAPen(rp, pens[left_top_pen]);
     RectFill(rp, left, top, left + HBORDER - 1, bottom);
-    
+
     /* Right */
     SetAPen(rp, pens[right_bottom_pen]);
     RectFill(rp, right - HBORDER + 1, top, right, bottom);
@@ -504,10 +506,11 @@ VOID RenderFrame(struct RastPort *rp, struct IBox *gadbox, UWORD *pens,
     /* Top */
     SetAPen(rp, pens[left_top_pen]);    		
     RectFill(rp, left + HBORDER, top, right - 1, top + HBORDER - 1);
-    
+ 
     /* Bottom */
     SetAPen(rp, pens[right_bottom_pen]);
     RectFill(rp, left + 1, bottom - HBORDER + 1, right, bottom);
 
     ReturnVoid("RenderFrame");
 }
+
