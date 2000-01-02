885b760d2b938ec52cbe66152b4e58defc6b46f8
nlorentz <nlorentz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-01-02 22:42:49 +0000

    Added cybergfx
    git-svn-id: https://svn.aros.org/svn/aros/trunk@6684 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/inline/cybergraphics.h
@@ -0,0 +1,13 @@
+#ifndef _INLINE_CYBERGRAPHICS_H
+#define _INLINE_CYBERGRAPHICS_H
+
+#ifndef __INLINE_MACROS_H
+#include <inline/macros.h>
+#endif
+
+#ifndef CGFX_BASE_NAME
+#define CGFX_BASE_NAME CyberGfxBase
+#endif
+
+
+#endif /* _INLINE_CYBERGRAPHICS_H */
