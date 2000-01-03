e0cf98346e65c58f06b8bc8012b3e8034bf55c23
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-01-03 08:09:17 +0000

    AROS size updated and misc.resource, intuition.library and console.device added
    git-svn-id: https://svn.aros.org/svn/aros/trunk@6691 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-pc/boot.h
+++ arch/i386-pc/boot.h
@@ -4,6 +4,6 @@
 #define DEF_INITSEG	0x9000
 #define DEF_SYSSEG	0x1000
 #define DEF_SETUPSEG	0x9020
-#define DEF_SYSSIZE	580 /* kernel size in sectors */
+#define DEF_SYSSIZE	780 /* kernel size in sectors */
 
 #endif /* AROS_BOOT_H */
