6ac2ba46cdff7ae8167305f20957bf5a5a6f87fd
SDuvan <SDuvan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-01-05 17:38:28 +0000

    New CLI command
    git-svn-id: https://svn.aros.org/svn/aros/trunk@6709 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/c/RequestFile.c
@@ -0,0 +1,267 @@
+/*
+    (C) 1997-99 AROS - The Amiga Replacement OS
+    $Id$
+
+    Desc: RequestFile CLI command
+    Lang: English
+*/
+
+/*****************************************************************************
+
+    NAME
+
+        RequestFile
+
+    SYNOPSIS
+
+        DRAWER,FILE/K,PATTERN/K,TITLE/K,POSITIVE/K,NEGATIVE/K,
+        ACCEPTPATTERN/K,REJECTPATTERN/K,SAVEMODE/S,MULTISELECT/S,
+        DRAWERSONLY/S,NOICONS/S,PUBSCREEN/K
+
+    LOCATION
+
+        Workbench:c
+
+    FUNCTION
+
+    INPUTS
+
+    RESULT
+
+        Standard DOS error codes.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+        ?         laguest  Initial version
+	29.12.99  SDuvan   Fixes to make it compile; bugfixes; cleanup
+
+******************************************************************************/
+
+#include <proto/asl.h>
+#include <proto/dos.h>
+#include <proto/exec.h>
+
+#include <dos/dos.h>
+#include <dos/rdargs.h>
+#include <dos/var.h>
+#include <dos/dosasl.h>
+#include <exec/types.h>
+#include <exec/memory.h>
+#include <libraries/asl.h>
+#include <utility/tagitem.h>
+#include <workbench/startup.h>
+
+#include <stdio.h>
+
+#define ARG_TEMPLATE    "DRAWER,FILE/K,PATTERN/K,TITLE/K,POSITIVE/K," \
+                        "NEGATIVE/K,ACCEPTPATTERN/K,REJECTPATTERN/K," \
+                        "SAVEMODE/S,MULTISELECT/S,DRAWERSONLY/S," \
+                        "NOICONS/S,PUBSCREEN/K"
+			
+#define MAX_PATH_LEN    512
+			
+
+enum { ARG_DRAWER = 0, ARG_FILE, ARG_PATTERN, ARG_TITLE, ARG_POSITIVE,
+       ARG_NEGATIVE, ARG_ACCEPTPAT, ARG_REJECTPAT, ARG_SAVEMODE,
+       ARG_MULTISELECT, ARG_DRAWERSONLY, ARG_NOICONS, ARG_PUBSCREEN,
+       TOTAL_ARGS };
+
+static const char version[] = "$VER: RequestFile 41.1 (29.12.1999)\n";
+
+struct TagItem FileTags[] =
+{
+    /* Note: The ordering of these is _important_! */
+    { ASLFR_InitialDrawer , NULL  },
+    { ASLFR_InitialFile   , NULL  },
+    { ASLFR_InitialPattern, NULL  },
+    { ASLFR_TitleText     , NULL  },
+    { ASLFR_PositiveText  , NULL  },
+    { ASLFR_NegativeText  , NULL  },
+    { ASLFR_AcceptPattern , NULL  },
+    { ASLFR_RejectPattern , NULL  },
+    { ASLFR_DoSaveMode    , FALSE },
+    { ASLFR_DoMultiSelect , FALSE },
+    { ASLFR_DrawersOnly   , FALSE },
+    { ASLFR_RejectIcons   , FALSE },
+    { ASLFR_PubScreenName , NULL  },
+    { ASLFR_DoPatterns    , FALSE },
+    { TAG_DONE            , NULL  }
+};
+
+
+/* Temporary to avoid bugs in ASL library */
+struct TagItem frTags[] =
+{
+    { ASL_Hail,	  (ULONG)"" },
+    { ASL_Height,     400 },
+    { ASL_Width,      240 },
+    { ASL_LeftEdge,   20 },
+    { ASL_TopEdge,    20 },
+    { ASL_OKText,     (ULONG)"OKAY" },
+    { ASL_CancelText, (ULONG)"Not OK" },
+    //  { ASL_File,	  (ULONG)"asl.library" },
+    //	{ ASL_Dir,	  (ULONG)"Sys:" },
+    { TAG_DONE,       NULL }
+};
+
+
+struct Library *AslBase;
+
+int main(int argc, char *argv[])
+{
+    struct RDArgs        *rda;
+    struct FileRequester *FileReq;
+    struct WBArg         *WBFiles;
+    IPTR                 *args[TOTAL_ARGS] = { NULL, NULL, NULL, NULL,
+					       NULL, NULL, NULL, NULL,
+					       NULL, NULL, NULL, NULL,
+					       NULL };
+    int                   Return_Value = RETURN_OK;
+    IPTR                  DisplayArgs[1];
+    char                 *Buffer;
+    LONG                  DrawerLength;
+    BOOL                  Success;
+    int                   i;
+
+#define  DoSave    (args[ARG_SAVEMODE] != NULL)
+#define  DoMulti   (args[ARG_MULTISELECT] != NULL)
+#define  DoDrawers (args[ARG_DRAWERSONLY] != NULL)
+#define  DoIcons   (args[ARG_NOICONS] != NULL)
+#define  DoPattern (args[ARG_PATTERN] != NULL)
+
+    AslBase = (struct Library *)OpenLibrary("asl.library", 39);
+
+    if(AslBase == NULL)
+	return RETURN_FAIL; 
+
+    Buffer = (char *)AllocVec(MAX_PATH_LEN, MEMF_ANY | MEMF_CLEAR);
+    if (Buffer != NULL)
+    {
+        rda = ReadArgs(ARG_TEMPLATE, (LONG *)args, NULL);
+        if(rda != NULL)
+        {
+            FileTags[ARG_DRAWER].ti_Data        = (ULONG)args[ARG_DRAWER];
+            FileTags[ARG_FILE].ti_Data          = (ULONG)args[ARG_FILE];
+            FileTags[ARG_PATTERN].ti_Data       = (ULONG)args[ARG_PATTERN];
+            FileTags[ARG_TITLE].ti_Data         = (ULONG)args[ARG_TITLE];
+            FileTags[ARG_POSITIVE].ti_Data      = (ULONG)args[ARG_POSITIVE];
+            FileTags[ARG_NEGATIVE].ti_Data      = (ULONG)args[ARG_NEGATIVE];
+            FileTags[ARG_ACCEPTPAT].ti_Data     = (ULONG)args[ARG_ACCEPTPAT];
+            FileTags[ARG_REJECTPAT].ti_Data     = (ULONG)args[ARG_REJECTPAT];
+            FileTags[ARG_SAVEMODE].ti_Data      = DoSave;
+            FileTags[ARG_MULTISELECT].ti_Data   = DoMulti;
+            FileTags[ARG_DRAWERSONLY].ti_Data   = DoDrawers;
+            FileTags[ARG_NOICONS].ti_Data       = DoIcons;
+            FileTags[ARG_PUBSCREEN].ti_Data     = (ULONG)args[ARG_PUBSCREEN];
+            FileTags[ARG_PUBSCREEN + 1].ti_Data = DoPattern;
+
+            FileReq = (struct FileRequester *)AllocAslRequest(ASL_FileRequest,
+                                                              FileTags);
+            if(FileReq != NULL)
+            {
+                Success = AslRequest(FileReq, frTags); /* Temporary tags */
+
+                if(Success != FALSE)
+                {
+                    if(DoMulti == FALSE)
+                    {
+                        DrawerLength = strlen(FileReq->fr_Drawer) - 1;
+			
+                        Success = AddPart(Buffer, FileReq->fr_Drawer,
+					  MAX_PATH_LEN);
+			
+                        if(Success != FALSE)
+                        {
+                            Success = AddPart((Buffer + DrawerLength),
+                                              FileReq->fr_File,
+                                              MAX_PATH_LEN - DrawerLength);
+			    
+                            if(Success != FALSE)
+                            {
+                                DisplayArgs[0] = (IPTR)Buffer;
+                                VPrintf("\"%s\"\n", DisplayArgs);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        WBFiles = FileReq->fr_ArgList;
+			
+                        DrawerLength = strlen(FileReq->fr_Drawer) - 1;
+			
+                        Success = AddPart(Buffer, FileReq->fr_Drawer,
+                                          MAX_PATH_LEN);
+			
+                        if(Success != FALSE)
+                        {
+                            for (i = 0; i != FileReq->fr_NumArgs; i++)
+                            {
+                                Success = AddPart((Buffer + DrawerLength),
+                                                  WBFiles[i].wa_Name,
+                                                  MAX_PATH_LEN - DrawerLength);
+				
+                                if(Success != FALSE)
+                                {
+                                    DisplayArgs[0] = (IPTR)Buffer;
+                                    VPrintf("\"%s\" ", DisplayArgs);
+                                }
+                            }    
+                        }
+			
+                        PutStr("\n");
+                    }
+                }
+                else
+                {
+                    if (IoErr() == 0)
+                    {
+                        /* We cancelled it!!
+                         */
+                        Return_Value = RETURN_WARN;
+			
+                        SetIoErr(ERROR_BREAK);
+                    }
+                    else
+                    {
+			PrintFault(IoErr(), NULL);
+                        Return_Value = RETURN_FAIL;
+                    }
+                }
+		
+                FreeAslRequest((struct FileRequester *)FileReq);
+            }
+            else
+            {
+                PrintFault(IoErr(), "RequestFile #2");
+		Return_Value = RETURN_ERROR;
+            }
+	    
+            FreeArgs(rda);
+        }
+        else
+        {
+            PrintFault(IoErr(), "RequestFile #1");
+	    Return_Value = RETURN_ERROR;
+        }
+	
+        FreeVec(Buffer);
+    }
+    else
+    {
+	SetIoErr(ERROR_NO_FREE_STORE);
+	PrintFault(IoErr(), NULL);
+	Return_Value = RETURN_FAIL;
+    }
+    
+    return Return_Value;
+    
+} /* main */
