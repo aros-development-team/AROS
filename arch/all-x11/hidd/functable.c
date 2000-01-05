8b68765f38f0e823094081a2b70130b243f13d7c
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-01-05 10:40:46 +0000

    Update to make this library work. Unfortunately it has been necessary to add functable.c to the sources.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@6706 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/all-x11/hidd/functable.c
@@ -0,0 +1,27 @@
+/*
+    Copyright (C) 1995-1998 AROS - The Amiga Research OS
+    *** Automatic generated file. Do not edit ***
+    Desc: Function table for X11Cl
+    Lang: english
+*/
+#ifndef LIBCORE_COMPILER_H
+#   include <libcore/compiler.h>
+#endif
+#ifndef NULL
+#define NULL ((void *)0)
+#endif
+
+#include "libdefs.h"
+extern void AROS_SLIB_ENTRY(LC_BUILDNAME(OpenLib),LibHeader) (void);
+extern void AROS_SLIB_ENTRY(LC_BUILDNAME(CloseLib),LibHeader) (void);
+extern void AROS_SLIB_ENTRY(LC_BUILDNAME(ExpungeLib),LibHeader) (void);
+extern void AROS_SLIB_ENTRY(LC_BUILDNAME(ExtFuncLib),LibHeader) (void);
+
+void *const LIBFUNCTABLE[]=
+{
+    AROS_SLIB_ENTRY(LC_BUILDNAME(OpenLib),LibHeader),
+    AROS_SLIB_ENTRY(LC_BUILDNAME(CloseLib),LibHeader),
+    AROS_SLIB_ENTRY(LC_BUILDNAME(ExpungeLib),LibHeader),
+    AROS_SLIB_ENTRY(LC_BUILDNAME(ExtFuncLib),LibHeader),
+    (void *)-1L
+};
