c03a362805047782f65cf62e6b0dc7b46914f987
bernie <bernie@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-01-07 18:00:47 +0000

    ready for pattern matching but not fully functional yet
    git-svn-id: https://svn.aros.org/svn/aros/trunk@6721 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/delete.c
+++ workbench/c/delete.c
@@ -1,40 +1,149 @@
 /*
-    (C) 1995-98 AROS - The Amiga Research OS
+    (C) 1995-97 AROS - The Amiga Replacement OS
     $Id$
 
-    Desc: Delete CLI command
+    Desc: Delete CLI Command
     Lang: english
 */
 
-#include <exec/memory.h>
-#include <exec/execbase.h>
-#include <proto/exec.h>
-#include <dos/dos.h>
+/*****************************************************************************
+
+    NAME
+	Delete
+
+    SYNOPSIS
+	Delete <files> [ALL] [FORCE] [QUIET]
+
+	FILE/M/A,ALL/S,QUIET/S,FORCE/S
+
+    LOCATION
+	Workbench/c
+
+    FUNCTION
+	Deletes files and directories
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+
 #include <proto/dos.h>
-#include <utility/tagitem.h>
+#include <proto/exec.h>
 
-static const char version[] = "$VER: delete 41.1 (14.3.1997)\n";
+#include <dos/dos.h>
+#include <dos/dosasl.h>
+#include <exec/memory.h>
 
-int main (int argc, char ** argv)
+#define ARG_TEMPLATE    "FILE/M/A,ALL/S,QUIET/S,FORCE/S"
+#define ARG_FILE	0
+#define ARG_ALL		1
+#define ARG_QUIET	2
+#define ARG_FORCE	3
+#define TOTAL_ARGS      4
+
+/* To define whether a command line switch was set or not.
+ */
+#define NOT_SET         0
+
+/* Maximum file path length.
+ */
+#define MAX_PATH_LEN    512
+
+static const char version[] = "$VER: delete 41.2 (6.1.2000)\n";
+static /*const*/ char cmdname[] = "Delete";
+
+
+int Do_Delete(struct AnchorPath *a, STRPTR *files, BOOL all, BOOL quiet, BOOL force);
+
+
+int main(int argc, char *argv[])
 {
-    STRPTR args[1]={ 0 };
-    struct RDArgs *rda;
-    LONG error=0;
+	struct RDArgs		* rda;
+	struct AnchorPath	* apath;
+	LONG			args[TOTAL_ARGS] = {};
+	int			Return_Value;
 
-    rda=ReadArgs("FILE/A",(IPTR *)args,NULL);
+	Return_Value = RETURN_OK;
 
-    if(rda!=NULL)
-    {
-#warning FIXME: pattern matching
-	if (!DeleteFile(args[0]))
-	    error = RETURN_ERROR;
+	apath = AllocVec(sizeof(struct AnchorPath) + MAX_PATH_LEN, MEMF_ANY | MEMF_CLEAR);
+	if (apath)
+	{
+		/* This is ap_Strlen under AmigaOS. */
+		apath->ap_Strlen = MAX_PATH_LEN;
 
-	FreeArgs(rda);
-    }
-    else
-	error=RETURN_FAIL;
+		if ((rda = ReadArgs(ARG_TEMPLATE, (LONG *)args, NULL)))
+		{
+			Return_Value = Do_Delete(apath,
+				(STRPTR *)args[ARG_FILE],
+				(BOOL)args[ARG_ALL],
+				(BOOL)args[ARG_QUIET],
+				(BOOL)args[ARG_FORCE]);
 
-    if(error)
-	PrintFault(IoErr(),"Delete");
-    return error;
-}
+			FreeArgs(rda);
+		}
+		else
+		{
+			PrintFault(IoErr(), cmdname);
+			Return_Value = RETURN_FAIL;
+		}
+
+	}
+	else
+		Return_Value = RETURN_FAIL;
+
+	FreeVec(apath);
+
+	return Return_Value;
+
+} /* main */
+
+/* Defines whether MathFirst(), etc has matched a file.
+ */
+#define MATCHED_FILE 0
+
+int Do_Delete(struct AnchorPath *a, STRPTR *files, BOOL all, BOOL quiet, BOOL force)
+{
+	LONG Result;
+	int  Return_Value;
+
+	Return_Value = RETURN_OK;
+
+	while (*files != NULL)
+	{
+		Result = MatchFirst(*files, a);
+
+		do
+		{
+			if (Result == MATCHED_FILE)
+			{
+				if (!DeleteFile(a->ap_Buf))
+					Return_Value = RETURN_FAIL;
+			}
+			else
+			{
+				PrintFault(IoErr(), cmdname);
+				Return_Value = RETURN_FAIL;
+			}
+		} while (((Result = MatchNext(a)) == MATCHED_FILE) && Return_Value == RETURN_OK);
+
+		MatchEnd(a);
+
+		files++;
+	}
+
+	return Return_Value;
+
+} /* Do_Delete */
