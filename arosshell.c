fa7ac7b8efef6515cfb8145a78b9c3940e70b166
bernie <bernie@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-01-14 22:16:58 +0000

    experimental boot console
    git-svn-id: https://svn.aros.org/svn/aros/trunk@6803 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arosshell.c
+++ arosshell.c
@@ -9,6 +9,7 @@
 #include <dos/dos.h>
 #include <intuition/intuitionbase.h>
 #include <proto/dos.h>
+#include <proto/intuition.h>
 #include <proto/exec.h>
 
 extern struct DosLibrary *DOSBase;
@@ -16,32 +17,75 @@ struct IntuitionBase * IntuitionBase;
 
 #define CANNOT_OPEN_INTUITION	"Cannot open intuition.library\n"
 #define CANNOT_LOAD_SHELL	"Unable to load C:shell\n"
+#define CANNOT_OPEN_CON		"Cannot open boot console\n"
+
+#warning FIXME: for some reason opening the console here will crash the system 
+#undef BOOT_ON_CONSOLE_WINDOW
+
 
 int main(void)
 {
-    BPTR segs;
+	BPTR segs, in, out;
+	LONG rc;
 
-    if (!(IntuitionBase = (struct IntuitionBase *)OpenLibrary ("intuition.library",39)) )
-    {
-	Write (Output (), CANNOT_OPEN_INTUITION, sizeof(CANNOT_OPEN_INTUITION)-1);
-	return 20;
-    }
+	if ((IntuitionBase = (struct IntuitionBase *)OpenLibrary("intuition.library", 39)))
+	{
 
-    /* Load the boot shell */
-    segs = LoadSeg ("C:shell");
+#ifdef BOOT_ON_CONSOLE_WINDOW
+		/* Open a new console for the initial shell
+		 */
+		if (!(in = Open("CON:////AROS Boot Shell/AUTO", MODE_READWRITE)))
+		{
+			/* No need to abort in case of failure: a process may live even
+			 * with a NULL pr_COS/pr_CIS.
+			 */
+			PutStr(CANNOT_OPEN_CON);
+		}
 
-    if (segs)
-    {
-	/* Execute it. */
-	RunCommand (segs, AROS_STACKSIZE, "FROM S:Startup-Sequence", 23);
-	UnLoadSeg (segs);
-    }
-    else
-    {
-	Write (Output (), CANNOT_LOAD_SHELL, sizeof(CANNOT_LOAD_SHELL)-1);
-    }
+		out = in; /* FIXME: out = DupLock(in); */
 
-    CloseLibrary ((struct Library *)IntuitionBase);
+		/* Use boot console for standard I/O. */
+		in = SelectInput(in);
+		out = SelectOutput(out);
+		/* FIXME: what about pr_CES? */
+#endif
 
-    return 0;
+		/* Load the boot shell
+		 * TODO: Switch to SystemTagList() when available
+		 */
+		if ((segs = LoadSeg("C:shell")))
+		{
+			/* Execute it. */
+			rc = RunCommand(segs, AROS_STACKSIZE, "FROM S:Startup-Sequence\n", -1);
+			UnLoadSeg(segs);
+		}
+		else
+		{
+			PutStr(CANNOT_LOAD_SHELL);
+			rc = 20;
+		}
+
+#ifdef BOOT_ON_CONSOLE_WINDOW
+		/* Restore original input/output */
+		SelectOutput(out);
+		SelectInput(in);
+
+		if (in)
+			Close(in);
+
+		/* FIXME:
+			if (out)
+				Close(out);
+		 */
+#endif
+
+		CloseLibrary((struct Library *)IntuitionBase);
+	}
+	else
+	{
+		PutStr(CANNOT_OPEN_INTUITION);
+		rc =20;
+	}
+
+	return rc;
 } /* main */
