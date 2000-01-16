0854ae6c90d93970e6885da5b2036fe5a3e2b7da
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-01-16 15:26:27 +0000

    Also test FormatDate now.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@6818 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/localetest.c
+++ test/localetest.c
@@ -2,7 +2,9 @@
 #include <proto/exec.h>
 #include <utility/hooks.h>
 #include <libraries/locale.h>
+#include <dos/datetime.h>
 #include <stdio.h>
+
 
 
   struct Library * LocaleBase;
@@ -13,8 +15,6 @@ void printchar(struct Hook * myhook,
 {
   if ('\0' != c)
    printf("%c",c);
-  else
-   printf("(NUL)");  
 }
 
 struct Data
@@ -29,7 +29,7 @@ struct Data
   
 };
 
-void locale_test(void)
+void formatstring_test(void)
 {
   struct Hook myhook;
   char hello[]      = {"Hello"};
@@ -57,6 +57,47 @@ void locale_test(void)
   FormatString(NULL, "%4$s! (%5$u %7$lX %6$u %6$U %7$lU)  %1$.3s %2$s %3$s %4$s!\n", &dataStream, &myhook);
 }
 
+void formatdate_test(void)
+{
+  struct Hook myhook;
+  struct DateStamp date;
+
+  myhook.h_Entry = (APTR)printchar;
+
+  printf("Doing a simple FormatDate test!\n");  
+
+  /*
+  ** Just a simple test for FormatString
+  */
+  
+  date.ds_Minute  = 59 + 60*8; // 8:59
+  date.ds_Tick    = 50*30; // 30 seconds
+  
+  printf("\n\tNow: 8:59:30\n");
+  FormatDate(NULL, "24hour style (leading 0s): %H\n", &date, &myhook);
+  FormatDate(NULL, "12hour style (leading 0s): %I\n", &date, &myhook);
+  FormatDate(NULL, "24hour style: %q\n", &date, &myhook);
+  FormatDate(NULL, "12hour style: %Q\n", &date, &myhook);
+  FormatDate(NULL, "Number of seconds (leading 0s): %S\n", &date, &myhook);
+  FormatDate(NULL, "H:M:S style (T): %T\n", &date, &myhook);
+  FormatDate(NULL, "H:M:S style (X): %X\n", &date, &myhook);
+
+
+  date.ds_Minute  = 59 + 60*18; // 18:59
+  date.ds_Tick    = 50*30; // 30 seconds
+  
+  printf("\n\tNow: 18:59:30\n");
+  FormatDate(NULL, "24hour style (leading 0s): %H\n", &date, &myhook);
+  FormatDate(NULL, "12hour style (leading 0s): %I\n", &date, &myhook);
+  FormatDate(NULL, "24hour style: %q\n", &date, &myhook);
+  FormatDate(NULL, "12hour style: %Q\n", &date, &myhook);
+  FormatDate(NULL, "Number of seconds (leading 0s): %S\n", &date, &myhook);
+  FormatDate(NULL, "H:M:S style (T): %T\n", &date, &myhook);
+  FormatDate(NULL, "H:M:S style (X): %X\n", &date, &myhook);
+}
+
+
+
 int main(void)
 {
 
@@ -66,8 +107,9 @@ int main(void)
   
   if (LocaleBase)
   {
-    locale_test();
+    formatstring_test();
   
+    formatdate_test();
     CloseLibrary(LocaleBase);
   }
   else
