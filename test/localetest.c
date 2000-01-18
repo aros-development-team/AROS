aaa5bbc2b00fe6c074e4af02407c0b4995abb44e
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-01-18 09:49:52 +0000

    Expanded localetest to also test GetCatalogString.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@6835 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/localetest.c
+++ test/localetest.c
@@ -1,13 +1,16 @@
 #include <proto/locale.h>
 #include <proto/exec.h>
+#include <proto/utility.h>
 #include <utility/hooks.h>
 #include <libraries/locale.h>
 #include <dos/datetime.h>
+#include <utility/date.h>
 #include <stdio.h>
 
 
 
-  struct Library * LocaleBase;
+struct Library * LocaleBase;
+struct Library * UtilityBase;
 
 void printchar(struct Hook * myhook,
                char c,
@@ -29,17 +32,17 @@ struct Data
   
 };
 
-void formatstring_test(void)
+void formatstring_test(struct Locale * locale)
 {
   struct Hook myhook;
   char hello[]      = {"Hello"};
-  char locale[]     = {"Locale"};
+  char local[]      = {"Locale"};
   char welcomes[]   = {"welcomes"};
   char you[]        = {"you"};
   
   struct Data dataStream = 
   {  hello,
-     locale,
+     local,
      welcomes,
      you,
      1000,
@@ -53,14 +56,16 @@ void formatstring_test(void)
   /*
   ** Just a simple test for FormatString
   */
-  FormatString(NULL, "%s! %s %s %s %u %u %lx!\n", &dataStream, &myhook);
-  FormatString(NULL, "%4$s! (%5$u %7$lX %6$u %6$U %7$lU)  %1$.3s %2$s %3$s %4$s!\n", &dataStream, &myhook);
+  FormatString(locale, "%s! %s %s %s %u %u %lx!\n", &dataStream, &myhook);
+  FormatString(locale, "%4$s! (%5$u %7$lX %6$u %6$U %7$lU)  %1$.3s %2$s %3$s %4$s!\n", &dataStream, &myhook);
 }
 
-void formatdate_test(void)
+void formatdate_test(struct Locale * locale)
 {
   struct Hook myhook;
   struct DateStamp date;
+  struct ClockData cdata;
+  ULONG seconds;
 
   myhook.h_Entry = (APTR)printchar;
 
@@ -70,51 +75,112 @@ void formatdate_test(void)
   ** Just a simple test for FormatString
   */
   
-  date.ds_Minute  = 59 + 60*8; // 8:59
-  date.ds_Tick    = 50*30; // 30 seconds
+  cdata.sec   = 30;
+  cdata.min   = 59;
+  cdata.hour  = 8;
+  cdata.mday  = 17;
+  cdata.month = 1;
+  cdata.year  = 2000;
+  cdata.wday  = 0; // don't care
   
-  printf("\n\tNow: 8:59:30\n");
-  FormatDate(NULL, "24hour style (leading 0s): %H\n", &date, &myhook);
-  FormatDate(NULL, "12hour style (leading 0s): %I\n", &date, &myhook);
-  FormatDate(NULL, "24hour style: %q\n", &date, &myhook);
-  FormatDate(NULL, "12hour style: %Q\n", &date, &myhook);
-  FormatDate(NULL, "Number of seconds (leading 0s): %S\n", &date, &myhook);
-  FormatDate(NULL, "H:M:S style (T): %T\n", &date, &myhook);
-  FormatDate(NULL, "H:M:S style (X): %X\n", &date, &myhook);
+  seconds = Date2Amiga(&cdata);
+  date.ds_Days = seconds / 86400;
+  seconds = seconds % 86400;
+  date.ds_Minute = seconds / 60;
+  seconds = seconds % 60;
+  date.ds_Tick = seconds * 50;
+  
+  FormatDate(locale, "24hour style (leading 0s): %H\n", &date, &myhook);
+  FormatDate(locale, "12hour style (leading 0s): %I\n", &date, &myhook);
+  FormatDate(locale, "24hour style: %q\n", &date, &myhook);
+  FormatDate(locale, "12hour style: %Q\n", &date, &myhook);
+  FormatDate(locale, "Number of seconds (leading 0s): %S\n", &date, &myhook);
+  FormatDate(locale, "H:M:S style (T): %T\n", &date, &myhook);
+  FormatDate(locale, "H:M:S style (X): %X\n", &date, &myhook);
 
 
-  date.ds_Minute  = 59 + 60*18; // 18:59
-  date.ds_Tick    = 50*30; // 30 seconds
+  cdata.sec   = 30;
+  cdata.min   = 59;
+  cdata.hour  = 18;
+  cdata.mday  = 30;
+  cdata.month = 7;
+  cdata.year  = 2000;
+  cdata.wday  = 0; // don't care
   
-  printf("\n\tNow: 18:59:30\n");
-  FormatDate(NULL, "24hour style (leading 0s): %H\n", &date, &myhook);
-  FormatDate(NULL, "12hour style (leading 0s): %I\n", &date, &myhook);
-  FormatDate(NULL, "24hour style: %q\n", &date, &myhook);
-  FormatDate(NULL, "12hour style: %Q\n", &date, &myhook);
-  FormatDate(NULL, "Number of seconds (leading 0s): %S\n", &date, &myhook);
-  FormatDate(NULL, "H:M:S style (T): %T\n", &date, &myhook);
-  FormatDate(NULL, "H:M:S style (X): %X\n", &date, &myhook);
+  seconds = Date2Amiga(&cdata);
+  date.ds_Days = seconds / 86400;
+  seconds = seconds % 86400;
+  date.ds_Minute = seconds / 60;
+  seconds = seconds % 60;
+  date.ds_Tick = seconds * 50;
+  
+  FormatDate(locale, "24hour style (leading 0s): %H\n", &date, &myhook);
+  FormatDate(locale, "12hour style (leading 0s): %I\n", &date, &myhook);
+  FormatDate(locale, "24hour style: %q\n", &date, &myhook);
+  FormatDate(locale, "12hour style: %Q\n", &date, &myhook);
+  FormatDate(locale, "Number of seconds (leading 0s): %S\n", &date, &myhook);
+  FormatDate(locale, "%%H:%%M:%%S style (T): %T\n", &date, &myhook);
+  FormatDate(locale, "%%H:%%M:%%S style (X): %X\n", &date, &myhook);
+
+  printf("\n");  
+  FormatDate(locale, "%%a %%b %%d %%h:%%m:%%s %%y style: %c\n", &date, &myhook);
+  FormatDate(locale, "%%a %%b %%e %%T %%Z %%Y style: %C\n", &date, &myhook);
+  FormatDate(locale, "%%m/%%d/%%y style: %D\n", &date, &myhook);
+
+  FormatDate(locale, "Week number - Sunday first day of week: %U\n", &date, &myhook);
+  FormatDate(locale, "Week number - Monday first day of week: %W\n", &date, &myhook);
+  
 }
 
+
+void getstringtest(struct Locale * locale)
+{
+  struct Catalog * cat;
+  char catname[256];
+  
+  printf("Please enter name of catalog: ");
+  scanf("%s",catname);
+   
+  cat = OpenCatalogA(locale,catname,NULL);
+  if (cat)
+  {
+    int i = 0;
+    while (i < 65535)
+    { 
+      char * str = GetCatalogStr(cat,
+                                 i,
+                                 NULL);
+      if (str)
+        printf("ID: %d - string : %s\n",i,str);
+
+      i++;
+    }
+  }
+  CloseCatalog(cat);
+}
 
 
 int main(void)
 {
-
-  printf("Opening local.library\n");
   LocaleBase = OpenLibrary("locale.library",0);
-  
+  UtilityBase = OpenLibrary("utility.library",0);  
   
   if (LocaleBase)
   {
-    formatstring_test();
-  
-    formatdate_test();
-    CloseLibrary(LocaleBase);
+    struct Locale * locale;
+    locale = OpenLocale(NULL); 
+    formatstring_test(locale);
+    formatdate_test(locale);
+    getstringtest(locale);
+    
+    if (locale) CloseLocale(locale);
   }
   else
   {
-    printf("Locale.library could not be opened!\n");
+    printf("A library could not be opened!\n");
   }
+
+  if (LocaleBase) CloseLibrary(LocaleBase);
+  if (UtilityBase) CloseLibrary(UtilityBase);
   return 0;
 }
