0706ca6862c7e3a3a58dcd459455c9340786e577
SDuvan <SDuvan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-01-20 20:28:54 +0000

    New function -- called __sprintf to avoid trouble with interference from clib includes; needs a global SysBase, so should generally only be used from 'user' programs
    git-svn-id: https://svn.aros.org/svn/aros/trunk@6887 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/alib/sprintf.c
@@ -0,0 +1,67 @@
+/*
+    (C) 2000 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: 
+    Lang: English
+*/
+
+#include <proto/exec.h>
+#include <aros/asmcall.h>
+
+/* Putchar procedure needed by RawDoFmt() */
+AROS_UFH2(void, __putChr,
+    AROS_UFHA(UBYTE   , chr, D0),
+    AROS_UFHA(STRPTR *, p  , A3))
+{
+    AROS_LIBFUNC_INIT
+    *(*p)++ = chr;
+    AROS_LIBFUNC_EXIT
+}
+
+
+/*****************************************************************************
+
+    NAME */
+
+       VOID  __sprintf(
+
+/*  SYNOPSIS */
+	     UBYTE *buffer, UBYTE *format, ...)
+
+/*  FUNCTION
+
+    Print a formatted string to a buffer.
+
+    INPUTS
+
+    buffer   --  the buffer to fill
+    format   --  the format string, see the RawDoFmt() documentation for
+                 information on which formatting commands there are
+
+    RESULT
+
+    NOTES
+
+    This routines needs access to SysBase, which makes it impossible to use
+    from libraries. For user programs, though, it is possible to use it as
+    SysBase is set up in startup.c.
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    exec.library/RawDoFmt()
+
+    INTERNALS
+
+    HISTORY
+
+    07.01.2000  SDuvan  implemented
+
+*****************************************************************************/
+{
+    RawDoFmt(format, &format+1, (VOID_FUNC)__putChr, &buffer);
+} /* sprintf */
