b7fa4a90b6a77a79dec55598e91a8e0506e5e602
SDuvan <SDuvan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-01-23 18:01:49 +0000

    Implemented
    git-svn-id: https://svn.aros.org/svn/aros/trunk@6951 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/execute.c
+++ rom/dos/execute.c
@@ -1,11 +1,14 @@
 /*
-    (C) 1995-96 AROS - The Amiga Research OS
+    (C) 1995-2000 AROS - The Amiga Research OS
     $Id$
 
     Desc:
-    Lang: english
+    Lang: English
 */
+
 #include "dos_intern.h"
+#include <dos/dosextens.h>
+#include <utility/tagitem.h>
 
 /*****************************************************************************
 
@@ -16,17 +19,35 @@
 
 /*  SYNOPSIS */
 	AROS_LHA(STRPTR, string, D1),
-	AROS_LHA(BPTR  , file, D2),
-	AROS_LHA(BPTR  , file2, D3),
+	AROS_LHA(BPTR  , input , D2),
+	AROS_LHA(BPTR  , output, D3),
 
 /*  LOCATION */
 	struct DosLibrary *, DOSBase, 37, Dos)
 
 /*  FUNCTION
 
+    Execute a CLI command specified in 'string'. This string may contain
+    features you may use on the shell commandline like redirection using >,
+    < or >>. Execute() doesn't return until the command(s) that should be
+    executed are finished.
+        If 'input' is not NULL, more commands will be read from this stream
+    until end of file is reached. 'output' will be used as the output stream
+    of the commands (if output is not redirected). If 'output' is NULL the
+    current window is used for output -- note that programs run from the
+    Workbench doesn't normally have a current window.
+
     INPUTS
 
+    string  --  pointer to a NULL-terminated string with commands
+                (may be NULL)
+    input   --  stream to use as input (may be NULL)
+    output  --  stream to use as output (may be NULL)
+
     RESULT
+
+    Boolean tellning whether Execute() could find and start the specified
+    command(s). (This is NOT the return code of the command(s).)
 
     NOTES
 
@@ -36,20 +57,35 @@
 
     SEE ALSO
 
+    SystemTagList()
+
     INTERNALS
+    
+    To get the right result, the function ExecCommand() (used by both Execute()
+    and SystemTagList()) uses NP_Synchronous to wait for the commands to
+    finish. This is not the way AmigaOS does it as NP_Synchronous is not
+    implemented (but defined).
 
     HISTORY
 	27-11-96    digulla automatically created from
 			    dos_lib.fd and clib/dos_protos.h
+	2x-12-99    SDuvan  implemented
 
 *****************************************************************************/
 {
     AROS_LIBFUNC_INIT
-    AROS_LIBBASE_EXT_DECL(struct DosLibrary *,DOSBase)
+    AROS_LIBBASE_EXT_DECL(struct DosLibrary *, DOSBase)
 
-#warning TODO: Write dos/Execute()
-    aros_print_not_implemented ("Execute");
+    LONG   result;
 
-    return DOSFALSE;
+    result = ExecCommand(RUN_EXECUTE, string, "c:shell", input, output, NULL,
+			 DOSBase);
+    
+    if(result == 0)
+	return DOSTRUE;
+    else
+	return DOSFALSE;
+    
     AROS_LIBFUNC_EXIT
 } /* Execute */
+
--- rom/dos/systemtaglist.c
+++ rom/dos/systemtaglist.c
@@ -1,16 +1,21 @@
 /*
-    (C) 1995-96 AROS - The Amiga Research OS
+    (C) 1995-2000 AROS - The Amiga Research OS
     $Id$
 
     Desc:
-    Lang: english
+    Lang: English
 */
+
 #include "dos_intern.h"
+#include <utility/tagitem.h>
+#include <dos/dostags.h>
+#include <proto/utility.h>
+#include <dos/dosextens.h>
 
 /*****************************************************************************
 
     NAME */
-#include <utility/tagitem.h>
+
 #include <proto/dos.h>
 
 	AROS_LH2(LONG, SystemTagList,
@@ -24,11 +29,48 @@
 
 /*  FUNCTION
 
+    Execute a command via a shell. As defaults, the process will use the
+    current Input() and Output(), and the current directory as well as the
+    path will be inherited from your process. If no path is specified, this
+    path will be used to find the command.
+        Normally, the boot shell is used but other shells may be specified
+    via tags. The tags are passed through to CreateNewProc() except those
+    who conflict with SystemTagList(). Currently, these are
+
+        NP_Seglist
+	NP_FreeSeglist
+	NP_Entry
+	NP_Input
+	NP_Output
+	NP_CloseInput
+	NP_CloseOutput
+	NP_HomeDir
+	NP_Cli
+
     INPUTS
+
+    command  --  program and arguments as a string
+    tags     --  see <dos/dostags.h>. Note that both SystemTagList() tags and
+                 tags for CreateNewProc() may be passed.
 
     RESULT
 
+    The return code of the command executed or -1 or if the command could
+    not run because the shell couldn't be created. If the command is not
+    found, the shell will return an error code, usually RETURN_ERROR.
+    
     NOTES
+
+    You must close the input and output filehandles yourself (if needed)
+    after System() returns if they were specified via SYS_Input or
+    SYS_Output (also, see below).
+        You may NOT use the same filehandle for both SYS_Input and SYS_Output.
+    If you want them to be the same CON: window, set SYS_Input to a filehandle
+    on the CON: window and set SYS_Output to NULL. Then the shell will
+    automatically set the output by opening CONSOLE: on that handler.
+        If you specified SYS_Asynch, both the input and the output filehandles
+    will be closed when the command is finished (even if this was your Input()
+    and Output().
 
     EXAMPLE
 
@@ -36,19 +78,46 @@
 
     SEE ALSO
 
+    Execute(), CreateNewProc(), Input(), Output(), <dos/dostags.h>
+
     INTERNALS
 
     HISTORY
 	27-11-96    digulla automatically created from
 			    dos_lib.fd and clib/dos_protos.h
+	9.1.2000    SDuvan  implemented
 
 *****************************************************************************/
 {
     AROS_LIBFUNC_INIT
-    AROS_LIBBASE_EXT_DECL(struct DosLibrary *,DOSBase)
+    AROS_LIBBASE_EXT_DECL(struct DosLibrary *, DOSBase)
 
-#warning TODO: Write dos/SystemTagList()
-    aros_print_not_implemented ("SystemTagList");
+    BPTR   input, output;
+    BOOL   isAsynchronous;
+    STRPTR shell, cShell;
+    ULONG  type;		/* Type of execution */
+
+    input  = (BPTR)GetTagData(SYS_Input , (IPTR)Input() , tags);
+    output = (BPTR)GetTagData(SYS_Output, (IPTR)Output(), tags);
+
+    isAsynchronous = GetTagData(SYS_Asynch, (IPTR)FALSE, tags);
+    
+#warning  "User shell vs. boot shell -- what's the difference?"
+
+    if((BOOL)GetTagData(SYS_UserShell, FALSE, tags))
+	shell = "C:shell";
+    else
+	shell = "C:shell";
+    
+    cShell = (STRPTR)GetTagData(SYS_CustomShell, (IPTR)NULL, tags);
+
+    if(cShell != NULL)
+	shell = cShell;
+
+    type = (command == NULL) ? RUN_EXECUTE : isAsynchronous ? 
+	RUN_SYSTEM : RUN_SYSTEM_ASYNCH;
+    
+    return ExecCommand(type, command, shell, input, output, tags, DOSBase);
 
     return RETURN_FAIL;
     AROS_LIBFUNC_EXIT
