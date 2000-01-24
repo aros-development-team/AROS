50d5506ea967694ce72239c8cf8f046d685b5459
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-01-24 20:14:09 +0000

    fill stack with 0xE1
    git-svn-id: https://svn.aros.org/svn/aros/trunk@6969 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-unix/exec/stackswap.c
+++ arch/all-unix/exec/stackswap.c
@@ -83,6 +83,8 @@ struct ExecBase * SysBase = (struct ExecBase *)0x0BAD0BAD;
 #error SP(env) undefined
 #endif
 
+#define STACKSNOOP
+
 /******************************************************************************
 
     NAME */
@@ -159,6 +161,20 @@ struct ExecBase * SysBase = (struct ExecBase *)0x0BAD0BAD;
 
 	newSP -= t; /* Make room for t elements */
 
+    #ifdef STACKSNOOP
+        
+	{
+	    UBYTE *startfill = sss->stk_Lower;
+	    UBYTE *endfill = ((UBYTE *)newSP) - 16;
+	    
+	    while(startfill <= endfill)
+	    {
+	        *startfill++ = 0xE1;
+	    }
+	}
+	
+    #endif
+ 
 	/*
 	    Move the contents of the old stack on the new stack.
 	    We will copy local variables, the frame pointer, the
