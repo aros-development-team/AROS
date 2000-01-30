7ce360f903fd9451914902d5b9cfd6365ea8b69e
mreckt <mreckt@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-01-30 23:03:27 +0000

    Test program for ExNext() dos.library call.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@7036 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/ExNext.c
@@ -0,0 +1,88 @@
+  /*
+    (C) 2000 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Test ExNext() function
+    Lang: english
+*/
+
+#include <proto/dos.h>
+#include <proto/exec.h>
+
+#include <dos/dos.h>
+#include <dos/exall.h>
+#include <dos/rdargs.h>
+#include <dos/var.h>
+#include <exec/memory.h>
+#include <exec/types.h>
+
+#include <utility/tagitem.h>
+#include <stdio.h>
+
+#include <aros/debug.h>
+
+#define ARG_TEMPLATE    "DIRECTORY"
+#define TOTAL_ARGS      1
+
+static const char version[] = "$VER: ExNext 41.1 (30.01.2000)\n";
+
+int main(int argc, char *argv[])
+{
+    struct RDArgs	* rda;
+    IPTR		* args[TOTAL_ARGS] = { NULL };
+    int			  Return_Value;
+    BPTR		  lock;
+
+    Return_Value = RETURN_OK;
+
+    rda = ReadArgs(ARG_TEMPLATE, (LONG *)args, NULL);
+    if (rda)
+    {
+	if (!args[0])
+	    lock = Lock("", ACCESS_READ);
+	else
+	    lock = Lock((STRPTR)args[0], ACCESS_READ);
+
+	if (lock)
+	{
+	    struct FileInfoBlock * FIB;
+	    BOOL success;
+	    FIB = AllocVec(sizeof(struct FileInfoBlock), MEMF_CLEAR);
+	    if (FIB)
+	    {
+	      success = Examine(lock, FIB);
+	      kprintf("calling ExNext()...\n");
+	      success = ExNext(lock, FIB);
+	      kprintf("called ExNext()\n");
+	      while (success != DOSFALSE)
+	      {
+		/* don't show dirs */
+		if (FIB->fib_DirEntryType < 0)
+		    printf("%s\n",FIB->fib_FileName);
+		else
+		    printf("%s (not a file)\n", FIB->fib_FileName);
+		kprintf("calling ExNext()...\n");
+		success = ExNext(lock, FIB);
+		kprintf("called ExNext()\n");
+	      }
+	      FreeVec(FIB);
+	    }
+	    UnLock(lock);
+	}
+	else
+	{
+	    PrintFault(IoErr(), "ExNext");
+	    Return_Value = RETURN_FAIL;
+	}
+    }
+    else
+    {
+        PrintFault(IoErr(), "ExNext");
+        Return_Value = RETURN_ERROR;
+    }
+
+    if (rda)
+        FreeArgs(rda);
+
+    return (Return_Value);
+} /* main */
