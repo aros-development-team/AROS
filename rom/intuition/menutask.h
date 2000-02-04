73a3d1672e8f4ebf302dce1006580175ef35aee1
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-02-04 21:48:53 +0000

    header file for menutask.c
    git-svn-id: https://svn.aros.org/svn/aros/trunk@7097 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/intuition/menutask.h
@@ -0,0 +1,51 @@
+#define MENUTASK_NAME "Menu Handler"
+#define MENUTASK_STACKSIZE 8192
+#define MENUTASK_PRIORITY 0
+
+#define ITEM_ITEM    1
+#define ITEM_SUBITEM 2
+
+#define AMIGAKEY_KEY_SPACING     4 /* GadTools assumes this, too */
+#define AMIGAKEY_BORDER_SPACING  2
+
+/* Structure passed to the DefaultMenuHandler task when it's initialized */
+
+struct MenuTaskParams
+{
+    struct IntuitionBase 	*IntuitionBase;
+    struct Task			*Caller;
+    struct MsgPort		*MenuHandlerPort; /* filled in by MenuHandler task */
+    BOOL			success;
+};
+
+struct MenuHandlerData
+{
+    struct Window 	*win;
+    struct Screen 	*scr;
+    struct DrawInfo	*dri;
+    struct Window	*menubarwin;
+    struct Window 	*menuwin;
+    struct Window	*submenuwin;
+    struct Menu		*menu;
+    struct Menu		*activemenu;
+    struct MenuItem	*activeitem;
+    struct MenuItem	*activesubitem;
+    struct Rectangle	submenubox;
+    struct Image	*checkmark;
+    struct Image	*amigakey;
+    WORD		activemenunum;
+    WORD		activeitemnum;
+    WORD		activesubitemnum;
+    WORD		maxcommkeywidth_menu;
+    WORD		maxcommkeywidth_submenu;
+    WORD		scrmousex;
+    WORD		scrmousey;
+    UWORD		firstmenupick;
+    UWORD		lastmenupick;
+    BOOL 		active;
+};
+
+BOOL InitDefaultMenuHandler(struct IntuitionBase *IntuitionBase);
+struct Task *CreateMenuHandlerTask(APTR taskparams, struct IntuitionBase *IntuitionBase);
+void DefaultMenuHandler(struct MenuTaskParams *taskparams);
+
