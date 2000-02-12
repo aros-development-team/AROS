03689fdb47b6a3d1e053ca5d445d17e96f647d79
SDuvan <SDuvan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-02-12 20:07:37 +0000

    Error codes for C: commands
    git-svn-id: https://svn.aros.org/svn/aros/trunk@7187 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/dos/dos_commanderrors.h
@@ -0,0 +1,18 @@
+
+/*
+    (C) 2000 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: 
+    Lang: English
+*/
+
+#ifndef  DOS_COMMANDERRORS_H
+#define  DOS_COMMANDERRORS_H
+
+/* Errors related to C: commands */
+#define  ERROR_NO_MATCHING_ELSEENDIF   10000
+#define  ERROR_SCRIPT_ONLY             10001
+#define  ERROR_NUMBER_OF_ARGUMENTS     10002
+
+#endif /* DOS_COMMANDERRORS_H */
