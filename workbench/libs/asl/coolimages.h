c2a3a73f2543e239234c51ec8cd130478fa3d78c
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-02-23 21:41:07 +0000

    cool images header
    git-svn-id: https://svn.aros.org/svn/aros/trunk@7388 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/asl/coolimages.h
@@ -0,0 +1,28 @@
+#ifndef EXEC_TYPES_H
+#    include <exec/types.h>
+#endif
+
+#define DEF_COOLIMAGEHEIGHT 16
+
+struct CoolImage
+{
+    const UBYTE	*data;
+    const UBYTE	*pal;
+    WORD 	width;
+    WORD	height;
+    WORD	depth;
+};
+
+extern const struct CoolImage cool_saveimage,
+			      cool_loadimage,
+			      cool_useimage,
+			      cool_cancelimage,
+			      cool_dotimage,
+			      cool_dotimage2,
+			      cool_warnimage,
+			      cool_diskimage,
+			      cool_switchimage,
+			      cool_monitorimage,
+			      cool_infoimage,
+			      cool_askimage,
+			      cool_keyimage;
