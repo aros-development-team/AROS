dfd962dc1183b034d02dc18ecd89af22c133a9d7
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-02-23 21:31:36 +0000

    header for filereq support functions
    git-svn-id: https://svn.aros.org/svn/aros/trunk@7378 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/asl/filereqsupport.h
@@ -0,0 +1,29 @@
+/*
+    (C) 1995-97 AROS - The Amiga Research OS
+
+    Desc:
+    Lang: english
+*/
+
+#ifndef AROS_ASMCALL_H
+#   include <aros/asmcall.h>
+#endif
+#ifndef DOS_EXALL_H
+#   include <dos/exall.h>
+#endif
+#ifndef DOS_DOSEXTENS_H
+#   include <dos/dosextens.h>
+#endif
+
+#ifndef LAYOUT_H
+#   include "layout.h"
+#endif
+
+BOOL FRGetDirectory(STRPTR path, struct LayoutData *ld, struct AslBase_intern *AslBase);
+BOOL FRGetVolumes(struct LayoutData *ld, struct AslBase_intern *AslBase);
+BOOL FRNewPath(STRPTR path, struct LayoutData *ld, struct AslBase_intern *AslBase);
+BOOL FRAddPath(STRPTR path, struct LayoutData *ld, struct AslBase_intern *AslBase);
+BOOL FRParentPath(struct LayoutData *ld, struct AslBase_intern *AslBase);
+void FRSetFile(STRPTR file, struct LayoutData *ld, struct AslBase_intern *AslBase);
+void FRFreeListviewList(struct LayoutData *ld, struct AslBase_intern *AslBase);
+
