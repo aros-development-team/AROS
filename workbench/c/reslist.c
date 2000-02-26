0b9271753f583146595113c6a600c922fea3053d
iaint <iaint@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-02-26 12:47:45 +0000

    A small utility for listing resources added to the exec.library resource list
    git-svn-id: https://svn.aros.org/svn/aros/trunk@7412 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/c/reslist.c
@@ -0,0 +1,101 @@
+/*
+    Copyright (C) 1995-2000 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: 
+    Lang: english
+*/
+
+#include <exec/memory.h>
+#include <exec/tasks.h>
+#include <exec/execbase.h>
+#include <proto/exec.h>
+#include <dos/dosextens.h>
+#include <proto/dos.h>
+#include <aros/machine.h>
+
+static const char version[] = "$VER: reslist 41.1 (24.2.2000)\n";
+
+struct res
+{
+    STRPTR name;
+    APTR address;
+};
+
+static int addres(struct Node *r, struct res **l, STRPTR *e)
+{
+    STRPTR s1, s2;
+
+    (*l)->address = r;
+
+    s1 = r->ln_Name;
+
+    if(s1!=NULL)
+    {
+        s2=s1;
+        while(*s2++)
+            ;
+        while(s2>s1)
+        {
+            if(*e<=(STRPTR)l)
+                return 0;
+            *--(*e)=*--s2;
+        }
+    }
+    if((STRPTR)(*l+1)>*e)
+        return 0;
+
+    (*l)->name=*e;
+    ++*l;
+
+    return 1;
+}
+
+static int fillbuffer(struct res **buffer, IPTR size)
+{
+    STRPTR end=(STRPTR)*buffer+size;
+    struct Node *r;
+    Forbid();
+    for(r=(struct Node *)SysBase->ResourceList.lh_Head;
+        r->ln_Succ!=NULL;
+        r=(struct Node *)r->ln_Succ)
+        if(!addres(r,buffer,&end))
+        {
+            Permit();
+            return 0;
+        }
+    Permit();
+    return 1;
+}
+
+int main(int argc, char *argv[])
+{
+    IPTR size;
+    struct res *buffer,*ress,*ress2;
+    for(size=2048;;size+=2048)
+    {
+        buffer=AllocVec(size,MEMF_ANY);
+        if(buffer==NULL)
+        {
+            FPuts(Output(),"Not Enough memory for library buffer\n");
+            return 20;
+        }
+        ress=buffer;
+        if(fillbuffer(&ress,size))
+        {
+	    FPuts(Output(),"address\t\tname\n"
+                           "------------------------------------------------------------\n");
+	    for(ress2=buffer;ress2<ress;ress2++)
+	    {
+		IPTR args[1];
+		args[0] = (IPTR)ress2->address;
+		args[1] = (IPTR)ress2->name;
+
+		VPrintf("0x%08.lx\t%s\n", args);
+	    }
+	    FreeVec(buffer);
+            return 0; 
+        }
+        FreeVec(buffer);
+    }
+}
