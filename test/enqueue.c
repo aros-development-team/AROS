dac6625aef8263946c39857f779c5e1b9407daa5
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-02-27 21:06:36 +0000

    enqueue test
    git-svn-id: https://svn.aros.org/svn/aros/trunk@7455 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/enqueue.c
@@ -0,0 +1,40 @@
+#define AROS_ALMOST_COMPATIBLE
+
+#include <exec/lists.h>
+#include <proto/exec.h>
+
+#include <stdio.h>
+
+struct Node node1,node2,node3,node4;
+struct List list;
+
+int main(void)
+{
+    struct Node *n;
+    
+    NEWLIST(&list);
+
+    node1.ln_Pri = 3;
+    node1.ln_Name = "Eins";
+
+    node2.ln_Pri = 2;
+    node2.ln_Name = "Zwei";
+
+    node3.ln_Pri = 1;
+    node3.ln_Name = "Drei";
+
+    node4.ln_Pri = 3;
+    node4.ln_Name = "Vier";
+    
+    Enqueue(&list, &node1);
+    Enqueue(&list, &node2);
+    Enqueue(&list, &node3);
+    Enqueue(&list, &node4);
+    
+    ForeachNode(&list, n)
+    {
+        printf("%s %ld\n", n->ln_Name, n->ln_Pri);
+    }
+    
+    return 0;
+}
