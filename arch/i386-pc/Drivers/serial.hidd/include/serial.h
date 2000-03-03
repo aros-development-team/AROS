414028c82b002de72ae7f7b9fe4593c71bf08eff
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-03-03 00:03:30 +0000

    Include file for i386 native serial hidd.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@7500 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/i386-pc/Drivers/serial.hidd/include/serial.h
@@ -0,0 +1,134 @@
+#ifndef HIDD_SERIAL_H
+#define HIDD_SERIAL_H
+
+/*
+    Copyright (C) 1998 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Definitions for the Serial HIDD system.
+    Lang: english
+*/
+#ifndef EXEC_TYPES_H
+#   include <exec/types.h>
+#endif
+#ifndef HIDD_HIDD_H
+#   include <hidd/hidd.h>
+#endif
+#ifndef OOP_OOP_H
+#   include <oop/oop.h>
+#endif
+
+#include <utility/utility.h>
+
+
+#define CLID_Hidd_Serial	"hidd.serial.serial"
+#define CLID_Hidd_SerialUnit	"hidd.serial.serialunit"
+
+#define IID_Hidd_Serial		"hidd.serial.serial"
+#define IID_Hidd_SerialUnit	"hidd.serial.serialunit"
+
+
+/**** Some tags for taglists  ***********************************************/
+
+#define HIDDA_SerialUnit_BPSRate	0x0001
+#define HIDDA_SerialUnit_DataLength	0x0002
+
+/**** serial definitions ****************************************************/
+
+enum
+{
+    /* Methods for a serial hidd */
+
+    moHidd_Serial_NewUnit = 0,       
+    moHidd_Serial_DisposeUnit,
+    moHidd_Serial_NumMethods		// always keep this the last one!
+};
+
+
+
+
+/* messages for a serial hidd */
+
+struct pHidd_Serial_NewUnit
+{
+    MethodID	mID;
+    ULONG	unitnum;
+};
+
+struct pHidd_Serial_DisposeUnit
+{
+    MethodID    mID;
+    Object      *unit;
+};
+
+
+/**** Serial Unit definitions ******************************************************/
+
+
+enum
+{
+    /* Methods for a serial unit */
+
+    moHidd_SerialUnit_Init,
+    moHidd_SerialUnit_Write,
+    moHidd_SerialUnit_SetBaudrate,
+    moHidd_SerialUnit_SetParameters,
+    moHidd_SerialUnit_SendBreak,
+    moHidd_SerialUnit_GetCapabilities,
+    moHidd_SerialUnit_NumMethods	// always keep this the last one!
+};
+
+
+/* messages for a serial unit */
+
+struct pHidd_SerialUnit_Init
+{
+    MethodID	mID;
+    VOID	*DataReceived;
+    VOID	*WriteData;
+};
+
+struct pHidd_SerialUnit_Write
+{
+    MethodID	mID;
+    ULONG	Length;
+    UBYTE	*Outbuffer;
+};
+
+struct pHidd_SerialUnit_SetBaudrate
+{
+    MethodID	mID;
+    ULONG	baudrate;
+};
+
+struct pHidd_SerialUnit_SetParameters
+{
+    MethodID		mID;
+    struct TagItem     *tags;
+};
+
+struct pHidd_SerialUnit_SendBreak
+{
+    MethodID	mID;
+    int		duration;
+};
+
+struct pHidd_SerialUnit_GetCapabilities
+{
+    MethodID         mID;
+    struct TagItem * taglist;
+};
+
+/* Predeclarations of stubs in libhiddserialstubs */
+
+Object * HIDD_Serial_NewUnit		(Object *obj, ULONG unitnum);
+VOID     HIDD_Serial_DisposeUnit	(Object *obj, Object *unit);
+
+BOOL     HIDD_SerialUnit_Init	(Object *obj, VOID * DataReceived, VOID * WriteData);
+ULONG    HIDD_SerialUnit_Write	(Object *obj, UBYTE * data, ULONG length);
+BOOL     HIDD_SerialUnit_SetBaudrate(Object *obj, ULONG baudrate);
+BOOL     HIDD_SerialUnit_SetParameters(Object *obj, struct TagItem *tags);
+BYTE     HIDD_SerialUnit_SendBreak(Object *obj, int duration);
+VOID     HIDD_SerialUnit_GetCapabilities(Object *obj, struct TagItem *tags);
+
+#endif /* HIDD_SERIAL_H */
