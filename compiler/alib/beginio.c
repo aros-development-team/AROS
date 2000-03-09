c235ea2763d3759be49af091ed3ef1ae01f2c6cb
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-03-09 21:28:00 +0000

    Added BeginIO().
    git-svn-id: https://svn.aros.org/svn/aros/trunk@7575 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/alib/beginio.c
@@ -0,0 +1,50 @@
+/*
+    (C) 1995-96 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: amiga.lib function BeginIO()
+    Lang: english
+*/
+#include <proto/exec.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <exec/ports.h>
+#include <proto/alib.h>
+
+	void BeginIO (
+
+/*  SYNOPSIS */
+	struct IORequest * ioReq)
+
+/*  FUNCTION
+	Call the BeginIO() function of the given device.
+
+    INPUTS
+        ioReq - a filled out IORequest structure.
+
+    RESULT
+	None.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+        AbortIO()
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+{
+    /* Call BeginIO() vector */
+    AROS_LVO_CALL1NR(
+	AROS_LCA(struct IORequest *,ioReq,A1),
+	struct Device *,ioReq->io_Device,5,
+    );
+} /* BeginIO */
