fa8527db455c4660c107c4343a0209c4ba6a6004
bernie <bernie@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-03-12 23:10:13 +0000

    very incomplete, but enough to port ARexx enabled applications without tweaking too much their source code
    git-svn-id: https://svn.aros.org/svn/aros/trunk@7618 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/rexx/errors.h
@@ -0,0 +1,17 @@
+#ifndef REXX_ERRORS_H
+#define REXX_ERRORS_H
+/*
+    (C) 2000 AROS - The Amiga Research OS
+
+    $Id$
+
+    Desc: ARexx data structures
+    Lang: English
+*/
+
+#define RC_OK		0L
+#define RC_WARN		5L
+#define RC_ERROR	10L
+#define RC_FATAL	20L
+
+#endif /* REXX_ERRORS_H */
