155ba0957466017993c235378fc81131340328f8
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-03-14 06:51:02 +0000

    increased kernel size. native should compile now
    git-svn-id: https://svn.aros.org/svn/aros/trunk@7644 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-pc/boot.h
+++ arch/i386-pc/boot.h
@@ -4,6 +4,6 @@
 #define DEF_INITSEG	0x9000
 #define DEF_SYSSEG	0x1000
 #define DEF_SETUPSEG	0x9020
-#define DEF_SYSSIZE	780 /* kernel size in sectors */
+#define DEF_SYSSIZE	980 /* kernel size in sectors WARNING: DO NOT exceed 1016!!! */
 
 #endif /* AROS_BOOT_H */
