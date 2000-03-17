67fbfc505af90760c77125dbb3a5d075b822d7ce
hkiel <hkiel@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-03-17 09:34:10 +0000

    Keep compiler happy
    git-svn-id: https://svn.aros.org/svn/aros/trunk@7675 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/demos/easyreq.c
+++ workbench/demos/easyreq.c
@@ -10,63 +10,70 @@ struct IntuitionBase *IntuitionBase;
 
 static struct EasyStruct es;
 
-void main(void)
+int main(void)
 {
     struct Window *req;
-    
+
     LONG result, counter = 0;
-    
+
     if ((IntuitionBase = (struct IntuitionBase *)OpenLibrary("intuition.library",39)))
     {
-        
+
 	es.es_StructSize = sizeof(es);
 	es.es_Flags = 0;
-	
+
 	es.es_Title = "First Requester";
 	es.es_TextFormat = "I'm a very nice requester :)";
 	es.es_GadgetFormat = "Yep!";
-	
+
 	EasyRequestArgs(0, &es, 0, 0);
-	
+
 	es.es_Title = "Second Requester";
 	es.es_TextFormat = "This requester should have\ntwo text lines!";
 	es.es_GadgetFormat = "Ok|Cancel";
-	
+
 	result = EasyRequestArgs(0, &es, 0, 0);
 	puts((result == 1) ? "You clicked on \"OK\"." : "You clicked on \"Cancel\".");
-	
+
 	es.es_Title = "Third Requester";
 	es.es_TextFormat = "One\nTwo\nThree\nFour\n\nTest Test";
 	es.es_GadgetFormat = "10|20|30|40|50|60|70|80|90|100";
-	
+
 	result = EasyRequestArgs(0, &es, 0, 0);
 	printf("Result %ld\n",result);
-	
+
 	es.es_Title = "Fourth Requester";
 	es.es_TextFormat = "Requester Text with args:\n\nArg 1: %ld  Arg 2: %ld  Arg 3: %ld\nStringarg: %s";
 	es.es_GadgetFormat = "Coooool";
-	
+
 	EasyRequest(0, &es, 0, 10, 20, 30, "I'm the string");
-	
+
 	es.es_Title = "Fifth Requester";
 	es.es_TextFormat = "I'm an asynchronous Requester.\nWatch shell output while\nrequester is open!";
 	es.es_GadgetFormat = "Incredible|Great|Not bad";
-	
+
 	req = BuildEasyRequestArgs(0, &es, 0, 0);
 	if (req && (req != (struct Window *)1))
 	{
 	    do
 	    {
-	        Delay(20);
-	    	result = SysReqHandler(req, 0, FALSE);
+		Delay(20);
+		result = SysReqHandler(req, 0, FALSE);
 		printf("*** Async Counter: %ld ***\n",++counter);
-		
+
 	    } while (result == -2);
-	    
+
 	    printf("You clicked on \"%s\"\n",(result == 1) ? "Incredible" : (result == 2) ? "Great" : "Not bad"); 
 	}
 	FreeSysRequest(req);
-	
-    	CloseLibrary((struct Library *)IntuitionBase);
+
+	CloseLibrary((struct Library *)IntuitionBase);
     }
+    else
+    {
+	fprintf(stderr, "Could not open intuition.library V39 or greater.\n");
+	return 10;
+    }
+
+return 0;
 }
--- workbench/demos/sysreq.c
+++ workbench/demos/sysreq.c
@@ -10,31 +10,38 @@ struct IntuitionBase *IntuitionBase;
 
 static struct IntuiText body, body2, pos, neg;
 
-void main(void)
+int main(void)
 {
     if ((IntuitionBase = (struct IntuitionBase *)OpenLibrary("intuition.library",39)))
     {
-        pos.IText = "Positive text";
+	pos.IText = "Positive text";
 	neg.IText = "Negative text";
-	
+
 	body.IText = "Text Line 1";
 	body2.IText = "Text Line 2";
 	body2.TopEdge = 20;
-	
+
 	AutoRequest(0, &body, &pos, &neg, 0, 0, 0, 0);
-	
+
 	body.NextText = &body2;
-	
+
 	AutoRequest(0, &body, &pos, &neg, 0, 0, 0, 0);
-	
+
 	AutoRequest(0, &body, 0, &neg, 0, 0, 0, 0);
-	
+
 	body2.LeftEdge = 50;
 	body2.IText = "This is a long text line";
-	
+
 	AutoRequest(0, &body, 0, &neg, 0, 0, 0, 0);
-	
-	
-    	CloseLibrary((struct Library *)IntuitionBase);
+
+
+	CloseLibrary((struct Library *)IntuitionBase);
     }
+    else
+    {
+	fprintf(stderr, "Could not open intuition.library V39 or greater.\n");
+	return 10;
+    }
+
+return 0;
 }
