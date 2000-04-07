3d193de9dfa3cc18f0aa61da597635329583fa4f
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-04-07 19:47:24 +0000

    try to make this work like in AmigaOS where for names without colon this func always returns TRUE. If the name contains a colon then to get TRUE it must either be an existing device (where FSA_IS_FILESYSTEM is sent to the filesystem handler) or an existing volume/assign (for late and non binding assign the assign must "work")
    git-svn-id: https://svn.aros.org/svn/aros/trunk@7820 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/isfilesystem.c
+++ rom/dos/isfilesystem.c
@@ -9,6 +9,7 @@
 #include <dos/dosextens.h>
 #include <dos/filesystem.h>
 #include "dos_intern.h"
+#include <string.h>
 
 /*****************************************************************************
 
@@ -36,6 +37,10 @@
 	DF0:, HD0:, ... are filesystems.
 	CON:, PIPE:, AUX:, ... are not
 
+        In AmigaOS if devicename contains no ":" then result
+	is always TRUE (-1). Also volume and assign names return
+	TRUE.
+	
     EXAMPLE
 
     BUGS
@@ -54,38 +59,95 @@
     AROS_LIBBASE_EXT_DECL(struct DosLibrary *,DOSBase)
 
     /* Get pointer to process structure */
-    struct Process *me=(struct Process *)FindTask(NULL);
+    struct Process *me = (struct Process *)FindTask(NULL);
     struct DosList *dl;
-    BOOL success=0;
+    STRPTR devicename_copy;
+    STRPTR colon;
     
-
-    dl=LockDosList(LDF_DEVICES|LDF_READ);
-    dl=FindDosEntry(dl,devicename,LDF_DEVICES);
-    if(dl!=NULL)
+    BOOL success = TRUE;
+    
+    colon = strchr(devicename, ':');
+    if (colon != NULL)
     {
+	UWORD stringlen = (UWORD)(colon - devicename);
+	
+	devicename_copy = AllocVec(stringlen + 1, MEMF_PUBLIC | MEMF_CLEAR);
+	if (devicename_copy != NULL)
+	{
+	    CopyMem(devicename, devicename_copy, stringlen);
+	   	
+	    success = FALSE;
+	    	
+	    dl = LockDosList(LDF_DEVICES | LDF_VOLUMES | LDF_ASSIGNS | LDF_READ);
+	    dl = FindDosEntry(dl, devicename_copy, LDF_DEVICES);
 
-        /* Get pointer to I/O request. Use stackspace for now. */
-        struct IOFileSys io,*iofs=&io;
+	    if (dl != NULL)
+	    {
+		switch(dl->dol_Type)
+		{
+		    case DLT_DEVICE:
+			{
+        		    /* Get pointer to I/O request. Use stackspace for now. */
+        		    struct IOFileSys io,*iofs=&io;
 
-        /* Prepare I/O request. */
-        iofs->IOFS.io_Message.mn_Node.ln_Type=NT_REPLYMSG;
-        iofs->IOFS.io_Message.mn_ReplyPort   =&me->pr_MsgPort;
-        iofs->IOFS.io_Message.mn_Length      =sizeof(struct IOFileSys);
-        iofs->IOFS.io_Device =dl->dol_Device;
-        iofs->IOFS.io_Unit   =dl->dol_Unit;
-        iofs->IOFS.io_Command=FSA_IS_FILESYSTEM;
-        iofs->IOFS.io_Flags  =0;
+        		    /* Prepare I/O request. */
+        		    iofs->IOFS.io_Message.mn_Node.ln_Type	= NT_REPLYMSG;
+        		    iofs->IOFS.io_Message.mn_ReplyPort  	= &me->pr_MsgPort;
+        		    iofs->IOFS.io_Message.mn_Length     	= sizeof(struct IOFileSys);
+        		    iofs->IOFS.io_Device 			= dl->dol_Device;
+        		    iofs->IOFS.io_Unit  			= dl->dol_Unit;
+        		    iofs->IOFS.io_Command			= FSA_IS_FILESYSTEM;
+        		    iofs->IOFS.io_Flags  			= 0;
 
-        /* Send the request. */
-        DoIO(&iofs->IOFS);
-        
-        /* Set return code */
-        if(!iofs->io_DosError) {
-            success=iofs->io_Union.io_IS_FILESYSTEM.io_IsFilesystem;
-	}
-    }
-    /* All Done. */
-    UnLockDosList(LDF_DEVICES|LDF_READ);
+        		    /* Send the request. */
+        		    DoIO(&iofs->IOFS);
+
+        		    /* Set return code */
+        		    if(!iofs->io_DosError) {
+        			success = iofs->io_Union.io_IS_FILESYSTEM.io_IsFilesystem;
+			    }
+			 }
+			 break;
+		     
+		     case DLT_VOLUME:
+		     case DLT_DIRECTORY:	/* normal assign */
+		         success = TRUE;
+			 break;
+		
+		     case DLT_LATE:
+		     case DLT_NONBINDING:
+		         {
+			     APTR old_windowptr = me->pr_WindowPtr;
+			     BPTR lock;
+			     
+			     me->pr_WindowPtr = (APTR)-1;
+			     
+			     if ((lock = Lock(devicename_copy, ACCESS_READ)))
+			     {
+			         success = TRUE;
+				 UnLock(lock);
+			     }
+			     
+			     me->pr_WindowPtr = old_windowptr;
+			     
+			 }
+			 break;
+			 
+		} /* switch(dl->dol_Type) */
+		 
+	    } /* if (dl != NULL) */
+
+	    /* All Done. */
+
+	    UnLockDosList(LDF_DEVICES | LDF_VOLUMES | LDF_ASSIGNS | LDF_READ);
+	    
+	    FreeVec(devicename_copy);
+	    
+	} /* if (devicename_copy != NULL) */
+	
+    } /* if (colon != NULL) */
+    
     return success;
+
     AROS_LIBFUNC_EXIT
 } /* IsFilesystem */
