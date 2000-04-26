655a6c7e0aa7b01e94224127d62d9e32dafe0604
SDuvan <SDuvan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-04-26 18:40:35 +0000

    Implemented
    git-svn-id: https://svn.aros.org/svn/aros/trunk@7975 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/inhibit.c
+++ rom/dos/inhibit.c
@@ -1,10 +1,11 @@
 /*
-    (C) 1995-96 AROS - The Amiga Research OS
+    (C) 2000 AROS - The Amiga Research OS
     $Id$
 
     Desc:
-    Lang: english
+    Lang: English
 */
+
 #include "dos_intern.h"
 
 /*****************************************************************************
@@ -23,11 +24,22 @@
 
 /*  FUNCTION
 
+    Stop a filesystem from being used.
+
     INPUTS
+
+    name   --  Name of the device to inhibit (including a ':')
+    onoff  --  Specify whether to inhinit (DOSTRUE) or uninhibit (DOSFALSE)
+               the device
 
     RESULT
 
+    A boolean telling whether the action was carried out.
+
     NOTES
+
+    After uninhibiting a device anything might have happened like the disk
+    in the drive was removed.
 
     EXAMPLE
 
@@ -38,17 +50,34 @@
     INTERNALS
 
     HISTORY
-	27-11-96    digulla automatically created from
-			    dos_lib.fd and clib/dos_protos.h
+         
+    26.03.2000  --  SDuvan  implemented
 
 *****************************************************************************/
 {
     AROS_LIBFUNC_INIT
-    AROS_LIBBASE_EXT_DECL(struct DosLibrary *,DOSBase)
+    AROS_LIBBASE_EXT_DECL(struct DosLibrary *, DOSBase)
 
-#warning TODO: Write dos/Inhibit()
-    aros_print_not_implemented ("Inhibit");
+    struct Process  *me = (struct Process *)FindTask(NULL);
+    struct DevProc  *dp = GetDeviceProc(name, NULL);
+    struct IOFileSys iofs;
 
-    return DOSFALSE;
+    if(dp == NULL)
+	return DOSFALSE;
+
+    iofs.IOFS.io_Device = ((struct FileHandle *)BADDR(dp->dvp_Lock))->fh_Device;
+    iofs.IOFS.io_Message.mn_Node.ln_Type = NT_REPLYMSG;
+    iofs.IOFS.io_Message.mn_ReplyPort	 = &me->pr_MsgPort;
+    iofs.IOFS.io_Message.mn_Length	 = sizeof(struct IOFileSys);
+    iofs.IOFS.io_Flags = 0;
+    iofs.IOFS.io_Command = FSA_INHIBIT;
+    iofs.io_Union.io_INHIBIT.io_Inhibit = onoff == DOSTRUE ? TRUE : FALSE;
+
+    DoIO((struct IORequest *)&iofs);
+
+    FreeDeviceProc(dp);
+
+    return iofs.io_DosError == 0 ? DOSTRUE : DOSFALSE;
+
     AROS_LIBFUNC_EXIT
 } /* Inhibit */
