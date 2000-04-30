e125d0668a16b1752db75317edef2195f85956e0
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-04-30 20:32:30 +0000

    can now also handle UNTIL and breaking (but which does not really work actually anyway, because of console.device) --> should be finished.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@7991 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/wait.c
+++ workbench/c/wait.c
@@ -7,37 +7,153 @@
 */
 #include <exec/execbase.h>
 #include <exec/libraries.h>
-#include <proto/exec.h>
+#include <devices/timer.h>
 #include <dos/dos.h>
-#include <proto/dos.h>
 
-static const char version[] = "$VER: wait 41.1 (14.3.1997)\n";
+#include <proto/dos.h>
+#include <proto/exec.h>
+
+#include <string.h>
+#include <stdio.h>
+
+static const char version[] = "$VER: wait 41.2 (30.04.2000)\n";
 
 int main (int argc, char ** argv)
 {
-    IPTR args[4]={ 0, 0, 0, 0 };
-    struct RDArgs *rda;
-    LONG error=0;
-    ULONG delay = 1;
+    IPTR 		args[4] = { 0, 0, 0, 0 };
+    struct RDArgs 	*rda;
+    LONG 		error = RETURN_OK;
+    ULONG 		delay = 1;
+    
 #define ERROR(a) { error=a; goto end; }
 
-    rda=ReadArgs("time/N,SEC=SECS/S,MIN=MINS/S,UNTIL/K",args,NULL);
-    if(rda==NULL)
-	ERROR(RETURN_FAIL);
+    rda = ReadArgs("time/N,SEC=SECS/S,MIN=MINS/S,UNTIL/K",args,NULL);
+    if(rda == NULL)
+    {
+	PrintFault(IoErr(),"Wait");
+        ERROR(RETURN_FAIL);
+    }
+    
+    if (args[3])
+    {
+        /* UNTIL */
+	struct DateTime  	dt;
+	struct DateStamp 	ds;
+	LONG 			now_secs, then_secs, diff_secs;
+	UBYTE			timestring[9];
 
-    if (args[0])
-	delay = *((ULONG *)args[0]);
+	DateStamp(&ds);	
+	now_secs = ds.ds_Minute * 60 + ds.ds_Tick / TICKS_PER_SECOND;
 
-    if (args[2])
-	delay *= 60L;
+	if (strlen((char *)args[3]) > 5)
+	{
+	    puts("Time should be HH:MM");
+	    ERROR(RETURN_FAIL);
+	}
+
+	strcpy(timestring, (UBYTE *)args[3]);
+	strcat(timestring, ":00");
+		
+	memset(&dt, 0, sizeof(dt));	
+	dt.dat_StrTime = timestring;
+	if (!StrToDate(&dt))
+	{
+	    puts("Time should be HH:MM");
+	    ERROR(RETURN_FAIL);
+	}
+	
+	then_secs = dt.dat_Stamp.ds_Minute * 60 + dt.dat_Stamp.ds_Tick / TICKS_PER_SECOND;
+	diff_secs = then_secs - now_secs;
+	if (diff_secs < 0) diff_secs += 60L * 60L * 24L;
+
+	delay = diff_secs * TICKS_PER_SECOND;
+
+    }
+    else
+    {
+	if (args[0])
+	    delay = *((ULONG *)args[0]);
+
+	if (args[2])
+	    delay *= 60L;
+	    
+	delay *= TICKS_PER_SECOND;
+    }
 
     if (delay > 0)
-	Delay (delay * 50L);
+    {
+	if (delay <= TICKS_PER_SECOND)
+	{
+	    /* Don't care about breaking if delay is less than 1 second */
+	    Delay (delay);
+	} else {
+	    struct MsgPort 	*timermp;
+	    struct timerequest  *timerio;
+	    BOOL		memok = FALSE, devok = FALSE;
+
+	    if ((timermp = CreateMsgPort()))
+	    {
+	        if (timerio = (struct timerequest *)CreateIORequest(timermp, sizeof(struct timerequest)))
+		{
+		    memok = TRUE;
+		    if (OpenDevice("timer.device", UNIT_VBLANK, &timerio->tr_node, 0) == 0)
+		    {
+		        ULONG timermask, sigs;
+			BOOL  done = FALSE;
+			
+		        devok = TRUE;
+			
+        		timerio->tr_node.io_Command = TR_ADDREQUEST;
+			timerio->tr_time.tv_secs    = delay / TICKS_PER_SECOND;
+			timerio->tr_time.tv_micro   = 1000000UL / TICKS_PER_SECOND * (delay % TICKS_PER_SECOND);
+			
+			timermask = 1L << timermp->mp_SigBit;
+			
+			SendIO(&timerio->tr_node);
+			
+			while(!done)
+			{
+			    sigs = Wait(SIGBREAKF_CTRL_C | timermask);
+			    if (sigs & timermask)
+			    {
+			        done = TRUE;
+			    }
+			    if (sigs & SIGBREAKF_CTRL_C)
+			    {
+			        if (!CheckIO(&timerio->tr_node)) AbortIO(&timerio->tr_node);
+				WaitIO(&timerio->tr_node);
+				
+				error = RETURN_WARN;				
+				done = TRUE;
+			    }
+			    
+			} /* while(!finished) */
+			CloseDevice(&timerio->tr_node);
+			
+		    } /* if (OpenDevice("timer.device", UNIT_VBLANK, &timerio->tr_node, 0) == 0) */
+		    DeleteIORequest(&timerio->tr_node);
+		    
+		} /* if (timerio = (struct timerequest *)CreateIORequest(timermp, sizeof(struct timerequest))) */
+		DeleteMsgPort(timermp);
+		
+	    } /* if ((timermp = CreateMsgPort())) */
+	    
+	    if (!memok)
+	    {
+	    	PrintFault(ERROR_NO_FREE_STORE,"Wait");
+		ERROR(RETURN_FAIL);
+	    } else if (!devok)
+	    {
+	        puts("Wait: Could not open timer.device!");
+		ERROR(RETURN_FAIL);
+	    }
+	    
+	}
+	
+    } /* if (delay > 0) */
 
 end:
-    if (rda)
-        FreeArgs(rda);
-    if(error)
-	PrintFault(IoErr(),"Wait");
+    if (rda) FreeArgs(rda);
+    
     return error;
 }
