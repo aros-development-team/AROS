1683fa44b3cb9ace00f4b0ce0d14867640ebe9d3
lbischoff <lbischoff@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-05-16 18:39:19 +0000

    Forgot this one.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@8109 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/alib/addappwindow.c
@@ -0,0 +1,53 @@
+/*
+    (C) 2000 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Varargs version of workbench.library/AddAppWindowA()
+    Lang: english
+*/
+#define AROS_TAGRETURNTYPE struct AppWindow *
+#include <utility/tagitem.h>
+
+/*****************************************************************************
+
+    NAME */
+#define NO_INLINE_STDARG /* turn off inline def */
+#include <proto/workbench.h>
+
+	struct AppWindow * AddAppWindow (
+
+/*  SYNOPSIS */
+	ULONG id,
+	ULONG userdata,
+	struct Window * window,
+	struct MsgPort * msgport,
+	Tag tag1,
+	...)
+
+/*  FUNCTION
+        This is the varargs version of workbench.library/AddAppWindowA().
+        For information see workbench.library/AddAppWindowA().
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+        workbench/AddAppWindowA()
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+    AROS_SLOWSTACKTAGS_PRE(tag1)
+    AddAppWindowA (id, userdata, window, msgport, AROS_SLOWSTACKTAGS_ARG(tag1));
+    AROS_SLOWSTACKTAGS_POST
+} /* AddAppWindow */
