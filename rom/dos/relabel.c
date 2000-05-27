2e3c1c31197a19d940b4a20d5476df2ab345303e
SDuvan <SDuvan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-05-27 08:35:57 +0000

    Implemented function
    git-svn-id: https://svn.aros.org/svn/aros/trunk@8162 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/relabel.c
+++ rom/dos/relabel.c
@@ -1,9 +1,9 @@
 /*
-    (C) 1995-96 AROS - The Amiga Research OS
+    (C) 1995-2000 AROS - The Amiga Research OS
     $Id$
 
     Desc:
-    Lang: english
+    Lang: English
 */
 #include "dos_intern.h"
 
@@ -23,9 +23,16 @@
 
 /*  FUNCTION
 
+    Change names of a volume.
+
     INPUTS
 
+    drive    --  The name of the device to rename (including the ':').
+    newname  --  The new name for the device (without the ':').
+
     RESULT
+
+    A boolean telling whether the name change was successful or not.
 
     NOTES
 
@@ -38,17 +45,47 @@
     INTERNALS
 
     HISTORY
-	27-11-96    digulla automatically created from
-			    dos_lib.fd and clib/dos_protos.h
+
+    28.04.2000  SDuvan  implemented
 
 *****************************************************************************/
 {
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct DosLibrary *,DOSBase)
 
-#warning TODO: Write dos/Relabel()
-    aros_print_not_implemented ("Relabel");
+    LONG error;
+    struct Device  *dev;
+    struct Process *me = (struct Process *)FindTask(NULL);
+    struct IOFileSys io;
 
-    return DOSFALSE;
+    error = DevName(drive, &dev, DOSBase);
+    if(error)
+    {
+	SetIoErr(error);
+	return DOSFALSE;
+    }
+
+    /* Prepare I/O request. */
+    io.IOFS.io_Message.mn_Node.ln_Type = NT_REPLYMSG;
+    io.IOFS.io_Message.mn_ReplyPort = &me->pr_MsgPort;
+    io.IOFS.io_Message.mn_Length = sizeof(struct IOFileSys);
+    io.IOFS.io_Device = dev;
+    io.IOFS.io_Unit = NULL;	/* No unit needed as all information is in the
+				   io_RELABEL struct. */
+    io.IOFS.io_Flags = 0;
+    io.IOFS.io_Command = FSA_RELABEL;
+    io.io_Union.io_RELABEL.io_NewName = newname;
+    io.io_Union.io_RELABEL.io_Result  = FALSE;
+
+    error = DoIO(&io.IOFS);
+
+    if(error)
+    {
+        SetIoErr(error);
+	return DOSFALSE;
+    }
+
+    return io.io_Union.io_RELABEL.io_Result ? DOSTRUE : DOSFALSE;
+
     AROS_LIBFUNC_EXIT
 } /* Relabel */
