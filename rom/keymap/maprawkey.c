dd4e96181206608375c028111cb9fd5535b42ce3
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-05-28 21:36:17 +0000

    MapRawKey must not follow the InputEvent chain (ie_NextEvent).
    git-svn-id: https://svn.aros.org/svn/aros/trunk@8177 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/keymap/maprawkey.c
+++ rom/keymap/maprawkey.c
@@ -29,11 +29,12 @@
 	struct Library *, KeymapBase, 7, Keymap)
 
 /*  FUNCTION
-	Converts possibly linked IECLASS_RAWKEY events to
-	ANSI bytes.
+	Converts IECLASS_RAWKEY events to ANSI bytes.
+	The event list (event->ie_NextEvent) is not traversed!
+	
     INPUTS
         event - InputEvent that should be converted.
-          	ie_NextEvent can point ot other events.
+          	ie_NextEvent is ignored!
           
         buffer - buffer into which the mapped ANSI bytes will be put.
         
@@ -67,8 +68,9 @@
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct Library *,KeymapBase)
     
-    UWORD code, qual;
+    UWORD 	   code, qual;
     struct BufInfo bufinfo;
+    struct KeyInfo ki;
     
     
     bufinfo.Buffer	 = buffer;
@@ -78,190 +80,183 @@
     if (!keyMap)
     	keyMap = KMBase(KeymapBase)->DefaultKeymap;
     
-    for ( ;event; event = event->ie_NextEvent )
+    	
+    /* Don't handle non-rawkey events */
+    if (event->ie_Class != IECLASS_RAWKEY)
+    	goto done;
+
+    code = event->ie_Code;
+    qual = event->ie_Qualifier;
+
+    /* Get info on keypress */
+    if (!GetKeyInfo(&ki, code, qual, keyMap))
+    	goto done; /* Invalid key mapping (like CTRL-ALT-A if a doesn't support CTRL-ALT */
+
+    /* Handle decoding of the the different keytypes (normal, KCF_STRING, KCF_DEAD and KCF_NOP) */
+
+    switch (ki.Key_MapType & (KC_NOQUAL|KCF_STRING|KCF_DEAD|KCF_NOP))
     {
-    	struct KeyInfo ki;
-    	
-        /* Don't handle non-rawkey events */
-    	if (event->ie_Class != IECLASS_RAWKEY)
-    	    continue;
+    case KC_NOQUAL: {
+    	BYTE idx;
+    	UBYTE c;
 
+    	D(bug("mrk: KC_NOQUAL\n"));
 
-    	code = event->ie_Code;
-    	qual = event->ie_Qualifier;
-    	    
-    	/* Get info on keypress */
-    	if (!GetKeyInfo(&ki, code, qual, keyMap))
-    	    continue; /* Invalid key mapping (like CTRL-ALT-A if a doesn't support CTRL-ALT */
- 	
-    	/* Handle decoding of the the different keytypes (normal, KCF_STRING, KCF_DEAD and KCF_NOP) */
-    	    
-    	switch (ki.Key_MapType & (KC_NOQUAL|KCF_STRING|KCF_DEAD|KCF_NOP))
+    	D(bug("mrk: getting idx at [%d][%d]\n", ki.Key_MapType & KC_VANILLA, ki.KCFQual));
+    	idx = keymaptype_table[ki.Key_MapType & KC_VANILLA][ki.KCFQual];
+    	if (idx != -1)
     	{
-    	case KC_NOQUAL: {
-    	    BYTE idx;
-    	    UBYTE c;
-    	        
-    	    D(bug("mrk: KC_NOQUAL\n"));
-	
-    	    D(bug("mrk: getting idx at [%d][%d]\n", ki.Key_MapType & KC_VANILLA, ki.KCFQual));
-    	    idx = keymaptype_table[ki.Key_MapType & KC_VANILLA][ki.KCFQual];
-    	    if (idx != -1)
+    	    D(bug("mrk: valid qual, idx=%d, key mapping=%04x\n", idx, ki.Key_Mapping));
+    	    if (idx == -2)
     	    {
-    	        D(bug("mrk: valid qual, idx=%d, key mapping=%04x\n", idx, ki.Key_Mapping));
-    	        if (idx == -2)
-    	        {
-    	            D(bug("mrk: Ctrl-C mode\n"));
-    	            /* Special-case where bit 5 & 6 should be cleared */
-    	            idx = 3;
-    	            c = GetMapChar(ki.Key_Mapping, idx);
-    	                
-		    /* clear bit 5 and 5 */
-    	            c &= ~((1 << 5)|(1 << 6));
-    	        }
-    	        else
-    	        {
-    	             c = GetMapChar(ki.Key_Mapping, idx);
-    	        }
-    	            
-    	        D(bug("mrk: Putting %c (%d 0x%x) into buffer\n", c, c, c));
-    	        
-    	        if (c != 0) /* If we get a 0 from the keymap, it means the char converts to "" */
-    	        {
-    	            if (!WriteToBuffer(&bufinfo, &c, 1))
-    	                goto overflow;
-    	        }
-    	            	
-    	            
-    	        }
-    	    } break;
-    	        
-    	case KCF_STRING: {
-    	    BYTE idx;
+    	        D(bug("mrk: Ctrl-C mode\n"));
+    	        /* Special-case where bit 5 & 6 should be cleared */
+    	        idx = 3;
+    	        c = GetMapChar(ki.Key_Mapping, idx);
 
-    	    D(bug("mrk: KCF_STRING\n"));
-    	    D(bug("mrk: getting idx at [%d][%d]\n", ki.Key_MapType & KC_VANILLA, ki.KCFQual));
-    	    idx = keymapstr_table[ki.Key_MapType & KC_VANILLA][ki.KCFQual];
-    	    if (idx != -1)
-    	    {
-    	        UBYTE *str_descrs = (UBYTE *)ki.Key_Mapping;
-    	        UBYTE len, offset;
-    	        
-    	        /* Since each string descriptor uses two bytes we multiply by 2 */
-    	        idx *= 2;
-    	             
-    	        /* Get string info from string descriptor table */
-    	            
-    	        len    = str_descrs[idx];
-    	        offset = str_descrs[idx + 1];
-    	            
-    	        D(bug("mrk: len=%d, offset=%d\n", len, offset));
-    	             
-    	        /* Write string to buffer */
-    	        if (!WriteToBuffer(&bufinfo, &(str_descrs[offset]), len))
-    	            goto overflow;
-    	            
-    	             
+		/* clear bit 5 and 5 */
+    	        c &= ~((1 << 5)|(1 << 6));
     	    }
-    	        
-    	    }break;
-    	        
-    	case KCF_DEAD: {
-    	    BYTE idx;
-    	    	
-    	    /* Get the index to the right dead key descrptor */
-    	    D(bug("mrk: KCF_DEAD\n"));
-    	    D(bug("mrk: getting idx at [%d][%d]\n", ki.Key_MapType & KC_VANILLA, ki.KCFQual));
-    	        
-    	    idx = keymapstr_table[ki.Key_MapType & KC_VANILLA][ki.KCFQual];
-    	    if (idx != -1)
+    	    else
     	    {
+    	         c = GetMapChar(ki.Key_Mapping, idx);
+    	    }
 
-    	    	UBYTE *dead_descr = (UBYTE *)ki.Key_Mapping;
-    	        UBYTE dead_type;
-    	        UBYTE dead_val;
-    	            
-    	        /* Each dead descripto is 2 bytes */
-    	        idx *= 2;
-    	            
-    	        dead_type = dead_descr[idx];
-    	        dead_val  = dead_descr[idx + 1];
-    	            
-    	        
-    	        if (dead_type == 0)
-    	        {
-    	            /* Val is the key to output */
-    	            if (!WriteToBuffer(&bufinfo, &dead_val, 1))
-    	            	goto overflow;
-    	        }
-    	        else if (dead_type == DPF_DEAD)
-    	        {
-    	            /* Do absolutely nothing. DPF_DEAD keys
-    	            ** are not possible to output, and are not
-    	            ** interesting by themselves.
-    	            ** However, if a DPF_MOD key follows..
-    	            */
+    	    D(bug("mrk: Putting %c (%d 0x%x) into buffer\n", c, c, c));
 
-    	        }
-    	        else if (dead_type == DPF_MOD)
+    	    if (c != 0) /* If we get a 0 from the keymap, it means the char converts to "" */
+    	    {
+    	        if (!WriteToBuffer(&bufinfo, &c, 1))
+    	            goto overflow;
+    	    }
+
+
+    	    }
+    	} break;
+
+    case KCF_STRING: {
+    	BYTE idx;
+
+    	D(bug("mrk: KCF_STRING\n"));
+    	D(bug("mrk: getting idx at [%d][%d]\n", ki.Key_MapType & KC_VANILLA, ki.KCFQual));
+    	idx = keymapstr_table[ki.Key_MapType & KC_VANILLA][ki.KCFQual];
+    	if (idx != -1)
+    	{
+    	    UBYTE *str_descrs = (UBYTE *)ki.Key_Mapping;
+    	    UBYTE len, offset;
+
+    	    /* Since each string descriptor uses two bytes we multiply by 2 */
+    	    idx *= 2;
+
+    	    /* Get string info from string descriptor table */
+
+    	    len    = str_descrs[idx];
+    	    offset = str_descrs[idx + 1];
+
+    	    D(bug("mrk: len=%d, offset=%d\n", len, offset));
+
+    	    /* Write string to buffer */
+    	    if (!WriteToBuffer(&bufinfo, &(str_descrs[offset]), len))
+    	        goto overflow;
+
+
+    	}
+
+    	}break;
+
+    case KCF_DEAD: {
+    	BYTE idx;
+
+    	/* Get the index to the right dead key descrptor */
+    	D(bug("mrk: KCF_DEAD\n"));
+    	D(bug("mrk: getting idx at [%d][%d]\n", ki.Key_MapType & KC_VANILLA, ki.KCFQual));
+
+    	idx = keymapstr_table[ki.Key_MapType & KC_VANILLA][ki.KCFQual];
+    	if (idx != -1)
+    	{
+
+    	    UBYTE *dead_descr = (UBYTE *)ki.Key_Mapping;
+    	    UBYTE dead_type;
+    	    UBYTE dead_val;
+
+    	    /* Each dead descripto is 2 bytes */
+    	    idx *= 2;
+
+    	    dead_type = dead_descr[idx];
+    	    dead_val  = dead_descr[idx + 1];
+
+
+    	    if (dead_type == 0)
+    	    {
+    	        /* Val is the key to output */
+    	        if (!WriteToBuffer(&bufinfo, &dead_val, 1))
+    	            goto overflow;
+    	    }
+    	    else if (dead_type == DPF_DEAD)
+    	    {
+    	        /* Do absolutely nothing. DPF_DEAD keys
+    	        ** are not possible to output, and are not
+    	        ** interesting by themselves.
+    	        ** However, if a DPF_MOD key follows..
+    	        */
+
+    	    }
+    	    else if (dead_type == DPF_MOD)
+    	    {
+    	        /* Now, lets have a look at the last two previous
+    	        ** keypresses.
+    	        **
+    	        ** dk_idx defaults to 0, which is the index to use if it turns out that
+    	        ** the deadable key had no valid deadkeys before it
+    	        */
+    	        WORD dk_idx = 0; /* Defaults to 0, which is the index to be used into the transition table */
+    	        WORD dki_1;
+
+
+    	        /* Get deadkey index for Prev1 */	
+    	        dki_1 = GetDeadKeyIndex(event->ie_Prev2DownCode, event->ie_Prev2DownQual, keyMap);
+    	        if (dki_1 != -1) /* Was it a dead key ? */
     	        {
-    	            /* Now, lets have a look at the last two previous
-    	            ** keypresses.
-    	            **
-    	            ** dk_idx defaults to 0, which is the index to use if it turns out that
-    	            ** the deadable key had no valid deadkeys before it
-    	            */
-    	            WORD dk_idx = 0; /* Defaults to 0, which is the index to be used into the transition table */
-    	            WORD dki_1;
-    	            
-    	            	
-    	            /* Get deadkey index for Prev1 */	
-    	            dki_1 = GetDeadKeyIndex(event->ie_Prev2DownCode, event->ie_Prev2DownQual, keyMap);
-    	            if (dki_1 != -1) /* Was it a dead key ? */
-    	            {
-			/* Is this a double deadkey (higher nibble set ?) */
-			if (dki_1 >> DP_2DFACSHIFT)
+		    /* Is this a double deadkey (higher nibble set ?) */
+		    if (dki_1 >> DP_2DFACSHIFT)
+		    {
+			WORD dki_2;
+
+			dki_2 = GetDeadKeyIndex(event->ie_Prev2DownCode, event->ie_Prev2DownQual, keyMap);
+			if (dki_2 != -1) /* Was it a ded key ? */
 			{
-			    WORD dki_2;
-			    
-			    dki_2 = GetDeadKeyIndex(event->ie_Prev2DownCode, event->ie_Prev2DownQual, keyMap);
-			    if (dki_2 != -1) /* Was it a ded key ? */
-			    {
-			        /* Compute deadkey index - explained in RKM:L p. 826 */
-			        dk_idx = (dki_1 & DP_2DINDEXMASK) * (dki_1 >> DP_2DFACSHIFT) + (dki_2 & DP_2DINDEXMASK);
-			    }
+			    /* Compute deadkey index - explained in RKM:L p. 826 */
+			    dk_idx = (dki_1 & DP_2DINDEXMASK) * (dki_1 >> DP_2DFACSHIFT) + (dki_2 & DP_2DINDEXMASK);
 			}
-			else
-			{
-			    dk_idx = dki_1;
-			}
-    	            }
-    	            
+		    }
+		    else
+		    {
+			dk_idx = dki_1;
+		    }
     	        }
-    	        else
-    	        {
-    	            kprintf("Keymap contains illegal deadkey type for code %04x, event->ie_Code\n");
-    	        }
-    	            
-    	            
-    	    } /* if (valid key) */
-    	    	
-    	    	
-    	    } break;
-    	        
-    	case KCF_NOP:
-    	    continue;
-    	        
-    	default:
-    	    kprintf("Error in keymap, more than one decode action specified for code %04x\n", event->ie_Code);
-    	    break;
-    	        
-    	} /* switch */
-    	    
-    	
-    } /* for (each event in the chain) */
-    
-    
+
+    	    }
+    	    else
+    	    {
+    	        kprintf("Keymap contains illegal deadkey type for code %04x, event->ie_Code\n");
+    	    }
+
+
+    	} /* if (valid key) */
+
+
+    	} break;
+
+    case KCF_NOP:
+    	break;
+
+    default:
+    	kprintf("Error in keymap, more than one decode action specified for code %04x\n", event->ie_Code);
+    	break;
+
+    } /* switch */
+
+done:    	        
     ReturnInt ("MapRawKey", WORD, bufinfo.CharsWritten);
     
 overflow:
