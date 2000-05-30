c200df841f5a57b4ce5b7c74e3128188a8caea63
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-05-30 21:21:40 +0000

    added a comment on why in input.device each keys accept qualifier keys have to be treated as repeatable = without looking at keymap->repeatable tables.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@8207 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/devs/input/support.c
+++ rom/devs/input/support.c
@@ -118,7 +118,16 @@ BOOL IsRepeatableKey(UWORD key)
     BOOL result = TRUE;
     
     key &= ~IECODE_UP_PREFIX;
-     
+    
+    /* stegerg: It looks like this is really all so no need to
+       check the keymap->repeatable tables. I have checked this
+       on the Amiga. All keys except the qualifier keys are
+       treated as repeatable raw (!!!) keys. Here in input.device
+       we have only raw keys. For vanilla keys there's MapRawKey()
+       and this function thakes care of filtering out repeated
+       raw keys if the corresponding KeyMap->repeatable table says
+       that this key is not repeatable */
+       
     if (IsQualifierKey(key))
     {
         result = FALSE;
