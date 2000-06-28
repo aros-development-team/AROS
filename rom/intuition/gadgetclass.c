8dcb25f0d7b4564f497fed577d69620128d93a70
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-06-28 17:01:48 +0000

    implement also ICM_CHECKLOOP ICM_CLEARLOOP ICM_SETLOOP, because of the icclass which is integrated in the gadgetclass instance data. I'm not sure if this is really required, but it certainly does not hurt.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@8316 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/intuition/gadgetclass.c
+++ rom/intuition/gadgetclass.c
@@ -83,256 +83,256 @@ static ULONG set_gadgetclass(Class *cl, Object *o, struct opSet *msg)
 
 	switch(tag->ti_Tag)
 	{
-	case GA_Left:
-	    EG(o)->LeftEdge = (WORD)tidata;
-	    EG(o)->Flags &= ~GFLG_RELRIGHT;
-	    retval = 1UL;
-	    break;
+	    case GA_Left:
+		EG(o)->LeftEdge = (WORD)tidata;
+		EG(o)->Flags &= ~GFLG_RELRIGHT;
+		retval = 1UL;
+		break;
 
-	case GA_Top:
-	    EG(o)->TopEdge = (WORD)tidata;
-	    EG(o)->Flags &= ~GFLG_RELBOTTOM;
-	    retval = 1UL;
-	    break;
+	    case GA_Top:
+		EG(o)->TopEdge = (WORD)tidata;
+		EG(o)->Flags &= ~GFLG_RELBOTTOM;
+		retval = 1UL;
+		break;
 
-	case GA_Width:
-	    EG(o)->Width = (WORD)tidata;
-	    EG(o)->Flags &= ~GFLG_RELWIDTH;
-	    retval = 1UL;
-	    break;
+	    case GA_Width:
+		EG(o)->Width = (WORD)tidata;
+		EG(o)->Flags &= ~GFLG_RELWIDTH;
+		retval = 1UL;
+		break;
 
-	case GA_Height:
-	    EG(o)->Height = (WORD)tidata;
-	    EG(o)->Flags &= ~GFLG_RELHEIGHT;
-	    retval = 1UL;
-	    break;
+	    case GA_Height:
+		EG(o)->Height = (WORD)tidata;
+		EG(o)->Flags &= ~GFLG_RELHEIGHT;
+		retval = 1UL;
+		break;
 
-	case GA_RelRight:
-	    EG(o)->LeftEdge = (WORD)tidata;
-	    EG(o)->Flags |= GFLG_RELRIGHT;
-	    retval = 1UL;
-	    break;
+	    case GA_RelRight:
+		EG(o)->LeftEdge = (WORD)tidata;
+		EG(o)->Flags |= GFLG_RELRIGHT;
+		retval = 1UL;
+		break;
 
-	case GA_RelBottom:
-	    EG(o)->TopEdge = (WORD)tidata;
-	    EG(o)->Flags |= GFLG_RELBOTTOM;
-	    retval = 1UL;
-	    break;
+	    case GA_RelBottom:
+		EG(o)->TopEdge = (WORD)tidata;
+		EG(o)->Flags |= GFLG_RELBOTTOM;
+		retval = 1UL;
+		break;
 
-	case GA_RelWidth:
-	    EG(o)->Width = (WORD)tidata;
-	    EG(o)->Flags |= GFLG_RELWIDTH;
-	    retval = 1UL;
-	    break;
+	    case GA_RelWidth:
+		EG(o)->Width = (WORD)tidata;
+		EG(o)->Flags |= GFLG_RELWIDTH;
+		retval = 1UL;
+		break;
 
-	case GA_RelHeight:
-	    EG(o)->Height = (WORD)tidata;
-	    EG(o)->Flags |= GFLG_RELHEIGHT;
-	    retval = 1UL;
-	    break;
+	    case GA_RelHeight:
+		EG(o)->Height = (WORD)tidata;
+		EG(o)->Flags |= GFLG_RELHEIGHT;
+		retval = 1UL;
+		break;
 
-	case GA_RelSpecial:
-	    EG(o)->Flags |= GFLG_RELSPECIAL;
-	    retval = 1UL;
-	    break;
+	    case GA_RelSpecial:
+		EG(o)->Flags |= GFLG_RELSPECIAL;
+		retval = 1UL;
+		break;
 
-	case GA_Bounds:
-	    EG(o)->BoundsLeftEdge = ((struct IBox *)tidata)->Left;
-	    EG(o)->BoundsTopEdge  = ((struct IBox *)tidata)->Top;
-	    EG(o)->BoundsWidth    = ((struct IBox *)tidata)->Width;
-	    EG(o)->BoundsHeight   = ((struct IBox *)tidata)->Height;
-	    EG(o)->MoreFlags |= GMORE_BOUNDS;
-	    retval = 1UL;
-	    break;
-	   	    
-	case GA_Previous:
-	    if( (tidata != 0L) && (msg->MethodID == OM_NEW) )
-	    {
-		EG(o)->NextGadget = ((struct ExtGadget *)tidata)->NextGadget;
-		((struct ExtGadget *)tidata)->NextGadget = EG(o);
-	    }
-	    break;
+	    case GA_Bounds:
+		EG(o)->BoundsLeftEdge = ((struct IBox *)tidata)->Left;
+		EG(o)->BoundsTopEdge  = ((struct IBox *)tidata)->Top;
+		EG(o)->BoundsWidth    = ((struct IBox *)tidata)->Width;
+		EG(o)->BoundsHeight   = ((struct IBox *)tidata)->Height;
+		EG(o)->MoreFlags |= GMORE_BOUNDS;
+		retval = 1UL;
+		break;
 
-	case GA_IntuiText:
-	    EG(o)->GadgetText = (struct IntuiText *)tidata;
-	    EG(o)->Flags &= ~GFLG_LABELMASK;
-	    EG(o)->Flags |= GFLG_LABELITEXT;
-	    retval = 1UL;
-	    break;
+	    case GA_Previous:
+		if( (tidata != 0L) && (msg->MethodID == OM_NEW) )
+		{
+		    EG(o)->NextGadget = ((struct ExtGadget *)tidata)->NextGadget;
+		    ((struct ExtGadget *)tidata)->NextGadget = EG(o);
+		}
+		break;
 
-	case GA_Text:
-	    EG(o)->GadgetText = (struct IntuiText *)tidata;
-	    EG(o)->Flags &= ~GFLG_LABELMASK;
-	    EG(o)->Flags |= GFLG_LABELSTRING;
-	    retval = 1UL;
-	    break;
+	    case GA_IntuiText:
+		EG(o)->GadgetText = (struct IntuiText *)tidata;
+		EG(o)->Flags &= ~GFLG_LABELMASK;
+		EG(o)->Flags |= GFLG_LABELITEXT;
+		retval = 1UL;
+		break;
 
-	case GA_LabelImage:
-	    EG(o)->GadgetText = (struct IntuiText *)tidata;
-	    EG(o)->Flags &= ~GFLG_LABELMASK;
-	    EG(o)->Flags |= GFLG_LABELIMAGE;
-	    retval = 1UL;
-	    break;
+	    case GA_Text:
+		EG(o)->GadgetText = (struct IntuiText *)tidata;
+		EG(o)->Flags &= ~GFLG_LABELMASK;
+		EG(o)->Flags |= GFLG_LABELSTRING;
+		retval = 1UL;
+		break;
 
-	case GA_Image:
-	    EG(o)->GadgetRender = (APTR)tidata;
-	    EG(o)->Flags |= GFLG_GADGIMAGE;
-	    retval = 1UL;
-	    break;
+	    case GA_LabelImage:
+		EG(o)->GadgetText = (struct IntuiText *)tidata;
+		EG(o)->Flags &= ~GFLG_LABELMASK;
+		EG(o)->Flags |= GFLG_LABELIMAGE;
+		retval = 1UL;
+		break;
 
-	case GA_Border:
-	    EG(o)->GadgetRender = (APTR)tidata;
-	    EG(o)->Flags &= ~GFLG_GADGIMAGE;
-	    retval = 1UL;
-	    break;
+	    case GA_Image:
+		EG(o)->GadgetRender = (APTR)tidata;
+		EG(o)->Flags |= GFLG_GADGIMAGE;
+		retval = 1UL;
+		break;
 
-	case GA_SelectRender:
-	    EG(o)->SelectRender = (APTR)tidata;
-	    EG(o)->Flags |= (GFLG_GADGIMAGE & GFLG_GADGHIMAGE);
-	    retval = 1UL;
-	    break;
+	    case GA_Border:
+		EG(o)->GadgetRender = (APTR)tidata;
+		EG(o)->Flags &= ~GFLG_GADGIMAGE;
+		retval = 1UL;
+		break;
 
-	case GA_SpecialInfo:
-	    EG(o)->SpecialInfo = (APTR)tidata;
-	    break;
+	    case GA_SelectRender:
+		EG(o)->SelectRender = (APTR)tidata;
+		EG(o)->Flags |= (GFLG_GADGIMAGE & GFLG_GADGHIMAGE);
+		retval = 1UL;
+		break;
 
-	case GA_GZZGadget:
-	    if ( (BOOL)tidata != FALSE )
-		EG(o)->GadgetType |= GTYP_GZZGADGET;
-	    else
-		EG(o)->GadgetType &= ~GTYP_GZZGADGET;
-	    break;
+	    case GA_SpecialInfo:
+		EG(o)->SpecialInfo = (APTR)tidata;
+		break;
 
-	case GA_SysGadget:
-	    if ( (BOOL)tidata != FALSE )
-		EG(o)->GadgetType |= GTYP_SYSGADGET;
-	    else
-		EG(o)->GadgetType &= ~GTYP_SYSGADGET;
-	    break;
+	    case GA_GZZGadget:
+		if ( (BOOL)tidata != FALSE )
+		    EG(o)->GadgetType |= GTYP_GZZGADGET;
+		else
+		    EG(o)->GadgetType &= ~GTYP_GZZGADGET;
+		break;
 
-	case GA_Selected:
-	    if ( (BOOL)tidata != FALSE )
-		EG(o)->Flags |= GFLG_SELECTED;
-	    else
-		EG(o)->Flags &= ~GFLG_SELECTED;
-	    retval = 1UL;
-	    break;
+	    case GA_SysGadget:
+		if ( (BOOL)tidata != FALSE )
+		    EG(o)->GadgetType |= GTYP_SYSGADGET;
+		else
+		    EG(o)->GadgetType &= ~GTYP_SYSGADGET;
+		break;
 
-	case GA_Disabled:
-	    if ( (BOOL)tidata != FALSE )
-		EG(o)->Flags |= GFLG_DISABLED;
-	    else
-		EG(o)->Flags &= ~GFLG_DISABLED;
-	    retval = 1UL;
-	    break;
+	    case GA_Selected:
+		if ( (BOOL)tidata != FALSE )
+		    EG(o)->Flags |= GFLG_SELECTED;
+		else
+		    EG(o)->Flags &= ~GFLG_SELECTED;
+		retval = 1UL;
+		break;
 
-	case GA_EndGadget:
-	    if ( (BOOL)tidata != FALSE )
-		EG(o)->Activation |= GACT_ENDGADGET;
-	    else
-		EG(o)->Activation &= ~GACT_ENDGADGET;
-	    break;
+	    case GA_Disabled:
+		if ( (BOOL)tidata != FALSE )
+		    EG(o)->Flags |= GFLG_DISABLED;
+		else
+		    EG(o)->Flags &= ~GFLG_DISABLED;
+		retval = 1UL;
+		break;
 
-	case GA_Immediate:
-	    if ( (BOOL)tidata != FALSE )
-		EG(o)->Activation |= GACT_IMMEDIATE;
-	    else
-		EG(o)->Activation &= ~GACT_IMMEDIATE;
-	    break;
+	    case GA_EndGadget:
+		if ( (BOOL)tidata != FALSE )
+		    EG(o)->Activation |= GACT_ENDGADGET;
+		else
+		    EG(o)->Activation &= ~GACT_ENDGADGET;
+		break;
 
-	case GA_RelVerify:
-	    if ( (BOOL)tidata != FALSE )
-		EG(o)->Activation |= GACT_RELVERIFY;
-	    else
-		EG(o)->Activation &= ~GACT_RELVERIFY;
-	    break;
+	    case GA_Immediate:
+		if ( (BOOL)tidata != FALSE )
+		    EG(o)->Activation |= GACT_IMMEDIATE;
+		else
+		    EG(o)->Activation &= ~GACT_IMMEDIATE;
+		break;
 
-	case GA_FollowMouse:
-	    if ( (BOOL)tidata != FALSE )
-		EG(o)->Activation |= GACT_FOLLOWMOUSE;
-	    else
-		EG(o)->Activation &= ~GACT_FOLLOWMOUSE;
-	    break;
+	    case GA_RelVerify:
+		if ( (BOOL)tidata != FALSE )
+		    EG(o)->Activation |= GACT_RELVERIFY;
+		else
+		    EG(o)->Activation &= ~GACT_RELVERIFY;
+		break;
 
-	case GA_RightBorder:
-	    if ( (BOOL)tidata != FALSE )
-		EG(o)->Activation |= GACT_RIGHTBORDER;
-	    else
-		EG(o)->Activation &= ~GACT_RIGHTBORDER;
-	    break;
+	    case GA_FollowMouse:
+		if ( (BOOL)tidata != FALSE )
+		    EG(o)->Activation |= GACT_FOLLOWMOUSE;
+		else
+		    EG(o)->Activation &= ~GACT_FOLLOWMOUSE;
+		break;
 
-	case GA_LeftBorder:
-	    if ( (BOOL)tidata != FALSE )
-		EG(o)->Activation |= GACT_LEFTBORDER;
-	    else
-		EG(o)->Activation &= ~GACT_LEFTBORDER;
-	    break;
+	    case GA_RightBorder:
+		if ( (BOOL)tidata != FALSE )
+		    EG(o)->Activation |= GACT_RIGHTBORDER;
+		else
+		    EG(o)->Activation &= ~GACT_RIGHTBORDER;
+		break;
 
-	case GA_TopBorder:
-	    if ( (BOOL)tidata != FALSE )
-		EG(o)->Activation |= GACT_TOPBORDER;
-	    else
-		EG(o)->Activation &= ~GACT_TOPBORDER;
-	    break;
+	    case GA_LeftBorder:
+		if ( (BOOL)tidata != FALSE )
+		    EG(o)->Activation |= GACT_LEFTBORDER;
+		else
+		    EG(o)->Activation &= ~GACT_LEFTBORDER;
+		break;
 
-	case GA_BottomBorder:
-	    if ( (BOOL)tidata != FALSE )
-		EG(o)->Activation |= GACT_BOTTOMBORDER;
-	    else
-		EG(o)->Activation &= ~GACT_BOTTOMBORDER;
-	    break;
+	    case GA_TopBorder:
+		if ( (BOOL)tidata != FALSE )
+		    EG(o)->Activation |= GACT_TOPBORDER;
+		else
+		    EG(o)->Activation &= ~GACT_TOPBORDER;
+		break;
 
-	case GA_ToggleSelect:
-	    if ((BOOL)tidata)
-		EG(o)->Activation |= GACT_TOGGLESELECT;
-	    else
-		EG(o)->Activation &= ~GACT_TOGGLESELECT;
-	    break;
+	    case GA_BottomBorder:
+		if ( (BOOL)tidata != FALSE )
+		    EG(o)->Activation |= GACT_BOTTOMBORDER;
+		else
+		    EG(o)->Activation &= ~GACT_BOTTOMBORDER;
+		break;
 
-	case GA_TabCycle:
-	    if ((BOOL)tidata)
-		EG(o)->Flags |= GFLG_TABCYCLE;
-	    else
-		EG(o)->Flags &= ~GFLG_TABCYCLE;
-	    break;
+	    case GA_ToggleSelect:
+		if ((BOOL)tidata)
+		    EG(o)->Activation |= GACT_TOGGLESELECT;
+		else
+		    EG(o)->Activation &= ~GACT_TOGGLESELECT;
+		break;
 
-	case GA_Highlight:
-	    EG(o)->Flags &= ~GFLG_GADGHIGHBITS;
-	    EG(o)->Flags |= tidata & GFLG_GADGHIGHBITS;
-	    break;
+	    case GA_TabCycle:
+		if ((BOOL)tidata)
+		    EG(o)->Flags |= GFLG_TABCYCLE;
+		else
+		    EG(o)->Flags &= ~GFLG_TABCYCLE;
+		break;
 
-	case GA_SysGType:
-	    EG(o)->GadgetType &= ~GTYP_SYSTYPEMASK;
-	    EG(o)->GadgetType |= tidata & GTYP_SYSTYPEMASK;
-	    break;
+	    case GA_Highlight:
+		EG(o)->Flags &= ~GFLG_GADGHIGHBITS;
+		EG(o)->Flags |= tidata & GFLG_GADGHIGHBITS;
+		break;
 
-	case GA_ID:
-	    /* GA_ID should NOT be set if this is a OM_UPDATE.
-	    ** This is because gadgets should send their GA_ID
-	    ** when doing a OM_NOTIFY, so that the receiver
-	    ** might see who sent the message.
-	    ** But we surely don't want to change the GA_ID
-	    ** of the reciver to that of the sender.
-	    */
-	    if (msg->MethodID != OM_UPDATE)
-	    	EG(o)->GadgetID = tidata;
-	    break;
+	    case GA_SysGType:
+		EG(o)->GadgetType &= ~GTYP_SYSTYPEMASK;
+		EG(o)->GadgetType |= tidata & GTYP_SYSTYPEMASK;
+		break;
 
-	case GA_UserData:
-	    EG(o)->UserData = (APTR)tidata;
-	    break;
+	    case GA_ID:
+		/* GA_ID should NOT be set if this is a OM_UPDATE.
+		** This is because gadgets should send their GA_ID
+		** when doing a OM_NOTIFY, so that the receiver
+		** might see who sent the message.
+		** But we surely don't want to change the GA_ID
+		** of the reciver to that of the sender.
+		*/
+		if (msg->MethodID != OM_UPDATE)
+	    	    EG(o)->GadgetID = tidata;
+		break;
 
-	 
-	case ICA_TARGET:
-	    GD(o)->IC.ic_Target = (Object *)tidata;
-	    break;
-	     
-	case ICA_MAP:
-	    GD(o)->IC.ic_Mapping = (struct TagItem *)tidata;
-	    break;
+	    case GA_UserData:
+		EG(o)->UserData = (APTR)tidata;
+		break;
+
+	    case ICA_TARGET:
+		GD(o)->IC.ic_Target = (Object *)tidata;
+		break;
+
+	    case ICA_MAP:
+		GD(o)->IC.ic_Mapping = (struct TagItem *)tidata;
+		break;
 	    
-	}
-    }
+	} /* switch tag */
+	
+    } /* while NextTagItem */
 
     if ((msg->MethodID == OM_NEW) &&
     	(EG(o)->Flags & GFLG_GADGIMAGE) &&
@@ -355,46 +355,47 @@ static ULONG get_gadgetclass(Class *cl, Object *o, struct opGet *msg)
 
     switch (msg->opg_AttrID)
     {
-    case GA_Left:
-    case GA_RelRight:
-	*msg->opg_Storage = (IPTR) EG(o)->LeftEdge;
-	break;
+	case GA_Left:
+	case GA_RelRight:
+	    *msg->opg_Storage = (IPTR) EG(o)->LeftEdge;
+	    break;
 
-    case GA_Top:
-    case GA_RelBottom:
-	*msg->opg_Storage = (IPTR) EG(o)->TopEdge;
-	break;
+	case GA_Top:
+	case GA_RelBottom:
+	    *msg->opg_Storage = (IPTR) EG(o)->TopEdge;
+	    break;
 
-    case GA_Width:
-    case GA_RelWidth:
-	*msg->opg_Storage = (IPTR) EG(o)->Width;
-	break;
+	case GA_Width:
+	case GA_RelWidth:
+	    *msg->opg_Storage = (IPTR) EG(o)->Width;
+	    break;
 
-    case GA_Height:
-    case GA_RelHeight:
-	*msg->opg_Storage = (IPTR) EG(o)->Height;
-	break;
+	case GA_Height:
+	case GA_RelHeight:
+	    *msg->opg_Storage = (IPTR) EG(o)->Height;
+	    break;
 
-    case GA_Selected:
-	*msg->opg_Storage = (IPTR)((BOOL)(EG(o)->Flags & GFLG_SELECTED));
-	break;
+	case GA_Selected:
+	    *msg->opg_Storage = (IPTR)((BOOL)(EG(o)->Flags & GFLG_SELECTED));
+	    break;
 
-    case GA_Disabled:
-	*msg->opg_Storage = (IPTR)((BOOL)(EG(o)->Flags & GFLG_DISABLED));
-	break;
+	case GA_Disabled:
+	    *msg->opg_Storage = (IPTR)((BOOL)(EG(o)->Flags & GFLG_DISABLED));
+	    break;
 
-    case GA_ID:
-	*msg->opg_Storage = (IPTR)EG(o)->GadgetID;
-	break;
+	case GA_ID:
+	    *msg->opg_Storage = (IPTR)EG(o)->GadgetID;
+	    break;
 
-    case GA_UserData:
-	*msg->opg_Storage = (IPTR)EG(o)->UserData;
+	case GA_UserData:
+	    *msg->opg_Storage = (IPTR)EG(o)->UserData;
 
-    default:
-	*msg->opg_Storage = (IPTR)NULL;
-	retval = 0UL;
-	break;
-    }
+	default:
+	    *msg->opg_Storage = (IPTR)NULL;
+	    retval = 0UL;
+	    break;
+	    
+    } /* switch attrid) */
 
     return(retval);
 }
@@ -432,69 +433,83 @@ AROS_UFH3S(IPTR, dispatch_gadgetclass,
 
     switch(msg->MethodID)
     {
-    case GM_RENDER:
-	retval = 1UL;
-	break;
+	case GM_RENDER:
+	    retval = 1UL;
+	    break;
 
-    case GM_LAYOUT:
-    case GM_DOMAIN:
-    case GM_GOACTIVE:
-    case GM_HANDLEINPUT:
-    case GM_GOINACTIVE:
-	/* our subclasses handle these methods */
-	break;
+	case GM_LAYOUT:
+	case GM_DOMAIN:
+	case GM_GOACTIVE:
+	case GM_HANDLEINPUT:
+	case GM_GOINACTIVE:
+	    /* our subclasses handle these methods */
+	    break;
 
-    case GM_HITTEST:
-	retval = (IPTR)hittest_gadgetclass(cl, o, (struct gpHitTest *)msg);
-	break;
+	case GM_HITTEST:
+	    retval = (IPTR)hittest_gadgetclass(cl, o, (struct gpHitTest *)msg);
+	    break;
 
-    case GM_HELPTEST:
-        retval = GMR_HELPHIT;
-	break;
-	        
-    case OM_NEW:
-	retval = DoSuperMethodA(cl, o, msg);
+	case GM_HELPTEST:
+            retval = GMR_HELPHIT;
+	    break;
 
-	if (retval)
-	{
-	    /* set some defaults */
-	    /*
-	      The instance object is cleared memory!
-	      memset (EG(retval), 0, sizeof(struct GadgetData));
-	    */
-	    EG(retval)->Flags      = GFLG_EXTENDED;
-	    EG(retval)->GadgetType = GTYP_CUSTOMGADGET;
+	case OM_NEW:
+	    retval = DoSuperMethodA(cl, o, msg);
 
-	    /* Handle our special tags - overrides defaults */
-	    set_gadgetclass(cl, (Object*)retval, (struct opSet *)msg);
-	}
-	break;
+	    if (retval)
+	    {
+		/* set some defaults */
+		/*
+		  The instance object is cleared memory!
+		  memset (EG(retval), 0, sizeof(struct GadgetData));
+		*/
+		EG(retval)->Flags      = GFLG_EXTENDED;
+		EG(retval)->GadgetType = GTYP_CUSTOMGADGET;
 
-    case OM_SET:
-    case OM_UPDATE:
-	retval = DoSuperMethodA(cl, o, msg);
-	retval += (IPTR)set_gadgetclass(cl, o, (struct opSet *)msg);
-	break;
+		/* Handle our special tags - overrides defaults */
+		set_gadgetclass(cl, (Object*)retval, (struct opSet *)msg);
+	    }
+	    break;
 
-    case OM_GET:
-	retval = (IPTR)get_gadgetclass(cl, o, (struct opGet *)msg);
-	break;
-	
-    case OM_NOTIFY:
-    	DoNotify(cl, o, &(GD(o)->IC), (struct opUpdate *)msg);
-    	break;
-    	
-    case OM_DISPOSE:
-    	FreeICData(&(GD(o)->IC));
-	retval = DoSuperMethodA(cl, o, (Msg)msg);
-    	break;    	
+	case OM_SET:
+	case OM_UPDATE:
+	    retval = DoSuperMethodA(cl, o, msg);
+	    retval += (IPTR)set_gadgetclass(cl, o, (struct opSet *)msg);
+	    break;
 
-    default:
-	retval = DoSuperMethodA(cl, o, msg);
-	break;
+	case OM_GET:
+	    retval = (IPTR)get_gadgetclass(cl, o, (struct opGet *)msg);
+	    break;
+
+	case OM_NOTIFY:
+    	    DoNotify(cl, o, &(GD(o)->IC), (struct opUpdate *)msg);
+    	    break;
+
+	case OM_DISPOSE:
+    	    FreeICData(&(GD(o)->IC));
+	    retval = DoSuperMethodA(cl, o, (Msg)msg);
+    	    break;    	
+
+	case ICM_SETLOOP:
+            GD(o)->IC.ic_LoopCounter += 1UL;
+	    break;
+
+	case ICM_CLEARLOOP:
+            GD(o)->IC.ic_LoopCounter -= 1UL;
+	    break;
+
+	case ICM_CHECKLOOP:
+            retval = GD(o)->IC.ic_LoopCounter;
+	    break;
+
+	default:
+	    retval = DoSuperMethodA(cl, o, msg);
+	    break;
+
     } /* switch */
 
     return retval;
+    
 }  /* dispatch_gadgetclass */
 
 
