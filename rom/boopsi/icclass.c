a2311385735bb73a5fe26da3a18ffe7be2078069
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-06-28 16:51:04 +0000

    OM_UPDATE must be treated like OM_NOTIFY. cleaned up src a bit.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@8309 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/boopsi/icclass.c
+++ rom/boopsi/icclass.c
@@ -38,10 +38,12 @@
 
 #include "intern.h"
 
-/****************************************************************************/
+/**********************************************************************************************/
 
 #undef BOOPSIBase
 #define BOOPSIBase	((struct Library *)(cl->cl_UserData))
+
+/**********************************************************************************************/
 
 /* icclass boopsi dispatcher
  */
@@ -51,121 +53,130 @@ AROS_UFH3S(IPTR, dispatch_icclass,
     AROS_UFHA(Msg,      msg, A1)
 )
 {
-    IPTR retval = 0UL;
-    struct ICData *ic = NULL;
+    struct ICData	*ic = NULL;
+    IPTR 		retval = 0UL;
 
     if (msg->MethodID != OM_NEW)
 	ic = INST_DATA(cl, o);
 
     switch(msg->MethodID)
     {
-    case OM_NEW:
-	retval = DoSuperMethodA(cl, o, msg);
+	case OM_NEW:
+	    retval = DoSuperMethodA(cl, o, msg);
 
-	if (!retval)
-	    break;
+	    if (!retval)
+		break;
 
-	ic = INST_DATA(cl, retval);
+	    ic = INST_DATA(cl, retval);
 
-	/* set some defaults */
-	ic->ic_Target	 = NULL;
-	ic->ic_Mapping	 = NULL;
-	ic->ic_CloneTags = NULL;
+	    /* set some defaults */
+	    ic->ic_Target	 = NULL;
+	    ic->ic_Mapping	 = NULL;
+	    ic->ic_CloneTags = NULL;
 
-	/* Handle our special tags - overrides defaults */
-	/* Fall through */
+	    /* Handle our special tags - overrides defaults */
+	    /* Fall through */
 
-    case OM_SET:
-	{
-	    struct TagItem *tstate = ((struct opSet *)msg)->ops_AttrList;
-	    struct TagItem *tag;
-
-	    while ((tag = NextTagItem((const struct TagItem **)&tstate)))
+	case OM_SET:
 	    {
-		switch(tag->ti_Tag)
-		{
-		case ICA_MAP:
-		    ic->ic_Mapping = (struct TagItem *)tag->ti_Data;
-		    break;
+		struct TagItem *tstate = ((struct opSet *)msg)->ops_AttrList;
+		struct TagItem *tag;
 
-		case ICA_TARGET:
-		    ic->ic_Target = (Object *)tag->ti_Data;
-		    break;
+		while ((tag = NextTagItem((const struct TagItem **)&tstate)))
+		{
+		    switch(tag->ti_Tag)
+		    {
+		    case ICA_MAP:
+			ic->ic_Mapping = (struct TagItem *)tag->ti_Data;
+			break;
+
+		    case ICA_TARGET:
+			ic->ic_Target = (Object *)tag->ti_Data;
+			break;
+		    }
 		}
 	    }
-	}
-	break;
-
-    case OM_NOTIFY:
-       /* Send update notification to target
-       */
-	retval = DoNotify(cl, o, INST_DATA(cl, o), (struct opUpdate *)msg);
-	break;
-
-    case OM_DISPOSE:
-	FreeICData(INST_DATA(cl, o));
-	DoSuperMethodA(cl, o, msg);
-	break;
-
-    case OM_GET:
-	switch (((struct opGet *)msg)->opg_AttrID)
-	{
-	case ICA_MAP:
-	    *((struct opGet *)msg)->opg_Storage = (ULONG)ic->ic_Mapping;
 	    break;
 
-	case ICA_TARGET:
-	    *((struct opGet *)msg)->opg_Storage = (ULONG)ic->ic_Target;
+	case OM_UPDATE: /* Maxon HotHelp says so and for example relied upon by modelclass */
+	case OM_NOTIFY:
+	   /* Send update notification to target
+	   */
+	    retval = DoNotify(cl, o, INST_DATA(cl, o), (struct opUpdate *)msg);
 	    break;
-	}
 
-	break;
+	case OM_DISPOSE:
+	    FreeICData(INST_DATA(cl, o));
+	    DoSuperMethodA(cl, o, msg);
+	    break;
 
-    /*
-	NOTE: I current don't see the purpose of the ICM_* methods
-	this implementation could be WAY off base...
-    */
+	case OM_GET:
+	    switch (((struct opGet *)msg)->opg_AttrID)
+	    {
+	    case ICA_MAP:
+		*((struct opGet *)msg)->opg_Storage = (ULONG)ic->ic_Mapping;
+		break;
 
-    case  ICM_SETLOOP:	      /* set/increment loop counter    */
-	{
-	    struct ICData *ic = INST_DATA(cl, o);
+	    case ICA_TARGET:
+		*((struct opGet *)msg)->opg_Storage = (ULONG)ic->ic_Target;
+		break;
+	    }
 
-	    ic->ic_LoopCounter += 1UL;
-	}
+	    break;
 
-	break;
+	/*
+	    NOTE: I current don't see the purpose of the ICM_* methods
+	    this implementation could be WAY off base...
+	    
+	    stegerg: well, it is for example needed by modeclass which is
+	    a superclass of icclass.
+	    
+	    IMPORTANT: ICM_SETLOOP, ICM_CLEARLOOP, ICM_CHECKLOOP are also
+	    handled by gadgetclass: change here <-> change there!
+	    
+	*/
 
-    case  ICM_CLEARLOOP:    /* clear/decrement loop counter */
-	{
-	    struct ICData *ic = INST_DATA(cl, o);
+	case  ICM_SETLOOP:	      /* set/increment loop counter    */
+	    {
+		struct ICData *ic = INST_DATA(cl, o);
 
-	    ic->ic_LoopCounter -= 1UL;
-	}
+		ic->ic_LoopCounter += 1UL;
+	    }
 
-	break;
+	    break;
 
-    case  ICM_CHECKLOOP:    /* set/increment loop	 */
-	{
-	    struct ICData *ic = INST_DATA(cl, o);
+	case  ICM_CLEARLOOP:    /* clear/decrement loop counter */
+	    {
+		struct ICData *ic = INST_DATA(cl, o);
 
-	    retval = (IPTR)ic->ic_LoopCounter;
-	}
+		ic->ic_LoopCounter -= 1UL;
+	    }
 
-	break;
+	    break;
 
-    default:
-	retval = DoSuperMethodA(cl, o, msg);
-	break;
+	case  ICM_CHECKLOOP:    /* set/increment loop	 */
+	    {
+		struct ICData *ic = INST_DATA(cl, o);
+
+		retval = (IPTR)ic->ic_LoopCounter;
+	    }
+
+	    break;
+
+	default:
+	    retval = DoSuperMethodA(cl, o, msg);
+	    break;
+	    
     } /* switch */
 
     return retval;
+    
 }  /* dispatch_icclass */
 
 #undef BOOPSIBase
 
-/****************************************************************************/
+/**********************************************************************************************/
 
-/* Initialize our image class. */
 struct IClass *InitICClass (struct Library * BOOPSIBase)
 {
     struct IClass *cl = NULL;
@@ -183,4 +194,6 @@ struct IClass *InitICClass (struct Library * BOOPSIBase)
 
     return (cl);
 }
+
+/**********************************************************************************************/
 
