2518a52ce37234d6c25422c508fa01b479d303dc
nlorentz <nlorentz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-07-01 19:16:24 +0000

    Enable x11 linedrawing again
    git-svn-id: https://svn.aros.org/svn/aros/trunk@8336 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-x11/hidd/x11gfx_intern.h
+++ arch/all-x11/hidd/x11gfx_intern.h
@@ -30,7 +30,7 @@ XImage *alloc_ximage(Display *display, int screen, ULONG width, UBYTE depth, UBY
 VOID free_ximage(XImage *image);
 
 
-#define USE_X11_DRAWFUNCS  0
+#define USE_X11_DRAWFUNCS  1
 
 /* Private Attrs and methods for the X11Gfx Hidd */
 
