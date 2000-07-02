e3c7ccc96fbc26262c44ecf5fe1627baa0711a0c
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-07-02 22:26:58 +0000

    print out return values in ScreenModeRequester struct.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@8352 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/demos/screenmodereq.c
+++ workbench/demos/screenmodereq.c
@@ -122,7 +122,16 @@ static void showrequester(char *msg, struct TagItem *tags)
 	if (AslRequest(sm, NULL))
 	{
 	    printf("\n-------------------------------------------------------\n\n");
-	} else printf("Requester was aborted\n");
+	    printf("DisplayID     = 0x%08x\n", sm->sm_DisplayID);
+	    printf("DisplayWidth  = %d\n", sm->sm_DisplayWidth);
+	    printf("DisplayHeight = %d\n", sm->sm_DisplayHeight);
+	    printf("DisplayDepth  = %d\n", sm->sm_DisplayDepth);
+	    printf("OverscanType  = %d\n", sm->sm_OverscanType);
+	    printf("AutoScroll    = %d\n", sm->sm_AutoScroll);
+	    printf("BitMapWidth   = %d\n", sm->sm_BitMapWidth);
+	    printf("BitMapHeight  = %d\n", sm->sm_BitMapHeight);
+	    
+	} else printf("\nRequester was aborted\n");
 	FreeAslRequest(sm);
     }
     else printf("Could not alloc FileRequester\n");
