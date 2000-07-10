988b2905ee1ae44b157a7877e83152bfa54d9b4f
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-07-10 19:56:22 +0000

    test program for writing files with iffparse.library.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@8427 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/iffparsewrite.c
@@ -0,0 +1,73 @@
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <proto/iffparse.h>
+#include <dos/dos.h>
+#include <libraries/iffparse.h>
+#include <datatypes/pictureclass.h>
+
+#include <stdio.h>
+#include <string.h>
+
+struct Library *IFFParseBase;
+
+static struct IFFHandle *iff;
+static BPTR fh;
+static LONG err;
+
+int main(void)
+{
+    IFFParseBase = OpenLibrary("iffparse.library",0);
+    if (!IFFParseBase)
+    {
+        printf("Could not open iffparse.library!");
+	exit(0);
+    }
+    
+    printf("Trying to write a test file called \"ram:test.iff\" ...\n\n");
+    
+    if ((iff = AllocIFF()))
+    {
+        if ((iff->iff_Stream = Open("ram:test.iff", MODE_NEWFILE)))
+	{
+	    InitIFFasDOS(iff);
+	    
+	    err = OpenIFF(iff, IFFF_WRITE);
+	    if (err == 0)
+	    {
+	        err = PushChunk(iff, ID_ILBM, ID_FORM, IFFSIZE_UNKNOWN);
+		if (err == 0)
+		{
+		    err = PushChunk(iff, ID_ILBM, MAKE_ID('A', 'B', 'C', 'D'), IFFSIZE_UNKNOWN);
+		    if (err == 0)
+		    {
+		        char *buffer = "1234567890";
+			
+			err = WriteChunkBytes(iff, buffer, strlen(buffer));
+			if (err == strlen(buffer))
+			{
+			    printf("********* No errors during writing :-) **************\n");
+						    
+			} else printf("WriteChunkBytes(iff, buffer, strlen(buffer)) returned %d\n",err);
+		        
+			PopChunk(iff);
+			
+		    } else printf("PushChunk(iff, ID_ILBM, MAKE_ID('A', 'B', 'C', 'D'), IFFSIZE_UNKNOWN) returned error %d\n",err);
+		    
+		    PopChunk(iff);
+		    
+		} else printf("PushChunk(iff, ID_ILBM, ID_FORM, IFFSIZE) returned error %d\n",err);
+		
+	        CloseIFF(iff);
+		
+	    } else printf("OpenIFF returned error %d\n",err);
+	    
+	    Close(iff->iff_Stream);
+	    
+	} else printf("Could not open ram:test.iff for write!\n");
+        
+	FreeIFF(iff);
+	
+    } else printf("AllocIFF failed!\n");
+    
+    return 0;
+}
