15d13ebcd8b6dd6c4d48d7a61bea6cb16ddb0af6
bernie <bernie@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-07-10 12:30:32 +0000

    var-args stubs for cybergraphics.library
    git-svn-id: https://svn.aros.org/svn/aros/trunk@8410 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/alib/lockbitmaptags.c
@@ -0,0 +1,42 @@
+#define AROS_TAGRETURNTYPE APTR
+#include "alib_intern.h"
+
+extern struct Library * CyberGfxBase;
+
+/*****************************************************************************
+
+    NAME */
+#define NO_INLINE_STDARG /* turn off inline def */
+#include <proto/cybergraphics.h>
+
+	APTR LockBitMapTags (
+
+/*  SYNOPSIS */
+	APTR handle,
+	Tag tag1,
+	...)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+    AROS_SLOWSTACKTAGS_PRE(tag1)
+    retval = LockBitMapTagList(handle, AROS_SLOWSTACKTAGS_ARG(tag1));
+    AROS_SLOWSTACKTAGS_POST
+} /* LockBitMapTags */
---
+++ compiler/alib/unlockbitmaptags.c
@@ -0,0 +1,42 @@
+#define AROS_TAGRETURNTYPEVOID
+#include "alib_intern.h"
+
+extern struct Library * CyberGfxBase;
+
+/*****************************************************************************
+
+    NAME */
+#define NO_INLINE_STDARG /* turn off inline def */
+#include <proto/cybergraphics.h>
+
+	void UnLockBitMapTags (
+
+/*  SYNOPSIS */
+	APTR handle,
+	Tag tag1,
+	...)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+    AROS_SLOWSTACKTAGS_PRE(tag1)
+    UnLockBitMapTagList(handle, AROS_SLOWSTACKTAGS_ARG(tag1));
+    AROS_SLOWSTACKTAGS_POST
+} /* UnLockBitMapTags */
