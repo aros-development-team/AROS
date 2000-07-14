3f5f72367c5e5aedbb3813bdb8cf2e4feb2ecbb6
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-07-14 20:43:02 +0000

    missing include
    git-svn-id: https://svn.aros.org/svn/aros/trunk@8454 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/gadgets/gradientslider.h
@@ -0,0 +1,24 @@
+#ifndef GADGETS_GRADIENTSLIDER_H
+#define GADGETS_GRADIENTSLIDER_H
+
+/*
+    (C) 2000 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: MethodIDs and AttrIDs for the gradientslider class.
+    Lang: english
+*/
+
+#ifndef UTILITY_TAGITEM_H
+#    include <utility/tagitem.h>
+#endif
+
+#define GRAD_Dummy	(TAG_USER + 0x05000000)
+#define GRAD_MaxVal	(GRAD_Dummy + 1)     /* slider's max value	   		*/
+#define GRAD_CurVal	(GRAD_Dummy + 2)     /* slider's current value	  		*/
+#define GRAD_SkipVal	(GRAD_Dummy + 3)     /* move amount of "body click" move amount */
+#define GRAD_KnobPixels (GRAD_Dummy + 4)     /* knob size	   			*/
+#define GRAD_PenArray	(GRAD_Dummy + 5)     /* pen colors		   		*/
+
+
+#endif /* GADGETS_GRADIENTSLIDER_H */
