15654756fcfea56d03c49c5cb505d8ad20ea04d1
nlorentz <nlorentz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-07-20 15:08:50 +0000

    New private gfx function for allocating screen bitmaps
    git-svn-id: https://svn.aros.org/svn/aros/trunk@8495 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/graphics/allocscreenbitmap.c
@@ -0,0 +1,58 @@
+/*
+    (C) 1995 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Private graphics function for allocating screen bitmaps
+    Lang: english
+*/
+#include "graphics_intern.h"
+#include <exec/memory.h>
+#include <graphics/rastport.h>
+#include <proto/exec.h>
+#include <oop/oop.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <graphics/rastport.h>
+#include <proto/graphics.h>
+
+	AROS_LH1(struct BitMap * , AllocScreenBitMap,
+
+/*  SYNOPSIS */
+	AROS_LHA(ULONG, modeid, D0),
+
+/*  LOCATION */
+	struct GfxBase *, GfxBase, 182, Graphics)
+
+/*  FUNCTION
+	Allocates a bitmap for use with a screen opened by OpenScreen()
+
+    INPUTS
+	modeid - the DisplayID of the screen to create
+
+    RESULT
+    	bitmap - pointer to the newly created bitmap.
+
+    NOTES
+	This function is private and AROS specific.
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+    AROS_LIBBASE_EXT_DECL(struct GfxBase *,GfxBase)
+    
+    return driver_AllocScreenBitMap(modeid, GfxBase);
+
+    AROS_LIBFUNC_EXIT
+} /* AllocScreenBitMap */
