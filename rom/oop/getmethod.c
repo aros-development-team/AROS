e181a48a15829cb2d498e934cecd55f3e1f56194
nlorentz <nlorentz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-07-20 21:03:55 +0000

    Added function to get a pointer directly to a method function. Should be somewhat faster then using stubs. Note that the method function you get may only be used for the particular object it was queried from
    git-svn-id: https://svn.aros.org/svn/aros/trunk@8513 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/oop/getmethod.c
@@ -0,0 +1,83 @@
+/*
+    (C) 1995-96 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Get a pointer to a method for an object
+    Lang: english
+*/
+#define AROS_ALMOST_COMPATIBLE
+#include <exec/lists.h>
+#include <proto/exec.h>
+#include <oop/oop.h>
+#include "intern.h"
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/oop.h>
+
+	AROS_LH2(VOID *, GetMethod,
+
+/*  SYNOPSIS */
+	AROS_LHA(Object  *, obj, 	A0),
+	AROS_LHA(MethodID,  mid,	D0),
+
+/*  LOCATION */
+	struct Library *, OOPBase, 21, OOP)
+
+/*  FUNCTION
+	Get a specific method function for a specific object and 
+	a specific interface. This a direct pointer to the method implementation.
+	The pointer should ONLY be used on the object you aquired.
+
+    INPUTS
+    	obj	- pointer to object to get method for.
+	mid	- method id for method to get. This may be obtained with GetMethodID()
+
+    RESULT
+    	The method asked for, or NULL if the method does not exist in
+	the object's class.
+
+    NOTES
+	!!! Use with EXTREME CAUTION. Very few programs need the extra speed gained
+	    by calling a method directly 
+	!!!
+
+    EXAMPLE
+
+    BUGS
+	It returns VOID *. I got compiler errors when returning
+	IPTR (*)(Class *, Object *, Msg)
+
+    SEE ALSO
+    	GetMethodID()
+
+    INTERNALS
+
+    HISTORY
+	29-10-95    digulla automatically created from
+			    intuition_lib.fd and clib/intuition_protos.h
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+    AROS_LIBBASE_EXT_DECL(struct Library*,OOPBase)
+    
+    struct IFMethod *ifm;
+    
+    /* First get mid */
+    /* Get the method from the object's class */
+    ifm = meta_findmethod((Object *)OCLASS(obj), mid, (struct Library *)OOPBase);
+    if (NULL == ifm)
+	return NULL;
+
+    /* Paranoia */    
+    if (NULL == ifm->MethodFunc) {
+	kprintf("!!! OOP/GetMethod(): IFMethod instance had no methodfunc. This should NEVER happen !!!\n");
+	return NULL;
+    }
+    
+    return (VOID *)ifm->MethodFunc;
+
+    AROS_LIBFUNC_EXIT
+} /* DisposeObject */
