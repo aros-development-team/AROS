20184b62f3fa866b801bc964f2ae9000d27b901d
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-08-09 12:32:34 +0000

    updated some macros so that they work like in OS 3.5 includes.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@8689 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/graphics/gfxmacros.h
+++ compiler/include/graphics/gfxmacros.h
@@ -17,6 +17,12 @@
 #   include <graphics/rastport.h>
 #endif
 
+/* NOTE: The following symbol must be defined in your source
+         code if one needs the old style macros. Otherwise
+	 one will get the new veresions, which are more robust */
+
+#ifdef OLD_GRAPHICS_GFXMACROS_H
+	 
 /* Some macros which should be functions... */
 #define SetDrPt(w,p) \
     { \
@@ -24,16 +30,19 @@
 	(w)->Flags |= FRST_DOT|0x10; \
 	(w)->linpatcnt = 15; \
     }
+
 #define SetAfPt(w,p,n) \
     { \
 	(w)->AreaPtrn = p; \
 	(w)->AreaPtSz = n; \
     }
+
 #define SetOPen(w,c) \
     { \
 	(w)->AOlPen = c; \
 	(w)->Flags |= AREAOUTLINE; \
     }
+
 #define SetAOlPen(w,p)  SetOutlinePen(w,p)
 #define SetWrMsk(w,m)   SetWriteMask(w,m)
 #define BNDRYOFF(w)     {(w)->Flags &= ~AREAOUTLINE;}
@@ -41,5 +50,42 @@
 /* Shortcuts */
 #define DrawCircle(rp,cx,cy,r)  DrawEllipse(rp,cx,cy,r,r);
 #define AreaCircle(rp,cx,cy,r)  AreaEllipse(rp,cx,cy,r,r);
+
+#else /* OLD_GRAPHICS_GFXMACROS_H */
+
+/* Some macros which should be functions... */
+#define SetDrPt(w,p) \
+    do { \
+	(w)->LinePtrn = p; \
+	(w)->Flags |= FRST_DOT|0x10; \
+	(w)->linpatcnt = 15; \
+    } while (0)
+
+#define SetAfPt(w,p,n) \
+    do { \
+	(w)->AreaPtrn = p; \
+	(w)->AreaPtSz = n; \
+    } while (0)
+
+#define SetOPen(w,c) \
+    do { \
+	(w)->AOlPen = c; \
+	(w)->Flags |= AREAOUTLINE; \
+    } while (0)
+
+#define SetAOlPen(w,p)  SetOutlinePen(w,p)
+#define SetWrMsk(w,m)   SetWriteMask(w,m)
+
+#define BNDRYOFF(w) \
+    do { \
+       	(w)->Flags &= ~AREAOUTLINE; \
+    } while (0)
+
+
+/* Shortcuts */
+#define DrawCircle(rp,cx,cy,r)  DrawEllipse(rp,cx,cy,r,r);
+#define AreaCircle(rp,cx,cy,r)  AreaEllipse(rp,cx,cy,r,r);
+
+#endif /* OLD_GRAPHICS_GFXMACROS_H */
 
 #endif /* GRAPHICS_GFXMACROS_H */
