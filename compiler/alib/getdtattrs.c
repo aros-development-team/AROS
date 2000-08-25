299fe03798cf9c82407b5ead9fb0a829d77831de
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-08-25 20:05:24 +0000

    was missing
    git-svn-id: https://svn.aros.org/svn/aros/trunk@8787 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/alib/getdtattrs.c
@@ -0,0 +1,53 @@
+/*
+    Copyright (C) 2000 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Varargs version of datatypes.library/GetDTAttrsA()
+    Lang: english
+*/
+#define AROS_TAGRETURNTYPE ULONG
+#include <intuition/classusr.h>
+#include <utility/tagitem.h>
+
+extern struct Library *DataTypesBase;
+
+/*****************************************************************************
+
+    NAME */
+#define NO_INLINE_STDARG /* turn off inline def */
+#include <proto/datatypes.h>
+
+	ULONG GetDTAttrs (
+
+/*  SYNOPSIS */
+	Object * o,
+	Tag tag1,
+	...)
+
+/*  FUNCTION
+        This is the varargs version of datatypes.library/GetDTAttrsA().
+        For information see datatypes.library/GetDTAttrsA().
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+        graphics/GetRPAttrsA()
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+    AROS_SLOWSTACKTAGS_PRE(tag1)
+    retval = GetDTAttrsA (o, AROS_SLOWSTACKTAGS_ARG(tag1));
+    AROS_SLOWSTACKTAGS_POST
+} /* GetRPAttrs */
