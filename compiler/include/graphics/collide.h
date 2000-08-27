7e99fe689b89dec49863e93fef7ee1ea424f6333
lbischoff <lbischoff@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-08-27 14:35:01 +0000

    Just added.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@8804 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/graphics/collide.h
@@ -0,0 +1,19 @@
+#ifndef GRAPHICS_COLLIDE_H
+#define GRAPHICS_COLLIDE_H
+
+/*
+    (C) 2000 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: collision detection and control
+    Lang: english
+*/
+
+#define BORDERHIT 0
+
+#define TOPHIT 1
+#define BOTTOMHIT 2
+#define LEFTHIT 4
+#define RIGHTHIT 8
+
+#endif /* GRAPHICS_COLLIDE_H */
