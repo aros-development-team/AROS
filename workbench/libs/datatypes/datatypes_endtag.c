55ab07e05e327790443348a4d13d8f4b4ce52e7c
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-09-11 19:16:18 +0000

    this is a generated file.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@8858 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/datatypes/datatypes_endtag.c
+++
@@ -1,8 +0,0 @@
-/*
-        (C) 1995-96 AROS - The Amiga Research OS
-        *** Automatic generated file. Do not edit ***
-        Desc: Resident endskip for DataTypes
-        Lang: english
-*/
-
-const char DataTypes_end = 0;
