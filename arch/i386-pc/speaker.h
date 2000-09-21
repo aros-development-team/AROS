87952a86c528fc72d777d1e4bfd07a4eae3c2681
lbischoff <lbischoff@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-09-21 21:17:54 +0000

    Some macros to access the PC-speaker. Hope this helps debugging AROS-native.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@8907 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/i386-pc/speaker.h
@@ -0,0 +1,28 @@
+#ifndef _SPEAKER_H
+#define _SPEAKER_H
+
+#define inb(port) \
+    ({	char __value;	\
+	__asm__ __volatile__ ("inb $" #port ",%%al":"=a"(__value));	\
+	__value;	})
+
+#define outb(val,port) \
+    ({	char __value=(val);	\
+	__asm__ __volatile__ ("outb %%al,$" #port::"a"(__value)); })
+
+
+#define SetSpkFreq(freq)                                  \
+    do                                                    \
+    {                                                     \
+	WORD counter = 0x1234DD / freq;                   \
+ 	outb (0xB6, 0x43);                                \
+	outb ((UBYTE) (counter & 0x00FF), 0x42);          \
+	outb ((UBYTE) ((counter & 0xFF00) >> 8), 0x42);   \
+    }                                                     \
+    while (0)
+
+#define SpkOn() outb (inb(0x61) | 3, 0x61)
+
+#define SpkOff() outb (inb(0x61) & ~3, 0x61)
+
+#endif /* _SPEAKER_H */
