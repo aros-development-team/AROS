0055b80106e5f4b6ca27204db7d18ca2cf0d5b42
hkiel <hkiel@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-09-30 20:36:52 +0000

    Support for include files.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@8945 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- tools/genskeleton/genskeleton.c
+++ tools/genskeleton/genskeleton.c
@@ -22,6 +22,10 @@
 #include <ctype.h>
 #include <sys/errno.h>
 
+/* Fool the CVS server not to replace empty cvs version tags in printfs */
+#define TAG	" $"
+#define VERSION	"$Id:"
+
 /****************************************************************************************/
 
 #define TYPE_LIBRARY 	0
@@ -59,7 +63,8 @@ static char lowername[300];
 static char uppername[300];
 static char shortname[300];
 static char basetype[300];
-static int  type, i;
+static char includesdir[300];
+static int  includes, type, i;
 static long l;
 
 /****************************************************************************************/
@@ -78,10 +83,10 @@ static void getstring(char *dest, char *def)
 static int getinteger(void)
 {
     int ret = 0;
-    
+
     getstring(s, "0");
     ret = strtol(s, 0, 10);
-    
+
     return ret;
 }
 
@@ -90,11 +95,11 @@ static int getinteger(void)
 static void getarguments(void)
 {
     int i2;
-    
+
     printf("Type (1 = Library, 2 = Device, 3 = Gadget, 4 = DataType): ");
     type = getinteger() - 1;
     if ((type < TYPE_MIN) || (type > TYPE_MAX)) type = TYPE_LIBRARY;
-    
+
     printf("Name (e.g. Intuition, GadTools, Asl)                    : ");
     getstring(name, "Dummy");
 
@@ -105,16 +110,37 @@ static void getarguments(void)
 	if (toupper(name[i]) == name[i]) shortname[i2++] = name[i];
     }
     strcat(shortname, "B");
-    
+
     printf("Lib Base Type (e.g. IntuitionBase, Library, Device)     : ");
     getstring(basetype, typeinfo[type].defbasetype);
-     
+
     printf("Destination directory                                   : ");
     getstring(destdir, "");
     if (destdir[0])
     {
         l = strlen(destdir);
 	if (destdir[l - 1] != '/') strcat(destdir, "/");
+    }
+
+    printf("Includes (0 = None, 1 = in libraries/, 2 = $(LIBNAME)/, 3 = other : ");
+    includes = getinteger();
+    switch (includes)
+    {
+	case 0:
+	    break;
+
+	case 1:
+	    sprintf( includesdir, "libraries");
+	    break;
+
+	case 2:
+	    sprintf( includesdir, "$(LIBNAME)");
+	    break;
+
+	default:
+	    printf("Includes Destination directory                          : ");
+	    getstring(includesdir, "");
+	    break;
     }
 
 }
@@ -128,10 +154,18 @@ static void showarguments(void)
     printf("Name                 : %s\n", name);
     printf("Lib Base Type        : %s\n", basetype);
     printf("Destination directory: %s\n", destdir);
+    if (includes)
+    {
+	printf("Includes directory   : %s\n", includesdir);
+    }
+    else
+    {
+	printf("No includes.\n");
+    }
     puts("");
     printf("Are you sure you want to continue (yes/no)? ");
     getstring(s, "");
-    
+
     if ((s[0] != 'y') && s[0] != 'Y')
     {
         fprintf(stderr, "\n*** Aborted\n");
@@ -156,17 +190,29 @@ static void preparedir(void)
 	    fprintf(stderr, "\n*** Aborted\n");
 	    exit(1);
 	}
-    } else {
-
+    }
+    else {
 	err = mkdir(destdir, 0766);
 	if (err)
 	{
 	    fprintf(stderr, "Error: could not create directory \"%s\"! Error = %i\n", destdir, err);
 	    exit(1);
 	}
+	if (includes)
+	{
+	    char incdir[300];
+
+	    sprintf(incdir, "%s/include", destdir);
+	    err = mkdir(incdir, 0766);
+	    if (err)
+	    {
+		fprintf(stderr, "Error: could not create directory \"%s\"! Error = %i\n", incdir, err);
+		exit(1);
+	    }
+	}
     }
     puts("");
-    
+
 }
 
 /****************************************************************************************/
@@ -174,19 +220,19 @@ static void preparedir(void)
 static void make_cvsignore(void)
 {
     FILE *f;
-    
+
     printf("Creating %s.cvsignore\n", destdir);
-    
+
     strcpy(s, destdir);
     strcat(s, ".cvsignore");
-    
+
     f = fopen(s, "w");
     if (!f)
     {
         fprintf(stderr, "Error: could not create \"%s\"!\n", s);
 	exit(1); 
     }
-    
+
     fprintf(f, "*_end.c\n"
     	       "*_endtag.c\n"
 	       "*.map\n"
@@ -202,26 +248,26 @@ static void make_cvsignore(void)
 static void make_libconf(void)
 {
     FILE *f;
-    
+
     printf("Creating %slib.conf\n", destdir);
-    
+
     strcpy(s, destdir);
     strcat(s, "lib.conf");
-    
+
     f = fopen(s, "w");
     if (!f)
     {
         fprintf(stderr, "Error: could not create \"%s\"!\n", s);
 	exit(1); 
     }
-    
+
     fprintf(f, "name %s\n", lowername);    
     fprintf(f, "basename %s\n", name);
     fprintf(f, "libbase %sBase\n", name);
     fprintf(f, "libbasetype %s\n", basetype);
     fprintf(f, "version 41.0\n");
     fprintf(f, "type %s\n", typeinfo[type].lowertypestring);
-    
+
     fclose(f);
 }
 
@@ -230,20 +276,20 @@ static void make_libconf(void)
 static void make_mmakefilesrc(void)
 {
     FILE *f;
-    
+
     printf("Creating %smmakefile.src\n", destdir);
-    
+
     strcpy(s, destdir);
     strcat(s, "mmakefile.src");
-    
+
     f = fopen(s, "w");
     if (!f)
     {
         fprintf(stderr, "Error: could not create \"%s\"!\n", s);
 	exit(1); 
     }
-    
-    fprintf(f, "# $Id$\n"
+
+    fprintf(f, "# "VERSION TAG"\n"
     	        "include $(TOP)/config/make.cfg\n"
 		"\n"
 		"USER_INCLUDES := -I.\n"
@@ -254,30 +300,47 @@ static void make_mmakefilesrc(void)
     fprintf(f, "LIBPOST   := %s\n", typeinfo[type].lowertypestring);
     fprintf(f, "DESTDIR   := %s\n", typeinfo[type].destdir);
     fprintf(f, "\n");
-    
+
     fprintf(f, "FILES     := %s_init\n\n", lowername);
-    
+
     fprintf(f, "FUNCTIONS := dummyfunction\n\n");
-    
+
+    if (includes)
+    {
+	fprintf(f, "INC_FILES = $(wildcard include/*.h)\n");
+	fprintf(f, "DEST_INC = $(foreach f,$(INC_FILES), $(INCDIR)/%s/$(notdir $f))\n",includesdir);
+	fprintf(f, "GEN_INC = $(foreach f,$(INC_FILES), $(GENINCDIR)/%s/$(notdir $f))\n\n",includesdir);
+	fprintf(f, "#MM\n");
+	fprintf(f, "setup-includes :\n");
+	fprintf(f, "%%mkdirs_q $(GENINCDIR)/%s $(INCDIR)/%s\n\n",includesdir, includesdir);
+	fprintf(f, "#MM\n");
+	fprintf(f, "includes-copy : $(DEST_INC) $(GEN_INC)\n");
+	fprintf(f, "\t@$(NOP)\n\n");
+	fprintf(f, "$(INCDIR)/%s/%%.h : include/%%.h\n",includesdir);
+	fprintf(f, "\t@$(CP) include/$(notdir $<) $@\n\n");
+	fprintf(f, "$(GENINCDIR)/%s/%%.h : include/%%.h\n",includesdir);
+	fprintf(f, "\t@$(CP) include/$(notdir $<) $@\n\n");
+    }
+
     fprintf(f, "%%define_libs prelibs=-l$(LIBNAME)\n\n");
-    
+
     fprintf(f, "%%genlib_cpak\n\n");
-    
+
     fprintf(f, "#MM %s%s : setup includes\n", typeinfo[type].metatarget, lowername);
     fprintf(f, "%s%s : $(SLIB)\n", typeinfo[type].metatarget, lowername);
     fprintf(f, "\t@$(NOP)\n\n");
-    
+
     fprintf(f, "#MM\n"
     	       "setup :\n"
 	       "\t%%mkdirs_q $(OBJDIR) $(LIBDIR) %s\n\n", typeinfo[type].destdir);
-	       
+
     fprintf(f, "#MM\n"
     	       "clean ::\n"
 	       "\t$(RM) $(OBJDIR) *.err libdefs.h functable.c functions.* mmakefile \\\n"
 	       "\t$(END_FILE).c $(LIB) $(SLIB)\n\n");
-    
+
     fprintf(f, "%%common\n");
-    	       
+
     fclose(f);
 }
 
@@ -286,24 +349,24 @@ static void make_mmakefilesrc(void)
 static void make_internh(void)
 {
     FILE *f;
-    
+
     printf("Creating %s%s_intern.h\n", destdir, lowername);
-    
+
     sprintf(s, "%s%s_intern.h", destdir, lowername);
-     
+
     f = fopen(s, "w");
     if (!f)
     {
         fprintf(stderr, "Error: could not create \"%s\"!\n", s);
 	exit(1); 
     }
-    
+
     fprintf(f, "#ifndef %s_INTERN_H\n", uppername);
     fprintf(f, "#define %s_INTERN_H\n", uppername);
     fprintf(f, "\n");
     fprintf(f, "/*\n");
     fprintf(f, "    (C) 2000 AROS - The Amiga Research OS\n");
-    fprintf(f, "    $Id$\n");
+    fprintf(f, "    "VERSION TAG"\n");
     fprintf(f, "\n");
     fprintf(f, "    Desc: Internal definitions for %s.%s\n", lowername, typeinfo[type].lowertypestring);
     fprintf(f, "    Lang: English\n");
@@ -350,7 +413,7 @@ static void make_internh(void)
     fprintf(f, "/****************************************************************************************/\n");
     fprintf(f, "\n");
     fprintf(f, "#endif /* %s_INTERN_H */\n", uppername);
-    
+
     fclose(f);
 }
 
@@ -359,11 +422,11 @@ static void make_internh(void)
 static void make_initc()
 {
     FILE *f;
-    
+
     printf("Creating %s%s_init.c\n", destdir, lowername);
-    
+
     sprintf(s, "%s%s_init.c", destdir, lowername);
-     
+
     f = fopen(s, "w");
     if (!f)
     {
@@ -373,7 +436,7 @@ static void make_initc()
 
     fprintf(f, "/*\n");
     fprintf(f, "    (C) 2000 AROS - The Amiga Research OS\n");
-    fprintf(f, "    $Id$\n");
+    fprintf(f, "    "VERSION TAG"\n");
     fprintf(f, "\n");
     fprintf(f, "    Desc: %s initialization code.\n", name);
     fprintf(f, "    Lang: English\n");
@@ -456,7 +519,7 @@ static void make_initc()
     fprintf(f, "}\n");
     fprintf(f, "\n");
     fprintf(f, "/****************************************************************************************/\n");
-    
+
     fclose(f);
 }
 
@@ -465,11 +528,11 @@ static void make_initc()
 static void make_dummyfunction(void)
 {
     FILE *f;
-    
+
     printf("Creating %sdummyfunction.c\n", destdir);
-    
+
     sprintf(s, "%sdummyfunction.c", destdir);
-     
+
     f = fopen(s, "w");
     if (!f)
     {
@@ -479,7 +542,7 @@ static void make_dummyfunction(void)
 
     fprintf(f, "/*\n");
     fprintf(f, "    (C) 2000 AROS - The Amiga Research OS\n");
-    fprintf(f, "    $Id$\n");
+    fprintf(f, "    "VERSION TAG"\n");
     fprintf(f, "\n");
     fprintf(f, "    Desc: %s function DummyFunction()\n", name);
     fprintf(f, "    Lang: English\n");
@@ -550,7 +613,7 @@ int main(void)
     make_internh();
     make_initc();
     make_dummyfunction();
-    
+
     return 0;
 }
 
