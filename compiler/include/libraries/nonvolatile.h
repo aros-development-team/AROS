6c6e7bee8d5673af831f61cb64a57de8c5eeb6c2
SDuvan <SDuvan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-11-05 12:52:40 +0000

    Nonvolatile.library definitions
    git-svn-id: https://svn.aros.org/svn/aros/trunk@9113 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/libraries/nonvolatile.h
@@ -0,0 +1,55 @@
+
+#ifndef  LIBRARIES_NONVOLATILE_H
+#define  LIBRARIES_NONVOLATILE_H
+
+/*
+    (C) 2000 AROS - The Amiga Research OS
+    $Id$
+
+    Desc:
+    Lang: English
+*/
+
+#include  <exec/types.h>
+#include  <exec/nodes.h>
+
+
+struct NVInfo
+{
+    ULONG  nvi_MaxStorage;
+    ULONG  nvi_FreeStorage;
+};
+
+
+struct NVEntry
+{
+    struct MinNode  nve_Node;
+    STRPTR          nve_Name;
+    ULONG           nve_Size;
+    ULONG           nve_Protection;
+};
+
+
+// Bit definitions for the mask in SetNVProtection() and NVEntry.nve_Protection
+
+#define  NVEB_DELETE   0
+#define  NVEB_APPNAME  31
+
+#define  NVEF_DELETE   (1 << NVEB_DELETE)
+#define  NVEF_APPNAME  (1 << NVEB_APPNAME)
+
+
+// Errors reported by StoreNV()
+
+#define  NVERR_BADNAME    1
+#define  NVERR_WRITEPROT  2
+#define  NVERR_FAIL       3
+#define  NVERR_FATAL      4
+
+
+// The size of the data returned by this library
+
+#define SizeNVData(d) ((((ULONG *)d)[-1]) - sizeof(ULONG))
+
+
+#endif  // LIBRARIES_NONVOLATILE_H
