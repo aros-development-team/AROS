a04942d60ab37f4e7401f2f4ea6992d933f142e9
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-11-21 03:14:31 +0000

    Now also compares the collision masks and determines whether there is a collision or not on a bit by bit basis (actually 16bit by 16bit basis). Don't know whether this works 100% correct.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@9233 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/graphics/docollision.c
+++ rom/graphics/docollision.c
@@ -84,9 +84,126 @@
              * Phew, this is not going to be easy.
              */
             int collision = FALSE;
-
+            int dy = _CurVSprite->Y - CurVSprite->Y;
+            UWORD *  collmask =  CurVSprite->CollMask;
+            UWORD * _collmask = _CurVSprite->CollMask;
+            unsigned short  wordsperline; 
+            unsigned short _wordsperline;
+            int i = 0, _i = 0;
+            int offset    , _offset = 0;
+            int _scroll = 0;
+            int line = 0;
 #warning Not comparing collision masks!!!!!!!!!
             // put collision mask comparison here!
+            /*
+             * I will hold the CurVSprite's collision mask still
+             * and adjust/shift the collision mask of _CurVSprite.
+             * If any and'ing results with non-zero, there is a
+             * collision.
+             */
+            if (CurVSprite->Flags & VSPRITE)
+              wordsperline = CurVSprite->Width >> 4; // should be 1
+            else
+              wordsperline = CurVSprite->Width;
+
+            if (_CurVSprite->Flags & VSPRITE)
+              _wordsperline = _CurVSprite->Width >> 4; // should be 1
+            else
+              _wordsperline = _CurVSprite->Width;
+            
+            if (dy > 0)
+            {
+               /*
+                * _CurVSprite is lower than CurVSprite
+                */
+               i =  wordsperline * dy;
+               line = _CurVSprite->Y;
+            }
+            else
+            {
+              _i = _wordsperline * dy;
+              line = CurVSprite->Y;
+            }
+            
+            offset = _CurVSprite->X - CurVSprite->X;
+            if (offset < 0)
+            {
+              _offset = (-offset) >> 4;
+              _scroll = (-offset) & 0xf;              
+              offset = 0;
+            }
+            else
+            {
+              /*
+               * _CurVSprite is further to the right than 
+               * CurVSprite.
+               */
+              _scroll = offset & 0xf;
+              offset >>= 4;
+            }
+            
+            while ((line < ( CurVSprite->Height+ CurVSprite->Y)) &&
+                   (line < (_CurVSprite->Height+_CurVSprite->Y))   )
+            {
+              int  curindex =  offset;
+              int _curindex = _offset;
+              while ( curindex <  wordsperline &&
+                     _curindex < _wordsperline)
+              {
+                if (_scroll > 0)
+                {
+                  if (collmask[curindex+i] & (_collmask[_curindex+_i] >> _scroll) )
+                  {
+                    collision = TRUE;
+                    break;
+                  }
+                  curindex++;
+                  if (curindex > wordsperline)
+                    break;
+                  
+                  if (collmask[curindex+i] & (_collmask[_curindex+_i] << (16-_scroll)))
+                  {
+                    collision = TRUE;
+                    break;
+                  }  
+                  _curindex++;
+                }
+                else
+                if (0 == _scroll)
+                {
+                  if (collmask[curindex+i] & _collmask[_curindex+_i])
+                  {
+                    collision = TRUE;
+                    break;
+                  }
+                  _curindex++;
+                   curindex++;
+                }
+                else
+                {
+                  if (collmask[curindex+i] & (_collmask[_curindex+_i] << (-_scroll)))
+                  {
+                    collision = TRUE;
+                    break;
+                  }
+                  _curindex++;
+                  if (_curindex > _wordsperline)
+                    break;
+                  
+                  if (collmask[curindex+i] & (_collmask[_curindex+_i] >> (16+_scroll)))
+                  {
+                    collision = TRUE;
+                    break;
+                  }  
+                  curindex++;
+                }
+              }
+               i+= wordsperline;
+              _i+=_wordsperline;
+              line++;
+            }
+
+
             collision = TRUE;
             
             if (TRUE == collision)
