22898550c0ccde23cb870dd1b5003e754cf6f97a
hkiel <hkiel@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-11-21 23:40:19 +0000

    This file is created automatically.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@9240 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/reqtools/reqtools_endtag.c
+++
@@ -1,8 +0,0 @@
-/*
-        (C) 1995-96 AROS - The Amiga Research OS
-        *** Automatic generated file. Do not edit ***
-        Desc: Resident endskip for ReqTools
-        Lang: english
-*/
-
-const char ReqTools_end = 0;
