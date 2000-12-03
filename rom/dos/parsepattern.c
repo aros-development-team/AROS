0143a581011808230c4f19a8c1724bf7cdb7dd6d
SDuvan <SDuvan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-12-03 11:31:02 +0000

    Merged the functions for case sensitive and insensitive pattern stuff, and increased readability of the code
    git-svn-id: https://svn.aros.org/svn/aros/trunk@9336 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/parsepattern.c
+++ rom/dos/parsepattern.c
@@ -1,9 +1,9 @@
 /*
-    (C) 1995-97 AROS - The Amiga Research OS
+    (C) 1995-2000 AROS - The Amiga Research OS
     $Id$
 
     Desc:
-    Lang: english
+    Lang: English
 */
 #include <proto/exec.h>
 #include <dos/dos.h>
@@ -74,146 +74,8 @@
 {
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct DosLibrary *,DOSBase)
-    STRPTR stack, end;
-    UBYTE a;
-    LONG iswild=0;
 
-    LONG *result=&((struct Process *)FindTask(NULL))->pr_Result2;
-#undef ERROR
-#define ERROR(a) { *result=a; return -1; }
-    stack=end=Dest+DestLength;
-#define PUT(a) { if(Dest>=stack) ERROR(ERROR_BUFFER_OVERFLOW); *Dest++=(a); }
-    
-    if(!*Source)
-    {
-        PUT(0);
-        return 0;
-    }
+    return patternParse(Source, Dest, DestLength, TRUE, DOSBase);
 
-    while(*Source)
-    {
-        switch(*Source++)
-        {
-            case '#':
-                iswild=1;
-		switch(*Source)
-		{
-		    case '?':
-			Source++;
-			PUT(P_ANY);
-			break;
-		    case ')':
-		    case '\0':
-			ERROR(ERROR_BAD_TEMPLATE);
-			break;
-		    default:
-			PUT(P_REPBEG);
-			*--stack=P_REPEND;
-			continue;
-                }
-                break;
-            case '~':
-		switch(*Source)
-		{
-		    case '\0':
-			a=Source[-1];
-			PUT(a);
-			break;
-		    case ')':
-			ERROR(ERROR_BAD_TEMPLATE);
-			break;
-		    default:
-			iswild=1;
-			PUT(P_NOT);
-			*--stack=P_NOTEND;
-			continue;
-		}
-		break;
-            case '?':
-                iswild=1;
-                PUT(P_SINGLE);
-                continue;
-            case '(':
-                PUT(P_ORSTART);
-                *--stack=P_OREND;
-                continue;
-            case '|':
-                iswild=1;
-		if(stack==end)
-		    ERROR(ERROR_BAD_TEMPLATE);
-		while(!(*stack==P_OREND||stack==end))
-		   PUT(*stack++);
-                PUT(P_ORNEXT);
-                continue;
-            case ')':
-                while(!(stack==end||*stack==P_OREND))
-		    PUT(*stack++);
-		if(stack==end)
-		    ERROR(ERROR_BAD_TEMPLATE)
-		else
-		    PUT(*stack++);
-                break;
-            case '[':
-		iswild=1;
-                if(*Source=='~')
-		{
-		    Source++;
-                    PUT(P_NOTCLASS);
-                }else
-                    PUT(P_CLASS);
-		a=*Source++;
-		if(!a)
-		    ERROR(ERROR_BAD_TEMPLATE);
-		do
-		{
-		    if(a=='\'')
-			a=*Source++;
-		    PUT(a);
-		    a=*Source++;
-		    if(!a)
-			ERROR(ERROR_BAD_TEMPLATE);
-		}while(a!=']');
-		PUT(P_CLASS);
-                break;
-            case '*':
-                if(DOSBase->dl_Flags&RNF_WILDSTAR)
-                {
-		    iswild=1;
-                    PUT(P_ANY);
-                }else
-                    PUT('*');
-                break;
-	    case '%':
-		continue;
-            case '\'':
-		switch(*Source)
-		{
-		    case '*':
-		    case '?':
-		    case '(':
-		    case '|':
-		    case ')':
-		    case '~':
-		    case '[':
-		    case ']':
-		    case '%':
-		    case '\'':
-			Source++;
-		    default:
-			break;
-		}
-                /* Fall through */
-            default:
-                a=Source[-1];
-                PUT(a);
-                break;
-        }
-	while(stack!=end&&*stack!=P_OREND)
-	    PUT(*stack++);
-    }
-    if(stack!=end)
-	ERROR(ERROR_BAD_TEMPLATE);
-    PUT(0);
-    return iswild;
     AROS_LIBFUNC_EXIT
 } /* ParsePattern */
