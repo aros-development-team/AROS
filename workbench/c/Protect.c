2649110887fe0e639939afd9637c7d827590f2d1
SDuvan <SDuvan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-12-04 19:07:35 +0000

    Reimplemented (mush simpler now) and added missing stuff, should be finished
    git-svn-id: https://svn.aros.org/svn/aros/trunk@9344 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/Protect.c
+++ workbench/c/Protect.c
@@ -1,9 +1,9 @@
 /*
-    (C) 1995-97 AROS - The Amiga Research OS
+    (C) 1995-2000 AROS - The Amiga Research OS
     $Id$
 
     Desc: Protect CLI command
-    Lang: english
+    Lang: English
 */
 
 /*****************************************************************************
@@ -29,9 +29,8 @@
 
     INPUTS
 
-        FILE    - Either a file, a directory or a pattern to match.
-
-        FLAGS   - One or more of the following flags:
+        FILE   --  Either a file, a directory or a pattern to match.
+        FLAGS  --  One or more of the following flags:
 
                     S - Script
                     P - Pure
@@ -41,23 +40,16 @@
                     E - Execute
                     D - Delete
 
-        ADD     - Allows the bits to be set and hence allowable.
-
-        SUB     - Allows the bits to be cleared and hence not allowable.
-
-        ALL     - Allows a recursive scan of the volume/directory.
-
-        QUIET   - Suppresses any output to the shell.
+        ADD    --  Allows the bits to be set and hence allowable.
+        SUB    --  Allows the bits to be cleared and hence not allowable.
+        ALL    --  Allows a recursive scan of the volume/directory.
+        QUIET  --  Suppresses any output to the shell.
 
     RESULT
 
         Standard DOS return codes.
 
     NOTES
-
-        Does not yet support multi-assigns.
-
-        Does not yet support User Groups.
 
     EXAMPLE
 
@@ -75,7 +67,9 @@
 
     HISTORY
 
-        27-Jul-1997     laguest     Initial inclusion into the AROS tree
+        27-Jul-1997  laguest  --  Initial inclusion into the AROS tree
+	3.12.2000    SDuvan   --  Rewrote, simplified and implemented missing
+	                          functionality
 
 ******************************************************************************/
 
@@ -91,18 +85,26 @@
 #include <exec/types.h>
 #include <utility/utility.h>
 
+#include <ctype.h>
+#include <stdio.h>
+
 #define Bit_Mask(bit)           (1L << bit)
 #define Bit_Clear(name, bit)    name &= ~Bit_Mask(bit)
 #define Bit_Set(name, bit)      name |= Bit_Mask(bit)
 
 #define ARG_TEMPLATE    "FILE/A,FLAGS,ADD/S,SUB/S,ALL/S,QUIET/S"
-#define ARG_FILE        0
-#define ARG_FLAGS       1
-#define ARG_ADD         2
-#define ARG_SUB         3
-#define ARG_ALL         4
-#define ARG_QUIET       5
-#define TOTAL_ARGS      6
+
+enum 
+{
+    ARG_FILE = 0,
+    ARG_FLAGS,
+    ARG_ADD,
+    ARG_SUB,
+    ARG_ALL,
+    ARG_QUIET,
+    NOOFARGS
+};
+
 
 /* To define whether a command line switch was set or not.
  */
@@ -111,74 +113,148 @@
 
 #define MAX_PATH_LEN    512
 
-static const char version[] = "$VER: Protect 41.0 (24.6.1997)\n";
+static const char version[] = "$VER: Protect 41.1 (2.12.2000)\n";
 
 struct UtilityBase *UtilityBase;
 
-int Do_Protect(struct AnchorPath *, STRPTR, STRPTR, LONG, LONG, LONG, LONG);
+int Do_Protect(struct AnchorPath *, STRPTR, STRPTR, BOOL, BOOL, BOOL, BOOL);
+
+int doProtect(struct AnchorPath *ap, STRPTR file, LONG flags, BOOL flagsSet,
+	      BOOL add, BOOL sub, BOOL all, BOOL quiet);
+BOOL setProtection(STRPTR file, LONG oldFlags, LONG flags, BOOL flagsSet, 
+		  BOOL add, BOOL sub);
 
 int main(int argc, char *argv[])
 {
-    struct RDArgs     * rda;
-    struct AnchorPath * apath;
-    IPTR              * args[TOTAL_ARGS] = { NULL, NULL, NULL, NULL, NULL, NULL };
-    int                 Return_Value;
+    struct RDArgs     *rda;
+    struct AnchorPath *apath;
 
-    Return_Value = RETURN_OK;
+    IPTR args[NOOFARGS] = { NULL,
+			    NULL,
+			    (IPTR)FALSE,
+			    (IPTR)FALSE,
+			    (IPTR)FALSE,
+			    (IPTR)FALSE };
 
+    int retval = RETURN_OK;
+    
     UtilityBase = (struct UtilityBase *)OpenLibrary("utility.library", 39);
+
     if (UtilityBase != NULL)
     {
         apath = AllocVec(sizeof(struct AnchorPath) + MAX_PATH_LEN,
                          MEMF_ANY | MEMF_CLEAR);
-        if (apath)
+
+        if (apath != NULL)
         {
-            /* Make sure DOS knows the buffer size.
-             */
+            /* Make sure DOS knows the buffer size. */
             apath->ap_Strlen = MAX_PATH_LEN;
 
-            /* The cast in ReadArgs() stops the compiler from complaining.
-             */
-            rda = ReadArgs(ARG_TEMPLATE, (LONG *)args, NULL);
-            if (rda) {
-                Return_Value = Do_Protect(apath,
-                                          (STRPTR)args[ARG_FILE],
-                                          (STRPTR)args[ARG_FLAGS],
-                                          (LONG)args[ARG_ADD],
-                                          (LONG)args[ARG_SUB],
-                                          (LONG)args[ARG_ALL],
-                                          (LONG)args[ARG_QUIET]
-                );
+            rda = ReadArgs(ARG_TEMPLATE, args, NULL);
+
+            if (rda != NULL)
+	    {
+		STRPTR  file = (STRPTR)args[ARG_FILE];
+		STRPTR  flags = (STRPTR)args[ARG_FLAGS];
+		BOOL    add = (BOOL)args[ARG_ADD];
+		BOOL    sub = (BOOL)args[ARG_SUB];
+		BOOL    all = (BOOL)args[ARG_ALL];
+		BOOL    quiet = (BOOL)args[ARG_QUIET];
+
+		LONG    flagValues = FIBF_READ | FIBF_WRITE | FIBF_DELETE |
+		                     FIBF_EXECUTE;
+
+		if (flags != NULL)
+		{
+		    while (*flags != 0)
+		    {
+			switch (toupper(*flags))
+			{
+			    /* Active low */
+			case 'R':
+			    flagValues &= ~FIBF_READ;
+			    break;
+			    
+			case 'W':
+			    flagValues &= ~FIBF_WRITE;
+			    break;
+
+			case 'D':
+			    flagValues &= ~FIBF_DELETE;
+			    break;
+
+			case 'E':
+			    flagValues &= ~FIBF_EXECUTE;
+			    break;
+
+			    /* Active high */
+			case 'A':
+			    flagValues |= FIBF_ARCHIVE;
+			    break;
+
+			case 'S':
+			    flagValues |= FIBF_SCRIPT;
+			    break;
+
+			case 'P':
+			    flagValues |= FIBF_PURE;
+			    break;
+
+			default:
+			    printf("Invalid flags - must be one of SPARWED\n");
+			    retval = RETURN_FAIL;
+			}
+
+			flags++;
+		    } /* while (*flags != 0) */
+		}
+
+		if (retval == RETURN_OK)
+		{
+		    if (!all &&IsDosEntryA(file, LDF_VOLUMES | LDF_DEVICES))
+		    {
+			printf("Can't set protection for %s - ", file);
+			SetIoErr(ERROR_OBJECT_WRONG_TYPE);
+			PrintFault(IoErr(), NULL);
+			
+			retval = RETURN_FAIL;
+		    }
+		    else
+		    {
+			retval = doProtect(apath, file, flagValues,
+					   flags != NULL, add, sub, all,
+					   quiet);
+		    }
+		}
+
 		FreeArgs(rda);
             }
             else
             {
                 PrintFault(IoErr(), "Protect");
-                Return_Value = RETURN_FAIL;
+                retval = RETURN_FAIL;
             }
         }
         else
         {
-            Return_Value = RETURN_FAIL;
+            retval = RETURN_FAIL;
         }
-
+	
         FreeVec(apath);
-
+	
         CloseLibrary((struct Library *)UtilityBase);
     }
     else
     {
         VPrintf("Need \'utility.library\' version 39 or above\n", NULL);
-        Return_Value = RETURN_FAIL;
+        retval = RETURN_FAIL;
     }
+    
+    return retval;
+}
 
-    return (Return_Value);
 
-} /* main */
-
-
-/* Defines whether MatchFirst(), etc has matched a file.
- */
+/* Defines whether MatchFirst(), etc has matched a file. */
 #define MATCHED_FILE    0
 
 int Do_Recursion(struct AnchorPath *, LONG, LONG, LONG *, LONG);
@@ -188,108 +264,204 @@ void NewBitMask(struct AnchorPath *, STRPTR, LONG *);
 void PrintFileName(struct AnchorPath *, LONG);
 int SafeSetProtection(struct AnchorPath *, LONG);
 
-int Do_Protect(struct AnchorPath *a,
-               STRPTR File,
-               STRPTR Flags,
-               LONG Add,
-               LONG Sub,
-               LONG All,
-               LONG Quiet)
+
+#define  isDir(fib) ((fib)->fib_DirEntryType >= 0)
+
+int doProtect(struct AnchorPath *ap, STRPTR file, LONG flags, BOOL flagsSet,
+	      BOOL add, BOOL sub, BOOL all, BOOL quiet)
+{
+    LONG  match;
+    int   retval = RETURN_OK;
+    LONG  indent = 0;
+    int   i;			/* Loop variable */
+    BOOL  error;
+
+    for (match = MatchFirst(file, ap); match == 0 && retval == RETURN_OK;
+	 match = MatchNext(ap))
+    {
+	if (isDir(&ap->ap_Info))
+	{
+	    if (all)
+	    {
+		ap->ap_Flags |= APF_DODIR;
+		indent++;
+	    }
+
+	    if (ap->ap_Flags & APF_DIDDIR)
+	    {
+		indent--;
+		ap->ap_Flags &= ~APF_DIDDIR; /* Should not be necessary */
+		continue;
+	    }
+	}
+
+	error = setProtection(ap->ap_Buf, ap->ap_Info.fib_Protection, flagsSet,
+			      flags, add, sub);
+
+	/* Fix indentation level */
+	for (i = 0; i < indent; i++)
+	{
+	    printf("  ");
+	}
+
+	if (!quiet || error)
+	{
+	    printf(FilePart(ap->ap_Buf));
+
+	    if (isDir(&ap->ap_Info))
+	    {
+		printf(" (dir)");
+	    }
+
+	    printf("...Done\n");
+	}
+    }
+
+    MatchEnd(ap);
+
+    return retval;
+}
+
+#define  ALL_OFF  (FIBF_READ | FIBF_WRITE | FIBF_DELETE | FIBF_EXECUTE)
+#define  addFlags(new, old)  (((old & ~new) & ALL_OFF) | \
+			      ((old | new) & ~ALL_OFF))
+     
+#define  subFlags(new, old)  (((old | new) & ALL_OFF) | \
+			      ((old & ~new) & ~ALL_OFF))
+     
+BOOL setProtection(STRPTR file, LONG oldFlags, LONG flags, BOOL flagsSet, 
+		   BOOL add, BOOL sub)
+{
+    LONG  newFlags;
+    
+    if (flagsSet)
+    {
+	if (add)
+	{
+	    /* Enable permission */
+	    newFlags = addFlags(flags, oldFlags);
+	}
+	else if (sub)
+	{
+	    /* Disable permissions */
+	    newFlags = subFlags(flags, oldFlags);
+	}
+	else
+	{
+	    /* Clear all permissions then set the ones given. */
+	    newFlags = flags;
+	}
+    }
+    else
+    {
+	/* No flags were given */
+	if (!add && !sub)
+	{
+	    /* Disable all permissions */
+	    newFlags = ALL_OFF;
+	}
+	else
+	{
+	    /* Do nothing */
+	    return FALSE;
+	}
+    }    
+
+    if (!SetProtection(file, newFlags))
+    {
+	return FALSE;
+    }
+    
+    return TRUE;
+}
+
+
+
+int Do_Protect(struct AnchorPath *a, STRPTR file, STRPTR flags,
+               BOOL add, BOOL sub, BOOL all, BOOL quiet)
 {
     LONG Result,
-         TabValue,
-         BitMask;
-    int  Return_Value;
+	TabValue,
+	BitMask;
+    int  retval = RETURN_OK;
     IPTR DevArg[2];
 
-    Return_Value = RETURN_OK;
     TabValue     = 0L;
     BitMask      = 0L;
 
-    if
-    (
-        IsDosEntryA((STRPTR)File, LDF_VOLUMES | LDF_DEVICES) == TRUE
-    &&
-        NOT_SET(All)
-    )
+    if(IsDosEntryA(file, LDF_VOLUMES | LDF_DEVICES) && !all)
     {
-        DevArg[0] = (IPTR)File;
-
+        DevArg[0] = (IPTR)file;
+	
         VPrintf("Can't set protection for %s - ", DevArg);
         SetIoErr(ERROR_OBJECT_WRONG_TYPE);
         PrintFault(IoErr(), NULL);
         
-        Return_Value = RETURN_FAIL;
+        retval = RETURN_FAIL;
     }
     else
     {
-        Result = MatchFirst((STRPTR)File, a);
-
+        Result = MatchFirst(file, a);
+	
         if (Result == MATCHED_FILE)
         {
             do
             {
-                if (Flags != NULL)  /* Flags are given */
+                if (flags != NULL)  /* Flags are given */
                 {
-                    if (IS_SET(Add))
+                    if (add)
                     {
                         /* Add the permission bits to the bitmask.
                          * In other words - Enable permissions.
                          */
     
-                        AddBitMask(a, Flags, &BitMask, TRUE);
-                        Return_Value = Do_Recursion(a, All, Quiet, &TabValue, BitMask);
+                        AddBitMask(a, flags, &BitMask, TRUE);
+                        retval = Do_Recursion(a, all, quiet, &TabValue, BitMask);
                     }
-                    else if (IS_SET(Sub))
+                    else if (sub)
                     {
                         /* Remove the permission bits from the bitmask.
-                         * In other words - Disable permissions.
-                         */
+                         * In other words - Disable permissions. */
 
-                        SubBitMask(a, Flags, &BitMask, TRUE);
-                        Return_Value = Do_Recursion(a, All, Quiet, &TabValue, BitMask);
+                        SubBitMask(a, flags, &BitMask, TRUE);
+                        retval = Do_Recursion(a, all, quiet, &TabValue, BitMask);
                     }
                     else
                     {
                         /* Clear all permissions then set the ones given.
                          */
-
-                        NewBitMask(a, Flags, &BitMask);
-                        Return_Value = Do_Recursion(a, All, Quiet, &TabValue, BitMask);
+			
+                        NewBitMask(a, flags, &BitMask);
+                        retval = Do_Recursion(a, all, quiet, &TabValue, BitMask);
                     }
                 }
                 else    /* No flags are given */
                 {
-                    if (NOT_SET(Add) && NOT_SET(Sub))
+                    if (!add && !sub)
                     {
                         /* Remove all permissions bits from the bitmask.
-                         * In other words - Disable ALL permissions.
-                         */
+                         * In other words - Disable ALL permissions. */
 
                         SubBitMask(a, "sparwed", &BitMask, TRUE);
-                        Return_Value = Do_Recursion(a, All, Quiet, &TabValue, BitMask);
+                        retval = Do_Recursion(a, all, quiet, &TabValue, BitMask);
                     }
                 }
             }
-            while
-            (
-                ((Result = MatchNext(a)) == MATCHED_FILE)
-            &&
-                Return_Value != RETURN_FAIL
-            );
+
+            while (((Result = MatchNext(a)) == MATCHED_FILE) &&
+                retval != RETURN_FAIL);
         }
         else
         {
             PrintFault(IoErr(), "Protect");
-            Return_Value = RETURN_FAIL;
+            retval = RETURN_FAIL;
         }
 
         MatchEnd(a);
     }
 
-    return (Return_Value);
-    
-} /* Do_Protect */
+    return retval;    
+}
 
 
 int Do_Recursion(struct AnchorPath *a,
