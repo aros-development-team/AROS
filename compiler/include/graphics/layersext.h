729768de39d2dc4d5837226856ad33e8a194fe0f
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-12-06 19:15:40 +0000

    some tags + defines for new layers.library
    git-svn-id: https://svn.aros.org/svn/aros/trunk@9348 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/graphics/layersext.h
@@ -0,0 +1,31 @@
+#ifndef GRAPHICS_LAYERSEXT_H
+#define GRAPHICS_LAYERSEXT_H
+
+/*
+    (C) 2000 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Layer extensions for the new AROS layers.library
+    Lang: english
+*/
+
+#ifndef UTILITY_TAGITEM_H
+#   include <utility/tagitem.h>
+#endif
+
+#define LA_Dummy	(TAG_USER + 1234)
+
+/* Tags for CreateLayerTagList */
+
+#define LA_Type		(LA_Dummy + 1) /* LAYERSIMPLE, LAYERSMART (default) -or LAYERSUPER */
+#define LA_Priority	(LA_Dummy + 2) /* -128 .. 127 or LPRI_NORMAL (default) or LPRI_BACKDROP */
+#define LA_Behind	(LA_Dummy + 3) /* BOOL. Default is FALSE */
+#define LA_Invisible	(LA_Dummy + 4) /* BOOL. Default is FALSE */
+#define LA_BackFill	(LA_Dummy + 5) /* struct Hook *. Default is LAYERS_BACKFILL */
+#define LA_SuperBitMap	(LA_Dummy + 6) /* struct BitMap *. Default is NULL (none) */
+#define LA_Shape	(LA_Dummy + 7) /* struct Region *. Default is NULL (rectangular shape) */
+
+#define LPRI_NORMAL 	0
+#define LPRI_BACKDROP	-50
+
+#endif /* GRAPHICS_LAYERSEXT_H */
