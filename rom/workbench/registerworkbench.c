e84168c4a266dca982e6e62a5ba482f7cf5a8a3c
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-12-14 21:36:01 +0000

    Two new public functions. The idea is that the Workbench Application (un)registers it's message port using these functions (which automatically call AlohaWorkbench()) where it will get notification messages from intuition.library (WBENCHOPEN and WBENCHCLOSE) and workbench.library (when App[Icons|MenuItems|Windows] are added/removed, when options are changed via WorkbenchControlA(), etc.).
    git-svn-id: https://svn.aros.org/svn/aros/trunk@9489 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/workbench/registerworkbench.c
@@ -0,0 +1,57 @@
+/*
+    (C) 1995-97 AROS - The Amiga Research OS
+    $Id$
+
+    Desc:
+    Lang: english
+*/
+
+#include "workbench_intern.h"
+#include <workbench/workbench.h>
+
+/*****************************************************************************
+
+    NAME */
+
+        #include <proto/workbench.h>
+
+        AROS_LH1(BOOL   , RegisterWorkbench,
+/*  SYNOPSIS */
+        AROS_LHA(struct MsgPort *, msgport, A0),
+/*  LOCATION */
+
+        struct WorkbenchBase *, WorkbenchBase, 23, Workbench)
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+    AROS_LIBBASE_EXT_DECL(struct WorkbenchBase *, WorkbenchBase)
+
+    if( (msgport != NULL) && (WorkbenchBase->wb_AppPort == NULL) ) {
+        AlohaWorkbench( msgport );
+        WorkbenchBase->wb_AppPort = msgport;
+
+        return TRUE;
+    }
+
+    return FALSE;
+
+    AROS_LIBFUNC_EXIT
+} /* RegisterWorkbench */
