de23560f53bd5095c4e54f58bb3a41953386e757
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-12-15 02:08:08 +0000

    Removed some stuff....
    git-svn-id: https://svn.aros.org/svn/aros/trunk@9501 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/intuition/intuition.h
+++ compiler/include/intuition/intuition.h
@@ -547,13 +547,8 @@ struct Window
 
     ULONG MoreFlags;
     
-    struct Window * firstchild;  // pointer to first child
-    struct Window * prevchild;   // if window is a child of a window
-    struct Window * nextchild;   // then they are concatenated here.
-    struct Window * parent;      // parent of this window
 };
 
-#define HAS_CHILDREN(w) (NULL != w->firstchild)
 
 struct NewWindow
 {
