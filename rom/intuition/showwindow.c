6c2d8ea4a9308301692113ff91ae426ae8163fdb
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-12-16 18:14:07 +0000

    Added public functions to show and hide a window and to test for its current visibility state. Untested.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@9514 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/intuition/showwindow.c
@@ -0,0 +1,66 @@
+/*
+    (C) 1995-99 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Intuition function HelpControl()
+    Lang: english
+*/
+#include "intuition_intern.h"
+#include <proto/layers.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/intuition.h>
+
+	AROS_LH1(VOID, ShowWindow,
+
+/*  SYNOPSIS */
+	AROS_LHA(struct Window *, window, A0),
+
+/*  LOCATION */
+	struct IntuitionBase *, IntuitionBase, 140, Intuition)
+
+/*  FUNCTION
+        Make a window visible. This function does not bring the
+        window back into the visible area of the screen but rather
+        switches it into visible state.
+        
+
+    INPUTS
+	window - The window to affect. 
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+    AROS_LIBBASE_EXT_DECL(struct IntuitionBase *,IntuitionBase)
+
+    struct IntuiActionMessage * msg;
+
+    if (FALSE == IsWindowVisible(window))
+    {
+    
+      msg = AllocIntuiActionMsg(AMCODE_SHOWWINDOW, window, IntuitionBase);
+
+      if (NULL != msg)
+      {
+        msg->iam_ShowWindow.yesno = TRUE;
+        SendIntuiActionMsg(msg, IntuitionBase);
+      }
+    }
+    AROS_LIBFUNC_EXIT
+} /* ShowWindow */
