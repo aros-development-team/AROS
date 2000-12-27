efab042453a862104b7959f4cda9317364a19ad4
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-12-27 16:08:43 +0000

    moved serial/parallel hidd stubs/includes to here, as they are machine independent.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@9637 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/hidds/parallel/include/parallel.h
@@ -0,0 +1,110 @@
+#ifndef HIDD_PARALLEL_H
+#define HIDD_PARALLEL_H
+
+/*
+    Copyright (C) 1998 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Definitions for the Parallel HIDD system.
+    Lang: english
+*/
+#ifndef EXEC_TYPES_H
+#   include <exec/types.h>
+#endif
+#ifndef HIDD_HIDD_H
+#   include <hidd/hidd.h>
+#endif
+#ifndef OOP_OOP_H
+#   include <oop/oop.h>
+#endif
+
+#include <utility/utility.h>
+
+
+#define CLID_Hidd_Parallel	"hidd.parallel.parallel"
+#define CLID_Hidd_ParallelUnit	"hidd.parallel.parallelunit"
+
+#define IID_Hidd_Parallel	"hidd.parallel.parallel"
+#define IID_Hidd_ParallelUnit	"hidd.parallel.parallelunit"
+
+
+
+/**** Graphics definitions ****************************************************/
+
+extern OOP_AttrBase HiddParallelUnitAB;
+
+enum {
+    aoHidd_ParallelUnit_Unit,
+    
+    num_Hidd_ParallelUnit_Attrs
+    
+};
+
+#define aHidd_ParallelUnit_Unit		(HiddParallelUnitAB + aoHidd_ParallelUnit_Unit)
+
+#define IS_HIDDPARALLELUNIT_ATTR(attr, idx) IS_IF_ATTR(attr, idx, HiddParallelUnitAB, num_Hidd_ParallelUnit_Attrs)
+
+enum
+{
+    moHidd_Parallel_NewUnit = 0,       
+    moHidd_Parallel_DisposeUnit,
+    moHidd_Parallel_NumMethods		// always keep this the last one!
+};
+
+
+
+
+struct pHidd_Parallel_NewUnit
+{
+    OOP_MethodID	mID;
+    ULONG		unitnum;
+};
+
+struct pHidd_Parallel_DisposeUnit
+{
+    OOP_MethodID    	mID;
+    OOP_Object         	*unit;
+};
+
+
+/**** Parallel Unit definitions ******************************************************/
+
+
+enum
+{
+    /* Methods for a parallel unit */
+
+    moHidd_ParallelUnit_Init,
+    moHidd_ParallelUnit_Write,
+    moHidd_ParallelUnit_NumMethods	// always keep this the last one!
+};
+
+
+/* messages for a parallel unit */
+
+struct pHidd_ParallelUnit_Init
+{
+    OOP_MethodID	mID;
+    VOID		*DataReceived;
+    VOID		*DataReceivedUserData;
+    VOID		*WriteData;
+    VOID		*WriteDataUserData;
+};
+
+struct pHidd_ParallelUnit_Write
+{
+    OOP_MethodID	mID;
+    ULONG		Length;
+    UBYTE		*Outbuffer;
+};
+
+/* Predeclarations of stubs in libhiddparallelstubs.h */
+
+OOP_Object * HIDD_Parallel_NewUnit	(OOP_Object *obj, ULONG unitnum);
+VOID     HIDD_Parallel_DisposeUnit	(OOP_Object *obj, OOP_Object *unit);
+
+BOOL     HIDD_ParallelUnit_Init		(OOP_Object *obj, VOID * DataReceived, VOID * DataReceivedUserData, VOID * WriteData, VOID * WriteDataUserData);
+ULONG    HIDD_ParallelUnit_Write	(OOP_Object *obj, UBYTE * data, ULONG length);
+
+
+#endif /* HIDD_PARALLEL_H */
