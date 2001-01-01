7dff3fb656d514d9c803be0a422ba705a3bd5abe
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-01-01 04:11:57 +0000

    Program to test the console's RAW mode.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@9667 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/rawtest.c
@@ -0,0 +1,64 @@
+#include <exec/types.h>
+#include <proto/dos.h>
+#include <stdlib.h>
+
+#define MODE_CON 0
+#define MODE_RAW 1
+
+void SetConsoleMode( LONG mode );
+TEXT GetChar( void );
+void PutChar( TEXT buffer );
+void PutString( STRPTR buffer );
+void Wait4Char( void );
+
+int main( void )
+{
+    TEXT ch;
+
+    SetConsoleMode( MODE_RAW );
+
+    while( TRUE )
+    {
+        Wait4Char();
+        ch = GetChar();
+        PutString( " >" );
+        PutChar( ch );
+        PutString( "< " );
+
+        if( ch == 'x' )
+            break;
+    }
+
+    SetConsoleMode( MODE_CON );
+
+    return 0;
+}
+
+void SetConsoleMode( LONG mode )
+{
+    SetMode( Input() , mode );
+}
+
+TEXT GetChar( void )
+{
+    TEXT buffer;
+
+    Read( Output() , &buffer , 1 );
+
+    return buffer;
+}
+
+void PutChar( TEXT buffer )
+{
+    Write( Output() , &buffer , 1 );
+}
+
+void PutString( STRPTR buffer )
+{
+    Write( Output() , buffer , strlen( buffer ) );
+}
+
+void Wait4Char( void )
+{
+    WaitForChar( Input() , 0 );
+}
