a09843f6cb95130839e411806bb9c8eae0a07c56
SDuvan <SDuvan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-01-06 15:24:00 +0000

    Cleaned up, use SetioErr() instead of custom procedure
    git-svn-id: https://svn.aros.org/svn/aros/trunk@9710 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/fputc.c
+++ rom/dos/fputc.c
@@ -2,6 +2,9 @@
     (C) 1995-96 AROS - The Amiga Research OS
     $Id$
     $Log$
+    Revision 1.9  2001/01/06 15:24:00  SDuvan
+    Cleaned up, use SetioErr() instead of custom procedure
+
     Revision 1.8  1998/10/20 16:44:37  hkiel
     Amiga Research OS
 
@@ -27,7 +30,7 @@
     Added standard header for all files
 
     Desc:
-    Lang: english
+    Lang: English
 */
 #include <exec/memory.h>
 #include <proto/exec.h>
@@ -78,73 +81,82 @@
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct DosLibrary *,DOSBase)
 
-    /* Get pointer to result. */
-    LONG *result=&((struct Process *)FindTask(NULL))->pr_Result2;
-
     /* Get pointer to filehandle */
-    struct FileHandle *fh=(struct FileHandle *)BADDR(file);
+    struct FileHandle *fh = (struct FileHandle *)BADDR(file);
 
     /* Check if file is in write mode */
-    if(!(fh->fh_Flags&FHF_WRITE))
+    if (!(fh->fh_Flags & FHF_WRITE))
     {
-	if(fh->fh_Pos<fh->fh_End)
+	if (fh->fh_Pos < fh->fh_End)
+	{
 	    /* Read mode. Try to seek back to the current position. */
-	    if(Seek(file,fh->fh_Pos-fh->fh_End,OFFSET_CURRENT)<0)
+	    if (Seek(file, fh->fh_Pos - fh->fh_End, OFFSET_CURRENT) < 0)
 	    {
-		fh->fh_Pos=fh->fh_End=fh->fh_Buf;
+		fh->fh_Pos = fh->fh_End = fh->fh_Buf;
+
 		return EOF;
 	    }
-
+	}
+	
 	/* Is there a buffer? */
-	if(fh->fh_Buf==NULL)
+	if (fh->fh_Buf == NULL)
 	{
 	    /* No. Get one. */
-	    fh->fh_Buf=AllocMem(IOBUFSIZE,MEMF_ANY);
-	    if(fh->fh_Buf==NULL)
+	    fh->fh_Buf = AllocMem(IOBUFSIZE, MEMF_ANY);
+
+	    if (fh->fh_Buf == NULL)
 	    {
 		/* Couldn't get buffer. Return error. */
-		*result=ERROR_NO_FREE_STORE;
+		SetIoErr(ERROR_NO_FREE_STORE);
+
 		return EOF;
 	    }
+	    
 	    /* Got it. Use it. */
-	    fh->fh_Flags|=FHF_BUF;
-	    fh->fh_Size=IOBUFSIZE;
+	    fh->fh_Flags |= FHF_BUF;
+	    fh->fh_Size = IOBUFSIZE;
 	}
-
+	
 	/* Prepare buffer */
-	fh->fh_Pos=fh->fh_Buf;
-	fh->fh_End=fh->fh_Buf+fh->fh_Size;
-	fh->fh_Flags|=FHF_WRITE;
+	fh->fh_Pos = fh->fh_Buf;
+	fh->fh_End = fh->fh_Buf + fh->fh_Size;
+	fh->fh_Flags |= FHF_WRITE;
     }
-
+    
     /* Check if there is still some space in the buffer */
-    if(fh->fh_Pos>=fh->fh_End)
+    if (fh->fh_Pos >= fh->fh_End)
     {
 	UBYTE *pos;
-
+	
 	/* Write the data. (In many pieces if the first one isn't enough). */
-	pos=fh->fh_Buf;
-	while(pos!=fh->fh_Pos)
+	pos = fh->fh_Buf;
+
+	while (pos != fh->fh_Pos)
 	{
 	    LONG size;
-	    size=Write(file,pos,fh->fh_Pos-pos);
+
+	    size = Write(file, pos, fh->fh_Pos - pos);
 
 	    /* An error happened? No success. */
-	    if(size<0)
+	    if (size < 0)
 	    {
-		fh->fh_Pos=fh->fh_End=fh->fh_Buf;
-		fh->fh_Flags&=~FHF_WRITE;
+		fh->fh_Pos = fh->fh_End = fh->fh_Buf;
+		fh->fh_Flags &= ~FHF_WRITE;
+
 		return EOF;
 	    }
-	    pos+=size;
+
+	    pos += size;
 	}
 
 	/* Reset buffer */
-	fh->fh_Pos=fh->fh_Buf;
+	fh->fh_Pos = fh->fh_Buf;
     }
 
     /* Write data and return */
-    *fh->fh_Pos++=character;
+    *fh->fh_Pos++ = character;
+
     return character;
+
     AROS_LIBFUNC_EXIT
 } /* FPutC */
