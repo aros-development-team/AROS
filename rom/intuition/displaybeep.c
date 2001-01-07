8cdd51e1bb3410f7a3e2f4c09578c64ecb1d7fc9
lbischoff <lbischoff@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-01-07 17:43:51 +0000

    Added support for hi- and truecolor screens via a borderless window. Not perfect but better than nothing. Would be nice if this window was tranparent. :-)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@9734 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/intuition/displaybeep.c
+++ rom/intuition/displaybeep.c
@@ -50,40 +50,73 @@
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct IntuitionBase *,IntuitionBase)
 
-    ULONG color[3];
-    UWORD BGPen;
-    struct DrawInfo *DInfo = NULL;
     struct MsgPort *TimerMsgPort = NULL;
     struct timerequest *MyTimerIO = NULL;
-   
+    BOOL VisualBeep = TRUE;
+    
     TimerMsgPort = CreateMsgPort();
     MyTimerIO = (struct timerequest *) CreateIORequest (TimerMsgPort, sizeof (struct timerequest));
     OpenDevice ("timer.device", UNIT_VBLANK, (struct IORequest *) MyTimerIO, 0);
     
     MyTimerIO->tr_time.tv_secs = 0;
-    MyTimerIO->tr_time.tv_micro = 200000;
+    MyTimerIO->tr_time.tv_micro = 250000;
     MyTimerIO->tr_node.io_Command = TR_ADDREQUEST;
   
-    if (! screen)
-        screen = IntuitionBase->FirstScreen;
+    if (VisualBeep)
+    {
+	if (! screen)
+            screen = IntuitionBase->FirstScreen;
 
-    DInfo = GetScreenDrawInfo (screen);
-    BGPen = DInfo->dri_Pens[BACKGROUNDPEN];
-	
-    GetRGB32 (screen->ViewPort.ColorMap, BGPen, 1, color);
+	if (screen->RastPort.BitMap->Depth <= 8)
+	// visual beep on CLUT-screen
+	{
+            struct DrawInfo *DInfo = NULL;
+            UWORD BGPen;
+            ULONG color[3];
 
-    SetRGB32 (&screen->ViewPort, BGPen, color[0] - 0x7FFFFFFF, color[1] - 0x7FFFFFFF, color[2] - 0x7FFFFFFF);
-    DoIO ((struct IORequest *) MyTimerIO);
-    SetRGB32 (&screen->ViewPort, BGPen, color[0], color[1], color[2]);
+	    DInfo = GetScreenDrawInfo (screen);
+	    BGPen = DInfo->dri_Pens[BACKGROUNDPEN];
 
-    FreeScreenDrawInfo (screen, DInfo);
+	    GetRGB32 (screen->ViewPort.ColorMap, BGPen, 1, color);
 
+            screen->Flags |= BEEPING;
+	    SetRGB32 (&screen->ViewPort, BGPen, color[0] - 0x7FFFFFFF, color[1] - 0x7FFFFFFF, color[2] - 0x7FFFFFFF);
+	    DoIO ((struct IORequest *) MyTimerIO);
+	    SetRGB32 (&screen->ViewPort, BGPen, color[0], color[1], color[2]);
+	    screen->Flags &= !BEEPING;
+
+	    FreeScreenDrawInfo (screen, DInfo);
+	}
+	else
+	// visual beep on hi- and truecolor screens
+	{
+            struct Window *BeepWindow;
+            struct TagItem window_tags[] = {
+                {WA_Left, 0},
+                {WA_Top, 0},
+                {WA_Width, -1},
+                {WA_Height, -1},
+                {WA_Flags, WFLG_SIMPLE_REFRESH | WFLG_BORDERLESS},
+                {WA_CustomScreen, (IPTR) screen},
+	        {WA_Priority, 50},  // Place in front of all other windows! 
+                {TAG_DONE, 0}
+            };
+            BeepWindow = (struct Window *) OpenWindowTagList (NULL, window_tags);
+            if (BeepWindow)
+	    {
+                DoIO ((struct IORequest *) MyTimerIO);
+                CloseWindow (BeepWindow);
+	    }
+	}   
+    }
+    
     CloseDevice ((struct IORequest *) MyTimerIO);
     DeleteIORequest ((struct IORequest *) MyTimerIO);
     DeleteMsgPort (TimerMsgPort);
 
-    
-#warning TODO: Add support for non-CLUT screens
+#warning TODO: Produce beep according to prefs
+#warning TODO: Handle BEEPING flag correctly
+#warning TODO: Make BeepWindow 50% transparent! :-) 
 #warning TODO: Use TimerIO (IntuitionBase->TimerIO) instead of self-made MyTimerIO!
 
     AROS_LIBFUNC_EXIT
