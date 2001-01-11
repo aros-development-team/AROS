8b22e8ea8d62d468692670070b2c5f1ac0732b85
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-01-11 18:13:40 +0000

    update
    git-svn-id: https://svn.aros.org/svn/aros/trunk@9762 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/asl/fontreqsupport.h
+++ workbench/libs/asl/fontreqsupport.h
@@ -23,9 +23,11 @@
 LONG FOGetFonts(struct LayoutData *ld, struct AslBase_intern *AslBase);
 void FOFreeFonts(struct LayoutData *ld, struct AslBase_intern *AslBase);
 struct ASLLVFontReqNode *FOGetActiveFont(struct LayoutData *ld, struct AslBase_intern *AslBase);
-void FOChangeActiveFont(struct LayoutData *ld, WORD delta, UWORD quali, struct AslBase_intern *AslBase);
+void FOChangeActiveFont(struct LayoutData *ld, WORD delta, UWORD quali, BOOL jump, struct AslBase_intern *AslBase);
+void FOChangeActiveSize(struct LayoutData *ld, WORD delta, UWORD quali, struct AslBase_intern *AslBase);
 void FOActivateFont(struct LayoutData *ld, WORD which, LONG size, struct AslBase_intern *AslBase);
 void FOActivateSize(struct LayoutData *ld, WORD which, struct AslBase_intern *AslBase);
+void FORestore(struct LayoutData *ld, STRPTR fontname, LONG fontsize, struct AslBase_intern *AslBase);
 void FOSetFontString(STRPTR name, struct LayoutData *ld, struct AslBase_intern *AslBase);
 void FOSetSizeString(LONG size, struct LayoutData *ld, struct AslBase_intern *AslBase);
 
