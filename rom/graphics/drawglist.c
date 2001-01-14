537df709647b1b40d70696fcae3a6be6f119517d
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-01-14 19:06:25 +0000

    some fixes
    git-svn-id: https://svn.aros.org/svn/aros/trunk@9806 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/graphics/drawglist.c
+++ rom/graphics/drawglist.c
@@ -1,12 +1,14 @@
 
 /*
-    (C) 1995-99 AROS - The Amiga Research OS
+    (C) 1995-2001 AROS - The Amiga Research OS
     $Id$
 
     Desc: Draw the list of gels
     Lang: english
 */
 #include "graphics_intern.h"
+
+void _ClearBobAndFollowClearPath(struct VSprite *, struct RastPort *);
 
 /*****************************************************************************
 
@@ -47,12 +49,13 @@
 
     struct VSprite * CurVSprite = rp->GelsInfo->gelHead;
     struct VSprite * AfterPathVSprite = NULL;
-    struct BitMap bm;
-    ULONG i;
-    int followdrawpath;
+    struct BitMap    bm;
+    ULONG   	     i;
+    int     	     followdrawpath;
 
 #ifndef BLTBITMAPRASTPORT_EXISTS
-    struct RastPort rp_bm;
+    struct RastPort  rp_bm;
+    
     InitRastPort(&rp_bm);
     rp_bm.BitMap = &bm;
 #endif
@@ -63,14 +66,14 @@
      */ 
     if (NULL != CurVSprite->DrawPath)
     {
-      followdrawpath = TRUE;
-      AfterPathVSprite = CurVSprite->NextVSprite;
-      CurVSprite = CurVSprite->DrawPath;
+	followdrawpath = TRUE;
+	AfterPathVSprite = CurVSprite->NextVSprite;
+	CurVSprite = CurVSprite->DrawPath;
     }
     else
     {
-      followdrawpath = FALSE;
-      CurVSprite = CurVSprite->NextVSprite;
+	followdrawpath = FALSE;
+	CurVSprite = CurVSprite->NextVSprite;
     }
     
     /*
@@ -78,159 +81,162 @@
      */
     while (NULL != CurVSprite->NextVSprite)
     {
-      /*
-       * Check a few flags that must not be set.
-       * The Bob should not be out of the rasrport or already be drawn
-       */
-      if ( 0 == (CurVSprite->Flags & (GELGONE)) ||
-          (NULL != (CurVSprite->VSBob) && 0 == (CurVSprite->VSBob->Flags & BDRAWN)))	
-      {
-	UBYTE * imagedata = (UBYTE *)CurVSprite->ImageData;
 	/*
-	 * If this VSprite was overlapping with other VSprites
-	 * the follow the ClearPath first and clear all the
-	 * VSprites on that path. That routine will also reset
-	 * the ClearPath variable on all encountered VSprites.
-	 * If it was not overlapping with other VSprites but
-	 * was visible before it will either clear or back up
-	 * the previous background.
+	 * Check a few flags that must not be set.
+	 * The Bob should not be out of the rasrport or already be drawn
 	 */
-void _ClearBobAndFollowClearPath(struct VSprite *, struct RastPort *);
+	if ( 0 == (CurVSprite->Flags & (GELGONE)) ||
+            (NULL != (CurVSprite->VSBob) && 0 == (CurVSprite->VSBob->Flags & BDRAWN)))	
+	{
+	    UBYTE * imagedata = (UBYTE *)CurVSprite->ImageData;
+	    /*
+	     * If this VSprite was overlapping with other VSprites
+	     * the follow the ClearPath first and clear all the
+	     * VSprites on that path. That routine will also reset
+	     * the ClearPath variable on all encountered VSprites.
+	     * If it was not overlapping with other VSprites but
+	     * was visible before it will either clear or back up
+	     * the previous background.
+	     */
 
-	if (NULL != CurVSprite->VSBob)
-	  _ClearBobAndFollowClearPath(CurVSprite,rp);
+	    if (NULL != CurVSprite->VSBob)
+	      _ClearBobAndFollowClearPath(CurVSprite,rp);
 
 
-	InitBitMap(&bm, 
-	           CurVSprite->Depth,
-		   CurVSprite->Width * 16,
-		   CurVSprite->Height);
+	    InitBitMap(&bm, 
+	               CurVSprite->Depth,
+		       CurVSprite->Width * 16,
+		       CurVSprite->Height);
 
-        /*
-         * If I am supposed to back up the background then
-         * I have to do it now!!
-         */
-        if (0 != (CurVSprite->Flags & SAVEBACK) &&
-            NULL != CurVSprite->VSBob)
-        {  
-          UBYTE * savedata = (UBYTE *)CurVSprite->VSBob->SaveBuffer;
-	  i=0;
-	  while (i < bm.Depth && i < 8)
-	  {
-	    bm.Planes[i++] = savedata;
-	    savedata += bm.Rows * bm.BytesPerRow;
-	  }
+            /*
+             * If I am supposed to back up the background then
+             * I have to do it now!!
+             */
+            if (0 != (CurVSprite->Flags & SAVEBACK) &&
+        	NULL != CurVSprite->VSBob)
+            {  
+        	UBYTE * savedata = (UBYTE *)CurVSprite->VSBob->SaveBuffer;
+		
+		i = 0;
+		while (i < bm.Depth && i < 8)
+		{
+		    bm.Planes[i++] = savedata;
+		    savedata += bm.Rows * bm.BytesPerRow;
+		}
 
 #ifdef BLTRASTPORTBITMAP_EXISTS
-          BltRastPortBitMap(rp,
-                            CurVSprite->X,
-                            CurVSprite->Y,
-                            &bm,
-                            0,
-                            0,
-                            CurVSprite->Width,
-                            CurVSprite->Height,
-                            0x0c0);
+        	BltRastPortBitMap(rp,
+                        	  CurVSprite->X,
+                        	  CurVSprite->Y,
+                        	  &bm,
+                        	  0,
+                        	  0,
+                        	  CurVSprite->Width,
+                        	  CurVSprite->Height,
+                        	  0x0c0);
 #else
 #warning Since BltRastPortBitMap (or something similar) does not exist I need to use ClipBlit here which is pretty ugly.
-          ClipBlit(rp,
-                   CurVSprite->X,
-                   CurVSprite->Y,
-                   &rp_bm,
-                   0,
-                   0,
-                   CurVSprite->Width << 4,
-                   CurVSprite->Height,
-                   0x0c0);
+        	ClipBlit(rp,
+                	 CurVSprite->X,
+                	 CurVSprite->Y,
+                	 &rp_bm,
+                	 0,
+                	 0,
+                	 CurVSprite->Width << 4,
+                	 CurVSprite->Height,
+                	 0x0c0);
 #endif
-#warning Cannot set this Flag since ClipBlit seems to copy some garbage.
-//          CurVSprite->Flags |= BACKSAVED;
-        }
+         	CurVSprite->Flags |= BACKSAVED;
+            }
 
 
-        if (0 == (CurVSprite->Flags & VSPRITE) &&
-            BOBSAWAY == (CurVSprite->VSBob->Flags & BOBSAWAY))
-        {
-          /*
-           * This Bob is to be removed...
-           */
-          CurVSprite->PrevVSprite->NextVSprite = CurVSprite->NextVSprite;
-          CurVSprite->NextVSprite->PrevVSprite = CurVSprite->PrevVSprite;
-          /*
-           * This does not damage the drawpath and clearpath since
-           * the structure is not freed.
-           */
-        }
-        else
-        {
-  	  /*
-	   * Now draw the VSprite/Bob at its current location.
-	   * The bitmap has already been initialized!
-	   */
-	  i = 0;
-	  while (i < bm.Depth && i < 8)
-	  {
-	    bm.Planes[i++] = imagedata;
-	    imagedata += bm.Rows * bm.BytesPerRow;
-	  }
+            if (0 == (CurVSprite->Flags & VSPRITE) &&
+        	BOBSAWAY == (CurVSprite->VSBob->Flags & BOBSAWAY))
+            {
+        	/*
+        	 * This Bob is to be removed...
+        	 */
+        	CurVSprite->PrevVSprite->NextVSprite = CurVSprite->NextVSprite;
+        	CurVSprite->NextVSprite->PrevVSprite = CurVSprite->PrevVSprite;
+        	/*
+        	 * This does not damage the drawpath and clearpath since
+        	 * the structure is not freed.
+        	 */
+            }
+            else
+            {
+  		/*
+		 * Now draw the VSprite/Bob at its current location.
+		 * The bitmap has already been initialized!
+		 */
+		i = 0;
+		while (i < bm.Depth && i < 8)
+		{
+		    bm.Planes[i++] = imagedata;
+		    imagedata += bm.Rows * bm.BytesPerRow;
+		}
 
-	  BltBitMapRastPort(&bm,
-                  	    0,
-                	    0,
-                	    rp,
-                	    CurVSprite->X,
-                	    CurVSprite->Y,
-                	    CurVSprite->Width *16,
-                	    CurVSprite->Height,
-                	    0x0c0);
+		BltBitMapRastPort(&bm,
+                  		  0,
+                		  0,
+                		  rp,
+                		  CurVSprite->X,
+                		  CurVSprite->Y,
+                		  CurVSprite->Width *16,
+                		  CurVSprite->Height,
+                		  0x0c0);
 
-	  /*
-	   * I will need to know the vsprite's coordinates
-	   * that it has now the next time as well for clearing
-	   * purposes.
-	   */
-          CurVSprite->OldX = CurVSprite->X;
-          CurVSprite->OldY = CurVSprite->Y;
-        
-          if (CurVSprite->VSBob)
-          {
+		/*
+		 * I will need to know the vsprite's coordinates
+		 * that it has now the next time as well for clearing
+		 * purposes.
+		 */
+        	CurVSprite->OldX = CurVSprite->X;
+        	CurVSprite->OldY = CurVSprite->Y;
+
+        	if (CurVSprite->VSBob)
+        	{
+        	    /*
+        	     * it's a bob! mark it as drawn.
+        	     */
+        	    CurVSprite->VSBob->Flags |= BDRAWN;
+        	}
+            }
+	}
+	
+	/*
+	 * Am I supposed to follow the drawpath.
+	 * If yes then follow it to its end.
+	 */
+	 
+	if (TRUE == followdrawpath)
+	{
+            CurVSprite = CurVSprite->DrawPath;
+            if (NULL == CurVSprite)
+            {
+        	followdrawpath = FALSE;
+        	CurVSprite = AfterPathVSprite;
+            }
+	}
+	else
+	{
+            CurVSprite = CurVSprite->NextVSprite;
             /*
-             * it's a bob! mark it as drawn.
+             * Does a DrawPath start here?
+             * If yes, then I will follow the DrawPath 
+             * after I am done with this VSprite.
              */
-            CurVSprite->VSBob->Flags |= BDRAWN;
-          }
-        }
-      }	
-      /*
-       * Am I supposed to follow the drawpath.
-       * If yes then follow it to its end.
-       */
-      if (TRUE == followdrawpath)
-      {
-         CurVSprite = CurVSprite->DrawPath;
-         if (NULL == CurVSprite)
-         {
-           followdrawpath = FALSE;
-           CurVSprite = AfterPathVSprite;
-         }
-      }
-      else
-      {
-        CurVSprite = CurVSprite->NextVSprite;
-        /*
-         * Does a DrawPath start here?
-         * If yes, then I will follow the DrawPath 
-         * after I am done with this VSprite.
-         */
-        if (NULL != CurVSprite->DrawPath)
-        {
-          followdrawpath = TRUE;
-          AfterPathVSprite = CurVSprite->NextVSprite;
-        }
-      }
+            if (NULL != CurVSprite->DrawPath)
+            {
+        	followdrawpath = TRUE;
+        	AfterPathVSprite = CurVSprite->NextVSprite;
+            }
+	}
+	
     } /* while not all bobs/vsprites are drawn */
 
     AROS_LIBFUNC_EXIT
+    
 } /* DrawGList */
 
 
@@ -243,69 +249,61 @@ void _ClearBobAndFollowClearPath(struct VSprite *, struct RastPort *);
  * If a recursion is not what we want this can be easily
  * changed.
  */
+ 
 void _ClearBobAndFollowClearPath(struct VSprite * CurVSprite, 
                                  struct RastPort * rp)
 {
-  if (NULL != CurVSprite->ClearPath)
-  {
+    if (NULL != CurVSprite->ClearPath)
+    {
+	/*
+	 * Clear the next one first. (recursion!!!)
+	 */
+	_ClearBobAndFollowClearPath(CurVSprite->ClearPath, rp);
+	CurVSprite->ClearPath = NULL;
+    }
+
     /*
-     * Clear the next one first. (recursion!!!)
+     * Only restore the background if the bob image
+     * that is currently there is to be replaced by
+     * the background. If SAVEBOB is set the user
+     * might want some kind of a brush effect.
      */
-    _ClearBobAndFollowClearPath(CurVSprite->ClearPath, rp);
-    CurVSprite->ClearPath = NULL;
-  }
-
-  /*
-   * Only restore the background if the bob image
-   * that is currently there is to be replaced by
-   * the background. If SAVEBOB is set the user
-   * might want some kind of a brush effect.
-   */
-  if (0 == (CurVSprite->Flags & SAVEBOB))
-  {
-    if (0 != (CurVSprite->Flags & BACKSAVED))
+    if (0 == (CurVSprite->Flags & SAVEBOB))
     {
-      ULONG i;
-      struct BitMap bm;
-      UBYTE * imagedata = (UBYTE *)CurVSprite->VSBob->SaveBuffer;
+	if (0 != (CurVSprite->Flags & BACKSAVED))
+	{
+	    struct BitMap     bm;
+	    UBYTE   	    * imagedata = (UBYTE *)CurVSprite->VSBob->SaveBuffer;
+	    ULONG   	      i;
+
+	    /*
+	     * Restore background!
+	     */	 
+	    InitBitMap(&bm, 
+                       CurVSprite->Depth,
+                       CurVSprite->Width * 16,
+                      CurVSprite->Height);
+	    i = 0;
+	    while (i < bm.Depth && i < 8)
+	    {
+              bm.Planes[i++] = imagedata;
+              imagedata += bm.Rows * bm.BytesPerRow;
+	    }
+
+	    BltBitMapRastPort(&bm,
+                              0,
+                              0,
+                              rp,
+                              CurVSprite->OldX,
+                              CurVSprite->OldY,
+                              CurVSprite->Width << 4,
+                              CurVSprite->Height,
+                              0x0c0);
+
+	    CurVSprite->Flags &= ~BACKSAVED;
+	    
+	} /* if (0 != (CurVSprite->Flags & BACKSAVED)) */
+	
+    } /* if (0 == (CurVSprite->Flags & SAVEBOB)) */
     
-      /*
-       * Restore background!
-       */	 
-      InitBitMap(&bm, 
-                 CurVSprite->Depth,
-                 CurVSprite->Width * 16,
-                CurVSprite->Height);
-      i=0;
-      while (i < bm.Depth && i < 8)
-      {
-        bm.Planes[i++] = imagedata;
-        imagedata += bm.Rows * bm.BytesPerRow;
-      }
-
-      BltBitMapRastPort(&bm,
-                        0,
-                        0,
-                        rp,
-                        CurVSprite->OldX,
-                        CurVSprite->OldY,
-                        CurVSprite->Width << 4,
-                        CurVSprite->Height,
-                        0x0c0);
-
-      CurVSprite->Flags &= ~BACKSAVED;
-    }
-    else
-    {
-      /*
-       * No background was saved. So let's restore the
-       * standard background!
-       */
-      EraseRect(rp,
-                CurVSprite->OldX,
-                CurVSprite->OldY,
-                CurVSprite->OldX + ( CurVSprite->Width << 4 ) - 1,
-                CurVSprite->OldY +   CurVSprite->Height       - 1);
-    }
-  }
 }
