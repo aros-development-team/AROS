a51133893ced966eda3cbb75ec2d23b1f27a0c11
SDuvan <SDuvan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-01-16 19:29:13 +0000

    New documented version of Run that uses SystemTagList()
    git-svn-id: https://svn.aros.org/svn/aros/trunk@9814 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/run.c
+++ workbench/c/run.c
@@ -1,10 +1,58 @@
 /*
-    (C) 1995-96 AROS - The Amiga Research OS
+    (C) 1995-2001 AROS - The Amiga Research OS
     $Id$
 
     Desc:
-    Lang:
+    Lang: English
 */
+
+/******************************************************************************
+
+    NAME
+
+        Run
+
+    SYNOPSIS
+
+        COMMAND/F
+
+    LOCATION
+
+        Workbench:C
+
+    FUNCTION
+
+        Run a program, that is start a program as a background process.
+        That means it doesn't take over the parent shell.
+
+    INPUTS
+
+        COMMAND  --  the program to run together with its arguments
+
+    RESULT
+
+    NOTES
+
+        To make it possible to close the current shell, redirect the output
+        using
+ 
+             Run >NIL: program arguments
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+
+#define  DEBUG 0
+#include <aros/debug.h>
+
 #include <exec/memory.h>
 #include <proto/exec.h>
 #include <dos/dosextens.h>
@@ -12,77 +60,58 @@
 #include <proto/dos.h>
 #include <utility/tagitem.h>
 
-static const char version[] = "$VER: run 41.1 (14.3.1997)\n";
+static const char version[] = "$VER: run 41.2 (5.1.2001)\n";
 
-int main (int argc, char ** argv)
+#define  ARG_TEMPLATE  "COMMAND/F"
+
+enum
 {
-    STRPTR args[1]={ 0 };
-    struct RDArgs *rda;
-    BPTR in, out, shell;
-    STRPTR s1, s2, buf;
-    struct Process *process;
-    ULONG num;
-    LONG error=0;
+    ARG_COMMAND = 0,
+    NOOFARGS
+};
 
-    rda=ReadArgs("COMMAND/A/F",(IPTR *)args,NULL);
-    if(rda!=NULL)
+
+int main(int argc, char **argv)
+{
+    struct RDArgs  *rda;
+
+    IPTR args[NOOFARGS] = { NULL };
+
+    LONG    error = RETURN_OK;
+
+    rda = ReadArgs(ARG_TEMPLATE, args, NULL);
+
+    if (rda != NULL)
     {
-	error=RETURN_ERROR;
-	s1=s2=(STRPTR)args[0];
-	while(*s2++)
-	    ;
-	buf=(STRPTR)AllocVec(8+s2-s1,MEMF_ANY);
-	if(buf!=NULL)
+	LONG res;
+
+	struct TagItem tags[] =
 	{
-	    CopyMem("COMMAND ",buf,8);
-	    CopyMem(s1,buf+8,s2-s1);
-	    shell=LoadSeg("c:shell");
-	    if(shell)
-	    {
-		in=Open("CONSOLE:",MODE_OLDFILE);
-		if(in)
-		{
-		    out=Open("CONSOLE:",MODE_NEWFILE);
-		    if(out)
-		    {
-			struct TagItem tags[]=
-			{
-			    { NP_Name, (IPTR)"Background task" },
-			    { NP_Arguments, 0 },
-			    { NP_Input, 0 },
-			    { NP_Output, 0 },
-			    { NP_Error, 0 },
-			    { NP_Seglist, 0 },
-			    { NP_Cli, 1 },
-			    { TAG_END, 0 }
-			};
-			tags[1].ti_Data=(IPTR)buf;
-			tags[2].ti_Data=(IPTR)in;
-			tags[3].ti_Data=(IPTR)out;
-			tags[5].ti_Data=(IPTR)shell;
-			Forbid();
-			process=CreateNewProc(tags);
-			if(process!=NULL)
-			{
-			    num=process->pr_TaskNum;
-			    out=in=shell=0;
-			    error=0;
-			}
-			Permit();
-			Close(out);
-			if(process&&VPrintf("[CLI %ld]\n",&num)<0)
-			   error=RETURN_ERROR;
-		    }
-		    Close(in);
-		}
-		UnLoadSeg(shell);
-	    }
-	    FreeVec(buf);
+	    { SYS_Input , Open("CONSOLE:", MODE_OLDFILE) },
+	    { SYS_Output, Open("CONSOLE:", MODE_NEWFILE) },
+	    { SYS_Asynch, TRUE },
+	    { TAG_DONE  , 0 }
+	};
+	
+	res = SystemTagList((STRPTR)args[ARG_COMMAND], tags);
+	
+	if (res == -1)
+	{
+	    error = RETURN_FAIL;
 	}
+
 	FreeArgs(rda);
-    }else
-	error=RETURN_FAIL;
-    if(error)
-	PrintFault(IoErr(),"Run");
+    }
+    else
+    {
+	error = RETURN_FAIL;
+    }
+
+    if(error != RETURN_OK)
+    {
+	PrintFault(IoErr(), "Run");
+    }
+
     return error;
 }
+
