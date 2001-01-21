81baf786ef7ce868529e62c6032f45ad8f0019d6
SDuvan <SDuvan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-01-21 18:41:38 +0000

    Added documentation
    git-svn-id: https://svn.aros.org/svn/aros/trunk@9851 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/why.c
+++ workbench/c/why.c
@@ -3,8 +3,47 @@
     $Id$
 
     Desc: Why CLI command
-    Lang: english
+    Lang: English
 */
+
+/******************************************************************************
+
+
+    NAME
+
+        Why
+
+    SYNOPSIS
+
+    LOCATION
+
+        Workbench:C
+
+    FUNCTION
+
+        Print additional information why an operation failed. Ordinarily
+	when a command fails a breif message is printed that typically
+	includes the name of the command that failed but provides few
+	details. Why fills in details related to the failed operation.
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+
 
 #include <stdio.h>
 #include <proto/exec.h>
@@ -14,6 +53,7 @@
 
 static const char version[] = "$VER: why 41.3 (26.8.98)\n";
 
+
 int main()
 {
     struct RDArgs *rda;
@@ -21,27 +61,37 @@ int main()
     struct CommandLineInterface *cli;
     LONG lasterror;
     int error = RETURN_OK;
-
+    
     rda = ReadArgs("", args, NULL);
+    
     if (rda != NULL)
     {
         if ((cli = Cli()) != NULL)
         {
             lasterror = cli->cli_Result2;
+
 	    if (cli->cli_ReturnCode == 0 || lasterror == 0)
+	    {
                 VPrintf("The last command did not set a return-value\n", NULL);
+	    }
             else
             {
                 PrintFault(lasterror, "The last command failed, reason");
                 SetIoErr(0);
             }
-        } else
+        } 
+	else
+	{
             error = RETURN_FAIL;
+	}
+	
 	FreeArgs(rda);
-    } else
+    }
+    else
     {
         PrintFault(IoErr(), "Why");
         error = RETURN_FAIL;
     }
-    return(error);
+
+    return error;
 }
