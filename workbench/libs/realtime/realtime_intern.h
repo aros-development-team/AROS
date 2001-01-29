8bd53b3fef5f55237a8600f017ad00facb96c9d8
SDuvan <SDuvan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-01-29 20:23:51 +0000

    Added interrupt structure and pulse task to the internal library base, added dos library
    git-svn-id: https://svn.aros.org/svn/aros/trunk@9904 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/realtime/realtime_intern.h
+++ workbench/libs/realtime/realtime_intern.h
@@ -21,6 +21,14 @@
 #include <exec/semaphores.h>
 #endif
 
+#ifndef   EXEC_TASKS
+#include <exec/tasks.h>
+#endif
+
+#ifndef   EXEC_INTERRUPTS
+#include <exec/interrupts.h>
+#endif
+
 
 #define  GPB(x)  ((struct internal_RealTimeBase *)x)
 
@@ -39,14 +47,19 @@ struct internal_RealTimeBase
     BPTR             rtb_SegList;
     struct ExecBase *rtb_SysBase;
     struct Library  *rtb_UtilityBase;
+    struct Library  *rtb_DOSBase;
 
     struct SignalSemaphore rtb_Locks[RT_MAXLOCK];
 
     struct MinList   rtb_ConductorList;
+
+    struct Interrupt rtb_VBlank;
+    struct Task     *rtb_PulseTask;
 };
 
 #define  UtilityBase  (GPB(RTBase)->rtb_UtilityBase)
 #define  SysBase      (GPB(RTBase)->rtb_SysBase)
+#define  DOSBase      (GPB(RTBase)->rtb_DOSBase)
 
 #define expunge() \
 AROS_LC0(BPTR, expunge, struct RealTimeBase *, RTBase, 3, RealTime)
