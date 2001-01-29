f9de45b32014bc78aa4d77576ad4e94c5ebef5b6
SDuvan <SDuvan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-01-29 20:32:47 +0000

    A tiny demo which shows that at least part of RealTime.library works
    git-svn-id: https://svn.aros.org/svn/aros/trunk@9910 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/demos/realtdemo.c
@@ -0,0 +1,108 @@
+/*
+    (C) 2001 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Test program for RealTime.library
+    Lang: English
+*/
+
+#define  AROS_ALMOST_COMPATIBLE
+
+#include <aros/debug.h>
+#include <aros/asmcall.h>
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <dos/dos.h>
+#include <exec/memory.h>
+#include <aros/machine.h>
+#include <utility/hooks.h>
+#include <utility/tagitem.h>
+
+#include <libraries/realtime.h>
+#include <proto/realtime.h>
+
+#include <stdio.h>
+
+AROS_UFP3(ULONG, myFunc,
+	  AROS_UFPA(struct Hook *  , hook   , A0),
+	  AROS_UFPA(struct pmTime *, message, A1),
+	  AROS_UFPA(struct Player *, player , A2));
+
+int main(int argc, char* argv[])
+{
+    struct Library *RTBase = OpenLibrary("realtime.library", 40);
+    struct Hook myHook;
+
+    struct TagItem tags[] = { { PLAYER_Name     , (IPTR)"Test player" },
+			      { PLAYER_Hook     , (IPTR)&myHook },
+			      { PLAYER_Conductor, (IPTR)"Test conductor" },
+			      { TAG_DONE        , (IPTR)NULL } };
+
+    struct Player *player;
+
+    myHook.h_Entry = (HOOKFUNC)myFunc;
+    myHook.h_SubEntry = NULL;
+    myHook.h_Data  = NULL;
+
+    if (RTBase == NULL)
+    {
+	printf("Couldn't open realtime.library\n");
+	exit(1);
+    }
+
+    player = CreatePlayerA(tags);
+
+    if (player == NULL)
+    {
+	printf("Couldn't create player\n");
+	CloseLibrary(RTBase);
+	exit(1);
+    }
+
+    SetConductorState(player, CONDSTATE_RUNNING, 0);
+
+    {
+	struct TagItem tags[] = { { PLAYER_Ready, TRUE },
+				  { TAG_DONE,     NULL } };
+
+	SetPlayerAttrsA(player, tags);
+    }
+
+    Wait(SIGBREAKF_CTRL_C);
+
+    CloseLibrary(RTBase);
+
+    return 0;
+}
+
+
+AROS_UFH3(ULONG, myFunc,
+	  AROS_UFHA(struct Hook *  , hook   , A0),
+	  AROS_UFHA(struct pmTime *, message, A1),
+	  AROS_UFHA(struct Player *, player , A2))
+{
+    switch (message->pmt_Method)
+    {
+    case PM_TICK:
+	kprintf("Tick at clock %u\n", message->pmt_Time);
+	break;
+
+    case PM_POSITION:
+	kprintf("Position change: Clock %u\n", message->pmt_Time);
+	break;
+
+    case PM_SHUTTLE:
+	kprintf("Shuttling into clock %u\n", message->pmt_Time);
+	break;
+
+    case PM_STATE:
+	kprintf("State change... old state = %u\n",
+		((struct pmState *)message)->pms_OldState);
+
+    default:
+	kprintf("Error: Bogus message with method %u\n", message->pmt_Method);
+	break;
+    }
+
+    return 0;
+}
