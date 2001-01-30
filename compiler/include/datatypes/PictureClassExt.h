a76ccb18be1894a8f787a305f602ee96ddeb6019
johnboy <johnboy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-01-30 20:17:39 +0000

    Added support for picture.datatype V44 in pictureclass.h Added dummy PictureClassExt.h for backwards compatibility to picture.datatype V43.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@9936 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/datatypes/PictureClassExt.h
@@ -0,0 +1,26 @@
+#ifndef DATATYPES_PICTURECLASSEXT_H
+#define DATATYPES_PICTURECLASSEXT_H
+
+/*
+    (C) 1999 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Header for picture.datatype V43 compatibility .
+    Lang: English
+*/
+
+/*
+
+    This is just a dummy header to stay compatible with
+    picture.datatype V43 .
+
+    The real stuff is in <datatypes/pictureclass.h> .
+    As it is in AmigaOS 3.5 .
+
+    Is it a good idea to use capital letters in the filename?
+    But this wasn't my idea. Blame the original author of
+    picture.datatype V43 !
+
+*/
+
+#endif  /* DATATYPES_PICTURECLASS_H */
