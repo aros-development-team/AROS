4adbdc1ea168c5202dd2ed9bded6f01c899afb64
ksvalast <ksvalast@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-02-01 04:12:15 +0000

    Forgot this one. Now, PlayMF should compile fine.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@9948 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/midi/mididefs.h
+++ compiler/include/midi/mididefs.h
@@ -10,6 +10,22 @@
 */
 
 
+
+/*
+  This defines are not complete. Please fill in whenever programs
+  require them. Look here for documentation:
+  http://www.harmony-central.com/MIDI/Doc/doc.html
+  
+  -Kjetil M.
+*/
+
+
+#define MS_Prog 0xc0
+#define MS_Ctrl 0xb0
+
+#define MM_ResetCtrl 0x79
+#define MM_AllOff 0x7b
+
 #define MIDIHiByte(word) ((word)>>7&0x7f)
 #define MIDILoByte(word) ((word)&0x7f)
 #define MIDIWord(hi,lo) (((hi)&0x7f)<<7|((lo)&0x7f))
