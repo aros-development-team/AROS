ca10fb5875681d9942ea4aeed4002d23ad0a7c32
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-02-04 01:08:09 +0000

    Implemented GetLanguageSelection().
    git-svn-id: https://svn.aros.org/svn/aros/trunk@9994 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/lowlevel/lowlevel_init.c
+++ workbench/libs/lowlevel/lowlevel_init.c
@@ -18,8 +18,8 @@
 #endif
 
 /* Customize libheader.c */
-#define LC_SYSBASE_FIELD(lib)	(((LIBBASETYPEPTR)(lib))->wb_SysBase)
-#define LC_SEGLIST_FIELD(lib)   (((LIBBASETYPEPTR)(lib))->wb_SegList)
+#define LC_SYSBASE_FIELD(lib)	(((LIBBASETYPEPTR)(lib))->ll_SysBase)
+#define LC_SEGLIST_FIELD(lib)   (((LIBBASETYPEPTR)(lib))->ll_SegList)
 #define LC_RESIDENTNAME		LowLevel_resident
 #define LC_RESIDENTFLAGS	RTF_AUTOINIT|RTF_COLDSTART
 #define LC_RESIDENTPRI		-120
@@ -29,16 +29,26 @@
 
 #define LC_NO_OPENLIB
 #define LC_NO_CLOSELIB
-#define LC_NO_EXPUNGELIB
 #define LC_STATIC_INITLIB
 
 #include <libcore/libheader.c>
 
 struct ExecBase   * SysBase; /* global variable */
+struct LocaleBase * LocaleBase;
 
 ULONG SAVEDS LC_BUILDNAME(L_InitLib) (LC_LIBHEADERTYPEPTR lh)
 {
-    SysBase = lh->wb_SysBase;
+    SysBase = lh->ll_SysBase;
+
+    LocaleBase = (struct LocaleBase *)OpenLibrary("locale.library",39);
+    if (!LocaleBase)
+        return FALSE;
 
     return TRUE;
 } /* L_InitLib */
+
+void SAVEDS L_ExpungeLib (LC_LIBHEADERTYPEPTR lh)
+{
+    if (LocaleBase)
+        CloseLibrary(LocaleBase);
+}
