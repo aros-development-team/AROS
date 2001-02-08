10e2f3473ae0bd3bf60a1110863c1c7ca8ea280f
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-02-08 15:32:28 +0000

    added suomi.language and suomi.country
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10038 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/locale/countries/suomi.c
@@ -0,0 +1,78 @@
+/*
+    Copyright (C) 2001 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Country data for Finland
+*/
+
+#include <exec/types.h>
+#include <libraries/locale.h>
+#include <libraries/iffparse.h>
+#include <prefs/locale.h>
+
+/* suomi.country: based on this file on Amiga Developer CD 2.1: 
+   NDK/NDK_3.5/Examples/Locale/Countries/make_country_files.c */
+   
+struct CountryPrefs suomiPrefs =
+{
+    /* Reserved */
+    { 0, 0, 0, 0 },
+
+    /* The country codes in the past have been rather inconsistant,
+       sometimes they are 1 character, 2 chars or 3. It would be nice
+       to have some consistency. Maybe use the 3 character name from
+       ISO 3166?
+    */
+
+    /* Country code, telephone code, measuring system */
+    MAKE_ID('F','I',0,0), 358, MS_ISO,
+
+    /* Date time format, date format, time format */
+    "%d.%m.%Y klo %H:%M:%S",
+    "%d.%m.%Y",
+    "%H:%M:%S",
+
+    /* Short datetime, short date, short time formats */
+    "%d.%m.%Y kl %H:%M:%S",
+    "%d.%m.%Y",
+    "%H:%M:%S",
+
+    /* Decimal point, group separator, frac group separator */
+    ",", ".", "",
+
+    /* For grouping rules, see <libraries/locale.h> */
+
+    /* Grouping, Frac Grouping */
+    { 3 }, { 0 },
+
+    /* Mon dec pt, mon group sep, mon frac group sep */
+    ",", ".", ".",
+
+    /* Mon Grouping, Mon frac grouping */
+    { 3 }, { 2 },
+
+    /* Mon Frac digits, Mon IntFrac digits, then number of digits in
+       the fractional part of the money value. Most countries that
+       use dollars and cents, would have 2 for this value
+
+       (As would many of those you don't).
+    */
+    2, 2,
+
+    /* Currency symbol, Small currency symbol */
+    "mk", "penni",
+
+    /* Int CS, this is the ISO 4217 symbol, followed by the character to
+       separate that symbol from the rest of the money. (\x00 for none).
+    */
+    "FIM",
+
+    /* Mon +ve sign, +ve space sep, +ve sign pos, +ve cs pos */
+    "", SS_NOSPACE, SP_PREC_ALL, CSP_SUCCEEDS,
+
+    /* Mon -ve sign, -ve space sep, -ve sign pos, -ve cs pos */
+    "-", SS_NOSPACE, SP_PREC_ALL, CSP_SUCCEEDS,
+
+    /* Calendar type */
+    CT_7MON
+};
