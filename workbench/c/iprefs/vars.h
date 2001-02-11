ac39e7f4616ad36d6bec553a14ee8ec9e0136bae
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-02-11 21:51:49 +0000

    first IPrefs code. not working yet.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10064 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/c/iprefs/vars.h
@@ -0,0 +1,47 @@
+/*
+    (C) 2001 AROS - The Amiga Research OS
+    $Id$
+
+    Desc:
+    Lang: English
+*/
+
+#ifndef VAR
+#define VAR extern
+#endif
+
+VAR struct IntuitionBase        *IntuitionBase;
+VAR struct GfxBase              *GfxBase;
+
+#ifdef _AROS
+VAR struct UtilityBase          *UtilityBase;
+VAR struct LocaleBase           *LocaleBase;
+#else
+VAR struct Library              *UtilityBase;
+VAR struct Library              *LocaleBase;
+#endif
+
+VAR struct Library              *KeymapBase;
+VAR struct Library              *LayersBase;
+VAR struct Library              *DataTypesBase;
+VAR struct Library              *DiskfontBase;
+VAR struct Library  	    	*IFFParseBase;
+
+VAR struct MsgPort  	    	*notifyport;
+
+VAR WORD    	    	    	prog_exitcode;
+VAR UBYTE   	    	    	s[256];
+
+VAR UBYTE   	    	    	envname[256];
+
+VAR UBYTE   	    	    	inputprefsname[256];
+VAR UBYTE   	    	    	fontprefsname[256];
+VAR UBYTE   	    	    	screenprefsname[256];
+VAR UBYTE   	    	    	localeprefsname[256];
+VAR UBYTE   	    	    	paletteprefsname[256];
+VAR UBYTE   	    	    	patternprefsname[256];
+VAR UBYTE   	    	    	icontrolprefsname[256];
+VAR UBYTE   	    	    	serialprefsname[256];
+VAR UBYTE   	    	    	printerprefsname[256];
+VAR UBYTE   	    	    	pointerprefsname[256];
+VAR UBYTE   	    	    	overscanprefsname[256];
---
+++ workbench/c/iprefs/version.h
@@ -0,0 +1,13 @@
+/*
+    (C) 2001 AROS - The Amiga Research OS
+    $Id$
+
+    Desc:
+    Lang: English
+*/
+
+
+#define VERSION 	1
+#define REVISION 	1
+#define DATESTR 	"11.02.2001"
+#define VERSIONSTR	"$VER: IPrefs 1.0 (" DATESTR ")"
