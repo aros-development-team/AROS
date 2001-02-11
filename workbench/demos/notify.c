0cd4a99f889c03ddf63c20b59a866c2310d5d8c8
SDuvan <SDuvan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-02-11 09:28:53 +0000

    Tiny demo of ram.handler notification
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10059 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/demos/notify.c
@@ -0,0 +1,58 @@
+/*
+    (C) 2001 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Test program for ram.handler notification
+    Lang: English
+*/
+
+#define  AROS_ALMOST_COMPATIBLE
+#include <proto/exec.h>
+#include <exec/memory.h>
+#include <aros/machine.h>
+#include <stdio.h>
+#include <dos/dos.h>
+#include <dos/dosextens.h>
+#include <dos/notify.h>
+#include <proto/exec.h>
+#include <proto/dos.h>
+
+
+int main(int argc, char* argv[])
+{
+    struct NotifyRequest *nr = AllocVec(sizeof(struct NotifyRequest),
+					MEMF_CLEAR);
+    BPTR lock;
+
+    if (nr == NULL)
+    {
+	printf("Could not allocate memory.\n");
+	exit(1);
+    }
+
+    nr->nr_Name = "Olle";
+    nr->nr_Flags = NRF_SEND_SIGNAL;
+    nr->nr_stuff.nr_Signal.nr_Task = FindTask(NULL);
+    nr->nr_stuff.nr_Signal.nr_SignalNum = SIGB_SINGLE;
+    
+    lock = Lock("Ram Disk:", SHARED_LOCK);
+
+    if (lock == NULL)
+    {
+	printf("Could not lock Ram Disk:\n");
+	exit(1);
+    }
+
+    CurrentDir(lock);
+
+    StartNotify(nr);
+
+    printf("Waiting for notification\n");
+
+    Wait(SIGF_SINGLE);
+
+    printf("Got notification!\n");
+
+    return 0;
+}
+
