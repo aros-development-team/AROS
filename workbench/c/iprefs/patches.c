02396838928a9b4a71ae97a44bfae2527687afab
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-02-13 21:46:14 +0000

    patch utility.library functions ToUpper/ToLower/Strnicmp/Stricmp to use the corresponding private locale.library replacement functions.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10086 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/iprefs/patches.c
+++ workbench/c/iprefs/patches.c
@@ -24,3 +24,39 @@ void Install_RawDoFmtPatch(void)
 }
 
 /*********************************************************************************************/
+
+void Install_StrnicmpPatch(void)
+{
+    SetFunction(&UtilityBase->ub_LibNode,
+    	    	-28 * LIB_VECTSIZE,
+		__AROS_GETVECADDR(LocaleBase, 32));
+}
+
+/*********************************************************************************************/
+
+void Install_StricmpPatch(void)
+{
+    SetFunction(&UtilityBase->ub_LibNode,
+    	    	-27 * LIB_VECTSIZE,
+		__AROS_GETVECADDR(LocaleBase, 33));
+}
+
+/*********************************************************************************************/
+
+void Install_ToLowerPatch(void)
+{
+    SetFunction(&UtilityBase->ub_LibNode,
+    	    	-30 * LIB_VECTSIZE,
+		__AROS_GETVECADDR(LocaleBase, 34));
+}
+
+/*********************************************************************************************/
+
+void Install_ToUpperPatch(void)
+{
+    SetFunction(&UtilityBase->ub_LibNode,
+    	    	-29 * LIB_VECTSIZE,
+		__AROS_GETVECADDR(LocaleBase, 35));
+}
+
+/*********************************************************************************************/
