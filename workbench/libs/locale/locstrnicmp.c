6b7eb86081601a19a39e2f86b74a61950acc8e3d
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-02-13 21:45:13 +0000

    locale sensitive replacements for utility.library:stricmp,strnicmp, tolower,toupper. IPrefs will patch utility.library to use this functions.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10085 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/locale/locstrnicmp.c
@@ -0,0 +1,77 @@
+/*
+    Copyright (C) 2001 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: LocStrnicmp - locale.library's private replacement
+    	  of utility.library/Strnicmp function. IPrefs will install
+	  the patch.
+	  
+    Lang: english
+*/
+
+#include <exec/types.h>
+#include <proto/exec.h>
+#include <proto/locale.h>
+#include "locale_intern.h"
+#include <aros/asmcall.h>
+
+extern struct LocaleBase *globallocalebase;
+
+ /*****************************************************************************
+
+    NAME */
+#include <proto/locale.h>
+
+	AROS_LH3(LONG, LocStrnicmp,
+
+/*  SYNOPSIS */
+	AROS_LHA(CONST_STRPTR, string1, A0),
+	AROS_LHA(CONST_STRPTR, string2, A1),
+	AROS_LHA(LONG        , length , D0),
+
+/*  LOCATION */
+	struct LocaleBase *, LocaleBase, 32, Locale)
+
+/*  FUNCTION
+    	See utility.library/Strnicmp
+	
+    INPUTS
+    	See utility.library/Strnicmp
+
+    RESULT
+
+    NOTES
+    	This function is not called by apps directly. Instead utility.library/Strnicmp
+	is patched to use this function. This means, that the LocaleBase parameter
+	above actually points to UtilityBase!!! But I may not rename it, because then
+	no entry for this function is generated in the Locale functable by the
+	corresponding script!
+	
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	utility.library/Strnicmp(), locale.library/StrnCmp().
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+#define LocaleBase globallocalebase
+
+    return StrnCmp(IntLB(LocaleBase)->lb_CurrentLocale,
+    	    	   string1,
+		   string2,
+		   length,
+		   SC_ASCII);
+    
+    AROS_LIBFUNC_EXIT
+    
+} /* LocStrnicmp */
+
+#undef LocaleBase
