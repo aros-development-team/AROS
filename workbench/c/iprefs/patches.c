53132bb68cb27b49714b4632ae64745aa00e163a
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-02-14 20:27:00 +0000

    also patch dos.library/datetostr to use the corresponding locale.library's replacement function.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10092 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/iprefs/patches.c
+++ workbench/c/iprefs/patches.c
@@ -16,47 +16,76 @@
 
 /*********************************************************************************************/
 
-void Install_RawDoFmtPatch(void)
+#define LIB_EXEC    1
+#define LIB_UTILITY 2
+#define LIB_DOS     3
+#define LIB_LOCALE  4
+
+/*********************************************************************************************/
+
+static struct patchinfo
 {
-    SetFunction(&SysBase->LibNode,
-    	    	-87 * LIB_VECTSIZE,
-		__AROS_GETVECADDR(LocaleBase, 31));
+    WORD    library;
+    WORD    whichfunc;
+    WORD    patchlibrary;
+    WORD    whichpatchfunc;
+}
+pi [] =
+{
+    {LIB_EXEC	, 87 , LIB_LOCALE, 31}, /* RawDoFmt  */
+    {LIB_UTILITY, 28 , LIB_LOCALE, 32}, /* Strnicmp  */
+    {LIB_UTILITY, 27 , LIB_LOCALE, 33}, /* Stricmp   */
+    {LIB_UTILITY, 30 , LIB_LOCALE, 34}, /* ToLower   */
+    {LIB_UTILITY, 29 , LIB_LOCALE, 35}, /* ToUpper   */
+    {LIB_DOS	, 124, LIB_LOCALE, 36}, /* DateToSTr */
+    {NULL   	    	    	     }
+};
+
+/*********************************************************************************************/
+
+static struct Library *GetLib(WORD which)
+{
+    struct Library *lib = NULL;
+    
+    switch(which)
+    {
+    	case LIB_EXEC:
+	    lib = (struct Library *)SysBase;
+	    break;
+	    
+	case LIB_DOS:
+	    lib = (struct Library *)DOSBase;
+	    break;
+	    
+	case LIB_UTILITY:
+	    lib = (struct Library *)UtilityBase;
+	    break;
+	    
+	case LIB_LOCALE:
+	    lib = (struct Library *)LocaleBase;
+	    break;
+    }
+    
+    return lib;
 }
 
 /*********************************************************************************************/
 
-void Install_StrnicmpPatch(void)
+void InstallPatches(void)
 {
-    SetFunction(&UtilityBase->ub_LibNode,
-    	    	-28 * LIB_VECTSIZE,
-		__AROS_GETVECADDR(LocaleBase, 32));
-}
-
-/*********************************************************************************************/
-
-void Install_StricmpPatch(void)
-{
-    SetFunction(&UtilityBase->ub_LibNode,
-    	    	-27 * LIB_VECTSIZE,
-		__AROS_GETVECADDR(LocaleBase, 33));
-}
-
-/*********************************************************************************************/
-
-void Install_ToLowerPatch(void)
-{
-    SetFunction(&UtilityBase->ub_LibNode,
-    	    	-30 * LIB_VECTSIZE,
-		__AROS_GETVECADDR(LocaleBase, 34));
-}
-
-/*********************************************************************************************/
-
-void Install_ToUpperPatch(void)
-{
-    SetFunction(&UtilityBase->ub_LibNode,
-    	    	-29 * LIB_VECTSIZE,
-		__AROS_GETVECADDR(LocaleBase, 35));
+    WORD i;
+    
+    Forbid();
+    for(i = 0; pi[i].library; i++)
+    {
+    	SetFunction(GetLib(pi[i].library),
+	    	    -pi[i].whichfunc * LIB_VECTSIZE,
+		    __AROS_GETVECADDR(GetLib(pi[i].patchlibrary), pi[i].whichpatchfunc));
+		    
+    }
+    Permit();
+    
+    patches_installed = TRUE;
 }
 
 /*********************************************************************************************/
