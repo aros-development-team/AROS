0778abf8fa33b4f2373fd4f26a4dd8f236b6e559
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-02-14 20:28:03 +0000

    function to test amiga2date from utility.library, which seems to contain some bug.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10093 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/amiga2date.c
@@ -0,0 +1,68 @@
+#include <dos/dos.h>
+#include <utility/date.h>
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <proto/utility.h>
+#include <stdio.h>
+
+struct UtilityBase *UtilityBase;
+struct DateTime dt;
+struct ClockData cd;
+
+char s[100];
+
+int main(void)
+{
+    UtilityBase = (struct UtilityBase *)OpenLibrary("utility.library", 0);
+    if (UtilityBase)
+    {
+	dt.dat_StrDate = "31-dec-2000";
+	dt.dat_Format = FORMAT_DOS;
+	
+	if (StrToDate(&dt))
+	{
+	    dt.dat_StrDate = s;
+	    if (DateToStr(&dt))
+	    {
+	    	printf("Verified date: \"%s\" days = %d min = %d tick = %d\n",
+		    	s,
+		    	dt.dat_Stamp.ds_Days,
+			dt.dat_Stamp.ds_Minute,
+			dt.dat_Stamp.ds_Tick);
+	    	
+		Amiga2Date(dt.dat_Stamp.ds_Days * 60 * 60 * 24 + 
+		    	   dt.dat_Stamp.ds_Minute * 60 +
+			   dt.dat_Stamp.ds_Tick / 50, &cd);
+			   
+		printf("\nAmiga2Date says:\n\n");
+		printf("sec   = %d\n", cd.sec);
+		printf("min   = %d\n", cd.min);
+		printf("hour  = %d\n", cd.hour);
+		printf("mday  = %d\n", cd.mday);
+		printf("month = %d\n", cd.month);
+		printf("year  = %d\n", cd.year);
+		printf("wday  = %d\n", cd.wday);
+
+    	    	printf("\n-------- One day later -----------\n\n");
+		
+		Amiga2Date((dt.dat_Stamp.ds_Days + 1) * 60 * 60 * 24 + 
+		    	   dt.dat_Stamp.ds_Minute * 60 +
+			   dt.dat_Stamp.ds_Tick / 50, &cd);
+			   
+		printf("\nAmiga2Date says:\n\n");
+		printf("sec   = %d\n", cd.sec);
+		printf("min   = %d\n", cd.min);
+		printf("hour  = %d\n", cd.hour);
+		printf("mday  = %d\n", cd.mday);
+		printf("month = %d\n", cd.month);
+		printf("year  = %d\n", cd.year);
+		printf("wday  = %d\n", cd.wday);
+
+	    }
+	    else puts("DateToStr failed!");
+	}
+	else puts("StrToDate failed!");
+	
+    	CloseLibrary((struct Library *)UtilityBase);
+    }
+}
