978ca706170c05cfcb836fc304ef55506bd82350
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-02-15 22:09:48 +0000

    a test program to verify correctness of DateToStr/StrToDate functions. Seems to work fine now. But not if IPrefs is running, where the locale.library's replacement datetostr/strtodate functions are used.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10106 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/strtodate.c
@@ -0,0 +1,37 @@
+#include <dos/datetime.h>
+#include <dos/dos.h>
+#include <proto/dos.h>
+#include <string.h>
+#include <stdio.h>
+
+struct DateTime dt;
+struct DateStamp ds;
+char s[100];
+
+LONG days = 0;
+
+int main(void)
+{
+    do
+    {
+    	dt.dat_Stamp.ds_Days = days;
+	dt.dat_Format = FORMAT_DOS;
+	dt.dat_StrDate = s;
+	
+	DateToStr(&dt);
+	
+	dt.dat_Stamp.ds_Days = -1;
+	StrToDate(&dt);
+	
+	//printf("date \"%s\" day = %ld\n", s, days);
+	
+	if (dt.dat_Stamp.ds_Days != days)
+	{
+	    printf("Bad results for date \"%s\" (day #%ld). "
+	    	   "StrToDate thought it was day #%ld\n", s, days, dt.dat_Stamp.ds_Days);
+	}
+	
+	days++;
+	
+    } while (days < 365 * 300); /* around 300 years */
+}
