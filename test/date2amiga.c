694dcf2a9dcf56f3b103c0f4cdb389fa8eb702f5
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-02-16 20:28:50 +0000

    yet another date test program.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10108 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/date2amiga.c
@@ -0,0 +1,67 @@
+#include <dos/datetime.h>
+#include <dos/dos.h>
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <proto/utility.h>
+#include <utility/date.h>
+#include <string.h>
+#include <stdio.h>
+
+struct UtilityBase *UtilityBase;
+struct DateTime dt;
+struct DateStamp ds;
+struct ClockData cd;
+ 
+char s[100];
+
+LONG days = 0;
+ULONG seconds = 0;
+ULONG secresult;
+
+int main(void)
+{
+    if (!(UtilityBase = (struct UtilityBase *)OpenLibrary("utility.library", 36)))
+    {
+    	printf("Can't open utility.library!\n");
+    }
+    else
+    {
+	do
+	{
+    	    dt.dat_Stamp.ds_Days = days;
+	    dt.dat_Format = FORMAT_DOS;
+	    dt.dat_StrDate = s;
+
+	    DateToStr(&dt);
+
+	    dt.dat_Stamp.ds_Days = -1;
+	    StrToDate(&dt);
+
+	    //printf("date \"%s\" day = %ld\n", s, days);
+
+	    if (dt.dat_Stamp.ds_Days != days)
+	    {
+		printf("StrToDate showed bad results for date \"%s\" (day #%ld). "
+	    	       "StrToDate thought it was day #%ld\n", s, days, dt.dat_Stamp.ds_Days);
+	    }
+	    else
+	    {
+	    	Amiga2Date(seconds, &cd);
+		if ((secresult = Date2Amiga(&cd)) != seconds)
+		{
+		    printf("Date2Amiga gave wrong values for date \"%s\" (day #%ld)"
+		           " (secs %ld) -> wrong secs is %ld"
+			   " --> clockdate: year = %ld month = %ld day = %ld\n"
+			   , s, days, seconds, secresult, cd.year, cd.month, cd.mday);
+		}
+		
+	    }
+
+	    days++;
+    	    seconds += 86400;
+	    
+	} while (days < 365 * 300); /* around 300 years */
+	
+	CloseLibrary((struct Library *)UtilityBase);
+    }
+}
