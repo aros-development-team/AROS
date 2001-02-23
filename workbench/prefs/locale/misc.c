a9edf40841c48c9d00ed9594ec52dc8d585ddb73
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-02-23 20:13:33 +0000

    started to work on Locale prefs program.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10142 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/prefs/locale/misc.c
@@ -0,0 +1,137 @@
+/*
+    (C) 2001 AROS - The Amiga Research OS
+    $Id$
+
+    Desc:
+    Lang: English
+*/
+
+/*********************************************************************************************/
+
+#include "global.h"
+#include "version.h"
+
+#include <string.h>
+
+/*********************************************************************************************/
+
+static struct NewMenu nm[] =
+{
+    {NM_TITLE, (STRPTR)MSG_MEN_PROJECT                                                  },
+     {NM_ITEM, (STRPTR)MSG_MEN_PROJECT_OPEN                                             },
+     {NM_ITEM, (STRPTR)MSG_MEN_PROJECT_SAVEAS                                           },
+     {NM_ITEM, NM_BARLABEL                                                              },
+     {NM_ITEM, (STRPTR)MSG_MEN_PROJECT_QUIT                                             },
+    {NM_TITLE, (STRPTR)MSG_MEN_EDIT                                                     },
+     {NM_ITEM, (STRPTR)MSG_MEN_EDIT_DEFAULT                                           	},
+     {NM_ITEM, (STRPTR)MSG_MEN_EDIT_LASTSAVED                                           },
+     {NM_ITEM, (STRPTR)MSG_MEN_EDIT_RESTORE                                           	},
+    {NM_TITLE, (STRPTR)MSG_MEN_SETTINGS                                                 },
+     {NM_ITEM, (STRPTR)MSG_MEN_SETTINGS_CREATEICONS, NULL, CHECKIT                      },
+    {NM_END}
+};
+
+/*********************************************************************************************/
+
+void InitMenus(void)
+{
+    struct NewMenu *actnm = nm;
+    
+    for(actnm = nm; actnm->nm_Type != NM_END; actnm++)
+    {
+	if (actnm->nm_Label != NM_BARLABEL)
+	{
+	    ULONG  id = (ULONG)actnm->nm_Label;
+	    STRPTR str = MSG(id);
+	    
+	    if (actnm->nm_Type == NM_TITLE)
+	    {
+		actnm->nm_Label = str;
+	    } else {
+		actnm->nm_Label = str + 2;
+		if (str[0] != ' ') actnm->nm_CommKey = str;
+	    }
+	    actnm->nm_UserData = (APTR)id;
+	    
+	} /* if (actnm->nm_Label != NM_BARLABEL) */
+	
+    } /* for(actnm = nm; nm->nm_Type != NM_END; nm++) */
+
+}
+
+/*********************************************************************************************/
+
+void MakeMenus(void)
+{
+    struct TagItem menu_tags[] =
+    {
+	{GTMN_NewLookMenus, TRUE},
+	{TAG_DONE               }
+    };
+    
+    menus = CreateMenusA(nm, NULL);
+    if (!menus) Cleanup(MSG(MSG_CANT_CREATE_MENUS));
+    
+    if (!LayoutMenusA(menus, vi, menu_tags)) Cleanup(MSG(MSG_CANT_CREATE_MENUS));
+    
+}
+
+/*********************************************************************************************/
+
+void KillMenus(void)
+{
+    if (win) ClearMenuStrip(win);
+    if (menus) FreeMenus(menus);
+    
+    menus = NULL;
+}
+
+/*********************************************************************************************/
+
+void SetMenuFlags(void)
+{
+    struct MenuItem *item;
+    
+    if (win) ClearMenuStrip(win);
+    
+    if (win) ResetMenuStrip(win, menus);
+}
+
+/*********************************************************************************************/
+
+STRPTR GetFile(void)
+{
+    static UBYTE         pathbuffer[300];
+    static UBYTE         filebuffer[300];
+    struct FileRequester *req;
+    STRPTR               retval = NULL;
+    
+    AslBase = OpenLibrary("asl.library", 39);
+    if (AslBase)
+    {
+	req = AllocAslRequestTags(ASL_FileRequest, ASLFR_TitleText    , (IPTR)MSG(MSG_ASL_OPEN_TITLE),
+						   ASLFR_DoPatterns   , TRUE                         ,
+						   TAG_DONE);
+	if (req)
+	{
+	    if (AslRequest(req, NULL))
+	    {
+		strncpy(filebuffer, req->fr_Drawer, 299);
+		AddPart(filebuffer, req->fr_File, 299);
+		
+		retval = filebuffer;
+		
+	    } /* if (AslRequest(req, NULL) */
+	    
+	    FreeAslRequest(req);
+	    
+	} /* if (req) */
+	
+	CloseLibrary(AslBase);
+	
+    } /* if (AslBase) */
+    
+    return retval;
+}
+
+/*********************************************************************************************/
---
+++ workbench/prefs/locale/version.h
@@ -0,0 +1,13 @@
+/*
+    (C) 1997-2000 AROS - The Amiga Research OS
+    $Id$
+
+    Desc:
+    Lang: English
+*/
+
+
+#define VERSION 	1
+#define REVISION 	1
+#define DATESTR 	"19.09.2000"
+#define VERSIONSTR	"$VER: MultiView 1.1 (" DATESTR ")"
