05e19bfa7b8502b42adf682c0d7dd5ee1899d66a
sheutlin <sheutlin@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-03-04 00:37:48 +0000

    dont't compile "jumphunk->vec.jmp = __AROS_ASMJMP;" when in native flavour
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10206 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/internalloadseg_aout.c
+++ rom/dos/internalloadseg_aout.c
@@ -222,7 +222,9 @@ BPTR InternalLoadSeg_AOUT(BPTR file,
   jumphunk->next = MKBADDR(texthunk + sizeof(ULONG));
   texthunk += sizeof(ULONG) + sizeof(BPTR);
 
+#if (!(AROS_FLAVOUR & AROS_FLAVOUR_STANDALONE))
   jumphunk->vec.jmp = __AROS_ASMJMP;
+#endif
   __AROS_SET_VEC(&jumphunk->vec, texthunk + header.a_entry);
 
   if ( AROS_UFC4(LONG, functionarray[0] /* Read */,
