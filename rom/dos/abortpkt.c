1c630b9eeae127d15bbdcf7c564f06e549cf8d4a
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-03-18 01:20:45 +0000

    Also checking whether dp_Arg7 != NULL before trying to abort.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10321 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/abortpkt.c
+++ rom/dos/abortpkt.c
@@ -47,14 +47,13 @@
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct DosLibrary *,DOSBase)
 
-    LONG res = AbortIO((struct IORequest*)pkt->dp_Arg7);
-
+    if (NULL != (struct IORequest *)pkt->dp_Arg7) {
+        if (0 == AbortIO((struct IORequest*)pkt->dp_Arg7)) 
+        {
 #warning Still have to decide where to finally free the IORequest structure if this goes well
-    if (0 == res) 
-    {
-        FreeMem(pkt->dp_Arg7, sizeof(struct IOFileSys));
-        pkt->dp_Arg7 = NULL;
+            FreeMem(pkt->dp_Arg7, sizeof(struct IOFileSys));
+            pkt->dp_Arg7 = NULL;
+        }
     }
-
     AROS_LIBFUNC_EXIT
 } /* AbortPkt */
