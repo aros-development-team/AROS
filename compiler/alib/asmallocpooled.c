b4c2b64369df8bf0f2e217d02b4ed8314f433410
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-03-27 00:56:50 +0000

    These functions simply call the exec functions. Some older programs like the gtlayout.library need those functions.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10388 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/alib/asmallocpooled.c
@@ -0,0 +1,46 @@
+/*
+    Copyright (C) 1997-2001 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Simply call the exec function 
+    Lang: english
+*/
+#define AROS_TAGRETURNTYPE APTR
+
+#include "alib_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+#define NO_INLINE_STDARG /* turn off inline def */
+#include <proto/exec.h>
+
+	AROS_UFH3(APTR, AsmAllocPooled,
+
+/*  SYNOPSIS */
+	AROS_UFHA(APTR, poolHeader, A0),
+	AROS_UFHA(ULONG, memSize  , D0),
+	AROS_UFHA(struct ExecBase *, SysBase, A6)) 
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+	AllocPooled(poolHeader,memSize);	
+} /* AsmAllocPooled */
