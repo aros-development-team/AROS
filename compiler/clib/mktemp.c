6576fca9f1474d538b94b06b9466797975b41dce
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-04-05 16:31:41 +0000

    implemented functions getenv() and mktemp(). Based on libnix source.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10492 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/mktemp.c
@@ -0,0 +1,77 @@
+/*
+    (C) 2001 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: function mktemp()
+    Lang: english
+*/
+
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <dos/dos.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <stdlib.h>
+
+	char *mktemp (
+
+/*  SYNOPSIS */
+	char *template)
+
+/*  FUNCTION
+	Make a unique temporary file name.
+
+    INPUTS
+	template- Name of the environment variable.
+
+    RESULT
+	Returns template.
+
+    NOTES
+    	Template must end in "XXXXXX".
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+    	Based on libnix mktemp
+
+    HISTORY
+	04.05.2001 stegerg created
+
+******************************************************************************/
+{ 
+    long pid = (long)FindTask(0L);
+    char *c = template + strlen(template);
+    BPTR lock;
+    
+    while (*--c == 'X')
+    {
+	*c = pid % 10 + '0';
+	pid /= 10;
+    }
+    
+    c++;
+    
+    if (*c)
+    {
+	for(*c = 'A'; *c <= 'Z'; (*c)++)
+	{
+	    if (!(lock = Lock(template, ACCESS_READ)))
+	    {
+        	return template;
+	    }
+	    UnLock(lock);
+	}
+	*c = 0;
+    }
+    
+    return template; 
+ 
+} /* mktemp */
+
