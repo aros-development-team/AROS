4b0c47b0190d381db761d0aaab6725c05ecdc245
petah <petah@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-04-05 01:55:04 +0000

    "Font" Preferences 0.5 (Developer's release 2). "Font" now stores the user settings in big endian byte order. The application now uses a custom made IFF Preferences Header in order for Linux GCC to flush the data to disk in an AmigaOS compatible way. Some minor bugs have been fixed. "Font" now features (working) "Open" and "Save As" menu options. Code previously stored in "getfont.c" has been moved to new project file "asl.c"; the project "mmakefile.src" has been changed to reflect this.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10474 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/prefs/font/handleiff.c
+++ workbench/prefs/font/handleiff.c
@@ -1,16 +1,46 @@
-#include <stdio.h>
 #include <proto/dos.h>
 #include <proto/iffparse.h>
 #include <prefs/prefhdr.h>
 #include <prefs/font.h>
 
+#include <aros/macros.h>
+
+#include <stdio.h>
+
+/* This structure is AROS custom. Apperantly, Linux GCC aligns everything
+   larger than one byte in four byte boundaries - with the sole expection
+   of bytes themselves. Therefor, to maintain compatibility we define our
+   AROS Preferences headers byte by byte to keep it six (6) bytes instead
+   of eight (8) which would be the case if Linux GCC called the shots.
+   Thank you Georg Steger for pointing this out!
+*/
+struct FilePrefHeader
+{
+ UBYTE ph_Version;
+ UBYTE ph_Type;
+ UBYTE ph_Flags[4];
+};
+
 extern struct Library *IFFParseBase;
 extern void quitApp(UBYTE *, UBYTE);
 struct IFFHandle *iffHandle;
 
+void convertEndian(struct FontPrefs *fontPrefs)
+{
+ UBYTE a;
+
+ for(a = 0; a <= 2; a++)
+  fontPrefs->fp_Reserved[a] = AROS_BE2LONG(fontPrefs->fp_Reserved);
+
+ fontPrefs->fp_Reserved2 = AROS_BE2WORD(fontPrefs->fp_Reserved2);
+ fontPrefs->fp_Type = AROS_BE2WORD(fontPrefs->fp_Type);
+ fontPrefs->fp_TextAttr.ta_YSize = AROS_BE2WORD(fontPrefs->fp_TextAttr.ta_YSize);
+}
+
 void writeIFF(UBYTE *fileName, struct FontPrefs **fontPrefs)
 {
- struct PrefHeader prefHeader; /* Allocate this memory using AllocMem()!*/
+ //struct PrefHeader prefHeader; /* Allocate this memory using AllocMem()!*/
+ struct FilePrefHeader prefHeader; // Allocate this memory using AllocMem() instead!
  UBYTE a = 0, b = 0;
 
  if(iffHandle = AllocIFF())
@@ -25,26 +55,27 @@ void writeIFF(UBYTE *fileName, struct FontPrefs **fontPrefs)
 
     prefHeader.ph_Version = PHV_CURRENT;
     prefHeader.ph_Type = NULL;
-    prefHeader.ph_Flags = 0; /* Set to 0; see <prefs/prefhdr.h> */
+    for(a = 0; a <= 3; a++)
+     prefHeader.ph_Flags[a] = 0; /* Set to 0; see <prefs/prefhdr.h> */
 
     PushChunk(iffHandle, ID_PREF, ID_PRHD, IFFSIZE_UNKNOWN); /* IFFSIZE_UNKNOWN? */
-    WriteChunkBytes(iffHandle, &prefHeader, sizeof(struct PrefHeader));
+    WriteChunkBytes(iffHandle, &prefHeader, sizeof(struct FilePrefHeader));
     PopChunk(iffHandle);
 
     for(a = 0; a <= 2; a++)
     {
-     //printf("\nCreating ID_FONT chunk... a = %d\n", a);
-
      b = PushChunk(iffHandle, ID_PREF, ID_FONT, sizeof(struct FontPrefs));
      if(b)
       printf("error: PushChunk() = %d ", b);
 
      printf("fontPrefs = %d bytes struct FontPrefs = %d bytes\n", sizeof(fontPrefs), sizeof(struct FontPrefs));
+     convertEndian(fontPrefs[a]);
      b = WriteChunkBytes(iffHandle, fontPrefs[a], sizeof(struct FontPrefs));
      //if(b)
       //printf("WriteChunkyBytes(fontPrefs) = %d ", b);
 
      b = PopChunk(iffHandle);
+     convertEndian(fontPrefs[a]);
      if(b)
       printf("error: PopChunk() = %d ", b);
 
@@ -63,7 +94,7 @@ void writeIFF(UBYTE *fileName, struct FontPrefs **fontPrefs)
   CloseIFF(iffHandle);
 
   if((BPTR)iffHandle->iff_Stream) // File can't be closed prior to CloseIFF()!
-   Close(iffHandle->iff_Stream);
+   Close((BPTR)iffHandle->iff_Stream); // Why isn't this stored in memory as a "BPTR"? Look up!
  }
  else
   printf("Can't allocate IFF handle!\n");
@@ -75,6 +106,8 @@ void readIFF(UBYTE *fileName, struct FontPrefs **readFontPrefs)
 {
  UBYTE a, b;
  struct ContextNode *conNode;
+
+ printf("reading %s preferences...\n", fileName);
 
  if(!(iffHandle = AllocIFF()))
   quitApp("Unable to allocate IFF handle!", 20);
@@ -88,11 +121,11 @@ void readIFF(UBYTE *fileName, struct FontPrefs **readFontPrefs)
    // We want some sanity checking here!
    for(a = 0; a <= 2; a++)
    {
-    printf("Reading chunk %d...\n", a);
-    b = StopChunk(iffHandle, ID_PREF, ID_FONT);
-    printf("StopChunk returned %d. sizeof(FontPrefs) = %d\n", b, sizeof(struct FontPrefs));
+    printf("StopChunk() returned %ld\n", StopChunk(iffHandle, ID_PREF, ID_FONT));
+    /*printf("StopChunk returned %d. sizeof(FontPrefs) = %d\n", b, sizeof(struct FontPrefs));*/
 
-    ParseIFF(iffHandle, IFFPARSE_SCAN);
+    /*b = ParseIFF(iffHandle, IFFPARSE_SCAN);*/
+    printf("ParseIFF returned %ld\n", ParseIFF(iffHandle, IFFPARSE_SCAN));
     conNode = CurrentChunk(iffHandle);
     // printf("chunk size = %ld\n", conNode->cn_Size);
 
@@ -100,11 +133,15 @@ void readIFF(UBYTE *fileName, struct FontPrefs **readFontPrefs)
     b = ReadChunkBytes(iffHandle, readFontPrefs[a], sizeof(struct FontPrefs));
     // printf("Read %d bytes\n", b);
     readFontPrefs[a]->fp_TextAttr.ta_Name = readFontPrefs[a]->fp_Name;
-    // printf("readFontPrefs->fp_Name = >%s<\n", readFontPrefs[a]->fp_Name);
-   }
-  }
+    printf("readFontPrefs->YSize = >%d<\n", readFontPrefs[a]->fp_TextAttr.ta_YSize);
 
-  CloseIFF(iffHandle);
+    convertEndian(readFontPrefs[a]);
+   }
+
+   CloseIFF(iffHandle);
+  }
+  else
+   printf("OpenIFF() failed!\n");
 
   Close((BPTR)iffHandle->iff_Stream);
   printf("Finished reading IFF file\n");
