b0a4f640460922ccb6dbe536bec3c8a4835c2ffe
petah <petah@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-04-19 12:44:36 +0000

    This is Developer's release 3 of "Font" Preferences for AROS, designated version 0.7. "Font" now all sports all original menu items (except for "Create Icons?"). The "code flow model" has been rearranged to allow for a much more easy to maintain implementation; the internal API has changed and code has been moved in between source modules. We're no longer API backwards compatible with earlier versions of "Font" Preferences! The GUI layout code is totally reworked - most parts are rewritten.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10666 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/prefs/font/global.h
+++ workbench/prefs/font/global.h
@@ -1,3 +1,13 @@
+struct AppGUIData
+{
+ UWORD		agd_MaxWidth;
+ UWORD		agd_MaxHeight;
+ UWORD		agd_BevelBoxOffset;
+ struct Window	*agd_Window;
+ struct Menu	*agd_Menu;
+ struct Gadget	*agd_GadgetList;
+};
+
 /* ReadArgs() stuff */
 #define ARG_FROM 0
 #define ARG_EDIT 1
@@ -8,7 +18,25 @@
 #define NUM_ARGS 5
 
 /* Menu item identifiers */
-#define MENU_ID_OPEN 1
-#define MENU_ID_SAVE 2
-#define MENU_ID_SAVEAS 3
-#define MENU_ID_QUIT 4
+#define MENU_ID_OPEN		1
+#define MENU_ID_SAVE		2
+#define MENU_ID_SAVEAS		3
+#define MENU_ID_QUIT		4
+#define MENU_ID_DEFAULTS	5
+#define MENU_ID_LASTSAVED	6
+#define MENU_ID_RESTORE		7
+
+/* GUI constants */
+#define WINDOW_OFFSET		3
+
+#define MX_INIT_ACTIVE		0 // Default MX choice
+
+#define GADGET_INNER_SPACING	3 // Should be proportional to font size?
+#define GADGET_OUTER_SPACING	3 // Should be proportional to font size?
+
+#define MX_CHOOSEFONT		0 
+#define BUTTON_GETFONT		1
+#define BUTTON_SAVE		2
+#define BUTTON_USE		3
+#define BUTTON_CANCEL		4
+#define GADGET_ID_TEXT		5
--- workbench/prefs/font/init.c
+++ workbench/prefs/font/init.c
@@ -6,27 +6,35 @@
 
 struct FontPrefs *fontPrefs[3];
 
-extern void quitApp(UBYTE *, UBYTE);
-
-void initPrefMem(void)
+void initDefaultPrefs(struct FontPrefs **fontPrefsPtr)
 {
  UBYTE a;
 
  for(a = 0; a <= 2; a++)
  {
-  if(!(fontPrefs[a] = AllocMem(sizeof(struct FontPrefs), MEMF_ANY | MEMF_CLEAR)))
-   quitApp("Unable to allocate FontPrefs memory!", RETURN_FAIL);
+  fontPrefs[a]->fp_Type = a;		/* Is this 0, 1, 2 or 1, 2, 3? Look it up! */
+  fontPrefs[a]->fp_FrontPen = 0;	/* Is this (really) default? Look it up! */
+  fontPrefs[a]->fp_BackPen = 0;		/* Is this (really) default? Look it up! */
+  fontPrefs[a]->fp_DrawMode = 0;	/* Is this (really) default? Look it up! */
 
-  fontPrefs[a]->fp_Type = a;
-  fontPrefs[a]->fp_FrontPen = 0; /* Is this (really) default? Check it up! */
-  fontPrefs[a]->fp_BackPen = 0; /* Is this (really) default? Check it up! */
-  fontPrefs[a]->fp_DrawMode = 0; /* Is this (really) default? Check it up! */
-
-  fontPrefs[a]->fp_TextAttr.ta_YSize = 8; /* Is this (really) default? Check it up! */
-  fontPrefs[a]->fp_TextAttr.ta_Style = 0; /* Is this (really) default? Check it up! */
-  fontPrefs[a]->fp_TextAttr.ta_Flags = 0; /* Is this (really) default? Check it up! */
+  fontPrefs[a]->fp_TextAttr.ta_YSize = 8; /* Is this (really) default? Look it up! */
+  fontPrefs[a]->fp_TextAttr.ta_Style = 0; /* Is this (really) default? Look it up! */
+  fontPrefs[a]->fp_TextAttr.ta_Flags = 0; /* Is this (really) default? Look it up! */
 
   strcpy(fontPrefs[a]->fp_Name, "topaz.font"); /* Is this (really) default? Check it up! */
   fontPrefs[a]->fp_TextAttr.ta_Name = fontPrefs[a]->fp_Name;
  }
+}
+
+BOOL initPrefMem(void)
+{
+ UBYTE a;
+
+ for(a = 0; a <= 2; a++)
+  if(!(fontPrefs[a] = AllocMem(sizeof(struct FontPrefs), MEMF_ANY | MEMF_CLEAR)))
+   return(FALSE); /* Some structures may have been allocated */
+
+ initDefaultPrefs(fontPrefs);
+ 
+ return(TRUE);
 }
