3048889df7225a5b1ac6eec6d9cf0806e47e29bb
falemagn <falemagn@gmail.com>
2001-04-19 00:15:37 +0000

    Function gcvt() implemented
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10657 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/gcvt.c
@@ -0,0 +1,56 @@
+/*
+    (C) 1995-96 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: ANSI C function gcvt()
+    Lang: english
+*/
+
+#include <stdio.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <stdlib.h>
+
+	char * gcvt (
+
+/*  SYNOPSIS */
+	double	  number,
+	size_t	  ndigit,
+	char	* buf
+	)
+
+/*  FUNCTION
+	Converts a number to a minimal length NULL terminated ASCII string.
+	It produces ndigit significant digits in either printf F format or
+	E format.
+
+    INPUTS
+	number  - The number to convert.
+	ndigits - The number of significan digits that the string has to have.
+	buf     - The buffer that will contain the result string.
+
+    RESULT
+	The address of the string pointed to by buf.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	ecvt(), fcvt(), sprintf()
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+{
+    sprintf (buf, "%.*G", ndigit, number);
+
+    return buf;
+} /* sprintf */
+
