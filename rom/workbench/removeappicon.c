cda9caabb7c6963e8de3c3f740ed7a44d071b399
SDuvan <SDuvan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-04-21 15:21:31 +0000

    Check if arguments are valid; added semaphore locking; added documentation
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10699 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/workbench/removeappicon.c
+++ rom/workbench/removeappicon.c
@@ -1,9 +1,9 @@
 /*
-    (C) 1995-97 AROS - The Amiga Research OS
+    (C) 1995-2001 AROS - The Amiga Research OS
     $Id$
 
     Desc: Remove an icon from Workbench's list of AppIcons.
-    Lang: english
+    Lang: English
 */
 
 #include "workbench_intern.h"
@@ -23,17 +23,29 @@
         struct WorkbenchBase *, WorkbenchBase, 11, Workbench)
 /*  FUNCTION
 
+    Try to remove an AppIcon from workbench.library's list of AppIcons.
+
     INPUTS
+
+    appIcon  --  pointer to an AppIcon got from AddAppIconA()
 
     RESULT
 
+    TRUE if the icon could be removed, FALSE otherwise.
+
     NOTES
+
+    You must do a final check for messages on your AppMessage port as messages
+    may have been sent between the last time you checked and the call to
+    this function.
 
     EXAMPLE
 
     BUGS
 
     SEE ALSO
+
+    AddAppIcon()
 
     INTERNALS
 
@@ -44,15 +56,26 @@
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct WorkbenchBase *, WorkbenchBase)
 
-    if( appIcon ) {
-        Remove( (struct Node *) appIcon );
-        FreeVec( appIcon );
-
-        /* TODO: Notify the Workbench Apps about this. */
-        return TRUE;
+    if (appIcon == NULL)
+    {
+	return FALSE;
     }
+    
+    LockWorkbench();
+    Remove((struct Node *)appIcon);
+    UnlockWorkbench();
 
-    return FALSE;
+    FreeVec(appIcon);
+
+    /*
+      Question is if we should negotiate with the (possible) workbench
+      application. Probably not... we just remove it from the list and
+      send a message to the workbench application that the icon should
+      be removed as soon as possible. 
+
+      NotifyWorkbench(WBNOTIFY_Delete, WBNOTIFY_AppIcon, WorkbenchBase); */
+
+    return TRUE;
 
     AROS_LIBFUNC_EXIT
 } /* RemoveAppIcon */
