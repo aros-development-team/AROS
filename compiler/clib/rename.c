9994a0c65504b86588e5bc326ee53ec9b7c67455
hkiel <hkiel@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-04-27 16:25:55 +0000

    Implemented clib/rename()
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10771 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/rename.c
@@ -0,0 +1,56 @@
+/*
+    (C) 2001 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: ANSI C function rename()
+    Lang: english
+*/
+
+#include <proto/dos.h>
+#include "__errno.h"
+
+/*****************************************************************************
+
+    NAME */
+#include <stdio.h>
+
+	int rename (
+
+/*  SYNOPSIS */
+	const char * oldpath,
+	const char * newpath)
+
+/*  FUNCTION
+	Renames a file or directory.
+
+    INPUTS
+	oldpath - Complete path to existing file or directory.
+	newpath - Complete path to the new file or directory.
+
+    RESULT
+	0 on success and -1 on error. In case of an error, errno is set.
+	
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+{
+    if (!Rename ((STRPTR)oldpath,(STRPTR)newpath))
+    {
+	errno = IoErr2errno (IoErr());
+	return -1;
+    }
+
+    return 0;
+    
+} /* rename */
+
