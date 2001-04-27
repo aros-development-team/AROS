b5b1cc55567f2025211148fa68572289a83d2d21
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-04-27 16:39:46 +0000

    added documentation
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10775 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/fdopen.c
+++ compiler/clib/fdopen.c
@@ -1,4 +1,12 @@
-#include <stdio.h>
+/*
+    Copyrigth 1995-2001 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: POSIX function fdopen()
+    Lang: english
+*/
+
+
 #include <stdlib.h>
 #include <fcntl.h>
 #include <errno.h>
@@ -9,7 +17,55 @@
 #include "__stdio.h"
 #include "__open.h"
 
-FILE *fdopen(int filedes, const char *mode)
+/*****************************************************************************
+
+    NAME */
+#include <stdio.h>
+
+	FILE *fdopen (
+
+/*  SYNOPSIS */
+	int         filedes,
+	const char *mode
+	)
+
+/*  FUNCTION
+	function associates a stream with an existing file descriptor.
+
+    INPUTS
+	filedes - The descriptor the stream has to be associated with
+	mode    - The mode of the stream  (same as with fopen()) must be com­
+                  patible with the mode of the file  descriptor.   The  file
+                  position  indicator  of  the  new  stream  is  set to that
+                  belonging to fildes, and the error and end-of-file indica­
+                  tors  are cleared.  Modes "w" or "w+" do not cause trunca­
+                  tion of the file.  The file descriptor is not dup'ed,  and
+                  will  be  closed  when  the  stream  created  by fdopen is
+                  closed.
+
+    RESULT
+	NULL on error or the new stream assiciated with the descriptor.
+
+	The new descriptor returned by the call is the lowest numbered
+	descriptor currently not in use by the process.
+
+    NOTES
+        This function must not be used in a shared library or
+        in a threaded application.
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	 open(), fclose(), fileno()
+
+    INTERNALS
+
+    HISTORY
+	27.04.2001 falemagn created
+
+******************************************************************************/
 {
     int sflags, oflags;
     fdesc *fdesc;
