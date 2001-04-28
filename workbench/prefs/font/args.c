d0b1fc44125b47fd49aa706ab0fdbf5c0144c8ae
petah <petah@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-04-28 03:59:59 +0000

    This is the fourth Developer's release of "Font" Preferences, designated version 0.8. A bug introduced in "Font" 0.2 was discovered to be resident in the "getArguments()" function. The problem is fixed. Additionally, some (disabled) code has been removed altoghether from the "main.c" source module.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10777 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/prefs/font/args.c
+++ workbench/prefs/font/args.c
@@ -6,8 +6,6 @@
 
 extern IPTR argArray[NUM_ARGS];
 
-extern void quitApp(UBYTE *, UBYTE);
-
 struct RDArgs * getArguments(void)
 {
  UBYTE a;
@@ -15,14 +13,14 @@ struct RDArgs * getArguments(void)
 
  /* Let's clear the array. Do we really need this or will ReadArgs() set
  every non given argument pointer to NULL? Check it up! */
- for(a = 0; a <= NUM_ARGS; a++)
+ for(a = 0; a < NUM_ARGS; a++)
   argArray[a] = NULL;
 
  /* We need an AROS "IPTR" class pointer here! An IPTR is guaranteed to be AmigaOS "ULONG" compatible? */
 
  if(tmpReadArgs = ReadArgs("FROM,EDIT/S,USE/S,SAVE/S,PUBSCREEN/K", argArray, NULL))
  {
-  for(a = 0; a <= NUM_ARGS; a++)
+  for(a = 0; a < NUM_ARGS; a++)
   {
    if(argArray[a])
    {
