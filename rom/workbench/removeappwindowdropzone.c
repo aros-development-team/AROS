f806ead652e1f92d0d68909c4cbe5fbd6a84bbd7
SDuvan <SDuvan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-04-29 08:00:06 +0000

    Added locking and documentation; better argument checking; added IoErr() support for information about errors
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10784 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/workbench/removeappwindowdropzone.c
+++ rom/workbench/removeappwindowdropzone.c
@@ -1,9 +1,9 @@
 /*
-    (C) 1995-97 AROS - The Amiga Research OS
+    (C) 1995-2001 AROS - The Amiga Research OS
     $Id$
 
     Desc: Remove a dropzone from a AppWindow's list of AppWindowDropZones.
-    Lang: english
+    Lang: English
 */
 
 #include <exec/types.h>
@@ -31,11 +31,29 @@
 
 /*  FUNCTION
 
+    Try to remove a drop zone from an AppWindow.
+
     INPUTS
+
+    appWindow  --  pointer to the AppWindow (as returned by AddAppWindow()) to
+                   try to remove the drop zone from; a value of NULL will
+		   result in no operation
+    dropZone   --  pointer to an AppWindowDropZone (as returned by
+                   AddAppWindowDropZone()); a value of NULL will result in
+		   no operation
 
     RESULT
 
+    TRUE if the drop zone could be removed, FALSE otherwise. In case of
+    failure, the reason may be obtained from dos.library/IoErr(). This
+    function may fail if the specified drop zone is not registered with
+    the supplied AppWindow.
+   
     NOTES
+    
+    You must check for drop zone messages for zones that you just removed as
+    there might have been messages sent between the last time you checked and
+    the call to this function.
 
     EXAMPLE
 
@@ -52,16 +70,44 @@
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct WorkbenchBase *, WorkbenchBase)
 
-    if( (aw != NULL) && (dropZone != NULL) ) {
-        Remove( (struct Node *) dropZone );
-        FreeVec( dropZone );
+    struct AppWindowDropZone *dz;
 
-        /* TODO: Notify the Workbench Apps about this. */
+    BOOL found = FALSE;		/* Is the drop zone 'dropZone' added to the
+				   window 'aw'? */
 
-        return TRUE;
+    if ((aw == NULL) || (dropZone == NULL))
+    {
+	SetIoErr(ERROR_REQUIRED_ARG_MISSING);
+
+	return FALSE;
     }
 
-    return FALSE;
+    LockWorkbench();
+    ForeachNode(&aw->aw_DropZones, (struct Node *)dz)
+    {
+	if (dz == dropZone)
+	{
+	    found = TRUE;
+	    break;
+	}
+    }
+
+    if (!found)
+    {
+	UnlockWorkbench();
+	SetIoErr(ERROR_OBJECT_NOT_FOUND);
+
+	return FALSE;
+    }
+
+    Remove((struct Node *)dropZone);
+    UnlockWorkbench();
+
+    FreeVec(dropZone);
+
+    /* NotifyWorkbench(WBNOTIFY_Delete, WBNOTIFY_DropZone, WorkbenchBase); */
+	
+    return TRUE;
 
     AROS_LIBFUNC_EXIT
 } /* RemoveAppWindowDropZone */
