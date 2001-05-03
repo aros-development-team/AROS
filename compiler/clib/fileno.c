f89fe0360eb0937de053f69af994b6a7d457cfaf
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-05-03 16:56:51 +0000

    The fileno() function
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10840 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/fileno.c
@@ -0,0 +1,52 @@
+/*
+    Copyright 1995-2001 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: ANSI C function fileno()
+    Lang: english
+*/
+
+#include <errno.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <stdio.h>
+
+	int fileno (
+
+/*  SYNOPSIS */
+	FILE *stream)
+
+/*  FUNCTION
+	Returns the descriptor associated with the stream
+
+    INPUTS
+	strem - the stream to get the descriptor from
+
+    RESULT
+	The integer descriptor
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	open()
+
+    INTERNALS
+
+    HISTORY
+	3.5.2001 falemagn created
+
+******************************************************************************/
+{
+    if (!stream) /* safety check */
+    {
+    	errno = EFAULT;
+	return -1;
+    }
+    return stream->fd;
+}
