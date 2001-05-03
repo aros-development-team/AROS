1a5c224ef0a9b4ed2738d7269721b6b179843013
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-05-03 16:54:02 +0000

    added more functions
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10837 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/include/unistd.h
+++ compiler/clib/include/unistd.h
@@ -11,14 +11,17 @@
 #include <sys/types.h>
 
 /* Prototypes */
+int access (const char *path, int mode);
 int close (int fd);
 int dup(int oldfd);
 int dup2(int oldfd, int newfd);
+int lseek(int fildes, off_t offset, int whence);
 ssize_t read (int fd, void * buf, size_t count);
-ssize_t write (int fd, const void * buf, size_t count);
+#define rmdir unlink
+int truncate(const char *path, off_t length);
 int unlink(const char *pathname);
 void usleep(unsigned long usec);
-int lseek(int fildes, off_t offset, int whence);
+ssize_t write (int fd, const void * buf, size_t count);
 
 /* Standard file descriptors */
 #define STDIN_FILENO  0 /* Standard input */
@@ -32,5 +35,12 @@ int lseek(int fildes, off_t offset, int whence);
 #define SEEK_END    2
 
 #endif
+
+/* Values for the second argument to access.
+   These may be OR'd together.  */
+#define	R_OK	4		/* Test for read permission.  */
+#define	W_OK	2		/* Test for write permission.  */
+#define	X_OK	1		/* Test for execute permission.  */
+#define	F_OK	0		/* Test for existence.  */
 
 #endif /* _UNISTD_H */
