98ef29a75ca4f346a938e5db5b6e8edc56500258
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-05-04 16:54:44 +0000

    yet another semaphore: InputHandlerLock -> will be locked throught the whole time while Intuition's InputHandler is running.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10860 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/intuition/freeclass.c
+++ rom/intuition/freeclass.c
@@ -1,5 +1,5 @@
 /*
-    (C) 1995-2001 AROS - The Amiga Research OS
+    Copyright (C) 1995-2001 AROS - The Amiga Research OS
     $Id$
 
     Desc: Free a BOOPSI Class (obs)
@@ -102,9 +102,10 @@
 
 	FreeMem (classPtr, sizeof (Class));
 
-	return (TRUE);
+	return TRUE;
     }
 
+    return FALSE;
 #else
 
     /* call boopsi.library function */
