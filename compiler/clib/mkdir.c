ee1715ca462b5217c9c29a1e79785942d47c7c5a
falemagn <falemagn@gmail.com>
2001-05-04 00:18:58 +0000

    added mkdir()
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10853 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/mkdir.c
@@ -0,0 +1,72 @@
+/*
+    Copyright 2001 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: POSIX function mkdir()
+    Lang: english
+*/
+
+#include <proto/dos.h>
+#include <errno.h>
+#include "__errno.h"
+
+/*****************************************************************************
+
+    NAME */
+#include <sys/stat.h>
+
+	int mkdir (
+
+/*  SYNOPSIS */
+	const char *path,
+	mode_t      mode)
+
+/*  FUNCTION
+	 Make a directory file
+
+    INPUTS
+	path - the path of the directory being created
+	mode - the permission flags for the directory
+
+    RESULT
+	0 on success or -1 on errorr.
+
+    NOTES
+
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	chmod(),  stat(),  umask()
+
+    INTERNALS
+
+    HISTORY
+	4.5.2001 falemagn created
+
+******************************************************************************/
+
+{
+    BPTR lock;
+
+    if (!path) /*safety check */
+    {
+    	errno = EFAULT;
+	return -1;
+    }
+
+    lock = CreateDir(path);
+
+    if (!lock)
+    {
+    	errno = IoErr2errno(IoErr());
+	return -1;
+    }
+
+    UnLock(lock);
+
+    return 0;
+}
+
