3e0244873793f11ce494e7a95ee69c6a56a95dc4
hkiel <hkiel@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-05-17 18:58:24 +0000

    Implemented setbuf() and setlinebuf() as wrappers to setvbuf() which is unimplemented (just returns 0)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10891 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/setbuf.c
@@ -0,0 +1,46 @@
+/*
+    (C) 2001 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: ANSI C function setbuf()
+    Lang: english
+*/
+
+
+/*****************************************************************************
+
+    NAME */
+#include <stdio.h>
+
+	int setbuf (
+
+/*  SYNOPSIS */
+	FILE *stream,
+	char *buf)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+	This is a simpler alias for setvbuf() according to manpage.
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+{
+
+    return setvbuf(stream, buf, buf ? _IOFBF : _IONBF, BUFSIZ);
+
+} /* setbuf */
+
