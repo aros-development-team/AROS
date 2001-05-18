4b441a1f8e6883ce855622710aa5f4acd2d6361b
hkiel <hkiel@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-05-18 11:57:37 +0000

    Implemented clib/system() according to libnix (thanks Georg)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10894 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/system.c
@@ -0,0 +1,56 @@
+/*
+    (C) 2001 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: ANSI C function system()
+    Lang: English
+*/
+
+#include <utility/tagitem.h>
+#include <dos/dos.h>
+#include <proto/dos.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <stdlib.h>
+
+	int system (
+
+/*  SYNOPSIS */
+	const char *string)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+	18.05.2001 Derived from libnix
+
+******************************************************************************/
+{
+    static struct TagItem list[]={ { TAG_END,0 } }; /* No Tags */
+
+    if (string == NULL)
+    {
+	return 1;
+    }
+    else
+    {
+	return SystemTagList ((char *)string, list);
+    }
+
+} /* system */
+
