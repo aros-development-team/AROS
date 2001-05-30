2ba3ddba3f0bd9472f76f501d255b5ebb5c35f17
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-05-30 00:18:07 +0000

    Exec functions derived from the i386 code. Need some more work and also task switching with the real time clock is not a good thing. Might want to change this to use one of the two timers. Currently switches tasks at rate of 1Hz.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10933 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ config/Attic/palm-native/exec/disable.c
@@ -0,0 +1,86 @@
+/*
+    Copyright (C) 1995-1997 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Disable() - Stop interrupts from occurring.
+    Lang: english
+*/
+
+#include <exec/types.h>
+#include <exec/execbase.h>
+#include <aros/libcall.h>
+#include <registers.h>
+
+/*****************************************************************************/
+#ifndef UseExecstubs
+
+/*  NAME */
+#include <proto/exec.h>
+
+	AROS_LH0(void, Disable,
+
+/*  LOCATION */
+	struct ExecBase *, SysBase, 20, Exec)
+
+/*  FUNCTION
+	This function will prevent interrupts from occuring. You can
+	start the interrupts again with a call to Enable().
+
+	Note that calls to Disable() nest, and for every call to
+	Disable() you need a matching call to Enable().
+
+	***** WARNING *****
+
+	Using this function is considered very harmful, and it is
+	not recommended to use this function for ** ANY ** reason.
+
+	It is quite possible to either crash the system, or to prevent
+	normal activities (disk/port i/o) from occuring.
+
+	Note: As taskswitching is driven by the interrupts subsystem,
+	      this function has the side effect of disabling
+	      multitasking.
+
+    INPUTS
+
+    RESULT
+	Interrupts will be disabled AFTER this call returns.
+
+    NOTES
+	This function preserves all registers.
+
+	To prevent deadlocks calling Wait() in disabled state breaks
+	the disable - thus interrupts may happen again.
+
+    EXAMPLE
+	No you DEFINITATELY don't want to use this function.
+
+    BUGS
+	The only architecture that you can rely on the registers being
+	saved is on the Motorola mc68000 family.
+
+    SEE ALSO
+	Forbid(), Permit(), Enable(), Wait()
+
+    INTERNALS
+	This function must be replaced in the $(KERNEL) or $(ARCH)
+	directories in order to do some work.
+
+    HISTORY
+
+******************************************************************************/
+#else
+void _Exec_Disable(struct ExecBase * SysBase)
+#endif
+{
+	AROS_LIBFUNC_INIT
+
+	/* Only disable interrupts if they are not already disabled. The
+	   initial (enabled) value of IDNestCnt is -1
+	 */
+	if( SysBase->IDNestCnt++ < 0) {
+		WREG_L(IMR) = ((ULONG)RREG_L(IMR) | (ULONG)0x400010);
+	}
+	AROS_LIBFUNC_EXIT
+
+} /* Disable() */
