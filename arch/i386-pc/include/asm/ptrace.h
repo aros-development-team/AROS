3c946a75bdd1767d3f1d62b607e1df17fd4c746c
sheutlin <sheutlin@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-06-08 22:39:02 +0000

    includes for new core
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10972 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/i386-pc/include/asm/ptrace.h
@@ -0,0 +1,24 @@
+#ifndef ASM_PTRACE_H
+#define ASM_PTRACE_H
+
+/* this struct defines the way the registers are stored on the 
+   stack during a system call. */
+
+struct pt_regs {
+	int  xes;
+	int  xds;
+	long edx;
+	long ecx;
+	long eax;
+	long orig_eax;
+	long eip;
+	int  xcs;
+	long eflags;
+	long esp;
+	int  xss;
+};
+
+#define user_mode(regs) ((3 & (regs)->xcs))
+#define instruction_pointer(regs) ((regs)->eip)
+
+#endif
