b4ef048561ea3b7c0886ef9eeb9829d7ada4d3fc
sheutlin <sheutlin@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-06-08 22:39:02 +0000

    includes for new core
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10972 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/i386-pc/include/asm/segments.h
@@ -0,0 +1,15 @@
+#ifndef _ASM_SEGMENTS_H
+#define _ASM_SEGMENTS_H
+
+#undef __STR
+#undef STR
+#define __STR(x) #x
+#define STR(x) __STR(x)
+
+#define KERNEL_CS 0x08
+#define KERNEL_DS 0x10
+
+#define USER_CS   0x1b
+#define USER_DS   0x23
+
+#endif /* _ASM_SEGMENTS_H */
