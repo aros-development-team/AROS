c90d2790e538897b0929e79e4fbe25348830fcba
sheutlin <sheutlin@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-06-08 22:44:17 +0000

    part I: new files of the new core
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10975 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/i386-pc/exec/prepareexecbase.c
@@ -0,0 +1,8 @@
+/*
+    Copyright (C) 1997 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Empty file to skip rom/exec/prepareexecbase.c
+    Lang: english
+*/
+
---
+++ arch/i386-pc/exec/reschedule.c
@@ -0,0 +1,84 @@
+/*
+    Copyright (C) 1997 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: 
+    Lang: english
+*/
+
+#include <exec/types.h>
+#include <exec/execbase.h>
+#include <aros/libcall.h>
+
+void Exec_Permit_Supervisor();
+
+/*****i***********************************************************************
+
+    NAME */
+#include <proto/exec.h>
+
+	AROS_LH1(void, Reschedule,
+
+/*  SYNOPSIS */
+	AROS_LHA(struct Task *, task, A0),
+
+/*  LOCATION */
+	struct ExecBase *, SysBase, 8, Exec)
+
+/*  FUNCTION
+	Reschedule will place the task into one of Execs internal task
+	lists. Which list it is placed in will depend upon whether the
+	task is ready to run, or whether it is waiting for an external
+	event to awaken it.
+
+	It is possible that in the future, more efficient schedulers
+	will be implemented. In which case this is the function that they
+	need to implement.
+
+	You should not do any costly calculations since you will be
+	running in interupt mode.
+
+    INPUTS
+	task    -   The task to insert into the list.
+
+    RESULT
+	The task will be inserted into one of Exec's task lists.
+
+    NOTES
+	Not actually complete yet. Some of the task states don't have any
+	supplied action.
+
+    EXAMPLE
+
+    BUGS
+	Only in relation to the comments within about low-priority tasks
+	not getting any processor time.
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    UBYTE flag = SysBase->AttnResched;	/* Save state of scheduling attention */
+    
+    SysBase->AttnResched |= 0x80;	/* Set scheduling attention */
+    
+    if (SysBase->TDNestCnt < 0)		/* If task switching enabled */
+    {
+	if (SysBase->IDNestCnt < 0)	/* And interrupts enabled */
+	{
+	    Supervisor(Exec_Permit_Supervisor);
+	}
+	else if (flag & 0x80)		/* Generate software interrupt */
+	{
+	    __asm__ ("movl $-3,%eax\n\tint $0x80");
+	}
+    }
+
+    AROS_LIBFUNC_EXIT
+} /* Reschedule */
