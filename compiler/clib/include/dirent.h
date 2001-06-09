b78e3bb7d42f6e1af5b5d899c139a62bc259a073
falemagn <falemagn@gmail.com>
2001-06-09 17:18:20 +0000

    header file for the *dir() functions
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10987 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/include/dirent.h
@@ -0,0 +1,48 @@
+#ifndef _DIRENT_H_
+#define _DIRENT_H_
+/*
+    Copyright (C) 1995-2001 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: header file dirent.h
+    Lang: english
+*/
+
+#include <sys/types.h>
+
+#ifndef NULL
+#define NULL (void *)0
+#endif
+
+#ifndef NAME_MAX
+#define NAME_MAX 255
+#endif
+
+struct dirent {
+#define	MAXNAMLEN NAME_MAX
+    char d_name[MAXNAMLEN + 1];	/* name must be no longer than this */
+};
+
+/* structure describing an open directory. */
+typedef struct _dirdesc {
+   int    fd;
+   struct dirent ent;
+   void   *priv;
+} DIR;
+
+
+DIR *opendir (const char *name);
+struct dirent *readdir (DIR *dir);
+void rewinddir (DIR *dir);
+int closedir (DIR *dir);
+
+long telldir (const DIR *dir);
+void seekdir (DIR *dir, off_t offset);
+int scandir (const char *dir, struct dirent ***namelist,
+              int (*select)(const struct dirent *),
+              int (*compar)(const struct dirent **, const struct dirent **));
+
+int alphasort(const struct dirent **a, const struct dirent **b);
+ssize_t getdirentries(int fd, char *buf, size_t  nbytes, off_t *basep);
+
+#endif /* !_DIRENT_H_ */
