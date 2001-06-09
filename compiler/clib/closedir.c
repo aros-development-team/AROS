c601ee6c0fce2f991cc7844e61e4a6384759ed63
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-06-09 18:10:46 +0000

    Added documentation
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10992 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/closedir.c
+++ compiler/clib/closedir.c
@@ -1,7 +1,48 @@
+/*
+    Copyright 1995-2001 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: POSIX function close()
+    Lang: english
+*/
+
 #include <errno.h>
+
+/*****************************************************************************
+
+    NAME */
 #include <dirent.h>
 
-int closedir(DIR *dir)
+	int closedir(
+
+/*  SYNOPSIS */
+	DIR *dir)
+
+/*  FUNCTION
+	 Closes a directory
+
+    INPUTS
+	dir - the directory stream pointing to the directory being closed
+
+    RESULT
+	The  closedir()  function  returns  0  on success or -1 on
+ 	failure.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+ 	close(), opendir(),  readdir(), rewinddir(), seekdir(),
+	telldir(), scandir()
+
+    INTERNALS
+
+    HISTORY
+	09.06.2001 falemagn created
+******************************************************************************/
 {
     if (!dir)
     {
