40c2a07ec344350fd0a133a31066c20412dddb56
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-07-05 20:41:44 +0000

    If the library opening fails print an error requester in case the program has been launched from workbench otherwise print an error string on the console. The programmer can force the use of the error requester by globally defining the variable __forceerrorrequester and setting its value to a value different than 0
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11070 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/autoinit/libraries.c
+++ compiler/autoinit/libraries.c
@@ -1,19 +1,82 @@
+#include <intuition/intuition.h>
+#include <dos/dosextens.h>
+#include <proto/intuition.h>
 #include <proto/exec.h>
+#include <proto/dos.h>
 #include <aros/symbolsets.h>
+#include <stdarg.h>
+
+/*
+  Redefine this variable in your program and set
+  its value to a value different than 0 to force
+  the use of a requester in case of error
+*/
+int __forceerrorrequester __attribute__((weak)) = 0;
+
+static void showerror(char *title, char *format, ...)
+{
+    struct Process *me = (struct Process *)FindTask(0);
+
+    va_list args;
+    va_start(args, format);
+
+
+    if (me->pr_CLI && !__forceerrorrequester)
+    {
+    	if (DOSBase)
+	{
+	    VPrintf(title, NULL);
+	    VPrintf(": ", NULL);
+	    VPrintf(format, args);
+	    VPrintf("\n", NULL);
+	}
+    }
+    else
+    {
+        if (IntuitionBase)
+	{
+    	    struct EasyStruct es =
+    	    {
+		sizeof(struct EasyStruct),
+		0,
+		title,
+		format,
+		"Exit"
+	    };
+
+	    EasyRequestArgs(NULL, &es, NULL, (APTR)args);
+	}
+    }
+
+    va_end(args);
+}
 
 int set_open_libraries(struct libraryset *set[])
 {
     int n = 1;
+    struct Process *me = (struct Process *)FindTask(0);
+
     while(set[n])
     {
-	*(set[n]->baseptr) = OpenLibrary(set[n]->name, set[n]->version);
+	*(set[n]->baseptr) = OpenLibrary(set[n]->name, *set[n]->versionptr);
 	if (!*(set[n]->baseptr))
+	{
+	    showerror("Library error",
+	              "Couldn't open version %ld of library \"%s\".",
+		       *set[n]->versionptr, set[n]->name);
 	    return 20;
+	}
 
         if (set[n]->postopenfunc)
 	{
 	    int ret = set[n]->postopenfunc();
-	    if (ret) return ret;
+	    if (ret)
+	    {
+	    	showerror("Library error",
+	                  "Couldn't initialize library \"%s\".",
+		           set[n]->name);
+	        return ret;
+	    }
         }
 
         n++;
@@ -23,8 +86,9 @@ int set_open_libraries(struct libraryset *set[])
 
 void set_close_libraries(struct libraryset *set[])
 {
-    int n = 1;
-    while(set[n])
+    int	n = ((int *)set)[0];
+
+    while (n)
     {
 	if (*(set[n]->baseptr))
 	{
@@ -32,6 +96,6 @@ void set_close_libraries(struct libraryset *set[])
 	    CloseLibrary(*(set[n]->baseptr));
         }
 
-	n++;
+	n--;
     }
 }
