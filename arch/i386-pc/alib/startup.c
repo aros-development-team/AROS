0afef759db0ad4aacb6a17a09024b79ae3bf0726
falemagn <falemagn@gmail.com>
2001-07-05 14:21:20 +0000

    addess support for libautoinit.a
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11063 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-pc/alib/startup.c
+++ arch/i386-pc/alib/startup.c
@@ -1,5 +1,5 @@
 /*
-    (C) 1995-96 AROS - The Amiga Research OS
+    Copyright (C) 1995-2000 AROS - The Amiga Research OS
     $Id$
 
     Desc: Common startup code
@@ -9,12 +9,12 @@
 #include <setjmp.h>
 #include <dos/dos.h>
 #include <exec/memory.h>
+#include <workbench/startup.h>
 #include <proto/exec.h>
 #include <proto/dos.h>
 #include <aros/asmcall.h>
-#if 1
-#   include <aros/debug.h>
-#endif
+#include <aros/debug.h>
+#include <aros/symbolsets.h>
 
 #if (AROS_FLAVOUR & AROS_FLAVOUR_NATIVE)
 asm("
@@ -27,22 +27,29 @@ asm("
 
 /* Don't define symbols before the entry point. */
 extern struct ExecBase * SysBase;
+extern struct WBStartup *WBenchMsg;
 extern int main (int argc, char ** argv);
-extern APTR __startup_mempool; /* malloc() and free() */
+
 extern jmp_buf __startup_jmp_buf;
 extern LONG __startup_error;
 
+DECLARESET(INIT);
+DECLARESET(EXIT);
+DECLARESET(LIBS);
+
 /*
-    This won't work for normal AmigaOS because you can't expect SysBase to
-    be in A6. The correct way is to use *(struct ExecBase **)4 and because
-    gcc emits strings for a certain function _before_ the code the program
-    will crash immediately because the first element in the code won't be
-    valid assembler code.
+    This won't work for normal AmigaOS because you can't expect SysBase to be
+    in A6. The correct way is to use *(struct ExecBase **)4 and because gcc
+    emits strings for a certain function _before_ the code the program will
+    crash immediately because the first element in the code won't be valid
+    assembler code.
 
     970314 ldp: It will now work because of the asm-stub above.
 
 */
-#warning TODO: handle WBStartup message
+
+
+#warning TODO: reset and initialize the FPU
 #warning TODO: resident startup
 AROS_UFH3(LONG, entry,
     AROS_UFHA(char *,argstr,A0),
@@ -50,156 +57,175 @@ AROS_UFH3(LONG, entry,
     AROS_UFHA(struct ExecBase *,sysbase,A6)
 )
 {
-    char * args,
-	** argv,
-	 * ptr;
-    int    argc,
-	   argmax;
+    char * args = NULL,	** argv = NULL, * ptr = NULL;
+    int    argc, argmax;
     LONG   namlen = 64;
     int    done = 0;
-    __startup_error = RETURN_FAIL;
+    struct Process *myproc;
 
-    SysBase=*(struct ExecBase**)4UL;
+    SysBase = *(struct ExecBase**)4UL;
 
     asm("finit");
 
-    if (argsize)
+    if
+    (
+        (__startup_error = set_open_libraries(SETNAME(LIBS))) ||
+        (__startup_error = set_call_funcs(SETNAME(INIT), 1))
+    )
     {
-	/* Copy args into buffer */
-	if (!(args = AllocMem (argsize+1, MEMF_ANY)) )
-	{
-	    argv = NULL;
-	    goto error;
-	}
+	set_call_funcs(SETNAME(EXIT), -1);
+	set_close_libraries(SETNAME(LIBS));
 
-	ptr = args;
-
-	while ((*ptr++ = *argstr++));
-
-	/* Find out how many arguments we have */
-	for (argmax=1,ptr=args; *ptr; )
-	{
-	    if (*ptr == ' ' || *ptr == '\t' || *ptr == '\n')
-	    {
-		/* Skip whitespace */
-		while (*ptr && (*ptr == ' ' || *ptr == '\t' || *ptr == '\n'))
-		    ptr ++;
-	    }
-
-	    if (*ptr == '"')
-	    {
-		/* "..." argument ? */
-		argmax ++;
-
-		ptr ++;
-
-		/* Skip until next " */
-		while (*ptr && *ptr != '"')
-		    ptr ++;
-
-		if (*ptr)
-		    ptr ++;
-	    }
-	    else if (*ptr)
-	    {
-		argmax ++;
-
-		while (*ptr && *ptr != ' ' && *ptr != '\t' && *ptr != '\n')
-		    ptr ++;
-	    }
-	}
-
-	if (!(argv = AllocMem (sizeof (char *) * argmax, MEMF_CLEAR)) )
-	{
-	    goto error;
-	}
-
-	/* create argv */
-	for (argc=1,ptr=args; *ptr; )
-	{
-	    if (*ptr == ' ' || *ptr == '\t' || *ptr == '\n')
-	    {
-		/* Skip whitespace */
-		while (*ptr && (*ptr == ' ' || *ptr == '\t' || *ptr == '\n'))
-		    ptr ++;
-	    }
-
-	    if (*ptr == '"')
-	    {
-		/* "..." argument ? */
-		ptr ++;
-		argv[argc++] = ptr;
-
-		/* Skip until next " */
-		while (*ptr && *ptr != '"')
-		    ptr ++;
-
-		/* Terminate argument */
-		if (*ptr)
-		    *ptr ++ = 0;
-	    }
-	    else if (*ptr)
-	    {
-		argv[argc++] = ptr;
-
-		while (*ptr && *ptr != ' ' && *ptr != '\t' && *ptr != '\n')
-		    ptr ++;
-
-		/* Not at end of string ? Terminate arg */
-		if (*ptr)
-		    *ptr ++ = 0;
-	    }
-	}
-    }
-    else
-    {
-	argv = &args;
-	argc = 0;
+	return __startup_error;
     }
 
-    DOSBase = (struct DosLibrary *)OpenLibrary (DOSNAME, 39);
+    myproc = (struct Process *)FindTask(NULL);
 
-    /*
-     * get program name
-     */
-    do {
-	if (!(argv[0] = AllocVec(namlen, MEMF_ANY)))
-	    goto error;
-      
-        if (!(GetProgramName(argv[0], namlen)))
+    /* Do we have a CLI structure? */
+    if (myproc->pr_CLI)
+    {
+	/* Yes, started from the CLI */
+
+	if (argsize)
 	{
-	    if (IoErr() == ERROR_LINE_TOO_LONG)
+	    /* Copy args into buffer */
+	    if (!(args = AllocMem(argsize+1, MEMF_ANY)))
 	    {
-		namlen *= 2;
-		FreeVec(argv[0]);
-	    }
-	    else
-	    {
+		argv = NULL;
 		goto error;
+	    }
+
+	    ptr = args;
+	    while ((*ptr++ = *argstr++)) {}
+
+	    /* Find out how many arguments we have */
+	    for (argmax=1,ptr=args; *ptr; )
+	    {
+		if (*ptr == ' ' || *ptr == '\t' || *ptr == '\n')
+		{
+		    /* Skip whitespace */
+		    while (*ptr && (*ptr == ' ' || *ptr == '\t' || *ptr == '\n'))
+			ptr ++;
+		}
+
+		if (*ptr == '"')
+		{
+		    /* "..." argument ? */
+		    argmax ++;
+		    ptr ++;
+
+		    /* Skip until next " */
+		    while (*ptr && *ptr != '"')
+			ptr ++;
+
+		    if (*ptr)
+			ptr ++;
+		}
+		else if (*ptr)
+		{
+		    argmax ++;
+
+		    while (*ptr && *ptr != ' ' && *ptr != '\t' && *ptr != '\n')
+			ptr ++;
+		}
+	    }
+
+	    if (!(argv = AllocMem (sizeof (char *) * argmax, MEMF_CLEAR)) )
+		goto error;
+
+	    /* create argv */
+	    for (argc=1,ptr=args; *ptr; )
+	    {
+		if (*ptr == ' ' || *ptr == '\t' || *ptr == '\n')
+		{
+		    /* Skip whitespace */
+		    while (*ptr && (*ptr == ' ' || *ptr == '\t' || *ptr == '\n'))
+			ptr ++;
+		}
+
+		if (*ptr == '"')
+		{
+		    /* "..." argument ? */
+		    ptr ++;
+		    argv[argc++] = ptr;
+
+		    /* Skip until next " */
+		    while (*ptr && *ptr != '"')
+			ptr ++;
+
+		    /* Terminate argument */
+		    if (*ptr)
+			*ptr ++ = 0;
+		}
+		else if (*ptr)
+		{
+		    argv[argc++] = ptr;
+
+			while (*ptr && *ptr != ' ' && *ptr != '\t' && *ptr != '\n')
+			    ptr ++;
+
+		    /* Not at end of string ? Terminate arg */
+		    if (*ptr)
+			*ptr ++ = 0;
+		}
 	    }
 	}
 	else
 	{
-	    done = 1;
+	    argmax = 1;
+	    argc = 1;
+	    if (!(argv = AllocMem (sizeof (char *), MEMF_ANY)))
+		goto error;
 	}
-    } while (!done);
 
-#if 0
-kprintf ("arg(%d)=\"%s\", argmax=%d, argc=%d\n", argsize, argstr, argmax, argc);
-{
-int t;
-for (t=0; t<argc; t++)
-    kprintf ("argv[%d] = \"%s\"\n", t, argv[t]);
-}
+	/*
+	 * get program name
+	 */
+	do {
+	    if (!(argv[0] = AllocVec(namlen, MEMF_ANY)))
+		goto error;
+
+	    if (!(GetProgramName(argv[0], namlen)))
+	    {
+		if (IoErr() == ERROR_LINE_TOO_LONG)
+		{
+		    namlen *= 2;
+		    FreeVec(argv[0]);
+		}
+		else
+		    goto error;
+	    }
+	    else
+		done = 1;
+	} while (!done);
+
+#if 0 /* Debug argument parsing */
+
+	kprintf("arg(%d)=\"%s\", argmax=%d, argc=%d\n", argsize, argstr, argmax, argc);
+	{
+	    int t;
+	    for (t=0; t<argc; t++)
+		kprintf("argv[%d] = \"%s\"\n", t, argv[t]);
+	}
+
 #endif
 
-    if (DOSBase != NULL)
-    {
-	if (!setjmp (__startup_jmp_buf))
-	    __startup_error = main (argc, argv);
-
-	CloseLibrary((struct Library *)DOSBase);
     }
+    else
+    {
+
+	/* Workbench startup. Get WBenchMsg and pass it to main() */
+
+	WaitPort(&myproc->pr_MsgPort);
+	WBenchMsg = (struct WBStartup *)GetMsg(&myproc->pr_MsgPort);
+	argv = (char **)WBenchMsg;
+	argsize = 0;
+	argc = 0;
+    }
+
+    /* Invoke main() */
+    if (!setjmp(__startup_jmp_buf))
+	__startup_error = main (argc, argv);
 
 error:
     if (argsize)
@@ -207,25 +233,36 @@ error:
 	if (argv) {
 	    if (argv[0])
 		FreeVec(argv[0]);
-	    FreeMem (argv, sizeof (char *) * argmax);
+	   FreeMem(argv, sizeof (char *) * argmax);
 	}
 
 	if (args)
-	    FreeMem (args, argsize+1);
+	    FreeMem(args, argsize+1);
     }
 
-    if (__startup_mempool)
-	DeletePool (__startup_mempool);
+    set_call_funcs(SETNAME(EXIT), -1);
+    set_close_libraries(SETNAME(LIBS));
+
+    /* Reply startup message to Workbench.
+     * We Forbid() to avoid being UnLoadSeg()ed before we're really finished.
+     */
+    if (WBenchMsg)
+    {
+	Forbid();
+	ReplyMsg((struct Message *)WBenchMsg);
+    }
 
     return __startup_error;
 } /* entry */
 
-struct ExecBase *SysBase;
-struct DosLibrary *DOSBase;
 
-APTR __startup_mempool = NULL;
+struct ExecBase *SysBase;
+struct WBStartup *WBenchMsg;
 jmp_buf __startup_jmp_buf;
 LONG __startup_error;
+DEFINESET(INIT);
+DEFINESET(EXIT);
+DEFINESET(LIBS);
 
 /*	Stub function for GCC __main().
 
@@ -238,4 +275,3 @@ void __main(void)
 {
 /* Do nothing. */
 }
-
