9977f4a7f8f1ce32af1913afa99d83ce77b3d6f0
falemagn <falemagn@gmail.com>
2001-07-06 00:49:59 +0000

    some more libraries added
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11076 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/autoinit/layers.c
@@ -0,0 +1,4 @@
+#include <graphics/layers.h>
+#include <aros/symbolsets.h>
+
+ADD2LIBS("layers.library", 39, LIBSET_LAYERS_PRI, struct LayersBase *, LayersBase, NULL, NULL);
---
+++ compiler/autoinit/locale.c
@@ -0,0 +1,3 @@
+#include <aros/symbolsets.h>
+
+ADD2LIBS("locale.library", 39, LIBSET_LOCALE_PRI, void *, LocaleBase, NULL, NULL);
