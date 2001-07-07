788728def8357713bc746da99979d65642562e9f
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-07-07 16:48:29 +0000

    corrected some bugs
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11135 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/autoinit/expansion.c
+++ compiler/autoinit/expansion.c
@@ -1,4 +1,4 @@
 #include <libraries/expansion.h>
 #include <aros/symbolsets.h>
 
-ADD2LIBS(EXPANSIONNAME, 39, LIBSET_EXPANSION_PRI, struct Library *, ExpansionBase, NULL, NULL);
+ADD2LIBS(EXPANSIONNAME, 39, LIBSET_USER_PRI, struct ExpansionBase *, ExpansionBase, NULL, NULL);
