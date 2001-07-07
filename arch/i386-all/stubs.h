cf5782b575a5cca8e7203d6dc92caaa7b4bd93df
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-07-07 21:43:38 +0000

    moved the macro to generate the stub functions to machine.h
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11144 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-all/stubs.h
+++
@@ -1,6 +0,0 @@
-#define STUBCODE                     \
-                ".text\n\t"          \
-                ".globl %s\n\t"      \
-		"%s:\n\t"            \
-		"movl %s,%%eax\n\t"  \
-		"jmp *%d(%%eax)\n\t"
