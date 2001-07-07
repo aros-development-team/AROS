3b4d1ba6a95c5d4b3071c9dc696518fbac9921d0
hkiel <hkiel@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-07-07 13:16:04 +0000

    Added expansion.library and aros.library
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11134 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/autoinit/aros.c
@@ -0,0 +1,4 @@
+#include <aros/arosbase.h>
+#include <aros/symbolsets.h>
+
+ADD2LIBS(AROSLIBNAME, 39, LIBSET_AROSLIB_PRI, struct Library *, ArosBase, NULL, NULL);
---
+++ compiler/autoinit/expansion.c
@@ -0,0 +1,4 @@
+#include <libraries/expansion.h>
+#include <aros/symbolsets.h>
+
+ADD2LIBS(EXPANSIONNAME, 39, LIBSET_EXPANSION_PRI, struct Library *, ExpansionBase, NULL, NULL);
