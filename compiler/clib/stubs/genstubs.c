a076f3d82177da38890d69cb98e3ffeb6c9ee8d9
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-07-08 17:07:04 +0000

    chaged the way stubs are generated
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11156 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/stubs/genstubs.c
+++ compiler/clib/stubs/genstubs.c
@@ -1,3 +1,11 @@
+/*
+    Copyright 1995-2001 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: stub function generator for clib functions
+    Lang: english
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -12,82 +20,35 @@ static char *names[]=
 NULL
 };
 
-void emitstub(int n)
-{
-    char *fname;
-    FILE *f;
-
-    fname = malloc(strlen(names[n])+3);
-    if (!fname) exit(1);
-
-    strcpy(fname, names[n]);
-    strcat(fname, ".S");
-
-    f = fopen(fname, "w");
-
-    if (!f) exit(1);
-
-    fprintf(f, STUBCODE,
-	       names[n], "aroscbase",
-	       &(__AROS_GETJUMPVEC(0, n+1+LIB_RESERVED)->vec));
-    fprintf(f, "\n");
-
-    fclose(f);
-    free(fname);
-}
-
-void emitstubslist(void)
-{
-    FILE *f;
-    int n;
-
-    f = fopen("stubslist", "w");
-    if (!f) exit(1);
-
-    for (n=0; names[n]; n++)
-    	fprintf(f, "%s\n", names[n]);
-
-    fclose(f);
-}
-
-void emitdeps(char *prog)
-{
-    FILE *f;
-    int n;
-
-    f = fopen("stubsdeps", "w");
-    if (!f) exit(1);
-
-    for (n=0; names[n]; n++)
-    	fprintf(f, "%s.S :\n\t%s stubs\n\n", names[n], prog);
-
-    fclose(f);
-}
-
 int main(int argc, char *argv[])
 {
     int n;
 
     if (argc != 2)
     {
-    	fprintf(stderr, "Argument required: either 'stubs' or 'list' or 'deps'\n");
+    	fprintf(stderr, "Argument required: either '-list' or the name of the function\n");
 	return 1;
     }
 
-    if (!strcmp(argv[1], "stubs"))
-    	for (n=0; names[n]; n++)
-            emitstub(n);
-    else
-    if (!strcmp(argv[1], "list"))
-	emitstubslist();
-    else
-    if (!strcmp(argv[1], "deps"))
-	emitdeps(argv[0]);
-    else
+    if (!strcmp(argv[1], "-list"))
     {
-    	fprintf(stderr, "Argument required: either 'stubs' or 'list' or 'deps'\n");
-	return 1;
+ 	for (n=0; names[n]; n++)
+    	    printf("%s\n", names[n]);
+
+	return 0;
     }
 
-    return 0;
+    for (n=0; names[n]; n++)
+	if (!strcmp(names[n], argv[1]))
+	{
+	    printf(STUBCODE,
+	           names[n], "aroscbase",
+	           &(__AROS_GETJUMPVEC(0, n+1+LIB_RESERVED)->vec));
+
+	    return 0;
+	}
+
+    fprintf(stderr, "Invalid function name \"%s\"\n", argv[1]);
+
+    return 1;
 }
