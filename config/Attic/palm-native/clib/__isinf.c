f3b86b0b0cdefcccfd45e986b30f3fa2be974b78
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-07-15 01:35:01 +0000

    Made these files compilable.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11240 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- config/Attic/palm-native/clib/__isinf.c
+++ config/Attic/palm-native/clib/__isinf.c
@@ -6,28 +6,7 @@
     Lang: english
 */
 
-/*
- * Written by J.T. Conklin <jtc@netbsd.org>.
- * Public domain.
- */
-
-/*
- * isinf(x) returns 1 is x is inf, else 0;
- * no branching!
- */
-
-#include "__math.h"
-#include <math.h>
-
-int __isinf(double val)
+int __isinf(void)
 {
-    int hx,lx;
-
-    hx = (__HI(val)&0x7fffffff);
-    lx = __LO(val);
-    hx &= 0x7fffffff;
-    hx ^= 0x7ff00000;
-    hx |= lx;
-
-    return (hx == 0);
+	return 0;
 } /* __isinf */
