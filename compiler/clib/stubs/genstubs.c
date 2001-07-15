bb1e832820ae8d4aa0be8194733ee841735c0d3c
hkiel <hkiel@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-07-15 11:41:18 +0000

    Append missing newline at EOF of generated files.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11253 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/stubs/genstubs.c
+++ compiler/clib/stubs/genstubs.c
@@ -44,6 +44,7 @@ int main(int argc, char *argv[])
 	    printf(STUBCODE,
 	           names[n], "aroscbase",
 	           &(__AROS_GETJUMPVEC(0, n+1+LIB_RESERVED)->vec));
+	    printf("\n");
 
 	    return 0;
 	}
