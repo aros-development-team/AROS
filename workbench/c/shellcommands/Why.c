060d4ba780302ac0ca291a9cf79bc3b842faaec7
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-07-22 23:16:30 +0000

    Modified to use the new system
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11457 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/c/shellcommands/Why.c
@@ -0,0 +1,86 @@
+/*
+    (C) 1997 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Why CLI command
+    Lang: English
+*/
+
+/******************************************************************************
+
+
+    NAME
+
+        Why
+
+    SYNOPSIS
+
+    LOCATION
+
+        Workbench:C
+
+    FUNCTION
+
+        Print additional information why an operation failed. Ordinarily
+	when a command fails a breif message is printed that typically
+	includes the name of the command that failed but provides few
+	details. Why fills in details related to the failed operation.
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+
+
+#include <stdio.h>
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <dos/dos.h>
+#include <dos/dosextens.h>
+
+#include "shcommands.h"
+
+AROS_SH0(Why, 41.3)
+{
+    AROS_SHCOMMAND_INIT
+
+    struct CommandLineInterface *cli;
+    LONG lasterror;
+    int error = RETURN_OK;
+
+    if ((cli = Cli()) != NULL)
+    {
+        lasterror = cli->cli_Result2;
+
+        if (cli->cli_ReturnCode == 0 || lasterror == 0)
+	{
+            VPrintf("The last command did not set a return-value\n", NULL);
+	}
+        else
+        {
+            PrintFault(lasterror, "The last command failed, reason");
+            SetIoErr(0);
+        }
+    }
+    else
+    {
+        error = RETURN_FAIL;
+    }
+
+    return error;
+
+    AROS_SHCOMMAND_EXIT
+}
