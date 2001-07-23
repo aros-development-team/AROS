d51614d13c4f60f5637e8293f6ba5d1424be3b43
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-07-23 11:41:31 +0000

    Added FailAt. Have a look at it to see how you can implement extended help without any effort at all. Simple, huh? :)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11483 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/c/shellcommands/FailAt.c
@@ -0,0 +1,92 @@
+/*
+    Copyright (C) 1995-2000 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: FailAt - Set the failure level of a process
+    Lang: English
+*/
+
+/**************************************************************************
+
+    NAME
+	FailAt
+
+    FORMAT
+	FailAt <limit>
+
+    SYNOPSIS
+	RCLIM/N
+
+    LOCATION
+	C:
+
+    FUNCTION
+	FailAt sets the return code limit of the current shell script. If
+	any command returns with a failure code of this value or higher
+	the script shall abort.
+
+	Common failure codes are:
+	    0	- No error
+	    5	- Warning
+	    10  - Error
+	    20	- Failure
+
+	The normal value for the return code limit is 10.
+
+    EXAMPLE
+	If we have a script with the commands
+
+	    Copy RAM:SomeFile DF0:
+	    Echo "Done!"
+
+	and the file RAM:SomeFile does not exist, the Copy command will
+	return with:
+
+	    Copy: object not found
+	    Copy: returned with error code 20
+
+	and the script will abort. However if you include the command
+
+	    FailAt 21
+
+	then the script will complete since the return code from Copy is
+	less than the return code limit.
+
+**************************************************************************/
+
+#include <exec/types.h>
+#include <exec/tasks.h>
+#include <dos/dosextens.h>
+#include <dos/rdargs.h>
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <utility/tagitem.h>
+
+#include "shcommands.h"
+
+AROS_SH1H(FailAt, 41.1,             "Set the return code failure limit of the current script",
+AROS_SHAH(LONG *, , RCLIM,/M, NULL, "The new return code limit"))
+{
+    AROS_SHCOMMAND_INIT
+
+    struct CommandLineInterface *cli = Cli();
+
+    if(cli == NULL)
+	return RETURN_FAIL;
+
+    /* Write current fail level */
+    if(SHArg(RCLIM) == NULL)
+    {
+	VPrintf("Fail limit: %ld\n", (IPTR *)&cli->cli_FailLevel);
+    }
+    /* Set new fail level */
+    else
+    {
+	cli->cli_FailLevel = *SHArg(RCLIM);
+    }
+
+    return RETURN_OK;
+
+    AROS_SHCOMMAND_EXIT
+}
+	    
