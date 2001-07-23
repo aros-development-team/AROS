19f7aa10d538063cfa785fb69bbf8be3eaf307dc
falemagn <falemagn@gmail.com>
2001-07-23 20:15:16 +0000

    more commands
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11505 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/c/shellcommands/Path.c
@@ -0,0 +1,82 @@
+/*
+    (C) 1995-96 AROS - The Amiga Research OS
+    $Id$
+
+    Desc:
+    Lang:
+*/
+#include <exec/memory.h>
+#include <proto/exec.h>
+#include <dos/dosextens.h>
+#include <proto/dos.h>
+
+#include "shcommands.h"
+
+AROS_SH5(Path, 41.1,
+AROS_SHA(STRPTR *, ,PATH,/M,NULL),
+AROS_SHA(BOOL, ,ADD,/S,FALSE),
+AROS_SHA(BOOL, ,SHOW,/S,TRUE),
+AROS_SHA(BOOL, ,RESET,/S,FALSE),
+AROS_SHA(BOOL, ,QUIET,/S,FALSE))
+{
+    AROS_SHCOMMAND_INIT
+
+    UBYTE Buffer[4096];
+    IPTR parg[1];
+    STRPTR *names=SHArg(PATH);
+    BPTR *cur, *next;
+    struct CommandLineInterface *cli;
+
+    cli=Cli();
+    if (*names)
+    {
+	/* Search last entry */
+	cur=&cli->cli_CommandDir;
+	while (cur[0])
+	    cur=(BPTR *)BADDR(cur[0]);
+
+	while(*names!=NULL)
+	{
+	    next=(BPTR *)AllocVec(2*sizeof(BPTR),MEMF_ANY);
+	    next[1]=Lock(*names,SHARED_LOCK);
+	    if(!next[1])
+	    {
+		FreeVec(next);
+		break;
+	    }
+	    cur[0]=MKBADDR(next);
+	    cur=next;
+	    if(!SHArg(QUIET))
+		VPrintf("%s added.\n",(ULONG *)names);
+	    names++;
+	}
+	cur[0] = 0;
+    }
+    else
+    {
+	BPTR l;
+
+	l = Lock ("", SHARED_LOCK);
+	if (l)
+	{
+	    NameFromLock (l, Buffer, sizeof (Buffer));
+	    parg[0] = (IPTR) Buffer;
+	    VPrintf ("Current Directory: %s\n", parg);
+	    UnLock (l);
+	}
+
+	cur=(BPTR *)BADDR(cli->cli_CommandDir);
+	while(cur)
+	{
+	    NameFromLock (cur[1], Buffer, sizeof (Buffer));
+	    parg[0] = (IPTR) Buffer;
+	    VPrintf ("%s\n", parg);
+	    cur=(BPTR *)BADDR(cur[0]);
+	}
+	VPrintf ("C:\n", NULL);
+    }
+
+    return RETURN_OK;
+
+    AROS_SHCOMMAND_EXIT
+}
---
+++ workbench/c/shellcommands/Unalias.c
@@ -0,0 +1,177 @@
+/*
+    (C) 1995-97 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Unalias CLI command
+    Lang: english
+*/
+
+/*****************************************************************************
+
+    NAME
+
+        Unalias
+
+    SYNOPSIS
+
+        NAME
+
+    LOCATION
+
+        Workbench:c
+
+    FUNCTION
+
+        Removes a previously defined shell alias.
+
+        If no parameters are specified, the current list of aliases are
+        displayed.
+
+    INPUTS
+
+        NAME    - The name of the alias to unset.
+
+    RESULT
+
+        Standard DOS error codes.
+
+    NOTES
+
+    EXAMPLE
+
+        Unalias DF
+
+    BUGS
+
+    SEE ALSO
+
+        Alias
+
+    INTERNALS
+
+    HISTORY
+
+        30-Jul-1997     laguest     Initial inclusion into the AROS tree
+
+******************************************************************************/
+
+#define AROS_ALMOST_COMPATIBLE
+
+#include <proto/dos.h>
+#include <proto/exec.h>
+
+#include <dos/dos.h>
+#include <dos/dosextens.h>
+#include <dos/rdargs.h>
+#include <dos/var.h>
+#include <exec/lists.h>
+#include <exec/nodes.h>
+#include <exec/types.h>
+#include <strings.h>
+
+#include "shcommands.h"
+
+#define BUFFER_SIZE 160
+
+void GetNewString(STRPTR, STRPTR, LONG);
+
+AROS_SH1(Unalias, 41.0,
+AROS_SHA(STRPTR, ,NAME, ,NULL))
+{
+    AROS_SHCOMMAND_INIT
+
+    struct Process  * UnaliasProc;
+    struct LocalVar * UnaliasNode;
+    IPTR              OutArgs[4];
+    int               Return_Value;
+    BOOL              Success;
+    LONG              VarLength;
+    char              Buffer1[BUFFER_SIZE];
+    char              Buffer2[BUFFER_SIZE];
+
+    if (SHArg(NAME) != NULL)
+    {
+        Success = DeleteVar(SHArg(NAME),
+                             GVF_LOCAL_ONLY | LV_ALIAS
+        );
+        if (Success == FALSE)
+        {
+            PrintFault(IoErr(), "Unalias");
+	    return RETURN_WARN;
+        }
+    }
+    else
+    {
+        /* Display a list of aliases.
+         */
+        Forbid();
+        UnaliasProc = (struct Process *)FindTask(NULL);
+        Permit();
+
+        if (UnaliasProc != NULL)
+        {
+            ForeachNode((struct List *)&(UnaliasProc->pr_LocalVars),
+                        (struct Node *)UnaliasNode
+            )
+            {
+                if (UnaliasNode->lv_Node.ln_Type == LV_ALIAS)
+                {
+                    /* Get a clean variable with no excess
+                     * characters.
+                     */
+                    VarLength = -1;
+                    VarLength = GetVar(UnaliasNode->lv_Node.ln_Name,
+                                       &Buffer1[0],
+                                       BUFFER_SIZE,
+                                       GVF_LOCAL_ONLY | LV_ALIAS
+                    );
+                    if (VarLength != -1)
+                    {
+                        GetNewString(&Buffer1[0],
+                                     &Buffer2[0],
+                                     VarLength
+                        );
+
+                        Buffer2[VarLength] = NULL;
+
+                        OutArgs[0] = (IPTR)UnaliasNode->lv_Node.ln_Name;
+                        OutArgs[1] = (IPTR)&Buffer2[0];
+                        OutArgs[2] = (IPTR)NULL;
+                        VPrintf("%-20s\t%-20s\n", &OutArgs[0]);
+                    }
+                }
+            }
+        }
+    }
+
+    return RETURN_OK;
+
+    AROS_SHCOMMAND_EXIT
+
+} /* main */
+
+void GetNewString(STRPTR s, STRPTR d, LONG l)
+{
+    int i;
+    int j;
+
+    i = j = 0;
+
+    while (i < l)
+    {
+        if (s[i] == '*' || s[i] == '\e')
+        {
+            d[j] = '*';
+
+            i++;
+            j++;
+        }
+        else
+        {
+            d[j] = s[i];
+            
+            i++;
+            j++;
+        }
+    }
+} /* GetNewString */
