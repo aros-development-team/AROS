f5bc7474b13d49eb5e8fcfbdd79d5f442528b0ee
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-07-23 00:41:21 +0000

    More commands ported to the new system
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11465 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/c/shellcommands/EndIf.c
@@ -0,0 +1,73 @@
+/*
+    (C) 2000 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: 
+    Lang: English
+*/
+
+/******************************************************************************
+
+    NAME
+
+        EndIf
+
+    SYNOPSIS
+
+    LOCATION
+
+        Workbench:C
+
+    FUNCTION
+
+        Ends an If block. If the condition of the If command is false,
+	execution will skip to the corresponding EndIf command, in case there
+	isn't an Else command present.
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+        If, Else
+
+    INTERNALS
+
+    HISTORY
+
+    10.01.2000  SDuvan   implemented
+
+******************************************************************************/
+
+#include <proto/dos.h>
+#include <dos/dos.h>
+#include <dos/dosextens.h>
+#include <dos_commanderrors.h>
+
+#include "shcommands.h"
+
+AROS_SH0(EndIf,41.1)
+{
+    AROS_SHCOMMAND_INIT
+
+    struct CommandLineInterface *cli = Cli();
+
+    if((cli != NULL) && (cli->cli_CurrentInput != cli->cli_StandardInput))
+    {
+	return RETURN_OK;	/* Normal operation: do nothing! */
+    }
+    else
+    {
+	PrintFault(ERROR_SCRIPT_ONLY, "EndIf");
+	return RETURN_ERROR;
+    }
+
+    AROS_SHCOMMAND_EXIT
+}
