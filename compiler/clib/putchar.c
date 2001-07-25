ea95680f34591ed4c06110e6c5f3d86e887b2b35
iaint <iaint@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-07-25 09:35:06 +0000

    putchar() references stdout, so it needs GETUSER;
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11523 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/putchar.c
+++ compiler/clib/putchar.c
@@ -1,6 +1,14 @@
+/*
+    Copyright (C) 2001 AROS - The Amiga Research OS
+    $Id$
+
+    ISO C function putchar()
+*/
 #include <stdio.h>
 
 int putchar(int c)
 {
+    GETUSER;
+
     return putc(c, stdout);
 }
