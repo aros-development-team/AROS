1eabca02f202aee9c112a607674cb1b1b6d9f2e5
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-07-26 14:41:34 +0000

    The "Resident" command
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11539 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/c/shellcommands/Resident.c
@@ -0,0 +1,240 @@
+#define AROS_ALMOST_COMPATIBLE
+
+#include <proto/dos.h>
+#include <dos/dosextens.h>
+#include <string.h>
+#include <exec/lists.h>
+#include <exec/nodes.h>
+#include <exec/memory.h>
+
+#include <string.h>
+
+#include "shcommands.h"
+
+struct SegNode
+{
+    struct MinNode node;
+    IPTR data[2];
+};
+
+static struct SegNode *NewSegNode(struct ExecBase *SysBase, STRPTR name, LONG uc);
+
+AROS_SH7(Resident, 41.1,
+AROS_SHA(STRPTR, ,NAME, ,NULL),
+AROS_SHA(STRPTR, ,FILE, ,NULL),
+AROS_SHA(BOOL, ,REMOVE,/S,FALSE),
+AROS_SHA(BOOL, ,ADD,/S,FALSE),
+AROS_SHA(BOOL, ,REPLACE,/S,FALSE),
+AROS_SHA(BOOL,PURE=,FORCE,/S,FALSE),
+AROS_SHA(BOOL, ,SYSTEM,/S,FALSE))
+{
+    AROS_SHCOMMAND_INIT
+
+    if (SHArg(FILE) || SHArg(NAME))
+    {
+	STRPTR name, file;
+	BPTR seglist;
+	struct FileInfoBlock *fib;
+
+	if (SHArg(FILE))
+	{
+	    name = SHArg(NAME);
+	    file = SHArg(FILE);
+        }
+	else
+	{
+	    name = FilePart(SHArg(NAME));
+	    file = SHArg(NAME);
+        }
+
+	SetIoErr(0);
+	if (SHArg(REMOVE))
+	{
+	    struct Segment *found;
+
+	    Forbid();
+	    found = FindSegment(name, NULL, TRUE);
+	    if (!found)
+	    {
+	        Permit();
+		SetIoErr(ERROR_OBJECT_NOT_FOUND);
+	    }
+	    else
+	    if (!RemSegment(found))
+	    {
+	        if (found->seg_UC == CMD_INTERNAL)
+	            found->seg_UC = CMD_DISABLED;
+		else
+		    SetIoErr(ERROR_OBJECT_IN_USE);
+            }
+
+	    if (IoErr())
+	    {
+	        PrintFault(IoErr(), SHArg(NAME));
+		return RETURN_FAIL;
+	    }
+
+	    return RETURN_OK;
+	}
+
+	if (SHArg(REPLACE))
+	{
+	    struct Segment *found;
+
+	    Forbid();
+	    found = FindSegment(name, NULL, TRUE);
+	    if (found && found->seg_UC == CMD_DISABLED)
+	    {
+		found->seg_UC = CMD_INTERNAL;
+		Permit();
+		return RETURN_OK;
+	    }
+	    Permit();
+	}
+
+	if (!SHArg(ADD)) SHArg(REPLACE) = TRUE;
+
+	if (!SHArg(FORCE) && (fib = (struct FileInfoBlock *)AllocDosObject(DOS_FIB, NULL)))
+	{
+	    BPTR lock;
+
+	    if ((lock = Lock(file, SHARED_LOCK)))
+	    {
+	         if (Examine(lock, fib))
+		 {
+		     if (fib->fib_Protection & FIBF_PURE)
+		         SetIoErr(ERROR_OBJECT_WRONG_TYPE);
+		 }
+
+		 UnLock(lock);
+	    }
+
+	    FreeDosObject(DOS_FIB, fib);
+	}
+
+	if (IoErr() || !(seglist = LoadSeg(file)))
+	{
+	    PrintFault(IoErr(), file);
+	    return RETURN_FAIL;
+        }
+
+	if (SHArg(REPLACE))
+	{
+	    struct Segment *found;
+
+	    Forbid();
+	    found = FindSegment(name, NULL, FALSE);
+
+	    if (found)
+	    {
+		if (found->seg_UC != 0)
+		{
+		     Permit();
+		     PrintFault(ERROR_OBJECT_IN_USE, file);
+		     UnLoadSeg(seglist);
+		     return RETURN_FAIL;
+		}
+
+  		UnLoadSeg(found->seg_Seg);
+		found->seg_Seg = seglist;
+		Permit();
+
+		return RETURN_OK;
+	    }
+	/* Fall through */
+	}
+
+	if (!AddSegment(name, seglist, SHArg(SYSTEM)?CMD_SYSTEM:0))
+	{
+	    UnLoadSeg(seglist);
+	    PrintFault(IoErr(), "Resident");
+	    return RETURN_FAIL;
+	}
+    }
+    else
+    {
+
+	struct MinList l;
+	struct Segment *curr;
+	struct SegNode *n;
+
+	NEWLIST((struct List *)&l);
+
+	SetIoErr(0);
+	Forbid();
+	curr = (struct Segment *)BADDR(DOSBase->dl_ResList);
+	while (curr)
+	{
+ 	    n = NewSegNode(SysBase, AROS_BSTR_ADDR(curr->seg_Name), curr->seg_UC);
+
+	    if (!n)
+	    {
+	        SetIoErr(ERROR_NO_FREE_STORE);
+		break;
+	    }
+
+	    AddTail((struct List *)&l, (struct Node *)n);
+	    curr = (struct Segment *)BADDR(curr->seg_Next);
+	}
+	Permit();
+
+	if (IoErr())
+	{
+	    PrintFault(IoErr(), "Resident");
+	    return RETURN_FAIL;
+        }
+
+	PutStr("NAME                           USECOUNT\n\n");
+	while ((n = (struct SegNode *)RemHead((struct List *)&l)))
+	{
+	    if (n->data[1] == CMD_SYSTEM)
+	        VPrintf("%-30s SYSTEM\n", n->data);
+	    else
+	    if (n->data[1] == CMD_INTERNAL)
+    	        VPrintf("%-30s INTERNAL\n", n->data);
+	    else
+	    if (n->data[1] == CMD_DISABLED)
+    	        VPrintf("%-30s DISABLED\n", n->data);
+	    else
+		VPrintf("%-30s %-ld\n", n->data);
+
+	    FreeVec((APTR)n->data[0]);
+	    FreeVec(n);
+        }
+    }
+
+
+    return RETURN_OK;
+
+    AROS_SHCOMMAND_EXIT
+}
+
+static STRPTR StrDup(struct ExecBase *SysBase, STRPTR str)
+{
+    size_t len = strlen(str)+1;
+    STRPTR ret = (STRPTR) AllocVec(len, MEMF_ANY);
+
+    if (ret)
+    {
+        CopyMem(str, ret, len);
+    }
+
+    return ret;
+}
+static struct SegNode *NewSegNode(struct ExecBase *SysBase, STRPTR name, LONG uc)
+{
+    struct SegNode *sn = AllocVec(sizeof (struct SegNode), MEMF_ANY);
+    if (sn)
+    {
+        sn->data[0] = (IPTR) StrDup(SysBase, name);
+	if (sn->data[0])
+	{
+ 	    sn->data[1] = uc;
+	    return sn;
+     	}
+
+	FreeVec(sn);
+    }
+
+    return NULL;
+}
