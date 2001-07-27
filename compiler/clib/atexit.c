583c839533dad983a073d5a0891a3827bea5afd0
falemagn <falemagn@gmail.com>
2001-07-27 12:12:22 +0000

    the atexit() function
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11557 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/atexit.c
@@ -0,0 +1,53 @@
+#include <exec/lists.h>
+#include <exec/nodes.h>
+#include <aros/symbolsets.h>
+
+#include <stdlib.h>
+
+struct AtExitNode
+{
+    struct MinNode node;
+    void (*func)(void);
+};
+
+#ifndef _CLIB_KERNEL_
+static struct MinList __atexit_list;
+#endif
+
+int atexit(void (*func)(void))
+{
+    GETUSER;
+
+    struct AtExitNode *aen = malloc(sizeof(*aen));
+
+    if (!aen) return -1;
+
+    aen->func = func;
+    AddHead((struct List *)&__atexit_list, (struct Node *)aen);
+
+    return 0;
+}
+
+int __init_atexit(void)
+{
+    GETUSER;
+
+    NEWLIST((struct List *)&__atexit_list);
+
+    return 0;
+}
+
+void __exit_atexit(void)
+{
+    GETUSER;
+
+    struct AtExitNode *aen;
+
+    while ((aen = (struct AtExitNode *)RemHead((struct List *)&__atexit_list)))
+    {
+        aen->func();
+    }
+}
+
+ADD2INIT(__init_atexit, 100);
+ADD2EXIT(__exit_atexit, 100);
