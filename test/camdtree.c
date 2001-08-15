075a6b57bf2de4d6e5b20c8ea010535659161b25
ksvalast <ksvalast@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-08-15 12:14:21 +0000

    A program that prints the current camd structure of midinodes, midilinks, midiclusters and hardware-links, and how they are connected. This program may make it even more hard to understand the structure of camd, so it shoulnd't be used for that purpose. :) (the program is nice for debugging)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11666 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/camdtree.c
@@ -0,0 +1,196 @@
+
+
+#include <stdio.h>
+#include <proto/exec.h>
+#include <proto/camd.h>
+#include <midi/camd.h>
+
+#define TABSIZE 4
+
+struct Library *CamdBase=NULL;
+
+ULONG GetMidiLinkAttrs(struct MidiLink *ml, Tag tag, ...){
+	return GetMidiLinkAttrsA(ml, (struct TagItem *)&tag );
+}
+ULONG GetMidiAttrs(struct MidiNode *ml, Tag tag, ...){
+	return GetMidiAttrsA(ml, (struct TagItem *)&tag );
+}
+
+struct MidiLink *GetMidiLinkFromOwnerNode(struct MinNode *node){
+	struct MidiLink dummy;
+	return (struct MidiLink *)((char *)((char *)(node)-((char *)&dummy.ml_OwnerNode-(char *)&dummy)));
+}
+
+void printSpaces(int level){
+	int lokke;
+	for(lokke=0;lokke<level*TABSIZE;lokke++){
+		printf(" ");
+	}
+}
+
+void printLink_brancheNodes(struct MidiLink *midilink,int level,int maxlevel);
+void printLink_brancheClusters(struct MidiLink *midilink,int level,int maxlevel);
+void printCluster(struct MidiCluster *cluster,int level,int maxlevel);
+
+
+void printNode(struct MidiNode *midinode,int level,int maxlevel){
+	char *nodename=NULL;
+	struct MinNode *node;
+
+	if(level==maxlevel) return;
+
+	GetMidiAttrs(midinode,MIDI_Name,&nodename,TAG_END);
+
+	printSpaces(level);
+	printf(
+		"%x, -%s-\n",
+		midinode,
+		nodename
+	);
+
+	if(level+1==maxlevel) return;
+
+	if( ! (IsListEmpty((struct List *)&midinode->mi_OutLinks))){
+		printSpaces(level);
+		printf("  -OutLinks:\n");
+		node=midinode->mi_OutLinks.mlh_Head;
+		while(node->mln_Succ!=NULL){
+			printLink_brancheClusters(GetMidiLinkFromOwnerNode(node),level+1,maxlevel);
+			node=node->mln_Succ;
+		}
+	}
+
+	if( ! (IsListEmpty((struct List *)&midinode->mi_InLinks))){
+		printSpaces(level);
+		printf("  -InLinks:\n");
+		node=midinode->mi_InLinks.mlh_Head;
+		while(node->mln_Succ!=NULL){
+			printLink_brancheClusters(GetMidiLinkFromOwnerNode(node),level+1,maxlevel);
+			node=node->mln_Succ;
+		}
+	}
+}
+
+
+BOOL printLink(struct MidiLink *midilink){
+	char *linkname=NULL;
+
+	if(midilink->ml_Node.ln_Type==NT_USER-MLTYPE_Receiver || midilink->ml_Node.ln_Type==NT_USER-MLTYPE_Sender){
+		GetMidiLinkAttrs(midilink,MLINK_Name,&linkname,TAG_END);
+		printf(
+			"%x, -%s-\n",
+			midilink,
+			linkname
+		);
+		return TRUE;
+	}
+
+	printf("%x, <driverdata> (private)\n",midilink);
+	return FALSE;
+}
+
+void printLink_brancheNodes(struct MidiLink *midilink,int level,int maxlevel){
+	struct MidiNode *midinode;
+
+	if(level==maxlevel) return;
+
+	printSpaces(level);
+	if(printLink(midilink)==TRUE){
+		midinode=midilink->ml_MidiNode;
+		printSpaces(level);
+		printf("  -Owner (MidiNode): \n");
+		printNode(midinode,level+1,maxlevel);
+	}
+}
+
+void printLink_brancheClusters(struct MidiLink *midilink,int level,int maxlevel){
+	struct MidiCluster *cluster;
+
+	if(level==maxlevel) return;
+
+	printSpaces(level);
+	printLink(midilink);
+
+	if(level+1==maxlevel) return;
+
+	printSpaces(level);
+	printf("  -Cluster: \n");
+	printCluster(midilink->ml_Location,level+1,maxlevel);
+}
+
+
+void printCluster(struct MidiCluster *cluster,int level,int maxlevel){
+	struct MidiLink *midilink;
+
+	if(level==maxlevel) return;
+
+	printSpaces(level);
+	printf("clustername: -%s-\n",cluster->mcl_Node.ln_Name);
+
+	if(level+1==maxlevel) return;
+
+	if(!(IsListEmpty(&cluster->mcl_Receivers))){
+		printSpaces(level);
+		printf("  ");
+		printf("-Receiver links:\n");
+
+		midilink=(struct MidiLink *)cluster->mcl_Receivers.lh_Head;
+		while(midilink->ml_Node.ln_Succ!=NULL){
+			printLink_brancheNodes(midilink,level+1,maxlevel);
+			midilink=(struct MidiLink *)midilink->ml_Node.ln_Succ;
+		}
+	}
+
+	if(!(IsListEmpty(&cluster->mcl_Senders))){
+		printSpaces(level);
+		printf("  ");
+		printf("-Sender links:\n");
+		midilink=(struct MidiLink *)cluster->mcl_Senders.lh_Head;
+		while(midilink->ml_Node.ln_Succ!=NULL){
+			printLink_brancheNodes(midilink,level+1,maxlevel);
+			midilink=(struct MidiLink *)midilink->ml_Node.ln_Succ;
+		}
+	}
+}
+
+
+int main(){
+
+	APTR lock;
+	struct MidiNode *midinode;
+	struct MidiCluster *cluster;
+
+	CamdBase=OpenLibrary("camd.library",40L);
+
+	if(CamdBase!=NULL){
+
+		lock=LockCAMD(CD_Linkages);
+
+		cluster=NextCluster(NULL);
+		if(cluster==NULL){
+
+			printf("No clusters available.\n");
+
+		}else{
+
+			printf("-Clusters:\n\n");
+			do{
+				printCluster(cluster,1,6);
+				printf("\n");
+				cluster=NextCluster(cluster);
+			}while(cluster!=NULL);
+
+		}
+
+		UnlockCAMD(lock);
+		CloseLibrary(CamdBase);
+
+	}else{
+		printf("Could not open at least V40 of camd.library.\n");
+		return 1;
+	}
+
+	return 0;
+}
+
+
