c34c8bb7d51f35b937d7326180fcc15bf6293b2c
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-08-16 16:10:48 +0000

    the fcntl() function. At the moment only F_SETFL is implemented
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11673 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/fcntl.c
@@ -0,0 +1,44 @@
+#include <proto/dos.h>
+
+#include <fcntl.h>
+#include <errno.h>
+
+#include "__errno.h"
+#include "__open.h"
+
+int fcntl(int fd, int cmd, int arg)
+{
+    GETUSER;
+    fdesc *desc = __getfdesc(fd);
+
+    if (!desc)
+    {
+	errno = EBADF;
+	return -1;
+    }
+
+    switch (cmd)
+    {
+	case F_GETFL:
+	    return desc->flags & (O_NONBLOCK|O_APPEND|O_ASYNC);
+
+	case F_SETFL:
+	    {
+	        int oldmode = __oflags2amode(desc->flags & ~(O_NONBLOCK|O_APPEND|O_ASYNC));
+		arg &= (O_NONBLOCK|O_APPEND|O_ASYNC);
+
+		if (ChangeMode(CHANGE_FH, desc->fh, oldmode | __oflags2amode(arg)) == DOSTRUE)
+		{
+	            desc->flags &= ~(O_NONBLOCK|O_APPEND|O_ASYNC);
+		    desc->flags |= arg;
+		    return 0;
+		}
+
+		errno = IoErr2errno(IoErr());
+		return -1;
+	    }
+	default:
+	    errno = EINVAL;
+	    return -1;
+    }
+}
