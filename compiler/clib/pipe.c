e50a988a1400cababc330f9eca2552ab394d8a75
falemagn <falemagn@gmail.com>
2001-08-16 16:08:45 +0000

    the pipe() function. Not really useful now because there's no vfork()
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11672 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/pipe.c
@@ -0,0 +1,54 @@
+#include <proto/dos.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#include "__open.h"
+
+static int clear_nonblock_flag (int desc)
+{
+    int oldflags = fcntl(desc, F_GETFL, 0);
+
+    if (oldflags == -1)
+	return -1;
+
+    oldflags &= ~O_NONBLOCK;
+
+    return fcntl(desc, F_SETFL, oldflags);
+}
+
+int pipe(int *pipedes)
+{
+    if (!pipedes)
+    {
+	GETUSER;
+
+	errno = EFAULT;
+
+	return -1;
+    }
+
+    pipedes[0] =  open("PIPEFS://unnamedpipe//", O_RDONLY|O_NONBLOCK);
+    if (pipedes[0])
+    {
+	fdesc *desc = __getfdesc(pipedes[0]);
+        BPTR olddir = CurrentDir(desc->fh);
+
+	if
+	(
+	    clear_nonblock_flag(pipedes[0]) != -1 &&
+       	    (pipedes[1] = open("", O_WRONLY))
+	)
+	{
+      	    CurrentDir(olddir);
+
+	    return 0;
+	}
+
+	CurrentDir(olddir);
+	close(pipedes[0]);
+    }
+
+    return -1;
+}
