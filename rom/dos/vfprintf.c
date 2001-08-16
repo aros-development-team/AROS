c4ed9e7fe72751b86a752429b074a92c6e65811f
SDuvan <SDuvan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-08-16 19:20:11 +0000

    Thread through DOSBase everywhere
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11678 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/vfprintf.c
+++ rom/dos/vfprintf.c
@@ -3,7 +3,7 @@
     $Id$
 
     Desc:
-    Lang: english
+    Lang: English
 */
 #include <proto/exec.h>
 #include <dos/dosextens.h>
@@ -13,21 +13,25 @@
 #include "dos_intern.h"
 
 AROS_UFH2(void,vfp_hook,
-    AROS_UFHA(UBYTE,        chr, D0),
-    AROS_UFHA(struct vfp *, vfp, A3)
-)
+	  AROS_UFHA(UBYTE,        chr, D0),
+	  AROS_UFHA(struct vfp *, vfp, A3))
 {
     AROS_LIBFUNC_INIT
-    extern struct DosLibrary *DOSBase;
-    if(vfp->count>=0)
+
+    struct DosLibrary *DOSBase = vfp->DOSBase;
+
+    if (vfp->count >= 0 && chr != '\0')
     {
-	if(FPUTC(vfp->file,chr)<0)
+	if (FPUTC(vfp->file, chr) < 0)
 	{
-	    vfp->count=-1;
+	    vfp->count = -1;
+
 	    return;
 	}
+
 	vfp->count++;
     }
+
     AROS_LIBFUNC_EXIT
 }
 
@@ -63,8 +67,6 @@ AROS_UFH2(void,vfp_hook,
     INTERNALS
 
     HISTORY
-	29-10-95    digulla automatically created from
-			    dos_lib.fd and clib/dos_protos.h
 
 *****************************************************************************/
 {
@@ -73,21 +75,23 @@ AROS_UFH2(void,vfp_hook,
 
     struct vfp vfp;
 
-    vfp.file=file;
-    vfp.count=0;
+    vfp.file = file;
+    vfp.count = 0;
+    vfp.DOSBase = DOSBase;
 
-    (void) RawDoFmt (format,
-	argarray,
-	(VOID_FUNC)AROS_ASMSYMNAME(vfp_hook),
-	&vfp
-    );
+    (void)RawDoFmt(format, argarray,
+		   (VOID_FUNC)AROS_ASMSYMNAME(vfp_hook), &vfp);
 
     /* Remove the last character (which is a NUL character) */
-    if(vfp.count>0)
+    /*
+    if (vfp.count > 0)
     {
 	vfp.count--;
 	((struct FileHandle *)BADDR(file))->fh_Pos--;
     }
+    */
+
     return vfp.count;
+
     AROS_LIBFUNC_EXIT
 } /* VFPrintf */
