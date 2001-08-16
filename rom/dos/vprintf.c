e9d66b0378da62b9787a19717b281b5779f3e08c
SDuvan <SDuvan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-08-16 19:20:11 +0000

    Thread through DOSBase everywhere
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11678 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/vprintf.c
+++ rom/dos/vprintf.c
@@ -3,13 +3,18 @@
     $Id$
 
     Desc:
-    Lang: english
+    Lang: English
 */
+
 #include <proto/exec.h>
 #include <dos/dos.h>
+#include <aros/asmcall.h>
 #include "dos_intern.h"
 
-void vfp_hook();
+AROS_UFP2(void, vfp_hook,
+    AROS_UFPA(UBYTE,        chr, D0),
+    AROS_UFPA(struct vfp *, vfp, A3)
+);
 
 /*****************************************************************************
 
@@ -42,8 +47,6 @@ void vfp_hook();
     INTERNALS
 
     HISTORY
-	29-10-95    digulla automatically created from
-			    dos_lib.fd and clib/dos_protos.h
 
 *****************************************************************************/
 {
@@ -51,19 +54,24 @@ void vfp_hook();
     AROS_LIBBASE_EXT_DECL(struct DosLibrary *,DOSBase)
 
     struct vfp vfp;
-    BPTR file=((struct Process *)FindTask(NULL))->pr_COS;
+    BPTR       file = ((struct Process *)FindTask(NULL))->pr_COS;
 
-    vfp.file=file;
-    vfp.count=0;
+    vfp.file = file;
+    vfp.count = 0;
+    vfp.DOSBase = DOSBase;
 
-    (void)RawDoFmt(format,argarray,vfp_hook,&vfp);
+    (void)RawDoFmt(format, argarray,
+		   (VOID_FUNC)AROS_ASMSYMNAME(vfp_hook), &vfp);
 
     /* Remove the last character (which is a NUL character) */
-    if(vfp.count>0)
+    /*
+    if (vfp.count > 0)
     {
 	vfp.count--;
 	((struct FileHandle *)BADDR(file))->fh_Pos--;
     }
+    */
+
     return vfp.count;
     AROS_LIBFUNC_EXIT
 } /* VPrintf */
