b37263622f3fd95146178900df8aebaffc5ad916
falemagn <falemagn@gmail.com>
2001-08-21 14:09:59 +0000

    improved and cleaned up
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11715 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-hosted/aros.c
+++ arch/all-hosted/aros.c
@@ -8,6 +8,7 @@
 #include <dos/dostags.h>
 #include <dos/dos.h>
 #include <dos/dosextens.h>
+#include <dos/filesystem.h>
 #include <intuition/intuitionbase.h>
 #include <proto/dos.h>
 #include <proto/intuition.h>
@@ -24,64 +25,60 @@ struct IntuitionBase * IntuitionBase;
 
 int main(void)
 {
-	BPTR segs, cis = NULL, cos = NULL, ces = NULL, tmp;
-	LONG rc;
-	struct Process *me = (struct Process *)FindTask(NULL);
+    BPTR segs, cis = NULL, cos = NULL, ces = NULL, tmp;
+    LONG rc;
+    struct Process *me = (struct Process *)FindTask(NULL);
 
 
-	if ((IntuitionBase = (struct IntuitionBase *)OpenLibrary("intuition.library", 39)))
-	{
-		/* Open a new console for the initial shell */
-		if ((cis = Open("CON:20/20///Boot Shell/AUTO", MODE_READWRITE))
-/* FIXME		&& (cos = DupLock(cis)) */
-/* FIXME		&& (ces = DupLock(cis)) */
-		   )
-		{
-			/* HACK: remove this when DupLock() will work correctly */
-			cos = ces = cis;
+    /* Open a new console for the initial shell */
+    if ((cis = Open("CON:20/20///Boot Shell/AUTO", FMF_READ)))
+    {
+	BPTR olddir = CurrentDir(cis);
 
-			/* Use boot console for standard I/O. */
-			cis = SelectInput(cis);
-			cos = SelectOutput(cos);
-			tmp = me->pr_CES; me->pr_CES = ces; ces = tmp;
+	cos = Open("", FMF_WRITE);
+	ces = Open("", FMF_WRITE);
 
-			/* Load the boot shell
-			 * TODO: Switch to SystemTagList() when available
-			 */
-			if ((segs = LoadSeg("C:shell")))
-			{
-				/* Execute it. */
-				rc = RunCommand(segs, AROS_STACKSIZE, "FROM S:Startup-Sequence\n", -1);
-				UnLoadSeg(segs);
-			}
-			else
-			{
-				PutStr(CANNOT_LOAD_SHELL);
-				rc = 20;
-			}
+	CurrentDir(olddir);
+    }
 
-			/* Restore original input/output */
-			tmp = me->pr_CES; me->pr_CES = ces; ces = tmp;
-			cos = SelectOutput(cos);
-			cis = SelectInput(cis);
-		}
-		else
-		{
-			PutStr(CANNOT_OPEN_CON);
-			rc = RETURN_FAIL;
-		}
+    if (!(cis && cos && ces))
+    {
+	PutStr(CANNOT_OPEN_CON);
+	rc = RETURN_FAIL;
 
-/* FIXME	if (ces) Close(ces); */
-/* FIXME	if (cos) Close(cos); */
-		if (cis) Close(cis);
+	goto err1;
+    }
 
-		CloseLibrary((struct Library *)IntuitionBase);
-	}
-	else
-	{
-		PutStr(CANNOT_OPEN_INTUITION);
-		rc = RETURN_FAIL;
-	}
 
-	return rc;
-} /* main */
+    /* Use boot console for standard I/O. */
+    cis = SelectInput(cis);
+    cos = SelectOutput(cos);
+    tmp = me->pr_CES; me->pr_CES = ces; ces = tmp;
+
+    /* Load the boot shell
+     * TODO: Switch to SystemTagList() when available
+     */
+    if ((segs = LoadSeg("C:shell")))
+    {
+	/* Execute it. */
+	rc = RunCommand(segs, AROS_STACKSIZE, "FROM S:Startup-Sequence\n", -1);
+	UnLoadSeg(segs);
+    }
+    else
+    {
+	PutStr(CANNOT_LOAD_SHELL);
+	rc = RETURN_FAIL;
+    }
+
+    /* Restore original input/output */
+    tmp = me->pr_CES; me->pr_CES = ces; ces = tmp;
+    cos = SelectOutput(cos);
+    cis = SelectInput(cis);
+
+err1:
+    Close(cos);
+    Close(cis);
+    Close(ces);
+
+    return rc;
+}
