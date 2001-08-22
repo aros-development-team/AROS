cc11ad70413a2d5cced92fb036346ff91d9262d5
iaint <iaint@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-08-22 09:20:51 +0000

    Support for the child tasking functions. These seem to work, but cannot be globally enabled yet because of changes required to AddTask() and RemTask() that cannot be added until i386-native exec_init code is changed to create the initial task properly.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11727 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/childwait.c
+++ rom/exec/childwait.c
@@ -1,9 +1,8 @@
 /*
-    (C) 1995 AROS - The Amiga Research OS
+    Copyright (C) 1995-2001 AROS - The Amiga Research OS
     $Id$
 
-    Desc:
-    Lang: english
+    ChildWait() - Wait for a task to finish it processing.
 */
 #include "exec_intern.h"
 #include <proto/exec.h>
@@ -12,7 +11,7 @@
 
     NAME */
 
-	AROS_LH1(void, ChildWait,
+	AROS_LH1(IPTR, ChildWait,
 
 /*  SYNOPSIS */
 	AROS_LHA(APTR, tid, D0),
@@ -21,16 +20,40 @@
 	struct ExecBase *, SysBase, 126, Exec)
 
 /*  FUNCTION
+	Wait for either a specific child task, or any child task to finish.
+	If you specify tid = 0, then this call will return when any child
+	task exits, otherwise it will not return until the requested task
+	finishes.
+
+	Note that the tid is NOT the task control block (ie struct Task *),
+	rather it is the value of the ETask et_UniqueID field. Passing in a
+	Task pointer will cause your Task to deadlock.
+
+	You must call ChildFree() on the returned task before calling
+	ChildWait() again. Ie.
+
+	    struct ETask *et;
+
+	    et = ChildWait(0);
+	    ChildFree(et->et_UniqueID);
 
     INPUTS
+	tid -	The UniqueID of a task.
 
     RESULT
+	Returns either the ETask structure of the child, or one of the
+	CHILD_* values on error.
+
+	This allows you to work out which of the children has exited.
 
     NOTES
+	Calling ChildWait() on a task that isn't your child will result in
+	a deadlock.
 
     EXAMPLE
 
     BUGS
+	Be careful with the return result of this function.
 
     SEE ALSO
 
@@ -45,8 +68,45 @@
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct ExecBase *,SysBase)
 
-#warning TODO: Write exec/ChildWait()
-    aros_print_not_implemented ("ChildWait");
+    struct Task *this = FindTask(NULL);
+    struct ETask *et;
+    struct ETask *child;
+
+    /*
+	ChildWait() will wait for either a specific or any child task to
+	finish. We we get it we return the task unique id to the caller.
+
+	Firstly, are we a new-style Task?
+    */
+    if ((this->tc_Flags & TF_ETASK) == 0)
+	return CHILD_NOTNEW;
+
+    et = this->tc_UnionETask.tc_ETask;
+
+    /*
+	Scanning this list is unsafe, I need to Forbid(). Note that the
+	Wait() below will break the Forbid() condition. This is how I need
+	it to be.
+    */
+    Forbid();
+
+    /* We do not return until the condition is met */
+    for (;;)
+    {
+	/* Check if it has returned already. This will also take the first. */
+	ForeachNode(&et->et_TaskMsgPort.mp_MsgList, child)
+	{
+	    if (tid == 0 || child->et_UniqueID == tid)
+		break;
+	}
+
+	/* No matching children, we have to wait */
+	SetSignal(0, SIGF_CHILD);
+	Wait(SIGF_CHILD);
+    }
+
+    Permit();
+    return (IPTR)child;
 
     AROS_LIBFUNC_EXIT
 } /* ChildWait */
--- rom/exec/etask.h
+++ rom/exec/etask.h
@@ -2,7 +2,7 @@
 #define _ETASK_H
 
 /*
-    (C) 1995-97 AROS - The Amiga Research OS
+    Copyright (C) 1995-2001 AROS - The Amiga Research OS
     $Id$
 
     Desc: Internal description of the ETask structure
@@ -15,12 +15,16 @@
 struct IntETask
 {
     struct ETask iet_ETask;
-    APTR	 iet_RT;	   /* Structure for resource tracking */
-    APTR	 iet_Context;	   /* Structure to store CPU registers */
+#ifdef DEBUG_ETASK
+    STRPTR	 iet_Me;
+#endif
+    APTR	 iet_RT;	/* Structure for resource tracking */
+    APTR	 iet_Context;	/* Structure to store CPU registers */
     APTR         iet_AroscUserData; /* Structure to store shared clib's data */
 };
 
-#define GetIntETask(task) ((struct IntETask *)(((struct Task *) \
-			    (task))->tc_UnionETask.tc_ETask))
+#define GetIntETask(task)   ((struct IntETask *)(((struct Task *) \
+				(task))->tc_UnionETask.tc_ETask))
+#define IntETask(etask)	    ((struct IntETask *)(etask))
 
 #endif /* _ETASK_H */
