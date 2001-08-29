8765aa48f5f5bd01dbb646fa706f79c38eb1d0a9
iaint <iaint@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-08-29 08:43:32 +0000

    Make memSize = 8, so that AROS actually runs.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11822 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/.unmaintained/all-netbsd/exec/init.c
+++ arch/.unmaintained/all-netbsd/exec/init.c
@@ -92,7 +92,7 @@ static const struct Resident *romtagList[] =
 /* So we can examine the memory */
 struct MemHeader mh;
 UBYTE *memory, *space;
-int memSize = 1;
+int memSize = 8;
 
 extern void InitCore(void);
 extern struct ExecBase *PrepareExecBase(struct MemHeader *mh);
