4a9d23e56a7b6de29654f20dafd12ccf45ca65d7
falemagn <falemagn@gmail.com>
2001-09-03 14:01:51 +0000

    uses BPTR instead of void *
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11876 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/__stat.h
+++ compiler/clib/__stat.h
@@ -10,9 +10,10 @@
 */
 
 #include <sys/stat.h>
+#include <exec/types.h>
 
 mode_t __prot_a2u(ULONG protect);
 uid_t  __amiga2unixid(UWORD id);
-int    __stat(void *lock, struct stat *sb);
+int    __stat(BPTR lock, struct stat *sb);
 
 #endif
