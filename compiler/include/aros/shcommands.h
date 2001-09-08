59052b7c73623727b962eb08a304d7075d3fc934
falemagn <falemagn@gmail.com>
2001-09-08 02:42:34 +0000

    Moved these include files here, so that they're available for all the applications that wish to use them
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11925 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/aros/shcommands.h
@@ -0,0 +1,10 @@
+#ifndef SHCOMMANDS_H
+#define SHCOMMANDS_H
+
+#if USE_EMBEDDED_COMMANDS
+#    include <aros/shcommands_embedded.h>
+#else
+#    include <aros/shcommands_notembedded.h>
+#endif
+
+#endif
