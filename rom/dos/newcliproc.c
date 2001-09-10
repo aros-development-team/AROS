ff39082feb91a5d65d27269ecb71bd5a4a29c08c
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-09-10 16:55:32 +0000

    DosDoIO() was called without SysBase parameter, because dos_intern.h which has a macro DosDoIO that automatically adds SysBase param was not included.
    DosDoIO() calls were therefore replaced with
    DoIO() as CTRL-C breaking here does not make
    sense anyway.
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11931 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/newcliproc.c
+++ rom/dos/newcliproc.c
@@ -67,12 +67,12 @@ AROS_UFHA(struct ExecBase *,SysBase,A6))
 	    iofs.IOFS.io_Device  = fhin->fh_Device;
     	    iofs.IOFS.io_Unit    = fhin->fh_Unit;
 
-	    DosDoIO(&iofs.IOFS);
+	    DoIO(&iofs.IOFS);
 
 	    iofs.IOFS.io_Device  = fhout->fh_Device;
     	    iofs.IOFS.io_Unit    = fhout->fh_Unit;
 
-	    DosDoIO(&iofs.IOFS);
+	    DoIO(&iofs.IOFS);
         }
 
 	rc = RunCommand(ShellSeg, cli->cli_DefaultStack * CLI_DEFAULTSTACK_UNIT, argstr, argsize);
