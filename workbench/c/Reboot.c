0c9b288d0115c8572b06c269b345b54989222493
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-09-28 14:02:48 +0000

    Does what it says it does. Useful when you don't have Amiga keys (or when the mapping doesn't work).
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12080 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/c/Reboot.c
@@ -0,0 +1,15 @@
+/*
+    (C) 1997-2001 AROS - The Amiga Research OS
+    $Id$
+ 
+    Desc: Reboot CLI command
+    Lang: English              
+ */
+
+#include <proto/exec.h>
+
+int main() {
+    ColdReboot();
+
+    return 0;
+}
