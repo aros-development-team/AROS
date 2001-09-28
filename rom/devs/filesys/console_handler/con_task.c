d4d965d612e6e27960791c443482b91fe73045a2
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-09-28 15:52:02 +0000

    use WFLG_NOCAREREFRESH
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12084 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/devs/filesys/console_handler/con_task.c
+++ rom/devs/filesys/console_handler/con_task.c
@@ -68,10 +68,12 @@ static const struct NewWindow default_nw =
     1,				/* DetailPen */
     0,				/* BlockPen */
     0,		    	    	/* IDCMP */
-    WFLG_DEPTHGADGET |
-    WFLG_SIZEGADGET |
-    WFLG_DRAGBAR |
-    WFLG_SIZEBRIGHT |
+    WFLG_DEPTHGADGET   |
+    WFLG_SIZEGADGET    |
+    WFLG_DRAGBAR       |
+    WFLG_SIZEBRIGHT    |
+    WFLG_SMART_REFRESH |
+    WFLG_NOCAREREFRESH |
     WFLG_ACTIVATE,
     0,				/* FirstGadget */
     0,				/* CheckMark */
