52b735c6f423ade59c7c18e2ee737d83cb617a80
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-09-29 17:26:56 +0000

    turned debug output off
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12100 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/devs/ramdrive_device.c
+++ workbench/devs/ramdrive_device.c
@@ -37,7 +37,7 @@
 #include "ramdrive_device_gcc.h"
 #endif
 
-#define DEBUG 1
+#define DEBUG 0
 #include <aros/debug.h>
 
 /****************************************************************************************/
