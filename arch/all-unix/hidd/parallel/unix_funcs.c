60c496095c90676da7b4741cde173106ced10b1b
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-09-29 15:19:01 +0000

    Same changes as for serial hidd.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12098 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/all-unix/hidd/parallel/unix_funcs.c
@@ -0,0 +1,18 @@
+/* Some POSIX includes */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+
+
+/*
+ * Some wrapper functions for the unix functions.
+ * Some of the constants have to be brought in here
+ * because they clash with AROS constants otherwise.
+ */
+int unix_open_nonblock(const char * pathname)
+{
+	return open(pathname, O_NONBLOCK|O_RDWR);
+}
