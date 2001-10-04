5d7dbadb3a9fa62eaac87cba436b22bdfa923609
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-10-04 21:12:03 +0000

    added special FPU version of PrepareContext.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12182 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-pc/exec/preparecontext.c
+++ arch/i386-pc/exec/preparecontext.c
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 1995-1997 AROS - The Amiga Research OS
+    Copyright (C) 1995-2001 AROS - The Amiga Research OS
     $Id$
 
     Desc: i386unix version of PrepareContext().
@@ -19,33 +19,27 @@
 
 #define Regs(t) ((struct pt_regs *)(GetIntETask(t)->iet_Context))
 
-AROS_LH3(BOOL, PrepareContext,
-    AROS_LHA(struct Task *, task, A0),
-    AROS_LHA(APTR, entryPoint, A1),
-    AROS_LHA(APTR, fallBack, A2),
-    struct ExecBase *, SysBase, 6, Exec)
+static ULONG *PrepareContext_Common(struct Task *task, APTR entryPoint, APTR fallBack, struct ExecBase *SysBase)
 {
-    AROS_LIBFUNC_INIT
-
     ULONG *regs;
 
     UBYTE *sp=(UBYTE *)task->tc_SPReg;
 
     /* Push fallBack address */
 
-    sp-=sizeof(APTR);
-    *(APTR*)sp=fallBack;
+    sp -= sizeof(APTR);
+    *(APTR*)sp = fallBack;
     
     if (!(task->tc_Flags & TF_ETASK) )
-        return FALSE;
+        return NULL;
 
     GetIntETask (task)->iet_Context = AllocTaskMem (task
         , SIZEOF_ALL_REGISTERS
         , MEMF_PUBLIC|MEMF_CLEAR
     );
 
-    if (!(regs=(ULONG*)GetIntETask (task)->iet_Context))
-        return FALSE;
+    if (!(regs = (ULONG*)GetIntETask (task)->iet_Context))
+        return NULL;
 
     /* We have to prepare whole context right now so Dispatch()
      * would work propertly */
@@ -59,16 +53,53 @@ AROS_LH3(BOOL, PrepareContext,
     *regs++ = 0;		/* eax */
     *regs++ = USER_DS;		/* xds */
     *regs++ = USER_DS;		/* xes */
-    *regs++ = entryPoint;	/* eip */
+    *regs++ = (ULONG)entryPoint;/* eip */
     *regs++ = USER_CS;		/* xcs */
     *regs++ = 0x3202;		/* eflags */
-    *regs++ = sp;		/* esp */
+    *regs++ = (ULONG)sp;    	/* esp */
     *regs++ = USER_DS;		/* xss */
 
     task->tc_SPReg = sp;
-    
-    /* We return the new stack pointer back to the caller. */
-    return TRUE;
+
+    return regs;
+}
+
+AROS_LH3(BOOL, PrepareContext,
+    AROS_LHA(struct Task *, task, A0),
+    AROS_LHA(APTR, entryPoint, A1),
+    AROS_LHA(APTR, fallBack, A2),
+    struct ExecBase *, SysBase, 6, Exec)
+{
+    AROS_LIBFUNC_INIT
+
+    return PrepareContext_Common(task, entryPoint, fallBack, SysBase) ? TRUE : FALSE;
 
     AROS_LIBFUNC_EXIT
 }
+
+#warning This needs to be fixed/updated if the way library params are passed is changed
+
+BOOL Exec_PrepareContext_FPU(struct Task *task, APTR entryPoint, APTR fallBack, struct ExecBase *SysBase)
+{
+    ULONG *regs;
+    BOOL  retval = FALSE;
+    
+    if ((regs = PrepareContext_Common(task, entryPoint, fallBack, SysBase)))    
+    {    
+    	UBYTE this_fpustate[SIZEOF_FPU_STATE];
+	
+	asm volatile("fnsave %1\n\t"
+                     "fwait\n\t"
+                     "fninit\n\t"
+                     "fnsave %0\n\t"
+                     "fwait\n\t"
+                     "frstor %1\n\t"
+                     "fwait\n\t" : "=m" (*regs), "=m" (this_fpustate) : : "memory");
+	
+	retval = TRUE;
+    }
+    
+    return retval;
+
+}
+
