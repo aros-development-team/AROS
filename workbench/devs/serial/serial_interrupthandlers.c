f5d51d3568f31c5d66c5448a88cd4ddb6b08a7e5
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-10-11 02:07:46 +0000

    Does not end up in an endless loop anymore when the general input buffer is full.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12276 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/devs/serial/serial_interrupthandlers.c
+++ workbench/devs/serial/serial_interrupthandlers.c
@@ -18,122 +18,109 @@ extern struct serialbase * pubSerialBase;
 
 ULONG RBF_InterruptHandler(UBYTE * data, ULONG length, ULONG unitnum, APTR userdata)
 {
-  struct SerialUnit * SU = NULL;
-  ULONG index = 0;
+	struct SerialUnit * SU = NULL;
+	ULONG index = 0;
 
-  D(bug("!Received %d bytes on unit %d\n",length,unitnum));
+	D(bug("!Received %d bytes on unit %d\n",length,unitnum));
 
-  SU = findUnit(pubSerialBase, unitnum);
+	SU = findUnit(pubSerialBase, unitnum);
     
-  if (NULL != SU)
-  {
-    if (0 != (SU->su_Status & STATUS_READS_PENDING))
-    {
-      struct IOStdReq * ioreq;
-      ioreq = (struct IOStdReq *)SU->su_ActiveRead;
+	if (NULL != SU) {
+		if (0 != (SU->su_Status & STATUS_READS_PENDING)) {
+			struct IOStdReq * ioreq;
+			ioreq = (struct IOStdReq *)SU->su_ActiveRead;
 
-      if (NULL == ioreq)
-      {
-        D(bug("\t\tpre  lh_Head: %p\n",SU->su_QReadCommandPort.mp_MsgList.lh_Head));
-        ioreq = (struct IOStdReq *)GetMsg(&SU->su_QReadCommandPort);
-        D(bug("\t\tpost lh_Head: %p\n",SU->su_QReadCommandPort.mp_MsgList.lh_Head));
-        SU->su_ActiveRead = (struct Message *)ioreq;
-      }
+			if (NULL == ioreq) {
+				D(bug("\t\tpre  lh_Head: %p\n",SU->su_QReadCommandPort.mp_MsgList.lh_Head));
+				ioreq = (struct IOStdReq *)GetMsg(&SU->su_QReadCommandPort);
+				D(bug("\t\tpost lh_Head: %p\n",SU->su_QReadCommandPort.mp_MsgList.lh_Head));
+				SU->su_ActiveRead = (struct Message *)ioreq;
+			}
       
-      while (NULL != ioreq)
-      {
-        /*
-        ** Copy the remaining data into a request buffer.
-        ** This loop woll possibly execute several times
-        */
-        UBYTE * destBuf;
-        UWORD indexDestBuf;
-        D(bug("Have a IORequest (%p) for Serial device!\n",ioreq));
+			while (NULL != ioreq) {
+				/*
+				** Copy the remaining data into a request buffer.
+				** This loop woll possibly execute several times
+				*/
+				UBYTE * destBuf;
+				UWORD indexDestBuf;
+				D(bug("Have a IORequest (%p) for Serial device!\n",ioreq));
         
-        destBuf = ioreq->io_Data;
-        indexDestBuf = ioreq->io_Actual;
-        /*
-        ** I copy as many bytes as I can into this request
-        */
-        while (index < length)
-        {
-          destBuf[indexDestBuf] = data[index];
+				destBuf = ioreq->io_Data;
+				indexDestBuf = ioreq->io_Actual;
+				/*
+				** I copy as many bytes as I can into this request
+				*/
+				while (index < length) {
+					destBuf[indexDestBuf] = data[index];
             
-          index++;
-          indexDestBuf++;
+					index++;
+					indexDestBuf++;
 
-          D(bug("io_Length %d:  io_Actual: %d\n",ioreq->io_Length,indexDestBuf));
+					D(bug("io_Length %d:  io_Actual: %d\n",ioreq->io_Length,indexDestBuf));
 
-          if ((-1 == ioreq->io_Length && 0 == destBuf[indexDestBuf-1]) ||
-              (indexDestBuf >= ioreq->io_Length))
-          {
-            /*
-            ** this request is done, I answer the message
-            */
-            ioreq->io_Actual = indexDestBuf;
-            ReplyMsg((struct Message *)ioreq);
+					if ((-1 == ioreq->io_Length && 0 == destBuf[indexDestBuf-1]) ||
+					    (indexDestBuf >= ioreq->io_Length)) {
+						/*
+						** this request is done, I answer the message
+						*/
+						ioreq->io_Actual = indexDestBuf;
+						ReplyMsg((struct Message *)ioreq);
               
-            /*
-            ** Get the next request ...
-            */
-            D(bug("\t\tpre  lh_Head: %p\n",SU->su_QReadCommandPort.mp_MsgList.lh_Head));
-            ioreq = (struct IOStdReq *)GetMsg(&SU->su_QReadCommandPort);
-            D(bug("\t\tpost lh_Head: %p\n",SU->su_QReadCommandPort.mp_MsgList.lh_Head));
-            D(bug("\t\tGot new ioreq (%p) from queue\n",ioreq));
-            SU->su_ActiveRead = (struct Message *)ioreq;
-            break;    
-          }
-        }
+						/*
+						** Get the next request ...
+						*/
+						D(bug("\t\tpre  lh_Head: %p\n",SU->su_QReadCommandPort.mp_MsgList.lh_Head));
+						ioreq = (struct IOStdReq *)GetMsg(&SU->su_QReadCommandPort);
+						D(bug("\t\tpost lh_Head: %p\n",SU->su_QReadCommandPort.mp_MsgList.lh_Head));
+						D(bug("\t\tGot new ioreq (%p) from queue\n",ioreq));
+						SU->su_ActiveRead = (struct Message *)ioreq;
+						break;    
+					}
+				}
         
-        if (index == length && NULL != ioreq)
-        {
-          ioreq->io_Actual = indexDestBuf;
-          break;
-        }
-      }
-      if (NULL == ioreq)
-        SU->su_Status &= ~STATUS_READS_PENDING;
+				if (index == length && NULL != ioreq) {
+					ioreq->io_Actual = indexDestBuf;
+					break;
+				}
+			}
+			if (NULL == ioreq)
+				SU->su_Status &= ~STATUS_READS_PENDING;
       
-    }
-  } /* if (NULL != su) */   
+		}
+	} /* if (NULL != su) */	 
 
-  if (index < length)
-  {
-    /*
-    ** there's no more IORequest, so I have to copy into the
-    ** genearal buffer
-    */
+	if (index < length) {
+		/*
+		** there's no more IORequest, so I have to copy into the
+		** genearal buffer
+		*/
  
-    D(bug("Copying data into general buffer\n"));
+		D(bug("Copying data into general buffer\n"));
 
-    SU->su_Status &= ~STATUS_READS_PENDING;
-    while (index < length)
-    {
-      if (0 == (SU->su_Status & STATUS_BUFFEROVERFLOW))
-      {
-        UWORD tmp = (SU->su_InputNextPos + 1) % SU->su_InBufLength;
-        SU->su_InputBuffer[SU->su_InputNextPos] = data[index];
-        index++;
+		SU->su_Status &= ~STATUS_READS_PENDING;
+		while (index < length) {
+			if (0 == (SU->su_Status & STATUS_BUFFEROVERFLOW)) {
+				UWORD tmp = (SU->su_InputNextPos + 1) % SU->su_InBufLength;
+				SU->su_InputBuffer[SU->su_InputNextPos] = data[index];
+				index++;
 
-        /*
-        ** I am advancing the circular index su_InputNextPos
-        */
-        if (tmp != SU->su_InputFirst)
-        {  
-          SU->su_InputNextPos = tmp;
+				/*
+				** I am advancing the circular index su_InputNextPos
+				*/
+				if (tmp != SU->su_InputFirst) {	
+					SU->su_InputNextPos = tmp;
 D(bug("%d %d %d\n",SU->su_InputNextPos,SU->su_InBufLength,tmp));
-        }
-        else
-        {
-          SU->su_Status |= STATUS_BUFFEROVERFLOW;
-          break;
-        }
+				} else {
+					SU->su_Status |= STATUS_BUFFEROVERFLOW;
+					break;
+				}
             
-      }
-    }
-  }
-  
-  return length;
+			} else 
+				break;
+		}
+	}
+	return length;
 }
 
 /*
@@ -145,7 +132,7 @@ ULONG WBE_InterruptHandler( ULONG unitnum, APTR userdata)
 	struct SerialUnit * SU;
 
 	SU = findUnit(pubSerialBase, unitnum);
-    
+
 	if (NULL != SU) {
 		/*
 		 * First get any active write 
