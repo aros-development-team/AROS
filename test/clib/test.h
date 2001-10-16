047d662b3427a2bd8b88c55fd3e8a680dc961472
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-10-16 12:05:00 +0000

    A simple framework for writing tests. See stpblk.c and strchr.c for examples on how to use it.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12338 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/clib/test.h
@@ -0,0 +1,14 @@
+/* Prototypes for mandatory functions */
+void cleanup( void );
+
+/* Return values */
+#define OK   0	/* All tests succeded */
+#define FAIL 5  /* Some of the tests failed */
+
+/* Testing macro */
+#define TEST(x) if( !x )                                                           \
+    	    	{                                                                  \
+                    printf( "Test FAILED in %s, line %d.\n", __FILE__, __LINE__ ); \
+		    cleanup();                                                     \
+		    return FAIL;                                                   \
+		}
