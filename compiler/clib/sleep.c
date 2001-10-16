f5c7e37627be55c8229a10b1b2621dd9f118a056
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-10-16 22:23:54 +0000

    Implemented sleep(). See BUGS though (not important until we have POSIX style signalling).
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12350 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/sleep.c
@@ -0,0 +1,56 @@
+/*
+    (C) 1995-96 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Sleeps for a specified number of seconds.
+    Lang: english
+*/
+#include <proto/dos.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <unistd.h>
+
+	unsigned int sleep (
+
+/*  SYNOPSIS */
+	unsigned int seconds )
+
+/*  FUNCTION
+	The sleep() function makes the current process sleep for the
+	specified number of seconds or until a signal arrives which
+	is not ignored.
+
+    INPUTS
+	seconds - The number of seconds to sleep
+
+    RESULT
+    	Zero if the requested time has elapsed, or the number of seconds
+	left to sleep when the process was signalled.
+
+    NOTES
+
+    EXAMPLE
+    	// Sleep for 10 seconds
+	sleep( 10 );
+	
+    BUGS
+	The current implementation simply uses the dos.library function
+	Delay() to sleep, and cannot be interrupted by incoming signals.
+	This shouldn't be of any importance, since AROS doesn't have 
+	POSIX style signalling yet (but when it is implemented, this
+	function needs to be changed). 
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+{
+    Delay( seconds * 50 );
+    
+    return 0;
+} /* sleep */
