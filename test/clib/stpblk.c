3a4960c65872fc2f66ccc9cd902c67c0080bfd99
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-10-16 12:05:37 +0000

    Implemented test for the stpblk() function.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12339 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/clib/stpblk.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include "test.h"
+
+int main() 
+{
+    char *hello = "  Hello";
+    char *empty = "       ";
+    
+    TEST( strcmp( "Hello", stpblk( hello ) ) == 0 );
+    TEST( strcmp( "",      stpblk( empty ) ) == 0 );
+    TEST( strlen( stpblk( hello ) ) == 5 );
+    TEST( strlen( stpblk( empty ) ) == 0 );
+    
+    return OK;
+}
+
+void cleanup() 
+{
+    /* Nothing to clean up */
+}
