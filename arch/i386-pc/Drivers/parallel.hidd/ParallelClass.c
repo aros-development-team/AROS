ecea90416c62d2605828c5ee7779261f7d819afd
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-10-20 21:12:55 +0000

    Parallel hidd for i386-native. Most likely does not work. No mmakefile so far.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12401 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/i386-pc/Drivers/parallel.hidd/ParallelClass.c
@@ -0,0 +1,222 @@
+/*
+    (C) 1998 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Parallel hidd class implementation.
+    Lang: english
+*/
+
+#include <proto/exec.h>
+#include <proto/utility.h>
+#include <proto/oop.h>
+#include <exec/libraries.h>
+
+#include <utility/tagitem.h>
+#include <hidd/parallel.h>
+
+
+#include "parallel_intern.h"
+
+#undef  SDEBUG
+#undef  DEBUG
+#define SDEBUG 1
+#define DEBUG 1
+#include <aros/debug.h>
+
+
+/*static AttrBase HiddGCAttrBase;*/
+
+static OOP_AttrBase HiddParallelUnitAB;
+
+static struct OOP_ABDescr attrbases[] =
+{
+    { IID_Hidd_ParallelUnit, &HiddParallelUnitAB },
+    { NULL,	NULL }
+};
+
+/*** HIDDParallel::NewUnit() *********************************************************/
+
+
+static OOP_Object *hiddparallel_newunit(OOP_Class *cl, OOP_Object *obj,
+					struct pHidd_Parallel_NewUnit *msg)
+{
+	OOP_Object *su = NULL;
+	struct HIDDParallelData * data = OOP_INST_DATA(cl, obj);
+	ULONG unitnum = -1;
+	
+	EnterFunc(bug("HIDDParallel::NewParallel()\n"));
+    
+	D(bug("Request for unit number %d\n",msg->unitnum));
+    
+	switch (msg->unitnum) {
+		case 0:
+		case 1:
+		case 2:
+			unitnum = msg->unitnum;
+
+			if (0 != (data->usedunits & (1 << unitnum))) {
+				unitnum = -1;
+			}
+	
+		break;
+	
+		case -1: /* search for the next available unit */
+			unitnum = 0;
+
+			while (unitnum < PAR_MAX_UNITS) {
+				if (0 == (data->usedunits & (1 << unitnum))) {
+					break;
+				}
+	    
+				unitnum++;
+			}
+		break;
+	
+	
+	}
+    
+	if (unitnum >= 0 && unitnum < PAR_MAX_UNITS) {
+		struct TagItem tags[] =
+		{
+			{aHidd_ParallelUnit_Unit, unitnum},
+			{TAG_DONE		       }
+		};
+
+		su = OOP_NewObject(NULL, CLID_Hidd_ParallelUnit, tags);
+		data->ParallelUnits[unitnum] = su;
+	
+		/*
+		** Mark it as used
+		*/
+		data->usedunits |= (1 << unitnum); 
+	}
+    
+	ReturnPtr("HIDDParallel::NewParallel", OOP_Object *, su);
+}
+
+
+/*** HIDDParallel::DisposeUnit() ****************************************************/
+
+static VOID hiddparallel_disposeunit(OOP_Class *cl, OOP_Object *obj,
+				     struct pHidd_Parallel_DisposeUnit *msg)
+{
+	OOP_Object * pu = msg->unit;
+	struct HIDDParallelData * data = OOP_INST_DATA(cl, obj);
+
+	EnterFunc(bug("HIDDParallel::DisposeUnit()\n"));
+    
+	if(pu) {
+		ULONG unitnum = 0;
+
+		while (unitnum < PAR_MAX_UNITS) {
+			if (data->ParallelUnits[unitnum] == pu) {
+				D(bug("Disposing ParallelUnit!\n"));
+				OOP_DisposeObject(pu);
+				data->ParallelUnits[unitnum] = NULL;
+				data->usedunits &= ~(1 << unitnum);
+				break;
+			}
+
+			unitnum++;
+		}
+	}
+
+	ReturnVoid("HIDDParallel::DisposeUnit");
+}
+
+
+
+/*************************** Classes *****************************/
+
+#undef OOPBase
+#undef SysBase
+#undef UtilityBase
+
+#define SysBase     (csd->sysbase)
+#define OOPBase     (csd->oopbase)
+#define UtilityBase (csd->utilitybase)
+
+#define NUM_PARALLELHIDD_METHODS 2
+
+OOP_Class *init_parallelhiddclass (struct class_static_data *csd)
+{
+	OOP_Class *cl = NULL;
+	BOOL  ok  = FALSE;
+    
+	struct OOP_MethodDescr parallelhidd_descr[NUM_PARALLELHIDD_METHODS + 1] = 
+	{
+        {(IPTR (*)())hiddparallel_newunit,     moHidd_Parallel_NewUnit},
+		{(IPTR (*)())hiddparallel_disposeunit, moHidd_Parallel_DisposeUnit},
+		{NULL, 0UL}
+	};
+    
+    
+	struct OOP_InterfaceDescr ifdescr[] =
+	{
+		{parallelhidd_descr, IID_Hidd_Parallel, NUM_PARALLELHIDD_METHODS},
+		{NULL, NULL, 0}
+	};
+	
+	OOP_AttrBase MetaAttrBase = OOP_GetAttrBase(IID_Meta);
+	    
+	struct TagItem tags[] =
+	{
+		{ aMeta_SuperID,                (IPTR)CLID_Root},
+
+		{ aMeta_InterfaceDescr,         (IPTR)ifdescr},
+		{ aMeta_ID,                     (IPTR)CLID_Hidd_Parallel},
+		{ aMeta_InstSize,               (IPTR)sizeof (struct HIDDParallelData) },
+		{TAG_DONE, 0UL}
+	};
+    
+
+	EnterFunc(bug("    init_parallelhiddclass(csd=%p)\n", csd));
+
+	cl = OOP_NewObject(NULL, CLID_HiddMeta, tags);
+	D(bug("Class=%p\n", cl));
+
+	if(cl) {
+		D(bug("ParallelHiddClass ok\n"));
+		cl->UserData = (APTR)csd;
+	
+		csd->parallelunitclass = init_parallelunitclass(csd);
+		D(bug("parallelunitclass: %p\n", csd->parallelunitclass));
+
+		if(csd->parallelunitclass) {
+ 			if (OOP_ObtainAttrBases(attrbases)) {
+				D(bug("ParallelUnitClass ok\n"));
+		
+				ok = TRUE;
+			}
+		}
+	}
+    
+	if(ok == FALSE) {
+		free_parallelhiddclass(csd);
+		cl = NULL;
+	} else {
+		OOP_AddClass(cl);
+	}
+    
+	ReturnPtr("init_parallelhiddclass", OOP_Class *, cl);
+}
+
+
+void free_parallelhiddclass(struct class_static_data *csd)
+{
+	EnterFunc(bug("free_parallelhiddclass(csd=%p)\n", csd));
+
+	if(csd) {
+		OOP_RemoveClass(csd->parallelhiddclass);
+	
+		free_parallelunitclass(csd);
+
+		if(csd->parallelhiddclass) {
+			OOP_DisposeObject((OOP_Object *) csd->parallelhiddclass);
+		}
+	
+		csd->parallelhiddclass = NULL;
+	}
+	
+	ReturnVoid("free_parallelhiddclass");
+}
---
+++ arch/i386-pc/Drivers/parallel.hidd/parallel_intern.h
@@ -0,0 +1,122 @@
+#ifndef PARALLEL_HIDD_INTERN_H
+#define PARALLEL_HIDD_INTERN_H
+
+/* Include files */
+
+#include <sys/termios.h>
+
+#ifndef EXEC_LIBRARIES_H
+#   include <exec/libraries.h>
+#endif
+#ifndef OOP_OOP_H
+#   include <oop/oop.h>
+#endif
+#ifndef HIDD_PARALLEL_H
+#   include <hidd/parallel.h>
+#endif
+#include <dos/dos.h>
+
+#define PAR_MAX_UNITS	3
+
+struct HIDDParallelData
+{
+    OOP_Class *ParallelHIDDClass;
+
+    OOP_Object		*ParallelUnits[PAR_MAX_UNITS];
+    UBYTE		usedunits;
+};
+
+
+#define PAR_UNIT_0_F	1
+#define PAR_UNIT_1_F	2
+#define PAR_UNIT_2_F	4
+
+
+struct class_static_data
+{
+    struct ExecBase      * sysbase;
+    struct Library       * utilitybase;
+    struct Library       * oopbase;
+
+    OOP_Class		 *parallelhiddclass;
+    OOP_Class		 *parallelunitclass;
+};
+
+struct HIDDParallelUnitData
+{
+    VOID (*DataWriteCallBack)	(ULONG unitnum, APTR userdata);
+    VOID (*DataReceivedCallBack)(UBYTE *buffer, ULONG len, ULONG unitnum, APTR userdata);
+    VOID		*DataWriteUserData;
+    VOID		*DataReceivedUserData;
+    
+    ULONG 		unitnum;
+    int			filedescriptor;
+
+    BOOL                stopped;
+    
+    struct MsgPort	*replyport_read;
+    struct Interrupt 	*softint_read;
+    HIDD		unixio_read;
+    
+    struct MsgPort	*replyport_write;
+    struct Interrupt 	*softint_write;
+    HIDD		unixio_write;
+    
+};
+
+
+/* Library base */
+
+struct IntHIDDParallelBase
+{
+    struct Library            hdg_LibNode;
+    BPTR                      hdg_SegList;
+    struct ExecBase          *hdg_SysBase;
+    struct Library           *hdg_UtilityBase;
+
+    struct class_static_data *hdg_csd;
+};
+
+
+#define CSD(x) ((struct class_static_data *)x)
+
+#undef SysBase
+#define SysBase (CSD(cl->UserData)->sysbase)
+
+#undef UtilityBase
+#define UtilityBase (CSD(cl->UserData)->utilitybase)
+
+#undef OOPBase
+#define OOPBase (CSD(cl->UserData)->oopbase)
+
+
+/* pre declarations */
+
+OOP_Class *init_parallelhiddclass(struct class_static_data *csd);
+void   free_parallelhiddclass(struct class_static_data *csd);
+
+OOP_Class *init_parallelunitclass(struct class_static_data *csd);
+void   free_parallelunitclass(struct class_static_data *csd);
+
+/*
+ * Define some names for the registers
+ */
+#define PAR_DATA       0       /* In/Out: Parallel Data *
+#define PAR_SP         1       /* In: Status Port */
+#define PAR_PCP        2       /* In/Out: Parallel Control Port */
+
+#define PAR_SP_BUSY       0x80
+#define PAR_SP_ACK        0x40
+#define PAR_SP_PE         0x20
+#define PAR_SP_SLCT       0x10
+#define PAR_SP_ERROR      0x08
+#define PAR_SP_IRQ_STATUS 0x04
+
+#define PAR_PCP_DIRECTION   0x20   /* Mask for direction bit */
+#define PAR_PCP_IRQ_EN      0x10   /* enables the parallel port irq */
+#define PAR_PCP_SLCT_IN     0x08   /* control fr select in signal */
+#define PAR_PCP_INIT        0x04   /* initialize printer signal */
+#define PAR_PCP_AUTO_FD_XT  0x02   /* automatic feed xt */
+#define PAR_PCP_STROBE      0x01   /* strobe signal */
+
+#endif /* PARALLEL_HIDD_INTERN_H */
