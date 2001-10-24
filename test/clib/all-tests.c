24f6b092a09d3b1c2bbee81bdd3d62a5491e1acf
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-10-24 12:36:33 +0000

    Completed the testing framework. After having written a new test program using the framework (ie. test.h), add it to the list at the top of all-tests.c. Then run all-tests, which in turns runs all tests, summarizes the results and prints any eventual error messages.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12459 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/clib/all-tests.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "test.h"
+
+char *tests[] =
+{
+    "strchr",
+    "stpblk",
+    
+     NULL
+};
+
+int main() {
+    int total = 0, failed = 0;
+    int i, rc;
+    char buffer[128];
+
+    mkdir( "T:TestOutput" );
+
+    for( i = 0; tests[i] != NULL; i++ )
+    {
+    	total++;
+	sprintf( buffer, "%s >T:TestOutput/test-%d.log", tests[i], i );
+	
+	rc = system( buffer );
+	
+	if( rc == OK ) 
+	{
+	    printf( "." );
+	    fflush( stdout );
+	} 
+	else 
+	{
+	    failed++;
+	    printf( "F" );
+	    fflush( stdout );
+	}
+	    
+    }
+    
+    printf( "\n\nA total of %d tests run: %d succeded, %d failed.\n", total, total - failed, failed );  
+    
+    if( failed > 0 ) 
+    {
+    	printf( "\nError messages:\n" );
+    	system( "join T:TestOutput/test-#? as T:TestOutput/all-tests.log" );
+	system( "type T:TestOutput/all-tests.log" );
+    }
+    
+    system( "delete T:TestOutput ALL QUIET" );
+}
