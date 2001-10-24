0439264419a27ec863849b51491eb28cbc34b196
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-10-24 16:52:31 +0000

    Implemented chdir().
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12467 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/chdir.c
@@ -0,0 +1,104 @@
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved. 
+    $Id$
+
+    Desc: ANSI C function chdir()
+    Lang: English
+*/
+
+#include <exec/types.h>
+#include <dos/dos.h>
+#include <proto/dos.h>
+#include <aros/symbolsets.h>
+#include <errno.h>
+
+#ifndef _CLIB_KERNEL_
+static BOOL __startup_cd_changed;  /* Need this since NULL is a valid lock */
+static BPTR __startup_cd_lock;     
+#endif
+
+/*****************************************************************************
+
+    NAME */
+#include <unistd.h>
+
+	int chdir(
+
+/*  SYNOPSIS */
+	const char *path )
+
+/*  FUNCTION
+	Change the current working directory to the one specified by path.
+
+    INPUTS
+    	path - Path of the directory to change to.
+	
+    RESULT
+	If the current directory was changed successfully, zero is returned.	
+    	Otherwise, -1 is returned and errno set apropriately.
+	
+    NOTES
+    	At program exit, the current working directory will be changed back
+	to the one that was current when the program first started. If you
+	do not desire this behavious, use dos.library/CurrentDir() instead.
+			
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+{
+    GETUSER;
+
+    BPTR oldlock;
+    BPTR newlock = Lock( path, SHARED_LOCK );
+    
+    if( newlock == NULL )
+    {
+    	errno = IoErr2errno( IoErr() );
+	return -1;
+    }
+    
+    oldlock = CurrentDir( newlock );
+    
+    if( __startup_cd_changed )
+    {
+    	UnLock( oldlock );
+    }
+    else
+    {
+    	__startup_cd_changed = TRUE;
+	__startup_cd_lock    = oldlock;
+    }    
+    
+    return 0;
+}
+
+int __init_chdir(void)
+{
+    GETUSER;
+
+    __startup_cd_changed = FALSE;
+
+    return 0;
+}
+
+void __exit_chdir(void)
+{
+    GETUSER;
+
+    if( __startup_cd_changed )
+    {
+    	BPTR lock = CurrentDir( __startup_cd_lock );
+	UnLock( lock );
+    }
+}
+
+ADD2INIT(__init_chdir, 110);
+ADD2EXIT(__exit_chdir, 110);
