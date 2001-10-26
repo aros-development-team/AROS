1a6ea4b7fa687946ef0b40caa74665eb79f8e579
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-10-26 18:21:34 +0000

    Setting window->ZipLeftEdge/ZipTopEdge/ZipWidth/ZipHeight inside DoMoveSizeWindow() was wrong. Added some warnings about REFRESH_LOCK/REFRESH_UNLOCK (IntuitionBase->RefreshLock semaphore) possibly having to be changed to not use RefreshLock semaphore, but instead ObtainSem(GadgetLock) + LockLayers().
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12522 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/intuition/beginrefresh.c
+++ rom/intuition/beginrefresh.c
@@ -1,41 +1,11 @@
 /*
-    (C) 1995-96 AROS - The Amiga Research OS
+    Copyright (C) 1995-2001 AROS - The Amiga Research OS
     $Id$
-    $Log$
-    Revision 1.9  2000/06/19 19:47:16  stegerg
-    use new refreshlock semaphore. also use locklayerinfo, because for
-    GZZ windows 2 layers are locked.
-
-    Revision 1.8  2000/01/21 12:35:22  bergers
-    No more debugging output.
-
-    Revision 1.7  2000/01/11 16:05:00  bergers
-    Update. Move some code from intuition_driver to this directoy.
-
-    Revision 1.6  1998/10/20 16:45:51  hkiel
-    Amiga Research OS
-
-    Revision 1.5  1997/01/27 00:36:36  ldp
-    Polish
-
-    Revision 1.4  1996/12/10 14:00:00  aros
-    Moved #include into first column to allow makedepend to see it.
-
-    Revision 1.3  1996/11/08 11:28:00  aros
-    All OS function use now Amiga types
-
-    Moved intuition-driver protos to intuition_intern.h
-
-    Revision 1.2  1996/10/24 15:51:17  aros
-    Use the official AROS macros over the __AROS versions.
-
-    Revision 1.1  1996/10/21 17:06:48  aros
-    A couple of new functions
-
 
     Desc:
     Lang: english
 */
+
 #include "intuition_intern.h"
 
 /*****************************************************************************
@@ -75,6 +45,9 @@
 {
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct IntuitionBase *,IntuitionBase)
+
+#warning BeginRefresh: Remove this ObtainSem, if LOCK_REFRESH macro in inputhandler_actions.c is
+#warning               changed to do ObtainSem(GadgetLock) + LockLayers()
 
     ObtainSemaphore(&GetPrivScreen(window->WScreen)->RefreshLock);
     
