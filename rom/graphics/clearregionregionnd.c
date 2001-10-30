3e914917ef04bb499807202159fc9b8f11a7f108
falemagn <falemagn@gmail.com>
2001-10-30 21:08:38 +0000

    Added the non-destructive version of the region handling functions
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12564 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/graphics/clearregionregionnd.c
@@ -0,0 +1,86 @@
+/*
+    (C) 1995-96 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Graphics function AndRegionRegion()
+    Lang: english
+*/
+#include "graphics_intern.h"
+#include <graphics/regions.h>
+#include "intregions.h"
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/graphics.h>
+
+	AROS_LH2(struct Region *, ClearRegionRegionND,
+
+/*  SYNOPSIS */
+	AROS_LHA(struct Region *, R1, A0),
+	AROS_LHA(struct Region *, R2, A1),
+
+/*  LOCATION */
+	struct GfxBase *, GfxBase, 141, Graphics)
+
+/*  FUNCTION
+	Clear the given Region region1 from the given Region region2.
+
+    INPUTS
+	region1 - pointer to a region structure
+	region2 - pointer to a region structure
+
+    RESULT
+        The resulting region or NULL in case there's no enough free memory
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	XorRegionRegion(), OrRegionRegion()
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    struct Region *R3 = NewRegion();
+
+    if (R3)
+    {
+        if
+        (
+            _DoOperationBandBand
+            (
+                _ClearBandBand,
+                MinX(R1),
+                MinX(R2),
+	        MinY(R1),
+                MinY(R2),
+                R1->RegionRectangle,
+                R2->RegionRectangle,
+                &R3->RegionRectangle,
+                &R3->bounds,
+                GfxBase
+            )
+        )
+        {
+
+            _TranslateRegionRectangles(R3->RegionRectangle, -MinX(R3), -MinY(R3));
+
+            return R3;
+        }
+
+        DisposeRegion(R3);
+    }
+
+    return NULL;
+
+    AROS_LIBFUNC_EXIT
+}
---
+++ rom/graphics/xorrectregionnd.c
@@ -0,0 +1,75 @@
+/*
+    (C) 1995-96 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: Graphics function XorRectRegion()
+    Lang: english
+*/
+#include "graphics_intern.h"
+#include <graphics/regions.h>
+#include "intregions.h"
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/graphics.h>
+
+	AROS_LH2(struct Region *, XorRectRegionND,
+
+/*  SYNOPSIS */
+	AROS_LHA(struct Region    *, Reg,  A0),
+	AROS_LHA(struct Rectangle *, Rect, A1),
+
+/*  LOCATION */
+	struct GfxBase *, GfxBase, 152, Graphics)
+
+/*  FUNCTION
+	Exclusive-OR the given rectangle to the given
+	region
+
+    INPUTS
+	region - pointer to a region structure
+	rectangle - pointer to a rectangle structure
+
+    RESULT
+	The resulting region or NULL in case there's no enough free memory
+
+    NOTES
+	All relevant data is copied, you may throw away the
+	given rectangle after calling this function
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	AndRectRegion(), OrRectRegion(), ClearRectRegion()
+
+    INTERNALS
+
+    HISTORY
+	27-11-96    digulla automatically created from
+			    graphics_lib.fd and clib/graphics_protos.h
+	19-01-97    mreckt  intital version
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    struct Region R;
+    struct RegionRectangle rr;
+
+    InitRegion(&R);
+
+    R.bounds = *Rect;
+    R.RegionRectangle = &rr;
+
+    rr.Next = NULL;
+    MinX(&rr) = MinY(&rr) = 0;
+    MaxX(&rr) = Rect->MaxX - Rect->MinX;
+    MaxY(&rr) = Rect->MaxY - Rect->MinY;
+
+    return XorRegionRegionND(&R, Reg);
+
+    AROS_LIBFUNC_EXIT
+} /* XorRectRegion */
