6977e1bd1fde110965d770fa1e04bc947527f598
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-10-30 20:39:40 +0000

    source code cleanup. rewrote ps2 mouse interrupt routine.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12563 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-pc/Drivers/mouse.hidd/drv_ps2.c
+++ arch/i386-pc/Drivers/mouse.hidd/drv_ps2.c
@@ -1,10 +1,12 @@
 /*
-    (C) 1999-2001 AROS - The Amiga Research OS
+    Copyright (C) 1999-2001 AROS - The Amiga Research OS
     $Id$
 
     Desc: PS/2 mouse driver.
     Lang: English.
 */
+
+/****************************************************************************************/
 
 #include <asm/speaker.h>
 #undef inb
@@ -28,6 +30,8 @@
 #define DEBUG 1
 #include <aros/debug.h>
 
+/****************************************************************************************/
+
 #define HiddMouseAB	(MSD(cl)->hiddMouseAB)
 
 /* defines for buttonstate */
@@ -38,10 +42,12 @@
 
 void mouse_usleep(ULONG usec);
 
-/***** Test procedure ***********************************************/
+/****************************************************************************************/
 
 void mouse_ps2int(HIDDT_IRQ_Handler *, HIDDT_IRQ_HwInfo *);
 int mouse_ps2reset(struct mouse_data *);
+
+/****************************************************************************************/
 
 int test_mouse_ps2(OOP_Class *cl, OOP_Object *o)
 {
@@ -77,18 +83,21 @@ int test_mouse_ps2(OOP_Class *cl, OOP_Object *o)
                 /* Either no PS/2 mouse or problems with it */
                 /* Remove mouse interrupt */
                 HIDD_IRQ_RemHandler(data->u.ps2.irqhidd, irq);
+		
                 /* Dispose IRQ object */
                 OOP_DisposeObject(data->u.ps2.irqhidd);
+		
                 /* Free IRQ structure as it's not needed anymore */
                 FreeMem(irq, sizeof(HIDDT_IRQ_Handler));
             }
         }
     }
-
-    return 0; /* Report no PS/2 mouse */
+    /* Rep
+    ort no PS/2 mouse */
+    return 0; 
 }
 
-/*****  *************************************************************/
+/****************************************************************************************/
 
 #undef SysBase
 #define SysBase (hw->sysBase)
@@ -101,12 +110,16 @@ int test_mouse_ps2(OOP_Class *cl, OOP_Object *o)
         aux_write_ack(val);			\
 	})
 
+/****************************************************************************************/
+
 unsigned char handle_kbd_event(void);
 void kb_wait(void);
 void kbd_write_cmd(int cmd);
 void aux_write_ack(int val);
 void kbd_write_output_w(int data);
 void kbd_write_command_w(int data);
+
+/****************************************************************************************/
 
 int kbd_detect_aux()
 {
@@ -133,169 +146,176 @@ int kbd_detect_aux()
 	    }
 	    break;
 	}
+	
 	mouse_usleep(1000);
+	
     } while (--loops);
     
     return retval;
 }
 
+/****************************************************************************************/
+
 void mouse_ps2int(HIDDT_IRQ_Handler *irq, HIDDT_IRQ_HwInfo *hw)
 {
-    UBYTE                       info;
     struct mouse_data           *data =(struct mouse_data *)irq->h_Data;
     struct pHidd_Mouse_Event    *e = &data->u.ps2.event;
-
-    unsigned int work = 10000;
+    UWORD   	    	    	buttonstate;
+    WORD    	    	    	work = 10000;
+    UBYTE                       info, mousecode, *mouse_data;
     
     info = kbd_read_status();
 
-    while ((info & KBD_STATUS_OBF))             /* data from information port */
+    for(; ((info = kbd_read_status()) & KBD_STATUS_OBF) && work; work--)
     {
-        if (info & KBD_STATUS_MOUSE_OBF)   /* If bit 5 set data from mouse. */
-        {
-            UBYTE *mouse_data=data->u.ps2.mouse_data;
-            	    
-            UBYTE mousecode = kbd_read_input();
+    	if (!(info & KBD_STATUS_MOUSE_OBF))
+	{
+	    /*
+	    ** Data from keyboard. Hopefully this gets through to keyboard interrupt
+	    ** if we break out of for loop here :-\
+	    */	    
+	    break;
+	}
+	
+	mousecode = kbd_read_input();
+	
+	if (info & (KBD_STATUS_GTO | KBD_STATUS_PERR))
+	{
+            /* Ignore errors and messages for keyboard -> eat status/error byte */
+	    continue;
+	}
+	
+	if ((mousecode == AUX_ACK) && (data->u.ps2.expected_mouse_acks))
+	{
+	    D(bug("  Got a mouse ACK!\n"));
+	    data->u.ps2.expected_mouse_acks--;
+	    continue;
+	}
 
-	    if (info & (KBD_STATUS_GTO | KBD_STATUS_PERR))
-	    {
-    	    	info = kbd_read_status();
-	    	continue;
-	    }
+    #if 1
+	if (mousecode == AUX_RECONNECT)
+	{
+            data->u.ps2.mouse_collected_bytes = 0;
+	    
+	    /* Ping mouse */
+    	    aux_write(KBD_OUTCMD_ENABLE); 
+ 	}
+    #endif
+    
+    	/* Mouse Packet Byte */
+	
+	mouse_data = data->u.ps2.mouse_data;
+	
+        data->u.ps2.expected_mouse_acks = 0;
+        mouse_data[data->u.ps2.mouse_collected_bytes] = mousecode;
+	
+	/* Packet validity check. Bit 3 of first mouse packet byte must be set */
+	
+	if ((mouse_data[0] & 8) == 0)
+	{
+            data->u.ps2.mouse_collected_bytes = 0;
+	    continue;
+	}
+	
+        data->u.ps2.mouse_collected_bytes++;
+	
+	if (data->u.ps2.mouse_collected_bytes != 3)
+	{
+	    /* Mouse Packet not yet complete */
+	    continue;
+	}
+	
+	/* We have a complete mouse packet :-) */
+	
+    	data->u.ps2.mouse_collected_bytes = 0;
+                        
+	/*
+         * Let's see whether these data can be right...
+         *
+         * D7 D6 D5 D4 D3 D2 D1 D0
+         * YV XV YS X2  1  M  R  L
+         * X7 .  .  .  .  .  .  X1   (X, signed)
+         * Y7 .  .  .  .  .  .  Y1   (Y, signed)
+         *
+         * YV,XV : over flow in x/y direction
+         * XS,YS : represents sign of X and Y
+         * X,Y   : displacement in x and y direction.
+         * X and Y are signed, XS, YS are there to double check the
+         * sign and correctnes of the collected data (?).
+         *
+         * http://www.hut.fi/~then/mytexts/mouse.htm
+         */
 
-//            if (0xfa == mousecode)
-//            /* Check whether we are excepting ACK */
-//            if (data->u.ps2.expected_mouse_acks)
-	    if ((AUX_ACK == mousecode) && (data->u.ps2.expected_mouse_acks))
-            {
-                //if (mousecode == AUX_ACK)
-                //{
-                    D(bug("                             Got a mouse ACK!\n"));
-                    data->u.ps2.expected_mouse_acks--;
-                //}
-                //else data->u.ps2.expected_mouse_acks = 0;
-            }
-#if 1
-            else if (mousecode == AUX_RECONNECT)
-            {
-                data->u.ps2.mouse_collected_bytes = 0;
-                aux_write(KBD_OUTCMD_ENABLE);   /* Ping mouse */
-            }
-#endif
-            else
-            {
-                data->u.ps2.expected_mouse_acks = 0;
-                mouse_data[data->u.ps2.mouse_collected_bytes] = mousecode;
-                if (0 == (mouse_data[0] & 8))
-		{
-                    data->u.ps2.mouse_collected_bytes = 0;
-		    //Sound(400,100000);
-		}
-                else
-                {
-                    data->u.ps2.mouse_collected_bytes++;
+    #if 0
+        D(bug("Got the following: 1. byte: 0x%x, dx=%d, dy=%d\n",
+              mouse_data[0],
+              mouse_data[1],
+              mouse_data[2]));
+    #endif
+        
+        e->x = mouse_data[1];
+        e->y = mouse_data[2];
 
-                    if (data->u.ps2.mouse_collected_bytes == 3)
-                    {
-                        data->u.ps2.mouse_collected_bytes = 0;
-                        /*
-                         * Let's see whether these data can be right...
-                         *
-                         * D7 D6 D5 D4 D3 D2 D1 D0
-                         * YV XV YS X2  1  M  R  L
-                         * X7 .  .  .  .  .  .  X1   (X, signed)
-                         * Y7 .  .  .  .  .  .  Y1   (Y, signed)
-                         *
-                         * YV,XV : over flow in x/y direction
-                         * XS,YS : represents sign of X and Y
-                         * X,Y   : displacement in x and y direction.
-                         * X and Y are signed, XS, YS are there to double check the
-                         * sign and correctnes of the collected data (?).
-                         *
-                         * http://www.hut.fi/~then/mytexts/mouse.htm
-                         */
-#if 0
-                        if ( (( (mouse_data[0] & 0x10) && (char)mouse_data[1] <  0) ||
-                              (!(mouse_data[0] & 0x10) && (char)mouse_data[1] >= 0)   ) &&
-                             (( (mouse_data[0] & 0x20) && (char)mouse_data[2] <  0) ||
-                              (!(mouse_data[0] & 0x20) && (char)mouse_data[2] >= 0    )))
-#endif
-                        {
-
-                            UWORD buttonstate;
-                            
-			#if 0
-                            D(bug("Got the following: 1. byte: 0x%x, dx=%d, dy=%d\n",
-                                  mouse_data[0],
-                                  mouse_data[1],
-                                  mouse_data[2]));
-                        #endif    
-                            e->x = mouse_data[1];
-                            e->y = mouse_data[2];
-			    
-			    if (mouse_data[0] & 0x10) e->x -= 256;
-			    if (mouse_data[0] & 0x20) e->y -= 256;
+	if (mouse_data[0] & 0x10) e->x -= 256;
+	if (mouse_data[0] & 0x20) e->y -= 256;
 			    			    
-			    /* dy is reversed! */
-    	    	    	    e->y = -(e->y);
-			    
-                            if (e->x || e->y)
-                            {
-                                e->button   = vHidd_Mouse_NoButton;
-                                e->type     = vHidd_Mouse_Motion;
+	/* dy is reversed! */
+    	e->y = -(e->y);
+	
+	if (e->x || e->y)
+	{
+            e->button   = vHidd_Mouse_NoButton;
+            e->type     = vHidd_Mouse_Motion;
 
-                                data->mouse_callback(data->callbackdata, e);
-                            }
+            data->mouse_callback(data->callbackdata, e);
+	}
                             
-                            buttonstate = mouse_data[0] & 0x07;
+        buttonstate = mouse_data[0] & 0x07;
 
-                            if ((buttonstate & LEFT_BUTTON) 
-                                    != (data->buttonstate & LEFT_BUTTON))
-                            {
-                                e->button = vHidd_Mouse_Button1;
-                                e->type = (buttonstate & LEFT_BUTTON) ?
-                                    vHidd_Mouse_Press : vHidd_Mouse_Release;
-                                data->mouse_callback(data->callbackdata, e);
-                            }
-                            if ((buttonstate & RIGHT_BUTTON) 
-                                    != (data->buttonstate & RIGHT_BUTTON))
-                            {
-                                e->button = vHidd_Mouse_Button2;
-                                e->type = (buttonstate & RIGHT_BUTTON) ?
-                                    vHidd_Mouse_Press : vHidd_Mouse_Release;
-                                data->mouse_callback(data->callbackdata, e);
-                            }
-                            if ((buttonstate & MIDDLE_BUTTON) 
-                                    != (data->buttonstate & MIDDLE_BUTTON))
-                            {
-                                e->button = vHidd_Mouse_Button3;
-                                e->type = (buttonstate & MIDDLE_BUTTON) ?
-                                    vHidd_Mouse_Press : vHidd_Mouse_Release;
-                                data->mouse_callback(data->callbackdata, e);
-                            }
+	if ((buttonstate & LEFT_BUTTON) != (data->buttonstate & LEFT_BUTTON))
+	{
+            e->button = vHidd_Mouse_Button1;
+            e->type   = (buttonstate & LEFT_BUTTON) ? vHidd_Mouse_Press : vHidd_Mouse_Release;
+	    
+            data->mouse_callback(data->callbackdata, e);
+	}
+	
+	if ((buttonstate & RIGHT_BUTTON) != (data->buttonstate & RIGHT_BUTTON))
+	{
+            e->button = vHidd_Mouse_Button2;
+            e->type   = (buttonstate & RIGHT_BUTTON) ? vHidd_Mouse_Press : vHidd_Mouse_Release;
+	    
+            data->mouse_callback(data->callbackdata, e);
+	}
+	
+	if ((buttonstate & MIDDLE_BUTTON) != (data->buttonstate & MIDDLE_BUTTON))
+	{
+            e->button = vHidd_Mouse_Button3;
+            e->type = (buttonstate & MIDDLE_BUTTON) ? vHidd_Mouse_Press : vHidd_Mouse_Release;
+	    
+            data->mouse_callback(data->callbackdata, e);
+	}
 
-                            data->buttonstate = buttonstate;
-                        }
-                    }
-                }
-            }                                       
-        } else break;
-        info = kbd_read_status();
+        data->buttonstate = buttonstate;
+ 
+    } /* for(; ((info = kbd_read_statues()) & KBD_STATUS_OBF) && work; work--) */
 
-      /* Protect as from forever loop */
-      if (!--work)
-      {
-          D(bug("kbd.hidd: controller jammed (0x%02X).\n", info));
-          break;
-      }	
-    } /* while data can be read */
+    if (!work)
+    {
+        D(bug("kbd.hidd: controller jammed (0x%02X).\n", info));
+    }	
+
 }
+
+/****************************************************************************************/
 
 #undef SysBase
 #define SysBase (*(struct ExecBase **)4L)
 
 #define AUX_INTS_OFF (KBD_MODE_KCC | KBD_MODE_DISABLE_MOUSE | KBD_MODE_SYS | KBD_MODE_KBD_INT)
 #define AUX_INTS_ON  (KBD_MODE_KCC | KBD_MODE_SYS | KBD_MODE_MOUSE_INT | KBD_MODE_KBD_INT)
+
+/****************************************************************************************/
 
 int mouse_ps2reset(struct mouse_data *data)
 {
@@ -344,6 +364,7 @@ int mouse_ps2reset(struct mouse_data *data)
      * If this is not read then the mouse does not
      * work on my computer.- Stefan
      */
+     
     kbd_read_data();
 
     D(bug("Initialized PS/2 mouse!\n"));
@@ -351,4 +372,4 @@ int mouse_ps2reset(struct mouse_data *data)
     return 1;
 }
 
-
+/****************************************************************************************/
