08006307c38cc97413cbc9ced7776e9f25a23d07
falemagn <falemagn@gmail.com>
2001-10-30 22:15:28 +0000

    Moved these programs here from the workbench/demos directory
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12571 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/helloworld.c
@@ -0,0 +1,16 @@
+/*
+    (C) 1995-96 AROS - The Amiga Research OS
+    $Id$
+
+    Desc: most simple demo for AROS
+    Lang: english
+*/
+#include <stdio.h>
+
+static const char version[] = "$VER: helloworld 41.1 (14.3.1997)\n";
+
+int main (int argc, char ** argv)
+{
+    printf ("Hello, world\n");
+    return 0;
+}
