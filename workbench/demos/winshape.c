87cf461f2517a71743e4960d3bc675c2874286ad
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-10-30 21:51:53 +0000

    moved these programs to the test directory
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12567 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/demos/winshape.c
+++ workbench/demos/winshape.c
@@ -28,18 +28,18 @@ WORD	    	    	actshape;
 static void cleanup(char *msg)
 {
     if(msg) printf("winshape: %s\n", msg);
-    
+
     if (win) CloseWindow(win);
-    
-    if (shape) DisposeRegion(shape);
-    
+
+//    if (shape) DisposeRegion(shape);
+
     if (dri) FreeScreenDrawInfo(scr, dri);
     if (scr) UnlockPubScreen(0, scr);
-    
+
     if (LayersBase) CloseLibrary(LayersBase);
     if (GfxBase) CloseLibrary((struct Library *)GfxBase);
     if (IntuitionBase) CloseLibrary((struct Library *)IntuitionBase);
-    
+
     exit(0);
 }
 
@@ -71,33 +71,33 @@ static void getvisual(void)
 static void makeshape(void)
 {
     struct Rectangle rect;
-    
+
 #if 1
     if (!(shape = NewRectRegion(0, 0, WINWIDTH - 1, scr->WBorTop + scr->Font->ta_YSize + 1 - 1)))
     	cleanup("Can't create region!\n");
 #else
     if (!(shape = NewRegion())) cleanup("Can't create region!\n");
-    
+
     rect.MinX = 0;
     rect.MinY = 0;
     rect.MaxX = WINWIDTH - 1;
     rect.MaxY = scr->WBorTop + scr->Font->ta_YSize + 1 - 1;
-    
+
     if (!(OrRectRegion(shape, &rect))) cleanup("Can't create region!\n");
 #endif
-    
+
     rect.MinX = WINCX - 20;
     rect.MinY = 20;
     rect.MaxX = WINCX + 20;
     rect.MaxY = WINHEIGHT - 1 - 20;
-    
+
     if (!(OrRectRegion(shape, &rect))) cleanup("Can't create region!\n");
-    
+
     rect.MinX = 20;
     rect.MinY = WINCY - 20;
     rect.MaxX = WINWIDTH - 1 - 20;
     rect.MaxY = WINCY + 20;
-    
+
     if (!(OrRectRegion(shape, &rect))) cleanup("Can't create region!\n");
 
 }
@@ -120,22 +120,22 @@ static void makewin(void)
 	{WA_Shape   	, (IPTR)shape	    	    	    	},
 	{TAG_DONE   	    	    	    	    	    	}
     };
-    
+
     win = OpenWindowTagList(0, win_tags);
-    if (!win) cleanup("Can't create window!");    
-    
+    if (!win) cleanup("Can't create window!");
+
     rp = win->RPort;
     SetAPen(rp, dri->dri_Pens[FILLPEN]);
     SetBPen(rp, dri->dri_Pens[SHINEPEN]);
     SetDrMd(rp, JAM2);
-    
+
     SetAfPt(rp, pattern, 1);
     RectFill(rp, win->BorderLeft,
     	    	 win->BorderTop,
 		 WINWIDTH - 1 - win->BorderRight,
 		 WINHEIGHT - 1 - win->BorderBottom);
     SetAfPt(rp, 0, 0);
-    
+
     SetAPen(rp, dri->dri_Pens[SHINEPEN]);
     RectFill(rp, WINCX - 20, 20, WINCX + 20, 20);
     RectFill(rp, WINCX - 20, 21, WINCX - 20, WINCY - 20);
@@ -143,7 +143,7 @@ static void makewin(void)
     RectFill(rp, 20, WINCY - 20, 20, WINCY + 20);
     RectFill(rp, WINCX - 20, WINCY + 20, WINCX - 20, WINHEIGHT - 1 - 20);
     RectFill(rp, WINCX + 20, WINCY - 20, WINWIDTH - 1 - 20, WINCY - 20);
-    
+
     SetAPen(rp, dri->dri_Pens[SHADOWPEN]);
     RectFill(rp, WINCX + 20, 20, WINCX + 20, WINCY - 20);
     RectFill(rp, WINCX + 20, WINCY + 20, WINWIDTH - 1 - 20, WINCY + 20);
