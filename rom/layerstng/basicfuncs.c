cd9a117b2bd5430ab241ecf221da831989dba482
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-10-30 19:37:11 +0000

    Fixed copyright notice.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12558 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/layerstng/basicfuncs.c
+++ rom/layerstng/basicfuncs.c
@@ -1,3 +1,11 @@
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc:
+    Lang: english
+*/
+
 #include <aros/config.h>
 #include <aros/asmcall.h>
 #include <exec/memory.h>
