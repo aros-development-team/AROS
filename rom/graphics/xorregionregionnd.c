bbfa1e6b965d7b4ee45dff616f15e98612100fcb
falemagn <falemagn@gmail.com>
2001-10-31 14:44:59 +0000

    some more small optimizations
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12584 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/graphics/xorregionregionnd.c
+++ rom/graphics/xorregionregionnd.c
@@ -56,7 +56,19 @@
 {
     AROS_LIBFUNC_INIT
 
-    struct Region *R3 = NewRegion();
+    struct Region *R3;
+
+    if
+    (
+        !R1->RegionRectangle            ||
+        !R2->RegionRectangle            ||
+        !overlap(R1->bounds, R2->bounds)
+    )
+    {
+        return OrRegionRegionND(R1, R2);
+    }
+
+    R3 = NewRegion();
 
     if (R3)
     {
@@ -110,14 +122,15 @@
         if (res)
         {
             _TranslateRegionRectangles(R3->RegionRectangle, -MinX(R3), -MinY(R3));
-
-            return R3;
         }
-
-        DisposeRegion(R3);
+        else
+        {
+            DisposeRegion(R3);
+            R3 = NULL;
+        }
     }
 
-    return NULL;
+    return R3;
 
     AROS_LIBFUNC_EXIT
 }
