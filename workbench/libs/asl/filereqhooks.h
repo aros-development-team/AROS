2c5b6df14249e60074e5d5d605db9af659860e3c
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-11-11 11:03:25 +0000

    Fixed copyright notice.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12679 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/asl/filereqhooks.h
+++ workbench/libs/asl/filereqhooks.h
@@ -2,7 +2,7 @@
 #define FILEREQHOOKS_H
 
 /*
-    (C) 1997 AROS - The Amiga Research OS
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: File requester specific defs.
