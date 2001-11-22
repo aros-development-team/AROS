4f1a303908afdd7e284e0487012f6c3b22f4b6ea
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-11-22 15:43:01 +0000

    If the second argument is NULL, use the current time.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12705 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/utime.c
+++ compiler/clib/utime.c
@@ -6,11 +6,22 @@
 int utime(const char *filename, struct utimbuf *buf)
 {
     struct timeval ts[2];
+    
+    if( buf == NULL )
+    {
+        time_t tt = time( NULL );
 
-    ts[0].tv_sec = buf->actime;
-    ts[0].tv_usec= 0;
-    ts[1].tv_sec = buf->modtime;
-    ts[1].tv_usec= 0;
+        ts[0].tv_sec = tt;
+        ts[1].tv_sec = tt;
+    }
+    else
+    {
+        ts[0].tv_sec = buf->actime;
+        ts[1].tv_sec = buf->modtime;
+    }
 
+    ts[0].tv_usec = 0;
+    ts[1].tv_usec = 0;
+    
     return utimes(filename, ts);
 }
