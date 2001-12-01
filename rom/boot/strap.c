edce9c758ef5e3d9dc0f80453cf59fccf1de0d0b
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-12-01 16:36:29 +0000

    Revamped the DOS booting process. Almost everything (mounting all filesystems, checking for bootable disks, prompt for no bootable disk is found, making the default assigns, ...) is now done by the Boot Process instead of being all over the place. The Boot Strap now basically just runs InitResident() on dos.library, and DOSBoot() just creates the Boot Process.
    Whether a filesystem is bootable is currently only determined by checking if
    there is a disk present, but could be extended to check for basic files
    (Startup-Sequnce) etc. Multiple BootNodes is now properly supported; the
    Boot Process will search for a bootable filesystem with the highest priority
    going through the whole list of BootNodes. If no bootable filesystems was found,
    the user is prompted to insert a bootable disk.
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12725 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/boot/strap.c
+++ rom/boot/strap.c
@@ -25,6 +25,8 @@
 #endif
 #include <aros/macros.h>
 
+#define BOOT_CHECK 0
+
 void InitKeyboard(void);
 void putc(char);
 void putstring(char *);
@@ -56,156 +58,22 @@ AROS_LH2(int, init,
     AROS_LHA(ULONG, seglist, A0),
     struct ExecBase *, SysBase, 0, boot)
 {
-	AROS_LIBFUNC_INIT
-	struct ExpansionBase *ExpansionBase;
-	struct Resident *dosresident;
-	struct BootNode *bn;
-	char key;
-	static char transl[] =
-    { ' ', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', ' ', ' ',
-      ' ', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', ' ', ' ', 10,
-      ' ', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ' ', ' ', ' ', ' ', 
-      ' ', 'Z', 'X', 'C', 'V', 'B', 'N', 'M' };
+    AROS_LIBFUNC_INIT
+    
+    struct Resident *DOSResident;
 
-
-	InitKeyboard();
-	ExpansionBase = (struct ExpansionBase *)OpenLibrary("expansion.library", 0);
-	if (ExpansionBase == NULL)
-		Alert(AT_DeadEnd | AG_OpenLib | AN_BootStrap | AO_ExpansionLib);
-
-	for (bn=(struct BootNode *)ExpansionBase->MountList.lh_Head;;bn=(struct BootNode *)ExpansionBase->MountList.lh_Head)
-	{
-		while (bn->bn_Node.ln_Succ)
-		{
-			if (((struct DosList *)bn->bn_DeviceNode)->dol_Device==0)
-			{
-				if (checkBoot((struct DeviceNode *)bn->bn_DeviceNode))
-				{
-					if (bn!=(struct BootNode *)ExpansionBase->MountList.lh_Head)
-					{
-						Remove(&bn->bn_Node);
-						AddHead(&ExpansionBase->MountList, &bn->bn_Node);
-					}
-					CloseLibrary((struct Library *)ExpansionBase);
-					dosresident=FindResident("dos.library");
-					if (dosresident)
-					{
-						InitResident(dosresident, NULL);
-						goto start_boot;
-					}
-					else
-						Alert(AT_DeadEnd | AG_OpenLib | AN_BootStrap | AO_DOSLib);
-				}
-			}
-			bn = (struct BootNode *)bn->bn_Node.ln_Succ;
-		}
-		putstring
-			(
-					"No bootable disk found.\n"
-					"Insert bootable disk in any drive and press Enter.\n"
-			);
-		do
-		{
-			key = GetK();
-			key = transl[(key == 0x39) ? 1 : key - 1];
-			UnGetK();
-		} while(key !=10);
-	}
-
-start_boot:
-	return 0;
-	AROS_LIBFUNC_EXIT
+    DOSResident = FindResident( "dos.library" );
+					
+    if( DOSResident == NULL )
+    {
+        Alert( AT_DeadEnd | AG_OpenLib | AN_BootStrap | AO_DOSLib );
+    }
+	
+    InitResident( DOSResident, NULL );
+    				
+    return 0;
+    
+    AROS_LIBFUNC_EXIT
 }
 
-void putstring(char *string) {
-
-	while (*string)
-		Putc(*string++);
-}
-
-
-int checkBoot(struct DeviceNode *dn) {
-struct FileSysStartupMsg *fssm = BADDR(dn->dn_Startup);
-struct DosEnvec *de = BADDR(fssm->fssm_Environ);
-struct IOExtTD *iotd;
-ULONG buf[128];
-int retval=0;
-struct MsgPort *mp=CreateMsgPort();
-
-	if (mp)
-	{
-		iotd=(struct IOExtTD *)CreateIORequest(mp,sizeof(struct IOExtTD));
-		if (iotd)
-		{
-			kprintf("Trying to boot from %S\n", AROS_BSTR_ADDR(fssm->fssm_Device));
-			if (!OpenDevice(AROS_BSTR_ADDR(fssm->fssm_Device),fssm->fssm_Unit, (struct IORequest *)&iotd->iotd_Req,0))
-			{
-				if (strncmp(AROS_BSTR_ADDR(fssm->fssm_Device), "ide.device", 10) == 0)
-				{
-				    UQUAD offset64 = de->de_LowCyl;
-
-				    offset64 *= de->de_Surfaces;
-				    offset64 *= de->de_BlocksPerTrack;
-				    offset64 *= (de->de_SizeBlock*4);
-
-				    iotd->iotd_Req.io_Command = NSCMD_TD_READ64;
-				    iotd->iotd_Req.io_Offset  = offset64 &  0xFFFFFFFF;
-				    iotd->iotd_Req.io_Actual  = offset64 >> 32;
-				    kprintf("Reading at offset 0x%08x%08x\n",
-				        iotd->iotd_Req.io_Actual, iotd->iotd_Req.io_Offset);
-
-				}
-				else
-				{
-				    iotd->iotd_Req.io_Command = CMD_READ;
-			  	    iotd->iotd_Req.io_Offset =
-				        de->de_LowCyl*de->de_Surfaces*
-					de->de_BlocksPerTrack*(de->de_SizeBlock*4);
-
-				    kprintf("Reading at offset 0x%08x\n",
-				    iotd->iotd_Req.io_Offset);
-				}
-
-				iotd->iotd_Req.io_Length = 512;
-				iotd->iotd_Req.io_Data = buf;
-
-				if (!DoIO((struct IORequest *)&iotd->iotd_Req))
-				{
-					if ((AROS_BE2LONG(buf[0]) & 0xFFFFFF00)==0x444F5300)
-						retval=1;
-					else
-					{
-						iotd->iotd_Req.io_Offset =
-							((de->de_LowCyl*de->de_Surfaces*
-							de->de_BlocksPerTrack)+1)*(de->de_SizeBlock*4);
-						if (!DoIO((struct IORequest *)&iotd->iotd_Req))
-						{
-							if ((AROS_BE2LONG(buf[0]) & 0xFFFFFF00)==0x444F5300)
-								retval=1;
-						}
-					}
-				}
-				if (strcmp(AROS_BSTR_ADDR(fssm->fssm_Device),"trackdisk.device")==0)
-				{
-					iotd->iotd_Req.io_Command = TD_MOTOR;
-					iotd->iotd_Req.io_Length = 0;
-					DoIO((struct IORequest *)&iotd->iotd_Req);
-				}
-				CloseDevice((struct IORequest *)&iotd->iotd_Req);
-			}
-			DeleteIORequest((struct IORequest *)&iotd->iotd_Req);
-		}
-		else
-		{
-			Alert(AT_DeadEnd | AG_NoMemory | AN_BootStrap);
-		}
-		DeleteMsgPort(mp);
-	}
-	else
-	{
-		Alert(AT_DeadEnd | AG_NoMemory | AN_BootStrap);
-	}
-	return retval;
-}
 static const char boot_end = 0;
-
--- rom/dos/dosboot.c
+++ rom/dos/dosboot.c
@@ -10,6 +10,7 @@
 
 # define  DEBUG  1
 # include <aros/debug.h>
+
 #include <aros/macros.h>
 
 #include <exec/types.h>
@@ -30,9 +31,13 @@
 #include <proto/exec.h>
 #include <proto/dos.h>
 
+#include <string.h>
+
 extern void boot();
 
-BOOL init_hidds(struct ExecBase *, struct DosLibrary *);
+BOOL init_hidds( struct ExecBase *, struct DosLibrary * );
+BOOL mount( struct DeviceNode *dn ); 
+BOOL isBootable( CONST_STRPTR deviceName );
 
 AROS_UFH3(void, intBoot,
     AROS_UFHA(APTR, argString, A0),
@@ -43,57 +48,107 @@ AROS_UFH3(void, intBoot,
     AROS_USERFUNC_INIT
 
     struct ExpansionBase *ExpansionBase = NULL;
-    struct Library *DOSBase = NULL;
-    BPTR lock;
-    struct BootNode *bn;
-    STRPTR bootname, s1;
-    ULONG len;
+    struct Library       *DOSBase       = NULL;
+    struct BootNode      *bootNode      = NULL;
+    STRPTR                bootName;
+    LONG                  bootNameLength;        
+    BPTR                  lock;
+    LONG                  second;
 
+#   define deviceName (((struct DosList *) bootNode->bn_DeviceNode)->dol_DevName)
     
-    DOSBase = OpenLibrary("dos.library", 0);
+    /**** Open all required libraries **********************************************/
+    DOSBase       =                          OpenLibrary( "dos.library", 0 );
+    ExpansionBase = (struct ExpansionBase *) OpenLibrary( "expansion.library", 0 );
 
-    if (DOSBase == NULL)
+    if( DOSBase == NULL )
     {
+        D(bug( "Could not open dos.library, something's wrong!\n" ));
 	Alert(AT_DeadEnd| AG_OpenLib | AN_DOSLib | AO_DOSLib);
     }
     
-    ExpansionBase = (struct ExpansionBase *)OpenLibrary("expansion.library",
-							0);
-
-    if (ExpansionBase == NULL)
+    if( ExpansionBase == NULL )
     {
-	D(bug("Urk, no expansion.library, something's wrong!\n"));
+	D(bug( "Could not open expansion.library, something's wrong!\n"));
 	Alert(AT_DeadEnd | AG_OpenLib | AN_DOSLib | AO_ExpansionLib);
     }
 
-    /* We have to do all the locking in this because we don't
-       have a Process in DOSBoot yet, and we need a process
-       to create locks etc.
-    */
-    bn = (struct BootNode *)ExpansionBase->MountList.lh_Head;
-    s1 = ((struct DosList *)bn->bn_DeviceNode)->dol_DevName;
+    /**** Try to mount all filesystems in the MountList ****************************/
+    D(bug( "Examining MountList:\n" ));
+    
+    ForeachNode( &ExpansionBase->MountList, bootNode )
+    {
+	D(bug
+        (
+            "Node: %p, DevNode: %p, Name = %s\n", 
+            bootNode, bootNode->bn_DeviceNode,
+	    deviceName ? deviceName : "(null)" 
+	));
+#if (AROS_FLAVOUR & AROS_FLAVOUR_EMULATION)
+	AddDosEntry( (struct DosList *) bootNode->bn_DeviceNode );
+#else
+    	/* 
+            Try to mount the filesystem. If it fails, remove the BootNode 
+	    from the list so DOS doesn't try to boot from it later. 
+        */ 
 
-    while (*s1++)
-	;
+	if( !mount( (struct DeviceNode *) bootNode->bn_DeviceNode ) )
+	{
+	    REMOVE( bootNode );
+	}
+#endif
+    }
+   
+    /**** Try to find a bootable filesystem ****************************************/   
+    while( TRUE )
+    {
+        ForeachNode( &ExpansionBase->MountList, bootNode )
+        {
+            /* 
+                Check if the mounted filesystem is bootable. If it's not,
+                it's probably some kind of transient error (ie. no disk
+                in drive or wrong disk) so we only move it to the end of
+                the list. 
+            */
+        
+            if( isBootable( deviceName ) )
+            {
+                goto boot;
+            }
+        }
+        
+        kprintf( "No bootable disk was found.\n" );
+        kprintf( "Please insert a bootable disk in any drive.\n" );
+        
+        for( second = 5; second > 0; second-- ) {
+            kprintf( "Retrying in %d seconds...\n", second );
+            Delay( 50 ); 
+        }
+        
+        /* FIXME: Should there be a prompt instead of a automatic retry? */        
+    }
 
-    len = s1 - ((struct DosList *)bn->bn_DeviceNode)->dol_DevName;
-    bootname = AllocMem(len + 2, MEMF_ANY);
+boot:
 
-    if (bootname == NULL)
+    /* Construct the complete device name of the boot device */
+    bootNameLength = strlen( deviceName ) + 2;
+    bootName       = AllocMem( bootNameLength, MEMF_ANY);
+
+    if( bootName == NULL )
     {
 	Alert(AT_DeadEnd | AG_NoMemory | AO_DOSLib | AN_StartMem);
     }
 
-    CopyMem(((struct DosList *)bn->bn_DeviceNode)->dol_DevName,
-	    bootname, len);
-    bootname[len - 1] = ':';
-    bootname[len] = '\0';
+    strcpy( bootName, deviceName );
+    strcat( bootName, ":" );
     
-    CloseLibrary((struct Library *)ExpansionBase);
+    /* We don't need expansion.library any more */
+    CloseLibrary( (struct Library *) ExpansionBase );
 
-    D(bug("Locking primary boot device %s\n", bootname));
+    /* Lock the boot device and add some default assigns */
+    D(bug("Locking primary boot device %s\n", bootName));
     
-    lock = Lock(bootname, SHARED_LOCK);
+    lock = Lock(bootName, SHARED_LOCK);
     
     if (lock != NULL)
     {
@@ -104,7 +159,7 @@ AROS_UFH3(void, intBoot,
 	Alert(AT_DeadEnd | AG_BadParm | AN_DOSLib);
     }
     
-    FreeMem(bootname, len + 2);
+    FreeMem( bootName, bootNameLength );
     lock = Lock("SYS:", SHARED_LOCK);
 
     if (lock != NULL)
@@ -156,63 +211,33 @@ AROS_UFH3(void, intBoot,
 	      AROS_UFCA(ULONG, argSize, D0),
 	      AROS_UFCA(struct ExecBase *, SysBase, A6));
 
+#   undef deviceName
+
     AROS_USERFUNC_EXIT
 }
 
 void DOSBoot(struct ExecBase *SysBase, struct DosLibrary *DOSBase)
 {
-    struct ExpansionBase *ExpansionBase;
-    struct BootNode *bn;
     struct TagItem bootprocess[] =
     {
-	{ NP_Entry,	(IPTR)intBoot },
-	{ NP_Name,	(IPTR)"Boot Process" },
-	{ NP_Input,	(IPTR)NULL },
-	{ NP_Output,	(IPTR)NULL },
-	{ NP_CurrentDir,(IPTR)NULL },
-	{ NP_Cli,	0 },
+	{ NP_Entry,	 (IPTR) intBoot },
+	{ NP_Name,	 (IPTR) "Boot Process" },
+	{ NP_Input,	 (IPTR) NULL },
+	{ NP_Output,	 (IPTR) NULL },
+	{ NP_CurrentDir, (IPTR) NULL },
+	{ NP_Cli,	 (IPTR) 0 },
 	{ TAG_END, }
     };
     
-    ExpansionBase = (struct ExpansionBase *)OpenLibrary("expansion.library",0);
-
-    if (ExpansionBase == NULL)
+    if( CreateNewProc( bootprocess ) == NULL )
     {
-	D(bug("Urk, no expansion.library, something's wrong!\n"));
-	Alert(AT_DeadEnd | AG_OpenLib | AN_DOSLib | AO_ExpansionLib);
-    }
-    
-    D(bug("Examining MountList: \n"));
-    
-    ForeachNode(&ExpansionBase->MountList, bn)
-    {
-	D(bug("Node: %p, DevNode: %p, Name = %s\n", bn,
-	      bn->bn_DeviceNode,
-	      ((struct DosList *)bn->bn_DeviceNode)->dol_DevName
-	      ? ((struct DosList *)bn->bn_DeviceNode)->dol_DevName
-	      : "(null)"
-	      ));
-#if (AROS_FLAVOUR & AROS_FLAVOUR_EMULATION)
-	AddDosEntry((struct DosList *)bn->bn_DeviceNode);
-#else
-    	/* Try to mount the filesystem. If it fails, remove the BootNode 
-	   from the list so DOS doesn't try to boot from it later. */ 
-
-	if( !mount( (struct DeviceNode *) bn->bn_DeviceNode ) )
-	{
-	    REMOVE( bn );
-	}
-#endif
-    }
-
-    if (CreateNewProc(bootprocess) == NULL)
-    {
-	D(bug("CreateNewProc() failed with %ld\n",
-	      ((struct Process *)FindTask(NULL))->pr_Result2));
+	D(bug
+        (
+            "CreateNewProc() failed with %ld\n",
+	    ((struct Process *) FindTask( NULL ))->pr_Result2
+        ));
 	Alert( AT_DeadEnd | AN_DOSLib | AG_ProcCreate );
     }
-
-    CloseLibrary((struct Library*)ExpansionBase);
 }
 
 BOOL mount( struct DeviceNode *dn ) 
@@ -258,4 +283,46 @@ BOOL mount( struct DeviceNode *dn )
     }
     
     return rc;
+}
+
+BOOL isBootable( CONST_STRPTR deviceName )
+{
+    BOOL            result = FALSE;
+    BPTR            lock;
+    STRPTR          buffer;
+    LONG            bufferLength;
+    struct InfoData info;
+    
+    bufferLength = strlen( deviceName ) + 2;
+    
+    if( (buffer = AllocMem( bufferLength, MEMF_ANY ) ) == NULL ) 
+    {
+        Alert( AT_DeadEnd | AG_NoMemory | AN_DOSLib );
+    }
+    
+    strcpy( buffer, deviceName );        
+    strcat( buffer, ":" );
+       
+    if( (lock = Lock( buffer, SHARED_LOCK )) == 0 )
+    {
+        D(bug( "dosboot: could not lock '%s'\n", buffer ));
+        goto cleanup;
+    }
+    
+    if( !Info( lock, &info ) )
+    {
+        D(bug( "dosboot: could not get info on '%s'\n", buffer ));
+        goto cleanup;
+    }
+    
+    if( info.id_DiskType != ID_NO_DISK_PRESENT )
+    {
+        result = TRUE;
+    }
+   
+cleanup:
+    if( buffer != NULL ) FreeMem( buffer, bufferLength );
+    if( lock   != 0    ) UnLock( lock );
+    
+    return result;
 }
