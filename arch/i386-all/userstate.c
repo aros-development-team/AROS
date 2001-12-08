5217459ef8dbc401086e41a8981adb12ae935806
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-12-08 18:32:01 +0000

    Fixed copyright notice.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12742 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-all/userstate.c
+++ arch/i386-all/userstate.c
@@ -1,5 +1,5 @@
 /*
-    (C) 1995-96 AROS - The Amiga Research OS
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: Switch CPU to userstate (if supported)
