a7ce88c20891ac254259977a8358d7be6ec08510
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-12-20 21:53:18 +0000

    tm_wday and tm_mon start from 1 in struct tm
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12825 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/gmtime.c
+++ compiler/clib/gmtime.c
@@ -93,7 +93,7 @@ static char monthtable[] =
     utim.tm_hour = tim % 24;
     tim = tim / 24 + 719162;
 
-    utim.tm_wday = (tim + 1) % 7;
+    utim.tm_wday = (tim + 1) % 7 + 1;
 
     /* 146097 number of days from 1.1.1 to 1.1.401 */
     utim.tm_year = tim / 146097 * 400 - 1899;
@@ -155,7 +155,7 @@ static char monthtable[] =
 	tim-=monthtable[i];
     }
 
-    utim.tm_mon = i;
+    utim.tm_mon = i + 1;
     utim.tm_mday = tim + 1;
 
     utim.tm_isdst = __dstflag;
