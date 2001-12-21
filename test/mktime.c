4468bcaa3a7f13a7a18f0efdea891e2fde2e40e6
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-12-21 00:43:33 +0000

    Added test for mktime and gmtime C-lib functions.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12830 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/mktime.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(void)
+{
+    time_t t;
+    struct tm *tm;
+  
+    t=time(NULL);
+    printf("Time before: %d\n", (int)t);
+    tm=gmtime(&t);
+    puts(asctime(tm));
+    t=mktime(tm);
+    printf("Time after: %d\n", (int)t);
+  
+    return 0;
+}
+    
