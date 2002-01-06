d9f7288c9f0f5ffcf639824a94a6068fc174f49b
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-01-06 15:41:52 +0000

    If CREATE_ROM is define GfxBase is taken from data variable passed when TOF_Blank is called.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12927 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/graphics/graphics_init.c
+++ rom/graphics/graphics_init.c
@@ -296,6 +296,9 @@ AROS_UFH4(ULONG, TOF_VBlank,
     AROS_USERFUNC_INIT
 
     struct Node *tNode;
+#ifdef CREATE_ROM
+    struct GfxBase * GfxBase = (struct GfxBase *)data;
+#endif
 
     if(!IsListEmpty(&GfxBase->TOF_WaitQ))
     {
