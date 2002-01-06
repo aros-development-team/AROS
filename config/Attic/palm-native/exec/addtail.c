6c622eeed68f4c7674d6fef3ba67f46d39e92fe9
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-01-06 00:41:03 +0000

    Removed two of the asserts since they seem to need SysBase passed to the function as well which is not the case for those two functions.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12910 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ config/Attic/palm-native/exec/addtail.c
@@ -0,0 +1,72 @@
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: Add a node at the end of a list.
+    Lang: english
+*/
+#include <exec/lists.h>
+#include <proto/exec.h>
+
+/*****************************************************************************
+
+    NAME */
+
+	AROS_LH2I(void, AddTail,
+
+/*  SYNOPSIS */
+	AROS_LHA(struct List *, list, A0),
+	AROS_LHA(struct Node *, node, A1),
+
+/*  LOCATION */
+	struct ExecBase *, SysBase, 41, Exec)
+
+/*  FUNCTION
+	Insert Node node at the end of a list.
+
+    INPUTS
+	list - The list to insert the node into
+	node - This node is to be inserted
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+	struct List * list;
+	struct Node * pred;
+
+	// Insert Node at end of the list
+	AddTail (list, node);
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+	26-08-95    digulla created after EXEC-Routine
+	26-10-95    digulla adjusted to new calling scheme
+
+******************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+//  ASSERT_VALID_PTR(node); argh! TypeOfMem() doesn't know about the data segment!
+//    ASSERT_VALID_PTR(list);
+
+    /*
+	Make the node point to the head of the list. Our predecessor is the
+	previous last node of the list.
+    */
+    node->ln_Succ	       = (struct Node *)&list->lh_Tail;
+    node->ln_Pred	       = list->lh_TailPred;
+
+    /*
+	Now we are the last now. Make the old last node point to us
+	and the pointer to the last node, too.
+    */
+    list->lh_TailPred->ln_Succ = node;
+    list->lh_TailPred	       = node;
+    AROS_LIBFUNC_EXIT
+} /* AddTail */
