8f4d835d59d329b2ebfc7cc556edcb3c2e2f3632
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-01-17 20:58:38 +0000

    removed same SAS C specifc pragams which seem to be useless.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13043 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/reqtools/rtlocale.h
+++ workbench/libs/reqtools/rtlocale.h
@@ -14,16 +14,6 @@ char *REGARGS GetStr (struct Catalog *, char *);
 struct Catalog *REGARGS RT_OpenCatalog (struct Locale *);
 void REGARGS RT_CloseCatalog (struct Catalog *);
 
-#ifndef _AROS
-
-#ifdef __SASC
-#pragma libcall LocaleBase OpenCatalogA 96 A9803
-#pragma libcall LocaleBase CloseCatalog 24 801
-#pragma libcall LocaleBase GetCatalogStr 48 90803
-#endif
-
-#endif
-
 void CloseCatalog (struct Catalog *);
 STRPTR GetCatalogStr (struct Catalog *, LONG, STRPTR);
 struct Catalog *OpenCatalogA (struct Locale *, STRPTR, struct TagItem *);
