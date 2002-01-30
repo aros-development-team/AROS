05647f72fa3ac88bcfde6d804046594648c6bc54
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-01-30 08:57:20 +0000

    The brand new PCI hidd. Scanning work properly, Querying is done as well.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13081 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/i386-pc/Drivers/pci.hidd/pci_hw.c
@@ -0,0 +1,124 @@
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: PCI class
+    Lang: english
+*/
+
+#include <exec/types.h>
+#include <exec/lists.h>
+#include <exec/memory.h>
+#include <proto/exec.h>
+#include <oop/oop.h>
+#include <hidd/pci.h>
+
+#include "pci.h"
+#include "pci_hw.h"
+
+#ifdef SysBase
+#	undef SysBase
+#endif /* SysBase */
+
+#ifdef rkprintf
+#	undef rkprintf
+#endif
+
+char stab[400];
+
+#define rkprintf(x...)	scr_RawPutChars(stab, sprintf(stab, x))
+
+ULONG readPCIConfigLong(UBYTE bus, UBYTE dev, UBYTE sub, UBYTE word)
+{
+	ULONG addr = 0x80000000;
+	addr |= (bus & 0xff) << 16;
+	addr |= (dev & 0x1f) << 11;
+	addr |= (sub & 0x07) << 8;
+	addr |= (word & 0x3f) << 2;
+	
+	outl(PCI_AddressPort, addr);
+	addr=inl(PCI_DataPort);
+
+	return addr;
+}
+
+void writePCIConfigLong(UBYTE bus, UBYTE dev, UBYTE sub, UBYTE word, ULONG value)
+{
+	ULONG addr = 0x80000000;
+	addr |= (bus & 0xff) << 16;
+	addr |= (dev & 0x1f) << 11;
+	addr |= (sub & 0x07) << 8;
+	addr |= (word & 0x3f) << 2;
+	
+	outl(PCI_AddressPort, addr);
+	outl(PCI_DataPort, value);
+}
+
+int isPCIDeviceAvailable(UBYTE bus, UBYTE dev, UBYTE sub)
+{
+	int ret=0;
+	ULONG val = readPCIConfigLong(bus, dev, sub, 0);
+
+	if ((val & 0xffff) != 0xffff) ret=1;
+
+	return ret;
+}
+
+void scanPCIBuses(struct pci_staticdata *psd, struct ExecBase *SysBase)
+{
+	int bus;
+	int dev;
+	int sub;
+
+	for (bus=0; bus < 256; bus++)
+	{
+		for (dev=0; dev < 32; dev++)
+		{
+			for (sub=0; sub < 8; sub++)
+			{
+				if (isPCIDeviceAvailable(bus, dev, sub))
+				{
+					Noded_PCI_Device *ndev;
+					ndev = AllocMem(sizeof (Noded_PCI_Device), MEMF_PUBLIC | MEMF_CLEAR);
+					ndev->node.ln_Pri = bus - 128;
+					if (ndev)
+					{
+						ULONG val;
+						HIDDT_PCI_Device *d = &ndev->dev;
+						d->Address = (bus << 8) | (dev << 3) | (sub);
+						val = readPCIConfigLong(bus, dev, sub, 0);
+						d->VendorID = val & 0xffff;
+						d->DeviceID = val >> 16;
+						val = readPCIConfigLong(bus, dev, sub, 2);
+						d->RevisionID	= val & 0xff;
+						d->Interface	= (val >> 8) & 0xff;
+						d->SubClass	= (val >> 16) & 0xff;
+						d->Class		= (val >> 24) & 0xff;
+						val = readPCIConfigLong(bus, dev, sub, 11);
+						d->SubsystemVendorID	= val && 16;
+						d->SubsystemID			= val >> 16;
+						val = readPCIConfigLong(bus, dev, sub, 15);
+						d->INTLine	= (val >> 8) & 0xff;
+						d->IRQLine = val & 0xff;
+
+						d->BaseAddress[0] = (APTR)readPCIConfigLong(bus, dev, sub, 4);
+						d->BaseAddress[1] = (APTR)readPCIConfigLong(bus, dev, sub, 5);
+						d->BaseAddress[2] = (APTR)readPCIConfigLong(bus, dev, sub, 6);
+						d->BaseAddress[3] = (APTR)readPCIConfigLong(bus, dev, sub, 7);
+						d->BaseAddress[4] = (APTR)readPCIConfigLong(bus, dev, sub, 8);
+						d->BaseAddress[5] = (APTR)readPCIConfigLong(bus, dev, sub, 9);
+
+						rkprintf("PCI: %d:%d:%d = %04.4lx:%04.4lx (%d/%d/%d), 0x%08.8lx 0x%08.8lx 0x%08.8lx\n",
+							bus,dev,sub,
+							d->VendorID,d->DeviceID,
+							d->Class,d->SubClass,d->Interface,
+							d->BaseAddress[0],d->BaseAddress[1],d->BaseAddress[2]
+							);
+
+						Enqueue(&psd->devices, (struct Node*)ndev);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ arch/i386-pc/Drivers/pci.hidd/pciclass.c
@@ -0,0 +1,205 @@
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: PCI class
+    Lang: english
+*/
+
+#define AROS_ALMOST_COMPATIBLE 1
+
+#include <hidd/pci.h>
+
+#include <proto/exec.h>
+#include <proto/utility.h>
+#include <proto/oop.h>
+
+#include <exec/alerts.h>
+#include <exec/memory.h>
+#include <exec/lists.h>
+#include <aros/system.h>
+
+#include "pci.h"
+
+#define DEBUG 1
+#include <aros/debug.h>
+
+/* Don't initialize them with "= 0", otherwise they end up in the DATA segment! */
+
+static OOP_AttrBase HiddPCIAttrBase;
+
+static struct OOP_ABDescr attrbases[] =
+{
+    { IID_Hidd_PCI,	&HiddPCIAttrBase	},
+    { NULL, NULL }
+};
+
+struct pci_data
+{
+    int	dummy;	//dummy!!!!!!!!!
+};
+
+/*** PCI::FindDevice() *******************************************/
+
+static HIDDT_PCI_Device *pci_finddevice(OOP_Class *cl, OOP_Object *obj, struct pHidd_PCI_FindDevice *msg)
+{
+	HIDDT_PCI_Device mask, *dev;
+	APTR *ret, *walk;
+	int length;
+	
+	EnterFunc(bug("PCI::FindDevice()\n"));
+
+	mask.VendorID			= GetTagData(tHidd_PCI_VendorID, -1, msg->deviceTags);
+	mask.DeviceID			= GetTagData(tHidd_PCI_DeviceID, -1, msg->deviceTags);
+	mask.RevisionID			= GetTagData(tHidd_PCI_RevisionID, -1, msg->deviceTags);
+	mask.Class				= GetTagData(tHidd_PCI_Class, -1, msg->deviceTags);
+	mask.SubClass			= GetTagData(tHidd_PCI_SubClass, -1, msg->deviceTags);
+	mask.Interface			= GetTagData(tHidd_PCI_Interface, -1, msg->deviceTags);
+	mask.SubsystemVendorID	= GetTagData(tHidd_PCI_SubsystemVendorID, -1, msg->deviceTags);
+	mask.SubsystemID		= GetTagData(tHidd_PCI_SubsystemID, -1, msg->deviceTags);
+
+	ListLength(&PSD(cl)->devices, length);
+	
+	ret = walk = AllocVec(sizeof(APTR)*length, MEMF_PUBLIC | MEMF_CLEAR);
+
+	ForeachNode(&PSD(cl)->devices, dev)
+	{
+		if (mask.VendorID != 0xffff)
+		{
+			if (mask.VendorID != dev->VendorID)
+				break;
+		}
+		if (mask.DeviceID != 0xffff)
+		{
+			if (mask.DeviceID != dev->DeviceID)
+				break;
+		}
+		if (mask.RevisionID != 0xff)
+		{
+			if (mask.RevisionID != dev->RevisionID)
+				break;
+		}
+		if (mask.Class != 0xff)
+		{
+			if (mask.Class != dev->Class)
+				break;
+		}
+		if (mask.SubClass != 0xff)
+		{
+			if (mask.SubClass != dev->SubClass)
+				break;
+		}
+		if (mask.Interface != 0xff)
+		{
+			if (mask.Interface != dev->Interface)
+				break;
+		}
+		if (mask.SubsystemVendorID != 0xffff)
+		{
+			if (mask.SubsystemVendorID != dev->SubsystemVendorID)
+				break;
+		}
+		if (mask.SubsystemID != 0xffff)
+		{
+			if (mask.SubsystemID != dev->SubsystemID)
+				break;
+		}
+		*walk++ = dev;
+	}
+
+	ReturnPtr("PCI::FindDevice", HIDDT_PCI_Device *, ret);
+}
+
+/*** PCI::FreeQuery() ********************************************/
+
+static void pci_freequery(OOP_Class *cl, OOP_Object *obj, struct pHidd_PCI_FreeQuery *msg)
+{
+	if (msg->devices)
+	{
+		FreeVec(msg->devices);
+	}
+}
+
+/*************************** Classes *****************************/
+
+#undef OOPBase
+#undef SysBase
+#undef UtilityBase
+
+#define SysBase     (psd->sysbase)
+#define OOPBase     (psd->oopbase)
+#define UtilityBase (psd->utilitybase)
+
+#define NUM_PCI_METHODS moHidd_PCI_NumMethods
+
+OOP_Class *init_pciclass (struct pci_staticdata *psd)
+{
+    OOP_Class *cl = NULL;
+    BOOL  ok  = FALSE;
+    
+    struct OOP_MethodDescr pcihidd_descr[NUM_PCI_METHODS + 1] = 
+    {
+		{(IPTR (*)())pci_finddevice,	moHidd_PCI_FindDevice},
+		{(IPTR (*)())pci_freequery,		moHidd_PCI_FreeQuery},
+        {NULL, 0UL}
+    };
+    
+    struct OOP_InterfaceDescr ifdescr[] =
+    {
+        {pcihidd_descr, IID_Hidd_PCI, NUM_PCI_METHODS},
+        {NULL, NULL, 0}
+    };
+    
+    OOP_AttrBase MetaAttrBase = OOP_GetAttrBase(IID_Meta);
+        
+    struct TagItem tags[] =
+    {
+        { aMeta_SuperID,                (IPTR)CLID_Root},
+
+        { aMeta_InterfaceDescr,         (IPTR)ifdescr},
+        { aMeta_ID,                     (IPTR)CLID_Hidd_PCI},
+        { aMeta_InstSize,               (IPTR)sizeof (struct pci_data) },
+        {TAG_DONE, 0UL}
+    };
+    
+    EnterFunc(bug("    init_pciclass(psd=%p)\n", psd));
+
+    cl = OOP_NewObject(NULL, CLID_HiddMeta, tags);
+    D(bug("Class=%p\n", cl));
+    if(cl)
+    {
+        D(bug("PCIClass ok\n"));
+        cl->UserData = (APTR)psd;
+       	psd->pciclass = cl;
+        
+        ok = TRUE;
+    }
+
+    if(ok == FALSE)
+    {
+        free_pciclass(psd);
+        cl = NULL;
+    }
+    else
+    {
+        OOP_AddClass(cl);
+    }
+
+    ReturnPtr("init_pciclass", OOP_Class *, cl);
+}
+
+void free_pciclass(struct pci_staticdata *psd)
+{
+    EnterFunc(bug("free_pciclass(psd=%p)\n", psd));
+
+    if(psd)
+    {
+        OOP_RemoveClass(psd->pciclass);
+	
+        if(psd->pciclass) OOP_DisposeObject((OOP_Object *) psd->pciclass);
+        psd->pciclass = NULL;
+    }
+
+    ReturnVoid("free_pciclass");
+}
+
