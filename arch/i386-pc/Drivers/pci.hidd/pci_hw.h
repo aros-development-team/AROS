cafbaf857e3f114f7f3c86ede1da6c74785e9e6d
jogr0326 <jogr0326@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-02-05 08:55:24 +0000

    Updated the PCI bus scanning algorithm. Only scans the present buses instead of all 256 now. It also has proper handling of multifunction devices.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13122 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-pc/Drivers/pci.hidd/pci_hw.h
+++ arch/i386-pc/Drivers/pci.hidd/pci_hw.h
@@ -9,15 +9,6 @@
     Lang: English.
 */
 
-#define inl(port) \
-    ({	long __value;	\
-	__asm__ __volatile__ ("inl %%dx,%%eax":"=a"(__value):"d"(port));	\
-	__value;	})
-
-#define outl(port,val) \
-    ({	long __value=(val);	\
-	__asm__ __volatile__ ("outl %%eax,%%dx"::"a"(__value),"d"(port)); })
-
 #define PCI_AddressPort	0x0cf8
 #define PCI_DataPort	0x0cfc
 
