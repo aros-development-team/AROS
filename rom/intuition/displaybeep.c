28bc2f006e54735edc35ec5d4535e0c18d5aa4c3
elesueur <elesueur@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-02-15 10:22:56 +0000

    Fixed !/~ mix up.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13231 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/intuition/displaybeep.c
+++ rom/intuition/displaybeep.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2002, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: Intuition function DisplayBeep()
@@ -54,11 +54,11 @@
     struct timerequest *BeepTimerIO = NULL;
     BOOL VisualBeep = TRUE;
     BOOL AllScreens = FALSE;
-    
+
     TimerMsgPort = CreateMsgPort();
     BeepTimerIO = (struct timerequest *) CreateIORequest (TimerMsgPort, sizeof (struct timerequest));
     OpenDevice ("timer.device", UNIT_VBLANK, (struct IORequest *) BeepTimerIO, 0);
-    
+
     BeepTimerIO->tr_time.tv_secs = 0;
     BeepTimerIO->tr_time.tv_micro = 250000;
     BeepTimerIO->tr_node.io_Command = TR_ADDREQUEST;
@@ -79,64 +79,64 @@
 	{
 	    if (!(screen->Flags & BEEPING))
 	    {
-	        if (screen->RastPort.BitMap->Depth <= 8)
-	        // visual beep on CLUT-screen
-	        {
- 	            /*
-	            struct DrawInfo *DInfo = NULL;
- 	            UWORD BGPen;
- 	            ULONG color[3];
+		if (screen->RastPort.BitMap->Depth <= 8)
+		// visual beep on CLUT-screen
+		{
+		    /*
+		    struct DrawInfo *DInfo = NULL;
+		    UWORD BGPen;
+		    ULONG color[3];
 
-	            DInfo = GetScreenDrawInfo (screen);
-	            BGPen = DInfo->dri_Pens[BACKGROUNDPEN];
- 	            */
-	
-	            Forbid();
-	
-	            GetRGB32 (screen->ViewPort.ColorMap, 0, 1, GetPrivScreen(screen)->DisplayBeepColor0);
- 	            screen->SaveColor0 = GetRGB4 (screen->ViewPort.ColorMap, 0);
-	
-	            SetRGB32 (&screen->ViewPort, 0,
-	        	      GetPrivScreen(screen)->DisplayBeepColor0[0] - 0x7FFFFFFF,
-	        	      GetPrivScreen(screen)->DisplayBeepColor0[1] - 0x7FFFFFFF,
-	        	      GetPrivScreen(screen)->DisplayBeepColor0[2] - 0x7FFFFFFF
-	        	     );
- 	            screen->Flags |= BEEPING;
-	            Permit();
-	
-	            // FreeScreenDrawInfo (screen, DInfo);
-	        }
-	        else
-	        // visual beep on hi- and truecolor screens
-	        {
-	            // struct Window *BeepWindow;
-	            struct TagItem window_tags[] = {
-	        	    {WA_Left, 0},
-	        	    {WA_Top, 0},
-	        	    {WA_Width, -1},
-	        	    {WA_Height, -1},
-	        	    {WA_Flags, WFLG_SIMPLE_REFRESH | WFLG_BORDERLESS},
-	        	    {WA_CustomScreen, (IPTR) screen},
-	        	    {WA_Priority, 50},  // Place in front of all other windows!
-	        	    {TAG_DONE, 0}
-	            };
-	
-	            Forbid();
-	            GetPrivScreen(screen)->DisplayBeepWindow = (struct Window *) OpenWindowTagList (NULL, window_tags);
-	            screen->Flags |= BEEPING;
-	            Permit();
-	        }
-	
-	        //LockIBase();
-	        if (AllScreens) screen = screen->NextScreen;
-	        //UnlockIBase();
+		    DInfo = GetScreenDrawInfo (screen);
+		    BGPen = DInfo->dri_Pens[BACKGROUNDPEN];
+		    */
+
+		    Forbid();
+
+		    GetRGB32 (screen->ViewPort.ColorMap, 0, 1, GetPrivScreen(screen)->DisplayBeepColor0);
+		    screen->SaveColor0 = GetRGB4 (screen->ViewPort.ColorMap, 0);
+
+		    SetRGB32 (&screen->ViewPort, 0,
+			      GetPrivScreen(screen)->DisplayBeepColor0[0] - 0x7FFFFFFF,
+			      GetPrivScreen(screen)->DisplayBeepColor0[1] - 0x7FFFFFFF,
+			      GetPrivScreen(screen)->DisplayBeepColor0[2] - 0x7FFFFFFF
+			     );
+		    screen->Flags |= BEEPING;
+		    Permit();
+
+		    // FreeScreenDrawInfo (screen, DInfo);
+		}
+		else
+		// visual beep on hi- and truecolor screens
+		{
+		    // struct Window *BeepWindow;
+		    struct TagItem window_tags[] = {
+			    {WA_Left, 0},
+			    {WA_Top, 0},
+			    {WA_Width, -1},
+			    {WA_Height, -1},
+			    {WA_Flags, WFLG_SIMPLE_REFRESH | WFLG_BORDERLESS},
+			    {WA_CustomScreen, (IPTR) screen},
+			    {WA_Priority, 50},  // Place in front of all other windows!
+			    {TAG_DONE, 0}
+		    };
+
+		    Forbid();
+		    GetPrivScreen(screen)->DisplayBeepWindow = (struct Window *) OpenWindowTagList (NULL, window_tags);
+		    screen->Flags |= BEEPING;
+		    Permit();
+		}
+
+		//LockIBase();
+		if (AllScreens) screen = screen->NextScreen;
+		//UnlockIBase();
 	    }
 	} while (AllScreens && screen);
     }
-    
+
 /* Wait */
     DoIO ((struct IORequest *) BeepTimerIO);
-    
+
 /* Stop Beep */
     if (VisualBeep)
     {
@@ -156,12 +156,12 @@
 		{
 		    Forbid();
 		    // SetRGB4 (&screen->ViewPort, 0, screen->SaveColor0 & 0x000F, (screen->SaveColor0 & 0x00F0) >> 4, (screen->SaveColor0 & 0x0F00) >> 8);
-		    SetRGB32 (&screen->ViewPort, 0, 
-		    	      GetPrivScreen(screen)->DisplayBeepColor0[0],
-		    	      GetPrivScreen(screen)->DisplayBeepColor0[1],
-		    	      GetPrivScreen(screen)->DisplayBeepColor0[2]
+		    SetRGB32 (&screen->ViewPort, 0,
+			      GetPrivScreen(screen)->DisplayBeepColor0[0],
+			      GetPrivScreen(screen)->DisplayBeepColor0[1],
+			      GetPrivScreen(screen)->DisplayBeepColor0[2]
 			     );
-		    screen->Flags &= !BEEPING;
+		    screen->Flags &= ~BEEPING;
 		    Permit();
 		}
 		else
@@ -172,16 +172,16 @@
 			Forbid();
 			CloseWindow (GetPrivScreen(screen)->DisplayBeepWindow);
 			GetPrivScreen(screen)->DisplayBeepWindow = NULL;
-			screen->Flags &= !BEEPING;
+			screen->Flags &= ~BEEPING;
 			Permit();
 		    }
 		}
             }
-	    
+
             //LockIBase();
 	    if (AllScreens) screen = screen->NextScreen;
 	    //UnlockIBase();
-	    
+
 	} while (AllScreens && screen);
     }
 
@@ -193,7 +193,7 @@
 #warning TODO: Make this "multitasking proof"!
 #warning TODO: Produce beep according to prefs
 #warning TODO: Check if BEEPING flag is handled correctly
-#warning TODO: Make BeepWindow 50% transparent! :-) 
+#warning TODO: Make BeepWindow 50% transparent! :-)
 #warning TODO: Use TimerIO (IntuitionBase->TimerIO) instead of self-made BeepTimerIO?
 
     AROS_LIBFUNC_EXIT
