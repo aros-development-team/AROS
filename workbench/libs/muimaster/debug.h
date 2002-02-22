612e7d1bf7fd00f49b21f047f2d3460ebd8f0496
sebauer <sebauer@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-02-22 18:47:27 +0000

    Written a test application for test cases. Notifyclass seems to work (but the files all need a lot of finetuning) on amiga
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13297 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/muimaster/debug.h
@@ -0,0 +1,31 @@
+#ifndef __DEBUG_H
+#define __DEBUG_H
+
+/* Debug Macros */
+
+#ifdef _AROS
+
+#undef DEBUG
+
+#ifdef MYDEBUG
+#define DEBUG 1
+#else
+#define DEBUG 0
+#endif
+#include <aros/debug.h>
+
+#else /* _AROS */
+
+#define bug kprintf
+
+#ifdef MYDEBUG
+void kprintf(char *string, ...);
+#define D(x) {kprintf("%s/%ld (%s): ", __FILE__, __LINE__, FindTask(NULL)->tc_Node.ln_Name);(x);};
+#else
+#define D(x) ;
+
+#endif /* MYDEBUG */
+
+#endif /*_AROS */
+
+#endif /* __DEBUG_H */
