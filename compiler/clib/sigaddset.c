00c52df6b2417e698d4904a87f5d0e06583f843c
hkiel <hkiel@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-02-24 12:44:18 +0000

    contrib/development/build/makedepend needs these functions, so I added empty templates which return "failure" so that mekedepend at least compiles.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13312 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/sigaddset.c
@@ -0,0 +1,45 @@
+/*
+    Copyright © 1995-2002, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: ANSI C function sigaddset()
+    Lang: English
+*/
+
+/*****************************************************************************
+
+    NAME */
+#include <signal.h>
+
+	int sigaddset (
+
+/*  SYNOPSIS */
+	sigset_t *set,
+	int signum)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+{
+#warning TODO: implement sigaddset()
+
+    return -1; /* return failure */
+
+} /* sigaddset */
+
