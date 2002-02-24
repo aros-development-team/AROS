8267058486660c172af4e70fd51fb6f13d131103
sebauer <sebauer@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-02-24 20:03:30 +0000

    compiles now on amiga again
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13322 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/test.c
+++ workbench/libs/muimaster/test.c
@@ -33,9 +33,10 @@ void main(void)
 
     MUIMasterBase_instance.sysbase = *((struct ExecBase **)4);
     MUIMasterBase_instance.utilitybase = OpenLibrary("utility.library",37);
+    MUIMasterBase_instance.aslbase = OpenLibrary("asl.library",37);
+    MUIMasterBase_instance.gfxbase = OpenLibrary("graphics.library",37);
+    MUIMasterBase_instance.intuibase = OpenLibrary("intuition.library",37);
     __zune_prefs_init(&__zprefs);
-
-
 
     app = ApplicationObject,
     	SubWindow, wnd = WindowObject,
