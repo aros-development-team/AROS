38e8dda254d86ff0fff835dec229bf435ef91826
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-02-27 03:56:28 +0000

    Only compiles if AROS_NO_SIGNAL_H is not set.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13367 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/sigpending.c
+++ compiler/clib/sigpending.c
@@ -9,6 +9,7 @@
 /*****************************************************************************
 
     NAME */
+#ifndef AROS_NO_SIGNAL_H
 #include <signal.h>
 
 	int sigpending (
@@ -42,3 +43,4 @@
 
 } /* sigpending */
 
+#endif
