4eec5aef7789842e54ed2d573b03c589db6ce1e8
sebauer <sebauer@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-02-27 18:29:48 +0000

    Added my older drag and drop stuff
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13375 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/muimaster/dragndrop.h
@@ -0,0 +1,28 @@
+#ifndef _DRAGNDROP_H
+#define _DRAGNDROP_H
+
+/* Tags for GUI_CreateBitMapNodeA() */
+#define GUI_BitMap				(TAG_USER+1)	/* struct BitMap * */
+#define GUI_Mask					(TAG_USER+2)	/* APTR */
+#define GUI_LeftOffset		(TAG_USER+3)	/* LONG */
+#define GUI_TopOffset		(TAG_USER+4)	/* LONG */
+#define GUI_Width				(TAG_USER+5)	/* LONG */
+#define GUI_Height				(TAG_USER+6)	/* LONG */
+
+struct DragNDrop *CreateDragNDropA( struct TagItem *tlist );
+VOID DeleteDragNDrop( struct DragNDrop *dnd );
+
+BOOL PrepareDragNDrop(struct DragNDrop *dnd,struct Screen *scr);
+VOID FinishDragNDrop(struct DragNDrop *dnd);
+
+VOID DrawDragNDrop(struct DragNDrop *dnd, LONG x, LONG y);
+VOID UndrawDragNDrop(struct DragNDrop *dnd);
+
+struct BitMapNode *CreateBitMapNodeA( struct TagItem *tagList );
+struct BitMapNode *CreateBitMapNode( ULONG tags,... );
+VOID DeleteBitMapNode(struct BitMapNode *bmn );
+
+VOID AttachBitMapNode( struct DragNDrop *dnd, struct BitMapNode *bmn );
+VOID DetachBitMapNode( struct BitMapNode *bmn );
+
+#endif
