f5f34b488716679f3b2e5c70bbbe317797f437c3
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-03-02 21:27:00 +0000

    Dummy bullet.library, to get the autogenerated includes.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13427 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/bullet/openengine.c
@@ -0,0 +1,45 @@
+/*
+    Copyright © 1995-2002, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: Bullet function OpenEngine()
+    Lang: English
+*/
+
+/*****************************************************************************
+
+    NAME */
+
+	AROS_LH0(struct GlyphEngine *, OpenEngine,
+
+/*  SYNOPSIS */
+
+/*  LOCATION */
+	struct Library *, BulletBase, 5, Bullet)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+    AROS_LIBBASE_EXT_DECL(struct Library *,BulletBase)
+    
+    AROS_LIBFUNC_EXIT
+
+} /* OpenEngine */
