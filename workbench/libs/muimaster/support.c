aa723e4892b7c4a248c46536771019c5764ed614
sebauer <sebauer@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-03-03 22:36:53 +0000

    Changed the condition in isRegionWithinBounds() as suggested by Georg.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13446 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/support.c
+++ workbench/libs/muimaster/support.c
@@ -24,8 +24,8 @@ extern struct Library *MUIMasterBase;
 **************************************************************************/
 int isRegionWithinBounds(struct Region *r, int left, int top, int width, int height)
 {
-	if ((left < r->bounds.MinX) && (left + width  - 1 > r->bounds.MaxX)
-	 && (top  < r->bounds.MinY) && (top  + height - 1 > r->bounds.MaxY))
+	if ((left <= r->bounds.MinX) && (left + width  - 1 >= r->bounds.MaxX)
+	 && (top  <= r->bounds.MinY) && (top  + height - 1 >= r->bounds.MaxY))
 		return 1;
 
 	return 0;
