33f18d1ea40a70a5f1797b3c76a7c533eece8177
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-03-06 18:42:41 +0000

    Does not have anymore it's own LoadSeg_AOS, but instead calls the normal LoadSeg() from dos.library.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13472 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/diskfont/diskfont_io.c
+++ workbench/libs/diskfont/diskfont_io.c
@@ -27,303 +27,6 @@
 
 /****************************************************************************************/
 
-VOID EndianConv(ULONG *valptr)
-{
-    ULONG tmp;
-
-    tmp = UB(valptr)[0] << 24 | UB(valptr)[1] << 16 | UB(valptr)[2] << 8 | UB(valptr)[3];
-
-    *valptr = tmp;
-
-    return;	
-}
-
-/****************************************************************************************/
-
-struct hunk
-{
-    ULONG size;
-    UBYTE *memory;
-};
-
-/****************************************************************************************/
-
-static int read_block(BPTR file, APTR buffer, ULONG size)
-{
-    LONG subsize;
-    UBYTE *buf=(UBYTE *)buffer;
-
-    while(size)
-    {
-	subsize=Read(file,buf,size);
-	if(subsize==0)
-	{
-	    ((struct Process *)FindTask(NULL))->pr_Result2=ERROR_BAD_HUNK;
-	    return 1;
-	}
-	if(subsize<0)
-	    return 1;
-	buf+=subsize;
-	size-=subsize;
-    }
-    return 0;
-}
-
-/****************************************************************************************/
-
-BPTR LoadSeg_AOS(BPTR file)
-{
-    struct hunk 	*hunktab = NULL;
-    ULONG 		hunktype, count = 0, first, last, offset, curhunk = 0, numhunks;
-    LONG 		t;
-    UBYTE 		name_buf[255];
-    register int 	i;
-    BPTR 		last_p = 0;
-  #if DEBUG
-    static STRPTR 	segtypes[] = { "CODE", "DATA", "BSS", };
-  #endif
-
-
-  #define ERROR(a)    { *error=a; goto end; }
-    LONG 		*error=&((struct Process *)FindTask(NULL))->pr_Result2;
-
-    if (Seek(file, 0, OFFSET_BEGINNING) < 0)
-	goto end;
-	
-    while(!read_block(file, &hunktype, sizeof(hunktype)))
-    {
-  	EndianConv(&hunktype);
-
-	switch(hunktype)
-	{
-	    ULONG tmp, req;
-
-	case HUNK_SYMBOL:
-	    while(!read_block(file, &count, sizeof(count)) && count)
-	    {
-		EndianConv(&count);
-		if (Seek(file, (count+1)*4, OFFSET_CURRENT) < 0)
-		    goto end;
-	    }
-	    break;
-	    
-	case HUNK_UNIT:
-	    if (read_block(file, &count, sizeof(count)))
-		goto end;
-	      
-	    EndianConv(&count);
-	    count /= 4;
-	    
-	    if (read_block(file, name_buf, count))
-		goto end;
-	      
-	    D(bug("HUNK_UNIT: \"%.*s\"\n", count, name_buf));
-	    break;
-	  
-	case HUNK_HEADER:
-	    D(bug("HUNK_HEADER:\n"));
-	    while (1)
-	    {
-		if (read_block(file, &count, sizeof(count)))
-		    goto end;
-		    
-		EndianConv(&count);
-		
-		if (count == 0L)
-		    break;
-		    
-		count *= 4;
-		
-		if (read_block(file, name_buf, count))
-		    goto end;
-		    
-		D(bug("\tlibname: \"%.*s\"\n", count, name_buf));
-	    }
-	    
-	    if (read_block(file, &numhunks, sizeof(numhunks)))
-		goto end;
-		
-	    EndianConv(&numhunks);
-	    
-	    D(bug("\tHunk count: %ld\n", numhunks));
-	    hunktab = (struct hunk *)AllocVec(sizeof(struct hunk) * numhunks,
-					      MEMF_CLEAR);
-	    if (hunktab == NULL)
-		ERROR(ERROR_NO_FREE_STORE);
-		
-	    if (read_block(file, &first, sizeof(first)))
-		goto end;
-		
-	    EndianConv(&first);
-	    D(bug("\tFirst hunk: %ld\n", first));
-	    curhunk = first;
-	    
-	    if (read_block(file, &last, sizeof(last)))
-		goto end;
-	      
-	    EndianConv(&last);
-	    D(bug("\tLast hunk: %ld\n", last));
-	    
-	    for (i = first; i <= last; i++)
-	    {
-		if (read_block(file, &count, sizeof(count)))
-		    goto end;
-		    
-		EndianConv(&count);
-		tmp = count & 0xC0000000;
-		count &= 0x3FFFFFFF;
-		D(bug("\tHunk %d size: 0x%06lx bytes in ", i, count*4));
-		
-		switch(tmp)
-		{
-		case 0x80000000:
-		    D(bug("FAST"));
-		    req = MEMF_FAST;
-		    break;
-		    
-		case 0x40000000:
-		    D(bug("CHIP"));
-		    req = MEMF_CHIP;
-		    break;
-		    
-		default:
-		    D(bug("ANY"));
-		    req = MEMF_ANY;
-		    break;
-		    
-		}
-		
-		D(bug(" memory\n"));
-		hunktab[i].size = count * 4;
-		hunktab[i].memory = (UBYTE *)AllocVec(hunktab[i].size + sizeof(BPTR),
-	                                	      (req | MEMF_CLEAR));
-		if (hunktab[i].memory == NULL)
-		    ERROR(ERROR_NO_FREE_STORE);
-		  
-		hunktab[i].memory += sizeof(BPTR);
-	    }
-	    break;
-	  
-	case HUNK_CODE:
-	case HUNK_DATA:
-	case HUNK_BSS:
-	    if (read_block(file, &count, sizeof(count)))
-		goto end;
-		
-	    EndianConv(&count);
-	    tmp = count & 0xC0000000;
-	    count &= 0x3fffffff;
-	    D(bug("HUNK_%s(%d): Length: 0x%06lx bytes in ",
-		  segtypes[hunktype-HUNK_CODE], curhunk, count*4));
-
-	    switch(tmp)
-	    {
-	    case 0x8000000:
-		D(bug("FAST"));
-		req = MEMF_FAST;
-		break;
-	      
-	    case 0x40000000:
-		D(bug("CHIP"));
-		req = MEMF_CHIP;
-		break;
-	      
-	    default:
-		D(bug("ANY"));
-		req = MEMF_ANY;
-		break;
-		
-	    }
-	    
-	    D(bug(" memory\n"));
-	    if (hunktype != HUNK_BSS && count)
-		if (read_block(file, hunktab[curhunk].memory, count*4))
-		    goto end;
-	    break;
-	  
-	case HUNK_RELOC32:
-	    D(bug("HUNK_RELOC32:\n"));
-	    while (1)
-	    {
-		ULONG *addr;
-
-		if (read_block(file, &count, sizeof(count)))
-		    goto end;
-		    
-		EndianConv(&count);
-		
-		if (count == 0L)
-		    break;
-		    
-		i = count;
-		
-		if (read_block(file, &count, sizeof(count)))
-		    goto end;
-		    
-		EndianConv(&count);
-		
-		D(bug("\tHunk #%ld:\n", count));
-		while (i > 0)
-		{
-		    if (read_block(file, &offset, sizeof(offset)))
-			goto end;
-			
-		    EndianConv(&offset);
-		    D(bug("\t\t0x%06lx\n", offset));
-		    addr = (ULONG *)(hunktab[curhunk].memory + offset);
-		    EndianConv(addr);
-		    *addr += (ULONG)(hunktab[count].memory);
-		    D(bug("\taddr: %p\tRelocaddr: %p\thunkaddr: %p\n", addr, *addr, hunktab[count].memory));
-		    --i;
-		}
-		D(bug("%d\n", 0x0646 -0x0076));
-	    }
-	    break;
-	  
-	case HUNK_END:
-	    D(bug("HUNK_END\n"));
-	    ++curhunk;
-	    break;
-	  
-	case HUNK_RELOC16:
-	case HUNK_RELOC8:
-	case HUNK_NAME:
-	case HUNK_EXT:
-	case HUNK_DEBUG:
-	case HUNK_OVERLAY:
-	case HUNK_BREAK:
-	default:
-	    ERROR(ERROR_BAD_HUNK);
-	  
-	} /* switch(hunktype) */
-      
-    } /* while(!read_block(file, &hunktype, sizeof(hunktype)))*/
-    
-    /* Clear caches */
-    for (t=last; t >= (LONG)first; t--)
-    {
-	if (hunktab[t].size)
-	{
-	    CacheClearE(hunktab[t].memory, hunktab[t].size, CACRF_ClearI|CACRF_ClearD);
-	    ((BPTR *)hunktab[t].memory)[-1] = last_p;
-	    last_p = MKBADDR((BPTR *)hunktab[t].memory-1);
-	}
-    }
-    FreeVec(hunktab);
-    hunktab = NULL;
-end:
-    if (hunktab != NULL)
-    {
-	for (t = first; t <= last; t++)
-	    if (hunktab[t].memory != NULL)
-		FreeVec(hunktab[t].memory - sizeof(BPTR));
-	FreeVec(hunktab);
-    }
-    return last_p;
-}
-
-/****************************************************************************************/
-
 #define SKIPLONG(ptr) 	ptr += sizeof (LONG);
 
 #define SKIPWORD(ptr)	ptr += sizeof (WORD)
@@ -352,10 +55,16 @@ We don't need endian conversion of pointers since this is done inside LoadSeg_AO
 SKIPPTR(ptr);
 */
 
+#if 0
 #define COPYPTR(ptr, destptr) \
 ((APTR)destptr) = (APTR)(ptr[0] | ptr[1] << 8 | ptr [2] << 16 | ptr[3] << 24); \
 SKIPPTR(ptr);
+#else
 
+#define COPYPTR(ptr,destptr) \
+((APTR)destptr) = *((APTR *)(ptr)); \
+SKIPPTR(ptr);
+#endif
 /****************************************************************************************/
 
 /****************/
@@ -471,12 +180,12 @@ D(bug("Textfont struct converted\n"));
     if (!tf)
 	goto failure;
 
-D(bug("charkern in temp:  %p\n", 	tmp_tf.tf_CharKern));
+    D(bug("charkern in temp:  %p\n", 	tmp_tf.tf_CharKern));
 
     /*	Copy allready converted stuff into allocated mem */
     CopyMem(&tmp_tf, tf, sizeof (struct TextFont));
 
-D(bug("tmp_tf copied, charkern=%p\n", tf->tf_CharKern));
+    D(bug("tmp_tf copied, charkern=%p\n", tf->tf_CharKern));
 
     /* Calculate size of one character data bitmap */	
     chardatasize = tf->tf_YSize * tf->tf_Modulo;
@@ -490,7 +199,7 @@ D(bug("tmp_tf copied, charkern=%p\n", tf->tf_CharKern));
 	#undef CTF
 	#define CTF(tf) ((struct ColorTextFont *)tf)
 
-D(bug("Colorfont found\n"));
+    	D(bug("Colorfont found\n"));
 
 	/* Convert extended colortextfont info */
 	CONVWORD(ptr, CTF(tf)->ctf_Flags);
@@ -548,13 +257,13 @@ D(bug("Colorfont found\n"));
     }
     else
     {
-D(bug("B&W font\t Chardatasize: %d\n", chardatasize));
+    	D(bug("B&W font\t Chardatasize: %d\n", chardatasize));
 	/* Character data for B/W fonts */
 	if (!(tf->tf_CharData = AllocVec(chardatasize, MEMF_ANY)))
 	    goto failure;
-D(bug("chardataptr=%p\n",chardata_ptr));
+    	D(bug("chardataptr=%p\n",chardata_ptr));
 	CopyMem(chardata_ptr, tf->tf_CharData, chardatasize);
-D(bug("Chardata copied\n"));
+    	D(bug("Chardata copied\n"));
     }		
 
     /* ----------------------- */
@@ -562,7 +271,8 @@ D(bug("Chardata copied\n"));
     if (!(tf->tf_Message.mn_Node.ln_Name = AllocVec( strlen(fontname) + 1, MEMF_ANY)))
 	goto failure;
     strcpy(tf->tf_Message.mn_Node.ln_Name, fontname);
-D(bug("Fontname copied, %s\n", fontname));
+
+    D(bug("Fontname copied, %s\n", fontname));
 
     /* ----------------------- */
     /* Handle taglist */
@@ -571,7 +281,8 @@ D(bug("Fontname copied, %s\n", fontname));
 	UWORD numtags = 0;
 	ULONG tag;
 	struct TagItem *taglist;
-D(bug("Tagged font\n"));
+	
+    	D(bug("Tagged font\n"));
 
 	/* Convert the tags */
 	ptr = taglist_ptr;
@@ -610,7 +321,7 @@ D(bug("Tagged font\n"));
     }
     else
     {
-D(bug("No tags, extending it\n"));
+    	D(bug("No tags, extending it\n"));
 
 	if (!ExtendFont(tf, NULL))
 	    goto failure;
@@ -618,7 +329,7 @@ D(bug("No tags, extending it\n"));
     /* ----------------------- */
     /* Allocate memory for charloc */
 
-D(bug("Doing charloc\n"));
+    D(bug("Doing charloc\n"));
     if (!(tf->tf_CharLoc = AllocVec(numchars * sizeof (ULONG) , MEMF_ANY)))
 	goto failure;
 
@@ -630,7 +341,7 @@ D(bug("Doing charloc\n"));
         CONVLONG(ptr,  *((ULONG *)destptr) ++);
 	D(bug("charloc[%d]: %x\n", i, ((ULONG *)destptr)[-1]));
     }
-D(bug("Charloc OK\n"));	
+    D(bug("Charloc OK\n"));	
 
     /* ----------------------- */
     /* Only proportional fonts have a CharSpace array */
@@ -638,7 +349,7 @@ D(bug("Charloc OK\n"));
     if (charspace_ptr/* tf->tf_Flags & FPF_PROPORTIONAL*/)
     {
 
-D(bug("Proportional font\n"));
+    	D(bug("Proportional font\n"));
 	if (!(tf->tf_CharSpace = AllocVec(numchars * sizeof (UWORD) , MEMF_ANY)))
 	    goto failure;
 
@@ -650,13 +361,13 @@ D(bug("Proportional font\n"));
 	    CONVWORD(ptr,  *destptr ++ );
 	}
 
-D(bug("Charspace OK\n"));
+    	D(bug("Charspace OK\n"));
     }
 
     /* ----------------------- */
     /* Allocate memory for charkern */
 
-D(bug("Doing Charkern, ptr =%p\n", charkern_ptr));	
+    D(bug("Doing Charkern, ptr =%p\n", charkern_ptr));	
     if (charkern_ptr)
     {
 	if (!(tf->tf_CharKern = AllocVec(numchars * sizeof (UWORD) , MEMF_ANY)))
@@ -670,11 +381,11 @@ D(bug("Doing Charkern, ptr =%p\n", charkern_ptr));
 	    CONVWORD(ptr,  *destptr ++);
 	    D(bug("Setting to %d\n", destptr[-1]));
 	}
-D(bug("Charkern OK\n"));	
+    	D(bug("Charkern OK\n"));	
     }
 
 
-D(bug("Charkern, ptr =%p\n", tf->tf_CharKern));	
+    D(bug("Charkern, ptr =%p\n", tf->tf_CharKern));	
     /* ----------------------- */
 
     ReturnPtr("ConvTextFont", struct TextFont *, tf);
@@ -775,7 +486,7 @@ struct TextFont *ReadDiskFont(
 	
 	if ((fh = Open(filename, MODE_OLDFILE)) != 0)
 	{
-	    if ((seglist = LoadSeg_AOS(fh)) != 0)
+	    if ((seglist = LoadSeg(filename)) != 0)
 	    {
 		tf = ConvDiskFont(seglist, reqattr->tta_Name, DiskfontBase);
 			
