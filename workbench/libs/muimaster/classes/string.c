c1f48ebdcb584349e8a1d4ccaa6ada840cdb8a48
sebauer <sebauer@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-03-08 09:27:56 +0000

    Stringclass which simply uses the textclass added
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13490 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/muimaster/classes/string.c
@@ -0,0 +1,85 @@
+#include <exec/types.h>
+
+#include <clib/alib_protos.h>
+#include <proto/exec.h>
+#include <proto/intuition.h>
+#include <proto/utility.h>
+#ifdef _AROS
+#include <proto/muimaster.h>
+#endif
+
+extern struct Library *MUIMasterBase;
+
+#include "muimaster_intern.h"
+#include "mui.h"
+#include "support.h"
+
+#define MYDEBUG 1
+#include "debug.h"
+
+/**************************************************************************
+ OM_NEW
+**************************************************************************/
+static ULONG String_New(struct IClass *cl, Object * obj, struct opSet *msg)
+{
+    struct MUI_SliderData *data;
+    struct TagItem *tags, *tag;
+
+    for (tags = msg->ops_AttrList; (tag = NextTagItem(&tags));)
+    {
+	switch (tag->ti_Tag)
+	{
+	}
+    }
+
+    obj = (Object *)DoSuperNew(cl, obj,
+	MUIA_Text_Editable, TRUE,
+	TAG_MORE, msg->ops_AttrList);
+
+    if (!obj)
+    {
+    	CoerceMethod(cl,obj,OM_DISPOSE);
+	return NULL;
+    }
+
+    data = INST_DATA(cl, obj);
+    return (ULONG)obj;
+}
+
+/**************************************************************************
+ OM_SET
+**************************************************************************/
+static ULONG String_Set(struct IClass *cl, Object * obj, struct opSet *msg)
+{
+    struct MUI_StringData *data = INST_DATA(cl, obj);
+    return DoSuperMethodA(cl,obj,(Msg)msg);
+}
+
+
+#ifndef _AROS
+__asm IPTR String_Dispatcher(register __a0 struct IClass *cl, register __a2 Object *obj, register __a1 Msg msg)
+#else
+AROS_UFH3S(IPTR, Slider_Dispatcher,
+	AROS_UFHA(Class  *, cl,  A0),
+	AROS_UFHA(Object *, obj, A2),
+	AROS_UFHA(Msg     , msg, A1))
+#endif
+{
+    switch (msg->MethodID)
+    {
+	case OM_NEW: return String_New(cl, obj, (struct opSet *)msg);
+	case OM_SET: return String_Set(cl, obj, (struct opSet *)msg);
+    }
+    return DoSuperMethodA(cl, obj, msg);
+}
+
+
+/*
+ * Class descriptor.
+ */
+const struct __MUIBuiltinClass _MUI_String_desc = { 
+    MUIC_String, 
+    MUIC_Text, 
+    sizeof(struct MUI_StringData), 
+    String_Dispatcher 
+};
