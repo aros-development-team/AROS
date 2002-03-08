d31d06d4aa174130abeae79e83182767ee30ab9a
sebauer <sebauer@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-03-08 19:44:24 +0000

    Added headers.c and let sas create a gst file so it also compiles fast on my Amiga. Fixed problem if the active gadget uses MUIA_Window_ActiveObject
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13500 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/muimaster/headers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include <clib/alib_protos.h>
+
+#include <proto/exec.h>
+#include <proto/graphics.h>
+#include <proto/commodities.h>
+#include <proto/layers.h>
+#include <proto/intuition.h>
+#include <proto/dos.h>
+#include <proto/iffparse.h>
+#include <proto/timer.h>
+#include <proto/utility.h>
+#include <proto/asl.h>
+
+/* for the generation of the all.gst file */
