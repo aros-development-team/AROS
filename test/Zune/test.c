39350a8dc66c935b3dfa8e3e8b337f1c4a100c63
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-03-08 16:57:22 +0000

    update
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13498 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/Zune/test.c
+++ test/Zune/test.c
@@ -22,18 +22,71 @@ Object *MUI_NewObject(char *classname, int tag,...)
     return MUI_NewObjectA(classname, (struct TagItem*)&tag);
 }
 
+AROS_UFH0(void, repeat_function)
+{
+    printf("MUI_Timer\n");
+}
+
+/* The custom class */
+
+struct DropText_Data
+{
+    ULONG times;
+};
+
+#ifndef _AROS
+__saveds __asm IPTR dispatcher(register __a0 struct IClass *cl, register __a2 Object *obj, register __a1 Msg msg)
+#else
+AROS_UFH3S(IPTR, dispatcher,
+	AROS_UFHA(Class  *, cl,  A0),
+	AROS_UFHA(Object *, obj, A2),
+	AROS_UFHA(Msg     , msg, A1))
+#endif
+{
+    switch (msg->MethodID)
+    {
+    	case MUIM_DragQuery: return MUIV_DragQuery_Accept;
+    	case MUIM_DragDrop:
+    	{
+	    struct DropText_Data *data = (struct DropText_Data*)INST_DATA(cl,obj);
+	    char buf[100];
+	    data->times++;
+	    sprintf(buf,"%ld times",data->times); /* no MUIM_SetAsString yet */
+	    set(obj,MUIA_Text_Contents,buf);
+    	}
+    }
+    return DoSuperMethodA(cl,obj,(Msg)msg);
+}
+
+struct MUI_CustomClass *CL_DropText;
+
+#define DropTextObject (Object*)NewObject(CL_DropText->mcc_Class, NULL
+
+
+/* Main prog */
+
 void main(void)
 {
     Object *app;
     Object *wnd;
+    Object *open_button,*second_wnd;
     Object *quit_button;
-
+    Object *repeat_button;
+    
+    struct Hook hook;
+    
     MUIMasterBase = OpenLibrary("muimaster.library", 0);
     if (!MUIMasterBase)
     {
     	puts("Could not open muimaster.library!\n");
 	exit(0);
     }
+
+
+    hook.h_Entry = (HOOKFUNC)repeat_function;
+
+    /* should check the result in a real program! */
+    CL_DropText = MUI_CreateCustomClass(NULL,MUIC_Text,NULL,sizeof(struct DropText_Data), dispatcher);
 
     app = ApplicationObject,
     	SubWindow, wnd = WindowObject,
@@ -46,18 +99,28 @@ void main(void)
 		    GroupFrameT("A horizontal group"),
 		    Child, ColGroup(2),
 			GroupFrameT("A column group"),
-			Child, TextObject, MUIA_CycleChain, 1, ButtonFrame, MUIA_Background, MUII_ButtonBack, MUIA_Text_PreParse, "\33c", MUIA_Text_Contents, "Button1", MUIA_InputMode, MUIV_InputMode_RelVerify, End,
-			Child, TextObject, MUIA_CycleChain, 1, ButtonFrame, MUIA_Background, MUII_ButtonBack, MUIA_Text_PreParse, "\33c", MUIA_Text_Contents, "Button2", MUIA_InputMode, MUIV_InputMode_RelVerify, End,
-			Child, TextObject, MUIA_CycleChain, 1, ButtonFrame, MUIA_Background, MUII_ButtonBack, MUIA_Text_PreParse, "\33c", MUIA_Text_Contents, "Button3", MUIA_InputMode, MUIV_InputMode_RelVerify, End,
+			Child, repeat_button = TextObject, MUIA_CycleChain, 1, ButtonFrame, MUIA_Background, MUII_ButtonBack, MUIA_Text_PreParse, "\33c", MUIA_Text_Contents, "Repeat", MUIA_InputMode, MUIV_InputMode_RelVerify, End,
+			Child, TextObject, MUIA_CycleChain, 1, ButtonFrame, MUIA_Background, MUII_ButtonBack, MUIA_Text_PreParse, "\33c", MUIA_Text_Contents, "Drag Me", MUIA_Draggable, TRUE, MUIA_InputMode, MUIV_InputMode_RelVerify, End,
+			Child, open_button = TextObject, MUIA_CycleChain, 1, ButtonFrame, MUIA_Background, MUII_ButtonBack, MUIA_Text_PreParse, "\33c", MUIA_Text_Contents, "Open Window", MUIA_InputMode, MUIV_InputMode_RelVerify, End,
 			Child, TextObject, MUIA_CycleChain, 1, ButtonFrame, MUIA_Background, MUII_ButtonBack, MUIA_Text_PreParse, "\33c", MUIA_Text_Contents, "Button4", MUIA_InputMode, MUIV_InputMode_RelVerify, End,
 			Child, TextObject, MUIA_CycleChain, 1, ButtonFrame, MUIA_Background, MUII_ButtonBack, MUIA_Text_PreParse, "\33c", MUIA_Text_Contents, "Button5", MUIA_InputMode, MUIV_InputMode_RelVerify, End,
 			Child, TextObject, MUIA_CycleChain, 1, ButtonFrame, MUIA_Background, MUII_ButtonBack, MUIA_Text_PreParse, "\33c", MUIA_Text_Contents, "Button6", MUIA_InputMode, MUIV_InputMode_RelVerify, End,
 			End,
 		    Child, VGroup,
 			GroupFrameT("A vertical group"),
-			Child, TextObject, MUIA_CycleChain, 1, ButtonFrame, MUIA_Background, MUII_ButtonBack, MUIA_Text_PreParse, "\33c", MUIA_Text_Contents, "Button7", MUIA_InputMode, MUIV_InputMode_RelVerify, End,
+			Child, DropTextObject, MUIA_CycleChain, 1, ButtonFrame, MUIA_Background, MUII_ButtonBack, MUIA_Text_PreParse, "\33c", MUIA_Text_Contents, "Drop Here", MUIA_Dropable, TRUE, MUIA_InputMode, MUIV_InputMode_RelVerify, End,
 			Child, TextObject, MUIA_CycleChain, 1, ButtonFrame, MUIA_Background, MUII_ButtonBack, MUIA_Text_PreParse, "\33c", MUIA_Text_Contents, "Button8", MUIA_InputMode, MUIV_InputMode_RelVerify, End,
 			End,
+		    End,
+		Child, StringObject,
+		    StringFrame,
+		    MUIA_CycleChain,1,
+		    MUIA_String_AdvanceOnCR, TRUE,
+		    End,
+
+    	    	Child, SliderObject,
+    	    	    MUIA_Group_Horiz, TRUE,
+    	    	    MUIA_CycleChain,1,
 		    End,
 
     	    	Child, HGroup,
@@ -74,18 +137,24 @@ void main(void)
     	    	    End,
     	        End,
     	    End,
-    	End;
+    	SubWindow, second_wnd = WindowObject,
+    	    MUIA_Window_Title, "Second window",
 
+    	    WindowContents, VGroup,
+		Child, DropTextObject, MUIA_Text_Contents, "Drop here", MUIA_Dropable, TRUE, End,
+		End,
+    	    End,
+    	End;
 
     if (app)
     {
 	ULONG sigs = 0;
 
-    	kprintf("Application Object created at 0x%lx\n",app);
-    	kprintf("Window Object created at 0x%lx\n",wnd);
-
     	DoMethod(wnd, MUIM_Notify, MUIA_Window_CloseRequest, TRUE, app, 2, MUIM_Application_ReturnID, MUIV_Application_ReturnID_Quit);
+    	DoMethod(second_wnd, MUIM_Notify, MUIA_Window_CloseRequest, TRUE, second_wnd, 3, MUIM_Set, MUIA_Window_Open, FALSE);
+    	DoMethod(open_button, MUIM_Notify, MUIA_Pressed, FALSE, second_wnd, 3,  MUIM_Set, MUIA_Window_Open, TRUE);
     	DoMethod(quit_button, MUIM_Notify, MUIA_Pressed, FALSE, app, 2, MUIM_Application_ReturnID, MUIV_Application_ReturnID_Quit);
+    	DoMethod(repeat_button, MUIM_Notify, MUIA_Timer, MUIV_EveryTime, app, 2, MUIM_CallHook, &hook);
 	set(wnd,MUIA_Window_Open,TRUE);
 
 	while((LONG) DoMethod(app, MUIM_Application_NewInput, &sigs) != MUIV_Application_ReturnID_Quit)
@@ -100,6 +169,7 @@ void main(void)
 
 	MUI_DisposeObject(app);
     }
-    
+    MUI_DeleteCustomClass(CL_DropText);
+
     CloseLibrary(MUIMasterBase);
 }
