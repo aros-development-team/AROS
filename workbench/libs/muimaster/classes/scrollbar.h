a1930eaad1c63529449de7c324100f59ca2d7d0f
sebauer <sebauer@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-03-12 21:19:18 +0000

    Scrollbar class added
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13589 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/muimaster/classes/scrollbar.h
@@ -0,0 +1,25 @@
+#ifndef _MUI_CLASSES_SCROLLBAR_H
+#define _MUI_CLASSES_SCROLLBAR_H
+
+/****************************************************************************/
+/** Scrollbar                                                              **/
+/****************************************************************************/
+
+#ifdef _DCC
+extern char MUIC_Scrollbar[];
+#else
+#define MUIC_Scrollbar "Scrollbar.mui"
+#endif
+
+/* Attributes */
+
+#define MUIA_Scrollbar_Type                 0x8042fb6b /* V11 i.. LONG              */
+
+#define MUIV_Scrollbar_Type_Default 0
+#define MUIV_Scrollbar_Type_Bottom 1
+#define MUIV_Scrollbar_Type_Top 2
+#define MUIV_Scrollbar_Type_Sym 3
+
+extern const struct __MUIBuiltinClass _MUI_Scrollbar_desc;
+
+#endif
