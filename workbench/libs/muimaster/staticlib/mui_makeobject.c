142895b716c27f744e5c4e3f2dffed414134a09c
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-03-13 19:06:19 +0000

    MUI_MakeObject vararg function was missing.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13608 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/muimaster/staticlib/mui_makeobject.c
@@ -0,0 +1,92 @@
+/*
+    Copyright (C) 1995-2000 AROS - The Amiga Research OS
+    $Id$
+
+    Desc:
+    Lang: english
+*/
+
+#include <stdarg.h>
+
+/*****************************************************************************
+
+    NAME */
+#define NO_INLINE_STDARG /* turn off inline def */
+#include <proto/muimaster.h>
+extern struct Library * MUIMasterBase;
+
+	Object * MUI_MakeObject (
+
+/*  SYNOPSIS */
+	LONG type, 
+	...)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+    va_list args;
+    IPTR    param[4];
+    WORD    i, numparams = 0;
+        
+    switch(type)
+    {
+	case 2: /* MUIO_Button */
+	case 3: /* MUIO_CheckMark */
+	case 8: /* MUIO_PopButton */
+	case 9: /* MUIO_HSpace */
+	case 10: /* MUIO_VSpace */
+	case 11: /* MUIO_HBar */
+	case 12: /* MUIO_VBar */
+	case 15: /* MUIO_BarTitle */
+    	    numparams = 1;
+	    break;
+	        
+    	case 1: /* MUIO_Label */
+	case 4: /* MUIO_Cycle */
+	case 5: /* MUIO_Radio */
+	case 7: /* MUIO_String */
+	case 13: /* MUIO_MenustripNM */
+	    numparams = 2;
+	    break;
+	    
+	case 6: /* MUIO_Slider */
+	    numparams = 3;
+	    break;
+
+	case 14: /* MUIO_MenuItem */
+	case 16: /* MUIO_NumericButton */
+    	    numparams = 4;
+	    break;	    	    	
+    }
+    
+    if (numparams == 0) return NULL;
+    
+    va_start(args, type);
+    
+    for(i = 0; i < numparams; i++)
+    {
+    	param[i] = va_arg(args, IPTR);
+    }
+    
+    va_end (args);
+    
+    return MUI_MakeObjectA(type, param);
+    
+} /* MUI_MakeObject */
