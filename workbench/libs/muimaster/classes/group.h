d5a16a0a22c3718efc8ca218f987f910858c2922
sebauer <sebauer@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-03-19 21:00:49 +0000

    Moved private date to group.c
    No longer updates the whole group incl. frame when switching to another active page
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13703 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/classes/group.h
+++ workbench/libs/muimaster/classes/group.h
@@ -1,25 +1,5 @@
 #include "muimaster_intern.h"
 
-struct MUI_GroupData
-{
-    Object      *family;
-    struct Hook *layout_hook;
-    ULONG        flags;
-#define GROUP_HORIZ       (1<<1)
-#define GROUP_SAME_WIDTH  (1<<2)
-#define GROUP_SAME_HEIGHT (1<<3)
-#define GROUP_CHANGING    (1<<4)
-#define GROUP_PAGEMODE    (1<<5)
-    ULONG        columns;
-    ULONG        rows;
-    LONG         active_page;
-    ULONG        horiz_spacing;
-    ULONG        vert_spacing;
-    ULONG        num_childs;
-    ULONG        horiz_weight_sum;
-    ULONG        vert_weight_sum;
-};
-
 
 /* Hook message for custom layout */
 
