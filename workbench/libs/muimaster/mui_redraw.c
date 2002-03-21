78a2ea6b9b43decd1a043758e32e8849019253ab
sebauer <sebauer@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-03-21 18:49:55 +0000

    MUI_Render works now.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13737 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/mui_redraw.c
+++ workbench/libs/muimaster/mui_redraw.c
@@ -8,6 +8,8 @@
 
 #include <clib/alib_protos.h>
 #include <intuition/classusr.h>
+#include <proto/graphics.h>
+#include <proto/intuition.h>
 #ifdef _AROS
 #include <proto/muimaster.h>
 #endif
@@ -54,14 +56,59 @@ __asm VOID MUI_Redraw(register __a0 Object *obj, register __d0 ULONG flags)
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct MUIMasterBase *,MUIMasterBase)
 
+    Object *wnd;
+    Object *parent;
+    struct Region *region = NULL;
+    APTR clip;
+
     if (!(muiAreaData(obj)->mad_Flags & MADF_CANDRAW)) return;
+
+    get(obj,MUIA_WindowObject,&wnd);
+    parent = obj;
+
+    while (get(parent,MUIA_Parent,&parent))
+    {
+    	if (!parent) break;
+    	if (parent == wnd) break;
+
+	if (_flags(parent) & MADF_ISVIRTUALGROUP)
+	{
+	    struct Rectangle rect;
+
+	    rect.MinX = _mleft(parent);
+	    rect.MinY = _mtop(parent);
+	    rect.MaxX = _mright(parent);
+	    rect.MaxY = _mbottom(parent);
+
+	    if (!region)
+	    {
+	    	if ((region = NewRegion()))
+	    	{
+		    OrRectRegion(region, &rect);
+	    	}
+	    } else
+	    {
+		AndRectRegion(region, &rect);
+	    }
+	}
+    }
 
     muiRenderInfo(obj)->mri_ClipRect.MinX = _left(obj);
     muiRenderInfo(obj)->mri_ClipRect.MinY = _top(obj);
     muiRenderInfo(obj)->mri_ClipRect.MaxX = _right(obj);
     muiRenderInfo(obj)->mri_ClipRect.MaxY = _bottom(obj);
 
+    if (region)
+    {
+	clip = MUI_AddClipRegion(muiRenderInfo(obj),region);
+    }
     DoMethod(obj, MUIM_Draw, flags);
+
+    if (region)
+    {
+	/* This call actually also frees the region */
+	MUI_RemoveClipRegion(muiRenderInfo(obj),region);
+    }
 
     AROS_LIBFUNC_EXIT
 
