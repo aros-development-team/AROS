02f1d76c1973c49441b2a26a6a4f91fb0e94bd47
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-03-26 22:11:51 +0000

    implemented Bodychunk class
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13806 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/muimaster/classes/bodychunk.h
@@ -0,0 +1,23 @@
+#ifndef _MUI_CLASSES_BODYCHUNK_H
+#define _MUI_CLASSES_BODYCHUNK_H
+
+/****************************************************************************/
+/** Bodychunk                                                              **/
+/****************************************************************************/
+
+#ifdef _DCC
+extern char MUIC_Bodychunk[];
+#else
+#define MUIC_Bodychunk "Bodychunk.mui"
+#endif
+
+/* Attributes */
+
+#define MUIA_Bodychunk_Body                 0x8042ca67 /* V8  isg UBYTE *           */
+#define MUIA_Bodychunk_Compression          0x8042de5f /* V8  isg UBYTE             */
+#define MUIA_Bodychunk_Depth                0x8042c392 /* V8  isg LONG              */
+#define MUIA_Bodychunk_Masking              0x80423b0e /* V8  isg UBYTE             */
+
+extern const struct __MUIBuiltinClass _MUI_Bodychunk_desc;
+
+#endif
