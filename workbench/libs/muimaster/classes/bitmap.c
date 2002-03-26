0a31019faab05abe1db09b58be3288d2998e71cb
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-03-26 18:26:07 +0000

    implemented bitmap class. MUIA_Bitmap_Transparent is not yet really handled completely, ie. mask is not yet created.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13804 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/muimaster/classes/bitmap.c
@@ -0,0 +1,486 @@
+#include <graphics/gfx.h>
+#include <graphics/view.h>
+#include <clib/alib_protos.h>
+#include <proto/exec.h>
+#include <proto/graphics.h>
+#include <proto/utility.h>
+
+#ifdef _AROS
+#include <proto/muimaster.h>
+#endif
+
+#include "mui.h"
+#include "muimaster_intern.h"
+#include "support.h"
+
+#warning TODO: MUIA_BitMap_Transparent handling (mask creation)
+
+extern struct Library *MUIMasterBase;
+
+struct MUI_BitmapData
+{
+    struct BitMap *bm, *remapped_bm;
+    ULONG   	  *sourcecolors;
+    LONG    	   width, height, precision, transparent;
+    UBYTE   	  *mappingtable;
+    PLANEPTR  	   mask;
+    WORD    	  *remaptable;
+    BYTE    	   usefriend;
+};
+
+static void remap_bitmap(struct IClass *cl, Object *obj)
+{
+    struct MUI_BitmapData *data = INST_DATA(cl, obj);
+    struct BitMap   	  *friendbm = NULL;
+    struct RastPort 	  temprp, bmrp, *scrrp;
+    UBYTE   	    	  *linebuffer;
+    LONG    	    	  bmflags = 0;
+    WORD    	    	  bmdepth, x, y;
+    
+    if (!data->mappingtable && !data->sourcecolors) return;
+    if (!data->bm || (data->width < 1) || (data->height < 1)) return;
+    
+    /* Don't remap if bitmap is hicolor/truecolor */
+    if (GetBitMapAttr(data->bm, BMA_DEPTH) > 8) return;
+    
+    if (!data->mappingtable && !data->remaptable)
+    {
+    	data->remaptable = AllocVec(256 * sizeof(WORD), MEMF_PUBLIC | MEMF_CLEAR);
+	if (!data->remaptable) return;
+    }
+    
+    scrrp = &_screen(obj)->RastPort;
+    
+    if (data->usefriend)
+    {
+    	friendbm = scrrp->BitMap;
+	bmflags |= BMF_MINPLANES;
+    }
+    
+    linebuffer = AllocVec(data->width + 16, MEMF_PUBLIC);
+    if (!linebuffer) return;
+    
+    bmdepth = GetBitMapAttr(scrrp->BitMap, BMA_DEPTH);
+    
+    data->remapped_bm = AllocBitMap(data->width, data->height, bmdepth, bmflags, friendbm);
+    if (!data->remapped_bm)
+    {
+    	FreeVec(linebuffer);
+    	return;
+    }
+    
+    InitRastPort(&temprp);
+    temprp.BitMap = AllocBitMap(data->width, 1, 1, 0, NULL);
+    
+    InitRastPort(&bmrp);
+    
+    for(y = 0; y < data->height; y++)
+    {
+    	/* Read a line from source bitmap */
+	
+        bmrp.BitMap = data->bm;
+    	if (temprp.BitMap)
+	{
+	    ReadPixelLine8(&bmrp, 0, y, data->width, linebuffer, &temprp);
+	}
+	else
+	{
+	    for(x = 0; x < data->width; x++)
+	    {
+	    	linebuffer[x] = ReadPixel(&bmrp, x, y);
+	    }
+	}
+	
+	/* Remap the line */
+	if (data->mappingtable)
+	{
+	    for(x = 0; x < data->width; x++)
+	    {
+	    	linebuffer[x] = data->mappingtable[linebuffer[x]];
+	    }
+	}
+	else
+	{
+	    for(x = 0; x < data->width; x++)
+	    {
+	    	UBYTE pixel = linebuffer[x];
+		UBYTE remappixel = data->remaptable[pixel];
+		
+	    	if (!remappixel)
+		{
+		    struct TagItem tags[] =
+		    {
+		    	{OBP_Precision, data->precision },
+			{OBP_FailIfBad, FALSE	    	},
+			{TAG_DONE   	    	    	}
+		    };
+		    
+		    data->remaptable[pixel] = remappixel = ObtainBestPenA(_screen(obj)->ViewPort.ColorMap,
+		    	    	    	    	      			  data->sourcecolors[pixel * 3],
+									  data->sourcecolors[pixel * 3 + 1],
+									  data->sourcecolors[pixel * 3 + 2],
+									  tags) | 0x100;
+		}
+		
+		linebuffer[x] = (remappixel & 0xFF);
+	    }
+	}
+	
+	/* Write line into destination bitmap */
+	
+        bmrp.BitMap = data->remapped_bm;
+    	if (temprp.BitMap)
+	{
+	    WritePixelLine8(&bmrp, 0, y, data->width, linebuffer, &temprp);
+	}
+	else
+	{
+	    for(x = 0; x < data->width; x++)
+	    {
+	    	SetAPen(&bmrp, linebuffer[x]);
+	    	WritePixel(&bmrp, x, y);
+	    }
+	}
+	
+    } /* for(y = 0; y < data->height; y++) */
+    
+#ifdef _AROS
+    DeinitRastPort(&temprp);
+    DeinitRastPort(&bmrp);
+#endif
+
+    if (temprp.BitMap)
+    {
+    	WaitBlit();
+	FreeBitMap(temprp.BitMap);
+    }
+    
+    FreeVec(linebuffer);
+}
+
+/**************************************************************************
+ OM_NEW
+**************************************************************************/
+static IPTR Bitmap_New(struct IClass *cl, Object *obj, struct opSet *msg)
+{
+    struct MUI_BitmapData   *data;
+    struct TagItem  	    *tag, *tags;
+    
+    obj = (Object *)DoSuperMethodA(cl, obj, (Msg)msg);
+    if (!obj) return FALSE;
+    
+    data = INST_DATA(cl, obj);
+
+    data->precision = PRECISION_GUI;
+    data->transparent = -1;
+	
+    /* parse initial taglist */
+
+    for (tags = msg->ops_AttrList; (tag = NextTagItem((const struct TagItem **)&tags)); )
+    {
+	switch (tag->ti_Tag)
+	{
+	    case MUIA_Bitmap_Bitmap:
+	    	data->bm = (struct BitMap *)tag->ti_Data;
+		break;
+		
+	    case MUIA_Bitmap_Height:
+	    	data->height = (LONG)tag->ti_Data;
+		break;
+		
+	    case MUIA_Bitmap_MappingTable:
+	    	data->mappingtable = (UBYTE *)tag->ti_Data;
+		break;
+		
+	    case MUIA_Bitmap_Precision:
+	    	data->precision = (LONG)tag->ti_Data;
+		break;
+		
+	    case MUIA_Bitmap_SourceColors:
+	    	data->sourcecolors = (ULONG *)tag->ti_Data;
+		break;
+		
+	    case MUIA_Bitmap_Transparent:
+	    	data->transparent = (LONG)tag->ti_Data;
+		break;
+		
+	    case MUIA_Bitmap_UseFriend:
+	    	data->usefriend = (tag->ti_Data != 0);
+		break;
+		
+	    case MUIA_Bitmap_Width:
+	    	data->width = (LONG)tag->ti_Data;
+		break;
+		
+    	}
+    }
+    
+    return (IPTR)obj;
+}
+
+/**************************************************************************
+ OM_DISPOSE
+**************************************************************************/
+static IPTR Bitmap_Dispose(struct IClass *cl, Object *obj, Msg msg)
+{
+    struct MUI_BitmapData *data = INST_DATA(cl, obj);
+
+    if (data->remapped_bm)
+    {
+    	WaitBlit();
+    	FreeBitMap(data->remapped_bm);
+    }
+    if (data->remaptable) FreeVec(data->remaptable);
+    
+    return DoSuperMethodA(cl, obj, msg);
+}
+
+/**************************************************************************
+ OM_SET
+**************************************************************************/
+static IPTR Bitmap_Set(struct IClass *cl, Object *obj, struct opSet *msg)
+{
+    struct MUI_BitmapData   *data  = INST_DATA(cl, obj);
+    struct TagItem          *tags  = msg->ops_AttrList;
+    struct TagItem          *tag;
+
+    while ((tag = NextTagItem((const struct TagItem **)&tags)) != NULL)
+    {
+	switch (tag->ti_Tag)
+	{
+	    case MUIA_Bitmap_Bitmap:
+	    	if (!data->remapped_bm)
+		{
+		    data->bm = (struct BitMap *)tag->ti_Data;
+		}
+		break;
+		
+	    case MUIA_Bitmap_Height:
+	    	data->height = (LONG)tag->ti_Data;
+		break;
+		
+	    case MUIA_Bitmap_MappingTable:
+	    	data->mappingtable = (UBYTE *)tag->ti_Data;
+		break;
+		
+	    case MUIA_Bitmap_Precision:
+	    	data->precision = (LONG)tag->ti_Data;
+		break;
+		
+	    case MUIA_Bitmap_SourceColors:
+	    	data->sourcecolors = (ULONG *)tag->ti_Data;
+		break;
+		
+	    case MUIA_Bitmap_Transparent:
+	    	data->transparent = (LONG)tag->ti_Data;
+		break;
+		
+	    case MUIA_Bitmap_Width:
+	    	data->width = (LONG)tag->ti_Data;
+		break;
+
+	}
+    }
+
+    return DoSuperMethodA(cl, obj, (Msg)msg);
+}
+
+/**************************************************************************
+ OM_GET
+**************************************************************************/
+static IPTR Bitmap_Get(struct IClass *cl, Object *obj, struct opGet *msg)
+{
+#define STORE *(msg->opg_Storage)
+
+    struct MUI_BitmapData *data = INST_DATA(cl, obj);
+
+    switch(msg->opg_AttrID)
+    {
+	case MUIA_Bitmap_Bitmap:
+	    STORE = (IPTR)data->bm;
+	    return TRUE;
+
+	case MUIA_Bitmap_Height:
+	    STORE = (IPTR)data->height;
+	    return TRUE;
+
+	case MUIA_Bitmap_MappingTable:
+	    STORE = (IPTR)data->mappingtable;
+	    return TRUE;
+
+	case MUIA_Bitmap_Precision:
+	    STORE = (IPTR)data->precision;
+	    return TRUE;
+
+	case MUIA_Bitmap_RemappedBitmap:
+	    STORE = (IPTR)data->remapped_bm;
+	    return TRUE;
+
+	case MUIA_Bitmap_SourceColors:
+	    STORE = (IPTR)data->sourcecolors;
+	    return TRUE;
+
+	case MUIA_Bitmap_Transparent:
+	    STORE = (IPTR)data->transparent;
+	    return TRUE;
+
+	case MUIA_Bitmap_Width:
+	    STORE = (IPTR)data->width;
+	    return TRUE;
+    }
+
+    return DoSuperMethodA(cl, obj, (Msg)msg);
+#undef STORE
+}
+
+/**************************************************************************
+ MUIM_Setup
+**************************************************************************/
+static IPTR Bitmap_Setup(struct IClass *cl, Object *obj, Msg msg)
+{
+    struct MUI_BitmapData *data = INST_DATA(cl, obj);
+
+    if (!DoSuperMethodA(cl, obj, (Msg)msg))
+	return FALSE;
+
+    remap_bitmap(cl, obj);
+    
+    return TRUE;
+}
+
+/**************************************************************************
+ MUIM_Cleanup
+**************************************************************************/
+static IPTR Bitmap_Cleanup(struct IClass *cl, Object *obj, Msg msg)
+{
+    struct MUI_BitmapData *data = INST_DATA(cl, obj);
+
+    if (data->remapped_bm)
+    {
+    	WaitBlit();
+	FreeBitMap(data->remapped_bm);
+	data->remapped_bm = NULL;
+	
+	if (data->remaptable)
+	{
+	    WORD i;
+	    
+	    for(i = 0; i < 256; i++)
+	    {
+	    	if (data->remaptable[i])
+		{
+		    ReleasePen(_screen(obj)->ViewPort.ColorMap, data->remaptable[i] & 0xFF);
+		    data->remaptable[i] = 0;
+		}
+	    }
+	}
+    }
+    
+    return DoSuperMethodA(cl, obj, (Msg)msg);
+}
+
+/**************************************************************************
+ MUIM_AskMinMax
+**************************************************************************/
+static IPTR Bitmap_AskMinMax(struct IClass *cl, Object *obj, struct MUIP_AskMinMax *msg)
+{
+    DoSuperMethodA(cl, obj, (Msg)msg);
+    
+    msg->MinMaxInfo->MinWidth  += 1;
+    msg->MinMaxInfo->MinHeight += 1;
+    
+    msg->MinMaxInfo->DefWidth  += 1;
+    msg->MinMaxInfo->DefHeight += 1;
+    
+    msg->MinMaxInfo->MaxWidth  = MUI_MAXMAX;
+    msg->MinMaxInfo->MaxHeight = MUI_MAXMAX;
+    
+    return TRUE;
+}
+
+/**************************************************************************
+ MUIM_Draw
+**************************************************************************/
+static IPTR Bitmap_Draw(struct IClass *cl, Object *obj, struct MUIP_Draw *msg)
+{
+    struct MUI_BitmapData *data = INST_DATA(cl, obj);
+    struct BitMap   	  *bm;
+    
+    DoSuperMethodA(cl,obj,(Msg)msg);
+    
+    bm = data->remapped_bm ? data->remapped_bm : data->bm;
+    if (bm)
+    {
+    	LONG width, height;
+	
+	width = data->width;
+	height = data->height;
+	
+	if (width  > _mwidth(obj)) width = _mwidth(obj);
+	if (height > _mheight(obj)) height = _mheight(obj);
+	
+	if ((width > 0) && (height > 0))
+	{
+	    if (data->mask)
+	    {
+	    	BltMaskBitMapRastPort(bm, 0, 0, _rp(obj), _mleft(obj), _mtop(obj), width, height, 0xE0, data->mask);
+	    }
+	    else
+	    {
+	    	BltBitMapRastPort(bm, 0, 0, _rp(obj), _mleft(obj), _mtop(obj), width, height, 0xC0);
+	    }
+	}
+    }
+    
+    return TRUE;
+}
+
+#ifndef _AROS
+__asm IPTR Bitmap_Dispatcher( register __a0 struct IClass *cl, register __a2 Object *obj, register __a1 Msg msg)
+#else
+AROS_UFH3S(IPTR,Bitmap_Dispatcher,
+	AROS_UFHA(Class  *, cl,  A0),
+	AROS_UFHA(Object *, obj, A2),
+	AROS_UFHA(Msg     , msg, A1))
+#endif
+{
+    switch (msg->MethodID)
+    {
+	case OM_NEW:
+	    return Bitmap_New(cl, obj, (struct opSet *)msg);
+	    
+	case OM_DISPOSE:
+	    return Bitmap_Dispose(cl, obj, msg);
+	    
+	case OM_SET:
+	    return Bitmap_Set(cl, obj, (struct opSet *)msg);
+	    
+	case OM_GET:
+	    return Bitmap_Get(cl, obj, (struct opGet *)msg);
+	    
+	case MUIM_Setup:
+	    return Bitmap_Setup(cl, obj, msg);
+	    
+	case MUIM_Cleanup:
+	    return Bitmap_Cleanup(cl, obj, msg);
+	    
+	case MUIM_AskMinMax:
+	    return Bitmap_AskMinMax(cl, obj, (struct MUIP_AskMinMax *)msg);
+	    
+	case MUIM_Draw:
+	    return Bitmap_Draw(cl, obj, (struct MUIP_Draw *)msg);
+	    
+    }
+    
+    return DoSuperMethodA(cl, obj, msg);
+}
+
+/*
+ * Class descriptor.
+ */
+const struct __MUIBuiltinClass _MUI_Bitmap_desc = { 
+    MUIC_Bitmap, 
+    MUIC_Area, 
+    sizeof(struct MUI_BitmapData), 
+    Bitmap_Dispatcher 
+};
--- workbench/libs/muimaster/support.c
+++ workbench/libs/muimaster/support.c
@@ -114,7 +114,8 @@ static const struct __MUIBuiltinClass *builtins[] =
     &_MUI_Scrollgroup_desc,
     &_MUI_Scrollbutton_desc,
     &_MUI_Semaphore_desc,
-    &_MUI_Dataspace_desc
+    &_MUI_Dataspace_desc,
+    &_MUI_Bitmap_desc
 
 };
 
