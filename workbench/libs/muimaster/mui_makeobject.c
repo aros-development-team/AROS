ee833e7a31992717d672c9444715efcdbd27533d
sebauer <sebauer@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-03-30 22:14:13 +0000

    Supports more types now
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13860 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/mui_makeobject.c
+++ workbench/libs/muimaster/mui_makeobject.c
@@ -124,9 +124,18 @@ __asm Object *MUI_MakeObjectA(register __d0 LONG type, register __a0 IPTR *param
 		MUIA_Background   , MUII_ButtonBack,
 		TAG_DONE);
 	}
-#if 0
+
 	case MUIO_Checkmark: /* STRPTR label */
 	{
+	    return MUI_NewObject(MUIC_Text, //Image
+//	            MUIA_Image_Spec, MUII_CheckMark,
+		    MUIA_Text_Contents, "C",
+	            MUIA_InputMode, MUIV_InputMode_Toggle,
+//	            MUIA_Frame, MUIV_Frame_None,
+//	            MUIA_ShowSelState, FALSE,
+		    TAG_DONE);
+
+#if 0
 	    return MUI_NewObject(MUIC_Group,
 		MUIA_Group_Horiz,TRUE,
 		Child, Label(params[0]),
@@ -138,23 +147,20 @@ __asm Object *MUI_MakeObjectA(register __d0 LONG type, register __a0 IPTR *param
 		    TAG_DONE),
 		TAG_DONE);
 	    break;
+#endif
 	}
 
 	case MUIO_Cycle: /* STRPTR label, STRPTR *entries */
-	    return MUI_NewObject(MUIC_Group,
-		MUIA_Group_Horiz,TRUE,
-		Child, LLabel1(params[0]),
-		Child, MUI_NewObject(MUIC_Cycle,
+		return MUI_NewObject(MUIC_Cycle,
 		    MUIA_Cycle_Entries, params[1],
-		    TAG_DONE),
-		TAG_DONE);
+		    TAG_DONE);
 	    break;
 
 	case MUIO_Radio: /* STRPTR label, STRPTR *entries */
 	    break;
-#endif
 
-#if 0
+
+
 	case MUIO_Slider: /* STRPTR label, LONG min, LONG max */
 	    return SliderObject, /* control char is missing */
 	        MUIA_Numeric_Min, params[1],
@@ -163,17 +169,21 @@ __asm Object *MUI_MakeObjectA(register __d0 LONG type, register __a0 IPTR *param
 	    break;
 
 	case MUIO_String: /* STRPTR label, LONG maxlen */
+	    return MUI_NewObject(MUIC_String,
+	    	MUIA_String_MaxLen,params[1],
+	    	TAG_DONE);
 	    break;
 
 
 	case MUIO_PopButton: /* STRPTR imagespec */
-	    return MUI_NewObject(MUIC_Image,
-		MUIA_Image_Spec, params[0],
+	    return MUI_NewObject(MUIC_Text,//MUIC_Image,
+//		MUIA_Image_Spec, params[0],
 		MUIA_InputMode, MUIV_InputMode_RelVerify,
-		MUIA_ShowSelState, FALSE,
+		MUIA_Text_Contents, "Pop",
+//		MUIA_ShowSelState, FALSE,
 		TAG_DONE);
 	    break;
-#endif
+
 	case MUIO_HSpace: /* LONG space   */
 	    return MUI_NewObject(MUIC_Rectangle,
 		MUIA_FixWidth, params[0],
