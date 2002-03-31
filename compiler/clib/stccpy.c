ec8d958c5d39db3c59d7ba8c504eafb807446d35
hkiel <hkiel@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-03-31 00:55:54 +0000

    Implemented stccpy()
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13867 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/stccpy.c
@@ -0,0 +1,64 @@
+/*
+    Copyright © 2002, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: C function stccpy()
+    Lang: english
+*/
+
+/*****************************************************************************
+
+    NAME */
+#include <string.h>
+
+	size_t stccpy (
+
+/*  SYNOPSIS */
+	char	   * dest,
+	const char * src,
+	size_t	     n)
+
+/*  FUNCTION
+	Copy a string. Works like an assignment "dest=src;". At most
+	n characters are copied.
+
+    INPUTS
+	dest - The string is copied into this variable. Make sure it is
+		large enough.
+	src - This is the new contents of dest.
+	n - How many characters to copy at most. If the string src is
+		smaller than that, only strlen(str)+1 bytes are copied.
+
+    RESULT
+	The number of copied characters.
+
+    NOTES
+	No check is beeing made that dest is large enough for src.
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	strncpy()
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+{
+    char * ptr = dest;
+
+    while (n && (*ptr = *src))
+    {
+	ptr ++;
+	src ++;
+	n--;
+    }
+
+    while (n--)
+	*ptr++ = '\0';
+
+    return (ptr-dest);
+} /* stccpy */
