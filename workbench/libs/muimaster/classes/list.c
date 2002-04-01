2fc2a8693711efe19a6b261f80c379363d77a8e8
sebauer <sebauer@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-04-01 12:31:16 +0000

    Commited implementation progress
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13925 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/classes/list.c
+++ workbench/libs/muimaster/classes/list.c
@@ -6,6 +6,7 @@
 #include <proto/exec.h>
 #include <proto/graphics.h>
 #include <proto/utility.h>
+#include <proto/dos.h>
 
 #ifdef _AROS
 #include <proto/muimaster.h>
@@ -20,9 +21,22 @@ extern struct Library *MUIMasterBase;
 
 struct ListEntry
 {
-    APTR entry;
+    APTR data;
     LONG *widths; /* The widths of the column */
     LONG height; /* line height */
+};
+
+struct ColumnInfo
+{
+    int colno; /* Column number */
+    int entry_width; /* width of the entries (the maximum of the widths of all entries) */
+    int user_width; /* user setted width -1 if entry width */
+    int min_width; /* min width percentage */
+    int max_width; /* min width percentage */
+    int weight;
+    int delta; /* ignored for the first and last column, defaults to 4 */
+    int bar;
+    char *prepare;
 };
 
 struct MUI_ListData
@@ -44,7 +58,14 @@ struct MUI_ListData
 
     LONG entries_first; /* first visible entry */
     LONG entries_active;
+    LONG insert_position; /* pos of the last insertion */
+
+    /* Column managment, only setted by ParseListFormat() and freed by CleanListFormat() */
     LONG columns; /* Number of columns the list has */
+    struct ColumnInfo *ci;
+    char **preparses;
+    char **strings; /* the strings for the display function, one more as needed (for the entry position) */
+    
 };
 
 /**************************************************************************
@@ -100,6 +121,90 @@ static int SetListSize(struct MUI_ListData *data, LONG size)
 }
 
 /**************************************************************************
+ Prepares the insertion of count entries at pos.
+ This function doesn't care if there is enough space in the datastructure.
+ SetListSize() must be used first.
+ With current implementation, this call will never fail
+**************************************************************************/
+static int PrepareInsertListEntries(struct MUI_ListData *data, int pos, int count)
+{
+    memmove(&data->entries[pos+count],&data->entries[pos],data->entries_num - pos);
+    return 1;
+}
+
+/**************************************************************************
+ Inserts a already initialized array of Entries at the given position.
+ This function doesn't care if there is enough space in the datastructure
+ Returns 1 if something failed (never in current implementation)
+**************************************************************************/
+#if 0
+static int InsertListEntries(struct MUI_ListData *data, int pos, struct ListEntry **array, int count)
+{
+    memmove(&data->entries[pos+count],&data->entries[pos],data->entries_num - pos);
+    memcpy(&data->entries[pos],array,count);
+    return 1;
+}
+#endif
+
+
+/**************************************************************************
+ Removes count (already deinitalized) list entries starting az pos.
+**************************************************************************/
+static void RemoveListEntries(struct MUI_ListData *data, int pos, int count)
+{
+    memmove(&data->entries[pos],&data->entries[pos+count],data->entries_num - (pos + count));
+}
+
+/**************************************************************************
+ Frees all memory allocated by ParseListFormat()
+**************************************************************************/
+static void FreeListFormat(struct MUI_ListData *data)
+{
+    if (data->ci)
+    {
+    	FreeVec(data->ci);
+    	data->ci = NULL;
+    }
+    if (data->preparses)
+    {
+    	FreeVec(data->preparses);
+    	data->preparses = NULL;
+    }
+    if (data->strings)
+    {
+    	FreeVec(data->strings);
+    	data->strings = 0;
+    }
+    data->columns = 0;
+}
+
+/**************************************************************************
+ Parses the given format string (also frees a previouly parsed format).
+ Return 0 on failure.
+**************************************************************************/
+static int ParseListFormat(struct MUI_ListData *data, char *format)
+{
+    int new_columns;
+
+    FreeListFormat(data);
+
+    new_columns = 1;
+
+    if (!(data->preparses = (char**)AllocVec(new_columns*sizeof(char*),0)))
+	return 0;
+
+    if (!(data->strings = (char**)AllocVec((new_columns+1)*sizeof(char*),0))) /* the entry pos is stored in the first elment (MUI) */
+	return 0;
+
+    if (!(data->ci = (struct ColumnInfo *)AllocVec(new_columns*sizeof(struct ColumnInfo),0)))
+	return 0;
+
+    data->columns = new_columns;
+
+    return 1;
+}
+
+/**************************************************************************
  OM_NEW
 **************************************************************************/
 static IPTR List_New(struct IClass *cl, Object *obj, struct opSet *msg)
@@ -107,6 +212,7 @@ static IPTR List_New(struct IClass *cl, Object *obj, struct opSet *msg)
     struct MUI_ListData   *data;
     struct TagItem  	    *tag, *tags;
     APTR *array = NULL;
+    char *format = NULL;
     
     obj = (Object *)DoSuperMethodA(cl, obj, (Msg)msg);
     if (!obj) return FALSE;
@@ -159,6 +265,7 @@ static IPTR List_New(struct IClass *cl, Object *obj, struct opSet *msg)
 
     if (!data->pool)
     {
+    	/* No memory pool given, so we create out own */
 	data->pool = data->intern_pool = CreatePool(0,data->intern_puddle_size,data->intern_tresh_size);
 	if (!data->pool)
 	{
@@ -167,13 +274,21 @@ static IPTR List_New(struct IClass *cl, Object *obj, struct opSet *msg)
 	}
     }
 
+    /* parese the list format */
+    if (!(ParseListFormat(data,format)))
+    {
+	CoerceMethod(cl,obj,OM_DISPOSE);
+	return NULL;
+    }
+
     if (array)
     {
     	int i;
-    	for (i=0;array[i];i++);
+    	for (i=0;array[i];i++); /* Count the number of elements */
+    	/* Insert them */
     	DoMethod(obj, MUIM_List_Insert, array, i, MUIV_List_Insert_Top);
     }
-    
+   
     return (IPTR)obj;
 }
 
@@ -183,6 +298,11 @@ static IPTR List_New(struct IClass *cl, Object *obj, struct opSet *msg)
 static IPTR List_Dispose(struct IClass *cl, Object *obj, Msg msg)
 {
     struct MUI_ListData *data = INST_DATA(cl, obj);
+    if (!data->intern_pool && data->pool)
+    {
+	/* Call destruct method for every entry and free the entries manual to avoid notification */
+    }
+
     if (data->intern_pool) DeletePool(data->intern_pool);
     if (data->entries) FreeVec(data->entries);
     DoSuperMethodA(cl,obj,msg);
@@ -238,6 +358,7 @@ static ULONG List_Get(struct IClass *cl, Object *obj, struct opGet *msg)
 	case MUIA_List_Entries: STORE = data->entries_num; return 1;
 	case MUIA_List_First: STORE = data->entries_first; return 1;
 	case MUIA_List_Active: STORE = data->entries_active; return 1;
+	case MUIA_List_InsertPosition: STORE = data->insert_position; return 1;
     }
 
     if (DoSuperMethodA(cl, obj, (Msg) msg)) return 1;
@@ -252,7 +373,10 @@ static ULONG List_Get(struct IClass *cl, Object *obj, struct opGet *msg)
 STATIC ULONG List_Insert(struct IClass *cl, Object *obj, struct MUIP_List_Insert *msg)
 {
     struct MUI_ListData *data = INST_DATA(cl, obj);
-    LONG pos;
+    LONG pos,count;
+
+    count = msg->count;
+
     switch (msg->pos)
     {
     	case    MUIV_List_Insert_Top:
@@ -277,15 +401,53 @@ STATIC ULONG List_Insert(struct IClass *cl, Object *obj, struct MUIP_List_Insert
 		else if (msg->pos < 0) pos = 0;
 		else pos = msg->pos;
 	        break;		
-
     }
 
-    if (!(SetListSize(data,data->entries_num + msg->count)))
+    if (!(SetListSize(data,data->entries_num + count)))
 	return ~0;
 
     if (pos != -1)
     {
+    	LONG until = pos + count;
+    	APTR *toinsert = msg->entries;
+
+	if (!(PrepareInsertListEntries(data, pos, count)))
+	    return ~0;
+
+	while (pos < until)
+	{
+	    struct ListEntry *lentry;
+
+	    if (!(lentry = AllocListEntry(data)))
+	    {
+	    	/* Panic, but we must be in a consistent state, so remove
+	    	** the space where the following list entries should have gone
+	    	*/
+	    	RemoveListEntries(data, pos, until - pos);
+		return ~0;
+	    }
+
+	    /* now call the construct method which returns us a pointer which we need to store */
+	    lentry->data = (APTR)DoMethod(obj, MUIM_List_Construct, *toinsert, data->pool);
+	    if (!lentry->data)
+	    {
+	    	FreeListEntry(data,lentry);
+	    	RemoveListEntries(data, pos, until - pos);
+		return ~0;
+	    }
+
+	    data->entries[pos] = lentry;
+	    data->entries_num++;
+
+	    toinsert++;
+	    pos++;
+	}
+
+    	data->insert_position = pos;
     	return (ULONG)pos;
+    } else
+    {
+	/* Sort insertion must works differnt */
     }
     return ~0;
 }
@@ -303,7 +465,19 @@ STATIC ULONG List_InsertSingle(struct IClass *cl, Object *obj, struct MUIP_List_
 **************************************************************************/
 STATIC ULONG List_Construct(struct IClass *cl, Object *obj, struct MUIP_List_Construct *msg)
 {
-    return NULL;
+    struct MUI_ListData *data = INST_DATA(cl, obj);
+    if (!data->construct_hook) return (ULONG)msg->entry;
+    if ((ULONG)data->construct_hook == MUIV_List_ConstructHook_String)
+    {
+    	int len = msg->entry?strlen((char*)msg->entry):0;
+    	ULONG *mem = AllocPooled(msg->pool,len+5);
+    	if (!mem) return 0;
+    	mem[0] = len+5;
+    	if (msg->entry) strcpy((char*)(mem+1),(char*)msg->entry);
+    	else *(char*)(mem+1) = 0;
+    	return (ULONG)(mem+1);
+    }
+    return CallHookPkt(data->construct_hook,msg->pool,msg->entry);
 }
 
 /**************************************************************************
@@ -311,7 +485,17 @@ STATIC ULONG List_Construct(struct IClass *cl, Object *obj, struct MUIP_List_Con
 **************************************************************************/
 STATIC ULONG List_Destruct(struct IClass *cl, Object *obj, struct MUIP_List_Destruct *msg)
 {
-    return NULL;
+    struct MUI_ListData *data = INST_DATA(cl, obj);
+    if (!data->destruct_hook) return 0;
+    if ((ULONG)data->destruct_hook == MUIV_List_DestructHook_String)
+    {
+    	ULONG *mem = ((ULONG*)msg->entry)-1;
+	FreePooled(msg->pool,mem,mem[0]);
+    } else
+    {
+	CallHookPkt(data->destruct_hook,msg->pool,msg->entry);
+    }
+    return 0;
 }
 
 /**************************************************************************
@@ -319,6 +503,7 @@ STATIC ULONG List_Destruct(struct IClass *cl, Object *obj, struct MUIP_List_Dest
 **************************************************************************/
 STATIC ULONG List_Compare(struct IClass *cl, Object *obj, struct MUIP_List_Compare *msg)
 {
+    struct MUI_ListData *data = INST_DATA(cl, obj);
     return NULL;
 }
 
@@ -327,6 +512,7 @@ STATIC ULONG List_Compare(struct IClass *cl, Object *obj, struct MUIP_List_Compa
 **************************************************************************/
 STATIC ULONG List_Display(struct IClass *cl, Object *obj, struct MUIP_List_Display *msg)
 {
+    struct MUI_ListData *data = INST_DATA(cl, obj);
     return NULL;
 }
 
