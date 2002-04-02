b11976305623041a8be9d0b161d14951cdb6195a
sheutlin <sheutlin@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-04-02 12:17:02 +0000

    added support for more DosEnvec attributes
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13961 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/tools/HDToolBox/details.h
+++ workbench/tools/HDToolBox/details.h
@@ -6,7 +6,8 @@
 void det_Init(struct Window *, struct PartitionNode *);
 void det_Ok(struct PartitionNode *);
 void det_Cancel(struct PartitionNode *);
-void changeType(struct Window *, struct PartitionNode *, ULONG);
+void changeType(struct Window *, struct PartitionNode *, UBYTE *, UWORD);
+void setDetGadgets(struct Window *, struct PartitionNode *);
 void setPartitionTable(struct Window *, struct PartitionNode *, BOOL);
 #endif /* DETAILS_H */
 
