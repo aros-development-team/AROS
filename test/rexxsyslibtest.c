705041fc78c2328290d40a2581096820c8209c4c
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-04-06 21:46:53 +0000

    Added rexxsyslib test program.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14011 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/rexxsyslibtest.c
@@ -0,0 +1,37 @@
+#include <proto/exec.h>
+#include <proto/rexxsyslib.h>
+#include <rexx/storage.h>
+
+struct Library *RexxSysBase;
+
+int main(void)
+{
+  UBYTE *s;
+  
+  RexxSysBase = OpenLibrary("rexxsyslib.library", 0);
+  if (RexxSysBase == NULL)
+  {
+    puts("Error opening rexxsyslib.library");
+    exit(20);
+  }
+  else
+  {
+    puts("Opening rexxsyslib.library succeeded");
+  }
+
+  puts("Creating Argstring");
+  s = CreateArgstring("1234", 4);
+  if (s == NULL)
+  {
+    puts("Error creating Argstring");
+    exit(20);
+  }
+  puts("Getting length");
+  printf("Length: %d\n",LengthArgstring(s));
+  puts("Deleting Argstring");
+  DeleteArgstring(s);
+
+  CloseLibrary(RexxSysBase);
+  exit(0);
+}
+	 
