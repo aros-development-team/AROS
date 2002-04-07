a73a6a2184a906a56101394751a07efca1d8788b
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-04-07 13:27:12 +0000

    Added some new features. Have a look at the documentation at the top of the file to know about it
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14021 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/alib/detach.c
+++ compiler/alib/detach.c
@@ -4,6 +4,43 @@
 
     Desc: Common startup code
     Lang: english
+
+    Use: To make a program detach itself from the launching CLI, use detach.o as your
+         program's start file, before any other usual start file.
+
+         In your program's source code you have the option to use some variables to determine
+         the behaviour of the detaching procedure.
+
+         These variables are defined as "weak", so that defining them both in this module and in your
+         program doesn't produce linking errors. Defining these variables in your program's source code
+         makes it possible to make your program stay independent from this module, so that you can decide
+         whether your program should be detacheable or not just by adding a flag to the linker's command line
+         and make the program still work in case you decided to not make it detacheable.
+
+         These are the variables:
+
+             1) LONG __detacher_must_wait_for_signal;
+
+                Define the above variable only if you need the detacher process to wait
+                for a signal to arrive, before exiting and effectively detaching your program.
+
+                It must contain a bitmask suited for the use of exec.library/Wait()
+
+	     2) struct Process *__detacher_process;
+
+                The above variable will hold a pointer to the detacher process. Use it for anything
+                you wish, but specially for sending to it the "termination" signal.
+
+                This pointer is guaranteed to hold NULL when there's no detacher process around anymore.
+
+                Remember to initialize it to NULL!
+
+             3) STRPTR __detached_name;
+
+                Define the above variable only if you want the detached process to have a name different
+                than the detacher's one. Set its value to a pointer to a string holding the name you want
+                the detached process to have.
+
 */
 
 #include <aros/config.h>
@@ -28,6 +65,7 @@ AROS_UFPA(struct ExecBase *,sysbase,A6));
 
 extern LONG            __detacher_must_wait_for_signal __attribute__((weak));
 extern struct Process *__detacher_process              __attribute__((weak));
+extern STRPTR          __detached_name                 __attribute__((weak));
 
 AROS_UFH3(LONG, detach_entry,
 AROS_UFHA(char *,argstr,A0),
@@ -55,19 +93,22 @@ AROS_UFHA(struct ExecBase *,SysBase,A6))
 
 	mysegment = cli->cli_Module;
         cli->cli_Module = NULL;
+
+        if (!__detached_name)
+          __detached_name = FindTask(NULL)->tc_Node.ln_Name;
     }
 
     if (mysegment)
     {
-	struct Process *newproc;
+        struct Process *newproc;
 
 	struct TagItem tags[] =
         {
-	    { NP_Seglist,   (IPTR)mysegment          },
-	    { NP_Name,      (IPTR)"Detached Process" },
-	    { NP_Arguments, (IPTR)argstr             },
-	    { NP_Cli,       TRUE                     },
-            { TAG_DONE,     0                        }
+	    { NP_Seglist,   (IPTR)mysegment       },
+	    { NP_Name,      (IPTR)__detached_name },
+	    { NP_Arguments, (IPTR)argstr          },
+	    { NP_Cli,       TRUE                  },
+            { TAG_DONE,     0                     }
         };
 
         mysegment = NULL;
@@ -82,6 +123,8 @@ AROS_UFHA(struct ExecBase *,SysBase,A6))
 	if (__detacher_must_wait_for_signal)
 	    Wait(__detacher_must_wait_for_signal);
 
+        __detacher_process = NULL;
+
 	return newproc ? RETURN_OK : RETURN_FAIL;
     }
 
@@ -95,3 +138,4 @@ AROS_UFHA(struct ExecBase *,SysBase,A6))
 
 LONG            __detacher_must_wait_for_signal __attribute__((weak)) = 0;
 struct Process *__detacher_process              __attribute__((weak)) = NULL;
+STRPTR          __detached_name                 __attribute__((weak)) = NULL;
