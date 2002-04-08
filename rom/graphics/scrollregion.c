038afc8ccb0bfcc31e2be2a0848fbb880eca6fa6
falemagn <falemagn@gmail.com>
2002-04-08 18:17:27 +0000

    added ScrollRegion(), to - go figure - scroll a region of rectangles within a given rectangular area
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14042 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/graphics/scrollregion.c
@@ -0,0 +1,103 @@
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: Graphics function ScrollRegion()
+    Lang: english
+*/
+#include "graphics_intern.h"
+#include <graphics/regions.h>
+#include "intregions.h"
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/graphics.h>
+
+	AROS_LH4(BOOL, ScrollRegion,
+
+/*  SYNOPSIS */
+	AROS_LHA(struct Region *,    region, A0),
+	AROS_LHA(struct Rectangle *, rect,   A1),
+	AROS_LHA(WORD,               dx,     D0),
+	AROS_LHA(WORD,               dy,     D1),
+
+/*  LOCATION */
+	struct GfxBase *, GfxBase, 191, Graphics)
+
+/*  FUNCTION
+	Scroll the rectangles in the region by the amount of pixels specified, within the
+        specified rectangle.
+
+    INPUTS
+	region - pointer to a region structure
+	rect   - pointer to the rectangle within which the scrolling has to happen.
+                 If NULL, the region's bounds are used instead.
+	dx, dy - the amount of pixels by which to scroll the region. Negative values mean
+                 respectively left and up, positive values mean right and down.
+    RESULT
+	TRUE if the operation succeeded, FALSE otherwise.
+
+    NOTES
+       This function doesn't exist in AmigaOS
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	CreateRegion()
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    BOOL res = FALSE;
+
+    if (!rect)
+    {
+        TranslateRect(Bounds(region), dx, dy);
+
+        res = TRUE;
+    }
+    else
+    {
+        struct Region *cutRegion;
+
+        cutRegion = AndRectRegionND(region, rect);
+        if (cutRegion)
+        {
+	    struct Region *newRegion;
+
+            TranslateRect(Bounds(cutRegion), dx, dy);
+
+            AndRectRegion(cutRegion, rect);
+            
+            newRegion = ClearRectRegionND(region, rect);
+            if (newRegion)
+            {
+                if (OrRegionRegion(cutRegion, newRegion))
+                {
+                    struct Region tmp = *region;
+
+                    *region    = *newRegion;
+                    *newRegion = tmp;
+
+                    res = TRUE;
+                }
+
+                DisposeRegion(newRegion);
+            }
+
+            DisposeRegion(cutRegion);
+        }
+    }
+
+    return res;
+
+    AROS_LIBFUNC_EXIT
+}
