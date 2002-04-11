ab5cbbdedc169664609a7abdfdb2391ddfbc7a66
sebauer <sebauer@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-04-11 11:57:26 +0000

    Implemented popstring class
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14086 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/classes/popstring.c
+++ workbench/libs/muimaster/classes/popstring.c
@@ -4,6 +4,7 @@
 #include <proto/exec.h>
 #include <proto/graphics.h>
 #include <proto/utility.h>
+#include <proto/intuition.h>
 
 #ifdef _AROS
 #include <proto/muimaster.h>
@@ -19,7 +20,12 @@ extern struct Library *MUIMasterBase;
 
 struct MUI_PopstringData
 {
-    int dummy;
+    struct Hook *close_hook;
+    struct Hook *open_hook;
+    Object *string;
+    Object *button;
+    int open;
+    int toggle;
 };
 
 /**************************************************************************
@@ -29,8 +35,17 @@ static IPTR Popstring_New(struct IClass *cl, Object *obj, struct opSet *msg)
 {
     struct MUI_PopstringData   *data;
     struct TagItem  	    *tag, *tags;
+    Object *string, *button;
+
+    button = (Object*)GetTagData(MUIA_Popstring_Button,NULL,msg->ops_AttrList);
+    string = (Object*)GetTagData(MUIA_Popstring_String,NULL,msg->ops_AttrList);
     
-    obj = (Object *)DoSuperMethodA(cl, obj, (Msg)msg);
+    obj = (Object *)DoSuperNew(cl, obj,
+    			MUIA_Group_Horiz, TRUE,
+    			MUIA_Group_Spacing, 0,
+    			Child, string,
+    			Child, button,
+			TAG_MORE, msg->ops_AttrList);
     if (!obj) return FALSE;
     
     data = INST_DATA(cl, obj);
@@ -41,11 +56,80 @@ static IPTR Popstring_New(struct IClass *cl, Object *obj, struct opSet *msg)
     {
 	switch (tag->ti_Tag)
 	{
+	    case MUIA_Popstring_Toggle: data->toggle = tag->ti_Data; break;
+	    case MUIA_Popstring_CloseHook: data->close_hook = (struct Hook*)tag->ti_Data;break;
+	    case MUIA_Popstring_OpenHook: data->open_hook = (struct Hook*)tag->ti_Data;break;
     	}
     }
-    
+
     return (IPTR)obj;
 }
+
+/**************************************************************************
+ OM_SET
+**************************************************************************/
+STATIC IPTR Popstring_Set(struct IClass *cl, Object *obj, struct opSet *msg)
+{
+    struct TagItem *tags,*tag;
+    struct MUI_PopstringData *data = INST_DATA(cl, obj);
+
+    for (tags = msg->ops_AttrList; (tag = NextTagItem(&tags)); )
+    {
+	switch (tag->ti_Tag)
+	{
+	    case MUIA_Popstring_Toggle: data->toggle = tag->ti_Data; break;
+	    case MUIA_Popstring_CloseHook: data->close_hook = (struct Hook*)tag->ti_Data;break;
+	    case MUIA_Popstring_OpenHook: data->open_hook = (struct Hook*)tag->ti_Data;break;
+ 	}
+    }
+
+    return DoSuperMethodA(cl,obj,(Msg)msg);
+}
+
+
+/**************************************************************************
+ MUIM_Popstring_Close
+**************************************************************************/
+static IPTR Popstring_Close(struct IClass *cl, Object *obj, struct MUIP_Popstring_Close *msg)
+{
+    struct MUI_PopstringData *data = INST_DATA(cl, obj);
+    if (data->close_hook && data->open)
+    {
+    	DoMethod(_app(obj), MUIM_Application_PushMethod, obj, 4, MUIM_CallHook, data->close_hook, data->string, msg->result);
+    	set(data->button,MUIA_Disabled, FALSE);
+    }
+    return 0;
+}
+
+/**************************************************************************
+ MUIM_Popstring_Open
+**************************************************************************/
+static IPTR Popstring_Open(struct IClass *cl, Object *obj, struct MUIP_Popstring_Open *msg)
+{
+    struct MUI_PopstringData *data = INST_DATA(cl, obj);
+    if (data->open_hook)
+    {
+	if (data->open && data->toggle)
+	{
+	    DoMethod(obj,MUIM_Popstring_Close);
+	    return 0;
+	}
+
+	if (!data->open)
+	{
+	    if (DoMethod(obj, MUIM_CallHook, data->open_hook, data->string))
+	    {
+		/* Opening the popup window was successful */
+		data->open = 1;
+		if (!data->toggle) set(data->button,MUIA_Disabled, TRUE);
+	    }
+	}
+    }
+}
+
+/**************************************************************************
+ OM_NEW
+**************************************************************************/
 
 #ifndef _AROS
 __asm IPTR Popstring_Dispatcher( register __a0 struct IClass *cl, register __a2 Object *obj, register __a1 Msg msg)
@@ -58,9 +142,10 @@ AROS_UFH3S(IPTR,Popstring_Dispatcher,
 {
     switch (msg->MethodID)
     {
-	case OM_NEW:
-	    return Popstring_New(cl, obj, (struct opSet *)msg);
-	    
+	case OM_NEW: return Popstring_New(cl, obj, (struct opSet *)msg);
+	case OM_SET: return Popstring_Set(cl, obj, (struct opSet *)msg);
+	case MUIM_Popstring_Close: return Popstring_Close(cl, obj, (struct MUIP_Popstring_Close*)msg);
+	case MUIM_Popstring_Open: return Popstring_Open(cl, obj, (struct MUIP_Popstring_Open*)msg);
     }
     
     return DoSuperMethodA(cl, obj, msg);
