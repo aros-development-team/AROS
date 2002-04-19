c0674493d6ec2daaaa72567c3a5747a83eac8240
iaint <iaint@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-04-19 10:11:57 +0000

    Maths support, required for the new libm.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14198 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/__math.h
+++ compiler/clib/__math.h
@@ -1,85 +1,256 @@
-#ifndef __MATH_H
-#define __MATH_H
-
-/* This is a partial copy of fdlibm.h from the fdlibm package by Sun */
 /*
  * ====================================================
  * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
  *
- * Developed at SunSoft, a Sun Microsystems, Inc. business.
+ * Developed at SunPro, a Sun Microsystems, Inc. business.
  * Permission to use, copy, modify, and distribute this
  * software is freely granted, provided that this notice
  * is preserved.
  * ====================================================
  */
 
-#ifndef AROS_MACHINE_H
-#   include <aros/machine.h>
-#endif
-#include <math.h>
+/*
+ * from: @(#)fdlibm.h 5.1 93/09/24
+ * from: $FreeBSD: src/lib/msun/src/math_private.h,v 1.6 1999/08/28 00:06:43 peter Exp $
+ * $Id$
+ */
 
-#if AROS_BIG_ENDIAN
-#   define __HI(x) *(int*)&x
-#   define __LO(x) *(1+(int*)&x)
-#   define __HIp(x) *(int*)x
-#   define __LOp(x) *(1+(int*)x)
-#else
-#   define __HI(x) *(1+(int*)&x)
-#   define __LO(x) *(int*)&x
-#   define __HIp(x) *(1+(int*)x)
-#   define __LOp(x) *(int*)x
-#endif
+#ifndef _MATH_PRIVATE_H_
+#define _MATH_PRIVATE_H_
 
-/* Use non-standard matherr() and smart code */
-#if !defined(__FreeBSD__)
-#define _LIB_VERSION	0
-#define _POSIX_ 	1
-#define _SVID_		2
-#define _IEEE_		3
-#elif defined(__FreeBSD__)
-#undef _LIB_VERSION
-#define _LIB_VERSION 	0
-#endif
+#include <aros/system.h>
+#include <stdint.h>
 
-/* We only include this section if we don't have glibc v2 or FreeBSD */
-#if (!defined(__GLIBC__) || (__GLIBC__ < 2)) && !defined(__FreeBSD__)
-struct exception
+/* The original fdlibm code used statements like:
+	n0 = ((*(int*)&one)>>29)^1;		* index of high word *
+	ix0 = *(n0+(int*)&x);			* high word of x *
+	ix1 = *((1-n0)+(int*)&x);		* low word of x *
+   to dig two 32 bit words out of the 64 bit IEEE floating point
+   value.  That is non-ANSI, and, moreover, the gcc instruction
+   scheduler gets it wrong.  We instead use the following macros.
+   Unlike the original code, we determine the endianness at compile
+   time, not at run time; I don't see much benefit to selecting
+   endianness at run time.  */
+
+/* A union which permits us to convert between a double and two 32 bit
+   ints.  */
+
+#if AROS_BIG_ENDIAN == 1
+
+typedef union
 {
-    int    type;
-    char  *name;
-    double arg1;
-    double arg2;
-    double retval;
-};
+  double value;
+  struct
+  {
+    uint32_t msw;
+    uint32_t lsw;
+  } parts;
+} ieee_double_shape_type;
+
 #endif
 
-/* exception.type */
-#define DOMAIN		1
-#define SING		2
-#define OVERFLOW	3
-#define UNDERFLOW	4
-#define TLOSS		5
-#define PLOSS		6
+#if AROS_BIG_ENDIAN == 0
 
-#if defined(__FreeBSD__)
-#undef HUGE_VAL
+typedef union
+{
+  double value;
+  struct
+  {
+    uint32_t	lsw;
+    uint32_t	msw;
+  } parts;
+} ieee_double_shape_type;
+
 #endif
 
-#ifdef copysign
-#  undef copysign
+/* Get two 32 bit ints from a double.  */
+
+#define EXTRACT_WORDS(ix0,ix1,d)				\
+do {								\
+  ieee_double_shape_type ew_u;					\
+  ew_u.value = (d);						\
+  (ix0) = ew_u.parts.msw;					\
+  (ix1) = ew_u.parts.lsw;					\
+} while (0)
+
+/* Get the more significant 32 bit int from a double.  */
+
+#define GET_HIGH_WORD(i,d)					\
+do {								\
+  ieee_double_shape_type gh_u;					\
+  gh_u.value = (d);						\
+  (i) = gh_u.parts.msw;						\
+} while (0)
+
+/* Get the less significant 32 bit int from a double.  */
+
+#define GET_LOW_WORD(i,d)					\
+do {								\
+  ieee_double_shape_type gl_u;					\
+  gl_u.value = (d);						\
+  (i) = gl_u.parts.lsw;						\
+} while (0)
+
+/* Set a double from two 32 bit ints.  */
+
+#define INSERT_WORDS(d,ix0,ix1)					\
+do {								\
+  ieee_double_shape_type iw_u;					\
+  iw_u.parts.msw = (ix0);					\
+  iw_u.parts.lsw = (ix1);					\
+  (d) = iw_u.value;						\
+} while (0)
+
+/* Set the more significant 32 bits of a double from an int.  */
+
+#define SET_HIGH_WORD(d,v)					\
+do {								\
+  ieee_double_shape_type sh_u;					\
+  sh_u.value = (d);						\
+  sh_u.parts.msw = (v);						\
+  (d) = sh_u.value;						\
+} while (0)
+
+/* Set the less significant 32 bits of a double from an int.  */
+
+#define SET_LOW_WORD(d,v)					\
+do {								\
+  ieee_double_shape_type sl_u;					\
+  sl_u.value = (d);						\
+  sl_u.parts.lsw = (v);						\
+  (d) = sl_u.value;						\
+} while (0)
+
+/* A union which permits us to convert between a float and a 32 bit
+   int.  */
+
+typedef union
+{
+  float value;
+  uint32_t word;
+} ieee_float_shape_type;
+
+/* Get a 32 bit int from a float.  */
+
+#define GET_FLOAT_WORD(i,d)					\
+do {								\
+  ieee_float_shape_type gf_u;					\
+  gf_u.value = (d);						\
+  (i) = gf_u.word;						\
+} while (0)
+
+/* Set a float from a 32 bit int.  */
+
+#define SET_FLOAT_WORD(d,i)					\
+do {								\
+  ieee_float_shape_type sf_u;					\
+  sf_u.word = (i);						\
+  (d) = sf_u.value;						\
+} while (0)
+
+/* ieee style elementary functions */
+extern double __ieee754_sqrt (double);
+extern double __ieee754_acos (double);
+extern double __ieee754_acosh (double);
+extern double __ieee754_log (double);
+extern double __ieee754_atanh (double);
+extern double __ieee754_asin (double);
+extern double __ieee754_atan2 (double,double);
+extern double __ieee754_exp (double);
+extern double __ieee754_cosh (double);
+extern double __ieee754_fmod (double,double);
+extern double __ieee754_pow (double,double);
+extern double __ieee754_lgamma_r (double,int *);
+extern double __ieee754_gamma_r (double,int *);
+extern double __ieee754_lgamma (double);
+extern double __ieee754_gamma (double);
+extern double __ieee754_log10 (double);
+extern double __ieee754_sinh (double);
+extern double __ieee754_hypot (double,double);
+extern double __ieee754_j0 (double);
+extern double __ieee754_j1 (double);
+extern double __ieee754_y0 (double);
+extern double __ieee754_y1 (double);
+extern double __ieee754_jn (int,double);
+extern double __ieee754_yn (int,double);
+extern double __ieee754_remainder (double,double);
+extern int32_t __ieee754_rem_pio2 (double,double*);
+extern double __ieee754_scalb (double,double);
+
+/* fdlibm kernel function */
+extern double __kernel_standard (double,double,int);
+extern double __kernel_sin (double,double,int);
+extern double __kernel_cos (double,double);
+extern double __kernel_tan (double,double,int);
+extern int    __kernel_rem_pio2 (double*,double*,int,int,int,const int32_t*);
+
+
+/* ieee style elementary float functions */
+extern float __ieee754_sqrtf (float);
+extern float __ieee754_acosf (float);
+extern float __ieee754_acoshf (float);
+extern float __ieee754_logf (float);
+extern float __ieee754_atanhf (float);
+extern float __ieee754_asinf (float);
+extern float __ieee754_atan2f (float,float);
+extern float __ieee754_expf (float);
+extern float __ieee754_coshf (float);
+extern float __ieee754_fmodf (float,float);
+extern float __ieee754_powf (float,float);
+extern float __ieee754_lgammaf_r (float,int *);
+extern float __ieee754_gammaf_r (float,int *);
+extern float __ieee754_lgammaf (float);
+extern float __ieee754_gammaf (float);
+extern float __ieee754_log10f (float);
+extern float __ieee754_sinhf (float);
+extern float __ieee754_hypotf (float,float);
+extern float __ieee754_j0f (float);
+extern float __ieee754_j1f (float);
+extern float __ieee754_y0f (float);
+extern float __ieee754_y1f (float);
+extern float __ieee754_jnf (int,float);
+extern float __ieee754_ynf (int,float);
+extern float __ieee754_remainderf (float,float);
+extern int32_t __ieee754_rem_pio2f (float,float*);
+extern float __ieee754_scalbf (float,float);
+
+/* float versions of fdlibm kernel functions */
+extern float __kernel_sinf (float,float,int);
+extern float __kernel_cosf (float,float);
+extern float __kernel_tanf (float,float,int);
+extern int   __kernel_rem_pio2f (float*,float*,int,int,int,const int32_t*);
+
+/*
+    This is how to specify which are implemented in assembler,
+    unfortunately we do not do this quite yet.
+#ifdef	__alpha__
+*/
+#define __generic___ieee754_acos	__ieee754_acos
+#define __generic___ieee754_asin	__ieee754_asin
+#define __generic___ieee754_atan2	__ieee754_atan2
+#define __generic___ieee754_exp		__ieee754_exp
+#define __generic___ieee754_fmod	__ieee754_fmod
+#define __generic___ieee754_log		__ieee754_log
+#define __generic___ieee754_log10	__ieee754_log10
+#define __generic___ieee754_remainder	__ieee754_remainder
+#define __generic___ieee754_scalb	__ieee754_scalb
+#define __generic___ieee754_sqrt	__ieee754_sqrt
+#define	__generic_atan			atan
+#define	__generic_ceil			ceil
+#define	__generic_copysign		copysign
+#define	__generic_cos			cos
+#define	__generic_finite		finite
+#define	__generic_floor			floor
+#define	__generic_ilogb			ilogb
+#define	__generic_log1p			log1p
+#define	__generic_logb			logb
+#define	__generic_rint			rint
+#define	__generic_scalbn		scalbn
+#define	__generic_significand		significand
+#define	__generic_sin			sin
+#define	__generic_tan			tan
+/*
 #endif
+*/
 
-extern double __kernel_standard (double, double, int);
-extern double scalbn		(double, int);
-extern double copysign		(double x, double y);
-extern double __ieee754_pow	(double, double);
-extern double __ieee754_log10	(double);
-extern double __ieee754_log	(double);
-extern int    isinf		(double);
-extern int    __isinf		(double);
-
-#if 0 /* Unused */
-extern int    matherr		(struct exception *);
-#endif
-
-#endif /* __MATH_H */
+#endif /* _MATH_PRIVATE_H_ */
