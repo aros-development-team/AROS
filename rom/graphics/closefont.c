72570031ce1138d5961ead594addc02f4a7a45c3
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-04-22 18:07:47 +0000

    no longer tries to freevec the font/fontdata.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14242 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/graphics/closefont.c
+++ rom/graphics/closefont.c
@@ -47,8 +47,6 @@
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct GfxBase *,GfxBase)
 
-    BOOL killfont = FALSE;
-
     if (!textFont) return;
 
     ASSERT_VALID_PTR(textFont);
@@ -58,52 +56,8 @@
     if ((textFont->tf_Accessors == 0) && !(textFont->tf_Flags & FPF_ROMFONT))
     {
     	RemFont(textFont);
-	killfont = TRUE;
     }
     Permit();
-
-    if (!killfont) return;
- 
-    /* Free font data */
-
-    /* !!! NOTE. FreeXXX functions has to match AllocXXX in
-       workbench/libs/diskfont/diskfont_io.c
-    */
-
-    if (textFont->tf_Style & FSF_COLORFONT)
-    {
-	struct ColorFontColors *cfc;
-	UWORD 	    	       i;
-
-	for (i = 0; i < 8; i ++)
-	{
-	    if (CTF(textFont)->ctf_CharData[i]) FreeVec(CTF(textFont)->ctf_CharData[i]);
-	}
-
-	cfc = CTF(textFont)->ctf_ColorFontColors;
-	if (cfc)
-	{
-	    if (cfc->cfc_ColorTable) FreeVec(cfc->cfc_ColorTable);
-	    FreeVec(cfc);
-	}
-
-    }
-    else
-    {
-	/* Not a colortextfont, only one plane */
-	FreeVec(textFont->tf_CharData);
-    }
-    
-    StripFont(textFont);
-
-    if (textFont->tf_CharSpace) FreeVec(textFont->tf_CharSpace);
-    if (textFont->tf_CharKern) FreeVec(textFont->tf_CharKern);
-
-    /* All fonts have a tf_CharLoc allocated */    
-    FreeVec(textFont->tf_CharLoc); 
-
-    FreeVec(textFont->tf_Message.mn_Node.ln_Name);
-    FreeVec(textFont);
 
     AROS_LIBFUNC_EXIT
     
