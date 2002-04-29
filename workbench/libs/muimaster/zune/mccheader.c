4cc9f173ba6ee02f42bc1ea068f3c778d5810cef
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-04-29 10:59:40 +0000

    Changed copyright notice.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14354 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/zune/mccheader.c
+++ workbench/libs/muimaster/zune/mccheader.c
@@ -1,3 +1,11 @@
+/* 
+    Copyright © 1999, David Le Corfec.
+    Copyright © 2002, The AROS Development Team.
+    All rights reserved.
+
+    $Id$
+*/
+
 #ifndef MCC_HEADER_C
 #define MCC_HEADER_C
 
