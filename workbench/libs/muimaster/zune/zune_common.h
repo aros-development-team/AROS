34198f1b779ad6db8105720e9c0bb28bbeb0f922
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-04-29 10:59:40 +0000

    Changed copyright notice.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14354 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/zune/zune_common.h
+++ workbench/libs/muimaster/zune/zune_common.h
@@ -1,3 +1,11 @@
+/* 
+    Copyright © 1999, David Le Corfec.
+    Copyright © 2002, The AROS Development Team.
+    All rights reserved.
+
+    $Id$
+*/
+
 #ifndef __ZUNE_COMMON_H__
 #define __ZUNE_COMMON_H__
 
