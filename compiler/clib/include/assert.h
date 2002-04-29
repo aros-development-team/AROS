65f3c161c556a35455e608d3df5e1c89a59417f3
iaint <iaint@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-04-29 09:36:49 +0000

    Make assert() evaluate to a state when NDEBUG is defined.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14339 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/include/assert.h
+++ compiler/clib/include/assert.h
@@ -1,5 +1,5 @@
-#ifndef _ASSERT_H
-#define _ASSERT_H
+#ifndef _ASSERT_H_
+#define _ASSERT_H_
 
 /*
     Copyright © 1995-2001, The AROS Development Team. All rights reserved.
@@ -14,11 +14,11 @@
 #endif
 
 #ifdef NDEBUG
-#define assert(expr)	/* eps */
+#define assert(expr)	((void)0)
 #else
-#define assert(expr)	((expr)) ? (void)0 : __assert(#expr,__FILE__,__LINE__)
+#define assert(expr)	(((expr)) ? (void)0 : __assert(#expr,__FILE__,__LINE__))
 #endif
 
 extern void __assert (const char *, const char *, unsigned int);
 
-#endif /* _ASSERT_H */
+#endif /* _ASSERT_H_ */
