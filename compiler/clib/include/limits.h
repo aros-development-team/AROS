5b498a620f8c5e9b2580f61855ab42de08c1ba72
hkiel <hkiel@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-05-01 17:23:15 +0000

    A primitve version of limit.h holding the minimal and maximal values for different integer types.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14383 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/include/limits.h
@@ -0,0 +1,70 @@
+/*
+ *  ISO C99 Standard: Sizes of integer types <limits.h>
+ */
+
+#ifndef LIMITS_H
+#define LIMITS_H 1
+
+/*
+ *  Define ANSI <limits.h> for standard 32-bit words.
+ *  We're assuming
+ *  8-bit 'char',
+ *  16-bit 'short int', and
+ *  32-bit 'int' and 'long int'.
+ */
+
+/* Number of bits in a 'char' */
+#define CHAR_BIT	8
+
+/* Minimum and maximum number displayable with 'signed char' */
+#define SCHAR_MIN	(-128)
+#define SCHAR_MAX	127
+
+/* Minimum and maximum number displayable with 'char' */
+#define CHAR_MIN	SCHAR_MIN
+#define CHAR_MAX	SCHAR_MAX
+
+/* Maximum number displayable with 'unsigned char'. Minimum is 0. */
+#define UCHAR_MAX	255
+
+
+/* Minimum and maximum number displayable with 'signed short int' */
+#define SHRT_MIN	(-32768)
+#define SHRT_MAX	32767
+
+/* Maximum number displayable with 'unsigned short int'. Minimum is 0. */
+#define USHRT_MAX	65535
+
+
+/* Minimum and maximum displayable with 'signed int' */
+#define INT_MAX		2147483647
+#define INT_MIN		(-INT_MAX - 1)
+
+/* Maximum value displayable with 'unsigned int'. Minimum is 0. */
+#  define UINT_MAX	4294967295U
+
+/* Minimum and maximum number displayable with `signed long int' */
+#if __WORDSIZE == 64
+#   define LONG_MAX	9223372036854775807L
+#else
+#   define LONG_MAX	2147483647L
+#endif
+#define LONG_MIN	(-LONG_MAX - 1L)
+
+/* Maximum number displayable with `unsigned long int'. Minimum is 0. */
+#if __WORDSIZE == 64
+#   define ULONG_MAX    18446744073709551615UL
+#else
+#   define ULONG_MAX    4294967295UL
+#endif
+
+
+
+/*
+ *  Maximal length of a path name.
+ *  This is value is borrowed from Linux and may be totally wrong for AROS,
+ *  at least it is defined to make some apps happy.
+ */
+#define PATH_MAX	4095
+
+#endif
