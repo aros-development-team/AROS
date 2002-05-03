1a8121235ef665036a08a132c9453d1816ac5557
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-05-03 22:06:23 +0000

    Prototypes for functions in arosc_init.c (needed by arosc_functable.c).
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14395 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/arosc_init.h
@@ -0,0 +1,9 @@
+#ifndef _AROSC_INIT_H
+#define _AROSC_INIT_H
+
+#include <libraries/arosc.h>
+
+int arosc_internalinit( struct AroscUserData *userdata );
+int arosc_internalexit( void );
+
+#endif /* _AROSC_INIT_H */
