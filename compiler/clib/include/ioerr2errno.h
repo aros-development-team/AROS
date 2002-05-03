54226828d8a7a8b9f62673783e4a572634b3c0b8
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-05-03 22:08:16 +0000

    Prototype for the IoErr2errno() function. Should this one be private?
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14397 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/include/ioerr2errno.h
@@ -0,0 +1,12 @@
+/*
+    Copyright © 1995-2002, The AROS Development Team. All rights reserved.
+    
+    $Id$
+*/
+
+#ifndef _IOERR2ERRNO_H
+#define _IOERR2ERRNO_H
+
+int IoErr2errno( int ioerr );
+
+#endif /* _IOERR2ERRNO_H */
