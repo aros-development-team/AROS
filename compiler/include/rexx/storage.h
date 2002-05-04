e894ef9f8207fee387f21000b3e6b67ad380e255
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-05-04 13:30:35 +0000

    Defined the RxsLib structure now. Needed to store the lists anyway somewhere so could as well do it the same way as AmigaOS. Defined RexxRsrc structure and some flags. Changed rm_Args field of RexxMsg from STRPTR to IPTR because for FillRexxMsg possibly a number can be stored inthere.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14429 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/rexx/storage.h
+++ compiler/include/rexx/storage.h
@@ -27,7 +27,7 @@ struct RexxMsg
 	LONG		rm_Action;	/* command (action) code */
 	LONG		rm_Result1;	/* primary result (return code) */
 	IPTR		rm_Result2;	/* secondary result */
-	STRPTR		rm_Args[16];	/* argument block (ARG0-ARG15) */
+	IPTR		rm_Args[16];	/* argument block (ARG0-ARG15) */
 	struct MsgPort *rm_PassPort;	/* forwarding port */
 	STRPTR		rm_CommAddr;	/* host address (port name) */
 	STRPTR		rm_FileExt;	/* file extension */
@@ -69,11 +69,29 @@ struct RexxMsg
 
 struct RexxArg
 {
-        LONG  ra_Size;
-        UWORD ra_Length;
-        UBYTE ra_Depricated1; /* Was ra_Flags but not used anymore */
-        UBYTE ra_Depricated2; /* Was ra_Hash but not used anymore */
-        BYTE  ra_Buff[8];
+	LONG  ra_Size;
+	UWORD ra_Length;
+	UBYTE ra_Depricated1; /* Was ra_Flags but not used anymore */
+	UBYTE ra_Depricated2; /* Was ra_Hash but not used anymore */
+	BYTE  ra_Buff[8];
 };
+
+struct RexxRsrc
+{
+	struct Node rr_Node;
+	WORD        rr_Unused1; /* rr_Func */
+	APTR        rr_Unused2; /* rr_Base */
+	LONG        rr_Size; /* Total size of structure */
+	LONG        rr_Args1; /* Meaning depends on type of Resource */
+	LONG        rr_Args2; /* Meaning depedns on type of Resource */
+};
+
+/* Types for the resource nodes */
+#define RRT_ANY   0
+#define RRT_LIB   1  /* A function library */
+/*#define RRT_PORT  2  Not used */
+/*#define RRT_FILE  3  Not used */
+#define RRT_HOST  4  /* A function host */
+#define RRT_CLIP  5  /* A clip on the clip list */
 
 #endif /* REXX_STORAGE_H */
