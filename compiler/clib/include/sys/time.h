cf025c6d9ddd98657de571da02b606c3fc30cbbd
iaint <iaint@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-05-04 03:55:12 +0000

    POSIXify <sys/time.h> includes extra stuff, but includes <time.h> which is probably not allowed (namespace pollution - I don't see how to get around it though). Add C++ stuff.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14411 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/include/sys/time.h
+++ compiler/clib/include/sys/time.h
@@ -1,24 +1,42 @@
-#ifndef _SYS_TIME_H
-#define _SYS_TIME_H
+#ifndef _SYS_TIME_H_
+#define _SYS_TIME_H_
 
 /*
-    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2002, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: ANSI-C header file sys/time.h
     Lang: english
 */
 
+#include <sys/types.h>
+#include <time.h>		/* XXX Probably not allowed */
+
 /* This structure describes a specific time. See also <devices/timer.h>. */
 #ifndef DEVICES_TIMER_H
 struct timeval
 {
-    long tv_sec;  /* Seconds passed. */
-    long tv_usec; /* Microseconds passed in the current second. */
+    time_t	tv_sec;	    /* Seconds passed. */
+    suseconds_t	tv_usec;    /* Microseconds passed in the current second. */
 };
 #endif /* DEVICES_TIMER_H */
 
-/* This structure describes a timezone. */
+/* struct itimerval is used by the interval timers getitimer()/setitimer() */
+struct itimerval
+{
+    struct timeval	it_interval;	    /* timer interval */
+    struct timeval	it_value;	    /* current value */
+};
+
+/* Which interval timer */
+#define ITIMER_REAL	    0	    /* Decrements in real time */
+#define ITIMER_VIRTUAL	    1	    /* Decrements in task virtual time */
+#define ITIMER_PROF	    2	    /* Decrements in task virtual/system time */
+
+/*
+    This structure describes a timezone. Note that most implementations of the
+    C library no longer use the timezone information passed to gettimeofday().
+*/
 struct timezone
 {
     int  tz_minuteswest; /* Minutes west of Greenwich. */
@@ -38,9 +56,21 @@ struct timezone
 #define DST_TUR     9  /* Turkish style. */
 #define DST_AUSTALT 10 /* Alternate Australian style. */
 
+
+__BEGIN_DECLS
+
 /* clib functions */
+int getitimer(int which, struct itimerval *);
+int setitimer(int which, const struct itimerval *, struct itimerval *);
 int gettimeofday(struct timeval * tv, struct timezone * tz);
 int settimeofday(const struct timeval * tv, const struct timezone * tz);
 int utimes(const char *file, struct timeval tvp[2]);
 
-#endif /* _SYS_TIME_H */
+__END_DECLS
+
+/*
+    SUSv2 says that select() is defined here. BSD however defines it in
+    unistd.h. So does AROS, as that makes more sense.
+*/
+
+#endif /* _SYS_TIME_H_ */
