9750f41e32d1d51804836acb05d35a1ec0c87fbc
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-05-05 16:46:11 +0000

    Added missing parts.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14446 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/rexx/storage.h
+++ compiler/include/rexx/storage.h
@@ -19,6 +19,53 @@
 #   include <dos/dosextens.h>
 #endif
 
+struct RexxMsg
+{
+	struct Message	rm_Node;
+	IPTR		rm_Private1; /* Was rm_TaskBlock */
+	IPTR		rm_Private2; /* Was rm_LibBase */
+	LONG		rm_Action; /* What to do ? */
+	LONG		rm_Result1; /* The first result as a number */
+	IPTR		rm_Result2; /* The second result, most of the time an argstring */
+	IPTR		rm_Args[16]; /* 16 possible arguments for function calls */
+	struct MsgPort *rm_PassPort;
+	STRPTR		rm_CommAddr; /* The starting host environment */
+	STRPTR		rm_FileExt; /* The file extension for macro files */
+	struct FileHandle *rm_Stdin; /* Input filehandle to use */
+	struct FileHandle *rm_Stdout; /* Output filehandle to use */
+	LONG		rm_Unused1; /* Was rm_avail */
+};
+
+/* Shortcuts for the arguments */
+#define ARG0(msg) (msg->rm_Args[0])
+#define ARG1(msg) (msg->rm_Args[1])
+#define ARG2(msg) (msg->rm_Args[2])
+
+/* The command for in rm_Action */
+#define RXCOMM   0x01000000
+#define RXFUNC   0x02000000
+#define RXCLOSE  0x03000000
+#define RXQUERY  0x04000000
+#define RXADDFH  0x07000000
+#define RXADDLIB 0x08000000
+#define RXREMLIB 0x09000000
+#define RXADDCON 0x0A000000
+#define RXREMCON 0x0B000000
+#define RXTCOPN  0x0C000000
+#define RXTCCLS  0x0D000000
+
+#define RXCODEMASK 0xFF000000
+#define RXARGMASK  0x0000000F
+
+/* Flags that can be combined with the commands */
+#define RXFB_NOIO     16
+#define RXFB_RESULT   17
+#define RXFB_STRING   18
+#define RXFB_TOKEN    19
+#define RXFB_NONRET   20
+#define RXFB_FUNCLIST 5
+
+/* Convert from bit number to number */
 #define RXFF_NOIO	(1<<RXFB_NOIO)
 #define RXFF_RESULT	(1<<RXFB_RESULT)
 #define RXFF_STRING	(1<<RXFB_STRING)
@@ -42,7 +89,7 @@ struct RexxRsrc
 	APTR        rr_Unused2; /* rr_Base */
 	LONG        rr_Size; /* Total size of structure */
 	LONG        rr_Args1; /* Meaning depends on type of Resource */
-	LONG        rr_Args2; /* Meaning depedns on type of Resource */
+	LONG        rr_Args2; /* Meaning depends on type of Resource */
 };
 
 /* Types for the resource nodes */
