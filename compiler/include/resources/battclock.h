d4a418f46c7bb3528da900995af42ec49dc3efd2
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-05-15 15:59:21 +0000

    this one was missing
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14582 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/resources/battclock.h
@@ -0,0 +1,11 @@
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#ifndef RESOURCES_BATTCLOCK_H
+#define RESOURCES_BATTCLOCK_H 1
+
+#define BATTCLOCKNAME   "battclock.resource"
+
+#endif /* RESOURCES_BATTCLOCK_H */
