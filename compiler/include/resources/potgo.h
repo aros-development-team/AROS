567868a082ee17ac85b18e92b16f1a893a3c0df5
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-05-20 13:00:54 +0000

    Inlclude file for potgo.resource.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14612 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/resources/potgo.h
@@ -0,0 +1,11 @@
+/*
+    Copyright © 1995-2002, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#ifndef RESOURCES_POTGO_H
+#define RESOURCES_POTGO_H
+
+#define POTGONAME "potgo.resource"
+
+#endif /* RESOURCES_POTGO_H */
