91e4a38af7544a11395433363c9a78f91df7227b
sebauer <sebauer@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-05-21 19:29:54 +0000

    Added dummy settingsgroup class. However I'm actually not so sure what this class should do, but it is probably needed for the mui prefs (and external classes)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14626 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/muimaster/classes/settingsgroup.h
@@ -0,0 +1,24 @@
+/*
+    Copyright © 2002, The AROS Development Team. 
+    All rights reserved.
+    
+    $Id$
+*/
+
+#ifndef _MUI_CLASSES_SETTINGSGROUP_H
+#define _MUI_CLASSES_SETTINGSGROUP_H
+
+#ifndef METHOD_USER
+#define METHOD_USER TAG_USER
+#endif
+
+#define MUIC_Settingsgroup  "Settingsgroup.mui"
+
+/* Settingsgroups methods */
+
+#define MUIM_Settingsgroup_ConfigToGadgets (METHOD_USER|0x00427043) /* MUI: V11 */
+#define MUIM_Settingsgroup_GadgetsToConfig (METHOD_USER|0x00425242) /* MUI: V11 */
+struct MUIP_Settingsgroup_ConfigToGadgets  {ULONG MethodID; Object *configdata; };
+struct MUIP_Settingsgroup_GadgetsToConfig  {ULONG MethodID; Object *configdata; };
+
+#endif
