7b15aea1075e7843530bd38429bd678ff7326655
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-06-03 00:25:44 +0000

    Added some flags. The dragonball documentation is all wrong about them.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14695 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/.unmaintained/m68k-pp-native/include/asm/irq.h
+++ arch/.unmaintained/m68k-pp-native/include/asm/irq.h
@@ -1,6 +1,5 @@
 #ifndef ASM_IRQ_H
 #define ASM_IRQ_H
-
 /*
     Copyright © 1995-2001, The AROS Development Team. All rights reserved.
     $Id$
@@ -9,14 +8,7 @@
     Lang: english
 */
 
-//#include <asm/segments.h>
-//#include <asm/linkage.h>
-//#include <asm/ptrace.h>
-
-/* interrupt control.. */
-
-//#define __sti() __asm__ __volatile__ ("sti": : :"memory")
-//#define __cli() __asm__ __volatile__ ("cli": : :"memory")
+#include <exec/ptrace.h>
 
 /*********************************************************************/
 
@@ -24,69 +16,6 @@
 #define STR(x) __STR(x)
 
 /*********************************************************************/
-
-#if 0
-/*
- * This is standard macro used to save register frame on the supervisor
- * stack. Supposes that orig_eax is saved already.
- */
-#define SAVE_REGS       	\
-    "cld		\n\t"	\
-    "pushl   %eax       \n\t"	\
-    "pushl   %ecx       \n\t"	\
-    "pushl   %edx       \n\t"	\
-    "push    %ds        \n\t"	\
-    "push    %es        \n\t"	\
-    "movl    $"STR(KERNEL_DS)",%edx\n\t" \
-    "mov     %dx,%ds    \n\t"	\
-    "mov     %dx,%es	\n\t"
-
-#define RESTORE_REGS    	\
-    "pop     %es	\n\t"   \
-    "pop     %ds        \n\t"	\
-    "popl    %edx	\n\t"   \
-    "popl    %ecx       \n\t"	\
-    "popl    %eax	\n\t"
-
-#define R_es        0x00
-#define R_ds        0x04
-#define R_edx       0x08
-#define R_ecx       0x0c
-#define R_eax       0x10
-#define R_param     0x14
-
-/*
-    Save/restore all registers. These macros are used mostly inside Switch
-    procedure.
-*/
-
-#define SAVE_ALL        	\
-    "cld		\n\t"	\
-    "push    %es        \n\t"	\
-    "push    %ds        \n\t"	\
-    "pushl   %eax       \n\t"	\
-    "pushl   %ebp       \n\t"	\
-    "pushl   %edi       \n\t"	\
-    "pushl   %esi       \n\t"	\
-    "pushl   %edx       \n\t"	\
-    "pushl   %ecx       \n\t"	\
-    "pushl   %ebx       \n\t"	\
-    "movl    $"STR(KERNEL_DS)",%edx \n\t"\
-    "mov     %dx,%ds    \n\t"	\
-    "mov     %dx,%es	\n\t"
-
-#define RESTORE_ALL     	\
-    "popl    %ebx       \n\t"	\
-    "popl    %ecx       \n\t"	\
-    "popl    %edx       \n\t"	\
-    "popl    %esi       \n\t"	\
-    "popl    %edi       \n\t"	\
-    "popl    %ebp       \n\t"	\
-    "popl    %eax       \n\t"	\
-    "pop     %ds        \n\t"	\
-    "pop     %es	\n\t"
-
-#endif
 
 #define FIRST_EXT_VECTOR    0x20    /* This is exactly what Intel says about */
 #define SYSTEM_VECTOR       0x80    /* Vector callable from user mode */
@@ -123,7 +52,7 @@ struct irqDescriptor
     struct irqServer        *id_server;     /* Server pointer */
     unsigned int            id_depth;       /* Disable depth for nested IRQs */
     unsigned int            id_count;       /* IRQ counter */
-    unsigned int id_unused[3];
+    unsigned int            id_unused[3];
 };
 
 /* IRQ status */
