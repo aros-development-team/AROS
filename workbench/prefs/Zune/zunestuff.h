cfb02ad52007c9b93f0a8ef4ef21c4d30090c29e
sebauer <sebauer@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-06-10 09:23:10 +0000

    Loading/Saving implemented
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14751 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/prefs/Zune/zunestuff.h
+++ workbench/prefs/Zune/zunestuff.h
@@ -5,9 +5,13 @@
     $Id$
 */
 
+#ifndef _ZUNE_ZUNESTUFF_H
+#define _ZUNE_ZUNESTUFF_H
+
 Object *MakeLabel(STRPTR str);
 Object *MakeButton(STRPTR str);
 LONG xget(Object * obj, ULONG attr);
 
+#define getstring(obj) (char*)xget(obj,MUIA_String_Contents)
 
-
+#endif
