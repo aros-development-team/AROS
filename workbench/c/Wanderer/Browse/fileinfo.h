617aca8ecd4e42f8980045b403c23af4e6e31fd2
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-06-11 19:04:27 +0000

    The beginnings of a file browser for the AROS workbench. It's very raw code, and only commited because of public demand. ;-)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14766 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/c/Wanderer/Browse/fileinfo.h
@@ -0,0 +1,20 @@
+#ifndef W_FILEINFO_H
+#define W_FILEINFO_H
+
+#include <exec/types.h>
+
+/*** Structures **************************************************************/
+
+struct FileInfo
+{
+    STRPTR *fi_Name;
+    UWORD   fi_NameLength;
+    LONG    fi_Size;
+};
+
+/*** Prototypes **************************************************************/
+
+struct FileInfo *allocFileInfo( APTR pool );
+void             freeFileInfo( struct FileInfo *info, APTR pool );
+
+#endif /* W_FILEINFO_H */
---
+++ workbench/c/Wanderer/Browse/formatting.h
@@ -0,0 +1,10 @@
+#ifndef W_FORMATTING_H
+#define W_FORMATTING_H
+
+#include <exec/types.h>
+
+/*** Prototypes **************************************************************/
+
+void formatSize( STRPTR buffer, ULONG bufferSize, LONG size );
+
+#endif /* W_FORMATTING_H */
