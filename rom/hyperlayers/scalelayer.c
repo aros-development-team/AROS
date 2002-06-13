2853297429efd750a24ce3ca49d8cf4ff0738543
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-06-13 03:41:57 +0000

    Explicit cast to avoid warnings.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14797 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/hyperlayers/scalelayer.c
+++ rom/hyperlayers/scalelayer.c
@@ -73,7 +73,7 @@ AROS_UFP3(struct Region *, ScaleLayerCallback,
   struct Region * oldshape;
   struct Hook hook;
   
-  hook.h_Entry = (IPTR)ScaleLayerCallback;
+  hook.h_Entry = (IPTR *)ScaleLayerCallback;
   hook.h_Data  = (APTR)&parm;
 
   oldshape = ChangeLayerShape(l, 0, &hook);
