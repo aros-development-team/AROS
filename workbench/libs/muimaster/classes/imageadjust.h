c166c7b4c5e31f36f9435cd91ed1d067852c2bd2
sebauer <sebauer@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-07-03 22:28:43 +0000

    Started implementation of the "External" page
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14913 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/classes/imageadjust.h
+++ workbench/libs/muimaster/classes/imageadjust.h
@@ -10,6 +10,10 @@
 
 #define MUIC_Imageadjust "Imageadjust.mui"
 
+#ifndef METHOD_USER
+#define METHOD_USER TAG_USER
+#endif
+
 #define MUIA_Imageadjust_Type (TAG_USER|0x00422f2b) /* MUI: V11 i.. LONG */
 #define MUIA_Imageadjust_Spec (TAG_USER|0x004279e1) /* MUI: ??? .g. char * */
 
@@ -22,5 +26,7 @@ enum
 };
 
 extern const struct __MUIBuiltinClass _MUI_Imageadjust_desc; /* PRIV */
+
+#define MUIM_Imageadjust_ReadExternal (METHOD_USER|0x10101010) /* PRIV */
 
 #endif
