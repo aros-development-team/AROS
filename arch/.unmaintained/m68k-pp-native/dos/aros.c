b8e46d12ebe570c4b204c92ffeb3b3a6f9f11adc
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-07-03 00:19:56 +0000

    Added some debugging code - is deactiavted.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14900 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/.unmaintained/m68k-pp-native/dos/aros.c
+++ arch/.unmaintained/m68k-pp-native/dos/aros.c
@@ -13,6 +13,11 @@
 #include <proto/intuition.h>
 #include <proto/exec.h>
 
+#include <asm/registers.h>
+
+#define DEBUG 1
+#include <aros/debug.h>
+
 #define CANNOT_LOAD_SHELL	"Unable to load C:shell\n"
 #define CANNOT_OPEN_CON		"Cannot open boot console\n"
 
@@ -20,44 +25,72 @@ extern void hidd_demo(struct ExecBase * SysBase);
 
 int main(struct ExecBase * SysBase, struct Library * DOSBase)
 {
-    LONG            rc = RETURN_FAIL;
-    hidd_demo(SysBase);
+	LONG            rc = RETURN_FAIL;
 
 
-{
-//    BPTR sseq = Open("S:Startup-Sequence", FMF_READ);
-    BPTR cis  = Open("CON:20/20///Boot Shell/AUTO", FMF_READ);
+	BPTR sseq = Open("S:Startup-Sequence", FMF_READ);
+	BPTR cis = Open("CON:20/20///Boot Shell/AUTO", FMF_READ);
+	// Necessary! Don't remove because otherwise it carshes!
+	hidd_demo(SysBase);
 
+	if (cis) {
+		struct TagItem tags[] =
+		{
+			{ SYS_Asynch,      TRUE       },
+			{ SYS_Background,  FALSE      },
+			{ SYS_Input,       (IPTR)cis  },
+			{ SYS_Output,      (IPTR)NULL },
+			{ SYS_Error,       (IPTR)NULL },
+			{ SYS_ScriptInput, (IPTR)sseq },
+			{ TAG_DONE,       0           }
+		};
 
-    if (cis)
-    {
-	struct TagItem tags[] =
-        {
-            { SYS_Asynch,      TRUE       },
-	    { SYS_Background,  FALSE      },
-	    { SYS_Input,       (IPTR)cis  },
-	    { SYS_Output,      (IPTR)NULL },
-	    { SYS_Error,       (IPTR)NULL },
-//	    { SYS_ScriptInput, (IPTR)sseq },
-	    { TAG_DONE,       0           }
-        };
-
-        rc = SystemTagList("", tags);
-	if (rc != -1)
-	{
-	    cis  = NULL;
-//	    sseq = NULL;
+		rc = SystemTagList("", tags);
+		if (rc != -1) {
+			cis  = NULL;
+			sseq = NULL;
+		} else
+			rc = RETURN_FAIL;
+	} else {
+		PutStr(CANNOT_OPEN_CON);
 	}
-	else
-	    rc = RETURN_FAIL;
-    }
-    else
-    {
-        PutStr(CANNOT_OPEN_CON);
-    }
+	Close(cis);
+	Close(sseq);
+	
 
-    Close(cis);
-//    Close(sseq);
+#if 0
+D(bug("Entering endless loop here in %s\n",__FUNCTION__));
+while (1) {
+	struct Task * me = FindTask(NULL), * t;
+	int i = 0;
+	D(bug("IPR: 0x%x, IMR: 0x%x, SR=0x%x\n",RREG_L(IPR),RREG_L(IMR),SetSR(0x0,0x0)));
+	Forbid();
+	D(bug("This task %p has priority: %d, name: %s\n",
+	      me,
+	      me->tc_Node.ln_Pri,
+	      me->tc_Node.ln_Name));
+	D(bug("IDNestCount: %d\n",SysBase->IDNestCnt));
+	while (i++ < 3) {
+		t = (struct Task *)SysBase->TaskReady.lh_Head;
+		while (NULL != t->tc_Node.ln_Succ) {
+			D(bug("ready task %p with priority: %d, name: %s\n",
+			      t,
+			      t->tc_Node.ln_Pri,
+			      t->tc_Node.ln_Name));
+			t = (struct Task *)t->tc_Node.ln_Succ;
+		}
+		t = (struct Task *)SysBase->TaskWait.lh_Head;
+		while (NULL != t->tc_Node.ln_Succ) {
+			D(bug("waiting task %p with priority: %d, name: %s\n",
+			      t,
+			      t->tc_Node.ln_Pri,
+			      t->tc_Node.ln_Name));
+			t = (struct Task *)t->tc_Node.ln_Succ;
+		}
+	}
+	Permit();
+	Delay(5000);
 }
-    return rc;
+#endif
+	return rc;
 }
