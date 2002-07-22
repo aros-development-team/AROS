7d4f256b38a9677b5840d2ad94aab6fbfcab70bc
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-07-22 20:05:13 +0000

    Added definition of arexx error definitions
    git-svn-id: https://svn.aros.org/svn/aros/trunk@15004 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/rexx/errors.h
+++ compiler/include/rexx/errors.h
@@ -13,4 +13,54 @@
 #define RC_ERROR	10L
 #define RC_FATAL	20L
 
+/* Added old error definitions, see Arexx documentation for their meaning */
+#define ERR10_001  1
+#define ERR10_002  2
+#define ERR10_003  3
+#define ERR10_004  4
+#define ERR10_005  5
+#define ERR10_006  6
+#define ERR10_007  7
+#define ERR10_008  8
+#define ERR10_009  9
+#define ERR10_010 10
+#define ERR10_011 11
+#define ERR10_012 12
+#define ERR10_013 13
+#define ERR10_014 14
+#define ERR10_015 15
+#define ERR10_016 16
+#define ERR10_017 17
+#define ERR10_018 18
+#define ERR10_019 19
+#define ERR10_020 20
+#define ERR10_021 21
+#define ERR10_022 22
+#define ERR10_023 23
+#define ERR10_024 24
+#define ERR10_025 25
+#define ERR10_026 26
+#define ERR10_027 27
+#define ERR10_028 28
+#define ERR10_029 29
+#define ERR10_030 30
+#define ERR10_031 31
+#define ERR10_032 32
+#define ERR10_033 33
+#define ERR10_034 34
+#define ERR10_035 35
+#define ERR10_036 36
+#define ERR10_037 37
+#define ERR10_038 38
+#define ERR10_039 39
+#define ERR10_040 40
+#define ERR10_041 41
+#define ERR10_042 42
+#define ERR10_043 43
+#define ERR10_044 44
+#define ERR10_045 45
+#define ERR10_046 46
+#define ERR10_047 47
+#define ERR10_048 48
+
 #endif /* REXX_ERRORS_H */
