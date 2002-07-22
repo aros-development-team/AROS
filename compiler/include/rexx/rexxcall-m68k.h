63da6d4671ba49cc5ef40278e84fccd35154cf4d
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-07-22 19:38:50 +0000

    Moved rexxcall.h and rexxcall-m68k.h from contrib/regina/rexxmast to here
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14998 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/rexx/rexxcall-m68k.h
@@ -0,0 +1,33 @@
+/*
+    Copyright © 1995-2002, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: Macro's to make calling arexx library querying functions portable
+    Lang: English
+*/
+
+#ifndef AROS_REXXCALL_H
+#define AROS_REXXCALL_H
+
+/* Some macro's to make ARexx portable to non-m68k platforms */
+#define RexxCallQueryLibFunc(rexxmsg, libbase, offset, retargstringptr) \
+  ({ \
+    int _offset=abs(offset)/6; \
+    AROS_LC2(ULONG, RexxCallQueryLibFunc, \
+	       AROS_LCA(struct RexxMsg *, rexxmsg, A0), \
+	       AROS_LCA(STRPTR *, retargstringptr, A1), \
+	       struct Library *, libbase, _offset, rexxcall); \
+  })
+
+#define AROS_AREXXLIBQUERYFUNC(f,m,lt,l,o,p) \
+  AROS_LH2(ULONG, f, \
+	     AROS_LCA(struct RexxMsg *, m, A0), \
+	     AROS_LCA(STRPTR *, _retargstringptr, A1), \
+	     lt, l, o, p)
+
+#warning FIXME: retargstringptr has to be returned in A0 also, asm has to be added
+#define ReturnRexxQuery(rc,arg) \
+  *_retargstringptr = arg; \
+  return rc;
+
+#endif
---
+++ compiler/include/rexx/rexxcall.h
@@ -0,0 +1,35 @@
+/*
+    Copyright © 1995-2002, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: Macro's to make calling arexx library querying functions portable
+    Lang: English
+*/
+
+#ifndef REXX_REXXCALL_H
+#define REXX_REXXCALL_H
+
+#include <exec/types.h>
+
+/* Some macro's to make ARexx portable to non-m68k platforms */
+#define RexxCallQueryLibFunc(rexxmsg, libbase, offset, retargstringptr) \
+  ({ \
+    int _offset=abs(offset)/6; \
+    AROS_LC2(ULONG, RexxCallQueryLibFunc, \
+	       AROS_LCA(struct RexxMsg *, rexxmsg, A0), \
+	       AROS_LCA(STRPTR *, retargstringptr, A1), \
+	       struct Library *, libbase, _offset, rexxcall); \
+  })
+
+#define AROS_AREXXLIBQUERYFUNC(f,m,lt,l,o,p) \
+  AROS_LH2(ULONG, f, \
+	     AROS_LCA(struct RexxMsg *, m, A0), \
+	     AROS_LCA(STRPTR *, _retargstringptr, A1), \
+	     lt, l, o, p)
+
+#define ReturnRexxQuery(rc,arg) \
+  ({ *_retargstringptr = arg; \
+     return rc; \
+  })
+
+#endif
