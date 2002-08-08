f87a0f22c755754a037ec3a668abe23b70b30d21
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-08-08 19:30:04 +0000

    Extended systemtags test a little bit.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@15140 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/systemtags.c
+++ test/systemtags.c
@@ -7,4 +7,6 @@ int main(void)
 {
   SystemTags("dir", SYS_Asynch, TRUE, SYS_Input, Open("NIL:", MODE_OLDFILE),
 	     SYS_Output, Open("RAW:////Dir/CLOSE/WAIT", MODE_NEWFILE), TAG_DONE);
+  SystemTags("systemtagsslave", SYS_Asynch, TRUE, SYS_Input, DupLock(Input()),
+	     SYS_Output, DupLock(Output()), TAG_DONE);
 }
---
+++ test/systemtagsslave.c
@@ -0,0 +1,18 @@
+#include <proto/dos.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    BPTR oldlock = CurrentDir(NULL);
+    BPTR lock = DupLock(oldlock);
+    CurrentDir(oldlock);
+  
+    system("dir");
+   
+    lock = CurrentDir(lock);
+    UnLock(lock);
+  
+    return 0;
+}
