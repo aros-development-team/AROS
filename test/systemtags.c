af54715c9fc44ec198509ca901d2802619927ad1
falemagn <falemagn@gmail.com>
2002-08-10 01:27:20 +0000

    Use SYS_DupStream insread of DupLock(). There are two main reasons to do so: 1) DupLock() can fail and return NULL, but NULL has special meanings to SystemTagList(); 2) DupLock() serves to duplicate Locks, not file handles. Although they are the same in AROS, this is not a good practive anyway because it defeats the semantic of DupLock(). Since this approach is anyway AROS-specific, you can just as well use SYS_DupStream, which is AROS-specific too, but much cleaner and doesn't have the 1st problem either.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@15162 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/systemtags.c
+++ test/systemtags.c
@@ -7,6 +7,6 @@ int main(void)
 {
   SystemTags("dir", SYS_Asynch, TRUE, SYS_Input, Open("NIL:", MODE_OLDFILE),
 	     SYS_Output, Open("RAW:////Dir/CLOSE/WAIT", MODE_NEWFILE), TAG_DONE);
-  SystemTags("systemtagsslave", SYS_Asynch, TRUE, SYS_Input, DupLock(Input()),
-	     SYS_Output, DupLock(Output()), TAG_DONE);
+  SystemTags("systemtagsslave", SYS_Asynch, TRUE, SYS_Input, SYS_DupStream,
+	     SYS_Output, SYS_DupStream, TAG_DONE);
 }
