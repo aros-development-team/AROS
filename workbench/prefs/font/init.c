ef66916efdfcd76ba634c3acbcbec5540fdbd423
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-09-07 09:16:19 +0000

    Let the autoinit library open the libraries for us instead of doing it manually (reduces source code size a little :o)).
    git-svn-id: https://svn.aros.org/svn/aros/trunk@15332 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/prefs/font/init.c
+++ workbench/prefs/font/init.c
@@ -14,40 +14,6 @@ struct AppGUIData *appGUIData;
 struct FontPrefs *fontPrefs[3];
 extern IPTR argArray[NUM_ARGS];
 
-// Our local library bases. They don't need to be set to NULL. David Gerber has pointed out that
-// ANSI-C requires automated NULL initialization when nothing else is specified. However, if the
-// library pointers are declared as "extern" it seems that some AROS link module will initialize
-// them automatically which is a bad thing as our Open/CloseLibrary() pair will override whatever
-// data already present in the library pointers.
-
-struct IntuitionBase *IntuitionBase = NULL;
-struct GfxBase *GfxBase = NULL;
-struct Library *GadToolsBase = NULL;
-struct LocaleBase *LocaleBase = NULL;
-struct Library *IFFParseBase = NULL;
-struct Library *AslBase = NULL;
-struct Library *DiskFontBase = NULL;
-
-struct libInfo
-{
- APTR		lT_Library;
- STRPTR		lT_Name;
- ULONG		lT_Version;
-};
-
-// For now, library version 37 are required. It should be sufficient, but it hasn't been
-// more thoroughly checked!
-struct libInfo libTable[] =
-{
- { &IntuitionBase,	"intuition.library",	37L},
- { &GadToolsBase,	"gadtools.library",	37L},
- { &GfxBase,		"graphics.library",	37L},
- { &IFFParseBase,	"iffparse.library",	37L},
- { &AslBase,		"asl.library",		37L},
- { &DiskFontBase,	"diskfont.library",	37L},
- { NULL }
-};
-
 void initDefaultPrefs(struct FontPrefs **fontPrefsPtr)
 {
     UBYTE a;
@@ -117,26 +83,3 @@ struct AppGUIData *initAppGUIDataMem(struct Catalog *catalogPtr)
     return(appGUIData);
 }
 
-BOOL initLibs(void)
-{
-    struct libInfo *tmpLibInfo = libTable;
-    UBYTE tmpString[128]; // What if library name plus error message exceeds 128 bytes?
-
-    kprintf("Opening libs...\n");
-
-    while(tmpLibInfo->lT_Library)
-    {
-	if(!((*(struct Library **)tmpLibInfo->lT_Library = OpenLibrary(tmpLibInfo->lT_Name, tmpLibInfo->lT_Version))))
-	{
-	    sprintf(tmpString, getCatalog(catalogPtr, MSG_CANT_OPEN_LIB), tmpLibInfo->lT_Name,tmpLibInfo->lT_Version);
-	    return(FALSE);
-	}
-
-    else
-	kprintf("Opened %s!\n", tmpLibInfo->lT_Name);
-
-	tmpLibInfo++;
-    }
-
-    return(TRUE);
-}
