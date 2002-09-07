3d8c328b994394fdf73c63c801c082b07b12a351
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-09-07 09:22:49 +0000

    Some formatting changes.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@15333 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/prefs/font/main.c
+++ workbench/prefs/font/main.c
@@ -22,11 +22,11 @@ extern struct FontPrefs *fontPrefs[3];	// init.c
 STRPTR getCatalog(struct Catalog *catalogPtr, ULONG id)
 {
     STRPTR string;
-    
+
     if(catalogPtr)
-	string = GetCatalogStr(catalogPtr, id, CatCompArray[id].cca_Str);
+        string = GetCatalogStr(catalogPtr, id, CatCompArray[id].cca_Str);
     else
-	string = CatCompArray[id].cca_Str;
+        string = CatCompArray[id].cca_Str;
 
     return(string);
 }
@@ -34,7 +34,7 @@ STRPTR getCatalog(struct Catalog *catalogPtr, ULONG id)
 STRPTR formatErrorMsg(STRPTR message)
 {
     static UBYTE bufferMem[128];	// We better not override the current space
-    
+
     sprintf(bufferMem, getCatalog(catalogPtr, MSG_CANT_ALLOCATE_MEM), message);
 
     return(bufferMem);
@@ -52,53 +52,51 @@ void quitApp(STRPTR errorMsg, UBYTE errorCode)
 
     extern struct RDArgs *readArgs;
     if(errorMsg)
-	displayError(errorMsg);
+        displayError(errorMsg);
 
-   	CloseCatalog(catalogPtr); // Passing NULL is valid!
+    CloseCatalog(catalogPtr); // Passing NULL is valid!
 
     if(appGUIData)
     {
-	if(appGUIData->agd_VisualInfo)
-	    FreeVisualInfo(appGUIData->agd_VisualInfo);
-	    
-	if(appGUIData->agd_Screen)
-	{
-	    // We don't have to check for agd_DrawInfo - passing NULL is valid!
-	    FreeScreenDrawInfo(appGUIData->agd_Screen, appGUIData->agd_DrawInfo);
-		
-	    //UnlockPubScreen(NULL, appGUIData->agd_Screen); // CloseWindow should do this for us (petah)
-	}
+        if(appGUIData->agd_VisualInfo)
+            FreeVisualInfo(appGUIData->agd_VisualInfo);
 
-	if(appGUIData->agd_GadgetList)
-	    FreeGadgets(appGUIData->agd_GadgetList);
+        if(appGUIData->agd_Screen)
+        {
+            // We don't have to check for agd_DrawInfo - passing NULL is valid!
+            FreeScreenDrawInfo(appGUIData->agd_Screen, appGUIData->agd_DrawInfo);
+        }
 
-	if(appGUIData->agd_Menu)
-	{  
-	    ClearMenuStrip(appGUIData->agd_Window); // Really necessary?
-	    FreeMenus(appGUIData->agd_Menu);
-	}
+        if(appGUIData->agd_GadgetList)
+            FreeGadgets(appGUIData->agd_GadgetList);
 
-	freeRegisterTabs();
+        if(appGUIData->agd_Menu)
+        {
+            ClearMenuStrip(appGUIData->agd_Window); // Really necessary?
+            FreeMenus(appGUIData->agd_Menu);
+        }
 
-	if(appGUIData->agd_Window)
-	    CloseWindow(appGUIData->agd_Window);
+        freeRegisterTabs();
 
-	FreeMem(appGUIData, sizeof(struct AppGUIData));
+        if(appGUIData->agd_Window)
+            CloseWindow(appGUIData->agd_Window);
+
+        FreeMem(appGUIData, sizeof(struct AppGUIData));
     }
 
     // We don't have to check for a TRUE value. However, there is no reason
     // why not to - it might turn out useful in the future.
     if(readArgs)
-	FreeArgs(readArgs);
+        FreeArgs(readArgs);
 
     if(iffHandle)
-	FreeIFF(iffHandle);
+        FreeIFF(iffHandle);
     else
-	kprintf("No iffHandle?!\n");
+        kprintf("No iffHandle?!\n");
 
     for(a = 0; a <= 2; a++)
-	if(fontPrefs[a])
-	    FreeMem(fontPrefs[a], sizeof(struct FontPrefs));
+        if(fontPrefs[a])
+            FreeMem(fontPrefs[a], sizeof(struct FontPrefs));
 
     exit(errorCode);
 }
@@ -110,50 +108,56 @@ int main(void)
     // NULL is not runtime critical but should be dealt with in a smarter fashion!
     // The default language (= built-in) language is english and doesn't need to be set (Introduced 0.13)
     if(!(catalogPtr = OpenCatalog(NULL, "Sys/fontprefs.catalog", OC_Version, REQ_CAT_VERSION, TAG_DONE)))
-	PrintFault(IoErr(), NULL); // If invalid catalog --> "unknown error"?
+        PrintFault(IoErr(), NULL); // If invalid catalog --> "unknown error"?
 
     if(!(initPrefMem()))
-	quitApp(formatErrorMsg("fontPrefs"), RETURN_FAIL);
+        quitApp(formatErrorMsg("fontPrefs"), RETURN_FAIL);
 
     printf("Welcome to Font Preferences for AROS!\nThis is alpha class software: read CAVEATS before you continue!\n");
 
     // Check for shell (Desktop?) arguments
     switch(processArguments())
     {
-	case APP_STOP:
-	    quitApp(NULL, RETURN_OK);
-	break;
+        case APP_STOP:
+            quitApp(NULL, RETURN_OK);
+            break;
 
-	case APP_FAIL:
-	    quitApp(NULL, RETURN_FAIL);
-	break;
+        case APP_FAIL:
+            quitApp(NULL, RETURN_FAIL);
+            break;
     }
 
     if((appGUIData = initAppGUIDataMem(catalogPtr)))
     {
-	if(createRegisterTabs())
-	{
+        if(createRegisterTabs())
+        {
 
-	    if((appGUIData = createGadgets()))
-	    {
-		// Open the application window and start the main I/O event loop!
+            if((appGUIData = createGadgets()))
+            {
+                // Open the application window and start the main I/O event loop!
 
-		if((appGUIData = openAppWindow()))
-		    quitApp(NULL, inputLoop());
-		    
-		else
-		    quitApp(getCatalog(catalogPtr, MSG_CANT_CREATE_WIN), RETURN_FAIL); // Don't call quitApp(), change this!
-	    }
-	    else
-		quitApp(getCatalog(catalogPtr, MSG_CANT_CREATE_GADGET), RETURN_FAIL);
-	}
-	else
-	    quitApp(formatErrorMsg("Registertabs"), RETURN_FAIL);
+                if((appGUIData = openAppWindow()))
+                    quitApp(NULL, inputLoop());
+                else
+                    quitApp(getCatalog(catalogPtr, MSG_CANT_CREATE_WIN), RETURN_FAIL); 
+                    // Don't call quitApp(), change this!
+            }
+            else
+            {
+                quitApp(getCatalog(catalogPtr, MSG_CANT_CREATE_GADGET), RETURN_FAIL);
+            }
+        }
+        else
+        {
+            quitApp(formatErrorMsg("Registertabs"), RETURN_FAIL);
+        }
     }
     else
-	quitApp(NULL, RETURN_FAIL);
+    {
+        quitApp(NULL, RETURN_FAIL);
+    }
 
     quitApp(NULL, RETURN_OK);
-    
-    exit(0); // Suppresses GCC warning
+
+    return 0; // Suppresses GCC warning
 }
