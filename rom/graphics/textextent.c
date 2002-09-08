c83d2b1603f6d7038296dcae75f66741fe65ec93
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-09-08 16:05:44 +0000

    calc. based on TextLength() are too primitive and don't really work because of kerning/spacing tables which can contain negative values, etc. Another problem was that it did not take bold/italic algo style into account.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@15337 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/graphics/textextent.c
+++ rom/graphics/textextent.c
@@ -51,13 +51,85 @@
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct GfxBase *,GfxBase)
 
+    struct TextFont *tf = rp->Font;
+    
     textExtent->te_Width = TextLength(rp, string, count);
-    textExtent->te_Height = rp->Font->tf_YSize;
-    textExtent->te_Extent.MinX = 0;
-    textExtent->te_Extent.MinY = -rp->Font->tf_Baseline;
-    textExtent->te_Extent.MaxX = textExtent->te_Width - 1;
-    textExtent->te_Extent.MaxY = textExtent->te_Height - 1 - rp->Font->tf_Baseline;
+    textExtent->te_Height = tf->tf_YSize;
+    textExtent->te_Extent.MinY = -tf->tf_Baseline;
+    textExtent->te_Extent.MaxY = textExtent->te_Height - 1 - tf->tf_Baseline;
 
+    /* MinX/MaxX can be a bit more complicated if there are kerning/space tables */
+    
+    if ((tf->tf_Flags & FPF_PROPORTIONAL) || tf->tf_CharKern || tf->tf_CharSpace)
+    {
+    	WORD  idx;
+	WORD  defaultidx = NUMCHARS(tf) - 1; /* Last glyph is the default glyph */
+    	WORD  x, x2;
+	UBYTE c;
+	
+    	textExtent->te_Extent.MinX = 0;
+    	textExtent->te_Extent.MaxX = 0;
+	x = 0;
+	
+	while(count--)
+	{
+	    c = *string++;
+	    
+	    if ( c < tf->tf_LoChar || c > tf->tf_HiChar)
+	    {
+		idx = defaultidx;
+	    }
+	    else
+	    {
+		idx = c - tf->tf_LoChar;
+	    }
+	    
+	    #define CHECK_MINMAX(x) \
+	    	if ((x) < textExtent->te_Extent.MinX) textExtent->te_Extent.MinX = (x); \
+		if ((x) > textExtent->te_Extent.MaxX) textExtent->te_Extent.MaxX = (x);
+		
+	    x += ((WORD *)tf->tf_CharKern)[idx];
+	    CHECK_MINMAX(x);
+	    
+	    x2 = x + ( ( ((ULONG *)tf->tf_CharLoc)[idx] ) & 0xFFFF);
+	    CHECK_MINMAX(x2);
+	    
+	    x += ((WORD *)tf->tf_CharSpace)[idx];
+	    CHECK_MINMAX(x);
+	    
+	    x += rp->TxSpacing;
+	    CHECK_MINMAX(x);
+	    	    	    
+	}
+    }
+    else
+    {
+    	/* Normal non-proportional Font */
+    	textExtent->te_Extent.MinX = 0;
+    	textExtent->te_Extent.MaxX = textExtent->te_Width - 1;
+    }
+    
+    if (rp->AlgoStyle & FSF_BOLD)
+    {
+    	textExtent->te_Extent.MaxX += tf->tf_BoldSmear;
+    }
+    
+    if (rp->AlgoStyle & FSF_ITALIC)
+    {
+    	/*  ######            ######
+	**  ##  ##            ##  ##
+	**  ##  ##           ##  ##
+	**  ##  ##  ===>     ##  ##
+	**  ##  ##	    ##  ##
+	**..##..##..      ..##..##..
+	**  ##  ##         ##  ##
+	**  ######         ######
+	*/
+	
+	textExtent->te_Extent.MaxX += tf->tf_Baseline / 2;
+	textExtent->te_Extent.MinX -= (tf->tf_YSize - tf->tf_Baseline) / 2;
+    }
+    
     AROS_LIBFUNC_EXIT
     
 } /* TextExtent */
