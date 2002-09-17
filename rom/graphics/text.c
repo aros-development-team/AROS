03f32caec8ef3dc5dc7962969f122da163a87e47
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-09-17 17:10:13 +0000

    implemented italic/bold/underlined inversvid rendering through a blttemplate based thingy.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@15375 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/graphics/text.c
+++ rom/graphics/text.c
@@ -7,6 +7,10 @@
 */
 #include "graphics_intern.h"
 #include <graphics/rastport.h>
+#include <string.h>
+
+void BltTemplateBasedText(struct RastPort *rp, STRPTR text, ULONG len,
+    	    	    	  struct GfxBase *GfxBase);
 
 /*****************************************************************************
 
@@ -49,7 +53,236 @@
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct GfxBase *,GfxBase)
 
-    if (count) driver_Text (rp, string, count, GfxBase);
+    if (count)
+    {
+    	if ((rp->DrawMode & INVERSVID) ||
+	    (rp->AlgoStyle & (FSF_BOLD | FSF_ITALIC | FSF_UNDERLINED)))
+	{
+	    BltTemplateBasedText(rp, string, count, GfxBase);
+	}
+	else
+	{
+    	    driver_Text (rp, string, count, GfxBase);
+	}
+    }
 
     AROS_LIBFUNC_EXIT
+    
 } /* Text */
+
+/***************************************************************************/
+
+void BltTemplateBasedText(struct RastPort *rp, STRPTR text, ULONG len,
+    	    	    	  struct GfxBase *GfxBase)
+{
+    struct TextExtent 	 te;
+    struct TextFont 	*tf;
+    WORD    	    	 raswidth, raswidth16, raswidth_bpr, rasheight, x, y, gx;
+    UBYTE   	    	*raster;
+    BOOL    	    	 is_bold, is_italic;
+    
+    TextExtent(rp, text, len, &te);
+    
+    raswidth  = te.te_Extent.MaxX - te.te_Extent.MinX + 1;
+    rasheight = te.te_Extent.MaxY - te.te_Extent.MinY + 1;
+    
+    raswidth16 = (raswidth + 15) & ~15;
+    raswidth_bpr = raswidth16 / 8;
+    
+    if ((raster = AllocRaster(raswidth, rasheight)))
+    {
+    	memset(raster, 0, RASSIZE(raswidth, rasheight));
+	
+	tf = rp->Font;
+	
+	x = -te.te_Extent.MinX;
+	
+	is_bold   = (rp->AlgoStyle & FSF_BOLD) != 0;
+	is_italic = (rp->AlgoStyle & FSF_ITALIC) != 0;
+	
+	while(len--)
+	{
+	    UBYTE c = *text++;
+	    ULONG idx;
+	    ULONG charloc;
+	    UWORD glyphwidth, glyphpos, bold;
+	    UBYTE *glyphdata;
+	    UBYTE *dst;
+	    ULONG srcmask;
+	    ULONG dstmask;
+	    
+	    if (c < tf->tf_LoChar || c > tf->tf_HiChar)
+	    {
+	    	idx = tf->tf_HiChar - tf->tf_LoChar;
+	    }
+	    else
+	    {
+	    	idx = c - tf->tf_LoChar;
+	    }
+
+	    charloc = ((ULONG *)tf->tf_CharLoc)[idx];
+
+    	    glyphwidth = charloc & 0xFFFF;
+	    glyphpos = charloc >> 16;
+	    
+    	    if (tf->tf_CharKern)
+	    {
+	    	x += ((WORD *)tf->tf_CharKern)[idx];
+	    }
+	    	
+	       
+	    for(bold = 0; bold <= is_bold; bold++)
+	    {
+	    	WORD wx;
+		WORD italicshift, italiccheck;
+		
+		if (is_italic)
+		{
+	    	    italiccheck = tf->tf_Baseline;
+	    	    italicshift = italiccheck / 2;		
+		}
+		else
+		{
+	    	    italicshift = 0;
+		}
+		
+		wx = x + italicshift + (bold ? tf->tf_BoldSmear : 0);
+		
+		glyphdata = ((UBYTE *)tf->tf_CharData) + glyphpos / 8;
+		dst = raster + wx / 8;
+
+		for(y = 0; y < rasheight; y++)
+		{
+	    	    UBYTE *glyphdatax = glyphdata;
+		    UBYTE *dstx = dst;
+		    UBYTE srcdata;
+
+	    	    srcmask = 0x80 >> (glyphpos & 7);
+	    	    dstmask = 0x80 >> (wx & 7);
+
+    	    	    srcdata = *glyphdatax;
+
+	    	    for(gx = 0; gx < glyphwidth; gx++)
+		    {
+			if (srcdata & srcmask)
+			{
+		    	    *dstx |= dstmask;
+			}
+
+			if (dstmask == 0x1)
+			{
+		    	    dstmask = 0x80;
+			    dstx++;
+			}
+			else
+			{
+		    	    dstmask >>= 1;
+			}
+
+			if (srcmask == 0x1)
+			{
+		    	    srcmask = 0x80;
+			    glyphdatax++;
+			    srcdata =*glyphdatax;
+			}
+			else
+			{
+		    	    srcmask >>= 1;
+			}
+			
+		    } /* for(gx = 0; gx < glyphwidth; gx++) */
+
+		    glyphdata += tf->tf_Modulo;
+		    dst += raswidth_bpr;
+		    
+		    if (is_italic)
+		    {
+		    	italiccheck--;
+			if (italiccheck & 1)
+			{
+			    italicshift--;
+			    
+			    wx--;
+			    if ((wx & 7) == 7) dst--;
+			    
+			}
+		    }
+		    
+		} /* for(y = 0; y < rasheight; y++) */
+		
+	    } /* for(bold = 0; bold < ((rp->AlgoStyle & FSF_BOLD) ? 2 : 1); bold++) */
+	    
+	    if (tf->tf_CharSpace)
+	    {
+	    	x += ((WORD *)tf->tf_CharSpace)[idx];
+	    }
+	    else
+	    {
+	    	x += tf->tf_XSize;
+	    }
+	    
+	    x += rp->TxSpacing;
+	    
+	} /* while(len--) */
+	
+	if (rp->AlgoStyle & FSF_UNDERLINED)
+	{
+	    UBYTE *dst;
+	    ULONG prev_word, act_word = 0, next_word, word;
+	    WORD count;
+	    LONG underline;
+	    
+	    underline = rp->TxBaseline + 1;
+	    if (underline < rasheight - 1) underline++;
+	    
+	    if (underline < rasheight)
+	    {
+		dst = raster + underline * (LONG)raswidth_bpr;
+		next_word = *(UWORD *)dst;
+
+		count  = raswidth16 / 16;
+
+		while(count--)
+		{
+	    	    prev_word = act_word;
+		    act_word = next_word;
+		    if (count > 1)
+		    {
+			next_word = ((UWORD *)dst)[1];
+		    }
+		    else
+		    {
+			next_word = 0;
+		    }
+
+		    word = ((act_word << 1) & 0xFFFF) + (next_word >> 15);
+		    word |= (act_word >> 1) + ((prev_word << 15) & 0xFFFF);
+		    word &= ~act_word;
+
+		    word = 0xFFFF &~ word;
+
+		    *(UWORD *)dst = word;
+		    dst += 2;
+
+		} /* while(count--) */
+		
+	    } /* if (underline < rasheight) */
+	    
+	} /* if (rp->AlgoStyle & FSF_UNDERLINED) */
+	
+	BltTemplate(raster,
+	    	    0,
+		    raswidth_bpr,
+		    rp,
+		    rp->cp_x + te.te_Extent.MinX,
+		    rp->cp_y - rp->TxBaseline,
+		    raswidth,
+		    rasheight);
+		    	
+    	FreeRaster(raster, raswidth, rasheight);
+	
+    } /* if ((raster = AllocRaster(raswidth, rasheight))) */
+    
+    Move(rp, rp->cp_x + te.te_Width, rp->cp_y);
+    
+}
