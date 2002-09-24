edfeb90720d1737b93d2c2352a6a8da75a633680
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-09-24 19:27:03 +0000

    implemented ActivateAslRequest (V44), but not tested.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@15459 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/asl/activateaslrequest.c
@@ -0,0 +1,69 @@
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc:
+    Lang: english
+*/
+
+
+#include <proto/exec.h>
+#include "asl_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+
+	AROS_LH1(void, ActivateAslRequest,
+
+/*  SYNOPSIS */
+	AROS_LHA(APTR, requester, A0),
+
+/*  LOCATION */
+	struct Library *, AslBase, 14, Asl)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+	27-11-96    digulla automatically created from
+			    asl_lib.fd and clib/asl_protos.h
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+    AROS_LIBBASE_EXT_DECL(struct Library *,AslBase)
+
+    struct ReqNode *reqnode;
+    
+    ObtainSemaphore( &(ASLB(AslBase)->ReqListSem));
+    
+    reqnode = FindReqNode(requester, ASLB(AslBase));
+    if (reqnode)
+    {
+    	if (reqnode->rn_ReqWindow)
+	{
+    	    ActivateWindow(reqnode->rn_ReqWindow);
+	    
+	} /* if (reqnode->rn_ReqWindow) */
+	
+    } /* if (reqnode) */
+    
+    ReleaseSemaphore(&(ASLB(AslBase)->ReqListSem));
+    
+    AROS_LIBFUNC_EXIT
+    
+} /* ActivateAslRequest */
