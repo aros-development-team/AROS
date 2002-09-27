71508d2a0e171bdb626d8d5b974456c3af5f106a
holmen <holmen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-09-27 16:57:59 +0000

    A basic BMP picture datatype. It doesn't work yet, committed for collective debugging ;)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@15491 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/classes/datatypes/bmp/libdefs.h
@@ -0,0 +1,17 @@
+#ifndef _LIBDEFS_H
+#define _LIBDEFS_H
+#define NAME_STRING      "bmp.datatype"
+#define NT_TYPE          NT_LIBRARY
+#define LC_BUILDNAME(n)  n
+#define LIBBASE          BMPBase
+#define LIBBASETYPE      struct Library
+#define LIBBASETYPEPTR   struct Library *
+#define VERSION_NUMBER   41
+#define REVISION_NUMBER  0
+#define BASENAME         BMP
+#define BASENAME_STRING  "BMP"
+#define VERSION_STRING   "$VER: bmp 41.0 (24.09.2002)\r\n"
+#define LIBEND           BMP_end
+#define LIBFUNCTABLE     BMP_functable
+#define COPYRIGHT_STRING ""
+#endif /* _LIBDEFS_H */
