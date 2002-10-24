845f5a0e820a29e7f469bb9e6d3f23c7639197aa
falemagn <falemagn@gmail.com>
2002-10-24 14:45:12 +0000

    alloca() now cheks whether there's enough space on the stack, before calling __builtin_alloca(). The calculation of the left space is just an approximate guess, and even if the test succeeds, there's no guarantee that the app won't crash, since some variables might be put on the stack _after_ the alloca() call. The only clean solution for this stack thingy is having dynamically expandable stacks, but for that to work really well, we need MMU support. One day, maybe...
    git-svn-id: https://svn.aros.org/svn/aros/trunk@15599 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/include/alloca.h
+++ compiler/clib/include/alloca.h
@@ -6,23 +6,40 @@
 */
 
 #ifndef	_ALLOCA_H
-#define	_ALLOCA_H	
+#define	_ALLOCA_H
 
+#include <proto/exec.h>
 #include <sys/types.h>
 
-__BEGIN_DECLS
 
 /* Discard any previous definition.  */
 #undef alloca
 
+__BEGIN_DECLS
+
 /* Allocate a block of memory which will be automatically freed upon function exiting. */
 extern void *alloca(size_t size);
 
+__END_DECLS
+
 /* GNU C provides a builtin alloca function. */
 #ifdef __GNUC__
-#    define alloca(size) __builtin_alloca(size)
+#    if AROS_STACK_GROWS_DOWNWARDS
+#        define alloca(size)                                                            \
+         ({                                                                             \
+             ((void *)(AROS_GET_SP - AROS_ALIGN(size)) <= FindTask(NULL)->tc_SPLower) ? \
+             NULL :                                                                     \
+             __builtin_alloca(size);                                                    \
+         })
+#    else
+#        define alloca(size)                                                            \
+         ({                                                                             \
+             ((void *)(AROS_GET_SP + AROS_ALIGN(size)) >= FindTask(NULL)->tc_SPUpper) ? \
+             NULL :                                                                     \
+             __builtin_alloca(size);                                                    \
+         })
+#    endif /* AROS_STACK_GROWS_DOWNWARDS */
 #endif /* GCC.  */
 
-__END_DECLS
 
 #endif /* alloca.h */
