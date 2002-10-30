477138d9ae45729beb47f5d606d45f16b5fdc053
psmith <psmith@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-10-30 14:16:21 +0000

    Library include file for desktop.library
    git-svn-id: https://svn.aros.org/svn/aros/trunk@15632 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/libraries/desktop.h
@@ -0,0 +1,21 @@
+#ifndef LIBRARIES_DESKTOP_H
+#define LIBRARIES_DESKTOP_H
+
+/*** Kinds for use with CreateDesktopObjectA() ****************/
+
+#define CDO_Window                        1
+#define CDO_IconContainer                 2
+#define CDO_DiskIcon                      3
+#define CDO_DrawerIcon                    4
+#define CDO_ToolIcon                      5
+#define CDO_ProjectIcon                   6
+#define CDO_TrashcanIcon                  7
+#define CDO_Desktop                       8
+
+/*** Tags for use with CreateDesktopObjectA() *****************/
+
+#define ICOA_Directory            TAG_USER+1
+
+
+#endif /* LIBRARIES_DESKTOP_H */
+
