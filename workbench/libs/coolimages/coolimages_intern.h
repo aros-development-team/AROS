a31ba0dbd3fc7c7ed029af0d4dfc43ee8c4bcd3e
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-10-30 17:36:30 +0000

    shared coolimages.library based on coolimages linklib.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@15644 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/coolimages/coolimages_intern.h
@@ -0,0 +1,64 @@
+/*
+    Copyright © 2002, The AROS Development Team. 
+    All rights reserved.
+    
+    $Id$
+*/
+
+#ifndef COOLIMAGES_INTERN_H
+#define COOLIMAGES_INTERN_H
+
+#ifndef EXEC_TYPES_H
+#   include <exec/types.h>
+#endif
+#ifndef EXEC_LIBRARIES_H
+#   include <exec/libraries.h>
+#endif
+#ifndef EXEC_MEMORY_H
+#   include <exec/memory.h>
+#endif
+#ifndef INTUITION_CLASSES_H
+#   include <intuition/classes.h>
+#endif
+#ifndef INTUITION_INTUITIONBASE_H
+#   include <intuition/intuitionbase.h>
+#endif
+#ifndef GRAPHICS_GFXBASE_H
+#   include <graphics/gfxbase.h>
+#endif
+#ifndef CYBERGRAPHX_CYBERGRAPHICS_H
+#   include <cybergraphx/cybergraphics.h>
+#endif
+#ifndef LINKLIBS_COOLIMAGES_H
+#   include <linklibs/coolimages.h>
+#endif
+#ifndef LIBRARIES_COOLIMAGES_H
+#   include <libraries/coolimages.h>
+#endif
+#ifndef UTILITY_UTILITY_H
+#   include <utility/utility.h>
+#endif
+
+
+
+/****************************************************************************************/
+
+struct CoolImagesBase_intern
+{
+    struct Library		library;
+    struct ExecBase		*sysbase;
+    BPTR			seglist;
+};
+
+/****************************************************************************************/
+
+#undef CIB
+#define CIB(b)	((struct CoolImagesBase_intern *)(b))
+
+#undef SysBase
+#define SysBase     	(CIB(CoolImagesBase)->sysbase)
+
+
+/****************************************************************************************/
+
+#endif /* COOLIMAGES_INTERN_H */
