f5ffda90237b17d3024377fc589b28672e2d3e0b
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-11-06 12:22:17 +0000

    The bench now can stop itself when it realizes that there's no need to continue
    git-svn-id: https://svn.aros.org/svn/aros/trunk@15684 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/taskswitchbench2.c
+++ test/taskswitchbench2.c
@@ -74,37 +74,69 @@ void Task2Entry()
 
 int __nocommandline = 1;
 
+/* 
+   define this to non-zero if you want the benchmark to end automatically
+   when it realizes that there's no need to continue.
+   
+   Beware that it can introduce some overhead (although very little).
+*/
+#define SELF_TIMED_TEST 1
+
 int main(void)
 {
     double elapsed = 0;
 
     /*
-       Since I'm SO lazy, I'm using processes here, even if tasks would work too, 'cause 
+       Since I'm SO lazy, I'm using processes here, even if tasks would work too, 'cause
        it's easier to create processes than tasks...
 
-       Ok ok... that's a lie, I could use CreateTask() from libamiga, but it seems to be 
+       Ok ok... that's a lie, I could use CreateTask() from libamiga, but it seems to be
        buggy under AROS?
-    */   
+    */
     task1 = (struct Task *)CreateNewProcTags(NP_Entry, (STACKIPTR)Task1Entry, TAG_DONE);
     task2 = (struct Task *)CreateNewProcTags(NP_Entry, (STACKIPTR)Task2Entry, TAG_DONE);
 
     printf
     (
         "The test is starting.\n"
+        #if !SELF_TIMED_TEST
         "Press CTRL-C to stop the test and get the results.\n\n"
+        #endif
     );
 
     gettimeofday(&start_tv, NULL);
 
     Signal(task2, SIGF_START);
+
+    #if SELF_TIMED_TEST
+    unsigned long i;
+    double oldratio = 0;
+
+    for (i=4;;i+=2)
+    {
+        double ratio;
+        unsigned long int counter2 = counter;
+
+        Delay(200);
+
+        ratio = (double)i/counter2;
+        if ((ratio - oldratio) <= .000000001) break;
+
+        oldratio = ratio;
+    }
+
+    #else
+
     Wait(SIGF_STOP);
+
+    #endif
 
     gettimeofday(&end_tv, NULL);
 
     Signal(task1, SIGF_STOP);
     Signal(task2, SIGF_STOP);
 
-    elapsed = ((double)(((end_tv.tv_sec * 1000000) + end_tv.tv_usec) - ((start_tv.tv_sec * 1000000) + start_tv.tv_usec)))/1000000.;
+    elapsed =  ((double)(((end_tv.tv_sec * 1000000) + end_tv.tv_usec) - ((start_tv.tv_sec * 1000000) + start_tv.tv_usec)))/1000000.;
 
     printf
     (
