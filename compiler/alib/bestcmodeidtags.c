21636c9870385e6913ca1c512ac39e699c1feb9e
ggreco <ggreco@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-11-11 17:37:27 +0000

    Added to amiga.lib the implementation of cybergraphics.library BestCModeIDTags() to use with SDL and others applications.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@15713 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/alib/bestcmodeidtags.c
@@ -0,0 +1,58 @@
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: Varargs version of AllocAslRequestA()
+    Lang: english
+*/
+#define AROS_TAGRETURNTYPE ULONG
+#include <utility/tagitem.h>
+
+
+/*****************************************************************************
+
+    NAME */
+// #include <libraries/cybergraphics.h>
+#include <proto/cybergraphics.h>
+extern struct Library *CyberGfxBase;
+#undef BestCModeIDTags /* Get rid of the macro from inline/ */
+
+	ULONG BestCModeIDTags (
+
+/*  SYNOPSIS */
+	Tag     tag1,
+	...)
+
+/*  FUNCTION
+	This is the varargs version of the cybergraphics.library call
+    BestCModeIDTagList().
+	For more information see the documentation of 
+    cybergraphics.library/BestCModeIDTagList().
+
+    INPUTS
+	tag1        -   TagList of display requirements.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+	11-11-2002  Gabriele Greco  Wrote.
+
+*****************************************************************************/
+{
+    AROS_LIBBASE_EXT_DECL(struct Library *,CyberGfxBase)
+
+    AROS_SLOWSTACKTAGS_PRE(tag1)
+
+    retval = BestCModeIDTagList(AROS_SLOWSTACKTAGS_ARG(tag1));
+
+    AROS_SLOWSTACKTAGS_POST
+
+} /* BestCModeIDTags */
