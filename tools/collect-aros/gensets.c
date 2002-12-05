b981c999041d22acc1d2f1a11e0e2f7937b5cd00
falemagn <falemagn@gmail.com>
2002-12-05 23:44:13 +0000

    Argh! I did something terribly wrong... let's revert back to the old version until I find the right way to do what I did in the previous release
    git-svn-id: https://svn.aros.org/svn/aros/trunk@15953 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- tools/collect-aros/gensets.c
+++ tools/collect-aros/gensets.c
@@ -8,6 +8,80 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+typedef struct node
+{
+    char *secname;
+    int   off_setname;
+    struct node *next;
+} node;
+
+
+
+node *new_node(char *name, int off){
+   node *n;
+
+   if (!(n          = calloc(1, sizeof(node))) ||
+       !(n->secname = strdup(name))
+      )
+   {
+	perror("Internal Error - gensets");
+	exit(1);
+   }
+
+   n->off_setname = off;
+
+   return n;
+}
+
+node *get_node(node **list, char *name, int off)
+{
+    node **curr = list;
+
+    while (*curr)
+    {
+	if (strcmp((*curr)->secname, name) == 0)
+	    return *curr;
+
+	curr = &((*curr)->next);
+    }
+
+    return (*curr = new_node(name, off));
+}
+
+int emit_sets(node *setlist, FILE *out)
+{
+    char setname_big[201];
+    int i;
+
+    while (setlist)
+    {
+        i = 0;
+
+        do
+        {
+            setname_big[i] = toupper(setlist->secname[setlist->off_setname + i]);
+        } while (setlist->secname[setlist->off_setname + i++]);
+
+        fprintf
+        (
+            out,
+            "    __%s_LIST__ = .;\n"
+            "    LONG((__%s_END__ - __%s_LIST__) / %d - 2)\n"
+            "    KEEP(*(SORT(%s.*)))\n"
+            "    KEEP(*(%s))\n"
+            "    LONG(0)\n"
+            "    __%s_END__ = .;\n",
+	    setname_big, setname_big, setname_big, sizeof(long),
+            setlist->secname, setlist->secname, setname_big
+        );
+
+        setlist = setlist->next;
+    }
+
+    return 1;
+}
+
+
 /*
     This routine is slow, but does the work and it's the simplest to write down.
     All this will get integrated into the linker anyway, so there's no point
@@ -16,82 +90,31 @@
 
 int gensets(FILE *in, FILE *out)
 {
-    char secbuf[201];
-    char setname_big[201];
-    char *secname = secbuf;
-    char *setname;
-    int  i;
-    int  have_ctors = 0;
-    int  have_dtors = 0;
+    char sec[201];
+    node *setlist = NULL;
 
-    while (fscanf(in, " %200s ", secname)>0)
+    while (fscanf(in, " %200s ", sec)>0)
     {
         char *idx;
-	int   i, have_set = 0;
-        int   is_ctors = 0, is_dtors = 0;
+	int   off;
 
-        if (strncmp(secname, ".aros.set.", 10) == 0)
-        {
-            have_set  = 1;
-            secname  += 9;
-        }
-
-        if (strncmp(secname, ".ctors", 5) == 0)
-	{
-            if (have_ctors)
-                continue;
-
-            have_ctors = is_ctors = 1;
-        }
+	if (strncmp(sec, ".aros.set.", 10) == 0)
+	    off = 10;
+	else
+	if (strncmp(sec, ".ctors", 5) == 0)
+            off = 1;
+	else
+	if (strncmp(sec, ".dtors", 5) == 0)
+            off = 1;
         else
-        if (strncmp(secname, ".dtors", 5) == 0)
-        {
-            if (have_dtors)
-                continue;
+	    continue;
 
-            have_dtors = is_dtors = 1;
-        }
-        else
-        if (!have_set)
-            continue;
-
-        setname = secname + 1;
-
-        idx = strchr(setname, '.');
+        idx = strchr(sec + off, '.');
         if (idx)
             *idx = '\0';
 
-        i = 0;
-        do
-        {
-            setname_big[i] = toupper(setname[i]);
-        } while (setname[i++]);
-
-
-        fprintf
-        (
-            out,
-            "    __%s_LIST__ = .;\n"
-            "    LONG((__%s_END__ - __%s_LIST__) / %d - 2)\n"
-            "%s"
-            "    KEEP(*(SORT(.aros.set.%s.*)))\n"
-            "    KEEP(*(.aros.set.%s))\n"
-            "%s"
-            "    LONG(0)\n"
-            "    __%s_END__ = .;\n",
-	    setname_big,
-            setname_big, setname_big, sizeof(long),
-            is_dtors?
-                "    KEEP(*(SORT(.dtors.*)))\n"
-                "    KEEP(*(.dtors))\n": "",
-            setname,
-            setname,
-            is_ctors?
-                "    KEEP(*(SORT(.ctors.*)))\n"
-                "    KEEP(*(.ctors))\n": "",
-            setname_big
-        );
+	get_node(&setlist, sec, off);
     }
 
-    return 1;
+    return emit_sets(setlist, out);
 }
