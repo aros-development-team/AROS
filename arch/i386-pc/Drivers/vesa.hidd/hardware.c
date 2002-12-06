74582ce3f6839526498d64b4a084adc381a969d2
jogr0326 <jogr0326@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-12-06 09:42:47 +0000

    Right. The big makeover for pc-i386. Added bootloader.resource to handle the information gathered from GRUB. Fixed vesa.hidd to remove silly bug and use the new resource. Removed the need for hidd.prefs in pc-i386, since all needed information will come from GRUB.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@15960 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-pc/Drivers/vesa.hidd/hardware.c
+++ arch/i386-pc/Drivers/vesa.hidd/hardware.c
@@ -10,46 +10,51 @@
 #define DEBUG 1 /* no SysBase */
 #include <aros/debug.h>
 #include <aros/macros.h>
+#include <aros/bootloader.h>
+#include <proto/bootloader.h>
 
 #include "hardware.h"
 #include "vesagfxclass.h"
-#include "multiboot.h"
 
 #undef SysBase
 extern struct ExecBase *SysBase;
 
 BOOL initVesaGfxHW(struct HWData *data)
 {
-    struct arosmb *mb;
-    ULONG masks [] = { 0x01, 0x03, 0x07, 0x0f ,0x1f, 0x3f, 0x7f, 0xff };
+    struct BootLoaderBase *BootLoaderBase;
+    struct VesaInfo *vi;
 
-    mb = (struct arosmb *)0x1000;
+    if (BootLoaderBase = OpenResource("bootloader.resource"))
+    {
+	D(bug("[Vesa] Init: Bootloader.resource opened\n"));
+	if (vi = (struct VesaInfo *)GetBootInfo(BL_Video))
+	{
+	    D(bug("[Vesa] Init: Got Vesa structure from resource\n"));
+	    data->width = vi->XSize; data->height = vi->YSize;
+	    data->bitsperpixel = data->depth = vi->BitsPerPixel;
+	    data->bytesperline = vi->BytesPerLine;
+	    data->redmask = vi->Masks[VI_Red];
+	    data->greenmask = vi->Masks[VI_Green];
+	    data->bluemask = vi->Masks[VI_Blue];
+	    data->redshift = vi->Shifts[VI_Red];
+	    data->greenshift = vi->Shifts[VI_Green];
+	    data->blueshift = vi->Shifts[VI_Blue];
+	    data->framebuffer = vi->FrameBuffer;
+	    data->bytesperpixel = 1;
+	    if (data->depth>16)
+		data->bytesperpixel = 4;
+	    else if (data->depth>8)
+		data->bytesperpixel = 2;
+	    D(bug("[Vesa] HwInit: Linear framebuffer at 0x%08x\n",data->framebuffer));
+	    D(bug("[Vesa] HwInit: Screenmode %dx%dx%d\n",data->width,data->height,data->depth));
+	    D(bug("[Vesa] HwInit: Masks R %08x<<%2d G %08x<<%2d B %08x<<%2d\n",
+			data->redmask, data->redshift,
+			data->greenmask, data->greenshift,
+			data->bluemask, data->blueshift));
+	    return TRUE;
+	}
+    }
 
-    data->width = mb->vmi.x_resolution;
-    data->height = mb->vmi.y_resolution;
-    data->depth = mb->vmi.bits_per_pixel;
-    data->redmask    = masks[mb->vmi.red_mask_size-1]<<mb->vmi.red_field_position;
-    data->greenmask  = masks[mb->vmi.green_mask_size-1]<<mb->vmi.green_field_position;
-    data->bluemask   = masks[mb->vmi.blue_mask_size-1]<<mb->vmi.blue_field_position;
-    data->redshift   = 32 - mb->vmi.red_field_position - mb->vmi.red_mask_size;
-    data->greenshift = 32 - mb->vmi.green_field_position - mb->vmi.green_mask_size;
-    data->blueshift  = 32 - mb->vmi.blue_field_position - mb->vmi.blue_mask_size;
-    data->bytesperpixel = 1;
-    if (data->depth>16)
-	data->bytesperpixel = 4;
-    else if (data->depth>8)
-	data->bytesperpixel = 2;
-    data->bitsperpixel = mb->vmi.bits_per_pixel;
-    data->bytesperline = mb->vmi.bytes_per_scanline;
-    data->fbsize = ((mb->vci.total_memory)<<6)*1024;
-    data->framebuffer = mb->vmi.phys_base;
-
-    D(bug("[Vesa] HwInit: Linear framebuffer at 0x%08x\n",data->framebuffer));
-    D(bug("[Vesa] HwInit: Screenmode %dx%dx%d\n",mb->vmi.x_resolution,mb->vmi.y_resolution,data->depth));
-    D(bug("[Vesa] HwInit: Masks R %08x<<%2d G %08x<<%2d B %08x<<%2d\n",
-		data->redmask, data->redshift,
-		data->greenmask, data->greenshift,
-		data->bluemask, data->blueshift));
-
-    return TRUE;
+    bug("[Vesa] HwInit: No Vesa information from the bootloader. Failing\n");
+    return FALSE;
 }
