47221bc345d698d3d6edab9ef20d57d7cad2473a
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-12-30 13:25:53 +0000

    libinit is library to call functions when opening closing libraries.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@16182 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/libinit/libinit.c
@@ -0,0 +1,40 @@
+/*
+    Copyright © 1995-2002, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: libinit library - functions calling when opening/closing libs
+*/
+
+#include <exec/types.h>
+#include <aros/asmcall.h>
+
+typedef AROS_UFH1(int, (*libfunc),
+		  AROS_UFHA(void*, libbase, A6)
+		 );
+
+AROS_UFH3(int, set_call_libfuncs,
+	  AROS_UFHA(libfunc*, list, A0),
+	  AROS_UFHA(int, order, D0),
+	  AROS_UFHA(void*, libbase, A6)
+	 )
+{
+    int n;
+
+    if (order>=0)
+    {
+	int ret;
+
+	n = 1;
+	while(list[n])
+	    if (!list[n++](libbase)) return FALSE;
+    }
+    else
+    {
+	n = ((int *)list)[0];
+
+	while (n)
+	    (void)list[n--](libbase);
+    }
+
+    return TRUE;
+}
