4d8c0205fdf84137d72dd7c64aaf03748efedda1
psmith <psmith@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-01-08 16:05:46 +0000

    added an operation to the icon menu ('Open') for testing
    git-svn-id: https://svn.aros.org/svn/aros/trunk@16218 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/desktop/loaddesktop.c
+++ test/desktop/loaddesktop.c
@@ -1,4 +1,7 @@
-// blah
+/*
+    Copyright © 1995-2002, The AROS Development Team. All rights reserved.
+    $Id$
+*/
 
 #define DEBUG 1
 #include <aros/debug.h>
@@ -34,16 +37,67 @@ int main(void)
 	BOOL running=TRUE;
 	ULONG signals=0;
 	struct Screen *screen;
-	struct NewMenu menuDat[]=
-	{
-		{NM_TITLE, "AROS", 0,0,0,(APTR)1},
-		{NM_ITEM, "Quit", "Q",0,0,(APTR)2},
-		{NM_END, NULL, 0,0,0,(APTR)0}
-	};
-	struct TagItem icTags[5];
+	struct DesktopOperationItem *doi;
+	LONG i=0, j=0;
+	struct NewMenu *menuDat;
+	struct TagItem icTags[6];
+	ULONG inputResult;
 
 	DesktopBase=OpenLibrary("desktop.library", 0);
 	MUIMasterBase=OpenLibrary("muimaster.library", 0);
+
+	doi=GetMenuItemList(DOC_ICONOP);
+	if(doi)
+	{
+		while(doi[i].doi_Code!=0 && doi[i].doi_Name!=NULL)
+			i++;
+	}
+
+	menuDat=(struct NewMenu*)AllocVec(sizeof(struct NewMenu)*(i+3), MEMF_ANY);
+	menuDat[0].nm_Type=NM_TITLE;
+	menuDat[0].nm_Label="AROS";
+	menuDat[0].nm_CommKey=0;
+	menuDat[0].nm_Flags=0;
+	menuDat[0].nm_MutualExclude=0;
+	menuDat[0].nm_UserData=0;
+	menuDat[1].nm_Type=NM_ITEM;
+	menuDat[1].nm_Label="Quit";
+	menuDat[1].nm_CommKey="Q";
+	menuDat[1].nm_Flags=0;
+	menuDat[1].nm_MutualExclude=0;
+	menuDat[1].nm_UserData=2;
+	j=2;
+
+	if(i>0)
+	{
+		menuDat[2].nm_Type=NM_TITLE;
+		menuDat[2].nm_Label="Icon";
+		menuDat[2].nm_CommKey=0;
+		menuDat[2].nm_Flags=0;
+		menuDat[2].nm_MutualExclude=0;
+		menuDat[2].nm_UserData=DOC_ICONOP;
+		j=3;
+
+		i=0;
+		while(doi[i].doi_Code!=0 && doi[i].doi_Name!=NULL)
+		{
+			menuDat[j].nm_Type=NM_ITEM;
+			menuDat[j].nm_Label=doi[i].doi_Name;
+			menuDat[j].nm_CommKey=0;
+			menuDat[j].nm_Flags=0;
+			menuDat[j].nm_MutualExclude=0;
+			menuDat[j].nm_UserData=doi[i].doi_Code;
+			i++;
+			j++;
+		}
+	}
+
+	menuDat[j].nm_Type=NM_END;
+	menuDat[j].nm_Label=NULL;
+	menuDat[j].nm_CommKey=0;
+	menuDat[j].nm_Flags=0;
+	menuDat[j].nm_MutualExclude=0;
+	menuDat[j].nm_UserData=0;
 
 	screen=LockPubScreen(NULL);
 
@@ -57,16 +111,24 @@ int main(void)
 		MUIA_Prop_UseWinBorder, MUIV_Prop_UseWinBorder_Right,
 		End;
 
-	icTags[0].ti_Tag=MUIA_FillArea;
-	icTags[0].ti_Data=FALSE;
+	icTags[0].ti_Tag=MUIA_InnerLeft;
+	icTags[0].ti_Data=0;
+	icTags[1].ti_Tag=MUIA_InnerTop;
+	icTags[1].ti_Data=0;
+	icTags[2].ti_Tag=MUIA_InnerBottom;
+	icTags[2].ti_Data=0;
+	icTags[3].ti_Tag=MUIA_InnerRight;
+	icTags[3].ti_Data=0;
+	icTags[4].ti_Tag=MUIA_FillArea;
+	icTags[4].ti_Data=FALSE;
 //	icTags[1].ti_Tag=ICOA_Directory;
 //	icTags[1].ti_Data="C:";
 //	icTags[2].ti_Tag=ICA_VertScroller;
 //	icTags[2].ti_Data=vert;
 //	icTags[3].ti_Tag=ICA_HorizScroller;
 //	icTags[3].ti_Data=horiz;
-	icTags[1].ti_Tag=TAG_END;
-	icTags[1].ti_Data=0;
+	icTags[5].ti_Tag=TAG_END;
+	icTags[5].ti_Data=0;
 
 	app=ApplicationObject,
 		SubWindow, win=WindowObject,
@@ -100,7 +162,8 @@ int main(void)
 
 		while(running)
 		{
-			switch(DoMethod(app, MUIM_Application_Input, &signals))
+			inputResult=DoMethod(app, MUIM_Application_Input, &signals);
+			switch(inputResult)
 			{
 				case MUIV_Application_ReturnID_Quit:
 					running=FALSE;
@@ -108,6 +171,30 @@ int main(void)
 				case 2:
 					running=FALSE;
 					break;
+				default:
+				{
+					// a menuitem was selected...
+					struct MinList *subjects=NULL;
+					Object *member, *ostate;
+
+					if(inputResult & DOC_ICONOP)
+					{
+						GetAttr(ICA_SelectedIcons, iconCon, &subjects);
+
+						ostate=subjects->mlh_Head;
+						while(member=NextObject(&ostate))
+						{
+							struct TagItem args[2];
+
+							args[0].ti_Tag=OPA_Target;
+							args[0].ti_Data=member;
+							args[1].ti_Tag=TAG_END;
+							args[1].ti_Data=0;
+
+							DoDesktopOperation(inputResult, args);
+						}
+					}
+				}
 			}
 
 			if(running && signals)
