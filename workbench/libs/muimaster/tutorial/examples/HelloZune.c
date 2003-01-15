ea09f26f1b412ff2fd549e9055b693ae2eef6cb8
indy <indy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-01-15 14:33:49 +0000

    Step by step examples for a mui/zune tutorial
    git-svn-id: https://svn.aros.org/svn/aros/trunk@16254 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/muimaster/tutorial/examples/HelloZune.c
@@ -0,0 +1,118 @@
+/*
+    Copyright © 2003, The AROS Development Team.
+    All rights reserved.
+
+    $Id$
+*/
+
+/* "muizunesupport.h" contains misc includes and 
+   init stuff which is not important at the moment. */
+   
+#include "muizunesupport.h"
+
+
+/* Objects */
+
+Object *app;
+Object *WD_Main;
+Object *BT_Hello;
+
+
+/****************************************************************
+ Allocate resources for gui
+*****************************************************************/
+
+BOOL init_gui(void)
+{
+    app = ApplicationObject,
+    	      MUIA_Application_Title      , (IPTR) "HelloZune",
+	      MUIA_Application_Version    , (IPTR) "$VER: HelloZune 0.1 (14.01.03)",
+	      MUIA_Application_Copyright  , (IPTR) "© 2003, The AROS Development Team", 
+	      MUIA_Application_Author     , (IPTR) "The AROS Development Team",
+	      MUIA_Application_Description, (IPTR) "Show basic structure of a MUI/Zune application",
+	      MUIA_Application_Base       , (IPTR) "HELLOZUNE",
+		
+    	      SubWindow, WD_Main = WindowObject,
+                  MUIA_Window_Title, (IPTR) "Hello Zune",
+
+   		  WindowContents, VGroup,
+     	    	      Child, HGroup,
+		          Child, BT_Hello = SimpleButton("Hello Zune"),
+    	              End,/* HGroup */
+		  End, /* VGroup */
+
+              End, /* WindowObject */
+
+    	  End; /* ApplicationObject */
+
+    if(app)
+    {
+        /* Quit application if the windowclosegadget or the esc key is pressed. */
+	
+	DoMethod(WD_Main, MUIM_Notify, MUIA_Window_CloseRequest, TRUE, 
+	         app, 2, 
+		 MUIM_Application_ReturnID, MUIV_Application_ReturnID_Quit);
+
+	return(TRUE);
+    }
+    
+    return(FALSE);
+} /* init_gui(void) */
+
+
+/****************************************************************
+ Deallocates all gui resources
+*****************************************************************/
+
+void deinit_gui(void)
+{
+    if(app){MUI_DisposeObject(app);}
+} /* deinit_gui(void) */
+
+
+
+/****************************************************************
+ The message loop
+*****************************************************************/
+
+void loop(void)
+{
+    ULONG sigs = 0;
+
+    while((LONG) DoMethod(app, MUIM_Application_NewInput, &sigs) != MUIV_Application_ReturnID_Quit)
+    {
+	if (sigs)
+	{
+	    sigs = Wait(sigs | SIGBREAKF_CTRL_C | SIGBREAKF_CTRL_D);
+	    if(sigs & SIGBREAKF_CTRL_C){break;}
+	    if(sigs & SIGBREAKF_CTRL_D){break;}
+	}
+    }
+} /* loop(void)*/
+
+
+/****************************************************************
+ The main entry point
+*****************************************************************/
+
+int main(int argc, char *argv[])
+{
+    if(open_libs())
+    {
+    	if(init_gui())
+    	{
+      	    set(WD_Main, MUIA_Window_Open, TRUE);
+	    
+    	    if(xget(WD_Main, MUIA_Window_Open))
+	    {
+                loop();
+	    }
+
+      	    set(WD_Main, MUIA_Window_Open, FALSE);
+
+	    deinit_gui();
+    	}
+	
+	close_libs();
+    }
+} /* main(int argc, char *argv[]) */
---
+++ workbench/libs/muimaster/tutorial/examples/muizunesupport.h
@@ -0,0 +1,130 @@
+/*
+    Copyright © 2003, The AROS Development Team. 
+    All rights reserved.
+    
+    $Id$
+*/
+
+#ifndef _ZUNE_MUISUPPORT_H
+#define _ZUNE_MUISUPPORT_H
+
+#include <string.h>
+
+#include <exec/memory.h>
+
+#include <libraries/asl.h>
+#include <libraries/mui.h>
+#include <prefs/prefhdr.h>
+
+#include <clib/alib_protos.h>
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <proto/intuition.h>
+#include <proto/utility.h>
+#include <proto/iffparse.h>
+
+#ifdef _AROS
+#include <proto/muimaster.h>
+#endif
+
+Object *MakeLabel(STRPTR str);
+LONG xget(Object * obj, ULONG attr);
+
+#define getstring(obj) (char *) xget(obj, MUIA_String_Contents)
+
+#define SimpleText(text) TextObject, MUIA_Text_Contents, (IPTR) text, End
+
+
+struct Library *MUIMasterBase;
+
+
+#ifndef _AROS
+
+/* On AmigaOS we build a fake library base, because it's not compiled as sharedlibrary yet */
+#include "muimaster_intern.h"
+
+int open_muimaster(void)
+{
+    static struct MUIMasterBase_intern MUIMasterBase_instance;
+    MUIMasterBase = (struct Library*)&MUIMasterBase_instance;
+
+    MUIMasterBase_instance.sysbase      = *((struct ExecBase **)4);
+    MUIMasterBase_instance.dosbase      = (void *)OpenLibrary("dos.library",        37);
+    MUIMasterBase_instance.utilitybase  = (void *)OpenLibrary("utility.library",    37);
+    MUIMasterBase_instance.aslbase      =         OpenLibrary("asl.library",        37);
+    MUIMasterBase_instance.gfxbase      = (void *)OpenLibrary("graphics.library",   37);
+    MUIMasterBase_instance.layersbase   =         OpenLibrary("layers.library",     37);
+    MUIMasterBase_instance.intuibase    = (void *)OpenLibrary("intuition.library",  37);
+    MUIMasterBase_instance.cxbase       =         OpenLibrary("commodities.library",37);
+    MUIMasterBase_instance.keymapbase   =         OpenLibrary("keymap.library",     37);
+    MUIMasterBase_instance.gadtoolsbase =         OpenLibrary("gadtools.library",   37);
+    MUIMasterBase_instance.iffparsebase =         OpenLibrary("iffparse.library",   37);
+    MUIMasterBase_instance.diskfontbase =         OpenLibrary("diskfont.library",   37);
+    __zune_prefs_init(&__zprefs);
+    InitSemaphore(&MUIMB(MUIMasterBase)->ZuneSemaphore);
+    return 1;
+}
+
+void close_muimaster(void)
+{
+}
+
+#else
+
+int open_muimaster(void)
+{
+    if ((MUIMasterBase = OpenLibrary("muimaster.library", 0))) return 1;
+    return 0;
+}
+
+void close_muimaster(void)
+{
+    if (MUIMasterBase) CloseLibrary(MUIMasterBase);
+}
+
+#endif
+
+
+/****************************************************************
+ Open needed libraries
+*****************************************************************/
+int open_libs(void)
+{
+    if (open_muimaster())
+    {
+	return 1;
+    }
+    
+    return 0;
+}
+
+/****************************************************************
+ Close opened libraries
+*****************************************************************/
+void close_libs(void)
+{
+    close_muimaster();
+}
+
+
+/****************************************************************
+ Create a simple label
+*****************************************************************/
+Object *MakeLabel(STRPTR str)
+{
+  return (MUI_MakeObject(MUIO_Label, str, 0));
+}
+
+
+/****************************************************************
+ Easy getting an attributes value
+*****************************************************************/
+LONG xget(Object * obj, ULONG attr)
+{
+    LONG x = 0;
+    get(obj, attr, &x);
+    return x;
+}
+
+
+#endif /* _ZUNE_MUISUPPORT_H */
