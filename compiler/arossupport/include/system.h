9b1108e2548af10da6321866f592583fdbc50bcd
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-01-16 22:34:23 +0000

    Removed definition of _AROS and __AROS__ preprocessor symbol
    git-svn-id: https://svn.aros.org/svn/aros/trunk@16258 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/arossupport/include/system.h
+++ compiler/arossupport/include/system.h
@@ -21,13 +21,6 @@
 #   ifndef _AMIGA /* One define for each system */
 #	define _AMIGA
 #   endif
-#else
-#   ifndef _AROS
-#       define _AROS
-#   endif
-#   ifndef __AROS__
-#       define __AROS__
-#   endif
 #endif
 
 /*
