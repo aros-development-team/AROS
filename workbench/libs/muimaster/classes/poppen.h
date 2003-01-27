a4108a4bd7618f5c05ec7520d64e7dc8d435211d
dlc <dlc@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-01-27 17:15:36 +0000

    added some missing pen related headers (classes not done yet)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@16312 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/muimaster/classes/poppen.h
@@ -0,0 +1,15 @@
+/*
+    Copyright © 2002, The AROS Development Team. 
+    All rights reserved.
+    
+    $Id$
+*/
+
+#ifndef _MUI_CLASSES_POPPEN_H
+#define _MUI_CLASSES_POPPEN_H
+
+#define MUIC_Poppen "Poppen.mui"
+
+extern const struct __MUIBuiltinClass _MUI_Poppen_desc; /* PRIV */
+
+#endif
