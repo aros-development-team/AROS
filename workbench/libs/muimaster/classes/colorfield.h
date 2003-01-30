731c61d3ada5499cd3f3c71241c347a3dd189446
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-01-30 18:04:07 +0000

    missing class. just dummy code for now
    git-svn-id: https://svn.aros.org/svn/aros/trunk@16343 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/muimaster/classes/colorfield.h
@@ -0,0 +1,21 @@
+/*
+    Copyright © 2002, The AROS Development Team. 
+    All rights reserved.
+    
+    $Id$
+*/
+
+#ifndef _MUI_CLASSES_COLORFIELD_H
+#define _MUI_CLASSES_COLORFIELD_H
+
+#define MUIC_Colorfield "Colorfield.mui"
+
+#define MUIA_Colorfield_Pen 	(TAG_USER|0x42713a)
+#define MUIA_Colorfield_Red 	(TAG_USER|0x4279f6)
+#define MUIA_Colorfield_Green	(TAG_USER|0x424466)
+#define MUIA_Colorfield_Blue 	(TAG_USER|0x42d3b0)
+#define MUIA_Colorfield_RGB 	(TAG_USER|0x42677a)
+
+extern const struct __MUIBuiltinClass _MUI_Colorfield_desc; /* PRIV */
+
+#endif
