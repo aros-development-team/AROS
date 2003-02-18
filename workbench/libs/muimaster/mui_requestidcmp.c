0cb206db186b434a136f610990efbbf79cd16b0d
dlc <dlc@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-02-18 00:48:15 +0000

    casts to remove warnings, and useless cosmetic cleanup
    git-svn-id: https://svn.aros.org/svn/aros/trunk@16466 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/mui_requestidcmp.c
+++ workbench/libs/muimaster/mui_requestidcmp.c
@@ -24,28 +24,40 @@ __asm VOID MUI_RequestIDCMP(register __a0 Object *obj, register __d0 ULONG flags
 	AROS_LH2(VOID, MUI_RequestIDCMP,
 
 /*  SYNOPSIS */
-	AROS_LHA(Object *, obj, A0),
-	AROS_LHA(ULONG, flags, D0),
+	AROS_LHA(Object *, obj  , A0),
+	AROS_LHA(ULONG   , flags, D0),
 
 /*  LOCATION */
 	struct Library *, MUIMasterBase, 15, MUIMaster)
 #endif
 /*  FUNCTION
 
+    For custom class implementors. Valid between MUIM_Setup/MUIM_Cleanup.
+    Indicates that this object wants to receive some events. MUIM_HandleInput
+    will then be called on this object whenever there's a matching event.
+
     INPUTS
+
+    obj   -- an object whose class is derived from Area
+    flags -- IDCMP flags
 
     RESULT
 
     NOTES
 
+    Deprecated in favor of MUIM_Window_AddEventHandler and MUIM_HandleEvent
+    event handling model (introduced in MUI V16).
+
     EXAMPLE
 
     BUGS
-	The function itself is a bug ;-) Remove it!
 
     SEE ALSO
 
     INTERNALS
+
+    This is a wrapper around MUIM_Window_RemEventHandler and
+    MUIM_Window_AddEventHandler.
 
     HISTORY
 
@@ -54,19 +66,23 @@ __asm VOID MUI_RequestIDCMP(register __a0 Object *obj, register __d0 ULONG flags
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct MUIMasterBase *,MUIMasterBase)
 
-/* Orginal idea: Get window object
- * Get list of objects that requested idcmp
- * put object and idcmp in list
- * window will check that list for matching idcmp
- */
-
-    /* Use the Eventhandler to simulate an MUIM_HandleInput */
+    /* Use the Eventhandler to simulate a MUIM_HandleInput */
     if (muiRenderInfo(obj) && (_flags(obj) & MADF_SETUP) && _win(obj))
     {
-	if (muiAreaData(obj)->mad_hiehn.ehn_Events) DoMethod(_win(obj),MUIM_Window_RemEventHandler, &muiAreaData(obj)->mad_hiehn);
+	if (muiAreaData(obj)->mad_hiehn.ehn_Events)
+	{
+	    DoMethod(_win(obj), MUIM_Window_RemEventHandler, (IPTR)&muiAreaData(obj)->mad_hiehn);
+	}
 	muiAreaData(obj)->mad_hiehn.ehn_Events |= flags;
-	if (muiAreaData(obj)->mad_hiehn.ehn_Events) DoMethod(_win(obj),MUIM_Window_AddEventHandler, &muiAreaData(obj)->mad_hiehn);
-    } else muiAreaData(obj)->mad_hiehn.ehn_Events |= flags;
+	if (muiAreaData(obj)->mad_hiehn.ehn_Events)
+	{
+	    DoMethod(_win(obj), MUIM_Window_AddEventHandler, (IPTR)&muiAreaData(obj)->mad_hiehn);
+	}
+    }
+    else
+    {
+	muiAreaData(obj)->mad_hiehn.ehn_Events |= flags;
+    }
 
     AROS_LIBFUNC_EXIT
 
