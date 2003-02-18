6c3870403810d028043251b7bf1df420b3c274e0
dlc <dlc@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-02-18 00:46:39 +0000

    useless aesthetic cleanup, and some casts to remove warnings
    git-svn-id: https://svn.aros.org/svn/aros/trunk@16464 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/mui_rejectidcmp.c
+++ workbench/libs/muimaster/mui_rejectidcmp.c
@@ -23,8 +23,8 @@ __asm VOID MUI_RejectIDCMP(register __a0 Object *obj, register __d0 ULONG flags)
 	AROS_LH2(VOID, MUI_RejectIDCMP,
 
 /*  SYNOPSIS */
-	AROS_LHA(Object *, obj, A0),
-	AROS_LHA(ULONG, flags, D0),
+	AROS_LHA(Object *, obj  , A0),
+	AROS_LHA(ULONG   , flags, D0),
 
 /*  LOCATION */
 	struct Library *, MUIMasterBase, 16, MUIMaster)
@@ -55,10 +55,20 @@ __asm VOID MUI_RejectIDCMP(register __a0 Object *obj, register __d0 ULONG flags)
 
     if (muiRenderInfo(obj) && (_flags(obj) & MADF_SETUP) && _win(obj))
     {
-	if (muiAreaData(obj)->mad_hiehn.ehn_Events) DoMethod(_win(obj),MUIM_Window_RemEventHandler, &muiAreaData(obj)->mad_hiehn);
+	if (muiAreaData(obj)->mad_hiehn.ehn_Events)
+	{
+	    DoMethod(_win(obj), MUIM_Window_RemEventHandler, (IPTR)&muiAreaData(obj)->mad_hiehn);
+	}
 	muiAreaData(obj)->mad_hiehn.ehn_Events &= ~flags;
-	if (muiAreaData(obj)->mad_hiehn.ehn_Events) DoMethod(_win(obj),MUIM_Window_AddEventHandler, &muiAreaData(obj)->mad_hiehn);
-    } else muiAreaData(obj)->mad_hiehn.ehn_Events &= ~flags;
+	if (muiAreaData(obj)->mad_hiehn.ehn_Events)
+	{
+	    DoMethod(_win(obj), MUIM_Window_AddEventHandler, (IPTR)&muiAreaData(obj)->mad_hiehn);
+	}
+    }
+    else
+    {
+	muiAreaData(obj)->mad_hiehn.ehn_Events &= ~flags;
+    }
 
     AROS_LIBFUNC_EXIT
 
