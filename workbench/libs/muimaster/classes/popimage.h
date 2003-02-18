8c3c72210d08776b7956d5ed17618fba17911efb
dlc <dlc@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-02-18 03:02:20 +0000

    removed MUIA_Imagedisplay_Spec; added private methods
    git-svn-id: https://svn.aros.org/svn/aros/trunk@16483 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/classes/popimage.h
+++ workbench/libs/muimaster/classes/popimage.h
@@ -10,7 +10,15 @@
 
 #define MUIC_Popimage "Popimage.mui"
 
-#define MUIA_Imagedisplay_Spec (TAG_USER|0x0042a547)
+#ifndef METHOD_USER
+#define METHOD_USER TAG_USER
+#endif
+
+/* Popimage methods */
+#define MUIM_Popimage_OpenWindow   (METHOD_USER|0x0042a548)     /* PRIV */
+#define MUIM_Popimage_CloseWindow  (METHOD_USER|0x0042a549)     /* PRIV */
+struct MUIP_Popimage_OpenWindow    {ULONG MethodID;};           /* PRIV */
+struct MUIP_Popimage_CloseWindow   {ULONG MethodID; LONG ok;};  /* PRIV */
 
 extern const struct __MUIBuiltinClass _MUI_Popimage_desc; /* PRIV */
 
