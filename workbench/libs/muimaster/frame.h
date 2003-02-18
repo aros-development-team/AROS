0d457bf06dbd5ccd9aabdf0177d5b15dfd7a6e20
dlc <dlc@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-02-18 00:39:51 +0000

    cleanup, mostly replaced FrameSpec with FrameSpec_intern to distinguish between external and internal representation
    git-svn-id: https://svn.aros.org/svn/aros/trunk@16459 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/frame.h
+++ workbench/libs/muimaster/frame.h
@@ -13,15 +13,44 @@
 #include <exec/types.h>
 #endif
 
+struct MUI_FrameSpec;
+
+/* MUI_*Spec really are ASCII strings.
+ */
+#if 0
 struct MUI_FrameSpec
 {
-    UWORD type;
-    UWORD state;           /* 0 = up, 1 = down */
-    UWORD innerLeft;
-    UWORD innerRight;
-    UWORD innerTop;
-    UWORD innerBottom;
+    UBYTE spec[7]; /* eg. "504444", "A13333" */
 };
+#endif
+
+typedef enum {
+    FST_NONE,
+    FST_RECT,
+    FST_BEVEL,
+    FST_THIN_BORDER,
+    FST_THICK_BORDER,
+    FST_ROUND_BEVEL, /* 5 */
+    FST_WIN_BEVEL,
+    FST_ROUND_THICK_BORDER,
+    FST_ROUND_THIN_BORDER,
+    FST_GRAY_BORDER,
+    FST_SEMIROUND_BEVEL,
+    FST_COUNT,
+} FrameSpecType;
+
+/* here values are converted from their ASCII counterparts
+ */
+struct MUI_FrameSpec_intern
+{
+    FrameSpecType type;
+    UBYTE state;           /* 0 = up, 1 = down */
+    UBYTE innerLeft;
+    UBYTE innerRight;
+    UBYTE innerTop;
+    UBYTE innerBottom;
+};
+
 
 struct MUI_RenderInfo;
 
@@ -36,25 +65,12 @@ struct ZuneFrameGfx {
 };
 
 struct ZuneFrameGfx *zune_zframe_get_index (int i);
-struct ZuneFrameGfx *zune_zframe_get (struct MUI_FrameSpec *frameSpec);
+struct ZuneFrameGfx *zune_zframe_get (struct MUI_FrameSpec_intern *frameSpec);
 
-typedef enum {
-    FST_NONE,
-    FST_RECT,
-    FST_BEVEL,
-    FST_THIN_BORDER,
-    FST_THICK_BORDER,
-    FST_WIN_BEVEL,
-    FST_GRAY_BORDER,
-    FST_SEMIROUND_BEVEL,
-    FST_ROUND_THIN_BORDER,
-    FST_ROUND_THICK_BORDER,
-    FST_ROUND_BEVEL,
-    FST_COUNT,
-} FrameSpecType;
-
-STRPTR zune_framespec_to_string (struct MUI_FrameSpec *frameSpec);
-BOOL zune_string_to_framespec(STRPTR str, struct MUI_FrameSpec *frameSpec);
+BOOL zune_frame_intern_to_spec (const struct MUI_FrameSpec_intern *intern,
+				STRPTR spec);
+BOOL zune_frame_spec_to_intern(CONST_STRPTR spec,
+			       struct MUI_FrameSpec_intern *intern);
 
 #endif
 
