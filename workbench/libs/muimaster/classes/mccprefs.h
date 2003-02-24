323adeed2689029d63b7f5dde57b78f0a9c3ff20
dlc <dlc@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-02-24 23:07:54 +0000

    added dummy Mccprefs class; dont know much about it yet, except that it is superclass of all the .mcp
    git-svn-id: https://svn.aros.org/svn/aros/trunk@16556 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/muimaster/classes/mccprefs.h
@@ -0,0 +1,15 @@
+/*
+    Copyright © 2003, The AROS Development Team. 
+    All rights reserved.
+    
+    $Id$
+*/
+
+#ifndef _MUI_CLASSES_MCCPREFS_H
+#define _MUI_CLASSES_MCCPREFS_H
+
+#define MUIC_Mccprefs "Mccprefs.mui"
+
+extern const struct __MUIBuiltinClass _MUI_Mccprefs_desc; /* PRIV */
+
+#endif
--- workbench/libs/muimaster/support.c
+++ workbench/libs/muimaster/support.c
@@ -154,7 +154,8 @@ static const struct __MUIBuiltinClass *builtins[] =
     &_MUI_Imagedisplay_desc,
     &_MUI_Pendisplay_desc,
     &_MUI_Penadjust_desc,
-    &_MUI_Poppen_desc
+    &_MUI_Poppen_desc,
+    &_MUI_Mccprefs_desc
 };
 
 #define NUM_BUILTINS  sizeof(builtins) / sizeof(struct __MUIBuiltinClass *)
