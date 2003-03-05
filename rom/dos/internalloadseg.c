bc805e4c128f6cbed8da10ccfe45f6964e75d15d
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-03-05 04:00:31 +0000

    Back to __mc68000__.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@16603 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/internalloadseg.c
+++ rom/dos/internalloadseg.c
@@ -85,6 +85,7 @@
         bug("Loaded as ELF exe\n");
 #endif
     }
+#ifndef __mc68000__
     if (!segs && IoErr()==ERROR_NOT_EXECUTABLE)
     {
       segs = InternalLoadSeg_ELF_AROS (fh, MKBADDR(NULL), functionarray, NULL, DOSBase);
@@ -93,6 +94,7 @@
         bug("Loaded as ELF exe\n");
 #endif
     }
+#endif
     if (!segs && IoErr()==ERROR_NOT_EXECUTABLE)
     {
       segs = InternalLoadSeg_AOS (fh, MKBADDR(NULL), functionarray, NULL, DOSBase);
