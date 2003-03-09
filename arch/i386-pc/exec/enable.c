ee5d13ffd67343afc6d11695d9f2a49c6ad6a92b
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-03-09 12:32:47 +0000

    when idnestcnt + tdnestcnt both < 0 (== task switching + interrupts enabled) then check for task switching like Permit() does. Need to do this manually as there's no dff09c like thing in x86 native, which would allow to do this automatically through delayed triggered software interrupt.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@16684 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-pc/exec/enable.c
+++ arch/i386-pc/exec/enable.c
@@ -11,6 +11,8 @@
 #include <aros/libcall.h>
 #include <proto/exec.h>
 
+void Exec_Permit_Supervisor();
+
 AROS_LH0(void, Enable,
     struct ExecBase *, SysBase, 21, Exec)
 {
@@ -19,6 +21,17 @@ AROS_LH0(void, Enable,
     if(--SysBase->IDNestCnt < 0)
     {
     	__asm__ __volatile__ ("sti");
+	
+	/* There's no dff09c like thing in x86 native which would allow
+	   us to set delayed (mark it as pending but it gets triggered
+	   only once interrupts are enabled again) software interrupt,
+	   so we check it manually here in Enable() == same stuff as
+	   in Permit(). */
+	   
+	if ((SysBase->TDNestCnt < 0) && (SysBase->AttnResched & 0x80))
+	{
+	    Supervisor(Exec_Permit_Supervisor);	    
+	}
     }
 
     AROS_LIBFUNC_EXIT
