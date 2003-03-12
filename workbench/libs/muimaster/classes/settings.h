35da730f3f63dfbd2649e591106307a4dae25b25
dlc <dlc@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-03-12 14:19:43 +0000

    Settings class is the main part (list+settingsgroup) in zune prefs window. Currently mostly dummy but should get (with Settingsgroup) most of zune prefs code.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@16743 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/muimaster/classes/settings.h
@@ -0,0 +1,19 @@
+/*
+    Copyright © 2002, The AROS Development Team. 
+    All rights reserved.
+    
+    $Id$
+*/
+
+#ifndef _MUI_CLASSES_SETTINGS_H
+#define _MUI_CLASSES_SETTINGS_H
+
+#ifndef METHOD_USER
+#define METHOD_USER TAG_USER
+#endif
+
+#define MUIC_Settings  "Settings.mui"
+
+extern const struct __MUIBuiltinClass _MUI_Settings_desc; /* PRIV */
+
+#endif
