3f0423be7f9eb5e497c64fe44bf70ee0976adbd8
gierich <gierich@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-03-24 12:48:52 +0000

    Linker library for JPEG files, documentation and test programs, based on v6b sources of IJG
    git-svn-id: https://svn.aros.org/svn/aros/trunk@16838 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/libjpeg/main/debug.h
@@ -0,0 +1,35 @@
+/*
+    $Id$
+*/
+
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#ifndef __DEBUG_H
+#define __DEBUG_H
+
+/* Debug Macros */
+
+#ifdef __AROS__
+
+#undef DEBUG
+#define DEBUG 1
+#include <aros/debug.h>
+
+#else /* not __AROS__ */
+
+#define bug kprintf
+
+#ifdef MYDEBUG
+void kprintf(char *string, ...);
+#define D(x) {kprintf("%s/%ld (%s): ", __FILE__, __LINE__, FindTask(NULL)->tc_Node.ln_Name);(x);};
+#else
+#define D(x) ;
+
+#endif /* MYDEBUG */
+
+#endif /* not __AROS__ */
+
+#endif /* __DEBUG_H */
