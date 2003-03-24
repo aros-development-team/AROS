daca1b1b470804e779f10e1872b0f730f85e8f8c
dlc <dlc@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-03-24 16:40:51 +0000

    added declaration for ImageClipboard global class pointer
    git-svn-id: https://svn.aros.org/svn/aros/trunk@16844 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/prefs/Zune/zunestuff.h
+++ workbench/prefs/Zune/zunestuff.h
@@ -10,8 +10,12 @@
 
 Object *MakeLabel(STRPTR str);
 Object *MakeButton(STRPTR str);
+
 LONG xget(Object * obj, ULONG attr);
 
 #define getstring(obj) (char*)xget(obj,MUIA_String_Contents)
+
+extern struct MUI_CustomClass *CL_ImageClipboard;
+
 
 #endif
