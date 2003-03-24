c950ba84ddaafc7c385e2c5a1664456c4763d574
dlc <dlc@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-03-24 00:19:15 +0000

    implemented MUIA_Imagedisplay_FreeHoriz and MUIA_Imagedisplay_FreeVert (default to TRUE) to allow showing an image in its default size
    git-svn-id: https://svn.aros.org/svn/aros/trunk@16835 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/classes/imagedisplay.h
+++ workbench/libs/muimaster/classes/imagedisplay.h
@@ -11,6 +11,8 @@
 #define MUIC_Imagedisplay "Imagedisplay.mui"
 
 #define MUIA_Imagedisplay_Spec      (TAG_USER|0x0042a547) /* MUI: V11 isg struct MUI_ImageSpec * */
+#define MUIA_Imagedisplay_FreeHoriz (TAG_USER|0x0042c356) /* Zune 20030323 i.. BOOL [TRUE] */
+#define MUIA_Imagedisplay_FreeVert  (TAG_USER|0x0042c422) /* Zune 20030323 i.. BOOL [TRUE] */
 
 extern const struct __MUIBuiltinClass _MUI_Imagedisplay_desc; /* PRIV */
 
