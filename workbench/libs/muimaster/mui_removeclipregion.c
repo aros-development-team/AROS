4cb51388081a1023f0c20d12a39db84eca9dd20a
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-04-01 21:11:40 +0000

    when in refreshmode (ie. InstallClipRegion() surrounded by EndRefrsh -> BeginRefresh()) protect with LockLayerInfo() /UnLockLayerInfo() to avoid damage getting lost because of async Intuition actions. Surrounded some code which I don't understand why it's there with #if 1 .. #endif + a comment.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@16959 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/mui_removeclipregion.c
+++ workbench/libs/muimaster/mui_removeclipregion.c
@@ -74,19 +74,29 @@ __asm VOID MUI_RemoveClipRegion(register __a0 struct MUI_RenderInfo *mri, regist
     mri->mri_rCount--;
 
     if ((w != NULL) && (mri->mri_Flags & MUIMRI_REFRESHMODE))
+    {
+    	LockLayerInfo(&w->WScreen->LayerInfo);
         EndRefresh(w, FALSE);
-
+    }
+    
+#if 1 /* stegerg: what's this good for */
     if ((w != NULL) && !(w->Flags & WFLG_SIMPLE_REFRESH))
         LockLayerInfo(&w->WScreen->LayerInfo);
+#endif
 
     InstallClipRegion(l, (mri->mri_rCount > 0)
         ? mri->mri_rArray[mri->mri_rCount-1] : NULL);
 
+#if 1 /* stegerg: what's this good for */
     if ((w != NULL) && !(w->Flags & WFLG_SIMPLE_REFRESH))
         UnlockLayerInfo(&w->WScreen->LayerInfo);
+#endif
 
     if ((w != NULL) && (mri->mri_Flags & MUIMRI_REFRESHMODE))
+    {
         BeginRefresh(w);
+	UnlockLayerInfo(&w->WScreen->LayerInfo);
+    }
 
     DisposeRegion(mri->mri_rArray[mri->mri_rCount]);
     mri->mri_rArray[mri->mri_rCount] = NULL;
