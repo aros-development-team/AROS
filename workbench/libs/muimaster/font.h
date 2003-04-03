8e092c7e67a3abfb4349dbb46858347080103769
dlc <dlc@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-04-03 01:01:21 +0000

    moved the font stuff from Area to its own file
    git-svn-id: https://svn.aros.org/svn/aros/trunk@16982 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/muimaster/font.h
@@ -0,0 +1,16 @@
+/* 
+    Copyright © 2003, The AROS Development Team.
+    All rights reserved.
+
+    $Id$
+*/
+
+#ifndef _ZUNE_FONT_H
+#define _ZUNE_FONT_H
+
+/* Returns a given text font, if necessary it opens the font.
+ * Must be called after Area's MUIM_Setup.
+ */
+struct TextFont *zune_font_get(Object *obj, LONG preset);
+
+#endif /* _ZUNE_FONT_H */
