d223dcd408a00a29392dec77b46e0d2c0780a50c
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-04-04 03:34:55 +0000

    Added serial output capability.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@17010 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/.unmaintained/m68k-pp-native/exec/rawputchar.c
+++ arch/.unmaintained/m68k-pp-native/exec/rawputchar.c
@@ -8,6 +8,10 @@
 
 #include <aros/config.h>
 
+#ifdef DO_SERIAL_DEBUG
+# include <asm/registers.h>
+#endif
+
 /*****i***********************************************************************
 
     NAME */
@@ -46,20 +50,33 @@
 
 *****************************************************************************/
 {
-    AROS_LIBFUNC_INIT
-    AROS_LIBBASE_EXT_DECL(struct ExecBase *,SysBase)
+	AROS_LIBFUNC_INIT
+	AROS_LIBBASE_EXT_DECL(struct ExecBase *,SysBase)
 
+#ifdef DO_SERIAL_DEBUG
+	/*
+	 * This does not work with xcopilot...
+	 */
+	if (chr) {
+		/*
+		 * Wait until FIFO is half empty
+		 */
+		while (0 == (RREG_W(UTX1) & FIFO_HALF_F)) {
+		}
+		WREG_W(UTX1) = (UWORD)chr;
+	}
+#else
 #ifdef AROS_PALM_DEBUG_HACK
-    /* This requires a patched XCopilot which turns writes to
-       address 0xdddddebc into output going to Linux shell
-       through a fprintf(stderr, chr). */
+	/* This requires a patched XCopilot which turns writes to
+	   address 0xdddddebc into output going to Linux shell
+	   through a fprintf(stderr, chr). */
 
-    /* Don't write 0 bytes */
-    if (chr)
-    {
-    	*(UBYTE *)0xdddddebc = chr;
-    }
+	/* Don't write 0 bytes */
+	if (chr) {
+		*(UBYTE *)0xdddddebc = chr;
+	}
+#endif
 #endif
 
-    AROS_LIBFUNC_EXIT
+	AROS_LIBFUNC_EXIT
 } /* RawPutChar */
