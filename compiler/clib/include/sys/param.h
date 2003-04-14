57e99050528ea4cd1af6616b0c92127e3fc6d2f2
falemagn <falemagn@gmail.com>
2003-04-14 03:58:54 +0000

    Oops, removed some trash and a bug
    git-svn-id: https://svn.aros.org/svn/aros/trunk@17194 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/include/sys/param.h
+++ compiler/clib/include/sys/param.h
@@ -15,7 +15,7 @@
 
 #include <sys/types.h>
 
-*
+/*
  * Definitions for byte order, according to byte significance from low
  * address to high.
  */
@@ -23,7 +23,7 @@
 #define BIG_ENDIAN      4321    /* MSB first: 68000, ibm, net */
 #define PDP_ENDIAN      3412    /* LSB first in word, MSW first in long */
 
-#ifdef AROS_BIG_ENDIAN
+#if AROS_BIG_ENDIAN
 #define BYTE_ORDER      BIG_ENDIAN
 #else
 #define BYTE_ORDER      LITTLE_ENDIAN
