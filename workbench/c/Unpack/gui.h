76cb535a75e58c691caaa89a2954afd9a7d5bae1
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-04-23 11:41:26 +0000

    Started work on GUI.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@17294 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/c/Unpack/gui.h
@@ -0,0 +1,18 @@
+#ifndef PKG_GUI_H
+#define PKG_GUI_H
+
+/*
+    Copyright © 2003, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <exec/types.h>
+
+/****************************************************************************/
+/** Prototypes **************************************************************/
+
+void GUI_Open();
+void GUI_Close();
+void GUI_Update( LONG position );
+
+#endif /* PKG_GUI_H */
