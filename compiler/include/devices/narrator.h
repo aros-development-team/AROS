9aeb1f7eea8dacdb78ed9b5f1a22fcdd4090cbdb
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-05-02 00:48:16 +0000

    Just some dummy constants to make Term happy.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@17447 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/devices/narrator.h
@@ -0,0 +1,30 @@
+#ifndef DEVICES_NARRATOR_H
+#define DEVICES_NARRATOR_H
+
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: Narrator device (not implemented)
+    Lang: english
+*/
+
+/*
+ * Some of the defines here are needed in programs (Term V4.7).
+ */
+#define MINRATE       1
+#define MAXRATE       100
+#define MINFREQ       200
+#define MAXFREQ       20000
+#define MINPITCH      10
+#define MAXPITCH      50
+#define MINVOL        1
+#define MAXVOL        100
+
+#define DEFRATE       50
+#define DEFFREQ       2000
+#define DEFPITCH      30
+#define DEFVOL        50
+#define DEFSEX        0
+
+#endif
