945463af561ff0c4d135cf6dea825c99827efa65
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-05-09 07:05:00 +0000

    Initial kernel.resource release. Provides only IO functionality. exec extensions will be commited later.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@17529 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/ppc-all/prep/kernel/kernel.h
@@ -0,0 +1,29 @@
+#ifndef _KERNEL_H
+#define _KERNEL_H
+
+#include <exec/types.h>
+#include <exec/execbase.h>
+#include <exec/nodes.h>
+
+#include "libdefs.h"
+
+struct KernelBase {
+    struct Node	    node;
+    struct ExecBase *sysBase;
+};
+
+#ifdef SysBase
+#undef SysBase
+#endif
+#define SysBase KenrelBase->sysBase
+
+#define _IO_BASE    0x80000000
+#define _BUS_BASE   0xc0000000
+
+#define VECTOR_COUNT	8
+
+extern UBYTE Kernel_end;
+extern void *const LIBFUNCTABLE[VECTOR_COUNT+1];
+
+#endif /* _KERNEL_H */
+
