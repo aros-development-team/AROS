2981e3877d8d4e7c92ed2913c29192a9eb4cf8e4
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-05-26 15:47:06 +0000

    Factored out common code.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@17715 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- tools/genmodule/genmoduledummy.c
+++ tools/genmodule/genmoduledummy.c
@@ -1,32 +1,10 @@
 /*
-    Copyright © 1995-2002, The AROS Development Team. All rights reserved.
-
-    Desc: main for genmodule. A tool to generate files for building modules.
+    Copyright © 1995-2003, The AROS Development Team. All rights reserved.
+    $Id$
+    
+    Main for genmoduledummy. A tool to generate files for building modules.
 */
 #include "genmodule.h"
-
-/* In funclist the information of all the functions of the module will be stored.
- * The list has to be sorted on the lvonum field
- */
-struct functionlist *funclist = NULL;
-
-/* In forcelist a list of basenames is present that need to be present in the
- * static link library so that certain libraries are opened by a program
- */
-struct forcelist *forcelist = NULL;
-
-/* global variables that store filename and paths derived from argv */
-char *conffile, *gendir, *genincdir, *reffile;
-
-/* global variables that store the configuration of the module */
-enum modtype modtype = UNSPECIFIED;
-enum libcall libcall = STACK;
-
-char *modulename = NULL, *basename = NULL, *modulenameupper = NULL, 
-     *libbase = NULL, *libbasetype = NULL, *libbasetypeextern = NULL, 
-     *datestring = "00.00.0000", *superclass = NULL;
-unsigned int majorversion = 0, minorversion = 0, firstlvo = 0;
-struct linelist *cdeflines = NULL, *protolines = NULL;
 
 int main(int argc, char **argv)
 {
