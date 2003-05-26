4a159342afba6f38209c185a3fc5f34d9536b51c
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-05-26 15:41:35 +0000

    Just the important bit of the dispatcher missing now.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@17714 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- tools/genmodule/writemccinit.c
+++ tools/genmodule/writemccinit.c
@@ -28,16 +28,91 @@ void writemccinit(void)
     (
         out,
         "/*\n"
-        "    *** Automatically generated file. Do not edit ***\n"
+        "    *** Automatically generated file. Do not edit! ***\n"
         "    Copyright © 1995-2003, The AROS Development Team. All rights reserved.\n"
         "*/\n"
-        "#include <libcore/libheader.c>\n"
+        "\n"
+        "#include <exec/types.h>\n"
+        "#include <exec/libraries.h>\n"
+        "#include <dos/dosextens.h>\n"
+        "#include <aros/libcall.h>\n"
+        "#include <aros/debug.h>\n"
+        "#include <libcore/base.h>\n"
+        "\n"
+        "#include <intuition/classes.h>\n"
+        "#include <intuition/classusr.h>\n"
+        "\n"
+        "#include <proto/exec.h>\n"
+        "#include <proto/intuition.h>\n"
+        "#include <proto/muimaster.h>\n"
+        "\n"
+        "#include <aros/symbolsets.h>\n"
+        "#include LC_LIBDEFS_FILE\n"
+        "\n"
     );
-    
+        
     for(linelistit = cdeflines; linelistit!=NULL; linelistit = linelistit->next)
     {
         fprintf(out, "%s\n", linelistit->line);
     }
+      
+    fprintf
+    (
+        out,
+        "/*** Variables **************************************************************/\n"
+        "struct ExecBase        *SysBase;\n"
+        "struct Library         *MUIMasterBase;\n"
+        "\n"
+        "struct MUI_CustomClass *MCC;\n"
+        "\n"
+        "\n"
+        "/*** Dispatcher *************************************************************/\n"
+        "BOOPSI_DISPATCHER( IPTR, %s_Dispatcher, CLASS, self, message )\n"
+        "{\n"
+        "    switch( message->MethodID )\n"
+        "    {\n",
+        modulename
+    );
+    
+    /* FIXME: Methods here... */
+    
+    /*case OM_NEW: return PreferencesWindow$OM_NEW( CLASS, self, (struct opSet *) message );*/
+    
+    fprintf
+    (
+        out,
+        "        default:     return DoSuperMethodA( CLASS, self, message );\n"
+        "    }\n"
+        "    \n"
+        "    return NULL;\n"
+        "}\n"
+        "\n"
+        "\n"
+        "/*** Library startup and shutdown *******************************************/\n"
+        "AROS_SET_LIBFUNC( MCC_Startup, LIBBASETYPE, LIBBASE )\n"
+        "{\n"
+        "    SysBase = LIBBASE->lh_SysBase;\n"
+        "    \n"
+        "    MUIMasterBase = OpenLibrary( \"muimaster.library\", 0 );\n"
+        "    if( MUIMasterBase == NULL ) return FALSE;\n"
+        "    \n"
+        "    MCC = MUI_CreateCustomClass( LIBBASE, \"%s\", NULL, 0, %s_Dispatcher );\n"
+        "    if( MCC == NULL ) return FALSE;\n"
+        "    \n"
+        "    return TRUE;\n"
+        "}\n"
+        "\n"
+        "AROS_SET_LIBFUNC( MCC_Shutdown, LIBBASETYPE, LIBBASE )\n"
+        "{\n"
+        "    CloseLibrary( MUIMasterBase );\n"
+        "    \n"
+        "    MUI_DeleteCustomClass( MCC );\n"
+        "}\n"
+        "\n"
+        "ADD2INITLIB( MCC_Startup, 0 );\n"
+        "ADD2EXPUNGELIB( MCC_Shutdown, 0 );\n",
+        superclass, modulename            
+    );
     
     fclose(out);
 }
