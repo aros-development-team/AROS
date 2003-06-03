b74aeb023a08f0144c623111dc6c66223703476e
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-06-03 21:45:14 +0000

    Changed method and attribute IDs according to the new allocation system. NOTE: Make sure all programs and the classes themselves are properly recompiled from scratch otherwise wierd things will happen!
    git-svn-id: https://svn.aros.org/svn/aros/trunk@17859 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/classes/scrollbutton.h
+++ workbench/libs/muimaster/classes/scrollbutton.h
@@ -6,15 +6,21 @@
 #ifndef _MUI_CLASSES_SCROLLBUTTON_H
 #define _MUI_CLASSES_SCROLLBUTTON_H
 
-#define MUIC_Scrollbutton "Scrollbutton.mui"
+/*** Name *******************************************************************/
+#define MUIC_Scrollbutton             "Scrollbutton.mui"
 
+/*** Identifier base ********************************************************/
+#define MUIB_Scrollbutton             (MUIB_ZUNE | 0x00003100)  
+
+/*** Attributes *************************************************************/
+#define MUIA_Scrollbutton_NewPosition (MUIB_Scrollbutton | 0x00000000) /* ..G ULONG (packed two WORDs)*/
+#define MUIA_Scrollbutton_Horiz       (MUIB_Scrollbutton | 0x00000001) /*     WORD */
+#define MUIA_Scrollbutton_Vert        (MUIB_Scrollbutton | 0x00000002) /*     WORD */
+#define MUIA_Scrollbutton_HorizProp   (MUIB_Scrollbutton | 0x00000003) /* ISG Object * */
+#define MUIA_Scrollbutton_VertProp    (MUIB_Scrollbutton | 0x00000004) /* ISG Object * */
+
+/*** Macros *****************************************************************/
 #define ScrollbuttonObject MUIOBJMACRO_START(MUIC_Scrollbutton)
-
-#define MUIA_Scrollbutton_NewPosition       (MUIB_MUI|0x1820a000) /* ..G ULONG (packed two WORDs)*/
-#define MUIA_Scrollbutton_Horiz             (MUIB_MUI|0x1820a001) /*     WORD */
-#define MUIA_Scrollbutton_Vert              (MUIB_MUI|0x1820a002) /*     WORD */
-#define MUIA_Scrollbutton_HorizProp         (MUIB_MUI|0x1820a003) /* ISG Object * */
-#define MUIA_Scrollbutton_VertProp          (MUIB_MUI|0x1820a004) /* ISG Object * */
 
 extern const struct __MUIBuiltinClass _MUI_Scrollbutton_desc; /* PRIV */
 
