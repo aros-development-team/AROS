05b97d72d1e43f8459b8dd46266e7899378c65d9
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-06-04 09:32:41 +0000

    Fixed attribute comments.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@17864 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/classes/scrollbutton.h
+++ workbench/libs/muimaster/classes/scrollbutton.h
@@ -13,11 +13,11 @@
 #define MUIB_Scrollbutton             (MUIB_ZUNE | 0x00003100)  
 
 /*** Attributes *************************************************************/
-#define MUIA_Scrollbutton_NewPosition (MUIB_Scrollbutton | 0x00000000) /* ..G ULONG (packed two WORDs)*/
-#define MUIA_Scrollbutton_Horiz       (MUIB_Scrollbutton | 0x00000001) /*     WORD */
-#define MUIA_Scrollbutton_Vert        (MUIB_Scrollbutton | 0x00000002) /*     WORD */
-#define MUIA_Scrollbutton_HorizProp   (MUIB_Scrollbutton | 0x00000003) /* ISG Object * */
-#define MUIA_Scrollbutton_VertProp    (MUIB_Scrollbutton | 0x00000004) /* ISG Object * */
+#define MUIA_Scrollbutton_NewPosition (MUIB_Scrollbutton | 0x00000000) /* --G  ULONG (2 x WORD) */
+#define MUIA_Scrollbutton_Horiz       (MUIB_Scrollbutton | 0x00000001) /* -SG  WORD             */
+#define MUIA_Scrollbutton_Vert        (MUIB_Scrollbutton | 0x00000002) /* -SG  WORD             */
+#define MUIA_Scrollbutton_HorizProp   (MUIB_Scrollbutton | 0x00000003) /* --G  Object *         */
+#define MUIA_Scrollbutton_VertProp    (MUIB_Scrollbutton | 0x00000004) /* --G  Object *         */
 
 /*** Macros *****************************************************************/
 #define ScrollbuttonObject MUIOBJMACRO_START(MUIC_Scrollbutton)
