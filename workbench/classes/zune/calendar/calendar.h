038813860297cb5eeea28d3587057a02942ae7e5
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-06-04 09:27:17 +0000

    Added applicability (ISG) and type comments for attributes.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@17863 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/classes/zune/calendar/calendar.h
+++ workbench/classes/zune/calendar/calendar.h
@@ -15,13 +15,13 @@
 #define MUIB_Calendar           (MUIB_AROS | 0x00000100)
 
 /*** Attributes *************************************************************/
-#define MUIA_Calendar_Date	(MUIB_Calendar | 0x00000000)
-#define MUIA_Calendar_MonthDay 	(MUIB_Calendar | 0x00000001)
-#define MUIA_Calendar_MonthDay0	(MUIB_Calendar | 0x00000002)
-#define MUIA_Calendar_Month 	(MUIB_Calendar | 0x00000003)
-#define MUIA_Calendar_Month0 	(MUIB_Calendar | 0x00000004)
-#define MUIA_Calendar_Year  	(MUIB_Calendar | 0x00000005)
-#define MUIA_Calendar_DayLabels (MUIB_Calendar | 0x00000006)
+#define MUIA_Calendar_Date	(MUIB_Calendar | 0x00000000) /* ISG  struct ClockData * */
+#define MUIA_Calendar_MonthDay 	(MUIB_Calendar | 0x00000001) /* -SG  UWORD              */
+#define MUIA_Calendar_MonthDay0	(MUIB_Calendar | 0x00000002) /* -SG  UWORD              */
+#define MUIA_Calendar_Month 	(MUIB_Calendar | 0x00000003) /* -SG  UWORD              */
+#define MUIA_Calendar_Month0 	(MUIB_Calendar | 0x00000004) /* -SG  UWORD              */
+#define MUIA_Calendar_Year  	(MUIB_Calendar | 0x00000005) /* -SG  UWORD              */
+#define MUIA_Calendar_DayLabels (MUIB_Calendar | 0x00000006) /* I--  STRPTR [12]        */
 
 /*** Macros *****************************************************************/
 #define CalendarObject MUIOBJMACRO_START(MUIC_Calendar)
