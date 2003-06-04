31798733b07e5026e74816f547dd14c37fdcca49
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-06-04 09:21:00 +0000

    Added applicability (ISG) and type comments for attributes.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@17862 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/classes/zune/clock/clock.h
+++ workbench/classes/zune/clock/clock.h
@@ -15,13 +15,13 @@
 #define MUIB_Clock                      (MUIB_AROS | 0x00000000)
 
 /*** Attributes *************************************************************/
-#define MUIA_Clock_Hour 	        (MUIB_Clock | 0x00000000)
-#define MUIA_Clock_Min 	    	        (MUIB_Clock | 0x00000001)
-#define MUIA_Clock_Sec 	    	        (MUIB_Clock | 0x00000002)
-#define MUIA_Clock_Time	    	        (MUIB_Clock | 0x00000003)
-#define MUIA_Clock_Ticked   	        (MUIB_Clock | 0x00000004)
-#define MUIA_Clock_Frozen  	        (MUIB_Clock | 0x00000005)
-#define MUIA_Clock_EditHand	        (MUIB_Clock | 0x00000006)
+#define MUIA_Clock_Hour 	        (MUIB_Clock | 0x00000000) /* -SG  UWORD              */
+#define MUIA_Clock_Min 	    	        (MUIB_Clock | 0x00000001) /* -SG  UWORD              */
+#define MUIA_Clock_Sec 	    	        (MUIB_Clock | 0x00000002) /* -SG  UWORD              */
+#define MUIA_Clock_Time	    	        (MUIB_Clock | 0x00000003) /* ISG  struct ClockData * */
+#define MUIA_Clock_Ticked   	        (MUIB_Clock | 0x00000004) /* ---  BOOL               */
+#define MUIA_Clock_Frozen  	        (MUIB_Clock | 0x00000005) /* ISG  BOOL               */
+#define MUIA_Clock_EditHand	        (MUIB_Clock | 0x00000006) /* ISG  WORD               */
 
 #define MUIV_Clock_EditHand_Hour        (0)
 #define MUIV_Clock_EditHand_Minute      (1)
