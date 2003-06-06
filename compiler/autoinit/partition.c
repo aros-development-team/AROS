3382530f58b0d37aa95457bcefde320ac6115c8e
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-06-06 10:12:29 +0000

    Added support for partition.library.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@17890 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/autoinit/partition.c
@@ -0,0 +1,9 @@
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <libraries/partition.h>
+#include <aros/symbolsets.h>
+
+ADD2LIBS("partition.library", 1, LIBSET_PARTITION_PRI, struct Library *, PartitionBase, NULL, NULL);
