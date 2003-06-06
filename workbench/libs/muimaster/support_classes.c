ee0d186d8fd1cdb53fc1b815c56c3fd2fcd7b2ef
dlc <dlc@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-06-06 17:14:23 +0000

    dont compile the unused settings class
    git-svn-id: https://svn.aros.org/svn/aros/trunk@17901 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/support_classes.c
+++ workbench/libs/muimaster/support_classes.c
@@ -127,7 +127,6 @@ static const struct __MUIBuiltinClass *builtins[] =
     &_MUI_Gauge_desc,
     &_MUI_Aboutmui_desc,
     &_MUI_Settingsgroup_desc,
-    &_MUI_Settings_desc,
     &_MUI_Imageadjust_desc,
     &_MUI_Popimage_desc,
     &_MUI_Scale_desc,
