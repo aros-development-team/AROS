1b8deba3c2e9b4f33a5c06495da58697d6d6ae52
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-06-10 19:31:01 +0000

    Formatting changes.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@17940 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/prefs/font/args.c
+++ workbench/prefs/font/args.c
@@ -36,31 +36,31 @@ struct RDArgs * getArguments(void)
     // yes, you need it :) [by falemagn]
 
     for(a = 0; a < NUM_ARGS; a++)
-	argArray[a] = NULL;
+        argArray[a] = NULL;
 
     // We need an AROS "IPTR" class pointer here! An IPTR is guaranteed to be AmigaOS "ULONG" compatible?
 
     if((tmpReadArgs = ReadArgs("FROM,EDIT/S,USE/S,SAVE/S,PUBSCREEN/K", argArray, NULL)))
     {
-	for(a = 0; a < NUM_ARGS; a++)
-	{
-	    if(argArray[a])
-	    {
-		kprintf("%d is set!\n", a);
+        for(a = 0; a < NUM_ARGS; a++)
+        {
+            if(argArray[a])
+            {
+                kprintf("%d is set!\n", a);
 
-		/* ARG_PUBSCREEN is set (currently) set to NULL by Fonts Preferences.
-		   If not set, it'll default to Workbench (NULL). If we can't open
-		   the specified screen, should we try the Workbench screen instead? */
+                /* ARG_PUBSCREEN is set (currently) set to NULL by Fonts Preferences.
+                   If not set, it'll default to Workbench (NULL). If we can't open
+                   the specified screen, should we try the Workbench screen instead? */
 
-		if(a == ARG_PUBSCREEN)
-		    kprintf("pubscreen is %s!\n", (char *)argArray[ARG_PUBSCREEN]);
-	    }
-	}
+                if(a == ARG_PUBSCREEN)
+                    kprintf("pubscreen is %s!\n", (char *)argArray[ARG_PUBSCREEN]);
+            }
+        }
 
-	return tmpReadArgs;
+        return tmpReadArgs;
     }
     else
-	return NULL;
+        return NULL;
 }
 
 // Deal with shell arguments. If failure, should we quit or not? If started
@@ -71,42 +71,42 @@ UBYTE processArguments(void)
     extern struct FontPrefs *fp_Current[3];
 
     if(!(readArgs = getArguments()))
-	PrintFault(IoErr(), NULL);
+        PrintFault(IoErr(), NULL);
 
  /* If FROM is set, then also check for the USE and SAVE keywords - but only then. There isn't
     any point in just replacing the same settings with the very same values? */
 
     if(argArray[ARG_FROM])
     {
-	if(!(FP_Read((CONST_STRPTR) argArray[ARG_FROM], fp_Current)))
-	    return(APP_FAIL);
+        if(!(FP_Read((CONST_STRPTR) argArray[ARG_FROM], fp_Current)))
+            return(APP_FAIL);
 
      /* If USE or SAVE is set, write the FROM file to ENV: and/or ENVARC: and then quit. Is this
-	what the "Classic" Font Preferences does? Look it up! (As a side note, if FILE is not
-	found, the old settings will be overwritten with default values. Should we avoid this and
-	implement some error checking in writeIFF() ? What if FROM is not set? Should we still 
-	react for USE and SAVE (which we currently don't)? Request for comments to author! */
+        what the "Classic" Font Preferences does? Look it up! (As a side note, if FILE is not
+        found, the old settings will be overwritten with default values. Should we avoid this and
+        implement some error checking in writeIFF() ? What if FROM is not set? Should we still 
+        react for USE and SAVE (which we currently don't)? Request for comments to author! */
 
-	if(argArray[ARG_USE] || argArray[ARG_SAVE])
-	{
-	    if(!(FP_Write("ENV:sys/font.prefs", fp_Current)))
-		return(APP_FAIL);
+        if(argArray[ARG_USE] || argArray[ARG_SAVE])
+        {
+            if(!(FP_Write("ENV:sys/font.prefs", fp_Current)))
+                return(APP_FAIL);
 
-	    if(argArray[ARG_SAVE])
-		if(!(FP_Write("ENVARC:sys/font.prefs", fp_Current)))
-		    return(APP_FAIL);
+            if(argArray[ARG_SAVE])
+                if(!(FP_Write("ENVARC:sys/font.prefs", fp_Current)))
+                    return(APP_FAIL);
 
-	    // Don't launch the rest of the program, just exit
-	    return(APP_STOP);
-	}
+            // Don't launch the rest of the program, just exit
+            return(APP_STOP);
+        }
     }
     else
-	if(!(FP_Read("ENV:sys/font.prefs", fp_Current)))
-	    return(APP_FAIL);
+        if(!(FP_Read("ENV:sys/font.prefs", fp_Current)))
+            return(APP_FAIL);
 
     // What is "EDIT" supposed to do? Look it up!
     if(argArray[ARG_EDIT])
-	kprintf("EDIT keyword set!\n");
+        kprintf("EDIT keyword set!\n");
 
     return APP_RUN;
 }
