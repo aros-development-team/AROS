b7d5989791c33d2e0cca251f2698ca5c03a38455
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-06-21 16:01:51 +0000

    Removed usage of archtool.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18052 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/mathieeesingbas/ieeespneg.c
@@ -0,0 +1,63 @@
+/*
+    Copyright © 1995-2003, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include "mathieeesingbas_intern.h"
+
+/*
+    FUNCTION
+        Switch the sign of the given ieeesp number
+
+    RESULT
+        -y
+
+        Flags:
+          zero     : result is zero
+          negative : result is negative
+          overflow : 0
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+
+    INTERNALS
+      ALGORITHM:
+        Return -0 if y == 0.
+        Otherwise flip the sign-bit.
+
+    HISTORY
+*/
+
+AROS_LH1(float, IEEESPNeg,
+    AROS_LHA(float, y, D0),
+    struct MathIeeeSingBasBase *, MathIeeeSingBasBase, 10, Mathieeesingbas
+)
+{
+    AROS_LIBFUNC_INIT
+    
+  if (0 == y || 0x80000000 == y)
+  {
+    SetSR( Zero_Bit, Zero_Bit | Negative_Bit | Overflow_Bit);
+    return (IEEESPSign_Mask ^ y);
+  }
+
+  /* flip sign-bit */
+  y ^= IEEESPSign_Mask;
+
+  if(y < 0)
+  /* result is negative */
+    SetSR(Negative_Bit, Zero_Bit | Negative_Bit | Overflow_Bit);
+  else
+  /* result is positive */
+    SetSR(0, Zero_Bit | Overflow_Bit | Negative_Bit );
+
+  return y;
+
+    AROS_LIBFUNC_EXIT
+}
---
+++ rom/mathieeesingbas/ieeesptst.c
@@ -0,0 +1,70 @@
+/*
+    Copyright © 1995-2003, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include "mathieeesingbas_intern.h"
+
+/*
+    FUNCTION
+        Compare a ieeesp-number against zero.
+
+    RESULT
+	<code>
+        +1 : y > 0.0
+         0 : y = 0.0
+        -1 : y < 0.0
+
+        Flags:
+          zero     : result is zero
+          negative : result is negative
+          overflow : 0
+	</code>
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+
+    INTERNALS
+      ALGORITHM:
+	<code>
+        Sign is negative: return -1
+        y == 0          : return 0
+        Otherwise       : return 1
+	</code>
+
+    HISTORY
+*/
+
+AROS_LH1(LONG, IEEESPTst,
+    AROS_LHA(float, y, D0),
+    struct MathIeeeSingBasBase *, MathIeeeSingBasBase, 8, Mathieeesingbas
+)
+{
+    AROS_LIBFUNC_INIT
+    
+  /* y is negative */
+  if (y < 0)
+  {
+    SetSR(Negative_Bit, Zero_Bit | Overflow_Bit | Negative_Bit);
+    return -1;
+  }
+
+  /* fnum1 is zero */
+  if (0 == y)
+  {
+    SetSR(Zero_Bit, Zero_Bit | Overflow_Bit | Negative_Bit);
+    return 0;
+  }
+
+  /* fnum1 is positive */
+  SetSR(0, Zero_Bit | Overflow_Bit | Negative_Bit );
+  return 1;
+
+    AROS_LIBFUNC_EXIT
+}
