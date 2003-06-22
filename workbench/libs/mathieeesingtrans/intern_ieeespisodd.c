dc1dc12385f5e84237db9320c3010c9c16f95237
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-06-22 12:17:58 +0000

    Fixed formatting.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18071 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/mathieeesingtrans/intern_ieeespisodd.c
+++ workbench/libs/mathieeesingtrans/intern_ieeespisodd.c
@@ -1,9 +1,6 @@
 /*
-    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2003, The AROS Development Team. All rights reserved.
     $Id$
-
-    Desc:
-    Lang: english
 */
 
 #include <libraries/mathieeesp.h>
@@ -14,14 +11,11 @@
 #include <exec/types.h>
 #include "mathieeesingtrans_intern.h"
 
-
 LONG intern_IEEESPisodd(LONG fnum)
 {
-  LONG Exponent = ((fnum & IEEESPExponent_Mask) >> 23) - 0x7f;
-  LONG Mask = (0x00800000 >> Exponent);
-
-  if ((fnum & Mask) != 0)
-    return TRUE;
-  else
-    return FALSE;
+    LONG Exponent = ((fnum & IEEESPExponent_Mask) >> 23) - 0x7f;
+    LONG Mask = (0x00800000 >> Exponent);
+    
+    if ((fnum & Mask) != 0) return TRUE;
+    else                    return FALSE;
 } /* intern_IEEESPisodd  */
