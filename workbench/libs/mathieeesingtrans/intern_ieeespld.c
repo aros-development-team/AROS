f65b67790aff91c93ecb9d1ff93763891c4892b9
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-06-22 12:17:58 +0000

    Fixed formatting.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18071 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/mathieeesingtrans/intern_ieeespld.c
+++ workbench/libs/mathieeesingtrans/intern_ieeespld.c
@@ -1,11 +1,7 @@
 /*
-    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2003, The AROS Development Team. All rights reserved.
     $Id$
-
-    Desc:
-    Lang: english
 */
-
 
 #include <libraries/mathieeesp.h>
 #include <aros/libcall.h>
@@ -15,50 +11,51 @@
 #include <exec/types.h>
 #include "mathieeesingtrans_intern.h"
 
-
-LONG intern_IEEESPLd(struct MathIeeeSingTransBase * MathIeeeSingTransBase, ULONG fnum)
+LONG intern_IEEESPLd
+(
+    struct MathIeeeSingTransBase *MathIeeeSingTransBase, ULONG fnum
+)
 {
-  ULONG i = 0, Res = 0;
-  ULONG Mask = 0;
-
-  if (onehalf == fnum)
-    return minusone;
-
-  while (Mask == 0 && i <= 23)
-  {
-    /* if (sqrtone < fnum) */
-    if ( 0x00b504f3 < ((fnum & IEEESPMantisse_Mask) | 0x00800000) )
+    ULONG i = 0, Res = 0;
+    ULONG Mask = 0;
+    
+    if (onehalf == fnum) return minusone;
+    
+    while (Mask == 0 && i <= 23)
     {
-      i++;
-      fnum = IEEESPMul(fnum, fnum);
+        /* if (sqrtone < fnum) */
+        if ( 0x00b504f3 < ((fnum & IEEESPMantisse_Mask) | 0x00800000) )
+        {
+            i++;
+            fnum = IEEESPMul(fnum, fnum);
+        }
+        else
+        {
+            Mask = 0x40000000;
+            fnum = IEEESPMul(fnum, fnum);
+            fnum+= 0x00800000;
+        }
     }
-    else
+    
+    while ((char) Mask != 0x40)
     {
-      Mask = 0x40000000;
-      fnum = IEEESPMul(fnum, fnum);
-      fnum+= 0x00800000;
+        if ( 0x00b504f3 < ((fnum & IEEESPMantisse_Mask) | 0x00800000) )
+        {
+            fnum = IEEESPMul(fnum, fnum);
+        }
+        else
+        {
+            Res |= Mask;
+            fnum = IEEESPMul(fnum, fnum);
+            fnum+= 0x00800000;
+        }
+        Mask >>= 1;
     }
-  }
-
-  while ((char) Mask != 0x40)
-  {
-    if ( 0x00b504f3 < ((fnum & IEEESPMantisse_Mask) | 0x00800000) )
-      fnum = IEEESPMul(fnum, fnum);
-    else
-    {
-      Res |= Mask;
-      fnum = IEEESPMul(fnum, fnum);
-      fnum+= 0x00800000;
-    }
-    Mask >>= 1;
-  }
-
-  /* for precision */
-
-  if ((char) Res < 0)
-    Res += 0x100;
-
-  Res >>= 8;
-
-  return (Res | ( (0x7e - i) << 23 ) | IEEESPSign_Mask );
+    
+    /* for precision */
+    if ((char) Res < 0) Res += 0x100;
+    
+    Res >>= 8;
+    
+    return (Res | ( (0x7e - i) << 23 ) | IEEESPSign_Mask );
 } /* intern_SPLd  */
