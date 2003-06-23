c2f63a406ddb7643f225ddc20395e6c6d6389e0f
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-06-23 17:07:52 +0000

    First stab at a (automatic) partitioning tool. WARNING! It does not (yet) ask the user for confirmation, so be careful! Nukes ide.device unit 0 and sets up a RDB partition table and a FFS partition using the whole disk.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18120 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/c/Partition/main.c
@@ -0,0 +1,104 @@
+/*
+    Copyright © 2003, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <utility/tagitem.h>
+#include <libraries/partition.h>
+#include <devices/trackdisk.h>
+
+#include <proto/exec.h>
+#include <proto/partition.h>
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DEBUG 0
+#include <aros/debug.h>
+
+int main()
+{
+    struct PartitionHandle *root = NULL,
+                           *rdbp = NULL;
+    struct DosEnvec         tableDE,
+                            partitionDE;
+    struct DriveGeometry    tableDG;
+    LONG                    rc;
+    LONG                    reserved = 0;
+    
+    memset(&tableDG, 0, sizeof(struct DriveGeometry));
+    
+    /* Step 1: Destroy the existing partitiontable, if any exists. */
+    root = OpenRootPartition("ide.device", 0);
+    if (DestroyPartitionTable(root) != 0)
+    {
+        printf("*** ERROR: Could not destroy partition table. Continuing anyway...\n");
+    }
+    CloseRootPartition(root);
+    
+    /* Step 2: Create a root RDB partition table. */
+    root = OpenRootPartition("ide.device", 0);
+    rc = CreatePartitionTable(root, PHPTT_RDB);
+    if (rc != 0)
+    {
+        printf("*** ERROR: Creating partition table failed. Aborting.\n");
+        exit(RETURN_FAIL); /* FIXME: take care of allocated resources... */
+    }
+    
+    /* Step 3: Create a RDB partition in the table. */
+    memset(&tableDE, 0, sizeof(struct DosEnvec));
+    memset(&partitionDE, 0, sizeof(struct DosEnvec));
+    
+    GetPartitionAttrsTags
+    (
+        root, 
+        
+        PT_DOSENVEC, &tableDE, 
+        PT_GEOMETRY, &tableDG,
+        
+        TAG_DONE
+    );
+    // FIXME: broken???
+    // GetPartitionTableAttrsTags(root, PTT_RESERVED, (IPTR) &reserved, TAG_DONE);
+    reserved = 2;
+    
+    CopyMem(&tableDE, &partitionDE, sizeof(struct DosEnvec));
+    
+    partitionDE.de_SizeBlock      = tableDG.dg_SectorSize >> 2;
+    partitionDE.de_Surfaces       = tableDG.dg_Heads;
+    partitionDE.de_BlocksPerTrack = tableDG.dg_TrackSectors;
+    partitionDE.de_BufMemType     = tableDG.dg_BufMemType;
+       
+    partitionDE.de_TableSize      = DE_DOSTYPE;
+    partitionDE.de_Reserved       = 2;
+    partitionDE.de_HighCyl        = tableDE.de_HighCyl;
+    partitionDE.de_LowCyl         = tableDE.de_LowCyl + reserved;
+    partitionDE.de_NumBuffers     = 100;
+    partitionDE.de_MaxTransfer    = 0xFFFFFF;
+    partitionDE.de_Mask           = 0xFFFFFFFE;
+    partitionDE.de_DosType        = ID_FFS_DISK;
+    
+    D(bug("* highcyl = %ld\n", partitionDE.de_HighCyl));
+    D(bug("* lowcyl = %ld\n", partitionDE.de_LowCyl));
+    D(bug("* table lowcyl = %ld\n", tableDE.de_LowCyl));
+    D(bug("* table reserved = %ld\n", reserved));
+    
+    rdbp = AddPartitionTags
+    (
+        root,
+        
+        PT_DOSENVEC, (IPTR) &partitionDE,
+        PT_NAME,     (IPTR) "DH0",
+        PT_BOOTABLE,        TRUE,
+        PT_AUTOMOUNT,       TRUE,
+        
+        TAG_DONE
+    );
+    
+    WritePartitionTable(root);
+    ClosePartitionTable(root);
+    CloseRootPartition(root);
+
+    return RETURN_OK;
+}
