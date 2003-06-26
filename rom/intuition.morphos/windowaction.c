0b5b1d6a2b2fed54a6f0021ad6c3bb9026e391dc
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-06-26 21:56:08 +0000

    Empty "implementation" of WindowAction() (a MOS extension).
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18162 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/intuition.morphos/windowaction.c
@@ -0,0 +1,23 @@
+/*
+    Copyright © 2003, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <utility/tagitem.h>
+
+#include "intuition_intern.h"
+
+AROS_LH3(void, WindowAction,
+    AROS_LHA(struct Window *, window, A0),
+    AROS_LHA(ULONG, action, D0),
+    AROS_LHA(struct TagItem *, tags, A1),
+    struct IntuitionBase *, IntuitionBase, 157, Intuition
+)
+{
+    AROS_LIBFUNC_INIT
+    
+#   warning TODO: Write intuition/WindowAction()
+    aros_print_not_implemented ("WindowAction");
+
+    AROS_LIBFUNC_EXIT
+} /* WindowAction */
