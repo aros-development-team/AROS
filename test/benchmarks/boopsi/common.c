87b073a3e6e57a252ed231fb3d8b7504a7915c54
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-06-27 10:54:16 +0000

    Split the domethod and newobject benchmarks into separate files.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18168 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/benchmarks/boopsi/common.c
@@ -0,0 +1,109 @@
+/*
+    Copyright © 2003, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#define MUIMASTER_YES_INLINE_STDARG
+
+#include <sys/time.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <exec/types.h>
+#include <utility/tagitem.h>
+#include <libraries/mui.h>
+
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <proto/intuition.h>
+#include <proto/muimaster.h>
+#include <proto/utility.h>
+
+/*** Instance data **********************************************************/
+struct Test_DATA
+{
+    ULONG td_Dummy1,
+          td_Dummy2,
+          td_Dummy3,
+          td_Dummy4;
+};
+
+/*** Methods ****************************************************************/
+#define MUIM_Test_Dummy (TAG_USER | 0x42)
+
+IPTR Test$OM_NEW
+(
+    Class *CLASS, Object *self, struct opSet *message 
+)
+{
+    struct Test_DATA *data = NULL;
+        
+    self = (Object *) DoSuperMethodA(CLASS, self, (Msg) message);
+    if (self == NULL) goto error;
+    
+    data = INST_DATA(CLASS, self);
+    data->td_Dummy1 = 42;
+    data->td_Dummy2 = 42;
+    data->td_Dummy3 = 42;
+    data->td_Dummy4 = 42;
+       
+    return (IPTR) self;
+    
+error:
+    
+    return NULL;
+}
+
+IPTR Test$MUIM_Test_Dummy
+(
+    Class *CLASS, Object *self, Msg message
+)
+{
+    struct Test_DATA *data = INST_DATA(CLASS, self);
+    
+    /* 
+        Need to do *something* so that the compiler doesn't optimize away
+        this function call completely...
+    */
+    data->td_Dummy1 += data->td_Dummy2; 
+    
+    return TRUE;
+}
+
+
+/*** Dispatcher *************************************************************/
+BOOPSI_DISPATCHER(IPTR, Test_Dispatcher, CLASS, self, message)
+{
+    switch (message->MethodID)
+    {
+        case OM_NEW:          return Test$OM_NEW(CLASS, self, (struct opSet *) message);
+        case MUIM_Test_Dummy: return Test$MUIM_Test_Dummy(CLASS, self, message);
+        default:              return DoSuperMethodA(CLASS, self, message);
+    }
+    
+    return NULL;
+}
+
+/*** Setup ******************************************************************/
+struct MUI_CustomClass *Test_CLASS;
+
+BOOL Test_Initialize()
+{
+    Test_CLASS = MUI_CreateCustomClass
+    (
+        NULL, MUIC_Notify, NULL, 
+        sizeof(struct Test_DATA), Test_Dispatcher
+    );
+
+    if (Test_CLASS != NULL)
+        return TRUE;
+    else
+        return FALSE;
+}
+
+void Test_Deinitialize()
+{
+    MUI_DeleteCustomClass(Test_CLASS);
+}
+
+#define TestObject BOOPSIOBJMACRO_START(Test_CLASS->mcc_Class)
