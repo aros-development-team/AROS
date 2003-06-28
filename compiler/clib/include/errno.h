23fad24786ee1d076f4c1bdbfb6e0417f417df98
falemagn <falemagn@gmail.com>
2003-06-28 18:24:16 +0000

    Include aros/system.h for __BEGIN_CDECL and friends
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18202 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/include/errno.h
+++ compiler/clib/include/errno.h
@@ -9,6 +9,8 @@
     Lang: english
 */
 
+#include <aros/system.h>
+
 #define EPERM		 1	/* Operation not permitted */
 #define ENOENT		 2	/* No such file or directory */
 #define ESRCH		 3	/* No such process */
