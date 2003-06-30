c281adf0d89911697bc8c60b991c13b4d08fce1a
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-06-30 21:20:19 +0000

    Tests to benchmarks performance of frequent allocations/deallocations of small amounts of memory.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18250 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/benchmarks/exec/allocvec.c
@@ -0,0 +1,46 @@
+/*
+    Copyright © 2003, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <sys/time.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <exec/types.h>
+#include <exec/memory.h>
+#include <proto/exec.h>
+
+int main()
+{
+    struct timeval  tv_start, 
+                    tv_end;
+    int             count   = 1000000;
+    double          elapsed = 0.0;
+    int             i;
+    APTR            memory;
+    
+    gettimeofday(&tv_start, NULL);
+    
+    for(i = 0; i < count; i++)
+    {    
+        memory = AllocVec(100, MEMF_ANY);
+        if (memory) FreeVec(memory);
+    }
+    
+    gettimeofday(&tv_end, NULL);
+    
+    elapsed = ((double)(((tv_end.tv_sec * 1000000) + tv_end.tv_usec) 
+            - ((tv_start.tv_sec * 1000000) + tv_start.tv_usec)))/1000000.;
+    
+    printf
+    (
+        "Elapsed time:           %f seconds\n"
+        "Number of allocations:  %d\n"
+        "Allocations per second: %f\n"
+        "Seconds per allocation: %f\n",
+        elapsed, count, (double) count / elapsed, (double) elapsed / count
+    );
+   
+    return 0;
+}
