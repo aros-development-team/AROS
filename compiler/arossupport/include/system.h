1f734926146a528954a5411a0ee8b5469a1d76b2
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-06-30 14:44:53 +0000

    changed formatting of some macros
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18229 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/arossupport/include/system.h
+++ compiler/arossupport/include/system.h
@@ -24,10 +24,10 @@
 #endif
 
 #if defined __GNUC__ && defined __GNUC_MINOR__
-# define __GNUC_PREREQ(maj, min) \
-        ((__GNUC__ << 16) + __GNUC_MINOR__ >= ((maj) << 16) + (min))
+#    define __GNUC_PREREQ(maj, min) \
+         ((__GNUC__ << 16) + __GNUC_MINOR__ >= ((maj) << 16) + (min))
 #else
-# define __GNUC_PREREQ(maj, min) 0
+#    define __GNUC_PREREQ(maj, min) 0
 #endif
 
 
