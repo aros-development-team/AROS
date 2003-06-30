18ee625f1bb466009c0facdfd60d7640c4ceaf12
dlc <dlc@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-06-30 21:08:32 +0000

    added preliminary listview page
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18249 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/prefs/Zune/listviewsp.c
@@ -0,0 +1,220 @@
+/*
+    Copyright © 2002, The AROS Development Team. 
+    All rights reserved.
+    
+    $Id$
+*/
+
+#include <graphics/gfx.h>
+#include <graphics/view.h>
+#include <clib/alib_protos.h>
+#include <libraries/asl.h>
+#include <libraries/mui.h>
+#include <proto/exec.h>
+#include <proto/graphics.h>
+#include <proto/utility.h>
+#include <proto/intuition.h>
+
+#ifdef __AROS__
+#include <proto/alib.h>
+#include <proto/intuition.h>
+#include <proto/muimaster.h>
+#endif
+
+#include "zunestuff.h"
+#include <string.h>
+
+/*  #define DEBUG 1 */
+/*  #include <aros/debug.h> */
+
+extern struct Library *MUIMasterBase;
+
+struct MUI_ListviewsPData
+{
+    Object *multi_cycle;
+    Object *refresh_cycle;
+    Object *smooth_checkmark;
+    Object *smooth_slider;
+
+    Object *fonts_normal_string;
+    Object *fonts_fixed_string;
+    Object *fonts_leading_slider;
+
+    Object *input_popframe;
+    Object *input_popimage;
+    Object *readonly_popframe;
+    Object *readonly_popimage;
+
+    Object *active_popimage;
+    Object *selected_popimage;
+    Object *activeselected_popimage;
+};
+
+static CONST_STRPTR multi_labels[] =
+{
+    "shifted",
+    "always",
+    NULL,
+};
+
+static CONST_STRPTR refresh_labels[] =
+{
+    "linear",
+    "mixed",
+    NULL,
+};
+
+static Object*MakeListviewspSlider (void)
+{
+    Object *obj = MUI_MakeObject(MUIO_Slider, (IPTR)"", 0, 9);
+    set(obj, MUIA_CycleChain, 1);
+    return obj;
+}
+
+static IPTR ListviewsP_New(struct IClass *cl, Object *obj, struct opSet *msg)
+{
+    struct MUI_ListviewsPData *data;
+    struct MUI_ListviewsPData d;
+    
+    obj = (Object *)DoSuperNew(cl, obj,
+			       MUIA_Group_Columns, 2,
+
+			       Child, VGroup,
+			       MUIA_Group_VertSpacing, 2,
+			       GroupFrameT("Control"),
+			       Child, HVSpace,
+			       Child, ColGroup(2),
+			       Child, Label("Multi:"),
+			       Child, d.multi_cycle =
+			       MakeCycle("Multi:", multi_labels),
+			       Child, Label("Refresh:"),
+			       Child, d.refresh_cycle =
+			       MakeCycle("Refresh:", refresh_labels),
+			       Child, Label("Smooth:"),
+			       Child, HGroup,
+			       Child, d.smooth_checkmark = MakeCheck(NULL),
+			       Child, d.smooth_slider = MakeListviewspSlider(),
+			       End, /* HGroup */
+			       End, /* ColGroup */
+			       Child, HVSpace,
+			       End, /* Control VGroup */
+
+			       Child, VGroup,
+			       GroupFrameT("Fonts"),
+			       Child, VSpace(0),
+			       Child, ColGroup(2),
+			       MUIA_Group_VertSpacing, 2,
+			       Child, Label("Normal:"),
+			       Child, PopaslObject,
+			       MUIA_Popasl_Type, ASL_FontRequest,
+			       MUIA_Popstring_String, d.fonts_normal_string = StringObject,
+			       MUIA_CycleChain, 1,
+			       StringFrame, 
+			       End, /* String */
+			       MUIA_Popstring_Button, PopButton(MUII_PopUp),
+			       End, /* PopaslObject */
+
+			       Child, Label("Fixed:"),
+			       Child, PopaslObject,
+			       MUIA_Popasl_Type, ASL_FontRequest,
+			       MUIA_Popstring_String, d.fonts_fixed_string = StringObject,
+			       MUIA_CycleChain, 1,
+			       StringFrame, 
+			       End, /* String */
+			       MUIA_Popstring_Button, PopButton(MUII_PopUp),
+			       End, /* PopaslObject */
+
+			       Child, Label("Leading:"),
+			       Child, d.fonts_leading_slider = MakeListviewspSlider(),
+
+			       End, /* ColGroup */
+			       Child, VSpace(0),
+			       End, /* Fonts */
+
+			       Child, ColGroup(3),
+			       MUIA_Group_VertSpacing, 2,
+			       GroupFrameT("Design"),
+			       Child, FreeLabel("Input\nLists:"),
+			       Child, d.input_popframe = MakePopframe(),                  
+			       Child, d.input_popimage = MakeBackgroundPopimage(),
+			       Child, FreeLabel("Readonly\nLists:"),
+			       Child, d.readonly_popframe = MakePopframe(),                  
+			       Child, d.readonly_popimage = MakeBackgroundPopimage(),
+			       Child, HVSpace,
+			       Child, CLabel("Frame"),
+			       Child, CLabel("Background"),
+			       End, /* Design ColGroup(3) */
+
+			       Child, ColGroup(3),
+			       MUIA_Group_VertSpacing, 2,
+			       GroupFrameT("Cursor"),
+			       Child, FreeLabel("Active:"),
+			       Child, d.active_popimage = MakeBackgroundPopimage(),
+			       Child, VGroup,
+			       Child, HVSpace,
+			       Child, Label("Active &\nSelected:"),
+			       End, /* VGroup */
+			       Child, FreeLabel("Selected:"),
+			       Child, d.selected_popimage = MakeBackgroundPopimage(),
+			       Child, d.activeselected_popimage = MakeBackgroundPopimage(),
+			       End, /* Cursor ColGroup */
+    	TAG_MORE, msg->ops_AttrList);
+
+    if (!obj) return FALSE;
+    
+    data = INST_DATA(cl, obj);
+    *data = d;
+    set(data->refresh_cycle, MUIA_CycleChain, 1);
+    set(data->multi_cycle, MUIA_CycleChain, 1);
+
+    return (IPTR)obj;
+}
+
+
+/*
+ * MUIM_Settingsgroup_ConfigToGadgets
+ */
+static IPTR ListviewsP_ConfigToGadgets(struct IClass *cl, Object *obj,
+				    struct MUIP_Settingsgroup_ConfigToGadgets *msg)
+{
+    struct MUI_ListviewsPData *data = INST_DATA(cl, obj);
+    STRPTR spec;
+
+    return 1;    
+}
+
+
+/*
+ * MUIM_Settingsgroup_ConfigToGadgets
+ */
+static IPTR ListviewsP_GadgetsToConfig(struct IClass *cl, Object *obj,
+				    struct MUIP_Settingsgroup_GadgetsToConfig *msg)
+{
+    struct MUI_ListviewsPData *data = INST_DATA(cl, obj);
+    STRPTR str;
+
+    return TRUE;
+}
+
+
+BOOPSI_DISPATCHER(IPTR, ListviewsP_Dispatcher, cl, obj, msg)
+{
+    switch (msg->MethodID)
+    {
+	case OM_NEW: return ListviewsP_New(cl, obj, (struct opSet *)msg);
+	case MUIM_Settingsgroup_ConfigToGadgets: return ListviewsP_ConfigToGadgets(cl,obj,(APTR)msg);break;
+	case MUIM_Settingsgroup_GadgetsToConfig: return ListviewsP_GadgetsToConfig(cl,obj,(APTR)msg);break;
+    }
+    
+    return DoSuperMethodA(cl, obj, msg);
+}
+
+/*
+ * Class descriptor.
+ */
+const struct __MUIBuiltinClass _MUIP_Listviews_desc = { 
+    "Listviews",
+    MUIC_Group,
+    sizeof(struct MUI_ListviewsPData),
+    (void*)ListviewsP_Dispatcher 
+};
