f7b4f38b41aae9f38747a1d879a6df3a6fd145c3
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-07-01 17:23:35 +0000

    Skeleton files for the remaining v44 functions.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18269 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/icon/changetoselectediconcolor.c
@@ -0,0 +1,49 @@
+/*
+    Copyright © 1995-2003, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <exec/types.h>
+#include <workbench/icon.h>
+#include <proto/icon.h>
+
+#include "icon_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+
+    AROS_LH1(VOID, ChangeToSelectedIconColor,
+
+/*  SYNOPSIS */
+        AROS_LHA(struct ColorRegister *, cr, A0),
+
+/*  LOCATION */
+        struct Library *, IconBase, 33, Icon)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+    AROS_LIBBASE_EXT_DECL(struct Library *, IconBase)
+    
+#   warning TODO: Implement icon/ChangeToSelectedIconColor()
+    aros_print_not_implemented("icon/ChangeToSelectedIconColor()");
+    
+    AROS_LIBFUNC_EXIT
+} /* ChangeToSelectedIconColor() */
---
+++ workbench/libs/icon/iconcontrol.c
@@ -0,0 +1,51 @@
+/*
+    Copyright © 1995-2003, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <exec/types.h>
+#include <workbench/icon.h>
+#include <utility/tagitem.h>
+#include <proto/icon.h>
+
+#include "icon_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+
+    AROS_LH2(ULONG, IconControlA,
+
+/*  SYNOPSIS */
+        AROS_LHA(struct DiskObject *, icon, A0),
+        AROS_LHA(struct Tagitem *,    tags, A1),
+
+/*  LOCATION */
+        struct Library *, IconBase, 26, Icon)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+    AROS_LIBBASE_EXT_DECL(struct Library *, IconBase)
+    
+#   warning TODO: Implement icon/IconControlA()
+    aros_print_not_implemented("icon/IconControlA()");
+    
+    AROS_LIBFUNC_EXIT
+} /* IconControlA() */
