7c4725c94eb668556e8e97143b84499c2fb5f218
dlc <dlc@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-07-06 20:27:26 +0000

    minmax adjusted to the one of the list object. Cosmetic cleanups.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18412 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/classes/listview.c
+++ workbench/libs/muimaster/classes/listview.c
@@ -22,7 +22,7 @@ extern struct Library *MUIMasterBase;
 
 struct MUI_ListviewData
 {
-    Object *list, *group, *vert/*  , */ /*  *horiz, *//*   *button */;
+    Object *list, *group, *vert;
     struct Hook *layout_hook;
     struct Hook hook;
 };
@@ -34,40 +34,28 @@ ULONG Listview_Layout_Function(struct Hook *hook, Object *obj, struct MUI_Layout
     {
 	case    MUILM_MINMAX:
 		{
-		    /* Calulate the minmax dimension of the group,
+		    /* Calculate the minmax dimension of the group,
 		    ** We only have a fixed number of children, so we need no NextObject()
 		    */
-		    WORD maxxxxwidth = 0;
-		    WORD maxxxxheight = 0;
+		    lm->lm_MinMax.MinWidth = _minwidth(data->list) + _minwidth(data->vert);
+		    lm->lm_MinMax.DefWidth = _defwidth(data->list) + _defwidth(data->vert);
+		    lm->lm_MinMax.MaxWidth = _maxwidth(data->list) + _maxwidth(data->vert);
+		    lm->lm_MinMax.MaxWidth = MIN(lm->lm_MinMax.MaxWidth, MUI_MAXMAX);
 
-		    maxxxxwidth = _minwidth(data->list) + _minwidth(data->vert);
-		    if (/*  _minwidth(data->horiz) + */ _minwidth(data->vert) > maxxxxwidth) maxxxxwidth = /*  _minwidth(data->horiz) */ + _minwidth(data->vert);
-		    lm->lm_MinMax.MinWidth = maxxxxwidth;
-
-		    maxxxxheight = _minheight(data->list) /*  + _minheight(data->horiz) */;
-		    if (_minheight(data->vert) /*  + _minheight(data->horiz) */ > maxxxxheight) maxxxxheight = _minheight(data->vert) /*  + _minheight(data->horiz) */;
-		    lm->lm_MinMax.MinHeight = maxxxxheight;
-
-		    maxxxxwidth = _defwidth(data->list) + _defwidth(data->vert);
-		    /*  if (_defwidth(data->horiz) > maxxxxwidth) maxxxxwidth = _defwidth(data->horiz); */
-		    lm->lm_MinMax.DefWidth = maxxxxwidth;
-
-		    maxxxxheight = _defheight(data->list)/*   + _defheight(data->horiz) */;
-		    if (_defheight(data->vert) > maxxxxheight) maxxxxheight = _defheight(data->vert);
-		    lm->lm_MinMax.DefHeight = maxxxxheight;
-
-		    lm->lm_MinMax.MaxWidth  = MUI_MAXMAX;
-		    lm->lm_MinMax.MaxHeight = MUI_MAXMAX;
+		    lm->lm_MinMax.MinHeight = MAX(_minheight(data->list), _minheight(data->vert));
+		    lm->lm_MinMax.DefHeight = MAX(_defheight(data->list), lm->lm_MinMax.MinHeight);
+		    lm->lm_MinMax.MaxHeight = MIN(_maxheight(data->list), _maxheight(data->vert));
+		    lm->lm_MinMax.MaxHeight = MIN(lm->lm_MinMax.MaxHeight, MUI_MAXMAX);
 		    return 0;
 		}
 
 		case MUILM_LAYOUT:
 		{
-		    /* Now place the objects between (0,0,lm->lm_Layout.Width-1,lm->lm_Layout.Height-1)
+		    /* Now place the objects between
+		     * (0, 0, lm->lm_Layout.Width - 1, lm->lm_Layout.Height - 1)
 		    */
 
 		    LONG vert_width = _minwidth(data->vert);
-		    /*  LONG horiz_height = _minheight(data->horiz); */
 		    LONG lay_width = lm->lm_Layout.Width;
 		    LONG lay_height = lm->lm_Layout.Height;
 		    LONG cont_width;
@@ -75,18 +63,14 @@ ULONG Listview_Layout_Function(struct Hook *hook, Object *obj, struct MUI_Layout
 
 		    /* We need all scrollbars and the button */
 		    set(data->vert, MUIA_ShowMe, TRUE); /* We could also overload MUIM_Show... */
-		    /*  set(data->horiz, MUIA_ShowMe, TRUE); */
-		    /*  set(data->button, MUIA_ShowMe, TRUE); */
 		    cont_width = lay_width - vert_width;
-		    cont_height = lay_height /*  - horiz_height */;
+		    cont_height = lay_height;
 
 		    MUI_Layout(data->vert, cont_width, 0, vert_width, cont_height,0);
-		    /*  if (xget(data->horiz, MUIA_ShowMe)) */
-/*  			MUI_Layout(data->horiz, 0, cont_height, cont_width, horiz_height, 0); */
-		    /*  MUI_Layout(data->button, cont_width, cont_height, vert_width, horiz_height, 0); */
 
-		    /* Layout the group a second time, note that setting _mwidth() and _mheight() should be enough, or we invent a new flag */
-		    MUI_Layout(data->list,0,0,cont_width,cont_height,0);
+		    /* Layout the group a second time, note that setting _mwidth() and
+		       _mheight() should be enough, or we invent a new flag */
+		    MUI_Layout(data->list, 0, 0, cont_width, cont_height, 0);
 		    return 1;
 		}
     }
@@ -111,17 +95,12 @@ ULONG Listview_Function(struct Hook *hook, APTR dummyobj, void **msg)
 		nnset(data->list,MUIA_List_VertProp_First,val);
 		break;
 
-	case	2:
-		{
-		    /*  get(data->horiz,MUIA_Prop_First,&val); */
-//		    SetAttrs(data->contents,MUIA_Virtgroup_Left, val, MUIA_NoNotify, TRUE, MUIA_Group_Forward, FALSE, TAG_DONE);
-		    break;
-		}
-	case	3:/*   nnset(data->horiz, MUIA_Prop_First, val) */; break;
-
-	case	LIST_VERT_FIRST: nnset(data->vert, MUIA_Prop_First, val); break;
-	case	LIST_VERT_VISIBLE: nnset(data->vert, MUIA_Prop_Visible, val); break;
-	case	LIST_VERT_ENTRIES: nnset(data->vert, MUIA_Prop_Entries, val); break;
+	case	LIST_VERT_FIRST:
+	    nnset(data->vert, MUIA_Prop_First, val); break;
+	case	LIST_VERT_VISIBLE:
+	    nnset(data->vert, MUIA_Prop_Visible, val); break;
+	case	LIST_VERT_ENTRIES:
+	    nnset(data->vert, MUIA_Prop_Entries, val); break;
     }
     return 0;
 }
@@ -134,7 +113,7 @@ static IPTR Listview_New(struct IClass *cl, Object *obj, struct opSet *msg)
     struct MUI_ListviewData   *data;
     struct TagItem *tag, *tags;
     struct Hook *layout_hook;
-    Object *group, *vert/*  , *horiz, *button */;
+    Object *group, *vert;
     Object *list = (Object*)GetTagData(MUIA_Listview_List, NULL, msg->ops_AttrList);
     LONG entries,first,visible;
     if (!list) return NULL;
@@ -147,16 +126,14 @@ static IPTR Listview_New(struct IClass *cl, Object *obj, struct opSet *msg)
 
     obj = (Object *)DoSuperNewTags(cl, obj, NULL,
 	MUIA_Group_Horiz, FALSE,
-	MUIA_InnerLeft,0,
-	MUIA_InnerRight,0,
+	MUIA_InnerLeft, 0,
+	MUIA_InnerRight, 0,
 	Child, group = GroupObject,
-	    MUIA_InnerLeft,0,
-	    MUIA_InnerRight,0,
+	    MUIA_InnerLeft, 0,
+	    MUIA_InnerRight, 0,
 	    MUIA_Group_LayoutHook, layout_hook,
 	    Child, list,
 	    Child, vert = ScrollbarObject, MUIA_Group_Horiz, FALSE, End,
-/*  	    Child, horiz = ScrollbarObject, MUIA_Group_Horiz, TRUE, End, */
-/*  	    Child, button = ScrollbuttonObject, End, */
 	    End,
 	TAG_DONE);
 
@@ -170,8 +147,6 @@ static IPTR Listview_New(struct IClass *cl, Object *obj, struct opSet *msg)
     layout_hook->h_Data = data;
     data->list = list;
     data->vert = vert;
-/*      data->horiz = horiz; */
-/*      data->button = button; */
     data->group = group;
     data->layout_hook = layout_hook;
 
@@ -197,10 +172,17 @@ static IPTR Listview_New(struct IClass *cl, Object *obj, struct opSet *msg)
 	MUIA_Prop_Entries, entries,
 	TAG_DONE);
 
-    DoMethod(vert, MUIM_Notify, MUIA_Prop_First, MUIV_EveryTime, (IPTR)obj, 4, MUIM_CallHook, (IPTR)&data->hook, PROP_VERT_FIRST, MUIV_TriggerValue);
-    DoMethod(list, MUIM_Notify, MUIA_List_VertProp_First, MUIV_EveryTime, (IPTR)obj, 4, MUIM_CallHook, (IPTR)&data->hook, LIST_VERT_FIRST, MUIV_TriggerValue);
-    DoMethod(list, MUIM_Notify, MUIA_List_VertProp_Visible, MUIV_EveryTime, (IPTR)obj, 4, MUIM_CallHook, (IPTR)&data->hook, LIST_VERT_VISIBLE, MUIV_TriggerValue);
-    DoMethod(list, MUIM_Notify, MUIA_List_VertProp_Entries, MUIV_EveryTime, (IPTR)obj, 4, MUIM_CallHook, (IPTR)&data->hook, LIST_VERT_ENTRIES, MUIV_TriggerValue);
+    DoMethod(vert, MUIM_Notify, MUIA_Prop_First, MUIV_EveryTime, (IPTR)obj, 4,
+	     MUIM_CallHook, (IPTR)&data->hook, PROP_VERT_FIRST, MUIV_TriggerValue);
+    DoMethod(list, MUIM_Notify, MUIA_List_VertProp_First, MUIV_EveryTime,
+	     (IPTR)obj, 4, MUIM_CallHook, (IPTR)&data->hook, LIST_VERT_FIRST,
+	     MUIV_TriggerValue);
+    DoMethod(list, MUIM_Notify, MUIA_List_VertProp_Visible, MUIV_EveryTime,
+	     (IPTR)obj, 4, MUIM_CallHook, (IPTR)&data->hook, LIST_VERT_VISIBLE,
+	     MUIV_TriggerValue);
+    DoMethod(list, MUIM_Notify, MUIA_List_VertProp_Entries, MUIV_EveryTime,
+	     (IPTR)obj, 4, MUIM_CallHook, (IPTR)&data->hook, LIST_VERT_ENTRIES,
+	     MUIV_TriggerValue);
 
     return (IPTR)obj;
 }
