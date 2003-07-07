f6e3de711d3870d4ad75ae2a07923543a8d32045
sebauer <sebauer@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-07-07 15:58:58 +0000

    Added DoSuperNewTags().
    The .h file defines now all known classes to be built in. Should be probably be moved into a separate config include file
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18432 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/support_amigaos.c
+++ workbench/libs/muimaster/support_amigaos.c
@@ -1,15 +1,34 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
+#include <clib/alib_protos.h>
+#include <proto/exec.h>
 
 #include "support_amigaos.h"
 
-char *StrDup(char *x)
+char *StrDup(const char *x)
 {
     char *dup;
     if (!x) return NULL;
     dup = AllocVec(strlen(x) + 1, MEMF_PUBLIC);
     if (dup) CopyMem((x), dup, strlen(x) + 1);
     return dup;
+}
+
+Object *DoSuperNewTagList(struct IClass *cl, Object *obj,void *dummy, struct TagItem *tags)
+{
+	  return (Object*)DoSuperMethod(cl,obj,OM_NEW,tags,NULL);
+}
+
+Object *DoSuperNewTags(struct IClass *cl, Object *obj, void *dummy, ...)
+{
+    va_list argptr;
+    va_start(argptr,dummy);
+    obj = DoSuperNewTagList(cl,obj,dummy,(struct TagItem*)argptr);
+    va_end(argptr);
+    return obj;
 }
 
 int snprintf(char *buf, int size, const char *fmt, ...)
@@ -35,4 +54,5 @@ int strlcat(char *buf, char *src, int len)
 	    *buf++ = *src++;
 	*buf = 0;
     }
+    return 0; /* Actually don't know right rt here */
 }
