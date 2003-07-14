9421afea90819747d90232a973eb76788de93921
dlc <dlc@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-07-14 20:23:51 +0000

    made draggable by default
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18624 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/classes/popimage.c
+++ workbench/libs/muimaster/classes/popimage.c
@@ -42,12 +42,12 @@ IPTR Popimage__OM_NEW(struct IClass *cl, Object *obj, struct opSet *msg)
         InnerSpacing(4,4),
         MUIA_Background, MUII_ButtonBack,
         MUIA_InputMode,  MUIV_InputMode_RelVerify,
+        MUIA_Draggable,  TRUE,
         
         TAG_MORE, (IPTR) msg->ops_AttrList
     );
 
     if (!obj) return FALSE;
-    
     data = INST_DATA(cl, obj);
     data->wintitle = NULL;
     data->adjust_type = MUIV_Imageadjust_Type_All;
