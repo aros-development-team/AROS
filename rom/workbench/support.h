dccc9df5a564008e9d129bf0a4d6fceb83f67a0c
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-07-19 22:05:06 +0000

    + Whoops, I can't call StrDup() without a global SysBase variable. Made a   private version of the function here, which takes the libbase as a parameter. + Pass WorkbenchBase to the handler process using NP_UserData.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18688 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/workbench/support.h
+++ rom/workbench/support.h
@@ -11,15 +11,17 @@
 #include "workbench_intern.h"
 
 /*** Prototypes ************************************************************/
-BOOL   StartHandler(struct WorkbenchBase *WorkbenchBase);
-
+BOOL   __StartHandler(struct WorkbenchBase *WorkbenchBase);
 VOID   __AddHiddenDevice(STRPTR name, struct WorkbenchBase *WorkbenchBase);
 VOID   __RemoveHiddenDevice(STRPTR name, struct WorkbenchBase *WorkbenchBase);
 STRPTR __AllocateNameFromLock(BPTR lock, struct WorkbenchBase *WorkbenchBase);
+STRPTR __StrDup(CONST_STRPTR string, struct WorkbenchBase *WorkbenchBase);
 
 /*** Macros *****************************************************************/
+#define StartHandler() (__StartHandler(WorkbenchBase))
 #define AddHiddenDevice(name) (__AddHiddenDevice((name), WorkbenchBase))
 #define RemoveHiddenDevice(name) (__RemoveHiddenDevice((name), WorkbenchBase))
 #define AllocateNameFromLock(lock) (__AllocateNameFromLock((lock), WorkbenchBase))
+#define StrDup(string) (__StrDup((string), WorkbenchBase))
 
 #endif /* __WORKBENCH_SUPPORT_H__ */
