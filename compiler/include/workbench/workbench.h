065e3f9dcd675da2a261739a3f2ad2186ed7d77d
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-07-22 18:18:58 +0000

    Formatting changes.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18725 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/workbench/workbench.h
+++ compiler/include/workbench/workbench.h
@@ -2,11 +2,8 @@
 #define WORKBENCH_WORKBENCH_H
 
 /*
-    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2003, The AROS Development Team. All rights reserved.
     $Id$
-
-    Desc: Workbench structures
-    Lang: english
 */
 
 #ifndef EXEC_LISTS_H
@@ -34,12 +31,11 @@
 #endif
 
 /*** Workbench library name *************************************************/
-
 #define WORKBENCHNAME           "workbench.library"
 
 /*** Structures and associated definitions **********************************/
-
-struct DrawerData {
+struct DrawerData
+{
     struct NewWindow dd_NewWindow;
     LONG             dd_CurrentX;
     LONG             dd_CurrentY;
@@ -64,35 +60,36 @@ struct DrawerData {
 
 struct DiskObject
 {
-    UWORD               do_Magic;
-    UWORD               do_Version;
-    struct Gadget       do_Gadget;
-    UBYTE               do_Type;        /* see below */
-    STRPTR              do_DefaultTool;
-    STRPTR            * do_ToolTypes;
-    LONG                do_CurrentX;
-    LONG                do_CurrentY;
-    struct DrawerData * do_DrawerData;
-    STRPTR              do_ToolWindow;
-    LONG                do_StackSize;
+    UWORD              do_Magic;
+    UWORD              do_Version;
+    struct Gadget      do_Gadget;
+    UBYTE              do_Type;        /* see below */
+    STRPTR             do_DefaultTool;
+    STRPTR            *do_ToolTypes;
+    LONG               do_CurrentX;
+    LONG               do_CurrentY;
+    struct DrawerData *do_DrawerData;
+    STRPTR             do_ToolWindow;
+    LONG               do_StackSize;
 };
 
-#define WBDISK    1
-#define WBDRAWER  2
-#define WBTOOL    3
-#define WBPROJECT 4
-#define WBGARBAGE 5
-#define WBDEVICE  6
-#define WBKICK    7
-#define WBAPPICON 8
+#define WBDISK    (1)
+#define WBDRAWER  (2)
+#define WBTOOL    (3)
+#define WBPROJECT (4)
+#define WBGARBAGE (5)
+#define WBDEVICE  (6)
+#define WBKICK    (7)
+#define WBAPPICON (8)
 
-#define WB_DISKVERSION  1
-#define WB_DISKREVISION 1
-#define WB_DISKREVISIONMASK 0xFF
+#define WB_DISKVERSION  (1)
+#define WB_DISKREVISION (1)
+#define WB_DISKREVISIONMASK (0xFF)
 
-#define WB_DISKMAGIC 0xE310
+#define WB_DISKMAGIC (0xE310)
 
-struct FreeList {
+struct FreeList
+{
     WORD        fl_NumFree;
     struct List fl_MemList;
 };
@@ -101,7 +98,8 @@ struct FreeList {
 #define GFLG_GADGBACKFILL 0x0001
 #define NO_ICON_POSITION  0x80000000
 
-struct AppMessage {
+struct AppMessage
+{
     struct Message am_Message;
     UWORD          am_Type;     /* see below */
     ULONG          am_UserData;
@@ -140,7 +138,8 @@ struct AppMessage {
 #define AMCLASSICON_Selected   11
 #define AMCLASSICON_Unselected 12
 
-struct AppIconRenderMsg {
+struct AppIconRenderMsg
+{
     struct RastPort   *arm_RastPort;
     struct DiskObject *arm_Icon;
     STRPTR             arm_Label;
@@ -152,7 +151,8 @@ struct AppIconRenderMsg {
     ULONG              arm_State;
 };
 
-struct AppWindowDropZoneMsg {
+struct AppWindowDropZoneMsg
+{
     struct RastPort *adzm_RastPort;
     struct IBox      adzm_DropZoneBox;
     ULONG            adzm_ID;
@@ -164,7 +164,8 @@ struct AppWindowDropZoneMsg {
 #define ADZMACTION_Enter (0)
 #define ADZMACTION_Leave (1)
 
-struct IconSelectMsg {
+struct IconSelectMsg
+{
     ULONG           ism_Length;
     BPTR            ism_Drawer;
     STRPTR          ism_Name;
@@ -186,108 +187,100 @@ struct IconSelectMsg {
 #define ISMACTION_Stop     (3)
 
 /*** Private structures *****************************************************/
-
 struct AppWindow;
 struct AppWindowDropZone;
 struct AppIcon;
 struct AppMenuItem;
 
 /*** Start of workbench.library tags ****************************************/
-
-#define WBA_Dummy                          (TAG_USER+0xA000)
+#define WBA_BASE                          (TAG_USER+0xA000)
 
 /*** Tags for use with AddAppIconA() ****************************************/
-
 /* The different menu items the AppIcon responds to (BOOL) */
-#define WBAPPICONA_SupportsOpen            (WBA_Dummy+1)
-#define WBAPPICONA_SupportsCopy            (WBA_Dummy+2)
-#define WBAPPICONA_SupportsRename          (WBA_Dummy+3)
-#define WBAPPICONA_SupportsInformation     (WBA_Dummy+4)
-#define WBAPPICONA_SupportsSnapshot        (WBA_Dummy+5)
-#define WBAPPICONA_SupportsUnSnapshot      (WBA_Dummy+6)
-#define WBAPPICONA_SupportsLeaveOut        (WBA_Dummy+7)
-#define WBAPPICONA_SupportsPutAway         (WBA_Dummy+8)
-#define WBAPPICONA_SupportsDelete          (WBA_Dummy+9)
-#define WBAPPICONA_SupportsFormatDisk      (WBA_Dummy+10)
-#define WBAPPICONA_SupportsEmptyTrash      (WBA_Dummy+11)
+#define WBAPPICONA_SupportsOpen            (WBA_BASE+1)
+#define WBAPPICONA_SupportsCopy            (WBA_BASE+2)
+#define WBAPPICONA_SupportsRename          (WBA_BASE+3)
+#define WBAPPICONA_SupportsInformation     (WBA_BASE+4)
+#define WBAPPICONA_SupportsSnapshot        (WBA_BASE+5)
+#define WBAPPICONA_SupportsUnSnapshot      (WBA_BASE+6)
+#define WBAPPICONA_SupportsLeaveOut        (WBA_BASE+7)
+#define WBAPPICONA_SupportsPutAway         (WBA_BASE+8)
+#define WBAPPICONA_SupportsDelete          (WBA_BASE+9)
+#define WBAPPICONA_SupportsFormatDisk      (WBA_BASE+10)
+#define WBAPPICONA_SupportsEmptyTrash      (WBA_BASE+11)
 
 /* Propagate the AppIcons position back to original DiskObject (BOOL) */
-#define WBAPPICONA_PropagatePosition       (WBA_Dummy+12)
+#define WBAPPICONA_PropagatePosition       (WBA_BASE+12)
 
 /* Call this hook when rendering this AppIcon (struct Hook *) */
-#define WBAPPICONA_RenderHook              (WBA_Dummy+13)
+#define WBAPPICONA_RenderHook              (WBA_BASE+13)
 
 /* Notify the AppIcon when it's select state changes (BOOL) */
-#define WBAPPICONA_NotifySelectState        (WBA_Dummy+14)
+#define WBAPPICONA_NotifySelectState        (WBA_BASE+14)
 
 /*** Tags for use with AddAppMenuItemA() ************************************/
-
 /* Command key string for this AppMenu (STRPTR) */
-#define WBAPPMENUA_CommandKeyString         (WBA_Dummy+15)
+#define WBAPPMENUA_CommandKeyString         (WBA_BASE+15)
 
 /*** Tags for use with OpenWorkbenchObjectA() *******************************/
-
-#define WBOPENA_ArgLock                     (WBA_Dummy+16)
-#define WBOPENA_ArgName                     (WBA_Dummy+17)
+#define WBOPENA_ArgLock                     (WBA_BASE+16)
+#define WBOPENA_ArgName                     (WBA_BASE+17)
 
 /*** Tags for use with WorkbenchControlA() **********************************/
-
-#define WBCTRLA_IsOpen                      (WBA_Dummy+18)
-#define WBCTRLA_DuplicateSearchPath         (WBA_Dummy+19)
-#define WBCTRLA_FreeSearchPath              (WBA_Dummy+20)
-#define WBCTRLA_GetDefaultStackSize         (WBA_Dummy+21)
-#define WBCTRLA_SetDefaultStackSize         (WBA_Dummy+22)
-#define WBCTRLA_RedrawAppIcon               (WBA_Dummy+23)
-#define WBCTRLA_GetProgramList              (WBA_Dummy+24)
-#define WBCTRLA_FreeProgramList             (WBA_Dummy+25)
-#define WBCTRLA_GetSelectedIconList         (WBA_Dummy+36)
-#define WBCTRLA_FreeSelectedIconList        (WBA_Dummy+37)
-#define WBCTRLA_GetOpenDrawerList           (WBA_Dummy+38)
-#define WBCTRLA_FreeOpenDrawerList          (WBA_Dummy+39)
-#define WBCTRLA_GetHiddenDeviceList         (WBA_Dummy+42)
-#define WBCTRLA_FreeHiddenDeviceList        (WBA_Dummy+43)
-#define WBCTRLA_AddHiddenDeviceName         (WBA_Dummy+44)
-#define WBCTRLA_RemoveHiddenDeviceName      (WBA_Dummy+45)
-#define WBCTRLA_GetTypeRestartTime          (WBA_Dummy+47)
-#define WBCTRLA_SetTypeRestartTime          (WBA_Dummy+48)
+#define WBCTRLA_IsOpen                      (WBA_BASE+18)
+#define WBCTRLA_DuplicateSearchPath         (WBA_BASE+19)
+#define WBCTRLA_FreeSearchPath              (WBA_BASE+20)
+#define WBCTRLA_GetDefaultStackSize         (WBA_BASE+21)
+#define WBCTRLA_SetDefaultStackSize         (WBA_BASE+22)
+#define WBCTRLA_RedrawAppIcon               (WBA_BASE+23)
+#define WBCTRLA_GetProgramList              (WBA_BASE+24)
+#define WBCTRLA_FreeProgramList             (WBA_BASE+25)
+#define WBCTRLA_GetSelectedIconList         (WBA_BASE+36)
+#define WBCTRLA_FreeSelectedIconList        (WBA_BASE+37)
+#define WBCTRLA_GetOpenDrawerList           (WBA_BASE+38)
+#define WBCTRLA_FreeOpenDrawerList          (WBA_BASE+39)
+#define WBCTRLA_GetHiddenDeviceList         (WBA_BASE+42)
+#define WBCTRLA_FreeHiddenDeviceList        (WBA_BASE+43)
+#define WBCTRLA_AddHiddenDeviceName         (WBA_BASE+44)
+#define WBCTRLA_RemoveHiddenDeviceName      (WBA_BASE+45)
+#define WBCTRLA_GetTypeRestartTime          (WBA_BASE+47)
+#define WBCTRLA_SetTypeRestartTime          (WBA_BASE+48)
 
 /*** Tags for use with AddAppWindowDropZoneA() ******************************/
-
-#define WBDZA_Left                          (WBA_Dummy+26)
-#define WBDZA_RelRight                      (WBA_Dummy+27)
-#define WBDZA_Top                           (WBA_Dummy+28)
-#define WBDZA_RelBottom                     (WBA_Dummy+29)
-#define WBDZA_Width                         (WBA_Dummy+30)
-#define WBDZA_RelWidth                      (WBA_Dummy+31)
-#define WBDZA_Height                        (WBA_Dummy+32)
-#define WBDZA_RelHeight                     (WBA_Dummy+33)
-#define WBDZA_Box                           (WBA_Dummy+34)
-#define WBDZA_Hook                          (WBA_Dummy+35)
+#define WBDZA_Left                          (WBA_BASE+26)
+#define WBDZA_RelRight                      (WBA_BASE+27)
+#define WBDZA_Top                           (WBA_BASE+28)
+#define WBDZA_RelBottom                     (WBA_BASE+29)
+#define WBDZA_Width                         (WBA_BASE+30)
+#define WBDZA_RelWidth                      (WBA_BASE+31)
+#define WBDZA_Height                        (WBA_BASE+32)
+#define WBDZA_RelHeight                     (WBA_BASE+33)
+#define WBDZA_Box                           (WBA_BASE+34)
+#define WBDZA_Hook                          (WBA_BASE+35)
 
 /*** Reserved tags **********************************************************/
-
-#define WBA_Reserved1                       (WBA_Dummy+40)
-#define WBA_Reserved2                       (WBA_Dummy+41)
-#define WBA_Reserved3                       (WBA_Dummy+46)
-#define WBA_Reserved4                       (WBA_Dummy+49)
-#define WBA_Reserved5                       (WBA_Dummy+50)
-#define WBA_Reserved6                       (WBA_Dummy+51)
-#define WBA_Reserved7                       (WBA_Dummy+52)
-#define WBA_Reserved8                       (WBA_Dummy+53)
-#define WBA_Reserved9                       (WBA_Dummy+54)
-#define WBA_Reserved10                      (WBA_Dummy+55)
-#define WBA_Reserved11                      (WBA_Dummy+56)
-#define WBA_Reserved12                      (WBA_Dummy+57)
-#define WBA_Reserved13                      (WBA_Dummy+58)
-#define WBA_Reserved14                      (WBA_Dummy+59)
-#define WBA_Reserved15                      (WBA_Dummy+60)
-#define WBA_Reserved16                      (WBA_Dummy+61)
-#define WBA_Reserved17                      (WBA_Dummy+62)
-#define WBA_Reserved18                      (WBA_Dummy+63)
-#define WBA_Reserved19                      (WBA_Dummy+64)
+#define WBA_Reserved1                       (WBA_BASE+40)
+#define WBA_Reserved2                       (WBA_BASE+41)
+#define WBA_Reserved3                       (WBA_BASE+46)
+#define WBA_Reserved4                       (WBA_BASE+49)
+#define WBA_Reserved5                       (WBA_BASE+50)
+#define WBA_Reserved6                       (WBA_BASE+51)
+#define WBA_Reserved7                       (WBA_BASE+52)
+#define WBA_Reserved8                       (WBA_BASE+53)
+#define WBA_Reserved9                       (WBA_BASE+54)
+#define WBA_Reserved10                      (WBA_BASE+55)
+#define WBA_Reserved11                      (WBA_BASE+56)
+#define WBA_Reserved12                      (WBA_BASE+57)
+#define WBA_Reserved13                      (WBA_BASE+58)
+#define WBA_Reserved14                      (WBA_BASE+59)
+#define WBA_Reserved15                      (WBA_BASE+60)
+#define WBA_Reserved16                      (WBA_BASE+61)
+#define WBA_Reserved17                      (WBA_BASE+62)
+#define WBA_Reserved18                      (WBA_BASE+63)
+#define WBA_Reserved19                      (WBA_BASE+64)
 
 /*** Last tag ***************************************************************/
 
-#define WBA_LAST_TAG                        (WBA_Dummy+64)
+#define WBA_LAST_TAG                        (WBA_BASE+64)
 
 #endif /* WORKBENCH_WORKBENCH_H */
