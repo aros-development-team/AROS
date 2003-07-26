cc22a75a27382067727eadda35ef81048e4a3c1c
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-07-26 00:09:07 +0000

    Fixed warnings.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18807 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/systemtags.c
+++ test/systemtags.c
@@ -12,28 +12,44 @@ static void StartCommand(void)
 {
   struct FileHandle *fh = Open("S:Startup-Sequence", MODE_OLDFILE);
     
-  SystemTags("type in:", SYS_Input, fh);
+  SystemTags("type in:", SYS_Input, (IPTR) fh);
     
   Signal(t, 1<<sig);  
 }
 
 int main(void)
 {
-  struct FileHandle *fh;
-  
-  SystemTags("dir", SYS_Asynch, TRUE, SYS_Input, Open("NIL:", MODE_OLDFILE),
-	     SYS_Output, Open("RAW:////Dir/CLOSE/WAIT", MODE_NEWFILE), TAG_DONE);
-  SystemTags("systemtagsslave", SYS_Asynch, TRUE, SYS_Input, SYS_DupStream,
-	     SYS_Output, SYS_DupStream, TAG_DONE);
-  fh = Open("S:Startup-Sequence", MODE_OLDFILE);
-  assert(fh!=NULL);
-  SystemTags("type in:", SYS_Input, fh, TAG_DONE);
-
-  sig = AllocSignal(-1);
-  t = FindTask(NULL);
-  
-  CreateNewProcTags(NP_Entry, StartCommand, NP_Input, Input(), NP_CloseInput, FALSE);
+    struct FileHandle *fh;
     
-  Wait(1<<sig);
-  FreeSignal(sig);
+    SystemTags
+    (
+        "dir", 
+        SYS_Asynch,        TRUE, 
+        SYS_Input,  (IPTR) Open("NIL:", MODE_OLDFILE),
+        SYS_Output, (IPTR) Open("RAW:////Dir/CLOSE/WAIT", MODE_NEWFILE), 
+        TAG_DONE
+    );
+    
+    SystemTags
+    (
+        "systemtagsslave", 
+        SYS_Asynch, TRUE,
+        SYS_Input,  SYS_DupStream,
+        SYS_Output, SYS_DupStream, 
+        TAG_DONE
+    );
+    
+    fh = Open("S:Startup-Sequence", MODE_OLDFILE);
+    assert(fh!=NULL);
+    SystemTags("type in:", SYS_Input, (IPTR) fh, TAG_DONE);
+    
+    sig = AllocSignal(-1);
+    t = FindTask(NULL);
+    
+    CreateNewProcTags(NP_Entry, (IPTR) StartCommand, NP_Input, (IPTR) Input(), NP_CloseInput, FALSE);
+    
+    Wait(1<<sig);
+    FreeSignal(sig);
+    
+    return 0;
 }
