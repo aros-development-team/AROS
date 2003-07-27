5263c71d04afba1b53a420f111fe3eb7db326027
sebauer <sebauer@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-07-27 15:09:47 +0000

    Implemented sprintf() and snprintf() using RawDoFmt()
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18872 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/support_amigaos.c
+++ workbench/libs/muimaster/support_amigaos.c
@@ -13,7 +13,7 @@ char *StrDup(const char *x)
     char *dup;
     if (!x) return NULL;
     dup = AllocVec(strlen(x) + 1, MEMF_PUBLIC);
-    if (dup) CopyMem((x), dup, strlen(x) + 1);
+    if (dup) CopyMem((char*)x, dup, strlen(x) + 1);
     return dup;
 }
 
@@ -31,14 +31,50 @@ Object *DoSuperNewTags(struct IClass *cl, Object *obj, void *dummy, ...)
     return obj;
 }
 
+struct snprintf_msg
+{
+	int size;
+	char *buf;
+};
+
+__asm void snprintf_func(register __d0 UBYTE chr, register __a3 struct snprintf_msg *msg)
+{
+    if (msg->size)
+    {
+		  *msg->buf++ = chr;
+    	msg->size--;
+    }
+}
+
 int snprintf(char *buf, int size, const char *fmt, ...)
 {
+#if 1
+    struct snprintf_msg msg;
+		if (!size) return 0;
+
+    msg.size = size;
+    msg.buf = buf;
+
+    RawDoFmt(fmt, (((ULONG *)&fmt)+1), snprintf_func, &msg);
+
+    buf[size-1] = 0;
+
+		return (int)strlen(buf);
+#else
     int ret;
     va_list argptr;
     va_start(argptr,fmt);
     ret = vsprintf(buf,fmt,argptr);
     va_end(argptr);
     return ret;
+#endif
+}
+
+int sprintf(char *buf, const char *fmt, ...)
+{
+		static const ULONG cpy_func = 0x16c04e75; /* move.b d0,(a3)+ ; rts */
+		RawDoFmt(fmt, (((ULONG *)&fmt)+1), (void(*)())&cpy_func, buf);
+		return (int)strlen(buf);
 }
 
 int strlcat(char *buf, char *src, int len)
