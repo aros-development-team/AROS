841b8acdb736cb159b5655f60391e82a6f0c049b
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-07-27 23:20:55 +0000

    Implemented support for ICONGETA_IsDefaultIcon and added some skeleton code for the other unimplemented tags. Also some miscellanous cleanup.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18891 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/icon/geticontaglist.c
+++ workbench/libs/icon/geticontaglist.c
@@ -1,9 +1,6 @@
 /*
-    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2003, The AROS Development Team. All rights reserved.
     $Id$
-
-    Desc:
-    Lang: english
 */
 
 #define DEBUG 1
@@ -22,7 +19,7 @@
 	AROS_LH2(struct DiskObject *, GetIconTagList,
 
 /*  SYNOPSIS */
-	AROS_LHA(STRPTR, name, A0),
+	AROS_LHA(STRPTR,           name, A0),
 	AROS_LHA(struct TagItem *, tags, A1),
 /*  LOCATION */
 	struct Library *, IconBase, 30, Icon)
@@ -43,68 +40,105 @@
 
     INTERNALS
 
-    HISTORY
-
 *****************************************************************************/
 {
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct Library *,IconBase)
 
-    struct TagItem  *tag, *tagp;
+    struct TagItem *tstate = tags,
+                   *tag;
+                   
     struct DiskObject *dob = NULL;
-    int type = -1;
-    char *def_name = NULL;
-    int fail = 1;
-
-    /* parse tags */
-    for (tagp = tags; (tag = NextTagItem(&tagp)); )
+    
+    LONG    defaultType       = -1;
+    STRPTR  defaultName       = NULL;
+    BOOL    failIfUnavailable = TRUE;
+    LONG   *isDefaultIcon     = NULL;
+    
+    /* Parse taglist -------------------------------------------------------*/
+    while ((tag = NextTagItem(&tstate)) != NULL)
     {
-	switch (tag->ti_Tag)
-	{
-	    case    ICONGETA_GetDefaultType:
-		    type = tag->ti_Data;
-		    break;
-
-	    case    ICONGETA_GetDefaultName:
-		    def_name = (char*)tag->ti_Data;
-		    break;
-
-	    case    ICONGETA_FailIfUnavailable:
-		    fail = tag->ti_Data;
-		    break;
-	}
+        switch (tag->ti_Tag)
+        {
+            case ICONGETA_GetDefaultType:
+                defaultType = tag->ti_Data; 
+                break;
+                
+            case ICONGETA_GetDefaultName:
+                defaultName = (STRPTR) tag->ti_Data; 
+                break;
+                
+            case ICONGETA_FailIfUnavailable:
+                failIfUnavailable = tag->ti_Data;
+                break;
+                
+            case ICONGETA_IsDefaultIcon:
+                isDefaultIcon  = (LONG *) tag->ti_Data;
+                *isDefaultIcon = FALSE;
+                // FIXME: implement
+                break;
+                
+                
+            case ICONGETA_GetPaletteMappedIcon:
+                // FIXME: implement
+                break;
+                
+            case ICONGETA_RemapIcon:
+                // FIXME: implement
+                break;
+                
+            case ICONGETA_GenerateImageMasks:
+                // FIXME: implement
+                break;
+                
+            case ICONGETA_Screen:
+                // FIXME: implement
+                break;
+            
+            case ICONGETA_Label:
+                // FIXME: implement
+                break;
+                
+            case ICONA_ErrorCode:
+                // FIXME: implement
+                break;
+        }
     }
+    
 
    /* TODO: We use the old GetDiskObjectXXX() calls here,
     * but it would be better if those functions use
     * GetIconTagList() because this supersets all */
 
-    if (type != -1 || def_name)
+    if (defaultType != -1 || defaultName != NULL)
     {
-	/* the name argument have to be ignored */
+	*isDefaultIcon = TRUE;
+        
+        /* the name argument have to be ignored */
 
-	if (def_name)
+	if (defaultName)
 	{
 	    char buf[360];
 	    /* TODO: has aros snprintf()? */
-	    sprintf(buf,"ENV:Sys/def_%s",def_name);
-	    dob = GetDiskObject(def_name);
+	    sprintf(buf,"ENV:Sys/def_%s",defaultName);
+	    dob = GetDiskObject(defaultName);
 	}
 
-	if (!dob && type != -1)
+	if (!dob && defaultType != -1)
 	{
-	    dob = GetDefDiskObject(type);
+	    dob = GetDefDiskObject(defaultType);
 	}
-    }   else
+    }
+    else
     {
 	/* icon.library v44 uses a global identification hook
 	 * to determine the filetype. We just call GetDiskObjectNew() for now */
 
-	if (fail)
-	{
-	    dob = GetDiskObject(name);
-	} else
-	{
+	dob = GetDiskObject(name);
+        
+        if (dob == NULL && !failIfUnavailable)
+        {
+            *isDefaultIcon = TRUE;
 	    dob = GetDiskObjectNew(name);
 	}
     }
