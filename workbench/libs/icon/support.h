d7b7ada77ac19f7ff432ee4f1f9b3135a3066df3
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-07-28 19:15:52 +0000

    GetDefDiskObject() is now a simple wrapper around GetIconTagList().
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18910 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/icon/support.h
+++ workbench/libs/icon/support.h
@@ -14,9 +14,12 @@
 /*** Prototypes *************************************************************/
 BPTR __OpenIcon_WB(CONST_STRPTR name, LONG mode, struct IconBase *IconBase);
 BOOL __CloseIcon_WB(BPTR file, struct IconBase *IconBase);
+BPTR __OpenDefaultIcon_WB(CONST_STRPTR name, LONG mode, struct IconBase *IconBase);
+BOOL __CloseDefaultIcon_WB(BPTR file, struct IconBase *IconBase);
 
+struct DiskObject *__ReadIcon_WB(BPTR file, struct IconBase *IconBase);
 
-VOID GetDefIconName(LONG, UBYTE *);
+VOID GetDefIconName (LONG def_type, UBYTE * deficonname);
 LONG CalcIconHash(struct DiskObject *dobj);
 VOID AddIconToList(struct NativeIcon *icon, struct IconBase *IconBase);
 VOID RemoveIconFromList(struct NativeIcon *icon, struct IconBase *IconBase);
@@ -25,5 +28,9 @@ struct NativeIcon *GetNativeIcon(struct DiskObject *dobj, struct IconBase *IconB
 /*** Macros *****************************************************************/
 #define OpenIcon(name, mode) (__OpenIcon_WB((name), (mode), IconBase))
 #define CloseIcon(file) (__CloseIcon_WB((file), IconBase))
+#define OpenDefaultIcon(name, mode) (__OpenDefaultIcon_WB((name), (mode), IconBase))
+#define CloseDefaultIcon(file) (__CloseDefaultIcon_WB((file), IconBase))
+
+#define ReadIcon(file) (__ReadIcon_WB((file), IconBase))
 
 #endif /* _SUPPORT_H_ */
---
+++ workbench/libs/icon/support_builtin.h
@@ -0,0 +1,17 @@
+#ifndef _SUPPORT_BUILTIN_H_
+#define _SUPPORT_BUILTIN_H_
+
+/*
+    Copyright © 2003, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Headers for the builtin icon images and support functions.
+*/
+
+/*** Prototypes *************************************************************/
+struct DiskObject *__GetBuiltinIcon_WB(LONG type, struct IconBase *IconBase);
+
+/*** Macros *****************************************************************/
+#define GetBuiltinIcon(type) (__GetBuiltinIcon_WB((type), IconBase))
+
+#endif /* _SUPPORT_BUILTIN_H_ */
