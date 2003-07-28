4023de48404fd5bddf9b9f54ba2df4e6fe9c6139
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-07-28 17:22:13 +0000

    Implemented AllocVecPooled() and FreeVecPooled() functions, which are available in MorphOS (and are definately usefull).
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18907 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/exec/freevecpooled.c
@@ -0,0 +1,27 @@
+/*
+    Copyright © 2003, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <aros/libcall.h>
+#include <proto/exec.h>
+
+#include "exec_intern.h"
+
+AROS_LH2(void, FreeVecPooled,
+    AROS_LHA(APTR, pool,   D0),
+    AROS_LHA(APTR, memory, D1),
+    struct ExecBase *, SysBase, 150, Exec)
+{
+    AROS_LIBFUNC_INIT
+    
+    if (memory != NULL)
+    {
+        IPTR *real = (IPTR *) memory;
+        IPTR size  = *--real;
+
+        FreePooled(pool, real, size);
+    }
+    
+    AROS_LIBFUNC_EXIT
+} /* FreeVecPooled() */
