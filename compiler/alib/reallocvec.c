7ce7b33219acb66c880b737a6a6e496cc8971196
hkiel <hkiel@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-07-30 12:06:29 +0000

    Implemented a new function ReAllocVec(). May be used with AllocVec() and FreeVec().
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18985 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/alib/reallocvec.c
@@ -0,0 +1,109 @@
+/*
+    Copyright © 1995-2003, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: New Exec pendant of ANSI C function realloc() using AllocVec()
+    Lang: english
+*/
+
+#define REALLOC_MININCREASE 1024
+#define REALLOC_MINDECREASE 4096
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/exec.h>
+
+//	AROS_LH3(APTR, AllocVec,
+	APTR ReAllocVec (
+
+/*  SYNOPSIS */
+	APTR  oldmem,
+	ULONG newsize,
+	ULONG requirements)
+//	AROS_LHA(APTR,  oldmem,       A0),
+//	AROS_LHA(ULONG, byteSize,     D0),
+//	AROS_LHA(ULONG, requirements, D1),
+/*  SYNOPSIS */
+//	struct ExecBase *, SysBase, 1xx, Exec)
+
+/*  FUNCTION
+	Change the size of an AllocVec:ed part of memory. The memory must
+	have been allocated by AllocVec(). If you reduce the
+	size, the old contents will be lost. If you enlarge the size,
+	the new contents will be undefined.
+
+    INPUTS
+	oldmen - What you got from AllocVec().
+	newsize - The new size.
+	requirements - The (new) requirements.
+		Note that if no new block of memory is allocated, the
+		requirements are not considered.
+
+    RESULT
+	A pointer to the allocated memory or NULL. If you don't need the
+	memory anymore, you can pass this pointer to FreeVec().
+
+    NOTES
+	If you get NULL, the memory at oldmem will not have been freed and
+	can still be used.
+	Note that if no new block of memory is allocated, the requirements
+	are not considered.
+
+        This function must not be used in a shared library or in a
+	threaded application. (???)
+
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	AllocVec(), FreeVec(), CopyMem()
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+{
+//    AROS_LIBFUNC_INIT
+    UBYTE * mem, * newmem;
+    ULONG oldsize;
+
+    if (!oldmem)
+	return AllocVec (newsize, requirements);
+
+    mem = (UBYTE *)oldmem - AROS_ALIGN(sizeof(ULONG));
+    oldsize = *((ULONG *)mem);
+
+    /* Reduce or enlarge the memory ? */
+    if (newsize < oldsize)
+    {
+	/* Don't change anything for small changes */
+	if ((oldsize - newsize) < REALLOC_MINDECREASE)
+	    newmem = oldmem;
+	else
+	    goto copy;
+    }
+    else if (newsize == oldsize) /* Keep the size ? */
+	newmem = oldmem;
+    else
+    {
+	/* It is likely that if memory is ReAllocVec:ed once it will
+	   be ReAllocVec:ed again, so don't be too stingy with memory */
+	if ((newsize - oldsize) < REALLOC_MININCREASE)
+	    newsize = oldsize + REALLOC_MININCREASE;
+copy:
+	newmem = AllocVec(newsize, requirements);
+
+	if (newmem)
+	{
+	    CopyMem (oldmem, newmem, newsize);
+	    FreeVec (oldmem);
+	}
+    }
+
+    return newmem;
+//    AROS_LIBFUNC_EXIT
+} /* ReAllocVec */
