e4af51cdb823a8f09f2356513fbc8bf6faef7c5d
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-07-30 12:23:38 +0000

    Very simple Delete module (more a test right now).
    git-svn-id: https://svn.aros.org/svn/aros/trunk@18989 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/system/Wanderer/Tools/Delete/main.c
@@ -0,0 +1,83 @@
+/*
+    Copyright © 2003, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#define MUIMASTER_YES_INLINE_STDARG
+
+#define DEBUG 1
+#include <aros/debug.h>
+
+#include <proto/intuition.h>
+#include <proto/exec.h>
+#include <proto/muimaster.h>
+#include <proto/workbench.h>
+#include <proto/dos.h>
+
+#include <dos/dos.h>
+#include <libraries/asl.h>
+#include <libraries/mui.h>
+#include <workbench/workbench.h>
+#include <workbench/startup.h>
+
+int main(int argc, char **argv)
+{
+    if (argc == 0)
+    {
+        struct WBStartup *startup = (struct WBStartup *) argv;
+        
+        if (startup->sm_NumArgs == 2)
+        {
+            BPTR   lock = startup->sm_ArgList[1].wa_Lock;
+            STRPTR name = startup->sm_ArgList[1].wa_Name;
+            
+            if
+            (
+                MUI_Request
+                (
+                    NULL, NULL, 0,
+                    "Delete?", "Delete|Cancel",
+                    "Are you sure you want to delete \"%s\"?", name
+                ) == 1
+            )
+            {
+                BPTR cd = CurrentDir(lock);
+                
+                DeleteFile(name); // FIXME: check error
+                
+                CurrentDir(cd);
+            }
+        }
+        else if (startup->sm_NumArgs > 2)
+        {
+            ULONG i;
+            
+            for (i = 1; i < startup->sm_NumArgs; i++)
+            {
+                OpenWorkbenchObject
+                (
+                    "SYS:System/Delete",
+                    WBOPENA_ArgLock, startup->sm_ArgList[i].wa_Lock,
+                    WBOPENA_ArgName, startup->sm_ArgList[i].wa_Name,
+                    TAG_DONE
+                );
+            }
+        }
+        else
+        {
+            MUI_RequestA
+            (
+                NULL, NULL, 0, 
+                "Info", "OK", "Error: Wrong number of arguments.", NULL
+            );
+            return 20;
+        }
+    }
+    else
+    {
+        PutStr("Info: Must be started from Wanderer.\n");
+        return 40;
+    }
+
+    return 0;
+}
