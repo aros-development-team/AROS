70702f9ef58beffe99ca7a1d86b1a9a43ea6621c
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-08-01 18:08:34 +0000

    Two test programs for icon.library. 'examine' opens an icon and prints some information about it (included tooltypes), and 'writetooltypes' writes an icon with some tooltypes set.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19037 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/icon/writetooltypes.c
@@ -0,0 +1,44 @@
+#include <workbench/workbench.h>
+#include <workbench/icon.h>
+
+#include <proto/icon.h>
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    STRPTR newToolTypes[] =
+    {
+        "AROS=RULES",
+        "Tooltypes are actually free-form text",
+        "Ain't that neat?",
+        NULL
+    };
+    
+    struct DiskObject *icon = GetIconTags
+    (
+        "writetooltypes", ICONGETA_FailIfUnavailable, FALSE, TAG_DONE
+    );
+    
+    if (icon != NULL)
+    {
+        STRPTR *oldToolTypes = icon->do_ToolTypes;
+        
+        icon->do_ToolTypes = newToolTypes;
+        if (!PutIconTags("writetooltypes", icon, TAG_DONE))
+        {
+            printf("ERROR: Failed to write icon.\n");
+        }
+        icon->do_ToolTypes = oldToolTypes;
+        
+        FreeDiskObject(icon);
+    }
+    else
+    {
+        printf("ERROR: Failed to open icon for file\n");
+        return 20;
+    }
+        
+
+    return 0;
+}
