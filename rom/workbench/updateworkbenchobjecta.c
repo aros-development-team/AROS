281991e4074d92bb6ff229c257890b2137530057
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-08-01 16:41:10 +0000

    New function for informing the workbench application (wanderer) that it should update it's display of a particular object. This function WILL GO AWAY when there is a adequate notification system in dos.library. Implementation: sends message of type WBHM_TYPE_UPDATE to the application.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19033 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/workbench/updateworkbenchobjecta.c
@@ -0,0 +1,103 @@
+/*
+    Copyright © 1995-2003, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Inform the file manager that an object has changed.
+*/
+
+#define DEBUG 1
+#include <aros/debug.h>
+
+#include <exec/types.h>
+#include <exec/ports.h>
+#include <utility/tagitem.h>
+#include <intuition/intuition.h>
+#include <workbench/workbench.h>
+#include <proto/utility.h>
+
+#include <string.h>
+
+#include "workbench_intern.h"
+#include "support.h"
+#include "support_messages.h"
+#include "handler.h"
+#include "handler_support.h"
+
+/*****************************************************************************
+
+    NAME */
+
+        #include <proto/workbench.h>
+
+        AROS_LH3(BOOL, UpdateWorkbenchObjectA,
+
+/*  SYNOPSIS */
+        AROS_LHA(STRPTR,           name, A0),
+        AROS_LHA(LONG,             type, D1),
+        AROS_LHA(struct TagItem *, tags, A1),
+
+/*  LOCATION */
+        struct WorkbenchBase *, WorkbenchBase, 25, Workbench)
+
+/*  FUNCTION
+        Informs the workbench application that an object has changed, and that
+        it should update it's visual representation. 
+
+    INPUTS
+        name - Name of object that has changed.
+        type - Type of object (WBDISK, WBTOOL, ...).
+        tags - Additional options.
+        
+    TAGS
+        No tags are defined at this time.
+
+    NOTES
+        This function is TEMPORARY! It will hopefully go away before AROS 1.0,
+        and it might change it's API several times before that!
+
+    EXAMPLE
+
+    BUGS
+        The existance of this function is a bug itself. It should be removed
+        once there is a adequate notification API in dos.library that works.
+
+    SEE ALSO
+
+    INTERNALS
+
+******************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+    AROS_LIBBASE_EXT_DECL(struct WorkbenchBase *, WorkbenchBase)
+  
+    BOOL                     success = FALSE;
+    struct WBCommandMessage *wbcm    = NULL;
+    struct WBHandlerMessage *wbhm    = NULL;
+    
+    if
+    (
+           (wbcm = CreateWBCM(WBCM_TYPE_RELAY))  != NULL
+        && (wbhm = CreateWBHM(WBHM_TYPE_UPDATE)) != NULL
+    )
+    {
+        if ((wbhm->wbhm_Data.Update.Name = StrDup(name)) != NULL)
+        {
+            wbhm->wbhm_Data.Update.Type   = type;
+            wbcm->wbcm_Data.Relay.Message = wbhm;
+            
+            PutMsg(&WorkbenchBase->wb_HandlerPort, (struct Message *) wbcm);            
+            
+            success = TRUE;
+        }
+    }
+    
+    if (!success)
+    {
+        if (wbcm != NULL) DestroyWBCM(wbcm);
+        if (wbhm != NULL) DestroyWBHM(wbhm);
+    }
+    
+    return success;
+
+    AROS_LIBFUNC_EXIT
+} /* UpdateWorkbenchObjectA() */
