be5fbc2872ecf53921e7bcd329b47404c77917c3
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-08-01 18:08:34 +0000

    Two test programs for icon.library. 'examine' opens an icon and prints some information about it (included tooltypes), and 'writetooltypes' writes an icon with some tooltypes set.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19037 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/icon/examine.c
@@ -0,0 +1,83 @@
+#include <workbench/workbench.h>
+#include <workbench/icon.h>
+
+#include <proto/icon.h>
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    const char * const typeNames[] =
+    {
+        NULL,        /* not used */
+        "WBDISK",    /* 1 */
+        "WBDRAWER",  /* 2 */
+        "WBTOOL",    /* 3 */
+        "WBPROJECT", /* 4 */
+        "WBGARBAGE", /* 5 */
+        "WBDEVICE",  /* 6 */
+        "WBKICK",    /* 7 */
+        "WBAPPICON"  /* 8 */
+    };
+            
+    if (argc == 2)
+    {
+        LONG               isDefault = FALSE;
+        struct DiskObject *icon      = GetIconTags
+        (
+            argv[1],
+            ICONGETA_FailIfUnavailable,        FALSE, 
+            ICONGETA_IsDefaultIcon,     (IPTR) &isDefault,
+            TAG_DONE
+        );
+        
+        if (icon != NULL)
+        {
+            printf
+            (
+                "Name:          %s\n"
+                "Fake icon:     %s\n"
+                "Type:          %s (%d)\n"
+                "Default tool:  %s\n"
+                "Position:      x = %ld (0x%lx), y = %ld (0x%lx)\n"
+                "Drawer data:   %s\n"
+                "Stack size:    %ld\n"
+                "Tooltypes:     %s\n\n",
+                argv[1], isDefault ? "yes" : "no",
+                typeNames[icon->do_Type], icon->do_Type, icon->do_DefaultTool,
+                icon->do_CurrentX, icon->do_CurrentX, 
+                icon->do_CurrentY, icon->do_CurrentY,
+                icon->do_DrawerData != NULL ? "yes" : "no",
+                icon->do_StackSize,
+                icon->do_ToolTypes != NULL ? "yes" : "no"
+            );
+            
+            if (icon->do_ToolTypes != NULL)
+            {
+                char *tt = NULL;
+                int   i  = 0;
+                
+                printf("Tooltype data:\n");
+                
+                while ((tt = icon->do_ToolTypes[i]) != NULL)
+                {
+                    printf("%s\n", tt);
+                    i++;
+                }
+            }
+            
+            FreeDiskObject(icon);
+        }
+        else
+        {
+            printf("ERROR: Failed to open icon.\n");
+            return 20;
+        }
+    }
+    else
+    {
+        printf("Usage: %s <name>    (without trailing \".info\")\n", argv[0]);
+    }
+    
+    return 0;
+}
