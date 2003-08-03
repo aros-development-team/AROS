075a9b9d5df8791572b468488d686f7ea748f5bd
dlc <dlc@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-08-03 18:42:23 +0000

    optimized a bit by testing the MADF_INVIRTUALGROUP before searching for a virtgroup parent
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19060 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/mui_redraw.c
+++ workbench/libs/muimaster/mui_redraw.c
@@ -55,11 +55,7 @@
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct MUIMasterBase *,MUIMasterBase)
 
-    Object *wnd;
-    Object *parent;
-    struct Region *region = NULL;
     APTR clip = NULL;
-    struct Rectangle *clip_rect;
     ULONG disabled;
 
     if (!(_flags(obj) & MADF_CANDRAW)) return;
@@ -69,54 +65,64 @@
 /*  	return; */
     _flags(obj) |= MADF_DRAWING;
 
-    get(obj,MUIA_WindowObject,&wnd);
-    parent = obj;
 
-    while (get(parent,MUIA_Parent,&parent))
+    if (_flags(obj) & MADF_INVIRTUALGROUP)
     {
-    	if (!parent) break;
-    	if (parent == wnd) break;
+	Object *wnd;
+	Object *parent;
+	struct Region *region = NULL;
+	struct Rectangle *clip_rect;
 
-	if (_flags(parent) & MADF_ISVIRTUALGROUP)
+	get(obj,MUIA_WindowObject,&wnd);
+	parent = obj;
+
+	while (get(parent,MUIA_Parent,&parent))
 	{
-	    struct Rectangle rect;
+	    if (!parent) break;
+	    if (parent == wnd) break;
 
-	    rect.MinX = _mleft(parent);
-	    rect.MinY = _mtop(parent);
-	    rect.MaxX = _mright(parent);
-	    rect.MaxY = _mbottom(parent);
+	    if (_flags(parent) & MADF_ISVIRTUALGROUP)
+	    {
+		struct Rectangle rect;
 
-	    if (!region)
-	    {
-	    	if ((region = NewRegion()))
-	    	{
-		    OrRectRegion(region, &rect);
-	    	}
-	    } else
-	    {
-		AndRectRegion(region, &rect);
+		rect.MinX = _mleft(parent);
+		rect.MinY = _mtop(parent);
+		rect.MaxX = _mright(parent);
+		rect.MaxY = _mbottom(parent);
+
+		if (!region)
+		{
+		    if ((region = NewRegion()))
+		    {
+			OrRectRegion(region, &rect);
+		    }
+		} else
+		{
+		    AndRectRegion(region, &rect);
+		}
 	    }
 	}
-    }
 
-    clip_rect = &muiRenderInfo(obj)->mri_ClipRect;
+	clip_rect = &muiRenderInfo(obj)->mri_ClipRect;
 
-    if (region)
-    {
-    	/* Maybe this should went to MUI_AddClipRegion() */
-    	clip_rect->MinX = MAX(_left(obj),region->bounds.MinX);
-    	clip_rect->MinY = MAX(_top(obj),region->bounds.MinY);
-    	clip_rect->MaxX = MIN(_right(obj),region->bounds.MaxX);
-    	clip_rect->MaxY = MIN(_bottom(obj),region->bounds.MaxY);
+	if (region)
+	{
+	    /* Maybe this should went to MUI_AddClipRegion() */
+	    clip_rect->MinX = MAX(_left(obj),region->bounds.MinX);
+	    clip_rect->MinY = MAX(_top(obj),region->bounds.MinY);
+	    clip_rect->MaxX = MIN(_right(obj),region->bounds.MaxX);
+	    clip_rect->MaxY = MIN(_bottom(obj),region->bounds.MaxY);
 
-	clip = MUI_AddClipRegion(muiRenderInfo(obj),region);
-    } else
-    {
-        clip_rect->MinX = _left(obj);
-        clip_rect->MinY = _top(obj);
-        clip_rect->MaxX = _right(obj);
-        clip_rect->MaxY = _bottom(obj);
-    }
+	    clip = MUI_AddClipRegion(muiRenderInfo(obj),region);
+	} else
+	{
+	    clip_rect->MinX = _left(obj);
+	    clip_rect->MinY = _top(obj);
+	    clip_rect->MaxX = _right(obj);
+	    clip_rect->MaxY = _bottom(obj);
+	}
+    } /* if object is in a virtual group */
+
     DoMethod(obj, MUIM_Draw, flags);
 
     if (get(obj, MUIA_Disabled, &disabled))
@@ -136,15 +142,15 @@
 #ifdef __AROS__
 #if 0
             /*
-                This aproach might be faster *provided* that the buffer is
-                allocated and filled *once* at startup of muimaster.library.
+	      This aproach might be faster *provided* that the buffer is
+	      allocated and filled *once* at startup of muimaster.library.
                 
-                In reality, the WritePixelArray() call has quite a big 
-                overhead, so you should only use this buffer if the gadget
-                completely fits inside, and fall back to allocating a new
-                buffer if the gadget is too big.
+	      In reality, the WritePixelArray() call has quite a big 
+	      overhead, so you should only use this buffer if the gadget
+	      completely fits inside, and fall back to allocating a new
+	      buffer if the gadget is too big.
                 
-                Perhaps a future optimization...
+	      Perhaps a future optimization...
             */
             LONG  width  = 200;
             LONG  height = 100;
@@ -158,13 +164,13 @@
                 for (x = 0; x < _width(obj); x += width)
                 {
                     WritePixelArrayAlpha
-                    (
-                        buffer, 0, 0, width * sizeof(LONG),
-                        _rp(obj), _left(obj) + x, _top(obj) + y, 
-                        x + width  > _width(obj)  ? _width(obj)  - x : width,
-                        y + height > _height(obj) ? _height(obj) - y : height,
-                        0
-                    );
+		    (
+		        buffer, 0, 0, width * sizeof(LONG),
+			_rp(obj), _left(obj) + x, _top(obj) + y, 
+			x + width  > _width(obj)  ? _width(obj)  - x : width,
+			y + height > _height(obj) ? _height(obj) - y : height,
+			0
+		    );
                 }
             }
 #else            
@@ -182,10 +188,10 @@
                 memset(buffer, 0xAA, width * height * sizeof(LONG));
 
                 WritePixelArrayAlpha
-                (
-                    buffer, 0, 0, width * sizeof(LONG), 
-                    _rp(obj), _left(obj), _top(obj), width, height, 0
-                );
+		(
+		    buffer, 0, 0, width * sizeof(LONG), 
+		    _rp(obj), _left(obj), _top(obj), width, height, 0
+		);
                 FreeVec(buffer);
             }   else
 #endif
@@ -202,7 +208,7 @@
                 SetAfPt(_rp(obj), NULL, 0);
             }
         }
-    }
+    } /* if (object is disabled) */
 
     /* copy buffer to window */
     if (muiRenderInfo(obj)->mri_BufferBM)
@@ -222,4 +228,4 @@
 
     AROS_LIBFUNC_EXIT
 
-} /* MUIA_Redraw */
+	} /* MUIA_Redraw */
