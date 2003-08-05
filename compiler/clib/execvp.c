a8436ecf30a2ce11587dbf43efd5f72aaa094747
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-08-05 16:29:23 +0000

    Stub for the execvp() POSIX function. Should be possible to implement in AROS, I think...
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19086 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/execvp.c
@@ -0,0 +1,47 @@
+/*
+    Copyright © 1995-2003, The AROS Development Team. All rights reserved.
+    $Id$
+
+    POSIX function execvp().
+*/
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <errno.h>
+
+
+/*****************************************************************************
+
+    NAME */
+#include <stdio.h>
+
+	int execvp(
+
+/*  SYNOPSIS */
+	const char *file, 
+        char *const argv[])
+        
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	
+    INTERNALS
+
+******************************************************************************/
+{
+#   warning TODO: implement execvp()
+    
+    return -1;
+} /* execvp() */
+
