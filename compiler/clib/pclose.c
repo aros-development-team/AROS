69ab0975cdac51a0358dd66ad08014176bf0e8bb
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-08-05 16:12:27 +0000

    Stub for POSIX function pclose().
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19084 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/pclose.c
@@ -0,0 +1,46 @@
+/*
+    Copyright © 1995-2003, The AROS Development Team. All rights reserved.
+    $Id$
+
+    POSIX function pclose().
+*/
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <errno.h>
+
+
+/*****************************************************************************
+
+    NAME */
+#include <stdio.h>
+
+	int pclose(
+
+/*  SYNOPSIS */
+	FILE * stream)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	popen()
+
+    INTERNALS
+
+******************************************************************************/
+{
+#   warning TODO: implement pclose()
+    return -1;
+} /* pclose() */
+
