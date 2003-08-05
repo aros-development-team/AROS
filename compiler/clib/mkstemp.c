d484ed5d82f182c375d658dc0fdc059f84ea9321
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-08-05 16:19:33 +0000

    Stub for the mkstemp() POSIX function.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19085 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/mkstemp.c
@@ -0,0 +1,42 @@
+/*
+    Copyright © 1995-2003, The AROS Development Team. All rights reserved.
+    $Id$
+
+    POSIX function mkstemp().
+*/
+
+#include <errno.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <stdlib.h>
+
+	int mkstemp(
+
+/*  SYNOPSIS */
+	char *template)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+******************************************************************************/
+{
+#   warning TODO: implement mkstemp()
+    
+    return -1;
+} /* mkstemp() */
+
