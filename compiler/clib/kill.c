f5e2c35307c901ccd3870dbe2c3181809107fa8d
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-08-05 12:19:42 +0000

    Stub for POSIX function kill(). Not implemented.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19080 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/kill.c
@@ -0,0 +1,41 @@
+/*
+    Copyright © 2003, The AROS Development Team. All rights reserved.
+    $Id$
+
+    POSIX function kill().
+*/
+
+/*****************************************************************************
+
+    NAME */
+#include <signal.h>
+
+	int kill (
+
+/*  SYNOPSIS */
+	pid_t pid,
+        int   sig)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+******************************************************************************/
+{
+#   warning TODO: Implement kill()
+    
+    return -1;
+} /* kill() */
+
