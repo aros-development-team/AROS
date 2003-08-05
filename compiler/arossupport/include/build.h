98cc7f47f87a1e041056c9bb76fcb5dc61e81528
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-08-05 23:49:23 +0000

    Added build type defines.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19101 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/arossupport/include/build.h
@@ -0,0 +1,15 @@
+#ifndef AROS_BUILD_H
+#define AROS_BUILD_H
+
+/*
+    Copyright © 2003, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#define AROS_BUILD_TYPE_PERSONAL  (0)
+#define AROS_BUILD_TYPE_NIGHTLY   (1)
+#define AROS_BUILD_TYPE_SNAPSHOT  (2)
+#define AROS_BUILD_TYPE_MILESTONE (3)
+#define AROS_BUILD_TYPE_RELEASE   (4)
+
+#endif /* AROS_BUILD_H */
