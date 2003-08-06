df00243cba3ece3c10eda110f33183427f60a212
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-08-06 01:16:48 +0000

    Added default for AROS_BUILD_TYPE.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19108 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/arossupport/include/build.h
+++ compiler/arossupport/include/build.h
@@ -12,4 +12,8 @@
 #define AROS_BUILD_TYPE_MILESTONE (3)
 #define AROS_BUILD_TYPE_RELEASE   (4)
 
+#ifndef AROS_BUILD_TYPE
+#   define AROS_BUILD_TYPE AROS_BUILD_TYPE_PERSONAL
+#endif 
+
 #endif /* AROS_BUILD_H */
