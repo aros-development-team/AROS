b0723e74e034a5381ece6b5b6c18d866c9a766c2
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-08-06 01:27:49 +0000

    + Moved ASCII logotype into a separate file. + Added support for AROS_BUILD_TYPE. An apropriate description ("Nightly Build",   "Snapshot", etc) will be shown based on the build type. Added necessary   catalog strings, and marked un-translated Swedish ones with "**" (I'm not   sure how to translate some phrases; help is apreciated ;-)).
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19109 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/system/AboutAROS/locale.h
+++ workbench/system/AboutAROS/locale.h
@@ -11,6 +11,21 @@
 #define CATCOMP_NUMBERS
 #include "strings.h"
 
+/*** Message aliases ********************************************************/
+#if AROS_BUILD_TYPE == AROS_BUILD_TYPE_PERSONAL
+#   define MSG_BUILD_TYPE MSG_PERSONAL_BUILD
+#elif AROS_BUILD_TYPE == AROS_BUILD_TYPE_NIGHTLY
+#   define MSG_BUILD_TYPE MSG_NIGHTLY_BUILD
+#elif AROS_BUILD_TYPE == AROS_BUILD_TYPE_SNAPSHOT
+#   define MSG_BUILD_TYPE MSG_SNAPSHOT
+#elif AROS_BUILD_TYPE == AROS_BUILD_TYPE_MILESTONE
+#   define MSG_BUILD_TYPE MSG_MILESTONE
+#elif AROS_BUILD_TYPE == AROS_BUILD_TYPE_RELEASE
+#   define MSG_BUILD_TYPE MSG_RELEASE
+#else
+#   error Unknown AROS_BUILD_TYPE
+#endif
+
 /*** Prototypes *************************************************************/
 /* Main *********************************************************************/
 STRPTR  _(ULONG ID);            /* Get a message, as a STRPTR */
