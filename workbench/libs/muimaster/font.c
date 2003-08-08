e36d52b468c46022a700b3f0383319df804c1ca6
dlc <dlc@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-08-08 11:31:08 +0000

    dont try to open font if name is an empty string
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19142 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/font.c
+++ workbench/libs/muimaster/font.c
@@ -14,6 +14,9 @@
 #include "font.h"
 #include "prefs.h"
 
+/*  #define MYDEBUG 1 */
+#include "debug.h"
+
 extern struct Library *MUIMasterBase;
 
 /* Returns a given text font, if necessary it opens the font.
@@ -22,25 +25,27 @@ extern struct Library *MUIMasterBase;
 
 struct TextFont *zune_font_get(Object *obj, LONG preset)
 {
-    struct MUI_GlobalInfo *mgi = muiGlobalInfo(obj);
-    struct MUI_RenderInfo *mri = muiRenderInfo(obj);
+    struct MUI_GlobalInfo *mgi;
+    struct MUI_RenderInfo *mri;
 
     if ((preset <= MUIV_Font_Inherit) && (preset >= MUIV_Font_NegCount))
     {
     	CONST_STRPTR name;
-	if (preset > 0) return NULL;
 
+	mri = muiRenderInfo(obj);
 	/* font already loaded, just return it */
 	if (mri->mri_Fonts[-preset])
 	{
-/*  	    D(bug("zune_font_get : return mri_Fonts[-preset]=%lx\n", ri->mri_Fonts[-preset])); */
+	    D(bug("zune_font_get : return mri_Fonts[%d]=%lx\n",
+		  preset, mri->mri_Fonts[-preset]));
 	    return mri->mri_Fonts[-preset];
 	}
 
+	mgi =  muiGlobalInfo(obj);
 	/* font name given, load it */
         name = mgi->mgi_Prefs->fonts[-preset];
-/*  	D(bug("zune_font_get : preset=%d, name=%s\n", preset, name)); */
-	if (name)
+	D(bug("zune_font_get : preset=%d, name=%s\n", preset, name));
+	if (name != NULL && name[0] != 0)
 	{
 	    struct TextAttr ta;
 	    if ((ta.ta_Name = (char*)AllocVec(strlen(name)+10,0)))
@@ -56,7 +61,8 @@ struct TextFont *zune_font_get(Object *obj, LONG preset)
 
 		if ((p = PathPart(ta.ta_Name)))
 		    strcpy(p,".font");
-/*  		D(bug("zune_font_get : OpenDiskFont(%s)\n", ta.ta_Name)); */
+		D(bug("zune_font_get : OpenDiskFont(%s)\n",
+		      ta.ta_Name));
 		mri->mri_Fonts[-preset] = OpenDiskFont(&ta);
 
 		FreeVec(ta.ta_Name);
@@ -67,6 +73,8 @@ struct TextFont *zune_font_get(Object *obj, LONG preset)
 	{
 	    if (preset != MUIV_Font_Normal && preset != MUIV_Font_Fixed) /* avoid infinite recursion */
 	    {
+		/* dont do this, would result in the font being closed more than once */
+/*  		return (mri->mri_Fonts[-preset] = zune_font_get(obj, MUIV_Font_Normal)); */
 		return zune_font_get(obj, MUIV_Font_Normal);
 	    }
 	}
@@ -79,7 +87,7 @@ struct TextFont *zune_font_get(Object *obj, LONG preset)
 		struct TextAttr scr_attr;
 		scr_attr = *(_screen(obj)->Font);
 		scr_attr.ta_Flags = 0;
-/*  	    D(bug("zune_font_get : OpenDiskFont(%s) (screen font)\n", scr_attr.ta_Name)); */
+		D(bug("zune_font_get : OpenDiskFont(%s) (screen font)\n", scr_attr.ta_Name));
 		mri->mri_Fonts[-preset] = OpenDiskFont(&scr_attr);
 	    }
 	    else /* MUIV_Font_Fixed */
@@ -89,6 +97,7 @@ struct TextFont *zune_font_get(Object *obj, LONG preset)
 		def_attr.ta_YSize = GfxBase->DefaultFont->tf_YSize;
 		def_attr.ta_Style = GfxBase->DefaultFont->tf_Style;
 		def_attr.ta_Flags = GfxBase->DefaultFont->tf_Flags;
+		D(bug("zune_font_get : OpenDiskFont(%s) (system font)\n", def_attr.ta_Name));
 		mri->mri_Fonts[-preset] = OpenDiskFont(&def_attr);
 	    }
 	}
