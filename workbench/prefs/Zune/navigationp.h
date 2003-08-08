60f89fc6e3664d6a6240fda4ccc95daf1ea09df8
dlc <dlc@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-08-08 11:49:15 +0000

    added Navigation prefs page
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19144 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/prefs/Zune/navigationp.h
@@ -0,0 +1,16 @@
+/*
+    Copyright © 2002, The AROS Development Team. 
+    All rights reserved.
+    
+    $Id$
+*/
+
+#ifndef _ZUNE_NAVIGATIONP_H
+
+#ifndef LIBRARIES_MUI_H
+#include <libraries/mui.h>
+#endif
+
+extern const struct __MUIBuiltinClass _MUIP_Navigation_desc;
+
+#endif
