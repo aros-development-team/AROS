bbb2595d415e4439f70634ea105f3b176263af85
dlc <dlc@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-08-09 10:53:46 +0000

    now tracks many more resources (Resources, Interrupts, Ports, Semaphores, GadTools gadgets)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19154 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/LeakWatch.c
+++ workbench/c/LeakWatch.c
@@ -10,7 +10,8 @@
  * memleaks, excepted that this program tries to detect much more leaks.
  * It has to not leak itself, and not even allocate memory between checks.
  * That's why lists and nodes are statically allocated (in a system that could
- * be improved to allow reuse of nodes :)
+ * be improved to allow reuse of nodes :) (like using an Allocate-based own
+ * memory allocator using a huge static array as heap)
  *
  * Use : launch in a new shell, and use ctrl-f whenever you want to check
  * for leak.
@@ -20,9 +21,13 @@
 
 #include <exec/memory.h>
 #include <exec/tasks.h>
+#ifndef EXEC_SEMAPHORES
+#   include <exec/semaphores.h>
+#endif
 #include <exec/execbase.h>
 #include <graphics/gfxbase.h>
 #include <graphics/text.h>
+#include <intuition/classes.h>
 #include <proto/exec.h>
 #include <proto/graphics.h>
 #include <dos/dosextens.h>
@@ -31,20 +36,59 @@
 
 #include <string.h>
 
+#define DEBUG 1
+#include <aros/debug.h>
+
 static const char version[] = "$VER: LeakWatch 0.1 (25.12.2002)\n";
 
+static struct Library *GadToolsBase;
+
+struct GadToolsBase_intern
+{
+    struct Library    		library;
+    struct ExecBase 		* sysbase;
+    BPTR	      		seglist;
+
+    struct IntuitionBase 	* intuibase;
+    struct Library	 	* dosbase;
+    struct GfxBase	 	* gfxbase;
+    struct Library		* layersbase;
+    struct Library	 	* utilitybase;
+
+    Class 			* buttonclass;
+    Class 			* textclass;
+    Class 			* sliderclass;
+    Class 			* scrollerclass;
+    Class 			* arrowclass;
+    Class 			* stringclass;
+    Class 			* listviewclass;
+    Class 			* checkboxclass;
+    Class 			* cycleclass;
+    Class 			* mxclass;
+    Class 			* paletteclass;
+    
+    /* Semaphore to protect the bevel object. */
+    struct SignalSemaphore   	bevelsema;
+    /* Actually an Object *. The image used for bevel boxes. */
+    struct Image           	* bevel;
+    struct SignalSemaphore   	classsema;
+};
+
+
+
 /* struct arrays : ORN = OpenedResourceNode, TR = TrackedResources, RD = ResourceDiff
+   MR = ModifiedResource STR=stringbuf
  */
-#define MAX_ORN 10000
-#define MAX_TR 1000
-#define MAX_RD 100
-#define MAX_STR 100000
-#define MAX_MR 1000
-#define MAX_NR 1000
+#define MAX_ORN  30000
+#define MAX_TR    3000
+#define MAX_RD     300
+#define MAX_STR 300000
+#define MAX_MR    2000
 
 /* All resources that have an opencount, eg. libs, devs, fonts */
 struct OpenedResourceNode {
     struct Node  node;
+    CONST_STRPTR type;
     CONST_STRPTR name;
     APTR         addr;
     ULONG        count;
@@ -58,6 +102,7 @@ struct TrackedResources {
 
 struct ModifiedResource {
     struct Node  node;
+    CONST_STRPTR type;
     CONST_STRPTR name;
     APTR         addr;
     ULONG        before_count;
@@ -135,6 +180,9 @@ CONST_STRPTR StaticStrDup (CONST_STRPTR str)
     UBYTE *t = start;
     int len;
 
+    if (NULL == str)
+	str = "<unnamed>";
+
     len = strlen(str);
     if (len + next_str + 1 > MAX_STR)
 	return NULL;
@@ -155,7 +203,7 @@ static struct TrackedResources *NewResourcesState(void);
 static void DeleteResourcesState(struct TrackedResources *rs);
 static struct ResourceDiff *NewStateDiff(const struct TrackedResources *old,
 					  const struct TrackedResources *new);
-static void DisplayStateDiff(const struct ResourceDiff *rd);
+static void DisplayStateDiff(const struct ResourceDiff *rd, int pagelines);
 static void DeleteStateDiff(struct ResourceDiff *rd);
 static struct TrackedResources * CopyResourcesState(const struct TrackedResources *src);
 
@@ -174,6 +222,7 @@ static BOOL AddLibs(struct List *opened)
 	    Permit();
 	    return FALSE;
 	}
+	orn->type = "Library";
 	orn->name = StaticStrDup(lib->lib_Node.ln_Name);
 	orn->addr = lib;
 	orn->count = lib->lib_OpenCnt;
@@ -198,6 +247,7 @@ static BOOL AddDevs(struct List *opened)
 	    Permit();
 	    return FALSE;
 	}
+	orn->type = "Device";
 	orn->name = StaticStrDup(dev->dd_Library.lib_Node.ln_Name);
 	orn->addr = dev;
 	orn->count = dev->dd_Library.lib_OpenCnt;
@@ -222,6 +272,7 @@ static BOOL AddFonts(struct List *opened)
 	    Permit();
 	    return FALSE;
 	}
+	orn->type = "Font";
 	orn->name = StaticStrDup(tf->tf_Message.mn_Node.ln_Name);
 	orn->addr = tf;
 	orn->count = tf->tf_Accessors;
@@ -231,6 +282,82 @@ static BOOL AddFonts(struct List *opened)
     return TRUE;
 }
 
+static BOOL AddNodeNames(struct List *opened, struct List *list, CONST_STRPTR type)
+{
+    struct Node *lib;
+
+    Forbid();
+    for(lib=(struct Node *)list->lh_Head;
+        lib->ln_Succ!=NULL;
+        lib=(struct Node *)lib->ln_Succ)
+    {
+	struct OpenedResourceNode *orn = get_orn();
+	if (!orn)
+	{
+	    Permit();
+	    return FALSE;
+	}
+	orn->type = type;
+	orn->name = StaticStrDup(lib->ln_Name);
+	orn->addr = lib;
+	orn->count = 0;
+	Enqueue(opened, (struct Node *)orn);
+    }
+    Permit();
+    return TRUE;
+}
+
+static BOOL AddASemaphore(struct List *opened,
+			  struct SignalSemaphore *ss,
+			  CONST_STRPTR name)
+{
+    struct OpenedResourceNode *orn = get_orn();
+
+    if (!orn)
+    {
+	return FALSE;
+    }
+
+    Forbid();
+    orn->type = "Semaphore";
+    orn->name = StaticStrDup(name);
+    orn->addr = ss;
+    orn->count = ss->ss_NestCount;
+    Permit();
+
+    Enqueue(opened, (struct Node *)orn);
+    return TRUE;
+}
+
+
+static BOOL AddAClass(struct List *opened,
+		      Class *cl, CONST_STRPTR name)
+{
+    struct OpenedResourceNode *orn;
+
+    if (NULL == cl)
+	return TRUE;
+
+    orn = get_orn();
+    if (!orn)
+    {
+	return FALSE;
+    }
+
+    Forbid();
+    orn->type = "Class";
+    if (NULL == name)
+	name = cl->cl_ID;
+    orn->name = StaticStrDup(name);
+    orn->addr = cl;
+    orn->count = cl->cl_ObjectCount;
+    Permit();
+
+    Enqueue(opened, (struct Node *)orn);
+    return TRUE;
+}
+
+
 /* Add opencount-based resources to the tracking list. */
 static BOOL AddOpenedResources(struct List *opened)
 {
@@ -239,6 +366,38 @@ static BOOL AddOpenedResources(struct List *opened)
     if (!AddDevs(opened))
 	return FALSE;
     if (!AddFonts(opened))
+	return FALSE;
+    if (!AddNodeNames(opened, &SysBase->ResourceList, "Resource"))
+	return FALSE;
+    if (!AddNodeNames(opened, &SysBase->IntrList, "Interrupt"))
+	return FALSE;
+    if (!AddNodeNames(opened, &SysBase->PortList, "Port"))
+	return FALSE;
+    if (!AddNodeNames(opened, &SysBase->SemaphoreList, "Semaphore"))
+	return FALSE;
+    if (!AddASemaphore(opened, &DOSBase->dl_DosListLock, "dl_DosListLock"))
+	return FALSE;
+    if (!AddAClass(opened, ((struct GadToolsBase_intern *)GadToolsBase)->buttonclass, "button"))
+	return FALSE;
+    if (!AddAClass(opened, ((struct GadToolsBase_intern *)GadToolsBase)->textclass, "text"))
+	return FALSE;
+    if (!AddAClass(opened, ((struct GadToolsBase_intern *)GadToolsBase)->sliderclass, "slider"))
+	return FALSE;
+    if (!AddAClass(opened, ((struct GadToolsBase_intern *)GadToolsBase)->scrollerclass, "scroller"))
+	return FALSE;
+    if (!AddAClass(opened, ((struct GadToolsBase_intern *)GadToolsBase)->arrowclass, "arrow"))
+	return FALSE;
+    if (!AddAClass(opened, ((struct GadToolsBase_intern *)GadToolsBase)->stringclass, "string"))
+	return FALSE;
+    if (!AddAClass(opened, ((struct GadToolsBase_intern *)GadToolsBase)->listviewclass, "listview"))
+	return FALSE;
+    if (!AddAClass(opened, ((struct GadToolsBase_intern *)GadToolsBase)->checkboxclass, "checkbox"))
+	return FALSE;
+    if (!AddAClass(opened, ((struct GadToolsBase_intern *)GadToolsBase)->cycleclass, "cycle"))
+	return FALSE;
+    if (!AddAClass(opened, ((struct GadToolsBase_intern *)GadToolsBase)->mxclass, "mx"))
+	return FALSE;
+    if (!AddAClass(opened, ((struct GadToolsBase_intern *)GadToolsBase)->paletteclass, "palette"))
 	return FALSE;
     return TRUE;
 }
@@ -253,11 +412,12 @@ static struct TrackedResources *NewResourcesState(void)
 	return NULL;
 
     /* flush */
-    FreeVec(AllocVec(~0ul/2, MEMF_CHIP));
+    FreeVec(AllocVec(~0ul/2, MEMF_ANY));
 
     /* opencount-based stuff */
     NEWLIST(&tr->opened);
-    AddOpenedResources(&tr->opened);
+    if (!AddOpenedResources(&tr->opened))
+	return NULL;
 
     /* memory */
     tr->freeMem = AvailMem(MEMF_ANY);
@@ -294,12 +454,13 @@ static void DeleteResourcesState(struct TrackedResources *rs)
     release_tr(rs);
 }
 
-void DisplayResourcesState(const struct TrackedResources *rs)
+void DisplayResourcesState(const struct TrackedResources *rs, int pagelines)
 {
     /* FIXME */
     struct OpenedResourceNode *orn;
-    IPTR tmp[3];
+    IPTR tmp[4];
     IPTR mem[1];
+    int currentlines;
 
     if (!rs)
 	return;
@@ -310,16 +471,29 @@ void DisplayResourcesState(const struct TrackedResources *rs)
     VFPrintf(Output(), " Free memory : %ld bytes\n", mem);
 
     FPuts(Output(), " Opened resources:\n");
+    currentlines = 3;
     for(orn=(struct OpenedResourceNode *)rs->opened.lh_Head;
         orn->node.ln_Succ!=NULL;
         orn=(struct OpenedResourceNode *)orn->node.ln_Succ)
     {
-	tmp[0] = (IPTR)orn->name;
-	tmp[1] = (IPTR)orn->addr;
-	tmp[2] = (IPTR)orn->count;
-	VFPrintf(Output(), "  %s (0x%lx) : %ld\n", tmp);
+	tmp[0] = (IPTR)orn->type;
+	tmp[1] = (IPTR)orn->name;
+	tmp[2] = (IPTR)orn->addr;
+	tmp[3] = (IPTR)orn->count;
+	
+	if (currentlines >= (pagelines - 2))
+	{
+	    ULONG buf;
+	    currentlines = 0;
+	    FPuts(Output(), "--- Press a key to continue ---\n");
+	    Flush(Input());
+	    Read(Input(), &buf, 1);
+	    Flush(Input());
+	}
+	VFPrintf(Output(), " %s: %s (0x%lx) : %lu\n", tmp);
+	currentlines++;
     }
-    FPuts(Output(), "--\n");
+    FPuts(Output(), "-- end of state\n");
 }
 
 /* Compute the delta between 2 resources snapshots.
@@ -360,6 +534,7 @@ static struct ResourceDiff *NewStateDiff(const struct TrackedResources *old,
 			struct ModifiedResource *mr = get_mr();
 			if (!mr)
 			    return NULL;
+			mr->type = other->type;
 			mr->name = other->name;
 			mr->addr = other->addr;
 			mr->before_count = other->count;
@@ -375,6 +550,7 @@ static struct ResourceDiff *NewStateDiff(const struct TrackedResources *old,
 	    if (!mr)
 		return NULL;
 	    
+	    mr->type = orn->type;
 	    mr->name = orn->name;
 	    mr->addr = orn->addr;
 	    mr->before_count = 0;
@@ -390,13 +566,13 @@ static struct ResourceDiff *NewStateDiff(const struct TrackedResources *old,
     return rd;
 }
 
-static void DisplayStateDiff(const struct ResourceDiff *rd)
+static void DisplayStateDiff(const struct ResourceDiff *rd, int pagelines)
 {
     /* FIXME */
     IPTR mem[2];
-    IPTR modified[4];
-    IPTR newv[3];
+    IPTR modified[5];
     struct ModifiedResource *mr;
+    int currentlines;
 
     FPuts(Output(), "LeakWatch report:\n");
     mem[0] = rd->memLost;
@@ -404,19 +580,31 @@ static void DisplayStateDiff(const struct ResourceDiff *rd)
     VFPrintf(Output(), " Memory lost : %ld byte%s\n", mem);
 
     FPuts(Output(), " Open count:\n");
+    currentlines = 3;
     for(mr=(struct ModifiedResource *)rd->modifiedOpened.lh_Head;
 	mr->node.ln_Succ!=NULL;
 	mr=(struct ModifiedResource *)mr->node.ln_Succ)
     {
-	modified[0] = (IPTR)mr->name;
-	modified[1] = (IPTR)mr->addr;
-	modified[2] = mr->before_count;
-	modified[3] = mr->after_count;
+	modified[0] = (IPTR)mr->type;
+	modified[1] = (IPTR)mr->name;
+	modified[2] = (IPTR)mr->addr;
+	modified[3] = mr->before_count;
+	modified[4] = mr->after_count;
 	
-	VFPrintf(Output(), "  %s (0x%lx) : %ld -> %d\n", modified);
+	if (currentlines >= (pagelines - 2))
+	{
+	    ULONG buf;
+	    currentlines = 0;
+	    FPuts(Output(), "--- Press a key to continue ---\n");
+	    Flush(Input());
+	    Read(Input(), &buf, 1);
+	    Flush(Input());
+	}
+	VFPrintf(Output(), " %s: %s (0x%lx) : %lu -> %lu\n", modified);
+	currentlines++;
     }
 
-    FPuts(Output(), "--\n");
+    FPuts(Output(), "-- end of diff\n");
     
 }
 
@@ -425,7 +613,6 @@ static void DeleteStateDiff(struct ResourceDiff *rd)
     /* FIXME */
     struct ModifiedResource *mr;
     struct ModifiedResource *tmpmr;
-    struct NewResource *tmpnr;
 
     if (!rd)
 	return;
@@ -484,28 +671,64 @@ static struct TrackedResources * CopyResourcesState(const struct TrackedResource
 
 int __nocommandline;
 
+void open_libs()
+{
+    GadToolsBase = OpenLibrary ( "gadtools.library", 0L );
+}
+
+void close_libs()
+{
+    CloseLibrary ( GadToolsBase );
+}
+
 int main(void)
 {
     struct TrackedResources *crs = NULL;
     struct TrackedResources *start_rs = NULL;
     BOOL quitme = FALSE;
+    int numlines = 30;
+    struct Task *this;
+    ULONG  portsig;
+    struct MsgPort *port;
+
+    this = FindTask(NULL);
+
+    port = CreateMsgPort();
+    if (!port)
+	return 2;
+    port->mp_Node.ln_Name = "LeakWatch";
+    port->mp_Node.ln_Pri  = 0;
+    AddPort(port);
+
+    portsig = 1L << port->mp_SigBit;
+
+    open_libs();
 
     FPuts(Output(), "LeakWatch running, CTRL-C to exit, CTRL-E to watch for leaks since beginning, CTRL-F to watch for leaks since last CTRL-F, CTRL-D for an usage snapshot\n");
+
     crs = NewResourcesState();
-    start_rs = CopyResourcesState(crs);
+    if (NULL == crs)
+	quitme = TRUE;
+    else
+	start_rs = CopyResourcesState(crs);
 
     while(!quitme)
     {
 	ULONG signals;
 
-	signals = Wait(SIGBREAKF_CTRL_F | SIGBREAKF_CTRL_E | SIGBREAKF_CTRL_D | SIGBREAKF_CTRL_C);
+	signals = Wait(portsig | SIGBREAKF_CTRL_F | SIGBREAKF_CTRL_E | SIGBREAKF_CTRL_D | SIGBREAKF_CTRL_C);
 
 	if (signals & SIGBREAKF_CTRL_D)
 	{
 	    struct TrackedResources *tr;
 
 	    tr = NewResourcesState();
-	    DisplayResourcesState(tr);
+	    if (NULL == tr)
+	    {
+		quitme = TRUE;
+		break;
+	    }
+	    DisplayResourcesState(tr, numlines);
 	    DeleteResourcesState(tr);
 	}
 	if (signals & SIGBREAKF_CTRL_E)
@@ -514,9 +737,14 @@ int main(void)
 
 	    DeleteResourcesState(crs);
 	    crs = NewResourcesState();
+	    if (NULL == crs)
+	    {
+		quitme = TRUE;
+		break;
+	    }
 	    /*  DisplayResourcesState(crs); */ /* only for debug */
 	    rd = NewStateDiff(start_rs, crs);
-	    DisplayStateDiff(rd);
+	    DisplayStateDiff(rd, numlines);
 	    DeleteStateDiff(rd);
 	}
 	if (signals & SIGBREAKF_CTRL_F)
@@ -525,8 +753,13 @@ int main(void)
 	    struct ResourceDiff *rd = NULL;
 
 	    crs = NewResourcesState();
+	    if (NULL == crs)
+	    {
+		quitme = TRUE;
+		break;
+	    }
 	    rd = NewStateDiff(ors, crs);
-	    DisplayStateDiff(rd);
+	    DisplayStateDiff(rd, numlines);
 	    DeleteStateDiff(rd);
 	    DeleteResourcesState(ors);
 	}
@@ -534,10 +767,30 @@ int main(void)
 	{
 	    quitme = TRUE;
 	}
+	if (signals & portsig)
+	{
+	    struct Message *msg;
+
+	    while((msg = (struct Message *)GetMsg(port)))
+	    {
+		D(bug("Received watch message.\n"));
+
+		ReplyMsg(msg);
+	    }
+
+	}
     } /* while(!quitme) */
 
     DeleteResourcesState(crs);
     DeleteResourcesState(start_rs);
 
+    close_libs();
+
+    if (port)
+    {
+	RemPort(port);
+    	DeleteMsgPort(port);
+    }
     return 0;
 }
+
