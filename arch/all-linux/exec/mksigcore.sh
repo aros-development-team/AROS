83149d1c687affb428df99ea572b360a3f2c79bf
sheutlin <sheutlin@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-08-14 18:40:01 +0000

    added powerpc
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19219 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-linux/exec/mksigcore.sh
+++ arch/all-linux/exec/mksigcore.sh
@@ -4,6 +4,8 @@ type=`grep "^struct sigcontext" /usr/include/asm/sigcontext.h | sed 's/{//'`
 handler=__sighandler_t
 if [ `uname -m` = "m68k" ]; then
   sed "s/@sigcontext@/$type/" ${1-.}/../m68k/sigcore.h.src > ${2}
+elif [ `uname -m` = "ppc" ]; then
+  sed "s/@sigcontext@/$type/" ${1-.}/../ppc/sigcore.h.src > ${2}
 else
   sed "s/@sigcontext@/$type/;s/@sighandler@/$handler/" ${1-.}/sigcore.h.src > ${2}
 fi
