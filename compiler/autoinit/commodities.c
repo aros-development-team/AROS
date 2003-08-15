cb3f7e706127a21db534eba21efe9b179ef10597
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-08-15 14:49:16 +0000

    added commodities.library
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19231 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/autoinit/commodities.c
@@ -0,0 +1,9 @@
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <intuition/intuitionbase.h>
+#include <aros/symbolsets.h>
+
+ADD2LIBS("commodities.library", 39, LIBSET_COMMODITIES_PRI, struct Library *, CxBase, NULL, NULL);
