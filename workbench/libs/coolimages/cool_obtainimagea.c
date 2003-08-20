1dfa8f78f7edd2c5b8361995bdfefe6b048e0675
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-08-20 10:35:20 +0000

    Fixed constness mismatch.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19316 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/coolimages/cool_obtainimagea.c
+++ workbench/libs/coolimages/cool_obtainimagea.c
@@ -1,7 +1,5 @@
 /*
-    Copyright © 2002, The AROS Development Team. 
-    All rights reserved.
-    
+    Copyright © 2002-2003, The AROS Development Team. All rights reserved.
     $Id$
 */
 
@@ -12,7 +10,7 @@
 /*****************************************************************************
 
     NAME */
-	AROS_LH2(struct CoolImage *, COOL_ObtainImageA,
+	AROS_LH2(const struct CoolImage *, COOL_ObtainImageA,
 
 /*  SYNOPSIS */
 	AROS_LHA(ULONG, imageid, D0),
@@ -37,96 +35,94 @@
 
     INTERNALS
 
-    HISTORY
-
 *****************************************************************************/
 {
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct Library *,CoolImagesBase)
 
-    struct CoolImage *retval = 0;
+    const struct CoolImage *image = NULL;
     
     switch(imageid)
     {
     	case COOL_SAVEIMAGE_ID:
-	    retval = &cool_saveimage;
+	    image = &cool_saveimage;
 	    break;
 	    
     	case COOL_LOADIMAGE_ID:
-	    retval = &cool_loadimage;
+	    image = &cool_loadimage;
 	    break;
 	    
     	case COOL_USEIMAGE_ID:
-	    retval = &cool_useimage;
+	    image = &cool_useimage;
 	    break;
 	    
     	case COOL_CANCELIMAGE_ID:
-	    retval = &cool_cancelimage;
+	    image = &cool_cancelimage;
 	    break;
 	    
     	case COOL_DOTIMAGE_ID:
-	    retval = &cool_dotimage;
+	    image = &cool_dotimage;
 	    break;
 	    
     	case COOL_DOTIMAGE2_ID:
-	    retval = &cool_dotimage2;
+	    image = &cool_dotimage2;
 	    break;
 	    
     	case COOL_WARNIMAGE_ID:
-	    retval = &cool_warnimage;
+	    image = &cool_warnimage;
 	    break;
 	    
     	case COOL_DISKIMAGE_ID:
-	    retval = &cool_diskimage;
+	    image = &cool_diskimage;
 	    break;
 	    
     	case COOL_SWITCHIMAGE_ID:
-	    retval = &cool_switchimage;
+	    image = &cool_switchimage;
 	    break;
 	    
     	case COOL_MONITORIMAGE_ID:
-	    retval = &cool_monitorimage;
+	    image = &cool_monitorimage;
 	    break;
 	    
     	case COOL_MOUSEIMAGE_ID:
-	    retval = &cool_mouseimage;
+	    image = &cool_mouseimage;
 	    break;
 	    
     	case COOL_INFOIMAGE_ID:
-	    retval = &cool_infoimage;
+	    image = &cool_infoimage;
 	    break;
 	    
     	case COOL_ASKIMAGE_ID:
-	    retval = &cool_askimage;
+	    image = &cool_askimage;
 	    break;
 	    
     	case COOL_KEYIMAGE_ID:
-	    retval = &cool_keyimage;
+	    image = &cool_keyimage;
 	    break;
 	    
     	case COOL_CLOCKIMAGE_ID:
-	    retval = &cool_clockimage;
+	    image = &cool_clockimage;
 	    break;
 	    
     	case COOL_FLAGIMAGE_ID:
-	    retval = &cool_flagimage;
+	    image = &cool_flagimage;
 	    break;
 	    
     	case COOL_HEADIMAGE_ID:
-	    retval = &cool_headimage;
+	    image = &cool_headimage;
 	    break;
 	    
     	case COOL_WINDOWIMAGE_ID:
-	    retval = &cool_windowimage;
+	    image = &cool_windowimage;
 	    break;
 	    
     	case COOL_KBDIMAGE_ID:
-	    retval = &cool_kbdimage;
+	    image = &cool_kbdimage;
 	    break;
 	    
     } /* switch(imageid) */
     
-    return retval;
+    return image;
     
     AROS_LIBFUNC_EXIT
 
