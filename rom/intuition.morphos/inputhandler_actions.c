589f304a4d6fb4222843c4503ec995609f00a613
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-08-22 19:21:27 +0000

    We have (Alloc|Free)VecPooled() in AROS now.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19400 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/intuition.morphos/inputhandler_actions.c
+++ rom/intuition.morphos/inputhandler_actions.c
@@ -36,10 +36,6 @@
 #include "inputhandler_actions.h"
 #include "menus.h"
 
-//FIXME: implement the functions...
-#define AllocVecPooled(pool, size) AllocVec(size, MEMF_ANY)
-#define FreeVecPooled(pool, ptr) FreeVec(ptr)
-
 #undef DEBUG
 #define DEBUG 0
 #include <aros/debug.h>
