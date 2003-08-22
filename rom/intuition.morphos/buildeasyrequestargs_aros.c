3e5942b6876bf3f459d04c8969fc37c97bd95ec5
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-08-22 11:04:16 +0000

    Don't use {IA_EdgesOnly,TRUE} for button frame images. Don't pass GA_Text during button gadget creation, but set after using SetAttrs() to avoid passed gadget size being ignored/changed in OM_NEW.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19389 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/intuition.morphos/buildeasyrequestargs_aros.c
+++ rom/intuition.morphos/buildeasyrequestargs_aros.c
@@ -545,7 +545,6 @@ static struct Gadget *buildeasyreq_makegadgets(struct reqdims *dims,
 	struct TagItem frame_tags[] =
         {
             {IA_FrameType	, FRAME_BUTTON	    	    	    	},
-            {IA_EdgesOnly	, TRUE 	    	    	    	    	},
             {IA_Width   	, dims->gadgetwidth 	    	    	},
             {IA_Height  	, dims->fontheight + BUTTONBORDER_Y * 2 },
             {TAG_DONE   	    	    	    	    	    	}
@@ -587,13 +586,17 @@ static struct Gadget *buildeasyreq_makegadgets(struct reqdims *dims,
 			    {GA_Top	 , dims->height -
 			     scr->WBorBottom - dims->fontheight -
 			     OUTERSPACING_Y - BUTTONBORDER_Y * 2		},
-			    {GA_Text	 , (IPTR)gadgetlabels[currentgadget]		},
 			    {GA_Image	 , (IPTR)gadgetframe				},
 			    {GA_RelVerify, TRUE						},
 			    {GA_DrawInfo , (IPTR)dri					},
 			    {TAG_DONE							}
 		    };
-
+    	    	struct TagItem gad2_tags[] =
+		{
+    			    {GA_Text	 , (IPTR)gadgetlabels[currentgadget]		},
+		    	    {TAG_DONE	    	    	    	    	    	    	}
+		};
+		
 		thisgadget = NewObjectA(NULL, FRBUTTONCLASS, gad_tags);
 
 		if (currentgadget == 0)
@@ -604,6 +607,8 @@ static struct Gadget *buildeasyreq_makegadgets(struct reqdims *dims,
 			intrequest_freegadgets(gadgetlist, IntuitionBase);
 			return NULL;
 		}
+
+    		SetAttrsA(thisgadget, gad2_tags);
 
 		xoffset += spacing;
 	}
