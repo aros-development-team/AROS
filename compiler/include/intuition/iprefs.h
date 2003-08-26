10cd369fad19bb2280a36c8ffe3d1da5e3c01417
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-08-26 20:11:19 +0000

    Stuff for communication between IPrefs and Intuition through SetIPrefs().
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19441 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/intuition/iprefs.h
@@ -0,0 +1,38 @@
+#ifndef INTUITION_IPREFS_H
+#define INTUITION_IPREFS_H
+
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: PRIVATE/TOP SECRET!!! Communication between IPrefs program and Intuition
+    Lang: english
+*/
+
+#ifndef EXEC_TYPES_H
+#   include <exec/types.h>
+#endif
+
+#define IPREFS_TYPE_ICONTROL 0
+
+struct IScreenModePrefs
+{
+    ULONG smp_DisplayID;
+    UWORD smp_Width;
+    UWORD smp_Height;
+    UWORD smp_Depth;
+    UWORD smp_Control;
+};
+
+struct IIControlPrefs
+{
+    UWORD ic_TimeOut;
+    WORD  ic_MetaDrag;
+    ULONG ic_Flags;
+    UBYTE ic_WBtoFront;
+    UBYTE ic_FrontToBack;
+    UBYTE ic_ReqTrue;
+    UBYTE ic_ReqFalse;
+};
+
+#endif /* GRAPHICS_SCREENS_H */
