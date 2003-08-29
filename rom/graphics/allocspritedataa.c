61032cc27d673f44f7038de5014d6cce31a12fe3
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-08-29 21:36:49 +0000

    Some code for extended sprites. Might want to try to emulate sprites with gels.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19502 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/graphics/allocspritedataa.c
+++ rom/graphics/allocspritedataa.c
@@ -5,11 +5,17 @@
     Desc: Graphics function AllocSpriteDataA()
     Lang: english
 */
+#include <proto/exec.h>
+#include <proto/graphics.h>
+#include <proto/utility.h>
 #include <aros/debug.h>
 #include <graphics/gfx.h>
 #include <graphics/sprite.h>
+#include <graphics/scale.h>
 #include <utility/tagitem.h>
 #include <exec/exec.h>
+#include <string.h>
+#include "graphics_intern.h"
 
 /*****************************************************************************
 
@@ -20,7 +26,7 @@
 
 /*  SYNOPSIS */
         AROS_LHA(struct BitMap *, bitmap, A2),
-        AROS_LHA(struct TagItem *, taglist, A1),
+        AROS_LHA(struct TagItem *, tagList, A1),
 
 /*  LOCATION */
         struct GfxBase *, GfxBase, 170, Graphics)
@@ -35,8 +41,8 @@
     RESULT
         SpritePtr - pointer to a ExtSprite structure,
                     or NULL if there is a failure.
-                    You should pass this poionter to FreeSpriteData()
-                    when finished with this sprite
+                    You should pass this pointer to FreeSpriteData()
+                    when this sprite is not needed anymore.
 
     NOTES
 
@@ -56,13 +62,85 @@
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct GfxBase *,GfxBase)
 
-    struct ExtSprite *sprite;
+    struct ExtSprite *sprite = NULL;
     
-#warning TODO: Write graphics/AllocSpriteDataA()
-    aros_print_not_implemented ("AllocSpriteDataA");
+    if (NULL != bitmap) {
+#define SCALE_NORMAL  16
+        ULONG height = (ULONG)GetBitMapAttr(bitmap, BMA_HEIGHT);
+        ULONG width = 16;
+        struct TagItem * tstate = tagList;
+        struct TagItem * tag;
+        struct BitScaleArgs bsa;
+        LONG xrep = 0, yrep = 0;
 
-    sprite = AllocVec(sizeof(*sprite), MEMF_PUBLIC | MEMF_CLEAR);
+        memset(&bsa, 0, sizeof(bsa));
     
+        while (NULL != (tag = NextTagItem(&tstate))) {
+            switch (tag->ti_Tag) {
+                case SPRITEA_Width:
+                	width = tag->ti_Data;
+                break;
+            
+                case SPRITEA_XReplication:
+                	xrep = tag->ti_Data;
+                break;
+            
+                case SPRITEA_YReplication:
+                	yrep = tag->ti_Data;
+                break;
+            
+                case SPRITEA_OutputHeight:
+                    if (tag->ti_Data > (ULONG)GetBitMapAttr(bitmap, BMA_HEIGHT)) {
+                        return NULL;
+                    }
+                    height = tag->ti_Data;
+                break;
+            
+                case SPRITEA_Attached:
+                
+                break;
+            
+            }
+        }
+
+        sprite = AllocVec(sizeof(*sprite), MEMF_PUBLIC | MEMF_CLEAR);
+        if (NULL != sprite) {
+
+            bsa.bsa_SrcWidth  = GetBitMapAttr(bitmap, BMA_WIDTH);
+            bsa.bsa_SrcHeight = GetBitMapAttr(bitmap, BMA_HEIGHT);
+
+            if (xrep > 0) {
+                bsa.bsa_XDestFactor = SCALE_NORMAL << xrep;
+            } else {
+                bsa.bsa_XDestFactor = SCALE_NORMAL >> (-xrep);
+            }
+
+            if (yrep > 0) {
+                bsa.bsa_YDestFactor = SCALE_NORMAL << yrep;
+            } else {
+                bsa.bsa_YDestFactor = SCALE_NORMAL >> (-yrep);
+            }
+
+            bsa.bsa_XSrcFactor  = SCALE_NORMAL;
+            bsa.bsa_YSrcFactor  = SCALE_NORMAL;
+            bsa.bsa_SrcBitMap   = bitmap;
+            bsa.bsa_DestBitMap  = AllocBitMap(width,
+                                              height,
+                                              4,
+                                              BMF_CLEAR|BMF_DISPLAYABLE,
+                                              NULL);
+            BitMapScale(&bsa);
+        
+            sprite->es_SimpleSprite.height = height;
+            sprite->es_SimpleSprite.x      = 0;
+            sprite->es_SimpleSprite.y      = 0;
+            sprite->es_SimpleSprite.num    = 0;
+            sprite->es_wordwidth           = width >> 4;
+            sprite->es_flags               = 0;
+            sprite->es_BitMap              = bsa.bsa_DestBitMap;
+            
+        }
+    }
     return sprite;
 
     AROS_LIBFUNC_EXIT
