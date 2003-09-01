6aab953ac9791a49e493ec9b75b62ee715816200
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-09-01 13:47:48 +0000

    Moved the __showerror function in a file of its own and made it available publicly.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19546 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/autoinit/libraries.c
+++ compiler/autoinit/libraries.c
@@ -9,57 +9,16 @@
 #include <proto/exec.h>
 #include <dos/dos.h>
 #include <aros/symbolsets.h>
+#include <aros/autoinit.h>
 #include <intuition/intuition.h>
 #include <dos/dosextens.h>
 #include <proto/intuition.h>
 #include <proto/dos.h>
-#include <stdarg.h>
 #include <stdlib.h>
 
 DECLARESET(LIBS);
 
-int __forceerrorrequester __attribute__((weak)) = 0;
-
 int __includelibrarieshandling;
-
-static void __showerror(char *title, char *format, ...)
-{
-    AROS_GET_SYSBASE_OK
-    struct Process *me = (struct Process *)FindTask(0);
-
-    va_list args;
-    va_start(args, format);
-
-
-    if (me->pr_CLI && !__forceerrorrequester)
-    {
-        PutStr(title);
-        PutStr(": ");
-#warning This next line might break on bizarre architectures.
-        VPrintf(format, (IPTR *)args);
-        PutStr("\n");
-    }
-    else
-    {
-     	if ((IntuitionBase = (struct IntuitionBase *)OpenLibrary("intuition.library", 0)))
-	{
-    	    struct EasyStruct es =
-    	    {
-		sizeof(struct EasyStruct),
-		0,
-		title,
-		format,
-		"Exit"
-	    };
-
-	    EasyRequestArgs(NULL, &es, NULL, (APTR)args);
-	    
-	    CloseLibrary((struct Library *)IntuitionBase);
-	}
-    }
-
-    va_end(args);
-}
 
 int set_open_libraries(void)
 {
