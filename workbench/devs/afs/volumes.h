4dad3cfc770e2eb83ab7bef2208024107ee1584d
sheutlin <sheutlin@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-09-04 19:16:12 +0000

    * calculate number of blocks in a filesystem
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19616 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/devs/afs/volumes.h
+++ workbench/devs/afs/volumes.h
@@ -12,30 +12,31 @@
 
 struct Volume {
 	struct Node ln;
-	struct Device *device;		// the handler this volume uses
+	struct Device *device;       /* the handler this volume uses */
 	struct DeviceList devicelist;
-	ULONG SizeBlock;				// Block size in words
+	ULONG SizeBlock;             /* Block size in words */
 	
 	struct AfsHandle *locklist;
-	struct AfsHandle ah;	// root handle (unfilled except header_block)
+	struct AfsHandle ah;         /* root handle (unfilled except header_block) */
 	char *blockdevice;
 	ULONG unit;
 	struct IOHandle ioh;
 	struct BlockCache *blockcache;
 
-	ULONG usedblockscount;
-	ULONG rootblock;
-	ULONG startblock;
-	ULONG lastblock;
+	ULONG usedblockscount;       /* nr of used blocks */
+	ULONG countblocks;           /* nr of blocks in filesystem */
+	ULONG rootblock;             /* rootblock position */
+	ULONG startblock;            /* absolute nr of start block on whole HD */
+	ULONG lastblock;             /* absolute nr of last block on whole HD */
 	ULONG dostype;
 	ULONG bitmapblockpointers[25];
 	ULONG bitmapextensionblock;
 
-	struct BlockCache *bitmapblock;	// last bitmap block used for marking
-	ULONG bstartblock;						// first block marked in "bitmapblock"
-	ULONG lastextensionblock;			// last used extensionblock (0=volume->bitmapblocks)
-	ULONG lastposition;					// last position in extensionblock
-	ULONG lastaccess;					// last marked block
+	struct BlockCache *bitmapblock; /* last bitmap block used for marking */
+	ULONG bstartblock;              /* first block marked in "bitmapblock" */
+	ULONG lastextensionblock; /* last used extensionblock (0=volume->bitmapblocks) */
+	ULONG lastposition;             /* last position in extensionblock */
+	ULONG lastaccess;               /* last marked block */
 
 	UWORD bootblocks;
 	UBYTE dosflags;
