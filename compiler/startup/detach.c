6374f7bf1f2c50c835b454bf4baedd334e6f4e88
falemagn <falemagn@gmail.com>
2003-09-10 18:29:09 +0000

    moved here all the stuff related to startup code
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19722 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/startup/detach.c
@@ -0,0 +1,133 @@
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: Common startup code
+    Lang: english
+
+    Use: To make a program detach itself from the launching CLI, use
+         detach.o as your program's start file, before any other usual start
+         file.
+
+	 In your program's source code you have the option to use some
+	 variables to determine the behaviour of the detaching procedure.
+
+	 These variables are defined as "weak", so that defining them both
+	 in this module and in your program doesn't produce linking errors.
+	 Defining these variables in your program's source code makes it
+	 possible to make your program stay independent from this module,
+	 so that you can decide whether your program should be detacheable
+	 or not just by adding a flag to the linker's command line and make
+	 the program still work in case you decided to not make it
+	 detacheable.
+
+         These are the variables:
+
+             1) LONG __detacher_must_wait_for_signal;
+
+		Define the above variable only if you need the detacher
+		process to wait for a signal to arrive, before exiting and
+		effectively detaching your program.
+
+		It must contain a bitmask suited for the use of
+		exec.library/Wait()
+
+	     2) struct Process *__detacher_process;
+
+		The above variable will hold a pointer to the detacher
+		process. Use it for anything you wish, but specially for
+		sending to it the "termination" signal.
+
+		This pointer is guaranteed to hold NULL when there's no
+		detacher process around anymore.
+
+                Remember to initialize it to NULL!
+
+             3) STRPTR __detached_name;
+
+		Define the above variable only if you want the detached
+		process to have a name different than the detacher's one.
+		Set its value to a pointer to a string holding the name you
+		want the detached process to have.
+*/
+
+#include <aros/config.h>
+#include <dos/dos.h>
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <aros/asmcall.h>
+
+#if (AROS_FLAVOUR & AROS_FLAVOUR_NATIVE)
+asm
+(
+    ".text\n"
+    "\n"
+    "move.l      4.w,a6\n"
+    "bra         _detach_entry\n"
+);
+#endif
+
+AROS_UFP3(LONG, __startup_entry,
+AROS_UFPA(char *,argstr,A0),
+AROS_UFPA(ULONG,argsize,D0),
+AROS_UFPA(struct ExecBase *,sysbase,A6));
+
+extern LONG            __detacher_must_wait_for_signal __attribute__((weak));
+extern struct Process *__detacher_process              __attribute__((weak));
+extern STRPTR          __detached_name                 __attribute__((weak));
+
+AROS_UFH3(LONG, __detach_entry,
+AROS_UFHA(char *,argstr,A0),
+AROS_UFHA(ULONG,argsize,D0),
+AROS_UFHA(struct ExecBase *,SysBase,A6))
+{
+    struct DosLibrary           *DOSBase;
+    struct CommandLineInterface *cli;
+    struct Process              *newproc;
+    BPTR                         mysegment = NULL;
+
+    DOSBase = (struct DosLibrary *)OpenLibrary(DOSNAME, 39);
+    if (!DOSBase) return RETURN_FAIL;
+
+    cli = Cli();
+    /*
+        We cannot be started from WorkBench
+    */
+    if (!cli) return RETURN_FAIL;
+
+    mysegment = cli->cli_Module;
+    cli->cli_Module = NULL;
+
+    if (!__detached_name)
+      __detached_name = FindTask(NULL)->tc_Node.ln_Name;
+
+    {
+        struct TagItem tags[] =
+        {
+	    { NP_Seglist,   (IPTR)mysegment       },
+	    { NP_Entry,     (IPTR)__startup_entry },
+	    { NP_Name,      (IPTR)__detached_name },
+	    { NP_Arguments, (IPTR)argstr          },
+	    { NP_Cli,       TRUE                  },
+            { TAG_DONE,     0                     }
+        };
+
+	__detacher_process = (struct Process *)FindTask(NULL);
+
+	/* CreateNewProc() will take care of freeing the seglist */
+	newproc = CreateNewProc(tags);
+    }
+
+    CloseLibrary((struct Library *)DOSBase);
+
+    if (newproc && __detacher_must_wait_for_signal)
+        Wait(__detacher_must_wait_for_signal);
+
+    __detacher_process = NULL;
+
+    return newproc ? RETURN_OK : RETURN_FAIL;
+}
+
+LONG            __detacher_must_wait_for_signal __attribute__((weak)) = 0;
+struct Process *__detacher_process              __attribute__((weak)) = NULL;
+STRPTR          __detached_name                 __attribute__((weak)) = NULL;
---
+++ compiler/startup/startup.c
@@ -0,0 +1,169 @@
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: Common startup code
+    Lang: english
+*/
+#include <aros/config.h>
+#include <setjmp.h>
+#include <dos/dos.h>
+#include <exec/memory.h>
+#include <workbench/startup.h>
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <aros/asmcall.h>
+#include <aros/debug.h>
+#include <aros/symbolsets.h>
+#include <aros/startup.h>
+
+#include "etask.h"
+
+
+/* Don't define symbols before the entry point. */
+extern struct ExecBase  *SysBase;
+extern struct WBStartup *WBenchMsg;
+extern int main (int argc, char ** argv);
+
+
+DECLARESET(INIT);
+DECLARESET(EXIT);
+DECLARESET(CTORS);
+DECLARESET(DTORS);
+
+/*
+    This won't work for normal AmigaOS because you can't expect SysBase to be
+    in A6. The correct way is to use *(struct ExecBase **)4 and because GCC
+    emits strings for a certain function _before_ the code the program will
+    crash immediately because the first element in the code won't be valid
+    assembler code.
+*/
+
+extern char *__argstr;
+extern ULONG __argsize;
+extern char **__argv;
+extern int  __argc;
+extern struct DosLibrary *DOSBase;
+
+extern struct aros_startup __aros_startup;
+
+#warning TODO: reset and initialize the FPU
+#warning TODO: resident startup
+AROS_UFH3(LONG, __startup_entry,
+    AROS_UFHA(char *,argstr,A0),
+    AROS_UFHA(ULONG,argsize,D0),
+    AROS_UFHA(struct ExecBase *,sysbase,A6)
+)
+{
+    AROS_USERFUNC_INIT
+
+    struct Process *myproc;
+
+    SysBase = sysbase;
+
+    /*
+        No one program will be able to do anything useful without the dos.library,
+        so we open it here instead of using the automatic opening system
+    */
+    DOSBase = (struct DosLibrary *)OpenLibrary(DOSNAME, 39);
+    if (!DOSBase) return RETURN_FAIL;
+
+    __argstr  = argstr;
+    __argsize = argsize;
+
+    myproc = (struct Process *)FindTask(NULL);
+
+    GetIntETask(myproc)->iet_startup = &__aros_startup;
+
+    /* Do we have a CLI structure? */
+    if (!myproc->pr_CLI)
+    {
+	/* Workbench startup. Get WBenchMsg and pass it to main() */
+
+	WaitPort(&myproc->pr_MsgPort);
+	WBenchMsg = (struct WBStartup *)GetMsg(&myproc->pr_MsgPort);
+	__argv = (char **) WBenchMsg;
+        __argc = 0;
+    }
+
+    if (!setjmp(__aros_startup.as_startup_jmp_buf))
+    {
+        if
+        (
+            !(__aros_startup.as_startup_error = set_open_libraries()) &&
+            !(__aros_startup.as_startup_error = set_call_funcs(SETNAME(INIT), 1))
+        )
+        {
+            /* ctors get called in inverse order than init funcs */
+            set_call_funcs(SETNAME(CTORS), -1);
+
+	    __aros_startup.as_startup_error = main (__argc, __argv);
+        }
+
+    }
+
+    /* dtors get called in inverse order than exit funcs */
+    {
+        int n = 1;
+
+        while (SETNAME(DTORS)[n]) ((VOID_FUNC)(SETNAME(DTORS)[n++]))();
+    }
+
+    set_call_funcs(SETNAME(EXIT), -1);
+    set_close_libraries();
+
+    /* Reply startup message to Workbench */
+    if (WBenchMsg)
+    {
+        Forbid(); /* make sure we're not UnLoadseg()ed before we're really done */
+        ReplyMsg((struct Message *) WBenchMsg);
+    }
+
+    CloseLibrary((struct Library *)DOSBase);
+
+    return __aros_startup.as_startup_error;
+
+    AROS_USERFUNC_EXIT
+} /* entry */
+
+/* if the programmer hasn't defined a symbol with the name __nocommandline
+   then the code to handle the commandline will be included from the autoinit.lib
+*/
+//extern void __nocommandline(void);
+extern int __nocommandline;
+static void *__importcommandline __attribute__((unused)) = &__nocommandline;
+
+/* pass these values to the command line handling function */
+char *__argstr;
+ULONG __argsize;
+
+/* the command line handling functions will pass these values back to us */
+char **__argv;
+int  __argc;
+
+struct ExecBase *SysBase;
+struct DosLibrary *DOSBase;
+struct WBStartup *WBenchMsg;
+static struct aros_startup __aros_startup;
+
+int do_arosc_internals = 1;
+
+DEFINESET(CTORS);
+DEFINESET(DTORS);
+DEFINESET(INIT);
+DEFINESET(EXIT);
+
+
+/*
+    Stub function for GCC __main().
+
+    The __main() function is originally used for C++ style constructors
+    and destructors in C. This replacement does nothing and gets rid of
+    linker-errors about references to __main().
+*/
+#ifdef AROS_NEEDS___MAIN
+void __main(void)
+{
+    /* Do nothing. */
+}
+#endif
