db26cc95b689e994325e9ba78c1695004570739d
sheutlin <sheutlin@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-09-22 21:23:25 +0000

    do also replace @sighandler@
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19806 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-linux/exec/mksigcore.sh
+++ arch/all-linux/exec/mksigcore.sh
@@ -5,7 +5,7 @@ handler=__sighandler_t
 if [ `uname -m` = "m68k" ]; then
   sed "s/@sigcontext@/$type/" ${1-.}/../m68k/sigcore.h.src > ${2}
 elif [ `uname -m` = "ppc" ]; then
-  sed "s/@sigcontext@/$type/" ${1-.}/../ppc/sigcore.h.src > ${2}
+  sed "s/@sigcontext@/$type/;s/@sighandler@/$handler/" ${1-.}/../ppc/sigcore.h.src > ${2}
 else
   sed "s/@sigcontext@/$type/;s/@sighandler@/$handler/" ${1-.}/sigcore.h.src > ${2}
 fi
