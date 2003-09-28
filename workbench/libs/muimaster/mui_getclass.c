d27952479127de9648913675de0a7a6b30834c4c
falemagn <falemagn@gmail.com>
2003-09-28 17:10:51 +0000

    Reworked the way MUI_GetClass() and related functions work: it doesn't use anymore a dynamically (re)allocated array, but rather an exec list by taking advantage of the already existing MinNode in the IClass structure. Also, it now does proper locking on shared data whilst before it didn't use to. Also added proper documentation
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19856 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/mui_getclass.c
+++ workbench/libs/muimaster/mui_getclass.c
@@ -17,91 +17,45 @@
 /*#define MYDEBUG 1*/
 #include "debug.h"
 
-static CONST_STRPTR const searchpaths[] = {
-    "PROGDIR:Zune/%s",
-    "Zune/%s",
-    "Classes/Zune/%s",
-#ifndef __AROS__
-    "MUI/%s",
-    "PROGDIR:MUI/%s",
-#endif
-    NULL,
-};
-
-#ifndef __AROS__
-struct MUI_CustomClass *MCC_Query(ULONG d0);
-#pragma  libcall mcclib MCC_Query 01e 001
-#endif
-
-static struct IClass *load_external_class(CONST_STRPTR classname, struct Library *MUIMasterBase)
-{
-    struct Library         *mcclib = NULL;
-    struct MUI_CustomClass *mcc    = NULL ;
-    Class                  *cl     = NULL;
-    CONST_STRPTR const     *pathptr;
-    TEXT                    s[255];
-
-    for (pathptr = searchpaths; *pathptr; pathptr++)
-    {
-	snprintf(s, 255, *pathptr, classname);
-	
-	D(bug("Trying opening of %s\n",s));
-
-        if ((mcclib = OpenLibrary(s, 0)))
-	{
-	    /* call MCC_Query(0) */
-	
-#ifdef __AROS__		
-	    mcc = AROS_LVO_CALL1(struct MUI_CustomClass *,
-				 AROS_LCA(LONG, 0, D0),
-				 struct Library *, mcclib, 5, lib);
-#else
-	    D(bug("Calling MCC Query. Librarybase at 0x%lx\n",mcclib));
-	    mcc = MCC_Query(0);
-#endif			
-	    if (mcc)
-	    {
-		cl = mcc->mcc_Class;
-		if (cl)
-		{
-		    mcc->mcc_Module = mcclib;
-		    D(bug("Successfully opened %s as external class\n",classname));
-		    return cl;
-		}
-	    }
-		
-	    if (!cl) CloseLibrary(mcclib);
-	}
-    }
-    D(bug("Failed to open external class %s\n",classname));
-    return NULL;
-}
-
 /*****************************************************************************
 
     NAME */
 	AROS_LH1(struct IClass *, MUI_GetClass,
 
 /*  SYNOPSIS */
-	AROS_LHA(char *, classname, A0),
+	AROS_LHA(ClassID, classid, A0),
 
 /*  LOCATION */
 	struct Library *, MUIMasterBase, 13, MUIMaster)
 
 /*  FUNCTION
+        Get a pointer to a MUI Class.
+
+	The main use for this function is to retrieve the pointer to a MUI class
+	for use by intuition.library/MakeClass() as superclass pointer. However,
+	this function is obsolete since MUI V8, so DO NOT USE IT, use
+	MUI_CreateCustomClass() instead.
 
     INPUTS
+	classid - the ID of the class whose pointer is to be retrieved.
 
     RESULT
+        The class pointer is returned. DO NOT use it for any other reason that
+	as an argument of intuition.library/MakeClass(). DO NOT assume anything
+	about its content.
 
     NOTES
+        Once the pointer is not needed anymore, do not forget to call MUI_FreeClass().
+
+        This function is OBSOLETE, don't use it, use MUI_CreateCustomClass instead.
 
     EXAMPLE
 
     BUGS
-	The function itself is a bug ;-) Remove it!
 
     SEE ALSO
+        MUI_FreeClass(), MUI_CreateCustomClass(), MUI_DeleteCustomClass()
+
 
     INTERNALS
 
@@ -110,55 +64,19 @@ static struct IClass *load_external_class(CONST_STRPTR classname, struct Library
 *****************************************************************************/
 {
     AROS_LIBFUNC_INIT
-    AROS_LIBBASE_EXT_DECL(struct Library *,MUIMasterBase)
+    AROS_LIBBASE_EXT_DECL(struct Library *, MUIMasterBase)
 
-    struct IClass *cl = NULL;
+    Class *cl;
 
-    if (classname == NULL)
+    if (!classid)
 	return NULL;
 
-    if (!(cl = GetPublicClass(classname,MUIMasterBase)))
-    {
-	/* do we have space for another class pointer? */
-	if (MUIMB(MUIMasterBase)->ClassCount == MUIMB(MUIMasterBase)->ClassSpace)
-	{
-	  struct IClass **t;
-	  int newSpace = MUIMB(MUIMasterBase)->ClassSpace ? MUIMB(MUIMasterBase)->ClassSpace+16 : 32;
+    cl = ZUNE_GetBuiltinClass(classid, MUIMasterBase);
 
-	  if (!(t = (struct IClass **)AllocVec(sizeof(Class *) * newSpace, MEMF_ANY)))
-	    return NULL;
+    if (!cl)
+        cl = ZUNE_GetExternalClass(classid, MUIMasterBase);
 
-	  if (MUIMB(MUIMasterBase)->Classes)
-	  {
-	    CopyMem(MUIMB(MUIMasterBase)->Classes, t, sizeof(struct IClass *) * MUIMB(MUIMasterBase)->ClassSpace);
-	    FreeVec(MUIMB(MUIMasterBase)->Classes);
-	  }
-
-	  MUIMB(MUIMasterBase)->Classes    = t;
-	  MUIMB(MUIMasterBase)->ClassSpace = newSpace;
-	}
-
-	cl = CreateBuiltinClass(classname, MUIMasterBase);
-
-    	if (!cl)
-	{
-	    cl = load_external_class(classname, MUIMasterBase);
-	}
-
-	if (cl)
-	{
-#ifndef __MAXON__
-#warning FIXME: I should increase the open count of library (use cl->hook->data)
-#endif
-#if 0
-	    ASSERT(cl->cl_ID != NULL);
-	    ASSERT(strcmp(classname, cl->cl_ID) == 0);
-#endif
-	    MUIMB(MUIMasterBase)->Classes[MUIMB(MUIMasterBase)->ClassCount++] = cl;
-	}
-    }
     return cl;
-
 
     AROS_LIBFUNC_EXIT
 
