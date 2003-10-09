ab61a09cd9fe5e6e566e45514aefe2234d4b6069
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-10-09 20:12:20 +0000

    Implemented popup menu (cycle to menu). Popup menu background rendering still a quick thing, ie. unfinished (atm simply renders with color #0 and RectFill).
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19929 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/classes/cycle.c
+++ workbench/libs/muimaster/classes/cycle.c
@@ -22,26 +22,35 @@
 #include "imspec.h"
 #include "muimaster_intern.h"
 #include "support.h"
+#include "prefs.h"
+#include "textengine.h"
 
 extern struct Library *MUIMasterBase;
 
 struct MUI_CycleData
 {
-    const char **entries;
-    int entries_active;
-    int entries_num;
-    int entries_width;
-    int entries_height;
-
-    int cycle_width;
-    int cycle_height;
-
-    Object *pageobj;
-
-    struct MUI_EventHandlerNode ehn;
-    struct Hook     	    	pressedhook;
+    const char      	      	**entries;
+    int     	    	    	  entries_active;
+    int     	    	    	  entries_num;
+    int     	    	    	  entries_width;
+    int     	    	    	  entries_height;
+    int     	    	    	  cycle_width;
+    int     	    	    	  cycle_height;
+    Object  	    	    	 *pageobj;
+    Object  	    	    	 *imgobj;   
+    struct MUI_EventHandlerNode   ehn;
+    struct Hook     	    	  pressedhook;
+    struct Window   	    	 *popwin;
+    WORD    	    	    	  popitemwidth;
+    WORD    	    	    	  popitemheight;
+    WORD    	    	    	  popitemoffx;
+    WORD    	    	    	  popitemoffy;
+    WORD    	    	    	  activepopitem;
 
 };
+
+#define POPITEM_EXTRAWIDTH 4
+#define POPITEM_EXTRAHEIGHT 2
 
 void PressedHookFunc(struct Hook *hook, Object *obj, APTR msg)
 {
@@ -93,6 +102,8 @@ static IPTR Cycle_New(struct IClass *cl, Object *obj, struct opSet *msg)
     data = INST_DATA(cl, obj);
 
     data->pageobj = pageobj;
+    data->imgobj = imgobj;
+    
     data->pressedhook.h_Entry = HookEntry;
     data->pressedhook.h_SubEntry = (HOOKFUNC)PressedHookFunc;
     data->pressedhook.h_Data = cl;
@@ -278,15 +289,325 @@ STATIC IPTR Cycle_Cleanup(struct IClass *cl, Object *obj, struct MUIP_Cleanup *m
     return DoSuperMethodA(cl,obj,(Msg)msg);
 }
 
+static void KillPopupWin(Object *obj, struct MUI_CycleData *data)
+{
+    struct MinList *childlist;
+    Object  	   *child, *cstate;
+
+    if (data->popwin)
+    {
+    	CloseWindow(data->popwin);
+    	data->popwin = NULL;
+    }
+    
+    get(data->pageobj, MUIA_Group_ChildList, (IPTR *)&childlist);
+
+    cstate = (Object *)childlist->mlh_Head;
+    while((child = NextObject(&cstate)))
+    {
+    	ZText *text;
+	
+	get(child, MUIA_UserData, (IPTR *)&text);
+	if (!text) break;
+	
+	zune_text_destroy(text);
+	
+	set(child, MUIA_UserData, 0);
+    }
+    
+    if (data->ehn.ehn_Events & IDCMP_MOUSEMOVE)
+    {
+    	DoMethod(_win(obj), MUIM_Window_RemEventHandler, (IPTR)&data->ehn);
+	data->ehn.ehn_Events &= ~IDCMP_MOUSEMOVE;
+	DoMethod(_win(obj), MUIM_Window_AddEventHandler, (IPTR)&data->ehn);    	
+    }
+    
+}
+
+static void RenderPopupItem(Object *obj, struct MUI_CycleData *data, WORD which)
+{
+    struct MinList  *childlist;
+    struct RastPort *saverp;
+    Object  	    *child, *cstate;
+    ZText   	    *text;
+    WORD    	     x1, y1, x2, y2, i = which;
+    
+    get(data->pageobj, MUIA_Group_ChildList, (IPTR *)&childlist);
+    cstate = (Object *)childlist->mlh_Head;
+
+    while((child = NextObject(&cstate)) && i--)
+    {
+    }
+   
+    if (!child) return;
+    
+    get(child, MUIA_UserData, (IPTR *)&text);
+    if (!text) return; /* paranoia */
+    
+    saverp = _rp(obj);
+    _rp(obj) = data->popwin->RPort;
+    
+    x1 = data->popitemoffx;
+    x2 = x1 + data->popitemwidth - 1;
+    y1 = data->popitemoffy + which * data->popitemheight;
+    y2 = y1 + data->popitemheight - 1;
+    
+    if (which == data->activepopitem)
+    {
+    	SetAPen(data->popwin->RPort, _pens(obj)[MPEN_FILL]);
+	RectFill(data->popwin->RPort, x1, y1, x2, y2);
+    }
+    else
+    {
+    	/* FIXME: Render with popup background */
+    	SetAPen(data->popwin->RPort, 0);
+	RectFill(data->popwin->RPort, x1, y1, x2, y2);
+    }
+    
+    SetAPen(data->popwin->RPort, _pens(obj)[MPEN_TEXT]);    
+    zune_text_draw(text, obj, x1, x2, y1 + POPITEM_EXTRAHEIGHT / 2);
+    
+    _rp(obj) = saverp;
+    
+}
+
+static BOOL MakePopupWin(Object *obj, struct MUI_CycleData *data)
+{
+    const struct ZuneFrameGfx 	*zframe;
+    struct MinList  	      	*childlist;
+    struct RastPort 	    	*rp, *saverp;
+    Object  	    	    	*child, *cstate;      
+    WORD    	    	    	 x, y, winx, winy, winw, winh, i;
+    
+    data->popitemwidth = 0;
+    data->popitemheight = 0;
+    
+    get(data->pageobj, MUIA_Group_ChildList, (IPTR *)&childlist);
+    cstate = (Object *)childlist->mlh_Head;
+    while((child = NextObject(&cstate)))
+    {
+    	if (_width(child) > data->popitemwidth) data->popitemwidth = _width(child);
+	if (_height(child) > data->popitemheight) data->popitemheight = _height(child);
+	set(child, MUIA_UserData, 0);
+    }
+    
+    data->popitemwidth  = _width(data->pageobj);
+    data->popitemheight = _height(data->pageobj);
+
+    data->popitemwidth  += POPITEM_EXTRAWIDTH;
+    data->popitemheight += POPITEM_EXTRAHEIGHT;
+    
+    zframe = zune_zframe_get(&muiGlobalInfo(obj)->mgi_Prefs->frames[MUIV_Frame_PopUp]);
+    
+    data->popitemoffx = muiGlobalInfo(obj)->mgi_Prefs->frames[MUIV_Frame_PopUp].innerLeft;
+    data->popitemoffy = muiGlobalInfo(obj)->mgi_Prefs->frames[MUIV_Frame_PopUp].innerTop;
+    
+    winw = data->popitemwidth + data->popitemoffx + 
+    	   muiGlobalInfo(obj)->mgi_Prefs->frames[MUIV_Frame_PopUp].innerRight;
+	   
+    winh = data->popitemheight * data->entries_num + data->popitemoffy +
+    	   muiGlobalInfo(obj)->mgi_Prefs->frames[MUIV_Frame_PopUp].innerBottom;
+    
+    if ((winw > _screen(obj)->Width) || (winh > _screen(obj)->Height))
+    {
+    	return FALSE;
+    }
+
+    i = 0;
+    cstate = (Object *)childlist->mlh_Head;
+    while((child = NextObject(&cstate)))
+    {
+    	ZText *text;
+	
+	text = zune_text_new("\33c", data->entries[i++], ZTEXT_ARG_NONE, 0);
+	if (!text) break;
+	
+	zune_text_get_bounds(text, obj);
+	set(child, MUIA_UserData, (IPTR)text);
+    }
+    
+    if (i != data->entries_num)
+    {
+    	KillPopupWin(obj, data);
+	return FALSE;
+    }
+    
+    winx = _window(obj)->LeftEdge + _mleft(data->pageobj) - 
+    	   data->popitemoffx - POPITEM_EXTRAWIDTH / 2;
+
+    if(muiGlobalInfo(obj)->mgi_Prefs->cycle_menu_position == CYCLE_MENU_POSITION_BELOW)
+    {    
+    	winy = _window(obj)->TopEdge + _bottom(obj) + 1;
+    }
+    else
+    {
+    	winy = _window(obj)->TopEdge + _mtop(data->pageobj) - data->popitemoffy -
+	       POPITEM_EXTRAHEIGHT / 2 - data->entries_active * data->popitemheight;
+    }
+    
+    data->popwin = OpenWindowTags(NULL, WA_CustomScreen, (IPTR)_screen(obj),
+    	    	    	    	    	WA_Left, winx,
+					WA_Top, winy,
+					WA_Width, winw,
+					WA_Height, winh,
+					WA_AutoAdjust, TRUE,
+					WA_Borderless, TRUE,
+					WA_Activate, FALSE,
+					WA_BackFill, (IPTR)LAYERS_NOBACKFILL,
+					TAG_DONE);
+					
+    if (!data->popwin)
+    {
+    	return FALSE;
+    }
+    
+    rp = data->popwin->RPort;
+
+    saverp = _rp(obj);
+    _rp(obj) = rp;
+    zframe->draw(muiRenderInfo(obj), 0, 0, winw, winh);    
+
+    /* FIXME: Render with popup background */
+    SetAPen(rp, 0);
+    RectFill(rp, zframe->ileft, zframe->itop,
+    	    	 winw - 1 - zframe->iright, winh - 1 - zframe->ibottom);
+		 
+    x = data->popitemoffx;
+    y = data->popitemoffy + POPITEM_EXTRAHEIGHT / 2;
+    
+    i = 0;
+    cstate = (Object *)childlist->mlh_Head;
+    while((child = NextObject(&cstate)))
+    {
+    	ZText *text;
+	
+	get(child, MUIA_UserData, (IPTR *)&text);
+	
+	SetAPen(_rp(obj), _pens(obj)[MPEN_TEXT]);
+	if (text) /* paranoia */
+	{
+	    zune_text_draw(text, obj, x, x + data->popitemwidth - 1, y);
+	}
+	
+	y += data->popitemheight;
+    }
+
+    _rp(obj) = saverp;
+        
+    data->activepopitem = -1;
+    
+    return TRUE;				    
+    	    	    	    	    	
+}
 
 /**************************************************************************
  ...
 **************************************************************************/
-static ULONG Cycle_HandleEvent(struct IClass *cl, Object *obj, struct MUIP_HandleEvent *msg)
+static IPTR Cycle_HandleEvent(struct IClass *cl, Object *obj, struct MUIP_HandleEvent *msg)
 {
+    struct MUI_CycleData *data = INST_DATA(cl, obj);
+    BOOL    	    	  fallthroughtomousemove = FALSE;
+    
+    if (!msg->imsg ||
+         data->entries_num < muiGlobalInfo(obj)->mgi_Prefs->cycle_menu_min_entries)
+    {
+    	return DoSuperMethodA(cl, obj, (Msg)msg);
+    }
+    
+    switch(msg->imsg->Class)
+    {
+    	case IDCMP_MOUSEBUTTONS:
+	    switch(msg->imsg->Code)
+	    {
+	    	case SELECTDOWN:		
+		    if (_between(_right(data->imgobj) + 1, msg->imsg->MouseX, _right(obj)) &&
+	        	_between(_top(obj), msg->imsg->MouseY, _bottom(obj)) &&
+			(muiAreaData(obj)->mad_Flags & MADF_CANDRAW) &&
+			!data->popwin)
+		    {
+		     	if (MakePopupWin(obj, data))
+		     	{
+    			    DoMethod(_win(obj), MUIM_Window_RemEventHandler, (IPTR)&data->ehn);
+			    data->ehn.ehn_Events |= IDCMP_MOUSEMOVE;
+			    DoMethod(_win(obj), MUIM_Window_AddEventHandler, (IPTR)&data->ehn);    	
+			    
+    		    	    fallthroughtomousemove = TRUE;    	    	    	    
+		     	}
+		    }
+		    break;
+		   
+		case SELECTUP:
+		case MENUUP:
+		case MIDDLEUP:
+		default:
+		    if (data->popwin)
+		    {
+		    	KillPopupWin(obj, data);
+			if ((msg->imsg->Code == SELECTUP) && (data->activepopitem != -1))
+			{
+			    set(obj, MUIA_Cycle_Active, data->activepopitem);
+			}
+		    	return MUI_EventHandlerRC_Eat;
+		    }
+		    break;
+		    
+		    
+	    } /* switch(msg->imsg->Code) */
+	    
+	    if (!fallthroughtomousemove)
+	    {
+	    	break;
+	    }
+	    
+	case IDCMP_MOUSEMOVE:
+	    if (data->popwin)
+	    {
+	    	WORD x = data->popwin->MouseX;
+		WORD y = data->popwin->MouseY - data->popitemoffy;
+		WORD newactive = -1;
+		
+		if ((x >= 0) && (y >= 0) &&
+		    (x < data->popwin->Width) && (y < data->popitemheight * data->entries_num))
+		{
+		    newactive = y / data->popitemheight;
+		} 
+		
+		if (newactive != data->activepopitem)
+		{
+    	    	    WORD oldactive = data->activepopitem;
+		    
+		    data->activepopitem = newactive;
+		    
+		    if (oldactive != -1) RenderPopupItem(obj, data, oldactive);
+		    if (newactive != -1) RenderPopupItem(obj, data, newactive);
+		    
+		}
+		
+	    	return MUI_EventHandlerRC_Eat;
+	    }
+	    break;
+	    
+    } /* switch(msg->imsg->Class) */
+    
+    
     return DoSuperMethodA(cl, obj, (Msg)msg);
+    
 }
 
+/**************************************************************************
+ ...
+**************************************************************************/
+static IPTR Cycle_Hide(struct IClass *cl, Object *obj, struct MUIP_Hide *msg)
+{
+    struct MUI_CycleData *data = INST_DATA(cl, obj);
+
+    if (data->popwin)
+    {
+    	KillPopupWin(obj, data);
+    }
+        
+    return DoSuperMethodA(cl, obj, (Msg)msg);
+}
 
 BOOPSI_DISPATCHER(IPTR, Cycle_Dispatcher, cl, obj, msg)
 {
@@ -297,6 +618,7 @@ BOOPSI_DISPATCHER(IPTR, Cycle_Dispatcher, cl, obj, msg)
 	case OM_GET: return Cycle_Get(cl, obj, (struct opGet *)msg);
 	case MUIM_Setup: return Cycle_Setup(cl, obj, (APTR)msg);
 	case MUIM_Cleanup: return Cycle_Cleanup(cl, obj, (APTR)msg);
+	case MUIM_Hide: return Cycle_Hide(cl, obj, (APTR)msg);
 	case MUIM_HandleEvent: return Cycle_HandleEvent(cl,obj,(APTR)msg);
     }
 
