d4f49f20464d7a19bd866ec1f5a1f0273ed7d959
hkiel <hkiel@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-10-18 20:07:10 +0000

    Example for StringObjects. Shows a bug (?) in Zune. Crashes after some times.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19984 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/Zune/string.c
@@ -0,0 +1,254 @@
+/*
+    Copyright © 1995-2003, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <exec/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <proto/alib.h>
+#include <proto/exec.h>
+#include <proto/intuition.h>
+#include <proto/muimaster.h>
+#include <libraries/mui.h>
+#include <libraries/coolimages.h>
+
+
+#define AddContents(obj)			\
+	DoMethod(root, MUIM_Group_InitChange);	\
+	DoMethod(root, OM_ADDMEMBER, (IPTR)obj);\
+	DoMethod(root, MUIM_Group_ExitChange);
+
+#define DelContents(obj)			\
+	DoMethod(root, MUIM_Group_InitChange);	\
+	DoMethod(root, OM_REMMEMBER, (IPTR)obj);\
+	DoMethod(root, MUIM_Group_ExitChange);	\
+	MUI_DisposeObject(wc);
+
+Object *app;
+Object *wnd;
+Object *root;
+Object *btabort, *btproceed;
+
+int quit = FALSE;
+
+enum
+{
+Push_NULL,
+Push_Proceed,
+Push_Abort,
+};
+
+/* ######################################################################## */
+
+
+const char GuiWinTitle[] ="AROS - Installer V43.3";
+struct Screen *scr;
+
+
+void init_gui()
+{
+
+    scr = LockPubScreen(NULL);
+
+    app = ApplicationObject,
+	MUIA_Application_Title, "AROS - Installer",
+
+   	SubWindow, wnd = WindowObject,
+	    MUIA_Window_Title,	GuiWinTitle,
+	    MUIA_Window_Width,	400,
+	    MUIA_Window_Height,	300,
+	    MUIA_Window_CloseGadget,	FALSE,
+	    MUIA_Window_NoMenus,	TRUE,
+	    MUIA_Window_ID,	MAKE_ID('A','I','N','S'),
+	    WindowContents,
+	    VGroup,
+		Child, root = VGroup, End,
+		Child, HBar(TRUE),
+		Child, HGroup,
+		MUIA_Group_SameSize, TRUE,
+		Child, btproceed = CoolImageIDButton("Proceed", COOL_USEIMAGE_ID),
+		Child, btabort   = CoolImageIDButton("Abort", COOL_CANCELIMAGE_ID),
+		End,
+	    End,
+	End,
+    End;
+    if (app == NULL)
+    {
+	/* failed to initialize GUI */
+printf("Failed to intialize Zune GUI\n");
+    }
+    set(btproceed,MUIA_CycleChain,1);
+    set(btabort,MUIA_CycleChain,1);
+
+    DoMethod(btproceed, MUIM_Notify, MUIA_Pressed, FALSE,(IPTR)app, 2,
+	MUIM_Application_ReturnID, Push_Proceed);
+    DoMethod(btabort, MUIM_Notify, MUIA_Pressed, FALSE,(IPTR)app, 2,
+	MUIM_Application_ReturnID, Push_Abort);
+    set(wnd, MUIA_Window_Open, TRUE);
+}
+
+/*
+ * Close GUI
+ */
+void deinit_gui()
+{
+    set(wnd, MUIA_Window_Open, FALSE);
+    MUI_DisposeObject(app);
+}
+
+
+
+/*
+ * Ask user for a number
+ */
+long int request_number(long int def)
+{
+long int retval;
+long int min, max;
+
+    retval = def;
+    max = 999;
+    min = 0;
+
+
+    {
+    char *out;
+    BOOL running = TRUE;
+    Object *st, *wc;
+    ULONG val, sigs;
+
+	out = StrDup("TEST");
+
+	wc = VGroup,
+	Child, VGroup, GroupFrame,
+		    Child, TextObject,
+			MUIA_Text_Contents, (IPTR)(out),
+			MUIA_Text_Editable, FALSE,
+			MUIA_Text_Multiline, TRUE,
+		    End,
+		    Child, st  = StringObject,
+			StringFrame,
+			MUIA_String_Accept,	(IPTR)"-0123456789",
+			MUIA_String_Integer,	retval,
+			MUIA_String_AdvanceOnCR,TRUE,
+			MUIA_CycleChain,	TRUE,
+		    End,
+		End,
+	    End;
+
+	if (wc)
+	{
+	    AddContents(wc);
+
+	    while (running)
+	    {
+		switch (val = DoMethod(app,MUIM_Application_Input,(IPTR)&sigs))
+		{
+		    case MUIV_Application_ReturnID_Quit:
+		    case Push_Abort:
+			DelContents(wc);
+			quit = TRUE;
+			break;
+		    case Push_Proceed:
+			GetAttr(MUIA_String_Integer, st, &retval);
+			if ( retval < max && retval > min)
+			{
+			    running = FALSE;
+			}
+			break;
+		    default:
+			break;
+		}
+	    }
+	    GetAttr(MUIA_String_Integer, st, &retval);
+
+	    DelContents(wc);
+	}
+	FreeVec(out);
+    }
+
+
+return retval;
+}
+
+
+char *request_string(char *def)
+{
+char *retval, *string;
+
+    string = def;
+    {
+    char *out;
+    BOOL running = TRUE;
+    Object *st, *wc;
+    ULONG val, sigs;
+
+	out = StrDup("test");
+
+	wc = VGroup,
+	Child, VGroup, GroupFrame,
+		    Child, TextObject,
+			MUIA_Text_Contents, (IPTR)(out),
+			MUIA_Text_Editable, FALSE,
+			MUIA_Text_Multiline, TRUE,
+		    End,
+		    Child, st  = StringObject,
+			StringFrame,
+			MUIA_String_Contents,	(IPTR)string,
+			MUIA_String_MaxLen,	128,
+			MUIA_String_AdvanceOnCR,TRUE,
+			MUIA_CycleChain,	TRUE,
+		    End,
+		End,
+	    End;
+
+	if (wc)
+	{
+	    AddContents(wc);
+
+	    while (running)
+	    {
+		switch (val = DoMethod(app,MUIM_Application_Input,(IPTR)&sigs))
+		{
+		    case MUIV_Application_ReturnID_Quit:
+		    case Push_Abort:
+			DelContents(wc);
+			quit = TRUE;
+			break;
+		    case Push_Proceed:
+			running = FALSE;
+			break;
+		    default:
+			break;
+		}
+	    }
+	    get(st, MUIA_String_Contents, (IPTR *)&string);
+
+	    DelContents(wc);
+	}
+	FreeVec(out);
+    }
+
+return retval;
+}
+
+
+int main()
+{
+
+    init_gui();
+
+    for(;;)
+    {
+	request_string("blah");
+	if(quit) break;
+	request_number(99);
+	if(quit) break;
+    }
+    deinit_gui();
+
+    return 0;
+}
