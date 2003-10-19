3a5451a81762313c5b12feb6261d0de7ccd00b28
falemagn <falemagn@gmail.com>
2003-10-19 20:07:35 +0000

    Trigger *nix path handling on.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19997 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/__arosc_nixmain.c
+++ compiler/clib/__arosc_nixmain.c
@@ -4,16 +4,18 @@
 
     Desc: special main function for code which has to use special *nix features.
           This function gets called from a function with a similar name statically
-	  linked with the program. This is so to make the program independent from the
-	  libc's version.
+	  linked with the program. This is so to make the program not depend on a
+	  particular libc version.
 
     Lang: english
 */
 
-#include <sys/syscall.h>
-
 int __arosc_nixmain(int (*main)(int argc, char *argv[]), int argc, char *argv[])
 {
+    /* Trigger *nix path handling on.  */
+    __doupath = 1;
+
+    /* Call the real main.  */
     return (*main)(argc, argv);
 }
 
