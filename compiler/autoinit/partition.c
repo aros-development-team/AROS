16062b09271e78055f6eb59c3080cc9b734a06ba
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-10-28 10:15:56 +0000

    Use LIBSET_USER_PRI rather than LIBSET_PARTITION_PRI, specially because the latter is nowhere defined, leading the autoinit code to not produce a working libset.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20025 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/autoinit/partition.c
+++ compiler/autoinit/partition.c
@@ -6,4 +6,4 @@
 #include <libraries/partition.h>
 #include <aros/symbolsets.h>
 
-ADD2LIBS("partition.library", 1, LIBSET_PARTITION_PRI, struct Library *, PartitionBase, NULL, NULL);
+ADD2LIBS("partition.library", 1, LIBSET_USER_PRI, struct Library *, PartitionBase, NULL, NULL);
