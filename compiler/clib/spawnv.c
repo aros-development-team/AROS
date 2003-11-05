8d633734587e648091a3fabbf88f4353950add3a
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-11-05 00:22:08 +0000

    Implemented the spawnv() function. Only the P_WAIT mode is supported for now.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20048 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/spawnv.c
+++ compiler/clib/spawnv.c
@@ -5,8 +5,34 @@
     spavnv() function, used to spawn new processes.
 */
 
+#include <proto/dos.h>
+#include <proto/exec.h>
+#include <dos/dos.h>
+#include <dos/dosextens.h>
+#include <dos/filesystem.h>
+#include <aros/debug.h>
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 #include <errno.h>
 
+#include "__errno.h"
+#include "__upath.h"
+
+typedef struct
+{
+    BPTR command;
+    LONG returncode;
+} childdata_t;
+
+AROS_UFP3(static LONG, wait_entry,
+AROS_UFPA(char *, argstr,A0),
+AROS_UFPA(ULONG, argsize,D0),
+AROS_UFPA(struct ExecBase *,SysBase,A6));
+
+static BPTR DupFHFromfd(int fd, ULONG mode);
+static char *join_args(char * const *argv);
 /*****************************************************************************
 
     NAME */
@@ -44,6 +70,7 @@
 	       pointer itself must never be NULL either.
 
     RESULT
+
 	If P_WAIT is specified, then the return code of the child program is returned.
 	If instead P_NOWAIT is used, then the pid of the newly created process is returned.
 	Finally, if P_OVERLAY is used, the function doesn't return unless an error has occurred,
@@ -51,6 +78,11 @@
 	hold the proper error code.
 
     NOTES
+
+        The way the child process behaves regarding parent's file descriptors, signal handlers
+	and so on is the same way it would behave with one of the exec*(3) functions.
+	This, for one, means that all filedescriptors are inherited except the ones which have
+	the close-on-exec flag set.
 
     EXAMPLE
 
@@ -63,8 +95,185 @@
 
     INTERNALS
 
+        For now only the stdin, stout and stderr file descriptors are inherited, and signals
+	are not handled yet.
+
 ******************************************************************************/
 {
-    errno = ENOSYS;
-    return -1;
+    const char *apath;
+    int ret = -1;
+
+    if (path == NULL || argv == NULL || argv[0] == NULL)
+    {
+        errno = EFAULT;
+
+	return -1;
+    }
+
+    apath = __path_u2a(path);
+    if (!apath)
+        return -1;
+
+    errno = 0;
+
+    switch (mode)
+    {
+        case P_WAIT:
+	{
+	    BPTR in, out, err;
+	    childdata_t childdata;
+
+	    struct TagItem tags[] =
+	    {
+	        { NP_Entry,       (IPTR)wait_entry },
+		{ NP_Input,       0                }, /* 1 */
+		{ NP_Output,      0                }, /* 2 */
+		{ NP_Error,       0                }, /* 3 */
+		{ NP_Arguments,   0                }, /* 4 */
+		{ NP_CloseInput,  FALSE            },
+		{ NP_CloseOutput, FALSE            },
+		{ NP_CloseError,  FALSE            },
+		{ NP_FreeSeglist, FALSE            },
+		{ NP_Cli,         TRUE             },
+		{ NP_Synchronous, TRUE             },
+		{ NP_Name,        (IPTR)argv[0]    },
+		{ NP_UserData,    (IPTR)&childdata },
+		{ TAG_DONE,       0                }
+	    };
+
+	    /* The helper entry function takes the loadseg result in
+	       the usedsata field of the task structure.  */
+	    childdata.command = LoadSeg(apath);
+	    if (!childdata.command)
+	        goto err_wait;
+
+	    tags[4].ti_Data = (IPTR)join_args(&argv[1]);
+	    if (!tags[4].ti_Data)
+	        goto err_wait;
+
+	    in  = DupFHFromfd(STDIN_FILENO,  FMF_READ);
+	    out = DupFHFromfd(STDOUT_FILENO, FMF_WRITE);
+	    err = DupFHFromfd(STDERR_FILENO, FMF_WRITE);
+
+	    if (in)  tags[1].ti_Data = (IPTR)in;
+	    else     tags[1].ti_Tag  = TAG_IGNORE;
+	    if (out) tags[2].ti_Data = (IPTR)out;
+	    else     tags[2].ti_Tag  = TAG_IGNORE;
+	    if (err) tags[3].ti_Data = (IPTR)err;
+	    else     tags[3].ti_Tag  = TAG_IGNORE;
+
+
+	    if (CreateNewProc(tags) != NULL)
+	        ret = childdata.returncode;
+	    else
+	        ret = -1;
+
+	    Close(in); Close(out); Close(err);
+
+	    err_wait:
+            UnLoadSeg(childdata.command);
+
+            break;
+	}
+
+	default:
+	    ret = -1;
+	    errno = ENOSYS;
+    }
+
+    if (ret == -1 && errno == 0)
+        errno = IoErr2errno(IoErr());
+
+    return ret;
+}
+
+
+AROS_UFH3(static LONG, wait_entry,
+AROS_UFHA(char *, argstr,A0),
+AROS_UFHA(ULONG, argsize,D0),
+AROS_UFHA(struct ExecBase *,SysBase,A6))
+{
+    struct DosLibrary *DOSBase;
+    LONG rc = -1;
+    childdata_t *childdata = (childdata_t *)FindTask(NULL)->tc_UserData;
+
+    DOSBase = (struct DosLibrary *)OpenLibrary(DOSNAME, 39);
+
+    if (DOSBase)
+    {
+        rc = RunCommand
+	(
+	    childdata->command, Cli()->cli_DefaultStack * CLI_DEFAULTSTACK_UNIT, argstr, argsize
+	);
+
+	CloseLibrary((struct Library *)DOSBase);
+
+    }
+
+    childdata->returncode = rc;
+    return rc;
+}
+
+#include "__open.h"
+static BPTR DupFHFromfd(int fd, ULONG mode)
+{
+    fdesc *fdesc = __getfdesc(fd);
+    BPTR ret = MKBADDR(NULL);
+
+    if (fdesc != NULL && fdesc->fh != MKBADDR(NULL))
+    {
+        BPTR olddir = CurrentDir(fdesc->fh);
+        ret = Open("", mode);
+        CurrentDir(olddir);
+    }
+
+    return ret;
+}
+
+/* Join all elements of an argv array so to build one big string with
+   all elements chained one after the other one.
+
+   The resulting string's pointer is valid only until a subsequent call
+   to this function.  */
+static char *join_args(char * const *argv)
+{
+  char *last_arg_ptr, *args;
+  size_t size = 0;
+  int argc;
+
+  if (!argv)
+      return NULL;
+
+  if (!argv[0])
+      return "";
+
+  if (!argv[1])
+      return argv[0];
+
+
+
+  for (argc = 0; argv[argc] != NULL; argc++)
+      size += strlen(argv[argc]);
+
+  args = __get_arosc_privdata()->acpd_joined_args = realloc_nocopy(args, size+argc);
+  if (!args)
+      return NULL;
+
+  args[0] = '\0';
+
+  last_arg_ptr = args;
+  for (argc = 0; argv[argc] != NULL; argc++)
+  {
+      size = strlen(argv[argc]);
+
+      memcpy(last_arg_ptr, argv[argc], size);
+
+      last_arg_ptr += size + 1;
+
+      last_arg_ptr[-1] = ' ';
+  }
+
+  last_arg_ptr[0] = '\0';
+
+  return args;
 }
