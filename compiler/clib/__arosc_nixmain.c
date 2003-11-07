85b599fcbf19fc89539ed5f7d74b62dcb53ff825
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-11-07 00:02:51 +0000

    Translate argv[0] from an AmigaDOS-style path into an unix-style path
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20061 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/__arosc_nixmain.c
+++ compiler/clib/__arosc_nixmain.c
@@ -10,13 +10,48 @@
     Lang: english
 */
 
+#include <setjmp.h>
+#include <string.h>
+#include <stdlib.h>
+#include <aros/startup.h>
+#include "__upath.h"
+
 int __arosc_nixmain(int (*main)(int argc, char *argv[]), int argc, char *argv[])
 {
+    volatile char *old_argv0 = NULL;
+
     /* Trigger *nix path handling on.  */
     __doupath = 1;
 
+    /* argv[0] usually contains the name of the program, possibly with the full
+       path to it. Here we translate that path, which is an AmigaDOS-style path, into
+       an unix-style one.  */
+    if (argv && argv[0])
+    {
+        char *new_argv0;
+
+	new_argv0 = strdup(__path_a2u(argv[0]));
+	if (new_argv0 == NULL)
+	    return EXIT_FAILURE;
+
+        old_argv0 = argv[0];
+	argv[0] = new_argv0;
+    }
+
     /* Call the real main.  */
-    return (*main)(argc, argv);
+    if (setjmp(__aros_startup_jmp_buf) == 0)
+    {
+        __aros_startup_error = (*main)(argc, argv);
+    }
+
+    /* Restore the old argv[0].  */
+    if (old_argv0)
+    {
+        free(argv[0]);
+	argv[0] = old_argv0;
+    }
+
+    return __aros_startup_error;
 }
 
 
