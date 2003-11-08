95590c3ce29199371ec530f8de57237bb6c11c07
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-11-08 21:03:16 +0000

    Half finished levelmeter class.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20064 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/muimaster/classes/levelmeter.h
@@ -0,0 +1,21 @@
+#ifndef _MUI_CLASSES_LEVELMETER_H
+#define _MUI_CLASSES_LEVELMETER_H
+
+/*
+    Copyright © 2002-2003, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+/*** Name *******************************************************************/
+#define MUIC_Levelmeter  	"Levelmeter.mui"
+
+/*** Identifier base (for Zune extensions) **********************************/
+//#define MUIB_Levelmeter   	(MUIB_ZUNE | 0x????)  
+
+/*** Attributes *************************************************************/
+#define MUIA_Levelmeter_Label	(MUIB_MUI | 0x00420dd5 /* V11 isg STRPTR */
+
+
+extern const struct __MUIBuiltinClass _MUI_Levelmeter_desc; /* PRIV */
+
+#endif /* _MUI_CLASSES_LEVELMETER_H */
---
+++ workbench/libs/muimaster/classes/levelmeter_private.h
@@ -0,0 +1,10 @@
+#ifndef _LEVELMETER_PRIVATE_H_
+#define _LEVELMETER_PRIVATE_H_
+
+struct Levelmeter_DATA
+{
+    LONG levelbgpen;
+    DOUBLE prevangle;
+};
+
+#endif /* _LEVELMETER_PRIVATE_H_ */
