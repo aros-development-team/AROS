ce9c6ad6e76de3441156dd9b6cab8eba66f41d20
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-11-14 17:22:14 +0000

    Includes <cybergraphx/cybergraphics.h>. Was tired of always "fixing" "#include <libraries/cybergraphics.h>"
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20117 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/libraries/cybergraphics.h
@@ -0,0 +1,16 @@
+#ifndef LIBRARIES_CYBERGRAPHICS_H
+#define LIBRARIES_CYBERGRAPHICS_H
+
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: 
+    Lang: English
+*/
+
+#ifndef CYBERGRAPHX_CYBERGRAPHICS_H
+#   include <cybergraphx/cybergraphics.h>
+#endif
+
+#endif /* LIBRARIES_CYBERGRAPHICS_H */
