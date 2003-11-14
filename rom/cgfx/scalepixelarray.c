2c1e49c0e433ba838bef1cdf8ea2f427e081b163
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-11-14 17:23:06 +0000

    Compile ScalePixelArray() function, although not implemented yet.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20118 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/cgfx/scalepixelarray.c
+++ rom/cgfx/scalepixelarray.c
@@ -15,16 +15,16 @@
 	AROS_LH10(LONG, ScalePixelArray,
 
 /*  SYNOPSIS */
-	AROS_LHA(APTR             , , A0),
-	AROS_LHA(UWORD            , , D0),
-	AROS_LHA(UWORD            , , D1),
-	AROS_LHA(UWORD            , , D2),
-	AROS_LHA(struct RastPort *, , A1),
-	AROS_LHA(UWORD            , , D3),
-	AROS_LHA(UWORD            , , D4),
-	AROS_LHA(UWORD            , , D5),
-	AROS_LHA(UWORD            , , D6),
-	AROS_LHA(UBYTE            , , D7),
+	AROS_LHA(APTR             , srcRect, A0),
+	AROS_LHA(UWORD            , SrcW, D0),
+	AROS_LHA(UWORD            , SrcH, D1),
+	AROS_LHA(UWORD            , SrcMod, D2),
+	AROS_LHA(struct RastPort *, RastPort, A1),
+	AROS_LHA(UWORD            , DestX, D3),
+	AROS_LHA(UWORD            , DestY, D4),
+	AROS_LHA(UWORD            , DestW, D5),
+	AROS_LHA(UWORD            , DestH, D6),
+	AROS_LHA(UBYTE            , SrcFormat, D7),
 
 /*  LOCATION */
 	struct Library *, CyberGfxBase, 15, Cybergraphics)
@@ -53,9 +53,14 @@
 {
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct Library *,CyberGfxBase)
+
+#if 0
     extern void aros_print_not_implemented (char *);
 
     aros_print_not_implemented ("ScalePixelArray");
+#endif
 
+    return 0;
+    
     AROS_LIBFUNC_EXIT
 } /* ScalePixelArray */
