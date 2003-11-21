cdd1d77994bc97fec3101242483565f010703170
falemagn <falemagn@gmail.com>
2003-11-21 22:39:17 +0000

    New version of collect-aros. It's now just a frontend to platform-specific code which perform the various tasks the original collect-aros did in a more specialized way.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20153 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- tools/collect-aros/collect-aros.c
+++ tools/collect-aros/collect-aros.c
@@ -1,185 +1,40 @@
-/*
-    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
-    $Id$
-
-    Desc: AROS linker wrapper that handles symbol sets
-    Lang: english
-    Original Author: falemagn
- */
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <errno.h>
-#include <stdarg.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <unistd.h>
 
+#include "misc.h"
+#include "docommand.h"
+#include "backend.h"
 #include "ldscript.h"
 #include "gensets.h"
 
-#define TMPTEMPLATE "aros-collect.XXXXXX"
+static char *ldscriptname, *tempoutput;
+static FILE *ldscriptfile;
 
-static char *old_path;
-static char *compiler_path;
-
-void fatalerror(int status)
+static void exitfunc(void)
 {
-    if (status)
-    {
-    	if (errno) perror("collect-aros");
-        exit(status);
-    }
-}
+    if (ldscriptfile != NULL)
+        fclose(ldscriptfile);
 
-void *xmalloc(size_t size)
-{
-    void *ret = malloc(size);
+    if (ldscriptname != NULL)
+        remove(ldscriptname);
 
-    fatalerror(!ret);
-
-    return ret;
-}
-
-FILE *xpopen(char *command)
-{
-    FILE *ret = popen(command, "r");
-
-    fatalerror(!ret);
-
-    return ret;
-}
-
-char *xtempnam(void)
-{
-    char *ret = strdup(TMPTEMPLATE);
-    int rc = mkstemp(ret); //tempnam("/tmp", NULL);
-    fatalerror(!rc);
-
-    return ret;
-}
-
-void xsystem(char *command)
-{
-    /*	For some bizarre reason, WEXITSTATUS() requires an lvalue
-	when used on FreeBSD.  */
-    int stat = system(command);
-    fatalerror(WEXITSTATUS(stat));
-}
-
-FILE *xfopen(char *name, char *mode)
-{
-    FILE *ret = fopen(name, mode);
-
-    fatalerror(!ret);
-
-    return ret;
-}
-
-void xwaitpid(pid_t pid)
-{
-    int status;
-
-    waitpid(pid, &status, 0);
-
-    fatalerror(WEXITSTATUS(status));
-}
-#define docommand(func, cmd, ...)                                             \
-do                                                                            \
-{                                                                             \
-    pid_t pid = vfork();                                                      \
-    fatalerror(pid == -1);                                                    \
-    if (pid == 0)                                                             \
-    {                                                                         \
-	func(cmd, __VA_ARGS__);                                               \
-	                                                                      \
-        fprintf(stderr, "collect-aros - Error while executing %s\n", cmd);    \
-                                                                              \
-	/* we can't use exit because it would close the I/O channels  of the  \
-	   parent process.  */                                                \
-	_exit(1);                                                             \
-    }                                                                         \
-                                                                              \
-    xwaitpid(pid);                                                            \
-} while (0)
-
-
-char *joinstrings(char *first, ...)
-{
-    va_list strings;
-    char *str, *s;
-    int size = 0;
-
-    if (!first)
-       	return NULL;
-
-    size += strlen(first);
-
-    va_start (strings, first);
-
-    while ((s=va_arg(strings, char *)))
-    	size += strlen(s);
-
-    va_end(strings);
-
-    str = xmalloc(size+1);
-
-    str[0]='\0';
-
-    strcat(str, first);
-
-    va_start(strings, first);
-
-    while ((s=va_arg(strings, char *)))
-    	strcat(str, s);
-
-    va_end (strings);
-
-    return str;
-}
-
-char *tempoutname  = NULL;
-char *ldscriptname = NULL;
-
-void exitfunc(void)
-{
-    remove(ldscriptname);
-    remove(tempoutname);
+    if (tempoutput != NULL)
+        remove(tempoutput);
 }
 
 int main(int argc, char *argv[])
 {
-    int cnt, ret = 0;
-    char *output;
-    char *command;
-    char **ldargs;
-    FILE *pipe;
-    FILE *ldscriptfile = NULL;
-    char buf[200];
-    int thereare  = 0, incremental            = 0,
-        strip_all = 0, ignore_missing_symbols = 0;
+    int cnt;
+    char *output, **ldargs;
+    int incremental = 0, ignore_undefined_symbols = 0;
+    int strip_all   = 0;
+    char *do_verbose = NULL;
 
-    char *compiler_path = getenv("COMPILER_PATH");
-    char *path          = getenv("PATH");
-    char *new_path;
+    setnode *setlist = NULL;
 
-    atexit(exitfunc);
-
-    if (!compiler_path) compiler_path = "";
-    if (!path) path = "";
-
-    new_path = malloc(5 + strlen(compiler_path) + 1 + strlen(path) + 1);
-    if (new_path)
-    {
-        strcat(new_path, "PATH=");
-        strcat(new_path, compiler_path);
-        strcat(new_path, ":");
-        strcat(new_path, path);
-
-	putenv(new_path);
-    }
+    program_name = argv[0];
 
     /* Do some stuff with the arguments */
     output = "a.out";
@@ -199,7 +54,7 @@ int main(int argc, char *argv[])
 	    /* Ignoring of missing symbols is requested */
 	    if (strncmp(&argv[cnt][1], "ius", 4) == 0)
 	    {
-	        ignore_missing_symbols = 1;
+	        ignore_undefined_symbols = 1;
 		argv[cnt][1] = 'r';  /* Just some non-harming option... */
 		argv[cnt][2] = '\0';
 	    }
@@ -219,11 +74,17 @@ int main(int argc, char *argv[])
 	        incremental = 1;
 	        break;
 	    }
-
+	    else
+	    /* verbose output */
+	    if (strncmp(&argv[cnt][1], "-verbose", 9) == 0)
+	    {
+	        do_verbose = argv[cnt];
+	        break;
+	    }
 	}
     }
 
-    ldargs = xmalloc(sizeof(char *) * (argc+2));
+    ldargs = xmalloc(sizeof(char *) * (argc+2 + 2*(incremental == 0)));
 
     ldargs[0] = "ld";
     ldargs[1] = "-r";
@@ -231,69 +92,53 @@ int main(int argc, char *argv[])
     for (cnt = 1; cnt < argc; cnt++)
     	ldargs[cnt+1] = argv[cnt];
 
+    if (!incremental)
+    {
+        atexit(exitfunc);
+
+	if
+	(
+	    !(tempoutput   = make_temp_file(NULL))     ||
+	    !(ldscriptname = make_temp_file(".x"))     ||
+	    !(ldscriptfile = fopen(ldscriptname, "w"))
+	)
+	{
+	    fatal(ldscriptname ? ldscriptname : "make_temp_file()", strerror(errno));
+	}
+
+        ldargs[cnt + 1] = "-o";
+        ldargs[cnt + 2] = tempoutput;
+	cnt += 2;
+    }
+
     ldargs[cnt+1] = NULL;
 
-    docommand(execvp, "ld", ldargs);
+    docommandvp("ld", ldargs);
 
     if (incremental)
-        return 0;
+        return EXIT_SUCCESS;
 
-    tempoutname  = xtempnam();
-    ldscriptname = joinstrings(tempoutname, "-ldscript.x", NULL);
-    command      = joinstrings("objdump -h ", output, NULL);
-    pipe         = xpopen(command);
-    ldscriptfile = xfopen(ldscriptname, "w");
+    collect_sets(tempoutput, &setlist);
 
-    fprintf(ldscriptfile, LDSCRIPT_PART1);
-    ret = gensets(pipe, ldscriptfile);
-    fprintf(ldscriptfile, LDSCRIPT_PART2);
+    fwrite(LDSCRIPT_PART1, sizeof(LDSCRIPT_PART1) - 1, 1, ldscriptfile);
+    emit_sets(setlist, ldscriptfile);
+    fwrite(LDSCRIPT_PART2, sizeof(LDSCRIPT_PART2) - 1, 1, ldscriptfile);
 
     fclose(ldscriptfile);
-    pclose(pipe);
+    ldscriptfile = NULL;
 
-    if (ret)
+    docommandlp("ld", "ld", "-r", "-o", output, tempoutput, "-T", ldscriptname, do_verbose, NULL);
+
+    if (!ignore_undefined_symbols && check_and_print_undefined_symbols(output))
     {
-	docommand(execlp, "ld", "ld", "-r", "-o", tempoutname, output, "-T", ldscriptname, NULL);
-	docommand(execlp, "mv", "mv", "-f", tempoutname, output, NULL);
-    }
-
-    free(tempoutname);
-
-    if (ignore_missing_symbols)
-    {
-        thereare = 0;
-	goto end;
-    }
-
-    command = joinstrings("nm -C -ul ", output, NULL);
-
-    pipe = xpopen(command);
-
-    while ((cnt = fread(buf, 1, sizeof(buf), pipe))!=0)
-    {
-	if (!thereare)
-	{
-	    thereare = 1;
-	    fprintf(stderr, "There are undefined symbols in %s:\n", output);
-        }
-
-	fwrite(buf, cnt, 1, stderr);
-    }
-
-    pclose(pipe);
-
-end:
-    if (thereare)
         remove(output);
-    else
-	chmod(output, 0766);
-
-    if (!thereare && strip_all)
-    {
-        docommand(execlp, "strip", "strip", "--strip-unneeded", output, NULL);
+        return EXIT_FAILURE;
     }
 
-    return thereare;
+    if (strip_all)
+    {
+        docommandlp("strip", "strip", "--strip-unneeded", output, NULL);
+    }
+
+    return 0;
 }
-
-
