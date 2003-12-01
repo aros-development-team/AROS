815f8004e00ee393716100d70771a90996f65e75
falemagn <falemagn@gmail.com>
2003-12-01 14:37:07 +0000

    Parameters from 5 to n are passed directly to gcc, so to account for -I options.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20223 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- tools/FlexCat/gcccatcomp.sh
+++ tools/FlexCat/gcccatcomp.sh
@@ -3,10 +3,11 @@
 # This script is used by gcc, and does't have to be used by anything else,
 # since it relies on gcc setting some things up for it.
 
-# $1 = .cd file
-# $2 = directory where to find FlexCat and gcccatcomp.sd. 
-# $3 = ,c file
-# $4 = .o file
+cd_file=$1 # $1 = .cd file
+dir=$2     # $2 = directory where to find FlexCat and gcccatcomp.sd. 
+c_file=$3  # $3 = ,c file
+o_file=$4  # $4 = .o file
 
+shift 4    # parameters from 5 to n are passes "as is" to gcc.
 
-$2/FlexCat $1 $3=$2/gcccatcomp.sd && $COLLECT_GCC $3 -c -o $4
+$dir/FlexCat $cd_file $c_file=$dir/gcccatcomp.sd && $COLLECT_GCC $c_file -c -o $o_file $@
