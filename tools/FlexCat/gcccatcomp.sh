1887b80cfcdd771a1921d7a115408619815a90bb
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-12-01 15:43:52 +0000

    Adapted to the fact that now the spec file handles the -c and -S arguments.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20226 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- tools/FlexCat/gcccatcomp.sh
+++ tools/FlexCat/gcccatcomp.sh
@@ -3,11 +3,10 @@
 # This script is used by gcc, and does't have to be used by anything else,
 # since it relies on gcc setting some things up for it.
 
-cd_file=$1 # $1 = .cd file
-dir=$2     # $2 = directory where to find FlexCat and gcccatcomp.sd. 
-c_file=$3  # $3 = ,c file
-o_file=$4  # $4 = .o file
+cd_file=$1  # $1 = .cd file
+dir=$2      # $2 = directory where to find FlexCat and gcccatcomp.sd. 
+c_file=$3   # $3 = ,c file
 
-shift 4    # parameters from 5 to n are passes "as is" to gcc.
+shift 3     # parameters from 4 to n are passes "as is" to $COLLECT_GCC.
 
-$dir/FlexCat $cd_file $c_file=$dir/gcccatcomp.sd && $COLLECT_GCC $c_file -c -o $o_file $@
+$dir/FlexCat $cd_file $c_file=$dir/gcccatcomp.sd && $COLLECT_GCC $c_file $@
