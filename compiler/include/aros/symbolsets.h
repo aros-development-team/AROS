25d8c922eeccfe155abf1bb25160ee434e7a13d4
falemagn <falemagn@gmail.com>
2003-12-01 20:53:17 +0000

    The way the library symbol set is handled has slightly changed:
        * Removed the afteropen/preclose function's ponters from the
    	  libset structure, as they are both not used and unneeded:
    	  in fact, if you want to add special init/deinit routines
    	  to be run respectively after opening the library and before
    	  closing it, then you can use the INIT/EXIT vectors, which
    	  are always scanned respectively after all libraries are opened
    	  and before all libraries are closed.
    
        * Removed the priority parameter from the ADD2LIBS() macro, because
    	  it doesn't really matter in which order libraries are opened,
    	  as each library already opens by itself all the libraries it needs.
    
        * Use the .set assembler pseudoop in order to generate a reference
    	  to a symbol which is used to drag special library handling code in
    	  the executable. Before a static symbol was used for to obtain the
    	  same effect, but static symbols take space in the executable, symbols
    	  generated with the .set pseudoop do not.
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20229 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/aros/symbolsets.h
+++ compiler/include/aros/symbolsets.h
@@ -18,8 +18,6 @@ struct libraryset
     STRPTR name;
     const ULONG  *versionptr;
     void   **baseptr;
-    int   (*postopenfunc)(void);
-    void  (*preclosefunc)(void);
 };
 
 extern int set_call_funcs(void *set[], int order);
@@ -102,48 +100,23 @@ void * SETNAME(set)[] __attribute__((weak))={0,0};
 #define ADD2OPENLIB(symbol, pri)    ADD2SET(symbol, openlib, pri)
 #define ADD2CLOSELIB(symbol, pri)   ADD2SET(symbol, closelib, pri)
 
-/*
-  this macro generates the necessary symbols to open and close automatically
-  a library. It will make an error message be showed if the library cannot
-  be open
-*/
-#define ADD2LIBS(name, ver, pri, btype, bname, postopenfunc, preclosefunc)   \
-btype bname;                                                                         \
-extern int __includelibrarieshandling;                                               \
-static const int *__setincludelibrarieshandling __unused = &__includelibrarieshandling; \
-const ULONG bname##_version __attribute__((weak)) = ver;                     \
-struct libraryset libraryset_##bname =                                       \
-{                                                                            \
-     name, &bname##_version, (void **)&bname, postopenfunc, preclosefunc     \
-};                                                                           \
-ADD2SET(libraryset_##bname, libs, pri)
+/* this macro generates the necessary symbols to open and close automatically
+   a library. An error message will be shown if the library cannot be open.  */
+#define ADD2LIBS(name, ver, btype, bname)                \
+btype bname;                                             \
+                                                         \
+asm(".set __setincludelibrarieshandling_" #bname ","     \
+    AROS_ASMSYMNAME("__includelibrarieshandling"));      \
+                                                         \
+const ULONG bname##_version __attribute__((weak)) = ver; \
+                                                         \
+struct libraryset libraryset_##bname =                   \
+{                                                        \
+     name, &bname##_version, (void **)&bname             \
+};                                                       \
+ADD2SET(libraryset_##bname, libs, 0) 
 
 #define ASKFORLIBVERSION(bname, ver) \
 const ULONG bname##_version = ver
-
-/* some already allocated priorities for library opening/closing */
-#define LIBSET_EXEC_PRI          0
-#define LIBSET_DOS_PRI           1
-#define LIBSET_KEYMAP_PRI        2
-#define LIBSET_INTUITION_PRI     3
-#define LIBSET_LAYERS_PRI        4
-#define LIBSET_GRAPHICS_PRI      5
-#define LIBSET_UTILITY_PRI       6
-#define LIBSET_IFFPARSE_PRI      7
-#define LIBSET_CYBERGRAPHICS_PRI 8
-#define LIBSET_DISKFONT_PRI      9
-#define LIBSET_LOCALE_PRI        10
-#define LIBSET_ASL_PRI           11
-#define LIBSET_GADTOOLS_PRI 	 12
-#define LIBSET_REALTIME_PRI 	 13
-#define LIBSET_ICON_PRI 	 14
-#define LIBSET_WORKBENCH_PRI 	 15
-#define LIBSET_DATATYPES_PRI 	 16
-#define LIBSET_COMMODITIES_PRI 	 17
-
-#define LIBSET_AROSC_PRI         20
-
-/* User priorities starts from here */
-#define LIBSET_USER_PRI      100  /*An enough hight value, I think... */
 
 #endif
