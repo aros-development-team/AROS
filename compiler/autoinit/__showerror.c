267183a3756d380b6c7186aa521289eeab56aed2
falemagn <falemagn@gmail.com>
2003-12-03 00:01:07 +0000

    removed the function __getprogramname(), because the program's name can always reliably be obtained by means of FindTask(NULL)->tc_None.ln_Name. Adapted the rest of the code accordingly.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20243 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/autoinit/__showerror.c
+++ compiler/autoinit/__showerror.c
@@ -18,16 +18,23 @@ int __forceerrorrequester __attribute__((weak)) = 0;
 void __showerror(char *format, ...)
 {
     AROS_GET_SYSBASE_OK
-    char *pname  = __getprogramname();
-
+    
     va_list args;
+    struct DosLibrary *DOSBase = NULL;
+    const char *name = FindTask(NULL)->tc_Node.ln_Name;
+
     va_start(args, format);
 
-    if (0 && Cli() != NULL && !__forceerrorrequester)
+    if
+    (
+        !__forceerrorrequester                                                 &&
+        (DOSBase = (struct DosLibrary *)OpenLibrary("dos.library", 0)) != NULL &&
+	Cli() != NULL
+    )
     {
-        if (pname)
+        if (name)
 	{
-            PutStr(pname);
+            PutStr(name);
             PutStr(": ");
 	}
 #warning This next line might break on bizarre architectures.
@@ -35,27 +42,26 @@ void __showerror(char *format, ...)
         PutStr("\n");
     }
     else
+    if ((IntuitionBase = (struct IntuitionBase *)OpenLibrary("intuition.library", 0)))
     {
-     	if ((IntuitionBase = (struct IntuitionBase *)OpenLibrary("intuition.library", 0)))
-	{
-    	    struct EasyStruct es =
-    	    {
-		sizeof(struct EasyStruct),
-		0,
-		pname,
-		format,
-		"Exit"
-	    };
+        struct EasyStruct es =
+    	{
+	    sizeof(struct EasyStruct),
+	    0,
+	    name,
+	    format,
+	    "Exit"
+	};
 
-	    EasyRequestArgs(NULL, &es, NULL, (APTR)args);
+	EasyRequestArgs(NULL, &es, NULL, (APTR)args);
 
-	    CloseLibrary((struct Library *)IntuitionBase);
-	}
+	CloseLibrary((struct Library *)IntuitionBase);
     }
 
     va_end(args);
-
-    if (pname)
-        FreeVec(pname);
+    
+    if (DOSBase != NULL)
+        CloseLibrary((struct Library *)DOSBase);
+    
 }
 
--- compiler/autoinit/autoinit.h
+++ compiler/autoinit/autoinit.h
@@ -7,16 +7,12 @@ __BEGIN_DECLS
 
 /* Shows an error message to the user. Depending on whether the program
    has been started from CLI or from Workbench(TM) the message will be
-   respectively shown as a message on the cli or in a Intuition's requester.
+   respectively shown as a message on the cli or as an Intuition requester.
 
    To make it always open a requester define a global variable like this:
 
        int __forceerrorrequester = 1;  */
 void __showerror __P((char *format, ...));
-
-/* Returns the name of the running program. In case of error NULL is returned
-   and an IoError is set accordingly.  */
-char *__getprogramname __P((void));
 
 __END_DECLS
 
