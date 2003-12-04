458a4354f8e0540ef4cc79c9ceff7ad73ebfb025
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-12-04 21:41:44 +0000

    Header file containing some macros which are used both by the catalog source description file gcccatcomp.sd and by user programs. The only macro user programs are supposed to use is GetString, which is the usual macro used to get strings from a catalog: just use it like you always used it, the only exception is that you don't need to include any other header file than aros/locale.h itself, the linker will take care of everything else and will also report an error if you used by mistake a message identifier which is not existent.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20261 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/aros/locale.h
@@ -0,0 +1,59 @@
+#ifndef _AROS_LOCALE_H
+#define _AROS_LOCALE_H
+
+#include <proto/locale.h>
+#include <aros/symbolsets.h>
+
+struct CatalogStr
+{
+    const ULONG cs_NUM;
+    struct Catalog ** const cs_CatalogPtr;
+    const char cs_STR[1];
+};
+
+#define CATALOG_BEGIN(catalog, version) \
+    static struct Catalog *catalog ##_catalog; \
+    static const ULONG catalog ## _catalog_version = version;
+
+#define CATALOG_STR(catalog, ID, str, num)     \
+    const struct                               \
+    {                                          \
+        const ULONG cs_NUM;                    \
+        struct Catalog * const *cs_CatalogPtr; \
+        const char cs_STR[sizeof(str)];        \
+    } ID ## _STR = { num, &catalog ## _catalog, str };
+    
+#define CATALOG_END(catalog) \
+    static int OpenCatalog_ ## catalog(void)               \
+    {                                                      \
+        if (LocaleBase != NULL)                            \
+            catalog ## _catalog = OpenCatalog              \
+	    (                                              \
+	        NULL, #catalog ".catalog",                 \
+		OC_Version, catalog ## _catalog_version,   \
+		TAG_DONE                                   \
+	    );                                             \
+	                                                   \
+	return 1;                                          \
+    }                                                      \
+                                                           \
+    static void CloseCatalog_ ## catalog(void)             \
+    {                                                      \
+        if (LocaleBase != NULL)                            \
+            CloseCatalog(catalog ## _catalog);             \
+    }                                                      \
+                                                           \
+    ADD2INIT(OpenCatalog_ ## catalog, 0);                  \
+    ADD2EXIT(CloseCatalog_ ## catalog, 0);              
+
+#define GetString(ID)                                                                   \
+({                                                                                      \
+    extern const struct CatalogStr ID ## _STR;                                          \
+                                                                                        \
+    LocaleBase != NULL ?                                                                \
+        GetCatalogStr(*ID ## _STR.cs_CatalogPtr, ID ## _STR.cs_NUM, ID ## _STR.cs_STR)  \
+    :                                                                                   \
+        (CONST_STRPTR)ID ## _STR.cs_STR;                                                      \
+})
+	
+#endif /* !_AROS_LOCALE_H */
