32312e0737801cf13d1097958e1bb3bf0cc264b1
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-12-04 22:21:46 +0000

    Make use of the new non-error-reporting feature, so that even if locale.library cannot be opened the program continues execution. It's actually not tested...
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20265 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/autoinit/locale.c
+++ compiler/autoinit/locale.c
@@ -5,4 +5,10 @@
 
 #include <aros/symbolsets.h>
 
-ADD2LIBS("locale.library", 39, void *, LocaleBase);
+/* We use a negative version number because this way the library autoopening code
+   knows that we don't want errors to be reported if this library cannot be opened.
+   
+   Given v is the version number provided, the real version number this library needs
+   to have is -(v + 1). The additional unit is needed so that even libraries with
+   version number 0 can be opened without the autoopening code reporting errors.  */
+ADD2LIBS("locale.library", -40, void *, LocaleBase);
