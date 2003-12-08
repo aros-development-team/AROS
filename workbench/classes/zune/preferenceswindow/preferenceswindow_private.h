acc7d183fc85e115e2dea8fdc585b2327e815829
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-12-08 16:32:56 +0000

    Implemented _Disabled attributes for all buttons, so you can disable them if you need to. Removed the code that disables the save button if ENVARC: isn't writeable, since this should be handled elsewhere (can't assume that the application will save settings in ENVARC:).
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20294 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/classes/zune/preferenceswindow/preferenceswindow_private.h
+++ workbench/classes/zune/preferenceswindow/preferenceswindow_private.h
@@ -5,6 +5,11 @@
 struct PreferencesWindow_DATA
 {
     struct Catalog *pwd_Catalog;
+    Object         *pwd_TestButton,
+                   *pwd_RevertButton,
+                   *pwd_SaveButton,
+                   *pwd_UseButton,
+                   *pwd_CancelButton;
 };
 
 #endif /* _PREFERENCESWINDOW_PRIVATE_H_ */
