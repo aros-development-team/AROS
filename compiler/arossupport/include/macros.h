b83c75b4430f080cf4778117ff88e9d9380b8abb
falemagn <falemagn@gmail.com>
2003-12-13 00:50:10 +0000

    Reworked the way byteswapping and bitswapping are handled, so that it's now possible to add cpu-specific code which handles that stuff in a more optimized way.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20318 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/arossupport/include/macros.h
+++ compiler/arossupport/include/macros.h
@@ -5,13 +5,95 @@
     Copyright © 1995-2001, The AROS Development Team. All rights reserved.
     $Id$
 
-    Desc: Work on 64bit data types
+    Desc: Miscellanous macros
     Lang: english
 */
 
 #ifndef AROS_SYSTEM_H
 #   include <aros/system.h>
 #endif
+
+/* Reverse the bits in a word */
+#define AROS_SWAP_BITS_WORD_GENERIC(_w)            \
+({                                                 \
+    register UWORD w = _w;                         \
+                                                   \
+    w = ((w >> 1) & 0x5555) | ((w << 1) & 0xaaaa); \
+    w = ((w >> 2) & 0x3333) | ((w << 2) & 0xcccc); \
+    w = ((w >> 4) & 0x0f0f) | ((w << 4) & 0xf0f0); \
+        ((w >> 8) & 0x00ff) | ((w << 8) & 0xff00); \
+})
+
+/* Reverse the bits in a long */
+#define AROS_SWAP_BITS_LONG_GENERIC(_l)                          \
+({                                                               \
+    register ULONG l = _l;                                       \
+	                                                         \
+    l = ((l >>  1) & 0x55555555UL) | ((l <<  1) & 0xaaaaaaaaUL); \
+    l = ((l >>  2) & 0x33333333UL) | ((l <<  2) & 0xccccccccUL); \
+    l = ((l >>  4) & 0x0f0f0f0fUL) | ((l <<  4) & 0xf0f0f0f0UL); \
+    l = ((l >>  8) & 0x00ff00ffUL) | ((l <<  8) & 0xff00ff00UL); \
+        ((l >> 16) & 0x0000ffffUL) | ((l << 16) & 0xffff0000UL); \
+})
+
+/* Reverse the bytes in a word */
+#define AROS_SWAP_BYTES_WORD_GENERIC(_w)       \
+({                                             \
+    register const UWORD w = _w;               \
+                                               \
+    ((w >> 8) & 0x00FF) | ((w & 0x00FF) << 8); \
+})
+
+/* Reverse the bytes in a long */
+#define AROS_SWAP_BYTES_LONG_GENERIC(_l) \
+({                                       \
+    register const ULONG l = _l;         \
+                                         \
+    ((l >> 24) & 0x000000FFUL) |         \
+    ((l >>  8) & 0x0000FF00UL) |         \
+    ((l <<  8) & 0x00FF0000UL) |         \
+    ((l << 24) & 0xFF000000UL);          \
+})
+
+/* Use the CPU-specific definitions of the above macros, if they exist, but reuse
+   the generic macros in case the given value is a compile-time constant, because
+   the compiler will optimize things out for us.  */
+#if !defined(AROS_SWAP_BITS_WORD_CPU)
+#   define AROS_SWAP_BITS_WORD(w) AROS_SWAP_BITS_WORD_GENERIC(w)
+#elif defined(__GNUC__)
+#   define AROS_SWAP_BITS_WORD(w) \
+        (__builtin_constant_p(w) ? AROS_SWAP_BITS_WORD_GENERIC(w) : AROS_SWAP_BITS_WORD_CPU(w))
+#else
+#   define AROS_SWAP_BITS_WORD(w) AROS_SWAP_BITS_WORD_CPU(w)
+#endif
+
+#if !defined(AROS_SWAP_BITS_LONG_CPU)
+#   define AROS_SWAP_BITS_LONG(l) AROS_SWAP_BITS_LONG_GENERIC(l)
+#elif defined(__GNUC__)
+#   define AROS_SWAP_BITS_LONG(l) \
+        (__builtin_constant_p(l) ? AROS_SWAP_BITS_LONG_GENERIC(l) : AROS_SWAP_BITS_LONG_CPU(l))
+#else
+#   define AROS_SWAP_BITS_LONG(l) AROS_SWAP_BITS_LONG_CPU(l)
+#endif
+
+#if !defined(AROS_SWAP_BYTES_WORD_CPU)
+#   define AROS_SWAP_BYTES_WORD(w) AROS_SWAP_BYTES_WORD_GENERIC(w)
+#elif defined(__GNUC__)
+#   define AROS_SWAP_BYTES_WORD(w) \
+        (__builtin_constant_p(w) ? AROS_SWAP_BYTES_WORD_GENERIC(w) : AROS_SWAP_BYTES_WORD_CPU(w))
+#else
+#   define AROS_SWAP_BYTES_WORD(w) AROS_SWAP_BYTES_WORD_CPU(w)
+#endif
+
+#if !defined(AROS_SWAP_BYTES_LONG_CPU)
+#   define AROS_SWAP_BYTES_LONG(l) AROS_SWAP_BYTES_LONG_GENERIC(l)
+#elif defined(__GNUC__)
+#   define AROS_SWAP_BYTES_LONG(l) \
+        (__builtin_constant_p(l) ? AROS_SWAP_BYTES_LONG_GENERIC(l) : AROS_SWAP_BYTES_LONG_CPU(l))
+#else
+#   define AROS_SWAP_BYTES_LONG(l) AROS_SWAP_BYTES_LONG_CPU(l)
+#endif
+
 
 /* Convert a word or long to big endian and vice versa on the current hardware */
 #if AROS_BIG_ENDIAN
@@ -20,30 +102,18 @@
 #   define AROS_BE2WORD(w)     (w)
 #   define AROS_BE2LONG(l)     (l)
 #else
-#   define AROS_WORD2BE(w)     ((((w) >> 8) & 0x00FF) | (((w) & 0x00FF) << 8))
-#   define AROS_LONG2BE(l)     \
-	(                                  \
-	    ((((unsigned long)(l)) >> 24) & 0x000000FFUL) | \
-	    ((((unsigned long)(l)) >>  8) & 0x0000FF00UL) | \
-	    ((((unsigned long)(l)) <<  8) & 0x00FF0000UL) | \
-	    ((((unsigned long)(l)) << 24) & 0xFF000000UL)   \
-	)
-#   define AROS_BE2WORD(w)     AROS_WORD2BE(w)
-#   define AROS_BE2LONG(l)     AROS_LONG2BE(l)
+#   define AROS_WORD2BE(w)     AROS_SWAP_BYTES_WORD(w)
+#   define AROS_LONG2BE(l)     AROS_SWAP_BYTES_LONG(l)
+#   define AROS_BE2WORD(w)     AROS_SWAP_BYTES_WORD(w)
+#   define AROS_BE2LONG(l)     AROS_SWAP_BYTES_LONG(l)
 #endif
 
 /* Convert a word or long to little endian and vice versa on the current hardware */
 #if AROS_BIG_ENDIAN
-#   define AROS_WORD2LE(w)     ((((w) >> 8) & 0x00FF) | (((w) & 0x00FF) << 8))
-#   define AROS_LONG2LE(l)     \
-	(                                  \
-	    ((((unsigned long)(l)) >> 24) & 0x000000FFUL) | \
-	    ((((unsigned long)(l)) >>  8) & 0x0000FF00UL) | \
-	    ((((unsigned long)(l)) <<  8) & 0x00FF0000UL) | \
-	    ((((unsigned long)(l)) << 24) & 0xFF000000UL)   \
-	)
-#   define AROS_LE2WORD(w)     AROS_WORD2LE(w)
-#   define AROS_LE2LONG(l)     AROS_LONG2LE(l)
+#   define AROS_WORD2LE(w)     AROS_SWAP_BYTES_WORD(w)
+#   define AROS_LONG2LE(l)     AROS_SWAP_BYTES_LONG(l)
+#   define AROS_LE2WORD(w)     AROS_SWAP_BYTES_WORD(w)
+#   define AROS_LE2LONG(l)     AROS_SWAP_BYTES_LONG(l)
 #else
 #    define AROS_WORD2LE(w)    (w)
 #    define AROS_LONG2LE(l)    (l)
@@ -107,30 +177,9 @@
 		:   ((l) & 0x40000000UL) ? 30 : (l) ? 31 : -1)
 #endif /* AROS_LEAST_BIT_POS */
 
-/* Swap to integer variables */
+/* Swap two integer variables */
 #ifndef AROS_SWAP
 #   define AROS_SWAP(x,y)       (x) ^= (y) ^= (x) ^= (y)
-#endif
-
-/* Reverse the bits in a word */
-#ifndef AROS_SWAP_BITS_WORD
-#   define AROS_SWAP_BITS_WORD(w) \
-	((w) = ((w) = ((((w) >> 1) & 0x5555) | (((w) << 1) & 0xaaaa)), \
-		(w) = ((((w) >> 2) & 0x3333) | (((w) << 2) & 0xcccc)), \
-		(w) = ((((w) >> 4) & 0x0f0f) | (((w) << 4) & 0xf0f0)), \
-		(w) = ((((w) >> 8) & 0x00ff) | (((w) << 8) & 0xff00))) \
-	)
-#endif
-
-/* Reverse the bits in a long */
-#ifndef AROS_SWAP_BITS_LONG
-#   define AROS_SWAP_BITS_LONG(l) \
-	((l) = ((l) = ((((l) >>  1) & 0x55555555UL) | (((l) <<  1) & 0xaaaaaaaaUL)), \
-		(l) = ((((l) >>  2) & 0x33333333UL) | (((l) <<  2) & 0xccccccccUL)), \
-		(l) = ((((l) >>  4) & 0x0f0f0f0fUL) | (((l) <<  4) & 0xf0f0f0f0UL)), \
-		(l) = ((((l) >>  8) & 0x00ff00ffUL) | (((l) <<  8) & 0xff00ff00UL)), \
-		(l) = ((((l) >> 16) & 0x0000ffffUL) | (((l) << 16) & 0xffff0000UL))) \
-	)
 #endif
 
 #endif /* AROS_MACROS_H */
