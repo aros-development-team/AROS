dded3dae9238323f141aca2fceedf615326bbf20
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-12-23 22:10:59 +0000

    Convert partition.library to use the %build_module genmf macro, not %build_module_macro; including AROS libinit stuff.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20393 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/partition/partition_init.c
+++ workbench/libs/partition/partition_init.c
@@ -6,6 +6,7 @@
    Lang: English
 */
 
+#include <aros/symbolsets.h>
 
 #define DEBUG 1
 #include <aros/debug.h>
@@ -14,26 +15,11 @@
 #include "partition_support.h"
 #include LC_LIBDEFS_FILE
 
-#undef SysBase
-
-/* customize libheader.c */
-#define LC_RESIDENTPRI 104
-#define LC_LIBBASESIZE sizeof(LIBBASETYPE)
-#define LC_NO_OPENLIB
-#define LC_NO_CLOSELIB
-#define LC_RESIDENTFLAGS (RTF_AUTOINIT | RTF_COLDSTART)
-
-#include <libcore/libheader.c>
-
-#define PartitionBase ((LIBBASETYPEPTR) lh)
-
-ULONG SAVEDS STDARGS LC_BUILDNAME(L_InitLib) (LC_LIBHEADERTYPEPTR lh)
+AROS_SET_LIBFUNC(Init, LIBBASETYPE, LIBBASE)
 {
-    PartitionBase->tables = (struct PartitionTableInfo **)PartitionSupport;
-   return TRUE;
+    LIBBASE->tables = (struct PartitionTableInfo **)PartitionSupport;
+    return TRUE;
 }
 
-void SAVEDS STDARGS LC_BUILDNAME(L_ExpungeLib) (LC_LIBHEADERTYPEPTR lh)
-{
-}
+ADD2INITLIB(Init, 0);
 
