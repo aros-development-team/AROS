2be7452b32ec283de7718a1000152caf9c486b21
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-01-02 01:25:02 +0000

    Added tm_gmtoff and tm_zone to struct tm.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20463 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/include/time.h
+++ compiler/clib/include/time.h
@@ -32,15 +32,17 @@ typedef __clock_t           clock_t;
 
 struct tm
 {
-    int tm_sec;
-    int tm_min;
-    int tm_hour;
-    int tm_mday;
-    int tm_mon;
-    int tm_year;
-    int tm_wday;
-    int tm_yday;
-    int tm_isdst;
+    int         tm_sec;
+    int         tm_min;
+    int         tm_hour;
+    int         tm_mday;
+    int         tm_mon;
+    int         tm_year;
+    int         tm_wday;
+    int         tm_yday;
+    int         tm_isdst;
+    long int    tm_gmtoff;
+    const char *tm_zone;
 };
 
 #if !defined(_ANSI_SOURCE) && defined(_P1003_1B_VISIBLE)
