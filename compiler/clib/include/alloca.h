732b03882b54a1d35bb228d4a3510c7710d12326
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-01-06 01:38:21 +0000

    declare __alloca_get_stack_limit() outside of alloca()'s scope.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20529 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/include/alloca.h
+++ compiler/clib/include/alloca.h
@@ -19,6 +19,9 @@ __BEGIN_DECLS
 /* Allocate a block of memory which will be automatically freed upon function exiting. */
 extern void *alloca(size_t size);
 
+/* Private function to get the upper or lower bound (depending on the architecture)
+   of the stack.  */
+extern void *__alloca_get_stack_limit(void);
 __END_DECLS
 
 /* GNU C provides a builtin alloca function. */
@@ -26,14 +29,6 @@ __END_DECLS
 #    if AROS_STACK_GROWS_DOWNWARDS
 #        define alloca(size)                                                            \
          ({                                                                             \
-             /*                                                                         \
-                 private function to get the upper or lower bound                       \
-                 (depending on the architecture) of the stack                           \
-             */                                                                         \
-             __BEGIN_DECLS                                                              \
-             extern void *__alloca_get_stack_limit();                                   \
-             __END_DECLS                                                                \
-                                                                                        \
              ((void *)(AROS_GET_SP - AROS_ALIGN(size)) <= __alloca_get_stack_limit()) ? \
              NULL :                                                                     \
              __builtin_alloca(size);                                                    \
@@ -41,14 +36,6 @@ __END_DECLS
 #    else
 #        define alloca(size)                                                            \
          ({                                                                             \
-             /*                                                                         \
-                 private function to get the upper or lower bound                       \
-                 (depending on the architecture) of the stack                           \
-             */                                                                         \
-             __BEGIN_DECLS                                                              \
-             extern void *__alloca_get_stack_limit();                                   \
-             __END_DECLS                                                                \
-                                                                                        \
              ((void *)(AROS_GET_SP + AROS_ALIGN(size)) >= __alloca_get_stack_limit()) ? \
              NULL :                                                                     \
              __builtin_alloca(size);                                                    \
