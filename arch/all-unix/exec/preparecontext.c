446ef0cca2e0b07133a4abb3be25815349ee4637
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-01-08 18:51:51 +0000

    New tagList param. Still unhandled.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20552 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-unix/exec/preparecontext.c
+++ arch/all-unix/exec/preparecontext.c
@@ -9,16 +9,18 @@
 #include <exec/types.h>
 #include <exec/execbase.h>
 #include <exec/memory.h>
+#include <utility/tagitem.h>
 #include <sigcore.h>
 #include "etask.h"
 #include "exec_util.h"
 
 #include <aros/libcall.h>
 
-AROS_LH3(BOOL, PrepareContext,
+AROS_LH4(BOOL, PrepareContext,
     AROS_LHA(struct Task *, task, A0),
     AROS_LHA(APTR, entryPoint, A1),
     AROS_LHA(APTR, fallBack, A2),
+    AROS_LHA(struct TagItem *, tagList, A3),
     struct ExecBase *, SysBase, 6, Exec)
 {
     AROS_LIBFUNC_INIT
