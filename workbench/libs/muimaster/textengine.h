35672a61fc71797a045efcd9d1539525d4f3b588
dlc <dlc@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-01-09 19:11:04 +0000

    removed all editing and unused code, tag is EDITING_REMOVAL_20040109
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20572 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/textengine.h
+++ workbench/libs/muimaster/textengine.h
@@ -24,23 +24,20 @@ typedef struct ZTextChunk {
     Object               *obj; /* Area subclass, see List_CreateImage */
     LONG                  dripen;
     LONG                  pen;
-    UBYTE                 style;
+    UBYTE                 style; /* FSF_UNDERLINED, FSF_BOLD, ... */
     WORD                  cwidth;
     WORD                  cheight;
 } ZTextChunk;
 
 typedef struct ZTextLine {
-    struct MinNode node; /* embedded node */
-    UBYTE  align;
+    struct MinNode node;       /* embedded node */
+    UBYTE  align;              /* ZTL_CENTER, ZTL_RIGHT, ZTL_LEFT */
     struct MinList chunklist;
     WORD   lwidth;
     WORD   lheight;
 } ZTextLine;
 
 struct ZText {
-    LONG xscroll; /* number of pixel which are not visible */
-    LONG yscroll;
-
     WORD width; /* store calculated bounds - read only ! */
     WORD height;
 
@@ -58,23 +55,8 @@ enum {
 };
 
 ZText *zune_text_new (CONST_STRPTR preparse, CONST_STRPTR content, int argtype, TEXT arg);
-char *zune_text_iso_string(ZText *text);
 void zune_text_destroy (ZText *text);
 void zune_text_get_bounds (ZText *text, Object *obj);
 void zune_text_draw (ZText *text, Object *obj, WORD left, WORD right, WORD top);
-
-#if 0
-void zune_text_draw_cursor (ZText *text, Object *obj, WORD left, WORD right, WORD top, LONG cursorx, LONG cursory);
-void zune_text_draw_single (ZText *text, Object *obj, WORD left, WORD right, WORD top, LONG xpos, LONG ypos, BOOL cursor);
-
-int zune_get_xpos_of_line(ZText *text, Object *obj, LONG y, LONG xpixel);
-//int zune_text_horiz_pixel_to_coord(ZText *text, Object *obj, struct ZTextLine *line
-int zune_text_get_char_pos(ZText *text, Object *obj, LONG x, LONG y, struct ZTextLine **line_ptr, struct ZTextChunk **chunk_ptr, int *offset_ptr, int *len_ptr);
-int zune_text_get_line_len(ZText *text, Object *obj, LONG y);
-int zune_text_get_lines(ZText *text);
-int zune_make_cursor_visible(ZText *text, Object *obj, LONG cursorx, LONG cursory, LONG left, LONG top, LONG right, LONG bottom);
-
-int zune_text_merge(ZText *text, Object *obj, int x, int y, ZText *tomerge);
-#endif
 
 #endif
