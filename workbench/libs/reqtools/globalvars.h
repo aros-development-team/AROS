2d8889e53a2bf5088e875c47315ec5a0eff92c73
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-01-13 19:46:04 +0000

    Don't define the global base variables for other libraries so autoinit can do its job.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20592 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/reqtools/globalvars.h
+++ workbench/libs/reqtools/globalvars.h
@@ -1,4 +1,13 @@
+#ifdef __RT_DEFINEVARS
+#define extern
+#endif
 extern struct ReqToolsBase  	*ReqToolsBase;
+extern struct Device 		*ConsoleDevice;
+extern struct IOStdReq		iorequest;
+extern Class			*ButtonImgClass;
+
+/* On AROS don't define the libbases because that will prevent autoinit */
+#if !defined(__RT_DEFINEVARS) || !defined(__AROS__)
 extern struct ExecBase 	    	*SysBase;
 extern struct DosLibrary 	*DOSBase;
 extern struct UtilityBase 	*UtilityBase;
@@ -7,6 +16,8 @@ extern struct GfxBase 		*GfxBase;
 extern struct LocaleBase 	*LocaleBase;
 extern struct Library 		*LayersBase;
 extern struct Library 		*GadToolsBase;
-extern struct Device 		*ConsoleDevice;
-extern struct IOStdReq		iorequest;
-extern Class			*ButtonImgClass;
+#endif
+
+#ifdef __RT_DEFINEVARS
+#undef extern
+#endif
