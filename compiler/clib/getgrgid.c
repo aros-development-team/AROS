5c028771789b2813d8da7892705da6d9d4897b72
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-01-13 00:12:27 +0000

    Implemented stubs for getgrgid() and getpwuid().
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20588 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/getgrgid.c
@@ -0,0 +1,13 @@
+/*
+    Copyright © 2004, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <grp.h>
+
+struct group *getgrgid(gid_t uid)
+{
+    #warning Implement getgrgid()
+    
+    return NULL;
+}
---
+++ compiler/clib/getpwuid.c
@@ -0,0 +1,13 @@
+/*
+    Copyright © 2004, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <pwd.h>
+
+struct passwd *getpwuid(uid_t uid)
+{
+    #warning Implement getpwuid()
+    
+    return NULL;
+}
