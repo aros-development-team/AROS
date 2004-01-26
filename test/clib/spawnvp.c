3cc48302bca442841cdfdf48af2ed15262818f31
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-01-26 19:35:38 +0000

    Dunno why, but at the time I forgot to commit this file.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20726 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/clib/spawnvp.c
@@ -0,0 +1,29 @@
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: Test program for the libc's spawnv() function.
+    Lang: English
+*/
+
+#include <process.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int ret;
+
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: %s <command with or without full path> <command's arguments>\n", argv[0]);
+        return 20;
+    }
+
+    if ((ret = spawnvp(P_WAIT, argv[1], &argv[1])) == -1)
+    {
+        perror(argv[1]);
+	return 20;
+    }
+
+    return ret;
+}
