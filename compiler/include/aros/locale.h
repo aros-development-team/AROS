0706c93d5f8519822f727b32bbd8be0f53cd0258
falemagn <falemagn@gmail.com>
2004-01-27 17:22:07 +0000

    Use the __CONCAT macro.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20733 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/aros/locale.h
+++ compiler/include/aros/locale.h
@@ -1,11 +1,13 @@
 #ifndef _AROS_LOCALE_H
 #define _AROS_LOCALE_H
 
-#define GetString(ID)              \
-({                                 \
-    extern const char *ID ## _STR; \
-                                   \
-    ID ## _STR;                    \
+#include <aros/system.h>
+
+#define GetString(ID)                      \
+({                                         \
+    extern const char *__CONCAT(ID, _STR); \
+                                           \
+    __CONCAT(ID, _STR);                    \
 })                                  
 	
 #endif /* !_AROS_LOCALE_H */
