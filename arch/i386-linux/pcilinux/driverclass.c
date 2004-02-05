14c4299a7abd67c5d4ddb28b0afb2136550d5291
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-02-05 00:06:08 +0000

    PCI linux driver. Use as driver template
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20801 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-linux/pcilinux/driverclass.c
+++ arch/i386-linux/pcilinux/driverclass.c
@@ -1,4 +1,12 @@
+/*
+    Copyright © 2004, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: PCI direct driver for i386 linux.
+    Lang: English
+*/
 #include <exec/types.h>
+#include <hidd/hidd.h>
 #include <hidd/pci.h>
 #include <oop/oop.h>
 
@@ -10,14 +18,21 @@
 
 #include "pci.h"
 
-#define DEBUG 1
+#define DEBUG 0
 #include <aros/debug.h>
 
-#define	HiddPCIDriverAB	(PSD(cl)->hiddPCIDriverAB)
+#ifdef HiddPCIDriverAttrBase
+#undef HiddPCIDriverAttrBase
+#endif // HiddPCIDriverAttrBase
+
+#define	HiddPCIDriverAttrBase	(PSD(cl)->hiddPCIDriverAB)
+#define HiddAttrBase		(PSD(cl)->hiddAB)
 
 #define CFGADD(bus,dev,func,reg)    \
     ( 0x80000000 | ((bus)<<16) |    \
     ((dev)<<11) | ((func)<<8) | ((reg)&~3))
+
+#define SysBase (PSD(cl)->sysbase)
 
 typedef union _pcicfg
 {
@@ -25,6 +40,41 @@ typedef union _pcicfg
     UWORD   uw[2];
     UBYTE   ub[4];
 } pcicfg;
+
+/*
+    We overload the New method in order to introduce the Hidd Name and
+    HardwareName attributes.
+*/
+static OOP_Object *pcidriver_new(OOP_Class *cl, OOP_Object *o, struct pRoot_New *msg)
+{
+    struct pRoot_New mymsg;
+    
+    struct TagItem mytags[] = {
+	{ aHidd_Name, (IPTR)"PCILinux" },
+	{ aHidd_HardwareName, (IPTR)"Linux direct access PCI driver" },
+	{ aHidd_PCIDriver_DirectBus, FALSE },
+	{ TAG_DONE, 0 }
+    };
+
+    mymsg.mID = msg->mID;
+    mymsg.attrList = (struct TagItem *)&mytags;
+
+    if (msg->attrList)
+    {
+        mytags[3].ti_Tag = TAG_MORE;
+        mytags[3].ti_Data = (IPTR)msg->attrList;
+    }
+ 
+    msg = &mymsg;
+ 
+    o = (OOP_Object *)OOP_DoSuperMethod(cl, o, (OOP_Msg)msg);
+
+    return o;
+}
+
+/*
+    Some in/out magic to access PCI bus on PCs
+*/
 
 static inline ULONG inl(UWORD port)
 {
@@ -53,24 +103,6 @@ static ULONG pcidriver_RL(OOP_Class *cl, OOP_Object *o,
     return temp;
 }
 
-static UWORD pcidriver_RW(OOP_Class *cl, OOP_Object *o, 
-    struct pHidd_PCIDriver_ReadConfigWord *msg)
-{
-    pcicfg temp;
-
-    temp.ul = pcidriver_RL(cl, o, (struct pHiddPCIDriver_ReadConfigLong *)msg);
-    return temp.uw[(msg->reg&2)>>1];
-}
-
-static UBYTE pcidriver_RB(OOP_Class *cl, OOP_Object *o, 
-    struct pHidd_PCIDriver_ReadConfigByte *msg)
-{
-    pcicfg temp;
-
-    temp.ul = pcidriver_RL(cl, o, (struct pHiddPCIDriver_ReadConfigLong *)msg); 
-    return temp.ub[msg->reg & 3];
-}
-
 static void pcidriver_WL(OOP_Class *cl, OOP_Object *o,
     struct pHidd_PCIDriver_WriteConfigLong *msg)
 {
@@ -82,15 +114,16 @@ static void pcidriver_WL(OOP_Class *cl, OOP_Object *o,
     outl(orig, PCI_AddressPort);
 }
 
-static void pcidriver_WW(OOP_Class *cl, OOP_Object *o,
-    struct pHidd_PCIDriver_WriteConfigWord *msg)
+/*
+    In case of linux driver, where we use PCI subsystem mostly for fun (at
+    least now), there is no need for CPUtoPCI and PCItoCPU translations.
+    Therefore we return 0xffffffff always
+*/
+static IPTR pcidriver_c2p(OOP_Class *cl, OOP_Object *o,
+    OOP_Msg msg)
 {
-}
-
-static void pcidriver_WB(OOP_Class *cl, OOP_Object *o,
-    struct pHidd_PCIDriver_WriteConfigByte *msg)
-{
-}
+    return -1;
+};
 
 /* Class initialization and destruction */
 
@@ -114,11 +147,12 @@ void free_pcidriverclass(struct pci_staticdata *psd, OOP_Class *cl)
 	    OOP_DisposeObject((OOP_Object *)cl);
 	
 	OOP_ReleaseAttrBase(IID_Hidd_PCIDriver);
+	OOP_ReleaseAttrBase(IID_Hidd);
     }
 }
 	
-#define _NUM_ROOT_METHODS	0
-#define _NUM_PCIDRIVER_METHODS	NUM_PCIDRIVER_METHODS
+#define _NUM_ROOT_METHODS	1
+#define _NUM_PCIDRIVER_METHODS	4
 
 OOP_Class *init_pcidriverclass(struct pci_staticdata *psd)
 {
@@ -127,17 +161,16 @@ OOP_Class *init_pcidriverclass(struct pci_staticdata *psd)
 
     struct OOP_MethodDescr root_descr[_NUM_ROOT_METHODS + 1] = 
     {
+	{ OOP_METHODDEF(pcidriver_new), moRoot_New },
 	{ NULL, 0UL }
     };
 
     struct OOP_MethodDescr pcidriver_descr[_NUM_PCIDRIVER_METHODS + 1] =
     {
-	{ OOP_METHODDEF(pcidriver_RB),  moHidd_PCIDriver_ReadConfigByte },
-	{ OOP_METHODDEF(pcidriver_RW),  moHidd_PCIDriver_ReadConfigWord },
 	{ OOP_METHODDEF(pcidriver_RL),  moHidd_PCIDriver_ReadConfigLong },
-	{ OOP_METHODDEF(pcidriver_WB),  moHidd_PCIDriver_WriteConfigByte },
-	{ OOP_METHODDEF(pcidriver_WW),  moHidd_PCIDriver_WriteConfigWord },
 	{ OOP_METHODDEF(pcidriver_WL),  moHidd_PCIDriver_WriteConfigLong },
+	{ OOP_METHODDEF(pcidriver_c2p), moHidd_PCIDriver_CPUtoPCI },
+	{ OOP_METHODDEF(pcidriver_c2p), moHidd_PCIDriver_PCItoCPU },
 	{ NULL, 0UL }
     };
 
@@ -167,18 +200,26 @@ OOP_Class *init_pcidriverclass(struct pci_staticdata *psd)
 	{
 	    cl->UserData = (APTR)psd;
 	    psd->hiddPCIDriverAB = OOP_ObtainAttrBase(IID_Hidd_PCIDriver);
+	    psd->hiddAB = OOP_ObtainAttrBase(IID_Hidd);
+
 	    if (psd->hiddPCIDriverAB)
 	    {
+		/*
+		    The class may be added to the system. Add the driver
+		    to PCI subsystem as well
+		*/
 		struct pHidd_PCI_AddHardwareDriver msg;
 		
 		OOP_AddClass(cl);
 		D(bug("LinuxPCI: Driver Class OK\n"));
 		
-		msg.managed = 1;
+		/*
+		    PCI is suppose to destroy the class on its Dispose
+		*/
 		msg.driverClass = cl;
 		msg.mID = OOP_GetMethodID(IID_Hidd_PCI, moHidd_PCI_AddHardwareDriver);
-		D(bug("LinuxPCI: Adding Driver to main the class OK\n"));
 
+		// Add it for God's sake! ;)
 		pci = OOP_NewObject(NULL, CLID_Hidd_PCI, NULL);
 		OOP_DoMethod(pci, (OOP_Msg)&msg);
 		OOP_DisposeObject(pci);
