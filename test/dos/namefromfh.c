9e5e336e5e8197220406de92f4d143791f0d2497
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-02-16 19:59:02 +0000

    Small test program which excercises NameFromFH().
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20930 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/dos/namefromfh.c
@@ -0,0 +1,30 @@
+#include <proto/dos.h>
+#include <stdio.h>
+
+int main(void)
+{
+    TEXT buffer[512];
+    BPTR fh = Open("__TEST__", MODE_NEWFILE);
+    
+    if (fh != NULL)
+    {
+        if (NameFromFH(fh, buffer, 512))
+        {
+            printf("got name: %s\n", buffer);
+        }
+        else
+        {
+            printf("namefromlock failed. ioerr = %d\n", IoErr());
+        }
+        
+        Close(fh);
+        DeleteFile("__TEST__");
+    }
+    else
+    {
+        printf("couldn't create file\n");
+    }
+    
+
+    return 0;
+}
