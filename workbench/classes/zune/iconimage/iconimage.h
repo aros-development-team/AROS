894f838f9217ef7635586af75a9b2d2ab7a1c72f
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-02-17 15:10:51 +0000

    License change. IconImage is now under the LGPL.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20944 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/classes/zune/iconimage/iconimage.h
+++ workbench/classes/zune/iconimage/iconimage.h
@@ -1,6 +1,14 @@
 #ifndef ZUNE_ICONIMAGE_H
 #define ZUNE_ICONIMAGE_H
 
+/*
+    Copyright © 2003-2004, The AROS Development Team. All rights reserved.
+    This file is part of the IconImage class, which is distributed under
+    the terms of version 2.1 of the GNU Lesser General Public License.
+    
+    $Id$
+*/
+
 #include <libraries/mui.h>
 
 /*** Name *******************************************************************/
--- workbench/classes/zune/iconimage/iconimage_private.h
+++ workbench/classes/zune/iconimage/iconimage_private.h
@@ -1,6 +1,14 @@
 #ifndef _ICONIMAGE_PRIVATE_H_
 #define _ICONIMAGE_PRIVATE_H_
 
+/*
+    Copyright © 2003-2004, The AROS Development Team. All rights reserved.
+    This file is part of the IconImage class, which is distributed under
+    the terms of version 2.1 of the GNU Lesser General Public License.
+    
+    $Id$
+*/
+
 #include <workbench/icon.h>
 
 /*** Instance data **********************************************************/
