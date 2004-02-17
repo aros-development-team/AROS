1e128b2932232d02910a305c1d45a8d6739dd837
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-02-17 15:05:52 +0000

    Wanderer prefs program. Quite good example how to use the new prefs classes.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20943 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/prefs/wanderer/locale.c
@@ -0,0 +1,57 @@
+/*
+    Copyright © 2004, The AROS Development Team. All rights reserved.
+    This file is part of the Wanderer Preferences program, which is distributed
+    under the terms of version 2 of the GNU General Public License.
+
+    $Id$
+*/
+
+#include <exec/types.h>
+#include <proto/locale.h>
+
+#define CATCOMP_ARRAY
+#include "strings.h"
+
+#define CATALOG_NAME     "System/Prefs/Wanderer.catalog"
+#define CATALOG_VERSION  0
+
+/*** Variables **************************************************************/
+struct Catalog *catalog;
+
+
+/*** Functions **************************************************************/
+/* Main *********************************************************************/
+CONST_STRPTR _(ULONG id)
+{
+    if (LocaleBase != NULL && catalog != NULL)
+    {
+	return GetCatalogStr(catalog, id, CatCompArray[id].cca_Str);
+    } 
+    else 
+    {
+	return CatCompArray[id].cca_Str;
+    }
+}
+
+/* Setup ********************************************************************/
+BOOL Locale_Initialize(void)
+{
+    if (LocaleBase != NULL)
+    {
+        catalog = OpenCatalog
+        ( 
+            NULL, CATALOG_NAME, OC_Version, CATALOG_VERSION, TAG_DONE 
+        );
+    }
+    else
+    {
+        catalog = NULL;
+    }
+    
+    return TRUE;
+}
+
+void Locale_Deinitialize(void)
+{
+    if(LocaleBase != NULL && catalog != NULL) CloseCatalog(catalog);
+}
---
+++ workbench/prefs/wanderer/wpeditor.h
@@ -0,0 +1,24 @@
+#ifndef _WPEDITOR_H_
+#define _WPEDITOR_H_
+
+/*
+    Copyright © 2004, The AROS Development Team. All rights reserved.
+    This file is part of the Wanderer Preferences program, which is distributed
+    under the terms of version 2 of the GNU General Public License.
+    
+    $Id$
+*/
+
+#include <exec/types.h>
+#include <libraries/mui.h>
+
+/*** Identifier base ********************************************************/
+#define MUIB_WPEditor                  (TAG_USER | 0x10000000)
+
+/*** Variables **************************************************************/
+extern struct MUI_CustomClass *WPEditor_CLASS;
+
+/*** Macros *****************************************************************/
+#define WPEditorObject BOOPSIOBJMACRO_START(WPEditor_CLASS->mcc_Class)
+
+#endif /* _WPEDITOR_H_ */
