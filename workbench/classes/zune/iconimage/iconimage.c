91bead3e046d0646cf105123d65154e07cc32329
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-02-17 15:10:51 +0000

    License change. IconImage is now under the LGPL.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20944 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/classes/zune/iconimage/iconimage.c
+++ workbench/classes/zune/iconimage/iconimage.c
@@ -1,5 +1,8 @@
 /*
-    Copyright © 2003, The AROS Development Team. All rights reserved.
+    Copyright © 2003-2004, The AROS Development Team. All rights reserved.
+    This file is part of the IconImage class, which is distributed under
+    the terms of version 2.1 of the GNU Lesser General Public License.
+    
     $Id$
 */
 
