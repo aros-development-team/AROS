eddcbedd5c5157aa5bc4723d208a38d908d31e8d
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-02-18 10:20:13 +0000

    Licensing change. AboutWindow class is now under the LGPL.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20949 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/classes/zune/aboutwindow/aboutwindow.h
+++ workbench/classes/zune/aboutwindow/aboutwindow.h
@@ -1,6 +1,14 @@
 #ifndef ZUNE_ABOUTWINDOW_H
 #define ZUNE_ABOUTWINDOW_H
 
+/*
+    Copyright © 2003-2004, The AROS Development Team. All rights reserved.
+    This file is part of the AboutWindow class, which is distributed under
+    the terms of version 2.1 of the GNU Lesser General Public License.
+    
+    $Id$
+*/
+
 #include <utility/tagitem.h>
 #include <libraries/mui.h>
 
--- workbench/classes/zune/aboutwindow/aboutwindow_private.h
+++ workbench/classes/zune/aboutwindow/aboutwindow_private.h
@@ -2,7 +2,10 @@
 #define _ABOUTWINDOW_PRIVATE_H_
 
 /*
-    Copyright © 2003, The AROS Development Team. All rights reserved.
+    Copyright © 2003-2004, The AROS Development Team. All rights reserved.
+    This file is part of the AboutWindow class, which is distributed under
+    the terms of version 2.1 of the GNU Lesser General Public License.
+    
     $Id$
 */
 
