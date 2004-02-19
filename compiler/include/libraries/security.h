d230bc570310d579686e072ccbde3b8820c2ac64
Kalamatee <kalamatee@gmail.com>
2004-02-19 00:56:49 +0000

    headers for security.library
    git-svn-id: https://svn.aros.org/svn/aros/trunk@20976 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/libraries/security.h
@@ -0,0 +1,91 @@
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc:
+    Lang: english
+*/
+#ifndef _SECURITY_INTERN_H
+#define _SECURITY_INTERN_H
+
+#ifndef EXEC_TYPES_H
+#include <exec/types.h>
+#endif
+#ifndef EXEC_NODES_H
+#include <exec/nodes.h>
+#endif
+#ifndef UTILITY_UTILITY_H
+#include <utility/utility.h>
+#endif
+#ifndef HIDD_HIDD_H
+#include <hidd/hidd.h>
+#endif
+#ifndef EXEC_LISTS_H
+#   include <exec/lists.h>
+#endif
+
+#define DEBUG   1
+
+#include <exec/resident.h>
+#include <exec/execbase.h>
+#include <exec/memory.h>
+#include <hardware/intbits.h>
+#include <asm/segments.h>
+#include <asm/linkage.h>
+#include <asm/ptrace.h>
+#include <dos/dosextens.h>
+
+#include <aros/arossupportbase.h>
+#include <aros/asmcall.h>
+#include <aros/config.h>
+#include <aros/debug.h>
+#include <aros/multiboot.h>
+
+#include <hardware/custom.h>
+
+   /*
+    *    Extended Owner Information Structure
+    *
+    *    A pointer to this structure is returned by secGetTaskExtOwner().
+    *    You MUST use secFreeExtOwner() to deallocate it!!
+    */
+
+struct muExtOwner {
+   UWORD                    uid;
+   UWORD                    gid;
+   UWORD                    NumSecGroups;               /* Number of Secondary Groups this */
+                                                        /* user belongs too. */
+};
+
+
+/* Function called to initialize the plugin.
+ * The plugin should perform any initialization it needs, and then formally
+ * register itself with the security.library.
+ * module should be passed to the security.library when a handler is registered.
+ * */
+
+
+/* Plugin Module Definition - must be located in the executeable */
+struct secPluginHeader	{
+#define secPLUGIN_RECOGNITION	MAKE_ID('m', 'S', 'p', 'I')
+    
+    ULONG                   plugin_magic;               /* This should be secPLUGIN_RECOGNITION */
+    /* Module Descriptor */
+    ULONG	            Version;	                /* = MUFS_PLUGIN_INTERFACE */
+    APTR                    Initialize;
+    APTR                    Terminate;
+//    secInitPluginFunc       Initialize;
+//    secTerminatePluginFunc  Terminate;
+};
+
+/* internal plugin records */
+typedef struct
+{
+    struct MinNode          Node;
+    ULONG                   reference_count;
+    BPTR                    SegList;
+    struct secPluginHeader * header;	                /* For locating the init/fini functions */
+    UBYTE                   modulename[64];	        /* For displaying */
+} secPluginModule;
+
+#endif /* _SECURITY_INTERN_H */
