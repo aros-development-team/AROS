e67adefc1bdcf2b257113faacc2a180fa7032e7a
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-02-20 20:43:49 +0000

    Rewrote to use PutPattern Bitmap method.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@21010 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/graphics/bltpattern.c
+++ rom/graphics/bltpattern.c
@@ -13,37 +13,24 @@
 #include "gfxfuncsupport.h"
 #include <proto/oop.h>
 
-struct pattern_info
-{
-    PLANEPTR 	    mask;
-    struct RastPort *rp;
-    LONG    	    mask_bpr; /* Bytes per row */
-    
-    LONG    	    orig_xmin;
-    LONG    	    orig_ymin;
-    
-    UBYTE   	    dest_depth;
-    
-    struct GfxBase *gfxbase;
-    
-};
-
-struct bltpattern_render_data
-{
-     struct render_special_info rsi;
-     struct pattern_info    	*pi;
-};
-
-static VOID pattern_to_buf(struct pattern_info *pi, LONG x_src, LONG y_src,
-    	    	    	   LONG x_dest, LONG y_dest, ULONG xsize, ULONG ysize,
-			   ULONG *buf, OOP_Object *dest_bm, HIDDT_Pixel *coltab);
-
-
 static ULONG bltpattern_render(APTR bpr_data, LONG srcx, LONG srcy,
     	    	    	       OOP_Object *dstbm_obj, OOP_Object *dst_gc,
 			       LONG x1, LONG y1, LONG x2, LONG y2,
 			       struct GfxBase *GfxBase);
 
+struct bp_render_data
+{
+    HIDDT_PixelLUT   pixlut;
+    UBYTE   	    *pattern;
+    UBYTE   	    *mask;
+    ULONG   	     maskmodulo;
+    WORD    	     patternheight;
+    WORD    	     patterndepth;
+    WORD    	     renderx1;
+    WORD    	     rendery1;
+    UBYTE   	     invertpattern;
+};
+	
 /*****************************************************************************
 
     NAME */
@@ -90,55 +77,35 @@ static ULONG bltpattern_render(APTR bpr_data, LONG srcx, LONG srcy,
 
     if (rp->AreaPtrn)
     {
-	struct pattern_info 	    	pi;
-	struct bltpattern_render_data 	bprd;
-	struct Rectangle    	    	rr;
-	OOP_Object  	    	    	*gc;
-	HIDDT_DrawMode      	    	old_drmd;
-	struct TagItem      	    	gc_tags[] =
-	{
-	    { aHidd_GC_DrawMode ,	vHidd_GC_DrawMode_Copy	},
-	    { TAG_DONE	    	    	    	    	    	}
-	};
+	struct bp_render_data 	bprd;
+	struct Rectangle 	rr;
 
-	EnterFunc(bug("driver_BltPattern(%d, %d, %d, %d, %d)\n"
-    	    , xMin, yMin, xMax, yMax, byteCnt));
-
-    	FIX_GFXCOORD(xMin);
+	FIX_GFXCOORD(xMin);
 	FIX_GFXCOORD(yMin);
-	FIX_GFXCOORD(xMax);
-	FIX_GFXCOORD(yMax);
-	
+
 	if (!OBTAIN_DRIVERDATA(rp, GfxBase))
-    	    ReturnVoid("driver_BltPattern");
+    	    return;
 
-	pi.mask	= mask;
-	pi.rp	= rp;
-	pi.gfxbase	= GfxBase;
-	pi.mask_bpr = byteCnt;
-	pi.dest_depth	= GetBitMapAttr(rp->BitMap, BMA_DEPTH);
-
-	pi.orig_xmin = xMin;
-	pi.orig_ymin = yMin; 
-
-	bprd.pi = &pi;
-
-	gc = GetDriverData(rp)->dd_GC;
-
-	OOP_GetAttr(gc, aHidd_GC_DrawMode, &old_drmd);
-	OOP_SetAttrs(gc, gc_tags);
-
+	bprd.pattern  	    = (UBYTE *)rp->AreaPtrn;
+	bprd.mask   	    = mask;
+	bprd.maskmodulo     = byteCnt;
+	bprd.patterndepth   = (rp->AreaPtSz >= 0) ? 1 : rp->BitMap->Depth;
+	bprd.patternheight  = 1L << ((rp->AreaPtSz >= 0) ? rp->AreaPtSz : -rp->AreaPtSz);
+	bprd.renderx1	    = xMin;
+	bprd.rendery1	    = yMin;	
+	bprd.invertpattern  = (rp->DrawMode & INVERSVID) ? TRUE : FALSE;
+    	bprd.pixlut.entries = bprd.patterndepth;
+	bprd.pixlut.pixels  = IS_HIDD_BM(rp->BitMap) ? HIDD_BM_PIXTAB(rp->BitMap) : NULL;
+	
 	rr.MinX = xMin;
 	rr.MinY = yMin;
 	rr.MaxX = xMax;
 	rr.MaxY = yMax;
 
-	do_render_func(rp, NULL, &rr, bltpattern_render, &bprd, TRUE, GfxBase);
+	do_render_func(rp, NULL, &rr, bltpattern_render, &bprd, FALSE, GfxBase);
 
-	gc_tags[0].ti_Data = old_drmd;
-	OOP_SetAttrs(gc, gc_tags);
-	
 	RELEASE_DRIVERDATA(rp, GfxBase);
+
 
     }
     else
@@ -166,139 +133,37 @@ static ULONG bltpattern_render(APTR bpr_data, LONG srcx, LONG srcy,
 
 /****************************************************************************************/
 
-#define GfxBase pi->gfxbase
-
-static VOID pattern_to_buf(struct pattern_info *pi, LONG x_src, LONG y_src,
-    	    	    	   LONG x_dest, LONG y_dest, ULONG xsize, ULONG ysize,
-			   ULONG *buf, OOP_Object *dest_bm, HIDDT_Pixel *coltab)
-{
-
-    /* x_src, y_src is the coordinates into the layer. */
-    LONG    	    y;
-    struct RastPort *rp;
-    ULONG   	    apen;
-    ULONG   	    bpen;
-    ULONG   	    drmd;
-    UBYTE   	    *apt;
-    LONG    	    mask_x = 0, mask_y = 0;
-    
-    rp   = pi->rp;
-    apen = GetAPen(rp);
-    bpen = GetBPen(rp);
-    drmd = GetDrMd(rp);
-    
-    apt = (UBYTE *)rp->AreaPtrn;
-    
-    if (pi->mask)
-    {
-    	mask_x = x_src; /* - pi->orig_xmin; */
-	mask_y = y_src; /* - pi->orig_ymin; */
-    }
-
-    x_src += pi->orig_xmin;
-    y_src += pi->orig_ymin;
-    
-    EnterFunc(bug("pattern_to_buf(%p, %d, %d, %d, %d, %d, %d, %p)\n"
-    			, pi, x_src, y_src, x_dest, y_dest, xsize, ysize, buf ));
-			
-
-    HIDD_BM_GetImage(dest_bm
-    	, (UBYTE *)buf
-	, xsize * sizeof (HIDDT_Pixel)
-	, x_dest, y_dest
-	, xsize, ysize
-	, vHidd_StdPixFmt_Native32
-    );
-
-    
-    for (y = 0; y < ysize; y ++)
-    {
-        LONG x;
-	
-	for (x = 0; x < xsize; x ++)
-	{
-	    ULONG set_pixel;
-	    ULONG pixval;
-	    
-	    /* Mask supplied ? */
-	    if (pi->mask)
-	    {
-		ULONG idx, mask;
-
-
-		idx = COORD_TO_BYTEIDX(x + mask_x, y + mask_y, pi->mask_bpr);
-		mask = XCOORD_TO_MASK(x + mask_x);
-		 
-		set_pixel = pi->mask[idx] & mask;
-		 
-	    }
-	    else set_pixel = 1UL;
-		
-		
-	    if (set_pixel)
-	    {
-		if (apt)
-		{
-		   set_pixel = pattern_pen(rp, x + x_src, y + y_src, apen, bpen, &pixval, GfxBase);
-		   if (set_pixel)
-		   {
-		   	D(bug(" s"));
-			if (drmd & COMPLEMENT)
-			{
-			    pixval = ~(*buf);
-			}
-			else
-			{
-			    pixval = (coltab != NULL) ? coltab[pixval] : pixval;
-			}
-		    	*buf = pixval;
-		   }
-		    
-		}
-		else *buf = apen;
-	    
-	    } /* if (pixel should be set */
-	    
-	    D(bug("(%d, %d): %d", x, y, *buf));
-	    buf ++;
-	    
-	} /* for (each column) */
-	
-    } /* for (each row) */
-      
-    ReturnVoid("pattern_to_buf");
-}
-
-#undef GfxBase
-
-/****************************************************************************************/
-
 static ULONG bltpattern_render(APTR bpr_data, LONG srcx, LONG srcy,
-    	    	    	       OOP_Object *dstbm_obj, OOP_Object *dst_gc,
-			       LONG x1, LONG y1, LONG x2, LONG y2,
-			       struct GfxBase *GfxBase)
+    	    	    	    	OOP_Object *dstbm_obj, OOP_Object *dst_gc,
+				LONG x1, LONG y1, LONG x2, LONG y2,
+				struct GfxBase *GfxBase)
 {
-    struct bltpattern_render_data *bprd;
-        
-    ULONG width, height;
-    
-    bprd = (struct bltpattern_render_data *)bpr_data;
+    struct bp_render_data  *bprd;
+    ULONG   	    	    width, height;
+    WORD    	    	    x;
+    UBYTE   	    	   *mask;
     
     width  = x2 - x1 + 1;
     height = y2 - y1 + 1;
+
+    bprd = (struct bp_render_data *)bpr_data;
+     
+    mask = bprd->mask + bprd->maskmodulo * srcy;
     
-    amiga2hidd_fast( (APTR)bprd->pi
-    	, dst_gc
-	, srcx /* bprd->rsi.layer_rel_srcx */
-	, srcy /* bprd->rsi.layer_rel_srcy */
-	, bprd->rsi.curbm
-	, x1, y1
-	, width, height
-	, pattern_to_buf
-	, GfxBase
-    );
-    
+    HIDD_BM_PutPattern(dstbm_obj, dst_gc, bprd->pattern,
+    	    	       srcx + bprd->renderx1,
+		       srcy + bprd->rendery1,
+		       bprd->patternheight, bprd->patterndepth,
+		       &bprd->pixlut,
+		       bprd->invertpattern,
+		       mask,
+		       bprd->maskmodulo,
+		       srcx,
+		       x1,
+		       y1,
+		       width,
+		       height);
+		       
+		    
     return width * height;
-    
 }
-/****************************************************************************************/
