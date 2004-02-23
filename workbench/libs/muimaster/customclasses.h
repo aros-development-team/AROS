0cfb769dc09ac442803beb854cff2952e1262a3f
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-02-23 19:49:08 +0000

    Added ZUNE_CUSTOMCLASS_10 macro.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@21059 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/customclasses.h
+++ workbench/libs/muimaster/customclasses.h
@@ -191,6 +191,30 @@ ADD2EXIT(name ## _Deinitialize, 100);                                 \
     __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m9, m9, m9_msg_type);  \
     __ZUNE_CUSTOMCLASS_END(name, base, parent_name, parent_class)  \
 
+#define ZUNE_CUSTOMCLASS_10(name, base, parent_name, parent_class,   \
+                           m1, m1_msg_type,                          \
+                           m2, m2_msg_type,                          \
+                           m3, m3_msg_type,                          \
+                           m4, m4_msg_type,                          \
+                           m5, m5_msg_type,                          \
+                           m6, m6_msg_type,                          \
+                           m7, m7_msg_type,                          \
+                           m8, m8_msg_type,                          \
+                           m9, m9_msg_type,                          \
+                           m10, m10_msg_type)                        \
+    __ZUNE_CUSTOMCLASS_START(name)                                   \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m1, m1, m1_msg_type);    \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m2, m2, m2_msg_type);    \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m3, m3, m3_msg_type);    \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m4, m4, m4_msg_type);    \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m5, m5, m5_msg_type);    \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m6, m6, m6_msg_type);    \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m7, m7, m7_msg_type);    \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m8, m8, m8_msg_type);    \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m9, m9, m9_msg_type);    \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m10, m10, m10_msg_type); \
+    __ZUNE_CUSTOMCLASS_END(name, base, parent_name, parent_class)    \
+
 /*************************************************************************/
 
 #define __ZUNE_CUSTOMCLASS_INLINEMETHOD(cname, mname, m_msg_type, m_code) \
