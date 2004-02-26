054aa481fc617255d15035401ea75af2c6639b5d
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-02-26 13:36:13 +0000

    Use normal startup code and not shellcommands stuff.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@21099 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/c/Partition/args.h
@@ -0,0 +1,27 @@
+#ifndef _ARGS_H_
+#define _ARGS_H_
+
+/*
+    Copyright © 2004, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <exec/types.h>
+
+/*** Structures *************************************************************/
+enum Argument
+{
+    FORCE, /* Don't ask the user for permission */
+    QUIET, /* Don't print anything */
+    COUNT  /* Number of arguments */
+};
+
+/*** Prototypes *************************************************************/
+BOOL ReadArguments(VOID);
+VOID FreeArguments(VOID);
+IPTR GetArgument(enum Argument arg);
+
+/*** Macros *****************************************************************/
+#define ARG(a) GetArgument((a))
+
+#endif /* _ARGS_H_ */
