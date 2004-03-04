d8fe851d5276c4cf0959eeb4cec800bd77a713fc
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-03-04 02:06:58 +0000

    Fixed a bunch of errors due to the fact that the latest version of gcc doesn't cope anymore with lvalue casts. Cleaned up some code along the way...
    git-svn-id: https://svn.aros.org/svn/aros/trunk@21152 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-all/jmpdefs.h
+++ arch/i386-all/jmpdefs.h
@@ -10,29 +10,29 @@
 
 #ifdef __linux__
 #   if defined(__GLIBC__) && (__GLIBC__ >= 2)
-#      define SP(env)	((APTR)(env[0].__jmpbuf[JB_SP]))
-#      define FP(env)	((APTR)(env[0].__jmpbuf[JB_BP]))
-#      define PC(env)	((APTR)(env[0].__jmpbuf[JB_PC]))
+#      define SP(env)	(*(APTR*)&(env[0].__jmpbuf[JB_SP]))
+#      define FP(env)	(*(APTR*)&(env[0].__jmpbuf[JB_BP]))
+#      define PC(env)	(*(APTR*)&(env[0].__jmpbuf[JB_PC]))
 #   else
-#      define SP(env)	((APTR)(env[0].__sp))
-#      define FP(env)	((APTR)(env[0].__bp))
-#      define PC(env)	((APTR)(env[0].__pc))
+#      define SP(env)	(*(APTR*)&(env[0].__sp))
+#      define FP(env)	(*(APTR*)&(env[0].__bp))
+#      define PC(env)	(*(APTR*)&(env[0].__pc))
 #   endif
 #endif
 #ifdef __FreeBSD__
-#   define SP(env)	((APTR)(env[0]._jb[2]))
-#   define FP(env)	((APTR)(env[0]._jb[3]))
-#   define PC(env)	((APTR)(env[0]._jb[0]))
+#   define SP(env)	(*(APTR)&(env[0]._jb[2]))
+#   define FP(env)	(*(APTR)&(env[0]._jb[3]))
+#   define PC(env)	(*(APTR)&(env[0]._jb[0]))
 #endif
 #ifdef __NetBSD__
-#   define SP(env)      ((APTR)(env[2]))
-#   define FP(env)      ((APTR)(env[3]))
-#   define PC(env)      ((APTR)(env[0]))
+#   define SP(env)      (*(APTR)&(env[2]))
+#   define FP(env)      (*(APTR)&(env[3]))
+#   define PC(env)      (*(APTR)&(env[0]))
 #endif
 #ifdef __OpenBSD__
-#   define SP(env)      ((APTR)(env[2]))
-#   define FP(env)      ((APTR)(env[3]))
-#   define PC(env)      ((APTR)(env[0]))
+#   define SP(env)      (*(APTR*)&(env[2]))
+#   define FP(env)      (*(APTR*)&(env[3]))
+#   define PC(env)      (*(APTR*)&(env[0]))
 #endif
 /* The number of stack longs that have to be copied from the old stack */
 #define NUM_LONGS	4
