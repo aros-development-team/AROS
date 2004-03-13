4260a6cd78c6ba938da21fdbdc85479793370387
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-03-13 23:28:09 +0000

    Fixed image path.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@21220 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/Zune/disable.c
+++ test/Zune/disable.c
@@ -29,7 +29,7 @@ int main(void)
 	    MUIA_Window_Activate,        TRUE,
             
     	    WindowContents, (IPTR) VGroup,
-                Child, (IPTR) (b1 = ImageButton("Test", "THEME:Images/Gadgets/Preferences/Test.png")),
+                Child, (IPTR) (b1 = ImageButton("Test", "THEME:Images/Gadgets/Prefs/Test")),
                 Child, (IPTR) HGroup,
                     Child, (IPTR) (b2 = SimpleButton("Disable")),
                     Child, (IPTR) (b3 = SimpleButton("Enable")),
