3e1a1d284a2f2d2c71214c8c2daa87808cca451c
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-03-19 18:59:57 +0000

    Solved a couple of bugs, pointed out by Georg on the ML. You now don't need anymore to define your own variables and manually send a signal to the detacher process, in order to control the detaching procedure, all you have to do is call the Detach() function declared in <aros/detach.h>. Not calling this function will result in your program being detached, if nothing else fails, before the main() function is reached.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@21306 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/startup/detach.c
+++ compiler/startup/detach.c
@@ -9,54 +9,24 @@
          detach.o as your program's start file, before any other usual start
          file.
 
-	 In your program's source code you have the option to use some
-	 variables to determine the behaviour of the detaching procedure.
-
-	 These variables are defined as "weak", so that defining them both
-	 in this module and in your program doesn't produce linking errors.
-	 Defining these variables in your program's source code makes it
-	 possible to make your program stay independent from this module,
-	 so that you can decide whether your program should be detacheable
-	 or not just by adding a flag to the linker's command line and make
-	 the program still work in case you decided to not make it
-	 detacheable.
-
-         These are the variables:
-
-             1) LONG __detacher_must_wait_for_signal;
-
-		Define the above variable only if you need the detacher
-		process to wait for a signal to arrive, before exiting and
-		effectively detaching your program.
-
-		It must contain a bitmask suited for the use of
-		exec.library/Wait()
-
-	     2) struct Process *__detacher_process;
-
-		The above variable will hold a pointer to the detacher
-		process. Use it for anything you wish, but specially for
-		sending to it the "termination" signal.
-
-		This pointer is guaranteed to hold NULL when there's no
-		detacher process around anymore.
-
-                Remember to initialize it to NULL!
-
-             3) STRPTR __detached_name;
-
-		Define the above variable only if you want the detached
-		process to have a name different than the detacher's one.
-		Set its value to a pointer to a string holding the name you
-		want the detached process to have.
-		
-	     4) LONG __detached_return_value;
+	 If you want the detached process to have a name different than the
+	 detacher's one, define the following variable in your program's
+	 source code:
+	 
+             STRPTR __detached_name = "My Preferred Program Name";
 	     
-	        Define the above variable if you want the detacher
-		process to exit with the value you set that variable 
-                to. Useful when the detached process failed to
-		initialize before detaching and wants this fact to
-		be reported to the user.
+	You can also decide to control exactly when to detach your program
+	from the shell. To do so, simply use the function
+	
+	    Detach();
+	    
+	declared in
+	
+	    <aros/detach.h>
+	    
+	when you want to detach the program. Not using this function will
+	result in your program being detached from the shell before the main()
+	function is reached.
 */
 
 #include <aros/config.h>
@@ -76,12 +46,17 @@ asm
 );
 #endif
 
-extern LONG            __detacher_must_wait_for_signal __attribute__((weak));
-extern struct Process *__detacher_process              __attribute__((weak));
-extern STRPTR          __detached_name                 __attribute__((weak));
-extern LONG            __detached_return_value         __attribute__((weak));
+int             __detached_manages_detach;
+STRPTR          __detached_name;
+LONG            __detached_return_value;
+struct Process *__detacher_process;
 
 DECLARESET(PROGRAM_ENTRIES);
+
+AROS_UFP3(static LONG, __detach_trampoline,
+AROS_UFHA(char *,argstr,A0),
+AROS_UFHA(ULONG,argsize,D0),
+AROS_UFHA(struct ExecBase *,SysBase,A6));
 
 AROS_UFH3(static LONG, __detach_entry,
 AROS_UFHA(char *,argstr,A0),
@@ -94,6 +69,7 @@ AROS_UFHA(struct ExecBase *,SysBase,A6))
     struct CommandLineInterface *cli;
     struct Process              *newproc;
     BPTR                         mysegment = NULL;
+    STRPTR                       detached_name;
 
     DOSBase = (struct DosLibrary *)OpenLibrary(DOSNAME, 39);
     if (!DOSBase) return RETURN_FAIL;
@@ -107,18 +83,17 @@ AROS_UFHA(struct ExecBase *,SysBase,A6))
     mysegment = cli->cli_Module;
     cli->cli_Module = NULL;
 
-    if (!__detached_name)
-      __detached_name = FindTask(NULL)->tc_Node.ln_Name;
-
+    detached_name = __detached_name ? __detached_name : (STRPTR)FindTask(NULL)->tc_Node.ln_Name;
+    
     {
         struct TagItem tags[] =
         {
-	    { NP_Seglist,   (IPTR)mysegment                                   },
-	    { NP_Entry,     (IPTR)SETELEM(__detach_entry, program_entries)[1] },
-	    { NP_Name,      (IPTR)__detached_name                             },
-	    { NP_Arguments, (IPTR)argstr                                      },
-	    { NP_Cli,       TRUE                                              },
-            { TAG_DONE,     0                                                 }
+	    { NP_Seglist,   (IPTR)mysegment            },
+	    { NP_Entry,     (IPTR)&__detach_trampoline },
+	    { NP_Name,      (IPTR)detached_name        },
+	    { NP_Arguments, (IPTR)argstr               },
+	    { NP_Cli,       TRUE                       },
+            { TAG_DONE,     0                          }
         };
 
 	__detacher_process = (struct Process *)FindTask(NULL);
@@ -128,26 +103,78 @@ AROS_UFHA(struct ExecBase *,SysBase,A6))
     }
 
     CloseLibrary((struct Library *)DOSBase);
-
-    if (newproc && __detacher_must_wait_for_signal)
-        Wait(__detacher_must_wait_for_signal);
-
-    __detacher_process = NULL;
-
-    if (!newproc || __detached_return_value)
+    
+    if (!newproc)
     {
-        PutStr(__detached_name); PutStr(": Failed to detach.\n");
+        cli->cli_Module = mysegment;
+	__detached_return_value = RETURN_ERROR;
+    }
+    else
+        Wait(SIGF_SINGLE);
+
+    if (__detached_return_value != RETURN_OK)
+    {
+        PutStr(FindTask(NULL)->tc_Node.ln_Name); PutStr(": Failed to detach.\n");
     }
     
-    return newproc ? __detached_return_value : RETURN_FAIL;
+    if (newproc)
+    {
+        Forbid();
+	Signal(&newproc->pr_Task, SIGF_SINGLE);
+    }
+    
+    return __detached_return_value;
     
     AROS_USERFUNC_EXIT
 }
 
-LONG            __detacher_must_wait_for_signal __attribute__((weak)) = 0;
-struct Process *__detacher_process              __attribute__((weak)) = NULL;
-STRPTR          __detached_name                 __attribute__((weak)) = NULL;
-LONG            __detached_return_value         __attribute__((weak)) = RETURN_OK;
-
 DEFINESET(PROGRAM_ENTRIES);
 ADD2SET(__detach_entry, program_entries, 0);
+
+void __Detach(LONG retval);
+
+AROS_UFH3(static LONG, __detach_trampoline,
+AROS_UFHA(char *,argstr,A0),
+AROS_UFHA(ULONG,argsize,D0),
+AROS_UFHA(struct ExecBase *,SysBase,A6))
+{
+    AROS_USERFUNC_INIT
+    
+    LONG retval;
+      
+    /* The program has two options: either take care of telling the detacher
+       process when exactly to go away, via the Detach() function, or let this
+       startup code take care of it. If __detached_manages_detach is TRUE, then
+       the detached process handles it, otherwise we handle it.  */
+    
+    if (!__detached_manages_detach)
+       __Detach(RETURN_OK);
+    
+    retval = AROS_UFC3(LONG, SETELEM(__detach_entry, program_entries)[1],
+             AROS_UFHA(char *,argstr,A0),
+             AROS_UFHA(ULONG,argsize,D0),
+             AROS_UFHA(struct ExecBase *,SysBase,A6));
+    
+    /* At this point the detacher process might still be around, 
+      If the program forgot to detach, or if it couldn't, but in any
+      case we need to tell the detacher to go away.  */
+    __Detach(retval);
+    
+    return 0;	  
+    
+    AROS_USERFUNC_EXIT
+}
+
+void __Detach(LONG retval)
+{
+    if (__detacher_process != NULL)
+    {
+        __detached_return_value = retval;
+	/* Tell the detacher process it can now go away */
+        Signal(&__detacher_process->pr_Task, SIGF_SINGLE);
+	
+	/* Wait for it to say "goodbye" */
+	Wait(SIGF_SINGLE);
+	__detacher_process = NULL;
+    }
+}
