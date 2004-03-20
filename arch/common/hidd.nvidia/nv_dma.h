d5c08bfc67ce53b56c96011695ae1b019eaa4c79
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-03-20 12:53:41 +0000

    fixes.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@21317 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/common/hidd.nvidia/nv_dma.h
+++ arch/common/hidd.nvidia/nv_dma.h
@@ -148,6 +148,10 @@
 #define STRETCH_BLIT_FORMAT_A8R8G8B8				    0x00000003
 #define STRETCH_BLIT_FORMAT_YUYV                                    0x00000005
 #define STRETCH_BLIT_FORMAT_UYVY                                    0x00000006
+#define STRETCH_BLIT_OPERATION					    0x0000E304
+#define STRETCH_BLIT_OPERATION_ROP				    0x00000001
+#define STRETCH_BLIT_OPERATION_COPY				    0x00000003
+#define STRETCH_BLIT_OPERATION_BLEND				    0x00000005
 #define STRETCH_BLIT_CLIP_POINT                                     0x0000E308
 #define STRETCH_BLIT_CLIP_POINT_X                                   15:0 
 #define STRETCH_BLIT_CLIP_POINT_Y                                   31:16
