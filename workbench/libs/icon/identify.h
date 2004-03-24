4e696437f00b5abcbd6e039b76d95790fb261ce6
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-03-24 23:35:29 +0000

    Added function FindType() which uses exactly the same logic as FindDefaultIcon() but returns the type (WBTOOL, WBPROJECT, etc) instead of the icon.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@21370 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/icon/identify.h
+++ workbench/libs/icon/identify.h
@@ -10,9 +10,11 @@
 #include <workbench/icon.h>
 
 /*** Prototypes *************************************************************/
+LONG               __FindType_WB(struct IconIdentifyMsg *iim, struct Library *IconBase);
 struct DiskObject *__FindDefaultIcon_WB(struct IconIdentifyMsg *iim, struct Library *IconBase);
 
 /*** Macros *****************************************************************/
+#define FindType(iim) (__FindType_WB((iim), IconBase))
 #define FindDefaultIcon(iim) (__FindDefaultIcon_WB((iim), IconBase))
 
 #endif /* _IDENTIFY_H_ */
