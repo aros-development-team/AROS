53253a1eb9d670e6b7cb629a2f9d6decdfd1e82e
weissms <weissms@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-04-05 23:21:46 +0000

    working in linux hosted
    git-svn-id: https://svn.aros.org/svn/aros/trunk@21478 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/.unmaintained/ppc-all/exec/execstubs.s
+++ arch/.unmaintained/ppc-all/exec/execstubs.s
@@ -1,17 +1,17 @@
 /*
     Copyright ? 1995-2003, The AROS Development Team. All rights reserved.
     $Id$
-                                                                                
+
     Desc: Stubs to call C functions while preserving all registers
     Lang: english
 */
    #include "include/machine.i"
-                                                                                
+
 /*
     Macros:
    PUSH - Save all registers on the stack
    POP - Restore all registers from the stack
-                                                                                
+
    STUB_ARG0(name) - Call a function preserving all registers
           which gets no arguments
    STUB_ARG1(name) - Call a function preserving all registers
@@ -20,89 +20,84 @@
           which gets two arguments
 */
 
-#define NUM_REGS   (32+2)
-#define OFFSET     (NUM_REGS*4)
-#define FIRST_ARG  ((NUM_REGS+0)*4)
-#define SECOND_ARG ((NUM_REGS+1)*4)
+#define NUM_REGS   (32 + 2)
+#define OFFSET     ((NUM_REGS) * 4)
+#define FIRST_ARG  (((NUM_REGS + 0) * 4) + OFFSET)
+#define SECOND_ARG (((NUM_REGS + 1) * 4) + OFFSET)
 
-#define PUSH \
-	stmw 0,-OFFSET(1);  \
-	li   1,-OFFSET;     \
-	mflr 0;             \
-	stw  0,OFFSET-8(1); \
-	mfcr 0;             \
-	stw  0,OFFSET-4(1)
+/* 4(1) is used by the called function
+   to store lr much the same way we do
+*/
+#define PUSH           \
+    stwu 1,-OFFSET(1) ;\
+    stw  0,8(1)       ;\
+    stmw 2,12(1)      ;\
+    mflr 0            ;\
+    stw  0,OFFSET-4(1);
 
-#define POP  \
-	lwz  0,OFFSET-4(1); \
-	mtcr 0;             \
-	lwz  0,OFFSET-8(1); \
-	mtlr 0;             \
-	lmw  2,8(1);        \
-	lwz  0,0(1);        \
-	lwz  1,4(1)
+#define POP             \
+    lwz  0,OFFSET-4(1); \
+    mtlr 0            ; \
+    lmw  2,12(1)      ; \
+    lwz  0,8(1)       ; \
+    lwz  1,0(1);
 
-#define STUB_ARG0(name) \
-	PUSH;             \
-	lis  9,name@ha;   \
-	la   0,name@l(9); \
-	mtlr 0;           \
-	blrl;             \
-	POP;              \
-	blrl
+#define STUB_ARG0(name)\
+    PUSH            ;  \
+    lis  9,name@ha  ;  \
+    la   0,name@l(9);  \
+    mtlr 0          ;  \
+    blrl            ;  \
+    POP             ;  \
+    blrl
 
-#define STUB_ARG1(name) \
-	PUSH;                  \
-	lwz  0,FIRST_ARG(1); \
-	stw  0,-4(1);        \
-	li   1,-4;         \
-	lis  9,name@ha;    \
-	la   0,name@l(9);  \
-	mtlr 0;            \
-	blrl;              \
-	li   1,4;          \
-	POP;               \
-	blrl
+#define STUB_ARG1(name)  \
+    PUSH               ; \
+    lwz  0,FIRST_ARG(1); \
+    stw  0,-4(1)       ; \
+    lis  9,name@ha     ; \
+    la   0,name@l(9)   ; \
+    mtlr 0             ; \
+    blrl               ; \
+    POP                ; \
+    blrl
 
-#define STUB_ARG2(name) \
-	PUSH;                   \
-	lwz  0,SECOND_ARG(1); \
-	stw  0,-4(1);         \
-	lwz  0,FIRST_ARG(1);  \
-	stw  0,-8(1);         \
-	li   1,-8;          \
-	lis  9,name@ha;     \
-	la   0,name@l(9);   \
-	mtlr 0;             \
-	blrl;               \
-	li   0,8;           \
-	POP;                \
-	blrl
+#define STUB_ARG2(name)   \
+    PUSH                ; \
+    lwz  0,SECOND_ARG(1); \
+    stw  0,-4(1)        ; \
+    lwz  0,FIRST_ARG(1) ; \
+    stw  0,-8(1)        ; \
+    lis  9,name@ha      ; \
+    la   0,name@l(9)    ; \
+    mtlr 0              ; \
+    blrl                ; \
+    POP                 ; \
+    blrl
 
 /* To save typing work */
-#define STUB0(cname,name)             \
-   .globl   cname        ; \
-   _FUNCTION(cname)      ; \
-cname:                ; \
+#define STUB0(cname,name)\
+   .globl   cname      ; \
+   _FUNCTION(cname)    ; \
+cname:                 ; \
    STUB_ARG0(name)
-                                                                                
-#define STUB1(cname,name)             \
-   .globl   cname        ; \
-   _FUNCTION(cname)      ; \
-cname:                ; \
+
+#define STUB1(cname,name)\
+   .globl   cname      ; \
+   _FUNCTION(cname)    ; \
+cname:                 ; \
    STUB_ARG1(name)
-                                                                                
-                                                                                
-#define STUB2(cname,name) \
-   .globl   cname       ; \
-   _FUNCTION(cname)     ; \
-cname:                  ; \
+
+#define STUB2(cname,name)\
+   .globl   cname      ; \
+   _FUNCTION(cname)    ; \
+cname:                 ; \
    STUB_ARG2(name)
-                                                                                
+
    .text
    _ALIGNMENT
 
-   /* Call functions and preserve registers */
+/* Call functions and preserve registers */
 #ifdef  UseExecstubs
    STUB1(AROS_SLIB_ENTRY(Forbid,Exec),AROS_CSYMNAME(_Exec_Forbid))
    STUB1(AROS_SLIB_ENTRY(Permit,Exec),AROS_CSYMNAME(_Exec_Permit))
