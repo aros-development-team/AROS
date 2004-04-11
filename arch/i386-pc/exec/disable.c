676ab7e8802f3ed35c9f4a8a6259304cb7efd93e
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-04-11 19:44:09 +0000

    Use preserve-all-registers stubs also for Disable() and Enable(). Sync execstubs.S with general one in arch/i386-all (save/restore only scratch registers as fixed by Fabio), although it should probably be killed anyway (now) ...
    git-svn-id: https://svn.aros.org/svn/aros/trunk@21505 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-pc/exec/disable.c
+++ arch/i386-pc/exec/disable.c
@@ -12,9 +12,15 @@
 #include <aros/atomic.h>
 #include <proto/exec.h>
 
+#undef  Exec
+#ifdef UseExecstubs
+#    define Exec _Exec
+#endif
+
 AROS_LH0(void, Disable,
     struct ExecBase *, SysBase, 20, Exec)
 {
+#undef Exec
     AROS_LIBFUNC_INIT
 
     /* Georg Steger */
