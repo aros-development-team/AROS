5d87ae84b4475252d3baa17ce64939394249eed5
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-04-21 16:02:02 +0000

    License change. About program is now under the GPL.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@21523 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/system/AboutAROS/logotype.h
+++ workbench/system/AboutAROS/logotype.h
@@ -2,7 +2,10 @@
 #define _LOGOTYPE_H_
 
 /*
-    Copyright © 2003, The AROS Development Team. All rights reserved.
+    Copyright © 2003-2004, The AROS Development Team. All rights reserved.
+    This file is part of the About program, which is distributed under
+    the terms of version 2 of the GNU General Public License.
+    
     $Id$
 */
 
