65cb73e62017bdcd2be81a8c2297ce11a964e78b
hkiel <hkiel@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-04-22 20:55:14 +0000

    Implemented dummy for getgroups()
    git-svn-id: https://svn.aros.org/svn/aros/trunk@21536 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/getgroups.c
@@ -0,0 +1,13 @@
+/*
+    Copyright © 2004, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <unistd.h>
+
+int getgroups(int gidsetlen, gid_t *gidset)
+{
+    #warning Implement getgroups()
+    
+    return 0;
+}
