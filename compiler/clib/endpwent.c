5289366b78d778e879736665cadc7dc2bc6e30f7
hkiel <hkiel@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-04-22 08:14:09 +0000

    Dummy implemented getpwent(), setpwent() and endpwent()
    git-svn-id: https://svn.aros.org/svn/aros/trunk@21531 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/endpwent.c
@@ -0,0 +1,13 @@
+/*
+    Copyright © 2004, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <pwd.h>
+
+void endpwent()
+{
+    #warning Implement endpwent()
+    
+    return;
+}
---
+++ compiler/clib/getpwent.c
@@ -0,0 +1,13 @@
+/*
+    Copyright © 2004, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <pwd.h>
+
+struct passwd *getpwent()
+{
+    #warning Implement getpwent()
+    
+    return NULL;
+}
