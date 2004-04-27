fa1675b71d3f193f8030f37869cc0ebdac6a1852
hkiel <hkiel@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-04-27 16:11:26 +0000

    Dummy for fchown()
    git-svn-id: https://svn.aros.org/svn/aros/trunk@21581 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/fchown.c
@@ -0,0 +1,13 @@
+/*
+    Copyright © 2004, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <unistd.h>
+
+int fchown(int fd, uid_t owner, gid_t group)
+{
+    #warning implement fchown
+    
+    return 0;
+}
