41fcba3f543cf7df235937f0e8c125f759566376
bernie <bernie@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-05-09 22:07:05 +0000

    #define _GNU_SOURCE to enable strndup() on Linux.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@21616 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- tools/MetaMake/mem.c
+++ tools/MetaMake/mem.c
@@ -18,6 +18,7 @@ along with GNU CC; see the file COPYING.  If not, write to
 the Free Software Foundation, 59 Temple Place - Suite 330,
 Boston, MA 02111-1307, USA.  */
 
+#define _GNU_SOURCE /* for string.h:strndup() */
 #include "config.h"
 
 #include <stdio.h>
