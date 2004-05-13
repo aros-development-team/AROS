b208ebf0f37d5af52d4e85c7f149a7911a665ca3
hkiel <hkiel@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-05-13 17:36:24 +0000

    Dummy implemented setuid() and setgid(). They always return 0 (=success) though they do nothing.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@21643 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/setuid.c
@@ -0,0 +1,40 @@
+/*
+    Copyright © 2004, The AROS Development Team. All rights reserved.
+    $Id$
+
+    POSIX function setuid().
+*/
+
+#include <sys/types.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <unistd.h>
+
+	int setuid(
+
+/*  SYNOPSIS */
+	uid_t uid)
+
+/*  FUNCTION
+	
+    INPUTS
+	
+    RESULT
+	
+    NOTES
+    	
+    EXAMPLE
+
+    BUGS
+    	
+    SEE ALSO
+        
+    INTERNALS
+
+******************************************************************************/
+{
+#warning Implement setuid() properly
+    return 0;
+} /* setuid() */
