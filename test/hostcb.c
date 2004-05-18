27673af0fbad2e7780579633384173d179272dc9
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-05-18 20:18:11 +0000

    More or less complete now (and should probably be moved into some other directory, but I don't know which). It's now possible to write host (X11) clipboard contents to stdout, into aros clipboard or into a file. And to put something into host clipboard, by taking it from a string (passed as argument to this tool), from the aros clipboard, or from a file.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@21649 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/hostcb.c
+++ test/hostcb.c
@@ -1,12 +1,17 @@
 /*
     Copyright © 1995-2004, The AROS Development Team. All rights reserved.
     $Id$
+
+    Tool to access host clipboard, when using a hosted version of AROS.
 */
 
 
 #include <dos/dos.h>
+#include <libraries/iffparse.h>
+#include <datatypes/textclass.h>
 #include <proto/exec.h>
 #include <proto/dos.h>
+#include <proto/iffparse.h>
 #include <aros/debug.h>
 #include <stdio.h>
 #include <string.h>
@@ -14,14 +19,19 @@
 
 /****************************************************************************************/
 
-#define ARG_TEMPLATE 	    "TOSTDOUT/S,TOAROSCLIP/S,TOFILE/K,FROMAROSCLIP/S,FROMSTRING/K,FROMFILE/K"
+#define ARG_TEMPLATE 	    "TOSTDOUT=OUT/S,TOAROSCLIP=TOAROS/S,TOFILE=TO/K," 	\
+    	    	    	    "FROMAROSCLIP=FROMAROS/S,FROMSTRING=STRING/K,"  	\
+			    "FROMFILE=FROM/K,AROSCLIPUNIT=AROSUNIT/N/K,QUIET/S"
+			    
 #define ARG_TOSTDOUT	    0
 #define ARG_TOAROSCLIP      1
 #define ARG_TOFILE  	    2
 #define ARG_FROMAROSCLIP    3
 #define ARG_FROMSTRING	    4
 #define ARG_FROMFILE	    5
-#define NUM_ARGS    	    6
+#define ARG_AROSCLIPUNIT    6
+#define ARG_QUIET   	    7
+#define NUM_ARGS    	    8
 
 #define CMD(msg)    	((msg)->mn_Node.ln_Pri)
 #define PARAM(msg)	((msg)->mn_Node.ln_Name)
@@ -30,10 +40,12 @@
 
 /****************************************************************************************/
 
+struct Library  *IFFParseBase;
 struct MsgPort  *replyport;
 struct Message   msg;
 struct RDArgs 	*myargs;
 BPTR	         fh;
+ULONG	    	 arosclipunit;
 STRPTR	    	 filebuffer, hostbuffer;
 IPTR	         args[NUM_ARGS];
 UBYTE	         s[256];
@@ -42,12 +54,17 @@ UBYTE	         s[256];
 
 static void cleanup(char *msg, ULONG retcode)
 {
-    if (msg) fprintf(stderr, "hostcb: %s\n", msg);
+    if (msg && !args[ARG_QUIET]) 
+    {
+    	fprintf(stderr, "hostcb: %s\n", msg);
+    }
     
     if (fh) Close(fh);
     if (hostbuffer) FreeVec(hostbuffer);
     if (filebuffer) FreeVec(filebuffer);
     if (myargs) FreeArgs(myargs);
+    
+    if (IFFParseBase) CloseLibrary(IFFParseBase);
     if (replyport) DeleteMsgPort(replyport);
     
     exit(retcode);
@@ -69,12 +86,14 @@ static void cmdfailed(void)
 
 /****************************************************************************************/
 
-static void initport(void)
+static void init(void)
 {
     replyport = CreateMsgPort();
     if (!replyport) cleanup("Out of memory", RETURN_ERROR);
 
     msg.mn_ReplyPort = replyport;
+    
+    IFFParseBase = OpenLibrary("iffparse.library", 36);
 }
 
 /****************************************************************************************/
@@ -86,6 +105,8 @@ static void getarguments(void)
     	Fault(IoErr(), 0, s, 255);
 	cleanup(s, RETURN_FAIL);
     }
+    
+    if (args[ARG_AROSCLIPUNIT]) arosclipunit = *(IPTR *)args[ARG_AROSCLIPUNIT];    
 }
 
 /****************************************************************************************/
@@ -106,6 +127,198 @@ static BOOL sendclipboardmsg(struct Message *msg, char command, void *param)
     Permit();
     
     return port ? TRUE : FALSE;    
+}
+
+/****************************************************************************************/
+
+static void toarosclip(void)
+{
+    struct IFFHandle *iff;
+    BOOL    	      sent;
+
+    if (!IFFParseBase) cleanup("Failed to open iffparse.library!", RETURN_FAIL);
+    
+    sent = sendclipboardmsg(&msg, 'R', NULL);    
+    if (sent)
+    {
+    	WaitPort(replyport);
+    	GetMsg(replyport);
+
+	if (SUCCESS(&msg) && RETVAL(&msg))
+	{
+    	    BOOL ok = FALSE;
+
+	    iff = AllocIFF();
+	    if (iff)
+	    {
+    		if ((iff->iff_Stream = (IPTR)OpenClipboard(arosclipunit)))
+		{
+		    InitIFFasClip(iff);
+		    
+		    if (!OpenIFF(iff, IFFF_WRITE))
+		    {
+	    		if (!PushChunk(iff, ID_FTXT, ID_FORM, IFFSIZE_UNKNOWN))
+			{
+			    if (!PushChunk(iff, 0, ID_CHRS, IFFSIZE_UNKNOWN))
+			    {
+			    	ULONG len = strlen(RETVAL(&msg));
+				
+				if (WriteChunkBytes(iff, RETVAL(&msg), len) == len)
+				{
+				    ok = TRUE;
+				}
+				PopChunk(iff);
+				
+			    } /* if (!PushChunk(iff, 0, ID_CHRS, IFFSIZE_UNKNOWN)) */
+			    
+			    PopChunk(iff);
+			    
+			} /* if (!PushChunk(iff, ID_FTXT, ID_FORM, IFFSIZE_UNKNOWN)) */
+			
+			CloseIFF(iff);
+			
+		    } /* if (!OpenIFF(iff, IFFF_WRITE)) */
+		    
+		    CloseClipboard((struct ClipboardHandle*)iff->iff_Stream);
+		    
+		} /* if ((iff->iff_Stream = (IPTR)OpenClipboard(arosclipunit))) */
+		
+		FreeIFF(iff);
+		
+	    } /* if (iff) */
+
+	    FreeVec(RETVAL(&msg));
+
+	    if (!ok) cleanup("Error writing to AROS clipboard!", RETURN_ERROR);
+	    
+	} /* if (SUCCESS(&msg) && RETVAL(&msg)) */
+	else
+	{
+	    cmdfailed();
+	}	    
+	
+    } /* if (sent) */
+    else
+    {
+    	noport(); 
+    }
+    
+}
+
+/****************************************************************************************/
+
+static void fromarosclip(void)
+{
+    struct IFFHandle *iff;
+    BOOL    	      sent, ok = FALSE;
+
+    if (!IFFParseBase) cleanup("Failed to open iffparse.library!", RETURN_FAIL);
+    
+    iff = AllocIFF();
+    if (iff)
+    {
+    	if ((iff->iff_Stream = (IPTR)OpenClipboard(arosclipunit)))
+	{
+	    InitIFFasClip(iff);
+
+	    if (!OpenIFF(iff, IFFF_READ))
+	    {
+	    	if (!StopChunk(iff, ID_FTXT, ID_CHRS))
+		{
+		    ULONG filebuffer_size = 0;
+		    
+		    for(;;)
+		    {
+		    	struct ContextNode *cn;
+		    	LONG 	    	    error;
+			
+		    	error = ParseIFF(iff, IFFPARSE_SCAN);
+		    	if ((error != 0) && (error != IFFERR_EOC)) break;
+			
+			cn = CurrentChunk(iff);
+			if (!cn)
+			{
+			    kprintf(" [ZERO CONTEXTNODE]\n\n");
+			    continue;
+			}
+			
+			if ((cn->cn_Type == ID_FTXT) && (cn->cn_ID == ID_CHRS))
+			{
+			    if (!filebuffer)
+			    {
+			    	filebuffer = AllocVec(cn->cn_Size + 1, MEMF_ANY);
+				if (!filebuffer) break;
+				
+				ok = TRUE;
+			    }
+			    else
+			    {
+			    	STRPTR new_filebuffer;
+				
+				new_filebuffer = AllocVec(filebuffer_size + cn->cn_Size + 1, MEMF_ANY);
+				if (!new_filebuffer)
+				{
+				    ok = FALSE;
+				    break;
+				}
+				
+				CopyMem(filebuffer, new_filebuffer, filebuffer_size);
+				FreeVec(filebuffer);
+				filebuffer = new_filebuffer;
+			    }
+			    
+			    if (ReadChunkBytes(iff, filebuffer + filebuffer_size, cn->cn_Size) != cn->cn_Size)
+			    {
+			    	ok = FALSE;
+				break;
+			    }
+			    
+			    filebuffer_size += cn->cn_Size;
+			    filebuffer[filebuffer_size] = '\0';
+			    
+			} /* if ((cn->cn_Type == ID_FTXT) && (cn->cn_ID == ID_CHRS)) */
+
+		    } /* for(;;) */
+		    
+		} /* if (!StopChunk(iff, ID_FTXT, ID_CHRS)) */
+					  
+		CloseIFF(iff);
+		
+	    } /* if (!OpenIFF(iff, IFFF_READ)) */
+	    
+	    CloseClipboard((struct ClipboardHandle*)iff->iff_Stream);
+	    
+	} /* if ((iff->iff_Stream = (IPTR)OpenClipboard(arosclipunit))) */
+	FreeIFF(iff);
+	
+    } /* if (iff) */
+    
+    if (!ok)
+    {
+	cleanup("Error reading from AROS clipboard!", RETURN_ERROR);
+    }
+    
+    sent = sendclipboardmsg(&msg, 'W', filebuffer);    
+    if (sent)
+    {
+    	WaitPort(replyport);
+    	GetMsg(replyport);
+
+	if (SUCCESS(&msg))
+	{
+    	    FreeVec(filebuffer),
+    	    filebuffer = 0;
+	}
+	else
+	{
+	    cmdfailed();
+	}	    
+    }
+    else
+    {
+    	noport(); 
+    }
+        
 }
 
 /****************************************************************************************/
@@ -276,8 +489,10 @@ static void fromfile(char *filename)
 
 int main(void)
 {
-    initport();
+    init();
     getarguments();
+    if (args[ARG_TOAROSCLIP]) toarosclip();
+    if (args[ARG_FROMAROSCLIP]) fromarosclip();
     if (args[ARG_TOSTDOUT]) tostdout();
     if (args[ARG_TOFILE]) tofile((char *)args[ARG_TOFILE]);
     if (args[ARG_FROMSTRING]) fromstring((char *)args[ARG_FROMSTRING]);
