08ebf3fd7b0097557d93e3aaa8d21e6c0834a03a
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-06-23 15:51:36 +0000

    + Locale_Initialize() cannot fail, so make it return VOID. + Fixed warnings.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@21806 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/utilities/Installer/locale.h
+++ workbench/utilities/Installer/locale.h
@@ -2,7 +2,7 @@
 #define _LOCALE_H
 
 /*
-    Copyright © 2003, The AROS Development Team. All rights reserved.
+    Copyright © 2003-2004, The AROS Development Team. All rights reserved.
     $Id$
 */
 
@@ -13,11 +13,11 @@
 
 /*** Prototypes *************************************************************/
 /* Main *********************************************************************/
-STRPTR  _(ULONG ID);            /* Get a message, as a STRPTR */
+CONST_STRPTR _(ULONG ID);       /* Get a message, as a STRPTR */
 #define __(id) ((IPTR) _(id))   /* Get a message, as an IPTR */
 
 /* Setup ********************************************************************/
-BOOL Locale_Initialize(void);
-void Locale_Deinitialize(void);
+VOID Locale_Initialize(VOID);
+VOID Locale_Deinitialize(VOID);
 
 #endif /* _LOCALE_H */
