e1f86f4958d6a177dbbc755aab0c907f0a382d1b
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-07-01 15:08:08 +0000

    Implemented support for F_DUPFD, with the help of Fabio Alemagna. Doesn't handle the semantics regarding the FD_CLOEXEC flag, since we don't support it yet.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@21903 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/fcntl.c
+++ compiler/clib/fcntl.c
@@ -18,40 +18,57 @@ int fcntl(int fd, int cmd, ...)
 
     if (!desc)
     {
-	errno = EBADF;
-	return -1;
+        errno = EBADF;
+        return -1;
     }
 
     switch (cmd)
     {
-	case F_GETFL:
-	    return desc->flags & (O_NONBLOCK|O_APPEND|O_ASYNC);
+        case F_DUPFD:
+        {
+            va_list ap;
+            int     arg;
+            
+            va_start(ap, cmd);
+            arg = va_arg(ap, int);
+            va_end(ap);
+            
+            /*
+                FIXME: FD_CLOEXEC must be off on the copy, once this flag
+                       is supported (related to F_GETFD and F_SETFD).
+            */
+            
+            return dup2(fd, __getfirstfd(arg));
+        }
+	
+        case F_GETFL:
+            return desc->flags & (O_NONBLOCK|O_APPEND|O_ASYNC);
 
-	case F_SETFL:
-	    {
-	        va_list ap;
-	        int arg;
-    
-	        int oldmode = __oflags2amode(desc->flags & ~(O_NONBLOCK|O_APPEND|O_ASYNC));
+        case F_SETFL:
+        {
+            va_list ap;
+            int     arg;
+            int     oldmode = __oflags2amode(desc->flags & ~(O_NONBLOCK|O_APPEND|O_ASYNC));
 
-	        va_start(ap, cmd);
-	        arg = va_arg(ap, int);
-	        va_end(ap);
+            va_start(ap, cmd);
+            arg = va_arg(ap, int);
+            va_end(ap);
   
-		arg &= (O_NONBLOCK|O_APPEND|O_ASYNC);
+            arg &= (O_NONBLOCK|O_APPEND|O_ASYNC);
 
-		if (ChangeMode(CHANGE_FH, desc->fh, oldmode | __oflags2amode(arg)) == DOSTRUE)
-		{
-	            desc->flags &= ~(O_NONBLOCK|O_APPEND|O_ASYNC);
-		    desc->flags |= arg;
-		    return 0;
-		}
+            if (ChangeMode(CHANGE_FH, desc->fh, oldmode | __oflags2amode(arg)) == DOSTRUE)
+            {
+                desc->flags &= ~(O_NONBLOCK|O_APPEND|O_ASYNC);
+                desc->flags |= arg;
+                return 0;
+            }
 
-		errno = IoErr2errno(IoErr());
-		return -1;
-	    }
-	default:
-	    errno = EINVAL;
-	    return -1;
+            errno = IoErr2errno(IoErr());
+            return -1;
+        }
+
+        default:
+            errno = EINVAL;
+            return -1;
     }
 }
