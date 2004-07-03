6b49746a3b51b67461981eb7a336c7b0a20b13b6
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-07-03 13:54:24 +0000

    Forward the 'mode' parameter to __open() if O_CREAT is specified.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@21919 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/open.c
+++ compiler/clib/open.c
@@ -80,6 +80,17 @@
 
 ******************************************************************************/
 {
-    return __open(__getfirstfd(0), pathname, flags, 0644);
+    mode_t mode = 0644;
+
+    if (flags & O_CREAT)
+    {
+        va_list ap;
+
+        va_start(ap, flags);
+        mode = va_arg(ap, int);
+        va_end(ap);
+    }
+    
+    return __open(__getfirstfd(0), pathname, flags, mode);
 } /* open */
 
