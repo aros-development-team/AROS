0abd47a4640f9b00c491d798fa5727b0da78cff5
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-07-14 01:45:28 +0000

    Removed prototypes for Locale_Initialize/Deinitialize() since they are not called explicitly from outside anymore.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@21942 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/system/AboutAROS/locale.h
+++ workbench/system/AboutAROS/locale.h
@@ -34,8 +34,4 @@
 STRPTR  _(ULONG ID);            /* Get a message, as a STRPTR */
 #define __(id) ((IPTR) _(id))   /* Get a message, as an IPTR */
 
-/* Setup ********************************************************************/
-BOOL Locale_Initialize(void);
-void Locale_Deinitialize(void);
-
 #endif /* _LOCALE_H */
