6e73d199ab3a8f186e0f5f6e8b12c492422682e2
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-08-29 19:47:14 +0000

    MUI undocumented tag MUIA_Imagedisplay_UseDefSize added. Found in nlist sources. Maybe the Zune private MUIA_Imagedisplay_FreeHoriz/ MUIA_Imagedisplay_FreeVert should be killed? Now? Also fixed some compiler warnings.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@22211 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/classes/imagedisplay.h
+++ workbench/libs/muimaster/classes/imagedisplay.h
@@ -13,10 +13,11 @@
 #define MUIB_Imagedisplay           (MUIB_ZUNE | 0x00001200)
 
 /*** Attributes *************************************************************/
-#define MUIA_Imagedisplay_Spec      (MUIB_MUI|0x0042a547) /* MUI: V11 isg struct MUI_ImageSpec * */
+#define MUIA_Imagedisplay_Spec       (MUIB_MUI|0x0042a547) /* MUI: V11 isg struct MUI_ImageSpec * */
+#define MUIA_Imagedisplay_UseDefSize (MUIB_MUI|0x0042186d) /* MUI: V11 i.. BOOL (undoc) */
 
-#define MUIA_Imagedisplay_FreeHoriz (MUIB_Imagedisplay | 0x00000000) /* Zune 20030323 i.. BOOL [TRUE] */
-#define MUIA_Imagedisplay_FreeVert  (MUIB_Imagedisplay | 0x00000001) /* Zune 20030323 i.. BOOL [TRUE] */
+#define MUIA_Imagedisplay_FreeHoriz  (MUIB_Imagedisplay | 0x00000000) /* Zune 20030323 i.. BOOL [TRUE] */
+#define MUIA_Imagedisplay_FreeVert   (MUIB_Imagedisplay | 0x00000001) /* Zune 20030323 i.. BOOL [TRUE] */
 
 
 extern const struct __MUIBuiltinClass _MUI_Imagedisplay_desc; /* PRIV */
