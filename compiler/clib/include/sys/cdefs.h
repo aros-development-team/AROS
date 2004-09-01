df740de66f4e043f0e545161cf0c0b7f2936e8cd
falemagn <falemagn@gmail.com>
2004-09-01 03:08:01 +0000

    Moved the endianess defines to the endian.h file, as this file is needed by some packages.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@22244 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/include/sys/cdefs.h
+++ compiler/clib/include/sys/cdefs.h
@@ -193,27 +193,6 @@
     END OF IMPORTANT STUFF
  ****************************************************************************/
 
-/*
-    BSD systems like to see BYTE_ORDER and friends.
-
-    FreeBSD 5 in its pedantic namespace also has versions with underscores
-    to make it easier to port stuff, create them as well.
-*/
-#define _LITTLE_ENDIAN      1234
-#define _BIG_ENDIAN         4321
-#define _PDP_ENDIAN         3412
-
-#if AROS_BIG_ENDIAN
-#   define _BYTE_ORDER  _BIG_ENDIAN
-#else
-#   define _BYTE_ORDER  _LITTLE_ENDIAN
-#endif
-
-#if __BSD_VISIBLE
-#   define LITTLE_ENDIAN    _LITTLE_ENDIAN
-#   define BIG_ENDIAN       _BIG_ENDIAN
-#   define PDP_ENDIAN       _PDP_ENDIAN
-#   define BYTE_ORDER       _BYTE_ORDER
-#endif
+#include <endian.h>
 
 #endif /* _SYS_CDEFS_H_ */
