5ecbbe3cef3c5afdda315ea645903ba112e59d1b
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-10-23 15:45:42 +0000

    Reverted last commit because it needs more commits in other places I don't want to commit yet. Alias function names are thus not working at the moment.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@22470 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- tools/genmodule/writestubs.c
+++ tools/genmodule/writestubs.c
@@ -5,15 +5,13 @@
     Function to write module_autoinit.c. Part of genmodule.
 */
 #include "genmodule.h"
-#include <aros/machine.h>
 
 void writestubs(void)
 {
-    FILE *out, *outasm, *f;
+    FILE *out;
     char line[256];
     struct functionlist *funclistit;
     struct arglist *arglistit;
-    struct aliaslist *aliasesit;
 
     snprintf(line, 255, "%s/%s_stubs.c", gendir, modulename);
     out = fopen(line, "w");
@@ -49,74 +47,32 @@ void writestubs(void)
         "\n"
     );
     
-    snprintf(line, 255, "%s/%s_astubs.S", gendir, modulename);
-    outasm = fopen(line, "w");
-    if (outasm==NULL)
-    {
-	fprintf(stderr, "Could not write %s\n", line);
-	exit(20);
-    }
-    fprintf
-    (
-        outasm,
-        "/*\n"
-        "    *** Automatically generated file. Do not edit ***\n"
-        "    Copyright © 1995-2003, The AROS Development Team. All rights reserved.\n"
-        "*/\n"
-    );
-    fprintf(outasm, STUBCODE_INIT);
-    
     for (funclistit = funclist; funclistit!=NULL; funclistit = funclistit->next)
     {
-	if (libcall != STACK)
+	fprintf(out,
+		"\n"
+		"%s %s(",
+		funclistit->type, funclistit->name);
+	for (arglistit = funclistit->arguments;
+	     arglistit!=NULL;
+	     arglistit = arglistit->next)
 	{
-	    fprintf(out,
-		    "\n"
-		    "%s %s(",
-		    funclistit->type, funclistit->name
-	    );
-	    for (arglistit = funclistit->arguments;
-		 arglistit!=NULL;
-		 arglistit = arglistit->next
-	    )
-	    {
-		if (arglistit != funclistit->arguments)
-		    fprintf(out, ", ");
-		fprintf(out, "%s %s", arglistit->type, arglistit->name);
-	    }
-	    fprintf(out,
-		    ")\n"
-		    "{\n"
-		    "    return AROS_LC%d(%s, %s,\n",
-		    funclistit->argcount, funclistit->type, funclistit->name
-	    );
-	    for (arglistit = funclistit->arguments;
-		 arglistit!=NULL;
-		 arglistit = arglistit->next
-	    )
-		fprintf(out, "                    AROS_LCA(%s,%s,%s),\n",
-			arglistit->type, arglistit->name, arglistit->reg
-		);
-	    fprintf(out, "                    %s *, %s, %u, %s);\n}\n", libbasetypeextern,
-		    libbase, funclistit->lvo, basename
-	    );
+	    if (arglistit != funclistit->arguments)
+		fprintf(out, ", ");
+	    fprintf(out, "%s %s", arglistit->type, arglistit->name);
 	}
-	else /* libcall==STACK */
-	{
-	    fprintf(outasm,
-		    STUBCODE,
-		    funclistit->name,
-		    libbase,
-		    &(__AROS_GETJUMPVEC(NULL, funclistit->lvo)->vec)
-	    );
-	}
-	
-	for (aliasesit = funclistit->aliases;
-	     aliasesit != NULL;
-	     aliasesit = aliasesit->next
-	)
-	    fprintf(outasm, ALIASCODE, funclistit->name, aliasesit->alias);
+	fprintf(out,
+		")\n"
+		"{\n"
+		"    return AROS_LC%d(%s, %s,\n",
+		funclistit->argcount, funclistit->type, funclistit->name);
+	for (arglistit = funclistit->arguments;
+	     arglistit!=NULL;
+	     arglistit = arglistit->next)
+	    fprintf(out, "                    AROS_LCA(%s,%s,%s),\n",
+		    arglistit->type, arglistit->name, arglistit->reg);
+	fprintf(out, "                    %s *, %s, %u, %s);\n}\n", libbasetypeextern,
+		libbase, funclistit->lvo, basename);
     }
     fclose(out);
-    fclose(outasm);
 }
