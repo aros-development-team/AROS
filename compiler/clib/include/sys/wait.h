600809adf24dc1182edc05cf49ee7d849dfc8fe2
Kalamatee <kalamatee@gmail.com>
2004-11-17 19:12:41 +0000

    include sys/types to prevent errors while compiling
    git-svn-id: https://svn.aros.org/svn/aros/trunk@22498 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/include/sys/wait.h
+++ compiler/clib/include/sys/wait.h
@@ -1,1 +1,22 @@
+/*
+    Copyright © 2002-2004, The AROS Development Team. 
+    All rights reserved.
+    
+    $Id$
+*/
+
+#ifndef SYS_WAIT_H_
+#define SYS_WAIT_H_
+
+#ifndef SYS_TYPES_H
+#include <sys/types.h>
+#endif
+
 pid_t wait(int *status);
+//pid_t   waitpid(pid_t, int *, int);
+
+//struct rusage;
+//pid_t   wait3(int *, int, struct rusage *);
+//pid_t   wait4(pid_t, int *, int, struct rusage *);
+
+#endif /* SYS_WAIT_H_ */
