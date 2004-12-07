cfed485ff4d64f0a64ba616540bb48691538aabd
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-12-07 22:32:29 +0000

    First try /usr/include/asm-$(CPU)/sigcontext.h before /usr/include/asm/sigcontext.h for finding the sigcontext struct. This way it can also be found when cross compiling. Be sure that sigcore.h is regenerated when mksigcore.h is changed
    git-svn-id: https://svn.aros.org/svn/aros/trunk@22536 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-linux/exec/mksigcore.sh
+++ arch/all-linux/exec/mksigcore.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-type=`grep "^struct sigcontext" /usr/include/asm/sigcontext.h | sed 's/{//'`
+if [ -f /usr/include/asm-${CPU}/sigcontext.h ]; then
+    type=`grep "^struct sigcontext" /usr/include/asm-${CPU}/sigcontext.h | sed 's/{//'`
+elif [ -f /usr/include/asm/sigcontext.h ]; then
+    type=`grep "^struct sigcontext" /usr/include/asm/sigcontext.h | sed 's/{//'`
+else
+    echo "Could not find asm/sigcontext.h"
+    exit 20
+fi
+
 handler=__sighandler_t
 if [ `uname -m` = "m68k" ]; then
   sed "s/@sigcontext@/$type/" ${1-.}/../m68k/sigcore.h.src > ${2}
