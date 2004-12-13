5a57bb9fcd937eec3bac63867b7d7d2c4e9b2be9
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-12-13 21:44:06 +0000

    Added support for calling device open and close init functions.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@22576 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/libinit/libinit.c
+++ compiler/libinit/libinit.c
@@ -46,3 +46,53 @@ AROS_UFH3(int, set_call_libfuncs,
     
     AROS_USERFUNC_EXIT
 }
+
+AROS_UFH6(int, set_call_devfuncs,
+	  AROS_UFHA(const void**, list, A0),
+	  AROS_UFHA(int, order, D2),
+	  AROS_UFHA(void*, ioreq, A1),
+	  AROS_UFHA(ULONG, unitnum, D0),
+	  AROS_UFHA(ULONG, flags, D1),
+	  AROS_UFHA(void*, libbase, A6)
+)
+{
+    AROS_USERFUNC_INIT
+    
+    int n;
+
+    if (order>=0)
+    {
+        n = 1;
+        while(list[n])
+        {
+            if
+	    (
+	        !AROS_UFC4(int, list[n++], 
+			   AROS_UFCA(void*, ioreq, A1),
+			   AROS_UFCA(ULONG, unitnum, D0),
+			   AROS_UFCA(ULONG, flags, D1),
+			   AROS_UFCA(void *, libbase, A6)
+		 )
+	    )
+		return FALSE;
+        }
+    }
+    else
+    {
+        n = ((int *)list)[0];
+        
+        while (n)
+        {
+	    AROS_UFC4(int, list[n--], 
+		      AROS_UFCA(void*, ioreq, A1),
+		      AROS_UFCA(ULONG, unitnum, D0),
+		      AROS_UFCA(ULONG, flags, D1),
+		      AROS_UFCA(void *, libbase, A6)
+	    );
+        }
+    }
+
+    return TRUE;
+    
+    AROS_USERFUNC_EXIT
+}
