54e10f9d231ec32ac23e0e5a751ff5bad0089f26
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-12-25 13:10:41 +0000

    misc.resource is now built with build_module. Use now also the autogenerated Resident structure.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@22619 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/misc/misc_init.c
+++ rom/misc/misc_init.c
@@ -15,85 +15,20 @@
 #include <proto/misc.h>
 #include <resources/misc.h>
 
-#include <aros/asmcall.h>
+#include <aros/symbolsets.h>
 #include "misc_intern.h"
 
 #include LC_LIBDEFS_FILE
 
-static const UBYTE name[];
-static const UBYTE idstring[];
-static const void * const functable[];
-extern const char LIBEND;
-
-struct MiscBase *AROS_SLIB_ENTRY(init, Misc)();
-
-extern void AROS_SLIB_ENTRY(AllocMiscResource ,Misc)();
-extern void AROS_SLIB_ENTRY(FreeMiscResource, Misc)();
-
-int misc_entry(void)
+AROS_SET_LIBFUNC(GM_UNIQUENAME(Init), LIBBASETYPE, MiscBase)
 {
-    return -1;
+    AROS_SET_LIBFUNC_INIT
+
+    InitSemaphore(&MiscBase->mb_Lock);
+
+    return TRUE;
+
+    AROS_SET_LIBFUNC_EXIT
 }
 
-const struct Resident Misc_resident =
-{
-    RTC_MATCHWORD,
-    (struct Resident *)&Misc_resident,
-    (APTR)&LIBEND,
-    RTF_COLDSTART,
-    41,
-    NT_RESOURCE,
-    45,
-    (UBYTE *)name,
-    (UBYTE *)&idstring[6],
-    (ULONG *)&AROS_SLIB_ENTRY(init,Misc)
-};
-
-static const UBYTE name[] = MISCNAME;
-static const UBYTE idstring[] = "misc 41.0 (23.7.1998)";
-
-static const void * const functable[] =
-{
-    &AROS_SLIB_ENTRY(AllocMiscResource, Misc),
-    &AROS_SLIB_ENTRY(FreeMiscResource,  Misc),
-    (void *)-1
-};
-
-
-#ifdef SysBase
-#undef SysBase
-#endif
-//#define SysBase (*(APTR*)4L)
-
-AROS_UFH3(struct MiscBase *, AROS_SLIB_ENTRY(init, Misc),
-    AROS_UFHA(ULONG,	dummy,	D0),
-    AROS_UFHA(ULONG,	slist,	A0),
-    AROS_UFHA(struct ExecBase *, SysBase, A6)
-)
-{
-    AROS_USERFUNC_INIT
-
-    UWORD neg = AROS_ALIGN(LIB_VECTSIZE * 3);
-    struct MiscBase *MiscBase = NULL;
-    
-    MiscBase = (struct MiscBase *)(((UBYTE *)
-	AllocMem( neg + sizeof(struct MiscBase),
-		 MEMF_CLEAR | MEMF_PUBLIC)) + neg);
-
-    if( MiscBase )
-    {
-	MiscBase->mb_SysBase = SysBase;
-	MiscBase->mb_Node.ln_Pri = 0;
-	MiscBase->mb_Node.ln_Type = NT_RESOURCE;
-	MiscBase->mb_Node.ln_Name = (STRPTR)name;
-
-	InitSemaphore(&MiscBase->mb_Lock);
-
-	MakeFunctions(MiscBase, (APTR)functable, NULL);
-	AddResource(MiscBase);
-    }
-
-    return MiscBase;
-
-    AROS_USERFUNC_EXIT
-}
+ADD2INITLIB(GM_UNIQUENAME(Init), 0)
