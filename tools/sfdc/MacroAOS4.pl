21b29fb6fcc50d4b565c3b6a6016a79fd6755f20
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2005-01-28 17:18:18 +0000

    Two more parens...
    git-svn-id: https://svn.aros.org/svn/aros/trunk@22761 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- tools/sfdc/MacroAOS4.pl
+++ tools/sfdc/MacroAOS4.pl
@@ -26,7 +26,7 @@ BEGIN {
 	if ($prototype->{type} eq 'function'  ||
 	    $prototype->{type} eq 'cfunction' ||
 	    $prototype->{type} eq 'varargs') {
-	    printf "	(((struct $sfd->{BaseName}IFace *)___base)->$prototype->{funcname})(";
+	    printf "	(((struct $sfd->{BaseName}IFace *)(___base))->$prototype->{funcname})(";
 	}
 	else {
 	    $self->SUPER::function_start (@_);
