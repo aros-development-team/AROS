e580e3110ea922cb9c55b03d9e43d08924ff2433
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2005-02-05 17:18:53 +0000

    Some macros to handle variadic arguments, the most important being CAST2IPTR, which casts all items in a variadic list of arguments to IPTR. Useful for the inline macros, so that one doesn't have to manually cast arguments to IPTR anymore.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@22827 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/aros/preprocessor/variadic.hpp
@@ -0,0 +1,8 @@
+# ifndef AROS_PREPROCESSOR_VARIADIC_HPP
+# define AROS_PREPROCESSOR_VARIADIC_HPP
+#
+# include <aros/preprocessor/variadic/first.hpp>
+# include <aros/preprocessor/variadic/rest.hpp>
+# include <aros/preprocessor/variadic/cast2iptr.hpp>
+#
+# endif
---
+++ compiler/include/aros/preprocessor/variadic/cast2iptr.hpp
@@ -0,0 +1,28 @@
+# ifndef AROS_PREPROCESSOR_VARIADIC_CAST2IPTR_HPP
+# define AROS_PREPROCESSOR_VARIADIC_CAST2IPTR_HPP
+# 
+# include <boost/preprocessor/repetition/for.hpp>
+# include <boost/preprocessor/logical/compl.hpp>
+# include <boost/preprocessor/facilities/is_empty.hpp>
+# include <boost/preprocessor/punctuation/comma_if.hpp>
+# include <aros/preprocessor/variadic/first.hpp>
+# include <aros/preprocessor/variadic/rest.hpp>
+
+# define AROS_PP_VARIADIC_CAST2IPTR_O(_, tuple) \
+      (AROS_PP_VARIADIC_REST tuple)
+
+# define AROS_PP_VARIADIC_CAST2IPTR_M(_, tuple)             \
+      (IPTR)(AROS_PP_VARIADIC_FIRST tuple)BOOST_PP_COMMA_IF( \
+          AROS_PP_VARIADIC_CAST2IPTR_P(,                    \
+              (AROS_PP_VARIADIC_REST tuple)))
+ 
+# define AROS_PP_VARIADIC_CAST2IPTR_P(_, tuple) \
+      BOOST_PP_COMPL(BOOST_PP_IS_EMPTY(AROS_PP_VARIADIC_FIRST tuple))
+ 
+# define AROS_PP_VARIADIC_CAST2IPTR(...)                              \
+      BOOST_PP_FOR(                                                   \
+          (__VA_ARGS__), AROS_PP_VARIADIC_CAST2IPTR_P,                \
+          AROS_PP_VARIADIC_CAST2IPTR_O, AROS_PP_VARIADIC_CAST2IPTR_M)
+
+# endif
+
