ae12fcde7085672c8ea6d723476443fcdeefc05a
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2005-02-06 17:35:28 +0000

    Implemented a substitute of Boost's IS_EMPTY predicate. Boost's one chokes on anything that begins or ends with an operator, the one I implemented happily eats all kind of junk :-) To Boost's defense, it must be said that Boost doesn't make use of variadic macros, as they're not part of the C++ standard (yet), and my implementation takes full advantage of variadic macros instead.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@22837 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/aros/preprocessor/facilities.hpp
@@ -0,0 +1,7 @@
+# ifndef AROS_PREPROCESSOR_FACILITIES_HPP
+# define AROS_PREPROCESSOR_FACILITIES_HPP
+#
+# include <boost/preprocessor/facilities.hpp>
+# include <aros/preprocessor/facilities/is_empty.hpp>
+#
+# endif
