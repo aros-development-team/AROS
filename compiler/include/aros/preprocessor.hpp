9ce32fc65802bc1e086b824e9497d499df61f62d
falemagn <falemagn@gmail.com>
2005-02-07 04:34:11 +0000

    Include also the facilities stuff.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@22861 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/aros/preprocessor.hpp
+++ compiler/include/aros/preprocessor.hpp
@@ -1,8 +1,10 @@
 # ifndef AROS_PREPROCESSOR_HPP
 # define AROS_PREPROCESSOR_HPP
-
+#
 # include <boost/preprocessor.hpp>
+#
 # include <aros/preprocessor/variadic.hpp>
-
+# include <aros/preprocessor/facilities.hpp>
+#
 # endif
 
