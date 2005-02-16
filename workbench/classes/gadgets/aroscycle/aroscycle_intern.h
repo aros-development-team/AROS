83cd020fcfbfc04430502ad3d11cde325465126f
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2005-02-16 20:50:31 +0000

    Converted aroscycle gadget to use %build_module and genmodule. Code compiles but is not run time tested because the gadget seems to be used nowhere inside AROS.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@22924 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/classes/gadgets/aroscycle/aroscycle_intern.h
+++ workbench/classes/gadgets/aroscycle/aroscycle_intern.h
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2005, The AROS Development Team. All rights reserved.
     $Id$
 */
 
@@ -39,8 +39,6 @@
 
 /***********************************************************************************/
 
-//#define GLOBAL_INTUIBASE
-
 #define TURN_OFF_DEBUG
 
 
@@ -48,9 +46,6 @@
 #define G(obj) ((struct Gadget *)(obj))
 
 /***********************************************************************************/
-
-/* Predeclaration */
-struct CycleBase_intern;
 
 /* CycleClass definitions */
 struct CycleData
@@ -65,61 +60,9 @@ struct CycleData
 
 /* Prototypes */
 
-void drawdisabledpattern(struct CycleBase_intern *AROSCycleBase, struct RastPort *rport, UWORD pen, WORD left, WORD top, UWORD width, UWORD height);
-void renderlabel (struct CycleBase_intern *AROSCycleBase, struct Gadget *gad, STRPTR label, struct RastPort *rport, struct GadgetInfo *ginfo);
+void drawdisabledpattern(struct RastPort *rport, UWORD pen, WORD left, WORD top, UWORD width, UWORD height);
+void renderlabel (struct Gadget *gad, STRPTR label, struct RastPort *rport, struct GadgetInfo *ginfo);
 BOOL pointingadget(struct Gadget *gad, struct GadgetInfo *gi, WORD x, WORD y);
-
-/***********************************************************************************/
-
-/* Library stuff */
-struct CycleBase_intern
-{
-    struct Library 	    library;
-    struct ExecBase	    *sysbase;
-    BPTR		    seglist;
-    struct Library	    *dosbase;
-
-    #ifndef GLOBAL_INTUIBASE
-    struct IntuitionBase    *intuitionbase;
-    #endif
-    struct GfxBase	    *gfxbase;
-    struct Library	    *utilitybase;
-    
-    struct IClass	    *classptr;
-	
-};
-
-/***********************************************************************************/
-
-/* The following typedefs are necessary, because the names of the global
-   variables storing the library base pointers	and the corresponding
-   structs are equal.
-   This is a hack, of course. */
-   
-typedef struct GfxBase GraphicsBase;
-typedef struct IntuitionBase IntuiBase;
-
-#undef CYB
-#define CYB(b) ((struct CycleBase_intern *)b)
-#undef UtilityBase
-#define UtilityBase 	CYB(AROSCycleBase)->utilitybase
-
-
-#ifndef GLOBAL_INTUIBASE
-#undef IntuitionBase
-#define IntuitionBase	CYB(AROSCycleBase)->intuitionbase
-#endif
-
-#undef GfxBase
-#define GfxBase		CYB(AROSCycleBase)->gfxbase
-#undef SysBase
-#define SysBase		CYB(AROSCycleBase)->sysbase
-#undef DOSBase
-#define DOSBase		CYB(AROSCycleBase)->dosbase
-
-
-#define expunge() \
-AROS_LC0(BPTR, expunge, struct CycleBase_intern *, AROSCycleBase, 3, AROSCycle)
 
 /***********************************************************************************/
 
