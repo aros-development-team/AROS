6594f99d4e377937bbc5d18279a485113bc369c6
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2005-02-16 20:42:34 +0000

    Converted gradientslider gadget to %build_module and genmodule. Also tried to update amiga support in library.c (untested)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@22922 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/classes/gadgets/gradientslider/gradientslider_intern.h
+++ workbench/classes/gadgets/gradientslider/gradientslider_intern.h
@@ -2,7 +2,7 @@
 #define GRADIENTSLIDER_INTERN_H
 
 /*
-    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2005, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: Internal definitions for gradientslider.gadget.
@@ -43,9 +43,11 @@
 #define ReturnPtr(a,b,c) return c
 #define ReturnInt(a,b,c) return c
 #define ReturnVoid(a) return
-#endif
 
 #define SysBase (((struct LibHeader *) GradientSliderBase)->lh_SysBase)
+
+#endif
+
 
 #undef 	EG
 #define EG(o) ((struct ExtGadget *)o)
@@ -80,68 +82,20 @@ struct GradientSliderData
 };
 
 
-struct GradientSliderBase_intern
-{
-    struct Library 		library;
-    UWORD   	    	    	pad;
-    struct IClass 		*classptr;
-    struct ExecBase		*sysbase;
-    BPTR			seglist;
-#ifndef GLOBAL_INTUIBASE
-    struct IntuitionBase	*intuibase;
-#endif
-    struct GfxBase		*gfxbase;
-    struct Library		*utilitybase;
-    struct Library		*cybergfxbase;
-    struct Library  	    	*layersbase;
-    
-};
-
 /***************************************************************************************************/
 
-struct IClass * InitGradientSliderClass (struct GradientSliderBase_intern *GradientSliderBase);
 VOID DrawGradient(struct RastPort *rp, WORD x1, WORD y1, WORD x2, WORD y2, UWORD *penarray,
-		  WORD numpens, WORD orientation, struct ColorMap *cm, 
-		  struct GradientSliderBase_intern *GradientSliderBase);
+		  WORD numpens, WORD orientation, struct ColorMap *cm
+);
 VOID DrawKnob(struct GradientSliderData *data, struct RastPort *rp, struct DrawInfo *dri, 
-	      struct IBox *box, WORD state, struct GradientSliderBase_intern *GradientSliderBase);
+	      struct IBox *box, WORD state
+);
 VOID GetGadgetIBox(Object *o, struct GadgetInfo *gi, struct IBox *ibox);
 VOID GetSliderBox(struct IBox *gadgetbox, struct IBox *sliderbox);
 VOID GetKnobBox(struct GradientSliderData *data, struct IBox *sliderbox, struct IBox * knobbox);
-void DrawDisabledPattern(struct RastPort *rport, struct IBox *gadbox, UWORD pen,
-			 struct GradientSliderBase_intern *GradientSliderBase);
+void DrawDisabledPattern(struct RastPort *rport, struct IBox *gadbox, UWORD pen);
 
 
 /***************************************************************************************************/
-
-/* The following typedefs are necessary, because the names of the global
-   variables storing the library base pointers	and the corresponding
-   structs are equal.
-   This is a hack, of course. */
-typedef struct GfxBase GraphicsBase;
-typedef struct IntuitionBase IntuiBase;
-
-/***************************************************************************************************/
-
-#undef GSB
-#define GSB(b) ((struct GradientSliderBase_intern *)b)
-#undef UtilityBase
-#define UtilityBase 	GSB(GradientSliderBase)->utilitybase
-
-
-#ifndef GLOBAL_INTUIBASE
-#undef IntuitionBase
-#define IntuitionBase	GSB(GradientSliderBase)->intuibase
-#endif
-
-#define CyberGfxBase	GSB(GradientSliderBase)->cybergfxbase
-
-#undef GfxBase
-#define GfxBase		GSB(GradientSliderBase)->gfxbase
-#undef SysBase
-#define SysBase		GSB(GradientSliderBase)->sysbase
-
-#undef LayersBase
-#define LayersBase		GSB(GradientSliderBase)->layersbase
 
 #endif /* GRADIENTSLIDER_INTERN_H */
