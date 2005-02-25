be0dae0e534e531123197a04757e5a0f00e6d452
sebauer <sebauer@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2005-02-25 12:42:59 +0000

    Predeclare some structures.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@22976 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/menu.h
+++ workbench/libs/muimaster/menu.h
@@ -8,6 +8,10 @@
 #ifndef _MUI_MENU_H
 #define _MUI_MENU_H
 
+struct NewMenu;
+struct Window;
+struct ZMenu;
+
 struct ZMenu *zune_open_menu(struct Window *wnd, struct NewMenu *newmenu);
 void zune_mouse_update(struct ZMenu *menu, int left_down);
 /* returns the user data of the selected entry */
