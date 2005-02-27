9dea90db4e0267a2cc629f86db2710b651d7a8fe
sebauer <sebauer@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2005-02-27 00:51:12 +0000

    Added some macros for the OS4 exec interface support.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@23029 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/support_aros.h
+++ workbench/libs/muimaster/support_aros.h
@@ -8,6 +8,13 @@
 #ifndef _MUIMASTER_SUPPORT_AROS_H_
 #define _MUIMASTER_SUPPORT_AROS_H_
 
+/*** OS4 Exec Interface support *********************************************/
+#define EXEC_INTERFACE_DECLARE(x)
+#define EXEC_INTERFACE_GET_MAIN(interface,libbase) 1
+#define EXEC_INTERFACE_DROP(interface)
+#define EXEC_INTERFACE_ASSIGN(a,b)
+
+/*** Misc OS4 Functions *****************************************************/
 LONG HexToLong(STRPTR s, ULONG *val);
 
 #endif
