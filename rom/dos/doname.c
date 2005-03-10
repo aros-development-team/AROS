aaca42fdeae3ea6eaa33b04eb2936478401e78ac
jmj <jmj@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2005-03-10 09:53:42 +0000

    fixed to have ':' at the beginning of a path resolve to the root directory of current directory's filesystem.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@23083 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/doname.c
+++ rom/dos/doname.c
@@ -13,11 +13,13 @@
 #include <proto/dos.h>
 #include <proto/utility.h>
 #include <string.h>
+
 #include "dos_intern.h"
 
 #ifdef DEBUG
 #undef DEBUG
 #endif
+
 # define  DEBUG  1
 # include <aros/debug.h>
 
@@ -25,7 +27,6 @@
 LONG DoName(struct IOFileSys *iofs, CONST_STRPTR name,
 	    struct DosLibrary *DOSBase)
 {
-    STRPTR volname;
     CONST_STRPTR pathname, s1 = NULL;
     BPTR cur, lock = (BPTR)NULL;
     struct DosList *dl;
@@ -34,46 +35,66 @@ LONG DoName(struct IOFileSys *iofs, CONST_STRPTR name,
     struct FileHandle *fh;
     struct Process *me = (struct Process *)FindTask(NULL);
 
+        STRPTR
+    volname = NULL;
+
     if (!Strnicmp(name, "PROGDIR:", 8) && me->pr_HomeDir != NULL)
     {
-	cur = me->pr_HomeDir;
-	volname = NULL;
-	pathname = name + 8;
-    }
-    else if (*name == ':')
-    {
-	cur = me->pr_CurrentDir;
-	volname = NULL;
-	pathname = name + 1;
+        cur = me->pr_HomeDir;
+        volname = NULL;
+        pathname = name + 8;
     }
     else
     {
-	/* Copy volume name */
-	cur      = me->pr_CurrentDir;
-	s1       = name;
-	pathname = name;
-	volname  = NULL;
+/* relative to root dir of current directory's filesystem */
+        if (*name == ':')
+        {
+            cur = me->pr_CurrentDir;
+            
+                char
+            buf[256];
 
-	while (*s1 != 0)
-	{
-	    if (*s1++ == ':')
-	    {
-		volname = (STRPTR)AllocMem(s1 - name, MEMF_ANY);
+/* get complete path name of current dir and make absolute path from <name> */            
+            if (NameFromLock(cur, buf, 255))
+            {
+                    char*
+                colon = strchr(buf, ':');
+                
+                if (NULL != colon)
+                {
+                    strcpy(colon + 1, name + 1);
 
-		if (volname == NULL)
-		{
-		    SetIoErr(ERROR_NO_FREE_STORE);
-		    return ERROR_NO_FREE_STORE;
-		}
+                    name = buf;
+                }
+            }
+        }
 
-		CopyMem(name, volname, s1 - name - 1);
-		volname[s1 - name - 1] = '\0';
-
-		pathname = s1;
-		
-		break;
-	    }
-	}
+    	/* Copy volume name */
+    	cur      = me->pr_CurrentDir;
+    	s1       = name;
+    	pathname = name;
+    	volname  = NULL;
+    
+    	while (*s1 != 0)
+    	{
+    	    if (*s1++ == ':')
+    	    {
+        		volname = (STRPTR)AllocMem(s1 - name, MEMF_ANY);
+        
+        		if (volname == NULL)
+        		{
+        		    SetIoErr(ERROR_NO_FREE_STORE);
+        		    return ERROR_NO_FREE_STORE;
+        		}
+        
+        		CopyMem(name, volname, s1 - name - 1);
+        		volname[s1 - name - 1] = '\0';
+        
+        		pathname = s1;
+        		
+        		break;
+    	    }
+    	}
     }
 
     if (!volname && !cur && !DOSBase->dl_SYSLock)
@@ -85,93 +106,96 @@ LONG DoName(struct IOFileSys *iofs, CONST_STRPTR name,
 
     if (volname != NULL)
     {
-	/* Find logical device */
-	dl = FindDosEntry(dl, volname, LDF_ALL);
+    	/* Find logical device */
+    	dl = FindDosEntry(dl, volname, LDF_ALL);
+    
+    	if (dl == NULL)
+    	{
+    	    UnLockDosList(LDF_ALL|LDF_READ);
+    	    FreeMem(volname, s1 - name);
+    	    SetIoErr(ERROR_DEVICE_NOT_MOUNTED);
+    
+    	    return ERROR_DEVICE_NOT_MOUNTED;
+    	}
+    	else if (dl->dol_Type == DLT_LATE)
+    	{
+    	    D(bug("Locking late assign %s\n",
+                dl->dol_misc.dol_assign.dol_AssignName));
+    
+    	    lock = Lock(dl->dol_misc.dol_assign.dol_AssignName, SHARED_LOCK);
 
-	if (dl == NULL)
-	{
-	    UnLockDosList(LDF_ALL|LDF_READ);
-	    FreeMem(volname, s1 - name);
-	    SetIoErr(ERROR_DEVICE_NOT_MOUNTED);
+    	    UnLockDosList(LDF_ALL | LDF_READ);
+    
+    	    if (lock != NULL)
+    	    {
+        		AssignLock(volname, lock);
 
-	    return ERROR_DEVICE_NOT_MOUNTED;
-	}
-	else if (dl->dol_Type == DLT_LATE)
-	{
-	    D(bug("Locking late assign %s\n",
-		  dl->dol_misc.dol_assign.dol_AssignName));
+        		dl = LockDosList(LDF_ALL | LDF_READ);
+        		dl = FindDosEntry(dl, volname, LDF_ALL);
+        
+        		if (dl == NULL)
+        		{
+        		    UnLockDosList(LDF_ALL | LDF_READ);
+        		    FreeMem(volname, s1 - name);
+        		    SetIoErr(ERROR_DEVICE_NOT_MOUNTED);
+        
+        		    return ERROR_DEVICE_NOT_MOUNTED;
+    	        }
+        
+        		device = dl->dol_Device;
+        		unit = dl->dol_Unit;
+    	    }
+    	    else
+    	    {
+        		FreeMem(volname, s1 - name);
+        
+        		return IoErr();
+    	    }
+    	}
+    	else if (dl->dol_Type == DLT_NONBINDING)
+    	{
+    	    lock = Lock(dl->dol_misc.dol_assign.dol_AssignName, SHARED_LOCK);
 
-	    lock = Lock(dl->dol_misc.dol_assign.dol_AssignName, SHARED_LOCK);
-	    UnLockDosList(LDF_ALL | LDF_READ);
-
-	    if (lock != NULL)
-	    {
-		AssignLock(volname, lock);
-		dl = LockDosList(LDF_ALL | LDF_READ);
-		dl = FindDosEntry(dl, volname, LDF_ALL);
-
-		if (dl == NULL)
-		{
-		    UnLockDosList(LDF_ALL | LDF_READ);
-		    FreeMem(volname, s1 - name);
-		    SetIoErr(ERROR_DEVICE_NOT_MOUNTED);
-
-		    return ERROR_DEVICE_NOT_MOUNTED;
-	        }
-
-		device = dl->dol_Device;
-		unit = dl->dol_Unit;
-	    }
-	    else
-	    {
-		FreeMem(volname, s1 - name);
-
-		return IoErr();
-	    }
-	}
-	else if (dl->dol_Type == DLT_NONBINDING)
-	{
-	    lock = Lock(dl->dol_misc.dol_assign.dol_AssignName, SHARED_LOCK);
-	    fh = (struct FileHandle *)BADDR(lock);
-
-	    if (fh != NULL)
-	    {
-		device = fh->fh_Device;
-		unit = fh->fh_Unit;
-	    }
-	    else
-	    {
-		UnLockDosList(LDF_ALL | LDF_READ);
-		FreeMem(volname, s1-name);
-
-		return IoErr();
-	    }
-	}
-	else
-	{
-	    device = dl->dol_Device;
-	    unit   = dl->dol_Unit;
-	}
-
-	pathname = s1;
+    	    fh = (struct FileHandle *)BADDR(lock);
+    
+    	    if (fh != NULL)
+    	    {
+        		device = fh->fh_Device;
+        		unit = fh->fh_Unit;
+    	    }
+    	    else
+    	    {
+        		UnLockDosList(LDF_ALL | LDF_READ);
+        		FreeMem(volname, s1-name);
+        
+        		return IoErr();
+    	    }
+    	}
+    	else
+    	{
+    	    device = dl->dol_Device;
+    	    unit   = dl->dol_Unit;
+    	}
+    
+    	pathname = s1;
     }
     else if (cur)
     {
-	fh       = (struct FileHandle *)BADDR(cur);
-	device   = fh->fh_Device;
-	unit     = fh->fh_Unit;
+    	fh       = (struct FileHandle *)BADDR(cur);
+    	device   = fh->fh_Device;
+    	unit     = fh->fh_Unit;
     }
     else
     {
-    #if 0
-    	/* stegerg: ?? */
-	device = DOSBase->dl_NulHandler;
-	unit = DOSBase->dl_NulLock;
-    #else
+        #if 0
+        	/* stegerg: ?? */
+    	device = DOSBase->dl_NulHandler;
+    	unit = DOSBase->dl_NulLock;
+        #else
     	fh = (struct FileHandle *)BADDR(DOSBase->dl_SYSLock);
-	device = fh->fh_Device;
-	unit = fh->fh_Unit;
-    #endif
+    	device = fh->fh_Device;
+    	unit = fh->fh_Unit;
+        #endif
     }
 
     iofs->IOFS.io_Device = device;
@@ -181,34 +205,38 @@ LONG DoName(struct IOFileSys *iofs, CONST_STRPTR name,
     /* Send the request. */
     DosDoIO(&iofs->IOFS);
 
-    if (iofs->io_DosError == ERROR_OBJECT_NOT_FOUND &&
-	dl->dol_Type == DLT_DIRECTORY)
+    if (iofs->io_DosError == ERROR_OBJECT_NOT_FOUND
+        && dl->dol_Type == DLT_DIRECTORY)
     {
-	struct AssignList *al;
+    	struct AssignList *al;
+    
+    	for (al = dl->dol_misc.dol_assign.dol_List;
+    	     al && iofs->io_DosError == ERROR_OBJECT_NOT_FOUND;
+    	     al = al->al_Next)
+    	{
+    	    fh = BADDR(al->al_Lock);
 
-	for (al = dl->dol_misc.dol_assign.dol_List;
-	     al && iofs->io_DosError == ERROR_OBJECT_NOT_FOUND;
-	     al = al->al_Next)
-	{
-	    fh = BADDR(al->al_Lock);
-	    iofs->IOFS.io_Device = fh->fh_Device;
-	    iofs->IOFS.io_Unit = fh->fh_Unit;
-	    DosDoIO(&iofs->IOFS);
-	}
+    	    iofs->IOFS.io_Device = fh->fh_Device;
+    	    iofs->IOFS.io_Unit = fh->fh_Unit;
+
+    	    DosDoIO(&iofs->IOFS);
+    	}
     }
 
     if (dl != NULL)
     {
-	if (dl->dol_Type == DLT_NONBINDING)
-	{
-	    UnLock(lock);
-	}
-
-	UnLockDosList(LDF_ALL | LDF_READ);
+    	if (dl->dol_Type == DLT_NONBINDING)
+    	{
+    	    UnLock(lock);
+    	}
+    
+    	UnLockDosList(LDF_ALL | LDF_READ);
     }
 
     if (volname != NULL)
-	FreeMem(volname, s1-name);
+    {
+        FreeMem(volname, s1-name);
+    }
 
     SetIoErr(iofs->io_DosError);
 
