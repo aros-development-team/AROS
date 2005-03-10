431e76abfd1bc8fd7a36de838b67d0d75c3400fd
jmj <jmj@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2005-03-10 08:10:52 +0000

    formatting cleanup
    git-svn-id: https://svn.aros.org/svn/aros/trunk@23074 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/namefromlock.c
+++ rom/dos/namefromlock.c
@@ -46,16 +46,6 @@ struct MyExAllData
 	!=0 if all went well, 0 in case of an error. IoErr() will
 	give additional information in that case.
 
-    NOTES
-
-    EXAMPLE
-
-    BUGS
-
-    SEE ALSO
-
-    INTERNALS
-
 *****************************************************************************/
 
 /*****************************************************************************
@@ -87,16 +77,6 @@ struct MyExAllData
 	!=0 if all went well, 0 in case of an error. IoErr() will
 	give additional information in that case.
 
-    NOTES
-
-    EXAMPLE
-
-    BUGS
-
-    SEE ALSO
-
-    INTERNALS
-
 *****************************************************************************/
 /*AROS alias NameFromFH NameFromLock */
 {
@@ -123,15 +103,19 @@ struct MyExAllData
 	
     if (length < 1)
     {
-    	SetIoErr(ERROR_LINE_TOO_LONG);
-	return DOSFALSE;
+        SetIoErr(ERROR_LINE_TOO_LONG);
+
+        return DOSFALSE;
     }
     
     /* Prepare I/O request. */
     iofs->IOFS.io_Message.mn_Node.ln_Type = NT_REPLYMSG;
     iofs->IOFS.io_Message.mn_ReplyPort	  = &me->pr_MsgPort;
     iofs->IOFS.io_Message.mn_Length	  = sizeof(struct IOFileSys);
-    iofs->IOFS.io_Device    	    	  = (fh == NULL) ? DOSBase->dl_NulHandler : fh->fh_Device;
+
+    iofs->IOFS.io_Device = (fh == NULL)
+                                ? DOSBase->dl_NulHandler
+                                : fh->fh_Device;
 
     /* Construct the name from top to bottom */
     name = buffer + length;
@@ -141,88 +125,95 @@ struct MyExAllData
     /* Loop over path */
     do
     {
-	/* Read name of current lock (into the user supplied buffer) */
-	iofs->IOFS.io_Unit  	    	    = curlock;
-	iofs->IOFS.io_Command	    	    = FSA_EXAMINE;
-	iofs->io_Union.io_EXAMINE.io_ead    = ead;
-	iofs->io_Union.io_EXAMINE.io_Size   = sizeof(stackead);
-	iofs->io_Union.io_EXAMINE.io_Mode   = ED_TYPE;
+    	/* Read name of current lock (into the user supplied buffer) */
+    	iofs->IOFS.io_Unit  	    	    = curlock;
+    	iofs->IOFS.io_Command	    	    = FSA_EXAMINE;
+    	iofs->io_Union.io_EXAMINE.io_ead    = ead;
+    	iofs->io_Union.io_EXAMINE.io_Size   = sizeof(stackead);
+    	iofs->io_Union.io_EXAMINE.io_Mode   = ED_TYPE;
+    
+    	DosDoIO(&iofs->IOFS);
 
-	DosDoIO(&iofs->IOFS);
-	error = iofs->io_DosError;
+    	error = iofs->io_DosError;
+    
+    	/* Move name to the top of the buffer. */
+    	if(!error)
+    	{
+    	    s1 = s2 = ead->ed_Name;
+    
+    	    while(*s2++)
+            {
+                ;
+            }
+    		
+    	    if(ead->ed_Type == ST_ROOT)
+    	    {
+                if (name > buffer)
+                {
+                    *--name=':';
+                }
+                else
+                {
+                    error = ERROR_LINE_TOO_LONG;
+                }
+    	    }
+    	    else if(oldlock != NULL)
+    	    {
+                if (name > buffer)
+                {		    
+                    *--name = '/';
+                }
+                else
+                {
+                    error = ERROR_LINE_TOO_LONG;
+                }
+    	    }
+    	    
+    	    if (!error)
+    	    {
+        		s2--;
+        
+        		if (name - (s2 - s1) >= buffer)
+        		{ 
+        	    	    while(s2 > s1)
+                        {
+                            *--name = *--s2;
+                        }
+        		}
+        		else
+        		{
+        	    	    error = ERROR_LINE_TOO_LONG;
+        		}
+    	    }
+    	    
+    	} /* if(!error) */
+    
+    	/* Read the parent's lock (if there is a parent) */
+    	if(!error && (ead->ed_Type != ST_ROOT))
+    	{
+    	    iofs->IOFS.io_Command   	    	= FSA_OPEN;
+    	    iofs->io_Union.io_OPEN.io_Filename	= "/";
+    	    iofs->io_Union.io_OPEN.io_FileMode	= 0;
 
-	/* Move name to the top of the buffer. */
-	if(!error)
-	{
-	    s1 = s2 = ead->ed_Name;
-
-	    while(*s2++)
-		;
-		
-	    if(ead->ed_Type == ST_ROOT)
-	    {
-	    	if (name > buffer)
-		{
-		    *--name=':';
-		}
-		else
-		{
-		    error = ERROR_LINE_TOO_LONG;
-		}
-	    }
-	    else if(oldlock != NULL)
-	    {
-	    	if (name > buffer)
-		{		    
-		    *--name = '/';
-		}
-		else
-		{
-		    error = ERROR_LINE_TOO_LONG;
-		}
-	    }
-	    
-	    if (!error)
-	    {
-		s2--;
-
-		if (name - (s2 - s1) >= buffer)
-		{ 
-	    	    while(s2 > s1)
-	    		*--name = *--s2;
-		}
-		else
-		{
-	    	    error = ERROR_LINE_TOO_LONG;
-		}
-	    }
-	    
-	} /* if(!error) */
-
-	/* Read the parent's lock (if there is a parent) */
-	if(!error && (ead->ed_Type != ST_ROOT))
-	{
-	    iofs->IOFS.io_Command   	    	= FSA_OPEN;
-	    iofs->io_Union.io_OPEN.io_Filename	= "/";
-	    iofs->io_Union.io_OPEN.io_FileMode	= 0;
-	    DosDoIO(&iofs->IOFS);
-	    
-	    curlock = iofs->IOFS.io_Unit;
-	    error = iofs->io_DosError;
-	}
-
-	/* Free the old lock if it was allocated by NameFromLock(). */
-	if(oldlock != NULL)
-	{
-	    iofs->IOFS.io_Unit	    = oldlock;
-	    iofs->IOFS.io_Command   = FSA_CLOSE;
-
-	    DosDoIO(&iofs->IOFS);
-	}
-	
-	oldlock = curlock;
-	
-    } while(!error && (ead->ed_Type != ST_ROOT));
+    	    DosDoIO(&iofs->IOFS);
+    	    
+    	    curlock = iofs->IOFS.io_Unit;
+    	    error = iofs->io_DosError;
+    	}
+    
+    	/* Free the old lock if it was allocated by NameFromLock(). */
+    	if(oldlock != NULL)
+    	{
+    	    iofs->IOFS.io_Unit	    = oldlock;
+    	    iofs->IOFS.io_Command   = FSA_CLOSE;
+    
+    	    DosDoIO(&iofs->IOFS);
+    	}
+    	
+    	oldlock = curlock;
+    	
+    }
+    while(!error && (ead->ed_Type != ST_ROOT));
 
     /* Move the name from the top to the bottom of the buffer. */
     
@@ -230,17 +221,19 @@ struct MyExAllData
     {
         UBYTE c, old_c = '\0';
 	
-	do
-	{
-	    c = *name++;
-	    if ((c != '/') || (old_c != ':'))
-	    {
-	        *buffer++ = c;
-	    }
-	    
-	    old_c = c;
-	    
-	} while (c);
+    	do
+    	{
+    	    c = *name++;
+
+    	    if ((c != '/') || (old_c != ':'))
+    	    {
+    	        *buffer++ = c;
+    	    }
+    	    
+    	    old_c = c;
+    	    
+    	}
+        while (c);
     }
      
     UnLock((BPTR)MKBADDR(fh));
@@ -249,7 +242,9 @@ struct MyExAllData
 
     SetIoErr(error);
 
-    return error ? DOSFALSE : DOSTRUE;
+    return error
+        ? DOSFALSE
+        : DOSTRUE;
     
     AROS_LIBFUNC_EXIT
     
