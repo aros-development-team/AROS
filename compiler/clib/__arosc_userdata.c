29ac2f57cf94752f0815a05ec553f683ad5d5b31
falemagn <falemagn@gmail.com>
2005-03-12 17:59:22 +0000

    Fall back to the static arosc_userdata if there's no userdata available in the context of the caller.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@23092 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/__arosc_userdata.c
+++ compiler/clib/__arosc_userdata.c
@@ -18,33 +18,39 @@ extern struct Library *aroscbase;
 
 struct arosc_userdata * __get_arosc_userdata(void)
 {
-    #ifndef AROSC_SHARED
-
-    static struct arosc_privdata acpd_static;
-
-    acpd_static.acpd_acud.acud_ctype_b       = &__ctype_b_array[128];
-    acpd_static.acpd_acud.acud_ctype_toupper = &__ctype_toupper_array[128];
-    acpd_static.acpd_acud.acud_ctype_tolower = &__ctype_tolower_array[128];
-
-    return &acpd_static.acpd_acud;
-
-    #else
-
-    struct arosc_privdata *acpd = GetIntETask(FindTask(NULL))->iet_acpd;
-    if (!acpd)
+    struct arosc_privdata *acpd = NULL;
+    
+    #ifdef AROSC_SHARED
+    struct Task *curtask = FindTask(NULL);
+    
+    acpd = GetIntETask(curtask)->iet_acpd;
+    
+    if (acpd == NULL)
     {
-        #warning FIXME: This is a quick hack to make old programs which use
+	#warning FIXME: This is a quick hack to make old programs which use
 	#warning        the libc from inside newly created tasks/processes
 	#warning        without going trough special libc routines
 	#warning        (yet to be written).
 
-        /* This might be NULL as well but... oh well...  */
-
-	acpd = GetIntETask(FindTask(NULL))->iet_acpd =
-	       GetIntETask(GetETask(FindTask(NULL))->et_Parent)->iet_acpd;
+        struct Task *parent = GetETask(curtask)->et_Parent;
+	
+	if (parent)
+            acpd = GetIntETask(parent)->iet_acpd;
     }
+    #endif 
 
+    if (acpd == NULL)
+    {
+        /* No acpd found? Fall back to the builtin one.  */
+        static struct arosc_privdata acpd_static =
+	{
+            .acpd_acud.acud_ctype_b       = &__ctype_b_array[128],
+            .acpd_acud.acud_ctype_toupper = &__ctype_toupper_array[128],
+            .acpd_acud.acud_ctype_tolower = &__ctype_tolower_array[128],
+	};
+
+        return &acpd_static.acpd_acud;
+    }
+   
     return &acpd->acpd_acud;
-
-    #endif
 }
