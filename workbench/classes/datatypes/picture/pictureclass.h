7e5a7a36ae1e19c2d3e8fad842bcab1b54c71bf2
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2005-03-31 23:48:56 +0000

    Converted ilbm.datatype to use %build_module genmf macro and the genmodule program. Converted method names to the format needed by genmodule (basename__methodname). I tried to keep code compiling on other platforms but this is not tested outside AROS.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@23127 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/classes/datatypes/picture/pictureclass.h
+++ workbench/classes/datatypes/picture/pictureclass.h
@@ -6,6 +6,9 @@
 #define	MIN(a,b) (((a) < (b)) ?	(a) : (b))
 #define	MAX(a,b) (((a) > (b)) ?	(a) : (b))
 
+#include <datatypes/pictureclass.h>
+#include <graphics/gfx.h>
+
 struct Picture_Data
 {
     /*
