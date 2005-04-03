3d6e72e5667eee05a87684fb1c245d8a1062ac01
jmj <jmj@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2005-04-03 13:24:42 +0000

    sorry, don't know what went wrong, but this somehow reached the wrong repo.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@23146 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- lps-jmj/src/org/openlaszlo/test/testseq.sh
+++
@@ -1,6 +0,0 @@
-# * P_LZ_COPYRIGHT_BEGIN ******************************************************
-# * Copyright 2001-2004 Laszlo Systems, Inc.  All Rights Reserved.            *
-# * Use is subject to license terms.                                          *
-# * P_LZ_COPYRIGHT_END ********************************************************
-
-java -cp . debugserver 5559 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
