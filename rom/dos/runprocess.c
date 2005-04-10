1e3eccd9f1facdeabdec72d1d6f55c95505f7fb7
sheutlin <sheutlin@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2005-04-10 17:20:43 +0000

    Declare the prototype of RunProcess() with the __stackparm macro because this functions assumes parameters are on the stack.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@23169 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/runprocess.c
+++ rom/dos/runprocess.c
@@ -32,6 +32,15 @@ extern void StackSwap (struct StackSwapStruct *, struct ExecBase *);
 #include <string.h>
 
 #define SysBase	    (DOSBase->dl_SysBase)
+LONG AROS_SLIB_ENTRY(RunProcess,Dos)
+(
+   struct Process        * proc,
+   struct StackSwapStruct * sss,
+   STRPTR          argptr,
+   ULONG        argsize,
+   LONG_FUNC       entry,
+   struct DosLibrary      * DOSBase
+) __stackparm;
 
 /**************************************************************************
 
