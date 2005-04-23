fc14091d9a15ef01cd0d579d0526944b2f93d595
apolkosnik <apolkosnik@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2005-04-23 19:53:32 +0000

    Let's see if that was the one breaking stdAlloc() on i386.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@23247 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/allocmem.c
+++ rom/exec/allocmem.c
@@ -304,7 +304,7 @@ static APTR stdAlloc(struct MemHeader *mh, ULONG byteSize, ULONG requirements)
                 {
                     /* Return the last bytes. */
                     p1->mc_Next=p2;
-                    mc=(struct MemChunk *)((UBYTE *)(p2+p2->mc_Bytes-byteSize));
+                    mc=(struct MemChunk *)((UBYTE *)p2+p2->mc_Bytes-byteSize);
                 }
                 else
                 {
