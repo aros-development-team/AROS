5f7359b9b3c895f59ac7d3508da5c48348e4757d
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2005-05-31 21:07:13 +0000

    New font support function which converts a colorfont planar bitmap data into a chunky pixel memory buffer.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@23338 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/graphics/fontsupport.h
+++ rom/graphics/fontsupport.h
@@ -8,8 +8,12 @@
     Lang: english
 */
 
-#define NUMCHARS(tf) ((tf->tf_HiChar - tf->tf_LoChar) + 2)
-#define CTF(x) ((struct ColorTextFont *)x)
+#ifndef GRAHPICS_TEXT_H
+#   include <graphics/text.h>
+#endif
+
+#define NUMCHARS(tf) 	((tf->tf_HiChar - tf->tf_LoChar) + 2)
+#define CTF(x)      	((struct ColorTextFont *)x)
 
 struct tfe_hashnode
 {
@@ -18,21 +22,30 @@ struct tfe_hashnode
     struct TextFontExtension	*ext;
     
     /* A bitmap describing the font */
-    OOP_Object *font_bitmap;
+    OOP_Object      	    	*font_bitmap;
+    /* Color font data in chunky format */
+    UBYTE   	    	    	*chunky_colorfont;
 };
+
+struct TextFontExtension_intern
+{
+    struct TextFontExtension  tfe;
+    struct tfe_hashnode      *hash;    
+};
+
+#define TFE_INTERN(tfe) (*(struct TextFontExtension_intern **)&tfe)
 
 extern struct tfe_hashnode *tfe_hashlookup(struct TextFont *tf, struct GfxBase *GfxBase);
 
-extern void tfe_hashadd(struct tfe_hashnode *hn
-		, struct TextFont *tf
-		, struct TextFontExtension 	*etf
-		, struct GfxBase *GfxBase);
+extern void tfe_hashadd(struct tfe_hashnode *hn, struct TextFont *tf,
+    	    	    	struct TextFontExtension *etf, struct GfxBase *GfxBase);
 
 
 extern void tfe_hashdelete(struct TextFont *tf, struct GfxBase *GfxBase);
 struct tfe_hashnode *tfe_hashnode_create(struct GfxBase *GfxBase);
 
 OOP_Object *fontbm_to_hiddbm(struct TextFont *font, struct GfxBase *GfxBase);
+UBYTE *colorfontbm_to_chunkybuffer(struct TextFont *font, struct GfxBase *GfxBase);
 	
 
 #endif /* FONTSUPPORT_H */
