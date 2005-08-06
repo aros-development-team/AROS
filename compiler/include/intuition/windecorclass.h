c7671c4b03182fb8917cb5b9329cacd3c05ceb85
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2005-08-06 20:27:18 +0000

    New methods/structs/attributes. Mostly for prop gadgets inside window border. Chaged flag defines "style" from "WD_???F_???" to "WDF_???_???".
    git-svn-id: https://svn.aros.org/svn/aros/trunk@23484 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/intuition/windecorclass.h
+++ compiler/include/intuition/windecorclass.h
@@ -17,10 +17,19 @@
 #   include <intuition/imageclass.h>
 #endif
 
+#ifndef INTUITION_INTUITION_H
+#   include <intuition/intuition.h>
+#endif
+
+#ifndef INTUITION_SCREENS_H
+#   include <intuition/screens.h>
+#endif
+
 /* Attributes for WINDECORCLASS */
 #define WDA_Dummy		    (TAG_USER + 0x22000)
-#define WDA_DrawInfo	    	    (WDA_Dummy + 1)
-#define WDA_Screen  	    	    (WDA_Dummy + 2)
+#define WDA_DrawInfo	    	    (WDA_Dummy + 1) 	    /* I.G */
+#define WDA_Screen  	    	    (WDA_Dummy + 2) 	    /* I.G */
+#define WDA_TrueColorOnly	    (WDA_Dummy + 3) 	    /* ..G */
 
 
 /* Methods for WINDECORCLASS */
@@ -33,6 +42,8 @@
 #define WDM_DRAW_WINBORDER  	    (WDM_Dummy + 5)
 #define WDM_DRAW_WINTITLE   	    (WDM_Dummy + 6)
 #define WDM_LAYOUT_BORDERGADGETS    (WDM_Dummy + 7)
+#define WDM_DRAW_BORDERPROPBACK     (WDM_Dummy + 8)
+#define WDM_DRAW_BORDERPROPKNOB     (WDM_Dummy + 9)
 
 struct wdpGetDefSizeSysImage
 {
@@ -40,9 +51,13 @@ struct wdpGetDefSizeSysImage
     STACKULONG	     wdp_Which;  	/* In: One of CLOSEIMAGE, SIZEIMAGE, ... */
     STACKULONG	     wdp_SysiSize;	/* In: lowres/medres/highres */
     struct TextFont *wdp_ReferenceFont; /* In: */
-    ULONG   	    *wdp_Width;  	/* Out */
-    ULONG   	    *wdp_Height; 	/* Out */
+    STACKULONG	    *wdp_Width;  	/* Out */
+    STACKULONG	    *wdp_Height; 	/* Out */
+    STACKULONG	     wdp_Flags;
+    
 };
+
+/* The sdpDrawSysImage struct in scrdecorclass.h must match this one!!! */
 
 struct wdpDrawSysImage
 {
@@ -53,7 +68,8 @@ struct wdpDrawSysImage
     STACKLONG	     wdp_Width;
     STACKLONG	     wdp_Height;
     STACKULONG	     wdp_Which;
-    STACKULONG	     wdp_State;  
+    STACKULONG	     wdp_State;
+    STACKULONG	     wdp_Flags;
 };
 
 struct wdpDrawWinBorder
@@ -70,6 +86,7 @@ struct wdpDrawWinTitle
     struct Window   *wdp_Window;
     struct RastPort *wdp_RPort;
     STACKULONG	     wdp_TitleAlign;
+    STACKULONG	     wdp_Flags;
 };
 
 struct wdpLayoutBorderGadgets
@@ -80,20 +97,44 @@ struct wdpLayoutBorderGadgets
     STACKULONG	     wdp_Flags;
 };
 
-/* WinDecor DrawWindowBorderFlags */ 
-#define  WD_DWBF_TOP_ONLY   	1   /* Draw top border only */
+struct wdpDrawBorderPropBack
+{
+    STACKULONG	      MethodID;
+    struct Window    *wdp_Window;
+    struct RastPort  *wdp_RPort;
+    struct Gadget    *wdp_Gadget;
+    struct Rectangle *wdp_RenderRect;
+    struct Rectangle *wdp_PropRect;
+    struct Rectangle *wdp_KnobRect;
+    STACKULONG	      wdp_Flags;
+};
+
+struct wdpDrawBorderPropKnob
+{
+    STACKULONG	      MethodID;
+    struct Window    *wdp_Window;
+    struct RastPort  *wdp_RPort;
+    struct Gadget    *wdp_Gadget;
+    struct Rectangle *wdp_RenderRect;
+    struct Rectangle *wdp_PropRect;
+    STACKULONG	      wdp_Flags;
+};
+
+/* WinDecor DrawWindowBorder Flags */ 
+#define  WDF_DWB_TOP_ONLY   	1   /* Draw top border only */
 
 /* WinDecor DrawWinTitle Title Align */
-
 #define WD_DWTA_LEFT 	    	0
 #define WD_DWTA_RIGHT 	    	1
 #define WD_DWTA_CENTER      	2
 
-/* WinDecor LayourBorderGadgetsFlags */
-#define WD_LBGF_INITIAL     	1   /* First time == During OpenWindow */
-#define WD_LBGF_SYSTEMGADGET	2   /* Is a system gadget (close/depth/zoom) */
-#define WD_LBGF_INGADLIST   	4   /* Gadget is already in window gadget list */
-#define WD_LBGF_MULTIPLE    	8   /* There may be multiple gadgets (linked
+/* WinDecor LayourBorderGadgets Flags */
+#define WDF_LBG_INITIAL     	1   /* First time == During OpenWindow */
+#define WDF_LBG_SYSTEMGADGET	2   /* Is a system gadget (close/depth/zoom) */
+#define WDF_LBG_INGADLIST   	4   /* Gadget is already in window gadget list */
+#define WDF_LBG_MULTIPLE    	8   /* There may be multiple gadgets (linked
                                        together through NextGadget. Follow it) */
+/* WinDecor DrawBorderPropKnob Flags */
+#define WDF_DBPK_HIT	    	1   /* Knob is hit / in use by user*/
 
 #endif /* INTUITION_WINDECORCLASS_H */
