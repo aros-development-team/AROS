b7f5e71fba5607886afcb20802d4a8c16d68b6a3
blippy <blippy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2005-09-11 16:35:08 +0000

    Changed pid from LONG to ULONG Annotated function further Doc improvement Introduced more variables to improve debugging
    git-svn-id: https://svn.aros.org/svn/aros/trunk@23606 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/mktemp.c
+++ compiler/clib/mktemp.c
@@ -8,6 +8,11 @@
 #include <proto/dos.h>
 #include <dos/dos.h>
 
+#include <exec/types.h>
+#include <assert.h>
+
+#include <stdio.h> /* FIXME: remove */
+
 /*****************************************************************************
 
     NAME */
@@ -28,11 +33,22 @@
 	Returns template.
 
     NOTES
-    	Template must end in "XXXXXX".
-
+    	Template must end in "XXXXXX" (i.e at least 6 X's).
+    	
+		Prior to this paragraph being created, mktemp() sometimes produced filenames
+		with '/' in them. AROS doesn't like that at all. Fortunately, the bug in this
+		function which produced it has been fixed. -- blippy
+		
+		For clarity, define the HEAD of the template to be the part before the tail,
+		and the TAIL to be the succession of X's. So in, T:temp.XXXXXX , the head is
+		T:temp. and the tail is XXXXXX .
+		
     EXAMPLE
 
     BUGS
+    	Cannot create more than 26 filenames for the same process id. This is because
+    	the "bumping" is only done to the first tail character - it should be
+    	generalised to bump more characters if necessary.
 
     SEE ALSO
 
@@ -42,30 +58,35 @@
 ******************************************************************************/
 { 
     AROS_GET_SYSBASE_OK
-    long  pid = (long)FindTask(0L);
+    ULONG  pid = (ULONG)FindTask(0L);
     char *c = template + strlen(template);
     BPTR  lock;
+    int remainder;
     
     while (*--c == 'X')
     {
-	*c = pid % 10 + '0';
-	pid /= 10;
+    	remainder = (int)(pid % 10L);
+    	assert(remainder>=0); 
+    	assert(remainder<10);
+		*c = remainder + '0';
+		pid /= 10L;
     }
     
-    c++;
+    c++; /* ... c now points to the 1st char of the template tail */
     
-    if (*c)
+    if (*c) /* FIXME: why would you get *c == 0 legitimately? */
     {
-	for(*c = 'A'; *c <= 'Z'; (*c)++)
-	{
+    	/* Loop over the first position of the tail, bumping it up as necessary */
+		for(*c = 'A'; *c <= 'Z'; (*c)++)
+		{
 	    if (!(lock = Lock(template, ACCESS_READ)))
 	    {
         	return template;
 	    }
 	    UnLock(lock);
+		}
+		*c = 0; /* FIXME: looks wrong. Why would you want to chop the tail? */
 	}
-	*c = 0;
-    }
     
     return template; 
  
