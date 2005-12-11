10ad731c2da6672effea4222b8b007c0f58b0674
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2005-12-11 11:57:46 +0000

    Converted the UNIX serial hidd class to use the genmodule tool via using %build_module in mmakefile.src. Most initialization code is now autogenerated, the names of the class methods are changed to the genmodule convention.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@23802 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-unix/hidd/serial/SerialClass.c
+++ arch/all-unix/hidd/serial/SerialClass.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2005, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: Serial hidd class implementation.
@@ -27,8 +27,8 @@
 
 /*** HIDDSerial::NewUnit() *********************************************************/
 
-static OOP_Object *hiddserial_newunit(OOP_Class *cl, OOP_Object *obj,
-				      struct pHidd_Serial_NewUnit *msg)
+OOP_Object *UXSer__Hidd_Serial__NewUnit(OOP_Class *cl, OOP_Object *obj,
+					struct pHidd_Serial_NewUnit *msg)
 {
   OOP_Object *su = NULL;
   struct HIDDSerialData * data = OOP_INST_DATA(cl, obj);
@@ -68,7 +68,7 @@ static OOP_Object *hiddserial_newunit(OOP_Class *cl, OOP_Object *obj,
   {
     struct TagItem tags[] =
     {
-#define csd CSD(cl->UserData)
+#define csd CSD(cl)
         {aHidd_SerialUnit_Unit, unitnum},
 #undef csd
 	{TAG_DONE		       }
@@ -88,8 +88,8 @@ static OOP_Object *hiddserial_newunit(OOP_Class *cl, OOP_Object *obj,
 
 /*** HIDDSerial::DisposeUnit() ****************************************************/
 
-static VOID hiddserial_disposeunit(OOP_Class *cl, OOP_Object *obj, 
-				   struct pHidd_Serial_DisposeUnit *msg)
+VOID UXSer__Hidd_Serial__DisposeUnit(OOP_Class *cl, OOP_Object *obj, 
+				     struct pHidd_Serial_DisposeUnit *msg)
 {
     OOP_Object * su = msg->unit;
     struct HIDDSerialData * data = OOP_INST_DATA(cl, obj);
@@ -118,110 +118,3 @@ static VOID hiddserial_disposeunit(OOP_Class *cl, OOP_Object *obj,
     
     ReturnVoid("HIDDSerial::DisposeUnit");
 }
-
-
-
-/*************************** Classes *****************************/
-
-#undef OOPBase
-#undef SysBase
-#undef UtilityBase
-
-#define SysBase     (csd->sysbase)
-#define OOPBase     (csd->oopbase)
-#define UtilityBase (csd->utilitybase)
-
-#define NUM_SERIALHIDD_METHODS moHidd_Serial_NumMethods
-
-OOP_Class *init_serialhiddclass (struct class_static_data *csd)
-{
-    OOP_Class *cl = NULL;
-    BOOL  ok  = FALSE;
-    
-    struct OOP_MethodDescr serialhidd_descr[NUM_SERIALHIDD_METHODS + 1] = 
-    {
-        {(IPTR (*)())hiddserial_newunit,         moHidd_Serial_NewUnit},
-        {(IPTR (*)())hiddserial_disposeunit,     moHidd_Serial_DisposeUnit},
-        {NULL, 0UL}
-    };
-    
-    
-    struct OOP_InterfaceDescr ifdescr[] =
-    {
-        {serialhidd_descr, IID_Hidd_Serial, NUM_SERIALHIDD_METHODS},
-        {NULL, NULL, 0}
-    };
-    
-    OOP_AttrBase MetaAttrBase = OOP_GetAttrBase(IID_Meta);
-        
-    struct TagItem tags[] =
-    {
-        { aMeta_SuperID,                (IPTR)CLID_Root},
-
-        { aMeta_InterfaceDescr,         (IPTR)ifdescr},
-        { aMeta_ID,                     (IPTR)CLID_Hidd_Serial},
-        { aMeta_InstSize,               (IPTR)sizeof (struct HIDDSerialData) },
-        {TAG_DONE, 0UL}
-    };
-    
-
-    EnterFunc(bug("    init_serialhiddclass(csd=%p)\n", csd));
-    
-    cl = OOP_NewObject(NULL, CLID_HiddMeta, tags);
-    D(bug("Class=%p\n", cl));
-    
-    if(cl)
-    {
-        D(bug("SerialHiddClass ok\n"));
-        cl->UserData = (APTR)csd;
-	
-        csd->serialunitclass = init_serialunitclass(csd);
-        D(bug("serialunitclass: %p\n", csd->serialunitclass));
-
-        if(csd->serialunitclass)
-        {
-            __IHidd_SerialUnitAB = OOP_ObtainAttrBase(IID_Hidd_SerialUnit);
-            if (NULL != __IHidd_SerialUnitAB) {
-        	D(bug("SerialUnitClass ok\n"));
-
-        	ok = TRUE;
-	    }
-        }
-    }
-
-    if(ok == FALSE)
-    {
-        free_serialhiddclass(csd);
-        cl = NULL;
-    }
-    else
-    {
-        OOP_AddClass(cl);
-    }
-
-    ReturnPtr("init_serialhiddclass", OOP_Class *, cl);
-}
-
-
-void free_serialhiddclass(struct class_static_data *csd)
-{
-    EnterFunc(bug("free_serialhiddclass(csd=%p)\n", csd));
-
-    if(csd)
-    {
-        OOP_RemoveClass(csd->serialhiddclass);
-	
-	free_serialunitclass(csd);
-	
-        if(csd->serialhiddclass)
-	{
-	    OOP_DisposeObject((OOP_Object *) csd->serialhiddclass);
-	}
-	
-        csd->serialhiddclass = NULL;
-    }
-
-    ReturnVoid("free_serialhiddclass");
-}
-
-
