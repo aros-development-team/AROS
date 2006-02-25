308dde1d71fda170dd229b00404f665c6e3d8267
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2006-02-25 05:56:55 +0000

    List of ATI supported cards
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24108 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/common/hidd.ati/ids.h
@@ -0,0 +1,23 @@
+#ifndef IDS_H_
+#define IDS_H_
+/*
+    Copyright © 2006, The AROS Development Team. All rights reserved.
+    $Id:$
+
+    Desc: ati.hidd initialization
+    Lang: English
+*/
+
+#include <exec/types.h>
+#include "ati.h"
+
+struct ATIDevice {
+    UWORD VendorID, ProductID __attribute__((packed));
+    CardType Type;
+    BOOL (*Init)(struct staticdata*);
+    BOOL masked_check;
+};
+
+extern const struct ATIDevice support[] __attribute__((section(".pci.supids")));
+
+#endif /*IDS_H_*/
