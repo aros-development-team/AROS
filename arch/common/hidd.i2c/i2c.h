fde2ab1c1e83e649848cc520d9555ea30f1efd2c
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2006-03-09 16:25:45 +0000

    The complete implementation of generic i2c bus class (needs to be extended with two HW-specific methods) and i2c device. Not yet tested.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24135 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/common/hidd.i2c/i2c.h
+++ arch/common/hidd.i2c/i2c.h
@@ -27,7 +27,9 @@
 /* Private data and structures unavailable outside the i2c base classes */
 
 typedef struct DevInstData {
+    struct SignalSemaphore  lock;
     OOP_Object  *driver;
+    STRPTR      name;
     UWORD       address;
     ULONG   HoldTime;
     ULONG   BitTimeout;
@@ -43,7 +45,9 @@ struct i2c_staticdata {
     
     struct SignalSemaphore  driver_lock;
     struct MinList          devices;
-    
+
+    STRPTR          name;
+        
     ULONG           HoldTime;
     ULONG           BitTimeout;
     ULONG           ByteTimeout;
@@ -57,6 +61,17 @@ struct i2c_staticdata {
 
     OOP_Class		*i2cClass;
     OOP_Class		*i2cDeviceClass;
+    
+    OOP_MethodID    mid_I2C_UDelay;
+    OOP_MethodID    mid_I2C_Start;
+    OOP_MethodID    mid_I2C_Stop;
+    OOP_MethodID    mid_I2C_PutBits;
+    OOP_MethodID    mid_I2C_GetBits;
+    OOP_MethodID    mid_I2C_PutByte;
+    OOP_MethodID    mid_I2C_GetByte;
+    OOP_MethodID    mid_I2C_Address;
+    OOP_MethodID    mid_I2C_WriteRead;
+
 };
 
 struct i2cbase {
@@ -73,6 +88,114 @@ struct i2cbase {
 
 #define METHOD(base, id, name) \
   base ## __ ## id ## __ ## name (OOP_Class *cl, OOP_Object *o, struct p ## id ## _ ## name *msg)
-  
+
+#define LOCK_DEV    ObtainSemaphore(&dev->lock);
+#define UNLOCK_DEV  ReleaseSemaphore(&dev->lock);
+
+#define LOCK_HW     ObtainSemaphore(&SD(cl)->driver_lock);
+#define UNLOCK_HW   ReleaseSemaphore(&SD(cl)->driver_lock);
+
+
+#define I2C_UDelay(__o, __delay) \
+    ({ \
+        struct pHidd_I2C_UDelay __p, *__m=&__p;    \
+        if (!SD(cl)->mid_I2C_UDelay)               \
+            SD(cl)->mid_I2C_UDelay = OOP_GetMethodID((STRPTR)IID_Hidd_I2C, moHidd_I2C_UDelay);    \
+        __p.mID   = SD(cl)->mid_I2C_UDelay;        \
+        __p.delay = (__delay);                        \
+        OOP_DoMethod((__o), (OOP_Msg)__m);          \
+    })
+
+#define I2C_Start(__o, __timeout) \
+    ({ \
+        struct pHidd_I2C_Start __p, *__m=&__p;    \
+        if (!SD(cl)->mid_I2C_Start)               \
+            SD(cl)->mid_I2C_Start = OOP_GetMethodID((STRPTR)IID_Hidd_I2C, moHidd_I2C_Start);    \
+        __p.mID   = SD(cl)->mid_I2C_Start;        \
+        __p.timeout = (__timeout);                        \
+        OOP_DoMethod((__o), (OOP_Msg)__m);          \
+    })
+
+#define I2C_Stop(__o, __device) \
+    ({ \
+        struct pHidd_I2C_Stop __p, *__m=&__p;    \
+        if (!SD(cl)->mid_I2C_Stop)               \
+            SD(cl)->mid_I2C_Stop = OOP_GetMethodID((STRPTR)IID_Hidd_I2C, moHidd_I2C_Stop);    \
+        __p.mID   = SD(cl)->mid_I2C_Stop;        \
+        __p.device = (__device);                        \
+        OOP_DoMethod((__o), (OOP_Msg)__m);          \
+    })
+
+#define I2C_PutBits(__o, __scl, __sda) \
+    ({ \
+        struct pHidd_I2C_PutBits __p, *__m=&__p;    \
+        if (!SD(cl)->mid_I2C_PutBits)               \
+            SD(cl)->mid_I2C_PutBits = OOP_GetMethodID((STRPTR)IID_Hidd_I2C, moHidd_I2C_PutBits);    \
+        __p.mID   = SD(cl)->mid_I2C_PutBits;        \
+        __p.scl   = (__scl);                        \
+        __p.sda   = (__sda);                        \
+        OOP_DoMethod((__o), (OOP_Msg)__m);          \
+    })
+
+#define I2C_PutByte(__o, __device, __byte) \
+    ({ \
+        struct pHidd_I2C_PutByte __p, *__m=&__p;    \
+        if (!SD(cl)->mid_I2C_PutByte)               \
+            SD(cl)->mid_I2C_PutByte = OOP_GetMethodID((STRPTR)IID_Hidd_I2C, moHidd_I2C_PutByte);    \
+        __p.mID   = SD(cl)->mid_I2C_PutByte;        \
+        __p.data   = (__byte);                        \
+        __p.device   = (__device);                        \
+        OOP_DoMethod((__o), (OOP_Msg)__m);          \
+    })
+
+#define I2C_GetByte(__o, __device, __byte, __last) \
+    ({ \
+        struct pHidd_I2C_GetByte __p, *__m=&__p;    \
+        if (!SD(cl)->mid_I2C_GetByte)               \
+            SD(cl)->mid_I2C_GetByte = OOP_GetMethodID((STRPTR)IID_Hidd_I2C, moHidd_I2C_GetByte);    \
+        __p.mID   = SD(cl)->mid_I2C_GetByte;        \
+        __p.data   = (__byte);                        \
+        __p.last   = (__last);                        \
+        __p.device   = (__device);                        \
+        OOP_DoMethod((__o), (OOP_Msg)__m);          \
+    })
+
+#define I2C_GetBits(__o, __scl, __sda) \
+    ({ \
+        struct pHidd_I2C_GetBits __p, *__m=&__p;    \
+        if (!SD(cl)->mid_I2C_GetBits)               \
+            SD(cl)->mid_I2C_GetBits = OOP_GetMethodID((STRPTR)IID_Hidd_I2C, moHidd_I2C_GetBits);    \
+        __p.mID   = SD(cl)->mid_I2C_GetBits;        \
+        __p.scl   = (__scl);                        \
+        __p.sda   = (__sda);                        \
+        OOP_DoMethod((__o), (OOP_Msg)__m);          \
+    })
+
+#define I2C_Address(__o, __device, __addr) \
+    ({ \
+        struct pHidd_I2C_Address __p, *__m=&__p;    \
+        if (!SD(cl)->mid_I2C_Address)               \
+            SD(cl)->mid_I2C_Address = OOP_GetMethodID((STRPTR)IID_Hidd_I2C, moHidd_I2C_Address);    \
+        __p.mID   = SD(cl)->mid_I2C_Address;        \
+        __p.address  = (__addr);                        \
+        __p.device   = (__device);                        \
+        OOP_DoMethod((__o), (OOP_Msg)__m);          \
+    })
+
+#define I2C_WriteRead(__o, __device, __wb, __wl, __rb, __rl) \
+    ({ \
+        struct pHidd_I2C_WriteRead __p, *__m=&__p;    \
+        if (!SD(cl)->mid_I2C_WriteRead)               \
+            SD(cl)->mid_I2C_WriteRead = OOP_GetMethodID((STRPTR)IID_Hidd_I2C, moHidd_I2C_WriteRead);    \
+        __p.mID   = SD(cl)->mid_I2C_WriteRead;        \
+        __p.device   = (__device);                        \
+        __p.writeBuffer = (__wb);   \
+        __p.writeLength = (__wl);   \
+        __p.readBuffer = (__rb);   \
+        __p.readLength = (__rl);   \
+        OOP_DoMethod((__o), (OOP_Msg)__m);          \
+    })
+
+
 #endif /* _I2C_H */
 
--- arch/common/hidd.i2c/i2cdeviceclass.c
+++ arch/common/hidd.i2c/i2cdeviceclass.c
@@ -21,77 +21,228 @@
 #define DEBUG 1
 #include <aros/debug.h>
 
-#ifdef HiddI2CDeviceAttrBase
+#undef HiddI2CAttrBase
 #undef HiddI2CDeviceAttrBase
-#endif // HiddPCIDeviceAttrBase
 
-#define	HiddI2CDeviceAttrBase	(SD(cl)->hiddI2CDeviceAB)
+#define HiddI2CAttrBase         (SD(cl)->hiddI2CAB)
+#define HiddI2CDeviceAttrBase   (SD(cl)->hiddI2CDeviceAB)
+#define HiddAttrBase            (SD(cl)->hiddAB)
+
 #define UtilityBase             (SD(cl)->utilitybase)
 
 BOOL METHOD(I2CDev, Hidd_I2CDevice, Read)
 {
+    tDevData *dev = (tDevData *)OOP_INST_DATA(cl, o);
+    BOOL r;
+    LOCK_DEV
+
+    r = I2C_WriteRead(dev->driver, o, NULL, 0, msg->readBuffer, msg->readLength);
+
+    UNLOCK_DEV
     
+    return r;
 }
 
 BOOL METHOD(I2CDev, Hidd_I2CDevice, ReadByte)
 {
+    tDevData *dev = (tDevData *)OOP_INST_DATA(cl, o);
+    BOOL r;
     
+    LOCK_DEV
+
+    r = I2C_WriteRead(dev->driver, o, &msg->subaddr, 1, msg->data, 1);
+
+    UNLOCK_DEV   
+    
+    return r;
 }
 
 BOOL METHOD(I2CDev, Hidd_I2CDevice, ReadBytes)
 {
+    tDevData *dev = (tDevData *)OOP_INST_DATA(cl, o);
+    BOOL r;
     
+    LOCK_DEV
+
+    r = I2C_WriteRead(dev->driver, o, &msg->subaddr, 1, msg->data, msg->length);
+
+    UNLOCK_DEV    
+    
+    return r;
 }
 
 BOOL METHOD(I2CDev, Hidd_I2CDevice, ReadStatus)
 {
+    tDevData *dev = (tDevData *)OOP_INST_DATA(cl, o);
+    BOOL r;
     
+    LOCK_DEV
+
+    r = I2C_WriteRead(dev->driver, o, NULL, 0, msg->status, 1);
+
+    UNLOCK_DEV
+    
+    return r;
 }
 
 BOOL METHOD(I2CDev, Hidd_I2CDevice, ReadWord)
 {
+    tDevData *dev = (tDevData *)OOP_INST_DATA(cl, o);
+    BOOL r;
+    UBYTE buff[2];
     
+    LOCK_DEV
+
+    if ((r = I2C_WriteRead(dev->driver, o, &msg->subaddr, 1, buff, 2)))
+    {
+        *msg->data = (buff[0] << 8) | buff[1];
+    }
+
+    UNLOCK_DEV
+    
+    return r;
 }
 
 BOOL METHOD(I2CDev, Hidd_I2CDevice, Write)
 {
+    tDevData *dev = (tDevData *)OOP_INST_DATA(cl, o);
+    BOOL r;
+    LOCK_DEV
+
+    r = I2C_WriteRead(dev->driver, o, msg->writeBuffer, msg->writeLength, NULL, 0);
+
+    UNLOCK_DEV
     
+    return r;
 }
 
 BOOL METHOD(I2CDev, Hidd_I2CDevice, WriteByte)
 {
+    tDevData *dev = (tDevData *)OOP_INST_DATA(cl, o);
+    BOOL r;
+    UBYTE buff[2];
     
+    buff[0] = msg->subaddr;
+    buff[1] = msg->data;
+    
+    LOCK_DEV
+
+    r = I2C_WriteRead(dev->driver, o, buff, 2, NULL, 0);
+
+    UNLOCK_DEV
+    
+    return r;
 }
 
 BOOL METHOD(I2CDev, Hidd_I2CDevice, WriteBytes)
 {
+    tDevData *dev = (tDevData *)OOP_INST_DATA(cl, o);
+    BOOL r = TRUE;
+    ULONG nWrite = msg->length;
+    UBYTE *WriteBuffer = msg->data;
     
+    LOCK_DEV
+
+    if (nWrite > 0)
+    {
+        if ((r = I2C_Address(dev->driver, o, dev->address & ~1)))
+        {
+            if ((r = I2C_PutByte(dev->driver, o, msg->subaddr)))
+            {
+                for (; nWrite > 0; WriteBuffer++, nWrite--)
+                    if (!(r = I2C_PutByte(dev->driver, o, *WriteBuffer)))
+                        break;
+            }
+            
+            I2C_Stop(dev->driver, o);
+        }
+    }
+
+    UNLOCK_DEV
+    
+    return r;
 }
 
 BOOL METHOD(I2CDev, Hidd_I2CDevice, WriteWord)
 {
+    tDevData *dev = (tDevData *)OOP_INST_DATA(cl, o);
+    BOOL r;
+    UBYTE buff[3];
     
+    buff[0] = msg->subaddr;
+    buff[1] = msg->data >> 8;
+    buff[2] = msg->data & 0xff;
+    
+    LOCK_DEV
+
+    r = I2C_WriteRead(dev->driver, o, buff, 2, NULL, 0);
+
+    UNLOCK_DEV
+    
+    return r;
 }
 
 BOOL METHOD(I2CDev, Hidd_I2CDevice, WriteVec)
 {
+    tDevData *dev = (tDevData *)OOP_INST_DATA(cl, o);
+    BOOL r = TRUE;
+    int s = 0;
+    ULONG nValues = msg->length;
+    UBYTE *vec = msg->data;
     
+    LOCK_DEV
+
+    if (nValues > 0)
+    {
+        for (; nValues > 0; nValues--, vec+=2)
+        {
+            if (!(r = I2C_Address(dev->driver, o, dev->address & ~1)))
+                break;
+            
+            s++;
+            
+            if (!(r = I2C_PutByte(dev->driver, o, vec[0])))
+                break;
+            
+            if (!(r = I2C_PutByte(dev->driver, o, vec[1])))
+                break;
+        }
+        
+        if (s > 0) I2C_Stop(dev->driver, o);
+    }
+
+    UNLOCK_DEV
+    
+    return r;
 }
 
 /*** Root */
+
 OOP_Object *METHOD(I2CDev, Root, New)
 {
     o = (OOP_Object *)OOP_DoSuperMethod(cl, o, (OOP_Msg) msg);
     if (o)
     {
-        struct TagItem *tags, *tag;
+        struct TagItem *tag;
+        const struct TagItem *tags = msg->attrList;
         tDevData *dev = (tDevData *)OOP_INST_DATA(cl, o);
         OOP_Object *driver = NULL;
-        UWORD address;
-                
+        UWORD address = 0;
+
+        InitSemaphore(&dev->lock);
+
+        dev->HoldTime = -1;
+        dev->AcknTimeout = -1;
+        dev->BitTimeout = -1;
+        dev->ByteTimeout = -1;
+        dev->RiseFallTime = -1;
+        dev->StartTimeout = -1;
+
+        dev->name = (STRPTR)"dev?";
+
         tags=msg->attrList;
     
-        while((tag = NextTagItem((struct TagItem **)&tags)))
+        while((tag = NextTagItem(&tags)))
         {
             ULONG idx;
         
@@ -107,6 +258,10 @@ OOP_Object *METHOD(I2CDev, Root, New)
                     case aoHidd_I2CDevice_Address:
                         dev->address = tag->ti_Data;
                         address = dev->address;
+                        break;
+
+                    case aoHidd_I2CDevice_Name:
+                        dev->name = (STRPTR)tag->ti_Data;
                         break;
         
                     case aoHidd_I2CDevice_HoldTime:
@@ -136,14 +291,51 @@ OOP_Object *METHOD(I2CDev, Root, New)
             }
         }
         
-        if (driver)
+        if (driver && address)
         {
+            IPTR val;
+            
+            if (dev->AcknTimeout == -1)
+            {
+                OOP_GetAttr(driver, aHidd_I2C_AcknTimeout, &val);
+                dev->AcknTimeout = val;
+            }
+            
+            if (dev->BitTimeout == -1)
+            {
+                OOP_GetAttr(driver, aHidd_I2C_BitTimeout, &val);
+                dev->BitTimeout = val;
+            }
+            
+            if (dev->ByteTimeout == -1)
+            {
+                OOP_GetAttr(driver, aHidd_I2C_ByteTimeout, &val);
+                dev->ByteTimeout = val;
+            }
+            
+            if (dev->HoldTime == -1)
+            {
+                OOP_GetAttr(driver, aHidd_I2C_HoldTime, &val);
+                dev->HoldTime = val;
+            }
+            
+            if (dev->RiseFallTime == -1)
+            {
+                OOP_GetAttr(driver, aHidd_I2C_RiseFallTime, &val);
+                dev->RiseFallTime = val;
+            }
+            
+            if (dev->StartTimeout == -1)
+            {
+                OOP_GetAttr(driver, aHidd_I2C_StartTimeout, &val);
+                dev->StartTimeout = val;
+            }          
         }
         else
         {
-             OOP_MethodID disp_mid = OOP_GetMethodID(IID_Root, moRoot_Dispose);
+             OOP_MethodID disp_mid = OOP_GetMethodID((STRPTR)IID_Root, moRoot_Dispose);
              OOP_CoerceMethod(cl, o, (OOP_Msg) &disp_mid);
-                            
+             o = NULL;
         }
     }
 
@@ -201,9 +393,8 @@ void METHOD(I2CDev, Root, Set)
 {
     tDevData *dev = (tDevData *)OOP_INST_DATA(cl, o);
     ULONG idx;
-    struct TagItem *tags, *tag;
-    
-    tags = msg->attrList;
+    struct TagItem *tag;
+    const struct TagItem *tags = msg->attrList;
     
     while ((tag = NextTagItem(&tags)))
     {
