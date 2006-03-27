d71c1a600b25fd788b8edd12eba58166a0f7322c
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2006-03-27 22:21:44 +0000

    Trivial fixes to command descriptions.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24241 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/shellcommands/CD.c
+++ workbench/c/shellcommands/CD.c
@@ -2,7 +2,7 @@
     Copyright © 1995-2001, The AROS Development Team. All rights reserved.
     $Id$
 
-    Desc: Cd CLI command
+    Desc: CD CLI command
     Lang: English
 */
 /*****************************************************************************
