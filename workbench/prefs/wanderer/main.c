e6f2621e7cda0c91e1ac3a50392a1619a99d4d2f
mattrust <mrustler@gmx.de>
2006-03-29 03:04:51 +0000

    Deleted all .cvsignore files
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24246 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/prefs/wanderer/main.c
+++ workbench/prefs/wanderer/main.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 2004, The AROS Development Team. All rights reserved.
+    Copyright © 2004-2006, The AROS Development Team. All rights reserved.
     This file is part of the Wanderer Preferences program, which is distributed
     under the terms of version 2 of the GNU General Public License.
     
@@ -8,6 +8,7 @@
 
 #define MUIMASTER_YES_INLINE_STDARG
 
+#include <aros/debug.h>
 #include <proto/intuition.h>
 #include <proto/muimaster.h>
 #include <libraries/mui.h>
@@ -16,6 +17,8 @@
 #include "locale.h"
 #include "wpeditor.h"
 
+#define VERSIONSTR "$VER: Wanderer 1.1 (18.02.2006)"
+
 int main(void)
 {
     Object *application, *window;
@@ -23,6 +26,12 @@ int main(void)
     Locale_Initialize();
     
     application = ApplicationObject,
+	MUIA_Application_Title, (IPTR)_(MSG_NAME),
+	MUIA_Application_Version, (IPTR)VERSIONSTR,
+	MUIA_Application_Copyright, (IPTR)"Copyright © 1995-2006, The AROS Development Team",
+	MUIA_Application_Author, (IPTR)"The AROS Development Team",
+	MUIA_Application_Base, (IPTR)"WANDERERPREF",
+        MUIA_Application_SingleTask, TRUE,
         SubWindow, (IPTR) (window = SystemPrefsWindowObject,
             WindowContents, (IPTR) WPEditorObject,
             End,
@@ -36,6 +45,10 @@ int main(void)
         SET(window, MUIA_Window_Open, FALSE);
         
         MUI_DisposeObject(application);
+    }
+    else
+    {
+	D(bug("wandererprefs: Can't create application"));
     }
 
     Locale_Deinitialize();
