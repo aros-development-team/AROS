6baecce96b4e7da904073f9412014848fd1635d4
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2006-04-05 20:37:25 +0000

    pcipc.hidd now uses %build_module for the build. The code has switched to the initialization provided by genmodule and autoopening of libraries.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24266 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-pc/Drivers/pcipc/driverclass.c
+++ arch/i386-pc/Drivers/pcipc/driverclass.c
@@ -1,10 +1,12 @@
 /*
-    Copyright © 2004, The AROS Development Team. All rights reserved.
+    Copyright © 2004-2006, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: PCI direct driver for i386 native.
     Lang: English
 */
+
+#define __OOP_NOATTRBASES__
 
 #include <exec/types.h>
 #include <hidd/pci.h>
@@ -16,13 +18,15 @@
 #include <proto/utility.h>
 #include <proto/oop.h>
 
+#include <aros/symbolsets.h>
+
 #include "pci.h"
 
 #define DEBUG 1
 #include <aros/debug.h>
 
-#undef SysBase
-#define SysBase (PSD(cl)->sysbase)
+#undef HiddPCIDriverAttrBase
+#undef HiddAttrBase
 
 #define	HiddPCIDriverAttrBase	(PSD(cl)->hiddPCIDriverAB)
 #define HiddAttrBase		(PSD(cl)->hiddAB)
@@ -42,7 +46,7 @@ typedef union _pcicfg
     We overload the New method in order to introduce the Hidd Name and
     HardwareName attributes.
 */
-static OOP_Object *pcidriver_new(OOP_Class *cl, OOP_Object *o, struct pRoot_New *msg)
+OOP_Object *PCPCI__Root__New(OOP_Class *cl, OOP_Object *o, struct pRoot_New *msg)
 {
     struct pRoot_New mymsg;
     
@@ -82,8 +86,8 @@ static inline void outl(ULONG val, UWORD port)
     asm volatile ("outl %0,%w1"::"a"(val),"Nd"(port));
 }
 
-static ULONG pcidriver_RL(OOP_Class *cl, OOP_Object *o, 
-    struct pHidd_PCIDriver_ReadConfigLong *msg)
+ULONG PCPCI__Hidd_PCIDriver__ReadConfigLong(OOP_Class *cl, OOP_Object *o, 
+					    struct pHidd_PCIDriver_ReadConfigLong *msg)
 {
     ULONG orig,temp;
     
@@ -97,26 +101,26 @@ static ULONG pcidriver_RL(OOP_Class *cl, OOP_Object *o,
     return temp;
 }
 
-static UWORD pcidriver_RW(OOP_Class *cl, OOP_Object *o, 
-    struct pHidd_PCIDriver_ReadConfigWord *msg)
+UWORD PCPCI__Hidd_PCIDriver__ReadConfigWord(OOP_Class *cl, OOP_Object *o, 
+					    struct pHidd_PCIDriver_ReadConfigWord *msg)
 {
     pcicfg temp;
 
-    temp.ul = pcidriver_RL(cl, o, (struct pHiddPCIDriver_ReadConfigLong *)msg);
+    temp.ul = PCPCI__Hidd_PCIDriver__ReadConfigLong(cl, o, (struct pHidd_PCIDriver_ReadConfigLong *)msg);
     return temp.uw[(msg->reg&2)>>1];
 }
 
-static UBYTE pcidriver_RB(OOP_Class *cl, OOP_Object *o, 
-    struct pHidd_PCIDriver_ReadConfigByte *msg)
+UBYTE PCPCI__Hidd_PCIDriver__ReadConfigByte(OOP_Class *cl, OOP_Object *o, 
+					    struct pHidd_PCIDriver_ReadConfigByte *msg)
 {
     pcicfg temp;
 
-    temp.ul = pcidriver_RL(cl, o, (struct pHiddPCIDriver_ReadConfigLong *)msg); 
+    temp.ul = PCPCI__Hidd_PCIDriver__ReadConfigLong(cl, o, (struct pHidd_PCIDriver_ReadConfigLong *)msg); 
     return temp.ub[msg->reg & 3];
 }
 
-static void pcidriver_WL(OOP_Class *cl, OOP_Object *o,
-    struct pHidd_PCIDriver_WriteConfigLong *msg)
+void PCPCI__Hidd_PCIDriver__WriteConfigLong(OOP_Class *cl, OOP_Object *o,
+					    struct pHidd_PCIDriver_WriteConfigLong *msg)
 {
     ULONG orig;
     
@@ -130,108 +134,50 @@ static void pcidriver_WL(OOP_Class *cl, OOP_Object *o,
 
 /* Class initialization and destruction */
 
-#undef OOPBase
-#undef SysBase
-#undef UtilityBase
-
-#define SysBase	    (psd->sysbase)
-#define OOPBase	    (psd->oopbase)
-#define UtilityBase (psd->utilitybase)
-
-void free_pcipcdriverclass(struct pci_staticdata *psd, OOP_Class *cl)
+AROS_SET_LIBFUNC(PCPCI_InitClass, LIBBASETYPE, LIBBASE)
 {
-    D(bug("PCI: Dummy Driver Class destruction\n"));
+    AROS_SET_LIBFUNC_INIT
     
-    if (psd)
-    {
-	OOP_RemoveClass(cl);
-
-	if (cl)
-	    OOP_DisposeObject((OOP_Object *)cl);
-	
-	OOP_ReleaseAttrBase(IID_Hidd_PCIDriver);
-	OOP_ReleaseAttrBase(IID_Hidd);
-    }
-}
-	
-#define _NUM_ROOT_METHODS	1
-#define _NUM_PCIDRIVER_METHODS	4
-
-OOP_Class *init_pcipcdriverclass(struct pci_staticdata *psd)
-{
-    OOP_Class *cl = NULL;
-    OOP_Object *pci = NULL;
-
-    struct OOP_MethodDescr root_descr[_NUM_ROOT_METHODS + 1] = 
-    {
-	{ OOP_METHODDEF(pcidriver_new), moRoot_New },
-	{ NULL, 0UL }
-    };
-
-    struct OOP_MethodDescr pcidriver_descr[_NUM_PCIDRIVER_METHODS + 1] =
-    {
-	{ OOP_METHODDEF(pcidriver_RB),  moHidd_PCIDriver_ReadConfigByte },
-	{ OOP_METHODDEF(pcidriver_RW),  moHidd_PCIDriver_ReadConfigWord },
-	{ OOP_METHODDEF(pcidriver_RL),  moHidd_PCIDriver_ReadConfigLong },
-	{ OOP_METHODDEF(pcidriver_WL),  moHidd_PCIDriver_WriteConfigLong },
-	{ NULL, 0UL }
-    };
-
-    struct OOP_InterfaceDescr ifdescr[] =
-    {
-	{ root_descr,	    IID_Root,		_NUM_ROOT_METHODS },
-	{ pcidriver_descr,  IID_Hidd_PCIDriver,	_NUM_PCIDRIVER_METHODS },
-	{ NULL, NULL, 0UL }
-    };
-
-    OOP_AttrBase MetaAttrBase = OOP_ObtainAttrBase(IID_Meta);
-
-    struct TagItem tags[] =
-    {
-	{ aMeta_SuperID,	(IPTR)CLID_Hidd_PCIDriver },
-	{ aMeta_InterfaceDescr,	(IPTR)ifdescr },
-	{ aMeta_InstSize,	(IPTR)0 },
-	{ TAG_DONE, 0UL }
-    };
-
+    OOP_Object *pci;
+    
     D(bug("PCPCI: Driver initialization\n"));
 
-    if (MetaAttrBase)
+    struct pHidd_PCI_AddHardwareDriver msg;
+    
+    LIBBASE->psd.hiddPCIDriverAB = OOP_ObtainAttrBase(IID_Hidd_PCIDriver);
+    LIBBASE->psd.hiddAB = OOP_ObtainAttrBase(IID_Hidd);
+    if (LIBBASE->psd.hiddPCIDriverAB == 0 || LIBBASE->psd.hiddAB == 0)
     {
-	cl = OOP_NewObject(NULL, CLID_HiddMeta, tags);
-	if (cl)
-	{
-	    cl->UserData = (APTR)psd;
-	    psd->hiddPCIDriverAB = OOP_ObtainAttrBase(IID_Hidd_PCIDriver);
-	    psd->hiddAB = OOP_ObtainAttrBase(IID_Hidd);
-
-	    if (psd->hiddPCIDriverAB)
-	    {
-		struct pHidd_PCI_AddHardwareDriver msg;
-		
-		OOP_AddClass(cl);
-		D(bug("PCPCI: Driver Class OK\n"));
-		
-		msg.driverClass = cl;
-		msg.mID = OOP_GetMethodID(IID_Hidd_PCI, moHidd_PCI_AddHardwareDriver);
-		D(bug("PCPCI: Adding Driver to main the class OK\n"));
-
-		pci = OOP_NewObject(NULL, CLID_Hidd_PCI, NULL);
-		OOP_DoMethod(pci, (OOP_Msg)&msg);
-		OOP_DisposeObject(pci);
-		psd->driverClass = cl;
-	    }
-	    else
-	    {
-		free_pcipcdriverclass(psd, cl);
-		cl = NULL;
-	    }
-	}
-	OOP_ReleaseAttrBase(IID_Meta);
+	D(bug("PCPCI: ObtainAttrBases failed\n"));
+	return FALSE;
     }
+    
+    msg.driverClass = LIBBASE->psd.driverClass;
+    msg.mID = OOP_GetMethodID(IID_Hidd_PCI, moHidd_PCI_AddHardwareDriver);
+    D(bug("PCPCI: Adding Driver to main the class OK\n"));
 
-    D(bug("PCPCI: Driver ClassPtr = %x\n", cl));
+    pci = OOP_NewObject(NULL, CLID_Hidd_PCI, NULL);
+    OOP_DoMethod(pci, (OOP_Msg)&msg);
+    OOP_DisposeObject(pci);
 
-    return cl;
+    D(bug("PCPCI: All OK\n"));
+
+    return TRUE;
+    
+    AROS_SET_LIBFUNC_EXIT
 }
 
+AROS_SET_LIBFUNC(PCPCI_ExpungeClass, LIBBASETYPE, LIBBASE)
+{
+    AROS_SET_LIBFUNC_INIT
+
+    D(bug("PCPCI: Class destruction\n"));
+    
+    OOP_ReleaseAttrBase(IID_Hidd_PCIDriver);
+    OOP_ReleaseAttrBase(IID_Hidd);
+    
+    AROS_SET_LIBFUNC_EXIT
+}
+	
+ADD2INITLIB(PCPCI_InitClass, 0)
+ADD2EXPUNGELIB(PCPCI_ExpungeClass, 0)
--- arch/i386-pc/Drivers/pcipc/pci.h
+++ arch/i386-pc/Drivers/pcipc/pci.h
@@ -9,9 +9,6 @@
 
 #include <dos/bptr.h>
 
-#include <aros/libcall.h>
-#include <aros/asmcall.h>
-
 #include <oop/oop.h>
 
 #include <aros/arossupportbase.h>
@@ -19,18 +16,7 @@
 
 #include LC_LIBDEFS_FILE
 
-extern UBYTE LIBEND;
-
-AROS_UFP3(struct pcibase *, Pcipc_init,
-    AROS_UFHA(struct pcibase *, pcibase, D0),
-    AROS_UFHA(BPTR, slist, A0),
-    AROS_UFHA(struct ExecBase *, SysBase, A6));
-
 struct pci_staticdata {
-    struct ExecBase	*sysbase;
-    struct Library	*oopbase;
-    struct Library	*utilitybase;
-    
     OOP_AttrBase	hiddPCIDriverAB;
     OOP_AttrBase	hiddAB;
 
@@ -39,21 +25,17 @@ struct pci_staticdata {
 
 struct pcibase {
     struct Library	    LibNode;
-    struct pci_staticdata   *psd;
+    struct pci_staticdata   psd;
     struct ExecBase	    *sysBase;
     BPTR		    slist;
 };
-
-OOP_Class *init_pcipcdriverclass(struct pci_staticdata *);
-VOID free_pcipcdriverclass(struct pci_staticdata *, OOP_Class *);
-
 
 #define PCI_AddressPort	0x0cf8
 #define PCI_DataPort	0x0cfc
 
 #define BASE(lib) ((struct pcibase*)(lib))
 
-#define PSD(cl) ((struct pci_staticdata*)cl->UserData)
+#define PSD(cl) (&((struct pcibase*)cl->UserData)->psd)
 
 #endif /* _PCI_H */
 
