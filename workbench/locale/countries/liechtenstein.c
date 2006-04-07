9fec1c11ca0dff779ab27466544fff9cb6867f8b
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2006-04-07 20:53:52 +0000

    New country files by Stefan Haubenthal.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24280 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/locale/countries/liechtenstein.c
@@ -0,0 +1,71 @@
+/*
+    Copyright © 1995-2005, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: Country data for Liechtenstein
+    Author: Stefan Haubenthal <polluks@sdf.lonestar.org>
+*/
+
+#include <libraries/locale.h>
+#include <prefs/locale.h>
+
+/* schweiz.country: based on this file on Amiga Developer CD 2.1: 
+   NDK/NDK_3.5/Examples/Locale/Countries/make_country_files.c */
+   
+struct CountryPrefs liechtensteinPrefs =
+{
+    /* Reserved */
+    { 0, 0, 0, 0 },
+
+    /* Country code (licence plate number), telephone code, measuring system */
+    MAKE_ID('F','L',0,0), 423, MS_ISO,
+
+    /* Date time format, date format, time format */
+    "%A, %e. %B %Y %H:%M:%S",
+    "%A, %e. %B %Y",
+    "%H:%M:%S",
+
+    /* Short datetime, short date, short time formats */
+    "%e.%m.%Y %H:%M:%S",
+    "%e.%m.%Y",
+    "%H:%M",
+
+    /* Decimal point, group separator, frac group separator */
+    ".", "'", "",
+
+    /* For grouping rules, see <libraries/locale.h> */
+
+    /* Grouping, Frac Grouping */
+    { 3 }, { 0 },
+
+    /* Mon dec pt, mon group sep, mon frac group sep */
+    ".", "'", "'",
+
+    /* Mon Grouping, Mon frac grouping */
+    { 3 }, { 3 },
+
+    /* Mon Frac digits, Mon IntFrac digits, then number of digits in
+       the fractional part of the money value. Most countries that
+       use dollars and cents, would have 2 for this value
+
+       (As would many of those you don't).
+    */
+    2, 2,
+
+    /* Currency symbol, Small currency symbol */
+    "SFr.", "Rp.",
+
+    /* Int CS, this is the ISO 4217 symbol, followed by the character to
+       separate that symbol from the rest of the money. (\x00 for none).
+    */
+    "CHF",
+
+    /* Mon +ve sign, +ve space sep, +ve sign pos, +ve cs pos */
+    "", SS_NOSPACE, SP_PREC_ALL, CSP_PRECEDES,
+
+    /* Mon -ve sign, -ve space sep, -ve sign pos, -ve cs pos */
+    "-", SS_NOSPACE, SP_SUCC_CURR, CSP_PRECEDES,
+
+    /* Calendar type */
+    CT_7MON
+};
