3b856347700b811e5cf04a676dc3c23119881a34
mattrust <mrustler@gmx.de>
2006-06-17 21:11:05 +0000

    Errors removed.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24552 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/classes/levelmeter.h
+++ workbench/libs/muimaster/classes/levelmeter.h
@@ -2,7 +2,7 @@
 #define _MUI_CLASSES_LEVELMETER_H
 
 /*
-    Copyright © 2002-2003, The AROS Development Team. All rights reserved.
+    Copyright © 2002-2006, The AROS Development Team. All rights reserved.
     $Id$
 */
 
@@ -13,7 +13,7 @@
 //#define MUIB_Levelmeter   	(MUIB_ZUNE | 0x????)  
 
 /*** Attributes *************************************************************/
-#define MUIA_Levelmeter_Label	(MUIB_MUI | 0x00420dd5 /* V11 isg STRPTR */
+#define MUIA_Levelmeter_Label	(MUIB_MUI | 0x00420dd5) /* V11 isg STRPTR */
 
 
 extern const struct __MUIBuiltinClass _MUI_Levelmeter_desc; /* PRIV */
--- workbench/libs/muimaster/classes/popobject.h
+++ workbench/libs/muimaster/classes/popobject.h
@@ -2,7 +2,7 @@
 #define _MUI_CLASSES_POPOBJECT_H
 
 /*
-    Copyright © 2002-2003, The AROS Development Team. All rights reserved.
+    Copyright © 2002-2006, The AROS Development Team. All rights reserved.
     $Id$
 */
 
@@ -11,7 +11,7 @@
 #define MUIC_Popobject              "Popobject.mui"
 
 /*** Identifier base ********************************************************/
-#define MUIB_Popobject              (MUIB_ZUNE | 0x00002400)  MUIB_Popobject
+#define MUIB_Popobject              (MUIB_ZUNE | 0x00002400)
 
 /*** Attributes *************************************************************/
 #define MUIA_Popobject_Follow       (MUIB_MUI|0x00424cb5) /* MUI: V7  isg BOOL          */
