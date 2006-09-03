e4e1b4a68857976cea64a233d571f2207dac4ce1
mattrust <mrustler@gmx.de>
2006-09-03 09:13:21 +0000

    Localization, French catalog and Icon by Olivier Adam. Added German catalog.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24732 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/utilities/Snoopy/locale.c
@@ -0,0 +1,54 @@
+/*
+    Copyright © 1995-2004, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <exec/types.h>
+#include <proto/locale.h>
+
+#define CATCOMP_ARRAY
+#include "strings.h"
+
+#define CATALOG_NAME    "System/Utilities/Snoopy.catalog"
+#define CATALOG_VERSION 1
+
+/*** Variables **************************************************************/
+struct Catalog *catalog;
+
+
+/*** Functions **************************************************************/
+/* Main *********************************************************************/
+CONST_STRPTR MSG(ULONG id)
+{
+    if(LocaleBase != NULL && catalog != NULL)
+    {
+        return GetCatalogStr(catalog, id, CatCompArray[id].cca_Str);
+    } 
+    else 
+    {
+        return CatCompArray[id].cca_Str;
+    }
+}
+
+/* Setup ********************************************************************/
+VOID Locale_Initialize(VOID)
+{
+    if(LocaleBase != NULL)
+    {
+        catalog = OpenCatalog
+        (
+            NULL, CATALOG_NAME, OC_Version, CATALOG_VERSION, TAG_DONE
+        );
+    }
+    else
+    {
+        catalog = NULL;
+    }
+}
+
+VOID Locale_Deinitialize(VOID)
+{
+    if(LocaleBase != NULL && catalog != NULL) CloseCatalog(catalog);
+}
+
+
