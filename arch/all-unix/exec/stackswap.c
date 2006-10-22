8e8c6c65970f30fa4bc05196635c7aea434b01bd
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2006-10-22 10:03:48 +0000

    StackSwap() has been deprecated, compiling AROS with the StackSwap() code enabled doesn't work anymore anyway, I therefore deleted all the code in this file (you can always go back to the previous version, if you want to). Invoking StackSwap() will cause AROS to abort().
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24914 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-unix/exec/stackswap.c
+++ arch/all-unix/exec/stackswap.c
@@ -5,84 +5,8 @@
     Desc: Change the stack of a task.
     Lang: english
 */
-#include <jmpdefs.h>
-#include <aros/config.h>
-
-static jmp_buf env;
-
-#define DEBUG 1
-
-#ifdef TEST
-/*******************************************************
-********************************************************
-***						     ***
-***   Compile this with -DTEST to get a standalone   ***
-***   program to check if it works on your system.   ***
-***						     ***
-********************************************************
-*******************************************************/
-
-/* I use this command line to compile it:
-
-    gcc -DTEST -I/usr/include -I ../../bin/linux-i386/AROS/include/ \
-	    -g stackswap.c -o t
-
-    -DTEST
-	    Enable the special test code
-    -I/usr/include
-	    Use the system's includes over AROS' ones (AROS comes with some
-	    include files with the same names as the system ones).
-    -I../../bin/linux-i386/AROS/include/
-	    Specify the correct directory for your system (ie. replace
-	    "linux-i386" by your OS and CPU)
-    -g
-	    Include debug infos in the final executable
-    stackswap.c
-	    Compile this code
-    -o t
-	    Write the executable to "t".
-
-*/
-
-#   define PROTO_EXEC_H
-#   define EXEC_TASKS_H
-
-#   include <exec/types.h>
-
-#   define AROS_LH1(ret,name,a,lt,lb,o,ln) \
-	ret name (a, lt lb)
-#   define AROS_LHA(t,n,r)  t n
-
-#   define Disable() /* eps */
-#   define Enable() /* eps */
-
-#   include <stdio.h>
-
-struct Task
-{
-    APTR	tc_SPReg;
-    APTR	tc_SPLower;
-    APTR	tc_SPUpper;
-};
-
-struct StackSwapStruct
-{
-    APTR  stk_Lower;   /* Lowest byte of stack */
-    ULONG stk_Upper;   /* Upper end of stack (size + Lowest) */
-    APTR  stk_Pointer; /* Stack pointer at switch point */
-};
-
-struct Task dummy;
-
-#   define FindTask(arg)    (!(arg) ? &dummy : NULL)
-
-struct ExecBase * SysBase = (struct ExecBase *)0x0BAD0BAD;
-
-#endif
-
-#ifndef SP
-#error SP(env) undefined
-#endif
+#include <stdlib.h>
+#include <aros/debug.h>
 
 /******************************************************************************
 
@@ -114,6 +38,12 @@ struct ExecBase * SysBase = (struct ExecBase *)0x0BAD0BAD;
     EXAMPLE
 
     BUGS
+        This function isn't really portable across compilers, as
+	it relies on the compiler NOT TO perform certain kind
+	of optimizations. 
+	
+	This function is therefore declared obsolete and should
+	never be used. In its place CallWithStack() can be used instead.
 
     SEE ALSO
 
@@ -127,157 +57,8 @@ struct ExecBase * SysBase = (struct ExecBase *)0x0BAD0BAD;
 {
     AROS_LIBFUNC_INIT
 
-    /* Protect env */
-    Disable ();
-
-    if (!(setjmp (env)) )
-    {
-	struct Task * this;
-
-	IPTR * newSP;
-	IPTR * oldSP;
-	APTR   ptr1;
-	APTR   ptr2;
-	int    t, i;
-	IPTR * fp;
-
-	newSP = (IPTR *)(sss->stk_Pointer);
-	oldSP = (IPTR *)SP(env);
-
-	/*
-	    Init the new stack. After that it should look like this:
-
-	    3	SysBase
-	    2	sss
-	    1	Return Address
-	    0	FP
-	*/
-
-	t = ((IPTR)FP(env) - (IPTR)oldSP)/sizeof(APTR) + NUM_LONGS;
-
-#if DEBUG
-#ifdef TEST
-    printf ("%d elements\n", t);
-#endif
-#endif
-
-	newSP -= t; /* Make room for t elements */
-
-    #if AROS_STACK_DEBUG
-        
-	{
-	    UBYTE *startfill = sss->stk_Lower;
-	    UBYTE *endfill = ((UBYTE *)newSP) - 16;
-	    
-	    while(startfill <= endfill)
-	    {
-	        *startfill++ = 0xE1;
-	    }
-	}
-	
-    #endif
- 
-	/*
-	    Move the contents of the old stack on the new stack.
-	    We will copy local variables, the frame pointer, the
-	    return address and the parameters sss and SysBase.
-	*/
-	for ( ; oldSP != FP(env); ) /* Copy local vars */
-	    *newSP++ = *oldSP++;
-
-	fp = newSP;
-
-	for (i=0; i<NUM_LONGS; i++) /* Copy NUM_LONGS */
-	    *newSP++ = *oldSP++;
-
-	newSP = (IPTR *)(sss->stk_Pointer) - t;
-
-	sss->stk_Pointer = oldSP; /* Save modified old SP */
-
-	SP(env) = newSP; /* Set new SP */
-
-	/*
-	    sheutlin:
-		PowerPC has the frame pointer only on the stack.
-		Therefore we can only set the frame pointer after having
-		set the new stack pointer
-	*/
-	FP(env) = fp; /* Set new frame pointer */
-
-	this = FindTask (NULL);
-
-	/* Exchange stack bounds between task and sss */
-	ptr1 = this->tc_SPLower;
-	ptr2 = sss->stk_Lower;
-	this->tc_SPLower = ptr2;
-	sss->stk_Lower	 = ptr1;
-
-	ptr1 = this->tc_SPUpper;
-	ptr2 = (IPTR *)sss->stk_Upper;
-	this->tc_SPUpper = ptr2;
-	sss->stk_Upper	 = (ULONG)ptr1;
-
-	longjmp (env, 1); /* Switch stack */
-    }
-
-    /* This is on the new stack */
-    Enable ();
+    kprintf("StackSwap() is deprecated and should NEVER be called. ABORTING.\n");
+    abort();
 
     AROS_LIBFUNC_EXIT
 } /* StackSwap */
-
-#ifndef NO_MAIN
-#ifdef TEST
-
-ULONG teststack[4096];
-
-int count = 0;
-APTR stacks[5];
-
-void checkstack (void)
-{
-    int addr;
-
-#if DEBUG
-    printf ("addr=%p\n", &addr);
-#endif
-
-    stacks[count++] = &addr;
-}
-
-int main (int argc, char ** argv)
-{
-    struct StackSwapStruct sss;
-
-    sss.stk_Lower = teststack;
-    sss.stk_Pointer = &teststack[sizeof(teststack) / sizeof(teststack[0])];
-    sss.stk_Upper = (ULONG)sss.stk_Pointer;
-
-#if DEBUG
-    printf ("teststack = %p\n", sss.stk_Pointer);
-#endif
-
-    checkstack ();
-
-    StackSwap (&sss, SysBase);
-    checkstack ();
-
-    StackSwap (&sss, SysBase);
-    checkstack ();
-
-    StackSwap (&sss, SysBase);
-    checkstack ();
-
-    StackSwap (&sss, SysBase);
-    checkstack ();
-
-    if (stacks[0] != stacks[2] || stacks[1] != stacks[3] || stacks[2] != stacks[4])
-	printf ("Test failed!\n");
-    else
-	printf ("Test ok.\n");
-
-    return 0;
-}
-
-#endif /* TEST */
-#endif /* !NO_MAIN */
