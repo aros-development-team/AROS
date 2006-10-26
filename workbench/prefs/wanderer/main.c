fc2ec5a5251dbcc191c33dacc184020d3285c553
olivieradam <reziztanzia@free.fr>
2006-10-26 19:46:19 +0000

    A first batch at wpeditor debug: Corrected the macro used for localization. Added (IPTR)'s in the Zune GUI body, and some Zune/MUIA_Application fields in the main function. For now, localized register-pages are working, although cycle-gadgets aren't. Replaced Printf()'s by D(bug()).
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24930 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/prefs/wanderer/main.c
+++ workbench/prefs/wanderer/main.c
@@ -5,38 +5,41 @@
     
     $Id$
 */
+#define DEBUG 1
+#include <aros/debug.h>
 
 #define MUIMASTER_YES_INLINE_STDARG
-
-#include <aros/debug.h>
-#define DEBUG 1
 
 #include <proto/intuition.h>
 #include <proto/muimaster.h>
 #include <libraries/mui.h>
+#include <dos/dos.h>
 
 #include <zune/systemprefswindow.h>
 #include "locale.h"
 #include "wpeditor.h"
 
-#define VERSIONSTR "$VER: Wanderer 1.1 (18.02.2006)"
+#define VERSIONSTR "$VER: Wanderer Prefs 1.1 (18.02.2006) ©1995-2006 The AROS Development Team"
 
 int main(void)
 {
     Object *application, *window;
-    
+    int rc = RETURN_OK;
+    D(bug("[WANPREFS] Starting...\n"));
     Locale_Initialize();
     
     application = ApplicationObject,
-        MUIA_Application_Title, (IPTR)_(MSG_NAME),
-        MUIA_Application_Version, (IPTR)VERSIONSTR,
+        MUIA_Application_Title, (IPTR) "Wanderer Prefs",
+        MUIA_Application_Version, (IPTR) VERSIONSTR,
+        MUIA_Application_Description, __(MSG_DESCRIPTION),
         MUIA_Application_Copyright, (IPTR)"Copyright © 1995-2006, The AROS Development Team",
         MUIA_Application_Author, (IPTR)"The AROS Development Team",
-        MUIA_Application_Base, (IPTR)"WANDERERPREF",
+        MUIA_Application_Base, (IPTR)"WANDERERPREFS",
         MUIA_Application_SingleTask, TRUE,
         SubWindow, (IPTR)(
             window = SystemPrefsWindowObject,
                 MUIA_Window_ID, MAKE_ID('W','W','I','N'),
+                MUIA_Window_Title, __(MSG_NAME),
                 WindowContents, (IPTR)WPEditorObject,
                 End,
             End),
@@ -48,13 +51,12 @@ int main(void)
         DoMethod(application, MUIM_Application_Execute);
         SET(window, MUIA_Window_Open, FALSE);
         MUI_DisposeObject(application);
-    }
-    else
-    {
-	   D(bug("wandererprefs: Can't create application"));
+    } else {
+        rc = RETURN_FAIL;
+        D(bug("[WANPREFS] Can't create application!\n"));
     }
 
     Locale_Deinitialize();
-
-    return 0;
+    D(bug("[WANPREFS] Quitting...\n"));
+    return rc;
 }
