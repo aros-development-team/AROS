89d95b623d8512f6cec922b5d8d3877572bf450c
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-02-07 20:40:37 +0000

    First version of the RX command for executing rexx scripts. Starting from workbench is not supported yet.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@25239 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/rexxc/RX.c
@@ -0,0 +1,165 @@
+/*
+    Copyright © 2007, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Run rexx scripts
+*/
+
+#include <dos/dos.h>
+#include <dos/dosextens.h>
+#include <rexx/storage.h>
+#include <rexx/errors.h>
+
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <proto/rexxsyslib.h>
+#include <proto/alib.h>
+
+#include <stdio.h>
+#include <ctype.h>
+
+static struct RexxMsg *msg = NULL;
+static struct MsgPort *rexxport = NULL, *replyport = NULL;
+static BPTR out;
+
+static BOOL init(void)
+{
+#ifdef __AROS__
+    out = Error();
+#else 
+    out = Output();
+#endif
+    
+    rexxport = FindPort("REXX");
+    if (rexxport == NULL)
+    {
+	FPuts(out, "starting rexxmast not implemented\n");
+	return FALSE;
+    }
+    
+    replyport = CreatePort(NULL, 0);
+    if (replyport == NULL)
+    {
+	FPuts(out, "Could not create a port\n");
+	return FALSE;
+    }
+    
+    msg = CreateRexxMsg(replyport, NULL, NULL);
+    if (msg == NULL)
+    {
+	FPuts(out, "Could not create RexxMsg\n");
+	return FALSE;
+    }
+    msg->rm_Action = RXCOMM | RXFF_RESULT;
+    msg->rm_Stdin = Input();
+    msg->rm_Stdout = Output();
+    
+    return TRUE;
+}
+
+void cleanup(void)
+{
+    if (msg)
+	DeleteRexxMsg(msg);
+    if (replyport)
+	DeletePort(replyport);
+}
+
+#include <aros/debug.h>
+int main(int argc, char **argv)
+{
+    struct RexxMsg *reply;
+    int ret;
+    
+    if (!init())
+    {
+	cleanup();
+	return RC_ERROR;
+    }
+    
+    if (argc == 1)
+    {
+	FPuts(out, "Required argument missing\n");
+	cleanup();
+	return RC_ERROR;
+    }
+
+    if (argc == 0)
+    {
+	/* TODO: start from workbench */
+	FPuts(out, "RX started from workbenc not implemented\n");
+	cleanup();
+	return RC_ERROR;
+    }
+    else
+    {
+	UBYTE *s;
+	struct Process *me = (struct Process *)FindTask(NULL);
+	ULONG length = 0;
+	
+	s = me->pr_Arguments;
+	while(isspace(*s)) s++;
+	
+	if (*s == '"')
+	{
+	    s++;
+	    while((s[length] != '"') && (s[length] != '\0')) length++;
+	    if (length == 0)
+	    {
+		FPuts(out, "Empty command\n");
+		cleanup();
+		return RC_ERROR;
+	    }
+	    if (s[length] == '\0')
+	    {
+		FPuts(out, "Unterminated string\n");
+		cleanup();
+		return RC_ERROR;
+	    }
+	    
+	    msg->rm_Args[0] = (IPTR)CreateArgstring(s, length);
+	    /* It is a literal command with 1 argument */
+	    msg->rm_Action |= (RXFF_STRING | 1);
+	}
+	else if (*s == '\'')
+	{
+	    s++;
+	    while((s[length] != '\'') && (s[length] != '\0')) length++;
+	    
+	    msg->rm_Args[0] = (IPTR)CreateArgstring(s, length);
+	    /* It is a literal command with 1 argument */
+	    msg->rm_Action |= (RXFF_STRING | 1);
+	}
+	else
+	{
+	    int i;
+
+	    for (i = 1; i < argc; i++)
+		msg->rm_Args[i-1] = (IPTR)argv[i];
+	    if(!FillRexxMsg(msg, argc, 0))
+	    {
+		FPuts(out, "Not enough memory\n");
+		return RC_ERROR;
+	    }
+	    msg->rm_Action |= argc;
+	}
+
+	PutMsg(rexxport, (struct Message *)msg);
+	do {
+	    reply = (struct RexxMsg *)WaitPort(replyport);
+	} while (reply != msg);
+
+	ret = msg->rm_Result1;
+	if (msg->rm_Result1 == RC_OK)
+	    FPrintf(out, "Command returned: %ld\n", msg->rm_Result2);
+	else
+	    FPrintf(out, "Error executing command %ld/%ld\n",
+		    msg->rm_Result1, msg->rm_Result2
+	    );
+    }
+
+    ClearRexxMsg(msg, msg->rm_Action & RXARGMASK);
+    cleanup();
+    
+    return ret;
+}
