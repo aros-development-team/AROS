92ce5ebd27b8fc3af34cb9e0b75eaafe1a558699
m0ns00n <m0ns00n@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-02-09 17:58:53 +0000

    New from Darius with progress bar thingie (WIP)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@25243 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/system/Wanderer/filesystems.h
+++ workbench/system/Wanderer/filesystems.h
@@ -1,16 +1,10 @@
-#ifndef _WANDERER_FILESYSTEMS_H
-#define	_WANDERER_FILESYSTEMS_H
-
-/*
-    Copyright © 2006-2007, The AROS Development Team. All rights reserved.
-    $Id: filesystems.c $
-*/
+#ifndef AMISTART_FILESYSTEMS_H
+#define	 AMISTART_FILESYSTEMS_H
 
 #include    <exec/types.h>
 
 /* FILEINFO CONSTANTS */
 
-#define	 COPYLEN 65536
 #define	 DELMODE_ASK		  0
 #define	 DELMODE_DELETE		  1
 #define	 DELMODE_ALL		  2
@@ -28,32 +22,37 @@
 #define	 ACTION_DELETE		  	 2
 #define	 ACTION_DIRTOABS		 4
 #define	 ACTION_MAKEDIRS		 8
+#define  ACTION_GETINFO          16
+
 
 #define	 PATH_NOINFO			 0
 #define	 PATH_RECURSIVE			 1
 #define	 PATH_NONRECURSIVE		 2
 
 
-#define	 PATHBUFFERSIZE			 8192
+#define	 PATHBUFFERSIZE		2048
+#define	 COPYLEN            131072
+#define	 POOLSIZE           COPYLEN * 2
 
-         struct dCopyStruct {
-             char  *spath;
-             char  *dpath;
-             char  *file;
-             APTR  userdata;
-             BPTR  slock;
-             ULONG flags;
-             unsigned long long filelen;
-             UWORD type;
-         };
 
-         struct  FileInfo {
-             ULONG   len;
-             ULONG   protection;
-             char    *comment;
-         };
+   		    struct	 dCopyStruct {
+		    		    char  *spath;
+		    		    char  *dpath;
+		    		    char  *file;
+                        APTR  userdata;
+                        BPTR  slock;
+                        ULONG flags;
+                        unsigned long long filelen;
+				        UWORD type;
+		    };
 
-            BOOL  actionDir(ULONG flags, char *source, char *dest, BOOL quit, UWORD delmode, UWORD protectmode, UWORD overwritemode, struct Hook *dHook, struct Hook *delHook, APTR userdata);
-            BOOL CopyContent(char *s, char *d, BOOL makeparentdir, ULONG flags, struct Hook *displayHook, struct Hook *delHook, APTR userdata);
+            struct  FileInfo {
+                ULONG   len;
+                ULONG   protection;
+                char    *comment;
+            };
 
-#endif /* _WANDERER_FILESYSTEMS_H */
+            BOOL  actionDir(APTR pool, ULONG flags, char *source, char *dest, BOOL quit, UWORD delmode, UWORD protectmode, UWORD overwritemode, struct Hook *dHook, struct Hook *delHook, APTR userdata);
+            BOOL CopyContent(APTR pool, char *s, char *d, BOOL makeparentdir, ULONG flags, struct Hook *displayHook, struct Hook *delHook, APTR userdata);
+
+#endif /* AMISTART_FILESYSTEMS_H */
