182ee0ecf49b32b3f234838e5dcf5f00f05e47c6
olivieradam <reziztanzia@free.fr>
2007-05-06 19:13:08 +0000

    Added CLEAR memory flag, as suggested by Georg.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@26013 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/devs/afs/os_aros_support.c
+++ workbench/devs/afs/os_aros_support.c
@@ -36,7 +36,7 @@ static void SendEvent(struct AFSBase *afsbase, LONG event) {
 
             if (!OpenDevice("input.device", 0, (struct IORequest*)InputRequest, 0)) {
 
-                if ((ie = AllocVec(sizeof(struct InputEvent), MEMF_PUBLIC))) {
+                if ((ie = AllocVec(sizeof(struct InputEvent), MEMF_PUBLIC|MEMF_CLEAR))) {
                     ie->ie_Class = event;
                     InputRequest->io_Command = IND_WRITEEVENT;
                     InputRequest->io_Data = ie;
