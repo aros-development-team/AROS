1ab2a949b0f5a9801c15186b64857a6a69af71ed
rob <rob@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-05-19 13:59:47 +0000

    r26534@plastic:  rob | 2007-05-19 23:57:56 +1000  more sane debug defines
    git-svn-id: https://svn.aros.org/svn/aros/trunk@26178 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/fs/fat/direntry.c
+++ workbench/fs/fat/direntry.c
@@ -22,11 +22,7 @@
 #include "fat_fs.h"
 #include "fat_protos.h"
 
-#if defined(DEBUG_FULL) && DEBUG_FULL != 0
-#define DEBUG 1
-#else
-#define DEBUG 0
-#endif
+#define DEBUG DEBUG_DIRENTRY
 #include <aros/debug.h>
 
 #define RESET_DIRHANDLE(dh)         \
--- workbench/fs/fat/main.c
+++ workbench/fs/fat/main.c
@@ -27,11 +27,7 @@
 #include "fat_fs.h"
 #include "fat_protos.h"
 
-#if defined(DEBUG_FULL) && DEBUG_FULL != 0
-#define DEBUG 1
-#else
-#define DEBUG 0
-#endif
+#define DEBUG DEBUG_MISC
 #include <aros/debug.h>
 
 struct DosLibrary *DOSBase;
