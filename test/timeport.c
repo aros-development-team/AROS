0270f4302941827dd738f9454a9456a17ad11c38
rob <rob@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-05-20 23:58:37 +0000

    r26567@plastic:  rob | 2007-05-21 06:30:16 +1000  comparative speed test for different port types
    git-svn-id: https://svn.aros.org/svn/aros/trunk@26203 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/timeport.c
@@ -0,0 +1,116 @@
+#include <exec/types.h>
+#include <exec/ports.h>
+#include <dos/dosextens.h>
+#include <dos/dostags.h>
+#include <aros/libcall.h>
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <time.h>
+
+#define NUM_MESSAGES (100000)
+
+struct MsgPort *port;
+
+AROS_UFH3(void, taskentry,
+          AROS_UFHA(STRPTR,            argPtr,  A0),
+          AROS_UFHA(ULONG,             argSize, D0),
+          AROS_UFHA(struct ExecBase *, SysBase, A6)) {
+    AROS_USERFUNC_INIT
+
+    int i;
+
+    for (i = 0; i < NUM_MESSAGES; i++) {
+        WaitPort(port);
+        ReplyMsg(GetMsg(port));
+    }
+
+    AROS_USERFUNC_EXIT
+}
+
+AROS_UFH3(void, intentry,
+          AROS_UFHA(APTR,              data,    A1),
+          AROS_UFHA(APTR,              code,    A5),
+          AROS_UFHA(struct ExecBase *, SysBase, A6)) {
+    AROS_USERFUNC_INIT
+
+    WaitPort(port);
+    ReplyMsg(GetMsg(port));
+
+    AROS_USERFUNC_EXIT
+}
+
+int main(int argc, char **argv) {
+    struct MsgPort *reply;
+    struct Message *msg;
+    struct Process *proc;
+    int i;
+    struct timeval start, end;
+    struct Interrupt *intr;
+
+    printf("testing with %d messages\n", NUM_MESSAGES);
+
+    port = CreateMsgPort();
+    reply = CreateMsgPort();
+
+    msg = AllocVec(sizeof(struct Message), MEMF_PUBLIC | MEMF_CLEAR);
+    msg->mn_Length = sizeof(struct Message);
+    msg->mn_ReplyPort = reply;
+
+    proc = CreateNewProcTags(NP_Entry,    (IPTR) taskentry,
+                             NP_Name,     "timeport task",
+                             TAG_DONE);
+
+    port->mp_Flags = PA_SIGNAL;
+    port->mp_SigTask = (struct Task *) proc;
+
+    gettimeofday(&start, NULL);
+
+    for (i = 0; i < NUM_MESSAGES; i++) {
+        PutMsg(port, msg);
+        WaitPort(reply);
+        GetMsg(reply);
+    }
+
+    gettimeofday(&end, NULL);
+
+    while (end.tv_usec < start.tv_usec) {
+        end.tv_sec--;
+        end.tv_usec += 1000000;
+    }
+
+    printf("MP_SIGNAL: %ld.%ld\n", end.tv_sec - start.tv_sec, end.tv_usec - start.tv_usec);
+
+    intr = AllocVec(sizeof(struct Interrupt), MEMF_PUBLIC | MEMF_CLEAR);
+    intr->is_Code = intentry;
+
+    port->mp_Flags = PA_SOFTINT;
+    port->mp_SoftInt = intr;
+
+    gettimeofday(&start, NULL);
+
+    for (i = 0; i < NUM_MESSAGES; i++) {
+        PutMsg(port, msg);
+        WaitPort(reply);
+        GetMsg(reply);
+    }
+
+    gettimeofday(&end, NULL);
+
+    while (end.tv_usec < start.tv_usec) {
+        end.tv_sec--;
+        end.tv_usec += 1000000;
+    }
+
+    printf("MP_SOFTINT: %ld.%ld\n", end.tv_sec - start.tv_sec, end.tv_usec - start.tv_usec);
+
+    FreeVec(intr);
+
+    FreeVec(msg);
+
+    DeleteMsgPort(reply);
+    DeleteMsgPort(port);
+
+    return 0;
+}
