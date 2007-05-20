37cda525fbd77a6836fcbba814c3009dbe352f01
shinkuro <shinkuro@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-05-20 11:36:49 +0000

    version.h: contains some define version for wanderer
    git-svn-id: https://svn.aros.org/svn/aros/trunk@26193 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/system/Wanderer/wanderer.c
+++ workbench/system/Wanderer/wanderer.c
@@ -45,7 +45,7 @@
 #include "../../libs/muimaster/classes/iconlist_attributes.h"
 #include "locale.h"
 
-#define VERSION "$VER: Wanderer 0.65 (16.04.2007) The AROS Dev Team"
+#include "version.h"
 
 #define KeyButton(name,key) TextObject, ButtonFrame, MUIA_Font, MUIV_Font_Button, MUIA_Text_Contents, (IPTR)(name), MUIA_Text_PreParse, "\33c", MUIA_Text_HiChar, (IPTR)(key), MUIA_ControlChar, key, MUIA_InputMode, MUIV_InputMode_RelVerify, MUIA_Background, MUII_ButtonBack, End
 
