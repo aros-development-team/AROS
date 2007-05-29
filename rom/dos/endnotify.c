b542eeab474e995df979df8f04cc8e35274f9d25
rob <rob@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-05-29 00:39:59 +0000

    r26715@plastic:  rob | 2007-05-29 09:16:22 +1000  more commentary
    git-svn-id: https://svn.aros.org/svn/aros/trunk@26240 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/endnotify.c
+++ rom/dos/endnotify.c
@@ -57,50 +57,64 @@
     struct IOFileSys iofs;
     struct DevProc *dvp;
 
+    /* get the device pointer and dir lock. note that we don't just use
+     * nr_Handler here, because we also need to supply a unit pointer so
+     * packet.handler can get its mount context */
     if ((dvp = GetDeviceProc(notify->nr_FullName, NULL)) == NULL)
         return;
 
+    /* setup the call */
     InitIOFS(&iofs, FSA_REMOVE_NOTIFY, DOSBase);
     iofs.io_Union.io_NOTIFY.io_NotificationRequest = notify;
 
     iofs.IOFS.io_Device = (struct Device *) dvp->dvp_Port;
 
-    if (dvp->dvp_Lock != NULL)
-        iofs.IOFS.io_Unit = ((struct FileHandle *) BADDR(dvp->dvp_Lock))->fh_Unit;
-    else
+    /* take the root lock from either the doslist entry or from the devproc */
+    if (dvp->dvp_Lock == NULL)
         iofs.IOFS.io_Unit = dvp->dvp_DevNode->dol_Ext.dol_AROS.dol_Unit;
+    else
+        iofs.IOFS.io_Unit = ((struct FileHandle *) BADDR(dvp->dvp_Lock))->fh_Unit;
 
     FreeDeviceProc(dvp);
 
+    /* go */
     DoIO(&iofs.IOFS);
 
+    /* free fullname if it was built in StartNotify() */
     if (notify->nr_FullName != notify->nr_Name)
         FreeVec(notify->nr_FullName);
 
+    /* if the filesystem has outstanding messages, they need to be replied */
     if (notify->nr_Flags & NRF_SEND_MESSAGE &&
-	(notify->nr_Flags & NRF_WAIT_REPLY || notify->nr_MsgCount)) {
+	(notify->nr_Flags & NRF_WAIT_REPLY || notify->nr_MsgCount > 0)) {
 
 	struct MsgPort *port = notify->nr_stuff.nr_Msg.nr_Port;
 	struct NotifyMessage *nm, *tmp;
 
 	notify->nr_Flags &= ~NRF_MAGIC;
 
+        /* protect access to the message list */
 	Disable();
 
+        /* loop over the messages */
 	ForeachNodeSafe(&port->mp_MsgList, nm, tmp) {
+            /* if its one of our notify messages */
 	    if (nm->nm_Class == NOTIFY_CLASS &&
 		nm->nm_Code == NOTIFY_CODE &&
 		nm->nm_NReq == notify) {
 
+                /* remove and reply */
 		Remove((struct Node *) nm);
 		ReplyMsg((struct Message *) nm);
 
+                /* decrement the count. bail early if we've done them all */
                 notify->nr_MsgCount--;
                 if (notify->nr_MsgCount == 0)
                     break;
 	    }
 	}
 
+        /* unlock the list */
 	Enable();
     }
 
