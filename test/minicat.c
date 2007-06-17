aab01eb07ca4b92e84692256e81a9e33dd09e092
rob <rob@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-06-17 22:13:36 +0000

    r26822@plastic:  rob | 2007-06-18 08:10:04 +1000  useful tool for testing pipes, might be good for other stuff too
    git-svn-id: https://svn.aros.org/svn/aros/trunk@26290 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/minicat.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <exec/types.h>
+#include <dos/dos.h>
+#include <dos/dosextens.h>
+#include <dos/bptr.h>
+#include <proto/dos.h>
+
+int main(int argc, char **argv) {
+    BPTR in, out;
+    char buf[256];
+    LONG len;
+
+    if (argc > 1) {
+        if ((in = Open(argv[1], MODE_OLDFILE)) == NULL) {
+            Fault(IoErr(), "minicat", buf, 255);
+            fprintf(stderr, "%s\n", buf);
+            return 1;
+        }
+    }
+    else
+        in = Input();
+
+    out = Output();
+
+    while ((len = Read(in, buf, 256)) > 0)
+        Write(out, buf, len);
+
+    if (argc > 1)
+        Close(in);
+
+    return 0;
+}
