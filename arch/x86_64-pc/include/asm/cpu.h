2d9c9b6d3aadd03cbaf24d5351fdff133b914a5c
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-09-10 21:27:16 +0000

    asm/cpu.h contains typedef of cpu context
    git-svn-id: https://svn.aros.org/svn/aros/trunk@26526 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/x86_64-pc/include/asm/cpu.h
+++ arch/x86_64-pc/include/asm/cpu.h
@@ -7,6 +7,33 @@
 extern "C" {
 #endif
 
+typedef struct regs {
+    uint64_t    ds;
+    uint64_t    r15;
+    uint64_t    r14;
+    uint64_t    r13;
+    uint64_t    r12;
+    uint64_t    r11;
+    uint64_t    r10;
+    uint64_t    r9;
+    uint64_t    r8;
+    uint64_t    rcx;
+    uint64_t    rdx;
+    uint64_t    rsi;
+    uint64_t    rdi;
+    uint64_t    rbx;
+    uint64_t    rbp;
+    uint64_t    rax;
+    uint64_t    irq_number;
+    uint64_t    error_code;
+    uint64_t    return_rip;
+    uint64_t    return_cs;
+    uint64_t    return_rflags;
+    uint64_t    return_rsp;
+    uint64_t    return_ss;
+} regs_t;
+
+    
 /* Segment registers */
 #define SEG_SUPER_CS    0x08
 #define SEG_SUPER_DS    0x10
