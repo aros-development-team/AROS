194086e618138ddb035d02f2c645c585efdcadf7
rob <rob@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-09-21 03:14:13 +0000

    squashed commit of private hostlib-resource branch
    git-svn-id: https://svn.aros.org/svn/aros/trunk@26770 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/hostlib.c
@@ -0,0 +1,40 @@
+#include <proto/exec.h>
+#include <proto/hostlib.h>
+#include <proto/dos.h>
+#include <stdio.h>
+
+APTR HostLibBase;
+
+int (*SDL_Init) (ULONG flags);
+void * (*SDL_SetVideoMode) (int width, int height, int bpp, ULONG flags);
+void (*SDL_Quit) (void);
+
+int main(int argc, char **argv) {
+    void *handle;
+    char *err;
+
+    if ((HostLibBase = OpenResource("hostlib.resource")) == NULL) {
+        fprintf(stderr, "can't open hostlib.resource\n");
+        return 1;
+    }
+
+    if ((handle = HostLib_Open("libSDL.so", &err)) == NULL) {
+        fprintf(stderr, "can't open sdl: %s\n", err);
+        return 1;
+    }
+
+    SDL_Init = HostLib_GetPointer(handle, "SDL_Init", NULL);
+    SDL_SetVideoMode = HostLib_GetPointer(handle, "SDL_SetVideoMode", NULL);
+    SDL_Quit = HostLib_GetPointer(handle, "SDL_Quit", NULL);
+
+    SDL_Init(0x20);
+    SDL_SetVideoMode(640, 480, 16, 0);
+
+    Delay(250);
+
+    SDL_Quit();
+
+    HostLib_Close(handle, NULL);
+
+    return 0;
+}
