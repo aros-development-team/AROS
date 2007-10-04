1ea215645ed864a4a09f7c43e038ff0990d2f67f
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-10-04 23:00:23 +0000

    Added 64-bit int alignment.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@26991 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/m68k-mac/include/aros/m68k/cpu.h
+++ arch/m68k-mac/include/aros/m68k/cpu.h
@@ -21,6 +21,7 @@
 #define AROS_SIZEOFPTR		   4 /* Size of a PTR */
 #define AROS_WORDALIGN		   2 /* Alignment for WORD */
 #define AROS_LONGALIGN		   4 /* Alignment for LONG */
+#define AROS_QUADALIGN		   8 /* Alignment for QUAD */
 #define AROS_PTRALIGN		   4 /* Alignment for PTR */
 #define AROS_IPTRALIGN		   4 /* Alignment for IPTR */
 #define AROS_DOUBLEALIGN	   4 /* Alignment for double */
