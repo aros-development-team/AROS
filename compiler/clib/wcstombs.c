db8d558780f20dd3614e804ddd4049acf6a4b511
rob <rob@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-10-30 07:48:54 +0000

    stubs for c99 functions mbtowc, wctomb, mcstowcs, wcstombs
    git-svn-id: https://svn.aros.org/svn/aros/trunk@27120 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/wcstombs.c
@@ -0,0 +1,16 @@
+/*
+    Copyright © 2007, The AROS Development Team. All rights reserved.
+    $Id$
+
+    ISO/ANSI C function wcstombs().
+*/
+
+#include <aros/debug.h>
+
+#include <stdlib.h>
+
+size_t wcstombs(char *s, const wchar_t *pwcs, size_t n) {
+    AROS_FUNCTION_NOT_IMPLEMENTED("arosc");
+    
+    return (size_t) -1;
+}
