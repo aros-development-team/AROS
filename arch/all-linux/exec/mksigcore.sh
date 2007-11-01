16825114041f81a74c10248c7110f0d1eb458c81
weissms <weissms@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-11-01 15:57:53 +0000

    Move i386 sigcore.h.src into its own directory. Simplifies handling of those files for the various target archs, simplifies mksigcore.sh and now dependencies should work not only for i386.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@27133 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-linux/exec/mksigcore.sh
+++ arch/all-linux/exec/mksigcore.sh
@@ -10,10 +10,6 @@ type=`${CC} -E /usr/include/asm/sigcontext.h | grep "^struct sigcontext" | sed '
 handler=__sighandler_t
 if [ ${CPU} = "m68k" ]; then
   sed "s/@sigcontext@/$type/" ${1-.}/../m68k/sigcore.h.src > ${2}
-elif [ ${CPU} = "x86_64" ]; then
-  sed "s/@sigcontext@/$type/;s/@sighandler@/$handler/" ${1-.}/../x86_64/sigcore.h.src > ${2}
-elif [ ${CPU} = "ppc" ]; then
-  sed "s/@sigcontext@/$type/;s/@sighandler@/$handler/" ${1-.}/../ppc/sigcore.h.src > ${2}
 else
-  sed "s/@sigcontext@/$type/;s/@sighandler@/$handler/" ${1-.}/sigcore.h.src > ${2}
+  sed "s/@sigcontext@/$type/;s/@sighandler@/$handler/" ${1-.}/../${CPU}/sigcore.h.src > ${2}
 fi
