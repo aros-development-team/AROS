25e7579ab915b5b6d51ac83113e0029871fc080d
rob <rob@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-11-10 21:31:52 +0000

    convenience macros for working with timevals. stolen from freebsd
    git-svn-id: https://svn.aros.org/svn/aros/trunk@27177 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/include/sys/time.h
+++ compiler/clib/include/sys/time.h
@@ -49,6 +49,31 @@ struct timezone
 #define DST_TUR     9  /* Turkish style. */
 #define DST_AUSTALT 10 /* Alternate Australian style. */
 
+/* Convenience macros for working with timevals */
+#define timerclear(tvp)         ((tvp)->tv_sec = (tvp)->tv_usec = 0)
+#define timerisset(tvp)         ((tvp)->tv_sec || (tvp)->tv_usec)
+#define timercmp(tvp, uvp, cmp)                                 \
+        (((tvp)->tv_sec == (uvp)->tv_sec) ?                             \
+            ((tvp)->tv_usec cmp (uvp)->tv_usec) :                       \
+            ((tvp)->tv_sec cmp (uvp)->tv_sec))
+#define timeradd(tvp, uvp, vvp)                                         \
+        do {                                                            \
+                (vvp)->tv_sec = (tvp)->tv_sec + (uvp)->tv_sec;          \
+                (vvp)->tv_usec = (tvp)->tv_usec + (uvp)->tv_usec;       \
+                if ((vvp)->tv_usec >= 1000000) {                        \
+                        (vvp)->tv_sec++;                                \
+                        (vvp)->tv_usec -= 1000000;                      \
+                }                                                       \
+        } while (0)
+#define timersub(tvp, uvp, vvp)                                         \
+        do {                                                            \
+                (vvp)->tv_sec = (tvp)->tv_sec - (uvp)->tv_sec;          \
+                (vvp)->tv_usec = (tvp)->tv_usec - (uvp)->tv_usec;       \
+                if ((vvp)->tv_usec < 0) {                               \
+                        (vvp)->tv_sec--;                                \
+                        (vvp)->tv_usec += 1000000;                      \
+                }                                                       \
+        } while (0)
 
 __BEGIN_DECLS
 
