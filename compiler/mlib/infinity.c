b726e32f98ceec8305aac2bdf0d138af84659149
rob <rob@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-11-17 21:41:09 +0000

    mlib update: arch-specific values for infinity and NaN
    git-svn-id: https://svn.aros.org/svn/aros/trunk@27234 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/mlib/infinity.c
+++ compiler/mlib/infinity.c
@@ -2,15 +2,23 @@
     Copyright © 2002, The AROS Development Team. All rights reserved.
     $Id$
 
-    Definition of the magic infinity value.
-    XXX Needs to be made better CPU dependant! Basically it assumes we are
-    dealing with IEEE 754 floating point.
+    Definition of infinity and NaN.
 */
 
+/* These definitions are used if no architecture-specific version of this file
+ * is available. They assume we're dealing with IEEE 754 floating point. */
+
 #include <aros/system.h>
+#include <math.h>
+-
+#if AROS_BIG_ENDIAN
+const union __infinity_un __infinity[] = { { 0x7f, 0xf0, 0, 0, 0, 0, 0, 0 } };
+#else
+const union __infinity_un __infinity[] = { 0, 0, 0, 0, 0, 0, 0xf0, 0x7f };
+#endif
 
 #if AROS_BIG_ENDIAN
-char __infinity[] = { 0x7f, 0xf0, 0, 0, 0, 0, 0, 0 };
+const union __nan_un __nan = { { 0xff, 0xc0, 0, 0 } };
 #else
-char __infinity[] = { 0, 0, 0, 0, 0, 0, 0xf0, 0x7f };
+const union __nan_un __nan = { { 0, 0, 0xc0, 0xff } };
 #endif
