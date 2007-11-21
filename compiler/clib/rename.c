b7f440bd71f5370838e2ac4c4c0636dad377eebf
agreppin <agreppin@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-11-21 14:37:31 +0000

    rename also unix filenames
    git-svn-id: https://svn.aros.org/svn/aros/trunk@27263 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/rename.c
+++ compiler/clib/rename.c
@@ -6,7 +6,13 @@
 */
 
 #include <proto/dos.h>
+#include <stdlib.h>
+#include <string.h>
 #include "__errno.h"
+#include "__upath.h"
+
+#define DEBUG 0
+#include <aros/debug.h>
 
 /*****************************************************************************
 
@@ -41,12 +47,20 @@
 
 ******************************************************************************/
 {
-    if (!Rename ((STRPTR)oldpath,(STRPTR)newpath))
+          char *aoldpath = strdup(__path_u2a(oldpath));
+    const char *anewpath =        __path_u2a(newpath);
+
+    if (!Rename (aoldpath, anewpath))
     {
-	errno = IoErr2errno (IoErr());
+	int ioerr = IoErr();
+	errno = IoErr2errno (ioerr);
+	D(bug("rename(%s, %s) errno=%d, IoErr=%d\n", aoldpath, anewpath,
+	      errno, ioerr));
+	free(aoldpath);
 	return -1;
     }
 
+    free(aoldpath);
     return 0;
 
 } /* rename */
