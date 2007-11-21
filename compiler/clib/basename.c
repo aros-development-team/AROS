f07f23d5aaaceafd3c545e5e26580113e1e2aa7b
agreppin <agreppin@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-11-21 20:41:26 +0000

    first commit
    git-svn-id: https://svn.aros.org/svn/aros/trunk@27271 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/basename.c
@@ -0,0 +1,54 @@
+/*
+    Copyright © 2004, The AROS Development Team. All rights reserved.
+    $Id$
+
+    POSIX function basename().
+*/
+
+#include "__upath.h"
+
+#define DEBUG 1
+#include <aros/debug.h>
+
+char *basename(char *filename)
+{
+    char *uname;
+    char *pos;
+
+    if (!filename || *filename == '\0')
+    {
+	D(bug("basename()=.\n"));
+        return ".";
+    }
+
+    uname = (char *)__path_a2u(filename);
+
+    pos = uname;
+
+    if (pos[0] == '/' && pos[1] == '\0')
+    {
+	D(bug("basename(/)=/\n"));
+	return uname;
+    }
+
+    D(bug("basename(%s)=", filename));
+
+    pos = uname + strlen(uname);
+    while (pos[-1] == '/')
+    {
+	--pos;
+	pos[0] = '\0';
+    }
+    while (--pos > uname)
+    {
+	if (pos[0] == '/')
+	{
+	    uname = ++pos;
+	    break;
+	}
+    }
+
+    D(bug("%s\n", uname));
+    return uname;
+}
+
---
+++ compiler/clib/include/libgen.h
@@ -0,0 +1,8 @@
+#ifndef _LINGEN_H
+#define _LINGEN_H
+
+extern char *basename(char *);
+extern char *dirname(char *);
+
+#endif
+
