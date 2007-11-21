8aa20e1bd3fb692da0f8a7b869c187a11a05ed35
agreppin <agreppin@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-11-21 20:31:53 +0000

    implement dirfd
    git-svn-id: https://svn.aros.org/svn/aros/trunk@27268 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/dirfd.c
@@ -0,0 +1,52 @@
+/*
+    Copyright © 1995-2003, The AROS Development Team. All rights reserved.
+    $Id$
+
+    POSIX function dirfd().
+*/
+
+#define DEBUG 0
+#include <aros/debug.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <dirent.h>
+
+	int dirfd(
+
+/*  SYNOPSIS */
+	DIR *dir)
+
+/*  FUNCTION
+	get directory stream file descriptor
+
+    INPUTS
+	dir - directory stream dir.
+
+    RESULT
+	on error -1 is returned.
+
+    NOTES
+       This descriptor is the one used internally by the directory stream.  As
+       a  result,  it  is  only useful for functions which do not depend on or
+       alter the file position, such as fstat(2) and fchdir(2).   It  will  be
+       automatically closed when closedir(3) is called.
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+ 	open(), readdir(), closedir(), rewinddir(), seekdir(),
+	telldir(), scandir()
+
+    INTERNALS
+
+******************************************************************************/
+{
+  D(bug("dirfd(d_type=%d)=%d\n", dir->ent.d_type, dir->fd));
+  if (dir->ent.d_type == DT_DIR)
+    return dir->fd;
+  return -1;
+}
