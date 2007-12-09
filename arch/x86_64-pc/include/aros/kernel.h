01d39789293578d9b2ff9a0158ca47425a4ae5ce
Kalamatee <kalamatee@gmail.com>
2007-12-09 11:32:24 +0000

    Fix problem with constraints under some builds of gcc
    git-svn-id: https://svn.aros.org/svn/aros/trunk@27420 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/x86_64-pc/include/aros/kernel.h
+++ arch/x86_64-pc/include/aros/kernel.h
@@ -3,6 +3,8 @@
 
 #ifndef BOOTSTRAP
 
+#include <stddef.h>
+
 #include <inttypes.h>
 #include <utility/tagitem.h>
 
@@ -33,14 +35,16 @@ typedef struct tls {
     void *              *KernelBase;    /* Base of kernel.resource */
 } tls_t;
 
+#define TLS_OFFSET(name) offsetof(tls_t, name)
+
 #define TLS_GET(name) \
     ({ \
         tls_t *__tls = (tls_t *)0; \
         typeof(__tls -> name) __ret; \
         switch (sizeof(__tls -> name)) { \
-case 2: asm volatile("movw %%gs:%P1,%0":"=r"(__ret):"n"(&__tls -> name):"memory"); break; \
-case 4: asm volatile("movl %%gs:%P1,%0":"=r"(__ret):"n"(&__tls -> name):"memory"); break; \
-case 8: asm volatile("movq %%gs:%P1,%0":"=r"(__ret):"n"(&__tls -> name):"memory"); break; \
+case 2: asm volatile("movw %%gs:%P1,%0":"=r"(__ret):"n"(TLS_OFFSET(name)):"memory"); break; \
+case 4: asm volatile("movl %%gs:%P1,%0":"=r"(__ret):"n"(TLS_OFFSET(name)):"memory"); break; \
+case 8: asm volatile("movq %%gs:%P1,%0":"=r"(__ret):"n"(TLS_OFFSET(name)):"memory"); break; \
         } \
         __ret;  \
     })
@@ -50,9 +54,9 @@ case 8: asm volatile("movq %%gs:%P1,%0":"=r"(__ret):"n"(&__tls -> name):"memory"
         tls_t *__tls = (tls_t *)0; \
         typeof(__tls -> name) __set = val; \
         switch (sizeof(__tls -> name)) { \
-case 2: asm volatile("movw %0,%%gs:%P1"::"r"(__set),"n"(&__tls -> name):"memory"); break; \
-case 4: asm volatile("movl %0,%%gs:%P1"::"r"(__set),"n"(&__tls -> name):"memory"); break; \
-case 8: asm volatile("movq %0,%%gs:%P1"::"r"(__set),"n"(&__tls -> name):"memory"); break; \
+case 2: asm volatile("movw %0,%%gs:%P1"::"r"(__set),"n"(TLS_OFFSET(name)):"memory"); break; \
+case 4: asm volatile("movl %0,%%gs:%P1"::"r"(__set),"n"(TLS_OFFSET(name)):"memory"); break; \
+case 8: asm volatile("movq %0,%%gs:%P1"::"r"(__set),"n"(TLS_OFFSET(name)):"memory"); break; \
         } \
     } while(0);
 
