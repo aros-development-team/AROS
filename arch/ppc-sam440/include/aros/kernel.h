7e29b4a2891413f99389cfa0ff57278a0f26114c
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-02-07 14:14:14 +0000

    aros/kernel.h header of kernel.resource. Phew. It would be really nice to make this header a global one. But in order to do that it's TAGs will have to be extended a bit.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@27824 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/ppc-sam440/include/aros/kernel.h
@@ -0,0 +1,29 @@
+#ifndef AROS_KERNEL_H
+#define AROS_KERNEL_H
+
+#include <inttypes.h>
+#include <utility/tagitem.h>
+
+typedef enum {
+    SCHED_RR = 1
+} KRN_SchedType;
+
+#define KRN_Dummy               (TAG_USER + 0x03d00000)
+#define KRN_KernelBase          (KRN_Dummy + 1)
+#define KRN_KernelLowest        (KRN_Dummy + 2)
+#define KRN_KernelHighest       (KRN_Dummy + 3)
+#define KRN_KernelBss           (KRN_Dummy + 4)
+#define KRN_GDT                 (KRN_Dummy + 5)
+#define KRN_IDT                 (KRN_Dummy + 6)
+#define KRN_PL4                 (KRN_Dummy + 7)
+#define KRN_VBEModeInfo         (KRN_Dummy + 8)
+#define KRN_VBEControllerInfo   (KRN_Dummy + 9)
+#define KRN_MMAPAddress         (KRN_Dummy + 10)
+#define KRN_MMAPLength          (KRN_Dummy + 11)
+#define KRN_CmdLine             (KRN_Dummy + 12)
+#define KRN_ProtAreaStart       (KRN_Dummy + 13)
+#define KRN_ProtAreaEnd         (KRN_Dummy + 14)
+#define KRN_VBEMode             (KRN_Dummy + 15)
+#define KRN_VBEPaletteWidth     (KRN_Dummy + 16)
+
+#endif /*AROS_KERNEL_H*/
