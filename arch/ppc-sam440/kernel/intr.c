c340d1031981c408ced9a9b429d09b7d73d1092c
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-02-07 22:40:41 +0000

    - added some empty functions of kernel.resource. - Kernel resource creates MMU map for itself upon startup now. - added some fancy commens :P
    git-svn-id: https://svn.aros.org/svn/aros/trunk@27826 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/ppc-sam440/kernel/intr.c
+++ arch/ppc-sam440/kernel/intr.c
@@ -1,7 +1,52 @@
+#include <aros/kernel.h>
+#include <aros/libcall.h>
 #include <asm/amcc440.h>
 #include <stddef.h>
 
-static void __attribute__((used)) __intr_template()
+
+AROS_LH4(void *, KrnAddIRQHandler,
+         AROS_LHA(uint8_t, irq, D0),
+         AROS_LHA(void *, handler, A0),
+         AROS_LHA(void *, handlerData, A1),
+         AROS_LHA(void *, handlerData2, A2),
+         struct KernelBase *, KernelBase, 7, Kernel)
+{
+    AROS_LIBFUNC_INIT
+
+    return NULL;
+
+    AROS_LIBFUNC_EXIT
+}
+
+AROS_LH1(void, KrnRemIRQHandler,
+         AROS_LHA(void *, handle, A0),
+         struct KernelBase *, KernelBase, 8, Kernel)
+{
+    AROS_LIBFUNC_INIT
+
+
+    AROS_LIBFUNC_EXIT
+}
+
+AROS_LH0I(void, KrnCli,
+         struct KernelBase *, KernelBase, 9, Kernel)
+{
+    AROS_LIBFUNC_INIT
+
+    AROS_LIBFUNC_EXIT
+}
+
+AROS_LH0I(void, KrnSti,
+         struct KernelBase *, KernelBase, 10, Kernel)
+{
+    AROS_LIBFUNC_INIT
+
+    AROS_LIBFUNC_EXIT
+}
+
+
+
+static void __attribute__((used)) __EXCEPTION_Prolog_template()
 {
     asm volatile(".globl __EXCEPTION_Prolog\n\t.type __EXCEPTION_Prolog,@function\n"
         "__EXCEPTION_Prolog:            \n\t"
@@ -39,7 +84,7 @@ static void __attribute__((used)) __intr_template()
                  "stw %%r1,%[srr1](%%r3)   \n\t"
                  "mfctr %%r0               \n\t"
                  "mflr %%r1                \n\t"
-                 "mfxer %%r2               \n\t"           
+                 "mfxer %%r2               \n\t"
                  "stw %%r0,%[ctr](%%r3)    \n\t"
                  "stw %%r1,%[lr](%%r3)     \n\t"
                  "stw %%r2,%[xer](%%r3)    \n\t"
@@ -60,12 +105,114 @@ static void __attribute__((used)) __intr_template()
                  [xer]"i"(offsetof(regs_t, xer))
                  );
     /*
-     * Registers %r0 to %r5 are now saved together with CPU state.
+     * Registers %r0 to %r5 are now saved together with CPU state. Go to the 
+     * trampoline code which will care about the rest. Adjust the stack frame pointer now,
+     * or else it will be destroyed later by C code.
      */
+    asm volatile("addi %r1,%r3,-16");
+    
+    /*
+     * Go to the trampoline code. Use long call within whole 4GB addresspace in order to
+     * avoid any trouble in future.
+     */
+    asm volatile(
+                 "lis %r5,__EXCEPTION_Trampoline@ha\n\t"
+                 "la %r5,__EXCEPTION_Trampoline@l(%r5)\n\t"
+                 "mtctr %r5\n\t"
+                 "bctrl\n\t"
+                 ".globl __EXCEPTION_HandlerPtr\n"
+    "__EXCEPTION_HandlerPtr: .long 0"
+            "");
     
     
     asm volatile(".size __EXCEPTION_Prolog, .-__EXCEPTION_Prolog\n\t"
                  ".globl __EXCEPTION_Prolog_size\n\t"
                  ".type  __EXCEPTION_Prolog_size,@object\n"
                  "__EXCEPTION_Prolog_size: .long . - __EXCEPTION_Prolog - 4");
+}
+
+static void __attribute__((used)) __EXCEPTION_Trampoline_template()
+{
+    asm volatile(".globl __EXCEPTION_Trampoline\n\t.type __EXCEPTION_Trampoline,@function\n"
+        "__EXCEPTION_Trampoline:            \n\t"
+                 "mflr %%r5             \n\t"   /* link register points to address of exception handler */
+                 "stw %%r6,%[gpr6](%%r3) \n\t"
+                 "stw %%r7,%[gpr7](%%r3) \n\t"
+                 "stw %%r8,%[gpr8](%%r3) \n\t"
+                 "stw %%r9,%[gpr9](%%r3) \n\t"
+                 "stw %%r10,%[gpr10](%%r3) \n\t"
+                 "stw %%r11,%[gpr11](%%r3) \n\t"
+                 "stw %%r12,%[gpr12](%%r3) \n\t"
+                 "stw %%r13,%[gpr13](%%r3) \n\t"
+                 "stw %%r14,%[gpr14](%%r3) \n\t"
+                 "stw %%r15,%[gpr15](%%r3) \n\t"
+                 "stw %%r16,%[gpr16](%%r3) \n\t"
+                 "stw %%r17,%[gpr17](%%r3) \n\t"
+                 "stw %%r18,%[gpr18](%%r3) \n\t"
+                 "stw %%r19,%[gpr19](%%r3) \n\t"
+                 "stw %%r20,%[gpr20](%%r3) \n\t"
+                 "stw %%r21,%[gpr21](%%r3) \n\t"
+                 "stw %%r22,%[gpr22](%%r3) \n\t"
+                 "stw %%r23,%[gpr23](%%r3) \n\t"
+                 "stw %%r24,%[gpr24](%%r3) \n\t"
+                 "stw %%r25,%[gpr25](%%r3) \n\t"
+                 "stw %%r26,%[gpr26](%%r3) \n\t"
+                 "stw %%r27,%[gpr27](%%r3) \n\t"
+                 "stw %%r28,%[gpr28](%%r3) \n\t"
+                 "stw %%r29,%[gpr29](%%r3) \n\t"
+                 "stw %%r30,%[gpr30](%%r3) \n\t"
+                 "stw %%r31,%[gpr31](%%r3) \n\t"
+                 
+                 "lwz %%r0,0(%%r5)      \n\t"   /* Load the address of exception handler into srr0 */
+                 "mtsrr0 %%r0           \n\t"
+                 
+                 "andi. %%r5,%%r5,0x3f00 \n\t"   /* %r5 shall contain the exception number */
+                 
+                 "lis %%r9, %[msrval]@h  \n\t"
+                 "ori %%r9,%%r9, %[msrval]@l \n\t"
+                 "mtsrr1 %%r9"
+                 ::
+                 [gpr6]"i"(offsetof(regs_t, gpr[6])),
+                 [gpr7]"i"(offsetof(regs_t, gpr[7])),
+                 [gpr8]"i"(offsetof(regs_t, gpr[8])),
+                 [gpr9]"i"(offsetof(regs_t, gpr[9])),
+                 [gpr10]"i"(offsetof(regs_t, gpr[10])),
+                 [gpr11]"i"(offsetof(regs_t, gpr[11])),
+                 [gpr12]"i"(offsetof(regs_t, gpr[12])),
+                 [gpr13]"i"(offsetof(regs_t, gpr[13])),
+                 [gpr14]"i"(offsetof(regs_t, gpr[14])),
+                 [gpr15]"i"(offsetof(regs_t, gpr[15])),
+                 [gpr16]"i"(offsetof(regs_t, gpr[16])),
+                 [gpr17]"i"(offsetof(regs_t, gpr[17])),
+                 [gpr18]"i"(offsetof(regs_t, gpr[18])),
+                 [gpr19]"i"(offsetof(regs_t, gpr[19])),
+                 [gpr20]"i"(offsetof(regs_t, gpr[20])),
+                 [gpr21]"i"(offsetof(regs_t, gpr[21])),
+                 [gpr22]"i"(offsetof(regs_t, gpr[22])),
+                 [gpr23]"i"(offsetof(regs_t, gpr[23])),
+                 [gpr24]"i"(offsetof(regs_t, gpr[24])),
+                 [gpr25]"i"(offsetof(regs_t, gpr[25])),
+                 [gpr26]"i"(offsetof(regs_t, gpr[26])),
+                 [gpr27]"i"(offsetof(regs_t, gpr[27])),
+                 [gpr28]"i"(offsetof(regs_t, gpr[28])),
+                 [gpr29]"i"(offsetof(regs_t, gpr[29])),
+                 [gpr30]"i"(offsetof(regs_t, gpr[30])),
+                 [gpr31]"i"(offsetof(regs_t, gpr[31])),
+
+                 [msrval]"i"(MSR_EE | MSR_ME | MSR_DS | MSR_IS)
+    );
+#warning TODO: Check msrval!!!!!!!
+
+    asm volatile(
+                 "sync\n\t"
+                 "rfi"
+    );
+                 
+    asm volatile(
+                 ".size __EXCEPTION_Trampoline, .-__EXCEPTION_Trampoline\n\t"
+                 ".globl __EXCEPTION_Trampoline_size\n\t"
+                 ".type  __EXCEPTION_Trampoline_size,@object\n"
+                 "__EXCEPTION_Trampoline_size: .long . - __EXCEPTION_Trampoline - 4"
+    );
+
 }
--- arch/ppc-sam440/kernel/kernel_init.c
+++ arch/ppc-sam440/kernel/kernel_init.c
@@ -1,3 +1,5 @@
+#include <aros/kernel.h>
+#include <aros/libcall.h>
 #include <inttypes.h>
 #include <exec/libraries.h>
 #include <utility/tagitem.h>
@@ -28,24 +30,59 @@ void __puts(char *str)
 
 static void __attribute__((used)) kernel_cstart(struct TagItem *msg)
 {
+    int i;
+    uint32_t v1,v2,v3;
+    
     rkprintf("[KRN] Kernel resource pre-exec init\n");
+    rkprintf("[KRN] MSR=%08x\n", rdmsr());
+    
+    /* Disable interrupts */
+    wrmsr(rdmsr() & ~(MSR_CE | MSR_EE | MSR_ME));
+
+    
 }
 
+/* A very very very.....
+ * ... very ugly code.
+ * 
+ * The AROS kernel gets executed at this place. The stack is unknown here, might be
+ * set properly up, might be totally broken aswell and thus one cannot trust the contents
+ * of %r1 register. Even worse, the kernel has been relocated most likely to some virtual 
+ * address and the MMU mapping might be not ready now.
+ * 
+ * The strategy is to create one MMU entry first, mapping first 16MB of ram into last 16MB 
+ * of address space in one turn and then making proper MMU map once the bss sections are cleared
+ * and the startup routine in C is executed. This "trick" assume two evil things:
+ * - the kernel will be loaded (and will fit completely) within first 16MB of RAM, and
+ * - the kernel will be mapped into top (last 16MB) of memory.
+ * 
+ * Yes, I'm evil ;) 
+ */ 
+ 
 asm(".section .aros.init,\"ax\"\n\t"
     ".globl start\n\t"
     ".type start,@function\n"
     "start:\n\t"
-    "lis %r9,tmp_stack_end@ha\n\t"
-    "mr %r29,%r3\n\t"
+    "mr %r29,%r3\n\t"           /* Don't forget the message */
+    "lis %r9,0xff00\n\t"        /* Virtual address 0xff000000 */
+    "li %r10,0\n\t"             /* Physical address 0x00000000 */
+    "ori %r9,%r9,0x0270\n\t"    /* 16MB page. Valid one */
+    "li %r11,0x043f\n\t"        /* Write through cache. RWX enabled :) */
+    "li %r0,0\n\t"              /* TLB entry number 0 */
+    "tlbwe %r9,%r0,0\n\t"
+    "tlbwe %r10,%r0,1\n\t"
+    "tlbwe %r11,%r0,2\n\t"
+    "isync\n\t"                         /* Invalidate shadow TLB's */
+    "lis %r9,tmp_stack_end@ha\n\t"      /* Use temporary stack while clearing BSS */
     "lwz %r1,tmp_stack_end@l(%r9)\n\t"
-    "bl __clear_bss\n\t"
-    "lis %r11,target_address@ha\n\t"
-    "mr %r3,%r29\n\t"
+    "bl __clear_bss\n\t"                /* Clear 'em ALL!!! */
+    "lis %r11,target_address@ha\n\t"    /* Load the address of init code in C */
+    "mr %r3,%r29\n\t"                   /* restore the message */
     "lwz %r11,target_address@l(%r11)\n\t"
-    "lis %r9,stack_end@ha\n\t"
+    "lis %r9,stack_end@ha\n\t"          /* Use brand new stack to do evil things */
     "mtctr %r11\n\t"
     "lwz %r1,stack_end@l(%r9)\n\t"
-    "bctrl\n\t"
+    "bctrl\n\t"                         /* And start the game... */
     "\n1: b 1b\n\t"
     ".string \"Native/CORE v3 (" __DATE__ ")\""
     "\n\t.text\n\t"
@@ -75,6 +112,7 @@ static uint32_t stack[STACK_SIZE] __attribute__((used));
 static uint32_t stack_super[STACK_SIZE] __attribute__((used));
 static const uint32_t *stack_end __attribute__((used, section(".text"))) = &stack[STACK_SIZE-16];
 static const void *target_address __attribute__((used, section(".text"))) = (void*)kernel_cstart;
+static struct TagItem *BootMsg;
 
 struct TagItem *krnNextTagItem(const struct TagItem **tagListPtr)
 {
@@ -130,4 +168,14 @@ IPTR krnGetTagData(Tag tagValue, intptr_t defaultVal, const struct TagItem *tagL
         return ti->ti_Data;
 
         return defaultVal;
+}
+
+AROS_LH0I(struct TagItem *, KrnGetBootInfo,
+         struct KernelBase *, KernelBase, 10, Kernel)
+{
+    AROS_LIBFUNC_INIT
+
+    return BootMsg;
+
+    AROS_LIBFUNC_EXIT
 }
