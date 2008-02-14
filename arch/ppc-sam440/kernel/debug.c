17d46c36d762e0fd60a1dff3fc4bf0c49a56979f
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-02-14 22:40:56 +0000

    Added variadic function KrnBug to the kernel.resource. At the very moment this function prints the debug onto serial port. In very near future it will store the debug messages in log stored in memory.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@27850 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/ppc-sam440/kernel/debug.c
@@ -0,0 +1,53 @@
+#include <asm/amcc440.h>
+#include <asm/io.h>
+#include <aros/libcall.h>
+#include <stdarg.h>
+
+#include "kernel_intern.h"
+
+struct PrivData {
+    struct KernelBase *kbase;
+};
+
+void __putc(char c)
+{
+    if (c == '\n')
+    {
+        while(!(inb(UART0_LSR) & UART_LSR_TEMT));
+        outb('\r', UART0_THR);
+    }
+    while(!(inb(UART0_LSR) & UART_LSR_TEMT));
+    outb(c, UART0_THR);
+}
+
+void __puts(char *str)
+{
+    while (*str)
+    {
+        if (*str == '\n')
+            __putc('\r');
+        __putc(*str++);
+    }
+}
+
+static int krnPutC(int chr, struct PrivData *data)
+{
+    __putc(chr);
+
+    return 1;
+}
+
+AROS_LH2(void, KrnBug,
+         AROS_LHA(const char *, format, A0),
+         AROS_LHA(va_list, args, A1),
+         struct KernelBase *, KernelBase, 11, Kernel)
+{
+    AROS_LIBFUNC_INIT
+
+    struct PrivData data;
+    
+    data.kbase = KernelBase;
+    __vcformat(&data, krnPutC, format, args);
+
+    AROS_LIBFUNC_EXIT
+}
