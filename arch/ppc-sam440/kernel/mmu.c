261fa82a6698fecc9b7bffb767a5e43edcdeb7d5
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-02-17 23:08:51 +0000

    MMU code.
    AROS on SAM440 puts the kernel.resource and all modules loaded together with it into uppest 16MB of addressable space. The kernel itself begins then at 0xff800000, whereas the physical location is 0x00800000. Starting from this point the executable sections (write protected) grow upwards and the data sections (not executable) are gowing downwards. The 8MB - sizeof(writable sections) area is reserved to the supervisor part of kernel.resource only (not executable, read / write from supervisor mode only).
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@27869 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/ppc-sam440/kernel/mmu.c
+++ arch/ppc-sam440/kernel/mmu.c
@@ -4,7 +4,157 @@
 
 #include "kernel_intern.h"
 
+static long tlb_bitmap[2] = { 0xffffffff, 0xffffffff };
+
+/* Alloc TLB in the bitmap. Returns -1 if the allocation cannot be done */
+static int alloc_tlb()
+{
+    /* It should be done in locked state only! */
+    
+    int bit = __builtin_clz(tlb_bitmap[0]);
+    if (bit == 32)
+    {
+        bit += __builtin_clz(tlb_bitmap[1]);
+    }
+    else
+    {
+        tlb_bitmap[0] &= ~(0x80000000 >> bit);
+    }
+    if (bit == 64)
+    {
+        return -1;
+    }
+    else
+    {
+        tlb_bitmap[1] &= ~(0x80000000 >> (bit-32));
+    }
+    return bit;
+}
+
+static int free_tlb(int entry)
+{
+    if (entry >=0 && entry < 32)
+    {
+        if (tlb_bitmap[0] & (0x80000000 >> entry))
+        {
+            D(bug("[KRN] Freeing already free TLB!!!\n"));
+        }
+        else
+            tlb_bitmap[0] |= (0x80000000 >> entry);
+    }
+    else if (entry < 64)
+    {
+        entry -= 32;
+        if (tlb_bitmap[1] & (0x80000000 >> entry))
+        {
+            D(bug("[KRN] Freeing already free TLB!!!\n"));
+        }
+        else
+            tlb_bitmap[1] |= (0x80000000 >> entry);
+    }
+    else
+    {
+        D(bug("[KRN] Wrong TLB\n"));
+    }
+}
+
+static struct mmu_page_size {
+    uint8_t     code;
+    uintptr_t   mask;
+} allowable_pages[] = {
+        { 0x90, 0x0fffffff },   /* 256MB */
+        { 0x70, 0x00ffffff },   /* 16MB */
+        { 0x50, 0x000fffff },   /* 1MB */
+        { 0x40, 0x0003ffff },   /* 256KB */
+        { 0x30, 0x0000ffff },   /* 64KB */
+        { 0x20, 0x00003fff },   /* 16KB */
+        { 0x10, 0x00000fff },   /* 4KB */
+        { 0x00, 0x000003ff },   /* 1KB */
+        { 0xff, 0xffffffff },   /* END MARKER */
+};
+
+void map_region(uintptr_t physbase, uintptr_t virtbase, uintptr_t length, uint32_t prot)
+{
+    int i;
+    int tlb;
+    
+    D(bug("[KRN] map_region(%p, %p, %08x, %04x)\n", physbase, virtbase, length, prot));
+    
+    /* While there is still something to map */
+    while (length)
+    {
+        i = 0;
+        /* Check all available page sizes and try to match the best (the biggest) usable TLB entry */
+        while (allowable_pages[i].code != 0xff)
+        {
+            if ((length > allowable_pages[i].mask) && !(physbase & allowable_pages[i].mask) && !(virtbase & allowable_pages[i].mask))
+                break;
+            i++;
+        }
+        
+        if (allowable_pages[i].code == 0xff)
+        {
+            D(bug("[KRN] map_region failed\n"));
+            return;
+        }
+        
+        /* get free TLB */
+        tlb = alloc_tlb();
+        if (tlb == -1)
+        {
+            D(bug("[KRN] map_region: No more free TLB entries\n"));
+            return;
+        }
+        
+        D(bug("[KRN] TLB%02x: %08x - %08x : %08x - %08x: ", tlb, 
+              physbase, physbase + allowable_pages[i].mask,
+              virtbase, virtbase + allowable_pages[i].mask));
+        
+        /* Do really write to the tlb */
+        asm volatile("tlbwe %0,%3,0; tlbwe %1,%3,1; tlbwe %2,%3,2"
+                     ::"r"(virtbase | allowable_pages[i].code | TLB_V), "r"(physbase), "r"(prot), "r"(tlb));
+        D(bug("%08x %08x %08x\n", virtbase | allowable_pages[i].code | 0x200, physbase, prot));
+        
+        length -= allowable_pages[i].mask + 1;
+        physbase += allowable_pages[i].mask + 1;
+        virtbase += allowable_pages[i].mask + 1;
+        
+    }
+}
+
 void mmu_init(struct TagItem *tags)
 {
+    /* 
+     * In order to reduce the usage of TLB entries, align the kernel 
+     * regions to the 64KB boundary. It wastes a tiny bit of RAM but saves a lot of
+     * TLB entries
+     */
+    uintptr_t krn_lowest = krnGetTagData(KRN_KernelLowest, 0, tags) & 0xffff0000;
+    uintptr_t krn_highest = (krnGetTagData(KRN_KernelHighest, 0, tags) + 0xffff) & 0xffff0000;
+    uintptr_t krn_base = (krnGetTagData(KRN_KernelBase, 0, tags));
+    
     D(bug("[KRN] MMU Init\n"));
+    D(bug("[KRN] lowest = %p, highest = %p\n", krn_lowest, krn_highest));
+
+    /* 
+     * The very first entry has to cover the executable part of kernel, 
+     * where exception handlers are located
+     */
+    map_region(krn_base, 0xff000000 + krn_base, krn_highest - krn_base, TLB_SR | TLB_SX | TLB_UR | TLB_UX);
+    /* Now the data area for kernel. Make it read/write for both user and supervisor. no execution allowed */
+    map_region(krn_lowest, 0xff000000 + krn_lowest, krn_base - krn_lowest, TLB_SR | TLB_SW | TLB_UR | TLB_UW);
+    /* The low memory will be RW assigned to the supervisor mode. No access from usermode! */
+    map_region(0, 0xff000000, krn_lowest, TLB_SR | TLB_SW);
+    
+    /* The regular RAM, make 1GB of it - amcc440 cannot do more. */
+    map_region(krn_highest, krn_highest, 0x40000000 - krn_highest, TLB_SR | TLB_SW | TLB_UR | TLB_UW | TLB_SX | TLB_UX );
+    
+    /* map the PCI bus */
+    map_region(0xa0000000, 0xa0000000, 0x40000000, TLB_SR | TLB_SW | TLB_UR | TLB_UW | TLB_G | TLB_I );
+
+    /* PCI control registers and onboard devices */
+    map_region(0xe0000000, 0xe0000000, 0x10000000, TLB_SR | TLB_SW | TLB_UR | TLB_UW | TLB_G | TLB_I);
+
+    /* flush TLB shadow regs */
+    asm volatile("isync;");
 }
