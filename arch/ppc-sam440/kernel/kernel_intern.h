390861cd28dbfbc862dce16777bebbd29931f50b
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-02-17 23:11:30 +0000

    The UIC interrupt controller of amcc440 supports 52 internal interrupt sources and 10 external
    git-svn-id: https://svn.aros.org/svn/aros/trunk@27870 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/ppc-sam440/kernel/kernel_intern.h
+++ arch/ppc-sam440/kernel/kernel_intern.h
@@ -19,7 +19,7 @@ struct KernelBase {
     struct Node         kb_Node;
     void *              kb_MemPool;
     struct List         kb_Exceptions[16];
-    struct List         kb_Interrupts[16];
+    struct List         kb_Interrupts[62];
 };
 
 struct KernelBSS {
