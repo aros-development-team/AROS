e09f996575baa36262c03b53b743f093c86ad8ca
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-02-25 23:13:36 +0000

    Complete amcc440 specific exec.library. Please note that this code doubles the x86_64 arch. Note for the future: perhaps we could make exec.library arch undependand. This library could actually use the kernel.resource for most (or all) hardware-dependant operations.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@27909 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/ppc-sam440/exec/rawputchar.c
@@ -0,0 +1,62 @@
+/*
+    Copyright © 1995-2007, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: Emit one character via raw IO
+    Lang: english
+*/
+
+/*****i***********************************************************************
+
+    NAME */
+#include <aros/libcall.h>
+#include <proto/exec.h>
+#include <proto/kernel.h>
+
+#include "../kernel/kernel_intern.h"
+
+        AROS_LH1(void, RawPutChar,
+
+/*  SYNOPSIS */
+        AROS_LHA(UBYTE, chr, D0),
+
+/*  LOCATION */
+        struct ExecBase *, SysBase, 86, Exec)
+
+/*  FUNCTION
+        Emits a single character.
+
+    INPUTS
+        chr - The character to emit
+
+    RESULT
+        None.
+
+    NOTES
+        This function is for very low level debugging only.
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+        RawIOInit(), RawPutChar(), RawMayGetChar()
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    void *KernelBase = getKernelBase();
+    
+    /* Don't write 0 bytes */
+    if (chr)
+    {
+        bug("%c",chr);
+    }
+
+    AROS_LIBFUNC_EXIT
+} /* RawPutChar */
