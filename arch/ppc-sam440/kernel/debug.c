fef00344bdc20d146b19c261fb7536347bf6fccb
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-02-25 23:07:50 +0000

    KrnBug returns an int, just like rkprintf and vprintf functions of AROSSupportBase should do.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@27905 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/ppc-sam440/kernel/debug.c
+++ arch/ppc-sam440/kernel/debug.c
@@ -28,20 +28,23 @@ static int krnPutC(int chr, struct PrivData *data)
     return 1;
 }
 
-AROS_LH2(void, KrnBug,
+AROS_LH2(int, KrnBug,
          AROS_LHA(const char *, format, A0),
          AROS_LHA(va_list, args, A1),
          struct KernelBase *, KernelBase, 11, Kernel)
 {
     AROS_LIBFUNC_INIT
     uint32_t tmp;
+    int result;
     struct PrivData data;
     
     /* Get store TimeBase of the Debug event in private data area */
     asm volatile("1: mftbu %0; mftbl %1; mftbu %2; cmpw %0,%2; bne- 1b":"=r"(data.tbu),"=r"(data.tbl), "=r"(tmp)::"cc");
     data.kbase = KernelBase;
 
-    __vcformat(&data, krnPutC, format, args);
+    result = __vcformat(&data, krnPutC, format, args);
 
+    return result;
+    
     AROS_LIBFUNC_EXIT
 }
