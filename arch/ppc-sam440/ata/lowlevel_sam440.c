4b83186c7291e9dddad03bf0182b76b3ff534ee4
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-04-03 21:02:27 +0000

    sam440 addons to the ata.device
    git-svn-id: https://svn.aros.org/svn/aros/trunk@28212 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/ppc-sam440/ata/lowlevel_sam440.c
@@ -0,0 +1,61 @@
+#include <exec/types.h>
+#include <asm/io.h>
+#include <asm/amcc440.h>
+
+VOID ata_insw(APTR address, UWORD port, ULONG count)
+{
+    UWORD *addr = address;
+    UWORD *p = (UWORD*)(port + PCIC0_IO);
+    
+    while(count)
+    {
+        *addr++ = inw(p);
+        count -= 2;
+    }
+}
+
+VOID ata_insl(APTR address, UWORD port, ULONG count)
+{
+    if (count & 2)
+        ata_insw(address, port, count);
+    else
+    {
+        ULONG *addr = address;
+        ULONG *p = (ULONG*)(port + PCIC0_IO);
+        
+        while(count)
+        {
+            *addr++ = inl(p);
+            count -= 4;
+        }
+    }
+}
+
+VOID ata_outsw(APTR address, UWORD port, ULONG count)
+{
+    UWORD *addr = address;
+    UWORD *p = (UWORD*)(port + PCIC0_IO);
+    
+    while(count)
+    {
+        outw(*addr++, p);
+        count -= 2;
+    }
+}
+
+VOID ata_outsl(APTR address, UWORD port, ULONG count)
+{
+    if (count & 2)
+        ata_outsw(address, port, count);
+    else
+    {
+        ULONG *addr = address;
+        ULONG *p = (ULONG*)(port + PCIC0_IO);
+        
+        while(count)
+        {
+            outl(*addr++, p);
+            count -= 4;
+        }
+    }
+}
