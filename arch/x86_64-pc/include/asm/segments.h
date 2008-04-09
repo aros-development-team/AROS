69aab817ad64b1833234c64a4d4491b791bb0b7d
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-04-09 20:55:55 +0000

    small reorganisation of segment registers
    git-svn-id: https://svn.aros.org/svn/aros/trunk@28272 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/x86_64-pc/include/asm/segments.h
+++ arch/x86_64-pc/include/asm/segments.h
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    Copyright ï¿½ 1995-2001, The AROS Development Team. All rights reserved.
     $Id: segments.h 22006 2004-08-05 19:06:53Z stegerg $
 */
 
@@ -17,9 +17,9 @@
 #define USER_CS32 0x1b
 #define USER_CS   0x2b
 #define USER_DS   0x23
-#define TASK_SEG  0x30
-#define SEG_GS    0x40
-#define SEG_LDT   0x48
+#define SEG_GS    0x30
+#define SEG_LDT   0x38
+#define TASK_SEG  0x40
 
 #define IN_USER_MODE \
 	({  short __value; \
