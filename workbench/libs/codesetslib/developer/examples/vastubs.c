c614406b1ff0b7ef8e2e106adcb19866659175bf
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-04-17 21:19:20 +0000

    r7591@lvps87-230-33-50:  verhaegs | 2008-04-17 22:58:03 +0200  Merged revision 95 of codesetslib:  * developer/examples/vastubs.c, developer/examples/makefile.os3: added    vararg stubs for OS3 to be able to link against clib2.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@28341 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/codesetslib/developer/examples/vastubs.c
@@ -0,0 +1,49 @@
+/***************************************************************************
+
+ codesets.library - Amiga shared library for handling different codesets
+ Copyright (C) 2001-2005 by Alfonso [alfie] Ranieri <alforan@tin.it>.
+ Copyright (C) 2005-2007 by codesets.library Open Source Team
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ codesets.library project: http://sourceforge.net/projects/codesetslib/
+
+ Most of the code included in this file was relicensed from GPL to LGPL
+ from the source code of SimpleMail (http://www.sf.net/projects/simplemail)
+ with full permissions by its authors.
+
+ $Id$
+
+***************************************************************************/
+
+#if defined(__VBCC__) || defined(NO_INLINE_STDARG)
+#if !defined(__PPC__)
+
+#include <exec/types.h>
+
+/* FIX V45 breakage... */
+#if INCLUDE_VERSION < 45
+#define MY_CONST_STRPTR CONST_STRPTR
+#else
+#define MY_CONST_STRPTR CONST STRPTR
+#endif
+
+#include <proto/intuition.h>
+
+APTR NewObject( struct IClass *classPtr, CONST_STRPTR classID, Tag tag1, ... )
+{ return NewObjectA(classPtr, classID, (struct TagItem *)&tag1); }
+ULONG SetAttrs( APTR object, ULONG tag1, ... )
+{ return SetAttrsA(object, (struct TagItem *)&tag1); }
+
+#else
+  #error "VARGS stubs are only save on m68k systems!"
+#endif
+#endif
