9c7066ddb8a259e656324d330e5a8b8f8dba4a36
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-04-19 11:37:31 +0000

    added (almost useless) cache management functions
    git-svn-id: https://svn.aros.org/svn/aros/trunk@28379 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/i386-pc/exec/cachecleare.c
@@ -0,0 +1,80 @@
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id: cachecleare.c 28252 2008-04-07 20:30:41Z schulz $
+
+    Desc: CacheClearE() - Clear the caches with extended control.
+    Lang: english
+*/
+
+#include <exec/types.h>
+#include <exec/execbase.h>
+#include <aros/libcall.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/exec.h>
+
+	AROS_LH3(void, CacheClearE,
+
+/*  SYNOPSIS */
+	AROS_LHA(APTR, address, A0),
+	AROS_LHA(ULONG, length, D0),
+	AROS_LHA(ULONG, caches, D1),
+
+/*  LOCATION */
+	struct ExecBase *, SysBase, 107, Exec)
+
+/*  FUNCTION
+	Flush the contents of the CPU instruction or data caches. If some
+	of the cache contains dirty data, push it to memory first.
+
+	For most systems DMA will not effect processor caches. If *any*
+	external (non-processor) event changes system memory, you MUST
+	clear the cache. For example:
+
+	    DMA
+	    Code relocation to run at a different address
+	    Building jump tables
+	    Loading code from disk
+
+    INPUTS
+	address -   Address to start the operation. This address may be
+		    rounded DOWN due to hardware granularity.
+	length	-   Length of the memory to flush. This will be rounded
+		    up, of $FFFFFFFF to indicate that all addresses
+		    should be cleared.
+	caches	-   Bit flags to indicate which caches should be cleared
+
+			CACRF_ClearI	-   Clear the instruction cache
+			CACRF_ClearD	-   Clear the data cache
+
+		    All other bits are reserved.
+
+    RESULT
+	The caches will be flushed.
+
+    NOTES
+	It is possible that on some systems the entire cache will be
+	even if this was not the specific request.
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	CacheClearU(), CacheControl()
+
+    INTERNALS
+	This is a rather CPU dependant function. You should replace it
+	in your $(KERNEL).
+
+******************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    asm volatile("wbinvd":::"memory");
+    
+    AROS_LIBFUNC_EXIT
+} /* CacheClearE */
+
