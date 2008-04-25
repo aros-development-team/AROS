34a00717c0e6abc42f5e2223303368c1c4eac4f4
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-04-25 08:38:13 +0000

    POSIX #include directives moved to the top because of AROS __unused macro conflicting with __unused field present in sys/stat.h stat structure on some GNU/Linux distributions.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@28455 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-unix/devs/filesys/emul_handler/emul_handler.c
+++ arch/all-unix/devs/filesys/emul_handler/emul_handler.c
@@ -15,6 +15,35 @@
 
 /*********************************************************************************************/
 
+#include <aros/host-conf.h>
+
+/* POSIX includes */
+#define timeval sys_timeval
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <errno.h>
+#include <sys/stat.h>
+#ifdef HAVE_SYS_STATFS_H
+#   include <sys/statfs.h>
+#endif
+#include <sys/time.h>
+#include <pwd.h>
+#undef timeval
+
+#ifdef HAVE_SYS_VFS_H
+#   include <sys/vfs.h>
+#endif
+#ifdef HAVE_SYS_PARAM_H
+#   include <sys/param.h>
+#endif
+#ifdef HAVE_SYS_MOUNT_H
+#   include <sys/mount.h>
+#endif
+
 /* AROS includes */
 
 # define  DEBUG  1
@@ -45,36 +74,6 @@
 #include <libraries/expansionbase.h>
 #include <proto/oop.h>
 #include <oop/oop.h>
-
-#include <aros/host-conf.h>
-
-/* POSIX includes */
-#define timeval sys_timeval
-#include <unistd.h>
-#include <fcntl.h>
-#include <dirent.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stddef.h>
-#include <errno.h>
-#include <sys/stat.h>
-#ifdef HAVE_SYS_STATFS_H
-#   include <sys/statfs.h>
-#endif
-#include <sys/time.h>
-#include <pwd.h>
-
-#undef timeval
-
-#ifdef HAVE_SYS_VFS_H
-#   include <sys/vfs.h>
-#endif
-#ifdef HAVE_SYS_PARAM_H
-#   include <sys/param.h>
-#endif
-#ifdef HAVE_SYS_MOUNT_H
-#   include <sys/mount.h>
-#endif
 
 /* Underlying OS's rename() Clib function prototype */
 #include <stdio.h>
