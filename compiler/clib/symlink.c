bb97a2cf9ae2cdcf36be7efaa644a00bc76064a5
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-05-10 11:59:35 +0000

    Implemented missing symlink function. Coreutils ln -s should work now.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@28653 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/symlink.c
+++ compiler/clib/symlink.c
@@ -7,15 +7,107 @@
 
 #include <aros/debug.h>
 
+#include <stdlib.h>
 #include <unistd.h>
+#include <proto/dos.h>
+#include <errno.h>
 #include "__errno.h"
+#include "__upath.h"
+#include <aros/debug.h>
 
 int symlink(const char *oldpath, const char *newpath)
 {
-#   warning Implement symlink()
-    AROS_FUNCTION_NOT_IMPLEMENTED("arosc");
+	int retval = -1;
+    BPTR lock;
+    struct FileInfoBlock *fib;
+    int errorset = FALSE;
+    LONG ioerr;
+    UBYTE *buffer;
+    int bufferincrease = 1;
+    int buffersize = bufferincrease;
 
-    errno = EPERM;
-    return -1;
+    if (!oldpath || !newpath) /*safety check */
+    {
+        errno = EFAULT;
+        return -1;
+    }
+
+    if(oldpath = strdup(__path_u2a(oldpath)))
+    {
+	    newpath = __path_u2a(newpath);
+	    if (!newpath)
+	    {
+	    	free(oldpath);
+	    	return -1;
+	    }
+	
+		if(lock = Lock((STRPTR)oldpath, SHARED_LOCK)) {
+			do
+			{
+				if(!(buffer = AllocVec(buffersize, MEMF_ANY)))
+		        {
+					ioerr = ERROR_NO_FREE_STORE;
+					errorset = TRUE;
+		            break;
+		        }
+				
+	    		/* Get the full path of oldpath */
+		    	if(NameFromLock(lock, buffer, buffersize))
+		    	{
+					if(MakeLink((STRPTR)newpath,
+						    (STRPTR)buffer,
+						    TRUE) == DOSTRUE)
+					    retval = RETURN_OK;
+					else
+					{
+						ioerr = IoErr();
+						errorset = TRUE;
+						break;
+					}
+		    	}
+		    	else if(IoErr() != ERROR_LINE_TOO_LONG)
+				{
+					ioerr = IoErr();
+					errorset = TRUE;
+					break;
+				}
+		    	FreeVec(buffer);
+		    	buffersize += bufferincrease;
+			}
+			while(retval != RETURN_OK);
+		    UnLock(lock);
+		}
+		else
+		{
+			ioerr = IoErr();
+			/* I'm not sure if MakeLink is allowed to create symlinks to
+			   non-existing files or directories. If yes, then it's fine to
+			   enable the following code */
+#if 0
+			if(ioerr == ERROR_OBJECT_NOT_FOUND)
+			{
+				/* On Unices it's perfectly fine to create symlinks to
+				   non-existing files or directories, however in this case it
+				   may be difficult to get the full absolute path, so we are
+				   simply trusting the user here for now */
+				if(MakeLink((STRPTR)newpath,
+					    (STRPTR)oldpath,
+					    TRUE) == DOSTRUE)
+				    retval = RETURN_OK;
+				else
+				{
+					ioerr = IoErr();
+					errorset = TRUE;
+				}			
+			}
+			else
+#endif
+				errorset = TRUE;		
+		}
+		free(oldpath);
+    }
+
+	if(errorset) errno = IoErr2errno(ioerr);
+    return retval;
 } /* symlink */
 
