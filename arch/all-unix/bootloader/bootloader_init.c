4c51e0f6f2eda723696935524931749ce6f25d1d
Kalamatee <kalamatee@gmail.com>
2008-05-20 21:24:28 +0000

    Generate bootloader protos for linux-i386 and linux-x86_64 (probably needed for linux-ppc also). Make dos generate the protos for bootmenu before compiling (needed by dosboot)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@28768 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/all-unix/bootloader/bootloader_init.c
@@ -0,0 +1,37 @@
+/*
+    Copyright © 1995-2006, The AROS Development Team. All rights reserved.
+    $Id: bootloader_init.c 25411 2007-03-12 06:54:19Z sonic $
+
+    Bootloader information initialisation.
+*/
+
+#define DEBUG 1
+#include <aros/debug.h>
+
+#include <exec/types.h>
+#include <exec/memory.h>
+#include <exec/resident.h>
+#include <utility/utility.h>
+#include <utility/tagitem.h>
+#include <proto/exec.h>
+#include <proto/bootloader.h>
+#include <proto/utility.h>
+
+#include <aros/symbolsets.h>
+#include <aros/bootloader.h>
+#include "bootloader_intern.h"
+#include LC_LIBDEFS_FILE
+
+#include <string.h>
+
+static int GM_UNIQUENAME(Init)(LIBBASETYPEPTR BootLoaderBase)
+{
+    BootLoaderBase->Flags = 0;
+    
+    NEWLIST(&(BootLoaderBase->Args));
+    NEWLIST(&(BootLoaderBase->DriveInfo));
+
+    return TRUE;
+}
+
+ADD2INITLIB(GM_UNIQUENAME(Init), 0)
---
+++ arch/all-unix/bootloader/bootloader_intern.h
@@ -0,0 +1,34 @@
+/*
+    Copyright © 1995-2007, The AROS Development Team. All rights reserved.
+    $Id: bootloader_intern.h 24260 2006-04-05 20:16:56Z verhaegs $
+
+    Internal data structures for bootloader.resource
+*/
+
+#ifndef BOOTLOADER_INTERN_H
+#define BOOTLOADER_INTERN_H
+
+#ifndef EXEC_TYPES_H
+#include <exec/types.h>
+#endif
+#ifndef EXEC_NODES_H
+#include <exec/nodes.h>
+#endif
+#ifndef UTILITY_UTILITY_H
+#include <utility/utility.h>
+#endif
+#ifndef HIDD_HIDD_H
+#include <hidd/hidd.h>
+#endif
+#include <aros/bootloader.h>
+
+struct BootLoaderBase
+{
+    struct Node		 bl_Node;
+    ULONG		 Flags;
+    STRPTR		 LdrName;
+    struct List		 Args;
+    struct List		 DriveInfo;
+};
+
+#endif //BOOTLOADER_INTERN_H
