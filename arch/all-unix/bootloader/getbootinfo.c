f1311b47accea3915540d7a3a4b4ec4cf2828079
Kalamatee <kalamatee@gmail.com>
2008-05-20 21:24:28 +0000

    Generate bootloader protos for linux-i386 and linux-x86_64 (probably needed for linux-ppc also). Make dos generate the protos for bootmenu before compiling (needed by dosboot)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@28768 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/all-unix/bootloader/getbootinfo.c
@@ -0,0 +1,63 @@
+/*
+    Copyright © 1995-2003, The AROS Development Team. All rights reserved.
+    $Id: getbootinfo.c 16670 2003-03-08 19:02:51Z jogr0326 $
+
+    GetBootInfo() function.
+*/
+#include "bootloader_intern.h"
+#include <proto/bootloader.h>
+#include <proto/utility.h>
+#include <aros/debug.h>
+
+/*****************************************************************************
+
+    NAME */
+	AROS_LH1(APTR, GetBootInfo,
+
+/*  SYNOPSIS */
+	AROS_LHA(ULONG, infoType, D0),
+
+/*  LOCATION */
+	struct BootLoaderBase *, BootLoaderBase, 1, Bootloader)
+
+/*  FUNCTION
+	Return information from the bootloader
+
+    INPUTS
+	infoType - The type of information requestes
+
+    RESULT
+	Pointer to resource or NULL if failed
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+    
+    switch (infoType)
+    {
+	case BL_Args:
+	    if (BootLoaderBase->Flags & MB_FLAGS_CMDLINE)
+		return (APTR)&(BootLoaderBase->Args);
+	    break;
+	case BL_Drives:
+	    if (BootLoaderBase->Flags & MB_FLAGS_DRIVES)
+		return (APTR)&(BootLoaderBase->DriveInfo);
+	    break;
+	default:
+	    bug("[BootLdr] GetBootInfo: Unknown info requested\n");
+    }
+
+    return NULL;
+
+    AROS_LIBFUNC_EXIT
+} /* GetBootInfo */
