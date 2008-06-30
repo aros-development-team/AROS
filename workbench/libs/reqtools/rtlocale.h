b49527a5aa51580de98ec6d7852088513128f3db
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-06-30 20:48:24 +0000

    r8427@lvps87-230-33-50:  verhaegs | 2008-06-22 11:57:57 +0200  For AROS use the prototypes provided by the locale includes for the  CloseCatalog, GetCatalogStr and OpenCatalogA functions.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@28951 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/reqtools/rtlocale.h
+++ workbench/libs/reqtools/rtlocale.h
@@ -8,12 +8,12 @@
 
 #ifndef __AROS__
 extern struct LocaleBase *LocaleBase;
+
+void CloseCatalog (struct Catalog *);
+STRPTR GetCatalogStr (struct Catalog *, LONG, STRPTR);
+struct Catalog *OpenCatalogA (struct Locale *, STRPTR, struct TagItem *);
 #endif
 
 char *REGARGS GetStr (struct Catalog *, char *);
 struct Catalog *REGARGS RT_OpenCatalog (struct Locale *);
 void REGARGS RT_CloseCatalog (struct Catalog *);
-
-void CloseCatalog (struct Catalog *);
-STRPTR GetCatalogStr (struct Catalog *, LONG, STRPTR);
-struct Catalog *OpenCatalogA (struct Locale *, STRPTR, struct TagItem *);
