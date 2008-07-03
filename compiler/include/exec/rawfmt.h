1fee807039ace84a9b31947d3006fa63ef88f214
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-07-03 06:06:37 +0000

    Cleaned up types
    git-svn-id: https://svn.aros.org/svn/aros/trunk@28963 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/exec/rawfmt.h
+++ compiler/include/exec/rawfmt.h
@@ -1,12 +1,14 @@
 #ifndef EXEC_RAWFMT_H
 #define EXEC_RAWFMT_H
 
+#include <aros/asmcall.h>
+
 /* Magic constants for RawDoFmt() anv VNewRawDoFmt() to be given as
    PutChProc */
 
-#define RAWFMTFUNC_STRING 0 /* Output to string given in PutChData	     */
-#define RAWFMTFUNC_SERIAL 1 /* Output to debug log (usually serial port)     */
-#define RAWFMTFUNC_COUNT  2 /* Just count characters, PutChData is a pointer
-			       to the counter (ULONG *)			     */
+#define RAWFMTFUNC_STRING (VOID_FUNC)0 /* Output to string given in PutChData	        */
+#define RAWFMTFUNC_SERIAL (VOID_FUNC)1 /* Output to debug log (usually serial port)     */
+#define RAWFMTFUNC_COUNT  (VOID_FUNC)2 /* Just count characters, PutChData is a pointer
+					  to the counter (ULONG *)			*/
 
 #endif
