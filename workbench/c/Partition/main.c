22fe279be0e5fa35940f626d16a39fb34dee2746
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-08-17 01:11:05 +0000

    - Rewrote and expanded autodoc.  - Now only indicates successful completion when no error occurs.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29187 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/Partition/main.c
+++ workbench/c/Partition/main.c
@@ -16,36 +16,74 @@
 
     LOCATION
 
-        Sys:C
+        C:
 
     FUNCTION
 
-        Partition allocates the space on a given device (usually a hard 
-        drive) so that it can be formatted after.
+        Partition creates either one or two AROS partitions on a given drive.
+        Existing partitions will be kept unless the WIPE option is specified
+        (or a serious bug occurs, for which we take no responsibility).
+        Partitions created by this command must be formatted before they can
+        be used.
+
+        By default, a single SFS System partition is created using the
+        largest amount of free space possible. A smaller size can be chosen
+        using the SYSSIZE argument. To also create a Work partition, either
+        WORKSIZE or MAXWORK must additionally be specified. The WORKSIZE
+        argument allows the size of the Work partition to be specified,
+        while setting the MAXWORK switch makes the Work partition as large
+        as possible.
+
+        The filesystems used by the System and Work partitions may be
+        specified using the SYSTYPE and WORKTYPE arguments respectively.
+        The available options are "SFS" (Smart Filesystem, the default), and
+        "FFSIntl" (the traditional so-called Fast Filesystem).
+
+        If you wish to use only AROS on the drive you run this command on,
+        you can specify the WIPE option, which destroys all existing
+        partitions on the drive. Be very careful with this option: it
+        deletes all other operating systems and data on the drive, and could
+        be disastrous if the wrong drive is accidentally partitioned.
+
+        If the drive does not already contain an extended partition, one is
+        created using the largest available region of free space. The AROS
+        partitions are then created as a logical partition within. This is
+        in order to make the addition of further partitions easier.
 
     INPUTS
 
-          DEVICE – Device type (ie. ata.device)
-	UNIT    --  The device disk unit designation
-	SYSSIZE   --  Sets the System (bootable) patition size.
-	WORKSIZE  --  Sets a secondary partition size.
-	MAXWORK  --  Sets the secondary partition with the rest of space available.
-	WIPE  --  Wipes all partitions defined earlier (CAUTION!)
-	FORCE – QUIET - - Won’t generate any kind of output 
+	DEVICE -- Device driver name (ata.device by default)
+	UNIT -- The drive's unit number (0 by default, which is the primary
+            master when using ata.device)
+	SYSSIZE -- The System (boot) partition size in megabytes.
+	SYSTYPE -- The file system to use for the system partition, either
+            "SFS" (the default) or "FFSIntl".
+	WORKSIZE -- The Work (secondary) partition size in megabytes. To use
+            this option, SYSSIZE must also be specified.
+	MAXWORK -- Make the Work partition as large as possible. To use this
+            option, SYSSIZE must also be specified.
+	WORKTYPE -- The file system to use for the work partition, either
+            "SFS" (the default) or "FFSIntl".
+	WIPE -- Destroy all other partitions on the drive, including those
+            used by other operating systems (CAUTION!).
+	FORCE -- Do not ask for confirmation before partitioning the drive.
+        QUIET -- Do not print any output. This option can only be used when
+            FORCE is also specified.
 
     RESULT
 
         Standard DOS error codes.
-	
+
     NOTES
 
-        This is a very dangerous command – Users should use the HDtoolbox in the utilities 
-         drawer instead.
+        Using HDToolBox instead of this command may sometimes be safer, as
+        it shows where partitions will be created on the drive before
+        changes are written to disk. However, HDToolBox can be unreliable.
 
     EXAMPLE
 
-        Partition ata.device 1 WIPE
-	
+        Partition ata.device 1 SYSSIZE 200 MAXWORK
+
     BUGS
 
     SEE ALSO
@@ -365,7 +403,7 @@ int main(void)
     if (root != NULL)
         CloseRootPartition(root);
 
-    if (!ARG(QUIET)) Printf("done\n");
+    if (!ARG(QUIET) && error == 0) Printf("Partitioning completed\n");
 
     PrintFault(error, NULL);
     return (error == 0) ? RETURN_OK : RETURN_FAIL;
