730f296a27fcf592baefd4c539e5c8adaabcd85e
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-08-21 12:17:32 +0000

    Next lap of NIH removal
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29211 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-mingw32/bootstrap/bootstrap.c
+++ arch/all-mingw32/bootstrap/bootstrap.c
@@ -3,27 +3,40 @@
 #include <strings.h>
 #include <signal.h>
 #include <windows.h>
-#include <aros/hostboot.h>
+#include <aros/system.h>
+
+#define __typedef_LONG /* LONG, ULONG, WORD, BYTE and BOOL are declared in Windows headers. Looks like everything  */
+#define __typedef_WORD /* is the same except BOOL. It's defined to short on AROS and to int on Windows. This means */
+#define __typedef_BYTE /* that you can't use it in OS-native part of the code and can't use any AROS structure     */
+#define __typedef_BOOL /* definition that contains BOOL.                                                           */
+typedef unsigned AROS_16BIT_TYPE UWORD;
+typedef unsigned char UBYTE;
+
+#include <aros/kernel.h>
+#include <utility/tagitem.h>
 
 #include "elfloader32.h"
 #include "hostlib.h"
-#include "../hostlib/hostlib_intern.h"
 
 static unsigned char __bss_track[32768];
+struct TagItem km[64];
 
-typedef int (*kernel_entry_fun_t)(struct HostBootInfo *);
+typedef int (*kernel_entry_fun_t)(struct TagItem *, struct HostInterface *);
 
 #define BASE_ALIGNMENT 16
 
-struct HostLibInterface HostLibIFace = {
-    Kern_HostLib_Open,
-    Kern_HostLib_Close,
-    Kern_HostLib_GetPointer,
-    Kern_HostLib_FreeErrorStr,
-    Kern_HostLib_GetInterface
+/*
+ * Some helpful functions that link us to the underlying host OS.
+ * Without them we would not be able to estabilish any interaction with it.
+ */
+struct HostInterface HostIFace = {
+    Host_HostLib_Open,
+    Host_HostLib_Close,
+    Host_HostLib_GetPointer,
+    Host_HostLib_FreeErrorStr,
+    Host_HostLib_GetInterface
 };
 
-struct HostBootInfo bootinfo;
 void *SysBase;
 
 int main(int argc, char ** argv)
@@ -40,6 +53,25 @@ int main(int argc, char ** argv)
   unsigned int memSize = 100;  
   printf("[Bootstrap] allocating working mem: %iMb\n",memSize);
   void * memory = malloc((memSize << 20));
+
+  //fill in kernel message related to allocated ram regions
+  struct TagItem *tag = km;
+
+  tag->ti_Tag = KRN_KernelBss;
+  tag->ti_Data = (unsigned long)__bss_track;
+  tag++;
+
+/* FIXME: These tags should point to a memory map in PC BIOS format, not to memory itself.
+   This is a temporary solution because hosted kernel should translate all AllocMem() requests
+   to host's allocation requests. In future a full-featured memory map will be implemented in order
+   to support loadable modules. */
+  tag->ti_Tag = KRN_MMAPAddress;
+  tag->ti_Data = (unsigned long)memory;
+  tag++;
+  
+  tag->ti_Tag = KRN_MMAPLength;
+  tag->ti_Data = (unsigned long)(memSize << 20);
+  tag++;
 
   //load elf-kernel and fill in the bootinfo
   void * file = fopen(argv[1], "rb");
@@ -63,15 +95,18 @@ int main(int argc, char ** argv)
   load_elf_file(file,0);
   kernel_entry_fun_t kernel_entry_fun = (kernel_entry_fun_t)base;
 
-  bootinfo.MemBase = memory;
-  bootinfo.MemSize = memSize << 20;
-  bootinfo.KernelBss = __bss_track;
-  bootinfo.KernelLowest = kernel_lowest();
-  bootinfo.KernelHighest = kernel_highest();
-  bootinfo.HostLib_Interface = &HostLibIFace;
+  tag->ti_Tag = KRN_KernelLowest;
+  tag->ti_Data = (unsigned long)kernel_lowest();
+  tag++;
+    
+  tag->ti_Tag = KRN_KernelHighest;
+  tag->ti_Data = (unsigned long)kernel_highest();
+  tag++;
+
+  tag->ti_Tag = TAG_DONE;
 
   printf("[Bootstrap] entering kernel@%p...\n",kernel_entry_fun);
-  int retval = kernel_entry_fun(&bootinfo);
+  int retval = kernel_entry_fun(km, &HostIFace);
 
   printf("kernel returned %i\n",retval);
 }  
--- arch/all-mingw32/bootstrap/hostlib.h
+++ arch/all-mingw32/bootstrap/hostlib.h
@@ -1,5 +1,5 @@
-void *Kern_HostLib_Open(const char *filename, char **error);
-BOOL Kern_HostLib_Close(void *handle, char **error);
-void Kern_HostLib_FreeErrorStr(char *error);
-void *Kern_HostLib_GetPointer(void *handle, const char *symbol, char **error);
-unsigned long Kern_HostLib_GetInterface(void *handle, char **names, void **funcs);
+void *Host_HostLib_Open(const char *filename, char **error);
+BOOL Host_HostLib_Close(void *handle, char **error);
+void Host_HostLib_FreeErrorStr(char *error);
+void *Host_HostLib_GetPointer(void *handle, const char *symbol, char **error);
+unsigned long Host_HostLib_GetInterface(void *handle, char **names, void **funcs);
