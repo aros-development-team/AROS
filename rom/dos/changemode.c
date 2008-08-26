19f4afbb3e17607ec42808da380bb11c65acff16
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-08-26 21:59:51 +0000

    For CHANGE_FH, MODE_#? was mistakenly accepted instead of #?_LOCK.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29281 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/changemode.c
+++ rom/dos/changemode.c
@@ -1,8 +1,8 @@
 /*
-    Copyright © 1995-2007, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2008, The AROS Development Team. All rights reserved.
     $Id$
 
-    Desc: Change the mode of a filehandle or -lock.
+    Desc: Change the mode of a filehandle or lock.
     Lang: English
 */
 #include <proto/exec.h>
@@ -26,20 +26,18 @@
 	struct DosLibrary *, DOSBase, 75, Dos)
 
 /*  FUNCTION
-	Try to change the mode used by a lock or filehandle.
+	Try to change the access mode of a lock or filehandle.
 
     INPUTS
 	type    - CHANGE_FH or CHANGE_LOCK.
 	object  - Filehandle or lock.
-	newmode - New mode (see <dos/dos.h>).
+	newmode - New mode, either SHARED_LOCK or EXCLUSIVE_LOCK.
 
     RESULT
 	!= 0 if all went well, otherwise 0. IoErr() gives additional
 	information in the latter case.
 
     NOTES
-	Since filehandles and locks are identical under AROS the type
-	argument is ignored.
 
     EXAMPLE
 
@@ -48,6 +46,8 @@
     SEE ALSO
 
     INTERNALS
+	Since filehandles and locks are identical under AROS the type
+	argument is ignored.
 
 *****************************************************************************/
 {
@@ -59,48 +59,15 @@
     /* Get pointer to I/O request. Use stackspace for now. */
     struct IOFileSys iofs;
 
-    /* Convert Open() and Lock() constants to filehandler flags. */
-    ULONG newflags, mask;
-
-    if(newmode == MODE_OLDFILE || newmode == MODE_READWRITE ||
-       newmode == ACCESS_READ)
-    {
-        newflags = 0;
-        mask     = FMF_LOCK;
-    }
-    else if(newmode == MODE_NEWFILE || newmode == ACCESS_WRITE)
-    {
-        newflags = FMF_LOCK;
-        mask     = FMF_LOCK;
-    }
-    else
-    {
-	if (newmode & FMF_APPEND)
-	{
-	    /* See if the handler supports FSA_SEEK */
-	    if (!(fh->fh_Flags & FHF_APPEND) && Seek(MKBADDR(fh), 0, OFFSET_END) != -1)
-	    {
-		/* if so then set the proper flag in the FileHandle struct */
-		 fh->fh_Flags |= FHF_APPEND;
-	    }
-	}
-	else
-	    fh->fh_Flags &= ~FHF_APPEND;
-
-	newmode &= ~FMF_APPEND;
-
-	newflags = newmode;
-        mask     = 0xFFFFFFFF;
-    }
-
     /* Prepare I/O request. */
     InitIOFS(&iofs, FSA_FILE_MODE, DOSBase);
 
     iofs.IOFS.io_Device = fh->fh_Device;
     iofs.IOFS.io_Unit   = fh->fh_Unit;
 
-    iofs.io_Union.io_FILE_MODE.io_FileMode = newflags;
-    iofs.io_Union.io_FILE_MODE.io_Mask     = mask;
+    iofs.io_Union.io_FILE_MODE.io_FileMode =
+        (newmode == EXCLUSIVE_LOCK) ? FMF_LOCK : 0;
+    iofs.io_Union.io_FILE_MODE.io_Mask = FMF_LOCK;
 
     /* Send the request. */
     DosDoIO(&iofs.IOFS);
