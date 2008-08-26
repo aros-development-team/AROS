d084ab81a50875de6482d27b80182d1bf7c67035
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-08-26 13:29:26 +0000

    Added test utility for statfs function.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29265 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/clib/statfs.c
@@ -0,0 +1,34 @@
+#include <string.h>
+#include <errno.h>
+#include <sys/mount.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include "test.h"
+
+char testfilename[] = "RAM:__TEST__";
+
+int main() 
+{
+    struct statfs buf;
+    int fd;
+    
+    fd = creat(testfilename, 0777);
+    TEST((fd != -1));
+    close(fd);
+    TEST((statfs(testfilename, &buf) != -1));  
+    
+    printf("Name:\t\t\t%s\n", buf.f_mntonname);
+    printf("Fundamental block size:\t%ld\n", buf.f_fsize);
+    printf("Optimal block size:\t%ld\n", buf.f_bsize);
+    printf("Number of blocks:\t%ld\n", buf.f_blocks);
+    printf("Free blocks:\t\t%ld\n", buf.f_bfree);
+    printf("Available blocks:\t%ld\n", buf.f_bavail);
+    
+    cleanup();
+    return OK;
+}
+
+void cleanup() 
+{
+    remove(testfilename);
+}
