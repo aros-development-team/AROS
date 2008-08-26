c7f938be620c1b459f4dddcaa67f9b8dc9a6ec29
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-08-26 18:36:39 +0000

    Added test utility for fchdir function.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29277 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/clib/fchdir.c
@@ -0,0 +1,51 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <proto/dos.h>
+#include "test.h"
+
+int fd = -1;
+
+int main()
+{
+    char path1[256];
+    char path2[256];
+    
+    /* First something simple */
+    TEST((chdir("SYS:") != -1));
+    TEST((getcwd(path1, sizeof(path1)) != (char*) -1));
+    TEST(((fd = open("SYS:", 0)) != -1));
+    TEST((fchdir(fd) != -1));
+    close(fd); fd = -1;
+    TEST((getcwd(path2, sizeof(path2)) != (char*) -1));
+    printf("Comparing paths: %s and %s\n", path1, path2);
+    TEST((strcmp(path1, path2) == 0));
+
+    /* Now more complicated case */
+    TEST((mkdir("T:__TEST__", 0777) != -1));
+
+    TEST((chdir("T:__TEST__") != -1));
+    TEST((getcwd(path1, sizeof(path1)) != (char*) -1));
+    TEST(((fd = open("T:__TEST__", 0)) != -1));
+    TEST((fchdir(fd) != -1));
+    close(fd); fd = -1;
+    TEST((getcwd(path2, sizeof(path2)) != (char*) -1));
+    printf("Comparing paths: %s and %s\n", path1, path2);
+    TEST((strcmp(path1, path2) == 0));
+    
+    /* Test directory is going to disappear soon, evacuate! */
+    TEST((chdir("SYS:") != -1));
+    
+    cleanup();
+    return OK;
+}
+
+void cleanup()
+{
+    if(fd != -1)
+	close(fd);
+    
+    DeleteFile("T:__TEST__");
+}
