f679baaf287dead2fdb372d2273a74e43e6a7b1f
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-08-26 15:48:29 +0000

    Added usleep function to clib.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29269 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/usleep.c
@@ -0,0 +1,73 @@
+/*
+    Copyright © 2008, The AROS Development Team. All rights reserved.
+    $Id$
+
+    POSIX function usleep().
+*/
+
+#include <aros/debug.h>
+
+#include <exec/exec.h>
+#include <proto/exec.h>
+#include <devices/timer.h>
+
+#include <errno.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <unistd.h>
+
+	int usleep (
+
+/*  SYNOPSIS */
+	useconds_t usec) 
+        
+/*  FUNCTION
+        Suspends program execution for a given number of microseconds.
+
+    INPUTS
+        usec - number of microseconds to wait
+
+    RESULT
+        0 on success, -1 on error
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	
+    INTERNALS
+
+******************************************************************************/
+{
+    struct MsgPort      *timerMsgPort;
+    struct timerequest  *timerIO;
+    int retval = -1;
+    
+    if(timerMsgPort = CreateMsgPort())
+    {
+	timerIO = (struct timerequest *) CreateIORequest(timerMsgPort, sizeof (struct timerequest));
+	if(timerIO)
+	{
+	    if(OpenDevice("timer.device", UNIT_MICROHZ, (struct IORequest *) timerIO, 0) == 0)
+	    {
+		timerIO->tr_node.io_Command = TR_ADDREQUEST;
+		timerIO->tr_time.tv_secs    = 0;
+		timerIO->tr_time.tv_micro   = usec;
+  
+		DoIO((struct IORequest *) timerIO);
+		retval = 0;
+
+		CloseDevice((struct IORequest *) timerIO);
+	    }
+	    DeleteIORequest((struct IORequest *) timerIO);
+	}
+	DeleteMsgPort(timerMsgPort);
+    }
+    return retval;
+} /* usleep() */
+
