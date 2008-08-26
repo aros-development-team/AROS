55b6e022a8787c1bc982a70ec05249e210244ad9
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-08-26 11:21:10 +0000

    Added missing path conversion and some documentation.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29260 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/chmod.c
+++ compiler/clib/chmod.c
@@ -7,14 +7,57 @@
 #include <proto/dos.h>
 
 #include <errno.h>
-#include <sys/stat.h>
+#include <sys/types.h>
 
 #include "__errno.h"
+#include "__upath.h"
 
 ULONG prot_u2a(mode_t protect);
 
-int chmod(const char *path, mode_t mode)
+/*****************************************************************************
+
+    NAME */
+#include <sys/stat.h>
+
+	int chmod(
+
+/*  SYNOPSIS */			
+	const char *path, 
+	mode_t mode)
+
+/*  FUNCTION
+	Change permission bits of a specified file.
+
+    INPUTS
+	path - path to the file
+	mode - permission bits
+    
+    RESULT
+    	Permission bits of the file given by path are changed accordingly
+    	to given mode.
+    
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+******************************************************************************/
 {
+    if (!path) /*safety check */
+    {
+	errno = EFAULT;
+	return -1;
+    }
+
+    path = __path_u2a(path);
+    if (path == NULL)
+        return -1;
+
     if (!SetProtection(path, prot_u2a(mode)))
     {
     	errno = IoErr2errno(IoErr());
