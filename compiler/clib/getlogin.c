7d5b0ac888b182650cb24d6b95353c23b0f1d032
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-08-26 16:35:55 +0000

    Added function stub for getlogin function used in some coreutils.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29274 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/getlogin.c
@@ -0,0 +1,43 @@
+/*
+    Copyright © 2008, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <aros/debug.h>
+
+#include <errno.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <unistd.h>
+
+	char * getlogin(
+
+/*  SYNOPSIS */
+	)
+        
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	
+    INTERNALS
+
+******************************************************************************/
+{
+#   warning Implement getlogin()
+    AROS_FUNCTION_NOT_IMPLEMENTED("arosc");
+    
+    errno = ENOSYS;
+    return NULL;
+}
