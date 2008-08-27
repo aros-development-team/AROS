a22bc3194ce228dd8575644716758e9396bf62ff
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-08-27 19:52:35 +0000

    Test utility for uname function.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29301 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/clib/uname.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/utsname.h>
+#include "test.h"
+
+int main()
+{
+    struct utsname buf;
+    TEST((uname(&buf) != -1));
+
+    printf("Operating system name: %s\n", buf.sysname);
+    printf("Operating system release: %s\n", buf.release);
+    printf("Operating system version: %s\n", buf.version);
+    printf("Machine: %s\n", buf.machine);
+    
+    cleanup();
+    return OK;
+}
+
+void cleanup()
+{
+}
