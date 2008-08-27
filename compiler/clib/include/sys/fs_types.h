31e4f576c665e61a423aedf39d18e30fe3628fdd
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-08-27 19:31:23 +0000

    Added mnt_names array needed by coreutils.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29299 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/include/sys/fs_types.h
+++ compiler/clib/include/sys/fs_types.h
@@ -15,4 +15,7 @@
 #define MOUNT_ADOS_IFFS 8
 #define MOUNT_MAXTYPE   8
 
+static char *mnt_names[] = { "none", "ufs", "nfs", "mfs", "pc", "ofs",
+                             "ffs", "iofs", "iffs" };
+
 #endif /* !_SYS_FS_TYPES_H */
