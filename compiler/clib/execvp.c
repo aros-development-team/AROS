cb36f842d53669f1c7c123555039a884fb275794
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-08-29 12:56:18 +0000

    Implemented execvp by using execve function.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29311 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/execvp.c
+++ compiler/clib/execvp.c
@@ -10,11 +10,12 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
+#include <stdlib.h>
 
 /*****************************************************************************
 
     NAME */
-#include <stdio.h>
+#include <unistd.h>
 
 	int execvp(
 
@@ -23,26 +24,88 @@
         char *const argv[])
         
 /*  FUNCTION
+	Executes a file with specified arguments.
 
     INPUTS
+        file - Name of the file to execute.
+        argv - Array of arguments given to main() function of the executed
+        file.
 
     RESULT
+        0 in case of success. In case of failure errno is set appropriately
+        and function returns -1.
 
     NOTES
 
     EXAMPLE
 
     BUGS
+        See execve documentation.
 
     SEE ALSO
+        execve
 	
     INTERNALS
 
 ******************************************************************************/
 {
-#   warning Implement execvp()
-    AROS_FUNCTION_NOT_IMPLEMENTED("arosc");
+    char *path, *path_item, *path_ptr;
+    char *emptyenv[] = { NULL };
+    char default_path[] = ":/bin:/usr/bin";
+    char *full_path;
+    int saved_errno;
     
+    if(index(file, '/') || index(file, ':'))
+    {
+	/* file argument is a path, don't search */
+	return execve(file, argv, emptyenv);
+    }
+    
+    /* argument is a file, search for this file in PATH variable entries */
+    
+    if(!(path = getenv("PATH")))
+	path = default_path;
+    else
+	path = strdup(path);
+    
+    path_ptr = path;
+    
+    while((path_item = strsep(&path_ptr, ",:"))) 
+    {
+	if(path_item[0] == '\0')
+	    path_item = ".";
+	
+	if(full_path = malloc(strlen(path_item) + strlen(file) + 2))
+	{
+	    full_path[0] = '\0';
+	    strcat(full_path, path_item);
+	    strcat(full_path, "/");
+	    strcat(full_path, file);
+	    
+	    /* try executing execve with this path */
+	    if(execve(full_path, argv, emptyenv) == 0)
+	    {
+		free(full_path);
+		return 0;
+	    }
+	    else
+		if(errno == EACCES)
+		    saved_errno = EACCES;
+	    free(full_path);
+	}
+	else
+	{
+	    saved_errno = ENOMEM;
+	    goto error;
+	}
+    }
+
+    /* set ENOENT error if there were errors other than EACCES */
+    if(saved_errno != EACCES)
+	saved_errno = ENOENT;
+
+error:
+    errno = saved_errno;
     return -1;
 } /* execvp() */
 
