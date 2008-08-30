7c5fbe6d93157d669c9c22a53b9fab1531d8a0e9
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-08-30 22:20:44 +0000

    OpenFromLock() now works with packet.handler. It no longer assumes locks are equivalent to handles and always gets the file handle from the handler.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29328 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/openfromlock.c
+++ rom/dos/openfromlock.c
@@ -17,6 +17,8 @@
 #define SFS_SPECIFIC_MESSAGE 0xff00
 #endif
 
+static BPTR SFSOpenFromLock(BPTR lock, struct DosLibrary *DOSBase);
+
 /*****************************************************************************
 
     NAME */
@@ -42,9 +44,6 @@
 	additional information in that case.
 
     NOTES
-	Since locks and filehandles in AROS are identical this function
-	is just the (useless) identity operator and thus can never fail.
-	It's there for compatibility to Amiga OS.
 
     EXAMPLE
 
@@ -53,10 +52,14 @@
     SEE ALSO
 
     INTERNALS
+	Since locks and filehandles in AROS are identical this function
+	is just the (useless) identity operator and thus can never fail.
+	It's there for compatibility with Amiga OS.
+
 	As of 04/17/08:
 	- the above note is not true anymore
-	- the AROS packet implementation and the various DOS packets convertion
-	  layers can't handle packet type ACTION_FH_FROM_LOCK wich does not
+	- the AROS packet implementation and the various DOS packets conversion
+	  layers can't handle packet type ACTION_FH_FROM_LOCK which does not
 	  exist in AROS as a consequence of the above note
 	- we can continue to hack this way until death
 	- the current OpenFromLock implementation is hacked to handle SFS
@@ -66,11 +69,66 @@
 {
     AROS_LIBFUNC_INIT
 
+    BPTR file = NULL, test_lock, old_dir;
+    ULONG mode;
+
+    if (lock == NULL)
+    {
+	SetIoErr(ERROR_INVALID_LOCK);
+	return NULL;
+    }
+
+    /* Try SFS hack first */
+    file = SFSOpenFromLock(lock, DOSBase);
+
+    /* If SFS hack failed, try more general method that should work for both
+     * device-based and packet-based handlers */
+    if (file == NULL)
+    {
+        /* Get lock's mode */
+        test_lock = DupLock(lock);
+        if (test_lock != (BPTR)NULL)
+        {
+            mode = SHARED_LOCK;
+            UnLock(test_lock);
+        }
+        else
+            mode = EXCLUSIVE_LOCK;
+
+        /* If necessary, switch to a SHARED_LOCK temporarily so we can open
+           the file */
+        if (mode == EXCLUSIVE_LOCK)
+            ChangeMode(CHANGE_LOCK, lock, SHARED_LOCK);
+
+        /* Open file and dispose of no longer needed lock */
+        old_dir = CurrentDir(lock);
+        file = Open("", MODE_OLDFILE);
+        CurrentDir(old_dir);
+        if (file != (BPTR)NULL)
+            UnLock(lock);
+
+        /* Restore old mode */
+        if (mode == EXCLUSIVE_LOCK)
+        {
+            if (file != (BPTR)NULL)
+                ChangeMode(CHANGE_FH, file, mode);
+            else
+                ChangeMode(CHANGE_LOCK, lock, mode);
+        }
+    }
+
+    return file;
+
+    AROS_LIBFUNC_EXIT
+} /* OpenFromLock */
+
+static BPTR SFSOpenFromLock(BPTR lock, struct DosLibrary *DOSBase)
+{
     struct MsgPort *mp;
     struct IOFileSys iofs;
     struct DosPacket pkt = {0};
-    struct FileHandle *file;
     struct FileHandle *fh;
+    BPTR file = NULL;
 
     struct ASFSHandle
     {
@@ -80,12 +138,6 @@
     };
 
     struct ASFSHandle *asfshandle;
-
-    if (!lock)
-    {
-	SetIoErr(ERROR_INVALID_LOCK);
-	return NULL;
-    }
 
     fh = (struct FileHandle *)BADDR(lock);
 
@@ -105,18 +157,18 @@
 	iofs.IOFS.io_Device  = fh->fh_Device;
 	iofs.IOFS.io_Unit    = fh->fh_Unit;
 
-	file = (struct FileHandle *)AllocDosObject(DOS_FILEHANDLE, NULL);
-	if (!file)
+	fh = (struct FileHandle *)AllocDosObject(DOS_FILEHANDLE, NULL);
+	if (!fh)
 	{
 	    DeleteMsgPort(mp);
 	    SetIoErr(ERROR_NO_FREE_STORE);
 	    return NULL;
 	}
 
-	file->fh_Pos = file->fh_End = (UBYTE *)-1;
+	fh->fh_Pos = fh->fh_End = (UBYTE *)-1;
 
 	pkt.dp_Type = ACTION_FH_FROM_LOCK;
-	pkt.dp_Arg1 = (SIPTR)MKBADDR(file);
+	pkt.dp_Arg1 = (SIPTR)MKBADDR(fh);
 	pkt.dp_Arg2 = (SIPTR)asfshandle->handle;
 
 	iofs.io_PacketEmulation = &pkt;
@@ -129,27 +181,27 @@
 	{
 	case ERROR_ACTION_NOT_KNOWN:
 	case ERROR_NOT_IMPLEMENTED:
-	    FreeDosObject(DOS_FILEHANDLE, file);
+	    FreeDosObject(DOS_FILEHANDLE, fh);
+            file = NULL;
             break;
 	default:
 	    if (pkt.dp_Res1)
 	    {
-		file->fh_Device = iofs.IOFS.io_Device;
-		file->fh_Unit   = iofs.IOFS.io_Unit;
-		lock = MKBADDR(file);
+		fh->fh_Device = iofs.IOFS.io_Device;
+		fh->fh_Unit   = iofs.IOFS.io_Unit;
+		file = MKBADDR(fh);
 	    }
 	    else
 	    {
 		SetIoErr(iofs.io_DosError);
-		FreeDosObject(DOS_FILEHANDLE, file);
-		lock = NULL;
+		FreeDosObject(DOS_FILEHANDLE, fh);
+		file = NULL;
 	    }
 	}
 
 	DeleteMsgPort(mp);
     }
 
-    return lock;
+    return file;
+}
 
-    AROS_LIBFUNC_EXIT
-} /* OpenFromLock */
