ae92661b40709e4e7ae511bab576a6c1c2a61ea7
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-09-01 19:31:47 +0000

    Added missing path conversion and some error handling.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29377 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/utimes.c
+++ compiler/clib/utimes.c
@@ -7,10 +7,24 @@
 #include <proto/dos.h>
 
 #include <sys/time.h>
+#include <errno.h>
+
+#include "__errno.h"
+#include "__upath.h"
 
 int utimes(const char *file, struct timeval tvp[2])
 {
     struct DateStamp ds;
+
+    if (!file) /*safety check */
+    {
+    	errno = EFAULT;
+	return -1;
+    }
+
+    file = __path_u2a(file);
+    if (!file)
+        return -1;
 
     if(tvp != NULL)
     {   
@@ -22,9 +36,11 @@ int utimes(const char *file, struct timeval tvp[2])
     }
     else
 	DateStamp(&ds);
-    
+
     if (SetFileDate(file, &ds))
 	return 0;
+    else
+	errno = IoErr2errno(IoErr());
 
     return -1;
 }
