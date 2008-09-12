cbad43b650b8461bdf9bc27338f86c0479423fa3
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-09-12 08:58:07 +0000

    - Disable more debug output - Use generic bootup code
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29474 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-mingw32/dos/aros.c
+++ arch/all-mingw32/dos/aros.c
@@ -1,14 +1,18 @@
 /*
-    Copyright ï¿½ 1995-2001, The AROS Development Team. All rights reserved.
-    $Id$
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id: aros.c 28764 2008-05-20 19:40:27Z NicJA $
 
     Desc: This is the "boot code" of AROS when it runs as an emulation.
     Lang: english
 */
+
+#include <aros/debug.h>
+
 #include <dos/dostags.h>
 #include <dos/dos.h>
 #include <dos/dosextens.h>
 #include <dos/filesystem.h>
+#include <libraries/expansionbase.h>
 #include <proto/dos.h>
 #include <proto/intuition.h>
 #include <proto/exec.h>
@@ -20,30 +24,49 @@ int submain(struct ExecBase * SysBase, struct DosLibrary * DOSBase)
 {
     LONG            rc = RETURN_FAIL;
 
-    BPTR sseq = Open("S:Startup-Sequence", FMF_READ);
-    BPTR cis  = Open("CON:103/20///Boot Shell/AUTO", FMF_READ);
+    BPTR cis  = Open("CON:20/20///Boot Shell/AUTO", FMF_READ);
 
     if (cis)
     {
-	struct TagItem tags[] =
+        struct ExpansionBase *ExpansionBase;
+        BPTR sseq = NULL;
+        BOOL opensseq = TRUE;
+
+        struct TagItem tags[] =
+            {
+                { SYS_Asynch,      TRUE       }, /* 0 */
+                { SYS_Background,  FALSE      }, /* 1 */
+                { SYS_Input,       (IPTR)cis  }, /* 2 */
+                { SYS_Output,      (IPTR)NULL }, /* 3 */
+                { SYS_Error,       (IPTR)NULL }, /* 4 */
+                { SYS_ScriptInput, (IPTR)NULL }, /* 5 */
+                { TAG_DONE,       0           }
+            };
+
+        if ((ExpansionBase = (struct ExpansionBase *)OpenLibrary("expansion.library", 0)) != NULL)
         {
-            { SYS_Asynch,      TRUE       },
-	    { SYS_Background,  FALSE      },
-	    { SYS_Input,       (IPTR)cis  },
-	    { SYS_Output,      (IPTR)NULL },
-	    { SYS_Error,       (IPTR)NULL },
-	    { SYS_ScriptInput, (IPTR)sseq },
-	    { TAG_DONE,       0           }
-        };
+            opensseq = !(ExpansionBase->Flags & EBF_DOSFLAG);
+            CloseLibrary(ExpansionBase);
+        }
+
+        D(bug("[SubMain] Open Startup Sequence = %d\n", opensseq));
+
+        if (opensseq)
+        {
+            sseq = Open("S:Startup-Sequence", FMF_READ);
+            tags[5].ti_Data = (IPTR)sseq;
+        }
 
         rc = SystemTagList("", tags);
-	if (rc != -1)
-	{
-	    cis  = NULL;
-	    sseq = NULL;
-	}
-	else
-	    rc = RETURN_FAIL;
+        if (rc != -1)
+        {
+            cis  = NULL;
+            sseq = NULL;
+        }
+        else
+            rc = RETURN_FAIL;
+        if (sseq != NULL)
+            Close(sseq);
     }
     else
     {
@@ -51,7 +74,6 @@ int submain(struct ExecBase * SysBase, struct DosLibrary * DOSBase)
     }
 
     Close(cis);
-    Close(sseq);
 
     return rc;
 }
--- arch/all-mingw32/kernel/host_intr.c
+++ arch/all-mingw32/kernel/host_intr.c
@@ -1,4 +1,4 @@
-#define DEBUG 1
+#define DEBUG 0
 
 #include <aros/system.h>
 #include <windows.h>
