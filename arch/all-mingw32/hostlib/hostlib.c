f2158e2c55eb40e479ef7fd8c40cb4828ca695ed
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-09-17 09:46:23 +0000

    - Disabled more debug output - Implemented hostlib.resource
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29487 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/all-mingw32/hostlib/hostlib.c
@@ -0,0 +1,123 @@
+#define DEBUG 1
+
+#include <aros/debug.h>
+#include <aros/kernel.h>
+#include <aros/libcall.h>
+#include <aros/symbolsets.h>
+#include <proto/exec.h>
+#include <proto/kernel.h>
+#include <proto/utility.h>
+    
+#include "hostlib_intern.h"
+
+#include LC_LIBDEFS_FILE
+
+AROS_LH2(void *, HostLib_Open,
+         AROS_LHA(const char *, filename, A0),
+         AROS_LHA(char **,      error,    A1),
+         struct HostLibBase *, HostLibBase, 1, HostLib)
+{
+    AROS_LIBFUNC_INIT
+
+    return HostLibBase->HostIFace->HostLib_Open(filename, error);
+
+    AROS_LIBFUNC_EXIT
+}
+
+AROS_LH2(BOOL, HostLib_Close,
+         AROS_LHA(void *,  handle, A0),
+         AROS_LHA(char **, error,  A1),
+         struct HostLibBase *, HostLibBase, 2, HostLib)
+{
+    AROS_LIBFUNC_INIT
+
+    return HostLibBase->HostIFace->HostLib_Close(handle, error);
+
+    AROS_LIBFUNC_EXIT
+}
+
+AROS_LH3(void *, HostLib_GetPointer,
+         AROS_LHA(void *,       handle, A0),
+         AROS_LHA(const char *, symbol, A1),
+         AROS_LHA(char **,      error,  A2),
+         struct HostLibBase *, HostLibBase, 3, HostLib)
+{
+    AROS_LIBFUNC_INIT
+
+    return HostLibBase->HostIFace->HostLib_GetPointer(handle, symbol, error);
+
+    AROS_LIBFUNC_EXIT
+}
+
+AROS_LH1(void, HostLib_FreeErrorStr,
+    	 AROS_LHA(char *, error, A0),
+    	 struct HostLibBase *, HostLibBase, 4, HostLib)
+{
+    AROS_LIBFUNC_INIT
+
+    HostLibBase->HostIFace->HostLib_FreeErrorStr(error);
+
+    AROS_LIBFUNC_EXIT
+}
+
+AROS_LH3(APTR *, HostLib_GetInterface,
+    	 AROS_LHA(void *,  handle,         A0),
+    	 AROS_LHA(const char **, symtable, A1),
+    	 AROS_LHA(ULONG *, unresolved,     A2),
+    	 struct HostLibBase *, HostLibBase, 5, HostLib)
+{
+    AROS_LIBFUNC_INIT
+
+    const char **c;
+    ULONG cnt = 0;
+    APTR *iface = NULL;
+
+    for (c = symtable; *c; c++)
+        cnt += sizeof(APTR);
+    if (cnt) {
+    	iface = AllocVec(cnt, MEMF_CLEAR);
+    	if (iface) {
+    	    cnt = HostLibBase->HostIFace->HostLib_GetInterface(handle, symtable, iface);
+    	    if (unresolved)
+    	        *unresolved = cnt;
+    	}
+    }
+    return iface;
+    
+    AROS_LIBFUNC_EXIT
+}
+
+AROS_LH1I(void, HostLib_DropInterface,
+   	 AROS_LHA(APTR *, interface, A0),
+   	 struct HostLibBase *, HostLibBase, 6, HostLib)
+{
+    AROS_LIBFUNC_INIT
+
+    FreeVec(interface);
+
+    AROS_LIBFUNC_EXIT
+}
+
+/* auto init */
+static int HostLib_Init(LIBBASETYPEPTR LIBBASE)
+{
+    struct UtilityBase *UlitityBase;
+    APTR KernelBase;
+    struct TagItem *BootInfo;
+    
+    UtilityBase = (struct UtilityBase *)OpenLibrary("utility.library", 36);
+    D(bug("[hostlib] UtilityBase = 0x%08lX\n", UtilityBase));
+    if (UtilityBase) {
+        KernelBase = OpenResource("kernel.resource");
+        D(bug("[hostlib] KernelBase = 0x%08lX\n", KernelBase));
+        if (KernelBase) {
+    	    BootInfo = KrnGetBootInfo();
+    	    HostLibBase->HostIFace = (struct HostInterface *)GetTagData(KRN_HostInterface, 0, BootInfo);
+    	    D(bug("[hostlib] HostIFace = 0x%08lX\n", HostLibBase->HostIFace));
+    	}
+    	CloseLibrary((struct Library *)UtilityBase);
+    }
+    return (int)HostLibBase->HostIFace;
+}
+
+ADD2INITLIB(HostLib_Init, 0)
