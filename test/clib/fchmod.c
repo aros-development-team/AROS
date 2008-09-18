2fd346e07ff1acd105d995d898cc097b4fa5aa0a
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-09-18 21:41:36 +0000

    Implemented fchmod() function along with a test utility.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29493 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/clib/fchmod.c
@@ -0,0 +1,37 @@
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include "test.h"
+
+char testfilename[] = "__TEST__";
+int fd;
+
+int main() 
+{
+    struct stat buf;
+    fd = creat(testfilename, 0700);
+    TEST((fd != -1));
+    TEST((fchmod(fd, 0444) != -1));
+    TEST((fstat(fd, &buf) != -1));
+    TEST(((buf.st_mode & 0777) == 0444));
+    TEST((fchmod(fd, 0555) != -1));
+    TEST((fstat(fd, &buf) != -1));
+    TEST(((buf.st_mode & 0777) == 0555));
+    TEST((fchmod(fd, 0666) != -1));
+    TEST((fstat(fd, &buf) != -1));
+    TEST(((buf.st_mode & 0777) == 0666));
+    TEST((fchmod(fd, 0777) != -1));
+    TEST((fstat(fd, &buf) != -1));
+    TEST(((buf.st_mode & 0777) == 0777));
+    close(fd);
+    cleanup();
+    return OK;
+}
+
+void cleanup() 
+{
+    close(fd);
+    remove(testfilename);
+}
