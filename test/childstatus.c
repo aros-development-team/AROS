9dc46b93641d893e31ed2cff226b35a3e9f964b5
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-09-25 17:10:17 +0000

    Added test utilities for ChildStatus() and ChildWait() functions.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29532 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/childstatus.c
@@ -0,0 +1,52 @@
+#include <proto/exec.h>
+#include <dos/dosextens.h>
+#include <proto/dos.h>
+#include <assert.h>
+
+LONG entry()
+{
+    Delay(50);
+    return 0;
+}
+
+int main()
+{
+    struct Process *child;
+    
+    struct TagItem tags[] =
+    {
+	{ NP_Entry,         (IPTR) entry              },
+    	{ NP_Cli,           (IPTR) TRUE               },
+        { NP_Name,          (IPTR) "test"             },
+        { NP_NotifyOnDeath, (IPTR) TRUE               },
+        { TAG_DONE,         0                         }
+    };
+    
+    child = CreateNewProc(tags);
+    
+    if(child)
+    {
+	ULONG childstatus;
+	ULONG childid = GetETask((struct Task*) child)->et_UniqueID;
+	Printf("Checking status value for non-existing child id\n");
+	childstatus = ChildStatus(-1);
+	assert(childstatus == CHILD_NOTFOUND);
+	Printf("Result: CHILD_NOTFOUND\n");
+	Printf("Checking status value for running child id\n");
+	childstatus = ChildStatus(childid);
+	assert(childstatus == CHILD_ACTIVE);
+	Printf("Result: CHILD_ACTIVE\n");
+	ChildWait(childid);
+	Printf("Checking status value for died child id\n");
+	childstatus = ChildStatus(childid);
+	assert(childstatus == CHILD_EXITED);
+	Printf("Result: CHILD_EXITED\n");
+	ChildFree(childid);
+	Printf("Checking status value for freed child id\n");
+	childstatus = ChildStatus(childid);
+	assert(childstatus == CHILD_NOTFOUND);
+	Printf("Result: CHILD_NOTFOUND\n");
+    }
+    else
+	PrintFault(IoErr(), "Couldn't create child process");
+}
---
+++ test/childwait.c
@@ -0,0 +1,36 @@
+#include <proto/exec.h>
+#include <dos/dosextens.h>
+#include <proto/dos.h>
+
+LONG entry()
+{
+    Delay(50);
+    return 0;
+}
+
+int main()
+{
+    struct Process *child;
+    
+    struct TagItem tags[] =
+    {
+	{ NP_Entry,         (IPTR) entry              },
+    	{ NP_Cli,           (IPTR) TRUE               },
+        { NP_Name,          (IPTR) "test"             },
+        { NP_NotifyOnDeath, (IPTR) TRUE               },
+        { TAG_DONE,         0                         }
+    };
+    
+    child = CreateNewProc(tags);
+    
+    if(child)
+    {
+	ULONG childid = GetETask((struct Task*) child)->et_UniqueID;
+	Printf("Waiting for child with id %d\n", childid);
+	ChildWait(childid);
+	Printf("Child exited, freeing child\n");
+	ChildFree(childid);
+    }
+    else
+	PrintFault(IoErr(), "Couldn't create child process");
+}
