4a5d08778e015feb493db6ba9dafbdfdf49fd607
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-09-26 00:32:47 +0000

    Replaced ugly sleep() calls with waitpid()s.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29538 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/clib/vfork.c
+++ test/clib/vfork.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <aros/debug.h>
+#include <sys/wait.h>
 #include "test.h"
 
 int main()
@@ -15,7 +16,7 @@ int main()
     if((int) pid > 0)
     {
 	printf("I'm parent, I have a child with pid %d\n", (int) pid);
-	sleep(1);
+	waitpid(pid, NULL, 0);
     }
     else if(pid == 0)
     {
@@ -31,19 +32,18 @@ int main()
     if((int) pid > 0)
     {
 	printf("I'm parent, I have a first child with pid %d\n", (int) pid);
-	sleep(1);
 	pid_t pid2 = vfork();
 	if((int) pid2 > 0)
 	{
 	    printf("I'm parent, I have a second child with pid %d\n", (int) pid2);
-	    sleep(1);
+	    waitpid(pid2, NULL, 0);
 	}
 	else if(pid2 == 0)
 	{
 	    execve("C:Echo", argv1, envp);
 	    _exit(1);
 	}
-	_exit(0);
+	waitpid(pid, NULL, 0);
     }
     else if(pid == 0)
     {
@@ -52,7 +52,7 @@ int main()
 	if((int) pid2 > 0)
 	{
 	    printf("I'm child, I have my child with pid %d\n", (int) pid2);
-	    sleep(1);
+	    waitpid(pid2, NULL, 0);
 	}
 	else if(pid2 == 0)
 	{
