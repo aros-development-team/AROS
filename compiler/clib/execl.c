e5f925d7652705f61e3ec6e7097640ab4bd34273
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-09-29 16:32:07 +0000

    Implemented execl() function.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29568 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/execl.c
+++ compiler/clib/execl.c
@@ -6,12 +6,13 @@
 */
 
 #include <aros/debug.h>
-
-#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
 
 /*****************************************************************************
 
     NAME */
+#include <unistd.h>
 
 	int execl(
 
@@ -37,9 +38,36 @@
 
 ******************************************************************************/
 {
-#   warning Implement execl()
-    AROS_FUNCTION_NOT_IMPLEMENTED("arosc");
+    va_list args;
+    char default_argv[] = { NULL };
+    char **argv;
 
-    return -1;
+    if(arg != NULL)
+    {
+        int argc = 1;
+        va_start(args,arg);
+        while(va_arg(args,const char *) != NULL)
+            argc++;
+        va_end(args);
+        
+        argv = (char**) malloc(sizeof(char*) * (argc + 1));
+        if(!argv)
+        {
+            errno = ENOMEM;
+            return -1;
+        }
+        
+        argv[0] = arg;
+        int argi;
+        va_start(args,arg);
+        for(argi = 1; argi < argc; argi++)
+            argv[argi] = va_arg(args,const char *);
+        va_end(args);
+        argv[argc] = NULL;
+    }
+    else
+	argv = default_argv;
+    
+    return execve(path, argv, environ);
 } /* execl() */
 
