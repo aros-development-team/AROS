7ac3605bb291005f9205bfb4bb0805b3b97c093f
weissms <weissms@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-10-05 22:36:27 +0000

    Add exitthread source.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29681 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/thread/exitthread.c
@@ -0,0 +1,60 @@
+/*
+ * thread.library - threading and synchronisation primitives
+ *
+ * Copyright © 2008 Markus Weiss
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the same terms as AROS itself.
+ */
+
+#include "thread_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+        AROS_LH1(void, ExitThread,
+
+/*  SYNOPSIS */
+        AROS_LHA(void *, result, A0),
+
+/*  LOCATION */
+        struct ThreadBase *, ThreadBase, 20, Thread)
+
+/*  FUNCTION
+        Exits the calling thread.
+
+    INPUTS
+        result    - pointer to storage for the thread's return value. You can
+                    pass NULL here if you don't care about the return value.
+
+    RESULT
+        This function never returns.
+
+    NOTES
+
+    EXAMPLE
+	void *result = (void*) 5;
+	ExitThread(result);
+
+    BUGS
+
+    SEE ALSO
+        WaitThread(), WaitAllThreads()
+
+    INTERNALS
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    struct _Thread *thread = _getthreadbytask(FindTask(NULL), ThreadBase);
+    if (thread == NULL)
+	/* if called from a task that is not a thread */
+        return;
+
+    thread->result = result;
+
+    longjmp(thread->jmp, 1);
+
+    AROS_LIBFUNC_EXIT
+} /* ExitThread */
