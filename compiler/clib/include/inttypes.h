be02f4e360dadec15050bad3faa5ef58391d26a4
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-10-16 11:04:40 +0000

    Implemented POSIX strtoimax() and strtoumax() functions.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29770 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/include/inttypes.h
+++ compiler/clib/include/inttypes.h
@@ -45,10 +45,10 @@
 
 __BEGIN_DECLS
 
-/* NOTIMPL intmax_t strtoimax(const char * restrict nptr,
-		    char ** restrict endptr, int base); */
-/* NOTIMPL uintmax_t strtoumax(const char * restrict nptr,
-		    char ** restrict endptr, int base); */
+intmax_t strtoimax(const char * restrict nptr,
+		    char ** restrict endptr, int base);
+uintmax_t strtoumax(const char * restrict nptr,
+		    char ** restrict endptr, int base);
 
 /* NOTIMPL intmax_t wcstoimax(const wchar_t * restrict nptr,
 		    wchar_t ** restrict endptr, int base); */
