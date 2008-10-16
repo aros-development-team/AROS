576b05f8fde2d0fe1e52ad9bbe42eaef13534045
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-10-16 21:59:13 +0000

    Added test utility for fcntl() - currently only F_GETFD and F_SETFD are tested.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29783 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/clib/fcntl.c
@@ -0,0 +1,43 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "test.h"
+
+int fd = -1;
+int fd2 = -1;
+
+int main()
+{
+    fd = mkstemp("T:lseekXXXXXX");
+    TEST((fd != -1));
+
+    /* Check if F_GETFD and F_SETFD is working */
+    TEST((fcntl(fd, F_GETFD) == 0));
+    TEST((fcntl(fd, F_SETFD, FD_CLOEXEC) == 0));
+    TEST((fcntl(fd, F_GETFD) == FD_CLOEXEC));
+    TEST((fcntl(fd, F_SETFD, 0) == 0));
+    TEST((fcntl(fd, F_GETFD) == 0));
+    
+    int fd2 = dup(fd);
+    TEST((fd2 != -1));
+
+    /* Check if descriptor flags are independent for duped descriptors */
+    TEST((fcntl(fd2, F_GETFD) == 0));
+    TEST((fcntl(fd2, F_SETFD, FD_CLOEXEC) == 0));
+    TEST((fcntl(fd2, F_GETFD) == FD_CLOEXEC));
+    TEST((fcntl(fd, F_GETFD) == 0));
+    TEST((fcntl(fd, F_SETFD, FD_CLOEXEC) == 0));
+    TEST((fcntl(fd2, F_SETFD, 0) == 0));
+    TEST((fcntl(fd, F_GETFD) == FD_CLOEXEC));
+    
+    cleanup();
+    return OK;
+}
+
+void cleanup()
+{
+    if(fd != -1)
+	close(fd);
+    if(fd2 != -1)
+	close(fd2);
+}
