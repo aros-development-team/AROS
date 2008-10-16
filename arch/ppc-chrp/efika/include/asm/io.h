e00c6bb29abc9a12199a9619f7804d9f4182f248
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-10-16 06:04:05 +0000

    MPC5200B may require strong synchronization (sync instruction) in all cases.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29767 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/ppc-chrp/efika/include/asm/io.h
+++ arch/ppc-chrp/efika/include/asm/io.h
@@ -16,23 +16,24 @@ static inline isync() {
 }
 
 static inline uint8_t inb(uint8_t *port) {
-    uint8_t ret; asm volatile("lbz%U1%X1 %0,%1; eieio":"=r"(ret):"m"(*port)); return ret;
+    uint8_t ret; asm volatile("sync; lbz%U1%X1 %0,%1; eieio":"=r"(ret):"m"(*port)); return ret;
 }
 
 static inline void outb(uint8_t val, char *port) {
-    asm volatile("stb%U0%X0 %1,%0; eieio"::"m"(*port),"r"(val));
+    asm volatile("stb%U0%X0 %1,%0; eieio; sync"::"m"(*port),"r"(val));
 }
 
 static inline uint16_t inw(uint16_t *port) {
-    uint16_t ret; asm volatile("lhz%U1%X1 %0,%1; eieio":"=r"(ret):"m"(*port)); return ret;
+    uint16_t ret; asm volatile("sync; lhz%U1%X1 %0,%1; eieio":"=r"(ret):"m"(*port)); return ret;
 }
 
 static inline void outw(uint16_t val, uint16_t *port) {
-    asm volatile("sth%U0%X0 %1,%0; eieio"::"m"(*port),"r"(val));
+    asm volatile("sth%U0%X0 %1,%0; eieio; sync"::"m"(*port),"r"(val));
 }
 
 static inline uint16_t inw_be(uint16_t *port) {
     return inw(port);
+
 }
 
 static inline void outw_be(uint16_t val, uint16_t *port) {
@@ -40,20 +41,20 @@ static inline void outw_be(uint16_t val, uint16_t *port) {
 }
 
 static inline uint16_t inw_le(uint16_t *port) {
-    uint16_t ret; asm volatile("lhbrx %0,0,%1; eieio":"=r"(ret):"r"(port),"m"(*port)); return ret;
+    uint16_t ret; asm volatile("sync; lhbrx %0,0,%1; eieio":"=r"(ret):"r"(port),"m"(*port)); return ret;
 }
 
 static inline void outw_le(uint16_t val, uint16_t *port) {
-    asm volatile("sthbrx %1,0,%2; eieio":"=m"(*port):"r"(val),"r"(port));
+    asm volatile("sthbrx %1,0,%2; eieio; sync":"=m"(*port):"r"(val),"r"(port));
 }
 
 
 static inline uint32_t inl(uint32_t *port) {
-    uint32_t ret; asm volatile("lwz%U1%X1 %0,%1; eieio":"=r"(ret):"m"(*port)); return ret;
+    uint32_t ret; asm volatile("sync; lwz%U1%X1 %0,%1; eieio":"=r"(ret):"m"(*port)); return ret;
 }
 
 static inline void outl(uint32_t val, uint32_t *port) {
-    asm volatile("stw%U0%X0 %1,%0; eieio"::"m"(*port),"r"(val));
+    asm volatile("stw%U0%X0 %1,%0; eieio; sync"::"m"(*port),"r"(val));
 }
 
 static inline uint32_t inl_be(uint32_t *port) {
@@ -65,11 +66,11 @@ static inline void outl_be(uint32_t val, uint32_t *port) {
 }
 
 static inline uint32_t inl_le(uint32_t *port) {
-    uint32_t ret; asm volatile("lwbrx %0,0,%1; eieio":"=r"(ret):"r"(port),"m"(*port)); return ret;
+    uint32_t ret; asm volatile("sync; lwbrx %0,0,%1; eieio":"=r"(ret):"r"(port),"m"(*port)); return ret;
 }
 
 static inline void outl_le(uint32_t val, uint32_t *port) {
-    asm volatile("stwbrx %1,0,%2; eieio":"=m"(*port):"r"(val),"r"(port));
+    asm volatile("stwbrx %1,0,%2; eieio; sync":"=m"(*port):"r"(val),"r"(port));
 }
 
 //static inline void pci_outb(uint8_t val, uint16_t port)
