fbd41751cff6faddfa58c57bb35eb312dd4570d9
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-10-18 21:26:19 +0000

    Undefined AROS to prevent errors when compiling fd2inline on AROS.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29809 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- tools/fd2inline/fd2inline.c
+++ tools/fd2inline/fd2inline.c
@@ -58,6 +58,9 @@ const static char version_str[]="$VER: fd2inline " VERSION " (24.2.2002)";
 #define REGS 16	 /* d0=0,...,a7=15 */
 #define FDS 1000
 
+/* To prevent the enum below from getting broken when building on AROS */
+#undef AROS
+
 typedef enum
 {
    d0, d1, d2, d3, d4, d5, d6, d7, a0, a1, a2, a3, a4, a5, a6, a7, illegal
