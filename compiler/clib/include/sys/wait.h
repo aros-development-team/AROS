ed0e87fe4f9fdaf09c6ca4df15cf6ec7a26b412f
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-10-18 23:02:34 +0000

    Added some macros for getting information from status value written by wait() and waitpid(). Most of them have hard-coded default values, flags connected with them are not currently set anywhere - POSIX signals are needed first.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29815 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/include/sys/wait.h
+++ compiler/clib/include/sys/wait.h
@@ -18,6 +18,14 @@
 #define WEXITED         0x00000004
 #define WCONTINUED      0x00000008
 
+#define WIFEXITED(status)       1
+#define WEXITSTATUS(status)     (status & 0xff)
+#define WIFSIGNALED(status)     0
+#define WTERMSIG(status)        0
+#define WIFSTOPPED(status)      0
+#define WSTOPSIG(status)        0
+#define WIFCONTINUED(status)    0
+
 pid_t wait(int *status);
 pid_t waitpid(pid_t pid, int *status, int options);
 
