2ddd642d4ca98c931d010564b3a7113eacbb68d1
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-10-22 13:57:44 +0000

    Fixed problem with cold reboot, disabled debug output
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29850 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-mingw32/bootstrap/shutdown.c
+++ arch/all-mingw32/bootstrap/shutdown.c
@@ -1,7 +1,8 @@
+#include <stdlib.h>
 #include <windows.h>
 #include "shutdown.h"
 
-#define D(x) x
+#define D(x)
 
 void Host_Shutdown(unsigned long action)
 {
@@ -11,17 +12,19 @@ void Host_Shutdown(unsigned long action)
     switch (action) {
     case SD_ACTION_POWEROFF:
         D(printf("[Shutdown] POWER OFF request\n"));
-        ExitProcess(0);
+        exit(0);
     	break;
     case SD_ACTION_COLDREBOOT:
         D(printf("[Shutdown] Cold reboot, dir: %s, name: %s, command line: %s\n", bootstrapdir, bootstrapname, cmdline));
     	FillMemory(&runinfo, sizeof(runinfo), 0);
         runinfo.cb = sizeof(runinfo);
-        if (CreateProcess(bootstrapname, cmdline, NULL, NULL, FALSE, 0, NULL, bootstrapdir, &runinfo, &ProcInfo)) {
+        /* If we create new process without CREATE_NEW_CONSOLE, strange thing will happen if we start AROS
+         * from within command line processor. Looks like it's Windows bug/misdesign. Well, let's reopen the console every time. */
+        if (CreateProcess(bootstrapname, cmdline, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, bootstrapdir, &runinfo, &ProcInfo)) {
             D(printf("[Shutdown] AROS re-run\n"));   
             CloseHandle(ProcInfo.hProcess);
             CloseHandle(ProcInfo.hThread);
-            ExitProcess(0);
+            exit(0);
         }
         D(printf("[Shutdown] Unable to re-run AROS\n"));
     }
