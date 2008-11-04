046dae4e573fbcb618b6d276d32a66a7612b115a
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-11-04 19:51:04 +0000

    Added soft link handling. Open is now split into FileLock-allocating Open() and InternalOpen() doing the real job. InternalOpen is calling itself recursively in case a soft link is present in path to opened file. Soft link nesting limit controls the maximum recursion depth.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29981 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/open.c
+++ rom/dos/open.c
@@ -16,6 +16,11 @@
 #include <proto/utility.h>
 #include "dos_intern.h"
 
+LONG InternalOpen(CONST_STRPTR name, LONG accessMode, 
+    struct FileHandle *handle, LONG soft_nesting, struct DosLibrary *DOSBase);
+
+#define MAX_SOFT_LINK_NESTING 16 /* Maximum level of soft links nesting */
+
 /*****************************************************************************
 
     NAME */
@@ -59,31 +64,61 @@
     AROS_LIBFUNC_INIT
 
     struct FileHandle *ret;
-    BPTR con, ast;
     LONG error;
-    struct Process *me;
 
     /* Sanity check */
     if (name == NULL) return NULL;
-
-    /* Get pointer to process structure */
-    me = (struct Process *)FindTask(NULL);
 
     /* Create filehandle */
     ret = (struct FileHandle *)AllocDosObject(DOS_FILEHANDLE,NULL);
 
     if(ret != NULL)
     {
-        LONG doappend = 0;
-
-	/* Get pointer to I/O request. Use stackspace for now. */
-	struct IOFileSys iofs;
-
-	/* Prepare I/O request. */
-	InitIOFS(&iofs, FSA_OPEN_FILE, DOSBase);
-
-	switch(accessMode)
+	if(InternalOpen(name, accessMode, ret, MAX_SOFT_LINK_NESTING, DOSBase))
 	{
+	    return MKBADDR(ret);	    
+	}
+	else
+	{
+	    error = IoErr();
+	    FreeDosObject(DOS_FILEHANDLE,ret);
+	}
+    }
+    else
+	error = ERROR_NO_FREE_STORE;
+
+    SetIoErr(error);
+    return NULL;
+
+    AROS_LIBFUNC_EXIT
+} /* Open */
+
+/* Try to open name recursively calling itself in case it's a soft link.
+   Store result in handle. Return boolean value indicating result. */
+LONG InternalOpen(CONST_STRPTR name, LONG accessMode, 
+    struct FileHandle *handle, LONG soft_nesting, struct DosLibrary *DOSBase)
+{
+    /* Get pointer to I/O request. Use stackspace for now. */
+    struct IOFileSys iofs;
+    /* Get pointer to process structure */
+    struct Process *me = (struct Process *)FindTask(NULL);
+    LONG ret = DOSFALSE;
+    LONG error = 0;
+    LONG error2 = 0;
+    LONG doappend = 0;
+    BPTR con, ast;
+
+    if(soft_nesting == 0)
+    {
+	SetIoErr(ERROR_TOO_MANY_LEVELS);
+	return DOSFALSE;
+    }
+
+    /* Prepare I/O request. */
+    InitIOFS(&iofs, FSA_OPEN_FILE, DOSBase);
+
+    switch(accessMode)
+    {
 	case MODE_OLDFILE:
 	    iofs.io_Union.io_OPEN_FILE.io_FileMode = FMF_MODE_OLDFILE;
 	    ast = con = me->pr_CIS;
@@ -111,105 +146,153 @@
 	    iofs.io_Union.io_OPEN_FILE.io_FileMode = accessMode;
 	    ast = con = me->pr_CIS;
 	    break;
-	}
+    }
 
-	iofs.io_Union.io_OPEN_FILE.io_Protection = 0;
+    iofs.io_Union.io_OPEN_FILE.io_Protection = 0;
 
-        /* check for an empty filename. this supports this form that was
-         * required pre-2.0, which didn't have OpenFromLock():
-         *
-         *     old = CurrentDir(lock);
-         *     fh = Open("", MODE_OLDFILE);
-         *     CurrentDir(old);
-         */
-        if (*name == '\0') {
-            BPTR cur;
-            struct FileHandle *fh;
+    /* check for an empty filename. this supports this form that was
+     * required pre-2.0, which didn't have OpenFromLock():
+     *
+     *     old = CurrentDir(lock);
+     *     fh = Open("", MODE_OLDFILE);
+     *     CurrentDir(old);
+     */
+    if (*name == '\0') {
+        BPTR cur;
+        struct FileHandle *fh;
 
-            cur = me->pr_CurrentDir;
-            if (!cur)
-                cur = DOSBase->dl_SYSLock;
+        cur = me->pr_CurrentDir;
+        if (!cur)
+            cur = DOSBase->dl_SYSLock;
 
-            if (cur) {
-                fh = BADDR(cur);
+        if (cur) {
+            fh = BADDR(cur);
 
-                iofs.io_Union.io_OPEN_FILE.io_Filename = "";
+            iofs.io_Union.io_OPEN_FILE.io_Filename = (STRPTR) "";
 
-                iofs.IOFS.io_Device = fh->fh_Device;
-                iofs.IOFS.io_Unit = fh->fh_Unit;
+            iofs.IOFS.io_Device = fh->fh_Device;
+            iofs.IOFS.io_Unit = fh->fh_Unit;
 
-                DosDoIO(&iofs.IOFS);
+            DosDoIO(&iofs.IOFS);
 
-                error = me->pr_Result2 = iofs.io_DosError;
-            } else {
-                error = ERROR_OBJECT_NOT_FOUND;
-                SetIoErr(error);
-            }
+            error = me->pr_Result2 = iofs.io_DosError;
+        } else {
+            error = ERROR_OBJECT_NOT_FOUND;
+            SetIoErr(error);
         }
-        else
-	if(!Stricmp(name, "CONSOLE:"))
-	{
-	    iofs.IOFS.io_Device = ((struct FileHandle *)BADDR(con))->fh_Device;
-	    iofs.IOFS.io_Unit   = ((struct FileHandle *)BADDR(con))->fh_Unit;
-	    iofs.io_Union.io_OPEN_FILE.io_Filename = "";
-	    DosDoIO(&iofs.IOFS);
-	    error = me->pr_Result2 = iofs.io_DosError;
-	}
-	else
-	if(!Stricmp(name, "*"))
-	{
-	    iofs.IOFS.io_Device = ((struct FileHandle *)BADDR(ast))->fh_Device;
-	    iofs.IOFS.io_Unit   = ((struct FileHandle *)BADDR(ast))->fh_Unit;
-	    iofs.io_Union.io_OPEN_FILE.io_Filename = "";
-	    DosDoIO(&iofs.IOFS);
-	    error = me->pr_Result2 = iofs.io_DosError;
-	}
-	else {
-            struct DevProc *dvp = NULL;
+    }
+    else if(!Stricmp(name, (STRPTR) "CONSOLE:"))
+    {
+	iofs.IOFS.io_Device = ((struct FileHandle *)BADDR(con))->fh_Device;
+	iofs.IOFS.io_Unit   = ((struct FileHandle *)BADDR(con))->fh_Unit;
+	iofs.io_Union.io_OPEN_FILE.io_Filename = (STRPTR) "";
+	DosDoIO(&iofs.IOFS);
+	error = me->pr_Result2 = iofs.io_DosError;
+    }
+    else if(!Stricmp(name, (STRPTR) "*"))
+    {
+	iofs.IOFS.io_Device = ((struct FileHandle *)BADDR(ast))->fh_Device;
+	iofs.IOFS.io_Unit   = ((struct FileHandle *)BADDR(ast))->fh_Unit;
+	iofs.io_Union.io_OPEN_FILE.io_Filename = (STRPTR) "";
+	DosDoIO(&iofs.IOFS);
+	error = me->pr_Result2 = iofs.io_DosError;
+    }
+    else 
+    {
+        struct DevProc *dvp = NULL;
 
-            iofs.io_Union.io_OPEN_FILE.io_Filename = StripVolume(name);
+        iofs.io_Union.io_OPEN_FILE.io_Filename = StripVolume(name);
 
-            do {
-                if ((dvp = GetDeviceProc(name, dvp)) == NULL) {
-                    error = IoErr();
+        do {
+            if ((dvp = GetDeviceProc(name, dvp)) == NULL) {
+                error = IoErr();
+                break;
+            }
+
+            error = DoIOFS(&iofs, dvp, NULL, DOSBase);
+        } while(error == ERROR_OBJECT_NOT_FOUND && accessMode != MODE_NEWFILE);
+
+        if (error == ERROR_NO_MORE_ENTRIES)
+            error = me->pr_Result2 = ERROR_OBJECT_NOT_FOUND;
+
+        if(error == ERROR_IS_SOFT_LINK)
+        {
+            ULONG buffer_size = 256;
+            STRPTR softname;
+            LONG continue_loop;
+            LONG written;
+
+            do
+            {
+                continue_loop = FALSE;
+                if(!(softname = AllocVec(buffer_size, MEMF_ANY)))
+                {
+                    error2 = ERROR_NO_FREE_STORE;
                     break;
                 }
 
-                error = DoIOFS(&iofs, dvp, NULL, DOSBase);
-            } while(error == ERROR_OBJECT_NOT_FOUND && accessMode != MODE_NEWFILE);
-
-            if (error == ERROR_NO_MORE_ENTRIES)
-                error = me->pr_Result2 = ERROR_OBJECT_NOT_FOUND;
-
-            FreeDeviceProc(dvp);
+                written = ReadLink(dvp->dvp_Port, dvp->dvp_Lock, name, softname, buffer_size);
+                if(written == -1)
+                {
+                    /* An error occured */
+                    error2 = IoErr();
+                }
+                else if(written == -2)
+                {
+                    /* If there's not enough space in the buffer, increase
+                       it and try again */
+                    continue_loop = TRUE;
+                    buffer_size *= 2;
+                }
+                else if(written >= 0)
+                {
+                    /* All OK */
+                    BPTR olddir;
+                    olddir = CurrentDir(dvp->dvp_Lock);
+                    ret = InternalOpen(softname, accessMode, handle, soft_nesting - 1, DOSBase);
+                    error2 = IoErr();
+                    CurrentDir(olddir);
+                }
+                else
+                    error2 = ERROR_UNKNOWN;
+                
+                FreeVec(softname);
+            }
+            while(continue_loop);
         }
 
-	if(error == 0)
-	{
-	    ret->fh_Device = iofs.IOFS.io_Device;
-	    ret->fh_Unit   = iofs.IOFS.io_Unit;
-	    if (doappend)
-	    {
-		 /* See if the handler supports FSA_SEEK */
-		 if (Seek(MKBADDR(ret), 0, OFFSET_END) != -1)
-		 {
-		     /* if so then set the proper flag in the FileHandle struct */
-		     ret->fh_Flags |= FHF_APPEND;
-		 }
-	    }
-	    if (IsInteractive(MKBADDR(ret)))
-	        SetVBuf(MKBADDR(ret), NULL, BUF_LINE, -1);
-
-	    return MKBADDR(ret);
-	}
-
-	FreeDosObject(DOS_FILEHANDLE,ret);
+        FreeDeviceProc(dvp);
     }
-
+    
+    if(!error)
+    {
+        handle->fh_Device = iofs.IOFS.io_Device;
+        handle->fh_Unit   = iofs.IOFS.io_Unit;
+        if (doappend)
+        {
+            /* See if the handler supports FSA_SEEK */
+            if (Seek(MKBADDR(handle), 0, OFFSET_END) != -1)
+            {
+        	/* if so then set the proper flag in the FileHandle struct */
+        	handle->fh_Flags |= FHF_APPEND;
+            }
+        }
+        if (IsInteractive(MKBADDR(handle)))
+            SetVBuf(MKBADDR(handle), NULL, BUF_LINE, -1);
+        
+        return DOSTRUE;
+    }
+    else if(error == ERROR_IS_SOFT_LINK)
+    {
+	if(!ret)
+	    SetIoErr(error2);
+	else
+	    SetIoErr(0);
+	return ret;
+    }
     else
-	SetIoErr(ERROR_NO_FREE_STORE);
-
-    return 0;
-
-    AROS_LIBFUNC_EXIT
-} /* Open */
+    {
+	SetIoErr(error);
+	return DOSFALSE;
+    }
+}
