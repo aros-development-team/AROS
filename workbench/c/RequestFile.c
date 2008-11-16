96b2ee3eb69cc7a42297ca3356a2a3b5b8442dfa
weissms <weissms@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-11-16 11:44:27 +0000

    Handle special case where DRAWERSONLY option is given and AddPart would access a NULL pointer. Thx Georg.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@30058 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/RequestFile.c
+++ workbench/c/RequestFile.c
@@ -172,8 +172,9 @@ int main(void)
                     {
 			strncpy(Buffer, FileReq->fr_Drawer, MAX_PATH_LEN);
 			
+                        /* is NULL when using DRAWERSONLY */
                         Success = AddPart(Buffer,
-                                          FileReq->fr_File,
+                                          FileReq->fr_File ? FileReq->fr_File : "",
                                           MAX_PATH_LEN);
 
                         if(Success != FALSE)
