ad8ab296b55f6a7712f4ea8a64bcc33cbc93bec1
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-11-20 22:11:53 +0000

    Autodoc improvements.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@30083 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/makelink.c
+++ rom/dos/makelink.c
@@ -2,7 +2,7 @@
     Copyright © 1995-2008, The AROS Development Team. All rights reserved.
     $Id$
 
-    Desc: Create a hard- or softlink.
+    Desc: Create a hard or soft link.
     Lang: English
 */
 #include <dos/dosextens.h>
@@ -30,20 +30,19 @@
         MakeLink() will create a link between two files or directories.
         A link is a filesystem object that refers to another file.
 
-        A soft link refers to another file by name, and is resolved by
-        the filesystem and the caller. Soft links are not restricted to
-        the same volume. The |dest| argument is a NUL terminated pathname
-        to the pre-existing object. Soft links can be used on directories.
+        A soft link refers to another file or directory by name, and is
+        resolved by the filesystem and the caller. Soft links are not
+        restricted to the same volume and the target does not have to exist.
 
         A hard link refers to another file by the location on a disk, and
         is resolved by the filesystem. Hard links are restricted to files
-        on the same volume. The |dest| argument is a lock on another file.
+        or directories on the same volume.
 
     INPUTS
 	name - The name of the link to create
-	dest - If 'soft' is TRUE this must be a filename, if it is FALSE a BPTR
+	dest - If 'soft' is TRUE this must be a filename; if it is FALSE a lock
 	       pointing to the file to be hard-linked must be provided
-	soft - TRUE, if a soft-link is to be created, FALSE for an hard-link
+	soft - TRUE, if a soft link is to be created, FALSE for a hard link
 
     RESULT
 	boolean - DOSTRUE or DOSFALSE. On error, IoErr() will contain more
