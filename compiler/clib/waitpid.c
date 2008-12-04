72afe71abe9d8a1a8ddeee0efdfa26239516ba0e
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-12-04 23:01:42 +0000

    Updated documentation.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@30106 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/waitpid.c
+++ compiler/clib/waitpid.c
@@ -28,20 +28,65 @@
 	int options)
 
 /*  FUNCTION
+	Waits for child process with given process id to change state. State
+	change is one of the following events: child has exited, child was
+	terminated by a signal, child was stopped by a signal, child was
+	resumed by a signal.
+	
+	The function stores status of the process that changed state in the
+	pointer given as status argument.
+
+	The following macros can be used to extract information from the
+	status value:
+
+	WIFEXITED(status)    - true if the process has exited
+	WEXITSTATUS(status)  - exit status of the exited process
+	WIFSIGNALED(status)  - true if the child process was terminated by a
+	                       signal
+	WTERMSIG(status)     - number of the signal that caused process
+	                       termination
+	WIFSTOPPED(status)   - true if the child process was stopped by a
+	                       signal
+	WSTOPSIG(status)     - number of the signal that caused child process
+	                       stop
+	WIFCONTINUED(status) - true if the child process was resumed by the
+	                       SIGCONT signal.
+
+	Unless WNOHANG option is set, parent process will be suspended until a
+	child changes state. If a child process has already changed state,
+	function returns immediately.
 
     INPUTS
+	pid - Process id of the process you want to wait for or -1 to wait for
+		any child process
+	status - Pointer to int where child status will be stored or NULL if
+		you don't want to store status.
+	options - ORed zero or more of the following constants:
+	
+	    WNOHANG - return immediately if no child process changed state
 
     RESULT
+	Process id of the child process on success or -1 on error. If an error
+	occurred, the global variable errno is set.
 
     NOTES
+	This function will work only for child processeses notifying parent
+	process of their death, for example processes created by vfork() call.
+	If you want to use it for other processes, remember to set the
+	NP_NotifyOnDeath tag value to TRUE during child process creation.
 
     EXAMPLE
 
     BUGS
 
     SEE ALSO
+	wait()
 
     INTERNALS
+	Since POSIX signals are not yet implemented, WIFSIGNALED, WIFSTOPPED
+	and WIFCONTINUED macros always return 0. WIFEXITED always returns 1.
+
+	The et_UniqueID field of the ETask structure is used as process id.
 
 ******************************************************************************/
 {
