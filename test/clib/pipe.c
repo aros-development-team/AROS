4cb336ad86625f5f417ddbefe61f112bf5e2c098
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-01-05 18:39:05 +0000

    Added some more fancy pipe tortures.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@30227 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/clib/pipe.c
+++ test/clib/pipe.c
@@ -6,6 +6,7 @@
 #define BUFLEN 100
 
 int filedes[2] = { 0, 0 };
+int dupfiledes[2] = { 0, 0 };
 
 int main()
 {
@@ -21,6 +22,7 @@ int main()
     TEST((pipe(filedes) == 0));
     TEST((write(filedes[1], source, BUFLEN) == BUFLEN));
     TEST((read(filedes[0], destination, BUFLEN) == BUFLEN));
+    TEST((memcmp(source, destination, BUFLEN) == 0));
 
     for(i = 0; i < BUFLEN; i++)
 	source[i] = str2[i % strlen(str2)];
@@ -28,6 +30,79 @@ int main()
     TEST((write(filedes[1], source, BUFLEN) == BUFLEN));
     TEST((read(filedes[0], destination, BUFLEN) == BUFLEN));
     TEST((memcmp(source, destination, BUFLEN) == 0));
+    
+    for(i = 0; i < 10000; i++)
+    {
+	char src = i % 256;
+	char dst;
+	TEST((write(filedes[1], &src, 1) == 1));
+	TEST((read(filedes[0], &dst, 1) == 1));
+	TEST((src == dst));
+    }
+    
+    dupfiledes[0] = dup(filedes[0]);
+    TEST((dupfiledes[0] != -1));
+
+    TEST((write(filedes[1], source, BUFLEN) == BUFLEN));
+    TEST((read(dupfiledes[0], destination, BUFLEN) == BUFLEN));
+    TEST((memcmp(source, destination, BUFLEN) == 0));
+
+    for(i = 0; i < 10000; i++)
+    {
+	char src = i % 256;
+	char dst;
+	TEST((write(filedes[1], &src, 1) == 1));
+	TEST((read(dupfiledes[0], &dst, 1) == 1));
+	TEST((src == dst));
+    }
+
+    dupfiledes[1] = dup(filedes[1]);
+    TEST((dupfiledes[1] != -1));
+
+    TEST((write(dupfiledes[1], source, BUFLEN) == BUFLEN));
+    TEST((read(filedes[0], destination, BUFLEN) == BUFLEN));
+    TEST((memcmp(source, destination, BUFLEN) == 0));
+
+    for(i = 0; i < 10000; i++)
+    {
+	char src = i % 256;
+	char dst;
+	TEST((write(dupfiledes[1], &src, 1) == 1));
+	TEST((read(filedes[0], &dst, 1) == 1));
+	TEST((src == dst));
+    }
+
+    TEST((write(dupfiledes[1], source, BUFLEN) == BUFLEN));
+    TEST((read(dupfiledes[0], destination, BUFLEN) == BUFLEN));
+    TEST((memcmp(source, destination, BUFLEN) == 0));
+
+    for(i = 0; i < 10000; i++)
+    {
+	char src = i % 256;
+	char dst;
+	TEST((write(dupfiledes[1], &src, 1) == 1));
+	TEST((read(dupfiledes[0], &dst, 1) == 1));
+	TEST((src == dst));
+    }
+
+    TEST((close(dupfiledes[0]) != -1));
+    dupfiledes[0] = 0;
+    TEST((close(dupfiledes[1]) != -1));
+    dupfiledes[1] = 0;
+    
+    TEST((write(filedes[1], source, BUFLEN) == BUFLEN));
+    TEST((read(filedes[0], destination, BUFLEN) == BUFLEN));
+    TEST((memcmp(source, destination, BUFLEN) == 0));
+    
+    for(i = 0; i < 10000; i++)
+    {
+	char src = i % 256;
+	char dst;
+	TEST((write(filedes[1], &src, 1) == 1));
+	TEST((read(filedes[0], &dst, 1) == 1));
+	TEST((src == dst));
+    }
+
     cleanup();
     return OK;
 }
@@ -38,4 +113,8 @@ void cleanup()
 	close(filedes[0]);
     if(filedes[1])
 	close(filedes[1]);
+    if(dupfiledes[0])
+	close(dupfiledes[0]);
+    if(dupfiledes[1])
+	close(dupfiledes[1]);
 }
