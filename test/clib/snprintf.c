b9ded2ad3cf582b356f5787d56b3cd8a501943c8
sszymczy <sszymczy@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-01-05 21:55:10 +0000

    Added test for snprintf() function (truncation behavior).
    git-svn-id: https://svn.aros.org/svn/aros/trunk@30233 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/clib/snprintf.c
@@ -0,0 +1,41 @@
+#include "test.h"
+#include <stdio.h>
+#include <string.h>
+
+#define TESTSTRING "test"
+#define TESTSTRING2 "123456789"
+#define TESTSTRING3 "0123456789"
+#define TESTSTRING4 "a long test string"
+
+#define BUFSIZE 10
+
+int main()
+{
+    char buf[BUFSIZE+1] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+    
+    /* first check strings shorter than buffer */
+    TEST((snprintf(buf, BUFSIZE, "%s", TESTSTRING) == strlen(TESTSTRING)));
+    TEST((buf[strlen(TESTSTRING)] == 0));
+    TEST((buf[strlen(TESTSTRING) + 1] == (char) 0xff));
+    
+    /* now strings with length equal to buffer size - 1 */
+    TEST((snprintf(buf, BUFSIZE, "%s", TESTSTRING2) == strlen(TESTSTRING2)));
+    TEST((buf[strlen(TESTSTRING2)] == 0));
+    TEST((buf[BUFSIZE] == (char) 0xff));
+
+    /* now strings with length equal to buffer size (no zero byte written) */
+    TEST((snprintf(buf, BUFSIZE, "%s", TESTSTRING3) == strlen(TESTSTRING3)));
+    TEST((buf[BUFSIZE-1] == TESTSTRING3[strlen(TESTSTRING3)-1]));
+    TEST((buf[BUFSIZE] == (char) 0xff));
+    
+    /* now strings longer than buffer size */
+    TEST((snprintf(buf, BUFSIZE, "%s", TESTSTRING4) == strlen(TESTSTRING4)));
+    TEST((buf[BUFSIZE-1] == TESTSTRING4[BUFSIZE-1]));
+    TEST((buf[BUFSIZE] == (char) 0xff));
+    return OK;
+}
+
+void cleanup()
+{
+    
+}
