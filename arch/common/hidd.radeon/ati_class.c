ac4fc64e9c1477a2b0f675f03299f648ab556182
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-01-19 07:57:52 +0000

    Huge commit with very few changes:
    1. Whitespace cleanup (my editor does it by itself, sorry for that)
    2. The BIOS is mapped from 0x000c000 only, if the PCI does not expose the ROM base address
    3. Stupid mistake which blanked the screen on some radeon models connected to the flat panel.
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@30311 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/common/hidd.radeon/ati_class.c
+++ arch/common/hidd.radeon/ati_class.c
@@ -81,23 +81,23 @@ OOP_Object *METHOD(ATI, Hidd_Gfx, Show)
             if (bm->fbgfx)
             {
                 bm->usecount++;
-                
+
                 LOCK_HW
-     
+
                 LoadState(sd, bm->state);
                 DPMS(sd, sd->dpms);
-        
+
                 fb = bm->BitMap;
                 ShowHideCursor(sd, sd->Card.cursorVisible);
-                
+
                 RADEONEngineReset(sd);
                 RADEONEngineRestore(sd);
-                
+
                 UNLOCK_HW
             }
         }
     }
-    
+
     if (!fb)
         fb = (OOP_Object *)OOP_DoSuperMethod(cl, o, (OOP_Msg)msg);
 
@@ -109,15 +109,15 @@ OOP_Object *METHOD(ATI, Hidd_Gfx, NewBitMap)
     BOOL displayable, framebuffer;
     OOP_Class *classptr = NULL;
     struct TagItem mytags[2];
-    struct pHidd_Gfx_NewBitMap mymsg; 
-    
+    struct pHidd_Gfx_NewBitMap mymsg;
+
     /* Displayable bitmap ? */
     displayable = GetTagData(aHidd_BitMap_Displayable, FALSE, msg->attrList);
     framebuffer = GetTagData(aHidd_BitMap_FrameBuffer, FALSE, msg->attrList);
 
     if (framebuffer)
     {
-        /* If the user asks for a framebuffer map we must ALLWAYS supply a class */ 
+        /* If the user asks for a framebuffer map we must ALLWAYS supply a class */
         classptr = sd->OnBMClass;
     }
     else if (displayable)
@@ -127,11 +127,11 @@ OOP_Object *METHOD(ATI, Hidd_Gfx, NewBitMap)
     else
     {
         HIDDT_ModeID modeid;
-        /* 
+        /*
             For the non-displayable case we can either supply a class ourselves
             if we can optimize a certain type of non-displayable bitmaps. Or we
             can let the superclass create on for us.
-           
+
             The attributes that might come from the user deciding the bitmap
             pixel format are:
             - aHidd_BitMap_ModeID:  a modeid. create a nondisplayable
@@ -141,7 +141,7 @@ OOP_Object *METHOD(ATI, Hidd_Gfx, NewBitMap)
             - aHidd_BitMap_Friend: if this is supplied and none of the two above
                 are supplied, then the pixel format of the created bitmap
                 will be the same as the one of the friend bitmap.
-                
+
             These tags are listed in prioritized order, so if
             the user supplied a ModeID tag, then you should not care about StdPixFmt
             or Friend. If there is no ModeID, but a StdPixFmt tag supplied,
@@ -149,9 +149,9 @@ OOP_Object *METHOD(ATI, Hidd_Gfx, NewBitMap)
             create the correct pixelformat. And as said above, if only Friend
             is supplied, you can create a bitmap with same pixelformat as Frien
         */
-        
+
         modeid = (HIDDT_ModeID)GetTagData(aHidd_BitMap_ModeID, vHidd_ModeID_Invalid, msg->attrList);
-        
+
         if (vHidd_ModeID_Invalid != modeid)
         {
             /* User supplied a valid modeid. We can use our offscreen class */
@@ -159,7 +159,7 @@ OOP_Object *METHOD(ATI, Hidd_Gfx, NewBitMap)
         }
         else
         {
-            /* 
+            /*
                We may create an offscreen bitmap if the user supplied a friend
                bitmap. But we need to check that he did not supplied a StdPixFmt
             */
@@ -184,15 +184,15 @@ OOP_Object *METHOD(ATI, Hidd_Gfx, NewBitMap)
                     if (gfxhidd == o)
                     {
                         /* Friend was NVidia hidd bitmap. Now we can supply our own class */
-                        classptr = sd->OffBMClass;          
+                        classptr = sd->OffBMClass;
                     }
                 }
             }
         }
     }
-   
+
     D(bug("[ATI] classptr = %p\n", classptr));
-    
+
     /* Do we supply our own class ? */
     if (NULL != classptr)
     {
@@ -202,15 +202,15 @@ OOP_Object *METHOD(ATI, Hidd_Gfx, NewBitMap)
         mytags[0].ti_Data   = (IPTR)classptr;
         mytags[1].ti_Tag    = TAG_MORE;
         mytags[1].ti_Data   = (IPTR)msg->attrList;
-        
+
         /* Like in Gfx::New() we init a new message struct */
         mymsg.mID       = msg->mID;
         mymsg.attrList  = mytags;
-        
+
         /* Pass the new message to the superclass */
         msg = &mymsg;
     }
-    
+
     return (OOP_Object*)OOP_DoSuperMethod(cl, o, (OOP_Msg)msg);
 }
 
@@ -239,7 +239,7 @@ void METHOD(ATI, Hidd_Gfx, CopyBox)
                 bm_src->framebuffer,msg->srcX,msg->srcY,bm_src->depth,
                 bm_dst->framebuffer,msg->destX,msg->destY,bm_dst->depth,
                 msg->width, msg->height));
-    
+
         /* Case -1: (To be fixed) one of the bitmaps have chunky outside GFX mem */
         if (!bm_src->fbgfx || !bm_dst->fbgfx)
         {
@@ -263,16 +263,16 @@ void METHOD(ATI, Hidd_Gfx, CopyBox)
             WORD yb = msg->destY;
             WORD w = msg->width;
             WORD h = msg->height;
-            
+
             if (bm_src->pitch_offset == bm_dst->pitch_offset)
             {
                 xdir = xb - xa;
                 ydir = ya - yb;
-                
+
                 if (xdir < 0) xa += w - 1, xb += w - 1;
-                if (ydir < 0) ya += h - 1, yb += h - 1;    
+                if (ydir < 0) ya += h - 1, yb += h - 1;
             }
-            
+
             LOCK_MULTI_BITMAP
             LOCK_BITMAP_BM(bm_src)
             LOCK_BITMAP_BM(bm_dst)
@@ -303,7 +303,7 @@ void METHOD(ATI, Hidd_Gfx, CopyBox)
             OUTREG(RADEON_DST_HEIGHT_WIDTH, (h  << 16) | w);
 
             UNLOCK_HW
-            
+
             UNLOCK_BITMAP_BM(bm_src)
             UNLOCK_BITMAP_BM(bm_dst)
         }
@@ -311,11 +311,11 @@ void METHOD(ATI, Hidd_Gfx, CopyBox)
         {
             OOP_DoSuperMethod(cl, o, (OOP_Msg)msg);
         }
-          
+
         bm_src->usecount++;
         bm_dst->usecount++;
-        
-        
+
+
     }
 }
 
@@ -342,7 +342,7 @@ BOOL METHOD(ATI, Hidd_Gfx, SetCursorShape)
         ULONG       *curimg = (ULONG*)((IPTR)sd->Card.CursorStart + (IPTR)sd->Card.FrameBuffer);
 
         CURSOR_SWAPPING_DECL_MMIO;
-        
+
         struct pHidd_BitMap_GetPixel __gp = {
             mID:    OOP_GetMethodID((STRPTR)CLID_Hidd_BitMap, moHidd_BitMap_GetPixel)
         }, *getpixel = &__gp;
@@ -360,10 +360,10 @@ BOOL METHOD(ATI, Hidd_Gfx, SetCursorShape)
 
         if (width > 64) width = 64;
         if (height > 64) height = 64;
-            
+
         maxw = 64;
         maxh = 64;
-    
+
         LOCK_HW
 
         if (!sd->Card.IsSecondary) {
@@ -371,7 +371,7 @@ BOOL METHOD(ATI, Hidd_Gfx, SetCursorShape)
             save1 |= RADEON_CRTC_ARGB_EN;
             OUTREG(RADEON_CRTC_GEN_CNTL, save1 & (ULONG)~RADEON_CRTC_CUR_EN);
         }
-    
+
         if (sd->Card.IsSecondary) {
             save2 = INREG(RADEON_CRTC_GEN_CNTL) & ~(ULONG)(RADEON_CRTC_ARGB_EN|RADEON_CRTC_ARGB_EN);
             save2 |= RADEON_CRTC_ARGB_EN;
@@ -382,7 +382,7 @@ BOOL METHOD(ATI, Hidd_Gfx, SetCursorShape)
 
         for (x = 0; x < maxw*maxh; x++)
            curimg[x] = 0;
-    
+
         for (y = 0; y < height; y++)
         {
             for (x = 0; x < width; x++)
@@ -391,7 +391,7 @@ BOOL METHOD(ATI, Hidd_Gfx, SetCursorShape)
                 getpixel->x = x;
                 getpixel->y = y;
                 pixel = OOP_DoMethod(msg->shape, (OOP_Msg)getpixel);
-                
+
                 if (pixfmt == vHidd_StdPixFmt_LUT8)
                 {
 		    if (pixel)
@@ -409,19 +409,19 @@ BOOL METHOD(ATI, Hidd_Gfx, SetCursorShape)
             for (x=width; x < maxw; x++, curimg++)
                 *curimg = 0;
         }
-    
+
         for (y=height; y < maxh; y++)
             for (x=0; x < maxw; x++)
                 { if (*curimg!=0x50000000) *curimg = 0; curimg++; }
 
         CURSOR_SWAPPING_END();
-        
+
         if (!sd->Card.IsSecondary)
             OUTREG(RADEON_CRTC_GEN_CNTL, save1);
 
         if (sd->Card.IsSecondary)
             OUTREG(RADEON_CRTC2_GEN_CNTL, save2);
-        
+
         UNLOCK_HW
     }
 
@@ -437,7 +437,7 @@ void METHOD(ATI, Hidd_Gfx, SetCursorVisible)
 void METHOD(ATI, Hidd_Gfx, SetCursorPos)
 {
     D(bug("[ATI] Set cursor pos %d:%d\n", msg->x, msg->y));
-    
+
     if (!sd->Card.IsSecondary) {
         OUTREG(RADEON_CUR_HORZ_VERT_OFF,  RADEON_CUR_LOCK);
         OUTREG(RADEON_CUR_HORZ_VERT_POSN, (RADEON_CUR_LOCK
@@ -447,9 +447,9 @@ void METHOD(ATI, Hidd_Gfx, SetCursorPos)
 
     D(bug("[ATI] OFF=%08x, HV_OFF=%08x, HV_POSN=%08x\n",
         INREG(RADEON_CUR_OFFSET),
-        INREG(RADEON_CUR_HORZ_VERT_OFF),        
+        INREG(RADEON_CUR_HORZ_VERT_OFF),
         INREG(RADEON_CUR_HORZ_VERT_POSN)));
-        
+
     } else {
         OUTREG(RADEON_CUR2_HORZ_VERT_OFF,  RADEON_CUR2_LOCK);
         OUTREG(RADEON_CUR2_HORZ_VERT_POSN, (RADEON_CUR2_LOCK
@@ -474,12 +474,12 @@ OOP_Object *METHOD(ATI, Root, New)
         { aHidd_PixFmt_Depth,       32  }, /* 9 */
         { aHidd_PixFmt_BytesPerPixel,   4   }, /* 10 */
         { aHidd_PixFmt_BitsPerPixel,    24  }, /* 11 */
-//        { aHidd_PixFmt_StdPixFmt,   vHidd_StdPixFmt_Native }, /* 12 Native */
-        { aHidd_PixFmt_StdPixFmt,   vHidd_StdPixFmt_BGR032 }, /* 12 Native */
+        { aHidd_PixFmt_StdPixFmt,   vHidd_StdPixFmt_Native }, /* 12 Native */
+//        { aHidd_PixFmt_StdPixFmt,   vHidd_StdPixFmt_BGR032 }, /* 12 Native */
         { aHidd_PixFmt_BitMapType,  vHidd_BitMapType_Chunky }, /* 15 */
         { TAG_DONE, 0UL }
     };
-   
+
     struct TagItem pftags_16bpp[] = {
         { aHidd_PixFmt_RedShift,    16  }, /* 0 */
         { aHidd_PixFmt_GreenShift,  21  }, /* 1 */
@@ -493,8 +493,8 @@ OOP_Object *METHOD(ATI, Root, New)
         { aHidd_PixFmt_Depth,       16  }, /* 9 */
         { aHidd_PixFmt_BytesPerPixel,   2   }, /* 10 */
         { aHidd_PixFmt_BitsPerPixel,    16  }, /* 11 */
-//        { aHidd_PixFmt_StdPixFmt,   vHidd_StdPixFmt_Native }, /* 12 */
-        { aHidd_PixFmt_StdPixFmt,   vHidd_StdPixFmt_RGB16_LE }, /* 12 */
+        { aHidd_PixFmt_StdPixFmt,   vHidd_StdPixFmt_Native }, /* 12 */
+//        { aHidd_PixFmt_StdPixFmt,   vHidd_StdPixFmt_RGB16_LE }, /* 12 */
         { aHidd_PixFmt_BitMapType,  vHidd_BitMapType_Chunky }, /* 15 */
         { TAG_DONE, 0UL }
     };
@@ -512,8 +512,8 @@ OOP_Object *METHOD(ATI, Root, New)
         { aHidd_PixFmt_Depth,       15  }, /* 9 */
         { aHidd_PixFmt_BytesPerPixel,   2   }, /* 10 */
         { aHidd_PixFmt_BitsPerPixel,    15  }, /* 11 */
-//        { aHidd_PixFmt_StdPixFmt,   vHidd_StdPixFmt_Native }, /* 12 */
-        { aHidd_PixFmt_StdPixFmt,   vHidd_StdPixFmt_RGB15_LE }, /* 12 */
+        { aHidd_PixFmt_StdPixFmt,   vHidd_StdPixFmt_Native }, /* 12 */
+//        { aHidd_PixFmt_StdPixFmt,   vHidd_StdPixFmt_RGB15_LE }, /* 12 */
         { aHidd_PixFmt_BitMapType,  vHidd_BitMapType_Chunky }, /* 15 */
         { TAG_DONE, 0UL }
     };
@@ -532,7 +532,7 @@ OOP_Object *METHOD(ATI, Root, New)
         1024, 1048, 1184, 1344,
          768,  771,  777,  806,
         "ATI:1024x768");
-     
+
     MAKE_SYNC(1152x864_60, 80000,
         1152, 1216, 1328, 1456,
          864,  870,  875,  916,
@@ -541,8 +541,8 @@ OOP_Object *METHOD(ATI, Root, New)
     MAKE_SYNC(1280x1024_60, 107991,
         1280, 1328, 1440, 1688,
         1024, 1025, 1028, 1066,
-        "ATI:1280x1024");    
-    
+        "ATI:1280x1024");
+
     MAKE_SYNC(1600x1200_60, 155982,
         1600, 1632, 1792, 2048,
         1200, 1210, 1218, 1270,
@@ -559,12 +559,12 @@ OOP_Object *METHOD(ATI, Root, New)
 	1440, 1520, 1672, 1904,
 	900, 901, 904, 932,
 	"ATI:1440x900");
-	
+
     MAKE_SYNC(1680x1050_60, 147140,
 	1680, 1784, 1968, 2256,
 	1050, 1051, 1054, 1087,
 	"ATI:1680x1050");
-    
+
     MAKE_SYNC(1920x1200_60, 154000,
 	1920, 1968, 2000, 2080,
 	1200, 1203, 1209, 1235,
@@ -574,7 +574,7 @@ OOP_Object *METHOD(ATI, Root, New)
 //	1920, 2048, 2256, 2592,
 //	1200, 1201, 1204, 1242,
 //	"ATI:1920x1200");
-    
+
     struct TagItem modetags[] = {
         { aHidd_Gfx_PixFmtTags, (IPTR)pftags_24bpp  },
         { aHidd_Gfx_PixFmtTags, (IPTR)pftags_16bpp  },
@@ -592,7 +592,7 @@ OOP_Object *METHOD(ATI, Root, New)
 
         { TAG_DONE, 0UL }
     };
-    
+
     struct TagItem mytags[] = {
         { aHidd_Gfx_ModeTags,   (IPTR)modetags  },
         { TAG_MORE, (IPTR)msg->attrList }
@@ -620,7 +620,7 @@ void METHOD(ATI, Root, Get)
 {
     ULONG idx;
     BOOL found = FALSE;
-    
+
     if (IS_GFX_ATTR(msg->attrID, idx))
     {
         switch (idx)
@@ -629,7 +629,7 @@ void METHOD(ATI, Root, Get)
                 *msg->storage = (IPTR)TRUE;
                 found = TRUE;
                 break;
-    
+
             case aoHidd_Gfx_DPMSLevel:
                 *msg->storage = sd->dpms;
                 found = TRUE;
@@ -657,10 +657,10 @@ void METHOD(ATI, Root, Set)
             {
                 case aoHidd_Gfx_DPMSLevel:
                     LOCK_HW
-                    
+
                     DPMS(sd, tag->ti_Data);
                     sd->dpms = tag->ti_Data;
-                    
+
                     UNLOCK_HW
                     break;
             }
