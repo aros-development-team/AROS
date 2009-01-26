65669dfffeb15f35ddcd5bd43bd3e747dc7d6263
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-01-26 22:13:06 +0000

    The IntETask has been extended by two UQUAD fields. One of them is the LaunchTime of the task, expressed in CPU cycles or any measurable unit (should be the same as the frequency returned by timer.device/ReadEClock). The second field contains the total time CPU has spent in this very task.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@30369 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/etask.h
+++ rom/exec/etask.h
@@ -2,7 +2,7 @@
 #define _ETASK_H
 
 /*
-    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    Copyright ï¿½ 1995-2001, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: Internal description of the ETask structure
@@ -22,6 +22,8 @@ struct IntETask
     APTR	 iet_Context;	/* Structure to store CPU registers */
     APTR         iet_acpd;      /* Structure to store shared clib's data */
     APTR	 iet_startup;   /* Structure to store startup code stuff */
+    UQUAD	 iet_CpuTime;
+    UQUAD	 iet_private1;
 };
 
 #define GetIntETask(task)   ((struct IntETask *)(((struct Task *) \
