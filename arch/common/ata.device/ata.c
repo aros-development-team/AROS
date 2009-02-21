ab64ec41dac09c5300d8382487bb94db85820023
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-02-21 10:14:43 +0000

    include LC_LIBDEFS_FILE
    git-svn-id: https://svn.aros.org/svn/aros/trunk@30558 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/common/ata.device/ata.c
+++ arch/common/ata.device/ata.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 2004-2009, The AROS Development Team. All rights reserved
+    Copyright ï¿½ 2004-2009, The AROS Development Team. All rights reserved
     $Id$
 
     Desc:
@@ -12,18 +12,18 @@
  * 2008-01-25  T. Wiszkowski       Rebuilt, rearranged and partially fixed 60% of the code here
  *                                 Enabled implementation to scan for other PCI IDE controllers
  *                                 Implemented ATAPI Packet Support for both read and write
- *                                 Corrected ATAPI DMA handling                            
+ *                                 Corrected ATAPI DMA handling
  *                                 Fixed major IDE enumeration bugs severely handicapping transfers with more than one controller
  *                                 Compacted source and implemented major ATA support procedure
  *                                 Improved DMA and Interrupt management
  *                                 Removed obsolete code
  * 2008-03-30  T. Wiszkowski       Added workaround for interrupt collision handling; fixed SATA in LEGACY mode.
  *                                 nForce and Intel SATA chipsets should now be operational.
- * 2008-04-03  T. Wiszkowski       Fixed IRQ flood issue, eliminated and reduced obsolete / redundant code                                 
- * 2008-04-05  T. Wiszkowski       Improved IRQ management 
+ * 2008-04-03  T. Wiszkowski       Fixed IRQ flood issue, eliminated and reduced obsolete / redundant code
+ * 2008-04-05  T. Wiszkowski       Improved IRQ management
  * 2008-04-07  T. Wiszkowski       Changed bus timeout mechanism
  * 2008-04-20  T. Wiszkowski       Corrected the flaw in drive identification routines leading to ocassional system hangups
- * 2008-05-11  T. Wiszkowski       Remade the ata trannsfers altogether, corrected the pio/irq handling 
+ * 2008-05-11  T. Wiszkowski       Remade the ata trannsfers altogether, corrected the pio/irq handling
  *                                 medium removal, device detection, bus management and much more
  */
 
@@ -43,6 +43,7 @@
 #include <proto/oop.h>
 
 #include "ata.h"
+#include LC_LIBDEFS_FILE
 
 //---------------------------IO Commands---------------------------------------
 
@@ -95,19 +96,19 @@ static void cmd_Read32(struct IORequest *io, LIBBASETYPEPTR LIBBASE)
         /* Call the Unit's access funtion */
         io->io_Error = unit->au_Read32(unit, block, count,
             IOStdReq(io)->io_Data, &cnt);
-        
+
         IOStdReq(io)->io_Actual = cnt;
     }
 }
 
 /*
-    NSCMD_TD_READ64, TD_READ64 implementation. Basically the same, just packs 
+    NSCMD_TD_READ64, TD_READ64 implementation. Basically the same, just packs
     the 64 bit offset in both io_Offset (31:0) and io_Actual (63:32)
 */
 static void cmd_Read64(struct IORequest *io, LIBBASETYPEPTR LIBBASE)
 {
     struct ata_Unit *unit = (struct ata_Unit *)IOStdReq(io)->io_Unit;
-    
+
     UQUAD block = (UQUAD)(IOStdReq(io)->io_Offset & 0xffffffff) |
         ((UQUAD)(IOStdReq(io)->io_Actual)) << 32;
     ULONG count = IOStdReq(io)->io_Length;
@@ -153,7 +154,7 @@ static void cmd_Read64(struct IORequest *io, LIBBASETYPEPTR LIBBASE)
 
             io->io_Error = unit->au_Read64(unit, block, count, IOStdReq(io)->io_Data, &cnt);
         }
-                
+
         IOStdReq(io)->io_Actual = cnt;
     }
 }
@@ -196,13 +197,13 @@ static void cmd_Write32(struct IORequest *io, LIBBASETYPEPTR LIBBASE)
         /* Call the Unit's access funtion */
         io->io_Error = unit->au_Write32(unit, block, count,
             IOStdReq(io)->io_Data, &cnt);
-        
+
         IOStdReq(io)->io_Actual = cnt;
     }
 }
 
 /*
-    NSCMD_TD_WRITE64, TD_WRITE64 implementation. Basically the same, just packs 
+    NSCMD_TD_WRITE64, TD_WRITE64 implementation. Basically the same, just packs
     the 64 bit offset in both io_Offset (31:0) and io_Actual (63:32)
 */
 static void cmd_Write64(struct IORequest *io, LIBBASETYPEPTR LIBBASE)
@@ -403,13 +404,13 @@ static void cmd_RemChangeInt(struct IORequest *io, LIBBASETYPEPTR LIBBASE)
     Forbid();
     Remove((struct Node *)io);
     Permit();
-} 
+}
 
 static void cmd_Eject(struct IORequest *io, LIBBASETYPEPTR LIBBASE)
 {
     struct ata_Unit *unit = (struct ata_Unit *)io->io_Unit;
 
-    D(bug("[ATA%02ld] cmd_Eject()\n", ((struct ata_Unit*)io->io_Unit)->au_UnitNum));    
+    D(bug("[ATA%02ld] cmd_Eject()\n", ((struct ata_Unit*)io->io_Unit)->au_UnitNum));
 
     IOStdReq(io)->io_Error = unit->au_Eject(unit);
 }
@@ -426,14 +427,14 @@ static void cmd_GetGeometry(struct IORequest *io, LIBBASETYPEPTR LIBBASE)
 
         dg->dg_SectorSize       = 1 << unit->au_SectorShift;
 
-        if (unit->au_Capacity48 != 0) 
+        if (unit->au_Capacity48 != 0)
         {
             if ((unit->au_Capacity48 >> 32) != 0)
                 dg->dg_TotalSectors     = 0xffffffff;
             else
                 dg->dg_TotalSectors     = unit->au_Capacity48;
         }
-        else    
+        else
             dg->dg_TotalSectors         = unit->au_Capacity;
 
         dg->dg_Cylinders                = unit->au_Cylinders;
@@ -465,7 +466,7 @@ static void cmd_DirectScsi(struct IORequest *io, LIBBASETYPEPTR LIBBASE)
     {
         io->io_Error = unit->au_DirectSCSI(unit, (struct SCSICmd *)IOStdReq(io)->io_Data);
     }
-    else io->io_Error = IOERR_BADADDRESS; 
+    else io->io_Error = IOERR_BADADDRESS;
 }
 
 //-----------------------------------------------------------------------------
@@ -560,7 +561,7 @@ static const UWORD NSDSupported[] = {
 static void HandleIO(struct IORequest *io, LIBBASETYPEPTR LIBBASE)
 {
     io->io_Error = 0;
-    
+
     /* Handle few commands directly here */
     switch (io->io_Command)
     {
@@ -579,16 +580,16 @@ static void HandleIO(struct IORequest *io, LIBBASETYPEPTR LIBBASE)
             }
             IOStdReq(io)->io_Actual = sizeof(struct NSDeviceQueryResult);
             break;
-            
+
         /*
-            New Style Devices report here the 'NSTY' - only if such value is 
-            returned here, the NSCMD_DEVICEQUERY might be called. Otherwice it should 
+            New Style Devices report here the 'NSTY' - only if such value is
+            returned here, the NSCMD_DEVICEQUERY might be called. Otherwice it should
             report error.
         */
         case TD_GETDRIVETYPE:
             IOStdReq(io)->io_Actual = DRIVE_NEWSTYLE;
             break;
-        
+
         /*
             Call all other commands using the command pointer tables for 32- and
             64-bit accesses. If requested function is defined call it, otherwise
@@ -597,14 +598,14 @@ static void HandleIO(struct IORequest *io, LIBBASETYPEPTR LIBBASE)
         default:
             if (io->io_Command <= (HD_SCSICMD+1))
             {
-                if (map32[io->io_Command]) 
+                if (map32[io->io_Command])
                     map32[io->io_Command](io, LIBBASE);
                 else
                     cmd_Invalid(io, LIBBASE);
             }
             else if (io->io_Command >= NSCMD_TD_READ64 && io->io_Command <= NSCMD_TD_FORMAT64)
             {
-                if (map64[io->io_Command - NSCMD_TD_READ64]) 
+                if (map64[io->io_Command - NSCMD_TD_READ64])
                     map64[io->io_Command - NSCMD_TD_READ64](io, LIBBASE);
                 else
                     cmd_Invalid(io, LIBBASE);
@@ -643,9 +644,9 @@ AROS_LH1(void, BeginIO,
     LIBBASETYPEPTR, LIBBASE, 5, ata)
 {
     AROS_LIBFUNC_INIT
-    
+
     struct ata_Unit *unit = (struct ata_Unit *)io->io_Unit;
-    
+
     io->io_Message.mn_Node.ln_Type = NT_MESSAGE;
 
     /* Disable interrupts for a while to modify message flags */
@@ -662,7 +663,7 @@ AROS_LH1(void, BeginIO,
         unit->au_Unit.unit_flags |= UNITF_ACTIVE | UNITF_INTASK;
         io->io_Flags &= ~IOF_QUICK;
         Enable();
-        
+
         /* Put the message to the bus */
         PutMsg(unit->au_Bus->ab_MsgPort, (struct Message *)io);
     }
@@ -726,7 +727,7 @@ AROS_LH1(ULONG, GetBlkSize,
 /*
     The daemon of ata.device first opens all ATAPI devices and then enters
     endless loop. Every 3 seconds it tells ATAPI units to check the media
-    presence. In case of any state change they will rise user-specified 
+    presence. In case of any state change they will rise user-specified
     functions.
 */
 static void DaemonCode(LIBBASETYPEPTR LIBBASE);
@@ -736,12 +737,12 @@ int ata_InitDaemonTask(LIBBASETYPEPTR LIBBASE)
 {
     struct Task     *t;
     struct MemList  *ml;
-    
+
     struct TagItem tags[] = {
         { TASKTAG_ARG1,     (IPTR)LIBBASE },
         { TAG_DONE, 0 }
     };
-    
+
     /* Get some memory */
     t = AllocMem(sizeof(struct Task), MEMF_PUBLIC | MEMF_CLEAR);
     ml = AllocMem(sizeof(struct MemList) + sizeof(struct MemEntry), MEMF_PUBLIC | MEMF_CLEAR);
@@ -765,7 +766,7 @@ int ata_InitDaemonTask(LIBBASETYPEPTR LIBBASE)
         t->tc_Node.ln_Name = "ATA.daemon";
         t->tc_Node.ln_Type = NT_TASK;
         t->tc_Node.ln_Pri  = TASK_PRI - 1;  /* The daemon should have a little bit lower Pri as handler tasks */
-        
+
         LIBBASE->ata_Daemon = t;
 
         NewAddTask(t, DaemonCode, NULL, (struct TagItem*)&tags);
@@ -774,9 +775,9 @@ int ata_InitDaemonTask(LIBBASETYPEPTR LIBBASE)
     return (t != NULL);
 }
 
-/* 
- * The daemon tries to send HD_SCSICMD+1 command (internal testchanged 
- * command) to all ATAPI devices in the system. They should already handle 
+/*
+ * The daemon tries to send HD_SCSICMD+1 command (internal testchanged
+ * command) to all ATAPI devices in the system. They should already handle
  * the command further.
  */
 void DaemonCode(LIBBASETYPEPTR LIBBASE)
@@ -790,8 +791,8 @@ void DaemonCode(LIBBASETYPEPTR LIBBASE)
 
     D(bug("[ATA**] You woke up DAEMON\n"));
 
-    /* 
-     * Prepare message ports and timer.device's request 
+    /*
+     * Prepare message ports and timer.device's request
      */
     mp          = CreateMsgPort();
     myport      = CreateMsgPort();
@@ -809,35 +810,35 @@ void DaemonCode(LIBBASETYPEPTR LIBBASE)
 
        for (d=0; d < MAX_BUSUNITS; d++)
        {
-          /* 
-           * Is a device ATAPI? 
+          /*
+           * Is a device ATAPI?
            */
           D(bug("[ATA++] Unit %d is of type %x\n", (b<<1)+d, bus->ab_Dev[d]));
 
           if (bus->ab_Dev[d] >= DEV_ATAPI)
           {
-             /* 
-              * Atapi device found. Create IOStdReq for it 
+             /*
+              * Atapi device found. Create IOStdReq for it
               */
              ios[count] = (struct IOStdReq *) CreateIORequest(myport, sizeof(struct IOStdReq));
 
              ios[count]->io_Command = HD_SCSICMD + 1;
-                    
+
              /*
-              * And force OpenDevice call. Don't use direct call as it's unsafe 
+              * And force OpenDevice call. Don't use direct call as it's unsafe
               * and not nice at all.
               *
               * so THIS is an OpenDevice().....
               */
              D(bug("[ATA++] Opening ATAPI device, unit %d\n", (b<<1)|d));
-             AROS_LVO_CALL3(void, 
+             AROS_LVO_CALL3(void,
                    AROS_LCA(struct IORequest *, (struct IORequest *)(ios[count]), A1),
-                   AROS_LCA(ULONG, (b << 1) | d, D0),   
+                   AROS_LCA(ULONG, (b << 1) | d, D0),
                    AROS_LCA(ULONG, 0, D1),
                    LIBBASETYPEPTR, LIBBASE, 1, ata);
-                        
-             /* 
-              * increase amount of ATAPI devices in system 
+
+             /*
+              * increase amount of ATAPI devices in system
               */
              count++;
           }
@@ -854,20 +855,21 @@ void DaemonCode(LIBBASETYPEPTR LIBBASE)
 
     D(bug("[ATA++] Starting sweep medium presence detection\n"));
 
-    if (count)
+//    if (count)
+    if (1)
     {
-        /* 
-         * Ok, open the timer.device 
+        /*
+         * Ok, open the timer.device
          */
         OpenDevice("timer.device", UNIT_VBLANK, (struct IORequest *)tr, 0);
 
-        /* 
-         * Endless loop 
+        /*
+         * Endless loop
          */
         for (b=0;;++b)
         {
-            /* 
-             * call separate IORequest for every ATAPI device 
+            /*
+             * call separate IORequest for every ATAPI device
              * we're calling HD_SCSICMD+1 command here -- anything like test unit ready?
              */
             if (0 == (b & 3))
@@ -891,7 +893,7 @@ void DaemonCode(LIBBASETYPEPTR LIBBASE)
                 }
             }
 
-            /* 
+            /*
              * And then hide and wait for 1 second
              */
             tr->tr_node.io_Command = TR_ADDREQUEST;
@@ -915,21 +917,21 @@ void DaemonCode(LIBBASETYPEPTR LIBBASE)
 static void TaskCode(struct ata_Bus *, struct Task*, struct SignalSemaphore*);
 static void ata_Interrupt(HIDDT_IRQ_Handler *, HIDDT_IRQ_HwInfo *);
 
-/* 
+/*
  * Make a task for given bus alive.
  */
 int ata_InitBusTask(struct ata_Bus *bus, struct SignalSemaphore *ready)
 {
     struct Task     *t;
     struct MemList  *ml;
-    
+
     struct TagItem tags[] = {
         { TASKTAG_ARG1,     (IPTR)bus },
         { TASKTAG_ARG2,     (IPTR)FindTask(0) },
         { TASKTAG_ARG3,     (IPTR)ready },
         { TAG_DONE, 0 }
     };
-    
+
     /*
         Need some memory. I don't know however, whether it wouldn't be better
         to take some RAM from device's memory pool.
@@ -970,7 +972,7 @@ int ata_InitBusTask(struct ata_Bus *bus, struct SignalSemaphore *ready)
         t->tc_Node.ln_Name = "ATA[PI] Subsystem";
         t->tc_Node.ln_Type = NT_TASK;
         t->tc_Node.ln_Pri  = TASK_PRI;
-        
+
         /* Wake up the task */
         NewAddTask(t, TaskCode, NULL, (struct TagItem*)&tags);
         Wait(SIGBREAKF_CTRL_C);
@@ -1003,7 +1005,7 @@ static int CreateInterrupt(struct ata_Bus *bus)
                 handlerinfo:    bus->ab_IntHandler,
                 id:             bus->ab_Irq,
             }, *msg = &__msg__;
-            
+
             if (OOP_DoMethod((OOP_Object *)o, (OOP_Msg)msg))
                 retval = 1;
 
@@ -1075,12 +1077,12 @@ static void TaskCode(struct ata_Bus *bus, struct Task* parent, struct SignalSema
     for (;;)
     {
         Wait(sig);
-        
+
         /* Even if you get new signal, do not process it until Unit is not active */
         if (!(bus->ab_Flags & UNITF_ACTIVE))
         {
             bus->ab_Flags |= UNITF_ACTIVE;
-            
+
             /* Empty the request queue */
             while ((msg = (struct IORequest *)GetMsg(bus->ab_MsgPort)))
             {
