fd1a5a3baf4c2a8d063ff9a7bb9de2aae637872a
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-03-01 13:05:08 +0000

    KRN_DebugInfo tag
    git-svn-id: https://svn.aros.org/svn/aros/trunk@30681 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/aros/kernel.h
+++ compiler/include/aros/kernel.h
@@ -47,6 +47,7 @@ typedef enum {
 #define KRN_MEMLower         	(KRN_Dummy + 17)
 #define KRN_MEMUpper          	(KRN_Dummy + 18)
 #define KRN_OpenFirmwareTree	(KRN_Dummy + 19)
-#define KRN_HostInterface	(KRN_Dummy + 20)
+#define KRN_HostInterface		(KRN_Dummy + 20)
+#define KRN_DebugInfo			(KRN_Dummy + 21)
 
 #endif /* AROS_KERNEL_H */
