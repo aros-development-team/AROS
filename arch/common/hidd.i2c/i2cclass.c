8834a716ad884cb34bb9d0e9842ae9e28e8ad4ab
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-03-01 17:19:48 +0000

    less debug, compute delays properly
    git-svn-id: https://svn.aros.org/svn/aros/trunk@30691 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/common/hidd.i2c/i2cclass.c
+++ arch/common/hidd.i2c/i2cclass.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 2004-2006, The AROS Development Team. All rights reserved.
+    Copyright ï¿½ 2004-2006, The AROS Development Team. All rights reserved.
     $Id$
 */
 
@@ -20,7 +20,7 @@
 
 #include "i2c.h"
 
-#define DEBUG 1
+#define DEBUG 0
 #include <aros/debug.h>
 #include <aros/atomic.h>
 
@@ -39,11 +39,11 @@ static void I2C_UDelay(tDrvData *drv, ULONG delay)
 {
     drv->mp.mp_SigTask = FindTask(NULL);
     drv->tr.tr_node.io_Command = TR_ADDREQUEST;
-    drv->tr.tr_time.tv_secs = delay / 100000;
-    drv->tr.tr_time.tv_micro = 10 * (delay % 100000);
-    
+    drv->tr.tr_time.tv_secs = delay / 10000;
+    drv->tr.tr_time.tv_micro = 10 * (delay % 10000);
+
     DoIO((struct IORequest *)&drv->tr);
-    
+
     drv->mp.mp_SigTask = NULL;
 }
 
@@ -51,38 +51,38 @@ static void I2C_UDelay(tDrvData *drv, ULONG delay)
 static BOOL RaiseSCL(OOP_Class *cl, OOP_Object *o, BOOL sda, ULONG timeout)
 {
     tDrvData *drv = (tDrvData *)OOP_INST_DATA(cl, o);
-    
+
     int i;
     BOOL scl;
-    
+
     I2C_PutBits(o, 1, sda);
     I2C_UDelay(drv, drv->RiseFallTime);
-    
+
     for (i=timeout; i>0; i -= drv->RiseFallTime)
     {
         I2C_GetBits(o, &scl, &sda);
         if (scl) break;
         I2C_UDelay(drv, drv->RiseFallTime);
     }
-    
+
     if (i <= 0)
     {
         bug("[I2C] RaiseSCL(<%s>,%d,%d) timeout\n", drv->name, sda, timeout);
         return FALSE;
     }
-    
+
     return TRUE;
 }
 
 static BOOL WriteBit(OOP_Class *cl, OOP_Object *o, BOOL sda, ULONG timeout)
 {
     tDrvData *drv = (tDrvData *)OOP_INST_DATA(cl, o);
-    
+
     BOOL r;
-    
+
     I2C_PutBits(o, 0, sda);
     I2C_UDelay(drv, drv->RiseFallTime);
-    
+
     r = RaiseSCL(cl, o, sda, timeout);
     I2C_UDelay(drv, drv->HoldTime);
 
@@ -95,17 +95,17 @@ static BOOL WriteBit(OOP_Class *cl, OOP_Object *o, BOOL sda, ULONG timeout)
 static BOOL ReadBit(OOP_Class *cl, OOP_Object *o, BOOL *sda, ULONG timeout)
 {
     tDrvData *drv = (tDrvData *)OOP_INST_DATA(cl, o);
-    
+
     BOOL scl, r;
-    
+
     r = RaiseSCL(cl, o, 1, timeout);
     I2C_UDelay(drv, drv->HoldTime);
-    
+
     I2C_GetBits(o, &scl, sda);
 
     I2C_PutBits(o, 0, 1);
     I2C_UDelay(drv, drv->HoldTime);
-    
+
     return r;
 }
 
@@ -126,10 +126,10 @@ BOOL METHOD(I2C, Hidd_I2C, Start)
     tDrvData *drv = (tDrvData *)OOP_INST_DATA(cl, o);
 
     LOCK_HW
-    
+
     if (!RaiseSCL(cl, o, 1, msg->timeout))
         return FALSE;
-    
+
     I2C_PutBits(o, 1, 0);
     I2C_UDelay(drv, drv->HoldTime);
     I2C_PutBits(o, 0, 0);
@@ -137,9 +137,9 @@ BOOL METHOD(I2C, Hidd_I2C, Start)
 
     D(bug("\n[I2C]: <"));
 
-    UNLOCK_HW    
+    UNLOCK_HW
 
-    return TRUE;    
+    return TRUE;
 }
 
 BOOL METHOD(I2C, Hidd_I2C, Address)
@@ -162,7 +162,7 @@ BOOL METHOD(I2C, Hidd_I2C, Address)
         I2C_Stop(o, msg->device);
     }
 
-    UNLOCK_HW    
+    UNLOCK_HW
 
     return FALSE;
 }
@@ -177,25 +177,25 @@ BOOL METHOD(I2C, Hidd_I2C, ProbeAddress)
         { aHidd_I2CDevice_Name,     (IPTR)"Probing" },
         { TAG_DONE, 0UL }
     };
-    
+
     BOOL r = FALSE;
 
     D(bug("[I2C] I2C::ProbeAddress(%04x)\n", msg->address));
 
     LOCK_HW
-        
+
     OOP_Object *probing = OOP_NewObject(SD(cl)->i2cDeviceClass, NULL, attrs);
-    
+
     if (probing)
     {
         r = I2C_Address(o, probing, msg->address);
         if (r)
           I2C_Stop(o, probing);
-          
+
         OOP_DisposeObject(probing);
     }
-    
-    UNLOCK_HW    
+
+    UNLOCK_HW
 
     return r;
 }
@@ -209,15 +209,15 @@ void METHOD(I2C, Hidd_I2C, Stop)
 
     I2C_PutBits(o, 0, 0);
     I2C_UDelay(drv, drv->RiseFallTime);
-    
+
     I2C_PutBits(o, 1, 0);
     I2C_UDelay(drv, drv->HoldTime);
     I2C_PutBits(o, 1, 1);
     I2C_UDelay(drv, drv->HoldTime);
 
     D(bug(">\n"));
-    
-    UNLOCK_HW    
+
+    UNLOCK_HW
 }
 
 BOOL METHOD(I2C, Hidd_I2C, PutByte)
@@ -229,7 +229,7 @@ BOOL METHOD(I2C, Hidd_I2C, PutByte)
     int i;
 
     LOCK_HW
-    
+
     if (!WriteBit(cl, o, (msg->data >> 7) & 1, dev->ByteTimeout))
     {
         UNLOCK_HW
@@ -263,15 +263,15 @@ BOOL METHOD(I2C, Hidd_I2C, PutByte)
                                        dev->ByteTimeout, dev->AcknTimeout);
             r = FALSE;
         }
-        
+
         D(bug("W%02x%c ", (int) msg->data, sda ? '-' : '+'));
     }
 
     I2C_PutBits(o, 0, 1);
     I2C_UDelay(drv, drv->HoldTime);
-    
+
     UNLOCK_HW
-    
+
     return r;
 }
 
@@ -282,7 +282,7 @@ BOOL METHOD(I2C, Hidd_I2C, GetByte)
 
     int i;
     BOOL sda;
-    
+
     LOCK_HW
 
     I2C_PutBits(o, 0, 1);
@@ -291,7 +291,7 @@ BOOL METHOD(I2C, Hidd_I2C, GetByte)
     if (!ReadBit(cl, o, &sda, dev->ByteTimeout))
     {
         UNLOCK_HW
-        return FALSE;        
+        return FALSE;
     }
 
 
@@ -313,8 +313,8 @@ BOOL METHOD(I2C, Hidd_I2C, GetByte)
     }
 
     D(bug("R%02x%c ", (int) *msg->data, msg->last ? '+' : '-'));
-    
-    UNLOCK_HW    
+
+    UNLOCK_HW
 
     return TRUE;
 }
@@ -333,7 +333,7 @@ BOOL METHOD(I2C, Hidd_I2C, WriteRead)
     UBYTE *ReadBuffer = msg->readBuffer;
 
     LOCK_HW
-    
+
     if (r && nWrite > 0) {
         r = I2C_Address(o, msg->device, dev->address & ~1);
         if (r) {
@@ -356,7 +356,7 @@ BOOL METHOD(I2C, Hidd_I2C, WriteRead)
 
     if (s) I2C_Stop(o, msg->device);
 
-    UNLOCK_HW    
+    UNLOCK_HW
 
     return r;
 }
@@ -376,7 +376,7 @@ void METHOD(I2C, Root, Get)
             case aoHidd_I2C_HoldTime:
                 *msg->storage = drv->HoldTime;
                 break;
-                    
+
             case aoHidd_I2C_BitTimeout:
                 *msg->storage = drv->BitTimeout;
                 break;
@@ -396,7 +396,7 @@ void METHOD(I2C, Root, Get)
             case aoHidd_I2C_RiseFallTime:
                 *msg->storage = drv->RiseFallTime;
                 break;
-            
+
             case aoHidd_I2C_Name:
                 *msg->storage = (IPTR)drv->name;
                 break;
@@ -425,7 +425,7 @@ void METHOD(I2C, Root, Set)
                 case aoHidd_I2C_HoldTime:
                     drv->HoldTime = tag->ti_Data;
                     break;
-                    
+
                 case aoHidd_I2C_BitTimeout:
                     drv->BitTimeout = tag->ti_Data;
                     break;
@@ -453,7 +453,7 @@ void METHOD(I2C, Root, Set)
 OOP_Object *METHOD(I2C, Root, New)
 {
     D(bug("[I2C] new()\n"));
-    
+
     o = (OOP_Object *)OOP_DoSuperMethod(cl, o, (OOP_Msg) msg);
     if (o)
     {
@@ -467,23 +467,23 @@ OOP_Object *METHOD(I2C, Root, New)
         drv->ByteTimeout = 5;
         drv->RiseFallTime = 2;
         drv->StartTimeout = 5;
-        
+
         drv->name = (STRPTR)"bus?";
 
         InitSemaphore(&drv->driver_lock);
-        
+
         D(bug("[I2C] Initializing MsgPort\n"));
-        
+
         /* Initialize MsgPort */
         drv->mp.mp_SigBit = SIGB_SINGLE;
         drv->mp.mp_Flags = PA_SIGNAL;
         drv->mp.mp_SigTask = FindTask(NULL);
         drv->mp.mp_Node.ln_Type = NT_MSGPORT;
         NEWLIST(&drv->mp.mp_MsgList);
-        
+
         drv->tr.tr_node.io_Message.mn_ReplyPort = &drv->mp;
         drv->tr.tr_node.io_Message.mn_Length = sizeof(drv->tr);
-        
+
         D(bug("[I2C] Trying to open UNIT_MICROHZ of timer.device\n"));
 
         if (!OpenDevice((STRPTR)"timer.device", UNIT_MICROHZ, (struct IORequest *)&drv->tr, 0))
@@ -495,7 +495,7 @@ OOP_Object *METHOD(I2C, Root, New)
             while((tag = NextTagItem(&tags)))
             {
                 ULONG idx;
-            
+
                 if (IS_I2C_ATTR(tag->ti_Tag, idx))
                 {
                     switch(idx)
@@ -503,27 +503,27 @@ OOP_Object *METHOD(I2C, Root, New)
                         case aoHidd_I2C_Name:
                             drv->name = (STRPTR)tag->ti_Data;
                             break;
-                
+
                         case aoHidd_I2C_HoldTime:
                             drv->HoldTime = tag->ti_Data;
                             break;
-            
+
                         case aoHidd_I2C_BitTimeout:
                             drv->BitTimeout = tag->ti_Data;
                             break;
-            
+
                         case aoHidd_I2C_ByteTimeout:
                             drv->ByteTimeout = tag->ti_Data;
                             break;
-                        
+
                         case aoHidd_I2C_AcknTimeout:
                             drv->AcknTimeout = tag->ti_Data;
                             break;
-            
+
                         case aoHidd_I2C_StartTimeout:
                             drv->StartTimeout = tag->ti_Data;
                             break;
-            
+
                         case aoHidd_I2C_RiseFallTime:
                             drv->RiseFallTime = tag->ti_Data;
                             break;
@@ -564,17 +564,17 @@ static int I2C_ExpungeClass(LIBBASETYPEPTR LIBBASE)
 static int I2C_InitClass(LIBBASETYPEPTR LIBBASE)
 {
     D(bug("[I2C] base class initialization\n"));
-    
+
     LIBBASE->sd.hiddI2CAB = OOP_ObtainAttrBase((STRPTR)IID_Hidd_I2C);
     LIBBASE->sd.hiddI2CDeviceAB = OOP_ObtainAttrBase((STRPTR)IID_Hidd_I2CDevice);
     LIBBASE->sd.hiddAB = OOP_ObtainAttrBase((STRPTR)IID_Hidd);
-    
+
     if (LIBBASE->sd.hiddI2CAB && LIBBASE->sd.hiddI2CDeviceAB && LIBBASE->sd.hiddAB)
     {
         D(bug("[I2C] Everything OK\n"));
         return TRUE;
     }
-    
+
     return FALSE;
 }
 
