187ec80d2eb4ac39d27d41219a7781c9c9ddaf4e
Kalamatee <kalamatee@gmail.com>
2009-03-03 00:44:46 +0000

    WIP: display a bit more detailed information about volumes (see warnings)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@30715 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/system/Wanderer/Tools/DiskInfo/diskinfo.c
+++ workbench/system/Wanderer/Tools/DiskInfo/diskinfo.c
@@ -12,6 +12,7 @@
 #include <utility/tagitem.h>
 #include <libraries/mui.h>
 #include <libraries/asl.h>
+#include <dos/filehandler.h>
 
 #include <proto/exec.h>
 #include <proto/dos.h>
@@ -73,74 +74,157 @@ Object *DiskInfo__OM_NEW
     ULONG                       percent        = 0;
     LONG                        disktype       = ID_NO_DISK_PRESENT;
     LONG                        aspect         = 0;
-    TEXT                        volname[108];
     TEXT                        size[64];
     TEXT                        used[64];
     TEXT                        free[64];
     TEXT                        blocksize[16];
     STRPTR                      status = NULL;
-    STRPTR			            dtr;
-    struct DosList	           *dl, *dn;
+    STRPTR			dosdevname = NULL;
+    STRPTR			filesystem = NULL;
+    STRPTR			fstype = NULL;
+    STRPTR			fshandler = NULL;
+    TEXT                        volname[108];
+    STRPTR                      volicon = NULL;
+    STRPTR			handlertype = "";
+    STRPTR			deviceinfo = "";
+ 
+    struct DosList	        *dl, *dn;
     BOOL                        disktypefound = FALSE;
 
     static struct InfoData id;
 
-    static STRPTR disktypelist[] = {"No Disk", "Unreadable",
-    "OFS", "FFS", "OFS-Intl", "FFS-Intl",
-    "OFS-DC", "FFS-DC", "Not DOS",
-    "KickStart", "MSDOS", "SFS0 BE", "SFS0 LE",
-    "FAT12", "FAT16", "FAT32", "CD-ROM" };
+    static STRPTR disktypelist[] = 
+    { 
+	"No Disk",
+	"Unreadable",
+	"OFS",
+	"FFS",
+	"OFS-Intl",
+	"FFS-Intl",
+	"OFS-DC",
+	"FFS-DC",
+	"Not DOS",
+	"KickStart",
+	"MSDOS",
+	"SFS0 BE",
+	"SFS0 LE",
+	"FAT12",
+	"FAT16",
+	"FAT32",
+	"CD-ROM"
+    };
 
     /* Parse initial taglist -----------------------------------------------*/
-    D(bug("[DiskInfo] OM_NEW\n"));
+    D(bug("[DiskInfo] %s\n", __PRETTY_FUNCTION__));
     while ((tag = NextTagItem(&tstate)) != NULL)
     {
         switch (tag->ti_Tag)
         {
             case MUIA_DiskInfo_Initial:
                 initial = (BPTR) tag->ti_Data;
-                D(bug("[DiskInfo] initial: 0x%08lX\n",initial));
+                D(bug("[DiskInfo] %s: initial lock @ 0x%p\n", __PRETTY_FUNCTION__, initial));
                 break;
+#warning "TODO: Remove MUIA_DiskInfo_Aspect"
             case MUIA_DiskInfo_Aspect:
                 aspect = tag->ti_Data;
-                D(bug("[DiskInfo] aspect: %d\n",aspect));
+                D(bug("[DiskInfo] %s: aspect: %d\n", __PRETTY_FUNCTION__, aspect));
                 break;
         }
     }
-
+    
     /* Initial lock is required */
     if (initial == NULL)
     {
         return NULL;
     }
 
-    // obtain the volume name from a lock
+    /* obtain volume's name from the lock */
     if (!NameFromLock(initial, volname, sizeof(volname))) {
         SetIoErr(ERROR_DEVICE_NOT_MOUNTED);
         return NULL;
     }
+    int volname_len = strlen(volname);
+    if ((volicon = AllocVec(volname_len + 5, MEMF_CLEAR)) == NULL)
+    {
+        SetIoErr(ERROR_DEVICE_NOT_MOUNTED);
+        return NULL;
+    }
+    strcpy(volicon, volname);
+    strcat(volicon, "disk");
     volname[strlen(volname)-1] = '\0';
-    dtr = _(MSG_UNKNOWN);
+    D(bug("[DiskInfo] %s: Volume '%s'\n", __PRETTY_FUNCTION__, volname));
+
+    /* find the volumes doslist information .. */
+    IPTR volunit = NULL;
+    filesystem = _(MSG_UNKNOWN);
     dl = LockDosList(LDF_VOLUMES|LDF_READ);
     if (dl) {
 	dn = FindDosEntry(dl, volname, LDF_VOLUMES);
 	if (dn) {
 	    ULONG i;
 
+	    volunit = dn->dol_Ext.dol_AROS.dol_Unit;
+
+	    D(bug("[DiskInfo] %s: Volume's unit @ %p\n", __PRETTY_FUNCTION__, volunit));
+
+	    if (dn->dol_Task != NULL)
+	    {
+		handlertype = "Packet Style device";
+	    }
+	    else if (dn->dol_Ext.dol_AROS.dol_Device != NULL)
+	    {
+		handlertype = "IOFS Style device";
+	    }
+
 	    disktype = dn->dol_misc.dol_volume.dol_DiskType;
 	    for (i = 0; i < sizeof(dt) / sizeof(LONG); ++i)
 	    {
 		if (disktype == dt[i])
 		{
-		    dtr = disktypelist[i];
-            disktypefound = TRUE;
+		    fstype = disktypelist[i];
+		    fshandler = dn->dol_Ext.dol_AROS.dol_Device->dd_Library.lib_Node.ln_Name;
+		    disktypefound = TRUE;
 		    break;
 		}
 	    }
-            D(bug("[DiskInfo] Disk Type: %s\n", dtr));
-	}    
+            D(bug("[DiskInfo] %s: Disk Type: %s\n", __PRETTY_FUNCTION__, filesystem));
+	}
 	UnLockDosList(LDF_VOLUMES|LDF_READ);
     }
+    /* If we know the volumes unit - find its device information .. */
+    if (volunit != NULL)
+    {
+	dl = LockDosList(LDF_DEVICES|LDF_READ);
+	if (dl) {
+	    while((dl = NextDosEntry(dl, LDF_DEVICES)))
+	    {
+		if (dl->dol_Ext.dol_AROS.dol_Unit == volunit)
+		{
+		    struct FileSysStartupMsg *fsstartup = (struct FileSysStartupMsg *)BADDR(dl->dol_misc.dol_handler.dol_Startup);
+		    dosdevname = (UBYTE*)AROS_BSTR_ADDR(dl->dol_Name);
+		    fshandler = (UBYTE*)AROS_BSTR_ADDR(dl->dol_misc.dol_handler.dol_Handler);
+
+		    D(bug("[DiskInfo] %s: Found Volumes device @ %p, '%s'\n", __PRETTY_FUNCTION__, dl, dosdevname));
+		    if (fsstartup != NULL)
+		    {
+			deviceinfo = AllocVec(strlen((UBYTE*)AROS_BSTR_ADDR(fsstartup->fssm_Device)) + (fsstartup->fssm_Unit/10 + 1) + 7, MEMF_CLEAR);
+			sprintf(deviceinfo,"%s %s %d", (UBYTE*)AROS_BSTR_ADDR(fsstartup->fssm_Device), _(MSG_UNIT), fsstartup->fssm_Unit);
+		    }
+		    D(bug("[DiskInfo] %s: Handler '%s'\n", __PRETTY_FUNCTION__, fshandler));
+		    break;
+		}
+	    }
+	    UnLockDosList(LDF_VOLUMES|LDF_READ);
+	}
+    }
+
+    if (fstype && fshandler)
+    {
+	filesystem = AllocVec(strlen(fstype) + strlen(fshandler) + 4, MEMF_CLEAR);
+	sprintf(filesystem, "%s (%s)", fstype, fshandler);
+    }
+
+    volname[strlen(volname)] = ':';
 
     /* Extract volume info from InfoData */
     if (Info(initial, &id) == DOSTRUE)
@@ -148,32 +232,39 @@ Object *DiskInfo__OM_NEW
         if (!disktypefound) /* Workaround for FFS-Intl having 0 as dol_DiskType */
         {
             LONG i;
-            dtr = _(MSG_UNKNOWN);
+            filesystem = _(MSG_UNKNOWN);
             disktype = id.id_DiskType;
 
             for (i = 0; i < sizeof(dt) / sizeof(LONG); ++i)
             {
                 if (disktype == dt[i])
                 {
-                    dtr = disktypelist[i];
+                    filesystem = disktypelist[i];
                     break;
                 }
             }
         }
 
-        percent = (100 * id.id_NumBlocksUsed/id.id_NumBlocks);
+        percent = (100 * (id.id_NumBlocksUsed / id.id_NumBlocks));
         FormatSize(size, id.id_NumBlocks, id.id_NumBlocks, id.id_BytesPerBlock, FALSE);
         FormatSize(used, id.id_NumBlocksUsed, id.id_NumBlocks, id.id_BytesPerBlock, TRUE);
         FormatSize(free, id.id_NumBlocks - id.id_NumBlocksUsed, id.id_NumBlocks, id.id_BytesPerBlock, TRUE);
         sprintf(blocksize, "%d %s", id.id_BytesPerBlock, _(MSG_BYTES));
 
-        switch(id.id_DiskState)
-        {
-            case(ID_WRITE_PROTECTED): status = _(MSG_READABLE); break;
-            case(ID_VALIDATING): status = _(MSG_VALIDATING); break;
-            case(ID_VALIDATED): status = _(MSG_READABLE_WRITABLE); break;
-            default: status = _(MSG_UNKNOWN);
-        }
+        switch (id.id_DiskState)
+	{
+	    case (ID_WRITE_PROTECTED):
+		status = _(MSG_READABLE);
+		break;
+	    case (ID_VALIDATING):
+		status = _(MSG_VALIDATING);
+		break;
+	    case (ID_VALIDATED):
+		status = _(MSG_READABLE_WRITABLE);
+		break;
+	    default:
+		status = _(MSG_UNKNOWN);
+	}
     }
 
     /* Create application and window objects -------------------------------*/
@@ -182,100 +273,152 @@ Object *DiskInfo__OM_NEW
         CLASS, self, NULL,
 
         MUIA_Application_Title, __(MSG_TITLE),
-        MUIA_Application_Version, (IPTR) "$VER: DiskInfo 0.3 ("ADATE") ©2006-2009 AROS Dev Team",
+        MUIA_Application_Version, (IPTR) "$VER: DiskInfo 0.5 ("ADATE") ©2006-2009 AROS Dev Team",
         MUIA_Application_Copyright, __(MSG_COPYRIGHT),
         MUIA_Application_Author, __(MSG_AUTHOR),
         MUIA_Application_Description, __(MSG_DESCRIPTION),
         MUIA_Application_Base, (IPTR) "DISKINFO",
         SubWindow, (IPTR) (window = (Object *)WindowObject,
-            MUIA_Window_Title,(IPTR) volname,
+            MUIA_Window_Title, (IPTR) volname,
             MUIA_Window_Activate,    TRUE,
             MUIA_Window_NoMenus,     TRUE,
             MUIA_Window_CloseGadget, TRUE,
 
             WindowContents, (IPTR) VGroup,
                 Child, (IPTR) HGroup,
-                    Child, (IPTR) VGroup,
-                        Child, (IPTR) IconImageObject,
-                            MUIA_InputMode, MUIV_InputMode_Toggle,
-                            MUIA_IconImage_File, (IPTR) initial,
-                        End,
-                    End,
+		    Child, (IPTR) VGroup,
+			Child, HVSpace,
+			Child, (IPTR) HGroup,
+			    Child, HVSpace,
+			    Child, (IPTR) IconImageObject,
+				MUIA_InputMode, MUIV_InputMode_Toggle,
+				MUIA_IconImage_File, (IPTR) volicon,
+			    End,
+			    Child, HVSpace,
+			End,
+			Child, HVSpace,
+		    End,
                     Child, (IPTR) (grp = (Object *)VGroup,
-                        Child, (IPTR) TextObject,
-                            MUIA_Text_PreParse, (IPTR) "\33I[6:24] \33b\33l",
-                            MUIA_Text_Contents, (IPTR) dtr,
-                        End,
-                        Child, (IPTR) GaugeObject, GaugeFrame,
-                            MUIA_Gauge_InfoText, __(MSG_PERCENTFULL),
-                            MUIA_Gauge_Horiz, TRUE,
-                            MUIA_Gauge_Current, percent,
-                        End,
-                    End),
-                End,
-                Child, (IPTR) HGroup,
-                    Child, (IPTR) VGroup, GroupFrame,
+			Child, HVSpace,
                         Child, (IPTR) ColGroup(2),
+#warning "TODO: Build this list only when data is realy available, and localise"
                             Child, (IPTR) TextObject, 
                                 MUIA_Text_PreParse, (IPTR) "\33r",
-                                MUIA_Text_Contents, __(MSG_NAME),
+                                MUIA_Text_Contents, (IPTR) __(MSG_DOSDEVICE),
                             End,
-                            Child, (IPTR) TextObject, TextFrame,
-                                MUIA_Background, MUII_TextBack,
-                                MUIA_Text_PreParse, (IPTR) "\33l",
-                                MUIA_Text_Contents, (IPTR) volname,
-                            End,
+			    Child, (IPTR) TextObject, 
+				MUIA_Text_PreParse, (IPTR) "\33l",
+				MUIA_Text_Contents, (IPTR) dosdevname,
+			    End,
                             Child, (IPTR) TextObject, 
                                 MUIA_Text_PreParse, (IPTR) "\33r",
-                                MUIA_Text_Contents, __(MSG_SIZE),
+                                MUIA_Text_Contents, (IPTR) __(MSG_DEVICEINFO),
                             End,
-                            Child, (IPTR) TextObject, TextFrame,
-                                MUIA_Background, MUII_TextBack,
-                                MUIA_Text_PreParse, (IPTR) "\33l",
-                                MUIA_Text_Contents, (IPTR) size,
-                            End,
+			    Child, (IPTR) TextObject, 
+				MUIA_Text_PreParse, (IPTR) "\33l",
+				MUIA_Text_Contents, (IPTR) deviceinfo,
+			    End,
                             Child, (IPTR) TextObject, 
                                 MUIA_Text_PreParse, (IPTR) "\33r",
-                                MUIA_Text_Contents, __(MSG_USED),
+                                MUIA_Text_Contents, (IPTR) __(MSG_FILESYSTEM),
                             End,
-                            Child, (IPTR) TextObject, TextFrame,
-                                MUIA_Background, MUII_TextBack,
-                                MUIA_Text_PreParse, (IPTR) "\33l",
-                                MUIA_Text_Contents, (IPTR) used,
-                            End,
-                            Child, (IPTR) TextObject,
-                                MUIA_Text_PreParse, (IPTR) "\33r",
-                                MUIA_Text_Contents, __(MSG_FREE),
-                            End,
-                            Child, (IPTR) TextObject, TextFrame,
-                                MUIA_Background, MUII_TextBack,
-                                MUIA_Text_PreParse, (IPTR) "\33l",
-                                MUIA_Text_Contents, (IPTR) free,
-                            End,
-                            Child, (IPTR) TextObject,
-                                MUIA_Text_PreParse, (IPTR) "\33r",
-                                MUIA_Text_Contents, __(MSG_BLOCK_SIZE),
-                            End,
-                            Child, (IPTR) TextObject, TextFrame,
-                                MUIA_Background, MUII_TextBack,
-                                MUIA_Text_PreParse, (IPTR) "\33l",
-                                MUIA_Text_Contents, (IPTR) blocksize,
-                            End,
-                            Child, (IPTR) TextObject,
-                                MUIA_Text_PreParse, (IPTR) "\33r",
-                                MUIA_Text_Contents, __(MSG_STATUS),
-                            End,
-                            Child, (IPTR) TextObject, TextFrame,
-                                MUIA_Background, MUII_TextBack,
-                                MUIA_Text_PreParse, (IPTR) "\33l",
-                                MUIA_Text_Contents, (IPTR) status,
-                            End,
-                        End,
-                    End,
+			    Child, (IPTR) TextObject, 
+				MUIA_Text_PreParse, (IPTR) "\33I[6:24] \33l",
+				MUIA_Text_Contents, (IPTR) filesystem,
+			    End,
+                            Child, (IPTR) HVSpace,
+			    Child, (IPTR) TextObject, 
+				MUIA_Text_PreParse, (IPTR) "\33l",
+				MUIA_Text_Contents, (IPTR) handlertype,
+			    End,
+			End,
+			Child, HVSpace,
+                    End),
+		    Child, HVSpace,
                 End,
-                Child, (IPTR) (grpformat = (Object *)HGroup,
-                    // grpformat object userlevel sensitive
-                End),
+                Child, (IPTR) VGroup,
+		    Child, (IPTR) HGroup,
+			MUIA_Weight, 100,
+			GroupFrame,
+			Child, HVSpace,
+			Child, (IPTR) ColGroup(2),
+			    Child, (IPTR) TextObject, 
+				MUIA_Text_PreParse, (IPTR) "\33r",
+				MUIA_Text_Contents, (IPTR) __(MSG_NAME),
+			    End,
+			    Child, (IPTR) TextObject, TextFrame,
+				MUIA_Background, MUII_TextBack,
+				MUIA_Text_PreParse, (IPTR) "\33b\33l",
+				MUIA_Text_Contents, (IPTR) volname,
+			    End,
+			    Child, (IPTR) VGroup,
+				Child, (IPTR) TextObject, 
+				    MUIA_Text_PreParse, (IPTR) "\33r",
+				    MUIA_Text_Contents, (IPTR) __(MSG_SIZE),
+				End,
+				Child, (IPTR) TextObject, 
+				    MUIA_Text_PreParse, (IPTR) "\33r",
+				    MUIA_Text_Contents, (IPTR) __(MSG_USED),
+				End,
+				Child, (IPTR) TextObject,
+				    MUIA_Text_PreParse, (IPTR) "\33r",
+				    MUIA_Text_Contents, (IPTR) __(MSG_FREE),
+				End,
+			    End,
+			    Child, (IPTR) HGroup,
+				Child, (IPTR) VGroup,
+				    Child, (IPTR) TextObject, TextFrame,
+					MUIA_Background, MUII_TextBack,
+					MUIA_Text_PreParse, (IPTR) "\33l",
+					MUIA_Text_Contents, (IPTR) size,
+				    End,
+				    Child, (IPTR) TextObject, TextFrame,
+					MUIA_Background, MUII_TextBack,
+					MUIA_Text_PreParse, (IPTR) "\33l",
+					MUIA_Text_Contents, (IPTR) used,
+				    End,
+				    Child, (IPTR) TextObject, TextFrame,
+					MUIA_Background, MUII_TextBack,
+					MUIA_Text_PreParse, (IPTR) "\33l",
+					MUIA_Text_Contents, (IPTR) free,
+				    End,				
+				End,
+				Child, (IPTR) GaugeObject, GaugeFrame,
+				    MUIA_Width, 4,
+				    MUIA_Gauge_InfoText, "",
+				    MUIA_Gauge_Horiz, FALSE,
+				    MUIA_Gauge_Current, percent,
+				End,
+			    End,
+			    Child, (IPTR) TextObject,
+				MUIA_Text_PreParse, (IPTR) "\33r",
+				MUIA_Text_Contents, (IPTR) __(MSG_BLOCK_SIZE),
+			    End,
+			    Child, (IPTR) TextObject, TextFrame,
+				MUIA_Background, MUII_TextBack,
+				MUIA_Text_PreParse, (IPTR) "\33l",
+				MUIA_Text_Contents, (IPTR) blocksize,
+			    End,
+			    Child, (IPTR) TextObject,
+				MUIA_Text_PreParse, (IPTR) "\33r",
+				MUIA_Text_Contents, (IPTR) __(MSG_STATUS),
+			    End,
+			    Child, (IPTR) TextObject, TextFrame,
+				MUIA_Background, MUII_TextBack,
+				MUIA_Text_PreParse, (IPTR) "\33l",
+				MUIA_Text_Contents, (IPTR) status,
+			    End,
+			    Child, HVSpace,
+			    Child, HVSpace,
+			End,
+			Child, HVSpace,
+		    End,
+		    Child, (IPTR) (grpformat = HGroup,
+			// grpformat object userlevel sensitive
+			Child, HVSpace,
+		    End),
+		    Child, HVSpace,
+                End,
             End,
         End),
         TAG_DONE);
