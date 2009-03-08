bb3a74184d1a5bfbf0beffbad80ef7e37fcec7d9
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-03-08 21:15:45 +0000

    Syncing the clib and build system changes with Efika...
    git-svn-id: https://svn.aros.org/svn/aros/trunk@30805 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/ppc-chrp/exec/exec_init.c
+++ arch/ppc-chrp/exec/exec_init.c
@@ -333,12 +333,19 @@ int exec_main(struct TagItem *msg, void *entry)
 
         if (t->tc_Flags & TF_ETASK)
         {
-            t->tc_UnionETask.tc_ETask = AllocTaskMem(t, sizeof(struct IntETask), MEMF_ANY|MEMF_CLEAR);
+            t->tc_UnionETask.tc_ETask = AllocVec
+            (
+        	sizeof(struct IntETask), 
+        	MEMF_ANY|MEMF_CLEAR
+            );
 
             if (!t->tc_UnionETask.tc_ETask)
             {
                 D(bug("[exec] Not enough memory for first task\n"));
             }
+
+            /* Initialise the ETask data. */
+            InitETask(t, t->tc_UnionETask.tc_ETask);
 
             GetIntETask(t)->iet_Context = AllocMem(SIZEOF_ALL_REGISTERS
                 , MEMF_PUBLIC|MEMF_CLEAR
