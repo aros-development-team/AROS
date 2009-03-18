6df214d1a28821726ee195a537b4eec4bd0912f2
Kalamatee <kalamatee@gmail.com>
2009-03-18 23:58:04 +0000

    Bump version
    git-svn-id: https://svn.aros.org/svn/aros/trunk@30975 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/system/Wanderer/version.h
+++ workbench/system/Wanderer/version.h
@@ -1,6 +1,6 @@
 #define WANDERERVERS    0
-#define WANDERERREV     80
-#define WANDERERSTR     "Wanderer 0.80 (13.03.2009)"
+#define WANDERERREV     81
+#define WANDERERSTR     "Wanderer 0.81 (18.03.2009)"
 #define WANDERERAUTH    "The AROS Dev Team"
 #define WANDERERCOPY    "© " WANDERERAUTH
 #define VERSION "$VER:  " WANDERERSTR " " WANDERERCOPY
