eb5d11fe061677b50a9a11c6bbde36a0a7f04df0
Kalamatee <kalamatee@gmail.com>
2009-03-20 17:26:46 +0000

    Add placeholders in about requestor to display used class versions
    git-svn-id: https://svn.aros.org/svn/aros/trunk@30985 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/system/Wanderer/version.h
+++ workbench/system/Wanderer/version.h
@@ -1,6 +1,6 @@
 #define WANDERERVERS    0
-#define WANDERERREV     81
-#define WANDERERSTR     "Wanderer 0.81 (18.03.2009)"
+#define WANDERERREV     82
+#define WANDERERSTR     "Wanderer 0.82 (20.03.2009)"
 #define WANDERERAUTH    "The AROS Dev Team"
 #define WANDERERCOPY    "© " WANDERERAUTH
 #define VERSION "$VER:  " WANDERERSTR " " WANDERERCOPY
