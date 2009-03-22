ea51e8889625c5d55d5026ff0b644446c6a61339
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-03-22 20:21:59 +0000

    Fix indentation.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@30991 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-pc/drivers/keyboard/kbdclass.c
+++ arch/i386-pc/drivers/keyboard/kbdclass.c
@@ -342,204 +342,207 @@ void kbd_process_key(struct kbd_data *data, UBYTE keycode)
     UWORD   	    event;          /* Event sent to handleevent method */
     WORD    	    amigacode;
 
-    	if ((keycode == KBD_REPLY_ACK) || (keycode == KBD_REPLY_RESEND))
-	{
-	    /* Ignore these */
-	    return;
-	}
-	
-	if ((keycode == 0xE0) || (keycode == 0xE1))
-	{
-	    /* Extended keycodes: E0 gets followed by one code, E1 by two */
-	    data->prev_keycode = keycode;
-	    return;
-	}
-	
-	if ((keycode == 0x00) || (keycode == 0xFF))
-	{
-	    /* 00 is error. FF is sent by some keyboards -> ignore it. */
-	    data->prev_keycode = 0;
-	    return;
-	}
+    if ((keycode == KBD_REPLY_ACK) || (keycode == KBD_REPLY_RESEND))
+    {
+        /* Ignore these */
+        return;
+    }
 
-	amigacode = NOKEY;
-    	event = 0;
-    	downkeycode = keycode & 0x7F;
-	releaseflag = keycode & 0x80;
+    if ((keycode == 0xE0) || (keycode == 0xE1))
+    {
+        /* Extended keycodes: E0 gets followed by one code, E1 by two */
+        data->prev_keycode = keycode;
+        return;
+    }
 
-	if (data->prev_keycode)
-	{
-	    if (data->prev_keycode == 0xE0)
-	    {
-	    	data->prev_keycode = 0;
-    	    	event = 0x4000 | keycode;	    
+    if ((keycode == 0x00) || (keycode == 0xFF))
+    {
+        /* 00 is error. FF is sent by some keyboards -> ignore it. */
+        data->prev_keycode = 0;
+        return;
+    }
 
-		if (downkeycode < NUM_E0KEYS)
-		{
-		    amigacode = e0_keytable[downkeycode];
-		    if (amigacode != NOKEY) amigacode |= releaseflag;
-		}
-	    } /* if (data->prev_keycode == 0xE0) */
-	    else
-	    {
-	    	/* Check Pause key: 0xE1 0x1D 0x45   0xE1 0x9D 0xC5 */
-	    	if ((data->prev_keycode == 0xE1) && (downkeycode == 0x1D))
-		{
-		    /* let's remember, that we still need third key */
-		    data->prev_keycode = 0x1234;
-		    return;
-		}
-		else if ((data->prev_keycode == 0x1234) && (downkeycode == 0x45))
-		{
-		    /* Got third key and yes, it is Pause */
-		    amigacode = 0x6E | releaseflag;
-		    data->prev_keycode = 0;
-		}
-		else
-		{
-		    /* Unknown */
-		    data->prev_keycode = 0;
-		    return;
-		}
-		
-	    } /* if (data->prev_keycode == 0xE0) else ... */
-	    
-	} /* if (data->prev_keycode) */
-	else
-	{
-	    /* Normal single byte keycode */
-	    event = keycode;
-	    if (downkeycode < NUM_STDKEYS)
-	    {
-		amigacode = std_keytable[downkeycode];
-		if (amigacode != NOKEY) amigacode |= releaseflag;
-	    }	    
-	}
+    amigacode = NOKEY;
+    event = 0;
+    downkeycode = keycode & 0x7F;
+    releaseflag = keycode & 0x80;
 
-        switch(event)
+    if (data->prev_keycode)
+    {
+        if (data->prev_keycode == 0xE0)
         {
-            case K_KP_Numl:
-                kbd_keystate ^= 0x02;	/* Turn Numlock bit on */
-                kbd_updateleds(kbd_keystate);
-                break;
- 
-            case K_Scroll_Lock:
-                kbd_keystate ^= 0x01;	/* Turn Scrolllock bit on */
-                kbd_updateleds(kbd_keystate);
-                break;
+            data->prev_keycode = 0;
+            event = 0x4000 | keycode;	    
 
-            case K_CapsLock:
-                kbd_keystate ^= 0x04;	/* Turn Capslock bit on */
-                kbd_updateleds(kbd_keystate);
-                break;
+            if (downkeycode < NUM_E0KEYS)
+            {
+                amigacode = e0_keytable[downkeycode];
+                if (amigacode != NOKEY) amigacode |= releaseflag;
+            }
+        } /* if (data->prev_keycode == 0xE0) */
+        else
+        {
+            /* Check Pause key: 0xE1 0x1D 0x45   0xE1 0x9D 0xC5 */
+            if ((data->prev_keycode == 0xE1) && (downkeycode == 0x1D))
+            {
+                /* let's remember, that we still need third key */
+                data->prev_keycode = 0x1234;
+                return;
+            }
+            else if ((data->prev_keycode == 0x1234) && (downkeycode == 0x45))
+            {
+                /* Got third key and yes, it is Pause */
+                amigacode = 0x6E | releaseflag;
+                data->prev_keycode = 0;
+            }
+            else
+            {
+                /* Unknown */
+                data->prev_keycode = 0;
+                return;
+            }
 
-            case K_LShift:
-                kbd_keystate |= FLAG_LSHIFT;
-                break;
+        } /* if (data->prev_keycode == 0xE0) else ... */
 
-            case (K_LShift | 0x80):
-                kbd_keystate &= ~FLAG_LSHIFT;
-                break;
+    } /* if (data->prev_keycode) */
+    else
+    {
+        /* Normal single byte keycode */
+        event = keycode;
+        if (downkeycode < NUM_STDKEYS)
+        {
+            amigacode = std_keytable[downkeycode];
+            if (amigacode != NOKEY) amigacode |= releaseflag;
+        }	    
+    }
 
-            case K_RShift:
-                kbd_keystate |= FLAG_RSHIFT;
-                break;
+    switch(event)
+    {
+        case K_KP_Numl:
+            kbd_keystate ^= 0x02;    /* Turn Numlock bit on */
+            kbd_updateleds(kbd_keystate);
+            break;
 
-            case (K_RShift | 0x80):
-                kbd_keystate &= ~FLAG_RSHIFT;
-                break;
+        case K_Scroll_Lock:
+            kbd_keystate ^= 0x01;    /* Turn Scrolllock bit on */
+            kbd_updateleds(kbd_keystate);
+            break;
 
-            case K_LCtrl:
-                kbd_keystate |= FLAG_LCTRL;
-                break;
+        case K_CapsLock:
+            kbd_keystate ^= 0x04;    /* Turn Capslock bit on */
+            kbd_updateleds(kbd_keystate);
+            break;
 
-            case (K_LCtrl | 0x80):
-                kbd_keystate &= ~FLAG_LCTRL;
-                break;
+        case K_LShift:
+            kbd_keystate |= FLAG_LSHIFT;
+            break;
 
-            case K_RCtrl:
-                kbd_keystate |= FLAG_RCTRL;
-                break;
+        case (K_LShift | 0x80):
+            kbd_keystate &= ~FLAG_LSHIFT;
+            break;
 
-            case (K_RCtrl | 0x80):
-                kbd_keystate &= ~FLAG_RCTRL;
-                break;
+        case K_RShift:
+            kbd_keystate |= FLAG_RSHIFT;
+            break;
 
-            case K_LMeta:
-                kbd_keystate |= FLAG_LMETA;
-                break;
+        case (K_RShift | 0x80):
+            kbd_keystate &= ~FLAG_RSHIFT;
+            break;
 
-            case (K_LMeta | 0x80):
-                kbd_keystate &= ~FLAG_LMETA;
-                break;
- 
-            case K_RMeta:
-                kbd_keystate |= FLAG_RMETA;
-                break;
+        case K_LCtrl:
+            kbd_keystate |= FLAG_LCTRL;
+            break;
 
-            case (K_RMeta | 0x80):
-                kbd_keystate &= ~FLAG_RMETA;
-                break;
+        case (K_LCtrl | 0x80):
+            kbd_keystate &= ~FLAG_LCTRL;
+            break;
 
-            case K_LAlt:
-                kbd_keystate |= FLAG_LALT;
-                break;
+        case K_RCtrl:
+            kbd_keystate |= FLAG_RCTRL;
+            break;
 
-            case (K_LAlt | 0x80):
-                kbd_keystate &= ~FLAG_LALT;
-                break;
+        case (K_RCtrl | 0x80):
+            kbd_keystate &= ~FLAG_RCTRL;
+            break;
 
-            case K_RAlt:
-                kbd_keystate |= FLAG_RALT;
-                break;
-		
-            case (K_RAlt | 0x80):
-                kbd_keystate &= ~FLAG_RALT;
-                break;
+        case K_LMeta:
+            kbd_keystate |= FLAG_LMETA;
+            break;
 
-    	    case K_Del:
-	    	kbd_keystate |= FLAG_DEL;
-		break;
-		
-	    case (K_Del | 0x80):
-	    	kbd_keystate &= ~FLAG_DEL;
-		break;
-		
-        } /* switch(event) */
+        case (K_LMeta | 0x80):
+            kbd_keystate &= ~FLAG_LMETA;
+            break;
 
-        if ( ((kbd_keystate & (FLAG_LCTRL|FLAG_LMETA|FLAG_RMETA)) == (FLAG_LCTRL|FLAG_LMETA|FLAG_RMETA)) ||
-	     ((kbd_keystate & (FLAG_LCTRL|FLAG_LALT|FLAG_DEL)) == (FLAG_LCTRL|FLAG_LALT|FLAG_DEL)) )
-	{
-	    amigacode = 0x78; /* Reset */
-	}
+        case K_RMeta:
+            kbd_keystate |= FLAG_RMETA;
+            break;
 
-    	D(bug("ki: amigacode %d (%x) last %d (%x)\n", amigacode, amigacode, data->prev_amigacode, data->prev_amigacode));
+        case (K_RMeta | 0x80):
+            kbd_keystate &= ~FLAG_RMETA;
+            break;
 
-        /* Update keystate */
-        data->kbd_keystate = kbd_keystate;
+        case K_LAlt:
+            kbd_keystate |= FLAG_LALT;
+            break;
+
+        case (K_LAlt | 0x80):
+            kbd_keystate &= ~FLAG_LALT;
+            break;
+
+        case K_RAlt:
+            kbd_keystate |= FLAG_RALT;
+            break;
+
+        case (K_RAlt | 0x80):
+            kbd_keystate &= ~FLAG_RALT;
+            break;
+
+        case K_Del:
+            kbd_keystate |= FLAG_DEL;
+            break;
+
+        case (K_Del | 0x80):
+            kbd_keystate &= ~FLAG_DEL;
+            break;
+
+    } /* switch(event) */
+
+    if (((kbd_keystate & (FLAG_LCTRL|FLAG_LMETA|FLAG_RMETA))
+        == (FLAG_LCTRL|FLAG_LMETA|FLAG_RMETA)) ||
+        ((kbd_keystate & (FLAG_LCTRL|FLAG_LALT|FLAG_DEL))
+        == (FLAG_LCTRL|FLAG_LALT|FLAG_DEL)) )
+    {
+        amigacode = 0x78; /* Reset */
+    }
+
+    D(bug("ki: amigacode %d (%x) last %d (%x)\n", amigacode, amigacode,
+        data->prev_amigacode, data->prev_amigacode));
+
+    /* Update keystate */
+    data->kbd_keystate = kbd_keystate;
 
 #if 0
-        if (amigacode == 0x78)    // Reset request
-            ColdReboot();
+    if (amigacode == 0x78)    // Reset request
+        ColdReboot();
 #endif
 
-    	if (amigacode == NOKEY) return;
+    if (amigacode == NOKEY) return;
 
-    	if (amigacode == data->prev_amigacode)
-	{
-	    /*
-	    ** Must be a repeated key. Ignore it, because we have our
-	    ** own kbd repeating in input.device
-	    */	    
-	    return;
-	}
+    if (amigacode == data->prev_amigacode)
+    {
+        /*
+        ** Must be a repeated key. Ignore it, because we have our
+        ** own kbd repeating in input.device
+        */	    
+        return;
+    }
 
-	data->prev_amigacode = amigacode;
-	
-	D(bug("ki: ********************* c %d (%x)\n", amigacode, amigacode));
+    data->prev_amigacode = amigacode;
 
-        /* Pass the code to handler */
-        data->kbd_callback(data->callbackdata, amigacode);
+    D(bug("ki: ********************* c %d (%x)\n", amigacode, amigacode));
+
+    /* Pass the code to handler */
+    data->kbd_callback(data->callbackdata, amigacode);
 
     return;
 }
