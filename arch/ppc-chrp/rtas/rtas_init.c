a011f14bbee00a972acf2d0a66e2633ba2f5ec15
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-03-24 21:53:57 +0000

    rtas.resource for CHRP PPC systems. Initial but usable commit.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@31004 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/ppc-chrp/rtas/rtas_init.c
@@ -0,0 +1,144 @@
+/*
+ * rtas_init.c
+ *
+ *  Created on: Mar 22, 2009
+ *      Author: misc
+ */
+
+/*
+ * bestcomm_init.c
+ *
+ *  Created on: Mar 15, 2009
+ *      Author: misc
+ */
+
+#define DEBUG 1
+
+#include <asm/mpc5200b.h>
+#include <aros/debug.h>
+#include <aros/symbolsets.h>
+#include <aros/libcall.h>
+#include <proto/openfirmware.h>
+#include <proto/kernel.h>
+#include <proto/exec.h>
+#include <proto/rtas.h>
+
+#include "rtas_private.h"
+
+static int rtas_init(struct RTASBase *RTASBase)
+{
+	int retval = TRUE;
+	void *OpenFirmwareBase;
+	void *key, *prop;
+
+	D(bug("[RTAS] RTAS Init.\n"));
+
+	OpenFirmwareBase = RTASBase->OpenFirmwareBase = OpenResource("openfirmware.resource");
+	RTASBase->KernelBase = OpenResource("kernel.resource");
+
+	/* Get some addresses from OF tree */
+	if (OpenFirmwareBase)
+	{
+		key = OF_OpenKey("/rtas");
+		if (key)
+		{
+			prop = OF_FindProperty(key, "load_base");
+			if (prop)
+			{
+				void **reg = OF_GetPropValue(prop);
+				D(bug("[RTAS] RTAS loaded at %08x\n", *reg));
+				RTASBase->rtas_base = *reg;
+			}
+
+			prop = OF_FindProperty(key, "entry");
+			if (prop)
+			{
+				void **reg = OF_GetPropValue(prop);
+				D(bug("[RTAS] Entry at %08x\n", *reg));
+				RTASBase->rtas_entry = *reg;
+			}
+		}
+	}
+	else
+		retval = FALSE;
+
+
+	return retval;
+}
+
+extern void return_entry();
+extern void return_2();
+
+AROS_LH5(int32_t, RTASCall,
+		AROS_LHA(const char *,	name,	A0),
+		AROS_LHA(uint32_t,		nargs,	D0),
+		AROS_LHA(uint32_t,		nrets,	D1),
+		AROS_LHA(uint32_t *,	output, A1),
+		AROS_LHA(va_list,		args,	A2),
+		struct RTASBase *, RTASBase, 0, Rtas)
+{
+	AROS_LIBFUNC_INIT
+
+	void *OpenFirmwareBase = RTASBase->OpenFirmwareBase;
+	void *KernelBase = RTASBase->KernelBase;
+
+	int32_t token = 0;
+	int32_t retval = -1;
+
+	D(bug("[RTAS] Calling method '%s'\n", name));
+
+	void *key = OF_OpenKey("/rtas");
+	if (key)
+	{
+		void *prop = OF_FindProperty(key, name);
+
+		if (prop)
+		{
+			token = *(int32_t *)OF_GetPropValue(prop);
+		}
+	}
+
+	if (token)
+	{
+		D(bug("[RTAS] Method '%s', token %d\n", name, token));
+		int i;
+
+		RTASBase->rtas_args.token = token;
+		RTASBase->rtas_args.nargs = nargs;
+		RTASBase->rtas_args.nret = nrets;
+		RTASBase->rtas_args.rets = KrnVirtualToPhysical(&RTASBase->rtas_args.args[nargs]);
+
+		for (i=0; i < nargs; i++)
+		{
+			RTASBase->rtas_args.args[i] = va_arg(args, uint32_t);
+		}
+
+		Disable();
+
+		{
+			register intptr_t args asm ("r4") = KrnVirtualToPhysical(&RTASBase->rtas_args);
+			register intptr_t base asm ("r5") = RTASBase->rtas_base;
+			register intptr_t entry asm ("r6") = RTASBase->rtas_entry;
+
+			asm volatile("li %%r3,%0; sc"::"i"(9 /*SC_RTAS*/),"r"(args),"r"(base),"r"(entry):"memory");
+		}
+
+		if (nrets)
+		{
+			for (i=0; i < nrets; i++)
+				output[i] = RTASBase->rtas_args.rets[i+1];
+		}
+
+		retval = RTASBase->rtas_args.rets[0];
+
+		Enable();
+	}
+	else
+		retval = -1;
+
+	return retval;
+
+	AROS_LIBFUNC_EXIT
+}
+
+ADD2INITLIB(rtas_init, 0)
