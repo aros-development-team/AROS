176b91af44175ebdb43bdf8464ceb345b5e4e726
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-03-24 21:53:57 +0000

    rtas.resource for CHRP PPC systems. Initial but usable commit.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@31004 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/ppc-chrp/rtas/rtas_private.h
@@ -0,0 +1,31 @@
+/*
+ * rtas_private.h
+ *
+ *  Created on: Mar 22, 2009
+ *      Author: misc
+ */
+
+#ifndef RTAS_PRIVATE_H_
+#define RTAS_PRIVATE_H_
+
+#include <exec/nodes.h>
+#include <inttypes.h>
+
+typedef struct {
+	uint32_t	token;
+	uint32_t	nargs;
+	uint32_t	nret;
+	uint32_t	args[16];
+	uint32_t	*rets;
+} rtas_args_t;
+
+struct RTASBase {
+	struct Node	rtas_Node;
+	void 		*rtas_base;
+	int 		(*rtas_entry)(rtas_args_t *args, void *base);
+	rtas_args_t	rtas_args;
+	void		*KernelBase;
+	void		*OpenFirmwareBase;
+};
+
+#endif /* RTAS_PRIVATE_H_ */
