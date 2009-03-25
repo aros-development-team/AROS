7f117c9b8303491f8e4a8c7a4f2c924dd78412ed
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-03-25 08:02:37 +0000

    - Attempt to fix the build, added missind metadependencies, really sorry. - autoinit code uses ADD2LIBS because it's not portable anyway, stabs.h is private in libnix - Removed obsolete mmakefile.src in include/libraries
    git-svn-id: https://svn.aros.org/svn/aros/trunk@31012 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/codesetslib/libcodesets/autoinit.c
+++ workbench/libs/codesetslib/libcodesets/autoinit.c
@@ -1,25 +1,4 @@
-#include <exec/libraries.h>
-#include <libraries/codesets.h>
 #include <proto/codesets.h>
-#include <proto/exec.h>
-#ifdef __AROS__
 #include <aros/symbolsets.h>
-#else
-#include <stabs.h>
-#endif
 
-struct Library *CodesetsBase;
-
-static int OpenCodesets(void)
-{
-    CodesetsBase = OpenLibrary("codesets.library", CODESETSVER);
-    return CodesetsBase ? 1 : 0;
-}
-
-static void CloseCodesets(void)
-{
-    CloseLibrary(CodesetsBase);
-}
-
-ADD2INIT(OpenCodesets, 127);
-ADD2EXIT(CloseCodesets, -127);
+ADD2LIBS("codesets.library", CODESETSVER, struct Library *, CodesetsBase);
