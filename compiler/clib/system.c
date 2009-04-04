72c148063dea302392ab0bbd49e58896d5007af6
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-04-04 15:01:40 +0000

    r10845@lvps87-230-33-50:  verhaegs | 2009-04-04 16:36:55 +0200  Cleaned up code for system() function in arosc library. It uses now SystemTags  when no sh is present in the system.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@31068 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/system.c
+++ compiler/clib/system.c
@@ -8,33 +8,20 @@
 #include "__arosc_privdata.h"
 
 #include <dos/dos.h>
-#include <dos/filesystem.h>
+//#include <dos/filesystem.h>
 #include <proto/dos.h>
-#include <utility/tagitem.h>
+//#include <utility/tagitem.h>
 #include <unistd.h>
-#include <sys/types.h>
+//#include <sys/types.h>
 #include <sys/wait.h>
 
 #include "__errno.h"
 #include "__open.h"
 #include "__upath.h"
-#include <process.h>
 
 #define DEBUG 0
 #include <aros/debug.h>
 
-typedef struct
-{
-    BPTR command;
-    LONG returncode;
-    struct arosc_privdata *ppriv;
-} childdata_t;
-
-static AROS_UFP3(LONG, wait_entry,
-    AROS_UFPA(char *, argstr,A0),
-    AROS_UFPA(ULONG, argsize,D0),
-    AROS_UFPA(struct ExecBase *,SysBase,A6)
-);
 static int system_sh(const char *string);
 static int system_no_sh(const char *string);
 
@@ -94,95 +81,6 @@ static int system_no_sh(const char *string);
 } /* system */
 
 
-static AROS_UFH3(LONG, wait_entry,
-    AROS_UFHA(char *, argstr,A0),
-    AROS_UFHA(ULONG, argsize,D0),
-    AROS_UFHA(struct ExecBase *,SysBase,A6)
-)
-{
-    AROS_USERFUNC_INIT
-
-    struct DosLibrary *DOSBase;
-    struct Library *aroscbase;
-    LONG rc = -1;
-    childdata_t *childdata = (childdata_t *)FindTask(NULL)->tc_UserData;
-    struct CommandLineInterface *cli;
-    LONG stacksize;
-    fdesc *in, *out, *err;
-    fdesc *newin, *newout, *newerr;
-
-    DOSBase = (struct DosLibrary *)OpenLibrary(DOSNAME, 39);
-    if (DOSBase == NULL)
-        goto err1;
-
-    aroscbase = OpenLibrary("arosc.library", 0);
-    if (aroscbase == NULL)
-        goto err2;
-
-    newin = __alloc_fdesc();
-    newout = __alloc_fdesc();
-    newerr = __alloc_fdesc();
-    if(!newin || !newout || !newerr)
-    {
-	goto err2;
-    }
-#define privdata __get_arosc_privdata()
-    D(bug("privdata: %p, ppriv: %p\n", privdata, childdata->ppriv));
-    privdata->acpd_parent_does_upath = childdata->ppriv->acpd_doupath;
-    __get_arosc_privdata()->acpd_flags |= KEEP_OLD_ACPD | DO_NOT_CLONE_ENV_VARS;
-
-    cli = Cli();
-    if (cli)
-	stacksize = cli->cli_DefaultStack * CLI_DEFAULTSTACK_UNIT;
-    else
-	stacksize = AROS_STACKSIZE;
-
-    if(__fd_array[STDIN_FILENO])
-	close(STDIN_FILENO);
-    if(__fd_array[STDOUT_FILENO])
-	close(STDOUT_FILENO);
-    if(__fd_array[STDERR_FILENO])
-	close(STDERR_FILENO);
-	
-    in = childdata->ppriv->acpd_fd_array[STDIN_FILENO];
-    out = childdata->ppriv->acpd_fd_array[STDOUT_FILENO];
-    err = childdata->ppriv->acpd_fd_array[STDERR_FILENO];
-
-    if(in) 
-	SelectInput(in->fcb->fh);
-    if(out) 
-	SelectOutput(out->fcb->fh);
-    if(err)
-	SelectError(err->fcb->fh);
-	
-    in->fcb->opencount++;
-    out->fcb->opencount++;
-    err->fcb->opencount++;
-    newin->fdflags = 0;
-    newout->fdflags = 0;
-    newerr->fdflags = 0;
-    newin->fcb  = in->fcb;
-    newout->fcb = out->fcb;
-    newerr->fcb = err->fcb;
-    __fd_array[STDIN_FILENO] = newin;
-    __fd_array[STDOUT_FILENO] = newout;
-    __fd_array[STDERR_FILENO] = newerr;
-	
-    rc = RunCommand(childdata->command, stacksize, argstr, argsize);
-
-    CloseLibrary(aroscbase);
-
-err2:
-    CloseLibrary((struct Library *)DOSBase);
-
-err1:
-    childdata->returncode = rc;
-
-    return rc;
-    
-    AROS_USERFUNC_EXIT
-}
-
 static int system_sh(const char *string)
 {
     pid_t pid = vfork();
@@ -209,108 +107,48 @@ static int system_sh(const char *string)
 	
 static int system_no_sh(const char *string)
 {
-    CONST_STRPTR apath;
-    char *args, *cmd;
-    BPTR seg;
+    struct arosc_privdata *pdata = __get_arosc_privdata();
+    const char *apath;
+    char *args, *cmd, *fullcmd;
+    fdesc *in, *out, *err;
     int ret;
 
     D(bug("system_no_sh(%s)\n", string));
 
-    cmd = strdup(string);
-    args = cmd;
+    args = strdup(string);
+    cmd = strsep(&args, " \t\n");
+    if (!args)
+        args = "";
 
-    while (++args)
-    {
-	switch (args[0])
-	{
-	case ' ':
-	case '\t':
-	case '\n':
-	    args[0] = '\0';
-	    break;
-	case '\0':
-	    args = NULL;
-	    break;
-	}
+    D(bug("system(cmd=%s, args=%s)\n", cmd, args));
 
-	if (!args)
-	    break;
+    apath = __path_u2a(cmd);
+    
+    fullcmd = malloc(strlen(apath) + strlen(args) + 2);
+    strcpy(fullcmd, apath);
+    strcat(fullcmd, " ");
+    strcat(fullcmd, args);
 
-	if (args[0] == '\0')
-	{
-	    ++args;
-	    break;
-	}
-    }
-
-    D(bug("system(cmd=%s, args=%s)\n", cmd, args ? args : ""));
-
-    apath = (STRPTR) __path_u2a(cmd);
-    seg = LoadSeg(apath);
-    if (seg == MKBADDR(NULL))
-    {
-	struct CommandLineInterface *cli = Cli();
-	BPTR oldCurDir = CurrentDir(NULL);
-	BPTR *paths = cli ? BADDR(cli->cli_CommandDir) : NULL;
-
-	for (; seg == MKBADDR(NULL) && paths; paths = BADDR(paths[0]))
-	{
-	    CurrentDir(paths[1]); 
-	    seg = LoadSeg(apath);
-	}
-
-	if (seg == MKBADDR(NULL))
-	{
-	    errno = IoErr2errno(IoErr());
-	    D(bug("system(cmd=%s, args=%s)=-1, errno=%d\n",
-		  cmd, args ? args : "", errno));
-	    CurrentDir(oldCurDir);
-	    free(cmd);
-	    return -1;
-	}
-	else
-	    errno = 0;
-
-	CurrentDir(oldCurDir);
-    }
-    else
-	errno = 0;
-
-    {
-	childdata_t childdata;
-
-	struct TagItem tags[] =
-	{
-	    { NP_Entry,       (IPTR)wait_entry },
-	    { NP_Arguments,   (IPTR)args       },
-	    { NP_CloseInput,  FALSE            },
-	    { NP_CloseOutput, FALSE            },
-	    { NP_CloseError,  FALSE            },
-	    { NP_FreeSeglist, FALSE            },
-	    { NP_Cli,         TRUE             },
-	    { NP_Synchronous, TRUE             },
-	    { NP_Name,        (IPTR)cmd        },
-	    { NP_UserData,    (IPTR)&childdata },
-	    { TAG_DONE,       0                }
-	};
-
-	childdata.command = seg;
-	childdata.ppriv = __get_arosc_privdata();
-
-	if (CreateNewProc(tags) != NULL)
-	    ret = childdata.returncode;
-	else
-	{
-	    ret = -1;
-	    errno = IoErr2errno(IoErr());
-	}
-
-	UnLoadSeg(seg);
-    }
-
-    D(bug("system(cmd=%s, args=%s)=%d, errno=%d\n",
-	  cmd, args ? args : "", ret, errno));
     free(cmd);
+    
+    in = pdata->acpd_fd_array[STDIN_FILENO];
+    out = pdata->acpd_fd_array[STDOUT_FILENO];
+    err = pdata->acpd_fd_array[STDERR_FILENO];
+    
+    ret = (int)SystemTags
+    (
+         fullcmd,
+         SYS_Input, (IPTR)(in ? in->fcb->fh : NULL),
+         SYS_Output, (IPTR)(out ? out->fcb->fh : NULL),
+         SYS_Error, (IPTR)(err ? err->fcb->fh : NULL),
+         NULL
+    );
+
+    free(fullcmd);
+    
+    if (ret == -1)
+        errno = IoErr2errno(IoErr());
+
     return ret;
 } /* system */
 
