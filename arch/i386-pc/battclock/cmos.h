e8437bcabef7de15269646ce90ec0f9b77df563b
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-04-28 20:40:06 +0000

    Implemented WriteBattClock() and added check to ReadBattClock() to wait until clock isn't being updated.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@31156 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/i386-pc/battclock/cmos.h
@@ -0,0 +1,23 @@
+/*
+    Copyright © 1995-2009, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: CMOS offsets.
+    Lang: English
+*/
+
+#ifndef CMOS_H
+#define CMOS_H
+
+#define CENTURY		0x32
+#define YEAR		0x09
+#define MONTH		0x08
+#define MDAY		0x07
+#define HOUR		0x04
+#define MIN		0x02
+#define SEC		0x00
+#define STATUS_A	0x0A
+#define STATUS_B	0x0B
+#define HEALTH		0x0E
+
+#endif /* CMOS_H */
