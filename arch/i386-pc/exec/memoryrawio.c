be70b921d8eab52e833b0890577a425988c98a40
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-05-29 16:28:42 +0000

    - Added ability to store raw debug output in a dynamically-expanding RAM    buffer for later retrieval.  - Added a boot option to choose the destination of debug output: debug=serial,    debug=serial2 (2nd port) or debug=memory.  - Use version information from .conf file.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@31321 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/i386-pc/exec/memoryrawio.c
@@ -0,0 +1,271 @@
+/*
+    Copyright © 2009, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: functions for memory-stored RawIOInit/RawPutChar
+    Lang: English
+*/
+
+#include <proto/exec.h>
+
+#include <string.h>
+
+
+#define BLOCK_SIZE (256 * 1024)
+#define TASK_PRIORITY 0
+#define STACK_SIZE 4096
+
+
+static const TEXT name[] = "bifteck";
+static const TEXT data_missed_msg[] = "<*** GAP IN DATA ***>\n";
+
+struct LogBlock
+{
+    struct MinNode node;
+    ULONG length;            /* number of data bytes that follow */
+};
+
+struct LogData
+{
+    struct SignalSemaphore lock;
+    struct MinList buffers;
+    struct LogBlock *block;
+    ULONG block_pos;
+    APTR pool;
+};
+
+void Putc(char);
+static void LogTask();
+
+static struct LogData *data;
+static struct Task *task;
+static BOOL data_missed = FALSE;
+
+
+/*****i***********************************************************************
+
+    NAME */
+	AROS_LH0(void, MemoryRawIOInit,
+
+/*  LOCATION */
+	struct ExecBase *, SysBase, 84, Exec)
+
+/*  FUNCTION
+	This is a private function. It initializes raw IO. After you
+	have called this function, you can use (!RawMayGetChar()) and
+	RawPutChar().
+
+    INPUTS
+	None.
+
+    RESULT
+	None.
+
+    NOTES
+	This function is for very low level debugging only.
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	RawPutChar(), RawMayGetChar()
+
+    INTERNALS
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    BOOL success = TRUE;
+    APTR stack;
+
+    /* Initialise data struct, memory pool and semaphore */
+
+    data = AllocMem(sizeof(struct LogData), MEMF_PUBLIC | MEMF_CLEAR);
+    if (data == NULL)
+        success = FALSE;
+
+    if (success)
+    {
+        data->pool = CreatePool(MEMF_PUBLIC, BLOCK_SIZE, BLOCK_SIZE);
+        NEWLIST(&data->buffers);
+        InitSemaphore(&data->lock);
+        data->lock.ss_Link.ln_Name = (char *)name;
+        if (data->pool == NULL)
+            success = FALSE;
+    }
+
+
+    if (success)
+    {
+       /* Allocate initial buffer */
+
+        data->block = (struct LogBlock *)AllocPooled(data->pool, BLOCK_SIZE);
+        data->block->length = BLOCK_SIZE - sizeof(struct LogBlock);
+        if (data->block == NULL)
+            success = FALSE;
+    }
+
+    if (success)
+    {
+       /* Add semaphore/data to public list along with initial buffer */
+
+        AddTail((struct List *)&data->buffers, (struct Node *)data->block);
+        AddSemaphore(&data->lock);
+    }
+
+    if (success)
+    {
+        /* Create a task that will allocate buffers as needed */
+
+        task =
+            AllocMem(sizeof(struct Task), MEMF_PUBLIC | MEMF_CLEAR);
+        stack = AllocMem(STACK_SIZE, MEMF_PUBLIC);
+        if (task == NULL || stack == NULL)
+            success = FALSE;
+    }
+
+    if (success)
+    {
+        /* Initialise and start task */
+
+        task->tc_Node.ln_Type = NT_TASK;
+        task->tc_Node.ln_Pri = TASK_PRIORITY;
+        task->tc_Node.ln_Name = (char *)name;
+        task->tc_SPUpper = stack + STACK_SIZE;
+        task->tc_SPLower = stack;
+        task->tc_SPReg = stack + STACK_SIZE;
+        NEWLIST(&task->tc_MemEntry);
+        task->tc_UserData = data;
+
+        if (AddTask(task, LogTask, NULL) == NULL)
+            success = FALSE;
+    }
+
+    if (success)
+    {
+        /* Signal task to allocate the second buffer */
+
+        Signal(task, SIGF_SINGLE);
+    }
+
+    if (!success)
+    {
+    }
+
+    AROS_LIBFUNC_EXIT
+}
+
+
+/*****i***********************************************************************
+
+    NAME */
+	AROS_LH1(void, MemoryRawPutChar,
+
+/*  SYNOPSIS */
+	AROS_LHA(UBYTE, chr, D0),
+
+/*  LOCATION */
+	struct ExecBase *, SysBase, 86, Exec)
+
+/*  FUNCTION
+	Emits a single character.
+
+    INPUTS
+	chr - The character to emit
+
+    RESULT
+	None.
+
+    NOTES
+	This function is for very low level debugging only.
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	RawIOInit(), RawPutChar(), RawMayGetChar()
+
+    INTERNALS
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    struct LogBlock *new_block;
+
+    /* Write to screen as well */
+
+    Putc(chr);
+
+    /* Check if we need to move on to a new block */
+
+    if (data->block_pos == data->block->length)
+    {
+        if (AttemptSemaphore(&data->lock))
+        {
+            /* Move on to the next block if it exists and no one else is
+             * accessing the list */
+
+            new_block = (struct LogBlock *)data->block->node.mln_Succ;
+            if (new_block->node.mln_Succ != NULL)
+            {
+                data->block = new_block;
+                data->block_pos = 0;
+
+                /* If data was missed, add a warning to the log */
+
+                if (data_missed)
+                {
+                    CopyMem(data_missed_msg,
+                        (UBYTE *)data->block + sizeof(struct LogBlock),
+                        strlen(data_missed_msg));
+                    data->block_pos += strlen(data_missed_msg);
+                    data_missed = FALSE;
+                }
+            }
+            ReleaseSemaphore(&data->lock);
+
+            /* Ask for another new block, which will hopefully be allocated
+             * by the time we need it */
+
+            Signal(task, SIGF_SINGLE);
+        }
+    }
+
+    /* Store character in buffer if there's space */
+
+    if (data->block_pos != data->block->length)
+    {
+        if (chr != '\03')
+            ((UBYTE *)data->block)[sizeof(struct LogBlock) + data->block_pos++] =
+                chr;
+    }
+    else
+        data_missed = TRUE;
+
+    AROS_LIBFUNC_EXIT
+}
+
+
+static void LogTask()
+{
+    struct LogBlock *block;
+
+    /* Add a new buffer block to the list each time we're signalled */
+
+    while (Wait(SIGF_SINGLE))
+    {
+        block = (struct LogBlock *)AllocPooled(data->pool, BLOCK_SIZE);
+        if (block != NULL)
+        {
+            block->length = BLOCK_SIZE - sizeof(struct LogBlock);
+            ObtainSemaphore(&data->lock);
+            AddTail((struct List *)&data->buffers, (struct Node *)block);
+            ReleaseSemaphore(&data->lock);
+        }
+    }
+}
+
