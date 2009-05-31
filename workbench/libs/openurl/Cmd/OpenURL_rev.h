9c9dce35c02d017883afeb6962a0dee23cf3f74a
mattrust <mrustler@gmx.de>
2009-05-31 09:33:43 +0000

    Sigh. Manual synching of 2 svn repos. sucks.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@31329 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/openurl/Cmd/OpenURL_rev.h
+++
@@ -1,9 +0,0 @@
-#define PRG         "OpenURL"
-#define VERSION     7
-#define REVISION    2
-#define VRSTRING    "7.2"
-#define DATE        "09.02.2006"
-#define VERS        "OpenURL "VRSTRING
-#define PRGNAME     VERS" ("DATE")"
-#define VSTRING     PRGNAME"\r\n"
-#define VERSTAG     "\0$VER: "PRGNAME
