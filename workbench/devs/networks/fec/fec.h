b95fe8cbffa8396cde37c5600a7fb0f11be4e0e5
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-06-02 21:39:11 +0000

    Not-so-large-code-update. Next step will be full device layer and transmit/receive rings.
    1. Some low-level functions added. PHY reset/link status/speed/duplex added. PHY is able to perform automatic negotiation now. Sigh, one could make a hidd (or something else) that is common PHY driver for all network cards. Have a look at the code duplication.... ;)
    
    2. FEC Unit tests every second the link status. If status changes, the unit is stopped. Then, link up event forces unit to start again (I will need to perform the start conditionally later). The automatic negotiation routine works as expected.
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@31355 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/devs/networks/fec/fec.h
+++ workbench/devs/networks/fec/fec.h
@@ -379,6 +379,113 @@ struct FECBase {
 
 #define RXTX_ALLOC_BUFSIZE  (ETH_MAXPACKETSIZE + 26)
 
+/* PHY definitions */
+
+/* phy seed setup */
+#define AUTO                    99
+#define _1000BASET              1000
+#define _100BASET               100
+#define _10BASET                10
+#define HALF                    22
+#define FULL                    44
+
+/* phy register offsets */
+#define PHY_BMCR                0x00
+#define PHY_BMSR                0x01
+#define PHY_PHYIDR1             0x02
+#define PHY_PHYIDR2             0x03
+#define PHY_ANAR                0x04
+#define PHY_ANLPAR              0x05
+#define PHY_ANER                0x06
+#define PHY_ANNPTR              0x07
+#define PHY_ANLPNP              0x08
+#define PHY_1000BTCR            0x09
+#define PHY_1000BTSR            0x0A
+#define PHY_EXSR                0x0F
+#define PHY_PHYSTS              0x10
+#define PHY_MIPSCR              0x11
+#define PHY_MIPGSR              0x12
+#define PHY_DCR                 0x13
+#define PHY_FCSCR               0x14
+#define PHY_RECR                0x15
+#define PHY_PCSR                0x16
+#define PHY_LBR                 0x17
+#define PHY_10BTSCR             0x18
+#define PHY_PHYCTRL             0x19
+
+/* PHY BMCR */
+#define PHY_BMCR_RESET          0x8000
+#define PHY_BMCR_LOOP           0x4000
+#define PHY_BMCR_100MB          0x2000
+#define PHY_BMCR_AUTON          0x1000
+#define PHY_BMCR_POWD           0x0800
+#define PHY_BMCR_ISO            0x0400
+#define PHY_BMCR_RST_NEG        0x0200
+#define PHY_BMCR_DPLX           0x0100
+#define PHY_BMCR_COL_TST        0x0080
+
+#define PHY_BMCR_SPEED_MASK     0x2040
+#define PHY_BMCR_1000_MBPS      0x0040
+#define PHY_BMCR_100_MBPS       0x2000
+#define PHY_BMCR_10_MBPS        0x0000
+
+/* phy BMSR */
+#define PHY_BMSR_100T4          0x8000
+#define PHY_BMSR_100TXF         0x4000
+#define PHY_BMSR_100TXH         0x2000
+#define PHY_BMSR_10TF           0x1000
+#define PHY_BMSR_10TH           0x0800
+#define PHY_BMSR_EXT_STAT       0x0100
+#define PHY_BMSR_PRE_SUP        0x0040
+#define PHY_BMSR_AUTN_COMP      0x0020
+#define PHY_BMSR_RF             0x0010
+#define PHY_BMSR_AUTN_ABLE      0x0008
+#define PHY_BMSR_LS             0x0004
+#define PHY_BMSR_JD             0x0002
+#define PHY_BMSR_EXT            0x0001
+
+/*phy ANLPAR */
+#define PHY_ANLPAR_NP           0x8000
+#define PHY_ANLPAR_ACK          0x4000
+#define PHY_ANLPAR_RF           0x2000
+#define PHY_ANLPAR_ASYMP        0x0800
+#define PHY_ANLPAR_PAUSE        0x0400
+#define PHY_ANLPAR_T4           0x0200
+#define PHY_ANLPAR_TXFD         0x0100
+#define PHY_ANLPAR_TX           0x0080
+#define PHY_ANLPAR_10FD         0x0040
+#define PHY_ANLPAR_10           0x0020
+#define PHY_ANLPAR_100          0x0380  /* we can run at 100 */
+/* phy ANLPAR 1000BASE-X */
+#define PHY_X_ANLPAR_NP         0x8000
+#define PHY_X_ANLPAR_ACK        0x4000
+#define PHY_X_ANLPAR_RF_MASK    0x3000
+#define PHY_X_ANLPAR_PAUSE_MASK 0x0180
+#define PHY_X_ANLPAR_HD         0x0040
+#define PHY_X_ANLPAR_FD         0x0020
+
+#define PHY_ANLPAR_PSB_MASK     0x001f
+#define PHY_ANLPAR_PSB_802_3    0x0001
+#define PHY_ANLPAR_PSB_802_9    0x0002
+
+/* phy 1000BTCR */
+#define PHY_1000BTCR_1000FD     0x0200
+#define PHY_1000BTCR_1000HD     0x0100
+
+/* phy 1000BTSR */
+#define PHY_1000BTSR_MSCF       0x8000
+#define PHY_1000BTSR_MSCR       0x4000
+#define PHY_1000BTSR_LRS        0x2000
+#define PHY_1000BTSR_RRS        0x1000
+#define PHY_1000BTSR_1000FD     0x0800
+#define PHY_1000BTSR_1000HD     0x0400
+
+/* phy EXSR */
+#define PHY_EXSR_1000XF         0x8000
+#define PHY_EXSR_1000XH         0x4000
+#define PHY_EXSR_1000TF         0x2000
+#define PHY_EXSR_1000TH         0x1000
+
 struct FECUnit {
 	struct Unit				feu_Unit;
 	struct SignalSemaphore	feu_Lock;
@@ -396,6 +503,11 @@ struct FECUnit {
 	fec_t					*feu_regs;
 
 	uint32_t				feu_phy_speed;
+	int8_t					feu_phy_id;
+
+	uint16_t				feu_speed;
+	uint8_t					feu_duplex;
+	uint8_t					feu_link;
 
     uint8_t             	feu_DevAddr[ETH_ADDRESSSIZE];
     uint8_t             	feu_OrgAddr[ETH_ADDRESSSIZE];
@@ -412,7 +524,6 @@ struct FECUnit {
 
     int                		(*start)(struct FECUnit *);
     int                		(*stop)(struct FECUnit *);
-    void               		(*udelay)(struct FECUnit *, uint32_t usec);
     void               		(*set_multicast)(struct FECUnit *);
     void               		(*set_mac_address)(struct FECUnit *);
 };
@@ -426,5 +537,11 @@ int FEC_MDIO_Write(struct FECUnit *unit, int32_t phy_id, int32_t reg, uint16_t d
 void FEC_HW_Init(struct FECUnit *unit);
 void FEC_PHY_Init(struct FECUnit *unit);
 void FEC_Reset_Stats(struct FECUnit *unit);
+int8_t FEC_PHY_Find(struct FECUnit *unit);
+int FEC_PHY_Reset(struct FECUnit *unit);
+int FEC_PHY_Link(struct FECUnit *unit);
+int FEC_PHY_Speed(struct FECUnit *unit);
+int FEC_PHY_Duplex(struct FECUnit *unit);
+void FEC_PHY_Setup_Autonegotiation(struct FECUnit *unit);
 
 #endif /* FEC_H_ */
