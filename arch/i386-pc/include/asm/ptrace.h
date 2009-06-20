a77c1f463c68d295f52f73a7b18bc3e329a90c54
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-06-20 21:17:25 +0000

    Use more appropriate types for pt_regs struct, e.g. 16-bit types for 16-bit values.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@31449 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-pc/include/asm/ptrace.h
+++ arch/i386-pc/include/asm/ptrace.h
@@ -1,26 +1,30 @@
 /*
-    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2009, The AROS Development Team. All rights reserved.
     $Id$
 */
 
 #ifndef ASM_PTRACE_H
 #define ASM_PTRACE_H
 
+#include <exec/types.h>
+
 /* this struct defines the way the registers are stored on the 
    stack during a system call. */
 
 struct pt_regs {
-	int  xes;
-	int  xds;
-	long edx;
-	long ecx;
-	long eax;
-	long orig_eax;
-	long eip;
-	int  xcs;
-	long eflags;
-	long esp;
-	int  xss;
+    UWORD xes;
+    UWORD pad1;
+    UWORD xds;
+    ULONG edx;
+    ULONG ecx;
+    ULONG eax;
+    ULONG orig_eax;
+    ULONG eip;
+    UWORD xcs;
+    UWORD pad2;
+    UWORD eflags;
+    ULONG esp;
+    UWORD xss;
 };
 
 #define user_mode(regs) ((3 & (regs)->xcs))
