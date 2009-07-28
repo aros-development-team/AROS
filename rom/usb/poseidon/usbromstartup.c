77358715fcd9c7294bc30b70a5ff0291aa157076
platon42 <platon42@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-07-28 16:26:11 +0000

    For kernel rom startup: only startup usb if kernel command line contains "enableusb" keyword. (Note that this is not yet functional as there is a linking problem with the various modules for the kernel. In this respect, the need to have unique symbols for completely separate romtags sucks under AROS).
    git-svn-id: https://svn.aros.org/svn/aros/trunk@31632 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/usb/poseidon/usbromstartup.c
+++ rom/usb/poseidon/usbromstartup.c
@@ -2,16 +2,43 @@
  * PsdStackloader should be started during startup-sequence nonetheless */
 
 #include <aros/symbolsets.h>
+#include <aros/bootloader.h>
 
 #include <proto/poseidon.h>
 #include <proto/exec.h>
+#include <proto/bootloader.h>
+
+#include <string.h>
 
 int usbromstartup_init(void)
 {
     struct Library *ps;
     struct PsdHardware *phw;
     ULONG cnt = 0;
+    APTR BootLoaderBase;
 
+    // for now, only enable USB during boot if enableusb kernel parameter has been given
+    if((BootLoaderBase = OpenResource("bootloader.resource")))
+    {
+        struct List *args = GetBootInfo(BL_Args);
+        BOOL enable = FALSE;
+        if(args)
+        {
+            struct Node *node;
+            for(node = args->lh_Head; node->ln_Succ; node = node->ln_Succ)
+            {
+                if(stricmp(node->ln_Name, "enableusb") == 0)
+                {
+                    enable = TRUE;
+                }
+            }
+        }
+        if(!enable)
+        {
+            return(0);
+        }
+    }
+    
     if((ps = OpenLibrary("poseidon.library", 4)))
     {
         psdAddClass("hub.class", 0);
