715e53d12b393d76bedd000b2fe75e9d5fefc195
o1i <o1i@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-11-16 13:00:43 +0000

    Fixed wrong parameter order. You would never get any SNOTIFY_SCREENDEPTH otherwise.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@32031 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/intuition/screendepth.c
+++ rom/intuition/screendepth.c
@@ -1,5 +1,5 @@
 /*
-    Copyright  1995-2007, The AROS Development Team. All rights reserved.
+    Copyright  1995-2009, The AROS Development Team. All rights reserved.
     Copyright  2001-2003, The MorphOS Development Team. All Rights Reserved.
     $Id$
 
@@ -75,7 +75,7 @@ static VOID int_screendepth(struct ScreenDepthActionMsg *msg,
 
     struct ScreenDepthActionMsg msg;
 
-    FireScreenNotifyMessageCode((IPTR) screen, flags, SNOTIFY_SCREENDEPTH, IntuitionBase);
+    FireScreenNotifyMessageCode((IPTR) screen, SNOTIFY_SCREENDEPTH, flags, IntuitionBase);
 
     if (reserved != NULL) return;
     SANITY_CHECK(screen)
