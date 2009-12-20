e863647d39785f2052c93b4db64474f91a503fe3
Kalamatee <kalamatee@gmail.com>
2009-12-20 00:11:56 +0000

    Add stubs for shutdown/coldreboot, and definitions for core_APIC calls
    git-svn-id: https://svn.aros.org/svn/aros/trunk@32118 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/x86_64-pc/exec/shutdowna.c
@@ -0,0 +1,73 @@
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id: coldreboot.c 18441 2003-07-07 20:01:00Z hkiel $
+
+    Desc: ShutdownA() - Shut down the operating system.
+    Lang: english
+*/
+
+#include <asm/io.h>
+#include <exec/tasks.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/exec.h>
+
+	AROS_LH1(ULONG, ShutdownA,
+
+/*  SYNOPSIS */
+	AROS_LHA(ULONG, action, D0),
+
+/*  LOCATION */
+	struct ExecBase *, SysBase, 173, Exec)
+
+/*  FUNCTION
+	This function will shut down the operating system.
+
+    INPUTS
+	action - what to do:
+	 * SD_ACTION_POWEROFF   - power off the machine.
+	 * SD_ACTION_COLDREBOOT - cold reboot the machine (not only AROS).
+
+    RESULT
+	This function does not return in case of success. Otherwise is returns
+	zero.
+
+    NOTES
+	It can be quite harmful to call this function. It may be possible that
+	you will lose data from other tasks not having saved, or disk buffers
+	not being flushed. Plus you could annoy the (other) users.
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	ColdReboot()
+
+******************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    switch (action)
+    {
+	    case SD_ACTION_COLDREBOOT:
+		    outb(0xFE, 0x64);
+		    break;
+
+	    case SD_ACTION_POWEROFF:
+		    /*
+			    Just wait till the power button is pressed
+			    (or until we can do it via acpi ;))
+		    */
+		    asm("hlt");
+		    break;
+
+	    default:
+		    break;
+    }
+    return 0;
+
+    AROS_LIBFUNC_EXIT
+}
