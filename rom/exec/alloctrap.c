86d418f314400cbe40e89ea9d00457079f0bd917
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-12-25 13:34:37 +0000

    - Implemented AllocTrap() and FreeTrap() - Removed #warning from FreeSignal()
    git-svn-id: https://svn.aros.org/svn/aros/trunk@32176 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/alloctrap.c
+++ rom/exec/alloctrap.c
@@ -35,14 +35,60 @@
     SEE ALSO
 
     INTERNALS
+        Very similar to AllocSignal()
 
 *****************************************************************************/
 {
     AROS_LIBFUNC_INIT
 
-#warning TODO: Write exec/AllocTrap()
-    aros_print_not_implemented ("AllocTrap");
+    struct Task *ThisTask;
+    ULONG mask;
+    ULONG mask1;
+    
+    ThisTask = FindTask(NULL);
+    mask = GetTrapAlloc(ThisTask);
 
-    return -1L;
+    /* Will any trap do? */
+    if(trapNum < 0)
+    {
+	/*
+	 * To get the last nonzero bit in a number I use a&~a+1:
+	 * Given a number that ends with a row of zeros  xxxx1000
+	 * I first toggle all bits in that number	 XXXX0111
+	 * then add 1 to toggle all but the last 0 again XXXX1000
+	 * and AND this with the original number	 00001000
+	 *
+	 * And since ~a+1=-a I can use a&-a instead.
+	 *
+	 * And to get the last zero bit I finally use ~a&-~a.
+	 */
+	mask1 = ~mask & - ~mask;
+
+	/* Is the bit already allocated? */
+	if(mask1 == 0)
+	    return -1;
+
+	/* And get the bit number */
+	trapNum=(mask1&0xffff0000?16:0)+(mask1&0xff00ff00?8:0)+
+		  (mask1&0xf0f0f0f0? 4:0)+(mask1&0xcccccccc?2:0)+
+		  (mask1&0xaaaaaaaa? 1:0);
+    }
+    else
+    {
+	mask1 = 1L << trapNum;
+
+	/* If trap bit is already allocated, return. */
+	if(mask & mask1)
+	    return -1;
+    }
+
+    if (ThisTask->tc_Flags & TF_ETASK) {
+        struct ETask *et = ThisTask->tc_UnionETask.tc_ETask;
+	
+	et->et_TrapAlloc |= mask1;
+    } else
+        ThisTask->tc_TrapAlloc |= mask1;
+
+    return trapNum;
     AROS_LIBFUNC_EXIT
-} /* AllocTrap */
+} /* AllocTrap() */
--- rom/exec/freetrap.c
+++ rom/exec/freetrap.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2007, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2009, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: Free a trap.
@@ -35,13 +35,24 @@
     SEE ALSO
 
     INTERNALS
+        Very similar to FreeSignal()
 
 *****************************************************************************/
 {
     AROS_LIBFUNC_INIT
 
-#warning TODO: Write exec/FreeTrap()
-    aros_print_not_implemented ("FreeTrap");
+    if(trapNum!=-1)
+    {
+        /* No more atomic problem - i beleive THIS is atomic. - sonic */
+        struct Task *me = SysBase->ThisTask;
+
+        if (me->tc_Flags & TF_ETASK) {
+	    struct ETask *et = me->tc_UnionETask.tc_ETask;
+	    
+	    et->et_TrapAlloc &= ~(1<<trapNum);
+	} else
+	    me->tc_TrapAlloc &= ~(1<<trapNum);
+    }
 
     AROS_LIBFUNC_EXIT
 } /* FreeTrap */
