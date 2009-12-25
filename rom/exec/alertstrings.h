e1745ee9d2cfa5e4693705722a1f8557ebd51f87
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-12-25 11:40:20 +0000

    A new shiny guru display on the debug output. :) Should actually work only on i386-native and Windows-hosted.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@32172 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/exec/alertstrings.h
@@ -0,0 +1,4 @@
+STRPTR Alert_AddString(STRPTR dest, STRPTR src);
+STRPTR Alert_GetTitle(ULONG alertNum);
+STRPTR Alert_GetTaskName(struct ExecBase *SysBase);
+void Alert_GetString(ULONG alertnum, STRPTR buf);
