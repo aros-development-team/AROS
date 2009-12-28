611f441519c8ec3501eb09c7ace6a386b2a9f218
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-12-28 22:20:48 +0000

    supertest now works with x86 and should at least compile on unhandled architectures.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@32221 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/supertest.c
+++ test/supertest.c
@@ -1,14 +1,17 @@
 /*
-    Copyright © 1995-2002, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2009, The AROS Development Team. All rights reserved.
     $Id$
 */
 
-#include <proto/exec.h>
 #include <exec/types.h>
-#include <clib/exec_protos.h>
-#include <stdio.h>
+#include <proto/exec.h>
+#include <proto/dos.h>
+
+extern UWORD a(void);
+extern UWORD b(void);
 
 #ifdef __GNUC__
+#ifdef __mc68000
 asm("
 	.globl	_a
 _a:
@@ -19,22 +22,42 @@ _b:
 	movew	sp@,d0
 	rte
 ");
+#else
+#ifdef __i386__
+__asm__(
+"	.globl	a\n"
+"a:\n"
+"	pushf\n"
+"	popl	%eax\n"
+"	ret\n"
+"	.globl	b\n"
+"b:\n"
+"	movl	%esp,%eax\n"
+"	iret\n"
+);
+#else
+#define a(A) 0
+#undef Supervisor
+#define Supervisor(A) 0
 #endif
-
-extern UWORD a(void);
-extern UWORD b(void);
+#endif
+#endif
 
 int main(void)
 {
     APTR ssp;
     UWORD ar;
-    printf("%04x\n",GetCC());
-    printf("%04lx\n",SetSR(0,0));
-    ssp=SuperState();
-    ar=a();
+
+    Printf("GetCC(): %04x\n",GetCC());
+    Printf("SetSR(): %04lx\n",SetSR(0,0));
+    ssp = SuperState();
+    ar = a();
     UserState(ssp);
-    printf("%04x\n",a());
-    printf("%04x\n",(UWORD)Supervisor((ULONG_FUNC)&b));
+    Printf("Supervisor flags: %04x\n", ar);
+    Printf("User flags: %04x\n", a());
+    Printf("Supervisor stack pointer: %04lx\n",
+        (IPTR)Supervisor((ULONG_FUNC)&b));
+
     return 0;
 }
 
