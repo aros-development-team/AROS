b4c4335b030f9d791ea6604e8063f4fdf13a62fc
mattrust <mrustler@gmx.de>
2010-01-15 14:19:31 +0000

    Cleanup.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@32339 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/prefs/serial/prefs.h
+++ workbench/prefs/serial/prefs.h
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2010, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc:
@@ -18,18 +18,6 @@ BOOL Prefs_HandleArgs(STRPTR from, BOOL use, BOOL save);
 BOOL Prefs_ImportFH(BPTR fh);
 BOOL Prefs_ExportFH(BPTR fh);
 BOOL Prefs_Default(VOID);
-
-//ULONG InitPrefs   (STRPTR filename, BOOL use, BOOL save);
-//VOID  CleanupPrefs(void);
-//BOOL  LoadPrefs   (STRPTR filename);
-//BOOL  LoadPrefsFH (BPTR fh);
-//BOOL  SavePrefs   (STRPTR filename);
-//BOOL  SavePrefsFH (BPTR fh);
-//BOOL  SaveEnv     ();
-//BOOL  DefaultPrefs(void);
-//VOID  RestorePrefs(void);
-//VOID  BackupPrefs (void);
-//VOID  CopyPrefs   (struct SerialPrefs *s, struct SerialPrefs *d);
 
 /*********************************************************************************************/
 
