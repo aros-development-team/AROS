124a9bac73fbbf88f988c78fd86a264f42e3c766
o1i <o1i@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-01-26 13:06:01 +0000

    If wanderer detects a m68k amigaOS executable and j-uae is running, let j-uae launch the amigaOS file.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@32455 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/system/Wanderer/uae_integration.h
@@ -0,0 +1,29 @@
+#ifndef _UAE_INTEGRATION_H_
+#define _UAE_INTEGRATION_H_
+
+/*
+    Copyright (C) 2010, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <exec/types.h>
+#include <utility/tagitem.h>
+#include "Classes/iconlist.h"
+
+struct JUAE_Launch_Message 
+{
+    struct Message  ExecMessage;
+    STRPTR          ln_Name;
+    struct TagItem *tags;
+    void           *mempool; 
+};
+
+#define J_UAE_PORT "J-UAE Execute"
+
+/*** Prototypes *************************************************************/
+
+BOOL is_68k        (STRPTR filename);
+void forward_to_uae(struct TagItem *argsTagList, struct IconList_Entry *ent);
+BOOL j_uae_running (void);
+
+#endif /* _UAE_INTEGRATION_H_ */
