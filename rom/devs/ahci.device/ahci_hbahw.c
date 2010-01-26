750e60b91db40efa59481d211180edc59a27ee6d
DizzyOfCRN <DizzyOfCRN@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-01-26 16:19:54 +0000

    More stuff to work on
    git-svn-id: https://svn.aros.org/svn/aros/trunk@32460 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/devs/ahci.device/ahci_hbahw.c
+++ rom/devs/ahci.device/ahci_hbahw.c
@@ -12,6 +12,62 @@
     Call every HBA function with pointer to ahci_hba_chip structure as a first argument
 */
 
+static void ahci_taskcode_hba(struct ahci_hba_chip *hba_chip, struct Task* parent) {
+    D(bug("[AHCI] (%04x:%04x) Hello! HBA task running...\n", hba_chip->VendorID, hba_chip->ProductID));
+
+    D(bug("Signaling parent %08x\n", parent));
+
+    Signal(parent, SIGBREAKF_CTRL_C);
+
+//    ahci_init_hba( hba_chip );
+
+    while(1){
+    }
+
+    D(bug("[AHCI] (%04x:%04x) Bye!\n", hba_chip->VendorID, hba_chip->ProductID));
+}
+
+BOOL ahci_setup_hbatask(struct ahci_hba_chip *hba_chip) {
+    D(bug("[AHCI] (%04x:%04x) HBA task setup...\n", hba_chip->VendorID, hba_chip->ProductID));
+
+    APTR stack;
+
+    hba_chip->hba_task = AllocMem(sizeof (struct Task), MEMF_PUBLIC|MEMF_CLEAR);
+    if (hba_chip->hba_task) {
+    	NEWLIST(&hba_chip->hba_task->tc_MemEntry);
+    	hba_chip->hba_task->tc_Node.ln_Type =NT_TASK;
+    	hba_chip->hba_task->tc_Node.ln_Name = "HBA Task";
+    	hba_chip->hba_task->tc_Node.ln_Pri  = HBA_TASK_PRI;
+
+    	stack = AllocMem(HBA_TASK_STACKSIZE, MEMF_PUBLIC);
+    	if(stack != NULL) {
+	        struct TagItem tags[] = {
+                {TASKTAG_ARG1, (IPTR)hba_chip},
+                {TASKTAG_ARG2, (IPTR)FindTask(0)},
+                {TAG_DONE}
+	        };
+	    
+	        hba_chip->hba_task->tc_SPLower = stack;
+	        hba_chip->hba_task->tc_SPUpper = (UBYTE *)stack + HBA_TASK_STACKSIZE;
+
+        #if AROS_STACK_GROWS_DOWNWARDS
+	        hba_chip->hba_task->tc_SPReg = (UBYTE *)hba_chip->hba_task->tc_SPUpper-SP_OFFSET;
+        #else
+	        hba_chip->hba_task->tc_SPReg = (UBYTE *)hba_chip->hba_task->tc_SPLower+SP_OFFSET;
+        #endif
+
+            if(NewAddTask(hba_chip->hba_task, ahci_taskcode_hba, NULL, tags) != NULL) {
+                Wait(SIGBREAKF_CTRL_C);
+                return TRUE;
+            }	
+
+            FreeMem(stack, HBA_TASK_STACKSIZE);	
+        }
+        FreeMem(hba_chip->hba_task,sizeof(struct Task));
+    }
+    return FALSE;
+}
+
 /*
     Set the AHCI HBA to a minimally initialized state.
 
@@ -28,6 +84,7 @@
             register with the appropriate enables. To enable the HBA to generate interrupts, system
             software must also set GHC.IE to a ‘1’.
 */
+
 void ahci_init_hba(struct ahci_hba_chip *hba_chip) {
     D(bug("[AHCI] (%04x:%04x) HBA init...\n", hba_chip->VendorID, hba_chip->ProductID));
 
@@ -45,16 +102,18 @@ void ahci_init_hba(struct ahci_hba_chip *hba_chip) {
         ((hba_chip->Version >> 4) & 0xf0) + (hba_chip->Version & 0x0f) ));
 
     /*
-        BIOS handoff if HBA supports it
+        BIOS handoff if HBA supports it and BIOS is the current owner of HBA
     */
 	if ( (hba_chip->Version >= AHCI_VERSION_1_20) ) {
 		if( (hba->cap2 && CAP2_BOH) ) {
             D(bug("[AHCI] HBA supports BIOS/OS handoff\n"));
-            hba->bohc |= BOHC_OOS;
-            while( (hba->bohc && BOHC_BOS) );
-            //wait 25ms
-            if( (hba->bohc && BOHC_BB) ) {
-                //Wait minimum of 2 seconds to give BIOS time for finishing outstanding commands.
+            if( (hba->bohc && BOHC_BOS) ) {
+                hba->bohc |= BOHC_OOS;
+                while( (hba->bohc && BOHC_BOS) );
+                //wait 25ms
+                if( (hba->bohc && BOHC_BB) ) {
+                    //Wait minimum of 2 seconds to give BIOS time for finishing outstanding commands.
+                }
             }
         }
     }
@@ -69,10 +128,10 @@ void ahci_init_hba(struct ahci_hba_chip *hba_chip) {
     */
 
     ULONG i;
-    hba_chip->PortNBR = 0;
+    hba_chip->PortCount = 0;
     for (i = 0; i <= 31; i++) {
         if( ((hba->pi) & (1<<i)) ) {
-            hba_chip->PortNBR++;
+            hba_chip->PortCount++;
         }
     }
 
@@ -89,7 +148,7 @@ void ahci_reset_hba(struct ahci_hba_chip *hba_chip) {
     /*
         Enable AHCI mode
     */
-	ahci_enable_hba(hba_chip);
+	ahci_enable_hba( hba_chip );
 
     /*
         Reset HBA
@@ -106,7 +165,7 @@ void ahci_reset_hba(struct ahci_hba_chip *hba_chip) {
     /*
         Reenable AHCI mode
     */
-    ahci_enable_hba(hba_chip);
+    ahci_enable_hba( hba_chip );
 
 	/*
         Clear interrupts
@@ -118,22 +177,21 @@ void ahci_reset_hba(struct ahci_hba_chip *hba_chip) {
 
 }
 
-BOOL ahci_enable_hba(struct ahci_hba_chip *hba_chip) {
+void ahci_enable_hba(struct ahci_hba_chip *hba_chip) {
     D(bug("[AHCI] (%04x:%04x) HBA enable...\n", hba_chip->VendorID, hba_chip->ProductID));
 
     struct ahci_hba *hba;
     hba = hba_chip->abar;
 
     /*
-        Access to any other register than to GHC is not allowed when GHC_AE = 0,
-        ...but the implementation of GHC_AE bit depends on CAP_SAM -> RW or RO ?!?
-        Clear other bits while setting AE (MRSM, IE and HR)
+        Check first if bit GHC_AE is not set (HBA acts as legacy) otherwise we may not be allowed to touch the bit as it may be RO (AHCI mode only)
+        Clear other bits while setting GHC_AE (GHC_MRSM, GHC_IE and GHC_HR)
     */
 
-    if( !(hba->cap == CAP_SAM) )
-	    hba->ghc = GHC_AE;
+    if( !(hba->ghc && GHC_AE) ){
+        hba->ghc = GHC_AE;
+    }
 
-    return ((hba->ghc && GHC_AE) ? TRUE:FALSE);
 }
 
 void ahci_disable_hba(struct ahci_hba_chip *hba_chip) {
@@ -142,7 +200,15 @@ void ahci_disable_hba(struct ahci_hba_chip *hba_chip) {
     struct ahci_hba *hba;
     hba = hba_chip->abar;
 
-    if( !(hba->cap == CAP_SAM) )
-        hba->ghc = 0x00000000;
+    /*
+        Check first if bit GHC_AE is set and if so then check whether it is RO or RW (if CAP_SAM not set then HBA can be AHCI or legacy)
+        If bit is RW then clear the bit along with the other bits
+    */
+
+    if( (hba->ghc && GHC_AE) ){
+        if( !(hba->ghc && CAP_SAM) ){
+            hba->ghc = 0x00000000;
+        }
+    }
 
 }
