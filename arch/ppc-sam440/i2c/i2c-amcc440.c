2ebde08d019ad8760322ec4122f22b37dc351f37
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-02-02 22:22:30 +0000

    i2c bus (IIC0) for amcc440 cpu
    git-svn-id: https://svn.aros.org/svn/aros/trunk@32557 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/ppc-sam440/i2c/i2c-amcc440.c
@@ -0,0 +1,41 @@
+/*
+ * i2c-amcc440.c
+ *
+ *  Created on: Feb 1, 2010
+ *      Author: misc
+ */
+
+
+#include <aros/symbolsets.h>
+#include <aros/debug.h>
+#include <proto/exec.h>
+
+#include <hidd/i2c.h>
+
+#include <asm/amcc440.h>
+#include <asm/io.h>
+
+#include "i2c-amcc440.h"
+
+void METHOD(I2C440, Hidd_I2C, PutBits)
+{
+	/* Put i2c bus into reset state. Mandatory in order to control it manually */
+	outb(IIC_XTCNTLSS_SRST, IIC0_XTCNTLSS);
+
+	UBYTE val=0;
+
+	val |= (msg->scl ? IIC_DIRECTCNTL_SCLC : 0);
+	val |= (msg->sda ? IIC_DIRECTCNTL_SDAC : 0);
+
+	outb(val, IIC0_DIRECTCNTL);
+}
+
+void METHOD(I2C440, Hidd_I2C, GetBits)
+{
+	UBYTE val = inb(IIC0_DIRECTCNTL);
+
+    *msg->sda = (val & IIC_DIRECTCNTL_MSDA) != 0;
+    *msg->scl = (val & IIC_DIRECTCNTL_MSCL) != 0;
+}
+
+ADD2LIBS((STRPTR)"i2c.hidd", 0, static struct Library *, I2CBase);
