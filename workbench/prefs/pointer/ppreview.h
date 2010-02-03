a650268abc1199bc9566d80b326007704d43ffdc
mattrust <mrustler@gmx.de>
2010-02-03 20:53:15 +0000

    W.I.P pointer preview class
    git-svn-id: https://svn.aros.org/svn/aros/trunk@32566 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/prefs/pointer/ppreview.h
@@ -0,0 +1,26 @@
+#ifndef _PPREVIEW_H_
+#define _PPREVIEW_H_
+
+/*
+    Copyright © 2010, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <exec/types.h>
+#include <libraries/mui.h>
+
+/*** Identifier base ********************************************************/
+#define MUIB_PPreview                  (TAG_USER | 0x10000000)
+
+/*** Variables **************************************************************/
+extern struct MUI_CustomClass *PPreview_CLASS;
+
+/*** Macros *****************************************************************/
+#define PPreviewObject BOOPSIOBJMACRO_START(PPreview_CLASS->mcc_Class)
+
+/*** Attributes *************************************************************/
+#define MUIA_PPreview_Alpha     (MUIB_PPreview | 0x00000000) /* ISG  UWORD */
+#define MUIA_PPreview_HSpotX    (MUIB_PPreview | 0x00000001) /* ISG  UWORD */
+#define MUIA_PPreview_HSpotY    (MUIB_PPreview | 0x00000002) /* ISG  UWORD */
+
+#endif /* _PPREVIEW_H_ */
