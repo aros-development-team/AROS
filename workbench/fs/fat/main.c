e080f7e9d9f4640fdb666ea313b61b10f713e72f
Kalamatee <kalamatee@gmail.com>
2010-02-04 16:09:52 +0000

    Add missing debug params
    git-svn-id: https://svn.aros.org/svn/aros/trunk@32572 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/fs/fat/main.c
+++ workbench/fs/fat/main.c
@@ -2,7 +2,7 @@
  * fat.handler - FAT12/16/32 filesystem handler
  *
  * Copyright © 2006 Marek Szyprowski
- * Copyright © 2007-2009 The AROS Development Team
+ * Copyright © 2007-2010 The AROS Development Team
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the same terms as AROS itself.
@@ -56,7 +56,7 @@ void handler(void) {
     glob->devnode = BADDR(startuppacket->dp_Arg3);
 
     D(bug("\nFATFS: opening libraries.\n"));
-    D(bug("\tFS task: %lx, port %lx\n"));
+    D(bug("\tFS task: %lx, port %lx\n", glob->ourtask, glob->ourport));
 
     glob->notifyport = CreateMsgPort();
 
