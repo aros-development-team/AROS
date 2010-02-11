31e94bc38e0d82af096c0313e98f2e607f861ff3
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-02-11 21:33:32 +0000

    - Removed framebuffer usage - Really support several different display modes - Implemented setting mouse pointer shape
    git-svn-id: https://svn.aros.org/svn/aros/trunk@32634 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-pc/drivers/vga.hidd/vga.h
+++ arch/i386-pc/drivers/vga.hidd/vga.h
@@ -24,67 +24,33 @@
 
 #include "bitmap.h"
 
-/***** Mouse HIDD *******************/
-
-/* IDs */
-#define IID_Hidd_HwMouse	"hidd.mouse.hw"
-#define CLID_Hidd_HwMouse	"hidd.mouse.hw"
-
-/* Methods */
-enum
-{
-    moHidd_Mouse_HandleEvent
-};
-
-struct pHidd_Mouse_HandleEvent
-{
-    OOP_MethodID mID;
-    ULONG event;
-};
-
-VOID Hidd_Mouse_HandleEvent(OOP_Object *o, ULONG event);
-
 /***** VGA gfx HIDD *******************/
 
 /* IDs */
 #define IID_Hidd_VGAgfx		"hidd.gfx.vga"
 #define CLID_Hidd_VGAgfx	"hidd.gfx.vga"
 
-struct MouseShape {
-    ULONG width;
-    ULONG height;
-    ULONG bpp;
-    UBYTE *data;
-};
-
 /* misc */
 
 struct vga_staticdata
 {
+    OOP_Class 	*vgaclass;
+    OOP_Class 	*bmclass;
+    OOP_Object 	*vgahidd;
     struct SignalSemaphore	sema;	/* Protecting this whole struct */
-    struct SignalSemaphore	HW_acc;	/* Exclusive hardware use */
     struct List		modelist;	/* List of modes supported */
+
+    /* The following should be object data, not class data! */
+    struct SignalSemaphore	HW_acc;	/* Exclusive hardware use */
     struct bitmap_data	*visible;	/* Point to visible bitmap */
 
-    OOP_Class 	*vgaclass;
-    OOP_Class 	*onbmclass;
-    OOP_Class 	*offbmclass;
-#if 0
-    OOP_Class 	*mouseclass;
-#endif
-    OOP_Object 	*vgahidd;
-#if 0
-    OOP_Object 	*mousehidd;
-#endif
     LONG		mouseX;		/* Pointer X position on screen */
     ULONG		mouseW;		/* Pointer width */
     LONG		mouseY;		/* Pointer Y position on screen */
     ULONG		mouseH;		/* Pointer height */
     ULONG		mouseVisible;	/* Is pointer visible flag */
     UBYTE		*mouseShape;	/* Points to pointer shape */
-
-    VOID	(*activecallback)(APTR, OOP_Object *, BOOL);
-    APTR	callbackdata;
+    UBYTE		mouseBase;	/* Pointer base color	   */
 };
 
 struct vgabase
@@ -94,13 +60,8 @@ struct vgabase
     struct vga_staticdata vsd;
 };
 
-#if 0
-    /* nlorentz: This function is no lonfger necessary	*/
-BOOL set_pixelformat(OOP_Object *);
-
-#endif
-
 void draw_mouse (struct vga_staticdata *);
+void erase_mouse (struct vga_staticdata *);
 
 #define XSD(cl) (&((struct vgabase *)cl->UserData)->vsd)
 
