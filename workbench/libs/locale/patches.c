5f3882af4aeed9842d47e5a9c570fdecafd520ac
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-04-27 12:15:50 +0000

    - Added support for localizing VNewRawDoFmt(), needs testing - Removed excessive code from patches.c
    git-svn-id: https://svn.aros.org/svn/aros/trunk@33101 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/locale/patches.c
+++ workbench/libs/locale/patches.c
@@ -35,19 +35,57 @@ extern void LIB_LocDateToStr(void);
 extern void LIB_LocStrToDate(void);
 extern void LIB_LocDosGetLocalizedString(void);
 
+#else
+
+AROS_LD4(APTR, LocRawDoFmt,
+	 AROS_LDA(CONST_STRPTR, FormatString, A0),
+	 AROS_LDA(APTR        , DataStream, A1),
+	 AROS_LDA(VOID_FUNC   , PutChProc, A2),
+	 AROS_LDA(APTR        , PutChData, A3),
+	 struct ExecBase *, SysBase, 31, Locale);
+AROS_LD4(APTR, LocVNewRawDoFmt,
+	 AROS_LDA(CONST_STRPTR, FormatString, A0),
+	 AROS_LDA(VOID_FUNC   , PutChProc, A2),
+	 AROS_LDA(APTR        , PutChData, A3),
+	 AROS_LDA(va_list     , DataStream, A1),
+	 struct ExecBase *, SysBase, 137, Locale);
+AROS_LD3(LONG, LocStrnicmp,
+	 AROS_LDA(CONST_STRPTR, string1, A0),
+	 AROS_LDA(CONST_STRPTR, string2, A1),
+	 AROS_LDA(LONG        , length , D0),
+	 struct UtilityBase *, UtilityBase, 32, Locale);
+AROS_LD2(LONG, LocStricmp,
+	 AROS_LDA(CONST_STRPTR, string1, A0),
+	 AROS_LDA(CONST_STRPTR, string2, A1),
+	 struct UtilityBase *, UtilityBase, 33, Locale);
+AROS_LD1(ULONG, LocToLower,
+	 AROS_LDA(ULONG, character, D0),
+	 struct UtilityBase *, UtilityBase, 34, Locale);
+AROS_LD1(ULONG, LocToUpper,
+	 AROS_LDA(ULONG, character, D0),
+	 struct UtilityBase *, UtilityBase, 35, Locale);
+AROS_LD1(LONG, LocDateToStr,
+	 AROS_LDA(struct DateTime *, datetime, D1),
+	 struct DosLibrary *, DOSBase, 36, Locale);
+AROS_LD1(LONG, LocStrToDate,
+	 AROS_LDA(struct DateTime *, datetime, D1),
+	 struct DosLibrary *, DOSBase, 37, Locale);
+AROS_LD1(CONST_STRPTR, LocDosGetLocalizedString,
+	 AROS_LDA(LONG, stringNum, D1),
+	 struct DosLibrary *, DOSBase, 38, Locale);
+
 #endif
 
 /*********************************************************************************************/
 
-#ifdef __MORPHOS__
 static struct patchinfo
 {
     WORD    library;
     WORD    whichfunc;
     APTR    whichpatchfunc;
-}
-pi [] =
-{
+} pi [] = {
+
+#ifdef __MORPHOS__
     {LIB_EXEC   , 87 , LIB_LocRawDoFmt},
     {LIB_UTILITY, 28 , LIB_LocStrnicmp},
     {LIB_UTILITY, 27 , LIB_LocStricmp},
@@ -56,30 +94,20 @@ pi [] =
     {LIB_DOS    , 124, LIB_LocDateToStr},
     {LIB_DOS    , 125, LIB_LocStrToDate},
     {LIB_DOS    , 163, LIB_LocDosGetLocalizedString},
-    {NULL}
-};
-
 #else
-static struct patchinfo
-{
-    WORD    library;
-    WORD    whichfunc;
-    WORD    patchlibrary;
-    WORD    whichpatchfunc;
-}
-pi [] =
-{
-    {LIB_EXEC   , 87 , LIB_LOCALE, 31}, /* RawDoFmt  */
-    {LIB_UTILITY, 28 , LIB_LOCALE, 32}, /* Strnicmp  */
-    {LIB_UTILITY, 27 , LIB_LOCALE, 33}, /* Stricmp   */
-    {LIB_UTILITY, 30 , LIB_LOCALE, 34}, /* ToLower   */
-    {LIB_UTILITY, 29 , LIB_LOCALE, 35}, /* ToUpper   */
-    {LIB_DOS    , 124, LIB_LOCALE, 36}, /* DateToStr */
-    {LIB_DOS    , 125, LIB_LOCALE, 37}, /* StrToDate */
-    {LIB_DOS    , 154, LIB_LOCALE, 38}, /* DosGetLocalizedString */
-    {0         	    	    	     }
+    {LIB_EXEC   , 87 , AROS_SLIB_ENTRY(LocRawDoFmt		, Locale)},
+/* Please test on various architectures before enabling
+    {LIB_EXEC   , 137, AROS_SLIB_ENTRY(LocVNewRawDoFmt		, Locale)}, */
+    {LIB_UTILITY, 28 , AROS_SLIB_ENTRY(LocStrnicmp		, Locale)},
+    {LIB_UTILITY, 27 , AROS_SLIB_ENTRY(LocStricmp		, Locale)},
+    {LIB_UTILITY, 30 , AROS_SLIB_ENTRY(LocToLower		, Locale)},
+    {LIB_UTILITY, 29 , AROS_SLIB_ENTRY(LocToUpper		, Locale)},
+    {LIB_DOS    , 124, AROS_SLIB_ENTRY(LocDateToStr		, Locale)},
+    {LIB_DOS    , 125, AROS_SLIB_ENTRY(LocStrToDate		, Locale)},
+    {LIB_DOS    , 154, AROS_SLIB_ENTRY(LocDosGetLocalizedString	, Locale)},
+ #endif
+    {0}
 };
-#endif
 
 /*********************************************************************************************/
 
@@ -133,8 +161,7 @@ void InstallPatches(void)
         NewSetFunction(GetLib(pi[i].library), pi[i].whichpatchfunc, -pi[i].whichfunc * LIB_VECTSIZE, PatchTags);
 #else
         SetFunction(GetLib(pi[i].library),
-	    	    -pi[i].whichfunc * LIB_VECTSIZE,
-		    __AROS_GETVECADDR(GetLib(pi[i].patchlibrary), pi[i].whichpatchfunc));
+	    	    -pi[i].whichfunc * LIB_VECTSIZE, pi[i].whichpatchfunc);
 #endif
     }
     Permit();
