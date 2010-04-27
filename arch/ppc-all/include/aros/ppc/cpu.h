4f4f2847f62298fd71237cb8057899040aef59dc
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-04-27 05:42:48 +0000

    Added NO_LINEAR_VARARGS definition
    git-svn-id: https://svn.aros.org/svn/aros/trunk@33099 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/ppc-all/include/aros/ppc/cpu.h
+++ arch/ppc-all/include/aros/ppc/cpu.h
@@ -45,6 +45,9 @@
 /* do we need a function attribute to get parameters on the stack? */
 #define __stackparm
 
+/* define this if we have no support for linear varargs in the compiler */
+#define NO_LINEAR_VARARGS 1
+
 #define AROS_SLOWSTACKTAGS      1
 #define AROS_SLOWSTACKMETHODS   1
 #define AROS_SLOWSTACKHOOKS     1
