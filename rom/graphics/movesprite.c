9f1b14584f49263970925929162353d9ed1aa26c
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-05-12 10:45:48 +0000

    - Take ViewPort origin into account - Written autodoc
    git-svn-id: https://svn.aros.org/svn/aros/trunk@33233 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/graphics/movesprite.c
+++ rom/graphics/movesprite.c
@@ -29,12 +29,25 @@
         struct GfxBase *, GfxBase, 71, Graphics)
 
 /*  FUNCTION
+	Move sprite to a new position on the screen. Coordinates
+	are specified relatively to given ViewPort, or relatively
+	to the entire View (physical display) if the ViewPort is NULL.
+
+	This function works also with extended sprites, since
+	struct SimpleSprite is a part of struct ExtSprite.
 
     INPUTS
+	vp     - a ViewPort for relative sprite positioning or NULL
+	sprite - a pointer to a sprite descriptor structure
+	x      - a new X coordinate
+	y      - a new Y coordinate
 
     RESULT
+	None.
 
     NOTES
+	AROS currently supports only one sprite #0 for mouse pointer.
+	Other sprite numbers are ignored by this function.
 
     EXAMPLE
 
@@ -51,16 +64,20 @@
 {
     AROS_LIBFUNC_INIT
 
-    sprite->x = x;
-    sprite->y = y;
-    
+    if (vp) {
+        sprite->x = x - vp->DxOffset;
+        sprite->y = y - vp->DyOffset;
+    } else {
+        sprite->x = x;
+	sprite->y = y;
+    }
+
     if (sprite->num) /* We have only sprite #0 for the mouse cursor */
         return;
-	
-    /* Note that we actually do not have ViewPorts and views, so we simply
-       ignore the ViewPort */
-    if (SDD(GfxBase)->gfxhidd)
-    	HIDD_Gfx_SetCursorPos(SDD(GfxBase)->gfxhidd, x, y);
+
+    if (SDD(GfxBase)->gfxhidd) {
+    	HIDD_Gfx_SetCursorPos(SDD(GfxBase)->gfxhidd, sprite->x, sprite->y);
+    }
 
     AROS_LIBFUNC_EXIT
 } /* MoveSprite */
