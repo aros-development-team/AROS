85cb1435dd01316b7f4e47da6146a91964905c70
mattrust <mrustler@gmx.de>
2010-05-18 16:41:58 +0000

    Indentation fixed.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@33308 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/identify/idexpansion.c
+++ workbench/libs/identify/idexpansion.c
@@ -49,14 +49,14 @@
 {
     AROS_LIBFUNC_INIT
 
-	STRPTR manufstr = NULL;
-	STRPTR prodstr = NULL;
-	STRPTR classstr = NULL;
-	ULONG strlength = 50;
-	struct ConfigDev **expansion = NULL;
-	BOOL secondary = FALSE;
-	ULONG *classid = NULL;
-	BOOL localize = TRUE;
+    STRPTR manufstr = NULL;
+    STRPTR prodstr = NULL;
+    STRPTR classstr = NULL;
+    ULONG strlength = 50;
+    struct ConfigDev **expansion = NULL;
+    BOOL secondary = FALSE;
+    ULONG *classid = NULL;
+    BOOL localize = TRUE;
 
     struct TagItem *tag;
     const struct TagItem *tags;
@@ -66,78 +66,78 @@
         switch (tag->ti_Tag)
         {
             case IDTAG_ConfigDev:
-				// doesn't return anything
+                // doesn't return anything
                 break;
 
             case IDTAG_ManufID:
-				// doesn't return anything
+                // doesn't return anything
                 break;
 
             case IDTAG_ProdID:
-				// doesn't return anything
+                // doesn't return anything
                 break;
 
             case IDTAG_ManufStr:
-				manufstr = (STRPTR)tag->ti_Data;
+                manufstr = (STRPTR)tag->ti_Data;
                 break;
 
             case IDTAG_ProdStr:
-				prodstr = (STRPTR)tag->ti_Data;
+                prodstr = (STRPTR)tag->ti_Data;
                 break;
 
             case IDTAG_ClassStr:
-				classstr = (STRPTR)tag->ti_Data;
+                classstr = (STRPTR)tag->ti_Data;
                 break;
 
             case IDTAG_StrLength:
-				strlength = tag->ti_Data;
+                strlength = tag->ti_Data;
                 break;
 
             case IDTAG_Expansion:
-				expansion = (struct ConfigDev **)tag->ti_Data;
+                expansion = (struct ConfigDev **)tag->ti_Data;
                 break;
 
             case IDTAG_Secondary:
-				secondary = tag->ti_Data ? TRUE : FALSE;
+                secondary = tag->ti_Data ? TRUE : FALSE;
                 break;
 
             case IDTAG_ClassID:
-				classid = (ULONG *)tag->ti_Data;
+                classid = (ULONG *)tag->ti_Data;
                 break;
 
             case IDTAG_Localize:
-				localize = tag->ti_Data ? TRUE : FALSE;
+                localize = tag->ti_Data ? TRUE : FALSE;
                 break;
 
         }
     }
 
-	if (strlength <=0)
-	{
-		return IDERR_NOLENGTH;
-	}
+    if (strlength <=0)
+    {
+        return IDERR_NOLENGTH;
+    }
 
-	// return something to avoid crashes when function is called
-	if (manufstr)
-	{
-		strlcpy(manufstr, "Unknown", strlength);
-	}
-	if (prodstr)
-	{
-		strlcpy(prodstr, "Unknown", strlength);
-	}
-	if (classstr)
-	{
-		strlcpy(classstr, "Unknown", strlength);
-	}
-	if (expansion)
-	{
-		*expansion = NULL;
-	}
-	if (classid)
-	{
-		*classid = IDCID_UNKNOWN;
-	}
+    // return something to avoid crashes when function is called
+    if (manufstr)
+    {
+        strlcpy(manufstr, "Unknown", strlength);
+    }
+    if (prodstr)
+    {
+        strlcpy(prodstr, "Unknown", strlength);
+    }
+    if (classstr)
+    {
+        strlcpy(classstr, "Unknown", strlength);
+    }
+    if (expansion)
+    {
+        *expansion = NULL;
+    }
+    if (classid)
+    {
+        *classid = IDCID_UNKNOWN;
+    }
 
     return 0;
 
