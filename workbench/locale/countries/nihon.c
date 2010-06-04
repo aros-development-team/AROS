1b77f77ce3a6656dc456b860dd7701eb164e4adb
polluks <polluks@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-06-04 10:14:25 +0000

    Currency symbol
    git-svn-id: https://svn.aros.org/svn/aros/trunk@33453 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/locale/countries/nihon.c
+++ workbench/locale/countries/nihon.c
@@ -2,7 +2,7 @@
     Copyright © 1995-2005, The AROS Development Team. All rights reserved.
     $Id$
 
-    Desc: Country data for nihon
+    Desc: Country data for Japan
 */
 
 #include <libraries/locale.h>
@@ -51,12 +51,12 @@ struct CountryPrefs nihonPrefs =
     2, 2,
 
     /* Currency symbol, Small currency symbol */
-    "YEN", "",
+    "¥", "",
 
     /* Int CS, this is the ISO 4217 symbol, followed by the character to
        separate that symbol from the rest of the money. (\x00 for none).
     */
-    "YEN",
+    "JPY",
 
     /* Mon +ve sign, +ve space sep, +ve sign pos, +ve cs pos */
     "", SS_NOSPACE, SP_PREC_ALL, CSP_SUCCEEDS,
