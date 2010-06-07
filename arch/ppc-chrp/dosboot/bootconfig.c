e7a6c9d199680ddae144b3de285d479e6636aa69
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-06-07 09:15:44 +0000

    Removed arch-specific inithidds.c because of upcoming global changes, this code was obsolete anyway
    git-svn-id: https://svn.aros.org/svn/aros/trunk@33476 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/ppc-chrp/dosboot/bootconfig.c
@@ -0,0 +1,15 @@
+#include <aros/bootloader.h>
+#include <libraries/bootmenu.h>
+#include <string.h>
+
+#include "dosboot_intern.h"
+
+/* This file contains architecture-dependent defaults */
+
+void InitBootConfig(struct BootConfig *bootcfg, APTR BootLoaderBase)
+{
+    strcpy(bootcfg->defaultgfx.libname,  "radeon.hidd");
+    strcpy(bootcfg->defaultgfx.hiddname, "RadeonDriver");
+    bootcfg->defaultkbd.hiddname[0]    = 0;
+    bootcfg->defaultmouse.hiddname[0]  = 0;
+}
