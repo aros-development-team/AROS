a7fa64f0408a2932333b167147118ecef6af7d5f
mattrust <mrustler@gmx.de>
2010-06-07 12:32:05 +0000

    Added automatic opening of library.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@33483 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/popupmenu/staticlib/autoinit.c
@@ -0,0 +1,11 @@
+/*
+    Copyright © 2010, The AROS Development Team. 
+    All rights reserved.
+    
+    $Id: pm_exlst.c 33426 2010-06-01 18:36:55Z mazze $
+*/
+
+#include <proto/popupmenu.h>
+#include <aros/symbolsets.h>
+
+ADD2LIBS((CONST_STRPTR)"popupmenu.library",10, struct PopupMenuBase *, PopupMenuBase);
