3d159c8f311527b83a049c42fab57efb43062929
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-06-09 12:03:55 +0000

    Some definitions for AddDisplayDriverA()
    git-svn-id: https://svn.aros.org/svn/aros/trunk@33516 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/graphics/driver.h
@@ -0,0 +1,26 @@
+#ifndef GRAPHICS_DRIVER_H
+#define GRAPHICS_DRIVER_H
+
+/*
+    Copyright © 2010, The AROS Development Team. All rights reserved.
+    $Id:$
+
+    Desc: Display driver definitions.
+    Lang: english
+    
+    Information contained in this file is AROS-specific.
+*/
+
+/* Tags for AddDisplayDriverA() */
+
+#define DDRV_BootMode	(TAG_USER + 0x01)	/* BOOL  Boot mode driver which will be unloaded when any next driver comes in, default = FALSE */
+#define DDRV_MonitorID	(TAG_USER + 0x02)	/* ULONG Monitor ID for this driver, default = next available */
+#define DDRV_ReserveIDs	(TAG_USER + 0x03)	/* ULONG How many monitor IDs to reserve, default = 1 */
+
+/* Return codes */
+
+#define DD_OK	     0	/* No error */
+#define DD_NO_MEM    1	/* Out of memory */
+#define DD_ID_EXISTS 2	/* Specified MonitorID is already allocated */
+
+#endif
