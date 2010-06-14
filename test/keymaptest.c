45550d5fa5a4dd18eeda5b0fd6dc5b478e7e8aa6
Ball000 <olivier.tigreat@gmail.com>
2010-06-14 20:30:43 +0000

    Improved indentation, got rid of tabs. Allowed to exit when ESC is pressed.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@33599 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/keymaptest.c
+++ test/keymaptest.c
@@ -19,36 +19,35 @@ int main (int argc, char **argv)
     
     KeymapBase = OpenLibrary("keymap.library", 37);
     if (!KeymapBase)
-    	printf("Failed to open keymap.library v37\n");
+        printf("Failed to open keymap.library v37\n");
     else
     {
-    	IntuitionBase = (struct IntuitionBase *)OpenLibrary("intuition.library", 37);
-    	if (!IntuitionBase)
-    	    printf("Failed to open intuition.library v37\n");
-    	else
-    	{
-    	    struct Window *win;
-    	    
-    	    win = OpenWindowTags(NULL,
-    	    	WA_Width,	100,
-    	    	WA_Height,	60,
-    	    	WA_Title,	(IPTR)"Maprawkey test",
-    	    	WA_Flags,	WFLG_ACTIVATE,
-    	    	WA_IDCMP,	IDCMP_RAWKEY,
-    	    	TAG_DONE);
-    	    	
-    	    if (!win)
-    	    	printf("Could not open window\n");
-    	    else
-    	    {
-    	    
-    	    	HandleEvents(win, buffer, BUFSIZE);	
-    	    	
-    	    	CloseWindow(win);
-    	    }
-    	    CloseLibrary((struct Library *)IntuitionBase);
-    	}
-    	CloseLibrary(KeymapBase);
+        IntuitionBase = (struct IntuitionBase *)OpenLibrary("intuition.library", 37);
+        if (!IntuitionBase)
+            printf("Failed to open intuition.library v37\n");
+        else
+        {
+            struct Window *win;
+            
+            win = OpenWindowTags(NULL,
+                WA_Width,    200,
+                WA_Height,    60,
+                WA_Title,    (IPTR)"Maprawkey test (ESC to exit)",
+                WA_Flags,    WFLG_ACTIVATE,
+                WA_IDCMP,    IDCMP_RAWKEY,
+                TAG_DONE);
+                
+            if (!win)
+                printf("Could not open window\n");
+            else
+            {
+                HandleEvents(win, buffer, BUFSIZE);
+                
+                CloseWindow(win);
+            }
+            CloseLibrary((struct Library *)IntuitionBase);
+        }
+        CloseLibrary(KeymapBase);
     }
     return (0);
 }
@@ -61,56 +60,56 @@ VOID HandleEvents(struct Window *win, UBYTE *buffer, LONG bufsize)
     struct IntuiMessage *imsg;
     struct MsgPort *port = win->UserPort;
     BOOL terminated = FALSE;
+    WORD written;
     struct InputEvent ie ={0,};
     
     ie.ie_Class = IECLASS_RAWKEY;
-	
+    
     while (!terminated)
     {
-	if ((imsg = (struct IntuiMessage *)GetMsg(port)) != NULL)
-	{
-	    
-	    switch (imsg->Class)
-	    {
-		
-	    case IDCMP_REFRESHWINDOW:
-	    	BeginRefresh(win);
-	    	EndRefresh(win, TRUE);
-	    	break;
-	    	
-	    case IDCMP_RAWKEY: {
-	    	WORD written;
-	    	
-	        ie.ie_Code 	= imsg->Code;
-	        ie.ie_Qualifier	= imsg->Qualifier;
-	        printf("rawkey: code=$%04x, qual=$%04x\n",
-	        	ie.ie_Code, ie.ie_Qualifier);
-	        
-	        ie.ie_EventAddress =  imsg->IAddress;
-	        
-	        written = MapRawKey(&ie, buffer, bufsize, NULL);
-	        if (written == -1)
-	            printf("Buffer owerflow !!\n");
-	        else if (written)
-	        {
-	            printf("Map:");
-	            Write(Output(), buffer, written);
-	            printf("\n");
-	        }
-	    	
-	    } break;
-		    					
-	    } /* switch (imsg->Class) */
-	    ReplyMsg((struct Message *)imsg);
-	    
-	    			
-	} /* if ((imsg = GetMsg(port)) != NULL) */
-	else
-	{
-	    Wait(1L << port->mp_SigBit);
-	}
+        if ((imsg = (struct IntuiMessage *)GetMsg(port)) != NULL)
+        {
+            
+            switch (imsg->Class)
+            {
+            
+            case IDCMP_REFRESHWINDOW:
+                BeginRefresh(win);
+                EndRefresh(win, TRUE);
+                break;
+                
+            case IDCMP_RAWKEY:
+                ie.ie_Code     = imsg->Code;
+                ie.ie_Qualifier    = imsg->Qualifier;
+                printf("rawkey: code=$%04x, qual=$%04x\n",
+                    ie.ie_Code, ie.ie_Qualifier);
+                
+                ie.ie_EventAddress =  imsg->IAddress;
+                
+                written = MapRawKey(&ie, buffer, bufsize, NULL);
+                if (written == -1)
+                    printf("Buffer owerflow !!\n");
+                else if (written)
+                {
+                    printf("Map:");
+                    Write(Output(), buffer, written);
+                    printf("\n");
+                }
+                if (ie.ie_Code == 197)
+                    terminated = TRUE;
+                break;
+                                    
+            } /* switch (imsg->Class) */
+            ReplyMsg((struct Message *)imsg);
+            
+                        
+        } /* if ((imsg = GetMsg(port)) != NULL) */
+        else
+        {
+            Wait(1L << port->mp_SigBit);
+        }
     } /* while (!terminated) */
-	
+    
     return;
 } /* HandleEvents() */
 
