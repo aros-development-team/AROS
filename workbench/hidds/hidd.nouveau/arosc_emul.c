5b6b03c8417b1fbf4bce7427f1e39b0b664d2fe3
deadwood-pl <deadwood@wp.pl>
2010-06-16 18:28:06 +0000

    Clean up arosc emulation for nouveau.hidd. getenv always returns NULL now.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@33617 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/hidds/hidd.nouveau/arosc_emul.c
+++ workbench/hidds/hidd.nouveau/arosc_emul.c
@@ -15,12 +15,13 @@
 #include <errno.h>
 
 /* 
-    The purpose of this file is to provide implementation for C functions which are used by Mesa/Gallium
-    and are not located in librom.a
+    The purpose of this file is to provide implementation for C functions which 
+    are used by a module and are not located in librom.a
 
-    Mesa/Gallium cannot use arosc.library because arosc.library context is bound with caller task
-    and gets destroyed when the task exits. mesa.library at that point is still in RAM and
-    crashes at expunge.
+    
+    A module cannot use arosc.library because arosc.library context is bound 
+    with caller task and gets destroyed when the task exits. After the
+    caller task exists, the module becomes instable
 */
 
 #define IMPLEMENT()  bug("------IMPLEMENT(%s)\n", __func__)
@@ -128,16 +129,10 @@ copy:
 }
 
 char *getenv (const char *name)
-{ 
-    /* This function is not thread-safe */
-    static TEXT buff[128] = {NULL};
-    
-    D(bug("arosc_emul_getenv: %s\n", name));
-
-    if (GetVar(name, buff, 128, GVF_BINARY_VAR) == -1)
-        return NULL;
-    else
-        return buff;
+{
+    /* For nouveau.hidd don't allow reading ENV. Maybe in future change
+       to reading tool types? */
+    return NULL;
 }
 
 int gettimeofday (struct timeval * tv,struct timezone * tz)
@@ -151,78 +146,10 @@ void abort (void)
     IMPLEMENT();
 }
 
-void exit (int code)
-{
-    IMPLEMENT();
-}
-	
-int puts (const char * str)
-{
-    bug("%s\n", str);
-    return 1;
-}
-
-int putchar(int c)
-{
-    bug("%c", c);
-    return c;
-}
-
-char * strdup (const char * orig)
-{
-    char * copy;
-    char * ptr;
-
-    if ((copy = malloc (strlen (orig)+1)))
-    {
-	ptr = copy;
-
-	while ((*ptr ++ = *orig ++));
-    }
-
-    return copy;
-}
-	
-double atof (const char * str)
-{
-    return strtod (str, (char **)NULL);
-}
-
-int printf (const char * format, ...)
-{
-    IMPLEMENT();
-    return EOF;
-}
-
-
 /* File operations */
-FILE * fopen (const char * pathname, const char * mode)
-{
-    IMPLEMENT();
-    return NULL;
-}
-
-int fclose (FILE * stream)
-{
-    IMPLEMENT();
-    return EOF;
-}
-
-int fputc (int c, FILE * stream)
-{
-    IMPLEMENT();
-    return EOF;
-}
-
 int fputs (const char * str, FILE * stream)
 {
     IMPLEMENT();
-    return EOF;
-}
-
-int fgetc (FILE * stream)
-{
-    IMPLEMENT();;
     return EOF;
 }
 
@@ -238,30 +165,7 @@ int fflush (FILE * stream)
     return EOF;
 }
 
-size_t fread (void * buf, size_t size, size_t nblocks, FILE * stream)
-{
-    IMPLEMENT();
-    return 0;
-}
-
-int ferror (FILE * stream)
-{
-    IMPLEMENT();
-    return TRUE;
-}
-
-void clearerr (FILE * stream)
-{
-    IMPLEMENT();
-}
-
 int fprintf (FILE * fh, const char * format, ...)
-{
-    IMPLEMENT();
-    return 0;
-}
-
-int vfprintf (FILE * stream, const char * format, va_list args)
 {
     IMPLEMENT();
     return 0;
