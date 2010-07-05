ab9f9411d4be2088e29704247729f9b8532f2a7e
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-07-05 17:17:12 +0000

    Added own startup code to native PS/2 keyboard and mouse drivers
    git-svn-id: https://svn.aros.org/svn/aros/trunk@33754 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/i386-pc/drivers/keyboard/startup.c
@@ -0,0 +1,25 @@
+#include <aros/symbolsets.h>
+#include <hidd/keyboard.h>
+#include <proto/oop.h>
+
+#include LC_LIBDEFS_FILE
+
+static int init_kbd(LIBBASETYPEPTR LIBBASE)
+{
+    OOP_Object *kbd;
+    OOP_Object *drv = NULL;
+
+    kbd = OOP_NewObject(NULL, CLID_Hidd_Kbd, NULL);
+    if (kbd) {
+        drv = HIDD_Kbd_AddHardwareDriver(kbd, LIBBASE->ksd.kbdclass, NULL);
+	OOP_DisposeObject(kbd);
+    }
+
+    if (!drv)
+	return FALSE;
+
+    LIBBASE->library.lib_OpenCnt = 1;
+    return TRUE;
+}
+
+ADD2INITLIB(init_kbd, 10);
---
+++ arch/i386-pc/drivers/mouse.hidd/startup.c
@@ -0,0 +1,25 @@
+#include <aros/symbolsets.h>
+#include <hidd/mouse.h>
+#include <proto/oop.h>
+
+#include LC_LIBDEFS_FILE
+
+static int init_mouse(LIBBASETYPEPTR LIBBASE)
+{
+    OOP_Object *ms;
+    OOP_Object *drv = NULL;
+    
+    ms = OOP_NewObject(NULL, CLID_Hidd_Mouse, NULL);
+    if (ms) {
+        drv = HIDD_Mouse_AddHardwareDriver(ms, LIBBASE->msd.mouseclass, NULL);
+	OOP_DisposeObject(ms);
+    }
+
+    if (!drv)
+	return FALSE;
+
+    LIBBASE->library.lib_OpenCnt = 1;
+    return TRUE;
+}
+
+ADD2INITLIB(init_mouse, 10);
