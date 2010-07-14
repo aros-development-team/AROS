c8c3f1502c6c46ff4be7c50cf89b6219951bee21
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-07-14 10:52:17 +0000

    - Added an attribute to get driver name, getting class name does not work because of fakegfx.hidd. - Implemented handling driver names in BestCModeIDTagList()
    git-svn-id: https://svn.aros.org/svn/aros/trunk@33810 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/cgfx/bestcmodeidtaglist.c
+++ workbench/libs/cgfx/bestcmodeidtaglist.c
@@ -13,6 +13,27 @@
 
 #include "cybergraphics_intern.h"
 
+static STRPTR ids[] = {
+    NULL,
+    "CVision64",
+    "Piccolo",
+    "PicassoII",
+    "Spectrum",
+    "Domino",
+    "RetinaZ3",
+    "PiccoSD64",
+    "A2410",
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    "CVision3D",
+    "Inferno",
+    "PicassoIV"
+};
+
+#define MAX_ID 15
+
 /*****************************************************************************
 
     NAME */
@@ -31,7 +52,36 @@
 	by the taglist.
 
     INPUTS
-	tags - A pointer to a TagList
+	tags - An optional pointer to a TagList containint requirements
+	       for the display mode. Valid tags are:
+
+	  CYBRBIDTG_Depth (ULONG) - depth the returned ModeID must support.
+				    Defaults to 8.
+	  CYBRBIDTG_NominalWidth  (UWORD),
+	  CYBRBIDTG_NominalHeight (UWORD) - desired width and height for the
+                                            display mode.
+          CYBRBIDTG_MonitorID (ULONG) - Specify numeric driver ID to find only
+					modes belonging to this driver. Useful
+					for systems with several graphics cards.
+					Defined board IDs are:
+					  1 - CVision64
+					  2 - Piccolo
+					  3 - PicassoII
+					  4 - Spectrum
+					  5 - Domino
+					  6 - RetinaZ3
+					  7 - PiccoSD64
+                                          8 - A2410
+					 13 - CVision3D (V41)
+					 14 - Inferno   (V41)
+					 15 - PicassoIV (V41)
+					Note that this tag exists only for
+					compatibility with old software. New
+					programs should use CYBRIDTG_BoardName
+					tag instead.
+	CYBRBIDTG_BoardName (STRPTR) - Specify the driver name directly. For
+				       example, pass "CVision3D" to get a
+				       CyberVision64/3D display mode ID
 
     RESULT
 	Best matchind display mode ID or INVALID_ID if there is no match
@@ -41,12 +91,13 @@
     EXAMPLE
 
     BUGS
-	At the moment MonitorID and BoardName tags are not implemented
 
     SEE ALSO
 	graphics.library/BestModeIDA()
 
     INTERNALS
+	This functions relies on processing CYBRIDTG_BoardName tag by
+	graphics.library/BestModeIDA()
 
     HISTORY
 	27-11-96    digulla automatically created from
@@ -59,10 +110,11 @@
     const struct TagItem *tstate;
     struct TagItem *tag;
     struct TagItem modetags[] = {
-	{ BIDTAG_NominalWidth,	800	},
-	{ BIDTAG_NominalHeight,	600	},
-	{ BIDTAG_Depth,		8	},
-	{ TAG_DONE, 0UL }
+	{ BIDTAG_NominalWidth  , 800	},
+	{ BIDTAG_NominalHeight , 600	},
+	{ BIDTAG_Depth         , 8	},
+	{ CYBRBIDTG_BoardName  , 0	},
+	{ TAG_DONE             , 0	}
     };
       
     for (tstate = tags; (tag = NextTagItem(&tstate)); ) {
@@ -78,13 +130,17 @@
 	    case CYBRBIDTG_NominalHeight:
 	        modetags[1].ti_Data = tag->ti_Data;
 	    	break;
-/* FIXME: These two need to be implemented
+
 	    case CYBRBIDTG_MonitorID:
+		if (tag->ti_Data > MAX_ID)
+		    return INVALID_ID;
+		modetags[3].ti_Data = (IPTR)ids[tag->ti_Data];
 	    	break;
 
 	    case CYBRBIDTG_BoardName:
-	    	break;*/
-		
+		modetags[3].ti_Data = tag->ti_Data;
+	    	break;
+
 	    default:
 	    	D(bug("!!! UNKOWN ATTR PASSED TO BestCModeIDTagList(): %x !!!\n", tag->ti_Tag));
 		break;
