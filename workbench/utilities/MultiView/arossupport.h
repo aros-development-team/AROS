5b840d9e7dfc8c916dce15bbd249e9cbf5b7b4b2
polluks <polluks@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-07-16 11:04:41 +0000

    Update backport.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@33822 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/utilities/MultiView/arossupport.h
+++ workbench/utilities/MultiView/arossupport.h
@@ -9,6 +9,13 @@
 #ifndef __AROS__
 
 #include <exec/types.h>
+#define STM_SEARCH 18
+#define STM_SEARCH_NEXT 19
+#define STM_SEARCH_PREV 20
+#define PGA_NotifyBehaviour PGA_Dummy+30
+#define PG_BEHAVIOUR_NICE 1
+#define PDTA_DitherQuality DTA_Dummy+222
+#define PDTA_DestMode DTA_Dummy+251
 
 extern ULONG *FindMethod(ULONG *methods, ULONG searchmethodid);
 extern struct DTMethod *FindTriggerMethod(struct DTMethod *methods, STRPTR command, ULONG method);
