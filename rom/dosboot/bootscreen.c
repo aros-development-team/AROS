3eb42eb3859e9ad5b38af3706a7a7fdf7969b94b
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-07-22 06:56:06 +0000

    - Implemented "no boot media" screen, without picture for now - Do not use timer.device directly since we have Delay() - Boot menu screen always occupies the whole display
    git-svn-id: https://svn.aros.org/svn/aros/trunk@33880 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/dosboot/bootscreen.c
@@ -0,0 +1,50 @@
+#include <exec/libraries.h>
+#include <graphics/gfxbase.h>
+#include <graphics/modeid.h>
+#include <intuition/screens.h>
+#include <proto/exec.h>
+#include <proto/graphics.h>
+#include <proto/intuition.h>
+
+#include "dosboot_intern.h"
+
+struct Screen *NoBootMediaScreen(struct DOSBootBase *DOSBootBase)
+{
+    struct Screen *scr = NULL;
+
+    GfxBase = OpenLibrary("graphics.library", 36);
+    if (GfxBase) {
+	IntuitionBase = OpenLibrary("intuition.library", 36);
+	if (IntuitionBase) {
+	    /* The same as in menu.c/initScreen() */
+	    ULONG mode = BestModeID(BIDTAG_DesiredWidth, 640, BIDTAG_DesiredHeight, 480,
+				    BIDTAG_Depth, 4, TAG_DONE);
+
+	    if (mode != INVALID_ID) {
+		scr = OpenScreenTags(NULL, SA_DisplayID, mode, SA_Draggable, FALSE, 
+				     SA_Quiet, TRUE, TAG_DONE);
+		if (scr) {
+
+		    /* TODO: Display a picture here */
+		    SetAPen(&scr->RastPort, 1);
+		    Move(&scr->RastPort, 215, 120);
+		    Text(&scr->RastPort, "No bootable media found...", 26);
+
+		    return scr;
+		}
+	    }
+	    CloseLibrary(IntuitionBase);
+	}
+	CloseLibrary(GfxBase);
+    }
+    return NULL;
+}
+
+void CloseBootScreen(struct Screen *scr, struct DOSBootBase *DOSBootBase)
+{
+    if (scr) {
+	CloseScreen(scr);
+	CloseLibrary(&IntuitionBase->LibNode);
+	CloseLibrary(&GfxBase->LibNode);
+    }
+}
