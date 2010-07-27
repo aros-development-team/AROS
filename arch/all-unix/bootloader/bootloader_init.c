3a1b021f242e7adfd2d0804eea9d82ad1e74b263
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-07-27 18:05:09 +0000

    Removed duplicating code
    git-svn-id: https://svn.aros.org/svn/aros/trunk@33930 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-unix/bootloader/bootloader_init.c
+++ arch/all-unix/bootloader/bootloader_init.c
@@ -32,42 +32,40 @@ extern char *BootLoader_Name;
 static int GM_UNIQUENAME(Init)(LIBBASETYPEPTR BootLoaderBase)
 {
     D(bug("[Bootldr] Init\n"));
-    BootLoaderBase->Flags = 0;
-    
     NEWLIST(&(BootLoaderBase->Args));
+    NEWLIST(&(BootLoaderBase->DriveInfo));
 
     D(if (BootLoader_Name) bug("[BootLdr] Init: Loadername = %s\n",BootLoader_Name);)
     BootLoaderBase->LdrName = BootLoader_Name;
-    if (Kernel_Args) {
-	    STRPTR cmd,buff;
-	    ULONG temp;
-	    struct Node *node;
-	    ULONG len = strlen(Kernel_Args) + 1;	    
 
-	    D(bug("[BootLdr] Kernel arguments: %s\n", Kernel_Args));
-	    /* First make a working copy of the command line */
-	    if ((buff = AllocMem(len, MEMF_ANY|MEMF_CLEAR)))
-	    {
-		strcpy(buff,Kernel_Args);
-		/* remove any leading spaces */
-		cmd = stpblk(buff);
-		while(cmd[0])
-		{
-		    /* Split the command line */
-		    temp = strcspn(cmd," ");
-		    cmd[temp++] = 0x00;
-		    D(bug("[BootLdr] Init: Argument %s\n",cmd));
-		    /* Allocate node and insert into list */
-		    node = AllocMem(sizeof(struct Node),MEMF_ANY|MEMF_CLEAR);
-		    node->ln_Name = cmd;
-		    AddTail(&(BootLoaderBase->Args),node);
-		    /* Skip to next part */
-		    cmd = stpblk(cmd+temp);
-		}
-		
-		BootLoaderBase->Flags |= BL_FLAGS_CMDLINE;
-	    }
+    if (Kernel_Args) {
+	STRPTR cmd,buff;
+	ULONG temp;
+	struct Node *node;
+	ULONG len = strlen(Kernel_Args) + 1;	    
+
+	D(bug("[BootLdr] Kernel arguments: %s\n", Kernel_Args));
+	/* First make a working copy of the command line */
+	buff = AllocMem(len, MEMF_ANY|MEMF_CLEAR)
+	if (buff) {
+	    strcpy(buff,Kernel_Args);
+	    /* remove any leading spaces */
+	    cmd = stpblk(buff);
+	    while(cmd[0]) {
+		/* Split the command line */
+		temp = strcspn(cmd," ");
+		cmd[temp++] = 0x00;
+		D(bug("[BootLdr] Init: Argument %s\n",cmd));
+		/* Allocate node and insert into list */
+		node = AllocMem(sizeof(struct Node),MEMF_ANY|MEMF_CLEAR);
+		node->ln_Name = cmd;
+		AddTail(&(BootLoaderBase->Args),node);
+		/* Skip to next part */
+		cmd = stpblk(cmd+temp);
+	    }		
+	    BootLoaderBase->Flags |= BL_FLAGS_CMDLINE;
 	}
+    }
     return TRUE;
 }
 
