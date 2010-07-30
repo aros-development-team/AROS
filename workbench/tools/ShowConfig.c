ae1ab901cd32ba2a095f5e052355bdc2921fae0a
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-07-30 21:55:03 +0000

    Extend ShowConfig to show processor information.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@33975 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/tools/ShowConfig.c
+++ workbench/tools/ShowConfig.c
@@ -5,8 +5,12 @@
 #include <proto/bootloader.h>
 #include <proto/exec.h>
 #include <proto/utility.h>
+#include <proto/processor.h>
+#include <resources/processor.h>
 
 #include <stdio.h>
+
+APTR ProcessorBase = NULL;
 
 ULONG ExtUDivMod32(ULONG a, ULONG b, ULONG *mod)
 {
@@ -59,6 +63,95 @@ ULONG ComputeKBytes(APTR a, APTR b)
     return (ULONG)(result >> 10);
 }
 
+static ULONG GetProcessorsCount()
+{
+    ULONG count = 0;
+    struct TagItem tags [] = 
+    {
+        {GCIT_NumberOfProcessors, (IPTR)&count},
+        {TAG_DONE, TAG_DONE}
+    };
+
+    GetCPUInfo(tags);
+
+    return count;
+}
+
+struct
+{
+    ULONG Architecture;
+    STRPTR Description;
+} ProcessorArchitecture [] =
+{
+    { PROCESSORARCH_UNKNOWN, "Unknown" },
+    { PROCESSORARCH_M68K, "Motorola 68K" },
+    { PROCESSORARCH_PPC, "PowerPC" },
+    { PROCESSORARCH_X86, "X86" },
+    { PROCESSORARCH_ARM, "ARM" },
+    { 0, NULL }   
+};
+
+struct
+{
+    ULONG Endianness;
+    STRPTR Description;
+} CurrentEndianness [] =
+{
+    { ENDIANNESS_UNKNOWN, "Unknown" },
+    { ENDIANNESS_LE, "LE" },
+    { ENDIANNESS_BE, "BE" },
+    { 0, NULL}
+};
+
+static VOID PrintProcessorInformation()
+{
+    ULONG count = GetProcessorsCount();
+    ULONG i, j;
+    TEXT modelstring[128];
+    ULONG architecture, endianness;
+    STRPTR architecturestring, endiannessstring;
+    
+    for (i = 0; i < count; i++)
+    {
+        struct TagItem tags [] =
+        {
+            {GCIT_SelectedProcessor, i},
+            {GCIT_ModelString, (IPTR)modelstring},
+            {GCIT_Architecture, (IPTR)&architecture},
+            {GCIT_Endianness, (IPTR)&endianness},
+            {TAG_DONE, TAG_DONE}
+        };
+        
+        GetCPUInfo(tags);
+
+        j = 0;
+        while(ProcessorArchitecture[j].Description != NULL)
+        {
+            if (ProcessorArchitecture[j].Architecture == architecture)
+            {
+                architecturestring = ProcessorArchitecture[j].Description;
+                break;
+            }
+            j++;
+        }
+
+        j = 0;
+        while(CurrentEndianness[j].Description != NULL)
+        {
+            if (CurrentEndianness[j].Endianness == endianness)
+            {
+                endiannessstring = CurrentEndianness[j].Description;
+                break;
+            }
+            j++;
+        }       
+        
+        
+        printf("PROCESSOR %d:\t[%s/%s] %s\n", i + 1, 
+            architecturestring, endiannessstring, modelstring);
+    }
+}
+
 int __nocommandline;
 char __stdiowin[]="CON://700//ShowConfig/AUTO/CLOSE/WAIT";
 
@@ -70,7 +163,11 @@ int main()
     struct List *args;
     struct Node *n;
     
-    printf("VERS:\tAROS version %d.%d, Exec version %d.%d\n", ArosBase->lib_Version, ArosBase->lib_Revision,
+    ProcessorBase = OpenResource(PROCESSORNAME);
+    if (ProcessorBase)
+        PrintProcessorInformation();        
+    
+    printf("VERS:\t\tAROS version %d.%d, Exec version %d.%d\n", ArosBase->lib_Version, ArosBase->lib_Revision,
 	   SysBase->LibNode.lib_Version, SysBase->LibNode.lib_Revision);
     
     printf("RAM:");
@@ -81,7 +178,7 @@ int main()
             memtype = "CHIP";
         if (mh->mh_Attributes & MEMF_FAST)
             memtype = "FAST";
-        printf("\tNode Type 0x%X, Attributes 0x%X (%s), at %p-%p (", mh->mh_Node.ln_Type, mh->mh_Attributes, memtype, mh->mh_Lower, mh->mh_Upper);
+        printf("\t\tNode Type 0x%X, Attributes 0x%X (%s), at $%p-$%p (", mh->mh_Node.ln_Type, mh->mh_Attributes, memtype, mh->mh_Lower, mh->mh_Upper);
         PrintNum(ComputeKBytes(mh->mh_Lower, mh->mh_Upper));
         printf(")\n");
     }
@@ -91,15 +188,15 @@ int main()
 	bootldr = GetBootInfo(BL_LoaderName);
 
 	if (bootldr)
-    	    printf("BOOTLDR:%s\n", bootldr);
+    	    printf("BOOTLDR:\t%s\n", bootldr);
 
 	args = GetBootInfo(BL_Args);
 	if (args) {
-            printf("ARGS:\t");
+            printf("ARGS:\t\t");
             for (n = args->lh_Head; n->ln_Succ; n = n->ln_Succ) {
         	printf("%s ", n->ln_Name);
-        	printf("\n");
             }
+        	printf("\n");
 	}
     }
     return 0;
