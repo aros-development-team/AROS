a8a929d90dfd1cdc95f46b31941eca09b2afc518
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-07-30 21:50:37 +0000

    Add generic implementation of processor.resource
    git-svn-id: https://svn.aros.org/svn/aros/trunk@33971 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/resources/processor.h
@@ -0,0 +1,122 @@
+#ifndef RESOURCES_PROCESSOR_H
+#define RESOURCES_PROCESSOR_H
+
+/*
+    Copyright © 2010, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Tags and defines for processors information queries
+*/
+
+#ifndef UTILITY_TAGITEM_H
+#   include <utility/tagitem.h>
+#endif
+
+#define PROCESSORNAME "processor.resource"
+
+/* Tags */
+#define GCIT_NumberOfProcessors     (TAG_USER +   1)
+#define GCIT_NumberOfCPUs           GCIT_NumberOfProcessors
+#define GCIT_Family                 (TAG_USER +   2)
+/* #define GCIT_Model              (TAG_USER +   3) */
+#define GCIT_ModelString            (TAG_USER +   4)
+/* #define GCIT_Version            (TAG_USER +   5) */
+/* #define GCIT_VersionString      (TAG_USER +   6) */
+/* #define GCIT_FrontsideSpeed     (TAG_USER +   7) */
+/* #define GCIT_ProcessorSpeed     (TAG_USER +   8) */
+#define GCIT_L1CacheSize            (TAG_USER +   9)
+#define GCIT_L2CacheSize            (TAG_USER +  10)
+#define GCIT_L3CacheSize            (TAG_USER +  11)
+#define GCIT_VectorUnit             (TAG_USER +  12)
+/* #define GCIT_Extensions         (TAG_USER +  13) */
+#define GCIT_CacheLineSize          (TAG_USER +  14)
+/* #define GCIT_CPUPageSize        (TAG_USER +  15) */
+/* #define GCIT_ExecPageSize       (TAG_USER +  16) */
+/* #define GCIT_TimeBaseSpeed      (TAG_USER +  17) */
+#define GCIT_SelectedProcessor      (TAG_USER + 100)
+#define GCIT_FamilyString           (TAG_USER + 101)
+#define GCIT_L1DataCacheSize        (TAG_USER + 102)
+#define GCIT_L1InstructionCacheSize (TAG_USER + 103)
+#define GCIT_Architecture           (TAG_USER + 104)
+#define GCIT_Endianness             (TAG_USER + 105)
+
+/* Space [TAG_USER + 200, TAG_USER + 499] is reserved for boolean feature
+   tags. Do no introduce non boolean, non feature tags in this space. */
+#define GCIT_FeaturesBase           (TAG_USER + 200)
+#define GCIT_SupportsFPU            (GCIT_FeaturesBase +   1)
+#define GCIT_SupportsAltiVec        (GCIT_FeaturesBase +   2)
+#define GCIT_SupportsVMX            (GCIT_FeaturesBase +   3)
+#define GCIT_SupportsMMX            (GCIT_FeaturesBase +   4)
+#define GCIT_SupportsMMXEXT         (GCIT_FeaturesBase +   5)
+#define GCIT_Supports3DNOW          (GCIT_FeaturesBase +   6)
+#define GCIT_Supports3DNOWEXT       (GCIT_FeaturesBase +   7)
+#define GCIT_SupportsSSE            (GCIT_FeaturesBase +   8)
+#define GCIT_SupportsSSE2           (GCIT_FeaturesBase +   9)
+#define GCIT_SupportsSSE3           (GCIT_FeaturesBase +  10)
+#define GCIT_SupportsSSSE3          (GCIT_FeaturesBase +  11)
+#define GCIT_SupportsSSE41          (GCIT_FeaturesBase +  12)
+#define GCIT_SupportsSSE42          (GCIT_FeaturesBase +  13)
+#define GCIT_SupportsSSE4A          (GCIT_FeaturesBase +  14)
+#define GCIT_SupportsVME            (GCIT_FeaturesBase +  15)
+#define GCIT_SupportsPSE            (GCIT_FeaturesBase +  16)
+#define GCIT_SupportsPAE            (GCIT_FeaturesBase +  17)
+#define GCIT_SupportsCX8            (GCIT_FeaturesBase +  18)
+#define GCIT_SupportsAPIC           (GCIT_FeaturesBase +  19)
+#define GCIT_SupportsCMOV           (GCIT_FeaturesBase +  20)
+#define GCIT_SupportsPSE36          (GCIT_FeaturesBase +  21)
+#define GCIT_SupportsCLFSH          (GCIT_FeaturesBase +  22)
+#define GCIT_SupportsACPI           (GCIT_FeaturesBase +  23)
+#define GCIT_SupportsFXSR           (GCIT_FeaturesBase +  24)
+#define GCIT_SupportsHTT            (GCIT_FeaturesBase +  25)
+#define GCIT_SupportsCX16           (GCIT_FeaturesBase +  26)
+#define GCIT_SupportsVirtualization (GCIT_FeaturesBase +  27)
+#define GCIT_SupportsNoExecutionBit (GCIT_FeaturesBase +  28)
+#define GCIT_Supports64BitMode      (GCIT_FeaturesBase +  29)
+#define GCIT_FeaturesLast           (TAG_USER + 499)
+
+/* Processor family defines */
+#define CPUFAMILY_UNKNOWN             0
+#define CPUFAMILY_60X                 1
+#define CPUFAMILY_7X0                 2
+#define CPUFAMILY_74XX                3
+#define CPUFAMILY_4XX                 4
+#define CPUFAMILY_AMD_K5            100
+#define CPUFAMILY_AMD_K6            101
+#define CPUFAMILY_AMD_K7            102
+#define CPUFAMILY_AMD_K8            103
+#define CPUFAMILY_AMD_K9            104
+#define CPUFAMILY_AMD_K10           105
+#define CPUFAMILY_INTEL_486         106
+#define CPUFAMILY_INTEL_PENTIUM     107
+#define CPUFAMILY_INTEL_PENTIUM_PRO 108
+#define CPUFAMILY_INTEL_PENTIUM4    109
+
+/* Vector unit type */
+#define VECTORTYPE_NONE               0
+#define VECTORTYPE_ALTIVEC            1
+#define VECTORTYPE_VMX                2
+#define VECTORTYPE_MMX              100
+#define VECTORTYPE_MMXEXT           101
+#define VECTORTYPE_3DNOW            102
+#define VECTORTYPE_3DNOWEXT         103
+#define VECTORTYPE_SSE              104
+#define VECTORTYPE_SSE2             105
+#define VECTORTYPE_SSE3             106
+#define VECTORTYPE_SSSE3            107
+#define VECTORTYPE_SSE41            108
+#define VECTORTYPE_SSE42            109
+#define VECTORTYPE_SSE4A            110
+
+/* Processor architecture defines */
+#define PROCESSORARCH_UNKNOWN         0
+#define PROCESSORARCH_M68K            1
+#define PROCESSORARCH_PPC             2
+#define PROCESSORARCH_X86             3
+#define PROCESSORARCH_ARM             4
+
+/* Endianness */
+#define ENDIANNESS_UNKNOWN            0
+#define ENDIANNESS_LE                 1
+#define ENDIANNESS_BE                 2
+
+#endif /* EXEC_PROCESSORS_H */
---
+++ rom/processor/getcpuinfo.c
@@ -0,0 +1,198 @@
+/*
+    Copyright © 2010, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: GetCPUInfo() - Provides information about installed CPUs
+    Lang: english
+*/
+
+#define DEBUG 0
+
+#include <aros/debug.h>
+#include <exec/types.h>
+#include <aros/libcall.h>
+#include <proto/utility.h>
+#include <resources/processor.h>
+
+#include "processor_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/processor.h>
+
+	AROS_LH1(void, GetCPUInfo,
+
+/*  SYNOPSIS */
+	AROS_LHA(struct TagItem *, tagList, A0),
+
+/*  LOCATION */
+	struct ProcessorBase *, ProcessorBase, 1, Processor)
+
+/*  FUNCTION
+
+        Provides information about selected processor in the system
+    
+    INPUTS
+
+        Function takes an array of tags. Data is returned for each tag. See
+        specific tag description. There is a control tag CGIT_SelecetProcessor.
+
+    TAGS
+
+        GCIT_SelectedProcessor - (ULONG) When this tag is set correctly, information 
+                                 about choosen processor is provided. If this 
+                                 tag is missing or this tag has invalid value, 
+                                 information about first processor is returned.
+
+        GCIT_NumberOfProcessors - (ULONG *) Provides the number of processors 
+                                 present in the system.
+
+        GCIT_ModelString - (STRPTR) Fills in a passed buffer with model
+                           information. Buffer should be at least 128 bytes long.
+
+        GCIT_Family - (ULONG *) Provides designation of processor family using
+                      one of the CPUFAMILY_XXX values.
+
+        GCIT_FamilyString - (STRPTR) Fills in a passed buffer with family
+                            information. Buffer should be at least 64 bytes long.
+
+        GCIT_VectorUnit - (ULONG *) Provides designation of available vectory
+                          unit using one of the VECTORTYPE_XXX values.
+
+        GCIT_Architecture - (ULONG *) Provides designation of processor
+                            architecture using one of the PROCESSORARCH_XXX 
+                            values.
+
+        GCIT_Endianness - (ULONG *) Provides designation of current processor
+                          endianness using one of the ENDIANNESS_XXX values.
+
+        Cache sizes - (ULONG *) Following tags are used to retrieve size of 
+                      specified caches.
+                      
+                      GCIT_L1CacheSize
+                      GCIT_L1DataCacheSize
+                      GCIT_L1InstructionCacheSize
+                      GCIT_L2CacheSize
+                      GCIT_L3CacheSize
+                      
+                      Size is returned in kB.
+
+        GCIT_CacheLineSize - (ULONG *) Provides the size of cache line in bytes.
+                             In case these sizes differ per cache level, the
+                             smallest size if provided.
+
+        Features - (BOOL *) Following tags are used to check availability of
+                   certain features. The result is always a boolean.
+                   
+                   GCIT_SupportsFPU
+                   GCIT_SupportsAltiVec
+                   GCIT_SupportsVMX
+                   GCIT_SupportsMMX
+                   GCIT_SupportsMMXEXT
+                   GCIT_Supports3DNOW
+                   GCIT_Supports3DNOWEXT
+                   GCIT_SupportsSSE
+                   GCIT_SupportsSSE2
+                   GCIT_SupportsSSE3
+                   GCIT_SupportsSSSE3
+                   GCIT_SupportsSSE41
+                   GCIT_SupportsSSE42
+                   GCIT_SupportsSSE4A
+                   GCIT_SupportsVME
+                   GCIT_SupportsPSE
+                   GCIT_SupportsPAE
+                   GCIT_SupportsCX8
+                   GCIT_SupportsAPIC
+                   GCIT_SupportsCMOV
+                   GCIT_SupportsPSE36
+                   GCIT_SupportsCLFSH
+                   GCIT_SupportsACPI
+                   GCIT_SupportsFXSR
+                   GCIT_SupportsHTT
+                   GCIT_SupportsCX16
+                   GCIT_SupportsVirtualization
+                   GCIT_SupportsNoExecutionBit
+                   GCIT_Supports64BitMode
+
+    RESULT
+
+        None
+
+    NOTES
+    
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+******************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    struct TagItem * passedTag = NULL;
+
+    
+    /* This is the default implementation */
+        
+    while ((passedTag = NextTagItem((const struct TagItem **)&tagList)) != NULL)
+    {
+        if ((passedTag->ti_Tag > GCIT_FeaturesBase) &&
+            (passedTag->ti_Tag <= GCIT_FeaturesLast))
+        {
+            *((BOOL *)passedTag->ti_Data) = FALSE;
+            continue;
+        }
+        else
+        {
+        switch(passedTag->ti_Tag)
+        {
+        case(GCIT_NumberOfProcessors):
+            *((ULONG *)passedTag->ti_Data) = 1;
+            break;
+        case(GCIT_ModelString):
+            strcpy((STRPTR)passedTag->ti_Data, "processor.resource not implemented");
+            break;
+        case(GCIT_Family):
+            *((ULONG *)passedTag->ti_Data) = CPUFAMILY_UNKNOWN;
+            break;
+        case(GCIT_FamilyString):
+            strcpy((STRPTR)passedTag->ti_Data, "processor.resource not implemented");
+            break;
+        case(GCIT_VectorUnit):
+            *((ULONG *)passedTag->ti_Data) = VECTORTYPE_NONE;
+            break;
+        case(GCIT_L1CacheSize):
+            *((ULONG *)passedTag->ti_Data) = 0;
+            break;
+        case(GCIT_L1DataCacheSize):
+            *((ULONG *)passedTag->ti_Data) = 0;
+            break;
+        case(GCIT_L1InstructionCacheSize):
+            *((ULONG *)passedTag->ti_Data) = 0;
+            break;
+        case(GCIT_L2CacheSize):
+            *((ULONG *)passedTag->ti_Data) = 0;
+            break;
+        case(GCIT_L3CacheSize):
+            *((ULONG *)passedTag->ti_Data) = 0;
+            break;
+        case(GCIT_CacheLineSize):
+            *((ULONG *)passedTag->ti_Data) = 0;
+            break;
+        case(GCIT_Architecture):
+            *((ULONG *)passedTag->ti_Data) = PROCESSORARCH_UNKNOWN;
+            break;
+        case(GCIT_Endianness):
+            *((ULONG *)passedTag->ti_Data) = ENDIANNESS_UNKNOWN;
+            break;
+        }
+        }
+    }
+
+    AROS_LIBFUNC_EXIT
+} /* GetCPUInfo() */
+
