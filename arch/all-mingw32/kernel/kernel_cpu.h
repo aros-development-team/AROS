7d7000136fe4d616d0dd08eb2901ce289ca362c0
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-08-05 12:10:30 +0000

    Implemented user-defined CPU exception handlers
    git-svn-id: https://svn.aros.org/svn/aros/trunk@34046 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-mingw32/kernel/kernel_cpu.h
+++ arch/all-mingw32/kernel/kernel_cpu.h
@@ -1,4 +1,7 @@
-/* First include CPU-dependant definitions */
+#ifndef KERNEL_CPU_H_
+#define KERNEL_CPU_H_
+
+/* First include CPU-dependent definitions */
 
 #ifdef __i386__
 #include "cpu_i386.h"
@@ -15,3 +18,14 @@
 #define goSuper() 0
 #define goUser()
 #define goBack(mode)
+
+struct ExceptionTranslation
+{
+    unsigned long ExceptionCode; /* Windows exception code		       */
+    char	  AmigaTrap;	 /* m68k trap number for exec.library	       */
+    char	  CPUTrap;	 /* Native CPU trap number for kernel.resource */
+};
+
+extern struct ExceptionTranslation Traps[];
+
+#endif
