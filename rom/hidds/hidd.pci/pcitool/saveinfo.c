a7689b9770994d359969c7e77afc1b71ac4a2dbd
mattrust <mrustler@gmx.de>
2010-08-09 10:06:05 +0000

    Fix for a freeze which I had on "hosted" when I wanted to save PCI info to RAM-Disk while no entry was available. Lots of cleanup.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@34073 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/hidds/hidd.pci/pcitool/saveinfo.c
+++ rom/hidds/hidd.pci/pcitool/saveinfo.c
@@ -6,14 +6,18 @@
 
 #include "saveinfo.h"
 
+BPTR DeviceInfoFile;
+
 /*Save only the displayed PCI Information to RAM Disk*/
 void SaveToDisk(struct PCIInfo *DeviceInfo)
 {
-	DeviceInfoFile = Open( "RAM:PCIToolInfo.txt", MODE_NEWFILE );
+    DeviceInfoFile = Open( "RAM:PCIToolInfo.txt", MODE_NEWFILE );
+    if (DeviceInfoFile)
+    {
 	FPuts( DeviceInfoFile, "Driver Name: ");
 	FPuts( DeviceInfoFile, DeviceInfo->Driver_name );
 	FPuts( DeviceInfoFile, "\n" );
-	
+
 	FPuts( DeviceInfoFile, "Direct bus: ");
 	FPuts( DeviceInfoFile, DeviceInfo->Direct_bus );
 	FPuts( DeviceInfoFile, "\n" );
@@ -73,7 +77,7 @@ void SaveToDisk(struct PCIInfo *DeviceInfo)
 	FPuts( DeviceInfoFile, "IRQ: ");
 	FPuts( DeviceInfoFile, DeviceInfo->IRQ );
 	FPuts( DeviceInfoFile, "\n" );
-	
+
 	FPuts( DeviceInfoFile, "Status: ");
 	FPuts( DeviceInfoFile, DeviceInfo->Status );
 	FPuts( DeviceInfoFile, "\n" );
@@ -101,313 +105,169 @@ void SaveToDisk(struct PCIInfo *DeviceInfo)
 	FPuts( DeviceInfoFile, "RangeList_5: ");
 	FPuts( DeviceInfoFile, DeviceInfo->Rangelist_5 );
 	FPuts( DeviceInfoFile, "\n" );
-	
+
 	/*Clean up the Device Values*/
 	CleanUpPCIToolInfo(DeviceInfo);
 
 	Close( DeviceInfoFile );
-
-} 
+    }
+    else
+    {
+	// TODO: Requester
+	PutStr("Can't open file for output\n");
+    }
+}
 
 /*Just Open a new Textfile in RAM Disk. No writing yet*/
 BOOL OpenPCIInfoFile(void)
 {
-	DeviceInfoFile = Open( "RAM:PCIToolInfo.txt", MODE_NEWFILE );
+    DeviceInfoFile = Open( "RAM:PCIToolInfo.txt", MODE_NEWFILE );
 
-	if(DeviceInfoFile)
-	{
-		return TRUE;
-	}
-	else
-	{
-		return FALSE;
-	}
+    if(DeviceInfoFile)
+    {
+	return TRUE;
+    }
+    else
+    {
+	return FALSE;
+    }
 }
 
 /*Closes the file opened by OpenPCIInfoFile*/
 void ClosePCIInfoFile(void)
 {
-	if(DeviceInfoFile)
-	{
-		Close(DeviceInfoFile);
-	}
+    if(DeviceInfoFile)
+    {
+	Close(DeviceInfoFile);
+    }
 }
 
 /*Writes a set of PCI Information to the Opened file*/
 void WriteToPCIInfoFile(struct PCIInfo *DeviceInfo)
 {
-	if(DeviceInfoFile)
-	{
-		FPuts( DeviceInfoFile, "\n" );
-		FPuts( DeviceInfoFile, "Driver Name: ");
-		FPuts( DeviceInfoFile, DeviceInfo->Driver_name );
-		FPuts( DeviceInfoFile, "\n" );
-	
-		FPuts( DeviceInfoFile, "Direct bus: ");
-		FPuts( DeviceInfoFile, DeviceInfo->Direct_bus );
-		FPuts( DeviceInfoFile, "\n" );
+    if(DeviceInfoFile)
+    {
+	FPuts( DeviceInfoFile, "\n" );
+	FPuts( DeviceInfoFile, "Driver Name: ");
+	FPuts( DeviceInfoFile, DeviceInfo->Driver_name );
+	FPuts( DeviceInfoFile, "\n" );
 
-		FPuts( DeviceInfoFile, "Hardware info: ");
-		FPuts( DeviceInfoFile, DeviceInfo->Hardware_info );
-		FPuts( DeviceInfoFile, "\n" );
+	FPuts( DeviceInfoFile, "Direct bus: ");
+	FPuts( DeviceInfoFile, DeviceInfo->Direct_bus );
+	FPuts( DeviceInfoFile, "\n" );
 
-		FPuts( DeviceInfoFile, "Description: ");
-		FPuts( DeviceInfoFile, DeviceInfo->Description );
-		FPuts( DeviceInfoFile, "\n" );
+	FPuts( DeviceInfoFile, "Hardware info: ");
+	FPuts( DeviceInfoFile, DeviceInfo->Hardware_info );
+	FPuts( DeviceInfoFile, "\n" );
 
-		FPuts( DeviceInfoFile, "Vendor name: ");
-		FPuts( DeviceInfoFile, DeviceInfo->Vendor_name );
-		FPuts( DeviceInfoFile, "\n" );
+	FPuts( DeviceInfoFile, "Description: ");
+	FPuts( DeviceInfoFile, DeviceInfo->Description );
+	FPuts( DeviceInfoFile, "\n" );
 
-		FPuts( DeviceInfoFile, "Product name: ");
-		FPuts( DeviceInfoFile, DeviceInfo->Product_name );
-		FPuts( DeviceInfoFile, "\n" );
+	FPuts( DeviceInfoFile, "Vendor name: ");
+	FPuts( DeviceInfoFile, DeviceInfo->Vendor_name );
+	FPuts( DeviceInfoFile, "\n" );
 
-		FPuts( DeviceInfoFile, "Subsystem: ");
-		FPuts( DeviceInfoFile, DeviceInfo->Subsystem );
-		FPuts( DeviceInfoFile, "\n" );
+	FPuts( DeviceInfoFile, "Product name: ");
+	FPuts( DeviceInfoFile, DeviceInfo->Product_name );
+	FPuts( DeviceInfoFile, "\n" );
 
-		FPuts( DeviceInfoFile, "VendorID: ");
-		FPuts( DeviceInfoFile, DeviceInfo->VendorID );
-		FPuts( DeviceInfoFile, "\n" );
+	FPuts( DeviceInfoFile, "Subsystem: ");
+	FPuts( DeviceInfoFile, DeviceInfo->Subsystem );
+	FPuts( DeviceInfoFile, "\n" );
 
-		FPuts( DeviceInfoFile, "ProductID: ");
-		FPuts( DeviceInfoFile, DeviceInfo->ProductID );
-		FPuts( DeviceInfoFile, "\n" );
+	FPuts( DeviceInfoFile, "VendorID: ");
+	FPuts( DeviceInfoFile, DeviceInfo->VendorID );
+	FPuts( DeviceInfoFile, "\n" );
 
-		FPuts( DeviceInfoFile, "RevisionID: ");
-		FPuts( DeviceInfoFile, DeviceInfo->RevisionID );
-		FPuts( DeviceInfoFile, "\n" );
+	FPuts( DeviceInfoFile, "ProductID: ");
+	FPuts( DeviceInfoFile, DeviceInfo->ProductID );
+	FPuts( DeviceInfoFile, "\n" );
 
-		FPuts( DeviceInfoFile, "Class: ");
-		FPuts( DeviceInfoFile, DeviceInfo->Class );
-		FPuts( DeviceInfoFile, "\n" );
+	FPuts( DeviceInfoFile, "RevisionID: ");
+	FPuts( DeviceInfoFile, DeviceInfo->RevisionID );
+	FPuts( DeviceInfoFile, "\n" );
 
-		FPuts( DeviceInfoFile, "Subclass: ");
-		FPuts( DeviceInfoFile, DeviceInfo->Subclass );
-		FPuts( DeviceInfoFile, "\n" );
+	FPuts( DeviceInfoFile, "Class: ");
+	FPuts( DeviceInfoFile, DeviceInfo->Class );
+	FPuts( DeviceInfoFile, "\n" );
 
-		FPuts( DeviceInfoFile, "Interface: ");
-		FPuts( DeviceInfoFile, DeviceInfo->Interface );
-		FPuts( DeviceInfoFile, "\n" );
+	FPuts( DeviceInfoFile, "Subclass: ");
+	FPuts( DeviceInfoFile, DeviceInfo->Subclass );
+	FPuts( DeviceInfoFile, "\n" );
 
-		FPuts( DeviceInfoFile, "ROM Base: ");
-		FPuts( DeviceInfoFile, DeviceInfo->ROM_Base );
-		FPuts( DeviceInfoFile, "\n" );
+	FPuts( DeviceInfoFile, "Interface: ");
+	FPuts( DeviceInfoFile, DeviceInfo->Interface );
+	FPuts( DeviceInfoFile, "\n" );
 
-		FPuts( DeviceInfoFile, "ROM Size: ");
-		FPuts( DeviceInfoFile, DeviceInfo->ROM_Size );
-		FPuts( DeviceInfoFile, "\n" );
+	FPuts( DeviceInfoFile, "ROM Base: ");
+	FPuts( DeviceInfoFile, DeviceInfo->ROM_Base );
+	FPuts( DeviceInfoFile, "\n" );
 
-		FPuts( DeviceInfoFile, "IRQ: ");
-		FPuts( DeviceInfoFile, DeviceInfo->IRQ );
-		FPuts( DeviceInfoFile, "\n" );
-	
-		FPuts( DeviceInfoFile, "Status: ");
-		FPuts( DeviceInfoFile, DeviceInfo->Status );
-		FPuts( DeviceInfoFile, "\n" );
+	FPuts( DeviceInfoFile, "ROM Size: ");
+	FPuts( DeviceInfoFile, DeviceInfo->ROM_Size );
+	FPuts( DeviceInfoFile, "\n" );
 
-		FPuts( DeviceInfoFile, "RangeList_0: ");
-		FPuts( DeviceInfoFile, DeviceInfo->Rangelist_0 );
-		FPuts( DeviceInfoFile, "\n" );
+	FPuts( DeviceInfoFile, "IRQ: ");
+	FPuts( DeviceInfoFile, DeviceInfo->IRQ );
+	FPuts( DeviceInfoFile, "\n" );
 
-		FPuts( DeviceInfoFile, "RangeList_1: ");
-		FPuts( DeviceInfoFile, DeviceInfo->Rangelist_1 );
-		FPuts( DeviceInfoFile, "\n" );
+	FPuts( DeviceInfoFile, "Status: ");
+	FPuts( DeviceInfoFile, DeviceInfo->Status );
+	FPuts( DeviceInfoFile, "\n" );
 
-		FPuts( DeviceInfoFile, "RangeList_2: ");
-		FPuts( DeviceInfoFile, DeviceInfo->Rangelist_2 );
-		FPuts( DeviceInfoFile, "\n" );
+	FPuts( DeviceInfoFile, "RangeList_0: ");
+	FPuts( DeviceInfoFile, DeviceInfo->Rangelist_0 );
+	FPuts( DeviceInfoFile, "\n" );
 
-		FPuts( DeviceInfoFile, "RangeList_3: ");
-		FPuts( DeviceInfoFile, DeviceInfo->Rangelist_3 );
-		FPuts( DeviceInfoFile, "\n" );
+	FPuts( DeviceInfoFile, "RangeList_1: ");
+	FPuts( DeviceInfoFile, DeviceInfo->Rangelist_1 );
+	FPuts( DeviceInfoFile, "\n" );
 
-		FPuts( DeviceInfoFile, "RangeList_4: ");
-		FPuts( DeviceInfoFile, DeviceInfo->Rangelist_4 );
-		FPuts( DeviceInfoFile, "\n" );
+	FPuts( DeviceInfoFile, "RangeList_2: ");
+	FPuts( DeviceInfoFile, DeviceInfo->Rangelist_2 );
+	FPuts( DeviceInfoFile, "\n" );
 
-		FPuts( DeviceInfoFile, "RangeList_5: ");
-		FPuts( DeviceInfoFile, DeviceInfo->Rangelist_5 );
-		FPuts( DeviceInfoFile, "\n" );
-		FPuts( DeviceInfoFile, "\n" );
-		/*Clean up the Device Values*/
-		CleanUpPCIToolInfo(DeviceInfo);
-	}
+	FPuts( DeviceInfoFile, "RangeList_3: ");
+	FPuts( DeviceInfoFile, DeviceInfo->Rangelist_3 );
+	FPuts( DeviceInfoFile, "\n" );
 
+	FPuts( DeviceInfoFile, "RangeList_4: ");
+	FPuts( DeviceInfoFile, DeviceInfo->Rangelist_4 );
+	FPuts( DeviceInfoFile, "\n" );
+
+	FPuts( DeviceInfoFile, "RangeList_5: ");
+	FPuts( DeviceInfoFile, DeviceInfo->Rangelist_5 );
+	FPuts( DeviceInfoFile, "\n" );
+	FPuts( DeviceInfoFile, "\n" );
+	/*Clean up the Device Values*/
+	CleanUpPCIToolInfo(DeviceInfo);
+    }
 }
 
 void CleanUpPCIToolInfo(struct PCIInfo *DeviceInfo)
 {
-	
-
-	CleanIndex = 0;
-	while(CleanIndex < 200)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->Driver_name[CleanIndex] = ' ';
-	}
-
-	CleanIndex = 0;
-	while(CleanIndex < 200)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->Direct_bus[CleanIndex] = ' ';
-	}
-
-	CleanIndex = 0;
-	while(CleanIndex < 200)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->Hardware_info[CleanIndex] = ' ';
-	}
-
-	CleanIndex = 0;
-	while(CleanIndex < 200)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->Description[CleanIndex] = ' ';
-	}
-
-	CleanIndex = 0;
-	while(CleanIndex < 200)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->Vendor_name[CleanIndex] = ' ';
-	}
-
-	CleanIndex = 0;
-	while(CleanIndex < 200)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->Product_name[CleanIndex] = ' ';
-	}
-	
-	CleanIndex = 0;
-	while(CleanIndex < 200)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->Subsystem[CleanIndex] = ' ';
-	}
-
-	CleanIndex = 0;
-	while(CleanIndex < 10)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->VendorID[CleanIndex] = ' ';
-	}
-
-	CleanIndex = 0;
-	while(CleanIndex < 10)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->ProductID[CleanIndex] = ' ';
-	}
-
-	CleanIndex = 0;
-	while(CleanIndex < 10)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->RevisionID[CleanIndex] = ' ';
-	}
-
-	CleanIndex = 0;
-	while(CleanIndex < 200)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->Class[CleanIndex] = ' ';
-	}
-
-	CleanIndex = 0;
-	while(CleanIndex < 200)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->Subclass[CleanIndex] = ' ';
-	}
-
-	CleanIndex = 0;
-	while(CleanIndex < 200)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->Interface[CleanIndex] = ' ';
-	}
-
-	CleanIndex = 0;
-	while(CleanIndex < 10)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->ROM_Base[CleanIndex] = ' ';
-	}
-
-	CleanIndex = 0;
-	while(CleanIndex < 10)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->ROM_Size[CleanIndex] = ' ';
-	}
-
-	CleanIndex = 0;
-	while(CleanIndex < 10)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->IRQ[CleanIndex] = ' ';
-	}
-
-	CleanIndex = 0;
-	while(CleanIndex < 200)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->Status[CleanIndex] = ' ';
-	}
-
-	
-CleanIndex = 0;
-	while(CleanIndex < 200)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->Rangelist_0[CleanIndex] = ' ';
-	}
-
-	CleanIndex = 0;
-	while(CleanIndex < 200)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->Rangelist_1[CleanIndex] = ' ';
-	}
-
-	CleanIndex = 0;
-	while(CleanIndex < 200)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->Rangelist_2[CleanIndex] = ' ';
-	}
-
-CleanIndex = 0;
-	while(CleanIndex < 200)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->Rangelist_3[CleanIndex] = ' ';
-	}
-
-
-CleanIndex = 0;
-	while(CleanIndex < 200)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->Rangelist_4[CleanIndex] = ' ';
-	}
-
-CleanIndex = 0;
-	while(CleanIndex < 200)
-	{	
-		CleanIndex = CleanIndex + 1;
-		DeviceInfo->Rangelist_5[CleanIndex] = ' ';
-	}
-
-
-
-	
-	
-	
+    DeviceInfo->Driver_name[0] = '\0';
+    DeviceInfo->Direct_bus[0] = '\0';
+    DeviceInfo->Hardware_info[0] = '\0';
+    DeviceInfo->Description[0] = '\0';
+    DeviceInfo->Vendor_name[0] = '\0';
+    DeviceInfo->Product_name[0] = '\0';
+    DeviceInfo->Subsystem[0] = '\0';
+    DeviceInfo->VendorID[0] = '\0';
+    DeviceInfo->ProductID[0] = '\0';
+    DeviceInfo->RevisionID[0] = '\0';
+    DeviceInfo->Class[0] = '\0';
+    DeviceInfo->Subclass[0] = '\0';
+    DeviceInfo->Interface[0] = '\0';
+    DeviceInfo->ROM_Base[0] = '\0';
+    DeviceInfo->ROM_Size[0] = '\0';
+    DeviceInfo->IRQ[0] = '0';
+    DeviceInfo->Status[0] = '\0';
+    DeviceInfo->Rangelist_0[0] = '\0';
+    DeviceInfo->Rangelist_1[0] = '\0';
+    DeviceInfo->Rangelist_2[0] = '\0';
+    DeviceInfo->Rangelist_3[0] = '\0';
+    DeviceInfo->Rangelist_4[0] = '\0';
+    DeviceInfo->Rangelist_5[0] = '\0';
 }
-
--- rom/hidds/hidd.pci/pcitool/saveinfo.h
+++ rom/hidds/hidd.pci/pcitool/saveinfo.h
@@ -14,30 +14,29 @@
 /*Memory for the PCI Information*/
 struct PCIInfo
 {
-char Driver_name[200];
-char Direct_bus[200];
-char Hardware_info[200];
-char Description[200];
-char Vendor_name[200];
-char Product_name[200];
-char Subsystem[200];
-char VendorID[10];
-char ProductID[10];
-char RevisionID[10];
-char Class[200];
-char Subclass[200];
-char Interface[200];
-char ROM_Base[10];
-char ROM_Size[10];
-char IRQ[10];
-char Status[200];
-char Rangelist_0[200];
-char Rangelist_1[200];
-char Rangelist_2[200];
-char Rangelist_3[200];
-char Rangelist_4[200];
-char Rangelist_5[200];
-
+    char Driver_name[200];
+    char Direct_bus[200];
+    char Hardware_info[200];
+    char Description[200];
+    char Vendor_name[200];
+    char Product_name[200];
+    char Subsystem[200];
+    char VendorID[10];
+    char ProductID[10];
+    char RevisionID[10];
+    char Class[200];
+    char Subclass[200];
+    char Interface[200];
+    char ROM_Base[10];
+    char ROM_Size[10];
+    char IRQ[10];
+    char Status[200];
+    char Rangelist_0[200];
+    char Rangelist_1[200];
+    char Rangelist_2[200];
+    char Rangelist_3[200];
+    char Rangelist_4[200];
+    char Rangelist_5[200];
 };
 
  
@@ -46,15 +45,17 @@ void SaveToDisk(struct PCIInfo *DeviceInfo);
 
 /*Just Open a new Textfile in RAM Disk. No writing yet*/
 BOOL OpenPCIInfoFile(void);
-/*Writes a set of PCI Information to the Opened file*/
+
 void ClosePCIInfoFile(void);
 /*Closes the file opened by OpenPCIInfoFile*/
+
+/*Writes a set of PCI Information to the Opened file*/
 void WriteToPCIInfoFile(struct PCIInfo *DeviceInfo);
-/*File Handle*/
-BPTR DeviceInfoFile;
-/*clean up*/
-int CleanIndex;
+
 void CleanUpPCIToolInfo(struct PCIInfo *DeviceInfo);
 
+
+/*File Handle*/
+extern BPTR DeviceInfoFile;
 
 #endif /*SAVEINFO_H_*/
