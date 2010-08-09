dc8f1378b34d03867193ba1d033507342de190cd
polluks <polluks@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-08-09 16:18:37 +0000

    not TLD but licence plate number
    git-svn-id: https://svn.aros.org/svn/aros/trunk@34076 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/devs/keymaps/pc105_b.c
+++ workbench/devs/keymaps/pc105_b.c
@@ -9,7 +9,7 @@
 
 #include <devices/keymap.h>
 
-STATIC char  keymapname[] = "pc105_be";
+STATIC char  keymapname[] = "pc105_b";
 
 STATIC CONST UBYTE lokeymaptypes[];
 STATIC CONST IPTR  lokeymap[];
