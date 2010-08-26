fd23d499652a870e1c5c31a1b9f2ac1d2f711bfd
polluks <polluks@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-08-26 18:19:52 +0000

    Thailand
    git-svn-id: https://svn.aros.org/svn/aros/trunk@34274 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/locale/countries/thailand.c
+++ workbench/locale/countries/thailand.c
@@ -1,43 +1,35 @@
 /*
-    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2005, The AROS Development Team. All rights reserved.
     $Id$
 
-    Desc: Country data for Timor-Leste
+    Desc: Country data for Thailand
 */
 
-#include <exec/types.h>
 #include <libraries/locale.h>
-#include <libraries/iffparse.h>
 #include <prefs/locale.h>
 
-/* timor_leste.country: based on this file by João Ralha  */
-   
-struct CountryPrefs timor_lestePrefs =
+/* thailand.country: software.thai.net/locale/ */
+
+struct CountryPrefs thailandPrefs =
 {
     /* Reserved */
     { 0, 0, 0, 0 },
 
-    /* The country codes in the past have been rather inconsistant,
-       sometimes they are 1 character, 2 chars or 3. It would be nice
-       to have some consistency. Maybe use the 3 character name from
-       ISO 3166?
-    */
-
-    /* Country code, telephone code, measuring system */
-    MAKE_ID('T','L',0,0), 670, MS_ISO,
+    /* Country code (licence plate number), telephone code, measuring system */
+    MAKE_ID('T',0,0,0), 66, MS_ISO,
 
     /* Date time format, date format, time format */
-    "%A, %e de %B de %Y, %H:%M:%S",
-    "%A, %e de %B de %Y",
+    "%A, %e. %B %Y, %H:%M:%S",
+    "%A, %e. %B %Y",
     "%H:%M:%S",
 
     /* Short datetime, short date, short time formats */
-    "%e %b %Y, %H:%M:%S",
-    "%e %b %Y",
-    "%H:%M",
+    "%d.%m.%Y %H:%M:%S",
+    "%d.%m.%Y",
+    "%H:%M:%S",
 
     /* Decimal point, group separator, frac group separator */
-    ",", "'", "'",
+    ".", ",", " ",
 
     /* For grouping rules, see <libraries/locale.h> */
 
@@ -45,11 +37,10 @@ struct CountryPrefs timor_lestePrefs =
     { 3 }, { 3 },
 
     /* Mon dec pt, mon group sep, mon frac group sep */
-    ",", ".", ".",
-
+    ".", ",", "",
 
     /* Mon Grouping, Mon frac grouping */
-    { 3 }, { 3 },
+    { 3 }, { 0 },
 
     /* Mon Frac digits, Mon IntFrac digits, then number of digits in
        the fractional part of the money value. Most countries that
@@ -57,18 +48,18 @@ struct CountryPrefs timor_lestePrefs =
 
        (As would many of those you don't).
     */
-    2, 3,
+    2, 2,
 
     /* Currency symbol, Small currency symbol */
-    "$", "¢",
+    "Bt", "Satang",
 
     /* Int CS, this is the ISO 4217 symbol, followed by the character to
        separate that symbol from the rest of the money. (\x00 for none).
     */
-    "USD",
+    "THB",
 
     /* Mon +ve sign, +ve space sep, +ve sign pos, +ve cs pos */
-    "+", SS_SPACE, SP_PREC_ALL, CSP_PRECEDES,
+    "", SS_SPACE, SP_PREC_ALL, CSP_SUCCEEDS,
 
     /* Mon -ve sign, -ve space sep, -ve sign pos, -ve cs pos */
     "-", SS_SPACE, SP_PREC_ALL, CSP_SUCCEEDS,
