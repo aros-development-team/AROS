deeed49067b11a6ca540eea1a36da02505a6ec61
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-08-31 08:32:23 +0000

    - Block access to ./attbin - Removed more "not implemented" warnings
    git-svn-id: https://svn.aros.org/svn/aros/trunk@34309 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/signal.c
+++ compiler/clib/signal.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 2004, The AROS Development Team. All rights reserved.
+    Copyright © 2004-2010, The AROS Development Team. All rights reserved.
     $Id$
 */
 
@@ -40,7 +40,6 @@
 ******************************************************************************/
 {
 #   warning Implement signal()
-    AROS_FUNCTION_NOT_IMPLEMENTED("arosc");
 
     errno = ENOSYS;
     return (__sighandler_t *) -1;
