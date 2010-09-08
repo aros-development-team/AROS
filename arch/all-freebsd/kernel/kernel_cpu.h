3d69dda93b769bb14b54b55fe6bc0670658810ba
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-09-08 08:29:42 +0000

    sigcore.h rewritten to match the new code architecture
    git-svn-id: https://svn.aros.org/svn/aros/trunk@34371 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/all-freebsd/kernel/kernel_cpu.h
@@ -0,0 +1,24 @@
+#ifndef KERNEL_CPU_H_
+#define KERNEL_CPU_H_
+
+/* First include CPU-dependent definitions */
+
+#ifdef __i386__
+#include "cpu_i386.h"
+#endif
+
+/* This macro serves as an indicator of included file */
+#ifndef SAVEREGS
+#error Unsupported CPU
+#endif
+
+/* Some common definitions */
+#define SIZEOF_ALL_REGISTERS	(sizeof(struct AROSCPUContext))
+
+#define cpumode_t __unused char
+
+#define goSuper() 0
+#define goUser()
+#define goBack(mode)
+
+#endif
