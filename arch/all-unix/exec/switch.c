c2db63d6c571f259e20cff7009d955defd44a830
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-09-11 18:28:06 +0000

    Implemented KrnSwitch() on UNIX-hosted, unified Wait()
    git-svn-id: https://svn.aros.org/svn/aros/trunk@34404 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-unix/exec/switch.c
+++ arch/all-unix/exec/switch.c
@@ -17,18 +17,7 @@ AROS_LH0(void, Switch,
 {
     AROS_LIBFUNC_INIT
 
-    struct Task *this = SysBase->ThisTask;
+    KrnSwitch();
 
-    /*
-	If the state is not TS_RUN then the task is already in a list
-    */
-
-    Disable();
-    
-    if( this->tc_State != TS_RUN )
-	KrnDispatch();
-
-    Enable();
-    
     AROS_LIBFUNC_EXIT
 } /* Switch() */
