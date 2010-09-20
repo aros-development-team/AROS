bb3c0b4e42c036efca43a5595e95f6015c6bd894
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-09-20 13:10:04 +0000

    Removed obsolete Switch() function
    git-svn-id: https://svn.aros.org/svn/aros/trunk@34457 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/switch.c
+++ rom/exec/switch.c
@@ -6,38 +6,29 @@
     Lang: english
 */
 
-#include <aros/asmcall.h>
-
-#include "etask.h"
-#include "exec_intern.h"
+#include <exec/execbase.h>
 
 /*****************************************************************************
 
     NAME */
 #include <proto/exec.h>
 
-	AROS_LH0(void *, Switch,
+	AROS_LH0(void, Switch,
 
 /*  LOCATION */
 	struct ExecBase *, SysBase, 9, Exec)
 
 /*  FUNCTION
-	Notify exec.library about task switch
+	This function is obsolete and subject to removal.
+	On AmigaOS(tm) this was a private function.
 
     INPUTS
-	None.
 
     RESULT
-	A pointer to a CPU context storage area for current task.
 
     NOTES
-	In AmigaOS this was a private function. In AROS this function
-	should be called only from within kernel.resource's lowlevel task
-	switcher.
-	There's no practical sense in calling this function from within
-	any user software.
-
-	This code normally runs in supervisor mode.
+	This function is still there because i386-native port
+	still uses it.
 
     EXAMPLE
 
@@ -51,24 +42,6 @@
 ******************************************************************************/
 {
     AROS_LIBFUNC_INIT
-
-    struct Task *task = SysBase->ThisTask;
-
-    /* store IDNestCnt into tasks's structure */  
-    task->tc_IDNestCnt = SysBase->IDNestCnt;
-
-    /* Upon leaving supervisor mode interrupts will be enabled
-       if nothing changes in Dispatch() */
-    SysBase->IDNestCnt = -1;
-
-    /* TF_SWITCH flag set? Call the switch routine */
-    if (task->tc_Flags & TF_SWITCH)
-	AROS_UFC1(void, task->tc_Switch,
-		  AROS_UFCA(struct ExecBase *, SysBase, A6));
-
-    /* Return context storage area. The caller is now suggested to save
-       task's context there. */
-    return GetIntETask(task)->iet_Context;
 
     AROS_LIBFUNC_EXIT
 } /* Switch() */
