b35c7361f1fa1ba917f76bbdb69d87e75c25eaa5
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-09-22 09:55:58 +0000

    64-bit compatibility fix
    git-svn-id: https://svn.aros.org/svn/aros/trunk@34474 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/resources/filesysres.h
+++ compiler/include/resources/filesysres.h
@@ -25,7 +25,7 @@ struct FileSysEntry
     ULONG       fse_Version;
     ULONG       fse_PatchFlags;
     ULONG       fse_Type;
-    LONG        fse_Task;
+    IPTR        fse_Task;
     BPTR        fse_Lock;
     BSTR        fse_Handler;
     ULONG       fse_StackSize;
