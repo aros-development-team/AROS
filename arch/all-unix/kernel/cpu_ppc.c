02c79fa42a82275ca5e5b12e0cdb929bef450fdb
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-09-28 12:13:57 +0000

    Improved
    git-svn-id: https://svn.aros.org/svn/aros/trunk@34534 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-unix/kernel/cpu_ppc.c
+++ arch/all-unix/kernel/cpu_ppc.c
@@ -6,6 +6,6 @@ struct SignalTranslation sigs[] = {
     {SIGBUS   ,  2,  3},
     {SIGFPE   , 11,  7},
     {SIGSEGV  ,  2,  3},
-    {SIGSTKFLT,  3,  3},
+    {SIGSTKFLT, 14,  3},
     {-1       , -1, -1}
 };
