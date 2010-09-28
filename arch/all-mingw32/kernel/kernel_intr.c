d88102d84d881d016bbdc2223b93b4950eeee1dd
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-09-28 08:01:24 +0000

    Moved CPU context manipulation to a separate file, will simplify things in future
    git-svn-id: https://svn.aros.org/svn/aros/trunk@34528 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-mingw32/kernel/kernel_intr.c
+++ arch/all-mingw32/kernel/kernel_intr.c
@@ -1,10 +1,14 @@
+/*
+ * Architecture-specific interrupt processing. We reimplement
+ * core_ExitInterrupt() because of non-typical idle loop implementation,
+ * hence the file name.
+ */
+
 #include <exec/execbase.h>
 #include <hardware/intbits.h>
 #include <proto/exec.h>
 
 #include <inttypes.h>
-
-#include "etask.h"
 
 #include "kernel_base.h"
 #include "kernel_debug.h"
@@ -16,137 +20,10 @@
 #include "kernel_traps.h"
 
 #define D(x) /* This may lock up. See notes in host_intr.c */
-#define DEXCEPT(x)
-#define DSLEEP(x)
 #define DSC(x)
 #define DTRAP(x)
 
-#define Sleep_Mode   (*KernelIFace.SleepState)
-#define LastErrorPtr (*KernelIFace.LastErrorPtr)
-
 #define IRET return (SysBase->IDNestCnt < 0) ? INT_ENABLE : INT_DISABLE
-
-/*
- * User-mode part of exception handling. Save context, call
- * exec handler, then resume task.
- * Note that interrupts are disabled and SysBase->IDNestCnt contains 0.
- * Real IDNestCnt count for the task is stored in its tc_IDNestCnt.
- *
- * We have to do this complex trick with disabling interrupts because
- * in Windows exception handler operates on thread's stack, this means
- * we can't modify the stack inside exception handler, i.e. we can't save
- * the context on task's stack.
- *
- * In order to overcome this we forcibly disable interrupts in core_Dispatch()
- * and make the task to jump here. After this iet_Context still contains
- * unmodified saved task context. Since we're running normally on our stack,
- * we can save the context on the stack here.
- * Inside Exception() interrupts and task switching will be enabled, so original
- * IDNestCnt will be lost. In order to prevent it we save it on the stack too.
- *
- * When we're done we pick up saved IDNestCnt from stack and raise
- * AROS_EXCEPTION_RESUME in order to jump back to the saved context.
- */
-static void core_Exception()
-{
-    /* Save return context and IDNestCnt on stack */
-    struct Task *task = SysBase->ThisTask;
-    char nestCnt = task->tc_IDNestCnt;
-    struct AROSCPUContext save;
-
-    DEXCEPT(bug("[KRN] Entered exception, task 0x%p, IDNestCnt %d\n", task, SysBase->IDNestCnt));
-    /* Save original context */
-    CopyMem(GetIntETask(task)->iet_Context, &save, sizeof(struct AROSCPUContext));
-
-    /* Call exec exception processing */
-    Exception();
-
-    /* Restore saved task state and resume it. Note that interrupts are
-       disabled again here */
-    task->tc_IDNestCnt = nestCnt;
-    SysBase->IDNestCnt = nestCnt;
-
-    D(bug("[KRN] Leaving exception, IDNestCnt %d\n", SysBase->IDNestCnt));
-    KernelIFace.core_raise(AROS_EXCEPTION_RESUME, (IPTR)&save);
-}
-
-/* CPU-specific Switch() bits. Actually just context save. */
-static void cpu_Switch(CONTEXT *regs)
-{
-    struct Task *t = SysBase->ThisTask;
-    struct AROSCPUContext *ctx = GetIntETask(t)->iet_Context;
-
-    /* Actually save the context */
-    CopyMem(regs, ctx, sizeof(CONTEXT));
-    ctx->LastError = *LastErrorPtr;
-
-    /* Update tc_SPReg */
-    t->tc_SPReg = GET_SP(ctx);
-
-    core_Switch();
-}
-
-/*
- * CPU-dependent wrapper around core_Dispatch(). Implements
- * context restore, CPU idle loop, and task exceptions.
- */
-static void cpu_Dispatch(CONTEXT *regs)
-{
-    struct Task *task = core_Dispatch();
-    struct AROSCPUContext *ctx;
-
-    if (!task)
-    {
-	/*
-	 * There are no ready tasks and we need to go idle.
-	 * Because of the way how our emulation works, we do not
-	 * have a real loop here, unlike most ports. Instead we
-	 * signal idle state and exit. Then there can be two possibilities:
-	 * a) we are called by our virtual machine's supervisor thread.
-	 *    In this case it will just not resume usermode thread, and
-	 *    will go on with interrupts processing.
-	 * b) we are called by usermode thread using core_Rise(). In this
-	 *    case we will continue execution of the code and hit while(Sleep_Mode);
-	 *    spinlock in cire_Rise(). We will spin until supervisor thread interrupts
-	 *    us and actually puts us asleep.
-	 * We can't implement idle loop similar to other ports here because of (b)
-	 * case. We would just deadlock then since interrupt processing is actually
-	 * disabled during Windows exception processing (which is also an interrupt
-	 * for us).
-	 */
-        if (Sleep_Mode == SLEEP_MODE_OFF)
-	{
-	    /* This will enable interrupts in core_LeaveInterrupt() */
-	    SysBase->IDNestCnt = -1;
-            DSLEEP(bug("[KRN] TaskReady list empty. Sleeping for a while...\n"));
-
-            /* We are entering sleep mode */
-	    Sleep_Mode = SLEEP_MODE_PENDING;
-        }
-        return;
-    }
-
-    DSLEEP(if (Sleep_Mode != SLEEP_MODE_OFF) bug("[KRN] Exiting sleep mode\n");)
-    Sleep_Mode = SLEEP_MODE_OFF;
-
-    D(bug("[KRN] Dispatched task 0x%p (%s)\n", task, task->tc_Node.ln_Name));
-    /* Restore the task's context */
-    ctx = GetIntETask(task)->iet_Context;
-    CopyMem(ctx, regs, sizeof(CONTEXT));
-    *LastErrorPtr = ctx->LastError;
-
-    /* Handle exception if requested */
-    if (task->tc_Flags & TF_EXCEPT)
-    {
-        DEXCEPT(bug("[KRN] Exception requested for task 0x%p, return PC = 0x%p\n", task, GET_PC(ctx)));
-
-	/* Disable interrupts, otherwise we may lose saved context */
-	SysBase->IDNestCnt = 0;
-
-	/* Make the task to jump to exception handler */
-	PC(regs) = (IPTR)core_Exception;
-    }
-}
 
 /*
  * Leave the interrupt. This function receives the register frame used to leave the supervisor
