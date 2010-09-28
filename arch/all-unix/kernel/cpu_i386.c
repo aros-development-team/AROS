87b90136b69205f8ba0d840addd49acf990712b0
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-09-28 11:59:00 +0000

    Exception translation tables for x86 and PPC, drafts
    git-svn-id: https://svn.aros.org/svn/aros/trunk@34533 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/all-unix/kernel/cpu_i386.c
@@ -0,0 +1,11 @@
+#include <signal.h>
+
+struct SignalTranslation sigs[] = {
+    {SIGILL   ,  4,  6},
+    {SIGTRAP  ,  9,  1},
+    {SIGBUS   ,  2, 13},
+    {SIGFPE   , 11, 16},
+    {SIGSEGV  ,  2, 14},
+    {SIGSTKFLT,  3, 12},
+    {-1       , -1, -1}
+};
