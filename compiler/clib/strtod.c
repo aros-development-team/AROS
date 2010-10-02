ed6ff0a51fb55903f43be33cbaf3d35736e3df5b
deadwood-pl <deadwood@wp.pl>
2010-10-02 16:41:10 +0000

    Made strtod more compatible with regards to endptr bahaviour.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@34555 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/strtod.c
+++ compiler/clib/strtod.c
@@ -55,67 +55,88 @@
 
 ******************************************************************************/
 {
+    /* Unit tests available in : tests/clib/strtod.c */
 #warning TODO: implement NAN handling
-    double        val = 0, precision;
-    int           exp = 0;
-    char          c = 0, c2 = 0;
+    double  val = 0, precision;
+    int     exp = 0;
+    char    c = 0, c2 = 0;
+    int     digits = 0;      
 
     /* skip all leading spaces */
     while (isspace (*str))
-	str ++;
+        str ++;
 
     /* start with scanning the floting point number */
     if (*str)
     {
         /* Is there a sign? */
-	if (*str == '+' || *str == '-')
-	    c = *str ++;
+        if (*str == '+' || *str == '-')
+            c = *str ++;
 
         /* scan numbers before the dot */
-	while (isdigit(*str))
-	{
-	    val = val * 10 + (*str - '0');
-	    str ++;
-	}
+        while (isdigit(*str))
+        {
+            digits++;
+            val = val * 10 + (*str - '0');
+            str ++;
+        }
 
-        /* see if there is the dot */
-	if(*str == '.')
-	{
+        /* see if there is the dot and there were digits before it or there is 
+            at least one digit after it */
+        if ((*str == '.') && ((digits > 0) || (isdigit(*(str + 1)))))
+        {
             str++;
-	    /* scan the numbers behind the dot */
+            /* scan the numbers behind the dot */
             precision = 0.1;
-	    while (isdigit (*str))
-	    {
-		val += ((*str - '0') * precision) ;
-		str ++;
-		precision = precision * 0.1;
-	    }
-	}
+            while (isdigit (*str))
+            {
+                digits++;
+                val += ((*str - '0') * precision) ;
+                str ++;
+                precision = precision * 0.1;
+            }
+        }
 
-        /* look for a sequence like "E+10" or "e-22" */
-	if(tolower(*str) == 'e')
-	{
+        /* look for a sequence like "E+10" or "e-22" if there were any digits up to now */
+        if ((digits > 0) && (tolower(*str) == 'e'))
+        {
+            int edigits = 0;
             str++;
-        
-	    if (*str == '+' || *str == '-')
-	        c2 = *str ++;
 
-	    while (isdigit (*str))
-	    {
-		exp = exp * 10 + (*str - '0');
-		str ++;
-	    }
-	    if (c2 == '-')
-	        exp = -exp;
-	    val *= pow (10, exp);
-	}
+            if (*str == '+' || *str == '-')
+                c2 = *str ++;
 
-	if (c == '-')
-	    val = -val;
+            while (isdigit (*str))
+            {
+                edigits++;
+                exp = exp * 10 + (*str - '0');
+                str ++;
+            }
+            
+            if (c2 == '-')
+                exp = -exp;
+            
+            if (edigits == 0) 
+            {
+                /* there were no digits after 'e' - rollback pointer */
+                str--; if (c2 != 0) str--;
+            }
+            
+            val *= pow (10, exp);
+        }
+
+        if (c == '-')
+            val = -val;
+
+        if ((digits == 0) && (c != 0))
+        {
+            /* there were no digits but there was sign - rollback pointer */
+            str--;
+        }
     }
 
     if (endptr)
-	*endptr = (char *)str;
+        *endptr = (char *)str;
 
     return val;
 } /* strtod */
