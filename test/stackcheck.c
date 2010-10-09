6c4262e404f9d031523575c99d21cc50927c1d64
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-10-09 07:21:59 +0000

    Added stack checking test
    git-svn-id: https://svn.aros.org/svn/aros/trunk@34628 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/stackcheck.c
@@ -0,0 +1,29 @@
+#include <exec/tasks.h>
+#include <proto/dos.h>
+#include <proto/exec.h>
+
+#include <stdio.h>
+
+int __nocommandline = 1;
+
+int main(void)
+{
+    int i;
+    struct Task *t = FindTask(NULL);
+
+    printf("Attempting weird suicide...\n");
+
+    /* Simulate runaway stack */
+    t->tc_SPLower = (APTR)10;
+    t->tc_SPUpper = (APTR)16;
+
+    for (i = 1; i < 6; i++)
+    {
+	printf("%d...\n", i);
+	/* Delay() should eventually end up in core_Dispatch() which should kill us */
+	Delay(50);
+    }
+
+    printf("Stack checking FAILED! GO FIX IT!\n");
+    return 0;
+}
