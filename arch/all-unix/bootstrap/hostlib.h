a15f76d84fd64600fe0299d9c06fb819e3983079
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-10-13 12:07:42 +0000

    UNIX-hosted bootstrap builds again
    git-svn-id: https://svn.aros.org/svn/aros/trunk@34672 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-unix/bootstrap/hostlib.h
+++ arch/all-unix/bootstrap/hostlib.h
@@ -1,5 +1,3 @@
-void *Host_HostLib_Open(const char *filename);
-int Host_HostLib_Close(void *handle);
-char *Host_HostLib_GetErrorStr(void);
-void *Host_HostLib_GetPointer(void *handle, const char *symbol);
-unsigned long Host_HostLib_GetInterface(void *handle, char **names, void **funcs);
+void *Host_HostLib_Open(const char *filename, char **error);
+int Host_HostLib_Close(void *handle, char **error);
+void *Host_HostLib_GetPointer(void *handle, const char *symbol, char **error);
