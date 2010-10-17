fad84a293b2bd04c614027137d5627688bc0c3e1
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-10-17 05:33:46 +0000

    arch/m68k-all: Flesh out the m68k common kernel routines
    Common routines that would be needed by all ports.
    
    Of special interest is the arch/m68k-all/include/gencall.c
    helper, which generates the GCC glue macros that
    (hopefully) will get us a working native library API
    without too many GCC patches.
    
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@34791 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/m68k-all/exec/getcc.c
@@ -0,0 +1,61 @@
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: GetCC() - Read the CPU condition codes in an easy way.
+    Lang: english
+*/
+
+#include <aros/debug.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/exec.h>
+
+	AROS_LH0(UWORD, GetCC,
+
+/*  LOCATION */
+	struct ExecBase *, SysBase, 88, Exec)
+
+/*  FUNCTION
+	Read the contents of the CPU condition code register in a system
+	independant way. The flags return will be in the same format as
+	the Motorola MC680x0 family of microprocessors.
+
+    INPUTS
+	None.
+
+    RESULT
+	The CPU condition codes or ~0ul if this function has not been
+	implemented.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+	This function may not be implemented on platforms other than
+	Motorola mc680x0 processors.
+
+    SEE ALSO
+	SetSR()
+
+    INTERNALS
+
+******************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    UWORD sr;
+
+    /*  As with SetSR() you can either do nothing, or alternatively read
+	you own registers and assemble them into the form of the MC680x0
+	condition codes.
+     */
+    asm volatile ( "move.w %%sr,%0\n" : "=g" (sr) : : );
+
+    return sr;
+
+    AROS_LIBFUNC_EXIT
+} /* GetCC() */
