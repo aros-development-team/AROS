fd040bf3f3e18dcab75c7da137211b78d305937b
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-10-17 15:11:32 +0000

    test/clib: Add test for mnt_names
    This is a regression test for mnt_names, if we desire to change
    its implementation in the future.
    
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@34806 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/clib/mnt_names.c
@@ -0,0 +1,27 @@
+#include <proto/dos.h>
+#include <dos/dos.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/fs_types.h>	/* mnt_names is defined here */
+#include "test.h"
+
+int main() 
+{
+    TEST(strcmp(mnt_names[MOUNT_NONE], "none") == 0);
+    TEST(strcmp(mnt_names[MOUNT_UFS], "ufs") == 0);
+    TEST(strcmp(mnt_names[MOUNT_NFS], "nfs") == 0);
+    TEST(strcmp(mnt_names[MOUNT_MFS], "mfs") == 0);
+    TEST(strcmp(mnt_names[MOUNT_PC], "pc") == 0);
+    TEST(strcmp(mnt_names[MOUNT_ADOS_OFS], "ofs") == 0);
+    TEST(strcmp(mnt_names[MOUNT_ADOS_FFS], "ffs") == 0);
+    TEST(strcmp(mnt_names[MOUNT_ADOS_IOFS], "iofs") == 0);
+    TEST(strcmp(mnt_names[MOUNT_ADOS_IFFS], "iffs") == 0);
+  
+    return OK;
+}
+
+void cleanup() 
+{
+}
