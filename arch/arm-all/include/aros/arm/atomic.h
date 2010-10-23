e714fc640dee62bae40c325c424bab64976b685d
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-10-23 15:28:06 +0000

    ARM atomics fixed
    git-svn-id: https://svn.aros.org/svn/aros/trunk@34895 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-all/include/aros/arm/atomic.h
+++ arch/arm-all/include/aros/arm/atomic.h
@@ -12,27 +12,27 @@
 do { \
    unsigned long temp; \
    int result; \
-   __asm__ __volatile__("\n1: ldrexb %0, [%2]; add %0, %0, #1; strexb %1, %0, [%2]; teq %1, #0; bne 1b" \
-		   	   	   	   	   :"=&r"(result), "=&r"(temp) \
-		   	   	   	   	   :"r"(var) \
+   __asm__ __volatile__("\n1: ldrexb %0, [%3]; add %0, %0, #1; strexb %1, %0, [%3]; teq %1, #0; bne 1b" \
+		   	   	   	   	   :"=&r"(result), "=&r"(temp), "+Qo"(var) \
+		   	   	   	   	   :"r"(&var) \
 		   	   	   	   	   :"cc"); \
 } while (0)
 #define __AROS_ATOMIC_INC_W(var) \
 do { \
    unsigned long temp; \
    int result; \
-   __asm__ __volatile__("\n1: ldrexh %0, [%2]; add %0, %0, #1; strexh %1, %0, [%2]; teq %1, #0; bne 1b" \
-		   	   	   	   	   :"=&r"(result), "=&r"(temp) \
-		   	   	   	   	   :"r"(var) \
+   __asm__ __volatile__("\n1: ldrexh %0, [%3]; add %0, %0, #1; strexh %1, %0, [%3]; teq %1, #0; bne 1b" \
+		   	   	   	   	   :"=&r"(result), "=&r"(temp), "+Qo"(var) \
+		   	   	   	   	   :"r"(&var) \
 		   	   	   	   	   :"cc"); \
 } while (0)
 #define __AROS_ATOMIC_INC_L(var) \
 do { \
    unsigned long temp; \
    int result; \
-   __asm__ __volatile__("\n1: ldrex %0, [%2]; add %0, %0, #1; strex %1, %0, [%2]; teq %1, #0; bne 1b" \
-		   	   	   	   	   :"=&r"(result), "=&r"(temp) \
-		   	   	   	   	   :"r"(var) \
+   __asm__ __volatile__("\n1: ldrex %0, [%3]; add %0, %0, #1; strex %1, %0, [%3]; teq %1, #0; bne 1b" \
+		   	   	   	   	   :"=&r"(result), "=&r"(temp), "+Qo"(var) \
+		   	   	   	   	   :"r"(&var) \
 		   	   	   	   	   :"cc"); \
 } while (0)
 
@@ -40,9 +40,9 @@ do { \
 do { \
    unsigned long temp; \
    int result; \
-   __asm__ __volatile__("\n1: ldrexb %0, [%2]; sub %0, %0, #1; strexb %1, %0, [%2]; teq %1, #0; bne 1b" \
-		   	   	   	   	   :"=&r"(result), "=&r"(temp) \
-		   	   	   	   	   :"r"(var) \
+   __asm__ __volatile__("\n1: ldrexb %0, [%3]; sub %0, %0, #1; strexb %1, %0, [%3]; teq %1, #0; bne 1b" \
+		   	   	   	   	   :"=&r"(result), "=&r"(temp), "+Qo"(var) \
+		   	   	   	   	   :"r"(&var) \
 		   	   	   	   	   :"cc"); \
 } while (0)
 #define __AROS_ATOMIC_DEC_W(var) \
@@ -51,66 +51,66 @@ do { \
    int result; \
    __asm__ __volatile__("\n1: ldrexh %0, [%2]; sub %0, %0, #1; strexh %1, %0, [%2]; teq %1, #0; bne 1b" \
 		   	   	   	   	   :"=&r"(result), "=&r"(temp) \
-		   	   	   	   	   :"r"(var) \
+		   	   	   	   	   :"r"(&var) \
 		   	   	   	   	   :"cc"); \
 } while (0)
 #define __AROS_ATOMIC_DEC_L(var) \
 do { \
    unsigned long temp; \
    int result; \
-   __asm__ __volatile__("\n1: ldrex %0, [%2]; sub %0, %0, #1; strex %1, %0, [%2]; teq %1, #0; bne 1b" \
-		   	   	   	   	   :"=&r"(result), "=&r"(temp) \
-		   	   	   	   	   :"r"(var) \
+   __asm__ __volatile__("\n1: ldrex %0, [%3]; sub %0, %0, #1; strex %1, %0, [%3]; teq %1, #0; bne 1b" \
+		   	   	   	   	   :"=&r"(result), "=&r"(temp), "+Qo"(var) \
+		   	   	   	   	   :"r"(&var) \
 		   	   	   	   	   :"cc"); \
 } while (0)
 
 #define __AROS_ATOMIC_AND_B(var, mask) \
 do { \
     unsigned long temp; int result; \
-    __asm__ __volatile__("\n1: ldrexb %0, [%2]; and %0, %0, %3; strexb %1, %0, [%2]; teq %1, #0; bne 1b" \
-                        :"=&r"(result), "=&r"(temp) \
-                        :"r"(var), "I"(mask) \
+    __asm__ __volatile__("\n1: ldrexb %0, [%3]; and %0, %0, %4; strexb %1, %0, [%3]; teq %1, #0; bne 1b" \
+                        :"=&r"(result), "=&r"(temp), "+Qo"(var) \
+                        :"r"(&var), "I"(mask) \
                         :"cc"); \
 } while(0)
 #define __AROS_ATOMIC_AND_W(var, mask) \
 do { \
     unsigned long temp; int result; \
-    __asm__ __volatile__("\n1: ldrexh %0, [%2]; and %0, %0, %3; strexh %1, %0, [%2]; teq %1, #0; bne 1b" \
-                        :"=&r"(result), "=&r"(temp) \
-                        :"r"(var), "Ir"(mask) \
+    __asm__ __volatile__("\n1: ldrexh %0, [%3]; and %0, %0, %4; strexh %1, %0, [%3]; teq %1, #0; bne 1b" \
+                        :"=&r"(result), "=&r"(temp), "+Qo"(var) \
+                        :"r"(&var), "Ir"(mask) \
                         :"cc"); \
 } while(0)
 #define __AROS_ATOMIC_AND_L(var, mask) \
 do { \
     unsigned long temp; int result; \
-    __asm__ __volatile__("\n1: ldrex %0, [%2]; and %0, %0, %3; strex %1, %0, [%2]; teq %1, #0; bne 1b" \
-                        :"=&r"(result), "=&r"(temp) \
-                        :"r"(var), "Ir"(mask) \
+    __asm__ __volatile__("\n1: ldrex %0, [%3]; and %0, %0, %4; strex %1, %0, [%3]; teq %1, #0; bne 1b" \
+                        :"=&r"(result), "=&r"(temp), "+Qo"(var) \
+                        :"r"(&var), "Ir"(mask) \
                         :"cc"); \
 } while(0)
 
 #define __AROS_ATOMIC_OR_B(var, mask) \
 do { \
     unsigned long temp; int result; \
-    __asm__ __volatile__("\n1: ldrexb %0, [%2]; or %0, %0, %3; strexb %1, %0, [%2]; teq %1, #0; bne 1b" \
-                        :"=&r"(result), "=&r"(temp) \
-                        :"r"(var), "I"(mask) \
+    __asm__ __volatile__("\n1: ldrexb %0, [%3]; or %0, %0, %4; strexb %1, %0, [%3]; teq %1, #0; bne 1b" \
+                        :"=&r"(result), "=&r"(temp), "+Qo"(var) \
+                        :"r"(&var), "I"(mask) \
                         :"cc"); \
 } while(0)
 #define __AROS_ATOMIC_OR_W(var, mask) \
     do { \
     unsigned long temp; int result; \
-    __asm__ __volatile__("\n1: ldrexh %0, [%2]; or %0, %0, %3; strexh %1, %0, [%2]; teq %1, #0; bne 1b" \
-                        :"=&r"(result), "=&r"(temp) \
-                        :"r"(var), "Ir"(mask) \
+    __asm__ __volatile__("\n1: ldrexh %0, [%3]; or %0, %0, %4; strexh %1, %0, [%3]; teq %1, #0; bne 1b" \
+                        :"=&r"(result), "=&r"(temp), "+Qo"(var) \
+                        :"r"(&var), "Ir"(mask) \
                         :"cc"); \
 } while(0)
 #define __AROS_ATOMIC_OR_L(var, mask) \
 do { \
     unsigned long temp; int result; \
-    __asm__ __volatile__("\n1: ldrex %0, [%2]; or %0, %0, %3; strex %1, %0, [%2]; teq %1, #0; bne 1b" \
-                        :"=&r"(result), "=&r"(temp) \
-                        :"r"(var), "Ir"(mask) \
+    __asm__ __volatile__("\n1: ldrex %0, [%3]; or %0, %0, %4; strex %1, %0, [%3]; teq %1, #0; bne 1b" \
+                        :"=&r"(result), "=&r"(temp), "+Qo"(var) \
+                        :"r"(&var), "Ir"(mask) \
                         :"cc"); \
 } while(0)
 
