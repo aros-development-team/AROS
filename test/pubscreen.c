9b9c6eb14fd069230605ddae53d0393e3ae200f8
mattrust <mrustler@gmx.de>
2010-10-26 22:54:22 +0000

    Added a test for a Pubscreen. My goal was to have a playground for applications which want to open a window on a Pubscreen, but it turned out that AROS has some issues with the Pubscreen feature.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@34973 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/pubscreen.c
@@ -0,0 +1,127 @@
+#include <proto/intuition.h>
+#include <proto/dos.h>
+#include <proto/exec.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+struct Screen *myscreen;
+struct Window *mywindow;
+BYTE signalnum;
+
+void clean_exit(STRPTR txt)
+{
+    if (mywindow) CloseWindow(mywindow);
+    if (myscreen) CloseScreen(myscreen);
+    FreeSignal(signalnum);
+
+    if (txt)
+    {
+        PutStr(txt);
+        exit(RETURN_FAIL);
+    }
+    exit(RETURN_OK);
+}
+
+int main(void)
+{
+    BOOL done = FALSE;
+    BOOL closewindow = FALSE;
+    ULONG signals, winsig, pubsig;
+    struct IntuiMessage *message;
+
+    if ((signalnum = AllocSignal(-1)) == -1)
+    {
+        clean_exit("Can't allocate signal\n");
+    }
+
+    if
+    (
+        (
+            myscreen = OpenScreenTags
+            (
+                NULL,
+                SA_PubName, "MYPUBSCREEN",
+                SA_PubSig, signalnum,
+                SA_LikeWorkbench, TRUE,
+                SA_Title, "Pubscreenname: MYPUBSCREEN",
+                TAG_DONE
+            )
+        ) == NULL
+    )
+    {
+        clean_exit("Can't open screen\n");
+    }
+
+    if
+    (
+        (
+            mywindow = OpenWindowTags
+            (
+                NULL,
+                WA_Left, 30,
+                WA_Top, 30,
+                WA_Width, 250,
+                WA_Height, 100,
+                WA_DragBar, TRUE,
+                WA_DepthGadget, TRUE,
+                WA_CloseGadget, TRUE,
+                WA_Activate, TRUE,
+                WA_Title, "Close me to close the screen",
+                WA_IDCMP, IDCMP_CLOSEWINDOW,
+                WA_PubScreen, myscreen,
+                TAG_DONE
+            )
+        ) == NULL
+    )
+    {
+        clean_exit("Can't open window\n");
+    }
+
+    if ((PubScreenStatus(myscreen, 0) & 1) == 0)
+    {
+        clean_exit("Can't make screen public");
+    }
+
+    winsig = 1L << mywindow->UserPort->mp_SigBit;
+    pubsig = 1L << signalnum;
+
+    while (!done)
+    {
+        signals = Wait(winsig | pubsig);
+
+        if (mywindow && (signals & winsig))
+        {
+            while (NULL != (message = (struct IntuiMessage *)GetMsg(mywindow->UserPort)))
+            {
+                if (message->Class == IDCMP_CLOSEWINDOW)
+                {
+                    closewindow = TRUE;
+                }
+                ReplyMsg((struct Message *)message);
+            }
+        }
+        if (signals & pubsig)
+        {
+            if (PubScreenStatus(myscreen, PSNF_PRIVATE) & 1)
+            {
+                done = TRUE;
+            }
+            else
+            {
+                PutStr("Failed to make screen private\n");
+            }
+        }
+
+        if (closewindow)
+        {
+            if (mywindow) CloseWindow(mywindow);
+            winsig = 0;
+            mywindow = NULL;
+            closewindow = FALSE;
+        }
+    }
+
+    clean_exit(NULL);
+    return 0;
+}
