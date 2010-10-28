4c21a417113389852bddaf7beef744a2774e7cfe
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-10-28 07:42:18 +0000

    Implemented ARMv6 atomic operations. Completely untested and perhaps horribly broken
    git-svn-id: https://svn.aros.org/svn/aros/trunk@35000 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/arm-all/include/aros/arm/atomic_v6.h
@@ -0,0 +1,192 @@
+/*
+ * atomic_v6.h
+ *
+ *  Created on: Oct 23, 2010
+ *      Author: misc
+ */
+
+#define __AROS_ATOMIC_INC_L(var) \
+do { \
+   unsigned long temp; \
+   int result; \
+   __asm__ __volatile__("\n1: ldrex %0, [%3]; add %0, %0, #1; strex %1, %0, [%3]; teq %1, #0; bne 1b" \
+		   	   	   	   	   :"=&r"(result), "=&r"(temp), "+Qo"(var) \
+		   	   	   	   	   :"r"(&var) \
+		   	   	   	   	   :"cc"); \
+} while (0)
+
+#define __AROS_ATOMIC_DEC_L(var) \
+do { \
+   unsigned long temp; \
+   int result; \
+   __asm__ __volatile__("\n1: ldrex %0, [%3]; sub %0, %0, #1; strex %1, %0, [%3]; teq %1, #0; bne 1b" \
+		   	   	   	   	   :"=&r"(result), "=&r"(temp), "+Qo"(var) \
+		   	   	   	   	   :"r"(&var) \
+		   	   	   	   	   :"cc"); \
+} while (0)
+
+static inline void atomic_and_l(ULONG *var, ULONG mask)
+{
+    unsigned long temp; int result;
+
+    __asm__ __volatile__("\n1: ldrex %0, [%2]; and %0, %0, %3; strex %1, %0, [%2]; teq %1, #0; bne 1b"
+                        :"=&r"(result), "=&r"(temp)
+                        :"r"(&var), "Ir"(mask)
+                        :"cc");
+}
+
+static inline void atomic_or_l(ULONG *var, ULONG mask)
+{
+    unsigned long temp; int result;
+
+    __asm__ __volatile__("\n1: ldrex %0, [%3]; or %0, %0, %4; strex %1, %0, [%3]; teq %1, #0; bne 1b"
+                        :"=&r"(result), "=&r"(temp)
+                        :"r"(&var), "Ir"(mask)
+                        :"cc");
+}
+
+static inline void atomic_inc_b(BYTE* p)
+{
+    CONST ULONG* addr  = (ULONG*)((ULONG)p & ~0x03); /* Longword-align the address   */
+    UBYTE shift        = ((ULONG)p & 0x03) << 3;     /* get number of bits to shift  */
+    unsigned long mask = (255L << shift);	     /* get mask for changed byte    */
+    unsigned long nmask = ~mask;		     /* get mask for unchanged bytes */
+    unsigned long result, temp;
+
+   __asm__ __volatile__(
+	"1:	ldrex %0, [%4]\n"		/* Load the longword and reserve location */
+	"	mov %1, #0x0101\n"		/* %1 = 0x01010101 */
+	"	lsl %1, %1, #16\n"
+	"	orr %1, %1, #0x0101\n"
+	"	uadd8 %1, %0, %1\n"		/* Increment all 4 bytes */
+	"	and %1, %1, %2\n"		/* Mask away unneeded bytes in the result */
+	"	and %0, %0, %3\n"		/* Now mask away modified byte in the original longword */
+	"	orr %0, %0, %1\n"		/* Merge back results */
+	"	strex %1, %0, [%4]\n"		/* Store */
+	"	teq %1, #0\n"			/* Repeat if not atomic */
+	"	bne 1b"
+	:"=&r"(result), "=&r"(temp)
+	:"r"(mask), "r"(nmask), "r"(addr)
+	:"cc");
+}
+
+static inline void atomic_dec_b(BYTE* p)
+{
+    CONST ULONG* addr  = (ULONG*)((ULONG)p & ~0x03); /* Longword-align the address   */
+    UBYTE shift        = ((ULONG)p & 0x03) << 3;     /* get number of bits to shift  */
+    unsigned long mask = (255L << shift);	     /* get mask for changed byte    */
+    unsigned long nmask = ~mask;		     /* get mask for unchanged bytes */
+    unsigned long result, temp;
+
+   __asm__ __volatile__(
+	"1:	ldrex %0, [%4]\n"		/* Load the longword and reserve location */
+	"	mov %1, #0x0101\n"
+	"	lsl %1, %1, #16\n"
+	"	orr %1, %1, #0x0101\n"
+	"	usub8 %1, %0, %1\n"		/* Decrement all 4 bytes */
+	"	and %1, %1, %2\n"		/* Mask away unneeded bytes in the result */
+	"	and %0, %0, %3\n"		/* Now mask away modified byte in the original longword */
+	"	orr %0, %0, %1\n"		/* Merge back results */
+	"	strex %1, %0, [%4]\n"		/* Store */
+	"	teq %1, #0\n"			/* Repeat if not atomic */
+	"	bne 1b"
+	:"=&r"(result), "=&r"(temp)
+	:"r"(mask), "r"(nmask), "r"(addr)
+	:"cc");
+}
+
+static inline void atomic_inc_w(WORD* p)
+{
+    CONST ULONG* addr  = (ULONG*)((ULONG)p & ~0x03); /* Longword-align the address   */
+    UBYTE shift        = ((ULONG)p & 0x03) << 3;     /* get number of bits to shift  */
+    unsigned long mask = (65535L << shift);	     /* get mask for changed bytes   */
+    unsigned long nmask = ~mask;		     /* get mask for unchanged bytes */
+    unsigned long result, temp;
+
+   __asm__ __volatile__(
+	"1:	ldrex %0, [%4]\n"		/* Load the longword and reserve location */
+	"	mov %1, #1\n"
+	"	lsl %1, %1, #16\n"
+	"	orr %1, %1, #1\n"
+	"	uadd16 %1, %0, %1\n"		/* Increment both words */
+	"	and %1, %1, %2\n"		/* Mask away unneeded bytes in the result */
+	"	and %0, %0, %3\n"		/* Now mask away modified bytes in the original longword */
+	"	orr %0, %0, %1\n"		/* Merge back results */
+	"	strex %1, %0, [%4]\n"		/* Store */
+	"	teq %1, #0\n"			/* Repeat if not atomic */
+	"	bne 1b"
+	:"=&r"(result), "=&r"(temp)
+	:"r"(mask), "r"(nmask), "r"(addr)
+	:"cc");
+}
+
+static inline void atomic_dec_w(WORD* p)
+{
+    CONST ULONG* addr  = (ULONG*)((ULONG)p & ~0x03); /* Longword-align the address   */
+    UBYTE shift        = ((ULONG)p & 0x03) << 3;     /* get number of bits to shift  */
+    unsigned long mask = (65535L << shift);	     /* get mask for changed bytes   */
+    unsigned long nmask = ~mask;		     /* get mask for unchanged bytes */
+    unsigned long result, temp;
+
+   __asm__ __volatile__(
+	"1:	ldrex %0, [%4]\n"		/* Load the longword and reserve location */
+	"	mov %1, #1\n"
+	"	lsl %1, %1, #16\n"
+	"	orr %1, %1, #1\n"
+	"	usub16 %1, %0, %1\n"		/* Increment both words */
+	"	and %1, %1, %2\n"		/* Mask away unneeded bytes in the result */
+	"	and %0, %0, %3\n"		/* Now mask away modified bytes in the original longword */
+	"	orr %0, %0, %1\n"		/* Merge back results */
+	"	strex %1, %0, [%4]\n"		/* Store */
+	"	teq %1, #0\n"			/* Repeat if not atomic */
+	"	bne 1b"
+	:"=&r"(result), "=&r"(temp)
+	:"r"(mask), "r"(nmask), "r"(addr)
+	:"cc");
+}
+
+static inline void atomic_or_b(UBYTE* p, UBYTE mask)
+{
+    CONST ULONG* addr  = (ULONG*)((ULONG)p & ~0x03); /* Longword-align the address   */
+    UBYTE shift        = ((ULONG)p & 0x03) << 3;     /* get number of bits to shift  */
+
+    atomic_or_l(addr, (ULONG)mask << shift);
+}
+
+static inline void atomic_and_b(UBYTE* p, UBYTE mask)
+{
+    CONST ULONG* addr  = (ULONG*)((ULONG)p & ~0x03); /* Longword-align the address   */
+    UBYTE shift        = ((ULONG)p & 0x03) << 3;     /* get number of bits to shift  */
+
+    atomic_and_l(addr, ~0UL & (mask << shift));
+}
+
+static inline void atomic_or_w(UWORD* p, UWORD mask)
+{
+    CONST ULONG* addr  = (ULONG*)((ULONG)p & ~0x03); /* Longword-align the address   */
+    UBYTE shift        = ((ULONG)p & 0x03) << 3;     /* get number of bits to shift  */
+
+    atomic_or_l(addr, (ULONG)mask << shift);
+}
+
+static inline void atomic_and_w(UWORD* p, UWORD mask)
+{
+    CONST ULONG* addr  = (ULONG*)((ULONG)p & ~0x03); /* Longword-align the address   */
+    UBYTE shift        = ((ULONG)p & 0x03) << 3;     /* get number of bits to shift  */
+
+    atomic_and_l(addr, ~0UL & (mask << shift));
+}
+
+#define __AROS_ATOMIC_INC_B(var) atomic_inc_b((BYTE *) &(var))
+#define __AROS_ATOMIC_DEC_B(var) atomic_dec_b((BYTE *) &(var))
+
+#define __AROS_ATOMIC_INC_W(var) atomic_inc_w((WORD *) &(var))
+#define __AROS_ATOMIC_DEC_W(var) atomic_dec_w((WORD *) &(var))
+
+#define __AROS_ATOMIC_AND_B(var, mask) atomic_and_b((UBYTE *) &(var), (mask))
+#define __AROS_ATOMIC_AND_W(var, mask) atomic_and_w((UWORD *) &(var), (mask))
+#define __AROS_ATOMIC_AND_L(var, mask) atomic_and_l((ULONG *) &(var), (mask))
+
+#define __AROS_ATOMIC_OR_B(var, mask) atomic_or_b((UBYTE *) &(var), (mask))
+#define __AROS_ATOMIC_OR_W(var, mask) atomic_or_w((UWORD *) &(var), (mask))
+#define __AROS_ATOMIC_OR_L(var, mask) atomic_or_l((ULONG *) &(var), (mask))
