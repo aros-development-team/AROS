2c6455c3cd58ba3a3ae31217ff755080206576be
ezrec <jason.mcmullan@gmail.com>
2010-10-28 05:13:21 +0000

    arch/m68k-amiga: Move Custom hw regs out of start
    .. just noticed hardware/custom.h .. I need to use
    that instead of this, but that is for another day.
    
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@34991 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/m68k-amiga/boot/start.c
+++ arch/m68k-amiga/boot/start.c
@@ -15,53 +15,11 @@
 #include "kernel_romtags.h"
 
 #define USE_GDBSTUB
+#include "amiga_hwreg.h"
 
 extern const struct Resident Exec_resident;
 
 extern void __clear_bss(const struct KernelBSS *bss);
-
-/** Screen/Serial Debug **/
-
-#define SERPER_BASE_PAL		3546895
-#define SERPER_BASE_NTSC	3579545
-#define SERPER			0x32
-#define   SERPER_BAUD(base, x)	(((base)/(x)-1) & 0x7fff)	/* Baud rate */
-#define SERDATR			0x18
-#define   SERDATR_OVRUN		(1 << 15)	/* Overrun */
-#define   SERDATR_RBF		(1 << 14)	/* Rx Buffer Full */
-#define   SERDATR_TBE		(1 << 13)	/* Tx Buffer Empty */
-#define   SERDATR_TSRE		(1 << 12)	/* Tx Shift Empty */
-#define   SERDATR_RXD		(1 << 11)	/* Rx Pin */
-#define   SERDATR_STP9		(1 <<  9)	/* Stop bit (if 9 data) */
-#define   SERDATR_STP8		(1 <<  8)	/* Stop bit (if 8 data) */
-#define   SERDATR_DB9_of(x)	((x) & 0x1ff)	/* 9-bit data */
-#define   SERDATR_DB8_of(x)	((x) & 0xff)	/* 8-bit data */
-#define ADKCON			0x9e
-#define   ADKCON_SETCLR		(1 << 15)
-#define   ADKCON_UARTBRK	(1 << 11)	/* Force break */
-#define SERDAT			0x30
-#define   SERDAT_STP9		(1 << 9)
-#define   SERDAT_STP8		(1 << 8)
-#define   SERDAT_DB9(x)		((x) & 0x1ff)
-#define   SERDAT_DB8(x)		((x) & 0xff)
-
-#define BPLCON0			0x100
-#define BPL1DAT			0x110
-#define COLOR00			0x180
-
-static inline void reg_w(ULONG reg, UWORD val)
-{
-	volatile UWORD *r = (void *)(0xdff000 + reg);
-
-	*r = val;
-}
-
-static inline UWORD reg_r(ULONG reg)
-{
-	volatile UWORD *r = (void *)(0xdff000 + reg);
-
-	return *r;
-}
 
 #define RGB(r,g,b)	((((r) & 0xf) << 8) | (((g) & 0xf) << 4) | (((b) & 0xf) << 0))
 
