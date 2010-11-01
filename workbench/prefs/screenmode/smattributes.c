b25d6c13d36f0b349071b55f39725986924083ea
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-11-01 20:28:45 +0000

    Initial version of Mode Attributes panel.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@35068 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/prefs/screenmode/smattributes.c
@@ -0,0 +1,163 @@
+/*
+    Copyright © 2010, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#define MUIMASTER_YES_INLINE_STDARG
+#define DEBUG 0
+
+#include <libraries/mui.h>
+
+#include <exec/rawfmt.h>
+#include <proto/exec.h>
+#include <proto/graphics.h>
+#include <proto/utility.h>
+#include <proto/intuition.h>
+#include <zune/customclasses.h>
+
+#include "locale.h"
+
+#include "smattributes.h"
+
+struct ScreenModeAttributes_DATA
+{
+    Object * objVisibleW;
+    Object * objVisibleH;
+    Object * objMinimumW;
+    Object * objMinimumH;
+    Object * objMaximumW;
+    Object * objMaximumH;
+    Object * objMaximumColors;
+    ULONG DisplayID;
+};
+
+Object *ScreenModeAttributes__OM_NEW(Class *CLASS, Object *self, struct opSet *message)
+{
+    struct ScreenModeAttributes_DATA *data;
+    Object  * objVisibleW, * objVisibleH, * objMinimumW, * objMinimumH, 
+            * objMaximumW, * objMaximumH, * objMaximumColors;
+    
+    
+    ULONG id;
+    
+    self = (Object *)DoSuperNewTags
+    (
+        CLASS, self, NULL,
+        MUIA_Group_Horiz, TRUE,
+        Child, (IPTR)VGroup,
+            Child, (IPTR)ColGroup(4),
+                Child, (IPTR)LLabel1("Visible Size:"), /* FIXME: Localize */
+                Child, (IPTR)(objVisibleW = (Object *)Label1("16368")),
+                Child, (IPTR)Label1("x"),
+                Child, (IPTR)(objVisibleH = (Object *)Label1("16384")),
+
+                Child, (IPTR)LLabel1("Minimum Size:"), /* FIXME: Localize */
+                Child, (IPTR)(objMinimumW = (Object *)Label1("16368")),
+                Child, (IPTR)Label1("x"),
+                Child, (IPTR)(objMinimumH = (Object *)Label1("16368")),
+
+                Child, (IPTR)LLabel1("Maximum Size:"), /* FIXME: Localize */
+                Child, (IPTR)(objMaximumW = (Object *)Label1("16368")),
+                Child, (IPTR)Label1("x"),
+                Child, (IPTR)(objMaximumH = (Object *)Label1("16368")),
+
+                Child, (IPTR)LLabel1("Maximum Colors:"), /* FIXME: Localize */
+                Child, (IPTR)(objMaximumColors = (Object *)LLabel1("16 M.")),
+                Child, (IPTR)RectangleObject, End,
+                Child, (IPTR)RectangleObject, End,
+                
+            End,  
+
+            Child, (IPTR)RectangleObject, End,
+
+        End,
+      
+        TAG_MORE, (IPTR)message->ops_AttrList
+    );
+    
+    if (!self)
+        goto err;
+    
+    D(bug("[smattributes] Created ScreenModeAttributes object 0x%p\n", self));
+    data = INST_DATA(CLASS, self);
+    
+    data->objVisibleW       = objVisibleW;
+    data->objVisibleH       = objVisibleH;
+    data->objMinimumW       = objMinimumW;
+    data->objMinimumH       = objMinimumH;
+    data->objMaximumW       = objMaximumW;
+    data->objMaximumH       = objMaximumH;
+    data->objMaximumColors  = objMaximumColors;
+
+    id = GetTagData(MUIA_ScreenModeAttributes_DisplayID, INVALID_ID, message->ops_AttrList);
+    D(bug("[smattributes] Setting initial ModeID 0x%08lX\n", id));
+    set(self, MUIA_ScreenModeAttributes_DisplayID, id);
+    
+    return self;
+
+err:
+    CoerceMethod(CLASS, self, OM_DISPOSE);
+    return NULL;
+}
+
+IPTR ScreenModeAttributes__OM_SET(Class *CLASS, Object *self, struct opSet *message)
+{
+    struct ScreenModeAttributes_DATA *data = INST_DATA(CLASS, self);    
+    const struct TagItem *tags;
+    struct TagItem *tag;
+    IPTR ret;
+
+    DB2(bug("[smattributes] OM_SET called\n"));
+    for (tags = message->ops_AttrList; (tag = NextTagItem(&tags)); )
+    {
+        switch (tag->ti_Tag)
+        {
+            case MUIA_ScreenModeAttributes_DisplayID:
+            {
+                struct DimensionInfo dim;
+
+                D(bug("[smattributes] Set DisplayID = 0x%08lx\n", tag->ti_Data));
+
+                if (GetDisplayInfoData(NULL, (UBYTE *)&dim, sizeof(dim), DTAG_DIMS, tag->ti_Data))
+                {
+                    TEXT buffer[128];
+                    ULONG val;
+
+                    val = dim.Nominal.MaxX - dim.Nominal.MinX + 1;
+                    RawDoFmt("%ld", &val, RAWFMTFUNC_STRING, buffer);
+                    set(data->objVisibleW, MUIA_Text_Contents, buffer);
+                    val = dim.Nominal.MaxY - dim.Nominal.MinY + 1;
+                    RawDoFmt("%ld", &val, RAWFMTFUNC_STRING, buffer);
+                    set(data->objVisibleH, MUIA_Text_Contents, buffer);
+
+                    RawDoFmt("%d", &dim.MinRasterWidth, RAWFMTFUNC_STRING, buffer);
+                    set(data->objMinimumW, MUIA_Text_Contents, buffer);
+                    RawDoFmt("%d", &dim.MinRasterHeight, RAWFMTFUNC_STRING, buffer);
+                    set(data->objMinimumH, MUIA_Text_Contents, buffer);
+
+                    RawDoFmt("%d", &dim.MaxRasterWidth, RAWFMTFUNC_STRING, buffer);
+                    set(data->objMaximumW, MUIA_Text_Contents, buffer);
+                    RawDoFmt("%d", &dim.MaxRasterHeight, RAWFMTFUNC_STRING, buffer);
+                    set(data->objMaximumH, MUIA_Text_Contents, buffer);
+
+                    val = 1 << dim.MaxDepth;
+                    RawDoFmt("%ld", &val, RAWFMTFUNC_STRING, buffer);
+                    set(data->objMaximumColors, MUIA_Text_Contents, buffer);
+                }
+                break;
+            }
+        }
+    }
+
+    DB2(bug("[smattributes] Calling OM_SET() on superclass\n"));
+    ret = DoSuperMethodA(CLASS, self, (Msg)message);
+    DB2(bug("[smattributes] OM_SET() on superclass returned %ld\n", ret));
+    return ret;
+}
+
+ZUNE_CUSTOMCLASS_2
+(
+    ScreenModeAttributes, NULL, MUIC_Group, NULL,   
+    OM_NEW,     struct opSet *,
+    OM_SET,     struct opSet *
+);
