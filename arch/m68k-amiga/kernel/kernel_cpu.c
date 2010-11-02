64274e24ea534ec1946f52b170b2fd65c26f1dcb
ezrec <jason.mcmullan@gmail.com>
2010-11-02 13:43:57 +0000

    arch/m68k-amiga/kernel: Try to handle exceptions
    Untested code, but less likely to fail than what
    was there before.
    
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@35085 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/m68k-amiga/kernel/kernel_cpu.c
+++ arch/m68k-amiga/kernel/kernel_cpu.c
@@ -21,6 +21,19 @@
 # endif
 #endif
 
+extern void cpu_Exception(void);
+asm (
+	"	.text\n"
+	"	.align 4\n"
+	"	.globl cpu_Exception\n"
+	"cpu_Exception:\n"
+	"	movem.l	%d0-%d1/%a0-%a1/%a6,%sp@-\n"
+	"	move.l	(4),%a6\n"
+	"	jsr	%a6@(-1 * 6 * 11 /* Exception */)\n"
+	"	movem.l	%sp@+,%d0-%d1/%a0-%a1/%a6\n"
+	"	rts\n"
+);
+
 void cpu_Switch(regs_t *regs)
 {
     struct Task *task = SysBase->ThisTask;
@@ -65,16 +78,15 @@ void cpu_Dispatch(regs_t *regs)
     	/* Exec_Exception() will Enable() */
     	Disable();
 
-    	task->tc_SPReg -= sizeof(regs_t);
-    	if (task->tc_SPReg <= task->tc_SPLower)
-    		Alert(AT_DeadEnd|AN_StackProbe);
-
-    	ctx->a[7] = (IPTR)task->tc_SPReg;
-	CopyMem(ctx, task->tc_SPReg, sizeof(regs_t));
-
 	/* Manipulate the current CPU context so Exec_Exception gets
 	 * executed after we leave Supervisor mode.
 	 */
-	regs->a[7] = (IPTR)task->tc_SPReg;
+    	task->tc_SPReg -= sizeof(ULONG);	/* RTS to original PC */
+    	if (task->tc_SPReg <= task->tc_SPLower)
+    		Alert(AT_DeadEnd|AN_StackProbe);
+    	*(ULONG *)(task->tc_SPReg) = regs->pc;
+
+	regs->a[7] = task->tc_SPReg;
+	regs->pc   = (IPTR)cpu_Exception;
     }
 }
