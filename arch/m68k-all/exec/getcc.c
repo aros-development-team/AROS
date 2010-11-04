aa2191d8ec8a9e03b0e56ca3cfc15d26ba6e98c8
ezrec <jason.mcmullan@gmail.com>
2010-11-04 05:24:45 +0000

    arch/m68k-all: An Exec/GetCC stub is required, evidently
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@35119 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/m68k-all/exec/getcc.c
@@ -0,0 +1,55 @@
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: GetCC() - Read the CPU condition codes in an easy way.
+    Lang: english
+*/
+
+#include <aros/debug.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/exec.h>
+
+	AROS_LH0(UWORD, GetCC,
+
+/*  LOCATION */
+	struct ExecBase *, SysBase, 88, Exec)
+
+/*  FUNCTION
+	Read the contents of the CPU condition code register in a system
+	independant way. The flags return will be in the same format as
+	the Motorola MC680x0 family of microprocessors.
+
+    INPUTS
+	None.
+
+    RESULT
+	The CPU condition codes or ~0ul if this function has not been
+	implemented.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+	This function may not be implemented on platforms other than
+	Motorola mc680x0 processors.
+
+    SEE ALSO
+	SetSR()
+
+    INTERNALS
+
+******************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    /* Overridden on the RegCall ABI's startup routine */
+    aros_print_not_implemented("GetCC");
+    return ~0;
+
+    AROS_LIBFUNC_EXIT
+} /* GetCC() */
