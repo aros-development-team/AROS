18fd71fb6ac5a21b926a3541ef2ccb1e645afe3c
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-11-10 08:49:02 +0000

    Added ARM support
    git-svn-id: https://svn.aros.org/svn/aros/trunk@35328 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-unix/kernel/kernel_cpu.h
+++ arch/all-unix/kernel/kernel_cpu.h
@@ -4,11 +4,15 @@
 /* First include CPU-dependent definitions */
 
 #ifdef __i386__
-#include "cpu_i386.h"
+#include <cpu_i386.h>
+#endif
+
+#ifdef __arm__
+#include <cpu_arm.h>
 #endif
 
 /* This macro serves as an indicator of included file */
-#ifndef PREPARE_INITIAL_FRAME
+#ifndef EXCEPTIONS_COUNT
 #error Unsupported CPU
 #endif
 
