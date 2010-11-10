06af340a9e02cbd403cfddf850ad6f640d582b6e
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-11-10 20:25:54 +0000

    - Use unified context on PPC Linux - Added missing USER_CFLAGS in arm-all/kernel - Removed excessive 0x's in kernel crash dump
    git-svn-id: https://svn.aros.org/svn/aros/trunk@35350 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/ppc-all/kernel/createcontext.c
@@ -0,0 +1,39 @@
+#include <aros/kernel.h>
+#include <aros/libcall.h>
+
+#include <kernel_base.h>
+#include <kernel_memory.h>
+
+AROS_LH0I(void *, KrnCreateContext,
+	  struct KernelBase *, KernelBase, 18, Kernel)
+{
+    AROS_LIBFUNC_INIT
+
+    struct ExceptionContext *ctx;
+    cpumode_t mode = goSuper();
+
+    ctx = krnAllocVec(sizeof(struct AROSCPUContext));
+
+    /* Initialize the storage if needed */
+    if (ctx)
+    {
+        ULONG i;
+
+	/*
+	 * Sys V PPC ABI says r2 is reserved so we copy the
+	 * current one and it will never be changed again
+	 */
+	__asm__ __volatile__ ("stw 2,%0":"=m"(ctx->gpr[2])::"memory");
+
+	/* Initialize FPU portion */
+	ctx->Flags = ECF_FPU;
+        for (i = 0; i < 32; i++)
+            ctx->fpr[i] = 0.0;
+    }
+
+    goBack(mode);
+
+    return ctx;
+
+    AROS_LIBFUNC_EXIT
+}
