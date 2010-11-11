b7a1172c2fc246d80d12f3e930462d0060ee9ebf
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-11-11 10:52:58 +0000

    Composing WIP: - create morror bitmap - change mode using mirror bitmap - copy changes to mirror bitmap (UpdateRect)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@35367 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/hidds/hidd.nouveau/nouveau_composing.c
+++ workbench/hidds/hidd.nouveau/nouveau_composing.c
@@ -173,13 +173,131 @@ static BOOL HIDDNouveauSwitchToVideoMode(OOP_Object * bm)
     return TRUE;
 }
 
-BOOL Composing_TopBitMapChanged(OOP_Object * newTopBitMap)
+#if ENABLE_COMPOSING
+OOP_Object * visiblebitmap = NULL;
+HIDDT_ModeID visiblemodeid = vHidd_ModeID_Invalid;
+OOP_Object * topbitmap = NULL;
+#endif
+
+BOOL Composing_TopBitMapChanged(OOP_Object * bm)
 {
-    return HIDDNouveauSwitchToVideoMode(newTopBitMap);
+#if ENABLE_COMPOSING
+    /* 
+        Desctiption: 
+        take incomming top bitmap
+        read its mode and sizes,
+        create a mirroring bitmap that fits the mode
+        switch mode 
+    */
+    /* TODO: needs locking mechanism */
+    OOP_Class * cl = OOP_OCLASS(bm);
+    OOP_Object * gfx = NULL;
+    OOP_Object * sync = NULL;
+    OOP_Object * pf = NULL;
+    OOP_Object * fbbitmap = NULL;
+    IPTR modeid, hdisp, vdisp, e;
+    struct TagItem bmtags[5];
+
+    OOP_GetAttr(bm, aHidd_BitMap_GfxHidd, &e);
+    gfx = (OOP_Object *)e;
+
+    /* Read display mode properties */
+    OOP_GetAttr(bm, aHidd_BitMap_ModeID, &modeid);
+    if (modeid == vHidd_ModeID_Invalid)
+    {
+        D(bug("[Nouveau] Invalid ModeID\n"));
+        return FALSE;
+    }
+    
+    /* If the mode is alrady visible, just switch top bitmap */
+    if (modeid == visiblemodeid)
+    {
+        topbitmap = bm;
+        return TRUE;
+    }
+
+    /* Get width and height of mode */
+    struct pHidd_Gfx_GetMode __getmodemsg = 
+    {
+        modeID:	modeid,
+        syncPtr:	&sync,
+        pixFmtPtr:	&pf,
+    }, *getmodemsg = &__getmodemsg;
+
+    getmodemsg->mID = OOP_GetMethodID(IID_Hidd_Gfx, moHidd_Gfx_GetMode);
+    OOP_DoMethod(gfx, (OOP_Msg)getmodemsg);
+
+    OOP_GetAttr(sync, aHidd_Sync_HDisp, &hdisp);
+    OOP_GetAttr(sync, aHidd_Sync_VDisp, &vdisp);
+
+    bug("TBCH: %d, %d, %d\n", modeid, hdisp, vdisp);
+    
+    /* Create a new bitmap that will be used for framebuffer */
+    bmtags[0].ti_Tag = aHidd_BitMap_Width;          bmtags[0].ti_Data = hdisp;
+    bmtags[1].ti_Tag = aHidd_BitMap_Height;         bmtags[1].ti_Data = vdisp;
+    bmtags[2].ti_Tag = aHidd_BitMap_Displayable;    bmtags[2].ti_Data = TRUE;
+    bmtags[3].ti_Tag = aHidd_BitMap_ModeID;         bmtags[3].ti_Data = modeid;
+    bmtags[4].ti_Tag = TAG_DONE;                    bmtags[4].ti_Data = TAG_DONE;
+    
+    fbbitmap = HIDD_Gfx_NewBitMap(gfx, bmtags);
+    if (fbbitmap)
+    {
+        BOOL ret = HIDDNouveauSwitchToVideoMode(fbbitmap);
+        if (ret)
+        {
+            /* Dispose the previous visiblebitmap */
+            HIDD_Gfx_DisposeBitMap(gfx, visiblebitmap);
+
+            /* Store bitmap/mode information */ 
+            topbitmap       = bm;
+            visiblemodeid   = modeid;
+            visiblebitmap   = fbbitmap;
+            return TRUE;
+        }
+        else
+        {
+            /* Dispose fbbitmap */
+            HIDD_Gfx_DisposeBitMap(gfx, fbbitmap);
+            return FALSE;
+        }
+    }
+
+    return FALSE;
+#else
+    return HIDDNouveauSwitchToVideoMode(bm);
+#endif
 }
 
 BOOL Composing_BitMapPositionChanged(OOP_Object * bm)
 {
+#if ENABLE_COMPOSING
+    return FALSE;
+#else
     return HIDDNouveauShowBitmapForSelectedMode(bm);
+#endif
+}
+
+VOID Composing_BitMapRectChanged(OOP_Object * bm, WORD x, WORD y, WORD width, WORD height)
+{
+#if ENABLE_COMPOSING
+    if (bm != topbitmap)
+        return;
+
+    if (visiblebitmap)
+    {
+        OOP_Class * cl = OOP_OCLASS(bm);
+        OOP_Object * gfx = NULL;
+        OOP_Object * tmpgc = NULL;
+        IPTR e;
+
+        OOP_GetAttr(bm, aHidd_BitMap_GfxHidd, &e);
+        gfx = (OOP_Object *)e;
+        
+        /* TODO: stubs have performance hit, change to direct DoMethod call with cached MethodID */
+        tmpgc = HIDD_Gfx_NewGC(gfx, NULL);        
+        HIDD_Gfx_CopyBox(gfx, bm, x, y, visiblebitmap, x, y, width, height, tmpgc);
+        HIDD_Gfx_DisposeGC(gfx, tmpgc);
+    }
+#endif
 }
 
