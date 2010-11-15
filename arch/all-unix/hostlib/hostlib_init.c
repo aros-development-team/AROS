0cc0d2285cdcf568e8e7e144ded49e1e7ef3eeb3
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-11-15 09:24:43 +0000

    Removed obsolete unneeded file
    git-svn-id: https://svn.aros.org/svn/aros/trunk@35508 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-unix/hostlib/hostlib_init.c
+++
@@ -1,7 +0,0 @@
-/*
- * This is a temporary kludge to get over generic hostlib_init.c
- * Without it initialization will fail because we have no BootMsg
- * and HostInterface yet.
- * This will be not needed when UNIX-hosted kickstart goes modular
- */
- 
