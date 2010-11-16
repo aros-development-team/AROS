d0a7fb0f1a2741a9e1706d130ab01a675ce711e5
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-11-16 13:05:42 +0000

    added proper register names
    git-svn-id: https://svn.aros.org/svn/aros/trunk@35547 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-linux/exec/cachecleare.c
+++ arch/arm-linux/exec/cachecleare.c
@@ -11,6 +11,8 @@
 #include <aros/libcall.h>
 #include <proto/exec.h>
 
+#include <aros/debug.h>
+
 AROS_LH3(void, CacheClearE,
 	 AROS_LHA(APTR, address, A0),
 	 AROS_LHA(ULONG, length, D0),
@@ -18,6 +20,8 @@ AROS_LH3(void, CacheClearE,
 	 struct ExecBase *, SysBase, 107, Exec)
 {
     AROS_LIBFUNC_INIT
+
+    D(bug("CacheClearE(%08x, %08x, %x)\n", address, length, caches));
 
     /* Linux supports only instruction cache flush */
     if (caches & CACRF_ClearI)
@@ -29,14 +33,14 @@ AROS_LH3(void, CacheClearE,
 
 	const int syscall = 0xf0002;
 	__asm __volatile (
-		"mov	 r0, %0\n"			
-		"mov	 r1, %1\n"
-		"mov	 r7, %2\n"
-		"mov     r2, #0x0\n"
+		"mov	 %%r0, %0\n"			
+		"mov	 %%r1, %1\n"
+		"mov	 %%r7, %2\n"
+		"mov     %%r2, #0x0\n"
 		"svc     0x00000000\n"
 		:
 		:	"r" (address), "r" ((IPTR)address + length), "r" (syscall)
-		:	"r0", "r1", "r2", "r7"
+		:	"r0", "r1", "r2", "r7", "memory"
 		);
 
 	/* It's okay to use library base now */
