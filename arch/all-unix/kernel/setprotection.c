bd28d3919c62b71a15ec68292c0a61e321c8a970
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-11-18 06:31:30 +0000

    - Added forgotten ABI barriers - Removed unneeded autodoc blocks - Use MAP_ANON instead of MAP_ANONYMOUS which is Linux-only
    git-svn-id: https://svn.aros.org/svn/aros/trunk@35596 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-unix/kernel/setprotection.c
+++ arch/all-unix/kernel/setprotection.c
@@ -5,44 +5,18 @@
 #include <kernel_intern.h>
 
 #include <sys/mman.h>
-
-/*****************************************************************************
-
-    NAME */
-#include <proto/kernel.h>
+#include <inttypes.h>
 
 AROS_LH3I(void, KrnSetProtection,
-
-/*  SYNOPSIS */
-	AROS_LHA(void *, address, A0),
-	AROS_LHA(uint32_t, length, D0),
-        AROS_LHA(KRN_MapAttr, flags, D1),
-
-/*  LOCATION */
-	struct KernelBase *, KernelBase, 21, Kernel)
-
-/*  FUNCTION
-
-    INPUTS
-
-    RESULT
-
-    NOTES
-
-    EXAMPLE
-
-    BUGS
-
-    SEE ALSO
-
-    INTERNALS
-
-******************************************************************************/
+	 AROS_LHA(void *, address, A0),
+	 AROS_LHA(uint32_t, length, D0),
+         AROS_LHA(KRN_MapAttr, flags, D1),
+	 struct KernelBase *, KernelBase, 21, Kernel)
 {
     AROS_LIBFUNC_INIT
 
     int flags_unix = 0;
-    
+
     if (flags & MAP_Readable)
 	flags_unix |= PROT_READ;
     if (flags & MAP_Writable)
@@ -51,6 +25,7 @@ AROS_LH3I(void, KrnSetProtection,
 	flags_unix |= PROT_EXEC;
 
     KernelIFace.mprotect(address, length, flags_unix);
+    AROS_HOST_BARRIER
 
     AROS_LIBFUNC_EXIT
 }
