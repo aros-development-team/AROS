118c596f22026689d1f977433c12936bcd0d1e0d
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-11-22 06:43:12 +0000

    malloc() wrappers are Linux-specific. They break other builds.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@35682 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/all-linux/bootstrap/malloc.c
@@ -0,0 +1,48 @@
+/* AROS-thread-safe versions of libc memory allocation routines */
+
+#include <proto/exec.h>
+
+static int memnest;
+#define MEMLOCK if (SysBase != NULL) Forbid();
+#define MEMUNLOCK if (SysBase != NULL) Permit();
+
+extern struct ExecBase *SysBase;
+extern void * __libc_malloc(size_t);
+extern void __libc_free(void *);
+extern void * __libc_calloc(size_t, size_t);
+extern void * __libc_realloc(void * mem, size_t newsize);
+
+void * malloc(size_t size)
+{
+    void *res;
+
+    MEMLOCK
+    memnest++;
+    res = __libc_malloc(size);
+    memnest--;
+    MEMUNLOCK
+
+    return res;
+}
+
+void free(void * addr)
+{
+    MEMLOCK
+    memnest++;
+    __libc_free(addr);
+    memnest--;
+    MEMUNLOCK
+}
+
+void * calloc(size_t n, size_t size)
+{
+    void *res;
+
+    MEMLOCK
+    memnest++;
+    res = __libc_calloc(n, size);
+    memnest--;
+    MEMUNLOCK
+    
+    return res;
+}
