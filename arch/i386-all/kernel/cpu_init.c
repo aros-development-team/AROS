b9b4b2503ae1e1174b71342d63359687b9c7220b
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-11-22 11:06:01 +0000

    Calculate full context size
    git-svn-id: https://svn.aros.org/svn/aros/trunk@35691 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-all/kernel/cpu_init.c
+++ arch/i386-all/kernel/cpu_init.c
@@ -9,9 +9,11 @@
 
 static int cpu_Init(struct KernelBase *KernelBase)
 {
-    /* Evaluate CPU capabilities */
     ULONG v1, v2, v3, v4;
 
+    KernelBase->kb_ContextSize = sizeof(struct AROSCPUContext);
+
+    /* Evaluate CPU capabilities */
     asm volatile("cpuid":"=a"(v1),"=b"(v2),"=c"(v3),"=d"(v4):"a"(1));
 
     if (v4 & (1 << 24))
@@ -24,15 +26,20 @@ static int cpu_Init(struct KernelBase *KernelBase)
 	    /* FPU + SSE */
 #ifdef USE_LEGACY_8087
 	    KernelBase->kb_ContextFlags = ECF_FPU|ECF_FPX;
+	    KernelBase->kb_ContextSize += 112;		/* 112 bytes for legacy 8087 frame */
 #else
 	    KernelBase->kb_ContextFlags = ECF_FPX;
 #endif
+	    KernelBase->kb_ContextSize += 512 + 15;	/* Add 15 bytes for alignment */
 	    break;
 	}
     }
     else
+    {
         /* FPU only */
 	KernelBase->kb_ContextFlags = ECF_FPU;
+	KernelBase->kb_ContextSize += 112;
+    }
 
     D(bug("[Kernel] CPU context flags: 0x%08X\n", KernelBase->kb_ContextFlags));
 
