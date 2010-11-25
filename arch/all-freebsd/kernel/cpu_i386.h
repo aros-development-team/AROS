ba717556bb092a646f1453871e06943f469de422
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-11-25 09:49:36 +0000

    Updated BSD ports to use unified CPU context
    git-svn-id: https://svn.aros.org/svn/aros/trunk@35755 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-freebsd/kernel/cpu_i386.h
+++ arch/all-freebsd/kernel/cpu_i386.h
@@ -3,15 +3,9 @@
     $Id$
 */
 
-#ifdef __AROS_EXEC_LIBRARY__
+#include <aros/i386/cpucontext.h>
 
-#define _MC_FPOWNED_NONE 0x020000
-#define _MC_FPFMT_NODEV  0x010000
-
-/* FreeBSD v4 is an ancient history now */
-#define __FreeBSD_version 500001
-
-#else
+#ifndef __AROS_EXEC_LIBRARY__
 
 #include <machine/psl.h>
 #include <sys/types.h>
@@ -37,7 +31,7 @@ typedef struct sigcontext regs_t;
 #define R3(sc)           (sc->sc_edx)
 #define R4(sc)           (sc->sc_edi)
 #define R5(sc)           (sc->sc_esi)
-#define R6(sc)           (sc->sc_isp)
+#define R6(sc)           (sc->sc_efl)
 
 #define GLOBAL_SIGNAL_INIT(sighandler) \
 	static void sighandler ## _gate (int sig, int code, struct sigcontext *sc) \
@@ -45,125 +39,132 @@ typedef struct sigcontext regs_t;
 	    sighandler( sig, (regs_t*)sc);             				   \
 	}
 
-#define SAVE_CPU(cc,sc)                                              \
-    do {                                                            \
-	(cc)->regs[0] = R0(sc);                                     \
-	(cc)->regs[1] = R1(sc);                                     \
-	(cc)->regs[2] = R2(sc);                                     \
-	(cc)->regs[3] = R3(sc);                                     \
-	(cc)->regs[4] = R4(sc);                                     \
-	(cc)->regs[5] = R5(sc);                                     \
-	(cc)->regs[6] = R6(sc);                                     \
-        (cc)->regs[7] = FP(sc);                                     \
-        (cc)->regs[8] = PC(sc);                                     \
-    } while (0)
+/* Save and restore the CPU GPRs in the CPU context */
+#define SAVE_CPU(cc, sc)	\
+    cc.eax    = R0(sc);		\
+    cc.ebx    = R1(sc);		\
+    cc.ecx    = R2(sc);		\
+    cc.edx    = R3(sc);		\
+    cc.edi    = R4(sc);		\
+    cc.esi    = R5(sc);		\
+    cc.eflags = R6(sc);		\
+    cc.ebp    = FP(sc);		\
+    cc.eip    = PC(sc);		\
+    cc.esp    = SP(sc);
 
-#define RESTORE_CPU(cc,sc)                                          \
-    do {                                                            \
-	R0(sc) = (cc)->regs[0];                                     \
-	R1(sc) = (cc)->regs[1];                                     \
-	R2(sc) = (cc)->regs[2];                                     \
-	R3(sc) = (cc)->regs[3];                                     \
-	R4(sc) = (cc)->regs[4];                                     \
-	R5(sc) = (cc)->regs[5];                                     \
-	R6(sc) = (cc)->regs[6];                                     \
-        FP(sc) = (cc)->regs[7];                                     \
-        PC(sc) = (cc)->regs[8];                                     \
-    } while (0)
+#define RESTORE_CPU(cc, sc)								 \
+    R0(sc) = cc.eax;									 \
+    R1(sc) = cc.ebx;									 \
+    R2(sc) = cc.ecx;    								 \
+    R3(sc) = cc.edx;									 \
+    R4(sc) = cc.edi;									 \
+    R5(sc) = cc.esi;        								 \
+    R6(sc) = sc->sc_efl = (sc->sc_efl & ~PSL_USERCHANGE) | (cc.eflags & PSL_USERCHANGE); \
+    FP(sc) = cc.ebp;									 \
+    PC(sc) = cc.eip;									 \
+    SP(sc) = cc.esp;
 
-#ifndef NO_FPU
+#if __FreeBSD_version >= 500001
+/*
+ * This is the FreeBSD 5.x and higher version
+ */
+#define SAVE_FPU(cc, sc)                                \
+{							\
+    APTR dest;						\
+    ULONG flag, len;					\
+    (cc)->fpformat = (sc)->sc_fpformat;			\
+    (cc)->ownedfp  = (sc)->sc_ownedfp;			\
+    switch (cc)->ownedfp				\
+    {							\
+    case _MC_FPOWNED_PCB:				\
+        flag = ECF_FPX; 				\
+    	dest = (cc)->regs.FPXData;			\
+    	len = sizeof(struct FPXContext);		\
+    	break;						\
+    case _MC_FPOWNED_FPU:				\
+        flag = ECF_FPU;					\
+    	dest = (cc)->regs.FPUData;			\
+    	len  = sizeof(struct FPUContext);		\
+    	break;						\
+    default:						\
+    	dest = NULL;					\
+    	break;						\
+    }							\
+    if (dest)						\
+    {							\
+        (cc)->regs.Flags |= flag;			\
+        CopyMemQuick(sc->sc_fpstate, dest, len);	\
+    }							\
+}
 
-#   if __FreeBSD_version >= 500001
-        /*
-         * This is the FreeBSD 5.x and higher version
-         */
-#       define SAVE_FPU(cc,sc)                                              \
-            do {                                                            \
-                int i_;                                                     \
-                (cc)->acc_u.acc_f5.fpformat = (sc)->sc_fpformat;            \
-                (cc)->acc_u.acc_f5.ownedfp = (sc)->sc_ownedfp;              \
-                for(i_ = 0; i_ < 128; ++i_)                                 \
-                    (cc)->acc_u.acc_f5.fpstate[i_] = (sc)->sc_fpstate[i_];  \
-            } while (0)
-
-#       define RESTORE_FPU(cc,sc)                                           \
-            do {                                                            \
-                int i_;                                                     \
-                (sc)->sc_fpformat = (cc)->acc_u.acc_f5.fpformat;            \
-                (sc)->sc_ownedfp = (cc)->acc_u.acc_f5.ownedfp;              \
-                for(i_ = 0; i_ < 128; ++i_)                                 \
-                    (sc)->sc_fpstate[i_] = (cc)->acc_u.acc_f5.fpstate[i_];  \
-            } while (0)
-
-#       define HAS_FPU(sc)                                                  \
-            (                                                               \
-                ((sc)->sc_ownedfp != _MC_FPOWNED_NONE)                      \
-                &&                                                          \
-                ((sc)->sc_fpformat != _MC_FPFMT_NODEV)                      \
-            )
-
-#   else
-
-        /*
-         * FreeBSD 4 and below have a different context format.
-         */
-#       define SAVE_FPU(cc,sc)                                              \
-            do {                                                            \
-                int i_;                                                     \
-                for (i_ = 0; i_ < 28; i_++)                                 \
-                    (cc)->acc_u.acc_f4.fpstate[i_] = (sc)->sc_fpregs[i_];   \
-            } while (0)
-
-#       define RESTORE_FPU(cc,sc)                                           \
-            do {                                                            \
-                int i_;                                                     \
-                for (i_ = 0; i_ < 28; i_++)                                 \
-                    (sc)->sc_fpregs[i_] = (cc)->acc_u.acc_f4.fpstate[i_];   \
-            } while (0)
-
-#       define HAS_FPU(sc)      0
-
-#   endif
+#define RESTORE_FPU(cc, sc)                             \
+{							\
+    APTR data;						\
+    ULONG flag, len;					\
+    if ((cc)->fpformat)					\
+    {							\
+	(sc)->sc_fpformat = (cc)->fpformat;	        \
+	(sc)->sc_ownedfp  = (cc)->ownedfp;           	\
+    }							\
+    switch (sc)->sc_ownedfp				\
+    {							\
+    case _MC_FPOWNED_PCB:				\
+        flag = ECF_FPX; 				\
+    	data = (cc)->regs.FPXData;			\
+    	len = sizeof(struct FPXContext);		\
+    	break;						\
+    case _MC_FPOWNED_FPU:				\
+        flag = ECF_FPU;					\
+    	data = (cc)->regs.FPUData;			\
+    	len  = sizeof(struct FPUContext);		\
+    	break;						\
+    default:						\
+    	data = NULL;					\
+    	break;						\
+    }							\
+    if ((cc)->regs.Flags & flag)			\
+        CopyMemQuick(data, sc->sc_fpstate, len);	\
+}
 
 #else
-    /* NO FPU VERSION */
 
-#   define SAVE_FPU(cc,sc)                                          \
-        do {                                                        \
-        } while (0)
+/*
+ * FreeBSD 4 and below have a different context format.
+ * Note that SSE is not supported at all. And on SSE machines
+ * legacy FPU context will not be saved (we don't define USE_LEGACY_8087
+ * in order to save some memory).
+ * This should not really matter because FreeBSD v4 is ancient history.
+ */
+#define SAVE_FPU(cc, sc)                                              		\
+    if ((cc)->FPUData)								\
+    {										\
+    	(cc)->Flags |= ECF_FPU;							\
+    	CopyMemQuick(sc->sc_fpregs, (cc)->FPUData, sizeof(struct FPUContext));	\
+    }
 
-#   define RESTORE_FPU(cc,sc)                                       \
-        do {                                                        \
-        } while (0)
-
-#   define HAS_FPU(sc)      0
+#define RESTORE_FPU(cc, sc)                                           		\
+    if ((cc)->Flags & ECF_FPU)							\
+    	CopyMemQuick((cc)->FPUData, sc->sc_fpregs, sizeof(struct FPUContext));
 
 #endif
 
-#define SAVEREGS(cc, sc)                                            \
-    do {                                                            \
-        if (HAS_FPU(sc))                                            \
-            SAVE_FPU(cc,sc);                                        \
-        SAVE_CPU(cc,sc);                                            \                                          \
-        cc->eflags = sc->sc_efl & PSL_USERCHANGE;                   \
-    } while (0)
+#define SAVEREGS(cc, sc)        	\
+    SAVE_CPU((cc)->regs, sc);		\
+    (cc)->isp = sc->sc_isp;		\
+    SAVE_FPU((cc), sc);			\
 
-#define RESTOREREGS(cc, sc)                                         \
-    do {                                                            \                                       \
-	RESTORE_CPU(cc,sc);                                         \
-        if (HAS_FPU(sc))                                            \
-            RESTORE_FPU(cc,sc);                                     \
-        sc->sc_efl = (sc->sc_efl & ~PSL_USERCHANGE) | cc->eflags;   \
-    } while (0)
+#define RESTOREREGS(cc, sc)     	\
+    RESTORE_CPU((cc)->regs, sc);	\
+    sc->sc_isp = (cc)->isp;		\
+    RESTORE_FPU((cc), sc);
 
-#define PRINT_SC(sc)                                                \
-	printf ("    SP=%08lx  FP=%08lx  PC=%08lx  FPU=%s\n"        \
-		"    R0=%08lx  R1=%08lx  R2=%08lx  R3=%08lx\n"      \
-		"    R4=%08lx  R5=%08lx  R6=%08lx\n"                \
-	    , SP(sc), FP(sc), PC(sc)                                \
-	    , HAS_FPU(sc) ? "yes" : "no"                            \
-	    , R0(sc), R1(sc), R2(sc), R3(sc)                        \
-	    , R4(sc), R5(sc), R6(sc)                                \
+#define PRINT_SC(sc)                                        \
+    bug("    SP=%08lx  FP=%08lx  PC=%08lx\n"        	    \
+	"    R0=%08lx  R1=%08lx  R2=%08lx  R3=%08lx\n"      \
+	"    R4=%08lx  R5=%08lx  R6=%08lx\n"                \
+	, SP(sc), FP(sc), PC(sc)                            \
+	, R0(sc), R1(sc), R2(sc), R3(sc)                    \
+	, R4(sc), R5(sc), R6(sc)                            \
 	)
 
 #endif /* __AROS_EXEC_LIBRARY__ */
@@ -172,57 +173,12 @@ typedef struct sigcontext regs_t;
 
 struct AROSCPUContext
 {
-    ULONG regs[9];	/* eax, ebx, ecx, edx, edi, esi, isp, fp, pc */
+    struct ExceptionContext regs; /* Public portion */
+    ULONG isp;			  /* Host-specific stuff follows */
+    int fpformat;
+    int ownedfp;
     int	errno_backup;
-    union
-    {
-        struct
-        {
-            int fpformat;
-            int ownedfp;
-            int fpstate[128] __attribute__ ((aligned (16)));
-        }
-            acc_f5;
-
-        struct
-        {
-            int fpstate[17];
-        }
-            acc_f4;
-    }
-        acc_u;
-
-    int eflags;
 };
 
-#define GET_PC(ctx) ((APTR)ctx->regs[8])
-#define SET_PC(ctx, val) ctx->regs[8] = (ULONG)val
-
-#ifndef NO_FPU
-
-#if __FreeBSD_version >= 500001
-/*
- * This is the FreeBSD 5.x and higher version
- */
-#define PREPARE_INITIAL_CONTEXT(cc)                                         \
-            do {                                                            \
-                (cc)->acc_u.acc_f5.fpformat = _MC_FPFMT_NODEV;              \
-                (cc)->acc_u.acc_f5.ownedfp = _MC_FPOWNED_NONE;              \
-            } while (0)
-#else
-
-#define PREPARE_INITIAL_CONTEXT(cc)                                         \
-            do {                                                            \
-                asm volatile("fninit\n\t"                                   \
-                             "fnsave %0\n\t"                                \
-                             "fwait" : "=m" ((cc)->acc_u.acc_f4.fpstate)    \
-                             );                                             \
-            } while (0)
-
-#endif
-
-#define PREPARE_INITIAL_FRAME(cc, sp, startpc)     \
-    do {                                           \
-        cc->regs[7] = 0;                           \
-        cc->regs[8] = (startpc);                   \
-    } while (0)
+#define GET_PC(ctx) (APTR)ctx->regs.eip
+#define SET_PC(ctx, pc) ctx->regs.eip = (ULONG)pc
--- arch/all-netbsd/kernel/cpu_i386.h
+++ arch/all-netbsd/kernel/cpu_i386.h
@@ -32,48 +32,50 @@ typedef struct sigcontext regs_t;
 	    sighandler( sig, (regs_t*)sc);       \
 	}
 
-#define SAVE_CPU(ctx, sc)	 \
-	(ctx->regs[0] = R0(sc)), \
-	(ctx->regs[1] = R1(sc)), \
-	(ctx->regs[2] = R2(sc)), \
-	(ctx->regs[3] = R3(sc)), \
-	(ctx->regs[4] = R4(sc)), \
-	(ctx->regs[5] = R5(sc)), \
-	(ctx->regs[6] = R6(sc))
+#define SAVE_CPU(cc, sc)	\
+    cc.eax    = R0(sc);		\
+    cc.ebx    = R1(sc);		\
+    cc.ecx    = R2(sc);		\
+    cc.edx    = R3(sc);		\
+    cc.edi    = R4(sc);		\
+    cc.esi    = R5(sc);		\
+    cc.eflags = R6(sc);		\
+    cc.ebp    = FP(sc);		\
+    cc.eip    = PC(sc);		\
+    cc.esp    = SP(sc);		\
 
-#define RESTORE_CPU(ctx,sc)	  \
-	((R0(sc) = ctx->regs[0]), \
-	(R1(sc) = ctx->regs[1]),  \
-	(R2(sc) = ctx->regs[2]),  \
-	(R3(sc) = ctx->regs[3]),  \
-	(R4(sc) = ctx->regs[4]),  \
-	(R5(sc) = ctx->regs[5]),  \
-	(R6(sc) = ctx->regs[6]))
+/*
+ * Restore CPU registers.
+ * Note that we do not restore segment registers because they
+ * are of own use by the host OS.
+ */
+#define RESTORE_CPU(cc, sc) \
+    R0(sc) = cc.eax;        \
+    R1(sc) = cc.ebx;        \
+    R2(sc) = cc.ecx;        \
+    R3(sc) = cc.edx;        \
+    R4(sc) = cc.edi;        \
+    R5(sc) = cc.esi;        \
+    R6(sc) = cc.eflags;     \
+    FP(sc) = cc.ebp;        \
+    PC(sc) = cc.eip;        \
+    SP(sc) = cc.esp;
 
-#define HAS_FPU		0
-#define SAVE_FPU(task,sc)	/* nop */
-#define RESTORE_FPU(task,sc)	/* nop */
+/* TODO: FPU/SSE support */
 
-#define SAVEREGS(ctx, sc)     \
-	((ctx)->pc = PC(sc)), \
-	((ctx)->fp = FP(sc)), \
-	/* SAVE_FPU((ctx), sc), */ \
-	SAVE_CPU((ctx), sc)
+#define SAVEREGS(ctx, sc)     		\
+    SAVE_CPU((ctx)->regs, sc);
 
-#define RESTOREREGS(ctx, sc) \
-	RESTORE_CPU((ctx),sc), \
-	/* RESTORE_FPU((ctx), sc), */ \
-	(FP(sc) = (ctx)->fp), \
-	(PC(sc) = (ctx)->pc))
+#define RESTOREREGS(ctx, sc)		\
+    RESTORE_CPU((ctx)->regs, sc);
 
 #define PRINT_SC(sc) \
-	printf ("    SP=%08lx  FP=%08lx  PC=%08lx  FPU=%s\n" \
-		"    R0=%08lx  R1=%08lx  R2=%08lx  R3=%08lx\n" \
-		"    R4=%08lx  R5=%08lx  R6=%08lx\n" \
-	    , SP(sc), FP(sc), PC(sc) \
-	    , HAS_FPU(sc) ? "yes" : "no" \
-	    , R0(sc), R1(sc), R2(sc), R3(sc) \
-	    , R4(sc), R5(sc), R6(sc) \
+    bug("    SP=%08lx  FP=%08lx  PC=%08lx\n" \
+	"    R0=%08lx  R1=%08lx  R2=%08lx  R3=%08lx\n" \
+	"    R4=%08lx  R5=%08lx  R6=%08lx\n" \
+	, SP(sc), FP(sc), PC(sc) \
+	, R0(sc), R1(sc), R2(sc), R3(sc) \
+	, R4(sc), R5(sc), R6(sc) \
 	)
 
 #endif /* __AROS_EXEC_LIBRARY__ */
@@ -82,30 +84,9 @@ typedef struct sigcontext regs_t;
 
 struct AROSCPUContext
 {
-    ULONG regs[7];	/* eax, ebx, ecx, edx, edi, esi, isp */
-    ULONG pc,fp;	/* store these on the stack to avoid sighandlers */
-    int	errno_backup;
+    struct ExceptionContext regs;
+    int errno_backup;
 };
 
-#define GET_PC(ctx) ((APTR)ctx->pc)
-#define SET_PC(ctx, val) ctx->pc = (ULONG)val
-
-#define PREPARE_INITIAL_CONTEXT(ctx) 	/* nop */
-
-#define PREPARE_INITIAL_FRAME(ctx, sp, startpc) \
-	ctx->pc = (ULONG)startpc; \
-	ctx->fp = 0
-
-#define PRINT_CPU_CONTEXT(ctx) \
-	printf ("    FP=%08lx  PC=%08lx\n" \
-		"    R0=%08lx  R1=%08lx  R2=%08lx  R3=%08lx\n" \
-		"    R4=%08lx  R5=%08lx  R6=%08lx\n" \
-	    , ctx->fp, ctx->pc, \
-	    , ctx->regs[0] \
-	    , ctx->regs[1] \
-	    , ctx->regs[2] \
-	    , ctx->regs[3] \
-	    , ctx->regs[4] \
-	    , ctx->regs[5] \
-	    , ctx->regs[6] \
-	)
+#define GET_PC(ctx) (APTR)ctx->regs.eip
+#define SET_PC(ctx, pc) ctx->regs.eip = (ULONG)pc
