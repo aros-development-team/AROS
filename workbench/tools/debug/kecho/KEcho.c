48569a5161e9cc78e23a2959c8694df549f61d90
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-12-02 21:00:49 +0000

    Added a new tool to write to debug output.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@35863 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/tools/debug/kecho/KEcho.c
@@ -0,0 +1,85 @@
+/*
+    Copyright © 1995-2010, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc:
+    Lang:
+*/
+
+/******************************************************************************
+
+    NAME
+
+        KEcho [<string>] [NOLINE]
+
+    SYNOPSIS
+
+        STRINGS/M,NOLINE/S
+
+    LOCATION
+
+       SYS:tests
+
+    FUNCTION
+
+        Appends one or more strings to the debug output.
+
+    INPUTS
+
+        STRINGS -- the strings to display
+        NOLINE -- no newline after the last string
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+******************************************************************************/
+
+#define DEBUG 0
+
+#include <exec/execbase.h>
+#include <exec/libraries.h>
+#include <proto/exec.h>
+#include <dos/dos.h>
+#include <proto/dos.h>
+#include <string.h>
+#include <aros/shcommands.h>
+#include <aros/debug.h>
+
+
+AROS_SH2(KEcho, 41.1,
+AROS_SHA(STRPTR *, , STRINGS, /M,   NULL),
+AROS_SHA(BOOL,     , NOLINE, /S,   FALSE))
+{
+    AROS_SHCOMMAND_INIT
+
+    LONG error = 0;
+    STRPTR *a, b;
+
+    a = SHArg(STRINGS);
+
+    if (a) while (*a != NULL)
+    {
+        b = *a;
+        kprintf("%s", *a);
+	a++;
+        if (*a)
+            kprintf(" ");
+    }
+
+    if (!SHArg(NOLINE))
+        kprintf("\n");
+
+    return error;
+
+    AROS_SHCOMMAND_EXIT
+}
+
