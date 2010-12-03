084e309ef3aa5a0de89e1f0f67e805e9eeb9e974
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-12-03 20:27:34 +0000

    rom/dos: Use stubs files instead of .alias in dos.conf
    This is needed for the DOS Packet support on Amiga m68k.
    
    Please report any regressions on other architectures to me.
    
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@35880 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/dos/unlock.c
@@ -0,0 +1,55 @@
+/*
+    Copyright © 1995-2007, The AROS Development Team. All rights reserved.
+    $Id: close.c 34705 2010-10-13 20:30:16Z jmcmullan $
+
+    Desc:
+    Lang: English
+*/
+
+#include <proto/exec.h>
+#include <dos/dosextens.h>
+#include <dos/filesystem.h>
+#include <proto/dos.h>
+#include "dos_intern.h"
+#include <aros/debug.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/dos.h>
+
+	AROS_LH1(BOOL, UnLock,
+
+/*  SYNOPSIS */
+	AROS_LHA(BPTR, lock, D1),
+
+/*  LOCATION */
+	struct DosLibrary *, DOSBase, 15, Dos)
+
+/*
+    FUNCTION
+	Free a lock created with Lock().
+
+    INPUTS
+	lock -- The lock to free
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    return Close(lock);
+
+    AROS_LIBFUNC_EXIT
+} /* UnLock */
