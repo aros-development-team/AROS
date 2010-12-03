7645015216a76e58e5db66d4cd22395f60140253
ezrec <jason.mcmullan@gmail.com>
2010-12-03 20:27:34 +0000

    rom/dos: Use stubs files instead of .alias in dos.conf
    This is needed for the DOS Packet support on Amiga m68k.
    
    Please report any regressions on other architectures to me.
    
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@35880 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/dos/duplockfromfh.c
@@ -0,0 +1,54 @@
+/*
+    Copyright © 1995-2007, The AROS Development Team. All rights reserved.
+    $Id: duplock.c 30792 2009-03-07 22:40:04Z neil $
+
+    Desc: dos.library function DupLockFromFH()
+    Lang: english
+*/
+
+#include <aros/debug.h>
+#include <proto/exec.h>
+#include "dos_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/dos.h>
+
+	AROS_LH1(BPTR, DupLockFromFH,
+
+/*  SYNOPSIS */
+	AROS_LHA(BPTR, handle, D1),
+
+/*  LOCATION */
+	struct DosLibrary *, DOSBase, 62, Dos)
+
+/*  FUNCTION
+	Clone a lock on a file or directory. This will only work on shared
+	locks.
+
+    INPUTS
+	lock - Old lock.
+
+    RESULT
+	The new lock or NULL in case of an error. IoErr() will give additional
+	information in that case.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    return DupLock(handle);
+
+    AROS_LIBFUNC_EXIT
+} /* DupLockFromFH */
---
+++ rom/dos/examinefh.c
@@ -0,0 +1,52 @@
+/*
+    Copyright © 1995-2010, The AROS Development Team. All rights reserved.
+    $Id: examine.c 32949 2010-04-08 23:57:16Z neil $
+
+    Desc: dos.library function ExamineFH().
+    Lang: English
+*/
+
+#include <aros/debug.h>
+#include <exec/memory.h>
+#include <proto/exec.h>
+#include <dos/filesystem.h>
+#include <dos/exall.h>
+#include "dos_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/dos.h>
+
+	AROS_LH2(BOOL, ExamineFH,
+
+/*  SYNOPSIS */
+	AROS_LHA(BPTR,                   handle, D1),
+	AROS_LHA(struct FileInfoBlock *, fib,    D2),
+
+/*  LOCATION */
+	struct DosLibrary *, DOSBase, 65, Dos)
+/*
+    FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+*/
+{
+    AROS_LIBFUNC_INIT
+
+    return Examine(handle, fib);
+
+    AROS_LIBFUNC_EXIT
+} /* ExamineFH */
