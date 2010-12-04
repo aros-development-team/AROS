914f267df436820bbc3987599aa4bce00b996717
DizzyOfCRN <DizzyOfCRN@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-12-04 13:24:08 +0000

    Cosmetic changes
    git-svn-id: https://svn.aros.org/svn/aros/trunk@35899 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/usb/pciusb/pci_aros.h
+++ rom/usb/pciusb/pci_aros.h
@@ -21,21 +21,21 @@
 
 #include <aros/io.h>
 
-#define READMEM16_LE(rb)                 AROS_WORD2LE(*((volatile UWORD *) (rb)))
-#define READMEM32_LE(rb)                 AROS_LONG2LE(*((volatile ULONG *) (rb)))
-#define	WRITEMEM32_LE(adr, value)	     *((volatile ULONG *) (adr)) = AROS_LONG2LE(value)
-#define CONSTWRITEMEM32_LE(adr, value)   *((volatile ULONG *) (adr)) = AROS_LONG2LE(value)
+#define READMEM16_LE(rb) AROS_WORD2LE(*((volatile UWORD *) (rb)))
+#define READMEM32_LE(rb) AROS_LONG2LE(*((volatile ULONG *) (rb)))
+#define	WRITEMEM32_LE(adr, value)	   *((volatile ULONG *) (adr)) = AROS_LONG2LE(value)
+#define CONSTWRITEMEM32_LE(adr, value) *((volatile ULONG *) (adr)) = AROS_LONG2LE(value)
 
 #define CONSTWRITEREG16_LE(rb, offset, value) *((volatile UWORD *) (((UBYTE *) (rb)) + ((ULONG) (offset)))) = AROS_WORD2LE(value)
 #define CONSTWRITEREG32_LE(rb, offset, value) *((volatile ULONG *) (((UBYTE *) (rb)) + ((ULONG) (offset)))) = AROS_LONG2LE(value)
-#define WRITEREG16_LE(rb, offset, value) *((volatile UWORD *) (((UBYTE *) (rb)) + ((ULONG) (offset)))) = AROS_WORD2LE(value)
-#define WRITEREG32_LE(rb, offset, value) *((volatile ULONG *) (((UBYTE *) (rb)) + ((ULONG) (offset)))) = AROS_LONG2LE(value)
+#define WRITEREG16_LE(rb, offset, value)      *((volatile UWORD *) (((UBYTE *) (rb)) + ((ULONG) (offset)))) = AROS_WORD2LE(value)
+#define WRITEREG32_LE(rb, offset, value)      *((volatile ULONG *) (((UBYTE *) (rb)) + ((ULONG) (offset)))) = AROS_LONG2LE(value)
 
-#define READREG16_LE(rb, offset)         AROS_WORD2LE(*((volatile UWORD *) (((UBYTE *) (rb)) + ((ULONG) (offset)))))
-#define READREG32_LE(rb, offset)         AROS_LONG2LE(*((volatile ULONG *) (((UBYTE *) (rb)) + ((ULONG) (offset)))))
+#define READREG16_LE(rb, offset) AROS_WORD2LE(*((volatile UWORD *) (((UBYTE *) (rb)) + ((ULONG) (offset)))))
+#define READREG32_LE(rb, offset) AROS_LONG2LE(*((volatile ULONG *) (((UBYTE *) (rb)) + ((ULONG) (offset)))))
 
-#define READIO16_LE(rb, offset)          AROS_WORD2LE(WORDIN((((UBYTE *) (rb)) + ((ULONG) (offset)))))
-#define WRITEIO16_LE(rb, offset, value)  WORDOUT((((UBYTE *) (rb)) + ((ULONG) (offset))), AROS_WORD2LE(value))
-#define WRITEIO32_LE(rb, offset, value)  LONGOUT((((UBYTE *) (rb)) + ((ULONG) (offset))), AROS_WORD2LE(value))
+#define READIO16_LE(rb, offset) AROS_WORD2LE(WORDIN((((UBYTE *) (rb)) + ((ULONG) (offset)))))
+#define WRITEIO16_LE(rb, offset, value) WORDOUT((((UBYTE *) (rb)) + ((ULONG) (offset))), AROS_WORD2LE(value))
+#define WRITEIO32_LE(rb, offset, value) LONGOUT((((UBYTE *) (rb)) + ((ULONG) (offset))), AROS_WORD2LE(value))
 
 #endif // PCI_AROS_H
