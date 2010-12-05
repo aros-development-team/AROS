bd421ecada94aa7ead0acd03f2cc264f0b216440
twilen <twilen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-12-05 09:19:52 +0000

    More dos packet converted dos functions added.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@35929 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/m68k-amiga/dos/setmode.c
@@ -0,0 +1,62 @@
+/*
+    Copyright © 1995-2007, The AROS Development Team. All rights reserved.
+    $Id: setmode.c 30792 2009-03-07 22:40:04Z neil $
+
+    Desc: Set the current mode of a console device.
+    Lang: english
+*/
+#include "dos_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/dos.h>
+
+	AROS_LH2(LONG, SetMode,
+
+/*  SYNOPSIS */
+	AROS_LHA(BPTR, fh, D1),
+	AROS_LHA(LONG, mode, D2),
+
+/*  LOCATION */
+	struct DosLibrary *, DOSBase, 71, Dos)
+
+/*  FUNCTION
+	SetMode() can be used to change a console handler between
+	RAW: mode and CON: mode.
+
+    INPUTS
+	fh      -   The filehandle describing the console.
+	mode    -   The new mode of the console:
+			1   - RAW: mode
+			0   - CON: mode
+
+    RESULT
+	This function will return whether it succeeded:
+
+	== DOSTRUE  console mode changed
+	!= DOSTRUE  console mode change failed.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    struct FileHandle *fh = BADDR(fh);
+    LONG status;
+    
+    status = dopacket1(DOSBase, NULL, fh->fh_Type, ACTION_SCREEN_MODE, mode);
+    
+    return status;
+
+    AROS_LIBFUNC_EXIT
+} /* SetMode */
