a5baaf91465a44a2c25f2d8132037d83e5f2e892
twilen <twilen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-12-06 17:52:00 +0000

    added setcomment and setfiledate dos packet version
    git-svn-id: https://svn.aros.org/svn/aros/trunk@35971 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/m68k-amiga/dos/setfiledate.c
@@ -0,0 +1,70 @@
+/*
+    Copyright © 1995-2008, The AROS Development Team. All rights reserved.
+    $Id: setfiledate.c 30792 2009-03-07 22:40:04Z neil $
+
+    Desc: Change the date of a file.
+    Lang: English
+*/
+#define DEBUG 0
+#include <aros/debug.h>
+#include <string.h>
+#include <proto/exec.h>
+#include <dos/dosextens.h>
+#include <dos/filesystem.h>
+#include <proto/dos.h>
+#include "dos_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/dos.h>
+#include <dos/dos.h>
+
+	AROS_LH2(BOOL, SetFileDate,
+
+/*  SYNOPSIS */
+	AROS_LHA(CONST_STRPTR,       name, D1),
+	AROS_LHA(const struct DateStamp *, date, D2),
+
+/*  LOCATION */
+	struct DosLibrary *, DOSBase, 66, Dos)
+
+/*  FUNCTION
+	Change the modification time of a file or directory.
+
+    INPUTS
+	name - name of the file
+	date - new file time
+
+    RESULT
+	Boolean success indicator. IoErr() gives additional information upon
+	failure.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    struct PacketHelperStruct phs;
+    LONG status = DOSFALSE;
+
+    D(bug("[SetFileDate] '%s' %x\n", name, date));
+
+    if (getpacketinfo(DOSBase, name, &phs)) {
+    	status = dopacket4(DOSBase, NULL, phs.port, ACTION_SET_DATE, NULL, phs.lock, phs.name, date);
+    	freepacketinfo(DOSBase, &phs);
+    }
+
+    return status;
+
+    AROS_LIBFUNC_EXIT
+} /* SetFileDate */
