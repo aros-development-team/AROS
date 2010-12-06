c1155fcfd88b7c64189a8b8feaa4e32701d4cc4f
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-12-06 04:43:50 +0000

    arch/m68k-amiga: Unbreak other architectures that need con.handler
    The mmake that was here was trampling other architectures
    con.handler builds. Fixed.
    
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@35952 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/m68k-amiga/devs/filesys/console_handler/resident.c
+++ arch/m68k-amiga/devs/filesys/console_handler/resident.c
@@ -1,6 +1,7 @@
 
 #define DEBUG 1
 #include <aros/debug.h>
+#include <aros/symbolsets.h>
 #include <exec/types.h>
 #include <exec/resident.h>
 #include <exec/alerts.h>
@@ -13,71 +14,56 @@
 #include "con_handler_intern.h"
 #include "support.h"
 
-#define _STR(A) #A
-#define STR(A) _STR(A)
-
-#define HANDLER_NAME "console.handler"
-#define VERSION 43
-#define REVISION 1
-
-#ifdef __AROS__
-#define rom_tag Con_ROMTag
-#endif
+#include LC_LIBDEFS_FILE
 
 extern LONG CONMain(void);
 
-static AROS_UFP3 (APTR, Init,
-		  AROS_UFPA(struct Library *, lh, D0),
-		  AROS_UFPA(BPTR, segList, A0),
-		  AROS_UFPA(struct ExecBase *, sysBase, A6));
-
-static const TEXT handler_name[] = HANDLER_NAME;
-static const TEXT version_string[] =
-   HANDLER_NAME " " STR(VERSION) "." STR(REVISION) " (" __DATE__ ")\n";
 static const TEXT dev_name_con[] = "CON";
 static const TEXT dev_name_raw[] = "RAW";
 
-extern const TEXT dos_name[];
-
-const struct Resident rom_tag =
-{
-   RTC_MATCHWORD,
-   (struct Resident *)&rom_tag,
-   (APTR)(&rom_tag + 1),
-   RTF_AFTERDOS,
-   VERSION,
-   NT_PROCESS,
-   -125,
-   (STRPTR)handler_name,
-   (STRPTR)version_string,
-   (APTR)Init
+/* This is both a segment, and the data for the segment.
+ * We will feed in to DOS/AddSegment() the BPTR to 
+ * &aws_Next as the 'seglist' to add.
+ */
+struct CON_Seg {
+    ULONG              cs_Size;      /* Length of segment in # of ULONGs */
+    ULONG              cs_Next;      /* Next segment (always 0 for this) */
+    struct FullJumpVec cs_Code;      /* Code to jump to CONMain */
 };
 
-#ifdef DOSBase
 #undef DOSBase
-#endif
 
-static AROS_UFH3 (APTR, Init,
-		  AROS_UFHA(struct Library *, lh, D0),
-		  AROS_UFHA(BPTR, segList, A0),
-		  AROS_UFHA(struct ExecBase *, sysBase, A6)
-)
+static int GM_UNIQUENAME(Init)(LIBBASETYPEPTR conbase)
 {
-   struct DosLibrary *DOSBase;
+   APTR DOSBase;
    struct DeviceNode *dev_node;
+   struct CON_Seg *cs_con;
 
-    AROS_USERFUNC_INIT
+   cs_con = AllocMem(sizeof(*cs_con), MEMF_ANY);
+   if (cs_con == NULL) {
+       Alert(AT_DeadEnd | AG_NoMemory);
+       return FALSE;
+   }
 
    /* Create device node and add it to the system. The handler will then be
       started when it is first accessed */
+   cs_con->cs_Size = 0;	/* Must be 0 to prevent UnLoadSeg */
+   cs_con->cs_Next = 0;	/* from killing us.               */
+   __AROS_SET_FULLJMP(&cs_con->cs_Code, CONMain);
+
 
    DOSBase = (struct DosLibrary *)OpenLibrary("dos.library", 0);
+   if (DOSBase == NULL) {
+       FreeMem(cs_con, sizeof(*cs_con));
+       Alert(AT_DeadEnd | AG_OpenLib | AO_DOSLib);
+       return FALSE;
+   }
 
    dev_node = (APTR)MakeDosEntry(dev_name_con, DLT_DEVICE);
    if(dev_node == NULL)
       Alert(AT_DeadEnd | AG_NoMemory);
-   dev_node->dn_StackSize = 10000;
-   dev_node->dn_SegList = MKBADDR((BPTR *)CONMain - 1);
+   dev_node->dn_StackSize = AROS_STACKSIZE;
+   dev_node->dn_SegList = MKBADDR(&cs_con->cs_Next);
    dev_node->dn_Startup = 0;
    if(!AddDosEntry((APTR)dev_node))
       Alert(AT_DeadEnd);
@@ -85,15 +71,36 @@ static AROS_UFH3 (APTR, Init,
    dev_node = (APTR)MakeDosEntry(dev_name_raw, DLT_DEVICE);
    if(dev_node == NULL)
       Alert(AT_DeadEnd | AG_NoMemory);
-   dev_node->dn_StackSize = 10000;
-   dev_node->dn_SegList = MKBADDR((BPTR *)CONMain - 1);
+   dev_node->dn_StackSize = AROS_STACKSIZE;
+   dev_node->dn_SegList = MKBADDR(&cs_con->cs_Next);
    dev_node->dn_Startup = 1;
    if(!AddDosEntry((APTR)dev_node))
       Alert(AT_DeadEnd);
 
    D(bug("CON: and RAW: added\n"));
 
-   AROS_USERFUNC_EXIT
-
-   return NULL;
+   CloseLibrary(DOSBase);
+   return TRUE;
 }
+
+/* NOTE: This is only here because architectures cannot
+ *       override a libraries's *.conf file
+ */
+AROS_LH1(void, beginio,
+ AROS_LHA(struct IOFileSys *, iofs, A1),
+           struct conbase *, conbase, 5, Con)
+{
+    AROS_LIBFUNC_INIT
+
+    AROS_LIBFUNC_EXIT
+}
+
+AROS_LH1(LONG, abortio,
+ AROS_LHA(struct IOFileSys *, iofs, A1),
+           struct conbase *, conbase, 6, Con)
+{
+	AROS_LIBFUNC_INIT
+	return 0;
+	AROS_LIBFUNC_EXIT
+}
+ADD2INITLIB(GM_UNIQUENAME(Init),0)
