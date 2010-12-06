d5d108707c9d097dde81c99bda47dff173786733
twilen <twilen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-12-06 17:52:00 +0000

    added setcomment and setfiledate dos packet version
    git-svn-id: https://svn.aros.org/svn/aros/trunk@35971 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/m68k-amiga/dos/setcomment.c
@@ -0,0 +1,77 @@
+/*
+    Copyright © 1995-2008, The AROS Development Team. All rights reserved.
+    $Id: setcomment.c 30792 2009-03-07 22:40:04Z neil $
+
+    Desc: Set a filecomment.
+    Lang: english
+*/
+#define DEBUG 0
+#include <aros/debug.h>
+#include <proto/exec.h>
+#include <dos/dosextens.h>
+#include <dos/filesystem.h>
+#include <proto/dos.h>
+#include "dos_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/dos.h>
+
+	AROS_LH2(BOOL, SetComment,
+
+/*  SYNOPSIS */
+	AROS_LHA(CONST_STRPTR, name,    D1),
+	AROS_LHA(CONST_STRPTR, comment, D2),
+
+/*  LOCATION */
+	struct DosLibrary *, DOSBase, 30, Dos)
+
+/*  FUNCTION
+	Change the comment on a file or directory. The comment may be any
+	NUL-terminated string. The supported size varies from filesystem
+	to filesystem. In order to clear an existing comment, an empty
+	string should be specified.
+
+    INPUTS
+	name	- name of the file
+	comment - new comment for the file.
+
+    RESULT
+	Boolean success indicator. IoErr() gives additional information upon
+	failure.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    struct PacketHelperStruct phs;
+    LONG status = DOSFALSE;
+
+    D(bug("[SetComment] '%s' '%s'\n", name, comment));
+
+    if (getpacketinfo(DOSBase, name, &phs)) {
+    	BSTR com = C2BSTR(comment);
+    	if (com) {
+    	    status = dopacket4(DOSBase, NULL, phs.port, ACTION_SET_COMMENT, NULL, phs.lock, phs.name, com);
+    	    FreeVec(BADDR(com));
+    	} else {
+    	    SetIoErr(ERROR_NO_FREE_STORE);
+    	}
+    	freepacketinfo(DOSBase, &phs);
+    }
+
+    return status;
+
+    AROS_LIBFUNC_EXIT
+} /* SetComment */
