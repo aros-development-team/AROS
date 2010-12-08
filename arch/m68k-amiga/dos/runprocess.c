818b32b2e4236bf1c73fcfbebe0d3f690c516016
ezrec <jason.mcmullan@gmail.com>
2010-12-08 05:34:02 +0000

    arch/m68k-amiga: BCPL - C:Echo works with ReadArgs
    BCPL C:Echo now seems to work with ReadArgs properly..
    
    
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36004 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/m68k-amiga/dos/runprocess.c
+++ arch/m68k-amiga/dos/runprocess.c
@@ -19,7 +19,7 @@
  *
  * D0 - Length of arg string
  * A0 - Pointer to arg string
- * A1 - BCPL 'reverse' stack pointer - SP-1500
+ * A1 - BCPL 'reverse' stack pointer - tc_SPLower
  * A2 - Global vector
  * A5 - BCPL 'jsr' routine
  * A6 - BCPL 'rts' routine
@@ -29,35 +29,35 @@ static ULONG CallEntry(APTR pReturn_Addr, struct StackSwapStruct* sss,
 {
     struct Process *me = (struct Process *)FindTask(NULL);
     ULONG ret;
-    ULONG _n1 = (ULONG)(argptr);
-    ULONG _n2 = (ULONG)(argsize);
-    ULONG _n3 = (ULONG)(SysBase);
     APTR pr_GlobVec = me->pr_GlobVec;
     APTR tc_SPLower = me->pr_Task.tc_SPLower;
 
     __asm__ __volatile__(
 	"move.l %%sp,%%a0\n\t"
 	"movem.l %%d2-%%d7/%%a2-%%a6,%%a0@-\n\t"
-	"move.l %5,%%a0@-\n\t"          /* sp+ 16 = SysBase */
-	"move.l %4,%%a0@-\n\t"          /* sp+ 12 = argsize */
-	"move.l %3,%%a0@-\n\t"          /* sp+ 8 = argptr */
+	"move.l %3,%%d0\n\t"            /* stksize = %a0 - tc_SPLower */
+	"neg.l  %%d0\n\t"
+	"lea.l  %%a0@(%%d0),%%a1\n\t"
+	"move.l %%a1,%%a0@-\n\t"        /* sp+ 8 = stksize  */
 	"move.l #0f,%%a0@-\n\t"         /* sp+ 4 = return address */
 	"move.l %%a0@,%1\n\t"           /* Save return address */
 	"move.l %2,%%a0@-\n\t"          /* sp+ 0 = address to go to */
-	"move.l %6,%%a2\n\t"            /* A2 - Global Vector */
-	"move.l %7,%%a1\n\t"            /* A1 - BCPL frame */
-	"move.l %%a0,%%sp\n\t"
-	"move.l %%sp@(8),%%a0\n\t"
-	"move.l %%sp@(12),%%d0\n\t"
+	"move.l %%a0,%%d1\n\t"
+	"move.l %3,%%a2\n\t"            /* A2 - Global Vector */
+	"move.l %4,%%a1\n\t"            /* A1 - BCPL frame */
+	"move.l %5,%%a0\n\t"            /* A0 - Argptr */
+	"move.l %6,%%d0\n\t"            /* D0 - Argsize */
+	"move.l %%d1,%%sp\n\t"
 	"lea.l  BCPL_jsr,%%a5\n\t"      /* A5 - BCPL jsr routine */
 	"lea.l  BCPL_rts,%%a6\n\t"      /* A6 - BCPL rts routine */
 	"rts    \n\t"
 	"0:\n\t"
-	"lea.l   %%sp@(12),%%sp\n\t"
+	"addq.l  #4,%%sp\n\t"
 	"movem.l %%sp@+,%%d2-%%d7/%%a2-%%a6\n\t"
 	"move.l  %%d0,%0"
 	: "=g" (ret), "=m"(*(APTR *)pReturn_Addr)
-	: "m" (entry), "m"(_n1), "m"(_n2), "m"(_n3), "m"(pr_GlobVec), "m"(tc_SPLower)
+	: "m" (entry), "m"(pr_GlobVec), "m"(tc_SPLower),
+	  "m" (argptr), "m" (argsize)
 	: "cc", "memory", "%d0", "%d1", "%a0", "%a1" );
 
     return ret;
