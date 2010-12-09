16534a3752063b6c915ff6316884d64a723cf97f
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-12-09 10:37:15 +0000

    - Improved nested alert processing logic - Some code refactoring, separated platform-specific parts of Alert()
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36017 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/alert.c
+++ rom/exec/alert.c
@@ -16,79 +16,10 @@
 #include "exec_intern.h"
 #include "exec_util.h"
 
-#define ALERT_WIDTH 80
-
 /* x86/64 kernel.resource doesn't have KrnIsSuper() */
 #ifndef KrnIsSuper
 #define KrnIsSuper() 0
 #endif
-
-static void PrintChars(char c, ULONG n)
-{
-    while (n--)
-        RawPutChar(c);
-}
-
-static int linelen(char *str)
-{
-    int l;
-
-    for (l = 0; str[l] && str[l] != '\n'; l++);
-    return l;
-}
-
-static char *PrintCentered(char *str)
-{
-    int len = linelen(str);
-    int i;
-    ULONG s;
-
-    if (len > (ALERT_WIDTH - 2))
-    	    len = (ALERT_WIDTH - 2);
-
-    s = ALERT_WIDTH - 2 - len;
-    
-    RawPutChar('#');
-    if (s & 1)
-        RawPutChar(' ');
-    s >>= 1;
-    PrintChars(' ', s);
-    for (i = 0; i < len; i++)
-        RawPutChar(str[i]);
-    PrintChars(' ', s);
-    RawPutChar('#');
-    RawPutChar('\n');
-
-    return &str[len];
-}
-
-static char *PrintLeftJustified(char *str)
-{
-    int len = linelen(str);
-    int i;
-    ULONG s;
-
-    if (len > (ALERT_WIDTH - 3))
-    	    len = (ALERT_WIDTH - 3);
-
-    s = ALERT_WIDTH - 3 - len;
-
-    RawPutChar('#');
-    RawPutChar(' ');
-    for (i = 0; i < len; i++)
-        RawPutChar(str[i]);
-    PrintChars(' ', s);
-    RawPutChar('#');
-    RawPutChar('\n');
-
-    return &str[len];
-}
-
-static void PrintFrame(void)
-{
-    PrintChars('#', ALERT_WIDTH);
-    RawPutChar('\n');
-}
 
 /*****************************************************************************
 
@@ -134,41 +65,23 @@ static void PrintFrame(void)
 {
     AROS_LIBFUNC_INIT
 
-    struct Task *task = SysBase->ThisTask;
-    char *buf;
-
-    /* If we are running in user mode we should first try to report a problem using AROS'
-       own way to do it */
+    /*
+     * If we are running in user mode we should first try to report a problem
+     * using Intuition display.
+     */
     if (!KrnIsSuper())
     {
-        alertNum = Exec_UserAlert(alertNum, task, SysBase);
+        alertNum = Exec_UserAlert(alertNum, SysBase);
 	if (!alertNum)
 	    return;
     }
 
-    /* 
-     * We're here if Intuition failed. Print alert to the debug output and reboot.
-     * In future we should have more intelligent handling for such a case. For
-     * example we should report what was wrong after we rebooted.
-     *
-     * Note that we use shared buffer in SysBase for alert text, so we Disable()
+    /*
+     * We're here if Intuition failed. Use safe (but not so
+     * system and user-friendly) way to post alerts.
      */
     Disable();
-
-    PrintFrame();
-    PrintCentered(Alert_GetTitle(alertNum));
-
-    FormatAlert(PrivExecBase(SysBase)->AlertBuffer, alertNum, task, SysBase);
-    /* Print first two lines (task and error) centered */
-    buf = PrintCentered(PrivExecBase(SysBase)->AlertBuffer);
-    buf = PrintCentered(buf + 1);
-    /* The rest is left-justified */
-    while (*buf)
-    	buf = PrintLeftJustified(buf + 1);
-
-    PrintFrame();
-    RawPutChar('\n');
-
+    Exec_SystemAlert(alertNum, SysBase);
     Enable();
 
     if (alertNum & AT_DeadEnd)
