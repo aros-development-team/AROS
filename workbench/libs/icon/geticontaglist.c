a7efd2887bf58d9176f9f5b278286bff291df22f
twilen <twilen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-12-11 18:21:10 +0000

    Use ParentDir() return code to set ST_ROOT. ST_ROOT does not exist in m68k-amiga original filesystems, ST_USERDIR is returned.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36050 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/icon/geticontaglist.c
+++ workbench/libs/icon/geticontaglist.c
@@ -201,6 +201,10 @@
                         iim.iim_Tags        = tags;
                         
                         iim.iim_ParentLock  = ParentDir(iim.iim_FileLock);
+#ifdef AROS_DOS_PACKETS
+                        if (iim.iim_ParentLock == BNULL && IoErr() == 0)
+                            iim.iim_FIB.fib_DirEntryType = ST_ROOT;
+#endif
                         iim.iim_FileHandle  = Open(name, MODE_OLDFILE);
                         
                         if (LB(IconBase)->ib_IdentifyHook != NULL)
