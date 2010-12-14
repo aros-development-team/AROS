69e5c9eac59938b9d6c8ccf2b3c16ec99c321bd4
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-12-14 14:09:10 +0000

    - Updated arch-specific exec parts - Added missing properties and dependencies
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36093 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/all-ios/exec/systemalert.c
@@ -0,0 +1,31 @@
+/*
+    Copyright © 1995-2010, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: Display an alert, iOS-hosted version
+    Lang: english
+*/
+
+#include <aros/debug.h>
+#include <exec/alerts.h>
+#include <exec/execbase.h>
+#include <proto/exec.h>
+#include <proto/kernel.h>
+#include <string.h>
+
+#include "exec_intern.h"
+#include "exec_util.h"
+
+
+void Exec_SystemAlert(ULONG alertNum, struct ExecBase *SysBase)
+{
+    UBYTE *buf;
+
+    buf = Alert_AddString(PrivExecBase(SysBase)->AlertBuffer, Alert_GetTitle(alertNum));
+    *buf++ = '\n';
+    FormatAlert(buf, alertNum, SysBase->ThisTask, SysBase);
+
+    /* Display an alert via our UIKit helper */
+    PD(SysBase).DisplayAlert(PrivExecBase(SysBase)->AlertBuffer);
+    AROS_HOST_BARRIER
+}
