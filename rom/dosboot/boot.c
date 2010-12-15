e136b004c2909200a5f22c475832ccb0b8a62aaf
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-12-15 15:20:13 +0000

    Revert "rom/dosboot: The correct 'New Shell' arguments are "\n", not """
    This reverts SVN commit r36101
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36108 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dosboot/boot.c
+++ rom/dosboot/boot.c
@@ -97,7 +97,7 @@ void __dosboot_Boot(APTR BootLoaderBase, struct DosLibrary *DOSBase, ULONG Flags
             tags[5].ti_Tag = TAG_IGNORE;
         }
 
-        rc = SystemTagList("\n", tags);
+        rc = SystemTagList("", tags);
         if (rc != -1)
         {
             cis  = BNULL;
