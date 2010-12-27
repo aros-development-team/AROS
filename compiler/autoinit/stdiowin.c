59a40b606c0c0ec3fd3de269bbb270d83102f72c
twilen <twilen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-12-27 21:04:31 +0000

    Prevent empty console window opening in dos packets mode. Ugly hack but I don't see any other easy solutions.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36274 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/autoinit/stdiowin.c
+++ compiler/autoinit/stdiowin.c
@@ -7,6 +7,7 @@
 #include <dos/dos.h>
 #include <proto/dos.h>
 #include <aros/startup.h>
+#include <dos/stdio.h>
 
 #define DEBUG 0
 #include <aros/debug.h>
@@ -19,7 +20,23 @@ extern char __stdiowin[];
 
 int __nostdiowin __attribute__((weak)) = 0;
 
-static BPTR __iowin = (BPTR)NULL, __old_in, __old_out, __old_err;
+static BPTR __iowinr = BNULL, __iowinw = BNULL, __iowine = BNULL;
+static BPTR __old_in, __old_out, __old_err;
+
+#ifdef AROS_DOS_PACKETS
+static BPTR DupFH(BPTR fh, LONG mode, struct DosLibrary * DOSBase)
+{
+    BPTR nfh;
+    struct MsgPort *old;
+
+    if (!fh)
+    	return BNULL;
+    old = SetConsoleTask(((struct FileHandle*)BADDR(fh))->fh_Type);
+    nfh = Open("*", mode);
+    SetConsoleTask(old);
+    return nfh;
+}
+#endif
 
 static void __startup_stdiowin(void)
 {
@@ -32,28 +49,56 @@ static void __startup_stdiowin(void)
     }
 
     D(bug("[__startup_stdiowin] Opening console window: %s\n", __stdiowin));
-    __iowin = Open(__stdiowin, MODE_OLDFILE);
-    if (!__iowin)
+
+    __iowinw = Open(__stdiowin, MODE_OLDFILE);
+#ifdef AROS_DOS_PACKETS
+    __iowinr = DupFH(__iowinw, MODE_OLDFILE, DOSBase);
+    if (__iowinr) {
+    	/* this is so ugly but ReadArgs() needs EOF or
+    	 * console window will always open and program
+    	 * pauses until RETURN is pressed.
+    	 */
+    	struct FileHandle *fh = BADDR(__iowinr);
+    	SetVBuf(__iowinr, NULL, BUF_LINE, 1);
+    	/* force EOF */
+    	fh->fh_Pos = fh->fh_End + 1;
+    }
+    __iowine = DupFH(__iowinw, MODE_OLDFILE, DOSBase);
+#else
+    __iowinr = __iowine = __iowinw;
+#endif
+
+    if (!__iowinr || !__iowinw || !__iowine)
     {
+#ifdef AROS_DOS_PACKETS
+    	Close(__iowinr);
+    	Close(__iowine);
+#endif
+    	Close(__iowinw);
         D(bug("[__startup_stdiowin] Failed!\n"));
         return;
     }
 
     D(bug("[__startup_stdiowin] Settign standard file handles\n"));
-    __old_in = SelectInput(__iowin);
-    __old_out = SelectOutput(__iowin);
-    __old_err = SelectError(__iowin);
+
+    __old_in = SelectInput(__iowinr);
+    __old_out = SelectOutput(__iowinw);
+    __old_err = SelectError(__iowine);
 
     __startup_entries_next();
+
     D(bug("[__startup_stdiowin] Program executed\n"));
 
-    if (__iowin) {
-        D(bug("[__startup_stdiowin] Closing __iowin\n"));
-        SelectInput(__old_in);
-        SelectOutput(__old_out);
-        SelectError(__old_err);
-        Close(__iowin);
-    }
+    SelectInput(__old_in);
+    SelectOutput(__old_out);
+    SelectError(__old_err);
+
+#ifdef AROS_DOS_PACKETS
+    Close(__iowinr);
+    Close(__iowine);
+#endif
+    Close(__iowinw);
+
     D(bug("[__startup_stdiowin] Done!\n"));
 }
 
