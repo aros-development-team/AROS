9714e17633b22becb0da1b074b06190562dc0798
twilen <twilen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-12-29 20:10:51 +0000

    vbeampos implemented.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36310 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/m68k-amiga/graphics/vbeampos.c
@@ -0,0 +1,61 @@
+/*
+    Copyright © 1995-2007, The AROS Development Team. All rights reserved.
+    $Id: vbeampos.c 34843 2010-10-20 04:54:32Z jmcmullan $
+
+    Desc: Read the current vertical position of the beam
+    Lang: english
+*/
+#include <aros/debug.h>
+#include "graphics_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+	#include <clib/graphics_protos.h>
+
+	AROS_LH0(LONG, VBeamPos,
+
+/*  SYNOPSIS */
+	/* void */
+
+/*  LOCATION */
+	struct GfxBase *, GfxBase, 64, Graphics)
+
+/*  FUNCTION
+
+    INPUTS
+        none
+
+    RESULT
+	None.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    volatile struct Custom *custom = (struct Custom*)0xdff000;
+    UWORD v1, v2;
+    
+    for (;;) {
+	v1 = custom->vhposr >> 8;
+	v2 = custom->vposr;
+	if (v1 == (custom->vhposr >> 8))
+	    break;
+    }
+
+    return v1 | ((v2 & 7) << 8);
+
+    AROS_LIBFUNC_EXIT
+} /* VBeamPos */
