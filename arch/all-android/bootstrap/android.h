e86657d389d3b6c6265bcf3f35e95d6c2c1cf2ae
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-12-30 12:55:00 +0000

    Glorified external variable names
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36320 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-android/bootstrap/android.h
+++ arch/all-android/bootstrap/android.h
@@ -1,7 +1,9 @@
 #include <jni.h>
 
-extern JNIEnv *jni;
-extern jclass cl;
-extern jobject obj;
+/* These three variables are referenced from AROS drivers */
+extern JNIEnv *Java_Env;
+extern jclass  Java_Class;
+extern jobject Java_Object;
+
 extern jmethodID DisplayAlert_mid;
 extern jmethodID DisplayError_mid;
