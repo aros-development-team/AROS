ae443779e2509a8c584e378cdf112b2712fa8ba1
mattrust <mrustler@gmx.de>
2011-01-01 09:33:29 +0000

    2010 -> 2011
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36359 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/shellcommands/Shell.c
+++ workbench/c/shellcommands/Shell.c
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 1995-2010, The AROS Development Team. All rights reserved.
+    Copyright (C) 1995-2011, The AROS Development Team. All rights reserved.
     $Id$
 
     The shell program.
@@ -628,7 +628,7 @@ LONG interact(struct InterpreterState *is)
 	    PutStr
 	    (
 	    	"AROS - The AROS Research Operating System\n"
-		"Copyright © 1995-2010, The AROS Development Team. All rights reserved.\n"
+		"Copyright © 1995-2011, The AROS Development Team. All rights reserved.\n"
 		"AROS is licensed under the terms of the AROS Public License (APL),\n"
 		"a copy of which you should have received with this distribution.\n"
 		"Visit http://www.aros.org/ for more information.\n"
