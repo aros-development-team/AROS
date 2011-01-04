d33486e48954275ef921ef3d55c147df70f59926
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-01-04 20:59:24 +0000

    - Added unixio.hidd back to the build - Reverted oss.library back to using unixio.hidd. - Added UNIX-hosted parallel.hidd back to the build.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36384 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-unix/libs/oss/oss_init.c
+++ arch/all-unix/libs/oss/oss_init.c
@@ -9,107 +9,23 @@
 #include <dos/dosextens.h>
 #include <aros/libcall.h>
 #include <aros/debug.h>
-#include <intuition/intuition.h>
+#include <hidd/unixio.h>
 
 #include <proto/exec.h>
-#include <proto/hostlib.h>
-#include <proto/intuition.h>
-#include <proto/kernel.h>
+#include <proto/oop.h>
+#include <proto/alib.h>
 
 #include <aros/symbolsets.h>
-
-#include <string.h>
-
 #include LC_LIBDEFS_FILE
 
+OOP_Object *unixio = NULL;
 int audio_fd;
-extern struct OSS_Base *GlobalOSSBase;
 
-static BOOL CheckArch(STRPTR MyArch)
+static int InitData(LIBBASETYPEPTR LIBBASE)
 {
-    APTR KernelBase;
-    STRPTR archs[2] = {MyArch, NULL};
-
-    KernelBase = OpenResource("kernel.resource");
-    D(bug("[OSS] KernelBase0x%p\n", KernelBase));
-    if (!KernelBase)
-	return FALSE;
-
-    archs[1] = (STRPTR)KrnGetSystemAttr(KATTR_Architecture);
-    D(bug("[OSS] My architecture: %s, kernel architecture: %s\n", archs[0], archs[1]));
-    if (!archs[1])
-	return FALSE;
-
-    if (strcmp(archs[0], archs[1]))
-    {
-	struct IntuitionBase *IntuitionBase;
-
-	IntuitionBase = (struct IntuitionBase *)OpenLibrary("intuition.library", 36);
-	if (IntuitionBase)
-	{
-            struct EasyStruct es = {
-        	sizeof (struct EasyStruct),
-        	0,
-        	"Incompatible architecture",
-		"This version of oss.library is built for use\n"
-		"with %s architecture, but your\n"
-		"system architecture is %s.",
-        	"Ok",
-	    };
-
-	    EasyRequestArgs(NULL, &es, NULL, (IPTR *)archs);
-
-	    CloseLibrary(&IntuitionBase->LibNode);
-	}
-	return FALSE;
-    }
-
-    D(bug("[OSS] Architecture check done\n"));
-    return TRUE;
-}
-
-static const char *libc_symbols[] = {
-    "open",
-    "close",
-    "ioctl",
-    "mmap",
-    "munmap",
-    "write",
-#ifdef HOST_OS_linux
-    "__errno_location",
-#else
-    "__error",
-#endif
-    NULL
-};
-
-static int InitData(LIBBASETYPEPTR OSSBase)
-{
-    ULONG r = 0;
-
-    D(bug("[OSS] Init\n"));
-
-    if (!CheckArch(AROS_ARCHITECTURE))
-	return FALSE;
-
-    HostLibBase = OpenResource("hostlib.resource");
-    D(bug("[OSS] HostLibBase is %p\n", HostLibBase));
-    if (!HostLibBase)
-	return FALSE;
-
-    OSSBase->LibCHandle = HostLib_Open(LIBC_NAME, NULL);
-    if (!OSSBase->LibCHandle)
-	return FALSE;
-
-    OSSBase->OSSIFace = (struct LibCInterface *)HostLib_GetInterface(OSSBase->LibCHandle, libc_symbols, &r);
-    D(bug("[OSS] libc interface 0x%p, %u unresolved symbols\n", OSSBase->OSSIFace, r));
-    if ((!OSSBase->OSSIFace) || r)
-	return FALSE;
-
-    InitSemaphore(&OSSBase->sem);
-    OSSBase->errnoPtr = OSSBase->OSSIFace->__error();
-
-    GlobalOSSBase = OSSBase;
+    unixio = OOP_NewObject(NULL, CLID_Hidd_UnixIO, NULL);
+    if (!unixio) return FALSE;
+    
     return TRUE;
 }
 
@@ -120,20 +36,17 @@ static int OpenLib(LIBBASETYPEPTR LIBBASE)
     return ((struct Library *)LIBBASE)->lib_OpenCnt ? FALSE : TRUE;
 }
 
-static int CleanUp(LIBBASETYPEPTR OSSBase)
+static int CleanUp(LIBBASETYPEPTR LIBBASE)
 {
-    if (!HostLibBase)
-	return TRUE;
-
-    if (OSSBase->OSSIFace)
-	HostLib_DropInterface((APTR *)OSSBase->OSSIFace);
-
-    if (OSSBase->LibCHandle)
-	HostLib_Close(OSSBase->LibCHandle, NULL);
-
+    if (unixio)
+    {
+	OOP_DisposeObject(unixio);
+	unixio = NULL;
+    }
     return TRUE;
 }
 
 ADD2INITLIB(InitData, 0);
 ADD2OPENLIB(OpenLib, 0);
 ADD2EXPUNGELIB(CleanUp, 0);
+ADD2LIBS("unixio.hidd", 0, static struct Library *, unixioBase);
--- arch/all-unix/libs/oss/oss_intern.h
+++ arch/all-unix/libs/oss/oss_intern.h
@@ -1,39 +1,5 @@
 #include <exec/libraries.h>
-#include <exec/semaphores.h>
+#include <oop/oop.h>
 
-#define timeval sys_timeval
-
-#include <sys/types.h>
-
-#undef timeval
-
-#ifdef HOST_OS_linux
-#define LIBC_NAME "libc.so.6"
-#else
-#define LIBC_NAME "libc.so"
-#endif
-
+extern OOP_Object *unixio;
 extern int audio_fd;
-
-struct LibCInterface
-{
-    int	    (*open)(char *path, int oflag, ...);
-    int	    (*close)(int filedes);
-    int	    (*ioctl)(int d, int request, ...);
-    void   *(*mmap)(void *start, size_t length, int prot, int flags, int fd, off_t offset);
-    int	    (*munmap)(void *start, size_t length);
-    ssize_t (*write)(int fildes, const void *buf, size_t nbyte);
-    int	   *(*__error)(void);
-};
-
-struct OSS_Base
-{
-    struct Library	   Lib;
-    APTR		   HostLibBase;
-    APTR		   LibCHandle;
-    struct LibCInterface  *OSSIFace;
-    int			  *errnoPtr;
-    struct SignalSemaphore sem;
-};
-
-#define HostLibBase OSSBase->HostLibBase
