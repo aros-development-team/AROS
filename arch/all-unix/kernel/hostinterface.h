51247e56d92a9880856d7179040e0da2a5afb305
weissms <weissms@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-01-05 15:33:26 +0000

    Turn hostlib function pointer prefix to small case again.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36394 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-unix/kernel/hostinterface.h
+++ arch/all-unix/kernel/hostinterface.h
@@ -1,13 +1,13 @@
-#define HOSTINTERFACE_VERSION 1
+#define HOSTINTERFACE_VERSION 2
 
 struct HostInterface
 {
     char *System;
     unsigned int Version;
 
-    void *(*HostLib_Open)(const char *, char **);
-    int   (*HostLib_Close)(void *, char **);
-    void *(*HostLib_GetPointer)(void *, const char *, char **);
+    void *(*hostlib_Open)(const char *, char **);
+    int   (*hostlib_Close)(void *, char **);
+    void *(*hostlib_GetPointer)(void *, const char *, char **);
     int   (*VKPrintF)(const char *, va_list);
     void  (*Reboot)(unsigned char action);
     struct MinList **ModListPtr;
