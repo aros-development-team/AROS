970e0f01270bd545ab79b0938f2d3d0d89bca44e
deadwood-pl <deadwood@wp.pl>
2011-01-07 08:01:32 +0000

    Add strtod, atof to emulation layer
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36425 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/mesa/src/aros/emul_arosc.c
+++ workbench/libs/mesa/src/aros/emul_arosc.c
@@ -14,6 +14,9 @@
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
+#include <math.h>
+#include <ctype.h>
+#include <limits.h>
 
 /* 
     The purpose of this file is to provide implementation for C functions which 
@@ -273,7 +276,101 @@ int fprintf (FILE * fh, const char * format, ...)
     IMPLEMENT();
     return 0;
 }
-	
+
+
+double strtod (const char * str,char ** endptr)
+{
+    /* Unit tests available in : tests/clib/strtod.c */
+    /* FIXME: implement NAN handling */
+    double  val = 0, precision;
+    int     exp = 0;
+    char    c = 0, c2 = 0;
+    int     digits = 0;      
+
+    /* skip all leading spaces */
+    while (isspace (*str))
+        str ++;
+
+    /* start with scanning the floting point number */
+    if (*str)
+    {
+        /* Is there a sign? */
+        if (*str == '+' || *str == '-')
+            c = *str ++;
+
+        /* scan numbers before the dot */
+        while (isdigit(*str))
+        {
+            digits++;
+            val = val * 10 + (*str - '0');
+            str ++;
+        }
+
+        /* see if there is the dot and there were digits before it or there is 
+            at least one digit after it */
+        if ((*str == '.') && ((digits > 0) || (isdigit(*(str + 1)))))
+        {
+            str++;
+            /* scan the numbers behind the dot */
+            precision = 0.1;
+            while (isdigit (*str))
+            {
+                digits++;
+                val += ((*str - '0') * precision) ;
+                str ++;
+                precision = precision * 0.1;
+            }
+        }
+
+        /* look for a sequence like "E+10" or "e-22" if there were any digits up to now */
+        if ((digits > 0) && (tolower(*str) == 'e'))
+        {
+            int edigits = 0;
+            str++;
+
+            if (*str == '+' || *str == '-')
+                c2 = *str ++;
+
+            while (isdigit (*str))
+            {
+                edigits++;
+                exp = exp * 10 + (*str - '0');
+                str ++;
+            }
+            
+            if (c2 == '-')
+                exp = -exp;
+            
+            if (edigits == 0) 
+            {
+                /* there were no digits after 'e' - rollback pointer */
+                str--; if (c2 != 0) str--;
+            }
+            
+            val *= pow (10, exp);
+        }
+
+        if (c == '-')
+            val = -val;
+
+        if ((digits == 0) && (c != 0))
+        {
+            /* there were no digits but there was sign - rollback pointer */
+            str--;
+        }
+    }
+
+    if (endptr)
+        *endptr = (char *)str;
+
+    return val;
+} /* strtod */
+
+double atof (const char * str)
+{
+    return strtod (str, (char **)NULL);
+} /* atof */
+
 int __init_emul(void)
 {
     /* malloc/calloc/realloc/free */
