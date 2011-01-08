2e4b4751598fcc5554c025c3d988bc5c2eab78c1
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-01-08 15:28:59 +0000

    Add ability to define, initialize and destroy static global tlses.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36452 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/mesa/src/aros/tls.h
+++ workbench/libs/mesa/src/aros/tls.h
@@ -7,6 +7,7 @@
 #define TLS_H
 
 #include <exec/types.h>
+#include <aros/symbolsets.h>
 
 struct TaskLocalStorage;
 
@@ -15,5 +16,18 @@ VOID InsertIntoTLS(struct TaskLocalStorage * tls, APTR ptr);
 APTR GetFromTLS(struct TaskLocalStorage * tls);
 VOID ClearFromTLS(struct TaskLocalStorage * tls);
 VOID DestroyTLS(struct TaskLocalStorage * tls);
+
+#define DECLARE_STATIC_TLS(tls)                 \
+static struct TaskLocalStorage * tls = NULL;    \
+static void auto_crate_##tls()                  \
+{                                               \
+    tls = CreateTLS();                          \
+}                                               \
+static void auto_destroy_##tls()                \
+{                                               \
+    DestroyTLS(tls);                            \
+}                                               \
+ADD2INIT(auto_crate_##tls, 5);                  \
+ADD2EXIT(auto_destroy_##tls, 5);
 
 #endif /* TLS_H */
