43e77872b1bd45da41f9914e5f7cf4cbe5e68d8b
deadwood-pl <deadwood@wp.pl>
2011-01-09 17:25:19 +0000

    Getting an item from TLS does not need semaphore lock anymore
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36475 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/mesa/src/aros/tls.c
+++ workbench/libs/mesa/src/aros/tls.c
@@ -7,101 +7,109 @@
 
 #include <proto/exec.h>
 
-struct TaskLocalStorage
-{
-    struct List             tls_TaskLocalList;
-    struct SignalSemaphore  tls_Semaphore;
-};
-
 struct TaskLocalNode
 {
-    struct Node     tl_Node;
-    struct Task *   tl_Task;
-    APTR            tl_Data;
+    struct TaskLocalNode    * tl_Succ;
+    struct Task             * tl_Task;
+    APTR                    tl_Data;
 };
+
+struct TaskLocalStorage
+{
+    struct TaskLocalNode    * tls_Head;
+    struct SignalSemaphore  tls_WriteSemaphore;
+};
+
+/* Implementation uses locking only when adding objects. Objects are always added
+   at head. The list is never reordered, thus reading can be done without locking */
+
+/* Idea: can consider adding list trimming when holding insert lock: instead of
+   adding an object to head of existing list, a new list would be created where
+   object would be copies (not pointer to!) of objects on current list. During
+   copying objects with NULL data items would not be copied. Once copying done,
+   new list would be put in place of old list. The old list however would then
+   leak. Maybe this leak could be fixed by implementing smart pointers/reference
+   counting on TaskLocalNode - this could however make iterations slowe */
 
 struct TaskLocalStorage * CreateTLS()
 {
     struct TaskLocalStorage * tls = 
         AllocVec(sizeof(struct TaskLocalStorage), MEMF_PUBLIC | MEMF_CLEAR);
 
-    InitSemaphore(&tls->tls_Semaphore);
-    NEWLIST(&tls->tls_TaskLocalList);
+    InitSemaphore(&tls->tls_WriteSemaphore);
+    tls->tls_Head = NULL;
 
     return tls;
 }
 
 VOID InsertIntoTLS(struct TaskLocalStorage * tls, APTR ptr)
 {
-    struct TaskLocalNode * tl;
+    struct TaskLocalNode * tl = tl = tls->tls_Head;
     struct Task * me = FindTask(NULL);
     struct TaskLocalNode * selected = NULL;
     
-    ObtainSemaphore(&tls->tls_Semaphore);
-    ForeachNode(&tls->tls_TaskLocalList, tl)
+    /* Assumption: one task cannot be reviewing the list and adding the head
+       "at the same time" - do not alter this function to recurse */
+    /* Assumption: only task A can add entry for task A */
+
+    /* Check if task's storage is already on the list */    
+    while(tl)
     {
         if (tl->tl_Task == me)
         {
             selected = tl;
             break;
         }
+        tl = tl->tl_Succ;
     }
     
-    if (ptr)
+    if (!selected)
     {
-        if (!selected)
-        {
-            /* Create, set values, add */
-            selected = AllocVec(sizeof(struct TaskLocalNode), MEMF_PUBLIC | MEMF_CLEAR);
-            selected->tl_Task = me;
-            ADDHEAD(&tls->tls_TaskLocalList, selected); /* Always add to head to move dead entries to back of list */
-        }
+        /* No, it is not. Create, set task pointer and at to head of list */
+        selected = AllocVec(sizeof(struct TaskLocalNode), MEMF_PUBLIC | MEMF_CLEAR);
+        selected->tl_Task = me;
+        ObtainSemaphore(&tls->tls_WriteSemaphore);
+        selected->tl_Succ = tls->tls_Head;
+        tls->tls_Head = selected;
+        ReleaseSemaphore(&tls->tls_WriteSemaphore);
         
-        selected->tl_Data = ptr;
     }
-    else
-    {
-        /* Since NULL was passed, delete the entry */
-        if (selected)
-        {
-            Remove(&selected->tl_Node);
-            FreeVec(selected);
-        }
-    }
-    
-    ReleaseSemaphore(&tls->tls_Semaphore);
+
+    /* Set the passed value */
+    selected->tl_Data = ptr;
 }
 
 APTR GetFromTLS(struct TaskLocalStorage * tls)
 {
-    struct TaskLocalNode * tl;
+    struct TaskLocalNode * tl = tls->tls_Head;
     struct Task * me = FindTask(NULL);
     APTR data = NULL;
     
-    ObtainSemaphore(&tls->tls_Semaphore);
-    ForeachNode(&tls->tls_TaskLocalList, tl)
+    while(tl)
     {
         if (tl->tl_Task == me)
         {
             data = tl->tl_Data;
             break;
-            /* TODO: be smart? if list long, move the found node to beginning? */
         }
+        tl = tl->tl_Succ;
     }
-    ReleaseSemaphore(&tls->tls_Semaphore);
 
     return data;
 }
 
 VOID DestroyTLS(struct TaskLocalStorage * tls)
 {
-    struct TaskLocalNode * n, *m;
-    ObtainSemaphore(&tls->tls_Semaphore);
-    ForeachNodeSafe(&tls->tls_TaskLocalList, n, m)
+    /* Destroy needs no lock. If a task is still iterating over list, we are doomed
+       anyway */
+    struct TaskLocalNode * tl = tls->tls_Head, * temp;
+
+    while (tl)
     {
-        Remove(&n->tl_Node);
-        FreeVec(n);
+        temp = tl->tl_Succ;
+        FreeVec(tl);
+        tl = temp;
     }
-    ReleaseSemaphore(&tls->tls_Semaphore);
+
     FreeVec(tls);
 }
