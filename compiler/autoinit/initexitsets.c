411649deaa303652a1e1413fb42e4072707d49db
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-01-12 15:09:51 +0000

    Dwarf2 unwinding now works. No more --enable-sjlj-exceptions needed. Tested on i386.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36541 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/autoinit/initexitsets.c
+++ compiler/autoinit/initexitsets.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 2009, The AROS Development Team. All rights reserved.
+    Copyright © 2009 - 2011, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: autoinit library - handle init and exit symolsets
@@ -12,6 +12,29 @@
 #include <aros/debug.h>
 
 int __noinitexitsets __attribute__((weak)) = 0;
+extern void *__eh_frame_start;
+
+/*
+ * These two functions could have been overriden by libgcc's Dwarf2 unwinder code.
+ * (used for example for C++ exception handling).
+ * We do this trick in order to avoid linking in the whole unwinder just by referencing
+ * these functions. This helps to keep C executables smaller.
+ * Note that we call them when libraries are already open because __register_frame()
+ * internally uses malloc(). See unwind-dw2-fde.c in gcc source code.
+ *
+ * TODO: Calling these functions from here means that only plain executables register their
+ * exception frames. This means that:
+ * a) C++ exceptions won't work inside shared libraries.
+ * This can be easily fixed in libraries startup code. But libraries are separate executables, and
+ * this would mean that they register their own frames in their own copy of libgcc runtime. As a
+ * consequence:
+ * b) C++ exceptions can't be thrown outside of shared libraries using this approach.
+ * (b) is more difficult to fix. When some program opens the library, it should be able to register
+ * its frames and actually process exceptions using program's runtime. This can be implemented
+ * in a number of ways, every of them involving development of some ABI standard.
+ */
+void __attribute__((weak)) __register_frame(void *begin) {}
+void __attribute__((weak)) __deregister_frame(void *begin) {}
 
 DEFINESET(CTORS);
 DEFINESET(DTORS);
@@ -24,6 +47,8 @@ static void __startup_initexit(void)
 
     if (set_open_libraries())
     {
+        __register_frame(&__eh_frame_start);
+
         if (set_call_funcs(SETNAME(INIT), 1, 1))
 	{
             /* ctors/dtors get called in inverse order than init funcs */
@@ -34,6 +59,8 @@ static void __startup_initexit(void)
             set_call_funcs(SETNAME(DTORS), 1, 0);
         }
         set_call_funcs(SETNAME(EXIT), -1, 0);
+
+	__deregister_frame(&__eh_frame_start);
     }
     set_close_libraries();
     
