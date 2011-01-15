a851f4f3717c366a1f0ed4bff13a07546b2e8cd8
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-01-15 15:54:53 +0000

    Use openvg.library in EGL to get st_api object.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36592 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/mesa/src/gallium/targets/egl/st_OpenVG.c
+++ workbench/libs/mesa/src/gallium/targets/egl/st_OpenVG.c
@@ -1,8 +1,8 @@
-#include "vg_api.h"
+#include <proto/openvg.h>
 #include "egl.h"
 
 PUBLIC struct st_api *
 st_api_create_OpenVG(void)
 {
-   return (struct st_api *) vg_api_get();
+   return (struct st_api *) GetOpenVGStateTrackerApi();
 }
