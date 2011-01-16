70dc5c0a4768cedb0155f75ea3b30e4d9f3a4a50
deadwood-pl <deadwood@wp.pl>
2011-01-16 19:45:45 +0000

    Fixes for amiga-m68k build.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36640 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/mesa/src/gallium/targets/egl/st_GL.c
+++ workbench/libs/mesa/src/gallium/targets/egl/st_GL.c
@@ -1,5 +1,7 @@
 #include <proto/exec.h>
+#if !defined(__mc68000)
 #include <proto/mesa.h>
+#endif
 #include <aros/symbolsets.h>
 
 #include "egl.h"
@@ -8,12 +10,14 @@ struct Library * MesaBase = NULL;
 
 PUBLIC struct st_api * st_api_create_OpenGL(void)
 {
+#if !defined(__mc68000)
     if (!MesaBase)
         MesaBase = OpenLibrary("mesa.library", 0L);
 
     if (MesaBase)
         return (struct st_api *) AROSMesaGetOpenGLStateTrackerApi();
     else
+#endif
         return NULL;
 }
 
