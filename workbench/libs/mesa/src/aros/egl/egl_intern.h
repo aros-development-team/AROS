580fef68a02dcfc93b785c6aa160b8606352356a
deadwood-pl <deadwood@wp.pl>
2011-01-16 15:45:45 +0000

    Skeleton of egl.library
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36619 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/mesa/src/aros/egl/egl_intern.h
@@ -0,0 +1,18 @@
+/*
+    Copyright 2011, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#ifndef EGL_INTERN_H
+#define EGL_INTERN_H
+
+#include <proto/exec.h>
+
+/****************************************************************************************/
+
+struct EGLBase
+{
+    struct Library  mglb_Lib;
+};
+
+#endif /* EGL_INTERN_H */
