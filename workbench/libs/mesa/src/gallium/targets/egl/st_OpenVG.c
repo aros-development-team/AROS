70ba6cd8f9973eb087778c8b240d0fb43a7835fe
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-01-16 15:44:36 +0000

    Open vega.libray in dynamic way.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36617 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/mesa/src/gallium/targets/egl/st_OpenVG.c
+++ workbench/libs/mesa/src/gallium/targets/egl/st_OpenVG.c
@@ -1,8 +1,26 @@
+#include <proto/exec.h>
 #include <proto/vega.h>
+#include <aros/symbolsets.h>
+
 #include "egl.h"
 
-PUBLIC struct st_api *
-st_api_create_OpenVG(void)
+struct Library * VegaBase = NULL;
+
+PUBLIC struct st_api * st_api_create_OpenVG(void)
 {
-   return (struct st_api *) GetOpenVGStateTrackerApi();
+    if (!VegaBase)
+        VegaBase = OpenLibrary("vega.library", 0L);
+
+    if (VegaBase)
+        return (struct st_api *) GetOpenVGStateTrackerApi();
+    else
+        return NULL;
 }
+
+static VOID CloseVega()
+{
+    if (VegaBase)
+        CloseLibrary(VegaBase);
+}
+
+ADD2EXPUNGELIB(CloseVega, 5)
