ffb8e2d2a7392570744c108baf35d41c52447d8b
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-01-27 08:38:04 +0000

    Added more tags for AddDisplayDriverA(), provided support for Amiga(tm) chipset driver.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36835 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/graphics/driver.h
+++ compiler/include/graphics/driver.h
@@ -2,7 +2,7 @@
 #define GRAPHICS_DRIVER_H
 
 /*
-    Copyright © 2010, The AROS Development Team. All rights reserved.
+    Copyright © 2010-2011, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: Display driver definitions.
@@ -13,10 +13,12 @@
 
 /* Tags for AddDisplayDriverA() */
 
-#define DDRV_BootMode	  (TAG_USER + 0x01)	/* BOOL  Boot mode driver which will be unloaded when any next driver comes in, default = FALSE */
-#define DDRV_MonitorID	  (TAG_USER + 0x02)	/* ULONG Monitor ID for this driver, default = next available */
-#define DDRV_ReserveIDs	  (TAG_USER + 0x03)	/* ULONG How many monitor IDs to reserve, default = 1 */
-#define DDRV_KeepBootMode (TAG_USER + 0x04)	/* BOOL  Do not shut down boot mode drivers, default = FALSE */
+#define DDRV_BootMode	  (TAG_USER + 0x01)	/* BOOL    Boot mode driver which will be unloaded when any next driver comes in, default = FALSE */
+#define DDRV_MonitorID	  (TAG_USER + 0x02)	/* ULONG   Monitor ID for this driver, default = next available */
+#define DDRV_ReserveIDs	  (TAG_USER + 0x03)	/* ULONG   How many monitor IDs to reserve, default = 1 */
+#define DDRV_KeepBootMode (TAG_USER + 0x04)	/* BOOL    Do not shut down boot mode drivers, default = FALSE */
+#define DDRV_ResultID	  (TAG_USER + 0x05)	/* ULONG * Obtain assigned monitor ID */
+#define DDRV_IDMask	  (TAG_USER + 0x06)	/* ULONG   Use own mask for monitor ID separation */
 
 /* Return codes */
 
--- rom/graphics/dispinfo.h
+++ rom/graphics/dispinfo.h
@@ -7,7 +7,8 @@
 
 /****************************************************************************************/
 
-/* RTG display Mode ID construction:
+/*
+   RTG display Mode ID construction:
 
    nnnn xx yy
 
@@ -15,26 +16,23 @@
      xx - sync index
      yy - pixelformat index
 
+   xx and yy parts are obtained from the HIDD. Card number is maintained by
+   graphics.library. Resulting mode ID is obtained by logical OR between these
+   two components.
+
    As i mentioned, RTG mode counting starts from 0x0010. Lower number means
    Amiga(tm) chipset mode. Modes from 0x0000 to 0x000A are officially defined
    in include/graphics/modeid.h, modes 0x000B - 0x000F are reserved, just in case.
 
    Note that chipset mode IDs store modifier flags instead of sync/pixelformat object
    indexes. When chipset driver is implemented, this will need to be handled in a special
-   way.
-
-   There is no more difference between Amiga mode ID and HIDD Mode ID. All are the
-   same, except HIDDs need to ignore card number (in future).
+   way (by overloading mode ID processing methods in the driver).
 
    Sonic <pavel_fedin@mail.ru>
-
 */
 
 #define AROS_RTG_MONITOR_ID   0x00100000 /* First RTG monitor ID     */
 #define AROS_MONITOR_ID_MASK  0xFFFF0000 /* Internal monitor ID mask */
-#define AROS_MONITOR_ID_SHIFT 16
-
-#define SYNC_IDX(mode) ((mode & 0x0000FF00) >> 8)
 
 /****************************************************************************************/
 
