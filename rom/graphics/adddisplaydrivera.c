2aede4d594e4ce9fd969da2d432453368aa693f7
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-01-27 08:38:04 +0000

    Added more tags for AddDisplayDriverA(), provided support for Amiga(tm) chipset driver.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36835 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/graphics/adddisplaydrivera.c
+++ rom/graphics/adddisplaydrivera.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2010, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2011, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: AROS-specific function for adding a display driver
@@ -51,6 +51,42 @@
 				are 100% sure that your driver won't conflict with boot mode
 				driver (like VGA or VESA) and won't attempt to take over its
 				hardware. Defaults to FALSE.
+	    DDRV_ResultID     - A pointer to ULONG location where ID assigned to your driver
+	    			will be placed. Useful if you reserve some ID for future use.
+	    			Note that returned ID will be the one just assigned to your
+	    			driver instance. Increment it yourself in order to obtain
+	    			other reserved IDs.
+	    DDRV_IDMask	      - A mask for separating monitor ID from HIDD-specific part.
+	    			This mask specifies what mode ID bits are monitor ID and
+	    			what bits actually specify the mode. A default value is
+	    			0xFFFF0000.
+	    			
+	    			Using the mask you can split your monitor ID into 'sub-Ids'.
+	    			Example:
+
+	    			Supplied tags: DDRV_IDMask, 0xFFFFFF00, DDRV_ResultID, &myid
+
+	    			After succesfull call myid will contain base ID assigned by
+	    			graphics.library to your driver, let's say 0x00140000. However,
+	    			since you specified longer mask, you leave only one byte for mode
+	    			designation, and reserve the whole range of IDs from 0x001400xx to
+	    			0x0014FFxx for different instances of your driver. They can now be
+	    			used by specifying DDRV_MonitorID with corresponding value.
+
+				Note that for this feature to work correctly, you also need to override
+				mode ID processing in your driver class. Default methods provided by
+				hidd.graphics.graphics base class suppose that the whole lower word
+				of mode ID specifies the display mode.
+
+				It is generally not allowed to specify shorter masks than 0xFFFF0000.
+				The only driver which can do this is Amiga(tm) chipset driver, which
+				need to occupy the reserved range of IDs from 0x0000xxxx to 0x000Axxxx.
+				In any other case supplying short mask will cause undefined behavior.
+
+				Since DDRV_ReserveIDs provide simpler way to reserve IDs for your driver
+				(without the need to override mode ID processing), this	option can be
+				considered experimental and even private. In fact the primary reason for
+				it to exist is to provide support for Amiga(tm) chipset	driver.
 
     RESULT
     	error - One of following codes:
@@ -71,10 +107,6 @@
 	the same hardware (for example native mode driver vs VESA driver). So be careful
 	while adding new display drivers on a working system. Know what you do.
 
-	There's no way to know which IDs have been reserved when using DDRV_ReserveIDs.
-
-	These issues will be addressed in future, this API is raw and incomplete.
-
     SEE ALSO
 
     INTERNALS
@@ -89,7 +121,7 @@
 
     struct monitor_driverdata *mdd;
     ULONG FirstID, NextID;
-    ULONG NumIDs;
+    ULONG NumIDs, IDMask;
     ULONG ret = DD_OK;
 
     EnterFunc(bug("AddDisplayDriverA(0x%p)\n", gfxhidd));
@@ -99,8 +131,18 @@
     ObtainSemaphore(&CDD(GfxBase)->displaydb_sem);
 
     FirstID = GetTagData(DDRV_MonitorID, CDD(GfxBase)->last_id, tags);
-    NumIDs = GetTagData(DDRV_ReserveIDs, 1, tags);
-    NextID = FirstID + (NumIDs << AROS_MONITOR_ID_SHIFT);
+    NumIDs  = GetTagData(DDRV_ReserveIDs, 1, tags);
+    IDMask  = GetTagData(DDRV_IDMask, AROS_MONITOR_ID_MASK, tags);
+
+    /*
+     * Calculate next free ID.
+     * Mechanism of increment calculation: we invert the mask and add 1 to it.
+     * This way for example 0xFFFF0000 becomes 0x00010000.
+     * Before doing this we make sure that mask used in this equation is not
+     * longer than 0xFFFF0000, for proper ID counting.
+     */
+    NextID = FirstID + NumIDs * (~(IDMask & AROS_MONITOR_ID_MASK) + 1);
+    D(bug("[AddDisplayDriverA] First ID 0x%08X, next ID 0x%08X\n", FirstID, NextID));
 
     /* First check if requested IDs are already allocated */
     for (mdd = CDD(GfxBase)->monitors; mdd; mdd = mdd->next) {
@@ -115,12 +157,16 @@
 	D(bug("[AddDisplayDriverA] Installing driver\n"));
 	mdd = driver_Setup(gfxhidd, GfxBase);
 	D(bug("[AddDisplayDriverA] monitor_driverdata 0x%p\n", mdd));
-	if (mdd) {
+
+	if (mdd)
+	{
 	    BOOL keep_boot;
 	    struct monitor_driverdata *last, *old;
+	    ULONG *ResultID;
 
 	    mdd->id   = FirstID;
-	    mdd->mask = AROS_MONITOR_ID_MASK;
+	    mdd->mask = IDMask;
+
 	    if (GetTagData(DDRV_BootMode, FALSE, tags))
 	        mdd->flags |= DF_BootMode;
 
@@ -166,6 +212,12 @@
 	    /* Remember next available ID */
 	    if (NextID > CDD(GfxBase)->last_id)
 		CDD(GfxBase)->last_id = NextID;
+
+	    /* Return the assigned ID if the caller asked to do so */
+    	    ResultID = (ULONG *)GetTagData(DDRV_ResultID, 0, tags);
+	    if (ResultID)
+	    	*ResultID = FirstID;
+
 	} else
 	    ret = DD_NO_MEM;
     }
