a0e055dacc56620b0b97698d971c8abbbdb72ac2
mattrust <mrustler@gmx.de>
2011-01-29 17:22:35 +0000

    Garshneblanker commodity removed. It didn't work. Maybe I'll make a new attempt as Sourceforge project.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36871 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/tools/commodities/gblanker/Blankers/LineBlanker/LineBlanker_rev.h
+++
@@ -1,6 +0,0 @@
-#define	VERSION		38
-#define	REVISION	1
-#define	DATE	"6.11.94"
-#define	VERS	"LineBlanker 38.1"
-#define	VSTRING	"LineBlanker 38.1 (6.11.94)\r\n"
-#define	VERSTAG	"\0$VER: LineBlanker 38.1 (6.11.94)"
--- workbench/tools/commodities/gblanker/protos/var.h
+++
@@ -1,7 +0,0 @@
-#ifndef G_VAR_H
-#define G_VAR_H
-
-LONG GetVar37( STRPTR, STRPTR, LONG, ULONG );
-LONG SetVar37( STRPTR, STRPTR, LONG, ULONG );
-
-#endif /* G_VAR_H */
