d68ec63a5ea66c14e024a612204b1f2daf956c1a
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-02-02 10:40:06 +0000

    - Removed architecture-specific runprocess.c, fixed m68k CallEntry() implementation - Removed obsolete AROS_UFC3R macro
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36929 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/m68k-all/dos/exit.c
+++ arch/m68k-all/dos/exit.c
@@ -19,19 +19,51 @@ AROS_LH1(void, Exit,
     AROS_LIBFUNC_EXIT
 }
 
+/*
+ * We need, to support older AmigaOS programs, the following register
+ * setup:
+ *
+ * D0 - Length of arg string
+ * A0 - Pointer to arg string
+ * A1 - BCPL 'reverse' stack pointer - tc_SPLower
+ * A2 - Global vector
+ * A5 - BCPL 'jsr' routine
+ * A6 - BCPL 'rts' routine
+ */
 static ULONG CallEntry(ULONG CallEntry(STRPTR argptr, ULONG argsize, LONG_FUNC entry, struct Process *me)
 {
-    /*
-     * TODO:
-     * 1. Rewrite the thing in clean asm, remove the obsolete macro from cpu.h.
-     * 2. Fix up the function for better compatibility. See comments in rom/dos/exit.c.
-     */
-    return AROS_UFC3R(ULONG, entry,
-		      AROS_UFCA(STRPTR, argptr, A0),
-		      AROS_UFCA(ULONG, argsize, D0),
-		      AROS_UFCA(struct ExecBase *, SysBase, A6),
-		      &me->pr_ReturnAddr,
-		      me->pr_Task.tc_SPUpper - me->pr_Task.tc_SPLower
-		     );
+    ULONG ret;
+    APTR pr_GlobVec = me->pr_GlobVec;
+    APTR tc_SPLower = me->pr_Task.tc_SPLower;
 
+    __asm__ __volatile__(
+	"move.l %%sp,%%a0\n\t"
+	"movem.l %%d2-%%d7/%%a2-%%a6,%%a0@-\n\t"
+	"move.l %4,%%d0\n\t"            /* stksize = %a0 - tc_SPLower */
+	"neg.l  %%d0\n\t"
+	"lea.l  %%a0@(%%d0),%%a1\n\t"
+	"move.l %%a1,%%a0@-\n\t"        /* sp+ 8 = stksize  */
+	"lea.l  %%a0@,%%a2\n\t"         /* Save address of return address */
+	"move.l %%a2,%1\n\t"
+	"move.l #0f,%%a0@-\n\t"         /* sp+ 4 = return address */
+	"move.l %2,%%a0@-\n\t"          /* sp+ 0 = address to go to */
+	"move.l %%a0,%%d1\n\t"
+	"move.l %3,%%a2\n\t"            /* A2 - Global Vector */
+	"move.l %4,%%a1\n\t"            /* A1 - BCPL frame */
+	"move.l %5,%%a0\n\t"            /* A0 - Argptr */
+	"move.l %6,%%d0\n\t"            /* D0 - Argsize */
+	"move.l %%d1,%%sp\n\t"
+	"lea.l  BCPL_jsr,%%a5\n\t"      /* A5 - BCPL jsr routine */
+	"lea.l  BCPL_rts,%%a6\n\t"      /* A6 - BCPL rts routine */
+	"rts    \n\t"
+	"0:\n\t"
+	"addq.l  #4,%%sp\n\t"
+	"movem.l %%sp@+,%%d2-%%d7/%%a2-%%a6\n\t"
+	"move.l  %%d0,%0"
+	: "=g" (ret), "=m"(me->pr_ReturnAddr)
+	: "m" (entry), "m"(pr_GlobVec), "m"(tc_SPLower),
+	  "m" (argptr), "m" (argsize)
+	: "cc", "memory", "%d0", "%d1", "%a0", "%a1", "%a2", "%a3" );
+
+    return ret;
 }
