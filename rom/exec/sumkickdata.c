b2a54c56367ab09a1496cd46559875acd2fc8b01
twilen <twilen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-02-06 09:51:32 +0000

    Implement SumKickData(), needed for reset proof KickTags.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@36981 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/sumkickdata.c
+++ rom/exec/sumkickdata.c
@@ -5,6 +5,7 @@
     Desc: Build checksum for Kickstart.
     Lang: english
 */
+#include <aros/debug.h>
 #include "exec_intern.h"
 #include <proto/exec.h>
 
@@ -40,9 +41,39 @@
 {
     AROS_LIBFUNC_INIT
 
-    /* TODO: Write exec/SumKickData() */
-    aros_print_not_implemented ("SumKickData");
+    ULONG chksum = 0;
 
-    return 0L;
+    if (SysBase->KickTagPtr) {
+    	IPTR *list = SysBase->KickTagPtr;
+ 	while(*list)
+	{
+   	    chksum += (ULONG)*list;
+            /* on amiga, if bit 31 is set then this points to another list of
+             * modules rather than pointing to a single module. bit 31 is
+             * inconvenient on architectures where code may be loaded above
+             * 2GB. on these platforms we assume aligned pointers and use bit
+             * 0 instead */
+#ifdef __mc68000__
+	    if(*list & 0x80000000) { list = (IPTR *)(*list & 0x7fffffff); continue; }
+#else
+            if(*list & 0x1) { list = (IPTR *)(*list & ~(IPTR)0x1); continue; }
+#endif
+	    list++;
+   	}
+    }
+
+    if (SysBase->KickMemPtr) {
+	struct MemList *ml = (struct MemList*)SysBase->KickMemPtr;
+	while (ml->ml_Node.ln_Succ) {
+	    UBYTE i;
+	    ULONG *p = (ULONG*)ml;
+	    for (i = 0; i < sizeof(struct MemList) / sizeof(ULONG); i++)
+	    	chksum += p[i];
+	    ml = (struct MemList*)ml->ml_Node.ln_Succ;
+	}
+    }
+
+    return chksum;
+
     AROS_LIBFUNC_EXIT
 } /* SumKickData */
