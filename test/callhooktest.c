24b2d6f5a9ef2113caaac8e05a7de27bfe56f54a
ezrec <jason.mcmullan@gmail.com>
2011-03-01 05:15:35 +0000

    test: Fix some incorrect hook usage
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@37285 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/callhooktest.c
+++ test/callhooktest.c
@@ -18,23 +18,30 @@ struct Params
 	STACKED char  p13; STACKED short  p14; STACKED int p15; STACKED long  p16;
 };
 
-long TestHook(struct Hook *hook, APTR object, struct Params *params)
+AROS_UFH3(long, TestHook,
+	AROS_UFHA(struct Hook *, hook, A0),
+	AROS_UFHA(APTR, object, A2),
+	AROS_UFHA(struct Params *, params, A1))
 {
-	printf("TestHook(%p, %p, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d)\n", hook, object,
+	AROS_USERFUNC_INIT
+	printf("TestHook(%p, %p, %d, %d, %d, %ld, %d, %d, %d, %ld, %d, %d, %d, %ld, %d, %d, %d, %ld)\n", hook, object,
 		params->p1, params->p2, params->p3, params->p4, params->p5, params->p6, params->p7, params->p8, 
 		params->p9, params->p10, params->p11, params->p12, params->p13, params->p14, params->p15, params->p16);
 	return 0xbabadada;
+	AROS_USERFUNC_EXIT
 }
 
 int main (int argc, char ** argv)
 {
 	struct Hook hook;
-	hook.h_Entry = &TestHook;
+	hook.h_Entry = (HOOKFUNC)TestHook;
 	long res = CallHook(&hook, 0x0, 
 		(char) 0x1, (short) -0x1, (int) 0x2, (long) -0x2,
 		(char) 0x3, (short) -0x3, (int) 0x4, (long) -0x4,
 		(char) 0x5, (short) -0x5, (int) 0x6, (long) -0x6,
 		(char) 0x7, (short) -0x7, (int) 0x8, (long) -0x8
 	);
-	printf("CallHook result: %p\n", res);
+	printf("CallHook result: %lx\n", res);
+
+	return (res == 0xbabadada) ? 0 : 1;
 }
--- test/scanvarstest.c
+++ test/scanvarstest.c
@@ -12,23 +12,33 @@
 #include <proto/utility.h>
 #include <dos/var.h>
 
-LONG print_var(struct Hook *hook, APTR userdata, struct ScanVarsMsg *message)
+AROS_UFH3(LONG, print_var,
+	AROS_UFHA(struct Hook *, hook, A0),
+	AROS_UFHA(APTR, userdata, A2),
+	AROS_UFHA(struct ScanVarsMsg *, message, A1))
 {
-	printf("Hook called with userdata: %s\n", (char*) userdata);
-    printf("%s=%.*s\n", message->sv_Name, message->sv_VarLen, message->sv_Var);
+    AROS_USERFUNC_INIT
+    printf("Hook called with userdata: %s\n", (char*) userdata);
+    printf("%s=%.*s\n", message->sv_Name, (int)message->sv_VarLen, message->sv_Var);
     return 0;
+    AROS_USERFUNC_EXIT
 }
 
-LONG print_var_break(struct Hook *hook, APTR userdata, struct ScanVarsMsg *message)
+AROS_UFH3(LONG, print_var_break,
+	AROS_UFHA(struct Hook *, hook, A0),
+	AROS_UFHA(APTR, userdata, A2),
+	AROS_UFHA(struct ScanVarsMsg *, message, A1))
 {
-	printf("Hook called with userdata: %s\n", (char*) userdata);
-    printf("%s=%.*s\n", message->sv_Name, message->sv_VarLen, message->sv_Var);
+    AROS_USERFUNC_INIT
+    printf("Hook called with userdata: %s\n", (char*) userdata);
+    printf("%s=%.*s\n", message->sv_Name, (int)message->sv_VarLen, message->sv_Var);
     if(!strncmp("var2", message->sv_Name, 4))
     {
     	printf("Scanned var2 variable!\n");
     	return 1;
     }
     return 0;
+    AROS_USERFUNC_EXIT
 }
 
 int main(void)
@@ -38,11 +48,11 @@ int main(void)
     LONG ret;
     
     memset(&hook, 0, sizeof(struct Hook));
-    hook.h_Entry = print_var;
+    hook.h_Entry = (HOOKFUNC)print_var;
 
     printf("Scanning local variables:\n");
     ret = ScanVars(&hook, GVF_LOCAL_ONLY, userdata);
-    printf("ScanVars returned %d\n", ret);
+    printf("ScanVars returned %d\n", (int)ret);
 
     printf("Adding some new local variables:\n");
     SetVar("var1","Value of variable 1", -1, GVF_LOCAL_ONLY);
@@ -50,10 +60,12 @@ int main(void)
     SetVar("var3","Value of variable 3", -1, GVF_LOCAL_ONLY);
     printf("Scanning local variables again:\n");
     ret = ScanVars(&hook, GVF_LOCAL_ONLY, userdata);
-    printf("ScanVars returned %d\n", ret);
+    printf("ScanVars returned %d\n", (int)ret);
 
     printf("Trying to print all variables up to var2:\n");
-    hook.h_Entry = print_var_break;
+    hook.h_Entry = (HOOKFUNC)print_var_break;
     ret = ScanVars(&hook, GVF_LOCAL_ONLY, userdata);
-    printf("ScanVars returned %d\n", ret);
+    printf("ScanVars returned %d\n", (int)ret);
+
+    return 0;
 }
