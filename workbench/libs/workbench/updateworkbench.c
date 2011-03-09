df4ea8da7c1e0d7f70c88ebfea13f4202e8164c7
ezrec <jason.mcmullan@gmail.com>
2011-03-09 22:08:29 +0000

    workbench.library: Some documentation updates for UpdateWorkbench
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@37434 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/workbench/updateworkbench.c
+++ workbench/libs/workbench/updateworkbench.c
@@ -26,17 +26,35 @@
     AROS_LH3(BOOL, UpdateWorkbench,
 /*  SYNOPSIS */
     AROS_LHA(CONST_STRPTR,    name,   A0),
-    AROS_LHA(BPTR,            lock,   A1),
-    AROS_LHA(ULONG,           flags,  D0),
+    AROS_LHA(BPTR,      parentlock,   A1),
+    AROS_LHA(LONG,          action,   D0),
 
 /*  LOCATION */
     struct WorkbenchBase *, WorkbenchBase, 5, Workbench)
 
 /*  FUNCTION
+ 
+    This function does the "magic" of letting Workbench know that
+    an object has been added, changed, or removed. The name is
+    the name of the object, the lock is a lock on the directory that
+    contains the object. The action determines what has happened.
+    If UPDATEWB_ObjectAdded, the object is either NEW or has CHANGED.
+    If UPDATEWB_ObjectRemoved, the object has been deleted.
 
     INPUTS
+    
+    name         - Name of the object (without the .info)
+    parentlock   - Lock on the object's parent directory.
+    action       - UPDATEWB_ObjectAdded for a new or changed object
+                   UPDATEWB_ObjectRemoved for a deleted object
 
     RESULT
+
+    Workbench will update its display, if needed. An object that has
+    been deleted will be removed from the display. An object that is
+    new will be added to the respective display if it is not already
+    there; if it is already there, its appearance will be changed if
+    necessary.
 
     NOTES
 
@@ -52,7 +70,7 @@
 {
     AROS_LIBFUNC_INIT
     
-    D(bug("UpdateWorkbench(\"%s\", LOCK %p, 0x%08x)\n", name, lock, flags));
+    D(bug("UpdateWorkbench(\"%s\", LOCK %p, 0x%08x)\n", name, BADDR(parentlock), action));
     return TRUE;
         
     AROS_LIBFUNC_EXIT
