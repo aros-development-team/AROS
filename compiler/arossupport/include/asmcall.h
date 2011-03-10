49ffa30cbac7a2e33af5962f48d54120d37c1097
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-03-10 05:44:24 +0000

    AROS_HANDLER: Remove unused, borken macro
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@37439 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/arossupport/include/asmcall.h
+++ compiler/arossupport/include/asmcall.h
@@ -774,12 +774,6 @@ typedef unsigned int (*ULONG_FUNC)();
     ) {
 #endif
 
-/* Function declaration for DOS Handler startup code
- */
-#ifndef AROS_HANDLER
-#define AROS_HANDLER(t,n) AROS_UFH0(t,n)
-#endif
-
 /******************************************************************************
 *****  ENDE aros/asmcall.h
 ******************************************************************************/
