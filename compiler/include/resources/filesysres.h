b996a1649517b359107a16515d8aedc38c4817c5
ezrec <jason.mcmullan@gmail.com>
2011-03-10 05:44:33 +0000

    FileSystem.resource: Define what the fse_PatchFlags do
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@37441 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/resources/filesysres.h
+++ compiler/include/resources/filesysres.h
@@ -35,4 +35,27 @@ struct FileSysEntry
     BPTR        fse_GlobalVec;
 };
 
+/* fse_PatchFlags - when set, the corresponding
+ * field from struct FileSysEntry is used instead
+ * of the DOS defaults.
+ */
+#define FSEB_TYPE      0
+#define FSEF_TYPE      (1 << 0)
+#define FSEB_TASK      1
+#define FSEF_TASK      (1 << 1)
+#define FSEB_LOCK      2
+#define FSEF_LOCK      (1 << 2)
+#define FSEB_HANDLER   3
+#define FSEF_HANDLER   (1 << 3)
+#define FSEB_STACKSIZE 4
+#define FSEF_STACKSIZE (1 << 4)
+#define FSEB_PRIORITY  5
+#define FSEF_PRIORITY  (1 << 5)
+#define FSEB_STARTUP   6
+#define FSEF_STARTUP   (1 << 6)
+#define FSEB_SEGLIST   7
+#define FSEF_SEGLIST   (1 << 7)
+#define FSEB_GLOBALVEC 8
+#define FSEF_GLOBALVEC (1 << 8)
+
 #endif /* RESOURCES_FILESYSRES_H */
