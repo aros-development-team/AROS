8560c238b57e856f7742912756947c34421c76f7
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-03-14 06:11:50 +0000

    Added forgotten file
    git-svn-id: https://svn.aros.org/svn/aros/trunk@37517 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/network/stacks/AROSTCP/netinclude/net/sana2errno.h
@@ -0,0 +1,23 @@
+#ifndef NET_SANA2ERRNO_H
+#define NET_SANA2ERRNO_H
+/*
+ *      SANA-II error lists and printing
+ *
+ *      Copyright © 1994 AmiTCP/IP Group,
+ *                       Network Solutions Development, Inc.
+ *                       All rights reserved.
+ *
+ *      $Id: sana2errno.h,v 1.1.1.1 2005/03/15 16:00:46 laire Exp $
+ */
+
+extern const short io_nerr;
+extern const char * const io_errlist[]; 
+extern const short sana2io_nerr;
+extern const char * const sana2io_errlist[];
+extern const short sana2wire_nerr;
+extern const char * const sana2wire_errlist[];
+
+void sana2perror(const char *banner, struct IOSana2Req *ios2);
+void Sana2PrintFault(const char *banner, struct IOSana2Req *ios2);
+
+#endif /* !NET_SANA2ERRNO_H */
