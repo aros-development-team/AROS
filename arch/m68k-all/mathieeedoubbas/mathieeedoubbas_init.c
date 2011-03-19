b5b80dd83ab334c760cb27fa2eea53b5d6fe9725
twilen <twilen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-03-19 18:32:39 +0000

    Added more 68k mathieeedoubbas FPU versions.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@37662 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/m68k-all/mathieeedoubbas/mathieeedoubbas_init.c
+++ arch/m68k-all/mathieeedoubbas/mathieeedoubbas_init.c
@@ -15,12 +15,28 @@
 
 extern void AROS_SLIB_ENTRY(Mul_6888x,MathIeeeDoubBas)(void);
 extern void AROS_SLIB_ENTRY(Div_6888x,MathIeeeDoubBas)(void);
+extern void AROS_SLIB_ENTRY(Add_6888x,MathIeeeDoubBas)(void);
+extern void AROS_SLIB_ENTRY(Sub_6888x,MathIeeeDoubBas)(void);
+extern void AROS_SLIB_ENTRY(Neg_6888x,MathIeeeDoubBas)(void);
+extern void AROS_SLIB_ENTRY(Abs_6888x,MathIeeeDoubBas)(void);
+extern void AROS_SLIB_ENTRY(Fix_6888x,MathIeeeDoubBas)(void);
+extern void AROS_SLIB_ENTRY(Flt_6888x,MathIeeeDoubBas)(void);
 
 static int IEEEDP_Init(struct MathIeeeDoubBasBase *lh)
 {
-    if (SysBase->AttnFlags & (AFF_68881 | AFF_68040)) {
+    if (SysBase->AttnFlags & (AFF_68881 | AFF_68882 | AFF_68040)) {
+	SetFunc(5, Fix_6888x);
+	SetFunc(6, Flt_6888x);
+	//SetFunc(7, Cmp_6888x); use software
+	//SetFunc(8, Tst_6888x); use software
+	SetFunc(9, Abs_6888x);
+	SetFunc(10, Neg_6888x);
+	SetFunc(11, Add_6888x);
+	SetFunc(12, Sub_6888x);
 	SetFunc(13, Mul_6888x);
 	SetFunc(14, Div_6888x);
+	//SetFunc(15, Floor_6888x); use software
+	//SetFunc(16, Ceil_6888x); use software
     }
     return TRUE;
 }
