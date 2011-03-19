a5ab34a030c50707c007edd8e0a513ad814cb697
mattrust <mrustler@gmx.de>
2011-03-19 10:58:47 +0000

    Documentation style fix.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@37658 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/localtime_r.c
+++ compiler/clib/localtime_r.c
@@ -22,36 +22,28 @@
 	Splits the system time in seconds into a structure.
 	The members of the tm structure are:
 
-	\begin{description}
-	\item {tm_sec} The number of seconds after the minute, normally in
-		the range 0 to 59, but can be up to 61 to allow for leap
-		seconds.
+	tm_sec - The number of seconds after the minute, normally in
+		 the range 0 to 59, but can be up to 61 to allow for leap
+		 seconds.
 
-	\item{tm_min} The number of minutes after the hour, in the range 0
-		to 59.
+	tm_min - The number of minutes after the hour, in the range 0 to 59.
 
-	\item{tm_hour} The number of hours past midnight, in the range 0 to
-		23.
+	tm_hour - The number of hours past midnight, in the range 0 to 23.
 
-	\item{tm_mday} The day of the month, in the range 1 to 31.
+	tm_mday - The day of the month, in the range 1 to 31.
 
-	\item{tm_mon} The number of months since January, in the range 0 to
-		11.
+	tm_mon - The number of months since January, in the range 0 to 11.
 
-	\item{tm_year} The number of years since 1900.
+	tm_year - The number of years since 1900.
 
-	\item{tm_wday} The number of days since Sunday, in the range 0 to
-		6.
+	tm_wday - The number of days since Sunday, in the range 0 to 6.
 
-	\item{tm_yday} The number of days since January 1, in the range  0
-		to 365.
+	tm_yday - The number of days since January 1, in the range  0 to 365.
 
-	\item{tm_isdst} A flag that indicates whether daylight saving time
-		is in effect at the time described. The value is positive
-		if daylight saving time is in effect, zero if it is not,
-		and negative if the information is not available.
-
-	\end{description}
+	tm_isdst - A flag that indicates whether daylight saving time
+		   is in effect at the time described. The value is positive
+		   if daylight saving time is in effect, zero if it is not,
+		   and negative if the information is not available.
 
     INPUTS
 	tt - A time in seconds from the 1. Jan 1970
--- compiler/clib/printf.c
+++ compiler/clib/printf.c
@@ -28,52 +28,48 @@
 	correspond properly (after type promotion) with the conversion
 	specifier. After the %, the following appear in sequence:
 
-	\begin{itemize}
-	\item Zero or more of the following flags:
+	Zero or more of the following flags:
 
-	\begin{description}
-	\item{#} specifying that the value should be converted to an
-	``alternate form''. For c, d, i, n, p, s, and u conversions, this
-	option has no effect. For o conversions, the precision of the
-	number is increased to force the first character of the output
-	string to a zero (except if a zero value is printed with an
-	explicit precision of zero). For x and X conversions, a non-zero
-	result has the string `0x' (or `0X' for X conversions) prepended to
-	it. For e, E, f, g, and G conversions, the result will always
-	contain a decimal point, even if no digits follow it (normally, a
-	decimal point appears in the results of those conversions only if a
-	digit follows). For g and G conversions, trailing zeros are not
-	removed from the result as they would otherwise be.
+	# - specifying that the value should be converted to an
+	    ``alternate form''. For c, d, i, n, p, s, and u conversions, this
+	    option has no effect. For o conversions, the precision of the
+	    number is increased to force the first character of the output
+	    string to a zero (except if a zero value is printed with an
+	    explicit precision of zero). For x and X conversions, a non-zero
+	    result has the string `0x' (or `0X' for X conversions) prepended to
+	    it. For e, E, f, g, and G conversions, the result will always
+	    contain a decimal point, even if no digits follow it (normally, a
+	    decimal point appears in the results of those conversions only if a
+	    digit follows). For g and G conversions, trailing zeros are not
+	    removed from the result as they would otherwise be.
 
-	\item{0} specifying zero padding. For all conversions except n, the
-	converted value is padded on the left with zeros rather than
-	blanks. If a precision is given with a numeric conversion (d, i, o,
-	u, i, x, and X), the 0 flag is ignored.
+	0 - specifying zero padding. For all conversions except n, the
+	    converted value is padded on the left with zeros rather than
+	    blanks. If a precision is given with a numeric conversion (d, i, o,
+	    u, i, x, and X), the 0 flag is ignored.
 
-	\item{-} (a negative field width flag) indicates the converted
-	value is to be left adjusted on the field boundary. Except for n
-	conversions, the converted value is padded on the right with
-	blanks, rather than on the left with blanks or zeros. A -
-	overrides a 0 if both are given.
+	- - (a negative field width flag) indicates the converted
+	    value is to be left adjusted on the field boundary. Except for n
+	    conversions, the converted value is padded on the right with
+	    blanks, rather than on the left with blanks or zeros. A -
+	    overrides a 0 if both are given.
 
-	\item{ } (a space) specifying that a blank should be left before a
-	positive number produced by a signed conversion (d, e, E, f, g, G,
-	or i). + specifying that a sign always be placed before a number
-	produced by a signed conversion. A + overrides a space if both are
-	used.
+	  - (a space) specifying that a blank should be left before a
+	    positive number produced by a signed conversion (d, e, E, f, g, G,
+	    or i). + specifying that a sign always be placed before a number
+	    produced by a signed conversion. A + overrides a space if both are
+	    used.
 
-	\item{'} specifying that in a numerical argument the output is to
-	be grouped if the locale information indicates any. Note that many
-	versions of gcc cannot parse this option and will issue a warning.
+	' - specifying that in a numerical argument the output is to
+	    be grouped if the locale information indicates any. Note that many
+	    versions of gcc cannot parse this option and will issue a warning.
 
-	\end{description}
-
-	\item An optional decimal digit string specifying a minimum field
+	An optional decimal digit string specifying a minimum field
 	width. If the converted value has fewer characters than the field
 	width, it will be padded with spaces on the left (or right, if the
 	left-adjustment flag has been given) to fill out the field width.
 
-	\item An optional precision, in the form of a period (`.') followed
+	An optional precision, in the form of a period (`.') followed
 	by an optional digit string. If the digit string is omitted, the
 	precision is taken as zero. This gives the minimum number of digits
 	to appear for d, i, o, u, x, and X conversions, the number of
@@ -82,12 +78,12 @@
 	conversions, or the maximum number of characters to be printed from
 	a string for s conversions.
 
-	\item The optional character h, specifying that a following d, i,
+	The optional character h, specifying that a following d, i,
 	o, u, x, or X conversion corresponds to a short int or unsigned
 	short int argument, or that a following n conversion corresponds to
 	a pointer to a short int argument.
 
-	\item The optional character l (ell) specifying that a following d,
+	The optional character l (ell) specifying that a following d,
 	i, o, u, x, or X conversion applies to a pointer to a long int or
 	unsigned long int argument, or that a following n conversion
 	corresponds to a pointer to a long int argument. Linux provides a
@@ -95,20 +91,20 @@
 	ll can be used in combination with float conversions. This usage
 	is, however, strongly discouraged.
 
-	\item The character L specifying that a following e, E,
+	The character L specifying that a following e, E,
 	f, g, or G conversion corresponds to a long double
 	argument, or a following d, i, o, u, x, or X conversion corresponds to a long long argument. Note
 	that long long is not specified in ANSI C and
 	therefore not portable to all architectures.
 
-	\item The optional character q. This is equivalent to L. See the
+	The optional character q. This is equivalent to L. See the
 	STANDARDS and BUGS sections for comments on the use of ll, L, and
 	q.
 
-	\item A Z character specifying that the following integer (d, i, o,
+	A Z character specifying that the following integer (d, i, o,
 	u, i, x, and X), conversion corresponds to a size_t argument.
 
-	\item A character that specifies the type of conversion to be
+	A character that specifies the type of conversion to be
 	applied.
 
 	A field width or precision, or both, may be indicated by an
@@ -120,65 +116,61 @@
 
 	The conversion specifiers and their meanings are:
 
-	\begin{description}
-	\item{diouxX} The int (or appropriate variant) argument is
-	converted to signed decimal (d and i), unsigned octal (o, unsigned
-	decimal (u, or unsigned hexadecimal (x and X) notation. The letters
-	abcdef are used for x conversions; the letters ABCDEF are used for
-	X conversions. The precision, if any, gives the minimum number of
-	digits that must appear; if the converted value requires fewer
-	digits, it is padded on the left with zeros.
+	diouxX - The int (or appropriate variant) argument is
+		 converted to signed decimal (d and i), unsigned octal (o, unsigned
+		 decimal (u, or unsigned hexadecimal (x and X) notation. The letters
+		 abcdef are used for x conversions; the letters ABCDEF are used for
+		 X conversions. The precision, if any, gives the minimum number of
+		 digits that must appear; if the converted value requires fewer
+		 digits, it is padded on the left with zeros.
 
-	\item{eE} The double argument is rounded and converted in the style
-	[<->]d.dddedd where there is one digit before the decimal-point
-	character and the number of digits after it is equal to the
-	precision; if the precision is missing, it is taken as 6; if the
-	precision is zero, no decimal-point character appears. An E
-	conversion uses the letter E (rather than e) to introduce the
-	exponent. The exponent always contains at least two digits; if the
-	value is zero, the exponent is 00.
+	eE - The double argument is rounded and converted in the style
+	     [<->]d.dddedd where there is one digit before the decimal-point
+	     character and the number of digits after it is equal to the
+	     precision; if the precision is missing, it is taken as 6; if the
+	     precision is zero, no decimal-point character appears. An E
+	     conversion uses the letter E (rather than e) to introduce the
+	     exponent. The exponent always contains at least two digits; if the
+	     value is zero, the exponent is 00.
 
-	\item{f} The double argument is rounded and converted to decimal
-	notation in the style [-]ddd.ddd, where the number of digits after
-	the decimal-point character is equal to the precision
-	specification. If the precision is missing, it is taken as 6; if
-	the precision is explicitly zero, no decimal-point character
-	appears. If a decimal point appears, at least one digit appears
-	before it.
+	f - The double argument is rounded and converted to decimal
+	    notation in the style [-]ddd.ddd, where the number of digits after
+	    the decimal-point character is equal to the precision
+	    specification. If the precision is missing, it is taken as 6; if
+	    the precision is explicitly zero, no decimal-point character
+	    appears. If a decimal point appears, at least one digit appears
+	    before it.
 
-	\item{g} The double argument is converted in style f or e (or E for
-	G conversions). The precision specifies the number of significant
-	digits. If the precision is missing, 6 digits are given; if the
-	precision is zero, it is treated as 1. Style e is used if the
-	exponent from its conversion is less than -4 or greater than or
-	equal to the precision. Trailing zeros are removed from the
-	fractional part of the result; a decimal point appears only if it
-	is followed by at least one digit.
+	g - The double argument is converted in style f or e (or E for
+	    G conversions). The precision specifies the number of significant
+	    digits. If the precision is missing, 6 digits are given; if the
+	    precision is zero, it is treated as 1. Style e is used if the
+	    exponent from its conversion is less than -4 or greater than or
+	    equal to the precision. Trailing zeros are removed from the
+	    fractional part of the result; a decimal point appears only if it
+	    is followed by at least one digit.
 
-	\item{c} The int argument is converted to an unsigned char, and the
-	resulting character is written.
+	c - The int argument is converted to an unsigned char, and the
+	    resulting character is written.
 
-	\item{s} The ``char *'' argument is expected to be a pointer to an
-	array of character type (pointer to a string). Characters from the
-	array are written up to (but not including) a terminating NUL
-	character; if a precision is specified, no more than the number
-	specified are written. If a precision is given, no null character
-	need be present; if the precision is not specified, or is greater
-	than the size of the array, the array must contain a terminating
-	NUL character.
+	s - The ``char *'' argument is expected to be a pointer to an
+	    array of character type (pointer to a string). Characters from the
+	    array are written up to (but not including) a terminating NUL
+	    character; if a precision is specified, no more than the number
+	    specified are written. If a precision is given, no null character
+	    need be present; if the precision is not specified, or is greater
+	    than the size of the array, the array must contain a terminating
+	    NUL character.
 
-	\item{p} The ``void *'' pointer argument is printed in hexadecimal
-	(as if by %#x or %#lx).
+	p - The ``void *'' pointer argument is printed in hexadecimal
+	    (as if by %#x or %#lx).
 
-	\item{n} The number of characters written so far is stored into the
-	integer indicated by the ``int *'' (or variant) pointer argument.
-	No argument is converted.
+	n - The number of characters written so far is stored into the
+	    integer indicated by the ``int *'' (or variant) pointer argument.
+	    No argument is converted.
 
-	\item{%} A `%' is written. No argument is converted. The complete
-	conversion specification is `%%'.
-
-	\end{description}
-	\end{itemize}
+	% - A `%' is written. No argument is converted. The complete
+	    conversion specification is `%%'.
 
 	In no case does a non-existent or small field width cause
 	truncation of a field; if the result of a conversion is wider than
