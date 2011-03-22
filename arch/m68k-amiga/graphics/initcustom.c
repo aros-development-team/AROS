6d014f31496bca1d9f2b55c305b9caf1a59046c1
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-03-22 14:15:56 +0000

    Some rearrangements, partially got rid of architecture-specific code in graphics.library
    git-svn-id: https://svn.aros.org/svn/aros/trunk@37721 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/m68k-amiga/graphics/initcustom.c
+++ arch/m68k-amiga/graphics/initcustom.c
@@ -1,16 +1,33 @@
 /*
-    Copyright © 1995-2010, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2011, The AROS Development Team. All rights reserved.
 
     Desc: Detect custom chipset revisions and PAL/NTSC state
     Lang: english
 */
 
 #define DEBUG 0
+
 #include <aros/debug.h>
+#include <aros/symbolsets.h>
 #include <graphics/gfxbase.h>
 #include <hardware/custom.h>
 #include <hardware/cia.h>
 
+/*
+ * TODO: it's not architecturally good to have hardware-specific code in graphics.library.
+ * During init graphics.library instantiates default graphics driver. One of ideas is to
+ * instantiate Amiga(tm) chipset driver instead on appropriate hardware. In this case the
+ * driver will be able to do this stuff for us.
+ * At the other hand, what if the user owns an Amiga with VGA monitor and graphics card and
+ * intentionally doesn't want to use chipset driver at all (assuming loadable modular kickstart)?
+ * This stuff is vital for correct functioning of timer.device, so it's logical to move this
+ * code to kernel.resource instead. But in this case we would have to: a) duplicate the code
+ * to set DisplayFlags and ChipsetFlags; or b) Add these flags to the list of kernel.resource
+ * attributes (which is again not quite good because these things are very Amiga-specific).
+ * UPD: c) We could move this code to something like amigacustom.resource which would be
+ * totally hardware-specific part. Presence of this resource would clearly indicate to the 
+ * software that it's actually running on Amiga hardware.
+ */
 static UWORD getline(void)
 {
 	volatile struct Custom *custom = (struct Custom*)0xdff000;
@@ -41,14 +58,13 @@ static UWORD gethighestline(UWORD linecnt)
 	return highest;
 }	
 
-void InitCustom(struct GfxBase *gfx)
+static int InitCustom(struct GfxBase *gfx)
 {
 	volatile struct Custom *custom = (struct Custom*)0xdff000;
-	volatile struct CIA *ciaa = (struct CIA*)0xbfe001;
 	UWORD vposr, deniseid;
 	UWORD flags = 0;
-	UBYTE chipflags = 0, todlow;
-	UWORD pos, todlo, pshz, todcnt;
+	UBYTE chipflags = 0;
+	UWORD pos;
 
 	Disable();
 
@@ -75,45 +91,19 @@ void InitCustom(struct GfxBase *gfx)
    		if (deniseid == 0xf8)
    			chipflags |= GFXF_AA_LISA;
    	}
+
+   	Enable();
+
    	if (vposr >= 0x2200)
    		chipflags |= GFXF_AA_ALICE;
- 
-	/* check powersupply tick rate */
-	ciaa->ciacra = 0x00;
-	ciaa->ciatodhi = 0;
-	ciaa->ciatodmid = 0;
-	ciaa->ciatodlow = 0;
-	ciaa->ciatalo = 0xff;
-	ciaa->ciatahi = 0xff;
-	todlo = ciaa->ciatodlow;
-	while (todlo == ciaa->ciatodlow);
-	ciaa->ciacra = 0x01;
-	todlo = ciaa->ciatodlow;
-	while (todlo == ciaa->ciatodlow);
-	ciaa->ciacra = 0x00;
-	todcnt = ~(((ciaa->ciatahi << 8) | ciaa->ciatalo) + 1);
-
-    	/* 50Hz/60Hz ticks:
-    	 * 50Hz PAL  14188
-    	 * 60Hz NTSC 11932
-    	 * 50Hz NTSC 14318
-    	 * 60Hz PAL  11823
-    	 */
-    	if (todcnt > 14188 + (14318 - 14188) / 2) {
-    		pshz = 50;
-    	} else if (todcnt <= 11823 + (11932 - 11823) / 2) {
-    		pshz = 60;
-    	} else if (todcnt > 14188 - (14188 - 11932) / 2) {
-    		pshz = 50;
-    	} else {
-     		pshz = 60;
-	}
-
-	Enable();
 
 	gfx->DisplayFlags = flags;
 	gfx->ChipRevBits0 = chipflags;
-	SysBase->PowerSupplyFrequency = pshz;
+
 	SysBase->VBlankFrequency = (flags & PAL) ? 50 : 60;
 
+	return TRUE;
 }
+
+/* This is run before the main init routine */
+ADD2INITLIB(InitCustom, -5);
---
+++ arch/m68k-amiga/kernel/kernel_timer.c
@@ -0,0 +1,53 @@
+/*
+ * This code probes Amiga hardware timings.
+ *
+ * On Amiga(tm) we have hardware VBlank interrupt, so we have no emulation code.
+ */
+
+#include <aros/symbolsets.h>
+#include <exec/execbase.h>
+#include <hardware/cia.h>
+
+static int Timer_Init(APTR KernelBase)
+{
+    volatile struct CIA *ciaa = (struct CIA*)0xbfe001;
+    UWORD todlo, todcnt;
+
+    /*
+     * Check powersupply tick rate.
+     * No Disable() here because we are running without interrupts during early init.
+     */
+    ciaa->ciacra = 0x00;
+    ciaa->ciatodhi = 0;
+    ciaa->ciatodmid = 0;
+    ciaa->ciatodlow = 0;
+    ciaa->ciatalo = 0xff;
+    ciaa->ciatahi = 0xff;
+    todlo = ciaa->ciatodlow;
+    while (todlo == ciaa->ciatodlow);
+    ciaa->ciacra = 0x01;
+    todlo = ciaa->ciatodlow;
+    while (todlo == ciaa->ciatodlow);
+    ciaa->ciacra = 0x00;
+    todcnt = ~(((ciaa->ciatahi << 8) | ciaa->ciatalo) + 1);
+
+    /*
+     * 50Hz/60Hz ticks:
+     * 50Hz PAL  14188
+     * 60Hz NTSC 11932
+     * 50Hz NTSC 14318
+     * 60Hz PAL  11823
+     */
+    if (todcnt > 14188 + (14318 - 14188) / 2)
+    	SysBase->PowerSupplyFrequency = 50;
+    else if (todcnt <= 11823 + (11932 - 11823) / 2)
+    	SysBase->PowerSupplyFrequency = 60;
+    else if (todcnt > 14188 - (14188 - 11932) / 2)
+    	SysBase->PowerSupplyFrequency = 50;
+    else
+    	SysBase->PowerSupplyFrequency = 60;
+
+    return TRUE;
+}
+
+ADD2INITLIB(Timer_Init, 0)
