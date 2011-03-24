bbaf595159bc05abc35b9ef19c0a110e75e675ce
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-03-24 08:38:17 +0000

    Added AROS boot taglist parsing routine
    git-svn-id: https://svn.aros.org/svn/aros/trunk@37759 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ boot/libconsole/init_taglist.c
@@ -0,0 +1,46 @@
+/*
+    Copyright © 1995-2011, The AROS Development Team. All rights reserved.
+    $Id: init_multiboot.c -1   $
+
+    Desc: Parse AROS boot taglist and init console.
+*/
+
+#include <aros/kernel.h>
+#include <hardware/vbe.h>
+#include <utility/tagitem.h>
+#include <proto/alib.h>
+
+#include <bootconsole.h>
+
+#include "console.h"
+
+void con_InitTagList(const struct TagItem *tags)
+{
+    struct TagItem *tag;
+    struct vbe_mode *vbemode = NULL;
+    /* By default we have 2.0 data (framebuffer pointer filled in) */
+    unsigned short vbever = 0x0200;
+
+    while ((tag = LibNextTagItem(&tags)))
+    {
+	switch (tag->ti_Tag)
+	{
+	case KRN_CmdLine:
+	    con_InitSerial((char *)tag->ti_Data);
+	    break;
+
+	case KRN_VBEModeInfo:
+	    vbemode = (struct vbe_mode *)tag->ti_Data;
+	    break;
+
+	case KRN_VBEControllerInfo:
+	    vbever = ((struct vbe_controller *)tag->ti_Data)->version;
+	    break;
+	}
+    }
+
+    if (vbemode)
+	con_InitVESA(vbever, vbemode);
+    else
+    	con_InitVGA();
+}
