b597b5872b9e062d6076caf2f91bc9cbe696e6bf
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-03-25 09:26:46 +0000

    More bootstrap rework. Separated CPU-specific code, added some useful debug
    git-svn-id: https://svn.aros.org/svn/aros/trunk@37775 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/x86_64-pc/bootstrap/bootstrap.c
+++ arch/x86_64-pc/bootstrap/bootstrap.c
@@ -9,11 +9,13 @@
 //#define DEBUG
 //#define DEBUG_MEM
 //#define DEBUG_MEM_TYPE MMAP_TYPE_RAM
+//#define DEBUG_TAGLIST
 
 #include <aros/kernel.h>
 #include <aros/multiboot.h>
 
 #include <bootconsole.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "bootstrap.h"
@@ -78,190 +80,14 @@ extern char *_prot_lo, *_prot_hi;
 static unsigned char __stack[65536] __attribute__((used));
 static unsigned char __bss_track[32768] __attribute__((used,section(".bss.aros.tables")));
 
-static struct {
-    struct segment_desc seg0;      /* seg 0x00 */
-    struct segment_desc super_cs;  /* seg 0x08 */
-    struct segment_desc super_ds;  /* seg 0x10 */
-} GDT __attribute__((used,aligned(128),section(".bss.aros.tables")));
-
-/*
-    The MMU pages and directories. They are stored at fixed location and may be either reused in the 
-    64-bit kernel, or replaced by it. Four PDE directories (PDE2M structures) are enough to map whole
-    4GB address space.
-*/
-static struct PML4E PML4[512] __attribute__((used,aligned(4096),section(".bss.aros.tables")));
-static struct PDPE PDP[512] __attribute__((used,aligned(4096),section(".bss.aros.tables")));
-static struct PDE2M PDE[4][512] __attribute__((used,aligned(4096),section(".bss.aros.tables")));
-
-static struct vbe_mode VBEModeInfo = {0};
-static struct vbe_controller VBEControllerInfo = {{0},0};
+static struct vbe_mode VBEModeInfo __attribute__((used, section(".bss.aros.tables")));
+static struct vbe_controller VBEControllerInfo __attribute__((used, section(".bss.aros.tables")));
 
 struct TagItem64 km[KRN__TAGCOUNT * 4] __attribute__((used, section(".bss.aros.tables")));
 
 struct TagItem64 *tag = &km[0];
 
-static struct {
-    void *off;
-    unsigned short seg;
-} __attribute__((packed)) KernelTarget = { (void*)KERNEL_TARGET_ADDRESS, SEG_SUPER_CS };
-
-/*
-    32-bit pseudo-registers used to load the Global and Interrupt Descriptor Tables. They are used only once.
-*/
-const struct
-{
-    unsigned short size __attribute__((packed));
-    unsigned int base __attribute__((packed));
-} 
-GDT_sel = {sizeof(GDT)-1, (unsigned int)&GDT};
-
 static const char default_mod_name[] = "<unknown>";
-
-/*
-    Setting descriptor tables up. It is perhaps not wise to embed it into the function. Most likely the more
-    effective solution would be to make a local copy of new descriptor table instead. But the code is more clear
-    and takes effectively *no place* since it is loaded at bootup and removed later.
-*/
-static void setup_tables()
-{
-    kprintf("[BOOT] Setting up descriptor tables.\n");
-
-    /* Supervisor segments */
-    GDT.super_cs.type=0x1a;	/* code segment */
-    GDT.super_cs.dpl=0;		/* supervisor level */
-    GDT.super_cs.p=1;		/* present */
-    GDT.super_cs.l=1;		/* long (64-bit) one */
-    GDT.super_cs.d=0;		/* must be zero */
-    GDT.super_cs.limit_low=0xffff;
-    GDT.super_cs.limit_high=0xf;
-    GDT.super_cs.g=1;
-    GDT.super_cs.base_low=0;
-    GDT.super_cs.base_mid=0;
-    GDT.super_cs.base_high=0;
-    
-    GDT.super_ds.type=0x12;	/* data segment */
-    GDT.super_ds.dpl=0;		/* supervisor level */
-    GDT.super_ds.p=1;		/* present */
-    GDT.super_ds.limit_low=0xffff;
-    GDT.super_ds.limit_high=0xf;
-    GDT.super_ds.g=1;
-    GDT.super_ds.d=1;   
-    GDT.super_ds.base_low=0;
-    GDT.super_ds.base_mid=0;
-    GDT.super_ds.base_high=0;
-}
-
-/*
-    The 64-bit long mode may be activated only, when MMU paging is enabled. Therefore the basic
-    MMU tables have to be prepared first. This routine uses 6 tables (2048 + 5 entries) in order
-    to map the first 4GB of address space as user-accessible executable RW area.
-    
-    This mapping may be changed later by the 64-bit kernel, in order to provide separate address spaces,
-    protect kernel from being overwritten and so on and so forth.
-*/
-static void setup_mmu()
-{
-    int i;
-    struct PDE2M *pdes[] = { &PDE[0], &PDE[1], &PDE[2], &PDE[3] };
-    
-    kprintf("[BOOT] Mapping first 4G area with MMU\n");
-
-    /* PML4 Entry - we need only the first out of 16 entries */
-    PML4[0].p  = 1; /* present */
-    PML4[0].rw = 1; /* read/write */
-    PML4[0].us = 1; /* accessible for user */
-    PML4[0].pwt= 0; /* write-through cache */
-    PML4[0].pcd= 0; /* cache enabled */
-    PML4[0].a  = 0; /* not yet accessed */
-    PML4[0].mbz= 0; /* must be zero */
-    PML4[0].base_low = (unsigned int)PDP >> 12;
-    PML4[0].avl= 0;
-    PML4[0].nx = 0;
-    PML4[0].avail = 0;
-    PML4[0].base_high = 0;
-
-    /*
-        PDP Entries. There are four of them used in order to define 2048 pages of 2MB each.
-     */
-    for (i=0; i < 4; i++)
-    {
-        int j;
-	
-        /* Set the PDP entry up and point to the PDE table */
-        PDP[i].p  = 1;
-        PDP[i].rw = 1;
-        PDP[i].us = 1;
-        PDP[i].pwt= 0;
-        PDP[i].pcd= 0;
-        PDP[i].a  = 0;
-        PDP[i].mbz= 0;
-        PDP[i].base_low = (unsigned int)pdes[i] >> 12;
-
-        PDP[i].nx = 0;
-        PDP[i].avail = 0;
-        PDP[i].base_high = 0;
-
-        for (j=0; j < 512; j++)
-        {
-            /* Set PDE entries - use 2MB memory pages, with full supervisor and user access */
-	    
-            struct PDE2M *PDE = pdes[i];
-            PDE[j].p  = 1;
-            PDE[j].rw = 1;
-            PDE[j].us = 1;
-            PDE[j].pwt= 0;  // 1
-            PDE[j].pcd= 0;  // 1
-            PDE[j].a  = 0;
-            PDE[j].d  = 0;
-            PDE[j].g  = 0;
-            PDE[j].pat= 0;
-            PDE[j].ps = 1;
-            PDE[j].base_low = ((i << 30) + (j << 21)) >> 13;
-            
-            PDE[j].avail = 0;
-            PDE[j].nx = 0;
-            PDE[j].base_high = 0;
-        }
-    }
-}
-
-/*
- * This tiny procedure sets the complete 64-bit environment up - it loads the descriptors, 
- * enables 64-bit mode, loads MMU tables and through paging it activates the 64-bit long mode.
- * 
- * After that it is perfectly safe to jump into the pure 64-bit kernel.
- */
-int leave_32bit_mode()
-{
-    unsigned int v1 = 0, v2 = 0, v3 = 0, v4 = 0;
-    int retval = 0;
-    asm volatile ("lgdt %0"::"m"(GDT_sel));
-
-    cpuid(0x80000000, v1, v2, v3, v4);
-    if (v1 > 0x80000000)
-    { 
-        cpuid(0x80000001, v1, v2, v3, v4);
-        if (v4 & (1 << 29))
-        {
-            /* Enable PAE */
-            wrcr(cr4, _CR4_PAE | _CR4_PGE);
-            
-            /* enable pages */
-            wrcr(cr3, &PML4);
-        
-            /* enable long mode */
-            rdmsr(EFER, &v1, &v2);
-            v1 |= _EFER_LME;
-            wrmsr(EFER, v1, v2);
-        
-            /* enable paging and activate long mode */
-            wrcr(cr0, _CR0_PG | _CR0_PE);
-            retval = 1;
-        }
-    }
-    
-    return retval;
-}
 
 /*
  * External modules.
@@ -530,59 +356,13 @@ static void setupFB(struct multiboot *mb)
     }
 }
 
-void change_kernel_address(const char *str)
+static void prepare_message(void *kick_base)
 {
-    char *kern = strstr(str, "base_address=");
-    if (kern)
-    {
-        unsigned long addr=0;
-        unsigned char a;
-        kern+=13;
-
-        while (*kern && *kern != ' ' )
-        {
-            a = *kern++;
-
-            if (a >= '0' && a <= '9')
-                a -= '0';
-            else if (a >= 'a' && a <= 'f')
-                a -= 'a' - 10;
-            else if (a >= 'A' && a <= 'F')
-                a -= 'A' - 10;
-            else break;
-            
-            addr = (addr << 4) + a;
-        }
-
-        if (addr >= 0x00200000)
-        {
-            kprintf("[BOOT] Kernel base address changed to %p\n", addr);
-
-            KernelTarget.off = (void*)addr;
-            set_base_address((void *)addr, __bss_track);
-        }
-        else
-        {
-            kprintf("[BOOT] Kernel base address too low (%p). Keeping default.\n", addr);
-        }
-    }
-}
-
-void prepare_message(struct multiboot *mb)
-{
-    D(kprintf("[BOOT] Kickstart 0x%p - 0x%p, protection 0x%p - 0x%p\n", kernel_lowest(), kernel_highest(), &_prot_lo, &_prot_hi));
-    /*km.GRUBData.low = mb; */
-
-    tag->ti_Tag = KRN_GDT;
-    tag->ti_Data = KERNEL_OFFSET | (unsigned long)&GDT;
-    tag++;
-
-    tag->ti_Tag = KRN_PL4;
-    tag->ti_Data = KERNEL_OFFSET | (unsigned long)&PML4;
-    tag++;
+    D(kprintf("[BOOT] Kickstart 0x%p - 0x%p (entry 0x%p), protection 0x%p - 0x%p\n", kernel_lowest(), kernel_highest(), kick_base,
+    	      &_prot_lo, &_prot_hi));
 
     tag->ti_Tag = KRN_KernelBase;
-    tag->ti_Data = KERNEL_OFFSET | (unsigned long)KernelTarget.off;
+    tag->ti_Data = KERNEL_OFFSET | (unsigned long)kick_base;
     tag++;
 
     tag->ti_Tag = KRN_KernelLowest;
@@ -631,6 +411,7 @@ static void __attribute__((used)) __bootstrap(unsigned int magic, unsigned int a
     int module_count = 0;
     unsigned long vesa_size = 0;
     unsigned long vesa_base = 0;
+    void *kick_base = (void *)KERNEL_TARGET_ADDRESS;
     struct module *m;
     const char *cmdline = NULL;
 
@@ -652,11 +433,22 @@ static void __attribute__((used)) __bootstrap(unsigned int magic, unsigned int a
     	D(kprintf("[BOOT] Command line @ %p : '%s'\n", mb->cmdline, cmdline));
     }
 
-    set_base_address((void *)KERNEL_TARGET_ADDRESS, __bss_track);
-    
     if (cmdline)
     {
-    	change_kernel_address(cmdline);
+        char *kern = strstr(cmdline, "base_address=");
+        
+        if (kern)
+        {
+            unsigned long p = strtoul(&kern[13], NULL, 0);
+
+            if (p >= 0x00200000)
+            {
+            	kick_base = (void *)p;
+            	kprintf("[BOOT] Kernel base address changed to %p\n", kick_base);
+            }
+            else
+	        kprintf("[BOOT] Kernel base address too low (%p). Keeping default.\n", p);
+	}
 
     	vesa = strstr(cmdline, "vesa=");
     	if (vesa)
@@ -664,9 +456,7 @@ static void __attribute__((used)) __bootstrap(unsigned int magic, unsigned int a
 
 	tag->ti_Tag = KRN_CmdLine;
     	tag->ti_Data = KERNEL_OFFSET | (unsigned long)cmdline;
-
     	tag++;
-    	tag->ti_Tag = TAG_DONE;
     }
 
     if ((mb->mmap_addr) && (mb->mmap_length))
@@ -735,8 +525,6 @@ static void __attribute__((used)) __bootstrap(unsigned int magic, unsigned int a
         tag++;
     }
 
-    tag->ti_Tag = TAG_DONE;
-
     /*
      * If vesa= option was given, set up the specified video mode explicitly.
      * Otherwise specify to AROS what has been passed to us by the bootloader.
@@ -746,16 +534,17 @@ static void __attribute__((used)) __bootstrap(unsigned int magic, unsigned int a
     else
     	setupFB(mb);
 
-    /* Setup stage - prepare 64-bit environment */
-    setup_tables();
-    setup_mmu();
+    /* Setup stage - prepare the environment */
+    setup_mmu(kick_base);
 
-    /* Load the first ELF relocable object - the kernel itself */
-    kprintf("[BOOT] Loading kernel\n");
-    //load_elf_file(&_binary_aros_o_start, 0); //((unsigned long long)KERNEL_HIGH_OFFSET) << 39);
+    kprintf("[BOOT] Loading kickstart...\n");
+    set_base_address(kick_base, __bss_track);
 
     /* Search for external modules loaded by GRUB */
     module_count = find_modules(mb, mod);
+
+    if (module_count == 0)
+    	panic("No kickstart modules found, nothing to run");
 
     /* If any external modules are found, load them now */
     for (m = mod; module_count > 0; module_count--, m++)
@@ -765,26 +554,17 @@ static void __attribute__((used)) __bootstrap(unsigned int magic, unsigned int a
         kprintf("\n");
     }
 
-    /*
-     * Prepare machine to leave the 32-bit mode. Activate 64-bit mode.
-     */
-    prepare_message(mb);
+    /* Prepare the rest of boot taglist */
+    prepare_message(kick_base);
 
-    /*
-     * And do a necessary long jump into the 64-bit kernel
-     */
-    kprintf("[BOOT] Leaving 32-bit environment."
-        " LJMP $%x,$%p\n\n", SEG_SUPER_CS, KernelTarget.off);
+#ifdef DEBUG_TAGLIST
+    kprintf("[BOOT] Boot taglist:\n");
+    for (tag = km; tag->ti_Tag != TAG_DONE; tag++)
+    	kprintf("[BOOT] 0x%llp 0x%llp\n", tag->ti_Tag, tag->ti_Data);
+#endif
 
-    if (leave_32bit_mode())
-        asm volatile("ljmp *%0"::"m"(KernelTarget),"D"(&km));
-    else
-    	kprintf("[BOOT] PANIC! Your CPU does not support Long Mode\n");
+    /* Jump to the kickstart */
+    kick(km);
 
-    /*
-     * This code should not be reached at all.  
-     */
-    kprintf("HALT!\n");
-
-    for(;;) asm volatile("hlt");
+    panic("Failed to run the kickstart");
 }
