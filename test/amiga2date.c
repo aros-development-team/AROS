0f38867286190a9b46fba19210f91d4f5f6c1304
mattrust <mrustler@gmx.de>
2011-03-26 16:11:32 +0000

    2 more tests prepared.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@37790 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/amiga2date.c
+++ test/amiga2date.c
@@ -1,11 +1,11 @@
+#include <aros/debug.h>
+
 #include <dos/dos.h>
 #include <utility/date.h>
 #include <proto/exec.h>
 #include <proto/dos.h>
 #include <proto/utility.h>
-#include <stdio.h>
 
-struct UtilityBase *UtilityBase;
 struct DateTime dt;
 struct ClockData cd;
 
@@ -13,58 +13,86 @@ char s[100];
 
 int main(void)
 {
-    UtilityBase = (struct UtilityBase *)OpenLibrary("utility.library", 0);
-    if (UtilityBase)
+    int retval = RETURN_OK;
+
+    dt.dat_StrDate = "31-dec-2000";
+    dt.dat_Format = FORMAT_DOS;
+    
+    if (StrToDate(&dt))
     {
-	dt.dat_StrDate = "31-dec-2000";
-	dt.dat_Format = FORMAT_DOS;
-	
-	if (StrToDate(&dt))
-	{
-	    dt.dat_StrDate = s;
-	    if (DateToStr(&dt))
-	    {
-	    	printf("Verified date: \"%s\" days = %ld min = %ld tick = %ld\n",
-		    	s,
-		    	(long)dt.dat_Stamp.ds_Days,
-			(long)dt.dat_Stamp.ds_Minute,
-			(long)dt.dat_Stamp.ds_Tick);
-	    	
-		Amiga2Date(dt.dat_Stamp.ds_Days * 60 * 60 * 24 + 
-		    	   dt.dat_Stamp.ds_Minute * 60 +
-			   dt.dat_Stamp.ds_Tick / 50, &cd);
-			   
-		printf("\nAmiga2Date says:\n\n");
-		printf("sec   = %d\n", cd.sec);
-		printf("min   = %d\n", cd.min);
-		printf("hour  = %d\n", cd.hour);
-		printf("mday  = %d\n", cd.mday);
-		printf("month = %d\n", cd.month);
-		printf("year  = %d\n", cd.year);
-		printf("wday  = %d\n", cd.wday);
+        dt.dat_StrDate = s;
+        if (DateToStr(&dt))
+        {
+            if (strcmp(s, "31-Dec-00")
+                || dt.dat_Stamp.ds_Days != 8400
+                || dt.dat_Stamp.ds_Minute != 0
+                || dt.dat_Stamp.ds_Tick != 0)
+            {
+                retval = RETURN_ERROR;
+                bug("DateToStr returned: \"%s\" days = %ld min = %ld tick = %ld\n",
+                    s,
+                    (long)dt.dat_Stamp.ds_Days,
+                    (long)dt.dat_Stamp.ds_Minute,
+                    (long)dt.dat_Stamp.ds_Tick);
+            }
 
-    	    	printf("\n-------- One day later -----------\n\n");
-		
-		Amiga2Date((dt.dat_Stamp.ds_Days + 1) * 60 * 60 * 24 + 
-		    	   dt.dat_Stamp.ds_Minute * 60 +
-			   dt.dat_Stamp.ds_Tick / 50, &cd);
-			   
-		printf("\nAmiga2Date says:\n\n");
-		printf("sec   = %d\n", cd.sec);
-		printf("min   = %d\n", cd.min);
-		printf("hour  = %d\n", cd.hour);
-		printf("mday  = %d\n", cd.mday);
-		printf("month = %d\n", cd.month);
-		printf("year  = %d\n", cd.year);
-		printf("wday  = %d\n", cd.wday);
+            Amiga2Date(dt.dat_Stamp.ds_Days * 60 * 60 * 24 + 
+                       dt.dat_Stamp.ds_Minute * 60 +
+                       dt.dat_Stamp.ds_Tick / 50, &cd);
 
-	    }
-	    else puts("DateToStr failed!");
-	}
-	else puts("StrToDate failed!");
-	
-    	CloseLibrary((struct Library *)UtilityBase);
+            if (cd.sec != 0
+                || cd.min != 0
+                || cd.hour != 0
+                || cd.mday != 31
+                || cd.month != 12
+                || cd.year != 2000
+                || cd.wday != 0)
+            {
+                retval = RETURN_ERROR;
+                bug("\nAmiga2Date says:\n\n");
+                bug("sec   = %d\n", cd.sec);
+                bug("min   = %d\n", cd.min);
+                bug("hour  = %d\n", cd.hour);
+                bug("mday  = %d\n", cd.mday);
+                bug("month = %d\n", cd.month);
+                bug("year  = %d\n", cd.year);
+                bug("wday  = %d\n", cd.wday);
+            }
+
+            Amiga2Date((dt.dat_Stamp.ds_Days + 1) * 60 * 60 * 24 + 
+                       dt.dat_Stamp.ds_Minute * 60 +
+                       dt.dat_Stamp.ds_Tick / 50, &cd);
+
+            if (cd.sec != 0
+                || cd.min != 0
+                || cd.hour != 0
+                || cd.mday != 1
+                || cd.month != 1
+                || cd.year != 2001
+                || cd.wday != 1)
+            {
+                retval = RETURN_ERROR;
+                bug("\nAmiga2Date says (one day later:\n\n");
+                bug("sec   = %d\n", cd.sec);
+                bug("min   = %d\n", cd.min);
+                bug("hour  = %d\n", cd.hour);
+                bug("mday  = %d\n", cd.mday);
+                bug("month = %d\n", cd.month);
+                bug("year  = %d\n", cd.year);
+                bug("wday  = %d\n", cd.wday);
+            }
+        }
+        else
+        {
+            retval = RETURN_ERROR;
+            bug("Calling DateToStr failed!\n");
+        }
+    }
+    else
+    {
+        retval = RETURN_ERROR;
+        bug("Calling StrToDate failed!\n");
     }
 
-    return 0;
+    return retval;
 }
