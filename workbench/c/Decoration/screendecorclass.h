515d8a04ec774e3822a3aaac89aa4a241a19799f
deadwood-pl <deadwood@wp.pl>
2011-03-27 08:45:52 +0000

    ScreenDecorClass - expose original images loaded from theme in separate structure.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@37801 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/Decoration/screendecorclass.h
+++ workbench/c/Decoration/screendecorclass.h
@@ -58,8 +58,46 @@ struct scrdecor_data
     LONG             text_col, shadow_col;
 };
 
+/* Holds all images used for screen, window and menu decoration */
+struct DecorImages
+{
+    struct NewImage *img_sdepth;
+    struct NewImage *img_sbarlogo;
+    struct NewImage *img_stitlebar;
+
+    struct NewImage *img_size;
+    struct NewImage *img_close;
+    struct NewImage *img_depth;
+    struct NewImage *img_zoom;
+    struct NewImage *img_up;
+    struct NewImage *img_down;
+    struct NewImage *img_left;
+    struct NewImage *img_right;
+    struct NewImage *img_mui;
+    struct NewImage *img_popup;
+    struct NewImage *img_snapshot;
+    struct NewImage *img_iconify;
+    struct NewImage *img_lock;
+    struct NewImage *img_winbar_normal;
+    struct NewImage *img_border_normal;
+    struct NewImage *img_border_deactivated;
+    struct NewImage *img_verticalcontainer;
+    struct NewImage *img_verticalknob;
+    struct NewImage *img_horizontalcontainer;
+    struct NewImage *img_horizontalknob;
+
+    struct NewImage *img_menu;
+    struct NewImage *img_amigakey;
+    struct NewImage *img_menucheck;
+    struct NewImage *img_submenu;
+};
+
+
 struct ScreenData
 {
+    /* These are default decorator images translated to depth/attributes of screen
+       on which the decorator is used. WindowData and MenuData points into these
+       objects */
     struct NewImage img_sdepth;
     struct NewImage img_sbarlogo;
     struct NewImage img_stitlebar;
@@ -96,7 +134,8 @@ struct ScreenData
 };
 
 #define SDA_Configuration   0x20002
-#define SDA_ScreenData      0x20003
+#define SDA_ScreenData      0x20003 /* TODO: remove */
+#define SDA_DecorImages     0x20004
 
 IPTR ScrDecor_Dispatcher(struct IClass *cl, Object *obj, Msg msg);
 #endif
