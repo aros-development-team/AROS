d08ca13a32c5da92975e60fa70c050e1c8f30563
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-03-28 09:16:54 +0000

    - Fixed VBE structures definition, pointers are always 32-bit there - boot console support library moved to more appropriate location and renamed to libbootconsole.a to avoid conflict with console.device stub library
    git-svn-id: https://svn.aros.org/svn/aros/trunk@37861 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/boot/testboot.c
@@ -0,0 +1,62 @@
+#include <aros/kernel.h>
+#include <hardware/vbe.h>
+#include <utility/tagitem.h>
+
+#include <bootconsole.h>
+#include <stdarg.h>
+
+int __vcformat(void *data, int(*outc)(int, void *), const char * format, va_list args);
+int kprintf(const char *format, ...);
+
+void __startup start(struct TagItem *tags)
+{
+    struct vbe_mode *vbemode = NULL;
+
+    fb_Mirror = (void *)0x100000;
+
+    con_InitTagList(tags);
+
+    kprintf("Test module succesfully started\n");
+
+    kprintf("Taglist at 0x%p:\n", tags);
+    for (; tags->ti_Tag != TAG_DONE; tags++)
+    {
+    	kprintf("0x%08lX 0x%p\n", tags->ti_Tag, tags->ti_Data);
+
+    	switch (tags->ti_Tag)
+    	{
+    	case KRN_VBEModeInfo:
+	    vbemode = (struct vbe_mode *)tags->ti_Data;
+	    break;
+	}
+    }
+
+    if (vbemode)
+    {
+    	kprintf("VBE mode structure at 0x%p\n", vbemode);
+    	kprintf("Mode : %dx%dx%d\n", vbemode->x_resolution, vbemode->y_resolution, vbemode->bits_per_pixel);
+    	kprintf("Base : 0x%08X\n", vbemode->phys_base);
+    	kprintf("Pitch: %u\n", vbemode->bytes_per_scanline);
+    	kprintf("Flags: 0x%08X\n", vbemode->mode_attributes);
+    }
+
+    for (;;);
+}
+
+static int kputc(int c, void *data)
+{
+    fb_Putc(c);
+    return 1;
+}
+
+int kprintf(const char *format, ...)
+{
+    va_list ap;
+    int res;
+
+    va_start(ap, format);
+    res = __vcformat(NULL, kputc, format, ap);
+    va_end(ap);
+
+    return res;
+}
