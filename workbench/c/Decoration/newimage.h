d09aea99d379d7db694ef7b99e48d761f27f7003
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-03-28 21:23:12 +0000

    Always create deep copy of NewImage when re-creating on new screen
    git-svn-id: https://svn.aros.org/svn/aros/trunk@37876 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/Decoration/newimage.h
+++ workbench/c/Decoration/newimage.h
@@ -15,6 +15,7 @@ struct NewImage
     ULONG  *data; /* Buffer of RAW ARGB data */
     UWORD   w;
     UWORD   h;
+    BOOL    ok;
 
     ULONG   subimagescols;  /* Number of columns of subimages in image, ie. states of gadget*/
     ULONG   subimagesrows;  /* Number of rows of subimages in image, ie. states of gadget*/
@@ -23,10 +24,10 @@ struct NewImage
     BOOL    istiled;
     UWORD   tile_left, tile_top, tile_bottom, tile_right;
     UWORD   inner_left, inner_top, inner_bottom, inner_right;
-    APTR    mask;
-    Object  *o;
-    struct  BitMap  *bitmap;
-    BOOL    ok;
+
+    Object  *o;                 /* Stored DT object */
+    APTR    mask;               /* Mask pointer from DT object */
+    struct  BitMap  *bitmap;    /* Bitmap pointer from DT object */
     STRPTR  filename;
 };
 
@@ -42,8 +43,7 @@ void DisposeImageContainer(struct NewImage *ni);
 
 struct NewImage *GetImageFromFile(STRPTR path, STRPTR name,
     ULONG expectedsubimagescols, ULONG expectedsubimagesrows);
-void RemoveLUTImage(struct NewImage *ni);
-void SetImage(struct NewImage *in, struct NewImage *out, BOOL truecolor, struct Screen* scr);
+struct NewImage * CreateNewImageContainerMatchingScreen(struct NewImage *in, BOOL truecolor, struct Screen* scr);
 
 struct NewLUT8Image *NewLUT8ImageContainer(UWORD w, UWORD h);
 void DisposeLUT8ImageContainer(struct NewLUT8Image *ni);
