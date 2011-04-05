fb33bc408954056cb755871c9a5e551d8ce6f424
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-04-05 18:26:37 +0000

    You have not seen that directory. ignore it...
    small bits and pieces from my experimental branch. nothing usable anyway.
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38074 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/i386-pc/kernel/kernel_intern.h
@@ -0,0 +1,14 @@
+/*
+    Copyright © 1995-2010, The AROS Development Team. All rights reserved.
+    $Id:$
+
+    Desc: kernel_intern.h
+    Lang: english
+*/
+
+#ifndef KERNEL_INTERN_H_
+#define KERNEL_INTERN_H_
+
+#define kerncall __attribute__((regparm(3)))
+
+#endif /* KERNEL_INTERN_H_ */
