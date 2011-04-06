cc58dafe0b44ef6da6414174bc6e6bd626fffb22
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-04-06 09:36:53 +0000

    Added template files
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38086 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/all-hosted/devs/hostdisk/hostdisk_host.c
@@ -0,0 +1,47 @@
+/*
+ * This file is just a dummy nonfunctional template. These functions
+ * need host-specific implementations.
+ */
+
+#include <devices/trackdisk.h>
+#include <exec/errors.h>
+
+#include "hostdisk_host.h"
+#include "hostdisk_device.h"
+
+ULONG Host_Open(struct unit *Unit)
+{
+    return IOERR_NOCMD;
+}
+
+void Host_Close(struct unit *Unit)
+{
+
+}
+
+LONG Host_Read(struct unit *Unit, APTR buf, ULONG size, ULONG *ioerr)
+{
+    *ioerr = IOERR_NOCMD;
+    return -1;
+}
+
+LONG Host_Write(struct unit *Unit, APTR buf, ULONG size, ULONG *ioerr)
+{
+    *ioerr = IOERR_NOCMD;
+    return -1;
+}
+
+ULONG Host_Seek(struct unit *Unit, ULONG pos)
+{
+    return IOERR_NOCMD;
+}
+
+ULONG Host_Seek64(struct unit *Unit, ULONG pos, ULONG pos_hi)
+{
+    return IOERR_NOCMD;
+}
+
+ULONG Host_GetGeometry(struct unit *Unit, struct DriveGeometry *dg)
+{
+    return IOERR_NOCMD;
+}
---
+++ arch/all-hosted/devs/hostdisk/hostdisk_host.h
@@ -0,0 +1,9 @@
+/*
+ * This file is just a dummy nonfunctional template.
+ * It should contain host-specifc definitions.
+ */
+
+#warning hostdisk.device functionality is not implemented on this platform
+
+#define file_t void *
+#define INVALID_HANDLE_VALUE NULL
