871a79fb657e847667aa154c9add9e6b34b47cb8
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-04-06 17:59:54 +0000

    Also include original copyright year.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38102 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/system/Wanderer/Tools/WBNewDrawer/main.c
+++ workbench/system/Wanderer/Tools/WBNewDrawer/main.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 2011, The AROS Development Team. All rights reserved.
+    Copyright © 2006-2011, The AROS Development Team. All rights reserved.
     $Id$
 */
 
@@ -25,7 +25,7 @@
 #include <stdio.h>
 #include <string.h>
 
-char versionstring[] = "$VER: WBNewDrawer 0.7 (06.04.2011) ©2011 AROS Dev Team";
+char versionstring[] = "$VER: WBNewDrawer 0.7 (6.4.2011) ©2006-2011 AROS Dev Team";
 
 static STRPTR AllocateNameFromLock(BPTR lock);
 static void bt_ok_hook_function(void);
