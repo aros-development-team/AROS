21acf4aa927adff75204e3d25f1c976a25626141
Sami <Sami@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-04-09 11:47:29 +0000

    Call nReadPacket from urndis_decap and not vice versa.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38133 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/usb/classes/rndis/rndis.class.h
+++ rom/usb/classes/rndis/rndis.class.h
@@ -42,19 +42,18 @@ void nDoEvent(struct NepClassEth *ncp, ULONG events);
 BOOL nWritePacket(struct NepClassEth *ncp, struct IOSana2Req *ioreq);
 BOOL nReadPacket(struct NepClassEth *ncp, UBYTE *pktptr, ULONG len);
 
-BOOL nLoadClassConfig(struct NepEthBase *nh);
-BOOL nLoadBindingConfig(struct NepClassEth *ncp);
-LONG nOpenBindingCfgWindow(struct NepEthBase *nh, struct NepClassEth *ncp);
-
-void nGUITaskCleanup(struct NepClassEth *nh);
+//BOOL nLoadClassConfig(struct NepEthBase *nh);
+//BOOL nLoadBindingConfig(struct NepClassEth *ncp);
+//LONG nOpenBindingCfgWindow(struct NepEthBase *nh, struct NepClassEth *ncp);
+//void nGUITaskCleanup(struct NepClassEth *nh);
 
 uint32_t urndis_ctrl_init(struct NepClassEth *ncp);
 uint32_t urndis_ctrl_handle(struct NepClassEth *ncp, struct urndis_comp_hdr *hdr,void **buf, size_t *bufsz);
 void urndis_attach(struct NepClassEth *ncp);
 long urndis_encap(struct NepClassEth *ncp, BYTE *m,LONG len );
-void urndis_decap(struct NepClassEth *ncp, BYTE **buf, LONG *len);
+void urndis_decap(struct NepClassEth *ncp, const UBYTE *buf, const LONG datalen);
 
 AROS_UFP0(void, nEthTask);
-AROS_UFP0(void, nGUITask);
+//AROS_UFP0(void, nGUITask);
 
 #endif /* RNDIS_CLASS_H */
