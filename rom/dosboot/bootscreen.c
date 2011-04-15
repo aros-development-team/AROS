a58144de41e0ad3d9ae961e63ebb7cfb8c9e2216
twilen <twilen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-04-15 19:25:39 +0000

    Show boot screen image (select 4 planes) if AGA or better chipset. OCS/ECS still uses 2 planes because OCS/ECS hires is very slow.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38224 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dosboot/bootscreen.c
+++ rom/dosboot/bootscreen.c
@@ -1,3 +1,4 @@
+#include <aros/debug.h>
 #include <exec/alerts.h>
 #include <exec/libraries.h>
 #include <graphics/gfxbase.h>
@@ -11,33 +12,42 @@
 
 struct Screen *OpenBootScreen(struct DOSBootBase *DOSBootBase)
 {   
-    UWORD height;
+    UWORD height, depth;
     ULONG mode;
 
-    GfxBase = (void *)OpenLibrary("graphics.library", 36);
-    IntuitionBase = (void *)OpenLibrary("intuition.library", 36);
+    GfxBase = (void *)TaggedOpenLibrary(TAGGEDOPEN_GRAPHICS);
+    IntuitionBase = (void *)TaggedOpenLibrary(TAGGEDOPEN_INTUITION);
 
     if ((!IntuitionBase) || (!GfxBase))
 	/* We failed to open one of system libraries. AROS is in utterly broken state */
 	Alert(AT_DeadEnd|AN_BootStrap|AG_OpenLib);
 
+    height = 480;
+    mode = BestModeID(BIDTAG_DesiredWidth, 640, BIDTAG_DesiredHeight, height,
+			    BIDTAG_Depth, 8, TAG_DONE);
+    if (mode != INVALID_ID) {
+	/* if we got depth=8 mode, we must have fast enough hardware for 4 planes too,
+	 * either it is non-Amiga(tm) hardware or AGA chipset */
+	depth = 4;
+    } else {
+	/* we probably have OCS or ECS chipset, select 2 planes because 4 planes OCS/ECS hires is very slow */
+	depth = 2;
+    }
+    /* set PAL or NTSC default height if we are running on Amiga(tm) hardware
+     * we are using interlaced screen height because boot screen assumes 1:1 pixels */
     if (GfxBase->DisplayFlags & NTSC)
-    	height = 200;
+        height = 200 * 2;
     else if (GfxBase->DisplayFlags & PAL)
-    	height = 256;
-    else
-    	/* Neither PAL nor NTSC - non-Amiga(tm) hardware assumed */
-    	height = 480;
+        height = 256 * 2;
 
     /* We want the screen to occupy the whole display, so we find best maching
        mode ID and then open a screen with that mode */
     mode = BestModeID(BIDTAG_DesiredWidth, 640, BIDTAG_DesiredHeight, height,
-			    BIDTAG_Depth, 4, TAG_DONE);
-
+			    BIDTAG_Depth, depth, TAG_DONE);
     if (mode != INVALID_ID)
     {
 	struct Screen *scr = OpenScreenTags(NULL, SA_DisplayID, mode, SA_Draggable, FALSE, 
-					    SA_Quiet, TRUE, TAG_DONE);
+					    SA_Quiet, TRUE, SA_Depth, depth, TAG_DONE);
 
 	if (scr)
 	    return scr;
