6e58712e07065966585babe3a6d87e7319dffcd6
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-04-15 07:17:27 +0000

    - Updated documentation and moved into code files - Some more optimizations and code reuse
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38217 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/oop/basemetaclass.h
@@ -0,0 +1,3 @@
+IPTR basemeta_dosupermethod(OOP_Class *cl, OOP_Object *o, OOP_Msg msg);
+IPTR basemeta_coercemethod(OOP_Class *cl, OOP_Object *o, OOP_Msg msg);
+IPTR basemeta_domethod(OOP_Object *o, OOP_Msg msg);
