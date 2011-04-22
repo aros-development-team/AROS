a8be15ebaa523a5c5e33a14b10173e0408b36dad
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-04-22 09:35:38 +0000

    - Removed obsolete unneeded timer HIDD include - Added more comments and one more useful macro
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38308 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/timer/timer_intern.h
+++ rom/timer/timer_intern.h
@@ -30,30 +30,31 @@
 struct TimerBase
 {
     /* Required by the system */
-    struct Device	 tb_Device;
+    struct Device	 tb_Device;		/* Our base						*/
+    APTR		 tb_KernelBase;		/* kernel.resource base					*/
 
-    APTR		 tb_KernelBase;		/* kernel.resource base */
-    struct timeval	 tb_CurrentTime;	/* system time */
-    struct timeval	 tb_Elapsed;		/* Elapsed Time for VBlank */
+    /* Time counters */
+    struct timeval	 tb_CurrentTime;	/* Absolute system time. Can be set by TR_SETSYSTIME.	*/
+    struct timeval	 tb_Elapsed;		/* Relative system time. Used for measuring intervals.	*/
 
-    /* This is required for the vertical blanking stuff */
-    LONG		 tb_TimerIRQNum;	/* Timer IRQ number */
-    APTR		 tb_TimerIRQHandle;	/* Timer IRQ handle */
-    struct Interrupt	 tb_VBlankInt;		/* Used by older implementations, needs to be removed */
-    struct timeval	 tb_VBlankTime;		/* Periodic timer interval */
+    /* This is required for hardware-specific code */
+    LONG		 tb_TimerIRQNum;	/* Timer IRQ number					*/
+    APTR		 tb_TimerIRQHandle;	/* Timer IRQ handle					*/
+    struct Interrupt	 tb_VBlankInt;		/* Used by older implementations, needs to be removed	*/
+    struct timeval	 tb_VBlankTime;		/* Software-emulated periodic timer interval		*/
 
     /* Request queues */
     struct MinList	 tb_Lists[NUM_LISTS];
 
     /* EClock counter */
-    UQUAD                tb_ticks_total;	/* Effective EClock value */
-    ULONG                tb_ticks_sec;
-    ULONG                tb_ticks_elapsed;
-    ULONG                tb_prev_tick;
-    ULONG		 tb_eclock_rate;	/* EClock frequency */
+    UQUAD                tb_ticks_total;	/* Effective EClock value				*/
+    ULONG                tb_ticks_sec;		/* Fraction of second for CurrentTime in ticks		*/
+    ULONG                tb_ticks_elapsed;	/* Fraction of second for Elapsed in ticks		*/
+    ULONG                tb_prev_tick;		/* Hardware-specific					*/
+    ULONG		 tb_eclock_rate;	/* EClock frequency					*/
 
 #ifdef USE_VBLANK_EMU
-    struct timerequest   tb_vblank_timerequest; /* For vblank emulation */
+    struct timerequest   tb_vblank_timerequest; /* VBlank emulation request				*/
 #endif
 };
 
