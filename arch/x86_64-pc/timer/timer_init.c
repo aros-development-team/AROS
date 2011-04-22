b3927a6f5043ea321da3c72cc0390feee1495078
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-04-22 10:23:45 +0000

    Code merge
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38311 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/x86_64-pc/timer/timer_init.c
+++ arch/x86_64-pc/timer/timer_init.c
@@ -7,127 +7,95 @@
 
 /****************************************************************************************/
 
-#include <exec/types.h>
-#include <exec/io.h>
-#include <exec/errors.h>
 #include <exec/devices.h>
-#include <exec/alerts.h>
-#include <exec/initializers.h>
 #include <devices/timer.h>
-#include <hardware/intbits.h>
-
 #include <proto/exec.h>
-#include <proto/timer.h>
 #include <proto/kernel.h>
-
 #include <aros/symbolsets.h>
-
 #include <asm/io.h>
-
 #include <aros/debug.h>
 
-#include LC_LIBDEFS_FILE
+#include "ticks.h"
 
-void VBlankInt(struct TimerBase *TimerBase, struct ExecBase *SysBase);
+#define KernelBase LIBBASE->tb_KernelBase
 
 /****************************************************************************************/
 
-static int GM_UNIQUENAME(Init)(LIBBASETYPEPTR LIBBASE)
+static void TimerInt(struct TimerBase *TimerBase, struct ExecBase *SysBase)
 {
-    APTR KernelBase = OpenResource("kernel.resource");
+    /* Sync up with the hardware */
+    EClockUpdate(TimerBase);
+    /*
+     * Process MICROHZ requests.
+     * VBLANK is emulated by us, so we don't check it here. We always
+     * have one active request in this list, which will cause VBLANK checking.
+     */
+    handleMicroHZ(TimerBase, SysBase);
+    /* Request next interrupt from the hardware */
+    Timer0Setup(TimerBase);
+}
 
+/****************************************************************************************/
+
+static int hw_Init(struct TimerBase *LIBBASE)
+{
     /* We must have kernel.resource */
+    D(bug("[Timer] KernelBase = 0x%p\n", KernelBase));
     if (!KernelBase)
     	return FALSE;
 
-    /* Setup the timer.device data */
-    LIBBASE->tb_CurrentTime.tv_secs = 0;
-    LIBBASE->tb_CurrentTime.tv_micro = 0;
-    LIBBASE->tb_VBlankTime.tv_secs = 0;
-    LIBBASE->tb_VBlankTime.tv_micro = 1000000 / (SysBase->VBlankFrequency * SysBase->PowerSupplyFrequency);
-    LIBBASE->tb_Elapsed.tv_secs = 0;
-    LIBBASE->tb_Elapsed.tv_micro = 0;
+    /* Start up the interrupt server. We know that our HW timer is at IRQ 0 */
+    LIBBASE->tb_TimerIRQHandle = KrnAddIRQHandler(0, TimerInt, LIBBASE, SysBase);
+    D(bug("[Timer] IRQ handle = 0x%p\n", LIBBASE->tb_TimerIRQHandle));
+    if (!LIBBASE->tb_TimerIRQHandle)
+    	return FALSE;
 
-    D(kprintf("Timer period: %ld secs, %ld micros\n",
-	LIBBASE->tb_VBlankTime.tv_secs, LIBBASE->tb_VBlankTime.tv_micro));
-    
-    /* Initialise the lists */
-    NEWLIST( &LIBBASE->tb_Lists[0] );
-    NEWLIST( &LIBBASE->tb_Lists[1] );
-    NEWLIST( &LIBBASE->tb_Lists[2] );
-    NEWLIST( &LIBBASE->tb_Lists[3] );
-    NEWLIST( &LIBBASE->tb_Lists[4] );
-    
+    /* Shut off kernel's VBlank emulation */
+    KrnSetSystemAttr(KATTR_VBlankEnable, FALSE);
+
+    D(bug("[Timer] Initializing hardware...\n"));
+
+    /* We have fixed EClock rate */
+    SysBase->ex_EClockFrequency = 1193180;
+    LIBBASE->tb_eclock_rate     = 1193180;
+    LIBBASE->tb_prev_tick       = 0xffff;
+
     /* Start the timer2 */
     outb((inb(0x61) & 0xfd) | 1, 0x61); /* Enable the timer (set GATE on) */
-    outb(0xb4, 0x43);   /* Binary mode on Timer2, count mode 2 */
-    outb(0x00, 0x42);   /* We're counting whole range */
+    outb(0xb4, 0x43);			/* Binary mode on Timer2, count mode 2 */
+    outb(0x00, 0x42);			/* We're counting whole range */
     outb(0x00, 0x42);
-    
-    LIBBASE->tb_prev_tick = 0xffff;
 
-    /* Start up the interrupt server */
-    LIBBASE->tb_TimerIRQHandle = KrnAddIRQHandler(0, VBlankInt, LIBBASE, SysBase);
+    /* Own VBlank EMU */
+    D(bug("[Timer] Starting VBlank emulation (%u Hz)...\n", SysBase->VBlankFrequency));
 
-    /* VBlank EMU */
-    
     LIBBASE->tb_vblank_timerequest.tr_node.io_Command = TR_ADDREQUEST;
-    LIBBASE->tb_vblank_timerequest.tr_node.io_Device = (struct Device *)TimerBase;        
-    LIBBASE->tb_vblank_timerequest.tr_node.io_Unit = (struct Unit *)UNIT_MICROHZ;    
-    LIBBASE->tb_vblank_timerequest.tr_time.tv_secs = 0;
-    LIBBASE->tb_vblank_timerequest.tr_time.tv_micro = 1000000 / SysBase->VBlankFrequency;
-    
+    LIBBASE->tb_vblank_timerequest.tr_node.io_Device = &LIBBASE->tb_Device;
+    LIBBASE->tb_vblank_timerequest.tr_node.io_Unit   = (struct Unit *)UNIT_MICROHZ;    
+    LIBBASE->tb_vblank_timerequest.tr_time.tv_secs   = 0;
+    LIBBASE->tb_vblank_timerequest.tr_time.tv_micro  = 1000000 / SysBase->VBlankFrequency;
+
     SendIO(&LIBBASE->tb_vblank_timerequest.tr_node);
-    
+
+    D(bug("[Timer] Done\n"));
+
     return TRUE;
 }
 
 /****************************************************************************************/
 
-static int GM_UNIQUENAME(Open)
-(
-    LIBBASETYPEPTR LIBBASE,
-    struct timerequest *tr,
-    ULONG unitNum,
-    ULONG flags
-)
+/* This is executed after common code (we have priority = 10) */
+static int hw_Open(struct TimerBase *LIBBASE, struct timerequest *tr, ULONG unitNum, ULONG flags)
 {
     outb((inb(0x61) & 0xfd) | 1, 0x61); /* Enable the timer (set GATE on) */
 
-    /*
-        Normally, we should check the length of the message and other
-        such things, however the RKM documents an example where the
-        length of the timerrequest isn't set, so we must not check
-        this.
-
-        This fixes bug SF# 741580
-    */
-
-    switch(unitNum)
-    {
-	case UNIT_VBLANK:
-	case UNIT_WAITUNTIL:
-    case UNIT_MICROHZ:
-    case UNIT_ECLOCK:
-    case UNIT_WAITECLOCK:   
-	    tr->tr_node.io_Error = 0;
-	    tr->tr_node.io_Unit = (struct Unit *)unitNum;
-	    tr->tr_node.io_Device = (struct Device *)LIBBASE;
-	    break;
-
-	default:
-	    tr->tr_node.io_Error = IOERR_OPENFAIL;
-    }
-
     return TRUE;
 }
 
 /****************************************************************************************/
 
-static int GM_UNIQUENAME(Expunge)(LIBBASETYPEPTR LIBBASE)
+static int hw_Expunge(struct TimerBase *LIBBASE)
 {
-    APTR KernelBase = LIBBASE->tb_KernelBase;
-
     outb((inb(0x61) & 0xfd) | 1, 0x61); /* Enable the timer (set GATE on) */
     KrnRemIRQHandler(LIBBASE->tb_TimerIRQHandle);
     return TRUE;
@@ -135,6 +103,6 @@ static int GM_UNIQUENAME(Expunge)(LIBBASETYPEPTR LIBBASE)
 
 /****************************************************************************************/
 
-ADD2INITLIB(GM_UNIQUENAME(Init), 0)
-ADD2OPENDEV(GM_UNIQUENAME(Open), 0)
-ADD2EXPUNGELIB(GM_UNIQUENAME(Expunge), 0)
+ADD2INITLIB(hw_Init, 0)
+ADD2OPENDEV(hw_Open, 10)
+ADD2EXPUNGELIB(hw_Expunge, 0)
