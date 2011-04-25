4f0127626ebc6a3123076958aa2680363ecd22c6
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-04-25 12:00:05 +0000

    Added two new functions for debug I/O management
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38354 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/kernel/obtaininput.c
@@ -0,0 +1,51 @@
+#include <aros/kernel.h>
+#include <aros/libcall.h>
+
+#include <kernel_base.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/kernel.h>
+
+AROS_LH0I(int, KrnObtainInput,
+
+/*  SYNOPSIS */
+
+/*  LOCATION */
+	struct KernelBase *, KernelBase, 33, Kernel)
+
+/*  FUNCTION
+	Take over low-level debug input hardware and initialize the input
+
+    INPUTS
+	None
+
+    RESULT
+	Nonzero for success, zero for failure (for example there's no input channel)
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+******************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    /*
+     * The implementation of this function is entirely architecture-specific (at least for now).
+     * In fact this is a placeholder for an advanced low-level I/O subsystem, which will allow
+     * various hardware drivers (like ethernet) to provide their own debug I/O channels.
+     * This will allow to perform remote debugging and read boot logs on machines without serial
+     * ports, for example over Ethernet/USB/whatever.
+     */
+    return 0;
+
+    AROS_LIBFUNC_EXIT
+}
