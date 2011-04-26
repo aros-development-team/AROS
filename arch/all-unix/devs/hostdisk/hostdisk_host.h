1004fb4c5b96c4fdc36d999235009cdd06b57823
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-04-26 09:16:22 +0000

    UNIX version of hostdisk.device in progress
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38371 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/all-unix/devs/hostdisk/hostdisk_host.h
@@ -0,0 +1,65 @@
+/* avoid conflicts between our __unused define and the ones that might come in
+   via sys/stat.h */
+#undef __unused
+
+#ifdef HOST_LONG_ALIGNED
+#pragma pack(4)
+#endif
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#pragma pack()
+
+typedef int file_t;
+
+#define INVALID_HANDLE_VALUE -1
+
+/* Android is not a true Linux ;-) */
+#ifdef HOST_OS_android
+#undef HOST_OS_linux
+#endif
+
+#ifdef HOST_OS_linux
+#define LIBC_NAME "libc.so.6"
+#else
+#endif
+
+#ifdef HOST_OS_darwin
+#define LIBC_NAME "libSystem.dylib"
+#endif
+
+#ifndef LIBC_NAME
+#define LIBC_NAME "libc.so"
+#endif
+
+struct HostInterface
+{
+    int		   (*open)(char *path, int oflag, ...);
+    int		   (*close)(int filedes);
+    ssize_t        (*read)(int fildes, void *buf, size_t nbyte);
+    ssize_t	   (*write)(int fildes, const void *buf, size_t nbyte);
+#ifdef HOST_LONG_ALIGNED
+    off_t	   (*lseek)(int fildes, unsigned long offset_l, unsigned long offset_h, int whence);
+#else
+    off_t          (*lseek)(int fildes, off_t offset, int whence);
+#endif
+    int		  *(*__error)(void);
+#ifdef HOST_OS_linux
+    int		   (*__fxstat)(int ver, int fd, struct stat *buf);
+    #define fstat(fd, buf)  __fxstat(_STAT_VER, fd, buf)
+#else
+    int		   (*fstat)(int fd, struct stat *buf);
+#endif
+};
+
+#ifdef HOST_LONG_ALIGNED
+/*
+ * Somewhat dirty hack to adjust data packing to iOS ARM ABI.
+ * Perhaps this can be done in a cleaner and more CPU-abstract way.
+ * FIXME: Always assuming little-endian CPU
+ */
+#define LSeek(fildes, offset, offset_high, whence) hdskBase->iface->lseek(fildes, offset, offset_high, whence)
+#else
+#define LSeek(fildes, offset, offset_high, whence) hdskBase->iface->lseek(fildes, (UQUAD)offset | (UQUAD)offset_high << 32, whence)
+#endif
