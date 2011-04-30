8571469719553ae10361b0999054fd6f4d647012
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-04-30 11:09:00 +0000

    Check if th requested format is supported by nouveau bitmap
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38453 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/hidds/hidd.nouveau/nouveaubitmapclass.c
+++ workbench/hidds/hidd.nouveau/nouveaubitmapclass.c
@@ -710,49 +710,61 @@ void HACK_PATCHRGBCONV(OOP_Object * bitmap);
 /* PUBLIC METHODS */
 OOP_Object * METHOD(NouveauBitMap, Root, New)
 {
+    IPTR width, height, depth, displayable, bytesperpixel;
+    OOP_Object * pf;
+    struct HIDDNouveauBitMapData * bmdata = NULL;
+    HIDDT_StdPixFmt stdfmt = vHidd_StdPixFmt_Unknown;
+
     o = (OOP_Object *)OOP_DoSuperMethod(cl, o, (OOP_Msg)msg);
 
-    if (o)
-    {
-        IPTR width, height, depth, displayable;
-        OOP_Object * pf;
-        struct HIDDNouveauBitMapData * bmdata = OOP_INST_DATA(cl, o);
-        
-        OOP_GetAttr(o, aHidd_BitMap_Width,  &width);
-        OOP_GetAttr(o, aHidd_BitMap_Height, &height);
-        OOP_GetAttr(o, aHidd_BitMap_PixFmt, (APTR)&pf);
-        OOP_GetAttr(pf, aHidd_PixFmt_Depth, &depth);
-        OOP_GetAttr(o, aHidd_BitMap_Displayable, &displayable);
-        
-        /* Initialize properties */
-        bmdata->width = width;
-        bmdata->height = height;
-        bmdata->depth = depth;
-        if (depth <= 8)
-            bmdata->bytesperpixel = 1;
-        else if (depth <= 16)
-            bmdata->bytesperpixel = 2;
-        else
-            bmdata->bytesperpixel = 4;
-        bmdata->pitch = (bmdata->width + 63) & ~63;
-        bmdata->pitch *= bmdata->bytesperpixel;
-        if (displayable) bmdata->displayable = TRUE; else bmdata->displayable = FALSE;
-        InitSemaphore(&bmdata->semaphore);
+    if (!o)
+        goto exit_fail;
 
-        /* Display information */
-        bmdata->fbid = 0; /* Default value */
-        bmdata->xoffset = 0;
-        bmdata->yoffset = 0;
+    bmdata = OOP_INST_DATA(cl, o);
 
-        /* Creation of buffer object */
-        /* FIXME: check result of call */
-        nouveau_bo_new(SD(cl)->carddata.dev, NOUVEAU_BO_VRAM | NOUVEAU_BO_MAP, 0, 
-                bmdata->pitch * bmdata->height,
-                &bmdata->bo);
+    /* Initialize default values */
+    bmdata->fbid = 0;
+    bmdata->xoffset = 0;
+    bmdata->yoffset = 0;
+    
+    OOP_GetAttr(o, aHidd_BitMap_Width,  &width);
+    OOP_GetAttr(o, aHidd_BitMap_Height, &height);
+    OOP_GetAttr(o, aHidd_BitMap_PixFmt, (APTR)&pf);
+    OOP_GetAttr(o, aHidd_BitMap_Displayable, &displayable);
+    OOP_GetAttr(pf, aHidd_PixFmt_StdPixFmt, &stdfmt);
+    OOP_GetAttr(pf, aHidd_PixFmt_BytesPerPixel, &bytesperpixel);
+    OOP_GetAttr(pf, aHidd_PixFmt_Depth, &depth);
 
-        bmdata->compositing = (OOP_Object *)GetTagData(aHidd_BitMap_Nouveau_CompositingHidd, 0, msg->attrList);
-        /* FIXME: check if compositing hidd was passed */
-    }
+    /* Check if requested format is one of the supported ones */
+    if ((stdfmt != vHidd_StdPixFmt_BGR032) && (stdfmt != vHidd_StdPixFmt_RGB16_LE))
+        goto exit_fail;
+
+    /* Check if requested depth is a supported one */
+    if (depth < 16)
+        goto exit_fail;
+    
+    /* Check if requested byted per pixel is a supported one */
+    if ((bytesperpixel != 2) && (bytesperpixel != 4))
+        goto exit_fail;
+
+    /* Initialize properties */
+    bmdata->width = width;
+    bmdata->height = height;
+    bmdata->depth = depth;
+    bmdata->bytesperpixel = bytesperpixel;
+    bmdata->pitch = (bmdata->width + 63) & ~63;
+    bmdata->pitch *= bmdata->bytesperpixel;
+    if (displayable) bmdata->displayable = TRUE; else bmdata->displayable = FALSE;
+    InitSemaphore(&bmdata->semaphore);
+
+    /* Creation of buffer object */
+    /* FIXME: check result of call */
+    nouveau_bo_new(SD(cl)->carddata.dev, NOUVEAU_BO_VRAM | NOUVEAU_BO_MAP, 0, 
+            bmdata->pitch * bmdata->height,
+            &bmdata->bo);
+
+    bmdata->compositing = (OOP_Object *)GetTagData(aHidd_BitMap_Nouveau_CompositingHidd, 0, msg->attrList);
+    /* FIXME: check if compositing hidd was passed */
     
     /* TEMP - FIXME HACK FOR PATCHRGBCONV */
     /* Yes, it can be called more than once */
@@ -765,6 +777,15 @@ OOP_Object * METHOD(NouveauBitMap, Root, New)
     
     
     return o;
+
+exit_fail:
+
+    if (o)
+    {
+        /* TODO: dispose bitmap object */
+    }
+
+    return NULL;
 }
 
 VOID NouveauBitMap__Root__Dispose(OOP_Class *cl, OOP_Object *o, OOP_Msg msg)
