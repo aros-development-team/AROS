e19bbeed9182146fad1d825a92bb4c1cab93ea83
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-05-06 20:56:17 +0000

    tiny changes in mmakefile
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38585 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-efika/boot/boot.c
+++ arch/arm-efika/boot/boot.c
@@ -6,3 +6,23 @@
     Lang: english
 */
 
+#include <inttypes.h>
+#include <asm/arm/cpu.h>
+#include "boot.h"
+
+__startup void bootstrap(uintptr_t a0, uintptr_t a1, uintptr_t a2)
+{
+	uint32_t tmp;
+
+	/* Enable NEON and VFP */
+    asm volatile ("mrc p15, 0, %0, c1, c0, 2":"=r"(tmp));
+    tmp |= 3 << 20;
+    tmp |= 3 << 22;
+    asm volatile ("mcr p15, 0, %0, c1, c0, 2"::"r"(tmp));
+
+    fmxr(cr8, fmrx(cr8) | 1 << 30);
+
+
+}
+
+
