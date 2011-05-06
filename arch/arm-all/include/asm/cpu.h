bad56a5f2a8d893e52c61b4168bb12176f351bc6
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-05-06 20:55:54 +0000

    asm include directory for ARM cpus
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38584 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/arm-all/include/asm/cpu.h
@@ -0,0 +1,26 @@
+/*
+    Copyright © 1995-2010, The AROS Development Team. All rights reserved.
+    $Id:$
+
+    Desc: cpu.h
+    Lang: english
+*/
+
+#ifndef ASM_ARM_CPU_H
+#define ASM_ARM_CPU_H
+
+#define vfpreg(_vfp_) #_vfp_
+
+#define fmrx(_vfp_) ({                  \
+        unsigned long __v;                        \
+        asm("mrc p10, 7, %0, " vfpreg(_vfp_) ", cr0, 0 @ fmrx   %0, " #_vfp_    \
+            : "=r" (__v) : : "cc");     \
+        __v;                            \
+ })
+
+#define fmxr(_vfp_,_var_)               \
+        asm("mcr p10, 7, %0, " vfpreg(_vfp_) ", cr0, 0 @ fmxr   " #_vfp_ ", %0" \
+           : : "r" (_var_) : "cc")
+
+
+#endif /* ASM_ARM_CPU_H */
---
+++ arch/arm-all/include/asm/mx51.h
@@ -0,0 +1,13 @@
+/*
+    Copyright © 1995-2010, The AROS Development Team. All rights reserved.
+    $Id:$
+
+    Desc: mx51.h
+    Lang: english
+*/
+
+#ifndef ASM_ARM_MX51_H
+#define ASM_ARM_MX51_H
+
+
+#endif /* ASM_ARM_MX51_H */
