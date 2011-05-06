80a9225f6af9abfe7340ff4139a26c68c15bca1b
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-05-06 06:11:29 +0000

    Added Inhibit() test
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38562 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/dos/inhibit.c
@@ -0,0 +1,24 @@
+#include <dos/dos.h>
+#include <proto/dos.h>
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+    LONG ret;
+    LONG arg;
+
+    if (argc < 3)
+    {
+    	printf("Usage: %s <device> [ON|OFF]\n", argv[0]);
+    	return 0;
+    }
+
+    arg = !stricmp(argv[2], "ON");
+    ret = Inhibit(argv[1], arg);
+
+    printf("Inhibit(%d) returned %d\n", arg, ret);
+
+    return 0;
+}
