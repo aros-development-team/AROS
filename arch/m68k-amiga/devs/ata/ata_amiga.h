01d8ed6063c9e0fe953b42e58f531d5bd7a4e7ba
twilen <twilen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-05-12 17:42:04 +0000

    A4000 IDE interrupt register is WORD sized, IDE data port is at 0xdd2020 but all following registers have extra 2 byte offset. (Emulation had accidental register aliasing which masked the problem)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38670 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/m68k-amiga/devs/ata/ata_amiga.h
+++ arch/m68k-amiga/devs/ata/ata_amiga.h
@@ -3,12 +3,12 @@
 
 #define IDE_GAYLEAMIGA
 
-#define GAYLE_BASE_4000 0xdd2020
-#define GAYLE_IRQ_4000  0x1000
+#define GAYLE_BASE_4000 0xdd2022 /* 0xdd2020.W, 0xdd2026.B, 0xdd202a.B ... (argh!) */
+#define GAYLE_IRQ_4000  0xdd3020
 
-#define GAYLE_BASE_1200 0xda0000
-#define GAYLE_IRQ_1200  0x9000
-#define GAYLE_INT_1200  0xA000
+#define GAYLE_BASE_1200 0xda0000 /* 0xda0000.W, 0xda0004.B, 0xda0008.B ... */
+#define GAYLE_IRQ_1200  0xda9000
+#define GAYLE_INT_1200  0xdaa000
 
 #define GAYLE_IRQ_IDE	0x80
 #define GAYLE_INT_IDE	0x80
