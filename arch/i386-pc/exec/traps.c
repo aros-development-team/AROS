b9abcea85f68ca6b0b89aa8b67894c3cdde27ea4
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-05-13 22:18:49 +0000

    Do not try to get stack pointer from invalid ESP field, which coincidentally is at same address as pre-trap stack. Reverts r34376.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38677 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-pc/exec/traps.c
+++ arch/i386-pc/exec/traps.c
@@ -99,7 +99,7 @@ void set_system_gate(unsigned int n, void *addr)
 void printException(struct pt_regs regs)
 {
     kprintf("*** trap: eip = %x eflags = %x  ds = %x sp ~= %x\n",
-        regs.eip, regs.eflags, regs.xds, regs.esp);
+        regs.eip, regs.eflags, regs.xds, &regs.esp);
 }
 
 void handleException(ULONG exceptionNo)
