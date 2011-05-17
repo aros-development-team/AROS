3f2bfbfb989c6937d109a3dbc2416427aa6e4541
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-05-17 07:15:23 +0000

    Improved comment
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38708 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/kernel/kernel_memory.h
+++ rom/kernel/kernel_memory.h
@@ -13,8 +13,10 @@
 
 /*
  * 'access' parameter in krnAllocMem() is reserved to provide
- * user-mode access flags. When set to '0' the memory is supposed to be
- * accessible only in supervisor mode.
+ * user-mode access flags. In supervisor mode we assume to always
+ * have RW access to the allocated memory. Also, current code assumes
+ * krnAllocMem() will clear the memory (at least KrnCreateContext()
+ * implementations and kernel.resource init code). Perhaps this is not good.
  */
 
 #define krnAllocMem(len, access) AllocMem(len, MEMF_PUBLIC|MEMF_CLEAR)
