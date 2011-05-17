1a98622a9b45294a463c35b1cd2eb5b946285ee8
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-05-17 21:14:43 +0000

    Use amiga pool function for memory handling in libjpeg.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38727 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/libjpeg/jmemamiga.c
@@ -0,0 +1,114 @@
+/*
+ * jmemamiga.c
+ *
+ * Copyright (C) 1992-1996, Thomas G. Lane.
+ * This file is part of the Independent JPEG Group's software.
+ * For conditions of distribution and use, see the accompanying README file.
+ *
+ * Copyright (C) 2009-2011, The AROS development team.
+ *
+ * This file provides a simple implementation of the system-dependent portion
+ * of the JPEG memory manager using the Amiga API.  This implementation
+ * assumes that no backing-store files are needed: all required space
+ * can be obtained from the system.
+ * You'll need to have lots of main memory (or virtual memory) if you want
+ * to process big images.
+ * Note that the max_memory_to_use option is ignored by this implementation.
+ */
+
+#include <proto/exec.h>
+#include <exec/memory.h>
+#include <exec/types.h>
+
+static APTR _mempool;
+
+#define JPEG_INTERNALS
+#include "jinclude.h"
+#include "jpeglib.h"
+#include "jmemsys.h"		/* import the system-dependent declarations */
+
+/*
+ * Memory allocation and freeing are controlled by the regular library
+ * routines malloc() and free().
+ */
+
+GLOBAL(void *)
+jpeg_get_small (j_common_ptr cinfo, size_t sizeofobject)
+{
+    return (void *) AllocPooled(_mempool, sizeofobject);
+}
+
+GLOBAL(void)
+jpeg_free_small (j_common_ptr cinfo, void * object, size_t sizeofobject)
+{
+    FreePooled(_mempool, object, sizeofobject);
+}
+
+
+/*
+ * "Large" objects are treated the same as "small" ones.
+ * NB: although we include FAR keywords in the routine declarations,
+ * this file won't actually work in 80x86 small/medium model; at least,
+ * you probably won't be able to process useful-size images in only 64KB.
+ */
+
+GLOBAL(void FAR *)
+jpeg_get_large (j_common_ptr cinfo, size_t sizeofobject)
+{
+  return (void FAR *) AllocPooled(_mempool, sizeofobject);
+}
+
+GLOBAL(void)
+jpeg_free_large (j_common_ptr cinfo, void FAR * object, size_t sizeofobject)
+{
+    FreePooled(_mempool, object, sizeofobject);
+}
+
+
+/*
+ * This routine computes the total memory space available for allocation.
+ * Here we always say, "we got all you want bud!"
+ */
+
+GLOBAL(long)
+jpeg_mem_available (j_common_ptr cinfo, long min_bytes_needed,
+		    long max_bytes_needed, long already_allocated)
+{
+    long avail = (long)AvailMem(MEMF_LARGEST);
+
+    return max_bytes_needed > avail ? avail : max_bytes_needed;
+}
+
+
+/*
+ * Backing store (temporary file) management.
+ * Since jpeg_mem_available always promised the moon,
+ * this should never be called and we can just error out.
+ */
+
+GLOBAL(void)
+jpeg_open_backing_store (j_common_ptr cinfo, backing_store_ptr info,
+			 long total_bytes_needed)
+{
+  ERREXIT(cinfo, JERR_NO_BACKING_STORE);
+}
+
+
+/*
+ * These routines take care of any system-dependent initialization and
+ * cleanup required.
+ */
+
+GLOBAL(long)
+jpeg_mem_init (j_common_ptr cinfo)
+{
+    _mempool = CreatePool(MEMF_ANY | MEMF_SEM_PROTECTED, 65536L, 4096L);
+         
+    return AvailMem(MEMF_LARGEST);
+}
+
+GLOBAL(void)
+jpeg_mem_term (j_common_ptr cinfo)
+{
+    DeletePool(_mempool);
+}
