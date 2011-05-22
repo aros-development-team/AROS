7b8e88757035b28f6d33505cf875a2b9afc8a11c
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-05-22 15:40:58 +0000

    arch/m68k-amiga: Move debug init to the right place
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38810 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/m68k-all/debug/debug_gdb.c
+++ arch/m68k-all/debug/debug_gdb.c
@@ -8,14 +8,16 @@
 
 #include <aros/symbolsets.h>
 
-#include "kernel_base.h"
-#include "kernel_debug.h"
-#include "kernel_memory.h"
+#include <proto/exec.h>
+
+/* This is needed in order to bring in definition of struct segment */
+#include "debug_intern.h"
 
 #ifdef AROS_MODULES_DEBUG
+
 /* Provided for GdbStub debugging */
+const struct ELF_ModuleInfo *Debug_KickList = NULL;
 struct MinList *Debug_ModList;
-struct ELF_ModuleInfo *Debug_KickList; /* Not unused */
 
 typedef ULONG size_t;
 /* 'malloc' and 'free' are needed for GDB's strcmp(), which is
@@ -27,7 +29,7 @@ void *malloc(ULONG size)
 
     size = (size + sizeof(size_t) - 1) & ~(sizeof(size_t) - 1);
 
-    mem = krnAllocMem(size, -1);
+    mem = AllocMem(size, -1);
     *(mem++) = size;
     return mem;
 }
@@ -37,7 +39,7 @@ void free(void *ptr)
     size_t *mem = ptr;
 
     mem--;
-    krnFreeMem(mem, mem[0]);
+    FreeMem(mem, mem[0]);
 }
 
 /* Since GDB can't patch 'start' in the ROM with 
@@ -45,17 +47,16 @@ void free(void *ptr)
  * in the .bss segment.
  */
 UWORD start;
-
 #endif
 
-static int Kernel_DebugInit(struct KernelBase *KernelBase)
+static int Debug_GdbInit(struct DebugBase *DebugBase)
 {
 #ifdef AROS_MODULES_DEBUG
 	/* Provision for gdbstub debugging */
-	Debug_ModList = &KernelBase->kb_Modules;
+	Debug_ModList = &DebugBase->db_Modules;
 #endif
 
 	return TRUE;
 }
 
-ADD2INITLIB(Kernel_DebugInit, 10)
+ADD2INIT(Debug_GdbInit, 10)
