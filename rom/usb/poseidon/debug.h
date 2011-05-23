fce525fb578f466bee949abc1d6d99b24d2d5eb7
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-05-23 10:25:44 +0000

    - Use KPrintF() for debug output becaise format specifiers use RawDoFmt() semantics ('l' suffix for 32-bit values) - Debug output made 64-bit-friendly
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38825 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/usb/poseidon/debug.h
+++ rom/usb/poseidon/debug.h
@@ -9,7 +9,7 @@
 
 //#define USB3 1
 
-#include <aros/debug.h>
+#include <proto/debug.h>
 
 // DEBUG 0 should equal undefined DEBUG
 #ifdef DEBUG
@@ -20,8 +20,13 @@
 
 #ifdef DEBUG
 #define XPRINTF(l, x) do { if ((l) >= DB_LEVEL) \
-     { bug("%s:%s/%lu: ", __FILE__, __FUNCTION__, __LINE__); bug x;} } while (0)
+     { KPrintF("%s:%s/%lu: ", __FILE__, __FUNCTION__, __LINE__); KPrintF x;} } while (0)
+#if DEBUG > 1
+#define KPRINTF(l, x) do { if ((l) >= DB_LEVEL) \
+     { KPrintF("%s:%s/%lu: ", __FILE__, __FUNCTION__, __LINE__); KPrintF x;} } while (0)
+#else
 #define KPRINTF(l, x)
+#endif
 #define DB(x) x
    void dumpmem(void *mem, unsigned long int len);
 #else /* !DEBUG */
