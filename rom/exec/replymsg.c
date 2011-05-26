6db98b31c23093f4ccd71bcea8d92d86a9d93ac9
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-05-26 09:43:55 +0000

    Optimization, removed duplicated code
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38847 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/replymsg.c
+++ rom/exec/replymsg.c
@@ -1,14 +1,17 @@
 /*
-    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2011, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: Reply a message
     Lang: english
 */
-#include "exec_intern.h"
+
 #include <aros/libcall.h>
 #include <exec/ports.h>
 #include <proto/exec.h>
+
+#include "exec_intern.h"
+#include "exec_util.h"
 
 /*****************************************************************************
 
@@ -56,21 +59,7 @@
 
     if (port != NULL && port->mp_Flags & PA_FASTCALL)
     {
-        if (port->mp_SoftInt == NULL || ((struct Interrupt *) port->mp_SoftInt)->is_Code == NULL)
-            return;
-
-        ASSERT_VALID_PTR(port->mp_SoftInt);
-        ASSERT_VALID_PTR(((struct Interrupt *) port->mp_SoftInt)->is_Code);
-
-        message->mn_Node.ln_Type = NT_REPLYMSG;
-
-        /* call the "interrupt" with the message as an argument */
-        AROS_UFC4(void, ((struct Interrupt *) port->mp_SoftInt)->is_Code,
-            AROS_UFCA(APTR,              ((struct Interrupt *) port->mp_SoftInt)->is_Data, A1),
-            AROS_UFCA(ULONG_FUNC,        (ULONG_FUNC)((struct Interrupt *) port->mp_SoftInt)->is_Code, A5),
-            AROS_UFCA(struct Message *,  message,                                          D0),
-            AROS_UFCA(struct ExecBase *, SysBase,                                          A6));
-
+        FastPutMsg(port, message, SysBase);
         return;
     }
 
@@ -88,36 +77,7 @@
 	/* Mark the message as replied */
 	message->mn_Node.ln_Type=NT_REPLYMSG;
 
-	/* Add it to the replyport's list */
-	AddTail(&port->mp_MsgList,&message->mn_Node);
-
-	if(port->mp_SigTask)
-	{
-	    /* And trigger the arrival action. */
-	    switch(port->mp_Flags&PF_ACTION)
-	    {
-		case PA_SIGNAL:
-		    /* Send a signal */
-		    Signal((struct Task *)port->mp_SigTask,1<<port->mp_SigBit);
-		    break;
-
-		case PA_SOFTINT:
-		    /* Raise a software interrupt */
-		    Cause((struct Interrupt *)port->mp_SoftInt);
-		    break;
-
-		case PA_IGNORE:
-		    /* Do nothing */
-		    break;
-
-                case PA_CALL:
-                    /* Call the function in mp_SigTask. */
-                    AROS_UFC2(void, port->mp_SigTask,
-                        AROS_UFCA(struct MsgPort *,  port,    D0),
-                        AROS_UFCA(struct ExecBase *, SysBase, A6));
-                    break;
-	    }
-	}
+	InternalPutMsg(port, message, SysBase);
     }
 
     /* All done */
