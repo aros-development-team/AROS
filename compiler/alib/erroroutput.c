60ddbcdf9ff49cf6a8c7a0106c4c1dbb845e45ae
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-05-27 17:56:30 +0000

    Added AmigaOS4-compatible ErrorOutput()
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38881 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/alib/erroroutput.c
@@ -0,0 +1,49 @@
+/*
+    Copyright © 1995-2011, The AROS Development Team. All rights reserved.
+    $Id:$
+
+    Desc:
+    Lang: english
+*/
+
+#include <dos/dosextens.h>
+#include <proto/exec.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/alib.h>
+
+	BPTR ErrorOutput(
+
+/*  SYNOPSIS */
+	void)
+
+/*  FUNCTION
+	Returns the current error stream or 0 if there is no current
+	input stream.
+
+    INPUTS
+
+    RESULT
+	Error stream handle.
+
+    NOTES
+	This function is source-compatible with AmigaOS v4.
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+*****************************************************************************/
+{
+    /* Get pointer to process structure */
+    struct Process *me=(struct Process *)FindTask(NULL);
+
+    /* Nothing spectacular */
+    return me->pr_CES;
+}
