a8c923d317ae9f0b7a2d0944d96d136eb1bcc741
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-05-27 19:23:03 +0000

    Set old input and output back prior to closing redirected ones. Fixes memory trashing.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38887 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/Shell/redirection.c
+++ workbench/c/Shell/redirection.c
@@ -3,6 +3,7 @@
     $Id$
  */
 
+#include <aros/debug.h>
 #include <proto/dos.h>
 
 #include "Shell.h"
@@ -16,10 +17,18 @@ void Redirection_release(ShellState *ss, APTR DOSBase)
 {
     /* Close redirection files and install regular input and output streams */
     if (ss->newIn)
+    {
+	D(bug("[Shell] Closing redirected input 0x%p, old 0x%p\n", ss->newIn, ss->oldIn));
+	SelectInput(ss->oldIn);
 	Close(ss->newIn);
+    }
 
     if (ss->newOut)
+    {
+	D(bug("[Shell] Closing redirected output 0x%p, old 0x%p\n", ss->newOut, ss->oldOut));
+	SelectOutput(ss->oldOut);
 	Close(ss->newOut);
+    }
 
     ss->newIn = BNULL;
     ss->newOut = BNULL;
