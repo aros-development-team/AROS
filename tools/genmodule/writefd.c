5a5b8ae35c38472f39455b34f7abf3a7bce7a6ca
mattrust <mrustler@gmx.de>
2011-05-28 11:59:39 +0000

    Create fd file only when there is a function list. Write ##bias rather than .skip.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38891 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- tools/genmodule/writefd.c
+++ tools/genmodule/writefd.c
@@ -15,84 +15,91 @@ void writefd(struct config *cfg)
     unsigned int lvo;
 
     snprintf(line, 255, "%s/%s_lib.fd", cfg->gendir, cfg->modulename);
-    out = fopen(line, "w");
 
-    if (out == NULL)
+    if (cfg->funclist != NULL)
     {
-	perror(line);
-	exit(20);
-    }
+	out = fopen(line, "w");
 
-    /* When not a BOOPSI class write out the functionlist even if it is empty
-     * when it is a BOOPSI write only the list when it is not empty
-     * When cfg->basename != cfg->classlist->basename this means we are not in a BOOPSI class
-     * but there are extra classes defined
-     */
-    if (cfg->classlist == NULL
-	|| strcmp(cfg->basename, cfg->classlist->basename) != 0
-	|| cfg->funclist != NULL)
-    {
-	fprintf(out, "##base _%s\n", cfg->libbase);
-	fprintf(out, "##bias %u\n", cfg->firstlvo * 6);
-
-	fprintf(out, "##public\n");
-	
-	for (funclistit = cfg->funclist, lvo = cfg->firstlvo - 1;
-	     funclistit != NULL;
-	     funclistit = funclistit->next
-	)
+	if (out == NULL)
 	{
-	    if (funclistit->libcall == REGISTERMACRO)
-	    {
-		if (funclistit->lvo > lvo + 1)
-		{
-		    if (funclistit->lvo == lvo + 2)
-			fprintf(out, "\n");
-		    else
-			fprintf(out, ".skip %u\n", funclistit->lvo - lvo - 1);
-		}
-	    
-		fprintf(out, "%s(", funclistit->name);
-	    
-		for (arglistit = funclistit->arguments;
-		     arglistit!=NULL;
-		     arglistit = arglistit->next
-		)
-		{
-		    /* Print a , separator when not the first function argument */
-		    if (arglistit != funclistit->arguments)
-			fprintf(out, ", ");
-
-		    /* FIXME: only the argument name should be printed */
-		    fprintf(out, "%s", arglistit->arg);
-		}
-		fprintf(out, ") (");
-	    
-		for (arglistit = funclistit->arguments;
-		     arglistit != NULL;
-		     arglistit = arglistit->next
-		)
-		{
-		    /* Print a , separator when not the first function argument */
-		    if (arglistit != funclistit->arguments)
-			fprintf(out, ",");
-
-		    fprintf(out, "%s", arglistit->reg);
-		}
-		fprintf(out, ")\n");
-
-		lvo = funclistit->lvo;
-	    }
+	    perror(line);
+	    exit(20);
 	}
+	else
+	{
+	    fprintf(out, "##base _%s\n", cfg->libbase);
+	    fprintf(out, "##bias %u\n", cfg->firstlvo * 6);
 
-	fprintf(out, "##end\n");
-    }
+	    fprintf(out, "*\n"
+			 "* Automatically generated from '%s'.\n"
+			 "* Edits will be lost. This file is supposed to be a support file for identify.library.\n"
+			 "*\n",
+			 cfg->conffile
+	    );
 
-    if (ferror(out))
+	    fprintf(out, "##public\n");
+	    
+	    for (funclistit = cfg->funclist, lvo = cfg->firstlvo - 1;
+		 funclistit != NULL;
+		 funclistit = funclistit->next
+	    )
+	    {
+		if (funclistit->libcall == REGISTERMACRO)
+		{
+		    if (funclistit->lvo > lvo + 1)
+		    {
+			if (funclistit->lvo == lvo + 2)
+			    fprintf(out, "private()()\n");
+			else
+			    fprintf(out, "##bias %u\n", (funclistit->lvo - 1) * 6);
+		    }
+		
+		    fprintf(out, "%s(", funclistit->name);
+		
+		    for (arglistit = funclistit->arguments;
+			 arglistit!=NULL;
+			 arglistit = arglistit->next
+		    )
+		    {
+			/* Print a , separator when not the first function argument */
+			if (arglistit != funclistit->arguments)
+			    fprintf(out, ",");
+
+			/* FIXME: only the argument name should be printed */
+			fprintf(out, "%s", arglistit->arg);
+		    }
+		    fprintf(out, ")(");
+		
+		    for (arglistit = funclistit->arguments;
+			 arglistit != NULL;
+			 arglistit = arglistit->next
+		    )
+		    {
+			/* Print a , separator when not the first function argument */
+			if (arglistit != funclistit->arguments)
+			    fprintf(out, ",");
+
+			fprintf(out, "%s", arglistit->reg);
+		    }
+		    fprintf(out, ")\n");
+
+		    lvo = funclistit->lvo;
+		}
+	    }
+
+	    fprintf(out, "##end\n");
+
+	    if (ferror(out))
+	    {
+		perror(line);
+		exit(20);
+	    }
+
+	    fclose(out);
+	}
+    } /* cfg->funclist != NULL */
+    else
     {
-	perror(line);
-	exit(20);
+        remove(line);
     }
-
-    fclose(out);
 }
