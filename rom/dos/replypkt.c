dbd22e94444b9c2fe39f8690a64dedc4688c6faf
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-06-01 05:04:13 +0000

    Removed unneeded code
    git-svn-id: https://svn.aros.org/svn/aros/trunk@38951 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/replypkt.c
+++ rom/dos/replypkt.c
@@ -55,26 +55,19 @@
 
 void internal_ReplyPkt(struct DosPacket *dp, struct MsgPort *replyPort, LONG res1, LONG res2)
 {
-#ifdef AROS_DOS_PACKETS
-
     struct MsgPort *mp;
     struct Message *mn;
-	
+
     mp = dp->dp_Port;
     mn = dp->dp_Link;
     mn->mn_Node.ln_Name = (char*)dp;
     dp->dp_Port = replyPort;
     dp->dp_Res1 = res1;
     dp->dp_Res2 = res2;
+
+    /*
+     * Can't use ReplyMsg() here because mn_ReplyPort seems to be not set.
+     * Tested with SFS.
+     */
     PutMsg(mp, mn);
-
-#else
-
-    dp->dp_Res1 = res1;
-    dp->dp_Res2 = res2;
-    dp->dp_Port = replyPort;
-
-    ReplyMsg(dp->dp_Link);
-
-#endif
 }
