30af96574d27fc5ac47f4bc1130056bd46a589f2
mattrust <mrustler@gmx.de>
2011-06-09 06:57:51 +0000

    KATTR_MinStack doesn't exist anymore.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@39135 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/kernel/kernelattrs.c
+++ test/kernel/kernelattrs.c
@@ -18,7 +18,6 @@ int main(void)
     printf("Architecture   : %s\n", (char *)KrnGetSystemAttr(KATTR_Architecture));
     printf("VBlank enabled : %ld\n", KrnGetSystemAttr(KATTR_VBlankEnable));
     printf("Timer IRQ      : %ld\n", KrnGetSystemAttr(KATTR_TimerIRQ));
-    printf("Minimum stack  : %ld\n", KrnGetSystemAttr(KATTR_MinStack));
 #else
     printf("The test can't be built for this kernel.resource implementation\n");
 #endif
