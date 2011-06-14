e91375ee8b6032ac900369a4791cf1b723fb1d5d
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-06-14 13:47:49 +0000

    hidd.softpipe: Compiler delint
    Signed-off-by: Jason S. McMullan <jason.mcmullan@netronome.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@39205 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/hidds/hidd.softpipe/arosc_emul.c
+++ workbench/hidds/hidd.softpipe/arosc_emul.c
@@ -147,14 +147,16 @@ int gettimeofday (struct timeval * tv,struct timezone * tz)
     return 0;
 }
 
-void abort (void)
+__noreturn void abort (void)
 {
     IMPLEMENT();
+    for (;;);
 }
 
-void exit (int code)
+__noreturn void exit (int code)
 {
     IMPLEMENT();
+    for (;;);
 }
 	
 int puts (const char * str)
