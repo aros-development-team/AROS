9064ff014cd2af3e38b42bf294b9c74d66dabcb8
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-06-14 13:48:05 +0000

    gallium: Enable BSP socket networking for AROS
    Signed-off-by: Jason S. McMullan <jason.mcmullan@netronome.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@39207 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/mesa/src/gallium/auxiliary/util/u_network.c
+++ workbench/libs/mesa/src/gallium/auxiliary/util/u_network.c
@@ -6,7 +6,7 @@
 #if defined(PIPE_SUBSYSTEM_WINDOWS_USER)
 #  include <winsock2.h>
 #  include <windows.h>
-#elif defined(PIPE_OS_LINUX) || defined(PIPE_OS_HAIKU) || defined(PIPE_OS_APPLE) || defined(PIPE_OS_CYGWIN)
+#elif defined(PIPE_OS_LINUX) || defined(PIPE_OS_HAIKU) || defined(PIPE_OS_APPLE) || defined(PIPE_OS_CYGWIN) || defined(PIPE_OS_AROS)
 #  include <sys/socket.h>
 #  include <netinet/in.h>
 #  include <unistd.h>
