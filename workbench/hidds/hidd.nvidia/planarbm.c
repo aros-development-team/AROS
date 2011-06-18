96ef0a5b28fe9ab0cc0c7892f57f22bf0f5c635a
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-06-18 20:08:47 +0000

    nvidia.hidd: Compiler delint (x86_64)
    Signed-off-by: Jason S. McMullan <jason.mcmullan@netronome.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@39386 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/hidds/hidd.nvidia/planarbm.c
+++ workbench/hidds/hidd.nvidia/planarbm.c
@@ -87,7 +87,7 @@ OOP_Object *NVPlanBM__Root__New(OOP_Class *cl, OOP_Object *o, struct pRoot_New *
 
     data->planes_alloced = (BOOL)GetTagData(aHidd_PlanarBM_AllocPlanes, TRUE, msg->attrList);
     
-    #warning Fix this hack
+    /* TODO: Fix this hack */
     /* Because this class is used to emulate Amiga bitmaps, we
        have to see if it should have late initalisation
     */
