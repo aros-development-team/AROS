080588ebd78755750b3d6380eff854a7c08beb13
twilen <twilen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-06-19 12:37:50 +0000

    KickMem list is single-linked. Make sure checksum is always non-zero if lists are not empty.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@39400 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/sumkickdata.c
+++ rom/exec/sumkickdata.c
@@ -42,12 +42,15 @@
     AROS_LIBFUNC_INIT
 
     ULONG chksum = 0;
+    BOOL isdata = FALSE;
 
     if (SysBase->KickTagPtr) {
     	IPTR *list = SysBase->KickTagPtr;
  	while(*list)
 	{
    	    chksum += (ULONG)*list;
+   	    D(bug("KickTag: %p: %08x\n", list, *list));
+
             /* on amiga, if bit 31 is set then this points to another list of
              * modules rather than pointing to a single module. bit 31 is
              * inconvenient on architectures where code may be loaded above
@@ -59,20 +62,29 @@
             if(*list & 0x1) { list = (IPTR *)(*list & ~(IPTR)0x1); continue; }
 #endif
 	    list++;
+   	    isdata = TRUE;
    	}
     }
 
     if (SysBase->KickMemPtr) {
 	struct MemList *ml = (struct MemList*)SysBase->KickMemPtr;
-	while (ml->ml_Node.ln_Succ) {
+	while (ml) {
 	    UBYTE i;
 	    ULONG *p = (ULONG*)ml;
 	    for (i = 0; i < sizeof(struct MemList) / sizeof(ULONG); i++)
 	    	chksum += p[i];
+	    D(bug("MemList: %p: %08x %08x %d %08x %08x %08x%08x%08x%08x\n", ml, ml->ml_Node.ln_Succ, ml->ml_Node.ln_Pred, ml->ml_NumEntries,
+	    	ml->ml_ME[0].me_Un.meu_Addr, ml->ml_ME[0].me_Length,
+	    	p[sizeof(struct MemList) / sizeof(ULONG)],
+	    	p[sizeof(struct MemList) / sizeof(ULONG) + 1],
+	    	p[sizeof(struct MemList) / sizeof(ULONG) + 2],
+	    	p[sizeof(struct MemList) / sizeof(ULONG) + 3])); 
 	    ml = (struct MemList*)ml->ml_Node.ln_Succ;
+	    isdata = TRUE;
 	}
     }
-
+    if (isdata && !chksum)
+    	chksum--;
     return chksum;
 
     AROS_LIBFUNC_EXIT
