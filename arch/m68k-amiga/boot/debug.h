ff3edc52d7b61723ff0e42e08b32c09aff4bfc50
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-06-22 19:37:57 +0000

    arch/m68k-amiga: Add a DEBUGPRINTD() to print decimal numbers
    Signed-off-by: Jason S. McMullan <jason.mcmullan@netronome.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@39480 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/m68k-amiga/boot/debug.h
+++ arch/m68k-amiga/boot/debug.h
@@ -13,13 +13,16 @@ int DebugPutChar(register int chr);
 int DebugMayGetChar(void);
 
 #ifdef AROS_SERIAL_DEBUG
-void DebugPuts(register const char *buff);
+void DebugPutStr(register const char *buff);
 void DebugPutHex(const char *what, ULONG val);
+void DebugPutDec(const char *what, ULONG val);
 
-#define DEBUGPUTS(x) do { DebugPuts x; } while(0)
+#define DEBUGPUTS(x) do { DebugPutStr x; } while(0)
+#define DEBUGPUTD(x) do { DebugPutDec x; } while(0)
 #define DEBUGPUTHEX(x) do { DebugPutHex x; } while(0)
 #else
 #define DEBUGPUTS(x) do { } while (0)
+#define DEBUGPUTD(x) do { } while (0)
 #define DEBUGPUTHEX(x) do { } while (0)
 #endif
 
