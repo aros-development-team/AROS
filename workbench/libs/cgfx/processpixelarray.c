aa8df84a2f98ab265debf1842300dc62fc3c9b36
mattrust <mrustler@gmx.de>
2011-06-23 17:16:35 +0000

    Skeleton for a function which is needed by Scalos.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@39489 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/libs/cgfx/processpixelarray.c
@@ -0,0 +1,83 @@
+/*
+    Copyright © 1995-2011, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc:
+    Lang: english
+*/
+
+#include <aros/debug.h>
+#include <cybergraphx/cybergraphics.h>
+#include <exec/types.h>
+#include <proto/cybergraphics.h>
+#include <proto/utility.h>
+
+#include "cybergraphics_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+#include <clib/cybergraphics_protos.h>
+
+	AROS_LH8(VOID, ProcessPixelArray,
+
+/*  SYNOPSIS */
+	AROS_LHA(struct RastPort *, rp, A1),
+	AROS_LHA(ULONG, destX, D0),
+	AROS_LHA(ULONG, destY, D1),
+	AROS_LHA(ULONG, sizeX, D2),
+	AROS_LHA(ULONG, sizeY, D3),
+	AROS_LHA(ULONG, operation, D4),
+	AROS_LHA(LONG, value, D5),
+	AROS_LHA(struct TagItem *, taglist, A2),
+
+/*  LOCATION */
+	struct Library *, CyberGfxBase, 38, Cybergraphics)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    /* TODO: Implement me */
+
+    bug("ProcessPixelArray not implemented\n");
+
+#if 0
+    const struct TagItem *tstate;
+    struct TagItem *tag;
+
+    for (tstate = tags; (tag = NextTagItem(&tstate)); ) {
+	switch (tag->ti_Tag) {
+	    case aaa:
+	    	minwidth = (ULONG)tag->ti_Data;
+		break;
+		
+
+	    default:
+	    	D(bug("!!! UNKNOWN TAG PASSED TO ProcessPixelArray\n"));
+		break;
+	} 	
+    }
+
+#endif
+
+    AROS_LIBFUNC_EXIT
+}
