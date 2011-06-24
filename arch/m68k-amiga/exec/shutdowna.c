c1a66280da90dbf363f57dee9da0d6537e8bb748
twilen <twilen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-06-24 07:23:46 +0000

    Added UAE compatible SD_ACTION_POWEROFF support.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@39494 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/m68k-amiga/exec/shutdowna.c
+++ arch/m68k-amiga/exec/shutdowna.c
@@ -53,9 +53,33 @@
 
     switch (action) {
     case SD_ACTION_POWEROFF:
-    	/* No stock Amiga hardware is known to support this
-    	 */
+    {
+    	/* No stock Amiga hardware is known to support this.
+    	 * Emulation will support it (unless high compatibility setting enabled). */
+    	typedef ULONG (*UAELIBFUNC)(ULONG);
+    	UAELIBFUNC uaelibfunc = NULL;
+	APTR uaeres;
+	
+	uaeres = OpenResource("uae.resource");
+	if (uaeres) { /* new method that allows dynamic UAE ROM location */
+	    uaelibfunc = AROS_LVO_CALL1(APTR,
+		AROS_LCA(UBYTE*, "uaelib_demux", A0),
+		APTR, uaeres, 1, );
+	}
+	if (uaelibfunc == NULL) {
+	    /* old-style absolute address */
+	    uaelibfunc = (UAELIBFUNC)(0x00F00000 + 0xFF60);
+	    if ((((ULONG*)uaelibfunc)[0] & 0xff00ffff) != 0xa0004e75)
+	    	uaelibfunc = NULL;
+	}
+	if (uaelibfunc) {
+	    uaelibfunc(13);
+	    /* This may return. Quits when next vblank arrives */
+	    for(;;)
+		;
+	}
     	break;
+    }
     case SD_ACTION_COLDREBOOT:
     	ColdReboot();
     	break;
