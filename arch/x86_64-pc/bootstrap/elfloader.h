f454a37244da5fa6c14277ac41affece4537d101
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-07-01 08:39:33 +0000

    Improvements to ELF loader, picked up from InternalLoadSeg_ELF(). Seem to have fixed random memory trashing bugs (actually wrong relocations because of wrong symbol index treatment). Bootstrap now correctly reports loading problems instead of running a broken kickstart.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@39687 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/x86_64-pc/bootstrap/elfloader.h
+++ arch/x86_64-pc/bootstrap/elfloader.h
@@ -8,7 +8,7 @@
 
 unsigned long long DebugInfo_ptr;
 
-void load_elf_file(const char *Name, void *file, unsigned long long virt);
+int load_elf_file(const char *Name, void *file, unsigned long long virt);
 void *kernel_lowest();
 void *kernel_highest();
 void set_base_address(void *, void *);
