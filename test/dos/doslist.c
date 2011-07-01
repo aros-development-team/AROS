f1ce25878edfe338a120847784a11ff48621559f
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-07-01 06:46:03 +0000

    Added automatic test to ensure proper DosList structures alignment
    git-svn-id: https://svn.aros.org/svn/aros/trunk@39684 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/dos/doslist.c
@@ -0,0 +1,25 @@
+#include <aros/debug.h>
+#include <dos/dosextens.h>
+
+#include <stddef.h>
+
+int __nocommandline = 1;
+
+int main(void)
+{
+    const unsigned int dl_offset = offsetof(struct DeviceList, dl_Name);
+    const unsigned int di_offset = offsetof(struct DevInfo, dvi_Name);
+    const unsigned int gen_offset = offsetof(struct DosList, dol_Name);
+
+    if ((dl_offset != di_offset) || (dl_offset != gen_offset))
+    {
+    	bug("Broken DosList alignment.\n");
+    	bug("DosList.dol_Name  : %u bytes\n", gen_offset);
+    	bug("DeviceList.dl_Name: %u bytes\n", dl_offset);
+    	bug("DevInfo.dvi_Name  : %u bytes\n", di_offset);
+    	
+    	return 20;
+    }
+    else
+    	return 0;
+}
