62c77f63950710b5282541e233bb87f294b0f323
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-07-02 13:32:35 +0000

    Revert "workbench.library: Adjust do_Type to match Commorore headers"
    .. and guess what. The Commodore Amiga Macro Assembler 1.0 headers
    I used as reference are buggy in this respect. Reverted.
    
    This reverts commit r39715
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@39720 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/workbench/workbench.h
+++ compiler/include/workbench/workbench.h
@@ -63,7 +63,7 @@ struct DiskObject
     UWORD              do_Magic;
     UWORD              do_Version;
     struct Gadget      do_Gadget;
-    UWORD              do_Type;        /* see below */
+    UBYTE              do_Type;        /* see below */
     STRPTR             do_DefaultTool;
     STRPTR            *do_ToolTypes;
     LONG               do_CurrentX;
