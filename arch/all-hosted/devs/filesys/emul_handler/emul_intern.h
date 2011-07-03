41b61cb167ed48f6206285db88ef5657566de124
ezrec <jason.mcmullan@gmail.com>
2011-07-03 20:24:12 +0000

    emul.handler: Synthesize a ConfigDev, and add our own BootNode
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@39770 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-hosted/devs/filesys/emul_handler/emul_intern.h
+++ arch/all-hosted/devs/filesys/emul_handler/emul_intern.h
@@ -44,6 +44,7 @@ struct emulbase
     APTR		      ReadIRQ;
     APTR		      HostLibBase;
     APTR		      KernelBase;
+    struct ConfigDev         *eb_ConfigDev;
     struct Emul_PlatformData  pdata;	/* Platform-specific portion */
 };
 
