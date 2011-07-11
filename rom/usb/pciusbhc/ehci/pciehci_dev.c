9c0e799c09f32aec002507ab141b02f881c93638
DizzyOfCRN <DizzyOfCRN@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-07-11 13:33:01 +0000

    -Very much WIP, ehci barebone device, no hardware initializations at this stage just the basic devio stuff -Splitting pciusb.device down to individual host drivers results in individual pci(u/o/e/(x))hci.devices -Streamlined device initialization and unit node creation -Small functions to be inlined (e.g TermIO), jumping to the function takes more ticks than the actual code -Renamed device structures (same convention is expected in (u/o/(x))hci drivers)     - ehd =EHCI device base structure     - ehu =EHCI host unit structure     - ehc =EHCI host controller structure -Host controllers on the same bus will be grouped to the same unit as is the case now, but companion controllers will have their own drivers and groubed similary there (some port logic changes may be expected) -Not sure how to proceed with the hardware register definitions, making xhci.h was a pain, but redoing them all with structures and unions would be a lot of work...
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40008 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/usb/pciusbhc/ehci/pciehci_dev.c
@@ -0,0 +1,173 @@
+/*
+    Copyright © 2011, The AROS Development Team. All rights reserved
+    $Id: pciehci_dev.c 39842 2011-07-05 19:39:09Z DizzyOfCRN $
+*/
+
+#include LC_LIBDEFS_FILE
+
+static int Open(LIBBASETYPEPTR LIBBASE, struct IOUsbHWReq *ioreq,  ULONG unit, ULONG flags) {
+    LIBBASETYPE *ehd = (LIBBASETYPE *) LIBBASE;
+
+    KPRINTF2(DBL_DEVIO,("EHC Open: "));
+
+    if(ioreq->iouh_Req.io_Message.mn_Length < sizeof(struct IOUsbHWReq)) {
+        KPRINTF2(DBL_DEVIO, ("Invalid MN_LENGTH"));
+        ioreq->iouh_Req.io_Error = IOERR_BADLENGTH;
+    } else {
+        ioreq->iouh_Req.io_Error = IOERR_OPENFAIL;
+
+        struct ehu_unit *ehu;
+        ForeachNode(&ehd->ehd_unitlist, ehu) {
+            if(ehu->ehu_unitnumber == unit) {
+                ioreq->iouh_Req.io_Unit = ehu;
+                if(ehu->ehu_unitallocated) {
+                    ioreq->iouh_Req.io_Error = IOERR_UNITBUSY;
+                    KPRINTF2(DBL_DEVIO, ("Unit %ld already open", unit));
+                    break;
+                }else{
+                    ioreq->iouh_Req.io_Message.mn_Node.ln_Type = NT_REPLYMSG;
+                    ioreq->iouh_Req.io_Error = 0;
+                    KPRINTF2(DBL_DEVIO, ("Unit %ld opened\n", unit));
+                    return TRUE;
+                }
+            }
+        }
+
+        KPRINTF2(DBL_DEVIO, ("Unit %ld does not exist", unit));
+    }
+
+    KPRINTF2(DBL_DEVIO, (", could not open unit!\n"));
+    ioreq->iouh_Req.io_Unit = NULL;
+    return FALSE;
+}
+
+static int Close(LIBBASETYPEPTR LIBBASE, struct IOUsbHWReq *ioreq) {
+    LIBBASETYPE *ehd = (LIBBASETYPE *) LIBBASE;
+
+    KPRINTF2(DBL_DEVIO,("EHC Close: \n"));
+    return TRUE;
+}
+
+AROS_LH1(void, BeginIO,
+    AROS_LHA(struct IOUsbHWReq *, ioreq, A1),
+    LIBBASETYPEPTR, LIBBASE, 5, pciehci)
+{
+	AROS_LIBFUNC_INIT
+    LIBBASETYPE *ehd = (LIBBASETYPE *) LIBBASE;
+
+    KPRINTF2(DBL_DEVIO,("EHC BeginIO: "));
+
+    struct ehu_unit *ehu = (struct ehu_unit *) ioreq->iouh_Req.io_Unit;
+
+    ULONG ret = RC_OK;
+
+    ioreq->iouh_Req.io_Message.mn_Node.ln_Type = NT_MESSAGE;
+    ioreq->iouh_Req.io_Error                   = UHIOERR_NO_ERROR;
+
+    if (ioreq->iouh_Req.io_Command < NSCMD_DEVICEQUERY) {
+        switch (ioreq->iouh_Req.io_Command) {
+			case CMD_RESET:
+                KPRINTF2(DBL_DEVIO,("cmdReset\n"));
+//              ret = cmdReset(ioreq, unit, base);
+                break;
+
+			case CMD_FLUSH:
+                KPRINTF2(DBL_DEVIO,("cmdFlush\n"));
+//              ret = cmdFlush(ioreq, unit, base);
+                break;
+
+            case UHCMD_QUERYDEVICE:
+                KPRINTF2(DBL_DEVIO,("cmdQueryDevice\n"));
+//              ret = cmdQueryDevice(ioreq, unit, base);
+                break;
+
+			case UHCMD_USBRESET:
+                KPRINTF2(DBL_DEVIO,("cmdUsbReset\n"));
+//              ret = cmdUsbReset(ioreq, unit, base);
+                break;
+
+			case UHCMD_USBRESUME:
+                KPRINTF2(DBL_DEVIO,("cmdUsbResume\n"));
+//              ret = cmdUsbResume(ioreq, unit, base);
+                break;
+
+			case UHCMD_USBSUSPEND:
+                KPRINTF2(DBL_DEVIO,("cmdUsbSuspend\n"));
+//              ret = cmdUsbSuspend(ioreq, unit, base);
+                break;
+
+			case UHCMD_USBOPER:
+                KPRINTF2(DBL_DEVIO,("cmdUsbOper\n"));
+//              ret = cmdUsbOper(ioreq, unit, base);
+                break;
+
+			case UHCMD_CONTROLXFER:
+                KPRINTF2(DBL_DEVIO,("cmdControlXFer\n"));
+//              ret = cmdControlXFer(ioreq, unit, base);
+                break;
+
+			case UHCMD_BULKXFER:
+                KPRINTF2(DBL_DEVIO,("cmdBulkXFer\n"));
+//              ret = cmdBulkXFer(ioreq, unit, base);
+                break;
+
+			case UHCMD_INTXFER:
+                KPRINTF2(DBL_DEVIO,("cmdIntXFer\n"));
+//              ret = cmdIntXFer(ioreq, unit, base);
+                break;
+
+			case UHCMD_ISOXFER:
+                KPRINTF2(DBL_DEVIO,("cmdIsoXFer\n"));
+//              ret = cmdIsoXFer(ioreq, unit, base);
+                break;
+
+            default:
+                KPRINTF2(DBL_DEVIO,("IOERR_NOCMD\n"));
+//              ret = IOERR_NOCMD;
+                break;
+        }
+    } else {
+        switch(ioreq->iouh_Req.io_Command) {
+            case NSCMD_DEVICEQUERY:
+                KPRINTF2(DBL_DEVIO,("cmdNSDeviceQuery\n"));
+//              ret = cmdNSDeviceQuery((struct IOStdReq *) ioreq, unit, base);
+                break;
+
+            default:
+                KPRINTF2(DBL_DEVIO,("IOERR_NOCMD\n"));
+//              ret = IOERR_NOCMD;
+                break;
+        }
+    }
+
+    if(ret != RC_DONTREPLY) {
+        /* Set error codes */
+        if (ret != RC_OK) {
+            ioreq->iouh_Req.io_Error = ret & 0xff;
+        }
+        /* Terminate the iorequest */
+        ioreq->iouh_Req.io_Message.mn_Node.ln_Type = NT_FREEMSG;
+        /* If not quick I/O, reply the message */
+        if(!(ioreq->iouh_Req.io_Flags & IOF_QUICK)) {
+            ReplyMsg(&ioreq->iouh_Req.io_Message);
+        }
+    }
+
+	AROS_LIBFUNC_EXIT
+}
+
+AROS_LH1(LONG, AbortIO,
+	AROS_LHA(struct IOUsbHWReq *, ioreq, A1),
+	LIBBASETYPEPTR, LIBBASE, 6, pciehci)
+{
+	AROS_LIBFUNC_INIT
+    LIBBASETYPE *ehd = (LIBBASETYPE *) LIBBASE;
+
+    KPRINTF2(DBL_DEVIO,("EHC AbortIO: \n"));
+
+    return TRUE;
+	AROS_LIBFUNC_EXIT
+}
+
+ADD2OPENDEV(Open,0)
+ADD2CLOSEDEV(Close,0)
---
+++ rom/usb/pciusbhc/ehci/pciehci_init.c
@@ -0,0 +1,112 @@
+/*
+    Copyright © 2011, The AROS Development Team. All rights reserved
+    $Id: pciehci_init.c 39842 2011-07-05 19:39:09Z DizzyOfCRN $
+*/
+
+#include LC_LIBDEFS_FILE
+
+AROS_UFH3(void, ehc_enumhook,
+	AROS_UFHA(struct Hook *,    hook,       A0),
+	AROS_UFHA(OOP_Object *,     pcidevice,  A2),
+	AROS_UFHA(APTR,             message,    A1))
+{
+    AROS_USERFUNC_INIT
+    LIBBASETYPE *ehd = (LIBBASETYPE *) hook->h_Data;
+
+    BOOL Found = FALSE;
+
+    KPRINTF2(DBL_DEVIO,("EHC Controller found...\n"));
+
+    struct ehc_controller *ehc;
+    if((ehc = AllocPooled(ehd->ehd_mempool, sizeof(struct ehc_controller)))) {
+
+        OOP_GetAttr(pcidevice, aHidd_PCIDevice_Bus, &ehc->ehc_pcibus);
+        OOP_GetAttr(pcidevice, aHidd_PCIDevice_Dev, &ehc->ehc_pcidev);
+        OOP_GetAttr(pcidevice, aHidd_PCIDevice_Dev, &ehc->ehc_pcisub);
+
+        /* Try to match the controller with previous unit nodes created based on the bus address (if any) */
+        struct ehu_unit *ehu;
+        if (!IsListEmpty(&ehd->ehd_unitlist)) {
+            ForeachNode(&ehd->ehd_unitlist, ehu) {
+                if(((ehu->ehu_pcibus == ehc->ehc_pcibus)&&(ehu->ehu_pcidev == ehc->ehc_pcidev))) {
+                    Found = TRUE;
+                    break;
+                }
+            }
+        }
+
+        /* Add the controller to the unit node or create a new one */
+        if(Found) {
+                KPRINTF2(DBL_DEVIO,("EHC Found matching unit #%ld (%04x:%04x)\n", ehu->ehu_unitnumber, ehu->ehu_pcibus, ehu->ehu_pcidev));
+                ADDTAIL((struct List*)&ehu->ehu_cntrlist, (struct Node*)ehc);
+        }else{
+            if((ehu = AllocPooled(ehd->ehd_mempool, sizeof(struct ehu_unit)))) {
+                ehu->ehu_pcibus = ehc->ehc_pcibus;
+                ehu->ehu_pcidev = ehc->ehc_pcidev;
+                if (!IsListEmpty(&ehd->ehd_unitlist)) {
+                    struct ehu_unit *prev_ehu = (struct ehu_unit *)GetPred(ehu);
+                    ehu->ehu_unitnumber = prev_ehu->ehu_unitnumber + 1;
+                }else{
+                    ehu->ehu_unitnumber = 0;
+                }
+                NEWLIST((struct MinList *)&ehu->ehu_cntrlist);
+                ADDTAIL((struct List*)&ehu->ehu_cntrlist, (struct Node*)ehc);
+                ADDTAIL((struct List*)&ehd->ehd_unitlist, (struct Node*)ehu);
+                KPRINTF2(DBL_DEVIO,("EHC Created new unit #%ld (%04x:%04x)\n", ehu->ehu_unitnumber, ehu->ehu_pcibus, ehu->ehu_pcidev));
+            }
+        }
+    }
+
+    AROS_USERFUNC_EXIT
+}
+
+static int Init(LIBBASETYPEPTR LIBBASE) {
+    LIBBASETYPE *ehd = (LIBBASETYPE *) LIBBASE;
+
+    struct OOP_ABDescr attrbases[] = {
+        { (STRPTR)IID_Hidd,             &HiddAttrBase },
+        { (STRPTR)IID_Hidd_PCIDevice,   &HiddPCIDeviceAttrBase },
+        { NULL, NULL }
+    };
+
+    OOP_ObtainAttrBases(attrbases);
+
+    struct Hook pci_enumhooktag = {
+        h_Entry:        (IPTR (*)()) ehc_enumhook,
+        h_Data:         LIBBASE,
+    };
+
+    struct TagItem pci_enumtag [] = {
+        { tHidd_PCI_Class,      PCI_BASE_CLASS_SERIAL },
+        { tHidd_PCI_SubClass,   PCI_SUB_CLASS_USB },
+        { tHidd_PCI_Interface,  PCI_INTERFACE_EHCI },
+        { TAG_DONE, 0UL }
+    };
+
+    NEWLIST((struct MinList *)&ehd->ehd_unitlist);
+
+    if((ehd->ehd_mempool = CreatePool(MEMF_PUBLIC | MEMF_CLEAR | MEMF_SEM_PROTECTED, 16384, 4096))) {
+        if((ehd->ehd_pcihidd = OOP_NewObject(NULL, (STRPTR) CLID_Hidd_PCI, NULL))) {
+            HIDD_PCI_EnumDevices(ehd->ehd_pcihidd, &pci_enumhooktag, (struct TagItem *) &pci_enumtag);
+            if (!IsListEmpty(&ehd->ehd_unitlist)) {
+                return TRUE;
+            }
+        }
+
+        DeletePool(ehd->ehd_mempool);
+        ehd->ehd_mempool = NULL;
+    }
+    return FALSE;
+}
+
+static int Expunge(LIBBASETYPEPTR LIBBASE) {
+    LIBBASETYPE *ehd = (LIBBASETYPE *) LIBBASE;
+
+    DeletePool(ehd->ehd_mempool);
+
+    OOP_DisposeObject(ehd->ehd_pcihidd);
+    return TRUE;
+}
+
+ADD2INITLIB(Init,0)
+ADD2EXPUNGELIB(Expunge,0)
