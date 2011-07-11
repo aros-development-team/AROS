3826e6762acfe2d227c8a3f090ecb89474ce5c9e
DizzyOfCRN <DizzyOfCRN@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-07-11 13:33:01 +0000

    -Very much WIP, ehci barebone device, no hardware initializations at this stage just the basic devio stuff -Splitting pciusb.device down to individual host drivers results in individual pci(u/o/e/(x))hci.devices -Streamlined device initialization and unit node creation -Small functions to be inlined (e.g TermIO), jumping to the function takes more ticks than the actual code -Renamed device structures (same convention is expected in (u/o/(x))hci drivers)     - ehd =EHCI device base structure     - ehu =EHCI host unit structure     - ehc =EHCI host controller structure -Host controllers on the same bus will be grouped to the same unit as is the case now, but companion controllers will have their own drivers and groubed similary there (some port logic changes may be expected) -Not sure how to proceed with the hardware register definitions, making xhci.h was a pain, but redoing them all with structures and unions would be a lot of work...
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40008 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/usb/pciusbhc/ehci/pciehci.h
@@ -0,0 +1,81 @@
+/*
+    Copyright © 2011, The AROS Development Team. All rights reserved
+    $Id: pciehci.h 39842 2011-07-05 19:39:09Z DizzyOfCRN $
+*/
+
+#ifndef PCIEHCI_H
+#define PCIEHCI_H
+
+#include <aros/system.h>
+#include <aros/libcall.h>
+#include <aros/symbolsets.h>
+
+#include <exec/exec.h>
+#include <devices/newstyle.h>
+#include <devices/usbhardware.h>
+#include <hidd/pci.h>
+
+#include <proto/exec.h>
+#include <proto/debug.h>
+#include <proto/oop.h>
+
+#define PCI_BASE_CLASS_SERIAL   0x0c
+#define PCI_SUB_CLASS_USB       0x03
+#define PCI_INTERFACE_EHCI      0x20
+//#define PCI_INTERFACE_EHCI      0x00  /* Really UHCI, used to check unit node creation. Remove later */
+
+#define RC_OK         0                 /* Reply the iorequest with success */
+#define RC_DONTREPLY  -1                /* Magic cookie, don't set error fields & don't reply the ioreq */
+
+#undef HiddAttrBase
+#undef HiddPCIDeviceAttrBase
+
+#define HiddAttrBase            (ehd->ehd_hiddattrbase)
+#define HiddPCIDeviceAttrBase   (ehd->ehd_hiddpcideviceab)
+
+#define DBL_DEVIO 10
+#define DEBUG DBL_DEVIO
+
+#ifdef DEBUG
+#define KPRINTF(l, x) do { if ((l) >= DEBUG) \
+     { KPrintF("%s/%lu: ", __FUNCTION__, __LINE__); KPrintF x;} } while (0)
+#define KPRINTF2(l, x) do { if ((l) >= DEBUG) \
+     { KPrintF x;} } while (0)
+#else /* !DEBUG */
+#define KPRINTF(l, x) ((void) 0)
+#define KPRINTF2(l, x) ((void) 0)
+#endif /* DEBUG */
+
+
+struct ehc_controller {                 /* EHCI Controller Struct (ehc_) */
+    struct MinNode  ehc_contrnode;
+
+    APTR            ehc_unitptr;
+
+    IPTR            ehc_pcibus, ehc_pcidev, ehc_pcisub;
+};
+
+struct ehu_unit {                       /* EHCI Unit Structure (ehu_) */
+    struct MinNode  ehu_unitnode;
+
+    ULONG           ehu_unitnumber;
+    BOOL            ehu_unitallocated;  /* Unit opened */
+    IPTR            ehu_pcibus, ehu_pcidev;
+
+    struct List     ehu_cntrlist;
+};
+
+/* pciehci.device base*/
+struct pciehcibase {                    /* EHCI Device Structure (ehd_) */
+    struct Device   ehd_device;
+
+    struct List     ehd_unitlist;       /* Host Controller List */
+
+    APTR            ehd_mempool;
+
+    OOP_Object     *ehd_pcihidd;
+    OOP_AttrBase    ehd_hiddattrbase;
+    OOP_AttrBase    ehd_hiddpcideviceab;
+};
+
+#endif /* PCIEHCI_H */
