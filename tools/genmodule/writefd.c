4041fefc29360706f30c14dec58df5b2a7b2141f
ezrec <jason.mcmullan@gmail.com>
2011-07-14 20:33:10 +0000

    genmodule: Make a minimal *.fd file, even if there aren't any functions
    The *.fd will still identify the base for the library, which is useful.
    
    This also prevents genmodule from running every time for these modules.
    
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40132 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- tools/genmodule/writefd.c
+++ tools/genmodule/writefd.c
@@ -18,110 +18,103 @@ void writefd(struct config *cfg)
 
     snprintf(line, 255, "%s/%s_lib.fd", cfg->gendir, cfg->modulename);
 
-    if (cfg->funclist != NULL)
+    out = fopen(line, "w");
+
+    if (out == NULL)
     {
-	out = fopen(line, "w");
-
-	if (out == NULL)
-	{
-	    perror(line);
-	    exit(20);
-	}
-	else
-	{
-	    fprintf(out, "##base _%s\n", cfg->libbase);
-	    
-	    if (cfg->modtype == DEVICE)
-	    {
-		/* because of BeginIO/EndIO */
-		/* FIXME: do we need special handling for other module types, too? */
-		fprintf(out, "##bias %u\n", (cfg->firstlvo - 2) * 6);
-	    }
-	    else
-	    {
-		fprintf(out, "##bias %u\n", cfg->firstlvo * 6);
-	    }
-
-	    fprintf(out, "*\n"
-			 "* Automatically generated from '%s'.\n"
-			 "* Edits will be lost. This file is supposed to be a support file for identify.library.\n"
-			 "*\n",
-			 cfg->conffile
-	    );
-
-	    fprintf(out, "##public\n");
-	    
-	    for (funclistit = cfg->funclist, lvo = cfg->firstlvo - 1;
-		 funclistit != NULL;
-		 funclistit = funclistit->next
-	    )
-	    {
-		if (funclistit->libcall == REGISTERMACRO)
-		{
-		    if (funclistit->lvo > lvo + 1)
-		    {
-			if (funclistit->lvo == lvo + 2)
-			    fprintf(out, "private()()\n");
-			else
-			    fprintf(out, "##bias %u\n", (funclistit->lvo - 1) * 6);
-		    }
-		
-		    fprintf(out, "%s(", funclistit->name);
-		
-		    for (arglistit = funclistit->arguments;
-			 arglistit!=NULL;
-			 arglistit = arglistit->next
-		    )
-		    {
-			/* Print a , separator when not the first function argument */
-			if (arglistit != funclistit->arguments)
-			    fprintf(out, ",");
-
-			/* Print only variable name */
-			variable = arglistit->arg + strlen(arglistit->arg) - 1;
-			while ((variable >= arglistit->arg) &&
-			      (isalnum(*variable) || (*variable == '_')))
-			{
-			    variable--;
-			}
-			fprintf(out, "%s", variable + 1);
-		    }
-		    fprintf(out, ")(");
-		
-		    for (arglistit = funclistit->arguments;
-			 arglistit != NULL;
-			 arglistit = arglistit->next
-		    )
-		    {
-			/* Print a , separator when not the first function argument */
-			if (arglistit != funclistit->arguments)
-			    fprintf(out, ",");
-
-			/* Print register name in lower case */
-			for (lower = arglistit->reg; *lower != '\0'; lower++)
-			{
-			    fputc(tolower(*lower), out);
-			}
-		    }
-		    fprintf(out, ")\n");
-
-		    lvo = funclistit->lvo;
-		}
-	    }
-
-	    fprintf(out, "##end\n");
-
-	    if (ferror(out))
-	    {
-		perror(line);
-		exit(20);
-	    }
-
-	    fclose(out);
-	}
-    } /* cfg->funclist != NULL */
+        perror(line);
+        exit(20);
+    }
     else
     {
-	remove(line);
+        fprintf(out, "##base _%s\n", cfg->libbase);
+        
+        if (cfg->modtype == DEVICE)
+        {
+            /* because of BeginIO/EndIO */
+            /* FIXME: do we need special handling for other module types, too? */
+            fprintf(out, "##bias %u\n", (cfg->firstlvo - 2) * 6);
+        }
+        else
+        {
+            fprintf(out, "##bias %u\n", cfg->firstlvo * 6);
+        }
+
+        fprintf(out, "*\n"
+                     "* Automatically generated from '%s'.\n"
+                     "* Edits will be lost. This file is supposed to be a support file for identify.library.\n"
+                     "*\n",
+                     cfg->conffile
+        );
+
+        fprintf(out, "##public\n");
+        
+        for (funclistit = cfg->funclist, lvo = cfg->firstlvo - 1;
+             funclistit != NULL;
+             funclistit = funclistit->next
+        )
+        {
+            if (funclistit->libcall == REGISTERMACRO)
+            {
+                if (funclistit->lvo > lvo + 1)
+                {
+                    if (funclistit->lvo == lvo + 2)
+                        fprintf(out, "private()()\n");
+                    else
+                        fprintf(out, "##bias %u\n", (funclistit->lvo - 1) * 6);
+                }
+            
+                fprintf(out, "%s(", funclistit->name);
+            
+                for (arglistit = funclistit->arguments;
+                     arglistit!=NULL;
+                     arglistit = arglistit->next
+                )
+                {
+                    /* Print a , separator when not the first function argument */
+                    if (arglistit != funclistit->arguments)
+                        fprintf(out, ",");
+
+                    /* Print only variable name */
+                    variable = arglistit->arg + strlen(arglistit->arg) - 1;
+                    while ((variable >= arglistit->arg) &&
+                          (isalnum(*variable) || (*variable == '_')))
+                    {
+                        variable--;
+                    }
+                    fprintf(out, "%s", variable + 1);
+                }
+                fprintf(out, ")(");
+            
+                for (arglistit = funclistit->arguments;
+                     arglistit != NULL;
+                     arglistit = arglistit->next
+                )
+                {
+                    /* Print a , separator when not the first function argument */
+                    if (arglistit != funclistit->arguments)
+                        fprintf(out, ",");
+
+                    /* Print register name in lower case */
+                    for (lower = arglistit->reg; *lower != '\0'; lower++)
+                    {
+                        fputc(tolower(*lower), out);
+                    }
+                }
+                fprintf(out, ")\n");
+
+                lvo = funclistit->lvo;
+            }
+        }
+
+        fprintf(out, "##end\n");
+
+        if (ferror(out))
+        {
+            perror(line);
+            exit(20);
+        }
+
+        fclose(out);
     }
 }
