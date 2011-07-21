ce250be4e37691b9210b5f948fef195b3b12a329
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-07-21 17:03:24 +0000

    arosc.lib: assert() must generate no code if NDEBUG is defined
    This is required for POSIX.1-2001 conformance.
    
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40269 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/include/assert.h
+++ compiler/clib/include/assert.h
@@ -14,7 +14,10 @@
 #undef assert
 
 #ifdef NDEBUG
-#define assert(expr)	do { if (expr); } while (0)
+/* According to POSIX.1-2001, assert() must generate no code
+ * if NDEBUG is defined.
+ */
+#define assert(expr)	((void)0)
 #else
 #define assert(expr)	(((expr)) ? (void)0 : __assert(#expr,__FILE__,__LINE__))
 #endif
