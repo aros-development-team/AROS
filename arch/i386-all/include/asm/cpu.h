2f53be6658b719fc576b8aa230703badf809d163
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-07-22 07:07:04 +0000

    Added useful inline
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40279 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-all/include/asm/cpu.h
+++ arch/i386-all/include/asm/cpu.h
@@ -9,7 +9,19 @@
     Lang: english
 */
 
+#include <inttypes.h>
+
 /* This file is very very incomplete :) */
+
 #define HALT asm volatile("hlt")
+
+static inline void __attribute__((always_inline)) rdmsr(uint32_t msr_no, uint32_t *ret_lo, uint32_t *ret_hi)
+{
+    uint32_t ret1, ret2;
+
+    asm volatile("rdmsr":"=a"(ret1),"=d"(ret2):"c"(msr_no));
+    *ret_lo=ret1;
+    *ret_hi=ret2;
+}
 
 #endif
