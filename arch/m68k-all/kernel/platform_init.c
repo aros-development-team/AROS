d2a1c31c39639ca8b5bdb83ee753a0b8b14246a9
twilen <twilen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-07-22 17:38:42 +0000

    Implemented simple MMU support routines, can be used to debug illegal accesses.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40293 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/m68k-all/kernel/platform_init.c
@@ -0,0 +1,35 @@
+#define __KERNEL_NOLIBBASE__
+
+#include <aros/multiboot.h>
+#include <aros/symbolsets.h>
+#include <asm/cpu.h>
+#include <exec/lists.h>
+#include <proto/exec.h>
+
+#include <inttypes.h>
+
+#include "kernel_base.h"
+#include "kernel_debug.h"
+#include "kernel_intern.h"
+
+static int Platform_Init(struct KernelBase *LIBBASE)
+{
+    struct PlatformData *pd;
+
+    pd = AllocMem(sizeof(struct PlatformData), MEMF_PUBLIC|MEMF_CLEAR);
+    if (!pd)
+    	return FALSE;
+
+    LIBBASE->kb_PlatformData = pd;
+
+    if (SysBase->AttnFlags & AFF_68060)
+    	pd->mmu_type = MMU060;
+    else if (SysBase->AttnFlags & AFF_68040)
+    	pd->mmu_type = MMU040;
+    else if (SysBase->AttnFlags & AFF_68030)
+    	pd->mmu_type = MMU030;
+
+    return TRUE;
+}
+
+ADD2INITLIB(Platform_Init, 10)
---
+++ arch/m68k-all/kernel/setprotection.c
@@ -0,0 +1,54 @@
+#include <aros/kernel.h>
+#include <aros/libcall.h>
+
+#include <kernel_base.h>
+#include <kernel_intern.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/kernel.h>
+
+AROS_LH3I(void, KrnSetProtection,
+
+/*  SYNOPSIS */
+	AROS_LHA(void *, address, A0),
+	AROS_LHA(uint32_t, length, D0),
+        AROS_LHA(KRN_MapAttr, flags, D1),
+
+/*  LOCATION */
+	struct KernelBase *, KernelBase, 21, Kernel)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+******************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    BOOL invalid = (flags & (MAP_Readable | MAP_Writable | MAP_Executable)) == 0;
+    BOOL readonly = (flags & MAP_Writable) == 0;
+    UBYTE cm = (flags & MAP_CacheInhibit) ? CM_SERIALIZED : ((flags & MAP_WriteThrough) ? CM_WRITETHROUGH : CM_COPYBACK);
+    
+    /* We need MAP_Supervisor and MAP_CacheInhibitSerialized */
+    
+    if ((ULONG)address < 0x00200000 && cm == CM_SERIALIZED)
+	cm = CM_NONCACHEABLE; /* Chip RAM does not need to be non-cacheable + serialized, only noncacheable */
+    
+    map_region(KernelBase, address, NULL, length, invalid, readonly, FALSE, cm);
+
+    AROS_LIBFUNC_EXIT
+}
