ae11bc50b6fbc7773fc2d38e61cc91633ab170af
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-07-22 06:02:51 +0000

    Added KrnIsSuper() test
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40278 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/exec/supertest.c
+++ test/exec/supertest.c
@@ -7,6 +7,7 @@
 #include <exec/types.h>
 #include <proto/exec.h>
 #include <proto/dos.h>
+#include <proto/kernel.h>
 
 extern IPTR a(void);
 extern IPTR b(void);
@@ -61,25 +62,35 @@ int __nocommandline = 1;
 
 int main(void)
 {
+    APTR KernelBase = OpenResource("kernel.resource");
     struct Task *me = FindTask(NULL);
     APTR ssp;
     IPTR ar;
+    int issuper = 0;
 
-    Printf("GetCC()         : %04lx\n",GetCC());
-    Printf("SetSR()         : %08lx\n",SetSR(0,0));
+    Printf("GetCC()             : %04lx\n",GetCC());
+    Printf("SetSR()             : %08lx\n",SetSR(0,0));
+    
+    if (KernelBase)
+    	Printf("KrnIsSuper()        : %d\n", KrnIsSuper());
 
-    Printf("Task stack      : 0x%p - 0x%p\n", me->tc_SPLower, me->tc_SPUpper);
-    Printf("System stack    : 0x%p - 0x%p\n", SysBase->SysStkLower, SysBase->SysStkUpper);
+    Printf("Task stack          : 0x%p - 0x%p\n", me->tc_SPLower, me->tc_SPUpper);
+    Printf("Supervisor stack    : 0x%p - 0x%p\n", SysBase->SysStkLower, SysBase->SysStkUpper);
 
 #ifdef HAVE_ASM_CODE
     ssp = SuperState();
+
     ar = a();
+    if (KernelBase)
+    	issuper = KrnIsSuper();
+
     UserState(ssp);
 
-    Printf("Supervisor flags: 0x%p\n", ar);
-    Printf("User flags      : 0x%p\n", a());
-    Printf("Saved stack     : 0x%p\n", ssp);
-    Printf("Supervisor stack: 0x%p\n", Supervisor(b));
+    Printf("Supervisor mode test: %d\n", issuper);
+    Printf("Supervisor flags    : 0x%p\n", ar);
+    Printf("User flags          : 0x%p\n", a());
+    Printf("Saved stack         : 0x%p\n", ssp);
+    Printf("Supervisor stack    : 0x%p\n", Supervisor(b));
 #else
     Printf("This test is not implemented for this CPU\n");
 #endif
