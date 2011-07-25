17a6258a95c20ad2f953e6a74888241a8968b1ac
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-07-25 05:43:31 +0000

    Improved trap test, allows to check for illegal access at any specified address
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40312 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/exec/traptest.c
+++ test/exec/traptest.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()
+int main(int argc, char **argv)
 {
     int a, b, c;
-    int *ptr;
-    
-    a = 3;
-    b = 0;
-    printf("Attempting to divide by zero...\n");
-    c = a/b;
-    printf("Done, result is: %d!\n", c);
+    int do_illegal = 0;
+    int *ptr = (unsigned int *)0xABADC0DE;
 
-    printf("Trying illegal access now...\n");
-    ptr = (unsigned int *)0xABADC0DE;
+    if (argc > 1)
+    {
+    	if (!stricmp("illegal", argv[1]))
+    	{
+    	    do_illegal = 1;
+    	    if (argc > 2)
+    	    	ptr = (int *)atol(argv[2]);
+    	}
+    }
+
+    if (!do_illegal)
+    {
+    	a = 3;
+    	b = 0;
+	printf("Attempting to divide by zero...\n");
+	c = a/b;
+	printf("Done, result is: %d!\n", c);
+    }
+
+    printf("Trying illegal access at 0x%p now...\n", ptr);
     a = *ptr;
     printf("Done, result is: %d!\n", a);
 
