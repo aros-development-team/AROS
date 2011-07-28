a5653788b83eafcfcfc927134175301773e18892
Kalamatee <kalamatee@gmail.com>
2011-07-28 23:02:50 +0000

    Fix the AudioModes file.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40397 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/devs/AHI/Drivers/CMI8738/CMI8738.s
+++ workbench/devs/AHI/Drivers/CMI8738/CMI8738.s
@@ -7,62 +7,61 @@
 	
 	CHUNK_START	AUDM
 1:	
-	.long		AHIDB_AudioID,	0x00351001
-	.long		AHIDB_Volume,	TRUE
-	.long		AHIDB_Panning,	FALSE
-	.long		AHIDB_Stereo,	FALSE
-	.long		AHIDB_HiFi,	TRUE
-	.long		AHIDB_MultTable,FALSE
-	.long		AHIDB_Name,	2f-1b
-	.long		TAG_DONE
+	LONG2		AHIDB_AudioID,	0x00350001
+	LONG2		AHIDB_Volume,	TRUE
+	LONG2		AHIDB_Panning,	FALSE
+	LONG2		AHIDB_Stereo,	FALSE
+	LONG2		AHIDB_HiFi,	TRUE
+	LONG2		AHIDB_MultTable,FALSE
+	LONG2		AHIDB_Name,	2f-1b
+	LONG		TAG_DONE
 2:
 	.asciz		"CMI8738:HiFi 16 bit mono"
 	CHUNK_END
-
+		
 	CHUNK_START	AUDM
 1:	
-	.long		AHIDB_AudioID,	0x00351002
-	.long		AHIDB_Volume,	TRUE
-	.long		AHIDB_Panning,	TRUE
-	.long		AHIDB_Stereo,	TRUE
-	.long		AHIDB_HiFi,	TRUE
-	.long		AHIDB_MultTable,FALSE
-	.long		AHIDB_Name,	2f-1b
-	.long		TAG_DONE
+	LONG2		AHIDB_AudioID,	0x00350002
+	LONG2		AHIDB_Volume,	TRUE
+	LONG2		AHIDB_Panning,	TRUE
+	LONG2		AHIDB_Stereo,	TRUE
+	LONG2		AHIDB_HiFi,	TRUE
+	LONG2		AHIDB_MultTable,FALSE
+	LONG2		AHIDB_Name,	2f-1b
+	LONG		TAG_DONE
 2:
 	.asciz		"CMI8738:HiFi 16 bit stereo++"
 	CHUNK_END
 		
 	CHUNK_START	AUDM
 1:	
-	.long		AHIDB_AudioID,	0x00351003
-	.long		AHIDB_Volume,	TRUE
-	.long		AHIDB_Panning,	FALSE
-	.long		AHIDB_Stereo,	FALSE
-	.long		AHIDB_HiFi,	FALSE
-	.long		AHIDB_MultTable,FALSE
-	.long		AHIDB_Name,	2f-1b
-	.long		TAG_DONE
+	LONG2		AHIDB_AudioID,	0x00350003
+	LONG2		AHIDB_Volume,	TRUE
+	LONG2		AHIDB_Panning,	FALSE
+	LONG2		AHIDB_Stereo,	FALSE
+	LONG2		AHIDB_HiFi,	FALSE
+	LONG2		AHIDB_MultTable,FALSE
+	LONG2		AHIDB_Name,	2f-1b
+	LONG		TAG_DONE
 2:
 	.asciz		"CMI8738:16 bit mono"
 	CHUNK_END
-
+		
 	CHUNK_START	AUDM
 1:	
-	.long		AHIDB_AudioID,	0x00351004
-	.long		AHIDB_Volume,	TRUE
-	.long		AHIDB_Panning,	TRUE
-	.long		AHIDB_Stereo,	TRUE
-	.long		AHIDB_HiFi,	FALSE
-	.long		AHIDB_MultTable,FALSE
-	.long		AHIDB_Name,	2f-1b
-	.long		TAG_DONE
+	LONG2		AHIDB_AudioID,	0x00350004
+	LONG2		AHIDB_Volume,	TRUE
+	LONG2		AHIDB_Panning,	TRUE
+	LONG2		AHIDB_Stereo,	TRUE
+	LONG2		AHIDB_HiFi,	FALSE
+	LONG2		AHIDB_MultTable,FALSE
+	LONG2		AHIDB_Name,	2f-1b
+	LONG		TAG_DONE
 2:
 	.asciz		"CMI8738:16 bit stereo++"
 	CHUNK_END
 		
 	FORM_END
 
-	.balign	4,0
 	.END
 	
