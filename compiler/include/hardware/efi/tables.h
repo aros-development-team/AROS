b705a62c7596aeb24cef692146a086a398b0d82d
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-07-28 05:23:45 +0000

    Use correct calling convention for EFI services. Software power off now works.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40380 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/hardware/efi/tables.h
+++ compiler/include/hardware/efi/tables.h
@@ -1,7 +1,22 @@
 #ifndef HARDWARE_EFI_TABLES_H
 #define HARDWARE_EFI_TABLES_H
 
+/*
+    Copyright © 2011, The AROS Development Team. All rights reserved.
+    $Id: $
+
+    Desc: Basic definitions for EFI tables
+    Lang: english
+*/
+
 #include <exec/types.h>
+
+#ifdef __x86_64__
+/* On x86-64 EFI uses Microsoft calling convention */
+#define __eficall __attribute((ms_abi))
+#else
+#define __eficall
+#endif
 
 /* Common header of all tables */
 struct EFI_TableHeader
