95bf2f6c474448fbed60c7ccade9a40c2f58befe
twilen <twilen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-07-28 09:48:02 +0000

    KrnMapGlobal implemented.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40386 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/m68k-all/kernel/mapglobal.c
@@ -0,0 +1,50 @@
+#include <aros/kernel.h>
+#include <aros/libcall.h>
+
+#include <kernel_base.h>
+#include <kernel_intern.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/kernel.h>
+
+AROS_LH4I(int, KrnMapGlobal,
+
+/*  SYNOPSIS */
+	AROS_LHA(void *, virtual, A0),
+	AROS_LHA(void *, physical, A1),
+	AROS_LHA(uint32_t, length, D0),
+	AROS_LHA(KRN_MapAttr, flags, D1),
+
+/*  LOCATION */
+	struct KernelBase *, KernelBase, 16, Kernel)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+******************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    BOOL invalid = (flags & (MAP_Readable | MAP_Writable | MAP_Executable)) == 0;
+    BOOL readonly = (flags & MAP_Writable) == 0;
+    UBYTE cm = (flags & MAP_CacheInhibit) ? CM_SERIALIZED : ((flags & MAP_WriteThrough) ? CM_WRITETHROUGH : CM_COPYBACK);
+
+    map_region(KernelBase, virtual, physical, length, invalid, readonly, FALSE, cm);
+
+    AROS_LIBFUNC_EXIT
+}
