0cbcda3057e2dfb773a21847ee7617ab5c520117
twilen <twilen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-07-29 09:26:15 +0000

    Very minimal C:LoadWB added.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40407 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/c/LoadWB.c
@@ -0,0 +1,30 @@
+/*
+    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    $Id: LoadWB.c $
+
+    Desc:
+    Lang: English
+*/
+
+#define  DEBUG  0
+#include <aros/debug.h>
+
+#include <exec/types.h>
+#include <proto/exec.h>
+#include <workbench/workbench.h>
+#include <proto/workbench.h>
+
+#include <strings.h>
+#include <stdio.h>
+
+/* Very minimal C:LoadWB */
+
+int main(void)
+{
+    struct WorkbenchBase *WorkbenchBase = TaggedOpenLibrary(TAGGEDOPEN_WORKBENCH);
+    if (WorkbenchBase) {
+    	StartWorkbench(0, NULL);
+    	CloseLibrary((struct Library*)WorkbenchBase);
+    }
+    return 0;
+}
