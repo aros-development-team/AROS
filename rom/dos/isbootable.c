44d97deac64c9a756212dc0ee3d07650c7831c7f
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-07-29 10:44:17 +0000

    - Fixed warning on non-m68k about unused variable - It's not a wise idea to totally omit boot signature check on m68k
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40411 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/isbootable.c
+++ rom/dos/isbootable.c
@@ -20,81 +20,88 @@
 
 BOOL __dos_IsBootable(struct DosLibrary * DOSBase, BPTR lock)
 {
-    BOOL result = FALSE;
-
-    D(bug("[__dos_IsBootable] __dos_IsBootable('%s')\n", AROS_BOOT_CHECKSIG));
-    
 #if defined(AROS_BOOT_CHECKSIG)
     BPTR fh;
     STRPTR buffer;
     LONG bufferLength;
+    struct FileInfoBlock *abfile_fib;
+    BOOL result = FALSE;
+
+    D(bug("[__dos_IsBootable] __dos_IsBootable('%s')\n", AROS_BOOT_CHECKSIG));
 
     CurrentDir(lock);
     fh = Open(AROS_BOOT_CHECKSIG, MODE_OLDFILE);
-
-    if (fh)
+    if (!fh)
     {
-        LONG readsize;
-	struct FileInfoBlock *abfile_fib;
-
-	D(bug("[__dos_IsBootable] Opened succesfully\n"));
-
-	abfile_fib = AllocDosObject(DOS_FIB, NULL);
-	if (abfile_fib)
-	{
-    	    if (ExamineFH(fh, abfile_fib))
-    	    {
-            	bufferLength = abfile_fib->fib_Size + 1;
-
-        	buffer = AllocMem(bufferLength, MEMF_ANY);
-        	D(bug("[__dos_IsBootable] Allocated %d bytes for Buffer @ %p\n", bufferLength, buffer));
-
-		if (!buffer)
-        	{
-            	    Alert(AT_DeadEnd | AG_NoMemory | AN_DOSLib);
-        	}
-
-	        if ((readsize = Read(fh, buffer, (bufferLength - 1))) != -1)
-        	{
-            	    char *sigptr = NULL;
-
-            	    if (readsize != 0)
-            	    {
-                	buffer[readsize] = '\0';
-
-            	    	D(bug("[__dos_IsBootable] Buffer contains '%s'\n", buffer));
-            	    	if ((sigptr = strstr(buffer, AROS_CPU)) != 0)
-            	    	{
-                	    D(bug("[__dos_IsBootable] Signature '%s' found\n", AROS_CPU));
-                	    result = TRUE;
-                	}
-                    }
-            	}
-            }
-            FreeDosObject(DOS_FIB, abfile_fib);
-        }
-    	Close(fh);
+#ifdef __mc68000
+	/*
+	 * Original Amiga disks don't contain this signature. They are obviously bootable on m68k.
+	 * However if the disk DOES contain a signature, we should check it. This can happen to be
+	 * a disk for another architecture. Attempting to boot from it will cause crash.
+	 */
+    	return TRUE;
+#else
+	return FALSE;
+#endif
     }
+
+    D(bug("[__dos_IsBootable] Opened succesfully\n"));
+
+    abfile_fib = AllocDosObject(DOS_FIB, NULL);
+    if (abfile_fib)
+    {
+    	if (ExamineFH(fh, abfile_fib))
+    	{
+	    LONG readsize;
+
+            bufferLength = abfile_fib->fib_Size + 1;
+
+            buffer = AllocMem(bufferLength, MEMF_ANY);
+            D(bug("[__dos_IsBootable] Allocated %d bytes for Buffer @ %p\n", bufferLength, buffer));
+
+	    if (!buffer)
+            {
+            	Alert(AT_DeadEnd | AG_NoMemory | AN_DOSLib);
+            }
+
+	    readsize = Read(fh, buffer, bufferLength - 1);
+	    if (readsize > 0)
+            {
+            	char *sigptr = NULL;
+
+                buffer[readsize] = '\0';
+            	D(bug("[__dos_IsBootable] Buffer contains '%s'\n", buffer));
+
+            	if ((sigptr = strstr(buffer, AROS_CPU)) != 0)
+            	{
+                    D(bug("[__dos_IsBootable] Signature '%s' found\n", AROS_CPU));
+                    result = TRUE;
+                }
+            }
+        }
+        FreeDosObject(DOS_FIB, abfile_fib);
+    }
+
+    Close(fh);
+
+    D(bug("[__dos_IsBootable] returned %d\n", result));
+    return result;
 
 #elif defined(AROS_BOOT_CHECKEXEC)
     BPTR seg;
 
-    D(bug("[__dosboot_IsBootable]: Trying to load '%s' as an executable\n", AROS_BOOT_CHECKEXEC));
+    D(bug("[__dos_IsBootable]: Trying to load '%s' as an executable\n", AROS_BOOT_CHECKEXEC));
 
     CurrentDir(lock);
     if ((seg = LoadSeg(AROS_BOOT_CHECKEXEC)))
     {
-	D(bug("[DOSBoot] Success!\n"));
-
-    	result = TRUE;
     	UnLoadSeg(seg);
+
+	D(bug("[__dos_IsBootable] Success!\n"));	
+    	return TRUE;
     }
 
 #else
-    result = TRUE;
+    return TRUE;
 #endif
-
-    D(bug("[__dos_IsBootable] returned %d\n", result));
-
-    return result;
 }
