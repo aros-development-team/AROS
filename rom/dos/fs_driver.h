d3fe41b7af6fe385187551f0e78995f26f358491
ezrec <jason.mcmullan@gmail.com>
2011-08-01 01:48:37 +0000

    dos.library: Fix issue where 'cd :' always took you to SYS:
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40447 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/fs_driver.h
+++ rom/dos/fs_driver.h
@@ -8,7 +8,7 @@
 #define REF_DEVICE  1
 #define REF_CONSOLE 2
 
-LONG fs_LocateObject(BPTR *ret, BPTR parent, struct DevProc *dvp, CONST_STRPTR name, LONG accessMode, struct DosLibrary *DOSBase);
+LONG fs_LocateObject(BPTR *ret, struct MsgPort *port, BPTR parent, CONST_STRPTR name, LONG accessMode, struct DosLibrary *DOSBase);
 LONG fs_Open(struct FileHandle *fh, UBYTE reftype, BPTR ref, LONG accessMode, CONST_STRPTR name, struct DosLibrary *DOSBase);
 LONG fs_ReadLink(BPTR parent, struct DevProc *dvp, CONST_STRPTR path, STRPTR buffer, ULONG size, struct DosLibrary *DOSBase);
 LONG fs_ChangeSignal(BPTR handle, struct Process *task, struct DosLibrary *DOSBase);
