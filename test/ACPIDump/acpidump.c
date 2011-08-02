f8a44f436c4f3095879d48c2d00a743c82fd047f
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-08-02 06:51:48 +0000

    Updated. Dumps data only if --contents is specified.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40494 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/ACPIDump/acpidump.c
+++ test/ACPIDump/acpidump.c
@@ -11,12 +11,15 @@
 
 #include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
-static void dumpData(unsigned char * data, int length)
+static void dumpData(unsigned char *data, int length)
 {
       int len;
       int i, left = length;
-      while (left > 0) {
+
+      while (left > 0)
+      {
             len = printf("%04x:", length - left);
             for (i = 0; i < 16 && i < left; i++) {
                       printf(" %02x", data[i]);
@@ -33,7 +36,14 @@ static void dumpData(unsigned char * data, int length)
       }
 }
 
-void parseFADT(struct ACPI_TABLE_TYPE_FADT *fadt)
+static void dumpTable(struct ACPI_TABLE_DEF_HEADER *table, BOOL verbose)
+{
+     printf("%4.4s: 0x%p (%u bytes)\n", (char *)&table->signature, table, table->length);
+     if (verbose)
+         dumpData((unsigned char *)table, table->length);
+}
+
+static void parseFADT(struct ACPI_TABLE_TYPE_FADT *fadt, BOOL verbose)
 {
     unsigned char sum = 0;
     unsigned char *j, *k;
@@ -52,13 +62,15 @@ void parseFADT(struct ACPI_TABLE_TYPE_FADT *fadt)
             {
                 j = (unsigned char*)(IPTR)fadt->dsdt_addr;
                 printf("ACPI table %c%c%c%c %08X\n", j[0], j[1], j[2], j[3], fadt->dsdt_addr);
-                dumpData(j, ((struct ACPI_TABLE_DEF_HEADER*)j)->length);
+                if (verbose)
+                    dumpData(j, ((struct ACPI_TABLE_DEF_HEADER*)j)->length);
             }
             
             /* FACS don't have checksum */
             j = (unsigned char*)(IPTR)fadt->facs_addr;
             printf("ACPI table %c%c%c%c %08X\n", j[0], j[1], j[2], j[3], fadt->facs_addr);
-            dumpData(j, ((struct ACPI_TABLE_DEF_HEADER*)j)->length);
+            if (verbose)
+            	dumpData(j, ((struct ACPI_TABLE_DEF_HEADER*)j)->length);
         }
     }else
     {
@@ -68,155 +80,44 @@ void parseFADT(struct ACPI_TABLE_TYPE_FADT *fadt)
 
 }
 
-void parseRSDP(struct ACPI_TABLE_TYPE_RSDP *rsdp)
-{
-    struct ACPI2_TABLE_TYPE_RSDP *xsdp;
-    struct ACPI_TABLE_TYPE_RSDT *rsdt;
-    IPTR xsdt;
-
-    unsigned char sum;
-    unsigned char *j, *k;
-    unsigned int i;
-    int num;
-    char *offset;
-
-    xsdp = (struct ACPI2_TABLE_TYPE_RSDP*)rsdp;
-    if (xsdp->revision > 1 && xsdp->xsdt_address)
-    {
-        UQUAD address;
-
-        printf("ACPI is using XSDT table.\n");
-        xsdt = (IPTR)xsdp->xsdt_address;
-
-        printf("ACPI table XSDT %08X\n", (unsigned int)xsdt);
-        
-        sum = 0;
-
-        j = (unsigned char*)xsdt;
-        k = j + ((struct ACPI_TABLE_DEF_HEADER*)j)->length;
- 
-        /* checksuming, sum shall be zero here */
-        for (; j < k; sum += *(j++));  
- 
-        if(!sum)
-        {
-            j = (unsigned char*)xsdt;
-            dumpData(j, ((struct ACPI_TABLE_DEF_HEADER*)j)->length);
-
-            
-            /* how many table do we have ? */
-            num = (((struct ACPI_TABLE_DEF_HEADER *)xsdt)->length - sizeof(struct ACPI_TABLE_DEF_HEADER)) / sizeof(UQUAD);
-
-            /* loop through all the tables ptr */
-            offset = (char*)xsdt + sizeof(struct ACPI_TABLE_DEF_HEADER);
-            for(i=0 ; i < num; i++, offset += sizeof(UQUAD))
-            {
-                address = (UQUAD)(*(UQUAD*)offset);
-                if(!address) continue;
-                j = (unsigned char*)address;
-                k = j + ((struct ACPI_TABLE_DEF_HEADER*)j)->length;
-                
-                /* checksuming, sum shall be zero here */
-                for (; j < k; sum += *(j++));   
-                if(!sum)
-                {
-                    j = (unsigned char*)address;
-                    
-                    printf("ACPI table %c%c%c%c %08X\n",j[0], j[1], j[2], j[3],(unsigned int)address);
-                    dumpData(j, ((struct ACPI_TABLE_DEF_HEADER*)j)->length);
-                    if(j[0] == 'F' && j[1] == 'A' && j[2] == 'C' && j[3] == 'P') parseFADT((struct ACPI_TABLE_TYPE_FADT*)address);
-
-                } 
-                else
-                {
-                    
-                    /* reset sum for next checksum */
-                    sum = 0;    
-                    printf("ACPI table have wrong checksum !\n");
-                }
-            }        
-        }
-        else
-        {
-            printf("RSDT wrong checksum!\n");
-        }
-
- 
-    } /* XSDT END */
-    else
-    {  
-        
-        /* ACPI is using RSDT table */
-        unsigned long address;
-        
-        rsdt = (void *)(IPTR)rsdp->rsdt_address;
-
-        printf("ACPI table RSDT 0x%p\n", rsdt);
-        
-        sum = 0;
-
-        j = (unsigned char*)rsdt;
-        k = j + ((struct ACPI_TABLE_DEF_HEADER*)j)->length;
- 
-        /* checksuming, sum shall be zero here */
-        for (; j < k; sum += *(j++));   
- 
-        if(!sum)
-        {
-            j = (unsigned char*)rsdt;
-            dumpData(j, ((struct ACPI_TABLE_DEF_HEADER*)j)->length);
-
-            /* how many table do we have ? */
-            num = (((struct ACPI_TABLE_DEF_HEADER *)rsdt)->length - sizeof(struct ACPI_TABLE_DEF_HEADER)) / sizeof(unsigned int);
- 
-            /* loop through all the tables ptr*/
-            offset = (char*)rsdt + sizeof(struct ACPI_TABLE_DEF_HEADER);
-            for(i=0 ; i < num; i++, offset += sizeof(unsigned long))
-            {
-                address = (unsigned long)(*(unsigned long*)offset);
-                if(!address) continue;
-                j = (unsigned char*)address;
-                k = j + ((struct ACPI_TABLE_DEF_HEADER*)j)->length;
-                
-                /* checksuming, sum shall be zero here */
-                for (; j < k; sum += *(j++));   
-                if(!sum)
-                {
-                    j = (unsigned char*)address;
-                    
-                    printf("ACPI table %c%c%c%c %08X\n",j[0], j[1], j[2], j[3],(unsigned int)address);
-                    dumpData(j, ((struct ACPI_TABLE_DEF_HEADER*)j)->length);
-                    if(j[0] == 'F' && j[1] == 'A' && j[2] == 'C' && j[3] == 'P') parseFADT((struct ACPI_TABLE_TYPE_FADT*)address);
-
-                } 
-                else
-                {
-                    
-                    /* reset sum for next checksum */
-                    sum = 0;    
-                    printf("ACPI table have wrong checksum !\n");
-                }
-            }      
-        }
-        else
-        {
-            printf("RSDT wrong checksum!\n");
-        }
-    }
-}
-
 int main(int argc, char **argv)
 {
+    int i;
     struct ACPIBase *ACPIBase = OpenResource("acpi.resource");
-    
+    struct ACPI_TABLE_TYPE_FADT *fadt = NULL;
+    BOOL dump = 0;
+
     if (!ACPIBase)
     {
         printf("acpi.resource not found, no ACPI!\n");
         return 0;
     }
-    
+
+    if (argc > 1)
+    	dump = !stricmp(argv[1], "--contents");
+
+    printf("Root tables:\n");
     printf("RSD PTR : 0x%p\n", ACPIBase->ACPIB_RSDP_Addr);
-    parseRSDP(ACPIBase->ACPIB_RSDP_Addr);
+    
+    if (dump)
+    	dumpData((unsigned char *)ACPIBase->ACPIB_RSDP_Addr, (ACPIBase->ACPIB_RSDP_Addr->revision < 2) ? 20 : 36);
+
+    dumpTable(ACPIBase->ACPIB_SDT_Addr, dump);
+
+    printf("Top-level system description tables:\n");
+    for (i = 0; i < ACPIBase->ACPIB_SDT_Count; i++)
+    {
+    	dumpTable(ACPIBase->ACPIB_SDT_Entry[i], dump);
+
+    	if (ACPIBase->ACPIB_SDT_Entry[i]->signature == ACPI_MAKE_ID('F', 'A', 'C', 'P'))
+    	    fadt = (struct ACPI_TABLE_TYPE_FADT *)ACPIBase->ACPIB_SDT_Entry[i];
+    }
+
+    if (fadt)
+    {
+    	printf("FADT tables:\n");
+    	parseFADT(fadt, dump);
+    }
 
     return 0;
 } 
