740ddcd11ee535d6d0dcff865cef13dee8120ca4
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-08-08 07:18:00 +0000

    New KrnDisplayAlert() function for displaying critical errors
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40585 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/systemalert.c
+++ rom/exec/systemalert.c
@@ -1,133 +1,47 @@
 /*
-    Copyright © 1995-2010, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2011, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: Display an alert in supervisor mode.
     Lang: english
-
-    This function can be replaced with architecture-specific implementation.
-    It is called in Disable()d state and can do whatever it wants. Also it
-    can wait for some input, if possible.
-    One limitation: it must work no matter what. It's not adviced to Enable()
-    here because of this.
 */
 
+#include <aros/debug.h>
 #include <exec/execbase.h>
-#include <proto/exec.h>
+#include <proto/kernel.h>
 
 #include "exec_intern.h"
 #include "exec_util.h"
 
-#define ALERT_WIDTH 80
-
-static inline void PrintChars(char c, ULONG n, struct ExecBase *SysBase)
-{
-    while (n--)
-        RawPutChar(c);
-}
-
-static inline int linelen(char *str)
-{
-    int l;
-
-    for (l = 0; str[l] && str[l] != '\n'; l++);
-    return l;
-}
-
-static char *PrintCentered(char *str, struct ExecBase *SysBase)
-{
-    int len = linelen(str);
-    int i;
-    ULONG s;
-
-    if (len > (ALERT_WIDTH - 2))
-    	    len = (ALERT_WIDTH - 2);
-
-    s = ALERT_WIDTH - 2 - len;
-    
-    RawPutChar('#');
-    if (s & 1)
-        RawPutChar(' ');
-    s >>= 1;
-    PrintChars(' ', s, SysBase);
-    for (i = 0; i < len; i++)
-        RawPutChar(str[i]);
-    PrintChars(' ', s, SysBase);
-    RawPutChar('#');
-    RawPutChar('\n');
-
-    return &str[len];
-}
-
-static char *PrintLeftJustified(char *str, struct ExecBase *SysBase)
-{
-    int len = linelen(str);
-    int i;
-    ULONG s;
-
-    if (len > (ALERT_WIDTH - 3))
-    	    len = (ALERT_WIDTH - 3);
-
-    s = ALERT_WIDTH - 3 - len;
-
-    RawPutChar('#');
-    RawPutChar(' ');
-    for (i = 0; i < len; i++)
-        RawPutChar(str[i]);
-    PrintChars(' ', s, SysBase);
-    RawPutChar('#');
-    RawPutChar('\n');
-
-    return &str[len];
-}
-
-static inline void PrintFrame(struct ExecBase *SysBase)
-{
-    PrintChars('#', ALERT_WIDTH, SysBase);
-    RawPutChar('\n');
-}
-
 /*
- * Print alert to the debug output.
- * In future we should have more intelligent handling for such a case. For
- * example we should report what was wrong after we rebooted.
+ * Display an alert via kernel.resource. This is called in a critical, hardly recoverable condition.
+ * Interrupts and multitasking are disabled here.
  *
  * Note that we use shared buffer in SysBase for alert text.
  */
 void Exec_SystemAlert(ULONG alertNum, APTR location, APTR stack, UBYTE type, APTR data, struct ExecBase *SysBase)
 {
     char *buf;
+    char *extra;
 
-    RawPutChar('\n');
-    PrintFrame(SysBase);
+    D(bug("[SystemAlert] Code 0x%08X, type %d, data 0x%p\n", alertNum, type, data));
 
-    /* Print alert title centered */
-    PrintCentered(Alert_GetTitle(alertNum), SysBase);
+    /* Get the title */
+    buf = Alert_AddString(PrivExecBase(SysBase)->AlertBuffer, Alert_GetTitle(alertNum));
+    *buf++ = '\n';
+
+    D(bug("[SystemAlert] Got title: %s\n", PrivExecBase(SysBase)->AlertBuffer));
 
     /* Get the alert text */
-    FormatAlert(PrivExecBase(SysBase)->AlertBuffer, alertNum, SysBase->ThisTask, location, type, SysBase);
+    buf = FormatAlert(buf, alertNum, SysBase->ThisTask, location, type, SysBase);
+    extra = buf;
 
-    /* Print first two lines (task and error) centered */
-    buf = PrintCentered(PrivExecBase(SysBase)->AlertBuffer, SysBase);
-    buf = PrintCentered(buf + 1, SysBase);
+    FormatAlertExtra(buf, stack, type, data, SysBase);
+    /* FormatAlertExtra() output begins with LF, we replace it with FF in order to close the frame before extra */
+    *extra = 0x0F;
 
-    /* Empty line */
-    PrintCentered("", SysBase);
+    D(bug("[SystemAlert] Got extra:\n%s\n", extra + 1));
 
-    /* The rest is left-justified */
-    while (*buf)
-    	buf = PrintLeftJustified(buf + 1, SysBase);
-
-    PrintFrame(SysBase);
-
-    FormatAlertExtra(PrivExecBase(SysBase)->AlertBuffer, stack, type, data, SysBase);
-
-    /* Skip the leading '\n' provided by FormatAlertExtra() */
-    buf = &PrivExecBase(SysBase)->AlertBuffer[1];
-    while (*buf)
-	RawPutChar(*buf++);
-
-    RawPutChar('\n');
-    PrintFrame(SysBase);
-    RawPutChar('\n');
+    /* Display an alert via kernel.resource */
+    KrnDisplayAlert(alertNum, PrivExecBase(SysBase)->AlertBuffer);
 }
