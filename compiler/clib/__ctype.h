da68c848fe1a025967c935400d05959326b719cf
ezrec <jason.mcmullan@gmail.com>
2011-08-09 14:54:07 +0000

    Revert "arosc.library: Reduce the size of the ctype toupper and tolower tables by a factor of 4."
    This reverts commit r40607 - something is seriously wrong
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40626 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/__ctype.h
+++ compiler/clib/__ctype.h
@@ -2,7 +2,7 @@
 #define ___CTYPE_H
 
 extern const unsigned short int __ctype_b_array[384];
-extern const unsigned char __ctype_toupper_array[384];
-extern const unsigned char __ctype_tolower_array[384];
+extern const int __ctype_toupper_array[384];
+extern const int __ctype_tolower_array[384];
 
 #endif /* !___CTYPE_H */
