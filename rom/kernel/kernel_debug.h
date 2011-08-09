e25404a8618aa190bbc65fc75364a6fc07b76c62
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-08-09 10:47:34 +0000

    Removed absolutely unneeded __KrnBugBoot() definition. _bug() is a direct call, it's safe to use with KernelBase == NULL.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40613 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/kernel/kernel_debug.h
+++ rom/kernel/kernel_debug.h
@@ -25,17 +25,17 @@ static inline void _bug(struct KernelBase *KernelBase, const char *format, ...)
 
     va_start(args, format);
 
-    /* KernelBase can be NULL, use __KrnBugBoot if it is */
-    if (KernelBase != NULL) {
-    	/* We use AROS_CALL2 here, since there are files that
-    	 * include this that cannot tolerate <proto/kernel.h>
-    	 */
-    	AROS_CALL2(int, AROS_SLIB_ENTRY(KrnBug, Kernel, 12),
+    /* 
+     * We use AROS_CALL2 here, since there are files that
+     * include this that cannot tolerate <proto/kernel.h>
+     * Note that this is a direct call, not using an LVO. This is done
+     * in such a manner because this function can be user during early boot,
+     * while KernelBase is NULL.
+     */
+    AROS_CALL2(int, AROS_SLIB_ENTRY(KrnBug, Kernel, 12),
     		AROS_LCA(const char *, format, A0),
     		AROS_LCA(va_list, args, A1),
     		struct KernelBase *, KernelBase);
-    } else
-        __KrnBugBoot(format, args);
 
     va_end(args);
 }
