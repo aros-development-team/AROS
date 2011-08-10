bda8aa734aa70f642687aa26e7e2d1dd0c1b2d3d
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-08-10 06:47:05 +0000

    - Fixed alignment on 64 bits (Next segment pointer immegiately follows an ULONG size) - Always clear instruction cache at a given address, seems to have fixed random crashes on ARM. Depencency on __USE_FULL_JUMPVEC was a bug, it relates to library vectors only.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40639 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/arossupport/createseglist.c
+++ compiler/arossupport/createseglist.c
@@ -2,10 +2,18 @@
     Copyright © 2011, The AROS Development Team. All rights reserved.
     $Id$
 
-    Desc: Low-level debugging support.
+    Desc: Create a seglist for ROM code.
 */
 
+#include <aros/debug.h>
 #include <proto/exec.h>
+
+struct segment
+{
+    ULONG Size;	/* Length of segment in # of bytes */
+    IPTR  Next;	/* Next segment (always 0 for this) */
+    struct FullJumpVec Code;	/* Code to jump to the offset */
+} __attribute__((packed));
 
 /*****************************************************************************
 
@@ -43,22 +51,19 @@
 
 *****************************************************************************/
 {
-    struct {
-	ULONG Size;	/* Length of segment in # of bytes */
-	IPTR  Next;	/* Next segment (always 0 for this) */
-	struct FullJumpVec Code;	/* Code to jump to the offset */
-    } *segtmp;
+    struct segment *segtmp;
 
-    segtmp = AllocMem(sizeof(*segtmp), MEMF_CLEAR);
+    segtmp = AllocMem(sizeof(*segtmp), MEMF_ANY);
     if (!segtmp)
     	return BNULL;
 
     segtmp->Size = sizeof(*segtmp);
     segtmp->Next = (IPTR)0;
     __AROS_SET_FULLJMP(&segtmp->Code, function);
-#ifdef __AROS_USE_FULLJMP
+
     CacheClearE(&segtmp->Code, sizeof(struct FullJumpVec), CACRF_ClearI | CACRF_ClearD);
-#endif
+
+    D(bug("[CreateSegList] Created jump segment 0x%p, code 0x%p, target 0x%p\n", MKBADDR(&segtmp->Next), &segtmp->Code, function));
 
     return MKBADDR(&segtmp->Next);
 }
