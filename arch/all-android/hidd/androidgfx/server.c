ab11ecd6ae1afd7620486c3954d5139c834ff7bd
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-08-17 07:47:21 +0000

    - Rewritten server procotol once more. Now back- and forth-compatible in both directions, number of arguments is always sent. - Implemented input events on server side.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40753 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-android/hidd/androidgfx/server.c
+++ arch/all-android/hidd/androidgfx/server.c
@@ -1,5 +1,4 @@
 #define DEBUG 1
-#define CHECK_CONSISTENCY
 
 #include <aros/debug.h>
 #include <exec/alerts.h>
@@ -15,6 +14,15 @@
 #undef XSD
 #define XSD(x) ((struct agfx_staticdata *)x)
 
+/*
+ * WARNING!
+ * In this code every pipe error causes instant shutdown.
+ * This is intentional because error likely means broken pipe. This can happen
+ * if the parent process (bootstrap) exited. In this case we should immediately exit too,
+ * otherwise we end up in AROS process running out of any control. On non-rooted phone
+ * it can be shut down only by rebooting the phone.
+ */
+
 static void ReadPipe(int pipe, void *data, int len, struct agfx_staticdata *xsd)
 {
     int res;
@@ -24,30 +32,8 @@ static void ReadPipe(int pipe, void *data, int len, struct agfx_staticdata *xsd)
     if (res != len)
     {
 	D(bug("[AGFX.server] Error reading pipe, result %d, error %d\n", res, err));
-
-    	/* This likely means broken pipe. Our server is dead, we should die too. */
-    	ShutdownA(SD_ACTION_POWEROFF);
+ 	ShutdownA(SD_ACTION_POWEROFF);
     }
-}
-
-/*
- * This arrays specifies reply lengths for commands.
- * First ULONG (command echo) also counts here. Zero value means we don't need any reply at all.
- */
-static const int ReplyLength[] = 
-{
-    0,
-    3,	/* cmd_Query	*/
-    1,	/* cmd_Show	*/
-    0,	/* cmd_Update	*/
-    0	/* cmd_Scroll	*/
-};
-
-static inline void ReplyRequest(struct Request *request)
-{
-    DB2(bug("[AGFX.server] Replying request 0x%p\n", request));
-
-    Signal((struct Task *)request->owner, request->signal);
 }
 
 void agfxInt(int pipe, int mode, void *data)
@@ -56,98 +42,107 @@ void agfxInt(int pipe, int mode, void *data)
 
     if (mode & vHidd_UnixIO_Error)
     {
-    	/* This likely means broken pipe. Our server is dead, we should die too. */
     	D(bug("[AGFX.server] Error condition on input pipe\n"));
-
     	ShutdownA(SD_ACTION_POWEROFF);
     }
 
     if (mode & vHidd_UnixIO_Read)
     {
-    	ULONG cmd;
+    	struct Request header;
     	ULONG status = STATUS_ACK;
-    	int reply_len;
 
-	ReadPipe(pipe, &cmd, sizeof(cmd), data);
-    	DB2(bug("[AGFX.server] Command 0x%08X from server\n", cmd));
+	/* First read packet header */
+	ReadPipe(pipe, &header, sizeof(header), data);
+    	DB2(bug("[AGFX.server] Command 0x%08X with %u parameters from server\n", header.cmd, header.len));
 
-	if (cmd == cmd_Nak)
+	if (header.cmd == cmd_Nak)
 	{
-	    ReadPipe(pipe, &cmd, sizeof(cmd), data);
-	    D(bug("[AGFX.server] NAK %d received\n", cmd));
+	    /*
+	     * Special handling for NAK reply.
+	     * We know it has one parameter - original command.
+	     */
+	    ReadPipe(pipe, &header.cmd, sizeof(ULONG), data);
 
+	    D(bug("[AGFX.server] NAK %d received\n", header.cmd));
 	    status = STATUS_NAK;
 	}
 
-	reply_len = ReplyLength[cmd];
-	if (reply_len)
+	if (header.cmd & CMD_NEED_REPLY)
 	{
-	    /* If CMDF_QUICK is not set, the command needs reply */
-    	    struct Request *request = (struct Request *)RemHead((struct List *)&XSD(data)->waitQueue);
+	    /* A WaitRequest is being replied */
+	    if (!IsListEmpty(&XSD(data)->waitQueue))
+	    {
+	    	struct WaitRequest *request = (struct WaitRequest *)XSD(data)->waitQueue.mlh_Head;
 
-#ifdef CHECK_CONSISTENCY
-	    if (!request)
-	    {
-		bug("[AGFX.server] Bogus response 0x%08X from server, no pending request!\n", cmd);
-		Alert(AT_DeadEnd | AN_AsyncPkt);
-	    }
-	    if (cmd != request->cmd)
-	    {
-		bug("[AGFX.server] Got response 0x%08X for command 0x%08X from server!\n", cmd, request->cmd);
-		Alert(AT_DeadEnd | AN_AsyncPkt);
-	    }
-#endif
-	    /* One ULONG has already been read */
-	    if (--reply_len && (status == STATUS_ACK))
-	    {
-	    	ULONG *cmd = &request->cmd;
+	    	if (request->cmd == header.cmd)
+	    	{
+	    	    if (header.len && (status == STATUS_ACK))
+	    	    {
+	    	    	ULONG *pkt = &request->cmd;
 
-    	        ReadPipe(pipe, &cmd[request->len + 2], reply_len * sizeof(ULONG), data);
+	    	    	/* Read reply parameters */
+    	            	ReadPipe(pipe, &pkt[request->len + 2], header.len * sizeof(ULONG), data);
+    	            }
+
+    	    	    DB2(bug("[AGFX.server] Replying request 0x%p\n", request));
+
+		    Remove((struct Node *)request);
+    	    	    request->status = status;
+		    Signal(request->owner, request->signal);
+
+		    /* We have read the data, exit */
+		    return;
+    	    	}
+    	    	    D(else bug("[AGFX.server] Bogus reply 0x%08X for request 0x%08X\n", header.cmd, request->cmd);)
     	    }
-
-    	    request->status = status;
-    	    ReplyRequest(request);
+    	    	D(else bug("[AGFX.server] Bogus reply 0x%08X without a request\n", header.cmd);)
     	}
+
+	/*
+	 * If we are here, we haven't read the data portion.
+	 * This is either unknown command or bogus response.
+	 * We don't know what to do with arguments, so just swallow them.
+	 */
+    	for (status = 0; status < header.len; status++)
+    	    ReadPipe(pipe, &header.cmd, sizeof(ULONG), data);
     }
 }
 
-void DoRequest(struct Request *req, struct agfx_staticdata *xsd)
+void SendRequest(struct Request *req, struct agfx_staticdata *xsd)
 {
-    int len, res, err;
-    ULONG need_reply = ReplyLength[req->cmd];
+    /* Total packet length is header plus specified number of ULONG parameters */
+    int len = sizeof(struct Request) + req->len * sizeof(ULONG);
+    int res, err;
 
-    /* If ReplyLength for this command is not zero, we need to wait for reply */
-    if (need_reply)
-    {
-    	/*
-     	 * Set request owner.
-     	 * Limitation: one task can have only one request pending. But
-     	 * it's faster than CreateMsgPort() every time.
-     	 */
-    	req->owner  = FindTask(NULL);
-    	req->signal = SIGF_BLIT;
-
-	/* Add the request to the wait queue */
-    	Disable();
-    	AddTail((struct List *)&xsd->waitQueue, (struct Node *)req);
-    	Enable();
-
-    	SetSignal(0, SIGF_BLIT);
-    }
-
-    len = (req->len + 2) * sizeof(ULONG);
-    res = Hidd_UnixIO_WriteFile(xsd->unixio, xsd->DisplayPipe, &req->cmd, len, &err);
+    res = Hidd_UnixIO_WriteFile(xsd->unixio, xsd->DisplayPipe, req, len, &err);
     if (res != len)
     {
         D(bug("[AGFX.server] Error writing pipe, result %d, error %d\n", res, err));
-
-    	/* This likely means broken pipe. Our server is dead, we should die too. */
     	ShutdownA(SD_ACTION_POWEROFF);
     }
+}
 
-    if (need_reply)
-    {
-    	/* Wait for reply */
-    	Wait(SIGF_BLIT);
-    }
+void DoRequest(struct WaitRequest *req, struct agfx_staticdata *xsd)
+{
+    /*
+     * Set request owner.
+     * Limitation: one task can have only one request pending. But
+     * it's faster than CreateMsgPort() every time.
+     */
+    req->owner  = FindTask(NULL);
+    req->signal = SIGF_BLIT;
+
+    /* Add the request to the wait queue */
+    Disable();
+    AddTail((struct List *)&xsd->waitQueue, (struct Node *)req);
+    Enable();
+
+    /* Make sure the signal isn't already set */
+    SetSignal(0, SIGF_BLIT);
+
+    /* Actually send the request */
+    SendRequest((struct Request *)&req->cmd, xsd);
+
+    /* Wait for reply */
+    Wait(SIGF_BLIT);
 }
--- arch/all-android/hidd/androidgfx/server.h
+++ arch/all-android/hidd/androidgfx/server.h
@@ -1,18 +1,16 @@
 #define cmd_Nak	       -1
-#define cmd_Query	1
-#define cmd_Show	2
-#define cmd_Update	3
-#define cmd_Scroll	4
+#define cmd_Query      0x80000001
+#define cmd_Show       0x80000002
+#define cmd_Update     0x00000003
+#define cmd_Scroll     0x00000004
+
+#define CMD_NEED_REPLY 0x80000000
 
 #define STATUS_ACK	0
 #define STATUS_NAK	1
 
 /*
- * Requests are generally linear data blocks consisting of ULONGs.
- * This makes it simpler to parse them in Java code.
- * Data starting from 'cmd' is sent to the server. The reply will be
- * stored directly after sent portion.
- * Reply length is determined by command value using hardcoded table.
+ * Display server data packets consist of ULONGs. This makes it simpler to parse them in Java code.
  *
  * Yes, this is not 64-bit-safe. But:
  * a) Android isn't going to move to 64-bit machines for now.
@@ -20,44 +18,65 @@
  *    or 64 bits (long). And we don't want to cope with UQUADs here.
  * If support for 64 bits is ever needed, perhaps the best way is to make 64-bit
  * versions of some commands (which require a pointer), which use two ULONGs for it.
+ * For this purpose we store addresses at the end of packets. It will be possible to determine
+ * 64-bit versions just by increased length.
  */
 
+/*
+ * This is raw packet header.
+ * It is used for both requests and responses.
+ */
 struct Request
+{
+    ULONG cmd;			/* Command code		*/
+    ULONG len;			/* Number of parameters	*/
+    /* ULONG parameters follow */
+};
+
+/* This structure is used to query a request which needs a response.*/
+struct WaitRequest
 {
     struct MinNode  node;
     struct Task    *owner;
     ULONG	    signal;	/* Reply signal		*/
     ULONG	    status;	/* Delivery status	*/
-    ULONG	    cmd;	/* Command		*/
-    ULONG	    len;	/* Number of parameters */
-    /* ULONG parameters follow */
+    /* Encapsulated packet header */
+    ULONG	    cmd;
+    ULONG	    len;
 };
+
+/* Partucilar forms of command requests */
 
 /* Query display size */
 struct QueryRequest
 {
-    struct Request req;		/* cmd_Query, 1				*/
+    struct WaitRequest req;		/* cmd_Query, 1				*/
     /* Request parameters */
-    ULONG	   id;		/* Display ID, currently always zero	*/
+    ULONG	       id;		/* Display ID, currently always zero	*/
     /* Response data (8 bytes) */
-    ULONG	   width;
-    ULONG	   height;
+    ULONG	       width;
+    ULONG	       height;
 };
 
 /* Show a single bitmap on display */
 struct ShowRequest
 {
-    struct Request req;		/* cmd_Show, 7				*/
+    struct WaitRequest req;		/* cmd_Show, 7				*/
     /* Request */
-    ULONG	   displayid;	/* DisplayID, currently always zero	*/
-    ULONG	   left;	/* Offset				*/
-    ULONG	   top;
-    ULONG	   width;	/* Size					*/
-    ULONG	   height;
-    ULONG	   mod;		/* Bytes per line			*/
-    ULONG	   addr;	/* Start address			*/
+    ULONG	       displayid;	/* DisplayID, currently always zero	*/
+    ULONG	       left;		/* Offset				*/
+    ULONG	       top;
+    ULONG	       width;		/* Size					*/
+    ULONG	       height;
+    ULONG	       mod;		/* Bytes per line			*/
+    ULONG	       addr;		/* Start address			*/
     /* Response has no data, only echo */
 };
+
+/*
+ * The following requests don't use WaitRequest because they don't need to be replied.
+ * They are used with SendRequest() function.
+ */
 
 /* Update screen region */
 struct UpdateRequest
@@ -69,9 +88,9 @@ struct UpdateRequest
     ULONG	   top;
     ULONG	   right;
     ULONG	   bottom;
-    /* No response needed */
 };
 
+/* Scroll a bitmap */
 struct ScrollRequest
 {
     struct Request req;		/* cmd_Scroll, 3			*/
@@ -79,8 +98,8 @@ struct ScrollRequest
     ULONG	   id;		/* Bitmap ID, reserved			*/
     ULONG	   left;	/* New offset				*/
     ULONG	   top;
-    /* No response needed */
 };
 
 void agfxInt(int pipe, int mode, void *data);
-void DoRequest(struct Request *req, struct agfx_staticdata *xsd);
+void SendRequest(struct Request *req, struct agfx_staticdata *xsd);
+void DoRequest(struct WaitRequest *req, struct agfx_staticdata *xsd);
