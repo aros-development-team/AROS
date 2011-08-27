138268787258f39b86c57a45e4d7e9c8578513f4
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-08-27 08:05:58 +0000

    MorphOS compatibility check. NewRawDoFmt() must not mandate 'l' modifiers for ULONGs.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40902 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/exec/rawdofmt.c
+++ test/exec/rawdofmt.c
@@ -9,8 +9,9 @@ int main(void)
 {
     char buf[256];
     ULONG count = 0;
-    IPTR args[] = {
-        (IPTR)"one",
+    IPTR args[] =
+    {
+	(IPTR)"one",
 	(IPTR)"two",
 	3,
 	4,
@@ -18,21 +19,31 @@ int main(void)
 	6,
     };
 
+    /*
+     * NewRawDoFmt() should not need 'l' modifier for ULONGs.
+     * This is verified under MorphOS.
+     */
     printf("Checking NewRawDoFmt...\n");
-    NewRawDoFmt("%s plus %s will be %lu, next are %lu, %lu, %lu", (VOID_FUNC)RAWFMTFUNC_COUNT, &count, "one", "two", 3, 4, 5, 6);
+    NewRawDoFmt("%s plus %s will be %u, next are %u, %u, %u", (APTR)RAWFMTFUNC_COUNT, &count, "one", "two", 3, 4, 5, 6);
     printf("Count is %u\n", (unsigned)count);
-    NewRawDoFmt("%s plus %s will be %lu, next are %lu, %lu, %lu", (VOID_FUNC)RAWFMTFUNC_STRING, buf, "one", "two", 3, 4, 5, 6);
+    NewRawDoFmt("%s plus %s will be %u, next are %u, %u, %u", (APTR)RAWFMTFUNC_STRING, buf, "one", "two", 3, 4, 5, 6);
     printf("Formatted string is: %s\n", buf);
-    NewRawDoFmt("%s plus %s will be %lu, next are %lu, %lu, %lu\n", (VOID_FUNC)RAWFMTFUNC_SERIAL, NULL, "one", "two", 3, 4, 5, 6);
+    NewRawDoFmt("%s plus %s will be %u, next are %u, %u, %u\n", (APTR)RAWFMTFUNC_SERIAL, NULL, "one", "two", 3, 4, 5, 6);
     printf("Serial output done\n");
 
     count = 0;
+    
+    /*
+     * RawDoFmt() historically assumes UWORD argument
+     * without 'l' modifier. In AROS 'l' here stands for IPTR,
+     * not ULONG, for 64 bit compatibility.
+     */
     printf("Checking RawDoFmt...\n");
-    RawDoFmt("%s plus %s will be %lu, next are %lu, %lu, %lu", args, (VOID_FUNC)RAWFMTFUNC_COUNT, &count);
+    RawDoFmt("%s plus %s will be %lu, next are %lu, %lu, %lu", args, (APTR)RAWFMTFUNC_COUNT, &count);
     printf("Count is %u\n", (unsigned)count);
-    RawDoFmt("%s plus %s will be %lu, next are %lu, %lu, %lu", args, (VOID_FUNC)RAWFMTFUNC_STRING, buf);
+    RawDoFmt("%s plus %s will be %lu, next are %lu, %lu, %lu", args, (APTR)RAWFMTFUNC_STRING, buf);
     printf("Formatted string is: %s\n", buf);
-    RawDoFmt("%s plus %s will be %lu, next are %lu, %lu, %lu\n", args, (VOID_FUNC)RAWFMTFUNC_SERIAL, NULL);
+    RawDoFmt("%s plus %s will be %lu, next are %lu, %lu, %lu\n", args, (APTR)RAWFMTFUNC_SERIAL, NULL);
     printf("Serial output done\n");
 
     return 0;
