cc245be45468e6000cacec956ec86434fb99edda
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-08-28 16:26:15 +0000

    Code cleanup, removed #ifdef
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40930 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/sumkickdata.c
+++ rom/exec/sumkickdata.c
@@ -1,10 +1,11 @@
 /*
-    Copyright © 1995-2007, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2011, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: Build checksum for Kickstart.
     Lang: english
 */
+
 #include <aros/debug.h>
 #include "exec_intern.h"
 #include <proto/exec.h>
@@ -44,33 +45,43 @@
     ULONG chksum = 0;
     BOOL isdata = FALSE;
 
-    if (SysBase->KickTagPtr) {
+    D(bug("[SumKickData] KickTagPtr 0x%p KickMemPtr 0x%p\n", SysBase->KickTagPtr, SysBase->KickMemPtr));
+
+    if (SysBase->KickTagPtr)
+    {
     	IPTR *list = SysBase->KickTagPtr;
+
  	while(*list)
 	{
    	    chksum += (ULONG)*list;
    	    D(bug("KickTag: %p: %08x\n", list, *list));
 
-            /* on amiga, if bit 31 is set then this points to another list of
+            /*
+	     * on amiga, if bit 31 is set then this points to another list of
              * modules rather than pointing to a single module. bit 31 is
              * inconvenient on architectures where code may be loaded above
              * 2GB. on these platforms we assume aligned pointers and use bit
-             * 0 instead */
-#ifdef __mc68000__
-	    if(*list & 0x80000000) { list = (IPTR *)(*list & 0x7fffffff); continue; }
-#else
-            if(*list & 0x1) { list = (IPTR *)(*list & ~(IPTR)0x1); continue; }
-#endif
+             * 0 instead
+	     */
+	    if (*list & RESLIST_NEXT)
+	    {
+		list = (IPTR *)(*list & ~RESLIST_NEXT);
+		continue;
+	    }
 	    list++;
    	    isdata = TRUE;
    	}
     }
 
-    if (SysBase->KickMemPtr) {
+    if (SysBase->KickMemPtr)
+    {
 	struct MemList *ml = (struct MemList*)SysBase->KickMemPtr;
-	while (ml) {
+
+	while (ml)
+	{
 	    UBYTE i;
 	    ULONG *p = (ULONG*)ml;
+
 	    for (i = 0; i < sizeof(struct MemList) / sizeof(ULONG); i++)
 	    	chksum += p[i];
 	    D(bug("MemList: %p: %08x %08x %d %08x %08x %08x%08x%08x%08x\n", ml, ml->ml_Node.ln_Succ, ml->ml_Node.ln_Pred, ml->ml_NumEntries,
