2a6af6231efb3a0d4c74fdd411691bc1415610ea
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-08-30 04:56:11 +0000

    bcpl: Clean up some packet handling. BCPL putpkt is more like Dos/DoPkt().
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40977 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/m68k-all/dos/bcpl_putpkt.c
+++ arch/m68k-all/dos/bcpl_putpkt.c
@@ -19,7 +19,7 @@
     NAME */
 #include <proto/dos.h>
 
-AROS_UFH2(BOOL, Dos_BCPL_putpkt,
+AROS_UFH2(ULONG, Dos_BCPL_putpkt,
 /*  SYNOPSIS */
 	AROS_UFHA(BSTR, bdospacket, D1),
 	AROS_UFHA(struct DosLibrary *, DOSBase, A6))
@@ -41,22 +41,43 @@ AROS_UFH2(BOOL, Dos_BCPL_putpkt,
 
     struct Process *me   = (struct Process *)FindTask(NULL);
     struct DosPacket *dp = BADDR(bdospacket);
-    struct Message *mess = dp->dp_Link;
     struct MsgPort *port;
+
+    D(bug("BCPL putPkt: me->pr_MsgPort        = %p\n", &me->pr_MsgPort));
+    D(bug("BCPL putPkt: me->pr_FileSystemTask = %p\n", me->pr_FileSystemTask));
+    D(bug("BCPL putPkt: me->pr_ConsoleTask    = %p\n", me->pr_ConsoleTask));
+#define DMP(x)  D(bug("BCPL putPkt: dp->dp_%s = %p\n", #x, dp->dp_##x))
+    DMP(Link);
+    DMP(Port);
+    DMP(Type);
+    DMP(Res1);
+    DMP(Res2);
+    DMP(Arg1);
+    DMP(Arg2);
+    DMP(Arg3);
+    DMP(Arg4);
+    DMP(Arg5);
+    DMP(Arg6);
+    DMP(Arg7);
+#define MMP(x)  D(bug("BCPL putPkt: mn->mn_%s = %p\n", #x, dp->dp_Link->mn_##x))
+    MMP(Node.ln_Succ);
+    MMP(Node.ln_Pred);
+    MMP(Node.ln_Type);
+    MMP(Node.ln_Pri);
+    MMP(Node.ln_Name);
+    MMP(ReplyPort);
+    MMP(Length);
 
     port = dp->dp_Port;
 
-    dp->dp_Port = &me->pr_MsgPort;
-    mess->mn_ReplyPort = dp->dp_Port;
-    mess->mn_Node.ln_Name = (char *)dp;
-    mess->mn_Length = sizeof(*mess);
+    dp->dp_Link->mn_Node.ln_Name = (char *)dp;
+    dp->dp_Link->mn_Length = sizeof(*dp->dp_Link);
 
     D(bug("BCPL putPkt: Send to port %p, reply on port %p, DosPacket %p\n",
-	port, mess->mn_ReplyPort, dp));
+	port, &me->pr_MsgPort, dp));
 
-    PutMsg(port, mess);
-
-    return TRUE;
+    SendPkt(dp, port, &me->pr_MsgPort);
+    return (WaitPkt() == dp) ? DOSTRUE : DOSFALSE;
 
     AROS_USERFUNC_EXIT
 }
