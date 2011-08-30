d9a9fdc26eb0e252e4500f7c479be0210aaf901b
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-08-30 13:54:37 +0000

    Correctly obtain attribute bases, removed them from object data
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40993 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/devs/monitors/Composer/startup.c
+++ workbench/devs/monitors/Composer/startup.c
@@ -8,8 +8,26 @@
 
 #include "compositing_intern.h"
 
-/* Our global data */
+/* Libraries */
 struct Library *OOPBase;
+struct Library *UtilityBase;
+
+/* Attr bases */
+OOP_AttrBase HiddPixFmtAttrBase;
+OOP_AttrBase HiddSyncAttrBase;
+OOP_AttrBase HiddBitMapAttrBase;
+OOP_AttrBase HiddGCAttrBase;
+OOP_AttrBase HiddCompositingAttrBase;
+
+static const struct OOP_ABDescr attrbases[] = 
+{
+    { IID_Hidd_PixFmt,          &HiddPixFmtAttrBase },
+    { IID_Hidd_Sync,            &HiddSyncAttrBase },
+    { IID_Hidd_BitMap,          &HiddBitMapAttrBase },
+    { IID_Hidd_GC,              &HiddGCAttrBase },
+    { IID_Hidd_Compositing,     &HiddCompositingAttrBase },
+    { NULL, NULL }
+};
 
 static OOP_Class *InitClass(void)
 {
@@ -40,41 +58,54 @@ int __nocommandline = 1;
 
 int main(void)
 {
+    int ret = RETURN_FAIL;
     struct Process *me;
 
     /* 
      * Open oop.library manually because we don't want it to be
      * automatically close when we stay resident.
      */
-    OOPBase = OpenLibrary("oop.library", 42);
-    if (!OOPBase)
-    	return RETURN_FAIL;
-
-    if (OOP_FindClass(CLID_Hidd_Compositing))
+    UtilityBase = OpenLibrary("utility.library", 0);
+    if (UtilityBase)
     {
-    	/* Double-starting is a valid operation. */
-    	CloseLibrary(OOPBase);
-    	return RETURN_OK;
+    	OOPBase = OpenLibrary("oop.library", 42);
+	if (OOPBase)
+	{
+	    if (OOP_ObtainAttrBases(attrbases))
+	    {
+    	    	if (OOP_FindClass(CLID_Hidd_Compositing))
+    	    	{
+	    	    /* Double-starting is a valid operation. */
+	    	    ret = RETURN_OK;
+	    	}
+	    	else
+	    	{
+    		    if (InitClass())
+    		    {
+		    	/* TODO: Tell graphics.library that we are up and running */
+
+		    	/* Stay resident */
+		    	me = (struct Process *)FindTask(NULL);
+		    	if (me->pr_CLI)
+		    	{
+			    struct CommandLineInterface *cli = BADDR(me->pr_CLI);
+
+			    cli->cli_Module = BNULL;
+		    	}
+		    	else
+			    me->pr_SegList = BNULL;
+
+		    	/* Don't close our libraries. The HIDD needs them. */
+
+		    	return RETURN_OK;
+		    }
+		}
+	    }
+	    OOP_ReleaseAttrBases(attrbases);
+    	    CloseLibrary(OOPBase);
+    	}
+    	CloseLibrary(UtilityBase);
     }
 
-    if (!InitClass())
-    {
-    	CloseLibrary(OOPBase);
-    	return RETURN_FAIL;
-    }
-
-    /* TODO: Tell graphics.library that we are up and running */
-
-    /* Stay resident */
-    me = (struct Process *)FindTask(NULL);
-    if (me->pr_CLI)
-    {
-	struct CommandLineInterface *cli = BADDR(me->pr_CLI);
-
-	cli->cli_Module = BNULL;
-    }
-    else  
-	me->pr_SegList = BNULL;
-
-    return RETURN_OK;
+    return ret;
 }
