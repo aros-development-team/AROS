6ee1aafe85f4ba5152b429317787d6a2d945001a
twilen <twilen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-09-01 17:59:17 +0000

    m68k-amiga RTG driver works again.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@41046 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/m68k-amiga/hidd/uaegfx/startup.c
+++ arch/m68k-amiga/hidd/uaegfx/startup.c
@@ -18,28 +18,20 @@ BOOL Init_UAEGFXClass(LIBBASETYPEPTR LIBBASE);
 
 static int UAEGFX_Init(LIBBASETYPEPTR LIBBASE)
 {
-    struct GfxBase *GfxBase;
-    OOP_Object *gfxhidd;
-
+    ULONG err;
+ 	
     D(bug("************************* UAEGFX_Init ******************************\n"));
+
     if (!Init_UAEGFXClass(LIBBASE))
     	return FALSE;
-    GfxBase = (struct GfxBase *)TaggedOpenLibrary(TAGGEDOPEN_GRAPHICS);
-    if (!GfxBase)
-        return FALSE;
     LIBBASE->library.lib_OpenCnt = 1;
-    gfxhidd = OOP_NewObject(LIBBASE->csd.gfxclass, NULL, NULL);
-    D(bug("UAEGFX=0x%p\n", gfxhidd));
-    if (gfxhidd) {
-	ULONG err = AddDisplayDriver(gfxhidd, NULL, DDRV_KeepBootMode, TRUE, DDRV_IDMask, 0xF0000000, TAG_DONE);
-	D(bug("UAEGFX AddDisplayDriver() result: %u\n", err));
-	if (err) {
-	    OOP_DisposeObject(gfxhidd);
-	    gfxhidd = NULL;
-	}
-    }
-    CloseLibrary(&GfxBase->LibNode);
-    D(bug("UAEGFX_Init=0x%p\n", gfxhidd));
-    return gfxhidd ? TRUE : FALSE;
+
+    err = AddDisplayDriver(LIBBASE->csd.gfxclass, NULL,
+			   DDRV_KeepBootMode, TRUE,
+			   DDRV_IDMask      , 0xF0000000,
+			   TAG_DONE);
+
+    D(bug("UAEGFXHIDD AddDisplayDriver() result: %u\n", err));
+    return err ? FALSE : TRUE;
 }
 ADD2INITLIB(UAEGFX_Init, 0)
