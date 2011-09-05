f2ffe79274abc00b7c2f2e9f2b1d441077f05030
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-09-05 13:46:29 +0000

    Added internal function for displaying alerts in early startup code
    git-svn-id: https://svn.aros.org/svn/aros/trunk@41125 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/kernel/displayalert.c
+++ rom/kernel/displayalert.c
@@ -4,59 +4,6 @@
 #include <kernel_base.h>
 #include <kernel_debug.h>
 
-#define ALERT_WIDTH 80
-
-static inline void PrintChars(char c, ULONG n, struct KernelBase *KernelBase)
-{
-    while (n--)
-        krnPutC(c, KernelBase);
-}
-
-/*
- * This function calculates length of line.
- * It's similar to strlen(), but stops also at LF and FF codes.
- */
-static inline int linelen(const char *str)
-{
-    int l;
-
-    for (l = 0; str[l] && str[l] != '\n' && str[l] != 0x0F; l++);
-    return l;
-}
-
-static const char *PrintCentered(const char *str, struct KernelBase *KernelBase)
-{
-    int len = linelen(str);
-    int i;
-    ULONG s;
-
-    if (len > (ALERT_WIDTH - 2))
-    	    len = (ALERT_WIDTH - 2);
-
-    s = ALERT_WIDTH - 2 - len;
-
-    krnPutC('#', KernelBase);
-    if (s & 1)
-        krnPutC(' ', KernelBase);
-    s >>= 1;
-    PrintChars(' ', s, KernelBase);
-
-    for (i = 0; i < len; i++)
-        krnPutC(*str++, KernelBase);
-
-    PrintChars(' ', s, KernelBase);
-    krnPutC('#', KernelBase);
-    krnPutC('\n', KernelBase);
-
-    return str;
-}
-
-static inline void PrintFrame(struct KernelBase *KernelBase)
-{
-    PrintChars('#', ALERT_WIDTH, KernelBase);
-    krnPutC('\n', KernelBase);
-}
-
 /*****************************************************************************
 
     NAME */
@@ -112,28 +59,7 @@ AROS_LH2(void, KrnDisplayAlert,
      * This is a generic version that simply formats the text into debug log.
      * It can be replaced with machine-specific implementation which can do more.
      */
-    unsigned int i;
-
-    /* Make sure that the output starts from a new line */
-    krnPutC('\n', KernelBase);
-
-    PrintFrame(KernelBase);
-
-    /* Print first three lines (title, task and error) centered */
-    for (i = 0; i < 3; i++)
-    {
-    	text = PrintCentered(text, KernelBase);
-    	text++;	/* Skip a newline */
-    }
-
-    PrintFrame(KernelBase);
-
-    /* Print the rest of alert text */
-    while (*text)
-    	krnPutC(*text++, KernelBase);
-
-    krnPutC('\n', KernelBase);
-    PrintFrame(KernelBase);
+    krnDisplayAlert(text, KernelBase);
 
     AROS_LIBFUNC_EXIT
 }
--- rom/kernel/kernel_debug.h
+++ rom/kernel/kernel_debug.h
@@ -14,6 +14,7 @@
 
 int krnPutC(int chr, struct KernelBase *KernelBase);
 int krnBug(const char *format, va_list args, APTR kernelBase);
+void krnDisplayAlert(const char *text, struct KernelBase *KernelBase);
 
 static inline void _bug(APTR kernelBase, const char *format, ...)
 {
