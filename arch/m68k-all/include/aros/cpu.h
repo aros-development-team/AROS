e5070a967a69c2be6f3c545b6ba8075ad5c47cfb
twilen <twilen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-09-07 18:12:09 +0000

    HALT should also mask interrupts.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@41172 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/m68k-all/include/aros/cpu.h
+++ arch/m68k-all/include/aros/cpu.h
@@ -90,7 +90,7 @@ do                                                       \
 
 #define SIZEOF_ALL_REGISTERS		(20*4)
 
-#define HALT asm volatile("stop #0x2000");
+#define HALT asm volatile("stop #0x2700");
 #define KERNEL_PANIC_BUFFER (char*)0x800
 
 /* Macros for generating library stub functions and aliases. */
