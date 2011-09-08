c66e2776c81376d42073c151fba9d03f9d2c0014
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-09-08 20:04:41 +0000

    Added some more useful definitions
    git-svn-id: https://svn.aros.org/svn/aros/trunk@41210 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-all/include/asm/cpu.h
+++ arch/i386-all/include/asm/cpu.h
@@ -15,6 +15,12 @@
 
 #define HALT asm volatile("hlt")
 
+#define rdcr(reg) \
+    ({ long val; asm volatile("mov %%" #reg ",%0":"=r"(val)); val; })
+
+#define wrcr(reg, val) \
+    do { asm volatile("mov %0,%%" #reg::"r"(val)); } while(0)
+
 static inline void __attribute__((always_inline)) rdmsr(uint32_t msr_no, uint32_t *ret_lo, uint32_t *ret_hi)
 {
     uint32_t ret1, ret2;
@@ -22,6 +28,14 @@ static inline void __attribute__((always_inline)) rdmsr(uint32_t msr_no, uint32_
     asm volatile("rdmsr":"=a"(ret1),"=d"(ret2):"c"(msr_no));
     *ret_lo=ret1;
     *ret_hi=ret2;
+}
+
+static inline uint32_t __attribute__((always_inline)) rdmsri(uint32_t msr_no)
+{
+    uint32_t ret;
+
+    asm volatile("rdmsr":"=a"(ret):"c"(msr_no));
+    return ret;
 }
 
 #endif
