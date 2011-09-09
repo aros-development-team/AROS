f629323212356a252d77f03eebf525b0bd023b4c
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-09-09 05:43:07 +0000

    - Really print full 64-bit addresses - Print also entry length, just in case
    git-svn-id: https://svn.aros.org/svn/aros/trunk@41213 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/kernel/mmap.c
+++ test/kernel/mmap.c
@@ -36,29 +36,36 @@ int main(void)
     	printf("No boot information from the bootstrap!\n");
     	return 20;
     }
-    
+
     mmap = (struct mb_mmap *)GetTagData(KRN_MMAPAddress, 0, tags);
     len  = GetTagData(KRN_MMAPLength, 0, tags);
-    
+
     if (!mmap || !len)
     {
         printf("No memory map provided by the bootstrap!\n");
         return 20;
     }
-    
+
     printf("Memory map at 0x%p, length %lu:\n", mmap, len);
     while (len >= sizeof(struct mb_mmap))
     {
         unsigned int type = mmap->type;
-        
+        unsigned long long memaddr = mmap->addr;
+        unsigned long long memlen  = mmap->len;
+
         if (type > MMAP_TYPE_ACPINVS)
             type = 0;
 
-	printf("Entry type %d <%s> addr 0x%016llx len 0x%016llx\n", mmap->type, types[type], (unsigned long long)mmap->addr, (unsigned long long)mmap->len);
+#if __WORDSIZE < 64
+	memaddr |= (unsigned long long)mmap->addr_high << 32;
+	memlen  |= (unsigned long long)mmap->len_high << 32;
+#endif
+
+	printf("Entry size %d type %d <%s> addr 0x%016llx len 0x%016llx\n", mmap->size, mmap->type, types[type], memaddr, memlen);
 
         len -= mmap->size + 4;
         mmap = (struct mb_mmap *)(mmap->size + (unsigned long)mmap + 4);
     }
-    
+
     return 0;
 }
