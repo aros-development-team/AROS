f7f9742f739aec064b55c4033f7518a4c68752ce
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-09-10 17:26:51 +0000

    One more useful subroutine
    git-svn-id: https://svn.aros.org/svn/aros/trunk@41245 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-native/kernel/boot_utils.h
+++ arch/all-native/kernel/boot_utils.h
@@ -1,5 +1,7 @@
 #include <utility/tagitem.h>
 
+void krnCopyMem(const void *src, void *dest, unsigned long size);
+
 void RelocateBootMsg(const struct TagItem *msg);
 void RelocateTagData(struct TagItem *tag, unsigned long size);
 void RelocateStringData(struct TagItem *tag);
