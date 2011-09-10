dc9dd6075090f8fe0833a49fe0601926142fd023
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-09-10 17:28:04 +0000

    GDT and TSS definitions
    git-svn-id: https://svn.aros.org/svn/aros/trunk@41246 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-all/include/asm/cpu.h
+++ arch/i386-all/include/asm/cpu.h
@@ -15,6 +15,45 @@
 
 #define HALT asm volatile("hlt")
 
+/* Table descriptor used for lgdt and lidt commands */
+struct table_desc
+{
+    unsigned short size;
+    unsigned long  base;
+} __attribute__((packed));
+
+/* Segment descriptor in the GDT */
+struct segment_desc
+{
+    uint16_t    limit_low;
+    uint16_t    base_low;
+    uint8_t     base_mid;
+    unsigned    type:5, dpl:2, p:1;
+    unsigned    limit_high:4, avl:1, l:1, d:1, g:1;
+    uint8_t     base_high:8;
+} __attribute__((packed));
+
+/*
+ * TaskStateStructure, defined only in matter of making life (setup)
+ * more human-readable
+ */
+struct tss
+{
+    unsigned int   link,               /* link to previous task         */
+                   ssp,                /* Supervisor Stack Pointer      */
+                   ssp_seg,            /* SSP descriptor                */
+                   t0,t1,              /* Stack for CPL1 code           */
+                   t2,t3,              /* Stack for CPL2 code           */
+                   cr3,                /* used in paging                */
+                   eip,                /* Instruction pointer           */
+                   eflags,             /* Flags for given task          */
+                   r0,r1,r2,r3,        /* 8 general purpouse registers  */
+                   r4,r5,r6,r7,
+                   es,cs,ss,ds,fs,gs,  /* segment descriptors           */
+                   ldt;                /* LocalDescriptorTable          */
+    unsigned short trap,iomap;         /* trap flag and iomap pointer   */
+};
+
 #define rdcr(reg) \
     ({ long val; asm volatile("mov %%" #reg ",%0":"=r"(val)); val; })
 
