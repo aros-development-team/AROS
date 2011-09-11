d2f6892ee84cba4432ac283f3b70fd8d7a61aec6
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-09-11 17:18:45 +0000

    VGA display driver is self-installing now. bootmenu.resource init kludge is still needed only for PPC native.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@41263 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-native/hidd/vga.hidd/bitmap.h
+++ arch/all-native/hidd/vga.hidd/bitmap.h
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2010, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2011, The AROS Development Team. All rights reserved.
     $Id$
 */
 
@@ -17,7 +17,7 @@
 #define IID_Hidd_VGABitMap "hidd.bitmap.vgabitmap"
 
 #define HiddVGABitMapAB __abHidd_VGABitMap
-/* extern OOP_AttrBase HiddVGABitMapAB; */
+extern OOP_AttrBase HiddVGABitMapAB;
 
 enum {
     aoHidd_VGABitMap_Drawable,
