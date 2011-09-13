59c1e9f39ee9d49f5cbf38e260fdc60e71ac519b
ezrec <jason.mcmullan@gmail.com>
2011-09-13 16:08:03 +0000

    dos.library: Even more ReadItem() bug compatibility (and tests)
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@41306 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/dos/readitem.c
+++ test/dos/readitem.c
@@ -9,40 +9,92 @@
 #include <proto/exec.h>
 #include <proto/dos.h>
 
-static int ritest(ULONG test, CONST_STRPTR in, LONG ret, CONST_STRPTR out, LONG cur)
+#include <dos/stdio.h>
+
+#define IOERR_UNCHANGED -1
+
+static int ritest_buff(ULONG test, CONST_STRPTR in, LONG ret, LONG ioerr, CONST_STRPTR out, LONG cur)
 {
     static char buff[256];
     struct CSource cs;
     int failed = 0;
-    LONG err;
+    LONG err, reterr;
 
     cs.CS_Buffer = (APTR)in;
     cs.CS_Length = strlen(in);
     cs.CS_CurChr = 0;
 
+    SetIoErr(IOERR_UNCHANGED);
     err = ReadItem(buff, sizeof(buff), &cs);
+    reterr = IoErr();
     failed |= (err != ret) ? 1 : 0;
     failed |= (strcmp(buff, out) != 0) ? 1 : 0;
     failed |= (cur != cs.CS_CurChr) ? 1 : 0;
+    failed |= (reterr != ioerr) ? 1 : 0;
 
     if (failed) {
-        Printf("%ld: in: (%s)\n", test, in);
-        Printf("%ld: expected: ret %ld, buf (%s), cur %ld\n", test, ret, out, cur);
-        Printf("%ld: returned: ret %ld, buf (%s), cur %ld\n", test, err, buff, cs.CS_CurChr);
+        Printf("%ld: buff: in: (%s)\n", test, in);
+        Printf("%ld: buff: expected: ret %ld (%ld), buf (%s), cur %ld\n", test, ret, ioerr, out, cur);
+        Printf("%ld: buff: returned: ret %ld (%ld), buf (%s), cur %ld\n", test, err, reterr, buff, cs.CS_CurChr);
     }
 
     return failed;
 }
 
+static int ritest_file(ULONG test, CONST_STRPTR in, LONG ret, LONG ioerr, CONST_STRPTR out, LONG cur)
+{
+    static char buff[256];
+    int failed = 0;
+    LONG err, reterr;
+    BPTR io, oldin;
+
+    io = Open("RAM:readitem.tmp", MODE_NEWFILE);
+    Write(io, in, strlen(in));
+    Close(io);
+    io = Open("RAM:readitem.tmp", MODE_OLDFILE);
+
+    oldin = Input();
+    SelectInput(io);
+    SetIoErr(IOERR_UNCHANGED);
+    err = ReadItem(buff, sizeof(buff), NULL);
+    reterr = IoErr();
+    SelectInput(oldin);
+
+    failed |= (err != ret) ? 1 : 0;
+    failed |= (strcmp(buff, out) != 0) ? 1 : 0;
+    failed |= (reterr != ioerr) ? 1 : 0;
+
+    if (failed) {
+        Printf("%ld: file: in: (%s)\n", test, in);
+        Printf("%ld: file: expected: ret %ld (%ld), buf (%s)\n", test, ret, ioerr, out);
+        Printf("%ld: file: returned: ret %ld (%ld), buf (%s)\n", test, err, reterr, buff);
+    }
+
+    Close(io);
+    DeleteFile("RAM:readitem.tmp");
+    return failed;
+}
+
 #define RITEST(in, ret, out, cur) \
     do { \
-        failed += ritest(__LINE__, in, ret, out, cur); \
+        failed += ritest_buff(__LINE__, in, ret, IOERR_UNCHANGED, out, cur); \
+        tests++; \
+        failed += ritest_file(__LINE__, in, ret, RETURN_OK, out, cur); \
+        tests++; \
+    } while (0)
+
+#define RITERR(in, ret, out, cur, ioerr) \
+    do { \
+        failed += ritest_buff(__LINE__, in, ret, IOERR_UNCHANGED, out, cur); \
+        tests++; \
+        failed += ritest_file(__LINE__, in, ret, ioerr, out, cur); \
         tests++; \
     } while (0)
 
 int main(int argc, char **argv)
 {
     int failed = 0, tests = 0;
+    LONG ret;
 
     /* The following behaviour is versus AOS 3.1,
      * which should be used as the reference for
@@ -60,22 +112,22 @@ int main(int argc, char **argv)
     RITEST("\"word\"=thing\n",ITEM_QUOTED,"word",6);
     RITEST("\"word\" crazy\n",ITEM_QUOTED,"word",6);
     RITEST("\"word\"\tcrazy\n",ITEM_QUOTED,"word",6);
-    RITEST("\"word",ITEM_ERROR,"word",4);
-    RITEST("\"word\n\"\n",ITEM_ERROR,"word",5);
+    RITERR("\"word",ITEM_ERROR,"word",4,0);
+    RITERR("\"word\n\"\n",ITEM_ERROR,"word",5,0);
     RITEST("\"word=\"\n",ITEM_QUOTED,"word=",7);
     RITEST("\"word \"\n",ITEM_QUOTED,"word ",7);
-    RITEST("\"word*",ITEM_ERROR,"word",5);
-    RITEST("\"word*\n",ITEM_ERROR,"word",6);
-    RITEST("\"word**",ITEM_ERROR,"word*",6);
-    RITEST("\"word*e",ITEM_ERROR,"word\e",6);
-    RITEST("\"word*E",ITEM_ERROR,"word\e",6);
-    RITEST("\"word*n",ITEM_ERROR,"word\n",6);
-    RITEST("\"word*N",ITEM_ERROR,"word\n",6);
-    RITEST("\"word**\n",ITEM_ERROR,"word*",7);
-    RITEST("\"word*e\n",ITEM_ERROR,"word\e",7);
-    RITEST("\"word*E\n",ITEM_ERROR,"word\e",7);
-    RITEST("\"word*n\n",ITEM_ERROR,"word\n",7);
-    RITEST("\"word*N\n",ITEM_ERROR,"word\n",7);
+    RITERR("\"word*",ITEM_ERROR,"word",5,0);
+    RITERR("\"word*\n",ITEM_ERROR,"word",6,0);
+    RITERR("\"word**",ITEM_ERROR,"word*",6,0);
+    RITERR("\"word*e",ITEM_ERROR,"word\e",6,0);
+    RITERR("\"word*E",ITEM_ERROR,"word\e",6,0);
+    RITERR("\"word*n",ITEM_ERROR,"word\n",6,0);
+    RITERR("\"word*N",ITEM_ERROR,"word\n",6,0);
+    RITERR("\"word**\n",ITEM_ERROR,"word*",7,0);
+    RITERR("\"word*e\n",ITEM_ERROR,"word\e",7,0);
+    RITERR("\"word*E\n",ITEM_ERROR,"word\e",7,0);
+    RITERR("\"word*n\n",ITEM_ERROR,"word\n",7,0);
+    RITERR("\"word*N\n",ITEM_ERROR,"word\n",7,0);
     RITEST("\"word**\"",ITEM_QUOTED,"word*",8);
     RITEST("\"word*e\"",ITEM_QUOTED,"word\e",8);
     RITEST("\"word*E\"",ITEM_QUOTED,"word\e",8);
@@ -88,6 +140,93 @@ int main(int argc, char **argv)
     RITEST("",ITEM_NOTHING,"",0);
     RITEST("word\"hello \"world",ITEM_UNQUOTED,"word\"hello",11);
     RITEST("",ITEM_NOTHING,"",0);
+
+    /* Explicit tests for edge conditions */
+    {
+        int lfailed = 0;
+        LONG ioerr;
+        SetIoErr(IOERR_UNCHANGED);
+        ret = ReadItem(NULL, 128, NULL);
+        ioerr = IoErr();
+        tests++;
+        lfailed |= (ret != ITEM_NOTHING) ? 1 : 0;
+        lfailed |= (ioerr != IOERR_UNCHANGED) ? 1 : 0;
+        if (lfailed) {
+            Printf("Edge1: expected %ld (%ld)\n", 0, IOERR_UNCHANGED);
+            Printf("Edge1: returned %ld (%ld)\n", ret, ioerr);
+        }
+        failed += lfailed;
+    }
+
+    {
+        int lfailed = 0;
+        LONG ioerr;
+        BYTE buff[] = { 0x11, 0x22, 0x33, 0x44 };
+        SetIoErr(IOERR_UNCHANGED);
+        ret = ReadItem(buff, 0, NULL);
+        ioerr = IoErr();
+        tests++;
+        lfailed |= (ret != ITEM_NOTHING) ? 1 : 0;
+        lfailed |= (ioerr != IOERR_UNCHANGED) ? 1 : 0;
+        lfailed |= (buff[0] != 0x0) ? 1 : 0;
+        if (lfailed) {
+            Printf("Edge2: expected %ld (%ld), buff[0] = 0x00\n", 0, IOERR_UNCHANGED);
+            Printf("Edge2: returned %ld (%ld), buff[0] = 0x%02lx\n", ret, ioerr, (LONG)buff[0]);
+        }
+        failed += lfailed;
+    }
+
+    {
+        int lfailed = 0;
+        LONG ioerr;
+        BPTR io, oldio;
+
+        BYTE buff[] = { 0x11, 0x22, 0x33, 0x44 };
+
+        io = Open("NIL:", MODE_OLDFILE);
+        oldio = Input();
+        SelectInput(io);
+        SetIoErr(IOERR_UNCHANGED);
+        ret = ReadItem(buff, sizeof(buff), NULL);
+        ioerr = IoErr();
+        SelectInput(oldio);
+        Close(io);
+        tests++;
+        lfailed |= (ret != ITEM_NOTHING) ? 1 : 0;
+        lfailed |= (ioerr != IOERR_UNCHANGED) ? 1 : 0;
+        lfailed |= (buff[0] != 0x0) ? 1 : 0;
+        if (lfailed) {
+            Printf("Edge3: expected %ld (%ld), buff[0] = 0x00\n", ITEM_NOTHING, IOERR_UNCHANGED);
+            Printf("Edge3: returned %ld (%ld), buff[0] = 0x%02lx\n", ret, ioerr, (LONG)buff[0]);
+        }
+        failed += lfailed;
+    }
+
+    {
+        int lfailed = 0;
+        LONG ioerr;
+        BPTR oldio;
+        BYTE buff[] = { 0x11, 0x22, 0x33, 0x44 };
+
+        oldio = Input();
+        SelectInput(BNULL);
+        SetIoErr(IOERR_UNCHANGED);
+        ret = ReadItem(buff, sizeof(buff), NULL);
+        ioerr = IoErr();
+        SelectInput(oldio);
+        tests++;
+        lfailed |= (ret != ITEM_NOTHING) ? 1 : 0;
+        lfailed |= (ioerr != IOERR_UNCHANGED) ? 1 : 0;
+        lfailed |= (buff[0] != 0x0) ? 1 : 0;
+        if (lfailed) {
+            Printf("Edge4: expected %ld (%ld), buff[0] = 0x00\n", ITEM_NOTHING, IOERR_UNCHANGED);
+            Printf("Edge4: returned %ld (%ld), buff[0] = 0x%02lx\n", ret, ioerr, (LONG)buff[0]);
+        }
+        failed += lfailed;
+    }
+
+
+
 
     if (failed == 0)
         Printf("All %ld tests passed\n", tests);
