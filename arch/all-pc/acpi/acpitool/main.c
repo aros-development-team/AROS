d461220aaa134231abcab5dbd4fca1c5afff7b93
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-09-14 20:21:00 +0000

    ACPITool, work in progress
    git-svn-id: https://svn.aros.org/svn/aros/trunk@41348 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/all-pc/acpi/acpitool/main.c
@@ -0,0 +1,253 @@
+/*
+    Copyright (C) 2003-2011, The AROS Development Team.
+    $Id$
+*/
+
+#include <exec/memory.h>
+#include <libraries/mui.h>
+#include <resources/acpi.h>
+#include <utility/tagitem.h>
+#include <utility/hooks.h>
+
+#include <proto/acpi.h>
+#include <proto/dos.h>
+#include <proto/exec.h>
+#include <proto/muimaster.h>
+#include <proto/oop.h>
+#include <proto/utility.h>
+#include <proto/intuition.h>
+
+#include <aros/debug.h>
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "locale.h"
+#include "parsers.h"
+
+#define APPNAME "ACPITool"
+#define VERSION "ACPITool 0.1"
+
+#define IDB_SAVE 10001
+#define IDB_SAVEALL 10002
+
+const char version[] = "$VER: " VERSION " (" ADATE ")\n";
+
+APTR ACPIBase;
+struct ACPI_TABLE_TYPE_RSDP *RSDP;
+
+#define ACPI ((struct ACPIBase *)ACPIBase)
+
+static void GetRSDP(void)
+{
+    /*
+     * TODO: This is a placeholder.
+     * Its job is to check if RSDP is located in zero page and make a copy if so.
+     */
+    RSDP = ACPI->ACPIB_RSDP_Addr;
+}
+
+static void ShowError(Object *application, Object *window, CONST_STRPTR message, BOOL useIOError)
+{
+    TEXT   buffer[128];
+    STRPTR newline = "\n",
+           period  = ".",
+           extra   = buffer;
+           
+    /* Never use IO error if it is 0 */
+    if (IoErr() == 0) useIOError = FALSE;
+    
+    if (useIOError)
+    {
+        Fault(IoErr(), NULL, buffer, sizeof(buffer));
+        buffer[0] = toupper(buffer[0]);
+    }
+    else
+    {
+        newline = "";
+        period  = "";
+        extra   = "";
+    }
+            
+    MUI_Request
+    (
+        application, window, 0, _(MSG_TITLE), _(MSG_ERROR_OK), 
+        "%s:\n%s%s%s%s", _(MSG_ERROR_HEADER), message, newline, extra, period
+    );
+}
+
+void cleanup(CONST_STRPTR message)
+{
+    Locale_Deinitialize();
+
+    if (message != NULL)
+    {
+	ShowError(NULL, NULL, message, TRUE);
+	exit(RETURN_FAIL);
+    }
+}
+
+Object *MakeLabel(STRPTR str)
+{
+    return (MUI_MakeObject(MUIO_Label, str, 0));
+}
+
+static inline LONG xget(Object * obj, ULONG attr)
+{
+    IPTR x = 0;
+
+    GetAttr(attr, obj, &x);
+    return x;
+}
+
+Object *app;
+Object *MainWindow;
+Object *TablesList;
+Object *InfoList;
+
+struct Hook display_hook;
+
+AROS_UFH3(void, display_function,
+    AROS_UFHA(struct Hook *, h,  A0),
+    AROS_UFHA(char **, strings, A2),
+    AROS_UFHA(struct ACPI_TABLE_DEF_HEADER *, table, A1))
+{
+    AROS_USERFUNC_INIT
+
+    const struct Parser *t = FindParser(table->signature);
+
+    if (t)
+    {
+	strings[0] = t->name;
+	return;
+    }
+
+    snprintf(buf, sizeof(buf), "%s (%4.4s)", _(MSG_UNKNOWN), (char *)&table->signature);
+    strings[0] = buf;
+
+    AROS_USERFUNC_EXIT
+}
+
+static void display_callback(const char *str)
+{
+    DoMethod(InfoList, MUIM_List_InsertSingle, str, MUIV_List_Insert_Bottom);
+}
+
+AROS_UFH3(void, select_function,
+    AROS_UFHA(struct Hook *, h, A0),
+    AROS_UFHA(Object *, object, A2),
+    AROS_UFHA(APTR, msg, A1))
+{
+    AROS_USERFUNC_INIT
+
+    IPTR active;
+    struct ACPI_TABLE_DEF_HEADER *table;
+    const struct Parser *t;
+    void (*parser)();
+
+    DoMethod(InfoList, MUIM_List_Clear);
+    
+    GetAttr(MUIA_List_Active, object, &active);
+    if (active == MUIV_List_Active_Off)
+	return;
+
+    DoMethod(object, MUIM_List_GetEntry, active, &table);
+    t = FindParser(table->signature);
+    parser = t ? t->parser : header_parser;
+    parser(table, display_callback);
+
+    AROS_USERFUNC_EXIT
+}
+
+BOOL GUIinit()
+{
+    BOOL retval = FALSE;
+
+    app = ApplicationObject,
+	    MUIA_Application_Title,	    (IPTR)APPNAME,
+	    MUIA_Application_Version,	    (IPTR)VERSION,
+	    MUIA_Application_Copyright,	    (IPTR)"(C) 2004-2011, The AROS Development Team",
+	    MUIA_Application_Author,	    (IPTR)"Pavel Fedin",
+	    MUIA_Application_Base,	    (IPTR)APPNAME,
+	    MUIA_Application_Description,   __(MSG_DESCRIPTION),
+
+	    SubWindow, MainWindow = WindowObject,
+		MUIA_Window_Title,	__(MSG_WINTITLE),
+
+		WindowContents, HGroup,
+		    MUIA_Group_SameWidth, FALSE,
+
+		    Child, ListviewObject,
+			MUIA_Listview_List, TablesList = ListObject,
+			    ReadListFrame,
+			    MUIA_List_AdjustWidth, TRUE,
+			    MUIA_List_DisplayHook, &display_hook,
+			End, // List
+		    End, // ListView
+
+		    Child, ListviewObject,
+			MUIA_Listview_List, InfoList = ListObject,
+			    ReadListFrame,
+			    MUIA_List_AdjustWidth, TRUE,
+			    MUIA_List_ConstructHook, MUIV_List_ConstructHook_String,
+			    MUIA_List_DestructHook, MUIV_List_DestructHook_String,
+			End, // List
+		    End, // ListView
+		End, // WindowContents
+	    End, // MainWindow
+	End; // ApplicationObject
+
+    if (app)
+    {
+	/* Quit application if the windowclosegadget or the esc key is pressed. */
+	DoMethod(MainWindow, MUIM_Notify, MUIA_Window_CloseRequest, TRUE, 
+		 (IPTR)app, 2, 
+		 MUIM_Application_ReturnID, MUIV_Application_ReturnID_Quit);
+
+	retval = TRUE;
+    }
+
+    return retval;
+}
+
+int __nocommandline = 1;
+
+int main(void)
+{
+    display_hook.h_Entry = (APTR)display_function;
+//    select_hook.h_Entry = (APTR)select_function;
+
+    if (!Locale_Initialize())
+	cleanup(_(MSG_ERROR_LOCALE));
+
+    ACPIBase = OpenResource("acpi.resource");
+    if (!ACPIBase)
+	cleanup(_(MSG_ERROR_NO_ACPI));
+
+    GetRSDP();
+	
+    if(GUIinit())
+    {
+	unsigned int i;
+
+	/* Populate tables list */
+	DoMethod(TablesList, MUIM_List_InsertSingle, ACPI->ACPIB_SDT_Addr, MUIV_List_Insert_Bottom);
+	for (i = 0; i < ACPI->ACPIB_SDT_Count; i++)
+	    DoMethod(TablesList, MUIM_List_InsertSingle, ACPI->ACPIB_SDT_Entry[i], MUIV_List_Insert_Bottom);
+
+	set(MainWindow, MUIA_Window_Open, TRUE);
+
+	if (xget(MainWindow, MUIA_Window_Open))
+	{
+	    DoMethod(app, MUIM_Application_Execute);
+	    set(MainWindow, MUIA_Window_Open, FALSE);
+	}
+
+	DisposeObject(app);
+
+    }
+    cleanup(NULL);
+
+    return 0;
+}
---
+++ arch/all-pc/acpi/acpitool/parsers.c
@@ -0,0 +1,218 @@
+#include <resources/acpi.h>
+
+#include <stdio.h>
+
+#include "locale.h"
+#include "parsers.h"
+
+/* Buffer for text formatting */
+char buf[BUFFER_SIZE];
+
+static inline void MakeString(void (*callback)(const char *), const char *fmt, ...)
+{
+    va_list ap;
+
+    va_start(ap, fmt);
+    vsnprintf(buf, sizeof(buf), fmt, ap);
+    va_end(ap);
+
+    callback(buf);
+}    
+
+static const char *decode_enum(ULONG val, const char **table)
+{
+    unsigned int i;
+
+    for (i = 0; table[i]; i++)
+    {
+	if (val == i)
+	    return table[i];
+    }
+    return NULL;
+}
+
+static void parse_enum(const char *desc, ULONG val, const char **table, void (*cb)(const char *))
+{
+    const char *valname = decode_enum(val, table);
+
+    if (valname)
+	MakeString(cb, "%s: %s", desc, valname);
+    else
+	MakeString(cb, "%s: %s (%u)", desc, _(MSG_UNKNOWN), val);
+}    
+
+static void parse_flags(ULONG flags, const char **table, void (*cb)(const char *))
+{
+    unsigned int i;
+
+    for (i = 0; table[i]; i++)
+    {
+        const char *val = (flags & (1 << i)) ? _(MSG_YES) : _(MSG_NO);
+	
+	MakeString(cb, "  %s: %s", table[i], val);
+    }
+}    
+
+const char *spaces[] =
+{
+    "memory",
+    "I/O ports",
+    "PCI configuration space",
+    "Embedded controller",
+    "SMBus",
+    NULL
+};
+
+void parse_addr(const char *desc, struct GENERIC_ACPI_ADDR *addr, void (*cb)(const char *))
+{
+    BOOL print_id = FALSE;
+    const char *space;
+    const char *fmt;
+
+    if (addr->address_space_id == ACPI_SPACE_FIXED)
+	space = _(MSG_SPACE_FIXED);
+    else if (addr->address_space_id >= ACPI_SPACE_OEM)
+    {
+	space = _(MSG_SPACE_OEM);
+	print_id = TRUE;
+    }
+    else
+    {
+	space = decode_enum(addr->address_space_id, spaces);
+	if (!space)
+	{
+	    space = _(MSG_SPACE_UNKNOWN);
+	    print_id = TRUE;
+	}
+    }
+
+    if (print_id)
+	fmt = _(MSG_FMT_UNKNOWN_SPACE);
+    else
+	fmt = _(MSG_FMT_KNOWN_SPACE);
+
+    MakeString(cb, fmt, desc, addr->address,
+		addr->register_bit_offset, addr->register_bit_width,
+		space, addr->address_space_id);
+}
+
+void header_parser(struct ACPI_TABLE_DEF_HEADER *table, void (*cb)(const char *))
+{
+    MakeString(cb, "%s: %.4s %s %u",
+	       _(MSG_TABLE_SIGNATURE), &table->signature,
+	       _(MSG_REVISION), table->revision);    
+    MakeString(cb, "%s: %.6s", _(MSG_OEM_ID), &table->oem_id);
+    MakeString(cb, "%s: %.8s %s %u",
+	       _(MSG_OEM_TABLE_ID), &table->oem_table_id,
+	       _(MSG_REVISION), table->oem_revision);
+    MakeString(cb, "%s: %.4s %s %u",
+	       _(MSG_CREATOR_ID), &table->asl_compiler_id,
+	       _(MSG_REVISION), table->asl_compiler_revision);
+}
+
+void rsdt_parser(struct ACPI_TABLE_DEF_HEADER *rsdt, void (*cb)(const char *))
+{
+    header_parser(rsdt, cb);
+}
+
+static const char *Profiles[] =
+{
+    "Unspecified",
+    "Desktop",
+    "Mobile",
+    "Workstation",
+    "Enterprize server",
+    "SOHO server",
+    "Appliance",
+    "Performance server"
+};
+
+static const char *pc_flags[] =
+{
+    "Have legacy devices",
+    "Have 8042 keyboard controller",
+    "VGA not present",
+    "MSI not supported",
+    "PCIe ASPM controls",
+    NULL
+};
+
+static const char *facp_flags[] =
+{
+    "CPU has WBINVD instruction",
+    "WBINVD flushes all caches",
+    "C1 power state supported",
+    "C2 power state for multiple CPUs",
+    "No fixed power button",
+    "No fixed sleep button",
+    "RTC wake status in fixed registers",
+    "RTC wakeup from S4 supported",
+    "TMR_VAL is 32-bit",
+    "Docking supported",
+    "Reset register supported",
+    "Sealed case",
+    "CPU instruction execution needed for sleep",
+    "PCI Express wakeup supported",
+    "Use platform clock",
+    "RTC_STS valid after S4",
+    "Remote power on supported",
+    "Force APIC cluster model",
+    "Force APIC physical destination",
+    NULL
+};
+
+void facs_parser(struct ACPI_TABLE_TYPE_FADT *facs, void (*cb)(const char *))
+{
+    header_parser(&facs->header, cb);
+    
+    parse_enum(_(MSG_PM_PROFILE), facs->pm_profile, Profiles, cb);
+
+    MakeString(cb, "%s: %d", _(MSG_SCI_INT), facs->sci_int);
+    MakeString(cb, "%s: 0x%08X", _(MSG_SMI_CMD), facs->smi_cmd);
+    MakeString(cb, "%s: 0x%02X", _(MSG_ACPI_ENABLE), facs->acpi_enable);
+    MakeString(cb, "%s: 0x%02X", _(MSG_ACPI_DISABLE), facs->acpi_disable);
+    MakeString(cb, "%s: 0x%02X", _(MSG_S4BIOS), facs->s4bios_req);
+    MakeString(cb, "%s: 0x%02X", _(MSG_PSTATE), facs->pstate_cnt);
+    
+    if (!(facs->flags & FACP_FF_WBINVD))
+    {
+	MakeString(cb, "%s: %u", _(MSG_FLUSH_SIZE), facs->flush_size);
+	MakeString(cb, "%s: %u", _(MSG_FLUSH_STRIDE), facs->flush_stride);
+    }
+
+    if (facs->day_alarm)
+	MakeString(cb, "%s: 0x%02X", _(MSG_RTC_DAY_ALARM), facs->day_alarm);
+    if (facs->mon_alarm)
+	MakeString(cb, "%s: 0x%02X", _(MSG_RTC_MON_ALARM), facs->mon_alarm);
+    if (facs->century)
+	MakeString(cb, "%s: 0x%02X", _(MSG_RTC_CENTURY), facs->century);
+
+    MakeString(cb, "%s:", _(MSG_PC_FLAGS));
+    parse_flags(facs->pc_arch, pc_flags, cb);
+    
+    MakeString(cb, "%s:", _(MSG_FF_FLAGS));
+    parse_flags(facs->flags, facp_flags, cb);
+
+    parse_addr(_(MSG_RESET_REG), &facs->reset_reg, cb);
+    MakeString(cb, "%s: 0x%02X", _(MSG_RESET_VAL), facs->reset_value);
+}
+
+static const struct Parser Tables[] =
+{
+    {ACPI_MAKE_ID('R','S','D','T'), "System"  , rsdt_parser},
+    {ACPI_MAKE_ID('X','S','D','T'), "System"  , rsdt_parser},
+    {ACPI_MAKE_ID('F','A','C','S'), "Hardware", facs_parser},
+    {0, NULL, NULL}
+};
+
+const struct Parser *FindParser(unsigned int signature)
+{
+    const struct Parser *t;
+
+    for (t = Tables; t->name; t++)
+    {
+	if (signature == t->signature)
+	    return t;
+    }
+    return NULL;
+}
