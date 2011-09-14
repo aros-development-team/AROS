c94fefd0be2f63aec4159a6c500dbd99968c3af4
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-09-14 20:21:00 +0000

    ACPITool, work in progress
    git-svn-id: https://svn.aros.org/svn/aros/trunk@41348 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/all-pc/acpi/acpitool/locale.h
@@ -0,0 +1,23 @@
+#ifndef _LOCALE_H
+#define _LOCALE_H
+
+/*
+    Copyright © 2003, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <exec/types.h>
+
+#define CATCOMP_NUMBERS
+#include "strings.h"
+
+/*** Prototypes *************************************************************/
+/* Main *********************************************************************/
+STRPTR  _(ULONG ID);            /* Get a message, as a STRPTR */
+#define __(id) ((IPTR) _(id))   /* Get a message, as an IPTR */
+
+/* Setup ********************************************************************/
+BOOL Locale_Initialize(void);
+void Locale_Deinitialize(void);
+
+#endif /* _LOCALE_H */
---
+++ arch/all-pc/acpi/acpitool/parsers.h
@@ -0,0 +1,14 @@
+struct Parser
+{
+    unsigned int signature;
+    char *name;
+    void (*parser)();
+};
+
+#define BUFFER_SIZE 256
+
+extern char buf[BUFFER_SIZE];
+
+void header_parser(struct ACPI_TABLE_DEF_HEADER *table, void (*cb)(const char *));
+
+const struct Parser *FindParser(unsigned int signature);
