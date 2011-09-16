d2e9fa4502a385c089e3c3df5cd01f8932fb9a75
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-09-16 07:35:28 +0000

    Fixed MADT parser to work, use new acpi.resource function.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@41376 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-pc/acpi/acpitool/parsers.c
+++ arch/all-pc/acpi/acpitool/parsers.c
@@ -1,4 +1,5 @@
 #include <resources/acpi.h>
+#include <proto/acpi.h>
 
 #include <stdio.h>
 
@@ -67,7 +68,7 @@ const char *spaces[] =
     NULL
 };
 
-void parse_addr(const char *desc, struct GENERIC_ACPI_ADDR *addr, void (*cb)(const char *))
+static void parse_addr(const char *desc, struct GENERIC_ACPI_ADDR *addr, void (*cb)(const char *))
 {
     BOOL print_id = FALSE;
     const char *space;
@@ -102,9 +103,9 @@ void parse_addr(const char *desc, struct GENERIC_ACPI_ADDR *addr, void (*cb)(con
 
 void header_parser(struct ACPI_TABLE_DEF_HEADER *table, void (*cb)(const char *))
 {
-    MakeString(cb, "%s: %.4s %s %u",
+    MakeString(cb, "%s: %.4s %s %u, %s 0x%p",
 	       _(MSG_TABLE_SIGNATURE), &table->signature,
-	       _(MSG_REVISION), table->revision);    
+	       _(MSG_REVISION), table->revision, _(MSG_ADDRESS), table);
     MakeString(cb, "%s: %.6s", _(MSG_OEM_ID), &table->oem_id);
     MakeString(cb, "%s: %.8s %s %u",
 	       _(MSG_OEM_TABLE_ID), &table->oem_table_id,
@@ -114,7 +115,7 @@ void header_parser(struct ACPI_TABLE_DEF_HEADER *table, void (*cb)(const char *)
 	       _(MSG_REVISION), table->asl_compiler_revision);
 }
 
-void rsdt_parser(struct ACPI_TABLE_DEF_HEADER *rsdt, void (*cb)(const char *))
+static void rsdt_parser(struct ACPI_TABLE_DEF_HEADER *rsdt, void (*cb)(const char *))
 {
     header_parser(rsdt, cb);
 }
@@ -165,7 +166,7 @@ static const char *facp_flags[] =
     NULL
 };
 
-void fadt_parser(struct ACPI_TABLE_TYPE_FADT *fadt, void (*cb)(const char *))
+static void fadt_parser(struct ACPI_TABLE_TYPE_FADT *fadt, void (*cb)(const char *))
 {
     header_parser(&fadt->header, cb);
     
@@ -221,7 +222,7 @@ static const char *irq_trigger[] =
 
 static inline void parse_int_flags(ACPI_INT_FLAGS flags, void (*cb)(const char *))
 {
-/* FIXME: This does now work, deadlocks */
+/* FIXME: This does not work, deadlocks
     parse_enum(_(MSG_POLARITY), flags.polarity, irq_polarity, cb);
     parse_enum(_(MSG_TRIGGER), flags.trigger, irq_trigger, cb);
 */
@@ -242,9 +243,13 @@ static const char *int_types[] =
     NULL
 };
 
-void madt_parser(struct ACPI_TABLE_TYPE_MADT *madt, void (*cb)(const char *))
+AROS_UFH3(static void, madt_entry_parser,
+	  AROS_UFHA(struct Hook *, table_hook, A0),
+	  AROS_UFHA(struct ACPI_TABLE_DEF_ENTRY_HEADER *, entry, A2),
+	  AROS_UFHA(out_func, cb, A1))
 {
-    struct ACPI_TABLE_DEF_ENTRY_HEADER *entry;
+    AROS_USERFUNC_INIT
+
     struct ACPI_TABLE_TYPE_LAPIC *lapic;
     struct ACPI_TABLE_TYPE_IOAPIC *ioapic;
     struct ACPI_TABLE_TYPE_INT_SRCOVR *srcovr;
@@ -255,8 +260,134 @@ void madt_parser(struct ACPI_TABLE_TYPE_MADT *madt, void (*cb)(const char *))
     struct ACPI_TABLE_TYPE_PLAT_INTSRC *intsrc;
     struct ACPI_TABLE_TYPE_X2APIC *x2apic;
     struct ACPI_TABLE_TYPE_X2APIC_NMI *x2nmi;
-    int len = madt->header.length;
 
+    cb("");	/* Insert empty line */
+
+    switch (entry->type)
+    {
+    case ACPI_MADT_LAPIC:
+	lapic = (struct ACPI_TABLE_TYPE_LAPIC *)entry;
+
+	MakeString(cb, "%s:", _(MSG_LOCAL_APIC));
+	MakeString(cb, "  %s: 0x%02X", _(MSG_CPU_ID), lapic->acpi_id);
+	MakeString(cb, "  %s: 0x%02X", _(MSG_LAPIC_ID), lapic->id);
+	MakeString(cb, "  %s: %s", _(MSG_ENABLED), FLAG_VAL(lapic->flags.enabled));
+	break;
+
+    case ACPI_MADT_IOAPIC:
+	ioapic = (struct ACPI_TABLE_TYPE_IOAPIC *)entry;
+
+	MakeString(cb, "%s:", _(MSG_IOAPIC));
+	MakeString(cb, "  %s: 0x%02X", _(MSG_ID), ioapic->id);
+	MakeString(cb, "  %s: 0x%04X", _(MSG_ADDRESS), ioapic->address);
+	MakeString(cb, "  %s: %d", _(MSG_IRQ_BASE), ioapic->global_irq_base);
+	break;
+
+    case ACPI_MADT_INT_SRC_OVR:
+	srcovr = (struct ACPI_TABLE_TYPE_INT_SRCOVR *)entry;
+
+	MakeString(cb, "%s:", _(MSG_INT_SRC_OVR));
+	parse_enum(_(MSG_BUS), srcovr->bus, srcovr_buses, cb);
+	MakeString(cb, "  %s: %u", _(MSG_BUS_IRQ), srcovr->bus_irq);
+	MakeString(cb, "  %s: %u", _(MSG_GLOBAL_IRQ), srcovr->global_irq);
+	parse_int_flags(srcovr->flags, cb);
+	break;
+
+    case ACPI_MADT_NMI_SRC:
+    	nmisrc = (struct ACPI_TABLE_TYPE_LAPIC_NMI_SRC *)entry;
+
+	MakeString(cb, "%s:", _(MSG_NMI_SRC));
+	parse_int_flags(nmisrc->flags, cb);
+	MakeString(cb, "  %s: %u", _(MSG_GLOBAL_IRQ), nmisrc->global_irq);
+	break;
+
+    case ACPI_MADT_LAPIC_NMI:
+	nmi = (struct ACPI_TABLE_TYPE_LAPIC_NMI *)entry;
+
+	MakeString(cb, "%s:", _(MSG_LAPIC_NMI));
+	if (nmi->acpi_id == ACPI_ID_BROADCAST)
+	    MakeString(cb, "  %s: %s", _(MSG_CPU_ID), _(MSG_ALL));
+	else
+	    MakeString(cb, "  %s: 0x%02X", _(MSG_CPU_ID), nmi->acpi_id);
+	parse_int_flags(nmi->flags, cb);
+	MakeString(cb, "  %s: %u", _(MSG_LINT), nmi->lint);
+	break;
+
+    case ACPI_MADT_LAPIC_ADDR_OVR:
+	MakeString(cb, "%s: 0x%016llX", _(MSG_LAPIC_ADDR_OVR),
+		   ((struct ACPI_TABLE_TYPE_LAPIC_ADDROVR *)entry)->address);
+	break;
+
+    case ACPI_MADT_IOSAPIC:
+	iosapic = (struct ACPI_TABLE_TYPE_IOSAPIC *)entry;
+
+	MakeString(cb, "%s:", _(MSG_IOSAPIC));
+	MakeString(cb, "  %s: 0x%02X", _(MSG_ID), iosapic->id);
+	MakeString(cb, "  %s: %u", _(MSG_IRQ_BASE), iosapic->global_irq_base);
+	MakeString(cb, "  %s: 0x%016llX", _(MSG_ADDRESS), iosapic->address);
+	break;
+
+    case ACPI_MADT_LSAPIC:
+	lsapic = (struct ACPI_TABLE_TYPE_LSAPIC *)entry;
+
+	MakeString(cb, "%s:", _(MSG_LSAPIC));
+	MakeString(cb, "  %s: 0x%02X", _(MSG_CPU_ID), lsapic->acpi_id);
+	MakeString(cb, "  %s: 0x%02X", _(MSG_LSAPIC_ID), lsapic->id);
+	MakeString(cb, "  %s: 0x%02X", _(MSG_LSAPIC_EID), lsapic->eid);
+	MakeString(cb, "  %s: %s", _(MSG_ENABLED), FLAG_VAL(lsapic->flags.enabled));
+	break;
+
+    case ACPI_MADT_PLAT_INT_SRC:
+	intsrc = (struct ACPI_TABLE_TYPE_PLAT_INTSRC *)entry;
+
+	MakeString(cb, "%s:", _(MSG_PLAT_INT_SRC));
+	parse_int_flags(intsrc->flags, cb);
+	parse_enum(_(MSG_PLAT_INT_TYPE), intsrc->type, int_types, cb);
+	MakeString(cb, "  %s: 0x%02X", _(MSG_DEST_LSAPIC_ID), intsrc->id);
+	MakeString(cb, "  %s: 0x%02X", _(MSG_DEST_EID), intsrc->eid);
+	MakeString(cb, "  %s: 0x%02X", _(MSG_IOSAPIC_VECTOR), intsrc->iosapic_vector);
+	MakeString(cb, "  %s: %u", _(MSG_GLOBAL_IRQ), intsrc->global_irq);
+	MakeString(cb, "  %s: %s", _(MSG_CPEI_PROC_OVR),
+		   FLAG_VAL(intsrc->srcflags & ACPI_CPEI_PROC_OVERRIDE));
+	break;
+
+    case ACPI_MADT_X2APIC:
+	x2apic = (struct ACPI_TABLE_TYPE_X2APIC *)entry;
+
+	MakeString(cb, "%s:", _(MSG_LOCAL_X2APIC));
+	MakeString(cb, "  %s: 0x%08X", _(MSG_X2APIC_ID), x2apic->id);
+	MakeString(cb, "  %s: %s", _(MSG_ENABLED),
+		   FLAG_VAL(x2apic->flags & ACPI_X2APIC_ENABLED));
+	MakeString(cb, "  %s: 0x%08X", _(MSG_CPU_UID), x2apic->acpi_uid);
+	break;
+
+    case ACPI_MADT_X2APIC_NMI:
+	x2nmi = (struct ACPI_TABLE_TYPE_X2APIC_NMI *)entry;
+
+	MakeString(cb, "%s:",_(MSG_X2APIC_NMI));
+	parse_int_flags(x2nmi->flags, cb);
+	if (x2nmi->acpi_uid == ACPI_UID_BROADCAST)
+	    MakeString(cb, "  %s: %s", _(MSG_CPU_UID), _(MSG_ALL));
+	else
+	    MakeString(cb, "  %s: 0x%08X", _(MSG_CPU_UID), x2nmi->acpi_uid);
+	MakeString(cb, "  %s: %u", _(MSG_LINT), x2nmi->lint);
+	break;
+
+    default:
+	MakeString(cb, _(MSG_UNKNOWN_ENTRY), entry->type, entry->length);
+	break;
+    }
+
+    AROS_USERFUNC_EXIT
+}
+
+static const struct Hook madtHook =
+{
+    .h_Entry = (APTR)madt_entry_parser
+};
+
+static void madt_parser(struct ACPI_TABLE_TYPE_MADT *madt, void (*cb)(const char *))
+{
     D(printf("MADT at 0x%p, length %u\n", madt, len));
 
     header_parser(&madt->header, cb);
@@ -264,133 +395,7 @@ void madt_parser(struct ACPI_TABLE_TYPE_MADT *madt, void (*cb)(const char *))
     MakeString(cb, "%s: 0x%08X", _(MSG_LAPIC_ADDR), madt->lapic_address);
     MakeString(cb, "%s: %s", _(MSG_PCAT_COMPAT), FLAG_VAL(madt->flags.pcat_compat));
 
-    entry = madt->entries;
-    len -= sizeof(struct ACPI_TABLE_TYPE_MADT);
-
-    while (len > 0)
-    {
-	D(printf("MADT Entry 0x%p, length %u\n", entry, entry->length));
-
-        cb("");	/* Insert empty line */
-
-	switch (entry->type)
-	{
-	case ACPI_MADT_LAPIC:
-	    lapic = (struct ACPI_TABLE_TYPE_LAPIC *)entry;
-
-	    MakeString(cb, "%s:", _(MSG_LOCAL_APIC));
-	    MakeString(cb, "  %s: 0x%02X", _(MSG_CPU_ID), lapic->acpi_id);
-	    MakeString(cb, "  %s: 0x%02X", _(MSG_LAPIC_ID), lapic->id);
-	    MakeString(cb, "  %s: %s", _(MSG_ENABLED), FLAG_VAL(lapic->flags.enabled));
-	    break;
-
-	case ACPI_MADT_IOAPIC:
-	    ioapic = (struct ACPI_TABLE_TYPE_IOAPIC *)entry;
-	    
-	    MakeString(cb, "%s:", _(MSG_IOAPIC));
-	    MakeString(cb, "  %s: 0x%02X", _(MSG_ID), ioapic->id);
-	    MakeString(cb, "  %s: 0x%04X", _(MSG_ADDRESS), ioapic->address);
-	    MakeString(cb, "  %s: %d", _(MSG_IRQ_BASE), ioapic->global_irq_base);
-	    break;
-
-	case ACPI_MADT_INT_SRC_OVR:
-	    srcovr = (struct ACPI_TABLE_TYPE_INT_SRCOVR *)entry;
-
-	    MakeString(cb, "%s:", _(MSG_INT_SRC_OVR));
-	    parse_enum(_(MSG_BUS), srcovr->bus, srcovr_buses, cb);
-	    MakeString(cb, "  %s: %u", _(MSG_BUS_IRQ), srcovr->bus_irq);
-	    MakeString(cb, "  %s: %u", _(MSG_GLOBAL_IRQ), srcovr->global_irq);
-	    parse_int_flags(srcovr->flags, cb);
-	    break;
-
-	case ACPI_MADT_NMI_SRC:
-	    nmisrc = (struct ACPI_TABLE_TYPE_LAPIC_NMI_SRC *)entry;
-
-	    MakeString(cb, "%s:", _(MSG_NMI_SRC));
-	    parse_int_flags(nmisrc->flags, cb);
-	    MakeString(cb, "  %s: %u", _(MSG_GLOBAL_IRQ), nmisrc->global_irq);
-	    break;
-
-	case ACPI_MADT_LAPIC_NMI:
-	    nmi = (struct ACPI_TABLE_TYPE_LAPIC_NMI *)entry;
-
-	    MakeString(cb, "%s:", _(MSG_LAPIC_NMI));
-	    if (nmi->acpi_id == ACPI_ID_BROADCAST)
-		MakeString(cb, "  %s: %s", _(MSG_CPU_ID), _(MSG_ALL));
-	    else
-		MakeString(cb, "  %s: 0x%02X", _(MSG_CPU_ID), nmi->acpi_id);
-	    parse_int_flags(nmi->flags, cb);
-	    MakeString(cb, "  %s: %u", _(MSG_LINT), nmi->lint);
-	    break;
-
-	case ACPI_MADT_LAPIC_ADDR_OVR:
-	    MakeString(cb, "%s: 0x%016llX", _(MSG_LAPIC_ADDR_OVR),
-		       ((struct ACPI_TABLE_TYPE_LAPIC_ADDROVR *)entry)->address);
-	    break;
-
-	case ACPI_MADT_IOSAPIC:
-	    iosapic = (struct ACPI_TABLE_TYPE_IOSAPIC *)entry;
-
-	    MakeString(cb, "%s:", _(MSG_IOSAPIC));
-	    MakeString(cb, "  %s: 0x%02X", _(MSG_ID), iosapic->id);
-	    MakeString(cb, "  %s: %u", _(MSG_IRQ_BASE), iosapic->global_irq_base);
-	    MakeString(cb, "  %s: 0x%016llX", _(MSG_ADDRESS), iosapic->address);
-	    break;
-
-	case ACPI_MADT_LSAPIC:
-	    lsapic = (struct ACPI_TABLE_TYPE_LSAPIC *)entry;
-
-	    MakeString(cb, "%s:", _(MSG_LSAPIC));
-	    MakeString(cb, "  %s: 0x%02X", _(MSG_CPU_ID), lsapic->acpi_id);
-	    MakeString(cb, "  %s: 0x%02X", _(MSG_LSAPIC_ID), lsapic->id);
-	    MakeString(cb, "  %s: 0x%02X", _(MSG_LSAPIC_EID), lsapic->eid);
-	    MakeString(cb, "  %s: %s", _(MSG_ENABLED), FLAG_VAL(lsapic->flags.enabled));
-	    break;
-
-	case ACPI_MADT_PLAT_INT_SRC:
-	    intsrc = (struct ACPI_TABLE_TYPE_PLAT_INTSRC *)entry;
-
-	    MakeString(cb, "%s:", _(MSG_PLAT_INT_SRC));
-	    parse_int_flags(intsrc->flags, cb);
-	    parse_enum(_(MSG_PLAT_INT_TYPE), intsrc->type, int_types, cb);
-	    MakeString(cb, "  %s: 0x%02X", _(MSG_DEST_LSAPIC_ID), intsrc->id);
-	    MakeString(cb, "  %s: 0x%02X", _(MSG_DEST_EID), intsrc->eid);
-	    MakeString(cb, "  %s: 0x%02X", _(MSG_IOSAPIC_VECTOR), intsrc->iosapic_vector);
-	    MakeString(cb, "  %s: %u", _(MSG_GLOBAL_IRQ), intsrc->global_irq);
-	    MakeString(cb, "  %s: %s", _(MSG_CPEI_PROC_OVR),
-		       FLAG_VAL(intsrc->srcflags & ACPI_CPEI_PROC_OVERRIDE));
-	    break;
-
-	case ACPI_MADT_X2APIC:
-	    x2apic = (struct ACPI_TABLE_TYPE_X2APIC *)entry;
-
-	    MakeString(cb, "%s:", _(MSG_LOCAL_X2APIC));
-	    MakeString(cb, "  %s: 0x%08X", _(MSG_X2APIC_ID), x2apic->id);
-	    MakeString(cb, "  %s: %s", _(MSG_ENABLED),
-		       FLAG_VAL(x2apic->flags & ACPI_X2APIC_ENABLED));
-	    MakeString(cb, "  %s: 0x%08X", _(MSG_CPU_UID), x2apic->acpi_uid);
-	    break;
-
-	case ACPI_MADT_X2APIC_NMI:
-	    x2nmi = (struct ACPI_TABLE_TYPE_X2APIC_NMI *)entry;
-
-	    MakeString(cb, "%s:",_(MSG_X2APIC_NMI));
-	    parse_int_flags(x2nmi->flags, cb);
-	    if (x2nmi->acpi_uid == ACPI_UID_BROADCAST)
-		MakeString(cb, "  %s: %s", _(MSG_CPU_UID), _(MSG_ALL));
-	    else
-		MakeString(cb, "  %s: 0x%08X", _(MSG_CPU_UID), x2nmi->acpi_uid);
-	    MakeString(cb, "  %s: %u", _(MSG_LINT), x2nmi->lint);
-	    break;
-
-	default:
-	    MakeString(cb, _(MSG_UNKNOWN_ENTRY), entry->type, entry->length);
-	    break;
-	}
-	
-	entry = ((void *)entry + entry->length);
-	len -= entry->length;
-    }
+    ACPI_ScanEntries(&madt->header, ACPI_ENTRY_TYPE_ALL, &madtHook, cb);
 }
 
 static const struct Parser Tables[] =
@@ -398,7 +403,7 @@ static const struct Parser Tables[] =
     {ACPI_MAKE_ID('R','S','D','T'), "System"    , rsdt_parser},
     {ACPI_MAKE_ID('X','S','D','T'), "System"    , rsdt_parser},
     {ACPI_MAKE_ID('F','A','C','P'), "Hardware"  , fadt_parser},
-/*  {ACPI_MAKE_ID('A','P','I','C'), "Interrupts", madt_parser}, */
+    {ACPI_MAKE_ID('A','P','I','C'), "Interrupts", madt_parser},
     {0, NULL, NULL}
 };
 
