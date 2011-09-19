d08ea904ebc6632d730603b860b3953f313ea8d5
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-09-19 19:15:50 +0000

    Added HPET definitions
    git-svn-id: https://svn.aros.org/svn/aros/trunk@41474 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/resources/acpi.h
+++ compiler/include/resources/acpi.h
@@ -364,25 +364,33 @@ struct ACPI_TABLE_TYPE_X2APIC_NMI
 
 #define ACPI_UID_BROADCAST 0xFFFFFFFF
 
-struct ACPI_GEN_REGADDR
-{
-    unsigned char                           space_id;
-    unsigned char                           bit_width;
-    unsigned char                           bit_offset;
-    unsigned char                           resv;
-    unsigned int                            addrl;
-    unsigned int                            addrh;
-};
-
 struct ACPI_TABLE_TYPE_HPET
 {
     struct ACPI_TABLE_DEF_HEADER            header;
     unsigned int                            id;
-    struct ACPI_GEN_REGADDR                 addr;
+    struct GENERIC_ACPI_ADDR                addr;
     unsigned char                           number;
     UWORD                                   min_tick;
     unsigned char                           page_protect;
 };
+
+/* ID components */
+#define HPET_HW_REV_MASK		0x000000FF
+#define HPET_NUM_COMPARATORS_MASK	0x00001F00
+#define HPET_NUM_COMPARATORS_SHIFT	8
+#define HPET_COUNTER_SIZE		0x00002000
+#define HPET_LEGACY_REPLACEMENT		0x00008000
+#define HPET_PCI_VENDOR_MASK		0xFFFF0000
+#define HPET_PCI_VENDOR_SHIFT		16
+
+/* page_protect components */
+#define HPET_PAGE_PROTECT_MASK	0x0F
+#define HPET_OEM_ATTR_MASK	0xF0
+#define HPET_OEM_ATTR_SHIFT	4
+
+#define HPET_PAGE_NONE	0
+#define HPET_PAGE_4K	1
+#define HPET_PAGE_64K	2
 
 /*
     System Resource Affinity Table (SRAT) [ see http://www.microsoft.com/hwdev/design/srat.htm ]
