6f4f29a98a24bc4baa9dcf46e0c15cb5a2cf5b0d
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-09-20 12:14:54 +0000

    hpet.resource v1.0. Manages allocation of timer units.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@41484 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/all-pc/hpet/getunitattrsa.c
@@ -0,0 +1,71 @@
+#include <resources/hpet.h>
+#include <proto/arossupport.h>
+
+#include "hpet_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/hpet.h>
+
+AROS_LH2(BOOL, GetUnitAttrsA,
+
+/*  SYNOPSIS */
+	AROS_LHA(ULONG, unit, D0),
+	AROS_LHA(const struct TagItem *, tags, A0),
+
+/*  LOCATION */
+	struct HPETBase *, base, 4, Hpet)
+
+/*  FUNCTION
+	Query attributes of HPET unit.
+
+    INPUTS
+	unit - a number of previously allocated HPET unit.
+
+    RESULT
+    	TRUE in case of success or FALSE if the given unit number is out of range.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+******************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    struct TagItem *tag;
+
+    /* Owner is the only thing which can be modified, so we don't need a semaphore here */
+
+    if (unit >= base->unitCnt)
+    	return FALSE;
+
+    while ((tag = LibNextTagItem(&tags)))
+    {
+    	switch (tag->ti_Tag)
+    	{
+    	case HPET_BASE_ADDR:
+    	    *(IPTR *)tag->ti_Data = base->units[unit].base;
+    	    break;
+
+	case HPET_UNIT_ADDR:
+	    *(IPTR *)tag->ti_Data = base->units[unit].block;
+	    break;
+
+	case HPET_UNIT_OWNER:
+	    *(const char **)tag->ti_Data = base->units[unit].Owner;
+	    break;
+	}
+    }
+
+    return TRUE;
+
+    AROS_LIBFUNC_EXIT
+}
