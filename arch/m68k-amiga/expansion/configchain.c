97ec408cfc18590b9e320d1e95cfa2b40d0741ab
twilen <twilen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-09-26 18:16:48 +0000

    Added romboot resident that handles romtag init phase. This needs to be done before uaegfx is initialized because PicassoIV RTG board final initialization is done in romtag init.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@41608 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/m68k-amiga/expansion/configchain.c
+++ arch/m68k-amiga/expansion/configchain.c
@@ -16,71 +16,6 @@
 #include <clib/expansion_protos.h>
 #include <proto/exec.h>
 #include <exec/resident.h>
-#include <aros/asmcall.h>
-
-static void Enable68060SuperScalar(void)
-{
-    asm volatile (
-	".text\n"
-	/* enable supercalar */
-	"dc.l	0x4e7a0808\n"	// movec %pcr,%d0
-	"bset	#0,%d0\n"
-	"dc.l	0x4e7b0808\n"	// movec %d0,%pcr
-	/* enable code&data caches, store buffer and branch cache */
-	"dc.l	0x4e7a0002\n"	// movec %cacr,%d0
-	"or.l	#0xa0808000,%d0\n"
-	"dc.l	0x4e7b0002\n"	// movec %d0,%cacr
-	"rte\n"
-    );
-}
-static ULONG Check68030MMU(void)
-{
-    register UWORD ret asm("%d0");
-    asm volatile (
-    	".chip 68030\n"
-    	"subq.l	#4,%%sp\n"
-    	"pmove	%%tc,(%%sp)\n"
-    	"move.l	(%%sp),%%d0\n"
-    	"addq.l	#4,%%sp\n"
-    	"rte\n"
-    	: "=r" (ret)
-    );
-    return ret;
-};
-	
-
-// ROMTAG INIT time
-static void romtaginit(struct ExpansionBase *ExpansionBase)
-{
-	struct Node *node;
-	// look for possible romtags in expansion ROM image and InitResident() them if found
-	D(bug("romtaginit\n"));
-	ObtainConfigBinding();
-	ForeachNode(&IntExpBase(ExpansionBase)->eb_BoardList, node) {
-		struct ConfigDev *configDev = (struct ConfigDev*)node;
-		if (configDev->cd_Rom.er_DiagArea && (configDev->cd_Rom.er_DiagArea->da_Config & DAC_BOOTTIME) == DAC_CONFIGTIME && (configDev->cd_Flags & CDF_CONFIGME)) {
-			struct Resident *res;
-			UWORD *romptr = (UWORD*)configDev->cd_Rom.er_DiagArea;
-			UWORD *romend = (UWORD*)(((UBYTE*)configDev->cd_Rom.er_DiagArea) + configDev->cd_Rom.er_DiagArea->da_Size - 26); // 26 = real sizeof(struct Resident)!
-			struct CurrentBinding cb = {
-			    .cb_ConfigDev = configDev
-			};
-			SetCurrentBinding(&cb, sizeof(cb));
-			while (romptr <= romend) {
-				res = (struct Resident*)romptr;
-				if (res->rt_MatchWord == RTC_MATCHWORD && res->rt_MatchTag == res) {
-					D(bug("Diag board %08x InitResident %08x (%d %02x '%s')\n",
-						configDev->cd_BoardAddr, res, res->rt_Pri, res->rt_Flags, res->rt_Name));
-					InitResident(res, BNULL);
-					break; /* must not keep looking */
-				}
-				romptr++;
-			}
-		}
-	}
-	ReleaseConfigBinding();
-	D(bug("romtaginit done\n"));
-}
 
 AROS_UFP3(ULONG, MemoryTest,
     AROS_UFPA(APTR, startaddr, A0),
@@ -147,26 +82,6 @@ AROS_LH1(void, ConfigChain,
 
 	struct ConfigDev *configDev = NULL;
 
-	if (baseAddr == 0) {
-		// called by strap
-		romtaginit(ExpansionBase);
-
-		// enable 68040+ data caches and 68060 superscalar mode
-		// this is not the right place but we can't enable them
-		// any earlier (memory detection, boot roms that breaks if
-		// full 68060 caching enabled)
-		if (SysBase->AttnFlags & AFF_68060) {
-			Supervisor((ULONG_FUNC)Enable68060SuperScalar);
-		} else if (SysBase->AttnFlags & AFF_68040) {
-			CacheControl(CACRF_EnableD, CACRF_EnableD);
-		} else if (SysBase->AttnFlags & AFF_68030) {
-			ULONG tc = Supervisor((ULONG_FUNC)Check68030MMU);
-			if (tc & (1 << 31)) { /* Only if MMU enabled */
-				CacheControl(CACRF_EnableD, CACRF_EnableD);
-			}
-		}
-		return;
-	}
 	/* Try to guess if we have Z3 based machine.
 	 * Not all Z3 boards appear in Z2 region.
 	 *
