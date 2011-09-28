c250b34d7c6b1767a4cc479370b41d0ff643c020
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-09-28 06:34:18 +0000

    Rewritten similar to Android. Re-vitalized iOS build.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@41649 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/all-ios/kernel/_displayalert.c
@@ -0,0 +1,69 @@
+/*
+    Copyright © 1995-2011, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: Display an alert, iOS-hosted version
+    Lang: english
+*/
+
+#include <aros/atomic.h>
+#include <aros/libcall.h>
+#include <proto/exec.h>
+
+#include <inttypes.h>
+#include <signal.h>
+
+#include "hostinterface.h"
+#include "kernel_base.h"
+#include "kernel_debug.h"
+#include "kernel_intern.h"
+
+/* This comes from kernel_startup.c */
+extern struct HostInterface *HostIFace;
+
+static void (*displayAlert)(const char *text) = NULL;
+
+void krnDisplayAlert(const char *text, struct KernelBase *KernelBase)
+{
+    if (!displayAlert)
+    {
+	/*
+	 * Early alert. call hook is not initialized yet.
+	 * Fail back to debug output.
+	 */
+    	bug(text);
+    	return;
+    }
+
+    /* Display the alert via our UIKit helper */
+    displayAlert(text);
+    AROS_HOST_BARRIER
+}
+
+static int Alert_Init(void)
+{
+    APTR libHandle;
+    char *err;
+
+    /*
+     * We use local variable for the handle because we never expunge
+     * so we will never close it
+     */
+    libHandle = HostIFace->hostlib_Open("Libs/Host/alert.dylib", &err);
+    if (!libHandle)
+    {
+    	bug("Failed to load alert.dylib: %s\n", err);
+	return FALSE;
+    }
+
+    displayAlert = HostIFace->hostlib_GetPointer(libHandle, "DisplayAlert", &err);
+    if (displayAlert)
+	return TRUE;
+
+    HostIFace->hostlib_Close(libHandle, NULL);
+
+    bug("Failed to load DisplayAlert helper: %s\n", err);
+    return FALSE;
+}
+
+ADD2INIT(Alert_Init, 0);
