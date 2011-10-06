146d16980cf90ce169319a667f0a5447f97e4278
deadwood-pl <deadwood@wp.pl>
2011-10-06 19:34:14 +0000

    Fix amiga-m68k build.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@41771 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/mesa/src/gallium/include/pipe/p_config.h
+++ workbench/libs/mesa/src/gallium/include/pipe/p_config.h
@@ -141,7 +141,7 @@
 
 #if defined(PIPE_ARCH_X86) || defined(PIPE_ARCH_X86_64) || defined(PIPE_ARCH_ARM)
 #define PIPE_ARCH_LITTLE_ENDIAN
-#elif defined(PIPE_ARCH_PPC) || defined(PIPE_ARCH_PPC_64) || defined(PIPE_ARCH_M68k)
+#elif defined(PIPE_ARCH_PPC) || defined(PIPE_ARCH_PPC_64) || defined(PIPE_ARCH_M68K)
 #define PIPE_ARCH_BIG_ENDIAN
 #endif
 
