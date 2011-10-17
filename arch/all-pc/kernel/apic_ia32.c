06236ed9b1c569e43b1a6fc26cae96ae9cb236f8
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-10-17 20:28:28 +0000

    Fixed debug output
    git-svn-id: https://svn.aros.org/svn/aros/trunk@41904 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-pc/kernel/apic_ia32.c
+++ arch/all-pc/kernel/apic_ia32.c
@@ -193,8 +193,8 @@ ULONG core_APIC_Wake(APTR wake_apicstartrip, UBYTE wake_apicid, IPTR __APICBase)
     ULONG apic_ver = APIC_REG(__APICBase, APIC_VERSION);
 #endif
 
-    D(bug("[APIC] _APIC_IA32_wake(%d @ %p)\n", wake_apicid, wake_apicstartrip));
-    D(bug("[APIC] _APIC_IA32_wake: APIC ID %d Base @ %p\n", _APIC_IA32_GetID(__APICBase), __APICBase));
+    D(bug("[APIC] _APIC_IA32_wake(0x%02X @ %p)\n", wake_apicid, wake_apicstartrip));
+    D(bug("[APIC] _APIC_IA32_wake: APIC ID 0x%02X Base @ %p\n", core_APIC_GetID(__APICBase), __APICBase));
 
 #ifdef CONFIG_LEGACY
     /*
