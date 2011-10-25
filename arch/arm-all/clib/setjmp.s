f0bb961f7d5040b2bf471ecc9ac8ff463af38f85
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-10-25 10:44:06 +0000

    Don't use deprecated X form of ldmia/stmia. Fixes "Illegal instruction" crash on certain CPUs where X form has been dropped.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42024 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-all/clib/setjmp.s
+++ arch/arm-all/clib/setjmp.s
@@ -17,7 +17,7 @@ AROS_CDEFNAME(setjmp):
 	mov	ip, r0						/* Get the env address */
 	str	lr, [ip], #4					/* store return address explicitly */
 	stmia	ip!, {r4, r5, r6, r7, r8, r9, sl, fp, sp}	/* store non-scratch regs */
-	fstmiax	ip!, {d8-d15}					/* Store VFP registers - we assume they are available! */
+	fstmiad	ip!, {d8-d15}					/* Store VFP registers - we assume they are available! */
 	fmrx	r2, fpscr					/* VFP condition codes */
 	str	r2, [ip], #4
         ldr	r0, 1f						/* Save *(SysBase->ThisTask->tc_SPLower) */
--- arch/arm-all/clib/vfork_longjmp.s
+++ arch/arm-all/clib/vfork_longjmp.s
@@ -19,7 +19,7 @@ AROS_CDEFNAME(vfork_longjmp):
 	ldr	lr, [ip], #4					/* restore return address */
 	mov	r0, r1						/* return value from longjmp into r0 and generate condition code */
 	ldmia	ip!, {r4, r5, r6, r7, r8, r9, sl, fp, sp}	/* restore non-scratch regs */
-	fldmiax ip!, {d8-d15}					/* Restore VFP registers - we assume they are available! */
+	fldmiad ip!, {d8-d15}					/* Restore VFP registers - we assume they are available! */
 	ldr     r1, [ip], #4					/* restore VFP status reg */
   	fmxr    fpscr, r1
   	ldr	r1, [ip], #4					/* restore *(SysBase->ThisTask->tc_SPLower) */
