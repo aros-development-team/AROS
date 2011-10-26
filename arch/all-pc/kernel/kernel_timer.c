7700a761184e403a0a713fa54604234f33c435c4
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-10-26 11:55:46 +0000

    - Restructured APIC code to better support multicore systems. Now allows per-core LAPIC initialization. - Pick up IOAPIC base address. - Implemented timer calibration (still a bit inaccurate)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42057 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-pc/kernel/kernel_timer.c
+++ arch/all-pc/kernel/kernel_timer.c
@@ -6,6 +6,7 @@
 
 #define D(x)
 
+/* See timer/ticks.c for the explanation */
 static const unsigned int timer_rpr = 3599597124UL;
 
 static inline unsigned int usec2tick(unsigned int usec)
@@ -19,30 +20,9 @@ static inline unsigned int usec2tick(unsigned int usec)
 void pit_udelay(unsigned int usec)
 {
     unsigned int start = usec2tick(usec);
-    unsigned int tick;
 
     D(bug("[PIT] udelay: %d usec = %d ticks\n", usec, start));
 
-    /*
-     * Start up channel 0 in 'terminal count' mode (mode 0).
-     * It will count down to zero, and then will issue an interrupt,
-     * but we don't care about it since interrupts are neither used nor enabled.
-     * It's safe to use the timer freely here. timer.device starts up long after
-     * this code.
-     */
-    outb(CH0|ACCESS_FULL|MODE_TERMINAL, PIT_CONTROL);
-    ch_write(start, PIT_CH0);
-
-    /*
-     * In this mode the counter wraps around to 0xFFFF.
-     * It's assumed that the initial value is reasonably lower,
-     * and the CPU has time to detect the wraparound.
-     */
-    do
-    {
-        outb(CH0|ACCESS_LATCH, PIT_CONTROL);
-        tick = ch_read(PIT_CH0);
-    } while ((tick > 0) && (tick < start));
-
-    D(bug("[PIT] udelay done\n"));
+    pit_start(start);
+    pit_wait(start);
 }
--- arch/all-pc/kernel/kernel_timer.h
+++ arch/all-pc/kernel/kernel_timer.h
@@ -1,1 +1,34 @@
+#include <hardware/pit.h>
+
+/*
+ * Start up channel 0 in 'terminal count' mode (mode 0).
+ * It will count down to zero, and then will issue a single interrupt,
+ * but we don't care about it since interrupts are neither used nor enabled.
+ * It's safe to use the timer freely here. timer.device starts up long after
+ * this code.
+ */
+static inline void pit_start(unsigned short start)
+{
+    outb(CH0|ACCESS_FULL|MODE_TERMINAL, PIT_CONTROL);
+    ch_write(start, PIT_CH0);
+}
+
+/*
+ * In this mode the counter wraps around to 0xFFFF and continues decrementing.
+ * It's assumed that the start value is reasonably lower, and the CPU has time
+ * to detect the wraparound.
+ */
+static inline unsigned short pit_wait(unsigned short start)
+{
+    unsigned short tick;
+
+    do
+    {
+        outb(CH0|ACCESS_LATCH, PIT_CONTROL);
+        tick = ch_read(PIT_CH0);
+    } while ((tick > 0) && (tick < start));
+
+    return tick;
+}
+
 void pit_udelay(unsigned int usec);
