0c9f75b25c7f0cbc6ddccace95bbf589aec05b93
twilen <twilen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-10-26 18:45:42 +0000

    Fully implement CacheClearE(), increases performance, especially on 040/060 CPUs.
    - 68020+: only flush requested cache type(s).
    - 68040/060: flush cache pages instead of doing complete cache flush.
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42071 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/m68k-all/exec/cachecleare.c
+++ arch/m68k-all/exec/cachecleare.c
@@ -10,6 +10,10 @@
 #include <exec/execbase.h>
 #include <aros/libcall.h>
 
+extern void AROS_SLIB_ENTRY(CacheClearE_00,Exec,107)(void);
+extern void AROS_SLIB_ENTRY(CacheClearE_20,Exec,107)(void);
+extern void AROS_SLIB_ENTRY(CacheClearE_4060,Exec,107)(void);
+
 /*****************************************************************************
 
     NAME */
@@ -72,8 +76,31 @@
 {
     AROS_LIBFUNC_INIT
 
-    /* Just clear everything */
-    CacheClearU();
+    void (*func)();
+
+    if (SysBase->LibNode.lib_OpenCnt == 0)
+    	return;
+
+    Disable();
+    if (SysBase->AttnFlags & AFF_68060) {
+        /* 68060 support */
+        func = AROS_SLIB_ENTRY(CacheClearE_4060, Exec, 107);
+    } else if (SysBase->AttnFlags & AFF_68040) {
+        /* 68040 support */
+        func = AROS_SLIB_ENTRY(CacheClearE_4060, Exec, 107);
+    } else if (SysBase->AttnFlags & AFF_68020) {
+        /* 68020 support */
+        func = AROS_SLIB_ENTRY(CacheClearE_20, Exec, 107);
+    } else {
+        /* Everybody else (68000, 68010) */
+        func = AROS_SLIB_ENTRY(CacheClearE_00, Exec, 107);
+    }
+    AROS_UFC3(void, func,
+	AROS_UFCA(APTR, address, A0),
+	AROS_UFCA(ULONG, length, D0),
+	AROS_UFCA(ULONG, caches, D1));
+    SetFunction((struct Library *)SysBase, -LIB_VECTSIZE * 107, func);
+    Enable();
 
     AROS_LIBFUNC_EXIT
 } /* CacheClearE */
