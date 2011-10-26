3bca7b075fd1325df74388a267a55e8dd23dd6d1
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-10-26 20:31:46 +0000

    Implemented AP support functions. ShowConfig reports secondary CPUs.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42074 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/i386-pc/kernel/getcpucount.c
@@ -0,0 +1,16 @@
+#include <aros/kernel.h>
+#include <aros/libcall.h>
+
+#include "kernel_base.h"
+#include "kernel_intern.h"
+#include "apic.h"
+
+AROS_LH0(unsigned int, KrnGetCPUCount,
+	 struct KernelBase *, KernelBase, 36, Kernel)
+{
+    AROS_LIBFUNC_INIT
+
+    return KernelBase->kb_PlatformData->kb_APIC->count;
+
+    AROS_LIBFUNC_EXIT
+}
---
+++ arch/i386-pc/kernel/getcpunumber.c
@@ -0,0 +1,16 @@
+#include <aros/kernel.h>
+#include <aros/libcall.h>
+
+#include "kernel_base.h"
+#include "kernel_intern.h"
+#include "apic.h"
+
+AROS_LH0(unsigned int, KrnGetCPUNumber,
+	 struct KernelBase *, KernelBase, 37, Kernel)
+{
+    AROS_LIBFUNC_INIT
+
+    return core_APIC_GetNumber(KernelBase->kb_PlatformData->kb_APIC);
+
+    AROS_LIBFUNC_EXIT
+}
