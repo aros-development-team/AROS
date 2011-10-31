c7a63c3aba17597576ceb594591c7ea4a548a1ee
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-10-31 01:50:58 +0000

    exec.library: Add some CPP red tape to prevent double includes
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42137 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/exec_platform.h
+++ rom/exec/exec_platform.h
@@ -2,8 +2,12 @@
  * This file can be overriden in arch/all-$(ARCH)/exec. Currently
  * used only by Windows-hosted port
  */
+#ifndef __EXEC_PLATFORM_H
+#define __EXEC_PLATFORM_H
 
 struct Exec_PlatformData
 {
     /* No platform-specific data by default */
 };
+
+#endif /* __EXEC_PLATFORM_H */
