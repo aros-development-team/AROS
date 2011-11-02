c088f06423648f587c081a6747c52ebf05232971
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-11-02 02:21:57 +0000

    keyboard.hidd: Eliminate .bss from stubs
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42185 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/hidds/kbd/kbd_stubs.c
+++ rom/hidds/kbd/kbd_stubs.c
@@ -1,18 +1,20 @@
 #include <hidd/keyboard.h>
 #include <proto/oop.h>
-#include <oop/static_mid.h>
+
+#include "kbd.h"
 
 #undef OOPBase
 #define OOPBase	(OOP_OOPBASE(obj))
 
 OOP_Object *HIDD_Kbd_AddHardwareDriver(OOP_Object *obj, OOP_Class *driverClass, struct TagItem *tags)
 {
-    STATIC_MID;
+    struct kbd_staticdata *csd = CSD(OOP_OCLASS(obj));
     struct pHidd_Kbd_AddHardwareDriver p, *msg = &p;
 
-    if (!static_mid) static_mid = OOP_GetMethodID(IID_Hidd_Kbd, moHidd_Kbd_AddHardwareDriver);
+    if (!csd->cs_mHidd_Kbd_AddHardwareDriver)
+        csd->cs_mHidd_Kbd_AddHardwareDriver = OOP_GetMethodID(IID_Hidd_Kbd, moHidd_Kbd_AddHardwareDriver);
 
-    p.mID = static_mid;
+    p.mID = csd->cs_mHidd_Kbd_AddHardwareDriver;
     p.driverClass = driverClass;
     p.tags = tags;
 
@@ -21,12 +23,13 @@ OOP_Object *HIDD_Kbd_AddHardwareDriver(OOP_Object *obj, OOP_Class *driverClass, 
 
 void HIDD_Kbd_RemHardwareDriver(OOP_Object *obj, OOP_Object *driver)
 {
-    STATIC_MID;
+    struct kbd_staticdata *csd = CSD(OOP_OCLASS(obj));
     struct pHidd_Kbd_RemHardwareDriver p, *msg = &p;
 
-    if (!static_mid) static_mid = OOP_GetMethodID(IID_Hidd_Kbd, moHidd_Kbd_RemHardwareDriver);
+    if (!csd->cs_mHidd_Kbd_RemHardwareDriver)
+        csd->cs_mHidd_Kbd_RemHardwareDriver = OOP_GetMethodID(IID_Hidd_Kbd, moHidd_Kbd_RemHardwareDriver);
 
-    p.mID = static_mid;
+    p.mID = csd->cs_mHidd_Kbd_RemHardwareDriver;
     p.driverObject = driver;
 
     OOP_DoMethod(obj, (OOP_Msg) msg);
