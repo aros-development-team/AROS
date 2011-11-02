8c0e2fded56e6bcbc93d8ed1d67b290e87873a28
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-11-02 02:21:44 +0000

    kernel.resource: Move global variables to a separate file
    Eliminates conditional code in kernel_init.c
    
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42183 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/m68k-amiga/kernel/kernel_globals.c
@@ -0,0 +1,8 @@
+/*
+ * Copyright (C) 2011, The AROS Development Team.  All rights reserved.
+ * Author: Jason S. McMullan <jason.mcmullan@gmail.com>
+ *
+ * Licensed under the AROS PUBLIC LICENSE (APL) Version 1.1
+ */
+
+/* This file intentionally empty */
