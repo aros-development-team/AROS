49b124bb25fbc23edce678c610f0d4534320f9f1
deadwood-pl <deadwood@wp.pl>
2011-11-02 21:46:18 +0000

    Add second search path for sigcontext.h. Needed for Ubuntu 11.10.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42220 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-linux/kernel/mksigcore.sh
+++ arch/all-linux/kernel/mksigcore.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-if [ ! -f /usr/include/bits/sigcontext.h ] ; then
-    echo "Could not find bits/sigcontext.h"
-    exit 20
+sigcontextpath=/usr/include/bits/sigcontext.h
+if [ ! -f $sigcontextpath ] ; then
+    sigcontextpath=/usr/include/i386-linux-gnu/bits/sigcontext.h
+    if [ ! -f $sigcontextpath ] ; then
+        echo "Could not find bits/sigcontext.h"
+        exit 20
+    fi
 fi
 
-type=`${CC} -D_SIGNAL_H -E /usr/include/bits/sigcontext.h | grep "^struct sigcontext" | sed 's/{//'`
+type=`${CC} -D_SIGNAL_H -E ${sigcontextpath} | grep "^struct sigcontext" | sed 's/{//'`
 handler=__sighandler_t
 
 sed "s/@sigcontext@/$type/;s/@sighandler@/$handler/" ${1-.}/sigcore.h.${CPU}.src > ${2}
