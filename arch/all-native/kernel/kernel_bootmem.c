909e1c6bdf13ec0a7f61727745d0645653422e9d
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-11-02 06:52:20 +0000

    Added boot-time region size and consistency checks. TODO: Implement for i386 too (set BootMemLimit)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42192 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-native/kernel/kernel_bootmem.c
+++ arch/all-native/kernel/kernel_bootmem.c
@@ -9,7 +9,9 @@
 #include <aros/macros.h>
 #include <string.h>
 
+#include "kernel_base.h"
 #include "kernel_bootmem.h"
+#include "kernel_debug.h"
 
 void *krnAllocBootMem(unsigned long size)
 {
@@ -21,7 +23,15 @@ void *krnAllocBootMemAligned(unsigned long size, unsigned int align)
     void *addr = (void *)AROS_ROUNDUP2((unsigned long)BootMemPtr, align);
     void *end = addr + size;
 
-    /* TODO: Implement limit check */
+#ifdef __x86_64__
+    /* FIXME: Only x86-64 currently sets BootMemLimit */
+    if (end > BootMemLimit)
+    {
+	/* Our allocation must succeed. We can't continue without it. */
+    	krnPanic(NULL, "Not enough memory for boot information\n"
+    		       "Increase reserved space in bootstrap");
+    }
+#endif
 
     BootMemPtr = end;
     /* Clear the allocated memory. In many places we expect it. */
