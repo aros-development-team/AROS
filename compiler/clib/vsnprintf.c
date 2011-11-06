cba7ce9d685e6949f9f59cf405418965bd08df88
Sami <Sami@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-11-06 12:42:20 +0000

    Fix snprintf/vsnprintf to be more C99 compliant:  - Should always add a terminating \0.  - If output is NULL or size is zero, nothing is written.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42294 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/vsnprintf.c
+++ compiler/clib/vsnprintf.c
@@ -18,8 +18,8 @@ static int _vsnprintf_uc (int c, struct data * data)
 {
     if (data->n)
     {
-	*(data->str) ++ = c;
-	data->n --;
+        *(data->str) ++ = c;
+        data->n --;
     }
 
     return 1;
@@ -28,35 +28,39 @@ static int _vsnprintf_uc (int c, struct data * data)
 /*****************************************************************************
 
     NAME */
-	#include <stdio.h>
+#include <stdio.h>
 #include <stdarg.h>
 
-	int vsnprintf (
+int vsnprintf (
 
 /*  SYNOPSIS */
-	char	   * str,
-	size_t	     n,
-	const char * format,
-	va_list      args)
+    char        * str,
+    size_t      n,
+    const char * format,
+    va_list      args)
 
 /*  FUNCTION
-	Format a list of arguments and put them into the string str.
-	The function makes sure that no more than n characters (including
-	the terminal 0 byte) are written into str.
-
+    Format a list of arguments and put them into the string str.
+    The function makes sure that no more than n characters (including
+    the terminal 0 byte) are written into str.
+    If n is zero, nothing is written, and s may be a null pointer. Otherwise,
+    output characters beyond the n-1st are discarded rather than being written
+    to the array, and a null character is written at the end of the characters
+    actually written into the array. If copying takes place between objects
+    that overlap, the behavior is undefined.
+    
     INPUTS
-	str - The formatted result is stored here
-	n - The size of str
-	format - A printf() format string.
-	args - A list of arguments for the format string.
+    str - The formatted result is stored here
+    n - The size of str
+    format - A printf() format string.
+    args - A list of arguments for the format string.
 
     RESULT
-	The number of characters written into the string. The 0 byte at the
-	end is not included. If this is greater than or equal to n then
-	there was not enough room to write all characters. In this case the
-	output string is not null-terminated, and the return value is the
-	number of characters which would have been written if enough space had
-	been available.
+    Function returns the number of characters that would have been
+    written had n been sufficiently large, not counting the terminating null
+    character, or a negative value if an encoding error occurred. Thus, the
+    null-terminated output has been completely written if and only if the
+    returned value is nonnegative and less than n.
 
     NOTES
 
@@ -65,8 +69,8 @@ static int _vsnprintf_uc (int c, struct data * data)
     BUGS
 
     SEE ALSO
-	printf(), sprintf(), fprintf(), vprintf(), vfprintf(), snprintf(),
-	vsnprintf()
+    printf(), sprintf(), fprintf(), vprintf(), vfprintf(), snprintf(),
+    vsnprintf()
 
     INTERNALS
 
@@ -75,6 +79,8 @@ static int _vsnprintf_uc (int c, struct data * data)
     int rc;
     struct data data;
 
+    if (str == NULL && n != 0) n = 0;
+
     data.n = n;
     data.str = str;
 
@@ -82,8 +88,13 @@ static int _vsnprintf_uc (int c, struct data * data)
 
     if (data.n)
     {
-	*(data.str) = 0;
+        *(data.str) = 0;
+    }
+    else if ( n != 0 )
+    {
+        str[n-1] = 0;
     }
 
     return rc;
 } /* vsnprintf */
+
