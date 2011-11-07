dafb9d447106ef6f32b36e8fb2831b745317221d
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-11-07 08:40:08 +0000

    Enable IRQ2 at initialization stage. Should fix IRQs 8 - 15.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42302 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-pc/kernel/xtpic.c
+++ arch/all-pc/kernel/xtpic.c
@@ -12,19 +12,25 @@
 
 void XTPIC_Init(uint16_t *irqmask)
 {
-    /* Everything is disabled at init */
-    *irqmask = 0xFFFF;
+    /*
+     * After initialization everything is disabled except cascade interrupt (IRQ 2).
+     * Be careful and don't miss this! Without IRQ2 enabled slave 8259 (and its IRQs)
+     * will not work!
+     */
+    *irqmask = 0xFFFB;
 
-    /* Setup the 8259 */
-    asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x11),"i"(0x20)); /* Initialization sequence for 8259A-1 */
-    asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x11),"i"(0xa0)); /* Initialization sequence for 8259A-2 */
-    asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x20),"i"(0x21)); /* IRQs at 0x20 - 0x27 */
-    asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x28),"i"(0xa1)); /* IRQs at 0x28 - 0x2f */
-    asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x04),"i"(0x21)); /* 8259A-1 is master */
-    asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x02),"i"(0xa1)); /* 8259A-2 is slave */
-    asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x01),"i"(0x21)); /* 8086 mode for both */
+    /* Setup the 8259. Send four ICWs (see 8529 datasheet) */
+    asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x11),"i"(0x20)); /* Initialization sequence for 8259A-1 (edge-triggered, cascaded, ICW4 needed) */
+    asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x11),"i"(0xa0)); /* Initialization sequence for 8259A-2, the same as above */
+    asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x20),"i"(0x21)); /* IRQs for master at 0x20 - 0x27 */
+    asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x28),"i"(0xa1)); /* IRQs for slave at 0x28 - 0x2f */
+    asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x04),"i"(0x21)); /* 8259A-1 is master, slave at IRQ2 */
+    asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x02),"i"(0xa1)); /* 8259A-2 is slave, ID = 2 */
+    asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x01),"i"(0x21)); /* 8086 mode, non-buffered, nonspecial fully nested mode for both */
     asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x01),"i"(0xa1));
-    asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0xff),"i"(0x21)); /* Enable cascade int */
+    
+    /* Now initialize interrupt masks */
+    asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0xfb),"i"(0x21)); /* Enable cascade int */
     asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0xff),"i"(0xa1)); /* Mask all interrupts */
 }
 
