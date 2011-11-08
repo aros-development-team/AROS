ec0f17916fcaca9ba2c2963f7df833b674c9cc3f
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-11-08 08:48:42 +0000

    Reverted FPU instruction changes. Qualcomm MSM7225 seems not to have VFP at all. Investigation is in progress. adaptation will come later.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42312 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-all/clib/setjmp.s
+++ arch/arm-all/clib/setjmp.s
@@ -17,15 +17,8 @@ AROS_CDEFNAME(setjmp):
 	mov	ip, r0						/* Get the env address */
 	str	lr, [ip], #4					/* store return address explicitly */
 	stmia	ip!, {r4, r5, r6, r7, r8, r9, sl, fp, sp}	/* store non-scratch regs */
-	fstd	d8, [ip]					/* Store VFP registers - we assume they are available! */
-	fstd	d9, [ip, #8]
-	fstd	d10, [ip, #16]
-	fstd	d11, [ip, #24]
-	fstd	d12, [ip, #32]
-	fstd	d13, [ip, #40]
-	fstd	d14, [ip, #48]
-	fstd	d15, [ip, #56]
+	fstmiax	ip!, {d8-d15}					/* Store VFP registers - we assume they are available! */
 	fmrx	r2, fpscr					/* VFP condition codes */
-	str	r2, [ip, #64]
+	str	r2, [ip], #4
 	mov	r0, #0						/* return zero */
 	bx	lr
