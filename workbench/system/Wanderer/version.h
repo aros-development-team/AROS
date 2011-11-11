92ea1924ab9f4c7a598a28099d6356720d09f8fe
deadwood-pl <deadwood@wp.pl>
2011-11-11 15:16:43 +0000

    Update Wanderer version.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42407 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/system/Wanderer/version.h
+++ workbench/system/Wanderer/version.h
@@ -1,6 +1,6 @@
 #define WANDERERVERS    0
-#define WANDERERREV     90
-#define WANDERERSTR     "Wanderer 0.90 (11.11.2011)"
+#define WANDERERREV     91
+#define WANDERERSTR     "Wanderer 0.91 (11.11.2011)"
 #define WANDERERAUTH    "The AROS Dev Team"
 #define WANDERERCOPY    "© " WANDERERAUTH
 #define VERSION "$VER:  " WANDERERSTR " " WANDERERCOPY
