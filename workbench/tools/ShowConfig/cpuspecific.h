6470c7e473fa5de4efbd21a952c997690fd71f93
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-11-11 08:31:13 +0000

    Added CPU-specific code which displays processor features (currently for x86 only)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42381 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/tools/ShowConfig/cpuspecific.h
@@ -0,0 +1,15 @@
+#ifdef __x86_64__
+#define __x86__
+#define HAS_CPU_SPECIFIC
+#endif
+
+#ifdef __i386__
+#define __x86__
+#define HAS_CPU_SPECIFIC
+#endif
+
+#ifdef HAS_CPU_SPECIFIC
+void PrintCPUSpecificInfo(ULONG i, APTR ProcessorBase);
+#else
+#define PrintCPUSpecificInfo(n, base)
+#endif
