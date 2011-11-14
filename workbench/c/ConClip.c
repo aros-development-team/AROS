ecbf8edb7dc7e7c01453faa224d2db735c57c818
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-11-14 07:55:21 +0000

    Cleaned up setjmp/longjmp usage, unneeded overhead
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42440 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/ConClip.c
+++ workbench/c/ConClip.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2011, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: Conclip CLI command
@@ -72,7 +72,6 @@
 #include <proto/utility.h>
 
 #include <string.h>
-#include <setjmp.h>
 
 #undef DEBUG
 #define DEBUG 0
@@ -80,21 +79,21 @@
 
 /*****************************************************************************************/
 
-#define ARG_TEMPLATE 	"CLIPUNIT=UNIT/N,OFF/S"
-#define ARG_CLIPUNIT 	0
-#define ARG_OFF		1
-#define NUM_ARGS	2
+#define ARG_TEMPLATE    "CLIPUNIT=UNIT/N,OFF/S"
+#define ARG_CLIPUNIT    0
+#define ARG_OFF         1
+#define NUM_ARGS        2
 
 /*****************************************************************************************/
 
-#define CODE_COPY	'C'
-#define CODE_PASTE	'V'
+#define CODE_COPY       'C'
+#define CODE_PASTE      'V'
 
 struct MyEditHookMsg
 {
-    struct Message 	msg;
-    struct SGWork	*sgw;
-    WORD 		code;
+    struct Message      msg;
+    struct SGWork       *sgw;
+    WORD                code;
 };
 
 /*****************************************************************************************/
@@ -104,13 +103,12 @@ const STRPTR CONCLIP_PORTNAME = "ConClip.rendezvous";
 
 /*****************************************************************************************/
 
-static struct MsgPort 	*progport;
-static struct Hook	edithook, *oldedithook;
-static struct Task	*progtask;
-static ULONG 		clipunit, portmask;
-static BOOL 		off;
-static UBYTE 		s[256];
-static jmp_buf 		exit_buf;
+static struct MsgPort   *progport;
+static struct Hook      edithook, *oldedithook;
+static struct Task      *progtask;
+static ULONG            clipunit, portmask;
+static BOOL             off;
+static UBYTE            s[256];
 
 /*****************************************************************************************/
 
@@ -120,8 +118,6 @@ static void cleanup(STRPTR msg)
 
     if (oldedithook) SetEditHook(oldedithook);
     if (progport) DeletePort(progport);
-
-    longjmp(exit_buf, 1);
 }
 
 /*****************************************************************************************/
@@ -134,127 +130,135 @@ static void init(void)
 
 /*****************************************************************************************/
 
-static void getarguments(void)
+static BOOL getarguments(void)
 {
-    struct RDArgs 	*myargs;
-    IPTR 		args[NUM_ARGS] = {0, 0};
+    struct RDArgs       *myargs;
+    IPTR                args[NUM_ARGS] = {0, 0};
     
     if (!(myargs = ReadArgs(ARG_TEMPLATE, args, NULL)))
     {
         Fault(IoErr(), 0, s, 255);
-	cleanup(s);
+        cleanup(s);
+        return FALSE;
     }
     
     if (args[ARG_CLIPUNIT]) clipunit = (ULONG)(*(IPTR *)args[ARG_CLIPUNIT]);
     if (args[ARG_OFF]) off = args[ARG_OFF] ? TRUE : FALSE;
     
     FreeArgs(myargs);
+    return TRUE;
 }
 
 /*****************************************************************************************/
 
-static void checkport(void)
+static BOOL checkport(void)
 {
     Forbid();
     progport = FindPort(CONCLIP_PORTNAME);
     if (progport)
     {
         if (off) Signal(progport->mp_SigTask, SIGBREAKF_CTRL_C);
-	Permit();
-	progport = NULL;
-	cleanup(NULL);
+        Permit();
+        progport = NULL;
+        cleanup(NULL);
+        return FALSE;
     }
     progport = CreatePort(CONCLIP_PORTNAME, 1);
     Permit();
 
-    if (!progport) cleanup("Could not create MsgPort!");
+    if (!progport)
+    {
+        cleanup("Could not create MsgPort!");
+        return FALSE;
+    }
     
     portmask = 1L << progport->mp_SigBit;
+    return TRUE;
 }
 
 /*****************************************************************************************/
 
 AROS_UFH3(ULONG, conclipeditfunc,
-    AROS_UFHA(struct Hook *,		hook,		A0),
-    AROS_UFHA(struct SGWork *,		sgw,		A2),
-    AROS_UFHA(ULONG *, 			command,	A1)
+    AROS_UFHA(struct Hook *,            hook,           A0),
+    AROS_UFHA(struct SGWork *,          sgw,            A2),
+    AROS_UFHA(ULONG *,                  command,        A1)
 )
 {
     AROS_USERFUNC_INIT
 
-    struct MsgPort 	 *port, replyport;
+    struct MsgPort       *port, replyport;
     struct MyEditHookMsg msg;
-    BOOL		 calloldhook = TRUE;
-    ULONG 		 retcode = 0;
+    BOOL                 calloldhook = TRUE;
+    ULONG                retcode = 0;
         
     switch (*command)
     {
-	case SGH_KEY:
-	    D(bug("ConClip/conclipeditfunc: is SGH_KEY\n"));
-	    
-	    if (sgw->IEvent->ie_Qualifier & IEQUALIFIER_RCOMMAND)
-	    {
-	        D(bug("ConClip/conclipeditfunc: qualifier RCOMMAND okay\n"));
+        case SGH_KEY:
+            D(bug("ConClip/conclipeditfunc: is SGH_KEY\n"));
+            
+            if (sgw->IEvent->ie_Qualifier & IEQUALIFIER_RCOMMAND)
+            {
+                D(bug("ConClip/conclipeditfunc: qualifier RCOMMAND okay\n"));
 
-	        switch(ToUpper(sgw->Code))
-		{
-		    case 'C':
-		    	if (!sgw->NumChars) break;
-			/* fall through */
+                switch(ToUpper(sgw->Code))
+                {
+                    case 'C':
+                        if (!sgw->NumChars) break;
+                        /* fall through */
 
-		    case 'V':
-	    		D(bug("ConClip/conclipeditfunc: key = %c\n", toupper(sgw->Code)));
+                    case 'V':
+                        D(bug("ConClip/conclipeditfunc: key = %c\n", toupper(sgw->Code)));
 
-		        if ((port = FindPort(CONCLIP_PORTNAME)))
-			{
-			    calloldhook = FALSE;
+                        if ((port = FindPort(CONCLIP_PORTNAME)))
+                        {
+                            calloldhook = FALSE;
 
-			    replyport.mp_Node.ln_Type	= NT_MSGPORT;
-			    replyport.mp_Node.ln_Name 	= NULL;
-			    replyport.mp_Node.ln_Pri 	= 0;
-			    replyport.mp_Flags 		= PA_SIGNAL;
-			    replyport.mp_SigBit 	= SIGB_SINGLE;
-			    replyport.mp_SigTask 	= FindTask(NULL);
-			    NewList(&replyport.mp_MsgList);
-			    
-			    msg.msg.mn_Node.ln_Type 	= NT_MESSAGE;
-			    msg.msg.mn_ReplyPort 	= &replyport;
-			    msg.msg.mn_Length 		= sizeof(msg);
-			    
-			    msg.code = ToUpper(sgw->Code);
-			    msg.sgw  = sgw;			    
-			    
-			    if ((msg.code == CODE_COPY) || (sgw->NumChars < sgw->StringInfo->MaxChars - 1))
-			    {
-				SetSignal(0, SIGF_SINGLE);
-				PutMsg(port, &msg.msg);
-				WaitPort(&replyport);
-			    }
-			    
-			    if (msg.code == CODE_PASTE)
-			    {
-			        WORD len = strlen(sgw->WorkBuffer);
-				
-				if (len != sgw->NumChars)
-				{
-				    sgw->NumChars = len;
-				    sgw->EditOp   = EO_BIGCHANGE;
-				    sgw->Actions  = SGA_USE | SGA_REDISPLAY;
-				    
-				    retcode = 1;
-				}
-				
-			    } /* if (msg.code == CODE_COPY) */
-			    
-			} /* if ((port = FindPort(CONCLIP_PORTNAME))) */
-		
-			break;
-		    
-		} /* switch(ToUpper(sgw->Code)) */
-		
-	    } /* if (sgw->IEvent->ie_Qualifier & IEQUALIFIER_RCOMMAND) */
-    	    break;    
-	    
+                            replyport.mp_Node.ln_Type   = NT_MSGPORT;
+                            replyport.mp_Node.ln_Name   = NULL;
+                            replyport.mp_Node.ln_Pri    = 0;
+                            replyport.mp_Flags          = PA_SIGNAL;
+                            replyport.mp_SigBit         = SIGB_SINGLE;
+                            replyport.mp_SigTask        = FindTask(NULL);
+                            NewList(&replyport.mp_MsgList);
+                            
+                            msg.msg.mn_Node.ln_Type     = NT_MESSAGE;
+                            msg.msg.mn_ReplyPort        = &replyport;
+                            msg.msg.mn_Length           = sizeof(msg);
+                            
+                            msg.code = ToUpper(sgw->Code);
+                            msg.sgw  = sgw;                         
+                            
+                            if ((msg.code == CODE_COPY) || (sgw->NumChars < sgw->StringInfo->MaxChars - 1))
+                            {
+                                SetSignal(0, SIGF_SINGLE);
+                                PutMsg(port, &msg.msg);
+                                WaitPort(&replyport);
+                            }
+                            
+                            if (msg.code == CODE_PASTE)
+                            {
+                                WORD len = strlen(sgw->WorkBuffer);
+                                
+                                if (len != sgw->NumChars)
+                                {
+                                    sgw->NumChars = len;
+                                    sgw->EditOp   = EO_BIGCHANGE;
+                                    sgw->Actions  = SGA_USE | SGA_REDISPLAY;
+                                    
+                                    retcode = 1;
+                                }
+                                
+                            } /* if (msg.code == CODE_COPY) */
+                            
+                        } /* if ((port = FindPort(CONCLIP_PORTNAME))) */
+                
+                        break;
+                    
+                } /* switch(ToUpper(sgw->Code)) */
+                
+            } /* if (sgw->IEvent->ie_Qualifier & IEQUALIFIER_RCOMMAND) */
+            break;    
+            
     } /* switch (*command) */
 
     if (calloldhook) retcode = CallHookPkt(oldedithook, sgw, command);
@@ -285,31 +289,31 @@ static void savetoclipboard(struct SGWork *sgw)
     
     if((iff = AllocIFF()))
     {
-	if((iff->iff_Stream = (IPTR)OpenClipboard(clipunit)))
-	{
-	    InitIFFasClip(iff);
-	    if(!OpenIFF(iff,IFFF_WRITE))
-	    {
-		if(!PushChunk(iff, ID_FTXT, ID_FORM, IFFSIZE_UNKNOWN))
-		{
-		    if(!PushChunk(iff, ID_FTXT, ID_CHRS, IFFSIZE_UNKNOWN))
-		    {
-		        WriteChunkBytes(iff, sgw->WorkBuffer, sgw->NumChars);
-			
-			PopChunk(iff);
-			
-		    } /* if(!PushChunk(iff, ID_FTXT, ID_CHRS, IFFSIZE_UNKNOWN)) */
-		    PopChunk(iff);
-		    
-		} /* if(!PushChunk(iff, ID_FTXT, ID_FORM, IFFSIZE_UNKNOWN)) */
-		CloseIFF(iff);
-		
-	    } /* if(!OpenIFF(iff,IFFF_WRITE)) */
-	    CloseClipboard((struct ClipboardHandle*)iff->iff_Stream);
-	    
-	} /* if((iff->iff_Stream = (IPTR)OpenClipboard(clipunit))) */
-	FreeIFF(iff);
-	
+        if((iff->iff_Stream = (IPTR)OpenClipboard(clipunit)))
+        {
+            InitIFFasClip(iff);
+            if(!OpenIFF(iff,IFFF_WRITE))
+            {
+                if(!PushChunk(iff, ID_FTXT, ID_FORM, IFFSIZE_UNKNOWN))
+                {
+                    if(!PushChunk(iff, ID_FTXT, ID_CHRS, IFFSIZE_UNKNOWN))
+                    {
+                        WriteChunkBytes(iff, sgw->WorkBuffer, sgw->NumChars);
+                        
+                        PopChunk(iff);
+                        
+                    } /* if(!PushChunk(iff, ID_FTXT, ID_CHRS, IFFSIZE_UNKNOWN)) */
+                    PopChunk(iff);
+                    
+                } /* if(!PushChunk(iff, ID_FTXT, ID_FORM, IFFSIZE_UNKNOWN)) */
+                CloseIFF(iff);
+                
+            } /* if(!OpenIFF(iff,IFFF_WRITE)) */
+            CloseClipboard((struct ClipboardHandle*)iff->iff_Stream);
+            
+        } /* if((iff->iff_Stream = (IPTR)OpenClipboard(clipunit))) */
+        FreeIFF(iff);
+        
     } /* if((iff = AllocIFF()))) */
 }
 
@@ -322,61 +326,61 @@ static void readfromclipboard(struct SGWork *sgw)
     
     if((iff = AllocIFF()))
     {
-	D(bug("ConClip/conclipeditfunc: AllocIFF okay\n"));
-	
-	if((iff->iff_Stream = (IPTR)OpenClipboard(clipunit)))
-	{
-	    D(bug("ConClip/conclipeditfunc: OpenClipboard okay\n"));
-	    
-	    InitIFFasClip(iff);
-	    if(!OpenIFF(iff, IFFF_READ))
-	    {
-		D(bug("ConClip/conclipeditfunc: OpenIff okay\n"));
-		
-		if (!(StopChunk(iff, ID_FTXT, ID_CHRS)))
-		{
-		    D(bug("ConClip/conclipeditfunc: StopChunk okay\n"));
-		    
-		    if (!ParseIFF(iff, IFFPARSE_SCAN))
-		    {
-			D(bug("ConClip/conclipeditfunc: ParseIFF okay\n"));
-			
-		        cn = CurrentChunk(iff);
-               		if ((cn->cn_Type == ID_FTXT) && (cn->cn_ID == ID_CHRS) && (cn->cn_Size > 0))
-			{
-			    WORD readsize;
-			    
-    			    D(bug("ConClip: readfromclipboard: Found FTXT CHRS Chunk\n"));
-			    D(bug("ConClip: readfromclipboard: Old text = \"%s\"\n", sgw->WorkBuffer));
+        D(bug("ConClip/conclipeditfunc: AllocIFF okay\n"));
+        
+        if((iff->iff_Stream = (IPTR)OpenClipboard(clipunit)))
+        {
+            D(bug("ConClip/conclipeditfunc: OpenClipboard okay\n"));
+            
+            InitIFFasClip(iff);
+            if(!OpenIFF(iff, IFFF_READ))
+            {
+                D(bug("ConClip/conclipeditfunc: OpenIff okay\n"));
+                
+                if (!(StopChunk(iff, ID_FTXT, ID_CHRS)))
+                {
+                    D(bug("ConClip/conclipeditfunc: StopChunk okay\n"));
+                    
+                    if (!ParseIFF(iff, IFFPARSE_SCAN))
+                    {
+                        D(bug("ConClip/conclipeditfunc: ParseIFF okay\n"));
+                        
+                        cn = CurrentChunk(iff);
+                        if ((cn->cn_Type == ID_FTXT) && (cn->cn_ID == ID_CHRS) && (cn->cn_Size > 0))
+                        {
+                            WORD readsize;
+                            
+                            D(bug("ConClip: readfromclipboard: Found FTXT CHRS Chunk\n"));
+                            D(bug("ConClip: readfromclipboard: Old text = \"%s\"\n", sgw->WorkBuffer));
 
-			    readsize = sgw->StringInfo->MaxChars - 1 - sgw->BufferPos;
-			    if (cn->cn_Size < readsize) readsize = cn->cn_Size;
-			    if (readsize > 0)
-			    {
-			        memmove(sgw->WorkBuffer + sgw->BufferPos + readsize,
-					sgw->WorkBuffer + sgw->BufferPos,
-					sgw->StringInfo->MaxChars - sgw->BufferPos - readsize);
-			        ReadChunkBytes(iff, sgw->WorkBuffer + sgw->BufferPos, readsize);
-    				
-				D(bug("ConClip: readfromclipboard: New text = \"%s\"\n", sgw->WorkBuffer));
-				
-				sgw->BufferPos += readsize;
-			    }
-			    
-			} /* if ((cn->cn_Type == ID_FTXT) && (cn->cn_ID == ID_CHRS) && (cn->cn_Size > 0)) */
-			
-		    } /* if (!ParseIFF(iff, IFFPARSE_SCAN)) */
-		    
-		} /* if (!(StopChunk(iff, ID_FTXT, ID_CHRS))) */
-		
-		CloseIFF(iff);
-		
-	    } /* if(!OpenIFF(iff, IFFF_READ)) */
-	    CloseClipboard((struct ClipboardHandle*)iff->iff_Stream);
-	    
-	} /* if((iff->iff_Stream = (IPTR)OpenClipboard(clipunit))) */
-	FreeIFF(iff);
-	
+                            readsize = sgw->StringInfo->MaxChars - 1 - sgw->BufferPos;
+                            if (cn->cn_Size < readsize) readsize = cn->cn_Size;
+                            if (readsize > 0)
+                            {
+                                memmove(sgw->WorkBuffer + sgw->BufferPos + readsize,
+                                        sgw->WorkBuffer + sgw->BufferPos,
+                                        sgw->StringInfo->MaxChars - sgw->BufferPos - readsize);
+                                ReadChunkBytes(iff, sgw->WorkBuffer + sgw->BufferPos, readsize);
+                                
+                                D(bug("ConClip: readfromclipboard: New text = \"%s\"\n", sgw->WorkBuffer));
+                                
+                                sgw->BufferPos += readsize;
+                            }
+                            
+                        } /* if ((cn->cn_Type == ID_FTXT) && (cn->cn_ID == ID_CHRS) && (cn->cn_Size > 0)) */
+                        
+                    } /* if (!ParseIFF(iff, IFFPARSE_SCAN)) */
+                    
+                } /* if (!(StopChunk(iff, ID_FTXT, ID_CHRS))) */
+                
+                CloseIFF(iff);
+                
+            } /* if(!OpenIFF(iff, IFFF_READ)) */
+            CloseClipboard((struct ClipboardHandle*)iff->iff_Stream);
+            
+        } /* if((iff->iff_Stream = (IPTR)OpenClipboard(clipunit))) */
+        FreeIFF(iff);
+        
     } /* if((iff = AllocIFF()))) */
 }
 
@@ -390,35 +394,35 @@ static void handleall(void)
     while(!quitme)
     {
         sigs = Wait(SIGBREAKF_CTRL_C | portmask);
-	
-	if (sigs & portmask)
-	{
-	    struct MyEditHookMsg *msg;
-	    
-	    while((msg = (struct MyEditHookMsg *)GetMsg(progport)))
-	    {
-	        switch(msg->code)
-		{
-		    case CODE_COPY:
-		        D(bug("ConClip: Received CODE_COPY message\n"));
-			savetoclipboard(msg->sgw);
-			break;
-			
-		    case CODE_PASTE:	
-		    	D(bug("ConClip: Received CODE_PASTE message\n"));
-			readfromclipboard(msg->sgw);
-			break;
-			
-		} /* switch(msg->code) */
-		
-	        ReplyMsg(&msg->msg);
-		
-	    } /* while((msg = (struct MyEditHookMsg *)GetMsg(progport))) */
-	    
-	} /* if (sigs & portmask) */
+        
+        if (sigs & portmask)
+        {
+            struct MyEditHookMsg *msg;
+            
+            while((msg = (struct MyEditHookMsg *)GetMsg(progport)))
+            {
+                switch(msg->code)
+                {
+                    case CODE_COPY:
+                        D(bug("ConClip: Received CODE_COPY message\n"));
+                        savetoclipboard(msg->sgw);
+                        break;
+                        
+                    case CODE_PASTE:    
+                        D(bug("ConClip: Received CODE_PASTE message\n"));
+                        readfromclipboard(msg->sgw);
+                        break;
+                        
+                } /* switch(msg->code) */
+                
+                ReplyMsg(&msg->msg);
+                
+            } /* while((msg = (struct MyEditHookMsg *)GetMsg(progport))) */
+            
+        } /* if (sigs & portmask) */
 
-	if (sigs & SIGBREAKF_CTRL_C) quitme = TRUE;
-	
+        if (sigs & SIGBREAKF_CTRL_C) quitme = TRUE;
+        
     } /* while(!quitme) */
 }
 
@@ -426,14 +430,16 @@ static void handleall(void)
 
 int main(void)
 {
-    if (setjmp(exit_buf))
+    init();
+
+    if (!getarguments())
+        return 0;
+    if (!checkport())
         return 0;
 
-    init();
-    getarguments();
-    checkport();
     installedithook();
     handleall();
+
     cleanup(NULL);
     return 0;
 }
--- workbench/c/SetDefaultFont.c
+++ workbench/c/SetDefaultFont.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2007, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2011, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc:
@@ -23,18 +23,18 @@
     FUNCTION
 
         Set the default system/screen Font
-	
+        
     INPUTS
 
         FONTNAME  --  the name of the font
-	FONTSIZE  --  the size of the font
-    	SCREEN    --  if specified set the default screen font otherwise
-	              set the default system font.
+        FONTSIZE  --  the size of the font
+        SCREEN    --  if specified set the default screen font otherwise
+                      set the default system font.
 
     RESULT
 
     NOTES
-    	The default system font must be mono spaced (non-proportional)
+        The default system font must be mono spaced (non-proportional)
 
     EXAMPLE
 
@@ -62,7 +62,6 @@
 #include <proto/diskfont.h>
 
 #include <string.h>
-#include <setjmp.h>
 
 const TEXT version[] = "$VER: SetDefaultFont 41.1 (1.2.2001)\n";
 
@@ -76,48 +75,47 @@ enum
     NOOFARGS
 };
 
-static struct RDArgs 	*myargs;
-static IPTR 	    	args[NOOFARGS];
-static char 	    	s[256];
-static char 	    	*fontname;
-static LONG 	    	fontsize;
-static BOOL 	    	screenfont;
-static jmp_buf 	    	exit_buf;
+static struct RDArgs    *myargs;
+static IPTR             args[NOOFARGS];
+static char             s[256];
+static char             *fontname;
+static LONG             fontsize;
+static BOOL             screenfont;
 
-static void Cleanup(char *msg, WORD rc)
+static void Cleanup(char *msg)
 {
     if (msg)
     {
-    	Printf("SetDefaultFont: %s\n",msg);
+        Printf("SetDefaultFont: %s\n",msg);
     }
     
     if (myargs)
     {
-	FreeArgs(myargs);
+        FreeArgs(myargs);
     }
-
-    if (rc != RETURN_OK)
-        longjmp(exit_buf, rc);
 }
 
 int GfxBase_version = 0;
 int IntuitionBase_version = 0;
 int DiskFontBase_version = 0;
 
-static void GetArguments(void)
+static ULONG GetArguments(void)
 {
     if (!(myargs = ReadArgs(ARG_TEMPLATE, args, 0)))
     {
-    	Fault(IoErr(), 0, s, 255);
-	Cleanup(s, RETURN_FAIL);
+        Fault(IoErr(), 0, s, 255);
+        Cleanup(s);
+        return RETURN_FAIL;
     }
     
     fontname   = (char *)args[ARG_FONTNAME];
     fontsize   = *(IPTR *)args[ARG_FONTSIZE];
     screenfont = args[ARG_SCREEN] ? TRUE : FALSE;
+
+    return 0;
 }
 
-static void Action(void)
+static ULONG Action(void)
 {
     struct TextAttr ta;
     struct TextFont *font;
@@ -133,38 +131,43 @@ static void Action(void)
     font = OpenDiskFont(&ta);
     if (!font)
     {
-    	Cleanup("Can't open font!", RETURN_FAIL);
+        Cleanup("Can't open font!");
+        return RETURN_FAIL;
     }
     
     if (screenfont)
     {
-    	SetDefaultScreenFont(font);
+        SetDefaultScreenFont(font);
     }
     else
     {
-   	if (font->tf_Flags & FPF_PROPORTIONAL)
-	{
-	    CloseFont(font);
-	    Cleanup("The font must be mono spaced (non-proportional)!",
-		    RETURN_ERROR);
-	}
-	
-	Forbid();
-    	GfxBase->DefaultFont = font;
-	Permit();
+        if (font->tf_Flags & FPF_PROPORTIONAL)
+        {
+            CloseFont(font);
+            Cleanup("The font must be mono spaced (non-proportional)!");
+            return RETURN_ERROR;
+        }
+
+        Forbid();
+        GfxBase->DefaultFont = font;
+        Permit();
     }
+
+    return 0;
 }
 
 int main(void)
 {
     int rc;
 
-    if ((rc = setjmp(exit_buf)) != 0)
+    rc = GetArguments();
+    if (rc)
         return rc;
 
-    GetArguments();    
-    Action();
-    Cleanup(0, RETURN_OK);
-    
+    rc = Action();
+    if (rc)
+        return rc;
+
+    Cleanup(0);    
     return 0;
 }
