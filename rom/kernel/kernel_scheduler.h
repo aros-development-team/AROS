288d99e835993e0693a75d87540285fab232d9e8
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-11-16 08:43:01 +0000

    - Removed kernel/scheduler.h, it was a bad design. Flag definitions brought back to execbase.h. - Some adjustments to ExecBase, reserved ULONGs converted to IPTRs. !!! 64 bit bincompat affected !!! - Added some comments to execbase.h. AFF_FPU flag declared public on ARM architecture. Needed for some floating-point-aware code.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42482 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/kernel/kernel_scheduler.h
+++ rom/kernel/kernel_scheduler.h
@@ -1,5 +1,3 @@
-#include <kernel/scheduler.h>
-
 BOOL core_Schedule(void);			/* Reschedule the current task if needed */
 void core_Switch(void);				/* Switch away from the current task     */
 struct Task *core_Dispatch(void);		/* Select the new task for execution     */
