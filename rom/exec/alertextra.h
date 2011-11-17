0e3ed6e7607f259f35edaced6582383470beb03c
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-11-17 11:56:35 +0000

    Merged two small .h files into larger one.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42512 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/alertextra.h
+++ rom/exec/alertextra.h
@@ -16,4 +16,18 @@ struct MMContext
 #define MM_FREE    1
 #define MM_OVERLAP 2
 
+struct MungwallHeader;
+
+struct MungwallContext
+{
+    struct MungwallHeader *hdr;
+    const char 		  *freeFunc;
+    IPTR		   freeSize;
+    APTR		   pre_start;
+    APTR		   pre_end;
+    APTR		   post_start;
+    APTR		   post_end;
+};
+
 char *FormatMMContext(char *buffer, struct MMContext *ctx, struct ExecBase *SysBase);
+char *FormatMWContext(char *buffer, struct MungwallContext *ctx, struct ExecBase *SysBase);
