d95aa1a1606c567b9cfa761551504b912a5e11da
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-11-17 05:26:25 +0000

    Detab
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42504 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-native/bootconsole/common.c
+++ arch/all-native/bootconsole/common.c
@@ -28,11 +28,11 @@ void con_InitVESA(unsigned short version, struct vbe_mode *mode)
 
         scr_Type = SCR_GFX;
 
-	/* Use 3.0-specific field if available */
-	if ((mode->mode_attributes & VM_LINEAR_FB) && (version >= 0x0300))
-	    pitch = mode->linear_bytes_per_scanline;
-	else
-	    pitch = mode->bytes_per_scanline;
+        /* Use 3.0-specific field if available */
+        if ((mode->mode_attributes & VM_LINEAR_FB) && (version >= 0x0300))
+            pitch = mode->linear_bytes_per_scanline;
+        else
+            pitch = mode->bytes_per_scanline;
 
         fb_Init(mode->x_resolution, mode->y_resolution, mode->bits_per_pixel, pitch);
     }
@@ -40,21 +40,19 @@ void con_InitVESA(unsigned short version, struct vbe_mode *mode)
     {
         scr_Type   = SCR_TEXT;
         scr_Width  = mode->x_resolution;
-    	scr_Height = mode->y_resolution;
+        scr_Height = mode->y_resolution;
 
-	/* CHECKME: is it correct? It should fall back to VGA buffer address for text modes */
-    	if (!scr_FrameBuffer)
-    	    scr_FrameBuffer = (void *)((unsigned long)mode->win_b_segment << 16);
+        /* CHECKME: is it correct? It should fall back to VGA buffer address for text modes */
+        if (!scr_FrameBuffer)
+            scr_FrameBuffer = (void *)((unsigned long)mode->win_b_segment << 16);
+        /*
+         * QEmu in text mode (VBE number 0x03) is known to supply neither phys_base nor
+         * window segments (all three are NULLs). This is a workaround for this.
+         */     
+        if (!scr_FrameBuffer)
+            scr_FrameBuffer = VGA_TEXT_ADDR;
 
-
-	/*
-	 * QEmu in text mode (VBE number 0x03) is known to supply neither phys_base nor
-	 * window segments (all three are NULLs). This is a workaround for this.
-	 */	
-	if (!scr_FrameBuffer)
-	    scr_FrameBuffer = VGA_TEXT_ADDR;
-
-	txt_Clear();
+        txt_Clear();
     }
 
     /* We must have valid framebuffer address here */
@@ -78,12 +76,12 @@ void con_InitSerial(char *cmdline)
     
     if (opts)
     {
-    	use_serial = 1;
-    	
-    	serial_Init(&opts[12]);
+        use_serial = 1;
+        
+        serial_Init(&opts[12]);
     }
     else
-    	use_serial = 0;
+        use_serial = 0;
 }
 
 void con_Putc(char c)
@@ -91,21 +89,21 @@ void con_Putc(char c)
     /* 0x03 character shuts off boot-time screen console */
     if (c == 0x03)
     {
-    	scr_Type = SCR_UNKNOWN;
-    	return;
+        scr_Type = SCR_UNKNOWN;
+        return;
     }
 
     if (use_serial)
-	serial_Putc(c);
+        serial_Putc(c);
 
     switch (scr_Type)
     {
     case SCR_TEXT:
-    	txt_Putc(c);
-    	break;
+        txt_Putc(c);
+        break;
 
     case SCR_GFX:
-    	fb_Putc(c);
-    	break;
+        fb_Putc(c);
+        break;
     }
 }
