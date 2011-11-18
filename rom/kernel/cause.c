3425f8627efc9d932847dc0b2076d8bfce68bc1f
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-11-18 07:16:38 +0000

    Added autodocs
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42529 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/kernel/cause.c
+++ rom/kernel/cause.c
@@ -16,12 +16,22 @@ AROS_LH0I(void, KrnCause,
 	struct KernelBase *, KernelBase, 3, Kernel)
 
 /*  FUNCTION
+        Run software interrupt processing sequence
 
     INPUTS
+        None
 
     RESULT
+        None
 
     NOTES
+        This entry point directly calls interrupt processing routine
+        in supervisor mode. It neither performs any checks of caller status
+        nor obeys interrupt enable state.
+
+        This function is safe to call only from within user mode.
+        This function is considered internal, and not meant to be called
+        by user's software.
 
     EXAMPLE
 
--- rom/kernel/dispatch.c
+++ rom/kernel/dispatch.c
@@ -16,12 +16,23 @@ AROS_LH0(void, KrnDispatch,
          struct KernelBase *, KernelBase, 4, Kernel)
 
 /*  FUNCTION
+        Run the next available task
 
     INPUTS
+        None
 
     RESULT
+        None
 
     NOTES
+        This entry point directly calls task dispatch routine in supervisor mode.
+        It neither performs any checks of caller status nor obeys interrupt enable
+        state. After calling this function, caller's task will be replaced by
+        another one, and caller's state will be lost.
+
+        This function is safe to call only from within user mode.
+        This function is considered internal, and not meant to be called
+        by user's software.
 
     EXAMPLE
 
