252773b7b330ef02425eed1b011bd09f32bcd7db
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-11-18 07:16:38 +0000

    Added autodocs
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42529 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/kernel/schedule.c
+++ rom/kernel/schedule.c
@@ -16,12 +16,22 @@ AROS_LH0(void, KrnSchedule,
          struct KernelBase *, KernelBase, 6, Kernel)
 
 /*  FUNCTION
+        Run task scheduling sequence
 
     INPUTS
+        None
 
     RESULT
+        None
 
     NOTES
+        This entry point directly calls task scheduling routine
+        in supervisor mode. It neither performs any checks of caller status
+        nor obeys interrupt enable state.
+
+        This function is safe to call only from within user mode.
+        This function is considered internal, and not meant to be called
+        by user's software.
 
     EXAMPLE
 
--- rom/kernel/switch.c
+++ rom/kernel/switch.c
@@ -16,12 +16,24 @@ AROS_LH0(void, KrnSwitch,
          struct KernelBase *, KernelBase, 5, Kernel)
 
 /*  FUNCTION
+        Save context of caller's task and dispatch the next available task
 
     INPUTS
+        None
 
     RESULT
+        None
 
     NOTES
+        This entry point directly calls task switch routine
+        in supervisor mode. It neither performs any checks of caller status
+        nor obeys interrupt enable state. After calling this function, caller's
+        task will be replaced by another one, and it's caller's responsibility
+        to do anything to prevent task loss.
+
+        This function is safe to call only from within user mode.
+        This function is considered internal, and not meant to be called
+        by user's software.
 
     EXAMPLE
 
