864b8068208c97e6bd88bf79ef93c4be82b20c8f
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-11-22 08:25:28 +0000

    Added support for no-VFP targets
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42566 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-all/clib/vfork_longjmp.s
+++ arch/arm-all/clib/vfork_longjmp.s
@@ -19,7 +19,18 @@ AROS_CDEFNAME(vfork_longjmp):
 	ldr	lr, [ip], #4					/* restore return address */
 	mov	r0, r1						/* return value from longjmp into r0 and generate condition code */
 	ldmia	ip!, {r4, r5, r6, r7, r8, r9, sl, fp, sp}	/* restore non-scratch regs */
+#ifdef __SOFTFP__
+	ldr	r1, 1f
+	ldr	r1, [r1]
+	mov	r2, AttnFlags
+	ldrh	r1, [r1, r2]
+	tst	r1, AFF_FPU
+	bxeq	lr
+#endif
 	fldmiax ip!, {d8-d15}					/* Restore VFP registers - we assume they are available! */
 	ldr     r1, [ip], #4					/* restore VFP status reg */
   	fmxr    fpscr, r1
 	bx	lr						/* Done! */
+#ifdef __SOFTFP__
+1:	.word	SysBase
+#endif
