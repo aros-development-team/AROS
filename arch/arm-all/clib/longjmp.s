5424d8707d75a11c865a0e3ccf1e9ebee0d2f56c
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-11-22 08:25:28 +0000

    Added support for no-VFP targets
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42566 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-all/clib/longjmp.s
+++ arch/arm-all/clib/longjmp.s
@@ -19,7 +19,18 @@ AROS_CDEFNAME(longjmp):
 	movs	r0, r1						/* return value from longjmp into r0 and generate condition code */
 	moveq	r0, #1						/* if retval = 0, then retval = 1 */
 	ldmia	ip!, {r4, r5, r6, r7, r8, r9, sl, fp, sp}	/* restore non-scratch regs */
+#ifdef __SOFTFP__
+	ldr	r1, 1f
+	ldr	r1, [r1]
+	mov	r2, AttnFlags
+	ldrh	r1, [r1, r2]
+	tst	r1, AFF_FPU
+	bxeq	lr
+#endif
 	fldmiax ip!, {d8-d15}					/* Restore VFP registers - we assume they are available! */
 	ldr     r1, [ip], #4					/* restore VFP status reg */
   	fmxr    fpscr, r1
 	bx      lr						/* Done! */
+#ifdef __SOFTFP__
+1:	.word	SysBase
+#endif
--- arch/arm-all/clib/setjmp.s
+++ arch/arm-all/clib/setjmp.s
@@ -17,8 +17,19 @@ AROS_CDEFNAME(setjmp):
 	mov	ip, r0						/* Get the env address */
 	str	lr, [ip], #4					/* store return address explicitly */
 	stmia	ip!, {r4, r5, r6, r7, r8, r9, sl, fp, sp}	/* store non-scratch regs */
+	mov	r0, #0						/* return zero */
+#ifdef __SOFTFP__
+	ldr	r1, 1f
+	ldr	r1, [r1]
+	mov	r2, AttnFlags
+	ldrh	r1, [r1, r2]
+	tst	r1, AFF_FPU
+	bxeq	lr
+#endif
 	fstmiax	ip!, {d8-d15}					/* Store VFP registers - we assume they are available! */
 	fmrx	r2, fpscr					/* VFP condition codes */
 	str	r2, [ip], #4
-	mov	r0, #0						/* return zero */
 	bx	lr
+#ifdef __SOFTFP__
+1:	.word	SysBase
+#endif
