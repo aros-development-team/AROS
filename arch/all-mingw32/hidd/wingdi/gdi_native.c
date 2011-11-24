5a01257480ed3e0cd461ce9cfe1da59a87ec00e4
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-11-24 08:50:33 +0000

    Added simple spinlocking to prevent loss/duplication of input events. Seems to have also fixed random freezes, caused (supposedly) by Intuition's mouse handling screwed up by wrong sequence of events.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42590 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-mingw32/hidd/wingdi/gdi_native.c
+++ arch/all-mingw32/hidd/wingdi/gdi_native.c
@@ -1,5 +1,5 @@
 /*
-    Copyright  1995-2010, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2011, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: Host-side part of GDI hidd. Handles windows and receives events.
@@ -10,6 +10,7 @@
 #include <windowsx.h>
 #include <stdio.h>
 #include <aros/irq.h>
+
 #include "gdi.h"
 #include "bitmap.h"
 
@@ -24,7 +25,8 @@ LRESULT CALLBACK bitmap_callback(HWND win, UINT msg, WPARAM wp, LPARAM lp);
 
 /* Global variables shared by all windows (theoretical)	*/
 
-static WNDCLASS display_class_desc = {
+static WNDCLASS display_class_desc =
+{
     CS_NOCLOSE,
     display_callback,
     0,
@@ -36,7 +38,9 @@ static WNDCLASS display_class_desc = {
     NULL,
     "AROS_Screen"
 };
-static WNDCLASS bitmap_class_desc = {
+
+static WNDCLASS bitmap_class_desc =
+{
     CS_NOCLOSE,
     bitmap_callback,
     0,
@@ -48,6 +52,7 @@ static WNDCLASS bitmap_class_desc = {
     NULL,
     "AROS_Bitmap"
 };
+
 static DWORD thread_id;	    /* Window service thread ID			      */
 static HWND window_active;  /* Currently active AROS window		      */
 static ULONG display_class; /* Display window class		   	      */
@@ -55,15 +60,41 @@ static ULONG bitmap_class;  /* Bitmap window class			      */
 static DWORD last_key;      /* Last pressed key - used to suppress autorepeat */
 
 /* Virtual hardware registers */
-static struct GDI_Control gdictl;
+static volatile struct GDI_Control gdictl;
 
 /****************************************************************************************/
 
 static ULONG SendKbdIRQ(UINT msg, DWORD key)
 {
     DKBD(printf("[GDI] Keyboard event 0x%04lX key 0x%04lX\n", msg, key));
+
+    /*
+     * Spin if AROS haven't read the previous event yet.
+     * This should fix swallowing or duplicating events, which happens because
+     * of race condition between this thread and AROS:
+     * 1. We signal a keypress.
+     * 2. AROS gets a signal and wakes up. But has no time to read the event, Windows context switches.
+     * 3. We get one more keypress, send another event.
+     * 4. AROS wakes up again, reads event. Note that this is already another event (3).
+     * 5. AROS sees one more event, and reads it again. Result: first keypress lost, second duplicated.
+     *
+     * Yes, this is Windows-unfriendly abnormal technique. It's used because it would be very bad to use
+     * something like critical sections on AROS side, because they would block the whole AROS. But, well,
+     * we're running an OS inside another OS, and noone told that is's so easy. :) And it's quite normal
+     * to use spinlocks in OS kernels. Remember, here we simulate a hardware.
+     * Well, we attempt to be a bit more system-friendly by using SwitchToThread() here.
+     *
+     * Be careful! Doing this here means AROS MUST read events and reset locks. Otherwise we'll deadlock
+     * here upon the second event. Never dispose a keyboard or mouse HIDD objects, for example. There's
+     * no need to, but who knows...
+     *
+     * In case of futrher problems a FIFO event queue needs to be implemented here.
+     */
+    while (gdictl.KbdEvent)
+        SwitchToThread();
+
     gdictl.KbdEvent = msg;
-    gdictl.KeyCode = key;
+    gdictl.KeyCode  = key;
 
     return KrnCauseIRQ(gdictl.KbdIrq);
 }
@@ -74,17 +105,23 @@ LRESULT CALLBACK key_callback(int code, WPARAM wp, KBDLLHOOKSTRUCT *lp)
 {
     DWORD key;
 
-    if ((code == HC_ACTION) && window_active) {
+    if ((code == HC_ACTION) && window_active)
+    {
     	wp &= 0xFFFFFFFB; /* This masks out difference between WM_SYSKEY* and WM_KEY* */
     	key = (lp->scanCode & 0xFF) | ((lp->flags & LLKHF_EXTENDED) << 8);
-    	/* Here we get raw keypresses, including autorepeats. We have to sort out autorepeats
-    	   in some smart way. */
-    	switch(wp) {
+
+    	/*
+    	 * Here we get raw keypresses, including autorepeats. We have to sort out
+    	 * autorepeats in some smart way.
+    	 */
+    	switch(wp)
+    	{
 	case WM_KEYDOWN:
 	    if (key == last_key)
 	        return 1;
 	    last_key = key;
 	    break;
+
 	case WM_KEYUP:
 	    if (key == last_key)
 	        last_key = 0;
@@ -99,15 +136,18 @@ LRESULT CALLBACK display_callback(HWND win, UINT msg, WPARAM wp, LPARAM lp)
 {
     struct gfx_data *gdata;
 
-    switch(msg) {
+    switch(msg)
+    {
     case WM_SETCURSOR:
         gdata = (struct gfx_data *)GetWindowLongPtr(win, GWLP_USERDATA);
 
-	if (gdata->cursor) {
+	if (gdata->cursor)
+	{
 	    SetCursor(gdata->cursor);
 	    return 0;
 	}
 	break;
+
     case WM_MOUSEMOVE:
     case WM_LBUTTONDOWN:
     case WM_LBUTTONUP:
@@ -116,8 +156,14 @@ LRESULT CALLBACK display_callback(HWND win, UINT msg, WPARAM wp, LPARAM lp)
     case WM_MBUTTONDOWN:
     case WM_MBUTTONUP:
     case WM_MOUSEWHEEL:
-	if (win == window_active) {
+	if (win == window_active)
+	{
 	    DMSE(printf("[GDI] Mouse event 0x%04X, window 0x%p\n", msg, win));
+
+            /* Spinlock to prevent event loss. See SendKbdIRQ() above for explanation. */
+	    while (gdictl.MouseEvent)
+                SwitchToThread();
+
             gdictl.MouseEvent = msg;
             gdictl.MouseX = GET_X_LPARAM(lp);
     	    gdictl.MouseY = GET_Y_LPARAM(lp);
@@ -126,32 +172,45 @@ LRESULT CALLBACK display_callback(HWND win, UINT msg, WPARAM wp, LPARAM lp)
             KrnCauseIRQ(gdictl.MouseIrq);
 	}
     	return 0;
-/* This keyboard-related fragment is not used on Windows NT because keyboard hook intercepts all keyboard messages.
-   It is left here for Windows 9x. */
+
+#ifndef __x86_64__
+    /*
+     * This keyboard-related fragment is not used on Windows NT because keyboard hook
+     * intercepts all keyboard messages. It is left here for Windows 9x.
+     */
     case WM_KEYDOWN:
     case WM_SYSKEYDOWN:
         if (lp & 0x40000000) /* Ignore autorepeats */
             return 0;
+
     case WM_KEYUP:
     case WM_SYSKEYUP:
         SendKbdIRQ(msg & 0xFFFFFFFB, (lp >> 16) & 0x000001FF);
         return 0;
+#endif
+
     case WM_ACTIVATE:
         DACT(printf("[GDI] WM_ACTIVATE, Window 0x%p, wParam 0x%08lX\n", win, wp));
         /* In some cases Windows can activate an iconified window (for example if we minimize it
            by clicking its button on the taskbar). We process deactivation messages regardless of
            minimized state, but we handle activation only when it's done on a non-minimized window.
            This behavior was discovered by trial and error, i hope it's really ok now. */
-    	if ((wp & 0x0000FFFF) != WA_INACTIVE) {
-            if (!(wp & 0xFFFF0000)) {
+    	if ((wp & 0x0000FFFF) != WA_INACTIVE)
+    	{
+            if (!(wp & 0xFFFF0000))
+            {
                 window_active = win;
 		gdictl.Active = (void *)GetWindowLongPtr(win, GWLP_USERDATA);
 		KrnCauseIRQ(gdictl.GfxIrq);
 	    }
-        } else {
+        }
+        else
+        {
             window_active = NULL;
+
 	    /* Send WM_KEYUP in order to prevent "stuck keys" phenomena */
-	    if (last_key) {
+	    if (last_key)
+	    {
 		SendKbdIRQ(WM_KEYUP, last_key);
 		last_key = 0;
 	    }
@@ -168,7 +227,8 @@ LRESULT CALLBACK bitmap_callback(HWND win, UINT msg, WPARAM wp, LPARAM lp)
     LONG x, y, xsize, ysize;
     struct bitmap_data *bmdata;
 
-    switch(msg) {
+    switch(msg)
+    {
     case WM_PAINT:
         bmdata = (struct bitmap_data *)GetWindowLongPtr(win, GWLP_USERDATA);
         window_dc = BeginPaint(win, &ps);
@@ -179,6 +239,7 @@ LRESULT CALLBACK bitmap_callback(HWND win, UINT msg, WPARAM wp, LPARAM lp)
         BitBlt(window_dc, x, y, xsize, ysize, bmdata->dc, x, y, SRCCOPY);
         EndPaint(win, &ps);
         return 0;
+
     default:
         return DefWindowProc(win, msg, wp, lp);
     }
@@ -194,13 +255,22 @@ DWORD WINAPI gdithread_entry(struct GDI_Control *ctl)
     LONG width, height;
     HWND prev;
 
+    ctl->KbdEvent   = 0; /* Just in case... */
+    ctl->MouseEvent = 0;
+
     keyhook = SetWindowsHookEx(WH_KEYBOARD_LL, (HOOKPROC)key_callback, display_class_desc.hInstance, 0);
-    do {
+
+    do
+    {
         res = GetMessage(&msg, NULL, 0, 0);
         D(printf("[GDI] GetMessage returned %ld\n", res));
-        if (res > 0) {
+
+        if (res > 0)
+        {
             D(printf("[GDI] Message %lu for window 0x%p\n", msg.message, msg.hwnd));
-            switch (msg.message) {
+
+            switch (msg.message)
+            {
             case NOTY_SHOW:
                 gdata = (struct gfx_data *)msg.wParam;
 		DWIN(printf("[GDI] NOTY_SHOW, Display data: 0x%p\n", gdata));
@@ -211,53 +281,69 @@ DWORD WINAPI gdithread_entry(struct GDI_Control *ctl)
 		/* Traverse through the bitmaps list and (re)open every bitmap's window. This will
 		   cause rearranging them in the correct Z-order */
 		for (bmdata = (struct bitmap_data *)gdata->bitmaps.mlh_Head;
-		     bmdata->node.mln_Succ; bmdata = (struct bitmap_data *)bmdata->node.mln_Succ) {
+		     bmdata->node.mln_Succ; bmdata = (struct bitmap_data *)bmdata->node.mln_Succ)
+		{
 
 		    /* Display window dimensions based on its size will be calculated only for the
 		       first (frontmost) bitmap */
-		    if (!width) {
-			width = GetSystemMetrics(SM_CXFIXEDFRAME) * 2 + bmdata->win_width;
+		    if (!width)
+		    {
+			width  = GetSystemMetrics(SM_CXFIXEDFRAME) * 2 + bmdata->win_width;
 			height = GetSystemMetrics(SM_CYFIXEDFRAME) * 2 + bmdata->win_height + GetSystemMetrics(SM_CYCAPTION);
 
 			DWIN(printf("[GDI] Display window: 0x%p\n", gdata->fbwin));
-			if (gdata->fbwin) {
+			if (gdata->fbwin)
+			{
 			    DWIN(printf("[GDI] Resizing display...\n"));
 			    SetWindowPos(gdata->fbwin, 0, 0, 0, width, height, SWP_NOACTIVATE|SWP_NOMOVE|SWP_NOZORDER);
 			}
 		    }
 
 		    /* Open display window if we don't have it. Try every time, for sure */
-		    if (!gdata->fbwin) {
+		    if (!gdata->fbwin)
+		    {
 			DWIN(printf("[GDI] Opening display...\n"));
-			/* We create the window as invisible, and show it only after setting gdata pointer.
-			   Otherwise we miss activation event (since gdata is NULL) */
+
+			/*
+			 * We create the window as invisible, and show it only after setting gdata pointer.
+			 * Otherwise we miss activation event (since gdata is NULL)
+			 */
 			gdata->fbwin = CreateWindow((LPCSTR)display_class, "AROS Screen", WS_OVERLAPPED|WS_CAPTION|WS_SYSMENU|WS_MINIMIZEBOX,
 						     CW_USEDEFAULT, CW_USEDEFAULT, width, height, NULL, NULL,
 						     display_class_desc.hInstance, NULL);
 			DWIN(printf("[GDI] Opened display window: 0x%p\n", gdata->fbwin));
-			if (gdata->fbwin) {
+			if (gdata->fbwin)
+			{
 			    SetWindowLongPtr(gdata->fbwin, GWLP_USERDATA, (LONG_PTR)gdata);
 			    ShowWindow(gdata->fbwin, SW_SHOW);
 			}
 		    }
 
 		    /* Open bitmap window only if we really have display window */
-		    if (gdata->fbwin) {
+		    if (gdata->fbwin)
+		    {
 			DWIN(printf("[GDI] Showing bitmap data 0x%p, window 0x%p\n", bmdata, bmdata->window));
-			/* WS_DISABLED here causes forwarding all input to the parent window (i. e. display window).
-			    In future we may need some more sophisticated input handling because current driver architecture
-			    allows further transformation to rootless mode where every screen will have its own separate window
-			    on a Windows desktop. */
-			if (!bmdata->window) {
+
+			/*
+			 * WS_DISABLED here causes forwarding all input to the parent window (i. e. display window).
+			 * In future we may need some more sophisticated input handling because current driver architecture
+			 * allows further transformation to rootless mode where every screen will have its own separate window
+			 * on a Windows desktop.
+			 */
+			if (!bmdata->window)
+			{
 		            bmdata->window = CreateWindow((LPCSTR)bitmap_class, NULL, WS_BORDER|WS_CHILD|WS_CLIPSIBLINGS|WS_DISABLED, bmdata->bm_left - 1, bmdata->bm_top - 1, bmdata->bm_width + 2, bmdata->bm_height + 2,
 							   gdata->fbwin, NULL, display_class_desc.hInstance, NULL);
 			    DWIN(printf("[GDI] Opened bitmap window: 0x%p\n", bmdata->window));
 			}
 
-			if (bmdata->window) {		
+			if (bmdata->window)
+			{
 		            SetWindowLongPtr(bmdata->window, GWLP_USERDATA, (LONG_PTR)bmdata);
-		            /* We actually show the window only now, because otherwise it will pop up in front of all windows, and then
-		               immediately jump backwards, causing irritating flicker */
+		            /*
+		             * We actually show the window only now, because otherwise it will pop up in front of all windows, and then
+		             * immediately jump backwards, causing irritating flicker
+		             */
 		            SetWindowPos(bmdata->window, prev, 0, 0, 0, 0, SWP_NOMOVE|SWP_NOSIZE|SWP_SHOWWINDOW);
 		            UpdateWindow(bmdata->window);
 
@@ -267,7 +353,8 @@ DWORD WINAPI gdithread_entry(struct GDI_Control *ctl)
             	}
 
 		/* If we got no bitmaps, close the display window */
-		if ((prev == HWND_TOP) && gdata->fbwin) {
+		if ((prev == HWND_TOP) && gdata->fbwin)
+		{
 		    DestroyWindow(gdata->fbwin);
 		    gdata->fbwin = NULL;
 		}
@@ -282,7 +369,7 @@ DWORD WINAPI gdithread_entry(struct GDI_Control *ctl)
             }
 	}
     } while (res > 0);
-    
+
     /* TODO: Further cleanup (close windows etc) */
 
     if (keyhook)
@@ -299,33 +386,42 @@ struct GDI_Control *__declspec(dllexport) __aros GDI_Init(void)
     HANDLE th;
 
     irq = KrnAllocIRQ();
-    if (irq != -1) {
+    if (irq != -1)
+    {
         gdictl.GfxIrq = irq;
         irq = KrnAllocIRQ();
-	if (irq != -1) {
+	if (irq != -1)
+	{
 	    gdictl.KbdIrq = irq;
 	    irq = KrnAllocIRQ();
-	    if (irq != -1) {
+	    if (irq != -1)
+	    {
 	        gdictl.MouseIrq = irq;
 
 		display_class_desc.hInstance = GetModuleHandle(NULL);
-		display_class_desc.hIcon = LoadIcon(display_class_desc.hInstance, MAKEINTRESOURCE(101));
-		display_class_desc.hCursor = LoadCursor(NULL, IDC_ARROW);
+		display_class_desc.hIcon     = LoadIcon(display_class_desc.hInstance, MAKEINTRESOURCE(101));
+		display_class_desc.hCursor   = LoadCursor(NULL, IDC_ARROW);
 		display_class = RegisterClass(&display_class_desc);
 		D(printf("[GDI] Created display window class 0x%04x\n", display_class));
-		if (display_class) {
+
+		if (display_class)
+		{
 		    bitmap_class_desc.hInstance = display_class_desc.hInstance;
 		    bitmap_class = RegisterClass(&bitmap_class_desc);
 		    D(printf("[GDI] Created bitmap window class 0x%04x\n", bitmap_class));
-		    if (bitmap_class) {
+
+		    if (bitmap_class)
+		    {
 			gdictl.ShowDone = FALSE;
 			gdictl.Active   = NULL;
 
 		        window_active = NULL;
 		        last_key = 0;
 		        th = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)gdithread_entry, &gdictl, 0, &thread_id);
+
 		        D(printf("[GDI] Started thread 0x%p ID 0x%08lX\n", th, thread_id));
-		        if (th) {
+		        if (th)
+		        {
 			    CloseHandle(th);
 			    return &gdictl;
 		        }
--- arch/all-mingw32/hidd/wingdi/gdimouse.c
+++ arch/all-mingw32/hidd/wingdi/gdimouse.c
@@ -1,5 +1,5 @@
 /*
-    Copyright  1995-2010, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2011, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: GDI hidd handling mouse events.
@@ -8,21 +8,16 @@
 
 #define __OOP_NOATTRBASES__
 
+#include <aros/debug.h>
+#include <aros/symbolsets.h>
 #include <proto/kernel.h>
 #include <proto/utility.h>
 #include <proto/oop.h>
 #include <oop/oop.h>
-
 #include <hidd/hidd.h>
 #include <hidd/mouse.h>
 
-#include <aros/symbolsets.h>
-
-#define DEBUG 0
-#include <aros/debug.h>
-
 #include LC_LIBDEFS_FILE
-
 #include "gdi.h"
 
 /****************************************************************************************/
@@ -32,10 +27,78 @@ static OOP_AttrBase HiddMouseAB;
 static struct OOP_ABDescr attrbases[] =
 {
     { IID_Hidd_Mouse, &HiddMouseAB  },
-    { NULL  	    , NULL  	    }
+    { NULL          , NULL          }
 };
 
-static VOID MouseIntHandler(struct gdimouse_data *data, struct GDI_Control *ctl);
+/****************************************************************************************/
+
+static BOOL check_button(UWORD state, UWORD mask, UWORD arosbutton, struct pHidd_Mouse_Event *e, struct gdimouse_data *data)
+{
+    UWORD old = data->buttons & mask;
+
+    state &= mask;
+    if (old == state)
+        return FALSE;
+
+    e->button = arosbutton;
+    e->type = state ? vHidd_Mouse_Press : vHidd_Mouse_Release;
+    data->mouse_callback(data->callbackdata, e);
+
+    return TRUE;
+}
+
+static VOID MouseIntHandler(struct gdimouse_data *data, volatile struct GDI_Control *MOUSEDATA)
+{
+    unsigned short event;
+    struct pHidd_Mouse_Event e;
+    UWORD new_buttons;
+    BOOL button;
+
+    D(bug("[GDIMouse] Interrupt\n"));
+
+    /*
+     * Due to asynchronous nature of host-side window service thread GDI_Control structure acts like hardware registers.
+     * There can be many pending events before we get here and the structure will hold a summary of all states, however
+     * MouseEvent will contain only last event. Because of this we read it ASAP and pay as little attention to MouseEvent
+     * as possible.
+     */
+    switch(MOUSEDATA->MouseEvent)
+    {
+    case WM_MOUSEWHEEL:
+        /* Wheel delta comes only with WM_MOUSEWHEEL, otherwise it's zero */
+        e.y = -MOUSEDATA->WheelDelta; /* Windows gives us inverted data */
+        e.x = 0;
+
+        /*
+         * This signals that we have read all data from MouseEvent, and
+         * the structure can be reused for the next event.
+         */
+        MOUSEDATA->MouseEvent = 0;
+
+        e.type   = vHidd_Mouse_WheelMotion;
+        e.button = vHidd_Mouse_NoButton;
+        data->mouse_callback(data->callbackdata, &e);
+        break;
+ 
+    default:
+        e.x = MOUSEDATA->MouseX;
+        e.y = MOUSEDATA->MouseY;
+        new_buttons = MOUSEDATA->Buttons;
+
+        MOUSEDATA->MouseEvent = 0;
+
+        button = check_button(new_buttons, MK_LBUTTON, vHidd_Mouse_Button1, &e, data);
+        button |= check_button(new_buttons, MK_RBUTTON, vHidd_Mouse_Button2, &e, data);
+        button |= check_button(new_buttons, MK_MBUTTON, vHidd_Mouse_Button3, &e, data);
+        if (button)
+            data->buttons = new_buttons;
+        else {
+            e.button = vHidd_Mouse_NoButton;
+            e.type = vHidd_Mouse_Motion;
+            data->mouse_callback(data->callbackdata, &e);
+        }
+    }
+}
 
 /****************************************************************************************/
 
@@ -48,59 +111,58 @@ OOP_Object * GDIMouse__Root__New(OOP_Class *cl, OOP_Object *o, struct pRoot_New 
     ObtainSemaphoreShared( &XSD(cl)->sema);
     
     if (XSD(cl)->mousehidd)
-    	has_mouse_hidd = TRUE;
-	
+        has_mouse_hidd = TRUE;
+        
     ReleaseSemaphore( &XSD(cl)->sema);
     
     if (has_mouse_hidd) { /* Cannot open twice */
-    	D(bug("[GDIMouse] Attempt to create a second instance\n"));
-    	return NULL; /* Should have some error code here */
+        D(bug("[GDIMouse] Attempt to create a second instance\n"));
+        return NULL; /* Should have some error code here */
     }
 
     o = (OOP_Object *)OOP_DoSuperMethod(cl, o, (OOP_Msg)msg);
     D(bug("[GDIMouse] Object created by superclass: 0x%p\n"));
     if (o)
     {
-	struct gdimouse_data *data = OOP_INST_DATA(cl, o);
-	struct GDI_Control   *ctl = XSD(cl)->ctl;
-	struct TagItem       *tag, *tstate;
-	
-	data->buttons = 0;
-	data->interrupt = KrnAddIRQHandler(ctl->MouseIrq, MouseIntHandler, data, ctl);
-	D(bug("[GDIMouse] Mouse interrupt object: 0x%p\n", data->interrupt));
-    	if (data->interrupt) {
-	    tstate = msg->attrList;
-	    while ((tag = NextTagItem((const struct TagItem **)&tstate)))
-	    {
-		ULONG idx;
-	    
-		if (IS_HIDDMOUSE_ATTR(tag->ti_Tag, idx)) {
-		    switch (idx) {
-		    case aoHidd_Mouse_IrqHandler:
-		        D(bug("[GDIMouse] Callback address 0x%p\n", tag->ti_Data));
-		        data->mouse_callback = (VOID (*)())tag->ti_Data;
-			break;
-		    case aoHidd_Mouse_IrqHandlerData:
-		        D(bug("[GDIMouse] Callback data 0x%p\n", tag->ti_Data));
-		        data->callbackdata = (APTR)tag->ti_Data;
-			break;
-		    }
-		}
-	    } /* while (tags to process) */
-	
-	    /* Install the mouse hidd */
-	    ObtainSemaphore( &XSD(cl)->sema);
-	    XSD(cl)->mousehidd = o;
-	    ReleaseSemaphore( &XSD(cl)->sema);
-	    return o;
-	}
-    	OOP_MethodID disp_mid = OOP_GetMethodID(IID_Root, moRoot_Dispose);
-    	OOP_CoerceMethod(cl, o, (OOP_Msg) &disp_mid);
+        struct gdimouse_data *data = OOP_INST_DATA(cl, o);
+        struct GDI_Control   *ctl = XSD(cl)->ctl;
+        struct TagItem       *tag, *tstate;
+        
+        data->buttons = 0;
+        data->interrupt = KrnAddIRQHandler(ctl->MouseIrq, MouseIntHandler, data, ctl);
+        D(bug("[GDIMouse] Mouse interrupt object: 0x%p\n", data->interrupt));
+        if (data->interrupt) {
+            tstate = msg->attrList;
+            while ((tag = NextTagItem((const struct TagItem **)&tstate)))
+            {
+                ULONG idx;
+            
+                if (IS_HIDDMOUSE_ATTR(tag->ti_Tag, idx)) {
+                    switch (idx) {
+                    case aoHidd_Mouse_IrqHandler:
+                        D(bug("[GDIMouse] Callback address 0x%p\n", tag->ti_Data));
+                        data->mouse_callback = (VOID (*)())tag->ti_Data;
+                        break;
+                    case aoHidd_Mouse_IrqHandlerData:
+                        D(bug("[GDIMouse] Callback data 0x%p\n", tag->ti_Data));
+                        data->callbackdata = (APTR)tag->ti_Data;
+                        break;
+                    }
+                }
+            } /* while (tags to process) */
+        
+            /* Install the mouse hidd */
+            ObtainSemaphore( &XSD(cl)->sema);
+            XSD(cl)->mousehidd = o;
+            ReleaseSemaphore( &XSD(cl)->sema);
+            return o;
+        }
+        OOP_MethodID disp_mid = OOP_GetMethodID(IID_Root, moRoot_Dispose);
+        OOP_CoerceMethod(cl, o, (OOP_Msg) &disp_mid);
         return NULL;
     }
     return o;
 }
-
 
 /****************************************************************************************/
 
@@ -117,80 +179,16 @@ VOID GDIMouse__Root__Dispose(OOP_Class *cl, OOP_Object *o, OOP_Msg msg)
 
 /****************************************************************************************/
 
-static BOOL check_button(UWORD new, UWORD mask, UWORD arosbutton, struct pHidd_Mouse_Event *e, struct gdimouse_data *data)
-{
-    UWORD old = data->buttons & mask;
-
-    new &= mask;
-    if (old != new) {
-        e->button = arosbutton;
-        e->type = new ? vHidd_Mouse_Press : vHidd_Mouse_Release;
-        data->mouse_callback(data->callbackdata, e);
-        return TRUE;
-    } else
-        return FALSE;
-}
-
-static VOID MouseIntHandler(struct gdimouse_data *data, struct GDI_Control *MOUSEDATA)
-{
-    struct pHidd_Mouse_Event e;
-    UWORD new_buttons;
-    BOOL button;
-    /* Due to asynchronous nature of host-side window service thread GDI_Control structure acts like hardware registers.
-       There can be many pending events before we get here and the structure will hold a summary of all states, however
-       MouseEvent will contain only last event. Because of this we read it ASAP and pay as little attention to MouseEvent
-       as possible.
-    */
-
-    D(bug("[GDIMouse] Interrupt\n"));
-    switch(MOUSEDATA->MouseEvent) {
-    case WM_MOUSEWHEEL:
-        /* Wheel delta comes only with WM_MOUSEWHEEL, otherwise it's zero */
-        e.y = -MOUSEDATA->WheelDelta; /* Windows gives us inverted data */
-        e.x = 0;
-    	e.type   = vHidd_Mouse_WheelMotion;
-    	e.button = vHidd_Mouse_NoButton;
-    	data->mouse_callback(data->callbackdata, &e);
-    	break;
-    default:
-    	e.x = MOUSEDATA->MouseX;
-    	e.y = MOUSEDATA->MouseY;
-    	new_buttons = MOUSEDATA->Buttons;
-
-    	button = check_button(new_buttons, MK_LBUTTON, vHidd_Mouse_Button1, &e, data);
-    	button |= check_button(new_buttons, MK_RBUTTON, vHidd_Mouse_Button2, &e, data);
-    	button |= check_button(new_buttons, MK_MBUTTON, vHidd_Mouse_Button3, &e, data);
-    	if (button)
-    	    data->buttons = new_buttons;
-    	else {
-    	    e.button = vHidd_Mouse_NoButton;
-	    e.type = vHidd_Mouse_Motion;
-	    data->mouse_callback(data->callbackdata, &e);
-	}
-    }
-}
-
-/****************************************************************************************/
-
-#undef XSD
-#define XSD(cl) (&LIBBASE->xsd)
-
-/****************************************************************************************/
-
 static int GDIMouse_Init(LIBBASETYPEPTR LIBBASE)
 {
     return OOP_ObtainAttrBases(attrbases);
 }
-
-/****************************************************************************************/
 
 static int GDIMouse_Expunge(LIBBASETYPEPTR LIBBASE)
 {
     OOP_ReleaseAttrBases(attrbases);
     return TRUE;
 }
-
-/****************************************************************************************/
 
 ADD2INITLIB(GDIMouse_Init, 0)
 ADD2EXPUNGELIB(GDIMouse_Expunge, 0)
