9fd06cf4bdf648c11035fe59dae63ada13af06cc
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-11-24 09:47:24 +0000

    Fixed warnings on 64 bits, HWND is ULONG, not APTR.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42591 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-mingw32/hidd/wingdi/bitmap.h
+++ arch/all-mingw32/hidd/wingdi/bitmap.h
@@ -1,5 +1,5 @@
 /*
-    Copyright  1995-2010, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2011, The AROS Development Team. All rights reserved.
     $Id$
 */
 
@@ -12,17 +12,17 @@
 
 enum
 {
-    aoHidd_GDIBitMap_DeviceContext, /* ..G - bitmap's device context	      */
+    aoHidd_GDIBitMap_DeviceContext, /* ..G - bitmap's device context          */
     aoHidd_GDIBitMap_SysDisplay,    /* I.. - a display to be compatible with  */
 
     num_Hidd_GDIBitMap_Attrs
 };
 
-#define aHidd_GDIBitMap_DeviceContext	(HiddGDIBitMapAB + aoHidd_GDIBitMap_DeviceContext)
-#define aHidd_GDIBitMap_Window		(HiddGDIBitMapAB + aoHidd_GDIBitMap_Window)
-#define aHidd_GDIBitMap_SysDisplay	(HiddGDIBitMapAB + aoHidd_GDIBitMap_SysDisplay)
+#define aHidd_GDIBitMap_DeviceContext   (HiddGDIBitMapAB + aoHidd_GDIBitMap_DeviceContext)
+#define aHidd_GDIBitMap_Window          (HiddGDIBitMapAB + aoHidd_GDIBitMap_Window)
+#define aHidd_GDIBitMap_SysDisplay      (HiddGDIBitMapAB + aoHidd_GDIBitMap_SysDisplay)
 
-#define IS_BM_ATTR(attr, idx) 	    ( ( (idx) = (attr) - HiddBitMapAttrBase) < num_Hidd_BitMap_Attrs)
+#define IS_BM_ATTR(attr, idx)       ( ( (idx) = (attr) - HiddBitMapAttrBase) < num_Hidd_BitMap_Attrs)
 #define IS_GDIBM_ATTR(attr, idx)    ( ( (idx) = (attr) - HiddGDIBitMapAB) < num_Hidd_GDIBitMap_Attrs)
 
 /* This structure is used as instance data for the bitmap class.
@@ -31,17 +31,17 @@ enum
 struct bitmap_data
 {
     struct MinNode node;
-    APTR dc;	     /* Device context							        */
-    APTR bitmap;     /* Actual Windows bitmap object						*/
-    APTR dc_bitmap;  /* Original DC's bitmap object, needs to be put back before freeing the DC */
-    APTR display;    /* System display - to what DC should be compatible			*/
-    APTR window;     /* Window in which the bitmap is displayed				        */
-    LONG win_width;  /* Window size (cached from ModeID)					*/
-    LONG win_height;
-    LONG bm_width;   /* Requested bitmap size. (not rounded up)					*/
-    LONG bm_height;
-    LONG bm_left;    /* Bitmap edge coordinates							*/
-    LONG bm_top;
+    APTR  dc;         /* Device context                                                          */
+    APTR  bitmap;     /* Actual Windows bitmap object                                            */
+    APTR  dc_bitmap;  /* Original DC's bitmap object, needs to be put back before freeing the DC */
+    APTR  display;    /* System display - to what DC should be compatible                        */
+    ULONG window;     /* Window in which the bitmap is displayed                                 */
+    LONG  win_width;  /* Window size (cached from ModeID)                                        */
+    LONG  win_height;
+    LONG  bm_width;   /* Requested bitmap size. (not rounded up)                                 */
+    LONG  bm_height;
+    LONG  bm_left;    /* Bitmap edge coordinates                                                 */
+    LONG  bm_top;
 };
 
 #endif /* _BITMAP_H */
