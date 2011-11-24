ae8320da8beabb50c993581e9a1245251f1b8443
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-11-24 06:41:03 +0000

    Fixed stack alignment when calling exit code. Got rid of crashes under 64-bit Windows, AROS now boots up to "no bootable media" screen.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42586 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/x86_64-all/exec/preparecontext.c
+++ arch/x86_64-all/exec/preparecontext.c
@@ -15,13 +15,14 @@
 #include "exec_intern.h"
 #include "exec_util.h"
 
+extern void TaskExitStub(void);
+
 #define _PUSH(sp, val) *--sp = (IPTR)val
 
 BOOL PrepareContext(struct Task *task, APTR entryPoint, APTR fallBack,
                     const struct TagItem *tagList, struct ExecBase *SysBase)
 {
     IPTR args[2] = {0};
-    WORD numargs = 0;
     IPTR *sp = task->tc_SPReg;
     struct TagItem *t;
     struct ExceptionContext *ctx;
@@ -47,8 +48,6 @@ BOOL PrepareContext(struct Task *task, APTR entryPoint, APTR fallBack,
 #define STACKARG(x)                   \
         case TASKTAG_ARG ## x:        \
             args[x - 7] = t->ti_Data; \
-            if (x - 6 > numargs)      \
-                numargs = x - 6;      \
             break;
 
         REGARG(1, rdi)
@@ -62,13 +61,35 @@ BOOL PrepareContext(struct Task *task, APTR entryPoint, APTR fallBack,
         }
     }
 
-    /* On x86-64 C function gets on stack only last two arguments */
-    while (numargs > 0)
-        _PUSH(sp, args[--numargs]);
+    /*
+     * 64-bit ABI says that the stack must be 16-byte aligned *BEFORE*
+     * calling a function. When a return address is pushed by call instruction,
+     * the stack loses alignment, the callee knows about it and fixes it up.
+     * Commonly it's fixed automatically by pushing old frame pointer.
+     * A little of magic below ensures proper stack alignment for both entryPoint
+     * and fallBack routines.
+     */
+    _PUSH(sp, 0);        /* De-align the stack (this will be RSP value when fallBack is called) */
+    _PUSH(sp, fallBack); /* Push real exit code address for TaskExitStub */
 
-    /* First we push the return address */
-    _PUSH(sp, fallBack);
-        
+    /*
+     * Now stacked arguments for entry point. On x86-64 C function gets on stack only
+     * last two arguments, and we always push them both even if they are empty.
+     * This is needed for two reasons:
+     * 1. Keep stack 16-byte-aligned (we can't push only one, for example).
+     * 2. Simplify TaskExitStub, so that it always knows there are two arguments pushed.
+     */
+    _PUSH(sp, args[1]);
+    _PUSH(sp, args[0]);
+
+    /*
+     * Now push the return address. Stack becomes de-aligned here, this will be RSP
+     * value upon entering entryPoint.
+     * TaskExitStub will remove two arguments pushed above and execute 'ret' instruction.
+     * This way fallBack will be called with proper RSP value.
+     */
+    _PUSH(sp, TaskExitStub);
+
     /* Then set up the frame to be used by Dispatch() */
     ctx->rbp = 0;
     ctx->rip = (UQUAD)entryPoint;
