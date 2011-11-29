ead38cd7455efeee95ebcc1b2f616154eb6b477e
Ball000 <olivier.tigreat@gmail.com>
2011-11-29 21:18:49 +0000

    ASL file requester handling of "Parent" gadget: Let's do it better (partially reverting, sorry)...
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42652 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/asl/filereqsupport.c
+++ workbench/libs/asl/filereqsupport.c
@@ -125,27 +125,6 @@ static void fixpath(char *pathstring)
 
 /*****************************************************************************************/
 
-void FRClickOnVolumes(struct LayoutData *ld, struct AslBase_intern *AslBase)
-{
-    struct FRUserData *udata = (struct FRUserData *)ld->ld_UserData;
-    
-    if (udata->Flags & FRFLG_SHOWING_VOLUMES)
-    {
-        union
-        {
-            UBYTE *dir;
-            IPTR   baz;
-        } foo;
-
-        GetAttr(STRINGA_TextVal, udata->PathGad, &foo.baz);
-        FRGetDirectory(foo.dir, ld, AslBase);
-    } else {
-        FRGetVolumes(ld, AslBase);
-    }
-}
-
-/*****************************************************************************************/
-
 void FRRefreshListview(struct LayoutData *ld, struct AslBase_intern *AslBase)
 {
     struct FRUserData 	*udata = (struct FRUserData *)ld->ld_UserData;	
@@ -840,9 +819,8 @@ BOOL FRParentPath(struct LayoutData *ld, struct AslBase_intern *AslBase)
     BPTR lock = Lock(pathstring, ACCESS_READ);
     if (!lock)
     {
-        D(bug("Could not lock parent directory (\"%s\"), doing volumes instead\n", pathstring));
-        FRClickOnVolumes(ld, AslBase);
-        result = FALSE;
+        D(bug("Could not lock parent directory (\"%s\"), let's reach volumes list\n", pathstring));
+        result = FRGetVolumes(ld, AslBase);
     }
     else
     {
