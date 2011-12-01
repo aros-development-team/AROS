519c646351e5f976fdf1029203ce5f0e23a46028
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-12-01 12:36:48 +0000

    Ugly #ifdef's turned into nice macros
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42698 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/exec/chipset.h
@@ -0,0 +1,59 @@
+/*
+    Copyright © 2011, The AROS Development Team. All rights reserved.
+    $Id: $
+
+    Desc: Amiga(tm) chipset control macros
+    Lang: english
+*/
+
+#ifdef AROS_ARCH_amiga
+
+#include <hardware/custom.h>
+
+static inline void CUSTOM_ENABLE(ULONG intNumber)
+{
+    if (intNumber < INTB_INTEN)
+    {
+        volatile struct Custom *custom = (struct Custom *)(void **)0xdff000;
+
+	custom->intena = (UWORD)(INTF_SETCLR|(1L<<intNumber));
+    }
+}
+
+static inline void CUSTOM_DISABLE(ULONG intNumber, struct List *list)
+{
+    if (intNumber < INTB_INTEN)
+    {
+        /* disable interrupts if there are no more nodes on the list */
+        if (list->lh_TailPred == (struct Node *)list)
+        {
+	    volatile struct Custom *custom = (struct Custom *)(void **)0xdff000;
+
+	    custom->intena = (UWORD)((1<<intNumber));
+	}
+    }
+}
+
+static inline void CUSTOM_ACK(UWORD intBit)
+{
+    volatile struct Custom *custom = (struct Custom*)0xdff000;
+
+    custom->intena = intBit;
+    custom->intreq = intBit;
+}
+
+static inline void CUSTOM_CAUSE(UWORD intBit)
+{
+    volatile struct Custom *custom = (struct Custom*)0xdff000;
+
+    custom->intreq = INTF_SETCLR | intBit;
+}
+
+#else
+
+#define CUSTOM_ENABLE(intNumber)
+#define CUSTOM_DISABLE(intNumber, list)
+#define CUSTOM_ACK(intBit)
+#define CUSTOM_CAUSE(intBit)
+
+#endif
