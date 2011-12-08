772b38e657eeafcc60e1bf78b8b59d956086e373
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-12-08 07:19:14 +0000

    - Fixed displaying device name - Added ASCII form of DosType, for easier identification
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42760 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/mountlist.c
+++ test/mountlist.c
@@ -3,9 +3,27 @@
 #include <libraries/expansionbase.h>
 #include <proto/exec.h>
 
+#include <ctype.h>
 #include <stdio.h>
 
 int __nocommandline = 1;
+
+static void PrintDosType(ULONG dt)
+{
+    unsigned int i;
+
+    for (i = 0; i < 4; i++)
+    {
+    	unsigned char c = dt >> (24 - i * 8);
+
+	if (isprint(c))
+	    putchar(c);
+    	else
+    	    printf("\\%X", c);
+    }
+
+    RawPutChar('\n');
+}
 
 int main(void)
 {
@@ -23,7 +41,7 @@ int main(void)
         struct DeviceNode *dn = n->bn_DeviceNode;
 
         printf("BootNode %p, Flags 0x%08X, ConfigDev %p\n", n, n->bn_Flags, n->bn_Node.ln_Name);
-        printf("DeviceNode %p <%p>\n", dn, BADDR(dn->dn_Name));
+        printf("DeviceNode %p <%s>\n", dn, (char *)BADDR(dn->dn_Name));
         printf("Type %d, Task %p, SegList %p\n", (int)dn->dn_Type, BADDR(dn->dn_Task), BADDR(dn->dn_SegList));
 
         if (dn->dn_Startup)
@@ -31,15 +49,17 @@ int main(void)
             struct FileSysStartupMsg *fssm = BADDR(dn->dn_Startup);
 
             printf("FileSysStartupMsg %p", fssm);
-            if (fssm->fssm_Device)
+            if (fssm->fssm_Device && fssm->fssm_Device[0])
                 printf(" <%s unit %ld flags 0x%08X>", AROS_BSTR_ADDR(fssm->fssm_Device), (long)fssm->fssm_Unit, (unsigned int)fssm->fssm_Flags);
             printf("\n");
 
             if (fssm->fssm_Environ)
             {
                 struct DosEnvec *de = BADDR(fssm->fssm_Environ);
-                
-                printf("DosEnvec %p DosType 0x%08lX BootPri %ld\n", de, de->de_DosType, de->de_BootPri);
+
+                printf("DosEnvec %p DosType 0x%08lX <", de, de->de_DosType);
+                PrintDosType(de->de_DosType);
+                printf("> BootPri %ld\n", de->de_BootPri);
             }
         }
         printf("\n");
