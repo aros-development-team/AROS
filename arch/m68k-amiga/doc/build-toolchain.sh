14c782a373de0acfee29c9a7ef721f98cd9c42b1
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-12-09 11:42:56 +0000

    arch/m68k-amiga: Update bootstrap compiler script
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42805 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/m68k-amiga/doc/build-toolchain.sh
+++ arch/m68k-amiga/doc/build-toolchain.sh
@@ -1,11 +1,5 @@
 #!/bin/sh -x
 
-FETCH_ONLY=no
-if [ "$1" = "--fetch-only" ]; then
-	FETCH_ONLY=yes
-	shift
-fi
-
 PREFIX="$1"
 if [ -z "$PREFIX" ]; then
 	PREFIX="/opt/m68k-elf"
@@ -14,11 +8,13 @@ fi
 PATH=$PATH:`pwd`/fake
 export PATH
 
-BINUTILS_VERSION=2.20.1
-GCC_VERSION=4.5.1
-GMP_VERSION=4.3.2
-MPFR_VERSION=2.4.2
-MPC_VERSION=0.8.1
+BINUTILS_VERSION=2.22
+GCC_VERSION=4.6.2
+GMP_VERSION=5.0.1
+MPFR_VERSION=3.0.1
+MPC_VERSION=0.9
+GDB_VERSION=7.3.1
+NEWLIB_VERSION=1.19.0
 
 fetch () {
 	mkdir -p download || exit 1
@@ -51,54 +47,70 @@ unpack () {
 }
 
 config () {
+	[ -n "${STERILIZE}" ] && rm -rf "build/${configBASE}"
+	[ -e build/.$1.config -a build/.$1.config -nt src/$1-$2/configure ] && return 0
 	configBASE=$1
 	shift
 	configVERSION=$1
 	shift
 
-	[ -n "${STERILIZE}" ] && rm -rf "build/${configBASE}"
 	mkdir -p build/${configBASE}
-	[ ! -f build/${configBASE}/Makefile ] && (cd build/${configBASE}
+	(cd build/${configBASE}
 	 ../../src/${configBASE}-${configVERSION}/configure  \
 		--without-headers \
 		--disable-threads \
 		--disable-libssp \
 		--disable-nls \
 		--disable-shared \
+		--disable-lto \
 		--without-pic \
 		--with-gmp="${PREFIX}" \
 		--with-mpfr="${PREFIX}" \
 		--with-mpc="${PREFIX}" \
 		--prefix="${PREFIX}" \
 		"$@" || exit 1
-	)
+	) || exit $?
+	touch build/.${configBASE}.config
 }
 
 build () {
+	[ -e build/.$1.build -a build/.$1.build -nt build/.$1.config ] && return 0
 	make -C build/$1 || exit 1
+	touch build/.$1.build
 }
 
 deploy () {
+	[ -e build/.$1.deploy -a build/.$1.deploy -nt build/.$1.build ] && return 0
 	make -C build/$1 install || exit 1
+	touch build/.$1.deploy
 }
 
 fetch ftp://ftp.gnu.org/pub/gnu/binutils/binutils-${BINUTILS_VERSION}.tar.bz2
 fetch ftp://ftp.gnu.org/pub/gnu/gcc/gcc-${GCC_VERSION}/gcc-core-${GCC_VERSION}.tar.bz2
+fetch ftp://ftp.gnu.org/pub/gnu/gcc/gcc-${GCC_VERSION}/gcc-g++-${GCC_VERSION}.tar.bz2
+fetch ftp://ftp.gnu.org/pub/gnu/gcc/gcc-${GCC_VERSION}/gcc-objc-${GCC_VERSION}.tar.bz2
 fetch ftp://ftp.gnu.org/pub/gnu/gmp/gmp-${GMP_VERSION}.tar.bz2
 fetch ftp://ftp.gnu.org/pub/gnu/mpfr/mpfr-${MPFR_VERSION}.tar.bz2
-fetch http://www.evillabs.net/AROS/gcc-4.5.1-amiga-elf.patch
-fetch ftp://gcc.gnu.org/pub/gcc/infrastructure/mpc-${MPC_VERSION}.tar.gz
-(gzip -dc download/mpc-${MPC_VERSION}.tar.gz | bzip2 -c >download/mpc-${MPC_VERSION}.tar.bz2) || exit 1
-
-if [ "$FETCH_ONLY" = "yes" ]; then
-	exit 0
+fetch ftp://ftp.gnu.org/pub/gnu/gdb/gdb-${GDB_VERSION}.tar.bz2
+fetch http://www.evillabs.net/AROS/gcc-4.6.2-aros.patch
+fetch http://www.multiprecision.org/mpc/download/mpc-${MPC_VERSION}.tar.gz
+if [ download/mpc-${MPC_VERSION}.tar.gz -nt download/mpc-${MPC_VERSION}.tar.bz2 ]; then
+	(gzip -dc download/mpc-${MPC_VERSION}.tar.gz | bzip2 -c >download/mpc-${MPC_VERSION}.tar.bz2) || exit 1
+fi
+fetch ftp://sources.redhat.com/pub/newlib/newlib-${NEWLIB_VERSION}.tar.gz
+if [ download/newlib-${NEWLIB_VERSION}.tar.gz -nt download/newlib-${NEWLIB_VERSION}.tar.bz2 ]; then
+	(gzip -dc download/newlib-${NEWLIB_VERSION}.tar.gz | bzip2 -c >download/newlib-${NEWLIB_VERSION}.tar.bz2) || exit 1
 fi
 
 unpack binutils-${BINUTILS_VERSION}
 unpack gcc-core-${GCC_VERSION} gcc-${GCC_VERSION}
+unpack gcc-objc-${GCC_VERSION} gcc-${GCC_VERSION}
+unpack gcc-g++-${GCC_VERSION} gcc-${GCC_VERSION}
 unpack gmp-${GMP_VERSION}
 unpack mpfr-${MPFR_VERSION}
 unpack mpc-${MPC_VERSION}
+unpack gdb-${GDB_VERSION}
+unpack newlib-${NEWLIB_VERSION}
 
 config binutils ${BINUTILS_VERSION} \
 		--target=m68k-elf \
@@ -122,6 +134,34 @@ deploy mpc
 config gcc ${GCC_VERSION} \
 		--target=m68k-elf \
 		--program-prefix=m68k-elf- \
-		--enable-languages=c 
+		--without-target-libiberty \
+		--without-target-zlib \
+		--disable-bootstrap \
+		--enable-languages="c"
 build gcc
 deploy gcc
+
+config gdb ${GDB_VERSION} \
+		--target=m68k-elf \
+		--program-prefix=m68k-elf-
+build gdb
+deploy gdb
+
+# Minimal C library
+config newlib ${NEWLIB_VERSION} \
+		--target=m68k-elf \
+		--program-prefix=m68k-elf- \
+		--enable-languages="c"
+build newlib
+deploy newlib
+
+# Build C++ and ObjC
+config gcc ${GCC_VERSION} \
+		--with-newlib \
+		--target=m68k-elf \
+		--program-prefix=m68k-elf- \
+		--disable-bootstrap \
+		--enable-languages="c c++ objc"
+build gcc
+deploy gcc
+
