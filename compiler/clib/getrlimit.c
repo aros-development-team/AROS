93ec0eb4566a58589234b07b9d1e4218a98187d9
ezrec <jason.mcmullan@gmail.com>
2011-12-10 16:28:42 +0000

    arosc.library: Add getdtablesize/getrlimit/setrlimit stubs
    Needed for newer GNU Tar
    
    Staf V. - can you update the arosc.conf to put these in the 'right' places?
    
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@42828 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/clib/getrlimit.c
@@ -0,0 +1,61 @@
+/*
+    Copyright © 1995-2003, The AROS Development Team. All rights reserved.
+    $Id$
+
+    POSIX function getrlimit().
+*/
+
+#define getrlimit getrlimit
+
+#include <errno.h>
+#include <sys/resource.h>
+
+#include "__fdesc.h"
+
+/*****************************************************************************
+
+    NAME */
+
+	int getrlimit (
+
+/*  SYNOPSIS */
+	int resource,
+	struct rlimit *rlp)
+
+/*  FUNCTION
+	Get the limits of certain system resources
+
+    INPUTS
+	resource - the resource type to get
+	rlp      - returned resource information
+
+    RESULT
+	On success, returns 0. -1 and errno on error.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	setrlimit(), getdtablesize()
+
+    INTERNALS
+
+******************************************************************************/
+{
+    int retval = 0;
+
+    switch (resource) {
+    case RLIMIT_NOFILE: /* needed for getdtablesize() */
+        rlp->rlim_cur = rlp->rlim_max = __getfdslots(); 
+        break;
+    default:
+        retval = -1;
+        errno = EINVAL;
+        break;
+    }
+
+    return retval;
+}
