5cee4cd8c894f2282812cb00605862b496d0bf30
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-12-21 08:03:34 +0000

    Some (incomplete) adaptations to WinCE
    git-svn-id: https://svn.aros.org/svn/aros/trunk@43131 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-mingw32/bootstrap/shutdown.c
+++ arch/all-mingw32/bootstrap/shutdown.c
@@ -7,41 +7,52 @@
 
 #define D(x)
 
-static char *bootstrapname;
-static char *cmdline;
+/* Windows CE has no notion of "current directory" */
+#ifdef UNDER_CE
+#define bootstrapdir NULL
+#endif
+
+static LPTSTR bootstrapname;
+static LPTSTR cmdline;
 
 void SaveArgs(char **argv)
 {
+    /* FIXME: Adapt to WinCE by converting argv[0] to Unicode */
     bootstrapname = argv[0];
     cmdline = GetCommandLine();
 }
 
 void __aros Host_Shutdown(unsigned char warm)
 {
-    char var[SHARED_RAM_LEN];
     STARTUPINFO runinfo;
     PROCESS_INFORMATION ProcInfo;
+#ifndef UNDER_CE
+    char var[SHARED_RAM_LEN];
 
     if (warm)
-	sprintf(var, "%s=%p:%p", SHARED_RAM_VAR, RAM_Handle, RAM_Address);
+        sprintf(var, "%s=%p:%p", SHARED_RAM_VAR, RAM_Handle, RAM_Address);
     else
-	strcpy(var, SHARED_RAM_VAR "=");
+        strcpy(var, SHARED_RAM_VAR "=");
 
     D(printf("[Shutdown] Dir: %s, Name: %s, RAM: %s, Command line: %s\n", bootstrapdir, bootstrapname, var, cmdline));
     putenv(var);
     SetCurrentDirectory(bootstrapdir);
+#endif
     FillMemory(&runinfo, sizeof(runinfo), 0);
     runinfo.cb = sizeof(runinfo);
 
-    /* If we create new process without CREATE_NEW_CONSOLE, strange thing will happen if we start AROS
-     * from within command line processor. Looks like it's Windows bug/misdesign. Well, let's reopen the console every time. */
+    /*
+     * If we create new process without CREATE_NEW_CONSOLE, strange thing will happen if we start AROS
+     * from within command line processor. Looks like it's Windows bug/misdesign. Well, let's reopen the console every time.
+     */
     if (CreateProcess(bootstrapname, cmdline, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, bootstrapdir, &runinfo, &ProcInfo))
     {
-        D(printf("[Shutdown] AROS re-run\n"));   
+        D(printf("[Shutdown] AROS re-run\n"));
+
         CloseHandle(ProcInfo.hProcess);
         CloseHandle(ProcInfo.hThread);
         exit(0);
     }
+
     D(printf("[Shutdown] Unable to re-run AROS\n"));
 }
-
