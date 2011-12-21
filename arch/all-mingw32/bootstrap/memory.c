26f1327be407b5853ec9d6fcdd34e9a361446198
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-12-21 06:32:45 +0000

    Detab
    git-svn-id: https://svn.aros.org/svn/aros/trunk@43129 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-mingw32/bootstrap/memory.c
+++ arch/all-mingw32/bootstrap/memory.c
@@ -45,38 +45,38 @@ void *AllocateRAM(size_t len)
     var = getenv(SHARED_RAM_VAR);
     if (var)
     {
-	D(printf("[AllocateRAM] Found RAM specification: %s\n", var));
-	if (sscanf(var, "%p:%p", &RAM_Handle, &addr) != 2) {
-	    D(printf("[AllocateRAM] Error parsing specification\n"));
-	    RAM_Handle = NULL;
-	    addr = NULL;
-	}
+        D(printf("[AllocateRAM] Found RAM specification: %s\n", var));
+        if (sscanf(var, "%p:%p", &RAM_Handle, &addr) != 2) {
+            D(printf("[AllocateRAM] Error parsing specification\n"));
+            RAM_Handle = NULL;
+            addr = NULL;
+        }
     }
 
     D(printf("[AllocateRAM] Inherited memory handle 0x%p address 0x%p\n", RAM_Handle, addr));
     if (!RAM_Handle)
     {
         sa.nLength = sizeof(sa);
-	sa.lpSecurityDescriptor = NULL;
-	sa.bInheritHandle = TRUE;
-	RAM_Handle = CreateFileMapping(INVALID_HANDLE_VALUE, &sa, PAGE_EXECUTE_READWRITE, 0, len, NULL);
+        sa.lpSecurityDescriptor = NULL;
+        sa.bInheritHandle = TRUE;
+        RAM_Handle = CreateFileMapping(INVALID_HANDLE_VALUE, &sa, PAGE_EXECUTE_READWRITE, 0, len, NULL);
     }
     if (!RAM_Handle)
     {
-	D(printf("[AllocateRAM] PAGE_EXECUTE_READWRITE failed, retrying with PAGE_READWRITE\n"));
-	RAM_Handle = CreateFileMapping(INVALID_HANDLE_VALUE, &sa, PAGE_READWRITE, 0, len, NULL);
+        D(printf("[AllocateRAM] PAGE_EXECUTE_READWRITE failed, retrying with PAGE_READWRITE\n"));
+        RAM_Handle = CreateFileMapping(INVALID_HANDLE_VALUE, &sa, PAGE_READWRITE, 0, len, NULL);
     }
     D(printf("[AllocateRAM] Shared memory handle 0x%p\n", RAM_Handle));
     if (!RAM_Handle)
-	return NULL;
+        return NULL;
 
     RAM_Address = MapViewOfFileEx(RAM_Handle, FILE_MAP_ALL_ACCESS|FILE_MAP_EXECUTE, 0, 0, 0, addr);
     D(printf("[AllocateRAM] Requested address 0x%p, mapped at 0x%p\n", addr, RAM_Address));
 
     if (!RAM_Address)
     {
-	CloseHandle(RAM_Handle);
-	RAM_Handle = NULL;
+        CloseHandle(RAM_Handle);
+        RAM_Handle = NULL;
     }
 
     return RAM_Address;
