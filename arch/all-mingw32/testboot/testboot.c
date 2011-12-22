7f5d846dbe44b1ecccd0d0402bff0b3fafefec23
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-12-22 11:40:44 +0000

    Added small test module to prove that CE bootstrap works
    git-svn-id: https://svn.aros.org/svn/aros/trunk@43167 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/all-mingw32/testboot/testboot.c
@@ -0,0 +1,46 @@
+#include <aros/kernel.h>
+#include <utility/tagitem.h>
+#include <proto/arossupport.h>
+
+#include <stdarg.h>
+
+#include "../kernel/hostinterface.h"
+
+int __vcformat(void *data, int(*outc)(int, void *), const char * format, va_list args);
+int KPrintf(struct HostInterface *hif, const char *format, ...);
+
+int __startup start(const struct TagItem *tags)
+{
+    const struct TagItem *tag;
+    struct HostInterface *hif = (struct HostInterface *)LibGetTagData(KRN_HostInterface, 0, tags);
+
+    if (!hif)
+        return -1;
+
+    KPrintf(hif, "Test module succesfully started\n");
+    KPrintf(hif, "Taglist at 0x%p:\n", tags);
+
+    while ((tag = LibNextTagItem(&tags)))
+    {
+    	KPrintf(hif, "0x%08lX 0x%p\n", tag->ti_Tag, tag->ti_Data);
+    }
+
+    return 0;
+}
+
+static int kputc(int c, void *data)
+{
+    return ((struct HostInterface *)data)->KPutC(c);
+}
+
+int KPrintf(struct HostInterface *iface, const char *format, ...)
+{
+    va_list ap;
+    int res;
+
+    va_start(ap, format);
+    res = __vcformat(iface, kputc, format, ap);
+    va_end(ap);
+
+    return res;
+}
