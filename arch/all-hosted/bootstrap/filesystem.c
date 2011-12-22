d9caad8b6b41c5d945a939b956e4c912e7697d84
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-12-22 10:51:58 +0000

    Implemented current directory emulation for Windows CE.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@43166 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-hosted/bootstrap/filesystem.c
+++ arch/all-hosted/bootstrap/filesystem.c
@@ -6,21 +6,9 @@
     Lang: english
 */
 
-/*
- * A temporary workaround to get the bootstrap compiled for WinCE.
- * In fact Windows CE port requires emulation of current directory.
- */
-#ifndef UNDER_CE
-
 #include <dirent.h>
-#include <sys/stat.h>
-
-#ifdef _WIN32
-#define APPNAME "AROSBootstrap.exe"
-#else
 #include <unistd.h>
-#define APPNAME "AROSBootstrap"
-#endif
+#include <sys/stat.h>
 
 #include "filesystem.h"
 
@@ -30,10 +18,8 @@ int SetRootDirectory(void)
 
     /* If AROSBootstrap.exe is found in the current directory, this means the bootstrap
      was started in its own dir. Go one level up in order to reach the root */
-    if (!stat(APPNAME, &st))
+    if (!stat("AROSBootstrap", &st))
 	return chdir("..");
 
     return 0;
 }
-
-#endif
--- arch/all-hosted/bootstrap/filesystem.h
+++ arch/all-hosted/bootstrap/filesystem.h
@@ -1,10 +1,19 @@
 /*
- *  filesystem.h
- *  AROS
- *
- *  Created by Pavel Fedin on 10/13/10.
- *  Copyright 2010 AROS Development Team. All rights reserved.
- *
- */
+    Copyright ? 2010-2011, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: Declarations of filesystem control routines
+    Lang: english
+*/
 
 int SetRootDirectory(void);
+
+/*
+ * Windows CE has no notion of "current directory".  We have to emulate it,
+ * so here comes this wrapper.
+ */
+#ifdef UNDER_CE
+FILE *file_open (const char *filename, const char *mode);
+#else
+#define file_open fopen
+#endif
