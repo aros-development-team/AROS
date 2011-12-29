844f1f3858ebdd5d25e2306a9b4161eb9b612a2f
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-12-29 11:08:37 +0000

    Some work in progress.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@43291 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-unix/devs/unixio/deviceutil.c
+++ arch/all-unix/devs/unixio/deviceutil.c
@@ -1,142 +1,106 @@
-static int unixio_Open(struct unixioDev *, struct IORequest *ioreq, IPTR unit, ULONG flags)
+#define DEBUG 0
+#define DOPEN(x)
+
+#include <errno.h>
+
+#include <aros/debug.h>
+#include <aros/symbolsets.h>
+#include <exec/errors.h>
+#include <proto/exec.h>
+
+#include "unixio_device.h"
+
+static int unixdevice_Open(struct UnixDevice *unixioDev, struct IOStdReq *ioreq, STRPTR unitname, ULONG flags)
 {
-    struct UnitData *data;
+    struct UnitData *unit;
 
-    D(bug("unixio.device: Open unit %s\n",unit));
+    D(bug("unixio.device: open UnitData %s\n", unitname));
 
-    if (ioreq->io_Message.mn_Length < sizeof(struct IOStdReq))
+    ObtainSemaphore(&unixioDev->sigsem);
+
+    unit = (struct UnitData *)FindName(&unixioDev->units, unitname);
+
+    if (UnitData)
     {
-        D(bug("unixio.device/open: IORequest structure passed to OpenDevice is too small!\n"));
+        /* TODO: Check sharing permission here */
+        UnitData->usecount++;
+        ReleaseSemaphore(&unixioDev->sigsem);
 
-        ioreq->io_Error = IOERR_OPENFAIL;
-        return FALSE;
+        ioreq->io_Unit                    = (struct Unit *)unit;
+        ioreq->io_Error                   = 0;
+        ioreq->io_Message.mn_Node.ln_Type = NT_REPLYMSG;
+
+        DOPEN(bug("unixio.device: in libopen func. Unit is already open\n"));    
+        return TRUE;
     }
 
-    ioreq->io_Message.mn_Node.ln_Type = NT_REPLYMSG;
+    DOPEN(bug("unixio.device: in libopen func. Creating new UnitData ...\n"));
 
-    /*
-     * In the list of available units look for the one with the same 
-     UnitNumber as the given one  */
-  if (0 == ioreq->io_Error)
-  {
-    PU = findUnit(ParallelDevice, unitnum);
-
-    /* If there is no such unit, yet, then create it */
-    if (NULL == PU)
+    unit = (struct UnitData *)AllocVec(sizeof(struct UnitData) + strlen(unitname), MEMF_PUBLIC);
+    if (unit)
     {
-      D(bug("Creating Unit %d\n",unitnum));
-      PU = AllocMem(sizeof(struct ParallelUnit), MEMF_CLEAR|MEMF_PUBLIC);
-      if (NULL != PU)
-      {
-        PU->pu_OpenerCount	= 1;
-        PU->pu_UnitNum		= unitnum;
-        PU->pu_Flags		= ioreq->io_Flags;
-        
-        /*
-        ** Initialize the message ports
-        */
-        NEWLIST(&PU->pu_QReadCommandPort.mp_MsgList);
-        PU->pu_QReadCommandPort.mp_Node.ln_Type = NT_MSGPORT;
-          
-        NEWLIST(&PU->pu_QWriteCommandPort.mp_MsgList);
-        PU->pu_QWriteCommandPort.mp_Node.ln_Type= NT_MSGPORT;
-   
-        InitSemaphore(&PU->pu_Lock);
-        /* do further initilization here. Like getting the ParallelUnit Object etc. */
-
-        PU->pu_Unit  = HIDD_Parallel_NewUnit(ParallelDevice->ParallelObject, unitnum);
-        if (NULL != PU->pu_Unit)
+        unit->fd = Hidd_UnixIO_Open(unixioDev->unixio, unitname, O_RWDR|O_NONBLOCK, 0755, NULL);
+        if (unit->fd != -1)
         {
-          HIDD_ParallelUnit_Init(PU->pu_Unit, RBF_InterruptHandler, NULL, WBE_InterruptHandler, NULL);
-          ioreq->io_Device = (struct Device *)ParallelDevice;
-          ioreq->io_Unit   = (struct Unit *)PU;  
+            unit->unitNode.ln_Name = unit->unitName;
+            unit->unixio           = unixioDev->unixio;
+            unit->usecount         = 1;
+            unit->writeLength      = 0;
+            unit->stopped          = FALSE;
+            unit->eofmode          = FALSE;
+            NEWLIST(&unit->readQueue);
+            NEWLIST(&unit->writeQueue);
+            strcpy(unit->unitName, unitname);
 
-          /*
-          ** put it in the list of open units
-          */
-          AddHead(&ParallelDevice->UnitList, (struct Node *)PU);
+            AddTail((struct List *)&unixioDev->units, &UnitData->n);
+            ReleaseSemaphore(&unixioDev->sigsem);
 
-          ioreq->io_Error  = 0;
- 
-          return TRUE;
+            ioreq->io_Unit                    = (struct Unit *)unit;
+            ioreq->io_Error                   = 0;
+            ioreq->io_Message.mn_Node.ln_Type = NT_REPLYMSG;
+
+            return TRUE;
         }
-
-        D(bug("ParallelUnit could not be created!\n"));
-          
-        FreeMem(PU, sizeof(struct ParallelUnit));
-
-        ioreq->io_Error = ParErr_DevBusy;
-      }
     }
-    else
-    {
-      /* the unit does already exist. */
-      /* 
-      ** Check whether one more opener to this unit is tolerated 
-      */
-      if (0 != (PU->pu_Flags & PARF_SHARED))
-      {
-        /*
-        ** This unit is in shared mode and one more opener
-        ** won't hurt.
-        */
-        ioreq->io_Device = (struct Device *)ParallelDevice;
-        ioreq->io_Unit   = (struct Unit *)PU;
-        ioreq->io_Error  = 0;
 
-        PU->pu_OpenerCount++;
-      }
-      else
-      {
-        /*
-        ** I don't allow another opener
-        */
-        ioreq->io_Error = ParErr_DevBusy;
-      }
-    }
-  }
+    ReleaseSemaphore(&unixioDev->sigsem);
+    ioreq->io_Error = IOERR_OPENFAIL;
 
-  return TRUE;
+    return FALSE;
 }
 
+ADD2OPENDEV(unixdevice_Open, 0);
 
 /****************************************************************************************/
 
-static int GM_UNIQUENAME(Close)
-(
-    LIBBASETYPEPTR ParallelDevice,
-    struct IORequest *ioreq
-)
+static int unixdevice_Close((struct UnixDevice *unixioDev, struct IOStdReq *ioreq)
 {
-  struct ParallelUnit * PU = (struct ParallelUnit *)ioreq->io_Unit;
+    struct UnitData *unit = (struct UnitData *)ioreq->io_Unit;
+    ULONG usecnt;
 
-  /*
-  ** Check whether I am the last opener to this unit
-  */
-  if (1 == PU->pu_OpenerCount)
-  {
-    /*
-    ** I was the last opener. So let's get rid of it.
-    */
-    /*
-    ** Remove the unit from the list
-    */
-    Remove((struct Node *)&PU->pu_Node);
-    
-    HIDD_Parallel_DisposeUnit(ParallelDevice->ParallelObject, PU->pu_Unit);
-  
-    FreeMem(PU, sizeof(struct ParallelUnit));
-  
-  }
-  else
-  {
-    /*
-    ** There are still openers. Decrease the counter.
-    */
-    PU->pu_OpenerCount--;
-  }
+    D(bug("unixio.device: close unit %s\n", unit->n.ln_Name));
 
-  return TRUE;
+    ObtainSemaphore(&unixioDev->sigsem);
+
+    usecnt = --unit->usecount;
+    if (!usecnt)
+    {
+        D(bug("unixio.device: Unit is no more in use, disposing...\n"));
+
+        Remove(&unit->unitNode);
+    }
+
+    ReleaseSemaphore(&unixioDev->sigsem);
+
+    if (!usecnt)
+    {
+        Hidd_UnixIO_CloseFile(unixioDev->unixio, unit->fd, NULL);
+        FreeVec(unit);
+    }
+
+    return TRUE;
 }
+
+ADD2CLOSEDEV(unixdevice_Close, 0);
 
 /****************************************************************************************/
