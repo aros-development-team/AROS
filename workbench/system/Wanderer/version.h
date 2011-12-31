1807b34edd9f47042baa06bc1e920e545e4a2331
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-12-31 14:29:02 +0000

    Wanderer 1.0
    git-svn-id: https://svn.aros.org/svn/aros/trunk@43406 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/system/Wanderer/version.h
+++ workbench/system/Wanderer/version.h
@@ -1,6 +1,7 @@
-#define WANDERERVERS    0
-#define WANDERERREV     96
-#define WANDERERSTR     "Wanderer 0.96 (26.12.2011)"
+#define WANDERERNAME    "Wanderer"
+#define WANDERERVERS    1
+#define WANDERERREV     0
+#define WANDERERSTR     WANDERERNAME" 1.0 (31.12.2011)"
 #define WANDERERAUTH    "The AROS Dev Team"
 #define WANDERERCOPY    "© " WANDERERAUTH
 #define VERSION "$VER:  " WANDERERSTR " " WANDERERCOPY
