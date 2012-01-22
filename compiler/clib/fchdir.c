cfeb29f0922710e1a2848fe5ae4bbb6bb29c347f
mattrust <mrustler@gmx.de>
2012-01-22 14:45:33 +0000

    Tabs -> Spaces
    git-svn-id: https://svn.aros.org/svn/aros/trunk@43694 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/fchdir.c
+++ compiler/clib/fchdir.c
@@ -18,26 +18,26 @@
     NAME */
 #include <unistd.h>
 
-	int fchdir(
+        int fchdir(
 
 /*  SYNOPSIS */
-	int fd )
+        int fd )
 
 /*  FUNCTION
-	Change the current working directory to the directory given as an open
-	file descriptor.
+        Change the current working directory to the directory given as an open
+        file descriptor.
 
     INPUTS
         fd - File descriptor of the directory to change to.
-	
+        
     RESULT
-	If the current directory was changed successfully, zero is returned.	
-    	Otherwise, -1 is returned and errno set apropriately.
-	
+        If the current directory was changed successfully, zero is returned.
+        Otherwise, -1 is returned and errno set apropriately.
+        
     NOTES
-    	At program exit, the current working directory will be changed back
-	to the one that was current when the program first started. If you
-	do not desire this behaviour, use dos.library/CurrentDir() instead.
+        At program exit, the current working directory will be changed back
+        to the one that was current when the program first started. If you
+        do not desire this behaviour, use dos.library/CurrentDir() instead.
 
     EXAMPLE
 
@@ -53,11 +53,11 @@
     BPTR oldlock = BNULL;
     BPTR newlock = BNULL;
     BPTR handle = BNULL;
-    
+
     if ( __get_default_file(fd, (long*) &handle) != 0 )
     {
-    	errno = EBADF;
-        goto error;    
+        errno = EBADF;
+        goto error;
     }
 
     newlock = DupLockFromFH(handle);
@@ -67,21 +67,21 @@
         errno = __arosc_ioerr2errno( IoErr() );
         goto error;
     }
-    oldlock = CurrentDir( newlock ); 
+    oldlock = CurrentDir( newlock );
 
     if( aroscbase->acb_cd_changed )
     {
-    	UnLock( oldlock );
+        UnLock( oldlock );
     }
     else
     {
-    	aroscbase->acb_cd_changed = TRUE;
-	aroscbase->acb_cd_lock    = oldlock;
+        aroscbase->acb_cd_changed = TRUE;
+        aroscbase->acb_cd_lock    = oldlock;
     }       
     return 0;
 
 error:
-    if( newlock != BNULL ) 
+    if( newlock != BNULL )
         UnLock( newlock );
     
     return -1;
