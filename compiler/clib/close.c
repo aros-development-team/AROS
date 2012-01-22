e3e76098bee78df0edaf40504fdf2c08c93de6da
mattrust <mrustler@gmx.de>
2012-01-22 13:20:45 +0000

    Tabs => spaces
    git-svn-id: https://svn.aros.org/svn/aros/trunk@43692 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/close.c
+++ compiler/clib/close.c
@@ -19,21 +19,21 @@
     NAME */
 #include <unistd.h>
 
-	int close (
+        int close (
 
 /*  SYNOPSIS */
-	int fd)
+        int fd)
 
 /*  FUNCTION
-	Closes an open file. If this is the last file descriptor
-	associated with this file, then all allocated resources
-	are freed, too.
+        Closes an open file. If this is the last file descriptor
+        associated with this file, then all allocated resources
+        are freed, too.
 
     INPUTS
-	fd - The result of a successful open()
+        fd - The result of a successful open()
 
     RESULT
-	-1 for error or zero on success.
+        -1 for error or zero on success.
 
     NOTES
         This function must not be used in a shared library or
@@ -44,7 +44,7 @@
     BUGS
 
     SEE ALSO
-	open(), read(), write(), fopen()
+        open(), read(), write(), fopen()
 
     INTERNALS
 
@@ -56,33 +56,33 @@
     {
         errno = EBADF;
 
-	return -1;
+        return -1;
     }
 
     if (--fdesc->fcb->opencount == 0)
     {
         /* Due to a *stupid* behaviour of the dos.library we cannot handle closing failures cleanly :-(
-	if (
-	    !(fdesc->fcb->privflags & _FCB_DONTCLOSE_FH) &&
-	    !Close(fdesc->fh)
-	)
-	{
-	    fdesc->opencount++;
-	    errno = __arosc_ioerr2errno(IoErr());
+        if (
+            !(fdesc->fcb->privflags & _FCB_DONTCLOSE_FH) &&
+            !Close(fdesc->fh)
+        )
+        {
+            fdesc->opencount++;
+            errno = __arosc_ioerr2errno(IoErr());
 
-	    return -1;
+            return -1;
         }
         */
-	/* FIXME: Damn dos.library! We cannot report the error code correctly! This oughta change someday... */
-	/* Since the dos.library destroys the file handle anyway, even if the closing fails, we cannot
+        /* FIXME: Damn dos.library! We cannot report the error code correctly! This oughta change someday... */
+        /* Since the dos.library destroys the file handle anyway, even if the closing fails, we cannot
            report the error code correctly, so just close the file and get out of here */
 
-	if (!(fdesc->fcb->privflags & _FCB_DONTCLOSE_FH))
-	{
+        if (!(fdesc->fcb->privflags & _FCB_DONTCLOSE_FH))
+        {
             (void)Close(fdesc->fcb->fh);
-	}
+        }
 
-	FreeVec(fdesc->fcb);
+        FreeVec(fdesc->fcb);
     }
 
     __free_fdesc(fdesc);
