c41c7197e6fa07df24fa9104811957691393a83f
ezrec <jason.mcmullan@gmail.com>
2012-02-15 05:08:23 +0000

    printer.device: Bare minimal graphics subsystem
    No dithering, no color correction, no RGB->CYMK conversions
    
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@44033 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/devs/printer/gfx.c
+++ workbench/devs/printer/gfx.c
@@ -9,71 +9,394 @@
 
 #include <string.h>
 
+#include <aros/debug.h>
 #include <aros/printertag.h>
 
 #include <proto/exec.h>
 #include <proto/arossupport.h>
+#include <proto/graphics.h>
+#include <proto/cybergraphics.h>
+
+#include <cybergraphx/cybergraphics.h>
+
+#include <devices/prtgfx.h>
 
 #include <SDI/SDI_hook.h>
 
 #include "printer_intern.h"
 
-/* Default function that generates a raster line
- *
- * AROS differs from AOS 3.9, in that it passes the 
- * hook the struct IODRPReq * for the command, instead
- * of NULL, as the object argument.
- */
-HOOKPROTONH(rastSource, VOID, const struct IODRPReq *io, struct DRPSourceMsg *msg)
-{
-    /* Clear the buffer */
-    memset(msg->buf, 0, msg->width * msg->height * sizeof (ULONG));
-    return;
-}
+struct driverInfo {
+    struct PrtInfo pi;
+    BOOL di_8BitGuns;
+    BOOL di_ConvertSource;
+    BOOL di_FloydDithering;
+    BOOL di_AntiAlias;
+    BOOL di_ColorCorrection;
+    BOOL di_NewColor;
+    BOOL di_NoScaling;
+    LONG di_ColorSize;
+    LONG di_NumRows;
+};
 
-MakeStaticHook(rastSourceHook, rastSource);
+typedef LONG (*renderFunc )(SIPTR ct, LONG x, LONG y, LONG status);
 
-LONG  Printer_Gfx_DumpRPort(const struct IODRPReq *io, struct TagItem *tags)
+#define RENDER(ct, x, y, status) ({ \
+    D(bug("\tRENDER(%d): ct=%p x=%d y=%d\n", status, (APTR)ct, x, y)); \
+    ((renderFunc)pi->pi_render)((SIPTR)(ct), (LONG)(x), (LONG)(y), (LONG)(status)); })
+
+LONG  Printer_Gfx_DumpRPort(struct IODRPReq *io, struct TagItem *tags)
 {
     struct PrinterData *pd = (struct PrinterData *)io->io_Device;
     struct PrinterExtendedData *ped = &pd->pd_SegmentData->ps_PED;
-    LONG (*Render)(SIPTR ct, LONG x, LONG y, LONG status) = ped->ped_Render;
-    struct Hook *srcHook = &rastSourceHook;
+    struct PrinterGfxPrefs *gfx = &pd->pd_PUnit->pu_Prefs.pp_Gfx;
+    struct Hook *srcHook = NULL;
     struct TagItem *tag;
-    LONG aspectX = 1, aspectY = 1;
+    struct BitMap *bm;
+    LONG aspectXsrc = 1, aspectYsrc = 1;
+    LONG aspectXdst = 1, aspectYdst = 1;
+    LONG scaleXsrc, scaleXdst;
+    LONG scaleYsrc, scaleYdst;
+    LONG prnMarginLeft, prnMarginRight;
+    LONG prnMarginTop, prnMarginBottom;
+    LONG prnX, prnY;
+    LONG prnW, prnH;
     LONG err;
+    struct driverInfo di = {
+        .di_ColorSize = sizeof(union colorEntry),
+    };
+    struct PrtInfo *pi = &di.pi;
+    UBYTE dmatrix[4][4] = {
+        {  1,  9,  3, 11, },
+        { 13,  5, 15,  7, },
+        {  4, 12,  2, 10, },
+        { 16,  8, 14,  6, }
+    };
 
-    if (!(ped->ped_PrinterClass & 1) || Render == NULL) {
+    D(bug("%s: io=%p, tags=%p\n", __func__, io, tags));
+
+    pi->pi_render = (APTR)ped->ped_Render;
+    if (!(ped->ped_PrinterClass & 1) || pi->pi_render == NULL) {
         /* Not graphics. */
+        D(bug("\tNot a graphics printer (class = 0x%x)\n", ped->ped_PrinterClass));
         return PDERR_NOTGRAPHICS;
     }
 
+    /* Set up density and printer dimensions */
+    err = RENDER(io, io->io_Special, 0, PRS_PREINIT);
+    if (err < 0)
+        return err;
+
     while ((tag = LibNextTagItem(&tags))) {
         switch (tag->ti_Tag) {
-            case DRPA_SourceHook:
-                srcHook = (struct Hook *)tag->ti_Data;
+        case DRPA_SourceHook:
+            srcHook = (struct Hook *)tag->ti_Data;
+            break;
+        case DRPA_AspectX:
+            aspectXsrc = tag->ti_Data;
+            break;
+        case DRPA_AspectY:
+            aspectYsrc = tag->ti_Data;
+            break;
+        default:
+            break;
+        }
+    }
+        
+    if ((pd->pd_SegmentData->ps_Version >= 44) &&
+        (ped->ped_PrinterClass & PPCF_EXTENDED)) {
+        tags = ped->ped_TagList;
+        while ((tag = LibNextTagItem(&tags))) {
+            switch (tag->ti_Tag) {
+            case PRTA_8BitGuns:
+                di.di_8BitGuns = (BOOL)tag->ti_Data;
                 break;
-            case DRPA_AspectX:
-                aspectX = tag->ti_Data;
+            case PRTA_ConvertSource: 
+                di.di_ConvertSource = (BOOL)tag->ti_Data;
                 break;
-            case DRPA_AspectY:
-                aspectY = tag->ti_Data;
+            case PRTA_FloydDithering:
+                di.di_FloydDithering = (BOOL)tag->ti_Data;
+                break;
+            case PRTA_AntiAlias:
+                di.di_AntiAlias = (BOOL)tag->ti_Data;
+                break;
+            case PRTA_ColorCorrection:
+                di.di_ColorCorrection = (BOOL)tag->ti_Data;
+                break;
+            case PRTA_NoIO:
+                /* Handled in di.di_.c */
+                break;
+            case PRTA_NewColor:
+                di.di_NewColor = (BOOL)tag->ti_Data;
+                break;
+            case PRTA_ColorSize:
+                di.di_ColorSize = (ULONG)tag->ti_Data;
+                break;
+            case PRTA_NoScaling:
+                di.di_NoScaling = (BOOL)tag->ti_Data;
+                break;
+            case PRTA_DitherNames:
+            case PRTA_ShadingNames:
+            case PRTA_ColorCorrect:
+            case PRTA_DensityInfo:
+                /* Handled in :Prefs/Printer */
+                break;
+            case PRTA_LeftBorder:
+            case PRTA_TopBorder:
+            case PRTA_MixBWColor:
+            case PRTA_Preferences:
+                /* Advice for applications */
                 break;
             default:
                 break;
+            }
         }
     }
 
-    (void)aspectX;
-    (void)aspectY;
-    (void)srcHook;
-
-    /* Initialize page for printing */
-    if (!(err = Render((SIPTR)io, io->io_SrcWidth, io->io_SrcHeight, PRS_INIT))) {
-        err = Render(err, io->io_Special, 0, PRS_CLOSE);
+    if (di.di_NewColor) {
+        di.di_ConvertSource = TRUE;
+        di.di_FloydDithering = TRUE;
+        di.di_AntiAlias = TRUE;
+        di.di_ColorCorrection = TRUE;
     }
 
-    return 0;
+    if (di.di_NoScaling) {
+        di.di_FloydDithering = TRUE;
+        di.di_AntiAlias = TRUE;
+    }
+
+    if (di.di_ColorSize < 3) {
+        D(bug("\tPRTA_ColorSize was %d - illegal!\n", di.di_ColorSize));
+        return PDERR_BADDIMENSION;
+    }
+
+    prnMarginLeft = gfx->pg_PrintXOffset;
+    prnMarginRight = 0;
+    prnMarginTop = gfx->pg_PrintYOffset;
+    prnMarginBottom = 0;
+
+    prnX = 0;
+    prnY = 0;
+    prnW = io->io_DestCols;
+    prnH = io->io_DestRows;
+
+    di.di_NumRows = ped->ped_NumRows; /* Rows/Stripe */
+
+    if (io->io_Special == 0) {
+        /* From the OS 3.9 autodocs for printer.device... */
+        if (prnW == 0 && prnH > 0) {
+            prnW = (ped->ped_MaxXDots - (prnMarginLeft + prnMarginRight));
+        } else if (prnW == 0 && prnH == 0) {
+            prnW = (ped->ped_MaxXDots - (prnMarginLeft + prnMarginRight));
+            prnH = prnW * aspectYsrc / aspectXsrc;
+        } else if (prnW > 0 && prnH == 0) {
+            prnH = prnW * aspectYsrc / aspectXsrc;
+        } else if (prnW < 0 && prnH > 0) {
+            prnW = io->io_SrcWidth * (-prnW) / prnH;
+            prnH = prnW * aspectYsrc / aspectXsrc;
+        }
+    }
+
+    if (io->io_Special & SPECIAL_MILCOLS) {
+        prnW = io->io_DestCols * ped->ped_XDotsInch / 1000;
+    }
+    if (io->io_Special & SPECIAL_MILROWS) {
+        prnH = io->io_DestRows * ped->ped_YDotsInch / 1000;
+    }
+    /* The following math relies on the fact that at
+     * ped_Max?Dots is limited to 65535 pixels:
+     * At even 1200dpi, 65535 pixels is over 100 inches!
+     */
+    if (io->io_Special & SPECIAL_FRACCOLS) {
+        prnW = (ped->ped_MaxXDots * (io->io_DestCols >> 16)) >> 16;
+    }
+    if (io->io_Special & SPECIAL_FRACROWS) {
+        prnH = (ped->ped_MaxYDots * (io->io_DestRows >> 16)) >> 16;
+    }
+    /* Full page width (minus the margins) */
+    if (io->io_Special & SPECIAL_FULLCOLS) {
+        prnW = ped->ped_MaxXDots - (prnMarginLeft + prnMarginRight);
+    }
+    if (io->io_Special & SPECIAL_FULLROWS) {
+        prnH = ped->ped_MaxYDots - (prnMarginTop + prnMarginBottom);
+    }
+    if (io->io_Special & SPECIAL_ASPECT) {
+        prnH = prnH * aspectYdst / aspectXdst;
+    }
+
+    /* Centering */
+    if (io->io_Special & SPECIAL_CENTER) {
+        prnX = prnMarginLeft + (ped->ped_MaxXDots - (prnMarginLeft + prnMarginRight ) - prnW) / 2;
+        prnY = prnMarginLeft + (ped->ped_MaxYDots - (prnMarginTop  + prnMarginBottom) - prnH) / 2;
+    }
+
+    /* Scaling calculations. */
+    scaleXsrc = io->io_SrcWidth;
+    scaleYsrc = io->io_SrcHeight;
+    scaleXdst = prnW;
+    scaleYdst = prnH;
+
+    while (scaleXsrc > 168383 || scaleXdst > 168383) {
+        scaleXsrc >>= 1;
+        scaleXdst >>= 1;
+        if (scaleXsrc == 0 || scaleXdst == 0) {
+            D(bug("\tCan't scale X from %dx%d to %dx%d\n", io->io_SrcWidth, io->io_SrcHeight, prnW, prnH));
+            return PDERR_BADDIMENSION;
+        }
+    }
+
+    while (scaleYsrc > 168383 || scaleYdst > 168383) {
+        scaleYsrc >>= 1;
+        scaleYdst >>= 1;
+        if (scaleYsrc == 0 || scaleYdst == 0) {
+            D(bug("\tCan't scale Y from %dx%d to %dx%d\n", io->io_SrcWidth, io->io_SrcHeight, prnW, prnH));
+            return PDERR_BADDIMENSION;
+        }
+    }
+
+
+    prnW = ScalerDiv(io->io_SrcWidth, scaleXsrc, scaleXdst);
+    prnH = ScalerDiv(io->io_SrcHeight, scaleYsrc, scaleYdst);
+
+    D(bug("\tScaling %dx%d => %dx%d\n", io->io_SrcWidth, io->io_SrcHeight, prnW, prnH));
+
+    io->io_DestCols = prnW;
+    io->io_DestRows = prnH;
+
+
+    /* If nothing to print, we're done! */
+    if (io->io_Special & SPECIAL_NOPRINT) {
+        D(bug("\tOh, SPECIAL_NOPRINT. Done.\n"));
+        return 0;
+    }
+
+    /* Set up the PrtInfo structure */
+    pi->pi_rp = io->io_RastPort;
+    pi->pi_ScaleX = NULL; /* New di.di_s should *not* be using this */
+    pi->pi_dmatrix = (APTR)dmatrix;
+    pi->pi_width = prnW;
+    pi->pi_height = prnH;
+    pi->pi_xpos = prnX;
+    pi->pi_threshold = pd->pd_Preferences.PrintThreshold;
+    pi->pi_special = io->io_Special;
+    pi->pi_SourceHook = srcHook;
+
+   /* Initialize page for printing */
+    if (0 == (err = RENDER(io, prnW, prnH, PRS_INIT))) {
+        struct RastPort rp;
+        APTR pdata;
+        struct BitMap *src_bm = NULL;
+
+        if (di.di_NoScaling) {
+            prnW = io->io_SrcWidth;
+            prnH = io->io_SrcHeight;
+            scaleXsrc = scaleXdst = 1;
+            scaleYsrc = scaleYdst = 1;
+        }
+
+        /* Allocate a row for 24-bit RGB color information */
+        if (srcHook) {
+            APTR pdata;
+            struct RastPort src_rp;
+            InitRastPort(&src_rp);
+
+            /* In case we fail.. */
+            err = PDERR_INTERNALMEMORY;
+            if ((pdata = AllocMem(io->io_SrcWidth * 4, MEMF_PUBLIC))) {
+                if ((src_bm = AllocBitMap(io->io_SrcWidth, io->io_SrcHeight, 24, BMF_SPECIALFMT | SHIFT_PIXFMT(PIXFMT_RGB24) , io->io_RastPort->BitMap))) {
+                    struct DRPSourceMsg msg;
+                    LONG row;
+
+                    rp.BitMap = src_bm;
+                    
+                    msg.x = io->io_SrcX;
+                    msg.y = io->io_SrcY;
+                    msg.width = io->io_SrcWidth;
+                    msg.buf = (APTR)pdata;
+
+                    for (row = 0; row < io->io_SrcHeight; row++, msg.y++) {
+                        /* Collect the next source row */
+                        msg.height = 1;
+                        CallHookA(srcHook, io, &msg);
+                        /* Transfer to source bitmap */
+                        WritePixelArray(pdata, 0, 0, io->io_SrcWidth * 4, &src_rp, 0, row, io->io_SrcWidth, 1, RECTFMT_0RGB32);
+                    }
+                } else {
+                    D(bug("\tCan't allocate bitmap to hold srcHook data (%d x %d)\n", io->io_SrcWidth, io->io_SrcHeight));
+                }
+                FreeMem(pdata, io->io_SrcWidth * 4);
+            } else {
+                D(bug("\tCan't allocate a %dx4 row to hold srcHook data\n", io->io_SrcWidth));
+            }
+        } else {
+            src_bm = io->io_RastPort->BitMap;
+        }
+
+        if (src_bm) {
+            struct RastPort rp;
+            InitRastPort(&rp);
+            if ((pdata = AllocMem(prnW * di.di_ColorSize, MEMF_PUBLIC))) {
+                pi->pi_ColorInt = pdata;
+                pi->pi_ColorIntSize = prnW * di.di_ColorSize;
+
+                if ((bm = AllocBitMap(prnW, prnH, -1, 0, io->io_RastPort->BitMap))) {
+                    /* Render it ourselves */
+                    struct BitScaleArgs bsa = {
+                        .bsa_SrcBitMap = io->io_RastPort->BitMap,
+                        .bsa_SrcX = io->io_SrcX,
+                        .bsa_SrcY = io->io_SrcY,
+                        .bsa_SrcWidth = io->io_SrcWidth,
+                        .bsa_SrcHeight = io->io_SrcHeight,
+                        .bsa_XSrcFactor = scaleXsrc,
+                        .bsa_XDestFactor = scaleXdst,
+                        .bsa_YSrcFactor = scaleYsrc,
+                        .bsa_YDestFactor = scaleYdst,
+                        .bsa_DestBitMap = bm,
+                    };
+                    LONG row;
+                    LONG rleft = prnH;
+
+                    BitMapScale(&bsa);
+                    rp.BitMap = bm;
+
+                    /* If we make a temporary source bitmap, we no longer need it.
+                     */
+                    if (srcHook && src_bm) {
+                        FreeBitMap(src_bm);
+                    }
+
+                    for (row = 0; row < prnH; rleft -= di.di_NumRows) {
+                        LONG rows = (rleft > di.di_NumRows) ? di.di_NumRows : rleft;
+                        int i;
+                        for (i =0; i < rows; i++, row++) {
+                            ReadPixelArray(pdata, 0, 0, prnW * di.di_ColorSize,
+                                           &rp, 0, row, prnW, 1,
+                                           di.di_ColorSize == 3 ?
+                                             RECTFMT_BGR24 :
+                                             RECTFMT_BGR032);
+                            if (di.di_ConvertSource)
+                                RENDER(pdata, prnW, 1, PRS_CONVERT);
+                            if (di.di_ColorCorrection)
+                                RENDER(pdata, prnW, 1, PRS_CORRECT);
+                            RENDER(pi, 0, prnY + row, PRS_TRANSFER);
+                        }
+                        RENDER(0, 0, rows, PRS_FLUSH);
+                    }
+                    FreeBitMap(bm);
+                } else {
+                    D(bug("\tCan't allocate a %dx%d bitmap for the scaled data\n", prnW, prnH));
+                }
+                FreeMem(pdata, prnW * di.di_ColorSize);
+            } else {
+                D(bug("\tCan't allocate a %d x %d byte transfer row\n", prnW, di.di_ColorSize));
+            }
+        } else {
+            D(bug("\tCan't find nor synthesize a source bitmap\n"));
+        }
+
+        RENDER(err, io->io_Special, 0, PRS_CLOSE);
+    }
+
+    return err;
 }
 
 
