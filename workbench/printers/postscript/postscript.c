40aa64d3230e15f22a6fb5dad41b2864d91b1863
ezrec <jason.mcmullan@gmail.com>
2012-02-17 22:10:01 +0000

    Printers/Postscript: Support text colors, patch, line spacing, bold, italics, and underline codes
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@44083 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/printers/postscript/postscript.c
+++ workbench/printers/postscript/postscript.c
@@ -46,61 +46,61 @@ static LONG ps_DoSpecial(UWORD *command, UBYTE output_buffer[],
 static CONST_STRPTR PED_Commands[] = {
     "\377",                             /*  0 aRIS   (reset) */
     "\377\377",                         /*  1 aRIN   (initialize) */
-    "\377",                             /*  2 aIND   (linefeed) */
-    "\377",                             /*  3 aNEL   (CR/LF) */
+    ") aSHOW aIND (",                   /*  2 aIND   (linefeed) */
+    ") aSHOW aNEL (",                   /*  3 aNEL   (CR/LF) */
     "\377",                             /*  4 aRI    (reverse LF) */
-    "\377",                             /*  5 aSGR0  (Courier) */
-    "\377",                             /*  6 aSGR3  (italics) */
-    "\377",                             /*  7 aSGR23 (no italics) */
-    "\377",                             /*  8 aSGR4  (underline) */
-    "\377",                             /*  9 aSGR24 (no underline) */
-    "\377",                             /* 10 aSGR1  (boldface) */
-    "\377",                             /* 11 aSGR21 (no boldface) */
+    ") aSHOW aSGR0 (",                  /*  5 aSGR0  (Courier) */
+    ") aSHOW aSGR3 (",                  /*  6 aSGR3  (italics) */
+    ") aSHOW aSGR23 (",                 /*  7 aSGR23 (no italics) */
+    ") aSHOW aSGR4 (",                  /*  8 aSGR4  (underline) */
+    ") aSHOW aSGR24 (",                 /*  9 aSGR24 (no underline) */
+    ") aSHOW aSGR1 (",                  /* 10 aSGR1  (boldface) */
+    ") aSHOW aSGR22 (",                 /* 11 aSGR22 (no boldface) */
     "\377",                             /* 12 aSFC   (set text color) */
     "\377",                             /* 13 aSBC   (set background color) */
-    "\377",                             /* 14 aSHORP0 (normal pitch) */
-    "\377",                             /* 15 aSHORP2 (elite) */
-    "\377",                             /* 16 aSHORP1 (no elite) */
-    "\377",                             /* 17 aSHORP4 (condensed) */
-    "\377",                             /* 18 aSHORP3 (no condensed) */
-    "\377",                             /* 19 aSHORP6 (enlarge) */
-    "\377",                             /* 20 aSHORT5 (no enlarge) */
-    "\377",                             /* 21 aDEN6   (shadow) */ 
-    "\377",                             /* 22 aDEN5   (no shadow) */
-    "\377",                             /* 23 aDEN4   (double strike) */
-    "\377",                             /* 24 aDEN3   (no double strike) */
-    "\377",                             /* 25 aDEN2   (NLQ) */
-    "\377",                             /* 26 aDEN1   (no NLQ) */
-    "\377",                             /* 27 aSUS2   (superscript) */
-    "\377",                             /* 28 aSUS1   (no superscript) */
-    "\377",                             /* 29 aSUS4   (subscript) */
-    "\377",                             /* 30 aSUS3   (no subscript) */
-    "\377",                             /* 31 aSUS0   (normal) */
-    "\377",                             /* 32 aPLU    (partial line up) */
-    "\377",                             /* 33 aPLD    (partial line down) */
-    "\377",                             /* 34 aFNT0   (Courier) */
-    "\377",                             /* 35 aFNT1   (Helvetica) */
-    "\377",                             /* 36 aFNT2   (Font 2) */
-    "\377",                             /* 37 aFNT3   (Font 3) */
-    "\377",                             /* 38 aFNT4   (Font 4) */
-    "\377",                             /* 39 aFNT5   (Font 5) */
-    "\377",                             /* 40 aFNT6   (Font 6) */
-    "\377",                             /* 41 aFNT7   (Font 7) */
-    "\377",                             /* 42 aFNT8   (Font 8) */
-    "\377",                             /* 43 aFNT9   (Font 9) */
-    "\377",                             /* 44 aFNT10  (Font 10) */
-    "\377",                             /* 45 aPROP2  (proportional) */
-    "\377",                             /* 46 aPROP1  (no proportional) */
-    "\377",                             /* 47 aPROP0  (default proportion) */
+    ") aSHOW aSHORP0 (",                /* 14 aSHORP0 (normal pitch) */
+    ") aSHOW aSHORP2 (",                /* 15 aSHORP2 (elite) */
+    ") aSHOW aSHORP1 (",                /* 16 aSHORP1 (no elite) */
+    ") aSHOW aSHORP4 (",                /* 17 aSHORP4 (condensed) */
+    ") aSHOW aSHORP3 (",                /* 18 aSHORP3 (no condensed) */
+    ") aSHOW aSHORP6 (",                /* 19 aSHORP6 (enlarge) */
+    ") aSHOW aSHORP5 (",                /* 20 aSHORT5 (no enlarge) */
+    ") aSHOW aDEN6 (",                  /* 21 aDEN6   (shadow) */ 
+    ") aSHOW aDEN5 (",                  /* 22 aDEN5   (no shadow) */
+    ") aSHOW aDEN4 (",                  /* 23 aDEN4   (double strike) */
+    ") aSHOW aDEN3 (",                  /* 24 aDEN3   (no double strike) */
+    ") aSHOW aDEN2 (",                  /* 25 aDEN2   (NLQ) */
+    ") aSHOW aDEN1 (",                  /* 26 aDEN1   (no NLQ) */
+    ") aSHOW aSUS2 (",                  /* 27 aSUS2   (superscript) */
+    ") aSHOW aSUS1 (",                  /* 28 aSUS1   (no superscript) */
+    ") aSHOW aSUS4 (",                  /* 29 aSUS4   (subscript) */
+    ") aSHOW aSUS3 (",                  /* 30 aSUS3   (no subscript) */
+    ") aSHOW aSUS0 (",                  /* 31 aSUS0   (normal) */
+    ") aSHOW aPLU (",                   /* 32 aPLU    (partial line up) */
+    ") aSHOW aPLD (",                   /* 33 aPLD    (partial line down) */
+    ") aSHOW aFNT0 (",                  /* 34 aFNT0   (Courier) */
+    ") aSHOW aFNT1 (",                  /* 35 aFNT1   (Helvetica) */
+    ") aSHOW aFNT2 (",                  /* 36 aFNT2   (Font 2) */
+    ") aSHOW aFNT3 (",                  /* 37 aFNT3   (Font 3) */
+    ") aSHOW aFNT4 (",                  /* 38 aFNT4   (Font 4) */
+    ") aSHOW aFNT5 (",                  /* 39 aFNT5   (Font 5) */
+    ") aSHOW aFNT6 (",                  /* 40 aFNT6   (Font 6) */
+    ") aSHOW aFNT7 (",                  /* 41 aFNT7   (Font 7) */
+    ") aSHOW aFNT8 (",                  /* 42 aFNT8   (Font 8) */
+    ") aSHOW aFNT9 (",                  /* 43 aFNT9   (Font 9) */
+    ") aSHOW aFNT10 (",                 /* 44 aFNT10  (Font 10) */
+    ") aSHOW aPROP2 (",                 /* 45 aPROP2  (proportional) */
+    ") aSHOW aPROP1 (",                 /* 46 aPROP1  (no proportional) */
+    ") aSHOW aPROP0 (",                 /* 47 aPROP0  (default proportion) */
     "\377",                             /* 48 aTSS    (set proportional offset) */
-    "\377",                             /* 49 aJFY5   (left justify) */
-    "\377",                             /* 50 aJFY7   (right justify) */
-    "\377",                             /* 51 aJFY6   (full justify) */
-    "\377",                             /* 52 aJFY0   (no justify) */
-    "\377",                             /* 53 aJFY3   (letter space) */
-    "\377",                             /* 54 aJFY1   (word fill) */
-    "\377",                             /* 55 aVERP0  (1/8" line spacing) */
-    "\377",                             /* 56 aVERP1  (1/6" line spacing) */
+    ") aSHOW aJFY5 (",                  /* 49 aJFY5   (left justify) */
+    ") aSHOW aJFY7 (",                  /* 50 aJFY7   (right justify) */
+    ") aSHOW aJFY6 (",                  /* 51 aJFY6   (full justify) */
+    ") aSHOW aJFY0 (",                  /* 52 aJFY0   (no justify) */
+    ") aSHOW aJFY3 (",                  /* 53 aJFY3   (letter space) */
+    ") aSHOW aJFY1 (",                  /* 54 aJFY1   (word fill) */
+    ") aSHOW aVERP0 (",                 /* 55 aVERP0  (1/8" line spacing) */
+    ") aSHOW aVERP1 (",                 /* 56 aVERP1  (1/6" line spacing) */
     "\377",                             /* 57 aSLPP   (form length) */
     "\377",                             /* 58 aPERF   (skip n perfs) */
     "\377",                             /* 59 aPERF0  (no skip perfs) */
@@ -352,7 +352,7 @@ CONST TEXT ps_PageHeader[] =
 
 static void ps_VSendPage(char *buff, BOOL formfeed)
 {
-    ps_VWrite(buff, ps_PageHeader, formfeed ? ") show aFF" : "",
+    ps_VWrite(buff, ps_PageHeader, formfeed ? ") aSHOW aFF" : "",
         ps_PageID, ps_PageID,
         /* Aspect */
         PD->pd_Preferences.PrintAspect ? "Landscape" : "Portrait",
@@ -369,7 +369,7 @@ static void ps_VSendPage(char *buff, BOOL formfeed)
 
 static void ps_SendPage(BOOL formfeed)
 {
-    ps_PWrite(ps_PageHeader, formfeed ? ") show aFF" : "",
+    ps_PWrite(ps_PageHeader, formfeed ? ") aSHOW aFF" : "",
         ps_PageID, ps_PageID,
         /* Aspect */
         PD->pd_Preferences.PrintAspect ? "Landscape" : "Portrait",
@@ -399,8 +399,6 @@ static void ps_SendHeader(void)
         "/Page_Top %ld 72 mul %ld div 72 4 div sub def\n"
         /* 12 * left-margin-chars */
         "/Page_Left 12 %ld mul def\n"
-        "/aNEL { currentpoint exch pop Page_Left exch Spacing sub dup 0 lt { pop pop aFF aFB } { moveto } ifelse } bind def\n"
-        "/aIND { currentpoint exch pop Page_Left exch moveto } bind def\n"
         "/aFB { gsave Font_CPI setFont Page_Left Page_Top moveto } bind def\n"
         "/aFF  { grestore showpage } bind def\n"
         "/aImageBW {\n"
@@ -432,13 +430,96 @@ static void ps_SendHeader(void)
         "       false 4 colorimage %%\n"
         "} bind def\n"
         "\n"
-        "/setFont { 1200 exch div /Font_Height exch def Font_Name findfont Font_Height scalefont setfont } bind def\n"
+        "/setFont { Font_Name findfont 1200 Font_CPI div scalefont setfont } bind def\n"
         "/Spacing { 72 Spacing_LPI div } bind def\n"
-        "/Font_Name /Courier def\n"
+        "/Font_Name { \n"
+        "    Font_P {\n"
+        "        Font_I { \n"
+        "            Font_B { /Helvetica-BoldOblique } { /Courier-Oblique } ifelse\n"
+        "        } { \n"
+        "            Font_B { /Helvetica-Bold } { /Courier } ifelse\n"
+        "        } ifelse\n"
+        "    } {\n"
+        "        Font_I { \n"
+        "            Font_B { /Courier-BoldOblique } { /Courier-Oblique } ifelse\n"
+        "        } { \n"
+        "            Font_B { /Courier-Bold } { /Courier } ifelse\n"
+        "        } ifelse\n"
+        "    } ifelse\n"
+        " } bind def\n"
         "/Font_CPIDefault %ld def\n"
         "/Spacing_LPIDefault %ld def\n"
         "/Font_CPI Font_CPIDefault def\n"
+        "/Font_P false def\n"
+        "/Font_U false def\n"
+        "/Font_B false def\n"
+        "/Font_I false def\n"
+        "/Font_Bg [ 1 1 1 ] def\n"
+        "/Font_Fg [ 0 0 0 ] def\n"
         "/Spacing_LPI Spacing_LPIDefault def\n"
+        "/aSHOW { \n"
+        "     gsave Font_Bg aload pop setrgbcolor \n"
+        "           0 -2 rmoveto dup stringwidth pop dup 0 rlineto\n"
+        "           0 Spacing 2 sub rlineto neg 0 rlineto closepath fill\n"
+        "     grestore\n"
+        "     Font_U { gsave 0 -2 rmoveto dup stringwidth rlineto 0.3 setlinewidth stroke grestore } if\n"
+        "     Font_Fg aload pop setrgbcolor\n"
+        "     show } bind def\n"
+        "%% Commands\n"
+        "/aNEL { currentpoint exch pop Page_Left exch Spacing sub dup 0 lt { pop pop aFF aFB } { moveto } ifelse } bind def\n"
+        "/aIND { currentpoint exch pop Page_Left exch moveto } bind def\n"
+        "/aSGR0   { /Font_Fg [ 0 0 0 ] def /Font_Bg [ 1 1 1 ] def /Font_U false def /Font_B false def /Font_I false def Font_CPI setFont } bind def\n"
+        "/aSGR3   { /Font_I true def setFont } bind def\n"
+        "/aSGR23  { /Font_I false def setFont } bind def\n"
+        "/aSGR4   { /Font_U true def setFont } bind def\n"
+        "/aSGR24  { /Font_U false def setFont } bind def\n"
+        "/aSGR1   { /Font_B true def setFont } bind def\n"
+        "/aSGR22  { /Font_B false def setFont } bind def\n"
+        "/aColor  { [ [ 0 0 0 ] [ 1 0 0 ] [ 0 1 0] [ 1 1 0] [ 0 0 1] [ 0.3 0.3 0.3] [0 1 1] [0.7 0.7 0.7] [ 0 0 0 ] [ 0 0 0 ] ] exch get } bind def\n"
+        "/aSFC    { aColor /Font_Fg exch def } bind def\n"
+        "/aSBC    { aColor /Font_Bg exch def } bind def\n"
+        "/aSHORP0 { /Font_CPI Font_CPIDefault def setFont } bind def\n"
+        "/aSHORP1 { /Font_CPI Font_CPIDefault def setFont } bind def\n"
+        "/aSHORP2 { /Font_CPI 120 def setFont } bind def\n"
+        "/aSHORP3 { /Font_CPI Font_CPIDefault def setFont } bind def\n"
+        "/aSHORP4 { /Font_CPI 171 def setFont } bind def\n"
+        "/aSHORP5 { /Font_CPI Font_CPIDefault def setFont } bind def\n"
+        "/aSHORP6 { /Font_CPI 100 def setFont } bind def\n"
+        "/aDEN6   { } bind def\n"
+        "/aDEN5   { } bind def\n"
+        "/aDEN4   { } bind def\n"
+        "/aDEN3   { } bind def\n"
+        "/aDEN2   { } bind def\n"
+        "/aDEN1   { } bind def\n"
+        "/aSUS2   { } bind def\n"
+        "/aSUS1   { } bind def\n"
+        "/aSUS4   { } bind def\n"
+        "/aSUS3   { } bind def\n"
+        "/aSUS0   { } bind def\n"
+        "/aPLU    { } bind def\n"
+        "/aPLD    { } bind def\n"
+        "/aFNT0   { } bind def\n"
+        "/aFNT1   { } bind def\n"
+        "/aFNT2   { } bind def\n"
+        "/aFNT3   { } bind def\n"
+        "/aFNT4   { } bind def\n"
+        "/aFNT5   { } bind def\n"
+        "/aFNT6   { } bind def\n"
+        "/aFNT7   { } bind def\n"
+        "/aFNT8   { } bind def\n"
+        "/aFNT9   { } bind def\n"
+        "/aFNT10  { } bind def\n"
+        "/aPROP2  { /Font_P true def } bind def\n"
+        "/aPROP1  { /Font_P false def } bind def\n"
+        "/aPROP0  { /Font_P false def } bind def\n"
+        "/aJFY5   { } bind def\n"
+        "/aJFY7   { } bind def\n"
+        "/aJFY6   { } bind def\n"
+        "/aJFY0   { } bind def\n"
+        "/aJFY3   { } bind def\n"
+        "/aJFY1   { } bind def\n"
+        "/aVERP0  { /Spacing_LPI 8 def } bind def\n"
+        "/aVERP1  { /Spacing_LPI 6 def } bind def\n"
         "%%%%EndProlog\n"
         ;
 
@@ -478,7 +559,7 @@ static LONG ps_Open(union printerIO *ior)
 static VOID ps_Close(union printerIO *ior)
 {
     if (ps_HeaderSent) {
-        ps_PWrite(") show aFF\n"
+        ps_PWrite(") aSHOW aFF\n"
                   "%%%%Trailer\n"
                   "%%%%EOF\n");
     }
@@ -537,7 +618,7 @@ static LONG ps_RenderInit(struct IODRPReq *io, LONG width, LONG height)
     }
 
     /* Leave text printing mode */
-    ps_PWrite(") show grestore gsave\n");
+    ps_PWrite(") aSHOW grestore gsave\n");
 
     /* Move grapics location to (in dots):
      * x = alignOffsetX
@@ -549,9 +630,9 @@ static LONG ps_RenderInit(struct IODRPReq *io, LONG width, LONG height)
 
     /* Move text location to (in points):
      *  textx = PageLeft
-     *  texty = y * 72 / dpiY - Font_Height
+     *  texty = y * 72 / dpiY - Spacing
      */
-    ps_PWrite("Page_Left %ld 72 mul %ld div Font_Height sub moveto\n",
+    ps_PWrite("Page_Left %ld 72 mul %ld div Spacing sub moveto\n",
               y, PED->ped_YDotsInch);
 
     /* Save the graphics context - we don't want the scaling & translation
@@ -891,13 +972,12 @@ static LONG ps_DoSpecial(UWORD *command, UBYTE output_buffer[],
         PD->pd_PWaitEnabled = '\375';
         ret = -2;
         break;
-    case aIND:
-        strcpy(output_buffer, ") show aIND\n(");
+    case aSFC:
+        ps_VWrite(output_buffer, ") aSHOW %ld aSFC (\n", params[0] % 10);
         ret = strlen(output_buffer);
         break;
-    case aNEL:
-        (*current_line_position)++;
-        strcpy(output_buffer, ") show aNEL\n(");
+    case aSBC:
+        ps_VWrite(output_buffer, ") aSHOW %ld aSBC (\n", params[0] % 10);
         ret = strlen(output_buffer);
         break;
     default:
