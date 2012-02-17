c5e971f0545649ad239407af72ffaaa4c7c6ab38
ezrec <jason.mcmullan@gmail.com>
2012-02-17 04:46:33 +0000

    Tests/printer: Some trivial printer test, and example AOS 3.9 output
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@44075 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/printer/printerinfo.c
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2012, The AROS Development Team.  All rights reserved.
+ * Author: Jason S. McMullan <jason.mcmullan@gmail.com>
+ *
+ * Licensed under the AROS PUBLIC LICENSE (APL) Version 1.1
+ */
+
+#include <proto/dos.h>
+#include <proto/exec.h>
+#include <devices/prtbase.h>
+
+__startup int _main(void)
+{
+    struct {
+        CONST_STRPTR file;
+    } Args = { };
+    struct RDArgs *ra;
+    struct Library *DOSBase;
+
+    if (!(DOSBase = OpenLibrary("dos.library", 0)))
+        return RETURN_FAIL;
+
+    if (!(ra = ReadArgs("FILE", (IPTR *)&Args, NULL)))
+        return RETURN_FAIL;
+
+    if (Args.file) {
+        BPTR seg = LoadSeg(Args.file);
+        if (seg) {
+            struct PrinterSegment *pseg = BADDR(seg);
+            struct PrinterExtendedData *ped = &pseg->ps_PED;
+            int i;
+
+            Printf("%s:\n");
+            Printf("  ps_runAlert = 0x%08lx\n", pseg->ps_runAlert);
+            Printf("  ps_Version  = %ld\n", pseg->ps_Version );
+            Printf("  ps_Revision = %ld\n", pseg->ps_Revision);
+            Printf("  ped_PrinterName = \"%s\"\n", ped->ped_PrinterName);
+
+            Printf("  ped_DoSpecial= 0x%08lx\n", ped->ped_DoSpecial);
+            Printf("  ped_ConvFunc = 0x%08lx\n", ped->ped_ConvFunc);
+            Printf("  ped_Commands = 0x%08lx\n", ped->ped_Commands);
+            for (i = 0; i < 76; i++) {
+                if (ped->ped_Commands[i][0] == 255)
+                    continue;
+                Printf("  ped_Commands[%ld] = \"%s\"\n", i, ped->ped_Commands[i]);
+            }
+
+            UnLoadSeg(seg);
+        }
+    }
+
+    FreeArgs(ra);
+    CloseLibrary(DOSBase);
+
+    return RETURN_OK;
+}
+
