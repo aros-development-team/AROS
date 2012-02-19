8e38f3f31e423d9750c6af20e51f7e655f4e696a
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-02-19 22:15:27 +0000

    compiler/clib/include/siginfo_t.h: make struct for siginfo_t anonymous.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@44121 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/include/aros/types/sigaction_s.h
+++ compiler/clib/include/aros/types/sigaction_s.h
@@ -27,7 +27,7 @@ struct sigaction
 {
     union {
 	void		(*__sa_handler)(int);
-	void		(*__sa_sigaction)(int, struct __siginfo *, void *);
+	void		(*__sa_sigaction)(int, siginfo_t *, void *);
     } __sigaction_u;			/* signal handler */
     int			sa_flags;	/* see below */
     sigset_t		sa_mask;	/* signal mask to apply */
