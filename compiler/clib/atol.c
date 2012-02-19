2f9224c4ff5060fa6610c63e69bc3ae4c9f3d91e
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-02-19 22:15:08 +0000

    compiler/clib: call strtol() for implementing atoi() and atol()
    git-svn-id: https://svn.aros.org/svn/aros/trunk@44117 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/atol.c
+++ compiler/clib/atol.c
@@ -1,12 +1,9 @@
 /*
-    Copyright © 1995-2003, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2012, The AROS Development Team. All rights reserved.
     $Id$
 
     ANSI C function atol().
 */
-
-#include <limits.h>
-#include <ctype.h>
 
 /*****************************************************************************
 
@@ -50,68 +47,5 @@
 
 ******************************************************************************/
 {
-    unsigned long   val   = 0;
-    int             digit;
-    char            c = 0;
-    unsigned long   cutoff;
-    int             cutlim;
-    int		    any;
-
-    while (isspace (*str))
-        str ++;
-
-    if (*str)
-    {
-        if (*str == '+' || *str == '-')
-            c = *str ++; 
-
-        /*
-            Conversion loop, from FreeBSD's src/lib/libc/stdlib/strtoul.c
-
-            The previous AROS loop was
-            a) inefficient - it did a division each time around.
-            b) buggy - it returned the wrong value in endptr on overflow.
-        */
-        cutoff = (unsigned long)ULONG_MAX / (unsigned long)10;
-        cutlim = (unsigned long)ULONG_MAX % (unsigned long)10;
-        val = 0;
-        any = 0;
-
-        while (*str)
-        {
-            digit = *str;
-
-            if (!isdigit(digit))
-                break;
-
-            digit -= '0';
-
-            /*
-                any < 0 when we have overflowed. We still need to find the
-                end of the subject sequence
-            */
-            if (any < 0 || val > cutoff || (val == cutoff && digit > cutlim))
-            {
-                any = -1;
-            }
-            else
-            {
-                any = 1;
-                val = (val * 10) + digit;
-            }
-
-            str++;
-        }
-
-        /* Range overflow */
-        if (any < 0)
-        {
-            val = ULONG_MAX;
-        }
-
-        if (c == '-')
-            val = -val;
-    }
-
-    return val;
+    return strtol(str, (char **)NULL, 10);
 } /* atol */
