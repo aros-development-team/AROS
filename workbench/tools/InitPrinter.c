dea44477724e3f8e31d0e0fae814e9014ff8d99a
mattrust <mrustler@gmx.de>
2012-02-26 23:28:28 +0000

    InitPrinter, the world leading solution in printer initialization. Well, we had it on our status page: http://aros.sourceforge.net/introduction/status/tools.php
    git-svn-id: https://svn.aros.org/svn/aros/trunk@44278 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/tools/InitPrinter.c
@@ -0,0 +1,100 @@
+/*
+    Copyright © 2012, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <devices/printer.h>
+#include <dos/dos.h>
+
+#include <proto/dos.h>
+#include <proto/exec.h>
+
+#include <stdio.h>
+
+#define DEBUG 1
+#include <aros/debug.h>
+
+const char version[] = "$VER: InitPrinter 1.0 (27.02.2012) © AROS Dev Team";
+
+enum
+{
+    ARG_UNIT,
+    ARG_COUNT
+};
+
+static BOOL init_printer(ULONG unit)
+{
+    struct MsgPort *PrintMP;
+    struct IOPrtCmdReq *PrintIO;
+    BOOL success = FALSE;
+
+    if ((PrintMP = CreateMsgPort()))
+    {
+        if ((PrintIO = CreateIORequest(PrintMP, sizeof(struct IOPrtCmdReq))))
+        {
+            if (OpenDevice("printer.device", unit, (struct IORequest *)PrintIO, 0))
+            {
+                printf("Error: printer.device did not open\n");
+            }
+            else
+            {
+                D(bug("[InitPrinter] unit %d request %p msgport %p\n", unit, PrintIO, PrintMP));
+                PrintIO->io_PrtCommand = aRIN;     // Initialize
+                PrintIO->io_Parm0 = 0;
+                PrintIO->io_Parm1 = 0;
+                PrintIO->io_Parm2 = 0;
+                PrintIO->io_Parm3 = 0;
+                PrintIO->io_Command = PRD_PRTCOMMAND;
+
+                if (DoIO((struct IORequest *)PrintIO))
+                {
+                    printf("Printer reset failed. Error: %d\n", PrintIO->io_Error);
+                }
+                else
+                {
+                    success = TRUE;
+                }
+                CloseDevice((struct IORequest *)PrintIO);
+            }
+            DeleteIORequest(PrintIO);
+        }
+        else
+        {
+            printf("Error: Could not create I/O request\n");
+        }
+        DeleteMsgPort(PrintMP);
+    }
+    else
+    {
+        printf("Error: Could not create message port\n");
+    }
+    return success;
+}
+
+
+int main(void)
+{
+    struct RDArgs *rda;
+    IPTR args[ARG_COUNT] = {0};
+    ULONG unit = 0;
+    ULONG retval = RETURN_ERROR;
+
+    if ((rda = ReadArgs("UNIT/N", args, NULL)))
+    {
+        if (args[ARG_UNIT])
+        {
+            unit = *(LONG *)args[ARG_UNIT];
+        }
+        if (init_printer(unit))
+        {
+            retval = RETURN_OK;
+        }
+        FreeArgs(rda);
+    }
+    else
+    {
+        printf("Error: Could not read arguments\n");
+    }
+
+    return retval;
+}
