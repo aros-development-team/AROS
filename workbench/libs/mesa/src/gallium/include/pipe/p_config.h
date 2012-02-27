e620809a85f24a9412b423f799e0b7fcafdbe256
ezrec <jason.mcmullan@gmail.com>
2012-02-27 23:21:34 +0000

    mesa.library: Add '__powerpc__' to the list of PPC archs
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@44286 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/mesa/src/gallium/include/pipe/p_config.h
+++ workbench/libs/mesa/src/gallium/include/pipe/p_config.h
@@ -107,7 +107,7 @@
 #endif
 #endif
 
-#if defined(__ppc__) || defined(__ppc64__) || defined(__PPC__)
+#if defined(__ppc__) || defined(__ppc64__) || defined(__PPC__) || defined(__powerpc__)
 #define PIPE_ARCH_PPC
 #if defined(__ppc64__) || defined(__PPC64__)
 #define PIPE_ARCH_PPC_64
