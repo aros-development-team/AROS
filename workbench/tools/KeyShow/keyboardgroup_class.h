6b9fd8cfea0654efaf71444ce97bce34dfbf45fc
mattrust <mrustler@gmx.de>
2012-03-04 13:37:55 +0000

    Added attribute for switching between AMIGA and PC105 keyboard layouts. Added tooltype for keyboard type. Parse highmap, too, because some high keys behave like normal keys. Don't handle caps lock like a qualifier key.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@44334 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/tools/KeyShow/keyboardgroup_class.h
+++ workbench/tools/KeyShow/keyboardgroup_class.h
@@ -13,6 +13,11 @@
 #define MUIB_KeyboardGroup              (TAG_USER | 0x10000000)
 
 /*** Attributes *************************************************************/
+#define MUIA_KeyboardGroup_Type         (MUIB_KeyboardGroup | 0x00000000) /* I--  ULONG */
+
+/*** Values *****************************************************************/
+#define MUIV_KeyboardGroup_Type_Amiga   (0ul)
+#define MUIV_KeyboardGroup_Type_PC105   (1ul)
 
 /*** Variables **************************************************************/
 extern struct MUI_CustomClass *KeyboardGroup_CLASS;
