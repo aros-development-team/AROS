3a414d156e677464abde0c358b8d2e5a1897c231
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-04-06 20:59:00 +0000

    compiler/clib/include: Provide our own stddef.h
    No gcc provisions are made anymore. It is assumed that compiler will override these files if needed.
    Patch gcc to do avoid incompatibilities.
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@44543 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/include/aros/types/null.h
+++ compiler/clib/include/aros/types/null.h
@@ -2,21 +2,18 @@
 #define _AROS_TYPES_NULL_H
 
 /*
-    Copyright © 2010-2011, The AROS Development Team. All rights reserved.
+    Copyright © 2010-2012, The AROS Development Team. All rights reserved.
     $Id: /aros/branches/ABI_V1/trunk-aroscsplit/AROS/compiler/arosstdc/include/aros/types/null.h 35477 2010-11-07T22:33:25.524511Z verhaegs  $
 
     NULL
 */
 
-#ifndef __cplusplus
-
-#ifndef __GNUC__
-#define NULL ((void *)0)
-#else
-#define __need_NULL
-#include <stddef.h>
+#ifdef NULL
+#undef NULL
 #endif
 
+#ifndef __cplusplus
+#define NULL ((void *)0)
 #else
 #define NULL 0
 #endif
--- compiler/clib/include/aros/types/wchar_t.h
+++ compiler/clib/include/aros/types/wchar_t.h
@@ -2,18 +2,13 @@
 #define _AROS_TYPES_WCHAR_T_H
 
 /*
-    Copyright © 2010-2011, The AROS Development Team. All rights reserved.
+    Copyright © 2010-2012, The AROS Development Team. All rights reserved.
     $Id: /aros/branches/ABI_V1/trunk-aroscsplit/AROS/compiler/arosstdc/include/aros/types/wchar_t.h 36769 2011-01-11T21:35:35.950252Z verhaegs  $
 */
 
 #ifndef __cplusplus
 
-#ifndef __GNUC__
 typedef char wchar_t;
-#else
-#define __need_whar_t
-#include <stddef.h>
-#endif
 
 #endif
 
