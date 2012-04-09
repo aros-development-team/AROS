8e0c7cf0e71c2fa18e372bd6e03636f5a43b4a2d
Kalamatee <kalamatee@gmail.com>
2012-04-09 16:10:29 +0000

    bump version
    git-svn-id: https://svn.aros.org/svn/aros/trunk@44590 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/network/stacks/AROSTCP/bsdsocket/version.h
+++ workbench/network/stacks/AROSTCP/bsdsocket/version.h
@@ -27,15 +27,21 @@
 #define MIAMILIBNAME    "miami.library"
 
 #define VERSION         4
-#define REVISION        53
-#define DATE    "25.03.2007"
-#define VERS    SOCLIBNAME "4.52"
+#define REVISION        55
+#define DATE    "09.04.2012"
+#define VERS    SOCLIBNAME "4.55"
 #define VSTRING SOCLIBNAME STR(VERSION) "." STR(REVISION) "(" DATE ")"
-#define VERSTAG "\0$VER:" SOCLIBNAME "4.53 (" DATE ")"
+#define VERSTAG "\0$VER:" SOCLIBNAME "4.55 (" DATE ")"
 
 #define MIAMI_VERSION 13
 #define MIAMI_REVISION 5
 #define MIAMI_VSTRING  MIAMILIBNAME STR(MIAMI_VERSION) "." STR(MIAMI_REVISION) "(" DATE ")"
 
-#define STACK_RELEASE "AROSTCP kernel v0.22 " CPU_TYPE " (" DATE ")"
+#if defined(__CONFIG_ROADSHOW__)
+#define ROADSHOWSTR "Roadshow "
+#else
+#define ROADSHOWSTR ""
+#endif
+
+#define STACK_RELEASE "AROSTCP " ROADSHOWSTR "kernel v0.23 " CPU_TYPE " (" DATE ")"
 
