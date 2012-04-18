67c884bca89b84aafbbeab9dbcf328a334e493e1
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-04-18 19:32:03 +0000

    termios.h: Put some non-standard fields in a private struct.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@44645 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/cfgetispeed.c
+++ compiler/clib/cfgetispeed.c
@@ -1,10 +1,13 @@
-#include <termios.h>
+#include "__arosc_termios.h"
 
 /* FIXME: Add autodocs */
 speed_t cfgetispeed(const struct termios *__termios_p)
 {
     if (__termios_p)
-        return __termios_p->c_ispeed;
+    {
+        struct termios_intern *t = (struct termios_intern *)__termios_p;
+        return t->c_ispeed;
+    }
 
     /* According to man pages and online standard documents providing an
      * invalid pointer does not result in an error. Tests on Linux resulted
