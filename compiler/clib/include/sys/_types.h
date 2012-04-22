892e08260f73cd748e43fa5a398f07c2de784cfc
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-04-22 13:12:03 +0000

    compiler/clib/include: removed unused & unneeded files.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@44673 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/clib/include/sys/_types.h
+++
@@ -1,6 +0,0 @@
-/*
- * This file is present only for backwards compatibility with previously released
- * AROS gcc versions which require it.
- * It is referenced by <prefix>/lib/gcc/<cpu>-aros/<version>/include/stddef.h.
- * Future gcc releases will not depend on this file any more.
- */
