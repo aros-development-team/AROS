bb5a0ec47a39e63a5c3abfab510d0dd47179e3c3
mattrust <mrustler@gmx.de>
2012-05-05 15:52:32 +0000

    Some unused things removed.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@44750 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/tools/commodities/startmenu/support.h
+++
@@ -1,6 +0,0 @@
-/*
-    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
-    $Id$
-*/
-
-void RenderFrame(struct RastPort *rp, struct DrawInfo *dri, WORD x1, WORD y1, WORD x2, WORD y2, BOOL recessed);
