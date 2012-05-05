3048397327349a9d73bc765210829984de8281a5
olivieradam <reziztanzia@free.fr>
2012-05-05 00:35:22 +0000

    Custom classes: add ZUNE_CUSTOMCLASS_21 Listview class: Respect MUIA_CycleChain
    git-svn-id: https://svn.aros.org/svn/aros/trunk@44740 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/customclasses.h
+++ workbench/libs/muimaster/customclasses.h
@@ -366,6 +366,53 @@ ADD2EXIT(name ## _Deinitialize, 100);                                 \
     __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m20, m20, m20_msg_type); \
     __ZUNE_CUSTOMCLASS_END(name, base, parent_name, parent_class)    \
 
+#define ZUNE_CUSTOMCLASS_21(name, base, parent_name, parent_class,   \
+                           m1, m1_msg_type,                          \
+                           m2, m2_msg_type,                          \
+                           m3, m3_msg_type,                          \
+                           m4, m4_msg_type,                          \
+                           m5, m5_msg_type,                          \
+                           m6, m6_msg_type,                          \
+                           m7, m7_msg_type,                          \
+                           m8, m8_msg_type,                          \
+                           m9, m9_msg_type,                          \
+                           m10, m10_msg_type,                        \
+                           m11, m11_msg_type,                        \
+                           m12, m12_msg_type,                        \
+                           m13, m13_msg_type,                        \
+                           m14, m14_msg_type,                        \
+                           m15, m15_msg_type,                        \
+                           m16, m16_msg_type,                        \
+                           m17, m17_msg_type,                        \
+                           m18, m18_msg_type,                        \
+                           m19, m19_msg_type,                        \
+                           m20, m20_msg_type,                        \
+                           m21, m21_msg_type)                        \
+    __ZUNE_CUSTOMCLASS_START(name)                                   \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m1, m1, m1_msg_type);    \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m2, m2, m2_msg_type);    \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m3, m3, m3_msg_type);    \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m4, m4, m4_msg_type);    \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m5, m5, m5_msg_type);    \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m6, m6, m6_msg_type);    \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m7, m7, m7_msg_type);    \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m8, m8, m8_msg_type);    \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m9, m9, m9_msg_type);    \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m10, m10, m10_msg_type); \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m11, m11, m11_msg_type); \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m12, m12, m12_msg_type); \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m13, m13, m13_msg_type); \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m14, m14, m14_msg_type); \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m15, m15, m15_msg_type); \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m16, m16, m16_msg_type); \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m17, m17, m17_msg_type); \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m18, m18, m18_msg_type); \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m19, m19, m19_msg_type); \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m20, m20, m20_msg_type); \
+    __ZUNE_CUSTOMCLASS_METHOD(name ## __ ## m21, m21, m21_msg_type); \
+    __ZUNE_CUSTOMCLASS_END(name, base, parent_name, parent_class)    \
+
+
 /*************************************************************************/
 
 #define __ZUNE_CUSTOMCLASS_REMPARENS(...) __VA_ARGS__
