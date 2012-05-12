5627a303b76d7a5803c7aa700d08cb2b8c2b14ca
mattrust <mrustler@gmx.de>
2012-05-12 17:30:05 +0000

    Expat as shared library. Code taken from Fredrik Wikstroms diskimage.device but built from a *.conf file.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@44792 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/expat.h
@@ -0,0 +1,12 @@
+/*
+    Copyright © 2012, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#ifndef _EXPAT_H
+#define _EXPAT_H
+
+#include <libraries/expat.h>
+#include <proto/expat.h>
+
+#endif /* _EXPAT_H */
