ae69ec3bd85101d73022cf57b91e872a70d20dc1
mattrust <mrustler@gmx.de>
2012-05-16 05:58:34 +0000

    Test if the trick to get the template of a DOS command is working. That trick is necessary if we want to clone or port a tool like "R".
    git-svn-id: https://svn.aros.org/svn/aros/trunk@44816 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/dos/r.c
@@ -0,0 +1,47 @@
+#include <proto/dos.h>
+#include <proto/exec.h>
+
+#include <aros/debug.h>
+
+TEXT buffer[100];
+
+/*
+ * A tool like "R" http://www.geit.de/eng_r.html requires that we can
+ * call a command such that it prints the template and then stops.
+ */
+
+int main(void)
+{
+    // in the real "R" we would check for an unused file name first
+    BPTR input = Open("t:00000001.request.infile", MODE_NEWFILE);
+    BPTR output = Open("t:00000001.request.outfile", MODE_NEWFILE);
+
+    // shut up DOS error message
+    struct Process *me = (struct Process*)FindTask(NULL);
+    BPTR oldwin = me->pr_WindowPtr;
+    me->pr_WindowPtr = (BPTR)-1;
+
+    // execute the command. The purpose of "*>NIL:" is to
+    // trigger an error
+    Execute("dir *>NIL: ?", input, output);
+
+    // restore window ptr
+    me->pr_WindowPtr = oldwin;
+
+    Seek(output, 0, OFFSET_BEGINNING);
+
+    // read the template
+    if (FGets(output, buffer, sizeof buffer))
+    {
+        bug("*****\n%s*****\n", buffer);
+    }
+    else
+    {
+        bug("%s\n", "-----");
+    }
+
+    Close(input);
+    Close(output);
+
+    return 0;
+}
