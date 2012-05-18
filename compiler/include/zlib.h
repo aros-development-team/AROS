f4d3039a643f26dbe841dacd528c9ed64ac1a307
mattrust <mrustler@gmx.de>
2012-05-18 15:19:41 +0000

    Introducing zlib as shared library.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@44833 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/zlib.h
@@ -0,0 +1,12 @@
+/*
+    Copyright © 2012, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#ifndef _ZLIB_AU_H
+#define _ZLIB_AU_H
+
+#include <libraries/z_au.h>
+#include <proto/z_au.h>
+
+#endif /* _ZLIB_AU_H */
