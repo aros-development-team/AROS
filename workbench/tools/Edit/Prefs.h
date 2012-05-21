f9691aef5fa93c8409fa78ded1147a606ce12a7f
mattrust <mrustler@gmx.de>
2012-05-21 22:39:30 +0000

    Renamed Preference Edit to Editor to match name of main application.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@44852 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/tools/Edit/Prefs.h
+++ workbench/tools/Edit/Prefs.h
@@ -56,7 +56,7 @@ typedef struct prefs
 /** Command search to edit preference of editor **/
 #define	SYS_DIR        "SYS:"
 #define	PREF_DIR       "Prefs/"
-#define	PREF_NAME      "Edit"
+#define	PREF_NAME      "Editor"
 
 /** Character types to split words **/
 #define	ALPHA          0
