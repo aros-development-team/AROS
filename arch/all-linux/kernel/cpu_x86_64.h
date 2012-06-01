c3a34f68e4ac9cc40d13dce7ef4ae60fd920e315
ezrec <jason.mcmullan@gmail.com>
2012-06-01 03:24:52 +0000

    arch/x86_64(linux): Update the sa_sighandler function type to current standards
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@44910 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-linux/kernel/cpu_x86_64.h
+++ arch/all-linux/kernel/cpu_x86_64.h
@@ -77,7 +77,7 @@ struct ucontext;
 */
 
 #define GLOBAL_SIGNAL_INIT(sighandler) \
-	static void sighandler ## _gate (int sig, siginfo_t *blub, struct ucontext *u) 	\
+	static void sighandler ## _gate (int sig, siginfo_t *blub, void *u) 	\
 	{ 										\
 	    sighandler(sig, u); 							\
 	}
