25651d973256f49a99e083fc3f432bd20655d986
olivieradam <olivieradam@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-06-18 13:10:04 +0000

    BIB Prefs: Added support for iconification.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@45058 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/prefs/boingiconbar/main.c
+++ workbench/prefs/boingiconbar/main.c
@@ -11,9 +11,11 @@
 #define MUIMASTER_YES_INLINE_STDARG
 
 #include <proto/alib.h>
+#include <proto/icon.h>
 #include <proto/intuition.h>
 #include <proto/muimaster.h>
 
+#include <workbench/startup.h>
 #include <zune/systemprefswindow.h>
 
 #include "locale.h"
@@ -31,12 +33,26 @@ int main(int argc, char **argv)
 {
     Object *application;
     Object *window;
+    struct DiskObject *disko;
+    static struct WBStartup *argmsg;
+    static struct WBArg *wb_arg;
+    static STRPTR cxname;
 
     D(bug("[bib prefs] InitLocale\n"));
     Locale_Initialize();
 
     D(bug("[bib prefs] started\n"));
 
+    if (argc)
+    {
+        cxname = argv[0];
+    } else {
+        argmsg = (struct WBStartup *)argv;
+        wb_arg = argmsg->sm_ArgList;
+        cxname = wb_arg->wa_Name;
+    }
+    disko = GetDiskObject(cxname);
+    
     /* init */
     if (ReadArguments(argc, argv))
     {
@@ -52,6 +68,7 @@ int main(int argc, char **argv)
                 MUIA_Application_Version, (IPTR) VERSION,
                 MUIA_Application_Description, __(MSG_TITLE),
                 MUIA_Application_Base, (IPTR) "BIBPREF",
+                MUIA_Application_DiskObject, (IPTR)disko,
                 SubWindow, (IPTR)(window = (Object *)SystemPrefsWindowObject,
                     MUIA_Window_ID, MAKE_ID('B','I','P','8'),
                     WindowContents, (IPTR) BibEditorObject,
@@ -70,6 +87,8 @@ int main(int argc, char **argv)
         FreeArguments();
     }
 
+    if (disko != NULL)
+        FreeDiskObject(disko);
     Locale_Deinitialize();
     return 0;
 }
