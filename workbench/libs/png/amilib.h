ef16e0fb91fbbe5f8c97bb84254cd6453e68b54e
mattrust <mrustler@gmx.de>
2012-07-04 21:01:08 +0000

    Attempt to fix the crashes when using png_au.library by opening z_au.library.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@45163 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/png/amilib.h
+++ workbench/libs/png/amilib.h
@@ -7,6 +7,7 @@ struct PngBase
 {
    struct Library _lib;
    struct Library *_aroscbase;
+   struct Library *_zbase;
 };
 
 #endif /* _AMILIB_H */
--- workbench/libs/png/init-aros.c
+++ workbench/libs/png/init-aros.c
@@ -5,6 +5,7 @@
 #include <aros/debug.h>
 
 IPTR aroscbase_offset;
+IPTR zbase_offset;
 
 
 static int InitFunc(LIBBASETYPEPTR LIBBASE)
@@ -12,25 +13,29 @@ static int InitFunc(LIBBASETYPEPTR LIBBASE)
     D(bug("Inside Init func of png.library\n"));
 
     aroscbase_offset = offsetof(LIBBASETYPE, _aroscbase);
+    zbase_offset = offsetof(LIBBASETYPE, _zbase);
 
     return TRUE;
 }
 
 static int OpenFunc(LIBBASETYPEPTR LIBBASE)
 {
-   D(bug("Opening png.library\n"));
+    D(bug("Opening png.library\n"));
 
-   LIBBASE->_aroscbase = OpenLibrary("arosc.library", 0);
+    LIBBASE->_aroscbase = OpenLibrary("arosc.library", 0);
+    D(bug("[png.library::OpenLib] aroscbase=%p\n", LIBBASE->_aroscbase));
 
-   D(bug("[png.library::OpenLib] aroscbase=%p\n", LIBBASE->_aroscbase));
+    LIBBASE->_zbase = OpenLibrary("z_au.library", 0);
+    D(bug("[png.library::OpenLib] zbase=%p\n", LIBBASE->_zbase));
 
-   return LIBBASE->_aroscbase != NULL;
+    return LIBBASE->_aroscbase != NULL && LIBBASE->_zbase != NULL;
 }
 
 static int CloseFunc(LIBBASETYPEPTR LIBBASE)
 {
    D(bug("Closing png.library\n"));
 
+   CloseLibrary(LIBBASE->_zbase);
    CloseLibrary(LIBBASE->_aroscbase);
 
    return TRUE;
