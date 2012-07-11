49ac92d6b1b30136de5e520b30defa21ed5271fb
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-07-11 18:18:18 +0000

    arch/ppc-native: Fetch the PVR register via GCIT_Model
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@45199 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/ppc-native/processor/processor_intern_arch.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2012, The AROS Development Team.  All rights reserved.
+ * Author: Jason S. McMullan <jason.mcmullan@gmail.com>
+ *
+ * Licensed under the AROS PUBLIC LICENSE (APL) Version 1.1
+ */
+
+#ifndef PROCESSOR_INTERN_ARCH_H
+#define PROCESSOR_INTERN_ARCH_H
+
+#include <exec/types.h>
+
+#include "processor_intern.h"
+
+struct SystemProcessors {
+    ULONG       sp_PVR;
+};
+
+#endif /* PROCESSOR_INTERN_ARCH_H */
