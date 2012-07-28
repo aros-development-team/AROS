6bf759b17a4a9a6524a227b704248713ab916b42
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-07-28 00:02:02 +0000

    complex.h: #error for unsupported compiler; commented out non-available functions
    git-svn-id: https://svn.aros.org/svn/aros/trunk@45341 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/mlib/complex.h
+++ compiler/mlib/complex.h
@@ -13,6 +13,8 @@
 #ifdef __GNUC__
 #   define  _Complex	__complex__
 #   define  _Complex_I	1.0fi
+#else
+#error Unsupported compiler in complex.h
 #endif
 
 #define complex		_Complex
@@ -20,69 +22,69 @@
 
 __BEGIN_DECLS
 
-double complex cacos(double complex z);
-float complex cacosf(float complex z);
-long double complex cacosl(long double complex z);
-double complex casin(double complex z);
-float complex casinf(float complex z);
-long double complex casinl(long double complex z);
-double complex catan(double complex z);
-float complex catanf(float complex z);
-long double complex catanl(long double complex z);
-double complex ccos(double complex z);
-float complex ccosf(float complex z);
-long double complex ccosl(long double complex z);
-double complex csin(double complex z);
-float complex csinf(float complex z);
-long double complex csinl(long double complex z);
-double complex ctan(double complex z);
-float complex ctanf(float complex z);
-long double complex ctanl(long double complex z);
-double complex cacosh(double complex z);
-float complex cacoshf(float complex z);
-long double complex cacoshl(long double complex z);
-double complex casinh(double complex z);
-float complex casinhf(float complex z);
-long double complex casinhl(long double complex z);
-double complex catanh(double complex z);
-float complex catanhf(float complex z);
-long double complex catanhl(long double complex z);
-double complex ccosh(double complex z);
-float complex ccoshf(float complex z);
-long double complex ccoshl(long double complex z);
-double complex csinh(double complex z);
-float complex csinhf(float complex z);
-long double complex csinhl(long double complex z);
-double complex ctanh(double complex z);
-float complex ctanhf(float complex z);
-long double complex ctanhl(long double complex z);
-double complex cexp(double complex z);
-float complex cexpf(float complex z);
-long double complex cexpl(long double complex z);
-double complex clog(double complex z);
-float complex clogf(float complex z);
-long double complex clogl(long double complex z);
+//NOT IMPL double complex cacos(double complex z);
+//NOT IMPL float complex cacosf(float complex z);
+//NOT IMPL long double complex cacosl(long double complex z);
+//NOT IMPL double complex casin(double complex z);
+//NOT IMPL float complex casinf(float complex z);
+//NOT IMPL long double complex casinl(long double complex z);
+//NOT IMPL double complex catan(double complex z);
+//NOT IMPL float complex catanf(float complex z);
+//NOT IMPL long double complex catanl(long double complex z);
+//NOT IMPL double complex ccos(double complex z);
+//NOT IMPL float complex ccosf(float complex z);
+//NOT IMPL long double complex ccosl(long double complex z);
+//NOT IMPL double complex csin(double complex z);
+//NOT IMPL float complex csinf(float complex z);
+//NOT IMPL long double complex csinl(long double complex z);
+//NOT IMPL double complex ctan(double complex z);
+//NOT IMPL float complex ctanf(float complex z);
+//NOT IMPL long double complex ctanl(long double complex z);
+//NOT IMPL double complex cacosh(double complex z);
+//NOT IMPL float complex cacoshf(float complex z);
+//NOT IMPL long double complex cacoshl(long double complex z);
+//NOT IMPL double complex casinh(double complex z);
+//NOT IMPL float complex casinhf(float complex z);
+//NOT IMPL long double complex casinhl(long double complex z);
+//NOT IMPL double complex catanh(double complex z);
+//NOT IMPL float complex catanhf(float complex z);
+//NOT IMPL long double complex catanhl(long double complex z);
+//NOT IMPL double complex ccosh(double complex z);
+//NOT IMPL float complex ccoshf(float complex z);
+//NOT IMPL long double complex ccoshl(long double complex z);
+//NOT IMPL double complex csinh(double complex z);
+//NOT IMPL float complex csinhf(float complex z);
+//NOT IMPL long double complex csinhl(long double complex z);
+//NOT IMPL double complex ctanh(double complex z);
+//NOT IMPL float complex ctanhf(float complex z);
+//NOT IMPL long double complex ctanhl(long double complex z);
+//NOT IMPL double complex cexp(double complex z);
+//NOT IMPL float complex cexpf(float complex z);
+//NOT IMPL long double complex cexpl(long double complex z);
+//NOT IMPL double complex clog(double complex z);
+//NOT IMPL float complex clogf(float complex z);
+//NOT IMPL long double complex clogl(long double complex z);
 double cabs(double complex z);
 float cabsf(float complex z);
-long double cabsl(long double complex z);
-double complex cpow(double complex x, double complex y);
-float complex cpowf(float complex x, float complex y);
-long double complex cpowl(long double complex x, long double complex y);
-double complex csqrt(double complex z);
-float complex csqrtf(float complex z);
-long double complex csqrtl(long double complex z);
-double carg(double complex z);
-float cargf(float complex z);
-long double cargl(long double complex z);
+//NOT IMPL long double cabsl(long double complex z);
+//NOT IMPL double complex cpow(double complex x, double complex y);
+//NOT IMPL float complex cpowf(float complex x, float complex y);
+//NOT IMPL long double complex cpowl(long double complex x, long double complex y);
+//NOT IMPL double complex csqrt(double complex z);
+//NOT IMPL float complex csqrtf(float complex z);
+//NOT IMPL long double complex csqrtl(long double complex z);
+//NOT IMPL double carg(double complex z);
+//NOT IMPL float cargf(float complex z);
+//NOT IMPL long double cargl(long double complex z);
 double cimag(double complex z);
 float cimagf(float complex z);
 long double cimagl(long double complex z);
 double complex conj(double complex z);
 float complex conjf(float complex z);
 long double complex conjl(long double complex z);
-double complex cproj(double complex z);
-float complex cprojf(float complex z);
-long double complex cprojl(long double complex z);
+//NOT IMPL double complex cproj(double complex z);
+//NOT IMPL float complex cprojf(float complex z);
+//NOT IMPL long double complex cprojl(long double complex z);
 double creal(double complex z);
 float crealf(float complex z);
 long double creall(long double complex z);
