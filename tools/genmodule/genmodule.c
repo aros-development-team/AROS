c2442209d7a92894dcd47dfab3a64de11fb08fd8
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-07-28 21:13:08 +0000

    tools/genmodule: Bring back xxx_GetLibbase() functions.
    This mostly reverts commit r45334: "genmodule/ctype: Fix issue with static arosc ctype stubs"
    Next commit will contain use case for the functions.
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@45351 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- tools/genmodule/genmodule.c
+++ tools/genmodule/genmodule.c
@@ -21,6 +21,11 @@ int main(int argc, char **argv)
 	    writeautoinit(cfg, 0); /* normal */
 	    writeautoinit(cfg, 1); /* relbase */
         }
+        if (cfg->modtype == LIBRARY)
+        {
+            writegetlibbase(cfg, 0); /* normal */
+            writegetlibbase(cfg, 1); /* relbase */
+        }
         if (cfg->options & OPTION_STUBS)
         {
             writestubs(cfg, 0); /* normal */
