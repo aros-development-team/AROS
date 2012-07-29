a8825be802cf06952500992ccbb82ff4faabfadf
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-07-29 07:44:13 +0000

    pertaskvalue - example functional stub
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@45357 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/library/pertaskvalue_linklib.c
+++ test/library/pertaskvalue_linklib.c
@@ -8,7 +8,37 @@
  */
 #include "pertaskbase.h"
 
+#ifdef DOESNT_WORK
 struct PertaskBase *Pertask_GetLibbase(void);
+
+#else /* Does Work */
+
+#include <aros/symbolsets.h>
+#include <proto/exec.h>
+#include <aros/debug.h>
+
+static struct Library *_PertaskBase;
+
+static int pertask_linklib_init(void)
+{
+	_PertaskBase = OpenLibrary("pertask.library", 0);
+	bug("PertaskBase (link) = %p\n", _PertaskBase);
+	return TRUE;
+}
+ADD2INIT(pertask_linklib_init, 10);
+
+static int pertask_linklib_exit(void)
+{
+	CloseLibrary(_PertaskBase);
+	return TRUE;
+}
+ADD2EXIT(pertask_linklib_exit, 10);
+
+struct PertaskBase *Pertask_GetLibbase(void)
+{
+	return (struct PertaskBase *)_PertaskBase;
+}
+#endif
 
 int *__pertask_getvalueptr(void)
 {
