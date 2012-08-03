28323e95849d08c069f605e9b480e4384184792d
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-08-03 15:07:27 +0000

    Fixed AutoDoc indentation, spelling, grammar.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@45384 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/intuition/nextobject.c
+++ rom/intuition/nextobject.c
@@ -1,8 +1,8 @@
 /*
-    Copyright © 1995-2007, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2012, The AROS Development Team. All rights reserved.
     Copyright © 2001-2003, The MorphOS Development Team. All Rights Reserved.
     $Id$
- 
+
     Iterate through a list of objects.
 */
 
@@ -11,7 +11,7 @@
 #include "intuition_intern.h"
 
 /*****************************************************************************
- 
+
     NAME */
 #include <intuition/classes.h>
 #include <proto/intuition.h>
@@ -25,34 +25,32 @@
          struct IntuitionBase *, IntuitionBase, 111, Intuition)
 
 /*  FUNCTION
-    Use this function to iterate through a list of BOOPSI objects.
-    You may do whatever you want with the object returned, even
-    remove it from the list or disposing it and then continue to
-    iterate thought the list.
- 
+        Use this function to iterate through a list of BOOPSI objects.
+        You may do whatever you want with the object returned, even
+        remove it from the list or dispose it, and then continue to
+        iterate through the list.
+
     INPUTS
-    objectPtrPtr - the pointer to a variable. This must be the same
-        variable, as long as you iterate though the same list. This
-        variable must initially be filled with the lh_Head of a list.
- 
+        objectPtrPtr - the pointer to a variable. This must be the same
+            variable, as long as you iterate though the same list. This
+            variable must initially be filled with the lh_Head of a list.
+
     RESULT
-    A BOOPSI object, which can be manipulated.
- 
+        A BOOPSI object, which can be manipulated.
+
     NOTES
- 
+
     EXAMPLE
  
     BUGS
  
     SEE ALSO
-    NewObjectA(),
-    "Basic Object-Oriented Programming System for Intuition" and
-    "boopsi Class Reference" Dokument.
- 
+        NewObjectA(),
+        "Basic Object-Oriented Programming System for Intuition" and
+        "boopsi Class Reference" Document.
+
     INTERNALS
- 
-    HISTORY
- 
+
 *****************************************************************************/
 {
     AROS_LIBFUNC_INIT
