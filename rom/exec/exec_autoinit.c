963019438a75157af3dadb6bff55ad27fe92607b
ezrec <jason.mcmullan@gmail.com>
2012-08-16 06:17:38 +0000

    libexec.a - use __showerror from libautoinit.a
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@45574 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/exec_autoinit.c
+++ rom/exec/exec_autoinit.c
@@ -14,40 +14,27 @@
 #include <proto/exec.h>
 #include <proto/dos.h>
 
+/* From libautoinit.a */
+void __showerror(char *format, const IPTR *);
+
 /* Linklib to provide a 'SysBase' symbol. Also verifies that
  * the symbol is set appropriately.
  */
 struct ExecBase *SysBase;
 extern const LONG const __aros_libreq_SysBase __attribute__((weak));
 
-static AROS_UFH2(IPTR, RawPutc,
-        AROS_UFHA(UBYTE, c, D0),
-        AROS_UFHA(struct ExecBase *, SysBase, A3))
-{
-    AROS_USERFUNC_INIT
-    if (c)
-        RawPutChar(c);
-    return c;
-    AROS_USERFUNC_EXIT
-}
-
 static int SysBase_check_init(void)
 {
-    if (SysBase == NULL) {
-        IPTR arr[] = {
-                 (IPTR)(FindTask(NULL)->tc_Node.ln_Name),
-                 __aros_libreq_SysBase,
-        };
-        RawDoFmt("%s: Requires exec.library v%ld, none provided\n",arr,(VOID_FUNC)RawPutc,SysBase);
+    if (SysBase == NULL)
         return FALSE;
-    }
+
     if (__aros_libreq_SysBase > SysBase->LibNode.lib_Version) {
         IPTR arr[] = {
                  (IPTR)(FindTask(NULL)->tc_Node.ln_Name),
                  __aros_libreq_SysBase,
                  SysBase->LibNode.lib_Version
         };
-        RawDoFmt("%s: Requires exec.library v%ld, found v%ld\n",arr,(VOID_FUNC)RawPutc,SysBase);
+        __showerror("%s: Requires exec.library v%ld, found v%ld\n",arr);
         return FALSE;
     }
 
