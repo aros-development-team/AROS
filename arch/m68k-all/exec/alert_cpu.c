06e89689829ee2e4d32c3b1fbea308d993aa6685
twilen <twilen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-09-07 18:35:44 +0000

    Include SegTracker hunk information (if available) in m68k alerts.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@45744 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/m68k-all/exec/alert_cpu.c
+++ arch/m68k-all/exec/alert_cpu.c
@@ -14,18 +14,49 @@
 #include "exec_intern.h"
 #include "exec_util.h"
 
+AROS_UFP3(UBYTE*, SegTrack,
+    AROS_UFPA(ULONG, Address, A0),
+    AROS_UFPA(ULONG*, SegNum, A1),
+    AROS_UFPA(ULONG*, Offset, A2));
+
+struct SegSem
+{
+    struct SignalSemaphore seg_Semaphore;
+    APTR seg_Find;
+};
+#define SEG_SEM "SegTracker"
+
 char *FormatCPUContext(char *buffer, struct ExceptionContext *ctx, struct ExecBase *SysBase)
 {
+    struct SegSem *segSem;
     VOID_FUNC dest = buffer ? RAWFMTFUNC_STRING : RAWFMTFUNC_SERIAL;
     char *buf;
-    
-    buf = RawDoFmt("D0: %08lx %08lx %08lx %08lx\n"
+
+    RawDoFmt("D0: %08lx %08lx %08lx %08lx\n"
                    "D4: %08lx %08lx %08lx %08lx\n"
                    "A0: %08lx %08lx %08lx %08lx\n"
                    "A4: %08lx %08lx %08lx %08lx\n"
                    "SR:     %04x\n"
                    "PC: %08lx", ctx, dest, buffer);
+    buf = buffer + strlen(buffer);
 
+    segSem = (struct SegSem *)FindSemaphore(SEG_SEM);
+    if (segSem) {
+        ULONG SegNum, SegOffset, SegList;
+        Forbid();
+        UBYTE *name = AROS_UFC3(UBYTE*, segSem->seg_Find,
+            AROS_UFCA(ULONG, ctx->pc, A0),
+            AROS_UFCA(ULONG*, &SegNum, A1),
+            AROS_UFCA(ULONG*, &SegOffset, A2));
+        if (name) {
+            AROS_UFC3(UBYTE*, segSem->seg_Find,
+               AROS_UFCA(ULONG, ctx->pc, A0),
+               AROS_UFCA(ULONG*, &SegList, A1),
+               AROS_UFCA(ULONG*, &SegList, A2));
+            buf = NewRawDoFmt("\nSegTracker: %s\nHunk %ld, Offset $%08lx, SegList $%08lx\n", dest, buf, name, SegNum, SegOffset, SegList);
+        }
+        Permit();
+    }
     return buf - 1;
 }
 
