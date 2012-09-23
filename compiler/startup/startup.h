3c7acc9280b5376c8b9b18f843195cf172c010f2
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-09-23 18:59:31 +0000

    tools/genmodule: Mark PROGRAM_ENTRIES as handled in modules.
    PROGRAM_ENTRIES set is marked as handled in modules. The set is ignored though.
    Document this behaviour in aros/startup.h
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@45843 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/startup/startup.h
+++ compiler/startup/startup.h
@@ -4,7 +4,19 @@
 #include <setjmp.h>
 #include <proto/exec.h>
 #include <aros/symbolsets.h>
- 
+
+/* The AROS startup support code.
+
+   The following variables are made available to programs by the AROS startup
+   code.
+   There is also a PROGRAM_ENTRIES symbolset that allows to register a function
+   that can be used to do initialization before the main() function is called
+   and clean-up after it has run.
+   This PROGRAM_ENTRIES set is ignored by the startup code of shared libraries
+   and other modules. So if your code uses this PROGRAM_ENTRIES set and can be
+   linked in modules one should handle the case of a non-executed
+   PROGRAM_ENTRIES function gracefully.
+*/
 extern char *__argstr;
 extern ULONG __argsize;
 extern char **__argv;
