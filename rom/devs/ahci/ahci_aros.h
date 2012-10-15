7f94a34605996c33c6f223c9d2cfa0eafe0fa61c
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-10-15 04:28:19 +0000

    ahci.device: Add some more TAILQ list helpers
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@45906 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/devs/ahci/ahci_aros.h
+++ rom/devs/ahci/ahci_aros.h
@@ -537,10 +537,12 @@ struct sysctl_ctx_list {};
 #define TAILQ_HEAD(sname,type)  struct sname { struct type *tqh_first; struct type **tqh_last; }
 #define TAILQ_ENTRY(type)       struct { struct type *tqe_next; struct type **tqe_prev; }
 #define TAILQ_FIRST(head)       ((head)->tqh_first)
+#define TAILQ_EMPTY(head)       (TAILQ_FIRST(head) == NULL)
 #define TAILQ_INIT(head)        do {                                    \
     (head)->tqh_first = NULL;                                           \
     (head)->tqh_last = &(head)->tqh_first;                              \
 } while (0)
+#define TAILQ_NEXT(elm,field)   ((elm)->field.tqe_next)
 #define TAILQ_REMOVE(head,elm,field)  do {                              \
     if ((elm)->field.tqe_next)                                          \
         (elm)->field.tqe_next->field.tqe_prev = (elm)->field.tqe_prev;  \
@@ -554,6 +556,9 @@ struct sysctl_ctx_list {};
     *(head)->tqh_last = (elm);                                          \
     (head)->tqh_last = &(elm)->field.tqe_next;                          \
 } while (0)
+#define TAILQ_FOREACH(elm,head,field)                                   \
+    for ((elm) = ((head)->tqh_first);                                   \
+         (elm) != NULL; (elm)=(elm)->field.tqe_next)
 
 #define device_get_name(dev)    "ahci.device "
 #define device_get_unit(dev)    ((dev)->dev_HostID)
