b3e63e5e66a96c49428adc2668ac098f2710b906
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-11-08 16:05:13 +0000

    Mixed tabs/spaces to spaces.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46023 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/graphics/textfit.c
+++ rom/graphics/textfit.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2007, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2012, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: Tell how many characters will fit into a box.
@@ -14,45 +14,45 @@
 #include <graphics/text.h>
 #include <proto/graphics.h>
 
-	AROS_LH8(ULONG, TextFit,
+        AROS_LH8(ULONG, TextFit,
 
 /*  SYNOPSIS */
-	AROS_LHA(struct RastPort   *, rp, A1),
-	AROS_LHA(CONST_STRPTR       , string, A0),
-	AROS_LHA(ULONG              , strLen, D0),
-	AROS_LHA(struct TextExtent *, textExtent, A2),
-	AROS_LHA(struct TextExtent *, constrainingExtent, A3),
-	AROS_LHA(LONG               , strDirection, D1),
-	AROS_LHA(ULONG              , constrainingBitWidth, D2),
-	AROS_LHA(ULONG              , constrainingBitHeight, D3),
+        AROS_LHA(struct RastPort   *, rp, A1),
+        AROS_LHA(CONST_STRPTR       , string, A0),
+        AROS_LHA(ULONG              , strLen, D0),
+        AROS_LHA(struct TextExtent *, textExtent, A2),
+        AROS_LHA(struct TextExtent *, constrainingExtent, A3),
+        AROS_LHA(LONG               , strDirection, D1),
+        AROS_LHA(ULONG              , constrainingBitWidth, D2),
+        AROS_LHA(ULONG              , constrainingBitHeight, D3),
 
 /*  LOCATION */
-	struct GfxBase *, GfxBase, 116, Graphics)
+        struct GfxBase *, GfxBase, 116, Graphics)
 
 /*  FUNCTION
-	Tries to fill the given space with as many characters of the
-	font in rp as possible and returns that number.
+        Tries to fill the given space with as many characters of the
+        font in rp as possible and returns that number.
 
     INPUTS
-	rp - Use the settings in this RastPort (e.g. Font)
-	string - Use this string
-	strLen - The length of the string
-	textExtent - The size actually occupied will be returned here
-	constrainingExtent - If non-NULL, the routine will use the
-		dimensions of the box described here
-	strDirection - In which is the next character. Must be either 1
-		or -1. If it is -1, then string must point to the end (the
-		first character to check) of the text to fit (this is for
-		checking text which runs from right to left).
-	constrainingBitWidth - If constrainingExtent is NULL, then this
-		is the width of the bounding box.
-	constrainingBitHeight - If constrainingExtent is NULL, then this
-		is the height of the bounding box.
+        rp - Use the settings in this RastPort (e.g. Font)
+        string - Use this string
+        strLen - The length of the string
+        textExtent - The size actually occupied will be returned here
+        constrainingExtent - If non-NULL, the routine will use the
+                dimensions of the box described here
+        strDirection - In which is the next character. Must be either 1
+                or -1. If it is -1, then string must point to the end (the
+                first character to check) of the text to fit (this is for
+                checking text which runs from right to left).
+        constrainingBitWidth - If constrainingExtent is NULL, then this
+                is the width of the bounding box.
+        constrainingBitHeight - If constrainingExtent is NULL, then this
+                is the height of the bounding box.
 
     RESULT
-	The number of characters which fit in the bounding box.
-	If any characters fit in the bounding box, then textExtent will
-	tell how large the minimal bounding box for the string is.
+        The number of characters which fit in the bounding box.
+        If any characters fit in the bounding box, then textExtent will
+        tell how large the minimal bounding box for the string is.
 
     NOTES
 
@@ -61,90 +61,93 @@
     BUGS
 
     SEE ALSO
-	TextLength()
+        TextLength()
 
     INTERNALS
-
-    HISTORY
 
 *****************************************************************************/
 {
     AROS_LIBFUNC_INIT
 
     struct TextFont *tf = rp->Font;
-    ULONG   	     retval = 0;
-    
+    ULONG retval = 0;
+
     if (strLen && (constrainingBitHeight >= tf->tf_YSize))
     {
-   	BOOL ok = TRUE;
-	
-	textExtent->te_Extent.MinX 	= 0;
-	textExtent->te_Extent.MinY 	= -tf->tf_Baseline;
-    	textExtent->te_Extent.MaxX 	= 0;
-	textExtent->te_Extent.MaxY 	= tf->tf_YSize - tf->tf_Baseline - 1;
-    	textExtent->te_Width	= 0;
-	textExtent->te_Height 	= tf->tf_YSize;
-	
-	if (constrainingExtent)
-	{
-	    if ((constrainingExtent->te_Extent.MinY > textExtent->te_Extent.MinY) || 
-	    	(constrainingExtent->te_Extent.MaxY < textExtent->te_Extent.MaxY) ||
-	    	(constrainingExtent->te_Height < textExtent->te_Height))
-	    {
-	    	ok = FALSE;
-	    }    
-	}
-	
-	if (ok)
-	{
-	    while(strLen--)
-	    {
-    	    	struct TextExtent char_extent;
-	    	WORD 	    	  newwidth, newminx, newmaxx, minx, maxx;
-		
-	    	TextExtent(rp, string, 1, &char_extent);
-	    	string += strDirection;
-		
-		newwidth = textExtent->te_Width + char_extent.te_Width;
-		minx = textExtent->te_Width + char_extent.te_Extent.MinX;
-		maxx = textExtent->te_Width + char_extent.te_Extent.MaxX;
-		
-		newminx = (minx < textExtent->te_Extent.MinX) ? minx : textExtent->te_Extent.MinX;
-		newmaxx = (maxx > textExtent->te_Extent.MaxX) ? maxx : textExtent->te_Extent.MaxX;
-		
-		if ((ULONG)(newmaxx - newminx + 1) > constrainingBitWidth) break;
-		
-		if (constrainingExtent)
-		{
-		    if (constrainingExtent->te_Extent.MinX > newminx) break;
-		    if (constrainingExtent->te_Extent.MaxX < newmaxx) break;
-		    if (constrainingExtent->te_Width < newwidth) break;
-		}
-				
-		textExtent->te_Width = newwidth;
-		textExtent->te_Extent.MinX = newminx;
-		textExtent->te_Extent.MaxX = newmaxx;
-		
-    	    	retval++;		
-		
-	    } /* while(strLen--) */
-	    
-	} /* if (ok) */
-	
+        BOOL ok = TRUE;
+
+        textExtent->te_Extent.MinX = 0;
+        textExtent->te_Extent.MinY = -tf->tf_Baseline;
+        textExtent->te_Extent.MaxX = 0;
+        textExtent->te_Extent.MaxY = tf->tf_YSize - tf->tf_Baseline - 1;
+        textExtent->te_Width       = 0;
+        textExtent->te_Height      = tf->tf_YSize;
+
+        if (constrainingExtent)
+        {
+            if (constrainingExtent->te_Extent.MinY > textExtent->te_Extent.MinY
+                || 
+                constrainingExtent->te_Extent.MaxY < textExtent->te_Extent.MaxY
+                ||
+                constrainingExtent->te_Height < textExtent->te_Height)
+            {
+                ok = FALSE;
+            }    
+        }
+
+        if (ok)
+        {
+            while (strLen--)
+            {
+                struct TextExtent char_extent;
+                WORD newwidth, newminx, newmaxx, minx, maxx;
+
+                TextExtent(rp, string, 1, &char_extent);
+                string += strDirection;
+
+                newwidth = textExtent->te_Width + char_extent.te_Width;
+                minx = textExtent->te_Width + char_extent.te_Extent.MinX;
+                maxx = textExtent->te_Width + char_extent.te_Extent.MaxX;
+
+                newminx = (minx < textExtent->te_Extent.MinX) ?
+                    minx : textExtent->te_Extent.MinX;
+                newmaxx = (maxx > textExtent->te_Extent.MaxX) ?
+                    maxx : textExtent->te_Extent.MaxX;
+
+                if ((ULONG)(newmaxx - newminx + 1) > constrainingBitWidth)
+                    break;
+
+                if (constrainingExtent)
+                {
+                    if (constrainingExtent->te_Extent.MinX > newminx) break;
+                    if (constrainingExtent->te_Extent.MaxX < newmaxx) break;
+                    if (constrainingExtent->te_Width < newwidth) break;
+                }
+
+                textExtent->te_Width = newwidth;
+                textExtent->te_Extent.MinX = newminx;
+                textExtent->te_Extent.MaxX = newmaxx;
+
+                retval++;                
+
+            } /* while(strLen--) */
+
+        } /* if (ok) */
+
     } /* if (strLen && (constrainingBitHeight >= tf->tf_YSize)) */
 
     if (retval == 0)
     {
-    	textExtent->te_Width = 0;
-	textExtent->te_Height = 0;
-	textExtent->te_Extent.MinX = 0;
-	textExtent->te_Extent.MinY = 0;
-	textExtent->te_Extent.MaxX = 0;
-	textExtent->te_Extent.MaxY = 0;
+        textExtent->te_Width = 0;
+        textExtent->te_Height = 0;
+        textExtent->te_Extent.MinX = 0;
+        textExtent->te_Extent.MinY = 0;
+        textExtent->te_Extent.MaxX = 0;
+        textExtent->te_Extent.MaxY = 0;
     }
-    
+
     return retval;
-    
+
     AROS_LIBFUNC_EXIT
-    
+
 } /* TextFit */
--- rom/graphics/textlength.c
+++ rom/graphics/textlength.c
@@ -1,6 +1,6 @@
 /*
-    Copyright © 1995-2007, The AROS Development Team. All rights reserved.
-    $Id$	 $Log
+    Copyright © 1995-2012, The AROS Development Team. All rights reserved.
+    $Id$
 
     Desc: Graphics function TextLength()
     Lang: english
@@ -16,81 +16,78 @@
 #include <graphics/rastport.h>
 #include <proto/graphics.h>
 
-	AROS_LH3(WORD, TextLength,
+        AROS_LH3(WORD, TextLength,
 
 /*  SYNOPSIS */
-	AROS_LHA(struct RastPort *, rp, A1),
-	AROS_LHA(CONST_STRPTR     , string, A0),
-	AROS_LHA(ULONG            , count, D0),
+        AROS_LHA(struct RastPort *, rp, A1),
+        AROS_LHA(CONST_STRPTR     , string, A0),
+        AROS_LHA(ULONG            , count, D0),
 
 /*  LOCATION */
-	struct GfxBase *, GfxBase, 9, Graphics)
+        struct GfxBase *, GfxBase, 9, Graphics)
 
 /*  FUNCTION
-	Determines the length of a string in pixels.
-	
+        Determines the length of a string in pixels.
+
     INPUTS
-	rp     - RastPort
-	string - address of string
-	count  - number of characters of string
+        rp     - RastPort
+        string - address of string
+        count  - number of characters of string
 
     RESULT
-	Length of string in pixels.
+        Length of string in pixels.
 
     NOTES
-	Use the newer TextExtent() to get more information.
+        Use the newer TextExtent() to get more information.
 
     EXAMPLE
 
     BUGS
 
     SEE ALSO
-	Text(), TextExtent()
+        Text(), TextExtent()
 
     INTERNALS
-
-    HISTORY
-	29-10-95    digulla automatically created from
-			    graphics_lib.fd and clib/graphics_protos.h
 
 *****************************************************************************/
 {
     AROS_LIBFUNC_INIT
 
     struct TextFont *tf = rp->Font;
-    WORD    	    strlen;
+    WORD strlen;
 
-    if ((tf->tf_Flags & FPF_PROPORTIONAL) || tf->tf_CharKern || tf->tf_CharSpace)
+    if ((tf->tf_Flags & FPF_PROPORTIONAL) || tf->tf_CharKern
+        || tf->tf_CharSpace)
     {
-    	WORD  idx;
-	WORD  defaultidx = NUMCHARS(tf) - 1; /* Last glyph is the default glyph */
-	UBYTE c;
-	
-	for(strlen = 0; count; count--)
-	{
-	    c = *string++;
-	    
-	    if ( c < tf->tf_LoChar || c > tf->tf_HiChar)
-	    {
-		idx = defaultidx;
-	    }
-	    else
-	    {
-		idx = c - tf->tf_LoChar;
-	    }
-	    	    
-   	    strlen += ((WORD *)tf->tf_CharKern)[idx];
-	    strlen += ((WORD *)tf->tf_CharSpace)[idx];
-	    strlen += rp->TxSpacing;
-	}
+        WORD  idx;
+        WORD  defaultidx = NUMCHARS(tf) - 1; /* Last glyph is default glyph */
+        UBYTE c;
+
+        for(strlen = 0; count; count--)
+        {
+            c = *string++;
+
+            if ( c < tf->tf_LoChar || c > tf->tf_HiChar)
+            {
+                idx = defaultidx;
+            }
+            else
+            {
+                idx = c - tf->tf_LoChar;
+            }
+
+            strlen += ((WORD *)tf->tf_CharKern)[idx];
+            strlen += ((WORD *)tf->tf_CharSpace)[idx];
+            strlen += rp->TxSpacing;
+        }
     }
     else
     {
-    	strlen = count * (tf->tf_XSize + rp->TxSpacing);
+        strlen = count * (tf->tf_XSize + rp->TxSpacing);
     }
-    
+
     return strlen;
-    
+
     AROS_LIBFUNC_EXIT
-    
+
 } /* TextLength */
