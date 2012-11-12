85e0a0e23daf0e280224fa5b21d8715264893b9a
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-11-12 16:14:47 +0000

    - Now splits paragraphs into multiple lines, with each line fitting the    visible width.  - Implemented MUIA_Floattext_Justify.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46029 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/classes/floattext_private.h
+++ workbench/libs/muimaster/classes/floattext_private.h
@@ -1,3 +1,8 @@
+/*
+    Copyright © 2002-2012, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
 #ifndef _FLOATTEXT_PRIVATE_H_
 #define _FLOATTEXT_PRIVATE_H_
 
@@ -6,12 +11,12 @@
 
 struct Floattext_DATA
 {
-    struct Hook  construct_hook;
-    struct Hook  destruct_hook;
     STRPTR       text;
     BOOL         justify;
+    BOOL         typesetting;
     STRPTR       skipchars;
     ULONG        tabsize;
+    ULONG        oldwidth;
 };
 
 #endif /* _FLOATTEXT_PRIVATE_H_ */
