803a3bfc5b68f07da25569988ba3dec2b84f90ea
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-12-30 09:09:42 +0000

    Added missing properties.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46172 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-pc/battclock/battclock_intern.h
+++ arch/all-pc/battclock/battclock_intern.h
@@ -1,6 +1,6 @@
 /*
     Copyright © 1995-2011, The AROS Development Team. All rights reserved.
-    $Id: battclock_intern.h 36647 2011-01-17 14:01:23Z neil $
+    $Id$
 
     Desc: Internal data structures for battclock.resource, PC hardware version
     Lang: english
--- arch/all-pc/kernel/xtpic.c
+++ arch/all-pc/kernel/xtpic.c
@@ -1,6 +1,6 @@
 /*
     Copyright © 2011, The AROS Development Team. All rights reserved.
-    $Id:$
+    $Id$
 
     Desc: Legacy Intel 8259A PIC driver.
 */
