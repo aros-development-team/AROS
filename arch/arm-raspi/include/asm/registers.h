1d0f9216f33f2ab19c138e74cccf1c6ef93e20eb
DizzyOfCRN <DizzyOfCRN@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-01-11 13:45:59 +0000

    - Moved register definitions from serialdebug.h to registers.h - Comment here on commit log that the UART used for serial debug is not the mini uart but the one that is PL011 UART compliant, which I mistakenly thought wasn't compliant as the code trashes ouput.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46236 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-raspi/include/asm/registers.h
+++ arch/arm-raspi/include/asm/registers.h
@@ -28,6 +28,34 @@
 #define GPFEN0              0x20200058  // GPIO Pin Falling Edge Detect Enable 0
 #define GPFEN1              0x2020005C  // GPIO Pin Falling Edge Detect Enable 1
 
+#define GPPUD               0x20200094
+#define GPPUDCLK0           0x20200098
+
+#define UART0_BASE          0x20201000
+#define UART_DR                 (0x00)
+#define UART_RSRECR             (0x04)
+#define UART_FR                 (0x18)
+#define UART_ILPR               (0x20)
+#define UART_IBRD               (0x24)
+#define UART_FBRD               (0x28)
+#define UART_LCRH               (0x2C)
+#define UART_CR                 (0x30)
+#define UART_IFLS               (0x34)
+#define UART_IMSC               (0x38)
+#define UART_RIS                (0x3C)
+#define UART_MIS                (0x40)
+#define UART_ICR                (0x44)
+#define UART_DMACR              (0x48)
+#define UART_ITCR               (0x80)
+#define UART_ITIP               (0x84)
+#define UART_ITOP               (0x88)
+#define UART_TDR                (0x8C)
+
+#define ONEMS	              (0xb0/4)
+#define UBIR	              (0xa4/4)
+#define UBMR	              (0xa8/4)
+#define UCR2	              (0x84/4)
+
 #define AUX_IRQ             0x20215000  // Auxiliary Interrupt status
 #define AUX_ENABLES         0x20215004  // Auxiliary enables
 #define AUX_MU_IO_REG       0x20215040  // AUX_MU_IO_REG Mini Uart I/O Data
