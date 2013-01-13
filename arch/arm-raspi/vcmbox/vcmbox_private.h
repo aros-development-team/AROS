08344fae27559c56fc91f805aa4994977bc8d4fb
Kalamatee <kalamatee@gmail.com>
2013-01-13 19:39:21 +0000

    fix typos
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46265 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-raspi/vcmbox/vcmbox_private.h
+++ arch/arm-raspi/vcmbox/vcmbox_private.h
@@ -3,15 +3,15 @@
     $Id$
 */
 
-#ifndef VCMB_PRIVATE_H_
-#define VCMB_PRIVATE_H_
+#ifndef VCMBOX_PRIVATE_H_
+#define VCMBOX_PRIVATE_H_
 
 #include <exec/nodes.h>
 #include <inttypes.h>
 
-struct VCMBBase {
+struct VCMBoxBase {
     struct Node		vcmb_Node;
 };
 
 
-#endif /* VCMB_PRIVATE_H_ */
+#endif /* VCMBOX_PRIVATE_H_ */
