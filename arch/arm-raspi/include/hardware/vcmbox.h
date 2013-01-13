1f746ec73f29d44aeefae4350551980029582824
Kalamatee <kalamatee@gmail.com>
2013-01-13 19:36:23 +0000

    add a basic resource for videocore mailbox access.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46264 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/arm-raspi/include/hardware/vcmbox.h
@@ -0,0 +1,23 @@
+/*
+    Copyright © 2013, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#ifndef VCMB_H
+#define VCMB_H
+
+#define VCMB_CHANS      8
+#define VCMB_CHANMASK   0xF
+
+#define VCMB_BASE       0x2000B880
+#define VCMB_READ       0
+#define VCMB_POLL       16
+#define VCMB_SENDER     20
+#define VCMB_STATUS     24
+#define VCMB_CONFIG     28
+#define VCMB_WRITE      32
+
+#define VCMB_STATUS_READREADY   (1 << 30)
+#define VCMB_STATUS_WRITEREADY  (1 << 31)
+
+#endif	/* VCMB_H */
---
+++ arch/arm-raspi/vcmbox/vcmbox_private.h
@@ -0,0 +1,17 @@
+/*
+    Copyright © 2013, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#ifndef VCMB_PRIVATE_H_
+#define VCMB_PRIVATE_H_
+
+#include <exec/nodes.h>
+#include <inttypes.h>
+
+struct VCMBBase {
+    struct Node		vcmb_Node;
+};
+
+
+#endif /* VCMB_PRIVATE_H_ */
