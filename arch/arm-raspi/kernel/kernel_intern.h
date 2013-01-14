e786e993c8543d2ff487a5473f001f9c6867cf84
Kalamatee <kalamatee@gmail.com>
2013-01-14 17:22:29 +0000

    cleanup
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46274 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-raspi/kernel/kernel_intern.h
+++ arch/arm-raspi/kernel/kernel_intern.h
@@ -27,59 +27,7 @@
 
 struct KernelBase {
     struct Node         kb_Node;
-    void *              kb_MemPool;
-    struct List         kb_Exceptions[21];
-    struct List         kb_Interrupts[64];
-    struct MemHeader    *kb_SupervisorMem;
-    struct MinList		kb_Modules;
-//    context_t			*kb_FPUOwner;
-    struct List			kb_DeadTasks;
-    struct Task			*kb_LastDeadTask;
 };
-
-enum intr_types {
-    it_exception = 0xe0,
-    it_interrupt = 0xf0
-};
-
-struct IntrNode {
-    struct MinNode      in_Node;
-    void                (*in_Handler)(void *, void *);
-    void                *in_HandlerData;
-    void                *in_HandlerData2;
-    uint8_t             in_type;
-    uint8_t             in_nr;
-};
-
-/*
- * Exception handler differs a bit. It is suppost to have access to the CPU
- * context...
- */
-struct ExceptNode {
-    struct MinNode      in_Node;
-//    int                 (*in_Handler)(regs_t *, void *, void *);
-    void                *in_HandlerData;
-    void                *in_HandlerData2;
-    uint8_t             in_type;
-    uint8_t             in_nr;
-};
-
-typedef struct {
-	struct MinNode 	m_node;
-	char			*m_name;
-	char			*m_str;
-	intptr_t		m_lowest;
-	intptr_t		m_highest;
-	struct MinList	m_symbols;
-} module_t;
-
-typedef struct {
-	struct MinNode	s_node;
-	char			*s_name;
-	intptr_t		s_lowest;
-	intptr_t		s_highest;
-} symbol_t;
-
 
 void core_SetupMMU(void);
 void core_SetupIntr(void);
@@ -87,8 +35,6 @@ void core_SetupIntr(void);
 intptr_t krnGetTagData(Tag tagValue, intptr_t defaultVal, const struct TagItem *tagList);
 struct TagItem *krnFindTagItem(Tag tagValue, const struct TagItem *tagList);
 struct TagItem *krnNextTagItem(const struct TagItem **tagListPtr);
-
-//void core_Cause(struct ExecBase *SysBase);
 
 struct KernelBase *getKernelBase();
 
@@ -116,7 +62,5 @@ static inline void bug(const char *format, ...)
     AROS_SLIB_ENTRY(KrnBug, Kernel, 12)(format, args, kbase);
     va_end(args);
 }
-
-uint32_t findNames(intptr_t addr, char **module, char **function);
 
 #endif /*KERNEL_INTERN_H_*/
