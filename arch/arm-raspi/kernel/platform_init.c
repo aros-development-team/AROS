02e7d60526c1a1df89445d764607737f1fe11340
Kalamatee <kalamatee@gmail.com>
2013-01-17 20:57:24 +0000

    try to enable system timer 3
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46326 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-raspi/kernel/platform_init.c
+++ arch/arm-raspi/kernel/platform_init.c
@@ -20,12 +20,28 @@
 #include "etask.h"
 
 #include "kernel_intern.h"
-#include "kernel_fb.h"
+#include "kernel_arch.h"
 #include "kernel_romtags.h"
+
+/* Use system timer 3 for our scheduling heartbeat */
+#define VBLANK_TIMER 3
 
 static int PlatformInit(struct KernelBase *KernelBase)
 {
+    unsigned int stc;
     D(bug("[Kernel] PlatformInit()\n"));
+
+    // Let the fun begin ;)
+    // clock operates at 1000000hz
+    stc = *((volatile unsigned int *)(SYSTIMER_CLO));
+    stc += 20000; // (1000000 / 50)
+    *((volatile unsigned int *)(SYSTIMER_C0 + (VBLANK_TIMER * 4))) = stc;
+    *((volatile unsigned int *)(SYSTIMER_CS)) = (1 << VBLANK_TIMER);
+
+    ictl_enable_irq(IRQ_TIMER0 + VBLANK_TIMER, KernelBase);
+
+    D(bug("[Kernel] VBlank timer enabled\n"));
+
     return TRUE;
 }
 
