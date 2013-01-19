f6ebe6df9f1af01da6bbebce8929d8110277f4b0
Kalamatee <kalamatee@gmail.com>
2013-01-19 17:32:41 +0000

    rework the interrupt handling a little (broken atm) - and rename exception vector handlers
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46340 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-raspi/kernel/intvecs.s
+++ arch/arm-raspi/kernel/intvecs.s
@@ -13,10 +13,10 @@ _loop:   b       .
            ldr	pc, irq_handler_address
            ldr	pc, fiq_handler_address
 
-reset_handler_address:		       .word	__intrhand_reset
-undef_handler_address:	       .word	__intrhand_undef
-svc_handler_address:		       .word	__intrhand_swi
-prefetch_abort_handler_address:   .word	__intrhand_prefetchabort
-data_abort_handler_address:        .word	__intrhand_dataabort
-irq_handler_address:		       .word	__intrhand_irq
-fiq_handler_address:		       .word	__intrhand_fiq
+reset_handler_address:		       .word	__vectorhand_reset
+undef_handler_address:	       .word	__vectorhand_undef
+svc_handler_address:		       .word	__vectorhand_swi
+prefetch_abort_handler_address:   .word	__vectorhand_prefetchabort
+data_abort_handler_address:        .word	__vectorhand_dataabort
+irq_handler_address:		       .word	__vectorhand_irq
+fiq_handler_address:		       .word	__vectorhand_fiq
