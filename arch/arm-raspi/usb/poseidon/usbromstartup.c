bf0ab9b77a19a4514462e671b777b8d71de31bb7
Kalamatee <kalamatee@gmail.com>
2013-01-24 02:02:36 +0000

    add romstrat for poseidon with raspi hardware
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46371 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/arm-raspi/usb/poseidon/usbromstartup.c
@@ -0,0 +1,148 @@
+/* Very basic bootstrap for Poseidon in AROS kernel for enabling of USB booting and HID devices.
+ * PsdStackloader should be started during startup-sequence nonetheless */
+
+#define DEBUG 1
+
+#include <aros/asmcall.h>
+#include <aros/debug.h>
+#include <aros/symbolsets.h>
+#include <exec/resident.h>
+#include <proto/poseidon.h>
+#include <proto/exec.h>
+
+int __startup usbromstartup_entry(void)
+{
+    return -1;
+}
+
+static const char name[];
+static const char version[];
+static const UBYTE endptr;
+
+AROS_UFP3(static IPTR, usbromstartup_init,
+    AROS_UFHA(ULONG, dummy, D0),
+    AROS_UFHA(BPTR, seglist, A0),
+    AROS_UFHA(struct ExecBase *, SysBase, A6));
+
+const struct Resident usbHook =
+{
+    RTC_MATCHWORD,
+    (struct Resident *)&usbHook,
+    (APTR)&endptr,
+    RTF_COLDSTART,
+    41,
+    NT_TASK,
+    35,
+    name,
+    &version[5],
+    (APTR)usbromstartup_init
+};
+
+static const char name[] = "Poseidon ROM starter";
+static const char version[] = "$VER:Poseidon ROM startup v41.1";
+
+AROS_UFH3(static IPTR, usbromstartup_init,
+    AROS_UFHA(ULONG, dummy, D0),
+    AROS_UFHA(BPTR, seglist, A0),
+    AROS_UFHA(struct ExecBase *, SysBase, A6))
+{
+    AROS_USERFUNC_INIT
+
+    struct Library *ps;
+    struct PsdHardware *phw;
+    ULONG cnt = 0;
+
+    D(bug("[USBROMStartup] Loading poseidon...\n"));
+
+    if((ps = OpenLibrary("poseidon.library", 4)))
+    {
+        APTR msdclass;
+        IPTR usecount = 0;
+        ULONG bootdelay = 4;
+
+	D(bug("[USBROMStartup] Adding classes...\n"));
+
+        psdAddClass("hub.class", 0);
+        if(!(psdAddClass("hid.class", 0)))
+        {
+            psdAddClass("bootmouse.class", 0);
+            psdAddClass("bootkeyboard.class", 0);
+        }
+        msdclass = psdAddClass("massstorage.class", 0);
+
+        D(bug("[USBROMStartup] Added chipset drivers...\n"));
+
+        /* now this finds all usb hardware pci cards */
+//        while((phw = psdAddHardware("pciusb.device", cnt)))
+//        {
+//            D(bug("[USBROMStartup] Added pciusb.device unit %u\n", cnt));
+//
+//            psdEnumerateHardware(phw);
+//            cnt++;
+//        }
+
+//        if (cnt == 0) {
+            /* now this finds all other usb hardware pci cards */
+//            while((phw = psdAddHardware("ehci.device", cnt)))
+//            {
+//                D(bug("[USBROMStartup] Added ehci.device unit %u\n", cnt));
+
+//                psdEnumerateHardware(phw);
+//                cnt++;
+//            }
+//            cnt = 0;
+//            while((phw = psdAddHardware("ohci.device", cnt)))
+//            {
+//                D(bug("[USBROMStartup] Added ohci.device unit %u\n", cnt));
+
+//                psdEnumerateHardware(phw);
+//                cnt++;
+//            }
+//            cnt = 0;
+//            while((phw = psdAddHardware("uhci.device", cnt)))
+//            {
+//                D(bug("[USBROMStartup] Added uhci.device unit %u\n", cnt));
+
+//                psdEnumerateHardware(phw);
+//                cnt++;
+//            }
+//        }
+
+	D(bug("[USBROMStartup] Scanning classes...\n"));
+        psdClassScan();
+	D(bug("[USBROMStartup] classes enumerated\n"));
+
+        if(msdclass)
+        {
+            D(bug("[USBROMStartup] waiting for hubs..\n"));
+            psdDelayMS(1000); // wait for hubs to settle
+            D(bug("[USBROMStartup] checking for masstorage devices..\n"));
+            psdGetAttrs(PGA_USBCLASS, msdclass, UCA_UseCount, &usecount, TAG_END);
+            D(bug("[USBROMStartup] %d masstorage devices found\n", usecount));
+            if(usecount > 0)
+            {
+                D(bug("[USBROMStartup] adding boot delay\n"));
+
+                psdAddErrorMsg(RETURN_OK, (STRPTR)name,
+                               "Delaying further execution by %ld second(s) (boot delay).",
+                               bootdelay);
+                if(bootdelay > 1)
+                {
+                    psdDelayMS((bootdelay-1)*1000);
+                }
+            } else {
+                psdAddErrorMsg(RETURN_OK, (STRPTR)name, "Boot delay skipped, no mass storage devices found.");
+            }
+        }
+
+        D(bug("[USBROMStartup] cleaning up .. \n"));
+        CloseLibrary(ps);
+    }
+    return 0;
+
+    D(bug("[USBROMStartup] Finished...\n"));
+
+    AROS_USERFUNC_EXIT
+}
+
+static const UBYTE endptr = 0;
