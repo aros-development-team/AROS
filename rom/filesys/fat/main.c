a5b49c65893c9181a2fd97c0d2453dd1c45c5d42
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-01-29 02:08:15 +0000

    Fixed my own stupid bug, which led to a phantom FAT just past the last one being written to, potentially corrupting non-FAT blocks on FAT12 and FAT16 volumes.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46418 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/filesys/fat/main.c
+++ rom/filesys/fat/main.c
@@ -2,7 +2,7 @@
  * fat.handler - FAT12/16/32 filesystem handler
  *
  * Copyright © 2006 Marek Szyprowski
- * Copyright © 2007-2011 The AROS Development Team
+ * Copyright © 2007-2013 The AROS Development Team
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the same terms as AROS itself.
@@ -40,7 +40,7 @@ struct Library *IntuitionBase;
 struct Globals global_data;
 struct Globals *glob = &global_data;
 
-const TEXT version_string[] = "$VER: fat.handler 41.52 (28.12.2011)";
+const TEXT version_string[] = "$VER: fat.handler 41.55 (29.1.2013)";
 
 LONG handler(struct ExecBase *SysBase) {
     struct Message *msg;
