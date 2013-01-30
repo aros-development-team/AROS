914a1b0a511103393abd76bc334b351105a52803
Kalamatee <kalamatee@gmail.com>
2013-01-30 03:15:59 +0000

    prepare to build usable sync modes
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46422 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/arm-raspi/hidd.videocore/videocore_sdtv.c
@@ -0,0 +1,16 @@
+/*
+    Copyright © 2013, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#define DEBUG 1
+#include <aros/debug.h>
+
+#include "videocore_class.h"
+
+int VideoCore_SDTV_SyncGen(struct List *modelist)
+{
+    D(bug("[VideoCore] VideoCore_SDTV_SyncGen()\n"));
+
+    return 0;
+}
