4545ca3471415ff3deebbf7a456666d23f2c0120
Kalamatee <kalamatee@gmail.com>
2013-02-02 19:08:46 +0000

    re-enable the framebuffer
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46455 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-raspi/hidd.videocoregfx/videocoregfx_onbitmap.c
+++ arch/arm-raspi/hidd.videocoregfx/videocoregfx_onbitmap.c
@@ -91,86 +91,83 @@ OOP_Object *MNAME_ROOT(New)(OOP_Class *cl, OOP_Object *o, struct pRoot_New *msg)
         data->data = &XSD(cl)->data;
         data->mouse = &XSD(cl)->mouse;
 
-/*            RawPutChar(0x03);
+        RawPutChar(0x03);
 
-            (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[1] = VCTAG_REQ;
+        (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[1] = VCTAG_REQ;
 
-            (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[2] = VCTAG_SETRES;
-            (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[3] = 8;
-            (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[4] = 8;
-            (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[5] = data->width;
-            (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[6] = data->height;
+        (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[2] = VCTAG_SETRES;
+        (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[3] = 8;
+        (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[4] = 8;
+        (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[5] = data->width;
+        (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[6] = data->height;
 
-            (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[7] = VCTAG_SETVRES;      // duplicate physical size...
-            (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[8] = 8;
-            (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[9] = 8;
-            (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[10] = data->width;
-            (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[11] = data->height;
+        (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[7] = VCTAG_SETVRES;      // duplicate physical size...
+        (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[8] = 8;
+        (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[9] = 8;
+        (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[10] = data->width;
+        (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[11] = data->height;
 
-            (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[12] = VCTAG_SETDEPTH;
-            (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[13] = 4;
-            (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[14] = 4;
-            (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[15] = data->bpp;
+        (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[12] = VCTAG_SETDEPTH;
+        (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[13] = 4;
+        (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[14] = 4;
+        (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[15] = data->bpp;
 
-            (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[16] = VCTAG_FBALLOC;
-            (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[17] = 8;
-            (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[18] = 4;
-            (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[19] = 16;
-            (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[20] = 0;
+        (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[16] = VCTAG_FBALLOC;
+        (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[17] = 8;
+        (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[18] = 4;
+        (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[19] = 16;
+        (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[20] = 0;
 
-            (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[21] = 0;                 // terminate tags
+        (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[21] = 0;                 // terminate tags
 
-            (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[0] = (22 << 2);          // fill in request size
+        (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[0] = (22 << 2);          // fill in request size
 
-            VCMBoxWrite(VCMB_BASE, VCMB_FBCHAN, (unsigned int)(&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage);
-            if ((VCMBoxRead(VCMB_BASE, VCMB_FBCHAN) == (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage)
-                && ((&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[1] == VCTAG_RESP)
-                && ((&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[18] == (VCTAG_RESP + 8)))
-            {*/
-                IPTR hardbuffer;
- //               hardbuffer = (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[19];
-                hardbuffer = AllocVec( data->width * data->height * data->bytesperpix, MEMF_PUBLIC);
-                struct TagItem buffertags[] = {
-                    { aHidd_ChunkyBM_Buffer, hardbuffer},
-                    { TAG_DONE	     , 0    }
-                };
-                data->VideoData = buffertags[0].ti_Data;
-                OOP_SetAttrs(o, buffertags);
-                D(bug("[VideoCoreGfx] VideoCoreGfx.OnBitMap::New: FrameBuffer @ 0x%p\n", data->VideoData));
+        VCMBoxWrite(VCMB_BASE, VCMB_FBCHAN, (unsigned int)(&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage);
+        if ((VCMBoxRead(VCMB_BASE, VCMB_FBCHAN) == (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage)
+            && ((&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[1] == VCTAG_RESP)
+            && ((&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[18] == (VCTAG_RESP + 8)))
+        {
+             struct TagItem buffertags[] = {
+                { aHidd_ChunkyBM_Buffer, (&((struct VideoCoreGfxBase *)cl->UserData)->vsd)->vcsd_VCMBoxMessage[19]},
+                { TAG_DONE	     , 0    }
+            };
+            data->VideoData = buffertags[0].ti_Data;
+            OOP_SetAttrs(o, buffertags);
+            D(bug("[VideoCoreGfx] VideoCoreGfx.OnBitMap::New: FrameBuffer @ 0x%p\n", data->VideoData));
 
-                int x,y;
-                for (y = (data->height/2) - 10; y <(data->height/2) + 10; y++)
+#if (0)
+            int x,y;
+            for (y = (data->height/2) - 10; y <(data->height/2) + 10; y++)
+            {
+                for (x = (data->width/2) - 10; x <(data->width/2) + 10; x++)
                 {
-                    for (x = (data->width/2) - 10; x <(data->width/2) + 10; x++)
+                    void *p = data->VideoData + (y * (data->width * data->bytesperpix) + (x * data->bytesperpix));
+
+                    switch (data->bytesperpix)
                     {
-                        void *p = data->VideoData + (y * (data->width * data->bytesperpix) + (x * data->bytesperpix));
+                    case 4:
+                        *((int *)p) = 0x00FF0000;
+                        break;
 
-                        switch (data->bytesperpix)
-                        {
-                        case 4:
-                            *((int *)p) = 0x00FF0000;
-                            break;
+                    case 3:
+                        /* qemu's truecolor modes are known to be 3 bytes per pixel */
+                        *((short *)p) = 0x0000;
+                        *((char *)p + 2) = 0xFF;
+                        break;
 
-                        case 3:
-                            /* qemu's truecolor modes are known to be 3 bytes per pixel */
-                            *((short *)p) = 0x0000;
-                            *((char *)p + 2) = 0xFF;
-                            break;
+                    case 2:
+                        *((short *)p) = 0xF800;
+                        break;
 
-                        case 2:
-                            *((short *)p) = 0xF800;
-                            break;
-
-                        case 1:
-                            *((char *)p) = 0xF0;
-                            break;
-                        }
+                    case 1:
+                        *((char *)p) = 0xF0;
+                        break;
                     }
                 }
-
-                ReturnPtr("VideoCoreGfx.OnBitMap::New: Obj", OOP_Object *, o);
-/*
-            }*/
+            }
+#endif
+            ReturnPtr("VideoCoreGfx.OnBitMap::New: Obj", OOP_Object *, o);
+        }
 
         {
             OOP_MethodID disp_mid = OOP_GetMethodID(IID_Root, moRoot_Dispose);
