feca0efa51ed2d7143b4e7df99e546ac66abe82b
Kalamatee <kalamatee@gmail.com>
2013-02-02 18:52:42 +0000

    patch AllocMem since we dont really expose/use MEMF_CHIP except privately for the GPU memory
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46454 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-raspi/kernel/platform_init.c
+++ arch/arm-raspi/kernel/platform_init.c
@@ -22,13 +22,46 @@
 #include "kernel_arch.h"
 #include "kernel_romtags.h"
 
+#define DALLOCMEM(x)
+
+void *(*__AllocMem)();
+
+#define ExecAllocMem(bytesize, requirements)        \
+AROS_CALL2(void *, __AllocMem,                      \
+		AROS_LCA(ULONG, byteSize,     D0),			\
+		AROS_LCA(ULONG, requirements, D1),			\
+		struct ExecBase *, SysBase)
+
+AROS_LH2(APTR, AllocMem,
+	AROS_LHA(ULONG, byteSize,     D0),
+	AROS_LHA(ULONG, requirements, D1),
+	struct ExecBase *, SysBase, 33, Kernel)
+{
+    AROS_LIBFUNC_INIT
+
+    if (requirements & MEMF_CHIP)
+    {
+        DALLOCMEM(bug("[Kernel] AllocMem: Ignoring MEMF_CHIP flag\n"));
+        requirements &= ~MEMF_CHIP;
+    }
+    return ExecAllocMem(bytesize, requirements);
+
+    AROS_LIBFUNC_EXIT
+}
+
 static int PlatformInit(struct KernelBase *KernelBase)
 {
     D(bug("[Kernel] PlatformInit()\n"));
 
+    D(bug("[Kernel] PlatformInit: Patching in our AllocMem to ignore MEMF_CHIP..\n"));
+    
+    __AllocMem = SetFunction(SysBase, -33*LIB_VECTSIZE, AROS_SLIB_ENTRY(AllocMem, Kernel, 33));
+
+    D(bug("[Kernel] PlatformInit: Registering Heartbeat timer..\n"));
+
     KrnAddSysTimerHandler(KernelBase);
 
-    D(bug("[Kernel] PlatformInit: DOne..\n"));
+    D(bug("[Kernel] PlatformInit: Done..\n"));
 
     return TRUE;
 }
