edf9978a06f14e768fce06fb7d5a664f04bda78a
ezrec <jason.mcmullan@gmail.com>
2013-02-03 03:47:26 +0000

    nvdisk.library: Make ROMable for m68k
    For now, it will place "nonvolatile"s in RAM:NVRAM/ if there is no
    ENV-ARCHIVE:SYS/nv_location preference.
    
    In the future, we should use the CD32's NVRAM on m68k to back
    nonvolatile.library
    
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46475 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/nonvolatile/nvdisk/nvdisk_init.c
+++ workbench/libs/nonvolatile/nvdisk/nvdisk_init.c
@@ -37,6 +37,10 @@ static int Init(LIBBASETYPEPTR LIBBASE)
     BOOL error = TRUE;    // Notice the initialization to 'TRUE' here.
     char *temp = NULL;
     BPTR locFile;
+
+    LIBBASE->nvd_DOSBase = OpenLibrary("dos.library",0);
+    if (LIBBASE->nvd_DOSBase == NULL)
+        return 0;
     
     D(bug("Init nvdisk.library\n"));
 
@@ -46,43 +50,46 @@ static int Init(LIBBASETYPEPTR LIBBASE)
 
     if(locFile != BNULL)
     {
-	D(bug("Location file exists!\n"));
-	
-	temp = AllocVec(512, MEMF_CLEAR);
-		
-	if(temp != NULL)
-	{
-	    int i = 0;         // Loop variable
+        D(bug("Location file exists!\n"));
+        
+        temp = AllocVec(512, MEMF_CLEAR);
+                
+        if(temp != NULL)
+        {
+            int i = 0;         // Loop variable
 
-	    Read(locFile, temp, 512);
+            Read(locFile, temp, 512);
 
-	    // End string if a carriage return is encountered
-	    while(temp[i] != 0)
-	    {
-		if(temp[i] == '\n')
-		{
-		    temp[i] = 0;
-		    break;
-		}
+            // End string if a carriage return is encountered
+            while(temp[i] != 0)
+            {
+                if(temp[i] == '\n')
+                {
+                    temp[i] = 0;
+                    break;
+                }
 
-		i++;
-	    }
+                i++;
+            }
 
-	    nvdBase->nvd_location = Lock(temp, SHARED_LOCK);
-		    
-	    D(bug("NV location = %s\n", temp));
+            nvdBase->nvd_location = Lock(temp, SHARED_LOCK);
+                    
+            D(bug("NV location = %s\n", temp));
 
-	    FreeVec(temp);
-		    
-	    D(bug("Got lock = %p\n", nvdBase->nvd_location));
+            FreeVec(temp);
+        }    
+        Close(locFile);
+    } else {
+        /* Volatile mode, if we have no nv_location file */
+        UnLock(CreateDir("RAM:NVRAM"));
+        nvdBase->nvd_location = Lock("RAM:NVRAM", SHARED_LOCK);
+    }
 
-	    if(nvdBase->nvd_location != BNULL)
-	    {
-		error = FALSE;
-	    }
-	}
-	    
-	Close(locFile);
+    D(bug("Got lock = %p\n", nvdBase->nvd_location));
+
+    if(nvdBase->nvd_location != BNULL)
+    {
+        error = FALSE;
     }
 
     return !error;
@@ -91,11 +98,13 @@ static int Init(LIBBASETYPEPTR LIBBASE)
 static int Expunge(LIBBASETYPEPTR LIBBASE)
 {
     /*
-	This function is single-threaded by exec by calling Forbid.
-	Never break the Forbid() or strange things might happen.
+        This function is single-threaded by exec by calling Forbid.
+        Never break the Forbid() or strange things might happen.
     */
 
     UnLock(nvdBase->nvd_location);
+
+    CloseLibrary(nvdBase->nvd_DOSBase);
     
     return TRUE;
 }
