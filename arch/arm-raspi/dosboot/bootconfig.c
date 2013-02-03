7f02595d42bf2950df37ddbf94218512edb14570
Kalamatee <kalamatee@gmail.com>
2013-02-03 05:05:38 +0000

    remove unnecessary hack
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46481 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-raspi/dosboot/bootconfig.c
+++
@@ -1,9 +0,0 @@
-#include "dosboot_intern.h"
-
-/* This file contains architecture-dependent defaults */
-
-void InitBootConfig(struct BootConfig *bootcfg)
-{
-    bootcfg->gfxlib  = "videocore.hidd";
-    bootcfg->gfxhidd = "VIDEOCOREDriver";
-}
