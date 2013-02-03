423de6e75beda3b466fc0162e53a6339e9b0dd72
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-02-03 20:49:35 +0000

    Converted to use unified subsystem API
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46492 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/hidds/mouse/driverdata.c
@@ -0,0 +1,118 @@
+#include <aros/debug.h>
+#include <hidd/mouse.h>
+#include <oop/oop.h>
+#include <proto/utility.h>
+
+#include "mouse.h"
+
+/*
+ * This file contains private DriverData class.
+ * The problem is that together with the actual driver object we have
+ * to store some information about the driver, together with the callback.
+ * Unfortunately our drivers do not have any base class, so we cannot just
+ * add fields to its structure. In order to circumvent this limitation,
+ * we use proxy objects instead of real driver objects. Proxy object
+ * encapsulates driver object together with all the needed information.
+ * (De)masquerading drivers is done in subsystem class.
+ */
+
+static void GlobalCallback(struct driverNode *drv, struct pHidd_Mouse_ExtEvent *ev)
+{
+    struct pHidd_Mouse_ExtEvent xev;
+    struct mouse_data *data;
+
+    /* Ignore the event if the driver is not activated yet */
+    if (!drv->callbacks)
+        return;
+
+    /*
+     * The event passed in may be pHidd_Mouse_Event instead of pHidd_Mouse_ExtEvent,
+     * according to flags. In this case we add own flags
+     */
+    if (drv->flags != vHidd_Mouse_Extended)
+    {
+        xev.button = ev->button;
+        xev.x      = ev->x;
+        xev.y      = ev->y;
+        xev.type   = ev->type;
+        xev.flags  = drv->flags;
+        
+        ev = &xev;
+    }
+
+    for (data = (struct mouse_data *)drv->callbacks->mlh_Head; data->node.mln_Succ;
+         data = (struct mouse_data *)data->node.mln_Succ)
+    {
+        data->callback(data->callbackdata, ev);
+    }
+}
+
+OOP_Object *DriverData__Root__New(OOP_Class *cl, OOP_Object *o, struct pRoot_New *msg)
+{
+    struct Library *OOPBase = CSD(cl)->cs_OOPBase;
+    struct Library *UtilityBase = CSD(cl)->cs_UtilityBase;
+    OOP_Class *driverClass;
+
+    driverClass = (OOP_Class *)GetTagData(aHidd_Mouse_ClassPtr, 0, msg->attrList);
+    D(bug("[Mouse] AddHardwareDriver(0x%p)\n", driverClass));
+
+    o = (OOP_Object *)OOP_DoSuperMethod(cl, o, &msg->mID);
+    if (o)
+    {
+        struct driverNode *drvnode = OOP_INST_DATA(cl, o);
+        struct TagItem tags[] =
+        {
+            { aHidd_Mouse_IrqHandler    , (IPTR)GlobalCallback},
+            { aHidd_Mouse_IrqHandlerData, (IPTR)drvnode       },
+            { TAG_MORE                  , (IPTR)msg->attrList }
+        };
+
+        drvnode->drv = OOP_NewObject(driverClass, NULL, tags);
+        D(bug("[Mouse] Driver node 0x%p, driver 0x%p\n", drvnode, drvnode->drv));
+
+        if (drvnode->drv)
+        {
+            struct mouse_staticdata *csd = CSD(cl);
+            IPTR val = FALSE;
+
+            OOP_GetAttr(drvnode->drv, aHidd_Mouse_Extended, &val);
+            D(bug("[Mouse] Extended event: %d\n", val));
+            if (val)
+            {
+                drvnode->flags = vHidd_Mouse_Extended;
+            }
+            else
+            {
+                OOP_GetAttr(drvnode->drv, aHidd_Mouse_RelativeCoords, &val);
+                D(bug("[Mouse] Relative coordinates: %d\n", val));
+                drvnode->flags = val ? vHidd_Mouse_Relative : 0;
+            }
+            /* This enables sending interrupts to clients */
+            drvnode->callbacks = &csd->callbacks;
+
+            return o;
+        }
+        else
+        {
+            /*
+             * One more trick saving us from headache with OOP_GetMethodID():
+             * Given a method ID, we can obtain MethodBase of the same
+             * interface by simply subtracting method offset.
+             */
+            OOP_MethodID rootMethodBase = msg->mID - moRoot_New;
+            OOP_MethodID dispose_msg = rootMethodBase + moRoot_Dispose;
+
+            OOP_DoSuperMethod(cl, o, &dispose_msg);
+        }
+    }
+    return NULL;
+}
+
+void DriverData__Root__Dispose(OOP_Class *cl, OOP_Object *o, OOP_Msg msg)
+{
+    struct Library *OOPBase = CSD(cl)->cs_OOPBase;
+    struct driverNode *drvnode = OOP_INST_DATA(cl, o);
+
+    OOP_DisposeObject(drvnode->drv);
+    OOP_DoSuperMethod(cl, o, msg);
+}
---
+++ rom/hidds/mouse/subsystem.c
@@ -0,0 +1,202 @@
+/*
+    Copyright (C) 2013, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#define __OOP_NOATTRBASES__
+
+#include <aros/debug.h>
+#include <hidd/hidd.h>
+#include <hidd/mouse.h>
+#include <oop/oop.h>
+#include <utility/hooks.h>
+#include <utility/tagitem.h>
+
+#include "mouse.h"
+
+/*****************************************************************************************
+
+    NAME
+	--background--
+
+    LOCATION
+	CLID_Hidd_Mouse
+
+    NOTES
+	This class represents a "hub" for collecting input from various
+	pointing devices (mice, tablets, touchscreens, etc) in the
+	system. Events from all pointing devices are merged into a
+        single stream and propagated to all clients.
+
+	In order to get an access to pointing input subsystem you need to
+	create an object of CLID_Hidd_Mouse class. The actual returned
+        object is a singletone, you do not have to dispose it, and every
+        call will return the same object pointer. After getting this object
+        you can, for example, register your driver using moHW_AddDriver
+        method, or enumerate drivers using moHW_EnumDrivers.
+
+        If you wish to receive keyboard events, use objects of CLID_Hidd_Mouse
+        class. This class implements the same interface as driver class, but
+        represents receiver's side and is responsible for registering user's
+        interrupt handler in the listeners chain. These objects are not real
+        drivers and do not need to me registered within the subsystem.
+
+*****************************************************************************************/
+
+/*****************************************************************************************
+
+    NAME
+        --hardware_drivers--
+
+    LOCATION
+        CLID_HW_Mouse
+
+    NOTES
+        A hardware driver should be a subclass of CLID_Hidd and implement IID_Hidd_Mouse
+        interface according to the following rules:
+
+        1. A single object of driver class represents a single hardware unit.
+        2. A single driver object maintains a single callback address (passed to it
+           using aoHidd_Mouse_IrqHandler). Under normal conditions this callback is
+           supplied by CLID_HW_Mouse class.
+
+*****************************************************************************************/
+
+OOP_Object *MouseHW__Root__New(OOP_Class *cl, OOP_Object *o, struct pRoot_New *msg)
+{
+    struct mouse_staticdata *csd = CSD(cl);
+
+    if (!csd->hwObject)
+    {
+        struct TagItem new_tags[] =
+        {
+            {aHW_ClassName, (IPTR)"Pointing devices"},
+            {TAG_DONE     , 0                       }
+        };
+        struct pRoot_New new_msg =
+        {
+            .mID      = msg->mID,
+            .attrList = new_tags
+        };
+
+        csd->hwObject = (OOP_Object *)OOP_DoSuperMethod(cl, o, &new_msg.mID);
+    }
+    return csd->hwObject;
+}
+
+VOID MouseHW__Root__Dispose(OOP_Class *cl, OOP_Object *o, OOP_Msg msg)
+{
+
+}
+
+OOP_Object *MouseHW__HW__AddDriver(OOP_Class *cl, OOP_Object *o, struct pHW_AddDriver *Msg)
+{
+    /*
+     * Instantiate a proxy object instead of driver. The proxy object
+     * will create driver itself.
+     * Since driver object creation/disposition is handled by proxy,
+     * we do not have to use Setup/Cleanup methods in this class.
+     */
+    struct TagItem tags[] =
+    {
+        {aHidd_Mouse_ClassPtr, (IPTR)Msg->driverClass},
+        {TAG_MORE            , (IPTR)Msg->tags       }
+    };
+    struct pHW_AddDriver add_msg =
+    {
+        .mID         = Msg->mID,
+        .driverClass = CSD(cl)->dataClass,
+        .tags        = tags
+    };
+
+    return (OOP_Object *)OOP_DoSuperMethod(cl, o, &add_msg.mID);
+}
+
+/*
+ * For simplicity and speed our hooks access fields of DriverNode class
+ * directly. OOP_Object * is directly cast to struct driver Node *.
+ * This is perfectly OK becuase:
+ * a) This is our private class, nobody is going to mess with it.
+ * b) It does not have any base classes, and is derived directly from rootclass.
+ * In C++ terminology it's our friend class.
+ */
+AROS_UFH3(static void, searchFunc,
+    AROS_UFHA(struct Hook *, h,  A0),
+    AROS_UFHA(struct driverNode *, dn, A2),
+    AROS_UFHA(OOP_Object *, wanted, A1))
+{
+    AROS_USERFUNC_INIT
+
+    if (dn->drv == wanted)
+        h->h_Data = dn;
+    
+    AROS_USERFUNC_EXIT
+}
+
+BOOL MouseHW__HW__RemoveDriver(OOP_Class *cl, OOP_Object *o, struct pHW_RemoveDriver *Msg)
+{
+    /*
+     * We are given driver object, and now we need to look up
+     * its proxy. We use moHW_EnumDrivers method for this purpose.
+     * Our hook's h_Data will be filled in with proxy pointer.
+     * This double traversing objects list is what i really dislike
+     * in this design. An alternative would be to add some means
+     * to extend struct DriverNode in hidd/hwclass.c, however this
+     * would be also ugly, since it's private data.
+     * Looks like we are not going to have this problem anywhere else.
+     * Other subsystems (PCI, I2C) have base class for its drivers, and
+     * the needed data can be simply encapsulated there.
+     */
+
+    struct Hook searchHook =
+    {
+        .h_Entry = (HOOKFUNC)searchFunc,
+        .h_Data  = NULL
+    };
+
+    HW_EnumDrivers(o, &searchHook, Msg->driverObject);
+    
+    if (searchHook.h_Data)
+    {
+        struct pHW_RemoveDriver rem_msg =
+        {
+            .mID = Msg->mID,
+            .driverObject = searchHook.h_Data
+        };
+        
+        return OOP_DoSuperMethod(cl, o, &rem_msg.mID);
+    }
+    return FALSE;
+}
+
+AROS_UFH3(static void, enumFunc,
+    AROS_UFHA(struct Hook *, h,  A0),
+    AROS_UFHA(struct driverNode *, dn, A2),
+    AROS_UFHA(OOP_Object *, hookMsg, A1))
+{
+    AROS_USERFUNC_INIT
+
+    struct Hook *user_hook = h->h_Data;
+
+    D(bug("[Mouse] Enum: node 0x%p driver 0x%p\n", dn, dn->drv));
+    CALLHOOKPKT(user_hook, dn->drv, hookMsg);
+    
+    AROS_USERFUNC_EXIT
+}
+
+void MouseHW__HW__EnumDrivers(OOP_Class *cl, OOP_Object *o, struct pHW_EnumDrivers *Msg)
+{
+    struct Hook enumHook =
+    {
+        .h_Entry = (HOOKFUNC)enumFunc,
+        .h_Data  = Msg->callback
+    };
+    struct pHW_EnumDrivers enum_msg =
+    {
+        .mID      = Msg->mID,
+        .callback = &enumHook,
+        .hookMsg  = Msg->hookMsg
+    };
+    
+    OOP_DoSuperMethod(cl, o, &enum_msg.mID);
+}
