b64cdfde43079256433c78f1a7fcb94b7fbe2793
Kalamatee <kalamatee@gmail.com>
2013-02-03 01:55:55 +0000

    w.i.p - stubs for DesignWare® Hi-Speed USB 2.0 On-The-Go (HS OTG) Controller driver for Raspberry Pi (BCM2835)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46465 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/arm-raspi/usb/usb2otg/usb2otg_debug.h
@@ -0,0 +1,35 @@
+/*
+    Copyright © 2013, The AROS Development Team. All rights reserved.
+    $Id$
+
+    This file can be included multiple times with different DB_LEVEL
+*/
+#undef DB
+#undef KPRINTF
+
+#ifndef DB_LEVEL
+#define DB_LEVEL 200
+#endif
+
+//#define DEBUG 1
+
+#include <proto/debug.h>
+
+// DEBUG 0 should equal undefined DEBUG
+#ifdef DEBUG
+#if DEBUG == 0
+#undef DEBUG
+#endif
+#endif
+
+#ifdef DEBUG
+#define KPRINTF(l, x) do { if ((l) >= DB_LEVEL) \
+     { KPrintF("%s/%lu: ", __FUNCTION__, __LINE__); KPrintF x;} } while (0)
+#define DB(x) x
+void dumpmem_usb2otg(void *mem, unsigned long int len);
+#else /* !DEBUG */
+
+#define KPRINTF(l, x) ((void) 0)
+#define DB(x)
+
+#endif /* DEBUG */
