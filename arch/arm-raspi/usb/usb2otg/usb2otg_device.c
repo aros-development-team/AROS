29d801b7abb825b162bcb6fc79b572563d501d31
Kalamatee <kalamatee@gmail.com>
2013-02-05 02:16:36 +0000

    check for hs otg usb controller and dump settings. disable global usb irqs, and register irq handler.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46507 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-raspi/usb/usb2otg/usb2otg_device.c
+++ arch/arm-raspi/usb/usb2otg/usb2otg_device.c
@@ -3,10 +3,15 @@
     $Id$
 */
 
-#include "usb2otg_debug.h"
+#define DEBUG 1
+#include <aros/debug.h>
 
 #include <proto/exec.h>
+#include <proto/kernel.h>
 #include <proto/utility.h>
+
+#include <asm/bcm2835.h>
+#include <hardware/usb2otg.h>
 
 #include "usb2otg_intern.h"
 
@@ -19,6 +24,12 @@
 
 const char devname[]    = MOD_NAME_STRING;
 
+static void GlobalIRQHandler(struct USB2OTGUnit *USBUnit, struct ExecBase *SysBase)
+{
+    D(bug("[USB2OTG] %s: Received USB interrupt for Unit @ 0x%p\n",
+                 __PRETTY_FUNCTION__, USBUnit));
+}
+
 /*
  *===========================================================
  * Init(base)
@@ -26,39 +37,110 @@ const char devname[]    = MOD_NAME_STRING;
  */
 static int FNAME_DEV(Init)(LIBBASETYPEPTR USB2OTGBase)
 {
-    KPRINTF(10, ("%s: USB2OTGBase @ 0x%p, SysBase @ 0x%p\n",
+    unsigned int usbregval;
+
+    D(bug("[USB2OTG] %s: USB2OTGBase @ 0x%p, SysBase @ 0x%p\n",
                  __PRETTY_FUNCTION__, USB2OTGBase, SysBase));
 
-    if ((USB2OTGBase->hd_UtilityBase = (APTR)OpenLibrary("utility.library", 39)) != NULL)
+    usbregval = *((volatile unsigned int *)USB2OTG_VENDORID);
+
+    if ((usbregval & 0xFFFFF000) != 0x4F542000)
     {
-        USB2OTGBase->hd_MemPool = CreatePool(MEMF_PUBLIC | MEMF_CLEAR | MEMF_SEM_PROTECTED, 16384, 4096);
-        if (USB2OTGBase->hd_MemPool)
-        {
-            NewList(&USB2OTGBase->hd_Units);
-
-            KPRINTF(10, ("%s: Initialisation Ok\n",
-                        __PRETTY_FUNCTION__));
-
-        }
-        else
-        {
-            KPRINTF(10, ("%s: CreatePool() failed!\n",
-                        __PRETTY_FUNCTION__));
-
-            CloseLibrary((struct Library *) UtilityBase);
-            USB2OTGBase = NULL;
-        }
-    }
-    else
-    {
-        KPRINTF(10, ("%s: OpenLibrary(\"utility.library\", 39) failed!\n",
-                    __PRETTY_FUNCTION__));
+        D(bug("[USB2OTG] %s: No Supported HS OTG USB Core Found\n",
+                __PRETTY_FUNCTION__));
 
         USB2OTGBase = NULL;
     }
+    else
+    {
+        USB2OTGBase->hd_KernelBase = OpenResource("kernel.resource");
+        D(bug("[USB2OTG] %s: kernel.resource opened @ 0x%p\n",
+                __PRETTY_FUNCTION__, USB2OTGBase->hd_KernelBase));
 
-    KPRINTF(10, ("%s: OpenCnt = %ld\n",
+        D(bug("[USB2OTG] %s: HS OTG Core Release: %x.%x%x%x\n",
+                    __PRETTY_FUNCTION__, ((usbregval >> 12) & 0xF), ((usbregval >> 8) & 0xF), ((usbregval >> 4) & 0xF), (usbregval & 0xF)));
+
+        if ((USB2OTGBase->hd_UtilityBase = (APTR)OpenLibrary("utility.library", 39)) != NULL)
+        {
+            USB2OTGBase->hd_MemPool = CreatePool(MEMF_PUBLIC | MEMF_CLEAR | MEMF_SEM_PROTECTED, 16384, 4096);
+            if (USB2OTGBase->hd_MemPool)
+            {
+                int chan;
+
+                D(bug("[USB2OTG] %s: Allocated MemPool @ 0x%p\n",
+                            __PRETTY_FUNCTION__, USB2OTGBase->hd_MemPool));
+
+                for (chan = 0; chan < EPSCHANS_MAX; chan++) {
+                    D(bug("[USB2OTG] %s: Host Chan #%d FIFO @ 0x%p, characterisitics: %08x\n",
+                                __PRETTY_FUNCTION__,
+                                chan, USB2OTG_FIFOBASE + (chan * USB2OTG_FIFOSIZE),
+                                *((volatile unsigned int *)(USB2OTG_HOST_CHANBASE + (chan * USB2OTG_HOST_CHANREGSIZE) + USB2OTG_HOSTCHAN_CHARBASE))));
+                }
+
+                for (chan = 0; chan < EPSCHANS_MAX; chan++) {
+                    D(bug("[USB2OTG] %s: Device Endpoint #%d IN_CTL: %08x, OUT_CTL: %08x\n",
+                                __PRETTY_FUNCTION__, chan,
+                                *((volatile unsigned int *)(USB2OTG_DEV_INEP_BASE + (chan * USB2OTG_DEV_EPSIZE) + USB2OTG_DEV_INEP_DIEPCTL)),
+                                *((volatile unsigned int *)(USB2OTG_DEV_OUTEP_BASE + (chan * USB2OTG_DEV_EPSIZE) + USB2OTG_DEV_OUTEP_DOEPCTL))));
+                }
+
+                D(bug("[USB2OTG] %s: HWConfig1: %08x\n",
+                            __PRETTY_FUNCTION__, *((volatile unsigned int *)USB2OTG_HARDWARE)));
+                D(bug("[USB2OTG] %s: HWConfig2: %08x\n",
+                            __PRETTY_FUNCTION__, *((volatile unsigned int *)USB2OTG_LPMCONFIG)));
+                D(bug("[USB2OTG] %s: HWConfig3: %08x\n",
+                            __PRETTY_FUNCTION__, *((volatile unsigned int *)USB2OTG_LPMCONFIG + 4)));
+                D(bug("[USB2OTG] %s: HWConfig4: %08x\n",
+                            __PRETTY_FUNCTION__, *((volatile unsigned int *)USB2OTG_LPMCONFIG + 8)));
+
+                D(bug("[USB2OTG] %s: Host Mode Config: %08x\n",
+                            __PRETTY_FUNCTION__, *((volatile unsigned int *)USB2OTG_HOSTCFG)));
+                D(bug("[USB2OTG] %s: Device Mode Config: %08x\n",
+                            __PRETTY_FUNCTION__, *((volatile unsigned int *)USB2OTG_DEVCFG)));
+
+                D(bug("[USB2OTG] %s: Disabling USB Interrupts (Globaly)..\n", __PRETTY_FUNCTION__));
+                usbregval = *((volatile unsigned int *)USB2OTG_AHB);
+                usbregval &= ~USB2OTG_AHB_INTENABLE;
+                *((volatile unsigned int *)USB2OTG_AHB) = usbregval;
+                D(bug("[USB2OTG] %s: AHB : %08x\n", __PRETTY_FUNCTION__, *((volatile unsigned int *)USB2OTG_AHB)));
+
+                USB2OTGBase->hd_Unit = AllocPooled(USB2OTGBase->hd_MemPool, sizeof(struct USB2OTGUnit));
+
+                D(bug("[USB2OTG] %s: Unit Allocated at 0x%p\n",
+                            __PRETTY_FUNCTION__, USB2OTGBase->hd_Unit));
+
+                USB2OTGBase->hd_Unit->hu_GlobalIRQHandle = KrnAddIRQHandler(IRQ_VC_USB, GlobalIRQHandler, USB2OTGBase->hd_Unit, SysBase);
+
+                D(bug("[USB2OTG] %s: Installed Global IRQ Handler [handle @ 0x%p]\n",
+                            __PRETTY_FUNCTION__, USB2OTGBase->hd_Unit->hu_GlobalIRQHandle));
+
+                D(bug("[USB2OTG] %s: HS OTG Driver Initialised\n",
+                            __PRETTY_FUNCTION__));
+
+            }
+            else
+            {
+                D(bug("[USB2OTG] %s: Failed to Create MemPool\n",
+                            __PRETTY_FUNCTION__));
+
+                CloseLibrary((struct Library *) UtilityBase);
+                USB2OTGBase = NULL;
+            }
+        }
+        else
+        {
+            D(bug("[USB2OTG] %s: OpenLibrary(\"utility.library\", 39) failed!\n",
+                        __PRETTY_FUNCTION__));
+
+            USB2OTGBase = NULL;
+        }
+    }
+
+    D(bug("[USB2OTG] %s: OpenCnt = %ld\n",
                 __PRETTY_FUNCTION__, USB2OTGBase->hd_Library.lib_OpenCnt));
+
+    while (1)
+        asm volatile("mov r0, r0\n");
 
     return USB2OTGBase ? TRUE : FALSE;
 }
@@ -73,15 +155,15 @@ static int FNAME_DEV(Init)(LIBBASETYPEPTR USB2OTGBase)
  */
 static int FNAME_DEV(Open)(LIBBASETYPEPTR USB2OTGBase, struct IOUsbHWReq *ioreq, ULONG unit, ULONG flags)
 {
-    KPRINTF(10, ("%s: IOReq @ 0x%p, unit #%ld, flags = 0x%08lx, USB2OTGBase @ 0x%p\n",
+    D(bug("[USB2OTG] %s: IOReq @ 0x%p, unit #%ld, flags = 0x%08lx, USB2OTGBase @ 0x%p\n",
                 __PRETTY_FUNCTION__, ioreq, unit, flags, USB2OTGBase));
 
-    KPRINTF(10, ("%s: openCnt = %ld\n",
+    D(bug("[USB2OTG] %s: openCnt = %ld\n",
                 __PRETTY_FUNCTION__, USB2OTGBase->hd_Library.lib_OpenCnt));
 
     if (ioreq->iouh_Req.io_Message.mn_Length < sizeof(struct IOUsbHWReq))
     {
-        KPRINTF(20, ("%s: invalid MN_LENGTH!\n",
+        D(bug("[USB2OTG] %s: invalid MN_LENGTH!\n",
                     __PRETTY_FUNCTION__));
 
         ioreq->iouh_Req.io_Error = IOERR_BADLENGTH;
@@ -93,7 +175,7 @@ static int FNAME_DEV(Open)(LIBBASETYPEPTR USB2OTGBase, struct IOUsbHWReq *ioreq,
 //        ioreq->iouh_Req.io_Unit = Open_Unit(ioreq, unit, USB2OTGBase);
         if (!(ioreq->iouh_Req.io_Unit))
         {
-            KPRINTF(20, ("%s: could not open unit!\n",
+            D(bug("[USB2OTG] %s: could not open unit!\n",
                         __PRETTY_FUNCTION__));
 
         }
@@ -121,7 +203,7 @@ static int FNAME_DEV(Open)(LIBBASETYPEPTR USB2OTGBase, struct IOUsbHWReq *ioreq,
  */
 static int FNAME_DEV(Close)(LIBBASETYPEPTR USB2OTGBase, struct IOUsbHWReq *ioreq)
 {
-    KPRINTF(10, ("%s: IOReq @ 0x%p, USB2OTGBase @ 0x%p\n",
+    D(bug("[USB2OTG] %s: IOReq @ 0x%p, USB2OTGBase @ 0x%p\n",
                 __PRETTY_FUNCTION__, ioreq, USB2OTGBase));
 
 //    Close_Unit(USB2OTGBase, (struct USB2OTGUnit *) ioreq->iouh_Req.io_Unit, ioreq);
@@ -131,12 +213,11 @@ static int FNAME_DEV(Close)(LIBBASETYPEPTR USB2OTGBase, struct IOUsbHWReq *ioreq
     return TRUE;
 }
 
-
 static int FNAME_DEV(Expunge)(LIBBASETYPEPTR USB2OTGBase)
 {
     DeletePool(USB2OTGBase->hd_MemPool);
 
-    KPRINTF(5, ("%s: closing utility.library @ 0x%p\n",
+    D(bug("[USB2OTG] %s: closing utility.library @ 0x%p\n",
                 __PRETTY_FUNCTION__, UtilityBase));
 
     CloseLibrary((struct Library *) UtilityBase);
@@ -165,7 +246,7 @@ AROS_LH1(void, FNAME_DEV(BeginIO),
     struct USB2OTGUnit *unit = (struct USB2OTGUnit *) ioreq->iouh_Req.io_Unit;
     WORD ret;
 
-    KPRINTF(1, ("%s: IOReq @ 0x%08lx, USB2OTGBase @ 0x%08lx, cmd: %lu\n",
+    D(bug("[USB2OTG] %s: IOReq @ 0x%08lx, USB2OTGBase @ 0x%08lx, cmd: %lu\n",
                 __PRETTY_FUNCTION__, ioreq, USB2OTGBase, ioreq->iouh_Req.io_Command));
 
     ioreq->iouh_Req.io_Message.mn_Node.ln_Type = NT_MESSAGE;
@@ -240,7 +321,7 @@ AROS_LH1(void, FNAME_DEV(BeginIO),
 
     if (ret != RC_DONTREPLY)
     {
-        KPRINTF(1, ("%s: Terminating I/O..\n"
+        D(bug("[USB2OTG] %s: Terminating I/O..\n",
                     __PRETTY_FUNCTION__));
 
         if (ret != RC_OK)
@@ -270,7 +351,7 @@ AROS_LH1(LONG, FNAME_DEV(AbortIO),
 {
     AROS_LIBFUNC_INIT
 
-    KPRINTF(50, ("%s: IOReq @ 0x%p, command %ld, status %ld\n",
+    D(bug("[USB2OTG] %s: IOReq @ 0x%p, command %ld, status %ld\n",
                 __PRETTY_FUNCTION__, ioreq, ioreq->iouh_Req.io_Command, ioreq->iouh_Req.io_Message.mn_Node.ln_Type));
 
     /* Is it pending? */
