252dfe2f8aa67a51946b717bafde5c59c142f56c
Kalamatee <kalamatee@gmail.com>
2013-02-08 18:41:42 +0000

    dont force the sdtv modes for now since it looks nasty on screens which dont support it.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46538 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-raspi/hidd.videocoregfx/videocoregfx_sdtv.c
+++ arch/arm-raspi/hidd.videocoregfx/videocoregfx_sdtv.c
@@ -12,42 +12,44 @@ int FNAME_SUPPORT(SDTV_SyncGen)(struct List *modelist, OOP_Class *cl)
 {
     struct DisplayMode *sdtv_mode;
     int sdtv_modecount = 0;
+    BOOL sdtv_active = FALSE;
 
     D(bug("[VideoCoreGfx] %s()\n", __PRETTY_FUNCTION__));
 
 #warning "TODO: check if an SDTV is attached"
-    
-    if ((sdtv_mode = AllocMem(sizeof(struct DisplayMode), MEMF_PUBLIC)) != NULL)
+    if (sdtv_active)
     {
-        sdtv_mode->dm_clock = 25174;
-        sdtv_mode->dm_hdisp = 640;
-        sdtv_mode->dm_hstart = 656;
-        sdtv_mode->dm_hend = 752;
-        sdtv_mode->dm_htotal = 800;
-        sdtv_mode->dm_vdisp = 480;
-        sdtv_mode->dm_vstart = 490;
-        sdtv_mode->dm_vend = 492;
-        sdtv_mode->dm_vtotal = 525;
-        sdtv_mode->dm_descr = "VideoCore: PAL 640x480";
-        AddTail(modelist, &sdtv_mode->dm_Node);
-        sdtv_modecount++;
+        if ((sdtv_mode = AllocMem(sizeof(struct DisplayMode), MEMF_PUBLIC)) != NULL)
+        {
+            sdtv_mode->dm_clock = 25174;
+            sdtv_mode->dm_hdisp = 640;
+            sdtv_mode->dm_hstart = 656;
+            sdtv_mode->dm_hend = 752;
+            sdtv_mode->dm_htotal = 800;
+            sdtv_mode->dm_vdisp = 480;
+            sdtv_mode->dm_vstart = 490;
+            sdtv_mode->dm_vend = 492;
+            sdtv_mode->dm_vtotal = 525;
+            sdtv_mode->dm_descr = "VideoCore: PAL 640x480";
+            AddTail(modelist, &sdtv_mode->dm_Node);
+            sdtv_modecount++;
+        }
+
+        if ((sdtv_mode = AllocMem(sizeof(struct DisplayMode), MEMF_PUBLIC)) != NULL)
+        {
+            sdtv_mode->dm_clock = 25174;
+            sdtv_mode->dm_hdisp = 640;
+            sdtv_mode->dm_hstart = 656;
+            sdtv_mode->dm_hend = 752;
+            sdtv_mode->dm_htotal = 800;
+            sdtv_mode->dm_vdisp = 400;
+            sdtv_mode->dm_vstart = 410;
+            sdtv_mode->dm_vend = 412;
+            sdtv_mode->dm_vtotal = 445;
+            sdtv_mode->dm_descr = "VideoCore: NTSC 640x400";
+            AddTail(modelist, &sdtv_mode->dm_Node);
+            sdtv_modecount++;
+        }    
     }
-
-    if ((sdtv_mode = AllocMem(sizeof(struct DisplayMode), MEMF_PUBLIC)) != NULL)
-    {
-        sdtv_mode->dm_clock = 25174;
-        sdtv_mode->dm_hdisp = 640;
-        sdtv_mode->dm_hstart = 656;
-        sdtv_mode->dm_hend = 752;
-        sdtv_mode->dm_htotal = 800;
-        sdtv_mode->dm_vdisp = 400;
-        sdtv_mode->dm_vstart = 410;
-        sdtv_mode->dm_vend = 412;
-        sdtv_mode->dm_vtotal = 445;
-        sdtv_mode->dm_descr = "VideoCore: NTSC 640x400";
-        AddTail(modelist, &sdtv_mode->dm_Node);
-        sdtv_modecount++;
-    }    
-
     return sdtv_modecount;
 }
