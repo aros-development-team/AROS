a23655245836efd9f457ad9e8a6778d13f34584c
Kalamatee <kalamatee@gmail.com>
2013-02-11 18:57:00 +0000

    dont insist on processor resource
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46571 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-all/dos/probecpu.c
+++ arch/arm-all/dos/probecpu.c
@@ -6,6 +6,8 @@
     Lang: english
 */
 
+#define DEBUG 1
+
 #include <aros/debug.h>
 #include <aros/symbolsets.h>
 #include <resources/processor.h>
@@ -17,29 +19,26 @@ static ULONG ProbeCPU(struct IntDosBase *base)
 {
     APTR ProcessorBase = OpenResource("processor.resource");
 
-    if (!ProcessorBase)
+    if (ProcessorBase)
     {
-        D(bug("[DOS] No processor.resource! Won't boot!\n"));
-        return FALSE;
-    }    
+        base->arm_Arch   = CPUFAMILY_ARM_7;
+        base->arm_VFP    = TRUE;
+        base->arm_VFP_v3 = TRUE;
 
-    /*
-     * Durty hack allowing to boot up a BSP without working processor.resource implementation.
-     * It allows you to run any binaries. But be careful! It's strongly recommended to implement
-     * processor.resource for all ARM ports.
-     * The hack is based on the fact that not implemented processor.resource tags will leave their
-     * data untouched.
-     */
-    base->arm_Arch   = CPUFAMILY_ARM_7;
-    base->arm_VFP    = TRUE;
-    base->arm_VFP_v3 = TRUE;
+        GetCPUInfoTags(GCIT_Family       , &base->arm_Arch,
+                       GCIT_SupportsVFP  , &base->arm_VFP,
+                       GCIT_SupportsVFPv3, &base->arm_VFP_v3,
+                       TAG_DONE);
+    }
+    else
+    {
+        D(bug("[DOS] No processor.resource - using minimal support\n"));
+        base->arm_Arch   = (CPUFAMILY_ARM_3 - 3) + __ARM_ARCH__;
+        base->arm_VFP    = FALSE;
+        base->arm_VFP_v3 = FALSE;
+    }
 
-    GetCPUInfoTags(GCIT_Family       , &base->arm_Arch,
-                   GCIT_SupportsVFP  , &base->arm_VFP,
-                   GCIT_SupportsVFPv3, &base->arm_VFP_v3,
-                   TAG_DONE);
-
-    D(bug("[DOS] CPU architecture: %d, VFP support: %d, VFPv3 support: %d\n", base->arm_Arch, base->arm_VFP, base->arm_VFP_v3));
+    D(bug("[DOS] CPU architecture: %d [VFP support: %d, VFPv3 support: %d]\n", base->arm_Arch, base->arm_VFP, base->arm_VFP_v3));
 
     return TRUE;
 }
