1003a85adaf4ecd6694a865da813ea71e54b529a
mattrust <mrustler@gmx.de>
2013-02-15 20:58:16 +0000

    WIP GUI improved and event handler for entry selection.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46608 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/tools/SysExplorer/main.c
+++ workbench/tools/SysExplorer/main.c
@@ -25,7 +25,8 @@
 
 #include "locale.h"
 
-#define D(x) x
+#define DEBUG 1
+#include <aros/debug.h>
 
 #define APPNAME "SysExplorer"
 #define VERSION "SysExplorer 0.1"
@@ -46,23 +47,26 @@ Object *app;
 Object *MainWindow;
 Object *objTree;
 Object *menu_quit;
+Object *instance_name, *hardware_description, *vendor_info;
 
 AROS_UFP3(static void, enumFunc,
     AROS_UFPA(struct Hook *, h,  A0),
     AROS_UFPA(OOP_Object*, obj, A2),
     AROS_UFPA(void *, parent, A1));
 
-static inline LONG xget(Object * obj, ULONG attr)
-{
-    IPTR x = 0;
-
-    GetAttr(attr, obj, &x);
-    return x;
-}
+AROS_UFP3(static void, selectedFunc,
+    AROS_UFPA(struct Hook *, h,  A0),
+    AROS_UFPA(Object*, obj, A2),
+    AROS_UFPA(struct MUI_NListtree_TreeNode **, tn, A1));
 
 static struct Hook enum_hook =
 {
     .h_Entry = enumFunc,
+};
+
+static struct Hook selected_hook =
+{
+    .h_Entry = selectedFunc,
 };
 
 AROS_UFH3(static void, enumFunc,
@@ -78,6 +82,7 @@ AROS_UFH3(static void, enumFunc,
 
     if (name)
     {
+        // node
         struct MUI_NListtree_TreeNode *tn;
 
         tn = (APTR)DoMethod(objTree, MUIM_NListtree_Insert, name, obj,
@@ -90,9 +95,37 @@ AROS_UFH3(static void, enumFunc,
     }
     else
     {
+        // leave
         OOP_GetAttr(obj, aHidd_HardwareName, (IPTR *)&name);
         DoMethod(objTree, MUIM_NListtree_Insert, name, obj,
                  parent, MUIV_NListtree_Insert_PrevNode_Tail, 0);
+    }
+
+    AROS_USERFUNC_EXIT
+}
+
+AROS_UFH3(static void, selectedFunc,
+    AROS_UFHA(struct Hook *, h,  A0),
+    AROS_UFHA(Object*, obj, A2),
+    AROS_UFHA(struct MUI_NListtree_TreeNode **, tn, A1))
+{
+    AROS_USERFUNC_INIT
+
+    D(bug("selectedFunc called tn: %p name %s\n", *tn, (*tn)->tn_Name));
+
+    if ((*tn)->tn_Flags & TNF_LIST)
+    {
+        // node
+        SET(instance_name, MUIA_Text_Contents, "");
+        SET(hardware_description, MUIA_Text_Contents, "");
+        SET(vendor_info, MUIA_Text_Contents, "");
+    }
+    else
+    {
+        // leave
+        SET(instance_name, MUIA_Text_Contents, (*tn)->tn_Name);
+        SET(hardware_description, MUIA_Text_Contents, "FOO");
+        SET(vendor_info, MUIA_Text_Contents, "BAR");
     }
 
     AROS_USERFUNC_EXIT
@@ -103,48 +136,78 @@ static BOOL GUIinit()
     BOOL retval = FALSE;
 
     app = ApplicationObject,
-	    MUIA_Application_Title,	    (IPTR)APPNAME,
-	    MUIA_Application_Version,	    (IPTR)VERSION,
-	    MUIA_Application_Copyright,	    (IPTR)"(C) 2013, The AROS Development Team",
-	    MUIA_Application_Author,	    (IPTR)"Pavel Fedin",
-	    MUIA_Application_Base,	    (IPTR)APPNAME,
-	    MUIA_Application_Description,   __(MSG_DESCRIPTION),
+        MUIA_Application_Title,         (IPTR)APPNAME,
+        MUIA_Application_Version,       (IPTR)VERSION,
+        MUIA_Application_Copyright,     (IPTR)"(C) 2013, The AROS Development Team",
+        MUIA_Application_Author,        (IPTR)"Pavel Fedin",
+        MUIA_Application_Base,          (IPTR)APPNAME,
+        MUIA_Application_Description,   __(MSG_DESCRIPTION),
 
-            MUIA_Application_Menustrip, MenuitemObject,
-            	MUIA_Family_Child, MenuitemObject,
-                    MUIA_Menuitem_Title, __(MSG_MENU_PROJECT),
-                    MUIA_Family_Child,
-                    	menu_quit = MenuitemObject, MUIA_Menuitem_Title, __(MSG_MENU_QUIT),
-                    End,
-            	End,
+        MUIA_Application_Menustrip, MenuitemObject,
+            MUIA_Family_Child, MenuitemObject,
+                MUIA_Menuitem_Title, __(MSG_MENU_PROJECT),
+                MUIA_Family_Child,
+                    menu_quit = MenuitemObject, MUIA_Menuitem_Title, __(MSG_MENU_QUIT),
+                End,
             End,
+        End,
 
-	    SubWindow, MainWindow = WindowObject,
-		MUIA_Window_Title,	__(MSG_WINTITLE),
-
-		WindowContents, HGroup,
+        SubWindow, MainWindow = WindowObject,
+            MUIA_Window_Title,	__(MSG_WINTITLE),
+            MUIA_Window_ID, MAKE_ID('S', 'Y', 'E', 'X'),
+            WindowContents, HGroup,
+                Child, HGroup,
+                    GroupFrame,
+                    MUIA_FrameTitle, "HIDD Tree",
                     Child, NListviewObject,
                         MUIA_NListview_NList, objTree = NListtreeObject,
                             ReadListFrame,
                             MUIA_CycleChain, TRUE,
                         End,
                     End,
-		End, // WindowContents
-	    End, // MainWindow
-	End; // ApplicationObject
+                End,
+                Child, VGroup,
+                    Child, ColGroup(2),
+                        MUIA_FrameTitle, "Properties",
+                        GroupFrame,
+                        Child, Label("Driver instance name"),
+                        Child, instance_name = TextObject,
+                            MUIA_Text_Contents, "          ",
+                            TextFrame,
+                        End,
+                        Child, Label("Hardware description"),
+                        Child, hardware_description = TextObject,
+                            MUIA_Text_Contents, "          ",
+                            TextFrame,
+                        End,
+                        Child, Label("Vendor information"),
+                        Child, vendor_info = TextObject,
+                            MUIA_Text_Contents, "          ",
+                            TextFrame,
+                        End,
+                    End,
+                    Child, HVSpace,
+                End,
+            End, // WindowContents
+        End, // MainWindow
+    End; // ApplicationObject
 
     if (app)
     {
-	/* Quit application if the windowclosegadget or the esc key is pressed. */
-	DoMethod(MainWindow, MUIM_Notify, MUIA_Window_CloseRequest, TRUE, 
-		 app, 2, 
-		 MUIM_Application_ReturnID, MUIV_Application_ReturnID_Quit);
+        /* Quit application if the windowclosegadget or the esc key is pressed. */
+        DoMethod(MainWindow, MUIM_Notify, MUIA_Window_CloseRequest, TRUE, 
+                 app, 2, 
+                 MUIM_Application_ReturnID, MUIV_Application_ReturnID_Quit);
 
-	DoMethod(menu_quit, MUIM_Notify, MUIA_Menuitem_Trigger, MUIV_EveryTime,
-		 app, 2,
-		 MUIM_Application_ReturnID, MUIV_Application_ReturnID_Quit);
+        DoMethod(menu_quit, MUIM_Notify, MUIA_Menuitem_Trigger, MUIV_EveryTime,
+                 app, 2,
+                 MUIM_Application_ReturnID, MUIV_Application_ReturnID_Quit);
 
-	retval = TRUE;
+        DoMethod(objTree, MUIM_Notify, MUIA_NListtree_Active, MUIV_EveryTime,
+                 app, 3,
+                 MUIM_CallHook, &selected_hook, MUIV_TriggerValue);
+
+        retval = TRUE;
     }
 
     return retval;
@@ -173,27 +236,15 @@ int main(void)
             CALLHOOKPKT(&enum_hook, hwRoot, MUIV_NListtree_Insert_ListNode_Root);
         }
 
-	set(MainWindow, MUIA_Window_Open, TRUE);
+        SET(MainWindow, MUIA_Window_Open, TRUE);
 
-	if (xget(MainWindow, MUIA_Window_Open))
-	{
-	    ULONG retval = 0;
-	    ULONG signals = 0;
+        if (XGET(MainWindow, MUIA_Window_Open))
+        {
+            DoMethod(app, MUIM_Application_Execute);
+            SET(MainWindow, MUIA_Window_Open, FALSE);
+        }
 
-	    while (retval != MUIV_Application_ReturnID_Quit)
-	    {
-		retval = DoMethod(app, MUIM_Application_NewInput, &signals);
-
-		if (signals)
-		    signals = Wait(signals | SIGBREAKF_CTRL_C);
-
-		if (signals & SIGBREAKF_CTRL_C)
-		    retval = MUIV_Application_ReturnID_Quit;
-	    }
-	    set(MainWindow, MUIA_Window_Open, FALSE);
-	}
-
-	DisposeObject(app);
+        DisposeObject(app);
     }
 
     OOP_ReleaseAttrBases(abd);
