86a9838294d43acf86fdc536b850146e1254723e
Kalamatee <kalamatee@gmail.com>
2013-02-19 00:46:59 +0000

    stubs for ARM processor resource
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46637 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/arm-raspi/processor/processor_frequency.c
@@ -0,0 +1,29 @@
+/*
+    Copyright © 2013, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#define DEBUG 1
+
+#include <aros/config.h>
+#include <aros/debug.h>
+#include <proto/exec.h>
+#include <resources/processor.h>
+
+#include "processor_arch_intern.h"
+
+VOID ReadMaxFrequencyInformation(struct ARMProcessorInformation * info)
+{
+    D(bug("[processor.ARM] :%s()\n", __PRETTY_FUNCTION__));
+
+    info->MaxCPUFrequency = 0;
+}
+
+UQUAD GetCurrentProcessorFrequency(struct ARMProcessorInformation * info)
+{
+    UQUAD retFreq = info->MaxCPUFrequency;
+
+    D(bug("[processor.ARM] :%s()\n", __PRETTY_FUNCTION__));
+
+    return retFreq;
+}
