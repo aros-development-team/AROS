b67308fe052faf7ada5709e1ad64e0cabccd7185
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-02-23 11:11:31 +0000

    Added three new functions. Extremely convenient and useful for filling in base structures.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46673 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/oop/releaseattrbasesarray.c
@@ -0,0 +1,64 @@
+/*
+    Copyright © 1995-2013, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: Release array of attribute ID bases
+    Lang: english
+*/
+
+/*****************************************************************************
+
+    NAME */
+
+#include <proto/oop.h>
+#include <oop/oop.h>
+
+	AROS_LH2(void, OOP_ReleaseAttrBasesArray,
+
+/*  SYNOPSIS */
+	AROS_LHA(OOP_AttrBase *, bases, A0),
+        AROS_LHA(CONST_STRPTR *, ids, A1),
+
+/*  LOCATION */
+	struct Library *, OOPBase, 24, OOP)
+
+/*  FUNCTION
+        Release several attribute ID bases, stored in linear array.
+
+    INPUTS
+        bases - a pointer to array of bases
+        ids   - a NULL-terminated array of corresponding interface IDs
+
+    RESULT
+        None
+
+    NOTES
+        It is legal to have some entries in the array not filled in
+        (equal to 0). They will be skipped.
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+        OOP_ObtainAttrBasesArray
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    while (*ids)
+    {
+        if (*bases)
+        {
+            OOP_ReleaseAttrBase(*ids);
+            *bases = 0;
+        }
+    }
+
+    AROS_LIBFUNC_EXIT
+}
