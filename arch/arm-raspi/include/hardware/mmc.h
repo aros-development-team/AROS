aa14100164fc28007af3cd4c8903c2e0d1a8c65d
Kalamatee <kalamatee@gmail.com>
2013-03-05 15:07:17 +0000

    add mmc/sdhc defines - will move to a more generic location at a later date.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46815 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/arm-raspi/include/hardware/mmc.h
@@ -0,0 +1,73 @@
+/*
+    Copyright © 2013, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#ifndef _MMC_H
+#define _MMC_H
+
+/* MMC Command Responses */
+
+#define MMC_RSP_PRESENT                 (1 << 0)
+#define MMC_RSP_136	                (1 << 1)
+#define MMC_RSP_CRC	                (1 << 2)
+#define MMC_RSP_BUSY	                (1 << 3)
+#define MMC_RSP_OPCODE	                (1 << 4)
+
+#define MMC_RSP_NONE	                (0)
+#define MMC_RSP_R1	                (MMC_RSP_PRESENT|MMC_RSP_CRC|MMC_RSP_OPCODE)
+#define MMC_RSP_R1b	                (MMC_RSP_PRESENT|MMC_RSP_CRC|MMC_RSP_OPCODE|MMC_RSP_BUSY)
+#define MMC_RSP_R2	                (MMC_RSP_PRESENT|MMC_RSP_CRC|MMC_RSP_136)
+#define MMC_RSP_R3	                (MMC_RSP_PRESENT)
+#define MMC_RSP_R4	                (MMC_RSP_PRESENT)
+#define MMC_RSP_R5	                (MMC_RSP_PRESENT|MMC_RSP_CRC|MMC_RSP_OPCODE)
+#define MMC_RSP_R6	                (MMC_RSP_PRESENT|MMC_RSP_CRC|MMC_RSP_OPCODE)
+#define MMC_RSP_R7	                (MMC_RSP_PRESENT|MMC_RSP_CRC|MMC_RSP_OPCODE)
+
+/* MMC Commands */
+
+#define MMC_CMD_GO_IDLE_STATE           0
+#define MMC_CMD_SEND_OP_COND            1
+#define MMC_CMD_ALL_SEND_CID            2
+#define MMC_CMD_SET_RELATIVE_ADDR       3
+#define MMC_CMD_SET_DSR                 4
+#define MMC_CMD_SWITCH                  6
+#define MMC_CMD_SELECT_CARD             7
+#define MMC_CMD_SEND_EXT_CSD            8
+#define MMC_CMD_SEND_CSD                9
+#define MMC_CMD_SEND_CID                10
+#define MMC_CMD_STOP_TRANSMISSION       12
+#define MMC_CMD_SEND_STATUS             13
+#define MMC_CMD_SET_BLOCKLEN            16
+#define MMC_CMD_READ_SINGLE_BLOCK       17
+#define MMC_CMD_READ_MULTIPLE_BLOCK     18
+#define MMC_CMD_WRITE_SINGLE_BLOCK      24
+#define MMC_CMD_WRITE_MULTIPLE_BLOCK    25
+#define MMC_CMD_ERASE_GROUP_START       35
+#define MMC_CMD_ERASE_GROUP_END         36
+#define MMC_CMD_ERASE                   38
+#define MMC_CMD_APP_CMD                 55
+#define MMC_CMD_SPI_READ_OCR            58
+#define MMC_CMD_SPI_CRC_ON_OFF          59
+
+/* MMC Operating Voltages */
+
+#define MMC_VDD_165_195                 0x00000080
+#define MMC_VDD_200_210                 0x00000100
+#define MMC_VDD_210_220                 0x00000200
+#define MMC_VDD_220_230                 0x00000400
+#define MMC_VDD_230_240                 0x00000800
+#define MMC_VDD_240_250                 0x00001000
+#define MMC_VDD_250_260                 0x00002000
+#define MMC_VDD_260_270                 0x00004000
+#define MMC_VDD_270_280                 0x00008000
+#define MMC_VDD_280_290                 0x00010000
+#define MMC_VDD_290_300                 0x00020000
+#define MMC_VDD_300_310                 0x00040000
+#define MMC_VDD_310_320                 0x00080000
+#define MMC_VDD_320_330                 0x00100000
+#define MMC_VDD_330_340                 0x00200000
+#define MMC_VDD_340_350                 0x00400000
+#define MMC_VDD_350_360                 0x00800000
+
+#endif /* _MMC_H */
---
+++ arch/arm-raspi/include/hardware/sdhc.h
@@ -0,0 +1,162 @@
+/*
+    Copyright © 2013, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#ifndef _SDHC_H
+#define _SDHC_H
+
+#define SDHCI_MAKE_ADDR(addr, size)     (((addr & 0x7) << 12) | (size & 0xFFF))
+#define SDHCI_MAKE_CMD(cmd, flags)      (((cmd) << 8) | (flags & 0xFF))
+#define SDHCI_GET_CMD(x)                ((x >> 8) & 0x3F)
+
+/* SDHC Registers and Attributes */
+
+#define SDHCI_DMA_ADDRESS               0x00
+#define SDHCI_BLOCK_SIZE                0x04
+#define SDHCI_BLOCK_COUNT               0x06
+#define SDHCI_ARGUMENT                  0x08
+#define SDHCI_TRANSFERMOD_MODE          0x0C
+#define SDHCI_TRANSMOD_DMA              (1 << 0)
+#define SDHCI_TRANSMOD_BLK_CNT_EN       (1 << 1)
+#define SDHCI_TRANSMOD_ACMD12           (1 << 2)
+#define SDHCI_TRANSMOD_READ             (1 << 4)
+#define SDHCI_TRANSMOD_MULTI            (1 << 5)
+#define SDHCI_COMMAND                   0x0E
+#define SDHCI_CMD_RESP_NONE             (0)
+#define SDHCI_CMD_RESP_LONG             (1 << 0)
+#define SDHCI_CMD_RESP_SHORT            (1 << 1)
+#define SDHCI_CMD_RESP_SHORT_BUSY       (SDHCI_CMD_RESP_LONG | SDHCI_CMD_RESP_SHORT)
+#define SDHCI_CMD_RESP_MASK             (SDHCI_CMD_RESP_SHORT_BUSY)
+#define SDHCI_CMD_CRC   	        (1 << 3)
+#define SDHCI_CMD_INDEX	                (1 << 4)
+#define SDHCI_CMD_DATA		        (1 << 5)
+#define SDHCI_CMD_ABORTCMD	        (3 << 6)
+#define SDHCI_RESPONSE                  0x10
+#define SDHCI_BUFFER                    0x20
+#define SDHCI_PRESENT_STATE             0x24
+#define SDHCI_PS_CMD_INHIBIT            (1 << 0)
+#define SDHCI_PS_DATA_INHIBIT           (1 << 1)
+#define SDHCI_PS_DOING_WRITE            (1 << 8)
+#define SDHCI_PS_DOING_READ             (1 << 9)
+#define SDHCI_PS_SPACE_AVAILABLE        (1 << 10)
+#define SDHCI_PS_DATA_AVAILABLE         (1 << 11)
+#define SDHCI_PS_CARD_PRESENT           (1 << 16)
+#define SDHCI_PS_CARD_STATE_STABLE      (1 << 17)
+#define SDHCI_PS_CARD_DETECT_PIN_LVL    (1 << 18)
+#define SDHCI_PS_WRITE_PROTECT          (1 << 19)
+#define SDHCI_HOST_CONTROL              0x28
+#define SDHCI_HCTRL_LED                 (1 << 0)
+#define SDHCI_HCTRL_4BITBUS             (1 << 1)
+#define SDHCI_HCTRL_HISPD               (1 << 2)
+#define SDHCI_HCTRL_DMA_MASK	        (3 << 3)
+#define SDHCI_HCTRL_SDMA                (0)
+#define SDHCI_HCTRL_ADMA1               (1 << 3)
+#define SDHCI_HCTRL_ADMA32	        (1 << 4)
+#define SDHCI_HCTRL_ADMA64	        (SDHCI_HCTRL_ADMA1 | SDHCI_HCTRL_ADMA32)
+#define SDHCI_HCTRL_8BITBUS	        (1 << 5)
+#define SDHCI_HCTRL_CD_TEST_INS	        (1 << 6)
+#define SDHCI_HCTRL_CD_TEST	        (1 << 7)
+#define SDHCI_POWER_CONTROL             0x29
+#define SDHCI_POWER_ON                  (1 << 0)
+#define SDHCI_POWER_180                 (1 << 3) | (1 << 1)
+#define SDHCI_POWER_300                 (1 << 3) | (1 << 2)
+#define SDHCI_POWER_330                 (! << 3) | (1 << 2) | (1 << 1)
+#define SDHCI_BLOCK_GAP_CONTROL         0x2A
+#define SDHCI_WAKE_UP_CONTROL           0x2B
+#define SDHCI_WAKE_ONINT	        (1 << 0)
+#define SDHCI_WAKE_ONINSERT             (1 << 1)
+#define SDHCI_WAKE_ONREMOVE             (1 << 2)
+#define SDHCI_CLOCK_CONTROL	        0x2C
+#define SDHCI_CLOCK_INT_EN              (1 << 0)
+#define SDHCI_CLOCK_INT_STABLE          (1 << 1)
+#define SDHCI_CLOCK_CARD_EN             (1 << 2)
+#define SDHCI_DIVIDER_SHIFT             (8)
+#define SDHCI_DIVIDER_HI_SHIFT	        (6)
+#define SDHCI_DIV_MASK                  (0xFF)
+#define SDHCI_DIV_MASK_LEN              (8)
+#define SDHCI_DIV_HI_MASK               (3 << 8)
+#define SDHCI_TIMEOUT_CONTROL	        0x2E
+#define SDHCI_RESET	                0x2F
+#define SDHCI_RESET_ALL                 (1 << 0)
+#define SDHCI_RESET_CMD                 (1 << 1)
+#define SDHCI_RESET_DATA                (1 << 2)
+#define SDHCI_INT_STATUS	        0x30
+#define SDHCI_INT_ENABLE	        0x34
+#define SDHCI_SIGNAL_ENABLE	        0x38
+#define SDHCI_INT_RESPONSE              (1 << 0)
+#define SDHCI_INT_DATA_END              (1 << 2)
+#define SDHCI_INT_DMA_END               (1 << 3)
+#define SDHCI_INT_SPACE_AVAIL           (1 << 4)
+#define SDHCI_INT_DATA_AVAIL            (1 << 5)
+#define SDHCI_INT_CARD_INSERT           (1 << 6)
+#define SDHCI_INT_CARD_REMOVE           (1 << 7)
+#define SDHCI_INT_CARD_INT              (1 << 8)
+#define SDHCI_INT_ERROR                 (1 << 15)
+#define SDHCI_INT_TIMEOUT               (1 << 16)
+#define SDHCI_INT_CRC                   (1 << 17)
+#define SDHCI_INT_END_BIT               (1 << 18)
+#define SDHCI_INT_INDEX                 (1 << 19)
+#define SDHCI_INT_DATA_TIMEOUT          (1 << 20)
+#define SDHCI_INT_DATA_CRC              (1 << 21)
+#define SDHCI_INT_DATA_END_BIT          (1 << 22)
+#define SDHCI_INT_BUS_POWER             (1 << 23)
+#define SDHCI_INT_ACMD12ERR             (1 << 24)
+#define SDHCI_INT_ADMA_ERROR            (1 << 25)
+#define SDHCI_INT_CMD_MASK              (SDHCI_INT_RESPONSE|SDHCI_INT_TIMEOUT|SDHCI_INT_CRC|SDHCI_INT_END_BIT|SDHCI_INT_INDEX)
+#define SDHCI_INT_DATA_MASK             (SDHCI_INT_DATA_END|SDHCI_INT_DMA_END|SDHCI_INT_DATA_AVAIL|SDHCI_INT_SPACE_AVAIL|SDHCI_INT_DATA_TIMEOUT|SDHCI_INT_DATA_CRC|SDHCI_INT_DATA_END_BIT|SDHCI_INT_ADMA_ERROR)
+#define SDHCI_INT_ALL_MASK              (0xFFFFFFFF)
+#define SDHCI_INT_NORMAL_MASK           (0x00007FFF)
+#define SDHCI_INT_ERROR_MASK            (0xFFFF8000)
+#define SDHCI_ACMD12_ERR                0x3C
+#define SDHCI_CAPABILITIES              0x40
+#define SDHCI_TIMEOUT_CLK_MASK	        (0x3F)
+#define SDHCI_TIMEOUT_CLK_SHIFT         (0)
+#define SDHCI_TIMEOUT_CLK_UNIT	        (1 << 7)
+#define SDHCI_CLOCK_BASE_SHIFT          (8)
+#define SDHCI_CLOCK_BASE_MASK	        (0x3F << SDHCI_CLOCK_BASE_SHIFT)
+#define SDHCI_CLOCK_V3_BASE_MASK        (0xFF << SDHCI_CLOCK_BASE_SHIFT)
+#define SDHCI_MAX_BLOCK_SHIFT           (16)
+#define SDHCI_MAX_BLOCK_MASK            (0x03 << SDHCI_MAX_BLOCK_SHIFT)
+#define SDHCI_CAN_DO_8BIT               (1 << 18)
+#define SDHCI_CAN_DO_ADMA2              (1 << 19)
+#define SDHCI_CAN_DO_ADMA1              (1 << 20)
+#define SDHCI_CAN_DO_HISPD              (1 << 21)
+#define SDHCI_CAN_DO_SDMA               (1 << 22)
+#define SDHCI_CAN_VDD_330               (1 << 24)
+#define SDHCI_CAN_VDD_300               (1 << 25)
+#define SDHCI_CAN_VDD_180               (1 << 26)
+#define SDHCI_CAN_64BIT                 (1 << 27)
+#define SDHCI_CAPABILITIES1             0x44
+#define SDHCI_MAX_CURRENT               0x48
+#define SDHCI_SET_ACMD12_ERROR          0x50
+#define SDHCI_SET_INT_ERROR             0x52
+#define SDHCI_ADMA_ERROR                0x54
+#define SDHCI_ADMA_ADDRESS              0x58
+#define SDHCI_SLOT_INT_STATUS           0xFC
+#define SDHCI_HOST_VERSION              0xFE
+#define SDHCI_HVERS_SPEC_SHIFT          (0)
+#define SDHCI_HVERS_SPEC_MASK           (0xFF)
+#define SDHCI_HVERS_VENDVER_SHIFT       (8)
+#define SDHCI_HVERS_VENDVER_MASK        (SDHCI_HVERS_SPEC_MASK << SDHCI_HVERS_VENDVER_SHIFT)
+#define SDHCI_HVERS_SPEC100             (0)
+#define SDHCI_HVERS_SPEC200             (1 << 0)
+#define SDHCI_HVERS_SPEC300             (1 << 2)
+
+/* SDCard OCR Values */
+#define OCR_FASTBOOT                    (1 << 29)
+#define OCR_HCS                         (1 << 30)
+#define OCR_BUSY                        (1 << 31)
+
+/* SDCard Commands */
+#define SD_CMD_SEND_RELATIVE_ADDR       3
+#define SD_CMD_SWITCH_FUNC              6
+#define SD_CMD_SEND_IF_COND             8
+
+#define SD_CMD_APP_SET_BUS_WIDTH        6
+#define SD_CMD_ERASE_WR_BLK_START       32
+#define SD_CMD_ERASE_WR_BLK_END         33
+#define SD_CMD_APP_SEND_OP_COND         41
+#define SD_CMD_APP_SEND_SCR             51
+
+#endif /* _SDHC_H */
