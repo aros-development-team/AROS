52ba884bc8676aa5e5f8f6cb62c2e8f6f7496c88
Kalamatee <kalamatee@gmail.com>
2013-03-14 03:02:36 +0000

    specify the bare minimum if processor.resource isnt available
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46931 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-all/dos/probecpu.c
+++ arch/arm-all/dos/probecpu.c
@@ -19,23 +19,16 @@ static ULONG ProbeCPU(struct IntDosBase *base)
 {
     APTR ProcessorBase = OpenResource("processor.resource");
 
+    base->arm_Arch   = CPUFAMILY_ARM_3;
+    base->arm_VFP    = FALSE;
+    base->arm_VFP_v3 = FALSE;
+
     if (ProcessorBase)
     {
-        base->arm_Arch   = CPUFAMILY_ARM_7;
-        base->arm_VFP    = TRUE;
-        base->arm_VFP_v3 = TRUE;
-
         GetCPUInfoTags(GCIT_Family       , &base->arm_Arch,
                        GCIT_SupportsVFP  , &base->arm_VFP,
                        GCIT_SupportsVFPv3, &base->arm_VFP_v3,
                        TAG_DONE);
-    }
-    else
-    {
-        D(bug("[DOS] No processor.resource - using minimal support\n"));
-        base->arm_Arch   = (CPUFAMILY_ARM_3 - 3) + __ARM_ARCH__;
-        base->arm_VFP    = FALSE;
-        base->arm_VFP_v3 = FALSE;
     }
 
     D(bug("[DOS] CPU architecture: %d [VFP support: %d, VFPv3 support: %d]\n", base->arm_Arch, base->arm_VFP, base->arm_VFP_v3));
