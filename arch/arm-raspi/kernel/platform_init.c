9d601adefc5ad79d1482b9d104c967b6fbf5cab0
Kalamatee <kalamatee@gmail.com>
2013-03-16 13:08:39 +0000

    turn off debug
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46974 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-raspi/kernel/platform_init.c
+++ arch/arm-raspi/kernel/platform_init.c
@@ -60,7 +60,7 @@ static int PlatformInit(struct KernelBase *KernelBase)
     // TODO:
     // How to identify broadcom IP's?
     // Expose this as a seprate subsystem (like PCI?)
-    D(bug("[Kernel] PlatformInit: Integrated Peripherals -:\n"));
+    D(bug("[Kernel] Integrated Peripherals -:\n"));
     for (ptr = ARM_PERIIOBASE; ptr < (ARM_PERIIOBASE + ARM_PERIIOSIZE); ptr += ARM_PRIMECELLPERISIZE)
     {
         unsigned int perihreg = (*(volatile unsigned int *)(ptr + 0xFF0) & 0xFF) | (*(volatile unsigned int *)(ptr + 0xFF4) & 0xFF) << 8 | (*(volatile unsigned int *)(ptr + 0xFF8) & 0xFF) << 16 | (*(volatile unsigned int *)(ptr + 0xFFC) & 0xFF) << 24;
@@ -71,7 +71,7 @@ static int PlatformInit(struct KernelBase *KernelBase)
             unsigned int prod = (perihreg & 0xFFF);
             unsigned int rev = (perihreg & (0xF << 20)) >> 20;
             unsigned int config = (perihreg & (0x7F << 24)) >> 24;
-            D(bug("[Kernel] PlatformInit:           0x%p: manu %x, prod %x, rev %d, config %d\n", ptr, manu, prod, rev, config));
+            D(bug("[Kernel]           0x%p: manu %x, prod %x, rev %d, config %d\n", ptr, manu, prod, rev, config));
         }
 /*        else
         {
