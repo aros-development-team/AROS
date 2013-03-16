4329c8eab5e51a343c027164490605e0547edd7f
deadwood-pl <deadwood@wp.pl>
2013-03-16 09:02:09 +0000

    graphics.hidd: remove previous, partial implementation of color conversion functions
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46951 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/hidds/graphics/graphics_init.c
+++ rom/hidds/graphics/graphics_init.c
@@ -13,7 +13,6 @@
 #include <aros/symbolsets.h>
 
 #include "graphics_intern.h"
-#include "rgbconv.h"
 
 #include LC_LIBDEFS_FILE
 
@@ -35,74 +34,6 @@ static int GFX_Init(LIBBASETYPEPTR LIBBASE)
     InitSemaphore(&csd->sema);
     InitSemaphore(&csd->pfsema);
     InitSemaphore(&csd->rgbconvertfuncs_sem);
-
-    /* X -> BGR032 */
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_RGB24 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_BGR032 - FIRST_RGB_STDPIXFMT] = Convert_RGB24_To_BGR032;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_BGR24 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_BGR032 - FIRST_RGB_STDPIXFMT] = Convert_BGR24_To_BGR032;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_RGB16 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_BGR032 - FIRST_RGB_STDPIXFMT] = Convert_RGB16_To_BGR032;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_RGB16_LE - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_BGR032 - FIRST_RGB_STDPIXFMT] = Convert_RGB16LE_To_BGR032;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_BGR16 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_BGR032 - FIRST_RGB_STDPIXFMT] = Convert_BGR16_To_BGR032;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_BGR16_LE - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_BGR032 - FIRST_RGB_STDPIXFMT] = Convert_BGR16LE_To_BGR032;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_ARGB32 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_BGR032 - FIRST_RGB_STDPIXFMT] = Convert_ARGB32_To_BGR032;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_BGRA32 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_BGR032 - FIRST_RGB_STDPIXFMT] = Convert_BGRA32_To_BGR032;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_RGBA32 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_BGR032 - FIRST_RGB_STDPIXFMT] = Convert_RGBA32_To_BGR032;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_ABGR32 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_BGR032 - FIRST_RGB_STDPIXFMT] = Convert_ABGR32_To_BGR032;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_0RGB32 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_BGR032 - FIRST_RGB_STDPIXFMT] = Convert_ARGB32_To_BGR032;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_BGR032 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_BGR032 - FIRST_RGB_STDPIXFMT] = Convert_BGRA32_To_BGR032;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_RGB032 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_BGR032 - FIRST_RGB_STDPIXFMT] = Convert_RGBA32_To_BGR032;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_0BGR32 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_BGR032 - FIRST_RGB_STDPIXFMT] = Convert_ABGR32_To_BGR032;
-
-    /* X -> RGB16_LE */
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_RGB24 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_RGB16_LE - FIRST_RGB_STDPIXFMT] = Convert_RGB24_To_RGB16LE;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_BGR24 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_RGB16_LE - FIRST_RGB_STDPIXFMT] = Convert_BGR24_To_RGB16LE;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_RGB16 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_RGB16_LE - FIRST_RGB_STDPIXFMT] = Convert_RGB16_To_RGB16LE;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_RGB16_LE - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_RGB16_LE - FIRST_RGB_STDPIXFMT] = Convert_RGB16LE_To_RGB16LE;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_BGR16 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_RGB16_LE - FIRST_RGB_STDPIXFMT] = Convert_BGR16_To_RGB16LE;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_BGR16_LE - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_RGB16_LE - FIRST_RGB_STDPIXFMT] = Convert_BGR16LE_To_RGB16LE;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_ARGB32 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_RGB16_LE - FIRST_RGB_STDPIXFMT] = Convert_ARGB32_To_RGB16LE;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_BGRA32 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_RGB16_LE - FIRST_RGB_STDPIXFMT] = Convert_BGRA32_To_RGB16LE;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_RGBA32 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_RGB16_LE - FIRST_RGB_STDPIXFMT] = Convert_RGBA32_To_RGB16LE;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_ABGR32 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_RGB16_LE - FIRST_RGB_STDPIXFMT] = Convert_ABGR32_To_RGB16LE;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_0RGB32 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_RGB16_LE - FIRST_RGB_STDPIXFMT] = Convert_ARGB32_To_RGB16LE;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_BGR032 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_RGB16_LE - FIRST_RGB_STDPIXFMT] = Convert_BGRA32_To_RGB16LE;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_RGB032 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_RGB16_LE - FIRST_RGB_STDPIXFMT] = Convert_RGBA32_To_RGB16LE;
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_0BGR32 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_RGB16_LE - FIRST_RGB_STDPIXFMT] = Convert_ABGR32_To_RGB16LE;
-
-    /* BGR032 -> X */
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_BGR032 - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_ARGB32 - FIRST_RGB_STDPIXFMT] = Convert_BGR032_To_ARGB32;
-
-    /* RGB16_LE -> X */
-    csd->rgbconvertfuncs[vHidd_StdPixFmt_RGB16_LE - FIRST_RGB_STDPIXFMT]
-        [vHidd_StdPixFmt_ARGB32 - FIRST_RGB_STDPIXFMT] = Convert_RGB16LE_To_ARGB32;
 
     ReturnInt("GfxHIDD_Init", ULONG, TRUE);
 }
