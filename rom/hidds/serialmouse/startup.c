40219ff52f0e88810ab9c0d1f876681cbd74eff0
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-03-18 15:52:56 +0000

    Reapplication of r46546, but with history preserved:
    Author: sonic                                                                   
    
    Separated serial and PS/2 mice drivers. Serial temporarily excluded from
    the build, but should work, since there was not much refactoring.
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@46990 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/hidds/serialmouse/startup.c
@@ -0,0 +1,36 @@
+#include <aros/symbolsets.h>
+#include <hidd/hidd.h>
+#include <hidd/mouse.h>
+#include <proto/oop.h>
+
+#include "mouse.h"
+
+#undef HiddAttrBase
+#undef HWBase
+#undef OOPBase
+#define HiddAttrBase (LIBBASE->msd.hiddAttrBase)
+#define HWBase       (LIBBASE->msd.hwMethodBase)
+#define OOPBase      (LIBBASE->msd.oopBase)
+
+static int init_mouse(struct mousebase *LIBBASE)
+{
+    /* FIXME: implement proper detection. See TODO notice in mouseclass.c */
+    struct TagItem tags[] =
+    {
+        {aHidd_Name        , (IPTR)"SerMouse"    },
+        {aHidd_HardwareName, (IPTR)"Serial mouse"},
+        {TAG_DONE          , 0                   }
+    };
+    OOP_Object *ms = OOP_NewObject(NULL, CLID_HW_Mouse, NULL);
+
+    if (!ms)
+        return FALSE;
+
+    if (!HW_AddDriver(ms, LIBBASE->msd.mouseclass, tags))
+        return FALSE;
+
+    LIBBASE->library.lib_OpenCnt = 1;
+    return TRUE;
+}
+
+ADD2INITLIB(init_mouse, 10);
