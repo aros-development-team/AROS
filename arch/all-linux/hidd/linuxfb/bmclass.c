6706e5677776508eb02e9256eb40bbbef8af8719
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-03-25 20:06:32 +0000

    Use new mirrored framebuffer API of the base class. Killed code duplication.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@47075 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-linux/hidd/linuxfb/bmclass.c
+++ arch/all-linux/hidd/linuxfb/bmclass.c
@@ -32,64 +32,13 @@ OOP_Object *LinuxBM__Root__New(OOP_Class *cl, OOP_Object *o, struct pRoot_New *m
 
     if (NULL != o)
     {
-        OOP_Object * pf;
         struct BitmapData *data = OOP_INST_DATA(cl, o);
-        HIDDT_ModeID modeid = GetTagData(aHidd_BitMap_ModeID, vHidd_ModeID_Invalid, msg->attrList);
-        IPTR width  = 0;
-        IPTR height = 0;
-        IPTR mod    = 0;
-        IPTR bytesperpixel = 0;
+        OOP_Object *pf;
 
-        OOP_GetAttr(o, aHidd_BitMap_Width , &width);
-        OOP_GetAttr(o, aHidd_BitMap_Height, &height);
-        OOP_GetAttr(o, aHidd_BitMap_BytesPerRow, &mod);
-        OOP_GetAttr(o, aHidd_ChunkyBM_Buffer, (IPTR *)&data->VideoData);
-        OOP_GetAttr(o, aHidd_BitMap_PixFmt, (APTR)&pf);
-        OOP_GetAttr(pf, aHidd_PixFmt_BytesPerPixel, &bytesperpixel);
+        data->fbdev = GetTagData(aHidd_LinuxFBBitmap_FBDevInfo, -1, msg->attrList);
 
-        data->width         = width;
-        data->height        = height;
-        data->bytesperline  = mod;
-        data->bytesperpix   = bytesperpixel;
-
-        D(bug("[LinuxBM] Created bitmap %ldx%ld\n", width, height));
-        D(bug("[LinuxBM] Buffer at 0x%p, %ld bytes per row\n", data->VideoData, mod));
-
-        /*
-         * We rely on the fact that bitmaps with aHidd_BitMap_Displayable set to TRUE always
-         * also get aHidd_BitMap_ModeID with valid value. Currently this seems to be true and
-         * i beleive it should stay so.
-         */
-        if (modeid != vHidd_ModeID_Invalid)
-        {
-            IPTR dwidth  = 0;
-            IPTR dheight = 0;
-            OOP_Object *gfx = (OOP_Object *)GetTagData(aHidd_BitMap_GfxHidd, 0, msg->attrList);
-            OOP_Object *sync;
-            struct FBDevInfo * fbdevinfo = NULL;
-
-            D(bug("[LinuxBM] Display driver object: 0x%p\n", gfx));
-
-            HIDD_Gfx_GetMode(gfx, modeid, &sync, (OOP_Object **)&data->pixfmt);
-            OOP_GetAttr(sync, aHidd_Sync_HDisp, &dwidth);
-            OOP_GetAttr(sync, aHidd_Sync_VDisp, &dheight);
-
-            data->display_width  = dwidth;
-            data->display_height = dheight;
-            D(bug("[LinuxBM] Display size: %dx%d\n", dwidth, dheight));
-
-            fbdevinfo = (struct FBDevInfo *)GetTagData(aHidd_LinuxFBBitmap_FBDevInfo,
-                    (IPTR)NULL, msg->attrList);
-
-            if (fbdevinfo != NULL)
-            {
-                data->RealVideoData = fbdevinfo->baseaddr;
-                data->realbytesperline = fbdevinfo->pitch;
-                data->fbdev            = fbdevinfo->fbdev;
-            }
-
-            D(if (data->RealVideoData) bug("[LinuxBM] RealVideoData: 0x%p\n",data->RealVideoData));
-        }
+        OOP_GetAttr(o, aHidd_BitMap_PixFmt, (IPTR *)&pf);
+        OOP_GetAttr(pf, aHidd_PixFmt_BitsPerPixel, &data->bpp);
     }
 
     return o;
@@ -98,19 +47,21 @@ OOP_Object *LinuxBM__Root__New(OOP_Class *cl, OOP_Object *o, struct pRoot_New *m
 BOOL LinuxBM__Hidd_BitMap__SetColors(OOP_Class *cl, OOP_Object *o, struct pHidd_BitMap_SetColors *msg)
 {
     struct BitmapData *data = OOP_INST_DATA(cl, o);
-    ULONG xc_i, col_i;
+
+    if ((msg->firstColor + msg->numColors) > (1 << (data->bpp)))
+    {
+	return FALSE;
+    }
 
     if (!OOP_DoSuperMethod(cl, o, (OOP_Msg)msg))
     {
 	return FALSE;
     }
 
-    if ((msg->firstColor + msg->numColors) > (1 << (data->bytesperpix * 8)))
-	return FALSE;
-
-    if (data->visible && data->bytesperpix == 1)
+    if (data->fbdev != -1)
     {
         struct LinuxFB_staticdata *fsd = LSD(cl);
+        ULONG xc_i, col_i;
 
 	for ( xc_i = msg->firstColor, col_i = 0;
     	      col_i < msg->numColors; 
@@ -129,53 +80,6 @@ BOOL LinuxBM__Hidd_BitMap__SetColors(OOP_Class *cl, OOP_Object *o, struct pHidd_
 	}
     }
     return TRUE;
-}
-
-
-/**********  Bitmap::UpdateRect()  ***********************************/
-VOID LinuxBM__Hidd_BitMap__UpdateRect(OOP_Class *cl, OOP_Object *o, struct pHidd_BitMap_UpdateRect *msg)
-{
-    struct BitmapData *data = OOP_INST_DATA(cl, o);
-
-    /* NOTE: There is a strong assumption here that bitmap dimensions equal framebuffer dimensions */
-
-    if (data->visible)
-    {
-        ULONG x = msg->x * data->bytesperpix;
-        APTR src = data->VideoData     + msg->y * data->bytesperline     + x;
-        APTR dst = data->RealVideoData + msg->y * data->realbytesperline + x;
-        LONG y;
-
-        D(bug("[LinuxBM] 0x%p -> UpdateRect(%d, %d, %d, %d)\n", o, msg->x, msg->y, msg->width, msg->height));
-
-        for (y = 0; y < msg->height; y++)
-        {
-            CopyMem(src,dst, msg->width * data->bytesperpix);
-
-            src += data->bytesperline;
-            dst += data->realbytesperline;
-        }
-    }
-}
-
-VOID LinuxBM__Root__Set(OOP_Class *cl, OOP_Object *o, struct pRoot_Set *msg)
-{
-    struct BitmapData *data = OOP_INST_DATA(cl, o);
-    struct TagItem *tstate = msg->attrList;
-    struct TagItem *tag;
-    ULONG idx;
-
-    while ((tag = NextTagItem(&tstate)))
-    {
-        Hidd_BitMap_Switch(tag->ti_Tag, idx)
-        {
-        case aoHidd_BitMap_Visible:
-            data->visible = tag->ti_Data;
-            break;
-        }
-    }
-
-    OOP_DoSuperMethod(cl, o, &msg->mID);
 }
 
 /* TODO: The following needs to be moved to ChunkyBM class */
