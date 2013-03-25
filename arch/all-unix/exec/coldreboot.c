bec7db7b351291d9e8239bd46a2d7d99de22d4e8
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-03-25 20:13:36 +0000

    Call reset handlers also on warm reboot. Needed for LinuxFB driver to reset keyboard mode correctly.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@47077 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-unix/exec/coldreboot.c
+++ arch/all-unix/exec/coldreboot.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2012, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2013, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: ColdReboot() - Reboot the computer, Unix-hosted implementation.
@@ -9,11 +9,14 @@
 #include <aros/debug.h>
 
 #include "exec_intern.h"
+#include "exec_util.h"
 
 AROS_LH0(void, ColdReboot,
 	 struct ExecBase *, SysBase, 121, Exec)
 {
     AROS_LIBFUNC_INIT
+
+    Exec_DoResetCallbacks((struct IntExecBase *)SysBase, SD_ACTION_WARMREBOOT);
 
     PD(SysBase).SysIFace->exit(0x8F); /* Another magic value for the bootstrap */
     AROS_HOST_BARRIER
