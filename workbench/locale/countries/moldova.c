06d7aa2374a8b6179e4edfae2d28c0d6985e32e3
impulse <impulse@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-04-13 10:47:45 +0000

    Fixed encoding
    git-svn-id: https://svn.aros.org/svn/aros/trunk@47217 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/locale/countries/moldova.c
+++ workbench/locale/countries/moldova.c
@@ -1,5 +1,5 @@
 /*
-    Copyright ï¿½ 1995-2013, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2013, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: Country data for Republic of Moldova
