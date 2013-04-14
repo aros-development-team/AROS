dfd4c505a7f659c74f3d35c31454baed1afe2d90
Kalamatee <kalamatee@gmail.com>
2013-04-14 18:51:26 +0000

    update copyright
    git-svn-id: https://svn.aros.org/svn/aros/trunk@47234 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/locale/languages/templates/english.c
+++ workbench/locale/languages/templates/english.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2007, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2013, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: english.language description file.
