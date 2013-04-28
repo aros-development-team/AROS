e7978d3df3996cb8469b1cd49b9c79ef3e37275b
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-04-28 12:55:23 +0000

    exec.library: make sure initial puddle has enough space for allocator context
    git-svn-id: https://svn.aros.org/svn/aros/trunk@47316 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/memory.h
+++ rom/exec/memory.h
@@ -63,6 +63,7 @@ struct MemHeaderAllocatorCtx;
 struct MemHeaderAllocatorCtx * mhac_GetSysCtx(struct MemHeader * mh, struct ExecBase * SysBase);
 void mhac_PoolMemHeaderSetup(struct MemHeader * mh, struct ProtectedPool * pool);
 void mhac_MemChunkClaimed(struct MemChunk * mc, struct MemHeaderAllocatorCtx * mhac);
+ULONG mhac_GetCtxSize();
 
 struct MemHeader *FindMem(APTR address, struct ExecBase *SysBase);
 APTR stdAlloc(struct MemHeader *mh, struct MemHeaderAllocatorCtx *mhac, IPTR byteSize, ULONG requirements, struct TraceLocation *loc, struct ExecBase *SysBase);
