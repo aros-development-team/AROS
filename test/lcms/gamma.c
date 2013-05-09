97136d48bd02a2695c62adaf38c87cb42f166af1
mattrust <mrustler@gmx.de>
2013-05-09 23:00:33 +0000

    Trying to read gamma values from an ICC file.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@47391 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/lcms/gamma.c
@@ -0,0 +1,48 @@
+#include <lcms2.h>
+
+#include <stdio.h>
+
+// get gamma from ICC file
+
+int main(int argc, char **argv)
+{
+    char *filename;
+    cmsHPROFILE hProfile;
+    cmsToneCurve *tc_red, *tc_green, *tc_blue;
+    int i;
+
+    if (argc == 2)
+    {
+        filename = argv[1];
+    }
+    else
+    {
+        filename = "SYS:Prefs/Presets/Colortables/ICC/sRGB.icc";
+    }
+
+    hProfile = cmsOpenProfileFromFile(filename, "r");
+    if (hProfile)
+    {
+        tc_red = cmsReadTag(hProfile, cmsSigRedTRCTag);
+        tc_green = cmsReadTag(hProfile, cmsSigGreenTRCTag);
+        tc_blue = cmsReadTag(hProfile, cmsSigBlueTRCTag);
+        if (tc_red && tc_green && tc_blue)
+        {
+            for (i=0; i < 256; i++)
+            {
+                printf("i %d red %u green %u blue %u\n", i, cmsEvalToneCurve16(tc_red, i * 256) >> 8, cmsEvalToneCurve16(tc_green, i * 256) >> 8, cmsEvalToneCurve16(tc_blue, i * 256) >> 8);
+            }
+        }
+        else
+        {
+            puts("Gamma tags missing");
+        }
+        cmsCloseProfile(hProfile);
+    }
+    else
+    {
+        puts("Couldn't open ICC file");
+        return 1;
+    }
+    return 0;
+}
