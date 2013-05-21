753ff8d88a75902c09dce6d1bb35ec0138c681de
Kalamatee <kalamatee@gmail.com>
2013-05-21 11:37:03 +0000

    bump copyright date
    git-svn-id: https://svn.aros.org/svn/aros/trunk@47441 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/strtodate.c
+++ rom/dos/strtodate.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2010, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2013, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: Converts a string into a date
