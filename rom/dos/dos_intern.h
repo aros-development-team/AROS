1da80045fe76ef7e08c322b35f556a4fc5f87535
twilen <twilen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-06-01 18:05:51 +0000

    Log also process name in bogus filelock message, message shortened.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@47494 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/dos_intern.h
+++ rom/dos/dos_intern.h
@@ -313,7 +313,8 @@ void freepacketinfo(struct DosLibrary *DOSBase, struct PacketHelperStruct*);
 #define ASSERT_VALID_FILELOCK(lock) do { \
         struct FileLock *fl = BADDR(lock); \
         if (fl && fl->fl_Access != SHARED_LOCK && fl->fl_Access != EXCLUSIVE_LOCK) { \
-            bug("%s() called with a bogus FileLock! Lock=%x FL=%x Access %d %s/%s/%d\n", __FUNCTION__, lock, fl, fl->fl_Access, __FILE__,__FUNCTION__,__LINE__); \
+            bug("%s() bogus FileLock! '%s' %x %d %s/%s/%d\n", \
+                __FUNCTION__, SysBase->ThisTask->tc_Node.ln_Name, fl, fl->fl_Access, __FILE__,__FUNCTION__,__LINE__); \
         } \
     } while (0);
 
