f21da5b0228edcdb02887f506234bf8941cd76ab
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-06-02 21:30:48 +0000

    1. fixed mx51_uart include file (IMX_UART structure was incomplete) 2. the kernel_debug.c is more in sync with other targets
    git-svn-id: https://svn.aros.org/svn/aros/trunk@47502 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-efika/kernel/kernel_debug.c
+++ arch/arm-efika/kernel/kernel_debug.c
@@ -1,40 +1,65 @@
 /*
-    Copyright © 1995-2010, The AROS Development Team. All rights reserved.
+    Copyright ï¿½ 1995-2010, The AROS Development Team. All rights reserved.
     $Id:$
 
     Desc: kernel_debug.c
     Lang: english
 */
 
+#include <exec/types.h>
 #include <aros/kernel.h>
 #include <inttypes.h>
 
 #include <kernel_base.h>
 #include <kernel_debug.h>
 
-#define UART1_BASE_ADDR		0x73fbc000
+#include <hardware/mx51_uart.h>
 
-#define ONEMS	(0xb0/4)
-#define UBIR	(0xa4/4)
-#define UBMR	(0xa8/4)
-#define UCR2	(0x84/4)
+static inline void writel(uint32_t *addr, uint32_t value)
+{
+    *(volatile uint32_t *)addr = value;
+}
+
+static inline uint32_t readl(uint32_t *addr)
+{
+    return *(volatile uint32_t *)addr;
+}
 
 inline void waitBusy()
 {
-	volatile uint32_t *uart = (uint32_t *)UART1_BASE_ADDR;
-	while(!(uart[0x98 / 4] & (1 << 3)));
+    MX51_UART *uart = (MX51_UART *)UART1_BASE_ADDR;
+
+    while((readl(&uart->USR2) & UART_USR2_TXDC) == 0);
 }
 
 inline void putByte(uint8_t chr)
 {
-	volatile uint32_t *uart = (uint32_t *)UART1_BASE_ADDR;
-	uart[0x10] = chr;
-	if (chr == '\n')
-		uart[0x10] = '\r';
+    MX51_UART *uart = (MX51_UART *)UART1_BASE_ADDR;
+
+    if (chr == '\n')
+		writel(&uart->UTXD, '\r');
+    writel(&uart->UTXD, chr);
+}
+
+void (*_KrnPutC)(char) = NULL;
+
+void krnSerPutC(int chr)
+{
+	putByte(chr);
+	waitBusy();
 }
 
 int krnPutC(int chr, struct KernelBase *KernelBase)
 {
-	putByte(chr);
-	waitBusy();
+    if (chr == 0x03)
+        _KrnPutC = NULL;
+    else
+    {
+        if (_KrnPutC)
+            _KrnPutC(chr);
+
+        krnSerPutC(chr);
+    }
+
+    return 0;
 }
