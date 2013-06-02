ba77c27efc0ba58716e7644b4932b753a91ddf67
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-06-02 21:30:29 +0000

    include directory for EfikaMX specific files
    git-svn-id: https://svn.aros.org/svn/aros/trunk@47498 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/arm-efika/kernel/include/asm/imx51.h
@@ -0,0 +1,13 @@
+/*
+ * imx51.h
+ *
+ *  Created on: Jun 2, 2013
+ *      Author: michal
+ */
+
+#ifndef ASM_IMX51_H
+#define ASM_IMX51_H
+
+
+
+#endif /* ASM_IMX51_H */
---
+++ arch/arm-efika/kernel/include/hardware/mx51_uart.h
@@ -0,0 +1,13 @@
+/*
+ * mx51_uart.h
+ *
+ *  Created on: Jun 2, 2013
+ *      Author: michal
+ */
+
+#ifndef HARDWARE_MX51_UART_H
+#define HARDWARE_MX51_UART_H
+
+
+
+#endif /* HARDWARE_MX51_UART_H */
