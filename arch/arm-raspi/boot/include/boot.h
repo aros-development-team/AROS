f1749a28fb69833c9cd0898d47c570d9801f57b1
Kalamatee <kalamatee@gmail.com>
2013-06-08 12:37:37 +0000

    store the boot stack and tags at the end of the first page of memory - which will be protected in kernel resource and reused upon reset.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@47512 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-raspi/boot/include/boot.h
+++ arch/arm-raspi/boot/include/boot.h
@@ -11,14 +11,15 @@
 #include <aros/kernel.h>
 
 // Create some temporary storage in .bss for both stack and local allocations.
-#define BOOT_STACK_SIZE		65536
+#define BOOT_STACK_SIZE		(256 << 2)
+#define BOOT_TAGS_SIZE          (128 << 3)
 #define BOOT_TMP_SIZE		524288
+
+#define MAX_BSS_SECTIONS        256
 
 size_t mem_avail();
 size_t mem_used();
 const char *remove_path(const char *in);
-void arm_dcache_invalidate(uint32_t addr, uint32_t length);
-void arm_icache_invalidate(uint32_t addr, uint32_t length);
 void arm_flush_cache(uint32_t addr, uint32_t length);
 
 extern uint8_t __bootstrap_start;
@@ -30,9 +31,7 @@ extern long _binary_core_bin_size;
 
 void kprintf(const char *format, ...);
 
-#define _STR(x)		# x
+#define _STR(x)		#x
 #define STR(x)		_STR(x)
-
-#define MAX_BSS_SECTIONS 256
 
 #endif /* BOOT_H_ */
