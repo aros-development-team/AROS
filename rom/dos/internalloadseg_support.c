6134a91f8073fc83b0d6a9d5ff161bac03662d09
twilen <twilen@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-06-09 12:43:43 +0000

    Check if DOSBase is NULL (Called by m68k RDB filesystem loader) Removed code duplication.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@47522 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/internalloadseg_support.c
+++ rom/dos/internalloadseg_support.c
@@ -7,19 +7,15 @@
 #include "dos_intern.h"
 #include "internalloadseg.h"
 
-void register_elf(BPTR file, BPTR hunks, struct elfheader *eh, struct sheader *sh, struct DosLibrary *DOSBase)
+static char *getname(BPTR file, char **bufferp, struct DosLibrary *DOSBase)
 {
-    if (DebugBase)
-    {
+    if (DOSBase) {
         char *buffer = AllocMem(512, MEMF_ANY);
-
         if (buffer) {
-            if (NameFromFH(file, buffer, 512))
-            {
+            *bufferp = buffer;
+            if (NameFromFH(file, buffer, 512)) {
                 char *nameptr = buffer;
-                struct ELF_DebugInfo dbg = {eh, sh};
-
-    /* gdb support needs full paths */
+                /* gdb support needs full paths */
 #if !AROS_MODULES_DEBUG
                 /* First, go through the name, till end of the string */
                 while(*nameptr++);
@@ -27,37 +23,45 @@ void register_elf(BPTR file, BPTR hunks, struct elfheader *eh, struct sheader *s
                 while(nameptr > buffer && nameptr[-1] != ':' && nameptr[-1] != '/')
                     nameptr--;
 #endif
-                RegisterModule(nameptr, hunks, DEBUG_ELF, &dbg);
+                return nameptr;
             }
-            FreeMem(buffer, 512);
         }
+    }
+    return "unknown";
+}
+
+void register_elf(BPTR file, BPTR hunks, struct elfheader *eh, struct sheader *sh, struct DosLibrary *DOSBase)
+{
+    if (DOSBase && DebugBase)
+    {
+        char *buffer = NULL;
+        char *nameptr = getname(file, &buffer, DOSBase);
+        struct ELF_DebugInfo dbg = {eh, sh};
+        RegisterModule(nameptr, hunks, DEBUG_ELF, &dbg);
+        FreeMem(*buffer, 512);
     }
 }
 
+#undef DebugBase
+/* DOSBase is NULL if called by m68k RDB filesystem loader.
+ * No DosBase = Open debug.library manually, this enables
+ * us to have RDB segments in debug.library list.
+ */
 void register_hunk(BPTR file, BPTR hunks, APTR header, struct DosLibrary *DOSBase)
 {
+    struct Library *DebugBase;
+    if (DOSBase)
+        DebugBase = IDosBase(DOSBase)->debugBase;
+    else
+        DebugBase = OpenLibrary("debug.library", 0);
     if (DebugBase)
     {
-        char *buffer = AllocMem(512, MEMF_ANY);
-
-        if (buffer) {
-            if (NameFromFH(file, buffer, 512))
-            {
-                char *nameptr = buffer;
-                struct HUNK_DebugInfo dbg = { header };
-
-
-    /* gdb support needs full paths */
-#if !AROS_MODULES_DEBUG
-                /* First, go through the name, till end of the string */
-                while(*nameptr++);
-                /* Now, go back until either ":" or "/" is found */
-                while(nameptr > buffer && nameptr[-1] != ':' && nameptr[-1] != '/')
-                    nameptr--;
-#endif
-                RegisterModule(nameptr, hunks, DEBUG_HUNK, &dbg);
-            }
-            FreeMem(buffer, 512);
-        }
+        char *buffer = NULL;
+        char *nameptr = getname(file, &buffer, DOSBase);
+        struct HUNK_DebugInfo dbg = { header };
+        RegisterModule(nameptr, hunks, DEBUG_HUNK, &dbg);
+        FreeMem(*buffer, 512);
     }
+    if (!DOSBase)
+        CloseLibrary(DebugBase);
 }
