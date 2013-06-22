b8c3de1fc5ee8e81d6576aeae4b477b7c786241c
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-06-22 21:56:46 +0000

    Rewritten and debugger GetDrawInfoAttr() to match original's behavior. MOS documentation is very bad. ;-)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@47593 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/intuition/getdrawinfoattr.c
+++ rom/intuition/getdrawinfoattr.c
@@ -10,31 +10,7 @@
 
 #define ARG_MASK 0x0000000F
 
-static int GetDRIPen(struct DrawInfo *drawInfo, ULONG pen, IPTR *errorPtr, struct IntuitionBase *IntuitionBase)
-{
-    UWORD *pens;
-    ULONG max;
-
-    if (drawInfo)
-    {
-        pens = drawInfo->dri_Pens;
-        max  = drawInfo->dri_NumPens;
-    }
-    else
-    {
-        pens = GetPrivIBase(IntuitionBase)->DriPens8;
-        max  = NUMDRIPENS;
-    }
-
-    pen &= ARG_MASK;
-    if (pen >= max)
-    {
-        *errorPtr = TRUE;
-        return 0;
-    }
-
-    return pens[pen];
-}
+#define SetResult(x) if (resultPtr) *resultPtr = x;
 
 /*****************************************************************************
  
@@ -46,37 +22,48 @@ static int GetDRIPen(struct DrawInfo *drawInfo, ULONG pen, IPTR *errorPtr, struc
 /*  SYNOPSIS */
          AROS_LHA(struct DrawInfo *, drawInfo, A0),
          AROS_LHA(ULONG            , attrID, D0),
-         AROS_LHA(IPTR *           , errorPtr, A1),
+         AROS_LHA(IPTR *           , resultPtr, A1),
 
 /*  LOCATION */
          struct IntuitionBase *, IntuitionBase, 156, Intuition)
 
 /*  FUNCTION
          Gets value of the specified attribute from DrawInfo object, or
-         system default value of this attribute
+         system default value (for some attributes).
  
     INPUTS
-         drawInfo - an object pointer to query or NULL to get system default.
+         drawInfo - an object pointer to query. It is possible to set this
+                    argument to NULL when querying GDIA_Color or GDIA_Pen
+                    attributes. In this case values will be retrieved from
+                    system preferences.
          attrID   - ID of the attribute you want. The following IDs are
                     currently defined:
 
-           GDIA_Color       -
-           GDIA_Pen         -
-           GDIA_Version     -
-           GDIA_NumPens     -
-           GDIA_DirectColor -
-           GDIA_Font        -
-           GDIA_Depth       -
-           GDIA_CheckMark   -
-           GDIA_MenuKey     -
-           GDIA_ResolutionX -
-           GDIA_ResolutionY -
+           GDIA_Color       - 0RGB value of the color corresponding to a given pen.
+                              It is possible to retrieve these values only from
+                              DrawInfos belonging to direct-color screens. Pen ID
+                              should be ORed with attribute ID.
+           GDIA_Pen         - LUT color number corresponding to a given pen.
+           GDIA_Version     - Version number of the DrawInfo object.
+           GDIA_DirectColor - TRUE if the DrawInfo belongs to direct-color screen. Note
+                              that in case of failure it also sets success indicator to
+                              FALSE.
+           GDIA_NumPens     - Number of pens or colors defined in this DrawInfo object.
+           GDIA_Font        - Font specified in this DrawInfo.
+           GDIA_Depth       - Depth of this DrawInfo. Note that this attribute will
+                              return real depth of DrawInfo's screen, however dri_Depth
+                              member will contain 8 for AmigaOS(tm) compatibility.
+           GDIA_ResolutionX - X resolution in ticks
+           GDIA_ResolutionY - Y resolution in ticks
+           GDIA_CheckMark   - A pointer to CheckMark image object for the menu.
+           GDIA_MenuKey     - A pointer to Menu (Amiga) key image object for the menu.
 
-         errorPtr - an optional storage area for error indicator. You
-                    can set this parameter to NULL.
+         resultPtr - an optional storage area for success indicator. You
+                     can set this parameter to NULL.
  
     RESULT
-         A value of the specified attribute
+         A value of the specified attribute. resultPtr, if supplied, gets
+         TRUE for success and FALSE for failure.
  
     NOTES
          This function is compatible with MorphOS
@@ -90,130 +77,159 @@ static int GetDRIPen(struct DrawInfo *drawInfo, ULONG pen, IPTR *errorPtr, struc
     INTERNALS
  
 *****************************************************************************/
+
+/*
+ * MorphOS (as of v2.6) quirks which are intentionally not reproduced in this
+ * implementation:
+ *
+ * 1. MorphOS implementation does not accept struct DrawInfo with version
+      different from OS own number.
+ * 2. MorphOS implementation does not check dri_NumPens when reading dri_Pens,
+ *    this is clearly a bug.
+ * 3. In MorphOS GDIA_DirectColor and GDIA_Depth work only if DrawInfo has
+ *    attached screen, consequently returning failure with handmade DrawInfos.
+ */
+
 {
     AROS_LIBFUNC_INIT 
 
     struct GfxBase *GfxBase = GetPrivIBase(IntuitionBase)->GfxBase;
-    int pen;
-    IPTR err;
-    struct Color32 col;
+    ULONG pen;
 
-    if (!errorPtr) errorPtr = &err;
-    *errorPtr = FALSE;
+    SetResult(TRUE);
 
-    switch (attrID & ~ARG_MASK)
+    if (drawInfo)
     {
-    case GDIA_Color:
-        if (drawInfo)
+        /* Most of IDs make sense only with nonzero DrawInfo */
+        switch (attrID)
         {
-            if (!(drawInfo->dri_Flags & DRIF_DIRECTCOLOR))
+        case GDIA_Version:
+            return drawInfo->dri_Version;
+
+        case GDIA_DirectColor:
+            return (drawInfo->dri_Flags & DRIF_DIRECTCOLOR) ? TRUE : FALSE;
+
+        case GDIA_NumPens:
+            return drawInfo->dri_NumPens;
+
+        case GDIA_Font:
+            return (IPTR)drawInfo->dri_Font;
+
+        case GDIA_Depth:
+            return (drawInfo->dri_Version < 3) ? drawInfo->dri_Depth :
+                    GetPrivScreen(drawInfo->dri_Screen)->realdepth;
+
+        case GDIA_CheckMark:
+            if (drawInfo->dri_Version < 2)
             {
-                *errorPtr = TRUE;
+                SetResult(FALSE);
                 return 0;
             }
+            return (IPTR)drawInfo->dri_CheckMark;
+
+        case GDIA_MenuKey:
+            if (drawInfo->dri_Version < 2)
+            {
+                SetResult(FALSE);
+                return 0;
+            }
+            return (IPTR)drawInfo->dri_AmigaKey;
+
+        case GDIA_ResolutionX:
+            return drawInfo->dri_Resolution.X;
+
+        case GDIA_ResolutionY:
+            return drawInfo->dri_Resolution.Y;
         }
 
-        pen = GetDRIPen(drawInfo, attrID, errorPtr, IntuitionBase);
-        if (*errorPtr)
-            return 0;
-
-        if (drawInfo)
+        /*
+         * If we are here, this should be GDIA_Color or GDIA_Pen.
+         * First we need to extract pen ID.
+         */
+        pen = attrID & ARG_MASK;
+        if (pen < drawInfo->dri_NumPens)
         {
-            /* We have DrawInfo, decode the color using screen's ColorMap */
-            GetRGB32(drawInfo->dri_Screen->ViewPort.ColorMap, pen, 1, &col.red);
+            switch (attrID & ~ARG_MASK)
+            {
+            case GDIA_Color:
+                /*
+                 * According to original MorphOS semantics we return RGB colors
+                 * only for direct-color screens.
+                 */
+                if (drawInfo->dri_Flags & DRIF_DIRECTCOLOR)
+                {
+                    ULONG col[3];
+
+                    GetRGB32(drawInfo->dri_Screen->ViewPort.ColorMap, drawInfo->dri_Pens[pen], 1, col);
+                    return ((col[0] & 0xFF000000) >>  8) |
+                           ((col[1] & 0xFF000000) >> 16) |
+                            (col[2]               >> 24);
+                }
+                break;
+
+            case GDIA_Pen:
+                return drawInfo->dri_Pens[pen];
+            }
         }
-        else if (pen < 4)
+    }
+    else
+    {
+        /*
+         * No DrawInfo supplied.
+         * In this case we return only default colors/pens from our preferences.
+         */
+        pen = attrID & ARG_MASK;
+        if (pen < NUMDRIPENS)
         {
-            /* System default, first 4 pens */
-            col = GetPrivIBase(IntuitionBase)->Colors[pen];
+            struct Color32 *col;
+
+            /* Extract color number corresponding to the pen */
+            pen = GetPrivIBase(IntuitionBase)->DriPens8[pen];
+
+            switch (attrID & ~ARG_MASK)
+            {
+            case GDIA_Color:
+                /*
+                 * In AROS we don't have separate preferences for direct-color
+                 * screens. So we now decode color number into its RGB representation
+                 * according to system palette.
+                 */
+                if (pen < 4)
+                {
+                    /* First 4 colors */
+                    col = &GetPrivIBase(IntuitionBase)->Colors[pen];
+                }
+                else if ((pen > 16) && (pen < 20))
+                {
+                    /* These are pointer colors, indexes 8...10 in system palette */
+                    col = &GetPrivIBase(IntuitionBase)->Colors[pen - 17 + 8];
+                }
+                else
+                {
+                    /*
+                     * There's no known color value for other pens.
+                     * We also have 4 last colors defined, however we don't know which
+                     * pens correspond to them, because this depends on screen depth.
+                     * Well, after all, we are not MOS. :)
+                     */
+                    break;
+                }
+
+                return ((col->red   & 0xFF000000) >>  8) |
+                       ((col->green & 0xFF000000) >> 16) |
+                       ( col->blue                >> 24);
+
+            case GDIA_Pen:
+                return pen;
+            }
         }
-        else if ((pen > 16) && (pen < 20))
-        {
-            /* System default, pointer pens */
-            col = GetPrivIBase(IntuitionBase)->Colors[pen - 9];
-        }
-        else
-        {
-            /* There's no known color value for other pens */
-            return 0;
-        }
-
-        return ((col.red   & 0xFF000000) >>  8) |
-               ((col.green & 0xFF000000) >> 16) |
-               ( col.blue                >> 24);
-
-    case GDIA_Pen:
-        return GetDRIPen(drawInfo, attrID, errorPtr, IntuitionBase);
-
-    case GDIA_Version:
-        return drawInfo ? drawInfo->dri_Version : DRI_VERSION;
-
-    case GDIA_NumPens:
-        return drawInfo ? drawInfo->dri_NumPens : NUMDRIPENS;
-
-    case GDIA_DirectColor:
-        if (!drawInfo)
-        {
-            /* CHECKME: Is this correct ? */
-            return FALSE;
-        }
-
-        return (drawInfo && drawInfo->dri_Flags & DRIF_DIRECTCOLOR) ? TRUE : FALSE;
-
-    case GDIA_Font:
-        return (IPTR)(drawInfo ? drawInfo->dri_Font : GfxBase->DefaultFont);
-
-    case GDIA_Depth:
-        if (!drawInfo)
-        {
-            /*
-             * Default value for SA_Depth is OpenScreenTagList() is 1.
-             * CHECKME: Is this correct ?
-             */
-            return 1;
-        }
-
-        return (drawInfo->dri_Version < 3) ? drawInfo->dri_Depth :
-                GetPrivScreen(drawInfo->dri_Screen)->realdepth;
-
-    case GDIA_CheckMark:
-        if ((!drawInfo) || (drawInfo->dri_Version < 2))
-        {
-            *errorPtr = TRUE;
-            return 0;
-        }
-
-        return (IPTR)drawInfo->dri_CheckMark;
-
-    case GDIA_MenuKey:
-        if ((!drawInfo) || (drawInfo->dri_Version < 2))
-        {
-            *errorPtr = TRUE;
-            return 0;
-        }
-
-        return (IPTR)drawInfo->dri_AmigaKey;
-
-    case GDIA_ResolutionX:
-        if (!drawInfo)
-        {
-            *errorPtr = TRUE;
-            return 0;
-        }
-
-        return drawInfo->dri_Resolution.X;
-
-    case GDIA_ResolutionY:
-        if (!drawInfo)
-        {
-            *errorPtr = TRUE;
-            return 0;
-        }
-
-        return drawInfo->dri_Resolution.Y;
     }
 
-    *errorPtr = TRUE;
+    /*
+     * Unknown IDs as well as failed conditions end up here.
+     * Report error and return zero.
+     */
+    SetResult(FALSE);
     return 0;
 
     AROS_LIBFUNC_EXIT
