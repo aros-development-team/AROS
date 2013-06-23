77a61380e547c2dc2d57aa20bc79fd2b9e6f004d
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-06-23 10:49:46 +0000

    Debugging feature: report short names as file comments when compiled with DEBUG_NAMES enabled.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@47596 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/filesys/fat/lock.c
+++ rom/filesys/fat/lock.c
@@ -2,7 +2,7 @@
  * fat.handler - FAT12/16/32 filesystem handler
  *
  * Copyright © 2006 Marek Szyprowski
- * Copyright © 2007-2011 The AROS Development Team
+ * Copyright © 2007-2013 The AROS Development Team
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the same terms as AROS itself.
@@ -186,6 +186,9 @@ LONG LockFile(ULONG dir_cluster, ULONG dir_entry, LONG access, struct ExtFileLoc
 
         GetDirEntryShortName(&de, &(gl->name[1]), &len); gl->name[0] = (UBYTE) len;
         GetDirEntryLongName(&de, &(gl->name[1]), &len); gl->name[0] = (UBYTE) len;
+#if DEBUG_NAMES
+        GetDirEntryShortName(&de, &(gl->shortname[1]), &len); gl->shortname[0] = (UBYTE) len;
+#endif
 
         ReleaseDirHandle(&dh);
 
