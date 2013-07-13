0efb703a8a7ebb937291b68354cb8f21344730de
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-07-13 17:28:59 +0000

    arosc.library: bring back %qd support
    git-svn-id: https://svn.aros.org/svn/aros/trunk@47668 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/clib/sprintf.c
@@ -0,0 +1,55 @@
+#include "test.h"
+#include <stdio.h>
+#include <string.h>
+
+#define TESTNUMBER1         11
+#define TESTNUMBER1STRLEN   2
+#define TEST1RESULT         "11"
+#define TEST2RESULT         "11"
+#define TEST3RESULT         "11"
+
+#define BUFSIZE 10
+
+static void cleanbuffer(char * buf)
+{
+    memset(buf, 0xff, BUFSIZE);
+}
+
+static int stringsame(char * c1, char * c2, int size)
+{
+    int i;
+    for(i = 0; i < size; i++)
+        if (c1[i] != c2[i]) return 0;
+    return 1;
+}
+
+int main()
+{
+    char buf[BUFSIZE];
+    int n1 = TESTNUMBER1;
+    long long n2 = TESTNUMBER1;
+    long long n3 = TESTNUMBER1;
+
+
+    /* check standard %d conversion */
+    cleanbuffer(buf);
+    TEST((sprintf(buf, "%d", n1) == TESTNUMBER1STRLEN));
+    TEST((stringsame(buf, TEST1RESULT, TESTNUMBER1STRLEN) == 1));
+
+    /* check standard %qd conversion */
+    cleanbuffer(buf);
+    TEST((sprintf(buf, "%qd", n2) == TESTNUMBER1STRLEN));
+    TEST((stringsame(buf, TEST2RESULT, TESTNUMBER1STRLEN) == 1));
+
+    /* check standard %lld conversion */
+    cleanbuffer(buf);
+    TEST((sprintf(buf, "%lld", n3) == TESTNUMBER1STRLEN));
+    TEST((stringsame(buf, TEST3RESULT, TESTNUMBER1STRLEN) == 1));
+
+    return OK;
+}
+
+void cleanup()
+{
+
+}
