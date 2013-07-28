d14e2b4778a7ebd5c85d5e1d018defe5e88c8bdb
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-07-28 20:28:21 +0000

    test/vfork_execl.c: Tabs->spaces
    git-svn-id: https://svn.aros.org/svn/aros/trunk@47774 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/clib/vfork_execl.c
+++ test/clib/vfork_execl.c
@@ -11,55 +11,55 @@ int main()
     pid = vfork();
     if((int) pid > 0)
     {
-	printf("I'm parent, I have a child with pid %d\n", (int) pid);
-	waitpid(pid, NULL, 0);
+        printf("I'm parent, I have a child with pid %d\n", (int) pid);
+        waitpid(pid, NULL, 0);
     }
     else if(pid == 0)
     {
-	execl("C:Echo", "Echo", "I'm child", NULL);
-	_exit(1);
+        execl("C:Echo", "Echo", "I'm child", NULL);
+        _exit(1);
     }
     else
     {
-	TEST(0);
+        TEST(0);
     }
   
     pid = vfork();
     if((int) pid > 0)
     {
-	printf("I'm parent, I have a first child with pid %d\n", (int) pid);
-	pid_t pid2 = vfork();
-	if((int) pid2 > 0)
-	{
-	    printf("I'm parent, I have a second child with pid %d\n", (int) pid2);
-	    waitpid(pid2, NULL, 0);
-	}
-	else if(pid2 == 0)
-	{
-	    execl("C:Echo", "Echo", "I'm the second child of a parent", NULL);
-	    _exit(1);
-	}
-	waitpid(pid, NULL, 0);
+        printf("I'm parent, I have a first child with pid %d\n", (int) pid);
+        pid_t pid2 = vfork();
+        if((int) pid2 > 0)
+        {
+            printf("I'm parent, I have a second child with pid %d\n", (int) pid2);
+            waitpid(pid2, NULL, 0);
+        }
+        else if(pid2 == 0)
+        {
+            execl("C:Echo", "Echo", "I'm the second child of a parent", NULL);
+            _exit(1);
+        }
+        waitpid(pid, NULL, 0);
     }
     else if(pid == 0)
     {
-	//printf("I'm child of a parent\n");
-	pid_t pid2 = vfork();
-	if((int) pid2 > 0)
-	{
-	    //printf("I'm child, I have my child with pid %d\n", (int) pid2);
-	    waitpid(pid2, NULL, 0);
-	}
-	else if(pid2 == 0)
-	{
-	    execl("C:Echo", "Echo", "I'm child of a child", NULL);
-	    _exit(1);
-	}
-	_exit(0);
+        //printf("I'm child of a parent\n");
+        pid_t pid2 = vfork();
+        if((int) pid2 > 0)
+        {
+            //printf("I'm child, I have my child with pid %d\n", (int) pid2);
+            waitpid(pid2, NULL, 0);
+        }
+        else if(pid2 == 0)
+        {
+            execl("C:Echo", "Echo", "I'm child of a child", NULL);
+            _exit(1);
+        }
+        _exit(0);
     }
     else
     {
-	TEST(0);
+        TEST(0);
     }
 
     return OK;
