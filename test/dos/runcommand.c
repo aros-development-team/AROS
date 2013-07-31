b352e5b1d8e2f2fc1cbaba4eeecb82706896daef
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-07-31 17:28:23 +0000

    Revert "WIP: test/dos/runcommand.c"
    This reverts commit e1dac26d62b47cc01e825faad39dc305ea1522ae.
    I messed up.
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@47786 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/dos/runcommand.c
+++ test/dos/runcommand.c
@@ -6,42 +6,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int lastresult = RETURN_OK;
-struct Task *parent;
-LONG parent_signal;
-
 static LONG get_default_stack_size()
 {
     struct CommandLineInterface *cli = Cli();
     return cli->cli_DefaultStack * CLI_DEFAULTSTACK_UNIT;
 }
 
-LONG launcher(void)
-{
-    char *cmd="C:Echo", *args = "RunCommand In Child";
-    BPTR seglist;
-
-    FPuts(Output(), "Child calls Echo\n");
-    seglist = LoadSeg(cmd);
-    if (seglist != BNULL)
-    {
-        SetProgramName(cmd);
-        lastresult = RunCommand(
-            seglist,get_default_stack_size(),args,strlen(args)
-        );
-        UnLoadSeg(seglist);
-    }
-    else
-        FPuts(Output(), "Loading of Echo failed\n");
-    Signal(parent, 1 << parent_signal);
-
-    return 0;
-}
-
 int main(int argc, char **argv)
 {
     char *fname = "SYS:Utilities/Clock";
     char *full = "";
+    int lastresult = RETURN_OK;
     
     if(fname) {
         BPTR seglist = LoadSeg(fname);
@@ -53,35 +28,6 @@ int main(int argc, char **argv)
             UnLoadSeg(seglist);
         }
     }
-
-    if (lastresult != RETURN_OK)
-        goto done;
-
-    struct TagItem tags[] =
-    {
-        { NP_Entry,         (IPTR) launcher },
-        { NP_CloseInput,    (IPTR) FALSE },
-        { NP_CloseOutput,   (IPTR) FALSE },
-        { NP_CloseError,    (IPTR) FALSE },
-        { NP_Cli,           (IPTR) TRUE },
-        { NP_Name,          (IPTR) "runcommand_child" },
-        { NP_NotifyOnDeath, (IPTR) TRUE },
-        { TAG_DONE, 0 }
-    };
-    parent = FindTask(NULL);
-    parent_signal = AllocSignal(-1);
-    if (parent_signal == -1)
-    {
-        FPuts(Output(), "Signal Allocation failed\n");
-        lastresult = 20;
-        goto done;
-    }
-    FPuts(Output(), "Starting child...\n");
-    CreateNewProc(tags);
-    Wait(1 << parent_signal);
-    FreeSignal(parent_signal);
-    FPuts(Output(), "Child finished\n");
-
- done:
+    
     exit(lastresult);
 }
