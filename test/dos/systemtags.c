a4fa8674e9decba8ca740da728622dc83610c97f
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-08-06 13:30:48 +0000

    test/dos/systemtags: Extended tests for Input()/Output() mangling.
    Currently this seems to not behave as needed.
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@47791 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/dos/systemtags.c
+++ test/dos/systemtags.c
@@ -1,6 +1,11 @@
 #include <proto/dos.h>
 #include <proto/alib.h>
 
+#include "systemtags.h"
+
+#define DEBUG 0
+#include <aros/debug.h>
+
 int main(void)
 {
     LONG result;
@@ -20,6 +25,25 @@ int main(void)
     result = SystemTags("DoesntExist",
         SYS_Asynch, TRUE, SYS_Input, SYS_DupStream, SYS_Output, SYS_DupStream,
         TAG_DONE
+    );
+    Printf("Execution results %ld == 0 ?\n", result);
+
+    /* Test with duplicated Input/Output stream */
+    result = SystemTags("systemtags_slave dont print",
+        SYS_Input, SYS_DupStream, SYS_Output, SYS_DupStream,
+        TAG_DONE
+    );
+    Printf("Execution results %ld == 0 ?\n", result);
+
+    /* Test passing of Input/Output streams */
+    struct STData data;
+    data.input = Input();
+    data.output = Output();
+    D(bug("[systemtags]Input: %p, Output: %p\n", data.input, data.output));
+    UBYTE buf[30];
+    __sprintf(buf, "systemtags_slave %p", &data);
+    result = SystemTags(buf, SYS_Input, BNULL, SYS_Output, BNULL,
+                        TAG_DONE
     );
     Printf("Execution results %ld == 0 ?\n", result);
 
---
+++ test/dos/systemtags_slave.c
@@ -0,0 +1,41 @@
+#include <proto/dos.h>
+#include <proto/alib.h>
+#include <dos/dos.h>
+
+#include <stdio.h>
+
+#include "systemtags.h"
+
+#define DEBUG 0
+#include <aros/debug.h>
+
+int main(int argc, char *argv[])
+{
+    struct STData *data;
+    BPTR input, output;
+    UBYTE buf[80];
+
+    if (argc == 2)
+    {
+        sscanf(argv[1], "%p", &data);
+
+        D(bug("[systemtags_slave]Input: %p, Output: %p\n",
+              data->input, data->output
+        ));
+
+          input = SelectInput(data->input);
+          output = SelectOutput(data->output);
+    }
+
+    FPuts(Output(), "Type something (+return):\n");
+    FGets(Input(), buf, 80);
+    FPrintf(Output(), "Got: '%s'\n", buf);
+
+    if (argc == 2)
+    {
+        SelectInput(input);
+        SelectOutput(output);
+    }
+
+    return 0;
+}
