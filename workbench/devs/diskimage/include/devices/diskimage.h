c636fe480fd44839038d43fafd9282fd70c09fe5
ezrec <jason.mcmullan@gmail.com>
2013-09-04 18:12:05 +0000

    diskimage.device: If we are using GCC, simplify the PLUGIN_NODE() macro
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@47965 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/devs/diskimage/include/devices/diskimage.h
+++ workbench/devs/diskimage/include/devices/diskimage.h
@@ -141,10 +141,14 @@ struct DiskImagePluginTable plugin_table = { \
 };
 #endif
 
+#ifdef __GNUC__
+#define PLUGIN_NODE(pri,name) { .ln_Name = name, .ln_Pri = pri }
+#else
 #if defined(__AROS__) && defined(AROS_FLAVOUR) && !(AROS_FLAVOUR & AROS_FLAVOUR_BINCOMPAT)
 #define PLUGIN_NODE(pri,name) { NULL, NULL, name, 0, pri }
 #else
 #define PLUGIN_NODE(pri,name) { NULL, NULL, 0, pri, name }
+#endif
 #endif
 
 #ifdef USED_PLUGIN_API_VERSION
