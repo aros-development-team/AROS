168cee11aed197a4ccde9901f266e4a720419358
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-09-05 11:03:53 +0000

    Avoid redrawing the list when the same entry is clicked on again.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@47981 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/classes/list.c
+++ workbench/libs/muimaster/classes/list.c
@@ -1060,6 +1060,11 @@ IPTR List__OM_SET(struct IClass *cl, Object *obj, struct opSet *msg)
             _handle_bool_tag(data->flags, tag->ti_Data, LIST_DRAGSORTABLE);
             set(obj, MUIA_Draggable, tag->ti_Data);
             break;
+
+        case MUIA_Selected:
+            /* Swallow this so the Area class doesn't redraw us */
+            tag->ti_Tag = TAG_IGNORE;
+            break;
         }
     }
 
