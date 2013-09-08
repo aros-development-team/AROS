3b1da611a5a3783d1de682a78049a3ce96715c04
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-09-08 21:21:07 +0000

    arosc_emul.c/emul_arosc.c: Remove most of the functions from c emul code.
    The stdc.library and stdcio.library versions of the functions can be
    used now.
    These libraries are now peropenerbase and so modules can have their own private
    libbase for these libraries.
    
    abort() and exit() will Guru when called from a library and non appropriately
    initialized. This is as good as going into endless loop.
    The functions gettimeofday(), usleep() are part of posixc.library and remain
    in the emul files.
    The atexit() implementation for mesa also stays; maybe this should be looked at
    further how this exactly has to be implemented.
    
    For SDL.library now an internal dummy sigaction() is provided.
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@48057 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/hidds/softpipe/arosc_emul.c
+++ workbench/hidds/softpipe/arosc_emul.c
@@ -1,297 +1,41 @@
 /*
-    Copyright 2009, The AROS Development Team. All rights reserved.
+    Copyright 2009-2013, The AROS Development Team. All rights reserved.
     $Id$
 */
 
-#include <aros/symbolsets.h>
+#include <proto/exec.h>
 #include <proto/dos.h>
-
-#define DEBUG 0
-#include <aros/debug.h>
-
-#include <sys/types.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <errno.h>
+#include <proto/timer.h>
 
 /* 
-    The purpose of this file is to provide implementation for C functions which are used by Mesa/Gallium
-    and are not located in librom.a
-
-    Mesa/Gallium cannot use arosc.library because arosc.library context is bound with caller task
-    and gets destroyed when the task exits. mesa.library at that point is still in RAM and
-    crashes at expunge.
+    The purpose of this file is to provide implementation for C functions part
+    of arosnixc.library in code where one does not want to use this library.
 */
 
-#define IMPLEMENT()  bug("------IMPLEMENT(%s)\n", __func__)
-
-/* malloc/calloc/realloc/free */
-
-/* This is a copy of implementation from arosc.library so that mesa.library uses its 
-   own space for malloc/calloc/realloc calls */
-
-#define MEMALIGN_MAGIC ((size_t) 0x57E2CB09)
-APTR __mempool = NULL;
-
-void * malloc (size_t size)
-{
-    UBYTE *mem = NULL;
-
-    D(bug("arosc_emul_malloc\n"));
-    /* Allocate the memory */
-    mem = AllocPooled (__mempool, size + AROS_ALIGN(sizeof(size_t)));
-    if (mem)
-    {
-        *((size_t *)mem) = size;
-        mem += AROS_ALIGN(sizeof(size_t));
-    }
-    else
-        errno = ENOMEM;
-
-    return mem;
-}
-
-void free (void * memory)
-{
-    D(bug("arosc_emul_free\n"));
-    if (memory)
-    {
-        unsigned char *mem;
-        size_t         size;
-
-        mem = ((UBYTE *)memory) - AROS_ALIGN(sizeof(size_t));
-
-        size = *((size_t *) mem);
-        if (size == MEMALIGN_MAGIC)
-            free(((void **) mem)[-1]);
-        else 
-        {
-            size += AROS_ALIGN(sizeof(size_t));
-            FreePooled (__mempool, mem, size);
-        }
-    }
-}
-
-void * calloc (size_t count, size_t size)
-{
-    ULONG * mem;
-    D(bug("arosc_emul_calloc\n"));
-    size *= count;
-
-    /* Allocate the memory */
-    mem = malloc (size);
-
-    if (mem)
-        memset (mem, 0, size);
-
-    return mem;
-}
-
-void * realloc (void * oldmem, size_t size)
-{
-    
-    UBYTE * mem, * newmem;
-    size_t oldsize;
-    
-    D(bug("arosc_emul_realloc\n"));
-    
-    if (!oldmem)
-        return malloc (size);
-
-    mem = (UBYTE *)oldmem - AROS_ALIGN(sizeof(size_t));
-    oldsize = *((size_t *)mem);
-
-    /* Reduce or enlarge the memory ? */
-    if (size < oldsize)
-    {
-        /* Don't change anything for small changes */
-        if ((oldsize - size) < 4096)
-            newmem = oldmem;
-        else
-            goto copy;
-    }
-    else if (size == oldsize) /* Keep the size ? */
-        newmem = oldmem;
-    else
-    {
-copy:
-        newmem = malloc (size);
-
-        if (newmem)
-        {
-            CopyMem (oldmem, newmem, size);
-            free (oldmem);
-        }
-    }
-
-    return newmem;
-}
-
-char *getenv (const char *name)
-{ 
-    /* This function is not thread-safe */
-    static TEXT buff[128] = { };
-    
-    D(bug("arosc_emul_getenv: %s\n", name));
-
-    if (GetVar(name, buff, 128, GVF_BINARY_VAR) == -1)
-        return NULL;
-    else
-        return buff;
-}
-
 struct timezone;
+
 int gettimeofday (struct timeval * tv,struct timezone * tz)
 {
-    IMPLEMENT();
-    return 0;
-}
+    struct MsgPort * timerport = CreateMsgPort();
+    struct timerequest * timereq = (struct timerequest *)CreateIORequest(timerport, sizeof(*timereq));
 
-__noreturn void abort (void)
-{
-    IMPLEMENT();
-    for (;;);
-}
 
-__noreturn void exit (int code)
-{
-    IMPLEMENT();
-    for (;;);
-}
-	
-int puts (const char * str)
-{
-    bug("%s\n", str);
-    return 1;
-}
-
-int putchar(int c)
-{
-    bug("%c", c);
-    return c;
-}
-
-char * strdup (const char * orig)
-{
-    char * copy;
-    char * ptr;
-
-    if ((copy = malloc (strlen (orig)+1)))
+    if (timereq)
     {
-	ptr = copy;
+        if (OpenDevice("timer.device", UNIT_VBLANK, (struct IORequest *)timereq, 0) == 0)
+        {
+            #define TimerBase ((struct Device *)timereq->tr_node.io_Device)
 
-	while ((*ptr ++ = *orig ++));
+            GetSysTime(tv);
+            
+            #undef TimerBase
+            
+            CloseDevice((struct IORequest *)timereq);
+        }
     }
+    
+    DeleteIORequest((struct IORequest *)timereq);
+    DeleteMsgPort(timerport);
 
-    return copy;
-}
-	
-double atof (const char * str)
-{
-    return strtod (str, (char **)NULL);
-}
-
-int printf (const char * format, ...)
-{
-    IMPLEMENT();
-    return EOF;
-}
-
-
-/* File operations */
-FILE * fopen (const char * pathname, const char * mode)
-{
-    IMPLEMENT();
-    return NULL;
-}
-
-int fclose (FILE * stream)
-{
-    IMPLEMENT();
-    return EOF;
-}
-
-int fputc (int c, FILE * stream)
-{
-    IMPLEMENT();
-    return EOF;
-}
-
-int fputs (const char * str, FILE * stream)
-{
-    IMPLEMENT();
-    return EOF;
-}
-
-int fgetc (FILE * stream)
-{
-    IMPLEMENT();;
-    return EOF;
-}
-
-size_t fwrite (const void * restrict buf, size_t size, size_t nblocks, FILE * restrict stream)
-{
-    IMPLEMENT();
     return 0;
 }
-
-int fflush (FILE * stream)
-{
-    IMPLEMENT();
-    return EOF;
-}
-
-size_t fread (void * buf, size_t size, size_t nblocks, FILE * stream)
-{
-    IMPLEMENT();
-    return 0;
-}
-
-int ferror (FILE * stream)
-{
-    IMPLEMENT();
-    return TRUE;
-}
-
-void clearerr (FILE * stream)
-{
-    IMPLEMENT();
-}
-
-int fprintf (FILE * fh, const char * format, ...)
-{
-    IMPLEMENT();
-    return 0;
-}
-
-int vfprintf (FILE * stream, const char * format, va_list args)
-{
-    IMPLEMENT();
-    return 0;
-}
-	
-int __init_emul(void)
-{
-    /* malloc/calloc/realloc/free */
-    __mempool = CreatePool(MEMF_ANY | MEMF_SEM_PROTECTED, 65536L, 4096L);
-
-    if (!__mempool)
-    {
-        return 0;
-    }
-
-    return 1;
-}
-
-
-void __exit_emul(void)
-{
-    /* malloc/calloc/realloc/free */
-    if (__mempool)
-    {
-        DeletePool(__mempool);
-    }
-}
-
-ADD2INIT(__init_emul, 0);
-ADD2EXIT(__exit_emul, 0);
