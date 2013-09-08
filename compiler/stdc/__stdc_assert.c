3cc78fa5b5329b14d21bbd3c8463b7bf4849b7e1
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-09-08 21:19:42 +0000

    [csplit] Move assert() support function to shared libraries.
    Three versions are implemented:
     * __stdc_assert() in stdc.library that uses a requester.
     * __stdcdos_assert() in stdcio.library that uses that library's I/O functions.
     * __nixc_assert() in posixc.library that uses that library's I/O functions.
     * delint by Jason S. McMullan
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@48050 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/stdc/__stdc_assert.c
+++ compiler/stdc/__stdc_assert.c
@@ -1,51 +1,79 @@
 /*
-    Copyright © 1995-2003, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2013, The AROS Development Team. All rights reserved.
     $Id$
 
-    assert()
+    C99 assert() stdc.library support
 */
-#include <assert.h>
-#include <stdio.h>
+#include <proto/exec.h>
+#include <proto/intuition.h>
+#include <aros/arossupportbase.h>
+#include <intuition/intuition.h>
+#include <aros/debug.h>
+
 #include <stdlib.h>
+
+#include "__optionallibs.h"
 
 /*****************************************************************************
 
-    NAME
+    NAME */
 #include <assert.h>
 
-	void assert (
+	void __stdc_assert (
 
-    SYNOPSIS
-	expr)
+/*  SYNOPSIS */
+	const char * expr,
+        const char * file,
+        unsigned int line)
 
-    FUNCTION
-	Evaluates the expression expr and if it's FALSE or NULL, then
-	printf a message and stops the program. The message will
-	contain the expression, the name of the file with the assert
-	in it and the line in the file.
+/*  FUNCTION
+        This is a function that is used for implementation of the C99 assert()
+        function.
 
     INPUTS
 	expr - The expression to evaluate. The type of the expression does
 		not matter, only if its zero/NULL or not.
+        file - Name of the source file.
+        line - Line number of assert() call.
 
     RESULT
-	The function doesn't return.
+        The function doesn't return.
 
     NOTES
+        Different versions of this function are available. This function
+        is used when a program is using stdc.library but not
+        stdcio.library or posixc.library.
+        Because no normal DOS file I/O is available an attempt will be made
+        to display the assertion in a requester and thus deviating from the
+        C99 standard that says it to go to the error stream.
 
     EXAMPLE
-	// Make sure that x equals 1
-	assert (x==1);
 
     BUGS
 
     SEE ALSO
+        assert()
 
     INTERNALS
+
 ******************************************************************************/
-void __assert (const char * expr, const char * file, unsigned int line)
 {
-    fprintf (stderr, "Assertion (%s) failed in %s:%u\n", expr, file, line);
-    exit (10);
-} /* assert */
+    if (__intuition_available())
+    {
+        struct EasyStruct es =
+            {
+                sizeof(struct EasyStruct),
+                0,
+                (CONST_STRPTR)"Failed Assertion",
+                (CONST_STRPTR)"Assertion (%s) failed in %s:%u",
+                (CONST_STRPTR)"OK"
+            };
+        EasyRequest(NULL, &es, NULL, expr, file, line);
+    }
+    else
+        kprintf("Assertion (%s) failed in %s:%u", expr, file, line);
+
+    abort();
+}
+
 
