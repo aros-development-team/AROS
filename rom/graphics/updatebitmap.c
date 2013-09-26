da616741d6bf776beccae825cf318e046ea3d01a
Kalamatee <kalamatee@gmail.com>
2013-09-26 23:56:51 +0000

    bump copyright on affected files
    git-svn-id: https://svn.aros.org/svn/aros/trunk@48144 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/graphics/updatebitmap.c
+++ rom/graphics/updatebitmap.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 2011, The AROS Development Team. All rights reserved.
+    Copyright © 2011-2013, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: Update specified region of the bitmap, taking software composition into account.
