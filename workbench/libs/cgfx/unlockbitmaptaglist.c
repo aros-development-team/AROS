320a4d27ec5afb014441f4f8f44715e96e2834d4
Kalamatee <kalamatee@gmail.com>
2013-10-09 02:14:37 +0000

    implement updating of RectList entries. bump version to reflect recent changes.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@48267 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/cgfx/unlockbitmaptaglist.c
+++ workbench/libs/cgfx/unlockbitmaptaglist.c
@@ -6,13 +6,24 @@
     Lang: english
 */
 
+#define DEBUG 0
 #include <aros/debug.h>
-#include <cybergraphx/cybergraphics.h>
-#include <hidd/graphics.h>
+
 #include <proto/oop.h>
 #include <proto/utility.h>
 
+#include <cybergraphx/cybergraphics.h>
+#include <hidd/graphics.h>
+
 #include "cybergraphics_intern.h"
+
+
+struct RectList
+{
+     ULONG rl_num;             // no. of rects in this list
+     struct RectList *rl_next; // pointer to next list
+     struct Rectangle rect1;   // First Rectangle in the list
+};
 
 /*****************************************************************************
 
@@ -57,13 +68,13 @@
 *****************************************************************************/
 {
     AROS_LIBFUNC_INIT
-    
+
     struct TagItem *tag;
     BOOL reallyunlock = TRUE;
     struct RectList *rl = NULL;
     struct BitMap *bm = (struct BitMap *)Handle;
 
-    if (bm)
+    if ((bm) && (IS_HIDD_BM(bm)))
     {
         while ((tag = NextTagItem(&Tags)))
         {
@@ -72,28 +83,50 @@
                 case UBMI_REALLYUNLOCK:
                     reallyunlock = (BOOL)tag->ti_Data;
                     break;
-                    
+
                 case UBMI_UPDATERECTS:
                 {
                     rl = (struct RectList *)tag->ti_Data;
                     break;
                 }
-            
+
                 default:
-                    D(bug("!!! UNKNOWN TAG PASSED TO UnLockBitMapTagList() !!!\n"));
+                    D(bug("[Cybergfx] %s: !!! UNKNOWN TAG %08x !!!\n", __PRETTY_FUNCTION__, tag->ti_Tag));
                     break;
             }
         }
-        
+
         if (reallyunlock)
             HIDD_BM_ReleaseDirectAccess(HIDD_BM_OBJ(bm));
 
         if (rl)
         {
-            
+            while (rl)
+            {
+                struct Rectangle *rectCurrent;
+                int count;
+
+                if ((count = rl->rl_num) > 0)
+                {
+                    D(bug("[Cybergfx] %s: RectList @ 0x%p [%d entries]\n", __PRETTY_FUNCTION__, rl, rl->rl_num));
+
+                    for (rectCurrent = &rl->rect1; count > 0; count--)
+                    {
+                        D(bug("[Cybergfx] %s: Updating BitMap Rect [%d, %d -> %d, %d]\n", __PRETTY_FUNCTION__, rectCurrent->MinX, rectCurrent->MinY, rectCurrent->MaxX, rectCurrent->MaxY));
+                        UpdateBitMap(bm, rectCurrent->MinX, rectCurrent->MinY, rectCurrent->MaxX - rectCurrent->MinX + 1, rectCurrent->MaxY - rectCurrent->MinY + 1);
+                        rectCurrent = (struct Rectangle *)((int)rectCurrent + sizeof(struct Rectangle));
+                    }
+                }
+                rl = rl->rl_next;
+            }
         }
         else
             UpdateBitMap(bm, 0, 0, GetCyberMapAttr(bm, CYBRMATTR_WIDTH), GetCyberMapAttr(bm, CYBRMATTR_HEIGHT));
     }
+    else
+    {
+        D(bug("[Cybergfx] %s: Called on Illegal BitMap @ 0x%p\n", __PRETTY_FUNCTION__, bm));
+    }
+
     AROS_LIBFUNC_EXIT
 } /* UnLockBitMapTagList */
