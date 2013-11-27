128afd056ee9f61b95d221fc38c615783aa4efc7
mattrust <mrustler@gmx.de>
2013-11-27 00:03:50 +0000

    copy_files builder added. Cleanup, documenting, commenting.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@48461 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- tools/metamakeng/mmbuildenv.py
+++ tools/metamakeng/mmbuildenv.py
@@ -1,36 +1,146 @@
 # -*- coding: iso-8859-15 -*-
 
+import glob
+
 import mmfunction
 
 class BuildEnv:
     def __init__(self, project):
         self.project = project
 
+    ###############################################################################################
 
-    def add_virtual_metatarget(self, targetname, dependencies):
-        self.project.targets.add_target(targetname, None, False, dependencies)
-
+    # support functions
 
     def get_variable(self, name):
+        """Return value of a variable.
+
+        Metamake keeps a dictionary of variables which are read
+        from config files. If the variable doesn' exist it is
+        read from the system environment. If that fails an exception
+        is raised.
+
+        Keyword arguments:
+        name -- the name of the variable whose value should be returned
+
+        """
         return self.project.vars[name]
 
 
     def substitute(self, str):
+        """Substitute patterns in a string.
+
+        Substitute $(VARIBLE) entries in a string by the value
+        of that variable. Returns the changed string.
+
+        Keyword arguments:
+        str -- the string whose templates should be substituted
+
+        """
         return self.project.vars.subst(str)
 
 
     def add_function(self, targetname, function):
-        self.add_virtual_metatarget(targetname, [])             # metatarget must exist before we can add functions
-        self.project.targets.add_function(targetname, function) # add the function object
+        """Bind a builder function to a metatarget.
 
+        Keyword arguments:
+        targetname -- the metatarget name
+        function -- the builder function object
+
+        """
+        # metatarget must exist before we can add functions
+        self.add_virtual_metatarget(targetname)
+         # add the function object
+        self.project.targets.add_function(targetname, function)
+
+
+    def split(self, str, subst=False):
+        """Split a string to a list.
+
+        Separation is done by whitespace.
+
+        Keyword arguments:
+        str -- the string to be splitted
+        subst -- if True then templates like $(VARIABLE) are substituted.
+
+        """
+        if subst:
+            list = self.project.vars.split_subst(str)
+        else:
+            list = str.split()
+        return list
+
+
+    def glob(self, path):
+        """Do pattern matching.
+
+        Example: cfiles = buildenv.glob("*.c")
+
+        Keyword arguments:
+        path -- the path to
+
+        """
+        return glob.glob(path)
+
+    ###############################################################################################
+
+    # metatarget handling
+
+    def add_virtual_metatarget(self, targetname, dependencies=[]):
+        """Create a virtual metatarget.
+
+        If the metatarget already exists the dependencies will be added.
+
+        Keyword arguments:
+        targetname -- metatarget
+        dependencies -- list of names of dependencies
+
+        """
+        self.project.targets.add_target(targetname, None, False, dependencies)
+
+    ###############################################################################################
+
+    # builders
 
     def output(self, mmake, text):
-        mmake = self.substitute(mmake)                  # substitute variables like $(TOP)
-        function = mmfunction.Output(self, text)        # create a function object
-        self.add_function(mmake, function)              # add it to the function list of the metatarget
+        """Builder which outputs some text.
+
+        This builder exists mainly for testing purposes and as an example
+        how a builder must be written.
+
+        Keyword arguments:
+        text -- the string which should be printed
+
+        """
+        # substitute variables like $(TOP)
+        mmake = self.substitute(mmake)
+        # create a function object
+        function = mmfunction.Output(self, text)
+        # add it to the function list of the metatarget
+        self.add_function(mmake, function)
 
 
     def mkdirs(self, mmake, dirs):
-        mmake = self.substitute(mmake)                  # substitute variables like $(TOP)
-        function = mmfunction.MkDirs(self, dirs)        # create a function object
-        self.add_function(mmake, function)              # add it to the function list of the metatarget
+        """Builder which creates directories.
+
+        Keyword arguments:
+        dirs -- list of directory names
+
+        """
+        mmake = self.substitute(mmake)
+        function = mmfunction.MkDirs(self, dirs)
+        self.add_function(mmake, function)
+
+
+    def copy_files(self, mmake, files, src, dst):
+        """Builder which copies files.
+
+        Keyword arguments:
+        files -- list of file names
+        src -- source directory
+        dst -- destination directory
+
+        """
+        mmake = self.substitute(mmake)
+        function = mmfunction.CopyFiles(self, files, src, dst)
+        self.add_function(mmake, function)
--- tools/metamakeng/mmfunction.py
+++ tools/metamakeng/mmfunction.py
@@ -1,22 +1,29 @@
 # -*- coding: iso-8859-15 -*-
 
-import errno, logging, os, sys
+import errno, logging, os, shutil, sys
 
 class Function:
     def __init__(self, buildenv):
         self.buildenv = buildenv
+        # set the directory so that the builder can be called
+        # in the right directory when executed.
         self.directory = buildenv.get_variable("CURDIR")
 
 
 class Output(Function):
     def __init__(self, buildenv, text):
         Function.__init__(self, buildenv)
+        # remember the argument(s)
         self.text = text
 
 
     def execute(self):
+        # do substitution on the argument. We must do it during execution
+        # because some variables like CURDIR are constantly changed.
         text = self.buildenv.substitute(self.text)
+        # do the action
         print text
+        # return True if it worked well
         return True
 
 
@@ -37,4 +44,33 @@ class MkDirs(Function):
                     pass # path exists and is directory
                 else:
                     raise
+        return True
+
+
+class CopyFiles(Function):
+    def __init__(self, buildenv, files, src, dst):
+        Function.__init__(self, buildenv)
+        self.files = files
+        self.src = src
+        self.dst = dst
+
+
+    def execute(self):
+        dstdir = self.buildenv.substitute(self.dst)
+        srcdir = self.buildenv.substitute(self.src)
+        try:
+            os.makedirs(dstdir)
+        except OSError as exc:
+            if exc.errno == errno.EEXIST and os.path.isdir(dstdir):
+                pass # path exists and is directory
+            else:
+                raise
+
+        for file in self.files:
+            file = self.buildenv.substitute(file)
+            srcfile = os.path.join(srcdir, file)
+            dstfile = os.path.join(dstdir, file)
+            if not os.path.exists(dstfile) or (os.path.getmtime(dstfile) < os.path.getmtime(srcfile)):
+                logging.info("[MMAKE] copying file '%s' to '%s'" % (srcfile, dstfile))
+                shutil.copyfile(srcfile, dstfile)
         return True
