4c4283471da798bc1f440e7a5c23cb1a1b319658
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-12-04 05:08:49 +0000

    battclock.resource(all-pc): Use acpica.library instead of acpi.resource
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@48500 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-pc/battclock/battclock_init.c
+++ arch/all-pc/battclock/battclock_init.c
@@ -1,7 +1,6 @@
 #include <aros/debug.h>
 #include <aros/symbolsets.h>
-#include <resources/acpi.h>
-#include <proto/acpi.h>
+#include <proto/acpica.h>
 #include <proto/exec.h>
 
 #include "battclock_intern.h"
@@ -10,22 +9,23 @@
 /* auto init */
 static int BattClock_Init(struct BattClockBase *BattClockBase)
 {
-    APTR ACPIBase;
-
     InitSemaphore(&BattClockBase->sem);
     BattClockBase->century = CENTURY;	/* Default offset */
 
-    ACPIBase = OpenResource("acpi.resource");
-    if (ACPIBase)
+    if (ACPICABase)
     {
-    	struct ACPI_TABLE_TYPE_FADT *fadt = ACPI_FindSDT(ACPI_MAKE_ID('F','A','C','P'));
+        ACPI_TABLE_FADT *fadt;
+        ACPI_STATUS err;
 
-    	if (fadt && (fadt->header.length >= offsetof(struct ACPI_TABLE_TYPE_FADT, century)) &&
-    	    fadt->century)
-    	{
-    	    D(bug("[BattClock] Got RTC century offset 0x%02X from ACPI\n", fadt->century));
-    	    BattClockBase->century = fadt->century;
-    	}
+        err = AcpiGetTable("FACP", 1, (ACPI_TABLE_HEADER **)&fadt);
+        if (err == AE_OK) {
+            if ((fadt->Header.Length >= offsetof(ACPI_TABLE_FADT, Century)) &&
+                fadt->Century)
+            {
+                D(bug("[BattClock] Got RTC century offset 0x%02X from ACPI\n", fadt->Century));
+                BattClockBase->century = fadt->Century;
+            }
+        }
     }
 
     return 1;
