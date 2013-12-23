449369dbd55b630a8a71911e2dd0cbbfc06cad27
deadwood-pl <deadwood@wp.pl>
2013-12-23 15:37:42 +0000

    debug.library: introduce a two-level index of segments
    The current solution with flat list of loaded segments work fairly
    well with small total number of segments. However is a larger C++
    application is loaded the system collapses and requires minutes to
    load debugging information.
    
    This is a first patch that make changes to the system. First it
    introduces a two-level index with first index being modules and second
    segments within module. This allows to quickly cuts off whole ranges
    of segments from being searched. The segment arrays within modules
    are also sorted, so a binary search algorithm is used to locate
    the required segment quicker. Lastly, if system is compiled with
    DEBUG_MODULES, the module_t structure has a string generated which
    is a parameter to the add-symbol-file command. Having this string generated
    in .gdbinit script was taking 2-3 minutes in my test case, now it is
    almost instant.
    
    These changes make debuging C++ binaries using gdb feasible in sense
    that 90% of time is not spend waiting for symbols to be loaded.
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@48612 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/debug/debug_intern.h
+++ rom/debug/debug_intern.h
@@ -5,46 +5,57 @@
     Desc:
 */
 
+#include <aros/config.h>
 #include <dos/bptr.h>
 #include <exec/libraries.h>
 #include <exec/semaphores.h>
 
 typedef struct
 {
-    char *s_name;        /* Symbol name                        */
-    void *s_lowest;        /* Start address                     */
-    void *s_highest;        /* End address                             */
+    char *s_name;               /* Symbol name */
+    void *s_lowest;             /* Start address */
+    void *s_highest;            /* End address */
 } dbg_sym_t;
+
+struct segment;
 
 typedef struct
 {
-    BPTR          m_seg;     /* First segment pointer */
-    char          *m_shstr;   /* Section headers table                */
-    char          *m_str;     /* Symbol names table                */
-    unsigned int   m_segcnt;  /* Count of segments                */
-    dbg_sym_t     *m_symbols; /* Array of associated symbols    */
-    unsigned long  m_symcnt;  /* Number of symbols in the array */
-    char           m_name[1]; /* Module name, variable length        */
+    struct MinNode  m_node;         /* For linking into a list */
+    BPTR            m_seg;          /* First segment pointer */
+    char            *m_shstr;       /* Section headers table */
+    char            *m_str;         /* Symbol names table */
+    struct segment  **m_segments;   /* Sorted array of segments */
+    unsigned int    m_segcnt;       /* Count of segments */
+#if AROS_MODULES_DEBUG
+    char            *m_seggdbhlp;   /* Pre-built string for add-symbol-file */
+#endif
+    dbg_sym_t       *m_symbols;     /* Array of associated symbols */
+    unsigned long   m_symcnt;       /* Number of symbols in the array */
+    void *          m_lowest;       /* Lowest address of all segments */
+    void *          m_highest;      /* Highest address of all segments */
+    char            m_name[1];      /* Module name, variable length */
 } module_t;
 
 struct segment
 {
-    struct MinNode s_node;        /* For linking into the list          */
-    BPTR           s_seg;        /* DOS segment pointer                 */
-    void *           s_lowest;        /* Start address                      */
-    void *           s_highest;        /* End address                              */
-    module_t *           s_mod;        /* Module descriptor                      */
-    char *           s_name;        /* Segment name                         */
-    unsigned int   s_num;        /* Segment number                      */
+    struct MinNode  s_node;     /* For linking into the list */
+    BPTR            s_seg;      /* DOS segment pointer */
+    void *          s_lowest;   /* Start address */
+    void *          s_highest;  /* End address */
+    module_t *      s_mod;      /* Module descriptor */
+    char *          s_name;     /* Segment name */
+    unsigned int    s_num;      /* Segment number */
 };
 
 struct DebugBase
 {
-    struct Library         db_Lib;
-    struct MinList         db_Modules;
-    struct ELF_ModuleInfo *db_KernelModules;
-    struct SignalSemaphore db_ModSem;
-    APTR                   db_KernelBase;
+    struct Library          db_Lib;
+    struct MinList          db_Modules;
+    struct MinList          db_LoadedModules;
+    struct ELF_ModuleInfo   *db_KernelModules;
+    struct SignalSemaphore  db_ModSem;
+    APTR                    db_KernelBase;
 };
 
 #define DBGBASE(x) ((struct DebugBase *)x)
