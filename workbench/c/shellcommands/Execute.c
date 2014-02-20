f0256b02cb4b510c26faf2d591eef55d4abf7e10
o1i <o1i@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2014-02-20 08:24:16 +0000

    Retab
    git-svn-id: https://svn.aros.org/svn/aros/trunk@48820 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/shellcommands/Execute.c
+++ workbench/c/shellcommands/Execute.c
@@ -71,10 +71,14 @@ AROS_SHA(STRPTR, ,ARGUMENTS, /F, NULL))
     STRPTR extraargs = NULL;
     BPTR from;
     LONG len;
+    BPTR ces;
     struct Process *me = (struct Process *)FindTask(NULL);
 
     if (!cli)
         return RETURN_ERROR;
+
+    if(! (ces=me->pr_CES))
+        ces=me->pr_COS;
 
     if (! arguments)
         arguments = "";
@@ -106,12 +110,12 @@ AROS_SHA(STRPTR, ,ARGUMENTS, /F, NULL))
 
     if (!from)
     {
-	IPTR data[] = { (IPTR)SHArg(NAME) };
-	VFPrintf(me->pr_CES, "EXECUTE: can't open %s\n", data);
-	PrintFault(IoErr(), NULL);
-	if (extraargs)
-	    FreeVec(extraargs);
-	return RETURN_FAIL;
+        IPTR data[] = { (IPTR)SHArg(NAME) };
+        VFPrintf(me->pr_CES, "EXECUTE: can't open %s\n", data);
+        PrintFault(IoErr(), NULL);
+        if (extraargs)
+            FreeVec(extraargs);
+        return RETURN_FAIL;
     }
 
     if (cli->cli_StandardInput == cli->cli_CurrentInput)
@@ -120,108 +124,108 @@ AROS_SHA(STRPTR, ,ARGUMENTS, /F, NULL))
     }
     else
     {
-	struct DateStamp ds;
-	BYTE tmpname[256];
-	BPTR tmpfile = BNULL;
-	ULONG count = 0;
-	BYTE tmpdir[4];
-	BPTR tmplock;
-	struct Window *win;
-	struct Process *proc = (struct Process*)FindTask(0);
+        struct DateStamp ds;
+        BYTE tmpname[256];
+        BPTR tmpfile = BNULL;
+        ULONG count = 0;
+        BYTE tmpdir[4];
+        BPTR tmplock;
+        struct Window *win;
+        struct Process *proc = (struct Process*)FindTask(0);
 
-	DateStamp(&ds);
-	
-	win = proc->pr_WindowPtr;
-	proc->pr_WindowPtr = (struct Window *)-1;
-	tmplock = Lock("T:", SHARED_LOCK);
-	proc->pr_WindowPtr = win;
-	if (tmplock) {
-	    strcpy(tmpdir, "T:");
-	    UnLock(tmplock);
-	} else {
-	    strcpy(tmpdir, ":T/");
-	}
+        DateStamp(&ds);
+        
+        win = proc->pr_WindowPtr;
+        proc->pr_WindowPtr = (struct Window *)-1;
+        tmplock = Lock("T:", SHARED_LOCK);
+        proc->pr_WindowPtr = win;
+        if (tmplock) {
+            strcpy(tmpdir, "T:");
+            UnLock(tmplock);
+        } else {
+            strcpy(tmpdir, ":T/");
+        }
 
         do {
             count++;
             __sprintf(tmpname, "%sTmp%lu%lu%lu%lu%lu", tmpdir,
                       ((struct Process *)FindTask(NULL))->pr_TaskNum,
                       ds.ds_Days, ds.ds_Minute, ds.ds_Tick, count);
-	    tmpfile = Open(tmpname, MODE_NEWFILE);
+            tmpfile = Open(tmpname, MODE_NEWFILE);
         } while (tmpfile == BNULL && IoErr() == ERROR_OBJECT_IN_USE);
 
-	if (tmpfile)
-	{
-	    LONG c;
+        if (tmpfile)
+        {
+            LONG c;
 
-	    //if (FPuts(tmpfile, ".pushis\n") != -1)
-		while((c = FGetC(from)) != -1 && FPutC(tmpfile, c) != -1);
+            //if (FPuts(tmpfile, ".pushis\n") != -1)
+                while((c = FGetC(from)) != -1 && FPutC(tmpfile, c) != -1);
 
-	    c = IoErr();
-	    Close(from);
+            c = IoErr();
+            Close(from);
 
-	    if (c)
-	    {
-		FPuts(me->pr_CES,
-		      "EXECUTE: error while creating temporary file\n");
-		PrintFault(c, NULL);
-		Close(tmpfile);
-		DeleteFile(tmpname);
-		if (extraargs)
-		    FreeVec(extraargs);
+            if (c)
+            {
+                FPuts(me->pr_CES,
+                      "EXECUTE: error while creating temporary file\n");
+                PrintFault(c, NULL);
+                Close(tmpfile);
+                DeleteFile(tmpname);
+                if (extraargs)
+                    FreeVec(extraargs);
 
-		return RETURN_FAIL;
-	    }
+                return RETURN_FAIL;
+            }
 
-	    c = '\n';
-	    FPutC(tmpfile, c);
+            c = '\n';
+            FPutC(tmpfile, c);
 
-	    //FPuts(tmpfile, ".popis\n");
+            //FPuts(tmpfile, ".popis\n");
 
-	    while((c = FGetC(cli->cli_CurrentInput)) != -1 && FPutC(tmpfile, c) != -1);
+            while((c = FGetC(cli->cli_CurrentInput)) != -1 && FPutC(tmpfile, c) != -1);
 
-	    c = IoErr();
+            c = IoErr();
 
-	    if (c)
-	    {
-		FPuts(me->pr_CES, "EXECUTE: error while creating temporary file\n");
-		PrintFault(c, NULL);
-		Close(tmpfile);
-		DeleteFile(tmpname);
-		if (extraargs)
-		    FreeVec(extraargs);
+            if (c)
+            {
+                FPuts(me->pr_CES, "EXECUTE: error while creating temporary file\n");
+                PrintFault(c, NULL);
+                Close(tmpfile);
+                DeleteFile(tmpname);
+                if (extraargs)
+                    FreeVec(extraargs);
 
-		return RETURN_FAIL;
-	    }
+                return RETURN_FAIL;
+            }
 
-	    Close(cli->cli_CurrentInput);
-	    if (AROS_BSTR_strlen(cli->cli_CommandFile))
-	        DeleteFile(AROS_BSTR_ADDR(cli->cli_CommandFile));
+            Close(cli->cli_CurrentInput);
+            if (AROS_BSTR_strlen(cli->cli_CommandFile))
+                DeleteFile(AROS_BSTR_ADDR(cli->cli_CommandFile));
 
-	    {
-	        LONG len = strlen(tmpname);
-	        CopyMem(tmpname, AROS_BSTR_ADDR(cli->cli_CommandFile), len);
-	        AROS_BSTR_setstrlen(cli->cli_CommandFile, len);
-	    }
+            {
+                LONG len = strlen(tmpname);
+                CopyMem(tmpname, AROS_BSTR_ADDR(cli->cli_CommandFile), len);
+                AROS_BSTR_setstrlen(cli->cli_CommandFile, len);
+            }
 
-	    cli->cli_CurrentInput = tmpfile;
-	    Seek(tmpfile, 0, OFFSET_BEGINNING);
-	}
-	else
-	{
-	    /*
-	      we should try to open ":T", but since ":"
-	      is not handled correctly yet, we just give up
-	    */
-	    LONG c = IoErr();
-	    FPuts(me->pr_CES, "EXECUTE: error while creating temporary file\n");
-	    PrintFault(c, NULL);
-	    Close(from);
-	    if (extraargs)
-	        FreeVec(extraargs);
+            cli->cli_CurrentInput = tmpfile;
+            Seek(tmpfile, 0, OFFSET_BEGINNING);
+        }
+        else
+        {
+            /*
+              we should try to open ":T", but since ":"
+              is not handled correctly yet, we just give up
+            */
+            LONG c = IoErr();
+            FPuts(me->pr_CES, "EXECUTE: error while creating temporary file\n");
+            PrintFault(c, NULL);
+            Close(from);
+            if (extraargs)
+                FreeVec(extraargs);
 
-	    return RETURN_FAIL;
-	}
+            return RETURN_FAIL;
+        }
     }
 
     /* Update cli_CommandName to be the name of the script */
