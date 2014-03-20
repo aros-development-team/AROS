56896fc5b2dfad3d43c4d914b5a74e515a562d89
DizzyOfCRN <DizzyOfCRN@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2014-03-20 17:33:18 +0000

    - Stack based Kernel tag passing, .bss used only by elfloader
    git-svn-id: https://svn.aros.org/svn/aros/trunk@48871 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-sun4i/boot/support.c
+++ arch/arm-sun4i/boot/support.c
@@ -10,10 +10,6 @@
 
 #include <sys/types.h>
 
-static unsigned char __tmpspace[BOOT_TMP_SIZE];
-static unsigned char *first_free = &__tmpspace[0];
-static unsigned long free_memory = BOOT_TMP_SIZE;
-
 void arm_flush_cache(uint32_t addr, uint32_t length) {
         while (length) {
                 __asm__ __volatile__("mcr p15, 0, %0, c7, c14, 1"::"r"(addr));
@@ -39,29 +35,6 @@ void arm_dcache_invalidate(uint32_t addr, uint32_t length) {
                 length -= 32;
         }
         __asm__ __volatile__("mcr p15, 0, %0, c7, c10, 4"::"r"(addr));
-}
-
-void *malloc(size_t size) {
-	void *ret = NULL;
-
-	size = (size + 15) & ~15;
-
-	if (size <= free_memory) {
-		ret = first_free;
-
-		first_free += size;
-		free_memory -= size;
-	}
-
-	return ret;
-}
-
-size_t mem_avail() {
-	return free_memory;
-}
-
-size_t mem_used() {
-	return BOOT_TMP_SIZE - free_memory;
 }
 
 int32_t strlen(const char *c) {
