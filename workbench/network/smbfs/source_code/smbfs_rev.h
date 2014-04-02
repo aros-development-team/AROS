061ff18e43844f516f3a2e10ea7ff2839384bbae
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2014-04-02 00:03:13 +0000

    Corrected handler name (even if this file isn't used on AROS).
    git-svn-id: https://svn.aros.org/svn/aros/trunk@48898 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/network/smbfs/source_code/smbfs_rev.h
+++ workbench/network/smbfs/source_code/smbfs_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		2
 #define REVISION	2
 #define DATE		"1.4.2014"
-#define VERS		"smb.handler 2.2"
-#define VSTRING		"smb.handler 2.2 (1.4.2014)\r\n"
+#define VERS		"smb-handler 2.2"
+#define VSTRING		"smb-handler 2.2 (1.4.2014)\r\n"
 #define VERSTAG		"\0$VER: smb-handler 2.2 (1.4.2014)"
