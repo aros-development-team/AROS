9be7f230819e0a9a43a767411af6cc4d4db995bc
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2014-04-25 15:45:34 +0000

    Print out that we are expecting a failed assertion, so that it doesn't look like a failed test.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@48972 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/clib/assert.c
+++ test/clib/assert.c
@@ -1,7 +1,14 @@
+/*
+    Copyright © 2013-2014, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
 #include <assert.h>
+#include <stdio.h>
 
 int main(void)
 {
+    printf("Expecting a failed assertion...\n");
     assert(0);
     
     return 20;
