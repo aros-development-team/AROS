db9eeea9361d5b4dc218da13639b8177b5f3e11c
deadwood-pl <deadwood@wp.pl>
2014-04-27 15:28:33 +0000

    dos.library: allow FRead to read directly from the buffer without need for going through FGetC
    Previous implementation caused a function call for each byte read. 1MB of
    data = 1 million calls.
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@48974 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/fgetc.c
+++ rom/dos/fgetc.c
@@ -57,94 +57,15 @@
 {
     AROS_LIBFUNC_INIT
 
-    /* Get pointer to filehandle */
-    struct FileHandle *fh = (struct FileHandle *)BADDR(file);
+    UBYTE c;
+    LONG res;
 
-    LONG  size;
-    LONG  bufsize;
+    res = vbuf_fetch(file, &c, 1, DOSBase);
 
-    if (fh == NULL)
-    {
+    if (res < 0)
         return EOF;
-    }
-    
-    /* If the file is in write mode... */
-    if(fh->fh_Flags & FHF_WRITE)
-    {
-        /* write the buffer (in many pieces if the first one isn't enough). */
-        LONG pos = 0;
-
-        while(pos != fh->fh_Pos)
-        {
-            size = Write(file, BADDR(fh->fh_Buf) + pos, fh->fh_Pos - pos);
-
-            /* An error happened? Return it. */
-            if(size < 0)
-            {
-                return EOF;
-            }
-
-            pos += size;
-        }
-
-        /* Reinit filehandle. */
-        fh->fh_Flags &= ~FHF_WRITE;
-        fh->fh_Pos = fh->fh_End = 0;
-    }
-
-    /* No normal characters left. */
-    if(fh->fh_Pos >= fh->fh_End)
-    {
-        /* Check for a pushed back EOF. */
-        if(fh->fh_Pos > fh->fh_End)
-        {
-            D(bug("FGetC: Weird pos: fh_Pos (%d) > fh_End (%d)\n", fh->fh_Pos, fh->fh_End));
-            /* Return EOF. */
-            return EOF;
-        }
-
-        /* Is there a buffer? */
-        if(fh->fh_Buf == BNULL)
-        {
-            if (NULL == vbuf_alloc(fh, NULL, IOBUFSIZE))
-            {
-                D(bug("FGetC: Can't allocate buffer\n"));
-                return(EOF);
-            }
-        }
-
-        /* Fill the buffer. */
-        if (fh->fh_Buf != fh->fh_OrigBuf) {
-            D(bug("FGetC: Can't trust fh_BufSize. Using 208 as the buffer size.\n"));
-            bufsize = 208;
-        } else {
-            bufsize = fh->fh_BufSize;
-        }
-        size = Read(file, BADDR(fh->fh_Buf), bufsize);
-
-        /* Prepare filehandle for data. */
-        if(size <= 0)
-            size = 0;
-
-        fh->fh_Pos = 0;
-        fh->fh_End = size;
-
-        /* No data read? Return EOF. */
-        if(size == 0)
-        {
-            D(bug("FGetC: Tried to Read() to a %d byte buffer, got 0)\n", bufsize));
-            return EOF;
-        }
-    }
-
-    /* If fh_End == 0, simulate an EOF */
-    if (fh->fh_End == 0) {
-        D(bug("FGetC: Got an EOF via fh_End == 0\n"));
-        return EOF;
-    }
-
-    /* All OK. Get data. */
-    return ((UBYTE *)BADDR(fh->fh_Buf))[fh->fh_Pos++];
+    else
+        return c;
 
     AROS_LIBFUNC_EXIT
 } /* FGetC */
