47746707486bda734dc0d37c4568f9eb93351420
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2014-05-18 03:39:51 +0000

    Added missing properties.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@49007 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/timer/include/timer.h
+++ rom/timer/include/timer.h
@@ -3,7 +3,7 @@
 
 /*
     Copyright © 1995-2001, The AROS Development Team. All rights reserved.
-    $Id: timer.h 30792 2009-03-07 22:40:04Z neil $
+    $Id$
 
     Desc: Definitions for the Timer HIDD system.
     Lang: english
