8543bfcd4e1e91f3ad62cbda5cad4832871a24de
mattrust <mrustler@gmx.de>
2014-07-20 17:43:29 +0000

    Make it buildable with GCC 4.8 on OpenSUSE.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@49122 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-pc/boot/grub2-aros/grub-core/gnulib/stdio.in.h
+++ arch/all-pc/boot/grub2-aros/grub-core/gnulib/stdio.in.h
@@ -140,8 +140,10 @@ _GL_WARN_ON_USE (fflush, "fflush is not always POSIX compliant - "
 /* It is very rare that the developer ever has full control of stdin,
    so any use of gets warrants an unconditional warning.  Assume it is
    always declared, since it is required by C89.  */
+#if defined gets
 #undef gets
 _GL_WARN_ON_USE (gets, "gets is a security hole - use fgets instead");
+#endif
 
 #if @GNULIB_FOPEN@
 # if @REPLACE_FOPEN@
