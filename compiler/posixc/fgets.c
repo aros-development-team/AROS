580e86cbad7bb94613a0e7774f2acc6608d0ad33
deadwood-pl <deadwood@wp.pl>
2014-07-24 18:56:31 +0000

    posixc.library: fgets.c tabs->spaces
    git-svn-id: https://svn.aros.org/svn/aros/trunk@49153 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/posixc/fgets.c
+++ compiler/posixc/fgets.c
@@ -18,44 +18,44 @@
     NAME */
 #include <stdio.h>
 
-	char * fgets (
+    char * fgets (
 
 /*  SYNOPSIS */
-	char * buffer,
-	int    size,
-	FILE * stream)
+    char * buffer,
+    int    size,
+    FILE * stream)
 
 /*  FUNCTION
-	Read one line of characters from the stream into the buffer.
-	Reading will stop, when a newline ('\n') is encountered, EOF
-	or when the buffer is full. If a newline is read, then it is
-	put into the buffer. The last character in the buffer is always
-	'\0' (Therefore at most size-1 characters can be read in one go).
+        Read one line of characters from the stream into the buffer.
+        Reading will stop, when a newline ('\n') is encountered, EOF
+        or when the buffer is full. If a newline is read, then it is
+        put into the buffer. The last character in the buffer is always
+        '\0' (Therefore at most size-1 characters can be read in one go).
 
     INPUTS
-	buffer - Write characters into this buffer
-	size - This is the size of the buffer in characters.
-	stream - Read from this stream
+        buffer - Write characters into this buffer
+        size - This is the size of the buffer in characters.
+        stream - Read from this stream
 
     RESULT
-	buffer or NULL in case of an error or EOF.
+        buffer or NULL in case of an error or EOF.
 
     NOTES
 
     EXAMPLE
-	// Read a file line by line
-	char line[256];
+        // Read a file line by line
+        char line[256];
 
-	// Read until EOF
-	while (fgets (line, sizeof (line), fh))
-	{
-	    // Evaluate the line
-	}
+        // Read until EOF
+        while (fgets (line, sizeof (line), fh))
+        {
+            // Evaluate the line
+        }
 
     BUGS
 
     SEE ALSO
-	fopen(), gets(), fputs()
+        fopen(), gets(), fputs()
 
     INTERNALS
 
@@ -65,25 +65,25 @@
 
     if (!fdesc)
     {
-    	errno = EBADF;
-	stream->flags |= __POSIXC_STDIO_ERROR;
+        errno = EBADF;
+        stream->flags |= __POSIXC_STDIO_ERROR;
 
-	return NULL;
+        return NULL;
     }
 
     buffer = FGets (fdesc->fcb->handle, buffer, size);
 
     if (!buffer)
     {
-	if (IoErr ())
-	{
-	    errno = __stdc_ioerr2errno(IoErr());
-	    stream->flags |= __POSIXC_STDIO_ERROR;
+        if (IoErr ())
+        {
+            errno = __stdc_ioerr2errno(IoErr());
+            stream->flags |= __POSIXC_STDIO_ERROR;
+            }
+        else
+        {
+            stream->flags |= __POSIXC_STDIO_EOF;
         }
-	else
-	{
-	    stream->flags |= __POSIXC_STDIO_EOF;
-	}
     }
 
     return buffer;
