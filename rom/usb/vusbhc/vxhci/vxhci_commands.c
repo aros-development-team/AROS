49892d0ca12c62597c850d2ca646184fda7c4b7c
DizzyOfCRN <DizzyOfCRN@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2014-08-03 09:36:55 +0000

    - Brake down multilevel switch statements into smaller and manageable pieces, first check the request direction.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@49226 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/usb/vusbhc/vxhci/vxhci_commands.c
+++ rom/usb/vusbhc/vxhci/vxhci_commands.c
@@ -6,15 +6,38 @@
     Lang: english
 */
 
+#include <aros/macros.h>
+
 #include <proto/exec.h>
 #include <proto/arossupport.h>
 
+#include <devices/usb_hub.h>
 #include <devices/usbhardware.h>
 
 #define DEBUG 1
 #include <aros/debug.h>
 
 #include LC_LIBDEFS_FILE
+
+/* we cannot use AROS_WORD2LE in struct initializer */
+#if AROS_BIG_ENDIAN
+#define WORD2LE(w) (UWORD)(((w) >> 8) & 0x00FF) | (((w) << 8) & 0xFF00)
+#else
+#define WORD2LE(w) (w)
+#endif
+
+const struct UsbStdDevDesc RHDevDesc = { sizeof(struct UsbStdDevDesc), UDT_DEVICE, WORD2LE(0x0110), HUB_CLASSCODE, 0, 0, 8, WORD2LE(0x0000), WORD2LE(0x0000), WORD2LE(0x0100), 1, 2, 0, 1 };
+
+const struct UsbSSHubDesc  RHSSHubDesc = { 12,                                           // 0 Number of bytes in this descriptor, including this byte. (12 bytes)
+                                           UDT_SSHUB,                                    // 1 Descriptor Type, value: 2AH for SuperSpeed hub descriptor
+                                           0,                                            // 2 Number of downstream facing ports that this hub supports. The maximum number of ports a hub can support is 15
+                                           WORD2LE(UHCF_INDIVID_POWER|UHCF_INDIVID_OVP), // 3 wHubCharacteristics
+                                           0,                                            // 5 bPwrOn2PwrGood
+                                           10,                                           // 6 bHubContrCurrent
+                                           0,                                            // 7 bHubHdrDecLat
+                                           0,                                            // 8 wHubDelay
+                                           0                                             // 10 DeviceRemovable
+                                         };
 
 WORD cmdQueryDevice(struct IOUsbHWReq *ioreq) {
     struct TagItem *taglist = (struct TagItem *) ioreq->iouh_Data;
@@ -37,6 +60,10 @@ WORD cmdQueryDevice(struct IOUsbHWReq *ioreq) {
                 break;
             case UHA_Copyright:
                 *((STRPTR *) tag->ti_Data) ="©2014 The AROS Development Team";
+                count++;
+                break;
+            case UHA_ProductName:
+                *((STRPTR *) tag->ti_Data) = "VXHCI";
                 count++;
                 break;
             case UHA_Capabilities:
@@ -121,17 +148,50 @@ WORD cmdControlXFerRootHub(struct IOUsbHWReq *ioreq) {
 
     /* Endpoint 0 is used for control transfers only and can not be assigned to any other function. */
     if(ioreq->iouh_Endpoint != 0) {
-        return(UHIOERR_STALL);
-    }
-
-    if(len != ioreq->iouh_Length) {
-        return(UHIOERR_STALL);
+        return UHIOERR_BADPARAMS;
     }
 
     /* Check the request type */
-    switch(rt) {
+    if(rt&&URTF_IN) {
+        bug("[VXHCI] cmdControlXFerRootHub: Request direction is device to host\n");
+    } else {
+        bug("[VXHCI] cmdControlXFerRootHub: Request direction is host to device\n");
     }
 
+/*
+    switch(rt) {
+        case (URTF_STANDARD|URTF_DEVICE):
+            bug("[VXHCI] cmdControlXFerRootHub: switch(rt) (URTF_STANDARD|URTF_DEVICE)\n");
+            switch(req) {
+                case USR_SET_ADDRESS:
+                    bug("[VXHCI] cmdControlXFerRootHub: switch(req) (USR_SET_ADDRESS) SetAddress = %ld\n", val);
+                    //unit->hu_RootHubAddr = val;
+                    ioreq->iouh_Actual = len;
+                    return(0);
+                case USR_SET_CONFIGURATION:
+                    bug("[VXHCI] cmdControlXFerRootHub: switch(req) (USR_SET_CONFIGURATION) SetConfiguration = %ld\n", val);
+                    ioreq->iouh_Actual = len;
+                    return(0);
+            }
+            break;
+        case (URTF_IN|URTF_STANDARD|URTF_DEVICE):
+            bug("[VXHCI] cmdControlXFerRootHub: switch(rt) (URTF_IN|URTF_STANDARD|URTF_DEVICE)\n");
+            break;
+        case (URTF_CLASS|URTF_OTHER):
+            bug("[VXHCI] cmdControlXFerRootHub: switch(rt) (URTF_CLASS|URTF_OTHER)\n");
+            break;
+        case (URTF_IN|URTF_CLASS|URTF_OTHER):
+            bug("[VXHCI] cmdControlXFerRootHub: switch(rt) (URTF_IN|URTF_CLASS|URTF_OTHER)\n");
+            break;
+        case (URTF_IN|URTF_CLASS|URTF_DEVICE):
+            bug("[VXHCI] cmdControlXFerRootHub: switch(rt) (URTF_IN|URTF_CLASS|URTF_DEVICE)\n");
+            break;
+        default:
+            bug("[VXHCI] cmdControlXFerRootHub: switch(rt) DEFAULT\n");
+            break;
+    } /* switch(rt) */
+
+    return UHIOERR_BADPARAMS;
 }
 
 
