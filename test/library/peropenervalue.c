307f7b832f2db852e4146fc8a01a4d5b71991d21
deadwood-pl <deadwood@wp.pl>
2014-08-24 12:41:40 +0000

    tests/library: extend the peropenertest with reg call functions
    git-svn-id: https://svn.aros.org/svn/aros/trunk@49526 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/library/peropenervalue.c
+++ test/library/peropenervalue.c
@@ -1,7 +1,7 @@
 #include <proto/peropener.h>
 #include "peropenerbase.h"
 
-void PeropenerSetValue(int value)
+void PeropenerSetValueStack(int value)
 {
     struct PeropenerBase *PeropenerBase = (struct PeropenerBase *)__aros_getbase_PeropenerBase();
 
@@ -9,9 +9,35 @@ void PeropenerSetValue(int value)
 }
 
 
-int PeropenerGetValue(void)
+int PeropenerGetValueStack(void)
 {
     struct PeropenerBase *PeropenerBase = (struct PeropenerBase *)__aros_getbase_PeropenerBase();
 
     return PeropenerBase->value;
 }
+
+AROS_LH1(void, PeropenerSetValueReg,
+AROS_LHA(int, value, D0),
+struct Library *, PeropenerBase, 7, Peropener)
+{
+    AROS_LIBFUNC_INIT
+
+    struct PeropenerBase *PeropenerBase = (struct PeropenerBase *)__aros_getbase_PeropenerBase();
+
+    PeropenerBase->value = value;
+
+    AROS_LIBFUNC_EXIT
+}
+
+AROS_LH0(int, PeropenerGetValueReg,
+struct Library *, PeropenerBase, 8, Peropener)
+{
+    AROS_LIBFUNC_INIT
+
+    struct PeropenerBase *PeropenerBase = (struct PeropenerBase *)__aros_getbase_PeropenerBase();
+
+    return PeropenerBase->value;
+
+    AROS_LIBFUNC_EXIT
+}
+
