48d22180d8a97eb1837bc52048bbf773442eb8c2
mattrust <mrustler@gmx.de>
2014-08-25 16:35:34 +0000

    Test bed for SDI headers.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@49532 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/sdi/SDI_lib.h
@@ -0,0 +1,199 @@
+#ifndef SDI_LIB_H
+#define SDI_LIB_H
+
+/* Includeheader
+
+        Name:           SDI_lib.h
+        Versionstring:  $VER: SDI_lib.h 1.12 (01.04.2014)
+        Author:         Jens Maus
+        Distribution:   PD
+        Project page:   http://sf.net/p/adtools/code/HEAD/tree/trunk/sdi/
+        Description:    defines to hide OS specific library function definitions
+        Id:             $Id$
+        URL:            $URL: svn://svn.code.sf.net/p/adtools/code/trunk/sdi/SDI_lib.h $
+
+ 1.0   09.05.04 : initial version which allows to hide OS specific shared
+                  library function definition like it has been introduced with
+                  the new interface based library system in AmigaOS4.
+ 1.1   13.05.04 : replaced the LIBENTRY() macro with some more sophisticated
+                  LFUNC_ macros which allow to maintain varargs library functions
+                  in a much easier manner.
+ 1.2   22.05.04 : removed the anyway not required SAVEDS and ASM statements
+                  from the LIBFUNC macro for OS4. Also removed the LIBFUNC
+                  statement in the 68k LIBPROTOVA() macro so that no registers
+                  can be used in there (which should be the default).
+ 1.3   04.07.04 : added empty LIBFUNC define for MorphOS which was missing.
+ 1.4   05.10.04 : added missing LIBFUNC call to OS3/MOS interface
+ 1.5   19.05.05 : fixed some documentation glitches (Guido Mersmann)
+ 1.6   08.06.05 : swapped LIBFUNC and ret within the prototype, because
+                  c standard says attributes first and vbcc want them like
+                  this. (Guido Mersmann)
+                  changed the documentation to explain that LIBFUNC must be
+                  set first. (Guido Mersmann)
+ 1.7   11.12.05 : adapted all macros to be somewhat more compatible to also
+                  OS3 and MorphOS. Now in the real use case (codesets.library)
+                  it required a fundamental rework of the macros. (Jens Langner)
+ 1.8   28.02.06 : removed "##" in front of the OS3 __VARARGS__ usage as they
+                  causing errors on GCC >= 3.x.
+ 1.9   15.03.09 : fixed some missing function prototype in LIBPROTOVA()
+ 1.10  30.04.09 : added approriate LIBPROTOVA() definition for OS3 and MorphOS
+                  to at least make the functions known. The same pattern as for
+                  LIBSTUBVA() will be used now (Thore Böckelmann)
+ 1.11  04.05.09 : reverted the faulty LIBPROTOVA() definition to its previous
+                  version (Thore Böckelmann)
+ 1.12  01.04.14 : removed the necessity of stub functions for AmigaOS4 (Thore
+                  Böckelmann)
+*/
+
+/*
+** This is PD (Public Domain). This means you can do with it whatever you want
+** without any restrictions. I only ask you to tell me improvements, so I may
+** fix the main line of this files as well.
+**
+** To keep confusion level low: When changing this file, please note it in
+** above history list and indicate that the change was not made by myself
+** (e.g. add your name or nick name).
+**
+** Find the latest version of this file at:
+** http://sf.net/p/adtools/code/HEAD/tree/trunk/sdi/
+**
+** Jens Maus <mail@jens-maus.de>
+** Dirk Stöcker <soft@dstoecker.de>
+*/
+
+#include "SDI_compiler.h"
+
+/*
+** Library function macros to handle the definition/usage for different
+** Operating System versions.
+** Currently special library function handling for AmigaOS version 4 is
+** supported
+**
+** Example:
+**
+** Defines a library jump function "TestFunc" with a ULONG return value and
+** which is called by the corresponding library vector of a shared library.
+**
+** LIBPROTO(TestFunc, ULONG, REG(a6, UNUSED __BASE_OR_IFACE), REG(a0, char *text))
+** {
+**   Printf(text);
+**   return 0;
+** }
+**
+** Please note the use of the LIBPROTO macro which defines this function
+** automatically as a function which is directly called from within a shared
+** library. For AmigaOS4 the library interface pointer is passed as a variable
+** named "self" to the function. For all other systems the library base pointer
+** is passed.
+** Since this macro contains compiler attributes it must be called first, even
+** if some compiler allow attributes and result type mixed, other do not and we
+** want to keep the stuff compiler independent.
+**
+** If you now require to have some OS/compiler independent prototype
+** definition please use the following statement:
+**
+** LIBPROTO(TestFunc, ULONG, REG(a6, UNUSED __BASE_OR_IFACE), REG(a0, char *text));
+**
+** This will ensure that you get a proper prototype for the same function
+** where this macro will automatically take care that a LIBSTUB_* stub
+** function will also automatically defined as required if you are generating
+** a shared library for MorphOS.
+**
+** So if you then want to add this function to a library interface please
+** use the LFUNC_* macros to generate your library function vector
+** like this example one:
+**
+** #define libvector LFUNC_FAS(SomeFunc)    \
+**                   LFUNC_FA_(TestFunc)    \
+**                   LFUNC_VA_(VarargsFunc)
+**
+** This way you can then easily put the "libvector" define in your real
+** library function vector of your shared library instead of having to
+** specify each function with surrounded "#ifdef" defines. These macros
+** will then also take automatically care that the varargs functions
+** will only be specified on OS versions where these functions are now
+** real functions, like with AmigaOS4.
+**
+** Stub functions are needed for MorphOS only and usually look like this:
+**
+** LIBSTUB(TestFunc, ULONG)
+** {
+**   __BASE_OR_IFACE = (__BASE_OR_IFACE_TYPE)REG_A6;
+**   return CALL_LFUNC(TestFunc, (char *)REG_A0);
+** }
+**
+** The CALL_LFUNC macro must be used to internally call one of the public
+** library functions without going through the interface/jump table.
+** The CALL_LFUNC_NP macro does the same job for functions without further
+** parameters, except the implicit interface/base pointer.
+**
+** By using this schema a developer might ensure full source code backward
+** compatibility to AmigaOS3 without having to introduce dozens of #ifdef
+** statements in his code.
+*/
+
+#ifdef LIBFUNC
+#undef LIBFUNC
+#endif
+
+#if defined(__amigaos4__)
+  #define LIBFUNC
+  #if !defined(__cplusplus) &&                                        \
+    (__STDC_VERSION__ >= 199901L || __GNUC__ >= 3 ||                  \
+    (__GNUC__ == 2 && __GNUC_MINOR__ >= 95))
+    #define LIBPROTO(name, ret, ...)                                  \
+      LIBFUNC ret LIB_##name(__VA_ARGS__)
+    #define LIBPROTOVA(name, ret, ...)                                \
+      LIBFUNC ret VARARGS68K LIB_##name(__VA_ARGS__)
+    #define LIBSTUB(name, ret, ...)
+    #define CALL_LFUNC_NP(name, ...) LIB_##name(__BASE_OR_IFACE_VAR)
+    #define CALL_LFUNC(name, ...) LIB_##name(__BASE_OR_IFACE_VAR, __VA_ARGS__)
+  #endif
+  #define LFUNC_FAS(name) LIB_##name
+  #define LFUNC_VAS(name) LIB_##name
+  #define LFUNC_FA_(name) ,LIB_##name
+  #define LFUNC_VA_(name) ,LIB_##name
+  #define LFUNC(name)     LIB_##name
+#elif defined(__MORPHOS__)
+  #define LIBFUNC
+  #if !defined(__cplusplus) &&                                        \
+    (__STDC_VERSION__ >= 199901L || __GNUC__ >= 3 ||                  \
+    (__GNUC__ == 2 && __GNUC_MINOR__ >= 95))
+    #define LIBPROTO(name, ret, ...)                                  \
+      LIBFUNC ret LIBSTUB_##name(void);                               \
+      LIBFUNC ret LIB_##name(__VA_ARGS__)
+    #define LIBPROTOVA(name, ret, ...)
+    #define LIBSTUB(name, ret, ...)                                   \
+      LIBFUNC ret LIBSTUB_##name(void)
+    #define CALL_LFUNC_NP(name, ...) LIB_##name(__BASE_OR_IFACE_VAR)
+    #define CALL_LFUNC(name, ...) LIB_##name(__BASE_OR_IFACE_VAR, __VA_ARGS__)
+  #endif
+  #define LFUNC_FAS(name) LIBSTUB_##name
+  #define LFUNC_VAS(name)
+  #define LFUNC_FA_(name) ,LIBSTUB_##name
+  #define LFUNC_VA_(name)
+  #define LFUNC(name)     LIBSTUB_##name
+#else
+  #define LIBFUNC SAVEDS ASM
+  #if !defined(__cplusplus) &&                                        \
+    (__STDC_VERSION__ >= 199901L || __GNUC__ >= 3 ||                  \
+    (__GNUC__ == 2 && __GNUC_MINOR__ >= 95))
+    #define LIBPROTO(name, ret, ...)                                  \
+      LIBFUNC ret LIB_##name(__VA_ARGS__)
+    #define LIBPROTOVA(name, ret, ...)
+    #define LIBSTUB(name, ret, ...)
+    #define CALL_LFUNC_NP(name, ...) LIB_##name(__BASE_OR_IFACE_VAR)
+    #define CALL_LFUNC(name, ...) LIB_##name(__BASE_OR_IFACE_VAR, __VA_ARGS__)
+  #endif
+  #define LFUNC_FAS(name) LIB_##name
+  #define LFUNC_VAS(name)
+  #define LFUNC_FA_(name) ,LIB_##name
+  #define LFUNC_VA_(name)
+  #define LFUNC(name)     LIB_##name
+#endif
+
+#if !defined(LIBPROTO) || !defined(LIBPROTOVA)
+  #error "OS or compiler is not yet supported by SDI_lib.h"
+#endif
+
+#endif /* SDI_LIB_H */
---
+++ test/sdi/examples/libraries/example_lib.c
@@ -0,0 +1,604 @@
+/* Example source
+
+        Name:           example_lib.c
+		Versionstring:  $VER: example_lib.c 1.2 (21.06.2007)
+        Author:         Jens Langner
+        Distribution:   PD
+        Description:    shows how the SDI_lib.h header include can be used
+
+  1.0   05.10.04 : initial version showing how the SDI_lib.h headers can help
+                   when used for maintaining AmigaOS compatible shared library
+                   interfaces upon compatible operating systems like AmigaOS3,
+                   AmigaOS4 and MorphOS.
+
+  1.1   20.06.07 : moved LIBFUNC to be first in line acording to changes in
+				   SDI_lib.h. (Guido Mersmann)
+
+  1.2   21.06.07 : added missing libvector to MorphOS/AmigaOS3 libvectors[]
+				   (Guido Mersmann)
+
+  Please note that this example is just for educational purposes and wasn't
+  checked for complete correctness. However, it should compile and probably also
+  work as expected. But please note that its purpose is to show how a shared
+  library can held platform independent over all different available AmigaOS
+  platforms like OS3, OS4 and MorphOS.
+
+  Feel free to comment and submit any suggestions directly to
+  Jens.Langner@light-speed.de
+
+*/
+
+#include <exec/types.h>
+#include <exec/libraries.h>
+#include <exec/resident.h>
+#include <dos/dos.h>
+
+#include <proto/exec.h>
+
+#ifdef __amigaos4__
+#include <exec/emulation.h>
+#endif
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <ctype.h>
+
+#include <SDI_lib.h>
+#include <SDI_stdarg.h>
+
+#define VERSION		1
+#define REVISION	3
+#define DATE        "01.04.2014"
+#define VERS        "example.library 1.3"
+#define VSTRING		"example.library 1.3 01.04.2014)\r\n"
+#define VERSTAG		"\0$VER: example.library 1.3 01.04.2014)"
+
+static const char UserLibName[] = "example.library";
+static const char UserLibID[]   = "\0$VER: example.library 1.3 01.04.2014)";
+
+#if defined(__MORPHOS__)
+struct ExecBase *SysBase;
+#endif
+
+/******************************************************************************/
+/*             >> User defineable library functions start here <<             */
+/*  The following section should illustrate how the SDI_lib.h macros make it  */
+/*   possible to easily maintain a typical Amiga shared library interface if  */
+/*      compatibility over all common (OS3/OS4, MorphOS) is required          */
+/******************************************************************************/
+
+/******************************************************************************/
+/* Local Structures & Prototypes                                              */
+/******************************************************************************/
+
+struct LibraryHeader
+{
+  struct Library	libBase;
+  struct Library *sysBase;
+  ULONG           segList;
+};
+
+#if defined(__amigaos4__)
+#define __BASE_OR_IFACE_TYPE	struct ExampleIFace *
+#define __BASE_OR_IFACE_VAR		IExample
+#else
+#define __BASE_OR_IFACE_TYPE	struct LibraryHeader *
+#define __BASE_OR_IFACE_VAR		ExampleBase
+#endif
+#define __BASE_OR_IFACE			__BASE_OR_IFACE_TYPE __BASE_OR_IFACE_VAR
+
+// first the prototypes of all our public library functions
+LIBPROTO(SayHelloOS4, char *, REG(a6, UNUSED __BASE_OR_IFACE));
+LIBPROTO(SayHelloOS3, char *, REG(a6, UNUSED __BASE_OR_IFACE));
+LIBPROTO(SayHelloMOS, char *, REG(a6, UNUSED __BASE_OR_IFACE));
+LIBPROTO(Uppercase, char *, REG(a6, UNUSED __BASE_OR_IFACE), REG(a0, char *txt));
+LIBPROTO(SPrintfA, char *, REG(a6, UNUSED __BASE_OR_IFACE), REG(a0, char *buf), REG(a1, char *format), REG(a2, APTR args));
+LIBPROTOVA(SPrintf, char *, REG(a6, UNUSED __BASE_OR_IFACE), REG(a0, char *buf), REG(a1, char *format), ...);
+
+// let us now create the libvector.
+// Please note that the start of the vectors has to be always the "LFUNC_FAS"
+// macro
+#define libvector LFUNC_FAS(SayHelloOS4) \
+                  LFUNC_FA_(SayHelloOS3) \
+                  LFUNC_FA_(SayHelloMOS) \
+                  LFUNC_FA_(Uppercase)   \
+                  LFUNC_FA_(SPrintfA)    \
+                  LFUNC_VA_(SPrintf)
+
+// Now the real implementations of the library functions above like in
+// a normal AmigaOS shared library follow
+LIBPROTO(SayHelloOS4, char *, REG(a6, UNUSED __BASE_OR_IFACE))
+{
+  return "Hello AmigaOS4!!!";
+}
+
+LIBPROTO(SayHelloOS3, char *, REG(a6, UNUSED __BASE_OR_IFACE))
+{
+  return "Hello AmigaOS3!!!";
+}
+
+LIBPROTO(SayHelloMOS, char *, REG(a6, UNUSED __BASE_OR_IFACE))
+{
+  return "Hello MorphOS!!!";
+}
+
+LIBPROTO(Uppercase, char *, REG(a6, UNUSED __BASE_OR_IFACE), REG(a0, char *txt))
+{
+  char *p = txt;
+
+  while(p)
+  {
+    *p = toupper(*p);
+    p++;
+  }
+
+  return txt;
+}
+
+LIBPROTO(SPrintfA, char *, REG(a6, UNUSED __BASE_OR_IFACE), REG(a0, char *buf), REG(a1, char *format), REG(a2, APTR args))
+{
+  RawDoFmt(fmt, args, NULL, buf);
+
+  return(buf);
+}
+
+#if defined(__amigaos4__)
+// for AmigaOS4 varargs functions are separate entries in the interface structure and
+// hence must be defined as separate functions
+LIBPROTOVA(SPrintf, char *, REG(a6, UNUSED __BASE_OR_IFACE), REG(a0, char *buf), REG(a1, char *format), ...)
+{
+  char *ret;
+  VA_LIST args;
+
+  VA_START(args, format);
+  // the SPrintf function will be call via the interface
+  ret = SPrintfA(buf, format, VA_ARG(args, ULONG));
+  VA_END(args);
+
+  return(ret);
+}
+#elif defined(__MORPHOS__)
+// define stub functions for all functions in the jump table which take the
+// parameters from the emulated 68k registers. The parameters don't need to
+// be specified again, because these are really functions taking no direct
+// parameters. Only the type of the returned value is required.
+LIBSTUB(SayHelloOS4, char *)
+{
+  __BASE_OR_IFACE = (__BASE_OR_IFACE_TYPE)REG_A6;
+  return CALL_LFUNC_NP(SayHelloOS4);
+}
+
+LIBSTUB(SayHelloOS3, char *)
+{
+  __BASE_OR_IFACE = (__BASE_OR_IFACE_TYPE)REG_A6;
+  return CALL_LFUNC_NP(SayHelloOS3);
+}
+
+LIBSTUB(SayHelloMOS, char *)
+{
+  __BASE_OR_IFACE = (__BASE_OR_IFACE_TYPE)REG_A6;
+  return CALL_LFUNC_NP(SayHelloMOS);
+}
+
+LIBSTUB(Uppercase, char *)
+{
+  __BASE_OR_IFACE = (__BASE_OR_IFACE_TYPE)REG_A6;
+  return CALL_LFUNC(Uppercase, (char *)REG_A0);
+}
+
+LIBSTUB(SPrintfA, char *)
+{
+  __BASE_OR_IFACE = (__BASE_OR_IFACE_TYPE)REG_A6;
+  return CALL_LFUNC(SprintfA, (char)REG_A0, (char *)REG_A1, (APTR)REG_A2);
+}
+#endif
+
+/******************************************************************************/
+/*    Starting from here starts the "standard" Amiga library initialization.  */
+/*  All the above should have illustrated enough how "easy" it is to use the  */
+/*  SDI_lib.h header file and how it can help in developing a shared library  */
+/*               for multiple AmigaOS compatible platforms.                   */
+/******************************************************************************/
+
+#if defined(__amigaos4__)
+
+LIBFUNC static struct LibraryHeader * LibInit    (struct LibraryHeader *base, BPTR librarySegment, struct ExecIFace *pIExec);
+LIBFUNC static BPTR                   LibExpunge (struct LibraryManagerInterface *Self);
+LIBFUNC static struct LibraryHeader * LibOpen    (struct LibraryManagerInterface *Self, ULONG version);
+LIBFUNC static BPTR                   LibClose   (struct LibraryManagerInterface *Self);
+
+#elif defined(__MORPHOS__)
+
+LIBFUNC static struct LibraryHeader * LibInit   (struct LibraryHeader *base, BPTR librarySegment, struct ExecBase *sb);
+LIBFUNC static BPTR                   LibExpunge(void);
+LIBFUNC static struct LibraryHeader * LibOpen   (void);
+LIBFUNC static BPTR                   LibClose  (void);
+LIBFUNC static LONG                   LibNull   (void);
+
+#else
+
+LIBFUNC static struct LibraryHeader * LibInit    (REG(a0, BPTR Segment), REG(d0, struct LibraryHeader *lh), REG(a6, struct ExecBase *sb));
+LIBFUNC static BPTR                   LibExpunge (REG(a6, struct LibraryHeader *base));
+LIBFUNC static struct LibraryHeader * LibOpen    (REG(a6, struct LibraryHeader *base));
+LIBFUNC static BPTR                   LibClose   (REG(a6, struct LibraryHeader *base));
+LIBFUNC static LONG                   LibNull    (void);
+
+#endif
+
+/******************************************************************************/
+/* Dummy entry point and LibNull() function all in one                        */
+/******************************************************************************/
+
+#if defined(__amigaos4__)
+int _start(void)
+#else
+int Main(void)
+#endif
+{
+  return RETURN_FAIL;
+}
+
+#if !defined(__amigaos4__)
+LIBFUNC static LONG LibNull(VOID)
+{
+  return(0);
+}
+#endif
+
+/******************************************************************************/
+/* Local data structures                                                      */
+/******************************************************************************/
+
+#if defined(__amigaos4__)
+/* ------------------- OS4 Manager Interface ------------------------ */
+STATIC ULONG LibObtain(struct LibraryManagerInterface *Self)
+{
+  return(Self->Data.RefCount++);
+}
+
+STATIC ULONG LibRelease(struct LibraryManagerInterface *Self)
+{
+  return(Self->Data.RefCount--);
+}
+
+STATIC CONST APTR LibManagerVectors[] =
+{
+  (APTR)LibObtain,
+  (APTR)LibRelease,
+  (APTR)NULL,
+  (APTR)NULL,
+  (APTR)LibOpen,
+  (APTR)LibClose,
+  (APTR)LibExpunge,
+  (APTR)NULL,
+  (APTR)-1
+};
+
+STATIC CONST struct TagItem LibManagerTags[] =
+{
+  {MIT_Name,             (ULONG)"__library"},
+  {MIT_VectorTable,      (ULONG)LibManagerVectors},
+  {MIT_Version,          1},
+  {TAG_DONE,             0}
+};
+
+/* ------------------- Library Interface(s) ------------------------ */
+
+STATIC CONST APTR LibVectors[] =
+{
+  (APTR)LibObtain,
+  (APTR)LibRelease,
+  (APTR)NULL,
+  (APTR)NULL,
+  (APTR)libvector,
+  (APTR)-1
+};
+
+STATIC CONST struct TagItem MainTags[] =
+{
+  {MIT_Name,              (ULONG)"main"},
+  {MIT_VectorTable,       (ULONG)LibVectors},
+  {MIT_Version,           1},
+  {TAG_DONE,              0}
+};
+
+
+STATIC CONST ULONG LibInterfaces[] =
+{
+  (ULONG)LibManagerTags,
+  (ULONG)MainTags,
+  (ULONG)0
+};
+
+/* --------------------- m68k Library stubs ------------------------ */
+
+STATIC ULONG stub_OpenPPC(ULONG *regarray)
+{
+	struct Library *Base = (struct Library *) regarray[REG68K_A6/4];
+	struct ExtendedLibrary *ExtLib = (struct ExtendedLibrary *) ((ULONG)Base + Base->lib_PosSize);
+	struct LibraryManagerInterface *Self = (struct LibraryManagerInterface *) ExtLib->ILibrary;
+
+	return (ULONG) Self->Open(0);
+}
+struct EmuTrap stub_Open = { TRAPINST, TRAPTYPE, stub_OpenPPC };
+
+STATIC ULONG stub_ClosePPC(ULONG *regarray)
+{
+	struct Library *Base = (struct Library *) regarray[REG68K_A6/4];
+	struct ExtendedLibrary *ExtLib = (struct ExtendedLibrary *) ((ULONG)Base + Base->lib_PosSize);
+	struct LibraryManagerInterface *Self = (struct LibraryManagerInterface *) ExtLib->ILibrary;
+
+	return (ULONG) Self->Close();
+}
+struct EmuTrap stub_Close = { TRAPINST, TRAPTYPE, stub_ClosePPC };
+
+STATIC ULONG stub_ExpungePPC(ULONG *regarray)
+{
+  return 0UL;
+}
+struct EmuTrap stub_Expunge = { TRAPINST, TRAPTYPE, stub_ExpungePPC };
+
+STATIC ULONG stub_ReservedPPC(ULONG *regarray)
+{
+  return 0UL;
+}
+struct EmuTrap stub_Reserved = { TRAPINST, TRAPTYPE, stub_ReservedPPC };
+
+STATIC ULONG stub_SayHelloOS4PPC(ULONG *regarray)
+{
+	struct Library *Base = (struct Library *) regarray[REG68K_A6/4];
+	struct ExtendedLibrary *ExtLib = (struct ExtendedLibrary *) ((ULONG)Base + Base->lib_PosSize);
+	struct LibraryManagerInterface *Self = (struct LibraryManagerInterface *) ExtLib->ILibrary;
+
+	return (char *)Self->SayHelloOS4();
+}
+struct EmuTrap stub_SayHelloOS4 = { TRAPINST, TRAPTYPE, stub_SayHelloOS4PPC };
+
+STATIC ULONG stub_SayHelloOS3PPC(ULONG *regarray)
+{
+	struct Library *Base = (struct Library *) regarray[REG68K_A6/4];
+	struct ExtendedLibrary *ExtLib = (struct ExtendedLibrary *) ((ULONG)Base + Base->lib_PosSize);
+	struct LibraryManagerInterface *Self = (struct LibraryManagerInterface *) ExtLib->ILibrary;
+
+	return (char *)Self->SayHelloOS3();
+}
+struct EmuTrap stub_SayHelloOS3 = { TRAPINST, TRAPTYPE, stub_SayHelloOS3PPC };
+
+STATIC ULONG stub_SayHelloMOSPPC(ULONG *regarray)
+{
+	struct Library *Base = (struct Library *) regarray[REG68K_A6/4];
+	struct ExtendedLibrary *ExtLib = (struct ExtendedLibrary *) ((ULONG)Base + Base->lib_PosSize);
+	struct LibraryManagerInterface *Self = (struct LibraryManagerInterface *) ExtLib->ILibrary;
+
+	return (char *)Self->SayHelloMOS();
+}
+struct EmuTrap stub_SayHelloMOS = { TRAPINST, TRAPTYPE, stub_SayHelloMOSPPC };
+
+STATIC ULONG stub_UppercasePPC(ULONG *regarray)
+{
+	struct Library *Base = (struct Library *) regarray[REG68K_A6/4];
+	struct ExtendedLibrary *ExtLib = (struct ExtendedLibrary *) ((ULONG)Base + Base->lib_PosSize);
+	struct LibraryManagerInterface *Self = (struct LibraryManagerInterface *) ExtLib->ILibrary;
+
+	return (char *)Self->Uppercase(
+		(char *)regarray[8]
+	);
+}
+struct EmuTrap stub_Uppercase = { TRAPINST, TRAPTYPE, stub_UppercasePPC };
+
+STATIC ULONG stub_SPrintfAPPC(ULONG *regarray)
+{
+	struct Library *Base = (struct Library *) regarray[REG68K_A6/4];
+	struct ExtendedLibrary *ExtLib = (struct ExtendedLibrary *) ((ULONG)Base + Base->lib_PosSize);
+	struct LibraryManagerInterface *Self = (struct LibraryManagerInterface *) ExtLib->ILibrary;
+
+	return (char *)Self->SPrintfA(
+		(char *)regarray[8],
+		(char *)regarray[9],
+		(APTR)regarray[10]
+	);
+}
+struct EmuTrap stub_SPrintfA = { TRAPINST, TRAPTYPE, stub_SPrintfAPPC };
+
+STATIC ULONG VecTable68K[] =
+{
+  (ULONG)&stub_Open,
+  (ULONG)&stub_Close,
+  (ULONG)&stub_Expunge,
+  (ULONG)&stub_Reserved,
+  (ULONG)&stub_SayHelloOS4,
+  (ULONG)&stub_SayHelloOS3,
+  (ULONG)&stub_SayHelloMOS,
+  (ULONG)&stub_Uppercase,
+  (ULONG)&stub_SprintfA,
+  (ULONG)-1
+};
+
+/* ----------------------- LibCreate Tags -------------------------- */
+
+STATIC CONST struct TagItem LibCreateTags[] =
+{
+  {CLT_DataSize,   (ULONG)(sizeof(struct LibraryHeader))},
+  {CLT_InitFunc,   (ULONG)LibInit},
+  {CLT_Interfaces, (ULONG)LibInterfaces},
+  {CLT_Vector68K,  (ULONG)VecTable68K},
+  {TAG_DONE,       0}
+};
+
+#else
+
+STATIC CONST APTR LibVectors[] =
+{
+  #ifdef __MORPHOS__
+  (APTR)FUNCARRAY_32BIT_NATIVE,
+  #endif
+  (APTR)LibOpen,
+  (APTR)LibClose,
+  (APTR)LibExpunge,
+  (APTR)LibNull,
+  (APTR)libvector,
+  (APTR)-1
+};
+
+STATIC CONST ULONG LibInitTab[] =
+{
+  sizeof(struct LibraryHeader),
+  (ULONG)LibVectors,
+  (ULONG)NULL,
+  (ULONG)LibInit
+};
+
+#endif
+
+/* ------------------- ROM Tag ------------------------ */
+static const USED_VAR struct Resident ROMTag =
+{
+  RTC_MATCHWORD,
+  (struct Resident *)&ROMTag,
+  (struct Resident *)&ROMTag + 1,
+  #if defined(__amigaos4__)
+  RTF_AUTOINIT|RTF_NATIVE,      // The Library should be set up according to the given table.
+  #elif defined(__MORPHOS__)
+  RTF_AUTOINIT|RTF_PPC,
+  #else
+  RTF_AUTOINIT,
+  #endif
+  VERSION,
+  NT_LIBRARY,
+  0,
+  (APTR)UserLibName,
+  VSTRING,
+  #if defined(__amigaos4__)
+  (APTR)LibCreateTags           // This table is for initializing the Library.
+  #else
+  (APTR)LibInitTab
+  #endif
+};
+
+#if defined(__MORPHOS__)
+/*
+ * To tell the loader that this is a new emulppc elf and not
+ * one for the ppc.library.
+ * ** IMPORTANT **
+ */
+ULONG	USED_VAR __amigappc__=1;
+ULONG	USED_VAR __abox__=1;
+
+#endif /* __MORPHOS */
+
+/******************************************************************************/
+/* Standard Library Functions, all of them are called in Forbid() state.      */
+/******************************************************************************/
+
+#ifndef __amigaos4__
+#define DeleteLibrary(LIB) \
+  FreeMem((STRPTR)(LIB)-(LIB)->lib_NegSize, (ULONG)((LIB)->lib_NegSize+(LIB)->lib_PosSize))
+#endif
+
+#if defined(__amigaos4__)
+static struct LibraryHeader * LibInit(struct LibraryHeader *base, BPTR librarySegment, struct ExecIFace *pIExec)
+{
+  struct ExecBase *sb = (struct ExecBase *)pIExec->Data.LibBase;
+  IExec = pIExec;
+#elif defined(__MORPHOS__)
+static struct LibraryHeader * LibInit(struct LibraryHeader *base, BPTR librarySegment, struct ExecBase *sb)
+{
+#else
+LIBFUNC static struct LibraryHeader * LibInit(REG(a0, BPTR librarySegment), REG(d0, struct LibraryHeader *base), REG(a6, struct ExecBase *sb))
+{
+#endif
+
+  SysBase = (APTR)sb;
+
+  base->libBase.lib_Node.ln_Type = NT_LIBRARY;
+  base->libBase.lib_Node.ln_Pri  = 0;
+  base->libBase.lib_Node.ln_Name = (char *)UserLibName;
+  base->libBase.lib_Flags        = LIBF_CHANGED | LIBF_SUMUSED;
+  base->libBase.lib_Version      = VERSION;
+  base->libBase.lib_Revision     = REVISION;
+  base->libBase.lib_IdString     = (char *)UserLibID;
+
+  base->segList = librarySegment;
+  base->sysBase = (APTR)SysBase;
+
+  return(base);
+}
+
+#if defined(__amigaos4__)
+static BPTR LibExpunge(struct LibraryManagerInterface *Self)
+{
+  struct LibraryHeader *base = (struct LibraryHeader *)Self->Data.LibBase;
+#elif defined(__MORPHOS__)
+static BPTR LibExpunge(void)
+{
+	struct LibraryHeader *base = (struct LibraryHeader*)REG_A6;
+#else
+LIBFUNC static BPTR LibExpunge(REG(a6, struct LibraryHeader *base))
+{
+#endif
+  BPTR rc;
+
+  if(base->libBase.lib_OpenCnt > 0)
+  {
+    base->libBase.lib_Flags |= LIBF_DELEXP;
+    return(0);
+  }
+
+  SysBase = (APTR)base->sysBase;
+  rc = base->segList;
+
+  Remove((struct Node *)base);
+  DeleteLibrary(&base->libBase);
+
+  return(rc);
+}
+
+#if defined(__amigaos4__)
+static struct LibraryHeader *LibOpen(struct LibraryManagerInterface *Self, ULONG version)
+{
+  struct LibraryHeader *base = (struct LibraryHeader *)Self->Data.LibBase;
+#elif defined(__MORPHOS__)
+static struct LibraryHeader *LibOpen(void)
+{
+  struct LibraryHeader *base = (struct LibraryHeader*)REG_A6;
+#else
+LIBFUNC static struct LibraryHeader * LibOpen(REG(a6, struct LibraryHeader *base))
+{
+#endif
+
+  base->libBase.lib_Flags &= ~LIBF_DELEXP;
+  base->libBase.lib_OpenCnt++;
+
+  return base;
+}
+
+#if defined(__amigaos4__)
+static BPTR LibClose(struct LibraryManagerInterface *Self)
+{
+  struct LibraryHeader *base = (struct LibraryHeader *)Self->Data.LibBase;
+#elif defined(__MORPHOS__)
+static BPTR LibClose(void)
+{
+  struct LibraryHeader *base = (struct LibraryHeader *)REG_A6;
+#else
+LIBFUNC static BPTR LibClose(REG(a6, struct LibraryHeader *base))
+{
+#endif
+
+  if(base->libBase.lib_OpenCnt > 0 &&
+     --base->libBase.lib_OpenCnt == 0)
+  {
+    if(base->libBase.lib_Flags & LIBF_DELEXP)
+    {
+      #if defined(__amigaos4__)
+      return LibExpunge(Self);
+      #elif defined(__MORPHOS__)
+      return LibExpunge();
+      #else
+      return LibExpunge(base);
+      #endif
+    }
+  }
+
+  return 0;
+}
