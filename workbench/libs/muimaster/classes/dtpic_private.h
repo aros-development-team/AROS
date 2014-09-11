53a2374d6cd2a7a85db0d7d8fefd4694fa25c49c
mattrust <mrustler@gmx.de>
2014-09-11 06:59:44 +0000

    dtpic.mui: added MUI4 attributes. (Only attribute handling, implementation will follow later)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@49611 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/classes/dtpic_private.h
+++ workbench/libs/muimaster/classes/dtpic_private.h
@@ -9,6 +9,10 @@ struct Dtpic_DATA
     APTR dto;
     struct BitMapHeader *bmhd;
     struct BitMap *bm;
+    LONG alpha;
+    BOOL darkenselstate;
+    LONG fade;
+    BOOL lightenonmouse;
 };
 
 #endif /* _DTPIC_PRIVATE_H_ */
