f8e87afc8b8cb60f1fd1945eedfeac5edf696ebb
mattrust <mrustler@gmx.de>
2014-09-13 15:46:30 +0000

    Dtpic class updated: - added event handling (no rendering changes yes) - defined DtpicObject, making it an official Zune class - name attribute no longer StrDup'ped, following   AOS4-MUI4 autodoc.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@49618 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/classes/dtpic_private.h
+++ workbench/libs/muimaster/classes/dtpic_private.h
@@ -1,18 +1,31 @@
 #ifndef _DTPIC_PRIVATE_H_
 #define _DTPIC_PRIVATE_H_
 
+#include <exec/types.h>
+#include <libraries/mui.h>
+
 /*** Instance data **********************************************************/
 struct Dtpic_DATA
 {
     struct Library *datatypesbase;
+
     STRPTR name;
     APTR dto;
     struct BitMapHeader *bmhd;
     struct BitMap *bm;
+    struct MUI_EventHandlerNode ehn;
+
+    BOOL highlighted;   // mouse pointer is within object
+    BOOL selected;      // gadget is selected
+    LONG deltaalpha;    // increment/decrement for each tick
+    LONG currentalpha;  // the actual alpha for rendering
+    BOOL eh_active;     // TRUE after MUIM_Window_AddEventHandler
+
     LONG alpha;
     BOOL darkenselstate;
     LONG fade;
     BOOL lightenonmouse;
+
 };
 
 #endif /* _DTPIC_PRIVATE_H_ */
