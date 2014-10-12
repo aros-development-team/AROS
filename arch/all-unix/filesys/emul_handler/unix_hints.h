8547ecdb630bde47a4a571d68d43c2967ec0f1ed
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2014-10-12 03:53:08 +0000

    On i386-darwin, some POSIX conformant versions of functions have a "$UNIX2003" suffix. We now add this suffix where necessary, as otherwise we link to legacy function versions that may cause bugs. For example, rewinddir(), seekdir() and telldir() did not work when called from emul-handler (at least under OS X 10.6), leading to the root AROS directory not being listable more than once!
    git-svn-id: https://svn.aros.org/svn/aros/trunk@49703 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-unix/filesys/emul_handler/unix_hints.h
+++ arch/all-unix/filesys/emul_handler/unix_hints.h
@@ -1,4 +1,9 @@
 /*
+    Copyright © 1995-2014, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+/*
  * Some really black magic. Adjusts compilation for various UNIX weirdness.
  * This stuff is placed in a separate file because it needs to be included
  * before any UNIX headers.
@@ -24,13 +29,31 @@
 
 #else
 
-/* 
+/*
  * Use 32-bit inode_t on Darwin. Otherwise we are expected to use "stat$INODE64"
  * instead of "stat" function which is available only on MacOS 10.6.
  */
 #define _DARWIN_NO_64_BIT_INODE
 #endif
 
+#ifdef HOST_OS_darwin
+
+#ifdef __i386__
+/*
+ * Under i386 there are both POSIX-conformant and non-POSIX-conformant
+ * variants of certain functions, the former suffixed by $UNIX2003. Bugs
+ * occur if we use non-conformant versions of some functions (e.g. rewinddir()
+ * doesn't rewind), so we always use the conformant functions where there is a
+ * choice
+ */
+#define UNIX2003_SUFFIX "$UNIX2003"
+#endif
+#endif
+
 #ifndef INODE64_SUFFIX
 #define INODE64_SUFFIX
+#endif
+
+#ifndef UNIX2003_SUFFIX
+#define UNIX2003_SUFFIX
 #endif
