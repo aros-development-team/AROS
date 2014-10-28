d9dfff470776ae3a9a2c3d519c82c05185988ba8
DizzyOfCRN <DizzyOfCRN@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2014-10-28 09:57:39 +0000

    BROM loads 4K of boot0 code (=bootstrap) make sure the first level bootcode fits inside. bootstrap code never returns and is inside boot0 section, make sure text section is behind boot0. Make also a map file and assembler output file.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@49730 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-sun4i/bootstrap/bootstrap.c
+++ arch/arm-sun4i/bootstrap/bootstrap.c
@@ -6,6 +6,13 @@
     Lang: english
 */
 
-void bootstrap(void) {
+#define asmdelay(t) asm volatile("mov r0, %[value]\n1: sub r0, #1\nbne 1b\n"::[value] "i" (t) : "r0", "cc");
+
+void __attribute__((noreturn, section(".bootstrap"))) bootstrap(void) {
+
+    asmdelay(200);
+    asmdelay(200);
+
     while(1);
 }
+
