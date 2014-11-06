de23ede3481ab6a06b323c3e78a8ec440a2bf253
DizzyOfCRN <DizzyOfCRN@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2014-11-06 16:50:10 +0000

    Convert timer module c structure definition to volatile pointers (and apparently fix it also)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@49761 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-sun4i/include/hardware/tmr.h
+++ arch/arm-sun4i/include/hardware/tmr.h
@@ -17,51 +17,49 @@
 #include <inttypes.h>
 #endif
 
-#define SUN4I_TMR_BASE			0x01c20c00
+#define SUN4I_TIMER_BASE        0x01c20c00
 
-struct TMR {
-	uint32_t IRQ_EN;
-	uint32_t IRQ_STA;
-	uint32_t 0_CTRL;
-	uint32_t 0_INTR_VAL;
-	uint32_t 0_CUR_VAL;
-	uint32_t 1_CTRL;
-	uint32_t 1_INTR_VAL;
-	uint32_t 1_CUR_VAL;
-	uint32_t 2_CTRL;
-	uint32_t 2_INTR_VAL;
-	uint32_t 2_CUR_VAL;
-	uint32_t 3_CTRL;
-	uint32_t 3_INTR_VAL;
-	uint32_t 3_CUR_VAL;
-	uint32_t 4_CTRL;
-	uint32_t 4_INTR_VAL;
-	uint32_t 4_CUR_VAL;
-	uint32_t 5_CTRL;
-	uint32_t 5_INTR_VAL;
-	uint32_t 5_CUR_VAL;
-	uint32_t AVS_CTRL;
-	uint32_t AVS0;
-	uint32_t AVS1;
-	uint32_t AVS_DIV;
-	uint32_t WDT_CTRL;
-	uint32_t WDT_MODE;
-	uint32_t CNT64_CTRL;
-	uint32_t CNT64_LO;
-	uint32_t CNT64_HI;
-	uint32_t 32KHZ_OSC_CTRL;
-	uint32_t RTC_DATE;
-	uint32_t RTC_TIME;
-	uint32_t ALARM_CNT;
-	uint32_t ALARM_WK;
-	uint32_t ALARM_EN;
-	uint32_t ALARM_IRQ_EN;
-	uint32_t ALARM_IRQ_STA;
-	uint32_t GP0;
-	uint32_t GP1;
-	uint32_t GP2;
-	uint32_t GP3;
-	uint32_t CPU_CFG;
-}__attribute__((__packed__));
+#define TIMER_IRQ_EN            (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0000))
+#define TIMER_IRQ_STA           (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0004))
+#define TIMER0_CTRL             (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0010))
+#define TIMER0_INTR_VAL         (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0014))
+#define TIMER0_CUR_VAL          (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0018))
+#define TIMER1_CTRL             (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0020))
+#define TIMER1_INTR_VAL         (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0024))
+#define TIMER1_CUR_VAL          (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0028))
+#define TIMER2_CTRL             (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0030))
+#define TIMER2_INTR_VAL         (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0034))
+#define TIMER2_CUR_VAL          (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0038))
+#define TIMER3_CTRL             (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0040))
+#define TIMER3_INTR_VAL         (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0044))
+#define TIMER3_CUR_VAL          (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0048))
+#define TIMER4_CTRL             (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0050))
+#define TIMER4_INTR_VAL         (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0054))
+#define TIMER4_CUR_VAL          (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0058))
+#define TIMER5_CTRL             (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0060))
+#define TIMER5_INTR_VAL         (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0064))
+#define TIMER5_CUR_VAL          (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0068))
+#define TIMER_AVS_CTRL          (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0080))
+#define TIMER_AVS0              (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0084))
+#define TIMER_AVS1              (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0088))
+#define TIMER_AVS_DIV           (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x008c))
+#define TIMER_WDT_CTRL          (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0090))
+#define TIMER_WDT_MODE          (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0094))
+#define TIMER_CNT64_CTRL        (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x00a0))
+#define TIMER_CNT64_LO          (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x00a4))
+#define TIMER_CNT64_HI          (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x00a8))
+#define TIMER_32KHZ_OSC_CTRL    (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0100))
+#define TIMER_RTC_DATE          (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0104))
+#define TIMER_RTC_TIME          (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0108))
+#define TIMER_ALARM_CNT         (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x010c))
+#define TIMER_ALARM_WK          (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0110))
+#define TIMER_ALARM_EN          (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0114))
+#define TIMER_ALARM_IRQ_EN      (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0118))
+#define TIMER_ALARM_IRQ_STA     (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x011c))
+#define TIMER_GP0               (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0120))
+#define TIMER_GP1               (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0124))
+#define TIMER_GP2               (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0128))
+#define TIMER_GP3               (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x012c))
+#define TIMER_CPU_CFG           (*(volatile uint32_t *)(SUN4I_TIMER_BASE + 0x0140))
 
 #endif
