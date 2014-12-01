605283bfc7af12745214df5382d5762750d13872
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2014-12-01 02:26:51 +0000

    Bale out early from PS/2 mouse detection if an incorrect result is received from the reset command. This avoids most of a 15 second boot delay when no PS/2 mouse is attached.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@49816 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/hidds/i8042/startup.c
+++ rom/hidds/i8042/startup.c
@@ -27,13 +27,13 @@ static int init_kbd(struct kbdbase *LIBBASE)
     if ((!kbd) || (!ms))
     {
         /* This can be triggered by old base kickstart */
-        D(bug("[i8042} Subsystem classes not found\n"));
+        D(bug("[i8042] Subsystem classes not found\n"));
         return FALSE;
     }
 
     if (!HW_AddDriver(kbd, LIBBASE->ksd.kbdclass, NULL))
     {
-        D(bug("{i8042} No controller detected\n"));
+        D(bug("[i8042] No controller detected\n"));
         return FALSE;
     }
     LIBBASE->library.lib_OpenCnt = 1;
@@ -41,7 +41,7 @@ static int init_kbd(struct kbdbase *LIBBASE)
     /* Mouse can be missing, it's not a failure */
     if (HW_AddDriver(ms, LIBBASE->ksd.mouseclass, NULL))
     {
-        D(bug("{i8042] Mouse driver installed\n"));    
+        D(bug("[i8042] Mouse driver installed\n"));
         LIBBASE->library.lib_OpenCnt++;
     }
 
