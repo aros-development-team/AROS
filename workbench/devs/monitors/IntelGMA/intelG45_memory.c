3fbfdeed5f2db284b6e701aa553b1b073f51d0e5
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2015-03-11 20:56:47 +0000

    Tabs to spaces.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50195 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/devs/monitors/IntelGMA/intelG45_memory.c
+++ workbench/devs/monitors/IntelGMA/intelG45_memory.c
@@ -12,72 +12,81 @@
 
 intptr_t G45_VirtualToPhysical(struct g45staticdata *sd, intptr_t virtual)
 {
-	intptr_t page = virtual >> 12;	/* get page number */
-	intptr_t result = -1;
+    intptr_t page = virtual >> 12;      /* get page number */
+    intptr_t result = -1;
 
-	if (page >= 0 && page < sd->Card.GATT_size / 4)
-	{
-		uint32_t pte = readl(&sd->Card.GATT[page]);
-		if (pte & 1)
-		{
-			result = pte & 0xfffff000;
-			result |= virtual & 0xfff;
-		}
-	}
+    if (page >= 0 && page < sd->Card.GATT_size / 4)
+    {
+        uint32_t pte = readl(&sd->Card.GATT[page]);
+        if (pte & 1)
+        {
+            result = pte & 0xfffff000;
+            result |= virtual & 0xfff;
+        }
+    }
 
-	return result;
+    return result;
 }
 
-void G45_AttachMemory(struct g45staticdata *sd, intptr_t physical, intptr_t virtual, intptr_t length)
+void G45_AttachMemory(struct g45staticdata *sd, intptr_t physical,
+    intptr_t virtual, intptr_t length)
 {
-	intptr_t page = virtual >> 12;
+    intptr_t page = virtual >> 12;
 
-	if (page > 0)
-	{
-		physical &= 0xfffff000;
-		length &= 0xfffff000;
+    if (page > 0)
+    {
+        physical &= 0xfffff000;
+        length &= 0xfffff000;
 
-		do {
-			writel(physical | 1, &sd->Card.GATT[page]);
+        do
+        {
+            writel(physical | 1, &sd->Card.GATT[page]);
 
-			physical += 4096;
-			length -= 4096;
-			page++;
-		} while((page < sd->Card.GATT_size / 4) && length);
-	}
+            physical += 4096;
+            length -= 4096;
+            page++;
+        }
+        while ((page < sd->Card.GATT_size / 4) && length);
+    }
     CacheClearU();
 }
 
-void G45_AttachCacheableMemory(struct g45staticdata *sd, intptr_t physical, intptr_t virtual, intptr_t length)
+void G45_AttachCacheableMemory(struct g45staticdata *sd, intptr_t physical,
+    intptr_t virtual, intptr_t length)
 {
-	intptr_t page = virtual >> 12;
+    intptr_t page = virtual >> 12;
 
-	if (page > 0)
-	{
-		physical &= 0xfffff000;
-		length &= 0xfffff000;
+    if (page > 0)
+    {
+        physical &= 0xfffff000;
+        length &= 0xfffff000;
 
-		do {
-			writel(physical | 7, &sd->Card.GATT[page]);
+        do
+        {
+            writel(physical | 7, &sd->Card.GATT[page]);
 
-			physical += 4096;
-			length -= 4096;
-			page++;
-		} while((page < sd->Card.GATT_size / 4) && length);
-	}
+            physical += 4096;
+            length -= 4096;
+            page++;
+        }
+        while ((page < sd->Card.GATT_size / 4) && length);
+    }
     CacheClearU();
 }
 
-void G45_DetachMemory(struct g45staticdata *sd, intptr_t virtual, intptr_t length)
+void G45_DetachMemory(struct g45staticdata *sd, intptr_t virtual,
+    intptr_t length)
 {
-	intptr_t page = virtual >> 12;
+    intptr_t page = virtual >> 12;
 
-	if (page >= 0) do
-	{
-		writel(0, &sd->Card.GATT[page]);
+    if (page >= 0)
+        do
+        {
+            writel(0, &sd->Card.GATT[page]);
 
-		page++;
-		length--;
-	} while(length && page < (sd->Card.GATT_size / 4));
+            page++;
+            length--;
+        }
+        while (length && page < (sd->Card.GATT_size / 4));
     CacheClearU();
 }
