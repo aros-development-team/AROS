15a84ea463a7e04375e02cbe02153a30e084f685
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2015-03-28 02:21:35 +0000

    - Round up modification time to the next even second because of FAT's    two-second granularity in this field. When copying files based on    newness, it is better to have a time one second newer than it should    be rather than one second older.  - Removed the manual version string that was already superseded by a    generated one.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50246 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/filesys/fat/main.c
+++ rom/filesys/fat/main.c
@@ -2,7 +2,7 @@
  * fat-handler - FAT12/16/32 filesystem handler
  *
  * Copyright © 2006 Marek Szyprowski
- * Copyright © 2007-2014 The AROS Development Team
+ * Copyright © 2007-2015 The AROS Development Team
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the same terms as AROS itself.
@@ -39,8 +39,6 @@ struct Library *IntuitionBase;
 
 struct Globals global_data;
 struct Globals *glob = &global_data;
-
-const TEXT version_string[] = "$VER: fat-handler 41.58 (28.11.2014)";
 
 LONG handler(struct ExecBase *SysBase) {
     struct Message *msg;
