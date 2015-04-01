e4b03458ed6feae3eb7831e49272e0b67d6bc4b9
Kalamatee <kalamatee@gmail.com>
2015-04-01 02:07:17 +0000

    move amba code to its own location (wip)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50279 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-raspi/kernel/platform_bcm2708.c
+++ arch/arm-raspi/kernel/platform_bcm2708.c
@@ -9,32 +9,7 @@
 
 static void bcm2708_init(void)
 {
-#if (0)
-    // TODO:
-    // How to identify broadcom IP's?
-    // Expose this as a seprate subsystem (like PCI?)
-    D(bug("[KRN:BCM2708] Integrated Peripherals -:\n"));
-    for (ptr = ARM_PERIIOBASE; ptr < (ARM_PERIIOBASE + ARM_PERIIOSIZE); ptr += ARM_PRIMECELLPERISIZE)
-    {
-        unsigned int perihreg = (*(volatile unsigned int *)(ptr + 0xFF0) & 0xFF) | (*(volatile unsigned int *)(ptr + 0xFF4) & 0xFF) << 8 | (*(volatile unsigned int *)(ptr + 0xFF8) & 0xFF) << 16 | (*(volatile unsigned int *)(ptr + 0xFFC) & 0xFF) << 24;
-        if (perihreg == ARM_PRIMECELLID)
-        {
-            perihreg = (*(volatile unsigned int *)(ptr + 0xFE0) & 0xFF) | (*(volatile unsigned int *)(ptr + 0xFE4) & 0xFF) << 8 | (*(volatile unsigned int *)(ptr + 0xFE8) & 0xFF) << 16 | (*(volatile unsigned int *)(ptr + 0xFEC) & 0xFF) << 24;
-            unsigned int manu = (perihreg & (0x7F << 12)) >> 12;
-            unsigned int prod = (perihreg & 0xFFF);
-            unsigned int rev = (perihreg & (0xF << 20)) >> 20;
-            unsigned int config = (perihreg & (0x7F << 24)) >> 24;
-            D(bug("[KRN:BCM2708]           0x%p: manu %x, prod %x, rev %d, config %d\n", ptr, manu, prod, rev, config));
-        }
-/*        else
-        {
-            if (perihreg)
-            {
-                D(bug("[Kernel] PlatformPostInit:           0x%p: PrimeCellID != %08x\n", ptr, perihreg));
-            }
-        }*/
-    }
-#endif
+
 }
 
 static void bcm2708_toggle_led(int LED, int state)
