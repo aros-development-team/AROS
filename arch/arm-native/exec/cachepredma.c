4998ed6f573e4240221e774313ab7042da4d4024
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2015-04-11 20:35:12 +0000

    First cache management functions in place
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50336 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/arm-native/exec/cachepredma.c
@@ -0,0 +1,40 @@
+/*
+    Copyright © 1995-2013, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: CachePreDMA() - Do what is necessary for DMA.
+    Lang: english
+*/
+
+#define DEBUG 0
+
+#include <aros/debug.h>
+#include <exec/types.h>
+#include <aros/libcall.h>
+#include <proto/kernel.h>
+
+#include "exec_intern.h"
+
+/* See rom/exec/cachepredma.c for documentation */
+
+#include <proto/exec.h>
+
+AROS_LH3(APTR, CachePreDMA,
+    AROS_LHA(APTR,    address, A0),
+    AROS_LHA(ULONG *, length,  A1),
+    AROS_LHA(ULONG,   flags,  D0),
+    struct ExecBase *, SysBase, 127, Exec)
+{
+    AROS_LIBFUNC_INIT
+
+    void *addr = KrnVirtualToPhysical(address);
+
+    D(bug("[exec] CachePreDMA(%08x, %d, %c) = %08x\n", address, *length, flags & DMA_ReadFromRAM ? 'R':'W', addr));
+
+    /* At PreDMA stage only data caches need to be flushed */
+    CacheClearE(address, *length, CACRF_ClearD);
+
+    return addr;
+
+    AROS_LIBFUNC_EXIT
+} /* CachePreDMA() */
