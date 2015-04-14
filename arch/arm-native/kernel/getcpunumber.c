98433e6d207291ab0c7c9be0569bef458951f2a0
Kalamatee <kalamatee@gmail.com>
2015-04-14 01:39:37 +0000

    switch to super before accessing the register
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50376 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-native/kernel/getcpunumber.c
+++ arch/arm-native/kernel/getcpunumber.c
@@ -14,9 +14,33 @@ AROS_LH0(unsigned int, KrnGetCPUNumber,
 {
     AROS_LIBFUNC_INIT
 
+    register unsigned int superSP;
     uint32_t tmp;
 
+    asm volatile (
+        "       stmfd   sp!, {lr}               \n"
+        "       mov     r1, sp                  \n"
+        "       swi     %[swi_no]               \n"
+        "       mov     %[superSP], sp          \n"
+        "       mov     sp, r1                  \n"
+        "       ldmfd   sp!, {lr}               \n"
+        : [superSP] "=r" (superSP)
+        : [swi_no] "I" (6 /*SC_SUPERSTATE*/) : "r1"
+    );
+
     asm volatile (" mrc p15, 0, %0, c0, c0, 5 " : "=r" (tmp));
+
+    if (superSP)
+    {
+        asm volatile (
+            "       stmfd   sp!, {lr}               \n"
+            "       mov     r1, sp                  \n"
+            "       mov     sp, %[superSP]          \n"
+            "       cpsie   i, %[mode_user]         \n"
+            "       mov     sp, r1                  \n"
+            "       ldmfd   sp!, {lr}               \n"
+            : : [superSP] "r" (superSP), [mode_user] "I" (CPUMODE_USER) : "r1" );
+    }
 
     if (tmp & (2 << 30))
     {
