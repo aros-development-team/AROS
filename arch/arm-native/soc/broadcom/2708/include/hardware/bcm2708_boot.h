e0f2bb226b18528ebb311d0711b5da7278122c8e
Kalamatee <kalamatee@gmail.com>
2015-04-14 17:02:02 +0000

    more corrections
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50383 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-native/soc/broadcom/2708/include/hardware/bcm2708_boot.h
+++ arch/arm-native/soc/broadcom/2708/include/hardware/bcm2708_boot.h
@@ -11,16 +11,19 @@
 /* define the boot strap memory layout */
 
 #define __bcm2708_vectsize              0x20
-#define __bcm2708_bootstacksize         16
+#define __bcm2708_bootstacksize         (256 << 2)
+#define __bcm2708_boottagssize          (128 << 3)
 
 struct bcm2708bootmem
 {
     UBYTE       bm_vectors[__bcm2708_vectsize];
-    UBYTE       bm_padding[(0x1000 - __bcm2708_vectsize - __bcm2708_bootstacksize)];
+    UBYTE       bm_padding1[(0x1000 - __bcm2708_vectsize - __bcm2708_bootstacksize - __bcm2708_boottagssize - 16)];
+    UBYTE       bm_boottags[__bcm2708_boottagssize];
     UBYTE       bm_bootstack[__bcm2708_bootstacksize];
+    UBYTE       bm_padding2[16];
     /* 0x1000 */
     UBYTE       bm_mboxmsg[0x1000];
-    UBYTE       bm_padding2[0x2000];
+    UBYTE       bm_padding3[0x2000];
     /* 0x4000 */
     pde_t       bm_pde[4096];
 }  __attribute__((packed));
