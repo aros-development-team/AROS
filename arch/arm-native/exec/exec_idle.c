3fb08cadbb62dd29a5c479a10bece73afeccc5f4
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2015-04-19 19:41:24 +0000

    Exec's idle task promotes itself to supervisor (actually system mode, which can be safely rescheduled by exec library), enables interrupts again and does put CPU to sleep in endless loop.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50430 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-native/exec/exec_idle.c
+++ arch/arm-native/exec/exec_idle.c
@@ -5,13 +5,20 @@
 
 #include <proto/exec.h>
 #include "exec_intern.h"
+#include "kernel_intern.h"
+#include "kernel_cpu.h"
+#include "kernel_syscall.h"
 
 void IdleTask(struct ExecBase *SysBase)
 {
-    D(bug("[Kernel] Idle task started up"));
+    D(bug("[Kernel] Idle task started up\n"));
+
+    asm volatile ("swi %[swi_no]" : : [swi_no] "I" (SC_SUPERSTATE) : "lr");
+    asm volatile ("swi %[swi_no]" : : [swi_no] "I" (SC_STI) : "lr");
 
     do
     { /* forever */
         D(bug("[IDLE] Nothing to do ..\n"));
+        asm volatile("mov r0, #0\n\t mcr p15, 0, r0, c7, c0, 4":::"r0");
     } while(1);
 }
