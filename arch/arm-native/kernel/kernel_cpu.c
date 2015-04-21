d9d27711933f034d2d8968dffeadcb035098d80a
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2015-04-21 18:49:01 +0000

    Wake up additional ARM cores of raspberry pi 2. This was necessary step since after bootup the cores were busy looping and polling registers, thus making raspi2 slower than the old raspi models. Now rpi2 shows its real power ;)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50440 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-native/kernel/kernel_cpu.c
+++ arch/arm-native/kernel/kernel_cpu.c
@@ -10,6 +10,8 @@
 
 #include <proto/kernel.h>
 
+#include <strings.h>
+
 #include "etask.h"
 
 #include "kernel_intern.h"
@@ -25,12 +27,41 @@
 extern struct Task *sysIdleTask;
 uint32_t __arm_affinitymask __attribute__((section(".data"))) = 1;
 
+asm(
+"       .globl mpcore_trampoline                \n"
+"       .type mpcore_trampoline,%function       \n"
+"mpcore_trampoline:                             \n"
+"               ldr     r3, mpcore_pde          \n"
+"               mcr     p15, 0, r3, c2, c0, 0   \n"
+"               mov     r3, #0                  \n"
+"               mcr     p15, 0, r3, c2, c0, 2   \n"
+"               mov     r3, #1                  \n"
+"               mcr     p15, 0, r3, c3, c0, 0   \n"
+"               mrc     p15, 0, r4, c1, c0, 0   \n"
+"               mov     r3, #0                  \n"
+"               mcr     p15, 0, r3, c7, c10, 4  \n"
+"               orr     r4, r4, #0x800000       \n"
+"               orr     r4, r4, #1              \n"
+"               mcr     p15, 0, r4, c1, c0, 0   \n"
+"               mcr     p15, 0, r3, c7, c5, 4   \n"
+"               ldr     sp, mpcore_data         \n"
+"               ldr     pc, mpcore_code         \n"
+
+"mpcore_pde:    .word   0                       \n"
+"mpcore_code:   .word   0                       \n"
+"mpcore_data:   .word   0                       \n"
+"mpcore_end:  "
+);
+
+extern mpcore_trampoline();
+extern uint32_t mpcore_end;
+extern uint32_t mpcore_pde;
+
 void cpu_Register()
 {
     uint32_t tmp;
 
     asm volatile ("mrc p15, 0, %0, c1, c0, 0" : "=r"(tmp));
-    tmp &= ~1;                                  /* Disable MMU */
     tmp |= (1 << 2) | (1 << 12) | (1 << 11);    /* I and D caches, branch prediction */
     tmp = (tmp & ~2) | (1 << 22);               /* Unaligned access enable */
     asm volatile ("mcr p15, 0, %0, c1, c0, 0" : : "r"(tmp));
@@ -41,7 +72,9 @@ void cpu_Register()
 
     __arm_affinitymask |= (1 << (tmp & 0x3));
 
-    asm volatile("wfi");
+    bug("[KRN] Core %d up and waiting for interrupts\n", tmp & 0x3);
+
+    for (;;) asm volatile("wfi");
 }
 
 void cpu_Delay(int usecs)
@@ -49,6 +82,10 @@ void cpu_Delay(int usecs)
     unsigned int delay;
     for (delay = 0; delay < usecs; delay++) asm volatile ("mov r0, r0\n");
 }
+
+void arm_flush_cache(uint32_t addr, uint32_t length);
+
+uint32_t tmp_stacks_smp[4*1024];
 
 void cpu_Probe(struct ARM_Implementation *krnARMImpl)
 {
@@ -59,12 +96,47 @@ void cpu_Probe(struct ARM_Implementation *krnARMImpl)
     {
         krnARMImpl->ARMI_Family = 7;
 
+        if (krnARMImpl->ARMI_Delay)
+        {
         // Read the Multiprocessor Affinity Register (MPIDR)
         asm volatile ("mrc p15, 0, %0, c0, c0, 5" : "=r" (tmp));
 
         if (tmp & (2 << 30))
         {
-            //Multicore system
+            void *trampoline_src = mpcore_trampoline;
+            void *trampoline_dst = (void *)0x2000;
+            uint32_t trampoline_length = (uintptr_t)&mpcore_end - (uintptr_t)mpcore_trampoline;
+            uint32_t trampoline_data_offset = (uintptr_t)&mpcore_pde - (uintptr_t)mpcore_trampoline;
+
+            bug("[KRN] Multicore system\n");
+
+            bug("[KRN] Copy SMP trampoline from %p to %p (%d bytes)\n", trampoline_src, trampoline_dst, trampoline_length);
+            bcopy(trampoline_src, trampoline_dst, trampoline_length);
+
+            bug("[KRN] Patching data for trampoline at offset %d\n", trampoline_data_offset);
+            asm volatile ("mrc p15, 0, %0, c2, c0, 0":"=r"(tmp));
+            ((uint32_t *)(trampoline_dst + trampoline_data_offset))[0] = tmp; // pde
+            ((uint32_t *)(trampoline_dst + trampoline_data_offset))[1] = (uint32_t)cpu_Register;
+
+            bug("[KRN] Waking up cores\n");
+
+            ((uint32_t *)(trampoline_dst + trampoline_data_offset))[2] = &tmp_stacks_smp[4*1024-16];
+            arm_flush_cache((uint32_t)trampoline_dst, 512);
+            *((uint32_t *)(0x4000008c + 0x10)) = trampoline_dst;
+            cpu_Delay(10000000);
+
+            ((uint32_t *)(trampoline_dst + trampoline_data_offset))[2] = &tmp_stacks_smp[3*1024-16];
+            arm_flush_cache((uint32_t)trampoline_dst, 512);
+            *((uint32_t *)(0x4000008c + 0x20)) = trampoline_dst;
+
+            cpu_Delay(10000000);
+
+            ((uint32_t *)(trampoline_dst + trampoline_data_offset))[2] = &tmp_stacks_smp[2*1024-16];
+            arm_flush_cache((uint32_t)trampoline_dst, 512);
+            *((uint32_t *)(0x4000008c + 0x30)) = trampoline_dst;
+            cpu_Delay(10000000);
+
+        }
         }
     }
     else
