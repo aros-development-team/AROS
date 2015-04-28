f15c73b91d68eb5b592737e97d7a19e9966eb958
Kalamatee <kalamatee@gmail.com>
2015-04-28 00:06:04 +0000

    add the arm version of getcpumask
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50472 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/arm-native/kernel/getcpumask.c
@@ -0,0 +1,22 @@
+/*
+    Copyright © 2015, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc:
+*/
+
+#include <aros/kernel.h>
+#include <aros/libcall.h>
+
+#include <kernel_base.h>
+
+AROS_LH1(uint32_t, KrnGetCPUMask,
+	AROS_LHA(uint32_t, id, D0),
+	struct KernelBase *, KernelBase, 38, Kernel)
+{
+    AROS_LIBFUNC_INIT
+
+    return (1 << id);
+
+    AROS_LIBFUNC_EXIT
+}
