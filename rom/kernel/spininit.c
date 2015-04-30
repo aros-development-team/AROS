af17c61a1649eb44bc6b0c39617058df1815e474
Kalamatee <kalamatee@gmail.com>
2015-04-30 01:54:12 +0000

    placeholder
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50515 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/kernel/spininit.c
@@ -0,0 +1,50 @@
+/*
+    Copyright © 2015, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc:
+*/
+
+#include <aros/kernel.h>
+#include <aros/libcall.h>
+
+#include <kernel_base.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/kernel.h>
+
+        AROS_LH1(void, KrnSpinInit,
+
+/*  SYNOPSIS */
+	AROS_LHA(spinlock_t *, lock, A0),
+
+/*  LOCATION */
+	struct KernelBase *, KernelBase, 38, Kernel)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+******************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    /* The implementation of this function is architecture-specific */
+    return;
+
+    AROS_LIBFUNC_EXIT
+}
