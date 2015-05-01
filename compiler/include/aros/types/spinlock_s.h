7d7461356ea6690dfde2841e6c263e157933ea1f
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2015-05-01 12:44:51 +0000

    add default initializers for spinlocks
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50548 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/aros/types/spinlock_s.h
+++ compiler/include/aros/types/spinlock_s.h
@@ -7,6 +7,10 @@ typedef struct {
     volatile unsigned long lock;
 } spinlock_t;
 
+#define SPINLOCK_INIT_UNLOCKED  { 0 }
+#define SPINLOCK_INIT_WRITE_LOCKED  { 0x80000000 }
+#define SPINLOCK_INIT_READ_LOCKED(n) { n }
+
 #define SPINLOCK_MODE_READ  0
 #define SPINLOCK_MODE_WRITE 1
 
