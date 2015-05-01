4a5f16479d45b7a3b39db5d7cb4c41787baf13ec
Kalamatee <kalamatee@gmail.com>
2015-05-01 13:51:00 +0000

    wip commit
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50556 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/task/execfuncs.c
@@ -0,0 +1,115 @@
+/*
+    Copyright © 2015, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#define DEBUG 0
+
+#include <aros/debug.h>
+#include <aros/symbolsets.h>
+
+#include <proto/exec.h>
+#include <proto/kernel.h>
+
+#include "taskres_intern.h"
+
+extern struct TaskResBase *internTaskResBase;
+
+void TaskResAddTask(struct Task *task)
+{
+    struct TaskListEntry *newEntry;
+
+    if ((newEntry = AllocMem(sizeof(struct TaskListEntry), MEMF_CLEAR)) != NULL)
+    {
+        newEntry->tle_Task = task;
+        AddTail(&internTaskResBase->trb_TaskList, &newEntry->tle_Node);
+    }
+}
+
+AROS_LH3(APTR, AddTask,
+	AROS_LHA(struct Task *,     task,      A1),
+	AROS_LHA(APTR,              initialPC, A2),
+	AROS_LHA(APTR,              finalPC,   A3),
+	struct ExecBase *, SysBase, 47, Task)
+{
+    AROS_LIBFUNC_INIT
+
+    APTR newTask;
+
+    D(bug("[TaskRes] AddTask()\n"));
+
+    newTask = AROS_CALL3(APTR, internTaskResBase->trb_AddTask,
+                AROS_LCA(struct Task *,     task,      A1),
+                AROS_LCA(APTR,              initialPC, A2),
+                AROS_LCA(APTR,              finalPC,   A3),
+		struct ExecBase *, SysBase);
+
+    if (newTask)
+        TaskResAddTask(newTask);
+        
+    return newTask;
+
+    AROS_LIBFUNC_EXIT
+}
+
+AROS_LH4(APTR, NewAddTask,
+        AROS_LHA(struct Task *,     task,      A1),
+        AROS_LHA(APTR,              initialPC, A2),
+        AROS_LHA(APTR,              finalPC,   A3),
+        AROS_LHA(struct TagItem *,  tagList,   A4),
+        struct ExecBase *, SysBase, 176, Task)
+{
+    AROS_LIBFUNC_INIT
+
+    APTR newTask;
+
+    D(bug("[TaskRes] NewAddTask()\n"));
+
+    newTask = AROS_CALL4(APTR, internTaskResBase->trb_NewAddTask,
+                AROS_LCA(struct Task *,     task,      A1),
+                AROS_LCA(APTR,              initialPC, A2),
+                AROS_LCA(APTR,              finalPC,   A3),
+                AROS_LCA(struct TagItem *,  tagList,   A4),
+		struct ExecBase *, SysBase);
+
+    if (newTask)
+        TaskResAddTask(newTask);
+
+    return newTask;
+
+    AROS_LIBFUNC_EXIT
+}
+
+AROS_LH1(void, RemTask,
+        AROS_LHA(struct Task *, task, A1),
+        struct ExecBase *, SysBase, 48, Task)
+{
+    AROS_LIBFUNC_INIT
+
+    struct TaskListEntry *tmpEntry;
+
+    D(bug("[TaskRes] RemTask()\n"));
+
+    AROS_CALL1(APTR, internTaskResBase->trb_RemTask,
+                AROS_LCA(struct Task *,     task,      A1),
+		struct ExecBase *, SysBase);
+
+    ForeachNode(&internTaskResBase->trb_TaskList, tmpEntry)
+    {
+        if (tmpEntry->tle_Task == task)
+        {
+            /*
+               TODO:
+                if the list is locked flag the entry to be removed,
+                else remove it immediately
+            */
+            Remove(&tmpEntry->tle_Node);
+            FreeMem(tmpEntry, sizeof(struct TaskListEntry));
+            break;
+        }
+    }
+
+    return;
+
+    AROS_LIBFUNC_EXIT
+}
--- rom/task/init.c
+++ rom/task/init.c
@@ -11,11 +11,51 @@
 
 #include "taskres_intern.h"
 
+extern APTR AROS_SLIB_ENTRY(AddTask, Task, 47);
+extern APTR AROS_SLIB_ENTRY(NewAddTask, Task, 176);
+extern APTR AROS_SLIB_ENTRY(RemTask, Task, 48);
+
+struct TaskResBase *internTaskResBase = NULL;
+
 static LONG taskres_Init(struct TaskResBase *TaskResBase)
 {
+    struct TaskListEntry *taskEntry = NULL;
+    struct Task *curTask = NULL;
+
     KernelBase = OpenResource("kernel.resource");
     if (!KernelBase)
     	return FALSE;
+
+    internTaskResBase = TaskResBase;
+
+    NEWLIST(&TaskResBase->trb_TaskList);
+    NEWLIST(&TaskResBase->trb_LockedLists);
+
+    TaskResBase->trb_AddTask = SetFunction((struct Library *)SysBase, -47*LIB_VECTSIZE, AROS_SLIB_ENTRY(AddTask, Task, 47));
+    TaskResBase->trb_NewAddTask = SetFunction((struct Library *)SysBase, -176*LIB_VECTSIZE, AROS_SLIB_ENTRY(NewAddTask, Task, 176));
+    TaskResBase->trb_RemTask = SetFunction((struct Library *)SysBase, -48*LIB_VECTSIZE, AROS_SLIB_ENTRY(RemTask, Task, 48));
+
+    /*
+       Add existing tasks to our internal list ..
+       TODO: get the running tasks from all cores ..
+    */
+
+    ForeachNode(&SysBase->TaskReady, curTask)
+    {
+        if ((taskEntry = AllocMem(sizeof(struct TaskListEntry), MEMF_CLEAR)) != NULL)
+        {
+            taskEntry->tle_Task = curTask;
+            AddTail(&TaskResBase->trb_TaskList, &taskEntry->tle_Node);
+        }
+    }
+    ForeachNode(&SysBase->TaskWait, curTask)
+    {
+        if ((taskEntry = AllocMem(sizeof(struct TaskListEntry), MEMF_CLEAR)) != NULL)
+        {
+            taskEntry->tle_Task = curTask;
+            AddTail(&TaskResBase->trb_TaskList, &taskEntry->tle_Node);
+        }
+    }
 
     return TRUE;
 }
