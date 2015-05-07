988593307f6723c02588876e5f672240a0a325ab
Kalamatee <kalamatee@gmail.com>
2015-05-07 00:20:17 +0000

    add defines for ipi types, and stub out handler
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50606 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/arm-native/kernel/kernel_ipi.h
@@ -0,0 +1,22 @@
+#ifndef KERNEL_IPI_H_
+#define KERNEL_IPI_H_
+/*
+    Copyright © 2015, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+/*
+ * List of all possible (private) kernal IPI message types.
+ */
+
+#define IPI_CAUSE               0x000
+#define IPI_DISPATCH            0x001
+#define IPI_SWITCH              0x002
+#define IPI_SCHEDULE            0x003
+#define IPI_CLI                 0x004
+#define IPI_STI                 0x005
+#define IPI_REBOOT	        0x100
+#define IPI_ADDTASK	        0x101
+#define IPI_REMTASK	        0x102
+
+#endif
