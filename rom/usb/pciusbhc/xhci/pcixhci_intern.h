5a6fc7cc72d6e0f9019384894c9fd29a6270ffc9
dizzyofcrn <dizzyofcrn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2015-05-09 06:58:35 +0000

    Define *erstbl as volatile struct xhci_erstbl *erstbl
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50632 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/usb/pciusbhc/xhci/pcixhci_intern.h
+++ rom/usb/pciusbhc/xhci/pcixhci_intern.h
@@ -95,7 +95,7 @@ struct PCIXHCIHostController {
     UQUAD                        *dcbaa;     /* Device Context Base Address Array */
     UQUAD                        *spbaba;    /* Scratch Pad Buffer Address Base Array */
 
-    IPTR                         *erstbl;
+    volatile struct xhci_erste   *erstbl;
 
     char                          intname[256];
     struct Interrupt              inthandler;
