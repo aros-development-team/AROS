c7fb5f8a814c2bc4d014bfbdebc71dbe1aaf0296
Kalamatee <kalamatee@gmail.com>
2015-05-13 02:08:17 +0000

    clean up comments/fix typos
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50660 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-native/kernel/spinlock.c
+++ arch/arm-native/kernel/spinlock.c
@@ -27,12 +27,12 @@ AROS_LH3(spinlock_t *, KrnSpinLock,
     if (mode == SPINLOCK_MODE_WRITE)
     {
         asm volatile(
-                "1:     ldrex   %0, [%1]        \n\t"   // Load the lock value and gain exclusive lock to memory
-                "       teq     %0, #0          \n\t"   // is the value 0? It means the lock was free
-                "       wfene                   \n\t"   // Wait for Event if lock was not free
-                "       strexeq %0, %2, [%1]    \n\t"   // Store value to memory and check if it succeeded
-                "       teq     %0, #0          \n\t"   // Test if write succeeded
-                "       bne     1b              \n\t"   // Try again if locking failed
+                "1:     ldrex   %0, [%1]        \n\t"   // Load the lock value, gaining exclusive access
+                "       teq     %0, #0          \n\t"   // Is the lock free?
+                "       wfene                   \n\t"   // Wait for Event if allready obtained
+                "       strexeq %0, %2, [%1]    \n\t"   // Try to exclusively write the lock value to memory
+                "       teq     %0, #0          \n\t"   // Did it succeeded?
+                "       bne     1b              \n\t"   // If we failed, try to obtain the lock again
                 : "=&r"(lock_value)
                 : "r" (&lock->lock), "r"(0x80000000)
                 : "cc"
@@ -41,14 +41,14 @@ AROS_LH3(spinlock_t *, KrnSpinLock,
     else
     {
         asm volatile(
-                "1:     ldrex   %0, [%2]        \n\t"   // Load lock value and gain exclusive lock to memory
+                "1:     ldrex   %0, [%2]        \n\t"   // Load the lock value, gaining exclusive access
                 "       adds    %0, %0, #1      \n\t"   // Increase the lock value and update conditional bits
-                "       wfemi                   \n\t"   // Wait for event if lock value was negative
-                "       strexpl %1, %0, [%2]    \n\t"   // Store value to memory if positive and check result
-                "       rsbpls  %0, %1, #0      \n\t"   // Reverse substract and update conditionals: if strex write was
-                                                        // successful, %0 contains 0. #0 - %0 clears N flag. If write failed
-                                                        // %0 contains 1. #0 - %0 sets N flag (value 0xffffffff).
-                "       bmi     1b              \n\t"   // Try again if N flag is set (because of locok value or write failure
+                "       wfemi                   \n\t"   // Wait for event if lock value is negative
+                "       strexpl %1, %0, [%2]    \n\t"   // Try to exclusively write the lock value to memory if positive
+                "       rsbpls  %0, %1, #0      \n\t"   // Reverse substract and update conditionals:
+                                                        // - if strex write was successful, %0 contains 0. #0 - %0 clears N flag.
+                                                        // - If write failed %0 contains 1. #0 - %0 sets N flag (value 0xffffffff).
+                "       bmi     1b              \n\t"   // Try again if N flag is set (because of lock value, or write failure)
                 : "=&r"(lock_value), "=&r"(result)
                 : "r"(&lock->lock)
                 : "cc"
