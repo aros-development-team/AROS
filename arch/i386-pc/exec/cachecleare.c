c130ac8783e301e43f9847ef5e1344093ce3bc49
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2015-05-17 23:07:45 +0000

    Restored the inappropriately deleted x86 implementation of CacheClearE(). The code is unchanged, but the comments have been updated (removed the duplicate AutoDoc, but kept and clarified the 'Internals' section).
    The associated Exec_Wbinvd() assembly function is also restored (by 
    resurrecting part of the old corelow.S as cache.S).
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50701 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/i386-pc/exec/cachecleare.c
@@ -0,0 +1,55 @@
+/*
+    Copyright © 1995-2015, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: CacheClearE() - Clear the caches with extended control.
+    Lang: english
+*/
+
+#include <exec/types.h>
+#include <exec/execbase.h>
+#include <aros/libcall.h>
+
+void Exec_Wbinvd();
+
+#include <proto/exec.h>
+
+AROS_LH3(void, CacheClearE,
+    AROS_LHA(APTR, address, A0),
+    AROS_LHA(ULONG, length, D0),
+    AROS_LHA(ULONG, caches, D1),
+    struct ExecBase *, SysBase, 107, Exec)
+/*
+    INTERNALS
+        Although this function is not needed for BM-DMA on x86 due to 
+        strong cache coherency (the CPU snoops the address lines and 
+        invalidates all out-of-date cache), it is needed for some other 
+        operations. For example, when updating graphics memory address 
+        translation tables, changes may be invisible to the graphics 
+        card/chip if not explicitly written back from the cache.
+
+        Drivers performing DMA operations should use 
+        CachePreDMA()/CachePostDMA() instead, which maximise performance 
+        on x86 by doing nothing!
+*/
+{
+    AROS_LIBFUNC_INIT
+
+    /* A full flush has been requested */
+    if (length == 0xFFFFFFFF)
+    {
+        Supervisor((ULONG_FUNC)Exec_Wbinvd);
+        return;
+    }
+
+    /* A partial flush requested */
+    if (caches & CACRF_ClearD)
+    {
+        /* TODO: Detect if CPU supports clflush instruction and use it instead
+           of wbinvd to provide more optimized cache flushes */
+        Supervisor((ULONG_FUNC)Exec_Wbinvd);
+    }
+
+    AROS_LIBFUNC_EXIT
+} /* CacheClearE */
+
