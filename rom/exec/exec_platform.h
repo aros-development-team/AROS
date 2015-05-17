71fb0686851299d78b4720404e07df066dbee727
Kalamatee <kalamatee@gmail.com>
2015-05-17 01:41:14 +0000

    add tdnestcnt overrides
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50685 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/exec_platform.h
+++ rom/exec/exec_platform.h
@@ -8,13 +8,14 @@ struct Exec_PlatformData
 {
     /* No platform-specific data by default */
 };
-
+#define TDNESTCOUNT_GET                 (SysBase->TDNestCnt)
+#define TDNESTCOUNT_SET(val)            (SysBase->TDNestCnt=(x))
 #define FLAG_SCHEDSWITCH_CLEAR          SysBase->AttnResched &= ~ARF_AttnSwitch
 #define FLAG_SCHEDSWITCH_SET            SysBase->AttnResched |= ARF_AttnSwitch
-#define FLAG_SCHEDSWITCH_ISSET            (SysBase->AttnResched & ARF_AttnSwitch)
+#define FLAG_SCHEDSWITCH_ISSET          (SysBase->AttnResched & ARF_AttnSwitch)
 #define FLAG_SCHEDDISPATCH_CLEAR        SysBase->AttnResched &= ~ARF_AttnDispatch
 #define FLAG_SCHEDDISPATCH_SET          SysBase->AttnResched |= ARF_AttnDispatch
-#define FLAG_SCHEDDISPATCH_ISSET          (SysBase->AttnResched & ARF_AttnDispatch)
+#define FLAG_SCHEDDISPATCH_ISSET        (SysBase->AttnResched & ARF_AttnDispatch)
 
 #define GET_THIS_TASK                   (SysBase->ThisTask)
 #define SET_THIS_TASK(x)                (SysBase->ThisTask=(x))
