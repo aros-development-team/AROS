a6c3ed3244482fdaa6ba4cf9ad6ab91d41b8978b
Kalamatee <kalamatee@gmail.com>
2015-05-17 03:19:07 +0000

    provide overrides for scheduling flags, tdnestcnt and idnestcnt
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50690 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-unix/exec/exec_platform.h
+++ arch/all-unix/exec/exec_platform.h
@@ -43,7 +43,38 @@ struct Exec_PlatformData
 
 #define HostLibBase PD(SysBase).HostLibBase
 
-#define GET_THIS_TASK           (SysBase->ThisTask)
-#define SET_THIS_TASK(x)        (SysBase->ThisTask=(x))
+#ifdef AROS_NO_ATOMIC_OPERATIONS
+#define IDNESTCOUNT_INC                 SysBase->IDNestCnt++
+#define IDNESTCOUNT_DEC                 SysBase->IDNestCnt--
+#define TDNESTCOUNT_INC                 SysBase->TDNestCnt++
+#define TDNESTCOUNT_DEC                 SysBase->TDNestCnt--
+#define FLAG_SCHEDQUANTUM_CLEAR         SysBase->SysFlags &= ~SFF_QuantumOver
+#define FLAG_SCHEDQUANTUM_SET           SysBase->SysFlags |= SFF_QuantumOver
+#define FLAG_SCHEDSWITCH_CLEAR          SysBase->AttnResched &= ~ARF_AttnSwitch
+#define FLAG_SCHEDSWITCH_SET            SysBase->AttnResched |= ARF_AttnSwitch
+#define FLAG_SCHEDDISPATCH_CLEAR        SysBase->AttnResched &= ~ARF_AttnDispatch
+#define FLAG_SCHEDDISPATCH_SET          SysBase->AttnResched |= ARF_AttnDispatch
+#else
+#define IDNESTCOUNT_INC                 AROS_ATOMIC_INC(SysBase->IDNestCnt)
+#define IDNESTCOUNT_DEC                 AROS_ATOMIC_DEC(SysBase->IDNestCnt)
+#define TDNESTCOUNT_INC                 AROS_ATOMIC_INC(SysBase->TDNestCnt)
+#define TDNESTCOUNT_DEC                 AROS_ATOMIC_DEC(SysBase->TDNestCnt)
+#define FLAG_SCHEDQUANTUM_CLEAR         AROS_ATOMIC_AND(SysBase->SysFlags, ~SFF_QuantumOver)
+#define FLAG_SCHEDQUANTUM_SET           AROS_ATOMIC_OR(SysBase->SysFlags, SFF_QuantumOver)
+#define FLAG_SCHEDSWITCH_CLEAR          AROS_ATOMIC_AND(SysBase->AttnResched, ~ARF_AttnSwitch)
+#define FLAG_SCHEDSWITCH_SET            AROS_ATOMIC_OR(SysBase->AttnResched, ARF_AttnSwitch)
+#define FLAG_SCHEDDISPATCH_CLEAR        AROS_ATOMIC_AND(SysBase->AttnResched, ~ARF_AttnDispatch)
+#define FLAG_SCHEDDISPATCH_SET          AROS_ATOMIC_OR(SysBase->AttnResched, ARF_AttnDispatch)
+#endif
+#define IDNESTCOUNT_GET                 (SysBase->IDNestCnt)
+#define IDNESTCOUNT_SET(val)            (SysBase->IDNestCnt=(x))
+#define TDNESTCOUNT_GET                 (SysBase->TDNestCnt)
+#define TDNESTCOUNT_SET(val)            (SysBase->TDNestCnt=(x))
+#define FLAG_SCHEDQUANTUM_ISSET         (SysBase->SysFlags & SFF_QuantumOver)
+#define FLAG_SCHEDSWITCH_ISSET          (SysBase->AttnResched & ARF_AttnSwitch)
+#define FLAG_SCHEDDISPATCH_ISSET        (SysBase->AttnResched & ARF_AttnDispatch)
+
+#define GET_THIS_TASK                   (SysBase->ThisTask)
+#define SET_THIS_TASK(x)                (SysBase->ThisTask=(x))
 
 #endif /* __EXEC_PLATFORM_H */
