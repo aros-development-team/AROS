c77f883a4fac8b7be523a62b28976466cd30b4e7
Kalamatee <kalamatee@gmail.com>
2015-05-17 21:55:42 +0000

    ipi reshuffle. add a "NOP" IPI.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50700 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-native/kernel/kernel_ipi.h
+++ arch/arm-native/kernel/kernel_ipi.h
@@ -9,14 +9,14 @@
  * List of all possible (private) kernal IPI message types.
  */
 
-#define IPI_CAUSE               0x000
-#define IPI_DISPATCH            0x001
-#define IPI_SWITCH              0x002
-#define IPI_SCHEDULE            0x003
-#define IPI_CLI                 0x004
-#define IPI_STI                 0x005
-#define IPI_REBOOT	        0x100
+#define IPI_NOP                 0x000
+#define IPI_CAUSE               0x001
+#define IPI_DISPATCH            0x002
+#define IPI_SWITCH              0x003
+#define IPI_SCHEDULE            0x004
+#define IPI_CLI                 0x005
+#define IPI_STI                 0x006
 #define IPI_ADDTASK	        0x101
 #define IPI_REMTASK	        0x102
-
+#define IPI_REBOOT	        0x10F
 #endif
