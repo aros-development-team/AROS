241864df5d4c843b2bc24b05e0363b7989c9afc1
Kalamatee <kalamatee@gmail.com>
2015-05-17 01:27:46 +0000

    add overrides for the scheduling flags
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50683 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/exec_platform.h
+++ rom/exec/exec_platform.h
@@ -9,7 +9,12 @@ struct Exec_PlatformData
     /* No platform-specific data by default */
 };
 
-#define GET_THIS_TASK           (SysBase->ThisTask)
-#define SET_THIS_TASK(x)        (SysBase->ThisTask=(x))
+#define FLAG_SCHEDSWITCH_CLEAR          SysBase->AttnResched &= ~ARF_AttnSwitch
+#define FLAG_SCHEDSWITCH_SET            SysBase->AttnResched |= ARF_AttnSwitch
+#define FLAG_SCHEDDISPATCH_CLEAR        SysBase->AttnResched &= ~ARF_AttnDispatch
+#define FLAG_SCHEDDISPATCH_SET          SysBase->AttnResched |= ARF_AttnDispatch
+
+#define GET_THIS_TASK                   (SysBase->ThisTask)
+#define SET_THIS_TASK(x)                (SysBase->ThisTask=(x))
 
 #endif /* __EXEC_PLATFORM_H */
