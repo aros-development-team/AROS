eec50423c26ad940f234871f94c4d0e8fc93769e
Kalamatee <kalamatee@gmail.com>
2015-05-17 01:30:55 +0000

    add tests for the scheduling flags
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50684 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/exec_platform.h
+++ rom/exec/exec_platform.h
@@ -11,8 +11,10 @@ struct Exec_PlatformData
 
 #define FLAG_SCHEDSWITCH_CLEAR          SysBase->AttnResched &= ~ARF_AttnSwitch
 #define FLAG_SCHEDSWITCH_SET            SysBase->AttnResched |= ARF_AttnSwitch
+#define FLAG_SCHEDSWITCH_ISSET            (SysBase->AttnResched & ARF_AttnSwitch)
 #define FLAG_SCHEDDISPATCH_CLEAR        SysBase->AttnResched &= ~ARF_AttnDispatch
 #define FLAG_SCHEDDISPATCH_SET          SysBase->AttnResched |= ARF_AttnDispatch
+#define FLAG_SCHEDDISPATCH_ISSET          (SysBase->AttnResched & ARF_AttnDispatch)
 
 #define GET_THIS_TASK                   (SysBase->ThisTask)
 #define SET_THIS_TASK(x)                (SysBase->ThisTask=(x))
