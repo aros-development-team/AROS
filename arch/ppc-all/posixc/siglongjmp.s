164befc5d5e09a2b3ad90d3ece8ce7803ad7060f
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2015-05-19 22:53:54 +0000

    Moved duplicate AutoDocs to central location.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50719 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/ppc-all/posixc/siglongjmp.s
+++ arch/ppc-all/posixc/siglongjmp.s
@@ -2,63 +2,9 @@
     Copyright © 2015, The AROS Development Team. All rights reserved.
     $Id$
 
-    Desc: ANSI C function siglongjmp()
+    Desc: POSIX.1-2008 function siglongjmp()
     Lang: english
 */
-
-/******************************************************************************
-
-    NAME
-#include <setjmp.h>
-
-	void siglongjmp (jmp_buf env, int val);
-
-    FUNCTION
-	Save the current context so that you can return to it later.
-
-    INPUTS
-	env - The context/environment to restore
-	val - This value is returned by setjmp() when you return to the
-		saved context. You cannot return 0. If val is 0, then
-		setjmp() returns with 1.
-
-    RESULT
-	This function doesn't return.
-
-    NOTES
-
-    EXAMPLE
-	jmp_buf env;
-
-	... some code ...
-
-	if (!setjmp (env))
-	{
-	    ... this code is executed after setjmp() returns ...
-
-	    // This is no good example on how to use this function
-	    // You should not do that
-	    if (error)
-		siglongjmp (env, 5);
-
-	    ... some code ...
-	}
-	else
-	{
-	    ... this code is executed if you call siglongjmp(env) ...
-	}
-
-    BUGS
-
-    SEE ALSO
-	setjmp()
-
-    INTERNALS
-
-    HISTORY
-	12.10.1999 Code rewritten according to glibc
-
-******************************************************************************/
 
 #include "aros/ppc/asm.h"
 
--- compiler/posixc/siglongjmp.c
+++ compiler/posixc/siglongjmp.c
@@ -1,1 +1,60 @@
+/*
+    Copyright © 2015, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: POSIX.1-2008 function siglongjmp()
+    Lang: english
+*/
+
+/******************************************************************************
+
+    NAME
+#include <setjmp.h>
+
+	void siglongjmp (jmp_buf env, int val);
+
+    FUNCTION
+	Save the current context so that you can return to it later.
+
+    INPUTS
+	env - The context/environment to restore
+	val - This value is returned by setjmp() when you return to the
+		saved context. You cannot return 0. If val is 0, then
+		setjmp() returns with 1.
+
+    RESULT
+	This function doesn't return.
+
+    NOTES
+
+    EXAMPLE
+	jmp_buf env;
+
+	... some code ...
+
+	if (!setjmp (env))
+	{
+	    ... this code is executed after setjmp() returns ...
+
+	    // This is no good example on how to use this function
+	    // You should not do that
+	    if (error)
+		siglongjmp (env, 5);
+
+	    ... some code ...
+	}
+	else
+	{
+	    ... this code is executed if you call siglongjmp(env) ...
+	}
+
+    BUGS
+
+    SEE ALSO
+	setjmp()
+
+    INTERNALS
+
+******************************************************************************/
+
 #error siglongjmp has to be implemented for each cpu
