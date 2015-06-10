2176773215e189cfb37a110378f414dd2b95dee1
deadwood-pl <deadwood@wp.pl>
2015-06-10 20:42:56 +0000

    Listtree.mcc: implement MUIM_Listtree_Insert and MUIM_Listtree_GetEntry
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50798 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/classes/zune/listtree/listtree_private.h
+++ workbench/classes/zune/listtree/listtree_private.h
@@ -12,7 +12,6 @@
 struct Listtree_DATA
 {
     /*- Private ------------------------------------------------------------*/
-    struct MinList nodes;
     APTR pool;
     struct Hook *constrhook;
     struct Hook *destrhook;
