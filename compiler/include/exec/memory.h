f7bf89a2214f120b8d6d2765af1fbc005efc583a
Kalamatee <kalamatee@gmail.com>
2015-06-19 01:44:31 +0000

    add bit definitions for the memory flags. add MEMB/F_VIRTUAL / MEMB/F_PRIVATE
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50839 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/exec/memory.h
+++ compiler/include/exec/memory.h
@@ -58,21 +58,41 @@ struct MemList
 
 /* Memory allocation flags */
 #define MEMF_ANY           0L
-#define MEMF_PUBLIC        (1L<<0)
-#define MEMF_CHIP          (1L<<1)
-#define MEMF_FAST          (1L<<2)
-#define MEMF_EXECUTABLE    (1L<<4)  /* AmigaOS v4 compatible */
-#define MEMF_LOCAL         (1L<<8)
-#define MEMF_24BITDMA      (1L<<9)
-#define MEMF_KICK          (1L<<10)
-#define MEMF_31BIT         (1L<<12) /* Low address space (<2GB). Effective only on 64 bit machines. */
-#define MEMF_CLEAR         (1L<<16) /* Explicitly clear memory after allocation */
-#define MEMF_LARGEST       (1L<<17)
-#define MEMF_REVERSE       (1L<<18)
-#define MEMF_TOTAL         (1L<<19)
-#define MEMF_HWALIGNED     (1L<<20) /* For AllocMem() - align address and size to physical page boundary */
-#define MEMF_SEM_PROTECTED (1L<<20) /* For CreatePool() - add semaphore protection to the pool */
-#define MEMF_NO_EXPUNGE    (1L<<31)
+#define MEMB_PUBLIC        0
+#define MEMF_PUBLIC        (1L << MEMB_PUBLIC)
+#define MEMB_CHIP          1
+#define MEMF_CHIP          (1L << MEMB_CHIP)
+#define MEMB_FAST          2
+#define MEMF_FAST          (1L << MEMB_FAST)
+#define MEMB_VIRTUAL       3            /* AmigaOS v4 compatible */
+#define MEMF_VIRTUAL       (1L << MEMB_VIRTUAL)
+#define MEMB_EXECUTABLE    4            /* AmigaOS v4 compatible */
+#define MEMF_EXECUTABLE    (1L << MEMB_EXECUTABLE)
+#define MEMB_LOCAL         8
+#define MEMF_LOCAL         (1L << MEMB_LOCAL)
+#define MEMB_24BITDMA      9
+#define MEMF_24BITDMA      (1L << MEMB_24BITDMA)
+#define MEMB_KICK          10
+#define MEMF_KICK          (1L << MEMB_KICK)
+#define MEMB_PRIVATE       11
+#define MEMF_PRIVATE       (1L << MEMB_PRIVATE)
+#define MEMB_31BIT         12           /* Low address space (<2GB). Effective only on 64 bit machines. */
+#define MEMF_31BIT         (1L << MEMB_31BIT)
+
+#define MEMB_CLEAR         16           /* Explicitly clear memory after allocation */
+#define MEMF_CLEAR         (1L << MEMB_CLEAR)
+#define MEMB_LARGEST       17
+#define MEMF_LARGEST       (1L << MEMB_LARGEST)
+#define MEMB_REVERSE       18
+#define MEMF_REVERSE       (1L << MEMB_REVERSE)
+#define MEMB_TOTAL         19
+#define MEMF_TOTAL         (1L << MEMB_TOTAL)
+#define MEMB_HWALIGNED     20           /* For AllocMem() - align address and size to physical page boundary */
+#define MEMF_HWALIGNED     (1L << MEMB_HWALIGNED)
+#define MEMB_SEM_PROTECTED 20           /* For CreatePool() - add semaphore protection to the pool */
+#define MEMF_SEM_PROTECTED (1L << MEMB_SEM_PROTECTED)
+#define MEMB_NO_EXPUNGE    31
+#define MEMF_NO_EXPUNGE    (1L << MEMB_NO_EXPUNGE)
 
 /*
  * Mask for flags that describe physical properties of the memory.
