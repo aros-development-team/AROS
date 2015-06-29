f517ae37b884e49c4736a3ac593085c6892be8a8
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2015-06-29 16:19:35 +0000

    - Also examine the resized file to check it has the new size.  - Return AmigaDOS return codes.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50891 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/setfilesize.c
+++ test/setfilesize.c
@@ -1,42 +1,83 @@
 /*
-    Copyright © 1995-2014, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2015, The AROS Development Team. All rights reserved.
     $Id$
 */
 
 #include <exec/types.h>
-#include <dos/dosextens.h>
-#include <dos/bptr.h>
+
 #include <proto/exec.h>
 #include <proto/dos.h>
+
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
-    BPTR fh;
-    LONG size;
+int main(int argc, char **argv)
+{
+    BPTR fh = BNULL;
+    LONG result = RETURN_OK, error = 0, size;
+    struct FileInfoBlock *fib;
 
-    if (argc != 3) {
-        printf("usage: %s filename newsize\n", argv[0]);
-        return 1;
+    fib = AllocDosObject(DOS_FIB, NULL);
+    if (fib == NULL)
+    {
+        error = IoErr();
+        result = RETURN_FAIL;
     }
 
-    fh = Open(argv[1], MODE_READWRITE);
-    if (fh == BNULL) {
-        PrintFault(IoErr(), "SetFileSize");
-        return 0;
+    if (error == 0 && argc != 3)
+    {
+        printf("Usage: %s filename newsize\n", argv[0]);
+        error = ERROR_REQUIRED_ARG_MISSING;
+        result = RETURN_WARN;
     }
 
-    size = SetFileSize(fh, atol(argv[2]), OFFSET_BEGINNING);
-    if (size < 0) {
-        PrintFault(IoErr(), "SetFileSize");
+    if (error == 0)
+    {
+        fh = Open(argv[1], MODE_READWRITE);
+        if (fh == BNULL)
+        {
+            error = IoErr();
+            result = RETURN_ERROR;
+        }
+    }
+
+    if (error == 0)
+    {
+        size = atol(argv[2]);
+        if (SetFileSize(fh, atol(argv[2]), OFFSET_BEGINNING) != size)
+        {
+            error = IoErr();
+            result = RETURN_ERROR;
+        }
+    }
+
+    if (error == 0)
+    {
+        if (!ExamineFH(fh, fib))
+        {
+            error = IoErr();
+            result = RETURN_ERROR;
+        }
+    }
+
+    if (error == 0)
+    {
+        Printf("New size is %ld bytes\n", fib->fib_Size);
+        if (fib->fib_Size != size)
+        {
+            if (fib->fib_Size > size)
+                error = ERROR_OBJECT_TOO_LARGE;
+            else
+                error = ERROR_UNKNOWN;
+            result = RETURN_ERROR;
+        }
+    }
+
+    if (fib != NULL)
+        FreeDosObject(DOS_FIB, fib);
+    if (fh != BNULL)
         Close(fh);
-        return 0;
-    }
 
-    Printf("New size is %ld bytes\n", size);
-
-    Close(fh);
-
-    return 0;
+    PrintFault(error, "SetFileSize");
+    return result;
 }
