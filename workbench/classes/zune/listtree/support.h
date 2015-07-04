5bb7930090ad83a7e2dff55f34a552e2157caec5
deadwood-pl <deadwood@wp.pl>
2015-07-04 20:16:58 +0000

    Listtree.mcc: extract the internal version of NListtree to separate file
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50925 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/classes/zune/listtree/support.h
@@ -0,0 +1,9 @@
+/*
+    Copyright © 2015, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+
+extern struct MUI_CustomClass * CL_NListtreeInt;
+
+#define MUIA_NListtreeInt_Listtree              0xfec81401UL    /* .s. Object * */
