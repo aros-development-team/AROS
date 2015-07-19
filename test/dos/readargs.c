0f96e9bc3150b9cb845045231e489090a665f125
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2015-07-19 18:01:37 +0000

    Return a non-zero result if some tests failed.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@50967 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/dos/readargs.c
+++ test/dos/readargs.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011, The AROS Development Team.  All rights reserved.
+ * Copyright (C) 2011-2015, The AROS Development Team.  All rights reserved.
  * Author: Jason S. McMullan <jason.mcmullan@gmail.com>
  *
  * Licensed under the AROS PUBLIC LICENSE (APL) Version 1.1
@@ -397,5 +397,5 @@ int main(int argc, char **argv)
 
     Flush(Output());
 
-    return 0;
+    return tests_failed == 0 ? RETURN_OK : RETURN_WARN;
 }
