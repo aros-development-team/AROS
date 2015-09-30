b8638b8181e90dfce9f4fbfcf05f0aae7037f93c
Kalamatee <kalamatee@gmail.com>
2015-09-30 13:19:15 +0000

    add strnlen()
    git-svn-id: https://svn.aros.org/svn/aros/trunk@51121 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/stdc/strnlen.c
@@ -0,0 +1,50 @@
+/*
+    Copyright © 2015, The AROS Development Team. All rights reserved.
+    $Id$
+
+    C99 function strnlen().
+*/
+
+/*****************************************************************************
+
+    NAME */
+#include <string.h>
+
+	size_t strnlen (
+
+/*  SYNOPSIS */
+	const char * ptr, size_t n)
+
+/*  FUNCTION
+	Calculate the length of a string (without the terminating 0 byte).
+
+    INPUTS
+	ptr - The string to get its length for
+	n - The max length
+
+    RESULT
+	The length of the string.
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+******************************************************************************/
+{
+    const char * start = ptr;
+
+    while (*ptr && n)
+    {
+        ptr ++;
+        n--;
+    }
+
+    return (((long)ptr) - ((long)start));
+} /* strnlen */
+
