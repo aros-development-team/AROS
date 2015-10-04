43684b5db496ad1a0c53281f22bb385b8e5a2bf4
deadwood-pl <deadwood@wp.pl>
2015-10-04 17:23:32 +0000

    devs/AHI: skeleton of alsa.audio driver
    git-svn-id: https://svn.aros.org/svn/aros/trunk@51130 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/devs/AHI/Drivers/Alsa/DriverData.h
@@ -0,0 +1,34 @@
+#ifndef AHI_Drivers_Alsa_DriverData_h
+#define AHI_Drivers_Alsa_DriverData_h
+
+#include <exec/libraries.h>
+#include <dos/dos.h>
+#include <proto/dos.h>
+
+#include "DriverBase.h"
+
+struct AlsaBase
+{
+    struct DriverBase driverbase;
+    struct DosLibrary*   dosbase;
+};
+
+#define DRIVERBASE_SIZEOF (sizeof (struct AlsaBase))
+
+#define DOSBase (*(struct DosLibrary**) &AlsaBase->dosbase)
+
+struct AlsaData
+{
+    struct DriverData   driverdata;
+    UBYTE        flags;
+    UBYTE        pad1;
+    BYTE        mastersignal;
+    BYTE        slavesignal;
+    struct Process*    mastertask;
+    struct Process*    slavetask;
+    struct AlsaBase*    ahisubbase;
+    APTR        mixbuffer;
+};
+
+
+#endif /* AHI_Drivers_Alsa_DriverData_h */
---
+++ workbench/devs/AHI/Drivers/Alsa/alsa-playslave.c
@@ -0,0 +1,94 @@
+/*
+    Copyright © 2015, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <config.h>
+
+#include <devices/ahi.h>
+#include <exec/execbase.h>
+#include <libraries/ahi_sub.h>
+
+#include "DriverData.h"
+#include "library.h"
+
+#define dd ((struct AlsaData*) AudioCtrl->ahiac_DriverData)
+
+/******************************************************************************
+** The slave process **********************************************************
+******************************************************************************/
+
+#undef SysBase
+
+void Slave( struct ExecBase* SysBase );
+
+#include <aros/asmcall.h>
+
+AROS_UFH3(LONG, SlaveEntry,
+      AROS_UFHA(STRPTR, argPtr, A0),
+      AROS_UFHA(ULONG, argSize, D0),
+      AROS_UFHA(struct ExecBase *, SysBase, A6))
+{
+   AROS_USERFUNC_INIT
+   Slave( SysBase );
+   AROS_USERFUNC_EXIT
+}
+
+void
+Slave( struct ExecBase* SysBase )
+{
+  struct AHIAudioCtrlDrv* AudioCtrl;
+  struct DriverBase*      AHIsubBase;
+  struct AlsaBase*        AlsaBase;
+  BOOL                    running;
+  ULONG                   signals;
+
+  AudioCtrl  = (struct AHIAudioCtrlDrv*) FindTask(NULL)->tc_UserData;
+  AHIsubBase = (struct DriverBase*) dd->ahisubbase;
+  AlsaBase   = (struct AlsaBase*) AHIsubBase;
+
+  dd->slavesignal = AllocSignal( -1 );
+
+  if( dd->slavesignal != -1 )
+  {
+    // Everything set up. Tell Master we're alive and healthy.
+
+    Signal( (struct Task*) dd->mastertask,
+            1L << dd->mastersignal );
+
+    running = TRUE;
+
+    while( running )
+    {
+      signals = SetSignal(0L,0L);
+
+      if( signals & ( SIGBREAKF_CTRL_C | (1L << dd->slavesignal) ) )
+      {
+        running = FALSE;
+      }
+      else
+      {
+        CallHookPkt( AudioCtrl->ahiac_PlayerFunc, AudioCtrl, NULL );
+        CallHookPkt( AudioCtrl->ahiac_MixerFunc, AudioCtrl, dd->mixbuffer );
+
+        // TODO
+        // The mixing buffer is now filled with AudioCtrl->ahiac_BuffSamples
+        // of sample frames (type AudioCtrl->ahiac_BuffType). Send them
+        // to the sound card here.
+      }
+    }
+  }
+
+  FreeSignal( dd->slavesignal );
+  dd->slavesignal = -1;
+
+  Forbid();
+
+  // Tell the Master we're dying
+
+  Signal( (struct Task*) dd->mastertask, 1L << dd->mastersignal );
+
+  dd->slavetask = NULL;
+
+  // Multitaking will resume when we are dead.
+}
