e120f7f589219457edae10dd6c756b7b9f39f1f0
dizzyofcrn <dizzyofcrn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2015-10-14 15:18:49 +0000

    Insert new skeleton code for hosted usb support  - Will need a new hub class for hosted  - libusb hotplug code should reject hubs for the sake of simplicity  - Should provide isochronous transfer method out of the box  - Should provide support for USB3.0 controllers out of the box
    Theory of workings:
     - libusb hotplug code detects usb device insertion and rejects the device if it is of type hub
     - If there are free "ports" on Aros side then the device is claimed by Aros (one usb2.0 and one usb3.0 port for example), rest are handed out back to the system.
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@51146 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/usb/vusbhc/libusb-bridge/libusb.h
@@ -0,0 +1,12 @@
+/*
+    Copyright © 2015, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <exec/types.h>
+
+BOOL libusb_bridge_init();
+VOID libusb_bridge_cleanup();
+
+int call_libusb_init();
+
