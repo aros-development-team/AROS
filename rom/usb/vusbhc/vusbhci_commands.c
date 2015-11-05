f2bb3cd24812ddb8bf17dab1fcb79279931cbc71
dizzyofcrn <dizzyofcrn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2015-11-05 07:18:06 +0000

    Enable bulk transfer request to be sent to libusb Added note for the buffer usage
    git-svn-id: https://svn.aros.org/svn/aros/trunk@51199 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/usb/vusbhc/vusbhci_commands.c
+++ rom/usb/vusbhc/vusbhci_commands.c
@@ -928,7 +928,7 @@ WORD cmdControlXFer(struct IOUsbHWReq *ioreq) {
         return(cmdControlXFerRootHub(ioreq));
     }
 
-    mybug_unit(0, ("Sending transfer request to libusb\n\n"));
+    mybug_unit(-1, ("Sending transfer request to libusb\n\n"));
     return(do_libusb_ctrl_transfer(ioreq));
 }
 
@@ -960,8 +960,8 @@ WORD cmdIntXFer(struct IOUsbHWReq *ioreq) {
         return(cmdIntXFerRootHub(ioreq));
     }
 
-    //mybug_unit(0, ("Sending transfer request to libusb\n\n"));
-    //return(do_libusb_intr_transfer(ioreq));
+    mybug_unit(-1, ("Sending transfer request to libusb\n\n"));
+    return(do_libusb_intr_transfer(ioreq));
     return(RC_DONTREPLY);
 }
 
@@ -989,8 +989,8 @@ WORD cmdBulkXFer(struct IOUsbHWReq *ioreq) {
         return UHIOERR_USBOFFLINE;
     }
 
-    //mybug_unit(0, ("Sending transfer request to libusb\n\n"));
-    //return(do_libusb_bulk_transfer(ioreq));
+    mybug_unit(-1, ("Sending transfer request to libusb\n\n"));
+    return(do_libusb_bulk_transfer(ioreq));
     return(RC_DONTREPLY);
 }
 
