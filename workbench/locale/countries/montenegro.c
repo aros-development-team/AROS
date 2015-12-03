8e2110178629387782d6de2592be50f7669a7867
polluks <polluks@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2015-12-03 13:36:31 +0000

    Added Slovak and Slovene languages; Montenegro country and flag.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@51225 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/locale/countries/montenegro.c
+++ workbench/locale/countries/montenegro.c
@@ -1,40 +1,40 @@
 /*
-    Copyright © 1995-2013, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2015, The AROS Development Team. All rights reserved.
     $Id$
 
-    Desc: Country data for Hellas (Greece)
-    Author: George Sokianos <walkero@yahoo.com>
+    Desc: Country data for Crna Gora (Montenegro)
+    Author: Stefan Haubenthal <polluks@sdf.lonestar.org>
 */
 
 #include "country_locale.h"
 #include <libraries/locale.h>
 
-struct IntCountryPrefs greecePrefs =
+struct IntCountryPrefs croatiaPrefs =
 {
     {
         /* Reserved */
         { 0, 0, 0, 0 },
 
         /* Country code (licence plate number), telephone code, measuring system */
-        MAKE_ID('G','R',0,0), 30, MS_ISO,
+        MAKE_ID('M','N','E',0), 382, MS_ISO,
 
         /* Date time format, date format, time format */
-        "%d/%m/%Y %q:%M:%S",
-        "%A %e %B %Y",
-        "%q:%M:%S",
+        "%A, %e. %B %Y. %H:%M:%S",
+        "%A, %e. %B %Y.",
+        "%H:%M:%S",
 
         /* Short datetime, short date, short time formats */
-        "%H:%M:%S %d/%m/%Y",
-        "%e-%b-%Y",
+        "%d.%m.%y. %H:%M:%S",
+        "%d.%m.%y.",
         "%H:%M:%S",
 
         /* Decimal point, group separator, frac group separator */
-        ",", ".", "",
+        ",", ".", ".",
 
         /* For grouping rules, see <libraries/locale.h> */
 
         /* Grouping, Frac Grouping */
-        { 3 }, { 255 },
+        { 3 }, { 3 },
 
         /* Mon dec pt, mon group sep, mon frac group sep */
         ",", ".", ".",
@@ -42,37 +42,32 @@ struct IntCountryPrefs greecePrefs =
         /* Mon Grouping, Mon frac grouping */
         { 3 }, { 3 },
 
-#ifdef _EURO
         /* Mon Frac digits, Mon IntFrac digits, then number of digits in
            the fractional part of the money value. Most countries that
            use dollars and cents, would have 2 for this value
 
            (As would many of those you don't).
         */
-        2, 3,
+        2, 2,
 
         /* Currency symbol, Small currency symbol */
-        "Euro", "Lepto",
+        "Euro", "Cent",
 
         /* Int CS, this is the ISO 4217 symbol, followed by the character to
            separate that symbol from the rest of the money. (\x00 for none).
         */
         "EUR",
-#else
-        0, 3,
-        "drx", "d",
-        "GRD",
-#endif
+
         /* Mon +ve sign, +ve space sep, +ve sign pos, +ve cs pos */
-        "", SS_SPACE, SP_PREC_ALL, CSP_PRECEDES,
+        "", SS_SPACE, SP_PREC_ALL, CSP_SUCCEEDS,
 
         /* Mon -ve sign, -ve space sep, -ve sign pos, -ve cs pos */
-        "-", SS_SPACE, SP_SUCC_CURR, CSP_PRECEDES,
+        "-", SS_SPACE, SP_PREC_ALL, CSP_SUCCEEDS,
 
         /* Calendar type */
         CT_7MON
     },
-    "$VER: greece.country 44.1 (06.06.2015)",
-    "Hellas",
-    "Countries/Greece"
+    "$VER: montenegro.country 44.0 (03.12.2015)",
+    "Crna Gora",
+    "Countries/Montenegro"
 };
---
+++ workbench/locale/languages/slovak.c
@@ -0,0 +1,99 @@
+/*
+    Copyright © 1995-2015, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: slovak.language description file.
+    Credits: Jaroslav Pokorný & ATO-CZ
+*/
+
+#define LANGSTR     "slovak"            /* String version of above      */
+#define NLANGSTR    "slovenèina"        /* Native version of LANGSTR    */
+#define LANGVER     41                  /* Version number of language   */
+#define LANGREV     0                   /* Revision number of language  */
+#define LANGTAG     "\0$VER: " LANGSTR ".language 41.0 (09.07.2015)"
+#define NLANGTAG    "$NLANG:" NLANGSTR
+
+#include "lang_openclose.inc"
+#include "lang_getlangstring.inc"
+
+/* -----------------------------------------------------------------------
+   Library function table - you will need to alter this
+   I have this right here at the end of the library so that I do not
+   have to have prototypes for the functions. Although you could do that.
+   ----------------------------------------------------------------------- */
+
+void *const functable[] =
+{
+    &AROS_SLIB_ENTRY(open,language,1),
+    &AROS_SLIB_ENTRY(close,language,2),
+    &AROS_SLIB_ENTRY(expunge,language,3),
+    &AROS_SLIB_ENTRY(null,language,0),
+    &AROS_SLIB_ENTRY(mask,language,5),
+
+    /* Note, shorter function table, as only getlangstring is used */
+
+    /* 0 - 3 */
+    &AROS_SLIB_ENTRY(null, language, 0),
+    &AROS_SLIB_ENTRY(null, language, 0),
+    &AROS_SLIB_ENTRY(null, language, 0),
+    &AROS_SLIB_ENTRY(getlangstring, language, 9),
+    (void *)-1
+};
+
+/*
+    Note how only the required data structures are kept...
+
+    This is the list of strings. It is an array of pointers to strings,
+    although how it is laid out is implementation dependant.
+*/
+const STRPTR ___strings[] =
+{
+    /* A blank string */
+    "",
+
+    /*  The days of the week. Starts with the first day of the week.
+	In English this would be Sunday, this depends upon the settings
+	of Locale->CalendarType.
+    */
+    "nedeãa", "pondelok", "utorok", "streda", "îtvrtok",
+    "piatok", "sobota",
+
+    /* Abbreviated days of the week */
+    "Ne", "Po", "Ut", "St", "Ît", "Pi", "So",
+
+    /* Months of the year */
+    "január", "február", "marec",
+    "apríl", "máj", "jún",
+    "júl", "august", "september",
+    "október", "november", "december",
+
+    /* Abbreviated months of the year */
+    "jan", "feb", "mar", "apr", "máj", "jún",
+    "júl", "aug", "sep", "okt", "nov", "dec",
+
+    "Áno", /* Yes, affirmative response */
+    "Nie", /* No/negative response */
+
+    /* AM/PM strings AM 0000 -> 1159, PM 1200 -> 2359 */
+    "doob.", "poob.",
+
+    /* Soft and hard hyphens */
+    "\xAD", "-",
+
+    /* Open and close quotes */
+    "\"", "\"",
+
+    /* Days: But not actual day names
+       Yesterday - the day before the current
+       Today - the current day
+       Tomorrow - the next day
+       Future.
+    */
+    "vçera", "dnes", "zajtra", "budúcnosï",
+
+    /* Native language name */
+    NLANGSTR
+};
+
+/* This is the end of ROMtag marker. */
+const char end=0;
