05630748c22318ab5335308ca599a19f5c434379
Kalamatee <kalamatee@gmail.com>
2016-01-10 01:49:46 +0000

    minor refactor;
    moved timer specific code from realtime_init to its own file.
    se the "exposed" tick frequency to a real value based on the timer we use.
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@51263 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/realtime/realtime_init.c
+++ workbench/libs/realtime/realtime_init.c
@@ -8,18 +8,9 @@
 
 /* HISTORY:      25.06.99   SDuvan  Began implementing... */
 
-
-
-#include <utility/utility.h>
-
 #include <exec/types.h>
 #include <exec/resident.h>
-#include <exec/interrupts.h>
-#include <hardware/intbits.h>
-#include <dos/dos.h>
-#include <proto/dos.h>
 #include <proto/exec.h>
-#include <utility/tagitem.h>
 #include <aros/symbolsets.h>
 
 #include <exec/libraries.h>
@@ -34,8 +25,6 @@
 BOOL AllocTimer(struct internal_RealTimeBase *RealTimeBase);
 void FreeTimer(struct internal_RealTimeBase *RealTimeBase);
 
-extern void Pulse();
-
 static int Init(LIBBASETYPEPTR LIBBASE)
 {
     /* This function is single-threaded by exec by calling Forbid. */
@@ -49,33 +38,12 @@ static int Init(LIBBASETYPEPTR LIBBASE)
 
     NEWLIST(&RealTimeBase->rtb_ConductorList);
 
-    RealTimeBase->rtb_TickErr = 0;	/* How may such a thing be measured? */
-
-    /* I use a process here just to be able to use CreateNewProc() so
-       I don't have to fiddle with stack order and such... */
-    {
-	struct TagItem tags[] = { { NP_Entry   , (IPTR)Pulse            },
-	                          { NP_Name    , (IPTR)"RealTime Pulse" },
-				  { NP_Priority, (IPTR)127              },
-			          { NP_UserData, (IPTR)RealTimeBase     },
-			          { TAG_DONE   , (IPTR)NULL             } };
-	
-	GPB(RealTimeBase)->rtb_PulseTask = (struct Task *)CreateNewProc(tags);
-    }
-    
-    if (GPB(RealTimeBase)->rtb_PulseTask == NULL)
-    {
-	return FALSE;
-    }
-
-    kprintf("Realtime pulse task created\n");
-
     if (!AllocTimer((struct internal_RealTimeBase *)RealTimeBase))
     {
 	return FALSE;
     }
-	
-    kprintf("Realtime pulse timer created\n");
+
+    D(bug("[realtime.library] initialized\n"));
 
     return TRUE;
 }
@@ -90,50 +58,7 @@ static int Expunge(LIBBASETYPEPTR LIBBASE)
 
     FreeTimer(RealTimeBase);
 
-    /* Shut down the pulse message task -- must be done AFTER freeing the
-       timer! */
-    Signal(RealTimeBase->rtb_PulseTask, SIGBREAKF_CTRL_C);
-    
     return TRUE;
-}
-
-
-/* RealTime timer interrupt -- currently only a VBlank interrupt */
-static AROS_INTH1(rtVBlank, struct internal_RealTimeBase *, RealTimeBase)
-{
-    AROS_INTFUNC_INIT
-
-    // kprintf("Signalling task %p\n", RealTimeBase->rtb_PulseTask);
-    Signal(RealTimeBase->rtb_PulseTask, SIGF_SINGLE);
-
-    return 0;
-
-    AROS_INTFUNC_EXIT
-}
-
-
-BOOL AllocTimer(struct internal_RealTimeBase *RealTimeBase)
-{
-    /* TODO */
-    /* This should be replaced by some timer.device thing when an accurate
-       timer is available -- UNIT_MICROHZ? */
-
-    RealTimeBase->rtb_VBlank.is_Code         = (APTR)&rtVBlank;
-    RealTimeBase->rtb_VBlank.is_Data         = (APTR)RealTimeBase;
-    RealTimeBase->rtb_VBlank.is_Node.ln_Name = "RealTime VBlank server";
-    RealTimeBase->rtb_VBlank.is_Node.ln_Pri  = 127;
-    RealTimeBase->rtb_VBlank.is_Node.ln_Type = NT_INTERRUPT;
-    
-    /* Add a VBLANK server to take care of the heartbeats. */
-    AddIntServer(INTB_VERTB, &RealTimeBase->rtb_VBlank);
-
-    return TRUE;
-}
-
-
-void FreeTimer(struct internal_RealTimeBase *RealTimeBase)
-{
-    RemIntServer(INTB_VERTB, &RealTimeBase->rtb_VBlank);
 }
 
 
---
+++ workbench/libs/realtime/realtime_timer.c
@@ -0,0 +1,84 @@
+/*
+    Copyright © 2015, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: timer implementation.
+    Lang: English.
+*/
+
+#define DEBUG 1
+#include <aros/debug.h>
+
+#include <proto/dos.h>
+#include <proto/exec.h>
+
+#include <exec/interrupts.h>
+#include <hardware/intbits.h>
+
+#include "realtime_intern.h"
+
+extern void Pulse();
+
+/* RealTime timer interrupt -- currently only a VBlank interrupt */
+static AROS_INTH1(rtVBlank, struct internal_RealTimeBase *, RealTimeBase)
+{
+    AROS_INTFUNC_INIT
+
+    // kprintf("Signalling task %p\n", RealTimeBase->rtb_PulseTask);
+    Signal(RealTimeBase->rtb_PulseTask, SIGF_SINGLE);
+
+    return 0;
+
+    AROS_INTFUNC_EXIT
+}
+
+
+BOOL AllocTimer(struct internal_RealTimeBase *RealTimeBase)
+{
+    RealTimeBase->rtb_TickErr = 0;	/* How may such a thing be measured?            */
+    RealTimeBase->rtb_Reserved1 = 60;   /* NB: rtb_Reserved1 contains the TICK_FREQ,
+                                         * which in our case is vblank (60)             */
+
+    /* I use a process here just to be able to use CreateNewProc() so
+       I don't have to fiddle with stack order and such... */
+    {
+	struct TagItem tags[] = { { NP_Entry   , (IPTR)Pulse            },
+	                          { NP_Name    , (IPTR)"RealTime Pulse" },
+				  { NP_Priority, (IPTR)127              },
+			          { NP_UserData, (IPTR)RealTimeBase     },
+			          { TAG_DONE   , (IPTR)NULL             } };
+	
+	RealTimeBase->rtb_PulseTask = (struct Task *)CreateNewProc(tags);
+    }
+    
+    if (RealTimeBase->rtb_PulseTask == NULL)
+    {
+	return FALSE;
+    }
+
+    D(bug("[realtime.library] pulse task created @ 0x%p\n", GPB(RealTimeBase)->rtb_PulseTask));
+
+    /* TODO */
+    /* This should be replaced by some timer.device thing when an accurate
+       timer is available -- UNIT_MICROHZ? */
+
+    RealTimeBase->rtb_VBlank.is_Code         = (APTR)&rtVBlank;
+    RealTimeBase->rtb_VBlank.is_Data         = (APTR)RealTimeBase;
+    RealTimeBase->rtb_VBlank.is_Node.ln_Name = "RealTime VBlank server";
+    RealTimeBase->rtb_VBlank.is_Node.ln_Pri  = 127;
+    RealTimeBase->rtb_VBlank.is_Node.ln_Type = NT_INTERRUPT;
+    
+    /* Add a VBLANK server to take care of the heartbeats. */
+    AddIntServer(INTB_VERTB, &RealTimeBase->rtb_VBlank);
+
+    return TRUE;
+}
+
+
+void FreeTimer(struct internal_RealTimeBase *RealTimeBase)
+{
+    RemIntServer(INTB_VERTB, &RealTimeBase->rtb_VBlank);
+
+    if (RealTimeBase->rtb_PulseTask)
+        Signal(RealTimeBase->rtb_PulseTask, SIGBREAKF_CTRL_C);
+}
