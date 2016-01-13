b82b049b2762500ef665c529c29f84876b218aff
polluks <polluks@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2016-01-13 15:47:29 +0000

    Finally activate the skeleton of the ARexx port
    git-svn-id: https://svn.aros.org/svn/aros/trunk@51272 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/utilities/MultiView/main.c
+++ workbench/utilities/MultiView/main.c
@@ -10,6 +10,7 @@
 #include "compilerspecific.h"
 #include "debug.h"
 #include "arossupport.h"
+#include "ARexx/MultiView.h"
 
 #include <setjmp.h>
 #include <stdio.h>
@@ -73,6 +74,7 @@ static struct libinfo
 static struct TextAttr  textattr;
 static struct TextFont  *font;
 static struct RDArgs    *myargs;
+static struct RexxHost  *host;
 static IPTR             args[NUM_ARGS];
 static UBYTE            fontname[256];
 static WORD             winwidth, winheight;
@@ -184,6 +186,9 @@ void Cleanup(CONST_STRPTR msg)
 
     CloseLibs();
     CleanupLocale();
+
+    if (host)
+        CloseDownARexxHost(host);
 
     longjmp(exit_buf, 0);
 }
@@ -1590,6 +1595,7 @@ int main(int argc, char **argv)
     tdt_text_wordwrap = TRUE;
     separate_screen   = FALSE;
 
+    host = SetupARexxHost("MULTIVIEW.1", NULL);
     InitLocale("System/Utilities/MultiView.catalog", 1);
     InitMenus(nm);
     InitMenus(nmpict);
