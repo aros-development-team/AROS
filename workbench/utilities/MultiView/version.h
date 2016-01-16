16565b65488f7ccc1029a688b99d7b5a46640b9c
Kalamatee <kalamatee@gmail.com>
2016-01-16 05:40:37 +0000

    bump version
    git-svn-id: https://svn.aros.org/svn/aros/trunk@51280 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/utilities/MultiView/version.h
+++ workbench/utilities/MultiView/version.h
@@ -8,6 +8,6 @@
 
 
 #define VERSION         1
-#define REVISION        4
-#define DATESTR         "02.02.2011"
-#define VERSIONSTR      "$VER: MultiView 1.4 (" DATESTR ")"
+#define REVISION        5
+#define DATESTR         "16.01.2016"
+#define VERSIONSTR      "$VER: MultiView 1.5 (" DATESTR ")"
