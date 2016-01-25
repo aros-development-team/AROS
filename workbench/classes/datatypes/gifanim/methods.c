6bf4fcc4c6eed59861b673e6545b75ec6fa4004a
Kalamatee <kalamatee@gmail.com>
2016-01-25 16:25:39 +0000

    make sure we know this bits AROS specific ..
    git-svn-id: https://svn.aros.org/svn/aros/trunk@51413 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/classes/datatypes/gifanim/methods.c
+++ workbench/classes/datatypes/gifanim/methods.c
@@ -219,6 +219,7 @@ IPTR DT_GetMethod(struct IClass *cl, Object *o, struct opGet *msg)
 
     switch(msg->opg_AttrID)
     {
+#if defined(__AROS__)
     case DTA_DataType:
         {
             struct DataType     *dt = NULL;
@@ -252,7 +253,7 @@ IPTR DT_GetMethod(struct IClass *cl, Object *o, struct opGet *msg)
                 *msg->opg_Storage = (IPTR) NULL;
         }
         break;
-
+#endif
     default:
         return DoSuperMethodA (cl, o, (Msg) msg);
     }
