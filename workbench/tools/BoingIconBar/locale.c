2a3c3df70eec8d27f4360ce42171b31480e29efe
mattrust <mrustler@gmx.de>
2016-03-03 23:12:13 +0000

    Added "Quit" menu entry. Added checks for return values of e.g. AllocVec(). Minor fixes and catalogs updated.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@51601 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/tools/BoingIconBar/locale.c
+++ workbench/tools/BoingIconBar/locale.c
@@ -11,7 +11,7 @@
 #include "strings.h"
 
 #define CATALOG_NAME     "System/Tools/BoingIconBar.catalog"
-#define CATALOG_VERSION  0
+#define CATALOG_VERSION  2
 
 /*** Variables **************************************************************/
 static struct Catalog *catalog;
