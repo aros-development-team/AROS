30867804fb1d37fc9cb9f52b8ae39442ef39b357
mattrust <mrustler@gmx.de>
2016-03-30 10:07:22 +0000

    Bumped required version to 2 and build only catalogs which don't cause warnings.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@52404 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/system/Wanderer/Tools/DiskInfo/locale.c
+++ workbench/system/Wanderer/Tools/DiskInfo/locale.c
@@ -10,7 +10,7 @@
 #include "strings.h"
 
 #define CATALOG_NAME     "System/System/Wanderer/Tools/DiskInfo.catalog"
-#define CATALOG_VERSION  0
+#define CATALOG_VERSION  2
 
 /*** Variables **************************************************************/
 struct Catalog *catalog;
