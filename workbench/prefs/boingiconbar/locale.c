f3b719a92664db46d75fe18f18602be8f91942b0
mattrust <mrustler@gmx.de>
2016-03-30 15:07:15 +0000

    Bumped required version to 2. German CT updated.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@52443 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/prefs/boingiconbar/locale.c
+++ workbench/prefs/boingiconbar/locale.c
@@ -10,7 +10,7 @@
 #include "strings.h"
 
 #define CATALOG_NAME     "System/Prefs/BIBPrefs.catalog"
-#define CATALOG_VERSION  0
+#define CATALOG_VERSION  2
 
 /*** Variables **************************************************************/
 struct Catalog *catalog;
