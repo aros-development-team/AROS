46a706495d2e6f829237824ca86bf559a5f25d77
Kalamatee <kalamatee@gmail.com>
2016-03-31 01:47:01 +0000

    add place holder for aarch64 mmu defines
    git-svn-id: https://svn.aros.org/svn/aros/trunk@52462 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/aarch64-all/include/asm/mmu.h
@@ -0,0 +1,12 @@
+#ifndef ASM_AARCH64_MMU_H
+#define ASM_AARCH64_MMU_H
+
+/*
+    Copyright © 2016, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <exec/types.h>
+#include <inttypes.h>
+
+#endif /* ASM_AARCH64_MMU_H */
