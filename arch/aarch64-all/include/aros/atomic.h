80c0396599f41d118bcb66361bbe8ffa778b8173
Kalamatee <kalamatee@gmail.com>
2016-03-31 01:06:49 +0000

    add arm v8 (aarch64) atomic operations
    git-svn-id: https://svn.aros.org/svn/aros/trunk@52459 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/aarch64-all/include/aros/atomic.h
@@ -0,0 +1,13 @@
+/*
+    Copyright © 2016, The AROS Development Team. All rights reserved.
+    $Id$
+ */
+
+#ifndef AROS_AARCH64_ATOMIC_H
+#define AROS_AARCH64_ATOMIC_H
+
+#ifdef __ARM_ARCH_8A__
+#include <aros/aarch64/atomic_v8.h>
+#endif
+
+#endif /* AROS_AARCH64_ATOMIC_H */
