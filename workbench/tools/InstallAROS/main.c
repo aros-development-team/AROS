f0d6302d89f99a7af8c533396be25777487601ad
deadwood-pl <deadwood@wp.pl>
2016-06-11 19:48:11 +0000

    InstallAROS: replace duplicated literals with existing defines
    git-svn-id: https://svn.aros.org/svn/aros/trunk@52757 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/tools/InstallAROS/main.c
+++ workbench/tools/InstallAROS/main.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 2003-2014, The AROS Development Team. All rights reserved.
+    Copyright © 2003-2016, The AROS Development Team. All rights reserved.
     $Id$
 */
 
@@ -794,7 +794,7 @@ IPTR Install__MUIM_IC_NextStep(Class * CLASS, Object * self, Msg message)
                     tmp_grub = AllocVec(100, MEMF_CLEAR | MEMF_PUBLIC);
 
                     GET(dest_volume, MUIA_String_Contents, &option);
-                    sprintf(tmp_grub, "%s:boot/pc/grub",
+                    sprintf(tmp_grub, "%s:"ARCH_PATH"/"GRUB_PATH,
                         (CONST_STRPTR) option);
 
                     /* Guess the best disk to install GRUB's bootblock to */
@@ -3335,7 +3335,7 @@ int main(int argc, char *argv[])
 
     /* Let GRUB text/graphics choice default to same as on boot media */
     source_path[pathend] = '\0';
-    AddPart(source_path, "boot/grub/unicode.pf2", 256);
+    AddPart(source_path, ARCH_PATH"/"GRUB_PATH"/unicode.pf2", 256);
     if ((lock = Lock(source_path, SHARED_LOCK)) != BNULL)
     {
         gfx_font_exists = TRUE;
