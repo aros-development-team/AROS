d0570be2ca8f4c8c10892a699a63b1f2268190d5
o1i <o1i@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2016-06-29 10:05:45 +0000

    autodoc added
    git-svn-id: https://svn.aros.org/svn/aros/trunk@52766 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/posixc/tcsetattr.c
+++ compiler/posixc/tcsetattr.c
@@ -1,13 +1,51 @@
 /*
-    Copyright © 1995-2014, The AROS Development Team. All rights reserved.
+    Copyright (C) 1995-2016, The AROS Development Team. All rights reserved.
     $Id$
 */
 
-#include <termios.h>
 
-/* FIXME: add autodoc */
-int tcsetattr(int __fd, int __optional_actions,
-              const struct termios *__termios_p)
+
+/*****************************************************************************
+
+    NAME */
+
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <errno.h>
+
+	int tcsetattr(
+
+/*  SYNOPSIS */
+	int fd,
+	int opt,
+	const struct termios *t)
+
+/*  FUNCTION
+        Set terminal attributes.
+
+    INPUTS
+        fd      - file descriptor
+        opt     - optional actions
+        t       - struct termios containing the requested changes
+
+    RESULT
+        0       - success
+        1       - error 
+
+    NOTES
+        Will return success, if *any* of the changes were successfull.
+
+    EXAMPLE
+
+    BUGS
+        Not implemented.
+
+    SEE ALSO
+        ioctl()
+
+    INTERNALS
+
+******************************************************************************/
 {
     /* TODO: Implement tcsetattr() */
     return 0;
