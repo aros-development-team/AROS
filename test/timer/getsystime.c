f76b125d819e5418ea619d154404a3f5ecaac248
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2016-07-04 18:08:50 +0000

    Added test for GetSysTimer(), mostly written by mazze.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@52768 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/timer/getsystime.c
@@ -0,0 +1,56 @@
+/*
+    Copyright © 1995-2016, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <devices/timer.h>
+
+#include <proto/timer.h>
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <clib/alib_protos.h>
+
+struct Device *TimerBase;
+struct MsgPort *TimerMP;
+struct timerequest *TimerIO;
+
+int main(int argc, char **argv)
+{
+    struct timeval t1, t2;
+    ULONG secs = 10;
+
+    if (argc == 2)
+        secs = atoi(argv[1]);
+
+    printf("Waiting %u seconds\n", secs);
+
+    if (TimerMP = CreatePort(NULL, 0))
+    {
+        if (TimerIO = (struct timerequest *)CreateExtIO(TimerMP,
+            sizeof(struct timerequest)))
+        {
+            if (!(OpenDevice(TIMERNAME, UNIT_MICROHZ,
+                (struct IORequest *)TimerIO, 0)))
+            {
+                TimerBase = TimerIO->tr_node.io_Device;
+
+                GetSysTime(&t1);
+
+                Delay(secs * 50);
+
+                GetSysTime(&t2);
+
+                SubTime(&t2, &t1);
+                printf("result %u secs %u micros\n", 
+                    t2.tv_secs,
+                    t2.tv_micro);
+                CloseDevice((struct IORequest *)TimerIO);
+            }
+            DeleteExtIO((struct IORequest *)TimerIO);
+        }
+        DeletePort(TimerMP);
+    }
+    return 0;
+}
