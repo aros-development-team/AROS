4dd9bb395153f99894b706fd823b6fbf09330ab2
deadwood-pl <deadwood@wp.pl>
2016-08-14 18:56:36 +0000

    posixc.library: implement switching on/off canonical mode for terminal.
    Non-canonical mode maps to RAW mode of console-handler.
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@52860 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/posixc/tcgetattr.c
+++ compiler/posixc/tcgetattr.c
@@ -1,13 +1,63 @@
 /*
-    Copyright © 1995-2014, The AROS Development Team. All rights reserved.
+    Copyright © 2016, The AROS Development Team. All rights reserved.
     $Id$
 */
 
-#include <termios.h>
+#include <proto/dos.h>
 
-/* FIXME: add autodoc */
-int tcgetattr(int __fd, struct termios *__termios_p)
+#include "__fdesc.h"
+
+/*****************************************************************************
+
+    NAME */
+
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <errno.h>
+
+    int tcgetattr(
+
+/*  SYNOPSIS */
+        int fd,
+        struct termios *t)
+
+/*  FUNCTION
+        Get terminal attributes.
+
+    INPUTS
+        fd      - file descriptor
+        t       - struct termios where attributes are put
+
+    RESULT
+         0      - success
+        -1      - error
+
+    NOTES
+        Currently supports only ICANON flag
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+        ioctl()
+
+    INTERNALS
+
+******************************************************************************/
 {
-    /* TODO: Implement tcgetattr() */
+    fdesc *fdesc = __getfdesc(fd);
+
+    if (!fdesc)
+    {
+        errno = EBADF;
+        return -1;
+    }
+
+    if (fdesc->fcb->privflags & _FCB_CONSOLERAW)
+        t->c_lflag &= ~ICANON;
+    else
+        t->c_lflag |= ICANON;
+
     return 0;
 }
