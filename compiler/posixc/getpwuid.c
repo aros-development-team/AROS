7bd05bc6ddd26a782e98e12fadf5f02ca03522e3
deadwood-pl <deadwood@wp.pl>
2016-08-14 18:56:26 +0000

    posixc.library: further simplify emulation code
    git-svn-id: https://svn.aros.org/svn/aros/trunk@52857 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/posixc/getpwuid.c
+++ compiler/posixc/getpwuid.c
@@ -39,19 +39,13 @@
 
 ******************************************************************************/
 {
-    static struct passwd _return;
+    static struct passwd _return = {0};
 
-    if (_user.ur_uid == uid)
-    {
-        _return.pw_name     = _user.ur_name;
-        _return.pw_uid      = _user.ur_uid;
-        _return.pw_dir      = _user.ur_dir;
-        _return.pw_shell    = _user.ur_shell;
-        _return.pw_passwd   = _user.ur_passwd;
-        _return.pw_gecos    = _user.ur_gecos;
+    if (_return.pw_name == NULL)
+        __fill_passwd(&_return);
 
+    if (_return.pw_uid == uid)
         return &_return;
-    }
 
     return NULL;
 }
