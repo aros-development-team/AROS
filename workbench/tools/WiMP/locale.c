01352e91c8b8ada4315a978ed1ba9415f83b3db2
mmartinka <mmartinka@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2016-10-13 12:54:53 +0000

    Add locale file for WiMP
    git-svn-id: https://svn.aros.org/svn/aros/trunk@52987 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/tools/WiMP/locale.c
@@ -0,0 +1,57 @@
+/*
+    Copyright © 1995-2016, The AROS Development Team. All rights reserved.
+*/
+
+#include <aros/symbolsets.h>
+#include <exec/types.h>
+#include <proto/locale.h>
+
+#define CATCOMP_ARRAY
+#include "strings.h"
+
+#define CATALOG_NAME     "System/Tools/WiMP.catalog"
+#define CATALOG_VERSION  1
+
+/*** Variables **************************************************************/
+static struct Catalog *catalog;
+
+
+/*** Functions **************************************************************/
+/* Main *********************************************************************/
+CONST_STRPTR _(ULONG id)
+{
+    if (LocaleBase != NULL && catalog != NULL)
+    {
+        return GetCatalogStr(catalog, id, CatCompArray[id].cca_Str);
+    } 
+    else 
+    {
+        return CatCompArray[id].cca_Str;
+    }
+}
+
+/* Setup ********************************************************************/
+BOOL Locale_Initialize(VOID)
+{
+    if (LocaleBase != NULL)
+    {
+        catalog = OpenCatalog
+        ( 
+            NULL, CATALOG_NAME, OC_Version, CATALOG_VERSION, TAG_DONE 
+        );
+    }
+    else
+    {
+        catalog = NULL;
+    }
+    
+    return TRUE;
+}
+
+VOID Locale_Deinitialize(VOID)
+{
+    if(LocaleBase != NULL && catalog != NULL) CloseCatalog(catalog);
+}
+
+ADD2INIT(Locale_Initialize,   90);
+ADD2EXIT(Locale_Deinitialize, 90);
