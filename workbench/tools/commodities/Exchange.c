00b4eda12a5f80febacac0d0d2fcd870be4505ea
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2016-11-02 09:19:56 +0000

    Tabs to spaces.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@53045 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/tools/commodities/Exchange.c
+++ workbench/tools/commodities/Exchange.c
@@ -3,7 +3,7 @@
     $Id$
 
     Exchange -- controls commodities.
- */
+*/
 
 /******************************************************************************
 
@@ -26,8 +26,8 @@
     INPUTS
 
         CX_PRIORITY  --  Priority of the Exchange broker
-	CX_POPKEY    --  Hotkey combination for Exchange
-	CX_POPUP     --  Appear at startup
+        CX_POPKEY    --  Hotkey combination for Exchange
+        CX_POPUP     --  Appear at startup
 
     RESULT
 
@@ -95,7 +95,8 @@ enum {
     NUM_ARGS
 };
 
-static Object *app, *wnd, *listgad, *textgad1, *textgad2, *showgad, *hidegad, *cyclegad, *killgad;
+static Object *app, *wnd, *listgad, *textgad1, *textgad2, *showgad,
+    *hidegad, *cyclegad, *killgad;
 static CONST_STRPTR cyclestrings[3];
 static struct Catalog *catalog;
 static UBYTE s[257];
@@ -133,11 +134,11 @@ static CONST_STRPTR _(ULONG id)
 {
     if (LocaleBase != NULL && catalog != NULL)
     {
-	return GetCatalogStr(catalog, id, CatCompArray[id].cca_Str);
-    } 
-    else 
+        return GetCatalogStr(catalog, id, CatCompArray[id].cca_Str);
+    }
+    else
     {
-	return CatCompArray[id].cca_Str;
+        return CatCompArray[id].cca_Str;
     }
 }
 
@@ -149,14 +150,12 @@ static int Locale_Initialize(VOID)
 {
     if (LocaleBase != NULL)
     {
-	catalog = OpenCatalog
-	    ( 
-	     NULL, CATALOG_NAME, OC_Version, CATALOG_VERSION, TAG_DONE 
-	    );
+        catalog = OpenCatalog(NULL, CATALOG_NAME, OC_Version,
+            CATALOG_VERSION, TAG_DONE);
     }
     else
     {
-	catalog = NULL;
+        catalog = NULL;
     }
 /* Note that in AROS constructors should return value opposite to the standard one.
    Probably it's a misdesign, but we can do nothing with it. */
@@ -186,39 +185,40 @@ static void GetArguments(int argc, char **argv)
     static STRPTR cxname;
     if (argc)
     {
-	if (!(myargs = ReadArgs(ARG_TEMPLATE, args, NULL)))
-	{
-	    Fault(IoErr(), 0, s, 256);
-	    Cleanup(s);
-	}
-	if (args[ARG_CXPRI]) cx_pri = *(LONG*)args[ARG_CXPRI];
-	if (args[ARG_CXPOPKEY])
-	{
-	    cx_popkey = StrDup((char *)args[ARG_CXPOPKEY]);
-	}
-	else
-	{
-	    cx_popkey = StrDup(DEF_POPKEY);
-	}
-	if (args[ARG_CXPOPUP]) cx_popup = TRUE;
-	FreeArgs(myargs);
-    cxname = argv[0];
+        if (!(myargs = ReadArgs(ARG_TEMPLATE, args, NULL)))
+        {
+            Fault(IoErr(), 0, s, 256);
+            Cleanup(s);
+        }
+        if (args[ARG_CXPRI]) cx_pri = *(LONG*)args[ARG_CXPRI];
+        if (args[ARG_CXPOPKEY])
+        {
+            cx_popkey = StrDup((char *)args[ARG_CXPOPKEY]);
+        }
+        else
+        {
+            cx_popkey = StrDup(DEF_POPKEY);
+        }
+        if (args[ARG_CXPOPUP]) cx_popup = TRUE;
+        FreeArgs(myargs);
+        cxname = argv[0];
     }
     else
     {
         argmsg = (struct WBStartup *)argv;
         wb_arg = argmsg->sm_ArgList;
         cxname = wb_arg->wa_Name;
-	wbargs = ArgArrayInit(argc, (UBYTE**)argv);
-	cx_pri = ArgInt(wbargs, "CX_PRIORITY", 0);
-	cx_popkey = StrDup(ArgString(wbargs, "CX_POPKEY", DEF_POPKEY));
-	if (strnicmp(ArgString(wbargs, "CX_POPUP", "NO"), "Y", 1) == 0)
-	{
-	    cx_popup = TRUE;
-	}
-	ArgArrayDone();
+        wbargs = ArgArrayInit(argc, (UBYTE**)argv);
+        cx_pri = ArgInt(wbargs, "CX_PRIORITY", 0);
+        cx_popkey = StrDup(ArgString(wbargs, "CX_POPKEY", DEF_POPKEY));
+        if (strnicmp(ArgString(wbargs, "CX_POPUP", "NO"), "Y", 1) == 0)
+        {
+            cx_popup = TRUE;
+        }
+        ArgArrayDone();
     }
-    D(bug("Exchange Arguments pri %d popkey %s popup %d\n", cx_pri, cx_popkey, cx_popup));
+    D(bug("Exchange Arguments pri %d popkey %s popup %d\n", cx_pri,
+        cx_popkey, cx_popup));
     disko = GetDiskObject(cxname);
 }
 
@@ -226,12 +226,12 @@ static void GetArguments(int argc, char **argv)
 
 static struct NewMenu nm[] =
 {
-    {NM_TITLE, (STRPTR)MSG_MEN_PROJECT         },
-     {NM_ITEM, (STRPTR)MSG_MEN_PROJECT_HIDE    },
-     {NM_ITEM, (STRPTR)MSG_MEN_PROJECT_ICONIFY },
-     {NM_ITEM, NM_BARLABEL    	               },
-     {NM_ITEM, (STRPTR)MSG_MEN_PROJECT_QUIT    },
-    {NM_END                                    }
+    {NM_TITLE, (STRPTR)MSG_MEN_PROJECT        },
+    {NM_ITEM, (STRPTR)MSG_MEN_PROJECT_HIDE    },
+    {NM_ITEM, (STRPTR)MSG_MEN_PROJECT_ICONIFY },
+    {NM_ITEM, NM_BARLABEL                     },
+    {NM_ITEM, (STRPTR)MSG_MEN_PROJECT_QUIT    },
+    {NM_END                                   }
 };
 
 /*********************************************************************************************/
@@ -242,21 +242,21 @@ static void InitMenus(void)
 
     for(actnm = nm; actnm->nm_Type != NM_END; actnm++)
     {
-	if (actnm->nm_Label != NM_BARLABEL)
-	{
-	    ULONG  id = (IPTR)actnm->nm_Label;
-	    CONST_STRPTR str = _(id);
+        if (actnm->nm_Label != NM_BARLABEL)
+        {
+            ULONG  id = (IPTR)actnm->nm_Label;
+            CONST_STRPTR str = _(id);
 
-	    if (actnm->nm_Type == NM_TITLE)
-	    {
-		actnm->nm_Label = str;
-	    } else {
-		actnm->nm_Label = str + 2;
-		if (str[0] != ' ') actnm->nm_CommKey = str;
-	    }
-	    actnm->nm_UserData = (APTR)(IPTR)id;
+            if (actnm->nm_Type == NM_TITLE)
+            {
+                actnm->nm_Label = str;
+            } else {
+                actnm->nm_Label = str + 2;
+                if (str[0] != ' ') actnm->nm_CommKey = str;
+            }
+            actnm->nm_UserData = (APTR)(IPTR)id;
 
-	} /* if (actnm->nm_Label != NM_BARLABEL) */
+        } /* if (actnm->nm_Label != NM_BARLABEL) */
 
     } /* for(actnm = nm; nm->nm_Type != NM_END; nm++) */
 }
@@ -267,19 +267,19 @@ static void showSimpleMessage(CONST_STRPTR msgString)
 {
     struct EasyStruct easyStruct;
 
-    easyStruct.es_StructSize	= sizeof(easyStruct);
-    easyStruct.es_Flags		= 0;
-    easyStruct.es_Title		= _(MSG_EXCHANGE_CXNAME);
-    easyStruct.es_TextFormat	= msgString;
-    easyStruct.es_GadgetFormat	= _(MSG_OK);		
+    easyStruct.es_StructSize = sizeof(easyStruct);
+    easyStruct.es_Flags = 0;
+    easyStruct.es_Title = _(MSG_EXCHANGE_CXNAME);
+    easyStruct.es_TextFormat = msgString;
+    easyStruct.es_GadgetFormat = _(MSG_OK);
 
-    if (IntuitionBase != NULL && !Cli() )
+    if (IntuitionBase != NULL && !Cli())
     {
-	EasyRequestArgs(NULL, &easyStruct, NULL, NULL);
+        EasyRequestArgs(NULL, &easyStruct, NULL, NULL);
     }
     else
     {
-	PutStr(msgString);
+        PutStr(msgString);
     }
 }
 
@@ -306,18 +306,19 @@ static void update_list(void)
     DoMethod(listgad, MUIM_List_Clear);
     ForeachNode(&brokerList, node)
     {
-	D(bug("Exchange: Brokernode '%s'\n", node->bc_Name));
-	DoMethod(listgad, MUIM_List_InsertSingle, node, MUIV_List_Insert_Bottom);
+        D(bug("Exchange: Brokernode '%s'\n", node->bc_Name));
+        DoMethod(listgad, MUIM_List_InsertSingle, node,
+            MUIV_List_Insert_Bottom);
 
-	/* Reselect broker if its name matches what was previously selected */
-	if (active_node != NULL
-	    && Stricmp(active_name, node->bc_Name) == 0)
-	{
-	    get(listgad, MUIA_List_InsertPosition, &new_pos);
-	    set(listgad, MUIA_List_Active, new_pos);
-	    D(bug("Exchange: matched old active node '%s' (entry %d)\n",
-		node->bc_Name, new_pos));
-	}
+        /* Reselect broker if its name matches what was previously selected */
+        if (active_node != NULL
+            && Stricmp(active_name, node->bc_Name) == 0)
+        {
+            get(listgad, MUIA_List_InsertPosition, &new_pos);
+            set(listgad, MUIA_List_Active, new_pos);
+            D(bug("Exchange: matched old active node '%s' (entry %d)\n",
+                node->bc_Name, new_pos));
+        }
     }
     set(listgad, MUIA_List_Quiet, FALSE);
 }
@@ -378,7 +379,7 @@ AROS_UFH3(void, list_display_func,
     AROS_USERFUNC_INIT
 
     *array = node->bc_Name;
-  
+
     AROS_USERFUNC_EXIT
 }
 
@@ -406,18 +407,19 @@ AROS_UFH3(void, list_select_func,
     AROS_USERFUNC_INIT
 
     struct BrokerCopy *bc;
-    DoMethod(listgad, MUIM_List_GetEntry, MUIV_List_GetEntry_Active, (IPTR)&bc);
+    DoMethod(listgad, MUIM_List_GetEntry, MUIV_List_GetEntry_Active,
+        (IPTR)&bc);
     if (bc)
     {
-	BOOL showHide = (bc->bc_Flags & COF_SHOW_HIDE) == 0;
-	BOOL active = (bc->bc_Flags & COF_ACTIVE) != 0;
-	nnset(textgad1, MUIA_Text_Contents, bc->bc_Title);
-	nnset(textgad2, MUIA_Text_Contents, bc->bc_Descr);
-	nnset(hidegad, MUIA_Disabled, showHide);
-	nnset(showgad, MUIA_Disabled, showHide);
-	nnset(cyclegad, MUIA_Cycle_Active, active ? 0 : 1);
+        BOOL showHide = (bc->bc_Flags & COF_SHOW_HIDE) == 0;
+        BOOL active = (bc->bc_Flags & COF_ACTIVE) != 0;
+        nnset(textgad1, MUIA_Text_Contents, bc->bc_Title);
+        nnset(textgad2, MUIA_Text_Contents, bc->bc_Descr);
+        nnset(hidegad, MUIA_Disabled, showHide);
+        nnset(showgad, MUIA_Disabled, showHide);
+        nnset(cyclegad, MUIA_Cycle_Active, active ? 0 : 1);
     }
-  
+
     AROS_USERFUNC_EXIT
 }
 
@@ -431,11 +433,12 @@ AROS_UFH3(void, inform_broker_func,
     AROS_USERFUNC_INIT
 
     struct BrokerCopy *bc;
-    DoMethod(listgad, MUIM_List_GetEntry, MUIV_List_GetEntry_Active, (IPTR) &bc);
+    DoMethod(listgad, MUIM_List_GetEntry, MUIV_List_GetEntry_Active,
+        (IPTR) &bc);
     if (bc)
     {
-	D(bug("Exchange: Broker inform %s\n", bc->bc_Node.ln_Name));
-	BrokerCommand(bc->bc_Node.ln_Name, *command);
+        D(bug("Exchange: Broker inform %s\n", bc->bc_Node.ln_Name));
+        BrokerCommand(bc->bc_Node.ln_Name, *command);
     }
 
     AROS_USERFUNC_EXIT
@@ -459,119 +462,130 @@ static void MakeGUI(void)
     list_select_hook.h_Entry = (HOOKFUNC)AROS_ASMSYMNAME(list_select_func);
     inform_broker_hook.h_Entry = (HOOKFUNC)AROS_ASMSYMNAME(inform_broker_func);
     show_hook.h_Entry = (HOOKFUNC)AROS_ASMSYMNAME(show_func);
-    
+
     snprintf(wintitle, sizeof(wintitle), _(MSG_EXCHANGE_WINTITLE), cx_popkey);
-    
+
     app = (Object *)ApplicationObject,
-	MUIA_Application_Title, __(MSG_EXCHANGE_CXNAME),
-	MUIA_Application_Version, (IPTR)version,
-	MUIA_Application_Copyright, (IPTR)"Copyright  © 1995-2012, The AROS Development TEAM",
-	MUIA_Application_Author, (IPTR)"The AROS Development Team",
-	MUIA_Application_Description, __(MSG_EXCHANGE_CXDESCR),
-	MUIA_Application_BrokerPri, cx_pri,
-	MUIA_Application_BrokerHook, (IPTR)&broker_hook,
-	MUIA_Application_Base, (IPTR)"EXCHANGE",
-	MUIA_Application_SingleTask, TRUE,
-	MUIA_Application_Menustrip, (IPTR)menu,
-    MUIA_Application_DiskObject, (IPTR)disko,
-	SubWindow, (IPTR)(wnd = (Object *)WindowObject,
-	    MUIA_Window_Title, (IPTR)wintitle,
-	    MUIA_Window_ID, MAKE_ID('E', 'X', 'C', 'H'),
-	    WindowContents, (IPTR)(HGroup,
-		Child, (IPTR)(VGroup,
-		    GroupFrameT(_(MSG_EXCHANGE_LISTVIEW)),
-		    Child, (IPTR)(ListviewObject,
-			MUIA_Listview_List, (IPTR)(listgad = (Object *)ListObject,
-			    InputListFrame,
-			    MUIA_List_DisplayHook, (IPTR)&list_disp_hook,
-			    MUIA_List_CompareHook, (IPTR)&list_comp_hook,
-			    MUIA_CycleChain, 1,
-			End),
-		    End),
-		End),
-		Child, (IPTR)(BalanceObject, MUIA_CycleChain, 1, End),
-		Child, (IPTR)(VGroup,
-		    MUIA_HorizWeight, 150,
-		    Child, (IPTR)(VGroup,
-			GroupFrameT(_(MSG_EXCHANGE_INFO)),
-			Child, (IPTR)(textgad1 = (Object *)TextObject, TextFrame,
-				MUIA_Background, MUII_TextBack, End),
-			Child, (IPTR)(textgad2 = (Object *)TextObject, TextFrame,
-				MUIA_Background, MUII_TextBack, End),
-		    End),
-		    Child, (IPTR)HVSpace,
-		    Child, (IPTR)(ColGroup(2),
-			MUIA_Group_SameSize, TRUE,
-			Child, (IPTR)(hidegad = SimpleButton(_(MSG_EXCHANGE_GAD_HIDE))),
-			Child, (IPTR)(showgad = SimpleButton(_(MSG_EXCHANGE_GAD_SHOW))),
-			Child, (IPTR)(cyclegad = MUI_MakeObject(MUIO_Cycle, NULL, cyclestrings)),
-			Child, (IPTR)(killgad = SimpleButton(_(MSG_EXCHANGE_GAD_REMOVE))),
-		    End),
-		End),
-	    End),
-	End),
-    End;
-    
+        MUIA_Application_Title, __(MSG_EXCHANGE_CXNAME),
+        MUIA_Application_Version, (IPTR)version,
+        MUIA_Application_Copyright,
+            (IPTR)"Copyright  © 1995-2016, The AROS Development Team",
+        MUIA_Application_Author, (IPTR)"The AROS Development Team",
+        MUIA_Application_Description, __(MSG_EXCHANGE_CXDESCR),
+        MUIA_Application_BrokerPri, cx_pri,
+        MUIA_Application_BrokerHook, (IPTR)&broker_hook,
+        MUIA_Application_Base, (IPTR)"EXCHANGE",
+        MUIA_Application_SingleTask, TRUE,
+        MUIA_Application_Menustrip, (IPTR)menu,
+        MUIA_Application_DiskObject, (IPTR)disko,
+        SubWindow, (IPTR)(wnd = (Object *)WindowObject,
+            MUIA_Window_Title, (IPTR)wintitle,
+            MUIA_Window_ID, MAKE_ID('E', 'X', 'C', 'H'),
+            WindowContents, (IPTR)(HGroup,
+                Child, (IPTR)(VGroup,
+                    GroupFrameT(_(MSG_EXCHANGE_LISTVIEW)),
+                    Child, (IPTR)(ListviewObject,
+                        MUIA_Listview_List,
+                            (IPTR)(listgad = (Object *)ListObject,
+                            InputListFrame,
+                            MUIA_List_DisplayHook, (IPTR)&list_disp_hook,
+                            MUIA_List_CompareHook, (IPTR)&list_comp_hook,
+                            MUIA_CycleChain, 1,
+                            End),
+                        End),
+                    End),
+                Child, (IPTR)(BalanceObject, MUIA_CycleChain, 1, End),
+                Child, (IPTR)(VGroup,
+                    MUIA_HorizWeight, 150,
+                    Child, (IPTR)(VGroup,
+                        GroupFrameT(_(MSG_EXCHANGE_INFO)),
+                        Child, (IPTR)(textgad1 = (Object *)TextObject,
+                            TextFrame,
+                            MUIA_Background, MUII_TextBack,
+                            End),
+                        Child, (IPTR)(textgad2 = (Object *)TextObject,
+                            TextFrame,
+                            MUIA_Background, MUII_TextBack,
+                            End),
+                        End),
+                    Child, (IPTR)HVSpace,
+                    Child, (IPTR)(ColGroup(2),
+                        MUIA_Group_SameSize, TRUE,
+                        Child, (IPTR)(hidegad =
+                            SimpleButton(_(MSG_EXCHANGE_GAD_HIDE))),
+                        Child, (IPTR)(showgad =
+                            SimpleButton(_(MSG_EXCHANGE_GAD_SHOW))),
+                        Child, (IPTR)(cyclegad =
+                            MUI_MakeObject(MUIO_Cycle, NULL, cyclestrings)),
+                        Child, (IPTR)(killgad =
+                            SimpleButton(_(MSG_EXCHANGE_GAD_REMOVE))),
+                        End),
+                    End),
+                End),
+            End),
+        End;
+
     if (! app)
-	Cleanup(NULL); // Propably double start
+        Cleanup(NULL); // Propably double start
 
     // enable hotkey
     maintask = FindTask(NULL);
     get(app, MUIA_Application_Broker, &broker);
     get(app, MUIA_Application_BrokerPort, &brokermp);
     if ( ! broker || ! brokermp)
-	Cleanup(_(MSG_CANT_CREATE_BROKER));
+        Cleanup(_(MSG_CANT_CREATE_BROKER));
 
     popfilter = CxFilter(cx_popkey);
     if (popfilter)
     {
-	CxObj *popsig = CxSignal(maintask, SIGBREAKB_CTRL_F);
-	if (popsig)
-	{
-	    CxObj *trans;
-	    AttachCxObj(popfilter, popsig);
-	    trans = CxTranslate(NULL);
-	    if (trans) AttachCxObj(popfilter, trans);
-	}
-	AttachCxObj(broker, popfilter);
+        CxObj *popsig = CxSignal(maintask, SIGBREAKB_CTRL_F);
+        if (popsig)
+        {
+            CxObj *trans;
+            AttachCxObj(popfilter, popsig);
+            trans = CxTranslate(NULL);
+            if (trans) AttachCxObj(popfilter, trans);
+        }
+        AttachCxObj(broker, popfilter);
     }
 
     // initial entry of brokers to listgadget
     update_list();
 
     DoMethod(wnd, MUIM_Notify, MUIA_Window_CloseRequest, TRUE,
-	(IPTR)wnd, 3, MUIM_Set, MUIA_Window_Open, FALSE);
+        (IPTR)wnd, 3, MUIM_Set, MUIA_Window_Open, FALSE);
 
     DoMethod(app, MUIM_Notify, MUIA_Application_DoubleStart, TRUE,
     (IPTR)wnd, 2, MUIM_CallHook, &show_hook);
 
     DoMethod(listgad, MUIM_Notify, MUIA_List_Active, MUIV_EveryTime,
-	(IPTR)listgad, 2, MUIM_CallHook, (IPTR)&list_select_hook);
+        (IPTR)listgad, 2, MUIM_CallHook, (IPTR)&list_select_hook);
 
     DoMethod(cyclegad, MUIM_Notify, MUIA_Cycle_Active, 0 /* Enable */,
-	(IPTR)app, 3, MUIM_CallHook, (IPTR)&inform_broker_hook, CXCMD_ENABLE);
+        (IPTR)app, 3, MUIM_CallHook, (IPTR)&inform_broker_hook, CXCMD_ENABLE);
 
     DoMethod(cyclegad, MUIM_Notify, MUIA_Cycle_Active, 1 /* Disable */,
-	(IPTR)app, 3, MUIM_CallHook, (IPTR)&inform_broker_hook, CXCMD_DISABLE);
+        (IPTR)app, 3, MUIM_CallHook, (IPTR)&inform_broker_hook, CXCMD_DISABLE);
 
     DoMethod(hidegad, MUIM_Notify, MUIA_Pressed, FALSE,
-	(IPTR)app, 3, MUIM_CallHook, (IPTR)&inform_broker_hook, CXCMD_DISAPPEAR);
+        (IPTR)app, 3, MUIM_CallHook, (IPTR)&inform_broker_hook,
+        CXCMD_DISAPPEAR);
 
     DoMethod(showgad, MUIM_Notify, MUIA_Pressed, FALSE,
-	(IPTR)app, 3, MUIM_CallHook, (IPTR)&inform_broker_hook, CXCMD_APPEAR);
+        (IPTR)app, 3, MUIM_CallHook, (IPTR)&inform_broker_hook, CXCMD_APPEAR);
 
     DoMethod(killgad, MUIM_Notify, MUIA_Pressed, FALSE,
-	(IPTR)app, 3, MUIM_CallHook, (IPTR)&inform_broker_hook, CXCMD_KILL);
+        (IPTR)app, 3, MUIM_CallHook, (IPTR)&inform_broker_hook, CXCMD_KILL);
 
     DoMethod(wnd, MUIM_Notify, MUIA_Window_MenuAction, MSG_MEN_PROJECT_QUIT,
-	(IPTR)app, 2, MUIM_Application_ReturnID, MUIV_Application_ReturnID_Quit);
+        (IPTR)app, 2, MUIM_Application_ReturnID,
+        MUIV_Application_ReturnID_Quit);
 
     DoMethod(wnd, MUIM_Notify, MUIA_Window_MenuAction, MSG_MEN_PROJECT_HIDE,
-	(IPTR)wnd, 3, MUIM_Set, MUIA_Window_Open, FALSE);
+        (IPTR)wnd, 3, MUIM_Set, MUIA_Window_Open, FALSE);
 
     DoMethod(wnd, MUIM_Notify, MUIA_Window_MenuAction, MSG_MEN_PROJECT_ICONIFY,
-	(IPTR)app, 3, MUIM_Set, MUIA_Application_Iconified, TRUE);
-     
+        (IPTR)app, 3, MUIM_Set, MUIA_Application_Iconified, TRUE);
 }
 
 /*********************************************************************************************/
@@ -581,22 +595,22 @@ static void HandleAll(void)
     ULONG sigs = 0;
 
     set(wnd, MUIA_Window_Open, cx_popup);
-    
+
     while((LONG) DoMethod(app, MUIM_Application_NewInput, (IPTR)&sigs)
-	    != MUIV_Application_ReturnID_Quit)
+            != MUIV_Application_ReturnID_Quit)
     {
-	if (sigs)
-	{
-	    sigs = Wait(sigs | SIGBREAKF_CTRL_C | SIGBREAKF_CTRL_F);
-	    if (sigs & SIGBREAKF_CTRL_C)
-	    {
-		break;
-	    }
-	    if (sigs & SIGBREAKF_CTRL_F)
-	    {
-	        CallHookPkt(&show_hook, NULL, NULL);
-	    }
-	}
+        if (sigs)
+        {
+            sigs = Wait(sigs | SIGBREAKF_CTRL_C | SIGBREAKF_CTRL_F);
+            if (sigs & SIGBREAKF_CTRL_C)
+            {
+                break;
+            }
+            if (sigs & SIGBREAKF_CTRL_F)
+            {
+                CallHookPkt(&show_hook, NULL, NULL);
+            }
+        }
     }
 }
 
@@ -610,8 +624,8 @@ static void Cleanup(CONST_STRPTR txt)
     FreeDiskObject(disko);
     if (txt)
     {
-	showSimpleMessage(txt);
-	exit(RETURN_ERROR);
+        showSimpleMessage(txt);
+        exit(RETURN_ERROR);
     }
     exit(RETURN_OK);
 }
@@ -623,7 +637,7 @@ int main(int argc, char **argv)
     D(bug("Exchange started\n"));
     NewList(&brokerList);
     GetArguments(argc, argv);
-    InitMenus();    
+    InitMenus();
     MakeGUI();
     HandleAll();
     Cleanup(NULL);
