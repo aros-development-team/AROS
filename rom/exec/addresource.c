9da8841442086f4a343d25aa0c28cda9c4b72265
Kalamatee <kalamatee@gmail.com>
2017-01-03 22:19:37 +0000

    bump copyright
    git-svn-id: https://svn.aros.org/svn/aros/trunk@53150 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/addresource.c
+++ rom/exec/addresource.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2011, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2017, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: Add a resource to the public list of resources.
