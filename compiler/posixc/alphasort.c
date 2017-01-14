8d3acf55181ca51c1894041324d944c25ac40a89
mattrust <mrustler@gmx.de>
2017-01-14 13:38:54 +0000

    POSIX functions scandir() and alphasort() implemented.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@53210 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/posixc/alphasort.c
@@ -0,0 +1,43 @@
+/*
+    Copyright © 1995-2017, The AROS Development Team. All rights reserved.
+    $Id$
+
+    POSIX.1-2008 function alphasort().
+*/
+
+#include <string.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <dirent.h>
+
+        int alphasort (
+
+/*  SYNOPSIS */
+        const struct dirent **a,
+        const struct dirent **b
+        )
+
+/*  FUNCTION
+        Support function for scandir().
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+        scandir()
+
+    INTERNALS
+
+******************************************************************************/
+{
+    return strcoll((*a)->d_name, (*b)->d_name);
+}
