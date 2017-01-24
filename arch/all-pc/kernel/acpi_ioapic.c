5856389b736db8a8901303be10723a9afe7edb3c
Kalamatee <kalamatee@gmail.com>
2017-01-24 00:05:52 +0000

    flesh out the ioapic detection/registration a little (and dump some info when debug is enabled)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@53280 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-pc/kernel/acpi_ioapic.c
+++ arch/all-pc/kernel/acpi_ioapic.c
@@ -16,8 +16,10 @@
 
 #include "acpi.h"
 #include "apic.h"
+#include "ioapic.h"
 
 #define D(x)
+#define DINTR(x)
 
 #define ACPI_MODPRIO_IOAPIC       50
 
@@ -27,6 +29,60 @@
 
 const char *ACPI_TABLE_MADT_STR __attribute__((weak)) = "APIC";
 
+#define IOREGSEL        0
+#define IOREGWIN        10
+
+ ULONG acpi_IOAPIC_ReadReg(APTR apic_base, UBYTE offset)
+ {
+     *(volatile ULONG *)(apic_base + IOREGSEL) = offset;
+     return *(volatile ULONG *)(apic_base + IOREGWIN);
+ }
+
+void acpi_IOAPIC_WriteReg(APTR apic_base, UBYTE offset, ULONG val)
+ {
+     *(volatile ULONG *)(apic_base + IOREGSEL) = offset;
+     *(volatile ULONG *)(apic_base + IOREGWIN) = val;
+ }
+
+ void acpi_IOAPIC_AllocPrivate(struct PlatformData *pdata)
+{
+    if (!pdata->kb_IOAPIC)
+    {
+        pdata->kb_IOAPIC = AllocMem(sizeof(struct IOAPICData) + pdata->kb_ACPI->acpi_ioapicCnt * sizeof(struct IOAPICCfgData), MEMF_CLEAR);
+        D(bug("[Kernel:ACPI-IOAPIC] IO-APIC Private @ 0x%p, for %d IOAPIC's\n", pdata->kb_IOAPIC, pdata->kb_ACPI->acpi_ioapicCnt));
+    }
+}
+ 
+/* Process the 'Interrupt Source' MADT Table */
+AROS_UFH2(IPTR, ACPI_hook_Table_Int_Src_Parse,
+	  AROS_UFHA(struct Hook *, table_hook, A0),
+	  AROS_UFHA(ACPI_MADT_INTERRUPT_SOURCE *, intsrc, A2))
+{
+    AROS_USERFUNC_INIT
+
+    DINTR(bug("[Kernel:ACPI-IOAPIC] ## %s()\n", __func__));
+    DINTR(bug("[Kernel:ACPI-IOAPIC]    %s: %d:%d, GSI %d, Flags 0x%x\n", __func__, intsrc->Id, intsrc->Eid,
+                intsrc->GlobalIrq, intsrc->IntiFlags));
+    DINTR(
+        if (intsrc->Type == 1)
+        {
+            bug("[Kernel:ACPI-IOAPIC]    %s: PMI, vector %d\n", __func__, intsrc->IoSapicVector);
+        }
+        else if(intsrc->Type == 2)
+        {
+            bug("[Kernel:ACPI-IOAPIC]    %s: INIT\n", __func__);
+        }
+        else if(intsrc->Type == 3)
+        {
+            bug("[Kernel:ACPI-IOAPIC]    %s: corrected\n", __func__);
+        }
+
+    )
+    return TRUE;
+
+    AROS_USERFUNC_EXIT
+}
+
 /* Process the 'Interrupt Source Overide' MADT Table */
 AROS_UFH2(IPTR, ACPI_hook_Table_Int_Src_Ovr_Parse,
 	  AROS_UFHA(struct Hook *, table_hook, A0),
@@ -34,7 +90,8 @@ AROS_UFH2(IPTR, ACPI_hook_Table_Int_Src_Ovr_Parse,
 {
     AROS_USERFUNC_INIT
 
-    D(bug("[Kernel:ACPI-IOAPIC] (HOOK) ACPI_hook_Table_Int_Src_Ovr_Parse: Bus %d, Source IRQ %d, Global IRQ %d, Flags 0x%x\n", intsrc->Bus, intsrc->SourceIrq,
+    DINTR(bug("[Kernel:ACPI-IOAPIC] ## %s()\n", __func__));
+    DINTR(bug("[Kernel:ACPI-IOAPIC]    %s: Bus %d, Source IRQ %d, GSI %d, Flags 0x%x\n", __func__, intsrc->Bus, intsrc->SourceIrq,
                 intsrc->GlobalIrq, intsrc->IntiFlags));
 
     return TRUE;
@@ -49,7 +106,8 @@ AROS_UFH2(IPTR, ACPI_hook_Table_NMI_Src_Parse,
 {
     AROS_USERFUNC_INIT
 
-    D(bug("[Kernel:ACPI-IOAPIC] (HOOK) ACPI_hook_Table_NMI_Src_Parse()\n"));
+    DINTR(bug("[Kernel:ACPI-IOAPIC] ## %s()\n", __func__));
+    DINTR(bug("[Kernel:ACPI-IOAPIC]    %s: GSI %d, Flags 0x%x\n", __func__, nmi_src->GlobalIrq, nmi_src->IntiFlags));
 
     /* FIXME: Uh... shouldn't we do something with this? */
 
@@ -66,22 +124,158 @@ AROS_UFH3(IPTR, ACPI_hook_Table_IOAPIC_Parse,
 {
     AROS_USERFUNC_INIT
 
-    struct ACPI_TABLE_HOOK *scanHook;
     struct PlatformData *pdata = tsdata->acpits_UserData;
 
-    D(bug("[Kernel:ACPI-IOAPIC] (HOOK) ACPI_hook_Table_IOAPIC_Parse: IOAPIC %d @ %p [irq base = %d]\n", ioapic->Id, ioapic->Address, ioapic->GlobalIrqBase));
+    D(bug("[Kernel:ACPI-IOAPIC] ## %s()\n", __func__));
 
-    if (!pdata->kb_ACPI->acpi_ioapicBase)
+    if (!pdata->kb_IOAPIC)
     {
-        pdata->kb_ACPI->acpi_ioapicBase = ioapic->Address;
+        acpi_IOAPIC_AllocPrivate(pdata);
+    }
+
+    if (pdata->kb_IOAPIC)
+    {
+        ULONG ioapicval;
+        int i;
+
+        bug("[Kernel:ACPI-IOAPIC] Registering IO-APIC #%d [ID=0x%d] @ %p [GSI = %d]\n",
+            pdata->kb_IOAPIC->ioapic_count, ioapic->Id, ioapic->Address, ioapic->GlobalIrqBase);
+
+        pdata->kb_IOAPIC->ioapics[pdata->kb_IOAPIC->ioapic_count].ioapicBase = (APTR)0 + ioapic->Address;
+        pdata->kb_IOAPIC->ioapics[pdata->kb_IOAPIC->ioapic_count].ioapicGSI = ioapic->GlobalIrqBase;
+
+        ioapicval = acpi_IOAPIC_ReadReg(
+            pdata->kb_IOAPIC->ioapics[pdata->kb_IOAPIC->ioapic_count].ioapicBase,
+            IOAPICREG_ID);
+        pdata->kb_IOAPIC->ioapics[pdata->kb_IOAPIC->ioapic_count].ioapicID = ((ioapicval >> 24) & 0xF);
+
+        D(bug("[Kernel:ACPI-IOAPIC]    %s:       #%d,",
+            __func__, pdata->kb_IOAPIC->ioapics[pdata->kb_IOAPIC->ioapic_count].ioapicID));
+        ioapicval = acpi_IOAPIC_ReadReg(
+            pdata->kb_IOAPIC->ioapics[pdata->kb_IOAPIC->ioapic_count].ioapicBase,
+            IOAPICREG_VER);
+        pdata->kb_IOAPIC->ioapics[pdata->kb_IOAPIC->ioapic_count].ioapicIRQs = ((ioapicval >> 16) & 0xF);
+        pdata->kb_IOAPIC->ioapics[pdata->kb_IOAPIC->ioapic_count].ioapicVer = (ioapicval & 0xFF);
+        D(bug(" ver %d, max irqs = %d,",
+            pdata->kb_IOAPIC->ioapics[pdata->kb_IOAPIC->ioapic_count].ioapicVer, pdata->kb_IOAPIC->ioapics[pdata->kb_IOAPIC->ioapic_count].ioapicIRQs));
+        ioapicval = acpi_IOAPIC_ReadReg(
+            pdata->kb_IOAPIC->ioapics[pdata->kb_IOAPIC->ioapic_count].ioapicBase,
+            IOAPICREG_ARB);
+        D(bug("arb %d\n", ((ioapicval >> 24) & 0xF)));
+
+        for (i = 0; i < pdata->kb_IOAPIC->ioapics[pdata->kb_IOAPIC->ioapic_count].ioapicIRQs; i++)
+        {
+            UQUAD tblentry;
+
+            ioapicval = acpi_IOAPIC_ReadReg(
+                pdata->kb_IOAPIC->ioapics[pdata->kb_IOAPIC->ioapic_count].ioapicBase,
+                IOAPICREG_REDTBLBASE + i);
+            tblentry = ((UQUAD)ioapicval << 32);
+            D(bug("[Kernel:ACPI-IOAPIC]    %s:       %08x", __func__, ioapicval));
+            ioapicval = acpi_IOAPIC_ReadReg(
+                pdata->kb_IOAPIC->ioapics[pdata->kb_IOAPIC->ioapic_count].ioapicBase,
+                IOAPICREG_REDTBLBASE + i + 1);
+            tblentry |= ioapicval;
+            D(bug("%08x", ioapicval));
+            if (tblentry)
+            {
+                D(bug(" :"));
+                if (tblentry & (1 << 13))
+                {
+                    D(bug(" Active LOW,"));
+                }
+                else
+                {
+                    D(bug(" Active HIGH,"));
+                }
+                
+                if (tblentry & (1 << 15))
+                {
+                    D(bug(" LEVEL"));
+                }
+                else
+                {
+                    D(bug(" EDGE"));
+                }
+
+                D(bug(" ->"));
+
+                if (tblentry & (1 << 11))
+                {
+                    D(bug(" Logical %d:%d", ((tblentry >> 60) & 0xF), ((tblentry >> 56) & 0xF)));
+                }
+                else
+                {
+                    D(bug(" Physical %d", ((tblentry >> 56) & 0xF)));
+                }
+            }
+            D(bug("\n"));
+        }
+
+        /* Build a default routing table for legacy (ISA) interrupts. */
+	/* TODO: implement legacy irq config.. */
+	D(bug("[Kernel:ACPI-IOAPIC]    %s: Configuring Legacy IRQs .. Skipped (UNIMPLEMENTED) ..\n", __func__));
+
+        pdata->kb_IOAPIC->ioapic_count++;
+    }
+    return TRUE;
+
+    AROS_USERFUNC_EXIT
+}
+
+/*
+ * Process the 'IO-APIC' MADT Table
+ * This function counts the available IO-APICs.
+ */
+AROS_UFH3(static IPTR, ACPI_hook_Table_IOAPIC_Count,
+	  AROS_UFHA(struct Hook *, table_hook, A0),
+	  AROS_UFHA(ACPI_MADT_IO_APIC *, ioapic, A2),
+	  AROS_UFHA(struct ACPI_TABLESCAN_DATA *, tsdata, A1))
+{
+    AROS_USERFUNC_INIT
+
+    struct PlatformData *pdata = tsdata->acpits_UserData;
+    struct ACPI_TABLE_HOOK *scanHook;
+
+    D(bug("[Kernel:ACPI-IOAPIC] ## %s()\n", __func__));
+
+    if (pdata->kb_ACPI->acpi_ioapicCnt == 0)
+    {
+        D(bug("[Kernel:ACPI-IOAPIC]    %s: Registering IO-APIC Table Parser...\n", __func__));
 
         scanHook = (struct ACPI_TABLE_HOOK *)AllocMem(sizeof(struct ACPI_TABLE_HOOK), MEMF_CLEAR);
         if (scanHook)
         {
-            D(bug("[Kernel:ACPI-IOAPIC] Registering Interrupt Override Table Parser...\n"));
-
             scanHook->acpith_Node.ln_Name = (char *)ACPI_TABLE_MADT_STR;
             scanHook->acpith_Node.ln_Pri = ACPI_MODPRIO_IOAPIC - 10;                            /* Queue 10 priority levels after the module parser */
+            scanHook->acpith_Hook.h_Entry = (APTR)ACPI_hook_Table_IOAPIC_Parse;
+            scanHook->acpith_HeaderLen = sizeof(ACPI_TABLE_MADT);
+            scanHook->acpith_EntryType = ACPI_MADT_TYPE_IO_APIC;
+            scanHook->acpith_UserData = pdata;
+            Enqueue(&pdata->kb_ACPI->acpi_tablehooks, &scanHook->acpith_Node);
+        }
+
+        scanHook = (struct ACPI_TABLE_HOOK *)AllocMem(sizeof(struct ACPI_TABLE_HOOK), MEMF_CLEAR);
+        if (scanHook)
+        {
+            DINTR(bug("[Kernel:ACPI-IOAPIC]    %s: Registering Interrupt Source Table Parser...\n", __func__));
+
+            scanHook->acpith_Node.ln_Name = (char *)ACPI_TABLE_MADT_STR;
+            scanHook->acpith_Node.ln_Pri = ACPI_MODPRIO_IOAPIC - 20;                            /* Queue 20 priority levels after the module parser */
+            scanHook->acpith_Hook.h_Entry = (APTR)ACPI_hook_Table_Int_Src_Parse;
+            scanHook->acpith_HeaderLen = sizeof(ACPI_TABLE_MADT);
+            scanHook->acpith_EntryType = ACPI_MADT_TYPE_INTERRUPT_SOURCE;
+            scanHook->acpith_UserData = pdata;
+            Enqueue(&pdata->kb_ACPI->acpi_tablehooks, &scanHook->acpith_Node);
+        }
+        
+        scanHook = (struct ACPI_TABLE_HOOK *)AllocMem(sizeof(struct ACPI_TABLE_HOOK), MEMF_CLEAR);
+        if (scanHook)
+        {
+            DINTR(bug("[Kernel:ACPI-IOAPIC]    %s: Registering Interrupt Override Table Parser...\n", __func__));
+
+            scanHook->acpith_Node.ln_Name = (char *)ACPI_TABLE_MADT_STR;
+            scanHook->acpith_Node.ln_Pri = ACPI_MODPRIO_IOAPIC - 30;                            /* Queue 30 priority levels after the module parser */
             scanHook->acpith_Hook.h_Entry = (APTR)ACPI_hook_Table_Int_Src_Ovr_Parse;
             scanHook->acpith_HeaderLen = sizeof(ACPI_TABLE_MADT);
             scanHook->acpith_EntryType = ACPI_MADT_TYPE_INTERRUPT_OVERRIDE;
@@ -92,25 +286,24 @@ AROS_UFH3(IPTR, ACPI_hook_Table_IOAPIC_Parse,
         scanHook = (struct ACPI_TABLE_HOOK *)AllocMem(sizeof(struct ACPI_TABLE_HOOK), MEMF_CLEAR);
         if (scanHook)
         {
-            D(bug("[Kernel:ACPI-IOAPIC] Registering NMI Source Table Parser...\n"));
+            DINTR(bug("[Kernel:ACPI-IOAPIC]    %s: Registering NMI Source Table Parser...\n", __func__));
 
             scanHook->acpith_Node.ln_Name = (char *)ACPI_TABLE_MADT_STR;
-            scanHook->acpith_Node.ln_Pri = ACPI_MODPRIO_IOAPIC - 20;                            /* Queue 20 priority levels after the module parser */
+            scanHook->acpith_Node.ln_Pri = ACPI_MODPRIO_IOAPIC - 40;                            /* Queue 40 priority levels after the module parser */
             scanHook->acpith_Hook.h_Entry = (APTR)ACPI_hook_Table_NMI_Src_Parse;
             scanHook->acpith_HeaderLen = sizeof(ACPI_TABLE_MADT);
             scanHook->acpith_EntryType = ACPI_MADT_TYPE_NMI_SOURCE;
             scanHook->acpith_UserData = pdata;
             Enqueue(&pdata->kb_ACPI->acpi_tablehooks, &scanHook->acpith_Node);
         }
-
-        /* Build a default routing table for legacy (ISA) interrupts. */
-	/* TODO: implement legacy irq config.. */
-	D(bug("[Kernel:ACPI-IOAPIC] Configuring Legacy IRQs .. Skipped (UNIMPLEMENTED) ..\n"));
     }
+    pdata->kb_ACPI->acpi_ioapicCnt++;
+
     return TRUE;
 
     AROS_USERFUNC_EXIT
 }
+
 
 void ACPI_IOAPIC_SUPPORT(struct PlatformData *pdata)
 {
@@ -119,17 +312,17 @@ void ACPI_IOAPIC_SUPPORT(struct PlatformData *pdata)
     scanHook = (struct ACPI_TABLE_HOOK *)AllocMem(sizeof(struct ACPI_TABLE_HOOK), MEMF_CLEAR);
     if (scanHook)
     {
-        D(bug("[Kernel:ACPI-IOAPIC] Registering IOAPIC Table Parser...\n"));
+        D(bug("[Kernel:ACPI-IOAPIC] %s: Registering IOAPIC Table Parser...\n", __func__));
         D(bug("[Kernel:ACPI-IOAPIC] %s: Table Hook @ 0x%p\n", __func__, scanHook));
         scanHook->acpith_Node.ln_Name = (char *)ACPI_TABLE_MADT_STR;
         scanHook->acpith_Node.ln_Pri = ACPI_MODPRIO_IOAPIC;
-        scanHook->acpith_Hook.h_Entry = (APTR)ACPI_hook_Table_IOAPIC_Parse;
+        scanHook->acpith_Hook.h_Entry = (APTR)ACPI_hook_Table_IOAPIC_Count;
         scanHook->acpith_HeaderLen = sizeof(ACPI_TABLE_MADT);
         scanHook->acpith_EntryType = ACPI_MADT_TYPE_IO_APIC;
         scanHook->acpith_UserData = pdata;
         Enqueue(&pdata->kb_ACPI->acpi_tablehooks, &scanHook->acpith_Node);
     }
-    D(bug("[Kernel:ACPI-IOAPIC] Registering done\n"));
+    D(bug("[Kernel:ACPI-IOAPIC] %s: Registering done\n", __func__));
 }
 
 DECLARESET(KERNEL__ACPISUPPORT)
---
+++ arch/all-pc/kernel/ioapic.h
@@ -0,0 +1,34 @@
+#ifndef KERNEL_IOAPIC_H
+#define KERNEL_IOAPIC_H
+/*
+    Copyright © 2017, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: Generic AROS APIC definitions.
+    Lang: english
+*/
+
+#include <asm/cpu.h>
+#include <proto/acpica.h>
+
+#define IOAPICREG_ID            0
+#define IOAPICREG_VER           1
+#define IOAPICREG_ARB           2
+#define IOAPICREG_REDTBLBASE    10
+
+struct IOAPICCfgData
+{
+    APTR ioapicBase;
+    UBYTE ioapicID;
+    UBYTE ioapicVer;
+    UBYTE ioapicIRQs;
+    UBYTE ioapicGSI;
+};
+
+struct IOAPICData
+{
+    ULONG	                ioapic_count;
+    struct IOAPICCfgData        ioapics[0];	/* Per-IOAPIC data					*/
+};
+
+#endif /* KERNEL_IOAPIC_H */
