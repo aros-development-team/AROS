cbfe3053bbd99a1956c891b9b17011f6722ae13d
Kalamatee <kalamatee@gmail.com>
2017-01-27 03:42:11 +0000

    don't define KBL_INTERNAL anymore, just use the base version. add a define for the HW_IRQ_BASE used in the code, and notes about x86 exceptions/irqs.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@53328 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-pc/kernel/kernel_arch.h
+++ arch/all-pc/kernel/kernel_arch.h
@@ -4,10 +4,34 @@
 /* Machine-specific definitions for IBM PC hardware */
 
 /* XT-PIC only has 16 IRQs */
-#define IRQ_COUNT 16
+#define IRQ_COUNT       16
 
-/* We have several types of IRQ handlers and distinguish them by node type */
-#define KBL_INTERNAL 0
+/*
+ *  The first Hardware IRQ starts at 32
+ *  (0 - 31 are cpu exceptions, see below..)
+ */
+#define HW_IRQ_BASE     32
+/*
+    0 - Divide Error
+    1 - Debug
+    2 - NMI
+    3 - Breakpoint
+    4 - Overflow
+    5 - Bound Range Exceeded
+    6 - Invalid Opcode
+    7 - Device Not Available
+    8 - Double Fault
+    9 - Coprocessor Segment Overrun
+    10 - Invalid TSS
+    11 - Segment Not Present
+    12 - Stack Fault
+    13 - GPF
+    14 - Page Fault
+    16 - x87 FPU Floating Point Error
+    17 - Alignment Check
+    18 - Machine-Check
+    19 - SIMD Floating-Point
+*/
 
 /* Interrupt controller functions */
 void ictl_enable_irq(unsigned char irq, struct KernelBase *KernelBase);
