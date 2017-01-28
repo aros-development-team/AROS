c77ac65a2982e8dc1d1e4f4acb31d617e55e396b
Kalamatee <kalamatee@gmail.com>
2017-01-28 05:24:04 +0000

    test allocbitmap
    git-svn-id: https://svn.aros.org/svn/aros/trunk@53349 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/graphics/allocbitmap.c
@@ -0,0 +1,33 @@
+/*
+    Copyright © 2016-2017, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <aros/debug.h>
+#include <proto/graphics.h>
+
+int main(int argc, char **argv)
+{
+    struct BitMap *bitmap = NULL;
+    ULONG flags = BMF_CLEAR | BMF_MINPLANES;
+    UBYTE depth;
+
+    for (depth = 1; depth < 33; depth ++)
+    {
+        if ((depth > 8) &&
+            ((depth != 15)  || (depth != 16) || (depth != 24) || (depth != 32)))
+            continue;
+
+        if ((bitmap = AllocBitMap((44 << 3), 220, depth, flags, NULL )) != NULL)
+        {
+            bug("%dbit BitMap @ 0x%p\n", depth, bitmap);
+            FreeBitMap(bitmap);
+            bug("Freed\n");
+        }
+        else
+        {
+            bug("Failed to allocate %dbit BitMap\n", depth);
+        }
+    }
+    return 0;
+}
