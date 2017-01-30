478a339972bf53754a3b5617f1a2f7545dbf73f3
Kalamatee <kalamatee@gmail.com>
2017-01-30 04:34:49 +0000

    add a little autodoc entry about the function.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@53391 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/posixc/sigsuspend.c
+++ compiler/posixc/sigsuspend.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2013, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2017, The AROS Development Team. All rights reserved.
     $Id$
 
     POSIX.1-2008 function sigsuspend().
@@ -20,27 +20,52 @@
 	const sigset_t *mask)
 
 /*  FUNCTION
+        replace the callers signal mask, and suspend it
+        until it signaled to terminate, or to invoke a
+        signal handler.
+
+        If the signal terminates the process, sigsuspend()
+        doesn't return.
+
+        If the signal is caught, sigsuspend() returns following the
+        signal handler, and the signal mask is restored to
+        the state prior to calling sigsuspend(). 
+
+        SIGKILL or SIGSTOP cannot be blocked; specifying
+        them in the mask has no effect on the process's signal mask. 
 
     INPUTS
 
     RESULT
+        always returns -1, normally with the error EINTR.
 
     NOTES
         Not implemented.
+
+        Normally used in conjunction with sigprocmask(), to prevent
+        signal delivery during critical code sections. Callers must 
+        block the signals with sigprocmask(). On completion, the caller
+        waits for signals by calling sigsuspend() with the return value
+        of sigprocmask()
 
     EXAMPLE
 
     BUGS
 
     SEE ALSO
+        kill, pause, sigaction, signal, sigprocmask,
+        sigwaitinfo, sigsetops, sigwait, signal
 
     INTERNALS
+        POSIX.1-2001
 
 ******************************************************************************/
 {
     /* TODO: Implement sigsuspend() */
     AROS_FUNCTION_NOT_IMPLEMENTED("posixc");
+
     errno = ENOSYS;
 
     return -1;
+
 } /* sigsuspend */
