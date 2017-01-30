5f7bd14276507af23eacc22760cfc2951ae2fe4c
Kalamatee <kalamatee@gmail.com>
2017-01-30 07:36:58 +0000

    doh .. fix the glaring typo.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@53398 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/hidds/base/system/system_init.c
+++ rom/hidds/base/system/system_init.c
@@ -19,37 +19,37 @@
 
 #include LC_LIBDEFS_FILE
 
-static int Systsem_Init(LIBBASETYPEPTR LIBBASE)
+static int System_Init(LIBBASETYPEPTR LIBBASE)
 {
     struct class_static_data *csd = &LIBBASE->hsi_csd;
     struct Library *OOPBase = csd->cs_OOPBase;
 
-    D(bug("[HiddSystsem] %s()\n", __PRETTY_FUNCTION__));
+    D(bug("[HiddSystem] %s()\n", __PRETTY_FUNCTION__));
 
     OOP_Object *hwroot = OOP_NewObject(NULL, CLID_HW_Root, NULL);
     csd->hwAttrBase = OOP_ObtainAttrBase(IID_HW);
 
     if (HW_AddDriver(hwroot, csd->oopclass, NULL))
     {
-        D(bug("[HiddSystsem] %s: initialised\n", __PRETTY_FUNCTION__));
+        D(bug("[HiddSystem] %s: initialised\n", __PRETTY_FUNCTION__));
         return TRUE;
     }
-    D(bug("[HiddSystsem] %s: failed\n", __PRETTY_FUNCTION__));
+    D(bug("[HiddSystem] %s: failed\n", __PRETTY_FUNCTION__));
     
     return FALSE;
 }
 
-static int Systsem_Expunge(LIBBASETYPEPTR LIBBASE)
+static int System_Expunge(LIBBASETYPEPTR LIBBASE)
 {
     D(struct class_static_data *csd = &LIBBASE->hsi_csd;)
 #if (0)
     struct Library *OOPBase = csd->cs_OOPBase;
 #endif
     
-    D(bug("[HiddSystsem] %s(csd=%p)\n", __PRETTY_FUNCTION__, csd));
+    D(bug("[HiddSystem] %s(csd=%p)\n", __PRETTY_FUNCTION__, csd));
     
     return TRUE;
 }
 
-ADD2INITLIB(Systsem_Init, -2)
-ADD2EXPUNGELIB(Systsem_Expunge, -2)
+ADD2INITLIB(System_Init, -2)
+ADD2EXPUNGELIB(System_Expunge, -2)
