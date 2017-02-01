1a35ea5759ec44efaf631cab2c461be2ccd4edc2
Kalamatee <kalamatee@gmail.com>
2017-02-01 03:47:33 +0000

    update copyright
    git-svn-id: https://svn.aros.org/svn/aros/trunk@53420 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/kernel/kernel_intr.c
+++ rom/kernel/kernel_intr.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 2011-2015, The AROS Development Team. All rights reserved.
+    Copyright © 2011-2017, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: High-level scheduler calling code
