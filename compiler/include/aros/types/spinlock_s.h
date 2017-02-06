ad2a28c862911080ce7bf4c6b26fa6208273cd27
Kalamatee <kalamatee@gmail.com>
2017-02-06 18:17:31 +0000

    add a few spinlock related defines
    git-svn-id: https://svn.aros.org/svn/aros/trunk@53509 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/aros/types/spinlock_s.h
+++ compiler/include/aros/types/spinlock_s.h
@@ -7,20 +7,24 @@ typedef struct {
     union
     {
         volatile struct {
-            unsigned int _pad1 : 4;
-            unsigned int write : 1;
-            unsigned int _pad2 : 3;
-            unsigned int readcount : 24;
+            unsigned int        _pad1 : 4;
+            unsigned int        write : 1;
+            unsigned int        _pad2 : 3;
+            unsigned int        readcount : 24;
         } slock;
-        volatile unsigned long lock;
+        volatile unsigned long  lock;
     };
 } spinlock_t;
 
-#define SPINLOCK_INIT_UNLOCKED  { 0 }
-#define SPINLOCK_INIT_WRITE_LOCKED  { 0x80000000 }
-#define SPINLOCK_INIT_READ_LOCKED(n) { n }
+#define SPINLOCK_UNLOCKED               0
+#define SPINLOCKB_WRITE                 27
+#define SPINLOCKF_WRITE                 (1 << SPINLOCKB_WRITE)
 
-#define SPINLOCK_MODE_READ  0
-#define SPINLOCK_MODE_WRITE 1
+#define SPINLOCK_INIT_UNLOCKED          { SPINLOCK_UNLOCKED }
+#define SPINLOCK_INIT_WRITE_LOCKED      { SPINLOCKF_WRITE }
+#define SPINLOCK_INIT_READ_LOCKED(n)    { n }
+
+#define SPINLOCK_MODE_READ              0
+#define SPINLOCK_MODE_WRITE             1
 
 #endif /* ! _AROS_TYPES_SPINLOCK_S_H_ */
