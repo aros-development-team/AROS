185cd204570c1ae87b0de41395ee2e932db0fcec
Kalamatee <kalamatee@gmail.com>
2017-02-11 02:32:34 +0000

    # use the correct mask size. # if an IRQ has been enabled, make sure we enable it when we init the controller. # use the correct mask to init the controller. # prevent anything disabling IRQ 2 for now. # make sure we are at prio 0 so we come before the APIC but after IOAPIC controllers.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@53575 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-pc/kernel/pic_i8259a.c
+++ arch/all-pc/kernel/pic_i8259a.c
@@ -17,10 +17,11 @@
 #include "i8259a.h"
 
 #define D(x)
+#define DINT(x)
 
 struct i8259a_Instance
 {
-    ULONG       irq_mask;
+    UWORD       irq_mask;
     UBYTE       irq_base;
 };
 
@@ -31,7 +32,7 @@ struct i8259a_Private
 
 icid_t i8259a_Register(struct KernelBase *KernelBase)
 {
-    D(bug("[Kernel:i8259a] %s()\n", __func__));
+    DINT(bug("[Kernel:i8259a] %s()\n", __func__));
 
     /* if we have been disabled, fail to register */
     if (i8259a_IntrController.ic_Flags & ICF_DISABLED)
@@ -42,73 +43,12 @@ icid_t i8259a_Register(struct KernelBase *KernelBase)
     return (icid_t)i8259a_IntrController.ic_Node.ln_Type;
 }
 
-BOOL i8259a_Init(struct KernelBase *KernelBase, icid_t instanceCount)
-{
-    struct i8259a_Private *xtpicPriv;
-    struct i8259a_Instance *xtPic;
-    int i;
-
-    D(bug("[Kernel:i8259a] %s(%d)\n", __func__, instanceCount));
-
-    /* sanity check .. */
-    if (i8259a_IntrController.ic_Flags & ICF_DISABLED)
-        return FALSE;
-
-    xtpicPriv = (struct i8259a_Private *)AllocMem(sizeof(struct i8259a_Instance) * instanceCount, MEMF_ANY);
-    if ((i8259a_IntrController.ic_Private = xtpicPriv) != NULL)
-    {
-        /* Take over the 8259a IRQ's */
-        for (i = 0; i < I8259A_IRQCOUNT; i++)
-        {
-            if (!krnInitInterrupt(KernelBase, i, i8259a_IntrController.ic_Node.ln_Type, 0))
-            {
-                bug("[Kernel:i8259a] %s: failed to acquire IRQ #%d\n", __func__, i);
-            }
-        }
-
-        /*
-         * After initialization everything is disabled except cascade interrupt (IRQ 2).
-         * Be careful and don't miss this! Without IRQ2 enabled slave 8259 (and its IRQs)
-         * will not work!
-         */
-
-        /* Setup Masks */
-        for (i = 0; i < instanceCount; i++)
-        {
-            xtPic = (struct i8259a_Instance *)&xtpicPriv->irq_ic[i];
-            xtPic->irq_mask = 0xFFFB;
-            xtPic->irq_base = HW_IRQ_BASE;               /* route irqs after the cpu's exceptions */
-        }
-
-        /* Setup the first registered 8259. Send four ICWs (see 8529 datasheet) */
-        xtPic = (struct i8259a_Instance *)&xtpicPriv->irq_ic[0];
-        asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x11),"i"(MASTER8259_CMDREG)); /* Initialization sequence for 8259A-1 (edge-triggered, cascaded, ICW4 needed) */
-        asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x11),"i"(SLAVE8259_CMDREG)); /* Initialization sequence for 8259A-2, the same as above */
-        asm("outb   %b0,%b1\n\tcall delay"::"a"(xtPic->irq_base),"i"(MASTER8259_MASKREG)); /* IRQs for master */
-        asm("outb   %b0,%b1\n\tcall delay"::"a"(xtPic->irq_base + 8),"i"(SLAVE8259_MASKREG)); /* IRQs for slave */
-        asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x04),"i"(MASTER8259_MASKREG)); /* 8259A-1 is master, slave at IRQ2 */
-        asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x02),"i"(SLAVE8259_MASKREG)); /* 8259A-2 is slave, ID = 2 */
-        asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x01),"i"(MASTER8259_MASKREG)); /* 8086 mode, non-buffered, nonspecial fully nested mode for both */
-        asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x01),"i"(SLAVE8259_MASKREG));
-
-        /* Now initialize interrupt masks */
-        asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0xfb),"i"(MASTER8259_MASKREG)); /* Enable cascade int */
-        asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0xff),"i"(SLAVE8259_MASKREG)); /* Mask all interrupts */
-
-        return TRUE;
-    }
-    return FALSE;
-}
-
-/* Small delay routine used by i8259a_Init() initializer */
-asm("\ndelay:\t.short   0x00eb\n\tret");
-
 BOOL i8259a_DisableIRQ(APTR icPrivate, icid_t icInstance, icid_t intNum)
 {
     struct i8259a_Private *xtpicPriv= (struct i8259a_Private *)icPrivate;
     struct i8259a_Instance *xtPic = (struct i8259a_Instance *)&xtpicPriv->irq_ic[icInstance];
 
-    D(bug("[Kernel:i8259a] %s(0x%p, %d, %d)\n", __func__, icPrivate, icInstance, intNum));
+    DINT(bug("[Kernel:i8259a] %s(0x%p, %d, %d)\n", __func__, icPrivate, icInstance, intNum));
 
     intNum &= 15;
 
@@ -117,7 +57,6 @@ BOOL i8259a_DisableIRQ(APTR icPrivate, icid_t icInstance, icid_t intNum)
     	/* IRQ2 must never be disabled. Doing so breaks communication between two 8259's */
     	return FALSE;
     }
-
     xtPic->irq_mask |= 1 << intNum;
 
     if (intNum >= 8)
@@ -133,9 +72,15 @@ BOOL i8259a_EnableIRQ(APTR icPrivate, icid_t icInstance, icid_t intNum) // uint1
     struct i8259a_Private *xtpicPriv= (struct i8259a_Private *)icPrivate;
     struct i8259a_Instance *xtPic = (struct i8259a_Instance *)&xtpicPriv->irq_ic[icInstance];
 
-    D(bug("[Kernel:i8259a] %s(0x%p, %d, %d)\n", __func__, icPrivate, icInstance, intNum));
+    DINT(bug("[Kernel:i8259a] %s(0x%p, %d, %d)\n", __func__, icPrivate, icInstance, intNum));
 
     intNum &= 15;
+
+    if (intNum == 2)
+    {
+    	/* IRQ2 must never be disabled. Doing so breaks communication between two 8259's */
+    	return FALSE;
+    }
     xtPic->irq_mask &= ~(1 << intNum);    
 
     if (intNum >= 8)
@@ -155,7 +100,7 @@ BOOL i8259a_AckIntr(APTR icPrivate, icid_t icInstance, icid_t intNum) // uint16_
     struct i8259a_Private *xtpicPriv= (struct i8259a_Private *)icPrivate;
     struct i8259a_Instance *xtPic = (struct i8259a_Instance *)&xtpicPriv->irq_ic[icInstance];
 
-    D(bug("[Kernel:i8259a] %s(0x%p, %d, %d)\n", __func__, icPrivate, icInstance, intNum));
+    DINT(bug("[Kernel:i8259a] %s(0x%p, %d, %d)\n", __func__, icPrivate, icInstance, intNum));
 
     intNum &= 15;
     xtPic->irq_mask |= 1 << intNum;
@@ -175,10 +120,91 @@ BOOL i8259a_AckIntr(APTR icPrivate, icid_t icInstance, icid_t intNum) // uint16_
     return TRUE;
 }
 
+
+BOOL i8259a_Init(struct KernelBase *KernelBase, icid_t instanceCount)
+{
+    struct i8259a_Private *xtpicPriv;
+    struct i8259a_Instance *xtPic;
+    int instance, irq;
+
+    DINT(bug("[Kernel:i8259a] %s(%d)\n", __func__, instanceCount));
+
+    /* sanity check .. */
+    if (i8259a_IntrController.ic_Flags & ICF_DISABLED)
+        return FALSE;
+
+    xtpicPriv = (struct i8259a_Private *)AllocMem(sizeof(struct i8259a_Instance) * instanceCount, MEMF_ANY);
+    if ((i8259a_IntrController.ic_Private = xtpicPriv) != NULL)
+    {
+        /*
+         * After initialization everything is disabled except cascade interrupt (IRQ 2).
+         * Be careful and don't miss this! Without IRQ2 enabled slave 8259 (and its IRQs)
+         * will not work!
+         */
+
+        /* Setup Masks */
+        for (instance = 0; instance < instanceCount; instance++)
+        {
+            int instIRQBase = (instance * I8259A_IRQCOUNT);
+
+            DINT(bug("[Kernel:i8259a] %s: initializing instance #%d\n", __func__, instance));
+
+            xtPic = (struct i8259a_Instance *)&xtpicPriv->irq_ic[instance];
+            xtPic->irq_mask = 0xFFFF;
+            xtPic->irq_base = HW_IRQ_BASE + instIRQBase;               /* route irqs after the cpu's exceptions */
+            if (instance == 0)
+            {
+                /* Take over the first 8259a IRQ's */
+                for (irq = instIRQBase; irq < (instIRQBase + I8259A_IRQCOUNT); irq++)
+                {
+                    if (!krnInitInterrupt(KernelBase, irq, i8259a_IntrController.ic_Node.ln_Type, instance))
+                    {
+                        bug("[Kernel:i8259a] %s: failed to acquire IRQ #%d\n", __func__, irq);
+                    }
+                    else
+                    {
+                        if ((irq - instIRQBase) != 2)
+                        {
+                            if (ictl_is_irq_enabled(irq, KernelBase))
+                                xtPic->irq_mask &= ~(1 << (irq - instIRQBase));
+                            else
+                                xtPic->irq_mask |= (1 << (irq - instIRQBase));
+                        }
+                        else
+                            xtPic->irq_mask &= ~(1 << (irq - instIRQBase));
+                    }
+                }
+
+                /* Setup the first registered 8259. Send four ICWs (see 8529 datasheet) */
+                asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x11),"i"(MASTER8259_CMDREG)); /* Initialization sequence for 8259A-1 (edge-triggered, cascaded, ICW4 needed) */
+                asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x11),"i"(SLAVE8259_CMDREG)); /* Initialization sequence for 8259A-2, the same as above */
+                asm("outb   %b0,%b1\n\tcall delay"::"a"(xtPic->irq_base),"i"(MASTER8259_MASKREG)); /* IRQs for master */
+                asm("outb   %b0,%b1\n\tcall delay"::"a"(xtPic->irq_base + 8),"i"(SLAVE8259_MASKREG)); /* IRQs for slave */
+                asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x04),"i"(MASTER8259_MASKREG)); /* 8259A-1 is master, slave at IRQ2 */
+                asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x02),"i"(SLAVE8259_MASKREG)); /* 8259A-2 is slave, ID = 2 */
+                asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x01),"i"(MASTER8259_MASKREG)); /* 8086 mode, non-buffered, nonspecial fully nested mode for both */
+                asm("outb   %b0,%b1\n\tcall delay"::"a"((char)0x01),"i"(SLAVE8259_MASKREG));
+
+                /* Now initialize interrupt masks */
+                asm("outb   %b0,%b1\n\tcall delay"::"a"((char)(xtPic->irq_mask & 0xFF)),"i"(MASTER8259_MASKREG)); /* Enable cascade int */
+                asm("outb   %b0,%b1\n\tcall delay"::"a"((char)(xtPic->irq_mask > 8)),"i"(SLAVE8259_MASKREG)); /* Mask all interrupts */
+            }
+        }
+        DINT(bug("[Kernel:i8259a] %s: complete\n", __func__));
+        return TRUE;
+    }
+    bug("[Kernel:i8259a] Failed to allocate controller storage!\n");
+    return FALSE;
+}
+
+/* Small delay routine used by i8259a_Init() initializer */
+asm("\ndelay:\t.short   0x00eb\n\tret");
+
 struct IntrController i8259a_IntrController =
 {
     {
-        .ln_Name = "8259A PIC"
+        .ln_Name = "8259A PIC",
+        .ln_Pri = 0
     },
     ICTYPE_I8259A,
     0,
