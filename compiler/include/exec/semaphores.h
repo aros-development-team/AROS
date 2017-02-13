97b641650cff3b3794eeceb660f7edef1f13efeb
Kalamatee <kalamatee@gmail.com>
2017-02-13 16:18:11 +0000

    update copyright
    git-svn-id: https://svn.aros.org/svn/aros/trunk@53622 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/exec/semaphores.h
+++ compiler/include/exec/semaphores.h
@@ -2,7 +2,7 @@
 #define EXEC_SEMAPHORES_H
 
 /*
-    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2017, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: Semaphore handling
