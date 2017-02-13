e6a38620628c19e225a8ada52f68ba744f906703
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2017-02-13 22:51:59 +0000

    atomic shared lock implemented
    git-svn-id: https://svn.aros.org/svn/aros/trunk@53631 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-pc/kernel/spinlock.c
+++ arch/all-pc/kernel/spinlock.c
@@ -49,8 +49,8 @@ static inline int lock_btsl(spinlock_t *lock)
 
 static inline int lock_cmpxchg(spinlock_t *lock, ULONG *err)
 {
-    uint8_t retval;
-    uint32_t ret;
+    UBYTE retval;
+    ULONG ret;
     asm volatile("lock cmpxchg %4, %0; setz %1"
         :"+m"(lock->lock),"=r"(retval),"=a"(ret)
         :"2"(SPINLOCK_UNLOCKED),"r"(SPINLOCKF_WRITE)
@@ -59,6 +59,20 @@ static inline int lock_cmpxchg(spinlock_t *lock, ULONG *err)
     *err = ret;
     return retval;
 }
+
+static inline int lock_cmpxchg_b(spinlock_t *lock, UBYTE *err)
+{
+    UBYTE retval;
+    UBYTE ret;
+    asm volatile("lock cmpxchgb %b4, %0; setz %1"
+        :"+m"(lock->block[3]),"=r"(retval),"=a"(ret)
+        :"2"(0),"d"(SPINLOCKF_UPDATING >> 24)
+        :"memory"
+    );
+    *err = ret;
+    return retval;
+}
+
 AROS_LH3(spinlock_t *, KrnSpinLock,
 	AROS_LHA(spinlock_t *, lock, A1),
 	AROS_LHA(struct Hook *, failhook, A0),
@@ -93,8 +107,18 @@ AROS_LH3(spinlock_t *, KrnSpinLock,
     }
     else
     {
-        while (lock->lock & SPINLOCKF_WRITE)
+        UBYTE tmp;
+        /*
+        Check if upper 8 bits of lock->lock are all 0, which means spinlock is not in UPDATING state and it is not
+        in the WRITE state. If we manage to obtain it, we set the UPDATING flag. Until we release UPDATING state
+        we are free to do whatever we want with the spinlock
+        */
+        while (!lock_cmpxchg_b(lock, &tmp))
         {
+            // Tell CPU we are spinning
+            asm volatile("pause");
+
+            // Call fail hook if available
             if (failhook)
             {
                 D(bug("[Kernel] %s: write-locked .. calling fail hook @ 0x%p ...\n", __func__, failhook);)
@@ -102,11 +126,12 @@ AROS_LH3(spinlock_t *, KrnSpinLock,
             }
             D(bug("[Kernel] %s: spinning on write lock ...\n", __func__);)
         };
-#if defined(AROS_NO_ATOMIC_OPERATIONS)
+        /*
+        At this point we have the spinlock in UPDATING state. So, update readcount field (no worry with atomic add,
+        spinlock is for our exclusive use here), and then release it just by setting updating flag to 0
+        */
         lock->slock.readcount++;
-#else
-        __AROS_ATOMIC_INC_L(lock->lock);
-#endif
+        lock->slock.updating = 0;
     }
 
     D(bug("[Kernel] %s: lock = %08x\n", __func__, lock->lock));
