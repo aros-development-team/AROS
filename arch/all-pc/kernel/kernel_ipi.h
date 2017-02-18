ecd9d807afba122d3fd2cad16fbfaf4ba107ad6c
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2017-02-18 21:46:08 +0000

    if IPI irq is send, pass it to IPI handler. Also added initial IPI code
    git-svn-id: https://svn.aros.org/svn/aros/trunk@53751 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/all-pc/kernel/kernel_ipi.h
@@ -0,0 +1,23 @@
+/*
+    Copyright © 1995-2017, The AROS Development Team. All rights reserved.
+    $Id:$
+*/
+
+#ifndef __KERNEL_IPI_H_
+#define __KERNEL_IPI_H_
+
+#include "kernel_base.h"
+/*
+    Private KERNEL IPI messages
+*/
+#define IPI_NOP         0
+#define IPI_STOP        1
+#define IPI_RESUME      2
+#define IPI_RESCHEDULE  3
+#define IPI_CALL_HOOK   4
+#define IPI_CAUSE       5
+
+void core_IPIHandle(struct ExceptionContext *regs, unsigned long ipi_number, struct KernelBase *KernelBase);
+void core_DoIPI(uint8_t ipi_number, unsigned int cpu_mask, struct KernelBase *KernelBase);
+
+#endif /* __KERNEL_IPI_H_ */
