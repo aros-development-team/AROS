959ec5a79ed7e4580c0b28f530fbd76ae00a1c12
Kalamatee <kalamatee@gmail.com>
2017-02-20 02:59:03 +0000

    cleanup. really update the scheduling data for the cpu we are running on, using the granularity value.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@53789 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-pc/kernel/apic_heartbeat.c
+++ arch/all-pc/kernel/apic_heartbeat.c
@@ -3,7 +3,7 @@
     $Id$
 */
 
-#define DEBUG 1
+#define DEBUG 0
 #include <aros/debug.h>
 
 #include <aros/asmcall.h>
@@ -21,13 +21,48 @@
 /*
  * Unlike the VBlankServer, we might not run at a fixed 60Hz.
  */
-AROS_INTH3(APICHeartbeatServer, struct List *, intList, intMask, custom)
+void APICHeartbeatServer(struct ExecBase *SysBase, void *unused)
 {
-    AROS_INTFUNC_INIT
+    struct KernelBase *KernelBase = __kernelBase;
+    struct PlatformData *pdata = KernelBase->kb_PlatformData;
+    struct APICData *apicData = pdata->kb_APIC;
+#if defined(__AROSEXEC_SMP__)
+    struct X86SchedulerPrivate  *apicScheduleData;
+    tls_t *apicTLS;
+#endif
+    UWORD current;
 
-    D(bug("[Kernel:APIC] %s()\n", __func__));
+    if (apicData->flags & APF_TIMER)
+    {
+#if defined(__AROSEXEC_SMP__)
+        apicid_t cpuNum = core_APIC_GetNumber(apicData);
 
-    return 1;
+        D(bug("[Kernel:APIC.%03u] %s()\n", cpuNum, __func__));
 
-    AROS_INTFUNC_EXIT
+        apicTLS = apicData->cores[cpuNum].cpu_TLS;
+        if ((apicTLS) && ((apicScheduleData = apicTLS->ScheduleData) != NULL))
+        {
+            if ((current = apicScheduleData->Elapsed))
+            {
+                if ((apicScheduleData->Elapsed = (current - apicScheduleData->Granularity)) == 0)
+                {
+                    __AROS_ATOMIC_OR_L(apicScheduleData->ScheduleFlags, TLSSF_Quantum);
+                    __AROS_ATOMIC_OR_L(apicScheduleData->ScheduleFlags, TLSSF_Switch);
+                }
+            }
+        }
+#else
+        D(bug("[Kernel:APIC] %s()\n", __func__));
+
+        current = SCHEDELAPSED_GET;
+        if (current)
+            SCHEDELAPSED_SET(--current);
+
+        if (current == 0)
+        {
+            FLAG_SCHEDQUANTUM_SET;
+            FLAG_SCHEDSWITCH_SET;
+        }
+#endif
+    }
 }
