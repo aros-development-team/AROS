910e4e2210c38d5334c41791552d65a5535d57a3
Kalamatee <kalamatee@gmail.com>
2017-02-22 22:22:01 +0000

    oops .. add the missing files
    git-svn-id: https://svn.aros.org/svn/aros/trunk@53853 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/all-pc/kernel/alloccpumask.c
@@ -0,0 +1,41 @@
+/*
+    Copyright © 2017, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc:
+*/
+
+#include <proto/exec.h>
+
+#include <aros/kernel.h>
+#include <aros/libcall.h>
+
+#include "kernel_base.h"
+#include "kernel_intern.h"
+
+AROS_LH0(void *, KrnAllocCPUMask,
+	struct KernelBase *, KernelBase, 42, Kernel)
+{
+    AROS_LIBFUNC_INIT
+
+    void *mask = NULL;
+    struct PlatformData *pdata = KernelBase->kb_PlatformData;
+    struct APICData *apicData;
+    int count;
+
+    if ((pdata) && (pdata->kb_APIC))
+    {
+        apicData  = pdata->kb_APIC;
+
+        count = apicData->apic_count / 32;
+
+        if ((count * 32) < apicData->apic_count)
+            count += 1;
+
+        mask = AllocMem(count * sizeof(ULONG), MEMF_CLEAR | MEMF_PUBLIC);
+    }
+
+    return mask;
+
+    AROS_LIBFUNC_EXIT
+}
