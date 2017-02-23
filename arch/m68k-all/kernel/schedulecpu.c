51c52970aa61daa3a9b7d97b848b1f4bd4d2fd0d
Kalamatee <kalamatee@gmail.com>
2017-02-23 03:02:17 +0000

    provide KrnScheduleCPU() for m68k, but only call Schedule since we can only have 1 CPU.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@53861 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/m68k-all/kernel/schedulecpu.c
@@ -0,0 +1,25 @@
+/*
+    Copyright © 2017, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc:
+*/
+
+#include <aros/kernel.h>
+
+#include <kernel_base.h>
+#include <kernel_syscall.h>
+
+#include <proto/kernel.h>
+
+AROS_LH1(void, KrnScheduleCPU,
+        AROS_LHA(void *, cpu_mask, A0),
+        struct KernelBase *, KernelBase, 47, Kernel)
+{
+    AROS_LIBFUNC_INIT
+
+    /* On m68k, there can be only one!... */
+    Supervisor(__AROS_GETVECADDR(SysBase,7));
+
+    AROS_LIBFUNC_EXIT
+}
