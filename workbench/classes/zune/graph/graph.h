a24feeaf8dfaf83968ea5a9f434e272dbcd5f19a
Kalamatee <kalamatee@gmail.com>
2017-02-23 18:33:00 +0000

    add code for a wip graph class that will be used to show the processor activity in sysmon.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@53868 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ workbench/classes/zune/graph/graph.h
@@ -0,0 +1,29 @@
+#ifndef ZUNE_GRAPH_H
+#define ZUNE_GRAPH_H
+
+/*
+    Copyright © 2017, The AROS Development Team. All rights reserved.
+    $Id: $
+*/
+
+#include <libraries/mui.h>
+
+/*** Name *******************************************************************/
+#define MUIC_Graph                      "Graph.mcc"
+
+/*** Identifier base ********************************************************/
+#define MUIB_Graph                      (MUIB_AROS | 0x00000000)
+
+/*** Attributes *************************************************************/
+#define MUIA_Graph_InfoText	        (MUIB_Graph | 0x00000001) /* ---  CONST_STRPTR          */
+#define MUIA_Graph_Entries  	        (MUIB_Graph | 0x00000002) /* ---  IPTR *                */
+#define MUIA_Graph_UpdateInterval       (MUIB_Graph | 0x00000003) /* ---  IPTR                  */
+#define MUIA_Graph_PeriodicTick  	(MUIB_Graph | 0x000000FF) /* ---  BOOL                  */
+
+/*** Methods ****************************************************************/
+#define MUIM_Graph_Timer    	        (MUIB_Graph | 0x000000FF)
+
+/*** Macros *****************************************************************/
+#define GraphObject MUIOBJMACRO_START(MUIC_Graph)
+
+#endif /* ZUNE_GRAPH_H */
