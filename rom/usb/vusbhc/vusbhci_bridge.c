0fbae4232106ab6d33696e078c390347c690b362
dizzyofcrn <dizzyofcrn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2017-02-24 12:40:06 +0000

    - INTR transfer made asynchronous  - Removing USB device may hang the system while transfers are pending...
    git-svn-id: https://svn.aros.org/svn/aros/trunk@53887 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/usb/vusbhc/vusbhci_bridge.c
+++ rom/usb/vusbhc/vusbhci_bridge.c
@@ -234,6 +234,57 @@ void call_libusb_event_handler() {
     LIBUSBCALL(libusb_handle_events, NULL);
 }
 
+void callbackUSBTransferComplete(struct libusb_transfer *xfr) {
+
+    struct IOUsbHWReq *ioreq = (struct IOUsbHWReq *) xfr->user_data;;
+    struct VUSBHCIUnit *unit = (struct VUSBHCIUnit *) ioreq->iouh_Req.io_Unit;
+
+    mybug_unit(-1, ("callbackUSBTransferComplete\n"));
+
+    int err;
+
+    switch(xfr->status) {
+        case LIBUSB_TRANSFER_COMPLETED:
+            mybug_unit(-1, ("LIBUSB_TRANSFER_COMPLETED\n"));
+            ioreq->iouh_Actual = xfr->actual_length;
+            err = UHIOERR_NO_ERROR;
+        break;
+
+        case LIBUSB_TRANSFER_NO_DEVICE:
+            mybug_unit(-1, ("LIBUSB_TRANSFER_NO_DEVICE\n"));
+            err = UHIOERR_USBOFFLINE;
+        break;
+
+        case LIBUSB_TRANSFER_TIMED_OUT:
+            mybug_unit(-1, ("LIBUSB_TRANSFER_TIMED_OUT\n"));
+            err = UHIOERR_TIMEOUT;
+        break;
+
+        case LIBUSB_TRANSFER_STALL:
+            mybug_unit(-1, ("LIBUSB_TRANSFER_STALL\n"));
+            err = UHIOERR_STALL;
+        break;
+
+        case LIBUSB_TRANSFER_OVERFLOW:
+            mybug_unit(-1, ("LIBUSB_TRANSFER_OVERFLOW\n"));
+            err = UHIOERR_OVERFLOW;
+        break;
+
+        default:
+            mybug_unit(-1, ("LIBUSB_TRANSFER_CANCELLED/LIBUSB_TRANSFER_ERROR/LIBUSB_OTHER_ERROR\n"));
+            err = UHIOERR_HOSTERROR;
+        break;
+
+    }
+
+    /* Set error codes */
+    ioreq->iouh_Req.io_Error = err & 0xff;
+
+    /* Terminate the iorequest */
+    ioreq->iouh_Req.io_Message.mn_Node.ln_Type = NT_FREEMSG;
+    ReplyMsg(&ioreq->iouh_Req.io_Message);
+}
+
 /*
     FIXME: libusb expects buffer to precede with enough space for setup data (8 bytes or LIBUSB_CONTROL_SETUP_SIZE)
             - Copy buffer need to be used
@@ -380,147 +431,55 @@ int do_libusb_ctrl_transfer(struct IOUsbHWReq *ioreq) {
 int do_libusb_intr_transfer(struct IOUsbHWReq *ioreq) {
     struct VUSBHCIUnit *unit = (struct VUSBHCIUnit *) ioreq->iouh_Req.io_Unit;
 
-    int rc = 0, transferred = 0, ret = UHIOERR_NO_ERROR;;
+    int rc = 0, transferred = 0;
     UBYTE endpoint = ioreq->iouh_Endpoint;
-ioreq->iouh_NakTimeout = 100;
+
     mybug_unit(-1, ("ioreq->iouh_Length %d\n", ioreq->iouh_Length));
-    mybug_unit(-1, ("ioreq->iouh_NakTimeout %d\n", ioreq->iouh_NakTimeout));
+    mybug_unit(-1, ("direction %d\n", (ioreq->iouh_Dir)));
+
+    struct libusb_transfer *xfr;
+
+    //ioreq->iouh_NakTimeout = 1000;
+
+    mybug_unit(-1, ("Allocating transfer...\n"));
+
+    xfr = LIBUSBCALL(libusb_alloc_transfer, 0);
+	if (xfr == NULL) {
+	    mybug_unit(-1, ("   Failed.\n"));
+	}
 
     switch(ioreq->iouh_Dir) {
         case UHDIR_IN:
             mybug_unit(-1, ("ioreq->iouh_Endpoint %d (IN)\n", endpoint));
-            mybug_unit(-1, ("ioreq->iouh_Endpoint %d (IN)(%x)\n", endpoint, (endpoint|LIBUSB_ENDPOINT_IN)));
-            rc = LIBUSBCALL(libusb_interrupt_transfer, dev_handle, (endpoint|LIBUSB_ENDPOINT_IN), (UBYTE *)ioreq->iouh_Data, ioreq->iouh_Length, &transferred, ioreq->iouh_NakTimeout);
+                libusb_fill_interrupt_transfer(xfr, dev_handle, (endpoint|LIBUSB_ENDPOINT_IN),
+                          (UBYTE *)ioreq->iouh_Data,
+                          ioreq->iouh_Length,
+                          callbackUSBTransferComplete,
+                          ioreq,
+                          ioreq->iouh_NakTimeout
+                          );
+            LIBUSBCALL(libusb_submit_transfer, xfr);
+            mybug_unit(-1, ("Called libusb_submit_transfer\n"));
+            call_libusb_event_handler();
         break;
 
         case UHDIR_OUT:
             mybug_unit(-1, ("ioreq->iouh_Endpoint %d (OUT)\n", endpoint));
-            mybug_unit(-1, ("ioreq->iouh_Endpoint %d (OUT)(%x)\n", endpoint, (endpoint|LIBUSB_ENDPOINT_OUT)));
-            rc = LIBUSBCALL(libusb_interrupt_transfer, dev_handle, (endpoint|LIBUSB_ENDPOINT_OUT), (UBYTE *)ioreq->iouh_Data, ioreq->iouh_Length, &transferred, ioreq->iouh_NakTimeout);
+                libusb_fill_interrupt_transfer(xfr, dev_handle, (endpoint|LIBUSB_ENDPOINT_OUT),
+                          (UBYTE *)ioreq->iouh_Data,
+                          ioreq->iouh_Length,
+                          callbackUSBTransferComplete,
+                          ioreq,
+                          ioreq->iouh_NakTimeout
+                          );
+
+            LIBUSBCALL(libusb_submit_transfer, xfr);
+            mybug_unit(-1, ("Called libusb_submit_transfer\n"));
+            call_libusb_event_handler();
         break;
     }
 
-    mybug_unit(-1, ("libusb_interrupt_transfer rc = %d, transferred %d\n", rc, transferred));
-
-    switch(rc) {
-	    case LIBUSB_SUCCESS:
-	        mybug_unit(-1, ("Success (no error)\n"));
-        break;
-
-	    case LIBUSB_ERROR_IO:
-	        mybug_unit(-1, ("Input/output error\n"));
-        break;
-
-	    case LIBUSB_ERROR_INVALID_PARAM:
-	        mybug_unit(-1, ("Invalid parameter\n"));
-        break;
-
-	    case LIBUSB_ERROR_ACCESS:
-	        mybug_unit(-1, ("Access denied (insufficient permissions)\n"));
-        break;
-
-	    case LIBUSB_ERROR_NO_DEVICE:
-	        mybug_unit(-1, ("No such device (it may have been disconnected)\n"));
-            ret = UHIOERR_USBOFFLINE;
-        break;
-
-	    case LIBUSB_ERROR_NOT_FOUND:
-	        mybug_unit(-1, ("Entity not found\n"));
-        break;
-
-	    case LIBUSB_ERROR_BUSY:
-	        mybug_unit(-1, ("Resource busy\n"));
-        break;
-
-	    case LIBUSB_ERROR_TIMEOUT:
-	        mybug_unit(-1, ("Operation timed out\n"));
-            ret = UHIOERR_TIMEOUT;
-        break;
-
-	    case LIBUSB_ERROR_OVERFLOW:
-	        mybug_unit(-1, ("Overflow\n"));
-        break;
-
-	    case LIBUSB_ERROR_PIPE:
-	        mybug_unit(-1, ("Pipe error */\n"));
-        break;
-
-	    case LIBUSB_ERROR_INTERRUPTED:
-	        mybug_unit(-1, ("System call interrupted (perhaps due to signal)\n"));
-        break;
-
-	    case LIBUSB_ERROR_NO_MEM:
-	        mybug_unit(-1, ("Insufficient memory\n"));
-        break;
-
-	    case LIBUSB_ERROR_NOT_SUPPORTED:
-	        mybug_unit(-1, ("Operation not supported or unimplemented on this platform\n"));
-        break;
-
-	    case LIBUSB_ERROR_OTHER:
-	        mybug_unit(-1, ("Other error\n"));
-        break;
-    }
-
-/*
-    ioreq->iouh_Req.io_Flags &= ~IOF_QUICK;
-    ioreq->iouh_Actual = 0;
-
-    Disable();
-    AddTail(&hc->hc_IntXFerQueue, (struct Node *) ioreq);
-    Enable();
-    SureCause(base, &hc->hc_CompleteInt);
-
-    KPRINTF(10, ("UHCMD_INTXFER processed ioreq: 0x%p\n", ioreq));
-    return(RC_DONTREPLY);
-*/
-
-    //ioreq->iouh_Req.io_Flags |= IOF_QUICK;
-    ioreq->iouh_Actual = transferred;
-/*
-    if(rc<0) {
-        rc = 0;
-    } else {
-        if(transferred) {
-            ioreq->iouh_Actual = transferred;
-        }
-    }
-*/
-    mybug_unit(-1, ("Done!\n\n"));
-    return ret;  
-}
-
-void callbackUSBTransferComplete(struct libusb_transfer *xfr) {
-
-    struct IOUsbHWReq *ioreq = (struct IOUsbHWReq *) xfr->user_data;;
-    struct VUSBHCIUnit *unit = (struct VUSBHCIUnit *) ioreq->iouh_Req.io_Unit;
-
-    mybug_unit(-1, ("callbackUSBTransferComplete\n"));
-
-    switch(xfr->status) {
-        case LIBUSB_TRANSFER_COMPLETED:
-            mybug_unit(-1, ("LIBUSB_TRANSFER_COMPLETED\n"));
-            // Success here, data transfered are inside 
-            // xfr->buffer
-            // and the length is
-            // xfr->actual_length
-
-            ioreq->iouh_Actual = xfr->actual_length;
-
-            /* Terminate the iorequest */
-            ioreq->iouh_Req.io_Message.mn_Node.ln_Type = NT_FREEMSG;
-            ReplyMsg(&ioreq->iouh_Req.io_Message);
-
-            break;
-        case LIBUSB_TRANSFER_CANCELLED:
-        case LIBUSB_TRANSFER_NO_DEVICE:
-        case LIBUSB_TRANSFER_TIMED_OUT:
-        case LIBUSB_TRANSFER_ERROR:
-        case LIBUSB_TRANSFER_STALL:
-        case LIBUSB_TRANSFER_OVERFLOW:
-            // Various type of errors here
-            break;
-    }
+    return UHIOERR_NO_ERROR;  
 }
 
 int do_libusb_bulk_transfer(struct IOUsbHWReq *ioreq) {
@@ -529,8 +488,8 @@ int do_libusb_bulk_transfer(struct IOUsbHWReq *ioreq) {
     int rc = 0, transferred = 0;
     UBYTE endpoint = ioreq->iouh_Endpoint;
 
-    mybug_unit(0, ("ioreq->iouh_Length %d\n", ioreq->iouh_Length));
-    mybug_unit(0, ("direction %d\n", (ioreq->iouh_Dir)));
+    mybug_unit(-1, ("ioreq->iouh_Length %d\n", ioreq->iouh_Length));
+    mybug_unit(-1, ("direction %d\n", (ioreq->iouh_Dir)));
 
     struct libusb_transfer *xfr;
 
@@ -544,8 +503,6 @@ int do_libusb_bulk_transfer(struct IOUsbHWReq *ioreq) {
     switch(ioreq->iouh_Dir) {
         case UHDIR_IN:
             mybug_unit(-1, ("ioreq->iouh_Endpoint %d (IN)\n", endpoint));
-//            rc = LIBUSBCALL(libusb_bulk_transfer, dev_handle, (endpoint|LIBUSB_ENDPOINT_IN), (UBYTE *)ioreq->iouh_Data, ioreq->iouh_Length, &transferred, ioreq->iouh_NakTimeout);
-
                 libusb_fill_bulk_transfer(xfr, dev_handle, (endpoint|LIBUSB_ENDPOINT_IN),
                           (UBYTE *)ioreq->iouh_Data,
                           ioreq->iouh_Length,
@@ -561,7 +518,6 @@ int do_libusb_bulk_transfer(struct IOUsbHWReq *ioreq) {
 
         case UHDIR_OUT:
             mybug_unit(-1, ("ioreq->iouh_Endpoint %d (OUT)\n", endpoint));
-//            rc = LIBUSBCALL(libusb_bulk_transfer, dev_handle, (endpoint|LIBUSB_ENDPOINT_OUT), (UBYTE *)ioreq->iouh_Data, ioreq->iouh_Length, &transferred, ioreq->iouh_NakTimeout);
                 libusb_fill_bulk_transfer(xfr, dev_handle, (endpoint|LIBUSB_ENDPOINT_OUT),
                           (UBYTE *)ioreq->iouh_Data,
                           ioreq->iouh_Length,
@@ -576,25 +532,6 @@ int do_libusb_bulk_transfer(struct IOUsbHWReq *ioreq) {
         break;
     }
 
-    mybug_unit(0, ("libusb_bulk_transfer rc = %d, transferred %d\n", rc, transferred));
-
-/*
-    0 on success (and populates transferred) 
-    LIBUSB_ERROR_TIMEOUT if the transfer timed out (and populates transferred) 
-    LIBUSB_ERROR_PIPE if the endpoint halted 
-    LIBUSB_ERROR_OVERFLOW if the device offered more data, see Packets and overflows 
-    LIBUSB_ERROR_NO_DEVICE if the device has been disconnected 
-    another LIBUSB_ERROR code on other failures 
-
-    if(rc<0) {
-        rc = 0;
-    } else {
-        if(transferred) {
-            ioreq->iouh_Actual = transferred;
-        }
-    }
-*/
-    mybug_unit(0, ("Done!\n\n"));
     return UHIOERR_NO_ERROR;
 }
 
