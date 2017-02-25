e6894e8230b48fdf32062f73afe2fce112f668fb
Kalamatee <kalamatee@gmail.com>
2017-02-25 04:14:00 +0000

    add a simple test that busy loops indefinitely for testing on smp builds.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@53898 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ test/misc/busyloop.c
@@ -0,0 +1,20 @@
+/*
+    Copyright © 2017, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+/*
+ * Busy loop forever ...
+ * TOOD: make it launch a task on a specified processor that
+ * busy loops until it is sent a signal.
+ */
+
+int main(void)
+{
+    do
+    {
+    }
+    while (1);
+
+    return 0;
+}
