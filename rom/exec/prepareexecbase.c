c6a521ce0bdffa6f7963467dcc0bf5a27c4af99d
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2017-02-26 20:19:00 +0000

    Exec locks provides means to protect exec's lists from beeing accessed. The lock provides protection by spinlock and, if requested, by forbid/permit and/or with disable/enable combination. macros for non-smp builds provide just simple Disable()/Enable() and Forbid/Permit pairs so that there should be no speed penalty.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@53920 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/exec/prepareexecbase.c
+++ rom/exec/prepareexecbase.c
@@ -366,6 +366,7 @@ struct ExecBase *PrepareExecBase(struct MemHeader *mh, struct TagItem *msg)
 
     SysBase->DebugAROSBase = PrepareAROSSupportBase(mh);
 #if defined(__AROSEXEC_SMP__)
+    PrivExecBase(SysBase)->ExecLockBase = NULL;
     PrivExecBase(SysBase)->ExecLockBase = ExecLock__PrepareBase(mh);
 #endif
 
