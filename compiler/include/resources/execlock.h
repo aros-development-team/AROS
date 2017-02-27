8d72f0f85d3d4692f09bf264ef50d1ccac77d223
Kalamatee <kalamatee@gmail.com>
2017-02-27 17:12:20 +0000

    add the resource public defines.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@53931 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/include/resources/execlock.h
@@ -0,0 +1,9 @@
+/*
+    Copyright © 2017, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#ifndef RESOURCES_EXECLOCK_H
+#define RESOURCES_EXECLOCK_H
+
+#endif /* !RESOURCES_EXECLOCK_H */
