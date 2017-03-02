8dbd91f64787948d21e39fad2b03ff04feec1573
Kalamatee <kalamatee@gmail.com>
2017-03-02 01:03:03 +0000

    add the common pc IRQTYPE definitions, and copy them for both the 32bit and 64bit targets. add the arch specific KrnAllocIRQ() stub, and call the MSI stubs where appropriate.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@54001 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ arch/all-pc/kernel/allocirq.c
@@ -0,0 +1,34 @@
+/*
+    Copyright © 2017, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <aros/kernel.h>
+
+#include <kernel_base.h>
+#include <kernel_debug.h>
+
+#include <proto/kernel.h>
+
+#define D(x)
+
+AROS_LH1(ULONG, KrnAllocIRQ,
+	AROS_LHA(ULONG, irq_type, D0),
+	struct KernelBase *, KernelBase, 38, Kernel)
+{
+    AROS_LIBFUNC_INIT
+
+    struct PlatformData *pdata = KernelBase->kb_PlatformData;
+    ULONG irqID = (ULONG)-1;
+
+    D(bug("[KRN] KrnAllocIRQ(%08x):\n", irq_type));
+
+    if ((irq_type == IRQTYPE_MSI) && (pdata->kb_PDFlags & PLATFORMF_HAVEMSI))
+    {
+        D(bug("[KRN] KrnAllocIRQ: Attempting to allocate MSI Interrupt...\n"));
+        irqID = core_APIC_AllocMSI();
+    }
+    return irqID;
+
+    AROS_LIBFUNC_EXIT
+}
