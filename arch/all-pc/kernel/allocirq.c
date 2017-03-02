8b45e93f22f40d562ede6ae031b39b63c00209db
Kalamatee <kalamatee@gmail.com>
2017-03-02 23:13:22 +0000

    add the missing count param for allocirq
    git-svn-id: https://svn.aros.org/svn/aros/trunk@54022 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/all-pc/kernel/allocirq.c
+++ arch/all-pc/kernel/allocirq.c
@@ -12,8 +12,9 @@
 
 #define D(x)
 
-AROS_LH1(ULONG, KrnAllocIRQ,
+AROS_LH2(ULONG, KrnAllocIRQ,
 	AROS_LHA(ULONG, irq_type, D0),
+	AROS_LHA(ULONG, count, D1),
 	struct KernelBase *, KernelBase, 38, Kernel)
 {
     AROS_LIBFUNC_INIT
