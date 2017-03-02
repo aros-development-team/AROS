3528f0ccb01cf7a16388f6b08232c92079b0454f
Kalamatee <kalamatee@gmail.com>
2017-03-02 01:27:31 +0000

    add another arch specific function for IRQ handling.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@54006 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/kernel/modifyirq.c
@@ -0,0 +1,52 @@
+/*
+    Copyright © 2017, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc:
+*/
+
+#include <aros/kernel.h>
+
+#include <kernel_base.h>
+#include <kernel_debug.h>
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/kernel.h>
+
+        AROS_LH2(ULONG, KrnModifyIRQA,
+
+/*  SYNOPSIS */
+	AROS_LHA(ULONG, irq, D0),
+	AROS_LHA(struct TagItem *, attribs, A0),
+
+/*  LOCATION */
+	struct KernelBase *, KernelBase, 39, Kernel)
+
+/*  FUNCTION
+	Modify an IRQ using the passed in tags.
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+******************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    /* The implementation of this function is architecture-specific */
+    return 0;
+
+    AROS_LIBFUNC_EXIT
+}
