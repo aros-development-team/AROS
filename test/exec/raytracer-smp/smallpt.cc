687a80a51a44d6b9267e5e311d47882ac00520fd
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2017-03-12 11:53:18 +0000

    Explicit mode (from smallpt-Authors website) added, at the moment as compile option (can be further changed to EXPLICIT/S parameter). Gives much better render results and is faster.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@54097 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- test/exec/raytracer-smp/smallpt.cc
+++ test/exec/raytracer-smp/smallpt.cc
@@ -8,6 +8,8 @@
 
 #include "renderer.h"
 
+#define EXPLICIT
+
 struct Vec {        // Usage: time ./smallpt 5000 && xv image.ppm
     double x;
     double y;
@@ -62,16 +64,25 @@ struct Sphere
 Sphere spheres[] = {//Scene: radius, position, emission, color, material 
    Sphere(1e5, Vec( 1e5+1,40.8,81.6), Vec(),Vec(.75,.25,.25),DIFF),//Left 
    Sphere(1e5, Vec(-1e5+99,40.8,81.6),Vec(),Vec(.25,.25,.75),DIFF),//Rght 
-   Sphere(1e5, Vec(50,40.8, 1e5),     Vec(),Vec(.75,.75,.75),DIFF),//Back 
+   Sphere(1e5, Vec(50,40.8, 1e5),     Vec(),Vec(.55,.55,.55),DIFF),//Back 
    Sphere(1e5, Vec(50,40.8,-1e5+170), Vec(),Vec(),           DIFF),//Frnt 
    Sphere(1e5, Vec(50, 1e5, 81.6),    Vec(),Vec(.75,.75,.75),DIFF),//Botm 
    Sphere(1e5, Vec(50,-1e5+81.6,81.6),Vec(),Vec(.75,.75,.75),DIFF),//Top 
-   Sphere(16.5,Vec(27,16.5,47),       Vec(),Vec(1,1,1)*.999, SPEC),//Mirr 
-   Sphere(16.5,Vec(73,16.5,78),       Vec(),Vec(.9,.9,.95)*.999, REFR),//Glas 
-   Sphere(10.5,Vec(33,16.5,98),       Vec(),Vec(1,0.6,0.7)*.999, REFR),//Glas 
-   Sphere(6.5, Vec(53,25.5,68),       Vec(),Vec(1,1,1)*.999, SPEC),//Mirr
-   Sphere(600, Vec(50,681.6-.27,81.6),Vec(12,12,12),  Vec(), DIFF) //Lite 
+   Sphere(16.5,Vec(27,16.5,47),       Vec(),Vec(.7,.7,.6)*.999, SPEC),//Mirr 
+   Sphere(16.5,Vec(73,16.5,78),       Vec(),Vec(.8,.7,.95)*.999, REFR),//Glas 
+   Sphere(10.5,Vec(23,10.5,98),       Vec(),Vec(.6,1,0.7)*.999, REFR),//Glas 
+   Sphere(8.,Vec(50,8.,108),       Vec(),Vec(1,0.6,0.7)*.999, REFR),//Glas 
+   Sphere(6.5, Vec(53,6.5,48),       Vec(),Vec(0.3,.4,.4)*.999, SPEC),//Mirr
+#ifdef EXPLICIT
+   Sphere(5, Vec(50,81.6-16.5,81.6),Vec(4,4,4)*20,  Vec(), DIFF),//Lite
+#else
+   Sphere(600, Vec(50,681.6-.27,81.6),Vec(12,12,12),  Vec(), DIFF), //Lite 
+#endif
  }; 
+
+#ifdef EXPLICIT
+int numSpheres = sizeof(spheres)/sizeof(Sphere);
+#endif
 
 inline double clamp(double x)
 {
@@ -90,6 +101,56 @@ inline bool intersect(const Ray &r, double &t, int &id)
     return t<inf; 
 }
 
+#ifdef EXPLICIT
+Vec radiance(const Ray &r, int depth, unsigned short *Xi,int E=1){
+  double t;                               // distance to intersection
+  int id=0;                               // id of intersected object
+  if (!intersect(r, t, id)) return Vec(); // if miss, return black
+  const Sphere &obj = spheres[id];        // the hit object
+  Vec x=r.o+r.d*t, n=(x-obj.p).norm(), nl=n.dot(r.d)<0?n:n*-1, f=obj.c;
+  double p = f.x>f.y && f.x>f.z ? f.x : f.y>f.z ? f.y : f.z; // max refl
+  if (++depth>10||!p) { if (erand48(Xi)<p) f=f*(1/p); else return obj.e*E;}
+  if (obj.refl == DIFF){                  // Ideal DIFFUSE reflection
+    double r1=2*M_PI*erand48(Xi), r2=erand48(Xi), r2s=sqrt(r2);
+    Vec w=nl, u=((fabs(w.x)>.1?Vec(0,1):Vec(1))%w).norm(), v=w%u;
+    Vec d = (u*cos(r1)*r2s + v*sin(r1)*r2s + w*sqrt(1-r2)).norm();
+
+    // Loop over any lights
+    Vec e;
+    for (int i=0; i<numSpheres; i++){
+      const Sphere &s = spheres[i];
+      if (s.e.x<=0 && s.e.y<=0 && s.e.z<=0) continue; // skip non-lights
+
+      Vec sw=s.p-x, su=((fabs(sw.x)>.1?Vec(0,1):Vec(1))%sw).norm(), sv=sw%su;
+      double cos_a_max = sqrt(1-s.rad*s.rad/(x-s.p).dot(x-s.p));
+      double eps1 = erand48(Xi), eps2 = erand48(Xi);
+      double cos_a = 1-eps1+eps1*cos_a_max;
+      double sin_a = sqrt(1-cos_a*cos_a);
+      double phi = 2*M_PI*eps2;
+      Vec l = su*cos(phi)*sin_a + sv*sin(phi)*sin_a + sw*cos_a;
+      l.norm();
+      if (intersect(Ray(x,l), t, id) && id==i){  // shadow ray
+        double omega = 2*M_PI*(1-cos_a_max);
+        e = e + f.mult(s.e*l.dot(nl)*omega)*M_1_PI;  // 1/pi for brdf
+      }
+    }
+
+    return obj.e*E+e+f.mult(radiance(Ray(x,d),depth,Xi,0));
+  } else if (obj.refl == SPEC)              // Ideal SPECULAR reflection
+    return obj.e + f.mult(radiance(Ray(x,r.d-n*2*n.dot(r.d)),depth,Xi));
+  Ray reflRay(x, r.d-n*2*n.dot(r.d));     // Ideal dielectric REFRACTION
+  bool into = n.dot(nl)>0;                // Ray from outside going in?
+  double nc=1, nt=1.5, nnt=into?nc/nt:nt/nc, ddn=r.d.dot(nl), cos2t;
+  if ((cos2t=1-nnt*nnt*(1-ddn*ddn))<0)    // Total internal reflection
+    return obj.e + f.mult(radiance(reflRay,depth,Xi));
+  Vec tdir = (r.d*nnt - n*((into?1:-1)*(ddn*nnt+sqrt(cos2t)))).norm();
+  double a=nt-nc, b=nt+nc, R0=a*a/(b*b), c = 1-(into?-ddn:tdir.dot(n));
+  double Re=R0+(1-R0)*c*c*c*c*c,Tr=1-Re,P=.25+.5*Re,RP=Re/P,TP=Tr/(1-P);
+  return obj.e + f.mult(depth>2 ? (erand48(Xi)<P ?   // Russian roulette
+    radiance(reflRay,depth,Xi)*RP:radiance(Ray(x,tdir),depth,Xi)*TP) :
+    radiance(reflRay,depth,Xi)*Re+radiance(Ray(x,tdir),depth,Xi)*Tr);
+}
+#else
 Vec radiance(const Ray &r, int depth, unsigned short *Xi)
 { 
     double t; // distance to intersection 
@@ -129,6 +190,7 @@ Vec radiance(const Ray &r, int depth, unsigned short *Xi)
         radiance(reflRay,depth,Xi)*RP:radiance(Ray(x,tdir),depth,Xi)*TP) : 
         radiance(reflRay,depth,Xi)*Re+radiance(Ray(x,tdir),depth,Xi)*Tr); 
 }
+#endif
 
 extern "C"
 void render_tile(int w, int h, int samps, int tile_x, int tile_y, ULONG *buffer, struct Task *gfxTask)
