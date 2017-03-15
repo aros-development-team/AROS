c080984fb357e32f1f56372685f38c88c00c1420
Kalamatee <kalamatee@gmail.com>
2017-03-15 05:21:53 +0000

    wip commit. acpi button driver.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@54133 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/hidds/acpi/button/acpibutton_intern.h
@@ -0,0 +1,55 @@
+#ifndef HIDDACPIBUTTON_INTERN_H
+#define HIDDACPIBUTTON_INTERN_H
+
+#include <proto/acpica.h>
+
+#include <exec/libraries.h>
+#include <dos/bptr.h>
+#include <oop/oop.h>
+
+#include <hidd/hidd.h>
+#include <hidd/acpibutton.h>
+
+#include <acpica/acnames.h>
+#include <acpica/accommon.h>
+
+struct HIDDACPIButtonData
+{
+    ACPI_HANDLE                 acpib_Handle;
+    ULONG                       acpib_Type;
+};
+
+struct class_static_data
+{
+    struct Library	        *cs_OOPBase;
+    struct Library              *cs_UtilityBase;
+    struct Library              *cs_ACPICABase;
+    BPTR                        cs_SegList;
+
+    OOP_Class                   *oopclass;
+
+    OOP_Object                  *powerButtonObj;
+    OOP_Object                  *sleepButtonObj;
+    OOP_Object                  *lidButtonObj;
+
+    OOP_AttrBase                hwAB;
+    OOP_AttrBase                hiddAB;
+    OOP_AttrBase                hiddACPIButtonAB;
+};
+
+/* Library base */
+
+struct HiddACPIButtonIntBase
+{
+    struct Library              hsi_LibNode;
+
+    struct class_static_data    hsi_csd;
+};
+
+#define CSD(x) (&((struct HiddACPIButtonIntBase *)x->UserData)->hsi_csd)
+
+#define __abHidd_ACPIButton     (CSD(cl)->hiddACPIButtonAB)
+#define __IHW 	                (CSD(cl)->hwAB)
+#define __IHidd	                (CSD(cl)->hiddAB)
+
+#endif /* !HIDDACPIBUTTON_INTERN_H */
---
+++ rom/hidds/acpi/button/include/acpibutton.h
@@ -0,0 +1,38 @@
+#ifndef HIDD_ACPIBUTTON_H
+#define HIDD_ACPIBUTTON_H
+
+#define CLID_Hidd_ACPIButton "hidd.acpibutton"
+#define IID_Hidd_ACPIButton CLID_Hidd_ACPIButton
+
+#define HiddACPIButtonAB __abHidd_ACPIButton
+
+#ifndef __OOP_NOATTRBASES__
+extern OOP_AttrBase HiddACPIButtonAB;
+#endif
+
+/* Attrs */
+
+enum {
+
+   aoHidd_ACPIButton_Type,
+   aoHidd_ACPIButton_Hook,
+
+   num_Hidd_ACPIButton_Attrs
+};
+
+#define aHidd_ACPIButton_Type          (aoHidd_ACPIButton_Type     + HiddACPIButtonAB)
+#define aHidd_ACPIButton_Hook          (aoHidd_ACPIButton_Hook     + HiddACPIButtonAB)
+
+#define IS_HIDDACPIBUTTON_ATTR(attr, idx) IS_IF_ATTR(attr, idx, HiddACPIButtonAB, num_Hidd_ACPIButton_Attrs)
+
+/* Button types */
+enum {
+   vHidd_ACPIButton_Power,
+   vHidd_ACPIButton_PowerF,
+   vHidd_ACPIButton_Sleep,
+   vHidd_ACPIButton_SleepF,
+   vHidd_ACPIButton_Lid
+};
+
+
+#endif /* !HIDD_ACPIBUTTON_H */
