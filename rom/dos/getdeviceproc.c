7abb9f8512d8dc491069a4e194850e4b845278ba
Kalamatee <kalamatee@gmail.com>
2017-05-13 03:41:47 +0000

    use __SRCFILENAME__ directly, and define it to __FILE__ if it hasnt been defined.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@54673 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/getdeviceproc.c
+++ rom/dos/getdeviceproc.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2016, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2017, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: GetDeviceProc() - Find the filesystem for a path.
@@ -322,7 +322,7 @@ static struct DevProc *deviceproc_internal(struct DosLibrary *DOSBase, CONST_STR
         FreeMem(dp, sizeof(struct DevProc));
         kprintf("%s:%d: DosList entry 0x%p has unknown type %d. Probably a bug, report it!\n"
                 "    GetDeviceProc() called for '%s'\n",
-                THIS_FILE, __LINE__, dl, dl->dol_Type, name);
+                __SRCFILENAME__, __LINE__, dl, dl->dol_Type, name);
         SetIoErr(ERROR_BAD_NUMBER);
         return NULL;
     }
