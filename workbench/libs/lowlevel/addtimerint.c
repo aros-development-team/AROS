82445a4fda8bd32b9a3d857134058da5096b32f4
Kalamatee <kalamatee@gmail.com>
2017-05-18 12:41:36 +0000

    minor update to the autodocs
    git-svn-id: https://svn.aros.org/svn/aros/trunk@54692 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/lowlevel/addtimerint.c
+++ workbench/libs/lowlevel/addtimerint.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2007, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2017, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: 
@@ -27,9 +27,20 @@
 
 /*  FUNCTION
  
+    Add a callback function that should be executed every time the timer
+    interrupt triggers.
+    
+    The timer will be allocated, but not configured or enabled - StartIntTimer()
+    must be called to initalize the correct paramaters.
+
     INPUTS
+
+    intRoutine  --  the callback function to invoke each vertical blank
+    intData     --  data passed to the callback function
  
     RESULT
+ 
+    A handle used to manipulate the interrupt or NULL if the call failed.
  
     BUGS
         This function is unimplemented.
