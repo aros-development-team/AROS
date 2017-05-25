edf21563684b1bbefc3d87fc2d25054a6cc8f526
Kalamatee <kalamatee@gmail.com>
2017-05-25 03:19:47 +0000

    convert interval to eclock value.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@54719 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/m68k-amiga/lowlevel/lowlevel_amiga.c
+++ arch/m68k-amiga/lowlevel/lowlevel_amiga.c
@@ -1,16 +1,23 @@
 /*
-    Copyright © 2013, The AROS Development Team. All rights reserved.
+    Copyright © 2013-2017, The AROS Development Team. All rights reserved.
     $Id$
 */
 
 #include <proto/exec.h>
 
 #include <aros/symbolsets.h>
+#include <graphics/gfxbase.h>
 
 #include "lowlevel_intern.h"
 
 static int Lowlevel_amiga_InitLib(struct LowLevelBase *LowLevelBase)
 {
+    struct GfxBase *GfxBase;
+
+    GfxBase = TaggedOpenLibrary(TAGGEDOPEN_GRAPHICS);
+    LowLevelBase->ll_EClockMult = (GfxBase->DisplayFlags & REALLY_PAL) ? 23245 : 23459;
+    CloseLibrary((struct Library*)GfxBase);
+
     LowLevelBase->ll_CIA.llciat_iCRBit = -1;
 
     LowLevelBase->ll_PotgoBase = OpenResource("potgo.resource");
@@ -19,6 +26,7 @@ static int Lowlevel_amiga_InitLib(struct LowLevelBase *LowLevelBase)
 
     LowLevelBase->ll_PortType[0] = 0;   /* Autosense */
     LowLevelBase->ll_PortType[1] = 0;   /* Autosense */
+
     return 1;
 }
 
