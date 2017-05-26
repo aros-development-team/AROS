4582c7269ee253fb98fd33163777b093588a4535
Kalamatee <kalamatee@gmail.com>
2017-05-26 23:02:07 +0000

    # separate the '020 optimized copymem into its own file. # don't patch exec during the first copymem call - but do it in the platform init. # add the '040/060 optimized copymem calls. # add the '040/060 optimized copymemquick calls.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@54724 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/m68k-all/exec/copymem.c
+++ arch/m68k-all/exec/copymem.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2013, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2017, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: CopyMem()
@@ -11,32 +11,5 @@
 #include <proto/exec.h>
 #include <aros/libcall.h>
 
-extern void AROS_SLIB_ENTRY(CopyMem_000,Exec,104)(void);
-extern void AROS_SLIB_ENTRY(CopyMem_020,Exec,104)(void);
 
-/* See rom/kernel/copymem.c for documentation */
-
-AROS_LH3I(void, CopyMem,
-    AROS_LHA(CONST_APTR,  source, A0),
-    AROS_LHA(APTR,  dest,   A1),
-    AROS_LHA(IPTR,  size,   D0),
-    struct ExecBase *, SysBase, 104, Exec)
-{
-    AROS_LIBFUNC_INIT
-    void (*func)(void);
-
-    Disable();
-    if (SysBase->AttnFlags & AFF_68020) {
-        /* 68020+ */
-        func = AROS_SLIB_ENTRY(CopyMem_020, Exec, 104);
-    } else {
-        /* 68000/68010 */
-        func = AROS_SLIB_ENTRY(CopyMem_000, Exec, 104);
-    }
-    SetFunction((struct Library *)SysBase, -LIB_VECTSIZE * 104, func);
-    Enable();
-
-    return CopyMem(source, dest, size);
-
-    AROS_LIBFUNC_EXIT
-} /* CopyMem */
+/* See copymem_.S for implementation */
