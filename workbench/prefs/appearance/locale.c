d88152fee7b29847206eadfcda5b7d197ccce68d
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2017-06-12 22:10:43 +0000

    Removed obsolete catalog strings.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@54778 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/prefs/appearance/locale.c
+++ workbench/prefs/appearance/locale.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 2013, The AROS Development Team. All rights reserved.
+    Copyright © 2013-2017, The AROS Development Team. All rights reserved.
     $Id$
 */
 
@@ -10,7 +10,7 @@
 #include "strings.h"
 
 #define CATALOG_NAME     "System/Prefs/Appearance.catalog"
-#define CATALOG_VERSION  1
+#define CATALOG_VERSION  2
 
 /*** Variables **************************************************************/
 struct Catalog *catalog;
