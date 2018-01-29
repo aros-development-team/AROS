3de829ded6ed15f101a579a8315de63c29355b62
balaton <balaton@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2018-01-29 00:15:44 +0000

    parthenope: Some headers had include guards before the license comment which is unusual. Clean these up.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@55039 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/ppc-sam440/boot/parthenope/src/debug.h
+++ arch/ppc-sam440/boot/parthenope/src/debug.h
@@ -1,6 +1,3 @@
-#ifndef DEBUG_H_
-#define DEBUG_H_
-
 /*
  * $Id$
  * 
@@ -19,6 +16,9 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  * MA 02111-1307 USA
  */
+
+#ifndef DEBUG_H_
+#define DEBUG_H_
 
 #include <context.h>
 
