3e766f1f184cb74d5229f64ad83992713fe89430
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2018-04-03 02:10:28 +0000

    Allow the PRINT_CPUCONTEXT macro to be used as a single statement.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@55075 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-pc/kernel/kernel_cpu.h
+++ arch/i386-pc/kernel/kernel_cpu.h
@@ -1,6 +1,9 @@
 /*
- * CPU-specific definitions.
- */
+    Copyright © 1995-2018, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: CPU-specific definitions.
+*/
 
 #ifndef KERNEL_CPU_H_
 #define KERNEL_CPU_H_
@@ -33,11 +36,13 @@
 	(__value & 0x03);	})
 
 #define PRINT_CPUCONTEXT(regs)										\
+{													\
     bug("[Kernel] Flags=0x%08X\n", regs->Flags);							\
     bug("[Kernel] stack=%04x:%08x rflags=%08x ip=%04x:%08x ds=0x%04X\n",				\
                  regs->ss, regs->esp, regs->eflags, regs->cs, regs->eip, regs->ds);			\
     bug("[Kernel] rax=%08x rbx=%08x rcx=%08x rdx=%08x\n", regs->eax, regs->ebx, regs->ecx, regs->edx);	\
-    bug("[Kernel] rsi=%08x rdi=%08x rbp=%08x rsp=%08x\n", regs->esi, regs->edi, regs->ebp, regs->esp);
+    bug("[Kernel] rsi=%08x rdi=%08x rbp=%08x rsp=%08x\n", regs->esi, regs->edi, regs->ebp, regs->esp);	\
+}
 
 #define SP(regs) regs->esp
 
