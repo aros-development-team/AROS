cae282038f1a0b84d7394b0b392bcafb29360b5b
Kalamatee <kalamatee@gmail.com>
2018-04-17 04:52:50 +0000

    add tests for the c99 float hex format specifier
    git-svn-id: https://svn.aros.org/svn/aros/trunk@55099 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- developer/debug/test/clib/sprintf_f.c
+++ developer/debug/test/clib/sprintf_f.c
@@ -10,8 +10,7 @@
 #include <strings.h>
 
 /*
-    Reason for this test is that formatted printing with %f
-    had crashed on x86_64.
+    Test output using various float printing formats.
 */
 
 int main(void)
@@ -26,8 +25,91 @@ int main(void)
 
     if (strcmp(buffer, " 3.140  6.280"))
     {
-        bug("created string is: %s\n", buffer);
+        bug("float/double string is: %s\n", buffer);
+        bug("expected result was: %s\n", " 3.140  6.280");
         retval = RETURN_ERROR;
     }
+
+	xd = 2;
+	sprintf(buffer, "%a", xd);
+    if (strcmp(buffer, "0x1p+1"))
+    {
+        bug("2 in hex: %s\n", buffer);
+        bug("expected result was: %s\n", "0x1p+1");
+        retval = RETURN_ERROR;
+    }
+
+	xd = 256;
+	sprintf(buffer, "%a", xd);
+    if (strcmp(buffer, "0x1p+8"))
+    {
+        bug("2^8 in hex: %s\n", buffer);
+        bug("expected result was: %s\n", "0x1p+8");
+        retval = RETURN_ERROR;
+    }
+
+	xd = 0.015625; //= 2^-6 
+ 	sprintf(buffer, "%a", xd);
+    if (strcmp(buffer, "0x1p-6"))
+    {
+        bug("2^-6 in hex: %s\n", buffer);
+        bug("expected result was: %s\n", "0x1p-6");
+        retval = RETURN_ERROR;
+    }
+
+	xd = 0.857421875;
+	sprintf(buffer, "%a", xd);
+    if (strcmp(buffer, "0x1.b7p-1"))
+    {
+        bug("0.857421875 in hex: %s\n", buffer);
+        bug("expected result was: %s\n", "0x1.b7p-1");
+        retval = RETURN_ERROR;
+    }
+
+	xd = 0x1p-1074; //Smallest double (unnormalized)
+	sprintf(buffer, "%a", xd);
+    if (strcmp(buffer, "0x0.0000000000001p-1022"))
+    {
+        bug("0x1p-1074 in hex: %s\n", buffer);
+        bug("expected result was: %s\n", "0x0.0000000000001p-1022");
+        retval = RETURN_ERROR;
+    }
+
+	xd = 3.1415926;
+	sprintf(buffer, "%A", xd);
+    if (strcmp(buffer, "0X1.921FB4D12D84AP+1"))
+    {
+        bug("3.1415926 in upper case hex: %s\n", buffer);
+        bug("expected result was: %s\n", "0X1.921FB4D12D84AP+1");
+        retval = RETURN_ERROR;
+    }
+
+	xd = 0.1;
+	sprintf(buffer, "%a", xd);
+    if (strcmp(buffer, "0x1.999999999999ap-4"))
+    {
+        bug("0.1 in hex: %s\n", buffer);
+        bug("expected result was: %s\n", "0x1.999999999999ap-4");
+        retval = RETURN_ERROR;
+    }
+
+	xd = 0x3.3333333333334p-5;
+	sprintf(buffer, "%a", xd);
+    if (strcmp(buffer, "0x1.999999999999ap-4"))
+    {
+        bug("0x3.3333333333334p-5 in hex: %s\n", buffer);
+        bug("expected result was: %s\n", "0x1.999999999999ap-4");
+        retval = RETURN_ERROR;
+    }
+
+	xd = 0xcc.ccccccccccdp-11;
+	sprintf(buffer, "%a", xd);
+    if (strcmp(buffer, "0x1.999999999999ap-4"))
+    {
+        bug("0xcc.ccccccccccdp-11 in hex: %s\n", buffer);
+        bug("expected result was: %s\n", "0x1.999999999999ap-4");
+        retval = RETURN_ERROR;
+    }
+
     return retval;
 }
