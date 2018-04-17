a5f8876ef7398685dd9b9fe92f3266e7a2b4ca4e
Kalamatee <kalamatee@gmail.com>
2018-04-17 04:00:27 +0000

    update copyrights
    git-svn-id: https://svn.aros.org/svn/aros/trunk@55097 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/stdc/__vcformat.c
+++ compiler/stdc/__vcformat.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2014, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2018, The AROS Development Team. All rights reserved.
     $Id$
 
     Function to format a string like printf().
