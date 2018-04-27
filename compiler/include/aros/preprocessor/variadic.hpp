0d4832bba5a68f713a005e8a713d69bf6d6a258a
falemagn <falemagn@gmail.com>
2018-04-27 12:55:21 +0000

    Added AROS_PP_VARIADIC_SIZE that increases up to 256 arguments boost's own limit of maximum 64 variadic arguments.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@55144 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/include/aros/preprocessor/variadic.hpp
+++ compiler/include/aros/preprocessor/variadic.hpp
@@ -1,6 +1,9 @@
 # ifndef AROS_PREPROCESSOR_VARIADIC_HPP
 # define AROS_PREPROCESSOR_VARIADIC_HPP
 #
+# define AROS_PP_VARIADIC_SIZE_OVERRIDES_BOOST 1
+# include <aros/preprocessor/variadic/size.hpp>
+#
 # include <aros/preprocessor/variadic/cast2iptr.hpp>
 # include <aros/preprocessor/variadic/cast2tagitem.hpp>
 #
