98788b911b236da037938f21fc9e6e7eff4f374d
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2018-05-27 20:19:17 +0000

    Use 32-bit register names in debug messages.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@55178 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/i386-pc/kernel/kernel_cpu.h
+++ arch/i386-pc/kernel/kernel_cpu.h
@@ -38,10 +38,10 @@
 #define PRINT_CPUCONTEXT(regs)										\
 {													\
     bug("[Kernel] Flags=0x%08X\n", regs->Flags);							\
-    bug("[Kernel] stack=%04x:%08x rflags=%08x ip=%04x:%08x ds=0x%04X\n",				\
+    bug("[Kernel] stack=%04x:%08x eflags=%08x eip=%04x:%08x ds=0x%04X\n",				\
                  regs->ss, regs->esp, regs->eflags, regs->cs, regs->eip, regs->ds);			\
-    bug("[Kernel] rax=%08x rbx=%08x rcx=%08x rdx=%08x\n", regs->eax, regs->ebx, regs->ecx, regs->edx);	\
-    bug("[Kernel] rsi=%08x rdi=%08x rbp=%08x rsp=%08x\n", regs->esi, regs->edi, regs->ebp, regs->esp);	\
+    bug("[Kernel] eax=%08x ebx=%08x ecx=%08x edx=%08x\n", regs->eax, regs->ebx, regs->ecx, regs->edx);	\
+    bug("[Kernel] esi=%08x edi=%08x ebp=%08x esp=%08x\n", regs->esi, regs->edi, regs->ebp, regs->esp);	\
 }
 
 #define SP(regs) regs->esp
