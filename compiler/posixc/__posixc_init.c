1be6834a9b1fd24cba1d8b68efe33fe6f4a4a989
Kalamatee <kalamatee@gmail.com>
2018-05-31 05:49:43 +0000

    remove duplicate consts
    git-svn-id: https://svn.aros.org/svn/aros/trunk@55216 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/posixc/__posixc_init.c
+++ compiler/posixc/__posixc_init.c
@@ -1,5 +1,5 @@
 /*
-    Copyright Â© 2012-2013, The AROS Development Team. All rights reserved.
+    Copyright © 2012-2018, The AROS Development Team. All rights reserved.
     $Id$
 
     Internal initialisation code for posixc.library
@@ -13,11 +13,11 @@
 #include "__posixc_intbase.h"
 
 /* We handle StdCBase */
-const ULONG const __aros_rellib_base_StdCBase = 0;
+const ULONG __aros_rellib_base_StdCBase = 0;
 SETRELLIBOFFSET(StdCBase, struct PosixCBase, StdCBase)
 
 /* We handle StdCIOBase */
-const ULONG const __aros_rellib_base_StdCIOBase = 0;
+const ULONG __aros_rellib_base_StdCIOBase = 0;
 SETRELLIBOFFSET(StdCIOBase, struct PosixCBase, StdCIOBase)
 
 static int __posixc_open(struct PosixCIntBase *PosixCBase)
