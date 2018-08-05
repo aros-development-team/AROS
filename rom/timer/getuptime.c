93c6bd1ec1f50f53cb858372be91e6dd697d4ac7
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2018-08-05 19:24:59 +0000

    Added GetUpTime call to timer device, similar to the one used on OS4. It can be used if one needs a monotonic clock.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@55388 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/timer/getuptime.c
@@ -0,0 +1,68 @@
+/*
+    Copyright © 1995-2018, The AROS Development Team. All rights reserved.
+    $Id$
+
+    Desc: GetUpTime() - Get time since machine was powered on.
+    Lang: english
+*/
+
+#include <proto/exec.h>
+
+#include "timer_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+#include <devices/timer.h>
+#include <proto/timer.h>
+
+	AROS_LH1(void, GetUpTime,
+
+/*  SYNOPSIS */
+	AROS_LHA(struct timeval *, dest, A0),
+
+/*  LOCATION */
+	struct Device *, TimerBase, 12, Timer)
+
+/*  FUNCTION
+	GetUpTime() will fill in the supplied timeval with the current
+	uptime.
+
+    INPUTS
+	dest    -   A pointer to the timeval you want the time stored in.
+
+    RESULT
+	The timeval "dest" will be filled with the current uptime. This timer
+	cannot be changed by the software and thus can be considered to be a
+	monotonic clock..
+
+    NOTES
+	This function is safe to call from interrupts.
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+	TR_GETSYSTIME, TR_SETSYSTIME, GetSysTime
+
+    INTERNALS
+
+    HISTORY
+	05-08-2018  schulz   Implemented.
+
+*****************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+
+    Disable();
+
+    /* Query the hardware */
+    EClockUpdate(GetTimerBase(TimerBase));
+    dest->tv_secs  = GetTimerBase(TimerBase)->tb_ElapsedTime.tv_secs;
+    dest->tv_micro = GetTimerBase(TimerBase)->tb_ElapsedTime.tv_micro;
+
+    Enable();
+
+    AROS_LIBFUNC_EXIT
+} /* GetUpTime */
