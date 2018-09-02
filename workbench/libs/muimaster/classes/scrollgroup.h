755b74470a78dbed9f45973a0860dbd360fb8624
wawatok <w.tokarski@gmx.net>
2018-09-02 18:27:29 +0000

    forwarding fix for some 64bit issues and adding the scrollgroup defines (NicJA).
    git-svn-id: https://svn.aros.org/svn/aros/trunk@55422 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/classes/scrollgroup.h
+++ workbench/libs/muimaster/classes/scrollgroup.h
@@ -13,6 +13,8 @@
 #define MUIB_Scrollgroup              (MUIB_ZUNE | 0x00002f00)
 
 /*** Attributes *************************************************************/
+#define MUIA_Scrollgroup_AutoBars \
+    (MUIB_MUI | 0x0042f50e)     /* V20 isg BOOL     */
 #define MUIA_Scrollgroup_Contents \
     (MUIB_MUI | 0x00421261)     /* V4  i.g Object * */
 #define MUIA_Scrollgroup_FreeHoriz \
@@ -21,6 +23,10 @@
     (MUIB_MUI | 0x004224f2)     /* V9  i.. BOOL     */
 #define MUIA_Scrollgroup_HorizBar \
     (MUIB_MUI | 0x0042b63d)     /* V16 ..g Object * */
+#define MUIA_Scrollgroup_NoHorizBar \
+    (MUIB_MUI | 0x0042cab1)     /* V20 isg BOOL     */
+#define MUIA_Scrollgroup_NoVertBar \
+    (MUIB_MUI | 0x004264c3)     /* V20 isg BOOL     */
 #define MUIA_Scrollgroup_UseWinBorder \
     (MUIB_MUI | 0x004284c1)     /* V13 i.. BOOL     */
 #define MUIA_Scrollgroup_VertBar \
