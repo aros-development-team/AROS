02cfa4952679eb216859b0e797e8996f0982ec37
wawatok <w.tokarski@gmx.net>
2018-09-23 23:15:12 +0000

    disable float support on m68k to prevent unwanted symbols being used in rom code. (NicJa)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@55462 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- compiler/fmtprintf/fmtprintf.c
+++ compiler/fmtprintf/fmtprintf.c
@@ -28,11 +28,12 @@
             size_t size1 = 0, size2 = 0;                /* How many chars in buffer? */
             const char *ptr = format + 1;               /* pointer to format string */
             size_t i, pad;		                /* Some temporary variables */
+#if defined(FULL_SPECIFIERS)
             union {			                /* floating point arguments %[aAeEfFgG] */
                 double dbl;
                 long double ldbl;
             } fparg;
-
+#endif
             do                                          /* read flags */
                 for (i = 0; i < sizeof(flagc); i++)
                     if (flagc[i] == *ptr)
