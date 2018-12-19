a9125b4fe0e3c689473027ec1b3c6704e9052640
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2018-12-19 19:05:20 +0000

    Added support for the MUI 4 "ESC A" special character sequence, which allows a raw ARGB image to be embedded in a text object.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@55505 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/libs/muimaster/mui.h
+++ workbench/libs/muimaster/mui.h
@@ -1,5 +1,5 @@
 /*
-    Copyright  2002-2015, The AROS Development Team. All rights reserved.
+    Copyright  2002-2018, The AROS Development Team. All rights reserved.
     $Id$
 */
 
@@ -312,6 +312,14 @@ struct MUI_RGBcolor
     ULONG red;
     ULONG green;
     ULONG blue;
+};
+
+struct MUI_AlphaData
+{
+    LONG width;
+    LONG height;
+    LONG dummy[2];
+    LONG data[0];
 };
 
 #ifndef _MUI_CLASSES_NOTIFY_H
