dcc97829fc6af72dc24fe74680a5b61addb34463
dizzyofcrn <dizzyofcrn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2019-01-14 13:50:13 +0000

    - Update after code refactoring  - Added test code to test opening and closing of arosx.library
    git-svn-id: https://svn.aros.org/svn/aros/trunk@55535 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ rom/usb/classes/arosx/arosx_test.c
@@ -0,0 +1,26 @@
+/*
+    Copyright © 2019, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+ 
+#include <aros/debug.h>
+ 
+#include <proto/exec.h>
+#include <proto/dos.h>
+//#include <proto/arosx.h>
+ 
+struct Library *AROSXBase;
+ 
+int main (int argc, char *argv[]) {
+ 
+    AROSXBase = OpenLibrary("arosx.library", 0);
+ 
+    if (AROSXBase) {
+        Printf("arosx.library opened, version %ld.%ld\n", AROSXBase->lib_Version, AROSXBase->lib_Revision);
+        CloseLibrary(AROSXBase);
+    } else {
+        PutStr("arosx.library failed to open.\n");
+    }
+ 
+    return 0;
+}
