f4d57512c8180cef843940cf659402e3eab24d09
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2019-01-30 20:01:34 +0000

    sdcard for raspi - endianess fixes
    git-svn-id: https://svn.aros.org/svn/aros/trunk@55622 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/arm-native/soc/broadcom/2708/sdcard/sdcard_bcm2708init.c
+++ arch/arm-native/soc/broadcom/2708/sdcard/sdcard_bcm2708init.c
@@ -1,9 +1,9 @@
 /*
-    Copyright © 2013-2015, The AROS Development Team. All rights reserved.
+    Copyright ï¿½ 2013-2015, The AROS Development Team. All rights reserved.
     $Id$
 */
 
-#define DEBUG 0
+#define DEBUG 1
 #include <aros/debug.h>
 
 #include <proto/exec.h>
@@ -39,12 +39,12 @@ static int FNAME_BCMSDC(BCM2708Init)(struct SDCardBase *SDCardBase)
         goto bcminit_fail;
     }
 
-    MBoxMessage[0] = 8 * 4;
-    MBoxMessage[1] = VCTAG_REQ;
-    MBoxMessage[2] = VCTAG_GETPOWER;
-    MBoxMessage[3] = 8;
-    MBoxMessage[4] = 4;
-    MBoxMessage[5] = VCPOWER_SDHCI;
+    MBoxMessage[0] = AROS_LONG2LE(8 * 4);
+    MBoxMessage[1] = AROS_LONG2LE(VCTAG_REQ);
+    MBoxMessage[2] = AROS_LONG2LE(VCTAG_GETPOWER);
+    MBoxMessage[3] = AROS_LONG2LE(8);
+    MBoxMessage[4] = AROS_LONG2LE(4);
+    MBoxMessage[5] = AROS_LONG2LE(VCPOWER_SDHCI);
     MBoxMessage[6] = 0;
 
     MBoxMessage[7] = 0; // terminate tag
@@ -55,35 +55,35 @@ static int FNAME_BCMSDC(BCM2708Init)(struct SDCardBase *SDCardBase)
         DINIT(bug("[SDCard--] %s: Failed to read controller's Power state\n", __PRETTY_FUNCTION__));
         goto bcminit_fail;
     }
-    
-    if (!(MBoxMessage[6] & VCPOWER_STATE_ON))
+
+    if (!(AROS_LE2LONG(MBoxMessage[6]) & VCPOWER_STATE_ON))
     {
         DINIT(bug("[SDCard--] %s: Powering on Arasan SDHCI controller...\n", __PRETTY_FUNCTION__));
 
-        MBoxMessage[0] = 8 * 4;
-        MBoxMessage[1] = VCTAG_REQ;
-        MBoxMessage[2] = VCTAG_SETPOWER;
-        MBoxMessage[3] = 8;
-        MBoxMessage[4] = 8;
-        MBoxMessage[5] = VCPOWER_SDHCI;
-        MBoxMessage[6] = VCPOWER_STATE_ON | VCPOWER_STATE_WAIT;
+        MBoxMessage[0] = AROS_LONG2LE(8 * 4);
+        MBoxMessage[1] = AROS_LONG2LE(VCTAG_REQ);
+        MBoxMessage[2] = AROS_LONG2LE(VCTAG_SETPOWER);
+        MBoxMessage[3] = AROS_LONG2LE(8);
+        MBoxMessage[4] = AROS_LONG2LE(8);
+        MBoxMessage[5] = AROS_LONG2LE(VCPOWER_SDHCI);
+        MBoxMessage[6] = AROS_LONG2LE(VCPOWER_STATE_ON | VCPOWER_STATE_WAIT);
 
         MBoxMessage[7] = 0; // terminate tag
 
         MBoxWrite((APTR)VCMB_BASE, VCMB_PROPCHAN, MBoxMessage);
-        if ((MBoxRead((APTR)VCMB_BASE, VCMB_PROPCHAN) != MBoxMessage) || (!(MBoxMessage[6] & VCPOWER_STATE_ON)))
+        if ((MBoxRead((APTR)VCMB_BASE, VCMB_PROPCHAN) != MBoxMessage) || (!(AROS_LE2LONG(MBoxMessage[6]) & VCPOWER_STATE_ON)))
         {
             DINIT(bug("[SDCard--] %s: Failed to power on controller\n", __PRETTY_FUNCTION__));
             goto bcminit_fail;
         }
     }
 
-    MBoxMessage[0] = 8 * 4;
-    MBoxMessage[1] = VCTAG_REQ;
-    MBoxMessage[2] = VCTAG_GETCLKRATE;
-    MBoxMessage[3] = 8;
-    MBoxMessage[4] = 4;
-    MBoxMessage[5] = VCCLOCK_SDHCI;
+    MBoxMessage[0] = AROS_LONG2LE(8 * 4);
+    MBoxMessage[1] = AROS_LONG2LE(VCTAG_REQ);
+    MBoxMessage[2] = AROS_LONG2LE(VCTAG_GETCLKRATE);
+    MBoxMessage[3] = AROS_LONG2LE(8);
+    MBoxMessage[4] = AROS_LONG2LE(4);
+    MBoxMessage[5] = AROS_LONG2LE(VCCLOCK_SDHCI);
     MBoxMessage[6] = 0;
 
     MBoxMessage[7] = 0; // terminate tag
@@ -101,10 +101,10 @@ static int FNAME_BCMSDC(BCM2708Init)(struct SDCardBase *SDCardBase)
         __BCM2708Bus->sdcb_IOBase = (APTR)ARASAN_BASE;
         __BCM2708Bus->sdcb_BusIRQ = IRQ_VC_ARASANSDIO;
 
-        __BCM2708Bus->sdcb_ClockMax = MBoxMessage[6];
-        __BCM2708Bus->sdcb_ClockMin = BCM2708SDCLOCK_MIN;        
+        __BCM2708Bus->sdcb_ClockMax = AROS_LE2LONG(MBoxMessage[6]);
+        __BCM2708Bus->sdcb_ClockMin = BCM2708SDCLOCK_MIN;
 
-        __BCM2708Bus->sdcb_LEDCtrl = FNAME_BCMSDCBUS(BCMLEDCtrl);
+        __BCM2708Bus->sdcb_LEDCtrl = (BYTE (*)(int))FNAME_BCMSDCBUS(BCMLEDCtrl);
         __BCM2708Bus->sdcb_IOReadByte = FNAME_BCMSDCBUS(BCMMMIOReadByte);
         __BCM2708Bus->sdcb_IOReadWord = FNAME_BCMSDCBUS(BCMMMIOReadWord);
         __BCM2708Bus->sdcb_IOReadLong = FNAME_BCMSDCBUS(BCMMMIOReadLong);
@@ -148,7 +148,7 @@ static int FNAME_BCMSDC(BCM2708Init)(struct SDCardBase *SDCardBase)
             __BCM2708Bus->sdcb_Private = (IPTR)sdcard_CurrentTime();
 
             FNAME_SDC(RegisterBus)(__BCM2708Bus, SDCardBase);
-            
+
             retVal = TRUE;
         }
         else
