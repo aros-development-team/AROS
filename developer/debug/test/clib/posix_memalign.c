811fa390ee023600c6c133ddb22b414a0f65b47a
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2019-02-14 22:29:52 +0000

    Extended the tests for posix_memalign().
    git-svn-id: https://svn.aros.org/svn/aros/trunk@55683 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- developer/debug/test/clib/posix_memalign.c
+++ developer/debug/test/clib/posix_memalign.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2014, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2019, The AROS Development Team. All rights reserved.
     $Id$
 */
 
@@ -10,16 +10,64 @@
 
 #define BLOCK_SIZE (64 * 4096)
 #define BLOCK_MASK ~(BLOCK_SIZE - 1)
+#define BLOCK_COUNT 16
 
 int main() 
 {
-    void * address = NULL;
+    void *blocks[BLOCK_COUNT];
+    int i;
+    long block_size, block_mask;
 
-    posix_memalign(&address, BLOCK_SIZE, BLOCK_SIZE);
+    /* Allocate a constant alignment with sizes that are multiples of it */
+    for (i = 0; i < BLOCK_COUNT; i++)
+    {
+        TEST(posix_memalign(&blocks[i], BLOCK_SIZE, BLOCK_SIZE * i) == 0);
+        TEST((IPTR)blocks[i] == ((IPTR)blocks[i] & (IPTR)BLOCK_MASK));
+    }
 
-    TEST(((IPTR)address == ((IPTR)address & (IPTR)BLOCK_MASK)));
+    for (i = 0; i < BLOCK_COUNT; i++)
+    {
+        free(blocks[i]);
+    }
 
-    free(address);
+    /* Allocate varying alignment with a constant size */
+    for (i = 0; i < BLOCK_COUNT; i++)
+    {
+        block_size = sizeof(void *) << i;
+        block_mask = ~(block_size - 1);
+        TEST(posix_memalign(&blocks[i], block_size, BLOCK_SIZE) == 0);
+        TEST((IPTR)blocks[i] == ((IPTR)blocks[i] & (IPTR)block_mask));
+    }
+
+    /* Reallocate the memory with a different size */
+    for (i = 0; i < BLOCK_COUNT; i++)
+    {
+        TEST((blocks[i] = realloc(blocks[i], BLOCK_SIZE * i)) != NULL);
+    }
+
+    for (i = 0; i < BLOCK_COUNT; i++)
+    {
+        free(blocks[i]);
+    }
+
+    /* Allocate a constant alignment with sizes that are smaller than it */
+    for (i = 0; i < BLOCK_COUNT; i++)
+    {
+        TEST(posix_memalign(&blocks[i], BLOCK_SIZE, BLOCK_SIZE - i * 1000)
+            == 0);
+        TEST((IPTR)blocks[i] == ((IPTR)blocks[i] & (IPTR)BLOCK_MASK));
+    }
+
+    /* Reallocate the memory with a different size */
+    for (i = 0; i < BLOCK_COUNT; i++)
+    {
+        TEST((blocks[i] = realloc(blocks[i], BLOCK_SIZE)) != NULL);
+    }
+
+    for (i = 0; i < BLOCK_COUNT; i++)
+    {
+        free(blocks[i]);
+    }
 
     return OK;
 }
