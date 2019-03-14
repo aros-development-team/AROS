983eb1ac85be04975fd23fdbc6611c7d8b21050d
Kalamatee <kalamatee@gmail.com>
2019-03-14 23:08:24 +0000

    create a small linklib to dump function entry/exit details.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@55817 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ compiler/instrfunc/profile_func_enter.c
@@ -0,0 +1,18 @@
+/*
+    Copyright © 2019, The AROS Development Team. All rights reserved.
+    $Id$
+    
+    Desc: called when a function is entered
+*/
+
+#include <aros/debug.h>
+#include <proto/exec.h>
+
+void __cyg_profile_func_enter (void *this_fn,
+                               void *call_site)
+{
+    struct Task *thisTask = FindTask(NULL);
+    int *frame = NULL;
+    frame = (int *)__builtin_frame_address(1); /*of the 'func'*/
+    bug("[0x%p>>] Enter Func @ 0x%p (frame @ 0x%p)", thisTask, this_fn, frame);
+}
