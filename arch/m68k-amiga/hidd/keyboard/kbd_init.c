b781cc0b54b2bbf79c75c91b368e1b464a992b07
Kalamatee <kalamatee@gmail.com>
2019-03-24 14:18:59 +0000

    merge to kbd_init and startup.c init functions.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@55928 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- arch/m68k-amiga/hidd/keyboard/kbd_init.c
+++ arch/m68k-amiga/hidd/keyboard/kbd_init.c
@@ -1,32 +1,71 @@
 /*
-    Copyright © 1995-2006, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2019, The AROS Development Team. All rights reserved.
     $Id$
-
-    Desc: kbd Hidd for standalone i386 AROS
-    Lang: english
 */
 
-#include <exec/types.h>
-#include <proto/exec.h>
-#include <utility/utility.h>
-#include <aros/symbolsets.h>
-
-#include "kbd.h"
-
-#include LC_LIBDEFS_FILE
-
-#undef  SDEBUG
-#undef  DEBUG
 #define DEBUG 0
 #include <aros/debug.h>
 
-static int AmigaKbd_Init(LIBBASETYPEPTR LIBBASE)
+#include <aros/symbolsets.h>
+#include <proto/oop.h>
+
+#include <hidd/hidd.h>
+#include <hidd/keyboard.h>
+
+#include "kbd.h"
+
+#undef XSD
+#define XSD(cl) 	ksd
+
+static int AmigaKbd_Init(struct kbdbase *LIBBASE)
 {
-    struct kbd_staticdata *xsd = &LIBBASE->ksd;
-	
-    InitSemaphore( &xsd->sema );
-	
-    return TRUE;
+    struct kbd_staticdata *ksd = &LIBBASE->ksd;
+    struct OOP_ABDescr attrbases[] =
+    {
+        {IID_Hidd	, &HiddAttrBase   },
+        {IID_Hidd_Kbd	, &HiddKbdAB   },
+        {NULL	    	, NULL      	    }
+    };
+    OOP_Object *kbd;
+    OOP_Object *drv = NULL;
+
+    EnterFunc(bug("AmigaKbd_Init\n"));
+
+    InitSemaphore( &ksd->sema );
+
+    kbd = OOP_NewObject(NULL, CLID_Hidd_Kbd, NULL);
+    if (kbd) {
+	if (OOP_ObtainAttrBases(attrbases))
+	{
+	    HiddKbdBase = OOP_GetMethodID(IID_Hidd_Kbd, 0);
+	    drv = HIDD_Kbd_AddHardwareDriver(kbd, LIBBASE->ksd.kbdclass, NULL);
+	}
+	OOP_DisposeObject(kbd);
+    }
+
+    if (!drv)
+	return FALSE;
+
+    LIBBASE->library.lib_OpenCnt = 1;
+
+    ReturnInt("AmigaKbd_Init", int, TRUE);
+}
+
+static int AmigaKbd_Expunge(struct kbdbase *LIBBASE)
+{
+    struct kbd_staticdata *ksd = &LIBBASE->ksd;
+    struct OOP_ABDescr attrbases[] =
+    {
+        {IID_Hidd_Kbd	, &LIBBASE->ksd.hiddKbdAB   },
+        {NULL	    	, NULL      	    }
+    };
+    
+    EnterFunc(bug("AmigaKbd_Expunge\n"));
+
+    OOP_ReleaseAttrBases(attrbases);
+    
+    ReturnInt("AmigaKbd_Expunge", int, TRUE);
 }
 
 ADD2INITLIB(AmigaKbd_Init, 0)
+ADD2EXPUNGELIB(AmigaKbd_Expunge, 0)
