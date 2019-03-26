8456124ddbc62625a71d19ff3763218dd79adfe5
Kalamatee <kalamatee@gmail.com>
2019-03-26 13:54:16 +0000

    add scripts to get the repo_type and _id. fix comment for repo_rev.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@56028 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- scripts/repo_id.sh
+++ scripts/repo_id.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 #
-# get repository revision via svn or via git
+# get repository ID via svn or via git
 #
 
 inside_git_repo="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
 
 if test -d $1/.svn; then
-    svn info $1 | sed -n 's/Revision: //p'
+    cd $1
+    svn info | grep '^URL' | awk '{print $NF}'
 else
     cd $1
     if [ "$inside_git_repo" == "true" ]; then
-        git log -1 | grep git-svn-id | sed 's|.*@\(.*\) .*|\1|'
+        git config --get remote.origin.url
     else
-        echo "NoRev"
+        echo "Unknown"
     fi
 fi
