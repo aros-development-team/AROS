e0c18e3572c018a371cbb06f546a7a330f01e327
Kalamatee <kalamatee@gmail.com>
2019-03-26 14:14:24 +0000

    sync behaviour with AboutAROS
    git-svn-id: https://svn.aros.org/svn/aros/trunk@56033 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/dos/boot.c
+++ rom/dos/boot.c
@@ -143,7 +143,14 @@ void __dos_Boot(struct DosLibrary *DOSBase, ULONG BootFlags, UBYTE Flags)
         BPTR cos = OpenFromLock(DupLockFromFH(cis));
         BYTE const C[] = "Copyright © 1995-2019, The AROS Development Team.\n"
                          "Licensed under the AROS Public License.\n"
-                         "Version SVN" SVNREV ", built on " ISODATE ".\n";
+#if defined(REPOTYPE)
+                         "Version " REPOTYPE "" REPOREVISION
+#if defined(REPOID)
+                         "(" REPOID ")"
+#endif
+                         "\n"
+#endif
+                         "built on " ISODATE ".\n";
 
         D(bug("[DOS] %s:  handle @ 0x%p (0x%p)\n", __func__, cis, cos);)
 
