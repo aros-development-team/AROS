8471bdeeada7c368f11158d9c69a65ff1fe97190
Kalamatee <kalamatee@gmail.com>
2019-03-26 13:14:36 +0000

    rename to repo_rev.sh
    git-svn-id: https://svn.aros.org/svn/aros/trunk@56026 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- scripts/repo_rev.sh
+++ scripts/repo_rev.sh
@@ -3,11 +3,13 @@
 # get svn revision via svn or via git
 #
 
+inside_git_repo="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
+
 if test -d $1/.svn; then
     svn info $1 | sed -n 's/Revision: //p'
 else
-    if test -d $1/.git; then
-        cd $1
+    cd $1
+    if [ "$inside_git_repo" == "true" ]; then
         git log -1 | grep git-svn-id | sed 's|.*@\(.*\) .*|\1|'
     else
         echo "NoRev"
