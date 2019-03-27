c0f0deff283f91ccaad1dd9600e8998f8bdb42f8
Kalamatee <kalamatee@gmail.com>
2019-03-27 02:08:25 +0000

    tabs->spaces
    git-svn-id: https://svn.aros.org/svn/aros/trunk@56046 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/c/iprefs/pointerprefs.c
+++ workbench/c/iprefs/pointerprefs.c
@@ -66,25 +66,25 @@ static void InstallPointer(struct PointerPrefs *pp, UWORD which)
     if (ic) {
         for (i = 0; i < ncols; i++) {
             D(bug("[PointerPrefs] Color %u RGB 0x%02X%02X%02X\n", i, colors->t_Red, colors->t_Green, colors->t_Blue));
-	    ic[i].ColorIndex = i + 8; /* Pointer colors have numbers 8-10 in the intuition's internal table */
-	    ic[i].Red   = colors->t_Red   * 0x0101;
-	    ic[i].Green = colors->t_Green * 0x0101;
-	    ic[i].Blue  = colors->t_Blue  * 0x0101;
-	    colors++;
-	}
+            ic[i].ColorIndex = i + 8; /* Pointer colors have numbers 8-10 in the intuition's internal table */
+            ic[i].Red   = colors->t_Red   * 0x0101;
+            ic[i].Green = colors->t_Green * 0x0101;
+            ic[i].Blue  = colors->t_Blue  * 0x0101;
+            colors++;
+        }
         ic[ncols].ColorIndex = -1; /* Terminator */
 
-	planes = (UBYTE *)colors;
-	for (i = 0; i < depth; i++) {
-	    bm.Planes[i] = planes;
-	    planes += bm.BytesPerRow * height;
-	}
+        planes = (UBYTE *)colors;
+        for (i = 0; i < depth; i++) {
+            bm.Planes[i] = planes;
+            planes += bm.BytesPerRow * height;
+        }
 
         /* First change palette, then image. It is important in order to get correct colors on
            truecolor screens */
         SetIPrefs(ic, ic_size, IPREFS_TYPE_OLD_PALETTE);
-	InstallBitMap(&bm, which, x, y, size, ysize);
-	FreeMem(ic, ic_size);
+        InstallBitMap(&bm, which, x, y, size, ysize);
+        FreeMem(ic, ic_size);
     }
 }
 
@@ -107,17 +107,17 @@ static void LoadPointerFile(STRPTR filename, ULONG which, UWORD installas, UWORD
     D(bug("[PointerPrefs] Datatype object: 0x%p\n", o));
     /* If datatypes failed, try to load AmigaOS 3.x prefs file */
     if (!o) {
-	/* Set numstopchunks to 1 because we want to avoid recursion
-	   if someone specifies new pointer prefs file as a target */
-	LoadPointerPrefs(filename, which, installas, 1);
-	return;
+        /* Set numstopchunks to 1 because we want to avoid recursion
+           if someone specifies new pointer prefs file as a target */
+        LoadPointerPrefs(filename, which, installas, 1);
+        return;
     }
 
     if (DoDTMethod(o, NULL, NULL, DTM_PROCLAYOUT, NULL, TRUE)) {
         D(bug("[PointerPrefs] Layout complete\n"));
 
         h_which = AROS_BE2WORD(installas);
-	GetDTAttrs(o, PDTA_DestBitMap, &bm, TAG_DONE);
+        GetDTAttrs(o, PDTA_DestBitMap, &bm, TAG_DONE);
         D(bug("[PointerPrers] BitMap: 0x%p\n", bm));
         D(bug("[PointerPrefs] Which: %d\n", h_which));
         D(bug("[PointerPrefs] Size: %dx%d\n", bm->BytesPerRow * 8, bm->Rows));
@@ -138,40 +138,40 @@ static void LoadPointerPrefs(STRPTR filename, WORD which, WORD installas, LONG n
     D(bug("[PointerPrefs] filename=%s\n",filename));
     iff = CreateIFF(filename, stopchunks, numstopchunks);
     if (iff) {
-    	while(ParseIFF(iff, IFFPARSE_SCAN) == 0)
-	{
-	    struct ContextNode   *cn;
+        while(ParseIFF(iff, IFFPARSE_SCAN) == 0)
+        {
+            struct ContextNode   *cn;
 
-	    cn = CurrentChunk(iff);
-	    
-	    switch(cn->cn_ID) {
-	    case ID_PNTR:
-	        pp = LoadChunk(iff, sizeof(struct PointerPrefs), MEMF_CHIP);
-		if (pp) {
-		    D(bug("[PointerPrefs] Got AmigaOS 3.0 pointer chunk, code is %d\n", AROS_BE2WORD(pp->pp_Which)));
-		    if ((which == -1) || (which == pp->pp_Which)) {
-		        InstallPointer(pp, AROS_BE2WORD((installas == -1) ? pp->pp_Which : installas));
-		    }
-		    FreeVec(pp);
-		}
-		break;
-	    case ID_NPTR:
-	        npp = LoadChunk(iff, sizeof(struct NewPointerPrefs), MEMF_ANY);
-		if (npp) {
-		    UWORD alpha = AROS_BE2WORD(npp->npp_AlphaValue);
-		    UWORD x     = AROS_BE2WORD(npp->npp_X);
-		    UWORD y     = AROS_BE2WORD(npp->npp_Y);
+            cn = CurrentChunk(iff);
+            
+            switch(cn->cn_ID) {
+            case ID_PNTR:
+                pp = LoadChunk(iff, sizeof(struct PointerPrefs), MEMF_CHIP);
+                if (pp) {
+                    D(bug("[PointerPrefs] Got AmigaOS 3.0 pointer chunk, code is %d\n", AROS_BE2WORD(pp->pp_Which)));
+                    if ((which == -1) || (which == pp->pp_Which)) {
+                        InstallPointer(pp, AROS_BE2WORD((installas == -1) ? pp->pp_Which : installas));
+                    }
+                    FreeVec(pp);
+                }
+                break;
+            case ID_NPTR:
+                npp = LoadChunk(iff, sizeof(struct NewPointerPrefs), MEMF_ANY);
+                if (npp) {
+                    UWORD alpha = AROS_BE2WORD(npp->npp_AlphaValue);
+                    UWORD x     = AROS_BE2WORD(npp->npp_X);
+                    UWORD y     = AROS_BE2WORD(npp->npp_Y);
 
-		    D(bug("[PointerPrefs] Got new pointer chunk\n"));
-		    D(bug("[PointerPrefs] Which %u, alpha: 0x%04X\n", AROS_BE2WORD(npp->npp_Which), alpha));
-		    SetIPrefs(&alpha, sizeof(alpha), IPREFS_TYPE_POINTER_ALPHA);
-		    LoadPointerFile(npp->npp_File, npp->npp_WhichInFile, npp->npp_Which, x, y);
-		    FreeVec(npp);
-		}
-		break;
-	    }
-	}
-	KillIFF(iff);
+                    D(bug("[PointerPrefs] Got new pointer chunk\n"));
+                    D(bug("[PointerPrefs] Which %u, alpha: 0x%04X\n", AROS_BE2WORD(npp->npp_Which), alpha));
+                    SetIPrefs(&alpha, sizeof(alpha), IPREFS_TYPE_POINTER_ALPHA);
+                    LoadPointerFile(npp->npp_File, npp->npp_WhichInFile, npp->npp_Which, x, y);
+                    FreeVec(npp);
+                }
+                break;
+            }
+        }
+        KillIFF(iff);
     }
 }
 
