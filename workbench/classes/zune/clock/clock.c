c209c887070ca515245cf5775a436794c872c490
Kalamatee <kalamatee@gmail.com>
2019-04-11 17:12:06 +0000

    _screen is only valid between show/hide.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@56205 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/classes/zune/clock/clock.c
+++ workbench/classes/zune/clock/clock.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2011, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2019, The AROS Development Team. All rights reserved.
     $Id$
 */
 
@@ -183,7 +183,16 @@ IPTR Clock__MUIM_Setup(Class *cl, Object *obj, struct MUIP_Setup *msg)
     if (!DoSuperMethodA(cl, obj, (Msg)msg)) return FALSE;
 
     DoMethod(_app(obj), MUIM_Application_AddInputHandler, (IPTR) &data->ihn);
+
+    return TRUE;
+}
+
+IPTR Clock__MUIM_Show(Class *cl, Object *obj, struct MUIP_Setup *msg)
+{
+    struct Clock_DATA *data = INST_DATA(cl, obj);
     
+    if (!DoSuperMethodA(cl, obj, (Msg)msg)) return FALSE;
+
     data->editpen = ObtainBestPen(_screen(obj)->ViewPort.ColorMap,
     	    	    	    	  0xFFFFFFFF,
 				  0xD8D8D8D8,
@@ -193,7 +202,6 @@ IPTR Clock__MUIM_Setup(Class *cl, Object *obj, struct MUIP_Setup *msg)
 				  TAG_DONE);
     return TRUE;
 }
-
 
 IPTR Clock__MUIM_Cleanup(Class *cl, Object *obj, struct MUIP_Cleanup *msg)
 {
