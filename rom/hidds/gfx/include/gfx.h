67830b686d4004405c271a2ec108d45a607aac4b
Kalamatee <kalamatee@gmail.com>
2019-04-25 18:32:27 +0000

    update copyright
    git-svn-id: https://svn.aros.org/svn/aros/trunk@56338 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- rom/hidds/gfx/include/gfx.h
+++ rom/hidds/gfx/include/gfx.h
@@ -2,7 +2,7 @@
 #define HIDD_GRAPHICS_H
 
 /*
-    Copyright © 1995-2017, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2019, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: Definitions for the Gfx Hidd system.
