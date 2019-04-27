9770e0632c622f8e31de6bd5ae3ee97d9cf5f266
mattrust <mrustler@gmx.de>
2019-04-27 15:36:06 +0000

    Delinted.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@56360 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- workbench/locale/help/genindex.py
+++ workbench/locale/help/genindex.py
@@ -21,36 +21,41 @@ def write_section(helpdir, filehandle, section):
     files.sort()
     filehandle.write('@NODE %s %s\n' % (section, section))
     filehandle.write('\n\n@{B}Section "%s"@{UB}\n\n' % (section))
-    for file in files:
-        if file[-6:] == ".guide":
-            name = file[:-6]
-            filehandle.write('    @{" %s " LINK "HELP:English/%s/%s/MAIN"}\n' % (name, section, file))
+    for filename in files:
+        if filename[-6:] == ".guide":
+            name = filename[:-6]
+            filehandle.write('    @{" %s " LINK "HELP:English/%s/%s/MAIN"}\n' % (name, section, filename))
     filehandle.write('@ENDNODE\n\n')
 
 ###############################################################################
 
-helpdir = sys.argv[1]
-print "genindex helpdir " + helpdir
+def main():
+    helpdir = sys.argv[1]
+    print "genindex helpdir " + helpdir
 
-sections = (
-    ("Commands", "C:"),
-    ("System",   "SYS:System, SYS:Tools and SYS:Utilites"),
-)
+    sections = (
+        ("Commands", "C:"),
+        ("System", "SYS:System, SYS:Tools and SYS:Utilites"),
+    )
 
-today = datetime.date.today()
+    today = datetime.date.today()
 
-filehandle = open(os.path.join(helpdir, "Index.guide"), "w")
-filehandle.write("@DATABASE Index.guide\n\n")
-filehandle.write("@$VER: Index.guide 1.0 (%d.%d.%d)\n" % (today.day, today.month, today.year))
-filehandle.write("@(C) Copyright (C) %d, The AROS Development Team. All rights reserved.\n\n" % (today.year))
+    filehandle = open(os.path.join(helpdir, "Index.guide"), "w")
+    filehandle.write("@DATABASE Index.guide\n\n")
+    filehandle.write("@$VER: Index.guide 1.0 (%d.%d.%d)\n" % (today.day, today.month, today.year))
+    filehandle.write("@(C) Copyright (C) %d, The AROS Development Team. All rights reserved.\n\n" % (today.year))
 
-filehandle.write('@NODE MAIN "Help sections"')
-filehandle.write('\n\n    @{B}Section@{TAB}Location of Programs@{UB}\n\n')
-for section in sections:
-    filehandle.write('    @{" %s " LINK "%s"}@{TAB}%s\n' % (section[0], section[0], section[1]))
-filehandle.write('@ENDNODE\n\n')
+    filehandle.write('@NODE MAIN "Help sections"')
+    filehandle.write('\n\n    @{B}Section@{TAB}Location of Programs@{UB}\n\n')
+    for section in sections:
+        filehandle.write('    @{" %s " LINK "%s"}@{TAB}%s\n' % (section[0], section[0], section[1]))
+    filehandle.write('@ENDNODE\n\n')
 
-for section in sections:
-    write_section(helpdir, filehandle, section[0])
+    for section in sections:
+        write_section(helpdir, filehandle, section[0])
 
-filehandle.close()
+    filehandle.close()
+
+
+if __name__ == "__main__":
+    main()
