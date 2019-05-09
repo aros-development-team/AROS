f395cdaf3bd92d44c53e1371aa54527cc76936e9
Kalamatee <kalamatee@gmail.com>
2019-05-09 14:14:11 +0100

    expose a generic m68k array of ROM image virt->phys mappings in the kernel platform data. implement an m68k-generic virtualtophysical function that checks if the specified virtual address lies within the ranges defined by the ROM image mappings (if they are provided).

--- arch/m68k-all/kernel/kernel_intern.h
+++ arch/m68k-all/kernel/kernel_intern.h
@@ -11,12 +11,23 @@
 /* Platform-specific part of KernelBase */
 struct PlatformData
 {
-	ULONG *MMU_Level_A;
-	UBYTE mmu_type;
-	UBYTE *page_ptr;
-	ULONG page_free;
-	UBYTE *zeropagedescriptor;
-	UBYTE cachemodestore; /* CachePreDMA()/CachePostDMA() */
+    ULONG *MMU_Level_A;
+    UBYTE mmu_type;
+    UBYTE *page_ptr;
+    ULONG page_free;
+    UBYTE *zeropagedescriptor;
+    UBYTE cachemodestore; /* CachePreDMA()/CachePostDMA() */
+
+    /*
+     * romimg points to an array of virt->phys address mappings
+     * for an m68k systems rom images (if used).
+     * e.g
+     *   rom_a_virt, rom_a_phys,
+     *   rom_b_virt, rom_b_phys,
+     *   NULL, NULL
+     */
+    ULONG romsize;
+    APTR *romimg;
 };
 
 extern BOOL map_region(struct KernelBase *kb, void *addr, void *physaddr, ULONG size, BOOL invalid, BOOL writeprotect, BOOL supervisor, UBYTE cachemode);
