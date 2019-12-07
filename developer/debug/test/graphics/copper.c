f0016d45042d1af8cd5f9de282fcad608f8e3f37
Kalamatee <kalamatee@gmail.com>
2019-12-07 22:03:05 +0000

    use FreeVPortCopLists to free the user copperlist.

--- developer/debug/test/graphics/copper.c
+++ developer/debug/test/graphics/copper.c
@@ -91,8 +91,7 @@ int main(int argc, char **argv)
 				else
 				{
 					Forbid();
-					uCop = pubscreen->ViewPort.UCopIns;
-					pubscreen->ViewPort.UCopIns = NULL;
+					FreeVPortCopLists(&pubscreen->ViewPort);
 					Permit();
 				}
 				VideoControl( pubscreen->ViewPort.ColorMap, vcTags);
