d24aae8cd188a68a7a25f07ddc5031d3f8ed41e7
Kalamatee <kalamatee@gmail.com>
2019-12-09 18:05:24 +0000

    define a new method for the monitor class to expose its bounds.

--- rom/intuition/monitorclass_private.h
+++ rom/intuition/monitorclass_private.h
@@ -2,21 +2,28 @@
 
 /* Attributes */
 
-#define MA_AROS_PRIVATE		(TAG_USER + 0x00020000)
+#define MA_AROS_PRIVATE         (TAG_USER + 0x00020000)
 
-#define MA_MonitorHandle	(MA_AROS_PRIVATE + 1)   /* [I..] struct MonitorHandle * graphics.library monitor handle */
-#define MA_PointerVisible	(MA_AROS_PRIVATE + 2)	/* [.S.] BOOL                   Mouse pointer is visible        */
+#define MA_MonitorHandle        (MA_AROS_PRIVATE + 1)   /* [I..] struct MonitorHandle * graphics.library monitor handle */
+#define MA_PointerVisible       (MA_AROS_PRIVATE + 2)	/* [.S.] BOOL                   Mouse pointer is visible        */
 #define MA_MonitorID            (MA_AROS_PRIVATE + 3)   /* [.G.] ULONG                  Monitor ID                      */
 
 /* Methods */
 
-#define MM_GetCompositionFlags 0x2401 /* Ask display composition flags			 */
-#define MM_SetPointerPos       0x2402 /* Set mouse pointer position			 */
-#define MM_CheckID	       0x2403 /* Check if the given mode ID matches this monitor */
-#define MM_SetPointerShape     0x2404 /* Set mouse pointer shape			 */
-#define MM_SetScreenGamma      0x2405 /* Set screen-specific gamma correction table      */
-#define MM_FindBest3dDepth     0x2406 /* Find best depth with 3D support                 */
-#define MM_Calc3dCapability    0x2407 /* Calculate 3D capability index                   */
+#define MM_GetCompositionFlags  0x2401 /* Ask display composition flags                         */
+#define MM_SetPointerPos        0x2402 /* Set mouse pointer position                            */
+#define MM_CheckID              0x2403 /* Check if the given mode ID matches this monitor       */
+#define MM_SetPointerShape      0x2404 /* Set mouse pointer shape			        */
+#define MM_SetScreenGamma       0x2405 /* Set screen-specific gamma correction table            */
+#define MM_FindBest3dDepth      0x2406 /* Find best depth with 3D support                       */
+#define MM_Calc3dCapability     0x2407 /* Calculate 3D capability index                         */
+#define MM_GetDisplayBounds     0x2408 /* Ask display dimensions                                */
+
+struct msGetDisplayBounds
+{
+    STACKED ULONG MethodID;
+    STACKED struct Rectangle *Bounds;
+};
 
 struct msGetCompositionFlags
 {
