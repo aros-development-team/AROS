84b0a5d8e6b09c7f5161106f0cf5854fdbfd0d4a
Kalamatee <kalamatee@gmail.com>
2019-12-29 16:48:40 +0000

    bump copyright

--- rom/intuition/setwindowpointera.c
+++ rom/intuition/setwindowpointera.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2013, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2019, The AROS Development Team. All rights reserved.
     Copyright © 2001-2003, The MorphOS Development Team. All Rights Reserved.
     $Id$
 */
