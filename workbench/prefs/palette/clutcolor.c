74f6cbcae89de44e1b61328ed4de58d50a05e302
Kalamatee <kalamatee@gmail.com>
2020-01-02 04:33:46 +0000

    dont try to allocate a pen ... use the pen specified by colorindex

--- workbench/prefs/palette/clutcolor.c
+++ workbench/prefs/palette/clutcolor.c
@@ -23,13 +23,8 @@
 /*** Instance data **********************************************************/
 struct CLUTColor_DATA
 {
-    UBYTE flags;
     UBYTE colorindex;
-    UBYTE pen;
 };
-
-#define FLAG_PEN_ALLOCATED  1
-#define FLAG_NO_PEN 	    2
 
 IPTR CLUTColor__OM_NEW(struct IClass *cl, Object *obj, struct opSet *msg)
 {
@@ -74,11 +69,6 @@ IPTR CLUTColor__OM_SET(struct IClass *cl, Object *obj,
         switch (tag->ti_Tag)
         {
         case MUIA_CLUTColor_Index:
-            if (data->flags & FLAG_PEN_ALLOCATED)
-            {
-                ReleasePen(_screen(obj)->ViewPort.ColorMap, data->pen);
-                data->flags &= ~(FLAG_PEN_ALLOCATED | FLAG_NO_PEN);
-            }
             data->colorindex = (UBYTE) tag->ti_Data;
             break;
 
@@ -99,7 +89,7 @@ IPTR CLUTColor__OM_GET(struct IClass *cl, Object *obj,
     switch (msg->opg_AttrID)
     {
     case MUIA_CLUTColor_Index:
-        *store = data->pen;
+        *store = data->colorindex;
         break;
 
     default:
@@ -118,20 +108,6 @@ IPTR CLUTColor__MUIM_Setup(struct IClass *cl, Object *obj,
     if (!(DoSuperMethodA(cl, obj, (Msg) msg)))
         return 0;
 
-    pen = ObtainPen(_screen(obj)->ViewPort.ColorMap,
-        (ULONG)data->colorindex, 0, 0, 0, PEN_NO_SETCOLOR);
-
-    if (pen == -1)
-    {
-        data->flags |= FLAG_NO_PEN;
-        data->pen = 0;
-    }
-    else
-    {
-        data->pen = (UBYTE) pen;
-        data->flags |= FLAG_PEN_ALLOCATED;
-    }
-
     return 1;
 }
 
@@ -139,14 +115,6 @@ IPTR CLUTColor__MUIM_Cleanup(struct IClass *cl, Object *obj,
     struct MUIP_Cleanup *msg)
 {
     struct CLUTColor_DATA *data = INST_DATA(cl, obj);
-
-    if (data->flags & FLAG_PEN_ALLOCATED)
-    {
-        ReleasePen(_screen(obj)->ViewPort.ColorMap, data->pen);
-        data->flags &= ~FLAG_PEN_ALLOCATED;
-        data->pen = 0;
-    }
-    data->flags &= ~FLAG_NO_PEN;
 
     return DoSuperMethodA(cl, obj, (Msg) msg);
 }
@@ -176,18 +144,7 @@ IPTR CLUTColor__MUIM_Draw(struct IClass *cl, Object *obj,
     if (!(msg->flags & (MADF_DRAWOBJECT | MADF_DRAWUPDATE)))
         return FALSE;
 
-    if (data->flags & FLAG_NO_PEN)
-    {
-        static UWORD pat[] = { 0x1111, 0x2222, 0x4444, 0x8888 };
-
-        SetAfPt(_rp(obj), pat, 2);
-        SetABPenDrMd(_rp(obj), _pens(obj)[MPEN_SHADOW],
-            _pens(obj)[MPEN_BACKGROUND], JAM2);
-    }
-    else
-    {
-        SetABPenDrMd(_rp(obj), data->pen, 0, JAM1);
-    }
+    SetABPenDrMd(_rp(obj), data->colorindex, 0, JAM1);
 
     RectFill(_rp(obj), _mleft(obj), _mtop(obj), _mright(obj),
         _mbottom(obj));
