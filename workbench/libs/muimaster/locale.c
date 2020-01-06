477157f7378c6d69cd43cdeeca97f2cdb1bf221d
Matthias Rustler <mrustler@gmx.de>
2020-01-06 10:53:56 +0100

    The function used for ADD2INIT must return TRUE on success.

--- workbench/libs/muimaster/locale.c
+++ workbench/libs/muimaster/locale.c
@@ -30,7 +30,7 @@ CONST_STRPTR _(ULONG id)
 }
 
 /* Setup ********************************************************************/
-VOID Locale_Initialize(VOID)
+BOOL Locale_Initialize(VOID)
 {
     if (LocaleBase != NULL)
     {
@@ -43,6 +43,7 @@ VOID Locale_Initialize(VOID)
     {
         catalog = NULL;
     }
+    return TRUE;
 }
 
 VOID Locale_Deinitialize(VOID)
