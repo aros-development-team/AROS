825b2f0e7fff7fde5156c3095db6b59028655feb
Kalamatee <kalamatee@gmail.com>
2020-01-12 05:19:57 +0000

    fix the tasklist test

--- developer/debug/test/exec/tasklist.c
+++ developer/debug/test/exec/tasklist.c
@@ -1,5 +1,8 @@
-#include <resources/task.h>
+
+#include <proto/exec.h>
 #include <proto/task.h>
+
+#include <resources/task.h>
 #include <stdio.h>
 
 /*
@@ -9,13 +12,18 @@
 
 int main(void)
 {
+    APTR TaskResBase;
     struct Task *task;
-    struct TaskList *tasklist = LockTaskList(LTF_ALL);
-    while ((task =  NextTaskEntry(tasklist, LTF_ALL)))
-    {
-        printf("Task %p Name %s State %d\n", task, task->tc_Node.ln_Name, task->tc_State);
-    }
-    UnLockTaskList(tasklist, LTF_ALL);
+    struct TaskList *tasklist;
 
+    if ((TaskResBase = OpenResource("task.resource")) != NULL)
+    {
+        tasklist = LockTaskList(LTF_ALL);
+        while ((task =  NextTaskEntry(tasklist, LTF_ALL)))
+        {
+            printf("Task %p Name %s State %d\n", task, task->tc_Node.ln_Name, task->tc_State);
+        }
+        UnLockTaskList(tasklist, LTF_ALL);
+    }
     return 0;
 }
