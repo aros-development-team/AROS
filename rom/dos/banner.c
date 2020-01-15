ed60c63f48726cbeb4c5343bb10c8f4cbdf44a7a
Kalamatee <kalamatee@gmail.com>
2020-01-15 04:17:46 +0000

    move the banner code into a separate file. get the copyright string from exec via taggedopenlibrary.

---
+++ rom/dos/banner.c
@@ -0,0 +1,30 @@
+/*
+    Copyright © 2020, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <proto/exec.h>
+#include <exec/rawfmt.h>
+
+#include <string.h>
+
+char *generate_banner()
+{
+    BYTE const CExtra[] = "Licensed under the AROS Public License.\n"
+#if defined(REPOTYPE)
+                         "Version " REPOTYPE "" REPOREVISION
+#if defined(REPOID)
+                         " (" REPOID ")"
+#endif
+                         "\n"
+#endif
+                         "built on " ISODATE ".\n";
+    IPTR CParams[3];
+    char *banner;
+    CParams[0] = (IPTR)TaggedOpenLibrary(-2);
+    CParams[1] = (IPTR)TaggedOpenLibrary(-3);
+    CParams[2] = (IPTR)CExtra;
+    banner = AllocVec(strlen((char *)CParams[0]) + strlen((char *)CParams[1]) + strlen((char *)CParams[2]) + 1, MEMF_CLEAR);
+    RawDoFmt("%s%s\n%s", (RAWARG)CParams, RAWFMTFUNC_STRING, banner);
+    return banner;
+}
