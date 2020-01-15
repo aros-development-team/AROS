1eb562f2eba14c1ca942ebf4053491817b5677c7
Kalamatee <kalamatee@gmail.com>
2020-01-15 02:23:05 +0000

    use the values provided by genmodule. re-write the autodoc description. TODO: use the TaggedOpenLibrary strings in dos/boot.c

--- rom/exec/taggedopenlibrary.c
+++ rom/exec/taggedopenlibrary.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2001, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2020, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc: Shortcut OpenLibrary call for system modules (private!)
@@ -8,6 +8,8 @@
 
 #include <aros/libcall.h>
 #include <proto/exec.h>
+
+#include LC_LIBDEFS_FILE
 
 static const char * const libnames[] =
 {
@@ -26,12 +28,12 @@ static const char * const libnames[] =
 static const char * const copyrights[] =
 {
     "AROS Research Operating System (AROS)",
-    "Copyright © 1995-1997 ",
+    "Copyright © 1995-2020 ",
     "AROS - The AROS Research OS  ",
     "Other parts © by respective owners.",
     "ALPHA ",
-    "exec.library",
-    "exec 41.11 (27.09.1998)\r\n"
+    MOD_NAME_STRING,
+    "exec " MOD_VERS_STRING " (" MOD_DATE_STRING ")\r\n"
 };
 
 /*****i* exec.library/TaggedOpenLibrary **************************************
@@ -48,10 +50,7 @@ static const char * const copyrights[] =
 	struct ExecBase *, SysBase, 135, Exec)
 
 /*  FUNCTION
-	Opens a library given by tag. This is mainly meant as a shortcut so
-	other system modules don't have to contain the complete library name
-	string, to save ROM space. Additionaly, this call can be used to get a
-	pointer to one of the system copyright notices and other strings.
+	Opens a library given by tag.
 
 	All libraries will be opened with version number 0.
 
@@ -65,11 +64,12 @@ static const char * const copyrights[] =
 	Pointer to library or pointer to text string.
 
     NOTES
-	This is an *INTERNAL* function, and is only meant to provide backwards
-	compatibility until all original Amiga system ROM modules that use it
-	have been implemented as part of AROS. This function *WILL BE REMOVED*
-	in the future. *DO NOT USE!* This can not be emhasized enough. This
-	also applies to AROS system programmers.
+	THIS FUNCTION IS PRIVATE/INTERNAL.
+	TaggedOpenLibrary provides a means for  system ROM modules
+	to open the standard system libraries without having to include
+	the library name in string form.
+	TaggedOpenLibrary also provides the standard system copyright
+	notice.
 
     EXAMPLE
 
