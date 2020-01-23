e088295d9eeebaa1dcbcb8279eacc079d5b92cac
Kalamatee <kalamatee@gmail.com>
2020-01-23 03:32:34 +0000

    disable debug and update copyright

--- workbench/libs/diskfont/diskfont_io.c
+++ workbench/libs/diskfont/diskfont_io.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 1995-2010, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2020, The AROS Development Team. All rights reserved.
     $Id$
 
     Functions for reading disk font files.
@@ -22,7 +22,6 @@
 
 /****************************************************************************************/
 
-#define DEBUG 1
 #include <aros/debug.h>
 
 /****************************************************************************************/
