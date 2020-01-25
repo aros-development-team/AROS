08d98b14140728c76f5a6f7771f537c65caaf6d3
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2020-01-25 20:08:37 +0100

    ALSA: Reformat audio mode file

--- workbench/devs/AHI/Drivers/Alsa/ALSA.s
+++ workbench/devs/AHI/Drivers/Alsa/ALSA.s
@@ -1,22 +1,22 @@
 
-    FORM_START    AHIM
+    FORM_START  AHIM
 
-    CHUNK_START    AUDN
-    .asciz        "alsa"
+    CHUNK_START AUDN
+    .asciz      "alsa"
     CHUNK_END
 
-    CHUNK_START    AUDM
+    CHUNK_START AUDM
 1:
-    LONG2        AHIDB_AudioID,    0x00420002
-    LONG2        AHIDB_Volume,    TRUE
-    LONG2        AHIDB_Panning,    TRUE
-    LONG2        AHIDB_Stereo,    TRUE
-    LONG2        AHIDB_HiFi,    FALSE
-    LONG2        AHIDB_MultTable,FALSE
-    LONG2        AHIDB_Name,    2f-1b
+    LONG2       AHIDB_AudioID,     0x00420002
+    LONG2       AHIDB_Volume,      TRUE
+    LONG2       AHIDB_Panning,     TRUE
+    LONG2       AHIDB_Stereo,      TRUE
+    LONG2       AHIDB_HiFi,        FALSE
+    LONG2       AHIDB_MultTable,   FALSE
+    LONG2       AHIDB_Name,        2f-1b
     LONG        TAG_DONE
 2:
-    .asciz        "ALSA:16 bit stereo++"
+    .asciz      "ALSA:16 bit stereo++"
     CHUNK_END
 
 
