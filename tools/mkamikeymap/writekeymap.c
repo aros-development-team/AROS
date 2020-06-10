ac271bda2e07dca7447bdd8d13bf9b577a5344aa
Kalamatee <kalamatee@gmail.com>
2020-06-10 15:10:58 +0100

    store the number of relocations in BE format. terminate the relocation block, and output the final HUNK_END.

--- tools/mkamikeymap/writekeymap.c
+++ tools/mkamikeymap/writekeymap.c
@@ -53,7 +53,7 @@ BOOL writeKeyMap(struct config *cfg)
 {
     FILE *out;
     struct Node *stNode;
-    ULONG stdtsize = 0, hunksize, reloccnt = 0;
+    ULONG stdtsize = 0, hunksize, reloccnt = 0, reloctmp;
     BOOL doverbose = cfg->verbose;
     D(doverbose = TRUE;)
     ULONG tmp;
@@ -67,6 +67,7 @@ BOOL writeKeyMap(struct config *cfg)
     if (doverbose)
         fprintf(stdout, "creating keymap '%s' ('%s')\n", cfg->keymap, kmname);
 
+    reloctmp = 0;
     for (i = 0; i < 0x40; i ++)
     {
         if ((cfg->LoKeyMapTypes[i] & (KCF_DEAD|KCF_STRING)) != 0)
@@ -74,10 +75,14 @@ BOOL writeKeyMap(struct config *cfg)
             stNode = (struct Node *)cfg->LoKeyMap[i];
             D(fprintf(stdout, "node @ 0x%p\n", stNode);)
             stdtsize += (UBYTE)stNode->ln_Pri;
-            reloccnt++;
+            reloctmp++;
         }
     }
 
+    reloccnt = reloctmp;
+    D(fprintf(stdout, "%u lokey relocations\n", reloccnt);)
+
+    reloctmp = 0;
     for (i = 0; i < 0x38; i ++)
     {
         if ((cfg->HiKeyMapTypes[i] & (KCF_DEAD|KCF_STRING)) != 0)
@@ -85,9 +90,12 @@ BOOL writeKeyMap(struct config *cfg)
             stNode = (struct Node *)cfg->HiKeyMap[i];
             D(fprintf(stdout, "node @ 0x%p\n", stNode);)
             stdtsize += (UBYTE)stNode->ln_Pri;
-            reloccnt++;
+            reloctmp++;
         }
     }
+
+    reloccnt += reloctmp;
+    D(fprintf(stdout, "%u hikey relocations\n", reloctmp);)
 
     if (doverbose)
     {
@@ -107,7 +115,7 @@ BOOL writeKeyMap(struct config *cfg)
 
     relocData = malloc((reloccnt + 12) << 2);
     relocData[r++] = htonl(HUNK_RELOC32);
-    relocData[r++] = reloccnt + 9;
+    relocData[r++] = htonl(reloccnt + 9);
     relocData[r++] = 0;
 
     hunkData = (APTR)((IPTR)hunkRaw + sizeof(struct KeyMap_Hunk));
@@ -171,6 +179,11 @@ BOOL writeKeyMap(struct config *cfg)
         }
     }
 
+    if ((r - 3) != (reloccnt + 9))
+    {
+        fprintf(stdout, "expected %u relocations - %u performed\n", (reloccnt + 9), (r - 3));
+    }
+
     out = fopen(cfg->keymap, "w");
     if (out==NULL)
     {
@@ -199,13 +212,18 @@ BOOL writeKeyMap(struct config *cfg)
     fwrite(hunkRaw, hunksize, 1, out);
 
     if (doverbose)
-        fprintf(stdout, "wrote %d bytes hunk code\n", (ntohl(hunkRaw->Length) << 2));
+        fprintf(stdout, "wrote %u bytes hunk code\n", (ntohl(hunkRaw->Length) << 2));
 
     /* write relocs .. */
     fwrite(relocData, ((reloccnt + 12) << 2), 1, out);
+    tmp = 0; // terminate the relocations ...
+    fwrite(&tmp, 4, 1, out);
 
     if (doverbose)
-        fprintf(stdout, "wrote %d bytes relocation data\n", ((reloccnt + 12) << 2));
+        fprintf(stdout, "wrote %u bytes relocation data\n", ((reloccnt + 12) << 2));
+
+    tmp = htonl(HUNK_END);
+    fwrite(&tmp, 4, 1, out);
 
     fclose(out);
 }
