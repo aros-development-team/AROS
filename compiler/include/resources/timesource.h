f386ade28799f7b37f3ff9c0187725053c699eac
Kalamatee <kalamatee@gmail.com>
2020-06-25 01:26:15 +0100

    provide more timesource tags

--- compiler/include/resources/timesource.h
+++ compiler/include/resources/timesource.h
@@ -11,10 +11,16 @@
 
 #include <utility/tagitem.h>
 
-#define TIMESOURCE_TAG_BASE	(TAG_USER + 0x0AAA0000)
+#define TIMESOURCE_TAG_BASE	        (TAG_USER + 0x0AAA0000)
 
 #define TIMESOURCE_COUNT	        (TIMESOURCE_TAG_BASE + 0)	        /* Number of units exposed by the resource              */
+#define TIMESOURCE_ID                   (TIMESOURCE_TAG_BASE + 1)	        /* ID (e.g. "PIT", "HPET")                              */
+#define TIMESOURCE_FRIENDLY             (TIMESOURCE_TAG_BASE + 2)	        /* Friendly Name                                        */
+#define TIMESOURCE_FREQUENCY            (TIMESOURCE_TAG_BASE + 3)	        /* Frequency the timesource operates at                 */
+#define TIMESOURCE_PERIODIC	        (TIMESOURCE_TAG_BASE + 4)	        /* Timesource supports periodic mode                    */
+#define TIMESOURCE_ONESHOT	        (TIMESOURCE_TAG_BASE + 5)	        /* Timesource supports oneshot mode                     */
 
-#define TIMESOURCE_UNIT_OWNER	(TIMESOURCE_TAG_BASE + 100)	/* (const strcut Node *) Current owner of the unit      */
+#define TIMESOURCE_UNIT_ID              (TIMESOURCE_TAG_BASE + 100)	        /* ID                                                   */
+#define TIMESOURCE_UNIT_OWNER	        (TIMESOURCE_TAG_BASE + 101)	        /* (const strcut Node *) Current owner of a unit        */
 
 #endif /* !RESOURCES_TIMESOURCE_H */
