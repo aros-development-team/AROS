16badb06dc0afbba40d93dbcb3d63c7bcb19d886
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2020-07-14 19:56:29 +0200

    Tabs->spaces + align comments

--- arch/x86_64-all/posixc/vfork.s
+++ arch/x86_64-all/posixc/vfork.s
@@ -16,14 +16,14 @@
     .set    stack,   15*8
 
 AROS_CDEFNAME(vfork):
-    lea     (-bufsize)(%rsp), %rsp /* _JMPLEN + 1 longs on the stack
-		                   it's our temporary jmp_buf */
+    lea     (-bufsize)(%rsp), %rsp  /* _JMPLEN + 1 longs on the stack
+                                    it's our temporary jmp_buf */
     mov     %rsp, %rdi
-    call    setjmp              /* fill jmp_buf on the stack with
-	                           current register values */
-    mov     bufsize(%rsp), %rax    /* set return address in jmp_buf */
-    mov     %rax, 0(%rdi)       /* to this function call return 
-	                           address */
-    lea     bufsize(%rsp), %rax    /* set stack value in jmp_buf */
-    mov     %rax, 120(%rdi)     /* this function call */
-    call    __vfork             /* __vfork call won't return */
+    call    setjmp                  /* fill jmp_buf on the stack with
+                                    current register values */
+    mov     bufsize(%rsp), %rax     /* set return address in jmp_buf */
+    mov     %rax, 0(%rdi)           /* to this function call return
+                                    address */
+    lea     bufsize(%rsp), %rax     /* set stack value in jmp_buf */
+    mov     %rax, 120(%rdi)         /* this function call */
+    call    __vfork                 /* __vfork call won't return */
