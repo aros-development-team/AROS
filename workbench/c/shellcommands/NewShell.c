ec068fa40082896e66cb3adf6dfe63e9eacece42
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2020-11-11 09:54:36 +0100

    tabs -> spaces

--- workbench/c/shellcommands/NewShell.c
+++ workbench/c/shellcommands/NewShell.c
@@ -27,46 +27,43 @@
         the active one. The new shell inherits most attributes of the parent
         shell like the current directory, stack size, prompt and so on.
         However, it is completely independent of the parent shell.
-	    The window belonging to the new shell may be specified by
+        The window belonging to the new shell may be specified by
         using the WINDOW keyword.
 
     INPUTS
 
         WINDOW  --  Specification of the shell window
 
-	            X         --  number of pixels from the left edge of 
-		                  the screen
-		    Y         --  number of pixels from the top edge of 
-		                  the screen
-		    WIDTH     --  width of the shell window in pixels
-		    HEIGHT    --  height of the shell window in pixels
-		    TITLE     --  text to appear in the shell window's 
-		                  title bar
-		    AUTO      --  the window automatically appears when the
-		                  program needs input or output
-		    ALT       --  the window appears in the specified size
-		                  and position when the zoom gadget is clicked
-		    BACKDROP  --  the window is a backdrop window
-		    CLOSE     --  include a close gadget
-		    INACTIVE  --  the window is not made active when opened
-		    NOBORDER  --  the window is borderless, only the size,
-		                  depth and zoom gadgets are available
-		    NOCLOSE   --  the window has no close gadget
-		    NODEPTH   --  the window has no depth gadget
-		    NODRAG    --  the window cannot be drag; implies NOCLOSE
-		    NOSIZE    --  the window has no size gadget
-		    SCREEN    --  name of a public screen to open the window on
-		    SIMPLE    --  if the window is enlarged the text expands to
-		                  fill the available space
-		    SMART     --  if the window is enlarged the text will not
-                                  expand
-		    WAIT      --  the window can only be closed by selecting
-                                  the close gadget or entering CTRL-\.
+            X         --  number of pixels from the left edge of the screen
+            Y         --  number of pixels from the top edge of the screen
+            WIDTH     --  width of the shell window in pixels
+            HEIGHT    --  height of the shell window in pixels
+            TITLE     --  text to appear in the shell window's title bar
+            AUTO      --  the window automatically appears when the
+                          program needs input or output
+            ALT       --  the window appears in the specified size
+                          and position when the zoom gadget is clicked
+            BACKDROP  --  the window is a backdrop window
+            CLOSE     --  include a close gadget
+            INACTIVE  --  the window is not made active when opened
+            NOBORDER  --  the window is borderless, only the size,
+                          depth and zoom gadgets are available
+            NOCLOSE   --  the window has no close gadget
+            NODEPTH   --  the window has no depth gadget
+            NODRAG    --  the window cannot be drag; implies NOCLOSE
+            NOSIZE    --  the window has no size gadget
+            SCREEN    --  name of a public screen to open the window on
+            SIMPLE    --  if the window is enlarged the text expands to
+                          fill the available space
+            SMART     --  if the window is enlarged the text will not
+                          expand
+            WAIT      --  the window can only be closed by selecting
+                          the close gadget or entering CTRL-\.
 
 
         FROM    --  File to execute before resorting to normal shell
-	            operations. If nothing is specified S:Shell-Startup
-		    is used.
+                    operations. If nothing is specified S:Shell-Startup
+                    is used.
 
     RESULT
 
@@ -117,27 +114,27 @@ AROS_SHA(LONG *, ,STACK,/N,NULL))
 
     if (win)
     {
-	struct TagItem tags[] =
+        struct TagItem tags[] =
         {
             { SYS_Asynch,      TRUE       },
-	    { SYS_Background,  FALSE      },
-	    { SYS_Input,       (IPTR)win  },
-	    { SYS_Output,      (IPTR)NULL },
-	    { SYS_Error,       (IPTR)NULL },
-	    { SYS_ScriptInput, (IPTR)from },
-	    { SYS_UserShell,   TRUE       },
-	    { NP_StackSize,    stack      },
-	    { TAG_DONE,        0          }
+            { SYS_Background,  FALSE      },
+            { SYS_Input,       (IPTR)win  },
+            { SYS_Output,      (IPTR)NULL },
+            { SYS_Error,       (IPTR)NULL },
+            { SYS_ScriptInput, (IPTR)from },
+            { SYS_UserShell,   TRUE       },
+            { NP_StackSize,    stack      },
+            { TAG_DONE,        0          }
         };
 
         rc = SystemTagList("", tags);
-	if (rc != -1)
-	{
-	    win  = BNULL;
-	    from = BNULL;
-	}
-	else
-	    rc = RETURN_FAIL;
+        if (rc != -1)
+        {
+            win  = BNULL;
+            from = BNULL;
+        }
+        else
+            rc = RETURN_FAIL;
     }
     else
     {
