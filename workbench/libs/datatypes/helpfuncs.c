1e1bfc4e97aa5d66574fbd34cf6df6f7d7d031dc
Matthias Rustler <mrustler@gmx.de>
2020-11-28 14:52:36 +0100

    Fix for the problem that ascii files weren't recognized by the Datatypes.library.
    C:AddDatatypes creates the root classes ascii and binary with Masklen set to 0.

--- workbench/libs/datatypes/helpfuncs.c
+++ workbench/libs/datatypes/helpfuncs.c
@@ -3,7 +3,7 @@
     $Id$
 */
 
-#define DEBUG 0
+//#define DEBUG 1
 #include <aros/debug.h>
 
 #define USE_BOOPSI_STUBS
@@ -237,7 +237,7 @@ struct CompoundDataType *ExamineLock(BPTR lock, struct FileInfoBlock *fib, struc
                         if((CheckArray = AllocVec((ULONG)(CheckSize)+1,
                                                   MEMF_CLEAR)))
                         {
-                            D(bug("datatypes.library/ExamineLock: Alloced CheckArray\n"));
+                            D(bug("datatypes.library/ExamineLock: Allocated CheckArray\n"));
                             
                             if((CheckSize = Read(file, CheckArray, 
                                                  (ULONG)CheckSize)) > 0)
@@ -332,7 +332,14 @@ struct CompoundDataType *FindDtInList(struct Library *DataTypesBase,
                 cur->DT.dtn_Node1.ln_Succ;
                 cur = (struct CompoundDataType *)cur->DT.dtn_Node1.ln_Succ)
         {
-            if (!(cur->DTH.dth_MaskLen) && (cur->Function))
+            /* we must handle the root datatypes because they have cur->DTH.dth_MaskLen set to 0 */
+            if (!strcmp(cur->DTH.dth_Name, "ascii") || !strcmp(cur->DTH.dth_Name, "binary"))
+            {
+                D(bug("[FindDtInList] base class %s found\n", cur->DTH.dth_Name));
+                found = TRUE;
+            }
+            
+            if (!found && !(cur->DTH.dth_MaskLen) && (cur->Function))
             {
                 D(bug("[FindDtInList] *** Calling %s Match Function @ 0x%p\n", cur->DT.dtn_Node1.ln_Name, cur->Function));
                 found = (cur->Function)(dthc);
