e001bf6d9ce68a1560c5d0b6da8d669499e3edb6
Kalamatee <kalamatee@gmail.com>
2020-12-17 22:08:38 +0000

    add some debug. add missing newline to some debug output. make sure the AVX features are enabled on secondary AP's if the system reports it is used.

--- arch/all-pc/kernel/smp_exec.c
+++ arch/all-pc/kernel/smp_exec.c
@@ -74,16 +74,16 @@ struct Task *cpu_InitBootStrap(cpuid_t cpuNo)
     /* Build bootstraps memory list */
     if ((ml = AllocMem(bstaskmlsize, MEMF_PUBLIC|MEMF_CLEAR)) == NULL)
     {
-        bug("[Kernel:%03u] FATAL : Failed to allocate memory for bootstrap task", cpuNo);
+        bug("[Kernel:%03u] FATAL : Failed to allocate memory for bootstrap task\n", cpuNo);
         return NULL;
     }
 
-    ml->ml_NumEntries      = 2;
+    D(bug("[Kernel:%03u] Allocated memory list for bootstrap task\n", cpuNo);)
 
     ml->ml_ME[0].me_Length = sizeof(struct Task);
     if ((ml->ml_ME[0].me_Addr = AllocMem(sizeof(struct Task),    MEMF_PUBLIC|MEMF_CLEAR)) == NULL)
     {
-        bug("[Kernel:%03u] FATAL : Failed to allocate task for bootstrap", cpuNo);
+        bug("[Kernel:%03u] FATAL : Failed to allocate task for bootstrap\n", cpuNo);
         FreeMem(ml, bstaskmlsize);
         return NULL;
     }
@@ -92,6 +92,32 @@ struct Task *cpu_InitBootStrap(cpuid_t cpuNo)
 
     NEWLIST(&bstask->tc_MemEntry);
     AddHead(&bstask->tc_MemEntry, &ml->ml_Node);
+
+    if (KernelBase->kb_ContextSize > AROS_ROUNDUP2(sizeof(struct AROSCPUContext), 16) + sizeof(struct FPFXSContext))
+    {
+        D(bug("[Kernel:%03u] %s: Enabling AVX ...\n", cpuNo, __func__));
+        D(bug("[Kernel:%03u] %s: Adjusting CR0 flags\n", cpuNo, __func__);)
+        /* Enable monitoring media instruction to generate #NM when CR0.TS is set.
+         * Disable coprocessor emulation.
+         */
+        wrcr(cr0, (rdcr(cr0) & ~_CR0_EM) | _CR0_MP);
+        
+        D(bug("[Kernel:%03u] %s: Enable #XF & XGETVB\n", cpuNo, __func__);)
+        /* Enable #XF instead of #UD when a SIMD exception occurs
+         * Enable xgetvb/xsetvb
+         */ 
+        wrcr(cr4, rdcr(cr4) | _CR4_OSXMMEXCPT | _CR4_OSXSBV);
+        D(bug("[Kernel:%03u] %s: Enable Saving AVX context\n", cpuNo, __func__);)
+        asm volatile (
+            "    xor        %%rcx, %%rcx\n\t"
+            "    xgetbv\n\t"                                                            /* Load XCR0 register                           */
+            "    or         $0b111, %%eax\n\t"                                          /* Set FPU, XMM, YMM bits                       */
+            "    xsetbv\n\t"                                                            /* Save back to XCR0                            */
+            : : : "%eax",  "%rcx"
+            );
+        D(bug("[Kernel:%03u] %s: AVX Ready!\n", cpuNo, __func__);)
+    }
+    D(bug("[Kernel:%03u] %s: Allocating CPU context...\n", cpuNo, __func__));
 
     if ((bsctx = KrnCreateContext()) == NULL)
     {
@@ -108,10 +134,14 @@ struct Task *cpu_InitBootStrap(cpuid_t cpuNo)
     if ((ml->ml_ME[1].me_Addr = AllocMem(20, MEMF_PUBLIC|MEMF_CLEAR)) == NULL)
     {
         bug("[Kernel:%03u] FATAL : Failed to allocate the bootstrap Task name\n", cpuNo);
+        KrnDeleteContext(bsctx);
         FreeMem(ml->ml_ME[0].me_Addr, ml->ml_ME[0].me_Length);
         FreeMem(ml, bstaskmlsize);
         return NULL;
     }
+
+    ml->ml_NumEntries      = 2;
+    D(bug("[Kernel:%03u] %s: Task Memory Allocated\n", cpuNo, __func__);)
 
     bstask->tc_Node.ln_Name = ml->ml_ME[1].me_Addr;
     bstNameArg[0] = cpuNo;
@@ -126,6 +156,7 @@ struct Task *cpu_InitBootStrap(cpuid_t cpuNo)
     if (!Exec_InitETask(bstask, NULL, SysBase))
     {
         bug("[Kernel:%03u] FATAL : Failed to initialize bootstrap ETask\n", cpuNo);
+        KrnDeleteContext(bsctx);
         FreeMem(ml->ml_ME[1].me_Addr, ml->ml_ME[1].me_Length);
         FreeMem(ml->ml_ME[0].me_Addr, ml->ml_ME[0].me_Length);
         FreeMem(ml, bstaskmlsize);
@@ -133,18 +164,23 @@ struct Task *cpu_InitBootStrap(cpuid_t cpuNo)
     }
     bstask->tc_UnionETask.tc_ETask->et_RegFrame = bsctx;
 
+    D(bug("[Kernel:%03u] %s: ETask initialized\n", cpuNo, __func__);)
+
     /* the bootstrap can only run on this CPU */
     IntETask(bstask->tc_UnionETask.tc_ETask)->iet_CpuNumber = cpuNo;
     IntETask(bstask->tc_UnionETask.tc_ETask)->iet_CpuAffinity = KrnAllocCPUMask();
     if (!IntETask(bstask->tc_UnionETask.tc_ETask)->iet_CpuAffinity)
     {
         bug("[Kernel:%03u] FATAL : Failed to initialize bootstrap CPU Affinity\n", cpuNo);
+        KrnDeleteContext(bsctx);
         FreeMem(ml->ml_ME[1].me_Addr, ml->ml_ME[1].me_Length);
         FreeMem(ml->ml_ME[0].me_Addr, ml->ml_ME[0].me_Length);
         FreeMem(ml, bstaskmlsize);
         return NULL;
     }
     KrnGetCPUMask(cpuNo, IntETask(bstask->tc_UnionETask.tc_ETask)->iet_CpuAffinity);
+
+    D(bug("[Kernel:%03u] %s: Initialization Complete\n", cpuNo, __func__);)
 
     bsctx->Flags = 0;
 
