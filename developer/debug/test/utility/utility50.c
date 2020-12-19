8cd6124cdc2f80c449fc922eae2d899f25cfbe84
Kalamatee <kalamatee@gmail.com>
2020-12-19 02:07:00 +0000

    delint. remove erroneous debug arg.

--- developer/debug/test/utility/utility50.c
+++ developer/debug/test/utility/utility50.c
@@ -17,27 +17,27 @@ int main(void)
     args[1] = 12345;
     
     // strlen must always be 11
-    printf("\nTesting VSNPrintf\n", buffer);
+    printf("\nTesting VSNPrintf\n");
 
     LONG count = VSNPrintf(buffer, sizeof buffer, "ab%scd%ldef", (RAWARG)args);
-    printf("\nbuffer '%s' len %d count %d\n", buffer, Strlen(buffer), count);
+    printf("\nbuffer '%s' len %d count %d\n", buffer, (int)Strlen(buffer), count);
 
     printf("\nTesting Strlcpy\n", buffer);
     
     Strlcpy(buffer, "123", sizeof(buffer));
-    printf("\nbuffer '%s' len %d (should be 3)\n", buffer, Strlen(buffer));
+    printf("\nbuffer '%s' len %d (should be 3)\n", buffer, (int)Strlen(buffer));
 
     Strlcpy(buffer, "APPLE", 5);
-    printf("buffer '%s' len %d (should be 4)\n", buffer, Strlen(buffer));
+    printf("buffer '%s' len %d (should be 4)\n", buffer, (int)Strlen(buffer));
     
     Strlcpy(buffer, "12345678901234567890", sizeof buffer);
-    printf("buffer '%s' len %d (should be 11)\n", buffer, Strlen(buffer));
+    printf("buffer '%s' len %d (should be 11)\n", buffer, (int)Strlen(buffer));
 
     printf("\nTesting Strlcat\n", buffer);
 
     Strlcpy(buffer, "12345678", sizeof buffer);
     Strlcat(buffer, "abcdefghijklmnopqr", sizeof buffer);
-    printf("\nbuffer '%s' len %d\n", buffer, Strlen(buffer));
+    printf("\nbuffer '%s' len %d\n", buffer, (int)Strlen(buffer));
     
     return 0;
 }
