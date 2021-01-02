64f7ee70705b23a947d581302e24c17b30c5caca
Kalamatee <kalamatee@gmail.com>
2021-01-02 14:05:47 +0000

    fix padding

--- tools/flexcat/src/vasprintf.c
+++ tools/flexcat/src/vasprintf.c
@@ -36,22 +36,22 @@
 
 int vasprintf(char **ptr, const char * format, va_list ap)
 {
-  int ret;
-  va_list ap2;
+    int ret;
+    va_list ap2;
 
-  *ptr = NULL;
+    *ptr = NULL;
 
-  VA_COPY(ap2, ap);
-  ret = vsnprintf(NULL, 0, format, ap2);
-  if(ret <= 0)
+    VA_COPY(ap2, ap);
+    ret = vsnprintf(NULL, 0, format, ap2);
+    if(ret <= 0)
+        return ret;
+
+    *ptr = (char *)malloc(ret+1);
+    if(*ptr == NULL)
+        return -1;
+
+    VA_COPY(ap2, ap);
+    ret = vsnprintf(*ptr, ret+1, format, ap2);
+
     return ret;
-
-  *ptr = (char *)malloc(ret+1);
-  if(*ptr == NULL)
-    return -1;
-
-  VA_COPY(ap2, ap);
-  ret = vsnprintf(*ptr, ret+1, format, ap2);
-
-  return ret;
 }
