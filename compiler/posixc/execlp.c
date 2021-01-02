5905d0b56b122bdc0ce92ac9956168fe6ccd018f
Kalamatee <kalamatee@gmail.com>
2021-01-02 14:08:24 +0000

    make sure va_end is called on the list.

--- compiler/posixc/execlp.c
+++ compiler/posixc/execlp.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 2008-2013, The AROS Development Team. All rights reserved.
+    Copyright © 2008-2020, The AROS Development Team. All rights reserved.
     $Id$
 
     POSIX.1-2008 function execlp().
@@ -58,6 +58,7 @@
     
     if(!(argv = __exec_valist2array(arg, args)))
     {
+        va_end(args);
         errno = ENOMEM;
         return -1;
     }
