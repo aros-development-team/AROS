b4a169275ec4d0fa452367bd84b6e807aa7abd15
Kalamatee <kalamatee@gmail.com>
2021-01-06 15:29:24 +0000

    correct expected result

--- developer/debug/test/dos/cunit-dos-fileseek.c
+++ developer/debug/test/dos/cunit-dos-fileseek.c
@@ -96,7 +96,7 @@ void testSEEK(void)
         CU_ASSERT(-1 != Seek( file, 4, OFFSET_CURRENT ));
         CU_ASSERT(-1 != Read( file, &buffer[7], 11 ));
         CU_ASSERT(-1 != Seek( file, 4, OFFSET_BEGINNING ));
-        CU_ASSERT(11 != Read( file, &buffer[18], 11 ));
+        CU_ASSERT(11 == Read( file, &buffer[18], 11 ));
         CU_ASSERT(-1 != Seek( file, -1, OFFSET_END ));
         CU_ASSERT(0 == Read( file, &buffer[29], 1 ));
     }
