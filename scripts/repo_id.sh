bd335a84f396af425d9c984c2883dbf666e145d1
Kalamatee <kalamatee@gmail.com>
2021-01-09 17:10:02 +0000

    use double quotes ..

--- scripts/repo_id.sh
+++ scripts/repo_id.sh
@@ -3,7 +3,7 @@
 # get repository ID via svn or via git
 #
 
-inside_git_repo=$(cd $1 && git rev-parse --is-inside-work-tree 2>/dev/null)
+inside_git_repo=$(cd "$1" && git rev-parse --is-inside-work-tree 2>/dev/null)
 
 if test -d "$1/.svn"; then
     cd "$1"
