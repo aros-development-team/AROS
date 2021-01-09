6c3d779fa9bd646da543d187055b31f06548be57
Kalamatee <kalamatee@gmail.com>
2021-01-09 15:55:30 +0000

    use double quotes and $() notation

--- scripts/repo_rev.sh
+++ scripts/repo_rev.sh
@@ -3,13 +3,13 @@
 # get repository revision via svn or via git
 #
 
-inside_git_repo=`cd $1 && git rev-parse --is-inside-work-tree 2>/dev/null`
+inside_git_repo=$(cd "$1" && git rev-parse --is-inside-work-tree 2>/dev/null)
 
-if test -d $1/.svn; then
-    svn info $1 | sed -n 's/Revision: //p'
+if test -d "$1/.svn"; then
+    svn info "$1" | sed -n 's/Revision: //p'
 else
     if [ "$inside_git_repo" = "true" ]; then
-        cd $1
+        cd "$1"
         git rev-parse --short HEAD
     else
         echo "NoRev"
--- scripts/repo_type.sh
+++ scripts/repo_type.sh
@@ -3,9 +3,9 @@
 # get repository type
 #
 
-inside_git_repo=`cd $1 && git rev-parse --is-inside-work-tree 2>/dev/null`
+inside_git_repo=$(cd "$1" && git rev-parse --is-inside-work-tree 2>/dev/null)
 
-if test -d $1/.svn; then
+if test -d "$1/.svn"; then
     echo "SVN"
 else
     if [ "$inside_git_repo" = "true" ]; then
