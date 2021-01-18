8695c0fdd6aed6e298e058e4dc8ae6cb28ac6f56
Matthias Rustler <mrustler@gmx.de>
2021-01-18 19:47:14 +0100

    Missing files with copyright year added

--- scripts/bumpyear.sh
+++ scripts/bumpyear.sh
@@ -12,6 +12,7 @@ DOC_PATH=./documentation
 sed -i "$SUB" $SOURCE_PATH/arch/m68k-amiga/boot/ext_entry.S
 sed -i "$SUB" $SOURCE_PATH/arch/m68k-amiga/boot/rom_entry.S
 sed -i "$SUB" $SOURCE_PATH/rom/dos/boot.c
+sed -i "$SUB" $SOURCE_PATH/rom/exec/taggedopenlibrary.c
 
 # warning: the following are within a submodule
 sed -i "$SUB" $SOURCE_PATH/workbench/system/AboutAROS/catalogs/aboutaros.cd
@@ -42,3 +43,12 @@ sed -i "$SUB" $DOC_PATH/targets/www/template/languages/pl
 sed -i "$SUB" $DOC_PATH/targets/www/template/languages/pt
 sed -i "$SUB" $DOC_PATH/targets/www/template/languages/ru
 sed -i "$SUB" $DOC_PATH/targets/www/template/languages/sv
+
+START_YEAR=2002
+SUB=s/$START_YEAR-$OLD_YEAR/$START_YEAR-$NEW_YEAR/g
+echo "$SUB"
+
+sed -i "$SUB" $SOURCE_PATH/workbench/libs/muimaster/catalogs/muimaster.cd
+sed -i "$SUB" $SOURCE_PATH/workbench/libs/muimaster/catalogs/german.ct
+sed -i "$SUB" $SOURCE_PATH/workbench/libs/muimaster/catalogs/polish.ct
+sed -i "$SUB" $SOURCE_PATH/workbench/libs/muimaster/catalogs/russian.ct
