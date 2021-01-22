2bd069257f5279142caa9d780f44156adc3b61d1
Kalamatee <kalamatee@gmail.com>
2021-01-22 13:50:25 +0000

    fix/silence some issues from codacy

--- developer/debug/gdb/ignore_errors.py
+++ developer/debug/gdb/ignore_errors.py
@@ -5,19 +5,22 @@ import gdb
 
 class IgnoreErrorsCommand (gdb.Command):
     """Execute a single command, ignoring all errors.
-       Only one-line commands are supported.
-       This is primarily useful in scripts."""
+    
+    Only one-line commands are supported.
+    This is primarily useful in scripts."""
 
     def __init__ (self):
+        """Register ignore-errors with gdb"""
         super (IgnoreErrorsCommand, self).__init__ ("ignore-errors",
                                                     gdb.COMMAND_OBSCURE,
                                                     # FIXME...
                                                     gdb.COMPLETE_COMMAND)
 
+    # pylint: disable=no-self-use
     def invoke (self, arg, from_tty):
         try:
             gdb.execute (arg, from_tty)
-        except:
+        except: # pylint: disable=bare-except
             pass
 
 IgnoreErrorsCommand ()
