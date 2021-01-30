8663921653fdfdfb803d34442a4138703dc46744
Matthias Rustler <mrustler@gmx.de>
2021-01-30 14:19:33 +0100

    cunit-crt-math.c: created tests for almost all C99 math functions

--- developer/debug/test/crt/stdc/math/cunit-crt-math.c
+++ developer/debug/test/crt/stdc/math/cunit-crt-math.c
@@ -3,6 +3,7 @@
 */
 
 #include <math.h>
+#include <fenv.h>
 #include <errno.h>
 
 #include <CUnit/Basic.h>
@@ -10,85 +11,17 @@
 
 #define GRANULARITY 0.000001
 
-#if 0
 /*
  * C99 double versions of functions
  */
 
-done:
-double	sin(double);
-double	cos(double);
-double	tan(double);
-double	acos(double);
-double	asin(double);
-double	atan(double);
-double	atan2(double, double);
-double	sqrt(double);
-
-double	sinh(double);
-double	cosh(double);
-double	tanh(double);
-
-double	asinh(double);
-double	acosh(double);
-double	atanh(double);
-double	exp(double);
-double	exp2(double);
-
+#if 0
 todo:
-double	expm1(double);
-double	frexp(double, int *);	/* fundamentally !__pure2 */
 int	ilogb(double) __pure2;
-double	ldexp(double, int);
-double	log(double);
-double	log10(double);
-double	log1p(double);
-double	log2(double);
-double	logb(double);
-double	modf(double, double *);	/* fundamentally !__pure2 */
 double	scalbn(double, int);
 double	scalbln(double, long);
-
-double	cbrt(double);
-double	fabs(double) __pure2;
-double	hypot(double, double);
-double	pow(double, double);
-
-double	erf(double);
-double	erfc(double);
-double	lgamma(double);
-double	tgamma(double);
-
-double	ceil(double);
-double	floor(double);
-double	nearbyint(double);
-double	rint(double);
-long	lrint(double);
-long long llrint(double);
-double	round(double);
-long	lround(double);
-long long llround(double);
-double	trunc(double);
-
-double	fmod(double, double);
-double	remainder(double, double);
-double	remquo(double, double, int *);
-
-double	copysign(double, double) __pure2;
-double  nan(const char *tagp);
 double	nextafter(double, double);
 double	nexttoward(double, long double);
-
-double	fdim(double, double);
-double	fmax(double, double) __pure2;
-double	fmin(double, double) __pure2;
-
-double	fma(double, double, double);
-
-/* aliases */
-double	gamma(double);
-double	drem(double, double);
-int	finite(double) __pure2;
 #endif
 
 /* The suite initialization function.
@@ -134,29 +67,23 @@ void testTAN(void)
 
 void testASIN(void)
 {
-    double x; // silence codacity
-    
     CU_ASSERT_DOUBLE_EQUAL(asin(0), 0.0, GRANULARITY);
     CU_ASSERT_DOUBLE_EQUAL(asin(-0.5), -0.523599, GRANULARITY);
     CU_ASSERT_DOUBLE_EQUAL(asin(1), M_PI_2, GRANULARITY);
     
-    x = 1.1;
     errno = 0;
-    CU_ASSERT(isnan(asin(x)));
+    CU_ASSERT(isnan(asin(1.1)));
     CU_ASSERT(errno == EDOM);
 }
 
 void testACOS(void)
 {
-    double x;
-    
     CU_ASSERT_DOUBLE_EQUAL(acos(0), M_PI_2, GRANULARITY);
     CU_ASSERT_DOUBLE_EQUAL(acos(-0.5), 2.094395, GRANULARITY);
     CU_ASSERT_DOUBLE_EQUAL(acos(1), 0, GRANULARITY);
     
-    x = 1.1;
     errno = 0;
-    CU_ASSERT(isnan(acos(x)));
+    CU_ASSERT(isnan(acos(1.1)));
     CU_ASSERT(errno == EDOM);
 }
 
@@ -170,7 +97,6 @@ void testATAN(void)
 
 void testATAN2(void)
 {
-    double x;
                               /* y, x */
     CU_ASSERT_DOUBLE_EQUAL(atan2(0, 2), 0.0, GRANULARITY);
     CU_ASSERT_DOUBLE_EQUAL(atan2(2, 2), 0.785398, GRANULARITY);
@@ -181,8 +107,7 @@ void testATAN2(void)
     CU_ASSERT_DOUBLE_EQUAL(atan2(-2, 0), -1.570796, GRANULARITY);
     CU_ASSERT_DOUBLE_EQUAL(atan2(-2, 2), -0.785398, GRANULARITY);
     
-    x = 0;
-    CU_ASSERT_DOUBLE_EQUAL(atan2(x, x), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(atan2(0, 0), 0.0, GRANULARITY);
 }
 
 void testSINH(void)
@@ -251,15 +176,27 @@ void testATANH(void)
 
 void testSQRT(void)
 {
-    double x;
-    
     CU_ASSERT_DOUBLE_EQUAL(sqrt(0), 0.0, GRANULARITY);
     CU_ASSERT_DOUBLE_EQUAL(sqrt(5), 2.236068, GRANULARITY);
 
-    x = -1;
     errno = 0;
-    CU_ASSERT(isnan(sqrt(x)));
+    CU_ASSERT(isnan(sqrt(-1)));
     CU_ASSERT(errno == EDOM);
+}
+
+void testHYPOT(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(hypot(2.1, 3.1), 3.744329, GRANULARITY);
+}
+
+void testPOW(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(pow(2.1, 3.1), 9.974239, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(pow(2.1, -3.1), 0.100258, GRANULARITY);
+    CU_ASSERT(isnan(pow(-2.1, 3.1)));
+    CU_ASSERT(isnan(pow(-2.1, -3.1)));
+    CU_ASSERT_DOUBLE_EQUAL(pow(0, 3.1), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(pow(2.1, 0), 1.0, GRANULARITY);
 }
 
 void testEXP(void)
@@ -274,6 +211,343 @@ void testEXP2(void)
     CU_ASSERT_DOUBLE_EQUAL(exp2(0), 1.0, GRANULARITY);
     CU_ASSERT_DOUBLE_EQUAL(exp2(5), 32.0, GRANULARITY);
     CU_ASSERT_DOUBLE_EQUAL(exp2(-5), 0.031250, GRANULARITY);
+}
+
+void testEXPM1(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(expm1(0), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(expm1(5), 147.413159, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(expm1(-5), -0.993262, GRANULARITY);
+}
+
+void testFREXP(void)
+{
+    int exp;
+    CU_ASSERT_DOUBLE_EQUAL(frexp(1024, &exp), 0.5, GRANULARITY);
+    CU_ASSERT(11 == exp);
+    CU_ASSERT_DOUBLE_EQUAL(frexp(-1024, &exp), -0.5, GRANULARITY);
+    CU_ASSERT(11 == exp);
+    CU_ASSERT_DOUBLE_EQUAL(frexp(0, &exp), 0.0, GRANULARITY);
+    CU_ASSERT(0 == exp);
+}
+
+void testLDEXP(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(ldexp(0, 0), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(ldexp(5, 4), 80.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(ldexp(-5, 4), -80.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(ldexp(5, -4), 0.312500, GRANULARITY);
+}
+
+void testLOG(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(log(4), 1.386294, GRANULARITY);
+
+    errno = 0;
+    CU_ASSERT(isnan(log(-3)));
+    CU_ASSERT(errno == EDOM);
+}
+
+void testLOG10(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(log10(4), 0.602060, GRANULARITY);
+
+    errno = 0;
+    CU_ASSERT(isnan(log10(-3)));
+    CU_ASSERT(errno == EDOM);
+}
+
+void testLOG1P(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(log1p(4), 1.609438, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(log1p(0), 0.0, GRANULARITY);
+
+    errno = 0;
+    CU_ASSERT(isnan(log1p(-3)));
+    CU_ASSERT(errno == EDOM);
+}
+
+void testLOG2(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(log2(4), 2.0, GRANULARITY);
+
+    errno = 0;
+    CU_ASSERT(isnan(log2(-3)));
+    CU_ASSERT(errno == EDOM);
+}
+
+void testLOGB(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(logb(5), 2.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(logb(-5), 2.0, GRANULARITY);
+}
+
+void testMODF(void)
+{
+    double i;
+    CU_ASSERT_DOUBLE_EQUAL(modf(5.5, &i), 0.5, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(i, 5, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(modf(-5.5, &i), -0.5, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(i, -5.0, GRANULARITY);
+}
+
+void testCBRT(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(cbrt(0), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(cbrt(5), 1.709976, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(cbrt(-5), -1.709976, GRANULARITY);
+}
+
+void testFABS(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(fabs(0), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(fabs(5), 5.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(fabs(-5), 5.0, GRANULARITY);
+}
+
+void testERF(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(erf(0), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(erf(1), 0.842701, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(erf(-1), -0.842701, GRANULARITY);
+}
+
+void testERFC(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(erfc(0), 1.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(erfc(1), 0.157299, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(erfc(-1), 1.842701, GRANULARITY);
+}
+
+void testLGAMMA(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(lgamma(1), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(lgamma(2), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(lgamma(5), 3.178054, GRANULARITY);
+}
+
+void testTGAMMA(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(tgamma(1), 1.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(tgamma(2), 1.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(tgamma(5), 24.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(tgamma(5.3), 38.077976, GRANULARITY);
+}
+
+void testCEIL(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(ceil(0), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(ceil(5.3), 6.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(ceil(-7.6), -7.0, GRANULARITY);
+}
+
+void testFLOOR(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(floor(0), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(floor(5.3), 5.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(floor(-7.6), -8.0, GRANULARITY);
+}
+
+void testNEARBYINT(void)
+{
+    fesetround(FE_TONEAREST);
+    CU_ASSERT_DOUBLE_EQUAL(nearbyint(0), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(nearbyint(5.3), 5.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(nearbyint(-7.6), -8.0, GRANULARITY);
+
+    fesetround(FE_DOWNWARD);
+    CU_ASSERT_DOUBLE_EQUAL(nearbyint(0), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(nearbyint(5.3), 5.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(nearbyint(-7.6), -8.0, GRANULARITY);
+
+    fesetround(FE_UPWARD);
+    CU_ASSERT_DOUBLE_EQUAL(nearbyint(0), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(nearbyint(5.3), 6.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(nearbyint(-7.6), -7.0, GRANULARITY);
+
+    fesetround(FE_TOWARDZERO);
+    CU_ASSERT_DOUBLE_EQUAL(nearbyint(0), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(nearbyint(5.3), 5.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(nearbyint(-7.6), -7.0, GRANULARITY);
+}
+
+void testRINT(void)
+{
+    fesetround(FE_TONEAREST);
+    CU_ASSERT_DOUBLE_EQUAL(rint(0), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(rint(5.3), 5.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(rint(-7.6), -8.0, GRANULARITY);
+
+    fesetround(FE_DOWNWARD);
+    CU_ASSERT_DOUBLE_EQUAL(rint(0), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(rint(5.3), 5.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(rint(-7.6), -8.0, GRANULARITY);
+
+    fesetround(FE_UPWARD);
+    CU_ASSERT_DOUBLE_EQUAL(rint(0), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(rint(5.3), 6.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(rint(-7.6), -7.0, GRANULARITY);
+
+    fesetround(FE_TOWARDZERO);
+    CU_ASSERT_DOUBLE_EQUAL(rint(0), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(rint(5.3), 5.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(rint(-7.6), -7.0, GRANULARITY);
+}
+
+void testLRINT(void)
+{
+    fesetround(FE_TONEAREST);
+    CU_ASSERT(0 == lrint(0));
+    CU_ASSERT(5 == lrint(5.3));
+    CU_ASSERT(-8 == lrint(-7.6));
+
+    fesetround(FE_DOWNWARD);
+    CU_ASSERT(0 == lrint(0));
+    CU_ASSERT(5 == lrint(5.3));
+    CU_ASSERT(-8 == lrint(-7.6));
+
+    fesetround(FE_UPWARD);
+    CU_ASSERT(0 == lrint(0));
+    CU_ASSERT(6 == lrint(5.3));
+    CU_ASSERT(-7 == lrint(-7.6));
+
+    fesetround(FE_TOWARDZERO);
+    CU_ASSERT(0 == lrint(0));
+    CU_ASSERT(5 == lrint(5.3));
+    CU_ASSERT(-7 == lrint(-7.6));
+}
+
+void testLLRINT(void)
+{
+    fesetround(FE_TONEAREST);
+    CU_ASSERT(0 == llrint(0));
+    CU_ASSERT(5 == llrint(5.3));
+    CU_ASSERT(-8 == llrint(-7.6));
+
+    fesetround(FE_DOWNWARD);
+    CU_ASSERT(0 == llrint(0));
+    CU_ASSERT(5 == llrint(5.3));
+    CU_ASSERT(-8 == llrint(-7.6));
+
+    fesetround(FE_UPWARD);
+    CU_ASSERT(0 == llrint(0));
+    CU_ASSERT(6 == llrint(5.3));
+    CU_ASSERT(-7 == llrint(-7.6));
+
+    fesetround(FE_TOWARDZERO);
+    CU_ASSERT(0 == llrint(0));
+    CU_ASSERT(5 == llrint(5.3));
+    CU_ASSERT(-7 == llrint(-7.6));
+}
+
+void testROUND(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(round(0), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(round(5.3), 5.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(round(-7.6), -8.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(round(5.5), 6.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(round(-7.5), -8.0, GRANULARITY);
+}
+
+void testLROUND(void)
+{
+    CU_ASSERT(0 == lround(0));
+    CU_ASSERT(5 == lround(5.3));
+    CU_ASSERT(-8 == lround(-7.6));
+    CU_ASSERT(6 == lround(5.5));
+    CU_ASSERT(-8 == lround(-7.5));
+}
+
+void testLLROUND(void)
+{
+    CU_ASSERT(0 == llround(0));
+    CU_ASSERT(5 == llround(5.3));
+    CU_ASSERT(-8 == llround(-7.6));
+    CU_ASSERT(6 == llround(5.5));
+    CU_ASSERT(-8 == llround(-7.5));
+}
+
+void testTRUNC(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(trunc(0), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(trunc(5.3), 5.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(trunc(-7.6), -7.0, GRANULARITY);
+}
+
+void testFMOD(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(fmod(2.1, 5.3), 2.1, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(fmod(5.3, 2.1), 1.1, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(fmod(-20.4, 2.1), -1.5, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(fmod(20.4, -2.1), 1.5, GRANULARITY);
+}
+
+void testREMAINDER(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(remainder(2.1, 5.3), 2.1, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(remainder(5.3, 2.1), -1, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(remainder(-20.4, 2.1), 0.6, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(remainder(20.4, -2.1), -0.6, GRANULARITY);
+}
+
+void testREMQUO(void)
+{
+    int quo;
+    CU_ASSERT_DOUBLE_EQUAL(remquo(2.1, 5.3, &quo), 2.1, GRANULARITY);
+    CU_ASSERT(quo >= 0);
+    CU_ASSERT_DOUBLE_EQUAL(remquo(5.3, 2.1, &quo), -1, GRANULARITY);
+    CU_ASSERT(quo >= 0);
+    CU_ASSERT_DOUBLE_EQUAL(remquo(-20.4, 2.1, &quo), 0.6, GRANULARITY);
+    CU_ASSERT(quo < 0);
+    CU_ASSERT_DOUBLE_EQUAL(remquo(20.4, -2.1, &quo), -0.6, GRANULARITY);
+    CU_ASSERT(quo < 0);
+}
+
+void testCOPYSIGN(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(copysign(2.1, 5.3), 2.1, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(copysign(-5.3, -2.1), -5.3, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(copysign(-20.4, 2.1), 20.4, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(copysign(20.4, -2.1), -20.4, GRANULARITY);
+}
+
+void testNAN(void)
+{
+    CU_ASSERT(isnan(nan("")));
+    CU_ASSERT(isnan(nan("-1")));
+    CU_ASSERT(isnan(nan("2")));
+}
+
+void testFDIM(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(fdim(20.3, 2.1), 18.2, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(fdim(-5.3, -2.1), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(fdim(-20.4, 2.1), 0.0, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(fdim(20.4, -2.1), 22.5, GRANULARITY);
+}
+
+void testFMAX(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(fmax(20.3, 2.1), 20.3, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(fmax(-5.3, -2.1), -2.1, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(fmax(-20.4, 2.1), 2.1, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(fmax(20.4, -2.1), 20.4, GRANULARITY);
+}
+
+void testFMIN(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(fmin(20.3, 2.1), 2.1, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(fmin(-5.3, -2.1), -5.3, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(fmin(-20.4, 2.1), -20.4, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(fmin(20.4, -2.1), -2.1, GRANULARITY);
+}
+
+void testFMA(void)
+{
+    CU_ASSERT_DOUBLE_EQUAL(fma(20.3, 2.1, 4.2), 46.83, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(fma(-5.3, -2.1, 4.2), 15.33, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(fma(-20.4, 2.1, 4.2), -38.64, GRANULARITY);
+    CU_ASSERT_DOUBLE_EQUAL(fma(20.4, -2.1, -4.2), -47.04, GRANULARITY);
 }
 
 int main()
@@ -307,8 +581,44 @@ int main()
         (NULL == CU_add_test(pSuite, "test of acosh()", testACOSH)) ||
         (NULL == CU_add_test(pSuite, "test of atanh()", testATANH)) ||
         (NULL == CU_add_test(pSuite, "test of sqrt()", testSQRT)) ||
+        (NULL == CU_add_test(pSuite, "test of pow()", testPOW)) ||
+        (NULL == CU_add_test(pSuite, "test of hypot()", testHYPOT)) ||
         (NULL == CU_add_test(pSuite, "test of exp()", testEXP)) ||
-        (NULL == CU_add_test(pSuite, "test of exp2()", testEXP2)))
+        (NULL == CU_add_test(pSuite, "test of exp2()", testEXP2)) ||
+        (NULL == CU_add_test(pSuite, "test of expm1()", testEXPM1)) ||
+        (NULL == CU_add_test(pSuite, "test of frexp()", testFREXP)) ||
+        (NULL == CU_add_test(pSuite, "test of ldexp()", testLDEXP)) ||
+        (NULL == CU_add_test(pSuite, "test of log()", testLOG)) ||
+        (NULL == CU_add_test(pSuite, "test of log()", testLOG10)) ||
+        (NULL == CU_add_test(pSuite, "test of log1p()", testLOG1P)) ||
+        (NULL == CU_add_test(pSuite, "test of log2()", testLOG2)) ||
+        (NULL == CU_add_test(pSuite, "test of logb()", testLOGB)) ||
+        (NULL == CU_add_test(pSuite, "test of modf()", testMODF)) ||
+        (NULL == CU_add_test(pSuite, "test of modf()", testCBRT)) ||
+        (NULL == CU_add_test(pSuite, "test of fabs()", testFABS)) ||
+        (NULL == CU_add_test(pSuite, "test of erf()", testERF)) ||
+        (NULL == CU_add_test(pSuite, "test of erfc()", testERFC)) ||
+        (NULL == CU_add_test(pSuite, "test of lgamma()", testLGAMMA)) ||
+        (NULL == CU_add_test(pSuite, "test of tgamma()", testTGAMMA)) ||
+        (NULL == CU_add_test(pSuite, "test of ceil()", testCEIL)) ||
+        (NULL == CU_add_test(pSuite, "test of floor()", testFLOOR)) ||
+        (NULL == CU_add_test(pSuite, "test of nearbyint()", testNEARBYINT)) ||
+        (NULL == CU_add_test(pSuite, "test of rint()", testRINT)) ||
+        (NULL == CU_add_test(pSuite, "test of lrint()", testLRINT)) ||
+        (NULL == CU_add_test(pSuite, "test of llrint()", testLLRINT)) ||
+        (NULL == CU_add_test(pSuite, "test of round()", testROUND)) ||
+        (NULL == CU_add_test(pSuite, "test of lround()", testLROUND)) ||
+        (NULL == CU_add_test(pSuite, "test of llround()", testLLROUND)) ||
+        (NULL == CU_add_test(pSuite, "test of trunc()", testTRUNC)) ||
+        (NULL == CU_add_test(pSuite, "test of fmod()", testFMOD)) ||
+        (NULL == CU_add_test(pSuite, "test of remainder()", testREMAINDER)) ||
+        (NULL == CU_add_test(pSuite, "test of remquo()", testREMQUO)) ||
+        (NULL == CU_add_test(pSuite, "test of copysign()", testCOPYSIGN)) ||
+        (NULL == CU_add_test(pSuite, "test of nan()", testNAN)) ||
+        (NULL == CU_add_test(pSuite, "test of fdim()", testFDIM)) ||
+        (NULL == CU_add_test(pSuite, "test of fmax()", testFMAX)) ||
+        (NULL == CU_add_test(pSuite, "test of fmin()", testFMIN)) ||
+        (NULL == CU_add_test(pSuite, "test of fma()", testFMA)))
     {
         CU_cleanup_registry();
         return CU_get_error();
