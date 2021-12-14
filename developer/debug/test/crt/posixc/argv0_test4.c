abba963d014100d2308d4d6cd2ae3efbddcdecb9
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2021-12-14 09:41:39 +0100

    Improve test description/behavior

--- developer/debug/test/crt/posixc/argv0_test4.c
+++ developer/debug/test/crt/posixc/argv0_test4.c
@@ -2,14 +2,26 @@
     Copyright (C) 1995-2020, The AROS Development Team. All rights reserved.
 */
 
+#include <stdio.h>
 #include <unistd.h>
 
 #include "test.h"
 
 int main(void)
 {
+    /* Compiled with -nix */
+    /* Expected: will run slave thanks to translating path */
+
     const char *cmd = "../posix/argv0_slave", *arg1 = "../posix/argv0_slave";
     execl(cmd, cmd, arg1, NULL);
 
+    TEST(0);
+
     return FAIL;
+}
+
+void cleanup(void)
+{
+    /* NOP */
+    return;
 }
