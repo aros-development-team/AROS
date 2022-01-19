ac8945659a011963e81827e944efff0f4617e2fe
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2022-01-19 17:41:45 +0100

    Any object can be target of MUIM_HandleEvent
    This fixes https://github.com/deadw00d/AROS/issues/53

--- workbench/libs/muimaster/classes/window.c
+++ workbench/libs/muimaster/classes/window.c
@@ -1922,11 +1922,6 @@ static ULONG InvokeEventHandler(struct MUI_EventHandlerNode *ehn,
 {
     ULONG res;
 
-    if (!(_flags(ehn->ehn_Object) & MADF_CANDRAW))
-        return 0;
-    if (!(_flags(ehn->ehn_Object) & MADF_SHOWME))
-        return 0;
-
     if (event != NULL
         && event->Class == IDCMP_MOUSEBUTTONS
         && event->Code == SELECTDOWN
