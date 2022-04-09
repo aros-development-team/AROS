1c6aa47cadf701ebb88ac92a890f257aef482757
Kalamatee <kalamatee@gmail.com>
2022-04-09 23:56:30 +0100

    expose AllocateRAM on x86_64

--- arch/all-mingw32/bootstrap/memory.c
+++ arch/all-mingw32/bootstrap/memory.c
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 1995-2014, The AROS Development Team. All rights reserved.
+    Copyright (C) 1995-2022, The AROS Development Team. All rights reserved.
 */
 
 #include <io.h>
@@ -18,6 +18,10 @@
 
 HANDLE RAM_Handle = NULL;
 void *RAM_Address = NULL;
+#if defined(__x86_64__)
+HANDLE RAM64_Handle = NULL;
+void *RAM64_Address = NULL;
+#endif
 
 void *AllocateRO(size_t len)
 {
@@ -47,14 +51,14 @@ void *AllocateRAM32(size_t len)
 
     if (var)
     {
-        D(fprintf(stderr, "[AllocateRAM] Found RAM specification: %s\n", var));
+        D(fprintf(stderr, "[AllocateRAM32] Found RAM specification: %s\n", var));
         if (sscanf(var, "%p:%p", &RAM_Handle, &addr) != 2) {
-            D(fprintf(stderr, "[AllocateRAM] Error parsing specification\n"));
+            D(fprintf(stderr, "[AllocateRAM32] Error parsing specification\n"));
             RAM_Handle = NULL;
             addr = NULL;
         }
     }
-    D(fprintf(stderr, "[AllocateRAM] Inherited memory handle 0x%p address 0x%p\n", RAM_Handle, addr));
+    D(fprintf(stderr, "[AllocateRAM32] Inherited memory handle 0x%p address 0x%p\n", RAM_Handle, addr));
 
     if (!RAM_Handle)
     {
@@ -65,20 +69,20 @@ void *AllocateRAM32(size_t len)
     }
     if (!RAM_Handle)
     {
-        D(fprintf(stderr, "[AllocateRAM] PAGE_EXECUTE_READWRITE failed, retrying with PAGE_READWRITE\n"));
+        D(fprintf(stderr, "[AllocateRAM32] PAGE_EXECUTE_READWRITE failed, retrying with PAGE_READWRITE\n"));
         RAM_Handle = CreateFileMapping(INVALID_HANDLE_VALUE, &sa, PAGE_READWRITE, 0, len, NULL);
     }
-    D(fprintf(stderr, "[AllocateRAM] Shared memory handle 0x%p\n", RAM_Handle));
+    D(fprintf(stderr, "[AllocateRAM32] Shared memory handle 0x%p\n", RAM_Handle));
     if (!RAM_Handle)
         return NULL;
 
     RAM_Address = MapViewOfFileEx(RAM_Handle, FILE_MAP_ALL_ACCESS|FILE_MAP_EXECUTE, 0, 0, 0, addr);
     if (!RAM_Address)
     {
-        D(fprintf(stderr, "[AllocateRAM] FILE_MAP_EXECUTE failed, retrying without it\n"));
+        D(fprintf(stderr, "[AllocateRAM32] FILE_MAP_EXECUTE failed, retrying without it\n"));
         RAM_Address = MapViewOfFileEx(RAM_Handle, FILE_MAP_ALL_ACCESS, 0, 0, 0, addr);
     }
-    D(fprintf(stderr, "[AllocateRAM] Mapped at 0x%p\n", RAM_Address));
+    D(fprintf(stderr, "[AllocateRAM32] Mapped at 0x%p\n", RAM_Address));
 
     if (!RAM_Address)
     {
@@ -88,3 +92,55 @@ void *AllocateRAM32(size_t len)
 
     return RAM_Address;
 }
+
+#if defined(__x86_64__)
+void *AllocateRAM(size_t len)
+{
+    SECURITY_ATTRIBUTES sa;
+    void *addr = NULL;
+    const char *var = getenv(SHARED_RAM_VAR);
+
+    if (var)
+    {
+        D(fprintf(stderr, "[AllocateRAM] Found RAM specification: %s\n", var));
+        if (sscanf(var, "%p:%p", &RAM64_Handle, &addr) != 2) {
+            D(fprintf(stderr, "[AllocateRAM] Error parsing specification\n"));
+            RAM64_Handle = NULL;
+            addr = NULL;
+        }
+    }
+    D(fprintf(stderr, "[AllocateRAM] Inherited memory handle 0x%p address 0x%p\n", RAM64_Handle, addr));
+
+    if (!RAM64_Handle)
+    {
+        sa.nLength = sizeof(sa);
+        sa.lpSecurityDescriptor = NULL;
+        sa.bInheritHandle = TRUE;
+        RAM64_Handle = CreateFileMapping(INVALID_HANDLE_VALUE, &sa, PAGE_EXECUTE_READWRITE, 0, len, NULL);
+    }
+    if (!RAM64_Handle)
+    {
+        D(fprintf(stderr, "[AllocateRAM] PAGE_EXECUTE_READWRITE failed, retrying with PAGE_READWRITE\n"));
+        RAM64_Handle = CreateFileMapping(INVALID_HANDLE_VALUE, &sa, PAGE_READWRITE, 0, len, NULL);
+    }
+    D(fprintf(stderr, "[AllocateRAM] Shared memory handle 0x%p\n", RAM64_Handle));
+    if (!RAM64_Handle)
+        return NULL;
+
+    RAM64_Address = MapViewOfFileEx(RAM64_Handle, FILE_MAP_ALL_ACCESS|FILE_MAP_EXECUTE, 0, 0, 0, addr);
+    if (!RAM64_Address)
+    {
+        D(fprintf(stderr, "[AllocateRAM] FILE_MAP_EXECUTE failed, retrying without it\n"));
+        RAM64_Address = MapViewOfFileEx(RAM64_Handle, FILE_MAP_ALL_ACCESS, 0, 0, 0, addr);
+    }
+    D(fprintf(stderr, "[AllocateRAM] Mapped at 0x%p\n", RAM64_Address));
+
+    if (!RAM64_Address)
+    {
+        CloseHandle(RAM64_Handle);
+        RAM64_Handle = NULL;
+    }
+
+    return RAM64_Address;
+}
+#endif
