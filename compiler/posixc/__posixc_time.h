8ae4be5a06510d82cee2c6c19063b364e78a0e65
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2023-04-29 15:01:35 +0200

    Add UNIX-AmigaOS offset for clock_gettime(CLOCK_REALTIME)
    This makes sure time values returned by clock_gettime, gettimeofday
    and stat are synchronized.
    Example: GNU make is comparing stat values with clock_gettime values.

--- compiler/posixc/__posixc_time.h
+++ compiler/posixc/__posixc_time.h
@@ -3,3 +3,10 @@
 #define TimerBase (PosixCBase->timerBase)
 
 void __init_timerbase(struct PosixCIntBase *PosixCBase);
+
+/* 2922 is the number of days between 1.1.1970 and 1.1.1978 (2 leap
+   years and 6 normal). The former number is the start value
+   for time(), the latter the start time for the AmigaOS
+   time functions.
+*/
+#define OFFSET_FROM_1970 2922*24*60*60
