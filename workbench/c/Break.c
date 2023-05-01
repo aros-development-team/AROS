df80727277d82f5bc833b6b65498fae8d152a416
Matthias Rustler <mrustler@gmx.de>
2023-05-01 08:09:51 +0200

    [C/Break] Source code cleanup Indentation fixed.

--- workbench/c/Break.c
+++ workbench/c/Break.c
@@ -85,7 +85,7 @@ static const char exthelp[] =
         "Break: Send break signal(s) to a CLI process\n"
         "\tPROCESS/N    signal receiver's CLI process number\n"
         "\tPORT         Portname for the Process to set flags for\n"
-                "\tNAME/K       Name of the process (as shown by tasklist)\n"
+        "\tNAME/K       Name of the process (as shown by tasklist)\n"
         "\tC/S          send CTRL-C signal\n"
         "\tD/S          send CTRL-D signal\n"
         "\tE/S          send CTRL-E signal\n"
@@ -98,112 +98,112 @@ int __nocommandline = 1;
 int
 main(void)
 {
-        struct RDArgs *rd, *rda = NULL;
+    struct RDArgs *rd, *rda = NULL;
 
-        IPTR args[TOTAL_ARGS] = { };
+    IPTR args[TOTAL_ARGS] = { };
 
-        int error = 0;
+    int error = 0;
 
-        if ((rda = AllocDosObject(DOS_RDARGS, NULL)))
+    if ((rda = AllocDosObject(DOS_RDARGS, NULL)))
+    {
+        rda->RDA_ExtHelp = (STRPTR) exthelp;
+
+        if ((rd = ReadArgs(ARG_TEMPLATE, (IPTR *) args, rda)))
         {
-                rda->RDA_ExtHelp = (STRPTR) exthelp;
-
-                if ((rd = ReadArgs(ARG_TEMPLATE, (IPTR *) args, rda)))
+            struct Process *pr = NULL;
+            Forbid();
+            if (args[ARG_PROCESS])
+                    pr = FindCliProc(*(IPTR *) args[ARG_PROCESS]);
+            else if (args[ARG_PORT])
+            {
+                struct MsgPort *MyPort;
+                if ((MyPort = (struct MsgPort*) FindPort((STRPTR) args[ARG_PORT])) != NULL)
                 {
-                        struct Process *pr = NULL;
-                        Forbid();
-                        if (args[ARG_PROCESS])
-                                pr = FindCliProc(*(IPTR *) args[ARG_PROCESS]);
-                        else if (args[ARG_PORT])
-                        {
-                                struct MsgPort *MyPort;
-                                if ((MyPort = (struct MsgPort*) FindPort((STRPTR) args[ARG_PORT])) != NULL)
-                                {
-                                        pr = (struct Process*) MyPort->mp_SigTask;
-                                }
-                        }
-                        else if (args[ARG_NAME])
-                        {
-                                pr = (struct Process *) FindTask((UBYTE *)args[ARG_NAME]);
-                        }
-                        if (pr != NULL)
-                        {
-                                ULONG
-                                mask = 0;
+                    pr = (struct Process*) MyPort->mp_SigTask;
+                }
+            }
+            else if (args[ARG_NAME])
+            {
+                pr = (struct Process *) FindTask((UBYTE *)args[ARG_NAME]);
+            }
+            if (pr != NULL)
+            {
+                ULONG
+                mask = 0;
 
-                                /* Figure out the mask of flags to send. */
-                                if (args[ARG_ALL])
-                                {
-                                        mask = SIGBREAKF_CTRL_C | SIGBREAKF_CTRL_D
-                                               | SIGBREAKF_CTRL_E | SIGBREAKF_CTRL_F;
-                                }
-                                else
-                                {
-                                        mask = (args[ARG_C] != 0 ? SIGBREAKF_CTRL_C : 0)
-                                               | (args[ARG_D] != 0 ? SIGBREAKF_CTRL_D : 0)
-                                               | (args[ARG_E] != 0 ? SIGBREAKF_CTRL_E : 0)
-                                               | (args[ARG_F]!= 0 ? SIGBREAKF_CTRL_F : 0);
-
-                                        if (0 == mask)
-                                        {
-                                                mask = SIGBREAKF_CTRL_C; /* default */
-                                        }
-                                }
-
-                                Signal((struct Task *) pr, mask);
-                                Permit();
-                        }
-                        else
-                        {
-                                /* There is no relevant error code, OBJECT_NOT_FOUND
-                                 * is a filesystem error, so we can't use that... */
-                                Permit();
-                                pr = (struct Process *) FindTask(NULL);
-
-                                BPTR errStream = (pr->pr_CES != BNULL)
-                                                 ? pr->pr_CES
-                                                 : Output();
-
-                                if (args[ARG_PROCESS])
-                                {
-                                        VFPrintf(errStream, "Break: Process %ld does not exist.\n", (APTR) args[ARG_PROCESS]);
-                                }
-                                else if (args[ARG_PORT])
-                                {
-                                        FPrintf(errStream, "Break: Port \"%s\" does not exist.\n", (LONG) args[ARG_PORT]);
-                                }
-                                else if (args[ARG_NAME])
-                                {
-                                        FPrintf(errStream, "Break: Task \"%s\" does not exist.\n", (LONG) args[ARG_NAME]);
-                                }
-                                else
-                                {
-                                        FPuts(errStream, "Break: Either PROCESS, PORT or NAME is required.\n");
-                                }
-                                error = -1;
-                        }
-
-                        FreeArgs(rd);
-                } /* ReadArgs() ok */
+                /* Figure out the mask of flags to send. */
+                if (args[ARG_ALL])
+                {
+                    mask = SIGBREAKF_CTRL_C | SIGBREAKF_CTRL_D
+                               | SIGBREAKF_CTRL_E | SIGBREAKF_CTRL_F;
+                }
                 else
                 {
-                        error = IoErr();
+                    mask = (args[ARG_C] != 0 ? SIGBREAKF_CTRL_C : 0)
+                           | (args[ARG_D] != 0 ? SIGBREAKF_CTRL_D : 0)
+                           | (args[ARG_E] != 0 ? SIGBREAKF_CTRL_E : 0)
+                           | (args[ARG_F]!= 0 ? SIGBREAKF_CTRL_F : 0);
+
+                    if (0 == mask)
+                    {
+                        mask = SIGBREAKF_CTRL_C; /* default */
+                    }
                 }
 
-                FreeDosObject(DOS_RDARGS, rda);
-        } /* Got rda */
+                Signal((struct Task *) pr, mask);
+                Permit();
+            }
+            else
+            {
+                /* There is no relevant error code, OBJECT_NOT_FOUND
+                 * is a filesystem error, so we can't use that... */
+                Permit();
+                pr = (struct Process *) FindTask(NULL);
+
+                BPTR errStream = (pr->pr_CES != BNULL)
+                                 ? pr->pr_CES
+                                 : Output();
+
+                if (args[ARG_PROCESS])
+                {
+                    VFPrintf(errStream, "Break: Process %ld does not exist.\n", (APTR) args[ARG_PROCESS]);
+                }
+                else if (args[ARG_PORT])
+                {
+                    FPrintf(errStream, "Break: Port \"%s\" does not exist.\n", (LONG) args[ARG_PORT]);
+                }
+                else if (args[ARG_NAME])
+                {
+                    FPrintf(errStream, "Break: Task \"%s\" does not exist.\n", (LONG) args[ARG_NAME]);
+                }
+                else
+                {
+                    FPuts(errStream, "Break: Either PROCESS, PORT or NAME is required.\n");
+                }
+                error = -1;
+            }
+
+            FreeArgs(rd);
+        } /* ReadArgs() ok */
         else
         {
-                error = IoErr();
+            error = IoErr();
         }
 
-        if (error != 0 && error != -1)
-        {
-                PrintFault(error, "Break");
-                return RETURN_FAIL;
-        }
+        FreeDosObject(DOS_RDARGS, rda);
+    } /* Got rda */
+    else
+    {
+        error = IoErr();
+    }
 
-        SetIoErr(0);
+    if (error != 0 && error != -1)
+    {
+        PrintFault(error, "Break");
+        return RETURN_FAIL;
+    }
 
-        return 0;
+    SetIoErr(0);
+
+    return 0;
 }
