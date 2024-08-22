936138e25b4e7a5c97bb0999cfd61ac3a59edb39
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2024-08-22 19:24:40 +0200

    Fix creation of drag image for icons
    DrawIconStateA draws ARGB data onto created temporary bitmap. This means
    it already combines icon color with bitmap color. This is not a problem
    for places where A is 0xff, but in other places (edges of icon graphics)
    we get values that "look good on background same as temporary bitmap"
    
    What we really want is a bitmap that is RAW icons ARGB image so that
    this bitmap gets alpha-blitted on background that we move over and is
    correctly integrated.
    
    This resolves graphics artifacts when dragging icons. Most visible on
    Prefs/Pointer icon.

--- workbench/system/Wanderer/Classes/iconlist.c
+++ workbench/system/Wanderer/Classes/iconlist.c
@@ -29,6 +29,7 @@
 #include <dos/datetime.h>
 #include <dos/filehandler.h>
 
+#include <aros/system.h>
 #include <exec/memory.h>
 #include <exec/rawfmt.h>
 #include <graphics/gfx.h>
@@ -167,10 +168,7 @@ for                                                        \
     node = (void *)(((struct Node *)(node))->ln_Pred)      \
 )
 
-#define RPALPHAFLAT     (1 << 0)
-#define RPALPHARADIAL   (1 << 1)
-
-static void RastPortSetAlpha(struct RastPort *arport, ULONG ax, ULONG ay, ULONG width, ULONG height, UBYTE val, UBYTE alphamode)
+static void RastPortSetOpaque(struct RastPort *arport, ULONG ax, ULONG ay, ULONG width, ULONG height, ULONG transpcolval)
 {
     ULONG       x, y;
     ULONG       alphaval, pixelval;
@@ -179,31 +177,48 @@ static void RastPortSetAlpha(struct RastPort *arport, ULONG ax, ULONG ay, ULONG 
     if ((buffer = AllocVec(width * height * sizeof(ULONG), MEMF_ANY)) == NULL)
         return;
 
-    ReadPixelArray(buffer, 0, 0, width * sizeof(ULONG), arport, 0, 0, width, height, RECTFMT_ARGB);
+    ReadPixelArray(buffer, 0, 0, width * sizeof(ULONG), arport, ax, ay, width, height, RECTFMT_ARGB);
 
     pixelptr = buffer;
+
+#if AROS_BIG_ENDIAN
+#else
+    // ARGB -> BGRA
+    transpcolval = ((transpcolval & 0x00FF0000) >> 8) | ((transpcolval & 0x0000FF00) << 8) | ((transpcolval & 0x000000FF) << 24);
+#endif
 
     for (y = 0; y < height; y++)
     {
         for (x = 0; x < width; x++)
         {
-            if((pixelval = *((ULONG *)pixelptr)))
-            {
-                if (alphamode == RPALPHARADIAL){
-                    //Set the alpha value based on distance from ax,ay
-                } else {
-                    alphaval = val;
-                }
-                pixelval = (pixelval & 0xffffff00) | alphaval;
-                *((ULONG *)pixelptr) = pixelval;
-            }
+
+            pixelval = *((ULONG *)pixelptr);
+
+#if AROS_BIG_ENDIAN
+            if ((pixelval & 0x00FFFFFF) == (transpcolval & 0x00FFFFFF))
+#else
+            if ((pixelval & 0xFFFFFF00) == (transpcolval & 0xFFFFFF00))
+#endif
+                alphaval = 0x0;
+            else
+                alphaval = 0xFF;
+
+#if AROS_BIG_ENDIAN
+            pixelval = (pixelval & 0x00FFFFFF) | (alphaval << 24);
+#else
+            pixelval = (pixelval & 0xFFFFFF00) | alphaval;
+#endif
+            *((ULONG *)pixelptr) = pixelval;
+
             pixelptr += sizeof(ULONG);
         }
     }
 
-    WritePixelArray(buffer, 0, 0, width * sizeof(ULONG), arport, 0, 0, width, height, RECTFMT_ARGB);
+    WritePixelArray(buffer, 0, 0, width * sizeof(ULONG), arport, ax, ay, width, height, RECTFMT_ARGB);
+
     FreeVec(buffer);
 }
+
 
 ///RectAndRect()
 // Entry/Label Area support functions
@@ -6482,6 +6497,44 @@ IPTR IconList__MUIM_IconList_GetIconPrivate(struct IClass *CLASS, Object *obj, s
     return (IPTR)node;
 }
 
+static void DrawIconOnDragImage(struct RastPort *rp, struct DiskObject *icon, LONG ax, LONG ay, BOOL transp)
+{
+    APTR image2;
+    ULONG height = 0;
+    ULONG width;
+    struct TagItem tags [] = {
+        { ICONCTRLA_GetARGBImageData2, (IPTR)&image2    },
+        { ICONCTRLA_GetHeight, (IPTR)&height            },
+        { TAG_DONE,                                     }
+    };
+
+
+    IconControlA(icon, tags);
+
+    if (height == 0) height = icon->do_Gadget.Height; /* Workaround, some glow icons report half of height only */
+    width = icon->do_Gadget.Width;
+
+    if (transp && image2)
+    {
+        /* Target has alpha, preserve source alpha values unchanged. */
+        WritePixelArray(image2, 0, 0, width * sizeof(ULONG), rp, ax, ay, width, height, RECTFMT_ARGB);
+    }
+    else if (transp && !image2)
+    {
+        /* Target has alpha, but source does not. Use FEDCBA color to mask out where resulting alpha should be 0 */
+        FillPixelArray(rp, ax, ay, width, height, 0xFFFEDCBA);
+
+        DrawIconStateA(rp, icon, NULL, ax, ay, IDS_SELECTED, __iconList_DrawIconStateTags);
+
+        RastPortSetOpaque(rp, ax, ay, width, height, 0xFFFEDCBA);
+    }
+    else
+    {
+        /* Target is opaque. */
+        DrawIconStateA(rp, icon, NULL, ax, ay, IDS_SELECTED, __iconList_DrawIconStateTags);
+    }
+}
+
 ///MUIM_CreateDragImage()
 /**************************************************************************
 MUIM_CreateDragImage
@@ -6585,6 +6638,12 @@ IPTR IconList__MUIM_CreateDragImage(struct IClass *CLASS, Object *obj, struct MU
             temprp.BitMap = img->bm;
             ULONG minY = 0;
 
+            if (!transp)
+            {
+                SetABPenDrMd(&temprp, _pens(obj)[MPEN_BACKGROUND], 0, JAM1);
+                RectFill(&temprp, 0, 0, img->width, img->height);
+            }
+
 #if defined(CREATE_FULL_DRAGIMAGE)
             ForeachNode(&data->icld_SelectionList, node)
             {
@@ -6616,13 +6675,8 @@ IPTR IconList__MUIM_CreateDragImage(struct IClass *CLASS, Object *obj, struct MU
 
                         IconList_GetIconImageOffsets(data, entry, &offsetx, &offsety);
 
-                        DrawIconStateA
-                            (
-                                &temprp, entry->ie_DiskObj, NULL,
-                                (entry->ie_IconX + 1) - first_x + offsetx, (entry->ie_IconY + 1) - first_y + offsety,
-                                IDS_SELECTED,
-                                __iconList_DrawIconStateTags
-                            );
+                        DrawIconOnDragImage(&temprp, entry->ie_DiskObj, (entry->ie_IconX + 1) - first_x + offsetx,
+                            (entry->ie_IconY + 1) - first_y + offsety, transp);
                     }
                 }
             }
@@ -6634,17 +6688,10 @@ IPTR IconList__MUIM_CreateDragImage(struct IClass *CLASS, Object *obj, struct MU
             }
             else
             {
-                DrawIconStateA
-                    (
-                        &temprp, entry->ie_DiskObj, NULL,
-                        0, 0,
-                        IDS_SELECTED,
-                        __iconList_DrawIconStateTags
-                    );
+                DrawIconOnDragImage(&temprp, entry->ie_DiskObj, 0, 0, transp);
             }
 #endif
-            if (transp)
-                RastPortSetAlpha(&temprp, data->click_x, data->click_y, img->width, img->height, 0xC0, RPALPHAFLAT);
+
             DeinitRastPort(&temprp);
         }
 
