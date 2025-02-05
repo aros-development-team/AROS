1c973683489811fa0db6d9ad24e8a5c8f7774269
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2025-02-05 18:26:50 +0100

    Add debug code for checking of XMM registers are preserved.
    Code uses pseudo-stack of 8 frames to save XMM registers and support
    nested exceptions.

--- arch/x86_64-pc/kernel/kernel_cpu.c
+++ arch/x86_64-pc/kernel/kernel_cpu.c
@@ -55,6 +55,30 @@
 #define DEBUGCOLOR_RESET
 #endif
 
+#define DEBUG_XMM 0 /* Keep the same with all-pc/kernel/cpu_intr.c !! */
+#if DEBUG_XMM
+extern UBYTE *pseudorsp;
+#define SAVE_XMM_INTO_AREA(area)                \
+    asm volatile (                              \
+        "       movaps %%xmm0, (%0)\n"          \
+        "       movaps %%xmm1, 16(%0)\n"        \
+        "       movaps %%xmm2, 32(%0)\n"        \
+        "       movaps %%xmm3, 48(%0)\n"        \
+        "       movaps %%xmm4, 64(%0)\n"        \
+        "       movaps %%xmm5, 80(%0)\n"        \
+        "       movaps %%xmm6, 96(%0)\n"        \
+        "       movaps %%xmm7, 112(%0)\n"       \
+        ::"r"(area));
+
+#define SAVE_XMM_AND_CHECK                      \
+UQUAD xmmpost[16];                              \
+SAVE_XMM_INTO_AREA(xmmpost)                     \
+UQUAD *xmmpre = (UQUAD *)localarea;             \
+for (int i = 0; i < 15; i++)                    \
+    if (xmmpre[i] != xmmpost[i]) bug("diff in core_Switch (%d) %lx vs %lx!!\n", i, xmmpre[i], xmmpost[i]);
+
+#endif
+
 void cpu_Dispatch(struct ExceptionContext *regs)
 {
     struct Task *task;
@@ -170,6 +194,9 @@ void cpu_Dispatch(struct ExceptionContext *regs)
     DSCHED(
         bug("[Kernel:%03u]" DEBUGCOLOR_SET " %s: Leaving..." DEBUGCOLOR_RESET "\n", cpunum, __func__);
     )
+#if DEBUG_XMM
+pseudorsp -= 16 * 8;
+#endif
     /*
      * Leave interrupt and jump to the new task.
      * We will restore CPU state right from this buffer,
@@ -213,6 +240,11 @@ void cpu_Switch(struct ExceptionContext *regs)
                 "       movaps 32(%0), %%xmm2\n"
                 "       movaps 48(%0), %%xmm3\n"
                 ::"r"(regs->FXSData));
+
+#if DEBUG_XMM
+APTR localarea = pseudorsp - (16 * 8);
+SAVE_XMM_AND_CHECK
+#endif
             /*
             * Cache x86 FPU / XMM / AVX512 / MXCSR state first
             * NB: See the note about lazy saving of the fpu above!!
