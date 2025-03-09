94f84b6433571d7758ce049ebe8fc04e956d31da
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2025-03-09 11:58:28 +0100

    Disable wctrans_t and wctype_t until implementation is provided
    These two types represent "enums" with some fixed values as well as
    some dynamic values derived from strings. They are independent from wchar_t
    and are specific to implementation of wctype() and wctrans() functions.
    (See C11 spec 7.30.2.2.2 and 7.30.3.2.2)
    
    Here we define them as int matching their definitions from Haiku, however
    definitions are commented out, so that no 3rd party can depend on them
    until implementation of wctype() and wctrans() are provided.

--- compiler/crt/stdc/include/aros/types/wctype_t.h
+++ compiler/crt/stdc/include/aros/types/wctype_t.h
@@ -2,19 +2,13 @@
 #define _AROS_TYPES_WCTYPE_T_H
 
 /*
-    Copyright © 2020, The AROS Development Team. All rights reserved.
+    Copyright © 2020-2025, The AROS Development Team. All rights reserved.
     $Id$
 */
 
-#include <aros/types/wchar_t.h>
-
 #ifndef __cplusplus
 
-#ifdef __WCTYPE_TYPE__
-typedef __WCTYPE_TYPE__ wchar_t;
-#else
-typedef wchar_t wctype_t;
-#endif
+/* typedef int wctype_t; */
 
 #endif
 
