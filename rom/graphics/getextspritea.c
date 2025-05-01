ac14f6f2fcfa4449f06df211b45919298b250a5c
Johan Gill <johan.gill@gmail.com>
2025-05-01 18:55:59 +0200

    Implement GetExtSpriteA() similarly to GetSprite().

--- rom/graphics/getextspritea.c
+++ rom/graphics/getextspritea.c
@@ -41,10 +41,8 @@
     SEE ALSO
 
     INTERNALS
-        AROS does not have complete sprite system, instead it has a (hacky)
-        minimal implementation enough to drive mouse pointer as a single
-        sprite #0. This assumes that this sprite is always allocated by the OS
-        itself and can't be used by user applications. So we just return error.
+        In AROS, sprite zero is reserved for the mouse cursor and is
+        never reserved by this function.
 
     HISTORY
 
@@ -53,7 +51,65 @@
 {
     AROS_LIBFUNC_INIT
 
-    return -1;
+    WORD pick = -1;
+    UBYTE SearchMask;
 
+    for (struct TagItem *tag = tags; tag->ti_Tag != TAG_END; ++tag) {
+        if (tag->ti_Tag == GSTAG_SPRITE_NUM) {
+            pick = tag->ti_Data;
+        }
+    }
+    if (pick > 7) {
+        pick = -1;
+    } else {
+        /*  let nobody else interrupt us while we're looking for a free
+         *  sprite
+         */
+        Disable();
+
+        if (-1 == pick) {
+            LONG Count = 1;
+            /* user just wants the next available sprite.
+             * Since AROS does not set up the Intuition pointer through the
+             * sprite API, we skip sprite 0 when searching for a sprite slot. */
+            SearchMask = 0x02;
+
+            /* look for the first not allocated sprite */
+            while (0 != (GfxBase->SpriteReserved & SearchMask)  &&  Count < 8) {
+                SearchMask <<= 1;
+                Count++;
+            }
+
+            if (8 != Count) {
+                /* we were able to allocated a free sprite */
+                /* mark the sprite as reserved for the user */
+                GfxBase->SpriteReserved |= SearchMask;
+                GfxBase->ExtSprites |= SearchMask;
+                pick = Count;
+            } else {
+                /* no sprite was available for the user */
+                pick = -1;
+            }
+
+        } else {
+            /* user wants one specific sprite */
+            SearchMask = 0x01 << pick;
+
+            /* is that sprite still available? */
+            if (0 == (GfxBase->SpriteReserved & SearchMask) ) {
+                /* yes -> mark it as reserved for the user */
+                GfxBase->SpriteReserved |= SearchMask;
+                GfxBase->ExtSprites |= SearchMask;
+            } else {
+                /* no, it's not available any more */
+                pick = -1;
+            }
+        }
+        Enable();
+    }
+
+    ((struct SimpleSprite *)sprite)->num = pick;
+
+    return pick;
     AROS_LIBFUNC_EXIT
 } /* GetExtSpriteA */
