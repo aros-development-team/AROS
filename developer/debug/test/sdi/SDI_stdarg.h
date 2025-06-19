0a110107357c57d4a4a3f001ccc4807546974b1c
Kalamatee <kalamatee@gmail.com>
2025-06-19 02:23:24 +0100

    correct va_start usage to satisfy gcc

--- developer/debug/test/sdi/SDI_stdarg.h
+++ developer/debug/test/sdi/SDI_stdarg.h
@@ -111,13 +111,14 @@
   #define VA_END(va)          va_end((va))
 #else
   #define VA_LIST             va_list
-  #define VA_START(va, start) va_start((va), (start))
-  #define VA_ARG(va, type)    (type)(va)
-  #if defined(__AROS__)
+  #if defined(__GNUC__)
+    #define VA_START(va, start) va_start((va), start)
     #define VA_COPY(d, s)     va_copy(d, s)
   #else
+    #define VA_START(va, start) va_start((va), (start))
     #define VA_COPY(d, s)     (d) = (s)
   #endif
+  #define VA_ARG(va, type)    (type)(va)
   #define VA_END(va)          va_end((va))
 #endif
 
