b4397fd5e4abef80c561d662e0bfe5c9b00870c7
Kalamatee <kalamatee@gmail.com>
2025-06-26 23:50:18 +0100

    add a couple more string helper macros

--- compiler/arossupport/include/strmacro.h
+++ compiler/arossupport/include/strmacro.h
@@ -5,11 +5,17 @@
     Copyright © 2025, The AROS Development Team. All rights reserved.
     $Id$
 
-    Desc: STR helper macro to stringify strings.
+    Desc: STR helper macro(s) to stringify, and concatenate, strings.
     Lang: english
 */
 
 #define ___AROS_STR(x) #x
 #define __AROS_STR(x) ___AROS_STR(x)
+
+#define __AROS_CAT(a, b) a##b
+#define __AROS_CAT_EXPAND(a, b) __AROS_CAT(a, b)
+
+#define __AROS_CAT3(a, b, c) a##b##c
+#define __AROS_CAT3_EXPAND(a, b, c) __AROS_CAT3(a, b, c)
 
 #endif /* AROS_STRMACRO_H */
