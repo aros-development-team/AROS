8fbd336b0bd200ded71621f9ae49827b901f63b2
Kalamatee <kalamatee@gmail.com>
2025-07-13 05:53:32 +0100

    let collect-aros understand -Ttext, and set the correct values in the script based on that. let collect-aros also understand --ld-path, and use the linker specified if provided.

--- tools/collect-aros/collect-aros.c
+++ tools/collect-aros/collect-aros.c
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 1995-2023, The AROS Development Team. All rights reserved.
+    Copyright (C) 1995-2025, The AROS Development Team. All rights reserved.
 */
 
 #include <stdio.h>
@@ -85,6 +85,7 @@ int main(int argc, char *argv[])
     int incremental = 0, ignore_undefined_symbols = 0;
     int strip_all   = 0;
     int cnt, i, libcnt = 0, relcnt = 0;
+    char *linkAddress = NULL;
 
     setnode *setlist = NULL, *liblist = NULL, *extralist = NULL;
 
@@ -148,6 +149,27 @@ int main(int argc, char *argv[])
                 argv[cnt][1] = 'r'; /* Just some non-harming option... */
             }
             else
+            /* Start address specified .. */
+            if ((strncmp(&argv[cnt][1], "Ttext", 6) == 0) || ((strncmp(&argv[cnt][1], "Wl,-Ttext", 10) == 0)))
+            {
+                if (argv[cnt][1] =='T')
+                    linkAddress = &argv[cnt][7];
+                else
+                    linkAddress = &argv[cnt][11];
+            }
+            else
+            /* Use the specified linker.. */
+            if ((strncmp(&argv[cnt][1], "-ld-path", 8) == 0) || ((strncmp(&argv[cnt][1], "Wl,--ld-path", 12) == 0)))
+            {
+                if (argv[cnt][1] =='-')
+                    ld_name = &argv[cnt][10];
+                else
+                    ld_name = &argv[cnt][14];
+                argv[cnt][1] = 'r';  /* Just some non-harming option... */
+                argv[cnt][2] = '\0';
+                break;
+            }
+            else
             /* The user just requested help info, don't do anything else */
             if (strncmp(&argv[cnt][1], "-help", 6) == 0)
             {
@@ -165,7 +187,7 @@ int main(int argc, char *argv[])
             }
         }
     }
-  
+
     ldargs = xmalloc(sizeof(char *) * (argc + EXTRA_ARG_CNT
         + ((incremental == 1) ? 0 : 2) + 1));
 
@@ -207,8 +229,7 @@ int main(int argc, char *argv[])
                 }
             }
     }
-  
-    
+      
     ldargs[cnt] = NULL;
               
     docommandvp(ld_name, ldargs);
@@ -225,6 +246,9 @@ int main(int argc, char *argv[])
     collect_extra(tempoutput, &extralist);
     collect_libs(tempoutput, &liblist);
 
+    if (linkAddress != NULL)
+        fprintf(ldscriptfile, "ENTRY(_start)\n");
+
     if (setlist) {
         struct setnode *n;
         for (n = setlist; n; n = n->next) {
@@ -235,14 +259,137 @@ int main(int argc, char *argv[])
     }
 
     fwrite(LDSCRIPT_PART1, sizeof(LDSCRIPT_PART1) - 1, 1, ldscriptfile);
+    if (linkAddress != NULL)
+        fprintf(ldscriptfile, "  . = %s;\n", linkAddress);
+#ifdef TARGET_FORMAT_EXE
+    fprintf(ldscriptfile, "  .tag  :\n");
+#else
+    if (linkAddress != NULL)
+        fprintf(ldscriptfile, "  .tag   :\n");
+    else
+        fprintf(ldscriptfile, "  .tag 0 :\n");
+#endif
+    fwrite(LDSCRIPT_PART2A, sizeof(LDSCRIPT_PART2A) - 1, 1, ldscriptfile);
+    if (linkAddress != NULL)
+        fprintf(ldscriptfile, "\n");
+    else
+        fprintf(ldscriptfile, "=0x90909090\n");
+#ifdef TARGET_FORMAT_EXE
+    fprintf(ldscriptfile, "  . = SEGMENT_START(\"text-segment\", 0x80000000);\n");
+    fprintf(ldscriptfile, "  .text  :\n");
+#else
+    if (linkAddress != NULL)
+        fprintf(ldscriptfile, "  .text   :\n");
+    else
+        fprintf(ldscriptfile, "  .text 0 :\n");
+#endif
+    fwrite(LDSCRIPT_PART2B, sizeof(LDSCRIPT_PART2B) - 1, 1, ldscriptfile);
+    if (linkAddress != NULL)
+        fprintf(ldscriptfile, "    KEEP(*(.text._start))\n");
+    fwrite(LDSCRIPT_PART3A, sizeof(LDSCRIPT_PART3A) - 1, 1, ldscriptfile);
+    if (linkAddress != NULL)
+        fprintf(ldscriptfile, "\n\n");
+    else
+        fprintf(ldscriptfile, "=0x90909090\n\n");
+#ifdef TARGET_FORMAT_EXE
+    fprintf(ldscriptfile, "  . = SEGMENT_START(\"rodata-segment\", . );\n");
+    fprintf(ldscriptfile, "  .rodata   :\n");
+#else
+    if (linkAddress != NULL)
+        fprintf(ldscriptfile, "  .rodata    :\n");
+    else
+        fprintf(ldscriptfile, "  .rodata  0 :\n");
+#endif
+    fwrite(LDSCRIPT_PART3B, sizeof(LDSCRIPT_PART3B) - 1, 1, ldscriptfile);
     emit_sets(setlist, ldscriptfile);
     emit_libs(liblist, ldscriptfile);
-    fwrite(LDSCRIPT_PART2, sizeof(LDSCRIPT_PART2) - 1, 1, ldscriptfile);
+    fprintf(ldscriptfile, "  }\n");
+#ifdef TARGET_FORMAT_EXE
+    fprintf(ldscriptfile, "  .rodata1  : { *(.rodata1) }\n");
+#else
+    if (linkAddress != NULL)
+        fprintf(ldscriptfile, "  .rodata1   : { *(.rodata1) }\n");
+    else
+        fprintf(ldscriptfile, "  .rodata1 0 : { *(.rodata1) }\n");
+#endif
+    fwrite(LDSCRIPT_PART4A, sizeof(LDSCRIPT_PART4A) - 1, 1, ldscriptfile);
+#ifdef TARGET_FORMAT_EXE
+    fprintf(ldscriptfile, "  .sdata2   : { *(.sdata2) *(.sdata2.*) *(.gnu.linkonce.s2.*) }\n");
+    fprintf(ldscriptfile, "  .sbss2    : { *(.sbss2)  *(.sbss2.*)  *(.gnu.linkonce.sb2.*) }\n\n");
+    fprintf(ldscriptfile, "  .data   :\n");
+#else
+    if (linkAddress != NULL)
+    {
+        fprintf(ldscriptfile, "  .sdata2    : { *(.sdata2) *(.sdata2.*) *(.gnu.linkonce.s2.*) }\n");
+        fprintf(ldscriptfile, "  .sbss2     : { *(.sbss2)  *(.sbss2.*)  *(.gnu.linkonce.sb2.*) }\n\n");
+        fprintf(ldscriptfile, "  .data      :\n");
+    }
+    else
+    {
+        fprintf(ldscriptfile, "  .sdata2  0 : { *(.sdata2) *(.sdata2.*) *(.gnu.linkonce.s2.*) }\n");
+        fprintf(ldscriptfile, "  .sbss2   0 : { *(.sbss2)  *(.sbss2.*)  *(.gnu.linkonce.sb2.*) }\n\n");
+        fprintf(ldscriptfile, "  .data    0 :\n");
+    }
+#endif
+    fwrite(LDSCRIPT_PART4B, sizeof(LDSCRIPT_PART4B) - 1, 1, ldscriptfile);
+#ifdef TARGET_FORMAT_EXE
+#else
+    if (linkAddress != NULL)
+        fprintf(ldscriptfile, "  .data1              : { *(.data1) }\n");
+    else
+        fprintf(ldscriptfile, "  .data1            0 : { *(.data1) }\n");
+#endif
+    fwrite(LDSCRIPT_PART4C, sizeof(LDSCRIPT_PART4C) - 1, 1, ldscriptfile);
+#ifdef TARGET_FORMAT_EXE
+    fprintf(ldscriptfile, "  .eh_frame          :\n");
+#else
+    if (linkAddress != NULL)
+        fprintf(ldscriptfile, "  .eh_frame           :\n");
+    else
+        fprintf(ldscriptfile, "  .eh_frame         0 :\n");
+#endif
+    fwrite(LDSCRIPT_PART4D, sizeof(LDSCRIPT_PART4D) - 1, 1, ldscriptfile);
     /* Append .eh_frame terminator only on final stage */
     if (incremental == 0)
         fputs("LONG(0)\n", ldscriptfile);
-    fwrite(LDSCRIPT_PART3, sizeof(LDSCRIPT_PART3) - 1, 1, ldscriptfile);
-    fwrite(LDSCRIPT_PART4, sizeof(LDSCRIPT_PART4) - 1, 1, ldscriptfile);
+    fwrite(LDSCRIPT_PART5A, sizeof(LDSCRIPT_PART5A) - 1, 1, ldscriptfile);
+#ifdef TARGET_FORMAT_EXE
+    fprintf(ldscriptfile, "  .gcc_except_table  : { *(.gcc_except_table) }\n");
+#else
+    if (linkAddress != NULL)
+        fprintf(ldscriptfile, "  .gcc_except_table   : { *(.gcc_except_table) }\n");
+    else
+        fprintf(ldscriptfile, "  .gcc_except_table 0 : { *(.gcc_except_table) }\n");
+#endif
+    fwrite(LDSCRIPT_PART5B, sizeof(LDSCRIPT_PART5B) - 1, 1, ldscriptfile);
+#ifdef TARGET_FORMAT_EXE
+    fprintf(ldscriptfile, "  .sdata    :\n");
+#else
+    if (linkAddress != NULL)
+        fprintf(ldscriptfile, "  .sdata     :\n");
+    else
+        fprintf(ldscriptfile, "  .sdata   0 :\n");
+#endif
+    fwrite(LDSCRIPT_PART5C, sizeof(LDSCRIPT_PART5C) - 1, 1, ldscriptfile);
+#ifdef TARGET_FORMAT_EXE
+    fprintf(ldscriptfile, "  .sbss  :\n");
+#else
+    if (linkAddress != NULL)
+        fprintf(ldscriptfile, "  .sbss   :\n");
+    else
+        fprintf(ldscriptfile, "  .sbss 0 :\n");
+#endif
+    fwrite(LDSCRIPT_PART5D, sizeof(LDSCRIPT_PART5D) - 1, 1, ldscriptfile);
+#ifdef TARGET_FORMAT_EXE
+    fprintf(ldscriptfile, "  .bss  :\n");
+#else
+    if (linkAddress != NULL)
+        fprintf(ldscriptfile, "  .bss   :\n");
+    else
+        fprintf(ldscriptfile, "  .bss 0 :\n");
+#endif
+    fwrite(LDSCRIPT_PART5E, sizeof(LDSCRIPT_PART5E) - 1, 1, ldscriptfile);
+    fwrite(LDSCRIPT_PART6, sizeof(LDSCRIPT_PART6) - 1, 1, ldscriptfile);
 
     fclose(ldscriptfile);
     ldscriptfile = NULL;
--- tools/collect-aros/ldscript.h
+++ tools/collect-aros/ldscript.h
@@ -11,51 +11,34 @@ const static char LDSCRIPT_PART1[] =
 "*/\n"
 "\n" \
 "SECTIONS\n"
-"{\n"
-#ifdef TARGET_FORMAT_EXE
-"  .tag  :\n"
-#else
-"  .tag 0 :\n"
-#endif
+"{\n";
+
+const static char LDSCRIPT_PART2A[] = 
 "  {\n"
 "    *(.tag.*)\n"
-"  } =0x90909090\n"
-#ifdef TARGET_FORMAT_EXE
-"  . = SEGMENT_START(\"text-segment\", 0x80000000);\n"
-"  .text  :\n"
-#else
-"  .text 0 :\n"
-#endif
+"  }";
+
+const static char LDSCRIPT_PART2B[] = 
 "  {\n"
 "    *(.aros.startup)\n"
-"    *(.text)\n"
+"    *(.text)\n";
+
+static const char LDSCRIPT_PART3A[] =
 "    *(.text.*)\n"
 "    *(.stub)\n"
 "    /* .gnu.warning sections are handled specially by elf32.em.  */\n"
 "    *(.gnu.warning)\n"
 "    *(.gnu.linkonce.t.*)\n"
-"  } =0x90909090\n"
-"\n"
-#ifdef TARGET_FORMAT_EXE
-"  . = SEGMENT_START(\"rodata-segment\", . );\n"
-"  .rodata   :\n"
-#else
-"  .rodata  0 :\n"
-#endif
+"  }";
+
+static const char LDSCRIPT_PART3B[] =
 "  {\n"
 "    *(.rodata)\n"
 "    *(.rodata.*)\n"
 "    *(.gnu.linkonce.r.*)\n"
 "    . = ALIGN(0x10);\n";
 
-
-static const char LDSCRIPT_PART2[] =
-"  }\n"
-#ifdef TARGET_FORMAT_EXE
-"  .rodata1  : { *(.rodata1) }\n"
-#else
-"  .rodata1 0 : { *(.rodata1) }\n"
-#endif
+static const char LDSCRIPT_PART4A[] =
 "\n"
 #ifdef TARGET_FORMAT_EXE
 "  . = DATA_SEGMENT_ALIGN(0x1000,0x1000);\n"
@@ -65,82 +48,55 @@ static const char LDSCRIPT_PART2[] =
 "\n"
 "     NOTE: these should go one after the other one, so some tricks\n"
 "           must be used in the ELF loader to satisfy that requirement\n"
-"  */\n"
-#ifdef TARGET_FORMAT_EXE
-"  .sdata2   : { *(.sdata2) *(.sdata2.*) *(.gnu.linkonce.s2.*) }\n"
-"  .sbss2    : { *(.sbss2)  *(.sbss2.*)  *(.gnu.linkonce.sb2.*) }\n"
-"\n"
-"  .data   :\n"
-#else
-"  .sdata2  0 : { *(.sdata2) *(.sdata2.*) *(.gnu.linkonce.s2.*) }\n"
-"  .sbss2   0 : { *(.sbss2)  *(.sbss2.*)  *(.gnu.linkonce.sb2.*) }\n"
-"\n"
-"  .data  0 :\n"
-#endif
+"  */\n";
+
+static const char LDSCRIPT_PART4B[] =
 "  {\n"
 "    *(.data)\n"
 "    *(.data.*)\n"
 "    *(.gnu.linkonce.d.*)\n"
-"  }\n"
-#ifdef TARGET_FORMAT_EXE
-"  .data1             : { *(.data1) }\n"
-#else
-"  .data1            0 : { *(.data1) }\n"
-#endif
+"  }\n";
+
+static const char LDSCRIPT_PART4C[] =
 "  /* ARM-specific exception stuff */\n"
 "  .ARM.extab   : { *(.ARM.extab* .gnu.linkonce.armextab.*) }\n"
 "   PROVIDE(__exidx_start = .);\n"
 "  .ARM.exidx   : { *(.ARM.exidx* .gnu.linkonce.armexidx.*) }\n"
-"   PROVIDE(__exidx_end = .);\n"
-#ifdef TARGET_FORMAT_EXE
-"  .eh_frame          :\n"
-#else
-"  .eh_frame         0 :\n"
-#endif
+"   PROVIDE(__exidx_end = .);\n";
+
+static const char LDSCRIPT_PART4D[] =
 "  {\n"
 "     PROVIDE(__eh_frame_start = .);\n"
 "     KEEP (*(.eh_frame))\n";
 
-static const char LDSCRIPT_PART3[] =
+static const char LDSCRIPT_PART5A[] =
 "     PROVIDE(__eh_frame_end = .);\n"
-"  }\n"
-#ifdef TARGET_FORMAT_EXE
-"  .gcc_except_table  : { *(.gcc_except_table) }\n"
-#else
-"  .gcc_except_table 0 : { *(.gcc_except_table) }\n"
-#endif
+"  }\n";
+
+static const char LDSCRIPT_PART5B[] =
 "\n"
 "  /* We want the small data sections together, so single-instruction offsets\n"
 "     can access them all, and initialized data all before uninitialized, so\n"
-"     we can shorten the on-disk segment size.  */\n"
-#ifdef TARGET_FORMAT_EXE
-"  .sdata    :\n"
-#else
-"  .sdata   0 :\n"
-#endif
+"     we can shorten the on-disk segment size.  */\n";
+
+static const char LDSCRIPT_PART5C[] =
 "  {\n"
 "    *(.sdata)\n"
 "    *(.sdata.*)\n"
 "    *(.gnu.linkonce.s.*)\n"
 "  }\n"
-"\n"
-#ifdef TARGET_FORMAT_EXE
-"  .sbss  :\n"
-#else
-"  .sbss 0 :\n"
-#endif
+"\n";
+
+static const char LDSCRIPT_PART5D[] =
 "  {\n"
 "    *(.sbss)\n"
 "    *(.sbss.*)\n"
 "    *(.gnu.linkonce.sb.*)\n"
 "    *(.scommon)\n"
 "  }\n"
-"\n"
-#ifdef TARGET_FORMAT_EXE
-"  .bss  :\n"
-#else
-"  .bss 0 :\n"
-#endif
+"\n";
+
+static const char LDSCRIPT_PART5E[] =
 "  {\n"
 "   PROVIDE(__bss_start = .);\n"
 "   *(.bss)\n"
@@ -156,5 +112,5 @@ static const char LDSCRIPT_PART3[] =
 #endif
 "  /DISCARD/ : { *(.note.GNU-stack) }\n";
 
-static const char LDSCRIPT_PART4[] =
+static const char LDSCRIPT_PART6[] =
 "}\n";
