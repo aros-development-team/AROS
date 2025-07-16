c22a20ccf14741aeff552fac3395d0459515b17e
Kalamatee <kalamatee@gmail.com>
2025-07-16 20:37:46 +0100

    only define, if it isnt already defined.

--- compiler/crt/stdc/include/aros/types/size_t.h
+++ compiler/crt/stdc/include/aros/types/size_t.h
@@ -2,13 +2,15 @@
 #define _AROS_TYPES_SIZE_T_H
 
 /*
-    Copyright © 2010-2020, The AROS Development Team. All rights reserved.
+    Copyright © 2010-2025, The AROS Development Team. All rights reserved.
     $Id$
 
     size_t
 */
 
+#ifndef __SIZE_TYPE__
 #define __SIZE_TYPE__  long unsigned int
+#endif
 typedef __SIZE_TYPE__ size_t;
 
 #endif /* _AROS_TYPES_SIZE_T_H */
