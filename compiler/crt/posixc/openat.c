6243544cf81d2581393746f7479167df9d94cc20
Kalamatee <kalamatee@gmail.com>
2025-07-18 20:03:44 +0100

    forgotten commit

---
+++ compiler/crt/posixc/openat.c
@@ -0,0 +1,16 @@
+/*
+    Copyright © 2025, The AROS Development Team.
+    All rights reserved.
+
+    POSIX.1-2008 function fstatat
+*/
+
+#include <aros/debug.h>
+
+#include <sys/stat.h>
+
+int openat(int dirfd, const char *path, int flags, ...)
+{
+    AROS_FUNCTION_NOT_IMPLEMENTED("posixc");
+	return 0;
+}
