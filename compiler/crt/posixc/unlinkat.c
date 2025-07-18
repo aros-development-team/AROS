5fc770b08f5412848209b7addf1530ecd7427b65
Kalamatee <kalamatee@gmail.com>
2025-07-18 23:57:07 +0100

    add unlinkat stub

---
+++ compiler/crt/posixc/unlinkat.c
@@ -0,0 +1,16 @@
+/*
+    Copyright © 2025, The AROS Development Team.
+    All rights reserved.
+
+    POSIX.1-2008 function fstatat
+*/
+
+#include <aros/debug.h>
+
+#include <unistd.h>
+
+int unlinkat(int dirfd, const char *pathname, int flags)
+{
+    AROS_FUNCTION_NOT_IMPLEMENTED("posixc");
+	return 0;
+}
