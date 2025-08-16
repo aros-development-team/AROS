bc76c7c214b281c3140dae894ea301bf5cc04c8b
Kalamatee <kalamatee@gmail.com>
2025-08-16 13:55:15 +0100

    separate-out the "render" function

--- workbench/libs/cgfx/gfxfuncsupport.h
+++ workbench/libs/cgfx/gfxfuncsupport.h
@@ -1,11 +1,18 @@
 /*
-    Copyright (C) 1995-2017, The AROS Development Team. All rights reserved.
+    Copyright (C) 1995-2025, The AROS Development Team. All rights reserved.
 */
 
 #include <hidd/gfx.h>
 #include <cybergraphx/cybergraphics.h>
 
 #include "cybergraphics_intern.h"
+
+struct extcol_render_data
+{
+    struct BitMap      *destbm;
+    HIDDT_Pixel         pixel;
+    struct IntCGFXBase *CyberGfxBase;
+};
 
 UBYTE GetRectFmtBytesPerPixel(UBYTE rectfmt, struct RastPort *rp,
     struct Library *CyberGfxBase);
