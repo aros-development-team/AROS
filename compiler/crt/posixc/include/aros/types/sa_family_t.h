9775057ff896b03a3c8131817353cfea6185a368
Kalamatee <kalamatee@gmail.com>
2025-09-28 20:48:54 +0100

    define sa_family_t

---
+++ compiler/crt/posixc/include/aros/types/sa_family_t.h
@@ -0,0 +1,15 @@
+#ifndef _AROS_TYPES_SA_FAMILY_T_H
+#define _AROS_TYPES_SA_FAMILY_T_H
+
+/*
+    Copyright © 2025, The AROS Development Team. All rights reserved.
+    $Id$
+
+    sa_family_t type definition
+*/
+
+#include <aros/cpu.h>
+
+typedef unsigned AROS_8BIT_TYPE sa_family_t;
+
+#endif /* _AROS_TYPES_SA_FAMILY_T_H */
