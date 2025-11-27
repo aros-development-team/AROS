17047a8422b8ea99af5939bf9a21ff729976bbd6
Kalamatee <kalamatee@gmail.com>
2025-11-27 23:05:26 +0000

    let some options be disabled from the boot command line.

--- rom/usb/poseidon/usbromearlystartup.c
+++ rom/usb/poseidon/usbromearlystartup.c
@@ -6,10 +6,15 @@
 
 #include <aros/asmcall.h>
 #include <aros/debug.h>
+#include <aros/bootloader.h>
 #include <aros/symbolsets.h>
 #include <exec/resident.h>
+
+#include <proto/bootloader.h>
 #include <proto/poseidon.h>
 #include <proto/exec.h>
+
+#include <string.h>
 
 int __startup usbromstartup_entry(void)
 {
@@ -40,7 +45,11 @@ const struct Resident usbEarlyResident __attribute__((used)) =
 };
 
 static const char name[] = "Poseidon Early ROM Init";
-static const char version[] = "$VER:Poseidon ROM Early-Startup v41.2";
+static const char version[] = "$VER:Poseidon ROM Early-Startup v41.3";
+
+#define USBROMSTART_FLAG_NOUSB  (1 << 0)
+#define USBROMSTART_FLAG_NOUSB3 (1 << 1)
+#define USBROMSTART_FLAG_NOBOOTMS (1 << 2)
 
 AROS_UFH3(static IPTR, usbromstartup_early,
     AROS_UFHA(ULONG, dummy, D0),
@@ -49,81 +58,94 @@ AROS_UFH3(static IPTR, usbromstartup_early,
 {
     AROS_USERFUNC_INIT
 
+    APTR BootLoaderBase;
     struct Library *ps;
     struct PsdHardware *phw;
-    ULONG cnt = 0;
+    ULONG bootflags = 0;
 
     D(bug("[USBROMStartup] %s: Loading poseidon...\n", __func__));
+    BootLoaderBase = OpenResource("bootloader.resource");
+    if (BootLoaderBase) {
+        struct List *args = GetBootInfo(BL_Args);
+        if (args) {
+            struct Node *node;
+            ForeachNode(args, node) {
+                if (strncmp(node->ln_Name, "nousb3", 6) == 0) {
+                    bootflags |= USBROMSTART_FLAG_NOUSB3;
+                    continue;
+                } 
+                if (strncmp(node->ln_Name, "nousb", 5) == 0) {
+                    bootflags |= USBROMSTART_FLAG_NOUSB;
+                    continue;
+                }
+                if (strncmp(node->ln_Name, "nobootms", 8) == 0) {
+                    bootflags |= USBROMSTART_FLAG_NOBOOTMS;
+                }
+            }
+        }
+    }
 
-    if((ps = OpenLibrary("poseidon.library", 4)))
-    {
-        APTR msdclass;
+    if ((!(bootflags & USBROMSTART_FLAG_NOUSB)) &&
+          (ps = OpenLibrary("poseidon.library", 4))) {
+        APTR msdclass = NULL;
         IPTR usecount = 0;
         ULONG bootdelay = 4;
+        ULONG cnt = 0;
 
         D(bug("[USBROMStartup] %s: Adding early ROM classes...\n", __func__));
 
-        psdAddClass("hubss.class", 0);
+        if (!(bootflags & USBROMSTART_FLAG_NOUSB3))
+            psdAddClass("hubss.class", 0);
         psdAddClass("hub.class", 0);
-        msdclass = psdAddClass("massstorage.class", 0);
+        if (!(bootflags & USBROMSTART_FLAG_NOBOOTMS))
+            msdclass = psdAddClass("massstorage.class", 0);
 
-        /* now this finds all usb hardware pci cards */
-        while((phw = psdAddHardware("pciusb.device", cnt)))
-        {
+        /* Find available usb hardware */
+        while((phw = psdAddHardware("pciusb.device", cnt))) {
             D(bug("[USBROMStartup] %s: Added pciusb.device unit %u\n", __func__, cnt));
-
             psdEnumerateHardware(phw);
             cnt++;
         }
-
+#if (1)
         if (cnt == 0) {
             /* now this finds all other usb hardware pci cards */
-            while((phw = psdAddHardware("ehci.device", cnt)))
-            {
+            while((phw = psdAddHardware("ehci.device", cnt))) {
                 D(bug("[USBROMStartup] %s: Added ehci.device unit %u\n", __func__, cnt));
-
                 psdEnumerateHardware(phw);
                 cnt++;
             }
             cnt = 0;
-            while((phw = psdAddHardware("ohci.device", cnt)))
-            {
+            while((phw = psdAddHardware("ohci.device", cnt))) {
                 D(bug("[USBROMStartup] %s: Added ohci.device unit %u\n", __func__, cnt));
-
                 psdEnumerateHardware(phw);
                 cnt++;
             }
             cnt = 0;
-            while((phw = psdAddHardware("uhci.device", cnt)))
-            {
+            while((phw = psdAddHardware("uhci.device", cnt))) {
                 D(bug("[USBROMStartup] %s: Added uhci.device unit %u\n", __func__, cnt));
-
                 psdEnumerateHardware(phw);
                 cnt++;
             }
         }
-
+#endif
         D(bug("[USBROMStartup] %s: Binding classes...\n", __func__));
         psdClassScan();
 
-        if(msdclass)
-        {
+        if(msdclass) {
             psdDelayMS(1000); // wait for hubs to settle
             psdGetAttrs(PGA_USBCLASS, msdclass, UCA_UseCount, &usecount, TAG_END);
-            if(usecount > 0)
-            {
+            if(usecount > 0) {
                 psdAddErrorMsg(RETURN_OK, (STRPTR)name,
                                "Delaying further execution by %ld second(s) (boot delay).",
                                bootdelay);
-                if(bootdelay > 1)
-                {
+                if(bootdelay > 1) {
                     psdDelayMS((bootdelay-1)*1000);
                 }
             } else {
-                psdAddErrorMsg(RETURN_OK, (STRPTR)name, "Boot delay skipped, no mass storage devices found.");
+                psdAddErrorMsg(RETURN_OK, (STRPTR)name,
+                               "Boot delay skipped, no mass storage devices found.");
             }
         }
-
         CloseLibrary(ps);
     }
     return 0;
