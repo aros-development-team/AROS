95acb31c282f88e19a2998fe82f5de9fbaed020a
deadwood <deadwood@wp.pl>
2026-05-05 13:00:34 +0200

    Update copyright years

--- workbench/devs/monitors/IntelGMA/intelgma_init.c
+++ workbench/devs/monitors/IntelGMA/intelgma_init.c
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2010-2017, The AROS Development Team. All rights reserved.
+    Copyright (C) 2010-2026, The AROS Development Team. All rights reserved.
 */
 
 #include <aros/debug.h>
