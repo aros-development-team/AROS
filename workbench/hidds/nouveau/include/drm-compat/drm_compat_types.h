07b59bd9192348919a5873c6798d5d486af34965
deadwood <deadwood@wp.pl>
2026-05-13 12:08:55 +0200

    Fix ALIGN macro to process compound attributes and follow operators order

--- workbench/hidds/nouveau/include/drm-compat/drm_compat_types.h
+++ workbench/hidds/nouveau/include/drm-compat/drm_compat_types.h
@@ -91,7 +91,7 @@ struct page
 #define PAGE_SIZE               ((1UL) << PAGE_SHIFT)
 #define PAGE_MASK               (~(PAGE_SIZE-1))
 #define PAGE_ALIGN(addr)        (typeof(addr))(((IPTR)(addr) + PAGE_SIZE - 1) & PAGE_MASK)
-#define ALIGN(val, align)       (val + align - 1) & (~(align - 1))
+#define ALIGN(val, align)       (((val) + (align) - 1) & (~((align) - 1)))
 #define IS_ALIGNED(x, n)        ((x & ((typeof(x))(n) - 1)) == 0)
 #define __aligned(x)            __attribute__((__alligned__(x)))
 
