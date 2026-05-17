46ab9d0a8ee37d7b328343e5ccb628952abb0e09
deadwood <deadwood@wp.pl>
2026-05-17 15:52:17 +0200

    Implement nouveau_init() for new drm codes.

--- workbench/hidds/nouveau/drm-aros/drm_drv.c
+++ workbench/hidds/nouveau/drm-aros/drm_drv.c
@@ -5,47 +5,12 @@
 #include <aros/debug.h>
 
 #include <drm-compat/drm_compat_funcs.h>
+#include <drm-compat/drm_compat_pci.h>
 #include <drm-compat/drm_compat_mem.h>
+#include <drm-aros/drm_aros_pci.h>
 #include <uapi/drm/drm.h>
 #include <drm/drm_drv.h>
 #include <drm/drm_print.h>
-
-// int drm_lastclose(struct drm_device * dev)
-// {
-//     if (dev->driver->lastclose)
-//         dev->driver->lastclose(dev);
-    
-//     if (dev->irq_enabled)
-//         drm_irq_uninstall(dev);
-    
-//     return 0;
-// }
-
-// static void drm_cleanup(struct drm_device * dev)
-// {
-//     drm_lastclose(dev);
-    
-//     if (dev->driver->unload)
-//         dev->driver->unload(dev);
-        
-//     if (drm_core_has_AGP(dev) && dev->agp)
-//     {
-//         kfree(dev->agp);
-//         dev->agp = NULL;
-//     }
-// }
-
-// void drm_exit(struct drm_driver * driver)
-// {
-//     drm_cleanup(driver->dev);
-    
-//     drm_aros_pci_shutdown(driver);
-    
-//     HIDDNouveauFree(driver->dev->pdev);
-//     HIDDNouveauFree(driver->dev);
-//     driver->dev = NULL;
-//     current_drm_driver = NULL;
-// }
 
 int drm_gem_init(struct drm_device *dev);
 
@@ -57,23 +22,10 @@ static int drm_dev_init(struct drm_device *dev, struct drm_driver *driver,
     dev->dev_private = NULL;
     dev->irq_enabled = 0;
 
-
     dev->driver_features = ~0u;
 
     // /* Init fields */
-    // INIT_LIST_HEAD(&dev->maplist);
     mutex_init(&dev->struct_mutex);
-    // int ret;
-
-    // if (drm_core_has_AGP(dev)) {
-    //     if (drm_device_is_agp(dev))
-    //         dev->agp = drm_agp_init(dev);
-    //     if (drm_core_check_feature(dev, DRIVER_REQUIRE_AGP)
-    //         && (dev->agp == NULL)) {
-    //         DRM_ERROR("Cannot initialize the agpgart module.\n");
-    //         return -1;
-    //     }
-    // }
     
     if (drm_core_check_feature(dev, DRIVER_GEM)) {
         if (drm_gem_init(dev)) {
@@ -82,21 +34,7 @@ static int drm_dev_init(struct drm_device *dev, struct drm_driver *driver,
             return -1;
         }
     }
-    
-    // if (!dev->driver->load)
-    //     return -1;
 
-    // ret = dev->driver->load(dev, 0);
-    // if (ret)
-    //     return -1;
-
-    // if (dev->driver->firstopen)
-    // {
-    //     ret = dev->driver->firstopen(dev);
-    //     if (ret)
-    //         return -1;
-    // }
-    
     return 0;
 }
 
@@ -119,23 +57,25 @@ struct drm_device *drm_dev_alloc(struct drm_driver *driver,
 	return dev;
 }
 
-// int drm_aros_init(struct drm_driver * driver)
-// {
-//     if (drm_aros_pci_init(driver))
-//         return -1;
+int nouveau_drm_probe(struct pci_dev *pdev, const struct pci_device_id *pent, struct drm_device **pdrm_dev);
+struct drm_device *current_drm_device;
 
-//     if (drm_aros_pci_find_supported_video_card(driver))
-//         return -1;
+int nouveau_init()
+{
+    struct pci_dev *pdev;
 
-//     bug("\003\n"); /* Tell vga text mode debug output to die */
+    if (drm_aros_pci_init())
+        return -1;
 
-//     if (drm_init_device(driver))
-//     {
-//         drm_exit(driver);
-//         return -1;
-//     }
+    pdev = drm_aros_pci_find_supported_video_card();
+
+    if (!pdev)
+        return -1;
+
+    // bug("\003\n"); /* Tell vga text mode debug output to die */
+
+    if (nouveau_drm_probe(pdev, NULL, &current_drm_device))
+        return -1;
     
-//     current_drm_driver = driver;
-    
-//     return 0;
-// }
+    return 0;
+}
