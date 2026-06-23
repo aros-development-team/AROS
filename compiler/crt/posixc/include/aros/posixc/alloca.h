5c78c7d52db439367c774f87e51e31eeef8550e3
Nick Andrews <kalamatee@gmail.com>
2026-06-24 00:36:54 +0100

    use __AROS_ENABLE_ALLOCA__ to enable alloca so that we don’t leak the GNU stdio extensions

--- compiler/crt/posixc/include/aros/posixc/alloca.h
+++ compiler/crt/posixc/include/aros/posixc/alloca.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 1995-2025, The AROS Development Team. All rights reserved.
+    Copyright (C) 1995-2026, The AROS Development Team. All rights reserved.
 
     UNIX header file <alloca.h>
     It is not part of POSIX or C99 but we provide it for legacy applications.
@@ -9,10 +9,20 @@
 #define _POSIXC_ALLOCA_H
 
 /*
- * This header is only available if explicitly requested.
- * It is not part of ISO C or POSIX, so we check for the appropriate feature macros.
+ * This header is only available if explicitly requested. It is not part of
+ * ISO C or POSIX, so it is exposed for the GNU/BSD/SVID feature sets, or when
+ * a translation unit opts in explicitly via the private __AROS_ENABLE_ALLOCA__
+ * flag. The private flag lets internal headers (e.g. amiga.lib's slow-stack
+ * varargs helpers) pull in alloca() without enabling a whole feature set such
+ * as _GNU_SOURCE, which would otherwise leak the GNU stdio extensions
+ * (dprintf()/getline()/...) into the including translation unit.
  */
-#if defined(_GNU_SOURCE) || defined(_BSD_SOURCE) || defined(_SVID_SOURCE)
+#if !defined(__AROS_ENABLE_ALLOCA__) && \
+    (defined(_GNU_SOURCE) || defined(_BSD_SOURCE) || defined(_SVID_SOURCE))
+#  define __AROS_ENABLE_ALLOCA__
+#endif
+
+#if defined(__AROS_ENABLE_ALLOCA__)
 
 #include <aros/system.h>
 #include <aros/types/size_t.h>
