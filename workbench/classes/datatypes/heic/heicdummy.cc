c2bc68d77c0aa4e04feff35cf1e80195e49f0beb
Nick Andrews <kalamatee@gmail.com>
2026-08-02 15:25:08 +0100

    workbench/classes/datatypes/heic: provide boost::throw_exception handler
    With BOOST_NO_EXCEPTIONS (the boost::optional fallback path built
    -fno-exceptions, e.g. arm), boost requires the application to supply
    the handler it calls instead of throwing.

--- workbench/classes/datatypes/heic/heicdummy.cc
+++ workbench/classes/datatypes/heic/heicdummy.cc
@@ -1,4 +1,22 @@
 /*
-    Copyright © 2020, The AROS Development Team. All rights reserved.
+    Copyright © 2020-2026, The AROS Development Team. All rights reserved.
     $Id$
 */
+
+/* When libheif is built against boost::optional (pre-C++17 toolchains)
+ * on a target compiled with -fno-exceptions, boost defines
+ * BOOST_NO_EXCEPTIONS and expects the application to provide the
+ * boost::throw_exception() handler it calls instead of throwing.
+ */
+#if defined(LIBHEIF_USE_BOOST_OPTIONAL) && !defined(__EXCEPTIONS)
+#include <exception>
+#include <cstdlib>
+
+namespace boost
+{
+    void throw_exception(std::exception const &e)
+    {
+        std::abort();
+    }
+}
+#endif
