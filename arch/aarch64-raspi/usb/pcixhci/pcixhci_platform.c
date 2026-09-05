c9d0c3243061225f61c30d1ea9600b2abbd17627
Bo Ståle Kopperud <bsek@bsek.org>
2026-09-05 16:02:33 +0200

    aarch64-raspi: complete the RP1 xHCI platform probe
    The controllers were registered without their transfer queues, DMA
    offset or interrupt, all of which rp1usb.resource now provides.

--- arch/aarch64-raspi/usb/pcixhci/pcixhci_platform.c
+++ arch/aarch64-raspi/usb/pcixhci/pcixhci_platform.c
@@ -5,35 +5,56 @@
     Raspberry Pi platform xHCI discovery override for pcixhci.device
 */
 
+/* Bring-up diagnostics: pciusbDebug() needs DEBUG > 0 in this unit. */
+#define DEBUG 1
+
 #include <exec/types.h>
 #include <exec/memory.h>
 #include <proto/exec.h>
+#include <proto/rp1usb.h>
 #include <aros/symbolsets.h>
 
 #include "pcixhci.h"
 
 void pcixhci_probe_platform(struct PCIDevice *hd)
 {
-    struct Library *rp1usb = OpenResource("rp1usb.resource");
-    if (rp1usb) {
-        IPTR *fields = (IPTR *)((UBYTE *)rp1usb + sizeof(struct Library));
-        if (fields[2]) { /* present */
-            IPTR bases[2] = { fields[0], fields[1] }; /* usb0, usb1 */
-            int i;
-            for (i = 0; i < 2; i++) {
-                if (bases[i]) {
-                    struct PCIController *hc = AllocPooled(hd->hd_MemPool, sizeof(struct PCIController));
-                    if (hc) {
-                        hc->hc_Device = hd;
-                        hc->hc_RegBase = (volatile APTR)bases[i];
-                        hc->hc_Flags = HCF_PLATFORM;
-                        hc->hc_FunctionNum = i;
-                        hc->hc_PCIIntLine = 0;
-                        hc->hc_DevID = 0x1DE40001 + i;
-                        AddTail(&hd->hd_TempHCIList, &hc->hc_Node);
-                        pciusbDebug("PCI", "RP1 platform xHCI%d @ 0x%p\n", i, (APTR)bases[i]);
-                    }
-                }
+    struct Library *RP1USBBase = OpenResource("rp1usb.resource");
+
+    if (RP1USBBase)
+    {
+        IPTR dmaoff = RP1USBGetDMAOffset();
+        int i;
+
+        for (i = 0; i < 2; i++)
+        {
+            IPTR base = RP1USBGetBase(i);
+
+            if (!base)
+                continue;
+            
+            struct PCIController *hc = AllocPooled(hd->hd_MemPool, sizeof(struct PCIController));
+            if (hc) {
+                hc->hc_Device = hd;
+                hc->hc_RegBase = (volatile APTR)base;
+                hc->hc_Flags = HCF_PLATFORM;
+                hc->hc_FunctionNum = i;
+                /* A GIC INTID, not a PCI interrupt line.  0 if MSI is down. */
+                hc->hc_PCIIntLine = RP1USBGetIRQ(i);
+                hc->hc_DevID = 0x1DE40001 + i;
+                hc->hc_PlatformDMAOffset = dmaoff;
+                hc->hc_IsoPTDCount = PCIUSB_ISO_PTD_COUNT;
+
+                NewList(&hc->hc_PeriodicTDQueue);
+                NewList(&hc->hc_CtrlXFerQueue);
+                NewList(&hc->hc_IntXFerQueue);
+                NewList(&hc->hc_IsoXFerQueue);
+                NewList(&hc->hc_BulkXFerQueue);
+                NewList(&hc->hc_TDQueue);
+                NewList(&hc->hc_AbortQueue);
+                NewMinList(&hc->hc_RTIsoHandlers);
+
+                AddTail(&hd->hd_TempHCIList, &hc->hc_Node);
+                pciusbDebug("PCI", "RP1 platform xHCI%d @ 0x%p\n", i, (APTR)base);
             }
         }
     }
