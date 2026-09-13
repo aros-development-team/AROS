4e89a33fb19a6ddad1aba03d074f05cb7d92336b
Andreas W. Just <299255+cyberman@users.noreply.github.com>
2026-09-13 22:30:47 +0200

    Shell: accept signed .key /N values

--- workbench/c/Shell/cliNan.c
+++ workbench/c/Shell/cliNan.c
@@ -1,11 +1,18 @@
 /*
-    Copyright (C) 1995-2010, The AROS Development Team. All rights reserved.
+    Copyright (C) 1995-2026, The AROS Development Team. All rights reserved.
  */
 
 #include "Shell.h"
 
 BOOL cliNan(CONST_STRPTR s)
 {
+    if (*s == '+' || *s == '-')
+    {
+        ++s;
+        if (*s == '\0')
+            return TRUE;
+    }
+
     for (; *s != '\0'; ++s)
         if (*s < '0' || *s > '9')
             return TRUE;
